169ddb12ad17cebf162d6da26beb32b174ffc211
Scott Taylor <wonderboymusic@git.wordpress.org>
2013-09-11 21:37:46 +0000

    Set `$_SERVER['REQUEST_METHOD'] = null` in `Tests_Auth::setUp()` to suppress undefined index notices when `wp_validate_auth_cookie()` is called.
    See #25282.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@25363 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/auth.php
+++ tests/phpunit/tests/auth.php
@@ -9,6 +9,8 @@ class Tests_Auth extends WP_UnitTestCase {
 	function setUp() {
 		parent::setUp();
 		$this->user_id = $this->factory->user->create();
+
+		$_SERVER['REQUEST_METHOD'] = null;
 	}
 
 	function test_auth_cookie_valid() {
