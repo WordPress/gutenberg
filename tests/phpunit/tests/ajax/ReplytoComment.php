402e61f2694edc928a35e3bc88348a69f6c0e43a
Scott Taylor <wonderboymusic@git.wordpress.org>
2013-09-13 22:17:51 +0000

    Fix some undefined index notices related to Comment unit tests: * There are several places where a `$_POST` index was unchecked before setting a variable * In `wp_notify_postauthor()`, `$comment` was being returned null, but its properties were being accessed. * In `check_ajax_referer()`, 3 different values can be checked for nonce on `$_REQUEST`, but only 1 had an `isset()`
    See #25282.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@25433 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/ajax/ReplytoComment.php
+++ tests/phpunit/tests/ajax/ReplytoComment.php
@@ -38,6 +38,8 @@ class Tests_Ajax_ReplytoComment extends WP_Ajax_UnitTestCase {
 
 		$post_id = $this->factory->post->create( array( 'post_status' => 'draft' ) );
 		$this->_draft_post = get_post( $post_id );
+
+		$_SERVER['REMOTE_ADDR'] = '';
 	}
 
 	/**
