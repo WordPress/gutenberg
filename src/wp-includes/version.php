c773d253ecd5b0ab4305ef94bf825d8cca48a351
Andrew Nacin <nacin@git.wordpress.org>
2013-10-22 22:09:18 +0000

    3.7-RC2
    git-svn-id: https://develop.svn.wordpress.org/trunk@25875 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.7-RC1-25851-src';
+$wp_version = '3.7-RC2-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
