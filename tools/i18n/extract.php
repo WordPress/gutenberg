54e6261a34c440bbc50c16e6fb2067d471dd6e30
Nikolay Bachiyski <nbachiyski@git.wordpress.org>
2013-10-26 00:46:34 +0000

    Extract: `_` and `_c` aren't among the common gettext function names
    Let's just remove them from the default rules
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@25919 602fd350-edb4-49c9-b593-d223f7449a82

--- tools/i18n/extract.php
+++ tools/i18n/extract.php
@@ -10,10 +10,8 @@ require_once "$pomo/translations.php";
 class StringExtractor {
 
 	var $rules = array(
-		'_' => array( 'string' ),
 		'__' => array( 'string' ),
 		'_e' => array( 'string' ),
-		'_c' => array( 'string' ),
 		'_n' => array( 'singular', 'plural' ),
 	);
 	var $comment_prefix = 'translators:';
