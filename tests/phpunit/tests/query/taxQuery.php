0923b7b93aa625720f7cad2350a17ea66b098c8d
Scott Taylor <wonderboymusic@git.wordpress.org>
2013-11-04 23:53:55 +0000

    Category and tag are typically checked before checking for a custom taxonomy. If the global query matches category or tag (even if it also has tax_query set), return category/tag as the queried object, instead of arbitrarily returning the first term in the `tax_query` stack (typically those added with 'pre_get_posts').
    Real world example: http://www.emusic.com/17dots/topics/daily-download/ - "tag" page, regionalized for US-only content using `pre_get_posts` passing in the terms "US" and "ALL" for "region" (custom tax). All of the theme functions would output "ALL" as the term name. Even though it was a tag archive, the queried object was an arbitrary term from `tax_query`.
    
    See [26006]. All unit tests pass.
    Fixes #20767.
    
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@26007 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/query/taxQuery.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * Test the is_*() functions in query.php across the URL structure
+ *
+ * This exercises both query.php and rewrite.php: urls are fed through the rewrite code,
+ * then we test the effects of each url on the wp_query object.
+ *
+ * @group query
+ * @group rewrite
+ */
+class Tests_Query_TaxQuery extends WP_UnitTestCase {
+	protected $tag_id;
+	protected $cat_id;
+	protected $tax_id;
+	protected $tax_id2;
+	protected $post_id;
+
+	protected $cat;
+	protected $tag;
+	protected $tax;
+
+	function setUp() {
+		parent::setUp();
+
+		set_current_screen( 'front' );
+
+		update_option( 'permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
+
+		$GLOBALS['wp_the_query'] = new WP_Query();
+		$GLOBALS['wp_query'] = $GLOBALS['wp_the_query'];
+
+		create_initial_taxonomies();
+		register_taxonomy( 'testtax', 'post', array( 'public' => true ) );
+
+		$GLOBALS['wp_rewrite']->init();
+		flush_rewrite_rules();
+
+		$this->tag_id = $this->factory->tag->create( array( 'slug' => 'tag-slug' ) );
+		$this->cat_id = $this->factory->category->create( array( 'slug' => 'cat-slug' ) );
+		$this->tax_id = $this->factory->term->create( array( 'taxonomy' => 'testtax', 'slug' => 'tax-slug' ) );
+		$this->tax_id2 = $this->factory->term->create( array( 'taxonomy' => 'testtax', 'slug' => 'tax-slug2' ) );
+		$this->post_id = $this->factory->post->create();
+		wp_set_object_terms( $this->post_id, $this->cat_id, 'category' );
+		wp_set_object_terms( $this->post_id, array( $this->tax_id, $this->tax_id2 ), 'testtax' );
+
+		$this->cat = get_term( $this->cat_id, 'category' );
+		_make_cat_compat( $this->cat );
+		$this->tag = get_term( $this->tag_id, 'post_tag' );
+
+		add_action( 'pre_get_posts', array( $this, 'pre_get_posts_tax_category_tax_query' ) );
+	}
+
+	function tearDown() {
+		parent::tearDown();
+
+		_unregister_taxonomy( 'testtax' );
+
+		remove_action( 'pre_get_posts', array( $this, 'pre_get_posts_tax_category_tax_query' ) );
+	}
+
+	function test_tag_action_tax() {
+		// tag with tax added
+		$this->go_to( home_url( "/tag/tag-slug/" ) );
+		$this->assertQueryTrue( 'is_tag', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertNotEmpty( get_query_var( 'tag_id' ) );
+		$this->assertEquals( get_queried_object(), $this->tag );
+	}
+
+	function test_tag_query_cat_action_tax() {
+		// tag + category with tax added
+		$this->go_to( home_url( "/tag/tag-slug/?cat=$this->cat_id" ) );
+		$this->assertQueryTrue( 'is_category', 'is_tag', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertNotEmpty( get_query_var( 'cat' ) );
+		$this->assertNotEmpty( get_query_var( 'tag_id' ) );
+		$this->assertEquals( get_queried_object(), $this->cat );
+	}
+
+	function test_tag_query_cat_query_tax_action_tax() {
+		// tag + category + tax with tax added
+		$this->go_to( home_url( "/tag/tag-slug/?cat=$this->cat_id&testtax=tax-slug2" ) );
+		$this->assertQueryTrue( 'is_category', 'is_tag', 'is_tax', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertNotEmpty( get_query_var( 'cat' ) );
+		$this->assertNotEmpty( get_query_var( 'tag_id' ) );
+		$this->assertNotEmpty( get_query_var( 'testtax' ) );
+		$this->assertEquals( get_queried_object(), $this->cat );
+	}
+
+	function test_cat_action_tax() {
+		// category with tax added
+		$this->go_to( home_url( "/category/cat-slug/" ) );
+		$this->assertQueryTrue( 'is_category', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'cat' ) );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertEquals( get_queried_object(), $this->cat );
+	}
+
+	function test_tax_query_tag_action_tax() {
+		// tax + tag with tax added
+		$this->go_to( home_url( "/testtax/tax-slug2/?tag_id=$this->tag_id" ) );
+		$this->assertQueryTrue( 'is_tag', 'is_tax', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertNotEmpty( get_query_var( 'tag_id' ) );
+		$this->assertEquals( get_queried_object(), $this->tag );
+	}
+
+	function test_tax_query_cat_action_tax() {
+		// tax + cat with tax added
+		$this->go_to( home_url( "/testtax/tax-slug2/?cat=$this->cat_id" ) );
+		$this->assertQueryTrue( 'is_category', 'is_tax', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tax_query' ) );
+		$this->assertNotEmpty( get_query_var( 'taxonomy' ) );
+		$this->assertNotEmpty( get_query_var( 'term_id' ) );
+		$this->assertNotEmpty( get_query_var( 'cat' ) );
+		$this->assertEquals( get_queried_object(), $this->cat );
+	}
+
+	function pre_get_posts_tax_category_tax_query( &$query ) {
+		$query->set( 'tax_query', array(
+			array( 'taxonomy' => 'testtax', 'field' => 'term_id', 'terms' => $this->tax_id )
+		) );
+	}
+}
