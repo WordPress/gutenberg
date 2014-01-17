9f5c78b676413b3224996a4d786a533c2df24db8
Andrew Nacin <nacin@git.wordpress.org>
2014-01-17 07:46:33 +0000

    Unit tests for get_url_in_content(). Return false when no content is passed, to match the return value of no links being found.
    props mdbitz.
    #26171.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@26972 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/formatting/GetUrlInContent.php
@@ -0,0 +1,49 @@
+﻿<?php
+
+/**
+ * @group formatting
+ */
+class Tests_Formatting_GetUrlInContent extends WP_UnitTestCase {
+	
+	/**
+	 * URL Content Data Provider
+	 *
+	 * array ( input_txt, converted_output_txt )
+	 */
+	public function get_input_output() {
+		return array (
+			array (
+				"",
+				false
+			), //empty content
+			array (
+				"<div>NO URL CONTENT</div>",
+				false
+			), //no URLs
+			array (
+				'<div href="/relative.php">NO URL CONTENT</div>',
+				false
+			), // ignore none link elements
+			array (
+				'ABC<div><a href="/relative.php">LINK</a> CONTENT</div>',
+				"/relative.php"
+			), // single link
+			array (
+				'ABC<div><a href="/relative.php">LINK</a> CONTENT <a href="/suppress.php">LINK</a></div>',
+				"/relative.php"
+			), // multiple links
+			array (
+				'ABC<div><a href="http://example.com/Mr%20WordPress 2">LINK</a> CONTENT </div>',
+				"http://example.com/Mr%20WordPress2"
+			), // escape link
+		);
+	}
+
+	/**
+	 * Validate the get_url_in_content function
+	 * @dataProvider get_input_output
+	 */
+	function test_get_url_in_content( $in_str, $exp_str ) {
+		$this->assertEquals($exp_str, get_url_in_content( $in_str ) );
+	}
+}
