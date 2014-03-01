8a617607a713541f1f4632a3148b60ac9385241e
Andrew Nacin <nacin@git.wordpress.org>
2014-03-01 21:44:43 +0000

    Strip backslashes, not just forward slashes, from untrailingslashit().
    trailingslashit() will now remove any forward or backslashes from the end of a string before appending a forward slash.
    
    props knutsp, willmot.
    fixes #22267.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@27344 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/formatting/Slashit.php
+++ tests/phpunit/tests/formatting/Slashit.php
@@ -21,11 +21,34 @@ class Tests_Formatting_Slashit extends WP_UnitTestCase {
 		$this->assertEquals("a", untrailingslashit("a////"));
 	}
 
+	/**
+	 * @ticket 22267
+	 */
+	function test_removes_trailing_backslashes() {
+		$this->assertEquals("a", untrailingslashit("a\\"));
+		$this->assertEquals("a", untrailingslashit("a\\\\\\\\"));
+	}
+
+	/**
+	 * @ticket 22267
+	 */
+	function test_removes_trailing_mixed_slashes() {
+		$this->assertEquals("a", untrailingslashit("a/\\"));
+		$this->assertEquals("a", untrailingslashit("a\\/\\///\\\\//"));
+	}
+
 	function test_adds_trailing_slash() {
 		$this->assertEquals("a/", trailingslashit("a"));
 	}
 
 	function test_does_not_add_trailing_slash_if_one_exists() {
 		$this->assertEquals("a/", trailingslashit("a/"));
+	}
+
+	/**
+	 * @ticket 22267
+	 */
+	function test_converts_trailing_backslash_to_slash_if_one_exists() {
+		$this->assertEquals("a/", trailingslashit("a\\"));
 	}
 }
