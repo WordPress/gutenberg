1f5eec43cda0755d64d76c43e50e3d8850677a61
Andrew Nacin <nacin@git.wordpress.org>
2014-03-11 07:53:15 +0000

    A nice round commit number for a beta 1.
    git-svn-id: https://develop.svn.wordpress.org/trunk@27500 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.9-alpha-27445-src';
+$wp_version = '3.9-beta1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
