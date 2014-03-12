36df368171520550ea3aa13ae483fe46c1a5a481
Scott Taylor <wonderboymusic@git.wordpress.org>
2014-03-12 04:07:41 +0000

    In `WP_Query::get_queried_object()`, account for `pre_get_posts` by checking for `tag` when `tag_id` isn't present. Tags still need to be rolled up into `tax_query`. Add a unit test confirming expected query vars during and after `pre_get_posts`.
    Props mattonomics for a patch.
    See #27362.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@27511 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/query.php
+++ tests/phpunit/tests/query.php
@@ -104,4 +104,35 @@ class Tests_Query extends WP_UnitTestCase {
 	function filter_posts_per_page( &$query ) {
 		$query->set( 'posts_per_rss', 30 );
 	}
+
+	/**
+	 * @ticket 26627
+	 */
+	function test_tag_queried_object() {
+		$slug = 'tag-slug-26627';
+		$this->factory->tag->create( array( 'slug' => $slug ) );
+		$tag = get_term_by( 'slug', $slug, 'post_tag' );
+
+		add_action( 'pre_get_posts', array( $this, '_tag_queried_object' ), 11 );
+
+		$this->go_to( get_term_link( $tag ) );
+
+		$this->assertQueryTrue( 'is_tag', 'is_archive' );
+		$this->assertNotEmpty( get_query_var( 'tag_id' ) );
+		$this->assertNotEmpty( get_query_var( 'tag' ) );
+		$this->assertEmpty( get_query_var( 'tax_query' ) );
+		$this->assertCount( 1, get_query_var( 'tag_slug__in' ) );
+		$this->assertEquals( get_queried_object(), $tag );
+
+		remove_action( 'pre_get_posts', array( $this, '_tag_queried_object' ), 11 );
+	}
+
+	function _tag_queried_object( &$query ) {
+		$tag = get_term_by( 'slug', 'tag-slug-26627', 'post_tag' );
+		$this->assertTrue( $query->is_tag() );
+		$this->assertTrue( $query->is_archive() );
+		$this->assertNotEmpty( $query->get( 'tag' ) );
+		$this->assertCount( 1, $query->get( 'tag_slug__in' ) );
+		$this->assertEquals( $query->get_queried_object(), $tag );
+	}
 }
