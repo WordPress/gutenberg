3e54a1fab513a891f11d33dcb9aca9e84450ad7d
Scott Taylor <wonderboymusic@git.wordpress.org>
2014-05-15 03:50:46 +0000

    Eliminate use of `extract()` in `get_the_taxonomies()`. Adds unit test.
    See #22400.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@28415 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/taxonomy.php
+++ tests/phpunit/tests/taxonomy.php
@@ -33,6 +33,22 @@ class Tests_Taxonomy extends WP_UnitTestCase {
 		}
 	}
 
+	function test_get_the_taxonomies() {
+		$post_id = $this->factory->post->create();
+
+		$taxes = get_the_taxonomies( $post_id );
+		$this->assertNotEmpty( $taxes );
+		$this->assertEquals( array( 'category' ), array_keys( $taxes ) );
+
+		$id = $this->factory->tag->create();
+		wp_set_post_tags( $post_id, array( $id ) );
+
+		$taxes = get_the_taxonomies( $post_id );
+		$this->assertNotEmpty( $taxes );
+		$this->assertCount( 2, $taxes );
+		$this->assertEquals( array( 'category', 'post_tag' ), array_keys( $taxes ) );
+	}
+
 	function test_get_link_taxonomy() {
 		foreach ( get_object_taxonomies('link') as $taxonomy ) {
 			$tax = get_taxonomy($taxonomy);
