f393fecbc56d8dead53b229ead9a2ec2b4b97364
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2014-06-22 01:40:38 +0000

    Add @ticket references.
    see #16854, #19198.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@28800 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/query/results.php
+++ tests/phpunit/tests/query/results.php
@@ -414,7 +414,10 @@ class Tests_Query_Results extends WP_UnitTestCase {
 		$this->assertNotEmpty( $posts3 );
 	}
 
-	function test_exlude_from_search_empty() {
+	/**
+	 * @ticket 19198
+	 */
+	function test_exclude_from_search_empty() {
 		global $wp_post_types;
 		foreach ( array_keys( $wp_post_types ) as $slug )
 			$wp_post_types[$slug]->exclude_from_search = true;
@@ -433,6 +436,9 @@ class Tests_Query_Results extends WP_UnitTestCase {
 		$this->assertNotRegExp( '#AND 1=0#', $this->q->request );
 	}
 
+	/**
+	 * @ticket 16854
+	 */
 	function test_query_author_vars() {
 		$author_1 = $this->factory->user->create( array( 'user_login' => 'admin1', 'user_pass' => rand_str(), 'role' => 'author' ) );
 		$post_1 = $this->factory->post->create( array( 'post_title' => rand_str(), 'post_author' => $author_1, 'post_date' => '2007-01-01 00:00:00' ) );
