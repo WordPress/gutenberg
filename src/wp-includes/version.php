32953775cd2d9217d00da43347a39c04f05555dc
Helen Hou-Sandi <helen@git.wordpress.org>
2014-08-27 06:13:09 +0000

    4.0-RC1.
    git-svn-id: https://develop.svn.wordpress.org/trunk@29647 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0-beta4-29589-src';
+$wp_version = '4.0-RC1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
