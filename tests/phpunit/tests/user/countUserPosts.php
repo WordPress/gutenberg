3544e20e9a0326a923fe5a9d3d326cc460de163b
Boone Gorges <boonebgorges@git.wordpress.org>
2014-11-13 02:18:30 +0000

    Introduce `$post_type` param for `count_user_posts()`.
    Props Caspie, engelen, DrewAPicture.
    Fixes #21364.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@30322 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/user/countUserPosts.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @group user
+ * @group post
+ */
+class Tests_User_CountUserPosts extends WP_UnitTestCase {
+	static $user_id;
+	static $post_ids = array();
+
+	public static function setUpBeforeClass() {
+		$factory = new WP_UnitTest_Factory();
+
+		self::$user_id = $factory->user->create( array(
+			'role' => 'author',
+			'user_login' => 'count_user_posts_user',
+		) );
+
+		self::$post_ids = $factory->post->create_many( 4, array(
+			'post_author' => self::$user_id,
+			'post_type'   => 'post',
+		) );
+		self::$post_ids = array_merge( self::$post_ids, $factory->post->create_many( 3, array(
+			'post_author' => self::$user_id,
+			'post_type'   => 'wptests_pt',
+		) ) );
+		self::$post_ids = array_merge( self::$post_ids, $factory->post->create_many( 2, array(
+			'post_author' => 12345,
+			'post_type'   => 'wptests_pt',
+		) ) );
+		self::$post_ids = array_merge( self::$post_ids, $factory->post->create_many( 1, array(
+			'post_author' => 12345,
+			'post_type'   => 'wptests_pt',
+		) ) );
+
+		self::commit_transaction();
+	}
+
+	public static function tearDownAfterClass() {
+		if ( is_multisite() ) {
+			wpmu_delete_user( self::$user_id );
+		} else {
+			wp_delete_user( self::$user_id );
+		}
+
+		foreach ( self::$post_ids as $post_id ) {
+			wp_delete_post( $post_id, true );
+		}
+
+		self::commit_transaction();
+	}
+
+	public function setUp() {
+		parent::setUp();
+		register_post_type( 'wptests_pt' );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		_unregister_post_type( 'wptests_pt' );
+	}
+
+	public function test_count_user_posts_post_type_should_default_to_post() {
+		$this->assertEquals( 4, count_user_posts( self::$user_id ) );
+	}
+
+	/**
+	 * @ticket 21364
+	 */
+	public function test_count_user_posts_post_type_post() {
+		$this->assertEquals( 4, count_user_posts( self::$user_id, 'post' ) );
+	}
+
+	/**
+	 * @ticket 21364
+	 */
+	public function test_count_user_posts_post_type_cpt() {
+		$this->assertEquals( 3, count_user_posts( self::$user_id, 'wptests_pt' ) );
+	}
+}
