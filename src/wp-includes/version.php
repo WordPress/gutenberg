9aa0e31b28c570373b422d23e0ee2a7be9964aca
John Blackbourn <johnbillion@git.wordpress.org>
2014-11-20 16:52:54 +0000

    WordPress 4.1-beta2
    git-svn-id: https://develop.svn.wordpress.org/trunk@30481 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.1-beta1-30411-src';
+$wp_version = '4.1-beta2-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
