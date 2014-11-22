e102a22f80d2db57be8c84a5d221965439db62ec
Boone Gorges <boonebgorges@git.wordpress.org>
2014-11-22 20:36:51 +0000

    Remove failing `is_textdomain_loaded()` test.
    The removed test has been added as a patch to the original ticket, #21319.
    
    Props MikeHansenMe.
    See #30284.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@30521 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/l10n.php
+++ tests/phpunit/tests/l10n.php
@@ -16,16 +16,4 @@ class Tests_L10n extends WP_UnitTestCase {
 		$this->assertTrue( unload_textdomain( 'wp-tests-domain' ) );
 		$this->assertFalse( is_textdomain_loaded( 'wp-tests-domain' ) );
 	}
-
-	/**
-	 * @ticket 21319
-	 */
-	function test_is_textdomain_loaded_for_no_translations() {
-		$this->assertFalse( load_textdomain( 'wp-tests-domain', DIR_TESTDATA . '/non-existent-file' ) );
-		$this->assertFalse( is_textdomain_loaded( 'wp-tests-domain' ) );
-		$this->assertInstanceOf( 'NOOP_Translations', get_translations_for_domain( 'wp-tests-domain' ) );
-		// Ensure that we don't confuse NOOP_Translations to be a loaded text domain.
-		$this->assertFalse( is_textdomain_loaded( 'wp-tests-domain' ) );
-		$this->assertFalse( unload_textdomain( 'wp-tests-domain' ) );
-	}
 }
