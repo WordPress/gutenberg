b7bcd40bac44f21839e9dbee9d8dfeb1e8564740
Boone Gorges <boonebgorges@git.wordpress.org>
2014-11-23 01:22:15 +0000

    Remove failing `test_network_limit()` XML-RPC test.
    Test added as a patch on #21292.
    
    Props MikeHansenMe.
    See #30284.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@30530 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/xmlrpc/wp/uploadFile.php
+++ tests/phpunit/tests/xmlrpc/wp/uploadFile.php
@@ -28,30 +28,4 @@ class Tests_XMLRPC_wp_uploadFile extends WP_XMLRPC_UnitTestCase {
 		$this->assertInternalType( 'string', $result['url'] );
 		$this->assertInternalType( 'string', $result['type'] );
 	}
-
-	/**
-	 * @ticket 21292
-	 */
-	function test_network_limit() {
-		$this->make_user_by_role( 'editor' );
-
-		update_option( 'blog_upload_space', 0.1 );
-
-		// create attachment
-		$filename = ( DIR_TESTDATA . '/images/canola.jpg' );
-		$contents = file_get_contents( $filename );
-		$data = array(
-			'name' => 'canola.jpg',
-			'type' => 'image/jpeg',
-			'bits' => $contents
-		);
-
-		$result = $this->myxmlrpcserver->mw_newMediaObject( array( 0, 'editor', 'editor', $data ) );
-
-		// Only multisite should have a limit
-		if ( is_multisite() )
-			$this->assertInstanceOf( 'IXR_Error', $result );
-		else
-			$this->assertNotInstanceOf( 'IXR_Error', $result );
-	}
 }
