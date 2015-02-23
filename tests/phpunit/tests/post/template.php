0421ee26ecf6042f5f03cea2b25e4122c574d54a
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2015-02-23 18:47:01 +0000

    Add unit tests for `get_page_template_slug()`.
    props tyxla.
    fixes #31389.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@31522 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/post/template.php
+++ tests/phpunit/tests/post/template.php
@@ -197,4 +197,49 @@ NO;
 
 		$this->assertContains( 'value="' . $p . '"', $found );
 	}
+
+	/**
+	 * @ticket 31389
+	 */
+	public function test_get_page_template_slug_by_id() {
+		$page_id = $this->factory->post->create( array(
+			'post_type' => 'page',
+		) );
+
+		$this->assertEquals( '', get_page_template_slug( $page_id ) );
+
+		update_post_meta( $page_id, '_wp_page_template', 'default' );
+		$this->assertEquals( '', get_page_template_slug( $page_id ) );
+
+		update_post_meta( $page_id, '_wp_page_template', 'example.php' );
+		$this->assertEquals( 'example.php', get_page_template_slug( $page_id ) );
+	}
+
+	/**
+	 * @ticket 31389
+	 */
+	public function test_get_page_template_slug_from_loop() {
+		$page_id = $this->factory->post->create( array(
+			'post_type' => 'page',
+		) );
+
+		update_post_meta( $page_id, '_wp_page_template', 'example.php' );
+		$this->go_to( get_permalink( $page_id ) );
+
+		$this->assertEquals( 'example.php', get_page_template_slug() );
+	}
+
+	/**
+	 * @ticket 31389
+	 */
+	public function test_get_page_template_slug_non_page() {
+		$post_id = $this->factory->post->create( array(
+			'post_type' => 'post',
+		) );
+
+		$this->assertFalse( get_page_template_slug( $post_id ) );
+
+		$this->go_to( get_permalink( $post_id ) );
+		$this->assertFalse( get_page_template_slug() );
+	}
 }
