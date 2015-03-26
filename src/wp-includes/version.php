e72c793502beb53e978380acb1c3fed6c2187d7c
Drew Jaynes <drewapicture@git.wordpress.org>
2015-03-26 18:14:37 +0000

    WordPress 4.2-beta3
    git-svn-id: https://develop.svn.wordpress.org/trunk@31902 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2-beta2-31866-src';
+$wp_version = '4.2-beta3-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
