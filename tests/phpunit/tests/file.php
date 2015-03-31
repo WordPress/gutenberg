842d6a9b3984e802d0759e85f82890f3ff0350b1
Dion Hulse <dd32@git.wordpress.org>
2015-03-31 02:10:06 +0000

    Add some logic into `wp_tempnam` to prevent it creating 'falsey' directory names that might get used elsewhere within WordPress. Although this logic looks a little strange at this low level, it's the best location within the Upgrades code for it to happen. Fixes #31811
    git-svn-id: https://develop.svn.wordpress.org/trunk@31936 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/file.php
+++ tests/phpunit/tests/file.php
@@ -152,4 +152,24 @@ class Tests_File extends WP_UnitTestCase {
 		unlink($this->dir . DIRECTORY_SEPARATOR . $filename);
 	}
 
+	/**
+	 * @dataProvider data_wp_tempnam_filenames
+	 */
+	function test_wp_tempnam( $case ) {
+		$file = wp_tempnam( $case );
+		unlink( $file );
+
+		$this->assertNotEmpty( basename( basename( $file, '.tmp' ), '.zip' ) );
+	}
+	function data_wp_tempnam_filenames() {
+		return array(
+			array( '0.zip' ),
+			array( '0.1.2.3.zip' ),
+			array( 'filename.zip' ),
+			array( 'directory/0.zip' ),
+			array( 'directory/filename.zip' ),
+			array( 'directory/0/0.zip' ),
+		);
+	}
+
 }
