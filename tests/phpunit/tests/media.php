7dd8a1364aa5dc1d9aba77e8f1f4cdb350f36d17
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-06-16 00:34:06 +0000

    In the `youtube_embed_url` embed handler, make `embed` a non-capturing group that alternately matches for `v` - YouTube supports both URL paths.
    Add unit test cases.
    
    Props dmchale for some patch work.
    Fixes #32161.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@32787 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/media.php
+++ tests/phpunit/tests/media.php
@@ -120,8 +120,6 @@ CAP;
 	 * @ticket 23149
 	 */
 	function test_youtube_com_secure_embed() {
-		global $wp_embed;
-
 		$out = wp_oembed_get( 'http://www.youtube.com/watch?v=oHg5SJYRHA0' );
 		$this->assertContains( 'https://www.youtube.com/embed/oHg5SJYRHA0?feature=oembed', $out );
 
@@ -130,6 +128,18 @@ CAP;
 
 		$out = wp_oembed_get( 'https://youtu.be/zHjMoNQN7s0' );
 		$this->assertContains( 'https://www.youtube.com/embed/zHjMoNQN7s0?feature=oembed', $out );
+	}
+
+	function test_youtube_embed_url() {
+		global $wp_embed;
+		$out = $wp_embed->autoembed( 'https://www.youtube.com/embed/QcIy9NiNbmo' );
+		$this->assertContains( 'https://youtube.com/watch?v=QcIy9NiNbmo', $out );
+	}
+
+	function test_youtube_v_url() {
+		global $wp_embed;
+		$out = $wp_embed->autoembed( 'https://www.youtube.com/v/QcIy9NiNbmo' );
+		$this->assertContains( 'https://youtube.com/watch?v=QcIy9NiNbmo', $out );
 	}
 
 	/**
