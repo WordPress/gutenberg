3b17ffb99029c5667f7e08a6101b2ebf45c6e968
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-06-22 21:25:19 +0000

    In `wp.media.view.UploaderStatus.filename()`, don't truncate the filename.
    Props helen.
    Fixes #31216.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@32908 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/js/media/views/uploader/status.js
+++ src/wp-includes/js/media/views/uploader/status.js
@@ -106,7 +106,7 @@ UploaderStatus = View.extend({
 	 * @returns {string}
 	 */
 	filename: function( filename ) {
-		return wp.media.truncate( _.escape( filename ), 24 );
+		return _.escape( filename );
 	},
 	/**
 	 * @param {Backbone.Model} error
