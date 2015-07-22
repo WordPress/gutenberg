b850d9d9f1906b5f80b843c87076755a3f5acb3b
Konstantin Obenland <obenland@git.wordpress.org>
2015-07-22 21:50:50 +0000

    4.3-beta4
    git-svn-id: https://develop.svn.wordpress.org/trunk@33370 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-beta3-33288-src';
+$wp_version = '4.3-beta4-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
