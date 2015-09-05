dde7dc7145bbaa4b1e4335bb41f29bb0fec021c1
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2015-09-05 14:17:24 +0000

    Update `Tests_Sanitize_Option::test_sanitize_option()` after [33858].
    See #23605.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@33907 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/option/sanitize-option.php
+++ tests/phpunit/tests/option/sanitize-option.php
@@ -70,7 +70,7 @@ class Tests_Sanitize_Option extends WP_UnitTestCase {
 			array( 'timezone_string', 'Europe/London', 'Europe/London' ),
 			array( 'timezone_string', get_option( 'timezone_string' ), 'invalid' ),
 			array( 'permalink_structure', '', '' ),
-			array( 'permalink_structure', '/%year%/%postname%', '/%year%/ %postname%' ),
+			array( 'permalink_structure', '/%year%/%20%postname%', '/%year%/ %postname%' ),
 			array( 'default_role', 'subscriber', 'subscriber' ),
 			array( 'default_role', 'subscriber', 'invalid' ),
 			array( 'default_role', 'editor', 'editor' ),
