504deb52fce61046959c816aeb9d646fc86eac0c
Boone Gorges <boonebgorges@git.wordpress.org>
2015-09-17 20:00:31 +0000

    Lazy-load comment meta on single post pages.
    [34268] introduced cache priming for commentmeta, enabled by default. To
    ensure performance on single post pages - where commentmeta is most likely
    to cause performance issues - we disable up-front cache-priming. Instead, we
    prime commentmeta caches for all comments in the loop the first time
    `get_comment_meta()` is called on the page.
    
    Props bradt, dd32, wonderboymusic, boonebgorges.
    Fixes #16894.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@34270 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/comment/metaCache.php
+++ tests/phpunit/tests/comment/metaCache.php
@@ -81,61 +81,38 @@ class Tests_Comment_Meta_Cache extends WP_UnitTestCase {
 		$this->assertSame( $num_queries + 3, $wpdb->num_queries );
 	}
 
-	public function test_comment_meta_cache() {
-		$post_id = $this->factory->post->create( array(
-			'post_status' => 'publish'
-		) );
+	/**
+	 * @ticket 16894
+	 */
+	public function test_comment_meta_should_be_lazy_loaded_for_all_comments_in_comments_template() {
+		global $wpdb;
 
-		$comment_ids = $this->factory->comment->create_post_comments( $post_id, 10 );
+		$p = $this->factory->post->create( array( 'post_status' => 'publish' ) );
+		$comment_ids = $this->factory->comment->create_post_comments( $p, 3 );
 
 		foreach ( $comment_ids as $cid ) {
 			update_comment_meta( $cid, 'sauce', 'fire' );
 		}
 
-		$post = get_post( $post_id );
+		$this->go_to( get_permalink( $p ) );
 
-		$this->assertEquals( $post->comment_count, count( $comment_ids ) );
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
 
-		$this->go_to( get_permalink( $post_id ) );
+				// Load comments with `comments_template()`.
+				$cform = get_echo( 'comments_template' );
 
-		$this->assertTrue( is_single() );
-		$this->assertTrue( have_posts() );
+				// First request will hit the database.
+				$num_queries = $wpdb->num_queries;
+				get_comment_meta( $comment_ids[0], 'sauce' );
+				$this->assertSame( $num_queries + 1, $wpdb->num_queries );
 
-		global $wp_query;
-
-		while ( have_posts() ) {
-			the_post();
-
-			$comment_args = array(
-				'order'   => 'ASC',
-				'orderby' => 'comment_date_gmt',
-				'status'  => 'approve',
-				'post_id' => get_the_ID(),
-			);
-
-			$comments = get_comments( $comment_args );
-
-			// This is beyond awful
-			$wp_query->comments = $comments;
-
-			wp_list_comments( array(
-				'echo' => false,
-				'callback' => array( $this, '_comment_callback' )
-			) );
+				// Second and third requests should be in cache.
+				get_comment_meta( $comment_ids[1], 'sauce' );
+				get_comment_meta( $comment_ids[2], 'sauce' );
+				$this->assertSame( $num_queries + 1, $wpdb->num_queries );
+			}
 		}
-	}
-
-	public function _comment_callback( $comment ) {
-		global $wpdb;
-
-		get_comment_meta( $comment->comment_ID, 'sauce' );
-
-		if ( 0 === $this->i ) {
-			$this->queries = $wpdb->num_queries;
-		} else {
-			$this->assertEquals( $this->queries, $wpdb->num_queries );
-		}
-
-		$this->i++;
 	}
 }
