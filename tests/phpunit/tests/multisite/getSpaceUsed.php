b415937e4a1f999b218420d4502c67067d6b5cf9
John Blackbourn <johnbillion@git.wordpress.org>
2015-09-24 20:32:49 +0000

    Another missed file from [34172].
    See #33877
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@34518 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/multisite/getSpaceUsed.php
+++ tests/phpunit/tests/multisite/getSpaceUsed.php
@@ -13,8 +13,6 @@ class Tests_Multisite_Get_Space_Used extends WP_UnitTestCase {
 		global $wpdb;
 		parent::setUp();
 		$this->suppress = $wpdb->suppress_errors();
-
-		$_SERVER['REMOTE_ADDR'] = '';
 	}
 
 	function tearDown() {
