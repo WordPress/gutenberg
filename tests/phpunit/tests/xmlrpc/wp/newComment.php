a9e30d8e94634dbc94f90ac92dd79ea9e3aa1173
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-09-25 20:19:19 +0000

    XML-RPC: Introduce the concept of unit testing to `wp_xmlrpc_server::wp_newComment()`:
    * Don't allow comments to be created for posts that have `comment_status` set to `'closed'`
    * Set some magic props on `WP_User` to vars before passing them to `wp_xmlrpc_server::escape()`
    
    Props wonderboymusic, jesin.
    Fixes #27471.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@34559 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/xmlrpc/wp/newComment.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @group xmlrpc
+ */
+class Tests_XMLRPC_wp_newComment extends WP_XMLRPC_UnitTestCase {
+	function test_new_comment_post_closed() {
+		$this->make_user_by_role( 'administrator' );
+		$post = $this->factory->post->create_and_get( array(
+			'comment_status' => 'closed'
+		) );
+
+		$this->assertEquals( 'closed', $post->comment_status );
+
+		$result = $this->myxmlrpcserver->wp_newComment( array( 1, 'administrator', 'administrator', $post->ID, array(
+			'comment_content' => rand_str( 100 ),
+			'status' => 'approved'
+		) ) );
+
+		$this->assertInstanceOf( 'IXR_Error', $result );
+		$this->assertEquals( 403, $result->code );
+	}
+}
