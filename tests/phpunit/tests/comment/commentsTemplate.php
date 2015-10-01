f14dc03ad6ec9578cf51eb4587291794d6e23e0e
Boone Gorges <boonebgorges@git.wordpress.org>
2015-10-01 03:10:13 +0000

    Simplify pagination logic in `comments_template()`.
    [34561] "fixed" the problem of newest-first comments showing fewer than
    'per_page' comments on the post permalink when the total number of comments
    was not divisible by 'per_page'. See #29462. But this fix caused numerous
    other problems. First, comment pages reported by `get_page_of_comment()`
    (which expects comment pages to be filled oldest-first) were no longer correct.
    Second, and more seriously, the new logic caused comments to be shifted
    between pages, making their permalinks non-permanent.
    
    The current changeset reverts the changed behavior. In order to preserve the
    performance improvements introduced in [34561], an additional query must be
    performed when 'default_comments_page=newest' and 'cpage=0' (ie, you're viewing
    the post permalink). A nice side effect of this revert is that we no longer
    need the hacks required to determine proper comment pagination, introduced in
    [34561].
    
    See #8071. See #34073.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@34729 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/comment/commentsTemplate.php
+++ tests/phpunit/tests/comment/commentsTemplate.php
@@ -325,4 +325,88 @@ class Tests_Comment_CommentsTemplate extends WP_UnitTestCase {
 		$found_cids = array_map( 'intval', $matches[1] );
 		$this->assertSame( array( $comment_1, $comment_2 ), $found_cids );
 	}
+
+	/**
+	 * @ticket 8071
+	 * @ticket 34073
+	 * @ticket 29462
+	 */
+	public function test_last_page_of_comments_should_be_full_when_default_comment_page_is_newest() {
+		$now = time();
+		$p = $this->factory->post->create();
+		$comment_1 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '1',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 100 ),
+		) );
+		$comment_2 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '2',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 200 ),
+		) );
+		$comment_3 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '3',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 300 ),
+		) );
+
+		update_option( 'default_comments_page', 'newest' );
+		update_option( 'comment_order', 'desc' );
+
+		$link = add_query_arg( array(
+			'cpage' => 1,
+			'comments_per_page' => 2,
+		), get_permalink( $p ) );
+
+		$this->go_to( $link );
+		$found = get_echo( 'comments_template' );
+
+		$comments = preg_match_all( '/id="comment-([0-9]+)"/', $found, $matches );
+
+		$found_cids = array_map( 'intval', $matches[1] );
+
+		$this->assertSame( array( $comment_2, $comment_3 ), $found_cids );
+	}
+
+	/**
+	 * @ticket 8071
+	 * @ticket 34073
+	 * @ticket 29462
+	 */
+	public function test_first_page_of_comments_should_have_remainder_when_default_comments_page_is_newest() {
+		$now = time();
+		$p = $this->factory->post->create();
+		$comment_1 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '1',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 100 ),
+		) );
+		$comment_2 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '2',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 200 ),
+		) );
+		$comment_3 = $this->factory->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '3',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 300 ),
+		) );
+
+		update_option( 'default_comments_page', 'newest' );
+		update_option( 'comment_order', 'desc' );
+
+		$link = add_query_arg( array(
+			'cpage' => 2,
+			'comments_per_page' => 2,
+		), get_permalink( $p ) );
+
+		$this->go_to( $link );
+		$found = get_echo( 'comments_template' );
+
+		$comments = preg_match_all( '/id="comment-([0-9]+)"/', $found, $matches );
+
+		$found_cids = array_map( 'intval', $matches[1] );
+
+		$this->assertSame( array( $comment_1 ), $found_cids );
+	}
 }
