6f80581b80a4cbdd2e34ecf72c5b9478ef939755
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-10-15 00:22:50 +0000

    Unit Tests: move `->test_readme()` out of `Tests_Basic` and into `Tests_External_HTTP_Basic` in `tests/external-http/`.
    I intend to move other `wp_remote_get()` tests into similar classes.
    
    See #30017, #33968.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@35172 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/external-http/basic.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @group external-http
+ */
+class Tests_External_HTTP_Basic extends WP_UnitTestCase {
+
+	function test_readme() {
+		$readme = file_get_contents( ABSPATH . 'readme.html' );
+		preg_match( '#<br /> Version (.*)#', $readme, $matches );
+		list( $version ) = explode( '-', $GLOBALS['wp_version'] );
+		$this->assertEquals( $version, trim( $matches[1] ), "readme.html's version needs to be updated to $version." );
+
+		preg_match( '#Recommendations.*PHP</a> version <strong>([0-9.]*)#s', $readme, $matches );
+
+		$response = wp_remote_get( 'https://secure.php.net/supported-versions.php' );
+		if ( 200 != wp_remote_retrieve_response_code( $response ) ) {
+			$this->markTestSkipped( 'Could not contact PHP.net to check versions.' );
+		}
+		$php = wp_remote_retrieve_body( $response );
+
+		preg_match_all( '#<tr class="stable">\s*<td>\s*<a [^>]*>\s*([0-9.]*)#s', $php, $phpmatches );
+
+		$this->assertContains( $matches[1], $phpmatches[1], "readme.html's Recommended PHP version is too old. Remember to update the WordPress.org Requirements page, too." );
+
+		preg_match( '#Recommendations.*MySQL</a> version <strong>([0-9.]*)#s', $readme, $matches );
+
+		$response = wp_remote_get( "https://dev.mysql.com/doc/relnotes/mysql/{$matches[1]}/en/" );
+		if ( 200 != wp_remote_retrieve_response_code( $response ) ) {
+			$this->markTestSkipped( 'Could not contact dev.MySQL.com to check versions.' );
+		}
+		$mysql = wp_remote_retrieve_body( $response );
+
+		preg_match( '#(\d{4}-\d{2}-\d{2}), General Availability#', $mysql, $mysqlmatches );
+
+		// Per https://www.mysql.com/support/, Oracle actively supports MySQL releases for 5 years from GA release
+		$mysql_eol = strtotime( $mysqlmatches[1] . ' +5 years' );
+
+		$this->assertLessThan( $mysql_eol, time(), "readme.html's Recommended MySQL version is too old. Remember to update the WordPress.org Requirements page, too." );
+	}
+}
