00ee9a62d1cb2599c04b0528026a66d12c129557
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2015-11-04 16:35:02 +0000

    Add missing `@group` to `Tests_Rel_No_Follow`.
    See #9959.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@35505 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/formatting/WPRelNoFollow.php
+++ tests/phpunit/tests/formatting/WPRelNoFollow.php
@@ -1,15 +1,22 @@
 <?php
+
 /**
- * @ticket 9959
+ * @group formatting
  */
 class Tests_Rel_No_Follow extends WP_UnitTestCase {
 
+	/**
+	 * @ticket 9959
+	 */
 	public function test_add_no_follow() {
 		$content = '<p>This is some cool <a href="/">Code</a></p>';
 		$expected = '<p>This is some cool <a href=\"/\" rel=\"nofollow\">Code</a></p>';
 		$this->assertEquals( $expected, wp_rel_nofollow( $content ) );
 	}
 
+	/**
+	 * @ticket 9959
+	 */
 	public function test_convert_no_follow() {
 		$content = '<p>This is some cool <a href="/" rel="weird">Code</a></p>';
 		$expected = '<p>This is some cool <a href=\"/\" rel=\"weird nofollow\">Code</a></p>';
