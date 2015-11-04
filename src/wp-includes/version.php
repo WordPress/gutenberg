d06c31d70c3dd819d6eae864d402d60f60c3a9e2
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-11-04 21:55:58 +0000

    Bump 4.4-beta3
    git-svn-id: https://develop.svn.wordpress.org/trunk@35528 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-beta3-35525-src';
+$wp_version = '4.4-beta3-35526-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
