447673101165cb8a752d0e03026b4c4de21612bf
Scott Taylor <wonderboymusic@git.wordpress.org>
2015-11-25 23:01:35 +0000

    WordPress 4.4 RC 1 version bump
    git-svn-id: https://develop.svn.wordpress.org/trunk@35744 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4-RC1-src';
+$wp_version = '4.4-RC1-35743-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
