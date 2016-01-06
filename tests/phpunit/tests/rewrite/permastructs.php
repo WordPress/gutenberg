e055e5b80ed20864e2a9292f6a7f767337c4cd7e
Pascal Birchler <swissspidy@git.wordpress.org>
2016-01-06 07:39:29 +0000

    Rewrite: Add a `remove_permastruct()` helper function.
    It can be used to remove permastructs that were added using `add_permastruct()`.
    
    Fixes #35235.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36181 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/rewrite/permastructs.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @group rewrite
+ */
+class Tests_Rewrite_Permastructs extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->set_permalink_structure( '/%postname%/' );
+	}
+
+	public function test_add_permastruct(  ) {
+		global $wp_rewrite;
+
+		add_permastruct( 'foo', 'bar/%foo%' );
+		$this->assertEqualSets( array(
+			'with_front'  => true,
+			'ep_mask'     => EP_NONE,
+			'paged'       => true,
+			'feed'        => true,
+			'walk_dirs'   => true,
+			'endpoints'   => true,
+			'forcomments' => false,
+			'struct'      => '/bar/%foo%',
+		), $wp_rewrite->extra_permastructs['foo'] );
+	}
+
+	public function test_remove_permastruct(  ) {
+		global $wp_rewrite;
+
+		add_permastruct( 'foo', 'bar/%foo%' );
+		$this->assertInternalType( 'array', $wp_rewrite->extra_permastructs['foo'] );
+		$this->assertSame( '/bar/%foo%', $wp_rewrite->extra_permastructs['foo']['struct'] );
+
+		remove_permastruct( 'foo' );
+		$this->assertFalse( isset( $wp_rewrite->extra_permastructs['foo'] ) );
+	}
+}
