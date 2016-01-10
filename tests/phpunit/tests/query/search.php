e75a1f5ab36faf43602db58807099b5b83710aab
Boone Gorges <boonebgorges@git.wordpress.org>
2016-01-10 03:25:15 +0000

    Avoid invalid SQL when building ORDER BY clause using long search strings.
    The introduction of negative search terms in 4.4 [34934] introduced the
    possibility that the ORDER BY clause of a search query could be assembled in
    such a way as to create invalid syntax. The current changeset fixes this by
    ensuring that the ORDER BY clause corresponding to the search terms is
    excluded when it would otherwise be empty.
    
    Props salvoaranzulla.
    Fixes #35361.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36251 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/query/search.php
+++ tests/phpunit/tests/query/search.php
@@ -125,4 +125,16 @@ class Tests_Query_Search extends WP_UnitTestCase {
 
 		$this->assertEqualSets( array( $p3 ), $q->posts );
 	}
+
+	/**
+	 * @ticket 35361
+	 */
+	public function test_search_orderby_should_be_empty_when_search_string_is_longer_than_6_words_and_exclusion_operator_is_used() {
+		$q = new WP_Query( array(
+			's' => 'foo1 foo2 foo3 foo4 foo5 foo6 foo7 -bar',
+			'fields' => 'ids',
+		) );
+
+		$this->assertNotRegExp( '|ORDER BY \(CASE[^\)]+\)|', $q->request );
+	}
 }
