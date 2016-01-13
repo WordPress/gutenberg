f7d238dfe3fa09018a49d12844d6e4c7097da978
Boone Gorges <boonebgorges@git.wordpress.org>
2016-01-13 03:12:34 +0000

    Ignore hierarchy in pagination calculation when comment threading is disabled.
    In order to calculate comment pagination when newest comments are displayed
    first, `comments_template()` must perform a separate query to determine the
    total number of paginating comments available on a post. See [34729], #8071,
    pagination calculation - can be defined as a top-level comment, or a comment
    with `parent=0`. However, when comment threading is disabled, yet comments
    exist in the database that have parents, all comments - even those with a
    parent - are "paginating". (This typically happens when comments threading was
    once enabled, but has since been turned off.) As such, the total-paginating-
    comments query should only be limited to top-level comments when
    'thread_comments' is disabled.
    
    Props jmdodd.
    Fixes #35419.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36275 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/comment/commentsTemplate.php
+++ tests/phpunit/tests/comment/commentsTemplate.php
@@ -728,4 +728,47 @@ class Tests_Comment_CommentsTemplate extends WP_UnitTestCase {
 		$found_cids = array_map( 'intval', $matches[1] );
 		$this->assertSame( array( $comment_2, $comment_3, $comment_1 ), $found_cids );
 	}
+
+	/**
+	 * @ticket 35419
+	 */
+	public function test_pagination_calculation_should_ignore_comment_hierarchy_when_threading_is_disabled() {
+		$now = time();
+		$p = self::factory()->post->create();
+		$comment_1 = self::factory()->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '1',
+			'comment_approved' => '1',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 300 ),
+		) );
+		$comment_2 = self::factory()->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '2',
+			'comment_approved' => '1',
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 200 ),
+		) );
+		$comment_3 = self::factory()->comment->create( array(
+			'comment_post_ID' => $p,
+			'comment_content' => '3',
+			'comment_approved' => '1',
+			'comment_parent' => $comment_1,
+			'comment_date_gmt' => date( 'Y-m-d H:i:s', $now - 100 ),
+		) );
+
+		update_option( 'thread_comments', 0 );
+
+		update_option( 'comment_order', 'asc' );
+		update_option( 'comments_per_page', 2 );
+		update_option( 'page_comments', 1 );
+		update_option( 'default_comments_page', 'newest' );
+
+		$this->go_to( get_permalink( $p ) );
+		$found = get_echo( 'comments_template' );
+
+		// Find the found comments in the markup.
+		preg_match_all( '|id="comment-([0-9]+)|', $found, $matches );
+
+		$found_cids = array_map( 'intval', $matches[1] );
+		$this->assertSame( array( $comment_3 ), $found_cids );
+	}
 }
