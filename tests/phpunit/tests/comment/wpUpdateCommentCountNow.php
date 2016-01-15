63e442bbb8cb3e1ab8951ec4054e3f1e13f3e074
Pascal Birchler <swissspidy@git.wordpress.org>
2016-01-15 13:41:35 +0000

    Comments: Add a new `pre_wp_update_comment_count_now` filter.
    This allows filtering a post's comment count before it is queried and updated in the database.
    
    Props peterwilsoncc for initial patch.
    Fixes #35060.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36318 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/comment/wpUpdateCommentCountNow.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @group  comment
+ * @covers wp_update_comment_count_now
+ */
+class Tests_Update_Comment_Count_Now extends WP_UnitTestCase {
+	public function _return_100() {
+		return 100;
+	}
+
+	public function test_invalid_post_bails_early() {
+		$this->assertFalse( wp_update_comment_count_now( 100 ) );
+		$this->assertFalse( wp_update_comment_count_now( null ) );
+		$this->assertFalse( wp_update_comment_count_now( 0 ) );
+	}
+
+	public function test_regular_post_updates_comment_count() {
+		global $wpdb;
+
+		$post_id = self::factory()->post->create();
+
+		self::factory()->comment->create_post_comments( $post_id, 1 );
+		$this->assertSame( '1', get_comments_number( $post_id ) );
+
+		$num_queries = $wpdb->num_queries;
+		$this->assertTrue( wp_update_comment_count_now( $post_id ) );
+		$this->assertSame( $num_queries + 2, $wpdb->num_queries );
+
+		$this->assertSame( '1', get_comments_number( $post_id ) );
+	}
+
+	public function test_using_filter_adjusts_comment_count_without_an_additional_database_query() {
+		global $wpdb;
+
+		add_filter( 'pre_wp_update_comment_count_now', array( $this, '_return_100' ) );
+
+		$post_id = self::factory()->post->create();
+
+		self::factory()->comment->create_post_comments( $post_id, 1 );
+		$this->assertSame( '100', get_comments_number( $post_id ) );
+
+		$num_queries = $wpdb->num_queries;
+		$this->assertTrue( wp_update_comment_count_now( $post_id ) );
+		// Only one query is made instead of two.
+		$this->assertSame( $num_queries + 1, $wpdb->num_queries );
+
+		$this->assertSame( '100', get_comments_number( $post_id ) );
+
+		remove_filter( 'pre_wp_update_comment_count_now', array( $this, '_return_100' ) );
+	}
+}
