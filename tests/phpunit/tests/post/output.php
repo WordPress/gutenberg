3541f7b90b0a57a10458628e5dfb5a07ab49775a
Pascal Birchler <swissspidy@git.wordpress.org>
2016-01-16 10:13:27 +0000

    Posts: Correctly pass `$post` to `post_password_required()` in `get_the_excerpt()`.
    Corrects the relevant test.
    
    Props sebastian.pisula for initial patch.
    Fixes #35486.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36329 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/post/output.php
+++ tests/phpunit/tests/post/output.php
@@ -199,11 +199,14 @@ EOF;
 
 	/**
 	 * @ticket 27246
+	 * @ticket 35486
 	 */
 	public function test_the_excerpt_password_protected_post() {
-		$GLOBALS['post'] = self::factory()->post->create_and_get( array( 'post_excerpt' => 'Post excerpt', 'post_password' => '1234' ) );
+		$post = self::factory()->post->create_and_get( array( 'post_excerpt' => 'Post excerpt', 'post_password' => '1234' ) );
+		$this->assertSame( 'There is no excerpt because this is a protected post.', get_the_excerpt( $post ) );
+
+		$GLOBALS['post'] = $post;
 		$this->assertSame( "<p>There is no excerpt because this is a protected post.</p>\n", get_echo( 'the_excerpt' ) );
-		$this->assertSame( 'There is no excerpt because this is a protected post.', get_the_excerpt() );
 	}
 
 	/**
