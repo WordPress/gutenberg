70a3997552a47bdefe75d4f6020da2784ae72c2f
Stephen Edgar <stephen@netweb.com.au>
2016-01-27 20:41:42 +1100

    Merge pull request #19 from stylelint/use-ava
    Use ava for tests and Stylelint 4.x

--- packages/stylelint-config-wordpress/__tests__/index.js
+++ packages/stylelint-config-wordpress/__tests__/index.js
@@ -1,39 +1,90 @@
 import config from "../"
 import stylelint from "stylelint"
-import test from "tape"
+import test from "ava"
 
-test("basic properties of config", t => {
-  t.ok(isObject(config.rules), "rules is object")
-  t.end()
-})
-
-function isObject(obj) {
-  return typeof obj === "object" && obj !== null
+const validCss = (
+`#selector-1,
+#selector-2,
+#selector-3 {
+	background: #fff;
+	color: #000;
 }
 
-const css = (
+#comment-form {
+
+	margin: 1em 0;
+}
+
+input[type="text"] {
+	line-height: 1.1;
+}
+
+.sample-output {
+	box-shadow: inset 0 0 1px 1px #eee;
+}
+
+.class { /* Correct usage of quotes */
+	background-image: url(images/bg.png);
+	font-family: "Helvetica Neue", sans-serif;
+}
+
+.class { /* Correct usage of zero values */
+	font-family: Georgia, serif;
+	text-shadow:
+		0 -1px 0 rgba(0, 0, 0, 0.5),
+		0 1px 0 #fff;
+}
+
+@media all and (max-width: 699px) and (min-width: 520px) {
+
+	/* Your selectors */
+}
+
+/**
+* #.# Section title
+*
+* Description of section, whether or not it has media queries, etc.
+*/
+
+.selector {
+	float: left;
+}
+
+a {
+	top: 0.2em;
+}
+`)
+
+const invalidCss = (
 `a {
-\ttop: .2em;
+	top: .2em;
 }
 
 `)
 
-stylelint.lint({
-  code: css,
-  config: config,
-})
-.then(checkResult)
-.catch(function (err) {
-  console.error(err.stack)
+test("no warnings with valid css", t => {
+  return stylelint.lint({
+    code: validCss,
+    config: config,
+  })
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.notOk(errored, "no errored")
+    t.is(warnings.length, 0, "flags no warnings")
+  })
 })
 
-function checkResult(data) {
-  const { errored, results } = data
-  const { warnings } = results[0]
-  test("expected warnings", t => {
-    t.ok(errored, "errored")
-    t.equal(warnings.length, 1, "flags one warning")
-    t.equal(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
-    t.end()
+test("a warning with invalid css", t => {
+  return stylelint.lint({
+    code: invalidCss,
+    config: config,
   })
-}
+  .then(data => {
+    const { errored, results } = data
+    const { warnings } = results[0]
+    t.ok(errored, "errored")
+    t.is(warnings.length, 1, "flags one warning")
+    t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
+  })
+})
