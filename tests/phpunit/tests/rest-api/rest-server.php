10bfe70f06396cb34cc559b3a516dcc56151124b
Ryan McCue <rmccue@git.wordpress.org>
2016-02-16 05:50:21 +0000

    REST API: Allow explicit HEAD callbacks.
    HEAD callbacks can now be registered independently, with the GET
    callback still used as a fallback.
    
    Fixes #34841.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36535 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/rest-api/rest-server.php
+++ tests/phpunit/tests/rest-api/rest-server.php
@@ -139,6 +139,29 @@ class Tests_REST_Server extends WP_Test_REST_TestCase {
 	}
 
 	/**
+	 * Plugins should be able to register explicit HEAD callbacks before the
+	 * GET callback.
+	 *
+	 * @depends test_head_request_handled_by_get
+	 */
+	public function test_explicit_head_callback() {
+		register_rest_route( 'head-request', '/test', array(
+			array(
+				'methods' => array( 'HEAD' ),
+				'callback' => '__return_true',
+			),
+			array(
+				'methods' => array( 'GET' ),
+				'callback' => '__return_false',
+				'permission_callback' => array( $this, 'permission_denied' ),
+			),
+		));
+		$request = new WP_REST_Request( 'HEAD', '/head-request/test' );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+	}
+
+	/**
 	 * Pass a capability which the user does not have, this should
 	 * result in a 403 error.
 	 */
