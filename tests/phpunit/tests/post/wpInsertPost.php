c592a7262eeea0a346ce34d72b1ee1d0eb284601
Eric Andrew Lewis <ericlewis@git.wordpress.org>
2016-02-21 21:44:14 +0000

    Posts: Non-trashed posts should take slug priority over trashed posts.
    When determining a unique post slug, trashed posts are taken into account. Previously, new posts would add suffixes to their slugs (e.g. `about-2`) when a post in the trash had the desired slug (e.g. `about`). 
    
    To avoid this behavior, when a post is trashed its slug (i.e. `post_name`) is now suffixed with `-%trashed%`. The post's pre-trash slug is stored as post meta, and if the post is restored from trash, its desired slug is reapplied. 
    
    For existing trashed posts which don't have the `-%trashed%` suffix, the suffix is added when a post with its desired slug is created.
    
    Props ocean90, boonebgorges, ryan, SergeyBiryukov, coffee2code, helen, williamsba1.
    See #11863.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36607 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/post/wpInsertPost.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @group post
+ */
+class Tests_WPInsertPost extends WP_UnitTestCase {
+
+	/**
+	 * @ticket 11863
+	 */
+	function test_trashing_a_post_should_add_trashed_suffix_to_post_name() {
+		$trashed_about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'publish'
+		) );
+		wp_trash_post( $trashed_about_page_id );
+		$this->assertEquals( 'about-%trashed%', get_post( $trashed_about_page_id )->post_name );
+	}
+
+	/**
+	 * @ticket 11863
+	 */
+	function test_trashed_posts_original_post_name_should_be_reassigned_after_untrashing() {
+		$about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'publish'
+		) );
+		wp_trash_post( $about_page_id );
+
+		wp_untrash_post( $about_page_id );
+		$this->assertEquals( 'about', get_post( $about_page_id )->post_name );
+	}
+
+	/**
+	 * @ticket 11863
+	 */
+	function test_creating_a_new_post_should_add_trashed_suffix_to_post_name_of_trashed_posts_with_the_desired_slug() {
+		$trashed_about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'trash'
+		) );
+
+		$about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'publish'
+		) );
+
+		$this->assertEquals( 'about-%trashed%', get_post( $trashed_about_page_id )->post_name );
+		$this->assertEquals( 'about', get_post( $about_page_id )->post_name );
+	}
+
+	/**
+	* @ticket 11863
+	*/
+	function test_untrashing_a_post_with_a_stored_desired_post_name_should_get_its_post_name_suffixed_if_another_post_has_taken_the_desired_post_name() {
+		$about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'publish'
+		) );
+		wp_trash_post( $about_page_id );
+
+		$another_about_page_id = self::factory()->post->create( array(
+			'post_type' => 'page',
+			'post_title' => 'About',
+			'post_status' => 'publish'
+		) );
+
+		wp_untrash_post( $about_page_id );
+
+		$this->assertEquals( 'about', get_post( $another_about_page_id )->post_name );
+		$this->assertEquals( 'about-2', get_post( $about_page_id )->post_name );
+	}
+}
