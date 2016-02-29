13e409ac15bf410da956e600b556f4d7902c0365
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2016-02-29 04:41:03 +0000

    Formatting: In `sanitize_title_with_dashes()`, convert `&nbsp`, `&ndash`, and `&mdash` HTML entities to hyphens on save.
    Props polevaultweb for initial patch.
    Fixes #31790.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@36775 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/formatting/SanitizeTitleWithDashes.php
+++ tests/phpunit/tests/formatting/SanitizeTitleWithDashes.php
@@ -63,9 +63,24 @@ class Tests_Formatting_SanitizeTitleWithDashes extends WP_UnitTestCase {
 		$this->assertEquals("dont-break-the-space", sanitize_title_with_dashes("don't break the space", '', 'save'));
 	}
 
+	/**
+	 * @ticket 31790
+	 */
+	function test_replaces_nbsp_entities() {
+		$this->assertEquals("dont-break-the-space", sanitize_title_with_dashes("don't&nbsp;break&#160;the&nbsp;space", '', 'save'));
+	}
+
 	function test_replaces_ndash_mdash() {
 		$this->assertEquals("do-the-dash", sanitize_title_with_dashes("Do – the Dash", '', 'save'));
 		$this->assertEquals("do-the-dash", sanitize_title_with_dashes("Do the — Dash", '', 'save'));
+	}
+
+	/**
+	 * @ticket 31790
+	 */
+	function test_replaces_ndash_mdash_entities() {
+		$this->assertEquals("do-the-dash", sanitize_title_with_dashes("Do &ndash; the &#8211; Dash", '', 'save'));
+		$this->assertEquals("do-the-dash", sanitize_title_with_dashes("Do &mdash; the &#8212; Dash", '', 'save'));
 	}
 
 	function test_replaces_iexcel_iquest() {
