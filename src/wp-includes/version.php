007f3b456c41097473edc7a9ebaab32cf50987ab
Mike Schroder <mikeschroder@git.wordpress.org>
2016-03-17 04:33:49 +0000

    Post 4.5-beta4 version bump.
    git-svn-id: https://develop.svn.wordpress.org/trunk@37027 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.5-beta4-src';
+$wp_version = '4.5-beta4-37027-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
