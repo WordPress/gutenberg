d62141c38d307ff350da29b11bf4ba7d107d4954
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-04-30 15:08:06 +0000

    Plugins: In `plugin_basename()` normalize the file path before unresolving symlinks.
    `$wp_plugin_paths` contains normalized paths, see `wp_register_plugin_realpath()`.
    
    Props jdgrimes, voldemortensen, flyingdr, ocean90.
    Fixes #29154.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@37332 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/functions/pluginBasename.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Tests for plugin_basename()
+ *
+ * @group functions.php
+ * @group plugins
+ */
+class Tests_Plugin_Basename extends WP_UnitTestCase {
+
+	/**
+	 * @ticket 29154
+	 */
+	function test_should_return_correct_basename_for_symlinked_plugins() {
+		global $wp_plugin_paths;
+
+		$old_wp_plugin_paths = $wp_plugin_paths;
+
+		$wp_plugin_paths[ WP_PLUGIN_DIR . '/a-symlinked-plugin' ] = 'C:/www/path/plugins/a-plugin';
+
+		$basename = plugin_basename( 'c:\www\path\plugins\a-plugin\plugin.php' );
+
+		$wp_plugin_paths = $old_wp_plugin_paths;
+
+		$this->assertSame( 'a-symlinked-plugin/plugin.php', $basename );
+	}
+}
