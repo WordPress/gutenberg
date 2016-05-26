8119c1b473f2657e767c61c2c7eb4fa3b40a0d6b
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-05-26 10:05:21 +0000

    Version bump.
    git-svn-id: https://develop.svn.wordpress.org/trunk@37575 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-alpha-37438-src';
+$wp_version = '4.6-alpha-37575-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
