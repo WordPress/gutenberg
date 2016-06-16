70afec287545e76c59c1af11e86bbf52ad3e21de
Andrew Ozz <azaozz@git.wordpress.org>
2016-06-16 04:53:42 +0000

    Media: properly refresh the position of the Plupload shim so it moves over the Select Files button or off the screen. Fixes selecting files in MS Edge on the Media grid and Media -> Add New screens.
    Fixes #37039.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@37722 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/js/media/views/frame/manage.js
+++ src/wp-includes/js/media/views/frame/manage.js
@@ -189,6 +189,10 @@ Manage = MediaFrame.extend({
 	addNewClickHandler: function( event ) {
 		event.preventDefault();
 		this.trigger( 'toggle:upload:attachment' );
+
+		if ( this.uploader ) {
+			this.uploader.refresh();
+		}
 	},
 
 	/**
