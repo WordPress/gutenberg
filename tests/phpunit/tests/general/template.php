9ea1a7eb4fcb359a9c78599c9fa27cdce9f7b5d9
Peter Wilson <peterwilsoncc@git.wordpress.org>
2016-06-26 11:29:13 +0000

    Posts: Fix back-compat for filters in get the modified time and date functions after [37738].
    When no valid post exists for `get_the_modified_time` or `get_the_modified_date` calls, the result (`false`) is passed through the functions respective filters to maintain back-compat.
    
    Introduces unit tests to ensure filters are applied and for the `get_the_modified_date` function.
    
    Fixes #37059.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@37866 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/general/template.php
+++ tests/phpunit/tests/general/template.php
@@ -370,6 +370,106 @@ class Tests_General_Template extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test get_the_modified_time failures are filtered
+	 *
+	 * @ticket 37059
+	 *
+	 * @since 4.6.0
+	 */
+	function test_get_the_modified_time_failures_are_filtered() {
+		// Remove global post objet
+		$GLOBALS['post'] = null;
+
+		$expected = 'filtered modified time failure result';
+		add_filter( 'get_the_modified_time', array( $this, '_filter_get_the_modified_time_failure' ) );
+		$actual = get_the_modified_time();
+		$this->assertEquals( $expected, $actual );
+		remove_filter( 'get_the_modified_time', array( $this, '_filter_get_the_modified_time_failure' ) );
+	}
+
+	function _filter_get_the_modified_time_failure( $the_time ) {
+		$expected = false;
+		$actual   = $the_time;
+		$this->assertEquals( $expected, $actual );
+
+		if ( false === $the_time ) {
+			return 'filtered modified time failure result';
+		}
+		return $the_time;
+	}
+
+	/**
+	 * Test get_the_modified_time with post_id parameter.
+	 *
+	 * @ticket 37059
+	 *
+	 * @since 4.6.0
+	 */
+	function test_get_the_modified_date_with_post_id() {
+		$details = array(
+				'post_date' => '2016-01-21 15:34:36',
+				'post_date_gmt' => '2016-01-21 15:34:36',
+		);
+		$post_id = $this->factory->post->create( $details );
+		$d = 'Y-m-d';
+		$expected = '2016-01-21';
+		$actual = get_the_modified_date( $d, $post_id );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Test get_the_modified_date
+	 *
+	 * @ticket 37059
+	 *
+	 * @since 4.6.0
+	 */
+	function test_get_the_modified_date_default() {
+		$details = array(
+				'post_date' => '2016-01-21 15:34:36',
+				'post_date_gmt' => '2016-01-21 15:34:36',
+		);
+		$post_id = $this->factory->post->create( $details );
+		$post = get_post( $post_id );
+
+		$GLOBALS['post'] = $post;
+
+		$expected = '2016-01-21';
+		$d = 'Y-m-d';
+		$actual = get_the_modified_date( $d );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Test get_the_modified_date failures are filtered
+	 *
+	 * @ticket 37059
+	 *
+	 * @since 4.6.0
+	 */
+	function test_get_the_modified_date_failures_are_filtered() {
+		// Remove global post objet
+		$GLOBALS['post'] = null;
+
+		$expected = 'filtered modified date failure result';
+		add_filter( 'get_the_modified_date', array( $this, '_filter_get_the_modified_date_failure' ) );
+		$actual = get_the_modified_date();
+		$this->assertEquals( $expected, $actual );
+		remove_filter( 'get_the_modified_date', array( $this, '_filter_get_the_modified_date_failure' ) );
+	}
+
+	function _filter_get_the_modified_date_failure( $the_date ) {
+		$expected = false;
+		$actual   = $the_date;
+		$this->assertEquals( $expected, $actual );
+
+		if ( false === $the_date ) {
+			return 'filtered modified date failure result';
+		}
+		return $the_date;
+	}
+
+	/**
 	 * Test get_the_modified_time with post_id parameter.
 	 *
 	 * @ticket 37059
