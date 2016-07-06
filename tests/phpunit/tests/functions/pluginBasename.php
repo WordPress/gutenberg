0037aa1dd57b064fb7170ec488bbd88956a4e631
Dominik Schilling <ocean90@git.wordpress.org>
2016-07-06 09:44:58 +0000

    Plugins: In `plugin_basename()` sort plugin paths before resolving symlinks.
    `arsort()` sorts the paths reverse-alphabetically while preserving the keys. It results in a longer path being listed before a shorter one with the same base directory(ies).
    
    Props jdgrimes, ocean90.
    Fixes #28441.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@37983 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/functions/pluginBasename.php
+++ tests/phpunit/tests/functions/pluginBasename.php
@@ -9,19 +9,56 @@
 class Tests_Plugin_Basename extends WP_UnitTestCase {
 
 	/**
+	 * @var array
+	 */
+	protected $wp_plugin_paths_backup;
+
+	/**
+	 * Normalized path to plugin directory.
+	 *
+	 * @var string
+	 */
+	protected $wp_plugin_path;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->wp_plugin_paths_backup = $GLOBALS['wp_plugin_paths'];
+		$this->wp_plugin_path = wp_normalize_path( WP_PLUGIN_DIR );
+	}
+
+	public function tearDown() {
+		$GLOBALS['wp_plugin_paths'] = $this->wp_plugin_paths_backup;
+
+		parent::tearDown();
+	}
+
+	/**
 	 * @ticket 29154
 	 */
-	function test_should_return_correct_basename_for_symlinked_plugins() {
+	function test_return_correct_basename_for_symlinked_plugins() {
 		global $wp_plugin_paths;
 
-		$old_wp_plugin_paths = $wp_plugin_paths;
-
-		$wp_plugin_paths[ wp_normalize_path( WP_PLUGIN_DIR ) . '/a-symlinked-plugin' ] = 'C:/www/path/plugins/a-plugin';
+		$wp_plugin_paths = array(
+			$this->wp_plugin_path . '/a-symlinked-plugin' => 'C:/www/path/plugins/a-plugin',
+		);
 
 		$basename = plugin_basename( 'c:\www\path\plugins\a-plugin\plugin.php' );
-
-		$wp_plugin_paths = $old_wp_plugin_paths;
-
 		$this->assertSame( 'a-symlinked-plugin/plugin.php', $basename );
+	}
+
+	/**
+	 * @ticket 28441
+	 */
+	function test_return_correct_basename_for_symlinked_plugins_with_path_conflicts() {
+		global $wp_plugin_paths;
+
+		$wp_plugin_paths = array(
+			$this->wp_plugin_path . '/plugin' => '/Users/me/Dropbox/Development/Repositories/plugin',
+			$this->wp_plugin_path . '/trunk' => '/Users/me/Dropbox/Development/Repositories/plugin/trunk',
+		);
+
+		$basename = plugin_basename( '/Users/me/Dropbox/Development/Repositories/plugin/trunk/plugin.php' );
+		$this->assertSame( 'trunk/plugin.php', $basename );
 	}
 }
