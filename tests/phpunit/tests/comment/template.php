946e6ac8de2baac007286dffc8dc5be130cc1c5e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2016-07-06 14:45:55 +0000

    I18N: Introduce an on/off switch for locales where comment number needs to be declined.
    When enabled, the switch would override the theme's pseudo-plural `'% Comments'` string with a correct form of `_n( '%s Comment', '%s Comments', $number )`.
    
    Historically, `comments_popup_link()` and `get_comments_number_text()` did not support plural forms and used a pseudo-plural style instead, so some locales were forced to come up with workarounds to display the number of comments in their language correctly.
    
    This change should make those functions more i18n-friendly.
    
    Fixes #13651.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@37987 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/comment/template.php
+++ tests/phpunit/tests/comment/template.php
@@ -30,4 +30,117 @@ class Tests_Comment_Template extends WP_UnitTestCase {
 		$this->assertEquals( 12, get_comments_number() );
 	}
 
+	/**
+	 * @ticket 13651
+	 */
+	function test_get_comments_number_text_declension_with_default_args() {
+		$post_id = $this->factory->post->create();
+		$permalink = get_permalink( $post_id );
+		$this->go_to( $permalink );
+
+		$this->assertEquals( __( 'No Comments' ), get_comments_number_text() );
+
+		$this->factory->comment->create_post_comments( $post_id, 1 );
+		$this->go_to( $permalink );
+
+		$this->assertEquals( __( '1 Comment' ), get_comments_number_text() );
+
+		$this->factory->comment->create_post_comments( $post_id, 1 );
+		$this->go_to( $permalink );
+
+		$this->assertEquals( sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ), get_comments_number_text() );
+
+	}
+
+	/**
+	 * @ticket 13651
+	 * @dataProvider data_get_comments_number_text_declension
+	 */
+	function test_get_comments_number_text_declension_with_custom_args( $number, $input, $output ) {
+		$post_id = $this->factory->post->create();
+		$permalink = get_permalink( $post_id );
+
+		$this->factory->comment->create_post_comments( $post_id, $number );
+		$this->go_to( $permalink );
+
+		add_filter( 'gettext_with_context', array( $this, '_enable_comment_number_declension' ), 10, 4 );
+
+		$this->assertEquals( $output, get_comments_number_text( false, false, $input ) );
+
+		remove_filter( 'gettext_with_context', array( $this, '_enable_comment_number_declension' ), 10, 4 );
+	}
+
+	function _enable_comment_number_declension( $translation, $text, $context, $domain ) {
+		if ( 'Comment number declension: on or off' === $context ) {
+			$translation = 'on';
+		}
+
+		return $translation;
+	}
+
+	function data_get_comments_number_text_declension() {
+		return array(
+			array(
+				2,
+				'Comments (%)',
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+			array(
+				2,
+				'2 Comments',
+				'2 Comments',
+			),
+			array(
+				2,
+				'2 Comments<span class="screen-reader-text"> on Hello world!</span>',
+				'2 Comments<span class="screen-reader-text"> on Hello world!</span>',
+			),
+			array(
+				2,
+				'2 Comments<span class="screen-reader-text"> on Hello % world!</span>',
+				'2 Comments<span class="screen-reader-text"> on Hello 2 world!</span>' // See #WP37103
+			),
+			array(
+				2,
+				__( '% Comments', 'twentyten' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+			array(
+				2,
+				_x( '%', 'comments number', 'twentyeleven' ),
+				'2',
+			),
+			array(
+				2,
+				__( '<b>%</b> Replies', 'twentyeleven' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '<b>2</b>' ),
+			),
+			array(
+				2,
+				__( '% <span class="reply">comments &rarr;</span>', 'twentyeleven' ),
+				sprintf( '2 <span class="reply">%s &rarr;</span>', trim( sprintf( _n( '%s Comment', '%s Comments', 2 ), '' ) ) ),
+			),
+			array(
+				2,
+				__( '% Replies', 'twentytwelve' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+			array(
+				2,
+				__( 'View all % comments', 'twentythirteen' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+			array(
+				2,
+				__( '% Comments', 'twentyfourteen' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+			array(
+				2,
+				__( '% Comments', 'twentyfifteen' ),
+				sprintf( _n( '%s Comment', '%s Comments', 2 ), '2' ),
+			),
+		);
+	}
+
 }
