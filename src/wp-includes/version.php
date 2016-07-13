e259c9497d1e384c30f05e8e647df70e042e3aaa
Dominik Schilling <ocean90@git.wordpress.org>
2016-07-13 18:40:39 +0000

    WordPress 4.6 Beta 3.
    git-svn-id: https://develop.svn.wordpress.org/trunk@38059 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.6-beta2-37993-src';
+$wp_version = '4.6-beta3-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
