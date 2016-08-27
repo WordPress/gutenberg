4225e71a6b6e8129628e1d55637e357fae331e50
Scott Taylor <wonderboymusic@git.wordpress.org>
2016-08-27 22:31:11 +0000

    Bootstrap: after r38409 and r38410, revert r38402 which reverted r38399.
    This fixes the paths in `wp-vendor/` that were including `src`. I want to drop this in so we can find out what else will break.
    
    See #36335.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@38411 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/testcase-xmlrpc.php
+++ tests/phpunit/includes/testcase-xmlrpc.php
@@ -1,7 +1,5 @@
 <?php
-include_once(ABSPATH . 'wp-admin/includes/admin.php');
-include_once(ABSPATH . WPINC . '/class-IXR.php');
-include_once(ABSPATH . WPINC . '/class-wp-xmlrpc-server.php');
+include_once( ABSPATH . 'wp-admin/includes/admin.php' );
 
 class WP_XMLRPC_UnitTestCase extends WP_UnitTestCase {
 	protected $myxmlrpcserver;
