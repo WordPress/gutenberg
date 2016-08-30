caf0e90b3036c65f26a33df893c3606aa93616f9
Boone Gorges <boonebgorges@git.wordpress.org>
2016-08-30 14:31:56 +0000

    Tests: Add docblocks for `Basic_Object` and `Basic_Subclass` classes.
    Props DylanAuty.
    See #37523.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@38444 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/class-basic-object.php
+++ tests/phpunit/includes/class-basic-object.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Unit Tests: Basic_Object cloass
+ *
+ * @package WordPress
+ * @subpackage UnitTests
+ * @since 4.7.0
+ */
 
+/**
+ * Class used to test accessing methods and properties
+ *
+ * @since 4.0.0
+ */
 class Basic_Object {
 	private $foo = 'bar';
 
--- tests/phpunit/includes/class-basic-subclass.php
+++ tests/phpunit/includes/class-basic-subclass.php
@@ -1,3 +1,15 @@
 <?php
+/**
+ * Unit Tests: Basic_Subclass class
+ *
+ * @package WordPress
+ * @subpackage UnitTests
+ * @since 4.7.0
+ */
 
+/**
+ * Class used to test accessing methods and properties.
+ *
+ * @since 4.0.0
+ */
 class Basic_Subclass extends Basic_Object {}
