2aef21a55b2bf1d8ce6f96a88840384b9f9f49e8
John Blackbourn <johnbillion@git.wordpress.org>
2016-09-08 22:53:57 +0000

    Themes: Improve child theme file inheritance by introducing functions for locating and fetching the URL or path to files within child and parent themes.
    The most useful function this introduces is `get_theme_file_uri()`, which returns the URL to the specified file in the child theme if it exists, and falls back to the URL to the specified file in the parent theme. This allows parent themes to reference files (including enqueuing CSS and JavaScript files) that can be overridden by the child theme simply by existing.
    
    This change also introduces `get_theme_file_path()`, which is the file path equivalent of `get_theme_file_uri()`.
    
    Finally, `get_parent_theme_file_uri()` and `get_parent_theme_file_path()` are also introduced, which allow a theme to specifically reference a file URL or file path in the parent theme. These can be used as replacements for `get_template_directory_uri()` and `get_template_directory()` respectively, for consistency.
    
    Props johnbillion, georgestephanis, gma992.
    Fixes #18302
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@38578 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/data/theme-file-child/parent-and-child.php
@@ -0,0 +1,1 @@
+<!-- parent and child -->
---
+++ tests/phpunit/tests/link/themeFile.php
@@ -0,0 +1,146 @@
+
+<?php
+/**
+ * @group link
+ */
+class Test_Theme_File extends WP_UnitTestCase {
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		symlink( DIR_TESTDATA . '/theme-file-parent', WP_CONTENT_DIR . '/themes/theme-file-parent' );
+		symlink( DIR_TESTDATA . '/theme-file-child', WP_CONTENT_DIR . '/themes/theme-file-child' );
+	}
+
+	public static function wpTearDownAfterClass() {
+		unlink( WP_CONTENT_DIR . '/themes/theme-file-parent' );
+		unlink( WP_CONTENT_DIR . '/themes/theme-file-child' );
+	}
+
+	/**
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_uri_with_parent_theme( $file, $expected_theme, $existence ) {
+		switch_theme( 'theme-file-parent' );
+
+		// Ensure the returned URL always uses the parent theme:
+		$this->assertSame( content_url( "themes/theme-file-parent/{$file}" ), get_theme_file_uri( $file ) );
+		$this->assertSame( content_url( "themes/theme-file-parent/{$file}" ), get_parent_theme_file_uri( $file ) );
+	}
+
+	/**
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_uri_with_child_theme( $file, $expected_theme, $existence ) {
+		switch_theme( 'theme-file-child' );
+
+		// Ensure the returned URL uses the expected theme:
+		$this->assertSame( content_url( "themes/{$expected_theme}/{$file}" ), get_theme_file_uri( $file ) );
+
+		// Ensure the returned URL always uses the parent theme:
+		$this->assertSame( content_url( "themes/theme-file-parent/{$file}" ), get_parent_theme_file_uri( $file ) );
+	}
+
+	/**
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_path_with_parent_theme( $file, $expected_theme, $existence ) {
+		switch_theme( 'theme-file-parent' );
+
+		// Ensure the returned path always uses the parent theme:
+		$this->assertSame( WP_CONTENT_DIR . "/themes/theme-file-parent/{$file}", get_theme_file_path( $file ) );
+		$this->assertSame( WP_CONTENT_DIR . "/themes/theme-file-parent/{$file}", get_parent_theme_file_path( $file ) );
+	}
+
+	/**
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_path_with_child_theme( $file, $expected_theme, $existence ) {
+		switch_theme( 'theme-file-child' );
+
+		// Ensure the returned path uses the expected theme:
+		$this->assertSame( WP_CONTENT_DIR . "/themes/{$expected_theme}/{$file}", get_theme_file_path( $file ) );
+
+		// Ensure the returned path always uses the parent theme:
+		$this->assertSame( WP_CONTENT_DIR . "/themes/theme-file-parent/{$file}", get_parent_theme_file_path( $file ) );
+	}
+
+	/**
+	 * Test the tests.
+	 *
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_existance( $file, $expected_theme, $existence ) {
+
+		if ( in_array( 'theme-file-child', $existence, true ) ) {
+			$this->assertFileExists( WP_CONTENT_DIR . "/themes/theme-file-child/{$file}" );
+		} else {
+			$this->assertFileNotExists( WP_CONTENT_DIR . "/themes/theme-file-child/{$file}" );
+		}
+
+		if ( in_array( 'theme-file-parent', $existence, true ) ) {
+			$this->assertFileExists( WP_CONTENT_DIR . "/themes/theme-file-parent/{$file}" );
+		} else {
+			$this->assertFileNotExists( WP_CONTENT_DIR . "/themes/theme-file-parent/{$file}" );
+		}
+
+	}
+
+	/**
+	 * @ticket 18302
+	 *
+	 * @dataProvider data_theme_files
+	 */
+	public function test_theme_file_uri_returns_valid_uri( $file, $expected_theme, $existence ) {
+		$uri = get_theme_file_uri( $file );
+		$parent_uri = get_parent_theme_file_uri( $file );
+
+		$this->assertSame( esc_url_raw( $uri ), $uri );
+		$this->assertSame( esc_url_raw( $parent_uri ), $parent_uri );
+	}
+
+	public function data_theme_files() {
+		$parent = 'theme-file-parent';
+		$child  = 'theme-file-child';
+
+		return array(
+			array(
+				'parent-only.php',
+				$parent,
+				array(
+					$parent,
+				),
+			),
+			array(
+				'child-only.php',
+				$child,
+				array(
+					$child,
+				),
+			),
+			array(
+				'parent-and-child.php',
+				$child,
+				array(
+					$parent,
+					$child,
+				),
+			),
+			array(
+				'neither.php',
+				$parent,
+				array(
+				),
+			),
+		);
+	}
+
+}
