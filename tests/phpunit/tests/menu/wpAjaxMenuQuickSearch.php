6c8c98fe5bbab391836ff83c29305764adbebcd0
Weston Ruter <westonruter@git.wordpress.org>
2016-09-09 04:59:01 +0000

    Menus: Prevent non-published posts/pages from being returned in search results for adding as nav menu items.
    Re-use the same query vars in searching as when listing posts. Aligns with behavior of nav menus in customizer.
    
    Fixes #33742.
    Props welcher, westonruter.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@38584 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/menu/wpAjaxMenuQuickSearch.php
+++ tests/phpunit/tests/menu/wpAjaxMenuQuickSearch.php
@@ -6,6 +6,33 @@
 class Tests_Menu_WpAjaxMenuQuickSeach extends WP_UnitTestCase {
 
 	/**
+	 * Current screen.
+	 *
+	 * @var mixed
+	 */
+	protected $current_screen;
+
+	/**
+	 * Set up. Workaround set_current_screen( null ) not working due to $hook_suffix not being set.
+	 */
+	function setUp() {
+		global $current_screen;
+		$this->current_screen = $current_screen;
+		parent::setUp();
+	}
+
+	/**
+	 * Tear down. Workaround set_current_screen( null ) not working due to $hook_suffix not being set.
+	 */
+	function tearDown() {
+		global $current_screen;
+		parent::tearDown();
+		$current_screen = $this->current_screen;
+	}
+
+	/**
+	 * Test search returns results for pages.
+	 *
 	 * @ticket 27042
 	 */
 	public function test_search_returns_results_for_pages() {
@@ -25,5 +52,32 @@ class Tests_Menu_WpAjaxMenuQuickSeach extends WP_UnitTestCase {
 
 		$results = explode( "\n", trim( $output ) );
 		$this->assertCount( 3, $results );
+	}
+
+	/**
+	 * Test that search only returns results for publihed posts.
+	 *
+	 * @ticket 33742
+	 */
+	public function test_search_returns_results_for_published_posts() {
+		require_once ABSPATH . 'wp-admin/includes/nav-menu.php';
+
+		// This will make sure that WP_Query sets is_admin to true.
+		set_current_screen( 'nav-menu.php' );
+
+		self::factory()->post->create( array( 'post_type' => 'post', 'post_status' => 'publish', 'post_title' => 'Publish', 'post_content' => 'FOO' ) );
+		self::factory()->post->create( array( 'post_type' => 'post', 'post_status' => 'draft', 'post_title' => 'Draft', 'post_content' => 'FOO' ) );
+		self::factory()->post->create( array( 'post_type' => 'post', 'post_status' => 'pending', 'post_title' => 'Pending', 'post_content' => 'FOO' ) );
+		self::factory()->post->create( array( 'post_type' => 'post', 'post_status' => 'future', 'post_title' => 'Future', 'post_content' => 'FOO', 'post_date' => gmdate( 'Y-m-d H:i:s', strtotime( '+1 month' ) ) ) );
+
+		$request = array(
+			'type' => 'quick-search-posttype-post',
+			'q' => 'FOO',
+		);
+		$output = get_echo( '_wp_ajax_menu_quick_search', array( $request ) );
+
+		$this->assertNotEmpty( $output );
+		$results = explode( "\n", trim( $output ) );
+		$this->assertCount( 1, $results );
 	}
 }
