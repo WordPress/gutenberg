b2f60b5419f9592c4525bd4125d85e9b087fc9e0
swissspidy <swissspidy@602fd350-edb4-49c9-b593-d223f7449a82>
2016-10-07 17:11:02 +0000

    Taxonomy: Introduce `WP_Taxonomy` and use it in `register_taxonomy()` and `unregister_taxonomy()`.
    This changes the global `$wp_taxonomies` to an array of `WP_Taxonomy ` objects. `WP_Taxonomy ` includes methods to handle rewrite rules and hooks.
    Each taxonomy argument becomes a property of `WP_Taxonomy`. Introducing such a class makes further improvements in the future much more feasible.
    
    Props boonebgorges for review.
    Fixes #36224. See #36217.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@38747 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/phpunit/tests/term/wpTaxonomy.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @group taxonomy
+ */
+class Tests_WP_Taxonomy extends WP_UnitTestCase {
+	public function test_instances() {
+		global $wp_taxonomies;
+
+		foreach ( $wp_taxonomies as $taxonomy ) {
+			$this->assertInstanceOf( 'WP_Taxonomy', $taxonomy );
+		}
+	}
+
+	public function test_does_not_add_query_var_if_not_public() {
+		$this->set_permalink_structure( '/%postname%' );
+
+		/* @var WP $wp */
+		global $wp;
+
+		$taxonomy        = rand_str();
+		$taxonomy_object = new WP_Taxonomy( $taxonomy, 'post' );
+
+		$taxonomy_object->add_rewrite_rules();
+		$this->assertFalse( in_array( 'foobar', $wp->public_query_vars ) );
+	}
+
+	public function test_adds_query_var_if_public() {
+		$this->set_permalink_structure( '/%postname%' );
+
+		/* @var WP $wp */
+		global $wp;
+
+		$taxonomy        = rand_str();
+		$taxonomy_object = new WP_Taxonomy( $taxonomy, 'post', array(
+			'public'    => true,
+			'rewrite'   => false,
+			'query_var' => 'foobar',
+		) );
+
+		$taxonomy_object->add_rewrite_rules();
+		$in_array = in_array( 'foobar', $wp->public_query_vars );
+
+		$taxonomy_object->remove_rewrite_rules();
+		$in_array_after = in_array( 'foobar', $wp->public_query_vars );
+
+		$this->assertTrue( $in_array );
+		$this->assertFalse( $in_array_after );
+	}
+
+	public function test_adds_rewrite_rules() {
+		$this->set_permalink_structure( '/%postname%' );
+
+		/* @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+
+		$taxonomy        = rand_str();
+		$taxonomy_object = new WP_Taxonomy( $taxonomy, 'post', array(
+			'public'  => true,
+			'rewrite' => true,
+		) );
+
+		$taxonomy_object->add_rewrite_rules();
+		$rewrite_tags = $wp_rewrite->rewritecode;
+
+		$taxonomy_object->remove_rewrite_rules();
+		$rewrite_tags_after = $wp_rewrite->rewritecode;
+
+		$this->assertTrue( false !== array_search( "%$taxonomy%", $rewrite_tags ) );
+		$this->assertFalse( array_search( "%$taxonomy%", $rewrite_tags_after ) );
+	}
+
+	public function test_adds_ajax_callback() {
+		$taxonomy        = rand_str();
+		$taxonomy_object = new WP_Taxonomy( $taxonomy, 'post', array(
+			'public'  => true,
+			'rewrite' => true,
+		) );
+
+		$taxonomy_object->add_hooks();
+		$has_action = has_action( "wp_ajax_add-$taxonomy", '_wp_ajax_add_hierarchical_term' );
+
+		$taxonomy_object->remove_hooks();
+		$has_action_after = has_action( "wp_ajax_add-$taxonomy", '_wp_ajax_add_hierarchical_term' );
+
+		$this->assertSame( 10, $has_action );
+		$this->assertFalse( $has_action_after );
+
+	}
+}
