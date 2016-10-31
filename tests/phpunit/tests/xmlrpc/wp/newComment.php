5b4f2b3021d8731c0fe20b4c591615e77d271fe1
John Blackbourn <johnbillion@git.wordpress.org>
2016-10-31 01:26:10 +0000

    XML-RPC: Correctly handle empty and duplicate comments.
    This prevents `wp_die()` being sent in response to an XML-RPC call that attempts to submit a duplicate comment, and correctly returns an error in response to an attempt to submit an empty comment.
    
    Props markoheijnen, websupporter.
    Fixes #14452, #38466.
    See #36901
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@39045 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/xmlrpc/wp/newComment.php
+++ tests/phpunit/tests/xmlrpc/wp/newComment.php
@@ -4,6 +4,30 @@
  * @group xmlrpc
  */
 class Tests_XMLRPC_wp_newComment extends WP_XMLRPC_UnitTestCase {
+
+	function test_valid_comment() {
+		$this->make_user_by_role( 'administrator' );
+		$post = self::factory()->post->create_and_get();
+ 
+		$result = $this->myxmlrpcserver->wp_newComment( array( 1, 'administrator', 'administrator', $post->ID, array(
+			'content' => rand_str( 100 )
+		) ) );
+ 
+		$this->assertNotInstanceOf( 'IXR_Error', $result );
+	}
+ 
+	function test_empty_comment() {
+		$this->make_user_by_role( 'administrator' );
+		$post = self::factory()->post->create_and_get();
+ 
+		$result = $this->myxmlrpcserver->wp_newComment( array( 1, 'administrator', 'administrator', $post->ID, array(
+			'content' => ''
+		) ) );
+ 
+		$this->assertInstanceOf( 'IXR_Error', $result );
+		$this->assertEquals( 403, $result->code );
+	}
+
 	function test_new_comment_post_closed() {
 		$this->make_user_by_role( 'administrator' );
 		$post = self::factory()->post->create_and_get( array(
@@ -19,4 +43,24 @@ class Tests_XMLRPC_wp_newComment extends WP_XMLRPC_UnitTestCase {
 		$this->assertInstanceOf( 'IXR_Error', $result );
 		$this->assertEquals( 403, $result->code );
 	}
+
+	function test_new_comment_duplicated() {
+		$this->make_user_by_role( 'administrator' );
+		$post = self::factory()->post->create_and_get();
+
+		$comment_args = array( 1, 'administrator', 'administrator', $post->ID, array(
+			'content' => rand_str( 100 ),
+		) );
+
+		// First time it's a valid comment
+		$result = $this->myxmlrpcserver->wp_newComment( $comment_args  );
+		$this->assertNotInstanceOf( 'IXR_Error', $result );
+
+		// Run second time for duplication error
+		$result = $this->myxmlrpcserver->wp_newComment( $comment_args );
+
+		$this->assertInstanceOf( 'IXR_Error', $result );
+		$this->assertEquals( 403, $result->code );
+	}
+
 }
