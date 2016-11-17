365241878f946a56fd07699eff0bff5add644e1d
Gary Pendergast <pento@git.wordpress.org>
2016-11-17 04:20:22 +0000

    Database: Add support for `LIKE`-escaped tables in `::get_table_from_query()`.
    The `SHOW TABLES LIKE` query can be used to search for tables that match a pattern, `wp\_123\_%`, for example. While this isn't the name of an actual table, the `wp_123_` prefix can be used by database drop-ins to direct the query correctly. This change removes the escaping and `%` modifier, to provide this usable prefix.
    
    Props andy, pento.
    Fixes #38751.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@39275 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/db.php
+++ tests/phpunit/tests/db.php
@@ -565,14 +565,6 @@ class Tests_DB extends WP_UnitTestCase {
 			"DELETE a FROM $table a",
 			"DELETE `a` FROM $table a",
 
-			// STATUS
-			"SHOW TABLE STATUS LIKE '$table'",
-			"SHOW TABLE STATUS WHERE NAME='$table'",
-
-			"SHOW TABLES LIKE '$table'",
-			"SHOW FULL TABLES LIKE '$table'",
-			"SHOW TABLES WHERE NAME='$table'",
-
 			// Extended
 			"EXPLAIN SELECT * FROM $table",
 			"EXPLAIN EXTENDED SELECT * FROM $table",
@@ -668,6 +660,33 @@ class Tests_DB extends WP_UnitTestCase {
 	 */
 	function test_get_table_from_query_false( $query ) {
 		$this->assertFalse( self::$_wpdb->get_table_from_query( $query ) );
+	}
+
+	/**
+	 * @ticket 38751
+	 */
+	function data_get_escaped_table_from_show_query() {
+		return array(
+			// Equality
+			array( "SHOW TABLE STATUS WHERE Name = 'test_name'", 'test_name' ),
+			array( "SHOW TABLE STATUS WHERE NAME=\"test_name\"", 'test_name' ),
+			array( "SHOW TABLES WHERE Name = \"test_name\"",     'test_name' ),
+			array( "SHOW FULL TABLES WHERE Name='test_name'",    'test_name' ),
+
+			// LIKE
+			array( "SHOW TABLE STATUS LIKE 'test\_prefix\_%'",   'test_prefix_' ),
+			array( "SHOW TABLE STATUS LIKE \"test\_prefix\_%\"", 'test_prefix_' ),
+			array( "SHOW TABLES LIKE 'test\_prefix\_%'",         'test_prefix_' ),
+			array( "SHOW FULL TABLES LIKE \"test\_prefix\_%\"",  'test_prefix_' ),
+		);
+	}
+
+	/**
+	 * @dataProvider data_get_escaped_table_from_show_query
+	 * @ticket 38751
+	 */
+	function test_get_escaped_table_from_show_query( $query, $table ) {
+		$this->assertEquals( $table, self::$_wpdb->get_table_from_query( $query ) );
 	}
 
 	/**
