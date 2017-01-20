3a449139db36d0d75e911a3773a826b4828cb7a4
Adam Silverstein <adamsilverstein@git.wordpress.org>
2017-01-20 16:14:23 +0000

    Menus: trigger an event when menu items are added or removed.
    Fire a `menu-item-added` event after a menu item is added to the DOM. Fire a `menu-removing-item` event before a menu item is removed from the DOM. Enables hooking into and responding to menu changes.
    
    Props welcher, adamsilverstein.
    Fixes #31218.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@39928 602fd350-edb4-49c9-b593-d223f7449a82

---
+++ tests/qunit/wp-admin/js/nav-menu.js
@@ -0,0 +1,80 @@
+/*global wpNavMenu */
+( function( QUnit, $ ) {
+	QUnit.module( 'nav-menu' );
+	var assert,
+		eventsExpected = 3,
+		eventsFired = 0;
+
+	// Fail if we don't see the expected number of events triggered in 1500 ms.
+	setTimeout( function() {
+		assert.equal(
+			eventsFired,
+			eventsExpected,
+			eventsExpected + ' wpNavMenu events should fire.'
+		);
+	}, 1500 );
+
+	QUnit.test( 'Testing wpNavMenu event triggers.', function( assertPassed ) {
+		assert = assertPassed;
+
+		assert.expect( 3 );
+
+		var testString = '<div>Hello World</div>';
+
+		// Mock the internal function calls so the don't fail.
+		$.fn.hideAdvancedMenuItemFields = function() {
+			return {
+				'appendTo':       function() { return true; },
+				'prependTo':      function() { return true; }
+			};
+		};
+
+		$.fn.extend( {
+			'childMenuItems':  function() { return $(); },
+			'shiftDepthClass': function() { return $(); }
+		} );
+
+		// Set up the events we should test.
+		var eventsToTest = [
+			{
+				'event':         'addMenuItemToBottom',
+				'data':          testString,
+				'expect':        $( testString ),
+				'shouldTrigger': 'menu-item-added'
+			},
+			{
+				'event':         'addMenuItemToTop',
+				'data':          testString,
+				'expect':        $( testString ),
+				'shouldTrigger': 'menu-item-added'
+			},
+			{
+				'event':         'removeMenuItem',
+				'data':          $( testString ),
+				'expect':        $( testString ),
+				'shouldTrigger': 'menu-removing-item'
+			}
+		];
+
+		// Test each of the events.
+		_.each( eventsToTest, function( theEvent ) {
+
+			var done = assert.async();
+
+			$( document ).on( theEvent.shouldTrigger, function( evt, passed ) {
+				assert.equal(
+					passed.html(),
+					theEvent.expect.html(),
+					'The ' + theEvent.event + ' should trigger ' + theEvent.shouldTrigger + '.'
+				);
+				eventsFired++;
+				done();
+			} );
+			wpNavMenu[ theEvent.event ]( theEvent.data );
+			$( document ).off( theEvent.shouldTrigger );
+		} );
+
+	} );
+
+
+} )( window.QUnit, jQuery );
