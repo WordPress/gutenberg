e0501833b6b2055e77abeee3bf55fdeafb5148ff
Matías Ventura <mv@matiasventura.com>
2017-02-07 16:34:50 +0100

    Add block controls to UI prototype.

--- blocks.js
+++ blocks.js
@@ -4,6 +4,8 @@ var text = document.getElementsByTagName( 'p' );
 var heading = document.getElementsByTagName( 'h2' );
 var blocks = [ ...text, ...heading ];
 
+var controls = document.getElementsByClassName( 'block-controls' )[0];
+
 window.addEventListener( 'click', clearBlocks, false );
 
 Array.from( blocks ).forEach( function( block ) {
@@ -14,10 +16,22 @@ function selectBlock( event ) {
 	clearBlocks();
 	event.stopPropagation();
 	event.target.className = 'is-selected';
+
+	var position = event.target.getBoundingClientRect();
+
+	// Show switcher
+	controls.style.opacity = 1;
+	controls.style.top = ( position.top + 18 ) + 'px';
 }
 
 function clearBlocks( event ) {
 	Array.from( blocks ).forEach( function( block ) {
 		block.className = '';
 	} );
+
+	hideControls();
+}
+
+function hideControls() {
+	controls.style.opacity = 0;
 }
