bfeda161b7eaec95b5b2974c612ead5ee229ca5e
Dion Hulse <dd32@git.wordpress.org>
2017-02-17 05:05:03 +0000

    HTTP API: Restore backwards compatibility with the `http_api_curl` filter - it expects that the handle parameter is passed as a reference, however [39212] missed that.
    Props pento.
    Fixes #39783.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@40068 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/http/curl.php
+++ tests/phpunit/tests/http/curl.php
@@ -8,4 +8,19 @@ require_once dirname( __FILE__ ) . '/base.php';
  */
 class Tests_HTTP_curl extends WP_HTTP_UnitTestCase {
 	var $transport = 'curl';
+
+	/**
+	 * @ticket 39783
+	 */
+	public function test_http_api_curl_stream_parameter_is_a_reference() {
+		add_action( 'http_api_curl', array( $this, '_action_test_http_api_curl_stream_parameter_is_a_reference' ), 10, 3 );
+		wp_remote_request( $this->fileStreamUrl, array( 'stream' => true, 'timeout' => 30 ) );
+		remove_action( 'http_api_curl', array( $this, '_action_test_http_api_curl_stream_parameter_is_a_reference' ), 10 );
+	}
+
+	public function _action_test_http_api_curl_stream_parameter_is_a_reference( &$stream, $r, $url ) {
+		// $stream not being a reference will cause a PHP warning.
+		// For counting tests purposes, let's do a fake assert.
+		$this->assertTrue( true );
+	}
 }
