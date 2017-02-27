975dc978c2f0b5bb85c76190475d1c0d029631fe
iseulde <ella@iseulde.com>
2017-02-27 16:20:02 +0100

    Show block UI on focus, hide on type.

---
+++ tinymce-single/blocks/core/gallery/register.js
@@ -0,0 +1,25 @@
+window.wp.blocks.register( {
+	name: 'gallery',
+	namespace: 'core',
+	type: 'image',
+	keywords: [],
+	icon: 'gridicons-image',
+	buttons: [
+		'block-align-left',
+		'block-align-center',
+		'block-align-right',
+		// {
+		// 	type: 'select',
+		// 	label: 'Columns'
+		// },
+		// {
+		// 	type: 'button',
+		// 	label: 'Columns',
+		// 	icon: 'gridicons-image',
+		// 	command: function( editor, node ) {
+		// 		// body...
+		// 	}
+		// },
+		'block-options'
+	]
+} );
