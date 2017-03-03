0eedc67fb47a44dc5b6c468c7e63d054bc910a81
Andrew Duthie <andrew@andrewduthie.com>
2017-03-03 17:26:56 -0500

    Include simplified, diverse content types in content

--- shared/post-content.js
+++ shared/post-content.js
@@ -1,1 +1,33 @@
-window.content = '<!-- wp:text --><p>Loem ipsum dolor sit amet, ferri vidisse nam eu, <a href="#">ad nec copiosae mnesarchum vituperatoribus</a>. Te brute dicunt sea, ut vis omnium menandri, ut sumo aliquam has. Eum aperiam interpretaris at, sea et recusabo expetenda, omnis tibique mea no. Pri suas partem ea, ius sonet numquam offendit cu, ad simul admodum pri. Eum cu unum choro albucius.</p><!-- /wp --><!-- wp:text align:right --><p style="text-align:right;">Eu integre accusata prodesset est, sed te impetus gubergren conceptam, ex sed wisi nostrum ocurreret. Esse velit omittantur ius te, alii dissentias ei vis. At sed unum veritus fabellas. Te volutpat appellantur duo. Ad natum fuisset intellegebat eam, causae invidunt usu id, et vis impetus appetere.</p><!-- /wp --><!-- wp:heading --><h3>Sea id autem nominavi deseruisse</h3><!-- /wp --><!-- wp:image --><figure><img src="http://moc.co/sandbox/example-post/pic2.jpg" /><figcaption>Beautiful landscape</figcaption></figure><!-- /wp --><!-- wp:text --><p>Est quis reque cetero ad. Sea id autem nominavi deseruisse. Veniam qualisque definitionem pri id, ea autem feugiat delenit ius, mei at loem affert accumsan. Dicat eruditi cu est, te pro dicant pericula conclusionemque, ei vim detracto euripidis intellegam. Eius postea volumus mei ad.</p><!-- /wp --><!-- wp:image align:align-full-width --><img style="margin-left:calc(50% - 50vw);width:100vw;max-width:none;padding-left:0;padding-right:0;" src="http://moc.co/sandbox/example-post/pic3.jpg" /><!-- /wp --><!-- wp:text --><p>Prima ridens denique his te, ferri illum volumus an his. <em>Eu vel dicat homero qualisqu</em>, vitae regione deserunt vis ei. Graeci incorrupte liberavisse no mea, saepe voluptaria usu ex, vis dicant euismod id. At dolor reprimique eos, quo altera detraxit moderatius id. Quo iudico utinam eu, ad alia munere mel.</p><!-- /wp --><!-- wp:quote --><blockquote><p>Loem ipsum dolor sit amet, ferri vidisse nam eu, ad nec copiosae mnesarchum vituperatoribus. Te brute dicunt sea, ut vis omnium menandri, ut sumo aliquam has. Eum aperiam interpretaris at, sea et recusabo expetenda, omnis tibique mea no. Pri suas partem ea, ius sonet numquam offendit cu, ad simul admodum pri. Eum cu unum choro albucius.</p><footer>By accusata prodesse</footer></blockquote><!-- /wp -->';
+window.content = [
+	'<!-- wp:heading -->',
+	'<h1>1.0 Is The Loneliest Number</h1>',
+	'<!-- /wp -->',
+
+	'<!-- wp:text -->',
+	'<p>I imagine prior to the launch of the iPod, or the iPhone, there were teams saying the same thing: the copy + paste guys are <em>so close</em> to being ready and we know Walt Mossberg is going to ding us for this so let\'s just not ship to the manufacturers in China for just a few more weeks… The Apple teams were probably embarrassed. But <strong>if you\'re not embarrassed when you ship your first version you waited too long</strong>.</p>',
+	'<!-- /wp -->',
+
+	'<!-- wp:image -->',
+	'<figure><img src="https://cldup.com/Bc9YxmqFnJ.jpg" /></figure>',
+	'<!-- /wp -->',
+
+	'<!-- wp:text -->',
+	'<p>A beautiful thing about Apple is how quickly they obsolete their own products. I imagine this also makes the discipline of getting things out there easier. Like I mentioned before, the longer it’s been since the last release the more pressure there is, but if you know that if your bit of code doesn’t make this version but there’s the +0.1 coming out in 6 weeks, then it’s not that bad. It’s like flights from San Francisco to LA, if you miss one you know there’s another one an hour later so it’s not a big deal. Amazon has done a fantastic job of this with the Kindle as well, with a new model every year.</p>',
+	'<!-- /wp -->',
+
+	'<!-- wp:quote -->',
+	'<blockquote><p>Real artists ship.</p><footer><p><a href="http://www.folklore.org/StoryView.py?story=Real_Artists_Ship.txt">Steve Jobs, 1983</a></p></footer></blockquote>',
+	'<!-- /wp -->',
+
+	'<!-- wp:image -->',
+	'<figure><img src="https://cldup.com/vuGcj2VB8M.jpg" /><figcaption>Beautiful landscape</figcaption></figure>',
+	'<!-- /wp -->',
+
+	'<!-- wp:text -->',
+	'<p>By shipping early and often you have the unique competitive advantage of hearing from real people what they think of your work, which in best case helps you anticipate market direction, and in worst case gives you a few people rooting for you that you can email when your team pivots to a new idea. Nothing can recreate the crucible of real usage.</p>',
+	'<!-- /wp -->',
+
+	'<!-- wp:embed -->',
+	'https://www.youtube.com/watch?v=Nl6U7UotA-M',
+	'<!-- /wp -->'
+].join( '' );
