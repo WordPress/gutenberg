b7147ead6406103357f01df25934d89557db3fde
Riad Benguella <benguella@gmail.com>
2017-03-03 15:45:56 +0100

    TinyMCE per block: Fix the quote markup (#175)
    - Use blockquote
     - Use footer for the cite

--- tinymce-per-block/src/blocks/quote-block/index.js
+++ tinymce-per-block/src/blocks/quote-block/index.js
@@ -20,22 +20,39 @@ registerBlock( 'quote', {
 		div.innerHTML = rawBlock.rawContent;
 		if (
 			div.childNodes.length !== 1 ||
-			div.firstChild.nodeName !== 'P'
+			! (
+				div.firstChild.nodeName === 'P' ||
+				(
+					div.firstChild.nodeName === 'BLOCKQUOTE' &&
+					div.firstChild.childNodes.length &&
+					div.firstChild.firstChild.nodeName === 'P'
+				)
+			)
 		) {
 			return false;
 		}
 
+		const content = div.firstChild.nodeName === 'P'
+			? div.innerHTML
+			: div.firstChild.firstChild.outerHTML;
+
+		const cite = div.firstChild.childNodes.length > 1
+			? div.firstChild.childNodes[ 1 ].innerHTML
+			: '';
+
 		return {
 			blockType: 'quote',
-			cite: rawBlock.attrs.cite || '',
-			content: rawBlock.rawContent,
+			cite,
+			content
 		};
 	},
 	serialize: ( block ) => {
+		const rawContent = `<blockquote>${ block.content }<footer>${ block.cite }</footer></blockquote>`;
+
 		return {
 			blockType: 'quote',
-			attrs: { cite: block.cite },
-			rawContent: block.content
+			attrs: {},
+			rawContent
 		};
 	}
 } );
