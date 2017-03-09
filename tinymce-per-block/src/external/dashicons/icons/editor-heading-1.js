52f1680c01507c2969ebc2140090a5ee7b528c35
Riad Benguella <benguella@gmail.com>
2017-03-09 11:17:17 +0100

    TinyMCE per block: Allowing two quote styles

--- tinymce-per-block/src/external/dashicons/icons/editor-heading-1.js
+++
@@ -1,7 +0,0 @@
-/**
- * External dependencies
- */
-import { createElement } from 'wp-elements';
-
-// This is a gridicon
-export default () => <svg className="dashicon" height="60px" version="1.1" viewBox="0 0 60 60" width="60px"><g fill="none" fillRule="evenodd" id="People" stroke="none" strokeWidth="1"><g fill="#000000" id="Icon-71"><path d="M32,19 L32,41 C32,41.553 31.552,42 31,42 C30.448,42 30,41.553 30,41 L30,21.414 L25.707,25.707 C25.316,26.098 24.684,26.098 24.293,25.707 C23.902,25.316 23.902,24.684 24.293,24.293 L30.293,18.293 C30.579,18.006 31.008,17.919 31.383,18.076 C31.756,18.23 32,18.596 32,19 M30,58 C14.561,58 2,45.439 2,30 C2,14.561 14.561,2 30,2 C45.439,2 58,14.561 58,30 C58,45.439 45.439,58 30,58 M30,0 C13.458,0 0,13.458 0,30 C0,46.542 13.458,60 30,60 C46.542,60 60,46.542 60,30 C60,13.458 46.542,0 30,0" id="number-one" /></g></g></svg>;
