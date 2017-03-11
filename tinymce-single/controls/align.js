0eb59e46b1d455517bbf1860f4b84e09aae32550
iseulde <ella@iseulde.com>
2017-03-11 13:49:54 +0100

    Add media placeholders

--- tinymce-single/controls/align.js
+++ tinymce-single/controls/align.js
@@ -1,31 +1,20 @@
-( function( wp, $ ) {
-	var RX_ALIGN_CLASSNAME = /^(text-align-|align)([\w-]+)/;
-
+( function( wp ) {
 	function createOnClick( classNamePrefix, position ) {
 		return function( node ) {
-			return $( node )
-				.removeClass( function( i, className ) {
-					return RX_ALIGN_CLASSNAME.test( className ) ? className : '';
-				} )
-				.addClass( classNamePrefix + position );
+			node.className = node.className.replace( new RegExp( classNamePrefix + '[^ ]+ ?', 'g' ), '' ).trim();
+			node.className += ( node.className ? ' ' : '' ) + classNamePrefix + position;
 		};
 	}
 
-	function createIsActive( classNamePrefix, position ) {
+	function createIsActive( classNamePrefix, position, normal ) {
 		return function( node ) {
-			var classNames = node.className.split( ' ' ),
-				i, className, match;
+			var matches = node.className.match( new RegExp( classNamePrefix + '([^ ]+)' ) );
 
-			for ( i = 0; i < classNames.length; i++ ) {
-				className = classNames[ i ];
-				match = className.match( RX_ALIGN_CLASSNAME );
-
-				if ( match && classNamePrefix === match[ 1 ] ) {
-					return position === match[ 2 ];
-				}
+			if ( matches ) {
+				return position === matches[ 1 ];
+			} else {
+				return position === normal;
 			}
-
-			return 'left' === position;
 		};
 	}
 
@@ -33,13 +22,13 @@
 		wp.blocks.registerControl( 'text-align-' + position, {
 			icon: 'gridicons-align-' + position,
 			onClick: createOnClick( 'text-align-', position ),
-			isActive: createIsActive( 'text-align-', position )
+			isActive: createIsActive( 'text-align-', position, 'left' )
 		} );
 
 		wp.blocks.registerControl( 'block-align-' + position, {
 			icon: 'gridicons-align-image-' + position,
 			onClick: createOnClick( 'align', position ),
-			isActive: createIsActive( 'align', position )
+			isActive: createIsActive( 'align', position, 'center' )
 		} );
 	} );
-} )( window.wp = window.wp || {}, window.jQuery );
+} )( window.wp );
