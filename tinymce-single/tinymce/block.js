8c249fcae3aba9ac64dc690a514e30858c049e36
Maurizio Napoleoni <napy84@gmail.com>
2017-03-13 22:15:17 +1000

    Keep the selection on drop

--- tinymce-single/tinymce/block.js
+++ tinymce-single/tinymce/block.js
@@ -312,7 +312,7 @@
 			editor.buttons.link.icon = 'gridicons-link';
 
 			var blockToolbarWidth = 0;
-
+			var beforeSelection;
 			function createBlockOutline( hover ) {
 				var outline = document.createElement( 'div' );
 				var handleLeft = document.createElement( 'div' );
@@ -332,6 +332,7 @@
 
 				DOM.bind( outline, 'mousedown', function( event ) {
 					var newEvent = Object.assign( {}, event );
+					beforeSelection = editor.selection.getBookmark();
 
 					if ( hover ) {
 						dragTarget = hoverTarget;
@@ -378,7 +379,7 @@
 								$draggedNode[0].removeAttribute( 'contenteditable' );
 							}
 						}
-
+						editor.selection.moveToBookmark(beforeSelection);
 						editor.nodeChanged();
 					} );
 				}
