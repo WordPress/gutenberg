d7b771424d7f27e9ef345dbfd49fe3dfd291fe28
iseulde <ella@iseulde.com>
2017-03-13 23:54:17 +0100

    Add custom text block for testing

---
+++ tinymce-single/blocks/my-awesome-plugin/custom-blue-box/register.js
@@ -0,0 +1,51 @@
+( function( wp ) {
+
+	function insertEmpty() {
+		return '<section><p><br></p></section>';
+	}
+
+	function fromBaseState( block, editor ) {
+		var section = document.createElement( 'section' );
+
+		section.setAttribute( 'data-wp-block-type', 'my-awesome-plugin:custom-blue-box' );
+
+		block.parentNode.insertBefore( section, block );
+
+		section.appendChild( block );
+	}
+
+	function toBaseState( block ) {
+		var firstChild = block.firstChild;
+
+		while ( block.firstChild ) {
+			block.parentNode.insertBefore( block.firstChild, block );
+		}
+
+		block.parentNode.removeChild( block );
+
+		window.wp.blocks.selectBlock( firstChild );
+	}
+
+	wp.blocks.registerBlock( {
+		name: 'custom-blue-box',
+		namespace: 'my-awesome-plugin',
+		displayName: 'Custom Box',
+		icon: 'gridicons-custom-post-type',
+		type: 'text',
+		editable: true,
+		placeholders: {
+			'p:first': 'Write in the magic box! ✨'
+		},
+		insert: insertEmpty,
+		fromBaseState: fromBaseState,
+		toBaseState: toBaseState,
+		controls: [
+			{
+				classes: 'remove-formatting',
+				icon: 'gridicons-custom-post-type',
+				onClick: toBaseState
+			}
+		]
+	} );
+
+} )( window.wp );
