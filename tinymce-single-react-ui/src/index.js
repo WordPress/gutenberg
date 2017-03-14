0420fe18cce7bbb3925a603698366b1918ac74f5
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-14 18:42:38 +1000

    tiny-with-react-ui: add focus and blur methods

--- tinymce-single-react-ui/src/index.js
+++ tinymce-single-react-ui/src/index.js
@@ -2,37 +2,34 @@ import '../../shared/post-content'
 import 'assets/stylesheets/main.scss'
 import React, { createElement, Component } from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
 import * as Icons from './external/dashicons/index'
 import Toolbar from './components/toolbar/Toolbar'
 import TinyMCEReactUI from './components/tinymce/tinymce-react-ui'
+import action from './reducers/tinymce/tinymce-react-ui'
 
-// ////////////////////////////////
-import { createStore } from 'redux'
-import Counter from './components/tinymce/components/Counter'
-import counter from './components/tinymce/reducers/counter'
+const store = createStore(action)
 
-const store = createStore(counter)
-
-const renderIt = () => render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  document.getElementById('root')
-)
-
-renderIt()
-store.subscribe(renderIt)
-// ////////////////////////////////
-
-
-render(
+const renderApp = () => render(
 	<div>
 		<Toolbar />
-		<TinyMCEReactUI content={window.content} />
+		<TinyMCEReactUI content={window.content}
+			onFocus={(bookmark) => store.dispatch({type: 'FOCUS', val: bookmark})}
+			onBlur={(bookmark) => store.dispatch({type: 'BLUR', val: bookmark})}
+			/>
 		<hr />
 		<br />
 	</div>,
 	document.getElementById('tiny-react')
 );
+
+renderApp()
+store.subscribe(renderApp)
+
+/*
+		<Counter
+			value={store.getState()}
+			onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+			onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+		/>
+*/
