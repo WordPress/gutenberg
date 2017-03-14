0818b9cc71d8c1959dfb95a5fb1927e57b3c9562
Maurizio Napoleoni <napy84@gmail.com>
2017-03-14 18:42:57 +1000

    tiny-with-react-ui: align

--- tinymce-single-react-ui/src/components/toolbar/Toolbar.js
+++ tinymce-single-react-ui/src/components/toolbar/Toolbar.js
@@ -7,16 +7,32 @@ import styles from './toolbar.scss'
 export default class Toolbar extends React.Component {
 	render() {
 		return (
-			<div className={styles.toolbar}>
-        <Button>
-					<Icons.EditorBoldIcon />
-				</Button>
-				<Button>
-					<Icons.EditorItalicIcon />
-				</Button>
-				<Button>
-					<Icons.EditorStrikethroughIcon />
-				</Button>
+			<div>
+				<div className={styles.toolbar}>
+					<Button>
+						<Icons.EditorBoldIcon />
+					</Button>
+					<Button>
+						<Icons.EditorItalicIcon />
+					</Button>
+					<Button>
+						<Icons.EditorStrikethroughIcon />
+					</Button>
+				</div>
+				<div className={styles.toolbar}>
+					<div>
+						<Button>
+							<Icons.EditorAlignLeftIcon />
+						</Button>
+						<Button>
+							<Icons.EditorAlignCenterIcon />
+						</Button>
+						<Button>
+							<Icons.EditorAlignRightIcon />
+						</Button>
+					</div>
+
+				</div>
 			</div>
 		)
 	}
