da4c6055350252edf5d24db5969894885f14c079
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-14 13:02:47 +1000

    tiny-with-react-ui: pull in ui modules

--- tinymce-single-react-ui/src/tinymce-react/tinymce-react-ui.js
+++ tinymce-single-react-ui/src/tinymce-react/tinymce-react-ui.js
@@ -1,6 +1,8 @@
 import { render } from 'react-dom';
 import { createElement, Component } from 'react';
 import TinyMCE from 'react-tinymce';
+import './react-toolbar'
+import './react-block'
 
 export default class TinyMCEReactUI extends Component {
   constructor(props) {
@@ -21,8 +23,16 @@ export default class TinyMCEReactUI extends Component {
             theme: false,
             inline: true,
             toolbar: false,
+            menubar: false,
+            keep_styles: false,
+            browser_spellcheck: true,
+            plugins: [
+              'react-toolbar', // fns to make button, list-box, menuitem,...
+              'react-block'    // block toolbar
+              ],
             skin_url: '//s1.wp.com/wp-includes/js/tinymce/skins/lightgray',
             entity_encoding: 'raw',
+            schema: 'html5-strict',
             formats: {
               strikethrough: { inline: 'del' }
             }
