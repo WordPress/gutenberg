1870b194940e3f550f743733dd73f702e318a9a2
Maurizio Napoleoni <napy84@gmail.com>
2017-03-14 14:17:36 +1000

    tiny-with-react-ui: Components

--- tinymce-single-react-ui/src/components/Toolbar.js
+++ tinymce-single-react-ui/src/components/Toolbar.js
@@ -1,4 +1,23 @@
-import React from 'react';
+import React, { createElement, Component } from 'react';
 import ReactDOM from 'react-dom';
+import * as Icons from '../external/dashicons';
+import Button from './Button';
 
 
+export default class Toolbar extends React.Component {
+	render() {
+		return (
+			<div>
+        <Button>
+					<Icons.EditorBoldIcon />
+				</Button>
+				<Button>
+					<Icons.EditorItalicIcon />
+				</Button>
+				<Button>
+					<Icons.EditorStrikethroughIcon />
+				</Button>
+			</div>
+		)
+	}
+}
--- tinymce-single-react-ui/src/index.js
+++ tinymce-single-react-ui/src/index.js
@@ -4,41 +4,9 @@
 import '../../shared/post-content';
 
 import React, { createElement, Component } from 'react';
-import * as Icons from './external/dashicons/index';
 import { render } from 'react-dom';
-
+import Toolbar from './components/Toolbar'
 import TinyMCEReactUI from 'tinymce-react/tinymce-react-ui';
-
-class Button extends Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div className="button"> { this.props.children } </div>
-		)
-	}
-}
-
-class Toolbar extends Component {
-	render() {
-		return (
-			<div>
-				<Button>
-					<Icons.EditorBoldIcon />
-				</Button>
-				<Button>
-					<Icons.EditorItalicIcon />
-				</Button>
-				<Button>
-					<Icons.EditorStrikethroughIcon />
-				</Button>
-			</div>
-		)
-	}
-}
-
 
 render(
 	<div>
