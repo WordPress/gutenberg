eb9a4d195b19e75d0c16b3eef00f94a3647270c5
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-15 08:29:04 +1000

    tiny-with-react-ui: hook up buttons to status logic

--- tinymce-single-react-ui/src/components/toolbar/Toolbar.js
+++ tinymce-single-react-ui/src/components/toolbar/Toolbar.js
@@ -2,11 +2,19 @@ import React, { createElement, Component } from 'react'
 import ReactDOM from 'react-dom'
 import cx from 'classnames';
 
-
 import * as Icons from '../../external/dashicons'
 import Dropbutton from '../dropbutton/Dropbutton'
 import Button from '../button/Button'
 import styles from './toolbar.scss'
+import {isBold, isItalic, isDel, isLink} from '../../utils/tag'
+
+const nodeOrParent = (pred, el) => (
+	pred(el.nodeName) || (el.parentNode && pred(el.parentNode.nodeName))
+)
+
+const status = (pred, el) => (
+	( el && nodeOrParent(pred, el) ) ? 'ACTIVE' : 'INACTIVE'
+)
 
 export default class Toolbar extends React.Component {
 
@@ -25,13 +33,13 @@ export default class Toolbar extends React.Component {
 		return (
 			<div>
 				<div className={styles.toolbar}>
-					<Button status={'ACTIVE'}>
+					<Button status={ status( isBold, this.props.node ) } >
 						<Icons.EditorBoldIcon />
 					</Button>
-					<Button status={'INACTIVE'}>
+					<Button status={ status( isItalic, this.props.node ) } >
 						<Icons.EditorItalicIcon />
 					</Button>
-					<Button>
+					<Button status={ status( isDel, this.props.node ) } >
 						<Icons.EditorStrikethroughIcon />
 					</Button>
 				</div>
