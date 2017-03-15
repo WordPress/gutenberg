212b97f0ad098675110cf787143ccce5f6ffb02f
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-16 00:08:16 +1000

    tiny-with-react-ui: fix up

--- tinymce-single-react-ui/src/Turducken.js
+++ tinymce-single-react-ui/src/Turducken.js
@@ -12,13 +12,9 @@ import 'assets/stylesheets/main.scss'
 let blockOpen  = (collapsed) => (collapsed !== null &&  collapsed) // block  if caret
 let inlineOpen = (collapsed) => (collapsed !== null && !collapsed) // inline if range selection
 
-let findBlockType = (editorNode, node) => {
-	if (editorNode && editorNode.children.length > 0 && node) {
-		let top = getTopLevelBlock(editorNode.children[0], node)
-		let topType = blockType(top)
-		return topType
-	}
-}
+// get tiny node from the container, and the top level block from the caret node
+let tinyNode = (containerNode) => ((containerNode && containerNode.children.length > 0) ? containerNode.children[0] : null)
+let topLevelBlock = (tinyNode, node) => ((tinyNode && node) ? getTopLevelBlock(tinyNode, node) : null)
 
 // Rect at the start of the Range
 let findStartOfRange = (range) => {
@@ -39,19 +35,26 @@ let rangeRect = (range) => {
 
 export default function Turducken(props) {
   let store = props.store
+  let collapsed = store.getState().get('collapsed')
+  let range = store.getState().get('range')
+  let node =  store.getState().get('node') // node of caret or ancestor of range
+  let editorRef = store.getState().get('editorRef')
+  let tiny = tinyNode(editorRef)
+  let topBlock = topLevelBlock(tiny, node)
+
   return (
     <div>
       <div>
-        <InlineToolbar isOpen={ inlineOpen(store.getState().get('collapsed')) }
-          rangeRect={ findStartOfRange(store.getState().get('range')) }
+        <InlineToolbar isOpen={ inlineOpen(collapsed) }
+          rangeRect={ findStartOfRange(range) }
           pageYOffset={ window.pageYOffset }
-          node={ store.getState().get('node') }
+          node={ node }
           />
-        <BlockToolbar  isOpen={ blockOpen(store.getState().get('collapsed')) }
-          blockType={ findBlockType(store.getState().get('editorRef'), store.getState().get('node')) }
-          blockAlign={ blockAlign(store.getState().get('node')) }
-          blockRect={ rangeRect(store.getState().get('node')) }
-        />
+        <BlockToolbar  isOpen={ blockOpen(collapsed) }
+          blockType={ blockType(topBlock) }
+          blockAlign={ blockAlign(topBlock) }
+          blockRect={ rangeRect(topBlock) }
+          />
       </div>
       <TinyMCEReact content={window.content}
         onSetup={ ( editorRef ) => store.dispatch( { type: 'SETUP', val: editorRef } ) }
