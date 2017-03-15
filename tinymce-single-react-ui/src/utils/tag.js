8bfda3e433b8758cb99859271a9da9ce2e8e8b51
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-15 14:15:15 +1000

    tiny-with-react-ui: helper funs that need to be somewhere better

--- tinymce-single-react-ui/src/utils/tag.js
+++ tinymce-single-react-ui/src/utils/tag.js
@@ -7,6 +7,13 @@ export const isItalic = (tag) => member( [ 'EM', 'I' ], tag )
 export const isDel = (tag) => member( [ 'DEL' ], tag )
 export const isLink = (tag) => member( [ 'A' ], tag )
 
+// valid blocks
+export const blockMap = {P: 'p', H1: 'h', H2: 'h', BLOCKQUOTE: 'blockquote'}
+export const blockList = ['p', 'h', 'blockquote']
+// TODO: dont default 'p' for unsupported tag
+export const blockType = (el) => ( (el && blockMap[el.nodeName]) || 'p')
+
+
 // Find the top-level block
 // export const getTopLevelBlock = ( editor, node ) => {
 // 		var rootNode = editor.getBody();
