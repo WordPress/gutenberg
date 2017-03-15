ebcf407efbeb99724139ca510144eba9e5a82842
Mike Pheasant <mike.pheasant@ephox.com>
2017-03-15 13:33:20 +1000

    tiny-with-react-ui: wip

--- tinymce-single-react-ui/src/utils/tag.js
+++ tinymce-single-react-ui/src/utils/tag.js
@@ -6,3 +6,18 @@ export const isBold = (tag) => member( [ 'STRONG', 'B' ], tag )
 export const isItalic = (tag) => member( [ 'EM', 'I' ], tag )
 export const isDel = (tag) => member( [ 'DEL' ], tag )
 export const isLink = (tag) => member( [ 'A' ], tag )
+
+// Find the top-level block
+// export const getTopLevelBlock = ( editor, node ) => {
+// 		var rootNode = editor.getBody();
+
+// 		if ( node === rootNode || ! editor.getBody().contains( node ) ) {
+// 			return null;
+// 		}
+
+// 		while ( node.parentNode !== rootNode ) {
+// 			node = node.parentNode;
+// 		}
+
+// 		return node;
+// 	}
