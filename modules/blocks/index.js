203c2baa49d05270feeb6777e968d9012f0dde68
Riad Benguella <benguella@gmail.com>
2017-03-17 16:22:39 +0100

    Changes per review

--- modules/blocks/index.js
+++ modules/blocks/index.js
@@ -1,47 +1,40 @@
 export { default as Editable } from './components/editable';
 
+/**
+ * Block settings keyed by block name.
+ *
+ * @var {Object} blocks
+ */
 const blocks = {};
 
 /**
  * Registers a block.
  *
- * @param  {string} namespace Block grouping unique to package or plugin
- * @param  {string} block     Block name
+ * @param  {string} name      Block name
  * @param  {Object} settings  Block settings
  */
-export function registerBlock( namespace, block, settings ) {
-	if ( ! blocks.namespace ) {
-		blocks[ namespace ] = {};
+export function registerBlock( name, settings ) {
+	if ( ! /^\w+\/\w+$/.test( name ) ) {
+		throw new Error( 'Use a namespace for your block name please. Example: namespace/block' );
 	}
-
-	blocks[ namespace ][ block ] = Object.assign(
-		{ name: block, namespace },
-		settings
-	);
+	blocks[ name ] = Object.assign( { name }, settings );
 }
 
 /**
  * Returns settings associated with a block.
  *
- * @param  {string}  namespace Block grouping unique to package or plugin
- * @param  {string}  block     Block name
- * @return {?Object}           Block settings
+ * @param  {string}  name Block name
+ * @return {?Object}      Block settings
  */
-export function getBlockSettings( namespace, block ) {
-	if ( ! blocks[ namespace ] || ! blocks[ namespace ][ block ] ) {
-		return null;
-	}
-
-	return blocks[ namespace ][ block ];
+export function getBlockSettings( name ) {
+	return blocks[ name ];
 }
 
 /**
  * Returns all registered blocks.
  *
- * @return {Array} Blocks settings
+ * @return {Array} Block settings
  */
 export function getBlocks() {
-	return Object.values( blocks ).reduce( ( memo, namespaceBlocks ) => {
-		return memo.concat( Object.values( namespaceBlocks ) );
-	} );
+	return Object.values( blocks );
 }
