f2b46d6c5546081036312eb066d61b7751282e66
Riad Benguella <benguella@gmail.com>
2017-03-21 13:11:21 +0100

    Plugin: Adding a first version of block parser using a `pegjs` grammar

--- modules/editor/editor.js
+++ modules/editor/editor.js
@@ -1,5 +1,10 @@
+import { parse } from './grammar';
+
 export default class Editor {
 	constructor( id, settings ) {
+		const blocks = parse( settings.content );
+		console.log( blocks );
+
 		document.getElementById( id ).innerHTML = settings.content;
 	}
 }
