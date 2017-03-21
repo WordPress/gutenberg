0f1ea2a4deaa3e60fbf238349260125543aeef3b
Andrew Duthie <andrew@andrewduthie.com>
2017-03-21 11:23:25 -0400

    Avoid assigning globals

--- bootstrap-test.js
+++ bootstrap-test.js
@@ -4,10 +4,4 @@
 import chai from 'chai';
 import dirtyChai from 'dirty-chai';
 
-/** Internal dependencies */
-import * as blocks from './modules/blocks';
-import * as element from './modules/element';
-
 chai.use( dirtyChai );
-
-global.wp = { blocks, element };
