20974d9a07635a6cbf5311fd6b83307f5250106a
Riad Benguella <benguella@gmail.com>
2017-03-23 11:48:05 +0100

    Plugin: Avoid throwing errors on invalid block registration

--- modules/blocks/test/registration.js
+++ modules/blocks/test/registration.js
@@ -2,50 +2,57 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 /**
  * Internal dependencies
  */
 import * as blocks from '../registration';
 
+/* eslint-disable no-console */
 describe( 'blocks API', () => {
 	// Reset block state before each test.
 	beforeEach( () => {
 		blocks.getBlocks().forEach( block => {
 			blocks.unregisterBlock( block.slug );
 		} );
+		sinon.spy( console, 'error' );
+	} );
+
+	afterEach( () => {
+		console.error.restore();
 	} );
 
 	describe( 'registerBlock', () => {
 		it( 'should reject numbers', () => {
-			expect(
-				() => blocks.registerBlock( 999 )
-			).to.throw( 'Block slugs must be strings.' );
+			const isRegistered = blocks.registerBlock( 999 );
+			expect( console.error ).to.have.been.calledWith( 'Block slugs must be strings.' );
+			expect( isRegistered ).to.eql( false );
 		} );
 
 		it( 'should reject blocks without a namespace', () => {
-			expect(
-				() => blocks.registerBlock( 'doing-it-wrong' )
-			).to.throw( /^Block slugs must contain a namespace prefix/ );
+			const isRegistered = blocks.registerBlock( 'doing-it-wrong' );
+			expect( console.error ).to.have.been.calledWith( 'Block slugs must contain a namespace prefix. Example: my-plugin/my-custom-block' );
+			expect( isRegistered ).to.eql( false );
 		} );
 
 		it( 'should reject blocks with invalid characters', () => {
-			expect(
-				() => blocks.registerBlock( 'still/_doing_it_wrong' )
-			).to.throw( /^Block slugs must contain a namespace prefix/ );
+			const isRegistered = blocks.registerBlock( 'still/_doing_it_wrong' );
+			expect( console.error ).to.have.been.calledWith( 'Block slugs must contain a namespace prefix. Example: my-plugin/my-custom-block' );
+			expect( isRegistered ).to.eql( false );
 		} );
 
 		it( 'should accept valid block names', () => {
-			expect(
-				() => blocks.registerBlock( 'my-plugin/fancy-block-4' )
-			).not.to.throw();
+			const isRegistered = blocks.registerBlock( 'my-plugin/fancy-block-4' );
+			expect( console.error ).to.not.have.been.called();
+			expect( isRegistered ).to.eql( true );
 		} );
 
 		it( 'should prohibit registering the same block twice', () => {
 			blocks.registerBlock( 'core/test-block' );
-			expect(
-				() => blocks.registerBlock( 'core/test-block' )
-			).to.throw( 'Block "core/test-block" is already registered.' );
+			const isRegistered = blocks.registerBlock( 'core/test-block' );
+			expect( console.error ).to.have.been.calledWith( 'Block "core/test-block" is already registered.' );
+			expect( isRegistered ).to.eql( false );
 		} );
 
 		it( 'should store a copy of block settings', () => {
