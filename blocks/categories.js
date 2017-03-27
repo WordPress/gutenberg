ec9d3463d7e2d8a291f50746a2d4f737bf1003bf
Riad Benguella <benguella@gmail.com>
2017-03-27 14:54:16 +0100

    Inserter: Display block by category
     - Adding the category to the block API
     - Adding `wp.blocks.getCategories`

---
+++ blocks/categories.js
@@ -0,0 +1,18 @@
+
+/**
+ * Block categories
+ *
+ * @var {Array} categories
+ */
+const categories = [
+	{ id: 'common', label: 'Common' }
+];
+
+/**
+ * Returns all the block categories
+ *
+ * @return {Array} Block categories
+ */
+export function getCategories() {
+	return categories;
+}
--- editor/inserter/index.js
+++ editor/inserter/index.js
@@ -1,18 +1,31 @@
+/**
+ * External dependencies
+ */
+import { groupBy } from 'lodash';
+
 function Inserter() {
 	const blocks = wp.blocks.getBlocks();
+	const blocksByCategory = groupBy( blocks, 'category' );
+	const categories = wp.blocks.getCategories();
 
 	return (
 		<div className="inserter">
 			<div className="inserter__arrow" />
 			<div className="inserter__content">
-				<div className="inserter__Category-blocks">
-					{ blocks.map( ( { slug, title, icon } ) => (
-						<div key={ slug } className="inserter__block">
-							<span className={ 'dashicons dashicons-' + icon } />
-							{ title }
+				{ categories
+					.filter( ( category ) => !! blocksByCategory[ category.id ] )
+					.map( ( category ) =>
+						<div key={ category.id } className="inserter__Category-blocks">
+							<div className="inserter__separator">{ category.label }</div>
+							{ blocksByCategory[ category.id ].map( ( { slug, title, icon } ) => (
+								<div key={ slug } className="inserter__block">
+									<span className={ 'dashicons dashicons-' + icon } />
+									{ title }
+								</div>
+							) ) }
 						</div>
-					) ) }
-				</div>
+					)
+				}
 			</div>
 			<input className="inserter__search" type="search" placeholder="Search..." />
 		</div>
