16951bc6245f74f3329e34e805715e9cbd0fab3c
BE-Webdesign <cromleyedwin89@gmail.com>
2017-03-27 19:24:44 -0400

    Revising changes
    Adding in JSX, cleaning up components.

--- editor/blocks/heading-block/index.js
+++ editor/blocks/heading-block/index.js
@@ -1,23 +1,35 @@
-const { query, html, prop } = wp.blocks.query;
+const Editable = wp.blocks.Editable;
+const { html, prop } = wp.blocks.query;
+
+function Heading( { nodeName = 'H1', children } ) {
+	// nodeName.toLowerCase() is used to map DOM nodeName values to proper tag.
+	return wp.element.createElement( nodeName.toLowerCase(), null, children );
+}
 
 wp.blocks.registerBlock( 'core/heading', {
 	title: 'Heading',
 	icon: 'heading',
+	category: 'common',
 
 	attributes: {
-		value: query( 'h1,h2,h3,h4,h5,h6', html() ),
-		headingType: query( 'h1,h2,h3,h4,h5,h6', prop( 'nodeName' ) ),
+		value: html( 'h1,h2,h3,h4,h5,h6' ),
+		headingType: prop( 'h1,h2,h3,h4,h5,h6', 'nodeName' )
 	},
 
 	edit( attributes, onChange ) {
-		return wp.element.createElement( wp.blocks.EditableHeading, {
-			value: attributes.value,
-			headingType: attributes.headingType,
-			onChange: ( value ) => onChange( { value } )
-		} );
+		const { headingType = 'H1', value } = attributes;
+
+		return (
+			<Heading nodeName={ headingType }>
+				<Editable
+					value={ value }
+					onChange={ ( nextValue ) => onChange( { value: nextValue } ) } />
+			</Heading>
+		);
 	},
 
 	save( attributes ) {
-		return wp.element.createElement( 'p', null, attributes.value );
+		const { headingType = 'H1', value } = attributes;
+		return <Heading nodeName={ headingType }>{ value }</Heading>;
 	}
 } );
