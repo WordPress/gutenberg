4897fae702e5150584076d6da4fc285277aa16a5
Riad Benguella <benguella@gmail.com>
2017-03-28 14:41:20 +0100

    Editor Chrome: Adding the editor mode switcher
     - Adding the `onChange` content handler
     - v1 of the editor header
     - Adding a v1 the text editor

---
+++ editor/editor/layout.js
@@ -0,0 +1,67 @@
+/**
+ * Internal dependencies
+ */
+import ModeSwitcher from './mode-switcher';
+import EditorText from './mode/text';
+import EditorVisual from './mode/visual';
+
+class Layout extends wp.element.Component {
+	constructor() {
+		super( ...arguments );
+		this.switchMode = this.switchMode.bind( this );
+		this.changeHtml = this.changeHtml.bind( this );
+		this.changeBlocks = this.changeBlocks.bind( this );
+		this.state = {
+			mode: 'visual',
+			html: '',
+			blocks: []
+		};
+	}
+
+	componentDidMount() {
+		this.setState( {
+			html: this.props.content,
+			blocks: wp.blocks.parse( this.props.content )
+		} );
+	}
+
+	switchMode( newMode ) {
+		// TODO: we need a serializer from blocks here
+		const html = this.state.html;
+		const blocks = this.mode === 'visual' ? this.state.blocks : wp.blocks.parse( this.state.html );
+		this.setState( {
+			mode: newMode,
+			html,
+			blocks
+		} );
+	}
+
+	changeHtml( html ) {
+		this.setState( {
+			html
+		} );
+	}
+
+	changeBlocks( blocks ) {
+		this.setState( {
+			blocks
+		} );
+	}
+
+	render() {
+		const { mode, html, blocks } = this.state;
+		return (
+			<div>
+				<div className="editor__header">
+					<ModeSwitcher mode={ mode } onSwitch={ this.switchMode } />
+				</div>
+				<div className="editor__body">
+					{ mode === 'text' && <EditorText html={ html } onChange={ this.changeHtml } /> }
+					{ mode === 'visual' && <EditorVisual blocks={ blocks } onChange={ this.changeBlocks } /> }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Layout;
---
+++ editor/editor/mode/text.js
@@ -0,0 +1,9 @@
+const Text = ( { html, onChange } ) => {
+	return (
+		<div className="editor-mode-text">
+			<textarea value={ html } onChange={ onChange } />
+		</div>
+	);
+};
+
+export default Text;
