a6b4e9f541e10cd5e73b5e05c0e2d309f2f9b638
Maurizio Napoleoni <napy84@gmail.com>
2017-03-31 13:47:30 +1000

    list-block: restored original inserter

--- editor/inserter/index.js
+++ editor/inserter/index.js
@@ -1,31 +1,22 @@
-class Inserter extends wp.element.Component {
-	constructor(...args) {
-		super(...args);
-	}
+function Inserter() {
+	const blocks = wp.blocks.getBlocks();
 
-	inserterAction() {
-		debugger;
-	}
-
-	render () {
-		const blocks = wp.blocks.getBlocks();
-		return (
-			<div className="inserter">
-				<div className="inserter__arrow" />
-				<div className="inserter__content">
-					<div className="inserter__Category-blocks">
-						{ blocks.map( ( { slug, title, icon } ) => (
-							<div key={ slug } className="inserter__block" onClick={ this.inserterAction } >
-								<span className={ 'dashicons dashicons-' + icon }  />
-								{ title }
-							</div>
-						) ) }
-					</div>
+	return (
+		<div className="inserter">
+			<div className="inserter__arrow" />
+			<div className="inserter__content">
+				<div className="inserter__Category-blocks">
+					{ blocks.map( ( { slug, title, icon } ) => (
+						<div key={ slug } className="inserter__block">
+							<span className={ 'dashicons dashicons-' + icon } />
+							{ title }
+						</div>
+					) ) }
 				</div>
-				<input className="inserter__search" type="search" placeholder="Search..." />
 			</div>
-		);
-	}
+			<input className="inserter__search" type="search" placeholder="Search..." />
+		</div>
+	);
 }
 
 export default Inserter;
