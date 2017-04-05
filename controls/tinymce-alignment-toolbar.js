4b1fff11a8018e9146e66f4f8e4833b38c439151
Mike Pheasant <mike.pheasant@ephox.com>
2017-04-06 09:37:30 +1000

    wrap the aligment toolbar with TinyMCE justify actions

---
+++ controls/tinymce-alignment-toolbar.js
@@ -0,0 +1,11 @@
+import AlignmentToolbar from './alignment-toolbar';
+
+const TinyMCEAlignmentToolbar = ( props ) => (
+	<AlignmentToolbar { ...props } actions={ {
+		left: tinymce.execCommand( 'JustifyLeft' ),
+		center: tinymce.execCommand( 'JustifyCenter' ),
+		right: tinymce.execCommand( 'JustifyRight' ),
+	} } />
+);
+
+export default TinyMCEAlignmentToolbar;
