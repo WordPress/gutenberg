60efc4d41c93270f8bb0d0aff663aee9ddb30b94
Mike Pheasant <mike.pheasant@ephox.com>
2017-04-06 13:05:40 +1000

    check if the editor has any element with focus

--- editor/modes/visual-editor/index.js
+++ editor/modes/visual-editor/index.js
@@ -9,17 +9,38 @@ import { connect } from 'react-redux';
 import InserterButton from '../../inserter/button';
 import VisualEditorBlock from './block';
 
-function VisualEditor( { blocks } ) {
-	return (
-		<div className="editor-visual-editor">
-			{ blocks.map( ( uid ) => (
-				<VisualEditorBlock key={ uid } uid={ uid } />
-			) ) }
-			<InserterButton />
-		</div>
-	);
+class VisualEditor extends wp.element.Component {
+	blurWithDomEl( onBlur ) {
+		return onBlur( this._domElement );
+	}
+
+	render() {
+		const { blocks, onBlur } = this.props;
+
+		return (
+			<div className="editor-visual-editor" onBlur={ () => this.blurWithDomEl( onBlur ) } ref={ ( el ) => this._domElement = el } >
+				{ blocks.map( ( uid ) => (
+					<VisualEditorBlock key={ uid } uid={ uid } />
+				) ) }
+				<InserterButton />
+			</div>
+		);
+	}
 }
 
-export default connect( ( state ) => ( {
+const mapStateToProps = ( state ) => ( {
 	blocks: state.blocks.order
-} ) )( VisualEditor );
+} );
+
+const mapDispatchToProps = 	( dispatch ) => ( {
+	onBlur( containerEl ) {
+		// if the VisualEditor gets a blur event, and if the document activeElement is not inside
+		// the VisualEditor, then it has lost focus
+		if ( ! document.activeElement || ( containerEl && ! containerEl.contains( document.activeElement ) ) ) {
+			dispatch( {
+				type: 'EDITOR_LOST_FOCUS'
+			} );
+		}
+	} } );
+
+export default connect( mapStateToProps, mapDispatchToProps )( VisualEditor );
