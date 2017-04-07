d893ef7e64d7bd03fb40a57d96bb548dd9e21196
Joen Asmussen <joen@automattic.com>
2017-04-07 13:02:23 +0200

    Add basic dummy items to toolbar.

--- editor/header/index.js
+++ editor/header/index.js
@@ -3,11 +3,26 @@
  */
 import './style.scss';
 import ModeSwitcher from './mode-switcher';
+import Dashicon from '../components/dashicon';
 
 function Header() {
 	return (
 		<header className="editor-header">
 			<ModeSwitcher />
+			<div className="editor-saved-state">
+				<Dashicon icon="yes" />
+				Saved
+			</div>
+			<div className="editor-tools">
+				<button className="editor-icon-button"><Dashicon icon="undo" /></button>
+				<button className="editor-icon-button"><Dashicon icon="redo" /></button>
+				<button className="editor-icon-button"><Dashicon icon="plus-alt" /></button>
+				<div className="editor-tools__tabs">
+					<button><Dashicon icon="visibility" /> Preview</button>
+					<button><Dashicon icon="admin-generic" /> Post Settings</button>
+				</div>
+				<button className="button button-primary button-large">Publish</button>
+			</div>
 		</header>
 	);
 }
