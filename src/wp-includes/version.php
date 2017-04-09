860363337f8623ef5edfc7ea31e9dec85f082e93
Andrew Ozz <azaozz@git.wordpress.org>
2017-04-09 23:00:47 +0000

    Update TinyMCE to 4.5.6. Has many improvements and bug fixes. Changelog: https://github.com/tinymce/tinymce/blob/4.5.x/changelog.txt.
    Fixes #40305 for trunk.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@40398 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4403-20160901';
+$tinymce_version = '4506-20170408';
 
 /**
  * Holds the required PHP version
