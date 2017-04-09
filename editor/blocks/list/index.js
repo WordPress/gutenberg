71829a7d42e85974a51800c95f7feab6895a73ad
Maurizio Napoleoni <napy84@gmail.com>
2017-04-10 08:09:07 +1000

    Block align toolbar

--- editor/blocks/list/index.js
+++ editor/blocks/list/index.js
@@ -16,16 +16,45 @@ wp.blocks.registerBlock( 'core/list', {
 		)
 	},
 
-	edit( { attributes } ) {
-		const { listType = 'ol', items = [] } = attributes;
-		const value = items.map( item => {
+	controls: [
+		{
+			icon: 'editor-alignleft',
+			title: wp.i18n.__( 'Align left' ),
+			isActive: ( { align } ) => ! align || 'left' === align,
+			onClick( attributes, setAttributes ) {
+				setAttributes( { align: undefined } );
+			}
+		},
+		{
+			icon: 'editor-aligncenter',
+			title: wp.i18n.__( 'Align center' ),
+			isActive: ( { align } ) => 'center' === align,
+			onClick( attributes, setAttributes ) {
+				setAttributes( { align: 'center' } );
+			}
+		},
+		{
+			icon: 'editor-alignright',
+			title: wp.i18n.__( 'Align right' ),
+			isActive: ( { align } ) => 'right' === align,
+			onClick( attributes, setAttributes ) {
+				setAttributes( { align: 'right' } );
+			}
+		}
+	],
+
+	edit( { attributes, setAttributes } ) {
+		const { listType = 'ol', items = [], align } = attributes;
+		const content = items.map( item => {
 			return `<li>${ item.value }</li>`;
 		} ).join( '' );
 
 		return (
 			<Editable
 				tagName={ listType }
-				value={ value } />
+				onChange={ ( value ) => setAttributes( { content: value } ) }
+				style={ align ? { textAlign: align } : null }
+				value={ content } />
 		);
 	},
 
