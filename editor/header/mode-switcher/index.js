c3dc925cbe6cd0760ead1df6a851bc50ed250af2
Andrew Duthie <andrew@andrewduthie.com>
2017-04-10 14:08:02 -0400

    Use mode switcher mode value from state directly

--- editor/header/mode-switcher/index.js
+++ editor/header/mode-switcher/index.js
@@ -9,41 +9,45 @@ import { connect } from 'react-redux';
 import './style.scss';
 import Dashicon from '../../components/dashicon';
 
-class ModeSwitcher extends wp.element.Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			value: this.props.mode
-		};
-
-		this.switchMode = this.switchMode.bind( this );
+/**
+ * Set of available mode options.
+ *
+ * @type {Array}
+ */
+const MODES = [
+	{
+		value: 'visual',
+		label: wp.i18n.__( 'Visual' )
+	},
+	{
+		value: 'text',
+		label: wp.i18n._x( 'Text', 'Name for the Text editor tab (formerly HTML)' )
 	}
+];
 
-	switchMode( mode ) {
-		this.setState( { value: mode.target.value } );
-		this.props.onSwitch( mode.target.value );
-	}
+function ModeSwitcher( { mode, onSwitch } ) {
+	// Disable reason: Toggling between modes should take effect immediately,
+	// arguably even with keyboard navigation. `onBlur` only would require
+	// another action to remove focus from the select (tabbing or clicking
+	// outside the field), which is unexpected when submit button is omitted.
 
-	render() {
-		const modes = [
-			{ value: 'visual', label: wp.i18n.__( 'Visual' ) },
-			{ value: 'text', label: wp.i18n._x( 'Text', 'Name for the Text editor tab (formerly HTML)' ) },
-		];
-		//const currentMode = modes.find( ( { value } ) => value === this.props.mode );
-
-		return (
-			<div className="editor-mode-switcher">
-				<select value={ this.state.value } onChange={ this.switchMode }>
-					{ modes.map( ( mode ) =>
-						<option key={ mode.value } value={ mode.value }>
-							{ mode.label }
-						</option>
-					) }
-				</select>
-				<Dashicon icon="arrow-down" />
-			</div>
-		);
-	}
+	/* eslint-disable jsx-a11y/no-onchange */
+	return (
+		<div className="editor-mode-switcher">
+			<select
+				value={ mode }
+				onChange={ ( event ) => onSwitch( event.target.value ) }
+			>
+				{ MODES.map( ( { value, label } ) =>
+					<option key={ value } value={ value }>
+						{ label }
+					</option>
+				) }
+			</select>
+			<Dashicon icon="arrow-down" />
+		</div>
+	);
+	/* eslint-enable jsx-a11y/no-onchange */
 }
 
 export default connect(
