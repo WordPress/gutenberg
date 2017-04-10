376d25d609d30dd20e5338922076eef33720b0c2
Andrew Duthie <andrew@andrewduthie.com>
2017-04-10 19:49:40 -0400

    Move block store into nested API directory

---
+++ blocks/api/index.js
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import * as query from 'hpq';
+
+export { query };
+export { default as Editable } from './components/editable';
+export { default as parse } from './parser';
+export { default as serialize } from './serializer';
+export { getCategories } from './categories';
+export {
+	registerBlock,
+	unregisterBlock,
+	setUnknownTypeHandler,
+	getUnknownTypeHandler,
+	getBlockSettings,
+	getBlocks
+} from './registration';
--- blocks/index.js
+++ blocks/index.js
@@ -1,18 +1,1 @@
-/**
- * External dependencies
- */
-import * as query from 'hpq';
-
-export { query };
-export { default as Editable } from './components/editable';
-export { default as parse } from './parser';
-export { default as serialize } from './serializer';
-export { getCategories } from './categories';
-export {
-	registerBlock,
-	unregisterBlock,
-	setUnknownTypeHandler,
-	getUnknownTypeHandler,
-	getBlockSettings,
-	getBlocks
-} from './registration';
+export * from './api';
