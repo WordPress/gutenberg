80c40c748708f0225e0abc7343abd9b37da0d26e
Andrew Duthie <andrew@andrewduthie.com>
2017-04-11 11:08:09 -0400

    Move inserter to components directory

--- editor/modes/visual-editor/index.js
+++ editor/modes/visual-editor/index.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
  * Internal dependencies
  */
 import './style.scss';
-import InserterButton from 'inserter/button';
+import InserterButton from 'components/inserter/button';
 import VisualEditorBlock from './block';
 
 function VisualEditor( { blocks } ) {
