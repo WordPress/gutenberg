15af0d5170f7b5950d11e288768f88352f38ded6
Joen Asmussen <joen@automattic.com>
2017-04-12 14:27:24 +0200

    Add dummy HTML formatting bar.

--- editor/modes/text-editor/index.js
+++ editor/modes/text-editor/index.js
@@ -11,12 +11,32 @@ import './style.scss';
 
 function TextEditor( { blocks, onChange } ) {
 	return (
-		<Textarea
-			defaultValue={ wp.blocks.serialize( blocks ) }
-			onBlur={ ( event ) => onChange( event.target.value ) }
-			className="editor-text-editor"
-			useCacheForDOMMeasurements
-		/>
+		<div>
+			<header className="editor-text-editor__formatting">
+				<div>
+					<button className="editor-text-editor__bold">b</button>
+					<button className="editor-text-editor__italic">i</button>
+					<button className="editor-text-editor__link">link</button>
+					<button>b-quote</button>
+					<button className="editor-text-editor__del">del</button>
+					<button>ins</button>
+					<button>img</button>
+					<button>ul</button>
+					<button>ol</button>
+					<button>li</button>
+					<button>code</button>
+					<button>more</button>
+					<button>close tags</button>
+				</div>
+			</header>
+			<Textarea
+				autoComplete="off"
+				defaultValue={ wp.blocks.serialize( blocks ) }
+				onBlur={ ( event ) => onChange( event.target.value ) }
+				className="editor-text-editor"
+				useCacheForDOMMeasurements
+			/>
+		</div>
 	);
 }
 
