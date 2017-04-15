c013f2990f4c664d3579d23b599da82ff8a4ca04
Riad Benguella <benguella@gmail.com>
2017-04-15 09:44:51 +0100

    Block Switcher: Addjng the transformations API and the block switcher

--- blocks/api/factory.js
+++ blocks/api/factory.js
@@ -4,9 +4,14 @@
 import uuid from 'uuid/v4';
 
 /**
+ * Internal dependencies
+ */
+import { getBlockSettings } from './registration';
+
+/**
  * Returns a block object given its type and attributes
  *
- * @param  {Object} blockType   BlockType
+ * @param  {String} blockType   BlockType
  * @param  {Object} attributes  Block attributes
  * @return {Object}             Block object
  */
@@ -16,4 +21,26 @@ export function createBlock( blockType, attributes = {} ) {
 		blockType,
 		attributes
 	};
+}
+
+/**
+ * Switch Block Type and returns the updated block
+ *
+ * @param  {Object} block      Block object
+ * @param  {string} blockType  BlockType
+ * @return {Object?}           Block object
+ */
+export function switchToBlockType( block, blockType ) {
+	const destinationSettings = getBlockSettings( blockType );
+	const transformation = destinationSettings.transformations
+		.find( t => t.blocks.indexOf( block.blockType ) !== -1 );
+	if ( ! transformation ) {
+		return null;
+	}
+
+	return Object.assign( {
+		uid: block.uid,
+		attributes: transformation.transform( block.attributes ),
+		blockType
+	} );
 }
