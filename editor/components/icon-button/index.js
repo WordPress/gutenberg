51caf6d8cdc5238ba9d5be4ce44afb99eb8d180b
Riad Benguella <benguella@gmail.com>
2017-04-18 14:23:18 +0100

    Components: Fix the icon button level prop

--- editor/components/icon-button/index.js
+++ editor/components/icon-button/index.js
@@ -10,12 +10,13 @@ import './style.scss';
 import Button from '../button';
 import Dashicon from '../dashicon';
 
-function IconButton( { icon, children, label, className, ...additionalProps } ) {
+function IconButton( { icon, level, children, label, className, ...additionalProps } ) {
 	const classes = classnames( 'editor-icon-button', className );
 
 	return (
-		<Button { ...additionalProps } aria-label={ label } className={ classes } data-level={ children ? children : null }>
+		<Button { ...additionalProps } aria-label={ label } className={ classes } data-level={ level ? level : null }>
 			<Dashicon icon={ icon } />
+			{ children }
 		</Button>
 	);
 }
