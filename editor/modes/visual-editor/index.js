1888ee958f45f8bf37c961db1a231d859794cb26
Andrew Duthie <andrew@andrewduthie.com>
2017-04-28 15:33:14 -0400

    Apply order by Flexbox order property

--- editor/modes/visual-editor/index.js
+++ editor/modes/visual-editor/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
+import { map } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,20 +11,21 @@ import './style.scss';
 import Inserter from '../../inserter';
 import VisualEditorBlock from './block';
 import PostTitle from '../../post-title';
-import { getBlockUids } from '../../selectors';
 
 function VisualEditor( { blocks } ) {
 	return (
 		<div className="editor-visual-editor">
 			<PostTitle />
-			{ blocks.map( ( uid ) => (
-				<VisualEditorBlock key={ uid } uid={ uid } />
-			) ) }
+			<div className="editor-visual-editor__blocks">
+				{ map( blocks, ( block, uid ) => (
+					<VisualEditorBlock key={ uid } uid={ uid } />
+				) ) }
+			</div>
 			<Inserter />
 		</div>
 	);
 }
 
 export default connect( ( state ) => ( {
-	blocks: getBlockUids( state ),
+	blocks: state.editor.blocksByUid,
 } ) )( VisualEditor );
