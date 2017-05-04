f0cd8603bec8bce61a3c6736e5eac4cd7156bfe6
Riad Benguella <benguella@gmail.com>
2017-05-04 15:04:56 +0100

    Testing Fix phpcs job

--- phpunit/class-dynamic-blocks-render-test.php
+++ phpunit/class-dynamic-blocks-render-test.php
@@ -1,26 +1,35 @@
 <?php
+/**
+ * Dynamic blocks rendering Test
+ *
+ * @package Gutenberg
+ */
 
-class DynamicBlocksRenderTest extends WP_UnitTestCase {
+/**
+ * Test do_blocks
+ */
+class Dynamic_Blocks_Render_Test extends WP_UnitTestCase {
 	/**
-	 * @expectedIncorrectUsage register_block
+	 * Dummy block rendering function.
+	 *
+	 * @param  array $attributes Block attributes.
+	 *
+	 * @return string             Block output.
 	 */
-	function test_invalid_non_string_slugs() {
-		register_block( 1, array() );
-	}
-
-	/**
-	 * @expectedIncorrectUsage register_block
-	 */
-	function test_invalid_slugs_without_namespace() {
-		register_block( 'text', array() );
-	}
-
 	function render_dummy_block( $attributes ) {
-		return $attributes[ 'value' ];
+		return $attributes['value'];
 	}
 
+	/**
+	 * Successfull dynamic block rendering
+	 */
 	function test_register_block() {
-		$settings = array( 'render' => [ $this, 'render_dummy_block' ] );
+		$settings = array(
+			'render' => array(
+				$this,
+				'render_dummy_block',
+			),
+		);
 		register_block( 'core/dummy', $settings );
 		$post_content =
 			'before' .
--- phpunit/class-registration-test.php
+++ phpunit/class-registration-test.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * Blocks registration Tests
+ *
+ * @package Gutenberg
+ */
 
-class RegistrationTest extends WP_UnitTestCase {
+/**
+ * Test register_block
+ */
+class Registration_Test extends WP_UnitTestCase {
 	/**
+	 * The block slug should be a string
+	 *
 	 * @expectedIncorrectUsage register_block
 	 */
 	function test_invalid_non_string_slugs() {
@@ -9,18 +19,25 @@ class RegistrationTest extends WP_UnitTestCase {
 	}
 
 	/**
+	 * The block slug should have a namespace
+	 *
 	 * @expectedIncorrectUsage register_block
 	 */
 	function test_invalid_slugs_without_namespace() {
 		register_block( 'text', array() );
 	}
 
+	/**
+	 * Successfull block registration
+	 */
 	function test_register_block() {
-		$settings = array( 'icon' => 'text' );
+		$settings = array(
+			'icon' => 'text',
+		);
 		$updated_settings = register_block( 'core/text', $settings );
 		$this->assertEquals( $updated_settings, array(
 			'icon' => 'text',
-			'slug' => 'core/text'
+			'slug' => 'core/text',
 		) );
 		unregister_block( 'core/text' );
 	}
