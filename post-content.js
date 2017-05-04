bc7b24396ed8b4f14433e02b46d9cafbf0da65bf
Matías Ventura <mv@matiasventura.com>
2017-05-04 14:43:04 +0200

    Introduce more blocks into default content, expand text.

--- post-content.js
+++ post-content.js
@@ -8,57 +8,81 @@ window._wpGutenbergPost = {
 			'<h1>Welcome to the Gutenberg Editor</h1>',
 			'<!-- /wp:core/heading -->',
 
-			'<!-- wp:core/button -->',
-			'<div class="aligncenter"><a href="https://github.com/WordPress/gutenberg"><span>Gutenberg on GitHub</span></a></div>',
-			'<!-- /wp:core/button -->',
-
 			'<!-- wp:core/text -->',
-			'<p>The goal of the new editor is to make adding rich content to WordPress simple and enjoyable. This whole post is composed of <em>pieces of content</em>—somewhat similar to Lego bricks—that you can move around and interact with. Move your cursor around and you\'ll notice the different blocks light up with outlines and arrows. Press the arrows to reposition blocks quickly, without fearing about losing things in the process of copying and pasting.</p>',
-			'<p>What you are reading now is a <strong>text</strong> <strong>block</strong>, the most basic block of all. A text block can have multiple paragraphs, if that\'s how you prefer to write your posts. But you can also split it by hitting enter twice. Once blocks are split they get their own controls to be moved freely around the post...</p>',
+			'<p>The goal of this new editor is to make adding rich content to WordPress simple and enjoyable. This whole post is composed of <em>pieces of content</em>—somewhat similar to LEGO bricks—that you can move around and interact with. Move your cursor around and you\'ll notice the different blocks light up with outlines and arrows. Press the arrows to reposition blocks quickly, without fearing about losing things in the process of copying and pasting.</p>',
+			'<p>What you are reading now is a <strong>text block</strong>, the most basic block of all. A text block can have multiple paragraphs, if that\'s how you prefer to write your posts. But you can also split it by hitting enter twice. Once blocks are split they get their own controls to be moved freely around the post...</p>',
 			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/text -->',
 			'<p style="text-align:right;">... like this one, which is separate from the above and right aligned.</p>',
 			'<!-- /wp:core/text -->',
 
+			'<!-- wp:core/text -->',
+			'<p>Headings are separate blocks as well, which helps with the outline and organization of your content.</p>',
+			'<!-- /wp:core/text -->',
+
 			'<!-- wp:core/heading -->',
-			'<h2>The <em>Inserter</em> Tool</h2>',
+			'<h2>A picture is worth a thousand words, or so the saying goes</h2>',
 			'<!-- /wp:core/heading -->',
 
 			'<!-- wp:core/text -->',
-			'<p>(Headings are separate blocks as well.)</p><p>Imagine everything that WordPress can do is available to you quickly and in the same place on the interface. No need to figure out HTML tags, classes, or remember shortcode syntax. That\'s the spirit behind the inserter—the (+) button you\'ll see around the editor—which allows you to browse all available content blocks and insert them into your post. Plugins and themes will be able to register their own. Go give it a try, you may discover things WordPress can insert into your posts that you didn\'t know about.</p>',
+			'<p>Handling images and media with the utmost care is a primary focus of the new editor. Hopefully you\'ll find aspects like adding captions or going full-width with your pictures much easier and robust than before.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/image -->',
+			'<figure><img src="https://cldup.com/E4PzNdrFSQ.jpg" class="aligncenter"/><figcaption><p>Give it a try. Press the &quot;really wide&quot; button on the image toolbar.</p></figcaption></figure>',
+			'<!-- /wp:core/image -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Try selecting and removing or editing the caption, now you don\'t have to be careful about selecting the image or other text by mistake and ruining the presentation.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/heading -->',
+			'<h1>The <em>Inserter</em> Tool</h1>',
+			'<!-- /wp:core/heading -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Imagine everything that WordPress can do is available to you quickly and in the same place on the interface. No need to figure out HTML tags, classes, or remember complicated shortcode syntax. That\'s the spirit behind the inserter—the (+) button you\'ll see around the editor—which allows you to browse all available content blocks and insert them into your post. Plugins and themes are able to register their own, opening up all sort of posibilities for rich editing and publishing.<p>Go give it a try, you may discover things WordPress can already insert into your posts that you didn\'t know about. Here\'s a short list of what you can currently find there:</p>',
 			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/list -->',
-			'<ul><li>Text</li><li>Headings</li><li>Images</li><li>Galleries</li><li>Embeds</li><li>Oh, and Lists of course :)</li></ul>',
+			'<ul><li>Text & Headings</li><li>Images & Videos</li><li>Galleries</li><li>Embeds, like YouTube, Tweets, or other WordPress posts.</li><li>Layout blocks, like Buttons, Hero Images, Separators, etc.</li><li>And <em>Lists</em> like this one of course :)</li></ul>',
 			'<!-- /wp:core/list -->',
+
+			'<!-- wp:core/text -->',
+			'<p>If you want to learn more about how to build additional blocks, or if you are interested in helping with the project, head over to the <a href="https://github.com/WordPress/gutenberg">GitHub repository</a>.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/button -->',
+			'<div class="aligncenter"><a href="https://github.com/WordPress/gutenberg"><span>Help build Gutenberg</span></a></div>',
+			'<!-- /wp:core/button -->',
 
 			'<!-- wp:core/separator -->',
 			'<hr/>',
 			'<!-- /wp:core/separator -->',
 
 			'<!-- wp:core/heading -->',
-			'<h2>A picture is worth a thousand words</h2>',
+			'<h1>Visual Editing</h1>',
 			'<!-- /wp:core/heading -->',
 
 			'<!-- wp:core/text -->',
-			'<p>Or so the saying goes. Working well with images and media is a primary focus of the new editor. Hopefully you\'ll find aspects like adding captions or going full-width with your images much easier and robust.</p>',
-			'<!-- /wp:core/text -->',
-
-			'<!-- wp:core/image -->',
-			'<figure><img src="https://cldup.com/Bc9YxmqFnJ.jpg" class="aligncenter"/><figcaption><p>Check out the &quot;really wide&quot; option on the image settings</p></figcaption></figure>',
-			'<!-- /wp:core/image -->',
-
-			'<!-- wp:core/text -->',
-			'<p>Another benefit of blocks is that you can edit them in place and manipulate you content directly. Try editing the following quote:</p>',
+			'<p>A huge benefit of blocks is that you can edit them in place and manipulate you content directly. Instead of having fields for editing things like the source of a quote, or the text of a button, you can directly change the content. Try editing the following quote:</p>',
 			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/quote -->',
-			'<blockquote class="blocks-quote-style-1"><p>Real artists ship.</p><footer><p><a href="http://www.folklore.org/StoryView.py?story=Real_Artists_Ship.txt">Steve Jobs, 1983</a></p></footer></blockquote>',
+			'<blockquote class="blocks-quote-style-1"><p>The editor will endeavour to create a new page and post building experience that makes writing rich posts effortless, and has “blocks” to make it easy what today might take shortcodes, custom HTML, or “mystery meat” embed discovery.</p><footer><p>Matt Mullenweg, 2017</p></footer></blockquote>',
 			'<!-- /wp:core/quote -->',
 
 			'<!-- wp:core/text -->',
-			'<p>Fields like the source of the quote are separate, and even if you remove it you can get it back.</p>',
+			'<p>The information corresponding to the source of the quote is a separate text field, similar to captions under images, so the structure of the quote is protected even if you select, modify, or remove the source. It\'s always easy to add it back.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Blocks can be anything you need. For instance, you may want to insert a subdued quote as part of the composition of your text, or you may prefer to display a giant stylized one. All of these options are available in the inserter.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/text -->',
+			'<p style="text-align:center;">Use fancy quote here...</p>',
 			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/image -->',
