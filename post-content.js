WordPress/gutenberg

commit 2aaf988f03a3220f258295e083c8e90d5c4b2193

Update dummy content to function as an introduction to the editor. (#623)
* Update dummy content to function as an introduction to the editor.

* Introduce more blocks into default content, expand text.

* Add some extra post content to the end.

* Also snuck in a "code" so we can check the link boundary.

post-content.js
@@ -5,50 +5,130 @@ window._wpGutenbergPost = {
 	content: {
 		raw: [
 			'<!-- wp:core/heading -->',
-			'<h1>1.0 Is The Loneliest Number</h1>',
+			'<h1>Welcome to the Gutenberg Editor</h1>',
 			'<!-- /wp:core/heading -->',
 
-			'<!-- wp:core/button -->',
-			'<div class="aligncenter"><a href="https://github.com/WordPress/gutenberg"><span>Gutenberg on GitHub</span></a></div>',
-			'<!-- /wp:core/button -->',
-
 			'<!-- wp:core/text -->',
-			'<p>I imagine prior to the launch of the iPod, or the iPhone, there were teams saying the same thing: the copy + paste guys are <em>so close</em> to being ready and we know Walt Mossberg is going to ding us for this so let\'s just not ship to the manufacturers in China for just a few more weeks… The Apple teams were probably embarrassed. But <strong>if you\'re not embarrassed when you ship your first version you waited too long</strong>.</p>',
+			'<p>The goal of this new editor is to make adding rich content to WordPress simple and enjoyable. This whole post is composed of <em>pieces of content</em>—somewhat similar to LEGO bricks—that you can move around and interact with. Move your cursor around and you\'ll notice the different blocks light up with outlines and arrows. Press the arrows to reposition blocks quickly, without fearing about losing things in the process of copying and pasting.</p>',
+			'<p>What you are reading now is a <strong>text block</strong>, the most basic block of all. A text block can have multiple paragraphs, if that\'s how you prefer to write your posts. But you can also split it by hitting enter twice. Once blocks are split they get their own controls to be moved freely around the post...</p>',
 			'<!-- /wp:core/text -->',
 
-			'<!-- wp:core/separator -->',
-			'<hr />',
-			'<!-- /wp:core/separator -->',
+			'<!-- wp:core/text -->',
+			'<p style="text-align:right;">... like this one, which is separate from the above and right aligned.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Headings are separate blocks as well, which helps with the outline and organization of your content.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/heading -->',
+			'<h2>A picture is worth a thousand words, or so the saying goes</h2>',
+			'<!-- /wp:core/heading -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Handling images and media with the utmost care is a primary focus of the new editor. Hopefully you\'ll find aspects like adding captions or going full-width with your pictures much easier and robust than before.</p>',
+			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/image -->',
-			'<figure><img src="https://cldup.com/Bc9YxmqFnJ.jpg" /></figure>',
+			'<figure><img src="https://cldup.com/E4PzNdrFSQ.jpg" class="aligncenter"/><figcaption><p>Give it a try. Press the &quot;really wide&quot; button on the image toolbar.</p></figcaption></figure>',
 			'<!-- /wp:core/image -->',
 
 			'<!-- wp:core/text -->',
-			'<p>A beautiful thing about Apple is <em>how quickly</em> they obsolete their own products. I imagine this also makes the discipline of getting things out there easier. Like I mentioned before, the longer it’s been since the last release the more pressure there is, but if you know that if your bit of code doesn’t make this version but there’s the +0.1 coming out in 6 weeks, then it’s not that bad.</p>',
-			'<p>It’s like flights from San Francisco to LA, if you miss one you know there’s another one an hour later so it’s not a big deal. Amazon has done a fantastic job of this with the Kindle as well, with a new model every year.</p>',
+			'<p>Try selecting and removing or editing the caption, now you don\'t have to be careful about selecting the image or other text by mistake and ruining the presentation.</p>',
 			'<!-- /wp:core/text -->',
 
-			'<!-- wp:core/quote -->',
-			'<blockquote><p>Real artists ship.</p><footer><p><a href="http://www.folklore.org/StoryView.py?story=Real_Artists_Ship.txt">Steve Jobs, 1983</a></p></footer></blockquote>',
-			'<!-- /wp:core/quote -->',
+			'<!-- wp:core/embed url:https://www.youtube.com/watch?v=Nl6U7UotA-M -->',
+			'<figure><iframe width="560" height="315" src="//www.youtube.com/embed/Nl6U7UotA-M" frameborder="0" allowfullscreen></iframe><figcaption><p>State of the Word 2016</p></figcaption></figure>',
+			'<!-- /wp:core/embed -->',
 
-			'<!-- wp:core/image -->',
-			'<figure><img src="https://cldup.com/vuGcj2VB8M.jpg" /><figcaption>Beautiful landscape</figcaption></figure>',
-			'<!-- /wp:core/image -->',
+			'<!-- wp:core/heading -->',
+			'<h2>The <em>Inserter</em> Tool</h2>',
+			'<!-- /wp:core/heading -->',
 
 			'<!-- wp:core/text -->',
-			'<p>By shipping early and often you have the unique competitive advantage of hearing from real people what they think of your work, which in best case helps you anticipate market direction, and in worst case gives you a few people rooting for you that you can email when your team pivots to a new idea. Nothing can recreate the crucible of real usage.</p>',
+			'<p>Imagine everything that WordPress can do is available to you quickly and in the same place on the interface. No need to figure out HTML tags, classes, or remember complicated shortcode syntax. That\'s the spirit behind the inserter—the <code>(+)</code> button you\'ll see around the editor—which allows you to browse all available content blocks and insert them into your post. Plugins and themes are able to register their own, opening up all sort of posibilities for rich editing and publishing.<p>Go give it a try, you may discover things WordPress can already insert into your posts that you didn\'t know about. Here\'s a short list of what you can currently find there:</p>',
 			'<!-- /wp:core/text -->',
 
 			'<!-- wp:core/list -->',
-			'<ul><li>Ship early</li><li>Ship often</li><li>Listen to feedback from real people</li><li>Anticipate market direction</li></ul>',
+			'<ul><li>Text & Headings</li><li>Images & Videos</li><li>Galleries</li><li>Embeds, like YouTube, Tweets, or other WordPress posts.</li><li>Layout blocks, like Buttons, Hero Images, Separators, etc.</li><li>And <em>Lists</em> like this one of course :)</li></ul>',
 			'<!-- /wp:core/list -->',
 
-			'<!-- wp:core/embed url="https://www.youtube.com/watch?v=Nl6U7UotA-M" -->',
-			'<iframe width="560" height="315" src="//www.youtube.com/embed/Nl6U7UotA-M" frameborder="0" allowfullscreen></iframe>',
-			'<!-- /wp:core/embed -->',
+			'<!-- wp:core/text -->',
+			'<p>If you want to learn more about how to build additional blocks, or if you are interested in helping with the project, head over to the <a href="https://github.com/WordPress/gutenberg">GitHub repository</a>.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/button -->',
+			'<div class="aligncenter"><a href="https://github.com/WordPress/gutenberg"><span>Help build Gutenberg</span></a></div>',
+			'<!-- /wp:core/button -->',
+
+			'<!-- wp:core/separator -->',
+			'<hr/>',
+			'<!-- /wp:core/separator -->',
+
+			'<!-- wp:core/heading -->',
+			'<h2>Visual Editing</h2>',
+			'<!-- /wp:core/heading -->',
+
+			'<!-- wp:core/text -->',
+			'<p>A huge benefit of blocks is that you can edit them in place and manipulate you content directly. Instead of having fields for editing things like the source of a quote, or the text of a button, you can directly change the content. Try editing the following quote:</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/quote -->',
+			'<blockquote class="blocks-quote-style-1"><p>The editor will endeavour to create a new page and post building experience that makes writing rich posts effortless, and has “blocks” to make it easy what today might take shortcodes, custom HTML, or “mystery meat” embed discovery.</p><footer><p>Matt Mullenweg, 2017</p></footer></blockquote>',
+			'<!-- /wp:core/quote -->',
+
+			'<!-- wp:core/text -->',
+			'<p>The information corresponding to the source of the quote is a separate text field, similar to captions under images, so the structure of the quote is protected even if you select, modify, or remove the source. It\'s always easy to add it back.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Blocks can be anything you need. For instance, you may want to insert a subdued quote as part of the composition of your text, or you may prefer to display a giant stylized one. All of these options are available in the inserter.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/quote -->',
+			'<blockquote class="blocks-quote-style-2"><p>There is no greater agony than bearing an untold story inside you.</p><footer><p>Maya Angelou</p></footer></blockquote>',
+			'<!-- /wp:core/quote -->',
+
+			'<!-- wp:core/separator -->',
+			'<hr/>',
+			'<!-- /wp:core/separator -->',
+
+			'<!-- wp:core/heading -->',
+			'<h2>Sea id autem nominavi deseruisse</h2>',
+			'<!-- /wp:core/heading -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Ea veniam homero eam. Ex inimicus molestiae cum, debet scaevola at eos. Vis assum veritus ut, has ea nostrud accusata, offendit appareat comprehensam ea pro. Ad quo quem veritus appellantur, te est quas phaedrum, eum alia habeo ad. Ei est erroribus imperdiet, omnis dicam propriae sed no. His vitae oratio fierent ne, cu duo tota eligendi, electram rationibus in qui.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/image -->',
+			'<figure><img src="https://cldup.com/uuUqE_dXzy.jpg" /></figure>',
+			'<!-- /wp:core/image -->',
+
+			'<!-- wp:core/text -->',
+			'<p>Est quis reque cetero ad. Sea id autem nominavi deseruisse. <strong>Veniam qualisque definitionem pri id</strong>, ea autem feugiat delenit ius, mei at loem affert accumsan. Dicat eruditi cu est, te pro dicant pericula conclusionemque, ei vim detracto euripidis intellegam. Eius postea volumus mei ad.</p>',
+			'<p>Prima ridens denique his te, ferri illum volumus an his. Eu vel dicat homero qualisqu, vitae regione deserunt vis ei. Graeci incorrupte liberavisse no mea, saepe voluptaria usu ex, vis dicant euismod id. At dolor reprimique eos, quo altera detraxit moderatius id. Quo iudico utinam eu, ad alia munere mel.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/image -->',
+			'<figure><img src="https://cldup.com/GCwahb3aOb.jpg" /></figure>',
+			'<!-- /wp:core/image -->',
+
+			'<!-- wp:core/image -->',
+			'<figure><img src="https://cldup.com/lUUQPv6w9c.jpg" /></figure>',
+			'<!-- /wp:core/image -->',
+
+			'<!-- wp:core/text -->',
+			'<p><Eu integre accusata prodesset est, <em>sed te impetus gubergren conceptam</em>, ex sed wisi nostrum ocurreret. Esse velit omittantur ius te, alii dissentias ei vis. At sed unum veritus fabellas. Te volutpat appellantur duo. Ad natum fuisset intellegebat eam, causae invidunt usu id, et vis impetus appetere.</p>',
+			'<!-- /wp:core/text -->',
+
+			'<!-- wp:core/heading -->',
+			'<h6>Loem</h6>',
+			'<!-- /wp:core/heading -->',
+
+			'<!-- wp:core/list -->',
+			'<ul><li>Est quis reque cetero ad</li><li>Sea id autem nominavi deseruisse</li><li>Veniam qualisque definitionem pri id, ea autem feugiat delenit ius, mei at loem affert accumsan</li><li>Dicat eruditi cu est, te pro dicant pericula conclusionemque</li><li>Eius postea volumus mei ad</li></ul>',
+			'<!-- /wp:core/list -->',
 		].join( '' ),
 	},
 };
-

commit 5a375d93c5023477459d38f135628b5d0663abfb

Fix embed.
This fixes a regression I introduced in https://github.com/WordPress/gutenberg/pull/623#issuecomment-299452242.

post-content.js
@@ -37,7 +37,7 @@ window._wpGutenbergPost = {
 			'<p>Try selecting and removing or editing the caption, now you don\'t have to be careful about selecting the image or other text by mistake and ruining the presentation.</p>',
 			'<!-- /wp:core/text -->',
 
-			'<!-- wp:core/embed url:https://www.youtube.com/watch?v=Nl6U7UotA-M -->',
+			'<!-- wp:core/embed url="https://www.youtube.com/watch?v=Nl6U7UotA-M" -->',
 			'<figure><iframe width="560" height="315" src="//www.youtube.com/embed/Nl6U7UotA-M" frameborder="0" allowfullscreen></iframe><figcaption><p>State of the Word 2016</p></figcaption></figure>',
 			'<!-- /wp:core/embed -->',
 