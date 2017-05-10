adc6085c7b16da4f54130a6bbf7cc3e8f55304b2
Marcus Kazmierczak <marcus@mkaz.com>
2017-05-10 10:39:16 -0700

    Make the linter happy

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -1,8 +1,6 @@
 
 export default class GalleryImage extends wp.element.Component {
-
 	render() {
-
 		return (
 			<div key={ this.props.i } className="blocks-gallery-image">
 				<img src={ this.props.img.src } alt={ this.props.img.alt } />
