abfaf37e80f6f3c73bafa9a4a79c7cdc7aac295a
Riad Benguella <benguella@gmail.com>
2017-05-15 12:46:05 +0100

    Components: Adding the FormToggle component

--- editor/sidebar/post-status/index.js
+++ editor/sidebar/post-status/index.js
@@ -5,6 +5,7 @@ import { __ } from 'i18n';
 import { Component } from 'element';
 import PanelBody from 'components/panel-body';
 import PanelBodyToggle from 'components/panel-body-toggle';
+import FormToggle from 'components/form-toggle';
 
 /**
  * Internal Dependencies
@@ -38,7 +39,12 @@ class PostStatus extends Component {
 				{ this.state.opened &&
 					<div>
 						<div className="editor-sidebar-post-status__row">
-							{ __( 'Pending Review' ) }
+							<span>{ __( 'Pending review' ) }</span>
+							<FormToggle
+								checked={ false }
+								onChange={ () => {} }
+								aria-label={ __( 'Request review for post' ) }
+							/>
 						</div>
 					</div>
 				}
