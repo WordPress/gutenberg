2b89f3ef07010f7ec25f724d490b80ff55f3281a
James Nylen <jnylen@gmail.com>
2017-05-15 15:19:51 -0400

    Build script updates:
    - Split into separate script
    - Do not use `npm pack`

---
+++ bin/build-plugin-zip.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Exit if any command fails
+set -e # exit on error
+
+# Change to the expected directory
+cd "$(dirname "$0")"
+cd ..
+
+# Remove any existing build
+rm -f gutenberg.zip
+
+# Run the build
+npm install
+npm run build
+
+# Generate the plugin zip file
+zip -r gutenberg.zip \
+    index.php \
+    post-content.js \
+    editor/build \
+    i18n/build \
+    element/build \
+    blocks/build \
+    README.md
