39b6b31f72c3ddd3cfc9b6f2e182ff59eb07e0e8
Riad Benguella <benguella@gmail.com>
2017-05-15 14:30:00 +0100

    Components: Adding an option header prop to the Panel component

--- components/panel/index.js
+++ components/panel/index.js
@@ -4,14 +4,20 @@
 import classnames from 'classnames';
 
 /**
+ * WordPress dependencies
+ */
+import PanelHeader from 'components/panel-header';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
 
-function Panel( { className, children } ) {
+function Panel( { header, className, children } ) {
 	const classNames = classnames( className, 'components-panel' );
 	return (
 		<div className={ classNames }>
+			{ header && <PanelHeader><strong>{ header }</strong></PanelHeader>}
 			{ children }
 		</div>
 	);
