b042858dbe3979d1248f65da7a93139da2535b45
Riad Benguella <benguella@gmail.com>
2017-05-15 15:26:52 +0100

    PanelBody: Adding the possibility to toggle the content of PanelBody

--- components/panel/body.js
+++ components/panel/body.js
@@ -1,9 +1,43 @@
-function PanelBody( { children } ) {
-	return (
-		<div className="components-panel__body">
-			{ children }
-		</div>
-	);
+/**
+ * WordPress dependencies
+ */
+import { Component } from 'element';
+
+/**
+ * Internal dependencies
+ */
+import PanelBodyToggle from './body-toggle';
+
+class PanelBody extends Component {
+	constructor( props ) {
+		super( ...arguments );
+		this.state = {
+			opened: props.initialOpen === undefined ? true : props.initialOpen,
+		};
+		this.toggle = this.toggle.bind( this );
+	}
+
+	toggle( event ) {
+		event.preventDefault();
+		this.setState( {
+			opened: ! this.state.opened,
+		} );
+	}
+	render() {
+		const { toggle, children } = this.props;
+		return (
+			<div className="components-panel__body">
+				{ !! toggle &&
+					<PanelBodyToggle
+						onClick={ this.toggle }
+						opened={ this.state.opened }
+						label={ toggle }
+					/>
+				}
+				{ this.state.opened && children }
+			</div>
+		);
+	}
 }
 
 export default PanelBody;
--- editor/sidebar/post-status/index.js
+++ editor/sidebar/post-status/index.js
@@ -2,9 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from 'i18n';
-import { Component } from 'element';
 import PanelBody from 'components/panel/body';
-import PanelBodyToggle from 'components/panel/body-toggle';
 import FormToggle from 'components/form-toggle';
 
 /**
@@ -12,44 +10,20 @@ import FormToggle from 'components/form-toggle';
  */
 import './style.scss';
 
-class PostStatus extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			opened: true,
-		};
-		this.toggle = this.toggle.bind( this );
-	}
-
-	toggle( event ) {
-		event.preventDefault();
-		this.setState( {
-			opened: ! this.state.opened,
-		} );
-	}
-
-	render() {
-		return (
-			<PanelBody>
-				<PanelBodyToggle
-					onClick={ this.toggle }
-					opened={ this.state.opened }
-					label={ __( 'Status & Visibility' ) }
-				/>
-				{ this.state.opened &&
-					<div>
-						<div className="editor-sidebar-post-status__row">
-							<span>{ __( 'Pending review' ) }</span>
-							<FormToggle
-								checked={ false }
-								aria-label={ __( 'Request review for post' ) }
-							/>
-						</div>
-					</div>
-				}
-			</PanelBody>
-		);
-	}
+function PostStatus() {
+	return (
+		<PanelBody toggle={ __( 'Status & Visibility' ) }>
+			<div>
+				<div className="editor-sidebar-post-status__row">
+					<span>{ __( 'Pending review' ) }</span>
+					<FormToggle
+						checked={ false }
+						aria-label={ __( 'Request review for post' ) }
+					/>
+				</div>
+			</div>
+		</PanelBody>
+	);
 }
 
 export default PostStatus;
