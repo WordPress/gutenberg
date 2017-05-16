f60e11db060a7a52e7d59502d1e29b0dece83935
James Johnson <james.johnson@ephox.com>
2017-05-16 11:59:28 +1000

    Moved list logic into Editable

--- blocks/library/list/index.js
+++ blocks/library/list/index.js
@@ -12,40 +12,6 @@ import Editable from '../../editable';
 
 const { children, prop } = hpq;
 
-function execCommand( command ) {
-	return ( { editor } ) => {
-		if ( editor ) {
-			editor.execCommand( command );
-		}
-	};
-}
-
-function listIsActive( listType ) {
-	return ( { nodeName = 'OL', internalListType } ) => {
-		return listType === ( internalListType ? internalListType : nodeName );
-	};
-}
-
-function listSetType( listType, editorCommand ) {
-	return ( { internalListType, editor }, setAttributes ) => {
-		if ( internalListType ) {
-			// only change list types, don't toggle off internal lists
-			if ( internalListType !== listType )	{
-				if ( editor ) {
-					editor.execCommand( editorCommand );
-				}
-			}
-		} else {
-			setAttributes( { nodeName: listType } );
-		}
-	};
-}
-
-function findInternalListType( { parents } ) {
-	const list = parents.find( ( node ) => node.nodeName === 'UL' || node.nodeName === 'OL' );
-	return list ? list.nodeName : null;
-}
-
 registerBlock( 'core/list', {
 	title: wp.i18n.__( 'List' ),
 	icon: 'editor-ul',
@@ -55,31 +21,6 @@ registerBlock( 'core/list', {
 		nodeName: prop( 'ol,ul', 'nodeName' ),
 		values: children( 'ol,ul' ),
 	},
-
-	controls: [
-		{
-			icon: 'editor-ul',
-			title: wp.i18n.__( 'Convert to unordered' ),
-			isActive: listIsActive( 'UL' ),
-			onClick: listSetType( 'UL', 'InsertUnorderedList' ),
-		},
-		{
-			icon: 'editor-ol',
-			title: wp.i18n.__( 'Convert to ordered' ),
-			isActive: listIsActive( 'OL' ),
-			onClick: listSetType( 'OL', 'InsertOrderedList' ),
-		},
-		{
-			icon: 'editor-outdent',
-			title: wp.i18n.__( 'Outdent list item' ),
-			onClick: execCommand( 'Outdent' ),
-		},
-		{
-			icon: 'editor-indent',
-			title: wp.i18n.__( 'Indent list item' ),
-			onClick: execCommand( 'Indent' ),
-		},
-	],
 
 	transforms: {
 		from: [
@@ -112,15 +53,8 @@ registerBlock( 'core/list', {
 		return (
 			<Editable
 				tagName={ nodeName.toLowerCase() }
-				onSetup={ ( nextEditor ) => {
-					setAttributes( { editor: nextEditor } );
-				}	}
-				onChange={ ( nextValues ) => {
-					setAttributes( { values: nextValues } );
-				} }
-				onNodeChange={ ( nodeInfo ) => {
-					setAttributes( { internalListType: findInternalListType( nodeInfo ) } );
-				} }
+				onChange={ ( nextValues ) => setAttributes( { values: nextValues } ) }
+				onListChange={ ( listType ) => setAttributes( { nodeName: listType } ) }
 				value={ values }
 				focus={ focus }
 				onFocus={ setFocus }
