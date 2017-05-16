512af5a3c7c703597b457e2900d491e858ef8033
Riad Benguella <benguella@gmail.com>
2017-05-16 09:40:34 +0100

    Sidebar: Fix panels after rebase

--- editor/sidebar/index.js
+++ editor/sidebar/index.js
@@ -6,10 +6,9 @@ import { Slot } from 'react-slot-fill';
 /**
  * WordPress dependencies
  */
-import { __ } from 'i18n';
 import Panel from 'components/panel';
-import PanelHeader from 'components/panel-header';
-import PanelBody from 'components/panel-body';
+import PanelHeader from 'components/panel/header';
+import PanelBody from 'components/panel/body';
 
 /**
  * Internal Dependencies
