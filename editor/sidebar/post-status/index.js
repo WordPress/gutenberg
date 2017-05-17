a84b6b9cc6a611bc061db2456f1aa10e18d99c34
Riad Benguella <benguella@gmail.com>
2017-05-17 09:58:58 +0100

    Chrome: Adding a functional pending review toggle

--- editor/sidebar/post-status/index.js
+++ editor/sidebar/post-status/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+
+/**
  * WordPress dependencies
  */
 import { __ } from 'i18n';
@@ -9,15 +14,38 @@ import FormToggle from 'components/form-toggle';
  * Internal Dependencies
  */
 import './style.scss';
-function PostStatus() {
+import { getEditedPostStatus } from '../../selectors';
+import { editPost } from '../../actions';
+
+function PostStatus( { status, onUpdateStatus } ) {
+	const onToggle = () => {
+		const updatedStatus = status === 'pending' ? 'draft' : 'pending';
+		onUpdateStatus( updatedStatus );
+	};
+
 	return (
 		<PanelBody title={ __( 'Status & Visibility' ) }>
 			<div className="editor-post-status__row">
 				<span>{ __( 'Pending review' ) }</span>
-				<FormToggle />
+				<FormToggle
+					checked={ status === 'pending' }
+					onChange={ onToggle }
+				/>
 			</div>
 		</PanelBody>
 	);
 }
 
-export default PostStatus;
+export default connect(
+	( state ) => ( {
+		status: getEditedPostStatus( state ),
+	} ),
+	( dispatch ) => {
+		return {
+			onUpdateStatus( status ) {
+				dispatch( editPost( { status } ) );
+			},
+		};
+	}
+)( PostStatus );
+
