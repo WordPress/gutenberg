0282d9dd981c089f670f2641d587a6bfaecf2b12
iseulde <ella@iseulde.com>
2017-05-17 15:27:04 +0200

    Add alignment for table block

--- blocks/library/table/index.js
+++ blocks/library/table/index.js
@@ -7,6 +7,20 @@ import Editable from '../../editable';
 
 const { children, query } = hpq;
 
+/**
+ * Returns an attribute setter with behavior that if the target value is
+ * already the assigned attribute value, it will be set to undefined.
+ *
+ * @param  {string}   align Alignment value
+ * @return {Function}       Attribute setter
+ */
+function toggleAlignment( align ) {
+	return ( attributes, setAttributes ) => {
+		const nextAlign = attributes.align === align ? undefined : align;
+		setAttributes( { align: nextAlign } );
+	};
+}
+
 registerBlock( 'core/table', {
 	title: wp.i18n.__( 'Table' ),
 	icon: 'editor-table',
@@ -17,15 +31,45 @@ registerBlock( 'core/table', {
 	},
 
 	controls: [
-
+		{
+			icon: 'align-left',
+			title: wp.i18n.__( 'Align left' ),
+			isActive: ( { align } ) => 'left' === align,
+			onClick: toggleAlignment( 'left' ),
+		},
+		{
+			icon: 'align-center',
+			title: wp.i18n.__( 'Align center' ),
+			isActive: ( { align } ) => 'center' === align,
+			onClick: toggleAlignment( 'center' ),
+		},
+		{
+			icon: 'align-right',
+			title: wp.i18n.__( 'Align right' ),
+			isActive: ( { align } ) => 'right' === align,
+			onClick: toggleAlignment( 'right' ),
+		},
+		{
+			icon: 'align-full-width',
+			title: wp.i18n.__( 'Wide width' ),
+			isActive: ( { align } ) => 'wide' === align,
+			onClick: toggleAlignment( 'wide' ),
+		},
 	],
+
+	getEditWrapperProps( attributes ) {
+		const { align } = attributes;
+		if ( 'left' === align || 'right' === align || 'wide' === align ) {
+			return { 'data-align': align };
+		}
+	},
 
 	edit( { attributes, setAttributes, focus, setFocus } ) {
 		const { rows = [] } = attributes;
 		const focussedKey = focus ? focus.editable || '0.0' : null;
 
 		return (
-			<table className="blocks-table">
+			<table>
 				<tbody>
 					{ rows.map( ( cells = [], i ) =>
 						<tr key={ i }>
