ba78f3967225a59e8e7bb94ab91d08103269b038
Riad Benguella <benguella@gmail.com>
2017-05-18 13:34:32 +0100

    Chrome: Adding visibility information

--- editor/sidebar/post-visibility/index.js
+++ editor/sidebar/post-visibility/index.js
@@ -3,12 +3,14 @@
  */
 import { connect } from 'react-redux';
 import clickOutside from 'react-click-outside';
+import { find } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { __ } from 'i18n';
 import { Component } from 'element';
+import Dashicon from 'components/dashicon';
 
 /**
  * Internal Dependencies
@@ -26,13 +28,20 @@ class PostVisibility extends Component {
 		super( ...arguments );
 		this.state = {
 			opened: false,
+			showInfo: false,
 		};
 		this.toggleDialog = this.toggleDialog.bind( this );
+		this.toggleVisibilityInfo = this.toggleVisibilityInfo.bind( this );
 	}
 
 	toggleDialog( event ) {
 		event.preventDefault();
 		this.setState( { opened: ! this.state.opened } );
+	}
+
+	toggleVisibilityInfo( event ) {
+		event.preventDefault();
+		this.setState( { showInfo: ! this.state.showInfo } );
 	}
 
 	handleClickOutside() {
@@ -41,44 +50,63 @@ class PostVisibility extends Component {
 
 	render() {
 		const { status, visibility, password, onUpdateVisibility } = this.props;
-		const visibilityLabels = {
-			'public': __( 'Public' ),
-			'private': __( 'Private' ),
-			password: __( 'Password Protected' ),
-		};
 
 		const setPublic = () => onUpdateVisibility( visibility === 'private' ? 'draft' : status );
 		const setPrivate = () => onUpdateVisibility( 'private' );
 		const setPasswordProtected = () => onUpdateVisibility( visibility === 'private' ? 'draft' : status, password || '' );
 		const updatePassword = ( event ) => onUpdateVisibility( status, event.target.value );
 
+		const visibilityOptions = [
+			{
+				value: 'public',
+				label: __( 'Public' ),
+				info: __( 'Visible to everyone.' ),
+				changeHandler: setPublic,
+			},
+			{
+				value: 'private',
+				label: __( 'Private' ),
+				info: __( 'Only visible to site admins and editors.' ),
+				changeHandler: setPrivate,
+			},
+			{
+				value: 'password',
+				label: __( 'Password Protected' ),
+				info: __( 'Protected with a password you choose. Only those with the password can view this post.' ),
+				changeHandler: setPasswordProtected,
+			},
+		];
+		const getVisibilityLabel = () => find( visibilityOptions, { value: visibility } ).label;
+
+
 		// Disable Reason: The input is inside the label, we shouldn't need the htmlFor
 		/* eslint-disable jsx-a11y/label-has-for */
 		return (
 			<div className="editor-post-visibility">
 				<span>{ __( 'Visibility' ) }</span>
-				<a href="" onClick={ this.toggleDialog }>{ visibilityLabels[ visibility ] }</a>
+				<a className="editor-post-visibility__toggle" href="" onClick={ this.toggleDialog }>
+					{ getVisibilityLabel( visibility ) }
+				</a>
 
 				{ this.state.opened &&
 					<div className="editor-post-visibility__dialog">
 						<div className="editor-post-visibility__dialog-arrow" />
 						<div className="editor-post-visibility__dialog-legend">
 							{ __( 'Post Visibility' ) }
+							<a className="editor-post-visibility__dialog-info-toggle" href="" onClick={ this.toggleVisibilityInfo }>
+								<Dashicon icon="info" />
+							</a>
 						</div>
-						<label className="editor-post-visibility__dialog-label">
-							<input type="radio" value="public" onChange={ setPublic } checked={ 'public' === visibility } />
-							<span>{ visibilityLabels.public }</span>
-						</label>
-						<label className="editor-post-visibility__dialog-label">
-							<input type="radio" value="private" onChange={ setPrivate } checked={ 'private' === visibility } />
-							<span>{ visibilityLabels.private }</span>
-						</label>
-						<label className="editor-post-visibility__dialog-label">
-							<input type="radio" value="password" onChange={ setPasswordProtected } checked={ 'password' === visibility } />
-							<span>{ visibilityLabels.password }</span>
-						</label>
+						{ visibilityOptions.map( ( { value, label, info, changeHandler } ) => (
+							<label key={ value } className="editor-post-visibility__dialog-label">
+								<input type="radio" value={ value } onChange={ changeHandler } checked={ value === visibility } />
+								<span>{ label }</span>
+								{ this.state.showInfo && <div className="editor-post-visibility__dialog-info">{ info }</div> }
+							</label>
+						) ) }
 						{ visibility === 'password' &&
 							<input
+								className="editor-post-visibility__dialog-password-input"
 								type="text"
 								onChange={ updatePassword }
 								value={ password }
