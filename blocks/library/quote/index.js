1b1a188cbc484b218b04e6b286ad41247c97f57a
Matías Ventura <mv@matiasventura.com>
2017-05-19 15:37:13 +0200

    Quote: add citation placeholder.

--- blocks/library/quote/index.js
+++ blocks/library/quote/index.js
@@ -132,6 +132,7 @@ registerBlock( 'core/quote', {
 					<Editable
 						tagName="footer"
 						value={ citation }
+						placeholder={ wp.i18n.__( 'Add citation…' ) }
 						onChange={
 							( nextCitation ) => setAttributes( {
 								citation: nextCitation,
