e54c0e731e146d515cc5d6276c7ebe396640d450
George Hotelling <george.hotelling@automattic.com>
2017-05-23 14:55:51 -0400

    background image not showing up

--- blocks/library/cover-image/index.js
+++ blocks/library/cover-image/index.js
@@ -1,18 +1,22 @@
 /**
+ * WordPress dependencies
+ */
+import Button from 'components/button';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
-import { registerBlock, query } from 'api';
-import Editable from 'components/editable';
+import { registerBlock, query } from '../../api';
+import Editable from '../../editable';
 // TODO: Revisit when we have a common components solution
-import Dashicon from '../../../editor/components/dashicon';
-import Button from '../../../editor/components/button';
+import Dashicon from '../../../components/dashicon';
 
-const { attr, children } = query;
+const { attr, text } = query;
 
 // hack based on https://www.w3.org/TR/CSS1/#url
 // does not support escaped values
-const URL_REGEX = /url\(\s*['"]?([^'")]*)['"]?\s*\)/i;
+// const URL_REGEX = /url\(\s*['"]?([^'")]*)['"]?\s*\)/i;
 
 /**
  * Returns an attribute setter with behavior that if the target value is
@@ -28,20 +32,20 @@ function toggleAlignment( align ) {
 	};
 }
 
-/**
- * Returns the URL from a CSS url() value
- * e.g. url( 'https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg');
- *
- * @param {Function} func function that provides the CSS property
- * @return {Function}     funtion that will return a string with the url
- */
-function extractUrl( func ) {
-	return ( node ) => {
-		const propValue = func( node );
-		const matches = URL_REGEX.match( propValue );
-		return matches[ 1 ];
-	};
-}
+// /**
+//  * Returns the URL from a CSS url() value
+//  * e.g. url( 'https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg');
+//  *
+//  * @param {Function} func function that provides the CSS property
+//  * @return {Function}     funtion that will return a string with the url
+//  */
+// function extractUrl( func ) {
+// 	return ( node ) => {
+// 		const propValue = func( node );
+// 		const matches = URL_REGEX.match( propValue );
+// 		return matches[ 1 ];
+// 	};
+// }
 
 registerBlock( 'core/cover-image', {
 	title: wp.i18n.__( 'Cover Image' ),
@@ -52,7 +56,7 @@ registerBlock( 'core/cover-image', {
 
 	attributes: {
 		url: attr( 'section', 'data-url' ),
-		title: children( 'h2' ),
+		title: text( 'h2' ),
 	},
 
 	controls: [
@@ -60,32 +64,32 @@ registerBlock( 'core/cover-image', {
 			icon: 'format-image',
 			title: wp.i18n.__( 'Change Image' ),
 			isActive: ( { align } ) => 'left' === align,
-			onClick: toggleAlignment( 'left' )
+			onClick: toggleAlignment( 'left' ),
 		},
 		{
 			icon: 'align-left',
 			title: wp.i18n.__( 'Align left' ),
 			isActive: ( { align } ) => 'left' === align,
-			onClick: toggleAlignment( 'left' )
+			onClick: toggleAlignment( 'left' ),
 		},
 		{
 			icon: 'align-center',
 			title: wp.i18n.__( 'Align center' ),
 			isActive: ( { align } ) => 'center' === align,
-			onClick: toggleAlignment( 'center' )
+			onClick: toggleAlignment( 'center' ),
 		},
 		{
 			icon: 'align-right',
 			title: wp.i18n.__( 'Align right' ),
 			isActive: ( { align } ) => 'right' === align,
-			onClick: toggleAlignment( 'right' )
+			onClick: toggleAlignment( 'right' ),
 		},
 		{
 			icon: 'align-full-width',
 			title: wp.i18n.__( 'Wide width' ),
 			isActive: ( { align } ) => 'wide' === align,
-			onClick: toggleAlignment( 'wide' )
-		}
+			onClick: toggleAlignment( 'wide' ),
+		},
 	],
 
 	getEditWrapperProps( attributes ) {
@@ -115,9 +119,14 @@ registerBlock( 'core/cover-image', {
 			);
 		}
 
+		const style = {
+			backgroundImage: url,
+			fontWeight: 'bold',
+		};
+
 		return (
-			<section className="blocks-image">
-				<section className="cover-image" data-url={ url } style={ { backgroundImage: `url('${ url }');` } }>
+			<section className="blocks-cover-image">
+				<section className="cover-image" data-url={ url } data-foo="bar" style={ style }>
 					{ title || !! focus ? (
 						<Editable
 							tagName="h2"
@@ -134,11 +143,14 @@ registerBlock( 'core/cover-image', {
 
 	save( { attributes } ) {
 		const { url, title } = attributes;
+		const style = {
+			backgroundImage: `url(${ url })`,
+		};
 
 		return (
-			<section className="cover-image" data-url={ url } style={ { backgroundImage: `url('${ url }');` } }>
+			<section className="cover-image" data-url={ url } style={ style }>
 				<h2>{ title }</h2>
 			</section>
 		);
-	}
+	},
 } );
