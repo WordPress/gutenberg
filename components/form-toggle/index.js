1a2c89b24a94d188d3b238a73e188fa2a431818e
Riad Benguella <benguella@gmail.com>
2017-05-26 15:51:01 +0100

    Accessibility: Drop the default FormToggle id and simplify the component
    Also fix the input z-index

--- components/form-toggle/index.js
+++ components/form-toggle/index.js
@@ -8,53 +8,35 @@ import { noop } from 'lodash';
  * WordPress dependencies
  */
 import { __ } from 'i18n';
-import { Component } from 'element';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-class FormToggle extends Component {
-	constructor() {
-		super( ...arguments );
-		this.id = this.constructor.instances++;
-	}
+function FormToggle( { className, checked, id, onChange = noop, showHint = true } ) {
+	const wrapperClasses = classNames(
+		'components-form-toggle',
+		className,
+		{ 'is-checked': checked }
+	);
 
-	render() {
-		const {
-			className,
-			checked,
-			onChange = noop,
-			showHint = true,
-			id = 'toggle-' + this.id,
-		} = this.props;
-
-		const wrapperClasses = classNames(
-			'components-form-toggle',
-			className,
-			{ 'is-checked': checked }
-		);
-
-		return (
-			<span className={ wrapperClasses }>
-				<input
-					className="components-form-toggle__input"
-					id={ id }
-					type="checkbox"
-					value={ checked }
-					onChange={ onChange }
-				/>
-				{ showHint &&
-					<span className="components-form-toggle__hint" aria-hidden>
-						{ checked ? __( 'On' ) : __( 'Off' ) }
-					</span>
-				}
-			</span>
-		);
-	}
+	return (
+		<span className={ wrapperClasses }>
+			<input
+				className="components-form-toggle__input"
+				id={ id }
+				type="checkbox"
+				value={ checked }
+				onChange={ onChange }
+			/>
+			{ showHint &&
+				<span className="components-form-toggle__hint" aria-hidden>
+					{ checked ? __( 'On' ) : __( 'Off' ) }
+				</span>
+			}
+		</span>
+	);
 }
-
-FormToggle.instances = 1;
 
 export default FormToggle;
