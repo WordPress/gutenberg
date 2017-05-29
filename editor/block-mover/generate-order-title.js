7cdab460d03a7cfca3c6eb2156114bf0a3c05438
Neil Anderson <njp.anderson@icloud.com>
2017-05-29 19:32:06 +0100

    Add valid title for lists with only one block

--- editor/block-mover/generate-order-title.js
+++ editor/block-mover/generate-order-title.js
@@ -1,17 +1,20 @@
 export default function( { typeTitle, position, isFirst, isLast, dir } ) {
 	const prefix = 'Move "' + typeTitle + '" block from position ' + position;
+	const errorPrefix = 'Block "' + typeTitle + '"';
 	let title = '';
 
-	if ( dir > 0 ) {
+	if ( isFirst && isLast ) {
+		title = errorPrefix + ' is the only block, and cannot be moved';
+	} else if ( dir > 0 ) {
 		// moving down
 		title = ( ! isLast ) ?
 			prefix + ' down to position ' + ( position + 1 ) :
-			'Block "' + typeTitle + '" is at the end of the content and can’t be moved down';
+			errorPrefix + ' is at the end of the content and can’t be moved down';
 	} else {
 		// moving up
 		title = ( ! isFirst ) ?
 			prefix + ' up to position ' + ( position - 1 ) :
-			'Block "' + typeTitle + '" is at the beginning of the content and can’t be moved up';
+			errorPrefix + ' is at the beginning of the content and can’t be moved up';
 	}
 
 	return title;
