7e241134b355eb9a0ca8505195000048dd3ed74f
James Nylen <jnylen@gmail.com>
2017-05-29 14:01:43 -0400

    Use shorter form of test assertions

--- components/button/test/index.js
+++ components/button/test/index.js
@@ -13,45 +13,45 @@ describe( 'Button', () => {
 	describe( 'basic rendering', () => {
 		it( 'without modifiers', () => {
 			const button = shallow( <Button /> );
-			expect( button.hasClass( 'components-button' ) ).to.equal( true );
-			expect( button.hasClass( 'button' ) ).to.equal( false );
-			expect( button.hasClass( 'button-large' ) ).to.equal( false );
-			expect( button.hasClass( 'button-primary' ) ).to.equal( false );
-			expect( button.hasClass( 'is-toggled' ) ).to.equal( false );
-			expect( button.hasClass( 'is-borderless' ) ).to.equal( false );
-			expect( button.prop( 'disabled' ) ).to.equal( undefined );
+			expect( button.hasClass( 'components-button' ) ).to.be.true();
+			expect( button.hasClass( 'button' ) ).to.be.false();
+			expect( button.hasClass( 'button-large' ) ).to.be.false();
+			expect( button.hasClass( 'button-primary' ) ).to.be.false();
+			expect( button.hasClass( 'is-toggled' ) ).to.be.false();
+			expect( button.hasClass( 'is-borderless' ) ).to.be.false();
+			expect( button.prop( 'disabled' ) ).to.be.undefined();
 			expect( button.prop( 'type' ) ).to.equal( 'button' );
 			expect( button.type() ).to.equal( 'button' );
 		} );
 
 		it( 'with isPrimary', () => {
 			const button = shallow( <Button isPrimary /> );
-			expect( button.hasClass( 'button' ) ).to.equal( true );
-			expect( button.hasClass( 'button-large' ) ).to.equal( false );
-			expect( button.hasClass( 'button-primary' ) ).to.equal( true );
+			expect( button.hasClass( 'button' ) ).to.be.true();
+			expect( button.hasClass( 'button-large' ) ).to.be.false();
+			expect( button.hasClass( 'button-primary' ) ).to.be.true();
 		} );
 
 		it( 'with isLarge', () => {
 			const button = shallow( <Button isLarge /> );
-			expect( button.hasClass( 'button' ) ).to.equal( true );
-			expect( button.hasClass( 'button-large' ) ).to.equal( true );
-			expect( button.hasClass( 'button-primary' ) ).to.equal( false );
+			expect( button.hasClass( 'button' ) ).to.be.true();
+			expect( button.hasClass( 'button-large' ) ).to.be.true();
+			expect( button.hasClass( 'button-primary' ) ).to.be.false();
 		} );
 
 		it( 'with isToggled', () => {
 			const button = shallow( <Button isToggled /> );
-			expect( button.hasClass( 'button' ) ).to.equal( false );
-			expect( button.hasClass( 'is-toggled' ) ).to.equal( true );
+			expect( button.hasClass( 'button' ) ).to.be.false();
+			expect( button.hasClass( 'is-toggled' ) ).to.be.true();
 		} );
 
 		it( 'with disabled', () => {
 			const button = shallow( <Button disabled /> );
-			expect( button.prop( 'disabled' ) ).to.equal( true );
+			expect( button.prop( 'disabled' ) ).to.be.true();
 		} );
 
 		it( 'does not render target without href present', () => {
 			const button = shallow( <Button target="_blank" /> );
-			expect( button.prop( 'target' ) ).to.equal( undefined );
+			expect( button.prop( 'target' ) ).to.be.undefined();
 		} );
 	} );
 
@@ -80,7 +80,7 @@ describe( 'Button', () => {
 		it( 'renders with an extra className', () => {
 			const button = shallow( <Button className="gutenberg" /> );
 
-			expect( button.hasClass( 'gutenberg' ) ).to.equal( true );
+			expect( button.hasClass( 'gutenberg' ) ).to.be.true();
 		} );
 	} );
 
