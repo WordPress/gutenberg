48ab10660e509d7116288c0f5fbe501f234e0d30
Riad Benguella <benguella@gmail.com>
2017-05-29 09:47:08 +0100

    Accessibility: Drop the embeded FormToggle label and replace with hint

--- editor/sidebar/discussion-panel/index.js
+++ editor/sidebar/discussion-panel/index.js
@@ -7,6 +7,7 @@ import { connect } from 'react-redux';
  * WordPress dependencies
  */
 import { __ } from 'i18n';
+import { Component } from 'element';
 import PanelBody from 'components/panel/body';
 import FormToggle from 'components/form-toggle';
 
@@ -17,32 +18,46 @@ import './style.scss';
 import { getEditedPostAttribute } from '../../selectors';
 import { editPost } from '../../actions';
 
-function DiscussionPanel( { pingStatus = 'open', commentStatus = 'open', ...props } ) {
-	const onTogglePingback = () => props.editPost( { ping_status: pingStatus === 'open' ? 'closed' : 'open' } );
-	const onToggleComments = () => props.editPost( { comment_status: commentStatus === 'open' ? 'closed' : 'open' } );
+class DiscussionPanel extends Component {
+	constructor() {
+		super( ...arguments );
+		this.id = this.constructor.instances++;
+	}
 
-	// Disable Reason: The input is inside the label, we shouldn't need the htmlFor
-	/* eslint-disable jsx-a11y/label-has-for */
-	return (
-		<PanelBody title={ __( 'Discussion' ) } initialOpen={ false }>
-			<label className="editor-discussion-panel__row">
-				<span>{ __( 'Allow Comments' ) }</span>
-				<FormToggle
-					checked={ commentStatus === 'open' }
-					onChange={ onToggleComments }
-				/>
-			</label>
-			<label className="editor-discussion-panel__row">
-				<span>{ __( 'Allow Pingbacks & Trackbacks' ) }</span>
-				<FormToggle
-					checked={ pingStatus === 'open' }
-					onChange={ onTogglePingback }
-				/>
-			</label>
-		</PanelBody>
-	);
-	/* eslint-enable jsx-a11y/label-has-for */
+	render() {
+		const { pingStatus = 'open', commentStatus = 'open', ...props } = this.props;
+		const onTogglePingback = () => props.editPost( { ping_status: pingStatus === 'open' ? 'closed' : 'open' } );
+		const onToggleComments = () => props.editPost( { comment_status: commentStatus === 'open' ? 'closed' : 'open' } );
+
+		const commentsToggleId = 'allow-comments-toggle-' + this.id;
+		const pingbacksToggleId = 'allow-pingbacks-toggle-' + this.id;
+
+		return (
+			<PanelBody title={ __( 'Discussion' ) } initialOpen={ false }>
+				<div className="editor-discussion-panel__row">
+					<label htmlFor={ commentsToggleId }>{ __( 'Allow Comments' ) }</label>
+					<FormToggle
+						checked={ commentStatus === 'open' }
+						onChange={ onToggleComments }
+						showHint={ false }
+						id={ commentsToggleId }
+					/>
+				</div>
+				<div className="editor-discussion-panel__row">
+					<label htmlFor={ pingbacksToggleId }>{ __( 'Allow Pingbacks & Trackbacks' ) }</label>
+					<FormToggle
+						checked={ pingStatus === 'open' }
+						onChange={ onTogglePingback }
+						showHint={ false }
+						id={ pingbacksToggleId }
+					/>
+				</div>
+			</PanelBody>
+		);
+	}
 }
+
+DiscussionPanel.instances = 1;
 
 export default connect(
 	( state ) => {
