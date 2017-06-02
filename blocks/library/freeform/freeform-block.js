28fad958eda2dcd22efa2fce2d53f250e110a671
James Johnson <james.johnson@ephox.com>
2017-06-02 17:29:27 +1000

    Try to hook into TinyMCE button state

--- blocks/library/freeform/freeform-block.js
+++ blocks/library/freeform/freeform-block.js
@@ -1,125 +1,243 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
-import { find } from 'lodash';
+import { nodeListToReact } from 'dom-react';
+import { isEqual, omitBy } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import Editable from '../../editable';
+import TinyMCE from '../../editable/tinymce';
 import BlockControls from '../../block-controls';
-
-function addPluginToSettings( plugin ) {
-	return ( settings ) => ( {
-		...settings,
-		plugins: ( settings.plugins || [] ).concat( plugin ),
-	} );
-}
-
-function execCommand( command ) {
-	return ( editor ) => {
-		if ( editor ) {
-			editor.execCommand( command );
-		}
-	};
-}
-
-function blockQuoteIsActive() {
-	return ( props, state ) => {
-		const { inBlockQuote } = state;
-		return inBlockQuote;
-	};
-}
-
-function listIsActive( expectedListType ) {
-	return ( props, state ) => {
-		const { listType } = state;
-		return expectedListType === listType;
-	};
-}
-
-function findListType( { parents } ) {
-	const list = find( parents, ( node ) => node.nodeName === 'UL' || node.nodeName === 'OL' );
-	return list ? list.nodeName : null;
-}
-
-function findInBlockQuote( { parents } ) {
-	const quote = find( parents, ( node ) => node.nodeName === 'BLOCKQUOTE' );
-	return ! ! quote;
-}
 
 const FREEFORM_CONTROLS = [
 	{
+		id: 'blockquote',
 		icon: 'editor-quote',
 		title: wp.i18n.__( 'Quote' ),
-		isActive: blockQuoteIsActive(),
-		onClick: execCommand( 'mceBlockQuote' ),
 	},
 	{
+		id: 'bullist',
 		icon: 'editor-ul',
 		title: wp.i18n.__( 'Convert to unordered' ),
-		isActive: listIsActive( 'UL' ),
-		onClick: execCommand( 'InsertUnorderedList' ),
 	},
 	{
+		id: 'numlist',
 		icon: 'editor-ol',
 		title: wp.i18n.__( 'Convert to ordered' ),
-		isActive: listIsActive( 'OL' ),
-		onClick: execCommand( 'InsertOrderedList' ),
+	},
+	{
+		id: 'bold',
+		icon: 'editor-bold',
+		title: wp.i18n.__( 'Bold' ),
+	},
+	{
+		id: 'italic',
+		icon: 'editor-italic',
+		title: wp.i18n.__( 'Italic' ),
+	},
+	{
+		id: 'strikethrough',
+		icon: 'editor-strikethrough',
+		title: wp.i18n.__( 'Strikethrough' ),
 	},
 ];
 
+const ALIGNMENT_CONTROLS = [
+	{
+		id: 'alignleft',
+		icon: 'editor-alignleft',
+		title: wp.i18n.__( 'Align left' ),
+	},
+	{
+		id: 'aligncenter',
+		icon: 'editor-aligncenter',
+		title: wp.i18n.__( 'Align center' ),
+	},
+	{
+		id: 'alignright',
+		icon: 'editor-alignright',
+		title: wp.i18n.__( 'Align right' ),
+	},
+];
+
+function createElement( type, props, ...children ) {
+	if ( props[ 'data-mce-bogus' ] === 'all' ) {
+		return null;
+	}
+
+	if ( props.hasOwnProperty( 'data-mce-bogus' ) ) {
+		return children;
+	}
+
+	return wp.element.createElement(
+		type,
+		omitBy( props, ( value, key ) => key.indexOf( 'data-mce-' ) === 0 ),
+		...children
+	);
+}
+
 export default class FreeformBlock extends wp.element.Component {
-	constructor() {
-		super();
+	constructor( props ) {
+		super( ...arguments );
+		this.getSettings = this.getSettings.bind( this );
+		this.setButtonActive = this.setButtonActive.bind( this );
+		this.onSetup = this.onSetup.bind( this );
+		this.onInit = this.onInit.bind( this );
+		this.onChange = this.onChange.bind( this );
+		this.onFocus = this.onFocus.bind( this );
+		this.updateFocus = this.updateFocus.bind( this );
+		this.updateContent = this.updateContent.bind( this );
+		this.setContent = this.setContent.bind( this );
+		this.getContent = this.getContent.bind( this );
+		this.controls = this.mapControls.bind( this );
+		this.editor = null;
+		this.savedContent = null;
 		this.state = {
-			editor: null,
-			listType: null,
-			inBlockQuote: false,
+			empty: ! props.value || ! props.value.length,
+			activeButtons: { },
 		};
-		this.setEditor = this.setEditor.bind( this );
-		this.controls = this.controls.bind( this );
 	}
 
-	setEditor( editor ) {
-		editor.on( 'nodeChange', ( nodeInfo ) => {
-			this.setState( {
-				listType: findListType( nodeInfo ),
-				inBlockQuote: findInBlockQuote( nodeInfo ),
-			} );
+	getSettings( baseSettings ) {
+		return {
+			...baseSettings,
+			plugins: ( baseSettings.plugins || [] ).concat( 'lists' ),
+		};
+	}
+
+	setButtonActive( id, active ) {
+		const activeButtons = {
+			...this.state.activeButtons,
+			[ id ]: active,
+		};
+		this.setState( { activeButtons } );
+	}
+
+	onSetup( editor ) {
+		this.editor = editor;
+		editor.on( 'init', this.onInit );
+		editor.on( 'focusout', this.onChange );
+		editor.on( 'focusin', this.onFocus );
+	}
+
+	onInit() {
+		FREEFORM_CONTROLS.forEach( ( control ) => {
+			if ( control.id ) {
+				const button = this.editor.buttons[ control.id ];
+				button.onPostRender.call( {
+					active: ( isActive ) => this.setButtonActive( control.id, isActive ),
+				}, this.editor );
+			}
 		} );
-		this.setState( { editor } );
+		this.updateFocus();
 	}
 
-	controls() {
-		return FREEFORM_CONTROLS.map( ( control ) => ( {
+	onChange() {
+		if ( ! this.editor.isDirty() ) {
+			return;
+		}
+
+		this.savedContent = this.getContent();
+		this.editor.save();
+		this.props.onChange( this.savedContent );
+	}
+
+	onFocus() {
+		if ( this.props.onFocus ) {
+			this.props.onFocus();
+		}
+	}
+
+	updateFocus() {
+		const { focus } = this.props;
+		if ( focus ) {
+			this.editor.focus();
+			// Offset = -1 means we should focus the end of the editable
+			if ( focus.offset === -1 ) {
+				this.editor.selection.select( this.editor.getBody(), true );
+				this.editor.selection.collapse( false );
+			}
+		} else {
+			this.editor.getBody().blur();
+		}
+	}
+
+	updateContent() {
+		const bookmark = this.editor.selection.getBookmark( 2, true );
+		this.savedContent = this.props.value;
+		this.setContent( this.savedContent );
+		this.editor.selection.moveToBookmark( bookmark );
+
+		// Saving the editor on updates avoid unecessary onChanges calls
+		// These calls can make the focus jump
+		this.editor.save();
+	}
+
+	setContent( content ) {
+		if ( ! content ) {
+			content = '';
+		}
+
+		content = wp.element.renderToString( content );
+		this.editor.setContent( content, { format: 'raw' } );
+	}
+
+	getContent() {
+		return nodeListToReact( this.editor.getBody().childNodes || [], createElement );
+	}
+
+	mapControls( controls ) {
+		return controls.map( ( control ) => ( {
 			...control,
-			onClick: () => control.onClick( this.state.editor ),
-			isActive: control.isActive( this.props, this.state ),
+			onClick: () => this.editor && this.editor.buttons[ control.id ].onclick(),
+			isActive: this.state.activeButtons[ control.id ],
 		} ) );
 	}
 
+	componentWillUnmount() {
+		this.onChange();
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( this.props.focus !== prevProps.focus ) {
+			this.updateFocus();
+		}
+
+		// The `savedContent` var allows us to avoid updating the content right after an `onChange` call
+		if (
+			this.props.content !== prevProps.content &&
+			this.props.content !== this.savedContent &&
+			! isEqual( this.props.content, prevProps.content ) &&
+			! isEqual( this.props.content, this.savedContent )
+		) {
+			this.updateContent();
+		}
+	}
+
 	render() {
-		const { content, focus, onFocus, onChange } = this.props;
+		const { content, focus } = this.props;
 
 		return [
 			focus && (
 				<BlockControls
-					key="controls"
-					controls={ this.controls() }
+					key="aligns"
+					controls={ this.mapControls( ALIGNMENT_CONTROLS ) }
 				/>
 			),
-			<Editable
+			focus && (
+				<BlockControls
+					key="controls"
+					controls={ this.mapControls( FREEFORM_CONTROLS ) }
+				/>
+			),
+			<TinyMCE
 				key="editor"
-				getSettings={ addPluginToSettings( 'lists' ) }
-				style={ { width: '100%' } }
-				onSetup={ this.setEditor }
-				onChange={ onChange }
-				value={ content }
-				focus={ focus }
-				onFocus={ onFocus }
-				showAlignments
-				className="blocks-freeform" />,
+				getSettings={ this.getSettings }
+				onSetup={ this.onSetup }
+				defaultValue={ content }
+				isEmpty={ this.state.empty }
+				/>,
 		];
 	}
 }
