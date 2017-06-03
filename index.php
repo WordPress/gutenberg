e94961092358a61f83d014f482bddb97a9a4dad7
Weston Ruter <weston@xwp.co>
2017-06-02 20:35:14 -0700

    Add missing requires for plugin API and gutenberg plugin file

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
  */
 
 _deprecated_file( __FILE__, '0.1.0' );
+
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
 deactivate_plugins( array( 'gutenberg/index.php' ) );
 activate_plugins( array( 'gutenberg/gutenberg.php' ) );
+
+require_once dirname( __FILE__ ) . '/gutenberg.php';
