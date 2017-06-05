dc951838faea787ea8adee98a9345c633782e16d
James Nylen <jnylen@gmail.com>
2017-06-05 11:09:31 -0400

    Make phpcs happy

--- lib/check-duplicate-plugins.php
+++ lib/check-duplicate-plugins.php
@@ -7,11 +7,13 @@
 
 if ( function_exists( 'the_gutenberg_project' ) ) {
 	if ( ! defined( 'GUTENBERG_MULTIPLE_COPIES' ) ) {
+		/**
+		 * Displays an admin notice indicating a problem with the plugin.
+		 */
 		function gutenberg_multiple_copies_admin_notice() {
 			echo '<div class="notice notice-error"><p>';
 			_e(
-				'You have multiple copies of the Gutenberg plugin active.'
-				. ' Please deactivate or delete all but one copy.',
+				'You have multiple copies of the Gutenberg plugin active. Please deactivate or delete all but one copy.',
 				'gutenberg'
 			);
 			echo '</p></div>';
