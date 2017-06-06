1f1f9b20b67b7f01efebcb33165ec4a0575f42e4
Riad Benguella <benguella@gmail.com>
2017-06-06 15:12:12 +0100

    Chrome: Show "copied" on the permalink button when copied to the clipboard

--- editor/post-permalink/index.js
+++ editor/post-permalink/index.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 /**
  * WordPress dependencies
  */
+import { Component } from 'element';
 import { __ } from 'i18n';
 import { Dashicon, ClipboardButton } from 'components';
 
@@ -15,21 +16,49 @@ import { Dashicon, ClipboardButton } from 'components';
 import './style.scss';
 import { getEditedPostAttribute } from '../selectors';
 
-function PostPermalink( { link } ) {
-	if ( ! link ) {
-		return null;
+class PostPermalink extends Component {
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			showCopyConfirmation: false,
+		};
+		this.onCopy = this.onCopy.bind( this );
 	}
 
-	return (
-		<div className="editor-post-permalink">
-			<Dashicon icon="admin-links" />
-			<span className="editor-post-permalink__label">{ __( 'Permalink:' ) }</span>
-			<span className="editor-post-permalink__link">{ link }</span>
-			<ClipboardButton className="button" text={ link }>
-				{ __( 'Copy' ) }
-			</ClipboardButton>
-		</div>
-	);
+	componentWillUnmout() {
+		clearTimeout( this.dismissCopyConfirmation );
+	}
+
+	onCopy() {
+		this.setState( {
+			showCopyConfirmation: true,
+		} );
+
+		clearTimeout( this.dismissCopyConfirmation );
+		this.dismissCopyConfirmation = setTimeout( () => {
+			this.setState( {
+				showCopyConfirmation: false,
+			} );
+		}, 4000 );
+	}
+
+	render() {
+		const { link } = this.props;
+		if ( ! link ) {
+			return null;
+		}
+
+		return (
+			<div className="editor-post-permalink">
+				<Dashicon icon="admin-links" />
+				<span className="editor-post-permalink__label">{ __( 'Permalink:' ) }</span>
+				<span className="editor-post-permalink__link">{ link }</span>
+				<ClipboardButton className="button" text={ link } onCopy={ this.onCopy }>
+					{ this.state.showCopyConfirmation ? __( 'Copied!' ) : __( 'Copy' ) }
+				</ClipboardButton>
+			</div>
+		);
+	}
 }
 
 export default connect(
