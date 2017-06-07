c7ca12a588c23e0c218fd313acb818b4377e5acb
Riad Benguella <benguella@gmail.com>
2017-06-07 10:24:33 +0100

    Chrome: FadeIn/Out border in the Post Title Field

--- editor/post-title/index.js
+++ editor/post-title/index.js
@@ -28,11 +28,36 @@ const REGEXP_NEWLINES = /[\r\n]+/g;
 class PostTitle extends Component {
 	constructor() {
 		super( ...arguments );
+		this.bindTextarea = this.bindTextarea.bind( this );
 		this.onChange = this.onChange.bind( this );
-		this.onFocus = this.onFocus.bind( this );
+		this.select = this.select.bind( this );
+		this.unselect = this.unselect.bind( this );
+		this.onSelectionChange = this.onSelectionChange.bind( this );
 		this.state = {
 			isSelected: false,
 		};
+	}
+
+	componentDidMount() {
+		document.addEventListener( 'selectionchange', this.onSelectionChange );
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener( 'selectionchange', this.onSelectionChange );
+	}
+
+	bindTextarea( ref ) {
+		this.textareaContainer = ref;
+	}
+
+	onSelectionChange() {
+		const textarea = this.textareaContainer.textarea;
+		if (
+			document.activeElement === textarea &&
+			textarea.selectionStart !== textarea.selectionEnd
+		) {
+			this.select();
+		}
 	}
 
 	onChange( event ) {
@@ -40,8 +65,12 @@ class PostTitle extends Component {
 		this.props.onUpdate( newTitle );
 	}
 
-	onFocus() {
+	select() {
 		this.setState( { isSelected: true } );
+	}
+
+	unselect() {
+		this.setState( { isSelected: false } );
 	}
 
 	handleClickOutside() {
@@ -60,18 +89,19 @@ class PostTitle extends Component {
 		const className = classnames( 'editor-post-title', { 'is-selected': isSelected } );
 
 		return (
-			<div
-				className={ className }
-				onFocus={ this.onFocus }
-			>
+			<div className={ className }>
 				{ isSelected && <PostPermalink /> }
 				<h1>
 					<Textarea
+						ref={ this.bindTextarea }
 						className="editor-post-title__input"
 						value={ title }
 						onChange={ this.onChange }
 						placeholder={ wp.i18n.__( 'Enter title here' ) }
+						onFocus={ this.select }
+						onClick={ this.select }
 						onKeyDown={ this.onKeyDown }
+						onKeyPress={ this.unselect }
 					/>
 				</h1>
 			</div>
