48a2622dabc588c0461fc9fb7f92e8272ae4d6a6
Riad Benguella <benguella@gmail.com>
2017-06-08 15:39:14 +0100

    Insepector: Avoid arrow function callback in TextControl

--- blocks/inspector-controls/text-control/index.js
+++ blocks/inspector-controls/text-control/index.js
@@ -12,17 +12,21 @@ class TextControl extends Component {
 	constructor() {
 		super( ...arguments );
 		this.instanceId = this.constructor.instances++;
+		this.onChange = this.onChange.bind( this );
+	}
+
+	onChange( event ) {
+		this.props.onChange( event.target.value );
 	}
 
 	render() {
-		const { label, value, onChange } = this.props;
+		const { label, value } = this.props;
 		const id = 'inspector-text-control-' + this.instanceId;
-		const updateValue = ( event ) => onChange( event.target.value );
 
 		return (
 			<div className="blocks-text-control">
 				<label className="blocks-text-control__label" htmlFor={ id }>{ label }</label>
-				<input className="blocks-text-control__input" id={ id } value={ value } onChange={ updateValue } />
+				<input className="blocks-text-control__input" id={ id } value={ value } onChange={ this.onChange } />
 			</div>
 		);
 	}
