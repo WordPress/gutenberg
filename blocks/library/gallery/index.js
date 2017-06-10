741e184d37312a46de6844b4da3b902a1ba739f1
Neil Anderson <njp.anderson@icloud.com>
2017-06-10 19:56:03 +0100

    Change auto-open prop to autoOpen
    Fixes #1083

--- blocks/library/gallery/index.js
+++ blocks/library/gallery/index.js
@@ -109,7 +109,7 @@ registerBlockType( 'core/gallery', {
 					<MediaUploadButton
 						onSelect={ setMediaUrl }
 						type="image"
-						auto-open
+						autoOpen
 						multiple="true"
 					>
 						{ wp.i18n.__( 'Insert from Media Library' ) }
