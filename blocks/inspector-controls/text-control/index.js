4b0ff41ee51cc56296e42c0ed9aaaf97e56c8252
Paul Wilde <paulwilde@gmail.com>
2017-06-12 18:05:12 +0100

    Allow custom attributes for the inspector text control. Default the type attribute to ‘text’ to inherit the WordPress CSS styles.

--- blocks/inspector-controls/text-control/index.js
+++ blocks/inspector-controls/text-control/index.js
@@ -3,14 +3,15 @@
  */
 import { withInstanceId } from 'components';
 
-function TextControl( { label, value, instanceId, onChange } ) {
+function TextControl( { label, value, instanceId, onChange, ...attributes } ) {
 	const id = 'inspector-text-control-' + instanceId;
+    const type = attributes.type || 'text';
 	const onChangeValue = ( event ) => onChange( event.target.value );
 
 	return (
 		<div className="blocks-inspector-control">
 			<label className="blocks-inspector-control__label" htmlFor={ id }>{ label }</label>
-			<input className="blocks-inspector-control__input" id={ id } value={ value } onChange={ onChangeValue } />
+			<input className="blocks-inspector-control__input" type={ type } id={ id } value={ value } onChange={ onChangeValue } { ...attributes } />
 		</div>
 	);
 }
