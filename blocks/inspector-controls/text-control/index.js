39e9229638cb7555e990bcc56098390b705f9868
Paul Wilde <paulwilde@gmail.com>
2017-06-12 17:57:33 +0100

    Add re-usable CSS classes for inspector controls.

--- blocks/inspector-controls/text-control/index.js
+++ blocks/inspector-controls/text-control/index.js
@@ -3,19 +3,14 @@
  */
 import { withInstanceId } from 'components';
 
-/**
- * Internal dependencies
- */
-import './style.scss';
-
 function TextControl( { label, value, instanceId, onChange } ) {
 	const id = 'inspector-text-control-' + instanceId;
 	const onChangeValue = ( event ) => onChange( event.target.value );
 
 	return (
-		<div className="blocks-text-control">
-			<label className="blocks-text-control__label" htmlFor={ id }>{ label }</label>
-			<input className="blocks-text-control__input" id={ id } value={ value } onChange={ onChangeValue } />
+		<div className="blocks-inspector-control">
+			<label className="blocks-inspector-control__label" htmlFor={ id }>{ label }</label>
+			<input className="blocks-inspector-control__input" id={ id } value={ value } onChange={ onChangeValue } />
 		</div>
 	);
 }
