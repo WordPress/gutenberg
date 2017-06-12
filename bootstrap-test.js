65dafee0dc7a25ec6befbd58cb71f1f74036a423
James Nylen <jnylen@gmail.com>
2017-06-12 17:49:16 -0400

    Try removing TinyMCE initialization hacks

--- bootstrap-test.js
+++ bootstrap-test.js
@@ -24,14 +24,6 @@ global.document = dom.window.document;
 global.navigator = dom.window.navigator;
 global.requestAnimationFrame = window.setTimeout;
 
-// These are necessary to load TinyMCE successfully
-global.URL = window.URL;
-global.window.tinyMCEPreInit = {
-	// Without this, TinyMCE tries to determine its URL by looking at the
-	// <script> tag where it was loaded from, which of course fails here.
-	baseURL: 'about:blank',
-};
-
 global.window._wpDateSettings = {
 	formats: {
 		date: 'j F Y',
