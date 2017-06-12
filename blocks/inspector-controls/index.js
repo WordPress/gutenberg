795194180b3b7a91d28762ba3e2c74ca872ca6a1
Paul Wilde <paulwilde@gmail.com>
2017-06-12 17:57:33 +0100

    Add re-usable CSS classes for inspector controls.

--- blocks/inspector-controls/index.js
+++ blocks/inspector-controls/index.js
@@ -3,6 +3,11 @@
  */
 import { Fill } from 'react-slot-fill';
 
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
 export default function InspectorControls( { children } ) {
 	return (
 		<Fill name="Inspector.Controls">
