f1865154062ce6b55154fffe54af6567e03b0c1d
James Johnson <james.johnson@ephox.com>
2017-06-13 16:37:15 +1000

    Allow toolbar items to have child elements

--- components/toolbar/index.js
+++ components/toolbar/index.js
@@ -17,22 +17,27 @@ function Toolbar( { controls, focus } ) {
 	return (
 		<ul className="components-toolbar">
 			{ controls.map( ( control, index ) => (
-				<IconButton
+				<li
 					key={ index }
-					icon={ control.icon }
-					label={ control.title }
-					data-subscript={ control.subscript }
-					onClick={ ( event ) => {
-						event.stopPropagation();
-						control.onClick();
-					} }
-					className={ classNames( 'components-toolbar__control', {
-						'is-active': control.isActive,
-						'left-divider': control.leftDivider,
-					} ) }
-					aria-pressed={ control.isActive }
-					focus={ focus && ! index }
-				/>
+					className={ control.leftDivider ? 'left-divider' : null }
+				>
+					<IconButton
+						icon={ control.icon }
+						label={ control.title }
+						data-subscript={ control.subscript }
+						onClick={ ( event ) => {
+							event.stopPropagation();
+							control.onClick();
+						} }
+						className={ classNames( 'components-toolbar__control', {
+							'is-active': control.isActive,
+						} ) }
+						aria-pressed={ control.isActive }
+						focus={ focus && ! index }
+						disabled={ control.isDisabled }
+					/>
+					{ control.children }
+				</li>
 			) ) }
 		</ul>
 	);
