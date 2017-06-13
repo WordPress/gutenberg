9cfeaf2ad8e6a72f99e1f33d0693f2cbba1182d5
Riad Benguella <benguella@gmail.com>
2017-06-13 14:42:03 +0100

    Chrome: Adding the post category selector

--- editor/sidebar/post-taxonomies/index.js
+++ editor/sidebar/post-taxonomies/index.js
@@ -9,11 +9,13 @@ import PanelBody from 'components/panel/body';
  */
 import './style.scss';
 import TagsSelector from './tags-selector';
+import CategoriesSelector from './categories-selector';
 
 function PostTaxonomies() {
 	return (
 		<PanelBody title={ __( 'Categories & Tags' ) } initialOpen={ false }>
 			<TagsSelector />
+			<CategoriesSelector />
 		</PanelBody>
 	);
 }
