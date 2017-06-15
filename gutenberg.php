a58408731500eae5485fcbd5dfe662dfb584002c
Matías Ventura <mv@matiasventura.com>
2017-06-15 14:41:38 +0200

    Use beta with YYMMDD date format.

--- gutenberg.php
+++ gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gutenberg
  * Plugin URI: https://github.com/WordPress/gutenberg
  * Description: Prototyping since 1440. This is the development plugin for the new block editor in core. <strong>Meant for development, do not run on real sites.</strong>
- * Version: 0.1.0
+ * Version: beta-20170615
  * Author: Gutenberg Team
  *
  * @package gutenberg
