e1e28872f6eacfbf99c919c0da24df5f67305208
Paul Wilde <paulwilde@gmail.com>
2017-06-19 21:22:49 +0100

    Remove wrapper div.

--- blocks/inspector-controls/range-control/index.js
+++ blocks/inspector-controls/range-control/index.js
@@ -13,11 +13,9 @@ function RangeControl( { label, value, instanceId, onChange, ...props } ) {
 	const id = 'inspector-range-control-' + instanceId;
 
 	return (
-		<BaseControl label={ label } id={ id }>
-			<div className="blocks-range-control">
-				<input className="blocks-range-control__input" id={ id } type="range" value={ value } onChange={ onChange } { ...props } />
-				<span className="blocks-range-control__hint">{ value }</span>
-			</div>
+		<BaseControl label={ label } id={ id } className="blocks-range-control">
+			<input className="blocks-range-control__input" id={ id } type="range" value={ value } onChange={ onChange } { ...props } />
+			<span className="blocks-range-control__hint">{ value }</span>
 		</BaseControl>
 	);
 }
