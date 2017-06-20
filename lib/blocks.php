d2133099a409b7900fbb019942e4cb5618ff5ed7
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-06-20 23:48:03 +0200

    Use WP_Block_Type_Registry class to manage registered block types.

--- lib/blocks.php
+++ lib/blocks.php
@@ -9,64 +9,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Silence is golden.' );
 }
 
-$wp_registered_blocks = array();
-
 /**
- * Registers a block.
+ * Registers a block type.
  *
- * @param  string $name Block name including namespace.
- * @param  array  $settings Block settings.
-
- * @return array            The block, if it has been successfully registered.
+ * @since 0.1.0
+ *
+ * @param string $name Block type name including namespace.
+ * @param array  $args {
+ *     Array of block type arguments. Any arguments may be defined, however the following
+ *     ones are supported by default.
+ *
+ *     @type callable $render Callback used to render blocks of this block type.
+ * }
+ * @return WP_Block_Type|false The registered block type on success, or false on failure.
  */
-function register_block_type( $name, $settings ) {
-	global $wp_registered_blocks;
-
-	if ( ! is_string( $name ) ) {
-		$message = __( 'Block names must be strings.' );
-		_doing_it_wrong( __FUNCTION__, $message, '0.1.0' );
-		return false;
-	}
-
-	$name_matcher = '/^[a-z0-9-]+\/[a-z0-9-]+$/';
-	if ( ! preg_match( $name_matcher, $name ) ) {
-		$message = __( 'Block names must contain a namespace prefix. Example: my-plugin/my-custom-block' );
-		_doing_it_wrong( __FUNCTION__, $message, '0.1.0' );
-		return false;
-	}
-
-	if ( isset( $wp_registered_blocks[ $name ] ) ) {
-		/* translators: 1: block name */
-		$message = sprintf( __( 'Block "%s" is already registered.' ), $name );
-		_doing_it_wrong( __FUNCTION__, $message, '0.1.0' );
-		return false;
-	}
-
-	$settings['name'] = $name;
-	$wp_registered_blocks[ $name ] = $settings;
-
-	return $settings;
+function register_block_type( $name, $args ) {
+	return WP_Block_Type_Registry::get_instance()->register( $name, $args );
 }
 
 /**
- * Unregisters a block.
+ * Unregisters a block type.
  *
- * @param  string $name Block name.
- * @return array        The previous block value, if it has been
- *                      successfully unregistered; otherwise `null`.
+ * @since 0.1.0
+ *
+ * @param string $name Block type name including namespace.
+ * @return WP_Block_Type|false The unregistered block type on success, or false on failure.
  */
 function unregister_block_type( $name ) {
-	global $wp_registered_blocks;
-	if ( ! isset( $wp_registered_blocks[ $name ] ) ) {
-		/* translators: 1: block name */
-		$message = sprintf( __( 'Block "%s" is not registered.' ), $name );
-		_doing_it_wrong( __FUNCTION__, $message, '0.1.0' );
-		return false;
-	}
-	$unregistered_block = $wp_registered_blocks[ $name ];
-	unset( $wp_registered_blocks[ $name ] );
-
-	return $unregistered_block;
+	return WP_Block_Type_Registry::get_instance()->unregister( $name );
 }
 
 /**
@@ -99,17 +69,17 @@ function parse_block_attributes( $attr_string ) {
  * @return string          Updated post content.
  */
 function do_blocks( $content ) {
-	global $wp_registered_blocks;
+	$registry = WP_Block_Type_Registry::get_instance();
 
 	// Extract the blocks from the post content.
 	$matcher = '#' . join( '', array(
 		'(?P<opener><!--\s*',
-		'wp:(?P<block_name>[a-z](?:[a-z0-9/]+)*)\s+',
+		'wp:(?P<block_type_name>[a-z](?:[a-z0-9/]+)*)\s+',
 		'(?P<attributes>(?:(?!-->).)*)',
 		'\s*/?-->\n?)',
 		'(?:',
 		'(?P<content>.*?)',
-		'(?P<closer><!--\s*/wp:\g{block_name}\s+-->\n?)',
+		'(?P<closer><!--\s*/wp:\g{block_type_name}\s+-->\n?)',
 		')?',
 	) ) . '#s';
 	preg_match_all( $matcher, $content, $matches, PREG_OFFSET_CAPTURE );
@@ -117,15 +87,16 @@ function do_blocks( $content ) {
 	$new_content = $content;
 	$offset_differential = 0;
 	foreach ( $matches[0] as $index => $block_match ) {
-		$block_name = $matches['block_name'][ $index ][0];
+		$block_type_name = $matches['block_type_name'][ $index ][0];
+		$block_type = $registry->get_registered( $block_type_name );
 
 		$output = '';
-		if ( isset( $wp_registered_blocks[ $block_name ] ) ) {
+		if ( null !== $block_type ) {
 			$block_attributes_string = $matches['attributes'][ $index ][0];
 			$block_attributes = parse_block_attributes( $block_attributes_string );
 
 			// Call the block's render function to generate the dynamic output.
-			$output = call_user_func( $wp_registered_blocks[ $block_name ]['render'], $block_attributes );
+			$output = call_user_func( $block_type->render, $block_attributes );
 		} elseif ( isset( $matches['content'][ $index ][0] ) ) {
 			$output = $matches['content'][ $index ][0];
 		}
