deae2f7cbf543094e838b75d674a4ebe3c97965e
Boone Gorges <boonebgorges@git.wordpress.org>
2017-06-21 03:56:25 +0000

    Use `WP_Term_Query` when transforming tax queries.
    This change allows tax query transformations to be cached.
    
    Props spacedmonkey.
    Fixes #37038.
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@40918 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/term/taxQuery.php
+++ tests/phpunit/tests/term/taxQuery.php
@@ -163,7 +163,7 @@ class Tests_Term_Tax_Query extends WP_UnitTestCase {
 		) );
 		$tq->transform_query( $tq->queries[0], 'term_taxonomy_id' );
 
-		$this->assertSame( $tt_ids, $tq->queries[0]['terms'] );
+		$this->assertEqualSets( $tt_ids, $tq->queries[0]['terms'] );
 		$this->assertSame( 'term_taxonomy_id', $tq->queries[0]['field'] );
 	}
 
@@ -181,7 +181,7 @@ class Tests_Term_Tax_Query extends WP_UnitTestCase {
 		) );
 		$tq->transform_query( $tq->queries[0], 'term_taxonomy_id' );
 
-		$this->assertSame( $tt_ids, $tq->queries[0]['terms'] );
+		$this->assertEqualSets( $tt_ids, $tq->queries[0]['terms'] );
 		$this->assertSame( 'term_taxonomy_id', $tq->queries[0]['field'] );
 	}
 
