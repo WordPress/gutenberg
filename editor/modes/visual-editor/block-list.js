08c94ba2891b83faa040c31188c74dbd9e2713d1
iseulde <ella@iseulde.com>
2017-06-22 13:10:43 +0200

    Make sure lastClientY is always accurate

--- editor/modes/visual-editor/block-list.js
+++ editor/modes/visual-editor/block-list.js
@@ -39,17 +39,12 @@ class VisualEditorBlockList extends Component {
 		this.onCut = this.onCut.bind( this );
 		this.setBlockRef = this.setBlockRef.bind( this );
 		this.appendDefaultBlock = this.appendDefaultBlock.bind( this );
+		this.setLastClientY = this.setLastClientY.bind( this );
 		this.onPointerMove = throttle( this.onPointerMove.bind( this ), 250 );
 		// Browser does not fire `*move` event when the pointer position changes
 		// relative to the document, so fire it with the last known position.
 		this.onScroll = () => this.onPointerMove( { clientY: this.lastClientY } );
 
-		this.state = {
-			selectionAtStart: null,
-		};
-
-		this.coordMap = {};
-		this.coordMapKeys = [];
 		this.lastClientY = 0;
 		this.refs = {};
 	}
@@ -57,14 +52,19 @@ class VisualEditorBlockList extends Component {
 	componentDidMount() {
 		document.addEventListener( 'copy', this.onCopy );
 		document.addEventListener( 'cut', this.onCut );
+		window.addEventListener( 'mousemove', this.setLastClientY );
+		window.addEventListener( 'touchmove', this.setLastClientY );
 	}
 
 	componentWillUnmount() {
 		document.removeEventListener( 'copy', this.onCopy );
 		document.removeEventListener( 'cut', this.onCut );
+		window.removeEventListener( 'mousemove', this.setLastClientY );
+		window.removeEventListener( 'touchmove', this.setLastClientY );
+	}
 
-		// Cancel throttled calls.
-		this.onPointerMove.cancel();
+	setLastClientY( { clientY } ) {
+		this.lastClientY = clientY;
 	}
 
 	setBlockRef( ref, uid ) {
@@ -82,7 +82,6 @@ class VisualEditorBlockList extends Component {
 		const y = clientY + window.pageYOffset;
 		const key = this.coordMapKeys.reduce( ( acc, topY ) => y > topY ? topY : acc );
 
-		this.lastClientY = clientY;
 		this.onSelectionChange( this.coordMap[ key ] );
 	}
 
@@ -118,7 +117,7 @@ class VisualEditorBlockList extends Component {
 		} ), {} );
 		// Cache an array of the Y coödrinates for use in `onPointerMove`.
 		this.coordMapKeys = Object.keys( this.coordMap );
-		this.setState( { selectionAtStart: uid } );
+		this.selectionAtStart = uid;
 
 		window.addEventListener( 'mousemove', this.onPointerMove );
 		window.addEventListener( 'touchmove', this.onPointerMove );
@@ -129,7 +128,7 @@ class VisualEditorBlockList extends Component {
 
 	onSelectionChange( uid ) {
 		const { onMultiSelect, selectionStart, selectionEnd } = this.props;
-		const { selectionAtStart } = this.state;
+		const { selectionAtStart } = this;
 		const isAtStart = selectionAtStart === uid;
 
 		if ( ! selectionAtStart ) {
@@ -146,7 +145,12 @@ class VisualEditorBlockList extends Component {
 	}
 
 	onSelectionEnd() {
-		this.setState( { selectionAtStart: null } );
+		// Cancel throttled calls.
+		this.onPointerMove.cancel();
+
+		delete this.coordMap;
+		delete this.coordMapKeys;
+		delete this.selectionAtStart;
 
 		window.removeEventListener( 'mousemove', this.onPointerMove );
 		window.removeEventListener( 'touchmove', this.onPointerMove );
