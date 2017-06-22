56f93adaa665bec68eaa8cf593eac7a52ae10ef9
iseulde <ella@iseulde.com>
2017-06-22 20:06:40 +0200

    Add tests

--- blocks/api/paste.js
+++ blocks/api/paste.js
@@ -18,32 +18,46 @@ import { createBlock } from './factory';
  * @param  {Array} nodes Array of Nodes.
  * @return {Array}       Array of block level HTMLElements
  */
-function normaliseToBlockLevelNodes( nodes ) {
-	return nodes.reduce( ( acc, node, index ) => {
+export function normaliseToBlockLevelNodes( nodes ) {
+	const decu = document.createDocumentFragment();
+	const accu = document.createDocumentFragment();
+
+	// A fragment is easier to work with.
+	nodes.forEach( node => decu.appendChild( node.cloneNode( true ) ) );
+
+	while ( decu.firstChild ) {
+		const node = decu.firstChild;
+
 		// Text nodes: wrap in a paragraph, or append to previous.
 		if ( node.nodeType === 3 ) {
-			if ( ! acc.length || acc[ acc.length - 1 ].nodeName !== 'P' ) {
-				acc.push( document.createElement( 'P' ) );
+			if ( ! accu.lastChild || accu.lastChild.nodeName !== 'P' ) {
+				accu.appendChild( document.createElement( 'P' ) );
 			}
 
-			acc[ acc.length - 1 ].appendChild( node );
-		// BR nodes: create a new paragraph on double, or append to
-		// previous.
+			accu.lastChild.appendChild( node );
+		// BR nodes: create a new paragraph on double, or append to previous.
 		} else if ( node.nodeName === 'BR' ) {
-			if ( nodes[ index + 1 ] && nodes[ index + 1 ].nodeName === 'BR' ) {
-				acc.push( document.createElement( 'P' ) );
+			if ( node.nextSibling && node.nextSibling.nodeName === 'BR' ) {
+				accu.appendChild( document.createElement( 'P' ) );
+				decu.removeChild( node.nextSibling );
 			}
 
 			// Don't append to an empty paragraph.
-			if ( acc[ acc.length - 1 ].hasChildNodes() ) {
-				acc[ acc.length - 1 ].appendChild( node );
+			if (
+				accu.lastChild &&
+				accu.lastChild.nodeName === 'P' &&
+				accu.lastChild.hasChildNodes()
+			) {
+				accu.lastChild.appendChild( node );
+			} else {
+				decu.removeChild( node );
 			}
 		} else if ( node.nodeType === 1 ) {
-			acc.push( node );
+			accu.appendChild( node );
 		}
+	}
 
-		return acc;
-	}, [] );
+	return Array.from( accu.childNodes );
 }
 
 export default function( nodes ) {
