c016d5952a3302384c5aa62e7cc243f1b282d7c7
Matias Ventura <mv@matiasventura.com>
2017-06-22 19:51:06 +0200

    Add button to directly open the inspector sidebar from a block. (#1372)
    * Add button to directly open the inspector sidebar from a block.
    
    * Only show block settings button if sidebar is closed.
    
    * Display setting above delete.
    
    * Always show inspector button.

--- editor/block-settings-menu/index.js
+++ editor/block-settings-menu/index.js
@@ -13,10 +13,24 @@ import { IconButton } from 'components';
  * Internal dependencies
  */
 import './style.scss';
+import { isEditorSidebarOpened } from 'editor/selectors';
 
-function BlockSettingsMenu( { onDelete } ) {
+function BlockSettingsMenu( { onDelete, selectBlock, isSidebarOpened, toggleSidebar } ) {
+	const toggleInspector = () => {
+		selectBlock();
+		if ( ! isSidebarOpened ) {
+			toggleSidebar();
+		}
+	};
+
 	return (
 		<div className="editor-block-settings-menu">
+			<IconButton
+				className="editor-block-settings-menu__control"
+				onClick={ toggleInspector }
+				icon="admin-generic"
+				label={ __( 'Show inspector' ) }
+			/>
 			<IconButton
 				className="editor-block-settings-menu__control"
 				onClick={ onDelete }
@@ -28,13 +42,25 @@ function BlockSettingsMenu( { onDelete } ) {
 }
 
 export default connect(
-	undefined,
+	( state ) => ( {
+		isSidebarOpened: isEditorSidebarOpened( state ),
+	} ),
 	( dispatch, ownProps ) => ( {
 		onDelete() {
 			dispatch( {
 				type: 'REMOVE_BLOCKS',
 				uids: [ ownProps.uid ],
 			} );
+		},
+		selectBlock() {
+			dispatch( {
+				type: 'TOGGLE_BLOCK_SELECTED',
+				selected: true,
+				uid: ownProps.uid,
+			} );
+		},
+		toggleSidebar() {
+			dispatch( { type: 'TOGGLE_SIDEBAR' } );
 		},
 	} )
 )( BlockSettingsMenu );
