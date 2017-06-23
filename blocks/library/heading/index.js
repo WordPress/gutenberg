a057ec1e237553bca5902e2b099ae15bfb25b03f
Riad Benguella <benguella@gmail.com>
2017-06-23 15:17:00 +0100

    Blocks: Allowing to override the default classname generated for a block

--- blocks/library/heading/index.js
+++ blocks/library/heading/index.js
@@ -26,6 +26,8 @@ registerBlockType( 'core/heading', {
 
 	category: 'common',
 
+	className: false,
+
 	attributes: {
 		content: children( 'h1,h2,h3,h4,h5,h6' ),
 		nodeName: prop( 'h1,h2,h3,h4,h5,h6', 'nodeName' ),
