9500701aedd73a1bb9eb8e2e74a37d17ced2dd98
Matias Ventura <mv@matiasventura.com>
2017-06-28 13:34:51 +0200

    EmbedBlock: include align class in saved markup. (#1436)
    * EmbedBlock: include align class in saved markup.
    
    * Do not add align class if no alignment is set.

--- blocks/library/embed/index.js
+++ blocks/library/embed/index.js
@@ -197,13 +197,13 @@ function getEmbedBlockSettings( { title, icon, category = 'embed' } ) {
 		},
 
 		save( { attributes } ) {
-			const { url, caption } = attributes;
+			const { url, caption, align } = attributes;
 			if ( ! caption || ! caption.length ) {
 				return url;
 			}
 
 			return (
-				<figure>{ '\n' }
+				<figure className={ align && `align${ align }` }>{ '\n' }
 					{ url }
 					<figcaption>{ caption }</figcaption>
 				</figure>
