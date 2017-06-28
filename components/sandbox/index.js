d489bc6e5cfcaccad051a3f478c5ab0815905d53
Nicola Heald <nicola@notnowlewis.com>
2017-06-28 16:46:49 +0100

    Move to two phase render for embedded preview
    Test with the following content:
    
    Short tweet:
    https://twitter.com/notnownikki/status/876229494465581056
    
    Tall tweet:
    https://twitter.com/PattyJenks/status/874034832430424065
    
    Youtube:
    https://www.youtube.com/watch?v=PfKUdmTq2MI
    
    Photo:
    https://cloudup.com/cl3Oq5MU8Rm
    
    Long tumblr post:
    http://doctorwho.tumblr.com/post/162052108791

--- components/sandbox/index.js
+++ components/sandbox/index.js
@@ -5,6 +5,22 @@ import ResizableIframe from 'components/resizable-iframe';
 
 export default class Sandbox extends wp.element.Component {
 
+	// Two phase render for sandboxed content.
+	// 1) Write the html into the hidden iframe's document. This
+	//    makes sure all the external content gets loaded, so
+	//    we have size information for images, etc. This is needed
+	//    because the MutationObserver on the resizing iframe can
+	//    sometimes pick up that there's an images added to the DOM,
+	//    and triggers a resize *before* the image has loaded, and so
+	//    goes not get the size information, and so doesn't resize
+	//    correctly. The hidden iframe is sandboxed and will not
+	//    allow scripts to run, because there was a condition where
+	//    scripts were running while the onLoad triggered, and we
+	//    didn't get consistent information about assets.
+	// 2) When the document has loaded, shift the content into the
+	//    resizing iframe.
+	// 3) Create new script elements where needed.
+
 	static get defaultProps() {
 		return {
 			html: '',
@@ -12,12 +28,9 @@ export default class Sandbox extends wp.element.Component {
 		};
 	}
 
-	componentDidMount() {
-		const body = this.node.getFrameBody();
-		const { html } = this.props;
-
-		body.innerHTML = html;
-
+	shiftContent() {
+		const body = this.iframe.getFrameBody();
+		body.innerHTML = this.props.html;
 		const scripts = body.getElementsByTagName( 'script' );
 		const newScripts = Array.from( scripts ).map( ( script ) => {
 			const newScript = document.createElement( 'script' );
@@ -28,16 +41,29 @@ export default class Sandbox extends wp.element.Component {
 			}
 			return newScript;
 		} );
-
 		newScripts.forEach( ( script ) => body.appendChild( script ) );
+	}
+
+	componentDidMount() {
+		const { html } = this.props;
+		this.preRenderer.contentWindow.document.open();
+		this.preRenderer.contentWindow.document.write( html );
+		this.preRenderer.contentWindow.document.close();
 	}
 
 	render() {
 		return (
-			<ResizableIframe
-				sandbox="allow-same-origin allow-scripts"
-				title={ this.props.title }
-				ref={ ( node ) => this.node = node } />
+			<div>
+				<iframe
+					ref={ ( node ) => this.preRenderer = node }
+					style={ { display: 'none' } }
+					sandbox="allow-same-origin"
+					onLoad={ this.shiftContent.bind( this ) } />
+				<ResizableIframe
+					sandbox="allow-same-origin allow-scripts"
+					title={ this.props.title }
+					ref={ ( node ) => this.iframe = node } />
+			</div>
 		);
 	}
 }
