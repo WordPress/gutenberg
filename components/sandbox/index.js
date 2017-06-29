8d4385f15d81a2217117a8402c3159ac353fc72f
Nicola Heald <nicola@notnowlewis.com>
2017-06-29 11:30:02 +0100

    Clearer explanation of the two phase render

--- components/sandbox/index.js
+++ components/sandbox/index.js
@@ -6,20 +6,32 @@ import ResizableIframe from 'components/resizable-iframe';
 export default class Sandbox extends wp.element.Component {
 
 	// Two phase render for sandboxed content.
-	// 1) Write the html into the hidden iframe's document. This
-	//    makes sure all the external content gets loaded, so
-	//    we have size information for images, etc. This is needed
-	//    because the MutationObserver on the resizing iframe can
-	//    sometimes pick up that there's an images added to the DOM,
-	//    and triggers a resize *before* the image has loaded, and so
-	//    goes not get the size information, and so doesn't resize
-	//    correctly. The hidden iframe is sandboxed and will not
-	//    allow scripts to run, because there was a condition where
-	//    scripts were running while the onLoad triggered, and we
-	//    didn't get consistent information about assets.
-	// 2) When the document has loaded, shift the content into the
-	//    resizing iframe.
-	// 3) Create new script elements where needed.
+    //
+	// 1) Render the document into a hidden iframe,
+	//    with scripts disabled.
+	//
+	//    This makes sure all initial assets are loaded and we
+	//    have all size information for the initial render
+	//    in the ResizableIframe. Scripts may add, delete,
+	//    or otherwise mess with the initial assets, and
+	//    we need everything fully loaded to make sure the
+	//    ResizableIframe has all size information as soon
+	//    as it renders.
+	//
+	//    This stop scenarios like this:
+	//
+	//      * ResizableIframe gets an img node added to the DOM
+	//      * MutationObserver says, "Hey, I have a new node! Please resize!",
+	//        but the img has not loaded, so the dimensions are 0,0
+	//      * ResizableIframe resizes, but to 0,0
+	//      * Image loads, and is hidden because the iframe is 0,0
+	//
+	//    Instead, we are certain that all initial assets are loaded
+	//    and the iframe resizes correctly when it gets the initial
+	//    state of the document.
+	//
+	// 2) When the document has been pre-rendered (all assets loaded)
+	//    the onLoad triggers and renders into the ResizableIframe.
 
 	static get defaultProps() {
 		return {
@@ -31,6 +43,8 @@ export default class Sandbox extends wp.element.Component {
 	shiftContent() {
 		const body = this.iframe.getFrameBody();
 		body.innerHTML = this.props.html;
+
+		// recreate script elements so they get executed
 		const scripts = body.getElementsByTagName( 'script' );
 		const newScripts = Array.from( scripts ).map( ( script ) => {
 			const newScript = document.createElement( 'script' );
