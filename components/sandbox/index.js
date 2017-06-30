8c59861bb101527cdf83c0b0db9d750cdb094257
Nicola Heald <nicola@notnowlewis.com>
2017-06-30 11:40:13 +0100

    Different rendering for embedded preview (#1566)
    Fixes various race conditions rendering embedded content.
    
    Test with the following:
    
    Short tweet:
    https://twitter.com/notnownikki/status/876229494465581056
    
    Tall tweet:
    https://twitter.com/PattyJenks/status/874034832430424065
    
    Youtube:
    https://www.youtube.com/watch?v=PfKUdmTq2MI
    
    Photo:
    https://cloudup.com/cl3Oq5MU8Rm
    
    Long tumblr post:
    http://doctorwho.tumblr.com/post/162052108791

--- components/sandbox/index.js
+++ components/sandbox/index.js
@@ -1,9 +1,145 @@
-/**
- * Internal dependencies
- */
-import ResizableIframe from 'components/resizable-iframe';
+import { renderToString } from 'element';
 
 export default class Sandbox extends wp.element.Component {
+
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			width: 0,
+			height: 0,
+		};
+		this.trySandbox = this.trySandbox.bind( this );
+		this.checkMessageForResize = this.checkMessageForResize.bind( this );
+	}
+
+	isFrameAccessible() {
+		try {
+			return !! this.iframe.contentDocument.body;
+		} catch ( e ) {
+			return false;
+		}
+	}
+
+	checkMessageForResize( event ) {
+		const iframe = this.iframe;
+
+		// Attempt to parse the message data as JSON if passed as string
+		let data = event.data || {};
+		if ( 'string' === typeof data ) {
+			try {
+				data = JSON.parse( data );
+			} catch ( e ) {} // eslint-disable-line no-empty
+		}
+
+		// Verify that the mounted element is the source of the message
+		if ( ! iframe || iframe.contentWindow !== event.source ) {
+			return;
+		}
+
+		// Update the state only if the message is formatted as we expect, i.e.
+		// as an object with a 'resize' action, width, and height
+		const { action, width, height } = data;
+		const { width: oldWidth, height: oldHeight } = this.state;
+
+		if ( 'resize' === action && ( oldWidth !== width || oldHeight !== height ) ) {
+			this.setState( { width, height } );
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener( 'message', this.checkMessageForResize, false );
+		this.trySandbox();
+	}
+
+	componentDidUpdate() {
+		this.trySandbox();
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'message', this.checkMessageForResize );
+	}
+
+	trySandbox() {
+		if ( ! this.isFrameAccessible() ) {
+			return;
+		}
+
+		const body = this.iframe.contentDocument.body;
+		if ( null !== body.getAttribute( 'data-resizable-iframe-connected' ) ) {
+			return;
+		}
+
+		const observeAndResizeJS = `
+			( function() {
+				var observer;
+
+				if ( ! window.MutationObserver || ! document.body || ! window.top ) {
+					return;
+				}
+
+				function sendResize() {
+					window.top.postMessage( {
+						action: 'resize',
+						width: document.body.offsetWidth,
+						height: document.body.offsetHeight
+					}, '*' );
+				}
+
+				observer = new MutationObserver( sendResize );
+				observer.observe( document.body, {
+					attributes: true,
+					attributeOldValue: false,
+					characterData: true,
+					characterDataOldValue: false,
+					childList: true,
+					subtree: true
+				} );
+
+				window.addEventListener( 'load', sendResize, true );
+
+				// Hack: Remove viewport unit styles, as these are relative
+				// the iframe root and interfere with our mechanism for
+				// determining the unconstrained page bounds.
+				function removeViewportStyles( ruleOrNode ) {
+					[ 'width', 'height', 'minHeight', 'maxHeight' ].forEach( function( style ) {
+						if ( /^\\d+(vmin|vmax|vh|vw)$/.test( ruleOrNode.style[ style ] ) ) {
+							ruleOrNode.style[ style ] = '';
+						}
+					} );
+				}
+
+				Array.prototype.forEach.call( document.querySelectorAll( '[style]' ), removeViewportStyles );
+				Array.prototype.forEach.call( document.styleSheets, function( stylesheet ) {
+					Array.prototype.forEach.call( stylesheet.cssRules || stylesheet.rules, removeViewportStyles );
+				} );
+
+				document.body.style.position = 'absolute';
+				document.body.setAttribute( 'data-resizable-iframe-connected', '' );
+
+				sendResize();
+		} )();`;
+
+		// put the html snippet into a html document, and then write it to the iframe's document
+		// we can use this in the future to inject custom styles or scripts
+		const htmlDoc = (
+			<html lang={ document.documentElement.lang }>
+				<head>
+					<title>{ this.props.title }</title>
+				</head>
+				<body data-resizable-iframe-connected="data-resizable-iframe-connected">
+					<div dangerouslySetInnerHTML={ { __html: this.props.html } } />
+					<script type="text/javascript" dangerouslySetInnerHTML={ { __html: observeAndResizeJS } } />
+				</body>
+			</html>
+		);
+
+		// writing the document like this makes it act in the same way as if it was
+		// loaded over the network, so DOM creation and mutation, script execution, etc.
+		// all work as expected
+		this.iframe.contentWindow.document.open();
+		this.iframe.contentWindow.document.write( '<!DOCTYPE html>' + renderToString( htmlDoc ) );
+		this.iframe.contentWindow.document.close();
+	}
 
 	static get defaultProps() {
 		return {
@@ -12,32 +148,16 @@ export default class Sandbox extends wp.element.Component {
 		};
 	}
 
-	componentDidMount() {
-		const body = this.node.getFrameBody();
-		const { html } = this.props;
-
-		body.innerHTML = html;
-
-		const scripts = body.getElementsByTagName( 'script' );
-		const newScripts = Array.from( scripts ).map( ( script ) => {
-			const newScript = document.createElement( 'script' );
-			if ( script.src ) {
-				newScript.src = script.src;
-			} else {
-				newScript.innerHTML = script.innerHTML;
-			}
-			return newScript;
-		} );
-
-		newScripts.forEach( ( script ) => body.appendChild( script ) );
-	}
-
 	render() {
 		return (
-			<ResizableIframe
-				sandbox="allow-same-origin allow-scripts"
+			<iframe
+				ref={ ( node ) => this.iframe = node }
 				title={ this.props.title }
-				ref={ ( node ) => this.node = node } />
+				scrolling="no"
+				sandbox="allow-scripts allow-same-origin"
+				onLoad={ this.trySandbox }
+				width={ this.state.width }
+				height={ this.state.height } />
 		);
 	}
 }
