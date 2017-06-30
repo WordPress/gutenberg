785f19cee862f47e68c21e7835b8e80a93f5af81
Ryan Welcher <ryan.welcher@10up.com>
2017-06-30 08:17:58 -0400

    Resetting changes that were addressed in master

--- editor/sidebar/word-count/index.js
+++ editor/sidebar/word-count/index.js
@@ -7,19 +7,36 @@ import { connect } from 'react-redux';
  * WordPress dependencies
  */
 import { __ } from 'i18n';
-import { WordCounter } from 'utils';
-import { PanelHeader, FormToggle, withInstanceId } from 'components';
+import { Component } from 'element';
+import { PanelHeader } from 'components';
 
-function WordCount() {
 
-	console.log( WordCounter.bind(this) );
-	return (
-		<PanelHeader label={ __( 'Word Count' ) } >
-			<div>Count</div>
-		</PanelHeader>
-	);
+class WordCount extends Component {
+
+	componentDidMount() {
+		console.log( window.wp.utils.WordCounter );
+	}
+
+	render() {
+		return (
+			<PanelHeader label={ __( 'Word Count' ) } >
+				<div>{ this.props.wordcount }</div>
+			</PanelHeader>
+		);
+	}
 }
+
 export default connect(
-	undefined,
-	undefined
+	( state ) => {
+		return {
+			wordcount: 200,
+		};
+	},
+	( dispatch ) => {
+		return {
+			onUpdateExcerpt( excerpt ) {
+				dispatch( editPost( { excerpt } ) );
+			},
+		};
+	}
 )( WordCount );
