9eaf8f8e5b0339aab4656a8b740130ea84a7836e
iseulde <ella@iseulde.com>
2017-07-03 20:27:36 +0200

    Embrace lodash and utils/keycodes

--- utils/keycodes.js
+++ utils/keycodes.js
@@ -2,6 +2,7 @@ export const BACKSPACE = 8;
 export const TAB = 9;
 export const ENTER = 13;
 export const ESCAPE = 27;
+export const SPACE = 32;
 export const LEFT = 37;
 export const UP = 38;
 export const RIGHT = 39;
