7e24416792fe4833e7f220504257fcd6e7b2f520
iseulde <ella@iseulde.com>
2017-07-03 10:35:03 +0200

    Use exact same old editor for 'classic text' block

---
+++ blocks/library/freeform/old-editor.js
@@ -0,0 +1,160 @@
+/**
+ * WordPress dependencies
+ */
+import { Component } from 'element';
+import { __ } from 'i18n';
+
+export default class OldEditor extends Component {
+	constructor( props ) {
+		super( props );
+		this.initialize = this.initialize.bind( this );
+		this.onSetup = this.onSetup.bind( this );
+	}
+
+	componentDidMount() {
+		const { baseURL, suffix } = window.wpEditorL10n.tinymce;
+
+		window.tinymce.EditorManager.overrideDefaults( {
+			base_url: baseURL,
+			suffix,
+		} );
+
+		if ( document.readyState === 'complete' ) {
+			this.initialize();
+		} else {
+			window.addEventListener( 'DOMContentLoaded', this.initialize );
+		}
+	}
+
+	componentWillUnmount() {
+		window.addEventListener( 'DOMContentLoaded', this.initialize );
+		wp.oldEditor.remove( this.props.id );
+	}
+
+	componentDidUpdate( prevProps ) {
+		const { id, attributes: { content } } = this.props;
+
+		if ( prevProps.attributes.content !== content ) {
+			window.tinymce.get( id ).setContent( content || '' );
+		}
+	}
+
+	initialize() {
+		const { id } = this.props;
+		const {
+			plugins,
+			external_plugins,
+			toolbar1,
+			toolbar2,
+			toolbar3,
+			toolbar4,
+		} = window.wpEditorL10n.tinymce.settings;
+
+		wp.oldEditor.initialize( id, {
+			tinymce: {
+				inline: true,
+				content_css: false,
+				fixed_toolbar_container: '#' + id + '-toolbar',
+				plugins,
+				external_plugins,
+				toolbar1: toolbar1.join( ',' ),
+				toolbar2: toolbar2.join( ',' ),
+				toolbar3: toolbar3.join( ',' ),
+				toolbar4: toolbar4.join( ',' ),
+				setup: this.onSetup,
+			},
+		} );
+	}
+
+	onSetup( editor ) {
+		const { attributes: { content }, setAttributes } = this.props;
+		const { ref } = this;
+		const initialContent = window.switchEditors.wpautop( content || '' );
+
+		editor.on( 'loadContent', () => editor.setContent( initialContent ) );
+
+		editor.on( 'blur', () => {
+			setAttributes( {
+				content: editor.getContent(),
+			} );
+		} );
+
+		editor.addButton( 'kitchensink', {
+			tooltip: __( 'More' ),
+			icon: 'dashicon dashicons-editor-kitchensink',
+			onClick: function() {
+				const button = this;
+				const active = ! button.active();
+
+				button.active( active );
+				editor.dom.toggleClass( ref, 'has-advanced-toolbar', active );
+			},
+		} );
+
+		/* eslint-disable */
+
+		// Re-register WP_More as it doesn't work with inline mode.
+		// This should be fixed in core.
+		// See wp-includes/js/tinymce/plugins/wordpress/plugin.js
+		// Swaps node.nodeName === 'BODY' to node === editor.getBody()
+		editor.on( 'init', () => editor.addCommand( 'WP_More', function( tag ) {
+			var parent, html, title,
+				classname = 'wp-more-tag',
+				dom = editor.dom,
+				node = editor.selection.getNode(),
+				rootNode = editor.getBody();
+
+			tag = tag || 'more';
+			classname += ' mce-wp-' + tag;
+			title = tag === 'more' ? 'Read more...' : 'Next page';
+			title = editor.editorManager.i18n.translate( title );
+			html = '<img src="' + tinymce.Env.transparentSrc + '" alt="" title="' + title + '" class="' + classname + '" ' +
+				'data-wp-more="' + tag + '" data-mce-resize="false" data-mce-placeholder="1" />';
+
+			// Most common case
+			if ( node === rootNode || ( node.nodeName === 'P' && node.parentNode == rootNode ) ) {
+				editor.insertContent( html );
+				return;
+			}
+
+			// Get the top level parent node
+			parent = dom.getParent( node, function( found ) {
+				if ( found.parentNode && found.parentNode === rootNode ) {
+					return true;
+				}
+
+				return false;
+			}, editor.getBody() );
+
+			if ( parent ) {
+				if ( parent.nodeName === 'P' ) {
+					parent.appendChild( dom.create( 'p', null, html ).firstChild );
+				} else {
+					dom.insertAfter( dom.create( 'p', null, html ), parent );
+				}
+
+				editor.nodeChanged();
+			}
+		} ) );
+
+		/* eslint-enable */
+	}
+
+	render() {
+		const { id } = this.props;
+
+		return [
+			<div
+				key="toolbar"
+				id={ id + '-toolbar' }
+				ref={ ref => this.ref = ref }
+				className="editor-visual-editor__block-controls freeform-toolbar"
+			/>,
+			<div
+				key="editor"
+				id={ id }
+				className="blocks-editable__tinymce"
+			/>,
+		];
+	}
+}
--- lib/client-assets.php
+++ lib/client-assets.php
@@ -422,6 +422,11 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 	 * Scripts
 	 */
 	wp_enqueue_media();
+	wp_enqueue_editor();
+
+	wp_add_inline_script(
+		'editor', 'window.wp.oldEditor = window.wp.editor;', 'after'
+	);
 
 	gutenberg_extend_wp_api_backbone_client();
 
@@ -429,10 +434,71 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 	wp_enqueue_script(
 		'wp-editor',
 		gutenberg_url( 'editor/build/index.js' ),
-		array( 'wp-api', 'wp-date', 'wp-i18n', 'wp-blocks', 'wp-element', 'wp-components', 'wp-utils' ),
+		array( 'wp-api', 'wp-date', 'wp-i18n', 'wp-blocks', 'wp-element', 'wp-components', 'wp-utils', 'editor' ),
 		filemtime( gutenberg_dir_path() . 'editor/build/index.js' ),
 		true // enqueue in the footer.
 	);
+
+	wp_localize_script( 'wp-editor', 'wpEditorL10n', array(
+		'tinymce' => array(
+			'baseURL' => includes_url( 'js/tinymce' ),
+			'suffix' => SCRIPT_DEBUG ? '' : '.min',
+			'settings' => array(
+				'external_plugins' => apply_filters( 'mce_external_plugins', array() ),
+				'plugins' => array_unique( apply_filters( 'tiny_mce_plugins', array(
+					'charmap',
+					'colorpicker',
+					'hr',
+					'lists',
+					'media',
+					'paste',
+					'tabfocus',
+					'textcolor',
+					'fullscreen',
+					'wordpress',
+					'wpautoresize',
+					'wpeditimage',
+					'wpemoji',
+					'wpgallery',
+					'wplink',
+					'wpdialogs',
+					'wptextpattern',
+					'wpview',
+				) ) ),
+				'toolbar1' => array_merge( apply_filters( 'mce_buttons', array(
+					'formatselect',
+					'|',
+					'alignleft',
+					'aligncenter',
+					'alignright',
+					'|',
+					'bullist',
+					'numlist',
+					'blockquote',
+					'|',
+					'bold',
+					'italic',
+					'strikethrough',
+					'link',
+				), 'editor' ), array( '|', 'kitchensink' ) ),
+				'toolbar2' => apply_filters( 'mce_buttons_2', array(
+					'hr',
+					'wp_more',
+					'forecolor',
+					'pastetext',
+					'removeformat',
+					'charmap',
+					'outdent',
+					'indent',
+					'undo',
+					'redo',
+					'wp_help',
+				), 'editor' ),
+				'toolbar3' => apply_filters( 'mce_buttons_3', array(), 'editor' ),
+				'toolbar4' => apply_filters( 'mce_buttons_4', array(), 'editor' ),
+			),
+		),
+	) );
 
 	// Register `wp-utils` as a dependency of `word-count` to ensure that
 	// `wp-utils` doesn't clobbber `word-count`.  See WordPress/gutenberg#1569.
