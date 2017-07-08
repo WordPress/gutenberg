069975c5fb5b649fa8e7822c8c822db5f336b6d9
Weston Ruter <weston@xwp.co>
2017-07-08 13:40:31 -0700

    Show error message upon trashing failure

--- editor/effects.js
+++ editor/effects.js
@@ -83,7 +83,7 @@ export default {
 		const messages = {
 			publish: __( 'Post published!' ),
 			'private': __( 'Post published privately!' ),
-			future: __( 'Post schduled!' ),
+			future: __( 'Post scheduled!' ),
 		};
 
 		// If we save a non published post, we don't show any notice
@@ -132,12 +132,31 @@ export default {
 		const { dispatch, getState } = store;
 		const { postId } = action;
 		const Model = wp.api.getPostTypeModel( getCurrentPostType( getState() ) );
-		new Model( { id: postId } ).destroy().done( () => {
-			dispatch( {
-				...action,
-				type: 'TRASH_POST_SUCCESS',
-			} );
-		} );
+		new Model( { id: postId } ).destroy().then(
+			() => {
+				dispatch( {
+					...action,
+					type: 'TRASH_POST_SUCCESS',
+				} );
+			},
+			( err ) => {
+				dispatch( {
+					...action,
+					type: 'TRASH_POST_FAILURE',
+					error: get( err, 'responseJSON', {
+						code: 'unknown_error',
+						message: __( 'An unknown error occurred.' ),
+					} ),
+				} );
+				let message;
+				if ( err.responseJSON && err.responseJSON.message ) {
+					message = err.responseJSON.message;
+				} else {
+					message = __( 'Trashing failed' );
+				}
+				dispatch( errorNotice( message ) );
+			}
+		);
 	},
 	TRASH_POST_SUCCESS( action ) {
 		const { postId, postType } = action;
