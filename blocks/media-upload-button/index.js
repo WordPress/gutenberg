59b2c0227695a1b079cf2c3e7dfdbb6c20357d96
Weston Ruter <weston@xwp.co>
2017-07-08 23:41:23 -0700

    Open the gallery from placeholder in the initial creation state

--- blocks/media-upload-button/index.js
+++ blocks/media-upload-button/index.js
@@ -5,11 +5,56 @@ import { Component } from 'element';
 import { __ } from 'i18n';
 import { Button } from 'components';
 
+// Getter for the sake of unit tests.
+const getGalleryDetailsMediaFrame = () => {
+	/**
+	 * Custom gallery details frame.
+	 *
+	 * @link https://github.com/xwp/wp-core-media-widgets/blob/905edbccfc2a623b73a93dac803c5335519d7837/wp-admin/js/widgets/media-gallery-widget.js
+	 * @class GalleryDetailsMediaFrame
+	 * @constructor
+	 */
+	return wp.media.view.MediaFrame.Post.extend( {
+
+		/**
+		 * Create the default states.
+		 *
+		 * @returns {void}
+		 */
+		createStates: function createStates() {
+			this.states.add( [
+				new wp.media.controller.Library( {
+					id: 'gallery',
+					title: wp.media.view.l10n.createGalleryTitle,
+					priority: 40,
+					toolbar: 'main-gallery',
+					filterable: 'uploaded',
+					multiple: 'add',
+					editable: false,
+
+					library: wp.media.query( _.defaults( {
+						type: 'image',
+					}, this.options.library ) ),
+				} ),
+
+				new wp.media.controller.GalleryEdit( {
+					library: this.options.selection,
+					editing: this.options.editing,
+					menu: 'gallery',
+				} ),
+
+				new wp.media.controller.GalleryAdd(),
+			] );
+		},
+	} );
+};
+
 class MediaUploadButton extends Component {
-	constructor( { multiple = false, type } ) {
+	constructor( { multiple = false, type, gallery = false } ) {
 		super( ...arguments );
 		this.openModal = this.openModal.bind( this );
 		this.onSelect = this.onSelect.bind( this );
+		this.onUpdate = this.onUpdate.bind( this );
 		this.onOpen = this.onOpen.bind( this );
 		const frameConfig = {
 			title: __( 'Select or Upload a media' ),
@@ -17,19 +62,47 @@ class MediaUploadButton extends Component {
 				text: __( 'Select' ),
 			},
 			multiple,
+			selection: new wp.media.model.Selection( [] ),
 		};
 		if ( !! type ) {
 			frameConfig.library = { type };
 		}
-		this.frame = wp.media( frameConfig );
+
+		if ( gallery ) {
+			const GalleryDetailsMediaFrame = getGalleryDetailsMediaFrame();
+			this.frame = new GalleryDetailsMediaFrame( {
+				frame: 'select',
+				mimeType: type,
+				state: 'gallery',
+			} );
+			wp.media.frame = this.frame;
+		} else {
+			this.frame = wp.media( frameConfig );
+		}
 
 		// When an image is selected in the media frame...
 		this.frame.on( 'select', this.onSelect );
+		this.frame.on( 'update', this.onUpdate );
 		this.frame.on( 'open', this.onOpen );
 	}
 
 	componentWillUnmount() {
 		this.frame.remove();
+	}
+
+	onUpdate( selections ) {
+		const { onSelect, multiple = false } = this.props;
+		const state = this.frame.state();
+		const selectedImages = selections || state.get( 'selection' );
+
+		if ( ! selectedImages || ! selectedImages.models.length ) {
+			return;
+		}
+		if ( multiple ) {
+			onSelect( selectedImages.models.map( ( model ) => model.toJSON() ) );
+		} else {
+			onSelect( selectedImages.models[ 0 ].toJSON() );
+		}
 	}
 
 	onSelect() {
