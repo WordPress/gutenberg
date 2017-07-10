71d8e19e520726ddce92f27fd5f747262836fe41
Matías Ventura <mv@matiasventura.com>
2017-07-10 12:10:29 +0100

    Introduce Editable.

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -1,10 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from 'i18n';
+
+/**
+ * Internal dependencies
+ */
+import Editable from '../../editable';
 
 export default function GalleryImage( props ) {
 	return (
 		<figure className="blocks-gallery-image" onClick={ props.onSelect }>
 			<img src={ props.img.url } alt={ props.img.alt } />
 			{ props.focus &&
-				<figcaption>Add caption...</figcaption>
+				<Editable
+					tagName="figcaption"
+					placeholder={ __( 'Write caption…' ) }
+					focus={ props.focus }
+					inlineToolbar
+				/>
 			}
 		</figure>
 	);
