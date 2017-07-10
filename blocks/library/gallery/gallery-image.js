47c8e184945a345e067a9247f748e999ecaa53c9
Matías Ventura <mv@matiasventura.com>
2017-07-10 11:48:04 +0100

    Set focus on individual gallery images when selecting them.

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -1,8 +1,11 @@
 
 export default function GalleryImage( props ) {
 	return (
-		<figure className="blocks-gallery-image">
+		<figure className="blocks-gallery-image" onClick={ props.onSelect }>
 			<img src={ props.img.url } alt={ props.img.alt } />
+			{ props.focus &&
+				<figcaption>Add caption...</figcaption>
+			}
 		</figure>
 	);
 }
