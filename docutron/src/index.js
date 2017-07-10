62f769e702d032e15048b946b822a4169aa844db
Andrew Duthie <andrew@andrewduthie.com>
2017-07-10 19:20:07 +0200

    Remove service worker registration
    May be "nice to have", but not intending to support or maintain

--- docutron/src/index.js
+++ docutron/src/index.js
@@ -14,8 +14,6 @@ import 'config';
  * Internal Dependencies
  */
 import App from './components/app';
-import registerServiceWorker from './registerServiceWorker';
 import './styles/main.css';
 
 render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
