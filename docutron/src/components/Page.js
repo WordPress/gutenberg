3346367928e6cc98805d921114467f2b4861f8d0
Andrew Duthie <andrew@andrewduthie.com>
2017-07-10 19:09:17 +0200

    Extract components to separate files

--- docutron/src/components/Page.js
+++ docutron/src/components/Page.js
@@ -7,28 +7,8 @@ import { Link } from 'react-router-dom';
 /**
  * Internal Dependencies
  */
+import MarkdownContent from './MarkdownContent';
 import { getNextStory, getPreviousStory } from '../config';
-import markdown from '../markdown';
-import Tabs from './Tabs';
-
-function MarkdownContent( { content } ) {
-	const blocks = markdown( content );
-
-	return (
-		<div>
-			{ blocks.map( ( block, index ) => {
-				if ( block.type === 'raw' ) {
-					return <div key={ index } dangerouslySetInnerHTML={ { __html: block.content } } />;
-				}
-				if ( block.type === 'codetabs' ) {
-					return <Tabs key={ index } tabs={ block.tabs } />;
-				}
-
-				return null;
-			} ) }
-		</div>
-	);
-}
 
 class Page extends Component {
 	componentDidMount() {
--- docutron/src/components/Sidebar.js
+++ docutron/src/components/Sidebar.js
@@ -2,44 +2,12 @@
  * External Dependencies
  */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { find } from 'lodash';
 
 /**
  * Internal Dependencies
  */
-import { getStories, getChildren } from '../config';
-
-function MenuItem( { item, searchResults } ) {
-	const children = getChildren( item.id );
-	const isVisible = ! searchResults ||
-		!! find( searchResults, ( story ) => {
-			return story.id === item.id ||
-				( story.parents && story.parents.indexOf( item.id ) !== -1 );
-		} );
-
-	if ( ! isVisible ) {
-		return null;
-	}
-
-	return (
-		<li>
-			{ ! children.length && <Link to={ item.path }>{ item.title }</Link> }
-			{ !! children.length && (
-				<div className="expandable">
-					<Link to={ item.path }>{ item.title }</Link>
-				</div>
-			) }
-			{ !! children.length && (
-				<ul>
-					{ children.map( ( story, index ) => (
-						<MenuItem key={ index } item={ story } searchResults={ searchResults } />
-					) ) }
-				</ul>
-			) }
-		</li>
-	);
-}
+import MenuItem from './MenuItem';
+import { getStories } from '../config';
 
 class Sidebar extends Component {
 	constructor() {
