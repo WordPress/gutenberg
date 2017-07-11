027678bf84fd6a21c46ef199f8ad3226159dea9d
Caleb Burks <19caleb95@gmail.com>
2017-07-11 02:40:24 -0500

    Export InspectorControls

--- blocks/index.js
+++ blocks/index.js
@@ -17,4 +17,5 @@ export { default as AlignmentToolbar } from './alignment-toolbar';
 export { default as BlockControls } from './block-controls';
 export { default as BlockDescription } from './block-description';
 export { default as Editable } from './editable';
+export { default as InspectorControls } from './inspector-controls';
 export { default as MediaUploadButton } from './media-upload-button';
