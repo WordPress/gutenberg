c694339f412c5da6bf927aeb385fafa7a44a914a
Andrew Duthie <andrew@andrewduthie.com>
2017-07-13 12:37:50 -0400

    Add DropZone component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@
 export { default as Button } from './button';
 export { default as ClipboardButton } from './clipboard-button';
 export { default as Dashicon } from './dashicon';
+export { default as DropZone } from './drop-zone';
 export { default as FormToggle } from './form-toggle';
 export { default as FormTokenField } from './form-token-field';
 export { default as IconButton } from './icon-button';
