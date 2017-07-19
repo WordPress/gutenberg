be62409416336566ae6db268a33fd4556fa067b7
Adam Silverstein <adam@10up.com>
2017-07-19 10:35:23 -0400

    clean up imports for (empty) tests file

--- packages/hooks/src/test/index.test.js
+++ packages/hooks/src/test/index.test.js
@@ -1,4 +1,20 @@
 /**
- * Internal Dependencies
+ * Internal Dependencies.
  */
-import { doAction, applyFilters, addAction, addFilter, doingAction, doingFilter, didAction, didFilter, hasAction, hasFilter, removeAllActions, removeAllFilters, currentFilter } from '../';
+import {
+	doAction
+	applyFilters
+	addAction
+	addFilter
+	doingAction
+	doingFilter
+	didAction
+	didFilter
+	hasAction
+	hasFilter
+	removeAllActions
+	removeAllFilters
+	currentFilter
+} from '../';
+
+// @todo port over qunit tests from core.
