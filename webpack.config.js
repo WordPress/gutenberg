562cf377ccfe55d2d0bc3e2886c8d508a4dc80a6
Adam Silverstein <adam@10up.com>
2017-07-20 18:23:36 -0400

    Run core build JS thru babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ module.exports = {
   entry: './packages/hooks/src/wp-hooks.js',
   output: {
     filename: './packages/hooks/build/wp-core/wp-hooks.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
+
 }
