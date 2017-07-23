28e67acdce9de6f64561b302c172625ca0f4a6e4
John Blackbourn <johnbillion@git.wordpress.org>
2017-07-23 00:01:39 +0000

    Docs: Correct some inline docs for the Ajax test case.
    See #41017
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@41119 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/testcase-ajax.php
+++ tests/phpunit/includes/testcase-ajax.php
@@ -23,7 +23,7 @@ abstract class WP_Ajax_UnitTestCase extends WP_UnitTestCase {
 	protected $_last_response = '';
 
 	/**
-	 * List of ajax actions called via POST
+	 * List of ajax actions called via GET
 	 * @var array
 	 */
 	protected static $_core_actions_get = array(
@@ -38,7 +38,7 @@ abstract class WP_Ajax_UnitTestCase extends WP_UnitTestCase {
 	protected $_error_level = 0;
 
 	/**
-	 * List of ajax actions called via GET
+	 * List of ajax actions called via POST
 	 * @var array
 	 */
 	protected static $_core_actions_post = array(
@@ -79,7 +79,7 @@ abstract class WP_Ajax_UnitTestCase extends WP_UnitTestCase {
 
 	/**
 	 * Set up the test fixture.
-	 * Override wp_die(), pretend to be ajax, and suppres E_WARNINGs
+	 * Override wp_die(), pretend to be ajax, and suppress E_WARNINGs
 	 */
 	public function setUp() {
 		parent::setUp();
