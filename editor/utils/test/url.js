71bcda1635c56f90d669658d06b764b3869445ba
Weston Ruter <weston@xwp.co>
2017-07-23 22:39:27 -0700

    Reuse addQueryArgs function in getGutenbergURL and getWPAdminURL

--- editor/utils/test/url.js
+++ editor/utils/test/url.js
@@ -17,4 +17,20 @@ describe( 'addQueryArgs', () => {
 
 		expect( addQueryArgs( url, args ) ).toEqual( 'https://andalouses.com/beach?night=false&sun=true&sand=false' );
 	} );
+
+	it( 'should append args to an URL with query string and URL fragment', () => {
+		const url = 'https://andalouses.com/beach?night=false#foo=bar';
+		const args = { sun: 'true', sand: 'false' };
+
+		expect( addQueryArgs( url, args ) ).toEqual( 'https://andalouses.com/beach?night=false&sun=true&sand=false#foo=bar' );
+	} );
+} );
+
+describe( 'getWPAdminURL', () => {
+	it( 'should append args to an URL and account for fragment', () => {
+		const url = 'edit.php#content';
+		const args = { post_id: 123 };
+
+		expect( addQueryArgs( url, args ) ).toEqual( 'edit.php?post_id=123#content' );
+	} );
 } );
