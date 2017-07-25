4744de30bc68204a3e3109294ba2c37fc2a4bc68
Riad Benguella <benguella@gmail.com>
2017-07-25 10:36:50 +0100

    Editable: Prevent the link input from being cleared when we move the mouse

--- blocks/editable/index.js
+++ blocks/editable/index.js
@@ -340,7 +340,7 @@ export default class Editable extends Component {
 		const formats = {};
 		const link = find( parents, ( node ) => node.nodeName.toLowerCase() === 'a' );
 		if ( link ) {
-			formats.link = { value: link.getAttribute( 'href' ) || '', link };
+			formats.link = { value: link.getAttribute( 'href' ) || '', node: link };
 		}
 		const activeFormats = this.editor.formatter.matchAll( [	'bold', 'italic', 'strikethrough' ] );
 		activeFormats.forEach( ( activeFormat ) => formats[ activeFormat ] = true );
