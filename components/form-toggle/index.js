289e068c278ea1909911bfb28be73e296efc7815
Andrea Fercia <a.fercia@gmail.com>
2017-07-25 15:52:21 +0200

    Better focus style for the switch toggle controls.

--- components/form-toggle/index.js
+++ components/form-toggle/index.js
@@ -30,12 +30,13 @@ function FormToggle( { className, checked, id, onChange = noop, showHint = true 
 				checked={ checked }
 				onChange={ onChange }
 			/>
+			<span className="components-form-toggle__track"></span>
+			<span className="components-form-toggle__thumb"></span>
 			{ showHint &&
 				<span className="components-form-toggle__hint" aria-hidden>
 					{ checked ? __( 'On' ) : __( 'Off' ) }
 				</span>
 			}
-			<span className="components-form-toggle__thumb"></span>
 		</span>
 	);
 }
