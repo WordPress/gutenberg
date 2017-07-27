773b7726da39ad67cd3f39e92eb788c14cef1307
Nicola Heald <nicola@notnowlewis.com>
2017-07-27 16:40:40 +0100

    Move php build/install into its own script, add patches

---
+++ bin/phpunit-shim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PHP_VERSION=`php -v`
+
+if [[ ${PHP_VERSION:0:7} == "PHP 5.2" ]]; then 
+	phpunit-3.6 "$@" 
+elif [[ ${PHP_VERSION:0:7} == "PHP 5.3" ]]; then
+	phpunit-4.8 "$@"
+else
+	`which phpunit` "$@"
+fi
