2dfb411860810fbfd9dc66218780e731976111bf
Marin Atanasov <tyxla@abv.bg>
2017-07-31 15:26:35 +0300

    Add frontend scripts for the Categories block in dropdown mode

--- lib/blocks/categories.php
+++ lib/blocks/categories.php
@@ -27,9 +27,15 @@ function gutenberg_render_block_core_categories( $attributes ) {
 	);
 
 	if ( ! empty( $attributes['displayAsDropdown'] ) ) {
+		$id = 'wp-block-categories-' . wp_rand();
+		$args['id'] = $id;
 		$wrapper_markup = '<div class="%1$s">%2$s</div>';
 		$items_markup = wp_dropdown_categories( $args );
 		$type = 'dropdown';
+
+		if ( ! is_admin() ) {
+			$wrapper_markup .= gutenberg_build_dropdown_script_block_core_categories( $id );
+		}
 	} else {
 		$wrapper_markup = '<div class="%1$s"><ul>%2$s</ul></div>';
 		$items_markup = wp_list_categories( $args );
@@ -45,6 +51,33 @@ function gutenberg_render_block_core_categories( $attributes ) {
 	);
 
 	return $block_content;
+}
+
+/**
+ * Generates the inline script for a categories dropdown field.
+ *
+ * @param string $dropdown_id ID of the dropdown field.
+ *
+ * @return string Returns the dropdown onChange redirection script.
+ */
+function gutenberg_build_dropdown_script_block_core_categories( $dropdown_id ) {
+	ob_start();
+	?>
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+	(function() {
+		var dropdown = document.getElementById( '<?php echo esc_js( $dropdown_id ); ?>' );
+		function onCatChange() {
+			if ( dropdown.options[ dropdown.selectedIndex ].value > 0 ) {
+				location.href = "<?php echo home_url(); ?>/?cat=" + dropdown.options[ dropdown.selectedIndex ].value;
+			}
+		}
+		dropdown.onchange = onCatChange;
+	})();
+	/* ]]> */
+	</script>
+	<?php
+	return ob_get_clean();
 }
 
 register_block_type( 'core/categories', array(
