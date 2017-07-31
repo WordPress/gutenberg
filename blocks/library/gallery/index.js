550ef082a083a6ff3ec5c6733c18413d7d7e8b23
Marcus Kazmierczak <marcus@mkaz.com>
2017-07-31 14:21:14 -0700

    Pass linkto attribute to GalleryImage

--- blocks/library/gallery/index.js
+++ blocks/library/gallery/index.js
@@ -164,11 +164,11 @@ registerBlockType( 'core/gallery', {
 	},
 
 	save( { attributes } ) {
-		const { images, columns = defaultColumnsNumber( attributes ), align = 'none', imageCrop = true } = attributes;
+		const { images, columns = defaultColumnsNumber( attributes ), align = 'none', imageCrop = true, linkto = 'none' } = attributes;
 		return (
 			<div className={ `align${ align } columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` } >
 				{ images.map( ( img ) => (
-					<GalleryImage key={ img.url } img={ img } />
+					<GalleryImage key={ img.url } img={ img } linkto={ linkto } />
 				) ) }
 			</div>
 		);
