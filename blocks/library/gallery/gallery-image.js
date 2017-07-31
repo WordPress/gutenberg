fca483f0d9b3d688c8bcff45f7e8dce8691003e6
Marcus Kazmierczak <marcus@mkaz.com>
2017-07-31 14:21:32 -0700

    Add media link if linkto set

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -1,5 +1,17 @@
 
 export default function GalleryImage( props ) {
+	const linkto = props.linkto;
+
+	if ( linkto === 'media' ) {
+		return (
+			<figure className="blocks-gallery-image">
+				<a href={ props.img.url }>
+					<img src={ props.img.url } alt={ props.img.alt } />
+				</a>
+			</figure>
+		);
+	}
+
 	return (
 		<figure className="blocks-gallery-image">
 			<img src={ props.img.url } alt={ props.img.alt } />
