90942a7f67881100917728f0870530182d54f3a1
Adam Silverstein <adam@10up.com>
2017-08-01 09:18:53 -0400

    Add a centralized webpack folder based build for packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,46 @@
+const entryPointNames = [
+	'hooks',
+	'url',
+];
+
+const externals = {
+	react: 'React',
+	'react-dom': 'ReactDOM',
+	'react-dom/server': 'ReactDOMServer',
+	tinymce: 'tinymce',
+	moment: 'moment',
+};
+
+entryPointNames.forEach( entryPointName => {
+	externals[ entryPointName ] = {
+		'this': [ 'wp', entryPointName ],
+	};
+} );
+
+const config = entryPointNames.reduce( ( memo, entryPointName ) => {
+		memo[ entryPointName ] = './packages/' + entryPointName + '/src/index.js';
+		return memo;
+ }, {} );
+
+
 module.exports = {
-  entry: './packages/hooks/src/wp-hooks.js',
-  output: {
-    filename: './packages/hooks/wp-core/wp-hooks.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+	entry: config,
+	externals,
+	output: {
+		filename: 'build/[name]/index.js',
+		path: __dirname,
+		library: [ 'wp', '[name]' ],
+		libraryTarget: 'this',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 }
