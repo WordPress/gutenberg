9c1005410ff37ae25a36825c107198605dfbf7cc
Weston Ruter <westonruter@git.wordpress.org>
2017-08-02 05:34:32 +0000

    Customize: Introduce `settings_previewed` arg and getter on `WP_Customize_Manager` which controls whether `WP_Customize_Setting::preview()` should be called on settings.
    The `settings_previewed` property eliminates the need for the Customizer components from having to look at global `doing_ajax` state. This is in particular needed when saving settings, as some settings will short-circuit the update operation if they detect no changes are introduced. This is also needed for low-level integrations with the Customizer, such as in REST API endpoints under development.
    
    Props stubgo, westonruter, utkarshpatel for testing.
    See #38900.
    Fixes #39221.
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@41205 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/customize/manager.php
+++ tests/phpunit/tests/customize/manager.php
@@ -216,6 +216,20 @@ class Tests_WP_Customize_Manager extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test WP_Customize_Manager::settings_previewed().
+	 *
+	 * @ticket 39221
+	 * @covers WP_Customize_Manager::settings_previewed()
+	 */
+	function test_settings_previewed() {
+		$wp_customize = new WP_Customize_Manager( array( 'settings_previewed' => false ) );
+		$this->assertSame( false, $wp_customize->settings_previewed() );
+
+		$wp_customize = new WP_Customize_Manager();
+		$this->assertSame( true, $wp_customize->settings_previewed() );
+	}
+
+	/**
 	 * Test WP_Customize_Manager::changeset_uuid().
 	 *
 	 * @ticket 30937
@@ -1289,6 +1303,54 @@ class Tests_WP_Customize_Manager extends WP_UnitTestCase {
 		$this->assertEquals( 'Unfilteredevil', apply_filters( 'content_save_pre', 'Unfiltered<script>evil</script>' ) );
 		wp_publish_post( $changeset_post_id ); // @todo If wp_update_post() is used here, then kses will corrupt the post_content.
 		$this->assertEquals( 'Unfiltered<script>evil</script>', get_option( 'scratchpad' ) );
+	}
+
+	/**
+	 * Test saving settings by publishing a changeset outside of Customizer entirely.
+	 *
+	 * Widgets get their settings registered and previewed early in the admin,
+	 * so this ensures that the previewing is bypassed when in the context of
+	 * publishing
+	 *
+	 * @ticket 39221
+	 * @covers _wp_customize_publish_changeset()
+	 * @see WP_Customize_Widgets::schedule_customize_register()
+	 * @see WP_Customize_Widgets::customize_register()
+	 */
+	function test_wp_customize_publish_changeset() {
+		global $wp_customize;
+		$wp_customize = null;
+
+		// Set the admin current screen to cause WP_Customize_Widgets::schedule_customize_register() to do early setting registration.
+		set_current_screen( 'edit' );
+		$this->assertTrue( is_admin() );
+
+		$old_sidebars_widgets = get_option( 'sidebars_widgets' );
+		$new_sidebars_widgets = $old_sidebars_widgets;
+		$this->assertGreaterThan( 2, count( $new_sidebars_widgets['sidebar-1'] ) );
+		$new_sidebar_1 = array_reverse( $new_sidebars_widgets['sidebar-1'] );
+
+		$post_id = $this->factory()->post->create( array(
+			'post_type' => 'customize_changeset',
+			'post_status' => 'draft',
+			'post_name' => wp_generate_uuid4(),
+			'post_content' => wp_json_encode( array(
+				'sidebars_widgets[sidebar-1]' => array(
+					'value' => $new_sidebar_1,
+				),
+			) ),
+		) );
+
+		// Save the updated sidebar widgets into the options table by publishing the changeset.
+		wp_publish_post( $post_id );
+
+		// Make sure previewing filters were never added, since WP_Customize_Manager should be constructed with settings_previewed=false.
+		$this->assertFalse( has_filter( 'option_sidebars_widgets' ) );
+		$this->assertFalse( has_filter( 'default_option_sidebars_widgets' ) );
+
+		// Ensure that the value has actually been written to the DB.
+		$updated_sidebars_widgets = get_option( 'sidebars_widgets' );
+		$this->assertEquals( $new_sidebar_1, $updated_sidebars_widgets['sidebar-1'] );
 	}
 
 	/**
