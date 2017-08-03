16b882bbe4013af59e81f7569cbe0308f6087933
James Nylen <jnylen@gmail.com>
2017-08-03 09:47:31 -0400

    Update WordPress dependencies after #2172

--- editor/enable-tracking-prompt/index.js
+++ editor/enable-tracking-prompt/index.js
@@ -6,8 +6,8 @@ import { connect } from 'react-redux';
 /**
  * WordPress dependencies
  */
-import { __ } from 'i18n';
-import { Button } from 'components';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
 
 /**
  * Internal dependencies
