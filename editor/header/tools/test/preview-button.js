d1e639ac873a4005e9a7473297ad62701b500549
Andrew Duthie <andrew@andrewduthie.com>
2017-08-03 11:22:19 -0400

    Preivwe Button: Save post before navigating to preview

---
+++ editor/header/tools/test/preview-button.js
@@ -0,0 +1,86 @@
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { PreviewButton } from '../preview-button';
+
+describe( 'PreviewButton', () => {
+	describe( 'constructor()', () => {
+		it( 'should initialize with non-awaiting-save', () => {
+			const instance = new PreviewButton( {} );
+
+			expect( instance.state.isAwaitingSave ).toBe( false );
+		} );
+	} );
+
+	describe( 'getWindowTarget()', () => {
+		it( 'returns a string unique to the post id', () => {
+			const instance = new PreviewButton( {
+				postId: 1,
+			} );
+
+			expect( instance.getWindowTarget() ).toBe( 'wp-preview-1' );
+		} );
+	} );
+
+	describe( 'componentDidUpdate()', () => {
+		it( 'should change popup location if save finishes', () => {
+			const wrapper = shallow(
+				<PreviewButton
+					postId={ 1 }
+					link="https://wordpress.org/?p=1"
+					modified="2017-08-03T15:05:50" />,
+				{ lifecycleExperimental: true }
+			);
+			wrapper.instance().previewWindow = {};
+			wrapper.setState( { isAwaitingSave: true } );
+
+			wrapper.setProps( { modified: '2017-08-03T15:05:52' } );
+
+			expect(
+				wrapper.instance().previewWindow.location
+			).toBe( 'https://wordpress.org/?p=1' );
+		} );
+	} );
+
+	describe( 'saveForPreview()', () => {
+		it( 'should open a popup window', () => {
+			const autosave = jest.fn();
+			const preventDefault = jest.fn();
+			const windowOpen = window.open;
+			window.open = jest.fn();
+
+			const wrapper = shallow(
+				<PreviewButton
+					postId={ 1 }
+					autosave={ autosave } />
+			);
+
+			wrapper.simulate( 'click', { preventDefault } );
+
+			expect( autosave ).toHaveBeenCalled();
+			expect( preventDefault ).toHaveBeenCalled();
+			expect( wrapper.state( 'isAwaitingSave' ) ).toBe( true );
+			expect( window.open ).toHaveBeenCalledWith( 'about:blank', 'wp-preview-1' );
+
+			window.open = windowOpen;
+		} );
+	} );
+
+	describe( 'render()', () => {
+		it( 'should render a link', () => {
+			const wrapper = shallow(
+				<PreviewButton
+					postId={ 1 }
+					link="https://wordpress.org/?p=1" />
+			);
+
+			expect( wrapper.prop( 'href' ) ).toBe( 'https://wordpress.org/?p=1' );
+			expect( wrapper.prop( 'target' ) ).toBe( 'wp-preview-1' );
+		} );
+	} );
+} );
