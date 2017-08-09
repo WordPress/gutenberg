b075215932075b121fa8229044bc9ba60deaa58a
Marcus Kazmierczak <marcus@mkaz.com>
2017-08-09 14:58:32 -0700

    Checkpoint: files uploading, but not editable in modal

--- utils/fileupload.js
+++ utils/fileupload.js
@@ -1,5 +1,9 @@
 
-export default function fileUpload( files, setAttributes ) {
+export function fileUpload( files, setAttributes ) {
+	if ( files.length < 1 ) {
+		return;
+	}
+
 	const media = files[ 0 ];
 
 	// Only allow image uploads
@@ -28,5 +32,50 @@ export default function fileUpload( files, setAttributes ) {
 		// Reset to empty on failure.
 		// TODO: Better failure messaging
 		setAttributes( { url: null } );
+	} );
+}
+
+export function filesUpload( filesList, setAttributes ) {
+	const files = [ ...filesList ];
+	if ( files.length < 1 ) {
+		return;
+	}
+
+	const imgs = [];
+	files.forEach( ( media, idx ) => {
+		// only allow image uploads
+		if ( ! /^image\//.test( media.type ) ) {
+			return;
+		}
+
+		// set temporary URL to create image placeholder
+		const tempUrl = window.URL.createObjectURL( media );
+		const img = { url: tempUrl };
+		imgs.push( img );
+		setAttributes( { images: imgs } );
+
+		// create upload payload
+		const data = new window.FormData();
+		data.append( 'file', media );
+
+		new wp.api.models.Media().save( null, {
+			data: data,
+			contentType: false,
+		} ).done( ( savedMedia ) => {
+			img.url = savedMedia.source_url;
+			setAttributes( { images: [
+				...imgs.slice( 0, idx ),
+				img,
+				...imgs.slice( idx + 1 ),
+			] } );
+		} ).fail( () => {
+			// Reset to empty on failure.
+			// TODO: Better failure messaging
+			img.url = null;
+			setAttributes( { images: [
+				...imgs.slice( 0, idx ),
+				...imgs.slice( idx + 1 ),
+			] } );
+		} );
 	} );
 }
