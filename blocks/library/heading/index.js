f723e07424d8e740687f233632311917955cf42b
iseulde <ella@iseulde.com>
2017-08-14 18:20:06 +0200

    Fix paste attributes after #1902

--- blocks/library/heading/index.js
+++ blocks/library/heading/index.js
@@ -62,8 +62,14 @@ registerBlockType( 'core/heading', {
 				type: 'raw',
 				source: ( node ) => /H\d/.test( node.nodeName ),
 				attributes: {
-					content: children( 'h1,h2,h3,h4,h5,h6' ),
-					nodeName: prop( 'h1,h2,h3,h4,h5,h6', 'nodeName' ),
+					content: {
+						type: 'array',
+						source: children( 'h1,h2,h3,h4,h5,h6' ),
+					},
+					nodeName: {
+						type: 'string',
+						source: prop( 'h1,h2,h3,h4,h5,h6', 'nodeName' ),
+					},
 				},
 			},
 			{
