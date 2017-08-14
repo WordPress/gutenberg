867e4f8c0e196111b463c3b0f69568622a174493
iseulde <ella@iseulde.com>
2017-08-14 20:15:57 +0200

    Add a tests for the paste handler

--- blocks/api/test/paste.js
+++ blocks/api/test/paste.js
@@ -1,23 +1,26 @@
 /**
  * External dependencies
  */
-import { equal } from 'assert';
+import { equal, deepEqual } from 'assert';
 
 /**
  * Internal dependencies
  */
-import { normaliseToBlockLevelNodes } from '../paste';
+import paste, { normaliseToBlockLevelNodes } from '../paste';
+import { registerBlockType, unregisterBlockType, setUnknownTypeHandlerName } from '../registration';
+import { createBlock } from '../factory';
+import { children, prop } from '../source';
+
+function createNodes( HTML ) {
+	document.body.innerHTML = HTML;
+	return Array.from( document.body.childNodes );
+}
+
+function outerHTML( nodes ) {
+	return nodes.map( node => node.outerHTML ).join( '' );
+}
 
 describe( 'normaliseToBlockLevelNodes', () => {
-	function createNodes( HTML ) {
-		document.body.innerHTML = HTML;
-		return Array.from( document.body.childNodes );
-	}
-
-	function outerHTML( nodes ) {
-		return nodes.map( node => node.outerHTML ).join( '' );
-	}
-
 	function transform( HTML ) {
 		return outerHTML( normaliseToBlockLevelNodes( createNodes( HTML ) ) );
 	}
@@ -45,3 +48,59 @@ describe( 'normaliseToBlockLevelNodes', () => {
 	} );
 } );
 
+describe( 'paste', () => {
+	beforeAll( () => {
+		registerBlockType( 'test/small', {
+			category: 'common',
+			attributes: {
+				content: {
+					type: 'array',
+					source: children( 'small' ),
+				},
+			},
+			transforms: {
+				from: [
+					{
+						type: 'raw',
+						source: ( node ) => node.nodeName === 'SMALL',
+					},
+				],
+			},
+			save: () => {},
+		} );
+
+		registerBlockType( 'test/unknown', {
+			category: 'common',
+			attributes: {
+				content: {
+					type: 'string',
+					source: prop( 'innerHTML' ),
+				},
+			},
+			save: () => {},
+		} );
+
+		setUnknownTypeHandlerName( 'test/unknown' );
+	} );
+
+	it( 'should convert recognised pasted content', () => {
+		const pastedBlock = paste( createNodes( '<small>test</small>' ) )[ 0 ];
+		const block = createBlock( 'test/small', { content: [ 'test' ] } );
+
+		equal( pastedBlock.name, block.name );
+		deepEqual( pastedBlock.attributes, block.attributes );
+	} );
+
+	it( 'should handle unknown pasted content', () => {
+		const pastedBlock = paste( createNodes( '<big>test</big>' ) )[ 0 ];
+
+		equal( pastedBlock.name, 'test/unknown' );
+		equal( pastedBlock.attributes.content, '<big>test</big>' );
+	} );
+
+	afterAll( () => {
+		unregisterBlockType( 'test/small' );
+		unregisterBlockType( 'test/unknown' );
+		setUnknownTypeHandlerName( undefined );
+	} );
+} );
