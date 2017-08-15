cb4e16c16fe47880ff7380364c5955bef088c22a
Stephen Edgar <stephen@netweb.com.au>
2017-08-16 09:43:52 +1000

    Use `andalouses.example`

--- packages/url/src/test/index.test.js
+++ packages/url/src/test/index.test.js
@@ -5,23 +5,23 @@ import { addQueryArgs } from '../';
 
 describe( 'addQueryArgs', () => {
 	test( 'should append args to an URL without query string', () => {
-		const url = 'https://example.com/beach';
+		const url = 'https://andalouses.example/beach';
 		const args = { sun: 'true', sand: 'false' };
 
-		expect( addQueryArgs( url, args ) ).toBe( 'https://example.com/beach?sun=true&sand=false' );
+		expect( addQueryArgs( url, args ) ).toBe( 'https://andalouses.example/beach?sun=true&sand=false' );
 	} );
 
 	test( 'should append args to an URL with query string', () => {
-		const url = 'https://example.com/beach?night=false';
+		const url = 'https://andalouses.example/beach?night=false';
 		const args = { sun: 'true', sand: 'false' };
 
-		expect( addQueryArgs( url, args ) ).toBe( 'https://example.com/beach?night=false&sun=true&sand=false' );
+		expect( addQueryArgs( url, args ) ).toBe( 'https://andalouses.example/beach?night=false&sun=true&sand=false' );
 	} );
 
 	test( 'should update args to an URL with conflicting query string', () => {
-		const url = 'https://example.com/beach?night=false&sun=false&sand=true';
+		const url = 'https://andalouses.example/beach?night=false&sun=false&sand=true';
 		const args = { sun: 'true', sand: 'false' };
 
-		expect( addQueryArgs( url, args ) ).toBe( 'https://example.com/beach?night=false&sun=true&sand=false' );
+		expect( addQueryArgs( url, args ) ).toBe( 'https://andalouses.example/beach?night=false&sun=true&sand=false' );
 	} );
 } );
