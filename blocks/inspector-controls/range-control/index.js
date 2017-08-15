cd51321daeed35e51c8ac2e53a9e010c18b562c7
Riad Benguella <benguella@gmail.com>
2017-08-15 10:21:20 +0100

    Inspector Controls: Adding help support to all inspector controls

--- blocks/inspector-controls/range-control/index.js
+++ blocks/inspector-controls/range-control/index.js
@@ -9,12 +9,12 @@ import { Dashicon, withInstanceId } from '@wordpress/components';
 import BaseControl from './../base-control';
 import './style.scss';
 
-function RangeControl( { label, value, instanceId, onChange, beforeIcon, afterIcon, ...props } ) {
+function RangeControl( { label, value, instanceId, onChange, beforeIcon, afterIcon, help, ...props } ) {
 	const id = 'inspector-range-control-' + instanceId;
 	const onChangeValue = ( event ) => onChange( Number( event.target.value ) );
 
 	return (
-		<BaseControl label={ label } id={ id } className="blocks-range-control">
+		<BaseControl label={ label } id={ id } help={ help } className="blocks-range-control">
 			{ beforeIcon && <Dashicon icon={ beforeIcon } size={ 20 } /> }
 			<input
 				className="blocks-range-control__slider"
@@ -22,6 +22,7 @@ function RangeControl( { label, value, instanceId, onChange, beforeIcon, afterIc
 				type="range"
 				value={ value }
 				onChange={ onChangeValue }
+				aria-describedby={ !! help ? id + '__help' : undefined }
 				{ ...props } />
 			{ afterIcon && <Dashicon icon={ afterIcon } /> }
 			<input
