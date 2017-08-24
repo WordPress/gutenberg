24f4ef5f2a1054cfe515aaca4698fab074f49ca4
Nicola Heald <nicola@notnowlewis.com>
2017-08-24 16:09:36 +0100

    Allow specifying url for audio block (#2521)
    Adds an edit component that deals with storing and verifying typed URLs, before they get saved to the block's attributes.
    
    Thanks @karmatosed for the CSS fixes.

--- blocks/library/audio/index.js
+++ blocks/library/audio/index.js
@@ -6,7 +6,8 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Dashicon, Placeholder, Toolbar } from '@wordpress/components';
+import { Button, Dashicon, Placeholder, Toolbar } from '@wordpress/components';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -43,67 +44,108 @@ registerBlockType( 'core/audio', {
 		}
 	},
 
-	edit( { attributes, setAttributes, className, focus } ) {
-		const { align, src } = attributes;
-		const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
-		const onSelectAudio = ( media ) => {
-			if ( media && media.url ) {
-				setAttributes( { src: media.url } );
-			}
-		};
-		const controls = (
-			focus && (
-				<BlockControls key="controls">
-					<BlockAlignmentToolbar
-						value={ align }
-						onChange={ updateAlignment }
-					/>
-					<Toolbar>
-						<li>
-							<MediaUploadButton
-								buttonProps={ {
-									className: 'components-icon-button components-toolbar__control',
-									'aria-label': __( 'Edit audio' ),
-								} }
-								onSelect={ onSelectAudio }
-								type="audio"
-								value={ src }
-							>
-								<Dashicon icon="edit" />
-							</MediaUploadButton>
-						</li>
-					</Toolbar>
-				</BlockControls>
-			)
-		);
-
-		if ( ! src ) {
-			return [
-				<Placeholder
-					key="placeholder"
-					icon="media-audio"
-					label={ __( 'Audio' ) }
-					instructions={ __( 'Select an audio file from your library, or upload a new one:' ) }
-					className={ className }>
-					<MediaUploadButton
-						buttonProps={ { isLarge: true } }
-						onSelect={ onSelectAudio }
-						type="audio"
-					>
-						{ __( 'Insert from Media Library' ) }
-					</MediaUploadButton>
-				</Placeholder>,
-			];
+	edit: class extends Component {
+		constructor( { className } ) {
+			super( ...arguments );
+			// edit component has its own src in the state so it can be edited
+			// without setting the actual value outside of the edit UI
+			this.state = {
+				editing: ! this.props.attributes.src,
+				src: this.props.attributes.src,
+				className,
+			};
 		}
+		render() {
+			const { align } = this.props.attributes;
+			const { setAttributes, focus } = this.props;
+			const { editing, className, src } = this.state;
+			const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
+			const switchToEditing = () => {
+				this.setState( { editing: true } );
+			};
+			const onSelectAudio = ( media ) => {
+				if ( media && media.url ) {
+					// sets the block's attribure and updates the edit component from the
+					// selected media, then switches off the editing UI
+					setAttributes( { src: media.url } );
+					this.setState( { src: media.url, editing: false } );
+				}
+			};
+			const onSelectUrl = ( event ) => {
+				event.preventDefault();
+				if ( src ) {
+					// set the block's src from the edit component's state, and switch off the editing UI
+					setAttributes( { src } );
+					this.setState( { editing: false } );
+				}
+				return false;
+			};
+			const controls = (
+				focus && (
+					<BlockControls key="controls">
+						<BlockAlignmentToolbar
+							value={ align }
+							onChange={ updateAlignment }
+						/>
+						<Toolbar>
+							<li>
+								<Button
+									buttonProps={ {
+										className: 'components-icon-button components-toolbar__control',
+										'aria-label': __( 'Edit audio' ),
+									} }
+									type="audio"
+									onClick={ switchToEditing }
+								>
+									<Dashicon icon="edit" />
+								</Button>
+							</li>
+						</Toolbar>
+					</BlockControls>
+				)
+			);
 
-		/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
-		return [
-			controls,
-			<div key="audio">
-				<audio controls="controls" src={ src } />
-			</div>,
-		];
-		/* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+			if ( editing ) {
+				return [
+					<Placeholder
+						key="placeholder"
+						icon="media-audio"
+						label={ __( 'Audio' ) }
+						instructions={ __( 'Select an audio file from your library, or upload a new one:' ) }
+						className={ className }>
+						<form onSubmit={ onSelectUrl }>
+							<input
+								type="url"
+								className="components-placeholder__input"
+								placeholder={ __( 'Enter URL of audio file here…' ) }
+								onChange={ event => this.setState( { src: event.target.value } ) }
+								value={ src || '' } />
+							<Button
+								isLarge
+								type="submit">
+								{ __( 'Use URL' ) }
+							</Button>
+						</form>
+						<MediaUploadButton
+							buttonProps={ { isLarge: true } }
+							onSelect={ onSelectAudio }
+							type="audio"
+						>
+							{ __( 'Insert from Media Library' ) }
+						</MediaUploadButton>
+					</Placeholder>,
+				];
+			}
+
+			/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+			return [
+				controls,
+				<div key="audio">
+					<audio controls="controls" src={ src } />
+				</div>,
+			];
+			/* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+		}
 	},
 
 	save( { attributes } ) {
