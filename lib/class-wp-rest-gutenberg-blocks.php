6f73d88eb801f41350f6ff42ae8377be948a1218
Adam Silverstein <adam@10up.com>
2017-09-02 00:27:05 -0400

    remove logging, add inline docs, clean up

--- lib/class-wp-rest-gutenberg-blocks.php
+++ lib/class-wp-rest-gutenberg-blocks.php
@@ -5,22 +5,6 @@
 class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 
 	/**
-	 * Post type.
-	 *
-	 * @since 1.1.0
-	 * @var string
-	 */
-	protected $block_type;
-
-	/**
-	 * Instance of a post meta fields object.
-	 *
-	 * @since 1.1.0
-	 * @var WP_REST_Post_Meta_Fields
-	 */
-	protected $meta;
-
-	/**
 	 * Constructor.
 	 *
 	 * @since 1.1.0
@@ -32,7 +16,7 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 	}
 
 	/**
-	 * Registers the routes for the objects of the controller.
+	 * Register the block routes.
 	 *
 	 * @since 1.1.0
 	 *
@@ -40,6 +24,7 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 	 */
 	public function register_routes() {
 
+		// Add a post collection block route.
 		register_rest_route( $this->namespace, '/' . $this->rest_base, array(
 			array(
 				'methods'             => WP_REST_Server::READABLE,
@@ -55,7 +40,7 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 			'context'  => $this->get_context_param( array( 'default' => 'view' ) ),
 		);
 
-
+		// Add a single post block route.
 		register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
 			'args' => array(
 				'id' => array(
@@ -74,7 +59,7 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 	}
 
 	/**
-	 * Checks if a given request has access to read posts.
+	 * Checks if a given request has access to read posts and their blocks.
 	 *
 	 * @since 1.1.0
 	 *
@@ -88,7 +73,7 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 	}
 
 	/**
-	 * Retrieves a collection of posts.
+	 * Retrieves a collection of posts blocks.
 	 *
 	 * @since 1.1.0
 	 *
@@ -97,33 +82,15 @@ class WP_Gutenberg_Block_Controller extends WP_REST_Controller {
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
 	public function get_items( $request ) {
-error_log('get_items');
+
 		// Retrieve the list of registered collection query parameters.
 		$registered = $this->get_collection_params();
 		$args = array();
 
-		/*
-		 * This array defines mappings between public API query parameters whose
-		 * values are accepted as-passed, and their internal WP_Query parameter
-		 * name equivalents (some are the same). Only values which are also
-		 * present in $registered will be set.
-		 */
-		$parameter_mappings = array();
-
-		/*
-		 * For each known parameter which is both registered and present in the request,
-		 * set the parameter's value on the query $args.
-		 */
-		foreach ( $parameter_mappings as $api_param => $wp_param ) {
-			if ( isset( $registered[ $api_param ], $request[ $api_param ] ) ) {
-				$args[ $wp_param ] = $request[ $api_param ];
-			}
-		}
-
 		/**
 		 * Filters the query arguments for a request.
 		 *
-		 * Enables adding extra arguments or setting defaults for a post collection request.
+		 * Enables adding extra arguments or setting defaults for a post blocks collection request.
 		 *
 		 * @since 1.1.0
 		 *
@@ -141,6 +108,8 @@ error_log('get_items');
 			if ( ! $this->check_read_permission( $post ) ) {
 				continue;
 			}
+
+			// Parse the blocks from post content.
 			$content = $post->post_content;
 			$data    = gutenberg_parse_blocks( $content );
 			$posts[] = $data;
@@ -156,6 +125,7 @@ error_log('get_items');
 	 * @since 1.1.0
 	 *
 	 * @param int $id Supplied ID.
+	 *
 	 * @return WP_Post|WP_Error Post object if ID is valid, WP_Error otherwise.
 	 */
 	protected function get_post( $id ) {
@@ -174,15 +144,16 @@ error_log('get_items');
 	}
 
 	/**
-	 * Checks if a given request has access to read a post.
+	 * Checks if a given request has access to read a post and its blocks.
 	 *
 	 * @since 1.1.0
 	 *
 	 * @param WP_REST_Request $request Full details about the request.
+	 *
 	 * @return bool|WP_Error True if the request has read access for the item, WP_Error object otherwise.
 	 */
 	public function get_item_permissions_check( $request ) {
-		$post = $this->get_post( $request['id'] );
+		$post = $this->get_post( (int) $request['id'] );
 		if ( is_wp_error( $post ) ) {
 			return $post;
 		}
@@ -191,7 +162,8 @@ error_log('get_items');
 			return $this->check_read_permission( $post );
 		}
 
-		return true;
+		$error = new WP_Error( 'rest_post_invalid', __( 'Invalid post.' ), array( 'status' => 404 ) )
+		return $error;
 	}
 
 	/**
@@ -200,6 +172,7 @@ error_log('get_items');
 	 * @since 1.1.0
 	 *
 	 * @param WP_REST_Request $request Full details about the request.
+	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
 	public function get_item( $request ) {
@@ -210,10 +183,6 @@ error_log('get_items');
 
 		$data     = $this->prepare_item_for_response( $post, $request );
 		$response = rest_ensure_response( $data );
-
-		if ( is_post_type_viewable( get_post_type_object( $post->post_type ) ) ) {
-			$response->link_header( 'alternate',  get_permalink( $post->ID ), array( 'type' => 'text/html' ) );
-		}
 
 		return $response;
 	}
@@ -226,12 +195,14 @@ error_log('get_items');
 	 *
 	 * @param array           $prepared_args Optional. Prepared WP_Query arguments. Default empty array.
 	 * @param WP_REST_Request $request       Optional. Full details about the request.
+	 *
 	 * @return array Items query arguments.
 	 */
 	protected function prepare_items_query( $prepared_args = array(), $request = null ) {
 		$query_args = array();
 
 		foreach ( $prepared_args as $key => $value ) {
+
 			/**
 			 * Filters the query_vars used in get_items() for the constructed query.
 			 *
@@ -244,14 +215,6 @@ error_log('get_items');
 			$query_args[ $key ] = apply_filters( "rest_query_var-{$key}", $value );
 		}
 
-		// Map to proper WP_Query orderby param.
-		if ( isset( $query_args['orderby'] ) && isset( $request['orderby'] ) ) {
-			$orderby_mappings = array(
-				'id'      => 'ID',
-			);
-
-		}
-
 		return $query_args;
 	}
 
@@ -261,6 +224,7 @@ error_log('get_items');
 	 * @since 1.1.0
 	 *
 	 * @param object|string $post_type Post type name or object.
+	 *
 	 * @return bool Whether the post type is allowed in REST.
 	 */
 	protected function check_is_post_type_allowed( $post_type ) {
@@ -281,6 +245,7 @@ error_log('get_items');
 	 * @since 1.1.0
 	 *
 	 * @param object $post Post object.
+	 *
 	 * @return bool Whether the post can be read.
 	 */
 	public function check_read_permission( $post ) {
@@ -309,6 +274,7 @@ error_log('get_items');
 	 *
 	 * @param WP_Post         $post    Post object.
 	 * @param WP_REST_Request $request Request object.
+	 *
 	 * @return WP_REST_Response Response object.
 	 */
 	public function prepare_item_for_response( $post, $request ) {
@@ -325,6 +291,7 @@ error_log('get_items');
 			$data['id'] = $post->ID;
 		}
 
+		// Parse the blocks from post content.
 		$content = $post->post_content;
 		$data    = gutenberg_parse_blocks( $content );
 
@@ -333,8 +300,6 @@ error_log('get_items');
 
 		/**
 		 * Filters the post data for a response.
-		 *
-		 * The dynamic portion of the hook name, `$this->post_type`, refers to the post type slug.
 		 *
 		 * @since 1.1.0
 		 *
@@ -358,45 +323,33 @@ error_log('get_items');
 			'$schema'    => 'http://json-schema.org/schema#',
 			'title'      => 'blocks',
 			'type'       => 'object',
+
 			// Base properties for every Block.
 			'properties' => array(
-				'name'            => array(
+				'blockName'            => array(
 					'description' => __( "The name of the block." ),
 					'type'        => 'string',
-					'context'     => array( 'view', 'edit', 'embed' ),
-				),
-
-				'id'              => array(
-					'description' => __( 'The id of the post containing the block.' ),
-					'type'        => 'integer',
-					'context'     => array( 'view', 'edit', 'embed' ),
+					'context'     => array( 'view' ),
 					'readonly'    => true,
 				),
-				'slug'            => array(
-					'description' => __( 'An alphanumeric identifier for the object unique to its type.' ),
+				'attrs'              => array(
+					'description' => __( 'The block data attributes.' ),
+					'type'        => 'array',
+					'context'     => array( 'view' ),
+					'readonly'    => true,
+				),
+				'rawContent'            => array(
+					'description' => __( 'The raw block content.' ),
 					'type'        => 'string',
-					'context'     => array( 'view', 'edit', 'embed' ),
+					'context'     => array( 'view' ),
 					'arg_options' => array(
 						'sanitize_callback' => array( $this, 'sanitize_slug' ),
 					),
-				),
-				'data'          => array(
-					'description' => __( 'The data underlying the block.' ),
-					'type'        => 'string',
-					'enum'        => array_keys( get_post_stati( array( 'internal' => false ) ) ),
-					'context'     => array( 'view', 'edit' ),
-				),
-				'rendered'            => array(
-					'description' => __( 'The rendered block content.' ),
-					'type'        => 'string',
-					'context'     => array( 'view', 'edit', 'embed' ),
-					'readonly'    => true,
 				),
 			),
 		);
 
 		$post_type_obj = get_post_type_object( $this->post_type );
-
 
 		return $this->add_additional_fields_schema( $schema );
 	}
@@ -429,5 +382,4 @@ error_log('get_items');
 		 */
 		return apply_filters( "rest_blocks_collection_params", $query_params, $post_type );
 	}
-
 }
