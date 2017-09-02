488690d99a581a7dd8d826b437f7804b447d28cb
Adam Silverstein <adam@10up.com>
2017-09-02 01:18:40 -0400

    restore register.php

--- lib/register.php
+++ lib/register.php
@@ -73,8 +73,15 @@ function gutenberg_add_edit_links_filters() {
 	add_filter( 'page_row_actions', 'gutenberg_add_edit_links', 10, 2 );
 	// For non-hierarchical post types.
 	add_filter( 'post_row_actions', 'gutenberg_add_edit_links', 10, 2 );
+
 }
 add_action( 'admin_init', 'gutenberg_add_edit_links_filters' );
+
+function load_blocks_rest_api() {
+	$controller = new WP_Gutenberg_Block_Controller;
+	$controller->register_routes();
+}
+add_action( 'init', 'load_blocks_rest_api' );
 
 /**
  * Registers additional links in the post/page screens to edit any post/page in
@@ -90,9 +97,8 @@ add_action( 'admin_init', 'gutenberg_add_edit_links_filters' );
 function gutenberg_add_edit_links( $actions, $post ) {
 	$can_edit_post = current_user_can( 'edit_post', $post->ID );
 	$title = _draft_or_post_title( $post->ID );
-	$post_type = get_post_type( $post );
 
-	if ( $can_edit_post && 'trash' !== $post->post_status && apply_filters( 'gutenberg_add_edit_link_for_post_type', true, $post_type, $post ) ) {
+	if ( $can_edit_post && 'trash' !== $post->post_status ) {
 		// Build the Gutenberg edit action. See also: WP_Posts_List_Table::handle_row_actions().
 		$gutenberg_url = menu_page_url( 'gutenberg', false );
 		$gutenberg_action = sprintf(
