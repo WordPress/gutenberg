40accca5ed53882a7a901340b27b92cf715d82fe
Riad Benguella <benguella@gmail.com>
2017-09-04 09:39:53 +0100

    Chrome: Hide post Author if the user don't have the right capabilities

--- editor/sidebar/post-author/index.js
+++ editor/sidebar/post-author/index.js
@@ -44,12 +44,12 @@ export class PostAuthor extends Component {
 	}
 
 	render() {
+		const { postAuthor, instanceId, user } = this.props;
 		const authors = this.getAuthors();
-		if ( authors.length < 2 ) {
+		if ( ! user.data || ! user.data.capabilities.publish_posts || authors.length < 2 ) {
 			return null;
 		}
 
-		const { postAuthor, instanceId } = this.props;
 		const selectId = 'post-author-selector-' + instanceId;
 
 		// Disable reason: A select with an onchange throws a warning
@@ -90,6 +90,7 @@ const applyConnect = connect(
 const applyWithAPIData = withAPIData( () => {
 	return {
 		users: '/wp/v2/users?context=edit&per_page=100',
+		user: '/wp/v2/users/me?context=edit',
 	};
 } );
 
