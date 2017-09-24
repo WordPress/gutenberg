c739a2012a29cd300c7cbfdd7dd17a56f33a47d3
Matías Ventura <mv@matiasventura.com>
2017-09-24 20:45:07 +0200

    Simplify title attr.

--- editor/document-outline/item.js
+++ editor/document-outline/item.js
@@ -26,7 +26,7 @@ const TableOfContentsItem = ( {
 		<button
 			className="document-outline__button"
 			onClick={ onClick }
-			title={ `"Heading Content here..." ${ __( '(click to focus this heading block)' ) }` }
+			title={ __( 'Click to focus this heading' ) }
 		>
 			<span className="document-outline__emdash" aria-hidden="true"></span>
 			<strong className="document-outline__level">
