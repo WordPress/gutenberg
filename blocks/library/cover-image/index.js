728097398c477ebdcccf2a9caa09e2f0d1276e79
Miguel Fonseca <miguelcsf@gmail.com>
2017-09-29 17:12:31 +0100

    Cover Image: Classes should be fraction of 100

--- blocks/library/cover-image/index.js
+++ blocks/library/cover-image/index.js
@@ -193,5 +193,5 @@ registerBlockType( 'core/cover-image', {
 function dimRatioToClass( ratio ) {
 	return ( ratio === 0 || ratio === 50 )
 		? null
-		: 'has-background-dim-' + Math.round( ratio / 10 );
+		: 'has-background-dim-' + ( 10 * Math.round( ratio / 10 ) );
 }
