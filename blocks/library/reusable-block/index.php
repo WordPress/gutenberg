f587eedaed3266dc574c06dd815f565418f3c525
Robert Anderson <robert@noisysocks.com>
2017-10-04 10:54:17 +1100

    Remove useless `} else {`

--- blocks/library/reusable-block/index.php
+++ blocks/library/reusable-block/index.php
@@ -38,9 +38,9 @@ function gutenberg_render_block_core_reusable_block( $attributes ) {
 	$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
 	if ( $block_type ) {
 		return $block_type->render( $block_attributes, $block_content );
-	} else {
-		return $block_content;
 	}
+
+	return $block_content;
 }
 
 register_block_type( 'core/reusable-block', array(
