3fc640bebd1ba7558edd464453f846cf95e7dc20
Riad Benguella <benguella@gmail.com>
2017-10-06 11:30:39 +0100

    Chrome: Always use the publish dropdown

--- editor/header/publish-with-dropdown/index.js
+++ editor/header/publish-with-dropdown/index.js
@@ -6,61 +6,42 @@ import { connect } from 'react-redux';
 /**
  * WordPress Dependencies
  */
-import { Component } from '@wordpress/element';
-import { Popover, IconButton } from '@wordpress/components';
+import { Dropdown, Button, Dashicon } from '@wordpress/components';
 
 /**
  * Internal Dependencies
  */
 import './style.scss';
 import PublishDropdown from '../publish-dropdown';
-import PublishButton from '../publish-button';
+import PublishButtonLabel from '../publish-button/label';
 import {
 	isSavingPost,
 	isEditedPostSaveable,
 	isEditedPostPublishable,
 } from '../../selectors';
 
-class PublishWithDropdown extends Component {
-	constructor() {
-		super( ...arguments );
-		this.toggleDropdown = this.toggleDropdown.bind( this );
-		this.closeDropdown = this.closeDropdown.bind( this );
-		this.state = {
-			opened: false,
-		};
-	}
+function PublishWithDropdown( { isSaving, isPublishable, isSaveable } ) {
+	const isButtonEnabled = ! isSaving && isPublishable && isSaveable;
 
-	toggleDropdown( event ) {
-		event.stopPropagation();
-		this.setState( ( state ) => ( { opened: ! state.opened } ) );
-	}
-
-	closeDropdown() {
-		this.setState( { opened: false } );
-	}
-
-	render() {
-		const { opened } = this.state;
-		const { isSaving, isPublishable, isSaveable } = this.props;
-		const isButtonEnabled = ! isSaving && isPublishable && isSaveable;
-
-		return (
-			<div className="editor-publish-with-dropdown">
-				<PublishButton />
-				<div className="editor-publish-with-dropdown__arrow-container">
-					<IconButton
-						icon="arrow-down"
-						onClick={ this.toggleDropdown }
-						disabled={ ! isButtonEnabled }
-					/>
-					<Popover isOpen={ opened }>
-						<PublishDropdown onSubmit={ this.closeDropdown } />
-					</Popover>
-				</div>
-			</div>
-		);
-	}
+	return (
+		<Dropdown
+			position="bottom left"
+			className="editor-publish-with-dropdown"
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<Button
+					className="editor-publish-with-dropdown__button"
+					isPrimary
+					onClick={ onToggle }
+					aria-expanded={ isOpen }
+					disabled={ ! isButtonEnabled }
+				>
+					<PublishButtonLabel />
+					<Dashicon icon="arrow-down" />
+				</Button>
+			) }
+			renderContent={ ( { onClose } ) => <PublishDropdown onSubmit={ onClose } /> }
+		/>
+	);
 }
 
 export default connect(
