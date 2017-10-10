605581cdd932cc651d4825ee5acb893ce8cc8302
Sören Wrede <soerenwrede@gmail.com>
2017-10-10 23:42:02 +0200

    Add default icon

--- blocks/block-icon/index.js
+++ blocks/block-icon/index.js
@@ -5,13 +5,18 @@ import { Dashicon } from '@wordpress/components';
 import { createElement, Component } from '@wordpress/element';
 
 export default function BlockIcon( { icon } ) {
+	if ( ! icon ) {
+		return <Dashicon icon={ 'wordpress' } />;
+	}
+
 	if ( 'string' === typeof icon ) {
 		return <Dashicon icon={ icon } />;
-	} else if ( 'function' === typeof icon ) {
+	}
+
+	if ( 'function' === typeof icon ) {
 		if ( icon.prototype instanceof Component ) {
 			return createElement( icon );
 		}
-
 		return icon();
 	}
 
