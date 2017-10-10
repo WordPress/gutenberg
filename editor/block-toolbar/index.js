5fc8f97cc1607a22a903dcd7be4463f843b8b2bf
Riad Benguella <benguella@gmail.com>
2017-10-10 16:26:50 +0100

    Accessibility: Keyboard shortcuts to navigate to/from/in the block's toolbar

--- editor/block-toolbar/index.js
+++ editor/block-toolbar/index.js
@@ -11,6 +11,7 @@ import classnames from 'classnames';
 import { IconButton, Toolbar } from '@wordpress/components';
 import { Component, Children } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { focus, keycodes } from '@wordpress/utils';
 
 /**
  * Internal Dependencies
@@ -19,6 +20,11 @@ import './style.scss';
 import BlockSwitcher from '../block-switcher';
 import BlockMover from '../block-mover';
 import BlockRightMenu from '../block-settings-menu';
+
+/**
+ * Module Constants
+ */
+const { LEFT, RIGHT, ESCAPE, ALT } = keycodes;
 
 function FirstChild( { children } ) {
 	const childrenArray = Children.toArray( children );
@@ -29,15 +35,69 @@ class BlockToolbar extends Component {
 	constructor() {
 		super( ...arguments );
 		this.toggleMobileControls = this.toggleMobileControls.bind( this );
+		this.bindNode = this.bindNode.bind( this );
+		this.onKeyUp = this.onKeyUp.bind( this );
+		this.onKeyDown = this.onKeyDown.bind( this );
 		this.state = {
 			showMobileControls: false,
 		};
+	}
+
+	componentDidMount() {
+		document.addEventListener( 'keyup', this.onKeyUp );
+	}
+
+	componentWillUnmout() {
+		document.removeEventListener( 'keyup', this.onKeyUp );
+	}
+
+	bindNode( ref ) {
+		this.toolbar = ref;
 	}
 
 	toggleMobileControls() {
 		this.setState( ( state ) => ( {
 			showMobileControls: ! state.showMobileControls,
 		} ) );
+	}
+
+	onKeyDown( event ) {
+		// This is required to avoid messing up with the WritingFlow navigation
+		event.stopPropagation();
+	}
+
+	onKeyUp( event ) {
+		// Is there a better way to focus the selected block
+		const selectedBlock = document.querySelector( '.editor-visual-editor__block.is-selected' );
+		const tabbables = focus.tabbable.find( this.toolbar );
+		const indexOfTabbable = tabbables.indexOf( document.activeElement );
+
+		if ( event.keyCode === ALT ) {
+			if ( tabbables.length ) {
+				tabbables[ 0 ].focus();
+			}
+			return;
+		}
+
+		switch ( event.keyCode ) {
+			case ESCAPE:
+				if ( indexOfTabbable !== -1 && selectedBlock ) {
+					selectedBlock.focus();
+				}
+				break;
+			case LEFT:
+				if ( indexOfTabbable > 0 ) {
+					tabbables[ indexOfTabbable - 1 ].focus();
+				}
+				event.stopPropagation();
+				break;
+			case RIGHT:
+				if ( indexOfTabbable !== -1 && indexOfTabbable !== tabbables.length - 1 ) {
+					tabbables[ indexOfTabbable + 1 ].focus();
+				}
+				event.stopPropagation();
+				break;
+		}
 	}
 
 	render() {
@@ -57,7 +117,7 @@ class BlockToolbar extends Component {
 				transitionLeave={ false }
 				component={ FirstChild }
 			>
-				<div className={ toolbarClassname }>
+				<div className={ toolbarClassname } ref={ this.bindNode } onKeyDown={ this.onKeyDown }>
 					<div className="editor-block-toolbar__group">
 						{ ! showMobileControls && [
 							<BlockSwitcher key="switcher" uid={ uid } />,
