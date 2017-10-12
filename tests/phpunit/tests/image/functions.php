5fac6cc075b5568dd0ab99461c7c717bfb0477da
Gary Pendergast <pento@git.wordpress.org>
2017-10-12 04:27:22 +0000

    Build/Test Tools: Revert [41784].
    [41784] was a workaround for a bug in PHPUnit 6.4. PHPUnit 6.4.1 has since been released, which includes a fix for this.
    
    Fixes #42124.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/trunk@41841 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/image/functions.php
+++ tests/phpunit/tests/image/functions.php
@@ -266,10 +266,10 @@ class Tests_Image_Functions extends WP_UnitTestCase {
 
 		// First, test with deprecated wp_load_image function
 		$editor1 = wp_load_image( DIR_TESTDATA );
-		$this->assertInternalType( 'string', $editor1 );
+		$this->assertNotInternalType( 'resource', $editor1 );
 
 		$editor2 = wp_get_image_editor( DIR_TESTDATA );
-		$this->assertWPError( $editor2 );
+		$this->assertNotInternalType( 'resource', $editor2 );
 
 		// Then, test with editors.
 		$classes = array('WP_Image_Editor_GD', 'WP_Image_Editor_Imagick');
