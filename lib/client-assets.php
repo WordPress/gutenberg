851cd18b8df74eaf4f12dfeec758cf09539291dc
Robert Anderson <robert@noisysocks.com>
2017-10-13 17:14:14 +1100

    Add reusable blocks effects
    Adds the effects necessary for supporting reusable blocks. There are 4:
    
    - FETCH_REUSABLE_BLOCKS: Loads reusable blocks from the API and inserts
      them into the store.
    - SAVE_REUSABLE_BLOCK: Persists a reusable block that's in the store to
      the API.
    - MAKE_BLOCK_STATIC: Transforms a reusable block on the page into a
      regular block.
    - MAKE_BLOCK_REUSABLE: Transforms a regular block on the page into a
      reusable block.

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -759,6 +759,11 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 	), $meta_box_url );
 	wp_localize_script( 'wp-editor', '_wpMetaBoxUrl', $meta_box_url );
 
+	// Ensure that we've got jQuery.
+	if ( ! wp_script_is( 'jquery', 'done' ) ) {
+		wp_enqueue_script( 'jquery' );
+	}
+
 	// Initialize the editor.
 	$gutenberg_theme_support = get_theme_support( 'gutenberg' );
 	$color_palette           = gutenberg_color_palette();
@@ -772,9 +777,14 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 		'colors'     => $color_palette,
 	);
 
-	wp_add_inline_script( 'wp-editor', 'wp.api.init().done( function() {'
-		. 'window._wpGutenbergEditor = wp.editor.createEditorInstance( \'editor\', window._wpGutenbergPost, ' . json_encode( $editor_settings ) . ' ); '
-		. '} );'
+	wp_add_inline_script(
+		'wp-editor',
+		(
+			'jQuery.when( wp.api.init(), wp.api.init( { versionString: \'gutenberg/v1/\' } ) ).done( function() {'
+			. 'wp.editor.createEditorInstance( \'editor\', window._wpGutenbergPost, ' . json_encode( $editor_settings ) . ' ); '
+			. 'window._wpGutenbergEditor = wp.editor.createEditorInstance( \'editor\', window._wpGutenbergPost, ' . json_encode( $editor_settings ) . ' ); '
+			. '} );'
+		)
 	);
 
 	/**
