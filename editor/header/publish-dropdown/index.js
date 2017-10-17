304c39e4d1aaffd1df8dfbebb8fb2597c9c5c44f
Riad Benguella <benguella@gmail.com>
2017-10-17 12:10:56 +0100

    Chrome: Use an expandable panel for the post visibility inside the publish dropdown

--- editor/header/publish-dropdown/index.js
+++ editor/header/publish-dropdown/index.js
@@ -2,20 +2,37 @@
  * WordPress Dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { withAPIData, PanelBody } from '@wordpress/components';
 
 /**
  * Internal Dependencies
  */
 import './style.scss';
-import PostVisibility from '../../sidebar/post-visibility';
+import PostVisibilityLabel from '../../post-visibility/label';
+import PostVisibilityForm from '../../post-visibility';
 import PostSchedule from '../../sidebar/post-schedule';
 import PublishButton from '../publish-button';
 
-function PublishDropdown( { onSubmit } ) {
+function PublishDropdown( { user, onSubmit } ) {
+	const canPublish = user.data && user.data.capabilities.publish_posts;
+
 	return (
 		<div className="editor-publish-dropdown">
 			<div><strong>{ __( 'All ready to go!' ) }</strong></div>
-			<PostVisibility />
+			{ ! canPublish &&
+				<div>
+					<span>{ __( 'Visibility' ) }</span>
+					<span><PostVisibilityLabel /></span>
+				</div>
+			}
+			{ canPublish &&
+				<PanelBody initialOpen={ false } title={ [
+					__( 'Visibility: ' ),
+					<PostVisibilityLabel key="label" />,
+				] }>
+					<PostVisibilityForm />
+				</PanelBody>
+			}
 			<PostSchedule />
 			<div className="editor-publish-dropdown__publish-button-container">
 				<PublishButton onSubmit={ onSubmit } />
@@ -24,4 +41,8 @@ function PublishDropdown( { onSubmit } ) {
 	);
 }
 
-export default PublishDropdown;
+export default withAPIData( () => {
+	return {
+		user: '/wp/v2/users/me?context=edit',
+	};
+} )( PublishDropdown );
