ed024aa8171142ea0038782c3f1aff9a5ff1bcc6
Adam Silverstein <adam@10up.com>
2017-10-22 10:44:26 -0400

    a11y - add built files

---
+++ packages/a11y/build/filterMessage.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var previousMessage = '';
+
+/**
+ * Filter the message to be announced to the screenreader.
+ *
+ * @param {String} message The message to be announced.
+ *
+ * @returns {String}
+ */
+var filterMessage = function filterMessage(message) {
+
+	/*
+  * Strip HTML tags (if any) from the message string. Ideally, messages should
+  * be simple strings, carefully crafted for specific use with A11ySpeak.
+  * When re-using already existing strings this will ensure simple HTML to be
+  * stripped out and replaced with a space. Browsers will collapse multiple
+  * spaces natively.
+  */
+	message = message.replace(/<[^<>]+>/g, ' ');
+
+	if (previousMessage === message) {
+		message += '\xA0';
+	}
+
+	previousMessage = message;
+
+	return message;
+};
+
+exports.default = filterMessage;
