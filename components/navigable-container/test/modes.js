fadd1a237ceb3e476434fb2502671575ba808262
morgan <morgan.smith@ephox.com>
2017-11-02 10:27:09 +1000

    grid navigation for irregular grids

--- components/navigable-container/test/modes.js
+++ components/navigable-container/test/modes.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import { omit } from 'lodash';
+import { omit, each } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -124,7 +124,7 @@ describe( 'calculateMode menu', () => {
 		} );
 	} );
 
-	it( 'should create tabbing mode without extra options specified', () => {
+	it( 'should create menu mode without extra options specified', () => {
 		const actual = calculateMode( {
 			mode: 'menu',
 			cycle: false,
@@ -175,6 +175,139 @@ describe( 'calculateMode menu', () => {
 			// Wrong keys for orientation horizontal
 			assertIndex( 4, 4, 5, shiftKey, DOWN, { mode: 'menu', cycle: true, orientation: 'horizontal' } );
 			assertIndex( 0, 0, 5, shiftKey, UP, { mode: 'menu', cycle: true, orientation: 'horizontal' } );
+		} );
+	} );
+} );
+
+describe( 'calculateMode grid', () => {
+	it( 'should create grid with defaults', () => {
+		const actual = calculateMode( { mode: 'grid' } );
+		expect( omit( actual, [ 'detect' ] ) ).toEqual( {
+			useTabstops: false,
+			deep: false,
+			widget: false,
+		} );
+	} );
+
+	it( 'should create grid mode with all options specified', () => {
+		const actual = calculateMode( {
+			mode: 'grid',
+			cycle: false,
+			deep: true,
+			widget: true,
+			width: 2,
+		} );
+
+		expect( omit( actual, [ 'detect' ] ) ).toEqual( {
+			useTabstops: false,
+			deep: true,
+			widget: true,
+		} );
+	} );
+
+	it( 'should create grid mode without extra options specified', () => {
+		const actual = calculateMode( {
+			mode: 'grid',
+			cycle: false,
+			deep: true,
+			widget: true,
+			extra: 10,
+		} );
+
+		expect( omit( actual, [ 'detect' ] ) ).toEqual( {
+			useTabstops: false,
+			deep: true,
+			widget: true,
+		} );
+	} );
+
+	it( 'checking move function for grid', () => {
+		/*
+		 0 1 2 3
+		 4 5 6 7
+		 8 9
+		 */
+
+		const shiftValues = [ false, true ];
+		shiftValues.forEach( ( shiftKey ) => {
+			const tests = {
+				0: [
+					{ key: DOWN, result: 4 },
+					{ key: UP, result: 8 },
+					{ key: RIGHT, result: 1 },
+					{ key: LEFT, result: 3 },
+				],
+
+				1: [
+					{ key: DOWN, result: 5 },
+					{ key: UP, result: 9 },
+					{ key: RIGHT, result: 2 },
+					{ key: LEFT, result: 0 },
+				],
+
+				2: [
+					{ key: DOWN, result: 6 },
+					{ key: UP, result: 6 },
+					{ key: RIGHT, result: 3 },
+					{ key: LEFT, result: 1 },
+				],
+
+				3: [
+					{ key: DOWN, result: 7 },
+					{ key: UP, result: 7 },
+					{ key: RIGHT, result: 0 },
+					{ key: LEFT, result: 2 },
+				],
+
+				4: [
+					{ key: DOWN, result: 8 },
+					{ key: UP, result: 0 },
+					{ key: RIGHT, result: 5 },
+					{ key: LEFT, result: 7 },
+				],
+
+				5: [
+					{ key: DOWN, result: 9 },
+					{ key: UP, result: 1 },
+					{ key: RIGHT, result: 6 },
+					{ key: LEFT, result: 4 },
+				],
+
+				6: [
+					{ key: DOWN, result: 2 },
+					{ key: UP, result: 2 },
+					{ key: RIGHT, result: 7 },
+					{ key: LEFT, result: 5 },
+				],
+
+				7: [
+					{ key: DOWN, result: 3 },
+					{ key: UP, result: 3 },
+					{ key: RIGHT, result: 4  },
+					{ key: LEFT, result: 6 },
+				],
+
+				8: [
+					{ key: DOWN, result: 0 },
+					{ key: UP, result: 4 },
+					{ key: RIGHT, result: 9 },
+					{ key: LEFT, result: 9 },
+				],
+
+				9: [
+					{ key: DOWN, result: 1 },
+					{ key: UP, result: 5 },
+					{ key: RIGHT, result: 8 },
+					{ key: LEFT, result: 8 },
+				],
+			};
+
+			each( tests, ( v, k ) => {
+				each( v, ( testcase ) => {
+					assertIndex( testcase.result, k, 10, shiftKey, testcase.key, { mode: 'grid', width: 4 } );
+					assertIndex( k, k, 10, shiftKey, TAB, { mode: 'grid', width: 4 } );
+				} );
+			} );
 		} );
 	} );
 } );
