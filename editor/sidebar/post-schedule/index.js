9c011c6ae214611f5aafeafdf73cfea838590aa4
Sören Wrede <soerenwrede@gmail.com>
2017-11-06 22:12:16 +0100

    Add label elements to scheduler and visibility (#3317)
    * Change span to label elements.
    
    * Extract withAPIData function from export
    
    * Restore destroyed line

--- editor/sidebar/post-schedule/index.js
+++ editor/sidebar/post-schedule/index.js
@@ -1,8 +1,13 @@
 /**
+ * External dependencies
+ */
+import { flowRight } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PanelRow, Dropdown, withAPIData } from '@wordpress/components';
+import { PanelRow, Dropdown, withAPIData, withInstanceId } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -11,19 +16,21 @@ import './style.scss';
 import PostScheduleLabel from '../../post-schedule/label';
 import PostScheduleForm from '../../post-schedule';
 
-export function PostSchedule( { user } ) {
+export function PostSchedule( { user, instanceId } ) {
 	if ( ! user.data || ! user.data.capabilities.publish_posts ) {
 		return null;
 	}
+	const postScheduleSelectorId = 'post-schedule-selector-' + instanceId;
 
 	return (
 		<PanelRow className="editor-post-schedule">
-			<span>{ __( 'Publish' ) }</span>
+			<label htmlFor={ postScheduleSelectorId }>{ __( 'Publish' ) }</label>
 			<Dropdown
 				position="bottom left"
 				contentClassName="editor-post-schedule__dialog"
 				renderToggle={ ( { onToggle, isOpen } ) => (
 					<button
+						id={ postScheduleSelectorId }
 						type="button"
 						className="editor-post-schedule__toggle button-link"
 						onClick={ onToggle }
@@ -38,8 +45,11 @@ export function PostSchedule( { user } ) {
 	);
 }
 
-export default withAPIData( () => {
-	return {
-		user: '/wp/v2/users/me?context=edit',
-	};
-} )( PostSchedule );
+const applyWithAPIData = withAPIData( () => ( {
+	user: '/wp/v2/users/me?context=edit',
+} ) );
+
+export default flowRight( [
+	applyWithAPIData,
+	withInstanceId,
+] )( PostSchedule );
