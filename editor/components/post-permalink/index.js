c45d13f805be1801d22abf8f42cf82d362a0e302
Gary Pendergast <gary@pento.net>
2017-11-10 23:37:27 +1100

    Fix some linting errors

--- editor/components/post-permalink/index.js
+++ editor/components/post-permalink/index.js
@@ -90,12 +90,12 @@ class PostPermalink extends Component {
 						{ prefix }
 					</span>
 					{ ! editingSlug &&
-						<span
+						<Button
 							className="editor-post-permalink__slug"
 							onClick={ this.onEditPermalink }
 						>
 							{ slug }
-						</span>
+						</Button>
 					}
 					{ editingSlug &&
 						<form className="editor-post-permalink__slug-form" onSubmit={ this.onSavePermalink }>
