4d78e5e51fc419e6ceaf1ab47ae25bde3b379a9d
Jorge <jorge.costa@developer.pt>
2017-11-15 18:46:29 +0000

    Created a withFallbackStyles higher order component.

---
+++ components/higher-order/with-fallback-styles/index.js
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+import { every, isEqual } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+
+export default ( mapNodeToProps ) => ( WrappedComponent ) => {
+	return class extends Component {
+		constructor() {
+			super( ...arguments );
+			this.nodeRef = null;
+			this.state = {
+				fallbackStyles: undefined,
+				grabStylesCompleted: false,
+			};
+
+			this.bindRef = this.bindRef.bind( this );
+		}
+
+		bindRef( node ) {
+			if ( ! node ) {
+				return;
+			}
+			this.nodeRef = node;
+		}
+
+		componentDidMount() {
+			this.grabFallbackStyles();
+		}
+
+		componentDidUpdate() {
+			this.grabFallbackStyles();
+		}
+
+		grabFallbackStyles() {
+			const { grabStylesCompleted, fallbackStyles } = this.state;
+			if ( this.nodeRef && ! grabStylesCompleted ) {
+				const newFallbackStyles = mapNodeToProps( this.nodeRef, this.props );
+				if ( ! isEqual( newFallbackStyles, fallbackStyles ) ) {
+					this.setState( {
+						fallbackStyles: newFallbackStyles,
+						grabStylesCompleted: !! every( newFallbackStyles ),
+					} );
+				}
+			}
+		}
+
+		render() {
+			return (
+				<div ref={ this.bindRef }>
+					<WrappedComponent { ...this.props } { ...this.state.fallbackStyles } />
+				</div>
+			);
+		}
+	};
+};
