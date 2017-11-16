ecb533918fad0ddc909d38224db1f3049fb52b31
Riad Benguella <benguella@gmail.com>
2017-11-16 09:23:03 +0100

    Chrome: Use NavigableMenu to allow arrow navigation for the ChoiceMenu Component

--- components/choice-menu/index.js
+++ components/choice-menu/index.js
@@ -3,36 +3,39 @@
  */
 import IconButton from '../icon-button';
 import Button from '../button';
+import { NavigableMenu } from '../navigable-container';
 import './style.scss';
 
 function ChoiceMenu( { label, value, choices, onSelect } ) {
 	return (
 		<div className="components-choice-menu">
 			<div className="components-choice-menu__label">{ label }</div>
-			{ choices.map( ( item ) => {
-				const isSelected = value === item.value;
-				if ( isSelected ) {
+			<NavigableMenu orientation="vertical">
+				{ choices.map( ( item ) => {
+					const isSelected = value === item.value;
+					if ( isSelected ) {
+						return (
+							<IconButton
+								key={ item.value }
+								className="components-choice-menu__item is-selected"
+								icon="yes"
+							>
+								{ item.label }
+							</IconButton>
+						);
+					}
+
 					return (
-						<IconButton
+						<Button
 							key={ item.value }
-							className="components-choice-menu__item is-selected"
-							icon="yes"
+							className="components-choice-menu__item"
+							onClick={ () => onSelect( item.value ) }
 						>
 							{ item.label }
-						</IconButton>
+						</Button>
 					);
-				}
-
-				return (
-					<Button
-						key={ item.value }
-						className="components-choice-menu__item"
-						onClick={ () => onSelect( item.value ) }
-					>
-						{ item.label }
-					</Button>
-				);
-			} ) }
+				} ) }
+			</NavigableMenu>
 		</div>
 	);
 }
