f35e0b85da4d4efda4d15b8bfb7d9a8839ce2b73
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-11-21 13:10:08 +0100

    Testing: Add first test using snapshots testing

--- components/notice/test/index.js
+++ components/notice/test/index.js
@@ -9,13 +9,12 @@ import { shallow } from 'enzyme';
 import Notice from '../index';
 
 describe( 'Notice', () => {
-	it( 'should have valid class names', () => {
+	it( 'should match snapshot', () => {
 		const wrapper = shallow( <Notice status="example" /> );
-		expect( wrapper.hasClass( 'notice' ) ).toBe( true );
-		expect( wrapper.hasClass( 'notice-alt' ) ).toBe( true );
-		expect( wrapper.hasClass( 'notice-example' ) ).toBe( true );
-		expect( wrapper.hasClass( 'is-dismissible' ) ).toBe( true );
+
+		expect( wrapper ).toMatchSnapshot();
 	} );
+
 	it( 'should not have is-dismissible class when isDismissible prop is false', () => {
 		const wrapper = shallow( <Notice isDismissible={ false } /> );
 		expect( wrapper.hasClass( 'is-dismissible' ) ).toBe( false );
