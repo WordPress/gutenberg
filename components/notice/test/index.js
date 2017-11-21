7dd31901062460707db3f41a87fd8d483ee568d5
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2017-11-21 08:23:59 +0100

    Testing: Update test descriptions for Notice

--- components/notice/test/index.js
+++ components/notice/test/index.js
@@ -9,14 +9,14 @@ import { shallow } from 'enzyme';
 import Notice from '../index';
 
 describe( 'Notice', () => {
-	it( 'should has valid class', () => {
+	it( 'should have valid class names', () => {
 		const wrapper = shallow( <Notice status="example" /> );
 		expect( wrapper.hasClass( 'notice' ) ).toBe( true );
 		expect( wrapper.hasClass( 'notice-alt' ) ).toBe( true );
 		expect( wrapper.hasClass( 'notice-example' ) ).toBe( true );
 		expect( wrapper.hasClass( 'is-dismissible' ) ).toBe( true );
 	} );
-	it( 'should not has is-dismissible class when isDismissible prop is false', () => {
+	it( 'should not have is-dismissible class when isDismissible prop is false', () => {
 		const wrapper = shallow( <Notice isDismissible={ false } /> );
 		expect( wrapper.hasClass( 'is-dismissible' ) ).toBe( false );
 	} );
