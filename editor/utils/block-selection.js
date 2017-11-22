327f2454313504a4578ad6780c24a3c42f984adc
morgan <morgan.smith@ephox.com>
2017-11-22 13:54:24 +1000

    handling basic toggling

--- editor/utils/block-selection.js
+++ editor/utils/block-selection.js
@@ -1,4 +1,32 @@
-export function resetSelection( current, uid ) {
+import { sortBy, includes, last, difference } from 'lodash';
+
+function getRange( start, end, ordering ) {
+	if ( start && ! end ) {
+		return [ start ];
+	} else if ( ! start && ! end ) {
+		return [ ];
+	}
+
+	const startIndex = ordering.indexOf( start );
+	const endIndex = ordering.indexOf( end );
+
+	if ( startIndex > endIndex ) {
+		return ordering.slice( endIndex, startIndex + 1 );
+	}
+
+	return ordering.slice( startIndex, endIndex + 1 );
+}
+
+function sortByOrder( ordering ) {
+	return ( a ) => ordering.indexOf( a );
+}
+
+function getSelected( current, ordering ) {
+	const ranged = getRange( current.start, current.end, ordering );
+	return sortBy( current.selected.concat( ranged ), sortByOrder( ordering ) );
+}
+
+export function reset( current, uid ) {
 	return {
 		selected: [ ],
 		start: uid,
@@ -6,5 +34,31 @@ export function resetSelection( current, uid ) {
 	};
 }
 
+export function toggle( current, uid, ordering ) {
+	const everything = getSelected( current, ordering );
 
+	if ( ! includes( everything, uid ) ) {
+		// This is not in our selection, so let's make it our new anchor
+		return { selected: everything, start: uid, end: null };
+	}
+
+	const withoutUid = difference( everything, [ uid ] );
+	console.log( 'withoutUid', withoutUid );
+
+	if ( current.start === uid ) {
+		const nextStart = last( withoutUid );
+		if ( nextStart ) {
+			return { selected: withoutUid.slice( 0, withoutUid.length - 1 ), start: nextStart, end: null };
+		}
+
+		// I don't know what should happen here
+		return { selected: [ ], start: uid, end: null };
+	}
+
+	return { selected: difference( withoutUid, [ current.start ] ), start: current.start, end: null };
+}
+
+export function includeRange( current, start, end, ordering ) {
+	return current;
+}
 
--- editor/utils/test/block-selection.js
+++ editor/utils/test/block-selection.js
@@ -1,25 +1,80 @@
-import { resetSelection } from '../block-selection';
+import { reset, toggle } from '../block-selection';
+
+const nothing = { selected: [ ], start: null, end: null };
+
+const ordering = [ 'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'rho' ];
 
 describe( 'block-selection', () => {
 	describe( 'reset', () => {
-		it( 'resetSelection sets alpha', () => {
-			const actual = resetSelection( { selected: [ ], start: null, end: null }, 'alpha' );
+		it( 'Nothing >>> alpha', () => {
+			const actual = reset( nothing, 'alpha' );
 			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
 		} );
 
-		it( 'resetSelection changes beta to alpha', () => {
-			const actual = resetSelection( { selected: [ ], start: 'beta', end: null }, 'alpha' );
+		it( '(Beta -> ..) >>> alpha', () => {
+			const actual = reset( { selected: [ ], start: 'beta', end: null }, 'alpha' );
 			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
 		} );
 
-		it( 'resetSelection changes beta to alpha and clears end', () => {
-			const actual = resetSelection( { selected: [ ], start: 'beta', end: 'gamma' }, 'alpha' );
+		it( '(Beta -> Gamma) >>> alpha', () => {
+			const actual = reset( { selected: [ ], start: 'beta', end: 'gamma' }, 'alpha' );
 			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
 		} );
 
-		it( 'resetSelection changes beta to alpha and clears selected, end', () => {
-			const actual = resetSelection( { selected: [ 'delta' ], start: 'beta', end: 'gamma' }, 'alpha' );
+		it( '([ delta ], Beta -> Gamma) >>> alpha', () => {
+			const actual = reset( { selected: [ 'delta' ], start: 'beta', end: 'gamma' }, 'alpha' );
 			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
+		} );
+	} );
+
+	describe( 'toggle', () => {
+		it( 'Nothing >>> alpha', () => {
+			const actual = toggle( nothing, 'alpha', ordering );
+			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
+		} );
+
+		it( 'Include a selection when only a start value', () => {
+			const current = {
+				selected: [ ],
+				start: 'alpha',
+				end: null,
+			};
+			const actual = toggle( current, 'beta', ordering );
+			expect( actual ).toEqual( { selected: [ 'alpha' ], start: 'beta', end: null } );
+		} );
+
+		it( 'Exclude a selection when only a start value (cannot do)', () => {
+			const current = {
+				selected: [ ],
+				start: 'alpha',
+				end: null,
+			};
+			const actual = toggle( current, 'alpha', ordering );
+			expect( actual ).toEqual( { selected: [ ], start: 'alpha', end: null } );
+		} );
+
+		it( 'Add to selection when no start value, but a selected array', () => {
+			const current = { selected: [ 'alpha' ], start: null, end: null };
+			const actual = toggle( current, 'beta', ordering );
+			expect( actual ).toEqual( { selected: [ 'alpha' ], start: 'beta', end: null } );
+		} );
+
+		it( 'Add to selection when no selected, but a start->end range', () => {
+			const current = { selected: [ ], start: 'alpha', end: 'gamma' };
+			const actual = toggle( current, 'epsilon', ordering );
+			expect( actual ).toEqual( { selected: [ 'alpha', 'beta', 'gamma' ], start: 'epsilon', end: null } );
+		} );
+
+		it( 'Add to selection selected AND a start->end range', () => {
+			const current = { selected: [ 'alpha' ], start: 'beta', end: 'delta' };
+			const actual = toggle( current, 'epsilon', ordering );
+			expect( actual ).toEqual( { selected: [ 'alpha', 'beta', 'gamma', 'delta' ], start: 'epsilon', end: null } );
+		} );
+
+		it( 'Toggle off selection something that is already within the range', () => {
+			const current = { selected: [ 'alpha' ], start: 'beta', end: 'delta' };
+			const actual = toggle( current, 'gamma', ordering );
+			expect( actual ).toEqual( { selected: [ 'alpha', 'delta' ], start: 'beta', end: null } );
 		} );
 	} );
 } );
