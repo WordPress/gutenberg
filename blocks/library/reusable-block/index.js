142c15af983c4435ea91545601654358c8a2099d
Robert Anderson <robert@noisysocks.com>
2017-11-23 11:59:11 +1100

    Add UI for editing a reusable block
    Adds the UI which displays when a reusable block is asked for its `edit`
    component. This includes a panel which includes UI that indicates
    whether a reusable block is loading and lets a user change the name of
    the block.

--- blocks/library/reusable-block/index.js
+++ blocks/library/reusable-block/index.js
@@ -1,12 +1,149 @@
 /**
+ * External dependencies
+ */
+import { pickBy, noop } from 'lodash';
+import { connect } from 'react-redux';
+
+/**
  * WordPress dependencies
  */
+import { Component } from '@wordpress/element';
+import { Placeholder, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { registerBlockType } from '../../api';
+import { getBlockType, registerBlockType } from '../../api';
+import ReusableBlockEditPanel from './edit-panel';
+
+class ReusableBlockEdit extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.startEditing = this.startEditing.bind( this );
+		this.stopEditing = this.stopEditing.bind( this );
+		this.setAttributes = this.setAttributes.bind( this );
+		this.setName = this.setName.bind( this );
+		this.updateReusableBlock = this.updateReusableBlock.bind( this );
+
+		this.state = {
+			isEditing: false,
+			name: null,
+			attributes: null,
+		};
+	}
+
+	componentDidMount() {
+		if ( ! this.props.reusableBlock ) {
+			this.props.fetchReusableBlock();
+		}
+	}
+
+	startEditing() {
+		this.setState( { isEditing: true } );
+	}
+
+	stopEditing() {
+		this.setState( {
+			isEditing: false,
+			name: null,
+			attributes: null,
+		} );
+	}
+
+	setAttributes( attributes ) {
+		this.setState( ( prevState ) => ( {
+			attributes: { ...prevState.attributes, ...attributes },
+		} ) );
+	}
+
+	setName( name ) {
+		this.setState( { name } );
+	}
+
+	updateReusableBlock() {
+		const { name, attributes } = this.state;
+
+		// Use pickBy to include only changed (assigned) values in payload
+		const payload = pickBy( {
+			name,
+			attributes,
+		} );
+
+		this.props.updateReusableBlock( payload );
+		this.props.saveReusableBlock();
+		this.stopEditing();
+	}
+
+	render() {
+		const { focus, reusableBlock, convertBlockToStatic } = this.props;
+		const { isEditing, name, attributes } = this.state;
+
+		if ( ! reusableBlock ) {
+			return <Placeholder><Spinner /></Placeholder>;
+		}
+
+		const blockType = getBlockType( reusableBlock.type );
+		const BlockEdit = blockType.edit || blockType.save;
+
+		return [
+			// We fake the block being read-only by wrapping it with an element that has pointer-events: none
+			<div key="edit" style={ { pointerEvents: isEditing ? 'auto' : 'none' } }>
+				<BlockEdit
+					{ ...this.props }
+					focus={ isEditing ? focus : null }
+					attributes={ { ...reusableBlock.attributes, ...attributes } }
+					setAttributes={ isEditing ? this.setAttributes : noop } />
+			</div>,
+			focus && (
+				<ReusableBlockEditPanel
+					key="panel"
+					isEditing={ isEditing }
+					name={ name !== null ? name : reusableBlock.name }
+					isSaving={ reusableBlock.isSaving }
+					onEdit={ this.startEditing }
+					onDetach={ convertBlockToStatic }
+					onChangeName={ this.setName }
+					onSave={ this.updateReusableBlock }
+					onCancel={ this.stopEditing } />
+			),
+		];
+	}
+}
+
+const ConnectedReusableBlockEdit = connect(
+	( state, ownProps ) => ( {
+		reusableBlock: state.reusableBlocks[ ownProps.attributes.ref ],
+	} ),
+	( dispatch, ownProps ) => ( {
+		fetchReusableBlock() {
+			dispatch( {
+				type: 'FETCH_REUSABLE_BLOCKS',
+				id: ownProps.attributes.ref,
+			} );
+		},
+		updateReusableBlock( reusableBlock ) {
+			dispatch( {
+				type: 'UPDATE_REUSABLE_BLOCK',
+				id: ownProps.attributes.ref,
+				reusableBlock,
+			} );
+		},
+		saveReusableBlock() {
+			dispatch( {
+				type: 'SAVE_REUSABLE_BLOCK',
+				id: ownProps.attributes.ref,
+			} );
+		},
+		convertBlockToStatic() {
+			dispatch( {
+				type: 'CONVERT_BLOCK_TO_STATIC',
+				uid: ownProps.id,
+			} );
+		},
+	} )
+)( ReusableBlockEdit );
 
 registerBlockType( 'core/reusable-block', {
 	title: __( 'Reusable Block' ),
@@ -19,6 +156,6 @@ registerBlockType( 'core/reusable-block', {
 		},
 	},
 
-	edit: () => <div>{ __( 'Reusable Blocks are coming soon!' ) }</div>,
+	edit: ConnectedReusableBlockEdit,
 	save: () => null,
 } );
