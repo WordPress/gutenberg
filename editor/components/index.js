6a25b9c9b294ed3f9039e4ad42938ba7c8f6b3ce
Riad Benguella <benguella@gmail.com>
2017-11-24 12:22:06 +0100

    Components: Extract Reusable DocumentOutlineCheck component

--- editor/components/index.js
+++ editor/components/index.js
@@ -1,6 +1,7 @@
 // Post Related Components
 export { default as AutosaveMonitor } from './autosave-monitor';
 export { default as DocumentOutline } from './document-outline';
+export { default as DocumentOutlineCheck } from './document-outline/check';
 export { default as EditorGlobalKeyboardShortcuts } from './editor-global-keyboard-shortcuts';
 export { default as EditorHistoryRedo } from './editor-history/redo';
 export { default as EditorHistoryUndo } from './editor-history/undo';
