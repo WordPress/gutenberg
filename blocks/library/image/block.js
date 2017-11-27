aceeb49c9edf726565b07d84e54ea5403ff36886
Mateusz Palaczyk <mate.palaczyk@gmail.com>
2017-11-27 20:21:00 +0100

    #3133 fix (squashed)

--- blocks/library/image/block.js
+++ blocks/library/image/block.js
@@ -123,6 +123,12 @@ class ImageBlock extends Component {
 		const uploadFromFiles = ( event ) => mediaUpload( event.target.files, setAttributes );
 		const dropFiles = ( files ) => mediaUpload( files, setAttributes );
 
+		const blockDescription = (
+			<BlockDescription>
+				<p>{ __( 'Worth a thousand words.' ) }</p>
+			</BlockDescription>
+		);
+	
 		const controls = (
 			focus && (
 				<BlockControls key="controls">
@@ -152,6 +158,11 @@ class ImageBlock extends Component {
 		if ( ! url ) {
 			return [
 				controls,
+				focus && (
+					<InspectorControls key="inspector">
+						{ blockDescription }
+					</InspectorControls>
+				),
 				<Placeholder
 					key="placeholder"
 					instructions={ __( 'Drag image here or insert from media library' ) }
@@ -194,9 +205,7 @@ class ImageBlock extends Component {
 			controls,
 			focus && (
 				<InspectorControls key="inspector">
-					<BlockDescription>
-						<p>{ __( 'Worth a thousand words.' ) }</p>
-					</BlockDescription>
+					{ blockDescription }
 					<h3>{ __( 'Image Settings' ) }</h3>
 					<TextControl label={ __( 'Textual Alternative' ) } value={ alt } onChange={ this.updateAlt } help={ __( 'Describe the purpose of the image. Leave empty if the image is not a key part of the content.' ) } />
 					{ ! isEmpty( availableSizes ) && (
