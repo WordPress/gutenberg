d9278f7b3d922d56534b3535ba1121ef76834fe4
Marcus Kazmierczak <marcus@mkaz.com>
2017-11-27 16:05:44 -0800

    Switch method of matching types to models

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -554,38 +554,38 @@ function gutenberg_register_vendor_script( $handle, $src, $deps = array() ) {
 function gutenberg_extend_wp_api_backbone_client() {
 	// Post Types Mapping.
 	$post_type_rest_base_mapping = array();
-	foreach ( get_post_types( array(), 'objects' ) as $post_type_object ) {
-		$rest_base = ! empty( $post_type_object->rest_base ) ? $post_type_object->rest_base : $post_type_object->name;
-		$post_type_rest_base_mapping[ $post_type_object->name ] = $rest_base;
-	}
+	$post_type_rest_base_mapping[ 'attachment' ] = "media";
+	$post_type_rest_base_mapping = apply_filters( 'post_type_rest_base_mapping', $post_type_rest_base_mapping );
 
 	// Taxonomies Mapping.
 	$taxonomy_rest_base_mapping = array();
 	foreach ( get_taxonomies( array(), 'objects' ) as $taxonomy_object ) {
-		$rest_base = ! empty( $taxonomy_object->rest_base ) ? $taxonomy_object->rest_base : $taxonomy_object->name;
-		$taxonomy_rest_base_mapping[ $taxonomy_object->name ] = $rest_base;
+		if ( ! empty( $taxonomy_object->rest_base ) ) {
+			$taxonomy_rest_base_mapping[ $taxonomy_object->name ] = $taxonomy_object->rest_base;
+		}
 	}
+	$taxonomy_rest_base_mapping = apply_filters( 'taxonomy_rest_base_mapping', $taxonomy_rest_base_mapping );
 
 	$script  = sprintf( 'wp.api.postTypeRestBaseMapping = %s;', wp_json_encode( $post_type_rest_base_mapping ) );
 	$script .= sprintf( 'wp.api.taxonomyRestBaseMapping = %s;', wp_json_encode( $taxonomy_rest_base_mapping ) );
 	$script .= <<<JS
 		wp.api.getPostTypeModel = function( postType ) {
-			var route = '/' + wpApiSettings.versionString + this.postTypeRestBaseMapping[ postType ] + '/(?P<id>[\\\\d]+)';
-			return _.find( wp.api.models, function( model ) {
-				return model.prototype.route && route === model.prototype.route.index;
-			} );
+			if ( this.postTypeRestBaseMapping[ postType ] ) {
+				postType = this.postTypeRestBaseMapping[ postType ];
+			}
+			return wp.api.models[ wp.api.utils.capitalizeAndCamelCaseDashes( postType ) ];
 		};
 		wp.api.getTaxonomyModel = function( taxonomy ) {
-			var route = '/' + wpApiSettings.versionString + this.taxonomyRestBaseMapping[ taxonomy ] + '/(?P<id>[\\\\d]+)';
-			return _.find( wp.api.models, function( model ) {
-				return model.prototype.route && route === model.prototype.route.index;
-			} );
+			if ( this.taxonomyRestBaseMapping[ taxonomy ] ) {
+				taxonomy = this.taxonomyRestBaseMapping[ taxonomy ];
+			}
+			return wp.api.models[ wp.api.utils.capitalizeAndCamelCaseDashes( taxonomy ) ];
 		};
 		wp.api.getTaxonomyCollection = function( taxonomy ) {
-			var route = '/' + wpApiSettings.versionString + this.taxonomyRestBaseMapping[ taxonomy ];
-			return _.find( wp.api.collections, function( model ) {
-				return model.prototype.route && route === model.prototype.route.index;
-			} );
+			if ( this.taxonomyRestBaseMapping[ taxonomy ] ) {
+				taxonomy = this.taxonomyRestBaseMapping[ taxonomy ];
+			}
+			return wp.api.collections[ wp.api.utils.capitalizeAndCamelCaseDashes( taxonomy ) ];
 		};
 JS;
 	wp_add_inline_script( 'wp-api', $script );
