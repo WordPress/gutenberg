ec1e3dcd01bfde874549a1854a562bcfe2f6e02d
Vladan Ostojic <vladan.ostojic@quantumglobe.com>
2017-12-01 17:20:50 +0100

    add toggle editor mode shortcut

---
+++ editor/components/editor-global-keyboard-shortcuts/editor-mode.js
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { KeyboardShortcuts } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { getEditorMode } from '../../selectors';
+
+class EditorModeKeyboardShortcuts extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.toggleMode = this.toggleMode.bind( this );
+	}
+
+	toggleMode() {
+		const { mode, switchMode } = this.props;
+		switchMode( mode === 'visual' ? 'text' : 'visual' );
+	}
+
+	render() {
+		return (
+			<KeyboardShortcuts shortcuts={ {
+				'mod+shift+alt+m': this.toggleMode,
+			} } />
+		);
+	}
+}
+
+export default connect(
+	( state ) => {
+		return {
+			mode: getEditorMode( state ),
+		};
+	},
+	( dispatch ) => {
+		return {
+			switchMode: ( mode ) => {
+				dispatch( {
+					type: 'SWITCH_MODE', mode: mode,
+				} );
+			},
+		};
+	},
+
+)( EditorModeKeyboardShortcuts );
