1318bafbc39630e1cd0704506af7b86acdcbb76f
morgan <morgan.smith@ephox.com>
2017-12-06 14:23:46 +1000

    cleanup

--- test/e2e/integration/004-managing-links.js
+++ test/e2e/integration/004-managing-links.js
@@ -8,9 +8,7 @@ describe( 'Managing blocks', () => {
 
 		cy.get( 'body' ).then( ( $body ) => {
 			const unselected = $body.find( 'button:contains("Fix toolbar to block"):not(".is-selected")' );
-			console.log('unselected', unselected);
 			if ( unselected.length ) {
-				console.log('found a non-selected button');
 				return 'button:contains("Fix toolbar to block")';
 			}
 
@@ -18,13 +16,8 @@ describe( 'Managing blocks', () => {
 		} ).then( ( selector ) => {
 			cy.log( ' selector " + selector ', selector );
 			cy.get( selector ).click();
-		}, ( err ) => {
-			cy.log(' error ', err);
 		} );
 
-		const lastBlockSelector = '.editor-block-list__block-edit:last [contenteditable="true"]:first';
-
-		// Using the placeholder
 		cy.get( '.editor-default-block-appender' ).click();
 
 		cy.get( 'button[aria-label="Link"]' ).click();
@@ -32,6 +25,5 @@ describe( 'Managing blocks', () => {
 		cy.focused().type( '{leftarrow}' );
 
 		cy.get( '.blocks-format-toolbar__link-modal' ).should( 'be.visible' );
-
 	} );
 } );
