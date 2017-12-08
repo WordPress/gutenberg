058a96872e4ef3180e5e1e2c4105ad3aa8cdf243
BE-Webdesign <cromleyedwin89@gmail.com>
2017-12-08 15:12:39 -0500

    Modularize AlignmentToolbar tests.

--- blocks/alignment-toolbar/test/index.js
+++ blocks/alignment-toolbar/test/index.js
@@ -9,25 +9,31 @@ import { shallow } from 'enzyme';
 import AlignmentToolbar from '../';
 
 describe( 'AlignmentToolbar', () => {
+	const value = 'left';
+	const onChange = jest.fn();
+
+	const wrapper = shallow( <AlignmentToolbar value={ value } onChange={ onChange } /> );
+
+	const controls = wrapper.props().controls;
+
+	beforeEach( () => {
+		onChange.mockClear();
+	} );
+
 	test( 'should render the component.', () => {
-		const value = 'left';
-		const onChange = jest.fn();
-
-		const wrapper = shallow( <AlignmentToolbar value={ value } onChange={ onChange } /> );
-
-		const controls = wrapper.props().controls;
-
 		expect( wrapper ).toMatchSnapshot();
+	} );
 
+	test( 'should call on change with null when a control is already active.', () => {
 		// Check onClick handler for an active control.
 		controls.find( ( control ) => control.isActive ).onClick();
 
 		expect( onChange ).toHaveBeenCalledTimes( 1 );
 		// Should be called null when active control is clicked.
 		expect( onChange ).toHaveBeenCalledWith( null );
+	} );
 
-		onChange.mockClear();
-
+	test( 'should call on change a new value when the control is not active.', () => {
 		// Check onClick handler for an inactive control.
 		const inactiveControl = controls.find( ( control ) => ! control.isActive );
 		inactiveControl.onClick();
