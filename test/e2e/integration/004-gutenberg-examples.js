9d6abb62df822ccc36d6bfc43e88aa7d154cab81
Aaron Jorbin <aaron@jorb.in>
2017-12-09 19:30:00 -0500

    add initial test for gutenberg examples
    When the INSTALL_GUTENBERG_EXAMPLES enviormental variable is true, the example plugins are tested

---
+++ test/e2e/integration/004-gutenberg-examples.js
@@ -0,0 +1,30 @@
+let conditionalDescribe = Cypress.env( 'EXAMPLES' ) ? describe : describe.skip;
+conditionalDescribe( 'Example Blocks', () => {
+	before( () => {
+		cy.newPost();
+	} );
+
+	it( 'Should insert example blocks', () => {
+		const lastBlockSelector = '.editor-block-list__block-edit:last [contenteditable="true"]:first';
+
+		cy.get( '.editor-header [aria-label="Insert block"]' ).click();
+		cy.get( '[placeholder="Search for a block"]' ).type( 'example' );
+		cy.get( '.editor-inserter__block' ).contains( 'Example' ).click();
+
+		cy.get( '.editor-header [aria-label="Insert block"]' ).click();
+		cy.get( '[placeholder="Search for a block"]' ).type( 'Example: Basic (esnext)' );
+		cy.get( '.editor-inserter__block' ).contains( 'Example: Basic (esnext)' ).click();
+
+
+		// Switch to Text Mode to check HTML Output
+		cy.get( '.editor-ellipsis-menu [aria-label="More"]' ).click();
+		cy.get( 'button' ).contains( 'Code Editor' ).click();
+
+
+		// Assertions
+		cy.get( '.editor-post-text-editor' )
+			.should( 'contain', 'wp-block-gutenberg-examples-example-01-basic' )
+			.should( 'contain', 'wp-block-gutenberg-examples-example-01-basic-esnext' )
+	});
+
+});
