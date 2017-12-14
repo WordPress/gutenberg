1342736459440087e6ae173e67d491558adb35cd
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-12-14 18:25:12 +0100

    Blocks: Add test for generated class name with BlockEdit

--- blocks/block-edit/index.js
+++ blocks/block-edit/index.js
@@ -18,7 +18,7 @@ import {
 } from '../api';
 
 export function BlockEdit( props ) {
-	const { name, attributes } = props;
+	const { name, attributes = {} } = props;
 	const blockType = getBlockType( name );
 
 	if ( ! blockType ) {
