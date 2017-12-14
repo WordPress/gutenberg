17916c0c96f97077f34df0ee55b49fcad29f01b9
Robert Anderson <robert@noisysocks.com>
2017-12-14 16:22:39 +1100

    Allow pressing ENTER to change Reusable Block name
    When the Reusable Block name field is focused, pressing ENTER should
    save the block. Similarly, pressing ESCAPE should cancel editing the
    block.

--- blocks/library/block/edit-panel/index.js
+++ blocks/library/block/edit-panel/index.js
@@ -3,46 +3,61 @@
  */
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { keycodes } from '@wordpress/utils';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
+const { ESCAPE } = keycodes;
+
 function ReusableBlockEditPanel( props ) {
 	const { isEditing, title, isSaving, onEdit, onDetach, onChangeTitle, onSave, onCancel } = props;
 
-	return (
-		<div className="reusable-block-edit-panel">
-			{ ! isEditing && ! isSaving && [
-				<span key="info" className="reusable-block-edit-panel__info">
-					<b>{ title }</b>
-				</span>,
-				<Button
-					key="edit"
-					isLarge
-					className="reusable-block-edit-panel__button"
-					onClick={ onEdit }>
-					{ __( 'Edit' ) }
-				</Button>,
-				<Button
-					key="detach"
-					isLarge
-					className="reusable-block-edit-panel__button"
-					onClick={ onDetach }>
-					{ __( 'Detach' ) }
-				</Button>,
-			] }
-			{ ( isEditing || isSaving ) && [
+	return [
+		! isEditing && ! isSaving && <div className="reusable-block-edit-panel">
+			<span key="info" className="reusable-block-edit-panel__info">
+				<b>{ title }</b>
+			</span>
+			<Button
+				key="edit"
+				isLarge
+				className="reusable-block-edit-panel__button"
+				onClick={ onEdit }>
+				{ __( 'Edit' ) }
+			</Button>
+			<Button
+				key="detach"
+				isLarge
+				className="reusable-block-edit-panel__button"
+				onClick={ onDetach }>
+				{ __( 'Detach' ) }
+			</Button>
+		</div>,
+		( isEditing || isSaving ) && (
+			<form
+				className="reusable-block-edit-panel"
+				onSubmit={ ( event ) => {
+					event.preventDefault();
+					onSave();
+				} }>
 				<input
 					key="title"
 					type="text"
 					disabled={ isSaving }
 					className="reusable-block-edit-panel__title"
 					value={ title }
-					onChange={ ( event ) => onChangeTitle( event.target.value ) } />,
+					onChange={ ( event ) => onChangeTitle( event.target.value ) }
+					onKeyDown={ ( event ) => {
+						if ( event.keyCode === ESCAPE ) {
+							event.stopPropagation();
+							onCancel();
+						}
+					} } />
 				<Button
 					key="save"
+					type="submit"
 					isPrimary
 					isLarge
 					isBusy={ isSaving }
@@ -50,7 +65,7 @@ function ReusableBlockEditPanel( props ) {
 					className="reusable-block-edit-panel__button"
 					onClick={ onSave }>
 					{ __( 'Save' ) }
-				</Button>,
+				</Button>
 				<Button
 					key="cancel"
 					isLarge
@@ -58,10 +73,10 @@ function ReusableBlockEditPanel( props ) {
 					className="reusable-block-edit-panel__button"
 					onClick={ onCancel }>
 					{ __( 'Cancel' ) }
-				</Button>,
-			] }
-		</div>
-	);
+				</Button>
+			</form>
+		),
+	];
 }
 
 export default ReusableBlockEditPanel;
