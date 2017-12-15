12cff29f77bf323630de0791095bc21fcb08155a
Sören Wrede <soerenwrede@gmail.com>
2017-12-15 03:01:45 +0100

    Audio Block: Hide Edit button if no src

--- blocks/library/audio/index.js
+++ blocks/library/audio/index.js
@@ -69,7 +69,8 @@ registerBlockType( 'core/audio', {
 			const { editing, className, src } = this.state;
 			const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
 			const switchToEditing = () => {
-				this.setState( { editing: true } );
+				setAttributes( { src: '' } );
+				this.setState( { editing: true, src: '' } );
 			};
 			const onSelectAudio = ( media ) => {
 				if ( media && media.url ) {
@@ -95,16 +96,14 @@ registerBlockType( 'core/audio', {
 						onChange={ updateAlignment }
 					/>
 					<Toolbar>
-						<Button
-							buttonProps={ {
-								className: 'components-icon-button components-toolbar__control',
-								'aria-label': __( 'Edit audio' ),
-							} }
+						{ src && <Button
+							className="components-icon-button components-toolbar__control"
+							aria-label={ __( 'Edit audio' ) }
 							type="audio"
 							onClick={ switchToEditing }
 						>
 							<Dashicon icon="edit" />
-						</Button>
+						</Button> }
 					</Toolbar>
 				</BlockControls>,
 
