7a280a940a9bb9a619554a316b4ac2da0ba59df5
Robert Anderson <robert@noisysocks.com>
2017-12-20 11:22:00 +1100

    Fix React warning
    Elements in this returned array require a key= attribute.

--- blocks/library/block/edit-panel/index.js
+++ blocks/library/block/edit-panel/index.js
@@ -16,34 +16,34 @@ function ReusableBlockEditPanel( props ) {
 	const { isEditing, title, isSaving, onEdit, onDetach, onChangeTitle, onSave, onCancel } = props;
 
 	return [
-		! isEditing && ! isSaving && <div className="reusable-block-edit-panel">
-			<span key="info" className="reusable-block-edit-panel__info">
-				<b>{ title }</b>
-			</span>
-			<Button
-				key="edit"
-				isLarge
-				className="reusable-block-edit-panel__button"
-				onClick={ onEdit }>
-				{ __( 'Edit' ) }
-			</Button>
-			<Button
-				key="detach"
-				isLarge
-				className="reusable-block-edit-panel__button"
-				onClick={ onDetach }>
-				{ __( 'Detach' ) }
-			</Button>
-		</div>,
+		( ! isEditing && ! isSaving ) && (
+			<div key="view" className="reusable-block-edit-panel">
+				<span className="reusable-block-edit-panel__info">
+					<b>{ title }</b>
+				</span>
+				<Button
+					isLarge
+					className="reusable-block-edit-panel__button"
+					onClick={ onEdit }>
+					{ __( 'Edit' ) }
+				</Button>
+				<Button
+					isLarge
+					className="reusable-block-edit-panel__button"
+					onClick={ onDetach }>
+					{ __( 'Detach' ) }
+				</Button>
+			</div>
+		),
 		( isEditing || isSaving ) && (
 			<form
+				key="edit"
 				className="reusable-block-edit-panel"
 				onSubmit={ ( event ) => {
 					event.preventDefault();
 					onSave();
 				} }>
 				<input
-					key="title"
 					type="text"
 					disabled={ isSaving }
 					className="reusable-block-edit-panel__title"
@@ -56,7 +56,6 @@ function ReusableBlockEditPanel( props ) {
 						}
 					} } />
 				<Button
-					key="save"
 					type="submit"
 					isPrimary
 					isLarge
@@ -67,7 +66,6 @@ function ReusableBlockEditPanel( props ) {
 					{ __( 'Save' ) }
 				</Button>
 				<Button
-					key="cancel"
 					isLarge
 					disabled={ isSaving }
 					className="reusable-block-edit-panel__button"
