6c8ddf2711c7524089733a64d3d83831efd7c9f0
Riad Benguella <benguella@gmail.com>
2017-12-21 11:54:49 +0100

    Chrome: Replace the publish dropdown by a sidebar panel

--- editor/edit-post/header/index.js
+++ editor/edit-post/header/index.js
@@ -8,41 +8,80 @@ import { connect } from 'react-redux';
  */
 import { __ } from '@wordpress/i18n';
 import { IconButton } from '@wordpress/components';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
-import { PostPreviewButton, PostSavedState, PostPublishWithDropdown } from '../../components';
+import {
+	PostPreviewButton,
+	PostSavedState,
+	PostPublishPanelToggle,
+	PostPublishPanel,
+} from '../../components';
 import EllipsisMenu from './ellipsis-menu';
 import HeaderToolbar from './header-toolbar';
 import { isEditorSidebarOpened } from '../../store/selectors';
 import { toggleSidebar } from '../../store/actions';
 
-function Header( { onToggleSidebar, isSidebarOpened } ) {
-	return (
-		<div
-			role="region"
-			aria-label={ __( 'Editor toolbar' ) }
-			className="editor-header"
-			tabIndex="-1"
-		>
-			<HeaderToolbar />
-			<div className="editor-header__settings">
-				<PostSavedState />
-				<PostPreviewButton />
-				<PostPublishWithDropdown />
-				<IconButton
-					icon="admin-generic"
-					onClick={ onToggleSidebar }
-					isToggled={ isSidebarOpened }
-					label={ __( 'Settings' ) }
-					aria-expanded={ isSidebarOpened }
-				/>
-				<EllipsisMenu />
+class Header extends Component {
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			isPublishPanelOpened: false,
+		};
+		this.togglePublishPanel = this.togglePublishPanel.bind( this );
+		this.closePublishPanel = this.closePublishPanel.bind( this );
+	}
+
+	togglePublishPanel() {
+		this.setState( ( state ) => ( {
+			isPublishPanelOpened: ! state.isPublishPanelOpened,
+		} ) );
+	}
+
+	closePublishPanel() {
+		this.setState( {
+			isPublishPanelOpened: false,
+		} );
+	}
+
+	render() {
+		const { onToggleSidebar, isSidebarOpened } = this.props;
+		return (
+			<div
+				role="region"
+				aria-label={ __( 'Editor toolbar' ) }
+				className="editor-header"
+				tabIndex="-1"
+			>
+				<HeaderToolbar />
+				<div className="editor-header__settings">
+					{ ! this.state.isPublishPanelOpened && [
+						<PostSavedState key="saved-state" />,
+						<PostPreviewButton key="preview-button" />,
+						<PostPublishPanelToggle
+							key="publish-button"
+							isOpen={ this.state.isPublishPanelOpened }
+							onToggle={ this.togglePublishPanel }
+						/>,
+						<IconButton
+							key="sidebar-toggle"
+							icon="admin-generic"
+							onClick={ onToggleSidebar }
+							isToggled={ isSidebarOpened }
+							label={ __( 'Settings' ) }
+							aria-expanded={ isSidebarOpened }
+						/>,
+						<EllipsisMenu key="ellipsis-menu" />,
+					] }
+
+					{ this.state.isPublishPanelOpened && <PostPublishPanel onClose={ this.closePublishPanel } /> }
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default connect(
