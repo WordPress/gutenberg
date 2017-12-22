f7b1cdffc23994ba26822167a10e55d849cea214
Riad Benguella <benguella@gmail.com>
2017-12-22 15:03:17 +0100

    Gallery Block: Drag and drop multiple images create a gallery block

--- blocks/library/gallery/index.js
+++ blocks/library/gallery/index.js
@@ -1,12 +1,13 @@
 /**
  * External dependencies
  */
-import { filter } from 'lodash';
+import { filter, every } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { createMediaFromFile } from '@wordpress/utils';
 
 /**
  * Internal dependencies
@@ -104,6 +105,21 @@ registerBlockType( 'core/gallery', {
 							return link === 'file' ? 'media' : link;
 						},
 					},
+				},
+			},
+			{
+				type: 'files',
+				isMatch( files ) {
+					return files.length !== 1 && every( files, ( file ) => file.type.indexOf( 'image/' ) === 0 );
+				},
+				transform( files ) {
+					return Promise.all( files.map( ( file ) => createMediaFromFile( file ) ) )
+						.then( ( medias ) => createBlock( 'core/gallery', {
+							images: medias.map( media => ( {
+								id: media.id,
+								url: media.source_url,
+							} ) ),
+						} ) );
 				},
 			},
 		],
