9dad2ff318c8bac1c9050d1ed030de2a70d4eb25
Adam Silverstein <adam@10up.com>
2017-12-24 14:24:41 -0500

    rename class file to match class name

--- lib/load.php
+++ lib/load.php
@@ -13,7 +13,7 @@ require dirname( __FILE__ ) . '/meta-box-partial-page.php';
 require dirname( __FILE__ ) . '/class-wp-block-type.php';
 require dirname( __FILE__ ) . '/class-wp-block-type-registry.php';
 require dirname( __FILE__ ) . '/class-wp-rest-reusable-blocks-controller.php';
-require dirname( __FILE__ ) . '/class-wp-rest-autosave-controller.php';
+require dirname( __FILE__ ) . '/class-wp-rest-autosaves-controller.php';
 require dirname( __FILE__ ) . '/blocks.php';
 require dirname( __FILE__ ) . '/client-assets.php';
 require dirname( __FILE__ ) . '/compat.php';
