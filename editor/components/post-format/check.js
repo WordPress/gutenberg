b69920a2d6225a739c65683604875e54df1baf26
Riad Benguella <benguella@gmail.com>
2017-12-25 14:40:09 +0100

    Chrome: User the PostTypeSupportCheck to check Post Formats support

--- editor/components/post-format/check.js
+++ editor/components/post-format/check.js
@@ -1,41 +1,10 @@
 /**
- * External dependencies
- */
-import { connect } from 'react-redux';
-import { get } from 'lodash';
-
-/**
- * WordPress dependencies
- */
-import { withAPIData } from '@wordpress/components';
-import { compose } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
-import { getCurrentPostType } from '../../store/selectors';
+import PostTypeSupportCheck from '../post-type-support-check';
 
-function PostFormatCheck( { postType, children } ) {
-	if ( ! get( postType.data, [ 'supports', 'post-formats' ] ) ) {
-		return null;
-	}
-
-	return children;
+function PostFormatCheck( props ) {
+	return <PostTypeSupportCheck { ...props } supportKey="post-formats" />;
 }
 
-export default compose( [
-	connect(
-		( state ) => {
-			return {
-				postTypeSlug: getCurrentPostType( state ),
-			};
-		},
-	),
-	withAPIData( ( props ) => {
-		const { postTypeSlug } = props;
-
-		return {
-			postType: `/wp/v2/types/${ postTypeSlug }?context=edit`,
-		};
-	} ),
-] )( PostFormatCheck );
+export default PostFormatCheck;
