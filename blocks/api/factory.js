a22c95969006d2bd4aff2b52d103cde85c65dd6d
iseulde <ella@iseulde.com>
2017-12-26 14:03:08 +0100

    Keep and handle image alignment classes for raw transforms

--- blocks/api/factory.js
+++ blocks/api/factory.js
@@ -26,15 +26,17 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { getBlockType, getBlockTypes } from './registration';
+import { getBlockAttributes } from './parser';
 
 /**
  * Returns a block object given its type and attributes.
  *
- * @param  {String} name             Block name
- * @param  {Object} blockAttributes  Block attributes
- * @return {Object}                  Block object
+ * @param  {String}  name             Block name
+ * @param  {Object}  blockAttributes  Block attributes
+ * @param  {?String} innerHTML        Block HTML
+ * @return {Object}                   Block object
  */
-export function createBlock( name, blockAttributes = {} ) {
+export function createBlock( name, blockAttributes = {}, innerHTML ) {
 	// Get the type definition associated with a registered block.
 	const blockType = getBlockType( name );
 
@@ -57,7 +59,7 @@ export function createBlock( name, blockAttributes = {} ) {
 		uid: uuid(),
 		name,
 		isValid: true,
-		attributes,
+		attributes: innerHTML ? getBlockAttributes( blockType, innerHTML, attributes ) : attributes,
 	};
 }
 
