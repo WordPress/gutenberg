a0b5858fe30bcfa12eb04ecfa5ddc6728106a5d3
Adam Silverstein <adam@10up.com>
2017-12-26 20:53:26 -0500

    improve autosave monitor

--- editor/components/autosave-monitor/index.js
+++ editor/components/autosave-monitor/index.js
@@ -15,12 +15,13 @@ import { autosave } from '../../store/actions';
 import {
 	isEditedPostDirty,
 	isEditedPostSaveable,
+	isAutosavingPost,
 } from '../../store/selectors';
 
 export class AutosaveMonitor extends Component {
 	componentDidUpdate( prevProps ) {
 		const { isDirty, isSaveable } = this.props;
-		if ( prevProps.isDirty !== isDirty ||
+		if ( isDirty ||
 				prevProps.isSaveable !== isSaveable ) {
 			this.toggleTimer( isDirty && isSaveable );
 		}
@@ -51,6 +52,7 @@ export default connect(
 		return {
 			isDirty: isEditedPostDirty( state ),
 			isSaveable: isEditedPostSaveable( state ),
+			isAutosaving: isAutosavingPost( state ),
 		};
 	},
 	{ autosave }
