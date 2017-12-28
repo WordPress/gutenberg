c854709c65c4e7ea23777ca5efd7a5907347bd69
Jorge <jorge.costa@developer.pt>
2017-12-28 18:57:20 +0000

    Added support for multiple lines. (+4 squashed commits) Squashed commits: [f08184b] Made gallery to image and image to gallery transformations take into consideration caption attributes. [0cf5d9e] Implemented caption editing and styles in the gallery block. [ebca968] Added caption to the attributes of images in the gallery and to the saving logic. [465b2c7] Added caption slimImageObject set of MediaUploadButton.
    This allows us to read to image caption from the media library in the gallery block.

--- blocks/library/gallery/gallery-image.js
+++ blocks/library/gallery/gallery-image.js
@@ -10,6 +10,11 @@ import { Component } from '@wordpress/element';
 import { IconButton, withAPIData, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import RichText from '../../rich-text';
+
 class GalleryImage extends Component {
 	componentWillReceiveProps( { image } ) {
 		if ( image && image.data && ! this.props.url ) {
@@ -21,7 +26,7 @@ class GalleryImage extends Component {
 	}
 
 	render() {
-		const { url, alt, id, linkTo, link, isSelected, onClick, onRemove } = this.props;
+		const { url, alt, id, linkTo, link, imageIndex, isSelected, caption, onClick, onRemove, focus, setAttributes, onFocus } = this.props;
 
 		let href;
 
@@ -56,6 +61,17 @@ class GalleryImage extends Component {
 					</div>
 				}
 				{ href ? <a href={ href }>{ img }</a> : img }
+				{ ( caption && caption.length > 0 ) || ( focus && isSelected ) ? (
+					<RichText
+						tagName="figcaption"
+						placeholder={ __( 'Write caption…' ) }
+						value={ caption }
+						focus={ focus && focus.editableIndex === imageIndex ? focus : undefined }
+						onFocus={ onFocus }
+						onChange={ newCaption => setAttributes( { caption: newCaption } ) }
+						inlineToolbar
+					/>
+				) : null }
 			</figure>
 		);
 		/* eslint-enable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
