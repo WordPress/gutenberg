254aa814337ca51bc24e1ee052de6298a95c9b7b
Vladan Ostojic <vladan.ostojic@quantumglobe.com>
2017-12-28 11:48:58 +0100

    check if mac when displaying editor mode toggle shortcut

--- editor/edit-post/keyboard-shortcuts.js
+++ editor/edit-post/keyboard-shortcuts.js
@@ -1,6 +1,10 @@
+// eslint-disable-next-line
+const isMac = navigator.platform.toUpperCase().indexOf( 'MAC' ) >= 0;
+const mod = isMac ? '⌘' : 'Ctrl';
+
 export default {
 	toggleEditorMode: {
 		value: 'mod+shift+alt+m',
-		label: '⌘+Shift+Alt+M',
+		label: `${ mod }+Shift+Alt+M`,
 	},
 };
