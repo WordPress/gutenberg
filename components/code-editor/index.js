c78dc36a3ea0edc9f32e9cdaeac247481c12073c
Robert Anderson <robert@noisysocks.com>
2018-01-08 16:08:17 +1100

    Prevent CodeEditor from propagating events
    This prevents the cursor from moving around to different blocks while
    editing code.

--- components/code-editor/index.js
+++ components/code-editor/index.js
@@ -8,6 +8,7 @@ class CodeEditor extends Component {
 		super( ...arguments );
 
 		this.onBlur = this.onBlur.bind( this );
+		this.onKeyHandled = this.onKeyHandled.bind( this );
 	}
 
 	componentDidMount() {
@@ -15,6 +16,7 @@ class CodeEditor extends Component {
 		this.editor = instance.codemirror;
 
 		this.editor.on( 'blur', this.onBlur );
+		this.editor.on( 'keyHandled', this.onKeyHandled );
 	}
 
 	componentWillReceiveProps( { value } ) {
@@ -25,6 +27,7 @@ class CodeEditor extends Component {
 
 	componentWillUnmount() {
 		this.editor.off( 'blur', this.onBlur );
+		this.editor.off( 'keyHandled', this.onKeyHandled );
 
 		this.editor.toTextArea();
 		this.editor = null;
@@ -34,6 +37,13 @@ class CodeEditor extends Component {
 		if ( this.props.onChange ) {
 			this.props.onChange( editor.getValue() );
 		}
+	}
+
+	onKeyHandled( editor, name, event ) {
+		// Stop events from propagating out of the component. This makes the editor
+		// behave like a textarea, e.g. pressing CMD+UP moves the cursor to the top of
+		// the editor, rather than to a different element.
+		event.stopImmediatePropagation();
 	}
 
 	render() {
