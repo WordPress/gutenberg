2bbf340495ec3490e0911103adfd1aa6bd890d04
Anton Timmermans <email@atimmer.com>
2018-01-12 11:17:15 +0100

    Make the selectors API discoverable
    All selectors are available on wp.data.selectors. Inside the HoC you
    get passed the object, so you can just do:
    
    ```js
    ( selectors ) => {
    	return title: selectors[ "core/editor" ].getEditedPostTitle();
    }
    ```

--- data/index.js
+++ data/index.js
@@ -1,4 +1,4 @@
-/**
+/*
  * External dependencies
  */
 import { connect } from 'react-redux';
@@ -6,6 +6,10 @@ import { createStore, combineReducers } from 'redux';
 import { flowRight } from 'lodash';
 
 /**
+ * @module wp.data
+ */
+
+/*
  * Module constants
  */
 const reducers = {};
@@ -19,12 +23,13 @@ const initialReducer = () => ( {} );
 const store = createStore( initialReducer, {}, flowRight( enhancers ) );
 
 /**
- * Registers a new sub reducer to the global state and returns a Redux-like store object.
+ * Registers a new sub reducer to the global state and returns a Redux-like
+ * store object.
  *
- * @param {String}  key     Reducer key
- * @param {Object}  reducer Reducer function
+ * @param {string}  key     Reducer key.
+ * @param {Object}  reducer Reducer function.
  *
- * @returns {Object}        Store Object
+ * @returns {Object}        Store Object.
  */
 export function registerReducer( key, reducer ) {
 	reducers[ key ] = reducer;
@@ -50,21 +55,51 @@ export function registerReducer( key, reducer ) {
 }
 
 /**
- * Registers selectors for external usage
+ * Registers a single selector for a redux state component.
  *
- * @param {String} key                 Reducer key
- * @param {Object} registeredSelectors Selectors
+ * @param {string}   reducerKey Part of the state shape to register the selector for.
+ * @param {string}   selectorKey Name of the public name for the selector.
+ * @param {Function} selector The selector function. Get's passed the sliced state as its first argument.
  */
-export function registerSelectors( key, registeredSelectors ) {
-	selectors[ key ] = registeredSelectors;
+export function registerSelector( reducerKey, selectorKey, selector ) {
+	if ( ! selectors.hasOwnProperty( reducerKey ) ) {
+		selectors[ reducerKey ] = {};
+	}
+
+	selectors[ reducerKey ][ selectorKey ] = ( ...args ) => {
+		return selector( store.getState()[ reducerKey ], ...args );
+	};
 }
 
-export const query = ( mapPropsToSelectors ) => ( WrappedComponent ) => {
-	return connect( ( state ) => {
-		const select = ( key, selectorName, ...args ) => {
-			return selectors[ key ][ selectorName ]( state[ key ], ...args );
-		};
+/**
+ * Registers selectors for external usage.
+ *
+ * @param {string} reducerKey          Part of the state shape to register the
+ *                                     selectors for.
+ * @param {Object} registeredSelectors Selectors to register. Keys will be used
+ *                                     as the public facing API. Selectors will
+ *                                     get passed the state as first argument.
+ */
+export function registerSelectors( reducerKey, registeredSelectors ) {
+	for ( const [ selectorKey, selector ] of Object.entries( registeredSelectors ) ) {
+		registerSelector( reducerKey, selectorKey, selector );
+	}
+}
 
-		return mapPropsToSelectors( select );
+/**
+ * Adds data to a component.
+ *
+ * @param {Function} mapSelectorsToProps Get's called with the selectors object
+ *                                       to determine the data for the component.
+ *
+ * @returns {ReactElement} Renders the wrapped component and passes it data.
+ */
+export const query = ( mapSelectorsToProps ) => ( WrappedComponent ) => {
+	return connect( ( state, ownProps ) => {
+		return mapSelectorsToProps( selectors, ownProps );
 	} )( WrappedComponent );
+};
+
+export {
+	selectors,
 };
