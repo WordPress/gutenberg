75213f24ffd426c8ec2367084c2dd12cfa238b18
Riad Benguella <benguella@gmail.com>
2018-01-15 09:41:42 +0100

    Data Modules: Export state using selectors

--- data/test/index.js
+++ data/test/index.js
@@ -1,4 +1,12 @@
-import { registerReducer } from '../';
+/**
+ * External dependencies
+ */
+import { render } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { registerReducer, registerSelectors, select, query } from '../';
 
 describe( 'store', () => {
 	it( 'Should append reducers to the state', () => {
@@ -10,5 +18,42 @@ describe( 'store', () => {
 
 		const store2 = registerReducer( 'red2', reducer2 );
 		expect( store2.getState() ).toEqual( 'ribs' );
+	} );
+} );
+
+describe( 'select', () => {
+	it( 'registers multiple selectors to the public API', () => {
+		const store = registerReducer( 'reducer1', () => 'state1' );
+		const selector1 = jest.fn( () => 'result1' );
+		const selector2 = jest.fn( () => 'result2' );
+
+		registerSelectors( 'reducer1', {
+			selector1,
+			selector2,
+		} );
+
+		expect( select( 'reducer1', 'selector1' ) ).toEqual( 'result1' );
+		expect( selector1 ).toBeCalledWith( store.getState() );
+
+		expect( select( 'reducer1', 'selector2' ) ).toEqual( 'result2' );
+		expect( selector2 ).toBeCalledWith( store.getState() );
+	} );
+} );
+
+describe( 'query', () => {
+	it( 'passes the relevant data to the component', () => {
+		registerReducer( 'reactReducer', () => 'reactState' );
+		registerSelectors( 'reactReducer', { reactSelector: state => state } );
+		const Component = query( ( selector ) => {
+			return {
+				data: selector( 'reactReducer', 'reactSelector' ),
+			};
+		} )( ( props ) => {
+			return <div>{ props.data }</div>;
+		} );
+
+		const tree = render( <Component /> );
+
+		expect( tree ).toMatchSnapshot();
 	} );
 } );
