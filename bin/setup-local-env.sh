10e84c12fcf5c87487561f16c21d9ac2bb0881a6
Gary Pendergast <gary@pento.net>
2018-01-16 00:19:21 +1100

    Add a welcome message at the end of setup-local-env.sh

--- bin/setup-local-env.sh
+++ bin/setup-local-env.sh
@@ -11,3 +11,19 @@ cd "$(dirname "$0")/.."
 
 # Check Docker is installed and running
 . "$(dirname "$0")/install-docker.sh"
+
+GUTENBERG=$(cat <<EOT
+,---.     |              |                   
+|  _..   .|--- ,---.,---.|---.,---.,---.,---.
+|   ||   ||    |---'|   ||   ||---'|    |   |
+\`---'\`---'\`---'\`---'\`   '\`---'\`---'\`    \`---|
+                                        \`---'
+EOT
+)
+
+CURRENT_URL=$(docker run -it --rm --volumes-from wordpress-dev --network container:wordpress-dev wordpress:cli option get siteurl)
+CURRENT_URL="${CURRENT_URL//[[:space:]]/}"
+
+echo -e "\nWelcome to...\n"
+echo -e "\033[95m$GUTENBERG\033[0m"
+echo -e "Run \033[32mnpm run dev\033[0m, then open \033[32m$CURRENT_URL\033[0m to get started!"
