9b98afdc4ebb076575cc667679c88bbcc00921eb
Gary Pendergast <gary@pento.net>
2018-01-16 12:30:46 +1100

    Add a timeout parameter to the ask() function

--- bin/includes.sh
+++ bin/includes.sh
@@ -8,14 +8,17 @@
 #
 # @param {string} prompt    The question to ask the user.
 # @param {string} [default] Optional. "Y" or "N", for the default option to use if none is entered.
+# @param {int}    [timeout] Optional. The number of seconds to wait before using the default option.
 #
 # @returns {bool} true if the user replies Yes, false if the user replies No.
 ##
 ask() {
     # Source: https://djm.me/ask
-    local prompt default reply
+    local timeout endtime timediff prompt default reply
 
     while true; do
+
+		timeout="${3:-}"
 
         if [ "${2:-}" = "Y" ]; then
             prompt="Y/n"
@@ -26,13 +29,28 @@ ask() {
         else
             prompt="y/n"
             default=
+			timeout=
         fi
 
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -en "$1 [$prompt] "
+		if [ -z "$timeout" ]; then
+        	# Ask the question (not using "read -p" as it uses stderr not stdout)
+        	echo -en "$1 [$prompt] "
 
-        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-        read reply </dev/tty
+        	# Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        	read reply </dev/tty
+		else
+			endtime=$((`date +%s` + $timeout));
+			while [ "$endtime" -ge `date +%s` ]; do
+				timediff=$(($endtime - `date +%s`))
+
+				echo -en "\r$1 [$prompt] (Default $default in ${timediff}s) "
+				read -t 1 reply </dev/tty
+
+				if [ -n "$reply" ]; then
+					break
+				fi
+			done
+		fi
 
         # Default?
         if [ -z "$reply" ]; then
