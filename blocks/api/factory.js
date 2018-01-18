473e1c0468cd86b1634bf5033f9e510ab2e559ea
iseulde <wp@iseulde.com>
2018-01-18 16:58:51 +0100

    Correct docs

--- blocks/api/factory.js
+++ blocks/api/factory.js
@@ -67,8 +67,9 @@ export function createBlock( name, blockAttributes = {} ) {
  * given transformation is able to execute in the situation specified in the
  * params.
  *
- * @param {string}  sourceName   Block name.
- * @param {boolean} isMultiBlock Array of possible block transformations.
+ * @param {String}  sourceName    Block name
+ * @param {String}  transformType The transform type to check for.
+ * @param {Boolean} isMultiBlock  Array of possible block transformations
  *
  * @returns {Function} Predicate that receives a block type.
  */
@@ -83,8 +84,9 @@ const isTransformForBlockSource = ( sourceName, transformType, isMultiBlock = fa
  * block type contains a transformation able to execute in the situation
  * specified in the params.
  *
- * @param {string}  sourceName   Block name.
- * @param {boolean} isMultiBlock Array of possible block transformations.
+ * @param {String}  sourceName    Block name
+ * @param {String}  transformType The transform type to check for.
+ * @param {Boolean} isMultiBlock  Array of possible block transformations
  *
  * @returns {Function} Predicate that receives a block type.
  */
