90b4ba2334e39005ca8f931d3543dd0ef07629fb
Jason Caldwell <jas@src.works>
2018-01-19 13:13:58 -0900

    Add reusable block render tests

---
+++ phpunit/class-reusable-blocks-render-test.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Reusuable block rendering tests.
+ *
+ * @package Gutenberg
+ */
+
+/**
+ * Tests reusuable block rendering.
+ */
+class Reusuable_Blocks_Render_Test extends WP_UnitTestCase {
+	/**
+	 * Fake user ID.
+	 *
+	 * @var int
+	 */
+	protected static $user_id;
+
+	/**
+	 * Fake block ID.
+	 *
+	 * @var int
+	 */
+	protected static $block_id;
+
+	/**
+	 * Fake post ID.
+	 *
+	 * @var int
+	 */
+	protected static $post_id;
+
+	/**
+	 * Create fake data before tests run.
+	 *
+	 * @param WP_UnitTest_Factory $factory Helper that creates fake data.
+	 */
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::$user_id = $factory->user->create( array(
+			'role' => 'editor',
+		) );
+
+		self::$post_id = $factory->post->create( array(
+			'post_author'  => self::$user_id,
+			'post_type'    => 'post',
+			'post_status'  => 'publish',
+			'post_title'   => 'Test Post',
+			'post_content' => '<p>Hello world!</p>',
+		) );
+
+		self::$block_id = $factory->post->create( array(
+			'post_author'  => self::$user_id,
+			'post_type'    => 'wp_block',
+			'post_status'  => 'publish',
+			'post_title'   => 'Test Block',
+			'post_content' => '<!-- wp:core/paragraph --><p>Hello world!</p><!-- /wp:core/paragraph -->',
+		) );
+	}
+
+	/**
+	 * Delete fake data after tests run.
+	 */
+	public static function wpTearDownAfterClass() {
+		wp_delete_post( self::$block_id, true );
+		wp_delete_post( self::$post_id, true );
+		self::delete_user( self::$user_id );
+	}
+
+	/**
+	 * Set up.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->registry = new WP_Block_Type_Registry();
+
+		$this->registry->register( 'core/block', array(
+			'attributes'      => array(
+				'ref' => array(
+					'type' => 'number',
+				),
+			),
+			'render_callback' => 'gutenberg_render_block_core_reusable_block',
+		) );
+	}
+
+	/**
+	 * Tear down.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		$this->registry = null;
+	}
+
+	/**
+	 * Test rendering of a reusable block.
+	 */
+	public function test_render() {
+		$block_type = $this->registry->get_registered( 'core/block' );
+
+		$output = $block_type->render( array( 'ref' => self::$block_id ) );
+		$this->assertSame( '<p>Hello world!</p>', $output );
+	}
+
+	/**
+	 * Test rendering of a reusable block when 'ref' is missing, which should fail by
+	 * rendering an empty string.
+	 */
+	public function test_ref_empty() {
+		$block_type = $this->registry->get_registered( 'core/block' );
+
+		$output = $block_type->render( array(), 'foo' );
+		$this->assertSame( '', $output );
+	}
+
+	/**
+	 * Test rendering of a reusable block when 'ref' points to wrong post type, which
+	 * should fail by rendering an empty string.
+	 */
+	public function test_ref_wrong_post_type() {
+		$block_type = $this->registry->get_registered( 'core/block' );
+
+		$output = $block_type->render( array( 'ref' => self::$post_id ), 'foo' );
+		$this->assertSame( '', $output );
+	}
+}
