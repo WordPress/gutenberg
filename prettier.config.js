441a37aa49f523b4379fcf4d87b79ef4485263c3
Stephen Edgar <stephen@netweb.com.au>
2018-01-22 16:13:41 +1100

    Build Tools: Try `prettier-eslint`

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	printWidth: 80,
+	proseWrap: 'preserve',
+	semi: true,
+	singleQuote: true,
+	useTabs: true,
+	trailingComma: 'es5',
+};
