bf3f29c4eb8de4533c6ac4500697d3058e71f65f
Aaron Jorbin <jorbin@git.wordpress.org>
2018-01-23 20:21:46 +0000

    REST API: Add existing term_id to the error data object when attempting to create a duplicate term.
    Merges [42350] into 4.9
    
    Props shooper, coleh, rachelbaker.
    Fixes #42597. See #41370.
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.9@42578 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/rest-api/rest-categories-controller.php
+++ tests/phpunit/tests/rest-api/rest-categories-controller.php
@@ -623,14 +623,18 @@ class WP_Test_REST_Categories_Controller extends WP_Test_REST_Controller_Testcas
 	 */
 	public function test_create_item_term_already_exists() {
 		wp_set_current_user( self::$administrator );
+		$existing_id = $this->factory->category->create( array( 'name' => 'Existing' ) );
+
 		$request = new WP_REST_Request( 'POST', '/wp/v2/categories' );
-		$request->set_param( 'name', 'test' );
-		$response = $this->server->dispatch( $request );
-		$this->assertEquals( 201, $response->get_status() );
+		$request->set_param( 'name', 'Existing' );
+
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 409, $response->get_status() );
 		$data = $response->get_data();
 		$this->assertEquals( 'term_exists', $data['code'] );
+		$this->assertEquals( $existing_id, (int) $data['data']['term_id'] );
+
+		wp_delete_term( $existing_id, 'category' );
 	}
 
 	public function test_create_item_invalid_taxonomy() {
