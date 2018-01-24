eff8ba53ebdbd497571483437bda6a9f2e92ba4d
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2018-01-24 23:37:18 +0000

    WordPress 4.9.3 Beta 1.
    git-svn-id: https://develop.svn.wordpress.org/branches/4.9@42608 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.9.3-alpha-42520-src';
+$wp_version = '4.9.3-beta1-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
