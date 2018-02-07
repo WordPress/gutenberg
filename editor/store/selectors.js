7be95abec17a9ff5cceb9c6898144b3605d89284
Andrew Duthie <andrew@andrewduthie.com>
2018-02-07 16:01:39 -0500

    State: Avoid parameters in state selectors getDependants

--- editor/store/selectors.js
+++ editor/store/selectors.js
@@ -432,8 +432,8 @@ export const getBlock = createSelector(
 			},
 		};
 	},
-	( state, uid ) => [
-		get( state, [ 'editor', 'present', 'blocksByUid', uid ] ),
+	( state ) => [
+		get( state, [ 'editor', 'present', 'blocksByUid' ] ),
 		get( state, [ 'editor', 'present', 'edits', 'meta' ] ),
 		get( state, 'currentPost.meta' ),
 	]
