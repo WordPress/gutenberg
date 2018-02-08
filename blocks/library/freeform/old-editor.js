d6a8a91827acacbc389fba957741dd278fe7a7f9
Ella Van Dorpe <iseulde@automattic.com>
2018-02-08 17:44:32 +0100

    Remove unused code after WP 4.9 (#4950)

--- blocks/library/freeform/old-editor.js
+++ blocks/library/freeform/old-editor.js
@@ -109,56 +109,6 @@ export default class OldEditor extends Component {
 				editor.dom.toggleClass( ref, 'has-advanced-toolbar', active );
 			},
 		} );
-
-		/* eslint-disable */
-
-		// Re-register WP_More as it doesn't work with inline mode.
-		// This should be fixed in core.
-		// See wp-includes/js/tinymce/plugins/wordpress/plugin.js
-		// Swaps node.nodeName === 'BODY' to node === editor.getBody()
-		editor.on( 'init', () => {
-			editor.addCommand( 'WP_More', function( tag ) {
-				var parent, html, title,
-					classname = 'wp-more-tag',
-					dom = editor.dom,
-					node = editor.selection.getNode(),
-					rootNode = editor.getBody();
-
-				tag = tag || 'more';
-				classname += ' mce-wp-' + tag;
-				title = tag === 'more' ? 'Read more...' : 'Next page';
-				title = editor.editorManager.i18n.translate( title );
-				html = '<img src="' + tinymce.Env.transparentSrc + '" alt="" title="' + title + '" class="' + classname + '" ' +
-					'data-wp-more="' + tag + '" data-mce-resize="false" data-mce-placeholder="1" />';
-
-				// Most common case
-				if ( node === rootNode || ( node.nodeName === 'P' && node.parentNode == rootNode ) ) {
-					editor.insertContent( html );
-					return;
-				}
-
-				// Get the top level parent node
-				parent = dom.getParent( node, function( found ) {
-					if ( found.parentNode && found.parentNode === rootNode ) {
-						return true;
-					}
-
-					return false;
-				}, editor.getBody() );
-
-				if ( parent ) {
-					if ( parent.nodeName === 'P' ) {
-						parent.appendChild( dom.create( 'p', null, html ).firstChild );
-					} else {
-						dom.insertAfter( dom.create( 'p', null, html ), parent );
-					}
-
-					editor.nodeChanged();
-				}
-			} );
-		} );
-
-		/* eslint-enable */
 	}
 
 	render() {
