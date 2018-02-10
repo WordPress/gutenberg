412e369693cc544a27a4150b2186db93640e99cf
Miguel Fonseca <miguelcsf@gmail.com>
2018-02-10 18:57:20 +0000

    Video block: Refactor `edit` as function with `withState`

--- blocks/library/video/index.js
+++ blocks/library/video/index.js
@@ -6,8 +6,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Placeholder, Toolbar, IconButton, Button } from '@wordpress/components';
-import { Component } from '@wordpress/element';
+import { Placeholder, Toolbar, IconButton, Button, withState } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -18,6 +17,12 @@ import MediaUpload from '../../media-upload';
 import RichText from '../../rich-text';
 import BlockControls from '../../block-controls';
 import BlockAlignmentToolbar from '../../block-alignment-toolbar';
+
+const enhance = withState( ( props ) => ( {
+	editing: ! props.attributes.src,
+	src: props.attributes.src,
+	className: props.className,
+} ) );
 
 export const name = 'core/video';
 
@@ -57,116 +62,111 @@ export const settings = {
 		}
 	},
 
-	edit: class extends Component {
-		constructor( { className } ) {
-			super( ...arguments );
-			// edit component has its own src in the state so it can be edited
-			// without setting the actual value outside of the edit UI
-			this.state = {
-				editing: ! this.props.attributes.src,
-				src: this.props.attributes.src,
-				className,
-			};
-		}
-
-		render() {
-			const { align, caption, id } = this.props.attributes;
-			const { setAttributes, isSelected } = this.props;
-			const { editing, className, src } = this.state;
-			const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
-			const switchToEditing = () => {
-				this.setState( { editing: true } );
-			};
-			const onSelectVideo = ( media ) => {
-				if ( media && media.url ) {
-					// sets the block's attribute and updates the edit component from the
-					// selected media, then switches off the editing UI
-					setAttributes( { src: media.url, id: media.id } );
-					this.setState( { src: media.url, editing: false } );
-				}
-			};
-			const onSelectUrl = ( event ) => {
-				event.preventDefault();
-				if ( src ) {
-					// set the block's src from the edit component's state, and switch off the editing UI
-					setAttributes( { src } );
-					this.setState( { editing: false } );
-				}
-				return false;
-			};
-			const controls = isSelected && (
-				<BlockControls key="controls">
-					<BlockAlignmentToolbar
-						value={ align }
-						onChange={ updateAlignment }
-					/>
-					<Toolbar>
-						<IconButton
-							className="components-icon-button components-toolbar__control"
-							label={ __( 'Edit video' ) }
-							onClick={ switchToEditing }
-							icon="edit"
-						/>
-					</Toolbar>
-				</BlockControls>
-			);
-
-			if ( editing ) {
-				return [
-					controls,
-					<Placeholder
-						key="placeholder"
-						icon="media-video"
-						label={ __( 'Video' ) }
-						instructions={ __( 'Select a video file from your library, or upload a new one' ) }
-						className={ className }>
-						<form onSubmit={ onSelectUrl }>
-							<input
-								type="url"
-								className="components-placeholder__input"
-								placeholder={ __( 'Enter URL of video file here…' ) }
-								onChange={ event => this.setState( { src: event.target.value } ) }
-								value={ src || '' } />
-							<Button
-								isLarge
-								type="submit">
-								{ __( 'Use URL' ) }
-							</Button>
-						</form>
-						<MediaUpload
-							onSelect={ onSelectVideo }
-							type="video"
-							id={ id }
-							render={ ( { open } ) => (
-								<Button isLarge onClick={ open } >
-									{ __( 'Add from Media Library' ) }
-								</Button>
-							) }
-						/>
-					</Placeholder>,
-				];
+	edit: enhance( ( {
+		// Provided by `withState`:
+		editing,
+		src,
+		className,
+		setState,
+		// Provided by parent:
+		...props
+	} ) => {
+		const { align, caption, id } = props.attributes;
+		const { setAttributes, isSelected } = props;
+		const updateAlignment = ( nextAlign ) => setAttributes( { align: nextAlign } );
+		const switchToEditing = () => {
+			setState( { editing: true } );
+		};
+		const onSelectVideo = ( media ) => {
+			if ( media && media.url ) {
+				// sets the block's attribute and updates the edit component from the
+				// selected media, then switches off the editing UI
+				setAttributes( { src: media.url, id: media.id } );
+				setState( { src: media.url, editing: false } );
 			}
+		};
+		const onSelectUrl = ( event ) => {
+			event.preventDefault();
+			if ( src ) {
+				// set the block's src from the edit component's state, and switch off the editing UI
+				setAttributes( { src } );
+				setState( { editing: false } );
+			}
+			return false;
+		};
 
-			/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+		const controls = isSelected && (
+			<BlockControls key="controls">
+				<BlockAlignmentToolbar
+					value={ align }
+					onChange={ updateAlignment }
+				/>
+				<Toolbar>
+					<IconButton
+						className="components-icon-button components-toolbar__control"
+						label={ __( 'Edit video' ) }
+						onClick={ switchToEditing }
+						icon="edit"
+					/>
+				</Toolbar>
+			</BlockControls>
+		);
+
+		if ( editing ) {
 			return [
 				controls,
-				<figure key="video" className={ className }>
-					<video controls src={ src } />
-					{ ( ( caption && caption.length ) || isSelected ) && (
-						<RichText
-							tagName="figcaption"
-							placeholder={ __( 'Write caption…' ) }
-							value={ caption }
-							onChange={ ( value ) => setAttributes( { caption: value } ) }
-							isSelected={ isSelected }
-							inlineToolbar
-						/>
-					) }
-				</figure>,
+				<Placeholder
+					key="placeholder"
+					icon="media-video"
+					label={ __( 'Video' ) }
+					instructions={ __( 'Select a video file from your library, or upload a new one' ) }
+					className={ className }>
+					<form onSubmit={ onSelectUrl }>
+						<input
+							type="url"
+							className="components-placeholder__input"
+							placeholder={ __( 'Enter URL of video file here…' ) }
+							onChange={ event => setState( { src: event.target.value } ) }
+							value={ src || '' } />
+						<Button
+							isLarge
+							type="submit">
+							{ __( 'Use URL' ) }
+						</Button>
+					</form>
+					<MediaUpload
+						onSelect={ onSelectVideo }
+						type="video"
+						id={ id }
+						render={ ( { open } ) => (
+							<Button isLarge onClick={ open } >
+								{ __( 'Add from Media Library' ) }
+							</Button>
+						) }
+					/>
+				</Placeholder>,
 			];
-			/* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
 		}
-	},
+
+		/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+		return [
+			controls,
+			<figure key="video" className={ className }>
+				<video controls src={ src } />
+				{ ( ( caption && caption.length ) || isSelected ) && (
+					<RichText
+						tagName="figcaption"
+						placeholder={ __( 'Write caption…' ) }
+						value={ caption }
+						onChange={ ( value ) => setAttributes( { caption: value } ) }
+						isSelected={ isSelected }
+						inlineToolbar
+					/>
+				) }
+			</figure>,
+		];
+		/* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+	} ),
 
 	save( { attributes } ) {
 		const { src, caption, align } = attributes;
