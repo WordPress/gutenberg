556a7682d0649a065a74c57cf2b034f1fc7dc432
Ajit Bohra <ajit@lubus.in>
2018-02-10 11:36:52 +0530

    Fix 4273

---
+++ editor/components/no-blocks/index.js
@@ -0,0 +1,14 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+function NoBlocks() {
+	return (
+		<span className="editor-inserter__no-results">
+			{__('No blocks found')}
+		</span>
+	);
+}
+
+export default NoBlocks;
