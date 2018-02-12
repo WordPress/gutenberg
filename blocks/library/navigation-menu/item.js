8a8f88cec5bc9308b6e456a430e0cb18f9b54cde
Kelly Dwan <ryelle@users.noreply.github.com>
2018-02-12 17:27:39 -0500

    Add the has-children class to items

--- blocks/library/navigation-menu/item.js
+++ blocks/library/navigation-menu/item.js
@@ -13,9 +13,12 @@ import { decodeEntities } from '@wordpress/utils';
 class MenuItem extends Component {
 	render() {
 		const { children, item } = this.props;
+		const classes = classnames( item.classes, {
+			'menu-item-has-children': !! children,
+		} );
 
 		return (
-			<li className={ classnames( item.classes ) }>
+			<li className={ classes }>
 				<a title={ item.title } rel={ item.xfn } href={ item.url } target="_blank">
 					{ decodeEntities( item.title.trim() ) || __( '(Untitled)' ) }
 				</a>
