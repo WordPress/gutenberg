a81ad0162b7131e90dbd2e48f932dab12bc1411d
Alexander Botteram <alexander@yoast.com>
2018-02-12 15:24:48 +0100

    Implemented getEditedPostAttributes

--- editor/store/index.js
+++ editor/store/index.js
@@ -10,6 +10,7 @@ import reducer from './reducer';
 import applyMiddlewares from './middlewares';
 import {
 	getEditedPostAttribute,
+	getEditedPostAttributes,
 	getSelectedBlockCount,
 } from './selectors';
 
@@ -26,6 +27,7 @@ loadAndPersist( store, reducer, 'preferences', STORAGE_KEY );
 
 registerSelectors( MODULE_KEY, {
 	getEditedPostAttribute,
+	getEditedPostAttributes,
 	getSelectedBlockCount,
 } );
 
