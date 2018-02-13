cdfc5a602fb63a1132d6a3e48bf049dc00a61ea9
IreneStr <irene@yoast.com>
2018-02-13 10:25:54 +0100

    Set editor as default activeGeneralSidebar

--- edit-post/store/defaults.js
+++ edit-post/store/defaults.js
@@ -1,7 +1,7 @@
 export const PREFERENCES_DEFAULTS = {
 	editorMode: 'visual',
 	viewportType: 'desktop', // 'desktop' | 'mobile'
-	activeGeneralSidebar: null, // null | 'editor' | 'plugin'
+	activeGeneralSidebar: editor, // null | 'editor' | 'plugin'
 	activeSidebarPanel: { // The keys in this object should match activeSidebarPanel values
 		editor: null, // 'document' | 'block'
 		plugin: null, // pluginId
