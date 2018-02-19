4178f8c04cee0b725ac3966688b3ce833fab48b1
Alexander Botteram <alexander@yoast.com>
2018-02-19 16:27:02 +0100

    Implemented test for editor-menu-item.js

--- edit-post/api/test/editor-emnu-item.js
+++ edit-post/api/test/editor-emnu-item.js
@@ -1,16 +1,26 @@
 import '@wordpress/jest-console';
 
-let registerEditorMenuItem, getEditorMenuItems;
+let registerEditorMenuItem, getEditorMenuItems, registerPlugin;
 function requireAll() {
-	const core = require( '../editor-menu-item' );
-	registerEditorMenuItem = core.registerEditorMenuItem;
-	getEditorMenuItems = core.getEditorMenuItems;
+	jest.resetModules();
+	const editorMenuItem = require( '../editor-menu-item' );
+	registerEditorMenuItem = editorMenuItem.registerEditorMenuItem;
+	getEditorMenuItems = editorMenuItem.getEditorMenuItems;
+	const core = require( '../plugins-core' );
+	registerPlugin = core.registerPlugin;
 }
 
 requireAll();
 
 describe( 'registerEditorMenuItem', () => {
-} );
+	beforeEach( requireAll );
 
-describe( 'getEditorMenuItems', () => {
+	it( 'successfully registers a editor menu item', () => {
+		registerPlugin( 'gutenberg/plugin', () => {} );
+		registerEditorMenuItem( 'gutenberg/plugin', {
+			title: 'Plugin',
+			target: 'gutenberg/plugin',
+		} );
+		expect( getEditorMenuItems()[ 'gutenberg/plugin' ] ).toBeDefined();
+	} );
 } );
