a19408037e7439c1f57c3cba0a4476d41ada0a99
Ajit Bohra <ajit@lubus.in>
2018-02-20 03:20:14 +0530

    Refactor & Cleanup

--- editor/components/inserter/no-blocks.js
+++ editor/components/inserter/no-blocks.js
@@ -3,8 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 
-function NoBlocks( props ) {
-	const { children } = props;
+function NoBlocks( { children } ) {
 	return (
 		<span className="editor-inserter__no-blocks">
 			{ !! children ? children : __( 'No blocks found' ) }
