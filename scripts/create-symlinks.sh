05e178ce14014112c82a6e9db874d47df78985d8
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2018-02-20 07:37:29 +0100

    Build: Use node script to symlink local npm dependencies (#79)
    * Build: Use node script to symlink local npm dependencies
    
    * Chore: Update symlink-or-copy to v1.2.0
    
    * Chore: Added npm-run-all to simplify npm scripts

--- scripts/create-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p node_modules/@wordpress
-ln -sf ../../packages/browserslist-config node_modules/@wordpress
-ln -sf ../../packages/babel-preset-default node_modules/@wordpress
-ln -sf ../../packages/jest-preset-default node_modules/@wordpress
-ln -sf ../../packages/scripts node_modules/@wordpress
-ln -sf ../@wordpress/scripts/bin/wp-scripts.js node_modules/.bin/wp-scripts
