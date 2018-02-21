c417b7ecc80e1779ae0411116cdfd514e17e6fce
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2018-02-21 12:28:50 +0100

    Update index.js

--- edit-post/components/plugins-panel/index.js
+++ edit-post/components/plugins-panel/index.js
@@ -17,37 +17,18 @@ import { getSidebarSettings } from '../../api/sidebar';
 import { getActivePlugin } from '../../store/selectors';
 import { closeGeneralSidebar } from '../../store/actions';
 
-/**
- * Returns the sidebar that should be rendered in the sidebar registered by
- * plugins.
- *
- * @param {string} plugin The currently active plugin.
- *
- * @return {Object} The React element to render as a panel.
- */
-export function getPluginSidebar( plugin ) {
+function PluginsPanel( { onClose, plugin } ) {
 	const pluginSidebar = getSidebarSettings( plugin );
 
 	if ( ! pluginSidebar ) {
-		return {
-			title: __( 'Error: Unregistered plugin requested.' ),
-			render: () => {
-				return <Panel>
-					<PanelBody>
-						{ sprintf( __( 'No matching plugin sidebar found for plugin "%s"' ), plugin ) }
-					</PanelBody>
-				</Panel>;
-			},
-		};
+		return null;
 	}
-	return pluginSidebar;
-}
 
-function PluginsPanel( { onClose, plugin } ) {
 	const {
 		title,
 		render,
-	} = getPluginSidebar( plugin );
+	} = pluginSidebar;
+
 	return (
 		<div
 			className="edit-post-sidebar edit-post-plugins-panel"
