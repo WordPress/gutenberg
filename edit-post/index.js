5d8797f7862fe88c2d4e7623d89220e3b4c75a24
Marcus Kazmierczak <marcus@mkaz.com>
2018-02-21 10:28:18 -0800

    Create WP timezone and do not set as global default
    We do not want to set the moment global defaut timezone
    to the blog timezone, since that can effect other plugins
    or pieces that use moment and rely on it being set to the
    user's timezone (different than site's timezone)
    
    Create WP timezone based off either dateString or offset
    both are setting in dateSettings via get options. Then
    when using dates, we need to use the new timezone:
    
    moment.tz( 'WP' ) instead of just moment()

--- edit-post/index.js
+++ edit-post/index.js
@@ -23,19 +23,17 @@ export * from './api';
 
 // Configure moment globally
 moment.locale( dateSettings.l10n.locale );
-if ( dateSettings.timezone.string ) {
-	moment.tz.setDefault( dateSettings.timezone.string );
-} else {
-	const momentTimezone = {
-		name: 'WP',
-		abbrs: [ 'WP' ],
-		untils: [ null ],
-		offsets: [ -dateSettings.timezone.offset * 60 ],
-	};
-	const unpackedTimezone = moment.tz.pack( momentTimezone );
-	moment.tz.add( unpackedTimezone );
-	moment.tz.setDefault( 'WP' );
-}
+
+// create WP timezone based off dateSettings
+const offsets = ( dateSettings.timezone.string ) ?  moment.tz.zone( dateSettings.timezone.string ).offsets : [ -dateSettings.timezone.offset * 60 ];
+const momentTimezone = {
+	name: 'WP',
+	abbrs: [ 'WP' ],
+	untils: [ null ],
+	offsets: [ offsets ],
+};
+const unpackedTimezone = moment.tz.pack( momentTimezone );
+moment.tz.add( unpackedTimezone );
 
 /**
  * Configure heartbeat to refresh the wp-api nonce, keeping the editor
