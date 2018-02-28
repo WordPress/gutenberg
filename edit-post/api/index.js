e55bdcc96b828393614e5449a435c282237b6386
Alexander Botteram <alexander@yoast.com>
2018-02-28 16:02:00 +0100

    Changed __experimentalRegisterMoreMenuItem to experimental__registerMoreMenuItem

--- edit-post/api/index.js
+++ edit-post/api/index.js
@@ -1,5 +1,7 @@
+/* eslint camelcase: 0 */
+
 export {
-	registerMoreMenuItem as __experimentalRegisterMoreMenuItem,
+	registerMoreMenuItem as experimental__registerMoreMenuItem,
 } from './more-menu-item';
 export {
 	registerSidebar,
