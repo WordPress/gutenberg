d8a10b0e04b57c4d80c7e42105410ea838419a38
Andrew Duthie <andrew@andrewduthie.com>
2018-02-28 09:02:41 -0500

    Build: Include string extraction in production build

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -79,8 +79,6 @@ status "Installing dependencies..."
 npm install
 status "Generating build..."
 npm run build
-status "Generating translation messages..."
-npm run gettext-strings
 status "Generating PHP file for wordpress.org to parse translations..."
 npm run pot-to-php
 
