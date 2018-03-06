6aadbee667c355a1e64a893cfdcbba26d5148fcc
Alain Schlesser <alain.schlesser@gmail.com>
2018-03-06 12:53:16 +0100

    Add test to assert that show_ui: false hides taxonomies

--- editor/components/post-taxonomies/test/index.js
+++ editor/components/post-taxonomies/test/index.js
@@ -68,4 +68,42 @@ describe( 'PostTaxonomies', () => {
 
 		expect( wrapperTwo.at( 0 ) ).toHaveLength( 2 );
 	} );
+
+	it( 'should not render taxonomy components that hide their ui', () => {
+		const genresTaxonomy = {
+			name: 'Genres',
+			slug: 'genre',
+			types: [ 'book' ],
+			hierarchical: true,
+			rest_base: 'genres',
+			visibility: {
+				show_ui: true,
+			},
+		};
+
+		const wrapperOne = shallow(
+			<PostTaxonomies postType="book"
+				taxonomies={ {
+					data: [ genresTaxonomy ],
+				} }
+			/>
+		);
+
+		expect( wrapperOne.at( 0 ) ).toHaveLength( 1 );
+
+		const wrapperTwo = shallow(
+			<PostTaxonomies postType="book"
+				taxonomies={ {
+					data: [
+						{
+							...genresTaxonomy,
+							visibility: { show_ui: false },
+						},
+					],
+				} }
+			/>
+		);
+
+		expect( wrapperTwo.at( 0 ) ).toHaveLength( 0 );
+	} );
 } );
