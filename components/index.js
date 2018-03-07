98ea5d2a03bb6c856bbc102c533fe62cea49c32f
Jorge <jorge.costa@developer.pt>
2018-03-07 14:25:29 +0000

    Renamed and moved QueryControls from blocks/QueryPanel to components/QueryControls. (#5319)
    This change also exposes the component to be available for general usage. A depency of applyWithAPIData was also removed from the component, because the component should not be the one dealing with data requests.

--- components/index.js
+++ components/index.js
@@ -31,6 +31,7 @@ export { default as PanelHeader } from './panel/header';
 export { default as PanelRow } from './panel/row';
 export { default as Placeholder } from './placeholder';
 export { default as Popover } from './popover';
+export { default as QueryControls } from './query-controls';
 export { default as RadioControl } from './radio-control';
 export { default as RangeControl } from './range-control';
 export { default as ResponsiveWrapper } from './responsive-wrapper';
