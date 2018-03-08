08cff749b299633c91769d62127b8868318f4ffe
twsp <twsp1996@gmail.com>
2018-03-08 18:54:37 -0500

    Added missing spaces around arguments

--- test/e2e/integration/005-splitting-merging-paragraph-blocks.js
+++ test/e2e/integration/005-splitting-merging-paragraph-blocks.js
@@ -8,19 +8,19 @@ describe( 'Adding nested blocks', () => {
 
 		// Insert paragraph block and split using Enter
 		cy.get( '.editor-default-block-appender' ).click();
-			cy.get( lastBlockSelector ).type( '{enter}' );
+		cy.get( lastBlockSelector ).type( '{enter}' );
 
 		// Assertion to check for two paragraph blocks
-		cy.get( '.mce-content-body' ).should( ($p) => {
-			expect( $p ).to.have.length(2);
+		cy.get( '.mce-content-body' ).should( ( $p ) => {
+			expect( $p ).to.have.length( 2 );
 		} );
 
 		// Merge second paragraph block back into first using Backspace
 		cy.get( lastBlockSelector ).type( '{backspace}' );
 
 		// Assertion to check for one paragraph block
-		cy.get( '.mce-content-body' ).should( ($p) => {
-		expect( $p ).to.have.length(1);
+		cy.get( '.mce-content-body' ).should( ( $p ) => {
+		expect( $p ).to.have.length( 1 );
 		} );
 	} );
 } );
