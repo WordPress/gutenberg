a9f9802d65503906a7b561e2687b06c0bbf8e91b
twsp <twsp1996@gmail.com>
2018-03-08 18:43:31 -0500

    Switched from spaces to tabs

--- test/e2e/integration/005-splitting-merging-paragraph-blocks.js
+++ test/e2e/integration/005-splitting-merging-paragraph-blocks.js
@@ -1,26 +1,26 @@
 describe( 'Adding nested blocks', () => {
-    before( () => {
-        cy.newPost();
-    } );
+	before( () => {
+		cy.newPost();
+	} );
 
-    it( 'Should split and merge paragraph blocks using Enter and Backspace', () => {
-        const lastBlockSelector = '.editor-block-list__block-edit:last [contenteditable="true"]:first';
+	it( 'Should split and merge paragraph blocks using Enter and Backspace', () => {
+		const lastBlockSelector = '.editor-block-list__block-edit:last [contenteditable="true"]:first';
 
-        // Insert paragraph block and split using Enter
-        cy.get( '.editor-default-block-appender' ).click();
-		cy.get( lastBlockSelector ).type( '{enter}' );
+		// Insert paragraph block and split using Enter
+		cy.get( '.editor-default-block-appender' ).click();
+			cy.get( lastBlockSelector ).type( '{enter}' );
 
-        // Assertion to check for two paragraph blocks
-        cy.get( '.mce-content-body' ).should( ($p) => {
-            expect( $p ).to.have.length(2);
-        } );
+		// Assertion to check for two paragraph blocks
+		cy.get( '.mce-content-body' ).should( ($p) => {
+			expect( $p ).to.have.length(2);
+		} );
 
-        // Merge second paragraph block back into first using Backspace
-        cy.get( lastBlockSelector ).type( '{backspace}' );
+		// Merge second paragraph block back into first using Backspace
+		cy.get( lastBlockSelector ).type( '{backspace}' );
 
-        // Assertion to check for one paragraph block
-        cy.get( '.mce-content-body' ).should( ($p) => {
-            expect( $p ).to.have.length(1);
-        } );
-    } );
+		// Assertion to check for one paragraph block
+		cy.get( '.mce-content-body' ).should( ($p) => {
+		expect( $p ).to.have.length(1);
+		} );
+	} );
 } );
