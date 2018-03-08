f9a478f54a8a6a53ea9edd27f1ece44567f8f69c
Tim Platts <twsp1996@gmail.com>
2018-03-08 17:22:37 -0500

    Added e2e test for splitting/merging paragraph blocks using Enter/Backspace

---
+++ test/e2e/integration/005-splitting-merging-paragraph-blocks.js
@@ -0,0 +1,26 @@
+describe( 'Adding nested blocks', () => {
+    before( () => {
+        cy.newPost();
+    } );
+
+    it( 'Should split and merge paragraph blocks using Enter and Backspace', () => {
+        const lastBlockSelector = '.editor-block-list__block-edit:last [contenteditable="true"]:first';
+
+        // Insert paragraph block and split using Enter
+        cy.get( '.editor-default-block-appender' ).click();
+		cy.get( lastBlockSelector ).type( '{enter}' );
+
+        // Assertion
+        cy.get( '.mce-content-body' ).should( ($p) => {
+            expect( $p ).to.have.length(2);
+        } );
+
+        // Merge second paragraph block back into first using Backspace
+        cy.get( lastBlockSelector ).type( '{backspace}' );
+
+        // Assertion
+        cy.get( '.mce-content-body' ).should( ($p) => {
+            expect( $p ).to.have.length(1);
+        } );
+    } );
+} );
