0c127e1a2729f6fa94f89d65d42f8e031bc9686a
Miina Sikk <miina.sikk@gmail.com>
2018-03-14 11:12:22 +0200

    Remove debugger line.

--- components/server-side-render/index.js
+++ components/server-side-render/index.js
@@ -26,7 +26,6 @@ export class ServerSideRender extends Component {
 
 	componentWillReceiveProps( nextProps ) {
 		if ( JSON.stringify( nextProps ) !== JSON.stringify( this.props ) ) {
-			debugger;
 			this.setState( { attributes: nextProps }, this.getOutput );
 		}
 	}
