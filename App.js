7425d99d7c7ff85d69089b01a614728dc8248ae6
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-03-15 17:52:25 +0200

    Load the Code block for android, test its rendering
    Use `yarn test-aandroid` to run the tests setting the Platform to
    "android"

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 // @flow
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { settings as codeBlock } from './gutenberg/blocks/library/code'
 
 export default class App extends React.Component<{}> {
   render() {
+    Code = codeBlock.edit;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Code
+          attributes={{content: 'Code block text'}}
+          setAttributes={ ( attrs ) => console.log( { attrs } ) }
+        />
       </View>
     );
   }
--- App.test.js
+++ App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+it('returns RN\'s View container and ', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered.type).toBe("View"); // parent container
+  expect(rendered.children[0].type).toBe("View"); // "Code" block
+});
