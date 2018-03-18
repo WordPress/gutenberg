712e5996af3b00e76b1b70d23aee0c3c42d33455
Andrew Duthie <andrew@andrewduthie.com>
2018-03-18 15:18:59 -0400

    Package: Add `@wordpress/custom-templated-path-webpack-plugin` package

---
+++ packages/custom-templated-path-webpack-plugin/test/fixtures/webpack.config.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+
+const { basename } = require( 'path' );
+
+/**
+ * Internal dependencies
+ */
+
+const CustomTemplatedPathPlugin = require( '../../' );
+
+module.exports = {
+	mode: 'development',
+	context: __dirname,
+	entry: './entry',
+	output: {
+		filename: '[basename].js',
+		path: __dirname,
+	},
+	plugins: [
+		new CustomTemplatedPathPlugin( {
+			basename( path, data ) {
+				console.log(data)
+
+				let rawRequest;
+				if ( data && data.chunk && data.chunk.entryModule ) {
+					rawRequest = data.chunk.entryModule.rawRequest;
+				}
+
+				if ( rawRequest ) {
+					return basename( rawRequest );
+				}
+
+				return path;
+			},
+		} ),
+	],
+};
