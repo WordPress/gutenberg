5bd4b197300bb9e28e22526613c59602d77ae52c
Alexander Botteram <alexander@yoast.com>
2018-03-19 15:10:44 +0100

    Close plugin om click, added styling and active status

--- edit-post/components/plugin-more-menu-item/more-menu-item-layout.js
+++ edit-post/components/plugin-more-menu-item/more-menu-item-layout.js
@@ -1,4 +1,14 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { IconButton } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
@@ -9,10 +19,25 @@ import './style.scss';
  * @return {WPElement} More menu item layout.
  */
 function MoreMenuItemLayout( { title, icon, isActive, onClick } ) {
+	icon = icon ? (
+		<span className="edit-post-plugin-more-menu-item__more-menu-item-layout__icon-container">
+			{ icon }
+		</span>
+	) : null;
+	icon = isActive ? 'yes' : icon;
+
+	const className = classnames( 'edit-post-plugin-more-menu-item__more-menu-item-layout', {
+		'is-active': isActive,
+		'has-icon': icon,
+	} );
+
 	return (
-		<button onClick={ onClick } className="edit-post-plugin-more-menu-item__more-menu-item-layout">
+		<IconButton
+			icon={ icon }
+			onClick={ onClick }
+			className={ className }>
 			{ title }
-		</button>
+		</IconButton>
 	);
 }
 
