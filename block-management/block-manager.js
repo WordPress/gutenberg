36bab58a17f7d24e437d497f65a1308077f30e82
Maxime Biais <maxime.biais@gmail.com>
2018-03-19 10:45:25 +0100

    Fix flow and prettier issues

--- block-management/block-manager.js
+++ block-management/block-manager.js
@@ -1,23 +1,26 @@
-/** @format */
-
-// @flow
+/**
+ * @format
+ * @flow
+ */
 
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 
-export default class BlockManager extends React.Component {
-	constructor( props ) {
+export default class BlockManager extends React.Component<{}, { blocks: Array<string> }> {
+	constructor( props: {} ) {
 		super( props );
 		this.state = {
-			1: 'Johannes Gensfleisch zur Laden zum Gutenberg',
-			2: 'Born: ~1400',
-			3: 'Died: February 3, 1468 (aged about 68)',
-			4: 'Known for the invention of the movable-type printing press',
-			5: "The world's first movable type printing press technology for printing paper books was made of porcelain materials and was invented around AD 1040 in China during the Northern Song Dynasty by the inventor Bi Sheng (990–1051).",
-			6: 'Around 1450, Johannes Gutenberg introduced the metal movable-type printing press in Europe, along with innovations in casting the type based on a matrix and hand mould.',
-			7: 'The small number of alphabetic characters needed for European languages was an important factor.[3] Gutenberg was the first to create his type pieces from an alloy of lead, tin, and antimony—and these materials remained standard for 550 years.',
-			8: 'The concept of inkjet printing originated in the 20th century, and the technology was first extensively developed in the early 1950s',
-			9: '',
+			blocks: [
+				'Johannes Gensfleisch zur Laden zum Gutenberg',
+				'Born: ~1400',
+				'Died: February 3, 1468 (aged about 68)',
+				'Known for the invention of the movable-type printing press',
+				"The world's first movable type printing press technology for printing paper books was made of porcelain materials and was invented around AD 1040 in China during the Northern Song Dynasty by the inventor Bi Sheng (990–1051).",
+				'Around 1450, Johannes Gutenberg introduced the metal movable-type printing press in Europe, along with innovations in casting the type based on a matrix and hand mould.',
+				'The small number of alphabetic characters needed for European languages was an important factor.[3] Gutenberg was the first to create his type pieces from an alloy of lead, tin, and antimony—and these materials remained standard for 550 years.',
+				'The concept of inkjet printing originated in the 20th century, and the technology was first extensively developed in the early 1950s',
+				'',
+			],
 		};
 	}
 
@@ -44,14 +47,18 @@ export default class BlockManager extends React.Component {
 		);
 	}
 
-	renderItem( value ) {
+	renderItem( value: { item: { key: number } } ) {
 		var key = value.item.key;
 		return (
 			<TextInput
 				style={ styles.listItem }
 				multiline={ true }
-				value={ this.state[ key ] }
-				onChangeText={ content => this.setState( { [ `${ key }` ]: content } ) }
+				value={ this.state.blocks[ key ] }
+				onChangeText={ content => {
+					var blocks = this.state.blocks;
+					blocks[ key ] = content;
+					this.setState( { blocks: blocks } );
+				} }
 			/>
 		);
 	}
