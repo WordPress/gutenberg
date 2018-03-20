e37b37fe0d54d78ea85e2e53795c18f7d49f1c45
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-03-20 17:00:32 +0200

    Code block is used deeper, in the BlockManager

--- App.js
+++ App.js
@@ -3,16 +3,10 @@
 // @flow
 
 import React from 'react';
-import { settings as codeBlock } from './gutenberg/blocks/library/code'
 import BlockManager from './block-management/block-manager';
 
 export default class App extends React.Component<{}> {
 	render() {
-		Code = codeBlock.edit;
-        <Code
-          attributes={{content: 'Code block text'}}
-          setAttributes={ ( attrs ) => console.log( { attrs } ) }
-        />
 		return <BlockManager />;
 	}
 }
