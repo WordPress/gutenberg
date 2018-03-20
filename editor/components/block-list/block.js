d4aa90530aba9f588a02717983bdafeceaa00029
Riad Benguella <benguella@gmail.com>
2018-03-20 13:19:18 +0100

    Fix in-between inserter

--- editor/components/block-list/block.js
+++ editor/components/block-list/block.js
@@ -106,7 +106,7 @@ export class BlockListBlock extends Component {
 
 	componentDidMount() {
 		if ( this.props.isSelected ) {
-			this.focusTabbable();
+			this.focusTabbable( true );
 		}
 	}
 
@@ -145,8 +145,10 @@ export class BlockListBlock extends Component {
 
 	/**
 	 * When a block becomes selected, transition focus to an inner tabbable.
+	 *
+	 * @param {boolean} forceInnerFocus Whether or not to force focus to inner content
 	 */
-	focusTabbable() {
+	focusTabbable( forceInnerFocus = false ) {
 		const { initialPosition, keyboardMode } = this.props;
 
 		// Focus is captured by the wrapper node, so while focus transition
@@ -159,7 +161,7 @@ export class BlockListBlock extends Component {
 
 		// In navigation mode, we should select the parent node only
 		// This could be triggered when we remove a block from navigation mode (backspace)
-		if ( keyboardMode === 'navigation' ) {
+		if ( ! forceInnerFocus && keyboardMode === 'navigation' ) {
 			this.wrapperNode.focus();
 			return;
 		}
