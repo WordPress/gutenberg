9ac0943929e4c86fbfe3ea90b8cab2fcfb498f3c
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-03-20 15:23:23 +0200

    Use the codestyle

--- App.test.js
+++ App.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
 
-it('returns RN\'s View container and ', () => {
+it("returns RN's View container and ", () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered.type).toBe("View"); // parent container
   expect(rendered.children[0].type).toBe("View"); // "Code" block
