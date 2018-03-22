397d36ea2bf34d4aa40d9f721ed8337d16dc1bc1
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-03-22 13:24:31 +0100

    Tools: Use Eslint overrides for e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ const { version } = require( './package' );
 /**
  * Regular expression string matching a SemVer string with equal major/minor to
  * the current package version. Used in identifying deprecations.
- *n
+ *
  * @type {string}
  */
 const majorMinorRegExp = escapeRegExp( version.replace( /\.\d+$/, '' ) ) + '(\\.\\d+)?';
