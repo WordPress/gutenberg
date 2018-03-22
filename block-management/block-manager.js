bf315d7eceb664c2f354ab26c96ba29ba64ade11
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-03-22 14:48:28 +0200

    Define the blocks list outside the BlockManager

--- block-management/block-manager.js
+++ block-management/block-manager.js
@@ -8,59 +8,26 @@ import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 import BlockHolder from './block-holder';
 import { ToolbarButton } from './constants';
 
-import { registerCoreBlocks } from '@gutenberg/blocks/library';
-
-type PropsType = {};
+export type BlockArray = Array<{
+	key: string,
+	blockType: string,
+	content: string,
+	focused: boolean,
+}>;
+type PropsType = {
+	blocks: BlockArray,
+};
 type StateType = {
 	refresh: boolean,
-	blocks: Array<{ key: string, blockType: string, content: string, focused: boolean }>,
 };
 
 export default class BlockManager extends React.Component<PropsType, StateType> {
 	constructor( props: PropsType ) {
 		super( props );
 
-		registerCoreBlocks();
-
-		// TODO: block state should be externalized (shared with Gutenberg at some point?).
-		// If not it should be created from a string parsing (commented HTML to json).
 		this.state = {
 			refresh: false,
-			blocks: [
-				{
-					key: '0',
-					blockType: 'title',
-					content: 'Hello World',
-					focused: false,
-				},
-				{
-					key: '1',
-					blockType: 'paragraph',
-					content:
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor tincidunt sapien, quis dictum orci sollicitudin quis. Proin sed elit id est pulvinar feugiat vitae eget dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-					focused: false,
-				},
-				{
-					key: '2',
-					blockType: 'paragraph',
-					content:
-						'書籍やウェブページや広告などのデザインのプロトタイプを制作したり顧客にプレゼンテーションしたりする際に、まだ正式な文章の出来上がっていないテキスト部分の書体（フォント）、タイポグラフィ、レイアウトなどといった視覚的なデザインを調整したりわかりやすく見せるために用いられる。',
-					focused: false,
-				},
-				{
-					key: '3',
-					blockType: 'core/code',
-					content: 'if name == "World":\n    return "Hello World"\nelse:\n    return "Hello Pony"',
-					focused: false,
-				},
-				{
-					key: '4',
-					blockType: 'paragraph',
-					content:
-						'Лорем ипсум долор сит амет, адиписци трацтатос еа еум. Меа аудиам малуиссет те, хас меис либрис елеифенд ин. Нец ех тота деленит сусципит. Яуас порро инструцтиор но нец.',
-					focused: false,
-				},
-			],
+			blocks: [ ...this.props.blocks ],
 		};
 	}
 
