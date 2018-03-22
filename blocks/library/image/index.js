59f1dc6670861ed63228c1a8022f2c82cfab6513
Hendrik Luehrsen <hendrik@luehrsen.de>
2018-03-22 16:09:36 +0100

    Remove the inline style for aligned images

--- blocks/library/image/index.js
+++ blocks/library/image/index.js
@@ -174,8 +174,6 @@ export const settings = {
 
 		if ( width ) {
 			figureStyle = { width };
-		} else if ( align === 'left' || align === 'right' ) {
-			figureStyle = { maxWidth: '50%' };
 		}
 
 		return (
