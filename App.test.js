1a150384c703d518ded63ab43ccb4eb9d089c731
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-03-23 13:56:03 +0200

    Fix: correct block type in test

--- App.test.js
+++ App.test.js
@@ -19,7 +19,7 @@ describe( 'App', () => {
 			.create( <App /> )
 			.root.findAllByType( BlockHolder )
 			.forEach( blockHolder => {
-				if ( blockHolder.props.blockType === 'code' ) {
+				if ( blockHolder.props.blockType === 'core/code' ) {
 					// TODO: hardcoded indices are ugly and error prone. Can we do better here?
 					const blockHolderContainer = blockHolder.children[ 0 ].children[ 0 ].children[ 0 ];
 					const contentComponent = blockHolderContainer.children[ 1 ];
