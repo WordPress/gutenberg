4f1c501a64e64081bbbe0d7fa6e001ee6555c27f
Gary Pendergast <gary@pento.net>
2018-03-23 15:53:55 +1100

    Display the sample permalink

--- editor/components/post-permalink/index.js
+++ editor/components/post-permalink/index.js
@@ -9,6 +9,7 @@ import { connect } from 'react-redux';
 import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Dashicon, Button } from '@wordpress/components';
+import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal Dependencies
@@ -27,8 +28,37 @@ class PostPermalink extends Component {
 		this.onFinishCopy = this.onFinishCopy.bind( this );
 	}
 
+	componentWillMount() {
+		if ( this.props.samplePermalinkData ) {
+			this.setState( {
+				samplePermalink: this.generatePermalink( this.props.samplePermalinkData[0], this.props.samplePermalinkData[1] ),
+			} );
+		}
+	}
+
 	componentWillUnmount() {
 		clearTimeout( this.dismissCopyConfirmation );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		console.log( nextProps );
+		if ( this.props.samplePermalinkData !== nextProps.samplePermalinkData ) {
+			this.setState( {
+				samplePermalink: this.generatePermalink( nextProps.samplePermalinkData[0], nextProps.samplePermalinkData[1] ),
+			} );
+		}
+	}
+
+	/**
+	 * Replace the %postname% and %pagename% tags in a sample permalink URL with actual values.
+	 *
+	 * @param {string} template  Permalink structure to replace the tags in.
+	 * @param {string} post_name The post slug to use in replacements.
+	 *
+	 * @return {string} Prepared permalink.
+	 */
+	generatePermalink( template, post_name ) {
+		return template.replace( /%(postname|pagename)%/, post_name );
 	}
 
 	onCopy() {
@@ -45,17 +75,27 @@ class PostPermalink extends Component {
 
 	render() {
 		const { isNew, link, permalinkStructure } = this.props;
+		const { samplePermalink } = this.state;
+
 		if ( isNew || ! link ) {
 			return null;
+		}
+
+		var hrefLink = link;
+		var displayLink = decodeURI( link );
+		if ( samplePermalink ) {
+			hrefLink = addQueryArgs( hrefLink, { preview: true } );
+			displayLink = samplePermalink;
 		}
 
 		return (
 			<div className="editor-post-permalink">
 				<Dashicon icon="admin-links" />
 				<span className="editor-post-permalink__label">{ __( 'Permalink:' ) }</span>
-				<Button className="editor-post-permalink__link" href={ link } target="_blank">
-					{ decodeURI( link ) }
+				<Button className="editor-post-permalink__link" href={ hrefLink } target="_blank">
+					{ samplePermalink }
 				</Button>
+
 				{ ! permalinkStructure &&
 					<Button
 						className="editor-post-permalink__change button"
@@ -75,6 +115,7 @@ export default connect(
 		return {
 			isNew: isEditedPostNew( state ),
 			link: getEditedPostAttribute( state, 'link' ),
+			samplePermalinkData: getEditedPostAttribute( state, 'sample_permalink' ),
 
 			permalinkStructure: window.wpApiSettings.schema.permalink_structure,
 		};
