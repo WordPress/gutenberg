2d14dfefbaf999d3f853139edf9f884abbae81e9
Alexander Botteram <alexander@yoast.com>
2018-03-26 10:28:56 +0200

    Added withSelect to PluginArea component to rerender on plugin registration

--- plugins/components/plugin-area/index.js
+++ plugins/components/plugin-area/index.js
@@ -4,20 +4,24 @@
 import { map } from 'lodash';
 
 /**
+ * WordPress dependencies
+ */
+import { withSelect } from '@wordpress/data';
+
+/**
  * Internal dependencies
  */
 import PluginContextProvider from '../plugin-context-provider';
-import { getPlugins } from '../../api';
 
 /**
  * A component that renders all plugin fills in a hidden div.
  *
  * @return {WPElement} Plugin area.
  */
-function PluginArea() {
+function PluginArea( { plugins } ) {
 	return (
 		<div style={ { display: 'none' } }>
-			{ map( getPlugins(), ( plugin ) => {
+			{ map( plugins, ( plugin ) => {
 				const { render: Plugin } = plugin;
 
 				return (
@@ -33,4 +37,10 @@ function PluginArea() {
 	);
 }
 
-export default PluginArea;
+/**
+ * Using withSelect instead of internal ./api/index.getPlugins() to force rerender
+ * when a new plugin is registered.
+ */
+export default withSelect( select => ( {
+	plugins: select( 'core/plugins' ).getPlugins(),
+} ) )( PluginArea );
