017aa876914344a2971e4653af95ece9f1c6beb7
Andrew Duthie <andrew@andrewduthie.com>
2018-03-28 15:18:24 -0400

    Testing: Add environment variables for headless and slowMo
    Following pattern established by E2E_EXECUTABLE_PATH

--- test/e2e/support/bootstrap.js
+++ test/e2e/support/bootstrap.js
@@ -8,11 +8,15 @@ jest.setTimeout( 100000 );
 
 const {
 	E2E_EXECUTABLE_PATH,
+	E2E_HEADLESS,
+	E2E_SLOWMO = 0,
 } = process.env;
 
 beforeAll( async () => {
 	global.browser = await puppeteer.launch( {
 		executablePath: E2E_EXECUTABLE_PATH,
+		headless: 'false' !== E2E_HEADLESS,
+		slowMo: Number( E2E_SLOWMO ),
 	} );
 } );
 
