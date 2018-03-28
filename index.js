877ab80cab5492a18e4d95376448ef7034039608
Maxime Biais <maxime.biais@gmail.com>
2018-03-28 17:00:24 +0200

    Fix for unregistered component and unresolved dependency

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent( 'gutenbergmobile', () => App );
+AppRegistry.registerComponent( 'gutenberg', () => App );
