3894220110d93077c6b6897c35f867742ed76c9f
Chris Shultz <cs.shultz@gmail.com>
2018-03-27 21:12:12 -0700

    Extensibility: X-WP-Source='Gutenberg' header added
    As a method for differentiating requests from Gutenberg editor,
    a header keyed 'X-WP-Source' is being added and set to 'Gutenberg'.

--- lib/compat.php
+++ lib/compat.php
@@ -170,6 +170,7 @@ function gutenberg_shim_api_request_emulate_http( $scripts ) {
 					options.headers = {};
 				}
 				options.headers['X-HTTP-Method-Override'] = options.method;
+				options.headers['X-WP-Source'] = 'Gutenberg';
 				options.method = 'POST';
 
 				options.contentType = 'application/json';
