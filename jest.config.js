3d81a7c4c3b3123246e21f7982afcb8be7e023fa
Maxime Biais <maxime.biais@gmail.com>
2018-03-28 19:04:03 +0200

    Force testEnvironment ot jsdom to inject window

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ if ( process.env.TEST_RN_PLATFORM ) {
 
 module.exports = {
 	preset: 'jest-react-native',
+	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		'@wordpress/hooks': '<rootDir>/wordpress/hooks',
 		'@wordpress/i18n': '<rootDir>/gutenberg/i18n',
