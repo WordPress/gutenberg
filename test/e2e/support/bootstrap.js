ee2e2719f0d5a500e1a19eb0ccfae152d70878f4
Andrew Duthie <andrew@andrewduthie.com>
2018-03-28 15:18:02 -0400

    Testing: Install Chrome Puppeteer executable as addon

--- test/e2e/support/bootstrap.js
+++ test/e2e/support/bootstrap.js
@@ -6,8 +6,14 @@ import puppeteer from 'puppeteer';
 // The Jest timeout is increased because these tests are a bit slow
 jest.setTimeout( 100000 );
 
+const {
+	E2E_EXECUTABLE_PATH,
+} = process.env;
+
 beforeAll( async () => {
-	global.browser = await puppeteer.launch();
+	global.browser = await puppeteer.launch( {
+		executablePath: E2E_EXECUTABLE_PATH,
+	} );
 } );
 
 afterAll( async () => {
