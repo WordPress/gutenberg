e83cfa3f02bbf8f95d501cb44a30c9e906936461
twsp <twsp1996@gmail.com>
2018-03-29 20:04:14 -0400

    Fixed formatting

--- test/e2e/specs/splitting-merging.test.js
+++ test/e2e/specs/splitting-merging.test.js
@@ -10,46 +10,46 @@ describe( 'splitting and merging blocks', () => {
 		await newPost();
 	} );
 
-    it ( 'Should split and merge paragraph blocks using Enter and Backspace', async () => {
-        //Use regular inserter to add paragraph block and text
-        await page.click( '.edit-post-header [aria-label="Add block"]' );
-        await page.keyboard.type( 'paragraph' );
-        await page.keyboard.press( 'Tab' );
-        await page.keyboard.press( 'Enter' );
-        await page.keyboard.type( 'FirstSecond' );
+	it ( 'Should split and merge paragraph blocks using Enter and Backspace', async () => {
+		//Use regular inserter to add paragraph block and text
+		await page.click( '.edit-post-header [aria-label="Add block"]' );
+		await page.keyboard.type( 'paragraph' );
+		await page.keyboard.press( 'Tab' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( 'FirstSecond' );
 
-        //Move caret between 'First' and 'Second' and press Enter to split paragraph blocks
-        for ( let i = 0; i < 6; i++ ) {
-            await page.keyboard.press( 'ArrowLeft' );
-        }
-        await page.keyboard.press( 'Enter' );
+		//Move caret between 'First' and 'Second' and press Enter to split paragraph blocks
+		for ( let i = 0; i < 6; i++ ) {
+			await page.keyboard.press( 'ArrowLeft' );
+		}
+		await page.keyboard.press( 'Enter' );
 
-        //Switch to Code Editor to check HTML output
-        await page.click( '.edit-post-more-menu [aria-label="More"]' );
-        let codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
-        await codeEditorButton.click( 'button' );
+		//Switch to Code Editor to check HTML output
+		await page.click( '.edit-post-more-menu [aria-label="More"]' );
+		let codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
+		await codeEditorButton.click( 'button' );
 
-        //Assert that there are now two paragraph blocks with correct content
-        let textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
-        expect( textEditorContent ).toMatchSnapshot();
+		//Assert that there are now two paragraph blocks with correct content
+		let textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
+		expect( textEditorContent ).toMatchSnapshot();
 
-        //Switch to Visual Editor to continue testing
-        await page.click( '.edit-post-more-menu [aria-label="More"]' );
-        let visualEditorButton = ( await page.$x( '//button[contains(text(), \'Visual Editor\')]' ) )[ 0 ];
-        await visualEditorButton.click( 'button' );
+		//Switch to Visual Editor to continue testing
+		await page.click( '.edit-post-more-menu [aria-label="More"]' );
+		let visualEditorButton = ( await page.$x( '//button[contains(text(), \'Visual Editor\')]' ) )[ 0 ];
+		await visualEditorButton.click( 'button' );
 
-        //Press Backspace to merge paragraph blocks
-        await page.click( '.is-selected' );
-        await page.keyboard.press( 'Home' );
-        await page.keyboard.press( 'Backspace' );
+		//Press Backspace to merge paragraph blocks
+		await page.click( '.is-selected' );
+		await page.keyboard.press( 'Home' );
+		await page.keyboard.press( 'Backspace' );
 
-        //Switch to Code Editor to check HTML output
-        await page.click( '.edit-post-more-menu [aria-label="More"]' );
-        codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
-        await codeEditorButton.click( 'button' );
+		//Switch to Code Editor to check HTML output
+		await page.click( '.edit-post-more-menu [aria-label="More"]' );
+		codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
+		await codeEditorButton.click( 'button' );
 
-        //Assert that there is now one paragraph with correct content
-        textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
-        expect( textEditorContent ).toMatchSnapshot();
-    } );
+		//Assert that there is now one paragraph with correct content
+		textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
+		expect( textEditorContent ).toMatchSnapshot();
+	} );
 } );
