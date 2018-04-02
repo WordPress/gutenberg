9113fcff45987e8a96c881aab734ca5ec83609f5
Andrew Duthie <andrew@andrewduthie.com>
2018-04-02 09:07:05 -0400

    Terms: Fetch terms as comma separated
    Separated `include` arguments is invalid, and would return only the last of the set of parameters requested for include

--- editor/components/post-taxonomies/flat-term-selector.js
+++ editor/components/post-taxonomies/flat-term-selector.js
@@ -46,7 +46,10 @@ class FlatTermSelector extends Component {
 	componentDidMount() {
 		if ( this.props.terms ) {
 			this.setState( { loading: false } );
-			this.initRequest = this.fetchTerms( { include: this.props.terms, per_page: 100 } );
+			this.initRequest = this.fetchTerms( {
+				include: this.props.terms.join( ',' ),
+				per_page: 100,
+			} );
 			this.initRequest.then(
 				() => {
 					this.setState( { loading: false } );
