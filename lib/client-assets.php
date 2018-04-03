15714ef291c87901c487f2f5a97891424a5bc582
Gary Pendergast <gary@pento.net>
2018-04-03 19:18:24 +1000

    Defer more scripts

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -986,8 +986,22 @@ JS;
  * @return string The <script> tag with the defer property added.
  */
 function gutenberg_editor_defer( $tag, $handle ) {
-	if ( 'wp-edit-post' === $handle ) {
-		return str_replace( ' src', ' defer="defer" src', $tag );
+	$async_handles = array(
+		'wp-blocks',
+		'wp-components',
+		'wp-core-data',
+		'wp-data',
+		'wp-edit-post',
+		'wp-editor',
+		'wp-element',
+		'wp-hooks',
+		'wp-plugins',
+		'wp-utils',
+		'wp-viewport',
+	);
+
+	if ( in_array( $handle, $async_handles, true ) ) {
+		return str_replace( ' src', ' defer src', $tag );
 	}
 
 	return $tag;
