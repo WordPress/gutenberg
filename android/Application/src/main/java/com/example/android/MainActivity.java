db2dd000dd7f7c6eec56a6436304279358ce65c3
Mario Zorz <mariozorz@gmail.com>
2018-04-03 14:19:17 -0300

    removed commented code

--- android/Application/src/main/java/com/example/android/MainActivity.java
+++ android/Application/src/main/java/com/example/android/MainActivity.java
@@ -63,43 +63,6 @@ public class MainActivity extends SampleActivityBase {
         return super.onKeyUp(keyCode, event);
     }
 
-
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
-        logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
-        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
-
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.menu_toggle_log:
-                mLogShown = !mLogShown;
-                ViewAnimator output = (ViewAnimator) findViewById(R.id.sample_output);
-                if (mLogShown) {
-                    output.setDisplayedChild(1);
-                } else {
-                    output.setDisplayedChild(0);
-                }
-                supportInvalidateOptionsMenu();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    */
-
     /** Create a chain of targets that will receive log data */
     @Override
     public void initializeLogging() {
@@ -112,11 +75,6 @@ public class MainActivity extends SampleActivityBase {
         MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
         logWrapper.setNext(msgFilter);
 
-        /* On screen logging via a fragment with a TextView.
-        LogFragment logFragment = (LogFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.log_fragment);
-        msgFilter.setNext(logFragment.getLogView());
-*/
         Log.i(TAG, "Ready");
     }
 }
