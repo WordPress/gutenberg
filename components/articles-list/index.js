6b39272f38cc7637cb0c3c8d09ebd82b188cf507
Sofia Sousa <csofiamsousa@gmail.com>
2018-04-04 14:29:30 +0100

    Add style to articles-list component

--- components/articles-list/index.js
+++ components/articles-list/index.js
@@ -1,23 +1,28 @@
 /**
- * External Dependeencies
+ * External Dependencies
  */
 import { map } from 'lodash';
 
 /**
- * WordPress Dependeencies
+ * WordPress Dependencies
  */
 import { Component } from '@wordpress/element';
+
+/**
+ * Internal Dependencies
+ */
+import './style.scss';
 
 class ArticlesList extends Component {
 	renderArticles() {
 		return map( this.props.articles, article => {
-			return ( <li key={ article.id }>{ article.title.rendered }</li> );
+			return ( <li className="components-articles-list-item" key={ article.id }>{ article.title.rendered }</li> );
 		} );
 	}
 
 	render() {
 		return (
-			<ul>
+			<ul className="components-articles-list">
 				{ this.renderArticles() }
 			</ul>
 		);
