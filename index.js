8d736332a1d89a3ce31d5c2573a037dc5b605cad
Danilo Ercoli <ercoli@gmail.com>
2018-04-04 11:18:28 +0200

    Bring the FlatList back in action!

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
-import {AppRegistry, StyleSheet, View} from 'react-native';
+import {AppRegistry, StyleSheet, View, FlatList} from 'react-native';
 import RCTAztecView from './AztecView';
 
 class AztecTextInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true, height: 200};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {isShowingText: true, height: 200};
+    }
 
-  render() {
-
- let myMinHeight = Math.max(200, this.state.height);
-    return (
-          <View style={styles.container}>
-            <RCTAztecView
-                 {...this.props}
-                 style={[styles.hello, {minHeight: myMinHeight}]}
-                 multiline={true}
-                 text = {this.props.text}
-                 onScroll = {(event) => {
-                     console.log(event.nativeEvent);
-                 }}
-                 onContentSizeChange= {(event) => {
-                      this.setState({height: event.nativeEvent.contentSize.height});
-                  }}
-                 color = {'black'}
-                 maxImagesWidth = {200} />
-          </View>
-        );
-  }
+    render() {
+        let myMinHeight = Math.max(200, this.state.height);
+        return (
+              <View style={styles.container}>
+              <FlatList
+                    data={[
+                      {key: 'Stefanos'},
+                      {key: 'Mario'},
+                      {key: 'Danilo'},
+                      {key: 'Maxime'},
+                      {key: 'Koke'},
+                      {key: 'Ondrej'},
+                      {key: 'Cate'},
+                    ]}
+                    renderItem={({item}) =>
+                    <RCTAztecView
+                         {...this.props}
+                         style={[styles.hello, {minHeight: myMinHeight}]}
+                         multiline={true}
+                         text = {this.props.text}
+                         onScroll = {(event) => {
+                             console.log(event.nativeEvent);
+                         }}
+                         onContentSizeChange= {(event) => {
+                              this.setState({height: event.nativeEvent.contentSize.height});
+                          }}
+                         color = {'black'}
+                         maxImagesWidth = {200} />
+                    }
+                  />
+              </View>
+              );
+    }
 }
 
 var styles = StyleSheet.create({
