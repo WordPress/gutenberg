57f50ec4c49107452e46213317042318caaa4988
Riad Benguella <benguella@gmail.com>
2018-04-05 08:54:07 +0100

    Clarify refactoring post merge with a comment

--- edit-post/store/effects.js
+++ edit-post/store/effects.js
@@ -31,6 +31,8 @@ const effects = {
 		// Allow toggling metaboxes panels
 		// We need to wait for all scripts to load
 		// If the meta box loads the post script, it will already trigger this.
+		// After merge in Core, make sure to drop the timeout and update the postboxes script
+		// to avoid the double binding.
 		setTimeout( () => {
 			const postType = select( 'core/editor' ).getCurrentPostType();
 			if ( window.postboxes.page !== postType ) {
