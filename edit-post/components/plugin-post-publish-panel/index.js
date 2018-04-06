658fdc43a75ee82269db628e545b53a2a19aca76
Chris Shultz <cs.shultz@gmail.com>
2018-04-05 19:53:01 -0700

    Publish sidebar extensibility: utilizing new createSlotFill factory
    Replacing repetitious code with newly created createSlotFill factory function
    for PluginPrePublishPanel and PluginPostPublishPanel slots.
    
    props @gziolo

--- edit-post/components/plugin-post-publish-panel/index.js
+++ edit-post/components/plugin-post-publish-panel/index.js
@@ -5,45 +5,12 @@
  * (which appears after a user fully publishes a post).
  *
  * @file   This files defines the PluginPostPublishPanel extension
- * @since  2.6.0
+ * @since  2.7.0
  */
-
-/**
- * WordPress dependencies
- */
-import { Slot, Fill } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
+import { createSlotFill } from '../../../components/slot-fill';
 
-/**
- * Name of slot to fill.
- *
- * @type {String}
- */
-const SLOT_NAME = 'PluginPostPublishPanel';
-
-/**
- * Renders the plugin sidebar fill.
- *
- * @return {WPElement} Plugin sidebar fill.
- */
-function PluginPostPublishPanel( { children } ) {
-	return (
-		<Fill name={ SLOT_NAME } >
-			{ children }
-		</Fill>
-	);
-}
-
-/**
- * The plugin sidebar slot.
- *
- * @return {WPElement} The plugin sidebar slot.
- */
-PluginPostPublishPanel.Slot = () => (
-	<Slot name={ SLOT_NAME } />
-);
-
-export default PluginPostPublishPanel;
+export default createSlotFill( 'PluginPostPublishPanel' );
--- edit-post/components/plugin-pre-publish-panel/index.js
+++ edit-post/components/plugin-pre-publish-panel/index.js
@@ -5,45 +5,12 @@
  * (which appears when a user pushes "Publish" from the main editor).
  *
  * @file   This files defines the PluginPrePublishPanel extension
- * @since  2.6.0
+ * @since  2.7.0
  */
-
-/**
- * WordPress dependencies
- */
-import { Slot, Fill } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
+import { createSlotFill } from '../../../components/slot-fill';
 
-/**
- * Name of slot to fill.
- *
- * @type {String}
- */
-const SLOT_NAME = 'PluginPrePublishPanel';
-
-/**
- * Renders the plugin sidebar fill.
- *
- * @return {WPElement} Plugin sidebar fill.
- */
-function PluginPrePublishPanel( { children } ) {
-	return (
-		<Fill name={ SLOT_NAME } >
-			{ children }
-		</Fill>
-	);
-}
-
-/**
- * The plugin sidebar slot.
- *
- * @return {WPElement} The plugin sidebar slot.
- */
-PluginPrePublishPanel.Slot = () => (
-	<Slot name={ SLOT_NAME } />
-);
-
-export default PluginPrePublishPanel;
+export default createSlotFill( 'PluginPrePublishPanel' );
