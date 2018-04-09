6f2f0f2f5f200ea8b20a6fc35b6a0b03be838962
Maxime Biais <maxime.biais@gmail.com>
2018-04-09 16:06:24 +0200

    Add read only text to the more tag

--- core-blocks/more/edit.native.js
+++ core-blocks/more/edit.native.js
@@ -8,9 +8,13 @@ import { View, Text } from 'react-native';
 import { __ } from '../../../i18n';
 
 export function edit( { attributes, setAttributes, isSelected } ) {
+  const { customText, noTeaser } = attributes;
+  const defaultText = __( 'Read more' );
+  const value = customText !== undefined ? customText : defaultText;
+
   return (
-    <View style={{ alignItems: 'center'}}>
-      <Text>&lt;!-- More --&gt;</Text>
+    <View style={{ alignItems: 'center', padding: 4}}>
+      <Text>&lt;!-- More: { value } --&gt;</Text>
     </View>
   );
 }
