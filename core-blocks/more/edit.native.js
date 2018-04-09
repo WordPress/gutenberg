129bdfc4aafdc14cb58120b3c771fa3738e9ad15
Maxime Biais <maxime.biais@gmail.com>
2018-04-09 16:03:30 +0200

    Embryo of a RN more tag

---
+++ core-blocks/more/edit.native.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import { View, Text } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '../../../i18n';
+
+export function edit( { attributes, setAttributes, isSelected } ) {
+  return (
+    <View style={{ alignItems: 'center'}}>
+      <Text>&lt;!-- More --&gt;</Text>
+    </View>
+  );
+}
