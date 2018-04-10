9618e28c955a998ba848af05289ed3dcd94443b5
iseulde <wp@iseulde.com>
2018-04-10 18:09:09 +0200

    Allow to change url, user and pass for e2e tests

--- test/e2e/support/utils.js
+++ test/e2e/support/utils.js
@@ -1,24 +1,34 @@
 /**
  * Node dependencies
  */
-import path from 'path';
-import url from 'url';
+import { join } from 'path';
+import { URL } from 'url';
 
-const BASE_URL = 'http://localhost:8888';
-const USERNAME = 'admin';
-const PASSWORD = 'password';
+const {
+	WP_BASE_URL = 'http://localhost:8888',
+	WP_USERNAME = 'admin',
+	WP_PASSWORD = 'password',
+} = process.env;
+
 const NAVIGATION_TIMEOUT = 20000;
 
-function getUrl( urlPath ) {
-	return path.join( BASE_URL, urlPath );
+function getUrl( ...WPPaths ) {
+	const url = new URL( WP_BASE_URL );
+
+	url.pathname = join( url.pathname, ...WPPaths );
+
+	return url.href;
 }
 
-function getCurrentPathName() {
-	return url.parse( page.url() ).pathname;
+function isWPPath( ...WPPaths ) {
+	const currentUrl = new URL( page.url() );
+
+	currentUrl.search = '';
+
+	return getUrl( ...WPPaths ) === currentUrl.href;
 }
 
-async function gotoUrl( destUrl ) {
-	const promise = page.goto( destUrl, { timeout: 0 } );
+async function navigationTimeout( promise ) {
 	await new Promise( ( resolve ) => {
 		let resolved = false;
 		const markResolvedAndResolve = () => {
@@ -32,31 +42,21 @@ async function gotoUrl( destUrl ) {
 	} );
 }
 
+async function goToWPPath( ...WPPaths ) {
+	await navigationTimeout( page.goto( getUrl( ...WPPaths ), { timeout: 0 } ) );
+}
+
 async function login() {
-	if ( getCurrentPathName() !== '/wp-login.php' ) {
-		await gotoUrl( getUrl( 'wp-login.php' ) );
-	}
-
-	await page.type( '#user_login', USERNAME );
-	await page.type( '#user_pass', PASSWORD );
+	await page.type( '#user_login', WP_USERNAME );
+	await page.type( '#user_pass', WP_PASSWORD );
 	await page.click( '#wp-submit' );
-
-	await new Promise( ( resolve ) => {
-		let resolved = false;
-		const markResolvedAndResolve = () => {
-			if ( ! resolved ) {
-				resolve();
-				resolved = true;
-			}
-		};
-		setTimeout( markResolvedAndResolve, NAVIGATION_TIMEOUT );
-		page.waitForNavigation( { timeout: 0 } ).then( markResolvedAndResolve );
-	} );
+	await navigationTimeout( page.waitForNavigation( { timeout: 0 } ) );
 }
 
 export async function visitAdmin( adminPath ) {
-	await gotoUrl( path.join( BASE_URL, 'wp-admin', adminPath ) );
-	if ( getCurrentPathName() === '/wp-login.php' ) {
+	await goToWPPath( 'wp-admin', adminPath );
+
+	if ( isWPPath( 'wp-login.php' ) ) {
 		await login();
 		return visitAdmin( adminPath );
 	}
