3d7ab77fabda9d973080c6e56da39ff87fe62388
Maxime Biais <maxime.biais@gmail.com>
2018-04-10 13:34:24 +0200

    Add PlainText input to the more block

--- core-blocks/more/edit.native.js
+++ core-blocks/more/edit.native.js
@@ -7,14 +7,28 @@ import { View, Text } from 'react-native';
  */
 import { __ } from '../../../i18n';
 
+/**
+ * Internal dependencies
+ */
+import PlainText from '../../plain-text';
+
 export function edit( { attributes, setAttributes, isSelected } ) {
   const { customText, noTeaser } = attributes;
   const defaultText = __( 'Read more' );
   const value = customText !== undefined ? customText : defaultText;
 
   return (
-    <View style={{ alignItems: 'center', padding: 4}}>
-      <Text>&lt;!-- More: { value } --&gt;</Text>
-    </View>
-  );
+    <View style={{ padding: 4, alignItems: 'center' }}>
+      <View style={{ alignItems: 'center', flexDirection: "row"}}>
+        <Text style={{ fontFamily: 'monospace' }}>&lt;!--</Text>
+        <PlainText
+          value={ value }
+          multiline={ false }
+          underlineColorAndroid="transparent"
+          onChange={ content => setAttributes( { content } ) }
+          placeholder={ defaultText }
+        />
+        <Text style={{ fontFamily: 'monospace' }}>--&gt;</Text>
+      </View>
+    </View>);
 }
