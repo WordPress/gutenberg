55bba080e16726ee87d90ad480c6e27169d0469b
Maxime Biais <maxime.biais@gmail.com>
2018-04-11 10:23:41 +0200

    Remove unused imports and type

--- block-management/toolbar.js
+++ block-management/toolbar.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import { ToolbarButton } from './constants';
 
 type PropsType = {
