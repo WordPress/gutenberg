3e54504cc96f147f32a0d2aa07a1d7d4415c152f
vishalkakadiya <vishalkakadiya123@gmail.com>
2018-04-16 17:16:16 +0530

    Revert change

--- gutenberg.php
+++ gutenberg.php
@@ -109,7 +109,6 @@ function gutenberg_build_files_notice() {
  * @since 1.5.0
  */
 function gutenberg_pre_init() {
-	global $wp_version;
 
 	if ( defined( 'GUTENBERG_DEVELOPMENT_MODE' ) && GUTENBERG_DEVELOPMENT_MODE && ! file_exists( dirname( __FILE__ ) . '/blocks/build' ) ) {
 		add_action( 'admin_notices', 'gutenberg_build_files_notice' );
