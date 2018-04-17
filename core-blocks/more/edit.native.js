01bd7930d37d43ed1db027c6813e3a02772cf46b
Maxime Biais <maxime.biais@gmail.com>
2018-04-17 11:07:00 +0200

    Fix styling on the core/more block

--- core-blocks/more/edit.native.js
+++ core-blocks/more/edit.native.js
@@ -13,22 +13,22 @@ import { __ } from '../../../i18n';
 import PlainText from '../../plain-text';
 
 export function edit( { attributes, setAttributes, isSelected } ) {
-  const { customText, noTeaser } = attributes;
-  const defaultText = __( 'Read more' );
-  const value = customText !== undefined ? customText : defaultText;
+	const { customText, noTeaser } = attributes;
+	const defaultText = __( 'Read more' );
+	const value = customText !== undefined ? customText : defaultText;
 
-  return (
-    <View style={{ padding: 4, alignItems: 'center' }}>
-      <View style={{ alignItems: 'center', flexDirection: "row"}}>
-        <Text style={{ fontFamily: 'monospace' }}>&lt;!--</Text>
-        <PlainText
-          value={ value }
-          multiline={ false }
-          underlineColorAndroid="transparent"
-          onChange={ content => setAttributes( { content } ) }
-          placeholder={ defaultText }
-        />
-        <Text style={{ fontFamily: 'monospace' }}>--&gt;</Text>
-      </View>
-    </View>);
+	return (
+		<View style={ { padding: 4, alignItems: 'center' } }>
+			<View style={ { alignItems: 'center', flexDirection: 'row' } }>
+				<Text style={ { fontFamily: 'monospace' } }>&lt;!--</Text>
+				<PlainText
+					value={ value }
+					multiline={ false }
+					underlineColorAndroid="transparent"
+					onChange={ content => setAttributes( { content } ) }
+					placeholder={ defaultText }
+				/>
+				<Text style={ { fontFamily: 'monospace' } }>--&gt;</Text>
+			</View>
+		</View> );
 }
