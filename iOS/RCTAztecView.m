2e279d5f1850ce2e241981661b9f35ac3cc8d35f
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-04-18 12:22:42 -0300

    Adds placeholder for our iOS Aztec view.

---
+++ iOS/RCTAztecView.m
@@ -0,0 +1,18 @@
+#import <UIKit/UIKit.h>
+#import <React/RCTViewManager.h>
+
+@interface RCTAztecView : RCTViewManager
+@end
+
+@implementation RCTAztecView
+
+RCT_EXPORT_MODULE()
+
+- (UIView *)view
+{
+  UIView* view = [[UIView alloc] init];
+
+  view.backgroundColor = [UIColor redColor];
+}
+
+@end
