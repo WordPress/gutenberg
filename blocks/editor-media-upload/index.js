8c30d8d86c1d491f08acb2cec83e769ae5b5585d
Daniel Bachhuber <daniel@bachhuber.co>
2018-04-17 19:53:08 -0700

    `mediaUpload()` doesn't return anything

--- blocks/editor-media-upload/index.js
+++ blocks/editor-media-upload/index.js
@@ -5,5 +5,5 @@ import { select } from '@wordpress/data';
 import { mediaUpload } from '@wordpress/utils';
 
 export default function editorMediaUpload( filesList, onFileChange, allowedType ) {
-	return mediaUpload( filesList, onFileChange, allowedType, select( 'core/editor' ).getCurrentPostId() );
+	mediaUpload( filesList, onFileChange, allowedType, select( 'core/editor' ).getCurrentPostId() );
 }
