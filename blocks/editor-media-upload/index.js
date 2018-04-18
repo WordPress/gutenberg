a00432469d3f6fe6dff4004ea58023136094b7f8
Daniel Bachhuber <daniel@bachhuber.co>
2018-04-17 19:53:23 -0700

    Add JSDoc for function

--- blocks/editor-media-upload/index.js
+++ blocks/editor-media-upload/index.js
@@ -4,6 +4,14 @@
 import { select } from '@wordpress/data';
 import { mediaUpload } from '@wordpress/utils';
 
+/**
+ * Upload a media file when the file upload button is activated.
+ * Wrapper around mediaUpload() that injects the current post ID.
+ *
+ * @param {Array}    filesList    List of files.
+ * @param {Function} onFileChange Function to be called each time a file or a temporary representation of the file is available.
+ * @param {string}   allowedType  The type of media that can be uploaded.
+ */
 export default function editorMediaUpload( filesList, onFileChange, allowedType ) {
 	mediaUpload( filesList, onFileChange, allowedType, select( 'core/editor' ).getCurrentPostId() );
 }
