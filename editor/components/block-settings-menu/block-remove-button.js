eca8c05958ae3cd640110939851353c8890f184d
Andrea Fercia <a.fercia@gmail.com>
2018-04-19 13:14:09 +0200

    Make the block Remove button appear on focus.

--- editor/components/block-settings-menu/block-remove-button.js
+++ editor/components/block-settings-menu/block-remove-button.js
@@ -12,7 +12,7 @@ import { compose } from '@wordpress/element';
 import { withDispatch } from '@wordpress/data';
 import { withEditorSettings } from '@wordpress/blocks';
 
-export function BlockRemoveButton( { onRemove, onClick = noop, isLocked, role } ) {
+export function BlockRemoveButton( { onRemove, onClick = noop, onFocus, onBlur, isLocked, role } ) {
 	if ( isLocked ) {
 		return null;
 	}
@@ -26,6 +26,8 @@ export function BlockRemoveButton( { onRemove, onClick = noop, isLocked, role } 
 			icon="trash"
 			label={ label }
 			role={ role }
+			onFocus={ onFocus }
+			onBlur={ onBlur }
 		/>
 	);
 }
