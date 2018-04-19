b402c73437f229fc2622394aff018ffcc3084faf
Maxime Biais <maxime.biais@gmail.com>
2018-04-19 14:00:36 +0200

    Move sources to src/, fix sass auto imports and update variables in package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
-import App from './app/App';
+import App from './src/app/App';
 AppRegistry.registerComponent( 'gutenberg', () => App );
