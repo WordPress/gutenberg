ad7631c3f3b3a64befbea1e410241a72e05c70f2
Ryan Welcher <ryan.welcher@10up.com>
2018-04-20 09:53:11 -0400

    Removing extra whitespace

--- edit-post/components/sidebar/post-status/index.js
+++ edit-post/components/sidebar/post-status/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PanelBody, PanelRow  } from '@wordpress/components';
+import { PanelBody, PanelRow } from '@wordpress/components';
 import { compose } from '@wordpress/element';
 import { withSelect, withDispatch } from '@wordpress/data';
 
