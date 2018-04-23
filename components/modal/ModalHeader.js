8acd0ae29fbec16a1d80aa164a5c0f1ae9a7a267
boblinthorst <bob.linthorst@gmail.com>
2018-04-23 14:51:26 +0200

    working version of a basic react modal

---
+++ components/modal/ModalHeader.js
@@ -0,0 +1,30 @@
+import { NavigableToolbar } from '../../editor/components';
+import { IconButton } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import './style.scss';
+
+const ModalHeader = ( { icon, title, onClose, closeLabel } ) => {
+	const label = closeLabel ? closeLabel : __( 'Close window' );
+
+	return (
+		<NavigableToolbar
+			className={ 'edit-post-plugin-screen-takeover__editor-screen-takeover-header' }
+		>
+			<div>
+				<span aria-hidden="true">
+					{ icon }
+				</span>
+				<h1 id="modalID" >
+					{ title }
+				</h1>
+			</div>
+			<IconButton
+				onClick={ onClose }
+				icon="no-alt"
+				label={ label }
+			/>
+		</NavigableToolbar>
+	);
+};
+
+export default ModalHeader;
---
+++ components/modal/index.js
@@ -0,0 +1,58 @@
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+/**
+ * External dependencies
+ */
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement( document.getElementById( 'wpwrap' ) );
+
+export default class Modal extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			isOpen: true,
+			height: window.innerHeight - 32,
+		};
+
+		this.updateWindowHeight = this.updateWindowHeight.bind( this );
+		this.onClose = this.onClose.bind( this );
+	}
+
+	componentDidMount() {
+		window.addEventListener( 'resize', this.updateWindowHeight );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.updateWindowHeight );
+	}
+
+	updateWindowHeight() {
+		this.setState( {
+			height: window.innerHeight - 32,
+		} );
+	}
+
+	onClose() {
+		this.setState( {
+			isOpen: false,
+		} );
+	}
+
+	render() {
+		const { children } = this.props;
+		return <ReactModal isOpen={ true } render={ true } className={ 'edit-post-plugin-screen-takeover__editor-screen-takeover' } overlayClassName={ 'edit-post-plugin-screen-takeover__editor-screen-takeover-overlay' }
+			aria-labelledby="modalID">
+			{ children }
+		</ReactModal>;
+	}
+}
