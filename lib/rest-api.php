3c54f0419dbef912878334340c4af5995f4a6991
Gary Pendergast <gary@pento.net>
2018-04-28 08:40:34 +1000

    Expose Gutenberg Data Format version in the REST API (#6436)
    * Add the block format version to the REST API post resource
    
    * Add the block_format to the post content response.
    
    * Add todo marker for including on schema
    
    * Fix PHP 5.2 incompatibility
    
    * Ensure this is an array to handle `isset()` behavior in 5.2

--- lib/rest-api.php
+++ lib/rest-api.php
@@ -233,16 +233,41 @@ function gutenberg_add_permalink_template_to_posts( $response, $post, $request )
 }
 
 /**
+ * Add the block format version to post content in the post REST API response.
+ *
+ * @todo This will need to be registered to the schema too.
+ *
+ * @param WP_REST_Response $response WP REST API response of a post.
+ * @param WP_Post          $post The post being returned.
+ * @param WP_REST_Request  $request WP REST API request.
+ * @return WP_REST_Response Response containing the block_format.
+ */
+function gutenberg_add_block_format_to_post_content( $response, $post, $request ) {
+	if ( 'edit' !== $request['context'] ) {
+		return $response;
+	}
+
+	$response_data = $response->get_data();
+	if ( is_array( $response_data['content'] ) && isset( $response_data['content']['raw'] ) ) {
+		$response_data['content']['block_format'] = gutenberg_content_block_version( $response_data['content']['raw'] );
+		$response->set_data( $response_data );
+	}
+
+	return $response;
+}
+
+/**
  * Whenever a post type is registered, ensure we're hooked into it's WP REST API response.
  *
  * @param string $post_type The newly registered post type.
  * @return string That same post type.
  */
-function gutenberg_register_permalink_template_function( $post_type ) {
+function gutenberg_register_post_prepare_functions( $post_type ) {
 	add_filter( "rest_prepare_{$post_type}", 'gutenberg_add_permalink_template_to_posts', 10, 3 );
+	add_filter( "rest_prepare_{$post_type}", 'gutenberg_add_block_format_to_post_content', 10, 3 );
 	return $post_type;
 }
-add_filter( 'registered_post_type', 'gutenberg_register_permalink_template_function' );
+add_filter( 'registered_post_type', 'gutenberg_register_post_prepare_functions' );
 
 /**
  * Includes the value for the 'viewable' attribute of a post type resource.
