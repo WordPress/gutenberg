4b3ec1b4b4c80e1ed5e1d785d8a6688af8f0981c
Riad Benguella <benguella@gmail.com>
2018-05-01 13:31:16 +0100

    Add deprecation message for wp.blocks.*

--- editor/index.js
+++ editor/index.js
@@ -1,4 +1,77 @@
+/**
+ * External dependencies
+ */
+import { forEach } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { deprecated } from '@wordpress/utils';
+
 import './store';
 import './hooks';
 
 export * from './components';
+
+import {
+	Autocomplete,
+	AlignmentToolbar,
+	BlockAlignmentToolbar,
+	BlockControls,
+	BlockEdit,
+	BlockFormatControls,
+	BlockIcon,
+	ColorPalette,
+	ContrastChecker,
+	ImagePlaceholder,
+	InnerBlocks,
+	InspectorAdvancedControls,
+	InspectorControls,
+	PlainText,
+	RichText,
+	RichTextProvider,
+	MediaUpload,
+	UrlInput,
+	UrlInputButton,
+} from './components';
+
+const componentsToDepreacate = {
+	Autocomplete,
+	AlignmentToolbar,
+	BlockAlignmentToolbar,
+	BlockControls,
+	BlockEdit,
+	BlockFormatControls,
+	BlockIcon,
+	ColorPalette,
+	ContrastChecker,
+	ImagePlaceholder,
+	InnerBlocks,
+	InspectorAdvancedControls,
+	InspectorControls,
+	PlainText,
+	RichText,
+	RichTextProvider,
+	MediaUpload,
+	UrlInput,
+	UrlInputButton,
+};
+
+forEach( componentsToDepreacate, ( WrappedComponent, key ) => {
+	wp.blocks[ key ] = class extends Component {
+		constructor() {
+			super( ...arguments );
+
+			deprecated( 'wp.blocks.' + key, {
+				version: '3.1',
+				alternative: 'wp.editor.' + key,
+				plugin: 'Gutenberg',
+			} );
+		}
+
+		render() {
+			return <WrappedComponent { ...this.props } />;
+		}
+	};
+} );
