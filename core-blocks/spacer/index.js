979d675fcf15e343cafa748c030289d1488a3f80
Matías Ventura <mv@matiasventura.com>
2018-05-02 14:40:53 +0200

    Improve setting controls with BaseControl and Panel.

--- core-blocks/spacer/index.js
+++ core-blocks/spacer/index.js
@@ -1,9 +1,15 @@
 /**
+ * External dependencies
+ */
+import ResizableBox from 're-resizable';
+
+/**
  * WordPress
  */
+import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import ResizableBox from 're-resizable';
 import { InspectorControls } from '@wordpress/blocks';
+import { BaseControl, PanelBody } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -31,53 +37,59 @@ export const settings = {
 	edit( { attributes, setAttributes, isSelected, toggleSelection } ) {
 		const { height } = attributes;
 
-		return [
-			<ResizableBox
-				size={ {
-					height,
-				} }
-				minHeight="20"
-				handleClasses={ {
-					top: 'wp-block-spacer__resize-handler-top',
-					bottom: 'wp-block-spacer__resize-handler-bottom',
-				} }
-				enable={ {
-					top: true,
-					right: false,
-					bottom: true,
-					left: false,
-					topRight: false,
-					bottomRight: false,
-					bottomLeft: false,
-					topLeft: false,
-				} }
-				onResizeStop={ ( event, direction, elt, delta ) => {
-					setAttributes( {
-						height: parseInt( height + delta.height, 10 ),
-					} );
-					toggleSelection( true );
-				} }
-				onResizeStart={ () => {
-					toggleSelection( false );
-				} }
-				key="spacer"
-			/>,
-			isSelected &&
-			<InspectorControls key="inspector">
-				<input
-					type="number"
-					onChange={ ( event ) => {
-						setAttributes( {
-							height: parseInt( event.target.value, 10 ),
-						} );
+		return (
+			<Fragment>
+				<ResizableBox
+					size={ {
+						height,
 					} }
-					aria-label={ __( 'Height for the spacer element in pixels.' ) }
-					value={ height }
-					min="20"
-					step="10"
+					minHeight="20"
+					handleClasses={ {
+						top: 'wp-block-spacer__resize-handler-top',
+						bottom: 'wp-block-spacer__resize-handler-bottom',
+					} }
+					enable={ {
+						top: true,
+						right: false,
+						bottom: true,
+						left: false,
+						topRight: false,
+						bottomRight: false,
+						bottomLeft: false,
+						topLeft: false,
+					} }
+					onResizeStop={ ( event, direction, elt, delta ) => {
+						setAttributes( {
+							height: parseInt( height + delta.height, 10 ),
+						} );
+						toggleSelection( true );
+					} }
+					onResizeStart={ () => {
+						toggleSelection( false );
+					} }
 				/>
-			</InspectorControls>,
-		];
+				{ isSelected &&
+					<InspectorControls>
+						<PanelBody title={ __( 'Spacer Settings' ) }>
+							<BaseControl label={ __( 'Height in pixels' ) }>
+								<input
+									type="number"
+									onChange={ ( event ) => {
+										setAttributes( {
+											height: parseInt( event.target.value, 10 ),
+										} );
+									} }
+									aria-label={ __( 'Height for the spacer element in pixels.' ) }
+									value={ height }
+									min="20"
+									step="10"
+								/>
+							</BaseControl>
+						</PanelBody>
+					</InspectorControls>
+				}
+			</Fragment>
+		);
 	},
 
 	save( { attributes } ) {
