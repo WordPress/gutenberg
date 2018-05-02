f2ae9ef313ce0f0ff8e07b082843cbd9a9ddc1fb
Andrew Duthie <andrew@andrewduthie.com>
2018-05-02 16:18:38 -0400

    Testing: Add Writing Flow E2E tests

---
+++ test/e2e/specs/writing-flow.test.js
@@ -0,0 +1,88 @@
+/**
+ * External dependencies
+ */
+import { times } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import '../support/bootstrap';
+import { newPost, newDesktopBrowserPage, pressWithModifier } from '../support/utils';
+
+describe( 'adding blocks', () => {
+	beforeAll( async () => {
+		await newDesktopBrowserPage();
+		await newPost();
+	} );
+
+	/**
+	 * Given an array of functions, each returning a promise, performs all
+	 * promises in sequence (waterfall) order.
+	 *
+	 * @param {Function[]} sequence Array of promise creators.
+	 *
+	 * @return {Promise} Promise resolving once all in the sequence complete.
+	 */
+	async function promiseSequence( sequence ) {
+		return sequence.reduce(
+			( current, next ) => current.then( next ),
+			Promise.resolve()
+		);
+	}
+
+	/**
+	 * Asserts that the nth paragraph matches expected snapshot content.
+	 *
+	 * @param {number} nthChild 1-based index of paragraph to assert against.
+	 */
+	async function expectParagraphToMatchSnapshot( nthChild ) {
+		const content = await page.$eval(
+			`.editor-block-list__block:nth-child( ${ nthChild } ) .wp-block-paragraph`,
+			( element ) => element.innerHTML
+		);
+
+		expect( content ).toMatchSnapshot();
+	}
+
+	it( 'Should navigate with arrow keys', async () => {
+		// Add demo content
+		await page.click( '.editor-default-block-appender__content' );
+		await page.keyboard.type( 'Hello World' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( 'Hello World' );
+
+		// Traverse back to first paragraph. With identical content, assume
+		// caret will be at end of content. Append.
+		await page.keyboard.press( 'ArrowUp' );
+		await page.keyboard.type( '!' );
+		await expectParagraphToMatchSnapshot( 1 );
+
+		// Likewise, pressing arrow down when subsequent paragraph is shorter
+		// should move caret to end of the paragraph. Remove second word.
+		await page.keyboard.press( 'ArrowDown' );
+		await promiseSequence( times( 6, () => () => page.keyboard.press( 'Backspace' ) ) );
+		await expectParagraphToMatchSnapshot( 2 );
+
+		// Arrow up should preserve caret X position. Add content to middle.
+		await page.keyboard.press( 'ArrowUp' );
+		await page.keyboard.type( ' to the' );
+		await expectParagraphToMatchSnapshot( 1 );
+
+		// Arrow up from uncollapsed selection behaves same as collapsed.
+		// Highlight first word of second paragraph and prepend to first.
+		await page.keyboard.press( 'ArrowDown' );
+		await page.keyboard.down( 'Shift' );
+		await promiseSequence( times( 5, () => () => page.keyboard.press( 'ArrowLeft' ) ) );
+		await page.keyboard.up( 'Shift' );
+		await page.keyboard.press( 'ArrowUp' );
+		await page.keyboard.type( 'Greeting: ' );
+		await expectParagraphToMatchSnapshot( 1 );
+
+		// Arrow up from second line of paragraph should _not_ place caret out
+		// of the current block.
+		await pressWithModifier( 'Shift', 'Enter' );
+		await page.keyboard.press( 'ArrowUp' );
+		await page.keyboard.type( 'The ' );
+		await expectParagraphToMatchSnapshot( 1 );
+	} );
+} );
