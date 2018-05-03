19160260e98a9e65e1358406d62e5c021ddc5804
Daniel Bachhuber <daniel@bachhuber.co>
2018-05-03 09:06:32 -0700

    JSX formatting style

--- editor/components/post-featured-image/check.js
+++ editor/components/post-featured-image/check.js
@@ -5,9 +5,11 @@ import PostTypeSupportCheck from '../post-type-support-check';
 import ThemeSupportCheck from '../theme-support-check';
 
 function PostFeaturedImageCheck( props ) {
-	return <ThemeSupportCheck supportKeys="post-thumbnails">
-		<PostTypeSupportCheck { ...props } supportKeys="thumbnail" />
-	</ThemeSupportCheck>;
+	return (
+		<ThemeSupportCheck supportKeys="post-thumbnails">
+			<PostTypeSupportCheck { ...props } supportKeys="thumbnail" />
+		</ThemeSupportCheck>
+	);
 }
 
 export default PostFeaturedImageCheck;
