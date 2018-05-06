55889c2b00a70eff7b0d5230fad9d1a7d129935b
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-05-06 16:36:34 +0200

    Use a template literal for the footnotes markup

--- editor/components/rich-text/index.js
+++ editor/components/rich-text/index.js
@@ -838,7 +838,7 @@ export class RichText extends Component {
 			return;
 		}
 		const uid = uuid();
-		this.editor.insertContent( '<sup data-footnote-id="' + uid + '"><a href="#' + uid + '" class="wp-footnote"><span class="screen-reader-text">' + __( 'See footnote' ) + '</span></a></sup> ' );
+		this.editor.insertContent( `<sup data-footnote-id="${ uid }"><a href="#${ uid }" class="wp-footnote"><span class="screen-reader-text">${ __( 'See footnote' ) }</span></a></sup> ` );
 	}
 
 	/**
