90c3e90519f8156a7b983b94d97b101bd1f2b358
iseulde <wp@iseulde.com>
2018-05-06 16:01:08 +0200

    No longer needed to omit keyboardShortcut

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,8 @@
-import { omit } from 'lodash';
-
 import * as focus from './focus';
-import * as keycodesAll from './keycodes';
+import * as keycodes from './keycodes';
 import * as viewPort from './viewport';
 import { decodeEntities } from './entities';
 
-const keycodes = omit( keycodesAll, [ 'keyboardShortcut' ] );
 export { focus };
 export { keycodes };
 export { decodeEntities };
