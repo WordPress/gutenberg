f564e6cd28630f6fc0c96b601a5330816e3bc738
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2018-05-07 10:56:01 +0200

    Build: Introduce the common `build` folder to be used by all modules (#6584)
    * Build: Put all build files into build top folder
    
    * Framework: Move a11y and dom-ready to their own modules
    
    * Edit Post: Move domReady logic to client-assets
    
    * Make phpcs happy by adding dot at the end of comment
    
    * Make Eslint happy by removing no longer used domReady import

--- edit-post/index.js
+++ edit-post/index.js
@@ -7,7 +7,6 @@ import { get, isString, some } from 'lodash';
  * WordPress dependencies
  */
 import { registerCoreBlocks } from '@wordpress/core-blocks';
-import domReady from '@wordpress/dom-ready';
 import { render, unmountComponentAtNode } from '@wordpress/element';
 import { deprecated } from '@wordpress/utils';
 
@@ -83,25 +82,21 @@ export function initializeEditor( id, post, settings ) {
 		);
 	}
 
-	return new Promise( ( resolve ) => {
-		domReady( () => {
-			const target = document.getElementById( id );
-			const reboot = reinitializeEditor.bind( null, target, settings );
+	const target = document.getElementById( id );
+	const reboot = reinitializeEditor.bind( null, target, settings );
 
-			registerCoreBlocks();
+	registerCoreBlocks();
 
-			render(
-				<Editor settings={ migratedSettings || settings } onError={ reboot } post={ post } />,
-				target
-			);
+	render(
+		<Editor settings={ migratedSettings || settings } onError={ reboot } post={ post } />,
+		target
+	);
 
-			resolve( {
-				initializeMetaBoxes( metaBoxes ) {
-					store.dispatch( initializeMetaBoxState( metaBoxes ) );
-				},
-			} );
-		} );
-	} );
+	return {
+		initializeMetaBoxes( metaBoxes ) {
+			store.dispatch( initializeMetaBoxState( metaBoxes ) );
+		},
+	};
 }
 
 export { default as PluginPostStatusInfo } from './components/sidebar/plugin-post-status-info';
