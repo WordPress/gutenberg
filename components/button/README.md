fad53c3a21b601085769ab30bd4d147151d5597b
Andrea Fercia <a.fercia@gmail.com>
2018-05-08 18:18:02 +0200

    First porting of functionalities from 6245 into the Button component.

--- components/button/README.md
+++ components/button/README.md
@@ -1,8 +1,18 @@
-This component is used to implement dang sweet buttons.
+# Button
 
-#### Props
+This component is used to implement buttons and links.
+It's also used to implement links that open in a new browser's tab.
 
-The following props are used to control the display of the component. Any additional props will be passed to the rendered `<a />` or `<button />` element. The presence of a `href` prop determines whether an anchor element is rendered instead of a button.
+## Props
+
+The component accepts the following props. Any additional props will be passed to the rendered `<a />` or `<button />` element. The presence of a `href` prop determines whether an anchor element is rendered instead of a button.
 
 * `isPrimary`: (bool) whether the button is styled as a primary button.
+* `isLarge`: (bool) whether the button is styled as a large button.
+* `isSmall`: (bool) whether the button is styled as a small button.
+* `isToggled`: (bool) whether the button is styled with an inversion of colors.
+* `isBusy`: (bool) whether the button is styled with a "busy" animation.
+* `disabled`: (bool) whether the `<button />` element is disabled.
 * `href`: (string) if this property is added, it will use an `a` rather than a `button` element.
+* `rel`: (string) the `rel` attribute for the `<a />` element.
+* `target`: (string) the `target` attribute for the `<a />` element.
--- components/button/index.js
+++ components/button/index.js
@@ -2,21 +2,25 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import { compact, uniq, includes } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { createElement, forwardRef } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import Dashicon from '../dashicon';
 import './style.scss';
 
 export function Button( props, ref ) {
 	const {
 		href,
 		target,
+		rel = '',
 		isPrimary,
 		isLarge,
 		isSmall,
@@ -25,8 +29,36 @@ export function Button( props, ref ) {
 		className,
 		disabled,
 		focus,
+		children,
 		...additionalProps
 	} = props;
+
+	const { icon = true } = additionalProps;
+	const isExternalLink = href && ( target && ! includes( [ '_self', '_parent', '_top' ], target ) );
+
+	const getRel = () => {
+		// Allow to omit the `rel` attribute passing a `null` value.
+		return rel === null ? rel : uniq( compact( [
+			...rel.split( ' ' ),
+			'external',
+			'noreferrer',
+			'noopener',
+		] ) ).join( ' ' );
+	};
+
+	const { opensInNewTabText = __(
+		/* translators: accessibility text */
+		'(opens in a new tab)'
+	) } = additionalProps;
+
+	const OpensInNewTabText = (
+		<span className="screen-reader-text">
+			{
+				// We need a space to separate this from previous text.
+				' ' + opensInNewTabText
+			}
+		</span>
+	);
 
 	const classes = classnames( 'components-button', className, {
 		button: ( isPrimary || isLarge || isSmall ),
@@ -35,18 +67,22 @@ export function Button( props, ref ) {
 		'button-small': isSmall,
 		'is-toggled': isToggled,
 		'is-busy': isBusy,
+		'components-external-link': isExternalLink,
+		'components-icon-button': isExternalLink && icon && ( isPrimary || isLarge || isSmall ),
 	} );
 
 	const tag = href !== undefined && ! disabled ? 'a' : 'button';
-	const tagProps = tag === 'a' ? { href, target } : { type: 'button', disabled };
+	const tagProps = tag === 'a' ? { href, target, rel } : { type: 'button', disabled };
+	const externalIcon = icon && isExternalLink && <Dashicon icon="external" />;
 
 	return createElement( tag, {
 		...tagProps,
 		...additionalProps,
 		className: classes,
 		autoFocus: focus,
+		rel: isExternalLink && getRel(),
 		ref,
-	} );
+	}, ...children, OpensInNewTabText, externalIcon );
 }
 
 export default forwardRef( Button );
