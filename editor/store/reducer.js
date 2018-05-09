b0a537bb874c63e1e864d7ebc3d64393a51512c2
Riad Benguella <benguella@gmail.com>
2018-05-09 13:17:34 +0100

    move editor settings defaults to the editor defaults file

--- editor/store/reducer.js
+++ editor/store/reducer.js
@@ -31,73 +31,8 @@ import { combineReducers } from '@wordpress/data';
  */
 import withHistory from '../utils/with-history';
 import withChangeDetection from '../utils/with-change-detection';
-import { PREFERENCES_DEFAULTS } from './defaults';
+import { PREFERENCES_DEFAULTS, EDITOR_SETTINGS_DEFAULTS } from './defaults';
 import { insertAt, moveTo } from './array';
-
-/**
- * The default editor settings
- *
- *  alignWide         boolean        Enable/Disable Wide/Full Alignments
- *  colors            Array          Palette colors
- *  maxWidth          number         Max width to constraint resizing
- *  blockTypes        boolean|Array  Allowed block types
- *  hasFixedToolbar   boolean        Whether or not the editor toolbar is fixed
- */
-const DEFAULT_SETTINGS = {
-	alignWide: false,
-	colors: [
-		{
-			name: 'pale pink',
-			color: '#f78da7',
-		},
-		{	name: 'vivid red',
-			color: '#cf2e2e',
-		},
-		{
-			name: 'luminous vivid orange',
-			color: '#ff6900',
-		},
-		{
-			name: 'luminous vivid amber',
-			color: '#fcb900',
-		},
-		{
-			name: 'light green cyan',
-			color: '#7bdcb5',
-		},
-		{
-			name: 'vivid green cyan',
-			color: '#00d084',
-		},
-		{
-			name: 'pale cyan blue',
-			color: '#8ed1fc',
-		},
-		{
-			name: 'vivid cyan blue',
-			color: '#0693e3',
-		},
-		{
-			name: 'very light gray',
-			color: '#eeeeee',
-		},
-		{
-			name: 'cyan bluish gray',
-			color: '#abb8c3',
-		},
-		{
-			name: 'very dark gray',
-			color: '#313131',
-		},
-	],
-
-	// This is current max width of the block inner area
-	// It's used to constraint image resizing and this value could be overriden later by themes
-	maxWidth: 608,
-
-	// Allowed block types for the editor, defaulting to true (all supported).
-	allowedBlockTypes: true,
-};
 
 /**
  * Returns a post attribute value, flattening nested rendered content using its
@@ -862,7 +797,7 @@ export function template( state = { isValid: true }, action ) {
  *
  * @return {Object} Updated state.
  */
-export function settings( state = DEFAULT_SETTINGS, action ) {
+export function settings( state = EDITOR_SETTINGS_DEFAULTS, action ) {
 	switch ( action.type ) {
 		case 'UPDATE_EDITOR_SETTINGS':
 			return {
