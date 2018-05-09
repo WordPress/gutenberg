fde27cfa33191cea1fcff0103179499c09ee8e22
Sofia Sousa <csofiamsousa@gmail.com>
2018-05-09 11:30:33 +0100

    tp25411 Add editor and shortcode global vars so oldEditor can run and convert blocks

--- wp-init.js
+++ wp-init.js
@@ -4,9 +4,12 @@
 import React from 'react';
 import jQuery from 'jquery';
 import moment from 'moment';
+import tinymce from 'tinymce';
 import { parse, format } from 'url';
+import { memoize } from 'lodash';
 
 window.jQuery = window.jQuery || jQuery;
+window.tinymce = window.tinymce || tinymce;
 
 window.wp = window.wp || {};
 window.wp.blocks = window.wp.blocks || {};
@@ -26,11 +29,66 @@ window.wp.url = window.wp.url || { addQueryArgs: function( url, args ) {
 	return format( { ...parsedURL, query } );
 } };
 
-window.wp.oldEditor = window.wp.oldEditor || {
-	initialize: function() {
-		
+window.wp.shortcode = window.wp.shortcode || {};
+window.wp.shortcode.regexp = window.wp.shortcode.regexp || memoize( function( tag ) {
+	return new RegExp( '\\[(\\[?)(' + tag + ')(?![\\w-])([^\\]\\/]*(?:\\/(?!\\])[^\\]\\/]*)*?)(?:(\\/)\\]|\\](?:([^\\[]*(?:\\[(?!\\/\\2\\])[^\\[]*)*)(\\[\\/\\2\\]))?)(\\]?)', 'g' );
+} );
+
+window.wp.editor = window.wp.editor || {};
+window.wp.editor.getDefaultSettings = window.wp.editor.getDefaultSettings || function() {
+	return {
+		tinymce: {
+			indent: true,
+			keep_styles: false,
+			menubar: false,
+			plugins: 'charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen',
+			resize: 'vertical',
+			skin: 'lightgray',
+			theme: 'modern',
+			toolbar1: 'bold,italic,bullist,numlist,link',
+		},
+		quicktags: {
+			buttons: 'strong,em,link,ul,ol,li,code',
+		},
+	};
+};
+
+window.wp.editor.autop = window.wp.editor.autop || function() {};
+window.wp.editor.getContent = window.wp.editor.getContent || function( id ) {
+	const editor = window.tinymce.get( id );
+
+	if ( editor && ! editor.isHidden() ) {
+		editor.save();
+	}
+
+	return jQuery( '#' + id ).val();
+};
+window.wp.editor.initialize = window.wp.editor.initialize || function( id, settings = { tinymce: true } ) {
+	const defaults = wp.editor.getDefaultSettings();
+	const init = jQuery.extend( {}, defaults.tinymce, settings.tinymce );
+
+	init.selector = '#' + id;
+
+	window.tinymce.init( init );
+
+	if ( ! window.wpActiveEditor ) {
+		window.wpActiveEditor = id;
 	}
 };
+window.wp.editor.remove = window.wp.editor.remove || function( id ) {
+	const mceInstance = window.tinymce.get( id );
+
+	if ( mceInstance ) {
+		if ( ! mceInstance.isHidden() ) {
+			mceInstance.save();
+		}
+
+		mceInstance.remove();
+	}
+};
+window.wp.editor.removep = window.wp.editor.removep || function() {};
+
+window.wp.oldEditor = window.wp.editor;
 
 window.wp.api = window.wp.api || {};
 window.wp.api.models = window.wp.api.models || {};
@@ -83,7 +141,7 @@ window.wpEditorL10n = window.wpEditorL10n || {
 		baseUrl: 'node_modules/tinymce',
 		settings: {
 			external_plugins: [],
-			plugins: 'charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpautoresize,wpeditimage,wpemoji,wpgallery,wplink,wpdialogs,wptextpattern,wpview',
+			plugins: 'charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen', // ,wordpress,wpautoresize,wpeditimage,wpemoji,wpgallery,wplink,wpdialogs,wptextpattern,wpview',
 			toolbar1: 'formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,kitchensink',
 			toolbar2: 'strikethrough,hr,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help',
 			toolbar3: '',
