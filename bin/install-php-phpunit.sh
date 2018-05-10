aeec361ecffde5651638f3c12de16e2639bf6695
Daniel Bachhuber <daniel@bachhuber.co>
2018-05-10 06:01:50 -0700

    Only redirect STDOUT, because we want to see errors if they occur

--- bin/install-php-phpunit.sh
+++ bin/install-php-phpunit.sh
@@ -50,12 +50,12 @@ if [[ ${SWITCH_TO_PHP:0:3} == "5.2" ]] || [[ ${SWITCH_TO_PHP:0:3} == "5.3" ]]; t
     # build PHP5.2
     $HOME/php-utils-bin/phpbrew install --patch ${THIS_DIR}/patches/node.patch --patch ${THIS_DIR}/patches/openssl.patch 5.2 +default +mysql +pdo \
     +gettext +phar +openssl -- --with-openssl-dir=/usr/include/openssl --enable-spl --with-mysql --with-mysqli=/usr/bin/mysql_config --with-pdo-mysql=/usr \
-    > /dev/null 2>&1
+    > /dev/null
 
     # build PHP5.3
     $HOME/php-utils-bin/phpbrew install --patch ${THIS_DIR}/patches/node.patch --patch ${THIS_DIR}/patches/openssl.patch 5.3 +default +mysql +pdo \
     +gettext +phar +openssl -- --with-openssl-dir=/usr/include/openssl --enable-spl --with-mysql --with-mysqli=/usr/bin/mysql_config --with-pdo-mysql=/usr \
-    > /dev/null 2>&1
+    > /dev/null
 
     # install PHPUnit 3.6. The only install method available is from source, using git branches old
     # enough that they don't rely on any PHP5.3+ features. This clones each needed dependency
