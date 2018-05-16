94e5191f4fb6e04faa24006ddd2e655a23799fcd
Brandon Payton <brandon@happycode.net>
2018-05-16 07:21:57 -0700

    Support adding a human-readable deprecation hint

--- utils/deprecation.js
+++ utils/deprecation.js
@@ -7,13 +7,15 @@
  * @param {?string} options.alternative Feature to use instead
  * @param {?string} options.plugin      Plugin name if it's a plugin feature
  * @param {?string} options.link        Link to documentation
+ * @param {?string} options.hint		Additional message to help transition away from the deprecated feature.
  */
-export function deprecated( feature, { version, alternative, plugin, link } = {} ) {
+export function deprecated( feature, { version, alternative, plugin, link, hint } = {} ) {
 	const pluginMessage = plugin ? ` from ${ plugin }` : '';
 	const versionMessage = version ? `${ pluginMessage } in ${ version }` : '';
 	const useInsteadMessage = alternative ? ` Please use ${ alternative } instead.` : '';
 	const linkMessage = link ? ` See: ${ link }` : '';
-	const message = `${ feature } is deprecated and will be removed${ versionMessage }.${ useInsteadMessage }${ linkMessage }`;
+	const hintMessage = hint ? ` Note: ${ hint }` : '';
+	const message = `${ feature } is deprecated and will be removed${ versionMessage }.${ useInsteadMessage }${ linkMessage }${ hintMessage }`;
 
 	// eslint-disable-next-line no-console
 	console.warn( message );
