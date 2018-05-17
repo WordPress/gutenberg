44ab9938a91329822c9431eb65489ed141e53252
Riad Benguella <benguella@gmail.com>
2018-05-17 09:34:00 +0100

    Fix unit tests

--- core-blocks/video/test/index.js
+++ core-blocks/video/test/index.js
@@ -1,11 +1,27 @@
 /**
+ * External dependencies
+ */
+import { get, mapValues } from 'lodash';
+
+/**
  * Internal dependencies
  */
-import { name, settings } from '../';
+import { name, definition, implementation } from '../';
 import { blockEditRender } from '../../test/helpers';
 
 describe( 'core/video', () => {
 	test( 'block edit matches snapshot', () => {
+		const settings = {
+			...definition,
+			...implementation,
+			attributes: mapValues( definition.attributes, ( attribute, key ) => {
+				const implementationAttribute = get( implementation.attributes, [ key ], {} );
+				return {
+					...attribute,
+					...implementationAttribute,
+				};
+			} ),
+		};
 		const wrapper = blockEditRender( name, settings );
 
 		expect( wrapper ).toMatchSnapshot();
