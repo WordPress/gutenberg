240be25ebe74d4842a2c2195650a9f12de67ebf6
Stephen Edgar <stephen@netweb.com.au>
2018-05-17 19:39:28 +1000

    Add `@wordpress/npm-package-json-lint-config` package (#119)
    * Add `@wordpress/npm-package-json-lint-config` package
    
    * Standardize packages `package.json` format per `@wordpress/npm-package-json-lint`
    
    * Add unused `npm-package-json-lint` rules for reference
    
    * Update `npm-package-json-lint` to `3.0.0-alpha3`
    
    * Add `valid-values-publishConfig` rule with `{"access": "public"}` option for _packages_ repo
    
    * use tabs for indentation
    
    * Update npm-package-json-lint to v3
    
    * Update npmPackageJsonLintConfig config formatting
    
    * Use latest `3.0.1` npm-package-json-lint release and remove `.npmpackagejsonlintrc.json`
    
    * Use `lint:pkg-json` as the npmPkgJsonLint script task name
    
    * fix `pretest` test script command
    
    * Npm package.json lint config: address my own comments
    
    * Add is-plain-object as devDependency

--- scripts/create-symlinks.js
+++ scripts/create-symlinks.js
@@ -12,6 +12,7 @@ mkdirp( 'node_modules/@wordpress', () => {
 		[ 'packages/browserslist-config', 'node_modules/@wordpress/browserslist-config' ],
 		[ 'packages/babel-preset-default', 'node_modules/@wordpress/babel-preset-default' ],
 		[ 'packages/jest-preset-default', 'node_modules/@wordpress/jest-preset-default' ],
+		[ 'packages/npm-package-json-lint-config', 'node_modules/@wordpress/npm-package-json-lint-config' ],
 		[ 'packages/scripts', 'node_modules/@wordpress/scripts' ],
 		[ 'node_modules/@wordpress/scripts/bin/wp-scripts.js', 'node_modules/.bin/wp-scripts' ],
 	];
