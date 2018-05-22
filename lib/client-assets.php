deabbc308acefcd2e255c7610765e256d9163937
Jorge <jorge.costa@developer.pt>
2018-05-22 15:40:09 +0100

    Add max upload size error message (#4203)
    Add a onError handler to media handler. Used the handler to validate max file size and general upload problems.
    Passed max upload size from in client assets to allow max file size validation.
    Added the ability for blocks to display error notices.

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -965,6 +965,15 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 		);
 	}
 
+	$max_upload_size = wp_max_upload_size();
+	if ( ! $max_upload_size ) {
+		$max_upload_size = 0;
+	}
+	// Initialize media settings.
+	wp_add_inline_script( 'wp-editor', 'window._wpMediaSettings = ' . wp_json_encode( array(
+		'maxUploadSize' => $max_upload_size,
+	) ), 'before' );
+
 	// Prepare Jed locale data.
 	$locale_data = gutenberg_get_jed_locale_data( 'gutenberg' );
 	wp_add_inline_script(
