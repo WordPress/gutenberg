59c6e004f0d61e959919fcdae78b82ecdea3ced7
Matías Ventura <mv@matiasventura.com>
2018-05-24 12:31:18 +0200

    Update some more icons.

--- packages/block-library/src/columns/index.js
+++ packages/block-library/src/columns/index.js
@@ -49,7 +49,7 @@ export const settings = {
 		__( 'beta' )
 	),
 
-	icon: 'columns',
+	icon: <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><path d="M10 18h5V5h-5v13zm-6 0h5V5H4v13zM16 5v13h5V5h-5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>,
 
 	category: 'layout',
 
