e86a6dfdb5d4b32e52be17ed63cc1c9331b082c2
John Blackbourn <johnbillion@git.wordpress.org>
2018-05-25 22:47:39 +0000

    Build/Test Tools: Allow the unit test framework to be used without the data directory in place.
    Fixes #43982
    
    Merges [43315] to the 4.9 branch.
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.9@43316 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -63,7 +63,11 @@ $phpmailer = new MockPHPMailer( true );
 if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
 	define( 'WP_DEFAULT_THEME', 'default' );
 }
-$wp_theme_directories = array( DIR_TESTDATA . '/themedir1' );
+$wp_theme_directories = array();
+
+if ( file_exists( DIR_TESTDATA . '/themedir1' ) ) {
+	$wp_theme_directories[] = DIR_TESTDATA . '/themedir1';
+}
 
 system( WP_PHP_BINARY . ' ' . escapeshellarg( dirname( __FILE__ ) . '/install.php' ) . ' ' . escapeshellarg( $config_file_path ) . ' ' . $multisite, $retval );
 if ( 0 !== $retval ) {
