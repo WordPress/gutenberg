10e40825493622315ff0867f37c5f17e48d7c877
Ronny Ewanek <ronny.ewanek@gmail.com>
2018-05-27 07:41:41 -0500

    Remove submit button; Add ellipsis button

--- editor/components/url-input/button.js
+++ editor/components/url-input/button.js
@@ -18,19 +18,19 @@ import UrlInput from './';
 
 class UrlInputButton extends Component {
 	constructor() {
-		super( ...arguments );
-		this.toggle = this.toggle.bind( this );
-		this.submitLink = this.submitLink.bind( this );
+		super(...arguments);
+		this.toggle = this.toggle.bind(this);
+		this.submitLink = this.submitLink.bind(this);
 		this.state = {
 			expanded: false,
 		};
 	}
 
 	toggle() {
-		this.setState( { expanded: ! this.state.expanded } );
+		this.setState({ expanded: !this.state.expanded });
 	}
 
-	submitLink( event ) {
+	submitLink(event) {
 		event.preventDefault();
 		this.toggle();
 	}
@@ -38,35 +38,36 @@ class UrlInputButton extends Component {
 	render() {
 		const { url, onChange } = this.props;
 		const { expanded } = this.state;
-		const buttonLabel = url ? __( 'Edit Link' ) : __( 'Insert Link' );
+		const buttonLabel = url ? __('Edit Link') : __('Insert Link');
 
 		return (
 			<div className="editor-url-input__button">
 				<IconButton
 					icon="admin-links"
-					label={ buttonLabel }
-					onClick={ this.toggle }
-					className={ classnames( 'components-toolbar__control', {
+					label={buttonLabel}
+					onClick={this.toggle}
+					className={classnames('components-toolbar__control', {
 						'is-active': url,
-					} ) }
+					})}
 				/>
-				{ expanded &&
+				{expanded &&
 					<form
 						className="editor-url-input__button-modal"
-						onSubmit={ this.submitLink }
+						onSubmit={this.submitLink}
 					>
 						<div className="editor-url-input__button-modal-line">
-							<IconButton
-								className="editor-url-input__back"
-								icon="arrow-left-alt"
-								label={ __( 'Close' ) }
-								onClick={ this.toggle }
-							/>
-							<UrlInput value={ url || '' } onChange={ onChange } data-test="UrlInput" />
+							<UrlInput value={url || ''} onChange={onChange} data-test="UrlInput" />
 							<IconButton
 								icon="editor-break"
-								label={ __( 'Submit' ) }
+								label={__('Submit')}
 								type="submit"
+							/>
+							<IconButton
+								className="editor-format-toolbar__link-settings-toggle"
+								icon="ellipsis"
+								label={__('Link Settings')}
+							// onClick={this.toggleLinkSettingsVisibility}
+							// aria-expanded={settingsVisible}
 							/>
 						</div>
 					</form>
