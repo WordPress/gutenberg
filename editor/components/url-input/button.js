b1aec98dfef7cf32f007df1f15a6fb3cfd17b693
Ronny Ewanek <ronny.ewanek@gmail.com>
2018-05-29 07:58:24 -0500

    Add popover and remove commented out code

--- editor/components/url-input/button.js
+++ editor/components/url-input/button.js
@@ -9,7 +9,7 @@ import classnames from 'classnames';
 import './style.scss';
 import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
-import { IconButton, ToggleControl } from '@wordpress/components';
+import { IconButton, ToggleControl, Popover } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -18,11 +18,11 @@ import UrlInput from './';
 
 class UrlInputButton extends Component {
 	constructor() {
-		super(...arguments);
-		this.toggle = this.toggle.bind(this);
-		this.submitLink = this.submitLink.bind(this);
-		this.toggleLinkSettingsVisibility = this.toggleLinkSettingsVisibility.bind(this);
-		this.setLinkTarget = this.setLinkTarget.bind(this);
+		super( ...arguments );
+		this.toggle = this.toggle.bind( this );
+		this.submitLink = this.submitLink.bind( this );
+		this.toggleLinkSettingsVisibility = this.toggleLinkSettingsVisibility.bind( this );
+		this.setLinkTarget = this.setLinkTarget.bind( this );
 
 		this.state = {
 			expanded: false,
@@ -31,49 +31,40 @@ class UrlInputButton extends Component {
 		};
 	}
 
-	// this.state = {
-	//
-	// 	linkValue: '',
-	// };
-
 	toggle() {
-		this.setState({ expanded: !this.state.expanded });
+		this.setState( { expanded: ! this.state.expanded } );
 	}
 
-
-
-	submitLink(event) {
+	submitLink( event ) {
 		event.preventDefault();
 		this.toggle();
 	}
 
 	toggleLinkSettingsVisibility() {
-		this.setState((state) => ({ settingsVisible: !state.settingsVisible }));
+		this.setState( ( state ) => ( { settingsVisible: ! state.settingsVisible } ) );
 	}
 
-	setLinkTarget(opensInNewWindow) {
-		this.setState({ opensInNewWindow });
-		if (opensInNewWindow) {
-
-			this.props.setAttributes({
+	setLinkTarget( opensInNewWindow ) {
+		this.setState( { opensInNewWindow } );
+		if ( opensInNewWindow ) {
+			this.props.setAttributes( {
 				target: '_blank',
 				rel: opensInNewWindow ? 'noreferrer noopener' : null,
-			})
+			} );
 		}
 	}
-
 
 	render() {
 		const { url, onChange } = this.props;
 		const { expanded, settingsVisible, opensInNewWindow } = this.state;
-		const buttonLabel = url ? __('Edit Link') : __('Insert Link');
+		const buttonLabel = url ? __( 'Edit Link' ) : __( 'Insert Link' );
 
 		const linkSettings = settingsVisible && (
 			<div className="editor-format-toolbar__link-modal-line editor-format-toolbar__link-settings">
 				<ToggleControl
-					label={__('Open in new window')}
-					checked={opensInNewWindow}
-					onChange={this.setLinkTarget}
+					label={ __( 'Open in new window' ) }
+					checked={ opensInNewWindow }
+					onChange={ this.setLinkTarget }
 				/>
 			</div>
 		);
@@ -82,34 +73,39 @@ class UrlInputButton extends Component {
 			<div className="editor-url-input__button">
 				<IconButton
 					icon="admin-links"
-					label={buttonLabel}
-					onClick={this.toggle}
-					className={classnames('components-toolbar__control', {
+					label={ buttonLabel }
+					onClick={ this.toggle }
+					className={ classnames( 'components-toolbar__control', {
 						'is-active': url,
-					})}
+					} ) }
 				/>
-				{expanded &&
-					<form
-						className="editor-url-input__button-modal"
-						onSubmit={this.submitLink}
+				{ expanded &&
+					<Popover
+						position="bottom center"
+						focusOnMount={ true }
 					>
-						<div className="editor-url-input__button-modal-line">
-							<UrlInput value={url || ''} onChange={onChange} data-test="UrlInput" />
-							<IconButton
-								icon="editor-break"
-								label={__('Submit')}
-								type="submit"
-							/>
-							<IconButton
-								className="editor-format-toolbar__link-settings-toggle"
-								icon="ellipsis"
-								label={__('Link Settings')}
-								onClick={this.toggleLinkSettingsVisibility}
-							// aria-expanded={settingsVisible}
-							/>
-						</div>
-						{linkSettings}
-					</form>
+						<form
+							className="editor-url-input__button-modal"
+							onSubmit={ this.submitLink }
+						>
+							<div className="editor-url-input__button-modal-line">
+								<UrlInput value={ url || '' } onChange={ onChange } data-test="UrlInput" />
+								<IconButton
+									icon="editor-break"
+									label={ __( 'Submit' ) }
+									type="submit"
+								/>
+								<IconButton
+									className="editor-format-toolbar__link-settings-toggle"
+									icon="ellipsis"
+									label={ __( 'Link Settings' ) }
+									onClick={ this.toggleLinkSettingsVisibility }
+									aria-expanded={ settingsVisible }
+								/>
+							</div>
+							{ linkSettings }
+						</form>
+					</Popover>
 				}
 			</div>
 		);
