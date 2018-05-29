47449bebcbeb7df7419300f46480d57ac723fbce
Brandon Payton <brandon@happycode.net>
2018-05-29 07:47:34 -0700

    Fix blocks completer to provide supported blocks
    This updates the blocks completer to use the editor data store so only
    supported blocks will be offered as completion options. In addition to
    respecting the supported blocks list, shared blocks are now included as
    completion options.

--- editor/hooks/default-autocompleters.js
+++ editor/hooks/default-autocompleters.js
@@ -8,6 +8,7 @@ import { clone } from 'lodash';
  */
 import { addFilter } from '@wordpress/hooks';
 import { getDefaultBlockName } from '@wordpress/blocks';
+import { dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -23,6 +24,14 @@ function setDefaultCompleters( completers, blockName ) {
 		// Add blocks autocompleter for Paragraph block
 		if ( blockName === getDefaultBlockName() ) {
 			completers.push( clone( blockAutocompleter ) );
+
+			/*
+			 * NOTE: This is a hack to help ensure shared blocks are loaded
+			 * so they may be included in the block completer. It can be removed
+			 * once we have a way for completers to Promise options while
+			 * store-based data dependencies are being resolved.
+			 */
+			dispatch( 'core/editor' ).fetchSharedBlocks();
 		}
 	}
 	return completers;
