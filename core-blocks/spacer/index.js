59f2ad86bc66ebf5f4008b2dfcfbd782966ee253
Andrea Fercia <a.fercia@gmail.com>
2018-05-29 16:33:32 +0200

    Properly associate the spacer height input label. (#6922)
    * Properly associate the sapcer height input label.
    
    * Update stylesheet selectors.
    
    * Better CSS classes.

--- core-blocks/spacer/index.js
+++ core-blocks/spacer/index.js
@@ -9,7 +9,7 @@ import ResizableBox from 're-resizable';
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/editor';
-import { BaseControl, PanelBody } from '@wordpress/components';
+import { BaseControl, PanelBody, withInstanceId } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -34,61 +34,64 @@ export const settings = {
 		},
 	},
 
-	edit( { attributes, setAttributes, toggleSelection } ) {
-		const { height } = attributes;
+	edit: withInstanceId(
+		( { attributes, setAttributes, toggleSelection, instanceId } ) => {
+			const { height } = attributes;
+			const id = `block-spacer-height-input-${ instanceId }`;
 
-		return (
-			<Fragment>
-				<ResizableBox
-					size={ {
-						height,
-					} }
-					minHeight="20"
-					handleClasses={ {
-						top: 'wp-block-spacer__resize-handler-top',
-						bottom: 'wp-block-spacer__resize-handler-bottom',
-					} }
-					enable={ {
-						top: true,
-						right: false,
-						bottom: true,
-						left: false,
-						topRight: false,
-						bottomRight: false,
-						bottomLeft: false,
-						topLeft: false,
-					} }
-					onResizeStop={ ( event, direction, elt, delta ) => {
-						setAttributes( {
-							height: parseInt( height + delta.height, 10 ),
-						} );
-						toggleSelection( true );
-					} }
-					onResizeStart={ () => {
-						toggleSelection( false );
-					} }
-				/>
-				<InspectorControls>
-					<PanelBody title={ __( 'Spacer Settings' ) }>
-						<BaseControl label={ __( 'Height in pixels' ) }>
-							<input
-								type="number"
-								onChange={ ( event ) => {
-									setAttributes( {
-										height: parseInt( event.target.value, 10 ),
-									} );
-								} }
-								aria-label={ __( 'Height for the spacer element in pixels.' ) }
-								value={ height }
-								min="20"
-								step="10"
-							/>
-						</BaseControl>
-					</PanelBody>
-				</InspectorControls>
-			</Fragment>
-		);
-	},
+			return (
+				<Fragment>
+					<ResizableBox
+						size={ {
+							height,
+						} }
+						minHeight="20"
+						handleClasses={ {
+							top: 'core-blocks-spacer__resize-handler-top',
+							bottom: 'core-blocks-spacer__resize-handler-bottom',
+						} }
+						enable={ {
+							top: true,
+							right: false,
+							bottom: true,
+							left: false,
+							topRight: false,
+							bottomRight: false,
+							bottomLeft: false,
+							topLeft: false,
+						} }
+						onResizeStop={ ( event, direction, elt, delta ) => {
+							setAttributes( {
+								height: parseInt( height + delta.height, 10 ),
+							} );
+							toggleSelection( true );
+						} }
+						onResizeStart={ () => {
+							toggleSelection( false );
+						} }
+					/>
+					<InspectorControls>
+						<PanelBody title={ __( 'Spacer Settings' ) }>
+							<BaseControl label={ __( 'Height in pixels' ) } id={ id }>
+								<input
+									type="number"
+									id={ id }
+									onChange={ ( event ) => {
+										setAttributes( {
+											height: parseInt( event.target.value, 10 ),
+										} );
+									} }
+									value={ height }
+									min="20"
+									step="10"
+								/>
+							</BaseControl>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+	),
 
 	save( { attributes } ) {
 		return <div style={ { height: attributes.height } } aria-hidden />;
