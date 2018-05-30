25fcc9cd8b6bc2cea7a2d8ba53819669d1681164
Sofia Sousa <csofiamsousa@gmail.com>
2018-05-30 14:27:52 +0100

    tp26103 Move dynamic row edit to a new file

--- core-blocks/rows/dynamic.js
+++ core-blocks/rows/dynamic.js
@@ -24,38 +24,7 @@ import {
 /**
  * Internal dependencies
  */
-import './style.scss';
 import edit from './edit';
-
-const COLUMNS_TOTAL = 12;
-const MIN_COLUMNS = 2;
-const MAX_COLUMNS = 6;
-
-/**
- * Returns the layouts configuration for a given number of columns.
- *
- * @param {number} columns Number of columns.
- *
- * @return {Object[]} Columns layout configuration.
- */
-const getColumnLayouts = memoize( ( columns, widths ) => {
-	let position = 1;
-
-	return times( columns, ( n ) => {
-		const startPostion = position;
-		position = position + widths[ `col${ n + 1 }` ];
-
-		return {
-			name: `col${ getWidth( widths, n ) } column-start${ startPostion }`,
-			label: sprintf( __( 'Column %d' ), n + 1 ),
-			icon: 'columns',
-		};
-	} );
-} );
-
-const getWidth = ( widths, index ) => {
-	return widths.split( ',' )[ index ];
-};
 
 export const name = 'rows/dynamic';
 
@@ -79,88 +48,11 @@ export const settings = {
 
 	description: __( 'Add a block that displays content in multiple columns, then add whatever content blocks you\'d like.' ),
 
-	supports: {
+	/* supports: {
 		align: [ 'wide', 'full' ],
-	},
+	},*/
 
-	edit( { attributes, setAttributes, className } ) {
-		const { columns, widths } = attributes;
-		const classes = classnames( className, 'wp-block-rows' );
-
-		const onColumnsChange = ( nextColumns ) => {
-			setAttributes( {
-				columns: nextColumns,
-			} );
-		};
-
-		const onWidthChange = ( index, nextWidth ) => {
-			const nextWidths = attributes.widths.split( ',' );
-			nextWidths[ index ] = nextWidth;
-
-			// console.log( 'nextWidths', nextWidths );
-
-			setAttributes( {
-				widths: nextWidths.join(),
-			} );
-		};
-
-		let partial = 0;
-
-		return (
-			<Fragment>
-				<InspectorControls>
-					<PanelBody>
-						<RangeControl
-							label={ __( 'Columns' ) }
-							value={ columns }
-							onChange={ ( nextColumns ) => onColumnsChange( nextColumns ) }
-							min={ MIN_COLUMNS }
-							max={ MAX_COLUMNS }
-						/>
-					</PanelBody>
-					{
-						times( columns, ( n ) => {
-							const colWidth = getWidth( widths, n );
-
-							const max = COLUMNS_TOTAL - ( partial + ( ( columns - 1 ) * MIN_COLUMNS ) );
-							const min = n === columns - 1 ? max : MIN_COLUMNS;
-
-							const availableWidths = range( min, max + 1 );
-
-							partial = partial + ( colWidth - MIN_COLUMNS );
-
-							if ( availableWidths.length === 1 ) {
-								onWidthChange( n, availableWidths[ 0 ] );
-							}
-
-							return (
-								<PanelBody title={ sprintf( __( 'Column %d width' ), n + 1 ) }>
-									<ButtonGroup aria-label={ __( 'Column width' ) }>
-										{
-											map( availableWidths, ( width ) => (
-												<Button
-													key={ width }
-													isSmall
-													isPrimary={ colWidth === width }
-													aria-pressed={ colWidth === width }
-													onClick={ () => onWidthChange( n, width ) }
-												>
-													{ width }
-												</Button>
-											) )
-										}
-									</ButtonGroup>
-								</PanelBody>
-							);
-						} )
-					}
-				</InspectorControls>
-				<div className={ classes }>
-					<InnerBlocks layouts={ getColumnLayouts( columns, widths ) } />
-				</div>
-			</Fragment>
-		);
-	},
+	edit,
 
 	save() {
 		return (
