c3dd54b0e604cfa73e853915e4cfa2586696f2ec
Robert Anderson <robert@noisysocks.com>
2018-05-30 12:57:46 +1000

    Use milliseconds when calculating frecency
    Date.now() returns the current time in milliseconds, but we were
    comparing it against constants measured in seconds.

--- editor/store/selectors.js
+++ editor/store/selectors.js
@@ -36,9 +36,9 @@ export const INSERTER_UTILITY_HIGH = 3;
 export const INSERTER_UTILITY_MEDIUM = 2;
 export const INSERTER_UTILITY_LOW = 1;
 export const INSERTER_UTILITY_NONE = 0;
-const SECONDS_PER_HOUR = 3600;
-const SECONDS_PER_DAY = 24 * 3600;
-const SECONDS_PER_WEEK = 7 * 24 * 3600;
+const MILLISECONDS_PER_HOUR = 3600 * 1000;
+const MILLISECONDS_PER_DAY = 24 * 3600 * 1000;
+const MILLISECONDS_PER_WEEK = 7 * 24 * 3600 * 1000;
 
 /**
  * Shared reference to an empty array for cases where it is important to avoid
@@ -1359,11 +1359,11 @@ export const getInserterItems = createSelector(
 
 			const duration = Date.now() - time;
 			switch ( true ) {
-				case duration < SECONDS_PER_HOUR:
+				case duration < MILLISECONDS_PER_HOUR:
 					return count * 4;
-				case duration < SECONDS_PER_DAY:
+				case duration < MILLISECONDS_PER_DAY:
 					return count * 2;
-				case duration < SECONDS_PER_WEEK:
+				case duration < MILLISECONDS_PER_WEEK:
 					return count / 2;
 				default:
 					return count / 4;
