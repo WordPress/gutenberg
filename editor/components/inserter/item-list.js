22308dc1bdfbf811c51f6b793988cc7c9f833b08
Andrea Fercia <a.fercia@gmail.com>
2018-05-31 15:07:25 +0200

    Remove ARIA roles and arrows navigation.

--- editor/components/inserter/item-list.js
+++ editor/components/inserter/item-list.js
@@ -1,14 +1,12 @@
 /**
  * External dependencies
  */
-import { isEqual } from 'lodash';
 import classnames from 'classnames';
 
 /**
  * WordPress dependencies
  */
 import { Component } from '@wordpress/element';
-import { NavigableMenu } from '@wordpress/components';
 import { getBlockMenuDefaultClassName } from '@wordpress/blocks';
 
 /**
@@ -16,63 +14,15 @@ import { getBlockMenuDefaultClassName } from '@wordpress/blocks';
  */
 import BlockIcon from '../block-icon';
 
-function deriveActiveItems( items ) {
-	return items.filter( ( item ) => ! item.isDisabled );
-}
-
 class ItemList extends Component {
-	constructor() {
-		super( ...arguments );
-		this.onNavigate = this.onNavigate.bind( this );
-		this.activeItems = deriveActiveItems( this.props.items );
-		this.state = {
-			current: this.activeItems.length > 0 ? this.activeItems[ 0 ] : null,
-		};
-	}
-
-	componentDidUpdate( prevProps ) {
-		if ( ! isEqual( this.props.items, prevProps.items ) ) {
-			this.activeItems = deriveActiveItems( this.props.items );
-
-			// Try and preserve any still valid selected state.
-			const currentIsStillActive = this.state.current && this.activeItems.some( ( item ) =>
-				item.id === this.state.current.id
-			);
-
-			if ( ! currentIsStillActive ) {
-				this.setState( {
-					current: this.activeItems.length > 0 ? this.activeItems[ 0 ] : null,
-				} );
-			}
-		}
-	}
-
-	onNavigate( index ) {
-		const { activeItems } = this;
-		const dest = activeItems[ index ];
-		if ( dest ) {
-			this.setState( {
-				current: dest,
-			} );
-		}
-	}
-
 	render() {
 		const { items, onSelect, onHover } = this.props;
-		const { current } = this.state;
 
 		return (
-			<NavigableMenu
-				className="editor-inserter__item-list"
-				orientation="both"
-				cycle={ false }
-				onNavigate={ this.onNavigate }
-			>
+			<div className="editor-inserter__item-list">
 				{ items.map( ( item ) => {
-					const isCurrent = current && current.id === item.id;
 					return (
 						<button
-							role="menuitem"
 							key={ item.id }
 							className={
 								classnames(
@@ -84,7 +34,6 @@ class ItemList extends Component {
 								)
 							}
 							onClick={ () => onSelect( item ) }
-							tabIndex={ isCurrent || item.isDisabled ? null : '-1' }
 							disabled={ item.isDisabled }
 							onMouseEnter={ () => onHover( item ) }
 							onMouseLeave={ () => onHover( null ) }
@@ -103,7 +52,7 @@ class ItemList extends Component {
 						</button>
 					);
 				} ) }
-			</NavigableMenu>
+			</div>
 		);
 	}
 }
