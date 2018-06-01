91ede092cfb956a1aee90eb61ce9015479b2307e
iseulde <wp@iseulde.com>
2018-06-01 14:39:01 +0200

    e2e: wait for media modal

--- test/e2e/specs/adding-inline-tokens.test.js
+++ test/e2e/specs/adding-inline-tokens.test.js
@@ -7,7 +7,7 @@ import path from 'path';
  * Internal dependencies
  */
 import '../support/bootstrap';
-import { newPost, newDesktopBrowserPage, getHTMLFromCodeEditor } from '../support/utils';
+import { newPost, newDesktopBrowserPage, getHTMLFromCodeEditor, insertBlock } from '../support/utils';
 
 describe( 'adding inline tokens', () => {
 	beforeAll( async () => {
@@ -18,41 +18,36 @@ describe( 'adding inline tokens', () => {
 	it( 'Should insert inline image', async () => {
 		// Create a paragraph.
 		await page.click( '.editor-default-block-appender' );
-		await page.keyboard.type( 'Paragraph with inline image: ' );
+		await page.keyboard.type( 'a ' );
 
-		// Use the global inserter to add an inline image.
-		await page.click( '.edit-post-header [aria-label="Add block"]' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Enter' );
+		insertBlock( 'Inline Image' );
 
-		// The media modal appears. Upload the test image.
-		const inputElement = await page.$( 'input[type=file]' );
+		// Wait for media modal to appear and upload image.
+		await page.waitForSelector( '.media-modal input[type=file]' );
+		const inputElement = await page.$( '.media-modal input[type=file]' );
 		const testImagePath = path.join( __dirname, '..', 'assets', '10x10_e2e_test_image_z9T8jK.png' );
 		await inputElement.uploadFile( testImagePath );
 
 		// Wait for upload.
-		await page.waitForSelector( 'li[aria-label="10x10_e2e_test_image_z9T8jK"]' );
+		await page.waitForSelector( '.media-modal li[aria-label="10x10_e2e_test_image_z9T8jK"]' );
 
 		// Insert the uploaded image.
-		await page.click( 'button.media-button-select' );
+		await page.click( '.media-modal button.media-button-select' );
 
 		// Check the content.
-		expect( await getHTMLFromCodeEditor() ).toMatch( /<!-- wp:paragraph -->\s*<p>Paragraph with inline image:\u00A0<img class="wp-image-\d+" style="width:10px" src="[^"]+\/10x10_e2e_test_image_z9T8jK\.png" alt="" \/><\/p>\s*<!-- \/wp:paragraph -->/ );
+		expect( await getHTMLFromCodeEditor() ).toMatch( /<!-- wp:paragraph -->\s*<p>a\u00A0<img class="wp-image-\d+" style="width:10px" src="[^"]+\/10x10_e2e_test_image_z9T8jK\.png" alt="" \/><\/p>\s*<!-- \/wp:paragraph -->/ );
 
-		// Open the media modal again by using the globar inserter
-		await page.click( '.edit-post-header [aria-label="Add block"]' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Tab' );
-		await page.keyboard.press( 'Enter' );
+		// Open the media modal again by inserting inline image
+		insertBlock( 'Inline Image' );
 
 		// Confirm deletion.
 		page.on( 'dialog', async ( dialog ) => {
 			await dialog.accept();
 		} );
 
-		// Delete the test image (first item).
-		await page.click( 'li.attachment' );
-		await page.click( 'button.delete-attachment' );
+		// Wait for media modal to appear and delete image.
+		await page.waitForSelector( '.media-modal li.attachment' );
+		await page.click( '.media-modal li.attachment' );
+		await page.click( '.media-modal button.delete-attachment' );
 	} );
 } );
