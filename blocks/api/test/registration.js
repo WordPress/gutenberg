WordPress/gutenberg

commit aa5cc43e13c3ed59864dd2a042e7876b0b3b3d6c

Add API to configure icon colors (#7068)
This PR allows the icon to configure 2 colors: background and foreground color.
If the foreground color is not passed it is automatically computed from the 
background color.
This colors right now are being used in the inserter.

blocks/api/registration.js
@@ -13,28 +13,33 @@ import { select, dispatch } from '@wordpress/data';
 import deprecated from '@wordpress/deprecated';
 
 /**
+ * Internal dependencies
+ */
+import { normalizeIconObject } from './utils';
+
+/**
  * Defined behavior of a block type.
  *
  * @typedef {WPBlockType}
  *
- * @property {string}             name       Block's namespaced name.
- * @property {string}             title      Human-readable label for a block.
- *                                           Shown in the block inserter.
- * @property {string}             category   Category classification of block,
- *                                           impacting where block is shown in
- *                                           inserter results.
- * @property {(string|WPElement)} icon       Slug of the Dashicon to be shown
- *                                           as the icon for the block in the
- *                                           inserter, or element.
- * @property {?string[]}          keywords   Additional keywords to produce
- *                                           block as inserter search result.
- * @property {?Object}            attributes Block attributes.
- * @property {Function}           save       Serialize behavior of a block,
- *                                           returning an element describing
- *                                           structure of the block's post
- *                                           content markup.
- * @property {WPComponent}        edit       Component rendering element to be
- *                                           interacted with in an editor.
+ * @property {string}                    name       Block's namespaced name.
+ * @property {string}                    title      Human-readable label for a block.
+ *                                                  Shown in the block inserter.
+ * @property {string}                    category   Category classification of block,
+ *                                                  impacting where block is shown in
+ *                                                  inserter results.
+ * @property {(Object|string|WPElement)} icon       Slug of the Dashicon to be shown
+ *                                                  as the icon for the block in the
+ *                                                  inserter, or element or an object describing the icon.
+ * @property {?string[]}                 keywords   Additional keywords to produce
+ *                                                  block as inserter search result.
+ * @property {?Object}                   attributes Block attributes.
+ * @property {Function}                  save       Serialize behavior of a block,
+ *                                                  returning an element describing
+ *                                                  structure of the block's post
+ *                                                  content markup.
+ * @property {WPComponent}               edit       Component rendering element to be
+ *                                                  interacted with in an editor.
  */
 
 /**
@@ -134,9 +139,9 @@ export function registerBlockType( name, settings ) {
 		);
 		return;
 	}
-	if ( ! settings.icon ) {
-		settings.icon = 'block-default';
-	}
+
+	settings.icon = normalizeIconObject( settings.icon );
+
 	if ( 'isPrivate' in settings ) {
 		deprecated( 'isPrivate', {
 			version: '3.1',

blocks/api/utils.js
@@ -1,18 +1,28 @@
 /**
  * External dependencies
  */
-import { every, keys, isEqual } from 'lodash';
+import { every, keys, isEqual, isFunction, isString } from 'lodash';
+import { default as tinycolor, mostReadable } from 'tinycolor2';
 
 /**
  * WordPress dependencies
  */
 import { applyFilters } from '@wordpress/hooks';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { getDefaultBlockName } from './registration';
 import { createBlock } from './factory';
+
+/**
+ * Array of icon colors containing a color to be used if the icon color
+ * was not explicitly set but the icon background color was.
+ *
+ * @type {Object}
+ */
+const ICON_COLORS = [ '#191e23', '#f8f9f9' ];
 
 /**
  * Determines whether the block is a default block
@@ -39,4 +49,38 @@ export function isUnmodifiedDefaultBlock( block ) {
 	return every( attributeKeys, ( key ) =>
 		isEqual( newDefaultBlock.attributes[ key ], block.attributes[ key ] )
 	);
+}
+
+/**
+ * Function that receives an icon as set by the blocks during the registration
+ * and returns a new icon object that is normalized so we can rely on just on possible icon structure
+ * in the codebase.
+ *
+ * @param {(Object|string|WPElement)} icon  Slug of the Dashicon to be shown
+ *                                          as the icon for the block in the
+ *                                          inserter, or element or an object describing the icon.
+ *
+ * @return {Object} Object describing the icon.
+ */
+export function normalizeIconObject( icon ) {
+	if ( ! icon ) {
+		return { src: 'block-default' };
+	}
+	if ( isString( icon ) || isFunction( icon ) || icon instanceof Component ) {
+		return { src: icon };
+	}
+
+	if ( icon.background ) {
+		const tinyBgColor = tinycolor( icon.background );
+		if ( ! icon.foreground ) {
+			const foreground = mostReadable(
+				tinyBgColor,
+				ICON_COLORS,
+				{ includeFallbackColors: true, level: 'AA', size: 'large' }
+			).toHexString();
+			icon.foreground = foreground;
+		}
+		icon.shadowColor = tinyBgColor.setAlpha( 0.3 ).toRgbString();
+	}
+	return icon;
 }

blocks/api/test/registration.js
@@ -84,7 +84,9 @@ describe( 'blocks', () => {
 			expect( console ).not.toHaveErrored();
 			expect( block ).toEqual( {
 				name: 'my-plugin/fancy-block-4',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 				save: noop,
 				category: 'common',
 				title: 'block title',
@@ -167,7 +169,9 @@ describe( 'blocks', () => {
 				save: noop,
 				category: 'common',
 				title: 'block title',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 				attributes: {
 					ok: {
 						type: 'boolean',
@@ -186,7 +190,9 @@ describe( 'blocks', () => {
 				save: noop,
 				category: 'common',
 				title: 'block title',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 			} );
 		} );
 
@@ -224,7 +230,9 @@ describe( 'blocks', () => {
 					save: noop,
 					category: 'common',
 					title: 'block title',
-					icon: 'block-default',
+					icon: {
+						src: 'block-default',
+					},
 				},
 			] );
 			const oldBlock = unregisterBlockType( 'core/test-block' );
@@ -234,7 +242,9 @@ describe( 'blocks', () => {
 				save: noop,
 				category: 'common',
 				title: 'block title',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 			} );
 			expect( getBlockTypes() ).toEqual( [] );
 		} );
@@ -276,7 +286,9 @@ describe( 'blocks', () => {
 				save: noop,
 				category: 'common',
 				title: 'block title',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 			} );
 		} );
 
@@ -289,7 +301,9 @@ describe( 'blocks', () => {
 				save: noop,
 				category: 'common',
 				title: 'block title',
-				icon: 'block-default',
+				icon: {
+					src: 'block-default',
+				},
 			} );
 		} );
 	} );
@@ -309,7 +323,9 @@ describe( 'blocks', () => {
 					save: noop,
 					category: 'common',
 					title: 'block title',
-					icon: 'block-default',
+					icon: {
+						src: 'block-default',
+					},
 				},
 				{
 					name: 'core/test-block-with-settings',
@@ -317,7 +333,9 @@ describe( 'blocks', () => {
 					save: noop,
 					category: 'common',
 					title: 'block title',
-					icon: 'block-default',
+					icon: {
+						src: 'block-default',
+					},
 				},
 			] );
 		} );

editor/components/block-inspector/index.js
@@ -34,7 +34,7 @@ const BlockInspector = ( { selectedBlock, count } ) => {
 	return [
 		<div className="editor-block-inspector__card" key="card">
 			<div className="editor-block-inspector__card-icon">
-				<BlockIcon icon={ blockType.icon } />
+				<BlockIcon icon={ blockType.icon && blockType.icon.src } />
 			</div>
 			<div className="editor-block-inspector__card-content">
 				<div className="editor-block-inspector__card-title">{ blockType.title }</div>

editor/components/block-switcher/index.js
@@ -47,7 +47,7 @@ export function BlockSwitcher( { blocks, onTransform, isLocked } ) {
 					<Toolbar>
 						<IconButton
 							className="editor-block-switcher__toggle"
-							icon={ <BlockIcon icon={ blockType.icon } /> }
+							icon={ <BlockIcon icon={ blockType.icon && blockType.icon.src } /> }
 							onClick={ onToggle }
 							aria-haspopup="true"
 							aria-expanded={ isOpen }
@@ -81,7 +81,7 @@ export function BlockSwitcher( { blocks, onTransform, isLocked } ) {
 								className="editor-block-switcher__menu-item"
 								icon={ (
 									<span className="editor-block-switcher__block-icon">
-										<BlockIcon icon={ icon } />
+										<BlockIcon icon={ icon && icon.src } />
 									</span>
 								) }
 								role="menuitem"

editor/components/inserter-with-shortcuts/index.js
@@ -36,7 +36,7 @@ function InserterWithShortcuts( { items, isLocked, onInsert } ) {
 					onClick={ () => onInsert( item ) }
 					label={ sprintf( __( 'Add %s' ), item.title ) }
 					icon={ (
-						<BlockIcon icon={ item.icon } />
+						<BlockIcon icon={ item.icon && item.icon.src } />
 					) }
 				/>
 			) ) }

editor/components/autocompleters/block.js
@@ -42,7 +42,7 @@ export function createBlockCompleter( {
 		getOptionLabel( inserterItem ) {
 			const { icon, title } = inserterItem;
 			return [
-				<BlockIcon key="icon" icon={ icon } />,
+				<BlockIcon key="icon" icon={ icon && icon.src } />,
 				title,
 			];
 		},

editor/components/autocompleters/test/block.js
@@ -54,7 +54,9 @@ describe( 'block', () => {
 	it( 'should render a block option label', () => {
 		const labelComponents = shallow( <div>
 			{ blockCompleter.getOptionLabel( {
-				icon: 'expected-icon',
+				icon: {
+					src: 'expected-icon',
+				},
 				title: 'expected-text',
 			} ) }
 		</div> ).children();

commit 1a7255fb751f1bf519968330c8ca5fcda740c1e5

Fix svg block icons; Add icon validation and test cases (#7140)
The condition to check SVG icons was not correct. Now we use a more specific function that validates the icon.
Test cases were added.

Fixes https://github.com/WordPress/gutenberg/issues/7139.
Regressed in https://github.com/WordPress/gutenberg/pull/7068.

blocks/api/registration.js
@@ -15,7 +15,7 @@ import deprecated from '@wordpress/deprecated';
 /**
  * Internal dependencies
  */
-import { normalizeIconObject } from './utils';
+import { isValidIcon, normalizeIconObject } from './utils';
 
 /**
  * Defined behavior of a block type.
@@ -141,6 +141,13 @@ export function registerBlockType( name, settings ) {
 	}
 
 	settings.icon = normalizeIconObject( settings.icon );
+	if ( ! isValidIcon( settings.icon.src ) ) {
+		console.error(
+			'The icon passed is invalid. ' +
+			'The icon should be a string, an element, a function, or an object following the specifications documented in https://wordpress.org/gutenberg/handbook/block-api/#icon-optional'
+		);
+		return;
+	}
 
 	if ( 'isPrivate' in settings ) {
 		deprecated( 'isPrivate', {

blocks/api/utils.js
@@ -8,7 +8,7 @@ import { default as tinycolor, mostReadable } from 'tinycolor2';
  * WordPress dependencies
  */
 import { applyFilters } from '@wordpress/hooks';
-import { Component } from '@wordpress/element';
+import { Component, isValidElement } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -52,6 +52,23 @@ export function isUnmodifiedDefaultBlock( block ) {
 }
 
 /**
+ * Function that checks if the parameter is a valid icon.
+ *
+ * @param {*} icon  Parameter to be checked.
+ *
+ * @return {boolean} True if the parameter is a valid icon and false otherwise.
+ */
+
+export function isValidIcon( icon ) {
+	return !! icon && (
+		isString( icon ) ||
+		isValidElement( icon ) ||
+		isFunction( icon ) ||
+		icon instanceof Component
+	);
+}
+
+/**
  * Function that receives an icon as set by the blocks during the registration
  * and returns a new icon object that is normalized so we can rely on just on possible icon structure
  * in the codebase.
@@ -66,7 +83,7 @@ export function normalizeIconObject( icon ) {
 	if ( ! icon ) {
 		return { src: 'block-default' };
 	}
-	if ( isString( icon ) || isFunction( icon ) || icon instanceof Component ) {
+	if ( isValidIcon( icon ) ) {
 		return { src: icon };
 	}
 

packages/element/src/index.js
@@ -10,6 +10,7 @@ import {
 	cloneElement,
 	Children,
 	Fragment,
+	isValidElement,
 } from 'react';
 import { render, findDOMNode, createPortal, unmountComponentAtNode } from 'react-dom';
 import {
@@ -118,6 +119,15 @@ export { Fragment };
  * @return {Object} Context object.
  */
 export { createContext };
+
+/**
+ * Checks if an object is a valid WPElement
+ *
+ * @param {Object} objectToCheck The object to be checked.
+ *
+ * @return {boolean} true if objectToTest is a valid WPElement and false otherwise.
+ */
+export { isValidElement };
 
 /**
  * Creates a portal into which a component can be rendered.

blocks/api/test/registration.js
@@ -180,6 +180,118 @@ describe( 'blocks', () => {
 			} );
 		} );
 
+		it( 'should validate the icon', () => {
+			const blockType = {
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: { chicken: 'ribs' },
+			};
+			const block = registerBlockType( 'core/test-block-icon-normalize-element', blockType );
+			expect( console ).toHaveErrored();
+			expect( block ).toBeUndefined();
+		} );
+
+		it( 'should normalize the icon containing an element', () => {
+			const blockType = {
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: ( <svg width="20" height="20" viewBox="0 0 20 20">
+					<circle cx="10" cy="10" r="10"
+						fill="red" stroke="blue" strokeWidth="10" />
+				</svg> ),
+			};
+			registerBlockType( 'core/test-block-icon-normalize-element', blockType );
+			expect( getBlockType( 'core/test-block-icon-normalize-element' ) ).toEqual( {
+				name: 'core/test-block-icon-normalize-element',
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: {
+					src: ( <svg width="20" height="20" viewBox="0 0 20 20">
+						<circle cx="10" cy="10" r="10"
+							fill="red" stroke="blue" strokeWidth="10" />
+					</svg> ),
+				},
+			} );
+		} );
+
+		it( 'should normalize the icon containing a string', () => {
+			const blockType = {
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: 'foo',
+			};
+			registerBlockType( 'core/test-block-icon-normalize-string', blockType );
+			expect( getBlockType( 'core/test-block-icon-normalize-string' ) ).toEqual( {
+				name: 'core/test-block-icon-normalize-string',
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: {
+					src: 'foo',
+				},
+			} );
+		} );
+
+		it( 'should normalize the icon containing a function', () => {
+			const MyTestIcon = () => {
+				return <svg width="20" height="20" viewBox="0 0 20 20">
+					<circle cx="10" cy="10" r="10"
+						fill="red" stroke="blue" strokeWidth="10" />
+				</svg>;
+			};
+			const blockType = {
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: MyTestIcon,
+			};
+			registerBlockType( 'core/test-block-icon-normalize-function', blockType );
+			expect( getBlockType( 'core/test-block-icon-normalize-function' ) ).toEqual( {
+				name: 'core/test-block-icon-normalize-function',
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: {
+					src: MyTestIcon,
+				},
+			} );
+		} );
+
+		it( 'should correctly register an icon with background and a custom svg', () => {
+			const blockType = {
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: {
+					background: '#f00',
+					src: ( <svg width="20" height="20" viewBox="0 0 20 20">
+						<circle cx="10" cy="10" r="10"
+							fill="red" stroke="blue" strokeWidth="10" />
+					</svg> ),
+				},
+			};
+			registerBlockType( 'core/test-block-icon-normalize-background', blockType );
+			expect( getBlockType( 'core/test-block-icon-normalize-background' ) ).toEqual( {
+				name: 'core/test-block-icon-normalize-background',
+				save: noop,
+				category: 'common',
+				title: 'block title',
+				icon: {
+					background: '#f00',
+					foreground: '#191e23',
+					shadowColor: 'rgba(255, 0, 0, 0.3)',
+					src: ( <svg width="20" height="20" viewBox="0 0 20 20">
+						<circle cx="10" cy="10" r="10"
+							fill="red" stroke="blue" strokeWidth="10" />
+					</svg> ),
+				},
+			} );
+		} );
+
 		it( 'should store a copy of block type', () => {
 			const blockType = { settingName: 'settingValue', save: noop, category: 'common', title: 'block title' };
 			registerBlockType( 'core/test-block-with-settings', blockType );