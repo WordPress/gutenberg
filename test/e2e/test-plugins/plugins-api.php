9dc4350cf094d7441c2e37daa9b917d1a967ddbd
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-06-12 16:36:32 +0200

    Testing: Add plugin registration for Publish Panels

--- test/e2e/test-plugins/plugins-api.php
+++ test/e2e/test-plugins/plugins-api.php
@@ -6,6 +6,7 @@
  *
  * @package gutenberg-test-plugin-plugins-api
  */
+
 wp_enqueue_script(
 	'gutenberg-test-plugins-api-post-status-info',
 	plugins_url( 'plugins-api/post-status-info.js', __FILE__ ),
@@ -16,6 +17,19 @@ wp_enqueue_script(
 		'wp-plugins',
 	),
 	filemtime( plugin_dir_path( __FILE__ ) . 'plugins-api/post-status-info.js' ),
+	true
+);
+
+wp_enqueue_script(
+	'gutenberg-test-plugins-api-publish-pane;',
+	plugins_url( 'plugins-api/publish-panel.js', __FILE__ ),
+	array(
+		'wp-edit-post',
+		'wp-element',
+		'wp-i18n',
+		'wp-plugins',
+	),
+	filemtime( plugin_dir_path( __FILE__ ) . 'plugins-api/publish-panel.js' ),
 	true
 );
 
