493a5790275d4ccb90387004bfdde1913be3ebea
Grzegorz (Greg) Ziółkowski <grzegorz.ziolkowski@automattic.com>
2018-06-13 12:11:29 +0200

    Update hooks-api.test.js

--- test/e2e/specs/hooks-api.test.js
+++ test/e2e/specs/hooks-api.test.js
@@ -24,7 +24,7 @@ describe( 'Using Hooks API', () => {
 	it( 'Should contain a reset block button on the sidebar', async () => {
 		await page.click( '.editor-default-block-appender__content' );
 		await page.keyboard.type( 'First paragraph' );
-		expect( await page.$( '.edit-post-sidebar .reset-block-button' ) ).not.toBeNull();
+		expect( await page.$( '.edit-post-sidebar .e2e-reset-block-button' ) ).not.toBeNull();
 	} );
 
 	it( 'Pressing reset block button resets the block', async () => {
@@ -32,7 +32,7 @@ describe( 'Using Hooks API', () => {
 		await page.keyboard.type( 'First paragraph' );
 		const paragraphContent = await page.$eval( 'div[data-type="core/paragraph"] p', ( element ) => element.textContent );
 		expect( paragraphContent ).toEqual( 'First paragraph' );
-		await page.click( '.edit-post-sidebar .reset-block-button' );
+		await page.click( '.edit-post-sidebar .e2e-reset-block-button' );
 		const newParagraphContent = await page.$eval( 'div[data-type="core/paragraph"] p', ( element ) => element.textContent );
 		expect( newParagraphContent ).toEqual( '' );
 	} );
