9289d24abbcc67e5a7cd495d53371fe7c6e0e9b1
antpb <itartist.pdf@gmail.com>
2018-06-14 15:50:46 +0100

    Fixing noisysocks suggested changes

---
+++ core-blocks/playlist/edit.js
@@ -0,0 +1,125 @@
+/**
+ * External dependencies
+ */
+ import { pick } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	Button,
+	FormFileUpload,
+	IconButton,
+	Placeholder,
+	ServerSideRender,
+	Toolbar,
+} from '@wordpress/components';
+import { Component, Fragment } from '@wordpress/element';
+import {
+	MediaUpload,
+ 	BlockControls,
+} from '@wordpress/editor';
+import {	RichText } from '@wordpress/blocks';
+
+class PlaylistEdit extends Component {
+  constructor() {
+    super( ...arguments );
+
+    this.initializePlaylist = this.initializePlaylist.bind( this );
+
+    // check for if ids is set to determine edit state
+    this.state = {
+      editing: ! this.props.attributes.ids,
+    };
+  }
+
+  initializePlaylist() {
+    window.wp.playlist.initialize();
+  }
+
+  render() {
+    const { ServerSideRender } = wp.components;
+    const { ids, type } = this.props.attributes;
+    const { setAttributes, className} = this.props;
+    const { editing } = this.state;
+    const switchToEditing = () => {
+      this.setState( { editing: true } );
+    };
+    const onSelectMedia = ( media ) => {
+      //check if there are returned media items and set attributes when there are
+      if ( media && media[0].url ) {
+        media = ( 1 < media.length ) ? media : [ media ];
+        const ids = JSON.stringify( media.map( ( item ) => item.id ) );
+        setAttributes( { ids, type: media[0].type  } );
+        this.setState( { editing: false } );
+      }
+    };
+    const onSelectUrl = ( event ) => {
+      event.preventDefault();
+      if ( ids ) {
+        this.setState( { editing: false } );
+      }
+      return false;
+    };
+    const setAudio = ( [ audio ] ) => onSelectMedia( audio );
+    const uploadFromFiles = ( event ) => editorMediaUpload( event.target.files, setAudio, 'audio' );
+    const config = {};
+
+    if ( editing ) {
+      return (
+        <Placeholder
+          icon="media-audio"
+          label={ __( 'Audio/Video Playlist' ) }
+          instructions={ __( 'Select audio or video files from your library, or upload a new ones' ) }
+          className={ className }>
+          <FormFileUpload
+            isLarge
+            className="wp-block-audio__upload-button"
+            onChange={ uploadFromFiles }
+            accept="audio/*"
+          >
+            { __( 'Upload' ) }
+          </FormFileUpload>
+          <MediaUpload
+            onSelect={ onSelectMedia }
+            type="audio"
+            multiple
+            playlist
+            render={ ( { open } ) => (
+              <Button isLarge onClick={ open }>
+                { __( 'Media Library' ) }
+              </Button>
+            ) }
+          />
+        </Placeholder>
+      );
+    }
+
+    /* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+    return (
+      <Fragment>
+        <BlockControls>
+          <Toolbar>
+            <IconButton
+              className="components-icon-button components-toolbar__control"
+              label={ __( 'Edit playlist' ) }
+              onClick={ switchToEditing }
+              icon="edit"
+            />
+          </Toolbar>
+        </BlockControls>
+        <figure className={ className }>
+        <ServerSideRender
+            block="core/playlist"
+            attributes={ this.props.attributes }
+            onChange={ this.initializePlaylist }
+        />
+        </figure>
+      </Fragment>
+    );
+    /* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/onclick-has-role, jsx-a11y/click-events-have-key-events */
+  }
+}
+
+export default PlaylistEdit;
