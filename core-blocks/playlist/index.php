bc65ce22f7d86328c2c22a64619b0af49e43505c
Joe McGill <joemcgill@gmail.com>
2018-06-15 15:48:00 -0500

    Clean up dynamic rendering PHP code
    This fixes up code style issues, adds correct docblocks, and removes unneccessary script rendering which isn't necessary during server rendering.

--- core-blocks/playlist/index.php
+++ core-blocks/playlist/index.php
@@ -5,34 +5,31 @@
  * @package gutenberg
  */
 
- function render_block_core_playlist( $attributes ) {
-   $attributes[ 'ids' ] = json_decode( $attributes[ 'ids' ] );
-   return wp_playlist_shortcode( $attributes );
- }
-
 /**
  * Renders the `core/playlist` block on server.
  *
  * @param array $attributes The block attributes.
- *
+ * @return string Playlist output. Empty string if the passed type is unsupported.
  */
- function playlist_block_callback() {
-     wp_register_script(
-         'gutenberg-playlist-block',
-         plugins_url( 'gutenberg/core-blocks/playlist/index.js', __FILE__ ),
-         array( 'wp-blocks', 'wp-element' )
-     );
- 		register_block_type( 'core/playlist', array(
-		    'editor_script'   => 'playlist_block_callback',
-				'render_callback' => 'render_block_core_playlist',
-        'attributes'      => array(
-          'ids'      => array(
-      		    'type' => 'string',
-      	     ),
-      		'type'     => array(
-      		    'type' => 'string',
-      		),
-        ),
- 		) );
- }
-add_action( 'init', 'playlist_block_callback' );
+function render_block_core_playlist( $attributes ) {
+	$attributes['ids'] = json_decode( $attributes['ids'] );
+	return wp_playlist_shortcode( $attributes );
+}
+
+/**
+ * Registers the `core/playlist` block on server.
+ */
+function register_block_core_playlist() {
+	register_block_type( 'core/playlist', array(
+		'render_callback' => 'render_block_core_playlist',
+		'attributes'      => array(
+			'ids'  => array(
+				'type' => 'string',
+			),
+			'type' => array(
+				'type' => 'string',
+			),
+		),
+	) );
+}
+add_action( 'init', 'register_block_core_playlist' );
