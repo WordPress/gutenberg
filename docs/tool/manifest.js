1614fae25e1bb38e492343e2d32656f246a97f5b
Riad Benguella <benguella@gmail.com>
2018-06-20 14:14:49 +0100

    Documentation: Generates the manifest dynamically to include the data module docs in the handbook

---
+++ docs/tool/manifest.js
@@ -0,0 +1,30 @@
+/**
+ * Node dependencies
+ */
+const lodash = require( 'lodash' );
+
+/**
+ * Generates the manifest for the given namespaces.
+ *
+ * @param {Object} parsedNamespaces Parsed Namespace Object.
+ *
+ * @return {Array} manifest.
+ */
+module.exports = function( parsedNamespaces ) {
+	return [ {
+		title: 'Data Package Reference',
+		slug: 'packages-data',
+		markdown_source: 'https://raw.githubusercontent.com/WordPress/gutenberg/master/docs/data/index.md',
+		parent: 'reference',
+	} ].concat(
+		Object.values( parsedNamespaces ).map( ( parsedNamespace ) => {
+			const slug = lodash.kebabCase( parsedNamespace.name );
+			return {
+				title: parsedNamespace.title,
+				slug: 'packages-data-' + slug,
+				markdown_source: 'https://raw.githubusercontent.com/WordPress/gutenberg/master/docs/data/' + slug + '.md',
+				parent: 'packages-data',
+			};
+		} )
+	);
+};
