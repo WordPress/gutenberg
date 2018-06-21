7213b6b4f2ebe35845168e8bf8c6be501ac846fd
Adam Silverstein <adam@10up.com>
2018-06-21 13:56:59 -0400

    Downshift based author selector, first pass

--- editor/components/post-author/index.js
+++ editor/components/post-author/index.js
@@ -11,6 +11,11 @@ import { withSelect, withDispatch } from '@wordpress/data';
  */
 import PostAuthorCheck from './check';
 
+/**
+ * External dependencies
+ */
+import Downshift from 'downshift'
+
 export class PostAuthor extends Component {
 	constructor() {
 		super( ...arguments );
@@ -18,32 +23,70 @@ export class PostAuthor extends Component {
 		this.setAuthorId = this.setAuthorId.bind( this );
 	}
 
-	setAuthorId( event ) {
+	setAuthorId( selected ) {
+		console.log( selected );
 		const { onUpdateAuthor } = this.props;
-		const { value } = event.target;
-		onUpdateAuthor( Number( value ) );
+		const { id } = selected;
+		onUpdateAuthor( Number( id ) );
 	}
 
 	render() {
 		const { postAuthor, instanceId, authors } = this.props;
 		const selectId = 'post-author-selector-' + instanceId;
 
-		// Disable reason: A select with an onchange throws a warning
+		const authorName = authors
+			.filter( author => author.id === postAuthor )[0]
+			.name
 
 		/* eslint-disable jsx-a11y/no-onchange */
 		return (
 			<PostAuthorCheck>
 				<label htmlFor={ selectId }>{ __( 'Author' ) }</label>
-				<select
-					id={ selectId }
-					value={ postAuthor }
-					onChange={ this.setAuthorId }
-					className="editor-post-author__select"
+				<Downshift
+					onChange={this.setAuthorId}
+					itemToString={author => (author ? author.value : '')}
+					defaultInputValue={ authorName }
 				>
-					{ authors.map( ( author ) => (
-						<option key={ author.id } value={ author.id }>{ author.name }</option>
-					) ) }
-				</select>
+					{({
+						getInputProps,
+						getItemProps,
+						getLabelProps,
+						getMenuProps,
+						isOpen,
+						inputValue,
+						highlightedIndex,
+						selectedItem,
+					}) => (
+						<div>
+						<input id={ selectId } {...getInputProps()} />
+						<ul className="editor-post-author__select" {...getMenuProps()}>
+							{isOpen
+							? authors
+								.map( author => ( { id: author.id, value: author.name} ) )
+								.filter( author =>
+									! inputValue ||
+									author.value.toLowerCase().includes(inputValue.toLowerCase() ) )
+								.map( ( author, index) => (
+									<li
+									{ ...getItemProps( {
+										key: author.id,
+										index,
+										item: author,
+										style: {
+										backgroundColor:
+											highlightedIndex === index ? 'lightgray' : 'white',
+										fontWeight: selectedItem === author ? 'bold' : 'normal',
+										},
+									} ) }
+									>
+									{author.value}
+									</li>
+								))
+							: null}
+						</ul>
+						</div>
+					) }
+				</Downshift>
 			</PostAuthorCheck>
 		);
 		/* eslint-enable jsx-a11y/no-onchange */
