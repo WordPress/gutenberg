9eff5be39a73a05298aca1d27fb5597c966dfa63
antpb <itartist.pdf@gmail.com>
2018-06-21 15:47:37 -0500

    @soean provided me the golden snippet of code to fix uploads

--- core-blocks/playlist/edit.js
+++ core-blocks/playlist/edit.js
@@ -48,11 +48,17 @@ class PlaylistEdit extends Component {
 	}
 
 	addFiles( files ) {
-		editorMediaUpload( {
-			allowedType: 'audio',
-			filesList: files,
-		} );
-	}
+	        const { setAttributes } = this.props;
+	        editorMediaUpload( {
+	            allowedType: 'audio',
+	            filesList: files,
+	            onFileChange: ( media ) => {
+	                const ids = JSON.stringify( media.map( ( item ) => item.id ) );
+	                setAttributes( { ids, type: 'audio' } );
+	                this.setState( { editing: false } );
+	            },
+	        } );
+	    }
 
 	render() {
 		const { attributes, setAttributes, className } = this.props;
