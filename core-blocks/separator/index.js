07f1b2436c4b5bbec402a691349377623596f829
Matías Ventura <mv@matiasventura.com>
2018-06-25 13:01:18 +0200

    Add alternate styles for separator.

--- core-blocks/separator/index.js
+++ core-blocks/separator/index.js
@@ -24,6 +24,11 @@ export const settings = {
 	keywords: [ __( 'horizontal-line' ), 'hr', __( 'divider' ) ],
 
 	transforms: {
+		styles: [
+			{ name: 'default', label: __( 'Short Line' ), isDefault: true },
+			{ name: 'wide', label: __( 'Wide Line' ) },
+			{ name: 'asterisks', label: __( 'Asterisks' ) },
+		],
 		from: [
 			{
 				type: 'pattern',
