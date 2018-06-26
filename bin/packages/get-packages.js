980d38bd56f7f44567c2ea709737010c5e88170f
Andrew Duthie <andrew@andrewduthie.com>
2018-06-26 12:15:24 -0400

    Build: Consolidate directory testing into filterPackages

--- bin/packages/get-packages.js
+++ bin/packages/get-packages.js
@@ -43,14 +43,27 @@ const createCommaSeparatedFilter = ( filterFn, list ) => {
 };
 
 /**
+ * Returns true if the given base file name for a file within the packages
+ * directory is itself a directory.
+ *
+ * @param {string} file Packages directory file.
+ *
+ * @return {boolean} Whether file is a directory.
+ */
+function isDirectory( file ) {
+	return fs.lstatSync( path.resolve( PACKAGES_DIR, file ) ).isDirectory();
+}
+
+/**
  * Filter predicate, returning true if the given base file name is to be
- * included in the build, based on BUILD_PACKAGES environment variable.
+ * included in the build.
  *
  * @param {string} pkg File base name to test.
  *
  * @return {boolean} Whether to include file in build.
  */
 const filterPackages = overEvery( compact( [
+	isDirectory,
 	INCLUDE_PACKAGES && createCommaSeparatedFilter( includes, INCLUDE_PACKAGES ),
 	EXCLUDE_PACKAGES && createCommaSeparatedFilter( negate( includes ), EXCLUDE_PACKAGES ),
 ] ) );
@@ -64,8 +77,7 @@ function getPackages() {
 	return fs
 		.readdirSync( PACKAGES_DIR )
 		.filter( filterPackages )
-		.map( ( file ) => path.resolve( PACKAGES_DIR, file ) )
-		.filter( ( f ) => fs.lstatSync( path.resolve( f ) ).isDirectory() );
+		.map( ( file ) => path.resolve( PACKAGES_DIR, file ) );
 }
 
 module.exports = getPackages;
