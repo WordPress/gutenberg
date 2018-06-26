2baf324c7098384ae6db8f39ec34f0e5c1cf1b13
Jorge <jorge.costa@developer.pt>
2018-06-26 17:22:15 +0100

    Make publish panel accessible

--- editor/components/post-publish-panel/index.js
+++ editor/components/post-publish-panel/index.js
@@ -62,14 +62,14 @@ class PostPublishPanel extends Component {
 	}
 
 	render() {
-		const { isScheduled, onClose, forceIsDirty, forceIsSaving, PrePublishExtension, PostPublishExtension } = this.props;
+		const { isScheduled, onClose, forceIsDirty, forceIsSaving, PrePublishExtension, PostPublishExtension, ...additionalProps } = this.props;
 		const { loading, submitted } = this.state;
 		return (
-			<div className="editor-post-publish-panel">
+			<div className="editor-post-publish-panel" { ...additionalProps }>
 				<div className="editor-post-publish-panel__header">
 					{ ! submitted && (
 						<div className="editor-post-publish-panel__header-publish-button">
-							<PostPublishButton onSubmit={ this.onSubmit } forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />
+							<PostPublishButton focusOnMount={ true } onSubmit={ this.onSubmit } forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />
 						</div>
 					) }
 					{ submitted && (
@@ -78,6 +78,7 @@ class PostPublishPanel extends Component {
 						</div>
 					) }
 					<IconButton
+						aria-expanded={ true }
 						onClick={ onClose }
 						icon="no-alt"
 						label={ __( 'Close Publish Panel' ) }
