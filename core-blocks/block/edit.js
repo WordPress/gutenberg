be104fc14c2d4758cda164c4791589e2cb14fe03
Sören Wrede <soerenwrede@gmail.com>
2018-06-27 23:06:02 +0200

    consistency for button lables and styles

--- core-blocks/block/edit.js
+++ core-blocks/block/edit.js
@@ -117,6 +117,7 @@ class SharedBlockEdit extends Component {
 				{ element }
 				{ ( isSelected || isEditing ) && (
 					<SharedBlockEditPanel
+						isSelected={ isSelected }
 						isEditing={ isEditing }
 						title={ title !== null ? title : sharedBlock.title }
 						isSaving={ isSaving && ! sharedBlock.isTemporary }
