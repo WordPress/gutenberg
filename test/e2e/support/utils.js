956ab99be7ba1c8b72490805836e1022ffcad69b
Jorge <jorge.costa@developer.pt>
2018-06-27 20:41:26 +0100

    Add end 2 end test to block icons

--- test/e2e/support/utils.js
+++ test/e2e/support/utils.js
@@ -174,18 +174,26 @@ export async function ensureSidebarOpened() {
 }
 
 /**
+ * Search for block in the global inserter
+ *
+ * @param {string} searchTerm The text to search the inserter for.
+ */
+export async function searchForBlock( searchTerm ) {
+	await page.click( '.edit-post-header [aria-label="Add block"]' );
+	// Waiting here is necessary because sometimes the inserter takes more time to
+	// render than Puppeteer takes to complete the 'click' action
+	await page.waitForSelector( '.editor-inserter__menu' );
+	await page.keyboard.type( searchTerm );
+}
+
+/**
  * Opens the inserter, searches for the given term, then selects the first
  * result that appears.
  *
  * @param {string} searchTerm The text to search the inserter for.
  */
 export async function insertBlock( searchTerm ) {
-	await page.click( '.edit-post-header [aria-label="Add block"]' );
-	// Waiting here is necessary because sometimes the inserter takes more time to
-	// render than Puppeteer takes to complete the 'click' action
-	await page.waitForSelector( '.editor-inserter__menu' );
-	// Filter and reveal buttons.
-	await page.keyboard.type( searchTerm );
+	await searchForBlock( searchTerm );
 	await page.click( `button[aria-label="${ searchTerm }"]` );
 }
 
