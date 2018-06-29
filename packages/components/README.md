a5ee024603e210ba1f50b0eae8081c82c1f43cf5
Riad Benguella <benguella@gmail.com>
2018-06-29 16:13:15 +0100

    Preparing the components package to be reusable

---
+++ packages/components/README.md
@@ -0,0 +1,29 @@
+Components
+==========
+
+This packages includes a library of generic React components to be used for creating common UI elements shared between screens and features of the WordPress dashboard.
+
+## Installation
+
+Install the module
+
+```bash
+npm install @wordpress/components --save
+```
+
+## Usage
+
+Within Gutenberg, these components can be accessed by importing from the `components` root directory:
+
+```jsx
+/**
+ * WordPress dependencies
+ */
+import { Button } from '@wordpress/components';
+
+export default function MyButton() {
+	return <Button>Click Me!</Button>;
+}
+```
+
+<br/><br/><p align="center"><img src="https://s.w.org/style/images/codeispoetry.png?1" alt="Code is Poetry." /></p>
---
+++ packages/components/src/external-link/index.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { compact, uniq } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Dashicon from '../dashicon';
+
+function ExternalLink( { href, children, className, rel = '', ...additionalProps } ) {
+	rel = uniq( compact( [
+		...rel.split( ' ' ),
+		'external',
+		'noreferrer',
+		'noopener',
+	] ) ).join( ' ' );
+	const classes = classnames( 'components-external-link', className );
+	return (
+		<a { ...additionalProps } className={ classes } href={ href } target="_blank" rel={ rel }>
+			{ children }
+			<span className="screen-reader-text">
+				{
+					/* translators: accessibility text */
+					__( '(opens in a new window)' )
+				}
+			</span>
+			<Dashicon icon="external" />
+		</a>
+	);
+}
+
+export default ExternalLink;
