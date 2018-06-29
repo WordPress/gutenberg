654f30fd21bb39042d173d9dcddf9e1e19e6db67
Riad Benguella <benguella@gmail.com>
2018-06-29 12:26:19 +0100

    Documentation: Adding the packages READMEs to the handbook

--- docs/tool/manifest.js
+++ docs/tool/manifest.js
@@ -7,11 +7,12 @@ const { kebabCase } = require( 'lodash' );
  * Generates the manifest for the given namespaces.
  *
  * @param {Object} parsedNamespaces Parsed Namespace Object.
+ * @param {Object} packagesConfig   Packages Docs Config.
  *
  * @return {Array} manifest.
  */
-module.exports = function( parsedNamespaces ) {
-	return [ {
+module.exports = function( parsedNamespaces, packagesConfig ) {
+	const dataManifest = [ {
 		title: 'Data Package Reference',
 		slug: 'data',
 		markdown_source: 'https://raw.githubusercontent.com/WordPress/gutenberg/master/docs/data/README.md',
@@ -27,4 +28,18 @@ module.exports = function( parsedNamespaces ) {
 			};
 		} )
 	);
+
+	const packagesManifest = Object.entries( packagesConfig ).map( ( [ packageSlug, config ] ) => {
+		const path = config.isNpmReady === false ?
+			'https://raw.githubusercontent.com/WordPress/gutenberg/master/' + packageSlug + '/README.md' :
+			'https://raw.githubusercontent.com/WordPress/gutenberg/master/packages/' + packageSlug + '/README.md';
+		return {
+			title: '@wordpress/' + packageSlug,
+			slug: 'packages-' + packageSlug,
+			markdown_source: path,
+			parent: 'packages',
+		};
+	} );
+
+	return packagesManifest.concat( dataManifest );
 };
