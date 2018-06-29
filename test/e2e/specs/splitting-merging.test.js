dd0e34796c3eaf8c7862e606654e57d2eaa57ffa
Andrew Duthie <andrew@andrewduthie.com>
2018-06-29 09:22:39 -0400

    Testing: Update splitting test to ensure caret merge positioning

--- test/e2e/specs/splitting-merging.test.js
+++ test/e2e/specs/splitting-merging.test.js
@@ -2,7 +2,13 @@
  * Internal dependencies
  */
 import '../support/bootstrap';
-import { newPost, newDesktopBrowserPage, insertBlock } from '../support/utils';
+import {
+	newPost,
+	newDesktopBrowserPage,
+	insertBlock,
+	getHTMLFromCodeEditor,
+	switchToEditor,
+} from '../support/utils';
 
 describe( 'splitting and merging blocks', () => {
 	beforeAll( async () => {
@@ -11,7 +17,7 @@ describe( 'splitting and merging blocks', () => {
 	} );
 
 	it( 'Should split and merge paragraph blocks using Enter and Backspace', async () => {
-		//Use regular inserter to add paragraph block and text
+		// Use regular inserter to add paragraph block and text
 		await insertBlock( 'Paragraph' );
 		await page.keyboard.type( 'FirstSecond' );
 
@@ -21,32 +27,23 @@ describe( 'splitting and merging blocks', () => {
 		}
 		await page.keyboard.press( 'Enter' );
 
-		//Switch to Code Editor to check HTML output
-		await page.click( '.edit-post-more-menu [aria-label="More"]' );
-		let codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
-		await codeEditorButton.click( 'button' );
-
-		//Assert that there are now two paragraph blocks with correct content
-		let textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
+		// Assert that there are now two paragraph blocks with correct content
+		let textEditorContent = await getHTMLFromCodeEditor();
 		expect( textEditorContent ).toMatchSnapshot();
 
-		//Switch to Visual Editor to continue testing
-		await page.click( '.edit-post-more-menu [aria-label="More"]' );
-		const visualEditorButton = ( await page.$x( '//button[contains(text(), \'Visual Editor\')]' ) )[ 0 ];
-		await visualEditorButton.click( 'button' );
+		// Switch to Visual Editor to continue testing
+		await switchToEditor( 'Visual' );
 
-		//Press Backspace to merge paragraph blocks
+		// Press Backspace to merge paragraph blocks
 		await page.click( '.is-selected' );
 		await page.keyboard.press( 'Home' );
 		await page.keyboard.press( 'Backspace' );
 
-		//Switch to Code Editor to check HTML output
-		await page.click( '.edit-post-more-menu [aria-label="More"]' );
-		codeEditorButton = ( await page.$x( '//button[contains(text(), \'Code Editor\')]' ) )[ 0 ];
-		await codeEditorButton.click( 'button' );
+		// Ensure that caret position is correctly placed at the between point.
+		await page.keyboard.type( 'Between' );
 
-		//Assert that there is now one paragraph with correct content
-		textEditorContent = await page.$eval( '.editor-post-text-editor', ( element ) => element.value );
+		// Assert that there is now one paragraph with correct content
+		textEditorContent = await getHTMLFromCodeEditor();
 		expect( textEditorContent ).toMatchSnapshot();
 	} );
 } );
