86e5df123b87d9e8cbfccc5f03331cca2215deca
Robert Anderson <robert@noisysocks.com>
2018-06-29 13:54:23 +1000

    Make FileBlockInspector a function component

--- core-blocks/file/inspector.js
+++ core-blocks/file/inspector.js
@@ -7,61 +7,56 @@ import {
 	SelectControl,
 	ToggleControl,
 } from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
-import {
-	InspectorControls,
-} from '@wordpress/editor';
+import { Fragment } from '@wordpress/element';
+import { InspectorControls } from '@wordpress/editor';
 
 function getDownloadButtonHelp( checked ) {
 	return checked ? __( 'The download button is shown.' ) : __( 'The download button is hidden.' );
 }
 
-export default class FileBlockInspector extends Component {
-	render() {
-		const {
-			hrefs,
-			openInNewWindow,
-			showDownloadButton,
-			changeLinkDestinationOption,
-			changeOpenInNewWindow,
-			changeShowDownloadButton,
-		} = this.props;
-		const { href, textLinkHref, attachmentPage } = hrefs;
+export default function FileBlockInspector( {
+	hrefs,
+	openInNewWindow,
+	showDownloadButton,
+	changeLinkDestinationOption,
+	changeOpenInNewWindow,
+	changeShowDownloadButton,
+} ) {
+	const { href, textLinkHref, attachmentPage } = hrefs;
 
-		let linkDestinationOptions = [ { value: href, label: __( 'URL' ) } ];
-		if ( attachmentPage ) {
-			linkDestinationOptions = [
-				{ value: href, label: __( 'Media File' ) },
-				{ value: attachmentPage, label: __( 'Attachment Page' ) },
-			];
-		}
-
-		return (
-			<Fragment>
-				<InspectorControls>
-					<PanelBody title={ __( 'Text Link Settings' ) }>
-						<SelectControl
-							label={ __( 'Link To' ) }
-							value={ textLinkHref }
-							options={ linkDestinationOptions }
-							onChange={ changeLinkDestinationOption }
-						/>
-						<ToggleControl
-							label={ __( 'Open in New Window' ) }
-							checked={ openInNewWindow }
-							onChange={ changeOpenInNewWindow }
-						/>
-					</PanelBody>
-					<PanelBody title={ __( 'Download Button Settings' ) }>
-						<ToggleControl
-							label={ __( 'Show Download Button' ) }
-							help={ getDownloadButtonHelp }
-							checked={ showDownloadButton }
-							onChange={ changeShowDownloadButton }
-						/>
-					</PanelBody>
-				</InspectorControls>
-			</Fragment>
-		);
+	let linkDestinationOptions = [ { value: href, label: __( 'URL' ) } ];
+	if ( attachmentPage ) {
+		linkDestinationOptions = [
+			{ value: href, label: __( 'Media File' ) },
+			{ value: attachmentPage, label: __( 'Attachment Page' ) },
+		];
 	}
+
+	return (
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={ __( 'Text Link Settings' ) }>
+					<SelectControl
+						label={ __( 'Link To' ) }
+						value={ textLinkHref }
+						options={ linkDestinationOptions }
+						onChange={ changeLinkDestinationOption }
+					/>
+					<ToggleControl
+						label={ __( 'Open in New Window' ) }
+						checked={ openInNewWindow }
+						onChange={ changeOpenInNewWindow }
+					/>
+				</PanelBody>
+				<PanelBody title={ __( 'Download Button Settings' ) }>
+					<ToggleControl
+						label={ __( 'Show Download Button' ) }
+						help={ getDownloadButtonHelp }
+						checked={ showDownloadButton }
+						onChange={ changeShowDownloadButton }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		</Fragment>
+	);
 }
