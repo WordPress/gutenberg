5a48bc2f9b9c8a3f0df245ec4d0cb6c299853a6b
nfmohit <nfmohit49@gmail.com>
2018-07-02 05:24:44 +0600

    Remove unnecessary min attribute from text-control component

--- components/text-control/index.js
+++ components/text-control/index.js
@@ -5,7 +5,7 @@ import BaseControl from '../base-control';
 import withInstanceId from '../higher-order/with-instance-id';
 import './style.scss';
 
-function TextControl( { label, value, help, className, instanceId, min, onChange, type = 'text', ...props } ) {
+function TextControl( { label, value, help, className, instanceId, onChange, type = 'text', ...props } ) {
 	const id = `inspector-text-control-${ instanceId }`;
 	const onChangeValue = ( event ) => onChange( event.target.value );
 
@@ -15,7 +15,6 @@ function TextControl( { label, value, help, className, instanceId, min, onChange
 				type={ type }
 				id={ id }
 				value={ value }
-				min={ min }
 				onChange={ onChangeValue }
 				aria-describedby={ !! help ? id + '__help' : undefined }
 				{ ...props }
