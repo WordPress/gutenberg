7e5bd6dbc7d3735d712a5e150d30468ebb0a113e
Sören Wrede <soerenwrede@gmail.com>
2018-07-02 23:30:16 +0200

    Add video block attributes

--- core-blocks/video/index.js
+++ core-blocks/video/index.js
@@ -26,19 +26,44 @@ export const settings = {
 	category: 'common',
 
 	attributes: {
+		autoplay: {
+			type: 'boolean',
+			source: 'attribute',
+			selector: 'video',
+			attribute: 'autoplay',
+		},
+		caption: {
+			type: 'array',
+			source: 'children',
+			selector: 'figcaption',
+		},
+		controls: {
+			type: 'boolean',
+			source: 'attribute',
+			selector: 'video',
+			attribute: 'controls',
+			default: true,
+		},
 		id: {
 			type: 'number',
+		},
+		loop: {
+			type: 'boolean',
+			source: 'attribute',
+			selector: 'video',
+			attribute: 'loop',
+		},
+		muted: {
+			type: 'boolean',
+			source: 'attribute',
+			selector: 'video',
+			attribute: 'muted',
 		},
 		src: {
 			type: 'string',
 			source: 'attribute',
 			selector: 'video',
 			attribute: 'src',
-		},
-		caption: {
-			type: 'array',
-			source: 'children',
-			selector: 'figcaption',
 		},
 	},
 
@@ -49,12 +74,22 @@ export const settings = {
 	edit,
 
 	save( { attributes } ) {
-		const { src, caption } = attributes;
+		const { autoplay, caption, controls, loop, muted, src } = attributes;
 		return (
 
 			<figure>
-				{ src && <video controls src={ src } /> }
-				{ caption && caption.length > 0 && <RichText.Content tagName="figcaption" value={ caption } /> }
+				{ src && (
+					<video
+						autoPlay={ autoplay }
+						controls={ controls }
+						src={ src }
+						loop={ loop }
+						muted={ muted }
+					/>
+				) }
+				{ caption && caption.length > 0 && (
+					<RichText.Content tagName="figcaption" value={ caption } />
+				) }
 			</figure>
 		);
 	},
