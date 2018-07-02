0e6c9c26a46c9c7db7811315552d36dfb7abc149
Sofia Sousa <csofiamsousa@gmail.com>
2018-07-02 15:21:56 +0100

    Export i18n module

--- gutenberg-package/src/js/index.js
+++ gutenberg-package/src/js/index.js
@@ -8,6 +8,7 @@ import '@wordpress/core-data';
 /**
  * WordPress dependencies
  */
+import * as i18n from '@wordpress/i18n';
 import * as blocks from '@wordpress/blocks';
 import * as components from '@wordpress/components';
 import * as coreBlocks from '@wordpress/core-blocks';
@@ -20,6 +21,7 @@ import * as plugins from '@wordpress/plugins';
 import * as storypage from 'storypage';
 
 export {
+	i18n,
 	blocks,
 	components,
 	coreBlocks,
