1465a4bdedeca547a95089e0edd0203c20bafbf8
Oliver Juhas <webmandesign@users.noreply.github.com>
2018-07-03 15:35:26 +0200

    Multiple parameters backcompat with `_doing_it_wrong` instead of `console.warn`

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -1152,11 +1152,10 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 	// Backcompat for Color Palette set as multiple parameters.
 	if ( is_string( $color_palette ) || isset( $color_palette['color'] ) ) {
 		$color_palette = get_theme_support( 'editor-color-palette' );
-		wp_add_inline_script(
-			'wp-edit-post',
-			'console.warn( "' .
-				__( 'Setting colors using multiple parameters is deprecated. Please pass a single parameter with an array of colors. See https://wordpress.org/gutenberg/handbook/extensibility/theme-support/ for details.', 'gutenberg' ) .
-			'");'
+		_doing_it_wrong(
+			'add_theme_support()',
+			__( 'Setting colors using multiple parameters is deprecated. Please pass a single parameter with an array of colors. See https://wordpress.org/gutenberg/handbook/extensibility/theme-support/ for details.', 'gutenberg' ),
+			'3.2.0'
 		);
 	}
 
