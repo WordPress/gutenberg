6d555c76f364656b8fd10a8e44d8fdc565029d66
Adam Silverstein <adam@10up.com>
2018-07-04 17:12:50 -0400

    use <Autocomplete /> component for more than 50 authors

--- editor/components/post-author/index.js
+++ editor/components/post-author/index.js
@@ -15,7 +15,8 @@ import PostAuthorCheck from './check';
 /**
  * External dependencies
  */
-import accessibleAutocomplete from 'accessible-autocomplete';
+import Autocomplete from 'accessible-autocomplete/react'
+
 import './accessible-autocomplete.css';
 import { findWhere, debounce } from 'underscore';
 
@@ -25,33 +26,6 @@ export class PostAuthor extends Component {
 
 		this.setAuthorId = this.setAuthorId.bind( this );
 		this.suggestAuthor = this.suggestAuthor.bind( this );
-	}
-
-	componentDidMount() {
-		const { instanceId, authors } = this.props;
-		this.authors = authors;
-		if ( authors.length > 50 ) {
-			accessibleAutocomplete.enhanceSelectElement( {
-				selectElement: document.querySelector( '#post-author-selector-' + instanceId ),
-				minLength: 2,
-				showAllValues: true,
-				autoselect: true,
-				displayMenu: 'overlay',
-				onConfirm: this.setAuthorId,
-				source: debounce( this.suggestAuthor, 300 ),
-				tNoResults: () => __( 'No results found' ),
-				tStatusQueryTooShort: ( minQueryLength ) => __( `Type in ${ minQueryLength } or more characters for results` ),
-				tStatusNoResults: () => __( 'No search results' ),
-				tStatusSelectedOption: ( selectedOption, length ) => __( `${ selectedOption } (1 of ${ length }) is selected` ),
-				tStatusResults: ( length, contentSelectedOption ) => {
-					const words = {
-						result: ( length === 1 ) ? __( 'result' ) : __( 'results' ),
-						is: ( length === 1 ) ? __( 'is' ) : __( 'are' ),
-					};
-					return <span>{ length } { words.result } { words.is } available. { contentSelectedOption }</span>;
-				},
-			} );
-		}
 	}
 
 	suggestAuthor( query, populateResults ) {
@@ -75,22 +49,52 @@ export class PostAuthor extends Component {
 	}
 
 	render() {
-		const { postAuthor, instanceId, authors } = this.props;
+		const { postAuthorId, postAuthor, instanceId, authors } = this.props;
 		const selectId = 'post-author-selector-' + instanceId;
+		let selector;
+
+		if ( authors.length < 50 ) {
+			selector = <select
+							id={ selectId }
+							value={ postAuthorId }
+							className="editor-post-author__select"
+						>
+							{ authors.map( ( author ) => (
+								<option key={ author.id } value={ author.id }>{ author.name }</option>
+							) ) }
+						</select>;
+		} else {
+			selector = <Autocomplete
+							id={ selectId }
+							source={ authors }
+							minLength={ 2 }
+							showAllValues={ true }
+							defaultValue={ postAuthor && postAuthor[0] ? postAuthor[0].name : '' }
+							autoselect={ true }
+							displayMenu='overlay'
+							onConfirm={ this.setAuthorId }
+							source={ debounce( this.suggestAuthor, 300 ) }
+							showNoResultsFound={ false }
+							tStatusQueryTooShort={ ( minQueryLength ) =>
+								__( `Type in ${ minQueryLength } or more characters for results` ) }
+							tStatusNoResults={ () => __( 'No search results' ) }
+							tStatusSelectedOption={ ( selectedOption, length ) => __( `${ selectedOption } (1 of ${ length }) is selected` ) }
+							tStatusResults={ ( length, contentSelectedOption ) => {
+								const words = {
+									result: ( length === 1 ) ? __( 'result' ) : __( 'results' ),
+									is: ( length === 1 ) ? __( 'is' ) : __( 'are' ),
+								};
+								return <span>{ length } { words.result } { words.is } available. { contentSelectedOption }</span>;
+							} }
+
+						/>;
+		}
 
 		/* eslint-disable jsx-a11y/no-onchange */
 		return (
 			<PostAuthorCheck>
 				<label htmlFor={ selectId }>{ __( 'Author' ) }</label>
-				<select
-					id={ selectId }
-					value={ postAuthor }
-					className="editor-post-author__select"
-				>
-					{ authors.map( ( author ) => (
-						<option key={ author.id } value={ author.id }>{ author.name }</option>
-					) ) }
-				</select>
+				{ postAuthor[0] && selector }
 			</PostAuthorCheck>
 		);
 		/* eslint-enable jsx-a11y/no-onchange */
@@ -100,7 +104,10 @@ export class PostAuthor extends Component {
 export default compose( [
 	withSelect( ( select ) => {
 		return {
-			postAuthor: select( 'core/editor' ).getEditedPostAttribute( 'author' ),
+			postAuthorId: select( 'core/editor' ).getEditedPostAttribute( 'author' ),
+			postAuthor: select( 'core' ).getAuthor(
+				select( 'core/editor' ).getEditedPostAttribute( 'author' )
+			),
 			authors: select( 'core' ).getAuthors(),
 		};
 	} ),
