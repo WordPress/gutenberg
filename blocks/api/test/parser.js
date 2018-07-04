5585180f26b30bf9ad440975309c95dd0ec6e738
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-07-04 13:01:54 +0300

    Run parse() testcases for the pre-generated parser too

--- blocks/api/test/parser.js
+++ blocks/api/test/parser.js
@@ -12,10 +12,11 @@ import {
 	asType,
 	createBlockWithFallback,
 	getAttributesAndInnerBlocksFromDeprecatedVersion,
-	default as parse,
+	default as parsePegjs,
 	parseWithAttributeSchema,
 	toBooleanAttributeMatcher,
 } from '../parser';
+import { parse as grammarParsePreGenerated } from '../post-grammar-parser-pre-generated';
 import {
 	registerBlockType,
 	unregisterBlockType,
@@ -24,6 +25,25 @@ import {
 } from '../registration';
 import { createBlock } from '../factory';
 import serialize from '../serializer';
+
+/**
+ * Parses the post content with a gremmar pre-generated from PegJS and returns a list of blocks.
+ * It's otherwise identical to parseWithGrammar from parser.js
+ * TODO: find a better way to avoid re-defining this function.
+ *
+ * @param {string} content The post content.
+ *
+ * @return {Array} Block list.
+ */
+export function parsePreGenerated( content ) {
+	return grammarParsePreGenerated( content ).reduce( ( memo, blockNode ) => {
+		const block = createBlockWithFallback( blockNode );
+		if ( block ) {
+			memo.push( block );
+		}
+		return memo;
+	}, [] );
+}
 
 describe( 'block parser', () => {
 	const defaultBlockSettings = {
@@ -504,187 +524,196 @@ describe( 'block parser', () => {
 	} );
 
 	describe( 'parse()', () => {
-		it( 'should parse the post content, including block attributes', () => {
-			registerBlockType( 'core/test-block', {
-				attributes: {
-					content: {
-						type: 'string',
-						source: 'text',
+		// run the test cases using the PegJS defined parser
+		testCases( parsePegjs );
+
+		// run the test cases using the pre-generated parser
+		testCases( parsePreGenerated );
+
+		// encapsulate the test cases so we can run them multiple time but with a different parse() function
+		function testCases( parse ) {
+			it( 'should parse the post content, including block attributes', () => {
+				registerBlockType( 'core/test-block', {
+					attributes: {
+						content: {
+							type: 'string',
+							source: 'text',
+						},
+						smoked: { type: 'string' },
+						url: { type: 'string' },
+						chicken: { type: 'string' },
 					},
-					smoked: { type: 'string' },
-					url: { type: 'string' },
-					chicken: { type: 'string' },
-				},
-				save: ( { attributes } ) => attributes.content,
-				category: 'common',
-				title: 'test block',
+					save: ( { attributes } ) => attributes.content,
+					category: 'common',
+					title: 'test block',
+				} );
+
+				const parsed = parse(
+					'<!-- wp:core/test-block {"smoked":"yes","url":"http://google.com","chicken":"ribs & \'wings\'"} -->' +
+					'Brisket' +
+					'<!-- /wp:core/test-block -->'
+				);
+
+				expect( parsed ).toHaveLength( 1 );
+				expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
+				expect( parsed[ 0 ].attributes ).toEqual( {
+					content: 'Brisket',
+					smoked: 'yes',
+					url: 'http://google.com',
+					chicken: 'ribs & \'wings\'',
+				} );
+				expect( typeof parsed[ 0 ].uid ).toBe( 'string' );
 			} );
 
-			const parsed = parse(
-				'<!-- wp:core/test-block {"smoked":"yes","url":"http://google.com","chicken":"ribs & \'wings\'"} -->' +
-				'Brisket' +
-				'<!-- /wp:core/test-block -->'
-			);
+			it( 'should parse empty post content', () => {
+				const parsed = parse( '' );
 
-			expect( parsed ).toHaveLength( 1 );
-			expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
-			expect( parsed[ 0 ].attributes ).toEqual( {
-				content: 'Brisket',
-				smoked: 'yes',
-				url: 'http://google.com',
-				chicken: 'ribs & \'wings\'',
+				expect( parsed ).toEqual( [] );
 			} );
-			expect( typeof parsed[ 0 ].uid ).toBe( 'string' );
-		} );
 
-		it( 'should parse empty post content', () => {
-			const parsed = parse( '' );
-
-			expect( parsed ).toEqual( [] );
-		} );
-
-		it( 'should parse the post content, ignoring unknown blocks', () => {
-			registerBlockType( 'core/test-block', {
-				attributes: {
-					content: {
-						type: 'string',
-						source: 'text',
+			it( 'should parse the post content, ignoring unknown blocks', () => {
+				registerBlockType( 'core/test-block', {
+					attributes: {
+						content: {
+							type: 'string',
+							source: 'text',
+						},
 					},
-				},
-				save: ( { attributes } ) => attributes.content,
-				category: 'common',
-				title: 'test block',
+					save: ( { attributes } ) => attributes.content,
+					category: 'common',
+					title: 'test block',
+				} );
+
+				const parsed = parse(
+					'<!-- wp:core/test-block -->\nRibs\n<!-- /wp:core/test-block -->' +
+					'<p>Broccoli</p>' +
+					'<!-- wp:core/unknown-block -->Ribs<!-- /wp:core/unknown-block -->'
+				);
+
+				expect( parsed ).toHaveLength( 1 );
+				expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
+				expect( parsed[ 0 ].attributes ).toEqual( {
+					content: 'Ribs',
+				} );
+				expect( typeof parsed[ 0 ].uid ).toBe( 'string' );
 			} );
 
-			const parsed = parse(
-				'<!-- wp:core/test-block -->\nRibs\n<!-- /wp:core/test-block -->' +
-				'<p>Broccoli</p>' +
-				'<!-- wp:core/unknown-block -->Ribs<!-- /wp:core/unknown-block -->'
-			);
+			it( 'should add the core namespace to un-namespaced blocks', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
 
-			expect( parsed ).toHaveLength( 1 );
-			expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
-			expect( parsed[ 0 ].attributes ).toEqual( {
-				content: 'Ribs',
+				const parsed = parse(
+					'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->'
+				);
+
+				expect( parsed ).toHaveLength( 1 );
+				expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
 			} );
-			expect( typeof parsed[ 0 ].uid ).toBe( 'string' );
-		} );
 
-		it( 'should add the core namespace to un-namespaced blocks', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
+			it( 'should ignore blocks with a bad namespace', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
 
-			const parsed = parse(
-				'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->'
-			);
+				setUnknownTypeHandlerName( 'core/unknown-block' );
 
-			expect( parsed ).toHaveLength( 1 );
-			expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
-		} );
+				const parsed = parse(
+					'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
+					'<p>Broccoli</p>' +
+					'<!-- wp:core/unknown/block -->Ribs<!-- /wp:core/unknown/block -->'
+				);
+				expect( parsed ).toHaveLength( 1 );
+				expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
+			} );
 
-		it( 'should ignore blocks with a bad namespace', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
+			it( 'should parse the post content, using unknown block handler', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
+				registerBlockType( 'core/unknown-block', unknownBlockSettings );
 
-			setUnknownTypeHandlerName( 'core/unknown-block' );
+				setUnknownTypeHandlerName( 'core/unknown-block' );
 
-			const parsed = parse(
-				'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
-				'<p>Broccoli</p>' +
-				'<!-- wp:core/unknown/block -->Ribs<!-- /wp:core/unknown/block -->'
-			);
-			expect( parsed ).toHaveLength( 1 );
-			expect( parsed[ 0 ].name ).toBe( 'core/test-block' );
-		} );
+				const parsed = parse(
+					'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
+					'<p>Broccoli</p>' +
+					'<!-- wp:core/unknown-block -->Ribs<!-- /wp:core/unknown-block -->'
+				);
 
-		it( 'should parse the post content, using unknown block handler', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
-			registerBlockType( 'core/unknown-block', unknownBlockSettings );
+				expect( parsed ).toHaveLength( 3 );
+				expect( parsed.map( ( { name } ) => name ) ).toEqual( [
+					'core/test-block',
+					'core/unknown-block',
+					'core/unknown-block',
+				] );
+			} );
 
-			setUnknownTypeHandlerName( 'core/unknown-block' );
+			it( 'should parse the post content, including raw HTML at each end', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
+				registerBlockType( 'core/unknown-block', unknownBlockSettings );
 
-			const parsed = parse(
-				'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
-				'<p>Broccoli</p>' +
-				'<!-- wp:core/unknown-block -->Ribs<!-- /wp:core/unknown-block -->'
-			);
+				setUnknownTypeHandlerName( 'core/unknown-block' );
 
-			expect( parsed ).toHaveLength( 3 );
-			expect( parsed.map( ( { name } ) => name ) ).toEqual( [
-				'core/test-block',
-				'core/unknown-block',
-				'core/unknown-block',
-			] );
-		} );
+				const parsed = parse(
+					'<p>Cauliflower</p>' +
+					'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
+					'\n<p>Broccoli</p>\n' +
+					'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
+					'<p>Romanesco</p>'
+				);
 
-		it( 'should parse the post content, including raw HTML at each end', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
-			registerBlockType( 'core/unknown-block', unknownBlockSettings );
+				expect( parsed ).toHaveLength( 5 );
+				expect( parsed.map( ( { name } ) => name ) ).toEqual( [
+					'core/unknown-block',
+					'core/test-block',
+					'core/unknown-block',
+					'core/test-block',
+					'core/unknown-block',
+				] );
+				expect( parsed[ 0 ].attributes.content ).toEqual( '<p>Cauliflower</p>' );
+				expect( parsed[ 2 ].attributes.content ).toEqual( '<p>Broccoli</p>' );
+				expect( parsed[ 4 ].attributes.content ).toEqual( '<p>Romanesco</p>' );
+			} );
 
-			setUnknownTypeHandlerName( 'core/unknown-block' );
+			it( 'should parse blocks with empty content', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
+				const parsed = parse(
+					'<!-- wp:core/test-block --><!-- /wp:core/test-block -->'
+				);
 
-			const parsed = parse(
-				'<p>Cauliflower</p>' +
-				'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
-				'\n<p>Broccoli</p>\n' +
-				'<!-- wp:test-block {"fruit":"Bananas"} -->\nBananas\n<!-- /wp:test-block -->' +
-				'<p>Romanesco</p>'
-			);
+				expect( parsed ).toHaveLength( 1 );
+				expect( parsed.map( ( { name } ) => name ) ).toEqual( [
+					'core/test-block',
+				] );
+			} );
 
-			expect( parsed ).toHaveLength( 5 );
-			expect( parsed.map( ( { name } ) => name ) ).toEqual( [
-				'core/unknown-block',
-				'core/test-block',
-				'core/unknown-block',
-				'core/test-block',
-				'core/unknown-block',
-			] );
-			expect( parsed[ 0 ].attributes.content ).toEqual( '<p>Cauliflower</p>' );
-			expect( parsed[ 2 ].attributes.content ).toEqual( '<p>Broccoli</p>' );
-			expect( parsed[ 4 ].attributes.content ).toEqual( '<p>Romanesco</p>' );
-		} );
+			it( 'should parse void blocks', () => {
+				registerBlockType( 'core/test-block', defaultBlockSettings );
+				registerBlockType( 'core/void-block', defaultBlockSettings );
+				const parsed = parse(
+					'<!-- wp:core/test-block --><!-- /wp:core/test-block -->' +
+					'<!-- wp:core/void-block /-->'
+				);
 
-		it( 'should parse blocks with empty content', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
-			const parsed = parse(
-				'<!-- wp:core/test-block --><!-- /wp:core/test-block -->'
-			);
+				expect( parsed ).toHaveLength( 2 );
+				expect( parsed.map( ( { name } ) => name ) ).toEqual( [
+					'core/test-block', 'core/void-block',
+				] );
+			} );
 
-			expect( parsed ).toHaveLength( 1 );
-			expect( parsed.map( ( { name } ) => name ) ).toEqual( [
-				'core/test-block',
-			] );
-		} );
-
-		it( 'should parse void blocks', () => {
-			registerBlockType( 'core/test-block', defaultBlockSettings );
-			registerBlockType( 'core/void-block', defaultBlockSettings );
-			const parsed = parse(
-				'<!-- wp:core/test-block --><!-- /wp:core/test-block -->' +
-				'<!-- wp:core/void-block /-->'
-			);
-
-			expect( parsed ).toHaveLength( 2 );
-			expect( parsed.map( ( { name } ) => name ) ).toEqual( [
-				'core/test-block', 'core/void-block',
-			] );
-		} );
-
-		it( 'should parse with unicode escaped returned to original representation', () => {
-			registerBlockType( 'core/code', {
-				category: 'common',
-				title: 'Code Block',
-				attributes: {
-					content: {
-						type: 'string',
+			it( 'should parse with unicode escaped returned to original representation', () => {
+				registerBlockType( 'core/code', {
+					category: 'common',
+					title: 'Code Block',
+					attributes: {
+						content: {
+							type: 'string',
+						},
 					},
-				},
-				save: ( { attributes } ) => attributes.content,
-			} );
+					save: ( { attributes } ) => attributes.content,
+				} );
 
-			const content = '$foo = "My \"escaped\" text.";';
-			const block = createBlock( 'core/code', { content } );
-			const serialized = serialize( block );
-			const parsed = parse( serialized );
-			expect( parsed[ 0 ].attributes.content ).toBe( content );
-		} );
+				const content = '$foo = "My \"escaped\" text.";';
+				const block = createBlock( 'core/code', { content } );
+				const serialized = serialize( block );
+				const parsed = parse( serialized );
+				expect( parsed[ 0 ].attributes.content ).toBe( content );
+			} );
+		}
 	} );
 } );
