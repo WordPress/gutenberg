3434b68d9cbe16f1849e839425e428a6e01960b4
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-07-05 16:36:00 -0300

    Adds GutenbergTextView, makes it resize according to the content.

--- ios/RNTAztecView/GutenbergTextView.swift
+++ ios/RNTAztecView/GutenbergTextView.swift
@@ -4,6 +4,8 @@ import Foundation
 class GutenbergTextView: Aztec.TextView {
     @objc var onContentSizeChange: RCTBubblingEventBlock? = nil
     
+    // MARK - View Height: Match to content height
+    
     override var contentSize: CGSize {
         get {
             return super.contentSize
@@ -20,8 +22,12 @@ class GutenbergTextView: Aztec.TextView {
         
         updateHeightToMatch(newSize.height)
         
+        guard let onContentSizeChange = onContentSizeChange else {
+            return
+        }
+        
         let body = packForRN(newSize, withName: "contentSize")
-        onContentSizeChange?(body)
+        onContentSizeChange(body)
     }
     
     func updateHeightToMatch(_ newHeight: CGFloat) {
@@ -30,12 +36,24 @@ class GutenbergTextView: Aztec.TextView {
                        size: CGSize(width: frame.width, height: newHeight))
     }
     
+    // MARK: - Native-to-RN Value Packing Logic
+    
     func packForRN(_ size: CGSize, withName name: String) -> [AnyHashable: Any] {
         
         let size = ["width": size.width,
                     "height": size.height]
         
         return [name: size]
+    }
+    
+    // MARK: - Set & get Contents
+    
+    @objc func setContents(_ contents: NSString) {
+        super.setHTML(contents as String)
+    }
+    
+    @objc func getContents() -> NSString {
+        return super.getHTML() as NSString
     }
 }
 
