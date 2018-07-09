c51139ae80897635e16c03472a3415ba7c1b8b23
Darren Ethier <darren@roughsmootheng.in>
2018-07-09 10:51:28 -0400

    Switch away from using enzyme.mount in tests
    This switches all tests in `viewport/test/with-viewport-match.js` from using enzyme.mount to `React.TestUtils`.  This is because `enzyme` does not fully support React 16.3+ (and movement to do so is really slow). This will fix issues with breakage due to the enzyme incompatibility as components receive React 16.3+ features (such as `forwardRef` usage in #7557).

--- viewport/test/with-viewport-match.js
+++ viewport/test/with-viewport-match.js
@@ -1,12 +1,14 @@
 /**
  * External dependencies
  */
-import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { noop } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { dispatch } from '@wordpress/data';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -15,14 +17,28 @@ import '../store';
 import withViewportMatch from '../with-viewport-match';
 
 describe( 'withViewportMatch()', () => {
-	const Component = () => <div>Hello</div>;
+	const ChildComponent = () => <div>Hello</div>;
+
+	// this is needed because TestUtils does not accept a stateless component.
+	// anything run through a HOC ends up as a stateless component.
+	const getTestComponent = ( WrappedComponent ) => {
+		class TestComponent extends Component {
+			render() {
+				return <WrappedComponent { ...this.props } />;
+			}
+		}
+		return <TestComponent />;
+	};
 
 	it( 'should render with result of query as custom prop name', () => {
 		dispatch( 'core/viewport' ).setIsMatching( { '> wide': true } );
-		const EnhancedComponent = withViewportMatch( { isWide: '> wide' } )( Component );
-		const wrapper = mount( <EnhancedComponent /> );
+		const EnhancedComponent = withViewportMatch(
+			{ isWide: '> wide' }
+		)( ChildComponent );
+		const wrapper = renderer.create( getTestComponent( EnhancedComponent ) );
 
-		expect( wrapper.find( Component ).props() ).toEqual( { isWide: true } );
+		expect( wrapper.root.findByType( ChildComponent ).props.isWide )
+			.toBe( true );
 
 		wrapper.unmount();
 	} );
