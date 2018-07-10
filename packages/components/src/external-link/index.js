d49339efc77af72d9eb3a6452411d87e1b096f23
Daniel Richards <daniel.richards@automattic.com>
2018-07-10 19:13:50 +0800

    Add prop for icon class name to ExternalLink

--- packages/components/src/external-link/index.js
+++ packages/components/src/external-link/index.js
@@ -14,7 +14,7 @@ import { __ } from '@wordpress/i18n';
  */
 import Dashicon from '../dashicon';
 
-function ExternalLink( { href, children, className, rel = '', ...additionalProps } ) {
+function ExternalLink( { href, children, className, rel = '', iconClassName, ...additionalProps } ) {
 	rel = uniq( compact( [
 		...rel.split( ' ' ),
 		'external',
@@ -31,7 +31,7 @@ function ExternalLink( { href, children, className, rel = '', ...additionalProps
 					__( '(opens in a new window)' )
 				}
 			</span>
-			<Dashicon icon="external" />
+			<Dashicon className={ iconClassName } icon="external" />
 		</a>
 	);
 }
