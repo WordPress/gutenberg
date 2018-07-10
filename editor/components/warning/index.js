745c01365ba6b52f862acb63c93e2cbc5f4cfec9
John Godley <johng75@gmail.com>
2018-07-10 12:06:50 +0100

    Block warning: restyle and move buttons inline with message
    The overall style is adjusted so it’s left aligned, and the buttons and ellipsis are right aligned (flowing to the line below if not enough space)

--- editor/components/warning/index.js
+++ editor/components/warning/index.js
@@ -11,16 +11,19 @@ import './style.scss';
 function Warning( { actions, children } ) {
 	return (
 		<div className="editor-warning">
-			{ children }
-			{ Children.count( actions ) > 0 && (
-				<div className="editor-warning__actions">
-					{ Children.map( actions, ( action, i ) => (
-						<span key={ i } className="editor-warning__action">
-							{ action }
-						</span>
-					) ) }
-				</div>
-			) }
+			<div className="editor-warning__contents">
+				<div className="editor-warning__message">{ children }</div>
+
+				{ Children.count( actions ) > 0 && (
+					<div className="editor-warning__actions">
+						{ Children.map( actions, ( action, i ) => (
+							<span key={ i } className="editor-warning__action">
+								{ action }
+							</span>
+						) ) }
+					</div>
+				) }
+			</div>
 		</div>
 	);
 }
