5c93af9fc506f1074f9c1cd903f6c323d23ce042
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-07-10 11:00:57 +0300

    Prevent Metro from picking up some packages from local
    Blacklisting the specific packages folders we want Metro to resolve
     from NPM, instead of automagically resolving them to local.

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,4 +1,5 @@
 /** @format */
+const blacklist = require( 'metro' ).createBlacklist;
 
 const enm = require( 'node-libs-react-native' );
 enm.fs = __dirname + '/__mocks__/nodejsMock.js';
@@ -7,6 +8,10 @@ enm.canvas = __dirname + '/__mocks__/nodejsMock.js';
 
 module.exports = {
 	extraNodeModules: enm,
+	getBlacklistRE: function() {
+		// blacklist the GB packages we want to consume from NPM (online) directly
+		return blacklist( [ /gutenberg\/packages\/(autop|hooks|i18n|is-shallow-equal)\/.*/ ] );
+	},
 	getTransformModulePath() {
 		return require.resolve( './sass-transformer.js' );
 	},
