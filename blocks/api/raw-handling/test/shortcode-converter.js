47bc2755b4df6b376ded685ea100f3b65db3a0e7
Daniel Bachhuber <daniel@bachhuber.co>
2018-07-11 10:15:55 -0700

    Properly convert successive shortcodes (#7846)
    * Add a failing test case for conversion of successive shortcodes
    
    * Add a second test case with two shortcode instances
    
    * Update test to inspect correct block
    
    * Remove `memize()` to avoid unexpected caching bug
    
    * Format as a proper function
    
    * Semi-colon is unnecessary

--- blocks/api/raw-handling/test/shortcode-converter.js
+++ blocks/api/raw-handling/test/shortcode-converter.js
@@ -35,4 +35,70 @@ describe( 'segmentHTMLToShortcodeBlock', () => {
 
 <p>Bar</p>` );
 	} );
+
+	it( 'should convert two instances of the same shortcode', () => {
+		const original = `<p>[foo one]</p>
+<p>[foo two]</p>`;
+
+		const transformed = segmentHTMLToShortcodeBlock( original, 0 );
+		expect( transformed[ 0 ] ).toEqual( '<p>' );
+		const firstExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo one]',
+		} );
+		// uid will always be random.
+		firstExpectedBlock.uid = transformed[ 1 ].uid;
+		expect( transformed[ 1 ] ).toEqual( firstExpectedBlock );
+		expect( transformed[ 2 ] ).toEqual( `</p>
+<p>` );
+		const secondExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo two]',
+		} );
+		// uid will always be random.
+		secondExpectedBlock.uid = transformed[ 3 ].uid;
+		expect( transformed[ 3 ] ).toEqual( secondExpectedBlock );
+		expect( transformed[ 4 ] ).toEqual( '</p>' );
+		expect( transformed ).toHaveLength( 5 );
+	} );
+
+	it( 'should convert four instances of the same shortcode', () => {
+		const original = `<p>[foo one]</p>
+<p>[foo two]</p>
+<p>[foo three]</p>
+<p>[foo four]</p>`;
+
+		const transformed = segmentHTMLToShortcodeBlock( original, 0 );
+		expect( transformed[ 0 ] ).toEqual( '<p>' );
+		const firstExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo one]',
+		} );
+		// uid will always be random.
+		firstExpectedBlock.uid = transformed[ 1 ].uid;
+		expect( transformed[ 1 ] ).toEqual( firstExpectedBlock );
+		expect( transformed[ 2 ] ).toEqual( `</p>
+<p>` );
+		const secondExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo two]',
+		} );
+		// uid will always be random.
+		secondExpectedBlock.uid = transformed[ 3 ].uid;
+		expect( transformed[ 3 ] ).toEqual( secondExpectedBlock );
+		expect( transformed[ 4 ] ).toEqual( `</p>
+<p>` );
+		const thirdExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo three]',
+		} );
+		// uid will always be random.
+		thirdExpectedBlock.uid = transformed[ 5 ].uid;
+		expect( transformed[ 5 ] ).toEqual( thirdExpectedBlock );
+		expect( transformed[ 6 ] ).toEqual( `</p>
+<p>` );
+		const fourthExpectedBlock = createBlock( 'core/shortcode', {
+			text: '[foo four]',
+		} );
+		// uid will always be random.
+		fourthExpectedBlock.uid = transformed[ 7 ].uid;
+		expect( transformed[ 7 ] ).toEqual( fourthExpectedBlock );
+		expect( transformed[ 8 ] ).toEqual( '</p>' );
+		expect( transformed ).toHaveLength( 9 );
+	} );
 } );
