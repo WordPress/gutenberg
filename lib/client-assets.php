1e9842f5478242c5099d1eed0a1c928f3dfd92f7
Daniel Bachhuber <daniel@bachhuber.co>
2018-07-11 16:53:41 -0700

    Backup and restore global `$post` when preloading API data (#7889)
    * Backup and restore global `$post` when preloading API data
    
    Because shortcode callbacks run on the API preload, they can
    unexpectedly stomp on the global `$post`. Historically, shortcodes have
    only ever rendered on the frontend.
    
    * chore: Tweak comments a bit

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -1068,11 +1068,21 @@ function gutenberg_editor_scripts_and_styles( $hook ) {
 		sprintf( '/wp/v2/users/me?post_type=%s&context=edit', $post_type ),
 	);
 
+	// Ensure the global $post remains the same after
+	// API data is preloaded. Because API preloading
+	// can call the_content and other filters, callbacks
+	// can unexpectedly modify $post resulting in issues
+	// like https://github.com/WordPress/gutenberg/issues/7468.
+	$backup_global_post = $post;
+
 	$preload_data = array_reduce(
 		$preload_paths,
 		'gutenberg_preload_api_request',
 		array()
 	);
+
+	// Restore the global $post as it was before API preloading.
+	$post = $backup_global_post;
 
 	wp_add_inline_script(
 		'wp-api-request',
