5fb24e44ceac6ac19b0d2bdffea87b467520f405
Riad Benguella <benguella@gmail.com>
2018-07-12 16:07:41 +0100

    Data Module: Introduce the "registry" concept (#7527)
    * Data: Move registry into own file
    
    * Export the "createRegistry" function
    
    * Adding remountOnPropChange HoC

---
+++ packages/data/src/components/remountOnPropChange/index.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+import { createHigherOrderComponent, Component } from '@wordpress/element';
+
+/**
+ * Higher-order component creator, creating a new component that remounts
+ * the wrapped component each time a given prop value changes.
+ *
+ * @param {string} propName Prop name to monitor.
+ *
+ * @return {Function} Higher-order component.
+ */
+const remountOnPropChange = ( propName ) => createHigherOrderComponent(
+	( WrappedComponent ) => class extends Component {
+		constructor( props ) {
+			super( ...arguments );
+			this.state = {
+				propChangeId: 0,
+				propValue: props[ propName ],
+			};
+		}
+
+		static getDerivedStateFromProps( props, state ) {
+			if ( props[ propName ] === state.propValue ) {
+				return null;
+			}
+
+			return {
+				propChangeId: state.propChangeId + 1,
+				propValue: props[ propName ],
+			};
+		}
+
+		render() {
+			return <WrappedComponent key={ this.state.propChangeId } { ...this.props } />;
+		}
+	},
+	'remountOnPropChange'
+);
+
+export default remountOnPropChange;
