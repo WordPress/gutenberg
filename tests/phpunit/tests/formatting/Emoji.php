f364b2fa5c29e37543d68ec6168be7c99f5f2b2c
Gary Pendergast <pento@git.wordpress.org>
2018-07-13 06:36:23 +0000

    Emoji: Update Twemoji to version 11.0.
    🦹
    
    Backport of [43377] to the 4.9 branch.
    
    Props kraftbj,
    Fixes #44339.
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/4.9@43444 602fd350-edb4-49c9-b593-d223f7449a82

--- tests/phpunit/tests/formatting/Emoji.php
+++ tests/phpunit/tests/formatting/Emoji.php
@@ -6,8 +6,8 @@
  */
 class Tests_Formatting_Emoji extends WP_UnitTestCase {
 
-	private $png_cdn = 'https://s.w.org/images/core/emoji/2.4/72x72/';
-	private $svn_cdn = 'https://s.w.org/images/core/emoji/2.4/svg/';
+	private $png_cdn = 'https://s.w.org/images/core/emoji/11/72x72/';
+	private $svn_cdn = 'https://s.w.org/images/core/emoji/11/svg/';
 
 	/**
 	 * @ticket 36525
