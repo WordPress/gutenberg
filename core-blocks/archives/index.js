07da41ea49bd64d4526b845c38060ecde908e734
Daniel Richards <daniel.richards@automattic.com>
2018-07-13 18:06:48 +0800

    Changes from PR #5495 squashed into single commit
    Co-authored-by: Pascal Birchler <hello@pascalbirchler.ch>
    Co-authored-by: Miina Sikk <miina.sikk@gmail.com>
    Co-authored-by: Weston Ruter <weston@xwp.co>

---
+++ core-blocks/archives/index.js
@@ -0,0 +1,40 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import ArchivesBlock from './block';
+export const name = 'core/archives';
+
+export const settings = {
+	title: __( 'Archives' ),
+
+	description: __( 'This block displays a monthly archive of your site’s Posts.' ),
+
+	icon: 'calendar-alt',
+
+	category: 'widgets',
+
+	keywords: [ __( 'archives' ) ],
+
+	supports: {
+		html: false,
+	},
+
+	getEditWrapperProps( attributes ) {
+		const { align } = attributes;
+		if ( 'left' === align || 'right' === align || 'center' === align ) {
+			return { 'data-align': align };
+		}
+	},
+
+	edit: ArchivesBlock,
+
+	save() {
+		// Handled by PHP.
+		return null;
+	},
+};
