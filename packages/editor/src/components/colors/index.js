bc9d9a16a53f44d8d5ddf40f60cf91d95da778cd
Jorge <jorge.costa@developer.pt>
2018-07-16 17:43:12 +0100

    chore: Update: Some enhancements to the color mechanism to improve code in accordance with the latest changes.

--- packages/editor/src/components/colors/index.js
+++ packages/editor/src/components/colors/index.js
@@ -1,2 +1,8 @@
-export { getColorClass, getColorName } from './utils';
+export {
+	getColorClass,
+	getColorClassName,
+	getColorName,
+	getColorObjectByAttributeValues,
+	getColorObjectByColorValue,
+} from './utils';
 export { default as withColors } from './with-colors';
--- packages/editor/src/components/colors/utils.js
+++ packages/editor/src/components/colors/utils.js
@@ -4,24 +4,46 @@
 import { find, kebabCase } from 'lodash';
 
 /**
- * Returns the color value based on an array of named colors and the definedColor or the customColor value.
- *
- * @param {Array}   colors        Array of color objects containing the "name", "slug" and "color" value as properties.
- * @param {?string} definedColor  A string containing the color slug.
- * @param {?string} customColor   A string containing the customColor value.
- *
- * @return {?string} If definedColor is passed and the name is found in colors it returns the color for that name.
- * 					 Otherwise, the customColor parameter is returned.
+ * WordPress dependencies
  */
-export const getColorValue = ( colors, definedColor, customColor ) => {
+import deprecated from '@wordpress/deprecated';
+
+/**
+ * Provided an array of color objects as set by the theme or by the editor defaults,
+ * and the values of the defined color or custom color returns a color object describing the color.
+ *
+ * @param {Array}   colors       Array of color objects as set by the theme or by the editor defaults.
+ * @param {?string} definedColor A string containing the color slug.
+ * @param {?string} customColor  A string containing the customColor value.
+ *
+ * @return {?string} If definedColor is passed and the name is found in colors,
+ *                   the color object exactly as set by the theme or editor defaults is returned.
+ *                   Otherwise, an object that just sets the color is defined.
+ */
+export const getColorObjectByAttributeValues = ( colors, definedColor, customColor ) => {
 	if ( definedColor ) {
 		const colorObj = find( colors, { slug: definedColor } );
 
-		return colorObj && colorObj.color;
+		if ( colorObj ) {
+			return colorObj;
+		}
 	}
-	if ( customColor ) {
-		return customColor;
-	}
+	return {
+		color: customColor,
+	};
+};
+
+/**
+* Provided an array of color objects as set by the theme or by the editor defaults, and a color value returns the color object matching that value or undefined.
+*
+* @param {Array}   colors      Array of color objects as set by the theme or by the editor defaults.
+* @param {?string} colorValue  A string containing the color value.
+*
+* @return {?string} Returns the color object included in the colors array whose color property equals colorValue.
+*                   Returns undefined if no color object matches this requirement.
+*/
+export const getColorObjectByColorValue = ( colors, colorValue ) => {
+	return find( colors, { color: colorValue } );
 };
 
 /**
@@ -33,41 +55,36 @@ export const getColorValue = ( colors, definedColor, customColor ) => {
 * @return {?string} If colorValue is defined and matches a color part of the colors array, it returns the color name for that color.
 */
 export const getColorName = ( colors, colorValue ) => {
-	const colorObj = find( colors, { color: colorValue } );
+	deprecated( 'getColorName function', {
+		version: '3.9',
+		alternative: '`getColorObjectByColorValue` function',
+		plugin: 'Gutenberg',
+	} );
+	const colorObj = getColorObjectByColorValue( colors, colorValue );
 	return colorObj ? colorObj.name : undefined;
 };
 
 /**
- * Returns a function that receives the color value and sets it using the attribute for named colors or for custom colors.
- *
- * @param {Array}  colors                   Array of color objects containing the "name", "slug" and "color" value as properties.
- * @param {string} colorAttributeName       Name of the attribute where named colors are stored.
- * @param {string} customColorAttributeName Name of the attribute where custom colors are stored.
- * @param {string} setAttributes            A function that receives an object with the attributes to set.
- *
- * @return {function} A function that receives the color value and sets the attributes necessary to correctly store it.
- */
-export const setColorValue = ( colors, colorAttributeName, customColorAttributeName, setAttributes ) =>
-	( colorValue ) => {
-		const colorObj = find( colors, { color: colorValue } );
-		setAttributes( {
-			[ colorAttributeName ]: colorObj && colorObj.slug ? colorObj.slug : undefined,
-			[ customColorAttributeName ]: colorObj && colorObj.slug ? undefined : colorValue,
-		} );
-	};
-
-/**
- * Returns a class based on the context a color is being used and its name.
+ * Returns a class based on the context a color is being used and its slug.
  *
  * @param {string} colorContextName Context/place where color is being used e.g: background, text etc...
- * @param {string} colorName        Name of the color.
+ * @param {string} colorSlug        Slug of the color.
  *
  * @return {string} String with the class corresponding to the color in the provided context.
  */
-export function getColorClass( colorContextName, colorName ) {
-	if ( ! colorContextName || ! colorName ) {
+export function getColorClassName( colorContextName, colorSlug ) {
+	if ( ! colorContextName || ! colorSlug ) {
 		return;
 	}
 
-	return `has-${ kebabCase( colorName ) }-${ colorContextName }`;
+	return `has-${ kebabCase( colorSlug ) }-${ colorContextName }`;
+}
+
+export function getColorClass( colorContextName, colorSlug ) {
+	deprecated( 'getColorClass function', {
+		version: '3.9',
+		alternative: '`getColorClassName` function',
+		plugin: 'Gutenberg',
+	} );
+	return getColorClassName( colorContextName, colorSlug );
 }
