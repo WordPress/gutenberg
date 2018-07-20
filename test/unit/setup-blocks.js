3261f08460b48b6650ba9cea7467e089b0f61bb0
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-07-20 07:54:04 +0200

    Testing: Fix broken setup caused by blocks package

--- test/unit/setup-blocks.js
+++
@@ -1,7 +0,0 @@
-// Bootstrap server-registered blocks
-// eslint-disable-next-line camelcase
-import { unstable__bootstrapServerSideBlockDefinitions } from '@wordpress/blocks';
-
-unstable__bootstrapServerSideBlockDefinitions(
-	require( 'core-blocks/test/server-registered.json' )
-);
