058648f6b8b165abfa6fcb857c8d34581d6b9485
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-07-23 10:45:13 -0300

    Adds code to wire text changes back to the RN side.

--- ios/RNTAztecView/RCTAztecView.swift
+++ ios/RNTAztecView/RCTAztecView.swift
@@ -2,6 +2,7 @@ import Aztec
 import Foundation
 
 class RCTAztecView: Aztec.TextView {
+    @objc var onChange: RCTBubblingEventBlock? = nil
     @objc var onContentSizeChange: RCTBubblingEventBlock? = nil
     
     private var previousContentSize: CGSize = .zero
@@ -28,7 +29,31 @@ class RCTAztecView: Aztec.TextView {
         onContentSizeChange(body)
     }
     
+    // MARK: - Edits
+    
+    open override func insertText(_ text: String) {
+        super.insertText(text)
+        
+        if let onChange = onChange {
+            let text = packForRN(getHTML(), withName: "text")
+            onChange(text)
+        }
+    }
+    
+    open override func deleteBackward() {
+        super.deleteBackward()
+        
+        if let onChange = onChange {
+            let text = packForRN(getHTML(), withName: "text")
+            onChange(text)
+        }
+    }
+    
     // MARK: - Native-to-RN Value Packing Logic
+    
+    func packForRN(_ text: String, withName name: String) -> [AnyHashable: Any] {
+        return [name: text]
+    }
     
     func packForRN(_ size: CGSize, withName name: String) -> [AnyHashable: Any] {
         
