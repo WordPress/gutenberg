5666d261220345466083b8629fafd637f5fd89ee
Daniel Bachhuber <daniel@bachhuber.co>
2018-07-26 07:01:35 -0700

    Add e2e test for TinyMCE wpautop behavior

--- test/e2e/specs/classic-editor.test.js
+++ test/e2e/specs/classic-editor.test.js
@@ -24,4 +24,36 @@ describe( 'classic editor', () => {
 		const textEditorContent = await page.$eval( '.wp-editor-area', ( element ) => element.value );
 		expect( textEditorContent ).toEqual( 'Typing in classic editor' );
 	} );
+
+	it( 'Should have wpautop disabled on a post containing blocks', async () => {
+		// Click the Text mode.
+		await page.click( '#content-html' );
+
+		// Enter some block text.
+		const original = `<!-- wp:paragraph -->
+<p>Foo bar four five six</p>
+<!-- /wp:paragraph -->
+
+This is another set of text`;
+		await page.keyboard.type( original );
+
+		// Save the post so that TinyMCE loads with wpautop disabled.
+		await page.click( '#save-post' );
+
+		// Switch to Visual mode.
+		await page.click( '#content-tmce' );
+		await page.click( '#content_ifr' );
+
+		// Switch back to Text mode.
+		await page.click( '#content-html' );
+
+		// Expected text is wrapped in `<p>` because `removep()` isn't run
+		// when switching out of Visual mode.
+		const expected = `<!-- wp:paragraph -->
+<p>Foo bar four five six</p>
+<!-- /wp:paragraph -->
+<p>This is another set of text</p>`;
+		const textEditorContent = await page.$eval( '.wp-editor-area', ( element ) => element.value );
+		expect( textEditorContent ).toEqual( expected );
+	} );
 } );
