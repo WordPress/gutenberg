81c61246fda6535acd5da197638b2d4162cafe23
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-07-26 11:47:28 +0200

    Tests: Improve copy in apiFetch mock

--- __mocks__/@wordpress/api-fetch/index.js
+++ __mocks__/@wordpress/api-fetch/index.js
@@ -1,6 +1,6 @@
 const apiFetch = jest.fn( () => {
 	return apiFetch.mockReturnValue;
 } );
-apiFetch.mockReturnValue = 'mock this value by overriding apiFetch.mockReturnValue';
+apiFetch.mockReturnValue = 'Mock this value by overriding apiFetch.mockReturnValue.';
 
 export default apiFetch;
