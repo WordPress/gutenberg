59cd86cb62ce83e2b65e52e8db2eec61b827cc30
Daniel Bachhuber <daniel@bachhuber.co>
2018-07-26 10:02:55 -0700

    Remove e2e tests, which are behaving oddly and will break with modal UX

--- test/e2e/specs/classic-editor.test.js
+++ test/e2e/specs/classic-editor.test.js
@@ -24,42 +24,4 @@ describe( 'classic editor', () => {
 		const textEditorContent = await page.$eval( '.wp-editor-area', ( element ) => element.value );
 		expect( textEditorContent ).toEqual( 'Typing in classic editor' );
 	} );
-
-	it( 'Should have wpautop disabled on a post containing blocks', async () => {
-		// Click the Text mode.
-		await page.click( '#content-html' );
-
-		// Enter some block text.
-		const original = `<!-- wp:paragraph -->
-<p>Foo bar four five six</p>
-<!-- /wp:paragraph -->
-
-This is another set of text`;
-		await page.type( '#content', original );
-
-		const initialTextEditorContent = await page.$eval( '.wp-editor-area', ( element ) => element.value );
-		expect( initialTextEditorContent ).toEqual( original );
-
-		// Save the post so that TinyMCE loads with wpautop disabled.
-		await Promise.all( [
-			page.waitForNavigation(),
-			page.click( '#save-post' ),
-		] );
-
-		// Switch to Visual mode.
-		await page.click( '#content-tmce' );
-		await page.click( '#content_ifr' );
-
-		// Switch back to Text mode.
-		await page.click( '#content-html' );
-
-		// Expected text is wrapped in `<p>` because `removep()` isn't run
-		// when switching out of Visual mode.
-		const expected = `<!-- wp:paragraph -->
-<p>Foo bar four five six</p>
-<!-- /wp:paragraph -->
-<p>This is another set of text</p>`;
-		const textEditorContent = await page.$eval( '.wp-editor-area', ( element ) => element.value );
-		expect( textEditorContent ).toEqual( expected );
-	} );
 } );
