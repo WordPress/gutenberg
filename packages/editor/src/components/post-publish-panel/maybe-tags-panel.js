1becfb8ed362f9f35f522b7d493f2fbf4ce185f2
Andrés <andres.maneiro@automattic.com>
2018-08-01 17:44:39 +0200

    Tags panel: control visibility
    Only show it when the tags taxonomy supports the current postType.

--- packages/editor/src/components/post-publish-panel/maybe-tags-panel.js
+++ packages/editor/src/components/post-publish-panel/maybe-tags-panel.js
@@ -2,11 +2,13 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { compose } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
 import { Dashicon, PanelBody } from '@wordpress/components';
 
 import FlatTermSelector from '../post-taxonomies/flat-term-selector';
 
-const MaybeTagsPanel = () => <PanelBody initialOpen={ true } title={ [
+const TagsPanel = () => <PanelBody initialOpen={ true } title={ [
 	<Dashicon key={ 'dashicon-lightbulb' } icon={ 'lightbulb' } />,
 	__( 'Tip:' ),
 	<span className="editor-post-publish-panel__link" key="label">{
@@ -16,4 +18,19 @@ const MaybeTagsPanel = () => <PanelBody initialOpen={ true } title={ [
 	<FlatTermSelector slug={ 'post_tag' } />
 </PanelBody>;
 
-export default MaybeTagsPanel;
+const MaybeTagsPanel = ( { hasTags } ) => {
+	if ( ! hasTags ) {
+		return null;
+	}
+	return ( <TagsPanel /> );
+};
+
+export default compose( [
+	withSelect( ( select ) => {
+		const postType = select( 'core/editor' ).getCurrentPostType();
+		const tags = select( 'core' ).getTaxonomy( 'post_tag' );
+		return {
+			hasTags: tags && tags.types.some( ( type ) => type === postType ),
+		};
+	} ),
+] )( MaybeTagsPanel );
