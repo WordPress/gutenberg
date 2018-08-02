82353c668c731c45c285ac17d42df701ce7e520b
William Earnhardt <wearnhardt@gmail.com>
2018-08-02 11:01:07 -0400

    Check date_gmt to determine post-schedule label

--- packages/editor/src/components/post-schedule/label.js
+++ packages/editor/src/components/post-schedule/label.js
@@ -5,15 +5,16 @@ import { __ } from '@wordpress/i18n';
 import { dateI18n, getSettings } from '@wordpress/date';
 import { withSelect } from '@wordpress/data';
 
-function PostScheduleLabel( { date } ) {
+function PostScheduleLabel( { date, date_gmt } ) {
 	const settings = getSettings();
-	return date ?
-		dateI18n( settings.formats.datetime, date ) :
-		__( 'Immediately' );
+	return ( null === date_gmt ) ?
+		__( 'Immediately' ) :
+		dateI18n( settings.formats.datetime, date );
 }
 
 export default withSelect( ( select ) => {
 	return {
 		date: select( 'core/editor' ).getEditedPostAttribute( 'date' ),
+		date_gmt: select( 'core/editor' ).getEditedPostAttribute( 'date_gmt' ),
 	};
 } )( PostScheduleLabel );
