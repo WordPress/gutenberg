4c28bd0d2740a5293fc449e9dd8b3ac9906ddba5
Andrew Duthie <andrew@andrewduthie.com>
2018-08-06 16:30:26 -0400

    TinyMCE: Remove container assignment hack (#8465)

---
+++ test/e2e/specs/rich-text.test.js
@@ -0,0 +1,29 @@
+/**
+ * Internal dependencies
+ */
+import '../support/bootstrap';
+import {
+	newPost,
+	newDesktopBrowserPage,
+	getEditedPostContent,
+	insertBlock,
+} from '../support/utils';
+
+describe( 'adding blocks', () => {
+	beforeEach( async () => {
+		await newDesktopBrowserPage();
+		await newPost();
+	} );
+
+	it( 'Should handle change in tag name gracefully', async () => {
+		// Regression test: The heading block changes the tag name of its
+		// RichText element. Historically this has been prone to breakage,
+		// specifically in destroying / reinitializing the TinyMCE instance.
+		//
+		// See: https://github.com/WordPress/gutenberg/issues/3091
+		await insertBlock( 'Heading' );
+		await page.click( '[aria-label="Heading 3"]' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+} );
