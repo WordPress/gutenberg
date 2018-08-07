42b870c6be84f9b2fefcc5bf4813598a23a16de1
Riad Benguella <benguella@gmail.com>
2018-08-07 13:23:08 +0100

    RichText: Removing/merging richtext should only trigger if the selection is collapsed (#8651)
    * RichText: Removing/merging richtext should only trigger if the selection is collapsed
    
    * Add e2e test to avoid uncollapsed selection to be mergedwq
    
    * Code Style improvements per review

--- test/e2e/specs/splitting-merging.test.js
+++ test/e2e/specs/splitting-merging.test.js
@@ -17,7 +17,7 @@ describe( 'splitting and merging blocks', () => {
 		await newPost();
 	} );
 
-	it( 'Should split and merge paragraph blocks using Enter and Backspace', async () => {
+	it( 'should split and merge paragraph blocks using Enter and Backspace', async () => {
 		// Use regular inserter to add paragraph block and text
 		await insertBlock( 'Paragraph' );
 		await page.keyboard.type( 'FirstSecond' );
@@ -55,7 +55,7 @@ describe( 'splitting and merging blocks', () => {
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 
-	it( 'Should merge into inline boundary position', async () => {
+	it( 'should merge into inline boundary position', async () => {
 		// Regression Test: Caret should reset to end of inline boundary when
 		// backspacing to delete second paragraph.
 		await insertBlock( 'Paragraph' );
@@ -71,7 +71,7 @@ describe( 'splitting and merging blocks', () => {
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 
-	it( 'Should delete an empty first line', async () => {
+	it( 'should delete an empty first line', async () => {
 		// Regression Test: When a paragraph block has line break, and the first
 		// line has no text, pressing backspace at the start of the second line
 		// should remove the first.
@@ -83,6 +83,20 @@ describe( 'splitting and merging blocks', () => {
 		await page.keyboard.up( 'Shift' );
 
 		// Delete the soft line break.
+		await page.keyboard.press( 'Backspace' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should not merge paragraphs if the selection is not collapsed', async () => {
+		await insertBlock( 'Paragraph' );
+		await page.keyboard.type( 'Foo' );
+		await insertBlock( 'Paragraph' );
+		await page.keyboard.type( 'Bar' );
+
+		await page.keyboard.down( 'Shift' );
+		await pressTimes( 'ArrowLeft', 3 );
+		await page.keyboard.up( 'Shift' );
 		await page.keyboard.press( 'Backspace' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
