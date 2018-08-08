c8120d0eb2303889274298ea0c48bd23aed8f1d1
Jorge Bernal <jbernal@gmail.com>
2018-08-08 18:09:38 +0200

    Adds React Native implementation of heading block

---
+++ core-blocks/heading/edit.native.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+import { View } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
+import { RichText } from '@wordpress/editor';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+
+const minHeight = 50;
+
+class HeadingEdit extends Component {
+	render() {
+		const {
+			attributes,
+			setAttributes,
+			style,
+			className,
+		} = this.props;
+
+		const {
+			align,
+			content,
+			level,
+			placeholder,
+		} = attributes;
+
+		const tagName = 'h' + level;
+
+		return (
+			<View>
+				<RichText
+					wrapperClassName="wp-block-heading"
+					tagName={ tagName }
+					content={ { contentTree: attributes.content, eventCount: attributes.eventCount } }
+					style={ {
+						minHeight: Math.max( minHeight, typeof attributes.aztecHeight === 'undefined' ? 0 : attributes.aztecHeight ),
+					} }
+					onChange={ ( value ) => { setAttributes( value ); } }
+					onContentSizeChange={ ( event ) => { setAttributes( { aztecHeight: event.aztecHeight } ); } }
+					className={ className }
+					placeholder={ placeholder || __( 'Write heading…' ) }
+				/>
+			</View>
+		);
+	}
+}
+export default HeadingEdit;
