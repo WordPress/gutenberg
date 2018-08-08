092bd6d23e1036713493347d9fa1b82b7d76d23d
Andrew Duthie <andrew@andrewduthie.com>
2018-08-08 11:13:43 -0400

    Testing: Extract post trashing to function

--- test/e2e/support/setup-test-framework.js
+++ test/e2e/support/setup-test-framework.js
@@ -27,29 +27,40 @@ async function setupBrowser() {
 	await setViewport( 'large' );
 }
 
+/**
+ * Navigates to the post listing screen and bulk-trashes any posts which exist.
+ *
+ * @return {Promise} Promise resolving once posts have been trashed.
+ */
+async function trashExistingPosts() {
+	// Visit `/wp-admin/edit.php` so we can see a list of posts and delete them.
+	await visitAdmin( 'edit.php' );
+
+	// If this selector doesn't exist there are no posts for us to delete.
+	const bulkSelector = await page.$( '#bulk-action-selector-top' );
+	if ( ! bulkSelector ) {
+		return;
+	}
+
+	// Select all posts.
+	await page.waitForSelector( '#cb-select-all-1' );
+	await page.click( '#cb-select-all-1' );
+	// Select the "bulk actions" > "trash" option.
+	await page.select( '#bulk-action-selector-top', 'trash' );
+	// Submit the form to send all draft/scheduled/published posts to the trash.
+	await page.click( '#doaction' );
+	return page.waitForXPath(
+		'//*[contains(@class, "updated notice")]/p[contains(text(), "moved to the Trash.")]'
+	);
+}
+
 // Before every test suite run, delete all content created by the test. This ensures
 // other posts/comments/etc. aren't dirtying tests and tests don't depend on
 // each other's side-effects.
 beforeAll( async () => {
 	enablePageDialogAccept();
 
-	// Visit `/wp-admin/edit.php` so we can see a list of posts and delete them.
-	await visitAdmin( 'edit.php' );
-
-	// If this selector doesn't exist there are no posts for us to delete.
-	const bulkSelector = await page.$( '#bulk-action-selector-top' );
-	if ( bulkSelector ) {
-		// Select all posts.
-		await page.waitForSelector( '#cb-select-all-1' );
-		await page.click( '#cb-select-all-1' );
-		// Select the "bulk actions" > "trash" option.
-		await page.select( '#bulk-action-selector-top', 'trash' );
-		// Submit the form to send all draft/scheduled/published posts to the trash.
-		await page.click( '#doaction' );
-		await page.waitForXPath(
-			'//*[contains(@class, "updated notice")]/p[contains(text(), "moved to the Trash.")]'
-		);
-	}
+	await trashExistingPosts();
 	await setupBrowser();
 } );
 
