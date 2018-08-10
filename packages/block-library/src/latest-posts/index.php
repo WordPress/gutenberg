3a92986674b8c9383f9d9fbd7c621e45f93889b7
nfmohit <nfmohit49@gmail.com>
2018-08-10 22:10:35 +0600

    Make sure Latest Posts block doesn't apply alignment classes if no alignment is selected

--- packages/block-library/src/latest-posts/index.php
+++ packages/block-library/src/latest-posts/index.php
@@ -47,7 +47,11 @@ function render_block_core_latest_posts( $attributes ) {
 		$list_items_markup .= "</li>\n";
 	}
 
-	$class = "wp-block-latest-posts align{$attributes['align']}";
+	$class = 'wp-block-latest-posts';
+	if ( isset( $attributes['align'] ) ) {
+		$class .= ' align' . $attributes['align'];
+	}
+
 	if ( isset( $attributes['postLayout'] ) && 'grid' === $attributes['postLayout'] ) {
 		$class .= ' is-grid';
 	}
@@ -99,7 +103,6 @@ function register_block_core_latest_posts() {
 			),
 			'align'           => array(
 				'type'    => 'string',
-				'default' => 'center',
 			),
 			'order'           => array(
 				'type'    => 'string',
