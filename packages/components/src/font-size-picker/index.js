695f7a768acd2186c563f60cc07bc6a495b9a3b0
Elio Rivero <eliorivero@gmail.com>
2018-08-11 21:09:42 -0300

    Text settings: add a tooltip to the font size selector buttons when the short identifier, for example S or M, is used as the button label.

--- packages/components/src/font-size-picker/index.js
+++ packages/components/src/font-size-picker/index.js
@@ -15,23 +15,45 @@ import { __ } from '@wordpress/i18n';
 import Button from '../button';
 import ButtonGroup from '../button-group';
 import RangeControl from '../range-control';
+import Tooltip from '../tooltip';
+
+/**
+ * If short and long font size names are available, render using the short name and add the tooltip using the long name.
+ * If only short or long names but not their counterparts are provided, render them using only what is provided.
+ *
+ * @param {string}   label    Font size name, can be long, Small, or short, S.
+ * @param {string}   value    Current font size.
+ * @param {string}   size     Font size that this button sets.
+ * @param {function} onChange Method that sets the font to the specified size.
+ *
+ * @return {Element} Element with name with tooltip.
+ */
+const fontSize = ( label, value, size, onChange ) => {
+	return (
+		<Button
+			key={ size }
+			isLarge
+			isPrimary={ value === size }
+			aria-pressed={ value === size }
+			onClick={ () => onChange( size ) }
+		>
+			{ label }
+		</Button>
+	);
+};
 
 export default function FontSizePicker( { fontSizes = [], fallbackFontSize, value, onChange } ) {
 	return (
 		<Fragment>
 			<div className="components-font-size-picker__buttons">
 				<ButtonGroup aria-label={ __( 'Font Size' ) }>
-					{ map( fontSizes, ( { name, size, shortName } ) => (
-						<Button
-							key={ size }
-							isLarge
-							isPrimary={ value === size }
-							aria-pressed={ value === size }
-							onClick={ () => onChange( size ) }
-						>
-							{ shortName || name }
-						</Button>
-					) ) }
+					{
+						map( fontSizes, ( { name, size, shortName } ) => shortName && name ? (
+							<Tooltip text={ <div className="components-font-size-picker__name">{ name }</div> }>
+								{ fontSize( shortName, value, size, onChange ) }
+							</Tooltip>
+						) : fontSize( shortName || name, value, size, onChange ) )
+					}
 				</ButtonGroup>
 				<Button
 					isLarge
