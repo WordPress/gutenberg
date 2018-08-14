c1b51b9cde3419d8d4cc67279c03243938b8926c
Mario Zorz <mariozorz@gmail.com>
2018-08-14 12:19:01 -0300

    removed dupe line

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -126,7 +126,6 @@ export const reducer = (
 		case ActionTypes.BLOCK.CREATE: {
 			// TODO we need to set focused: true and search for the currently focused block and
 			// set that one to `focused: false`.
-			blocks.push(action.block);
 			insertBlock(blocks, action.block, action.clientIdAbove);
 			return { blocks: blocks, refresh: ! state.refresh };
 		}
