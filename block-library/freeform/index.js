4e0b3a2a1a3d57284fe080cc7cc8676c6010375b
Matías Ventura <mv@matiasventura.com>
2018-08-15 12:33:59 +0200

    Clean "Classic" icon and linter issues.

--- block-library/freeform/index.js
+++ block-library/freeform/index.js
@@ -16,7 +16,7 @@ export const settings = {
 
 	description: __( 'It’s the classic WordPress editor and it’s a block! Drop the editor right in.' ),
 
-	icon: <svg version="1" width="24" height="24"><path fill="none" d="M0 0h24v24H0V0zm0 0h24v24H0V0z"/><g><path d="M20 7v10H4V7h16m0-2H4L2 7v10l2 2h16l2-2V7l-2-2z"/><path d="M11 8h2v2h-2zM11 11h2v2h-2zM8 8h2v2H8zM8 11h2v2H8zM5 11h2v2H5zM5 8h2v2H5zM8 14h8v2H8zM14 11h2v2h-2zM14 8h2v2h-2zM17 11h2v2h-2zM17 8h2v2h-2z"/></g></svg>,
+	icon: <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0,0h24v24H0V0z M0,0h24v24H0V0z" fill="none" /><path d="m20 7v10h-16v-10h16m0-2h-16c-1.1 0-1.99 0.9-1.99 2l-0.01 10c0 1.1 0.9 2 2 2h16c1.1 0 2-0.9 2-2v-10c0-1.1-0.9-2-2-2z" /><rect x="11" y="8" width="2" height="2" /><rect x="11" y="11" width="2" height="2" /><rect x="8" y="8" width="2" height="2" /><rect x="8" y="11" width="2" height="2" /><rect x="5" y="11" width="2" height="2" /><rect x="5" y="8" width="2" height="2" /><rect x="8" y="14" width="8" height="2" /><rect x="14" y="11" width="2" height="2" /><rect x="14" y="8" width="2" height="2" /><rect x="17" y="11" width="2" height="2" /><rect x="17" y="8" width="2" height="2" /></svg>,
 
 	category: 'formatting',
 
