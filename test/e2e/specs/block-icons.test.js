32a5a55c813a30a57f439712c1da2b45150b3e15
Riad Benguella <benguella@gmail.com>
2018-08-16 16:57:11 +0100

    Make sure the e2e icons test validates the colors properly

--- test/e2e/specs/block-icons.test.js
+++ test/e2e/specs/block-icons.test.js
@@ -10,6 +10,7 @@ import {
 import { activatePlugin, deactivatePlugin } from '../support/plugins';
 
 const INSERTER_BUTTON_SELECTOR = '.components-popover__content .editor-block-types-list__item';
+const INSERTER_ICON_WRAPPER_SELECTOR = `${ INSERTER_BUTTON_SELECTOR } .editor-block-types-list__item-icon`;
 const INSERTER_ICON_SELECTOR = `${ INSERTER_BUTTON_SELECTOR } .editor-block-icon`;
 const INSPECTOR_ICON_SELECTOR = '.edit-post-sidebar .editor-block-icon';
 
@@ -109,8 +110,8 @@ describe( 'Correctly Renders Block Icons on Inserter and Inspector', () => {
 		it( 'Renders the icon in the inserter with the correct colors', async () => {
 			await searchForBlock( blockTitle );
 			validateDashIcon( await getFirstInserterIcon() );
-			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgba(0, 0, 0, 0)' );
-			expect( await getColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(254, 0, 0)' );
+			expect( await getBackgroundColor( INSERTER_ICON_WRAPPER_SELECTOR ) ).toEqual( 'rgb(1, 0, 0)' );
+			expect( await getColor( INSERTER_ICON_WRAPPER_SELECTOR ) ).toEqual( 'rgb(254, 0, 0)' );
 		} );
 
 		it( 'Renders the icon in the inspector with the correct colors', async () => {
@@ -130,8 +131,8 @@ describe( 'Correctly Renders Block Icons on Inserter and Inspector', () => {
 		it( 'Renders the icon in the inserter with the correct background color and an automatically compute readable foreground color', async () => {
 			await searchForBlock( blockTitle );
 			validateSvgIcon( await getFirstInserterIcon() );
-			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgba(0, 0, 0, 0)' );
-			expect( await getColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(248, 249, 249)' );
+			expect( await getBackgroundColor( INSERTER_ICON_WRAPPER_SELECTOR ) ).toEqual( 'rgb(1, 0, 0)' );
+			expect( await getColor( INSERTER_ICON_WRAPPER_SELECTOR ) ).toEqual( 'rgb(248, 249, 249)' );
 		} );
 
 		it( 'Renders correctly the icon on the inspector', async () => {
