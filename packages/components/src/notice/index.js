837d908b524c28e4753e1b417d5a60eedea6722f
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-08-16 16:46:42 +0200

    Add styles for Notice component (#8856)

--- packages/components/src/notice/index.js
+++ packages/components/src/notice/index.js
@@ -9,17 +9,28 @@ import classnames from 'classnames';
  */
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import IconButton from '../icon-button';
+
 function Notice( { className, status, children, onRemove = noop, isDismissible = true } ) {
-	const classNames = classnames( className, 'notice notice-alt notice-' + status, {
+	const classNames = classnames( className, 'components-notice', {
+		[ `is-${ status }` ]: ! ! status,
+	}, {
 		'is-dismissible': isDismissible,
 	} );
 	return (
 		<div className={ classNames }>
-			{ isString( children ) ? <p>{ children }</p> : children }
+			{ isString( children ) ? <div className="components-notice__content">{ children }</div> : children }
 			{ isDismissible && (
-				<button className="notice-dismiss" type="button" onClick={ onRemove }>
-					<span className="screen-reader-text">{ __( 'Dismiss this notice' ) }</span>
-				</button>
+				<IconButton
+					className="components-notice__dismiss"
+					icon="no"
+					label={ __( 'Dismiss this notice' ) }
+					onClick={ onRemove }
+					tooltip={ false }
+				/>
 			) }
 		</div>
 	);
