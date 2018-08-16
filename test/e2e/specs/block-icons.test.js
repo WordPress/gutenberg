6c580c39cff9bbfb91c312978a8ec3194f191b67
Riad Benguella <benguella@gmail.com>
2018-08-16 15:28:11 +0100

    Fix block icons e2e test

--- test/e2e/specs/block-icons.test.js
+++ test/e2e/specs/block-icons.test.js
@@ -10,7 +10,7 @@ import {
 import { activatePlugin, deactivatePlugin } from '../support/plugins';
 
 const INSERTER_BUTTON_SELECTOR = '.components-popover__content .editor-block-types-list__item';
-const INSERTER_ICON_SELECTOR = `${ INSERTER_BUTTON_SELECTOR } .editor-block-types-list__item-icon`;
+const INSERTER_ICON_SELECTOR = `${ INSERTER_BUTTON_SELECTOR } .editor-block-icon`;
 const INSPECTOR_ICON_SELECTOR = '.edit-post-sidebar .editor-block-icon';
 
 async function getInnerHTML( selector ) {
@@ -36,7 +36,7 @@ async function getFirstInserterIcon() {
 describe( 'Correctly Renders Block Icons on Inserter and Inspector', () => {
 	const dashIconRegex = /<svg.*?class=".*?dashicons-cart.*?">.*?<\/svg>/;
 	const circleString = '<circle cx="10" cy="10" r="10" fill="red" stroke="blue" stroke-width="10"></circle>';
-	const svgIcon = `<svg width="20" height="20" viewBox="0 0 20 20">${ circleString }</svg>`;
+	const svgIcon = new RegExp( `<svg.*?viewBox="0 0 20 20".*?>${ circleString }</svg>` );
 
 	const getBlockIdFromBlockName = async ( blockName ) => {
 		return await page.$eval(
@@ -109,7 +109,7 @@ describe( 'Correctly Renders Block Icons on Inserter and Inspector', () => {
 		it( 'Renders the icon in the inserter with the correct colors', async () => {
 			await searchForBlock( blockTitle );
 			validateDashIcon( await getFirstInserterIcon() );
-			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(1, 0, 0)' );
+			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgba(0, 0, 0, 0)' );
 			expect( await getColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(254, 0, 0)' );
 		} );
 
@@ -130,7 +130,7 @@ describe( 'Correctly Renders Block Icons on Inserter and Inspector', () => {
 		it( 'Renders the icon in the inserter with the correct background color and an automatically compute readable foreground color', async () => {
 			await searchForBlock( blockTitle );
 			validateSvgIcon( await getFirstInserterIcon() );
-			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(1, 0, 0)' );
+			expect( await getBackgroundColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgba(0, 0, 0, 0)' );
 			expect( await getColor( INSERTER_ICON_SELECTOR ) ).toEqual( 'rgb(248, 249, 249)' );
 		} );
 
