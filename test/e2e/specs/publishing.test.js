bc82642e731534fc5f13447b8bc36074805dcb2c
Andrés <andres.maneiro@automattic.com>
2018-08-17 14:52:44 +0200

    Add e2e test for publishing a page

--- test/e2e/specs/publishing.test.js
+++ test/e2e/specs/publishing.test.js
@@ -7,22 +7,45 @@ import {
 } from '../support/utils';
 
 describe( 'Publishing', () => {
-	beforeEach( async () => {
-		await newPost();
+	describe( 'a post', () => {
+		beforeEach( async () => {
+			await newPost();
+		} );
+
+		it( 'Should publish a post and close the panel once we start editing again', async () => {
+			await page.type( '.editor-post-title__input', 'E2E Test Post' );
+
+			await publishPost();
+
+			// The post publish panel is visible
+			expect( await page.$( '.editor-post-publish-panel' ) ).not.toBeNull();
+
+			// Start editing again
+			await page.type( '.editor-post-title__input', ' (Updated)' );
+
+			// The post publish panel is not visible anymore
+			expect( await page.$( '.editor-post-publish-panel' ) ).toBeNull();
+		} );
 	} );
 
-	it( 'Should publish a post and close the panel once we start editing again', async () => {
-		await page.type( '.editor-post-title__input', 'E2E Test Post' );
+	describe( 'a page', () => {
+		beforeEach( async () => {
+			await newPost( 'page' );
+		} );
 
-		await publishPost();
+		it( 'Should publish a page and close the panel once we start editing again', async () => {
+			await page.type( '.editor-post-title__input', 'E2E Test Post' );
 
-		// The post publish panel is visible
-		expect( await page.$( '.editor-post-publish-panel' ) ).not.toBeNull();
+			await publishPost();
 
-		// Start editing again
-		await page.type( '.editor-post-title__input', ' (Updated)' );
+			// The post publish panel is visible
+			expect( await page.$( '.editor-post-publish-panel' ) ).not.toBeNull();
 
-		// The post publish panel is not visible anymore
-		expect( await page.$( '.editor-post-publish-panel' ) ).toBeNull();
+			// Start editing again
+			await page.type( '.editor-post-title__input', ' (Updated)' );
+
+			// The post publish panel is not visible anymore
+			expect( await page.$( '.editor-post-publish-panel' ) ).toBeNull();
+		} );
 	} );
 } );
