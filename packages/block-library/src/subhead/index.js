f90b2d66da4135564ebb04e0b90d888489cfde0e
Joen Asmussen <joen@automattic.com>
2018-08-20 15:53:16 +0200

    Update Heading icon (#9159)
    This refines the recent heading icon change in https://github.com/WordPress/gutenberg/pull/9152#issuecomment-414286401, per feedback.
    
    Although it is H for Heading, internationally that first letter is not quite as ubiquitous, whereas T fares a bit better. By changing from an H we are also avoiding the very same issue that we sought to address by bringing back the Paragraph icon, namely that there are going to be icons (for heading levels) right next to the switcher, and if they look too alike that might cause confusion.

--- packages/block-library/src/subhead/index.js
+++ packages/block-library/src/subhead/index.js
@@ -17,7 +17,7 @@ export const settings = {
 
 	description: __( 'What’s a subhead? Smaller than a headline, bigger than basic text.' ),
 
-	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.9 20h-3l.9-5h-4l-.9 5h-3L8.1 7h3l-.9 5h4l.9-5h3l-2.2 13z" /></svg>,
+	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7.1 6l-.5 3h4.5L9.4 19h3l1.8-10h4.5l.5-3H7.1z" /></svg>,
 
 	category: 'common',
 
