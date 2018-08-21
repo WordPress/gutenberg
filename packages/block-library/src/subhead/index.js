c47afdf7c9b31efde1451ef12c56e8042317077e
David Ryan <reachdavidryan@gmail.com>
2018-08-20 17:31:52 -0700

    revert subhead change

--- packages/block-library/src/subhead/index.js
+++ packages/block-library/src/subhead/index.js
@@ -15,7 +15,7 @@ export const name = 'core/subhead';
 export const settings = {
 	title: __( 'Subheading' ),
 
-	description: __( 'Short, explanatory subtitle to support section headings and overall topic.' ),
+	description: __( 'What’s a subhead? Smaller than a headline, bigger than basic text.' ),
 
 	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7.1 6l-.5 3h4.5L9.4 19h3l1.8-10h4.5l.5-3H7.1z" /></svg>,
 
