e603be2ee638d3cd9fd0ef47824f515bafd696d9
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-08-22 16:48:56 +0200

    Components: Use explicit name for Tooltip component in RN

--- packages/components/src/tooltip/index.native.js
+++ packages/components/src/tooltip/index.native.js
@@ -1,4 +1,9 @@
+/**
+ * WordPress dependencies
+ */
 import { Children } from '@wordpress/element';
 
 // For native mobile, just shortcircuit the Tooltip to return its child.
-export default ( props ) => Children.only( props.children );
+const Tooltip = ( props ) => Children.only( props.children );
+
+export default Tooltip;
