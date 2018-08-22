1cfcf9dd88bbed68375b66d9f906c9c0cef12945
Daniel Richards <daniel.richards@automattic.com>
2018-08-22 16:10:01 +0800

    Add MediaUploadCheck component and unit tests
    - Hardcode hasUploadPermissions value to true for now until implementation of
    server-side and redux state has been finalised
    
    Co-authored-by: imath <math.viet@gmail.com>

--- packages/editor/src/components/index.js
+++ packages/editor/src/components/index.js
@@ -22,6 +22,7 @@ export { default as RichText } from './rich-text';
 export { default as RichTextProvider } from './rich-text/provider';
 export { default as MediaPlaceholder } from './media-placeholder';
 export { default as MediaUpload } from './media-upload';
+export { default as MediaUploadCheck } from './media-upload/check';
 export { default as URLInput } from './url-input';
 export { default as URLInputButton } from './url-input/button';
 
