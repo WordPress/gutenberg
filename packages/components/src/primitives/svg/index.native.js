8a55551332101e50754b8d3b536c2e95b2837cab
SergioEstevao <sergioestevao@gmail.com>
2018-08-22 17:45:43 +0100

    Fix warning on SVG

--- packages/components/src/primitives/svg/index.native.js
+++ packages/components/src/primitives/svg/index.native.js
@@ -1,4 +1,13 @@
-export {
-	Path,
-	Svg as SVG,
+import {
+	Path as PATH,
+	Svg,
 } from 'react-native-svg';
+
+export const Path = PATH;
+export const SVG = ( props ) => {
+	return (
+		<Svg width={ props.width } height={ props.height } >
+			{ props.children }
+		</Svg>
+	);
+}
