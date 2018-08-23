99dffe348e1b0f4d16e91b6af99ac8b9962ccfac
Daniel Richards <daniel.richards@automattic.com>
2018-08-23 15:45:51 +0800

    Position url input underneath upload file buttons and apply margin
    Co-authored-by: imath <math.viet@gmail.com>

--- packages/editor/src/components/media-placeholder/index.js
+++ packages/editor/src/components/media-placeholder/index.js
@@ -128,23 +128,6 @@ class MediaPlaceholder extends Component {
 					onFilesDrop={ this.onFilesUpload }
 					onHTMLDrop={ onHTMLDrop }
 				/>
-				{ onSelectURL && (
-					<form onSubmit={ this.onSubmitSrc }>
-						<input
-							type="url"
-							className="components-placeholder__input"
-							aria-label={ labels.title }
-							placeholder={ __( 'Enter URL here…' ) }
-							onChange={ this.onChangeSrc }
-							value={ this.state.src }
-						/>
-						<Button
-							isLarge
-							type="submit">
-							{ __( 'Use URL' ) }
-						</Button>
-					</form>
-				) }
 				<FormFileUpload
 					isLarge
 					className="editor-media-placeholder__upload-button"
@@ -166,6 +149,26 @@ class MediaPlaceholder extends Component {
 						</Button>
 					) }
 				/>
+				{ onSelectURL && (
+					<form
+						className="editor-media-placeholder__url-input-form"
+						onSubmit={ this.onSubmitSrc }
+					>
+						<input
+							type="url"
+							className="components-placeholder__input"
+							aria-label={ labels.title }
+							placeholder={ __( 'Enter URL here…' ) }
+							onChange={ this.onChangeSrc }
+							value={ this.state.src }
+						/>
+						<Button
+							isLarge
+							type="submit">
+							{ __( 'Use URL' ) }
+						</Button>
+					</form>
+				) }
 			</Placeholder>
 		);
 	}
