516f66120b78b6f22dbf548032df67b40b5fc1e5
Mario Zorz <mariozorz@gmail.com>
2018-08-24 11:53:33 -0300

    stripping style if not an object in svg native implementation

--- packages/components/src/primitives/svg/index.native.js
+++ packages/components/src/primitives/svg/index.native.js
@@ -14,9 +14,13 @@ export {
 };
 
 export const SVG = ( props ) => {
-	if ( props.width !== undefined && props.height !== undefined ) {
+	// for some reason if a `className` prop is passed it gets converted to `style` here. 
+	// Given it carries a string (as it was originally className) but an object is expected for `style`,
+	// we need to check whether `style` exists and is a string, and strip in that case.
+	const safeProps = ( typeof props.style === 'string' || props.style instanceof String ) ? { ...omit( props, [ 'style', 'className' ]) } : { ...omit( props, [ 'className' ] ) };
+	if ( safeProps.width !== undefined && safeProps.height !== undefined ) {
 		return (
-			<Svg { ...omit( props, [ 'className' ]) } />
+			<Svg { ...safeProps } />
 		);
 	}
 
@@ -24,6 +28,6 @@ export const SVG = ( props ) => {
 	// i.e. viewBox="0 0 24 24" as <Svg> needs width and height to be explicitely set
 	const viewBoxCoords = props.viewBox.split( ' ' );
 	return (
-		<Svg { ...omit( {...props, width: viewBoxCoords[ 2 ], height: viewBoxCoords[ 3 ] }, [ 'className' ]) } />
+		<Svg { ...{ ...safeProps, width: viewBoxCoords[ 2 ], height: viewBoxCoords[ 3 ] } } />
 	);
 };
