3ed34248d2b57ddec890e520c8645918ce3780cc
Dennis Snell <dennis.snell@automattic.com>
2018-08-24 23:05:50 +0100

    Move code into `/packages` directory, prepare for review

--- packages/block-serialization-default-parser/index.js
+++ packages/block-serialization-default-parser/index.js
@@ -1,7 +1,3 @@
-/**
- * Sitting here for now in the PR because I don't have a better place for it.
- */
-
 let document;
 let offset;
 let output;
@@ -9,7 +5,7 @@ let stack;
 const tokenizer = /<!--\s+(?<closer>\/)?wp:(?<namespace>[a-z][a-z0-9_-]*\/)?(?<name>[a-z][a-z0-9_-]*)\s+(?<attrs>{(?:(?!}\s+-->)[^])+}\s+)?(?<void>\/)?-->/g;
 
 class Block {
-	constructor(name, attrs, innerBlocks, innerHTML) {
+	constructor( name, attrs, innerBlocks, innerHTML ) {
 		this.blockName = name;
 		this.attrs = attrs;
 		this.innerBlocks = innerBlocks;
@@ -18,15 +14,15 @@ class Block {
 }
 
 class Frame {
-	constructor(block, tokenStart, tokenLength, prevOffset) {
+	constructor( block, tokenStart, tokenLength, prevOffset ) {
 		this.block = block;
 		this.tokenStart = tokenStart;
 		this.tokenLength = tokenLength;
-		this.prevOffset = prevOffset || (tokenStart + tokenLength);
+		this.prevOffset = prevOffset || tokenStart + tokenLength;
 	}
 }
 
-export const parse = (doc) => {
+export const parse = ( doc ) => {
 	document = doc;
 	offset = 0;
 	output = [];
@@ -35,90 +31,83 @@ export const parse = (doc) => {
 
 	do {
 		// twiddle our thumbs
-	} while (proceed());
+	} while ( proceed() );
 
 	return output;
-}
+};
 
 function proceed() {
-	const [tokenType, block_name, attrs, startOffset, tokenLength] = nextToken();
+	const [ tokenType, blockName, attrs, startOffset, tokenLength ] = nextToken();
 	const stackDepth = stack.length;
 
-	switch (tokenType) {
+	switch ( tokenType ) {
 		case 'no-more-tokens':
 			// if not in a block then flush output
-			if (0 === stackDepth) {
+			if ( 0 === stackDepth ) {
 				addFreeform();
 				return false;
 			}
 
 			// Otherwise we have a problem
 			// This is an error
-			error('in a block but found no closer');
-			error('failed at ' + offset);
-
 			// we have options
 			//  - treat it all as freeform text
 			//  - assume an implicit closer (easiest when not nesting)
 
 			// for the easy case we'll assume an implicit closer
-			if (1 === stackDepth) {
-				error(' - treating as implicit closer');
-				addBlockFromStck();
+			if ( 1 === stackDepth ) {
+				addBlockFromStack();
 				return false;
 			}
 
 			// for the nested case where it's more difficult we'll
 			// have to assume that multiple closers are missing
 			// and so we'll collapse the whole stack piecewise
-			error(' - multiple closers are missing');
-			error(' - recursively collapsing stack of blocks');
-			while (0 < stack.length) {
-				addBlockFromStck();
+			while ( 0 < stack.length ) {
+				addBlockFromStack();
 			}
 			return false;
 
 		case 'void-block':
 			// easy case is if we stumbled upon a void block
 			// in the top-level of the document
-			if (0 === stackDepth) {
-				output.push(new Block(block_name, attrs, [], ''));
+			if ( 0 === stackDepth ) {
+				output.push( new Block( blockName, attrs, [], '' ) );
 				offset = startOffset + tokenLength;
 				return true;
 			}
 
 			// otherwise we found an inner block
 			addInnerBlock(
-				new Block(block_name, attrs, [], ''),
+				new Block( blockName, attrs, [], '' ),
 				startOffset,
-				tokenLength
+				tokenLength,
 			);
 			offset = startOffset + tokenLength;
 			return true;
 
 		case 'block-opener':
 			// we may have some HTML soup before the next block
-			if (startOffset > offset) {
-				addFreeform(startOffset - offset);
+			if ( startOffset > offset ) {
+				addFreeform( startOffset - offset );
 			}
 
 			// track all newly-opened blocks on the stack
-			stack.push(new Frame(
-				new Block(block_name, attrs, [], ''),
-				startOffset,
-				tokenLength,
-				startOffset + tokenLength
-			));
+			stack.push(
+				new Frame(
+					new Block( blockName, attrs, [], '' ),
+					startOffset,
+					tokenLength,
+					startOffset + tokenLength,
+				),
+			);
 			offset = startOffset + tokenLength;
 			return true;
 
 		case 'block-closer':
 			// if we're missing an opener we're in trouble
 			// This is an error
-			if (0 === stackDepth) {
-				error('found a closer with no opening block');
-				error('failed at offset ' + startOffset);
-
+			if ( 0 === stackDepth ) {
 				// we have options
 				//  - assume an implicit opener
 				//  - assume _this_ is the opener
@@ -128,30 +117,32 @@ function proceed() {
 			}
 
 			// if we're not nesting then this is easy - close the block
-			if (1 === stackDepth) {
-				addBlockFromStck(startOffset);
+			if ( 1 === stackDepth ) {
+				addBlockFromStack( startOffset );
 				offset = startOffset + tokenLength;
 				return true;
 			}
 
 			// otherwise we're nested and we have to close out the current
 			// block and add it as a new innerBlock to the parent
-			stackTop = stack.pop();
-			stackTop.block.innerHTML += document.substr(stackTop.prevOffset, startOffset - stackTop.prevOffset);
+			const stackTop = stack.pop();
+			stackTop.block.innerHTML += document.substr(
+				stackTop.prevOffset,
+				startOffset - stackTop.prevOffset,
+			);
 			stackTop.prevOffset = startOffset + tokenLength;
 
 			addInnerBlock(
 				stackTop.block,
 				stackTop.tokenStart,
 				stackTop.tokenLength,
-				startOffset + tokenLength
+				startOffset + tokenLength,
 			);
 			offset = startOffset + tokenLength;
 			return true;
 
 		default:
 			// This is an error
-			error('found unexpected token at offset ' + offset);
 			addFreeform();
 			return false;
 	}
@@ -163,74 +154,79 @@ function nextToken() {
 	// we're also using a trick here because the only difference between a
 	// block opener and a block closer is the leading `/` before `wp:` (and
 	// a closer has no attributes). we can trap them both and process the
-	// match back in PHP to see which one it was.
-	const matches = tokenizer.exec(document);
+	// match back in Javascript to see which one it was.
+	const matches = tokenizer.exec( document );
 
 	// we have no more tokens
-	if (null === matches) {
-		return ['no-more-tokens'];
+	if ( null === matches ) {
+		return [ 'no-more-tokens' ];
 	}
 
 	const startedAt = matches.index;
-	const match = matches[0];
+	const [ match, closerMatch, namespaceMatch, nameMatch, attrsMatch, voidMatch ] = matches;
 
 	const length = match.length;
-	const isCloser = !!matches.groups.closer;
-	const isVoid = !!matches.groups.void;
-	const namespace = matches.groups.namespace || 'core/';
-	const name = namespace + matches.groups.name;
-	const hasAttrs = !!matches.groups.attrs;
-	const attrs = hasAttrs ? JSON.parse(matches.groups.attrs) : null;
+	const isCloser = !! closerMatch;
+	const isVoid = !! voidMatch;
+	const namespace = namespaceMatch || 'core/';
+	const name = namespace + nameMatch;
+	const hasAttrs = !! attrsMatch;
+	const attrs = hasAttrs ? JSON.parse( attrsMatch ) : null;
 
 	// This state isn't allowed
 	// This is an error
-	if (isCloser && (isVoid || hasAttrs)) {
-		error('closing comment delimiters must not have attributes or void closers');
-		error('failed at offset ' + startedAt + ': ' + match);
-
+	if ( isCloser && ( isVoid || hasAttrs ) ) {
 		// we can ignore them since they don't hurt anything
+		// we may warn against this at some point or reject it
 	}
 
-	if (isVoid) {
-		return ['void-block', name, attrs, startedAt, length];
+	if ( isVoid ) {
+		return [ 'void-block', name, attrs, startedAt, length ];
 	}
 
-	if (isCloser) {
-		return ['block-closer', name, null, startedAt, length];
+	if ( isCloser ) {
+		return [ 'block-closer', name, null, startedAt, length ];
 	}
 
-	return ['block-opener', name, attrs, startedAt, length];
+	return [ 'block-opener', name, attrs, startedAt, length ];
 }
 
-function addFreeform(rawLength) {
+function addFreeform( rawLength ) {
 	const length = rawLength ? rawLength : document.length - offset;
 
-	if (0 === length) {
+	if ( 0 === length ) {
 		return;
 	}
 
-	output.push({
-		'attrs': {},
-		'innerHTML': document.substr(offset, length),
-	});
+	// why is this not a Frame? it's because the current grammar
+	// specifies an object that's different. we can update the
+	// specification and change here if we want to but for now we
+	// want this parser to be spec-compliant
+	output.push( {
+		attrs: {},
+		innerHTML: document.substr( offset, length ),
+	} );
 }
 
-function addInnerBlock(block, tokenStart, tokenLength, lastOffset) {
-	parent = stack[stack.length - 1];
-	parent.block.innerBlocks.push(block);
-	parent.block.innerHTML += document.substr(parent.prevOffset, tokenStart - parent.prevOffset);
+function addInnerBlock( block, tokenStart, tokenLength, lastOffset ) {
+	const parent = stack[ stack.length - 1 ];
+	parent.block.innerBlocks.push( block );
+	parent.block.innerHTML += document.substr(
+		parent.prevOffset,
+		tokenStart - parent.prevOffset,
+	);
 	parent.prevOffset = lastOffset ? lastOffset : tokenStart + tokenLength;
 }
 
-function addBlockFromStck(endOffset) {
-	stackTop = stack.pop();
-	prevOffset = stackTop.prevOffset;
+function addBlockFromStack( endOffset ) {
+	const stackTop = stack.pop();
+	const prevOffset = stackTop.prevOffset;
 
-	stackTop.block.innerHTML += endOffset ? document.substr(prevOffset, endOffset - prevOffset) : document.substr(prevOffset);
+	if ( endOffset ) {
+		stackTop.block.innerHTML += document.substr( prevOffset, endOffset - prevOffset );
+	} else {
+		stackTop.block.innerHTML += document.substr( prevOffset );
+	}
 
-	output.push(stackTop.block);
-}
-
-function error(message) {
-	console.log(message);
+	output.push( stackTop.block );
 }
--- packages/block-serialization-default-parser/parser.php
+++ packages/block-serialization-default-parser/parser.php
@@ -1,85 +1,16 @@
 <?php
 
-/**
- * Implements the formal specification for parsing Gutenberg documents
- * serialized into HTML (nominally in `post_content` of a WordPress post)
- *
- * @see https://github.com/WordPress/gutenberg/tree/master/packages/block-serialization-spec-parser
- *
- * ## What is different about this one from the spec-parser?
- *
- * This is a recursive-descent parser that scans linearly once through the input document.
- * Instead of directly recursing it utilizes a trampoline mechanism to prevent stack overflow.
- * In order to minimize data copying and passing it's built into a class with class properties.
- * Between every token (a block comment delimiter) we can instrument the parser and intervene.
- *
- * The spec parser is defined via a _Parsing Expression Grammar_ (PEG) which answers many
- * questions inherently that we must answer explicitly in this parser. The goal for this
- * implementation is to match the characteristics of the PEG so that it can be directly
- * swapped out so that the only changes are better runtime performance and memory usage.
- *
- * ## How does it work?
- *
- * It's pretty self-explanatory...haha
- *
- * Every Gutenberg document is nominally an HTML document which in addition to normal HTML may
- * also contain specially designed HTML comments - the block comment delimiters - which separate
- * and isolate the blocks which are serialized in the document.
- *
- * This parser attempts to create a kind of state-machine around the transitions triggered from
- * those delimiters - the "tokens" of the grammar. Every time we find one we should only be doing
- * one of a small set of actions:
- *
- *  - enter a new block
- *  - exit out of a block
- *
- * Those actions have different effects depending on the context; for instance, when we exit a
- * block we either need to add it to the output block list _or_ we need to append it as the
- * next `innerBlock` on the parent block below it in the block stack (the place where we track
- * open blocks). The details are documented below.
- *
- * The biggest challenge in this parser is making the right accounting of indices required to
- * to construct the `innerHTML` values for each block at every level of nesting depth. We take
- * a simple approach:
- *
- *  - start each newly-opened block with an empty `innerHTML`
- *  - whenever we push a first block into the `innerBlocks` list then add the content from
- *    where the content of the parent block started to where this inner block starts
- *  - whenever we push another block into the `innerBlocks` list then add the content from
- *    where the previous inner block ended to where this inner block starts
- *  - when we close out an open block we add the content from where the last inner block
- *    ended to where the closing block delimiter starts
- *  - if there are no inner blocks then we take the entire content between the opening and
- *    closing block comment delimiters as the `innerHTML`
- *
- * ## I meant, how does it perform?
- *
- * This parser operates much faster than the generated parser from the specification.
- * Because w know more about the parsing than the PEG does we can take advantage of several
- * tricks to improve our speed and memory usage:
- *
- *  - we only have one or two distinct tokens depending on how you look at it and they are
- *    all readily matched via a regular expression. instead of parsing on a character-per-
- *    character basis we can allow the PCRE RegExp engine skip over large swaths of the
- *    document for us in order to find those tokens.
- *  - since `preg_match()` takes an `offset` parameter we can crawl through the input
- *    without passing copies of the input text on every step. we can track our position
- *    in the string and only pass a number instead
- *  - not copying all those strings means that we'll also skip many memory allocations
- *
- */
-
-function rdt_parse( $document ) {
+function bsdp_parse($document ) {
     static $parser;
 
     if ( ! isset( $parser ) ) {
-        $parser = new RDT_Parser();
+        $parser = new BSDP_Parser();
     }
 
     return $parser->parse( $document );
 }
 
-class RDT_Block {
+class BSDP_Block {
     public $blockName;
     public $attrs;
     public $innerBlocks;
@@ -93,7 +24,7 @@ class RDT_Block {
     }
 }
 
-class RDT_Frame {
+class BSDP_Frame {
     public $block;
     public $token_start;
     public $token_length;
@@ -107,7 +38,7 @@ class RDT_Frame {
     }
 }
 
-class RDT_Parser {
+class BSDP_Parser {
     public $document;
     public $offset;
     public $output;
@@ -140,8 +71,6 @@ class RDT_Parser {
 
                 # Otherwise we have a problem
                 # This is an error
-                $this->error( 'in a block but found no closer' );
-                $this->error( 'failed at ' . $this->offset );
 
                 # we have options
                 #  - treat it all as freeform text
@@ -149,7 +78,6 @@ class RDT_Parser {
 
                 # for the easy case we'll assume an implicit closer
                 if ( 1 === $stack_depth ) {
-                    $this->error( ' - treating as implicit closer' );
                     $this->add_block_from_stack();
                     return false;
                 }
@@ -157,8 +85,6 @@ class RDT_Parser {
                 # for the nested case where it's more difficult we'll
                 # have to assume that multiple closers are missing
                 # and so we'll collapse the whole stack piecewise
-                $this->error( ' - multiple closers are missing' );
-                $this->error( ' - recursively collapsing stack of blocks' );
                 while ( 0 < count( $this->stack ) ) {
                     $this->add_block_from_stack();
                 }
@@ -168,14 +94,14 @@ class RDT_Parser {
                 # easy case is if we stumbled upon a void block
                 # in the top-level of the document
                 if ( 0 === $stack_depth ) {
-                    $this->output[] = new RDT_Block( $block_name, $attrs, array(), '' );
+                    $this->output[] = new BSDP_Block( $block_name, $attrs, array(), '' );
                     $this->offset = $start_offset + $token_length;
                     return true;
                 }
 
                 # otherwise we found an inner block
                 $this->add_inner_block(
-                    new RDT_Block( $block_name, $attrs, array(), '' ),
+                    new BSDP_Block( $block_name, $attrs, array(), '' ),
                     $start_offset,
                     $token_length
                 );
@@ -189,8 +115,8 @@ class RDT_Parser {
                 }
 
                 # track all newly-opened blocks on the stack
-                array_push( $this->stack, new RDT_Frame(
-                    new RDT_Block( $block_name, $attrs, array(), '' ),
+                array_push( $this->stack, new BSDP_Frame(
+                    new BSDP_Block( $block_name, $attrs, array(), '' ),
                     $start_offset,
                     $token_length,
                     $start_offset + $token_length
@@ -202,9 +128,6 @@ class RDT_Parser {
                 # if we're missing an opener we're in trouble
                 # This is an error
                 if ( 0 === $stack_depth ) {
-                    $this->error( 'found a closer with no opening block' );
-                    $this->error( 'failed at offset ' . $start_offset );
-
                     # we have options
                     #  - assume an implicit opener
                     #  - assume _this_ is the opener
@@ -237,7 +160,6 @@ class RDT_Parser {
 
             default:
                 # This is an error
-                $this->error( 'found unexpected token at offset ' . $this->offset );
                 $this->add_freeform();
                 return false;
         }
@@ -279,9 +201,6 @@ class RDT_Parser {
         # This state isn't allowed
         # This is an error
         if ( $is_closer && ( $is_void || $has_attrs ) ) {
-            $this->error( 'closing comment delimiters must not have attributes or void closers' );
-            $this->error( 'failed at offset ' . $started_at . ': ' . $match );
-
             # we can ignore them since they don't hurt anything
         }
 
@@ -309,7 +228,7 @@ class RDT_Parser {
         );
     }
 
-    function add_inner_block( RDT_Block $block, $token_start, $token_length, $last_offset = null ) {
+    function add_inner_block(BSDP_Block $block, $token_start, $token_length, $last_offset = null ) {
         $parent = $this->stack[ count( $this->stack ) - 1 ];
         $parent->block->innerBlocks[] = $block;
         $parent->block->innerHTML .= substr( $this->document, $parent->prev_offset, $token_start - $parent->prev_offset );
@@ -325,9 +244,5 @@ class RDT_Parser {
             : substr( $this->document, $prev_offset );
 
         $this->output[] = $stack_top->block;
-    }
-
-    function error( $message ) {
-        # silence is golden
     }
 }
