4b4b4d67928cb7b07d37468d9fb52412cd85e3f0
Daniel Richards <daniel.richards@automattic.com>
2018-08-24 16:50:21 +0800

    Add test for OPTIONS requests in preloading middleware

--- packages/api-fetch/src/middlewares/test/preloading.js
+++ packages/api-fetch/src/middlewares/test/preloading.js
@@ -4,26 +4,53 @@
 import createPreloadingMiddleware from '../preloading';
 
 describe( 'Preloading Middleware', () => {
-	it( 'should return the preloaded data if provided', () => {
-		const body = {
-			status: 'this is the preloaded response',
+	it( 'should return the preloaded response body if provided when using a GET request', () => {
+		const postsResponse = {
+			body: {
+				status: 'this is the preloaded response',
+			},
 		};
 		const preloadedData = {
 			GET: {
-				'wp/v2/posts': {
-					body,
-				},
+				'wp/v2/posts': postsResponse,
 			},
 		};
-		const prelooadingMiddleware = createPreloadingMiddleware( preloadedData );
+		const preloadingMiddleware = createPreloadingMiddleware( preloadedData );
 		const requestOptions = {
 			method: 'GET',
 			path: 'wp/v2/posts',
 		};
 
-		const response = prelooadingMiddleware( requestOptions );
+		const response = preloadingMiddleware( requestOptions );
 		return response.then( ( value ) => {
-			expect( value ).toEqual( body );
+			expect( value ).toEqual( postsResponse.body );
+		} );
+	} );
+
+	it( 'should return the preloaded full response data if provided when using an OPTIONS request', () => {
+		const postsResponse = {
+			headers: {
+				test: 'this is a preloaded header',
+			},
+			body: {
+				status: 'this is the preloaded response',
+			},
+		};
+
+		const preloadedData = {
+			OPTIONS: {
+				'wp/v2/posts': postsResponse,
+			},
+		};
+		const preloadingMiddleware = createPreloadingMiddleware( preloadedData );
+		const requestOptions = {
+			method: 'OPTIONS',
+			path: 'wp/v2/posts',
+		};
+
+		const response = preloadingMiddleware( requestOptions );
+		return response.then( ( value ) => {
+			expect( value ).toEqual( postsResponse );
 		} );
 	} );
 
