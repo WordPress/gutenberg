c75a0db13fe675b80cfd869aa926256b0a7c5553
Mario Zorz <mariozorz@gmail.com>
2018-08-23 22:03:11 -0300

    adds simple mechanism to obtain viewport from viewBox so height and width do not have to be indicated explicitely

--- packages/block-library/src/heading/index.js
+++ packages/block-library/src/heading/index.js
@@ -63,7 +63,7 @@ export const settings = {
 
 	description: __( 'Insert a headline above your post or page content.' ),
 
-	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height={ 24 } width={ 24 }><Path d="M5 4v3h5.5v12h3V7H19V4z" /><Path fill="none" d="M0 0h24v24H0V0z" /></SVG>,
+	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M5 4v3h5.5v12h3V7H19V4z" /><Path fill="none" d="M0 0h24v24H0V0z" /></SVG>,
 
 	category: 'common',
 
--- packages/components/src/primitives/svg/index.native.js
+++ packages/components/src/primitives/svg/index.native.js
@@ -7,9 +7,20 @@ import {
 export const Path = PATH;
 export const G = GROUP;
 export const SVG = ( props ) => {
-	return (
-		<Svg width={ props.width } height={ props.height } >
-			{ props.children }
-		</Svg>
-	);
+	if (props.width != undefined && props.height != undefined) {
+		return (
+			<Svg width={ props.width } height={ props.height } >
+				{ props.children }
+			</Svg>
+		);
+	} else {
+		// take viewport system to match the viewBox definition
+		// i.e. viewBox="0 0 24 24"
+		let viewBoxCoords = props.viewBox.split(' ');
+		return (
+			<Svg width={ viewBoxCoords[2] } height={ viewBoxCoords[3] } >
+				{ props.children }
+			</Svg>
+		);
+	}
 };
