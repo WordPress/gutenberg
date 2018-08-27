18ef67525ed24bb6a844184f30fb6dba63e7dbe2
Andrés <andres.maneiro@automattic.com>
2018-08-27 14:07:07 +0200

    Convert block-draggable into class

--- packages/editor/src/components/block-list/block-draggable.js
+++ packages/editor/src/components/block-list/block-draggable.js
@@ -7,25 +7,29 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import { Draggable } from '@wordpress/components';
+import { Component } from '@wordpress/element';
 
-function BlockDraggable( { rootClientId, index, clientId, layout, isDragging, ...props } ) {
-	const className = classnames( 'editor-block-list__block-draggable', {
-		'is-visible': isDragging,
-	} );
+class BlockDraggable extends Component {
+	render() {
+		const { rootClientId, index, clientId, layout, isDragging, ...props } = this.props;
+		const className = classnames( 'editor-block-list__block-draggable', {
+			'is-visible': isDragging,
+		} );
 
-	const transferData = {
-		type: 'block',
-		fromIndex: index,
-		rootClientId,
-		clientId,
-		layout,
-	};
+		const transferData = {
+			type: 'block',
+			fromIndex: index,
+			rootClientId,
+			clientId,
+			layout,
+		};
 
-	return (
-		<Draggable className={ className } transferData={ transferData } { ...props }>
-			<div className="editor-block-list__block-draggable-inner"></div>
-		</Draggable>
-	);
+		return (
+			<Draggable className={ className } transferData={ transferData } { ...props }>
+				<div className="editor-block-list__block-draggable-inner"></div>
+			</Draggable>
+		);
+	}
 }
 
 export default BlockDraggable;
