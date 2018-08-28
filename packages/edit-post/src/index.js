9b859a8c517c958b99c88b7fb71352c2316a95b3
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-08-28 11:01:10 +0200

    Revert changes for sidebar related components

--- packages/edit-post/src/index.js
+++ packages/edit-post/src/index.js
@@ -2,5 +2,3 @@ export { default as PluginBlockSettingsMenuItem } from './components/plugin-bloc
 export { default as PluginPostPublishPanel } from './components/plugin-post-publish-panel';
 export { default as PluginPostStatusInfo } from './components/plugin-post-status-info';
 export { default as PluginPrePublishPanel } from './components/plugin-pre-publish-panel';
-export { default as PluginSidebarMoreMenuItem } from './components/plugin-sidebar-more-menu-item';
-export { default as PluginsMoreMenuGroup } from './components/plugins-more-menu-group';
