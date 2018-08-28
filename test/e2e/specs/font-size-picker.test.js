edabec651580b55e90f8e6e7549e83951e5eada9
Matthew Riley MacPherson <hi@tofumatt.com>
2018-08-28 01:29:02 +0100

    chore: Tweak comments and grammar/style

--- test/e2e/specs/font-size-picker.test.js
+++ test/e2e/specs/font-size-picker.test.js
@@ -12,44 +12,47 @@ describe( 'Font Size Picker', () => {
 		await newPost();
 	} );
 
-	it( 'Should apply a named font size using the font size buttons', async () => {
-		// Creating a paragraph block with some content
+	it( 'should apply a named font size using the font size buttons', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph to be made "large"' );
 
 		const largeButton = ( await page.$x( '//*[contains(concat(" ", @class, " "), " components-font-size-picker__buttons ")]//*[text()=\'L\']' ) )[ 0 ];
 		await largeButton.click();
-		// Check content
+
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'Should apply a named font size using the font size input', async () => {
-		// Creating a paragraph block with some content
+	it( 'should apply a named font size using the font size input', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph to be made "small"' );
 
 		await page.click( '.blocks-font-size .components-range-control__number' );
 		await page.keyboard.type( '14' );
-		// Check content
+
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'Should apply a custom font size using the font size input', async () => {
-		// Creating a paragraph block with some content
+	it( 'should apply a custom font size using the font size input', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph to be made "small"' );
 
 		await page.click( '.blocks-font-size .components-range-control__number' );
 		await page.keyboard.type( '23' );
-		// Check content
+
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'Should reset a named font size using the reset button', async () => {
-		// Creating a paragraph block with some content
+	it( 'should reset a named font size using the reset button', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph with font size reset using button' );
 
@@ -59,13 +62,13 @@ describe( 'Font Size Picker', () => {
 		const resetButton = ( await page.$x( '//*[contains(concat(" ", @class, " "), " components-font-size-picker__buttons ")]//*[text()=\'Reset\']' ) )[ 0 ];
 		await resetButton.click();
 
-		// Check content
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'Should reset a named font size using input field', async () => {
-		// Creating a paragraph block with some content
+	it( 'should reset a named font size using input field', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph with font size reset using input field' );
 
@@ -76,13 +79,13 @@ describe( 'Font Size Picker', () => {
 		await page.keyboard.press( 'Backspace' );
 		await page.keyboard.press( 'Backspace' );
 
-		// Check content
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'Should reset a custom font size using input field', async () => {
-		// Creating a paragraph block with some content
+	it( 'should reset a custom font size using input field', async () => {
+		// Create a paragraph block with some content.
 		await clickBlockAppender();
 		await page.keyboard.type( 'Paragraph to be made "small"' );
 
@@ -94,7 +97,8 @@ describe( 'Font Size Picker', () => {
 		await page.click( '.blocks-font-size .components-range-control__number' );
 		await page.keyboard.press( 'Backspace' );
 		await page.keyboard.press( 'Backspace' );
-		// Check content
+
+		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
 		expect( content ).toMatchSnapshot();
 	} );
