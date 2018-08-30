0427c192c5d54e60affc2205bfa0d06ac513ca2f
Andrés <andres.maneiro@automattic.com>
2018-08-30 10:06:23 +0200

    BlockDraggable: convert it back to a functional component

--- packages/editor/src/components/block-list/block-draggable.js
+++ packages/editor/src/components/block-list/block-draggable.js
@@ -6,37 +6,33 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
 import { Draggable } from '@wordpress/components';
 
-class BlockDraggable extends Component {
-	render() {
-		const { isDragging, elementId, transferData, onDragStart, onDragEnd } = this.props;
-		const className = classnames( 'editor-block-list__block-draggable', {
-			'is-visible': isDragging,
-		} );
+const BlockDraggable = ( { isDragging, elementId, transferData, onDragStart, onDragEnd } ) => {
+	const className = classnames( 'editor-block-list__block-draggable', {
+		'is-visible': isDragging,
+	} );
 
-		return (
-			<Draggable
-				elementId={ elementId }
-				transferData={ transferData }
-				onDragStart={ onDragStart }
-				onDragEnd={ onDragEnd }
-			>
-				{
-					( { onDraggableStart, onDraggableEnd } ) => (
-						<div
-							className={ className }
-							onDragStart={ onDraggableStart }
-							onDragEnd={ onDraggableEnd }
-							draggable
-						>
-							<div className="editor-block-list__block-draggable-inner"></div>
-						</div> )
-				}
-			</Draggable>
-		);
-	}
-}
+	return (
+		<Draggable
+			elementId={ elementId }
+			transferData={ transferData }
+			onDragStart={ onDragStart }
+			onDragEnd={ onDragEnd }
+		>
+			{
+				( { onDraggableStart, onDraggableEnd } ) => (
+					<div
+						className={ className }
+						onDragStart={ onDraggableStart }
+						onDragEnd={ onDraggableEnd }
+						draggable
+					>
+						<div className="editor-block-list__block-draggable-inner"></div>
+					</div> )
+			}
+		</Draggable>
+	);
+};
 
 export default BlockDraggable;
