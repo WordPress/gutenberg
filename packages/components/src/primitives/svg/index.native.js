522785990733a8e0f36b7795215e873b1151df5e
Mario Zorz <mariozorz@gmail.com>
2018-09-04 13:56:36 -0300

    Modified comment to better reflect causes of className being conveted to style for RN

--- packages/components/src/primitives/svg/index.native.js
+++ packages/components/src/primitives/svg/index.native.js
@@ -14,7 +14,7 @@ export {
 };
 
 export const SVG = ( props ) => {
-	// for some reason if a `className` prop is passed it gets converted to `style` here.
+	// We're using the react-native-classname-to-style plugin, so when a `className` prop is passed it gets converted to `style` here.
 	// Given it carries a string (as it was originally className) but an object is expected for `style`,
 	// we need to check whether `style` exists and is a string, and strip in that case.
 	const safeProps = ( typeof props.style === 'string' || props.style instanceof String ) ? { ...omit( props, [ 'style', 'className' ] ) } : { ...omit( props, [ 'className' ] ) };
