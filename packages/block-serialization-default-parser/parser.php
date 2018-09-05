fe98a4aeabad54baa990235d24ff9dee0e3b061d
Dennis Snell <dennis.snell@automattic.com>
2018-09-05 16:38:43 +0100

    Add docblocks

--- packages/block-serialization-default-parser/parser.php
+++ packages/block-serialization-default-parser/parser.php
@@ -1,9 +1,51 @@
 <?php
 
+/**
+ * Class WP_Block_Parser_Block
+ *
+ * Holds the block structure in memory
+ *
+ * @since 3.8.0
+ */
 class WP_Block_Parser_Block {
+	/**
+	 * Name of block
+	 *
+	 * @example "core/paragraph"
+	 *
+	 * @since 3.8.0
+	 * @var string
+	 */
     public $blockName;
+
+	/**
+	 * Optional set of attributes from block comment delimiters
+	 *
+	 * @example null
+	 * @example array( 'columns' => 3 )
+	 *
+	 * @since 3.8.0
+	 * @var array|null
+	 */
     public $attrs;
+
+	/**
+	 * List of inner blocks (of this same class)
+	 *
+	 * @since 3.8.0
+	 * @var WP_Block_Parser_Block[]
+	 */
     public $innerBlocks;
+
+	/**
+	 * Resultant HTML from inside block comment delimieters
+	 * after removing inner blocks
+	 *
+	 * @example "...Just <!-- wp:test /--> testing..." -> "Just testing..."
+	 *
+	 * @since 3.8.0
+	 * @var string
+	 */
     public $innerHTML;
 
     function __construct( $name, $attrs, $innerBlocks, $innerHTML ) {
@@ -14,11 +56,54 @@ class WP_Block_Parser_Block {
     }
 }
 
+/**
+ * Class WP_Block_Parser_Frame
+ *
+ * Holds partial blocks in memory while parsing
+ *
+ * @internal
+ * @since 3.8.0
+ */
 class WP_Block_Parser_Frame {
+	/**
+	 * Full or partial block
+	 *
+	 * @since 3.8.0
+	 * @var WP_Block_Parser_Block
+	 */
     public $block;
+
+	/**
+	 * Byte offset into document for start of parse token
+	 *
+	 * @since 3.8.0
+	 * @var int
+	 */
     public $token_start;
+
+	/**
+	 * Byte length of entire parse token string
+	 *
+	 * @since 3.8.0
+	 * @var int
+	 */
     public $token_length;
+
+	/**
+	 * Byte offset into document for after parse token ends
+	 * (used during reconstruction of stack into parse production)
+	 *
+	 * @since 3.8.0
+	 * @var int
+	 */
     public $prev_offset;
+
+	/**
+	 * Byte offset into document where leading HTML before token starts
+	 *
+	 * @since 3.8.0
+	 * @var int
+	 */
     public $leading_html_start;
 
     function __construct( $block, $token_start, $token_length, $prev_offset = null, $leading_html_start = null ) {
@@ -30,12 +115,60 @@ class WP_Block_Parser_Frame {
     }
 }
 
+/**
+ * Class WP_Block_Parser
+ *
+ * Parses a document and constructs a list of parsed block objects
+ *
+ * @since 3.8.0
+ */
 class WP_Block_Parser {
+	/**
+	 * Input document being parsed
+	 *
+	 * @example "Pre-text\n<!-- wp:paragraph -->This is inside a block!<!-- /wp:paragraph -->"
+	 *
+	 * @since 3.8.0
+	 * @var string
+	 */
     public $document;
+
+	/**
+	 * Tracks parsing progress through document
+	 *
+	 * @since 3.8.0
+	 * @var int
+	 */
     public $offset;
+
+	/**
+	 * List of parsed blocks
+	 *
+	 * @since 3.8.0
+	 * @var WP_Block_Parser_Block[]
+	 */
     public $output;
+
+	/**
+	 * Stack of partially-parsed structures in memory during parse
+	 *
+	 * @since 3.8.0
+	 * @var WP_Block_Parser_Frame[]
+	 */
     public $stack;
 
+	/**
+	 * Parses a document and returns a list of block structures
+	 *
+	 * When encountering an invalid parse will return a best-effort
+	 * parse. In contrast to the specification parser this does not
+	 * return an error on invalid inputs.
+	 *
+	 * @since 3.8.0
+	 *
+	 * @param string $document
+	 * @return WP_Block_Parser_Block[]
+	 */
     function parse( $document ) {
         $this->document = $document;
         $this->offset   = 0;
@@ -49,6 +182,20 @@ class WP_Block_Parser {
         return $this->output;
     }
 
+	/**
+	 * Processes the next token from the input document
+	 * and returns whether to proceed eating more tokens
+	 *
+	 * This is the "next step" function that essentially
+	 * takes a token as its input and decides what to do
+	 * with that token before descending deeper into a
+	 * nested block tree or continuing along the document
+	 * or breaking out of a level of nesting.
+	 *
+	 * @internal
+	 * @since 3.8.0
+	 * @return bool
+	 */
     function proceed() {
         list( $token_type, $block_name, $attrs, $start_offset, $token_length ) = $this->next_token();
         $stack_depth = count( $this->stack );
@@ -168,6 +315,16 @@ class WP_Block_Parser {
         }
     }
 
+	/**
+	 * Scans the document from where we last left off
+	 * and finds the next valid token to parse if it exists
+	 *
+	 * Returns the type of the find: kind of find, block information, attributes
+	 *
+	 * @internal
+	 * @since 3.8.0
+	 * @return array
+	 */
     function next_token() {
         $matches = null;
 
@@ -222,6 +379,14 @@ class WP_Block_Parser {
         return array( 'block-opener', $name, $attrs, $started_at, $length );
     }
 
+	/**
+	 * Pushes a length of text from the input document
+	 * to the output list as a freeform block
+	 *
+	 * @internal
+	 * @since 3.8.0
+	 * @param null $length how many bytes of document text to output
+	 */
     function add_freeform( $length = null ) {
         $length = $length ? $length : strlen( $this->document ) - $this->offset;
 
@@ -235,6 +400,17 @@ class WP_Block_Parser {
         );
     }
 
+	/**
+	 * Given a block structure from memory pushes
+	 * a new block to the output list
+	 *
+	 * @internal
+	 * @since 3.8.0
+	 * @param WP_Block_Parser_Block $block the block to add to the output
+	 * @param int $token_start byte offset into the document where the first token for the block starts
+	 * @param int $token_length byte length of entire block from start of opening token to end of closing token
+	 * @param int|null $last_offset last byte offset into document if continuing form earlier output
+	 */
     function add_inner_block(WP_Block_Parser_Block $block, $token_start, $token_length, $last_offset = null ) {
         $parent = $this->stack[ count( $this->stack ) - 1 ];
         $parent->block->innerBlocks[] = $block;
@@ -242,6 +418,13 @@ class WP_Block_Parser {
         $parent->prev_offset = $last_offset ? $last_offset : $token_start + $token_length;
     }
 
+	/**
+	 * Pushes the top block from the parsing stack to the output list
+	 *
+	 * @internal
+	 * @since 3.8.0
+	 * @param int|null $end_offset byte offset into document for where we should stop sending text output as HTML
+	 */
     function add_block_from_stack( $end_offset = null ) {
         $stack_top = array_pop( $this->stack );
         $prev_offset = $stack_top->prev_offset;
