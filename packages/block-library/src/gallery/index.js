7f7e1a3bea26e910439d7f0713abe2b616ada058
Zebulan Stanphill <zebulanstanphill@gmail.com>
2018-09-06 00:22:52 -0500

    Change method of using icons in edit.js files

--- packages/block-library/src/gallery/index.js
+++ packages/block-library/src/gallery/index.js
@@ -66,12 +66,10 @@ const blockAttributes = {
 
 export const name = 'core/gallery';
 
-export const icon = <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><g><path d="M20 4v12H8V4h12m0-2H8L6 4v12l2 2h12l2-2V4l-2-2z" /><path d="M12 12l1 2 3-3 3 4H9z" /><path d="M2 6v14l2 2h14v-2H4V6H2z" /></g></svg>;
-
 export const settings = {
 	title: __( 'Gallery' ),
 	description: __( 'Display multiple images in an elegantly organized tiled layout.' ),
-	icon,
+	icon: <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z" /><g><path d="M20 4v12H8V4h12m0-2H8L6 4v12l2 2h12l2-2V4l-2-2z" /><path d="M12 12l1 2 3-3 3 4H9z" /><path d="M2 6v14l2 2h14v-2H4V6H2z" /></g></svg>,
 	category: 'common',
 	keywords: [ __( 'images' ), __( 'photos' ) ],
 	attributes: blockAttributes,
@@ -272,3 +270,5 @@ export const settings = {
 		},
 	],
 };
+
+export const icon = settings.icon;
