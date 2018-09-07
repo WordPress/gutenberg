e3f0c24138b010995767cb619073c47ccf2bb47d
Andrés <andres.maneiro@automattic.com>
2018-09-07 13:44:25 +0200

    Add data API

--- edit-post/components/header/publish-panel-toggle/index.js
+++ edit-post/components/header/publish-panel-toggle/index.js
@@ -3,18 +3,35 @@
  */
 import { __ } from '@wordpress/i18n';
 import { MenuItem } from '@wordpress/components';
+import { compose } from '@wordpress/compose';
+import { withSelect, withDispatch } from '@wordpress/data';
 
-const PublishPanelToggle = function( { onToggle, isActive } ) {
+const PublishPanelToggle = function( { onToggle, isEnabled } ) {
 	return (
 		<MenuItem
-			icon={ isActive && 'yes' }
-			isSelected={ isActive }
+			icon={ isEnabled && 'yes' }
+			isSelected={ isEnabled }
 			role="menuitemcheckbox"
 			onClick={ onToggle }
 		>
 			{ __( 'PrePublish Panel' ) }
 		</MenuItem>
 	);
-}
+};
 
-export default PublishPanelToggle;
+export default compose( [
+	withSelect( ( select ) => ( {
+		isEnabled: select( 'core/nux' ).isPrePublishPanelEnabled(),
+	} ) ),
+	withDispatch( ( dispatch, ownProps ) => ( {
+		onToggle() {
+			const { disablePrePublishPanel, enablePrePublishPanel } = dispatch( 'core/nux' );
+			if ( ownProps.isActive ) {
+				disablePrePublishPanel();
+			} else {
+				enablePrePublishPanel();
+			}
+			ownProps.onToggle();
+		},
+	} ) ),
+] )( PublishPanelToggle );
