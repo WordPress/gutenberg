8480ef044a0848622c563b83b3c6a4cb84560de9
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-09-08 15:58:56 +0300

    App runs fine, started from within a GB subfolder

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,25 +1,39 @@
 /** @format */
-const blacklist = require( 'metro' ).createBlacklist;
+const path = require( 'path' );
 
 const enm = require( 'node-libs-react-native' );
 enm.fs = __dirname + '/__mocks__/nodejsMock.js';
 enm.net = __dirname + '/__mocks__/nodejsMock.js';
 enm.canvas = __dirname + '/__mocks__/nodejsMock.js';
 
+const { lstatSync, readdirSync } = require( 'fs' );
+const getDirectoryNames = dir =>
+	readdirSync( dir ).filter( name => lstatSync( path.resolve( dir, name ) ).isDirectory() );
+
+const wppackagenames = getDirectoryNames( path.resolve( __dirname, '../packages/' ) );
+
+const mapper = function( accu, v ) {
+	accu[ '@wordpress/' + v ] = path.resolve( __dirname, '../packages/' + v );
+	return accu;
+};
+
+const wppackages = wppackagenames.reduce( mapper, {} );
+
 module.exports = {
-	extraNodeModules: enm,
-	getBlacklistRE: function() {
-		// Blacklist the GB packages we want to consume from NPM (online) directly.
-		// On the other hand, GB packages that are loaded from the source tree directly
-		// are automagically resolved by Metro so, there is no list of them anywhere.
-		return blacklist( [
-			/gutenberg\/packages\/(autop|compose|deprecated|hooks|i18n|is-shallow-equal)\/.*/,
-		] );
+	extraNodeModules: Object.assign( enm, wppackages ),
+	getProjectRoots() {
+		const roots = [ __dirname, path.resolve( __dirname, '../node_modules' ) ].concat(
+			Object.values( wppackages )
+		);
+		return roots;
 	},
 	getTransformModulePath() {
-		return require.resolve( './sass-transformer.js' );
+		return require.resolve( './sass-transformer-inside-gb.js' );
 	},
 	getSourceExts() {
 		return [ 'js', 'json', 'scss', 'sass' ];
+	},
+	getProvidesModuleNodeModules() {
+		return [ 'react-native-svg', 'react-native' ];
 	},
 };
