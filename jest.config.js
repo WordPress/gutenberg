67dc8efeae008ec536765ecc693c1fbeaf949d00
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-09-11 02:44:20 +0300

    Add missing GB packages to jest

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ module.exports = {
 		'node',
 	],
 	moduleNameMapper: {
-		'@wordpress\\/(blocks|data|element|deprecated|editor|redux-routine|block-library|components|keycodes|url|a11y)$':
+		'@wordpress\\/(blocks|data|element|deprecated|editor|redux-routine|block-library|components|keycodes|url|a11y|viewport|core-data|api-fetch|nux|block-serialization-default-parser)$':
 			'<rootDir>/gutenberg/packages/$1/src/index',
 
 		// Mock the CSS modules. See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
--- jest_gb.config.js
+++ jest_gb.config.js
@@ -6,7 +6,7 @@ const main = require( './jest.config.js' );
 module.exports = {
 	...main,
 	moduleNameMapper: {
-		'@wordpress\\/(blocks|data|element|deprecated|editor|redux-routine|block-library|components|keycodes|url|a11y)$':
+		'@wordpress\\/(blocks|data|element|deprecated|editor|redux-routine|block-library|components|keycodes|url|a11y|viewport|core-data|api-fetch|nux|block-serialization-default-parser)$':
 			'<rootDir>/../packages/$1/build/index',
 
 		// Mock the CSS modules. See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
