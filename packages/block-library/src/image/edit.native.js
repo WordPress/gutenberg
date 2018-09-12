a34b2c2ed25de125fc3fe282008a64babeda2845
etoledom <etoledom@icloud.com>
2018-09-12 13:31:34 -0300

    image/edit.native.js: Fixed lint issues.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -2,9 +2,9 @@
 
 import { View, Text } from 'react-native';
 
-export default function ImageEdit( { attributes, setAttributes } ) {
+export default function ImageEdit() {
 	return (
-		<View style={{padding: 12, backgroundColor: '#f2f2f2'}}>
-            <Text style={{textAlign: 'center'}}>Image</Text>
+		<View style={ { padding: 12, backgroundColor: '#f2f2f2' } }>
+			<Text style={ { textAlign: 'center' } }>Image</Text>
 		</View> );
 }
