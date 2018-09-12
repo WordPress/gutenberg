3adca2248fc8b5e5f32fd6e8f520dbbb18948262
etoledom <etoledom@icloud.com>
2018-09-12 12:33:30 -0300

    Adding a simple representation of an image block to be shown on mobile.

---
+++ packages/block-library/src/image/edit.native.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import { View, Text } from 'react-native';
+
+export default function ImageEdit( { attributes, setAttributes } ) {
+	return (
+		<View style={{padding: 12, backgroundColor: '#f2f2f2'}}>
+            <Text style={{textAlign: 'center'}}>Image</Text>
+		</View> );
+}
