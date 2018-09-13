2bc95f9b68d829431547c5ca94086d6beb6948be
etoledom <etoledom@icloud.com>
2018-09-13 14:48:10 -0300

    Mobile image block: Recognize when it has content.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -3,9 +3,19 @@
  */
 import { View, Text } from 'react-native';
 
-export default function ImageEdit() {
-	return (
+export default function ImageEdit( { attributes } ) {
+	const { url } = attributes;
+
+	const noImage = (
 		<View style={ { padding: 12, backgroundColor: '#f2f2f2' } }>
-			<Text style={ { textAlign: 'center' } }>Image</Text>
+			<Text style={ { textAlign: 'center' } }>No image selected </Text>
+		</View>
+	);
+
+	const image = (
+		<View style={ { padding: 12, backgroundColor: '#f2f2f2' } }>
+			<Text style={ { textAlign: 'center' } }>{ url }</Text>
 		</View> );
+
+	return ! url ? noImage : image;
 }
