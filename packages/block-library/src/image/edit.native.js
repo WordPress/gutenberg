3c559e96cf8a31b22a20c0f7fc75adf7b5d878f1
etoledom <etoledom@icloud.com>
2018-09-13 08:48:13 -0600

    RN-Mobile: Image block placeholder (#9850)
    * Level needs to be save block attributes or parse will fail.
    
    * Adding a simple representation of an image block to be shown on mobile.
    
    * image/edit.native.js: Fixed lint issues.
    
    * image/edit.native.js: Added import section title

---
+++ packages/block-library/src/image/edit.native.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { View, Text } from 'react-native';
+
+export default function ImageEdit() {
+	return (
+		<View style={ { padding: 12, backgroundColor: '#f2f2f2' } }>
+			<Text style={ { textAlign: 'center' } }>Image</Text>
+		</View> );
+}
