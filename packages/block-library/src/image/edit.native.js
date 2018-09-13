8be826513fded40403427baf451e28f008e90dd1
etoledom <etoledom@icloud.com>
2018-09-13 18:53:30 -0300

    RNMobile image block: Styled empty state.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -1,26 +1,39 @@
 /**
  * External dependencies
  */
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 
 export default function ImageEdit( { attributes } ) {
 	const { url, width, height } = attributes;
 
-	const noImage = (
-		<View style={ { padding: 12, backgroundColor: '#f2f2f2' } }>
-			<Text style={ { textAlign: 'center' ,fontWeight: "bold"} }>🖼 Image</Text>
-			<Text style={ { textAlign: 'center' } }>Upload a new image or select a file from your library.</Text>
-		</View>
-	);
+	const onUploadPress = ()=> {
 
-	const image = (
+	}
+
+	const onMediaLibraryPress = ()=> {
+		
+	}
+
+	if (! url) {
+		return (
+			<View style={ { alignItems: 'center', padding: 12, backgroundColor: '#f2f2f2' } }>
+				<Text style={ { textAlign: 'center', fontWeight: "bold", paddingBottom: 10} }>Image</Text>
+				<Text style={ { textAlign: 'center' } }>Upload a new image or select a file from your library.</Text>
+				<View style={{flexDirection: 'row', alignItems: 'center'}}>
+					<Button title="Upload" onPress={onUploadPress}/>
+					<Button title="Media Library" onPress={onMediaLibraryPress}/>
+				</View>
+			</View>
+		);
+	}
+
+	return (
 		<View style={ { flex: 1 } }>
-		<Image
-			style={{width:'100%', height:200}}
-			resizeMethod = 'scale'
-          source={{uri: url}}
-        />
-		</View> );
-
-	return ! url ? noImage : image;
+			<Image
+				style={{width:'100%', height:200}}
+				resizeMethod = 'scale'
+				source={{uri: url}}
+			/>
+		</View> 
+	);
 }
