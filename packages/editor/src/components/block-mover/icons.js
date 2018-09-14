f25364195314965be2db41b125d574c7b0bff142
Andrés <andres.maneiro@automattic.com>
2018-09-14 10:31:49 +0200

    Use AccesibleSVG component

--- packages/editor/src/components/block-mover/icons.js
+++ packages/editor/src/components/block-mover/icons.js
@@ -16,9 +16,9 @@ export const downArrow = (
 );
 
 export const dragHandle = (
-	<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" aria-hidden role="img" focusable="false">
+	<AccessibleSVG width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
 		<path d="M13,8c0.6,0,1-0.4,1-1s-0.4-1-1-1s-1,0.4-1,1S12.4,8,13,8z M5,6C4.4,6,4,6.4,4,7s0.4,1,1,1s1-0.4,1-1S5.6,6,5,6z M5,10
 			c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S5.6,10,5,10z M13,10c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S13.6,10,13,10z M9,6
 			C8.4,6,8,6.4,8,7s0.4,1,1,1s1-0.4,1-1S9.6,6,9,6z M9,10c-0.6,0-1,0.4-1,1s0.4,1,1,1s1-0.4,1-1S9.6,10,9,10z" />
-	</svg>
+	</AccessibleSVG>
 );
