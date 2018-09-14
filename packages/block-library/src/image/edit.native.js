0b7ba25de64c14de8d989b0fa5486f9c6b062a6a
etoledom <etoledom@icloud.com>
2018-09-14 18:21:28 -0300

    RNMobile Image Block: Transformed component to class to hold internal state.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -7,38 +7,45 @@ import { View, Image } from 'react-native';
  * Internal dependencies
  */
 import { MediaPlaceholder } from '@wordpress/editor';
+import { render } from '@wordpress/element/src';
+import { Component } from '@wordpress/element';
 
-export default function ImageEdit( { attributes } ) {
-	const { url } = attributes;
+class ImageEdit extends Component {
 
-	const onUploadPress = () => {
+	onUploadPress = () => {
 		// This method should present an image picker from
 		// the device.
 		//TODO: Implement upload image method.
 	};
 
-	const onMediaLibraryPress = () => {
+	onMediaLibraryPress = () => {
 		// This method should present an image picker from
 		// the WordPress media library.
 		//TODO: Implement media library method.
 	};
 
-	if ( ! url ) {
+	render() {
+		const { url, width, height } = this.props.attributes;
+
+		if ( ! url ) {
+			return (
+				<MediaPlaceholder
+					onUploadPress={ onUploadPress }
+					onMediaLibraryPress={ onMediaLibraryPress }
+				/>
+			);
+		}
+
 		return (
-			<MediaPlaceholder
-				onUploadPress={ onUploadPress }
-				onMediaLibraryPress={ onMediaLibraryPress }
-			/>
+			<View style={ { flex: 1 } }>
+				<Image
+					style={ { width: '100%', height: 200 } }
+					resizeMethod="scale"
+					source={ { uri: url } }
+				/>
+			</View>
 		);
 	}
-
-	return (
-		<View style={ { flex: 1 } }>
-			<Image
-				style={ { width: '100%', height: 200 } }
-				resizeMethod="scale"
-				source={ { uri: url } }
-			/>
-		</View>
-	);
 }
+
+export default ImageEdit;
