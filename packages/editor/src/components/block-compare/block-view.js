da632418e063d97d5ea7b81ba36d8152d6feee6f
John Godley <johng75@gmail.com>
2018-09-14 09:18:52 +0100

    Fix camelCase and cross-component class name
    Fix typo in tabIndex and use a class name local to the component

--- packages/editor/src/components/block-compare/block-view.js
+++ packages/editor/src/components/block-compare/block-view.js
@@ -7,7 +7,7 @@ const BlockView = ( { title, rawContent, renderedContent, action, actionText, cl
 	return (
 		<div className={ className }>
 			<div className="editor-block-compare__content">
-				<h1 className="components-modal__header-heading">{ title }</h1>
+				<h1 className="editor-block-compare__heading">{ title }</h1>
 
 				<div className="editor-block-compare__html">
 					{ rawContent }
@@ -19,7 +19,7 @@ const BlockView = ( { title, rawContent, renderedContent, action, actionText, cl
 			</div>
 
 			<div className="editor-block-compare__action">
-				<Button isLarge tabindex="0" onClick={ action }>{ actionText }</Button>
+				<Button isLarge tabIndex="0" onClick={ action }>{ actionText }</Button>
 			</div>
 		</div>
 	);
