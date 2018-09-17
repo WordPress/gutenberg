9904297e98c9267d294413be33e36cee1ac9fbba
Nicola Heald <nicola@notnowlewis.com>
2018-09-17 13:47:57 +0100

    Remove attempt to fix cover image

--- lib/kses.php
+++ lib/kses.php
@@ -7,19 +7,12 @@
  * @return array (Maybe) modified allowed HTML.
  */
 function gutenberg_kses_allowedtags( $tags ) {
-	$tags['a']['download'] = true;
-	$tags['img']['data-link'] = true;
-	$tags['img']['data-id']   = true;
-	$tags['div']['style'] = true;
+	$tags['a']['download']      = true;
+	$tags['img']['data-link']   = true;
+	$tags['img']['data-id']     = true;
+	$tags['div']['style']       = true;
 	$tags['div']['aria-hidden'] = true;
 	return $tags;
 }
 
 add_filter( 'wp_kses_allowed_html', 'gutenberg_kses_allowedtags', 10, 2 );
-
-function gutenberg_kses_allowed_style_css( $attributes ) {
-	$attributes[] = 'background-image';
-	return $attributes;
-}
-
-add_filter( 'safe_style_css', 'gutenberg_kses_allowed_style_css', 10, 1 );
