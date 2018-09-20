251dd209e505749453e4a92fbec9296734aff344
Joen Asmussen <asmussen@gmail.com>
2018-09-20 11:06:43 +0200

    Add "Exit Code Editor" toolbar.
    This PR intends to fix #9445. It adds a toolbar to the code editor with a label showing what you are doing, as well as an "Exit Code Editor" button that takes you back to the visual editor.
    
    I hope the space between this label and the close button can be used, in the future, to add a slot fill so a plugin could add quicktags here.

--- edit-post/components/text-editor/index.js
+++ edit-post/components/text-editor/index.js
@@ -2,6 +2,9 @@
  * WordPress dependencies
  */
 import { PostTextEditor, PostTitle } from '@wordpress/editor';
+import { IconButton } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { displayShortcut } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
@@ -11,6 +14,16 @@ import './style.scss';
 function TextEditor() {
 	return (
 		<div className="edit-post-text-editor">
+			<div className="edit-post-text-editor__toolbar">
+				<h2>{ __( 'Editing Code' ) }</h2>
+				<IconButton
+					//onClick={ onSave }
+					icon="no-alt"
+					shortcut={ displayShortcut.secondary( 'm' ) }
+				>
+					{ __( 'Exit Code Editor' ) }
+				</IconButton>
+			</div>
 			<div className="edit-post-text-editor__body">
 				<PostTitle />
 				<PostTextEditor />
