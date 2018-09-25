691a38fba2132e6a0869245ce25ba95cdb0692ab
SergioEstevao <sergioestevao@gmail.com>
2018-09-25 19:00:45 +0100

    Update example project to use RN 0.57

--- example/rn-cli.config.js
+++ example/rn-cli.config.js
@@ -1,13 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  getProjectRoots() {
-    return [__dirname, path.resolve(__dirname, '..')];
-  },
-  getProvidesModuleNodeModules() {
-    return [
-      'react-native',
-      'react',
-    ];
+  watchFolders: [path.resolve(__dirname, '..')],
+  resolver: {
+	  providesModuleNodeModules:[ 'react-native', 'react',],	  
   },
 };
