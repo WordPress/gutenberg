fa01cb214791c2dda4439d3402c26854d400a88a
SergioEstevao <sergioestevao@gmail.com>
2018-09-25 10:01:11 +0100

    Remove custom transform configuration for jest.

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 /** @flow
  * @format */
 
-const defaultPlatform = 'android';
-const rnPlatform = process.env.TEST_RN_PLATFORM || defaultPlatform;
-if ( process.env.TEST_RN_PLATFORM ) {
-	// eslint-disable-next-line no-console
-	console.log( 'Setting RN platform to: ' + process.env.TEST_RN_PLATFORM );
-} else {
-	// eslint-disable-next-line no-console
-	console.log( 'Setting RN platform to: default (' + defaultPlatform + ')' );
-}
-
 module.exports = {
 	verbose: true,
 	preset: 'react-native',
@@ -27,5 +17,4 @@ module.exports = {
 		// Mock the CSS modules. See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
 		'\\.(scss)$': '<rootDir>/__mocks__/styleMock.js',
 	},
-	transform: { '^.+\\.js$': './node_modules/react-native/jest/preprocessor.js' },
 };
