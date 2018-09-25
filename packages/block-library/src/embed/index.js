302ad62a61a961e4be03509184d16bf5258a7604
Nicola Heald <nicola@notnowlewis.com>
2018-09-25 14:17:18 +0100

    Toggle responsiveness for embed blocks

--- packages/block-library/src/embed/index.js
+++ packages/block-library/src/embed/index.js
@@ -10,10 +10,19 @@ import classnames from 'classnames/dedupe';
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
-import { Component, renderToString } from '@wordpress/element';
-import { Button, Placeholder, Spinner, SandBox, IconButton, Toolbar } from '@wordpress/components';
+import { Component, renderToString, Fragment } from '@wordpress/element';
+import {
+	Button,
+	Placeholder,
+	Spinner,
+	SandBox,
+	IconButton,
+	Toolbar,
+	PanelBody,
+	ToggleControl,
+} from '@wordpress/components';
 import { createBlock } from '@wordpress/blocks';
-import { RichText, BlockControls, BlockIcon } from '@wordpress/editor';
+import { RichText, BlockControls, BlockIcon, InspectorControls } from '@wordpress/editor';
 import { withSelect } from '@wordpress/data';
 
 // These embeds do not work in sandboxes
@@ -44,6 +53,8 @@ export function getEmbedEdit( title, icon ) {
 			this.maybeSwitchBlock = this.maybeSwitchBlock.bind( this );
 			this.setAttributesFromPreview = this.setAttributesFromPreview.bind( this );
 			this.maybeSetAspectRatioClassName = this.maybeSetAspectRatioClassName.bind( this );
+			this.getResponsiveHelp = this.getResponsiveHelp.bind( this );
+			this.toggleResponsive = this.toggleResponsive.bind( this );
 
 			this.state = {
 				editingURL: false,
@@ -138,21 +149,18 @@ export function getEmbedEdit( title, icon ) {
 		}
 
 		/**
-		 * Sets the appropriate CSS class to enforce an aspect ratio when the embed is resized
+		 * Gets the appropriate CSS class names to enforce an aspect ratio when the embed is resized
 		 * if the HTML has an iframe with width and height set.
 		 *
 		 * @param {string} html The preview HTML that possibly contains an iframe with width and height set.
+		 * @return {Object} Object with classnames set to true, for use with `classnames`.
 		 */
-		maybeSetAspectRatioClassName( html ) {
+		getAspectRatioClassNames( html ) {
 			const previewDocument = document.implementation.createHTMLDocument( '' );
 			previewDocument.body.innerHTML = html;
 			const iframe = previewDocument.body.querySelector( 'iframe' );
 
-			if ( ! iframe ) {
-				return;
-			}
-
-			if ( iframe.height && iframe.width ) {
+			if ( iframe && iframe.height && iframe.width ) {
 				const aspectRatio = ( iframe.width / iframe.height ).toFixed( 2 );
 				let aspectRatioClassName;
 
@@ -183,10 +191,32 @@ export function getEmbedEdit( title, icon ) {
 				}
 
 				if ( aspectRatioClassName ) {
-					const className = classnames( this.props.attributes.className, 'wp-has-aspect-ratio', aspectRatioClassName );
-					this.props.setAttributes( { className } );
+					return {
+						[ aspectRatioClassName ]: true,
+						'wp-has-aspect-ratio': true,
+					};
 				}
 			}
+
+			return {};
+		}
+
+		/**
+		 * Maybe sets the appropriate CSS class names to enforce an aspect ratio when the embed
+		 * is resized if the HTML has an iframe with width and height set.
+		 *
+		 * @param {string} html The preview HTML that possibly contains an iframe with width and height set.
+		 */
+		maybeSetAspectRatioClassName( html ) {
+			const { allowResponsive } = this.props.attributes;
+			if ( ! allowResponsive ) {
+				return;
+			}
+			const className = classnames(
+				this.props.attributes.className,
+				this.getAspectRatioClassNames( html )
+			);
+			this.props.setAttributes( { className } );
 		}
 
 		/***
@@ -217,21 +247,57 @@ export function getEmbedEdit( title, icon ) {
 			this.setState( { editingURL: true } );
 		}
 
+		getResponsiveHelp( checked ) {
+			return checked ? __( 'Videos are responsive.' ) : __( 'Toggle to make videos responsive.' );
+		}
+
+		toggleResponsive() {
+			const { allowResponsive, className } = this.props.attributes;
+			const { html } = this.props.preview;
+			const responsiveClassNames = this.getAspectRatioClassNames( html );
+
+			if ( allowResponsive ) {
+				// This will remove the responsive classes from the className attribute.
+				for ( const property in responsiveClassNames ) {
+					responsiveClassNames[ property ] = false;
+				}
+			}
+
+			this.props.setAttributes(
+				{
+					allowResponsive: ! allowResponsive,
+					className: classnames( className, responsiveClassNames ),
+				}
+			);
+		}
+
 		render() {
 			const { url, editingURL } = this.state;
-			const { caption, type } = this.props.attributes;
+			const { caption, type, allowResponsive } = this.props.attributes;
 			const { fetching, setAttributes, isSelected, className, preview, previewIsFallback } = this.props;
 			const controls = (
-				<BlockControls>
-					<Toolbar>
-						{ ( preview && ! previewIsFallback && <IconButton
-							className="components-toolbar__control"
-							label={ __( 'Edit URL' ) }
-							icon="edit"
-							onClick={ this.switchBackToURLInput }
-						/> ) }
-					</Toolbar>
-				</BlockControls>
+				<Fragment>
+					<BlockControls>
+						<Toolbar>
+							{ ( preview && ! previewIsFallback && <IconButton
+								className="components-toolbar__control"
+								label={ __( 'Edit URL' ) }
+								icon="edit"
+								onClick={ this.switchBackToURLInput }
+							/> ) }
+						</Toolbar>
+					</BlockControls>
+					<InspectorControls>
+						<PanelBody title={ __( 'Media Settings' ) } className="blocks-responsive">
+							<ToggleControl
+								label={ __( 'Responsive' ) }
+								checked={ allowResponsive }
+								help={ this.getResponsiveHelp }
+								onChange={ this.toggleResponsive }
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
 			);
 
 			if ( fetching ) {
@@ -330,6 +396,10 @@ const embedAttributes = {
 	},
 	providerNameSlug: {
 		type: 'string',
+	},
+	allowResponsive: {
+		type: 'boolean',
+		default: true,
 	},
 };
 
