85711af2dfae0bc5d81686a64ece8191ce9436e4
SergioEstevao <sergioestevao@gmail.com>
2018-09-25 10:47:20 +0100

    Reinstate the configuration for the platform.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 /** @flow
  * @format */
 
+const defaultPlatform = 'android';
+const rnPlatform = process.env.TEST_RN_PLATFORM || defaultPlatform;
+if ( process.env.TEST_RN_PLATFORM ) {
+	// eslint-disable-next-line no-console
+	console.log( 'Setting RN platform to: ' + process.env.TEST_RN_PLATFORM );
+} else {
+	// eslint-disable-next-line no-console
+	console.log( 'Setting RN platform to: default (' + defaultPlatform + ')' );
+}
+
 module.exports = {
 	verbose: true,
 	preset: 'react-native',
@@ -16,5 +26,17 @@ module.exports = {
 	moduleNameMapper: {
 		// Mock the CSS modules. See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
 		'\\.(scss)$': '<rootDir>/__mocks__/styleMock.js',
+	},
+	haste: {
+		defaultPlatform: rnPlatform,
+		platforms: [
+			'android',
+			'ios',
+			'native',
+		],
+		hasteImplModulePath: '<rootDir>/node_modules/react-native/jest/hasteImpl.js',
+		providesModuleNodeModules: [
+			'react-native',
+		],
 	},
 };
