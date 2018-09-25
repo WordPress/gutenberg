49db6f933db0dcde93d4ad2860eb84de62f7e7f7
SergioEstevao <sergioestevao@gmail.com>
2018-09-25 15:07:19 +0100

    Reinstate comments.

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,6 +1,8 @@
 /** @format */
 const path = require( 'path' );
 const blacklist = require( 'metro-config/src/defaults/blacklist' );
+// Blacklist the nested GB filetree so modules are not resolved in duplicates,
+//  both in the nested directory and the parent directory.
 const blacklistElements = blacklist( [ new RegExp( path.basename( __dirname ) + '/gutenberg/gutenberg-mobile/.*' ) ] );
 const enm = require( './extra-node-modules.config.js' );
 
