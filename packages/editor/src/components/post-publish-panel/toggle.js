02f09aeabd3749c1a5e65e44b8ef94074b9e3808
imath <math.viet@gmail.com>
2018-10-09 16:04:17 +0200

    Use a "Submit for review..." label for the major action of the post publish toggle

--- packages/editor/src/components/post-publish-panel/toggle.js
+++ packages/editor/src/components/post-publish-panel/toggle.js
@@ -41,6 +41,8 @@ export function PostPublishPanelToggle( {
 		return <PostPublishButton forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />;
 	}
 
+	const publishButtonLabel = hasPublishAction ? __( 'Publish…' ) : __( 'Submit for review…' );
+
 	return (
 		<Button
 			className="editor-post-publish-panel__toggle"
@@ -50,7 +52,7 @@ export function PostPublishPanelToggle( {
 			disabled={ ! isButtonEnabled }
 			isBusy={ isSaving && isPublished }
 		>
-			{ isBeingScheduled ? __( 'Schedule…' ) : __( 'Publish…' ) }
+			{ isBeingScheduled ? __( 'Schedule…' ) : publishButtonLabel }
 			<DotTip id="core/editor.publish">
 				{ __( 'Finished writing? That’s great, let’s get this published right now. Just click “Publish” and you’re good to go.' ) }
 			</DotTip>
