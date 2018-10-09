cc5bf5c150268b4a48ff9f68a41be5280a894253
Riad Benguella <benguella@gmail.com>
2018-10-09 09:12:45 +0100

    Move the HTML block into the blocks library package (#10396)(#10397)
    * Move the HTML block into the blocks library package
    
    * Move the Classic block to the packages
    
    * Move the Classic block to the packages

--- packages/block-library/src/classic/test/index.js
+++ packages/block-library/src/classic/test/index.js
@@ -2,9 +2,9 @@
  * Internal dependencies
  */
 import { name, settings } from '../';
-import { blockEditRender } from '../../../packages/block-library/src/test/helpers';
+import { blockEditRender } from '../../test/helpers';
 
-describe( 'core/html', () => {
+describe( 'core/freeform', () => {
 	test( 'block edit matches snapshot', () => {
 		const wrapper = blockEditRender( name, settings );
 
