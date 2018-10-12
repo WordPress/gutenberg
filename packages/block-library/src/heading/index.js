b255cd661c97c48ac7f106b6c03bcaa46218b511
mzorz <mariozorz@gmail.com>
2018-10-12 09:42:45 -0300

    React Native - SVG icons for Inserter (#9294)
    * exports SVG <G> tag and adds mobile compatible SVG icons for code, heading, more and paragraph blocks
    
    * adds simple mechanism to obtain viewport from viewBox so height and width do not have to be indicated explicitely
    
    * Update use of content size change event.
    
    * fixed lint issues
    
    * removed unneeded aliases
    
    * using omit from loadsh to exclude className from props when passing down to Svg component
    
    * moved imports to their corresponding section
    
    * - Force update the RichText component when a toolbar button is pressed
    - Fix an issue where eventCount was not correctly propogated to AztecText resulting in a re-rendering of the component from scratch
    
    * stripping style if not an object in svg native implementation
    
    * added empty native implementation of viewport
    
    * fixed lint issues
    
    * Modified comment to better reflect causes of className being conveted to style for RN
    
    * building an object out of the passed style/className concatenated string on svgnative implementation
    
    * fix array level indirection when looking into styles object
    
    * enforcing usage of SVG and Path primitives in image icon field for React Native compatibility

--- packages/block-library/src/heading/index.js
+++ packages/block-library/src/heading/index.js
@@ -14,6 +14,10 @@ import {
 	getBlockType,
 } from '@wordpress/blocks';
 import { RichText } from '@wordpress/editor';
+import {
+	Path,
+	SVG,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -60,7 +64,7 @@ export const settings = {
 
 	description: __( 'Introduce topics and help visitors (and search engines!) understand how your content is organized.' ),
 
-	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 4v3h5.5v12h3V7H19V4z" /><path fill="none" d="M0 0h24v24H0V0z" /></svg>,
+	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M5 4v3h5.5v12h3V7H19V4z" /><Path fill="none" d="M0 0h24v24H0V0z" /></SVG>,
 
 	category: 'common',
 
--- packages/components/src/primitives/svg/index.native.js
+++ packages/components/src/primitives/svg/index.native.js
@@ -4,14 +4,20 @@
 import { omit } from 'lodash';
 import { Svg } from 'react-native-svg';
 
-export const SVG = ( props ) => {
-	const appliedProps = omit( props, 'className' );
-
-	return <Svg { ...appliedProps } />;
-};
-
 export {
 	G,
 	Path,
 	Polygon,
 } from 'react-native-svg';
+
+export const SVG = ( props ) => {
+	const appliedProps = omit( props, 'className' );
+
+	return (
+		<Svg
+			height="100%"
+			width="100%"
+			{ ...appliedProps }
+		/>
+	);
+};
