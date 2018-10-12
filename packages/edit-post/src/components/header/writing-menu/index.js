301c02be4e0b9e4c2de1051a8b9c0ace326c2721
rachelmcr <rachel@automattic.com>
2018-10-12 10:51:46 +0100

    Add translator comment for 'View' menu label

--- packages/edit-post/src/components/header/writing-menu/index.js
+++ packages/edit-post/src/components/header/writing-menu/index.js
@@ -13,6 +13,7 @@ import FeatureToggle from '../feature-toggle';
 function WritingMenu( { onClose } ) {
 	return (
 		<MenuGroup
+			/* translators: Translate as a noun (label for writing menu) */
 			label={ __( 'View' ) }
 		>
 			<FeatureToggle feature="fixedToolbar" label={ __( 'Unified Toolbar' ) } onToggle={ onClose } />
