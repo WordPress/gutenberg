650ce0010f6084eb82476d1f9bbeab3d250dd110
Danilo Ercoli <ercoli@gmail.com>
2018-10-12 14:40:04 +0200

    Add onSplit method to Heading block for mobile, and wire to insertBlockAfter if defined in props.

--- packages/block-library/src/heading/edit.native.js
+++ packages/block-library/src/heading/edit.native.js
@@ -14,7 +14,7 @@ import { View } from 'react-native';
 import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
 import { RichText } from '@wordpress/editor';
-import { parse } from '@wordpress/blocks';
+import { parse, createBlock } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -24,6 +24,25 @@ import './editor.scss';
 const minHeight = 50;
 
 class HeadingEdit extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.splitBlock = this.splitBlock.bind( this );
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	splitBlock( htmlText, start, end ) {
+		const {
+			insertBlocksAfter,
+		} = this.props;
+
+		if ( insertBlocksAfter ) {
+			const blocks = [];
+			blocks.push( createBlock( 'core/paragraph', { content: 'Test' } ) );
+			insertBlocksAfter( blocks );
+		}
+	}
+
 	render() {
 		const {
 			attributes,
@@ -55,6 +74,7 @@ class HeadingEdit extends Component {
 							content: newParaBlock.attributes.content,
 						} );
 					} }
+					onSplit={ this.splitBlock }
 					onContentSizeChange={ ( event ) => {
 						setAttributes( { aztecHeight: event.aztecHeight } );
 					} }
