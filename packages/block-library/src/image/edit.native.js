e62a66dbf00e683a4b6fa6eb8ca201c560d3006d
etoledom <etoledom@icloud.com>
2018-10-15 10:20:09 -0300

    RNMobile Image Block: Adding image caption.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -1,15 +1,16 @@
 /**
  * External dependencies
  */
-import { View, Image } from 'react-native';
+import { View, Image, Text, TextInput } from 'react-native';
 
 /**
  * Internal dependencies
  */
 import { MediaPlaceholder } from '@wordpress/editor';
 
-export default function ImageEdit( { attributes } ) {
-	const { url } = attributes;
+export default function ImageEdit( props ) {
+	const { attributes, isSelected, setAttributes } = props;
+	const { url, caption } = attributes;
 
 	const onUploadPress = () => {
 		// This method should present an image picker from
@@ -39,6 +40,44 @@ export default function ImageEdit( { attributes } ) {
 				resizeMethod="scale"
 				source={ { uri: url } }
 			/>
+			{ CaptionTextInput( setAttributes, caption, isSelected ) }
 		</View>
 	);
+}
+
+const CaptionTextInput = ( setAttributes, caption, isSelected ) => {
+	const captionString = [] + caption; // caption can be a string or an array.
+
+	if ( ! captionString.length && ! isSelected ) {
+		return null;
+	}
+
+	const textContainer = ( children ) => {
+		return (
+			<View style={ { padding: 12, flex: 1 } }>
+				{ children() }
+			</View>
+		);
+	};
+
+	const textInput = () => {
+		return (
+			<TextInput
+				style={ { textAlign: 'center' } }
+				underlineColorAndroid="transparent"
+				value={ captionString }
+				placeholder={ 'Write caption…' }
+				onChangeText={ ( newCaption ) => setAttributes( { caption: newCaption } ) }
+			/>
+		);
+	};
+
+	const text = () => {
+		return ( <Text style={ { textAlign: 'center' } }>{ captionString }</Text> );
+	};
+
+	if ( isSelected ) {
+		return textContainer( textInput );
+	}
+	return textContainer( text );
 }
