3fd11fe6202efc5da20b696b74a15742f0582479
etoledom <etoledom@icloud.com>
2018-10-15 13:32:11 -0300

    HTML Editor: Removed unnecesary check to update component on iOS.
    Now it will never update if it's on iOS.

--- src/components/html-text-input.js
+++ src/components/html-text-input.js
@@ -40,13 +40,7 @@ export default class HTMLInputView extends React.Component<PropsType, StateType>
 	}
 
 	shouldComponentUpdate() {
-		if ( this.isIOS ) {
-			// iOS TextInput gets jumpy if it's updated on every key stroke.
-			// The first render will always be empty, so:
-			// If the previous state was empty, we let update the component to show the next state.
-			return this.state.html === '';
-		}
-		return true;
+		return !this.isIOS;
 	}
 
 	serializeBlocksToHtml() {
