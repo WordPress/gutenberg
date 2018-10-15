2a2467e6b3598b893aa85d30b27b3e469e62c1d9
Miguel Fonseca <miguelcsf@gmail.com>
2018-10-15 18:32:56 +0100

    Don't use dangerouslySetInnerHTML with i18n string

--- packages/block-library/src/missing/index.js
+++ packages/block-library/src/missing/index.js
@@ -17,7 +17,7 @@ function MissingBlockWarning( { attributes, convertToHTML } ) {
 	let messageHTML;
 	if ( hasContent && hasHTMLBlock ) {
 		messageHTML = sprintf(
-			__( 'Your site doesn\'t include support for the <code>%s</code> block. You can leave this block intact, convert its content to a Custom HTML block, or remove it entirely.' ),
+			__( 'Your site doesn\'t include support for the "%s" block. You can leave this block intact, convert its content to a Custom HTML block, or remove it entirely.' ),
 			originalName
 		);
 		actions.push(
@@ -27,7 +27,7 @@ function MissingBlockWarning( { attributes, convertToHTML } ) {
 		);
 	} else {
 		messageHTML = sprintf(
-			__( 'Your site doesn\'t include support for the <code>%s</code> block. You can leave this block intact or remove it entirely.' ),
+			__( 'Your site doesn\'t include support for the "%s" block. You can leave this block intact or remove it entirely.' ),
 			originalName
 		);
 	}
@@ -35,7 +35,7 @@ function MissingBlockWarning( { attributes, convertToHTML } ) {
 	return (
 		<Fragment>
 			<Warning actions={ actions }>
-				<span dangerouslySetInnerHTML={ { __html: messageHTML } } />
+				<span>{ messageHTML }</span>
 			</Warning>
 			<div>
 				<RawHTML>{ originalUndelimitedContent }</RawHTML>
