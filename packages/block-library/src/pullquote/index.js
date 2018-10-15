b5a906f252c3112f3381ea463bb6e5e472192a3f
Jorge <jorge.costa@developer.pt>
2018-10-15 16:36:21 +0100

    Added deprecations to make sure no previous pullquote fails the block validation.

--- packages/block-library/src/pullquote/index.js
+++ packages/block-library/src/pullquote/index.js
@@ -20,6 +20,7 @@ import {
 import {
 	default as edit,
 	SOLID_COLOR_STYLE_NAME,
+	SOLID_COLOR_CLASS,
 	BORDER_COLOR_STYLE_NAME,
 	BORDER_COLOR_CLASS,
 } from './edit';
@@ -120,6 +121,73 @@ export const settings = {
 	},
 
 	deprecated: [ {
+		attributes: {
+			...blockAttributes,
+		},
+		// pull quote version where the default style was the version with border color.
+		save( { attributes } ) {
+			const { mainColor, customMainColor, textColor, customTextColor, value, citation, className } = attributes;
+			const isSolidColorStyle = includes( className, SOLID_COLOR_CLASS );
+
+			let figureClass, figureStyles;
+			// Is solid color style
+			if ( isSolidColorStyle ) {
+				figureClass = getColorClassName( 'background-color', mainColor );
+				if ( ! figureClass ) {
+					figureStyles = {
+						backgroundColor: customMainColor,
+					};
+				}
+			// Is normal style and a custom color is being used ( we can set a style directly with its value)
+			} else if ( customMainColor ) {
+				figureStyles = {
+					borderColor: customMainColor,
+				};
+			// Is normal style and a named color is being used, we need to retrieve the color value to set the style,
+			// as there is no expectation that themes create classes that set border colors.
+			} else if ( mainColor ) {
+				const colors = get( select( 'core/editor' ).getEditorSettings(), [ 'colors' ], [] );
+				const colorObject = getColorObjectByAttributeValues( colors, mainColor );
+				figureStyles = {
+					borderColor: colorObject.color,
+				};
+			}
+
+			const blockquoteTextColorClass = getColorClassName( 'color', textColor );
+			const blockquoteClasses = textColor || customTextColor ? classnames( 'has-text-color', {
+				[ blockquoteTextColorClass ]: blockquoteTextColorClass,
+			} ) : undefined;
+			const blockquoteStyle = blockquoteTextColorClass ? undefined : { color: customTextColor };
+			return (
+				<figure className={ figureClass } style={ figureStyles }>
+					<blockquote className={ blockquoteClasses } style={ blockquoteStyle } >
+						<RichText.Content value={ value } multiline="p" />
+						{ ! RichText.isEmpty( citation ) && <RichText.Content tagName="cite" value={ citation } /> }
+					</blockquote>
+				</figure>
+			);
+		},
+		migrate: function( attributes ) {
+			if ( includes( attributes.className, SOLID_COLOR_CLASS ) ) {
+				// Block using the solid color style, markup is still tottaly valid and it should render as expected,
+				// we cam just use the same attributes as before.
+				return attributes;
+			}
+
+			// Block using the border color style (previous default),
+			// with some customization's that made the validation fail.
+			// Update its className to include a reference to the previous default color style,
+			// as classes for default class are not included.
+			let className = BORDER_COLOR_CLASS;
+			if ( attributes.className ) {
+				className = `${ BORDER_COLOR_CLASS } ${ attributes.className }`;
+			}
+			return {
+				...attributes,
+				className,
+			};
+		},
+	}, {
 		attributes: {
 			...blockAttributes,
 		},
