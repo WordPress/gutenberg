7db3427e97ac884d9202079e8e60b038d689df04
Danilo Ercoli <ercoli@gmail.com>
2018-10-15 17:43:01 +0200

    Fix Flow: forgot to update PropsType

--- src/block-management/block-holder.js
+++ src/block-management/block-holder.js
@@ -19,7 +19,7 @@ type PropsType = BlockType & {
 	onChange: ( clientId: string, attributes: mixed ) => void,
 	onToolbarButtonPressed: ( button: number, clientId: string ) => void,
 	onBlockHolderPressed: ( clientId: string ) => void,
-	insertBlocksAfter: ( clientId: string, blocks: Array<Object> ) => void,
+	insertBlocksAfter: ( blocks: Array<Object> ) => void,
 };
 
 type StateType = {
