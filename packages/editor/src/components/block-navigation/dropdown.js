c81b4a791bd69a31b07515b08ccb33dd7542ed30
Riad Benguella <benguella@gmail.com>
2018-10-16 09:38:29 +0100

    Use Path for a mobile-friendly SVG

--- packages/editor/src/components/block-navigation/dropdown.js
+++ packages/editor/src/components/block-navigation/dropdown.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { Fragment } from '@wordpress/element';
-import { IconButton, Dropdown, SVG, KeyboardShortcuts } from '@wordpress/components';
+import { IconButton, Dropdown, SVG, Path, KeyboardShortcuts } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -12,7 +12,7 @@ import BlockNavigation from './';
 
 const menuIcon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20">
-		<path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" />
+		<Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" />
 	</SVG>
 );
 
