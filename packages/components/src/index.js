0280835842fd0f08090ca40b67e506d1f7108fdc
Marin Atanasov <tyxla@abv.bg>
2018-10-16 15:04:00 +0300

    Components: Expose RawIcon component

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -44,6 +44,7 @@ export { default as Popover } from './popover';
 export { default as QueryControls } from './query-controls';
 export { default as RadioControl } from './radio-control';
 export { default as RangeControl } from './range-control';
+export { default as RawIcon } from './raw-icon';
 export { default as ResizableBox } from './resizable-box';
 export { default as ResponsiveWrapper } from './responsive-wrapper';
 export { default as SandBox } from './sandbox';
