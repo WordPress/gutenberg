3dd1a8b14e28dea83c3f35395ee4fad93bd5dd1e
Andrew Duthie <andrew@andrewduthie.com>
2018-10-17 13:37:51 -0400

    Editor: Restore focus on Escape by activeElement at focus

--- packages/block-editor/src/components/navigable-toolbar/index.js
+++ packages/block-editor/src/components/navigable-toolbar/index.js
@@ -11,53 +11,50 @@ import { Component, createRef } from '@wordpress/element';
 import { focus } from '@wordpress/dom';
 import { ESCAPE } from '@wordpress/keycodes';
 
-/**
- * Browser dependencies
- */
-
-const { Node, getSelection } = window;
-
 class NavigableToolbar extends Component {
 	constructor() {
 		super( ...arguments );
 
 		this.focusToolbar = this.focusToolbar.bind( this );
-		this.focusSelection = this.focusSelection.bind( this );
+		this.restoreFocus = this.restoreFocus.bind( this );
 
 		this.switchOnKeyDown = cond( [
-			[ matchesProperty( [ 'keyCode' ], ESCAPE ), this.focusSelection ],
+			[ matchesProperty( [ 'keyCode' ], ESCAPE ), this.restoreFocus ],
 		] );
 		this.toolbar = createRef();
 	}
 
+	/**
+	 * Shifts focus to the first tabbable element within the toolbar container,
+	 * if one exists.
+	 */
 	focusToolbar() {
 		const tabbables = focus.tabbable.find( this.toolbar.current );
-		if ( tabbables.length ) {
-			tabbables[ 0 ].focus();
-		}
-	}
-
-	/**
-	 * Programmatically shifts focus to the element where the current selection
-	 * exists, if there is a selection.
-	 */
-	focusSelection() {
-		// Ensure that a selection exists.
-		const selection = getSelection();
-		if ( ! selection ) {
+		if ( ! tabbables.length ) {
 			return;
 		}
 
-		// Focus node may be a text node, which cannot be focused directly.
-		// Find its parent element instead.
-		const { focusNode } = selection;
-		let focusElement = focusNode;
-		if ( focusElement.nodeType !== Node.ELEMENT_NODE ) {
-			focusElement = focusElement.parentElement;
-		}
+		// Track the original active element prior to shifting focus, so that
+		// focus can be returned if the user presses Escape while in toolbar.
+		//
+		// [TODO]: Currently, to support "stepping down" through multiple
+		// navigable toolbars, the assigned value is only cleared after
+		// pressing Escape. If the user manually shifts focus away and later
+		// returns to press Escape, it will still (wrongly) use the earlier-
+		// assigned value.
+		this.activeElementBeforeFocus = document.activeElement;
 
-		if ( focusElement ) {
-			focusElement.focus();
+		tabbables[ 0 ].focus();
+	}
+
+	/**
+	 * Restores focus to the active element at the time focus was
+	 * programattically shifted to the toolbar, if one exists.
+	 */
+	restoreFocus() {
+		if ( this.activeElementBeforeFocus ) {
+			this.activeElementBeforeFocus.focus();
+			delete this.activeElementBeforeFocus;
 		}
 	}
 
