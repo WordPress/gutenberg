42f980f0b60c993f5f4119bdc365b971765e4d5a
Chris Van Patten <chris@vanpattenmedia.com>
2018-10-17 20:12:02 -0400

    Update e2e tests

--- test/e2e/specs/font-size-picker.test.js
+++ test/e2e/specs/font-size-picker.test.js
@@ -26,23 +26,14 @@ describe( 'Font Size Picker', () => {
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'should apply a named font size using the font size input', async () => {
-		// Create a paragraph block with some content.
-		await clickBlockAppender();
-		await page.keyboard.type( 'Paragraph to be made "small"' );
-
-		await page.click( '.blocks-font-size .components-range-control__number' );
-		await page.keyboard.type( '13' );
-
-		// Ensure content matches snapshot.
-		const content = await getEditedPostContent();
-		expect( content ).toMatchSnapshot();
-	} );
-
 	it( 'should apply a custom font size using the font size input', async () => {
 		// Create a paragraph block with some content.
 		await clickBlockAppender();
-		await page.keyboard.type( 'Paragraph to be made "small"' );
+		await page.keyboard.type( 'Paragraph to be made "custom"' );
+
+		await page.click( '.components-font-size-picker__selector' );
+		const changeSizeButton = await page.waitForSelector( '.components-button.is-font-custom' );
+		await changeSizeButton.click();
 
 		await page.click( '.blocks-font-size .components-range-control__number' );
 		await page.keyboard.type( '23' );
@@ -55,10 +46,11 @@ describe( 'Font Size Picker', () => {
 	it( 'should reset a named font size using the reset button', async () => {
 		// Create a paragraph block with some content.
 		await clickBlockAppender();
-		await page.keyboard.type( 'Paragraph with font size reset using button' );
+		await page.keyboard.type( 'Paragraph with named font size reset using button' );
 
-		await page.click( '.blocks-font-size .components-range-control__number' );
-		await page.keyboard.type( '13' );
+		await page.click( '.components-font-size-picker__selector' );
+		const changeSizeButton = await page.waitForSelector( '.components-button.is-font-small' );
+		await changeSizeButton.click();
 
 		// Blur the range control
 		await page.click( '.components-base-control__label' );
@@ -71,37 +63,23 @@ describe( 'Font Size Picker', () => {
 		expect( content ).toMatchSnapshot();
 	} );
 
-	it( 'should reset a named font size using input field', async () => {
+	it( 'should reset a custom font size using the reset button', async () => {
 		// Create a paragraph block with some content.
 		await clickBlockAppender();
-		await page.keyboard.type( 'Paragraph with font size reset using input field' );
+		await page.keyboard.type( 'Paragraph with custom font size reset using button' );
 
 		await page.click( '.components-font-size-picker__selector' );
-		const changeSizeButton = await page.waitForSelector( '.components-button.is-font-large' );
+		const changeSizeButton = await page.waitForSelector( '.components-button.is-font-custom' );
 		await changeSizeButton.click();
-
-		await page.click( '.blocks-font-size .components-range-control__number' );
-		await page.keyboard.press( 'Backspace' );
-		await page.keyboard.press( 'Backspace' );
-
-		// Ensure content matches snapshot.
-		const content = await getEditedPostContent();
-		expect( content ).toMatchSnapshot();
-	} );
-
-	it( 'should reset a custom font size using input field', async () => {
-		// Create a paragraph block with some content.
-		await clickBlockAppender();
-		await page.keyboard.type( 'Paragraph to be made "small"' );
 
 		await page.click( '.blocks-font-size .components-range-control__number' );
 		await page.keyboard.type( '23' );
 
-		await page.keyboard.press( 'Backspace' );
+		// Blur the range control
+		await page.click( '.components-base-control__label' );
 
-		await page.click( '.blocks-font-size .components-range-control__number' );
-		await page.keyboard.press( 'Backspace' );
-		await page.keyboard.press( 'Backspace' );
+		const resetButton = ( await page.$x( '//*[contains(concat(" ", @class, " "), " components-font-size-picker__buttons ")]//*[text()=\'Reset\']' ) )[ 0 ];
+		await resetButton.click();
 
 		// Ensure content matches snapshot.
 		const content = await getEditedPostContent();
