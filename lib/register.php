e6818486ac2c22b769338dc5243e833beff4e81e
Andrew Duthie <andrew@andrewduthie.com>
2018-10-18 12:49:10 -0400

    REST API: Use posts endpoint for reusable blocks

--- lib/register.php
+++ lib/register.php
@@ -439,25 +439,27 @@ function gutenberg_register_post_types() {
 	register_post_type(
 		'wp_block',
 		array(
-			'labels'                => array(
+			'labels'          => array(
 				'name'          => __( 'Blocks', 'gutenberg' ),
 				'singular_name' => __( 'Block', 'gutenberg' ),
 				'search_items'  => __( 'Search Blocks', 'gutenberg' ),
 			),
-			'public'                => false,
-			'show_ui'               => true,
-			'show_in_menu'          => false,
-			'rewrite'               => false,
-			'show_in_rest'          => true,
-			'rest_base'             => 'blocks',
-			'rest_controller_class' => 'WP_REST_Blocks_Controller',
-			'capability_type'       => 'block',
-			'capabilities'          => array(
+			'public'          => false,
+			'show_ui'         => true,
+			'show_in_menu'    => false,
+			'rewrite'         => false,
+			'show_in_rest'    => true,
+			'rest_base'       => 'blocks',
+			'capability_type' => 'block',
+			'capabilities'    => array(
 				'read'         => 'read_blocks',
 				'create_posts' => 'create_blocks',
 			),
-			'map_meta_cap'          => true,
-			'supports'              => false,
+			'map_meta_cap'    => true,
+			'supports'        => array(
+				'title',
+				'editor',
+			),
 		)
 	);
 
--- packages/list-reusable-blocks/src/utils/export.js
+++ packages/list-reusable-blocks/src/utils/export.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { pick, kebabCase } from 'lodash';
+import { kebabCase } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -19,13 +19,16 @@ import { download } from './file';
  * @param {number} id
  */
 async function exportReusableBlock( id ) {
-	const postType = await apiFetch( { path: `/wp/v2/types/wp_block` } );
-	const reusableBlock = await apiFetch( { path: `/wp/v2/${ postType.rest_base }/${ id }` } );
+	const postType = await apiFetch( { path: `/wp/v2/types/wp_block?context=edit` } );
+	const post = await apiFetch( { path: `/wp/v2/${ postType.rest_base }/${ id }` } );
+	const title = post.title.raw;
+	const content = post.content.raw;
 	const fileContent = JSON.stringify( {
 		__file: 'wp_block',
-		...pick( reusableBlock, [ 'title', 'content' ] ),
+		title,
+		content,
 	}, null, 2 );
-	const fileName = kebabCase( reusableBlock.title ) + '.json';
+	const fileName = kebabCase( title ) + '.json';
 
 	download( fileName, fileContent, 'application/json' );
 }
