7b076c347b5e42999cb54aa5d3c968a5b891b59b
Nicola Heald <nicola@notnowlewis.com>
2018-10-23 09:20:35 +0100

    Fix `SVG` and `Path` in icons, add tests for `findBlock`

--- packages/block-library/src/embed/test/index.js
+++ packages/block-library/src/embed/test/index.js
@@ -7,6 +7,7 @@ import { render } from 'enzyme';
  * Internal dependencies
  */
 import { getEmbedEditComponent } from '../edit';
+import { findBlock } from '../util';
 
 describe( 'core/embed', () => {
 	test( 'block edit matches snapshot', () => {
@@ -14,5 +15,15 @@ describe( 'core/embed', () => {
 		const wrapper = render( <EmbedEdit attributes={ {} } /> );
 
 		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	test( 'findBlock matches a URL to a block name', () => {
+		const twitterURL = 'https://twitter.com/notnownikki';
+		const youtubeURL = 'https://www.youtube.com/watch?v=bNnfuvC1LlU';
+		const unknownURL = 'https://example.com/';
+
+		expect( findBlock( twitterURL ) ).toEqual( 'core-embed/twitter' );
+		expect( findBlock( youtubeURL ) ).toEqual( 'core-embed/youtube' );
+		expect( findBlock( unknownURL ) ).toEqual( 'core/embed' );
 	} );
 } );
