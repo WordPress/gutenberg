5b57cccfa93ec1c14bf1fe8e3ac84b46fb2598d0
Tugdual de Kerviler <dekervit@gmail.com>
2018-10-23 12:48:27 +0200

    Deprecate svg replacements

--- packages/components/src/primitives/svg/index.js
+++ packages/components/src/primitives/svg/index.js
@@ -8,11 +8,41 @@ import { createElement } from '@wordpress/element';
  */
 import deprecated from '@wordpress/deprecated';
 
-export const G = ( props ) => createElement( 'g', props );
-export const Path = ( props ) => createElement( 'path', props );
-export const Polygon = ( props ) => createElement( 'polygon', props );
+// deprecations
+export const G = ( props ) => {
+	deprecated( 'wp.components.G', {
+		version: '4.2',
+		alternative: 'g',
+		plugin: 'Gutenberg',
+	} );
+	return createElement( 'g', props );
+}
+
+export const Path = ( props ) => {
+	deprecated( 'wp.components.Path', {
+		version: '4.2',
+		alternative: 'path',
+		plugin: 'Gutenberg',
+	} );
+	return createElement( 'path', props );
+}
+
+export const Polygon = ( props ) => {
+	deprecated( 'wp.components.Polygon', {
+		version: '4.2',
+		alternative: 'polygon',
+		plugin: 'Gutenberg',
+	} );
+	return createElement( 'polygon', props );
+}
 
 export const SVG = ( props ) => {
+	deprecated( 'wp.components.SVG', {
+		version: '4.2',
+		alternative: 'svg',
+		plugin: 'Gutenberg',
+	} );
+
 	const appliedProps = {
 		...props,
 		role: 'img',
@@ -23,12 +53,11 @@ export const SVG = ( props ) => {
 	return <svg { ...appliedProps } />;
 };
 
-// deprecations
 export const AccessibleSVG = ( props ) => {
 	deprecated( 'wp.components.AccessibleSVG', {
 		version: '4.2',
-		alternative: 'wp.components.SVG',
+		alternative: 'svg',
 		plugin: 'Gutenberg',
 	} );
-	return <SVG { ...props } />;
+	return <svg { ...props } />;
 };
