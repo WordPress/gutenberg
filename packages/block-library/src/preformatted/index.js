3807693da6121cd7181345cab8a86b9002457181
Tammie Lister <tammie@automattic.com>
2018-10-23 21:15:56 +0100

    Try different block descriptions
    This follows the latest suggestions by @alexislloyd in #10919.

--- packages/block-library/src/preformatted/index.js
+++ packages/block-library/src/preformatted/index.js
@@ -11,7 +11,7 @@ export const name = 'core/preformatted';
 export const settings = {
 	title: __( 'Preformatted' ),
 
-	description: __( 'Add text that respects your spacing and tabs, and also allows styling.' ),
+	description: __( 'Add custom HTML code and preview it as you edit.' ),
 
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path d="M0,0h24v24H0V0z" fill="none" /><Path d="M20,4H4C2.9,4,2,4.9,2,6v12c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V6C22,4.9,21.1,4,20,4z M20,18H4V6h16V18z" /><Rect x="6" y="10" width="2" height="2" /><Rect x="6" y="14" width="8" height="2" /><Rect x="16" y="14" width="2" height="2" /><Rect x="10" y="10" width="8" height="2" /></SVG>,
 
--- packages/block-library/src/verse/index.js
+++ packages/block-library/src/verse/index.js
@@ -16,7 +16,7 @@ export const name = 'core/verse';
 export const settings = {
 	title: __( 'Verse' ),
 
-	description: __( 'A block for haiku? Why not? Blocks for all the things! (See what we did here?)' ),
+	description: __( 'Insert haiku. Use special spacing formats. Or quote song lyrics. (See what we did here?)' ),
 
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><Path d="M3 17v4h4l11-11-4-4L3 17zm3 2H5v-1l9-9 1 1-9 9zM21 6l-3-3h-1l-2 2 4 4 2-2V6z" /></SVG>,
 
