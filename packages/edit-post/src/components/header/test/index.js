2455a0b9b9a3c37beb9b831340871260029a9a0e
Andrés <andres.maneiro@automattic.com>
2018-10-24 10:48:57 +0200

    Tests for publish sidebar not enabled

--- packages/edit-post/src/components/header/test/index.js
+++ packages/edit-post/src/components/header/test/index.js
@@ -9,6 +9,22 @@ import { shallow } from 'enzyme';
 import { PostPublishButtonOrToggle } from '../post-publish-button-or-toggle';
 
 describe( 'PostPublishButtonOrToggle', () => {
+	it( 'should render button when the publish sidebar is not enabled and the viewport is > small', () => {
+		const wrapper = shallow( <PostPublishButtonOrToggle
+			isPublishSidebarEnabled={ false }
+			isSmallViewport={ false }
+		/> );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+
+	it( 'should render toggle when the publish sidebar is not enabled and the viewport is <= small', () => {
+		const wrapper = shallow( <PostPublishButtonOrToggle
+			isPublishSidebarEnabled={ false }
+			isSmallViewport={ true }
+		/> );
+		expect( wrapper ).toMatchSnapshot();
+	} );
+
 	it( 'should render button when the post is published', () => {
 		const wrapper = shallow( <PostPublishButtonOrToggle isPublished={ true } /> );
 		expect( wrapper ).toMatchSnapshot();
