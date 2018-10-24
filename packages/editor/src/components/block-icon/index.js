0b37479dd7e5e46bf2ee3ec3c3cdba3a31eb8ba9
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-10-24 11:13:30 +0200

    Ensure that default icon goes through the process of width and height normalization in BlockIcon Props to @jasmussen for helping to catch it.

--- packages/editor/src/components/block-icon/index.js
+++ packages/editor/src/components/block-icon/index.js
@@ -11,10 +11,10 @@ import { createElement, Component } from '@wordpress/element';
 
 function renderIcon( icon ) {
 	if ( 'string' === typeof icon ) {
-		if ( icon === 'block-default' ) {
-			return <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z" /></SVG>;
+		if ( icon !== 'block-default' ) {
+			return <Dashicon icon={ icon } size={ 20 } />;
 		}
-		return <Dashicon icon={ icon } size={ 20 } />;
+		icon = 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z" /></SVG>;
 	}
 	if ( 'function' === typeof icon ) {
 		if ( icon.prototype instanceof Component ) {
