f2d5e89a5fe0afc36ad755a4ff83419cc0796d74
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-10-25 10:41:03 +0200

    Make all plugin API e2e test independent

--- test/e2e/specs/plugins-api.test.js
+++ test/e2e/specs/plugins-api.test.js
@@ -14,11 +14,14 @@ import { activatePlugin, deactivatePlugin } from '../support/plugins';
 describe( 'Using Plugins API', () => {
 	beforeAll( async () => {
 		await activatePlugin( 'gutenberg-test-plugin-plugins-api' );
-		await newPost();
 	} );
 
 	afterAll( async () => {
 		await deactivatePlugin( 'gutenberg-test-plugin-plugins-api' );
+	} );
+
+	beforeEach( async () => {
+		await newPost();
 	} );
 
 	describe( 'Post Status Info', () => {
@@ -31,16 +34,13 @@ describe( 'Using Plugins API', () => {
 	} );
 
 	describe( 'Publish Panel', () => {
-		afterEach( async () => {
-			// Close Publish panel.
-			await page.click( '.editor-post-publish-panel__header button[aria-label="Close panel"]' );
-		} );
-
-		it( 'Should render publish panel inside Pre-publish sidebar', async () => {
+		beforeEach( async () => {
 			// Type something first to activate Publish button.
 			await clickBlockAppender();
 			await page.keyboard.type( 'First paragraph' );
+		} );
 
+		it( 'Should render publish panel inside Pre-publish sidebar', async () => {
 			await openPublishPanel();
 
 			const pluginPublishPanelText = await page.$eval( '.editor-post-publish-panel .my-publish-panel-plugin__pre', ( el ) => el.innerText );
@@ -66,8 +66,13 @@ describe( 'Using Plugins API', () => {
 		it( 'Should close plugins sidebar using More Menu item', async () => {
 			await clickOnMoreMenuItem( 'Sidebar title plugin' );
 
-			const pluginSidebar = await page.$( '.edit-post-sidebar' );
-			expect( pluginSidebar ).toBeNull();
+			const pluginSidebarOpened = await page.$( '.edit-post-sidebar' );
+			expect( pluginSidebarOpened ).not.toBeNull();
+
+			await clickOnMoreMenuItem( 'Sidebar title plugin' );
+
+			const pluginSidebarClosed = await page.$( '.edit-post-sidebar' );
+			expect( pluginSidebarClosed ).toBeNull();
 		} );
 	} );
 } );
