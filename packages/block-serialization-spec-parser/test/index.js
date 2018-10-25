9fbc36f1e1a8d7760131685c9eb59c86ffb2a48f
Dennis Snell <dennis.snell@automattic.com>
2018-10-25 14:47:32 -0700

    add index tests

--- packages/block-serialization-spec-parser/test/index.js
+++ packages/block-serialization-spec-parser/test/index.js
@@ -12,12 +12,26 @@ describe( 'block-serialization-spec-parser', () => {
 		expect( result ).toMatchSnapshot();
 	} );
 
-	test( 'adds empty block markers when no inner blocks exist', () => {
-		[
-			'HTML soup',
-			'<!-- wp:voidBlock /-->',
-			'<!-- wp:block --><!-- /wp:block -->',
-			'<!-- wp:block -->with content<!-- /wp:block -->',
-		].forEach( ( document ) => expect( parse( document ) ).toHaveProperty( 'blockMarkers', [] ) );
-	} );
+	test( 'adds empty block markers when no inner blocks exist', () => [
+		'HTML soup',
+		'<!-- wp:voidBlock /-->',
+		'<!-- wp:block --><!-- /wp:block -->',
+		'<!-- wp:block -->with content<!-- /wp:block -->',
+	].forEach( ( document ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', [] ) ) );
+
+	test( 'adds block markers for inner blocks', () => [
+		[ '<!-- wp:block --><!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 0 ] ],
+		[ '<!-- wp:block -->aa<!-- wp:voidInnerBlock /-->bb<!-- /wp:block -->', [ 2 ] ],
+		[ '<!-- wp:block -->aa<!-- wp:innerBlock -->bb<!-- /wp:innerBlock -->cc<!-- /wp:block -->', [ 2 ] ],
+		[ '<!-- wp:block --><!-- wp:start /-->aa<!-- wp:innerBlock -->bb<!-- /wp:innerBlock -->cc<!-- wp:end /--><!-- /wp:block -->', [ 0, 2, 4 ] ],
+	].forEach( ( [ document, markers ] ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', markers ) ) );
+
+	test( 'block markers report UCS2 length', () => [
+		[ '<!-- wp:block -->a<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 1 ] ],
+		[ '<!-- wp:block -->𐀀<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \ud800 \udc00
+		[ '<!-- wp:block -->𠀀<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \ud840 \udc00
+		[ '<!-- wp:block -->葛<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 1 ] ], // \u845B
+		[ '<!-- wp:block -->葛<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \u845B \ue0100
+		[ '<!-- wp:block -->❤️<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \u2764 \ufe0f
+	].forEach( ( [ document, markers ] ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', markers ) ) );
 } );
