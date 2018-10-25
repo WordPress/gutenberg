8e2c466f586e84ea14ec3e8b3f1a4097fad9a620
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2018-10-25 11:01:20 +0200

    Testing: Add missing e2e tests for Plugins API (#10513)
    * Testing: Add missing e2e tests for Plugins API
    
    * Make all plugin API e2e test independent

--- test/e2e/test-plugins/plugins-api/sidebar.js
+++ test/e2e/test-plugins/plugins-api/sidebar.js
@@ -1,106 +1,108 @@
-var Button = wp.components.Button;
-var PanelBody = wp.components.PanelBody;
-var PanelRow = wp.components.PanelRow;
-var compose = wp.compose.compose;
-var withDispatch = wp.data.withDispatch;
-var withSelect = wp.data.withSelect;
-var PlainText = wp.editor.PlainText;
-var Fragment = wp.element.Fragment;
-var el = wp.element.createElement;
-var __ = wp.i18n.__;
-var registerPlugin = wp.plugins.registerPlugin;
-var PluginSidebar = wp.editPost.PluginSidebar;
-var PluginSidebarMoreMenuItem = wp.editPost.PluginSidebarMoreMenuItem;
+( function() {
+	var Button = wp.components.Button;
+	var PanelBody = wp.components.PanelBody;
+	var PanelRow = wp.components.PanelRow;
+	var compose = wp.compose.compose;
+	var withDispatch = wp.data.withDispatch;
+	var withSelect = wp.data.withSelect;
+	var PlainText = wp.editor.PlainText;
+	var Fragment = wp.element.Fragment;
+	var el = wp.element.createElement;
+	var __ = wp.i18n.__;
+	var registerPlugin = wp.plugins.registerPlugin;
+	var PluginSidebar = wp.editPost.PluginSidebar;
+	var PluginSidebarMoreMenuItem = wp.editPost.PluginSidebarMoreMenuItem;
 
-function SidebarContents( props ) {
-	return el(
-		PanelBody,
-		{},
-		el(
-			PanelRow,
+	function SidebarContents( props ) {
+		return el(
+			PanelBody,
 			{},
 			el(
-				'label',
-				{
-					'htmlFor': 'title-plain-text'
-				},
-				__( 'Title:' ),
+				PanelRow,
+				{},
+				el(
+					'label',
+					{
+						'htmlFor': 'title-plain-text'
+					},
+					__( 'Title:' ),
+				),
+				el(
+					PlainText,
+					{
+						id: 'title-plain-text',
+						onChange: props.updateTitle,
+						placeholder: __( '(no title)' ),
+						value: props.title
+					}
+				)
 			),
 			el(
-				PlainText,
-				{
-					id: 'title-plain-text',
-					onChange: props.updateTitle,
-					placeholder: __( '(no title)' ),
-					value: props.title
-				}
+				PanelRow,
+				{},
+				el(
+					Button,
+					{
+						isPrimary: true,
+						onClick: props.resetTitle
+					},
+					__( 'Reset' )
+				)
 			)
-		),
-		el(
-			PanelRow,
+		);
+	}
+
+	var SidebarContentsWithDataHandling = compose( [
+		withSelect( function( select ) {
+			return {
+				title: select( 'core/editor' ).getEditedPostAttribute( 'title' ),
+			};
+		} ),
+		withDispatch( function( dispatch ) {
+			function editPost( title ) {
+				dispatch( 'core/editor' ).editPost( {
+					title: title
+				} );
+			}
+
+			return {
+				updateTitle: function( title ) {
+					editPost( title );
+				},
+				resetTitle: function() {
+					editPost( '' );
+				}
+			};
+		} )
+	] )( SidebarContents );
+
+	function MySidebarPlugin() {
+		return el(
+			Fragment,
 			{},
 			el(
-				Button,
+				PluginSidebar,
 				{
-					isPrimary: true,
-					onClick: props.resetTitle
+					name: 'title-sidebar',
+					title: __( 'Sidebar title plugin' )
 				},
-				__( 'Reset' )
-			)
-		)
-	);
-}
-
-var SidebarContentsWithDataHandling = compose( [
-	withSelect( function( select ) {
-		return {
-			title: select( 'core/editor' ).getEditedPostAttribute( 'title' ),
-		};
-	} ),
-	withDispatch( function( dispatch ) {
-		function editPost( title ) {
-			dispatch( 'core/editor' ).editPost( {
-				title: title
-			} );
-		}
-
-		return {
-			updateTitle: function( title ) {
-				editPost( title );
-			},
-			resetTitle: function() {
-				editPost( '' );
-			}
-		};
-	} )
-] )( SidebarContents );
-
-function MySidebarPlugin() {
-	return el(
-		Fragment,
-		{},
-		el(
-			PluginSidebar,
-			{
-				name: 'title-sidebar',
-				title: __( 'Sidebar title plugin' )
-			},
+				el(
+					SidebarContentsWithDataHandling,
+					{}
+				)
+			),
 			el(
-				SidebarContentsWithDataHandling,
-				{}
+				PluginSidebarMoreMenuItem,
+				{
+					target: 'title-sidebar'
+				},
+				__( 'Sidebar title plugin' )
 			)
-		),
-		el(
-			PluginSidebarMoreMenuItem,
-			{
-				target: 'title-sidebar'
-			},
-			__( 'Sidebar title plugin' )
-		)
-	);
-}
+		);
+	}
 
-registerPlugin( 'my-sidebar-plugin', {
-	icon: 'text',
-	render: MySidebarPlugin
-} );
+	registerPlugin( 'my-sidebar-plugin', {
+		icon: 'text',
+		render: MySidebarPlugin
+	} );
+} )();
