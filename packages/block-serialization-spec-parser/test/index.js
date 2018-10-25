d744794191734df9529509204597fc4476ea2047
Dennis Snell <dennis.snell@automattic.com>
2018-10-25 15:31:24 -0700

    update test values

--- packages/block-serialization-spec-parser/test/index.js
+++ packages/block-serialization-spec-parser/test/index.js
@@ -20,18 +20,18 @@ describe( 'block-serialization-spec-parser', () => {
 	].forEach( ( document ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', [] ) ) );
 
 	test( 'adds block markers for inner blocks', () => [
-		[ '<!-- wp:block --><!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 0 ] ],
-		[ '<!-- wp:block -->aa<!-- wp:voidInnerBlock /-->bb<!-- /wp:block -->', [ 2 ] ],
-		[ '<!-- wp:block -->aa<!-- wp:innerBlock -->bb<!-- /wp:innerBlock -->cc<!-- /wp:block -->', [ 2 ] ],
+		[ '<!-- wp:block --><!-- wp:void /--><!-- /wp:block -->', [ 0 ] ],
+		[ '<!-- wp:block -->aa<!-- wp:void /-->bb<!-- /wp:block -->', [ 2 ] ],
+		[ '<!-- wp:block -->aa<!-- wp:inner -->bb<!-- /wp:innerBlock -->cc<!-- /wp:block -->', [ 2 ] ],
 		[ '<!-- wp:block --><!-- wp:start /-->aa<!-- wp:innerBlock -->bb<!-- /wp:innerBlock -->cc<!-- wp:end /--><!-- /wp:block -->', [ 0, 2, 4 ] ],
 	].forEach( ( [ document, markers ] ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', markers ) ) );
 
 	test( 'block markers report UCS2 length', () => [
-		[ '<!-- wp:block -->a<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 1 ] ],
-		[ '<!-- wp:block -->𐀀<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \ud800 \udc00 - multi-lingual plane
-		[ '<!-- wp:block -->𠀀<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \ud840 \udc00 - ideographic plane
-		[ '<!-- wp:block -->葛<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 1 ] ], // \u845B - plain character
-		[ '<!-- wp:block -->葛<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \u845B \ue0100 - ^^^ plus special-purpose plane variant
-		[ '<!-- wp:block -->❤️<!-- wp:voidInnerBlock /--><!-- /wp:block -->', [ 2 ] ], // \u2764 \ufe0f - emoji
+		[ '<!-- wp:block -->a<!-- wp:void /--><!-- /wp:block -->', [ 1 ] ],
+		[ '<!-- wp:block -->𐀀<!-- wp:void /--><!-- /wp:block -->', [ 2 ] ], // \ud800 \udc00 - multi-lingual plane
+		[ '<!-- wp:block -->𠀀<!-- wp:void /--><!-- /wp:block -->', [ 2 ] ], // \ud840 \udc00 - ideographic plane
+		[ '<!-- wp:block -->葛<!-- wp:void /--><!-- /wp:block -->', [ 1 ] ], // \u845B - plain character
+		[ '<!-- wp:block -->葛��<!-- wp:void /--><!-- /wp:block -->', [ 3 ] ], // \u845B \ue0100 - ^^^ plus special-purpose plane variant
+		[ '<!-- wp:block -->❤️<!-- wp:void /--><!-- /wp:block -->', [ 2 ] ], // \u2764 \ufe0f - emoji
 	].forEach( ( [ document, markers ] ) => expect( parse( document )[ 0 ] ).toHaveProperty( 'blockMarkers', markers ) ) );
 } );
