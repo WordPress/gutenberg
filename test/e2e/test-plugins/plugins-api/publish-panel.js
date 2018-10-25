fe32cc68e55792d81b22328c5fdd932dcc6bb550
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2018-10-25 11:01:20 +0200

    Testing: Add missing e2e tests for Plugins API (#10513)
    * Testing: Add missing e2e tests for Plugins API
    
    * Make all plugin API e2e test independent

--- test/e2e/test-plugins/plugins-api/publish-panel.js
+++ test/e2e/test-plugins/plugins-api/publish-panel.js
@@ -1,47 +1,49 @@
-var el = wp.element.createElement;
-var Fragment = wp.element.Fragment;
-var __ = wp.i18n.__;
-var registerPlugin = wp.plugins.registerPlugin;
-var PluginPostPublishPanel = wp.editPost.PluginPostPublishPanel;
-var PluginPrePublishPanel = wp.editPost.PluginPrePublishPanel;
+( function() {
+	var el = wp.element.createElement;
+	var Fragment = wp.element.Fragment;
+	var __ = wp.i18n.__;
+	var registerPlugin = wp.plugins.registerPlugin;
+	var PluginPostPublishPanel = wp.editPost.PluginPostPublishPanel;
+	var PluginPrePublishPanel = wp.editPost.PluginPrePublishPanel;
 
-function PanelContent() {
-	return el(
-		'p',
-		{},
-		__( 'Here is the panel content!' )
-	);
-}
+	function PanelContent() {
+		return el(
+			'p',
+			{},
+			__( 'Here is the panel content!' )
+		);
+	}
 
-function MyPublishPanelPlugin() {
-	return el(
-		Fragment,
-		{},
-		el(
-			PluginPrePublishPanel,
-			{
-				className: 'my-publish-panel-plugin__pre',
-				title: __( 'My pre publish panel' )
-			},
+	function MyPublishPanelPlugin() {
+		return el(
+			Fragment,
+			{},
 			el(
-				PanelContent,
-				{}
-			)
-		),
-		el(
-			PluginPostPublishPanel,
-			{
-				className: 'my-publish-panel-plugin__post',
-				title: __( 'My post publish panel' )
-			},
+				PluginPrePublishPanel,
+				{
+					className: 'my-publish-panel-plugin__pre',
+					title: __( 'My pre publish panel' )
+				},
+				el(
+					PanelContent,
+					{}
+				)
+			),
 			el(
-				PanelContent,
-				{}
+				PluginPostPublishPanel,
+				{
+					className: 'my-publish-panel-plugin__post',
+					title: __( 'My post publish panel' )
+				},
+				el(
+					PanelContent,
+					{}
+				)
 			)
-		)
-	);
-}
+		);
+	}
 
-registerPlugin( 'my-publish-panel-plugin', {
-	render: MyPublishPanelPlugin
-} );
+	registerPlugin( 'my-publish-panel-plugin', {
+		render: MyPublishPanelPlugin
+	} );
+} )();
