578d0ef634f33f599e19806d0e479677b9594b59
Riad Benguella <benguella@gmail.com>
2018-10-25 10:04:51 +0100

    Add an eslint rule to use cross-environment SVG primitives (#10652)
    * Add an eslint rule to use cross-environment SVG primitives
    
    * Extend the list of forbidden elements
    Uses an existing react eslint rule to perform validation.
    Adds new components to make the existing icons mobile app friendly.
    
    * Change the way the default icon for block is set
    
    * Ensure that default icon goes through the process of width and height normalization in BlockIcon
    Props to @jasmussen for helping to catch it.

--- packages/editor/src/components/block-icon/index.js
+++ packages/editor/src/components/block-icon/index.js
@@ -6,23 +6,28 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Dashicon, SVG } from '@wordpress/components';
+import { Dashicon, Path, SVG } from '@wordpress/components';
 import { createElement, Component } from '@wordpress/element';
 
 function renderIcon( icon ) {
 	if ( 'string' === typeof icon ) {
-		return <Dashicon icon={ icon } size={ 20 } />;
-	} else if ( 'function' === typeof icon ) {
+		if ( icon !== 'block-default' ) {
+			return <Dashicon icon={ icon } size={ 20 } />;
+		}
+		icon = 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M19 7h-1V5h-4v2h-4V5H6v2H5c-1.1 0-2 .9-2 2v10h18V9c0-1.1-.9-2-2-2zm0 10H5V9h14v8z" /></SVG>;
+	}
+	if ( 'function' === typeof icon ) {
 		if ( icon.prototype instanceof Component ) {
 			return createElement( icon );
 		}
 
 		return icon();
-	} else if ( icon && ( icon.type === 'svg' || icon.type === SVG ) ) {
+	}
+	if ( icon && ( icon.type === 'svg' || icon.type === SVG ) ) {
 		const appliedProps = {
+			width: 24,
+			height: 24,
 			...icon.props,
-			width: icon.props.width || 24,
-			height: icon.props.height || 24,
 		};
 		return <SVG { ...appliedProps } />;
 	}
