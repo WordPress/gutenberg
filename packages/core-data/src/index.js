ac0eb4aa540b2873ab4d1d1f9efeddedca35e0ce
Riad Benguella <benguella@gmail.com>
2018-10-26 09:25:39 +0100

    Try clarifying entity shortcuts generation

--- packages/core-data/src/index.js
+++ packages/core-data/src/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { pick } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import { registerStore } from '@wordpress/data';
@@ -19,34 +14,32 @@ import * as resolvers from './resolvers';
 import { defaultEntities, getMethodName } from './entities';
 import { REDUCER_KEY } from './name';
 
-const createEntityRecordSelector = ( source ) => defaultEntities.reduce( ( result, entity ) => {
+// The entity selectors/resolvers and actions are shortcuts to their generic equivalents
+// (getEntityRecord, getEntityRecords, updateEntityRecord, updateEntityRecordss)
+// Instead of getEntityRecord, the consumer could use more user-frieldly named selector: getPostType, getTaxonomy...
+// The "kind" and the "name" of the entity are combined to generate these shortcuts.
+
+const entitySelectors = defaultEntities.reduce( ( result, entity ) => {
 	const { kind, name } = entity;
-	result[ getMethodName( kind, name ) ] = ( state, key ) => source.getEntityRecord( state, kind, name, key );
-	result[ getMethodName( kind, name, 'get', true ) ] = ( state, ...args ) => source.getEntityRecords( state, kind, name, ...args );
+	result[ getMethodName( kind, name ) ] = ( state, key ) => selectors.getEntityRecord( state, kind, name, key );
+	result[ getMethodName( kind, name, 'get', true ) ] = ( state, ...args ) => selectors.getEntityRecords( state, kind, name, ...args );
 	return result;
 }, {} );
 
-const createEntityRecordAction = ( source ) => defaultEntities.reduce( ( result, entity ) => {
+const entityResolvers = defaultEntities.reduce( ( result, entity ) => {
 	const { kind, name } = entity;
-	result[ getMethodName( kind, name ) ] = ( key ) => source.getEntityRecord( kind, name, key );
-	result[ getMethodName( kind, name, 'get', true ) ] = ( ...args ) => source.getEntityRecords( kind, name, ...args );
-	return result;
-}, {} );
-
-const createEntityRecordResolver = ( source ) => defaultEntities.reduce( ( result, entity ) => {
-	const { kind, name } = entity;
-	result[ getMethodName( kind, name ) ] = ( key ) => source.getEntityRecord( kind, name, key );
+	result[ getMethodName( kind, name ) ] = ( key ) => resolvers.getEntityRecord( kind, name, key );
 	const pluralMethodName = getMethodName( kind, name, 'get', true );
-	result[ pluralMethodName ] = ( ...args ) => source.getEntityRecords( kind, name, ...args );
-	result[ pluralMethodName ].shouldInvalidate = ( action, ...args ) => source.getEntityRecords.shouldInvalidate( action, kind, name, ...args );
+	result[ pluralMethodName ] = ( ...args ) => resolvers.getEntityRecords( kind, name, ...args );
+	result[ pluralMethodName ].shouldInvalidate = ( action, ...args ) => resolvers.getEntityRecords.shouldInvalidate( action, kind, name, ...args );
 	return result;
 }, {} );
 
-const entityActions = createEntityRecordAction(
-	pick( actions, [ 'saveEntityRecord' ] )
-);
-const entityResolvers = createEntityRecordResolver( resolvers );
-const entitySelectors = createEntityRecordSelector( selectors );
+const entityActions = defaultEntities.reduce( ( result, entity ) => {
+	const { kind, name } = entity;
+	result[ getMethodName( kind, name, 'save' ) ] = ( key ) => actions.saveEntityRecord( kind, name, key );
+	return result;
+}, {} );
 
 const store = registerStore( REDUCER_KEY, {
 	reducer,
