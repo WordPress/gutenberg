2f55f2c07ef7b47074290623f86209fa785e796a
Tugdual de Kerviler <dekervit@gmail.com>
2018-10-26 19:00:38 +0200

    Automap all react-native-svg components

--- packages/element/src/create-element.native.js
+++ packages/element/src/create-element.native.js
@@ -1,42 +1,15 @@
 /**
  * External dependencies
  */
-import { get, isString } from 'lodash';
+import { get, isString, lowerFirst, reduce } from 'lodash';
 import { createElement as createElementDefault } from 'react';
-import {
-	Circle,
-	Defs,
-	Ellipse,
-	G,
-	Line,
-	Path,
-	Polygon,
-	TextPath,
-	TSpan,
-	Svg,
-} from 'react-native-svg';
+import * as SVGComponents from 'react-native-svg';
 
 const htmlTagToNativeComponent = {
-	circle: Circle,
-	defs: Defs,
-	ellipse: Ellipse,
-	g: G,
-	line: Line,
-	path: Path,
-	polygon: Polygon,
-	svg: Svg,
-	textPath: TextPath,
-	tspan: TSpan,
-	/*Polyline,
-	Text,
-	Rect,
-	Use,
-	Image,
-	Symbol,
-	LinearGradient,
-	RadialGradient,
-	Stop,
-	ClipPath*/
+	...reduce( SVGComponents, ( result, component, componentName ) => {
+		result[ lowerFirst( componentName ) ] = component;
+		return result;
+	}, {} ),
 };
 
 const mapHTMLTagToNativeComponent = ( type ) => {
