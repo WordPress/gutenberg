f7143cd7efdee981aabb17b55a432d5bf57c8d68
Riad Benguella <benguella@gmail.com>
2018-10-29 12:30:56 +0100

    Override the default icon height

--- packages/editor/src/components/block-navigation/dropdown.js
+++ packages/editor/src/components/block-navigation/dropdown.js
@@ -12,7 +12,9 @@ import { rawShortcut, displayShortcut } from '@wordpress/keycodes';
 import BlockNavigation from './';
 
 const MenuIcon = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20"><Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" /></SVG>
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+		<Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" />
+	</SVG>
 );
 
 function BlockNavigationDropdown() {
