a27cc2f36ef5d29877fdf7f4341d120609c207c4
Miguel Fonseca <miguelcsf@gmail.com>
2018-10-29 16:12:27 +0000

    Factor out `save` method

--- packages/block-library/src/gallery/index.js
+++ packages/block-library/src/gallery/index.js
@@ -75,6 +75,39 @@ const deprecatedBlockAttributes = {
 	},
 };
 
+function save( { attributes } ) {
+	const { images, columns = defaultColumnsNumber( attributes ), imageCrop, linkTo } = attributes;
+	return (
+		<ul className={ `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` } >
+			{ images.map( ( image ) => {
+				let href;
+
+				switch ( linkTo ) {
+					case 'media':
+						href = image.url;
+						break;
+					case 'attachment':
+						href = image.link;
+						break;
+				}
+
+				const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ image.id ? `wp-image-${ image.id }` : null } />;
+
+				return (
+					<li key={ image.id || image.url } className="blocks-gallery-item">
+						<figure>
+							{ href ? <a href={ href }>{ img }</a> : img }
+							{ image.caption && image.caption.length > 0 && (
+								<RichText.Content tagName="figcaption" value={ image.caption } />
+							) }
+						</figure>
+					</li>
+				);
+			} ) }
+		</ul>
+	);
+}
+
 export const name = 'core/gallery';
 
 export const settings = {
@@ -175,74 +208,12 @@ export const settings = {
 
 	edit,
 
-	save( { attributes } ) {
-		const { images, columns = defaultColumnsNumber( attributes ), imageCrop, linkTo } = attributes;
-		return (
-			<ul className={ `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` } >
-				{ images.map( ( image ) => {
-					let href;
-
-					switch ( linkTo ) {
-						case 'media':
-							href = image.url;
-							break;
-						case 'attachment':
-							href = image.link;
-							break;
-					}
-
-					const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ image.id ? `wp-image-${ image.id }` : null } />;
-
-					return (
-						<li key={ image.id || image.url } className="blocks-gallery-item">
-							<figure>
-								{ href ? <a href={ href }>{ img }</a> : img }
-								{ image.caption && image.caption.length > 0 && (
-									<RichText.Content tagName="figcaption" value={ image.caption } />
-								) }
-							</figure>
-						</li>
-					);
-				} ) }
-			</ul>
-		);
-	},
+	save,
 
 	deprecated: [
 		{
 			attributes: deprecatedBlockAttributes,
-			save( { attributes } ) {
-				const { images, columns = defaultColumnsNumber( attributes ), imageCrop, linkTo } = attributes;
-				return (
-					<ul className={ `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` } >
-						{ images.map( ( image ) => {
-							let href;
-
-							switch ( linkTo ) {
-								case 'media':
-									href = image.url;
-									break;
-								case 'attachment':
-									href = image.link;
-									break;
-							}
-
-							const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ image.id ? `wp-image-${ image.id }` : null } />;
-
-							return (
-								<li key={ image.id || image.url } className="blocks-gallery-item">
-									<figure>
-										{ href ? <a href={ href }>{ img }</a> : img }
-										{ image.caption && image.caption.length > 0 && (
-											<RichText.Content tagName="figcaption" value={ image.caption } />
-										) }
-									</figure>
-								</li>
-							);
-						} ) }
-					</ul>
-				);
-			},
+			save,
 		},
 		{
 			attributes: deprecatedBlockAttributes,
