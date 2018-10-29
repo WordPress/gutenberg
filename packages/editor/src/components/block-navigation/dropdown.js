3ae65eec38b2e100106bd5cb4b866242d6e2b784
Riad Benguella <benguella@gmail.com>
2018-10-29 12:15:17 +0100

    Use the icon component in the IconButton

--- packages/editor/src/components/block-navigation/dropdown.js
+++ packages/editor/src/components/block-navigation/dropdown.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { Fragment } from '@wordpress/element';
-import { IconButton, Dropdown, Icon, SVG, Path, KeyboardShortcuts } from '@wordpress/components';
+import { IconButton, Dropdown, SVG, Path, KeyboardShortcuts } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { rawShortcut, displayShortcut } from '@wordpress/keycodes';
 
@@ -12,10 +12,7 @@ import { rawShortcut, displayShortcut } from '@wordpress/keycodes';
 import BlockNavigation from './';
 
 const MenuIcon = (
-	<Icon
-		icon={ <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" /></SVG> }
-		size={ 20 }
-	/>
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20"><Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" /></SVG>
 );
 
 function BlockNavigationDropdown() {
