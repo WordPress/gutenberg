e27c4612a40e9067b96ac11f4ebfaa37702de0ba
Pinar Olguc <pinarolguc@gmail.com>
2018-10-29 13:26:29 +0300

    Fix failing unittest

--- packages/block-library/src/nextpage/edit.js
+++ packages/block-library/src/nextpage/edit.js
@@ -6,7 +6,7 @@ import { __ } from '@wordpress/i18n';
 export default function NextPageEdit() {
 	return (
 		<div className="wp-block-nextpage">
-			<span>{ __( 'Page break' ) } </span>
+			<span>{ __( 'Page break' ) }</span>
 		</div>
 	);
 }
