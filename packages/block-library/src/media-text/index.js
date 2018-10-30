db728b7f37272c6e1e9c17eb848db179f8183c89
Tammie Lister <tammie@automattic.com>
2018-10-30 12:35:53 +0000

    Try different block descriptions (#10971)

--- packages/block-library/src/media-text/index.js
+++ packages/block-library/src/media-text/index.js
@@ -26,6 +26,8 @@ export const name = 'core/media-text';
 export const settings = {
 	title: __( 'Media & Text' ),
 
+	description: __( 'Set media and words side-by-side media for a richer layout.' ),
+
 	icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><Path d="M13 17h8v-2h-8v2zM3 19h8V5H3v14zM13 9h8V7h-8v2zm0 4h8v-2h-8v2z" /></SVG>,
 
 	category: 'layout',
--- packages/block-library/src/table/index.js
+++ packages/block-library/src/table/index.js
@@ -77,7 +77,7 @@ export const name = 'core/table';
 
 export const settings = {
 	title: __( 'Table' ),
-	description: __( 'Insert a table -- perfect for sharing charts and data.' ),
+	description: __( 'Insert a table — perfect for sharing charts and data.' ),
 	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><G><Path d="M20 3H5L3 5v14l2 2h15l2-2V5l-2-2zm0 2v3H5V5h15zm-5 14h-5v-9h5v9zM5 10h3v9H5v-9zm12 9v-9h3v9h-3z" /></G></SVG>,
 	category: 'formatting',
 
