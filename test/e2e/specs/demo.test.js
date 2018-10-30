defd55625cd03e76393b54c7641b65efdca6b4ef
Nicola Heald <nicola@notnowlewis.com>
2018-10-30 16:11:12 +0000

    Request mocking helpers for e2e tests

--- test/e2e/specs/demo.test.js
+++ test/e2e/specs/demo.test.js
@@ -1,12 +1,7 @@
 /**
- * External dependencies
- */
-import fetch from 'node-fetch';
-
-/**
  * Internal dependencies
  */
-import { visitAdmin } from '../support/utils';
+import { visitAdmin, matchURL, setUpResponseMocking, mockOrTransform } from '../support/utils';
 
 const MOCK_VIMEO_RESPONSE = {
 	url: 'https://vimeo.com/22439234',
@@ -17,47 +12,22 @@ const MOCK_VIMEO_RESPONSE = {
 	version: '1.0',
 };
 
+const couldNotBePreviewed = ( obj ) => {
+	return 'Embed Handler' === obj.provider_name;
+};
+
+const stripIframeFromEmbed = ( obj ) => {
+	return { ...obj, html: obj.html.replace( /src=[^\s]+/, '' ) };
+};
+
 describe( 'new editor state', () => {
 	beforeAll( async () => {
-		// Intercept embed requests so that scripts loaded from third parties
-		// cannot leave errors in the console and cause the test to fail.
-		await page.setRequestInterception( true );
-		page.on( 'request', async ( request ) => {
-			if ( request.url().indexOf( 'oembed%2F1.0%2Fproxy' ) !== -1 ) {
-				// Because we can't get the responses to requests and modify them on the fly,
-				// we have to make our own request, get the response, modify it, then use the
-				// modified values to respond to the request.
-				const response = await fetch(
-					request.url(),
-					{
-						headers: request.headers(),
-						method: request.method(),
-						body: request.postData(),
-					}
-				);
-				const preview = await response.json();
-				let responseBody;
-				if ( 'Embed Handler' === preview.provider_name ) {
-					// If Vimeo is down, that would make this test fail. So if we get a
-					// response from 'Embed Handler' instead of 'Vimeo', respond with a mock
-					// response, so we don't hold up development while Vimeo is down.
-					responseBody = MOCK_VIMEO_RESPONSE;
-				} else {
-					// Remove the first src attribute. This stops the Vimeo iframe loading the actual
-					// embedded content, but the height and width are preserved so layout related
-					// attributes, like aspect ratio CSS classes, remain the same.
-					preview.html = preview.html.replace( /src=[^\s]+/, '' );
-					responseBody = preview;
-				}
-				request.respond( {
-					content: 'application/json',
-					body: JSON.stringify( responseBody ),
-				} );
-			} else {
-				request.continue();
-			}
-		} );
-
+		setUpResponseMocking( [
+			{
+				match: matchURL( 'oembed%2F1.0%2Fproxy' ),
+				onRequestMatch: mockOrTransform( couldNotBePreviewed, MOCK_VIMEO_RESPONSE, stripIframeFromEmbed ),
+			},
+		] );
 		await visitAdmin( 'post-new.php', 'gutenberg-demo' );
 	} );
 
