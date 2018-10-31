13d3e588e082300a9562099eb7059b2d2af1b4b2
Andrés <andres.maneiro@automattic.com>
2018-10-31 14:02:22 +0100

    Make anchor always render after Additional CSS class

--- packages/editor/src/hooks/index.js
+++ packages/editor/src/hooks/index.js
@@ -2,7 +2,7 @@
  * Internal dependencies
  */
 import './align';
-import './anchor';
 import './custom-class-name';
+import './anchor';
 import './default-autocompleters';
 import './generated-class-name';
