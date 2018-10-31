b7d8fbfd0b97d579f732d9fc8f026e2919c2642d
Dennis Snell <dennis.snell@automattic.com>
2018-10-31 14:12:33 -0700

    Parser: Runs all parser implementations against the same tests (#11320)
    So far we haven't added too many tests to the parsers but the ones we
    _have_ added are in separate places and each implementation runs against
    a different set of tests.
    
    In this patch we're creating `shared-tests.js` in the spec parser that
    defines a suite of conformance tests for the parser and then we're using
    that base test-builder to dynamically create test suites for each
    implementation such that they all run the same suite.
    
    It's probably easier to understand by reading the code than this
    summary.
    
    Of note: by calling to `php` directly we're able to run the PHP parsers
    against the same tests as we are the JavaScript implementations. We
    should be able to do this for any implementation as long as the required
    binaries are available in the CI environment (Rust, for example).

--- packages/block-serialization-spec-parser/test/index.js
+++ packages/block-serialization-spec-parser/test/index.js
@@ -1,14 +1,13 @@
 /**
+ * External dependencies
+ */
+import path from 'path';
+
+/**
  * Internal dependencies
  */
-import { parse } from '../';
+import { jsTester, phpTester, parse } from '../';
 
-describe( 'block-serialization-spec-parser', () => {
-	test( 'parse() works properly', () => {
-		const result = parse(
-			'<!-- wp:core/more --><!--more--><!-- /wp:core/more -->'
-		);
+describe( 'block-serialization-spec-parser-js', jsTester( parse ) );
 
-		expect( result ).toMatchSnapshot();
-	} );
-} );
+phpTester( 'block-serialization-spec-parser-php', path.join( __dirname, 'test-parser.php' ) );
