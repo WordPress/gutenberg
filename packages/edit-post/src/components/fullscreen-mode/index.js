4baf6322bfef0976b888fe78fee1a065a92acbe5
John Godley <johng75@gmail.com>
2018-10-31 14:30:17 +0000

    Remove sticky-menu class preventing full admin page scroll
    Gutenberg adds ‘is-fullscreen-mode’ to the page body class, causing WordPress to add ‘sticky-menu’. This prevents the page being vertically scrolled, cutting off long admin menus.
    
    Remove ‘sticky-menu’ as part of the FullscreenMode setup

--- packages/edit-post/src/components/fullscreen-mode/index.js
+++ packages/edit-post/src/components/fullscreen-mode/index.js
@@ -7,6 +7,12 @@ import { withSelect } from '@wordpress/data';
 class FullscreenMode extends Component {
 	componentDidMount() {
 		this.sync();
+
+		// `is-fullscreen-mode` is set in PHP as a body class by Gutenberg, and this causes
+		// `sticky-menu` to be applied by WordPress and prevents the admin menu being scrolled
+		// even if `is-fullscreen-mode` is then removed. Let's remove `sticky-menu` here as
+		// a consequence of the FullscreenMode setup
+		document.body.classList.remove( 'sticky-menu' );
 	}
 
 	componentDidUpdate( prevProps ) {
