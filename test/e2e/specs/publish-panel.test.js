43ca9a5770ce674770814bf51b3ace6c890f2bcf
Andrés <andres.maneiro@automattic.com>
2018-11-05 10:42:17 +0100

    Simplify tests

--- test/e2e/specs/publish-panel.test.js
+++ test/e2e/specs/publish-panel.test.js
@@ -8,39 +8,37 @@ import {
 } from '../support/utils';
 
 describe( 'PostPublishPanel', () => {
-	[ 'post', 'page' ].forEach( ( postType ) => {
-		let werePrePublishChecksEnabled;
-		beforeEach( async () => {
-			await newPost( postType );
-			werePrePublishChecksEnabled = await arePrePublishChecksEnabled();
-			if ( ! werePrePublishChecksEnabled ) {
-				await enablePrePublishChecks();
-			}
+	let werePrePublishChecksEnabled;
+	beforeEach( async () => {
+		await newPost( );
+		werePrePublishChecksEnabled = await arePrePublishChecksEnabled();
+		if ( ! werePrePublishChecksEnabled ) {
+			await enablePrePublishChecks();
+		}
+	} );
+
+	afterEach( async () => {
+		if ( ! werePrePublishChecksEnabled ) {
+			await disablePrePublishChecks();
+		}
+	} );
+
+	it( 'publish button should have focus', async () => {
+		await openPublishPanel();
+
+		const focusedElementClassList = await page.$eval( ':focus', ( focusedElement ) => {
+			return Object.values( focusedElement.classList );
 		} );
+		expect( focusedElementClassList ).toContain( 'editor-post-publish-button' );
+	} );
 
-		afterEach( async () => {
-			if ( ! werePrePublishChecksEnabled ) {
-				await disablePrePublishChecks();
-			}
+	it( 'should retain focus within the panel', async () => {
+		await openPublishPanel();
+		await pressWithModifier( 'Shift', 'Tab' );
+
+		const focusedElementClassList = await page.$eval( ':focus', ( focusedElement ) => {
+			return Object.values( focusedElement.classList );
 		} );
-
-		it( 'publish button should have focus', async () => {
-			await openPublishPanel();
-
-			const focusedElementClassList = await page.$eval( ':focus', ( focusedElement ) => {
-				return Object.values( focusedElement.classList );
-			} );
-			expect( focusedElementClassList ).toContain( 'editor-post-publish-button' );
-		} );
-
-		it( 'should retain focus within the panel', async () => {
-			await openPublishPanel();
-			await pressWithModifier( 'Shift', 'Tab' );
-
-			const focusedElementClassList = await page.$eval( ':focus', ( focusedElement ) => {
-				return Object.values( focusedElement.classList );
-			} );
-			expect( focusedElementClassList ).toContain( 'components-checkbox-control__input' );
-		} );
+		expect( focusedElementClassList ).toContain( 'components-checkbox-control__input' );
 	} );
 } );
