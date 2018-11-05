c8fde1821bad2f043b35c27ea1d7ad5c2b17feb1
Andrea Sciamanna <andrea.s@onthegosystems.com>
2018-11-05 09:59:35 +0100

    Pass the current post IF to the filter

--- packages/block-library/src/block/index.php
+++ packages/block-library/src/block/index.php
@@ -17,14 +17,20 @@ function render_block_core_block( $attributes ) {
 		return '';
 	}
 
+	$post_id = null;
+	if ( get_post() ) {
+		$post_id = get_post()->ID;
+	}
+
 	/**
 	 * Filter to allow plugins to override the reusable block id.
 	 *
 	 * @since 3.5.0
 	 *
-	 * @param int $block_id     The id of the reusable block
+	 * @param int $block_id The id of the reusable block.
+	 * @param int $post_id  The post ID that the reusable block is being embedding in.
 	 */
-	$reusable_block_id = apply_filters( 'reusable_block_id', $attributes['ref'] );
+	$reusable_block_id = apply_filters( 'reusable_block_id', $attributes['ref'], $post_id );
 	$reusable_block    = get_post( $reusable_block_id );
 	if ( ! $reusable_block || 'wp_block' !== $reusable_block->post_type ) {
 		return '';
