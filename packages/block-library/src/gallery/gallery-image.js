14278906c763f14482ecb81555b7bde437f5c1a5
Edwin Cromley <cromleyedwin89@gmail.com>
2018-11-05 13:35:20 -0500

    Add aria label to images in Gallery block during edit mode. (#11466)
    * Add aria-label attribute specifying position of image in gallery block.
    * Update changelog text for block library Gallery Block.
    * Add translators comment for gallery image aria label string.

--- packages/block-library/src/gallery/gallery-image.js
+++ packages/block-library/src/gallery/gallery-image.js
@@ -86,7 +86,7 @@ class GalleryImage extends Component {
 	}
 
 	render() {
-		const { url, alt, id, linkTo, link, isSelected, caption, onRemove, setAttributes } = this.props;
+		const { url, alt, id, linkTo, link, isSelected, caption, onRemove, setAttributes, 'aria-label': ariaLabel } = this.props;
 
 		let href;
 
@@ -102,7 +102,7 @@ class GalleryImage extends Component {
 		// Disable reason: Image itself is not meant to be
 		// interactive, but should direct image selection and unfocus caption fields
 		// eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-		const img = url ? <img src={ url } alt={ alt } data-id={ id } onClick={ this.onImageClick } tabIndex="0" onKeyDown={ this.onImageClick } /> : <Spinner />;
+		const img = url ? <img src={ url } alt={ alt } data-id={ id } onClick={ this.onImageClick } tabIndex="0" onKeyDown={ this.onImageClick } aria-label={ ariaLabel } /> : <Spinner />;
 
 		const className = classnames( {
 			'is-selected': isSelected,
