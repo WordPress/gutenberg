5c9a7b5304a264cc9a165b3fa4c6ce10c5a8539f
Marko Savic <savicmarko1985@gmail.com>
2018-11-06 12:24:32 +0100

    Create edit.native.js file to bypass passing className from original one

---
+++ packages/editor/src/components/block-edit/edit.native.js
@@ -0,0 +1,28 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { withFilters } from '@wordpress/components';
+import { getBlockDefaultClassName, hasBlockSupport, getBlockType } from '@wordpress/blocks';
+
+export const Edit = ( props ) => {
+	const { attributes = {}, name } = props;
+	const blockType = getBlockType( name );
+
+	if ( ! blockType ) {
+		return null;
+	}
+
+	// `edit` and `save` are functions or components describing the markup
+	// with which a block is displayed. If `blockType` is valid, assign
+	// them preferentially as the render value for the block.
+	const Component = blockType.edit || blockType.save;
+
+	return <Component { ...props } />;
+};
+
+export default withFilters( 'editor.BlockEdit' )( Edit );
