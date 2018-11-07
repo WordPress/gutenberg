57a7405a89dbd6ad7763aa04dcd2aabdd26b25b9
Tugdual de Kerviler <dekervit@gmail.com>
2018-11-07 11:22:11 +0100

    Expose @wordpress/editor to Gutenberg mobile (#11486)
    * Make the Editor component work with native
    
    * Define @wordpress/date entrypoint for react-native
    
    * Expose BlockEdit to gutenberg mobile
    
    * Add withFilter back and remove className omit in BlockEdit

--- packages/components/src/index.native.js
+++ packages/components/src/index.native.js
@@ -3,3 +3,6 @@ export * from './primitives';
 export { default as Dashicon } from './dashicon';
 export { default as Toolbar } from './toolbar';
 export { default as withSpokenMessages } from './higher-order/with-spoken-messages';
+
+// Higher-Order Components
+export { default as withFilters } from './higher-order/with-filters';
--- packages/editor/src/components/index.native.js
+++ packages/editor/src/components/index.native.js
@@ -3,3 +3,4 @@ export * from './font-sizes';
 export { default as PlainText } from './plain-text';
 export { default as RichText } from './rich-text';
 export { default as MediaPlaceholder } from './media-placeholder';
+export { default as BlockEdit } from './block-edit';
