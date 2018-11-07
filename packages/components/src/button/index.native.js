47124182e80e7631de70dd36ada555bd79dc6c32
Marko Savic <savicmarko1985@gmail.com>
2018-11-07 12:40:33 +0100

    Fix style little bit

--- packages/components/src/button/index.native.js
+++ packages/components/src/button/index.native.js
@@ -12,9 +12,9 @@ export default function Button( props ) {
 			onPress={ onClick }
 			style={ { borderColor: ariaPressed ? 'black' : 'white', borderWidth: 1, borderRadius: 2 } }
 		>
-			<View style={ { height: 44, width: 44, flexDirection: 'row' } }>
+			<View style={ { height: 44, width: 44, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' } }>
 				{ children }
-				{ subscript && ( <Text style={ { fontVariant: [ 'small-caps' ], textAlignVertical: 'bottom' } }>{ subscript }</Text> ) }
+				{ subscript && ( <Text style={ { fontVariant: [ 'small-caps' ] } }>{ subscript }</Text> ) }
 			</View>
 		</TouchableOpacity>
 	);
