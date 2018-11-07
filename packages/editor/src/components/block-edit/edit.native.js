68b236185527d56b3b1eed64c3c01ce429542e53
Tugdual de Kerviler <dekervit@gmail.com>
2018-11-07 10:13:02 +0100

    Add withFilter back and remove className omit in BlockEdit

--- packages/editor/src/components/block-edit/edit.native.js
+++ packages/editor/src/components/block-edit/edit.native.js
@@ -1,31 +1,22 @@
 /**
- * External dependencies
- */
-import { omit } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import { withFilters } from '@wordpress/components';
 import { getBlockType } from '@wordpress/blocks';
 
 export const Edit = ( props ) => {
-	const { attributes = {}, name } = props;
+	const { name } = props;
 	const blockType = getBlockType( name );
 
 	if ( ! blockType ) {
 		return null;
 	}
 
-	const style = props.style || attributes.style;
 	const Component = blockType.edit;
 
 	return (
-		<Component
-			{ ...omit( props, [ 'className' ] ) }
-			style={ style }
-		/>
+		<Component { ...props } />
 	);
 };
 
-export default Edit;
+export default withFilters( 'editor.BlockEdit' )( Edit );
