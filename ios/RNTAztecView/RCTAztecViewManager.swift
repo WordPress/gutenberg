57091493611f34bde39ddec1b14402a4d1d94843
etoledom <etoledom@icloud.com>
2018-11-09 12:21:41 -0300

    Recover MediaProvider.

--- ios/RNTAztecView/RCTAztecViewManager.swift
+++ ios/RNTAztecView/RCTAztecViewManager.swift
@@ -7,18 +7,8 @@ public class RCTAztecViewManager: RCTViewManager {
 // NOTE:
 // Removing temporarily images and attachment handling in favour of a simpler Bridge implementation.
 
-//    var attachmentDelegate: Aztec.TextViewAttachmentDelegate
-//    var imageProvider: Aztec.TextViewAttachmentImageProvider
-//
-//    /// Customize the initializer to set up the Aztec delegate methods.
-//    /// Then the example app should implement RCTBridgeDelegate
-//    /// Then the example app should initialize this class and pass it to the bridge (since this class inherits from RCTBridgeModule
-//    public required init(attachmentDelegate: Aztec.TextViewAttachmentDelegate, imageProvider: TextViewAttachmentImageProvider) {
-//        self.attachmentDelegate = attachmentDelegate
-//        self.imageProvider = imageProvider
-//
-//        super.init()
-//    }
+    public var attachmentDelegate: Aztec.TextViewAttachmentDelegate?
+    public var imageProvider: Aztec.TextViewAttachmentImageProvider?
 
     public override static func requiresMainQueueSetup() -> Bool {
         return true
@@ -32,8 +22,11 @@ public class RCTAztecViewManager: RCTViewManager {
             defaultMissingImage: UIImage())
 
         view.isScrollEnabled = false
-//        view.textAttachmentDelegate = attachmentDelegate
-//        view.registerAttachmentImageProvider(imageProvider)
+
+        view.textAttachmentDelegate = attachmentDelegate
+        if let imageProvider = imageProvider {
+            view.registerAttachmentImageProvider(imageProvider)
+        }
 
         return view
     }
