3ac0e183faaa7ccb3611ee0c34f8bcacee1f392c
Vadim Nicolai <nicolai.vadim@gmail.com>
2018-11-14 14:52:54 +0200

    Replaced pressWithModifier with rawShortcut.

--- test/e2e/support/utils/login.js
+++ test/e2e/support/utils/login.js
@@ -1,16 +1,16 @@
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
-import { pressWithModifier } from './press-with-modifier';
-import { META_KEY } from './meta-key';
+import { rawShortcut } from '@wordpress/keycodes';
+
 import { WP_USERNAME, WP_PASSWORD } from './config';
 
 export async function login( username = WP_USERNAME, password = WP_PASSWORD ) {
 	await page.focus( '#user_login' );
-	await pressWithModifier( META_KEY, 'a' );
+	await page.keyboard.down( rawShortcut.primary( 'a' ) );
 	await page.type( '#user_login', username );
 	await page.focus( '#user_pass' );
-	await pressWithModifier( META_KEY, 'a' );
+	await page.keyboard.down( rawShortcut.primary( 'a' ) );
 	await page.type( '#user_pass', password );
 
 	await Promise.all( [ page.waitForNavigation(), page.click( '#wp-submit' ) ] );
