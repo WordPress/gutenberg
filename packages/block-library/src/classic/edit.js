07256f7b1ea13b4fac0df3cbe9eb89a099477134
Andrew Ozz <azaozz@users.noreply.github.com>
2018-11-14 15:01:30 +0200

    Restore the help modal in the classic block
    It has some duplicates with the main help modal, but many things are specific for the classic block. Also, can be "tweaked" specificlly for the classic block in the `wordpress` MCE plugin (in core).

--- packages/block-library/src/classic/edit.js
+++ packages/block-library/src/classic/edit.js
@@ -79,13 +79,6 @@ export default class ClassicEdit extends Component {
 
 		this.editor = editor;
 
-		// Disable TinyMCE's keyboard shortcut help.
-		editor.on( 'BeforeExecCommand', ( event ) => {
-			if ( event.command === 'WP_Help' ) {
-				event.preventDefault();
-			}
-		} );
-
 		if ( content ) {
 			editor.on( 'loadContent', () => editor.setContent( content ) );
 		}
