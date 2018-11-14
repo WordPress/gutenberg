9e74a5f094a55bbce158bea7e47a87dfb18d6d73
Tim Wright <timwright12@gmail.com>
2018-11-14 11:23:06 -0500

    Fix/issue 1387 sidebar icons nvda (#11816)
    * added hiding span around sidebar icons
    
    * updated comment
    
    * chore: Fix tests

--- packages/components/src/panel/body.js
+++ packages/components/src/panel/body.js
@@ -51,16 +51,22 @@ export class PanelBody extends Component {
 							onClick={ this.toggle }
 							aria-expanded={ isOpened }
 						>
-							{ isOpened ?
-								<SVG className="components-panel__arrow" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<G><Path fill="none" d="M0,0h24v24H0V0z" /></G>
-									<G><Path d="M12,8l-6,6l1.41,1.41L12,10.83l4.59,4.58L18,14L12,8z" /></G>
-								</SVG> :
-								<SVG className="components-panel__arrow" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<G><Path fill="none" d="M0,0h24v24H0V0z" /></G>
-									<G><Path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" /></G>
-								</SVG>
-							}
+							{ /*
+								Firefox + NVDA don't announce aria-expanded because the browser
+								repaints the whole element, so this wrapping span hides that.
+							*/ }
+							<span aria-hidden="true">
+								{ isOpened ?
+									<SVG className="components-panel__arrow" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<G><Path fill="none" d="M0,0h24v24H0V0z" /></G>
+										<G><Path d="M12,8l-6,6l1.41,1.41L12,10.83l4.59,4.58L18,14L12,8z" /></G>
+									</SVG> :
+									<SVG className="components-panel__arrow" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+										<G><Path fill="none" d="M0,0h24v24H0V0z" /></G>
+										<G><Path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" /></G>
+									</SVG>
+								}
+							</span>
 							{ title }
 							{ icon && <Icon icon={ icon } className="components-panel__icon" size={ 20 } /> }
 						</Button>
