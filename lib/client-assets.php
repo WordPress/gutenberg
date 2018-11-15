cb6a505958d5d1a95b3515a0fb863018ddd33760
George Hotelling <george.hotelling@automattic.com>
2018-11-15 15:58:34 -0500

    Add date formats to moment

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -262,8 +262,16 @@ function gutenberg_register_scripts_and_styles() {
 					'weekdays'      => array_values( $wp_locale->weekday ),
 					'weekdaysShort' => array_values( $wp_locale->weekday_abbrev ),
 					'week'          => array(
-						'dow' => (int) get_option( 'start_of_week', 0 )
-					)
+						'dow' => (int) get_option( 'start_of_week', 0 ),
+					),
+					'longDateFormat' => array(
+						'LT' => get_option( 'time_format', __( 'g:i a', 'default' ) ),
+						'LTS' => null,
+						'L' => null,
+						'LL' => get_option( 'date_format', __( 'F j, Y', 'default' ) ),
+						'LLL' => __( 'F j, Y g:i a', 'default' ),
+						'LLLL' => null,
+					),
 				)
 			)
 		),
