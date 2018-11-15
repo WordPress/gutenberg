33b4cb3ed8f8b31929cc6c42e5b964a8b5ce89bc
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-11-15 16:20:03 +0100

    Edit post: Remove old action handler for removed panels in preferences

--- packages/edit-post/src/store/reducer.js
+++ packages/edit-post/src/store/reducer.js
@@ -73,17 +73,6 @@ export const preferences = combineReducers( {
 					},
 				};
 			}
-
-			case 'REMOVE_PANEL': {
-				const { panelName } = action;
-				return {
-					...state,
-					[ panelName ]: {
-						...state[ panelName ],
-						removed: true,
-					},
-				};
-			}
 		}
 
 		return state;
