e7a19ebe0769bfb5b7b4b5050dc568d0a16993e9
Marcus Kazmierczak <marcus@mkaz.com>
2018-11-14 16:22:02 -0800

    Switch translation name to not be specific to Noto

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -758,24 +758,19 @@ function gutenberg_register_scripts_and_styles() {
 	// Editor Styles.
 	// This empty stylesheet is defined to ensure backwards compatibility.
 	gutenberg_override_style( 'wp-blocks', false );
-
 	$fonts_url = '';
 
 	/*
-	 * Translators: If there are characters in your language that are not supported
-	 * by a Noto font, translate this to 'off'. Do not translate into your own language.
+	 * Translators: Use this to specify the proper Google Font name and variants
+	 * to load that is supported by your language. Do not translate.
+	 * Set to 'off' to disable loading.
 	 */
-	if ( 'off' !== _x( 'on', 'Noto Font: on or off', 'gutenberg' ) ) {
-		/*
-		 * Translators: Use this to specify the proper Noto font family that is
-		 * supported by your language. This must correspond to a Google Font.
-		 */
-		$font_family = _x( 'Noto Serif:400,400i,700,700i', 'Google Noto Font', 'gutenberg' );
-		$query_args  = array(
+	$font_family = _x( 'Noto Serif:400,400i,700,700i', 'Google Font Name and Variants', 'gutenberg' );
+	if ( 'off' !== $font_family ) {
+		$query_args = array(
 			'family' => urlencode( $font_family ),
 		);
-
-		$fonts_url = esc_url_raw( add_query_arg( $query_args, 'https://fonts.googleapis.com/css' ) );
+		$fonts_url  = esc_url_raw( add_query_arg( $query_args, 'https://fonts.googleapis.com/css' ) );
 	}
 
 	gutenberg_override_style(
