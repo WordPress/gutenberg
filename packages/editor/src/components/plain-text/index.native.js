61c3afb1a882fae4a4c8c0e308a72b9a4b19c959
Mario Zorz <mariozorz@gmail.com>
2018-11-15 19:00:21 -0300

    handling isSelected in PlainText primitive through a deferred call to focus() on React Native TextInput

--- packages/editor/src/components/plain-text/index.native.js
+++ packages/editor/src/components/plain-text/index.native.js
@@ -9,8 +9,12 @@ import { TextInput } from 'react-native';
 import styles from './style.scss';
 
 function PlainText( { onChange, className, ...props } ) {
+	if (props.isSelected === true) {
+		setTimeout(() => this._input.focus(), 250);
+	}
 	return (
 		<TextInput
+			ref={(x) => this._input = x}
 			className={ [ styles[ 'editor-plain-text' ], className ] }
 			onChangeText={ ( text ) => onChange( text ) }
 			{ ...props }
