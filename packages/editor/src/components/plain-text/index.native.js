0578bc4f80c207ac80eea8b36ad1e4b41f5c6eb8
Mario Zorz <mariozorz@gmail.com>
2018-11-16 14:10:50 -0300

    fixed lint warnings

--- packages/editor/src/components/plain-text/index.native.js
+++ packages/editor/src/components/plain-text/index.native.js
@@ -4,30 +4,35 @@
 import { TextInput } from 'react-native';
 
 /**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
 import styles from './style.scss';
 
-export default class PlainText extends React.Component {
+export default class PlainText extends Component {
 	componentDidMount() {
 		// if isSelected is true, we should request the focus on this TextInput
-		if ( (this._input.isFocused() === false) && (this._input.props.isSelected === true) ) {
+		if ( ( this._input.isFocused() === false ) && ( this._input.props.isSelected === true ) ) {
 			this.focus();
 		}
 	}
 
-	focus () {
-	  this._input && this._input.focus()
+	focus() {
+		this._input.focus();
 	}
 
-	render () {
-	  return (
-		<TextInput
-			ref={(x) => this._input = x}
-			className={ [ styles[ 'editor-plain-text' ], this.props.className ] }
-			onChangeText={ ( text ) => this.props.onChange( text ) }
-			{...this.props}
-		/>
-	  )
+	render() {
+		return (
+			<TextInput
+				ref={ ( x ) => this._input = x }
+				className={ [ styles[ 'editor-plain-text' ], this.props.className ] }
+				onChangeText={ ( text ) => this.props.onChange( text ) }
+				{ ...this.props }
+			/>
+		);
 	}
 }
