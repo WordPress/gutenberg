c7e0119341784787b8fde1ef438b4148bd2c4258
Mario Zorz <mariozorz@gmail.com>
2018-11-16 12:07:52 -0300

    promoted PlainText native implentation to extend Component and reuse ReactNative lifecycle

--- packages/editor/src/components/plain-text/index.native.js
+++ packages/editor/src/components/plain-text/index.native.js
@@ -8,18 +8,26 @@ import { TextInput } from 'react-native';
  */
 import styles from './style.scss';
 
-function PlainText( { onChange, className, ...props } ) {
-	if (props.isSelected === true) {
-		setTimeout(() => this._input.focus(), 250);
+export default class PlainText extends React.Component {
+	componentDidMount() {
+		// if isSelected is true, we should request the focus on this TextInput
+		if ( (this._input.isFocused() === false) && (this._input.props.isSelected === true) ) {
+			this.focus();
+		}
 	}
-	return (
+
+	focus () {
+	  this._input && this._input.focus()
+	}
+
+	render () {
+	  return (
 		<TextInput
 			ref={(x) => this._input = x}
-			className={ [ styles[ 'editor-plain-text' ], className ] }
-			onChangeText={ ( text ) => onChange( text ) }
-			{ ...props }
+			className={ [ styles[ 'editor-plain-text' ], this.props.className ] }
+			onChangeText={ ( text ) => this.props.onChange( text ) }
+			{...this.props}
 		/>
-	);
+	  )
+	}
 }
-
-export default PlainText;
