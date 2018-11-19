526b9568fe058d8ccd4e845e4d43fb487011e058
Jorge <jorge.costa@developer.pt>
2018-11-19 12:57:59 +0000

    Add deprecation logic.

--- packages/block-library/src/gallery/index.js
+++ packages/block-library/src/gallery/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { filter, every, map } from 'lodash';
+import { filter, every, map, some } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -220,6 +220,66 @@ export const settings = {
 	},
 
 	deprecated: [
+		{
+			attributes: blockAttributes,
+			isEligible( { images, ids } ) {
+				return images &&
+					images.length > 0 &&
+					(
+						( ! ids && images ) ||
+						( ids && images && ids.length !== images.length ) ||
+						some( images, ( id, index ) => {
+							if ( ! id && ids[ index ] !== null ) {
+								return true;
+							}
+							return parseInt( id, 10 ) !== ids[ index ];
+						} )
+					);
+			},
+			migrate( attributes ) {
+				return {
+					...attributes,
+					ids: map( attributes.images, ( { id } ) => {
+						if ( ! id ) {
+							return null;
+						}
+						return parseInt( id, 10 );
+					} ),
+				};
+			},
+			save( { attributes } ) {
+				const { images, columns = defaultColumnsNumber( attributes ), imageCrop, linkTo } = attributes;
+				return (
+					<ul className={ `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` } >
+						{ images.map( ( image ) => {
+							let href;
+
+							switch ( linkTo ) {
+								case 'media':
+									href = image.url;
+									break;
+								case 'attachment':
+									href = image.link;
+									break;
+							}
+
+							const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ image.id ? `wp-image-${ image.id }` : null } />;
+
+							return (
+								<li key={ image.id || image.url } className="blocks-gallery-item">
+									<figure>
+										{ href ? <a href={ href }>{ img }</a> : img }
+										{ image.caption && image.caption.length > 0 && (
+											<RichText.Content tagName="figcaption" value={ image.caption } />
+										) }
+									</figure>
+								</li>
+							);
+						} ) }
+					</ul>
+				);
+			},
+		},
 		{
 			attributes: blockAttributes,
 			save( { attributes } ) {
