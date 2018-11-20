cf50aecd899ac5ae79a2e7a4c79e0b73b074477e
Andrew Duthie <andrew@andrewduthie.com>
2018-11-20 10:42:58 -0500

    Blocks: Add deprecation for attribute-sourced URL, alt

--- packages/block-library/src/image/index.js
+++ packages/block-library/src/image/index.js
@@ -118,6 +118,69 @@ const schema = {
 	},
 };
 
+function save( { attributes } ) {
+	const {
+		url,
+		alt,
+		caption,
+		align,
+		href,
+		rel,
+		linkClass,
+		width,
+		height,
+		id,
+		linkTarget,
+	} = attributes;
+
+	const classes = classnames( {
+		[ `align${ align }` ]: align,
+		'is-resized': width || height,
+	} );
+
+	const image = (
+		<img
+			src={ url }
+			alt={ alt }
+			className={ id ? `wp-image-${ id }` : null }
+			width={ width }
+			height={ height }
+		/>
+	);
+
+	const figure = (
+		<Fragment>
+			{ href ? (
+				<a
+					className={ linkClass }
+					href={ href }
+					target={ linkTarget }
+					rel={ rel }
+				>
+					{ image }
+				</a>
+			) : image }
+			{ ! RichText.isEmpty( caption ) && <RichText.Content tagName="figcaption" value={ caption } /> }
+		</Fragment>
+	);
+
+	if ( 'left' === align || 'right' === align || 'center' === align ) {
+		return (
+			<div>
+				<figure className={ classes }>
+					{ figure }
+				</figure>
+			</div>
+		);
+	}
+
+	return (
+		<figure className={ classes }>
+			{ figure }
+		</figure>
+	);
+}
+
 export const settings = {
 	title: __( 'Image' ),
 
@@ -244,68 +307,7 @@ export const settings = {
 
 	edit,
 
-	save( { attributes } ) {
-		const {
-			url,
-			alt,
-			caption,
-			align,
-			href,
-			rel,
-			linkClass,
-			width,
-			height,
-			id,
-			linkTarget,
-		} = attributes;
-
-		const classes = classnames( {
-			[ `align${ align }` ]: align,
-			'is-resized': width || height,
-		} );
-
-		const image = (
-			<img
-				src={ url }
-				alt={ alt }
-				className={ id ? `wp-image-${ id }` : null }
-				width={ width }
-				height={ height }
-			/>
-		);
-
-		const figure = (
-			<Fragment>
-				{ href ? (
-					<a
-						className={ linkClass }
-						href={ href }
-						target={ linkTarget }
-						rel={ rel }
-					>
-						{ image }
-					</a>
-				) : image }
-				{ ! RichText.isEmpty( caption ) && <RichText.Content tagName="figcaption" value={ caption } /> }
-			</Fragment>
-		);
-
-		if ( 'left' === align || 'right' === align || 'center' === align ) {
-			return (
-				<div>
-					<figure className={ classes }>
-						{ figure }
-					</figure>
-				</div>
-			);
-		}
-
-		return (
-			<figure className={ classes }>
-				{ figure }
-			</figure>
-		);
-	},
+	save,
 
 	deprecated: [
 		{
@@ -381,6 +383,25 @@ export const settings = {
 					</figure>
 				);
 			},
+		},
+		{
+			attributes: {
+				...blockAttributes,
+				url: {
+					type: 'string',
+					source: 'attribute',
+					selector: 'img',
+					attribute: 'src',
+				},
+				alt: {
+					type: 'string',
+					source: 'attribute',
+					selector: 'img',
+					attribute: 'alt',
+					default: '',
+				},
+			},
+			save,
 		},
 	],
 };
