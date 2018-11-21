c328570eb83ac3b8e6dae6509367a5a3ceee4fa8
Jorge Bernal <jbernal@gmail.com>
2018-11-21 10:23:48 +0100

    Store 3rd party podspecs in the repo

---
+++ bin/generate-podspecs.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Exit if any command fails
+set -e
+
+# Change to the expected directory.
+cd "$( dirname $0 )"
+cd ..
+
+# Check for cocoapods
+command -v pod > /dev/null || ( echo Cocoapods is required to generate podspecs; exit 1 )
+
+PODSPECS=$(cat <<EOP
+node_modules/react-native/React.podspec
+node_modules/react-native/ReactCommon/yoga/yoga.podspec
+node_modules/react-native/third-party-podspecs/Folly.podspec
+node_modules/react-native/third-party-podspecs/DoubleConversion.podspec
+node_modules/react-native/third-party-podspecs/glog.podspec
+EOP
+)
+
+DEST="react-native-gutenberg-bridge/third-party-podspecs"
+
+for podspec in $PODSPECS
+do
+    pod=`basename $podspec .podspec`
+    echo "Generating podspec for $pod"
+    INSTALL_YOGA_WITHOUT_PATH_OPTION=1 pod ipc spec $podspec > "$DEST/$pod.podspec.json"
+done
