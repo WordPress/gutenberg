92aa900423082f7b1ebc73390efa48efa45e9f83
Vadim Nicolai <nicolai.vadim@gmail.com>
2018-11-22 10:41:35 +0200

    Adjusted arePrePublishChecksEnabled docs and returned boolean.

--- test/e2e/support/utils/are-pre-publish-checks-enabled.js
+++ test/e2e/support/utils/are-pre-publish-checks-enabled.js
@@ -1,6 +1,9 @@
 /**
  * Verify if publish checks are enabled.
+ * @return {boolean} Boolean which represents the state of prepublish checks.
  */
 export async function arePrePublishChecksEnabled() {
-	await page.evaluate( () => window.wp.data.select( 'core/editor' ).isPublishSidebarEnabled() );
+	return page.evaluate( () =>
+		window.wp.data.select( 'core/editor' ).isPublishSidebarEnabled()
+	);
 }
