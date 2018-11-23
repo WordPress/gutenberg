f6ad3086d82028dde9525773b63d842e13b1aa57
Tugdual de Kerviler <dekervit@gmail.com>
2018-11-23 15:32:59 +0100

    fix missing trailing comma

--- packages/components/src/button/index.native.js
+++ packages/components/src/button/index.native.js
@@ -64,7 +64,7 @@ export default function Button( props ) {
 	const isDisabled = ariaDisabled || disabled;
 	const buttonViewStyle = {
 		opacity: isDisabled ? 0.2 : 1,
-		...( ariaPressed ? styles.buttonActive : styles.buttonInactive )
+		...( ariaPressed ? styles.buttonActive : styles.buttonInactive ),
 	};
 
 	return (
