4422de271f6afa653b7df64ebed6571035e2eab8
Andrew Duthie <andrew@andrewduthie.com>
2018-11-28 12:37:32 -0500

    API Fetch: Add test to assure against throwing next function

--- packages/api-fetch/src/test/index.js
+++ packages/api-fetch/src/test/index.js
@@ -196,5 +196,6 @@ describe( 'apiFetch', () => {
 		expect( customFetchHandler ).toHaveBeenCalledWith( {
 			path: '/random?_locale=user',
 		}, expect.any( Function ) );
+		expect( customFetchHandler.mock.calls[ 0 ][ 1 ] ).not.toThrow();
 	} );
 } );
