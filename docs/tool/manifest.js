61b3683f3c8a395003fffc680384336c323ea74e
Dion Hulse <contact@dd32.id.au>
2018-11-30 15:31:17 +1100

    Docs Tools: Don't overwrite the pages defined in toc.json when generating manifest.json

--- docs/tool/manifest.js
+++ docs/tool/manifest.js
@@ -13,24 +13,15 @@ const baseRepoUrl = `https://raw.githubusercontent.com/WordPress/gutenberg/maste
  * @return {Array} Manifest
  */
 function getPackageManifest( packageFolderNames ) {
-	return [
-		{
-			title: 'Packages',
-			slug: 'packages',
-			markdown_source: `${ baseRepoUrl }/docs/designers-developers/developers/packages.md`,
-			parent: null,
-		},
-	].concat(
-		packageFolderNames.map( ( folderName ) => {
-			const path = `${ baseRepoUrl }/packages/${ folderName }/README.md`;
-			return {
-				title: `@wordpress/${ folderName }`,
-				slug: `packages-${ folderName }`,
-				markdown_source: path,
-				parent: 'packages',
-			};
-		} )
-	);
+	return packageFolderNames.map( ( folderName ) => {
+		const path = `${ baseRepoUrl }/packages/${ folderName }/README.md`;
+		return {
+			title: `@wordpress/${ folderName }`,
+			slug: `packages-${ folderName }`,
+			markdown_source: path,
+			parent: 'packages',
+		};
+	} );
 }
 
 /**
@@ -41,24 +32,15 @@ function getPackageManifest( packageFolderNames ) {
  * @return {Array} Manifest
  */
 function getComponentManifest( componentPaths ) {
-	return [
-		{
-			title: 'Components Package Reference',
-			slug: 'components',
-			markdown_source: `${ baseRepoUrl }/packages/components.md`,
-			parent: null,
-		},
-		...componentPaths
-			.map( ( filePath ) => {
-				const slug = nth( filePath.split( '/' ), -2 );
-				return {
-					title: upperFirst( camelCase( slug ) ),
-					slug,
-					markdown_source: `${ baseRepoUrl }/${ filePath }`,
-					parent: 'components',
-				};
-			} ),
-	];
+	return componentPaths.map( ( filePath ) => {
+		const slug = nth( filePath.split( '/' ), -2 );
+		return {
+			title: upperFirst( camelCase( slug ) ),
+			slug,
+			markdown_source: `${ baseRepoUrl }/${ filePath }`,
+			parent: 'components',
+		};
+	} );
 }
 
 /**
@@ -69,22 +51,15 @@ function getComponentManifest( componentPaths ) {
  * @return {Array} Manifest
  */
 function getDataManifest( parsedNamespaces ) {
-	return [ {
-		title: 'Data Package Reference',
-		slug: 'data',
-		markdown_source: `${ baseRepoUrl }/docs/designers-developers/developers/data/README.md`,
-		parent: null,
-	} ].concat(
-		Object.values( parsedNamespaces ).map( ( parsedNamespace ) => {
-			const slug = `data-${ kebabCase( parsedNamespace.name ) }`;
-			return {
-				title: parsedNamespace.title,
-				slug,
-				markdown_source: `${ baseRepoUrl }/docs/designers-developers/developers/data/${ slug }.md`,
-				parent: 'data',
-			};
-		} )
-	);
+	return Object.values( parsedNamespaces ).map( ( parsedNamespace ) => {
+		const slug = `data-${ kebabCase( parsedNamespace.name ) }`;
+		return {
+			title: parsedNamespace.title,
+			slug,
+			markdown_source: `${ baseRepoUrl }/docs/designers-developers/developers/data/${ slug }.md`,
+			parent: 'data',
+		};
+	} );
 }
 
 module.exports = {
