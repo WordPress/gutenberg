c9447c350f95d275acbea22c4d063adf4e8a66a4
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-12-04 17:51:00 +0100

    Refactor CopyHandler to be less dependent on withData implementation

--- packages/editor/src/components/copy-handler/index.js
+++ packages/editor/src/components/copy-handler/index.js
@@ -8,14 +8,21 @@ import { withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 
 class CopyHandler extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.onCopy = ( event ) => this.props.onCopy( event );
+		this.onCut = ( event ) => this.props.onCut( event );
+	}
+
 	componentDidMount() {
-		document.addEventListener( 'copy', this.props.onCopy );
-		document.addEventListener( 'cut', this.props.onCut );
+		document.addEventListener( 'copy', this.onCopy );
+		document.addEventListener( 'cut', this.onCut );
 	}
 
 	componentWillUnmount() {
-		document.removeEventListener( 'copy', this.props.onCopy );
-		document.removeEventListener( 'cut', this.props.onCut );
+		document.removeEventListener( 'copy', this.onCopy );
+		document.removeEventListener( 'cut', this.onCut );
 	}
 
 	render() {
@@ -33,31 +40,38 @@ export default compose( [
 		} = select( 'core/editor' );
 		const { removeBlocks } = dispatch( 'core/editor' );
 
-		const selectedBlockClientId = getSelectedBlockClientId();
-		const selectedBlockClientIds = selectedBlockClientId ? [ selectedBlockClientId ] : getMultiSelectedBlockClientIds();
+		const onCopy = ( event ) => {
+			const selectedBlockClientIds = getSelectedBlockClientId() ?
+				[ getSelectedBlockClientId() ] :
+				getMultiSelectedBlockClientIds();
+
+			if ( selectedBlockClientIds.length === 0 ) {
+				return;
+			}
+
+			// Let native copy behaviour take over in input fields.
+			if ( ! hasMultiSelection() && documentHasSelection() ) {
+				return;
+			}
+
+			const serialized = serialize( getBlocksByClientId( selectedBlockClientIds ) );
+
+			event.clipboardData.setData( 'text/plain', serialized );
+			event.clipboardData.setData( 'text/html', serialized );
+
+			event.preventDefault();
+		};
 
 		return {
-			onCopy( event ) {
-				if ( selectedBlockClientIds.length === 0 ) {
-					return;
-				}
-
-				// Let native copy behaviour take over in input fields.
-				if ( ! hasMultiSelection() && documentHasSelection() ) {
-					return;
-				}
-
-				const serialized = serialize( getBlocksByClientId( selectedBlockClientIds ) );
-
-				event.clipboardData.setData( 'text/plain', serialized );
-				event.clipboardData.setData( 'text/html', serialized );
-
-				event.preventDefault();
-			},
+			onCopy,
 			onCut( event ) {
-				this.onCopy( event );
+				onCopy( event );
 
 				if ( hasMultiSelection() ) {
+					const selectedBlockClientIds = getSelectedBlockClientId() ?
+						[ getSelectedBlockClientId() ] :
+						getMultiSelectedBlockClientIds();
+
 					removeBlocks( selectedBlockClientIds );
 				}
 			},
