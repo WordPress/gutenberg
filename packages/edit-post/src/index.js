598f53fa3b320d81e33d622f40fc145f8a866148
Matthew Riley MacPherson <hi@tofumatt.com>
2018-12-04 14:54:08 +0000

    Tweak: Remove redundant [warning] in warn call

--- packages/edit-post/src/index.js
+++ packages/edit-post/src/index.js
@@ -72,7 +72,7 @@ export function initializeEditor( id, postType, postId, settings, initialEdits )
 	const documentMode = document.compatMode === 'CSS1Compat' ? 'Standards' : 'Quirks';
 	if ( documentMode !== 'Standards' ) {
 		// eslint-disable-next-line no-console
-		console.warn( '[Warning] Your browser is using Quirks Mode. \nThis can cause rendering issues such as blocks overlaying meta boxes in the editor. Quirks Mode can be triggered by PHP errors or HTML code appearing before the opening <!DOCTYPE html>. Try checking the raw page source or your site\'s PHP error log and resolving errors there, removing any HTML before the doctype, or disabling plugins.' );
+		console.warn( "Your browser is using Quirks Mode. \nThis can cause rendering issues such as blocks overlaying meta boxes in the editor. Quirks Mode can be triggered by PHP errors or HTML code appearing before the opening <!DOCTYPE html>. Try checking the raw page source or your site's PHP error log and resolving errors there, removing any HTML before the doctype, or disabling plugins." );
 	}
 
 	dispatch( 'core/nux' ).triggerGuide( [
