387bb77b0f21c73399f71445f85ad9db91750d7f
Ella Van Dorpe <iseulde@automattic.com>
2018-12-06 11:17:29 +0100

    RichText: fix onSetup doc (#12607)

--- packages/editor/src/components/rich-text/index.js
+++ packages/editor/src/components/rich-text/index.js
@@ -134,12 +134,9 @@ export class RichText extends Component {
 	}
 
 	/**
-	 * Handles the onSetup event for the TinyMCE component.
+	 * Sets a reference to the TinyMCE editor instance.
 	 *
-	 * Will setup event handlers for the TinyMCE instance.
-	 * An `onSetup` function in the props will be called if it is present.
-	 *
-	 * @param {tinymce} editor The editor instance as passed by TinyMCE.
+	 * @param {Editor} editor The editor instance as passed by TinyMCE.
 	 */
 	onSetup( editor ) {
 		this.editor = editor;
