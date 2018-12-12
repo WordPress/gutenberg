4bd0c34c44928d91347b539de6e31a3ad5f00a9c
Danilo Ercoli <ercoli@gmail.com>
2018-12-12 16:17:41 +0100

    Make sure to properly compare empty text and undefined text variable before resetting the eventCount field.

--- packages/editor/src/components/rich-text/index.native.js
+++ packages/editor/src/components/rich-text/index.native.js
@@ -274,8 +274,8 @@ export class RichText extends Component {
 
 		// If the component is changed React side (undo/redo/merging/splitting/custom text actions)
 		// we need to make sure the native is updated as well
-		if ( nextProps.value &&
-			this.lastContent &&
+		if ( ( typeof nextProps.value !== 'undefined' ) &&
+			( typeof this.lastContent !== 'undefined' ) &&
 			nextProps.value !== this.lastContent ) {
 			this.lastEventCount = undefined; // force a refresh on the native side
 		}
