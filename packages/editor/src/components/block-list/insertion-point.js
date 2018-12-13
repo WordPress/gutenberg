03cdac4c8e19fd8a7912a0b34ab18307c58cbff9
Andrew Duthie <andrew@andrewduthie.com>
2018-12-13 16:40:49 -0500

    Editor: Show block inserter only when focused, hovered, opened

--- packages/editor/src/components/block-list/insertion-point.js
+++ packages/editor/src/components/block-list/insertion-point.js
@@ -6,8 +6,9 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { Component, createRef } from '@wordpress/element';
 import { withSelect } from '@wordpress/data';
+import { focus } from '@wordpress/dom';
 
 /**
  * Internal dependencies
@@ -17,12 +18,30 @@ import Inserter from '../inserter';
 class BlockInsertionPoint extends Component {
 	constructor() {
 		super( ...arguments );
-		this.state = {
-			isInserterFocused: false,
-		};
 
 		this.onBlurInserter = this.onBlurInserter.bind( this );
 		this.onFocusInserter = this.onFocusInserter.bind( this );
+		this.toggleIsInserterHovered = this.toggleIsInserterHovered.bind( this );
+		this.toggleIsInserterOpened = this.toggleIsInserterOpened.bind( this );
+
+		this.inserterWrapperRef = createRef();
+
+		this.state = {
+			isInserterFocused: false,
+			isInserterHovered: false,
+			isInserterOpened: false,
+		};
+	}
+
+	componentDidUpdate() {
+		if ( this.forceFocusToInserter ) {
+			delete this.forceFocusToInserter;
+
+			const tabbable = focus.tabbable.find( this.inserterWrapperRef.current )[ 0 ];
+			if ( tabbable ) {
+				tabbable.focus();
+			}
+		}
 	}
 
 	onFocusInserter( event ) {
@@ -31,24 +50,59 @@ class BlockInsertionPoint extends Component {
 		// insertion and conflicts with contextual toolbar placement.
 		event.stopPropagation();
 
+		if ( this.state.isInserterFocused ) {
+			return;
+		}
+
+		const isFocusOnInserterWrapper = event.target === event.currentTarget;
+		if ( isFocusOnInserterWrapper ) {
+			this.forceFocusToInserter = true;
+		}
+
 		this.setState( {
 			isInserterFocused: true,
 		} );
 	}
 
-	onBlurInserter() {
+	onBlurInserter( event ) {
+		if ( event.target === this.inserterWrapperRef.current ) {
+			return;
+		}
+
+		if ( ! this.state.isInserterFocused ) {
+			return;
+		}
+
 		this.setState( {
 			isInserterFocused: false,
 		} );
 	}
 
+	toggleIsInserterHovered( event ) {
+		this.setState( {
+			isInserterHovered: event.type === 'mouseenter',
+		} );
+	}
+
+	toggleIsInserterOpened( isOpen ) {
+		this.setState( {
+			isInserterOpened: isOpen,
+		} );
+	}
+
 	render() {
-		const { isInserterFocused } = this.state;
+		const {
+			isInserterFocused,
+			isInserterHovered,
+			isInserterOpened,
+		} = this.state;
 		const {
 			showInsertionPoint,
 			rootClientId,
 			insertIndex,
 		} = this.props;
+
+		const isInserterVisible = isInserterFocused || isInserterHovered || isInserterOpened;
 
 		return (
 			<div className="editor-block-list__insertion-point">
@@ -56,25 +110,25 @@ class BlockInsertionPoint extends Component {
 					<div className="editor-block-list__insertion-point-indicator" />
 				) }
 				<div
+					ref={ this.inserterWrapperRef }
 					onFocus={ this.onFocusInserter }
 					onBlur={ this.onBlurInserter }
-					// While ideally it would be enough to capture the
-					// bubbling focus event from the Inserter, due to the
-					// characteristics of click focusing of `button`s in
-					// Firefox and Safari, it is not reliable.
-					//
-					// See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
-					tabIndex={ -1 }
+					onMouseEnter={ this.toggleIsInserterHovered }
+					onMouseLeave={ this.toggleIsInserterHovered }
+					tabIndex={ 0 }
 					className={
 						classnames( 'editor-block-list__insertion-point-inserter', {
-							'is-visible': isInserterFocused,
+							'is-visible': isInserterVisible,
 						} )
 					}
 				>
-					<Inserter
-						rootClientId={ rootClientId }
-						index={ insertIndex }
-					/>
+					{ isInserterVisible && (
+						<Inserter
+							rootClientId={ rootClientId }
+							index={ insertIndex }
+							onToggle={ this.toggleIsInserterOpened }
+						/>
+					) }
 				</div>
 			</div>
 		);
