0cf6ee31222d187b9ebb8cad7a4afdfd5c63f10c
Tugdual de Kerviler <dekervit@gmail.com>
2018-12-14 00:07:26 +0100

    Rerender on selection change to update record

--- packages/editor/src/components/rich-text/index.native.js
+++ packages/editor/src/components/rich-text/index.native.js
@@ -248,10 +248,6 @@ export class RichText extends Component {
 			this.lastEventCount = undefined; // force a refresh on the native side
 		}
 
-		if ( nextState.start !== this.state.start || nextState.end !== this.state.end ) {
-			return false;
-		}
-
 		return true;
 	}
 
