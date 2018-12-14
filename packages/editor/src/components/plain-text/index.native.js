08bd3f2c925d6686c9cf9f29b3bfab7055037472
Danilo Ercoli <ercoli@gmail.com>
2018-12-14 19:42:48 +0100

    Remove the call that does `blur` the undelying native component when deselecting RichText or PlainText.

--- packages/editor/src/components/plain-text/index.native.js
+++ packages/editor/src/components/plain-text/index.native.js
@@ -21,12 +21,6 @@ export default class PlainText extends Component {
 		}
 	}
 
-	componentDidUpdate( prevProps ) {
-		if ( ! this.props.isSelected && prevProps.isSelected ) {
-			this._input.blur();
-		}
-	}
-
 	focus() {
 		this._input.focus();
 	}
