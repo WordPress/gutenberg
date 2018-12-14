cf8dc8e8013ba757f944199ae5836d75815e44c4
etoledom <etoledom@icloud.com>
2018-12-14 21:49:35 +0200

    [iOS]: Stripping paragrap attributes from heading blocks.

--- ios/RNTAztecView/HeadingBlockFormatHandler.swift
+++ ios/RNTAztecView/HeadingBlockFormatHandler.swift
@@ -1,28 +1,25 @@
 import Aztec
 
 struct HeadingBlockFormatHandler: BlockFormatHandler {
-    let level: Header.HeaderType
+    private let level: Header.HeaderType
+    private let paragraphFormatter = HTMLParagraphFormatter(placeholderAttributes: nil)
+    private let headerFormatter: HeaderFormatter
 
     init?(block: BlockModel) {
         guard let level = HeadingBlockFormatHandler.headerLevel(from: block.tag) else {
             return nil
         }
         self.level = level
+        headerFormatter = HeaderFormatter(headerLevel: level)
     }
 
     func forceTypingFormat(on textView: RCTAztecView) {
-        textView.text = ensureNonEmptyString(textView.text)
-        let attributes = textView.typingAttributesSwifted
-        let formatter = HeaderFormatter(headerLevel: level)
-        textView.typingAttributesSwifted = formatter.apply(to: attributes, andStore: nil)
-    }
+        var attributes = textView.typingAttributesSwifted
 
-    private func ensureNonEmptyString(_ content: String) -> String {
-        let zeroWithSpace = String(Character.Name.zeroWidthSpace.rawValue)
-        guard content.isEmpty else {
-            return content.replacingOccurrences(of: zeroWithSpace, with: "")
-        }
-        return content.appending(zeroWithSpace)
+        attributes = paragraphFormatter.remove(from: attributes)
+        attributes = headerFormatter.apply(to: attributes, andStore: nil)
+
+        textView.typingAttributesSwifted = attributes
     }
 
     private static func headerLevel(from levelString: String) -> Header.HeaderType? {
