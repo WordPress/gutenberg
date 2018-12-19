83f6c902596e9b297d6f56ee50d2492110b7d3fd
William Earnhardt <wearnhardt@gmail.com>
2018-12-19 09:47:27 -0500

    Fix PHPCS issues in e2e tests

--- test/e2e/test-plugins/inner-blocks-templates.php
+++ test/e2e/test-plugins/inner-blocks-templates.php
@@ -6,6 +6,7 @@
  *
  * @package gutenberg-test-inner-blocks-templates
  */
+
 wp_enqueue_script(
 	'gutenberg-test-inner-blocks-templates',
 	plugins_url( 'inner-blocks-templates/index.js', __FILE__ ),
@@ -15,7 +16,7 @@ wp_enqueue_script(
 		'wp-element',
 		'wp-editor',
 		'wp-hooks',
-		'wp-i18n'
+		'wp-i18n',
 	),
 	filemtime( plugin_dir_path( __FILE__ ) . 'inner-blocks-templates/index.js' ),
 	true
