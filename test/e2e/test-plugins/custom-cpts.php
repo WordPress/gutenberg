b7a6ea0c1e103b3ca6c71a2701e6573c0d2f95a8
Jorge <jorge.costa@developer.pt>
2018-12-21 18:11:08 +0000

    chore: reformate identation

--- test/e2e/test-plugins/custom-cpts.php
+++ test/e2e/test-plugins/custom-cpts.php
@@ -7,38 +7,56 @@
  * @package gutenberg-test-custom-cpts
  */
 
-add_action( 'init', function() {
-	register_post_type( 'public_q_not_public', array(
-		'label'              => 'PublicQNotPublic',
-		'show_in_rest'       => true,
-		'public'             => false,
-		'publicly_queryable' => true,
-		'supports'           => [ 'title', 'editor', 'revisions' ],
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-	) );
-} );
+add_action(
+	'init',
+	function() {
+		register_post_type(
+			'public_q_not_public',
+			array(
+				'label'              => 'PublicQNotPublic',
+				'show_in_rest'       => true,
+				'public'             => false,
+				'publicly_queryable' => true,
+				'supports'           => [ 'title', 'editor', 'revisions' ],
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+			)
+		);
+	}
+);
 
-add_action( 'init', function() {
-	register_post_type( 'not_public_q_public', array(
-		'label'              => 'NotPublicQPublic',
-		'show_in_rest'       => true,
-		'public'             => true,
-		'publicly_queryable' => false,
-		'supports'           => [ 'title', 'editor', 'revisions' ],
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-	) );
-} );
+add_action(
+	'init',
+	function() {
+		register_post_type(
+			'not_public_q_public',
+			array(
+				'label'              => 'NotPublicQPublic',
+				'show_in_rest'       => true,
+				'public'             => true,
+				'publicly_queryable' => false,
+				'supports'           => [ 'title', 'editor', 'revisions' ],
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+			)
+		);
+	}
+);
 
-add_action( 'init', function() {
-	register_post_type( 'public_q_public', array(
-		'label'              => 'PublicQueryPublic',
-		'show_in_rest'       => true,
-		'public'             => true,
-		'publicly_queryable' => true,
-		'supports'           => [ 'title', 'editor', 'revisions' ],
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-	) );
-} );
+add_action(
+	'init',
+	function() {
+		register_post_type(
+			'public_q_public',
+			array(
+				'label'              => 'PublicQueryPublic',
+				'show_in_rest'       => true,
+				'public'             => true,
+				'publicly_queryable' => true,
+				'supports'           => [ 'title', 'editor', 'revisions' ],
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+			)
+		);
+	}
+);
