80cdc3a3728709c0dede170c01f7e49d17f449b5
Riad Benguella <benguella@gmail.com>
2018-12-25 09:47:13 +0100

    Avoid PHP notices in e2e tests plugins (#13094)

--- test/e2e/test-plugins/block-icons.php
+++ test/e2e/test-plugins/block-icons.php
@@ -7,17 +7,24 @@
  * @package gutenberg-test-block-icons
  */
 
-wp_enqueue_script(
-	'gutenberg-test-block-icons',
-	plugins_url( 'block-icons/index.js', __FILE__ ),
-	array(
-		'wp-blocks',
-		'wp-components',
-		'wp-element',
-		'wp-editor',
-		'wp-hooks',
-		'wp-i18n',
-	),
-	filemtime( plugin_dir_path( __FILE__ ) . 'block-icons/index.js' ),
-	true
-);
+/**
+ * Registers a custom script for the plugin.
+ */
+function enqueue_block_icons_plugin_script() {
+	wp_enqueue_script(
+		'gutenberg-test-block-icons',
+		plugins_url( 'block-icons/index.js', __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-components',
+			'wp-element',
+			'wp-editor',
+			'wp-hooks',
+			'wp-i18n',
+		),
+		filemtime( plugin_dir_path( __FILE__ ) . 'block-icons/index.js' ),
+		true
+	);
+}
+
+add_action( 'init', 'enqueue_block_icons_plugin_script' );
