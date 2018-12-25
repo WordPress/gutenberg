45166812baf39eecaab624f254baee6859aab812
Riad Benguella <benguella@gmail.com>
2018-12-25 09:47:13 +0100

    Avoid PHP notices in e2e tests plugins (#13094)

--- test/e2e/test-plugins/inner-blocks-templates.php
+++ test/e2e/test-plugins/inner-blocks-templates.php
@@ -7,17 +7,24 @@
  * @package gutenberg-test-inner-blocks-templates
  */
 
-wp_enqueue_script(
-	'gutenberg-test-inner-blocks-templates',
-	plugins_url( 'inner-blocks-templates/index.js', __FILE__ ),
-	array(
-		'wp-blocks',
-		'wp-components',
-		'wp-element',
-		'wp-editor',
-		'wp-hooks',
-		'wp-i18n',
-	),
-	filemtime( plugin_dir_path( __FILE__ ) . 'inner-blocks-templates/index.js' ),
-	true
-);
+/**
+ * Registers a custom script for the plugin.
+ */
+function enqueue_container_without_paragraph_plugin_script() {
+	wp_enqueue_script(
+		'gutenberg-test-inner-blocks-templates',
+		plugins_url( 'inner-blocks-templates/index.js', __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-components',
+			'wp-element',
+			'wp-editor',
+			'wp-hooks',
+			'wp-i18n',
+		),
+		filemtime( plugin_dir_path( __FILE__ ) . 'inner-blocks-templates/index.js' ),
+		true
+	);
+}
+
+add_action( 'init', 'enqueue_container_without_paragraph_plugin_script' );
