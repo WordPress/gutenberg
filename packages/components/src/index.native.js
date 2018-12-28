93057b2e9215f2d7cbcae8f90c004c8167c93d7d
Marko Savic <savicmarko1985@gmail.com>
2018-12-28 14:27:52 +0100

    Implemented react native spinner

--- packages/components/src/index.native.js
+++ packages/components/src/index.native.js
@@ -5,6 +5,7 @@ export { default as Toolbar } from './toolbar';
 export { default as ToolbarButton } from './toolbar-button';
 export { default as withSpokenMessages } from './higher-order/with-spoken-messages';
 export { default as IconButton } from './icon-button';
+export { default as Spinner } from './spinner';
 export { createSlotFill, Slot, Fill, Provider as SlotFillProvider } from './slot-fill';
 
 // Higher-Order Components
