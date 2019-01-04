721f8fae5574274b38d2f1f1e64f5cb7c24ea980
Alexander Botteram <a.botteram@gmail.com>
2019-01-04 13:21:55 +0100

    Refactored logic that handles the link attributes

--- packages/format-library/src/link/inline.js
+++ packages/format-library/src/link/inline.js
@@ -1,8 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { Component, createRef, useMemo } from '@wordpress/element';
+import { sprintf, __ } from '@wordpress/i18n';
+import { Component, createRef, Fragment, useMemo } from '@wordpress/element';
 import {
 	ToggleControl,
 	withSpokenMessages,
@@ -66,7 +66,7 @@ const URLPopoverAtLink = ( { isActive, addingLink, value, ...props } ) => {
 };
 
 class InlineLinkUI extends Component {
-	constructor() {
+	constructor( { activeAttributes } ) {
 		super( ...arguments );
 
 		this.editLink = this.editLink.bind( this );
@@ -79,22 +79,17 @@ class InlineLinkUI extends Component {
 		this.autocompleteRef = createRef();
 
 		this.state = {
-			opensInNewWindow: false,
+			attributes: activeAttributes,
 			inputValue: '',
 		};
 	}
 
 	static getDerivedStateFromProps( props, state ) {
-		const { activeAttributes: { url, target } } = props;
-		const opensInNewWindow = target === '_blank';
+		const { activeAttributes: { url } } = props;
 
 		if ( ! isShowingInput( props, state ) ) {
 			if ( url !== state.inputValue ) {
 				return { inputValue: url };
-			}
-
-			if ( opensInNewWindow !== state.opensInNewWindow ) {
-				return { opensInNewWindow };
 			}
 		}
 
@@ -112,20 +107,54 @@ class InlineLinkUI extends Component {
 		this.setState( { inputValue } );
 	}
 
-	setLinkTarget( opensInNewWindow ) {
-		const { activeAttributes: { url = '' }, value, onChange } = this.props;
+	setLinkAttributes( attributes ) {
+		this.setState( {
+			...this.state,
+			attributes,
+		}, () => {
+			const {
+				value,
+				onChange,
+				activeAttributes: {
+					url,
+				},
+			} = this.props;
 
-		this.setState( { opensInNewWindow } );
+			onChange( applyFormat( value, createLinkFormat( {
+				...this.state.attributes,
+				url,
+			} ) ) );
+		} );
+	}
+
+	setLinkTarget( opensInNewWindow ) {
+		const { attributes = { url: '' }, value } = this.props;
 
 		// Apply now if URL is not being edited.
 		if ( ! isShowingInput( this.props, this.state ) ) {
 			const selectedText = getTextContent( slice( value ) );
 
-			onChange( applyFormat( value, createLinkFormat( {
-				url,
-				opensInNewWindow,
-				text: selectedText,
-			} ) ) );
+			if ( opensInNewWindow ) {
+				this.setLinkAttributes( {
+					'aria-label': sprintf( __( '%s (opens in a new tab)' ), selectedText ),
+					target: '_blank',
+					rel: 'nofollow noreferrer',
+					...attributes,
+				} );
+			} else {
+				if ( typeof attributes.rel === 'string' ) {
+					attributes.rel = attributes.rel.split( ' ' ).filter( ( relItem ) => {
+						return relItem !== 'noopener' && relItem !== 'noreferrer';
+					} ).join( ' ' );
+				} else {
+					delete attributes.rel;
+				}
+
+				delete attributes.target;
+				attributes[ 'aria-label' ] = selectedText;
+
+				this.setLinkAttributes( attributes );
+			}
 		}
 	}
 
@@ -136,13 +165,13 @@ class InlineLinkUI extends Component {
 
 	submitLink( event ) {
 		const { isActive, value, onChange, speak } = this.props;
-		const { inputValue, opensInNewWindow } = this.state;
+		const { inputValue } = this.state;
 		const url = prependHTTP( inputValue );
 		const selectedText = getTextContent( slice( value ) );
 		const format = createLinkFormat( {
 			url,
-			opensInNewWindow,
 			text: selectedText,
+			...this.state.attributes,
 		} );
 
 		event.preventDefault();
@@ -190,7 +219,7 @@ class InlineLinkUI extends Component {
 			return null;
 		}
 
-		const { inputValue, opensInNewWindow } = this.state;
+		const { inputValue } = this.state;
 		const showInput = isShowingInput( this.props, this.state );
 
 		return (
@@ -202,11 +231,13 @@ class InlineLinkUI extends Component {
 				onClose={ this.resetState }
 				focusOnMount={ showInput ? 'firstElement' : false }
 				renderSettings={ () => (
-					<ToggleControl
-						label={ __( 'Open in New Tab' ) }
-						checked={ opensInNewWindow }
-						onChange={ this.setLinkTarget }
-					/>
+					<Fragment>
+						<ToggleControl
+							label={ __( 'Open in New Tab' ) }
+							checked={ this.state.attributes.target === '_blank' }
+							onChange={ this.setLinkTarget }
+						/>
+					</Fragment>
 				) }
 			>
 				{ showInput ? (
--- packages/format-library/src/link/utils.js
+++ packages/format-library/src/link/utils.js
@@ -83,28 +83,13 @@ export function isValidHref( href ) {
 /**
  * Generates the format object that will be applied to the link text.
  *
- * @param {string}  url              The href of the link.
- * @param {boolean} opensInNewWindow Whether this link will open in a new window.
- * @param {Object}  text             The text that is being hyperlinked.
+ * @param {Object} attributes The anchor tag attributes.
  *
  * @return {Object} The final format object.
  */
-export function createLinkFormat( { url, opensInNewWindow, text } ) {
-	const format = {
+export function createLinkFormat( attributes ) {
+	return {
 		type: 'core/link',
-		attributes: {
-			url,
-		},
+		attributes,
 	};
-
-	if ( opensInNewWindow ) {
-		// translators: accessibility label for external links, where the argument is the link text
-		const label = sprintf( __( '%s (opens in a new tab)' ), text );
-
-		format.attributes.target = '_blank';
-		format.attributes.rel = 'noreferrer noopener';
-		format.attributes[ 'aria-label' ] = label;
-	}
-
-	return format;
 }
