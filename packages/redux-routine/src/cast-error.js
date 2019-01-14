0ca25d55c7710d51b6f565022726c13cf9ac5f01
Darren Ethier <darren@roughsmootheng.in>
2019-01-14 11:21:07 -0500

    improve handling of non strings for errors
    - introduce ReduxRoutineResponseError, a custom error handler for non string error values.  The “response” is added to a `response` property on this new error handler thus exposing arbitrary responses to any client code wanting to extract from.
    - implement ReduxRoutineResponseError in `castError` so that this error object is always returned when the error value is not already an instance of Error.

--- packages/redux-routine/src/cast-error.js
+++ packages/redux-routine/src/cast-error.js
@@ -1,4 +1,9 @@
 /**
+ * External imports
+ */
+import { isString } from 'lodash';
+
+/**
  * Casts value as an error if it's not one.
  *
  * @param {*} error The value to cast.
@@ -7,8 +12,41 @@
  */
 export default function castError( error ) {
 	if ( ! ( error instanceof Error ) ) {
-		error = new Error( error );
+		error = new ReduxRoutineResponseError( error );
 	}
-
 	return error;
 }
+
+/**
+ * A custom error for handling non-string error responses.
+ *
+ * Error responses are exposed on the `response` property of the error instance.
+ *
+ * @param {mixed} errorResponse
+ * @param {...mixed} args
+ * @return {Error} An instance of ReduxRoutineResponseError
+ * @constructor
+ */
+function ReduxRoutineResponseError( errorResponse, ...args ) {
+	const instance = new Error(
+		isString( errorResponse ) ? errorResponse : 'ReduxRoutineResponseError',
+		...args
+	);
+	Object.setPrototypeOf( instance, Object.getPrototypeOf( this ) );
+	instance.response = errorResponse;
+	if ( Error.captureStackTrace ) {
+		Error.captureStackTrace( instance, ReduxRoutineResponseError );
+	}
+	return instance;
+}
+
+ReduxRoutineResponseError.prototype = Object.create( Error.prototype, {
+	constructor: {
+		value: Error,
+		enumerable: false,
+		writable: true,
+		configurable: true,
+	},
+} );
+
+Object.setPrototypeOf( ReduxRoutineResponseError, Error );
