1f9c722ee4b9a17d2659b8a624927dbe54b2d299
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-01-16 18:26:43 -0300

    Removes unused some code.

--- packages/editor/src/components/post-title/index.native.js
+++ packages/editor/src/components/post-title/index.native.js
@@ -8,7 +8,6 @@ import { TextInput } from 'react-native';
  */
 import { Component } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
-import { ENTER } from '@wordpress/keycodes';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { withFocusOutside } from '@wordpress/components';
 import { withInstanceId, compose } from '@wordpress/compose';
@@ -20,8 +19,6 @@ class PostTitle extends Component {
 		this.onChange = this.onChange.bind( this );
 		this.onSelect = this.onSelect.bind( this );
 		this.onUnselect = this.onUnselect.bind( this );
-		this.onKeyDown = this.onKeyDown.bind( this );
-		this.redirectHistory = this.redirectHistory.bind( this );
 
 		this.state = {
 			isSelected: false,
@@ -43,33 +40,6 @@ class PostTitle extends Component {
 
 	onChange( title ) {
 		this.props.onUpdate( title );
-	}
-
-	onKeyDown( event ) {
-		if ( event.keyCode === ENTER ) {
-			event.preventDefault();
-			this.props.onEnterPress();
-		}
-	}
-
-	/**
-	 * Emulates behavior of an undo or redo on its corresponding key press
-	 * combination. This is a workaround to React's treatment of undo in a
-	 * controlled textarea where characters are updated one at a time.
-	 * Instead, leverage the store's undo handling of title changes.
-	 *
-	 * @see https://github.com/facebook/react/issues/8514
-	 *
-	 * @param {KeyboardEvent} event Key event.
-	 */
-	redirectHistory( event ) {
-		if ( event.shiftKey ) {
-			this.props.onRedo();
-		} else {
-			this.props.onUndo();
-		}
-
-		event.preventDefault();
 	}
 
 	render() {
