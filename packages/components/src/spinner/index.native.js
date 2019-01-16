2668862ac2e307675f7d7cb351315189119208e9
Marko Savic <savicmarko1985@gmail.com>
2019-01-16 15:03:44 +0100

    Updated spinner to match the specs

--- packages/components/src/spinner/index.native.js
+++ packages/components/src/spinner/index.native.js
@@ -6,7 +6,7 @@ export default function Spinner( props 	) {
 	const width = progress + '%';
 
 	return (
-		<View style={ { flex: 1, height: 5, backgroundColor: '#a8bece' } }>
+		<View style={ { flex: 1, height: 5, backgroundColor: '#c8d7e1' } }>
 			<View style={ { width: width, height: 5, backgroundColor: '#0087be' } } />
 		</View>
 	);
