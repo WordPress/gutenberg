de314f56b3ea1713f3c2cdc98e77d0fcd85ea323
Andrés <andres.maneiro@automattic.com>
2019-01-16 13:09:22 +0100

    Test named export (const)

--- packages/docgen/tests/test-engine.js
+++ packages/docgen/tests/test-engine.js
@@ -8,12 +8,28 @@ const test = require( 'tape' );
  */
 const engine = require( '../src/engine' );
 
-test( 'engine returns intermediate representation', ( t ) => {
+test( 'engine returns IR for named export (function)', ( t ) => {
 	const ir = engine( `
 		/**
  		 * My declaration example.
  		 */
 		export function myDeclaration() {
+			// do nothing
+		}
+` );
+	t.deepEqual(
+		ir,
+		[ { name: 'myDeclaration', jsdoc: { description: 'My declaration example.', tags: [] } } ]
+	);
+	t.end();
+} );
+
+test( 'engine returns IR for named export (const)', ( t ) => {
+	const ir = engine( `
+		/**
+ 		 * My declaration example.
+ 		 */
+		export const myDeclaration = function() {
 			// do nothing
 		}
 ` );
