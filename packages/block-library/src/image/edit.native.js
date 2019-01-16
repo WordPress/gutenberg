277cb8c083fbb1c99ab46066b70d1c98ff3c2cb7
etoledom <etoledom@icloud.com>
2019-01-16 15:24:52 +0100

    [Mobile] Image height (#13096)
    * [rnmobile]: Convert image-block into class
    
    * [rnmobile]: Creating mobile version of `image-size`.
    
    * [rnmobile]: Adding util to image-block to share math between image-size components.
    
    * [rnmobile]: Adding newline at the end of image-size.native.js file
    
    * [rnmobile]: Replace wrong tabs characters
    
    * [rnmobile]: Clear image size when url changes.
    
    * [rnmobile]: Fixed lint issues
    
    * [rnmobile]: Removing unnecessary binding calls
    
    * [rnmobile]: Added necessary this.onLayout.bind( this )
    
    * [rnmobile]: Adding missing bind to `onMediaLibraryPress`.

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -9,67 +9,104 @@ import RNReactNativeGutenbergBridge from 'react-native-gutenberg-bridge';
  */
 import { MediaPlaceholder, RichText, BlockControls } from '@wordpress/editor';
 import { Toolbar, ToolbarButton } from '@wordpress/components';
+import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import ImageSize from './image-size';
 
-export default function ImageEdit( props ) {
-	const { attributes, isSelected, setAttributes } = props;
-	const { url, caption } = attributes;
+class ImageEdit extends Component {
+	constructor() {
+		super( ...arguments );
+		this.onMediaLibraryPress = this.onMediaLibraryPress.bind( this );
+	}
 
-	const onUploadPress = () => {
+	onUploadPress() {
 		// This method should present an image picker from
 		// the device.
 		//TODO: Implement upload image method.
-	};
+	}
 
-	const onMediaLibraryPress = () => {
+	onMediaLibraryPress() {
 		RNReactNativeGutenbergBridge.onMediaLibraryPress( ( mediaUrl ) => {
 			if ( mediaUrl ) {
-				setAttributes( { url: mediaUrl } );
+				this.props.setAttributes( { url: mediaUrl } );
 			}
 		} );
-	};
+	}
 
-	if ( ! url ) {
+	toolbarEditButton() {
 		return (
-			<MediaPlaceholder
-				onUploadPress={ onUploadPress }
-				onMediaLibraryPress={ onMediaLibraryPress }
-			/>
+			<Toolbar>
+				<ToolbarButton
+					className="components-toolbar__control"
+					label={ __( 'Edit image' ) }
+					icon="edit"
+					onClick={ this.onMediaLibraryPress }
+				/>
+			</Toolbar>
 		);
 	}
 
-	const toolbarEditButton = (
-		<Toolbar>
-			<ToolbarButton
-				className="components-toolbar__control"
-				label={ __( 'Edit image' ) }
-				icon="edit"
-				onClick={ onMediaLibraryPress }
-			/>
-		</Toolbar>
-	);
+	render() {
+		const { attributes, isSelected, setAttributes } = this.props;
+		const { url, caption, height, width } = attributes;
 
-	return (
-		<View style={ { flex: 1 } }>
-			<BlockControls>
-				{ toolbarEditButton }
-			</BlockControls>
-			<Image
-				style={ { width: '100%', height: 200 } }
-				resizeMethod="scale"
-				source={ { uri: url } }
-			/>
-			{ ( ! RichText.isEmpty( caption ) > 0 || isSelected ) && (
-				<View style={ { padding: 12, flex: 1 } }>
-					<TextInput
-						style={ { textAlign: 'center' } }
-						underlineColorAndroid="transparent"
-						value={ caption }
-						placeholder={ 'Write caption…' }
-						onChangeText={ ( newCaption ) => setAttributes( { caption: newCaption } ) }
-					/>
-				</View>
-			) }
-		</View>
-	);
+		if ( ! url ) {
+			return (
+				<MediaPlaceholder
+					onUploadPress={ this.onUploadPress }
+					onMediaLibraryPress={ this.onMediaLibraryPress }
+				/>
+			);
+		}
+
+		return (
+			<View style={ { flex: 1 } }>
+				<BlockControls>
+					{ this.toolbarEditButton() }
+				</BlockControls>
+				<ImageSize src={ url } >
+					{ ( sizes ) => {
+						const {
+							imageWidthWithinContainer,
+							imageHeightWithinContainer,
+						} = sizes;
+
+						let finalHeight = imageHeightWithinContainer;
+						if ( height > 0 && height < imageHeightWithinContainer ) {
+							finalHeight = height;
+						}
+
+						let finalWidth = imageWidthWithinContainer;
+						if ( width > 0 && width < imageWidthWithinContainer ) {
+							finalWidth = width;
+						}
+
+						return (
+							<View style={ { flex: 1 } } >
+								<Image
+									style={ { width: finalWidth, height: finalHeight } }
+									resizeMethod="scale"
+									source={ { uri: url } }
+									key={ url }
+								/>
+							</View>
+						);
+					} }
+				</ImageSize>
+				{ ( ! RichText.isEmpty( caption ) > 0 || isSelected ) && (
+					<View style={ { padding: 12, flex: 1 } }>
+						<TextInput
+							style={ { textAlign: 'center' } }
+							underlineColorAndroid="transparent"
+							value={ caption }
+							placeholder={ 'Write caption…' }
+							onChangeText={ ( newCaption ) => setAttributes( { caption: newCaption } ) }
+						/>
+					</View>
+				) }
+			</View>
+		);
+	}
 }
+
+export default ImageEdit;
