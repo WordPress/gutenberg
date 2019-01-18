84f4dbca217d917422d0b71b0249565b374f6868
Andrés <andres.maneiro@automattic.com>
2019-01-18 18:03:43 +0100

    Add tests for get name of named function and variable

---
+++ packages/docgen/tests/test-get-name-declaration.js
@@ -0,0 +1,35 @@
+/**
+ * Node dependencies.
+ */
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+/**
+ * Internal dependencies.
+ */
+const getNameDeclaration = require( '../src/get-name-declaration' );
+
+/**
+ * External dependencies.
+ */
+const test = require( 'tape' );
+
+test( 'returns name for named export (function)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-function.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.equal( name, 'myDeclaration' );
+	t.end();
+} );
+
+test( 'returns name for named export (variable)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-variable.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.equal( name, 'myDeclaration' );
+	t.end();
+} );
