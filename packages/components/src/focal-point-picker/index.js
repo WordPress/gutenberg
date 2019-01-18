7131d07f8752938adbd6aee0c5ca3d375102dc12
Jefferson Rabb <jeff@atavist.net>
2019-01-18 12:21:12 -0500

    Display horizontal and vertical positions as percentages in text boxes, for accessibility reasons. Slight corrections to the postion calculations.

--- packages/components/src/focal-point-picker/index.js
+++ packages/components/src/focal-point-picker/index.js
@@ -6,6 +6,7 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { Component, createRef } from '@wordpress/element';
 import { withInstanceId, compose } from '@wordpress/compose';
 
@@ -13,6 +14,7 @@ import { withInstanceId, compose } from '@wordpress/compose';
  * Internal dependencies
  */
 import BaseControl from '../base-control';
+import TextControl from '../text-control';
 import withFocusOutside from '../higher-order/with-focus-outside';
 
 export class FocalPointPicker extends Component {
@@ -22,6 +24,7 @@ export class FocalPointPicker extends Component {
 		this.state = {
 			isDragging: false,
 			bounds: {},
+			percentages: {},
 		};
 		this.containerRef = createRef();
 	}
@@ -73,20 +76,31 @@ export class FocalPointPicker extends Component {
 				top: event.pageY - pickerDimensions.top,
 			};
 			const left = Math.max(
-				bounds.left, Math.min(
+				bounds.left,
+				Math.min(
 					cursorPosition.left, bounds.right
 				)
 			);
 			const top = Math.max(
-				bounds.top, Math.min(
+				bounds.top,
+				Math.min(
 					cursorPosition.top, bounds.bottom
 				)
 			);
-			onChange( {
-				x: left / pickerDimensions.width,
-				y: top / pickerDimensions.height,
+			const percentages = {
+				x: ( left - bounds.left ) / ( pickerDimensions.width - ( bounds.left * 2 ) ),
+				y: ( top - bounds.top ) / ( pickerDimensions.height - ( bounds.top * 2 ) ),
+			};
+			this.setState( { percentages }, function() {
+				onChange( {
+					x: this.state.percentages.x,
+					y: this.state.percentages.y,
+				} );
 			} );
 		}
+	}
+	fractionToPercentage( fraction ) {
+		return Math.round( fraction * 100 ) + '%';
 	}
 	pickerDimensions() {
 		if ( this.containerRef.current ) {
@@ -106,12 +120,16 @@ export class FocalPointPicker extends Component {
 	}
 	render() {
 		const { instanceId, url, value, label, help, className } = this.props;
-		const { isDragging } = this.state;
+		const { bounds, isDragging, percentages } = this.state;
 		const pickerDimensions = this.pickerDimensions();
 		const containerStyle = { backgroundImage: `url(${ url })` };
+		const iconCoordinates = {
+			left: ( value.x * ( pickerDimensions.width - ( bounds.left * 2 ) ) ) + bounds.left,
+			top: ( value.y * ( pickerDimensions.height - ( bounds.top * 2 ) ) ) + bounds.top,
+		};
 		const iconContainerStyle = {
-			left: `${ value.x * pickerDimensions.width }px`,
-			top: `${ value.y * pickerDimensions.height }px`,
+			left: `${ iconCoordinates.left }px`,
+			top: `${ iconCoordinates.top }px`,
 		};
 		const iconContainerClasses = classnames(
 			'component-focal-point-picker__icon_container',
@@ -133,8 +151,18 @@ export class FocalPointPicker extends Component {
 					tabIndex="0"
 				>
 					<div className={ iconContainerClasses } style={ iconContainerStyle }>
-						<i className="component-focal-point-picker__icon"></i>
+						<i className="component-focal-point-picker__icon" />
 					</div>
+				</div>
+				<div className="component-focal-point-picker_position-display-container">
+					<TextControl
+						label={ __( 'Horizontal Pos.' ) }
+						value={ this.fractionToPercentage( percentages.x ) }
+					/>
+					<TextControl
+						label={ __( 'Vertical Pos.' ) }
+						value={ this.fractionToPercentage( percentages.y ) }
+					/>
 				</div>
 			</BaseControl>
 		);
@@ -159,8 +187,4 @@ FocalPointPicker.defaultProps = {
 	onChange: () => {},
 };
 
-export default compose( [
-	withInstanceId,
-	withFocusOutside,
-] )( FocalPointPicker );
-
+export default compose( [ withInstanceId, withFocusOutside ] )( FocalPointPicker );
