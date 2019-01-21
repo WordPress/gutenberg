1bd8ea03c4eb65b52fdb5dd1fbb578842e2d7912
Joen Asmussen <asmussen@gmail.com>
2019-01-21 10:54:54 +0100

    Use an SVG, and theme the colors
    - Change the ring to be an SVG. This allows us to change the colors to match the theme.
    - Add draggable false property to thumbnail, so you don't accidentally copy it to your desktop if you start the dragging operation outside the ring handle
    - Redesign the drag handle, by the way. Ring seems sufficient.
    - Increase the opacity a bit.
    - Change some of the CSS to leverage PostCSS rewriting.

--- packages/components/src/focal-point-picker/index.js
+++ packages/components/src/focal-point-picker/index.js
@@ -15,6 +15,7 @@ import { withInstanceId, compose } from '@wordpress/compose';
  */
 import BaseControl from '../base-control';
 import withFocusOutside from '../higher-order/with-focus-outside';
+import { Path, SVG } from '../primitives';
 
 const TEXTCONTROL_MIN = 0;
 const TEXTCONTROL_MAX = 100;
@@ -192,9 +193,13 @@ export class FocalPointPicker extends Component {
 							onLoad={ this.onLoad }
 							ref={ this.imageRef }
 							src={ url }
+							draggable="false"
 						/>
 						<div className={ iconContainerClasses } style={ iconContainerStyle }>
-							<i className="component-focal-point-picker__icon" />
+							<SVG className="component-focal-point-picker__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+								<Path className="component-focal-point-picker__icon-outline" d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z" />
+								<Path className="component-focal-point-picker__icon-fill" d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z" />
+							</SVG>
 						</div>
 					</div>
 				</div>
