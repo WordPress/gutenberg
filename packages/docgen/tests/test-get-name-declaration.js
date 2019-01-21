255af56c4aabdba93a7ea99fbb88e73edfd77f41
Andrés <andres.maneiro@automattic.com>
2019-01-21 17:42:55 +0100

    Reorganize test to prepare next steps

--- packages/docgen/tests/test-get-name-declaration.js
+++ packages/docgen/tests/test-get-name-declaration.js
@@ -14,97 +14,7 @@ const getNameDeclaration = require( '../src/get-name-declaration' );
  */
 const test = require( 'tape' );
 
-test( 'returns name for named export (class)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-class.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'MyDeclaration' ] );
-	t.end();
-} );
-
-test( 'returns name for named export (function)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-function.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'myDeclaration' ] );
-	t.end();
-} );
-
-test( 'returns name for named export (variable)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-variable.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'myDeclaration' ] );
-	t.end();
-} );
-
-test( 'returns name for named export (single identifier)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-identifier-single.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'myDeclaration' ] );
-	t.end();
-} );
-
-test( 'returns name for named export (multiple identifier)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-identifier-multiple.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'functionDeclaration', 'variableDeclaration', 'ClassDeclaration' ] );
-	t.end();
-} );
-
-test( 'returns name for default export (function anonymous)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/default-function-anonymous.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'default export' ] );
-	t.end();
-} );
-
-test( 'returns name for default export (function named)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/default-function-named.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'default export' ] );
-	t.end();
-} );
-
-test( 'returns name for default export (variable anonymous)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/default-variable-anonymous.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'default export' ] );
-	t.end();
-} );
-
-test( 'returns name for default export (variable named)', function( t ) {
-	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/default-variable-named.json' ),
-		'utf-8'
-	);
-	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'default export' ] );
-	t.end();
-} );
-
-test( 'returns name for default export (class anonymous)', function( t ) {
+test( 'default export class (anonymous)', function( t ) {
 	const token = fs.readFileSync(
 		path.join( __dirname, './fixtures/default-class-anonymous.json' ),
 		'utf-8'
@@ -114,7 +24,7 @@ test( 'returns name for default export (class anonymous)', function( t ) {
 	t.end();
 } );
 
-test( 'returns name for default export (class named)', function( t ) {
+test( 'default export class (named)', function( t ) {
 	const token = fs.readFileSync(
 		path.join( __dirname, './fixtures/default-class-named.json' ),
 		'utf-8'
@@ -124,7 +34,27 @@ test( 'returns name for default export (class named)', function( t ) {
 	t.end();
 } );
 
-test( 'returns name for default export (identifier)', function( t ) {
+test( 'default export function (anonymous)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/default-function-anonymous.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'default export' ] );
+	t.end();
+} );
+
+test( 'default export function (named)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/default-function-named.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'default export' ] );
+	t.end();
+} );
+
+test( 'default export identifier', function( t ) {
 	const token = fs.readFileSync(
 		path.join( __dirname, './fixtures/default-identifier.json' ),
 		'utf-8'
@@ -134,22 +64,92 @@ test( 'returns name for default export (identifier)', function( t ) {
 	t.end();
 } );
 
-test( 'returns name for named export redirections (*)', function( t ) {
+test( 'default export variable (anonymous)', function( t ) {
 	const token = fs.readFileSync(
-		path.join( __dirname, './fixtures/named-star.json' ),
+		path.join( __dirname, './fixtures/default-variable-anonymous.json' ),
 		'utf-8'
 	);
 	const name = getNameDeclaration( JSON.parse( token ) );
-	t.deepEqual( name, [ 'TODO: to look up in module' ] );
+	t.deepEqual( name, [ 'default export' ] );
 	t.end();
 } );
 
-test( 'returns name for named export redirections (default)', function( t ) {
+test( 'default export variable (named)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/default-variable-named.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'default export' ] );
+	t.end();
+} );
+
+test( 'named export class', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-class.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'MyDeclaration' ] );
+	t.end();
+} );
+
+test( 'named export default', function( t ) {
 	const token = fs.readFileSync(
 		path.join( __dirname, './fixtures/named-default.json' ),
 		'utf-8'
 	);
 	const name = getNameDeclaration( JSON.parse( token ) );
 	t.deepEqual( name, [ 'TODO: default export?' ] );
+	t.end();
+} );
+
+test( 'named export function', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-function.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'myDeclaration' ] );
+	t.end();
+} );
+
+test( 'named export identifier', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-identifier.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'myDeclaration' ] );
+	t.end();
+} );
+
+test( 'named export identifiers', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-identifiers.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'functionDeclaration', 'variableDeclaration', 'ClassDeclaration' ] );
+	t.end();
+} );
+
+test( 'named export variable', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/named-variable.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'myDeclaration' ] );
+	t.end();
+} );
+
+test( 'namespace export (*)', function( t ) {
+	const token = fs.readFileSync(
+		path.join( __dirname, './fixtures/namespace.json' ),
+		'utf-8'
+	);
+	const name = getNameDeclaration( JSON.parse( token ) );
+	t.deepEqual( name, [ 'TODO: to look up in module' ] );
 	t.end();
 } );
