1e6d4338148de0a674c71e410f6cf5f1f0562e07
Andrew Duthie <andrew@andrewduthie.com>
2019-01-23 11:05:47 -0500

    Plugin: Remove 5.0-merged block registration functions, integrations (#13412)

--- phpunit/class-blocks-api-test.php
+++ phpunit/class-blocks-api-test.php
@@ -82,37 +82,14 @@ class Blocks_API extends WP_UnitTestCase {
 	 * @covers ::strip_dynamic_blocks
 	 */
 	function test_strip_dynamic_blocks() {
+		$this->setExpectedDeprecated( 'strip_dynamic_blocks' );
+		$this->setExpectedDeprecated( 'get_dynamic_blocks_regex' );
+
 		// Simple dynamic block..
 		$content = '<!-- wp:core/block /-->';
 		$this->assertEmpty( strip_dynamic_blocks( $content ) );
 
 		// Dynamic block with options, embedded in other content.
 		$this->assertEquals( $this->filtered_content, strip_dynamic_blocks( $this->content ) );
-	}
-
-	/**
-	 * Tests that dynamic blocks don't cause an out-of-memory error.
-	 *
-	 * When dynamic blocks happen to generate an excerpt, they can cause an
-	 * infinite loop if that block is part of the post's content.
-	 *
-	 * `wp_trim_excerpt()` applies the `the_content` filter, which has
-	 * `do_blocks` attached to it, trying to render the block which again will
-	 * attempt to return an excerpt of that post.
-	 *
-	 * This infinite loop can be avoided by stripping dynamic blocks before
-	 * `the_content` gets applied, just like shortcodes.
-	 *
-	 * @covers ::strip_dynamic_blocks_add_filter, ::strip_dynamic_blocks_remove_filter
-	 */
-	function test_excerpt_infinite_loop() {
-		$query = new WP_Query(
-			array(
-				'post__in' => array( self::$post_id ),
-			)
-		);
-		$query->the_post();
-
-		$this->assertEmpty( do_blocks( '<!-- wp:core/dummy /-->' ) );
 	}
 }
