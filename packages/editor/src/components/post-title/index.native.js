a1b2ec98b4df0d0cbde6327152c438e609db7778
Diego Rey Mendez <diegoreymendez@users.noreply.github.com>
2019-01-24 18:22:50 -0300

    Add title to gutenberg mobile (#13199)
    * temporarily disable link formatting
    
    * Make sure RichText resigns focus when unmounted (#13048)
    
    * Implements a native version of post-title.
    
    * Removes some unnecessary log calls.
    
    * Submits a few lint fixes.
    
    * Fixes a linting issue.
    
    * When focusing the title, any focused block loses its focus.
    
    * FocusOut is now wired for post-title for mobile.
    
    * Removes unused some code.
    
    * Added a file I failed to commit.
    
    * Fixes a linting issue.

---
+++ packages/editor/src/components/post-title/index.native.js
@@ -0,0 +1,84 @@
+/**
+ * External dependencies
+ */
+import { TextInput } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
+import { withDispatch } from '@wordpress/data';
+import { withFocusOutside } from '@wordpress/components';
+import { withInstanceId, compose } from '@wordpress/compose';
+
+class PostTitle extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.onChange = this.onChange.bind( this );
+		this.onSelect = this.onSelect.bind( this );
+		this.onUnselect = this.onUnselect.bind( this );
+
+		this.state = {
+			isSelected: false,
+		};
+	}
+
+	handleFocusOutside() {
+		this.onUnselect();
+	}
+
+	onSelect() {
+		this.setState( { isSelected: true } );
+		this.props.clearSelectedBlock();
+	}
+
+	onUnselect() {
+		this.setState( { isSelected: false } );
+	}
+
+	onChange( title ) {
+		this.props.onUpdate( title );
+	}
+
+	render() {
+		const {
+			placeholder,
+			style,
+			title,
+		} = this.props;
+
+		const decodedPlaceholder = decodeEntities( placeholder );
+
+		return (
+			<TextInput
+				blurOnSubmit={ true }
+				textAlignVertical="top"
+				multiline
+				numberOfLines={ 0 }
+				onChangeText={ this.onChange }
+				onFocus={ this.onSelect }
+				placeholder={ decodedPlaceholder }
+				style={ style }
+				value={ title }>
+			</TextInput>
+		);
+	}
+}
+
+const applyWithDispatch = withDispatch( ( dispatch ) => {
+	const {
+		clearSelectedBlock,
+	} = dispatch( 'core/editor' );
+
+	return {
+		clearSelectedBlock,
+	};
+} );
+
+export default compose(
+	applyWithDispatch,
+	withInstanceId,
+	withFocusOutside
+)( PostTitle );
