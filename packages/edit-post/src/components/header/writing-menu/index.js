147d7f77196493672db2de6a46aafccdeeea5d91
Andrea Fercia <a.fercia@gmail.com>
2019-01-24 16:16:49 +0100

    Add speak messages to the feature toggle component. (#13385)
    * Add speak messages to the feature toggle component.
    
    * Use plain props.

--- packages/edit-post/src/components/header/writing-menu/index.js
+++ packages/edit-post/src/components/header/writing-menu/index.js
@@ -19,17 +19,26 @@ function WritingMenu( { onClose } ) {
 				feature="fixedToolbar"
 				label={ __( 'Top Toolbar' ) }
 				info={ __( 'Access all block and document tools in a single place' ) }
-				onToggle={ onClose } />
+				onToggle={ onClose }
+				messageActivated={ __( 'Top toolbar activated' ) }
+				messageDeactivated={ __( 'Top toolbar deactivated' ) }
+			/>
 			<FeatureToggle
 				feature="focusMode"
 				label={ __( 'Spotlight Mode' ) }
 				info={ __( 'Focus on one block at a time' ) }
-				onToggle={ onClose } />
+				onToggle={ onClose }
+				messageActivated={ __( 'Spotlight mode activated' ) }
+				messageDeactivated={ __( 'Spotlight mode deactivated' ) }
+			/>
 			<FeatureToggle
 				feature="fullscreenMode"
 				label={ __( 'Fullscreen Mode' ) }
 				info={ __( 'Work without distraction' ) }
-				onToggle={ onClose } />
+				onToggle={ onClose }
+				messageActivated={ __( 'Fullscreen mode activated' ) }
+				messageDeactivated={ __( 'Fullscreen mode deactivated' ) }
+			/>
 		</MenuGroup>
 	);
 }
