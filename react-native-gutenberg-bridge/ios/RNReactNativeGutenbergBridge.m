3b7cb0ff2a404c3b14ef699e8e45d0982074597c
Sergio Estevao <sergioestevao@gmail.com>
2019-01-28 16:26:33 +0000

    Implement reattach of events on iOS.

--- react-native-gutenberg-bridge/ios/RNReactNativeGutenbergBridge.m
+++ react-native-gutenberg-bridge/ios/RNReactNativeGutenbergBridge.m
@@ -6,6 +6,7 @@ RCT_EXTERN_METHOD(provideToNative_Html:(NSString *)html title:(NSString *)title 
 RCT_EXTERN_METHOD(onMediaLibraryPressed:(RCTResponseSenderBlock)callback)
 RCT_EXTERN_METHOD(onUploadMediaPressed:(RCTResponseSenderBlock)callback)
 RCT_EXTERN_METHOD(onCapturePhotoPressed:(RCTResponseSenderBlock)callback)
+RCT_EXTERN_METHOD(onImageQueryReattach)
 RCT_EXTERN_METHOD(editorDidLayout)
 
 @end
