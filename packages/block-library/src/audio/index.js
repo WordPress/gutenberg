be8ca3c84d0ed6b844811f0b80cb520d1c517e22
Sören Wrede <soerenwrede@gmail.com>
2019-01-29 16:04:58 +0100

    Hide empty Audio block on frontend

--- packages/block-library/src/audio/index.js
+++ packages/block-library/src/audio/index.js
@@ -14,6 +14,41 @@ import { createBlobURL } from '@wordpress/blob';
 
 export const name = 'core/audio';
 
+const blockAttributes = {
+	src: {
+		type: 'string',
+		source: 'attribute',
+		selector: 'audio',
+		attribute: 'src',
+	},
+	caption: {
+		type: 'string',
+		source: 'html',
+		selector: 'figcaption',
+	},
+	id: {
+		type: 'number',
+	},
+	autoplay: {
+		type: 'boolean',
+		source: 'attribute',
+		selector: 'audio',
+		attribute: 'autoplay',
+	},
+	loop: {
+		type: 'boolean',
+		source: 'attribute',
+		selector: 'audio',
+		attribute: 'loop',
+	},
+	preload: {
+		type: 'string',
+		source: 'attribute',
+		selector: 'audio',
+		attribute: 'preload',
+	},
+};
+
 export const settings = {
 	title: __( 'Audio' ),
 
@@ -23,40 +58,7 @@ export const settings = {
 
 	category: 'common',
 
-	attributes: {
-		src: {
-			type: 'string',
-			source: 'attribute',
-			selector: 'audio',
-			attribute: 'src',
-		},
-		caption: {
-			type: 'string',
-			source: 'html',
-			selector: 'figcaption',
-		},
-		id: {
-			type: 'number',
-		},
-		autoplay: {
-			type: 'boolean',
-			source: 'attribute',
-			selector: 'audio',
-			attribute: 'autoplay',
-		},
-		loop: {
-			type: 'boolean',
-			source: 'attribute',
-			selector: 'audio',
-			attribute: 'loop',
-		},
-		preload: {
-			type: 'string',
-			source: 'attribute',
-			selector: 'audio',
-			attribute: 'preload',
-		},
-	},
+	attributes: blockAttributes,
 
 	transforms: {
 		from: [
@@ -90,9 +92,31 @@ export const settings = {
 		const { autoplay, caption, loop, preload, src } = attributes;
 		return (
 			<figure>
-				<audio controls="controls" src={ src } autoPlay={ autoplay } loop={ loop } preload={ preload } />
-				{ ! RichText.isEmpty( caption ) && <RichText.Content tagName="figcaption" value={ caption } /> }
+				{ src && (
+					<audio
+						controls="controls"
+						src={ src }
+						autoPlay={ autoplay }
+						loop={ loop }
+						preload={ preload }
+					/>
+				) }
+				{ ! RichText.isEmpty( caption ) && (
+					<RichText.Content tagName="figcaption" value={ caption } />
+				) }
 			</figure>
 		);
 	},
+
+	deprecated: [
+		{
+			save() {
+				return (
+					<figure>
+						<audio controls />
+					</figure>
+				);
+			},
+		},
+	],
 };
