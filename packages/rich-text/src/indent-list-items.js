d9cb84e5bdc4d4e46884edb83ed8418ab8bc0b3d
Ella Van Durpe <iseulde@automattic.com>
2019-01-29 18:04:21 +0100

    RichText: List: fix indentation (#13563)
    * fix list indentation
    
    * Add more tests
    
    * Guard against negative lineIndex
    
    * Fix outdent error

--- packages/rich-text/src/indent-list-items.js
+++ packages/rich-text/src/indent-list-items.js
@@ -7,6 +7,36 @@ import { normaliseFormats } from './normalise-formats';
 import { getLineIndex } from './get-line-index';
 
 /**
+ * Gets the line index of the first previous list item with higher indentation.
+ *
+ * @param {Object} value      Value to search.
+ * @param {number} lineIndex  Line index of the list item to compare with.
+ *
+ * @return {boolean} The line index.
+ */
+function getTargetLevelLineIndex( { text, formats }, lineIndex ) {
+	const startFormats = formats[ lineIndex ] || [];
+
+	let index = lineIndex;
+
+	while ( index-- >= 0 ) {
+		if ( text[ index ] !== LINE_SEPARATOR ) {
+			continue;
+		}
+
+		const formatsAtIndex = formats[ index ] || [];
+
+		// Return the first line index that is one level higher. If the level is
+		// lower or equal, there is no result.
+		if ( formatsAtIndex.length === startFormats.length + 1 ) {
+			return index;
+		} else if ( formatsAtIndex.length <= startFormats.length ) {
+			return;
+		}
+	}
+}
+
+/**
  * Indents any selected list items if possible.
  *
  * @param {Object} value      Value to change.
@@ -23,61 +53,38 @@ export function indentListItems( value, rootFormat ) {
 	}
 
 	const { text, formats, start, end } = value;
+	const previousLineIndex = getLineIndex( value, lineIndex );
 	const formatsAtLineIndex = formats[ lineIndex ] || [];
-	const targetFormats = formats[ getLineIndex( value, lineIndex ) ] || [];
+	const formatsAtPreviousLineIndex = formats[ previousLineIndex ] || [];
 
 	// The the indentation of the current line is greater than previous line,
 	// then the line cannot be furter indented.
-	if ( formatsAtLineIndex.length > targetFormats.length ) {
+	if ( formatsAtLineIndex.length > formatsAtPreviousLineIndex.length ) {
 		return value;
 	}
 
 	const newFormats = formats.slice();
+	const targetLevelLineIndex = getTargetLevelLineIndex( value, lineIndex );
 
 	for ( let index = lineIndex; index < end; index++ ) {
 		if ( text[ index ] !== LINE_SEPARATOR ) {
 			continue;
 		}
 
-		// If the indentation of the previous line is the same as the current
-		// line, then duplicate the type and append all current types. E.g.
-		//
-		// 1. one
-		// 2. two <= Selected
-		//   * three <= Selected
-		//
-		// should become:
-		//
-		// 1. one
-		//   1. two <= Selected
-		//     * three <= Selected
-		//
-		//   ^ Inserted list
-		//
-		// Otherwise take the target formats and append traling lists. E.g.
-		//
-		// 1. one
-		//   * target
-		// 2. two <= Selected
-		//   * three <= Selected
-		//
-		// should become:
-		//
-		// 1. one
-		//   * target
-		//   * two <= Selected
-		//     * three <= Selected
-		//
-		if ( targetFormats.length === formatsAtLineIndex.length ) {
+		// Get the previous list, and if there's a child list, take over the
+		// formats. If not, duplicate the last level and create a new level.
+		if ( targetLevelLineIndex ) {
+			const targetFormats = formats[ targetLevelLineIndex ] || [];
+			newFormats[ index ] = targetFormats.concat(
+				( newFormats[ index ] || [] ).slice( targetFormats.length - 1 )
+			);
+		} else {
+			const targetFormats = formats[ previousLineIndex ] || [];
 			const lastformat = targetFormats[ targetFormats.length - 1 ] || rootFormat;
 
 			newFormats[ index ] = targetFormats.concat(
 				[ lastformat ],
 				( newFormats[ index ] || [] ).slice( targetFormats.length )
-			);
-		} else {
-			newFormats[ index ] = targetFormats.concat(
-				( newFormats[ index ] || [] ).slice( targetFormats.length - 1 )
 			);
 		}
 	}
