1eae8ea6cf3fa1b11a9cd01e097b9b8aa40e80df
Robert Anderson <robert@noisysocks.com>
2019-02-01 10:44:37 +1100

    Search block: Add aria-label to placeholder <input>

--- packages/block-library/src/search/edit.js
+++ packages/block-library/src/search/edit.js
@@ -11,25 +11,26 @@ export default function SearchEdit( { className, attributes, setAttributes } ) {
 		<div className={ className }>
 			<RichText
 				wrapperClassName="wp-block-search__label"
-				value={ label }
 				placeholder={ __( 'Add label' ) }
 				keepPlaceholderOnFocus
 				formattingControls={ [] }
+				value={ label }
 				onChange={ ( html ) => setAttributes( { label: html } ) }
 			/>
 			<input
 				className="wp-block-search__input"
-				value={ placeholder }
+				aria-label={ __( 'Optional placeholder' ) }
 				placeholder={ __( 'Optional placeholder' ) }
+				value={ placeholder }
 				onChange={ ( event ) => setAttributes( { placeholder: event.target.value } ) }
 			/>
 			<RichText
 				wrapperClassName="wp-block-search__button"
 				className="wp-block-search__button-rich-text"
-				value={ buttonText }
 				placeholder={ __( 'Add button text' ) }
 				keepPlaceholderOnFocus
 				formattingControls={ [] }
+				value={ buttonText }
 				onChange={ ( html ) => setAttributes( { buttonText: html } ) }
 			/>
 		</div>
