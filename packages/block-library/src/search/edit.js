d87c67dee3cf346e329fcdbbdd09790b0f4887f3
Robert Anderson <robert@noisysocks.com>
2019-02-01 11:07:53 +1100

    Search block: Use ellipsis at end of input placeholders

--- packages/block-library/src/search/edit.js
+++ packages/block-library/src/search/edit.js
@@ -12,7 +12,7 @@ export default function SearchEdit( { className, attributes, setAttributes } ) {
 			<RichText
 				wrapperClassName="wp-block-search__label"
 				aria-label={ __( 'Label text' ) }
-				placeholder={ __( 'Add label' ) }
+				placeholder={ __( 'Add label…' ) }
 				keepPlaceholderOnFocus
 				formattingControls={ [] }
 				value={ label }
@@ -21,7 +21,7 @@ export default function SearchEdit( { className, attributes, setAttributes } ) {
 			<input
 				className="wp-block-search__input"
 				aria-label={ __( 'Optional placeholder text' ) }
-				placeholder={ __( 'Optional placeholder' ) }
+				placeholder={ __( 'Optional placeholder…' ) }
 				value={ placeholder }
 				onChange={ ( event ) => setAttributes( { placeholder: event.target.value } ) }
 			/>
@@ -29,7 +29,7 @@ export default function SearchEdit( { className, attributes, setAttributes } ) {
 				wrapperClassName="wp-block-search__button"
 				className="wp-block-search__button-rich-text"
 				aria-label={ __( 'Button text' ) }
-				placeholder={ __( 'Add button text' ) }
+				placeholder={ __( 'Add button text…' ) }
 				keepPlaceholderOnFocus
 				formattingControls={ [] }
 				value={ buttonText }
