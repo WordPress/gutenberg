8b52a1aad49b780f4bea2ea5f9bf448b4bc982c1
Diego Rey Mendez <diegoreymendez@users.noreply.github.com>
2019-02-01 14:33:16 -0300

    Remaps some font names so that they work in iOS. (#13628)
    * Remaps some font names so that they work in iOS.
    
    * Improved the logic for picking the default font in some components.
    
    * Modifies the logic that sets the default font for the code component.
    
    * Changes the font family in a css file.
    
    * Adds an import to have a default font for native.
    
    * Standardizes the default font for the code component.
    
    * Simplifies the default font for the code block.
    
    * Simplifies the default font for the code block.
    
    * Configures the default font for plain-text from a css file.
    
    * Fixes the styling for the rich text components and restores a line that was removed by mistake.
    
    * Fixes a linting problem.
    
    * Fixes some linting issues.
    
    * Fixes a linting issue.

--- packages/block-library/src/code/edit.native.js
+++ packages/block-library/src/code/edit.native.js
@@ -36,7 +36,7 @@ export default function CodeEdit( props ) {
 				isSelected={ props.isSelected }
 				onFocus={ onFocus }
 				onBlur={ onBlur }
-				fontFamily={ 'monospace' }
+				fontFamily={ ( styles.blockCode.fontFamily ) }
 			/>
 		</View>
 	);
