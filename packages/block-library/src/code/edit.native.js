4828dc05eb5bd67e1449b3c50ba1654b8bbca3c1
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-02-01 12:24:40 -0300

    Standardizes the default font for the code component.

--- packages/block-library/src/code/edit.native.js
+++ packages/block-library/src/code/edit.native.js
@@ -36,7 +36,7 @@ export default function CodeEdit( props ) {
 				isSelected={ props.isSelected }
 				onFocus={ onFocus }
 				onBlur={ onBlur }
-				fontFamily={ ( Platform.OS === 'ios' ? 'Courier' : 'monospace' ) }
+				fontFamily={ ( styles.blockCode.fontFamily ) }
 			/>
 		</View>
 	);
