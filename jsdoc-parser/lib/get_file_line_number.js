b5c6c15ceeb82e2dd08df6b013a3fc7e9b2456e1
Andrés <andres.maneiro@automattic.com>
2019-02-05 17:50:57 +0100

    Use jsdoc-parser from node_modules

--- jsdoc-parser/lib/get_file_line_number.js
+++
@@ -1,8 +0,0 @@
-const fs = require( "fs" );
-const getLineFromPos = require( "get-line-from-pos" );
-
-module.exports = function( file_path, position ) {
-	var fileContents = fs.readFileSync( file_path, "utf-8" );
-
-	return getLineFromPos( fileContents, position );
-};
