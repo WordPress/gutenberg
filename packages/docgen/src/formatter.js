3de973dc08e8badcbf8399393616ce8b8e20cd54
Andrés <andres.maneiro@automattic.com>
2019-02-06 17:28:46 +0100

    Markdown: sort output by symbol name

--- packages/docgen/src/formatter.js
+++ packages/docgen/src/formatter.js
@@ -36,6 +36,17 @@ module.exports = function( artifacts ) {
 	const docs = [ '# API' ];
 	docs.push( '\n' );
 	docs.push( '\n' );
+	artifacts.sort( ( first, second ) => {
+		const firstName = first.name.toUpperCase();
+		const secondName = second.name.toUpperCase();
+		if ( firstName < secondName ) {
+			return -1;
+		}
+		if ( firstName > secondName ) {
+			return 1;
+		}
+		return 0;
+	} );
 	if ( artifacts && artifacts.length > 0 ) {
 		artifacts.forEach( ( artifact ) => {
 			docs.push( `## ${ artifact.name }` );
