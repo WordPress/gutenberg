48684f5782dabeb015eab098d3786dc4c40c70f1
Kjell Reigstad <kjell@kjellr.com>
2019-02-07 05:17:13 -0500

    Fix rounded corners in block icons (#13659)
    * Restore rounded corners to the Gallery icon.
    
    * Restore rounded corners to the File block icon.
    
    * Restore rounded corners to the Video icon.
    
    * Restore rounded corners to the Table block icon.
    
    * Restore rounded corners to the button icon.
    
    * Restore rounded corners to the Archives block icon.
    
    * Restore rounded corners to the Latest Comments block icon.
    
    * Restore rounded corners to the audio embed block icon.
    
    * Use the correct button icon.
    
    I originally swapped this out with the 7x5 version instead of tnhe 16x9 version we'd been using previously.
    
    * Restore rounded corners to the columns block icon.
    
    * Update tests

--- packages/block-library/src/columns/index.js
+++ packages/block-library/src/columns/index.js
@@ -70,7 +70,7 @@ export const name = 'core/columns';
 export const settings = {
 	title: __( 'Columns' ),
 
-	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><G><Path d="M21 4H3L2 5v14l1 1h18l1-1V5l-1-1zM8 18H4V6h4v12zm6 0h-4V6h4v12zm6 0h-4V6h4v12z" /></G></SVG>,
+	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><G><Path d="M4,4H20a2,2,0,0,1,2,2V18a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4ZM4 6V18H8V6Zm6 0V18h4V6Zm6 0V18h4V6Z" /></G></SVG>,
 
 	category: 'layout',
 
