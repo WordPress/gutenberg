adcde1f6775f237d7c685e001a9a82bb4b7e61fa
Robert Anderson <robert@noisysocks.com>
2019-02-07 11:38:06 +1100

    Add search block (#13583)
    * Adding search widget block
    
    Co-authored-by: Rachel Cherry <bamadesigner@gmail.com>
    
    * Search block: Move away from legacy get_search_form() markup
    
    Changes the Search block to render its own search form markup, instead
    of the same markup that get_search_form() renders. This lets us fully
    match the design spec.
    
    * s/Placeholder text/Placeholder Text/
    
    Co-Authored-By: noisysocks <robert@noisysocks.com>
    
    * Remove superfluous 'your'
    
    Co-Authored-By: noisysocks <robert@noisysocks.com>
    
    * Search: Add button and inline placeholder editing
    
    * Update CHANGELOG
    
    * Search block: Support custom CSS classes
    
    * Search block: Add aria-label to placeholder <input>
    
    * Search block: Style label as bold in theme.scss
    
    * Search block: Add aria-label to all inputs
    
    * Search block: Use ellipsis at end of input placeholders
    
    * Search block: Display placeholder in 'placeholder' attribute, not 'value'
    
    * Revert "Search block: Display placeholder in 'placeholder' attribute, not 'value'"
    
    This reverts commit 154cddcacb1b577b2c40cee5c901cb47e6b5e472.
    
    * Search block: Remove placeholder attribute when there is a placeholder value
    
    * Search block: Fix placeholder attribute receiving false instead of undefined

---
+++ packages/block-library/src/search/index.js
@@ -0,0 +1,29 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import edit from './edit';
+
+export const name = 'core/search';
+
+export const settings = {
+	title: __( 'Search' ),
+
+	description: __( 'Help visitors find your content.' ),
+
+	icon: 'search',
+
+	category: 'widgets',
+
+	keywords: [ __( 'find' ) ],
+
+	edit,
+
+	save() {
+		return null;
+	},
+};
