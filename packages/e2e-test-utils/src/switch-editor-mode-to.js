238d35b5568077fafb656bba42561799baa4a471
Riad Benguella <benguella@gmail.com>
2019-02-08 12:02:46 +0100

    Add a plugin to disable the CSS animation for more stable e2e tests

--- packages/e2e-test-utils/src/switch-editor-mode-to.js
+++ packages/e2e-test-utils/src/switch-editor-mode-to.js
@@ -1,9 +1,4 @@
 /**
- * Internal dependencies
- */
-import { waitForAnimation } from './wait-for-animation';
-
-/**
  * Switches editor mode.
  *
  * @param {string} mode String editor mode.
@@ -12,7 +7,6 @@ export async function switchEditorModeTo( mode ) {
 	await page.click(
 		'.edit-post-more-menu [aria-label="Show more tools & options"]'
 	);
-	await waitForAnimation();
 	const [ button ] = await page.$x(
 		`//button[contains(text(), '${ mode } Editor')]`
 	);
