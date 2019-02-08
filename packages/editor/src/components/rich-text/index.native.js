28d1d681a6b09524cf5bdb6f6b316e949559f323
Pinar Olguc <pinarolguc@gmail.com>
2019-02-08 13:38:26 +0300

    [Mobile]Remove unnecessary <strong> tags from post title (#13763)
    * Add extra rootTagsToEliminate prop
    
    * Fix lint issues
    
    * Revert unnecessary prop

--- packages/editor/src/components/rich-text/index.native.js
+++ packages/editor/src/components/rich-text/index.native.js
@@ -170,8 +170,18 @@ export class RichText extends Component {
 	 */
 
 	removeRootTagsProduceByAztec( html ) {
-		const openingTagRegexp = RegExp( '^<' + this.props.tagName + '>', 'gim' );
-		const closingTagRegexp = RegExp( '</' + this.props.tagName + '>$', 'gim' );
+		let result = this.removeRootTag( this.props.tagName, html );
+		if ( this.props.rootTagsToEliminate ) {
+			this.props.rootTagsToEliminate.forEach( ( element ) => {
+				result = this.removeRootTag( element, result );
+			} );
+		}
+		return result;
+	}
+
+	removeRootTag( tag, html ) {
+		const openingTagRegexp = RegExp( '^<' + tag + '>', 'gim' );
+		const closingTagRegexp = RegExp( '</' + tag + '>$', 'gim' );
 		return html.replace( openingTagRegexp, '' ).replace( closingTagRegexp, '' );
 	}
 
