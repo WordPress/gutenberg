83958ae7ea2113ad95a5becedf1e2cdaecd16ff2
Jonny Harris <jon@spacedmonkey.co.uk>
2019-02-11 23:24:24 +0000

    Check if thumbnail_url is set in getPhotoHtml
    thumbnail_url is an optional field in oembed endpoints. Check it is set first before using and fallback to url if not.

--- packages/block-library/src/embed/util.js
+++ packages/block-library/src/embed/util.js
@@ -51,8 +51,9 @@ export const isFromWordPress = ( html ) => {
 
 export const getPhotoHtml = ( photo ) => {
 	// 100% width for the preview so it fits nicely into the document, some "thumbnails" are
-	// actually the full size photo.
-	const photoPreview = <p><img src={ photo.thumbnail_url } alt={ photo.title } width="100%" /></p>;
+	// actually the full size photo. If thumbnails not found, use full image.
+	const imageUrl = ( photo.thumbnail_url ) ? photo.thumbnail_url : photo.url;
+	const photoPreview = <p><img src={ imageUrl } alt={ photo.title } width="100%" /></p>;
 	return renderToString( photoPreview );
 };
 
