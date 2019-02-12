773bc4e3f5536766e47132c8a93cc625ad679394
Joen Asmussen <asmussen@gmail.com>
2019-02-12 10:30:34 +0100

    Address feedback.
    Props @aduth for essentially writing this PR.

--- packages/components/src/icon-button/index.js
+++ packages/components/src/icon-button/index.js
@@ -23,7 +23,7 @@ class IconButton extends Component {
 		const { icon, children, label, className, tooltip, shortcut, labelPosition, ...additionalProps } = this.props;
 		const { 'aria-pressed': ariaPressed } = this.props;
 		const classes = classnames( 'components-icon-button', className, {
-			'has-text': children && children.length > 2,
+			'has-text': children,
 		} );
 		const tooltipText = tooltip || label;
 
