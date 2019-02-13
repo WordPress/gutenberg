1c398b697718f6a48d216f9a6a723ce1b080b9f3
Andrew Serong <andrewserong@gmail.com>
2019-02-14 08:24:12 +1100

    Move heading e2e tests to the heading block, remove extra active class check (#10326)

--- packages/e2e-tests/specs/blocks/heading.test.js
+++ packages/e2e-tests/specs/blocks/heading.test.js
@@ -5,6 +5,8 @@ import {
 	clickBlockAppender,
 	getEditedPostContent,
 	createNewPost,
+	insertBlock,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'Separator', () => {
@@ -24,6 +26,23 @@ describe( 'Separator', () => {
 		await page.keyboard.type( '4' );
 		await page.keyboard.press( 'ArrowLeft' );
 		await page.keyboard.type( '#### ' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should create a paragraph block above when pressing enter at start of heading', async () => {
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Abc' );
+		await pressKeyTimes( 'ArrowLeft', 3 );
+		await page.keyboard.press( 'Enter' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should create a paragraph block below when pressing enter at end of heading', async () => {
+		await insertBlock( 'Heading' );
+		await page.keyboard.type( 'Abc' );
+		await page.keyboard.press( 'Enter' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
