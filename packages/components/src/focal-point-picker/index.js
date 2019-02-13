7a709625235e61dd0ce9c6c99d52bad7e6decff3
m-e-h <info@martyhelmick.com>
2019-02-13 13:44:31 -0500

    lint

--- packages/components/src/focal-point-picker/index.js
+++ packages/components/src/focal-point-picker/index.js
@@ -14,7 +14,7 @@ import { withInstanceId, compose } from '@wordpress/compose';
  * Internal dependencies
  */
 import BaseControl from '../base-control';
-import NumberControl from "../number-control";
+import NumberControl from '../number-control';
 import withFocusOutside from '../higher-order/with-focus-outside';
 import { Path, SVG } from '../primitives';
 
@@ -97,15 +97,11 @@ export class FocalPointPicker extends Component {
 			};
 			const left = Math.max(
 				bounds.left,
-				Math.min(
-					cursorPosition.left, bounds.right
-				)
+				Math.min( cursorPosition.left, bounds.right )
 			);
 			const top = Math.max(
 				bounds.top,
-				Math.min(
-					cursorPosition.top, bounds.bottom
-				)
+				Math.min( cursorPosition.top, bounds.bottom )
 			);
 			const percentages = {
 				x: ( left - bounds.left ) / ( pickerDimensions.width - ( bounds.left * 2 ) ),
@@ -145,7 +141,9 @@ export class FocalPointPicker extends Component {
 			return {
 				width: this.containerRef.current.clientWidth,
 				height: this.containerRef.current.clientHeight,
-				top: this.containerRef.current.getBoundingClientRect().top + document.body.scrollTop,
+				top:
+					this.containerRef.current.getBoundingClientRect().top +
+					document.body.scrollTop,
 				left: this.containerRef.current.getBoundingClientRect().left,
 			};
 		}
@@ -202,39 +200,47 @@ export class FocalPointPicker extends Component {
 							draggable="false"
 						/>
 						<div className={ iconContainerClasses } style={ iconContainerStyle }>
-							<SVG className="components-focal-point-picker__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-								<Path className="components-focal-point-picker__icon-outline" d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z" />
-								<Path className="components-focal-point-picker__icon-fill" d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z" />
+							<SVG
+								className="components-focal-point-picker__icon"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 30 30"
+							>
+								<Path
+									className="components-focal-point-picker__icon-outline"
+									d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"
+								/>
+								<Path
+									className="components-focal-point-picker__icon-fill"
+									d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z"
+								/>
 							</SVG>
 						</div>
 					</div>
 				</div>
 				<div className="components-focal-point-picker_position-display-container">
 					<BaseControl label={ __( 'Horizontal Pos.' ) }>
-
-					<NumberControl
-						className="components-foval-point-control__number"
-						id={ horizontalPositionId }
-						max={ TEXTCONTROL_MAX }
-						min={ TEXTCONTROL_MIN }
-						aria-label={label}
-						value={ this.fractionToPercentage( percentages.x ) }
-						onChange={ this.horizontalPositionChanged }
-					/>
+						<NumberControl
+							className="components-foval-point-control__number"
+							id={ horizontalPositionId }
+							max={ TEXTCONTROL_MAX }
+							min={ TEXTCONTROL_MIN }
+							aria-label={ label }
+							value={ this.fractionToPercentage( percentages.x ) }
+							onChange={ this.horizontalPositionChanged }
+						/>
 
 						<span>%</span>
 					</BaseControl>
 					<BaseControl label={ __( 'Vertical Pos.' ) }>
-
-					<NumberControl
-						className="components-foval-point-control__number"
-						id={ verticalPositionId }
-						max={ TEXTCONTROL_MAX }
-						min={ TEXTCONTROL_MIN }
-						aria-label={label}
-						value={ this.fractionToPercentage( percentages.y ) }
-						onChange={ this.verticalPositionChanged }
-					/>
+						<NumberControl
+							className="components-foval-point-control__number"
+							id={ verticalPositionId }
+							max={ TEXTCONTROL_MAX }
+							min={ TEXTCONTROL_MIN }
+							aria-label={ label }
+							value={ this.fractionToPercentage( percentages.y ) }
+							onChange={ this.verticalPositionChanged }
+						/>
 
 						<span>%</span>
 					</BaseControl>
