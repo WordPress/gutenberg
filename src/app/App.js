95b589f10fd2b09a50f5b3c22060ababc985564e
Tugdual de Kerviler <dekervit@gmail.com>
2019-02-14 17:42:45 +0100

    Fix imports

--- src/app/App.js
+++ src/app/App.js
@@ -6,25 +6,12 @@ import '../globals';
 import React from 'react';
 
 // Gutenberg imports
-import { registerCoreBlocks } from '@wordpress/block-library';
-import { registerBlockType, setUnregisteredTypeHandlerName, unregisterBlockType } from '@wordpress/blocks';
 import { setLocaleData } from '@wordpress/i18n';
 
 import AppContainer from './AppContainer';
 import initialHtml from './initial-html';
 
-import * as UnsupportedBlock from '../block-types/unsupported-block/';
 import { getTranslation } from '../../i18n-cache';
-
-registerCoreBlocks();
-registerBlockType( UnsupportedBlock.name, UnsupportedBlock.settings );
-setUnregisteredTypeHandlerName( UnsupportedBlock.name );
-
-// disable Code and More blocks for release
-if ( ! __DEV__ ) {
-	unregisterBlockType( 'core/code' );
-	unregisterBlockType( 'core/more' );
-}
 
 type PropsType = {
 	initialData: string,
