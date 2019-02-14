1487f8cbe3273be3d236a4d6a2cc91cd48b8d3b6
Matthew Kevins <mmkevins@yahoo.com>
2019-02-15 03:11:29 +1000

    Fix lint errors on jsdom-patches

--- src/jsdom-patches.js
+++ src/jsdom-patches.js
@@ -1,13 +1,16 @@
 /** @flow
  * @format */
 
+/**
+ * External dependencies
+ */
 import jsdom from 'jsdom-jscore';
 import jsdomLevel1Core from 'jsdom-jscore/lib/jsdom/level1/core';
 
 // must be called to initialize jsdom before patching prototypes
-const doc = jsdom.html( '', null, null );
+jsdom.html( '', null, null );
 
-const { core } = jsdomLevel1Core.dom.level1; 
+const { core } = jsdomLevel1Core.dom.level1;
 const { Node, Element } = core;
 
 // Exception codes
@@ -23,22 +26,23 @@ const {
 	DOCUMENT_FRAGMENT_NODE,
 } = Node;
 
-Node.prototype.contains = function(otherNode) {
-	return this === otherNode
-		|| Array.prototype.some.call(this._childNodes, childNode => {
-			return childNode.contains(otherNode);
-		});
+Node.prototype.contains = function( otherNode ) {
+	return this === otherNode ||
+		Array.prototype.some.call( this._childNodes, ( childNode ) => {
+			return childNode.contains( otherNode );
+		} );
 };
 
 // copied from jsdom-jscore, but without WRONG_DOCUMENT_ERR exception
-Node.prototype.insertBefore =  function(/* Node */ newChild, /* Node*/ refChild) {
-	if (this._readonly === true) {
-		throw new core.DOMException(NO_MODIFICATION_ALLOWED_ERR, 'Attempting to modify a read-only node');
+Node.prototype.insertBefore = function( /* Node */ newChild, /* Node*/ refChild ) {
+	if ( this._readonly === true ) {
+		throw new core.DOMException( NO_MODIFICATION_ALLOWED_ERR, 'Attempting to modify a read-only node' );
 	}
 
 	// Adopt unowned children, for weird nodes like DocumentType
-	if (!newChild._ownerDocument) newChild._ownerDocument = this._ownerDocument;
-
+	if ( ! newChild._ownerDocument ) {
+		newChild._ownerDocument = this._ownerDocument;
+	}
 
 	/*
 	 * This is commented out to prevent WrongDocumentError
@@ -50,46 +54,49 @@ Node.prototype.insertBefore =  function(/* Node */ newChild, /* Node*/ refChild)
 	}
 	*/
 
-	if (newChild.nodeType && newChild.nodeType === ATTRIBUTE_NODE) {
-		throw new core.DOMException(HIERARCHY_REQUEST_ERR);
+	if ( newChild.nodeType && newChild.nodeType === ATTRIBUTE_NODE ) {
+		throw new core.DOMException( HIERARCHY_REQUEST_ERR );
 	}
 
 	// search for parents matching the newChild
-	var current = this;
+	let current = this;
 	do {
-		if (current === newChild) {
-			throw new core.DOMException(HIERARCHY_REQUEST_ERR);
+		if ( current === newChild ) {
+			throw new core.DOMException( HIERARCHY_REQUEST_ERR );
 		}
-	} while((current = current._parentNode));
+	} while ( ( current = current._parentNode ) );
 
 	// fragments are merged into the element
-	if (newChild.nodeType === DOCUMENT_FRAGMENT_NODE) {
-		var tmpNode, i = newChild._childNodes.length;
-		while (i-- > 0) {
-			tmpNode = newChild.removeChild(newChild.firstChild);
-			this.insertBefore(tmpNode, refChild);
+	if ( newChild.nodeType === DOCUMENT_FRAGMENT_NODE ) {
+		let tmpNode,
+			i = newChild._childNodes.length;
+		while ( i-- > 0 ) {
+			tmpNode = newChild.removeChild( newChild.firstChild );
+			this.insertBefore( tmpNode, refChild );
 		}
-	} else if (newChild === refChild) {
+	} else if ( newChild === refChild ) {
 		return newChild;
 	} else {
 		// if the newChild is already in the tree elsewhere, remove it first
-		if (newChild._parentNode) {
-			newChild._parentNode.removeChild(newChild);
+		if ( newChild._parentNode ) {
+			newChild._parentNode.removeChild( newChild );
 		}
 
-		if (refChild == null) {
+		if ( refChild === null ) {
+			// eslint-disable-next-line no-var
 			var refChildIndex = this._childNodes.length;
 		} else {
-			var refChildIndex = this._indexOf(refChild);
-			if (refChildIndex == -1) {
-				throw new core.DOMException(NOT_FOUND_ERR);
+			// eslint-disable-next-line no-redeclare, no-var
+			var refChildIndex = this._indexOf( refChild );
+			if ( refChildIndex === -1 ) {
+				throw new core.DOMException( NOT_FOUND_ERR );
 			}
 		}
 
-		Array.prototype.splice.call(this._childNodes, refChildIndex, 0, newChild);
+		Array.prototype.splice.call( this._childNodes, refChildIndex, 0, newChild );
 
 		newChild._parentNode = this;
-		if (this._attached && newChild._attach) {
+		if ( this._attached && newChild._attach ) {
 			newChild._attach();
 		}
 
