c1235d4486b36bab451f57866dc323451dc76963
Daniel Richards <daniel.p.richards@gmail.com>
2019-02-15 19:51:54 +0800

    Add feature flags for Phase 2 (#13324)
    * Add new package for editor configuration, initially containing just feature flags
    
    Rework build commands to use correct NODE_ENV for feature flags
    
    * Revert "Rework build commands to use correct NODE_ENV for feature flags"
    
    This reverts commit 4cb0a39282d57abb8cd5c398ae20b9466d67b056.
    
    Revert "Add new package for editor configuration, initially containing just feature flags"
    
    This reverts commit 0c21fc215b4e6f0d4b415aabb9f8297635b7d148.
    
    * Switch to using webpack define plugin to inject a global GUTENBERG_PHASE variable
    
    * Iterate: use window.GUTENBERG_PHASE to avoid thrown errors from an undefined global
    
    * Add custom eslint rule for usage of GUTENBERG_PHASE
    
    * Disable new eslint rule when used in webpack config
    
    * Add readme
    
    * Include phase 2 features in e2e tests
    
    * Allow use of GUTENBERG_PHASE in a ternary and update documentation.
    
    * Add links to docs
    
    * Minor docs changes
    
    * Switch from window.GUTENBERG_PHASE to process.env.GUTENBERG_PHASE
    
    * Update docs for feature flags. Move `Basic Use` section higher up, and simplify a sentence
    
    * Ensure GUTENBERG_PHASE environment variable is available for webpack
    
    * Ensure GUTENBERG_PHASE is a number
    
    * Ensure GUTENBERG_PHASE is set in unit tests
    
    * Use <rootDir> in jest config
    
    * switch to using package.json config to define the value of GUTENBERG_PHASE
    
    * Sort custom lint rules alphabetically
    
    * Add comment about GUTENBERG_PHASE
    
    * Update jest config for GUTENBERG_PHASE
    
    * Add webpack as dependency to main package.json

--- packages/e2e-tests/jest.config.js
+++ packages/e2e-tests/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
 	setupTestFrameworkScriptFile: '<rootDir>/config/setup-test-framework.js',
+	setupFiles: [
+		'<rootDir>/config/gutenberg-phase.js',
+	],
 };
