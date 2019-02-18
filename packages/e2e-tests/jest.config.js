242e873a33ef5c7ec305beae526406d5b4c188c5
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-02-18 15:11:29 +0100

    Upgrade Jest to version 24 (breaking changes)

--- packages/e2e-tests/jest.config.js
+++ packages/e2e-tests/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
-	setupTestFrameworkScriptFile: '<rootDir>/config/setup-test-framework.js',
 	setupFiles: [
 		'<rootDir>/config/gutenberg-phase.js',
+	],
+	setupFilesAfterEnv: [
+		'<rootDir>/config/setup-test-framework.js',
 	],
 };
