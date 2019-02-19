ba740126ec9d57f97fa3c4107b4e6762a2068964
Jorge <jorge.costa@developer.pt>
2019-02-19 13:25:31 +0000

    Added deprecations

--- packages/block-library/src/media-text/index.js
+++ packages/block-library/src/media-text/index.js
@@ -202,6 +202,51 @@ export const settings = {
 					mediaType,
 					mediaUrl,
 					mediaWidth,
+					mediaId,
+				} = attributes;
+				const mediaTypeRenders = {
+					image: () => <img src={ mediaUrl } alt={ mediaAlt } className={ ( mediaId && mediaType === 'image' ) ? `wp-image-${ mediaId }` : null } />,
+					video: () => <video controls src={ mediaUrl } />,
+				};
+				const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+				const className = classnames( {
+					'has-media-on-the-right': 'right' === mediaPosition,
+					[ backgroundClass ]: backgroundClass,
+					'is-stacked-on-mobile': isStackedOnMobile,
+				} );
+
+				let gridTemplateColumns;
+				if ( mediaWidth !== DEFAULT_MEDIA_WIDTH ) {
+					gridTemplateColumns = 'right' === mediaPosition ? `auto ${ mediaWidth }%` : `${ mediaWidth }% auto`;
+				}
+				const style = {
+					backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+					gridTemplateColumns,
+				};
+				return (
+					<div className={ className } style={ style }>
+						<figure className="wp-block-media-text__media" >
+							{ ( mediaTypeRenders[ mediaType ] || noop )() }
+						</figure>
+						<div className="wp-block-media-text__content">
+							<InnerBlocks.Content />
+						</div>
+					</div>
+				);
+			},
+		},
+		{
+			attributes: blockAttributes,
+			save( { attributes } ) {
+				const {
+					backgroundColor,
+					customBackgroundColor,
+					isStackedOnMobile,
+					mediaAlt,
+					mediaPosition,
+					mediaType,
+					mediaUrl,
+					mediaWidth,
 				} = attributes;
 				const mediaTypeRenders = {
 					image: () => <img src={ mediaUrl } alt={ mediaAlt } />,
--- packages/block-library/src/pullquote/index.js
+++ packages/block-library/src/pullquote/index.js
@@ -143,6 +143,52 @@ export const settings = {
 			...blockAttributes,
 		},
 		save( { attributes } ) {
+			const { mainColor, customMainColor, textColor, customTextColor, value, citation, className } = attributes;
+			const isSolidColorStyle = includes( className, SOLID_COLOR_CLASS );
+
+			let figureClass, figureStyles;
+			// Is solid color style
+			if ( isSolidColorStyle ) {
+				figureClass = getColorClassName( 'background-color', mainColor );
+				if ( ! figureClass ) {
+					figureStyles = {
+						backgroundColor: customMainColor,
+					};
+				}
+			// Is normal style and a custom color is being used ( we can set a style directly with its value)
+			} else if ( customMainColor ) {
+				figureStyles = {
+					borderColor: customMainColor,
+				};
+			// Is normal style and a named color is being used, we need to retrieve the color value to set the style,
+			// as there is no expectation that themes create classes that set border colors.
+			} else if ( mainColor ) {
+				const colors = get( select( 'core/editor' ).getEditorSettings(), [ 'colors' ], [] );
+				const colorObject = getColorObjectByAttributeValues( colors, mainColor );
+				figureStyles = {
+					borderColor: colorObject.color,
+				};
+			}
+
+			const blockquoteTextColorClass = getColorClassName( 'color', textColor );
+			const blockquoteClasses = textColor || customTextColor ? classnames( 'has-text-color', {
+				[ blockquoteTextColorClass ]: blockquoteTextColorClass,
+			} ) : undefined;
+			const blockquoteStyle = blockquoteTextColorClass ? undefined : { color: customTextColor };
+			return (
+				<figure className={ figureClass } style={ figureStyles }>
+					<blockquote className={ blockquoteClasses } style={ blockquoteStyle } >
+						<RichText.Content value={ value } multiline />
+						{ ! RichText.isEmpty( citation ) && <RichText.Content tagName="cite" value={ citation } /> }
+					</blockquote>
+				</figure>
+			);
+		},
+	}, {
+		attributes: {
+			...blockAttributes,
+		},
+		save( { attributes } ) {
 			const { value, citation } = attributes;
 			return (
 				<blockquote>
