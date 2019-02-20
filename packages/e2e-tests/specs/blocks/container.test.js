2ad9ee42ddba88bfc7b31b0ecd154f7c0447f500
Daniel Richards <daniel.richards@automattic.com>
2019-02-20 17:09:28 +0800

    Add e2e tests for the container block

--- packages/e2e-tests/specs/blocks/container.test.js
+++ packages/e2e-tests/specs/blocks/container.test.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import {
+	clickBlockAppender,
 	searchForBlock,
 	getEditedPostContent,
 	createNewPost,
@@ -12,9 +13,18 @@ describe( 'Container', () => {
 		await createNewPost();
 	} );
 
-	it( 'can be created by using the block inserter', async () => {
+	it( 'can be created using the block inserter', async () => {
 		await searchForBlock( 'Container' );
 		await page.click( '.editor-block-list-item-container' );
+		await page.keyboard.type( 'Container block' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'can be created using the slash inserter', async () => {
+		await clickBlockAppender();
+		await page.keyboard.type( '/container' );
+		await page.keyboard.press( 'Enter' );
 		await page.keyboard.type( 'Container block' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
