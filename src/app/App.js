30879f433b029458d9bbef3ce9547e7c06751f3c
Tugdual de Kerviler <dekervit@gmail.com>
2019-02-22 13:09:03 +0100

    Group initialization logic into the index file

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,10 @@
 /** @flow
  * @format */
 
-import { I18nManager } from 'react-native';
 import React from 'react';
-
-// Gutenberg imports
-import apiFetch from '@wordpress/api-fetch';
-import * as data from '@wordpress/data';
-import { registerCoreBlocks } from '@wordpress/block-library';
-import { registerBlockType, setUnregisteredTypeHandlerName, unregisterBlockType } from '@wordpress/blocks';
-import '@wordpress/format-library';
 
 import AppContainer from './AppContainer';
 import initialHtml from './initial-html';
-import * as UnsupportedBlock from '../block-types/unsupported-block';
 
 type PropsType = {
 	initialData: string,
@@ -22,38 +13,6 @@ type PropsType = {
 };
 
 export default class AppProvider extends React.Component<PropsType> {
-	constructor( props: PropsType ) {
-		super( props );
-		this.gutenbergSetup();
-		this.editorSetup();
-	}
-
-	gutenbergSetup() {
-		I18nManager.forceRTL( false ); // Change to `true` to debug RTL layout easily.
-
-		// wp-api-fetch
-		apiFetch.use( apiFetch.createRootURLMiddleware( 'https://public-api.wordpress.com/' ) );
-
-		// wp-data
-		const userId = 1;
-		const storageKey = 'WP_DATA_USER_' + userId;
-		data.use( data.plugins.persistence, { storageKey: storageKey } );
-		data.use( data.plugins.controls );
-	}
-
-	editorSetup() {
-		// register and setup blocks
-		registerCoreBlocks();
-		registerBlockType( UnsupportedBlock.name, UnsupportedBlock.settings );
-		setUnregisteredTypeHandlerName( UnsupportedBlock.name );
-
-		// disable Code and More blocks for release
-		if ( ! __DEV__ ) {
-			unregisterBlockType( 'core/code' );
-			unregisterBlockType( 'core/more' );
-		}
-	}
-
 	render() {
 		const { initialHtmlModeEnabled } = this.props;
 		let initialData = this.props.initialData;
