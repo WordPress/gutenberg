a3aa1e5902ae9d474cec2c36202fdb59ed6b2f0d
Andrew Duthie <andrew@andrewduthie.com>
2019-02-25 16:12:50 -0500

    Block Editor: Call onChange, onInput in componentDidUpdate

--- packages/block-editor/src/components/provider/index.js
+++ packages/block-editor/src/components/provider/index.js
@@ -3,61 +3,13 @@
  */
 import { Component } from '@wordpress/element';
 import { DropZoneProvider, SlotFillProvider } from '@wordpress/components';
-import { withDispatch, RegistryConsumer } from '@wordpress/data';
-import { createHigherOrderComponent, compose } from '@wordpress/compose';
-
-/**
- * Higher-order component which renders the original component with the current
- * registry context passed as its `registry` prop.
- *
- * @param {WPComponent} OriginalComponent Original component.
- *
- * @return {WPComponent} Enhanced component.
- */
-const withRegistry = createHigherOrderComponent(
-	( OriginalComponent ) => ( props ) => (
-		<RegistryConsumer>
-			{ ( registry ) => (
-				<OriginalComponent
-					{ ...props }
-					registry={ registry }
-				/>
-			) }
-		</RegistryConsumer>
-	),
-	'withRegistry'
-);
+import { withSelect, withDispatch } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
 
 class BlockEditorProvider extends Component {
 	componentDidMount() {
 		this.props.updateEditorSettings( this.props.settings );
 		this.props.resetBlocks( this.props.value );
-		this.attachChangeObserver( this.props.registry );
-	}
-
-	componentDidUpdate( prevProps ) {
-		const {
-			settings,
-			updateEditorSettings,
-			value,
-			resetBlocks,
-			registry,
-		} = this.props;
-
-		if ( settings !== prevProps.settings ) {
-			updateEditorSettings( settings );
-		}
-
-		if ( registry !== prevProps.registry ) {
-			this.attachChangeObserver( registry );
-		}
-
-		if ( this.isSyncingOutcomingValue ) {
-			this.isSyncingOutcomingValue = false;
-		} else if ( value !== prevProps.value ) {
-			this.isSyncingIncomingValue = true;
-			resetBlocks( value );
-		}
 	}
 
 	componentWillUnmount() {
@@ -66,61 +18,44 @@ class BlockEditorProvider extends Component {
 		}
 	}
 
-	/**
-	 * Given a registry object, overrides the default dispatch behavior for the
-	 * `core/block-editor` store to interpret a state change and decide whether
-	 * we should call `onChange` or `onInput` depending on whether the change
-	 * is persistent or not.
-	 *
-	 * This needs to be done synchronously after state changes (instead of using
-	 * `componentDidUpdate`) in order to avoid batching these changes.
-	 *
-	 * @param {WPDataRegistry} registry     Registry from which block editor
-	 *                                      dispatch is to be overriden.
-	 */
-	attachChangeObserver( registry ) {
-		if ( this.unsubscribe ) {
-			this.unsubscribe();
+	componentDidUpdate( prevProps ) {
+		const {
+			settings,
+			updateEditorSettings,
+			value,
+			resetBlocks,
+			blocks,
+			isLastBlockChangePersistent,
+			onChange,
+			onInput,
+		} = this.props;
+
+		if ( settings !== prevProps.settings ) {
+			updateEditorSettings( settings );
 		}
 
-		const {
-			getBlocks,
-			isLastBlockChangePersistent,
-		} = registry.select( 'core/block-editor' );
-
-		let blocks = getBlocks();
-		let isPersistent = isLastBlockChangePersistent();
-
-		this.unsubscribe = registry.subscribe( () => {
-			const {
-				onChange,
-				onInput,
-			} = this.props;
-			const newBlocks = getBlocks();
-			const newIsPersistent = isLastBlockChangePersistent();
-			if ( newBlocks !== blocks && this.isSyncingIncomingValue ) {
-				this.isSyncingIncomingValue = false;
-				blocks = newBlocks;
-				isPersistent = newIsPersistent;
-				return;
+		if ( ! this.syncedValue ) {
+			this.syncedValue = blocks;
+		} else if (
+			blocks !== prevProps.blocks ||
+			isLastBlockChangePersistent !== prevProps.isLastBlockChangePersistent
+		) {
+			const hasSyncedValue = this.syncedValue === blocks;
+			if ( ! hasSyncedValue ) {
+				this.syncedValue = blocks;
+				console.log( 'input' );
+				onInput( blocks );
 			}
 
-			if (
-				newBlocks !== blocks ||
-				// This happens when a previous input is explicitely marked as persistent.
-				( newIsPersistent && ! isPersistent )
-			) {
-				blocks = newBlocks;
-				isPersistent = newIsPersistent;
-
-				this.isSyncingOutcomingValue = true;
-				if ( isPersistent ) {
-					onChange( blocks );
-				} else {
-					onInput( blocks );
-				}
+			if ( isLastBlockChangePersistent ) {
+				console.log( 'change' );
+				onChange( blocks );
 			}
-		} );
+		} else if ( value !== prevProps.value && this.syncedValue !== value ) {
+			delete this.syncedValue;
+
+			resetBlocks( value );
+		}
 	}
 
 	render() {
@@ -137,6 +72,17 @@ class BlockEditorProvider extends Component {
 }
 
 export default compose( [
+	withSelect( ( select ) => {
+		const {
+			getBlocks,
+			isLastBlockChangePersistent,
+		} = select( 'core/block-editor' );
+
+		return {
+			blocks: getBlocks(),
+			isLastBlockChangePersistent: isLastBlockChangePersistent(),
+		};
+	} ),
 	withDispatch( ( dispatch ) => {
 		const {
 			updateEditorSettings,
@@ -148,5 +94,4 @@ export default compose( [
 			resetBlocks,
 		};
 	} ),
-	withRegistry,
 ] )( BlockEditorProvider );
