d4c1f807a74301feaa74b0d85e1ea2b61b24dfb2
Andrés <andres.maneiro@automattic.com>
2019-02-25 11:28:52 +0100

    Adapt formatter test to API

--- packages/docgen/tests/test-formatter-markdown.js
+++ packages/docgen/tests/test-formatter-markdown.js
@@ -9,10 +9,11 @@ const test = require( 'tape' );
 const formatter = require( '../src/formatter' );
 
 test( 'Formatter - returns markdown', ( t ) => {
-	const docs = formatter( [ { description: 'My declaration example.', tags: [], name: 'myDeclaration' } ] );
+	const docPath = process.cwd();
+	const docs = formatter( docPath, docPath + '-api.md', [ { path: docPath + '-code.js', description: 'My declaration example.', tags: [], name: 'myDeclaration', lineStart: 1, lineEnd: 2 } ], 'API docs' );
 	t.equal(
 		docs,
-		'# API\n\n## myDeclaration\n\nMy declaration example.\n'
+		'# API docs\n\n## myDeclaration\n\n[docgen/home/andres/src/gutenberg/packages/docgen-code.js#L1-L2](docgen/home/andres/src/gutenberg/packages/docgen-code.js#L1-L2)\n\nMy declaration example.\n'
 	);
 	t.end();
 } );
