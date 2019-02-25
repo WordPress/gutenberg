6768acba957c0dd3d7bd51192aaac45191dd5713
Andrew Duthie <andrew@andrewduthie.com>
2019-02-25 11:20:06 -0500

    Plugin: Remove PHP functions slated for removal in 5.2

--- lib/load.php
+++ lib/load.php
@@ -15,11 +15,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require dirname( __FILE__ ) . '/rest-api.php';
 }
 
-require dirname( __FILE__ ) . '/meta-box-partial-page.php';
-require dirname( __FILE__ ) . '/blocks.php';
 require dirname( __FILE__ ) . '/client-assets.php';
-require dirname( __FILE__ ) . '/compat.php';
-require dirname( __FILE__ ) . '/plugin-compat.php';
 require dirname( __FILE__ ) . '/i18n.php';
 require dirname( __FILE__ ) . '/register.php';
 require dirname( __FILE__ ) . '/demo.php';
