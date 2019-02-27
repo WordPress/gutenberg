fb03ec9fbeaeddff48dbaa030cace3bc20638a46
Andrés <andres.maneiro@automattic.com>
2019-02-27 14:16:20 +0100

    Make formatter-markdown test be deterministic

--- packages/docgen/src/test/formatter-markdown.js
+++ packages/docgen/src/test/formatter-markdown.js
@@ -5,8 +5,9 @@ const formatter = require( '../markdown/formatter' );
 
 describe( 'Formatter', () => {
 	it( 'returns markdown', () => {
-		const docPath = process.cwd();
-		const docs = formatter( docPath, docPath + '-api.md', [ {
+		const rootDir = '/home/my-path';
+		const docPath = 'docs';
+		const docs = formatter( rootDir, docPath + '-api.md', [ {
 			path: docPath + '-code.js',
 			description: 'My declaration example.',
 			tags: [
@@ -27,7 +28,7 @@ describe( 'Formatter', () => {
 			lineEnd: 2,
 		} ], 'API docs' );
 		expect( docs ).toBe(
-			'# API docs\n\n## myDeclaration\n\n[gutenberg/home/andres/src/gutenberg-code.js#L1-L2](gutenberg/home/andres/src/gutenberg-code.js#L1-L2)\n\nMy declaration example.\n\n**Parameters**\n\n- **firstParam** `number`: First declaration parameter.\n\n**Returns**\n\n`number` The result of the declaration.\n'
+			'# API docs\n\n## myDeclaration\n\n[../../../my-path/docs-code.js#L1-L2](../../../my-path/docs-code.js#L1-L2)\n\nMy declaration example.\n\n**Parameters**\n\n- **firstParam** `number`: First declaration parameter.\n\n**Returns**\n\n`number` The result of the declaration.\n'
 		);
 	} );
 } );
