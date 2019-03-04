70d49521c5ae38930f497e3a96ca851a1cab4b5a
Andrés <andres.maneiro@automattic.com>
2019-03-04 17:13:48 +0100

    Use bin script instead of npm script

---
+++ bin/generate-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -a packages=(
+	"e2e-test-utils"
+	"rich-text"
+)
+
+for package in "${packages[@]}"
+do
+	npx docgen packages/${package}/src/index.js --output packages/${package}/README.md --to-token;
+done
