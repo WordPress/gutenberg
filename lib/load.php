5bad3511fa132785389f6a66e3deb08180b661e5
Andrew Duthie <andrew@andrewduthie.com>
2019-03-05 17:14:26 -0500

    Blocks: Move blocks reregister function to standalone file

--- lib/load.php
+++ lib/load.php
@@ -24,45 +24,9 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require dirname( __FILE__ ) . '/rest-api.php';
 }
 
+require dirname( __FILE__ ) . '/blocks.php';
 require dirname( __FILE__ ) . '/client-assets.php';
 require dirname( __FILE__ ) . '/i18n.php';
 require dirname( __FILE__ ) . '/demo.php';
 require dirname( __FILE__ ) . '/widgets.php';
 require dirname( __FILE__ ) . '/widgets-page.php';
-
-/**
- * Substitutes the implementation of a core-registered block type, if exists,
- * with the built result from the plugin.
- */
-function gutenberg_reregister_core_block_types() {
-	// Blocks directory may not exist if working from a fresh clone.
-	$blocks_dir = dirname( __FILE__ ) . '/../build/block-library/blocks/';
-	if ( ! file_exists( $blocks_dir ) ) {
-		return;
-	}
-	$block_names = array(
-		'archives.php'        => 'core/archives',
-		'block.php'           => 'core/block',
-		'calendar.php'        => 'core/calendar',
-		'categories.php'      => 'core/categories',
-		'latest-comments.php' => 'core/latest-comments',
-		'latest-posts.php'    => 'core/latest-posts',
-		'legacy-widget.php'   => 'core/legacy-widget',
-		'rss.php'             => 'core/rss',
-		'shortcode.php'       => 'core/shortcode',
-		'search.php'          => 'core/search',
-		'tag-cloud.php'       => 'core/tag-cloud',
-	);
-	$registry = WP_Block_Type_Registry::get_instance();
-	foreach ( $block_names as $file => $block_name ) {
-		if ( ! file_exists( $blocks_dir . $file ) ) {
-			return;
-		}
-		if ( $registry->is_registered( $block_name ) ) {
-			$registry->unregister( $block_name );
-		}
-		require $blocks_dir . $file;
-	}
-}
-add_action( 'init', 'gutenberg_reregister_core_block_types' );
-
