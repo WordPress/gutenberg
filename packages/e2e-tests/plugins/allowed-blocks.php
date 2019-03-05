dc17306c4f36646a52fcadceb45df1f7b48bd46f
Jorge <jorge.costa@developer.pt>
2019-03-05 08:54:51 +0000

    Apply required fixes for phpcs errors

--- packages/e2e-tests/plugins/allowed-blocks.php
+++ packages/e2e-tests/plugins/allowed-blocks.php
@@ -9,12 +9,16 @@
 
 /**
  * Restrict the allowed blocks in the editor.
+ *
+ * @param  Array   $allowed_block_types An array of strings containing the previously allowed blocks.
+ * @param  WP_Post $post                The current post object.
+ * @return Array                        An array of strings containing the new allowed blocks after the filter is applied.
  */
 function my_plugin_allowed_block_types( $allowed_block_types, $post ) {
-    if ( $post->post_type !== 'post' ) {
-        return $allowed_block_types;
-    }
-    return array( 'core/paragraph', 'core/image' );
+	if ( 'post' !== $post->post_type ) {
+		return $allowed_block_types;
+	}
+	return array( 'core/paragraph', 'core/image' );
 }
 
 add_filter( 'allowed_block_types', 'my_plugin_allowed_block_types', 10, 2 );
