53e86ee99bb1816470291b9e942cd79224eba749
Ella van Durpe <iseulde@automattic.com>
2019-03-05 11:40:39 +0100

    RichText: don't use DOM to add line padding (#13850)
    * RichText: don't use DOM to add line padding
    
    * Clean up
    
    * Keep track of user inserted line breaks
    
    * Mark isEditableTree param unstable

--- packages/rich-text/src/insert-line-break.js
+++ packages/rich-text/src/insert-line-break.js
@@ -3,32 +3,14 @@
  */
 
 import { insert } from './insert';
-import { LINE_SEPARATOR } from './special-characters';
 
 /**
- * Inserts a line break at the given or selected position. Inserts two line
- * breaks if at the end of a line.
+ * Inserts a line break at the given or selected position.
  *
  * @param {Object} value Value to modify.
  *
- * @return {Object} The value with the line break(s) inserted.
+ * @return {Object} The value with the line break inserted.
  */
 export function insertLineBreak( value ) {
-	const { text, end } = value;
-	const length = text.length;
-
-	let toInsert = '\n';
-
-	// If the caret is at the end of the text, and there is no
-	// trailing line break or no text at all, we have to insert two
-	// line breaks in order to create a new line visually and place
-	// the caret there.
-	if (
-		( end === length || text[ end ] === LINE_SEPARATOR ) &&
-		( text[ end - 1 ] !== '\n' || length === 0 )
-	) {
-		toInsert = '\n\n';
-	}
-
-	return insert( value, toInsert );
+	return insert( value, '\n' );
 }
