f7ee93a630f11a924b38a61ab6d10bc572ef9115
etoledom <etoledom@icloud.com>
2019-03-06 08:34:17 +0100

    Small change to run CI

--- packages/components/src/dashicon/icon-class.native.js
+++ packages/components/src/dashicon/icon-class.native.js
@@ -1,3 +1,4 @@
 export const IconClass = ( icon, className, ariaPressed ) => {
 	return [ ariaPressed ? 'dashicon-active' : 'dashicon', 'dashicons-' + icon, className ].filter( Boolean ).join( ' ' );
 };
+ 
