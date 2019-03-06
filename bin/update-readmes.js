1bcee0b69bf39f12c28eeb1b7f3f09558a83d882
Andrés <nosolosw@users.noreply.github.com>
2019-03-06 18:46:02 +0100

    escape-html: set up autogenerated API docs (#14268)

--- bin/update-readmes.js
+++ bin/update-readmes.js
@@ -4,8 +4,34 @@ const path = require( 'path' );
 const childProcess = require( 'child_process' );
 
 const packages = [
+	//'a11y',
+	//'autop',
+	//'blob',
+	//'block-editor',
+	//'block-library',
+	//'block-serialization-default-parser',
+	//'blocks',
+	//'compose',
+	//'data',
+	//'date',
+	//'deprecated',
+	//'dom',
+	//'dom-ready',
 	'e2e-test-utils',
+	//'edit-post',
 	'element',
+	'escape-html',
+	//'html-entities',
+	//'i18n',
+	//'keycodes',
+	//'plugins',
+	//'priority-queue',
+	//'redux-routine',
+	//'rich-text',
+	//'shortcode',
+	//'url',
+	//'viewport',
+	//'wordcount',
 ];
 
 let aggregatedExitCode = 0;
@@ -14,6 +40,7 @@ packages.forEach( ( packageName ) => {
 		`packages/${ packageName }/src/index.js`,
 		`--output packages/${ packageName }/README.md`,
 		'--to-token',
+		'--ignore "unstable|experimental"',
 	];
 	const pathToDocGen = path.join( __dirname, '..', 'node_modules', '.bin', 'docgen' );
 	const { status, stderr } = childProcess.spawnSync(
