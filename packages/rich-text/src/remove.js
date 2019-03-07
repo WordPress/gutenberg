50d18e535337fc8ddf3eef94110c099955fc979a
Andrés <andres.maneiro@automattic.com>
2019-03-07 14:01:30 +0100

    Fix spaces

--- packages/rich-text/src/remove.js
+++ packages/rich-text/src/remove.js
@@ -9,7 +9,7 @@ import { create } from './create';
  * Remove content from a Rich Text value between the given `startIndex` and
  * `endIndex`. Indices are retrieved from the selection if none are provided.
  *
- * @param {Object}  value       Value to modify.
+ * @param {Object} value       Value to modify.
  * @param {number} [startIndex] Start index.
  * @param {number} [endIndex]   End index.
  *
