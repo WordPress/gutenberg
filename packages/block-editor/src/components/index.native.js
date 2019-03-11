c8e67cc10e9d151ec32e2b02c80ef7a9e361b904
Tugdual de Kerviler <dekervit@gmail.com>
2019-03-11 20:19:26 +0100

    Port editor refactoring for mobile

---
+++ packages/block-editor/src/components/index.native.js
@@ -0,0 +1,19 @@
+// Block Creation Components
+export { default as BlockControls } from './block-controls';
+export { default as BlockEdit } from './block-edit';
+export { default as BlockFormatControls } from './block-format-controls';
+export * from './colors';
+export * from './font-sizes';
+export { default as InspectorControls } from './inspector-controls';
+export { default as PlainText } from './plain-text';
+export {
+	default as RichText,
+	RichTextShortcut,
+	RichTextToolbarButton,
+	UnstableRichTextInputEvent,
+} from './rich-text';
+export { default as MediaPlaceholder } from './media-placeholder';
+export { default as URLInput } from './url-input';
+
+// Content Related Components
+export { default as DefaultBlockAppender } from './default-block-appender';
