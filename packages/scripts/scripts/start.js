9f3f392c0ed41b770311bbaf2bf67380e86e6907
Andrés <nosolosw@users.noreply.github.com>
2019-03-12 10:23:36 +0100

    Teach build and start commands to use Webpack default if none is provided (#13877)
    * Use a default webpack config if none is provided
    
    * Extract webpack utils from build command
    
    * Update start command
    
    * Add docs
    
    * Add Webpack documentation
    
    * Add example of how to overwrite the default plugins
    
    * Do not export hasWebpackConfig
    
    as it is not used anywhere else.
    
    * Always pass webpack CLI args to command
    
    * Update README
    
    * Remove section on extending the default webpack config file
    
    * Simplify array passing
    
    Co-Authored-By: nosolosw <nosolosw@users.noreply.github.com>
    
    * Simplify pushing to array
    
    Co-Authored-By: nosolosw <nosolosw@users.noreply.github.com>
    
    * Fix externals docs
    
    * Use webpack instead of Webpack
    
    * Add Changelog entry

--- packages/scripts/scripts/start.js
+++ packages/scripts/scripts/start.js
@@ -7,25 +7,11 @@ const { sync: resolveBin } = require( 'resolve-bin' );
 /**
  * Internal dependencies
  */
-const {
-	getCliArgs,
-	hasCliArg,
-	hasProjectFile,
-} = require( '../utils' );
+const { getWebpackArgs } = require( '../utils' );
 
-const hasWebpackConfig = hasCliArg( '--config' ) ||
-	hasProjectFile( 'webpack.config.js' ) ||
-	hasProjectFile( 'webpack.config.babel.js' );
-
-if ( hasWebpackConfig ) {
-	const { status } = spawn(
-		resolveBin( 'webpack' ),
-		[ '--watch', ...getCliArgs() ],
-		{ stdio: 'inherit' }
-	);
-	process.exit( status );
-} else {
-	// eslint-disable-next-line no-console
-	console.log( 'Webpack config file is missing.' );
-	process.exit( 1 );
-}
+const { status } = spawn(
+	resolveBin( 'webpack' ),
+	getWebpackArgs( [ '--watch' ] ),
+	{ stdio: 'inherit' }
+);
+process.exit( status );
