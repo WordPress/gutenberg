155f4da11981bacab1088b8fd23d916e86a5d5cb
etoledom <etoledom@icloud.com>
2019-03-15 12:16:04 +0100

    Added ModalLinkUI test: Links are saved when URL field has content

--- src/__tests__/links-ui.test.js
+++ src/__tests__/links-ui.test.js
@@ -11,8 +11,8 @@ describe( 'LinksUI', () => {
     } );
     
     it( 'Links are removed when no text is in the URL field', () => {
+        // Given
         const onRemove = jest.fn();
-
         const wrapper = shallow(
             <ModalLinkUI
                 onRemove={ onRemove }
@@ -20,10 +20,42 @@ describe( 'LinksUI', () => {
             />
         ).dive(); // -> dive() removes the HOC layer that was blocking access to ModalLinkUI
 
+        // When
+
         // Close the BottomSheet
         const bottomSheet = wrapper.find('BottomSheet').first();
         bottomSheet.simulate('close');
 
+        // Then
+
         expect( onRemove ).toHaveBeenCalledTimes( 1 );
+    })
+
+    it( 'Links are saved when URL field has content', () => {
+        // Given
+        const onRemove = jest.fn();
+        const wrapper = shallow(
+            <ModalLinkUI
+                onRemove={ onRemove }
+                onClose={ jest.fn() }
+            />
+        ).dive(); // -> dive() removes the HOC layer that was blocking access to ModalLinkUI
+        
+        // Mock `submitLink` for simplicity (we don't want to test submitLink itself here)
+        wrapper.instance().submitLink = jest.fn();
+
+        // When
+
+        // Simulate user typing on the URL Cell.
+        const bottomSheet = wrapper.find('BottomSheet').first();
+        const cell = bottomSheet.find('Cell').first();
+        cell.simulate('changeValue', "wordpress.com");
+
+        // Close the BottomSheet
+        bottomSheet.simulate('close');
+
+        // Then
+        expect( onRemove ).toHaveBeenCalledTimes( 0 );
+        expect( wrapper.instance().submitLink ).toHaveBeenCalledTimes( 1 );
     })
 } );
