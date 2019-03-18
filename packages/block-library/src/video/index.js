fff3458d7b4e02b291a026dd9f09a89e68850d27
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-03-18 23:42:25 +0600

    Added the 'playsinline' attribute to the block

--- packages/block-library/src/video/index.js
+++ packages/block-library/src/video/index.js
@@ -78,6 +78,12 @@ export const settings = {
 			selector: 'video',
 			attribute: 'src',
 		},
+		playsinline: {
+			type: 'boolean',
+			source: 'attribute',
+			selector: 'video',
+			attribute: 'playsinline',
+		},
 	},
 
 	transforms: {
