5b46ae01de5f4b368624c224a7022a871feb0897
Jorge Costa <jorge.costa@developer.pt>
2019-03-18 08:37:00 +0000

    Improve openAllBlockInserterCategories function; Fix intermittent failures in inner blocks allowed blocks test. (#14460)

--- packages/e2e-tests/specs/plugins/inner-blocks-allowed-blocks.test.js
+++ packages/e2e-tests/specs/plugins/inner-blocks-allowed-blocks.test.js
@@ -11,12 +11,7 @@ import {
 	openGlobalBlockInserter,
 } from '@wordpress/e2e-test-utils';
 
-// Todo: Understand why this test causing intermitent fails on travis.
-// Error:   ● Allowed Blocks Setting on InnerBlocks  › allows all blocks if the allowed blocks setting was not set
-// Node is detached from document
-// at ElementHandle._scrollIntoViewIfNeeded (../../node_modules/puppeteer/lib/ElementHandle.js:75:13)
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip( 'Allowed Blocks Setting on InnerBlocks ', () => {
+describe( 'Allowed Blocks Setting on InnerBlocks ', () => {
 	const paragraphSelector = '.block-editor-rich-text__editable.wp-block-paragraph';
 	beforeAll( async () => {
 		await activatePlugin( 'gutenberg-test-innerblocks-allowed-blocks' );
