548a77295d4418c7528c14a11db67f7c644734ad
Amartya <amartyautkarsh@dhcp-rhodes-3361.redrover.cornell.edu>
2019-03-27 11:33:14 -0400

    Wrap speak in UseEffect

--- packages/block-editor/src/components/contrast-checker/index.js
+++ packages/block-editor/src/components/contrast-checker/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import tinycolor from 'tinycolor2';
+import React, { useEffect } from 'react';
 
 /**
  * WordPress dependencies
@@ -35,11 +36,13 @@ function ContrastChecker( {
 	const msg = tinyBackgroundColor.getBrightness() < tinyTextColor.getBrightness() ?
 		__( 'This color combination may be hard for people to read. Try using a darker background color and/or a brighter text color.' ) :
 		__( 'This color combination may be hard for people to read. Try using a brighter background color and/or a darker text color.' );
-	if ( tinyBackgroundColor.getBrightness() < tinyTextColor.getBrightness() ) {
-		speak( __( 'This color combination may be hard for people to read. Try using a darker background color and/or a brighter text color.' ) );
-	} else {
-		speak( __( 'This color combination may be hard for people to read. Try using a brighter background color and/or a darker text color.' ) );
-	}
+	useEffect( () => {
+		if ( tinyBackgroundColor.getBrightness() < tinyTextColor.getBrightness() ) {
+			speak( __( 'This color combination may be hard for people to read. Try using a darker background color and/or a brighter text color.' ) );
+		} else {
+			speak( __( 'This color combination may be hard for people to read. Try using a brighter background color and/or a darker text color.' ) );
+		}
+	}, [ tinyBackgroundColor, tinyTextColor ] );
 	return (
 		<div className="editor-contrast-checker block-editor-contrast-checker">
 			<Notice status="warning" isDismissible={ false }>
