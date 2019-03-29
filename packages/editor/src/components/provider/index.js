523a2e75a5527966563393d63598d87824b8bf01
Andrew Duthie <andrew@andrewduthie.com>
2019-03-29 16:13:19 -0400

    Editor: Create own sub-registry in default EditorProvider use

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -19,6 +19,7 @@ import { decodeEntities } from '@wordpress/html-entities';
 /**
  * Internal dependencies
  */
+import withRegistryProvider from './with-registry-provider';
 import { mediaUpload } from '../../utils';
 import ReusableBlocksButtons from '../reusable-blocks-buttons';
 
@@ -165,6 +166,7 @@ class EditorProvider extends Component {
 }
 
 export default compose( [
+	withRegistryProvider,
 	withSelect( ( select ) => {
 		const {
 			__unstableIsEditorReady: isEditorReady,
