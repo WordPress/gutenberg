c2a352cdaabf7b7d5ffe4dd71fdd72220e00feaf
David Aguilera <david.aguilera@neliosoftware.com>
2019-03-29 14:25:48 +0100

    Escaping (and unescaping) shortcode and link characters 'dynamically' in CodeEdit

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -7,13 +7,14 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { PlainText } from '@wordpress/block-editor';
+import { escape, unescape } from './utils';
 
 export default function CodeEdit( { attributes, setAttributes, className } ) {
 	return (
 		<div className={ className }>
 			<PlainText
-				value={ attributes.content }
-				onChange={ ( content ) => setAttributes( { content } ) }
+				value={ unescape( attributes.content ) }
+				onChange={ ( content ) => setAttributes( { content: escape( content ) } ) }
 				placeholder={ __( 'Write code…' ) }
 				aria-label={ __( 'Code' ) }
 			/>
