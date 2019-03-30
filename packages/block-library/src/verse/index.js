bd5b20ba8f454e339bd9f04874f32fb8348e0fa2
Sören Wrede <soerenwrede@gmail.com>
2019-03-30 18:55:41 +0100

    Fix verse icon

--- packages/block-library/src/verse/index.js
+++ packages/block-library/src/verse/index.js
@@ -18,7 +18,7 @@ export const settings = {
 
 	description: __( 'Insert poetry. Use special spacing formats. Or quote song lyrics.' ),
 
-	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M21 11.01L3 11V13H21V11.01ZM3 16H17V18H3V16ZM15 6H3V8.01L15 8V6Z" /></SVG>,
+	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path d="M21 11.01L3 11V13H21V11.01ZM3 16H17V18H3V16ZM15 6H3V8.01L15 8V6Z" /></SVG>,
 
 	category: 'formatting',
 
