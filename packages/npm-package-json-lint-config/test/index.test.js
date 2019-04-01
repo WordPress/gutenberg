113819bd0df0daa2dfaa822f27977b27e3b52d9a
Andrew Duthie <andrew@andrewduthie.com>
2019-04-01 15:16:45 -0400

    NPM Package JSON Lint Config: Resolve syntax error in isPlainObject function call

--- packages/npm-package-json-lint-config/test/index.test.js
+++ packages/npm-package-json-lint-config/test/index.test.js
@@ -10,10 +10,10 @@ import config from '../';
 
 describe( 'npm-package-json-lint config tests', () => {
 	it( 'should be an object', () => {
-		expect( { isPlainObject }( config ) ).toBeTruthy();
+		expect( isPlainObject( config ) ).toBeTruthy();
 	} );
 
 	it( 'should have rules property as an object', () => {
-		expect( { isPlainObject }( config.rules ) ).toBeTruthy();
+		expect( isPlainObject( config.rules ) ).toBeTruthy();
 	} );
 } );
