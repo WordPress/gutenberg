c02cd9cffbc501ff3cc4e1651c42e74ea3a799cc
Javon Davis <JavonDavis@users.noreply.github.com>
2019-04-02 10:15:02 -0500

    Revert "Revert "Include UI tests for blocks with Appium""

---
+++ __device-tests__/pages/editor-page.js
@@ -0,0 +1,44 @@
+/**
+ * @flow
+ * @format
+ * */
+/**
+ * External dependencies
+ */
+import wd from 'wd';
+/**
+ * Internal dependencies
+ */
+import BlockInteraction from '../blocks/block-interaction';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export default class EditorPage {
+	driver: wd.PromiseChainWebdriver;
+	constructor( driver: wd.PromiseChainWebdriver ) {
+		this.driver = driver;
+	}
+
+	async expect( ) {
+		expect( await this.driver.hasElementByAccessibilityId( 'block-list' ) ).toBe( true );
+		return this;
+	}
+
+	async addNewBlock( block: BlockInteraction ) {
+		const blockName = block.name;
+
+		// Click add button
+		const addButton = await this.driver.elementByAccessibilityId( __( 'Add block' ) );
+		await addButton.click();
+
+		// Click on block of choice
+		const blockButton = await this.driver.elementByAccessibilityId( blockName );
+		await blockButton.click();
+		await block.setup();
+
+		return block;
+	}
+}
