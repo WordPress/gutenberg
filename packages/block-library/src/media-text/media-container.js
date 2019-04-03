b382d741978e437c6bb58265c83fa290293bdfbd
Michael P. Pfeiffer <michael@cssconf.eu>
2019-04-03 11:01:53 -0500

    Change default background-position to 50%/50% and update fixtures

--- packages/block-library/src/media-text/media-container.js
+++ packages/block-library/src/media-text/media-container.js
@@ -25,7 +25,7 @@ export function imageFillStyles( url, focalPoint ) {
 	return url ?
 		{
 			backgroundImage: `url(${ url })`,
-			backgroundPosition: focalPoint ? `${ focalPoint.x * 100 }% ${ focalPoint.y * 100 }%` : `initial`,
+			backgroundPosition: focalPoint ? `${ focalPoint.x * 100 }% ${ focalPoint.y * 100 }%` : `50% 50%`,
 		} :
 		{};
 }
