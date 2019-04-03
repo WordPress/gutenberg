0f522568efc9ed85bec1bbcbac0fdb8aa2c4c321
Javon <javondavis@users.noreply.github.com>
2019-04-03 18:45:41 -0500

    start restructuring functions to editor page

--- __device-tests__/pages/editor-page.js
+++ __device-tests__/pages/editor-page.js
@@ -15,11 +15,20 @@ import BlockInteraction from '../blocks/block-interaction';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { isAndroid } from '../helpers/utils';
 
 export default class EditorPage {
 	driver: wd.PromiseChainWebdriver;
+
 	constructor( driver: wd.PromiseChainWebdriver ) {
 		this.driver = driver;
+		this.accessibilityIdKey = 'name';
+		this.accessibilityIdXPathAttrib = 'name';
+
+		if ( isAndroid() ) {
+			this.accessibilityIdXPathAttrib = 'content-desc';
+			this.accessibilityIdKey = 'contentDescription';
+		}
 	}
 
 	async expect( ) {
@@ -27,9 +36,7 @@ export default class EditorPage {
 		return this;
 	}
 
-	async addNewBlock( block: BlockInteraction ) {
-		const blockName = block.name;
-
+	async addNewBlock( blockName: string ) {
 		// Click add button
 		const addButton = await this.driver.elementByAccessibilityId( __( 'Add block' ) );
 		await addButton.click();
@@ -37,8 +44,35 @@ export default class EditorPage {
 		// Click on block of choice
 		const blockButton = await this.driver.elementByAccessibilityId( blockName );
 		await blockButton.click();
-		await block.setup();
+	}
 
-		return block;
+	// Finds the wd element for new block that was added and sets the element attribute
+	// and accessibilityId attributes on this object
+	async getBlockAtPosition( position: number, blockName: string ) {
+		await this.driver.sleep( 2000 );
+		const blockLocator = `block-${ position }-${ blockName }`;
+		return await this.driver.elementByAccessibilityId( blockLocator );
+	}
+
+	// Paragraph Block functions
+
+	async addNewParagraphBlock() {
+		await this.addNewBlock( 'Paragraph' );
+	}
+
+	async getParagraphBlockAtPosition( position: number ) {
+		await this.driver.sleep( 2000 );
+		const blockName = 'core/paragraph';
+		return this.getBlockAtPosition( position, blockName );
+	}
+
+	async getTextViewForParagraphBlock( block: wd.Element ) {
+		await this.driver.sleep( 2000 );
+		let textViewElement = 'XCUIElementTypeTextView';
+		if ( isAndroid() ) {
+			textViewElement = 'android.widget.EditText';
+		}
+		const blockLocator = `//*[@${ this.accessibilityIdXPathAttrib }="${ this.accessibilityId }"]//${ textViewElement }`;
+		this.textViewElement = await this.driver.elementByXPath( blockLocator );
 	}
 }
