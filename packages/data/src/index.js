53e61f6db83f470bc0fd167e8951e993a3e456d3
Darren Ethier <darren@roughsmootheng.in>
2019-04-04 19:51:53 -0400

    expand jsdoc for wp.data.dispatch calls

--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -79,6 +79,9 @@ export const select = defaultRegistry.select;
  * Given the name of a registered store, returns an object of the store's action creators.
  * Calling an action creator will cause it to be dispatched, updating the state value accordingly.
  *
+ * Note: If the action creator is a generator then a promise is returned.
+ * Otherwise, the dispatch will return `undefined`.
+ *
  * @param {string} name Store name
  *
  * @example
