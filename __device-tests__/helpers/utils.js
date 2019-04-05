4272d85552d9e939bedf6e689b43e29aaae27c35
Javon <javondavis@users.noreply.github.com>
2019-04-05 17:22:27 -0500

    remove unused branch variable

--- __device-tests__/helpers/utils.js
+++ __device-tests__/helpers/utils.js
@@ -64,7 +64,6 @@ const setupDriver = async () => {
 		if ( isLocalEnvironment() ) {
 			desiredCaps.app = localIOSAppPath;
 		} else {
-			const branch = process.env.CIRCLE_BRANCH;
 			desiredCaps.app = `sauce-storage:Gutenberg.app.zip`; // App should be preloaded to sauce storage, this can also be a URL
 		}
 	}
