ca3e5215c9853b55f0c4d545860c96a99c00bdec
tfrommen <info@tfrommen.de>
2019-04-10 10:54:53 +0200

    Add unit test

--- packages/components/src/form-token-field/test/lib/token-field-wrapper.js
+++ packages/components/src/form-token-field/test/lib/token-field-wrapper.js
@@ -11,13 +11,10 @@ import { Component } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import fixtures from './fixtures';
 import TokenField from '../../';
 
-const suggestions = [
-	'the', 'of', 'and', 'to', 'a', 'in', 'for', 'is', 'on', 'that', 'by', 'this', 'with', 'i', 'you', 'it',
-	'not', 'or', 'be', 'are', 'from', 'at', 'as', 'your', 'all', 'have', 'new', 'more', 'an', 'was', 'we',
-	'associate', 'snake', 'pipes', 'sound',
-];
+const { specialSuggestions: { default: suggestions } } = fixtures;
 
 function unescapeAndFormatSpaces( str ) {
 	const nbsp = String.fromCharCode( 160 );
