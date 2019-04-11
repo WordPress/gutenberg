383a35927c2c9cf637f05191b3f3a9cb85bfe4c6
Daniel Richards <daniel.richards@automattic.com>
2019-04-11 16:22:10 +0800

    Rename section block to group

--- packages/e2e-tests/specs/blocks/group.test.js
+++ packages/e2e-tests/specs/blocks/group.test.js
@@ -8,24 +8,24 @@ import {
 	createNewPost,
 } from '@wordpress/e2e-test-utils';
 
-describe( 'Section', () => {
+describe( 'Group', () => {
 	beforeEach( async () => {
 		await createNewPost();
 	} );
 
 	it( 'can be created using the block inserter', async () => {
-		await searchForBlock( 'Section' );
-		await page.click( '.editor-block-list-item-section' );
-		await page.keyboard.type( 'Section block' );
+		await searchForBlock( 'Group' );
+		await page.click( '.editor-block-list-item-group' );
+		await page.keyboard.type( 'Group block' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 
 	it( 'can be created using the slash inserter', async () => {
 		await clickBlockAppender();
-		await page.keyboard.type( '/section' );
+		await page.keyboard.type( '/group' );
 		await page.keyboard.press( 'Enter' );
-		await page.keyboard.type( 'Section block' );
+		await page.keyboard.type( 'Group block' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
