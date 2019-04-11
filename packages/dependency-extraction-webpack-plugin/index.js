f53939109ae9960b697e674a4b75ee467a7b1fb0
Jon Surrell <jon.surrell@automattic.com>
2019-04-11 22:08:15 +0200

    Copy camelCaseDash from scripts
    See https://github.com/WordPress/gutenberg/pull/14869#discussion_r274363420

--- packages/dependency-extraction-webpack-plugin/index.js
+++ packages/dependency-extraction-webpack-plugin/index.js
@@ -1,9 +1,4 @@
 /**
- * WordPress dependencies
- */
-const { camelCaseDash } = require( '@wordpress/scripts/utils' );
-
-/**
  * External dependencies
  */
 const { createHash } = require( 'crypto' );
@@ -188,6 +183,22 @@ function basename( name ) {
 		return name;
 	}
 	return name.substr( name.lastIndexOf( '/' ) + 1 );
+}
+
+/**
+ * Given a string, returns a new string with dash separators converted to
+ * camelCase equivalent. This is not as aggressive as `_.camelCase` in
+ * converting to uppercase, where Lodash will also capitalize letters
+ * following numbers.
+ *
+ * Temporarily duplicated from @wordpress/scripts/utils.
+ *
+ * @param {string} string Input dash-delimited string.
+ *
+ * @return {string} Camel-cased string.
+ */
+function camelCaseDash( string ) {
+	return string.replace( /-([a-z])/g, ( match, letter ) => letter.toUpperCase() );
 }
 
 module.exports = DependencyExtractionWebpackPlugin;
