d1efe5c8edeef30929712062e1e4d05d2793c2a9
Dave Smith <getdavemail@gmail.com>
2019-04-11 17:50:57 +0100

    Removes dependency on compose

--- packages/block-editor/src/components/inner-blocks/button-block-appender.js
+++ packages/block-editor/src/components/inner-blocks/button-block-appender.js
@@ -1,20 +1,13 @@
 /**
- * WordPress dependencies
- */
-import { compose } from '@wordpress/compose';
-
-/**
  * Internal dependencies
  */
 import BBlockAppender from '../button-block-appender';
 import withClientId from './utils/with-client-id';
 
-const ButtonBlockAppender = compose( [
-	withClientId,
-] )( function( { clientId } ) {
+export const ButtonBlockAppender = function( { clientId } ) {
 	return (
 		<BBlockAppender rootClientId={ clientId } />
 	);
-} );
+};
 
-export default ButtonBlockAppender;
+export default withClientId( ButtonBlockAppender );
