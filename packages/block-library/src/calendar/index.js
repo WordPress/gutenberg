b8c38721410a90825318a3acf6c26a0e137000f7
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-04-14 00:42:50 +0300

    i18n: merge similar translation strings

--- packages/block-library/src/calendar/index.js
+++ packages/block-library/src/calendar/index.js
@@ -12,7 +12,7 @@ export const name = 'core/calendar';
 
 export const settings = {
 	title: __( 'Calendar' ),
-	description: __( 'A calendar of your site’s posts.' ),
+	description: __( 'A calendar of your site&#8217;s Posts.' ),
 	icon: 'calendar',
 	category: 'widgets',
 	keywords: [ __( 'posts' ), __( 'archive' ) ],
