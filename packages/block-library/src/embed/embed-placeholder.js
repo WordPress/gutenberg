18fdced8f9aa464bb5fea766a144b6e52753acd2
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-04-13 12:57:31 +0300

    adds cancel link to embed edit mode

--- packages/block-library/src/embed/embed-placeholder.js
+++ packages/block-library/src/embed/embed-placeholder.js
@@ -4,31 +4,44 @@
 import { __, _x } from '@wordpress/i18n';
 import { Button, Placeholder } from '@wordpress/components';
 import { BlockIcon } from '@wordpress/block-editor';
+import { Fragment } from '@wordpress/element';
 
 const EmbedPlaceholder = ( props ) => {
-	const { icon, label, value, onSubmit, onChange, cannotEmbed, fallback, tryAgain } = props;
+	const { icon, label, value, onSubmit, onChange, switchBackToURLInput, cannotEmbed, fallback, tryAgain } = props;
 	return (
 		<Placeholder icon={ <BlockIcon icon={ icon } showColors /> } label={ label } className="wp-block-embed">
-			<form onSubmit={ onSubmit }>
-				<input
-					type="url"
-					value={ value || '' }
-					className="components-placeholder__input"
-					aria-label={ label }
-					placeholder={ __( 'Enter URL to embed here…' ) }
-					onChange={ onChange } />
-				<Button
-					isLarge
-					type="submit">
-					{ _x( 'Embed', 'button label' ) }
-				</Button>
-				{ cannotEmbed &&
-					<p className="components-placeholder__error">
-						{ __( 'Sorry, this content could not be embedded.' ) }<br />
-						<Button isLarge onClick={ tryAgain }>{ _x( 'Try again', 'button label' ) }</Button> <Button isLarge onClick={ fallback }>{ _x( 'Convert to link', 'button label' ) }</Button>
-					</p>
-				}
-			</form>
+			<Fragment>
+				<form onSubmit={ onSubmit }>
+					<input
+						type="url"
+						value={ value || '' }
+						className="components-placeholder__input"
+						aria-label={ label }
+						placeholder={ __( 'Enter URL to embed here…' ) }
+						onChange={ onChange } />
+					<Button
+						isLarge
+						type="submit">
+						{ _x( 'Embed', 'button label' ) }
+					</Button>
+					{ cannotEmbed &&
+						<p className="components-placeholder__error">
+							{ __( 'Sorry, this content could not be embedded.' ) }<br />
+							<Button isLarge onClick={ tryAgain }>{ _x( 'Try again', 'button label' ) }</Button> <Button isLarge onClick={ fallback }>{ _x( 'Convert to link', 'button label' ) }</Button>
+						</p>
+					}
+				</form>
+				<div className="components-placeholder__cancel">
+					<Button
+						className="block-editor-embed-placeholder__cancel-button"
+						title={ __( 'Cancel' ) }
+						isLink
+						onClick={ switchBackToURLInput }
+					>
+						{ __( 'Cancel' ) }
+					</Button>
+				</div>
+			</Fragment>
 		</Placeholder>
 	);
 };
