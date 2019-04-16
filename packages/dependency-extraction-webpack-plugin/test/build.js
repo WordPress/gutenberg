30dc232c29c90e684ad22d81293b8990364c8d79
Jon Surrell <jon.surrell@automattic.com>
2019-04-16 13:00:37 +0200

    Use describe.each over explicit loop

--- packages/dependency-extraction-webpack-plugin/test/build.js
+++ packages/dependency-extraction-webpack-plugin/test/build.js
@@ -13,63 +13,60 @@ const configFixtures = fs.readdirSync( fixturesPath ).sort();
 
 afterAll( () => rimraf( path.join( __dirname, 'build' ) ) );
 
-configFixtures.forEach( ( configCase ) => {
-	describe( `Webpack ${ configCase }`, () => {
-		const testDirectory = path.join( fixturesPath, configCase );
-		const outputDirectory = path.join( __dirname, 'build', configCase );
+describe.each( configFixtures )( 'Webpack `%s`', ( configCase ) => {
+	const testDirectory = path.join( fixturesPath, configCase );
+	const outputDirectory = path.join( __dirname, 'build', configCase );
 
-		beforeAll( () => {
-			rimraf( outputDirectory );
-			mkdirp( outputDirectory );
-		} );
-		afterAll( () => rimraf( outputDirectory ) );
-
-		test( 'should produce expected output', () =>
-			new Promise( ( resolve ) => {
-				const options = Object.assign(
-					{
-						context: testDirectory,
-						entry: './index.js',
-						mode: 'production',
-						optimization: {
-							minimize: false,
-							namedChunks: true,
-							namedModules: true,
-						},
-						output: {},
-					},
-					require( path.join( testDirectory, 'webpack.config.js' ) )
-				);
-				options.output.path = outputDirectory;
-
-				webpack( options, ( err, stats ) => {
-					expect( err ).toBeNull();
-
-					const depsFiles = glob( `${ outputDirectory }/*.deps.json` );
-					const expectedLength =
-						typeof options.entry === 'object' ? Object.keys( options.entry ).length : 1;
-					expect( depsFiles ).toHaveLength( expectedLength );
-
-					// Deps files should match
-					depsFiles.forEach( ( depsFile ) => {
-						expect( require( depsFile ) ).toMatchSnapshot(
-							'Dependencies JSON should match snapshot'
-						);
-					} );
-
-					// Webpack stats external modules should match
-					const externalModules = stats.compilation.modules
-						.filter( ( { external } ) => external )
-						.sort()
-						.map( ( module ) => ( {
-							externalType: module.externalType,
-							request: module.request,
-							userRequest: module.userRequest,
-						} ) );
-					expect( externalModules ).toMatchSnapshot( 'External modules should match snapshot' );
-
-					resolve();
-				} );
-			} ) );
+	beforeAll( () => {
+		rimraf( outputDirectory );
+		mkdirp( outputDirectory );
 	} );
+
+	test( 'should produce expected output', () =>
+		new Promise( ( resolve ) => {
+			const options = Object.assign(
+				{
+					context: testDirectory,
+					entry: './index.js',
+					mode: 'production',
+					optimization: {
+						minimize: false,
+						namedChunks: true,
+						namedModules: true,
+					},
+					output: {},
+				},
+				require( path.join( testDirectory, 'webpack.config.js' ) )
+			);
+			options.output.path = outputDirectory;
+
+			webpack( options, ( err, stats ) => {
+				expect( err ).toBeNull();
+
+				const depsFiles = glob( `${ outputDirectory }/*.deps.json` );
+				const expectedLength =
+					typeof options.entry === 'object' ? Object.keys( options.entry ).length : 1;
+				expect( depsFiles ).toHaveLength( expectedLength );
+
+				// Deps files should match
+				depsFiles.forEach( ( depsFile ) => {
+					expect( require( depsFile ) ).toMatchSnapshot(
+						'Dependencies JSON should match snapshot'
+					);
+				} );
+
+				// Webpack stats external modules should match
+				const externalModules = stats.compilation.modules
+					.filter( ( { external } ) => external )
+					.sort()
+					.map( ( module ) => ( {
+						externalType: module.externalType,
+						request: module.request,
+						userRequest: module.userRequest,
+					} ) );
+				expect( externalModules ).toMatchSnapshot( 'External modules should match snapshot' );
+
+				resolve();
+			} );
+		} ) );
 } );
