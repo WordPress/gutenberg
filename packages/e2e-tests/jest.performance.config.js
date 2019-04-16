4b682f56142ecd6bf81f934a81d5d45a920dd499
Riad Benguella <benguella@gmail.com>
2019-04-16 09:07:06 +0100

    Add a CLI command to run the performance tests

--- packages/e2e-tests/jest.performance.config.js
+++ packages/e2e-tests/jest.performance.config.js
@@ -1,15 +1,21 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
+	testMatch: [
+		'**/performance.test.js',
+	],
 	setupFiles: [
 		'<rootDir>/config/gutenberg-phase.js',
 	],
 	setupFilesAfterEnv: [
 		'<rootDir>/config/setup-test-framework.js',
-		'@wordpress/jest-console',
-		'@wordpress/jest-puppeteer-axe',
 		'expect-puppeteer',
 	],
-	testPathIgnorePatterns: [
-		'e2e-tests/specs/performance.test.js',
+	transformIgnorePatterns: [
+		'node_modules',
+		'scripts/config/puppeteer.config.js',
+	],
+	reporters: [
+		'default',
+		'<rootDir>/config/performance-reporter.js',
 	],
 };
