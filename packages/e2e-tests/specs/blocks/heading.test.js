674532596597799165eb3a798667f1f07c6beed5
iseulde <wp@iseulde.com>
2019-04-19 18:06:34 +0200

    Add e2e tests

--- packages/e2e-tests/specs/blocks/heading.test.js
+++ packages/e2e-tests/specs/blocks/heading.test.js
@@ -7,7 +7,7 @@ import {
 	createNewPost,
 } from '@wordpress/e2e-test-utils';
 
-describe( 'Separator', () => {
+describe( 'Heading', () => {
 	beforeEach( async () => {
 		await createNewPost();
 	} );
@@ -24,6 +24,23 @@ describe( 'Separator', () => {
 		await page.keyboard.type( '4' );
 		await page.keyboard.press( 'ArrowLeft' );
 		await page.keyboard.type( '#### ' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should create a paragraph block above when pressing enter at the start', async () => {
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '## a' );
+		await page.keyboard.press( 'ArrowLeft' );
+		await page.keyboard.press( 'Enter' );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should create a paragraph block below when pressing enter at the end', async () => {
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '## a' );
+		await page.keyboard.press( 'Enter' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
