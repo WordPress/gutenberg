4e4c259a919c539e3cbdc020d7083574474b5505
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-04-19 15:45:57 +0200

    Fix 13776: Format is already registered to handle class name

--- packages/rich-text/src/store/test/selectors.js
+++ packages/rich-text/src/store/test/selectors.js
@@ -6,33 +6,69 @@ import deepFreeze from 'deep-freeze';
 /**
  * Internal dependencies
  */
-import { getFormatTypes, getFormatType } from '../selectors';
+import {
+	getFormatTypes,
+	getFormatType,
+	getFormatTypeForBareElement,
+	getFormatTypeForClassName,
+} from '../selectors';
 
 describe( 'selectors', () => {
+	const formatType = {
+		name: 'core/test-format',
+		className: null,
+		tagName: 'format',
+	};
+	const formatTypeBareTag = {
+		name: 'core/test-format-bare-tag',
+		className: null,
+		tagName: 'strong',
+	};
+	const formatTypeClassName = {
+		name: 'core/test-format-class-name',
+		className: 'class-name',
+		tagName: 'strong',
+	};
 	const defaultState = deepFreeze( {
 		formatTypes: {
-			'core/test-format': { name: 'core/test-format' },
-			'core/test-format-2': { name: 'core/test-format-2' },
+			'core/test-format': formatType,
+			'core/test-format-bare-tag': formatTypeBareTag,
+			'core/test-format-class-name': formatTypeClassName,
 		},
 	} );
 
 	describe( 'getFormatTypes', () => {
 		it( 'should get format types', () => {
 			const expected = [
-				{ name: 'core/test-format' },
-				{ name: 'core/test-format-2' },
+				formatType,
+				formatTypeBareTag,
+				formatTypeClassName,
 			];
-
 			expect( getFormatTypes( defaultState ) ).toEqual( expected );
 		} );
 	} );
 
 	describe( 'getFormatType', () => {
 		it( 'should get a format type', () => {
-			const expected = { name: 'core/test-format' };
 			const result = getFormatType( defaultState, 'core/test-format' );
 
-			expect( result ).toEqual( expected );
+			expect( result ).toEqual( formatType );
+		} );
+	} );
+
+	describe( 'getFormatTypeForBareElement', () => {
+		it( 'should get a format type', () => {
+			const result = getFormatTypeForBareElement( defaultState, 'strong' );
+
+			expect( result ).toEqual( formatTypeBareTag );
+		} );
+	} );
+
+	describe( 'getFormatTypeForClassName', () => {
+		it( 'should get a format type', () => {
+			const result = getFormatTypeForClassName( defaultState, 'class-name' );
+
+			expect( result ).toEqual( formatTypeClassName );
 		} );
 	} );
 } );
