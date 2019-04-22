c66ce87021d580c089e4acccf1fee1f1ddd497dd
fabiankaegy <fabian@arvernus.info>
2019-04-22 18:47:42 +0200

    Move to using MiniExtractCssPlugin

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -5,6 +5,7 @@ const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
 const LiveReloadPlugin = require( 'webpack-livereload-plugin' );
 const path = require( 'path' );
 const postcssPresetEnv = require( 'postcss-preset-env' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 /**
  * WordPress dependencies
@@ -36,6 +37,29 @@ const config = {
 	resolve: {
 		alias: {
 			'lodash-es': 'lodash',
+		},
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			cacheGroups: {
+				editor: {
+					name: 'editor',
+					test: /editor\.(sc|sa|c)ss$/,
+					enforce: true,
+				},
+				style: {
+					name: 'styles',
+					test: /style\.(sc|sa|c)ss$/,
+					enforce: true,
+				},
+				theme: {
+					name: 'theme',
+					test: /theme\.(sc|sa|c)ss$/,
+					enforce: true,
+				},
+				default: false,
+			},
 		},
 	},
 	module: {
@@ -77,13 +101,7 @@ const config = {
 				test: /\.(sc|sa|c)ss$/,
 				use: [
 					{
-						loader: require.resolve( 'file-loader' ),
-						options: {
-							name: '[name].css',
-						},
-					},
-					{
-						loader: require.resolve( 'extract-loader' ),
+						loader: MiniCssExtractPlugin.loader,
 					},
 					{
 						loader: require.resolve( 'css-loader' ),
@@ -96,7 +114,21 @@ const config = {
 						options: {
 							ident: 'postcss',
 							plugins: () => [
-								postcssPresetEnv(/* pluginOptions */),
+								postcssPresetEnv( {
+									// Start with a stage.
+									stage: 3,
+									// Override specific features you do (or don't) want.
+									features: {
+										// And, optionally, configure rules/features as needed.
+										'custom-media-queries': {
+											preserve: false,
+										},
+										'custom-properties': {
+											preserve: true,
+										},
+										'nesting-rules': true,
+									},
+								} ),
 							],
 						},
 					},
@@ -108,6 +140,8 @@ const config = {
 		// The WP_BUNDLE_ANALYZER global variable enables a utility that represents bundle
 		// content as a convenient interactive zoomable treemap.
 		process.env.WP_BUNDLE_ANALYZER && new BundleAnalyzerPlugin(),
+		// MiniCssExtractPlugin to extract the css thats gets imported into javascript
+		new MiniCssExtractPlugin(),
 		// WP_LIVE_RELOAD_PORT global variable changes port on which live reload works
 		// when running watch mode.
 		! isProduction &&
