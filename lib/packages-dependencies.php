df21c64cb69f0f9050d92bf7ff1077d8d9a576cb
Jon Surrell <jon.surrell@automattic.com>
2019-04-24 11:51:33 +0200

    Improve deprecated message

--- lib/packages-dependencies.php
+++ lib/packages-dependencies.php
@@ -1,5 +1,5 @@
 <?php
 /**
- * @deprecated since 5.6. Script dependencies should be read from generated `[script].deps.json`.
+ * @deprecated since 5.6. Script dependencies should be read from generated `build/[package]/index.deps.json`.
  * @package gutenberg
  */
