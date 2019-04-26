29c0c2a5b17232a80033915cec98ea07e6c4cefa
Jonathan Alpay <suriteka@gmail.com>
2019-04-26 12:24:19 +0200

    Fix linting errors

--- packages/e2e-tests/plugins/image-size.php
+++ packages/e2e-tests/plugins/image-size.php
@@ -11,20 +11,22 @@
  * Registers a custom script for the plugin.
  */
 function gutenberg_test_create_image_size() {
-    if ( function_exists( 'add_image_size' ) ) {
-        add_image_size( 'custom-size-one', 499 );
-    }
+	if ( function_exists( 'add_image_size' ) ) {
+		add_image_size( 'custom-size-one', 499 );
+	}
 }
 
 /**
  * Add custom size
+ *
+ * @param Array $sizes Size name.
  */
-function gutenberg_test_create_image_size_name($sizes){
-    $custom_sizes = array(
-    	'custom-size-one' => 'Custom Size One',
-    );
-    return array_merge( $sizes, $custom_sizes );
+function gutenberg_test_create_image_size_name( $sizes ) {
+	$custom_sizes = array(
+		'custom-size-one' => 'Custom Size One',
+	);
+	return array_merge( $sizes, $custom_sizes );
 }
 
 add_action( 'after_setup_theme', 'gutenberg_test_create_image_size' );
-add_filter('image_size_names_choose', 'gutenberg_test_create_image_size_name');
+add_filter( 'image_size_names_choose', 'gutenberg_test_create_image_size_name' );
