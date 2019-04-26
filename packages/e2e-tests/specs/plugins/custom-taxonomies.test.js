3fb9542855a8d2865c53b21fa7423a1f8793cad7
Nicolas Juen <njuen@beapi.fr>
2019-04-26 10:27:47 +0200

    Remove comments block for inline comments, make one single test instead of breaking it into pieces

--- packages/e2e-tests/specs/plugins/custom-taxonomies.test.js
+++ packages/e2e-tests/specs/plugins/custom-taxonomies.test.js
@@ -20,30 +20,21 @@ describe( 'Custom Taxonomies labels are used', () => {
 		await deactivatePlugin( 'gutenberg-test-custom-taxonomies' );
 	} );
 
-	it( 'Ensures the new item custom taxonomy labels are respected', async () => {
+	it( 'Ensures the custom taxonomy labels are respected', async () => {
 		const button = await page.$x( "//button[contains(text(), 'Model') and contains(@class, 'components-button')]" );
 		expect( button ).not.toBeFalsy();
 
 		const labelNew = await page.$x( "//label[@class='components-form-token-field__label' and contains(text(), 'Add New Model')]" );
 		expect( labelNew ).not.toBeFalsy();
-	} );
 
-	it( 'Ensures the custom taxonomy labels are respected', async () => {
-		/**
-         * Open the elements.
-         */
-		const [ label ] = await page.$x( "//button[contains(text(), 'Model') and contains(@class, 'components-button')]" );
-		await label.click();
+		// Open the sidebar.
+		await button.click();
 
-		/**
-         * Fill the elements.
-         */
+		// Fill with one entry
 		await page.type( 'input.components-form-token-field__input', 'Model 1' );
 		await page.keyboard.press( 'Enter' );
 
-		/**
-         * Check the xpath
-         */
+		// Check the "Remove Model"
 		const value = await page.$x( "//div[@class='components-form-token-field__input-container']//span//button[@aria-label='Remove Model']" );
 		expect( value ).not.toBeFalsy();
 	} );
