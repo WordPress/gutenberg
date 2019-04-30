ecf86a5cec4f0202a40021b0f690615677c061c4
etoledom <etoledom@icloud.com>
2019-04-30 21:02:51 +0200

    Cleanup RCTAztecViewManager

--- react-native-aztec/ios/RNTAztecView/RCTAztecViewManager.m
+++ react-native-aztec/ios/RNTAztecView/RCTAztecViewManager.m
@@ -27,8 +27,6 @@ RCT_EXPORT_VIEW_PROPERTY(fontWeight, NSString)
 RCT_EXPORT_VIEW_PROPERTY(disableEditingMenu, BOOL)
 RCT_REMAP_VIEW_PROPERTY(textAlign, textAlignment, NSTextAlignment)
 
-//RCT_REMAP_VIEW_PROPERTY(textAlign, textAlignment, NSTextAlignment)
-
 RCT_EXTERN_METHOD(applyFormat:(nonnull NSNumber *)node format:(NSString *)format)
 RCT_EXTERN_METHOD(setLink:(nonnull NSNumber *)node url:(nonnull NSString *)url title:(nullable NSString *)title)
 RCT_EXTERN_METHOD(removeLink:(nonnull NSNumber *)node)
