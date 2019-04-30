ef71cdf7dc1ba540a75095a48ef11b113745c4a5
Jon Surrell <jon.surrell@automattic.com>
2019-04-30 16:29:09 +0200

    Babel plugin JSX: Implement Fragment handling (#15120)
    Add imports for `<></>` JSX Fragments.

--- packages/babel-preset-default/index.js
+++ packages/babel-preset-default/index.js
@@ -58,12 +58,14 @@ module.exports = function( api ) {
 				require.resolve( '@wordpress/babel-plugin-import-jsx-pragma' ),
 				{
 					scopeVariable: 'createElement',
+					scopeVariableFrag: 'Fragment',
 					source: '@wordpress/element',
 					isDefault: false,
 				},
 			],
 			[ require.resolve( '@babel/plugin-transform-react-jsx' ), {
 				pragma: 'createElement',
+				pragmaFrag: 'Fragment',
 			} ],
 			require.resolve( '@babel/plugin-proposal-async-generator-functions' ),
 			maybeGetPluginTransformRuntime(),
