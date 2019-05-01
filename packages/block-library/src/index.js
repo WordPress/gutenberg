75c2e6f8333b773c01c19c9c30b32879ec992641
ashwin-pc <ashwinpc1993@gmail.com>
2019-05-01 02:51:38 -0400

    added table of contents block

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -54,6 +54,7 @@ import * as shortcode from './shortcode';
 import * as spacer from './spacer';
 import * as subhead from './subhead';
 import * as table from './table';
+import * as tableOfContents from './table-of-contents';
 import * as template from './template';
 import * as textColumns from './text-columns';
 import * as verse from './verse';
@@ -119,6 +120,7 @@ export const registerCoreBlocks = () => {
 		spacer,
 		subhead,
 		table,
+		tableOfContents,
 		tagCloud,
 		template,
 		textColumns,
