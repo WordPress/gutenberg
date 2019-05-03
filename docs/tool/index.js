1e29a81f2a2cd6681ae3481a880a6a414b241ebc
Andrés <andres.maneiro@automattic.com>
2019-05-03 16:55:51 +0200

    Use docgen to generate data docs

--- docs/tool/index.js
+++ docs/tool/index.js
@@ -7,19 +7,13 @@ const fs = require( 'fs' );
  * Internal dependencies
  */
 const config = require( './config' );
-const parser = require( './parser' );
-const generator = require( './generator' );
-const { getPackageManifest, getComponentManifest, getDataManifest, getRootManifest } = require( './manifest' );
-
-const parsedModules = parser( config.dataNamespaces );
-generator( parsedModules, config.dataDocsOutput );
+const { getPackageManifest, getComponentManifest, getRootManifest } = require( './manifest' );
 
 const rootManifest = getRootManifest( config.tocFileName );
 const packageManifest = getPackageManifest( config.packageFileNames );
 const componentManifest = getComponentManifest( config.componentPaths );
-const dataManifest = getDataManifest( parsedModules );
 
 fs.writeFileSync(
 	config.manifestOutput,
-	JSON.stringify( rootManifest.concat( packageManifest, componentManifest, dataManifest ), undefined, '\t' )
+	JSON.stringify( rootManifest.concat( packageManifest, componentManifest ), undefined, '\t' )
 );
