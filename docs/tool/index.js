58eff1e7433e6b7686d30a9f3c657bc3e8b175b7
Andrés <andres.maneiro@automattic.com>
2019-05-03 17:01:58 +0200

    Consolidate

--- docs/tool/index.js
+++ docs/tool/index.js
@@ -2,12 +2,17 @@
  * Node dependencies
  */
 const fs = require( 'fs' );
+const { join } = require( 'path' );
+const { execSync } = require( 'child_process' );
 
 /**
  * Internal dependencies
  */
 const config = require( './config' );
 const { getPackageManifest, getComponentManifest, getRootManifest } = require( './manifest' );
+
+// Update data files from code
+execSync( join( __dirname, 'update-data.js' ) );
 
 const rootManifest = getRootManifest( config.tocFileName );
 const packageManifest = getPackageManifest( config.packageFileNames );
