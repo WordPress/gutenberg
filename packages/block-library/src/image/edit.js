20da28aef67a4260498231ea3a7d414a7f559aa2
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-05-06 18:08:21 +0300

    assigns on component update the class name based on URL. still duplicates on front end

--- packages/block-library/src/image/edit.js
+++ packages/block-library/src/image/edit.js
@@ -163,6 +163,13 @@ class ImageEdit extends Component {
 				captionFocused: false,
 			} );
 		}
+		let mediaSizeClass = this.props.attributes.mediaSizeClass;
+		if ( ! mediaSizeClass ) {
+			mediaSizeClass = 'size-' + this.getImageSizeByURL();
+			this.props.setAttributes( {
+				mediaSizeClass,
+			} );
+		}
 	}
 
 	onUploadError( message ) {
@@ -395,6 +402,7 @@ class ImageEdit extends Component {
 			width,
 			height,
 			linkTarget,
+			mediaSizeClass,
 		} = attributes;
 		const isExternal = isExternalImage( id, url );
 		const editImageIcon = ( <SVG width={ 20 } height={ 20 } viewBox="0 0 20 20"><Rect x={ 11 } y={ 3 } width={ 7 } height={ 5 } rx={ 1 } /><Rect x={ 2 } y={ 12 } width={ 7 } height={ 5 } rx={ 1 } /><Path d="M13,12h1a3,3,0,0,1-3,3v2a5,5,0,0,0,5-5h1L15,9Z" /><Path d="M4,8H3l2,3L7,8H6A3,3,0,0,1,9,5V3A5,5,0,0,0,4,8Z" /></SVG> );
@@ -453,13 +461,6 @@ class ImageEdit extends Component {
 					{ mediaPlaceholder }
 				</>
 			);
-		}
-		let mediaSizeClass = attributes.mediaSizeClass;
-		if ( ! mediaSizeClass ) {
-			mediaSizeClass = 'size-' + this.getImageSizeByURL();
-			this.props.setAttributes( {
-				mediaSizeClass,
-			} );
 		}
 
 		const classNameWithoutSize = className.replace( /size-(.*)/, '' );
