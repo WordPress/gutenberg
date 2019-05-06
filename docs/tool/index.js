eab473913d7c98d0ed4a26c7572786a9fbcb8b92
Andrés <nosolosw@users.noreply.github.com>
2019-05-06 19:43:56 +0200

    Fix next/previous links in handbook (#15456)

--- docs/tool/index.js
+++ docs/tool/index.js
@@ -4,21 +4,18 @@
 const fs = require( 'fs' );
 const { join } = require( 'path' );
 const { execSync } = require( 'child_process' );
+const path = require( 'path' );
 
 /**
  * Internal dependencies
  */
-const config = require( './config' );
-const { getPackageManifest, getComponentManifest, getRootManifest } = require( './manifest' );
+const { getRootManifest } = require( './manifest' );
+
+const tocFileInput = path.resolve( __dirname, '../toc.json' );
+const manifestOutput = path.resolve( __dirname, '../manifest.json' );
 
 // Update data files from code
 execSync( join( __dirname, 'update-data.js' ) );
 
-const rootManifest = getRootManifest( config.tocFileName );
-const packageManifest = getPackageManifest( config.packageFileNames );
-const componentManifest = getComponentManifest( config.componentPaths );
-
-fs.writeFileSync(
-	config.manifestOutput,
-	JSON.stringify( rootManifest.concat( packageManifest, componentManifest ), undefined, '\t' )
-);
+// Process TOC file and generate manifest handbook
+fs.writeFileSync( manifestOutput, JSON.stringify( getRootManifest( tocFileInput ), undefined, '\t' ) );
