243385d6d051a0282822bd259beb44054e8d902e
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2019-05-07 16:20:05 +0300

    Add dev menu item to toggle html mode

--- android/app/src/main/java/com/gutenberg/MainApplication.java
+++ android/app/src/main/java/com/gutenberg/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.react.devsupport.interfaces.DevOptionHandler;
+import com.facebook.react.devsupport.interfaces.DevSupportManager;
 import com.horcrux.svg.SvgPackage;
 
 import org.wordpress.mobile.ReactNativeAztec.ReactAztecPackage;
@@ -23,85 +25,100 @@ public class MainApplication extends Application implements ReactApplication {
 
     private static final String TAG = "MainApplication";
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
+    private ReactNativeHost mReactNativeHost;
+    private RNReactNativeGutenbergBridgePackage mRnReactNativeGutenbergBridgePackage;
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(new MainReactPackage(), new SvgPackage(), new ReactAztecPackage(),
-                    new RNRecyclerviewListPackage(),
-                    new RNReactNativeGutenbergBridgePackage(new GutenbergBridgeJS2Parent() {
-                @Override
-                public void responseHtml(String title, String html, boolean changed) {
+    private ReactNativeHost createReactNativeHost() {
+        mRnReactNativeGutenbergBridgePackage = new RNReactNativeGutenbergBridgePackage(new GutenbergBridgeJS2Parent() {
+            @Override
+            public void responseHtml(String title, String html, boolean changed) {
+            }
+
+            @Override
+            public void requestMediaPickFromMediaLibrary(MediaSelectedCallback mediaSelectedCallback) {
+            }
+
+            @Override
+            public void requestMediaPickFromDeviceLibrary(MediaUploadCallback mediaUploadCallback) {
+            }
+
+            @Override
+            public void requestMediaPickerFromDeviceCamera(MediaUploadCallback mediaUploadCallback) {
+            }
+
+            @Override
+            public void requestMediaImport(String url, MediaSelectedCallback mediaSelectedCallback) {
+            }
+
+            @Override
+            public void mediaUploadSync(MediaUploadCallback mediaUploadCallback) {
+            }
+
+            @Override
+            public void requestImageFailedRetryDialog(int mediaId) {
+            }
+
+            @Override
+            public void requestImageUploadCancelDialog(int mediaId) {
+            }
+
+            @Override
+            public void requestImageUploadCancel(int mediaId) {
+            }
+
+            @Override
+            public void editorDidMount(boolean hasUnsupportedBlocks) {
+            }
+
+            @Override
+            public void editorDidEmitLog(String message, LogLevel logLevel) {
+                switch (logLevel) {
+                    case TRACE:
+                        Log.d(TAG, message);
+                        break;
+                    case INFO:
+                        Log.i(TAG, message);
+                        break;
+                    case WARN:
+                        Log.w(TAG, message);
+                        break;
+                    case ERROR:
+                        Log.e(TAG, message);
+                        break;
                 }
+            }
+        });
 
-                @Override
-                public void requestMediaPickFromMediaLibrary(MediaSelectedCallback mediaSelectedCallback) {
-                }
+        return new ReactNativeHost(this) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
 
-                @Override
-                public void requestMediaPickFromDeviceLibrary(MediaUploadCallback mediaUploadCallback) {
-                }
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return Arrays.asList(
+                        new MainReactPackage(),
+                        new SvgPackage(),
+                        new ReactAztecPackage(),
+                        new RNRecyclerviewListPackage(),
+                        mRnReactNativeGutenbergBridgePackage);
+            }
 
-                @Override
-                public void requestMediaPickerFromDeviceCamera(MediaUploadCallback mediaUploadCallback) {
-                }
-
-                @Override
-                public void requestMediaImport(String url, MediaSelectedCallback mediaSelectedCallback) {
-                }
-
-                @Override
-                public void mediaUploadSync(MediaUploadCallback mediaUploadCallback) {
-                }
-
-                @Override
-                public void requestImageFailedRetryDialog(int mediaId) {
-                }
-
-                @Override
-                public void requestImageUploadCancelDialog(int mediaId) {
-                }
-
-                @Override
-                public void requestImageUploadCancel(int mediaId) {
-                }
-
-                @Override
-                public void editorDidMount(boolean hasUnsupportedBlocks) {
-                }
-
-                @Override
-                public void editorDidEmitLog(String message, LogLevel logLevel) {
-                    switch (logLevel) {
-                        case TRACE:
-                            Log.d(TAG, message);
-                            break;
-                        case INFO:
-                            Log.i(TAG, message);
-                            break;
-                        case WARN:
-                            Log.w(TAG, message);
-                            break;
-                        case ERROR:
-                            Log.e(TAG, message);
-                            break;
-                    }
-                }
-            }));
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-    };
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+    }
 
     @Override
     public ReactNativeHost getReactNativeHost() {
+        if (mReactNativeHost == null) {
+            mReactNativeHost = createReactNativeHost();
+            createCustomDevOptions(mReactNativeHost);
+        }
+
         return mReactNativeHost;
     }
 
@@ -109,5 +126,16 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+    }
+
+    private void createCustomDevOptions(ReactNativeHost reactNativeHost) {
+        DevSupportManager devSupportManager = reactNativeHost.getReactInstanceManager().getDevSupportManager();
+
+        devSupportManager.addCustomDevOption("Show html", new DevOptionHandler() {
+            @Override
+            public void onOptionSelected() {
+                mRnReactNativeGutenbergBridgePackage.getRNReactNativeGutenbergBridgeModule().toggleEditorMode();
+            }
+        });
     }
 }
