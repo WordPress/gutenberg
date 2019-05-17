df43d4d8f69520ea48e7a9250b884d95af9871c7
Marcus Kazmierczak <marcus@mkaz.com>
2019-05-17 14:30:51 -0700

    Update links to new handbook location

--- packages/blocks/src/api/registration.js
+++ packages/blocks/src/api/registration.js
@@ -150,7 +150,7 @@ export function registerBlockType( name, settings ) {
 	if ( ! isValidIcon( settings.icon.src ) ) {
 		console.error(
 			'The icon passed is invalid. ' +
-			'The icon should be a string, an element, a function, or an object following the specifications documented in https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-registration/#icon-optional'
+			'The icon should be a string, an element, a function, or an object following the specifications documented in https://developer.wordpress.org/block-editor/developers/block-api/block-registration/#icon-optional'
 		);
 		return;
 	}
