831f54e1b8e3a476cb6319b3c460d5c66bf81afe
Riad Benguella <benguella@gmail.com>
2019-05-17 13:04:17 +0100

    Checking the current branch

--- bin/commander.js
+++ bin/commander.js
@@ -2,12 +2,14 @@
 
 /* eslint-disable no-console */
 
+// Dependencies
 const path = require( 'path' );
 const program = require( 'commander' );
 const inquirer = require( 'inquirer' );
 const semver = require( 'semver' );
 const chalk = require( 'chalk' );
 const fs = require( 'fs' );
+const SimpleGit = require( 'simple-git/promise' );
 
 // Common info
 const rootFolder = path.resolve( __dirname, '../' );
@@ -16,20 +18,38 @@ const packageLockPath = rootFolder + '/package-lock.json';
 const pluginFilePath = rootFolder + '/gutenberg.php';
 const packageJson = require( packageJsonPath );
 const packageLock = require( packageLockPath );
+const simpleGit = SimpleGit( rootFolder );
 
 // UI
 const error = chalk.bold.red;
+const warning = chalk.bold.keyword( 'orange' );
 const success = chalk.bold.green;
 
 program
 	.command( 'release-plugin-rc' )
+	.alias( 'rc' )
+	.description( 'Release an RC version of the plugin (supports only rc.1 for now)' )
 	.action( async () => {
-		// Choosing the right version
+		// Check the current branch and if the versions
+		const status = await simpleGit.status();
 		const parsedVersion = semver.parse( packageJson.version );
+		if ( status.current !== 'master' ) {
+			const { acceptBranch } = await inquirer.prompt( [ {
+				type: 'confirm',
+				name: 'acceptBranch',
+				message: 'Releasing plugin RC versions usually happens from the ' + warning( 'master' ) + ' branch. Do you want to continue from the current branch?',
+				default: false,
+			} ] );
+
+			if ( ! acceptBranch ) {
+				console.log( error( 'Aborting' ) );
+				process.exit( 1 );
+			}
+		}
+
+		// Choosing the right version
 		let nextVersion;
-		if ( parsedVersion.prerelease && parsedVersion.prerelease[ 0 ] === 'rc' ) {
-			nextVersion = semver.inc( packageJson.version, 'prerelease', 'rc' );
-		} else if ( parsedVersion.minor === 9 ) {
+		if ( parsedVersion.minor === 9 ) {
 			nextVersion = ( parsedVersion.major + 1 ) + '.0.0-rc.1';
 		} else {
 			nextVersion = parsedVersion.major + '.' + ( parsedVersion.minor + 1 ) + '.0-rc.1';
@@ -59,7 +79,7 @@ program
 		const content = await fs.readFileSync( pluginFilePath, 'utf8' );
 		await fs.writeFileSync( pluginFilePath, content.replace( ' * Version: ' + packageJson.version, ' * Version: ' + nextVersion ) );
 
-		console.log( success( 'The version has been updated, commit the diff.' ) );
+		console.log( success( 'The version has been updated, commit the diff to the release branch.' ) );
 	} );
 
 program.parse( process.argv );
