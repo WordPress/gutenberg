302b7cafb52a99bc5770a1fd0b574608033d9dc7
Andrew Duthie <andrew@andrewduthie.com>
2019-05-20 12:05:21 -0400

    Build Tooling: Define build concurrency explicitly

--- bin/packages/build.js
+++ bin/packages/build.js
@@ -44,7 +44,15 @@ if ( ! files.length ) {
 	};
 }
 
-const worker = workerFarm( require.resolve( './build-worker' ) );
+const options = {};
+if ( process.env.HARDWARE_CONCURRENCY ) {
+	// WorkerFarm is configured to use the number of CPUs available as the
+	// default value. This can be overwritten for finite control or for use in
+	// uncooperative continuous integration.
+	options.maxConcurrentWorkers = Number( process.env.HARDWARE_CONCURRENCY );
+}
+
+const worker = workerFarm( options, require.resolve( './build-worker' ) );
 
 let complete = 0;
 
