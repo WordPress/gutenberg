6aaeb56e0dc95907b83f677207c10241c9c23f8b
Darren Ethier <darren@roughsmootheng.in>
2019-05-20 18:40:59 -0400

    latest changes
    - reimplement useIsomorphicLayoutEffect
    - set mapOutput on initial render
    - implement forRender call instead of mapOutPut being stateful.

--- packages/data/src/components/use-select/index.js
+++ packages/data/src/components/use-select/index.js
@@ -4,10 +4,10 @@
 import { createQueue } from '@wordpress/priority-queue';
 import {
 	useLayoutEffect,
-	useState,
 	useRef,
 	useMemo,
-	useCallback,
+	useEffect,
+	useReducer,
 } from '@wordpress/element';
 import { isShallowEqualObjects } from '@wordpress/is-shallow-equal';
 
@@ -17,54 +17,59 @@ import { isShallowEqualObjects } from '@wordpress/is-shallow-equal';
 import useRegistry from '../registry-provider/use-registry';
 import useAsyncMode from '../async-mode-provider/use-async-mode';
 
+/**
+ * Favor useLayoutEffect to ensure the store subscription callback always has
+ * the selector from the latest render. If a store update happens between render
+ * and the effect, this could cause missed/stale updates or inconsistent state.
+ *
+ * Fallback to useEffect for server rendered components because currently React
+ * throws a warning when using useLayoutEffect in that environment.
+ */
+const useIsomorphicLayoutEffect =
+	typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+
 const renderQueue = createQueue();
 
-export default function useSelect( mapSelect, nextProps ) {
+export default function useSelect( mapSelect ) {
 	const registry = useRegistry();
 	const isAsync = useAsyncMode();
-	const [ mapOutput, setMapOutput ] = useState( mapSelect( registry.select ) );
 	const queueContext = useMemo( () => ( { queue: true } ), [ registry ] );
+	const [ , forceRender ] = useReducer( ( s ) => s + 1, 0 );
 
-	const latestMapSelect = useRef( mapSelect );
+	const latestMapSelect = useRef();
 	const latestIsAsync = useRef( isAsync );
-	const latestMapOutput = useRef( mapOutput );
+	const latestMapOutput = useRef();
 	const isMounted = useRef();
 
-	const onStoreChange = useCallback( () => {
-		if ( isMounted.current ) {
-			const newMapOutput = latestMapSelect.current( registry.select, registry );
-			if ( ! isShallowEqualObjects( latestMapOutput.current, newMapOutput ) ) {
-				latestMapOutput.current = newMapOutput;
-				setMapOutput( newMapOutput );
-			}
-		}
-	}, [ registry ] );
+	let mapOutput;
 
-	/**
-	 * Set current values.
-	 */
-	useLayoutEffect( () => {
+	if ( latestMapSelect.current !== mapSelect ) {
+		mapOutput = mapSelect( registry.select, registry );
+	} else {
+		mapOutput = latestMapOutput.current;
+	}
+
+	useIsomorphicLayoutEffect( () => {
 		latestMapSelect.current = mapSelect;
+		if ( latestIsAsync.current !== isAsync ) {
+			latestIsAsync.current = isAsync;
+			renderQueue.flush( queueContext );
+		}
+		latestMapOutput.current = mapOutput;
 		isMounted.current = true;
 	} );
 
-	/**
-	 * Flush queue if isAsync changes
-	 */
-	useLayoutEffect( () => {
-		latestIsAsync.current = isAsync;
-		renderQueue.flush( queueContext );
-	}, [ isAsync ] );
+	useIsomorphicLayoutEffect( () => {
+		const onStoreChange = () => {
+			if ( isMounted.current ) {
+				const newMapOutput = latestMapSelect.current( registry.select, registry );
+				if ( ! isShallowEqualObjects( latestMapOutput.current, newMapOutput ) ) {
+					latestMapOutput.current = newMapOutput;
+					forceRender();
+				}
+			}
+		};
 
-	/**
-	 * Map output should always be called if incoming prop dependencies
-	 * has changed.
-	 */
-	useLayoutEffect( () => {
-		onStoreChange();
-	}, [ registry, nextProps ] );
-
-	useLayoutEffect( () => {
 		const unsubscribe = registry.subscribe( () => {
 			if ( latestIsAsync.current ) {
 				renderQueue.add( queueContext, onStoreChange );
@@ -78,7 +83,7 @@ export default function useSelect( mapSelect, nextProps ) {
 			unsubscribe();
 			renderQueue.flush( queueContext );
 		};
-	}, [ onStoreChange ] );
+	}, [ registry ] );
 
 	return mapOutput;
 }
