4f54fd7302684f4497064daca9a12d752ea5bcba
Kjell Reigstad <kjell@kjellr.com>
2019-05-21 09:04:05 -0400

    Revert "Add hasChildSelected prop"
    This reverts commit a3e9dbd8352c3bc76e9c9a53897fafa27073e581.

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -63,7 +63,6 @@ const preventDrag = ( event ) => {
 
 function BlockListBlock( {
 	blockRef,
-	hasChildSelected,
 	mode,
 	isFocusMode,
 	hasFixedToolbar,
@@ -387,7 +386,6 @@ function BlockListBlock( {
 				const wrapperClassName = classnames(
 					'wp-block editor-block-list__block block-editor-block-list__block',
 					{
-						'has-child-selected': hasChildSelected,
 						'has-warning': ! isValid || !! hasError || isUnregisteredBlock,
 						'is-selected': shouldAppearSelected,
 						'is-multi-selected': isPartOfMultiSelection,
@@ -399,6 +397,7 @@ function BlockListBlock( {
 							isFocusMode && ( isSelected || isParentOfSelectedBlock ),
 						'has-child-selected': isParentOfSelectedBlock,
 						'is-focus-mode': isFocusMode,
+						'has-child-selected': isParentOfSelectedBlock,
 					},
 					className
 				);
@@ -605,7 +604,6 @@ const applyWithSelect = withSelect(
 		const { name, attributes, isValid } = block || {};
 
 		return {
-			hasChildSelected: hasSelectedInnerBlock( clientId ),
 			isPartOfMultiSelection:
 				isBlockMultiSelected( clientId ) || isAncestorMultiSelected( clientId ),
 			isFirstMultiSelected: isFirstMultiSelectedBlock( clientId ),
