8e9a42a91921c06c2830032e7cdf538d11aa8c20
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-05-23 08:46:12 +0300

    removes toolbar code duplication

--- packages/block-library/src/audio/edit.js
+++ packages/block-library/src/audio/edit.js
@@ -137,18 +137,22 @@ class AudioEdit extends Component {
 			this.setState( { src: media.url, isEditing: false } );
 		};
 		const editImageIcon = ( <SVG width={ 20 } height={ 20 } viewBox="0 0 20 20"><Rect x={ 11 } y={ 3 } width={ 7 } height={ 5 } rx={ 1 } /><Rect x={ 2 } y={ 12 } width={ 7 } height={ 5 } rx={ 1 } /><Path d="M13,12h1a3,3,0,0,1-3,3v2a5,5,0,0,0,5-5h1L15,9Z" /><Path d="M4,8H3l2,3L7,8H6A3,3,0,0,1,9,5V3A5,5,0,0,0,4,8Z" /></SVG> );
+		const getEditToolbar = ( isActive ) => {
+			return <Toolbar>
+				<IconButton
+					className={ classnames( 'components-toolbar__control', { 'is-active': isActive } ) }
+					label={ __( 'Edit audio' ) }
+					onClick={ toggleIsEditing }
+					icon={ editImageIcon }
+				/>
+			</Toolbar>;
+		};
+
 		if ( isEditing ) {
 			return (
 				<Fragment>
 					<BlockControls>
-						{ !! src && ( <Toolbar>
-							<IconButton
-								className={ classnames( 'components-toolbar__control is-active' ) }
-								label={ __( 'Edit audio' ) }
-								onClick={ toggleIsEditing }
-								icon={ editImageIcon }
-							/>
-						</Toolbar> ) }
+						{ !! src && getEditToolbar( true ) }
 					</BlockControls>
 					<MediaPlaceholder
 						icon={ <BlockIcon icon={ icon } /> }
@@ -170,14 +174,7 @@ class AudioEdit extends Component {
 		return (
 			<>
 				<BlockControls>
-					<Toolbar>
-						<IconButton
-							class={ 'components-toolbar__control' }
-							label={ __( 'Edit audio' ) }
-							onClick={ toggleIsEditing }
-							icon={ editImageIcon }
-						/>
-					</Toolbar>
+					{ getEditToolbar( false ) }
 				</BlockControls>
 				<InspectorControls>
 					<PanelBody title={ __( 'Audio Settings' ) }>
