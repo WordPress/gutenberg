92836a744b63f89f4300f6c2b02cae5a917d5d82
Andrew Duthie <andrew@andrewduthie.com>
2019-05-23 16:20:44 -0400

    Data: Implement preferences persistence using user settings

--- lib/compat.php
+++ lib/compat.php
@@ -24,3 +24,39 @@ function gutenberg_safe_style_css_column_flex_basis( $attr ) {
 	return $attr;
 }
 add_filter( 'safe_style_css', 'gutenberg_safe_style_css_column_flex_basis' );
+
+/**
+ * Adds an inline script to augment the default persistence method for data to
+ * use WordPress client user settings.
+ *
+ * @since 5.8.0
+ */
+function gutenberg_settings_cookie_persistence() {
+	// Ensure `utils` is a dependency, which makes available `userSettings`,
+	// `getUserSetting`, and `setUserSettings` window globals.
+	$data_script = wp_scripts()->query( 'wp-data', 'registered' );
+	if ( ! in_array( 'utils', $data_script->deps ) ) {
+		$data_script->deps[] = 'utils';
+	}
+
+	wp_add_inline_script(
+		'wp-data',
+		"
+( function() {
+	wp.data.use( wp.data.plugins.persistence, {
+		storage: {
+			getItem: function() {
+				return (
+					window.getUserSetting( 'dataState' ) ||
+					localStorage.getItem( 'WP_DATA_USER_' + window.userSettings.uid )
+				);
+			},
+			setItem: function( key, value ) {
+				window.setUserSetting( 'dataState', value );
+			}
+		}
+	} );
+} )();"
+	);
+}
+add_action( 'admin_enqueue_scripts', 'gutenberg_settings_cookie_persistence' );
