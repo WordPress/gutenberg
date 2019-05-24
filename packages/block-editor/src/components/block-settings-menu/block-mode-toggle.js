fe3f6d73b8f7c23f93132882801589a4104ebff2
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-05-24 14:12:26 +0300

    correct getSettings

--- packages/block-editor/src/components/block-settings-menu/block-mode-toggle.js
+++ packages/block-editor/src/components/block-settings-menu/block-mode-toggle.js
@@ -34,9 +34,9 @@ export function BlockModeToggle( { blockType, mode, onToggleMode, small = false,
 
 export default compose( [
 	withSelect( ( select, { clientId } ) => {
-		const { getBlock, getBlockMode, getEditorSettings } = select( 'core/block-editor' );
+		const { getBlock, getBlockMode, getSettings } = select( 'core/block-editor' );
 		const block = getBlock( clientId );
-		const isCodeEditingEnabled = getEditorSettings().codeEditingEnabled;
+		const isCodeEditingEnabled = getSettings().codeEditingEnabled;
 
 		return {
 			mode: getBlockMode( clientId ),
