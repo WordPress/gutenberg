b49864169b758c7f9676ee4c3159c2b25ce66707
Daniel Richards <daniel.richards@automattic.com>
2019-05-29 18:20:48 +0800

    Move drag utility to e2e-test-utils package

--- packages/e2e-tests/specs/blocks/spacer.test.js
+++ packages/e2e-tests/specs/blocks/spacer.test.js
@@ -5,34 +5,8 @@ import {
 	clickBlockAppender,
 	getEditedPostContent,
 	createNewPost,
+	dragAndResize,
 } from '@wordpress/e2e-test-utils';
-
-/**
- * Clicks an element, drags a particular distance and releases the mouse button.
- *
- * @param {Object} element The puppeteer element handle.
- * @param {number} delta   Object containing movement distances.
- * @param {number} delta.x Horizontal distance to drag.
- * @param {number} delta.y Vertical distance to drag.
- *
- * @return {Promise} Promise resolving when drag completes.
- */
-export async function dragAngDrop( element, delta ) {
-	const {
-		x: elementX,
-		y: elementY,
-		width: elementWidth,
-		height: elementHeight,
-	} = await element.boundingBox();
-
-	const originX = elementX + ( elementWidth / 2 );
-	const originY = elementY + ( elementHeight / 2 );
-
-	await page.mouse.move( originX, originY );
-	await page.mouse.down();
-	await page.mouse.move( originX + delta.x, originY + delta.y );
-	await page.mouse.up();
-}
 
 describe( 'Spacer', () => {
 	beforeEach( async () => {
@@ -55,8 +29,7 @@ describe( 'Spacer', () => {
 		await page.keyboard.press( 'Enter' );
 
 		const resizableHandle = await page.$( '.block-library-spacer__resize-container .components-resizable-box__handle' );
-		await dragAngDrop( resizableHandle, { x: 0, y: 50 } );
-
+		await dragAndResize( resizableHandle, { x: 0, y: 50 } );
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 
 		const selectedSpacer = await page.$( '[data-type="core/spacer"].is-selected' );
