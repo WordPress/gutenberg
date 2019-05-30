9dcce6ffe391e9cf6857431a6d2180b7bfe90e4d
Darren Ethier <darren@roughsmootheng.in>
2019-05-29 21:57:46 -0400

    remove unnecessary use of useMemo

--- packages/data/src/components/use-dispatch/use-dispatch.js
+++ packages/data/src/components/use-dispatch/use-dispatch.js
@@ -1,20 +1,11 @@
 /**
- * WordPress dependencies
- */
-import { useMemo } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
 import useRegistry from '../registry-provider/use-registry';
 
 const useDispatch = ( storeName ) => {
-	const registry = useRegistry();
-	return useMemo( () => {
-		return storeName === void 0 ?
-			registry.dispatch :
-			registry.dispatch( storeName );
-	}, [ registry, storeName ] );
+	const { dispatch } = useRegistry();
+	return storeName === void 0 ? dispatch : dispatch( storeName );
 };
 
 export default useDispatch;
