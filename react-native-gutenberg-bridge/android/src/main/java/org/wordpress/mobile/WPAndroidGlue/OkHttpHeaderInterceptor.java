bd73de7df18c858afd6569d7e24ce1798936d8be
Danilo Ercoli <ercoli@gmail.com>
2019-05-31 15:44:27 +0200

    Check if the HttpUrl obj of the current request is null before calling `toString` over it

--- react-native-gutenberg-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/OkHttpHeaderInterceptor.java
+++ react-native-gutenberg-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/OkHttpHeaderInterceptor.java
@@ -6,12 +6,12 @@ import org.wordpress.mobile.WPAndroidGlue.WPAndroidGlueCode.OnAuthHeaderRequeste
 
 import java.io.IOException;
 
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class OkHttpHeaderInterceptor implements Interceptor {
-
     private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
 
     private OnAuthHeaderRequestedListener mOnAuthHeaderRequestedListener;
@@ -23,13 +23,16 @@ public class OkHttpHeaderInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request.Builder builder = chain.request().newBuilder();
+        final HttpUrl httpUrl = chain.request().url();
+        // This should not be necessary here, but https://github.com/wordpress-mobile/WordPress-Android/issues/9833
+        // does seem to show that a null value is returned
+        String urlString = httpUrl != null && httpUrl.toString() != null ? httpUrl.toString() : "";
 
-        String authHeader = mOnAuthHeaderRequestedListener.onAuthHeaderRequested(chain.request().url().toString());
+        String authHeader = mOnAuthHeaderRequestedListener.onAuthHeaderRequested(urlString);
         if (!TextUtils.isEmpty(authHeader)) {
             builder.addHeader(AUTHORIZATION_HEADER_KEY, authHeader);
         }
 
         return chain.proceed(builder.build());
     }
-
 }
