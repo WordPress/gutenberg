ca8e65f86c0249c39ebf69eaa654ec10b5089ea7
Sergio Estevao <sergioestevao@gmail.com>
2019-06-04 16:19:48 +0100

    Add spacer between BlockQuotation elements.

--- packages/components/src/primitives/block-quotation/index.native.js
+++ packages/components/src/primitives/block-quotation/index.native.js
@@ -10,7 +10,11 @@ import styles from './style.scss';
 export const BlockQuotation = ( props ) => {
 	return (
 		<View style={ styles.wpBlockQuote } >
-			{ props.children }
+			{
+				props.children.map( ( child, index ) => {
+					return ( child && <View key={ 'spacer_' + index } style={ index !== 0 && styles.wpBlockQuoteSpacer }>{ child }</View> );
+				} )
+			}
 		</View>
 	);
 };
