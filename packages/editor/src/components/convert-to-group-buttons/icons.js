1a938adb4a291b1f78ff542e6ce2b61e58b08860
Dave Smith <getdavemail@gmail.com>
2019-06-05 11:37:03 +0100

    Reinstate missing Ungroup icon
    Accidentally got lost during renaming of “UnGroup” to “Ungroup”!

--- packages/editor/src/components/convert-to-group-buttons/icons.js
+++ packages/editor/src/components/convert-to-group-buttons/icons.js
@@ -15,5 +15,5 @@ const UngroupSVG = <SVG width="20" height="20" viewBox="0 0 24 24" fill="none" x
 	<Path d="M4 10H8V12H4V20H12V16H14V20C14 21.1 13.1 22 12 22H4C2.9 22 2 21.1 2 20V12C2 10.9 2.9 10 4 10Z" />
 </SVG>;
 
-export const UnGroup = <Icon icon={ UngroupSVG } />;
+export const Ungroup = <Icon icon={ UngroupSVG } />;
 
