20482098a232a420a567dfd4467cdb140f869444
Riad Benguella <benguella@gmail.com>
2019-06-06 16:40:47 +0100

    Adding snackbar notices support to the new widgets page

--- packages/edit-widgets/src/store/actions.js
+++ packages/edit-widgets/src/store/actions.js
@@ -8,6 +8,9 @@ import { get, map } from 'lodash';
  */
 import { parse, serialize } from '@wordpress/blocks';
 import { dispatch, select } from '@wordpress/data-controls';
+import { __ } from '@wordpress/i18n';
+
+const WIDGET_AREAS_SAVE_NOTICE_ID = 'WIDGET_AREAS_SAVE_NOTICE_ID';
 
 /**
  * Yields an action object that setups the widget areas.
@@ -72,5 +75,13 @@ export function* saveWidgetAreas() {
 				content: serialize( blocks ),
 			}
 		);
+		yield dispatch(
+			'core/notices',
+			'createSuccessNotice',
+			__( 'Block areas saved succesfully.' ),
+			{
+				id: WIDGET_AREAS_SAVE_NOTICE_ID,
+				type: 'snackbar',
+			} );
 	}
 }
