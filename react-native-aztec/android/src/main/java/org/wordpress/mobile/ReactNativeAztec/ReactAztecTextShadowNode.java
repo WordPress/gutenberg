57bfd96136357a6c976628bf09aba94f492887a2
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2019-06-07 03:29:45 +0300

    Implement the shadow node for laying out Aztec on Android

---
+++ react-native-aztec/android/src/main/java/org/wordpress/mobile/ReactNativeAztec/ReactAztecTextShadowNode.java
@@ -0,0 +1,25 @@
+package org.wordpress.mobile.ReactNativeAztec;
+
+import android.support.annotation.Nullable;
+
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.views.textinput.ReactTextInputShadowNode;
+
+public class ReactAztecTextShadowNode extends ReactTextInputShadowNode {
+    private @Nullable
+    ReadableMap mTextMap = null;
+    private @Nullable Integer mColor = null;
+
+    @ReactProp(name = PROP_TEXT)
+    public void setText(@Nullable ReadableMap inputMap) {
+        mTextMap = inputMap;
+        markUpdated();
+    }
+
+    @ReactProp(name = "color", customType = "Color")
+    public void setColor(@Nullable Integer color) {
+        mColor = color;
+        markUpdated();
+    }
+}
