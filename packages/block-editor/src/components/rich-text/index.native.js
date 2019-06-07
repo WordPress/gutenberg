8b0ca02057045198595af1717fb1bdab308325f7
Danilo Ercoli <ercoli@gmail.com>
2019-06-07 14:38:43 +0200

    Make sure selection is always a valid number before passing it to native layer

--- packages/block-editor/src/components/rich-text/index.native.js
+++ packages/block-editor/src/components/rich-text/index.native.js
@@ -675,12 +675,9 @@ export class RichText extends Component {
 
 	forceSelectionUpdate( start, end ) {
 		if ( ! this.needsSelectionUpdate ) {
-			if ( isNaN( start ) || isNaN( end ) ) {
-				return;
-			} 
 			this.needsSelectionUpdate = true;
-			this.selectionStart = start;
-			this.selectionEnd = end;
+			this.selectionStart = isNaN( start ) ? 0 : start;
+			this.selectionEnd = isNaN( end ) ? 0 : end;
 			this.forceUpdate();
 		}
 	}
@@ -800,7 +797,10 @@ export class RichText extends Component {
 		let selection = null;
 		if ( this.needsSelectionUpdate ) {
 			this.needsSelectionUpdate = false;
-			selection = { start: this.props.selectionStart, end: this.props.selectionEnd };
+			selection = { 
+				start: isNaN( this.props.selectionStart ) ? 0 : this.props.selectionStart,
+				end: isNaN( this.props.selectionEnd ) ? 0 : this.props.selectionEnd
+			};
 
 			// On AztecAndroid, setting the caret to an out-of-bounds position will crash the editor so, let's check for some cases.
 			if ( ! this.isIOS ) {
