4d08cc5ffd300f1cab70e0a613a997959f39659c
etoledom <etoledom@icloud.com>
2019-06-10 18:57:30 +0200

    [RNMobile] iOS Image block caption multiline fix (#16071)
    * Add unit test to cover multiline rich-text onEnter appends br tag
    
    * Fix iOS issue where pressing Enter on image caption won't create a new line

--- packages/block-editor/src/components/rich-text/test/index.native.js
+++ packages/block-editor/src/components/rich-text/test/index.native.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+
+/**
  * Internal dependencies
  */
 import { RichText } from '../index';
@@ -23,5 +28,29 @@ describe( 'RichText Native', () => {
 			const html = '<p><b>Hello</b> <strong>Hello</strong> WorldWorld!</p>';
 			expect( richText.willTrimSpaces( html ) ).toBe( false );
 		} );
+	} );
+
+	describe( 'Adds new line on Enter', () => {
+		let newValue;
+		const wrapper = shallow( <RichText 
+			rootTagsToEliminate={ [ 'p' ] }
+			value={ "" }
+			onChange={ ( value ) => {
+				newValue = value
+			} }
+			formatTypes={ [] }
+			onSelectionChange={ jest.fn() }
+		/> );
+
+		const event = {
+			nativeEvent: {
+				eventCount: 0,
+			}
+		};
+		wrapper.instance().onEnter(event);
+
+		it( ' Adds <br> tag to content after pressing Enter key', () => {
+			expect( newValue ).toEqual( "<br>" );
+		});
 	} );
 } );
