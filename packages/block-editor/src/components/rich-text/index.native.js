3188fe05c3e70fe7f07c6400b68e83d97167b297
etoledom <etoledom@icloud.com>
2019-06-10 18:57:30 +0200

    [RNMobile] iOS Image block caption multiline fix (#16071)
    * Add unit test to cover multiline rich-text onEnter appends br tag
    
    * Fix iOS issue where pressing Enter on image caption won't create a new line

--- packages/block-editor/src/components/rich-text/index.native.js
+++ packages/block-editor/src/components/rich-text/index.native.js
@@ -365,7 +365,7 @@ export class RichText extends Component {
 				const insertedLineSeparator = { ...insertLineSeparator( currentRecord ) };
 				this.onFormatChange( insertedLineSeparator );
 			}
-		} else if ( event.shiftKey || ! this.onSplit ) {
+		} else if ( event.shiftKey || ! onSplit ) {
 			this.needsSelectionUpdate = true;
 			const insertedLineBreak = { ...insert( currentRecord, '\n' ) };
 			this.onFormatChange( insertedLineBreak );
