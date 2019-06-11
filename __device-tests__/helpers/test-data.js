4b137838bd5d4f5ffc37d886063f6d4e4ed0520d
Javon <javondavis@users.noreply.github.com>
2019-06-11 13:06:17 -0500

    fix long text

--- __device-tests__/helpers/test-data.js
+++ __device-tests__/helpers/test-data.js
@@ -2,7 +2,7 @@ exports.shortText = `Rock music approaches at high velocity.`;
 
 exports.mediumText = `The finer continuum interprets the polynomial rabbit. When can the geology cheat? An astronomer runs. Should a communist consent?`;
 
-exports.longText = `Beneath the busy continuum blinks the ineffective husband. Why can't a metric bow outside the official subway? How can the prompt crop exhaust his tree? The sample rolls! After an across gasp overflows the ethical attorney. A riot pilots an excess goldfish. 
+exports.longText = `Beneath the busy continuum blinks the ineffective husband. Why a metric bow outside the official subway? How can the prompt crop exhaust his tree? The sample rolls! After an across gasp overflows the ethical attorney. A riot pilots an excess goldfish. 
 Does this chord crowd my emptied search? A theory bubbles under the cartoon. The discontinued speaker cracks every thick epic. Its extraordinary twin shifts behind the listener. An enlightened specimen stalls. The weapon apologizes?
 The finer continuum interprets the polynomial rabbit. When can the geology cheat? An astronomer runs. Should a communist consent?
 A parody guns the suffering pipeline. How does the sliced device amuse the tutorial? When will the beating companion dispose our average? The skull complains? An ink irons a paranoid into the earned juvenile.`;
