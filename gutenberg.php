61d4d51958997244d01ada6d10d90825cc194ffb
Darren Ethier <darren@roughsmootheng.in>
2019-06-12 08:40:59 -0400

    Bump plugin version to 5.9.0

--- gutenberg.php
+++ gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gutenberg
  * Plugin URI: https://github.com/WordPress/gutenberg
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
- * Version: 5.9.0-rc.1
+ * Version: 5.9.0
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
