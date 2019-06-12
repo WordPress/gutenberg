9045fe4b5bfebd511b2fef4f4a312f8fb25ec67a
Sören Wrede <soerenwrede@gmail.com>
2019-06-12 09:37:30 +0200

    Add translator comment

--- packages/block-library/src/tag-cloud/index.php
+++ packages/block-library/src/tag-cloud/index.php
@@ -31,7 +31,13 @@ function render_block_core_tag_cloud( $attributes ) {
 
 	if ( ! $tag_cloud ) {
 		$labels    = get_taxonomy_labels( get_taxonomy( $attributes['taxonomy'] ) );
-		$tag_cloud = esc_html( sprintf( __( 'Your site doesn&#8217;t have any %s, so there&#8217;s nothing to display here at the moment.' ), strtolower( $labels->name ) ) );
+		$tag_cloud = esc_html(
+			sprintf(
+				/* translators: %s: taxonomy name */
+				__( 'Your site doesn&#8217;t have any %s, so there&#8217;s nothing to display here at the moment.' ),
+				strtolower( $labels->name )
+			)
+		);
 	}
 
 	return sprintf(
