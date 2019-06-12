09db379b3d8f15989f64f89c297857f245500f27
Darren Ethier <darren@roughsmootheng.in>
2019-06-11 22:09:17 -0400

    doh! Add back in initial trigger of nux guides!

--- packages/edit-post/src/components/editor-initialization/index.js
+++ packages/edit-post/src/components/editor-initialization/index.js
@@ -8,8 +8,14 @@ import {
 } from './listener-hooks';
 
 /**
+ * WordPress dependencies
+ */
+import { useEffect } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
+
+/**
  * Data component used for initializing the editor and re-initializes
- * when postId changes or unmount.
+ * when postId changes or on unmount.
  *
  * @param {number} postId  The id of the post.
  * @return {null} This is a data component so does not render any ui.
@@ -18,5 +24,14 @@ export default function( { postId } ) {
 	useAdjustSidebarListener( postId );
 	useBlockSelectionListener( postId );
 	useUpdatePostLinkListener( postId );
+	const { triggerGuide } = useDispatch( 'core/nux' );
+	useEffect( () => {
+		triggerGuide( [
+			'core/editor.inserter',
+			'core/editor.settings',
+			'core/editor.preview',
+			'core/editor.publish',
+		] );
+	}, [ triggerGuide ] );
 	return null;
 }
