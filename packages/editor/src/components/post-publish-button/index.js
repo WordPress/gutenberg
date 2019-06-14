63b820b0032d218a9575b6ec36d810d4e3f6af2d
Mel Choyce <melchoyce@users.noreply.github.com>
2019-06-13 17:30:33 -0700

    Revert "Remove fourth tip"
    This reverts commit c227ab5236c988b01b0eef6125d23e7dca8a408d.

--- packages/editor/src/components/post-publish-button/index.js
+++ packages/editor/src/components/post-publish-button/index.js
@@ -110,6 +110,17 @@ export class PostPublishButton extends Component {
 
 		const componentProps = isToggle ? toggleProps : buttonProps;
 		const componentChildren = isToggle ? toggleChildren : buttonChildren;
+		return (
+			<Button
+				ref={ this.buttonNode }
+				{ ...componentProps }
+			>
+				{ componentChildren }
+				<DotTip tipId="core/editor.publish">
+					{ __( 'Finished writing? That’s great, let’s get this published right now. Just click “Publish” and you’re good to go.' ) }
+				</DotTip>
+			</Button>
+		);
 	}
 }
 
