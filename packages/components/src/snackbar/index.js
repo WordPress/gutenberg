2f63a6ee02c4302310cfe06b4fe3969397406f77
Seghir Nadir <nadir.seghir@gmail.com>
2019-06-14 15:20:55 +0100

    refactor snackbar to follow spec while keeping compatibility with notice (#16095)

--- packages/components/src/snackbar/index.js
+++ packages/components/src/snackbar/index.js
@@ -34,6 +34,13 @@ function Snackbar( {
 	}, [] );
 
 	const classes = classnames( className, 'components-snackbar' );
+	if ( actions && actions.length > 1 ) {
+		// we need to inform developers that snackbar only accepts 1 action
+		// eslint-disable-next-line no-console
+		console.warn( 'Snackbar can only have 1 action, use Notice if your message require many messages' );
+		// return first element only while keeping it inside an array
+		actions = [ actions[ 0 ] ];
+	}
 
 	return (
 		<div
@@ -50,7 +57,6 @@ function Snackbar( {
 				{ actions.map(
 					(
 						{
-							className: buttonCustomClasses,
 							label,
 							onClick,
 							url,
@@ -68,10 +74,8 @@ function Snackbar( {
 										onClick( event );
 									}
 								} }
-								className={ classnames(
-									'components-snackbar__action',
-									buttonCustomClasses
-								) }
+								className="components-snackbar__action"
+
 							>
 								{ label }
 							</Button>
