554fae33d02d30e12066bdb9c7f995004331bdb2
etoledom <etoledom@icloud.com>
2019-06-14 09:43:15 +0200

    Refactor PlainText to use `RCTAztecView`

--- packages/block-editor/src/components/plain-text/index.native.js
+++ packages/block-editor/src/components/plain-text/index.native.js
@@ -2,11 +2,15 @@
  * External dependencies
  */
 import { TextInput, Platform } from 'react-native';
+import RCTAztecView from 'react-native-aztec';
 
 /**
  * WordPress dependencies
  */
 import { Component } from '@wordpress/element';
+import { withInstanceId, compose } from '@wordpress/compose';
+import { withSelect, withDispatch } from '@wordpress/data';
+import { withBlockEditContext } from '../block-edit/context';
 
 /**
  * Internal dependencies
@@ -16,19 +20,36 @@ import styles from './style.scss';
 export default class PlainText extends Component {
 	constructor() {
 		super( ...arguments );
-		this.isIOS = Platform.OS === 'ios';
+
+		this.onContentSizeChange = this.onContentSizeChange.bind( this );
+		this.onChange = this.onChange.bind( this );
+		this.onSelectionChangeFromAztec = this.onSelectionChangeFromAztec.bind( this );
+
+		this.selection = {
+			start: 0,
+			end:0,
+		}
+
+		this.state = {
+			height: 0,
+		};
 	}
 
 	componentDidMount() {
-		// if isSelected is true, we should request the focus on this TextInput
-		if ( ( this._input.isFocused() === false ) && ( this._input.props.isSelected === true ) ) {
+		if ( ( this._input.isFocused() === false ) && ( this.props.isSelected === true ) ) {
 			this.focus();
 		}
 	}
 
 	componentDidUpdate( prevProps ) {
-		if ( ! this.props.isSelected && prevProps.isSelected && this.isIOS ) {
-			this._input.blur();
+		if ( this.props.isSelected === prevProps.isSelected ) {
+			return;
+		}
+
+		if ( this.props.isSelected ) {
+			this.focus();
+		} else {
+			this.blur();
 		}
 	}
 
@@ -36,21 +57,70 @@ export default class PlainText extends Component {
 		this._input.focus();
 	}
 
+	blur() {
+		this._input.blur();
+	}
+
+	onChange( event ) {
+		const value = event.nativeEvent.plainText;
+		this.props.onChange( value );
+	}
+
+	onPaste( event ) {
+
+	}
+
+	/*
+	* Handles any case where the content of the AztecRN instance has changed in size
+	*/
+	onContentSizeChange( contentSize ) {
+		const height = contentSize.height;
+		this.setState( { height } );
+	}
+
+	onSelectionChangeFromAztec( start, end ) {
+		const realStart = Math.min( start, end );
+		const realEnd = Math.max( start, end );
+
+		if ( this.start !== realStart || this.end !== realEnd ) {
+			this.setSelection( realStart, realEnd );
+		}
+	}
+
+	setSelection( start, end ) {
+		this.selection = {
+			start, 
+			end,
+		}
+	}
+
 	render() {
+		let minHeight = styles[ 'block-editor-plain-text' ].minHeight;
+
 		return (
-			<TextInput
+			<RCTAztecView
 				{ ...this.props }
-				ref={ ( x ) => this._input = x }
-				className={ [ styles[ 'block-editor-plain-text' ], this.props.className ] }
-				onChange={ ( event ) => {
-					this.props.onChange( event.nativeEvent.text );
+				ref={ ( ref ) => {
+					this._input = ref;
 				} }
-				onFocus={ this.props.onFocus } // always assign onFocus as a props
-				onBlur={ this.props.onBlur } // always assign onBlur as a props
+				className={ [ styles[ 'block-editor-plain-text' ], this.props.className ] }
+				onFocus={ this.props.onFocus }
+				onBlur={ this.props.onBlur }
 				fontFamily={ this.props.fontFamily || ( styles[ 'block-editor-plain-text' ].fontFamily ) }
-				fontSize={ this.props.fontSize }
-				fontWeight={ this.props.fontWeight }
-				fontStyle={ this.props.fontStyle }
+
+				style={ {
+					minHeight: this.state.height,
+				} }
+				plainText={ {
+					text: this.props.value,
+					selection: this.selection,
+				} }
+				onChange={ this.onChange }
+				onPaste={ this.onPaste }
+				onContentSizeChange={ this.onContentSizeChange }
+				onSelectionChange={ this.onSelectionChangeFromAztec }
+				maxImagesWidth={ 200 }
+				isMultiline={ true }
 			/>
 		);
 	}
