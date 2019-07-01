472066a2c6200da0a06918465876c74c67a3a8a1
Darren Ethier <darren@roughsmootheng.in>
2019-07-01 11:55:11 -0400

    update exports

--- packages/element/src/index.js
+++ packages/element/src/index.js
@@ -1,3 +1,4 @@
+export * from './components';
 export * from './react';
 export * from './react-platform';
 export * from './utils';
--- packages/element/src/utils.js
+++ packages/element/src/utils.js
@@ -24,3 +24,4 @@ export const isEmptyElement = ( element ) => {
 
 	return ! element;
 };
+
