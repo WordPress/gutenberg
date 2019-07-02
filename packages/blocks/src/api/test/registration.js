2c314ae6f84d3148b57ba55348b3e9dc0e9c9b95
Daniel Richards <daniel.richards@automattic.com>
2019-07-02 12:11:44 +0100

    Add test for applying registerBlockType filter to deprecated settings

--- packages/blocks/src/api/test/registration.js
+++ packages/blocks/src/api/test/registration.js
@@ -337,6 +337,42 @@ describe( 'blocks', () => {
 				expect( console ).toHaveErroredWith( 'Block settings must be a valid object.' );
 				expect( block ).toBeUndefined();
 			} );
+
+			it( 'should apply the blocks.registerBlockType filter to each of the deprecated settings as well as the main block settings', () => {
+				const blockSettingsWithDeprecations = {
+					...defaultBlockSettings,
+					deprecated: [
+						{
+							save() {
+								return 1;
+							},
+						},
+						{
+							save() {
+								return 2;
+							},
+						},
+					],
+				};
+
+				addFilter( 'blocks.registerBlockType', 'core/blocks/without-title', ( settings ) => {
+					return {
+						...settings,
+						attributes: {
+							...settings.attributes,
+							id: {
+								type: 'string',
+							},
+						},
+					};
+				} );
+
+				const block = registerBlockType( 'my-plugin/fancy-block-13', blockSettingsWithDeprecations );
+
+				expect( block.attributes.id ).toEqual( { type: 'string' } );
+				expect( block.deprecated[ 0 ].attributes.id ).toEqual( { type: 'string' } );
+				expect( block.deprecated[ 1 ].attributes.id ).toEqual( { type: 'string' } );
+			} );
 		} );
 	} );
 
