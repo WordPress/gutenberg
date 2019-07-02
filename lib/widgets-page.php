b3211fb8a89c7813469857fffc23f51eaed5f403
epiqueras <epiquerass@gmail.com>
2019-07-02 10:00:35 -0400

    Customizer: Add server registered blocks back in. The getter function is now defined?

--- lib/widgets-page.php
+++ lib/widgets-page.php
@@ -81,14 +81,11 @@ function gutenberg_widgets_init( $hook ) {
 		)
 	);
 
-	// TODO: Figure out the best way for `get_block_editor_server_block_settings` to be available in the Customizer.
-	if ( 'gutenberg_customizer' !== $hook ) { // `get_block_editor_server_block_settings` is undefined in the Customizer.
-		// Preload server-registered block schemas.
-		wp_add_inline_script(
-			'wp-blocks',
-			'wp.blocks.unstable__bootstrapServerSideBlockDefinitions(' . wp_json_encode( get_block_editor_server_block_settings() ) . ');'
-		);
-	}
+	// Preload server-registered block schemas.
+	wp_add_inline_script(
+		'wp-blocks',
+		'wp.blocks.unstable__bootstrapServerSideBlockDefinitions(' . wp_json_encode( get_block_editor_server_block_settings() ) . ');'
+	);
 
 	wp_enqueue_script( 'wp-edit-widgets' );
 	wp_enqueue_script( 'wp-format-library' );
