f1a7d9e6450b81b71b31ef480148837fd107c5ad
epiqueras <epiquerass@gmail.com>
2019-07-04 16:42:25 -0400

    Block Library: Add new post-content block and use it in the default post template.

--- lib/templates.php
+++ lib/templates.php
@@ -12,6 +12,7 @@ function gutenberg_register_templates() {
 	$post_post_type_object                = get_post_type_object( 'post' );
 	$post_post_type_object->template      = array(
 		array( 'core/post-title' ),
+		array( 'core/post-content', array(), array( array( 'core/paragraph' ) ) ),
 	);
 	$post_post_type_object->template_lock = 'insert';
 }
