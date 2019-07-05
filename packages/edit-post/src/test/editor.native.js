68c49d190093ac2e52c61c38f2495e93a72a9a1f
Danilo Ercoli <ercoli@gmail.com>
2019-07-05 17:21:28 +0200

    Update test again

--- packages/edit-post/src/test/editor.native.js
+++ packages/edit-post/src/test/editor.native.js
@@ -31,7 +31,7 @@ describe( 'Editor', () => {
 		appContainer.unmount();
 
 		expect( RNReactNativeGutenbergBridge.editorDidMount ).toHaveBeenCalledTimes( 1 );
-		expect( RNReactNativeGutenbergBridge.editorDidMount ).toHaveBeenCalledWith( [ 'wp:notablock' ] );
+		expect( RNReactNativeGutenbergBridge.editorDidMount ).toHaveBeenCalledWith( [ 'core/notablock' ] );
 	} );
 } );
 
