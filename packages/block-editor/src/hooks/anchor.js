3bafcf87dc650f97c0d3c17abc160f37220c554d
Robert Anderson <robert@noisysocks.com>
2019-07-08 10:17:49 +1000

    No need to localise link URLs

--- packages/block-editor/src/hooks/anchor.js
+++ packages/block-editor/src/hooks/anchor.js
@@ -76,7 +76,7 @@ export const withInspectorControl = createHigherOrderComponent( ( BlockEdit ) =>
 								<>
 									{ __( 'Enter a word or two — without spaces — to make a unique web address just for this heading, called an “anchor.” Then, you’ll be able to link directly to this section of your page.' ) }
 
-									<ExternalLink href={ __( 'https://wordpress.org/support/article/page-jumps/' ) }>
+									<ExternalLink href={ 'https://wordpress.org/support/article/page-jumps/' }>
 										{ __( 'Learn more about anchors' ) }
 									</ExternalLink>
 								</>
