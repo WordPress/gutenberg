d318a829e39aff8f0d03bb11aad737ac2f347801
Andrew Duthie <andrew@andrewduthie.com>
2019-07-09 08:28:13 -0400

    Editor: Resolve awaitNextStateChange JSDoc typo
    Co-Authored-By: Enrique Piqueras <epiqueras@users.noreply.github.com>

--- packages/editor/src/store/controls.js
+++ packages/editor/src/store/controls.js
@@ -4,7 +4,7 @@
 import { createRegistryControl } from '@wordpress/data';
 
 /**
- * Returns an control descriptor signalling to subscribe to the registry and
+ * Returns a control descriptor signalling to subscribe to the registry and
  * resolve the control promise only when the next state change occurs.
  *
  * @return {Object} Control descriptor.
