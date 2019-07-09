fdbc8fd67ad099a9d005c9f57375e7f46e715136
Jeff Ong <jeff.ong@automattic.com>
2019-07-09 15:55:30 -0400

    [UI Components] resolve missing styles from checkbox-control (#11089)

--- packages/components/src/checkbox-control/index.js
+++ packages/components/src/checkbox-control/index.js
@@ -7,10 +7,24 @@ import { withInstanceId } from '@wordpress/compose';
  * Internal dependencies
  */
 import BaseControl from '../base-control';
+import { G, Path, SVG } from '../primitives';
 
 function CheckboxControl( { label, className, heading, checked, help, instanceId, onChange, ...props } ) {
 	const id = `inspector-checkbox-control-${ instanceId }`;
 	const onChangeValue = ( event ) => onChange( event.target.checked );
+
+	const svgCheck = (
+		<SVG
+			viewBox="0 0 20 20"
+			xmlns="http://www.w3.org/2000/svg"
+			className="components-checkbox-control__checked"
+			role="presentation"
+		>
+			<G>
+				<Path d="M14.83 4.89l1.34.94-5.81 8.38H9.02L5.78 9.67l1.34-1.25 2.57 2.4z" />
+			</G>
+		</SVG>
+	);
 
 	return (
 		<BaseControl label={ heading } id={ id } help={ help } className={ className }>
@@ -25,6 +39,7 @@ function CheckboxControl( { label, className, heading, checked, help, instanceId
 				{ ...props }
 			/>
 			<label className="components-checkbox-control__label" htmlFor={ id }>
+				{ checked ? svgCheck : null }
 				{ label }
 			</label>
 		</BaseControl>
