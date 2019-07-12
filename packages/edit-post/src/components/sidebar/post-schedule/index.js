908901f45f1c5f9cf50cf797b73234140e1ac3a1
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2019-07-12 17:35:42 +0200

    Proof of concept to add an unschedule button to accompany the date picker

--- packages/edit-post/src/components/sidebar/post-schedule/index.js
+++ packages/edit-post/src/components/sidebar/post-schedule/index.js
@@ -4,35 +4,80 @@
 import { __ } from '@wordpress/i18n';
 import { PanelRow, Dropdown, Button } from '@wordpress/components';
 import { PostSchedule as PostScheduleForm, PostScheduleLabel, PostScheduleCheck } from '@wordpress/editor';
+import { withSelect, withDispatch } from '@wordpress/data';
 
-export function PostSchedule() {
-	return (
-		<PostScheduleCheck>
-			<PanelRow className="edit-post-post-schedule">
-				<span>
-					{ __( 'Publish' ) }
-				</span>
-				<Dropdown
-					position="bottom left"
-					contentClassName="edit-post-post-schedule__dialog"
-					renderToggle={ ( { onToggle, isOpen } ) => (
-						<>
-							<Button
-								type="button"
-								className="edit-post-post-schedule__toggle"
-								onClick={ onToggle }
-								aria-expanded={ isOpen }
-								isLink
-							>
-								<PostScheduleLabel />
-							</Button>
-						</>
-					) }
-					renderContent={ () => <PostScheduleForm /> }
-				/>
-			</PanelRow>
-		</PostScheduleCheck>
-	);
+import { Component } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
+
+class PostSchedule extends Component {
+	constructor( props ) {
+		super( props );
+		this.unscheduleAction = this.unscheduleAction.bind( this );
+	}
+
+	unscheduleAction() {
+		this.props.editDate( this.props.modified );
+	}
+
+	render() {
+		return (
+			<PostScheduleCheck>
+				<PanelRow className="edit-post-post-schedule">
+					<span>
+						{ __( 'Publish' ) }
+					</span>
+					<Dropdown
+						position="bottom left"
+						contentClassName="edit-post-post-schedule__dialog"
+						renderToggle={ ( { onToggle, isOpen } ) => (
+							<>
+								<Button
+									type="button"
+									className="edit-post-post-schedule__toggle"
+									onClick={ onToggle }
+									aria-expanded={ isOpen }
+									isLink
+								>
+									<PostScheduleLabel />
+								</Button>
+							</>
+						) }
+						renderContent={ () => <PostScheduleForm /> }
+					/>
+				</PanelRow>
+				{ this.props.isScheduled === true &&
+					<PanelRow className="edit-post-post-unschedule">
+						<Button
+							isSmall
+							isDefault
+							onClick={ this.unscheduleAction }
+						>
+							{ __( 'Unschedule' ) }
+						</Button>
+					</PanelRow>
+				}
+			</PostScheduleCheck>
+		);
+	}
 }
+
+PostSchedule = compose( [
+	withSelect( ( select ) => {
+		return {
+			status: select( 'core/editor' ).getEditedPostAttribute( 'status' ),
+			date: select( 'core/editor' ).getEditedPostAttribute( 'date' ),
+			modified: select( 'core/editor' ).getEditedPostAttribute( 'modified' ),
+			isScheduled: select( 'core/editor' ).isEditedPostBeingScheduled(),
+		};
+	} ),
+	withDispatch( ( dispatch ) => {
+		const { editPost } = dispatch( 'core/editor' );
+		return {
+			editDate( date ) {
+				editPost( { date } );
+			},
+		};
+	} ),
+] )( PostSchedule );
 
 export default PostSchedule;
--- packages/editor/src/components/post-schedule/label.js
+++ packages/editor/src/components/post-schedule/label.js
@@ -5,17 +5,23 @@ import { __ } from '@wordpress/i18n';
 import { dateI18n, __experimentalGetSettings } from '@wordpress/date';
 import { withSelect } from '@wordpress/data';
 
-export function PostScheduleLabel( { date, isFloating } ) {
-	const settings = __experimentalGetSettings();
+import { Component } from '@wordpress/element';
 
-	return date && ! isFloating ?
-		dateI18n( settings.formats.datetimeAbbreviated, date ) :
-		__( 'Immediately' );
+class PostScheduleLabel extends Component {
+	render() {
+		const settings = __experimentalGetSettings();
+
+		if ( this.props.date && ! this.props.isFloating ) {
+			return dateI18n( settings.formats.datetimeAbbreviated, this.props.date );
+		}
+		return __( 'Immediately' );
+	}
 }
 
 export default withSelect( ( select ) => {
 	return {
 		date: select( 'core/editor' ).getEditedPostAttribute( 'date' ),
 		isFloating: select( 'core/editor' ).isEditedPostDateFloating(),
+		isScheduled: select( 'core/editor' ).isEditedPostBeingScheduled(),
 	};
 } )( PostScheduleLabel );
