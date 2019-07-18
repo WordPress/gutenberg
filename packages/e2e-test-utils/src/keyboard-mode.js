c9469e6cf2e8d631b3b185851d1b90f36ced3266
Riad Benguella <benguella@gmail.com>
2019-07-18 12:22:58 +0200

    Fix the edit mode trigger in e2e tests

--- packages/e2e-test-utils/src/keyboard-mode.js
+++ packages/e2e-test-utils/src/keyboard-mode.js
@@ -4,6 +4,5 @@
  * @return {Promise} Promise resolving after enabling the keyboard edit mode.
  */
 export async function switchToEditMode() {
-	await page.mouse.move( 0, 0 );
-	await page.mouse.move( 10, 10 );
+	await page.click( '.edit-post-layout__content' );
 }
