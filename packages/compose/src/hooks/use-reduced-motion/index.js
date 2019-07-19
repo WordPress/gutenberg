7380adbef857d04f38212719e7f1a2f4d6819c3d
Jon Surrell <jon.surrell@automattic.com>
2019-07-19 13:34:50 +0200

    Guard process.env.FORCE_REDUCED_MOTION usage

--- packages/compose/src/hooks/use-reduced-motion/index.js
+++ packages/compose/src/hooks/use-reduced-motion/index.js
@@ -11,12 +11,22 @@ import useMediaQuery from '../use-media-query';
 const IS_IE = window.navigator.userAgent.indexOf( 'Trident' ) >= 0;
 
 /**
+ * Force reduced motion behavior at build time
+ *
+ * @type {boolean}
+ */
+const FORCE_REDUCED_MOTION =
+	typeof process === 'object' &&
+	typeof process.env === 'object' &&
+	!! process.env.FORCE_REDUCED_MOTION;
+
+/**
  * Hook returning whether the user has a preference for reduced motion.
  *
  * @return {boolean} Reduced motion preference value.
  */
 const useReducedMotion =
-	process.env.FORCE_REDUCED_MOTION || IS_IE ?
+	FORCE_REDUCED_MOTION || IS_IE ?
 		() => true :
 		() => useMediaQuery( '(prefers-reduced-motion: reduce)' );
 
