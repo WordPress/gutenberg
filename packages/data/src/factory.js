d87bef8fae150cc5b112455e77dd408cca22f390
Andrew Duthie <andrew@andrewduthie.com>
2019-07-20 11:54:38 +0200

    Data: Create registry selector with self-contained registry proxying

--- packages/data/src/factory.js
+++ packages/data/src/factory.js
@@ -1,4 +1,9 @@
 /**
+ * Internal dependencies
+ */
+import defaultRegistry from './default-registry';
+
+/**
  * Mark a selector as a registry selector.
  *
  * @param {function} registrySelector Function receiving a registry object and returning a state selector.
@@ -6,9 +11,25 @@
  * @return {function} marked registry selector.
  */
 export function createRegistrySelector( registrySelector ) {
-	registrySelector.isRegistrySelector = true;
+	const selector = ( ...args ) => registrySelector( selector.registry.select )( ...args );
 
-	return registrySelector;
+	/**
+	 * Flag indicating to selector registration mapping that the selector should
+	 * be mapped as a registry selector.
+	 *
+	 * @type {boolean}
+	 */
+	selector.isRegistrySelector = true;
+
+	/**
+	 * Registry on which to call `select`, stubbed for non-standard usage to
+	 * use the default registry.
+	 *
+	 * @type {WPDataRegistry}
+	 */
+	selector.registry = defaultRegistry;
+
+	return selector;
 }
 
 /**
