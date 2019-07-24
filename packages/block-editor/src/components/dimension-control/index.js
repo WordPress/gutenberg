e8ea617b0c2c850df7c36aa09ad54db83297b609
Dave Smith <getdavemail@gmail.com>
2019-07-24 12:31:48 +0100

    Improve i18n of strings.
    Addresses https://github.com/WordPress/gutenberg/pull/16730#discussion_r306758167
    
    Renames `type` to `property` to better indicate that it is a formal CSS property value and therefore should not be translated lest it break the underlying code.
    
    Added title attribute to enable true text-based strings to be translatable.

--- packages/block-editor/src/components/dimension-control/index.js
+++ packages/block-editor/src/components/dimension-control/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { startCase } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import {
@@ -12,15 +7,14 @@ import {
 	BaseControl,
 	Tooltip,
 } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import sizesTable from './sizes';
 
-function DimensionControl( { type, attributes, setAttributes, clientId } ) {
-	const humanTypeName = startCase( type );
-
+function DimensionControl( { title, property, attributes, setAttributes, clientId } ) {
 	const onChangeSpacingSize = ( event ) => {
 		const theSize = sizesTable.find( ( size ) => event.target.value === size.slug );
 
@@ -28,36 +22,36 @@ function DimensionControl( { type, attributes, setAttributes, clientId } ) {
 			return;
 		}
 
-		if ( attributes[ `${ type }Size` ] === theSize.slug ) {
+		if ( attributes[ `${ property }Size` ] === theSize.slug ) {
 			setAttributes( {
-				[ `${ type }Size` ]: '',
-				[ `${ type }Top` ]: 0,
-				[ `${ type }Right` ]: 0,
-				[ `${ type }Left` ]: 0,
-				[ `${ type }Bottom` ]: 0,
+				[ `${ property }Size` ]: '',
+				[ `${ property }Top` ]: 0,
+				[ `${ property }Right` ]: 0,
+				[ `${ property }Left` ]: 0,
+				[ `${ property }Bottom` ]: 0,
 			} );
 		} else {
 			setAttributes( {
-				[ `${ type }Size` ]: theSize.slug,
-				[ `${ type }Top` ]: theSize.size,
-				[ `${ type }Right` ]: theSize.size,
-				[ `${ type }Left` ]: theSize.size,
-				[ `${ type }Bottom` ]: theSize.size,
+				[ `${ property }Size` ]: theSize.slug,
+				[ `${ property }Top` ]: theSize.size,
+				[ `${ property }Right` ]: theSize.size,
+				[ `${ property }Left` ]: theSize.size,
+				[ `${ property }Bottom` ]: theSize.size,
 			} );
 		}
 	};
 
 	// Todo - update with unique Block instance ID?
-	const controlId = `block-spacing-${ type }-${ clientId }`;
+	const controlId = `block-spacing-${ property }-${ clientId }`;
 
 	return (
 		<BaseControl
 			id={ controlId }
-			help={ `Select the ${ type } for this Block` }
+			help={ sprintf( __( 'Select the %s for this Block' ), property ) }
 			className="block-editor-dimension-control"
 		>
 			<BaseControl.VisualLabel>
-				{ humanTypeName }
+				{ title }
 			</BaseControl.VisualLabel>
 			<ButtonGroup
 				id={ controlId }
@@ -66,7 +60,7 @@ function DimensionControl( { type, attributes, setAttributes, clientId } ) {
 				{ sizesTable.map( function( size ) {
 					const visualName = size.name.substring( 0, 1 );
 					const hiddenName = size.name.substring( 1 );
-					const isSelected = attributes[ `${ type }Size` ] === size.slug;
+					const isSelected = attributes[ `${ property }Size` ] === size.slug;
 					return (
 						<Tooltip key={ size.slug } text={ size.name }>
 							<Button
