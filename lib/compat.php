f5748c0c7b9e33c5a4774b03f6eefe44c11c1371
Andrew Duthie <andrew@andrewduthie.com>
2019-07-25 15:22:11 -0400

    Compat: Force updated post status from auto-draft to draft

--- lib/compat.php
+++ lib/compat.php
@@ -29,3 +29,23 @@ function gutenberg_safe_style_css_column_flex_basis( $attr ) {
 	return $attr;
 }
 add_filter( 'safe_style_css', 'gutenberg_safe_style_css_column_flex_basis' );
+
+/**
+ * Filters inserted post data to update an existing post's status to draft if
+ * currently an auto-draft. This is intended to ensure that a post which is
+ * explicitly updated should no longer be subjected to auto-draft purge.
+ *
+ * @param array $data    An array of slashed post data.
+ * @param array $postarr An array of sanitized, but otherwise unmodified post
+ *                       data.
+ *
+ * @return array Filtered post data.
+ */
+function gutenberg_update_post_autodraft_to_draft( $data, $postarr ) {
+	if ( ! empty( $postarr['ID'] ) && $postarr['post_status'] === 'auto-draft' ) {
+		$data['post_status'] = 'draft';
+	}
+
+	return $data;
+}
+add_filter( 'wp_insert_post_data', 'gutenberg_update_post_autodraft_to_draft', 10, 2 );
