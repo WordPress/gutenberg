ce2d57a18fe7381fc43e75161b75d279aea22bcd
Seghir Nadir <nadir.seghir@gmail.com>
2019-07-27 22:05:58 +0100

    add color to frontend render

--- packages/block-library/src/separator/save.js
+++ packages/block-library/src/separator/save.js
@@ -1,3 +1,33 @@
-export default function save() {
-	return <hr />;
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	getColorClassName,
+} from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const {
+		backgroundColor,
+		customBackgroundColor,
+	} = attributes;
+
+	const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+	const separatorClasses = classnames( {
+		'has-background': backgroundColor || customBackgroundColor,
+		[ backgroundClass ]: backgroundClass,
+	} );
+
+	const separatorStyle = {
+		backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+	};
+	return <hr
+		className={ separatorClasses }
+		style={ separatorStyle }
+	/>;
 }
