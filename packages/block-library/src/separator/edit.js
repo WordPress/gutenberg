0b4c44d9b024b7ecde3eec9fa3c565d1f70878a7
Seghir Nadir <nadir.seghir@gmail.com>
2019-07-27 17:14:28 +0100

    add color support to separator block

--- packages/block-library/src/separator/edit.js
+++ packages/block-library/src/separator/edit.js
@@ -1,8 +1,72 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { HorizontalRule } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
-export default function SeparatorEdit( { className } ) {
-	return <HorizontalRule className={ className } />;
+import {
+	Component,
+} from '@wordpress/element';
+
+import {
+	compose,
+	withInstanceId,
+} from '@wordpress/compose';
+
+import {
+	HorizontalRule,
+} from '@wordpress/components';
+
+import {
+	InspectorControls,
+	withColors,
+	PanelColorSettings,
+} from '@wordpress/block-editor';
+
+class SeparatorEdit extends Component {
+	render() {
+		const {
+			backgroundColor,
+			setBackgroundColor,
+			className,
+		} = this.props;
+		return (
+			<div>
+				<HorizontalRule
+					className={ classnames(
+						className, {
+							'has-background': backgroundColor.color,
+							[ backgroundColor.class ]: backgroundColor.class,
+						}
+					) }
+					style={ {
+						borderColor: backgroundColor.color,
+						color: backgroundColor.color,
+					} }
+				/>
+				<InspectorControls>
+					<PanelColorSettings
+						title={ __( 'Color Settings' ) }
+						colorSettings={ [
+							{
+								value: backgroundColor.color,
+								onChange: setBackgroundColor,
+								label: __( 'Background Color' ),
+							},
+						] }
+					>
+					</PanelColorSettings>
+				</InspectorControls>
+			</div>
+		);
+	}
 }
+
+export default compose( [
+	withInstanceId,
+	withColors( 'backgroundColor', { textColor: 'color' } ),
+] )( SeparatorEdit );
