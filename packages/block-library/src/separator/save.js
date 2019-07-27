3cba3fdd04adf1478697c1e5ee820978ab24d967
Seghir Nadir <nadir.seghir@gmail.com>
2019-07-27 23:05:00 +0100

    add support for dots style in frontend

--- packages/block-library/src/separator/save.js
+++ packages/block-library/src/separator/save.js
@@ -17,14 +17,17 @@ export default function save( { attributes } ) {
 	} = attributes;
 
 	const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+	const textClass = getColorClassName( 'color', backgroundColor );
 
 	const separatorClasses = classnames( {
-		'has-background': backgroundColor || customBackgroundColor,
+		'has-text-color has-background': backgroundColor || customBackgroundColor,
 		[ backgroundClass ]: backgroundClass,
+		[ textClass ]: textClass,
 	} );
 
 	const separatorStyle = {
 		backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+		color: textClass ? undefined : customBackgroundColor,
 	};
 	return <hr
 		className={ separatorClasses }
