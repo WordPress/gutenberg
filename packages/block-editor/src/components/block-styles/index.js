88701f74bffcbfc45eb5774b1d50e3f193daeffb
Marek Hrabe <marekhrabe@me.com>
2019-07-29 14:55:42 -0700

    use new component in style thumbnail

--- packages/block-editor/src/components/block-styles/index.js
+++ packages/block-editor/src/components/block-styles/index.js
@@ -17,7 +17,7 @@ import { getBlockType, cloneBlock } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import { BlockPreviewContent } from '../block-preview';
+import { UnifiedBlockPreview } from '../block-preview';
 
 /**
  * Returns the active style from the given className.
@@ -122,7 +122,7 @@ function BlockStyles( {
 						aria-label={ style.label || style.name }
 					>
 						<div className="editor-block-styles__item-preview block-editor-block-styles__item-preview">
-							<BlockPreviewContent
+							<UnifiedBlockPreview
 								blocks={ cloneBlock( block, {
 									className: styleClassName,
 								} ) }
