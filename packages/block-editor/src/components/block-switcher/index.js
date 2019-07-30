41b0a991224bf4f48025c652e3f88a77251d1b0c
Dave Smith <getdavemail@gmail.com>
2019-07-30 12:05:29 +0100

    Simplify modifying passed block attrs via cloneBlock

--- packages/block-editor/src/components/block-switcher/index.js
+++ packages/block-editor/src/components/block-switcher/index.js
@@ -8,7 +8,7 @@ import { castArray, filter, first, mapKeys, orderBy, uniq, map } from 'lodash';
  */
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { Dropdown, IconButton, Toolbar, PanelBody, Path, SVG } from '@wordpress/components';
-import { getBlockType, getPossibleBlockTransformations, switchToBlockType, hasChildBlocksWithInserterSupport } from '@wordpress/blocks';
+import { getBlockType, getPossibleBlockTransformations, switchToBlockType, hasChildBlocksWithInserterSupport, cloneBlock } from '@wordpress/blocks';
 import { Component } from '@wordpress/element';
 import { DOWN } from '@wordpress/keycodes';
 import { withSelect, withDispatch } from '@wordpress/data';
@@ -161,13 +161,11 @@ export class BlockSwitcher extends Component {
 
 						{ ( hoveredClassName !== null ) &&
 							<BlockPreview
-								blocks={ {
-									...blocks[ 0 ],
-									attributes: {
-										...blocks[ 0 ].attributes,
+								blocks={
+									cloneBlock( blocks[ 0 ], {
 										className: hoveredClassName,
-									},
-								} }
+									} )
+								}
 
 							/>
 						}
