dadf3f3c0442205d4d7b0acee4e9363c2c081ef4
Seghir Nadir <nadir.seghir@gmail.com>
2019-07-31 19:36:00 +0100

    add color support to separator block (#16784)
    * add color support to separator block
    
    * add color to frontend render
    
    * add support for dots style in frontend
    
    * fix review problems, update block.json file
    
    * fix border issue with theme.scss
    
    * extend border remove in editor
    
    * fix problems with no color selected

--- packages/block-library/src/separator/edit.js
+++ packages/block-library/src/separator/edit.js
@@ -1,8 +1,49 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { HorizontalRule } from '@wordpress/components';
+import {
+	InspectorControls,
+	withColors,
+	PanelColorSettings,
+} from '@wordpress/block-editor';
 
-export default function SeparatorEdit( { className } ) {
-	return <HorizontalRule className={ className } />;
+function SeparatorEdit( { color, setColor, className } ) {
+	return (
+		<>
+			<HorizontalRule
+				className={ classnames(
+					className, {
+						'has-background': color.color,
+						[ color.class ]: color.class,
+					}
+				) }
+				style={ {
+					backgroundColor: color.color,
+					color: color.color,
+				} }
+			/>
+			<InspectorControls>
+				<PanelColorSettings
+					title={ __( 'Color Settings' ) }
+					colorSettings={ [
+						{
+							value: color.color,
+							onChange: setColor,
+							label: __( 'Color' ),
+						},
+					] }
+				>
+				</PanelColorSettings>
+			</InspectorControls>
+		</>
+	);
 }
+
+export default withColors( 'color', { textColor: 'color' } )( SeparatorEdit );
