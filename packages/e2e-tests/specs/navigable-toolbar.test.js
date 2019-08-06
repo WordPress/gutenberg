69bf1f9979bc3a97a72c1581bc6850a36725ad9e
Pascal Birchler <pascalb@google.com>
2019-08-06 12:22:08 +0200

    Remove unnecessary nested describe

--- packages/e2e-tests/specs/navigable-toolbar.test.js
+++ packages/e2e-tests/specs/navigable-toolbar.test.js
@@ -22,20 +22,18 @@ describe.each( [ [ 'unified', true ], [ 'contextual', false ] ] )(
 			!! document.activeElement.closest( '.block-editor-block-toolbar' )
 		) );
 
-		describe( label, () => {
-			it( 'navigates in and out of toolbar by keyboard (Alt+F10, Escape)', async () => {
-				// Assumes new post focus starts in title. Create first new
-				// block by ArrowDown.
-				await page.keyboard.press( 'ArrowDown' );
+		it( 'navigates in and out of toolbar by keyboard (Alt+F10, Escape)', async () => {
+			// Assumes new post focus starts in title. Create first new
+			// block by ArrowDown.
+			await page.keyboard.press( 'ArrowDown' );
 
-				// [TEMPORARY]: A new paragraph is not technically a block yet
-				// until starting to type within it.
-				await page.keyboard.type( 'Example' );
+			// [TEMPORARY]: A new paragraph is not technically a block yet
+			// until starting to type within it.
+			await page.keyboard.type( 'Example' );
 
-				// Upward
-				await pressKeyWithModifier( 'alt', 'F10' );
-				expect( await isInBlockToolbar() ).toBe( true );
-			} );
+			// Upward
+			await pressKeyWithModifier( 'alt', 'F10' );
+			expect( await isInBlockToolbar() ).toBe( true );
 		} );
 	}
 );
