1ddab6ce0a50886a638956eab9fd934b4bb71e0d
CK <ck.lee@automattic.com>
2019-08-08 10:05:27 +1200

    Rename block-ratings to prefix with block-directory.

--- packages/block-directory/src/components/block-ratings/index.js
+++ packages/block-directory/src/components/block-ratings/index.js
@@ -9,10 +9,10 @@ import { _n, sprintf } from '@wordpress/i18n';
 import Stars from './stars';
 
 export const BlockRatings = ( { rating, ratingCount } ) => (
-	<div className="block-editor-block-ratings">
+	<div className="block-directory-block-ratings">
 		<Stars rating={ rating } />
 		<span
-			className="block-editor-block-ratings__rating-count"
+			className="block-directory-block-ratings__rating-count"
 			aria-label={
 				// translators: %d: number of ratings (number).
 				sprintf( _n( '%d total rating', '%d total ratings', ratingCount ), ratingCount )
