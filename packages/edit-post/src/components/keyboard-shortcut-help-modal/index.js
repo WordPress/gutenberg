9f32f131091ebd92b5e9a092a62f771478727519
Mark Uraine <uraine@gmail.com>
2019-08-08 02:42:13 -0700

    Modifies shortcut hierarchy in modal (#16724)
    * Fixes #16691. Moves the shortcut to open shortcut modal to it's own section right under the modal header.
    
    * Only render shortcut title when a value is provided
    
    * Use classname to modify style for first shortcut
    
    * Update snapshots
    
    * Using variable for elements top-margin for positioning.

--- packages/edit-post/src/components/keyboard-shortcut-help-modal/index.js
+++ packages/edit-post/src/components/keyboard-shortcut-help-modal/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { castArray } from 'lodash';
+import classnames from 'classnames';
 
 /**
  * WordPress dependencies
@@ -64,11 +65,13 @@ const ShortcutList = ( { shortcuts } ) => (
 	</ul>
 );
 
-const ShortcutSection = ( { title, shortcuts } ) => (
-	<section className="edit-post-keyboard-shortcut-help__section">
-		<h2 className="edit-post-keyboard-shortcut-help__section-title">
-			{ title }
-		</h2>
+const ShortcutSection = ( { title, shortcuts, className } ) => (
+	<section className={ classnames( 'edit-post-keyboard-shortcut-help__section', className ) }>
+		{ !! title && (
+			<h2 className="edit-post-keyboard-shortcut-help__section-title">
+				{ title }
+			</h2>
+		) }
 		<ShortcutList shortcuts={ shortcuts } />
 	</section>
 );
