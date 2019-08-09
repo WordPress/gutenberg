97502afc1d6d097d59c7069ca96af660a50bc6a4
epiqueras <epiquerass@gmail.com>
2019-08-09 16:47:18 -0400

    Block Editor: Fix undo level logic.

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -756,7 +756,12 @@ const applyWithDispatch = withDispatch( ( dispatch, ownProps, { select } ) => {
 			}
 		},
 		onReplace( blocks, indexToSelect ) {
-			__unstableMarkLastChangeAsPersistent();
+			if (
+				blocks.length &&
+				! isUnmodifiedDefaultBlock( blocks[ blocks.length - 1 ] )
+			) {
+				__unstableMarkLastChangeAsPersistent();
+			}
 			replaceBlocks( [ ownProps.clientId ], blocks, indexToSelect );
 		},
 		onShiftSelection() {
