a9f805445f10cc12bd3ed2fa16a2ae5ad5fce791
retrofox <rdsuarez@gmail.com>
2019-08-12 11:16:29 -0300

    block-alignment-toolbar: implement group by

--- packages/block-editor/src/components/block-alignment-toolbar/index.js
+++ packages/block-editor/src/components/block-alignment-toolbar/index.js
@@ -32,38 +32,66 @@ const BLOCK_ALIGNMENTS_CONTROLS = {
 		icon: 'align-full-width',
 		title: __( 'Full Width' ),
 	},
+	fullHeight: {
+		icon: 'image-flip-vertical',
+		title: __( 'Full height' ),
+	},
 };
 
-const DEFAULT_CONTROLS = [ 'left', 'center', 'right', 'wide', 'full' ];
+const DEFAULT_CONTROLS = [ [ 'left', 'center', 'right', 'wide', 'full' ], [ 'fullHeight' ] ];
 const DEFAULT_CONTROL = 'center';
-const WIDE_CONTROLS = [ 'wide', 'full' ];
+const WIDE_CONTROLS = [ 'wide', 'full', 'fullHeight' ];
 
-export function BlockAlignmentToolbar( { value, onChange, controls = DEFAULT_CONTROLS, isCollapsed = true, wideControlsEnabled = false } ) {
+export function BlockAlignmentToolbar( {
+	value,
+	onChange,
+	controls = DEFAULT_CONTROLS,
+	isCollapsed = true,
+	wideControlsEnabled = false,
+} ) {
+	/**
+	 * Control button handler.
+	 *
+	 * @param {string}    align Alignment value to set in the toolbar.
+	 * @return {Function} Handler function.
+	 */
 	function applyOrUnset( align ) {
 		return () => onChange( value === align ? undefined : align );
 	}
 
-	const enabledControls = wideControlsEnabled ?
+	/**
+	 * Given the control name returns a control object.
+	 *
+	 * @param {string}  name Control name
+	 * @return {Object} Control object.
+	 */
+	const mapButtonControl = ( name ) => ( {
+		...BLOCK_ALIGNMENTS_CONTROLS[ name ],
+		isActive: value === name,
+		onClick: applyOrUnset( name ),
+	} );
+
+	const enabledControls = wideControlsEnabled && false ?
 		controls :
 		controls.filter( ( control ) => WIDE_CONTROLS.indexOf( control ) === -1 );
 
-	const activeAlignmentControl = BLOCK_ALIGNMENTS_CONTROLS[ value ];
+	const activeAlignmentControl = wideControlsEnabled ? BLOCK_ALIGNMENTS_CONTROLS[ value ] : null;
 	const defaultAlignmentControl = BLOCK_ALIGNMENTS_CONTROLS[ DEFAULT_CONTROL ];
+
+	// Map buttons control.
+	// it can get the unidimensional shape: [ 'left', 'center', 'right' ]
+	// or 2-dimensional array if it desires the controls to be grouped:
+	// [ [ 'left', 'center', 'right' ], [ 'wide' ] ]
+	const toolbarControls = enabledControls.map( ( controlNames ) => (
+		Array.isArray( controlNames ) ? controlNames.map( mapButtonControl ) : mapButtonControl( controlNames )
+	) );
 
 	return (
 		<Toolbar
 			isCollapsed={ isCollapsed }
 			icon={ activeAlignmentControl ? activeAlignmentControl.icon : defaultAlignmentControl.icon }
 			label={ __( 'Change alignment' ) }
-			controls={
-				enabledControls.map( ( control ) => {
-					return {
-						...BLOCK_ALIGNMENTS_CONTROLS[ control ],
-						isActive: value === control,
-						onClick: applyOrUnset( control ),
-					};
-				} )
-			}
+			controls={ toolbarControls }
 		/>
 	);
 }
