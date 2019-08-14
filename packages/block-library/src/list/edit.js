5425410dc2910b160ff39e1385f1d3fde4d2a71a
iseulde <wp@iseulde.com>
2019-08-14 21:42:32 +0200

    Move list controls

--- packages/block-library/src/list/edit.js
+++ packages/block-library/src/list/edit.js
@@ -1,9 +1,17 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import { createBlock } from '@wordpress/blocks';
-import { RichText } from '@wordpress/block-editor';
+import { RichText, BlockControls, RichTextShortcut } from '@wordpress/block-editor';
+import { Toolbar } from '@wordpress/components';
+import {
+	__unstableIndentListItems as indentListItems,
+	__unstableOutdentListItems as outdentListItems,
+	__unstableChangeListType as changeListType,
+	__unstableIsListRootSelected as isListRootSelected,
+	__unstableIsActiveListType as isActiveListType,
+} from '@wordpress/rich-text';
 
 /**
  * Internal dependencies
@@ -18,12 +26,91 @@ export default function ListEdit( {
 	className,
 } ) {
 	const { ordered, values } = attributes;
+	const tagName = ordered ? 'ol' : 'ul';
+
+	const controls = ( { value, onChange } ) =>
+		<>
+			<RichTextShortcut
+				type="primary"
+				character="["
+				onUse={ () => {
+					onChange( outdentListItems( value ) );
+				} }
+			/>
+			<RichTextShortcut
+				type="primary"
+				character="]"
+				onUse={ () => {
+					onChange( indentListItems( value, { type: tagName } ) );
+				} }
+			/>
+			<RichTextShortcut
+				type="primary"
+				character="m"
+				onUse={ () => {
+					onChange( indentListItems( value, { type: tagName } ) );
+				} }
+			/>
+			<RichTextShortcut
+				type="primaryShift"
+				character="m"
+				onUse={ () => {
+					onChange( outdentListItems( value ) );
+				} }
+			/>
+			<BlockControls>
+				<Toolbar
+					controls={ [
+						{
+							icon: 'editor-ul',
+							title: __( 'Convert to unordered list' ),
+							isActive: isActiveListType( value, 'ul', tagName ),
+							onClick() {
+								onChange( changeListType( value, { type: 'ul' } ) );
+
+								if ( isListRootSelected( value ) ) {
+									setAttributes( { ordered: false } );
+								}
+							},
+						},
+						{
+							icon: 'editor-ol',
+							title: __( 'Convert to ordered list' ),
+							isActive: isActiveListType( value, 'ol', tagName ),
+							onClick() {
+								onChange( changeListType( value, { type: 'ol' } ) );
+
+								if ( isListRootSelected( value ) ) {
+									setAttributes( { ordered: true } );
+								}
+							},
+						},
+						{
+							icon: 'editor-outdent',
+							title: __( 'Outdent list item' ),
+							shortcut: _x( 'Backspace', 'keyboard key' ),
+							onClick() {
+								onChange( outdentListItems( value ) );
+							},
+						},
+						{
+							icon: 'editor-indent',
+							title: __( 'Indent list item' ),
+							shortcut: _x( 'Space', 'keyboard key' ),
+							onClick() {
+								onChange( indentListItems( value, { type: tagName } ) );
+							},
+						},
+					] }
+				/>
+			</BlockControls>
+		</>;
 
 	return (
 		<RichText
 			identifier="values"
 			multiline="li"
-			tagName={ ordered ? 'ol' : 'ul' }
+			tagName={ tagName }
 			onChange={ ( nextValues ) => setAttributes( { values: nextValues } ) }
 			value={ values }
 			wrapperClassName="block-library-list"
@@ -34,7 +121,8 @@ export default function ListEdit( {
 			__unstableOnSplitMiddle={ () => createBlock( 'core/paragraph' ) }
 			onReplace={ onReplace }
 			onRemove={ () => onReplace( [] ) }
-			onTagNameChange={ ( tag ) => setAttributes( { ordered: tag === 'ol' } ) }
-		/>
+		>
+			{ controls }
+		</RichText>
 	);
 }
