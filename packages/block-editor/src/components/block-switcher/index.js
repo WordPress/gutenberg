c819d0e0cb8e6a99e3d435150d5572a9edf17431
Riad Benguella <benguella@gmail.com>
2019-08-19 12:28:07 +0100

    Remove borders around inserter items for blocks with children blocks (#17083)

--- packages/block-editor/src/components/block-switcher/index.js
+++ packages/block-editor/src/components/block-switcher/index.js
@@ -8,7 +8,7 @@ import { castArray, filter, first, mapKeys, orderBy, uniq, map } from 'lodash';
  */
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { Dropdown, IconButton, Toolbar, PanelBody, Path, SVG } from '@wordpress/components';
-import { getBlockType, getPossibleBlockTransformations, switchToBlockType, hasChildBlocksWithInserterSupport, cloneBlock } from '@wordpress/blocks';
+import { getBlockType, getPossibleBlockTransformations, switchToBlockType, cloneBlock } from '@wordpress/blocks';
 import { Component } from '@wordpress/element';
 import { DOWN } from '@wordpress/keycodes';
 import { withSelect, withDispatch } from '@wordpress/data';
@@ -151,7 +151,6 @@ export class BlockSwitcher extends Component {
 												id: destinationBlockType.name,
 												icon: destinationBlockType.icon,
 												title: destinationBlockType.title,
-												hasChildBlocksWithInserterSupport: hasChildBlocksWithInserterSupport( destinationBlockType.name ),
 											} ) ) }
 											onSelect={ ( item ) => {
 												onTransform( blocks, item.id );
