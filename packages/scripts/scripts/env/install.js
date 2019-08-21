ecc8b259c8a94706943f174bd2993fb1a0778ad1
Gary <gary@pento.net>
2019-08-21 11:00:56 +1000

    Check if WordPress is already installed before trying to install it again.

--- packages/scripts/scripts/env/install.js
+++ packages/scripts/scripts/env/install.js
@@ -9,12 +9,17 @@ const DecompressZip = require( 'decompress-zip' );
  * Node dependencies.
  */
 const { execSync } = require( 'child_process' );
-const { env, cwd, stdout } = require( 'process' );
+const { env, exit, cwd, stdout } = require( 'process' );
 const { normalize } = require( 'path' );
-const { createWriteStream } = require( 'fs' );
+const { createWriteStream, existsSync } = require( 'fs' );
 const { tmpdir } = require( 'os' );
 
 env.WP_DEVELOP_DIR = cwd() + '/wordpress';
+
+if ( existsSync( normalize( cwd() + '/wordpress/wp-config-sample.php' ) ) ) {
+	stdout.write( 'It looks like WordPress is already installed, please delete the `wordpress` directory for a fresh install, or run `npm run env start` to start the existing environment.\n' );
+	exit( 1 );
+}
 
 if ( commandExistsSync( 'git' ) ) {
 	execSync( 'git clone --depth=1 git://develop.git.wordpress.org/ wordpress', { stdio: 'inherit' } );
