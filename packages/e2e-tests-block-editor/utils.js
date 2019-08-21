461ba76c15ba6bc32badc868ce1b1ca7760321d4
iseulde <wp@iseulde.com>
2019-08-22 00:09:10 +0200

    Typo

---
+++ packages/e2e-tests-block-editor/utils.js
@@ -0,0 +1,8 @@
+/**
+ * Returns a promise which resolves with the edited post content (HTML string).
+ *
+ * @return {Promise} Promise resolving with post content markup.
+ */
+export async function getEditedPostContent() {
+	return page.evaluate( () => window._getContent() );
+}
