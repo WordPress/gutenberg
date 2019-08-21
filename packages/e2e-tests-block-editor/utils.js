f91d7101ad8c64ecaa35b8a1a1266182b032d890
Ella van Durpe <wp@iseulde.com>
2019-08-22 00:10:10 +0200

    Delete utils.js

--- packages/e2e-tests-block-editor/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns a promise which resolves with the edited post content (HTML string).
- *
- * @return {Promise} Promise resolving with post content markup.
- */
-export async function getEditedPostContent() {
-	return page.evaluate( () => window._getContent() );
-}
