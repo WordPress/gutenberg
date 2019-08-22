6fd7805b6de5a246d374bb5f8a90f5c4f578bae2
Gary <gary@pento.net>
2019-08-22 17:33:15 +1000

    Auto switch between running PHPUnit tests from the src or build directory.

--- packages/scripts/scripts/env/test-php.js
+++ packages/scripts/scripts/env/test-php.js
@@ -4,6 +4,7 @@
 const { execSync } = require( 'child_process' );
 const { env } = require( 'process' );
 const { normalize } = require( 'path' );
+const { existsSync } = require( 'fs' );
 
 /**
  * Internal dependencies
@@ -14,5 +15,10 @@ const args = getArgsFromCLI();
 
 const localDir = env.LOCAL_DIR || 'src';
 
+let buildConfig = '';
+if ( localDir === 'build' && existsSync( 'phpunit/build-dir.php' ) ) {
+	buildConfig = `--prepend /var/www/${ localDir }/wp-content/plugins/${ env.npm_package_wp_env_plugin_dir }/phpunit/build-dir.php`;
+}
+
 // Run PHPUnit with the working directory set correctly.
-execSync( `npm run test:php -- -c /var/www/${ localDir }/wp-content/plugins/${ env.npm_package_wp_env_plugin_dir }/phpunit.xml.dist ` + args.join( ' ' ), { cwd: normalize( env.WP_DEVELOP_DIR ), stdio: 'inherit' } );
+execSync( `npm run test:php -- -c /var/www/${ localDir }/wp-content/plugins/${ env.npm_package_wp_env_plugin_dir }/phpunit.xml.dist ${ buildConfig } ` + args.join( ' ' ), { cwd: normalize( env.WP_DEVELOP_DIR ), stdio: 'inherit' } );
---
+++ phpunit/build-dir.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Config to force tests to run from WordPress' build directory, instead of the src directory.
+ *
+ * @package Gutenberg
+ */
+
+define( 'WP_RUN_CORE_TESTS', true );
