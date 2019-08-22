9e6054b3314f5e2184da3a73aced53c90bc384da
Gary <gary@pento.net>
2019-08-22 10:41:03 +1000

    Add a placeholder for setup-local-env.sh, pointing folks at the new way of doing things.

---
+++ bin/setup-local-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Exit if any command fails.
+set -e
+
+echo "Hi there! It looks like you're trying to use the old local environment setup script. This script has been retired, running \`npm run env install\` will setup a local environment for you, instead.
+
+Check out the documentation for more information: https://developer.wordpress.org/block-editor/contributors/develop/getting-started/"
