1cf07e901e62ec06ae867ba86208e34fa77baee7
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-08-22 18:02:25 +0300

    gave up the walker class

--- packages/block-library/src/navigation-menu/index.php
+++ packages/block-library/src/navigation-menu/index.php
@@ -1,41 +1,4 @@
 <?php
-
-/**
- * `core/navigation-menu` walker.
- * I WILL MOVE THIS FROM HERE!
- */
-class Walker_Simple_Example extends Walker {
-
-	/**
-	 * Set the properties of the element which give the ID of the current item and its parent.
-	 *
-	 * @var $db_fields array
-	 */
-	var $db_fields = array(
-		'parent' => 'parent',
-		'id' => 'id'
-	);
-
-	function start_lvl(&$output, $depth=0, $args=array()) {
-		$output .= "\n<ul>\n";
-	}
-
-
-	function end_lvl(&$output, $depth=0, $args=array()) {
-		$output .= "</ul>\n";
-	}
-
-
-	function start_el(&$output, $item, $depth=0, $args=array(), $current_object_id = 0) {
-		$output .= "<li>".$item->title[0];
-    }
-
-
-	function end_el(&$output, $item, $depth=0, $args=array()) {
-		$output .= "</li>";
-	}
-}
-
 /**
  * Server-side rendering of the `core/navigation-menu` block.
  *
@@ -52,32 +15,26 @@ class Walker_Simple_Example extends Walker {
  * @return string Returns the post content with the legacy widget added.
  */
 function render_block_navigation_menu( $attributes, $content, $block ) {
-	$block  = prepare_navigation_for_walker( $block );
-	$walker = new Walker_Simple_Example();
-	return '<ul>' . $walker->walk( $block, 0 ) . '</ul>';
+	return prepare_navigation( $block );
 }
 
 /**
- * Adds parent and child relation to innerBlocks
+ * Adds parent and child relatin to innerBlocks
  *
- * @param array    $block The block.
- * @param int|bool $parent the current parent or false if none.
- * @param array    $block_tree the current tree.
+ * @param array  $block The block.
+ * @param string $block_tree the current tree.
  *
- * @return array Returns the block with ids and parents for innerBlocks.
+ * @return string Returns the block with ids and parents for innerBlocks.
  */
-function prepare_navigation_for_walker( $block, $parent = false, $block_tree = array() ) {
+function prepare_navigation( $block, $block_tree = '' ) {
 	foreach ( (array) $block['innerBlocks'] as $key => $menu_item ) {
-		$current_block     = new stdClass();
-		$current_block->id = count( $block_tree ) + 1;
-		if ( $parent ) {
-			$current_block->parent = $parent;
+		$block_tree .= '<li>' . $menu_item['innerContent'][0];
+		if ( count( (array) $menu_item['innerBlocks'] ) > 0 ) {
+			$block_tree .= prepare_navigation( $menu_item, '' );
 		}
-		$current_block->title = $menu_item['innerContent'];
-		$block_tree[]         = $current_block;
-		$block_tree           = prepare_navigation_for_walker( $menu_item, $current_block->id, $block_tree );
+		$block_tree .= '</li>';
 	}
-	return $block_tree;
+	return '<ul>' . $block_tree . '</ul>';
 }
 
 /**
