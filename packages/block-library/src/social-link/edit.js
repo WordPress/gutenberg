a8e2e30affaef5227c366ca189998f7742224204
Marcus Kazmierczak <marcus@mkaz.com>
2019-08-21 21:00:40 -0700

    Show icons when block inserted, click to select
    Removes switching icon based on URL typed because someone is
    selecting the icon first. This makes it harder to change an icon
    after the fact, will require delete and insert.

--- packages/block-library/src/social-link/edit.js
+++ packages/block-library/src/social-link/edit.js
@@ -11,59 +11,44 @@ import {
 	Button,
 	Dropdown,
 } from '@wordpress/components';
+
 /**
  * Internal dependencies
  */
 import socialList from './social-list';
 import IconPicker from './icon-picker';
 
-// Receives an URL and tries to match to return an icon.
-const getIconByURL = ( url ) => {
-	const sites = Object.keys( socialList );
-	for ( const site of sites ) {
-		for ( const match of socialList[ site ] ) {
-			if ( url.includes( match ) ) {
-				return site;
-			}
-		}
-	}
-	return null;
-};
-
-const SocialLinkEdit = ( { attributes, setAttributes, isSelected } ) => {
+const SocialLinkEdit = ( { attributes, setAttributes } ) => {
 	const { icon, url } = attributes;
 
 	const classes = classNames( 'wp-social-icon', `wp-social-icon-${ icon }` );
 	return (
 		<>
-			<Dropdown
-				className="wp-block-social-link__icon-picker-dropdown"
-				position="bottom right"
-				renderToggle={ ( { isOpen, onToggle } ) => (
-					<Button
-						className={ classes }
-						onClick={ onToggle }
-						aria-expanded={ isOpen }
-					></Button>
-				) }
-				renderContent={ () => (
-					<IconPicker
-						icons={ Object.keys( socialList ) }
-						onClick={ ( value ) => setAttributes( { icon: value } ) }
-					/>
-				) }
-			/>
-			{
-				isSelected && (
-					<URLInput
-						value={ url }
-						onChange={ ( value ) => {
-							const iconByURL = getIconByURL( value );
-							const newIcon = ( iconByURL ) ? iconByURL : icon;
-							setAttributes( { url: value, icon: newIcon } );
-						} }
-					/>
-				)
+			{ icon && (
+				<Dropdown
+					className="wp-block-social-link__icon-picker-dropdown"
+					position="bottom right"
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<Button
+							className={ classes }
+							onClick={ onToggle }
+							aria-expanded={ isOpen }
+						></Button>
+					) }
+					renderContent={ () => (
+						<URLInput
+							value={ url }
+							onChange={ ( value ) => setAttributes( { url: value } ) }
+							disableSuggestions={ true }
+						/>
+					) }
+				/> )
+			}
+			{ ! icon &&
+				<IconPicker
+					icons={ Object.keys( socialList ) }
+					onClick={ ( value ) => setAttributes( { icon: value } ) }
+				/>
 			}
 		</>
 	);
