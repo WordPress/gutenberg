37fa3ff8d51224b11ab40e510e812335fa43b83b
Marcus Kazmierczak <marcus@mkaz.com>
2019-08-21 21:36:59 -0700

    Display URLInput Popover on insert
    Switches URLInput field to Popover so it can be controlled to display
    on insert. This allows the user to know they have to fill in the URL for
    the icon and removes an extra click on initial insert to set.

--- packages/block-library/src/social-link/edit.js
+++ packages/block-library/src/social-link/edit.js
@@ -7,9 +7,10 @@ import classNames from 'classnames';
  * WordPress dependencies
  */
 import { URLInput } from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
 import {
 	Button,
-	Dropdown,
+	Popover,
 } from '@wordpress/components';
 
 /**
@@ -18,32 +19,31 @@ import {
 import socialList from './social-list';
 import IconPicker from './icon-picker';
 
-const SocialLinkEdit = ( { attributes, setAttributes } ) => {
+const SocialLinkEdit = ( { attributes, setAttributes, isSelected } ) => {
 	const { icon, url } = attributes;
+	const [ showURLPopover, setPopover ] = useState( true );
 
 	const classes = classNames( 'wp-social-icon', `wp-social-icon-${ icon }` );
 	return (
 		<>
 			{ icon && (
-				<Dropdown
-					className="wp-block-social-link__icon-picker-dropdown"
-					position="bottom right"
-					renderToggle={ ( { isOpen, onToggle } ) => (
-						<Button
-							className={ classes }
-							onClick={ onToggle }
-							aria-expanded={ isOpen }
-						></Button>
+				<Button
+					className={ classes }
+					onClick={ () => setPopover( true ) }
+				>
+					{ isSelected && showURLPopover && (
+						<Popover
+							onFocusOutside={ () => setPopover( false ) }
+						>
+							<URLInput
+								value={ url }
+								onChange={ ( value ) => setAttributes( { url: value } ) }
+								disableSuggestions={ true }
+							/>
+						</Popover>
 					) }
-					renderContent={ () => (
-						<URLInput
-							value={ url }
-							onChange={ ( value ) => setAttributes( { url: value } ) }
-							disableSuggestions={ true }
-						/>
-					) }
-				/> )
-			}
+				</Button>
+			) }
 			{ ! icon &&
 				<IconPicker
 					icons={ Object.keys( socialList ) }
