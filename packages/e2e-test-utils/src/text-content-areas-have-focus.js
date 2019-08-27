c66437947f4480e7646db61f020496191c18ca90
TJ Nicolaides <tj.nicolaides@urbandoor.com>
2019-08-26 23:29:30 -0700

    Re-evaluate isFocusedTextContentArea and textContentAreaContent within each for block to verify the assertions are being made about the expected elements.

--- packages/e2e-test-utils/src/text-content-areas-have-focus.js
+++ packages/e2e-test-utils/src/text-content-areas-have-focus.js
@@ -12,13 +12,13 @@ import { textContentAreas } from './text-content-areas';
 
 export async function textContentAreasHaveFocus( content ) {
 	const blocks = await textContentAreas( { empty: false } );
-	const isFocusedTextContentArea = await page.evaluate( () => document.activeElement.contentEditable );
-	const textContentAreaContent = await page.evaluate( () => document.activeElement.innerHTML );
 
 	for ( let i = 0; i < blocks.length; i++ ) {
 		if ( i > 0 ) {
 			await page.keyboard.press( 'Tab' );
 		}
+		const isFocusedTextContentArea = await page.evaluate( () => document.activeElement.contentEditable );
+		const textContentAreaContent = await page.evaluate( () => document.activeElement.innerHTML );
 
 		// The value of 'contentEditable' should be the string 'true'
 		expect( isFocusedTextContentArea ).toBe( 'true' );
