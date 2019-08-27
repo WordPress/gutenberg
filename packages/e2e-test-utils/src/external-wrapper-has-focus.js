7c8c6663c9e2a10433447150e2add1eda44d4138
TJ Nicolaides <tj.nicolaides@urbandoor.com>
2019-08-26 21:01:32 -0700

    Assertion in externalWrapperHasFocus does not need to await

--- packages/e2e-test-utils/src/external-wrapper-has-focus.js
+++ packages/e2e-test-utils/src/external-wrapper-has-focus.js
@@ -6,5 +6,5 @@
 
 export async function externalWrapperHasFocus( blockType ) {
 	const activeElementDataType = await page.evaluate( () => document.activeElement.dataset.type );
-	await expect( activeElementDataType ).toEqual( blockType );
+	expect( activeElementDataType ).toEqual( blockType );
 }
