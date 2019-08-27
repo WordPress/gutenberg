27988e3089e381ba2b76896135650f457ed787f0
Marcus Kazmierczak <marcus@mkaz.com>
2019-08-27 16:43:23 -0700

    Update and fix tests, get to green
    - Add tests for individual social link blocks
    - Generate fixtures
    - Add block transforms (Group)
    - PHP has lint rules too

--- packages/e2e-tests/fixtures/block-transforms.js
+++ packages/e2e-tests/fixtures/block-transforms.js
@@ -404,11 +404,215 @@ export const EXPECTED_TRANSFORMS = {
 			'Group',
 		],
 	},
-	'core__social-link': {
-		originalBlock: 'Social link',
+	'core__social-link-amazon': {
 		availableTransforms: [
 			'Group',
 		],
+		originalBlock: 'Amazon',
+	},
+	'core__social-link-bandcamp': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Bandcamp',
+	},
+	'core__social-link-behance': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Behance',
+	},
+	'core__social-link-chain': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Link',
+	},
+	'core__social-link-codepen': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'CodePen',
+	},
+	'core__social-link-deviantart': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'DeviantArt',
+	},
+	'core__social-link-dribbble': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Dribbble',
+	},
+	'core__social-link-etsy': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Etsy',
+	},
+	'core__social-link-facebook': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Facebook',
+	},
+	'core__social-link-feed': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'RSS Feed',
+	},
+	'core__social-link-fivehundredpx': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: '500px',
+	},
+	'core__social-link-flickr': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Flickr',
+	},
+	'core__social-link-foursquare': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Foursquare',
+	},
+	'core__social-link-github': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Github',
+	},
+	'core__social-link-goodreads': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Goodreads',
+	},
+	'core__social-link-instagram': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Instagram',
+	},
+	'core__social-link-lastfm': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Last.fm',
+	},
+	'core__social-link-linkedin': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Linkedin',
+	},
+	'core__social-link-mail': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'E-mail',
+	},
+	'core__social-link-mastodon': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Mastodon',
+	},
+	'core__social-link-medium': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Medium',
+	},
+	'core__social-link-pinterest': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Pinterest',
+	},
+	'core__social-link-pocket': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Pocket',
+	},
+	'core__social-link-reddit': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Reddit',
+	},
+	'core__social-link-skype': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Skype',
+	},
+	'core__social-link-snapchat': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Snapshot',
+	},
+	'core__social-link-soundcloud': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Soundcloud',
+	},
+	'core__social-link-spotify': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Spotify',
+	},
+	'core__social-link-tumblr': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Tumblr',
+	},
+	'core__social-link-twitch': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Twitch',
+	},
+	'core__social-link-twitter': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Twitter',
+	},
+	'core__social-link-vimeo': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'Vimeo',
+	},
+	'core__social-link-vk': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'VK',
+	},
+	'core__social-link-wordpress': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'WordPress',
+	},
+	'core__social-link-youtube': {
+		availableTransforms: [
+			'Group',
+		],
+		originalBlock: 'YouTube',
 	},
 	'core__social-links': {
 		originalBlock: 'Social links',
