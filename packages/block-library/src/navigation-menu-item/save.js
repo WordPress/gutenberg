73cb901c4084bbc1e2cb19dbf9df9c07a7c9d9a6
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-08-28 15:20:01 +0300

    saving only comments as HTML in the DB b/c InnerBlocks are not ready for block validation in nested situations yet

--- packages/block-library/src/navigation-menu-item/save.js
+++ packages/block-library/src/navigation-menu-item/save.js
@@ -5,28 +5,8 @@ import {
 	InnerBlocks,
 } from '@wordpress/block-editor';
 
-export default function save( { attributes/*, innerBlocks*/ } ) {
-	const { destination, nofollow, title, opensInNewTab, label } = attributes;
+export default function save() {
 	return (
-		<li>
-			<a
-				href={ destination }
-				rel={ nofollow ? 'nofollow' : undefined }
-				title={ title }
-				target={ opensInNewTab ? '_blank' : undefined }
-				className="wp-block-navigation-menu-item"
-			>
-				{ label }
-			</a>
-			<ul>
-				<InnerBlocks.Content />
-			</ul>
-
-			{ /* This should make the HTML look the same but it invalidates the block */ }
-
-			{ /* ( innerBlocks.length > 0 ) && <ul>
-				<InnerBlocks.Content />
-			</ul> */ }
-		</li>
+		<InnerBlocks.Content />
 	);
 }
--- packages/block-library/src/navigation-menu/save.js
+++ packages/block-library/src/navigation-menu/save.js
@@ -5,10 +5,6 @@ import { InnerBlocks } from '@wordpress/block-editor';
 
 export default function save() {
 	return (
-		<nav className="wp-block-navigation-menu">
-			<ul>
-				<InnerBlocks.Content />
-			</ul>
-		</nav>
+		<InnerBlocks.Content />
 	);
 }
