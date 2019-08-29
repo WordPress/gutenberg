61556d8e2ab95fc6311b79ff1b9cbb75e85951ed
epiqueras <epiquerass@gmail.com>
2019-08-29 15:53:32 -0700

    Editor: Export block serialization utility.

--- packages/editor/src/utils/index.js
+++ packages/editor/src/utils/index.js
@@ -5,3 +5,5 @@ import mediaUpload from './media-upload';
 
 export { mediaUpload };
 export { cleanForSlug } from './url.js';
+
+export { default as serializeBlocks } from '../store/utils/serialize-blocks';
