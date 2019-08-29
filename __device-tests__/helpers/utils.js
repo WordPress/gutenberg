9208e4f906adc154cb42d9ca5e2cc58287840873
Javon <javondavis@users.noreply.github.com>
2019-08-29 12:11:49 -0500

    remove explicit call to implicit wait

--- __device-tests__/helpers/utils.js
+++ __device-tests__/helpers/utils.js
@@ -112,7 +112,7 @@ const setupDriver = async () => {
 	// eslint-disable-next-line no-console
 	console.log( status );
 
-	await driver.setImplicitWaitTimeout( 2000 );
+	// await driver.setImplicitWaitTimeout( 2000 );
 	await timer( 3000 );
 	await driver.setOrientation( 'PORTRAIT' );
 
