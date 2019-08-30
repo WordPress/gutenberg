232aca6f85ea0894c580696f20228a4708fd380f
Gary Pendergast <gary@pento.net>
2019-08-30 11:45:10 +1000

    Switch to using the Core local environment (#17004)

---
+++ packages/scripts/scripts/env/cli.js
@@ -0,0 +1,17 @@
+/**
+ * Node dependencies.
+ */
+const { execSync } = require( 'child_process' );
+const { env } = require( 'process' );
+
+/**
+ * Internal dependencies
+ */
+const { getArgsFromCLI } = require( '../../utils' );
+
+const args = getArgsFromCLI();
+
+const localDir = env.LOCAL_DIR || 'src';
+
+// Run WP-CLI with the working directory set correctly.
+execSync( `npm run env:cli -- --path=/var/www/${ localDir } ` + args.join( ' ' ), { cwd: env.WP_DEVELOP_DIR, stdio: 'inherit' } );
