4d3b09a55da6d92f3b3a9948187f92f608e7f7a7
Daniel Richards <daniel.richards@automattic.com>
2019-08-30 13:44:11 +0800

    Refactor navigator into own component and hook up to store

--- packages/block-library/src/navigation-menu/edit.js
+++ packages/block-library/src/navigation-menu/edit.js
@@ -13,36 +13,24 @@ import {
 	CheckboxControl,
 	PanelBody,
 	Toolbar,
-	IconButton,
-	SVG,
-	Path,
 } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { __ } from '@wordpress/i18n';
-
-const NavigatorIcon = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
-		<Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H6v2h11V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" />
-	</SVG>
-);
+import BlockNavigator from './block-navigator';
 
 function NavigationMenu( {
 	attributes,
 	setAttributes,
+	clientId,
 } ) {
 	return (
 		<Fragment>
 			<BlockControls>
 				<Toolbar>
-					<IconButton
-						className="components-toolbar__control"
-						label={ __( 'View navigator' ) }
-						onClick={ () => {} }
-						icon={ NavigatorIcon }
-					/>
+					<BlockNavigator clientId={ clientId } />
 				</Toolbar>
 			</BlockControls>
 			<InspectorControls>
