882a1ca345079bae8b545a32e55d51e9e0c6163c
Matthew Kevins <mmkevins@yahoo.com>
2019-09-04 21:22:55 +1000

    Create fakes for gallery tiles

---
+++ packages/block-library/src/gallery/x-platform.js
@@ -0,0 +1,9 @@
+export { PanelBody } from '@wordpress/components';
+export { RangeControl } from '@wordpress/components';
+export { SelectControl } from '@wordpress/components';
+export { ToggleControl } from '@wordpress/components';
+export { withNotices} from '@wordpress/components';
+
+export { blockicon } from '@wordpress/block-editor';
+export { mediaplaceholder  } from '@wordpress/block-editor';
+export { inspectorcontrols  } from '@wordpress/block-editor';
---
+++ packages/block-library/src/gallery/x-platform.native.js
@@ -0,0 +1,20 @@
+import { Text } from 'react-native';
+
+export { withNotices } from '@wordpress/components';
+export { blockicon } from '@wordpress/block-editor';
+
+const MediaPlaceholder = props => <Text>FakeMediaPlaceholder</Text>;
+const InspectorControls = props => <Text>FakeInspectorControls</Text>
+const RangeControl = props => <Text>FakeRangeControl</Text>
+const ToggleControl = props => <Text>FakeToggleControl</Text>
+const SelectControl = props => <Text>FakeSelectControl</Text>
+const PanelBody = props => <Text>FakePanelBody</Text>
+
+export {
+  MediaPlaceholder,
+  InspectorControls,
+  RangeControl,
+  ToggleControl,
+  SelectControl,
+  PanelBody
+};
