eeace3d67d1fce375799cff63e14094f2802a70b
Pinar Olguc <pinarolguc@gmail.com>
2019-09-05 21:21:21 +0300

    [iOS]Fix: Oddly formatted text hangs Gutenberg (#1352)
    * Enable batch editing on font change
    
    * Remove unnecessary `removeAttribute` call

--- react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
+++ react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
@@ -508,12 +508,14 @@ class RCTAztecView: Aztec.TextView {
         
         let fullRange = NSRange(location: 0, length: textStorage.length)
         
+        textStorage.beginEditing()
         textStorage.enumerateAttributes(in: fullRange, options: []) { (attributes, subrange, stop) in
             let oldFont = font(from: attributes)
             let newFont = applyFontConstraints(to: oldFont)
             
             textStorage.addAttribute(.font, value: newFont, range: subrange)
         }
+        textStorage.endEditing()
         
         refreshTypingAttributesAndPlaceholderFont()
     }
