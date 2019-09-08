12814c484e0c1b42485a9a70e7191a8cc0eb9199
Darren Ethier <darren@roughsmootheng.in>
2019-09-08 18:37:30 -0400

    add initial commit of createInterpolateElement

---
+++ packages/element/src/create-interpolate-element.js
@@ -0,0 +1,219 @@
+/**
+ * Internal dependencies
+ */
+import { getTokenCount, resetTokenCount } from './token-count';
+import { createElement, Fragment, isValidElement } from './react';
+
+/**
+ * External dependencies
+ */
+import { escapeRegExp, flatMap } from 'lodash';
+
+const getHasPropValue = ( config ) => !! config.value;
+const getHasChildren = ( config ) => !! config.hasChildren;
+
+/**
+ * Transforms a given string value into a collection of html like tags.
+ *
+ * @example
+ *
+ * If you provide `something` as the tagString, this will return:
+ *
+ * ```js
+ * const tags = getTagsFromString( 'something' );
+ * const tagsAre = tags === [
+ *     '<something>',
+ *     '</something>',
+ *     '<something/>',
+ * ];
+ * ```
+ *
+ * @param {string} tagString  Something like `a%1`.
+ *
+ * @return {string[]} The generated tags for the given string.
+ */
+export const getTagsFromString = ( tagString ) => {
+	return [
+		`<${ tagString }>`,
+		`</${ tagString }>`,
+		`<${ tagString }/>`,
+	];
+};
+
+/**
+ * Generates and returns the regular expression for the given arguments.
+ *
+ * @param {string}  searchString  The search string serving as the base for the
+ *                                expression.
+ * @param {boolean} hasChildren   Whether the interpolation item has children.
+ * @param {boolean} hasPropValue  Whether the interpolation item represents a
+ *                                prop value.
+ *
+ * @return {RegExp}  The generated regular expression.
+ */
+const getRegEx = ( searchString, hasChildren, hasPropValue ) => {
+	let pattern;
+	const [ openTag, closeTag, selfClosingTag ] = getTagsFromString( searchString );
+	if ( hasChildren ) {
+		pattern = escapeRegExp( openTag ) + '(.*)' + escapeRegExp( closeTag );
+	} else if ( hasPropValue ) {
+		pattern = escapeRegExp( searchString );
+	} else {
+		pattern = escapeRegExp( selfClosingTag );
+	}
+	return new RegExp( pattern );
+};
+
+/**
+ * Generates and returns the regular expression used for splitting a string
+ *
+ * @param {string} searchString  The search string serving as the base for the
+ *                               expression.
+ *
+ * @return {RegExp}  The generated regular expression
+ */
+const getSplitRegEx = ( searchString ) => new RegExp( '(' + escapeRegExp( searchString ) + ')' );
+
+/**
+ * Generates a String.prototype.match value for the incoming arguments.
+ *
+ * @param {string} interpolatedString  The string the match is performed on.
+ * @param {string} searchString				 The string used as the base for the
+ *                                     search
+ * @param {Object} conversionConfig    The configuration object for the
+ *                                     interpolation being performed.
+ *
+ * @return {Array|null}	An array if there is a match or null if not.
+ */
+const getMatchFromString = (
+	interpolatedString,
+	searchString,
+	conversionConfig
+) => {
+	const regEx = getRegEx(
+		searchString,
+		getHasChildren( conversionConfig ),
+		getHasPropValue( conversionConfig )
+	);
+	return interpolatedString.match( regEx );
+};
+
+/**
+ * Used to recursively create elements from the interpolation string using the
+ * conversion map.
+ *
+ * @param {string} potentialElement  The interpolation string (or fragment)
+ *                                   being processed.
+ * @param {Array[]}conversionMap     The interpolation map used for converting
+ *                                   the string to a react element.
+ *
+ * @return {Element|string|Array}  A react element, string or array.
+ */
+const recursiveCreateElement = ( potentialElement, conversionMap ) => {
+	/**
+	 * If the conversion map is not a valid array or empty then just return the
+	 * element.
+	 */
+	if ( ! Array.isArray( conversionMap ) || ! conversionMap.length ) {
+		return potentialElement;
+	}
+	const [ mapItem ] = conversionMap.slice( 0, 1 );
+	const [ searchString, conversionConfig ] = mapItem;
+	let keyIndex;
+
+	/**
+	 * This short circuits the process if the conversion map has an invalid config.
+	 */
+	if ( ! searchString || ! conversionConfig ) {
+		return potentialElement;
+	}
+
+	const match = getMatchFromString(
+		potentialElement,
+		searchString,
+		conversionConfig
+	);
+
+	// if there is no match for this string, then that means it is not an element
+	// so just return as is to be used as a direct child.
+	if ( match === null ) {
+		return potentialElement;
+	}
+
+	// if the full match returned equals the potential element, then we know
+	// we can create the element and restart the conversion on any children if
+	// necessary.
+	if ( match[ 0 ] === potentialElement ) {
+		// remove this item from the conversion map because it's no longer needed.
+		conversionMap.shift();
+
+		if ( getHasPropValue( conversionConfig ) ) {
+			// if value is a react element, then need to wrap in Fragment with a key
+			// to prevent key warnings.
+			if ( isValidElement( conversionConfig.value ) ) {
+				keyIndex = getTokenCount( 'key' );
+				return <Fragment key={ keyIndex }>{ conversionConfig.value }</Fragment>;
+			}
+			return conversionConfig.value;
+		}
+		keyIndex = getTokenCount( 'key' );
+		return getHasChildren( conversionConfig ) ?
+			createElement(
+				conversionConfig.tag,
+				{ ...conversionConfig.props, key: keyIndex },
+				recursiveCreateElement( match[ 1 ], conversionMap )
+			) :
+			createElement(
+				conversionConfig.tag,
+				{ ...conversionConfig.props, key: keyIndex }
+			);
+	}
+
+	// still here, so we need to split on the full match and loop through each
+	return flatMap(
+		potentialElement.split( getSplitRegEx( match[ 0 ] ) )
+			.filter( ( value ) => !! value ),
+		( element ) => {
+			return recursiveCreateElement( element, conversionMap );
+		}
+	);
+};
+
+/**
+ * This function creates an interpolated element from a passed in string with
+ * specific tags matching how the string should be converted to an element via
+ * the conversion map value.
+ *
+ * @example
+ * For example, for the given string:
+ *
+ * "This is a <span%1>string</span%1> with <a%1>a link</a%1>, a self-closing
+ * %1$s tag and a plain value %2$s"
+ *
+ * You would have something like this as the conversionMap value:
+ *
+ * ```js
+ * [
+ *     [ 'span%1', { tag: CustomComponent, props: {}, hasChildren: true } ],
+ *     [ 'a%1', { tag: 'a', props: { href: 'https://github.com' }, hasChildren: true } ],
+ *     [ '%1$s', { tag: CustomComponentB, props: {}, hasChildren: false } ],
+ *     [ '%2$s', { value: 'custom value' } ]
+ * ]
+ * ```
+ *
+ * @param {string}  interpolatedString  The interpolation string to be parsed.
+ * @param {Array[]} conversionMap       The map used to convert the string to
+ *                                      a react element.
+ *
+ * @return {Element}  A react element.
+ */
+const createInterpolateElement = ( interpolatedString, conversionMap ) => {
+	resetTokenCount();
+	return createElement(
+		Fragment,
+		{},
+		recursiveCreateElement( interpolatedString, conversionMap )
+	);
+};
+
+export default createInterpolateElement;
---
+++ packages/element/src/test/create-interpolate-element.js
@@ -0,0 +1,209 @@
+/**
+ * Internal dependencies
+ */
+import { createElement, Fragment } from '../react';
+import createInterpolateElement from '../create-interpolate-element';
+
+describe( 'createInterpolateElement', () => {
+	it( 'returns same string when there is no conversion map', () => {
+		const testString = 'This is a string';
+		expect(
+			createInterpolateElement( testString, [] )
+		).toEqual( <Fragment>This is a string</Fragment> );
+	} );
+	it( 'returns same string when there are no tokens in the string', () => {
+		const testString = 'This is a string';
+		expect(
+			createInterpolateElement(
+				testString,
+				[ [ '%1$s', { value: 10 } ] ]
+			)
+		).toEqual( <Fragment>{ testString }</Fragment> );
+	} );
+	it( 'returns same string when there is an invalid conversion map', () => {
+		const testString = 'This is a %1$s string';
+		expect(
+			createInterpolateElement(
+				testString,
+				[ '%1$s', { value: 10 } ],
+			)
+		).toEqual( <Fragment>{ testString }</Fragment> );
+	} );
+	it( 'returns same string when there is an invalid token in the string', () => {
+		const testString = 'This is a %1$s string';
+		expect(
+			createInterpolateElement(
+				testString,
+				[ [ '%2$s', { value: 20 } ] ]
+			)
+		).toEqual( <Fragment>{ testString }</Fragment> );
+	} );
+	it( 'returns expected react element for non nested components', () => {
+		const testString = 'This is a string with <a%1>a link</a%1>.';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a string with ',
+				createElement(
+					'a',
+					{ href: 'https://github.com', className: 'some_class', key: 1 },
+					'a link'
+				),
+				'.',
+			]
+		);
+		expect( createInterpolateElement(
+			testString,
+			[
+				[
+					'a%1',
+					{
+						tag: 'a',
+						props: { href: 'https://github.com', className: 'some_class' },
+						hasChildren: true,
+					},
+				],
+			]
+		) ).toEqual( expectedElement );
+	} );
+	it( 'returns expected react element for nested components', () => {
+		const testString = 'This is a <a1>string that is <em1>linked</em1></a1>.';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a ',
+				createElement(
+					'a',
+					{ key: 1 },
+					[
+						'string that is ',
+						createElement(
+							'em',
+							{ key: 2 },
+							'linked'
+						),
+					]
+				),
+				'.',
+			]
+		);
+		expect( createInterpolateElement(
+			testString,
+			[
+				[ 'a1', { tag: 'a', props: {}, hasChildren: true } ],
+				[ 'em1', { tag: 'em', props: {}, hasChildren: true } ],
+			]
+		) ).toEqual( expectedElement );
+	} );
+	it( 'returns a value for a prop value type token replacement', () => {
+		const testString = 'This is a string with a value token: %1$s';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a string with a value token: ',
+				10,
+			]
+		);
+		expect( createInterpolateElement(
+			testString,
+			[ [ '%1$s', { value: 10 } ] ],
+		) ).toEqual( expectedElement );
+	} );
+	it( 'returns expected output for a custom component with children ' +
+		'replacement', () => {
+		const TestComponent = ( props ) => {
+			return <div { ...props } >{ props.children }</div>;
+		};
+		const testString = 'This is a string with a <span1>Custom Component</span1>';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a string with a ',
+				createElement(
+					TestComponent,
+					{ key: 1 },
+					'Custom Component'
+				),
+			]
+		);
+		expect( createInterpolateElement(
+			testString,
+			[
+				[ 'span1', { tag: TestComponent, props: {}, hasChildren: true } ],
+			]
+		) ).toEqual( expectedElement );
+	} );
+	it( 'returns expected output for self closing custom component', () => {
+		const TestComponent = ( props ) => {
+			return <div { ...props } />;
+		};
+		const testString = 'This is a string with a self closing custom component: <span1/>';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a string with a self closing custom component: ',
+				createElement(
+					TestComponent,
+					{ key: 1 }
+				),
+			]
+		);
+		expect( createInterpolateElement(
+			testString,
+			[
+				[ 'span1', { tag: TestComponent, props: {}, hasChildren: false } ],
+			]
+		) ).toEqual( expectedElement );
+	} );
+	it( 'returns expected output for complex replacement', () => {
+		const TestComponent = ( props ) => {
+			return <div { ...props } />;
+		};
+		const testString = 'This is a complex string having a %1$s value, with ' +
+			'a <a1>nested <em1>%2$s</em1> link</a1> and value: %3$s';
+		const expectedElement = createElement(
+			Fragment,
+			{},
+			[
+				'This is a complex string having a ',
+				'concrete',
+				' value, with a ',
+				createElement(
+					'a',
+					{ key: 1 },
+					[
+						'nested ',
+						createElement(
+							'em',
+							{ key: 2 },
+							'value'
+						),
+						' link',
+					]
+				),
+				' and value: ',
+				createElement(
+					Fragment,
+					{ key: 3 },
+					<TestComponent />,
+				),
+			]
+		);
+		expect( JSON.stringify( createInterpolateElement(
+			testString,
+			[
+				[ '%1$s', { value: 'concrete' } ],
+				[ 'a1', { tag: 'a', props: {}, hasChildren: true } ],
+				[ 'em1', { tag: 'em', props: {}, hasChildren: true } ],
+				[ '%2$s', { value: 'value' } ],
+				[ '%3$s', { value: <TestComponent /> } ],
+			]
+		) ) ).toEqual( JSON.stringify( expectedElement ) );
+	} );
+	// test complex multi types replacements.
+} );
