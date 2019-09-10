7b2e04ac9fccc33227862fb1377dfb05072c340d
retrofox <rdsuarez@gmail.com>
2019-09-10 13:48:16 -0400

    block-library: remove `expand`. Add `fullScreen`

--- packages/block-library/src/image/index.js
+++ packages/block-library/src/image/index.js
@@ -31,19 +31,11 @@ export const settings = {
 	],
 	transforms,
 	getEditWrapperProps( attributes ) {
-		const { align, width, expand } = attributes;
-		const editProps = {};
+		const { align, width } = attributes;
 
-		if ( 'left' === align || 'center' === align || 'right' === align || 'wide' === align || 'full' === align ) {
-			editProps[ 'data-align' ] = align;
-			editProps[ 'data-resized' ] = !! width;
+		if ( 'left' === align || 'center' === align || 'right' === align || 'wide' === align || 'full' === align || 'fullScreen' === align ) {
+			return { 'data-align': align, 'data-resized': !! width };
 		}
-
-		if ( 'screen' === expand ) {
-			editProps[ 'data-expand' ] = expand;
-		}
-
-		return editProps;
 	},
 	edit,
 	save,
