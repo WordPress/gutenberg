b612c32b7d1adf0989198ca0e008c04e59111bf6
epiqueras <epiquerass@gmail.com>
2019-09-20 08:37:53 -0700

    Post Featured Image: Render drop zone placeholder over drop target.

--- packages/editor/src/components/post-featured-image/index.js
+++ packages/editor/src/components/post-featured-image/index.js
@@ -81,7 +81,6 @@ function PostFeaturedImage( {
 						modalClass={ ! featuredImageId ? 'editor-post-featured-image__media-modal' : 'editor-post-featured-image__media-modal' }
 						render={ ( { open } ) => (
 							<>
-								<DropZone onFilesDrop={ ! featuredImageId && onDropImage } />
 								<Button
 									className={ ! featuredImageId ? 'editor-post-featured-image__toggle' : 'editor-post-featured-image__preview' }
 									onClick={ open }
@@ -98,6 +97,7 @@ function PostFeaturedImage( {
 									{ !! featuredImageId && ! media && <Spinner /> }
 									{ ! featuredImageId && ( postLabel.set_featured_image || DEFAULT_SET_FEATURE_IMAGE_LABEL ) }
 								</Button>
+								<DropZone onFilesDrop={ ! featuredImageId && onDropImage } />
 							</>
 						) }
 						value={ featuredImageId }
