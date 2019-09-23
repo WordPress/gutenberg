6a0f3fcac254751d24fab6a4133a0bca11cec892
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-09-23 17:05:18 +0300

    Removed social links from registerCoreBlocks.

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -130,8 +130,6 @@ export const registerCoreBlocks = () => {
 		search,
 		separator,
 		reusableBlock,
-		socialLinks,
-		...socialLink.sites,
 		spacer,
 		subhead,
 		table,
@@ -171,7 +169,7 @@ export const __experimentalRegisterExperimentalCoreBlocks = process.env.GUTENBER
 		__experimentalEnableLegacyWidgetBlock ? legacyWidget : null,
 		__experimentalEnableMenuBlock ? navigationMenu : null,
 		__experimentalEnableMenuBlock ? navigationMenuItem : null,
-		...socialLink.sites,
 		socialLinks,
+		...socialLink.sites,
 	].forEach( registerBlock );
 } : undefined;
