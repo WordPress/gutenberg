5b9237b8c07767699827ecc963805ee04c82c578
Matías Ventura <mv@matiasventura.com>
2019-09-23 15:19:48 +0200

    Add example for "Gallery" block.

--- packages/block-library/src/gallery/index.js
+++ packages/block-library/src/gallery/index.js
@@ -22,6 +22,15 @@ export const settings = {
 	description: __( 'Display multiple images in a rich gallery.' ),
 	icon,
 	keywords: [ __( 'images' ), __( 'photos' ) ],
+	example: {
+		attributes: {
+			columns: 2,
+			images: [
+				{ url: 'https://images.unsplash.com/photo-1506653291967-767803668ace' },
+				{ url: 'https://images.unsplash.com/photo-1505764761634-1d77b57e1966' },
+			],
+		},
+	},
 	supports: {
 		align: true,
 	},
