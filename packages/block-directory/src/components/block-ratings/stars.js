15e381cb6848cb05cd91fec0c499c5b36fd9f609
Sören Wrede <soerenwrede@gmail.com>
2019-09-25 22:36:49 +0200

    Remove text domain (#17546)

--- packages/block-directory/src/components/block-ratings/stars.js
+++ packages/block-directory/src/components/block-ratings/stars.js
@@ -19,7 +19,7 @@ function Stars( {
 	const emptyStarCount = 5 - ( fullStarCount + halfStarCount );
 
 	return (
-		<div aria-label={ sprintf( __( '%s out of 5 stars', stars ), stars ) }>
+		<div aria-label={ sprintf( __( '%s out of 5 stars' ), stars ) }>
 			{ times( fullStarCount, ( i ) => <Icon key={ `full_stars_${ i }` } icon="star-filled" size={ 16 } /> ) }
 			{ times( halfStarCount, ( i ) => <Icon key={ `half_stars_${ i }` } icon="star-half" size={ 16 } /> ) }
 			{ times( emptyStarCount, ( i ) => <Icon key={ `empty_stars_${ i }` } icon="star-empty" size={ 16 } /> ) }
