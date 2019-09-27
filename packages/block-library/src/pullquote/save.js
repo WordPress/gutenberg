28b17e091821ac11866c27e961c989bca26b2dac
Jorge Costa <jorge.costa@automattic.com>
2019-09-27 18:39:02 +0100

    Classnames should return undefined instead of empty strings (#17529)
    Squashed commits:
    [9d994f516] Fix e2e test failure in pullquote block
    [c8b5cd37f] Support `has-background` class on solid pullquote style
    [e42e68420] Only render inline `style` when a custom color is set
    [1b7bc98e6] Add `has-background` class to Media and Text block

--- packages/block-library/src/pullquote/save.js
+++ packages/block-library/src/pullquote/save.js
@@ -22,25 +22,39 @@ import {
 import { SOLID_COLOR_CLASS } from './shared';
 
 export default function save( { attributes } ) {
-	const { mainColor, customMainColor, textColor, customTextColor, value, citation, className } = attributes;
+	const {
+		mainColor,
+		customMainColor,
+		textColor,
+		customTextColor,
+		value,
+		citation,
+		className,
+	} = attributes;
+
 	const isSolidColorStyle = includes( className, SOLID_COLOR_CLASS );
 
-	let figureClass, figureStyles;
+	let figureClasses, figureStyles;
+
 	// Is solid color style
 	if ( isSolidColorStyle ) {
-		figureClass = getColorClassName( 'background-color', mainColor );
-		if ( ! figureClass ) {
-			figureStyles = {
-				backgroundColor: customMainColor,
-			};
-		}
-		// Is normal style and a custom color is being used ( we can set a style directly with its value)
+		const backgroundClass = getColorClassName( 'background-color', mainColor );
+
+		figureClasses = classnames( {
+			'has-background': ( backgroundClass || customMainColor ),
+			[ backgroundClass ]: backgroundClass,
+		} );
+
+		figureStyles = {
+			backgroundColor: backgroundClass ? undefined : customMainColor,
+		};
+	// Is normal style and a custom color is being used ( we can set a style directly with its value)
 	} else if ( customMainColor ) {
 		figureStyles = {
 			borderColor: customMainColor,
 		};
-		// Is normal style and a named color is being used, we need to retrieve the color value to set the style,
-		// as there is no expectation that themes create classes that set border colors.
+	// If normal style and a named color are being used, we need to retrieve the color value to set the style,
+	// as there is no expectation that themes create classes that set border colors.
 	} else if ( mainColor ) {
 		const colors = get( select( 'core/block-editor' ).getSettings(), [ 'colors' ], [] );
 		const colorObject = getColorObjectByAttributeValues( colors, mainColor );
@@ -50,13 +64,15 @@ export default function save( { attributes } ) {
 	}
 
 	const blockquoteTextColorClass = getColorClassName( 'color', textColor );
-	const blockquoteClasses = textColor || customTextColor ? classnames( 'has-text-color', {
+	const blockquoteClasses = ( textColor || customTextColor ) && classnames( 'has-text-color', {
 		[ blockquoteTextColorClass ]: blockquoteTextColorClass,
-	} ) : undefined;
-	const blockquoteStyle = blockquoteTextColorClass ? undefined : { color: customTextColor };
+	} );
+
+	const blockquoteStyles = blockquoteTextColorClass ? undefined : { color: customTextColor };
+
 	return (
-		<figure className={ figureClass } style={ figureStyles }>
-			<blockquote className={ blockquoteClasses } style={ blockquoteStyle } >
+		<figure className={ figureClasses } style={ figureStyles }>
+			<blockquote className={ blockquoteClasses } style={ blockquoteStyles } >
 				<RichText.Content value={ value } multiline />
 				{ ! RichText.isEmpty( citation ) && <RichText.Content tagName="cite" value={ citation } /> }
 			</blockquote>
