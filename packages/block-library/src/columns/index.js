b5fe68187f94c0a204a336825e7c6aa6042c48aa
Riad Benguella <benguella@gmail.com>
2019-09-30 16:47:38 +0100

    Add block examples for all core blocks (#17493)

--- packages/block-library/src/columns/index.js
+++ packages/block-library/src/columns/index.js
@@ -24,8 +24,51 @@ export const settings = {
 		align: [ 'wide', 'full' ],
 		html: false,
 	},
+	example: {
+		innerBlocks: [
+			{
+				name: 'core/column',
+				innerBlocks: [
+					{
+						name: 'core/paragraph',
+						attributes: {
+							content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis pellentesque efficitur. Nam dapibus felis malesuada tincidunt rhoncus. Integer non malesuada tortor.',
+						},
+					},
+					{
+						name: 'core/image',
+						attributes: {
+							url: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Windbuchencom.jpg',
+						},
+					},
+					{
+						name: 'core/paragraph',
+						attributes: {
+							content: 'Suspendisse commodo neque lacus, a dictum orci interdum et. Ut vel mi ut leo fringilla rutrum.',
+						},
+					},
+				],
+			},
+			{
+				name: 'core/column',
+				innerBlocks: [
+					{
+						name: 'core/paragraph',
+						attributes: {
+							content: __( 'Etiam et egestas lorem. Vivamus sagittis sit amet dolor quis lobortis. Integer sed fermentum arcu, id vulputate lacus. Etiam fermentum sem eu quam hendrerit, eget faucibus urna pulvinar.' ),
+						},
+					},
+					{
+						name: 'core/paragraph',
+						attributes: {
+							content: __( 'Nam risus massa, ullamcorper consectetur eros fermentum, porta aliquet ligula. Sed vel mauris nec enim ultricies commodo.' ),
+						},
+					},
+				],
+			},
+		],
+	},
 	deprecated,
 	edit,
 	save,
 };
-
