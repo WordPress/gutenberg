5aafa7d0448187744f705ad1f3efb3bfe3b6b7fd
Jakub Binda <jakub.binda@gmail.com>
2019-10-01 11:46:19 +0200

    refactor bottomsheet controls

---
+++ packages/components/src/toggle-control/index.native.js
@@ -0,0 +1,36 @@
+/**
+ * WordPress dependencies
+ */
+// import { withInstanceId } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import SwitchCell from '../mobile/bottom-sheet/switch-cell';
+
+function ToggleControl( {
+	label,
+	checked,
+	help,
+	instanceId,
+	className,
+	onChange,
+	...props
+} ) {
+	const id = `inspector-toggle-control-${ instanceId }`;
+
+	return (
+		<SwitchCell
+			label={ label }
+			id={ id }
+			help={ help }
+			className={ className }
+			value={ checked }
+			onValueChange={ onChange }
+			aria-describedby={ !! help ? id + '__help' : undefined }
+			{ ...props }
+		/>
+	);
+}
+
+export default ToggleControl;
