b23d6f9d9f775a11e1404a9fedec2e72a785d98f
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2019-10-01 09:55:02 +0200

    Update index.native.js

--- packages/format-library/src/link/index.native.js
+++ packages/format-library/src/link/index.native.js
@@ -96,7 +96,7 @@ export const link = {
 		onRemoveFormat() {
 			const { onChange, speak, value } = this.props;
 			const startFormat = getActiveFormat( value, 'core/link' );
-			
+
 			// Before we try to remove anything we check if there is something at the caret position to remove.
 			if ( isCollapsed( value ) && startFormat === undefined ) {
 				return;
