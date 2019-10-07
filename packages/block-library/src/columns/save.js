36f3863ae3577d9e4563f2125c61f1a96b92874f
Rich Tabor <hi@richtabor.com>
2019-10-07 10:25:45 -0400

    Update save function

--- packages/block-library/src/columns/save.js
+++ packages/block-library/src/columns/save.js
@@ -6,17 +6,23 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { InnerBlocks } from '@wordpress/block-editor';
+import { InnerBlocks, getColorClassName } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { verticalAlignment } = attributes;
+	const { verticalAlignment, backgroundColor, customBackgroundColor } = attributes;
 
-	const wrapperClasses = classnames( {
+	const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+	const wrapperClasses = classnames( backgroundClass, {
+		'has-background': backgroundColor || customBackgroundColor,
 		[ `are-vertically-aligned-${ verticalAlignment }` ]: verticalAlignment,
 	} );
 
+	const styles = {
+		backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+	};
+
 	return (
-		<div className={ wrapperClasses }>
+		<div className={ wrapperClasses } style={ styles }>
 			<InnerBlocks.Content />
 		</div>
 	);
