816e3ed329767851134467bc220bbc79d29b99a0
Jorge Bernal <jorge@automattic.com>
2019-10-08 12:00:14 +0200

    Use snapshots for Image tests

--- packages/components/src/image/test/index.native.js
+++ packages/components/src/image/test/index.native.js
@@ -16,14 +16,11 @@ describe( 'Image', () => {
 
 	it( 'renders an img tag with src and alt', () => {
 		const image = shallow( <Image src="https://s.w.org/style/images/about/WordPress-logotype-wmark.png" alt="WordPress logo" /> );
-		expect( image.type().displayName ).toBe( 'Image' );
-		expect( image.prop( 'source' ) ).toMatchObject( { uri: 'https://s.w.org/style/images/about/WordPress-logotype-wmark.png' } );
-		expect( image.prop( 'accessibilityLabel' ) ).toBe( 'WordPress logo' );
+		expect( image ).toMatchSnapshot();
 	} );
 
 	it( 'renders an img tag passing any additional props', () => {
 		const image = shallow( <Image src="https://s.w.org/style/images/about/WordPress-logotype-wmark.png" alt="WordPress logo" data-id="123" /> );
-		expect( image.type().displayName ).toBe( 'Image' );
-		expect( image.prop( 'data-id' ) ).toBe( '123' );
+		expect( image ).toMatchSnapshot();
 	} );
 } );
