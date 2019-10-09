53ce51782ceeec748a6e290d5b7c240d00f890ff
Dave Smith <getdavemail@gmail.com>
2019-10-09 14:28:53 +0100

    Update to add post type to the fetchLinkSuggestions responsive mapping
    This is required to display the type of entitity in the search results for LinkControl

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -40,6 +40,7 @@ const fetchLinkSuggestions = async ( search ) => {
 		id: post.id,
 		url: post.url,
 		title: decodeEntities( post.title ) || __( '(no title)' ),
+		type: post.subtype || post.type,
 	} ) );
 };
 
