d738b6207d81b2bb28effef83e59278aa34d950b
lukewalczak <lukasz.walczak.pwr@gmail.com>
2019-10-09 09:45:30 +0200

    Remove overflow to have SafeArea over the BlockList

--- packages/components/src/mobile/keyboard-aware-flat-list/index.ios.js
+++ packages/components/src/mobile/keyboard-aware-flat-list/index.ios.js
@@ -12,7 +12,7 @@ export const KeyboardAwareFlatList = ( {
 	...listProps
 } ) => (
 	<KeyboardAwareScrollView
-		style={ { flex: 1, overflow: 'visible' } }
+		style={ { flex: 1 } }
 		keyboardDismissMode="none"
 		enableResetScrollToCoords={ false }
 		keyboardShouldPersistTaps="handled"
