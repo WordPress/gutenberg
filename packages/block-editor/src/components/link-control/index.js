d554b65ec4b0f9032d76fdac148793771c024339
Dave Smith <getdavemail@gmail.com>
2019-10-10 12:18:34 +0100

    Adds overflow scrolling to search results

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -74,24 +74,26 @@ function LinkControl( { defaultOpen = false, fetchSearchSuggestions, renderAddit
 	const renderSearchResults = ( { suggestionsListProps, buildSuggestionItemProps, suggestions, selectedSuggestion, handleSuggestionClick } ) => {
 		/* eslint-disable react/jsx-key */
 		return (
-			<div { ...suggestionsListProps } className="block-editor-link-control__search-results">
-				{ suggestions.map( ( suggestion, index ) => (
-					<button
-						{ ...buildSuggestionItemProps( suggestion, index ) }
-						onClick={ () => handleSuggestionClick( suggestion ) }
-						className={ classnames( 'block-editor-link-control__search-item', {
-							'is-selected': index === selectedSuggestion,
-						} ) }
+			<div className="block-editor-link-control__search-results-wrapper">
+				<div { ...suggestionsListProps } className="block-editor-link-control__search-results">
+					{ suggestions.map( ( suggestion, index ) => (
+						<button
+							{ ...buildSuggestionItemProps( suggestion, index ) }
+							onClick={ () => handleSuggestionClick( suggestion ) }
+							className={ classnames( 'block-editor-link-control__search-item', {
+								'is-selected': index === selectedSuggestion,
+							} ) }
 
-					>
-						<Icon className="block-editor-link-control__search-item-icon" icon="wordpress" />
-						<span className="block-editor-link-control__search-item-header">
-							<span className="block-editor-link-control__search-item-title">{ suggestion.title }</span>
-							<span className="block-editor-link-control__search-item-info">{ suggestion.info || suggestion.url || '' }</span>
-						</span>
-						<span className="block-editor-link-control__search-item-type">{ suggestion.type.toLowerCase() || '' }</span>
-					</button>
-				) ) }
+						>
+							<Icon className="block-editor-link-control__search-item-icon" icon="wordpress" />
+							<span className="block-editor-link-control__search-item-header">
+								<span className="block-editor-link-control__search-item-title">{ suggestion.title }</span>
+								<span className="block-editor-link-control__search-item-info">{ suggestion.info || suggestion.url || '' }</span>
+							</span>
+							<span className="block-editor-link-control__search-item-type">{ suggestion.type.toLowerCase() || '' }</span>
+						</button>
+					) ) }
+				</div>
 			</div>
 		);
 		/* eslint-enable react/jsx-key */
