b39d864da38f84de560f5c9944738ba41b0f289f
Haz <hazdiego@gmail.com>
2019-10-11 19:52:44 -0600

    Support current ToolbarButton usage

--- packages/components/src/toolbar-button/index.js
+++ packages/components/src/toolbar-button/index.js
@@ -4,10 +4,17 @@
 import classnames from 'classnames';
 
 /**
+ * WordPress dependencies
+ */
+import { useContext } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
 import IconButton from '../icon-button';
+import ToolbarContext from '../toolbar-context';
 import AccessibleToolbarButton from './accessible-toolbar-button';
+import ToolbarButtonContainer from './toolbar-button-container';
 
 function ToolbarButton( {
 	containerClassName,
@@ -22,28 +29,41 @@ function ToolbarButton( {
 	extraProps,
 	children,
 } ) {
+	const context = useContext( ToolbarContext );
+	const button = (
+		<IconButton
+			icon={ icon }
+			label={ title }
+			shortcut={ shortcut }
+			data-subscript={ subscript }
+			onClick={ ( event ) => {
+				event.stopPropagation();
+				onClick();
+			} }
+			className={ classnames(
+				'components-toolbar__control',
+				className,
+				{ 'is-active': isActive }
+			) }
+			aria-pressed={ isActive }
+			disabled={ isDisabled }
+			{ ...extraProps }
+		/>
+	);
+
+	if ( context ) {
+		return (
+			<AccessibleToolbarButton className={ containerClassName }>
+				{ button }
+			</AccessibleToolbarButton>
+		);
+	}
+
 	return (
-		<AccessibleToolbarButton className={ containerClassName }>
-			<IconButton
-				icon={ icon }
-				label={ title }
-				shortcut={ shortcut }
-				data-subscript={ subscript }
-				onClick={ ( event ) => {
-					event.stopPropagation();
-					onClick();
-				} }
-				className={ classnames(
-					'components-toolbar__control',
-					className,
-					{ 'is-active': isActive }
-				) }
-				aria-pressed={ isActive }
-				disabled={ isDisabled }
-				{ ...extraProps }
-			/>
+		<ToolbarButtonContainer className={ containerClassName }>
+			{ button }
 			{ children }
-		</AccessibleToolbarButton>
+		</ToolbarButtonContainer>
 	);
 }
 
