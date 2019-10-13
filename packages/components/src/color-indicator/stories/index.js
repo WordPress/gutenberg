fbe2a56a549869506d98349d66efb652a4066baf
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-13 07:39:26 -0700

    Add story for ColorIndicator component
    - Adds story for component
    - Change example color to WordPress blue (#0073AA) instead of red

---
+++ packages/components/src/color-indicator/stories/index.js
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies
+ */
+import ColorIndicator from '../';
+
+export default { title: 'Color Indicator', component: ColorIndicator };
+
+export const _default = () => (
+	<>
+		<ColorIndicator colorValue="#0073AA" />
+	</>
+);
