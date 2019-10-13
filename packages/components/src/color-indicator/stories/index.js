9464eaadd54bbf4a7ef1d6419cc3d82023fca28f
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-13 09:51:33 -0700

    Use Storybook info addon, show source inline

--- packages/components/src/color-indicator/stories/index.js
+++ packages/components/src/color-indicator/stories/index.js
@@ -3,10 +3,21 @@
  */
 import ColorIndicator from '../';
 
-export default { title: 'Color Indicator', component: ColorIndicator };
+export default {
+	title: 'Color Indicator',
+	component: ColorIndicator,
+	source: true,
+	parameters: {
+		info: 'Default notes',
+	},
+};
 
 export const _default = () => (
-	<>
-		<ColorIndicator colorValue="#0073AA" />
-	</>
+	<ColorIndicator colorValue="#0073AA" />
 );
+
+_default.story = {
+	parameters: {
+		info: 'Color indicator using WordPress blue #0073AA',
+	},
+};
