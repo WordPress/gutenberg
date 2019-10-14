ac797eac930ef166ccfc10865593f6c7b0f6546c
jasmussen <joen@automattic.com>
2019-10-14 15:22:02 +0200

    Encode comma.

--- packages/block-library/src/gallery/index.js
+++ packages/block-library/src/gallery/index.js
@@ -26,7 +26,7 @@ export const settings = {
 		attributes: {
 			columns: 2,
 			images: [
-				{ url: 'https://s.w.org/images/core/5.3/Glacial_lakes,_Bhutan.jpg' },
+				{ url: 'https://s.w.org/images/core/5.3/Glacial_lakes%2C_Bhutan.jpg' },
 				{ url: 'https://s.w.org/images/core/5.3/Sediment_off_the_Yucatan_Peninsula.jpg' },
 			],
 		},
