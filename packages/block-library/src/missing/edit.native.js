e7d8e59d557e3da23e42582552f5f30e7c7e405d
Wendy Chen <tilphousia@gmail.com>
2019-10-14 07:07:54 -0400

    WC: #479: Adding help icon and modal to unsupported block. Also putting original name in place of 'Unsupported'.

--- packages/block-library/src/missing/edit.native.js
+++ packages/block-library/src/missing/edit.native.js
@@ -1,12 +1,12 @@
 /**
  * External dependencies
  */
-import { View, Text } from 'react-native';
+import { Platform, View, Text } from 'react-native';
 
 /**
  * WordPress dependencies
  */
-import { Icon } from '@wordpress/components';
+import { BottomSheet, Button, Icon, IconButton } from '@wordpress/components';
 import { withPreferredColorScheme } from '@wordpress/compose';
 import { coreBlocks } from '@wordpress/block-library';
 import { normalizeIconObject } from '@wordpress/blocks';
@@ -18,13 +18,61 @@ import { __ } from '@wordpress/i18n';
  */
 import styles from './style.scss';
 
+const isAndroid = Platform.OS === 'android';
+const platformText = isAndroid ? 'Android' : 'iOS';
 export class UnsupportedBlockEdit extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = { showHelp: false };
+	}
+
+	toggleSheet() {
+		this.setState( {
+			showHelp: ! this.state.showHelp,
+		} );
+	}
+
+	renderHelpIcon() {
+		return <View style={ styles.helpIconContainer } >
+			<IconButton
+				className="unsupported-icon-help"
+				label={ __( 'Help icon' ) }
+				onClick={ this.toggleSheet.bind( this ) }
+				icon="editor-help"
+			/>
+		</View>;
+	}
+
+	renderSheet() {
+		return <BottomSheet
+			isVisible={ this.state.showHelp }
+			hideHeader={ true }
+			onClose={ this.toggleSheet.bind( this ) }
+			contentStyle={ styles.content }
+		>
+			<Icon icon="info-outline" style={ styles.infoIcon } size={ styles.infoIcon.size } />
+			<Text style={ styles.infoTitle }>
+				{ __( 'This block isn\'t yet supported on WordPress for ' + platformText ) }
+			</Text>
+			<Button
+				onClick={ this.toggleSheet.bind( this ) }
+				fixedRatio={ false }
+			>
+				<View style={ styles.infoCloseButton } >
+					<Text style={ styles.infoCloseButtonText } >
+						{ __( 'Close' ) }
+					</Text>
+				</View>
+			</Button>
+		</BottomSheet>;
+	}
+
 	render() {
 		const { originalName } = this.props.attributes;
 		const { getStylesFromColorScheme, preferredColorScheme } = this.props;
 		const blockType = coreBlocks[ originalName ];
 
-		const title = blockType ? blockType.settings.title : __( 'Unsupported' );
+		const title = blockType ? blockType.settings.title : originalName;
 		const titleStyle = getStylesFromColorScheme( styles.unsupportedBlockMessage, styles.unsupportedBlockMessageDark );
 
 		const icon = blockType ? normalizeIconObject( blockType.settings.icon ) : 'admin-plugins';
@@ -32,8 +80,10 @@ export class UnsupportedBlockEdit extends Component {
 		const iconClassName = 'unsupported-icon' + '-' + preferredColorScheme;
 		return (
 			<View style={ getStylesFromColorScheme( styles.unsupportedBlock, styles.unsupportedBlockDark ) }>
+				{ this.renderHelpIcon() }
 				<Icon className={ iconClassName } icon={ icon && icon.src ? icon.src : icon } color={ iconStyle.color } />
 				<Text style={ titleStyle }>{ title }</Text>
+				{ this.renderSheet() }
 			</View>
 		);
 	}
