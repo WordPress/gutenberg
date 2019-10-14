b565488f6c4725295819808cb6410254b46d8646
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-13 19:30:14 -0700

    Storybook: Add storybook source display and ColorIndicator component (#17924)
    * Add story for ColorIndicator component
    
    - Adds story for component
    - Change example color to WordPress blue (#0073AA) instead of red
    
    * Use Storybook info addon, show source inline
    
    * Switch to using storybook source, instead of info to show code
    
    * Remove extra storybook packages
    
    * Refresh packages
    
    * Improve match for source-loader
    
    * Apply suggestions from code review

---
+++ packages/components/src/color-indicator/stories/index.js
@@ -0,0 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import ColorIndicator from '../';
+
+export default {
+	title: 'Color Indicator',
+	component: ColorIndicator,
+};
+
+export const _default = () => (
+	<ColorIndicator colorValue="#0073aa" />
+);
