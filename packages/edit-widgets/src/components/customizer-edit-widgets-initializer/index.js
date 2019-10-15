f6b31330aa123edb59fd8aae2398ab1d83575bd5
Jorge <jorge.costa@developer.pt>
2019-10-15 19:30:56 +0100

    Use mobile UI in customizer

--- packages/edit-widgets/src/components/customizer-edit-widgets-initializer/index.js
+++ packages/edit-widgets/src/components/customizer-edit-widgets-initializer/index.js
@@ -7,6 +7,8 @@ import {
 	navigateRegions,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { ViewportMatchWidthProvider } from '@wordpress/viewport';
+import { __experimentalSimulateMediaQuery } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -15,19 +17,35 @@ import WidgetAreas from '../widget-areas';
 
 import './sync-customizer';
 
+const PARTIAL_PATHS = [
+	'block-editor/style.css',
+	'block-library/style.css',
+	'block-library/theme.css',
+	'block-library/editor.css',
+	'format-library/style.css',
+];
+
+const CUSTOMIZER_PANEL_WIDTH = 479;
+
 function CustomizerEditWidgetsInitializer( { settings } ) {
 	return (
-		<SlotFillProvider>
-			<div
-				className="edit-widgets-customizer-edit-widgets-initializer__content"
-				role="region"
-				aria-label={ __( 'Widgets screen content' ) }
-				tabIndex="-1"
-			>
-				<WidgetAreas blockEditorSettings={ settings } />
-			</div>
-			<Popover.Slot />
-		</SlotFillProvider>
+		<ViewportMatchWidthProvider width={ CUSTOMIZER_PANEL_WIDTH }>
+			<__experimentalSimulateMediaQuery
+				value={ CUSTOMIZER_PANEL_WIDTH }
+				partialPaths={ PARTIAL_PATHS }
+			/>
+			<SlotFillProvider>
+				<div
+					className="edit-widgets-customizer-edit-widgets-initializer__content"
+					role="region"
+					aria-label={ __( 'Widgets screen content' ) }
+					tabIndex="-1"
+				>
+					<WidgetAreas blockEditorSettings={ settings } />
+				</div>
+				<Popover.Slot />
+			</SlotFillProvider>
+		</ViewportMatchWidthProvider>
 	);
 }
 
