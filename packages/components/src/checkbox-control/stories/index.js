a7ab3c5b2122e9935afc1e48edd3dd72d349d86f
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-14 17:32:09 -0700

    Add variants for heading, label, help

--- packages/components/src/checkbox-control/stories/index.js
+++ packages/components/src/checkbox-control/stories/index.js
@@ -14,6 +14,49 @@ export const _default = () => {
 	const [ isChecked, setChecked ] = useState( true );
 	return (
 		<CheckboxControl
+			checked={ isChecked }
+			onChange={ setChecked }
+		/>
+	);
+};
+
+export const Heading = () => {
+	const [ isChecked, setChecked ] = useState( true );
+	return (
+		<CheckboxControl
+			heading="User"
+			checked={ isChecked }
+			onChange={ setChecked }
+		/>
+	);
+};
+
+export const Label = () => {
+	const [ isChecked, setChecked ] = useState( true );
+	return (
+		<CheckboxControl
+			label="Is author"
+			checked={ isChecked }
+			onChange={ setChecked }
+		/>
+	);
+};
+
+export const Help = () => {
+	const [ isChecked, setChecked ] = useState( true );
+	return (
+		<CheckboxControl
+			help="Is the user a author or not?"
+			checked={ isChecked }
+			onChange={ setChecked }
+		/>
+	);
+};
+
+export const All = () => {
+	const [ isChecked, setChecked ] = useState( true );
+	return (
+		<CheckboxControl
 			heading="User"
 			label="Is author"
 			help="Is the user a author or not?"
