bcf27f1c3a6c3f76c864868646b87e262d5b3e84
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-14 17:02:32 -0700

    Update story to match README

--- packages/components/src/checkbox-control/stories/index.js
+++ packages/components/src/checkbox-control/stories/index.js
@@ -11,13 +11,14 @@ import CheckboxControl from '../';
 export default { title: 'Checkbox Control', component: CheckboxControl };
 
 export const _default = () => {
-	const [ isChecked, setState ] = useState( true );
+	const [ isChecked, setChecked ] = useState( true );
 	return (
 		<CheckboxControl
-			label="Allow Comments"
-			help="Allow comments on post or not?"
+			heading="User"
+			label="Is author"
+			help="Is the user a author or not?"
 			checked={ isChecked }
-			onChange={ ( val ) => setState( val ) }
+			onChange={ setChecked }
 		/>
 	);
 };
