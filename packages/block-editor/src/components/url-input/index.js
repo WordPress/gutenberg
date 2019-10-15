7eef574b11faece7d17b0838388b17863814900f
Gary <gary@pento.net>
2019-10-15 21:08:16 +1100

    Remove the id attribute from URLInput
    This also removes its usage from the Button component.

--- packages/block-editor/src/components/url-input/index.js
+++ packages/block-editor/src/components/url-input/index.js
@@ -273,7 +273,7 @@ class URLInput extends Component {
 			selectedSuggestion,
 			loading,
 		} = this.state;
-		const baseControlId = `url-input-control-${ instanceId }`;
+		const id = `url-input-control-${ instanceId }`;
 
 		const suggestionsListboxId = `block-editor-url-input-suggestions-${ instanceId }`;
 		const suggestionOptionIdPrefix = `block-editor-url-input-suggestion-${ instanceId }`;
@@ -298,7 +298,7 @@ class URLInput extends Component {
 		return (
 			<BaseControl
 				label={ label }
-				id={ baseControlId }
+				id={ id }
 				className={ classnames( 'editor-url-input block-editor-url-input', className, {
 					'is-full-width': isFullWidth,
 					'has-border': hasBorder,
