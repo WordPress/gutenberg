b50391d3fd5201e72943cd78863a9ff9142431de
Marcus Kazmierczak <marcus@mkaz.com>
2019-10-16 01:35:10 -0700

    Storybook: Add stories for Checkbox control component (#17891)
    * Add checkbox control stories for Storybook
    
    * Update README example to match story, useState
    
    * Apply suggestions from code review
    
    👍
    
    Co-Authored-By: Enrique Piqueras <epiqueras@users.noreply.github.com>
    
    * Update story to match README
    
    * Add variants for heading, label, help
    
    * Update packages/components/src/checkbox-control/README.md
    
    Co-Authored-By: Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
    
    * Add Knobs addon to Storybook
    
    * Move storybook addon to dev-dependencies
    
    * Solve lint dependency by excluding stories, dont need in package.json
    
    * Apply suggestions from code review
    
    Co-Authored-By: Enrique Piqueras <epiqueras@users.noreply.github.com>
    
    * Update with story level decorators
    
    * Switch back to global withKnobs, per story not working
    
    * Change the name of the example in ChecboxControl story
    
    * Try with the uppercase name of the component exported from stories

--- packages/components/storybook/addons.js
+++ packages/components/storybook/addons.js
@@ -2,5 +2,6 @@
  * External dependencies
  */
 import '@storybook/addon-a11y/register';
+import '@storybook/addon-knobs/register';
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
--- packages/components/storybook/config.js
+++ packages/components/storybook/config.js
@@ -3,13 +3,14 @@
  */
 import { addDecorator, configure } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
-
+import { withKnobs } from '@storybook/addon-knobs';
 /**
  * Internal dependencies
  */
 import '../build-style/style.css';
 
 addDecorator( withA11y );
+addDecorator( withKnobs );
 configure(
 	[
 		require.context( '../docs', true, /\/.+\.mdx$/ ),
