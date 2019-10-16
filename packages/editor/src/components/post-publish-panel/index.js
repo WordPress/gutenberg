98ca5990ac95d0c45c09db9560d516c778d42518
Mapk <uraine@gmail.com>
2019-10-16 11:48:20 -0700

    Removed spacer and adjusted height of button. Centered button as well so should work better with translations. Less code. More button.

--- packages/editor/src/components/post-publish-panel/index.js
+++ packages/editor/src/components/post-publish-panel/index.js
@@ -75,7 +75,6 @@ export class PostPublishPanel extends Component {
 					) : (
 						<div className="editor-post-publish-panel__header-publish-button">
 							<PostPublishButton focusOnMount={ true } onSubmit={ this.onSubmit } forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />
-							<span className="editor-post-publish-panel__spacer"></span>
 						</div>
 					) }
 					<IconButton
