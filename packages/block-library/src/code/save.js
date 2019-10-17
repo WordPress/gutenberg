e043fdf83085f44a7365d3e1990fcec8fd90755c
iseulde <wp@iseulde.com>
2019-10-17 14:05:35 +0200

    Adjust code block escape util

--- packages/block-library/src/code/save.js
+++ packages/block-library/src/code/save.js
@@ -1,13 +1,8 @@
 /**
- * WordPress dependencies
- */
-import { escapeEditableHTML } from '@wordpress/escape-html';
-
-/**
  * Internal dependencies
  */
 import { escape } from './utils';
 
 export default function save( { attributes } ) {
-	return <pre><code>{ escape( escapeEditableHTML( attributes.content ) ) }</code></pre>;
+	return <pre><code>{ escape( attributes.content ) }</code></pre>;
 }
--- packages/block-library/src/code/utils.js
+++ packages/block-library/src/code/utils.js
@@ -4,6 +4,11 @@
 import { flow } from 'lodash';
 
 /**
+ * WordPress dependencies
+ */
+import { escapeEditableHTML } from '@wordpress/escape-html';
+
+/**
  * Escapes ampersands, shortcodes, and links.
  *
  * @param {string} content The content of a code block.
@@ -11,21 +16,9 @@ import { flow } from 'lodash';
  */
 export function escape( content ) {
 	return flow(
+		escapeEditableHTML,
 		escapeOpeningSquareBrackets,
 		escapeProtocolInIsolatedUrls
-	)( content || '' );
-}
-
-/**
- * Unescapes escaped ampersands, shortcodes, and links.
- *
- * @param {string} content Content with (maybe) escaped ampersands, shortcodes, and links.
- * @return {string} The given content with escaped characters unescaped.
- */
-export function unescape( content ) {
-	return flow(
-		unescapeProtocolInIsolatedUrls,
-		unescapeOpeningSquareBrackets,
 	)( content || '' );
 }
 
@@ -47,16 +40,6 @@ function escapeOpeningSquareBrackets( content ) {
 }
 
 /**
- * Returns the given content translating all &#91; into [.
- *
- * @param {string}  content The content of a code block.
- * @return {string} The given content with all &#91; into [.
- */
-function unescapeOpeningSquareBrackets( content ) {
-	return content.replace( /&#91;/g, '[' );
-}
-
-/**
  * Converts the first two forward slashes of any isolated URL into their HTML
  * counterparts (i.e. // => &#47;&#47;). For instance, https://youtube.com/watch?x
  * becomes https:&#47;&#47;youtube.com/watch?x.
@@ -72,21 +55,4 @@ function unescapeOpeningSquareBrackets( content ) {
  */
 function escapeProtocolInIsolatedUrls( content ) {
 	return content.replace( /^(\s*https?:)\/\/([^\s<>"]+\s*)$/m, '$1&#47;&#47;$2' );
-}
-
-/**
- * Converts the first two forward slashes of any isolated URL from the HTML entity
- * &#73; into /.
- *
- * An isolated URL is a URL that sits in its own line, surrounded only by spacing
- * characters.
- *
- * See https://github.com/WordPress/wordpress-develop/blob/5.1.1/src/wp-includes/class-wp-embed.php#L403
- *
- * @param {string}  content The content of a code block.
- * @return {string} The given content with the first two forward slashes of any
- *                  isolated URL from the HTML entity &#73; into /.
- */
-function unescapeProtocolInIsolatedUrls( content ) {
-	return content.replace( /^(\s*https?:)&#47;&#47;([^\s<>"]+\s*)$/m, '$1//$2' );
 }
