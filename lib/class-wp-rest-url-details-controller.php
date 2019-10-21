bb289dbfbcae392a27e5f000793a6a181b0880cc
Dave Smith <getdavemail@gmail.com>
2019-10-21 10:11:46 +0100

    i18n fixes and docblocks

--- lib/class-wp-rest-url-details-controller.php
+++ lib/class-wp-rest-url-details-controller.php
@@ -7,7 +7,8 @@
  */
 
 /**
- * Controller which provides REST endpoint for the widget areas.
+ * Controller which provides REST endpoint for retrieving information
+ * from a remote site's HTML response.
  *
  * @since 5.?.0
  *
@@ -57,16 +58,22 @@ class WP_REST_URL_Details_Controller extends WP_REST_Controller {
 
 
 
-
+	/**
+	 * Retrieves the contents of the <title> tag from the HTML
+	 * response.
+	 *
+	 * @access public
+	 * @param  WP_REST_REQUEST $request Full details about the request.
+	 * @return String|WP_Error          the title text or an error.
+	 */
 	public function get_title( $request ) {
 
-		// TODO: Sanitize and validate
 		$url = $request->get_param( 'url' );
 
 		$html_response = $this->get_remote_url_html( $url );
 
 		if ( is_wp_error( $html_response ) ) {
-			return new WP_Error( 'no_title', 'Unable to retrieve title tag. ' . $html_response->get_error_message(), array( 'status' => 404 ) );
+			return new WP_Error( 'no_title', __( 'Unable to retrieve title tag.', 'gutenberg' ) . $html_response->get_error_message(), array( 'status' => 404 ) );
 		}
 
 		$title_list = $html_response->getElementsByTagName( 'title' );
@@ -74,7 +81,7 @@ class WP_REST_URL_Details_Controller extends WP_REST_Controller {
 		$title = $title_list->item( 0 );
 
 		if ( empty( $title ) ) {
-			return new WP_Error( 'no_title', 'No title tag at remote url.', array( 'status' => 404 ) );
+			return new WP_Error( 'no_title', __( 'No title tag at remote url.', 'gutenberg' ), array( 'status' => 404 ) );
 		}
 
 		$title_text = $title->nodeValue;
@@ -82,18 +89,28 @@ class WP_REST_URL_Details_Controller extends WP_REST_Controller {
 		return rest_ensure_response( $title_text );
 	}
 
+	/**
+	 * Validates a given URL
+	 *
+	 * @param  String $url the url to validate
+	 * @return Boolean      whether or not the URL is considered valid.
+	 */
 	public function validate_url( $url ) {
 		return wp_http_validate_url( $url );
 	}
 
+	/**
+	 * Sanitizes a given URL.
+	 *
+	 * @param  String $url the URL to sanitize.
+	 * @return String      the sanitized version of the URL.
+	 */
 	public function sanitize_url( $url ) {
 		return esc_url_raw( $url );
 	}
 
 	/**
 	 * Checks whether a given request has permission to read remote urls.
-	 *
-	 * @since 5.7.0
 	 *
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return WP_Error|bool True if the request has access, WP_Error object otherwise.
@@ -117,17 +134,24 @@ class WP_REST_URL_Details_Controller extends WP_REST_Controller {
 	/* phpcs:enable */
 
 
+	/**
+	 * Retrives a DOMDocument representation of the
+	 * HTML from a remote URL
+	 *
+	 * @param  String $url the website url whose HTML we want to access.
+	 * @return DOMDocument      the loaded HTML response.
+	 */
 	private function get_remote_url_html( $url ) {
 
 		$response = wp_remote_get( $url );
 
 		if ( is_wp_error( $response ) || ! is_array( $response ) ) {
-			return new WP_Error( 'no_response', 'Unable to contact remote url . ' . $response->get_error_message(), array( 'status' => 404 ) );
+			return new WP_Error( 'no_response', __( 'Unable to contact remote url.', 'gutenberg' ) . $response->get_error_message(), array( 'status' => 404 ) );
 		}
 
 		$body = wp_remote_retrieve_body( $response );
 
-		$dom = new DOMDocument( '1.0', 'UTF - 8' );
+		$dom = new DOMDocument( '1.0', 'UTF-8' );
 
 		// set error level
 		$internalErrors = libxml_use_internal_errors( true );
