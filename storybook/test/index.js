0746ae57ae4ba091f1e1d4aa64329454608484ff
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-10-21 14:38:13 +0200

    Update StoryShots to work with the existing configuration

--- storybook/test/index.js
+++ storybook/test/index.js
@@ -4,7 +4,20 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import path from 'path';
 
+/**
+ * The list of components that should be skipped because they
+ * don't work with the default Storyshots setup.
+ *
+ * @type {string[]}
+ */
+const skippedComponents = [
+	'ClipboardButton',
+];
+
 initStoryshots( {
 	configPath: path.resolve( __dirname, '../' ),
 	suite: '@wordpress/components',
+	storyKindRegex: new RegExp(
+		`^((?!${ skippedComponents.join( '|' ) }).)*$`
+	),
 } );
