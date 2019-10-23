6cf43ac11464cb182f64be7a9b4263a7e0f832c3
Felix Arntz <felixarntz@users.noreply.github.com>
2019-10-24 01:35:06 +0200

    Add logic for basic (temporary) wp_template editing UI (#17625)
    * Templates: Add logic for basic temporary editing UI.
    
    * Templates: Fix menu filter.
    
    * Post Slug: Follow class name convention.

---
+++ packages/editor/src/components/post-slug/check.js
@@ -0,0 +1,10 @@
+/**
+ * Internal dependencies
+ */
+import PostTypeSupportCheck from '../post-type-support-check';
+
+export default function PostSlugCheck( { children } ) {
+	return (
+		<PostTypeSupportCheck supportKeys="slug">{ children }</PostTypeSupportCheck>
+	);
+}
