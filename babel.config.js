d5c67a112994efdd229b5315b4ce9e522deca588
Dratwas <drapich.piotr@gmail.com>
2019-10-23 23:39:41 +0200

    add flow strip types plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 const config = process.env.MOBILE ? require( './packages/react-native-editor/babel.config' ) : {
 	presets: [ '@wordpress/babel-preset-default' ],
-	plugins: [ 'babel-plugin-inline-json-import' ],
+	plugins: [ 'babel-plugin-inline-json-import', '@babel/plugin-transform-flow-strip-types' ],
 };
 module.exports = function( api ) {
 	api.cache( true );
