11bd1fd3985c1979b3454559f80af370cbd8df79
Matthew Kevins <mmkevins@yahoo.com>
2019-10-23 18:29:44 +1000

    Add groutSpacing prop to tiles

--- packages/block-library/src/gallery/tiles.native.js
+++ packages/block-library/src/gallery/tiles.native.js
@@ -8,32 +8,56 @@ import { View } from 'react-native';
  */
 import { Children } from '@wordpress/element';
 
+const containerStyle = {
+	flex: 1,
+	flexDirection: 'row',
+	flexWrap: 'wrap',
+};
+
 function Tiles( props ) {
 	const {
-		//	align,
 		columns,
-		//	imageCrop,
 		children,
+		groutSpacing = 10,
 	} = props;
 
+	const tileBorderWidth = groutSpacing / 2;
+	const tileCount = Children.count( children );
+	const lastRow = Math.floor( ( tileCount - 1 ) / columns );
+
+	const wrappedChilren = Children.map( children, ( child, index ) => {
+		const row = Math.floor( index / columns );
+		const isLastItem = ( index + 1 ) === tileCount;
+
+		const isInFirstRow = row === 0;
+		const isInLastRow = row === lastRow;
+
+		const isFirstInRow = index % columns === 0;
+		const isLastInRow = ( index + 1 ) % columns === 0 || isLastItem;
+
+		const tileStyle = {
+			flex: 1,
+			flexBasis: ( ( 1 / columns ) * 100 ) + '%',
+			overflow: 'hidden',
+			flexDirection: 'row',
+			alignItems: 'center',
+			borderColor: 'transparent',
+			borderLeftWidth: isFirstInRow ? 0 : tileBorderWidth,
+			borderRightWidth: isLastInRow ? 0 : tileBorderWidth,
+			borderTopWidth: isInFirstRow ? 0 : tileBorderWidth,
+			borderBottomWidth: isInLastRow ? 0 : tileBorderWidth,
+		};
+
+		return (
+			<View style={ tileStyle }>
+				{ child }
+			</View>
+		);
+	}	)
+
 	return (
-		<View
-			style={ {
-				flexDirection: 'row',
-				flexWrap: 'wrap',
-			} }
-		>
-			{ Children.map( children, ( child ) => {
-				return (
-					<View
-						style={ {
-							flex: 1,
-							flexBasis: ( ( 1 / columns ) * 100 ) + '%',
-						} }>
-						{ child }
-					</View>
-				);
-			}	) }
+		<View style={ containerStyle }>
+			{ wrappedChilren }
 		</View>
 	);
 }
