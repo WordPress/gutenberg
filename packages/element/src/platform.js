3e72e5c719db4e0641b4ddbc5d1ab5f9145e64a1
Sergio Estevao <sergioestevao@gmail.com>
2019-10-24 09:50:14 +0100

    Remove unnecessary lines.

--- packages/element/src/platform.js
+++ packages/element/src/platform.js
@@ -5,9 +5,6 @@
  * Copyright (c) 2016-present, Nicolas Gallagher.
  * Copyright (c) 2015-present, Facebook, Inc.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
  */
 const Platform = {
 	OS: 'web',
