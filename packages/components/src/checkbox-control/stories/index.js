bef4943ef6cc76e074018fba2574724c025e0d6f
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2019-10-24 23:10:09 +0200

    Storybook: Apply a set of enhancements to the existing stories (#18030)
    * Storybook: Apply a set of enhancements to the existing stories
    
    * Add basic knobs integration to all Button stories

--- packages/components/src/checkbox-control/stories/index.js
+++ packages/components/src/checkbox-control/stories/index.js
@@ -13,31 +13,42 @@ import { useState } from '@wordpress/element';
  */
 import CheckboxControl from '../';
 
-export default { title: 'Checkbox Control', component: CheckboxControl };
+export default { title: 'CheckboxControl', component: CheckboxControl };
 
-export const _default = () => {
-	const [ isChecked, setChecked ] = useState( true );
+const CheckboxControlWithState = ( { checked, ...props } ) => {
+	const [ isChecked, setChecked ] = useState( checked );
+
 	return (
 		<CheckboxControl
-			label="Is author"
+			{ ...props }
 			checked={ isChecked }
 			onChange={ setChecked }
 		/>
 	);
 };
 
-export const All = () => {
-	const [ isChecked, setChecked ] = useState( true );
+export const _default = () => {
+	const label = text( 'Label', 'Is author' );
+
+	return (
+		<CheckboxControlWithState
+			label={ label }
+			checked
+		/>
+	);
+};
+
+export const all = () => {
 	const heading = text( 'Heading', 'User' );
 	const label = text( 'Label', 'Is author' );
 	const help = text( 'Help', 'Is the user an author or not?' );
+
 	return (
-		<CheckboxControl
+		<CheckboxControlWithState
 			heading={ heading }
 			label={ label }
 			help={ help }
-			checked={ isChecked }
-			onChange={ setChecked }
+			checked
 		/>
 	);
 };
