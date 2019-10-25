c1de094dfc3e2b372747cb78a8981d3efef4bb19
Matthew Kevins <mmkevins@yahoo.com>
2019-10-25 22:02:03 +1000

    Use viewport HOC in gallery edit

--- packages/block-library/src/gallery/edit.js
+++ packages/block-library/src/gallery/edit.js
@@ -30,6 +30,7 @@ import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { getBlobByURL, isBlobURL, revokeBlobURL } from '@wordpress/blob';
 import { withSelect } from '@wordpress/data';
+import { withViewportMatch } from '@wordpress/viewport';
 
 /**
  * Internal dependencies
@@ -337,4 +338,5 @@ export default compose( [
 		return { mediaUpload };
 	} ),
 	withNotices,
+	withViewportMatch( { isMobile: '< small' } ),
 ] )( GalleryEdit );
