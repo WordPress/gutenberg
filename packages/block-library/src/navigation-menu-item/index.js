a486306f6695a7b5ffcac1c94ea772c1ead4a68a
tellthemachines <tellthemachines@users.noreply.github.com>
2019-10-27 15:44:07 -0700

    Nav menu item enhancements: display toolbar and remove dropdown (#17986)
    * Display toolbar and remove dropdown from menu item
    
    * Fixes block toolbar misalignment on IE.
    
    * Replace destination and deal with keypresses.
    
    * Update fixture.
    
    * Keydown management and attempt at close on blur.
    
    * Add definitive menu item icon.
    
    * Fix label/input styling.
    
    * Clean up styles after rebase.
    
    * Refactor stop propagation .
    
    * Remove duplicate dependency comments
    
    * Navigation Block: Rename 'destination' to 'url' in server-side code

--- packages/block-library/src/navigation-menu-item/index.js
+++ packages/block-library/src/navigation-menu-item/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-
+import { Path, SVG } from '@wordpress/components';
 /**
  * Internal dependencies
  */
@@ -18,7 +18,7 @@ export const settings = {
 
 	parent: [ 'core/navigation-menu' ],
 
-	icon: 'admin-links',
+	icon: <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24"><Path d="M12 7.27l4.28 10.43-3.47-1.53-.81-.36-.81.36-3.47 1.53L12 7.27M12 2L4.5 20.29l.71.71L12 18l6.79 3 .71-.71L12 2z" /></SVG>,
 
 	description: __( 'Add a page, link, or other item to your Navigation Menu.' ),
 
