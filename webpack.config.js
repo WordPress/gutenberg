619bb6fbf7aed80580cc0dc1c356e84683e67e52
Dratwas <drapich.piotr@gmail.com>
2019-10-28 17:04:57 +0100

    temporary fix for build

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ const WORDPRESS_NAMESPACE = '@wordpress/';
 
 const gutenbergPackages = Object.keys( dependencies )
 	.filter( ( packageName ) => packageName.startsWith( WORDPRESS_NAMESPACE ) )
+	// TODO fix config to build them as well.
+	.filter( ( packageName ) =>
+		! ( packageName.includes( 'react-native-editor' ) ) &&
+		! ( packageName.includes( 'react-native-bridge' ) ) &&
+		! ( packageName.includes( 'react-native-aztec' ) ) )
 	.map( ( packageName ) => packageName.replace( WORDPRESS_NAMESPACE, '' ) );
 
 module.exports = {
