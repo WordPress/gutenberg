436958cf57484d17e1fa69df8d37addfb559ab97
Jon Q <hello@jonquach.com>
2019-10-28 16:14:50 -0400

    WIP: Adding stories for PageHeader. Testing styles for alignment and position

--- packages/components/src/page-header/index.js
+++ packages/components/src/page-header/index.js
@@ -4,19 +4,38 @@
 import classnames from 'classnames';
 
 function PageHeader( props ) {
-	const { className, title, subtitle, ...restProps } = props;
+	const {
+		children,
+		className,
+		leftActions,
+		title,
+		subtitle,
+		...restProps
+	} = props;
 	const classNames = classnames( className, 'components-page-header' );
 
 	return (
 		<div { ...restProps } className={ classNames }>
-			<div className="components-page-header__title-wrapper">
-				<h1 className="components-page-header__title">{ title }</h1>
-			</div>
-			{ subtitle && (
-				<div className="components-page-header__subtitle">
-					{ subtitle }
+			{ leftActions && (
+				<div className="components-page-header__left-actions">
+					{ leftActions }
 				</div>
 			) }
+			<div className="components-page-header__content">
+				{ title && (
+					<div className="components-page-header__title-wrapper">
+						<h1 className="components-page-header__title">
+							{ title }
+						</h1>
+					</div>
+				) }
+				{ subtitle && (
+					<div className="components-page-header__subtitle">
+						{ subtitle }
+					</div>
+				) }
+			</div>
+			{ children }
 		</div>
 	);
 }
--- packages/components/src/page-header/stories/index.js
+++ packages/components/src/page-header/stories/index.js
@@ -3,15 +3,61 @@
  */
 import { text } from '@storybook/addon-knobs';
 import faker from 'faker';
+
 /**
  * Internal dependencies
  */
+import Button from '../../button';
+import Dropdown from '../../dropdown';
+import MenuGroup from '../../menu-group';
+import MenuItem from '../../menu-item';
+import IconButton from '../../icon-button';
 import PageHeader from '../';
+import './styles.css';
 
 export default { title: 'PageHeader', component: PageHeader };
 
+const InlineActions = () => {
+	return (
+		<div className="inline-buttons">
+			<Button isDefault isPrimary>
+				Add New
+			</Button>
+			<Button isDefault>Add New</Button>
+			<Button isDefault>Add New</Button>
+		</div>
+	);
+};
+
+const DropdownActions = () => {
+	return (
+		<div className="additional-actions">
+			<Dropdown
+				className="my-container-class-name"
+				contentClassName="my-popover-content-classname"
+				position="bottom right"
+				renderToggle={ ( { isOpen, onToggle } ) => (
+					<IconButton
+						icon="ellipsis"
+						isTertiary
+						onClick={ onToggle }
+						aria-expanded={ isOpen }
+					/>
+				) }
+				renderContent={ () => (
+					<MenuGroup>
+						<MenuItem icon="star-filled">Favorite</MenuItem>
+						<MenuItem icon="share-alt2">Share</MenuItem>
+						<MenuItem icon="admin-generic">Settings</MenuItem>
+					</MenuGroup>
+				) }
+			/>
+		</div>
+	);
+};
+
 export const _default = () => {
-	const title = text( 'title', faker.lorem.sentence() );
+	const title = text( 'title', 'Title' );
 	const subtitle = text( 'subtitle', faker.lorem.paragraph() );
 
 	const props = {
@@ -19,5 +65,34 @@ export const _default = () => {
 		subtitle,
 	};
 
-	return <PageHeader { ...props } />;
+	return (
+		<PageHeader { ...props }>
+			<InlineActions />
+			<DropdownActions />
+		</PageHeader>
+	);
+};
+
+export const simple = () => {
+	const title = text( 'title', 'Title' );
+
+	const props = {
+		title,
+	};
+
+	return (
+		<PageHeader { ...props }>
+			<InlineActions />
+			<DropdownActions />
+		</PageHeader>
+	);
+};
+
+export const actionBar = () => {
+	return (
+		<PageHeader leftActions={ <IconButton icon="insert" isTertiary /> }>
+			<InlineActions />
+			<DropdownActions />
+		</PageHeader>
+	);
 };
