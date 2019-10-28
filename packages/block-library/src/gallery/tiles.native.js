d27412e0a3a6c50dc785b0227bb98edf0bc41fe4
Matthew Kevins <mmkevins@yahoo.com>
2019-10-28 16:04:41 +1000

    Add simple tiles component for gallery

---
+++ packages/block-library/src/gallery/tiles.native.js
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { View } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { Children } from '@wordpress/element';
+
+function Tiles( props ) {
+	const {
+		tilesProps: {
+		//	align,
+			columns,
+		//	imageCrop,
+		},
+		children,
+	} = props;
+
+	return (
+		<View
+			style={ {
+				flexDirection: 'row',
+				flexWrap: 'wrap',
+			} }
+		>
+			{ Children.map( children, ( child ) => {
+				return (
+					<View
+						style={ {
+							flex: 1,
+							flexBasis: ( ( 1 / columns ) * 100 ) + '%',
+						} }>
+						{ child }
+					</View>
+				);
+			}	) }
+		</View>
+	);
+}
+
+export default Tiles;
