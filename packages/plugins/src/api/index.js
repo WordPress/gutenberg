3eb81d5b7edc5cfce4e9cabbb226f3500806fbcc
Ryan Welcher <ryan.welcher@10up.com>
2019-10-29 09:37:42 -0400

    Use Number.isInteger() instead to check the priority

--- packages/plugins/src/api/index.js
+++ packages/plugins/src/api/index.js
@@ -123,10 +123,7 @@ export function registerPlugin( name, settings ) {
 		return null;
 	}
 	const { priority = 10 } = settings;
-	if (
-		priority !== 0 &&
-		( priority.length === 0 || typeof priority !== 'number' )
-	) {
+	if ( ! Number.isInteger( priority ) ) {
 		console.error( 'The "priority" property must be a number' );
 		return null;
 	}
