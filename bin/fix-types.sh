6309a8e7c024bcabd4b0f6581d5d8b0314abbdf1
Dratwas <drapich.piotr@gmail.com>
2019-10-29 21:49:21 +0100

    change fix-types script

--- bin/fix-types.sh
+++ bin/fix-types.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 DIRNAME=$(dirname "$0")
+global_types_file="$DIRNAME""/../node_modules/@types/react-native/globals.d.ts"
+index_type_file="$DIRNAME""/../node_modules/@types/react-native/index.d.ts"
 
 # React Native declares global types that interfere with @types/node and lib dom.
-rm -f $DIRNAME/../node_modules/@types/react-native/globals.d.ts
-sed -i '' 's|/// <reference path="globals.d.ts" />||' $DIRNAME/../node_modules/@types/react-native/index.d.ts
 
-# Namespace the globals
-sed -i '' 's|declare global|declare namespace IgnoreTheseGlobals|' $DIRNAME/../node_modules/@types/react-native/index.d.ts
+# Remove duplicated types
+rm -f "$global_types_file"
+
+# Remove reference to removed global types
+sed -i '' 's|/// <reference path="globals.d.ts" />||' "$index_type_file"
+
+# Namespace the globals in index.d.ts file
+sed -i '' 's|declare global|declare namespace IgnoreTheseGlobals|' "$index_type_file"
