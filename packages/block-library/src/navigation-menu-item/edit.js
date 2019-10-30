4730cf846f482e52a41aba7aa27ebffb9547e41e
tellthemachines <isabel@tellthemachines.com>
2019-10-30 16:43:21 +1100

    Insert block directly from toolbar

--- packages/block-library/src/navigation-menu-item/edit.js
+++ packages/block-library/src/navigation-menu-item/edit.js
@@ -6,7 +6,9 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { withSelect } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
+import { createBlock } from '@wordpress/blocks';
+import { withDispatch, withSelect } from '@wordpress/data';
 import {
 	ExternalLink,
 	PanelBody,
@@ -46,13 +48,11 @@ function NavigationMenuItemEdit( {
 	isSelected,
 	isParentOfSelectedBlock,
 	setAttributes,
+	insertMenuItemBlock,
 } ) {
 	const [ isLinkOpen, setIsLinkOpen ] = useState( false );
 	const [ isEditingLink, setIsEditingLink ] = useState( false );
 	const [ urlInput, setUrlInput ] = useState( null );
-	const [ addSubmenu, setaddSubmenu ] = useState( false );
-
-	const showAppender = addSubmenu || hasDescendants;
 
 	const inputValue = urlInput !== null ? urlInput : url;
 
@@ -99,7 +99,7 @@ function NavigationMenuItemEdit( {
 						name="submenu"
 						icon={ <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24"><Path d="M14 5h8v2h-8zm0 5.5h8v2h-8zm0 5.5h8v2h-8zM2 11.5C2 15.08 4.92 18 8.5 18H9v2l3-3-3-3v2h-.5C6.02 16 4 13.98 4 11.5S6.02 7 8.5 7H12V5H8.5C4.92 5 2 7.92 2 11.5z" /><Path fill="none" d="M0 0h24v24H0z" /></SVG> }
 						title={ __( 'Submenu' ) }
-						onClick={ () => setaddSubmenu( ! addSubmenu ) }
+						onClick={ insertMenuItemBlock }
 					/> }
 				</Toolbar>
 				{ isLinkOpen &&
@@ -196,7 +196,7 @@ function NavigationMenuItemEdit( {
 				{ ( isSelected || isParentOfSelectedBlock ) &&
 					<InnerBlocks
 						allowedBlocks={ [ 'core/navigation-menu-item' ] }
-						renderAppender={ showAppender ? InnerBlocks.ButtonBlockAppender : false }
+						renderAppender={ hasDescendants ? InnerBlocks.ButtonBlockAppender : false }
 					/>
 				}
 			</div>
@@ -204,12 +204,32 @@ function NavigationMenuItemEdit( {
 	);
 }
 
-export default withSelect( ( select, ownProps ) => {
-	const { getClientIdsOfDescendants, hasSelectedInnerBlock } = select( 'core/block-editor' );
-	const { clientId } = ownProps;
+export default compose( [
+	withSelect( ( select, ownProps ) => {
+		const { getClientIdsOfDescendants, hasSelectedInnerBlock } = select( 'core/block-editor' );
+		const { clientId } = ownProps;
 
-	return {
-		isParentOfSelectedBlock: hasSelectedInnerBlock( clientId, true ),
-		hasDescendants: !! getClientIdsOfDescendants( [ clientId ] ).length,
-	};
-} )( NavigationMenuItemEdit );
+		return {
+			isParentOfSelectedBlock: hasSelectedInnerBlock( clientId, true ),
+			hasDescendants: !! getClientIdsOfDescendants( [ clientId ] ).length,
+		};
+	} ),
+	withDispatch( ( dispatch, ownProps ) => {
+		return {
+			insertMenuItemBlock() {
+				const { clientId } = ownProps;
+
+				const {
+					insertBlock,
+				} = dispatch( 'core/block-editor' );
+
+				const blockToInsert = createBlock( 'core/navigation-menu-item' );
+				insertBlock(
+					blockToInsert,
+					0,
+					clientId,
+				);
+			},
+		};
+	} ),
+] )( NavigationMenuItemEdit );
