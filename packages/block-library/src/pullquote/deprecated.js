6775d9284628710bef8ff535c686a7135769e855
Jorge <jorge.costa@developer.pt>
2019-10-30 19:32:19 +0000

    Fix: setting a preset color on pullquote default style makes the block invalid.

--- packages/block-library/src/pullquote/deprecated.js
+++ packages/block-library/src/pullquote/deprecated.js
@@ -52,6 +52,67 @@ const deprecated = [
 	{
 		attributes: blockAttributes,
 		save( { attributes } ) {
+			const {
+				mainColor,
+				customMainColor,
+				textColor,
+				customTextColor,
+				value,
+				citation,
+				className,
+			} = attributes;
+
+			const isSolidColorStyle = includes( className, SOLID_COLOR_CLASS );
+
+			let figureClasses, figureStyles;
+
+			// Is solid color style
+			if ( isSolidColorStyle ) {
+				const backgroundClass = getColorClassName( 'background-color', mainColor );
+
+				figureClasses = classnames( {
+					'has-background': ( backgroundClass || customMainColor ),
+					[ backgroundClass ]: backgroundClass,
+				} );
+
+				figureStyles = {
+					backgroundColor: backgroundClass ? undefined : customMainColor,
+				};
+			// Is normal style and a custom color is being used ( we can set a style directly with its value)
+			} else if ( customMainColor ) {
+				figureStyles = {
+					borderColor: customMainColor,
+				};
+			// If normal style and a named color are being used, we need to retrieve the color value to set the style,
+			// as there is no expectation that themes create classes that set border colors.
+			} else if ( mainColor ) {
+				const colors = get( select( 'core/block-editor' ).getSettings(), [ 'colors' ], [] );
+				const colorObject = getColorObjectByAttributeValues( colors, mainColor );
+				figureStyles = {
+					borderColor: colorObject.color,
+				};
+			}
+
+			const blockquoteTextColorClass = getColorClassName( 'color', textColor );
+			const blockquoteClasses = ( textColor || customTextColor ) && classnames( 'has-text-color', {
+				[ blockquoteTextColorClass ]: blockquoteTextColorClass,
+			} );
+
+			const blockquoteStyles = blockquoteTextColorClass ? undefined : { color: customTextColor };
+
+			return (
+				<figure className={ figureClasses } style={ figureStyles }>
+					<blockquote className={ blockquoteClasses } style={ blockquoteStyles } >
+						<RichText.Content value={ value } multiline />
+						{ ! RichText.isEmpty( citation ) && <RichText.Content tagName="cite" value={ citation } /> }
+					</blockquote>
+				</figure>
+			);
+		},
+	},
+	{
+		attributes: blockAttributes,
+		save( { attributes } ) {
 			const { mainColor, customMainColor, textColor, customTextColor, value, citation, className } = attributes;
 			const isSolidColorStyle = includes( className, SOLID_COLOR_CLASS );
 
