915c754e836e8bd893c47fd30ad130d2e781c377
retrofox <rdsuarez@gmail.com>
2019-10-31 16:02:02 -0300

    colors-selector: change SVG icon

--- packages/block-library/src/navigation-menu/block-colors-selector.js
+++ packages/block-library/src/navigation-menu/block-colors-selector.js
@@ -7,27 +7,14 @@ import { noop } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { IconButton, Dropdown, Toolbar, SVG } from '@wordpress/components';
+import { IconButton, Dropdown, Toolbar, SVG, Path } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { DOWN } from '@wordpress/keycodes';
 import { ColorPaletteControl, ContrastChecker } from '@wordpress/block-editor';
 
 const ColorSelectorSVGIcon = () => (
-	<SVG
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 20 20"
-	>
-		<text
-			transform="translate(-63 -14)"
-			fillRule="evenodd"
-			fontFamily="SFUIText-Bold, SF UI Text"
-			fontSize="12"
-			fontWeight="bold"
-			letterSpacing="-.5"
-			alignmentBaseline="middle"
-		>
-			<tspan x="63.453" y="23">Aa</tspan>
-		</text>
+	<SVG xmlns="https://www.w3.org/2000/svg" viewBox="0 0 20 20">
+		<Path d="M7.434 5l3.18 9.16H8.538l-.692-2.184H4.628l-.705 2.184H2L5.18 5h2.254zm-1.13 1.904h-.115l-1.148 3.593H7.44L6.304 6.904zM14.348 7.006c1.853 0 2.9.876 2.9 2.374v4.78h-1.79v-.914h-.114c-.362.64-1.123 1.022-2.031 1.022-1.346 0-2.292-.826-2.292-2.108 0-1.27.972-2.006 2.71-2.107l1.696-.102V9.38c0-.584-.42-.914-1.18-.914-.667 0-1.112.228-1.264.647h-1.701c.12-1.295 1.307-2.107 3.066-2.107zm1.079 4.1l-1.416.09c-.793.056-1.18.342-1.18.844 0 .52.45.837 1.091.837.857 0 1.505-.545 1.505-1.256v-.515z" />
 	</SVG>
 );
 
