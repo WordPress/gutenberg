9eaa636af2315b132d1b685de2260b487f45aab6
epiqueras <epiquerass@gmail.com>
2019-11-01 17:13:17 -0500

    Use Colors: Re-detect background color when attributes change.

--- packages/block-editor/src/components/colors/use-colors.js
+++ packages/block-editor/src/components/colors/use-colors.js
@@ -203,7 +203,7 @@ export default function __experimentalUseColors(
 				detectedBackgroundColorRef.current = backgroundColor;
 				return { backgroundColor };
 			} )( () => <></> ),
-		[]
+		[ attributes ]
 	);
 
 	return useMemo( () => {
