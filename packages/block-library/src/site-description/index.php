d9c1ff90789dbc6b3cc8eb4bcde9980a0001e966
Copons <copons@users.noreply.github.com>
2019-11-01 19:13:46 +0000

    Add classes and styles to the front end rendering

--- packages/block-library/src/site-description/index.php
+++ packages/block-library/src/site-description/index.php
@@ -10,8 +10,51 @@
  *
  * @return string The render.
  */
-function render_block_core_site_description() {
-	return sprintf( '<p>%s</p>', get_bloginfo( 'description' ) );
+function render_block_core_site_description( $attributes ) {
+	$styles = '';
+
+	$class = 'wp-block-site-description';
+	if ( isset( $attributes['className'] ) ) {
+		$class .= ' ' . $attributes['className'];
+	}
+
+	if ( isset( $attributes['align'] ) ) {
+		$class .= ' align' . $attributes['align'];
+	}
+
+	if ( isset( $attributes['textAlign'] ) ) {
+		$class .= ' has-text-align-' . $attributes['textAlign'];
+	}
+
+	if ( isset( $attributes['textColor'] ) ) {
+		$class .= ' has-text-color';
+		$class .= ' has-' . $attributes['textColor'] . '-color';
+	} elseif ( isset( $attributes['customTextColor'] ) ) {
+		$class  .= ' has-text-color';
+		$styles .= ' color: ' . $attributes['customTextColor'] . ';';
+	}
+
+	if ( isset( $attributes['backgroundColor'] ) ) {
+		$class .= ' has-background';
+		$class .= ' has-' . $attributes['backgroundColor'] . '-background-color';
+	} elseif ( isset( $attributes['customBackgroundColor'] ) ) {
+		$class  .= ' has-background';
+		$styles .= ' background-color: ' . $attributes['customBackgroundColor'] . ';';
+	}
+
+	if ( isset( $attributes['fontSize'] ) ) {
+		$class .= ' has-' . $attributes['fontSize'] . '-font-size';
+	} elseif ( isset( $attributes['customFontSize'] ) ) {
+		$styles .= ' font-size: ' . $attributes['customFontSize'] . 'px;';
+	}
+
+	ob_start();
+	?>
+	<p class="<?php echo esc_attr( $class ); ?>" style="<?php echo esc_attr( $styles ); ?>">
+		<?php bloginfo( 'description' ); ?>
+	</p>
+	<?php
+	return ob_get_clean();
 }
 
 /**
