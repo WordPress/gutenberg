a1328b21902fc36abd7e028fd874250f636cde9c
Haz <hazdiego@gmail.com>
2019-11-02 23:35:40 -0300

    Try without useMountedToolbarState

--- packages/components/src/toolbar/toolbar-container.js
+++ packages/components/src/toolbar/toolbar-container.js
@@ -6,41 +6,16 @@ import { useToolbarState, Toolbar } from 'reakit/Toolbar';
 /**
  * WordPress dependencies
  */
-import { forwardRef, useEffect, useRef, useCallback } from '@wordpress/element';
+import { forwardRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import ToolbarContext from '../toolbar-context';
 
-// When using slots, register/unregister are called with delay, and there's a
-// chance that the toolbar is already unmounted when this happens.
-function useMountedToolbarState( initialState ) {
-	const toolbar = useToolbarState( initialState );
-	const mounted = useRef( true );
-
-	useEffect( () => () => {
-		mounted.current = false;
-	}, [] );
-
-	return {
-		...toolbar,
-		register: useCallback( ( ...args ) => {
-			if ( mounted.current ) {
-				toolbar.register( ...args );
-			}
-		}, [ toolbar.register ] ),
-		unregister: useCallback( ( ...args ) => {
-			if ( mounted.current ) {
-				toolbar.unregister( ...args );
-			}
-		}, [ toolbar.unregister ] ),
-	};
-}
-
 function ToolbarContainer( { accessibilityLabel, ...props }, ref ) {
 	// https://reakit.io/docs/basic-concepts/#state-hooks
-	const toolbarState = useMountedToolbarState( { loop: true } );
+	const toolbarState = useToolbarState( { loop: true } );
 
 	return (
 		// This will provide state for `ToolbarButton`'s
