e19715804283deab64cd2de9a604e0bbe5c83e4e
Sergio Estevao <sergioestevao@gmail.com>
2019-11-03 21:57:22 +0000

    Prepare block for dark color mode in iOS.

--- packages/block-library/src/preformatted/edit.native.js
+++ packages/block-library/src/preformatted/edit.native.js
@@ -2,23 +2,31 @@
  * External dependencies
  */
 import { View } from 'react-native';
-
+/**
+ * WordPress dependencies
+ */
+import { withPreferredColorScheme } from '@wordpress/compose';
 /**
  * Internal dependencies
  */
 import WebPreformattedEdit from './edit.js';
 import styles from './styles.scss';
 
-export default function PreformattedEdit( props ) {
+function PreformattedEdit( props ) {
+	const { getStylesFromColorScheme } = props;
+	const richTextStyle = getStylesFromColorScheme( styles.wpRichText, styles.wpRichTextDark );
+	const wpBlockPreformatted = getStylesFromColorScheme( styles.wpBlockPreformatted, styles.wpBlockPreformattedDark );
 	const propsWithStyle = {
 		...props,
-		style: styles.wpRichText,
+		style: richTextStyle,
 	};
 	return (
-		<View style={ styles.wpBlockPreformatted } >
+		<View style={ wpBlockPreformatted } >
 			<WebPreformattedEdit
 				{ ...propsWithStyle }
 			/>
 		</View>
 	);
 }
+
+export default withPreferredColorScheme( PreformattedEdit );
