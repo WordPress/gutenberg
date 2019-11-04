47b491b79943e0dc5f06f0d6d89b80f0383ddf50
Wendy Chen <tilphousia@gmail.com>
2019-11-04 07:42:46 -0500

    WC: issue #479: Changing only the question mark icon to be tappable for the help block. Few color changes. Adding some text to the help block.

--- packages/block-library/src/missing/edit.native.js
+++ packages/block-library/src/missing/edit.native.js
@@ -33,28 +33,32 @@ export class UnsupportedBlockEdit extends Component {
 	}
 
 	renderHelpIcon() {
-		return <View style={ styles.helpIconContainer } >
-			<Icon
-				className="unsupported-icon-help"
-				label={ __( 'Help icon' ) }
-				icon="editor-help"
-			/>
-		</View>;
+		return <TouchableWithoutFeedback
+			accessibilityLabel={ __( 'Help icon' ) }
+			accessibilityRole={ 'button' }
+			accessibilityHint={ __( 'Tap here to show help' ) }
+			onPress={ this.toggleSheet.bind( this ) }
+		>
+			<View style={ styles.helpIconContainer } >
+				<Icon
+					className="unsupported-icon-help"
+					label={ __( 'Help icon' ) }
+					icon="editor-help"
+				/>
+			</View>
+		</TouchableWithoutFeedback>;
 	}
 
 	renderSheetButton( text, action ) {
-		// This margin is necessary for Android because of the way the modals on each platform are
-		// being rendered. This extra margin makes the modals look the same on each platform.
-		const marginBottom = isAndroid ? 16 : 0;
 		return <TouchableOpacity
 			onPress={ action() }
-			style={ [ styles.infoButton, { marginBottom } ] }
+			style={ styles.sheetButton }
 		>
-			<Text style={ styles.infoButtonText } >{ text }</Text>
+			<Text style={ styles.sheetButtonText } >{ text }</Text>
 		</TouchableOpacity>;
 	}
 
-	renderSheet() {
+	renderSheet( title ) {
 		return <BottomSheet
 			isVisible={ this.state.showHelp }
 			hideHeader
@@ -63,12 +67,11 @@ export class UnsupportedBlockEdit extends Component {
 			<View style={ styles.infoContainer } >
 				<Icon icon="editor-help" style={ styles.infoIcon } size={ styles.infoIcon.size } />
 				<Text style={ [ styles.infoText, styles.infoTitle ] }>
-					{ __( 'This block isn\'t yet supported on WordPress for ' + platformText ) }
+					{ __( title + ' isn\'t yet supported on WordPress for ' + platformText ) }
 				</Text>
 				<Text style={ [ styles.infoText, styles.infoDescription ] }>
 					{ __( 'We are working hard to add more blocks with each release. In the meantime, you can also edit this post on the web.' ) }
 				</Text>
-				{ this.renderSheetButton( __( 'Edit in Safari' ), () => this.toggleSheet.bind( this ) ) }
 				{ this.renderSheetButton( __( 'Close' ), () => this.toggleSheet.bind( this ) ) }
 			</View>
 		</BottomSheet>;
@@ -86,19 +89,12 @@ export class UnsupportedBlockEdit extends Component {
 		const iconStyle = getStylesFromColorScheme( styles.unsupportedBlockIcon, styles.unsupportedBlockIconDark );
 		const iconClassName = 'unsupported-icon' + '-' + preferredColorScheme;
 		return (
-			<TouchableWithoutFeedback
-				accessibilityLabel={ __( originalName + ' block' ) }
-				accessibilityRole={ 'button' }
-				accessibilityHint={ __( 'Tap the top right corner to show help' ) }
-				onPress={ this.toggleSheet.bind( this ) }
-			>
-				<View style={ getStylesFromColorScheme( styles.unsupportedBlock, styles.unsupportedBlockDark ) }>
-					{ this.renderHelpIcon() }
-					<Icon className={ iconClassName } icon={ icon && icon.src ? icon.src : icon } color={ iconStyle.color } />
-					<Text style={ titleStyle }>{ title }</Text>
-					{ this.renderSheet() }
-				</View>
-			</TouchableWithoutFeedback>
+			<View style={ getStylesFromColorScheme( styles.unsupportedBlock, styles.unsupportedBlockDark ) }>
+				{ this.renderHelpIcon() }
+				<Icon className={ iconClassName } icon={ icon && icon.src ? icon.src : icon } color={ iconStyle.color } />
+				<Text style={ titleStyle }>{ title }</Text>
+				{ this.renderSheet( title ) }
+			</View>
 		);
 	}
 }
