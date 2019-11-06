228068c67faa8e3f0d2a8a5ae5bfdf9f718708c9
Haz <hazdiego@gmail.com>
2019-11-05 22:13:37 -0300

    Remove ToolbarSlotFill from components package

--- packages/block-editor/src/components/block-controls/index.js
+++ packages/block-editor/src/components/block-controls/index.js
@@ -1,24 +1,36 @@
 /**
  * WordPress dependencies
  */
-import { createToolbarSlotFill, ToolbarGroup } from '@wordpress/components';
+import { useContext } from '@wordpress/element';
+import { createSlotFill, ToolbarGroup, ToolbarContext } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { ifBlockEditSelected } from '../block-edit/context';
 
-const { Fill, Slot } = createToolbarSlotFill( 'BlockControls' );
+const { Fill, Slot } = createSlotFill( 'BlockControls' );
 
-const BlockControlsFill = ( { controls, children } ) => (
-	<Fill>
-		<ToolbarGroup controls={ controls } />
-		{ children }
-	</Fill>
-);
+function BlockControlsSlot( props ) {
+	const accessibleToolbarState = useContext( ToolbarContext );
+	return <Slot { ...props } fillProps={ accessibleToolbarState || null } />;
+}
+
+function BlockControlsFill( { controls, children } ) {
+	return (
+		<Fill>
+			{ ( fillProps ) => (
+				<ToolbarContext.Provider value={ fillProps }>
+					<ToolbarGroup controls={ controls } />
+					{ children }
+				</ToolbarContext.Provider>
+			) }
+		</Fill>
+	);
+}
 
 const BlockControls = ifBlockEditSelected( BlockControlsFill );
 
-BlockControls.Slot = Slot;
+BlockControls.Slot = BlockControlsSlot;
 
 export default BlockControls;
--- packages/block-editor/src/components/block-format-controls/index.js
+++ packages/block-editor/src/components/block-format-controls/index.js
@@ -1,17 +1,35 @@
 /**
  * WordPress dependencies
  */
-import { createToolbarSlotFill } from '@wordpress/components';
+import { useContext } from '@wordpress/element';
+import { createSlotFill, ToolbarContext } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { ifBlockEditSelected } from '../block-edit/context';
 
-const { Fill, Slot } = createToolbarSlotFill( 'BlockFormatControls' );
+const { Fill, Slot } = createSlotFill( 'BlockFormatControls' );
 
-const BlockFormatControls = ifBlockEditSelected( Fill );
+function BlockFormatControlsSlot( props ) {
+	const accessibleToolbarState = useContext( ToolbarContext );
+	return <Slot { ...props } fillProps={ accessibleToolbarState || null } />;
+}
 
-BlockFormatControls.Slot = Slot;
+function BlockFormatControlsFill( props ) {
+	return (
+		<Fill { ...props }>
+			{ ( fillProps ) => (
+				<ToolbarContext.Provider value={ fillProps }>
+					{ props.children }
+				</ToolbarContext.Provider>
+			) }
+		</Fill>
+	);
+}
+
+const BlockFormatControls = ifBlockEditSelected( BlockFormatControlsFill );
+
+BlockFormatControls.Slot = BlockFormatControlsSlot;
 
 export default BlockFormatControls;
