e1ea787ac32223d30875dffbdc49bd6d0a12e60c
Sören Wrede <soerenwrede@gmail.com>
2019-11-08 15:33:22 +0100

    Add Actions prop

--- packages/components/src/snackbar/stories/index.js
+++ packages/components/src/snackbar/stories/index.js
@@ -19,3 +19,14 @@ export const _default = () => {
 		</Snackbar>
 	);
 };
+
+export const withActions = () => {
+	const content = text( 'Content', 'Use Snackbars with an action link to an external page.' );
+	const actions = [ { url: 'https://wordpress.org', label: 'Open WP.org' } ];
+
+	return (
+		<Snackbar actions={ actions }>
+			{ content }
+		</Snackbar>
+	);
+};
