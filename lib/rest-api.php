aafc34d1a5109a65e350ce67b6c696946a574735
Andrew Duthie <andrew@andrewduthie.com>
2019-11-08 19:07:52 -0500

    REST API: Include parsed blocks in REST Posts response

--- lib/rest-api.php
+++ lib/rest-api.php
@@ -52,7 +52,74 @@ function gutenberg_filter_oembed_result( $response, $handler, $request ) {
 }
 add_filter( 'rest_request_after_callbacks', 'gutenberg_filter_oembed_result', 10, 3 );
 
+/**
+ * Reducer callback operating on an array of block results for REST response.
+ * Given a block entity, adds an array formatted for use in the response, if
+ * the block is named.
+ *
+ * @param array $result Blocks to include.
+ * @param array $block  The parsed block to format.
+ * @return array        Block formatted for REST response.
+ */
+function gutenberg_format_block_for_rest( $result, $block ) {
+	if ( ! empty( $block['blockName'] ) ) {
+		$result[] = array(
+			'name'         => $block['blockName'],
+			'attributes'   => (object) $block['attrs'],
+			'inner_blocks' => array_reduce(
+				$block['innerBlocks'],
+				'gutenberg_format_block_for_rest',
+				array()
+			),
+		);
+	}
 
+	return $result;
+}
+
+/**
+ * Augments REST Posts controller response to include blocks data, only if
+ * response already includes `content.raw`.
+ *
+ * In a more correct implementation, this would use item schema as basis for
+ * whether `content.blocks` is included in a response.
+ *
+ * @since 6.9.0
+ *
+ * @param WP_REST_Response $response The response object.
+ * @return WP_HTTP_Response          The filtered response object.
+ */
+function gutenberg_add_blocks_to_rest_posts_content( $response ) {
+	if ( ! empty( $response->data['content']['raw'] ) ) {
+		$response->data['content']['blocks'] = array_reduce(
+			parse_blocks( $response->data['content']['raw'] ),
+			'gutenberg_format_block_for_rest',
+			array()
+		);
+	}
+
+	return $response;
+}
+
+/**
+ * Adds filter for all registered post types to augment REST API responses for
+ * that post type to include blocks data.
+ *
+ * In a core implementation, this would not be necessary, as the logic for
+ * including blocks data would be part of the controller implementation.
+ *
+ * @since 6.9.0
+ */
+function gutenberg_filter_rest_prepare_post_types() {
+	$post_types = get_post_types();
+	foreach ( $post_types as $post_type ) {
+		add_filter(
+			'rest_prepare_' . $post_type,
+			'gutenberg_add_blocks_to_rest_posts_content'
+		);
+	}
+}
+add_action( 'rest_pre_dispatch', 'gutenberg_filter_rest_prepare_post_types' );
 
 /**
  * Start: Include for phase 2
