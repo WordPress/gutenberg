02feda990a9901a8565280993b6c1831abdd44ad
Jorge Costa <jorge.costa@automattic.com>
2019-11-12 20:34:12 +0000

    Update packages/edit-post/src/components/sidebar/settings-sidebar/index.js
    Co-Authored-By: Enrique Piqueras <epiqueras@users.noreply.github.com>

--- packages/edit-post/src/components/sidebar/settings-sidebar/index.js
+++ packages/edit-post/src/components/sidebar/settings-sidebar/index.js
@@ -56,7 +56,7 @@ function TestSimulateMediaQuery() {
 				onChange={ ( newValue ) => ( setSimulationWidth( newValue ) ) }
 			/>
 			<__experimentalSimulateMediaQuery
-				value={ simulateValue }
+				value={ simulationWidth }
 				partialPaths={ PARTIAL_PATHS }
 			/>
 		</>
