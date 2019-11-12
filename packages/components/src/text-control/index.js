7c5c103a8565c436523251e01bc491162fdff82d
Oksana Romaniv <okskozub@gmail.com>
2019-11-12 13:59:54 +0200

    Add support for React defaultValue prop
    Add support for default value via React defaultValue prop to fill in the controlled input initial value without the need to use withState component or React hook/class.
    
    More here: [https://reactjs.org/docs/uncontrolled-components.html#default-values](https://reactjs.org/docs/uncontrolled-components.html#default-values)

--- packages/components/src/text-control/index.js
+++ packages/components/src/text-control/index.js
@@ -8,13 +8,14 @@ import { withInstanceId } from '@wordpress/compose';
  */
 import BaseControl from '../base-control';
 
-function TextControl( { label, hideLabelFromVision, value, help, className, instanceId, onChange, type = 'text', ...props } ) {
+function TextControl( { defaultValue, label, hideLabelFromVision, value, help, className, instanceId, onChange, type = 'text', ...props } ) {
 	const id = `inspector-text-control-${ instanceId }`;
 	const onChangeValue = ( event ) => onChange( event.target.value );
 
 	return (
 		<BaseControl label={ label } hideLabelFromVision={ hideLabelFromVision } id={ id } help={ help } className={ className }>
 			<input className="components-text-control__input"
+				defaultValue={ defaultValue }
 				type={ type }
 				id={ id }
 				value={ value }
