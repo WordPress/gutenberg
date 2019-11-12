dccfa7e88fbe7606255151e2c65b8bf82b4c72fb
Paul Stonier <pstonier@gmail.com>
2019-11-12 15:48:52 -0500

    fixed label undefined error

--- packages/block-editor/src/components/url-input/index.js
+++ packages/block-editor/src/components/url-input/index.js
@@ -260,6 +260,7 @@ class URLInput extends Component {
 			instanceId,
 			className,
 			isFullWidth,
+			label,
 			hasBorder,
 			__experimentalRenderSuggestions: renderSuggestions,
 			placeholder = __( 'Paste URL or type to search' ),
