3f1adc8c6ba43b69c18b87a1e56ddbaedc1c503b
Matthew Kevins <mmkevins@yahoo.com>
2019-11-14 21:02:37 +1000

    Add separate options for capturing photo and video

--- packages/block-editor/src/components/media-upload/index.native.js
+++ packages/block-editor/src/components/media-upload/index.native.js
@@ -20,12 +20,17 @@ export const MEDIA_TYPE_IMAGE = 'image';
 export const MEDIA_TYPE_VIDEO = 'video';
 
 export const MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_CHOOSE_FROM_DEVICE = 'choose_from_device';
-export const MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_MEDIA = 'take_media';
+export const MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_PHOTO = 'take_photo';
+export const MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_VIDEO = 'take_video';
 export const MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_WORD_PRESS_LIBRARY = 'wordpress_media_library';
 
 export const OPTION_TAKE_VIDEO = __( 'Take a Video' );
 export const OPTION_TAKE_PHOTO = __( 'Take a Photo' );
 export const OPTION_TAKE_PHOTO_OR_VIDEO = __( 'Take a Photo or Video' );
+
+const captureMediaMap = new Map();
+captureMediaMap.set( MEDIA_TYPE_IMAGE, MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_PHOTO );
+captureMediaMap.set( MEDIA_TYPE_VIDEO, MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_VIDEO );
 
 export class MediaUpload extends React.Component {
 	constructor( props ) {
@@ -54,24 +59,34 @@ export class MediaUpload extends React.Component {
 		} );
 	}
 
-	getTakeMediaLabel() {
-		const { allowedTypes = [] } = this.props;
+	getTakeMediaLabel( mediaType ) {
+		switch ( mediaType ) {
+			case MEDIA_TYPE_IMAGE:
+				return OPTION_TAKE_PHOTO;
+			case MEDIA_TYPE_VIDEO:
+				return OPTION_TAKE_VIDEO;
+		}
 
-		const isOneType = allowedTypes.length === 1;
-		const isImage = isOneType && allowedTypes.includes( MEDIA_TYPE_IMAGE );
-		const isVideo = isOneType && allowedTypes.includes( MEDIA_TYPE_VIDEO );
-
-		if ( isImage ) {
-			return OPTION_TAKE_PHOTO;
-		} else if ( isVideo ) {
-			return OPTION_TAKE_VIDEO;
-		} return OPTION_TAKE_PHOTO_OR_VIDEO;
+		return OPTION_TAKE_PHOTO;
 	}
 
 	getMediaOptionsItems() {
+		const { allowedTypes = [] } = this.props;
+
+		// multiple capture options for media text
+		const captureMediaOptionsItems = allowedTypes.filter( ( allowedType ) => {
+			return allowedType === MEDIA_TYPE_IMAGE || allowedType === MEDIA_TYPE_VIDEO;
+		} ).map( ( mediaType ) => {
+			return {
+				icon: this.getTakeMediaIcon(),
+				value: captureMediaMap.get( mediaType ),
+				label: this.getTakeMediaLabel( mediaType ),
+			};
+		} );
+
 		return [
 			{ icon: this.getChooseFromDeviceIcon(), value: MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_CHOOSE_FROM_DEVICE, label: __( 'Choose from device' ) },
-			{ icon: this.getTakeMediaIcon(), value: MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_MEDIA, label: this.getTakeMediaLabel() },
+			...captureMediaOptionsItems,
 			{ icon: this.getWordPressLibraryIcon(), value: MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_WORD_PRESS_LIBRARY, label: __( 'WordPress Media Library' ) },
 		];
 	}
@@ -113,20 +128,39 @@ export class MediaUpload extends React.Component {
 		} );
 	}
 
+	// request a single image or video from camera
+	requestSpecificMediaTypeFromDeviceCamera( mediaType ) {
+		const { onSelect } = this.props;
+
+		requestMediaPickFromDeviceCamera( [ mediaType ], false, ( media ) => {
+			if ( media && media.id ) {
+				onSelect( media );
+			}
+		} );
+	}
+
 	onPickerChange( value ) {
-		if ( value === MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_CHOOSE_FROM_DEVICE ) {
-			this.onPickerSelect( requestMediaPickFromDeviceLibrary );
-		} else if ( value === MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_MEDIA ) {
-			this.onPickerSelect( requestMediaPickFromDeviceCamera );
-		} else if ( value === MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_WORD_PRESS_LIBRARY ) {
-			this.onPickerSelect( requestMediaPickFromMediaLibrary );
-		} else {
-			const { onSelect, multiple = false } = this.props;
-			requestOtherMediaPickFrom( value, multiple, ( media ) => {
-				if ( ( multiple && media ) || ( media && media.id ) ) {
-					onSelect( media );
-				}
-			} );
+		switch ( value ) {
+			case MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_CHOOSE_FROM_DEVICE:
+				this.onPickerSelect( requestMediaPickFromDeviceLibrary );
+				break;
+			case MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_PHOTO:
+				this.requestSpecificMediaTypeFromDeviceCamera( MEDIA_TYPE_IMAGE );
+				break;
+			case MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_TAKE_VIDEO:
+				this.requestSpecificMediaTypeFromDeviceCamera( MEDIA_TYPE_VIDEO );
+				break;
+			case MEDIA_UPLOAD_BOTTOM_SHEET_VALUE_WORD_PRESS_LIBRARY:
+				this.onPickerSelect( requestMediaPickFromMediaLibrary );
+				break;
+			default:
+				const { onSelect, multiple = false } = this.props;
+				requestOtherMediaPickFrom( value, multiple, ( media ) => {
+					if ( ( multiple && media ) || ( media && media.id ) ) {
+						onSelect( media );
+					}
+				} );
+				break;
 		}
 	}
 
