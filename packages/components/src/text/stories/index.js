a89de2c5d2ea726278179404908b4be486ba2637
James Newell <james.newell@automattic.com>
2019-11-15 08:49:27 +1100

    added a _default story with all variants in the one story

--- packages/components/src/text/stories/index.js
+++ packages/components/src/text/stories/index.js
@@ -8,6 +8,21 @@ export default {
 	component: Text,
 };
 
+export const _default = () => (
+	<>
+		<Text variant="title.large" as="h1">Title Large</Text>
+		<Text variant="title.medium" as="h2">Title Medium</Text>
+		<Text variant="title.small" as="h3">Title Small</Text>
+		<Text variant="subtitle">Subtitle</Text>
+		<Text variant="subtitle.small">Subtitle Small</Text>
+		<Text variant="body">Body</Text>
+		<Text variant="body.small">Body Small</Text>
+		<Text variant="button">Button</Text>
+		<Text variant="caption">Caption</Text>
+		<Text variant="label">Label</Text>
+	</>
+);
+
 export const TitleLarge = () => <Text variant="title.large" as="h1">Title Large</Text>;
 export const TitleMedium = () => <Text variant="title.medium" as="h2">Title Medium</Text>;
 export const TitleSmall = () => <Text variant="title.small" as="h3">Title Small</Text>;
