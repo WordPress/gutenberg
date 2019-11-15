7f144968e3d7df9b83aef0cbc5c590af722ae213
Haz <hazdiego@gmail.com>
2019-11-15 14:10:47 +0100

    Update Toolbar stories

--- packages/components/src/toolbar/stories/index.js
+++ packages/components/src/toolbar/stories/index.js
@@ -78,35 +78,3 @@ export const withoutGroup = () => {
 		</ToolbarWithId>
 	);
 };
-
-export const standaloneToolbarGroup = () => {
-	return (
-		<ToolbarGroup>
-			<ToolbarButton icon="editor-bold" title="Bold" isActive />
-			<ToolbarButton icon="editor-italic" title="Italic" />
-			<ToolbarButton icon="admin-links" title="Link" />
-		</ToolbarGroup>
-	);
-};
-
-export const standaloneToolbarGroupWithControlsProp = () => {
-	return (
-		<ToolbarGroup
-			controls={ [
-				{ icon: 'editor-bold', title: 'Bold', isActive: true },
-				{ icon: 'editor-italic', title: 'Italic' },
-				{ icon: 'admin-links', title: 'Link' },
-			] }
-		/>
-	);
-};
-
-export const legacyToolbar = () => {
-	return (
-		<Toolbar>
-			<ToolbarButton icon="editor-bold" title="Bold" />
-			<ToolbarButton icon="editor-italic" title="Italic" />
-			<ToolbarButton icon="admin-links" title="Link" />
-		</Toolbar>
-	);
-};
