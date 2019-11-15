5012b87f112ac54a9a1444429f791339a8e51eb5
Marcus Kazmierczak <marcus@mkaz.com>
2019-11-14 17:44:45 -0800

    Add story for Tip component

---
+++ packages/components/src/tip/stories/index.js
@@ -0,0 +1,13 @@
+
+/**
+ * Internal dependencies
+ */
+import Tip from '../';
+
+export default { title: 'Components|Tip', component: Tip };
+
+export const _default = () => (
+	<Tip>
+		<p>An example tip.</p>
+	</Tip>
+);
