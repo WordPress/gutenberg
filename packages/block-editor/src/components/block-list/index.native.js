2674522f31a0940bc64d646880b96d001944028c
Jakub Binda <jakub.binda@gmail.com>
2019-11-18 10:03:46 +0100

    fix show appender in group block

--- packages/block-editor/src/components/block-list/index.native.js
+++ packages/block-editor/src/components/block-list/index.native.js
@@ -63,13 +63,12 @@ export class BlockList extends Component {
 		const willShowInsertionPoint = shouldShowInsertionPointBefore(); // call without the client_id argument since this is the appender
 		return (
 			<ReadableContentView>
-				{ willShowInsertionPoint ?
-					this.renderAddBlockSeparator() :	// show the new-block indicator when we're inserting a block or
-					<BlockListAppender				// show the default appender, as normal, when not inserting a block
-						rootClientId={ this.props.rootClientId }
-						renderAppender={ this.props.renderAppender }
-					/>
-				}
+				{ /* show the new-block indicator when we're inserting a block */ }
+				{ willShowInsertionPoint && this.renderAddBlockSeparator() }
+				<BlockListAppender				// show the default appender, anormal, when not inserting a block
+					rootClientId={ this.props.rootClientId }
+					renderAppender={ this.props.renderAppender }
+				/>
 			</ReadableContentView>
 		);
 	}
