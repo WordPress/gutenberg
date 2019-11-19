aaa66258163373fb78b793ab8089cfa8b333639f
Dave Smith <getdavemail@gmail.com>
2019-11-19 14:01:29 +0000

    Moves component to @wordpress/components package
    Also updates LinkControl with new reference

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -66,6 +66,7 @@ export { default as Spinner } from './spinner';
 export { default as TabPanel } from './tab-panel';
 export { default as TextControl } from './text-control';
 export { default as TextareaControl } from './textarea-control';
+export { default as TextHighlight } from './text-highlight';
 export { default as Tip } from './tip';
 export { default as ToggleControl } from './toggle-control';
 export { default as Toolbar } from './toolbar';
