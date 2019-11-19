24fbb35dce9eb54ae1d17a4f37c0935dd4e4c5ba
Konstantin Obenland <obenland@gmx.de>
2019-11-19 03:51:31 -0800

    Navigation: Drop menu suffix (#18551)
    * Navigation: Drop menu suffix
    
    * Update fixtures
    
    * blocks: update navigation menu name
    
    * navigation: fix php-lint/eslint errors

--- packages/block-library/src/navigation-link/index.js
+++ packages/block-library/src/navigation-link/index.js
@@ -16,7 +16,7 @@ export { metadata, name };
 export const settings = {
 	title: __( 'Navigation Link' ),
 
-	parent: [ 'core/navigation-menu' ],
+	parent: [ 'core/navigation' ],
 
 	icon: <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24"><Path d="M12 7.27l4.28 10.43-3.47-1.53-.81-.36-.81.36-3.47 1.53L12 7.27M12 2L4.5 20.29l.71.71L12 18l6.79 3 .71-.71L12 2z" /></SVG>,
 
