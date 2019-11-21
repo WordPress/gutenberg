2af475bd7ab88b4fbeb7dd6c8b349fd96be083c3
Jb <jb@MacBook-Air-de-Rachel.local>
2019-11-21 07:20:57 +0100

    Update deprecated pattterns

--- packages/block-library/src/embed/core-embeds.js
+++ packages/block-library/src/embed/core-embeds.js
@@ -147,7 +147,7 @@ export const others = [
 				inserter: false,
 			}
 		},
-		patterns: [ /^https?:\/\/((m|www)\.)?youtube\.com\/.+/i, /^https?:\/\/youtu\.be\/.+/i ],
+		patterns: [ /^https?:\/\/(www\.)?collegehumor\.com\/.+/i, /^https?:\/\/((m|www)\.)?youtube\.com\/.+/i, /^https?:\/\/youtu\.be\/.+/i ],
 	},
 	{
 		name: 'core-embed/crowdsignal',
