74551b5b6203ca4131f23951364d71b827bee657
Riad Benguella <benguella@gmail.com>
2019-11-22 12:38:33 +0100

    Revert "Move the view section of the More Menu to the options panel (#18601)"
    This reverts commit fb234f5bf1610eb3a6377e571f5ab46832f62179.

--- packages/edit-post/src/components/options-modal/options/base.js
+++ packages/edit-post/src/components/options-modal/options/base.js
@@ -3,14 +3,13 @@
  */
 import { CheckboxControl } from '@wordpress/components';
 
-function BaseOption( { label, help, isChecked, onChange, children } ) {
+function BaseOption( { label, isChecked, onChange, children } ) {
 	return (
 		<div className="edit-post-options-modal__option">
 			<CheckboxControl
 				label={ label }
 				checked={ isChecked }
 				onChange={ onChange }
-				help={ help }
 			/>
 			{ children }
 		</div>
