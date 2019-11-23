40ffa61ae8427654d0176a5198dda5156ae6e936
devdivdev <abdel.hamdani213@gmail.com>
2019-11-24 00:12:25 +0100

    Added a disable state to the trash button

--- packages/editor/src/components/post-trash/index.js
+++ packages/editor/src/components/post-trash/index.js
@@ -2,19 +2,28 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 import { Button } from '@wordpress/components';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 
 function PostTrash( { isNew, postId, postType, ...props } ) {
+	const [ disabled, setDisabled ] = useState( false );
+
 	if ( isNew || ! postId ) {
 		return null;
 	}
+	const onClick = () => {
+		setDisabled( true );
 
-	const onClick = () => props.trashPost( postId, postType );
+		props.trashPost( postId, postType )
+			.then( () => {
+				setDisabled( false );
+			} );
+	};
 
 	return (
-		<Button className="editor-post-trash button-link-delete" onClick={ onClick } isDefault isLarge>
+		<Button className="editor-post-trash button-link-delete" disabled={ disabled } onClick={ onClick } isDefault isLarge>
 			{ __( 'Move to Trash' ) }
 		</Button>
 	);
