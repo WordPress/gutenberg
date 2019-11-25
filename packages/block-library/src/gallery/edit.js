971f725d4ae53702bec0c240ede6b9452fb5517e
Matthew Kevins <mmkevins@yahoo.com>
2019-11-25 22:11:28 +1000

    Limit mediaUpload in blobURL check to web

--- packages/block-library/src/gallery/edit.js
+++ packages/block-library/src/gallery/edit.js
@@ -225,7 +225,7 @@ class GalleryEdit extends Component {
 	componentDidMount() {
 		const { attributes, mediaUpload } = this.props;
 		const { images } = attributes;
-		if ( every( images, ( { url } ) => isBlobURL( url ) ) ) {
+		if ( Platform.OS === 'web' && every( images, ( { url } ) => isBlobURL( url ) ) ) {
 			const filesList = map( images, ( { url } ) => getBlobByURL( url ) );
 			forEach( images, ( { url } ) => revokeBlobURL( url ) );
 			mediaUpload( {
