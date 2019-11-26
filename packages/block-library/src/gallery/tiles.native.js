2356c4b0810f23fd2dc34556c06ad79b78461614
Matthew Kevins <mmkevins@yahoo.com>
2019-11-26 16:17:20 +1000

    Use paddings instead of borders for inter-tile spacing

--- packages/block-library/src/gallery/tiles.native.js
+++ packages/block-library/src/gallery/tiles.native.js
@@ -33,21 +33,20 @@ function Tiles( props ) {
 		 * in a given row.
 		 *
 		 * In order to ensure equal sizing of tile contents, we distribute the
-		 * spacing such that each tile has an equal "share" of the fixed spacing.
-		 * To keep the tiles properly aligned within their rows, we calculate the
-		 * left and right border widths based on the tile's relative position within
-		 * the row.
+		 * spacing such that each tile has an equal "share" of the fixed spacing. To
+		 * keep the tiles properly aligned within their rows, we calculate the left
+		 * and right paddings based on the tile's relative position within the row.
 		 *
-		 * Note: we use transparent borders instead of margins so that the fixed
-		 * spacing is included within the relative spacing (i.e. width percentage),
-		 * and wrapping behavior is preserved.
+		 * Note: we use padding instead of margins so that the fixed spacing is
+		 * included within the relative spacing (i.e. width percentage), and
+		 * wrapping behavior is preserved.
 		 *
-		 *  - The left most tile in a row must have left border width of zero.
-		 *  - The right most tile in a row must have a right border width of zero.
+		 *  - The left most tile in a row must have left padding of zero.
+		 *  - The right most tile in a row must have a right padding of zero.
 		 *
-		 * The values of these widths are interpolated for tiles in between. The
-		 * right border width is complementary with the left border width of the
-		 * next tile (i.e. the right border of [tile n] + the left border of
+		 * The values of these left and right paddings are interpolated for tiles in
+		 * between. The right padding is complementary with the left padding of the
+		 * next tile (i.e. the right padding of [tile n] + the left padding of
 		 * [tile n + 1] will be equal for all tiles except the last one in a given
 		 * row).
 		 */
@@ -59,10 +58,10 @@ function Tiles( props ) {
 		return (
 			<View style={ [ styles.tileStyle, {
 				width: `${ 100 / rowLength }%`,
-				borderLeftWidth: spacing * ( indexInRow / rowLength ),
-				borderRightWidth: spacing * ( 1 - ( ( indexInRow + 1 ) / rowLength ) ),
-				borderTopWidth: row === 0 ? 0 : spacing / 2,
-				borderBottomWidth: row === lastRow ? 0 : spacing / 2,
+				paddingLeft: spacing * ( indexInRow / rowLength ),
+				paddingRight: spacing * ( 1 - ( ( indexInRow + 1 ) / rowLength ) ),
+				paddingTop: row === 0 ? 0 : spacing / 2,
+				paddingBottom: row === lastRow ? 0 : spacing / 2,
 			} ] }>
 				{ child }
 			</View>
