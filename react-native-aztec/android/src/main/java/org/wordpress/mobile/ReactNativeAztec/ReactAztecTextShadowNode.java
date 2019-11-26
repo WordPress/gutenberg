08d89c99d00a53e0da64aaff8c0098c2fd0022ba
Matt Chowning <matt.chowning@automattic.com>
2019-11-26 09:59:07 -0500

    Stop applying default padding in ReactAztecTextShadowNode

--- react-native-aztec/android/src/main/java/org/wordpress/mobile/ReactNativeAztec/ReactAztecTextShadowNode.java
+++ react-native-aztec/android/src/main/java/org/wordpress/mobile/ReactNativeAztec/ReactAztecTextShadowNode.java
@@ -10,6 +10,13 @@ public class ReactAztecTextShadowNode extends ReactTextInputShadowNode {
     private @Nullable ReadableMap mTextMap = null;
     private @Nullable Integer mColor = null;
 
+    @Override
+    public void setDefaultPadding(int spacingType, float padding) {
+        // Do nothing in order to avoid adding unwanted default padding to views
+        // (https://github.com/wordpress-mobile/gutenberg-mobile/issues/992) due to
+        // https://github.com/facebook/react-native/blob/6ebd3b046e5b71130281f1a7dbe7220eff95d74a/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputShadowNode.java#L70
+    }
+
     @ReactProp(name = PROP_TEXT)
     public void setText(@Nullable ReadableMap inputMap) {
         mTextMap = inputMap;
