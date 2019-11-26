2f19c48342b5b6db21f98d67a3b68a647c0dc97a
Javon <javondavis@users.noreply.github.com>
2019-11-26 07:22:00 -0500

    revert caps to iPhone Simulator

--- __device-tests__/helpers/caps.js
+++ __device-tests__/helpers/caps.js
@@ -2,7 +2,7 @@ exports.ios12 = {
 	browserName: '',
 	platformName: 'iOS',
 	platformVersion: '12.2',
-	deviceName: 'iPhone 11',
+	deviceName: 'iPhone Simulator',
 	os: 'iOS',
 	deviceOrientation: 'portrait',
 	automationName: 'XCUITest',
