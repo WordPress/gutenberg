fdf4ba37771bd8a22cfd948d412ce3e4b8bc6c25
Michael P. Pfeiffer <michael@cssconf.eu>
2019-11-27 10:20:48 +0100

    temporary fix for issues caused by browser scrolling in mobile Safari

--- packages/block-editor/src/components/block-mobile-toolbar/index.js
+++ packages/block-editor/src/components/block-mobile-toolbar/index.js
@@ -8,6 +8,19 @@ import { ifViewportMatches } from '@wordpress/viewport';
  */
 import BlockMover from '../block-mover';
 
+// This is a temporary fix for a couple of issues specific to Webkit on iOS.
+// Without this fix the browser scrolls the mobile toolbar off-screen.
+// Once supported in Safari we can replace this in favor of preventScroll.
+// For details see issue #18632 and PR #18686
+const isIphone = window.navigator.userAgent.indexOf( 'iPhone' ) !== -1;
+if ( isIphone ) {
+	document.addEventListener( 'focusin', function( ) {
+		setTimeout( () => {
+			window.scrollTo( 0, 0 );
+		}, 150 );
+	} );
+}
+
 function BlockMobileToolbar( { clientId, moverDirection } ) {
 	return (
 		<div className="block-editor-block-mobile-toolbar">
