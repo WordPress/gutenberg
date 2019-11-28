a9f3651ee36224dff638d6f2fbd31272a860bdac
Matthew Kevins <mmkevins@yahoo.com>
2019-11-28 15:31:53 +1000

    Fix scss imports for jest

--- packages/block-library/src/gallery/shared-icon.native.js
+++ packages/block-library/src/gallery/shared-icon.native.js
@@ -8,7 +8,7 @@ import { withPreferredColorScheme } from '@wordpress/compose';
  * Internal dependencies
  */
 import { icon } from './icons.js';
-import styles from './styles';
+import styles from './styles.scss';
 
 const IconWithColorScheme = withPreferredColorScheme( ( { getStylesFromColorScheme } ) => {
 	const colorSchemeStyles = getStylesFromColorScheme( styles.icon, styles.iconDark );
