7deb95e5125e42239e887dd0c1e60931f1990022
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-12-01 15:07:41 +0200

    removes useless exports from ImageURLInputUI

--- packages/block-editor/src/components/url-popover/image-url-input-ui.js
+++ packages/block-editor/src/components/url-popover/image-url-input-ui.js
@@ -31,11 +31,11 @@ import {
  */
 import URLPopover from './index';
 
-export const LINK_DESTINATION_NONE = 'none';
-export const LINK_DESTINATION_CUSTOM = 'custom';
-export const LINK_DESTINATION_MEDIA = 'media';
-export const LINK_DESTINATION_ATTACHMENT = 'attachment';
-export const NEW_TAB_REL = [ 'noreferrer', 'noopener' ];
+const LINK_DESTINATION_NONE = 'none';
+const LINK_DESTINATION_CUSTOM = 'custom';
+const LINK_DESTINATION_MEDIA = 'media';
+const LINK_DESTINATION_ATTACHMENT = 'attachment';
+const NEW_TAB_REL = [ 'noreferrer', 'noopener' ];
 
 const icon = <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path d="M0,0h24v24H0V0z" fill="none" /><Path d="m19 5v14h-14v-14h14m0-2h-14c-1.1 0-2 0.9-2 2v14c0 1.1 0.9 2 2 2h14c1.1 0 2-0.9 2-2v-14c0-1.1-0.9-2-2-2z" /><Path d="m14.14 11.86l-3 3.87-2.14-2.59-3 3.86h12l-3.86-5.14z" /></SVG>;
 
