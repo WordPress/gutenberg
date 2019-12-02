c6c17582845e0d7071b085f448243097043f6c64
Jon Q <hello@jonquach.com>
2019-12-02 10:33:38 -0500

    Import block-library, block-editor, and format-library SCSS into Playground

--- storybook/stories/playground/index.js
+++ storybook/stories/playground/index.js
@@ -20,13 +20,6 @@ import {
 import { registerCoreBlocks } from '@wordpress/block-library';
 import '@wordpress/format-library';
 
-/* eslint-disable no-restricted-syntax */
-import '@wordpress/block-library/build-style/style.css';
-import '@wordpress/block-library/build-style/editor.css';
-import '@wordpress/block-library/build-style/theme.css';
-import '@wordpress/format-library/build-style/style.css';
-/* eslint-enable no-restricted-syntax */
-
 /**
  * Internal dependencies
  */
