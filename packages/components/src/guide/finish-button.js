574e8eabe42ef2cb007da4ced99bc4bc853d5009
Robert Anderson <robert@noisysocks.com>
2019-12-02 14:54:59 +0100

    Implement welcome guide modal (#18041)
    * Implement welcome guide modal
    
    * Welcome guide: Update images
    
    * Welcome guide: Capitalise 'Block Editor' and 'Block Library'
    
    * Welcome guide: Position close button 24px from edge
    
    * Welcome guide: Improve a11y of page controls
    
    * Welcome guide: Add unit tests
    
    * Welcome guide: Add E2E tests
    
    * Welcome guide: Mark Guide as __experimental, as we may want to move it to @wordpress/components
    
    * Welcome guide: Add README.md
    
    * Welcome guide: Rename WelcomeGuideModal to WelcomeGuide
    
    * Welcome guide: Update fixtures
    
    * Welcome guide: Rename Guide.Page to GuidePage
    
    * Welcome guide: Remove unnecessary onSelect prop
    
    * Welcome guide: Move Guide to @wordpress/components
    
    * Welcome guide: Remove snapshot test
    
    * Welcome guide: Use CSS to hide and show finish buton
    
    * Welcome guide: Add a storybook
    
    * Welcome guide: Use break-small() mixin
    
    * Welcome guide: Generate storybook snapshot
    
    * Welcome guide: Update @wordpress/components changelog
    
    * E2E tests: Reload page after disabling welcome guide / tips
    
    * Welcome guide: Close guide when user clicks on overlay

---
+++ packages/components/src/guide/finish-button.js
@@ -0,0 +1,33 @@
+/**
+ * WordPress dependencies
+ */
+import { useRef, useLayoutEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import Button from '../button';
+
+export default function FinishButton( { className, onClick, children } ) {
+	const button = useRef( null );
+
+	// Focus the button on mount if nothing else is focused. This prevents a
+	// focus loss when the 'Next' button is swapped out.
+	useLayoutEffect( () => {
+		if ( document.activeElement === document.body ) {
+			button.current.focus();
+		}
+	}, [ button ] );
+
+	return (
+		<Button
+			ref={ button }
+			className={ className }
+			isPrimary
+			isLarge
+			onClick={ onClick }
+		>
+			{ children }
+		</Button>
+	);
+}
--- packages/edit-post/src/components/layout/index.js
+++ packages/edit-post/src/components/layout/index.js
@@ -47,6 +47,7 @@ import Sidebar from '../sidebar';
 import MetaBoxes from '../meta-boxes';
 import PluginPostPublishPanel from '../sidebar/plugin-post-publish-panel';
 import PluginPrePublishPanel from '../sidebar/plugin-pre-publish-panel';
+import WelcomeGuide from '../welcome-guide';
 
 function Layout( { isMobileViewport } ) {
 	const { closePublishSidebar, togglePublishSidebar } = useDispatch( 'core/edit-post' );
@@ -141,6 +142,7 @@ function Layout( { isMobileViewport } ) {
 				<ManageBlocksModal />
 				<OptionsModal />
 				<KeyboardShortcutHelpModal />
+				<WelcomeGuide />
 				<Popover.Slot />
 				<PluginArea />
 				{ showPageTemplatePicker && <__experimentalPageTemplatePicker /> }
