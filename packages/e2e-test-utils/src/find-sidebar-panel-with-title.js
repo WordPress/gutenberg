f93ffac04a1010ab8542bcf80a0b40a2303ab465
Andrew Duthie <andrew@andrewduthie.com>
2019-12-02 12:26:16 -0500

    E2E Test Utils: Fix return type for ElementHandle promise

--- packages/e2e-test-utils/src/find-sidebar-panel-with-title.js
+++ packages/e2e-test-utils/src/find-sidebar-panel-with-title.js
@@ -10,11 +10,11 @@ import { first } from 'lodash';
  *
  * @param {string} panelTitle The name of sidebar panel.
  *
- * @return {?ElementHandle} Object that represents an in-page DOM element.
+ * @return {Promise<ElementHandle|undefined>} Object that represents an in-page DOM element.
  */
 export async function findSidebarPanelWithTitle( panelTitle ) {
 	const classSelect = ( className ) => `[contains(concat(" ", @class, " "), " ${ className } ")]`;
 	const buttonSelector = `//div${ classSelect( 'edit-post-sidebar' ) }//button${ classSelect( 'components-button' ) }${ classSelect( 'components-panel__body-toggle' ) }[contains(text(),"${ panelTitle }")]`;
 	const panelSelector = `${ buttonSelector }/ancestor::*[contains(concat(" ", @class, " "), " components-panel__body ")]`;
-	return first( await await page.$x( panelSelector ) );
+	return first( await page.$x( panelSelector ) );
 }
