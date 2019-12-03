e52c91c2a171dcba0ca4bd962d4a462fde91644c
Dave Smith <getdavemail@gmail.com>
2019-12-03 08:51:40 +0000

    Fix inlining of var
    Addresses https://github.com/WordPress/gutenberg/pull/18609#discussion_r348392899

--- packages/components/src/text-highlight/stories/index.js
+++ packages/components/src/text-highlight/stories/index.js
@@ -11,8 +11,7 @@ import TextHighlight from '../';
 export default { title: 'Components|TextHighlight', component: TextHighlight };
 
 export const _default = () => {
-	const defaultText = 'We call the new editor Gutenberg. The entire editing experience has been rebuilt for media rich pages and posts.';
-	const textToMatch = text( 'Text', defaultText );
+	const textToMatch = text( 'Text', 'We call the new editor Gutenberg. The entire editing experience has been rebuilt for media rich pages and posts.' );
 
 	const textToHighlight = text( 'Text to be highlighted ', 'Gutenberg' );
 
