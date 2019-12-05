5e8f0cd9f4dd2bf32d1f3e8e52c1979b4fb85a34
Daniel Richards <daniel.richards@automattic.com>
2019-12-05 12:44:41 +0800

    Add tests for adding navigation blocks using two different methods

--- packages/e2e-tests/specs/editor/blocks/navigation.test.js
+++ packages/e2e-tests/specs/editor/blocks/navigation.test.js
@@ -4,26 +4,51 @@
  */
 import {
 	createNewPost,
+	getEditedPostContent,
 	insertBlock,
 } from '@wordpress/e2e-test-utils';
 
-describe( 'Adds Navigation links', () => {
+describe( 'Navigation', () => {
 	beforeEach( async () => {
 		await createNewPost();
 	} );
 
-	it( 'Should add a link with one click', async () => {
+	it( 'allows a navigation menu to be created using existing pages', async () => {
+		// Add the navigation block.
 		await insertBlock( 'Navigation' );
+
+		// Create an empty nav block.
+		const [ createFromExistingButton ] = await page.$x( '//button[text()="Create from all top pages"]' );
+		await createFromExistingButton.click();
+
+		// Snapshot should contain the default 'Sample Page'.
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'allows a navigation menu to be created from an empty menu using a mixture of internal and external links', async () => {
+		// Add the navigation block.
+		await insertBlock( 'Navigation' );
+
+		// Create an empty nav block.
 		const [ createEmptyButton ] = await page.$x( '//button[text()="Create empty"]' );
 		await createEmptyButton.click();
-		await page.waitForSelector( 'input[placeholder="Search or type url"]' );
-		await page.type( 'input[placeholder="Search or type url"]', 'http://example.com' );
 
+		// Add a link to the default Navigation Link block.
+		await page.type( 'input[placeholder="Search or type url"]', 'https://wordpress.org' );
 		await page.keyboard.press( 'Enter' );
+		await page.type( '.wp-block-navigation-link__content.is-selected', 'Home' );
+
+		// Add another Navigation Link block.
+		// Using 'click' here checks for regressions of https://github.com/WordPress/gutenberg/issues/18329,
+		// an issue where the block appender requires two clicks.
 		await page.click( '.wp-block-navigation .block-list-appender' );
 
-		const navigationLinkCount = await page.$$eval( '.wp-block-navigation-link', ( navigationLinks ) => navigationLinks.length );
+		// Add a link to the default Navigation Link block.
+		await page.type( 'input[placeholder="Search or type url"]', 'Sample Page' );
+		await page.keyboard.press( 'Enter' );
+		await page.type( '.wp-block-navigation-link__content.is-selected', 'Sample' );
 
-		expect( navigationLinkCount ).toBe( 2 );
+		// Expect a Navigation Block with two Navigation Links in the snapshot.
+		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 } );
