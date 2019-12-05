20d2cdf4f6cb639871509c5e7a10b93896324d6b
Jorge Costa <jorge.costa@automattic.com>
2019-12-05 21:47:53 +0000

    Fix: Video block crashes right after insert (#18954)

--- packages/block-library/src/video/edit.js
+++ packages/block-library/src/video/edit.js
@@ -54,7 +54,6 @@ class VideoEdit extends Component {
 
 		this.videoPlayer = createRef();
 		this.posterImageButton = createRef();
-		this.toggleAttribute = this.toggleAttribute.bind( this );
 		this.onSelectURL = this.onSelectURL.bind( this );
 		this.onSelectPoster = this.onSelectPoster.bind( this );
 		this.onRemovePoster = this.onRemovePoster.bind( this );
