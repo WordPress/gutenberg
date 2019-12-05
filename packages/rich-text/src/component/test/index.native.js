ef1bebffefe2295cd50df78afd0f433378b7a0b4
Marko Savic <savicmarko1985@gmail.com>
2019-12-05 16:08:16 +0100

    Add test case for preformatted block

--- packages/rich-text/src/component/test/index.native.js
+++ packages/rich-text/src/component/test/index.native.js
@@ -23,5 +23,11 @@ describe( 'RichText Native', () => {
 			const html = '<p><b>Hello</b> <strong>Hello</strong> WorldWorld!</p>';
 			expect( richText.willTrimSpaces( html ) ).toBe( false );
 		} );
+
+		it( 'reports false for Preformatted block', () => {
+			const html = '<pre>Hello World <br><br><br></pre>';
+			richText.props.tagName = "pre"
+			expect( richText.willTrimSpaces( html ) ).toBe( false );
+		} );
 	} );
 } );
