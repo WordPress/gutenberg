3e8cd97b40c7e2d14557bce01aeeee6bab076691
Mikko Saari <mikko@mikkosaari.fi>
2019-12-08 09:07:40 +0200

    Adds Telegram to the Social Link block

---
+++ packages/block-library/src/social-link/icons/telegram.js
@@ -0,0 +1,14 @@
+/**
+ * WordPress dependencies
+ */
+
+import {
+	Path,
+	SVG,
+} from '@wordpress/components';
+
+export const TelegramIcon = ( ) => (
+	<SVG width="24" height="24" viewBox="0 0 24 24" version="1.1">
+		<Path d="M8.619 15.177 L16.772 21.201 C17.702 21.714 18.374 21.449 18.605 20.337 L21.924 4.698 C22.264 3.336 21.405 2.718 20.515 3.122 L1.027 10.636 C-0.303 11.17 -0.295 11.912 0.785 12.243 L5.786 13.804 17.363 6.5 C17.91 6.168 18.412 6.346 18 6.712" />
+	</SVG>
+);
