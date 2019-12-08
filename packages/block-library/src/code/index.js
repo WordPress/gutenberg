fe22b9d890dbb51b1d7cc48326867f8d1f7dea3f
Shizumi Yoshiaki <yoshiaki@shizumi.net>
2019-12-09 08:01:01 +0900

    Add "__" accidentally deleted (#19001)
    * Fix #18151
    
    * Directly describe \ n without using sprintf.
    
    * Fixed "__" being deleted

--- packages/block-library/src/code/index.js
+++ packages/block-library/src/code/index.js
@@ -22,7 +22,7 @@ export const settings = {
 	icon,
 	example: {
 		attributes: {
-			content: '// A "block" is the abstract term used\n// to describe units of markup that\n// when composed together, form the\n// content or layout of a page.\nregisterBlockType( name, settings );',
+			content: __( '// A "block" is the abstract term used\n// to describe units of markup that\n// when composed together, form the\n// content or layout of a page.\nregisterBlockType( name, settings );' ),
 		},
 	},
 	supports: {
