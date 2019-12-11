fdd69535ecbec018bfabad783c890de3d72b7e07
jbinda <jakub.binda@gmail.com>
2019-12-11 09:38:21 +0100

    [RNMobile] Inner Block Navigate down through hierarchy (#17547)
    * WIP: navigate down in the hierarchy of InnerBlocks
    
    WIP: add hierarchy of click-down
    
    WIP: add hierarchy of click other element same parent
    
    EOD: click-down logic
    
    EOD: click-down logic fix
    
    WIP: dim unfocus element
    
    WIP: add navigate up button on floating toolbar
    
    EOD: change color of FloatingToolbar arrow to white
    
    change focus border color from gray to blue
    
    EOD: click-down logic
    
    lock onFocus on AztecView when block is not selected
    
    change dashed border color
    
    apply style to keep same innerblock size
    
    work with styles
    
    work with styles
    
    adjust borders margin
    
    pass isParentSelected to RichText to unlock onClick event
    
    * WIP: Navigation Dwon in Inner Block
    
    * merge master
    
    * adjust style
    
    * fix after merge
    
    * fix after merge
    
    * playing with borders
    
    * working full border
    
    * refactor v1
    
    * refactor v2
    
    * fixed dashed border android
    
    * refactor v3
    
    * adjust button-block-appender margin
    
    * refactor v4
    
    * reefactor v5
    
    * refactor v6
    
    * adjust for media text
    
    * adjust focus for media text
    
    * refactor apply styles
    
    * adjust media text media container
    
    * pass isParentSelected prop to BlockEdit
    
    * adjust dim style
    
    * Move FloatingToolbar logic to get shippable temporary version
    
    * fix after merge floating-toolbr
    
    * style media-text and fix jumping behaviour
    
    * add getPrefferedColorScheme for dashed border
    
    * enable FloatingToolbar for MediaText
    
    * refactor rich text handler
    
    * change dark-mode block holder border to blue
    
    * dark-mode for group appender and placeholder
    
    * adjust margin for group placeholder
    
    * adjust innerblock margin for media-text
    
    * adjust group placeholder border color
    
    * adjust appender button color
    
    * re-activate the stacking vertically on Media & Text
    
    * CR changes
    
    * CR changes for Android RichText focus
    
    * CR changes for MediaText margins
    
    * CR changes for getTree selector
    
    * CR changes: fix selection loop
    
    * clean passing props after seletion loop issue fix
    
    * replace block specific text
    
    * replace block specific style
    
    * fix getParent selectors
    
    * adjust selection logic
    
    * fix hasInnerBlocks
    
    * refactor selectors
    
    * refactor navigate-down logic
    
    * fix dimed style aplied
    
    * add unit test for selector
    
    * use getBlockCount to check if block has children
    
    * fix group margins, placeholder and appender
    
    * adjustments
    
    * add parameters to style
    
    * refactor for removing isGroupType
    
    * refactor group placeholder
    
    * adjust media-text
    
    * finalise group appender logic
    
    * selection and dim adjustments
    
    * handle semi-bordered
    
    * refactor after tug review
    
    * Update packages/block-editor/src/store/selectors.js
    
    Co-Authored-By: Jorge Bernal <jbernal@gmail.com>
    
    * Update packages/block-editor/src/store/selectors.js
    
    Co-Authored-By: Jorge Bernal <jbernal@gmail.com>
    
    * Update packages/block-library/src/media-text/edit.native.js
    
    Co-Authored-By: Jorge Bernal <jbernal@gmail.com>
    
    * export styles for media-text to stylesheet
    
    * fix order in test case
    
    * refactor stylesheet in mediatext
    
    * group appender logic
    
    * @dratwas CR refactor
    
    * @dratwas CR refactor on descendant

--- packages/block-editor/src/components/block-list/index.native.js
+++ packages/block-editor/src/components/block-list/index.native.js
@@ -21,7 +21,6 @@ import styles from './style.scss';
 import BlockListBlock from './block';
 import BlockListAppender from '../block-list-appender';
 import __experimentalBlockListFooter from '../block-list-footer';
-import FloatingToolbar from './block-mobile-floating-toolbar';
 
 const innerToolbarHeight = 44;
 
@@ -41,10 +40,6 @@ export class BlockList extends Component {
 
 	addBlockToEndOfPost( newBlock ) {
 		this.props.insertBlock( newBlock, this.props.blockCount );
-	}
-
-	blockHolderBorderStyle() {
-		return this.props.isFullyBordered ? styles.blockHolderFullBordered : styles.blockHolderSemiBordered;
 	}
 
 	onCaretVerticalPositionChange( targetId, caretY, previousCaretY ) {
@@ -76,15 +71,22 @@ export class BlockList extends Component {
 	}
 
 	render() {
-		const { clearSelectedBlock, blockClientIds, isFullyBordered, title, header, withFooter = true, renderAppender, isFirstBlock, selectedBlockParentId, isRootList } = this.props;
+		const {
+			clearSelectedBlock,
+			blockClientIds,
+			isFullyBordered,
+			title,
+			header,
+			withFooter = true,
+			renderAppender,
+			isRootList,
+		} = this.props;
 
-		const showFloatingToolbar = isFirstBlock && selectedBlockParentId !== '';
 		return (
 			<View
 				style={ { flex: isRootList ? 1 : 0 } }
 				onAccessibilityEscape={ clearSelectedBlock }
 			>
-				{ showFloatingToolbar && <FloatingToolbar.Slot fillProps={ { innerFloatingToolbar: showFloatingToolbar } } /> }
 				<KeyboardAwareFlatList
 					{ ...( Platform.OS === 'android' ? { removeClippedSubviews: false } : {} ) } // Disable clipping on Android to fix focus losing. See https://github.com/wordpress-mobile/gutenberg-mobile/pull/741#issuecomment-472746541
 					accessibilityLabel="block-list"
@@ -104,11 +106,14 @@ export class BlockList extends Component {
 					ListFooterComponent={ withFooter && this.renderBlockListFooter }
 				/>
 
-				{ renderAppender && blockClientIds.length > 0 &&
-					<BlockListAppender
-						rootClientId={ this.props.rootClientId }
-						renderAppender={ this.props.renderAppender }
-					/>
+				{ renderAppender && blockClientIds.length > 0 && (
+					<View style={ styles.paddingToContent }>
+						<BlockListAppender
+							rootClientId={ this.props.rootClientId }
+							renderAppender={ this.props.renderAppender }
+						/>
+					</View>
+				)
 				}
 
 			</View>
@@ -123,7 +128,6 @@ export class BlockList extends Component {
 	}
 
 	renderItem( { item: clientId, index } ) {
-		const blockHolderFocusedStyle = this.props.getStylesFromColorScheme( styles.blockHolderFocused, styles.blockHolderFocusedDark );
 		const { shouldShowBlockAtIndex, shouldShowInsertionPointBefore, shouldShowInsertionPointAfter } = this.props;
 		return (
 			<ReadableContentView>
@@ -135,8 +139,7 @@ export class BlockList extends Component {
 						clientId={ clientId }
 						rootClientId={ this.props.rootClientId }
 						onCaretVerticalPositionChange={ this.onCaretVerticalPositionChange }
-						borderStyle={ this.blockHolderBorderStyle() }
-						focusedBorderColor={ blockHolderFocusedStyle.borderColor }
+						isSmallScreen={ ! this.props.isFullyBordered }
 					/> ) }
 				{ shouldShowInsertionPointAfter( clientId ) && this.renderAddBlockSeparator() }
 			</ReadableContentView>
@@ -180,15 +183,18 @@ export default compose( [
 			getBlockInsertionPoint,
 			isBlockInsertionPointVisible,
 			getSelectedBlock,
-			getBlockRootClientId,
 		} = select( 'core/block-editor' );
+
+		const {
+			getGroupingBlockName,
+		} = select( 'core/blocks' );
 
 		const selectedBlockClientId = getSelectedBlockClientId();
 		const blockClientIds = getBlockOrder( rootClientId );
 		const insertionPoint = getBlockInsertionPoint();
 		const blockInsertionPointIsVisible = isBlockInsertionPointVisible();
 		const selectedBlock = getSelectedBlock();
-		const isSelectedGroup = selectedBlock && selectedBlock.name === 'core/group';
+		const hasInnerBlocks = selectedBlock && selectedBlock.name === getGroupingBlockName();
 		const shouldShowInsertionPointBefore = ( clientId ) => {
 			return (
 				blockInsertionPointIsVisible &&
@@ -216,13 +222,9 @@ export default compose( [
 
 		const selectedBlockIndex = getBlockIndex( selectedBlockClientId, rootClientId );
 
-		const isFirstBlock = selectedBlockIndex === 0;
-
-		const selectedBlockParentId = getBlockRootClientId( selectedBlockClientId );
-
 		const shouldShowBlockAtIndex = ( index ) => {
 			const shouldHideBlockAtIndex = (
-				! isSelectedGroup && blockInsertionPointIsVisible &&
+				! hasInnerBlocks && blockInsertionPointIsVisible &&
 				// if `index` === `insertionPoint.index`, then block is replaceable
 				index === insertionPoint.index &&
 				// only hide selected block
@@ -239,8 +241,6 @@ export default compose( [
 			shouldShowInsertionPointBefore,
 			shouldShowInsertionPointAfter,
 			selectedBlockClientId,
-			isFirstBlock,
-			selectedBlockParentId,
 			isRootList: rootClientId === undefined,
 		};
 	} ),
