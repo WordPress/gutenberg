a199290c604ea630f96536b436456814a76f614b
Jorge Costa <jorge.costa@automattic.com>
2019-12-11 16:17:42 +0000

    Update packages/compose/src/hooks/use-media-query/index.js
    Co-Authored-By: Andrew Duthie <andrew@andrewduthie.com>

--- packages/compose/src/hooks/use-media-query/index.js
+++ packages/compose/src/hooks/use-media-query/index.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from '@wordpress/element';
 /**
  * Runs a media query and returns its value when it changes.
  *
- * @param {?string} query Media Query.
+ * @param {string} [query] Media Query.
  * @return {boolean} return value of the media query.
  */
 export default function useMediaQuery( query ) {
