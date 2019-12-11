f0126d2cee0ce4d49c24dcc712d9baa9bd82a94d
Dratwas <drapich.piotr@gmail.com>
2019-12-11 17:44:23 +0100

    Add 'button' and 'a' tag primitives and use them in components/button

--- packages/components/src/button/index.js
+++ packages/components/src/button/index.js
@@ -6,26 +6,29 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { createElement, forwardRef } from '@wordpress/element';
+import { forwardRef } from '@wordpress/element';
 
-export function Button( props, ref ) {
-	const {
-		href,
-		target,
-		isPrimary,
-		isLarge,
-		isSmall,
-		isTertiary,
-		isPressed,
-		isBusy,
-		isDefault,
-		isLink,
-		isDestructive,
-		className,
-		disabled,
-		...additionalProps
-	} = props;
+/**
+ * Internal dependencies
+ */
+import { Button as PrimitiveButton, A } from '../styled-primitives/button';
 
+export function Button( {
+	href,
+	target,
+	isPrimary,
+	isLarge,
+	isSmall,
+	isTertiary,
+	isPressed,
+	isBusy,
+	isDefault,
+	isLink,
+	isDestructive,
+	className,
+	disabled,
+	...additionalProps
+}, ref ) {
 	const classes = classnames( 'components-button', className, {
 		'is-button': isDefault || isPrimary || isLarge || isSmall,
 		'is-default': isDefault || ( ! isPrimary && ( isLarge || isSmall ) ),
@@ -43,13 +46,10 @@ export function Button( props, ref ) {
 	const tagProps = tag === 'a' ?
 		{ href, target } :
 		{ type: 'button', disabled, 'aria-pressed': isPressed };
-
-	return createElement( tag, {
-		...tagProps,
-		...additionalProps,
-		className: classes,
-		ref,
-	} );
+	const propsToPass = { ...tagProps, ...additionalProps, className: classes, ref };
+	if ( tag === 'a' ) {
+		return <A { ...propsToPass } />;
+	} return <PrimitiveButton { ...propsToPass } />;
 }
 
 export default forwardRef( Button );
