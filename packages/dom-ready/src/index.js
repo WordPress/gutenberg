e05df61f90e48545833654de4a827ed97a43770b
Jon Surrell <jon.surrell@automattic.com>
2019-12-12 09:26:56 +0100

    Type dom-ready package

--- packages/dom-ready/src/index.js
+++ packages/dom-ready/src/index.js
@@ -1,7 +1,7 @@
 /**
  * Specify a function to execute when the DOM is fully loaded.
  *
- * @param {Function} callback A function to execute after the DOM is ready.
+ * @param {() => void} callback A function to execute after the DOM is ready.
  *
  * @example
  * ```js
@@ -14,16 +14,14 @@
  *
  * @return {void}
  */
-const domReady = function( callback ) {
+export default function domReady( callback ) {
 	if (
 		document.readyState === 'complete' || // DOMContentLoaded + Images/Styles/etc loaded, so we call directly.
 		document.readyState === 'interactive' // DOMContentLoaded fires at this point, so we call directly.
 	) {
-		return callback();
+		return void callback();
 	}
 
 	// DOMContentLoaded has not fired yet, delay callback until then.
 	document.addEventListener( 'DOMContentLoaded', callback );
-};
-
-export default domReady;
+}
