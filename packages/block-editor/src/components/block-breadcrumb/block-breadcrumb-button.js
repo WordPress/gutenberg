88d9b0d1783aa020405838f3f233b5575054e3ba
Dratwas <drapich.piotr@gmail.com>
2019-12-13 11:27:11 +0100

    use styled-components for css pseudoclasses

--- packages/block-editor/src/components/block-breadcrumb/block-breadcrumb-button.js
+++ packages/block-editor/src/components/block-breadcrumb/block-breadcrumb-button.js
@@ -1,7 +1,18 @@
 /**
  * WordPress dependencies
  */
-import { Button } from '@wordpress/components';
+import { StyledPrimitives, styled } from '@wordpress/components';
+
+const Button = styled( StyledPrimitives.Button )`
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+    color: ${ ( props ) => props.theme.colors.primary };
+  }
+  &:focus{
+		outline: none;
+	}
+`;
 
 export default function BlockBreadcrumbButton( { children, ...props } ) {
 	return <Button
@@ -11,8 +22,12 @@ export default function BlockBreadcrumbButton( { children, ...props } ) {
 		color={ 'dark-gray-500' }
 		fontSize={ 'inherit' }
 		line-height={ 'xlarge' }
-		height={ 'xlarge' }
+		height={ 28 }
 		isTertiary
+		display={ 'inline-flex' }
+		textDecoration={ 'none' }
+		bg={ 'none' }
+		border={ 0 }
 		{ ...props }
 	>
 		{ children }
---
+++ packages/components/src/styled-primitives/index.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+export { default as styled } from 'styled-components';
+
+/**
+ * Internal dependencies
+ */
+import { A, Button } from './button';
+
+export default {
+	A,
+	Button,
+};
