b216ad50be4794324200fe20aa7843c7a8364a96
Jorge Bernal <jorge@automattic.com>
2019-12-13 15:27:57 +0100

    Set a fixed height for the template preview

--- packages/block-editor/src/components/page-template-picker/preview.native.js
+++ packages/block-editor/src/components/page-template-picker/preview.native.js
@@ -4,11 +4,29 @@
 import { BlockEditorProvider, BlockList } from '@wordpress/block-editor';
 import { BottomSheet } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * External dependencies
  */
-import { View, Text, Modal } from 'react-native';
+import { Dimensions, View, ScrollView, StyleSheet, Text } from 'react-native';
+import Modal from 'react-native-modal';
+
+const useScreenDimensions = ( dimension = 'window' ) => {
+	const [ dimensions, setDimensions ] = useState( Dimensions.get( dimension ) );
+
+	useEffect( () => {
+		const onChange = result => {
+		setDimensions( result[ dimension ] );
+		};
+
+		Dimensions.addEventListener( 'change', onChange );
+
+		return () => Dimensions.removeEventListener( 'change', onChange );
+	} );
+
+	return dimensions;
+};
 
 // We are replicating this here because the one in @wordpress/block-editor always
 // tries to scale the preview and we would need a lot of cross platform code to handle
@@ -20,18 +38,27 @@ const BlockPreview = ( { blocks } ) => {
 	const settings = useSelect( ( select ) => {
 		return select( 'core/block-editor' ).getSettings()
 	} );
+	const { height: windowHeight } = useScreenDimensions();
+	const style = StyleSheet.create( {
+		container: {
+			height: windowHeight - 150,
+		},
+	} );
 
 	return (
 		<BlockEditorProvider
 			value={ blocks }
 			settings={ settings }
 		>
-			<View  style={{minHeight: '50%'}}>
-				<BlockList isPreview={ true } />
+			<View style={ style.container }>
+				<BlockList
+					isPreview={ true }
+				/>
 			</View>
 		</BlockEditorProvider>
 	);
 };
+BlockPreview.displayName = 'BlockPreview';
 
 const Preview = ( props ) => {
 	const { template, onDismiss } = props;
@@ -45,9 +72,7 @@ const Preview = ( props ) => {
 			isVisible={ !! template }
 			onClose={ onDismiss }
 		>
-			<View>
-				<BlockPreview blocks={ template.blocks } />
-			</View>
+			<BlockPreview blocks={ template.blocks } />
 		</BottomSheet>
 	);
 };
