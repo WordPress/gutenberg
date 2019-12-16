1d3582f81b4b5c3f0ac1a39a9cd7cf55b0ca9d12
Sergio Estevao <sergioestevao@gmail.com>
2019-12-16 11:36:32 +0000

    Implement deprecation path from post where ids can be strings.

--- packages/block-library/src/gallery/deprecated.js
+++ packages/block-library/src/gallery/deprecated.js
@@ -19,6 +19,136 @@ const deprecated = [
 		attributes: {
 			images: {
 				type: 'array',
+				default: [ ],
+				source: 'query',
+				selector: '.blocks-gallery-item',
+				query: {
+					url: {
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'src'
+					},
+					fullUrl: {
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-full-url'
+					},
+					link: {
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-link'
+					},
+					alt: {
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'alt',
+						default: ''
+					},
+					id: {
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-id'
+					},
+					caption: {
+						type: 'string',
+						source: 'html',
+						selector: '.blocks-gallery-item__caption'
+					}
+				}
+			},
+			ids: {
+				type: 'array',				  
+				default: [ ]
+			},
+			columns: {
+				'type': 'number'
+			},
+			caption: {
+				type: 'string',
+				source: 'html',
+				selector: '.blocks-gallery-caption'
+			},
+			imageCrop: {
+				type: 'boolean',
+				default: true
+			},
+			linkTo: {
+				type: 'string',
+				default: 'none'
+			}
+		},
+		isEligible( { ids } ) {
+			return ids &&
+				ids.length > 0 ;
+		},
+		migrate( attributes ) {
+			return {
+				...attributes,
+				ids: map( attributes.ids, ( id ) => {
+					if ( ! id ) {
+						return null;
+					}
+					if ( typeof( id ) === 'string' ) {
+						return parseInt( id, 10 );
+					}
+					if ( typeof( id ) === 'number' ) {
+						return id;
+					}
+
+					return null;
+				} ),
+			};
+		},
+		save( { attributes } ) {
+			const { images, columns = defaultColumnsNumber( attributes ), imageCrop, caption, linkTo } = attributes;
+		
+			return (
+				<figure className={ `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }` }>
+					<ul className="blocks-gallery-grid">
+						{ images.map( ( image ) => {
+							let href;
+		
+							switch ( linkTo ) {
+								case 'media':
+									href = image.fullUrl || image.url;
+									break;
+								case 'attachment':
+									href = image.link;
+									break;
+							}
+		
+							const img = (
+								<img
+									src={ image.url }
+									alt={ image.alt }
+									data-id={ image.id }
+									data-full-url={ image.fullUrl }
+									data-link={ image.link }
+									className={ image.id ? `wp-image-${ image.id }` : null }
+								/>
+							);
+		
+							return (
+								<li key={ image.id || image.url } className="blocks-gallery-item">
+									<figure>
+										{ href ? <a href={ href }>{ img }</a> : img }
+										{ ! RichText.isEmpty( image.caption ) && (
+											<RichText.Content tagName="figcaption" className="blocks-gallery-item__caption" value={ image.caption } />
+										) }
+									</figure>
+								</li>
+							);
+						} ) }
+					</ul>
+					{ ! RichText.isEmpty( caption ) && <RichText.Content tagName="figcaption" className="blocks-gallery-caption" value={ caption } /> }
+				</figure>
+			);
+		}
+	},
+	{
+		attributes: {
+			images: {
+				type: 'array',
 				default: [],
 				source: 'query',
 				selector: 'ul.wp-block-gallery .blocks-gallery-item',
@@ -71,7 +201,7 @@ const deprecated = [
 				type: 'string',
 				default: 'none',
 			},
-		},
+		},		
 		save( { attributes } ) {
 			const { images, columns = defaultColumnsNumber( attributes ), imageCrop, linkTo } = attributes;
 			return (
