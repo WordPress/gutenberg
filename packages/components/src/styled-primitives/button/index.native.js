1a0b98301bcca7ef53cf13aacbc2ed94f8071519
Dratwas <drapich.piotr@gmail.com>
2019-12-16 18:18:22 +0100

    change to TouchableOpacity

--- packages/components/src/styled-primitives/button/index.native.js
+++ packages/components/src/styled-primitives/button/index.native.js
@@ -6,7 +6,7 @@ import styled from 'styled-components/native';
 import { color, space, layout, flexbox, background, border, position, shadow }
 	from 'styled-system';
 
-export const Button = styled.TouchableWithoutFeedback`
+export const Button = styled.TouchableOpacity`
 ${ color }
 ${ space }
 ${ layout }
