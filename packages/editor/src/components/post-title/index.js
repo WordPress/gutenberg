5d42d249107017b525be0d518b93cd3f32fb0b11
Glen Davies <glen.davies@a8c.com>
2019-12-17 13:08:34 +1300

    Try using decode entities instead

--- packages/editor/src/components/post-title/index.js
+++ packages/editor/src/components/post-title/index.js
@@ -123,7 +123,7 @@ class PostTitle extends Component {
 							<Textarea
 								id={ `post-title-${ instanceId }` }
 								className="editor-post-title__input"
-								value={ title }
+								value={ decodeEntities( title ) }
 								onChange={ this.onChange }
 								placeholder={ decodedPlaceholder || __( 'Add title' ) }
 								onFocus={ this.onSelect }
