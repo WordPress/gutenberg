cb84a924ab472ca959d258a83716101ddac50c6b
epiqueras <epiquerass@gmail.com>
2019-12-17 15:18:38 -0300

    Template Part: Implement edit component.

--- packages/block-library/src/template-part/edit.js
+++ packages/block-library/src/template-part/edit.js
@@ -1,3 +1,71 @@
-export default function TemplatePartEdit() {
-	return 'Template Part Placeholder';
+/**
+ * WordPress dependencies
+ */
+import {
+	useEntitySyncedInnerBlocks,
+	EntityProvider,
+} from '@wordpress/core-data';
+import { InnerBlocks } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { useRef, useEffect } from '@wordpress/element';
+
+function TemplatePart() {
+	useEntitySyncedInnerBlocks( 'postType', 'wp_template_part', {
+		initialEdits: { status: 'publish' },
+	} );
+	return <InnerBlocks />;
+}
+
+export default function TemplatePartEdit( {
+	attributes: { postId: _postId, slug, theme },
+	setAttributes,
+} ) {
+	const postId = useSelect(
+		( select ) => {
+			if ( _postId ) {
+				// This is already a custom template part,
+				// use its CPT post.
+				return (
+					select( 'core' ).getEntityRecord(
+						'postType',
+						'wp_template_part',
+						_postId
+					) && _postId
+				);
+			}
+
+			// This is not a custom template part,
+			// load the auto-draft created from the
+			// relevant file.
+			const posts = select( 'core' ).getEntityRecords(
+				'postType',
+				'wp_template_part',
+				{
+					status: 'auto-draft',
+					slug,
+					meta: { theme },
+				}
+			);
+
+			return posts && posts[ 0 ].id;
+		},
+		[ _postId, slug, theme ]
+	);
+
+	// Set the post ID, once found, so that edits persist.
+	const initialPostId = useRef( _postId );
+	useEffect( () => {
+		if (
+			( initialPostId.current === undefined || initialPostId.current === null ) &&
+			postId !== undefined &&
+			postId !== null
+		) {
+			setAttributes( { postId } );
+		}
+	}, [ postId ] );
+	return postId ? (
+		<EntityProvider kind="postType" type="wp_template_part" id={ postId }>
+			<TemplatePart />
+		</EntityProvider>
+	) : null;
 }
