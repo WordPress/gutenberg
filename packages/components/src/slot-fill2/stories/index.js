13b60eff03aaed39bd743f3f80a176a0bb910766
Haz <hazdiego@gmail.com>
2019-12-19 19:39:24 -0300

    Update stories

--- packages/components/src/slot-fill2/stories/index.js
+++ packages/components/src/slot-fill2/stories/index.js
@@ -1,4 +1,14 @@
 /**
+ * External dependencies
+ */
+import { text, number } from '@storybook/addon-knobs';
+
+/**
+ * WordPress dependencies
+ */
+import { createContext, useContext } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
 import { Slot, Fill, Provider } from '../';
@@ -8,14 +18,46 @@ export default { title: 'Components|SlotFill', component: Slot };
 export const _default = () => {
 	return (
 		<Provider>
-			<div className="slot">
-				<Slot name="a" />
-			</div>
-			<div className="fill">
-				<Fill name="a">
-					<div className="fill-children">fill</div>
-				</Fill>
-			</div>
+			<h2>Profile</h2>
+			<p>Name: <Slot as="span" name="name" /></p>
+			<p>Age: <Slot as="span" name="age" /></p>
+			<Fill name="name">Grace</Fill>
+			<Fill name="age">33</Fill>
+		</Provider>
+	);
+};
+
+export const withFillProps = () => {
+	const name = text( 'name', 'Grace' );
+	const age = number( 'age', 33 );
+	return (
+		<Provider>
+			<h2>Profile</h2>
+			<p>Name: <Slot as="span" name="name" fillProps={ { name } } /></p>
+			<p>Age: <Slot as="span" name="age" fillProps={ { age } } /></p>
+			<Fill name="name">{ ( fillProps ) => fillProps.name }</Fill>
+			<Fill name="age">{ ( fillProps ) => fillProps.age }</Fill>
+		</Provider>
+	);
+};
+
+export const withContext = () => {
+	const Context = createContext();
+	const ContextFill = ( { name } ) => {
+		const value = useContext( Context );
+		return <Fill name={ name }>{ value }</Fill>;
+	};
+	return (
+		<Provider>
+			<h2>Profile</h2>
+			<p>Name: <Slot as="span" name="name" /></p>
+			<p>Age: <Slot as="span" name="age" /></p>
+			<Context.Provider value="Grace">
+				<ContextFill name="name" />
+			</Context.Provider>
+			<Context.Provider value={ 33 }>
+				<ContextFill name="age" />
+			</Context.Provider>
 		</Provider>
 	);
 };
