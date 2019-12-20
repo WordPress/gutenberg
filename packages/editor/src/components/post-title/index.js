e7312d303cbd19b609b763d4feca5f74359dd4d7
Ella van Durpe <ella@vandurpe.com>
2019-12-20 19:37:38 +0100

    Ensure edit mode with enter from title

--- packages/editor/src/components/post-title/index.js
+++ packages/editor/src/components/post-title/index.js
@@ -169,6 +169,7 @@ const applyWithDispatch = withDispatch( ( dispatch ) => {
 	const {
 		insertDefaultBlock,
 		clearSelectedBlock,
+		setNavigationMode,
 	} = dispatch( 'core/block-editor' );
 	const {
 		editPost,
@@ -178,6 +179,8 @@ const applyWithDispatch = withDispatch( ( dispatch ) => {
 
 	return {
 		onEnterPress() {
+			// Ensure we're in edit mode when inserting paragraph to edit.
+			setNavigationMode( false );
 			insertDefaultBlock( undefined, undefined, 0 );
 		},
 		onUpdate( title ) {
