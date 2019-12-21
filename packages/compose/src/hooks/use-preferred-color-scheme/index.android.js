dfa0fb9b66316b6e39a85bf7468cec3b05986c4a
Marko Savic <savicmarko1985@gmail.com>
2019-12-21 22:27:06 +0100

    Create android specific index file for preferred color scheme

---
+++ packages/compose/src/hooks/use-preferred-color-scheme/index.android.js
@@ -0,0 +1,24 @@
+import { getPreferredColorScheme } from 'react-native-gutenberg-bridge';
+import React, { useState, useEffect } from 'react'
+
+/**
+ * Returns the color scheme value when it changes. Possible values: [ 'light', 'dark' ]
+ *
+ * @return {string} return current color scheme.
+ */
+
+const usePreferredColorScheme = function () {
+    const [currentMode, setCurrentMode] = useState('light');
+
+    useEffect(() => {
+        getPreferredColorScheme((mode) => {
+            if (mode !== currentMode) {
+                setCurrentMode(mode)
+            }
+        })
+    });
+
+	return currentMode
+};
+
+export default usePreferredColorScheme;
