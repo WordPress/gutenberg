29537820870d4e14f387e5772ca2e8bac815c5da
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2019-12-23 17:09:01 +0100

    Update Page templates picker for web

--- packages/block-editor/src/components/page-template-picker/button.js
+++ packages/block-editor/src/components/page-template-picker/button.js
@@ -1,16 +1,18 @@
 /**
  * WordPress dependencies
  */
-import { Button } from '@wordpress/components';
+import { Button, Icon } from '@wordpress/components';
 
 const PickerButton = ( props ) => {
 	const {
+		icon,
 		label,
 		onPress,
 	} = props;
 
 	return (
 		<Button onClick={ onPress }>
+			<Icon icon={ icon } width={ 18 } height={ 18 } />
 			{ label }
 		</Button>
 	);
