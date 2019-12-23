0121009114be31af71f0ea42968392927fa321d7
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2019-12-23 16:00:16 +0100

    RNMobile - Page template picker: styles and icons, scroll and accessibility text

--- packages/block-editor/src/components/page-template-picker/button.native.js
+++ packages/block-editor/src/components/page-template-picker/button.native.js
@@ -1,20 +1,47 @@
 /**
  * External dependencies
  */
-import { Button } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
 
-const PickerButton = ( props ) => {
-	const {
-		label,
-		onPress,
-	} = props;
+/**
+ * WordPress dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import {
+	Icon,
+} from '@wordpress/components';
+import { withPreferredColorScheme } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import styles from './styles.scss';
+
+const PickerButton = ( {
+	icon,
+	label,
+	onPress,
+	getStylesFromColorScheme,
+} ) => {
+	const butonStyles = getStylesFromColorScheme( styles.button, styles.buttonDark );
+	const butonTextStyles = getStylesFromColorScheme( styles.buttonText, styles.buttonTextDark );
+	const accessibilityLabel = sprintf(
+		/* translators: accessibility text. Inform about list of predefined layout options. %1$s: Layout name, e.g About. */
+		__( 'Predefined layout picker. %1$s' ),
+		label
+	);
 
 	return (
-		<Button
+		<TouchableOpacity
+			accessibilityLabel={ accessibilityLabel }
+			activeOpacity={ 0.7 }
 			onPress={ onPress }
-			title={ label }
-		/>
+			style={ butonStyles }
+		>
+			<View style={ styles.buttonIconContent }><Icon icon={ icon } { ...styles.buttonIcon } /></View>
+			<Text style={ butonTextStyles }>{ label }</Text>
+		</TouchableOpacity>
 	);
 };
 
-export default PickerButton;
+export default withPreferredColorScheme( PickerButton );
