69b7d6859193f2e42ab72a59e0cc98a1c988b426
Riad Benguella <benguella@gmail.com>
2019-12-26 11:31:43 +0100

    Small optimization

--- packages/keyboard-shortcuts/src/hooks/use-shortcut.js
+++ packages/keyboard-shortcuts/src/hooks/use-shortcut.js
@@ -1,14 +1,7 @@
 /**
- * External dependencies
- */
-import { compact } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import { useSelect } from '@wordpress/data';
-import { rawShortcut } from '@wordpress/keycodes';
-import { useMemo } from '@wordpress/element';
 import { useKeyboardShortcut } from '@wordpress/compose';
 
 /**
@@ -19,23 +12,11 @@ import { useKeyboardShortcut } from '@wordpress/compose';
  * @param {Object} options    Shortcut options.
  */
 function useShortcut( name, callback, options ) {
-	const { combination, aliases } = useSelect( ( select ) => {
-		return {
-			combination: select( 'core/keyboard-shortcuts' ).getShortcutKeyCombination( name ),
-			aliases: select( 'core/keyboard-shortcuts' ).getShortcutAliases( name ),
-		};
+	const shortcuts = useSelect( ( select ) => {
+		return select( 'core/keyboard-shortcuts' ).getAllShortcutRawKeyCombinations( name );
 	}, [ name ] );
 
-	const shortcutKeys = useMemo( () => {
-		const shortcuts = compact( [ combination, ...aliases ] );
-		return shortcuts.map( ( shortcut ) => {
-			return shortcut.modifier ?
-				rawShortcut[ shortcut.modifier ]( shortcut.character ) :
-				shortcut.character;
-		} );
-	}, [ combination, aliases ] );
-
-	useKeyboardShortcut( shortcutKeys, callback, options );
+	useKeyboardShortcut( shortcuts, callback, options );
 }
 
 export default useShortcut;
