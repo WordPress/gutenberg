1dbcbb2c288bbf4a58040f6122e2ec20fd0adf51
Steven Dufresne <dufresnesteven@gmail.com>
2019-12-30 00:03:57 +0900

    Block Directory: Update the regular expression that determines whether the plugin is using an img URL or an icon slug. (#19316)
    * Block Directory: Update the regular expression that determines whether the plugin is using an img or an icon.
    
    Fixes: #19264
    
    * Remove an extra space in the test file.

---
+++ packages/block-directory/src/components/downloadable-block-header/test/fixtures/index.js
@@ -0,0 +1,20 @@
+const pluginBase = {
+	name: 'boxer/boxer',
+	title: 'Boxer',
+	description: 'Boxer is a Block that puts your WordPress posts into boxes on a page.',
+	id: 'boxer-block',
+	rating: 5,
+	rating_count: 1,
+	active_installs: 0,
+	author_block_rating: 5,
+	author_block_count: '1',
+	author: 'CK Lee',
+	assets: [
+		'http://plugins.svn.wordpress.org/boxer-block/trunk/build/index.js',
+		'http://plugins.svn.wordpress.org/boxer-block/trunk/build/view.js',
+	],
+	humanized_updated: '3 months ago',
+};
+
+export const pluginWithIcon = { ...pluginBase, icon: 'block-default' };
+export const pluginWithImg = { ...pluginBase, icon: 'https://ps.w.org/listicles/assets/icon-128x128.png' };
