6963a9985783b16d63975d1196a863b245f833fd
Dave Smith <getdavemail@gmail.com>
2020-01-02 12:18:43 +0000

    Adds justify controls

--- packages/block-library/src/social-links/edit.js
+++ packages/block-library/src/social-links/edit.js
@@ -1,15 +1,29 @@
 /**
+ * External dependencies
+ */
+import { upperFirst } from 'lodash';
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 
 import {
 	InnerBlocks,
+	BlockControls,
 } from '@wordpress/block-editor';
+
+import {
+	Toolbar,
+} from '@wordpress/components';
+
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import socialList from '../social-link/social-list';
+import * as navIcons from '../navigation/icons';
 
 const ALLOWED_BLOCKS = Object.keys( socialList ).map( ( site ) => {
 	return 'core/social-link-' + site;
@@ -25,9 +39,38 @@ const TEMPLATE = [
 	[ 'core/social-link-youtube' ],
 ];
 
-export const SocialLinksEdit = function( { className } ) {
+export const SocialLinksEdit = function( { attributes, setAttributes, className } ) {
+	const { itemsJustification } = attributes;
+
+	const blockClassNames = classnames( className, {
+		[ `items-justification-${ itemsJustification }` ]: itemsJustification,
+	} );
+
+	function handleItemsAlignment( align ) {
+		return () => {
+			const itemsJustificationSetting = itemsJustification === align ? undefined : align;
+			setAttributes( {
+				itemsJustification: itemsJustificationSetting,
+			} );
+		};
+	}
+
 	return (
-		<div className={ className }>
+		<div className={ blockClassNames }>
+
+			<BlockControls>
+				<Toolbar
+					icon={ itemsJustification ? navIcons[ `justify${ upperFirst( itemsJustification ) }Icon` ] : navIcons.justifyLeftIcon }
+					label={ __( 'Change items justification' ) }
+					isCollapsed
+					controls={ [
+						{ icon: navIcons.justifyLeftIcon, title: __( 'Justify items left' ), isActive: 'left' === itemsJustification, onClick: handleItemsAlignment( 'left' ) },
+						{ icon: navIcons.justifyCenterIcon, title: __( 'Justify items center' ), isActive: 'center' === itemsJustification, onClick: handleItemsAlignment( 'center' ) },
+						{ icon: navIcons.justifyRightIcon, title: __( 'Justify items right' ), isActive: 'right' === itemsJustification, onClick: handleItemsAlignment( 'right' ) },
+					] }
+				/>
+			</BlockControls>
+
 			<InnerBlocks
 				allowedBlocks={ ALLOWED_BLOCKS }
 				templateLock={ false }
