c6edad37b82294c5c667b3ec61269cc3209bc6b6
Dave Smith <getdavemail@gmail.com>
2020-01-02 14:39:41 +0000

    Updates save rendering to include justification

--- packages/block-library/src/social-links/save.js
+++ packages/block-library/src/social-links/save.js
@@ -2,10 +2,20 @@
  * WordPress dependencies
  */
 import { InnerBlocks } from '@wordpress/block-editor';
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
 
-export default function save( { className } ) {
+export default function save( { attributes, className } ) {
+	const { itemsJustification } = attributes;
+
+	const blockClassNames = classnames( className, {
+		[ `items-justified-${ itemsJustification }` ]: itemsJustification,
+	} );
+
 	return (
-		<ul className={ className }>
+		<ul className={ blockClassNames }>
 			<InnerBlocks.Content />
 		</ul>
 	);
