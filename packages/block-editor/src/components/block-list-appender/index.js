9ad9c444a28bd8aa21787d5b73d10c98751084bf
Ella van Durpe <ella@vandurpe.com>
2020-01-03 17:40:11 +0100

    Fix nested block appender click

--- packages/block-editor/src/components/block-list-appender/index.js
+++ packages/block-editor/src/components/block-list-appender/index.js
@@ -15,6 +15,10 @@ import { getDefaultBlockName } from '@wordpress/blocks';
 import DefaultBlockAppender from '../default-block-appender';
 import ButtonBlockAppender from '../button-block-appender';
 
+function stopPropagation( event ) {
+	event.stopPropagation();
+}
+
 function BlockListAppender( {
 	blockClientIds,
 	rootClientId,
@@ -50,16 +54,22 @@ function BlockListAppender( {
 		);
 	}
 
-	// A `tabIndex` is used on the wrapping `div` element in order to force a
-	// focus event to occur when an appender `button` element is clicked. In
-	// some browsers (Firefox, Safari), button clicks do not emit a focus event,
-	// which could cause this event to propagate unexpectedly. The `tabIndex`
-	// ensures that the interaction is captured as a focus, without also adding
-	// an extra tab stop.
-	//
-	// See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
 	return (
-		<div tabIndex={ -1 } className="block-list-appender">
+		<div
+			// A `tabIndex` is used on the wrapping `div` element in order to
+			// force a focus event to occur when an appender `button` element
+			// is clicked. In some browsers (Firefox, Safari), button clicks do
+			// not emit a focus event, which could cause this event to propagate
+			// unexpectedly. The `tabIndex` ensures that the interaction is
+			// captured as a focus, without also adding an extra tab stop.
+			//
+			// See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
+			tabIndex={ -1 }
+			// Prevent the block from being selected when the appender is
+			// clicked.
+			onFocus={ stopPropagation }
+			className="block-list-appender"
+		>
 			{ appender }
 		</div>
 	);
