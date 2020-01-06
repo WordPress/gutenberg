450a957b9631b0b6f0c723985c7914a916d571dc
Jon Q <hello@jonquach.com>
2020-01-06 10:50:29 -0500

    Components: Dashicon: Improving mapping and Stories
    This update introduces a key/value object map for the support Dashicon types.
    Originally, the Dashicon Paths were defined and targeted with a large `switch`
    statement. Changing it to a key/value map enables the collection to be accessed
    outside of the component. This is useful for cases like the DashIcon stories,
    allowing us to loop through all of the entries to render examples for each
    icon.

--- packages/components/src/dashicon/stories/index.js
+++ packages/components/src/dashicon/stories/index.js
@@ -1,12 +1,16 @@
 /**
  * External dependencies
  */
+import styled from '@emotion/styled';
 import { number, text } from '@storybook/addon-knobs';
 
 /**
  * Internal dependencies
  */
+import { Card } from '../../card';
+import { CardBody } from '../../card/body';
 import Dashicon from '../';
+import { ICONS } from '../icons';
 
 export default { title: 'Components|Dashicon', component: Dashicon };
 
@@ -15,11 +19,55 @@ export const _default = () => {
 	const color = text( 'Color', '#0079AA' );
 	const size = number( 'Size', 20 );
 
+	return <Dashicon icon={ icon } color={ color } size={ size } />;
+};
+
+export const dashIcons = () => {
+	const icons = Object.keys( ICONS );
+
 	return (
-		<Dashicon
-			icon={ icon }
-			color={ color }
-			size={ size }
-		/>
+		<Container>
+			<Grid>
+				{ icons.map( ( icon ) => (
+					<GridItem key={ icon }>
+						<Card size="small">
+							<CardBody>
+								<IconWrapper>
+									<Dashicon icon={ icon } size={ 20 } />
+								</IconWrapper>
+								<Label>{ icon }</Label>
+							</CardBody>
+						</Card>
+					</GridItem>
+				) ) }
+			</Grid>
+		</Container>
 	);
 };
+
+const Container = styled.div`
+	padding: 0 10px;
+`;
+
+const Grid = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	margin: 0 -10px;
+`;
+
+const GridItem = styled.div`
+	padding: 10px;
+	width: 120px;
+`;
+
+const IconWrapper = styled.div`
+	margin: 0 auto 8px;
+	display: flex;
+	justify-content: center;
+`;
+
+const Label = styled.div`
+	font-size: 10px;
+	text-align: center;
+	min-height: 32px;
+`;
