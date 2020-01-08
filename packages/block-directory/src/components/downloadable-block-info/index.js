dc7cfda8da51a9bb6f497a65e0cf9b33f7dfd133
dufresnesteven <steve.dufresne@automattic.com>
2020-01-08 10:36:18 +0900

    Remove unused classname of 'label'.

--- packages/block-directory/src/components/downloadable-block-info/index.js
+++ packages/block-directory/src/components/downloadable-block-info/index.js
@@ -13,7 +13,7 @@ function DownloadableBlockInfo( { description, activeInstalls, humanizedUpdated 
 			</p>
 			<div className="block-directory-downloadable-block-info__row">
 				<div className="block-directory-downloadable-block-info__column">
-					<Icon icon="chart-line" className="label"></Icon>{ sprintf( _n( '%d active installation', '%d active installations', activeInstalls ), activeInstalls ) }
+					<Icon icon="chart-line"></Icon>{ sprintf( _n( '%d active installation', '%d active installations', activeInstalls ), activeInstalls ) }
 				</div>
 				<div className="block-directory-downloadable-block-info__column">
 					<Icon icon="update"></Icon>
