9401b9f717270eae8740f18c11304eb571faab89
Bart <bartlomiej.kalisz@gmail.com>
2020-01-08 14:58:36 +0100

    Define allowedFormats option for NavigationLink Closes: #18318

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -155,6 +155,12 @@ function NavigationLinkEdit( {
 						onChange={ ( labelValue ) => setAttributes( { label: labelValue } ) }
 						placeholder={ itemLabelPlaceholder }
 						withoutInteractiveFormatting
+						allowedFormats={ [
+							'core/bold',
+							'core/italic',
+							'core/image',
+							'core/strikethrough',
+						] }
 					/>
 					{ isLinkOpen && (
 						<LinkControl
