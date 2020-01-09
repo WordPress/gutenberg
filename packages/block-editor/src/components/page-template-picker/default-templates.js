da3dee547030b2d043b6d12b3bbd5b7cdcb75aa5
Jorge Bernal <jorge@automattic.com>
2020-01-09 12:24:11 +0100

    Initial i18n support for page templates

--- packages/block-editor/src/components/page-template-picker/default-templates.js
+++ packages/block-editor/src/components/page-template-picker/default-templates.js
@@ -1,97 +1,172 @@
 /**
  * WordPress dependencies
  */
-import { parse } from '@wordpress/blocks';
+import {
+	createBlock,
+} from '@wordpress/blocks';
+import {
+	__,
+} from '@wordpress/i18n';
 
 /**
  * External dependencies
  */
+import {
+	map,
+} from 'lodash';
 import memoize from 'memize';
 
-const defaultTemplates = [
-	{
-		name: 'About',
-		icon: '👋',
-		content: `
-			<!-- wp:paragraph {"align":"left"} -->
-			<p class="has-text-align-left">Visitors will want to know who is on the other side of the page. Use this space to write about yourself, your site, your business, or anything you want. Use the testimonials below to quote others, talking about the same thing – in their own words.</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph {"align":"left"} -->
-			<p class="has-text-align-left">This is sample content, included with the template to illustrate its features. Remove or replace it with your own words and media.</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:heading {"align":"center","level":3} -->
-			<h3 class="has-text-align-center">What People Say</h3>
-			<!-- /wp:heading -->
-
-			<!-- wp:quote -->
-			<blockquote class="wp-block-quote"><p>The way to get started is to quit talking and begin doing.</p><cite>Walt Disney</cite></blockquote>
-			<!-- /wp:quote -->
-
-			<!-- wp:quote -->
-			<blockquote class="wp-block-quote"><p>It is our choices, Harry, that show what we truly are, far more than our abilities.</p><cite>J. K. Rowling</cite></blockquote>
-			<!-- /wp:quote -->
-
-			<!-- wp:quote -->
-			<blockquote class="wp-block-quote"><p>Don't cry because it's over, smile because it happened.</p><cite>Dr. Seuss</cite></blockquote>
-			<!-- /wp:quote -->
-
-			<!-- wp:separator {"className":"is-style-wide"} -->
-			<hr class="wp-block-separator is-style-wide"/>
-			<!-- /wp:separator -->
-
-			<!-- wp:heading {"align":"center"} -->
-			<h2 class="has-text-align-center">Let’s build something together.</h2>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph {"align":"center","textColor":"primary"} -->
-			<p class="has-text-color has-text-align-center has-primary-color"><strong><a href="#">Get in touch!</a></strong></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:separator {"className":"is-style-wide"} -->
-			<hr class="wp-block-separator is-style-wide"/>
-			<!-- /wp:separator -->
-		`,
+const defaultTemplates = [ {
+	// translators: title for "About" page template
+	name: __( 'About' ),
+	icon: '👋',
+	content: [ {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "About" page template
+			content: __( 'Visitors will want to know who is on the other side of the page. Use this space to write about yourself, your site, your business, or anything you want. Use the testimonials below to quote others, talking about the same thing – in their own words.' ),
+		},
 	},
 	{
-		name: 'Contact',
-		icon: '✉️',
-		content: `
-			<!-- wp:paragraph {"align":"left"} -->
-			<p class="has-text-align-left">Let's talk 👋 Don't hesitate to reach out with the contact information below, or send a message using the form.</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:heading {"align":"left"} -->
-			<h2 class="has-text-align-left">Get in Touch</h2>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph -->
-			<p>10 Street Road</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p>City,  10100</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p>USA</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><a href="mailto:mail@example.com">mail@example.com</a></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><a href="tel:5555551234">(555) 555 1234</a></p>
-			<!-- /wp:paragraph -->
-		`,
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "About" page template
+			content: __( 'This is sample content, included with the template to illustrate its features. Remove or replace it with your own words and media.' ),
+		},
 	},
+	{
+		name: 'core/heading',
+		attributes: {
+			align: 'center',
+			// translators: sample content for "About" page template
+			content: __( 'What People Say' ),
+			level: 3,
+		},
+	},
+	{
+		name: 'core/quote',
+		attributes: {
+			// translators: sample content for "About" page template
+			value: '<p>' + __( 'The way to get started is to quit talking and begin doing.' ) + '</p>',
+			// translators: sample content for "About" page template
+			citation: __( 'Walt Disney' ),
+		},
+	},
+	{
+		name: 'core/quote',
+		attributes: {
+			// translators: sample content for "About" page template
+			value: '<p>' + __( 'It is our choices, Harry, that show what we truly are, far more than our abilities.' ) + '</p>',
+			// translators: sample content for "About" page template
+			citation: __( 'J. K. Rowling' ),
+		},
+	},
+	{
+		name: 'core/quote',
+		attributes: {
+			// translators: sample content for "About" page template
+			value: '<p>' + __( "Don't cry because it's over, smile because it happened." ) + '</p>',
+			// translators: sample content for "About" page template
+			citation: __( 'Dr. Seuss' ),
+		},
+	},
+	{
+		name: 'core/separator',
+		attributes: {
+			className: 'is-style-wide',
+		},
+	},
+	{
+		name: 'core/heading',
+		attributes: {
+			align: 'center',
+			// translators: sample content for "About" page template
+			content: __( 'Let’s build something together.' ),
+			level: 2,
+		},
+	},
+	{
+		name: 'core/paragraph',
+		attributes: {
+			align: 'center',
+			// translators: sample content for "About" page template
+			content: '<strong><a href="#">' + __( 'Get in touch!' ) + '</a></strong>',
+			textColor: 'primary',
+		},
+	},
+	{
+		name: 'core/separator',
+		attributes: {
+			className: 'is-style-wide',
+		},
+	},
+	],
+},
+{
+	// translators: title for "Contact" page template
+	name: __( 'Contact' ),
+	icon: '✉️',
+	content: [ {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( "Let's talk 👋 Don't hesitate to reach out with the contact information below, or send a message using the form." ),
+		},
+	}, {
+		name: 'core/heading',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( 'Get in Touch' ),
+			level: 2,
+		},
+	}, {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( '10 Street Road' ),
+		},
+	}, {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( 'City,  10100' ),
+		},
+	}, {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( 'USA' ),
+		},
+	}, {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( '<a href="mailto:mail@example.com">mail@example.com</a>' ),
+		},
+	}, {
+		name: 'core/paragraph',
+		attributes: {
+			// translators: sample content for "Contact" page template
+			content: __( '<a href="tel:5555551234">(555) 555 1234</a>' ),
+			dropCap: false,
+		},
+	} ],
+},
 ];
+
+const createBlocks = ( template ) => {
+	return template.map( ( {
+		name,
+		attributes,
+		innerBlocks,
+	} ) => {
+		return createBlock( name, attributes, map( innerBlocks, createBlocks ) );
+	} );
+};
 
 const parsedTemplates = memoize( () => defaultTemplates.map( ( template ) => ( {
 	...template,
-	blocks: parse( template.content ),
+	blocks: createBlocks( template.content ),
 } ) ) );
 
 export default parsedTemplates;
