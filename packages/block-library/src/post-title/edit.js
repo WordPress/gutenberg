acc41baa51530e913f54fe9987f1297b04ccb2ca
epiqueras <epiquerass@gmail.com>
2020-01-10 16:07:49 -0500

    Block Library: Implement Post Title block editor.

--- packages/block-library/src/post-title/edit.js
+++ packages/block-library/src/post-title/edit.js
@@ -1,3 +1,27 @@
+/**
+ * WordPress dependencies
+ */
+import { useEntityProp, useEntityId } from '@wordpress/core-data';
+import { RichText } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+function PostTitleInput() {
+	const [ title, setTitle ] = useEntityProp( 'postType', 'post', 'title' );
+	return (
+		<RichText
+			tagName="h2"
+			placeholder={ __( 'Post Title' ) }
+			value={ title }
+			onChange={ setTitle }
+			allowedFormats={ [] }
+		/>
+	);
+}
+
 export default function PostTitleEdit() {
-	return <h2>{ 'Hello world!' }</h2>;
+	if ( ! useEntityId( 'postType', 'post' ) ) {
+		return 'Post Title Placeholder';
+	}
+
+	return <PostTitleInput />;
 }
