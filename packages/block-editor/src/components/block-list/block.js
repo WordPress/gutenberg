bb187ffecdd670c0b4186258e0efb20301cec291
Ella van Durpe <ella@vandurpe.com>
2020-01-14 10:24:10 +0100

    Merge multi selected effect

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -174,19 +174,23 @@ function BlockListBlock( {
 
 	// Focus the selected block's wrapper or inner input on mount and update
 	const isMounting = useRef( true );
-	useEffect( () => {
-		if ( isSelected && ! isMultiSelecting && ! isNavigationMode ) {
-			focusTabbable( ! isMounting.current );
-		}
-		isMounting.current = false;
-	}, [ isSelected, isMultiSelecting, isNavigationMode ] );
 
-	// Focus the first multi selected block
 	useEffect( () => {
-		if ( isFirstMultiSelected ) {
-			wrapper.current.focus();
+		if ( ! isMultiSelecting && ! isNavigationMode ) {
+			if ( isSelected ) {
+				focusTabbable( ! isMounting.current );
+			} else if ( isFirstMultiSelected ) {
+				wrapper.current.focus();
+			}
 		}
-	}, [ isFirstMultiSelected ] );
+
+		isMounting.current = false;
+	}, [
+		isSelected,
+		isFirstMultiSelected,
+		isMultiSelecting,
+		isNavigationMode,
+	] );
 
 	// Block Reordering animation
 	const animationStyle = useMovingAnimation( wrapper, isSelected || isPartOfMultiSelection, isSelected || isFirstMultiSelected, enableAnimation, animateOnChange );
