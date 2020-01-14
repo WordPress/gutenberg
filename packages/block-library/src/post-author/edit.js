4fb15c7fe3580bda245df260725103bd4ba7cdab
epiqueras <epiquerass@gmail.com>
2020-01-14 11:32:09 -0500

    Block Library: Render actual author name in Post Author block instead of their ID.

--- packages/block-library/src/post-author/edit.js
+++ packages/block-library/src/post-author/edit.js
@@ -2,10 +2,16 @@
  * WordPress dependencies
  */
 import { useEntityProp, useEntityId } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
+import { sprintf, __ } from '@wordpress/i18n';
 
 function PostAuthorDisplay() {
-	const [ author ] = useEntityProp( 'postType', 'post', 'author' );
-	return <address>{ author }</address>;
+	const [ authorId ] = useEntityProp( 'postType', 'post', 'author' );
+	const author = useSelect(
+		( select ) => select( 'core' ).getEntityRecord( 'root', 'user', authorId ),
+		[ authorId ]
+	);
+	return author ? <address>{ sprintf( __( 'By %s' ), author.name ) }</address> : null;
 }
 
 export default function PostAuthorEdit() {
