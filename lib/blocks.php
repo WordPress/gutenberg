eeb13f45a4f890179cfc9b0203f4019eb4d19f41
Enrique Piqueras <epiqueras@users.noreply.github.com>
2020-01-14 14:14:31 -0500

    Block Library: Add a Post Date block. (#19578)
    * Block Library: Add a Post Date block.
    
    * Block Library: Use time tags in Post Date block.
    
    * E2E Tests: Generate Post Date block fixtures.

--- lib/blocks.php
+++ lib/blocks.php
@@ -57,6 +57,7 @@ function gutenberg_reregister_core_block_types() {
 		'post-title.php'      => 'core/post-title',
 		'post-content.php'    => 'core/post-content',
 		'post-author.php'     => 'core/post-author',
+		'post-date.php'       => 'core/post-date',
 		'post-excerpt.php'    => 'core/post-excerpt',
 	);
 
