92be1164cc5ff595b565416aa4c43526eb0860ed
Jon Surrell <jon.surrell@automattic.com>
2020-01-15 12:56:48 +0100

    Fixup docblock

--- packages/redux-routine/src/is-iterator.js
+++ packages/redux-routine/src/is-iterator.js
@@ -1,11 +1,11 @@
 /**
  * Returns true if the given object is an iterator, or false otherwise.
  *
- * @see https://www.ecma-international.org/ecma-262/6.0/#sec-generator-objects
+ * @see https://www.ecma-international.org/ecma-262/6.0/#sec-%iteratorprototype%-object
  *
  * @param {*} object Object to test.
  *
- * @return {boolean} Whether object is a generator.
+ * @return {boolean} Whether object is an iterator.
  */
 export default function isIterator( object ) {
 	return !! object && typeof object[ Symbol.iterator ] === 'function';
