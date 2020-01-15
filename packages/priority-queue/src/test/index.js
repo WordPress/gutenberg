f5368e670bc4f31ff0e28c3988a2097a6b993dfa
Andrew Duthie <andrew@andrewduthie.com>
2020-01-15 18:42:55 -0500

    Priority Queue: Add test case to verify flush callback invoked

--- packages/priority-queue/src/test/index.js
+++ packages/priority-queue/src/test/index.js
@@ -19,11 +19,41 @@ describe( 'createQueue', () => {
 		queue = createQueue();
 	} );
 
-	it( 'runs callback after processing waiting queue', () => {
-		const callback = jest.fn();
-		queue.add( {}, callback );
-		expect( callback ).not.toHaveBeenCalled();
-		requestIdleCallback.tick();
-		expect( callback ).toHaveBeenCalled();
+	describe( 'add', () => {
+		it( 'runs callback after processing waiting queue', () => {
+			const callback = jest.fn();
+
+			queue.add( {}, callback );
+
+			expect( callback ).not.toHaveBeenCalled();
+			requestIdleCallback.tick();
+			expect( callback ).toHaveBeenCalled();
+		} );
+	} );
+
+	describe( 'flush', () => {
+		it( 'invokes all callbacks associated with element', () => {
+			const elementA = {};
+			const elementB = {};
+			const callbackElementA = jest.fn();
+			const callbackElementB = jest.fn();
+			queue.add( elementA, callbackElementA );
+			queue.add( elementB, callbackElementB );
+
+			expect( callbackElementA ).not.toHaveBeenCalled();
+			expect( callbackElementB ).not.toHaveBeenCalled();
+
+			queue.flush( elementA );
+
+			// Only ElementA callback should have been called (synchronously).
+			expect( callbackElementA ).toHaveBeenCalledTimes( 1 );
+			expect( callbackElementB ).not.toHaveBeenCalled();
+
+			// Verify that callback still called only once after tick (verify
+			// removal).
+			requestIdleCallback.tick();
+			expect( callbackElementA ).toHaveBeenCalledTimes( 1 );
+			expect( callbackElementB ).toHaveBeenCalledTimes( 1 );
+		} );
 	} );
 } );
