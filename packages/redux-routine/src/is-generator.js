d6ef64d008dbfdcc335674928fa11c82342365f2
Jon Surrell <jon.surrell@automattic.com>
2020-01-15 22:07:43 +0100

    Update docs

--- packages/redux-routine/src/is-generator.js
+++ packages/redux-routine/src/is-generator.js
@@ -1,14 +1,14 @@
 /**
- * Returns true if the given object is an iterator, or false otherwise.
+ * Returns true if the given object is a generator, or false otherwise.
  *
- * @see https://www.ecma-international.org/ecma-262/6.0/#sec-%iteratorprototype%-object
+ * @see https://www.ecma-international.org/ecma-262/6.0/#sec-generator-objects
  *
  * @param {*} object Object to test.
  *
  * @return {boolean} Whether object is a generator.
  */
 export default function isGenerator( object ) {
-	// Check for Symbol.iterator and next methods.
+	// Check that iterator (next) and iterable (Symbol.iterator) interfaces are satisfied.
 	// These checks seem to be compatible with several generator helpers as well as the native implementation.
 	return (
 		!! object &&
