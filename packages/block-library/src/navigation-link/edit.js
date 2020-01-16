de772cb999fa96e76b3edbd5196e7c1ab8a5fa26
Bart <bartlomiej.kalisz@gmail.com>
2020-01-16 19:16:27 +0100

    Isolate NavigationLink icons

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -14,9 +14,7 @@ import {
 	ExternalLink,
 	KeyboardShortcuts,
 	PanelBody,
-	Path,
 	Popover,
-	SVG,
 	TextareaControl,
 	TextControl,
 	ToggleControl,
@@ -40,7 +38,7 @@ import { Fragment, useState, useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import { submenuIcon } from './icons';
+import { toolbarSubmenuIcon, itemSubmenuIcon } from './icons';
 
 function NavigationLinkEdit( {
 	attributes,
@@ -99,7 +97,7 @@ function NavigationLinkEdit( {
 					/>
 					<ToolbarButton
 						name="submenu"
-						icon={ <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24"><Path d="M14 5h8v2h-8zm0 5.5h8v2h-8zm0 5.5h8v2h-8zM2 11.5C2 15.08 4.92 18 8.5 18H9v2l3-3-3-3v2h-.5C6.02 16 4 13.98 4 11.5S6.02 7 8.5 7H12V5H8.5C4.92 5 2 7.92 2 11.5z" /><Path fill="none" d="M0 0h24v24H0z" /></SVG> }
+						icon={ toolbarSubmenuIcon }
 						title={ __( 'Add submenu' ) }
 						onClick={ insertLinkBlock }
 					/>
@@ -171,7 +169,7 @@ function NavigationLinkEdit( {
 					/>
 					{ showSubmenuIcon &&
 						<div className="wp-block-navigation-link__submenu-icon">
-							{ submenuIcon }
+							{ itemSubmenuIcon }
 						</div>
 					}
 					{ isLinkOpen && (
