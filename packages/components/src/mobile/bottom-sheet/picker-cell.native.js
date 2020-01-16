fd1a18a4e46754b942be4bca3f1b34beb5e06d34
Matthew Kevins <mkevins@users.noreply.github.com>
2020-01-16 18:34:46 +1000

    [Mobile] Fix SelectControl to display label instead of value (#19547)
    * Display current label in PickerCell instead of value
    
    * Fix lint error

--- packages/components/src/mobile/bottom-sheet/picker-cell.native.js
+++ packages/components/src/mobile/bottom-sheet/picker-cell.native.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { find } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import Cell from './cell';
@@ -9,6 +14,7 @@ export default function BottomSheetPickerCell( props ) {
 		options,
 		hideCancelButton,
 		onChangeValue,
+		value,
 		...cellProps
 	} = props;
 
@@ -22,8 +28,15 @@ export default function BottomSheetPickerCell( props ) {
 		onChangeValue( newValue );
 	};
 
+	const option = find( options, { value } );
+	const label = option ? option.label : value;
+
 	return (
-		<Cell onPress={ onCellPress } editable={ false } { ...cellProps } >
+		<Cell
+			onPress={ onCellPress }
+			editable={ false }
+			value={ label }
+			{ ...cellProps } >
 			<Picker
 				leftAlign
 				hideCancelButton={ hideCancelButton }
