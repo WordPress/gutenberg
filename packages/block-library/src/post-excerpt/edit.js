2368b51a80e2b1b32257a8587fc55f15842d7edf
epiqueras <epiquerass@gmail.com>
2020-01-16 22:47:41 -0500

    Block Library: Add features to the Post Excerpt block.

--- packages/block-library/src/post-excerpt/edit.js
+++ packages/block-library/src/post-excerpt/edit.js
@@ -2,20 +2,96 @@
  * WordPress dependencies
  */
 import { useEntityProp, useEntityId } from '@wordpress/core-data';
-import { PlainText } from '@wordpress/block-editor';
+import { useMemo } from '@wordpress/element';
+import { InspectorControls, RichText } from '@wordpress/block-editor';
+import { PanelBody, RangeControl, ToggleControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
-function PostExcerptDisplay() {
-	const [ excerpt, setExcerpt ] = useEntityProp(
+function usePostContentExcerpt( excerptLength ) {
+	const [ , , { raw: rawPostContent } ] = useEntityProp(
 		'postType',
 		'post',
-		'excerpt'
+		'content'
 	);
-	return <PlainText value={ excerpt } onChange={ setExcerpt } />;
+	return useMemo( () => {
+		if ( ! rawPostContent ) {
+			return '';
+		}
+		const excerptElement = document.createElement( 'div' );
+		excerptElement.innerHTML = rawPostContent;
+		const excerpt =
+			excerptElement.textContent || excerptElement.innerText || '';
+		return excerpt
+			.trim()
+			.split( ' ', excerptLength )
+			.join( ' ' );
+	}, [ rawPostContent, excerptLength ] );
 }
 
-export default function PostExcerptEdit() {
+function PostExcerptEditor( {
+	attributes: { excerptLength, moreText, showMoreOnNewLine },
+	setAttributes,
+	isSelected,
+} ) {
+	const [ excerpt, setExcerpt ] = useEntityProp( 'postType', 'post', 'excerpt' );
+	const postContentExcerpt = usePostContentExcerpt( excerptLength );
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Post Excerpt Settings' ) }>
+					{ ! excerpt && (
+						<RangeControl
+							label={ __( 'Max words' ) }
+							value={ excerptLength }
+							onChange={ ( newExcerptLength ) =>
+								setAttributes( { excerptLength: newExcerptLength } )
+							}
+							min={ 10 }
+							max={ 100 }
+						/>
+					) }
+					<ToggleControl
+						label={ __( 'Show link on new line' ) }
+						checked={ showMoreOnNewLine }
+						onChange={ ( newShowMoreOnNewLine ) =>
+							setAttributes( { showMoreOnNewLine: newShowMoreOnNewLine } )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<RichText
+				className={
+					! showMoreOnNewLine && 'wp-block-post-excerpt__excerpt is-inline'
+				}
+				placeholder={ postContentExcerpt }
+				value={ excerpt || ( isSelected ? '' : postContentExcerpt ) }
+				onChange={ setExcerpt }
+				keepPlaceholderOnFocus
+			/>
+			{ ! showMoreOnNewLine && ' ' }
+			<RichText
+				tagName="a"
+				placeholder={ __( 'Read more…' ) }
+				value={ moreText }
+				onChange={ ( newMoreText ) => setAttributes( { moreText: newMoreText } ) }
+			/>
+		</>
+	);
+}
+
+export default function PostExcerptEdit( {
+	attributes,
+	setAttributes,
+	isSelected,
+} ) {
 	if ( ! useEntityId( 'postType', 'post' ) ) {
 		return 'Post Excerpt Placeholder';
 	}
-	return <PostExcerptDisplay />;
+	return (
+		<PostExcerptEditor
+			attributes={ attributes }
+			setAttributes={ setAttributes }
+			isSelected={ isSelected }
+		/>
+	);
 }
