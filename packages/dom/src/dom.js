aac5dccc3f6593fd54217d7fd3ca822ec170738e
Ella van Durpe <wp@iseulde.com>
2020-01-17 17:14:21 +0100

    Writing Flow: fix list selection (#19721)
    * Writing Flow: fix list selection
    
    * Add e2e test

--- packages/dom/src/dom.js
+++ packages/dom/src/dom.js
@@ -93,7 +93,8 @@ function isEdge( container, isReverse, onlyVertical ) {
 		return false;
 	}
 
-	const range = selection.getRangeAt( 0 ).cloneRange();
+	const originalRange = selection.getRangeAt( 0 );
+	const range = originalRange.cloneRange();
 	const isForward = isSelectionForward( selection );
 	const isCollapsed = selection.isCollapsed;
 
@@ -131,9 +132,10 @@ function isEdge( container, isReverse, onlyVertical ) {
 	// over its line boundary.
 	const buffer = 3 * parseInt( lineHeight, 10 ) / 4;
 	const containerRect = container.getBoundingClientRect();
+	const originalRangeRect = getRectangleFromRange( originalRange );
 	const verticalEdge = isReverse ?
-		containerRect.top + padding > rangeRect.top - buffer :
-		containerRect.bottom - padding < rangeRect.bottom + buffer;
+		containerRect.top + padding > originalRangeRect.top - buffer :
+		containerRect.bottom - padding < originalRangeRect.bottom + buffer;
 
 	if ( ! verticalEdge ) {
 		return false;
