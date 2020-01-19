963a64f8e33f726c2b48e3653bbaf1471b2e3222
epiqueras <epiquerass@gmail.com>
2020-01-19 08:40:20 -0500

    Core Data: Support passing a specific ID to entity hooks instead of reading from providers.

--- packages/core-data/src/entity-provider.js
+++ packages/core-data/src/entity-provider.js
@@ -74,16 +74,25 @@ export function useEntityId( kind, type ) {
  * specified property of the nearest provided
  * entity of the specified type.
  *
- * @param {string} kind The entity kind.
- * @param {string} type The entity type.
- * @param {string} prop The property name.
+ * You may optionally override the provided ID
+ * using the last parameter.
+ *
+ * @param {string} kind                           The entity kind.
+ * @param {string} type                           The entity type.
+ * @param {string} prop                           The property name.
+ * @param {string} [id=useEntityId( kind, type )] The optional entity ID override.
  *
  * @return {[*, Function]} A tuple where the first item is the
  *                          property value and the second is the
  *                          setter.
  */
-export function useEntityProp( kind, type, prop ) {
-	const id = useEntityId( kind, type );
+export function useEntityProp(
+	kind,
+	type,
+	prop,
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	id = useEntityId( kind, type )
+) {
 	const { value, fullValue } = useSelect(
 		( select ) => {
 			const { getEntityRecord, getEditedEntityRecord } = select( 'core' );
@@ -122,24 +131,38 @@ export function useEntityProp( kind, type, prop ) {
  * `BlockEditorProvider` and are intended to be used with it,
  * or similar components or hooks.
  *
- * @param {string} kind                            The entity kind.
- * @param {string} type                            The entity type.
+ * You may optionally override the provided ID
+ * using the last parameter.
+ *
+ * @param {string} kind                                    The entity kind.
+ * @param {string} type                                    The entity type.
  * @param {Object} options
- * @param {Object} [options.initialEdits]          Initial edits object for the entity record.
- * @param {string} [options.blocksProp='blocks']   The name of the entity prop that holds the blocks array.
- * @param {string} [options.contentProp='content'] The name of the entity prop that holds the serialized blocks.
+ * @param {Object} [options.initialEdits]                  Initial edits object for the entity record.
+ * @param {string} [options.blocksProp='blocks']           The name of the entity prop that holds the blocks array.
+ * @param {string} [options.contentProp='content']         The name of the entity prop that holds the serialized blocks.
+ * @param {string} [options.id=useEntityId( kind, type )]  The optional entity ID override.
  *
  * @return {[WPBlock[], Function, Function]} The block array and setters.
  */
 export function useEntityBlockEditor(
 	kind,
 	type,
-	{ initialEdits, blocksProp = 'blocks', contentProp = 'content' } = {}
+	{
+		initialEdits,
+		blocksProp = 'blocks',
+		contentProp = 'content',
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		id = useEntityId( kind, type ),
+	} = {}
 ) {
-	const [ content, setContent ] = useEntityProp( kind, type, contentProp );
+	const [ content, setContent ] = useEntityProp(
+		kind,
+		type,
+		contentProp,
+		id
+	);
 
 	const { editEntityRecord } = useDispatch( 'core' );
-	const id = useEntityId( kind, type );
 	const initialBlocks = useMemo( () => {
 		if ( initialEdits ) {
 			editEntityRecord( kind, type, id, initialEdits, {
@@ -157,7 +180,8 @@ export function useEntityBlockEditor(
 	const [ blocks = initialBlocks, onInput ] = useEntityProp(
 		kind,
 		type,
-		blocksProp
+		blocksProp,
+		id
 	);
 
 	const onChange = useCallback(
