6758a60944223a4e7dbc30c046c7ca8f2f19dd2a
Dave Smith <getdavemail@gmail.com>
2020-01-21 10:38:11 +0000

    Styling and i18n

--- packages/block-editor/src/components/link-control/search-create-button.js
+++ packages/block-editor/src/components/link-control/search-create-button.js
@@ -6,6 +6,7 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
+import { __, sprintf } from '@wordpress/i18n';
 import { safeDecodeURI } from '@wordpress/url';
 import {
 	Button,
@@ -13,19 +14,23 @@ import {
 } from '@wordpress/components';
 
 export const LinkControlSearchCreate = ( { searchTerm = '', onClick, itemProps } ) => {
+	if ( ! searchTerm.trim() ) {
+		return null;
+	}
+
 	return (
 		<Button
 			{ ...itemProps }
 			onClick={ onClick }
-			className={ classnames( 'block-editor-link-control__search-item', {
+			className={ classnames( 'block-editor-link-control__search-create block-editor-link-control__search-item', {
 			} ) }
 		>
 
-			<Icon className="block-editor-link-control__search-item-icon" icon="admin-page" />
+			<Icon className="block-editor-link-control__search-item-icon" icon="insert" />
 
 			<span className="block-editor-link-control__search-item-header">
 				<span className="block-editor-link-control__search-item-title">
-					{ searchTerm }
+					{ sprintf( __( 'Create new Page: %s' ), searchTerm ) }
 				</span>
 				<span className="block-editor-link-control__search-item-info">
 					{ safeDecodeURI( '/some-url/' ) }
