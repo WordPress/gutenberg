d66ea9625f9274fd507c46e84921a042a454ae82
Daniel Richards <daniel.richards@automattic.com>
2020-01-21 16:59:46 +0800

    Export components under single object

---
+++ packages/block-editor/src/components/roving-tab-index/index.js
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies
+ */
+import RovingTabIndexContainer from './container';
+import RovingTabIndexItem from './item';
+
+const RovingTabIndex = {
+	Container: RovingTabIndexContainer,
+	Item: RovingTabIndexItem,
+};
+
+export default RovingTabIndex;
