9190fb008102fcc9041445de3c0543626c33c788
Riad Benguella <benguella@gmail.com>
2020-01-23 10:35:53 +0100

    Remove the G elements from SVGs

--- packages/icons/src/library/button.js
+++ packages/icons/src/library/button.js
@@ -1,13 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { G, Path, SVG } from '@wordpress/primitives';
+import { Path, SVG } from '@wordpress/primitives';
 
 const button = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<G>
-			<Path d="M19 6H5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 10H5V8h14v8z" />
-		</G>
+		<Path d="M19 6H5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 10H5V8h14v8z" />
 	</SVG>
 );
 
