abb0f620c34ea894d14d55c6da97cda41859ec14
Konstantin Obenland <obenland@gmx.de>
2020-01-23 10:29:44 -0800

    Slightly simplify css class assembly

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -180,8 +180,8 @@ function build_navigation_html( $attributes, $block, $colors, $font_sizes, $is_l
 		$colors['css_classes'],
 		$font_sizes['css_classes']
 	);
-	$css_classes     = implode( ' ', $classes );
-	$class_attribute = sprintf( ' class="wp-block-navigation-link %s', esc_attr( trim( $css_classes ) ) );
+	$classes[]       = 'wp-block-navigation-link';
+	$css_classes     = trim( implode( ' ', $classes ) );
 	$style_attribute = ( $colors['inline_styles'] || $font_sizes['inline_styles'] )
 		? sprintf( ' style="%s"', esc_attr( $colors['inline_styles'] ) . esc_attr( $font_sizes['inline_styles'] ) )
 		: '';
@@ -189,7 +189,7 @@ function build_navigation_html( $attributes, $block, $colors, $font_sizes, $is_l
 	foreach ( (array) $block['innerBlocks'] as $key => $block ) {
 		$has_submenu = count( (array) $block['innerBlocks'] ) > 0;
 
-		$html .= '<li' . $class_attribute . ( $has_submenu ? ' has-child' : '' ) . '"' . $style_attribute . '>' .
+		$html .= '<li class="' . esc_attr( $css_classes . ( $has_submenu ? ' has-child' : '' ) ) . '"' . $style_attribute . '>' .
 			'<a class="wp-block-navigation-link__content"';
 
 		// Start appending HTML attributes to anchor tag.
