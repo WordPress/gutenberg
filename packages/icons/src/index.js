8f92cfe6548379ebaa3a271ae1d853754d395d12
Riad Benguella <benguella@gmail.com>
2020-01-23 11:10:12 +0100

    Move a dozen of block icons to the icons package (#19808)

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -1,4 +1,11 @@
 export { default as Icon } from './icon';
 
+export { default as archive } from './library/archive';
+export { default as audio } from './library/audio';
+export { default as button } from './library/button';
 export { default as check } from './library/check';
+export { default as calendar } from './library/calendar';
+export { default as category } from './library/category';
+export { default as classic } from './library/classic';
+export { default as code } from './library/code';
 export { default as paragraph } from './library/paragraph';
