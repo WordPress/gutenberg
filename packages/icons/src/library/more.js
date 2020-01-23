f197020b299e8e9967ffebc492e15530e34ac4cc
Riad Benguella <benguella@gmail.com>
2020-01-23 10:59:35 +0100

    Removing the useless G elements from SVGs

--- packages/icons/src/library/more.js
+++ packages/icons/src/library/more.js
@@ -1,13 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { G, Path, SVG } from '@wordpress/primitives';
+import { Path, SVG } from '@wordpress/primitives';
 
 const more = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<G>
-			<Path d="M2 9v2h19V9H2zm0 6h5v-2H2v2zm7 0h5v-2H9v2zm7 0h5v-2h-5v2z" />
-		</G>
+		<Path d="M2 9v2h19V9H2zm0 6h5v-2H2v2zm7 0h5v-2H9v2zm7 0h5v-2h-5v2z" />
 	</SVG>
 );
 
