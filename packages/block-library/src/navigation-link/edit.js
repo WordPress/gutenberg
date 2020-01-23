f10cfaf3eb02522527bf44316e923a78ec6239ab
retrofox <rdsuarez@gmail.com>
2020-01-23 16:40:00 -0300

    navigation: set border color using currentColor

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -205,7 +205,6 @@ function NavigationLinkEdit( {
 						[ `has-${ navigationBlockAttributes.backgroundColor }-background-color` ]: !! navigationBlockAttributes.backgroundColor,
 					} ) }
 				style={ {
-					borderColor: navigationBlockAttributes.valueTextColor,
 					color: navigationBlockAttributes.valueTextColor,
 					backgroundColor: navigationBlockAttributes.valueBackgroundColor,
 				} }
