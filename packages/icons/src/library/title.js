b9b18183b807ce4e6153272f4615d09884eb68c0
Riad Benguella <benguella@gmail.com>
2020-01-24 10:19:52 +0100

    Move more block icons to the icons package (#19838)

--- packages/icons/src/library/title.js
+++ packages/icons/src/library/title.js
@@ -1,11 +1,12 @@
 /**
  * WordPress dependencies
  */
-import { SVG, Path } from '@wordpress/components';
+import { SVG, Path } from '@wordpress/primitives';
 
-export default (
+const title = (
 	<SVG xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path fill="none" d="M0 0h24v24H0V0z" />
 		<Path d="M5 4v3h5.5v12h3V7H19V4H5z" />
 	</SVG>
 );
+
+export default title;
