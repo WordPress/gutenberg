1be262bb14de29505d715bfbc71f8992ccaeba1f
Riad Benguella <benguella@gmail.com>
2020-01-24 10:48:11 +0100

    Move more block icons to the icons library

--- packages/block-library/src/rss/index.js
+++ packages/block-library/src/rss/index.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { rss as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -13,7 +14,7 @@ export const name = 'core/rss';
 export const settings = {
 	title: __( 'RSS' ),
 	description: __( 'Display entries from any RSS or Atom feed.' ),
-	icon: 'rss',
+	icon,
 	category: 'widgets',
 	keywords: [ __( 'atom' ), __( 'feed' ) ],
 	supports: {
