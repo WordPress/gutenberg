d995d3bd35902a563a70cfcdd91d732f0ebf7391
Chip Snyder <chip.snyder3@gmail.com>
2020-01-27 11:43:43 -0500

    Adding comment for autocorrect changes

--- packages/block-library/src/shortcode/edit.native.js
+++ packages/block-library/src/shortcode/edit.native.js
@@ -38,6 +38,11 @@ export function ShortcodeEdit( props ) {
 				onBlur={ onBlur }
 				autoCorrect={ false }
 				autoComplete="off"
+				/*
+				 * For some devices autoCorrect and autoComplete are not enough to hide the suggestion toolbar.
+				 * Following the suggestion below we added the keyboard type as well.
+				 * https://stackoverflow.com/questions/37001070/how-to-avoid-the-suggestions-of-keyboard-for-android-in-react-native/51411575#51411575
+				*/
 				keyboardType={ Platform.OS === 'ios' ? 'default' : 'visible-password' }
 				placeholderTextColor={ placeholderStyle.color }
 			/>
