0bcf5be50c772bde4c5ac0eaed343b2c35c89825
Cameron Voell <cameronvoell@gmail.com>
2020-01-30 18:26:14 -0800

    Added tests for regex used for removing break tags

--- packages/rich-text/src/component/test/index.native.js
+++ packages/rich-text/src/component/test/index.native.js
@@ -30,4 +30,41 @@ describe( 'RichText Native', () => {
 			expect( richText.willTrimSpaces( html ) ).toBe( false );
 		} );
 	} );
+
+	describe( 'countBreaksBeforeParagraphTag', () => {
+		it( 'exists', () => {
+			expect( richText ).toHaveProperty( 'countBreaksBeforeParagraphTag' );
+		} );
+
+		it( 'is a function', () => {
+			expect( richText.countBreaksBeforeParagraphTag ).toBeInstanceOf( Function );
+		} );
+
+		it( 'Counts zero breaks when no breaks are in the text', () => {
+			const html = '<p><b>Hello</b> <strong>Hello</strong> WorldWorld! </p>';
+			expect( richText.countBreaksBeforeParagraphTag( html ) ).toBe( 0 );
+		} );
+
+		it( 'Counts zero breaks when breaks precede a non paragraph tag', () => {
+			const html = '<pre><b>Hello</b> <strong>Hello</strong> WorldWorld!<br></pre>';
+			expect( richText.countBreaksBeforeParagraphTag( html ) ).toBe( 0 );
+		} );
+
+		it( 'Counts zero breaks when no break is not directly in front of ending paragraph tag', () => {
+			const html = '<p><b>Hello</b> <br><strong>Hello</strong> WorldWorld!<br><br> </p>';
+			expect( richText.countBreaksBeforeParagraphTag( html ) ).toBe( 0 );
+		} );
+
+		it( 'Counts one break tag preceding the ending paragraph tag', () => {
+			const html = '<p><b>Hello</b> <strong>Hello</strong> WorldWorld!<br></p>';
+			expect( richText.countBreaksBeforeParagraphTag( html ) ).toBe( 1 );
+		} );
+
+		it( 'Counts multiple break tags preceding the ending paragraph tag', () => {
+			const html = '<p><b>Hello</b> <strong>Hello</strong> WorldWorld!<br><br><br></p>';
+			expect( richText.countBreaksBeforeParagraphTag( html ) ).toBe( 3 );
+		} );
+
+	} );
+
 } );
