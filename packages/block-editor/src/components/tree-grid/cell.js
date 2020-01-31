5833a77c14ea0945679c81b29ca8a0e5a6219c92
Daniel Richards <daniel.richards@automattic.com>
2020-01-31 15:36:00 +0800

    Forward props to td element

--- packages/block-editor/src/components/tree-grid/cell.js
+++ packages/block-editor/src/components/tree-grid/cell.js
@@ -3,9 +3,9 @@
  */
 import { RovingTabIndexItem } from '../roving-tab-index';
 
-export default function TreeGridCell( { children } ) {
+export default function TreeGridCell( { children, ...props } ) {
 	return (
-		<td role="gridcell">
+		<td role="gridcell" { ...props }>
 			<RovingTabIndexItem>
 				{ children }
 			</RovingTabIndexItem>
