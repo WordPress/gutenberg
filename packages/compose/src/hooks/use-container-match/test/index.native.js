74d92273cd1c782fd57f0fee9dbe56eccbada7f7
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-02-05 09:49:05 +0100

    Add useContainerMatch hook tests

---
+++ packages/compose/src/hooks/use-container-match/test/index.native.js
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import { create, act } from 'react-test-renderer';
+import { View } from 'react-native';
+
+/**
+ * Internal dependencies
+ */
+import useContainerMatch from '../';
+
+const TestComponent = ( { query } ) => {
+	const [ matches, onLayout ] = useContainerMatch( query );
+
+	return (
+		<View onLayout={ onLayout } matches={ matches } />
+	);
+};
+
+const renderWithOnLayout = ( component ) => {
+	const testComponent = create( component );
+
+	const mockNativeEvent = {
+		nativeEvent: {
+			layout: {
+				width: 300,
+			},
+		},
+	};
+
+	act( () => {
+		testComponent.toJSON().props.onLayout( mockNativeEvent );
+	} );
+
+	return testComponent.toJSON();
+};
+
+describe( 'useContainerMatch()', () => {
+	it( 'should return "{ isSmall: false }"', () => {
+		const component = renderWithOnLayout( <TestComponent query={ { isSmall: '< 200' } } /> );
+		expect( component.props.matches ).toMatchObject( { isSmall: false } );
+	} );
+
+	it( 'should return "{ isMedium: true }"', () => {
+		const component = renderWithOnLayout( <TestComponent query={ { isMedium: '> 250' } } /> );
+		expect( component.props.matches ).toMatchObject( { isMedium: true } );
+	} );
+
+	it( 'should return "{ isSmall: false, isMedium: true, isLarge: false }"', () => {
+		const component = renderWithOnLayout( <TestComponent query={ { isSmall: '< 200', isMedium: '> 250', isLarge: '> 400' } } /> );
+		expect( component.props.matches ).toMatchObject( { isSmall: false, isMedium: true, isLarge: false } );
+	} );
+
+	it( 'should return "{ isMedium: true }" using default operator', () => {
+		const component = renderWithOnLayout( <TestComponent query={ { isMedium: '200' } } /> );
+		expect( component.props.matches ).toMatchObject( { isMedium: true } );
+	} );
+} );
