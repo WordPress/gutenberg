05aef57cf109d5fae3150b0c61539070eef11ad5
dufresnesteven <steve.dufresne@automattic.com>
2020-02-05 11:32:49 +0900

    Lint the test files based on the new linter.

--- packages/e2e-tests/specs/editor/various/block-directory-add.test.js
+++ packages/e2e-tests/specs/editor/various/block-directory-add.test.js
@@ -10,8 +10,12 @@ import {
 } from '@wordpress/e2e-test-utils';
 
 // Urls to mock
-const SEARCH_URL = `rest_route=${ encodeURIComponent( '/__experimental/block-directory/search' ) }`;
-const INSTALL_URL = `rest_route=${ encodeURIComponent( '/__experimental/block-directory/install' ) }`;
+const SEARCH_URL = `rest_route=${ encodeURIComponent(
+	'/__experimental/block-directory/search'
+) }`;
+const INSTALL_URL = `rest_route=${ encodeURIComponent(
+	'/__experimental/block-directory/install'
+) }`;
 
 // Example Blocks
 const mockBlock1 = {
@@ -68,7 +72,8 @@ export function getJSONResponse( obj, contentType ) {
 }
 
 export function createJSONResponse( mockResponse, contentType = undefined ) {
-	return async ( request ) => request.respond( getJSONResponse( mockResponse, contentType ) );
+	return async ( request ) =>
+		request.respond( getJSONResponse( mockResponse, contentType ) );
 }
 
 describe( 'adding blocks from block directory', () => {
@@ -91,8 +96,14 @@ describe( 'adding blocks from block directory', () => {
 		// Search for the block via the inserter
 		await searchForBlock( impossibleBlockName );
 
-		const selectorContent = await page.evaluate( () => document.querySelector( '.block-editor-inserter__results' ).innerHTML );
-		expect( selectorContent ).not.toContain( '.block-editor-block-types-list' );
+		const selectorContent = await page.evaluate(
+			() =>
+				document.querySelector( '.block-editor-inserter__results' )
+					.innerHTML
+		);
+		expect( selectorContent ).not.toContain(
+			'.block-editor-block-types-list'
+		);
 	} );
 
 	it( 'Should be able to add (the first) block.', async () => {
@@ -101,7 +112,9 @@ describe( 'adding blocks from block directory', () => {
 			{
 				// Mock response for search with the block
 				match: ( request ) => request.url().includes( SEARCH_URL ),
-				onRequestMatch: createJSONResponse( JSON.stringify( [ mockBlock1, mockBlock2 ] ) ),
+				onRequestMatch: createJSONResponse(
+					JSON.stringify( [ mockBlock1, mockBlock2 ] )
+				),
 			},
 			{
 				// Mock response for install
@@ -110,8 +123,12 @@ describe( 'adding blocks from block directory', () => {
 			},
 			{
 				// Mock the response for the js asset once it gets injected
-				match: ( request ) => request.url().includes( mockBlock1.assets[ 0 ] ),
-				onRequestMatch: createJSONResponse( Buffer.from( block, 'utf8' ), 'application/javascript; charset=utf-8' ),
+				match: ( request ) =>
+					request.url().includes( mockBlock1.assets[ 0 ] ),
+				onRequestMatch: createJSONResponse(
+					Buffer.from( block, 'utf8' ),
+					'application/javascript; charset=utf-8'
+				),
 			},
 		] );
 
@@ -119,7 +136,9 @@ describe( 'adding blocks from block directory', () => {
 		await searchForBlock( mockBlock1.title );
 
 		// Grab the first block in the list -> Needs to be the first one, the mock response expects it.
-		const addBtn = await page.waitForSelector( '.block-directory-downloadable-blocks-list li:first-child button' );
+		const addBtn = await page.waitForSelector(
+			'.block-directory-downloadable-blocks-list li:first-child button'
+		);
 
 		// Add the block
 		await addBtn.click();
