WordPress/gutenberg

commit cfc9cd4135da010319471c2c071690ca4051c4bb

Social Link: group all variations under one block type (#19887)
* Social Link: group all variations under one block type

* Social Link: Update test fixtures

* Address review feedback

* Fix misnamed attribute 'type' -> 'site'

* Social Link: Rename attribute 'site' to 'service'

* Social Link: Update description

* Change attribute name to service (previously site)

* Lint: Equal sign alignment

* Social Link: Remove unnecessary block deprecation

This PR introduces `core/social-link` as a new block type. Strictly
speaking, any concerns of backwards compatibility should only deal with
the migration of `core/social-link-FOO` types to this new one --
something that the parser is handling directly, populating the new
`service` attribute in that process.

Thus, no specific provisions are needed for the extant but obsolete
`site` attribute: the attribute will be silently dropped during block
validation. This can be attested by loading any content with links in
the old `core/social-link-FOO` shape.

Co-authored-by: Marcus Kazmierczak <marcus@mkaz.com>

packages/block-library/src/social-link/index.php
@@ -13,17 +13,17 @@
  * @return string Rendered HTML of the referenced block.
  */
 function render_core_social_link( $attributes ) {
-	$site  = ( isset( $attributes['site'] ) ) ? $attributes['site'] : 'Icon';
-	$url   = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
-	$label = ( isset( $attributes['label'] ) ) ? $attributes['label'] : __( 'Link to ' ) . core_social_link_get_name( $site );
+	$service = ( isset( $attributes['service'] ) ) ? $attributes['service'] : 'Icon';
+	$url     = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
+	$label   = ( isset( $attributes['label'] ) ) ? $attributes['label'] : __( 'Link to ' ) . core_social_link_get_name( $service );
 
 	// Don't render a link if there is no URL set.
 	if ( ! $url ) {
 		return '';
 	}
 
-	$icon = core_social_link_get_icon( $site );
-	return '<li class="wp-social-link wp-social-link-' . $site . '"><a href="' . esc_url( $url ) . '" aria-label="' . esc_attr( $label ) . '"> ' . $icon . '</a></li>';
+	$icon = core_social_link_get_icon( $service );
+	return '<li class="wp-social-link wp-social-link-' . $service . '"><a href="' . esc_url( $url ) . '" aria-label="' . esc_attr( $label ) . '"> ' . $icon . '</a></li>';
 }
 
 /**
@@ -72,26 +72,30 @@ function register_block_core_social_link() {
 		'youtube',
 	);
 
+	$path     = __DIR__ . '/social-link/block.json';
+	$metadata = json_decode( file_get_contents( $path ), true );
+
 	foreach ( $sites as $site ) {
 		register_block_type(
 			'core/social-link-' . $site,
-			array(
-				'attributes'      => array(
-					'url'   => array(
-						'type' => 'string',
-					),
-					'site'  => array(
-						'type'    => 'string',
-						'default' => $site,
-					),
-					'label' => array(
-						'type' => 'string',
-					),
-				),
-				'render_callback' => 'render_core_social_link',
+			array_merge(
+				$metadata,
+				array(
+					'render_callback' => 'render_core_social_link',
+				)
 			)
 		);
 	}
+
+	register_block_type(
+		$metadata['name'],
+		array_merge(
+			$metadata,
+			array(
+				'render_callback' => 'render_core_social_link',
+			)
+		)
+	);
 }
 add_action( 'init', 'register_block_core_social_link' );
 

commit 6852356e5ec98a2ff10812f742e5ab25d8496023

Social Link: Fix server-side rendering for legacy format
Fixes a regression introduced in #19887.

While changing the logic in `register_block_core_social_link` to consume
the block type's `block.json` and switch to the `$service` attribute
name, I broke the handling of the legacy format for social links.

Old format: <!-- wp:social-link-wordpress {"url":"foo"} /-->
New format: <!-- wp:social-link {"service":"wordpress","url":"foo"} /-->

This fixes the handling of legacy social links by providing the adequate
attribute default for each `$service`.

packages/block-library/src/social-link/index.php
@@ -81,6 +81,18 @@ function register_block_core_social_link() {
 			array_merge(
 				$metadata,
 				array(
+					'attributes'      => array(
+						'url'     => array(
+							'type' => 'string',
+						),
+						'service' => array(
+							'type'    => 'string',
+							'default' => $site,
+						),
+						'label'   => array(
+							'type' => 'string',
+						),
+					),
 					'render_callback' => 'render_core_social_link',
 				)
 			)