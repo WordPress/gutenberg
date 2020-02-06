7e25eea02dcb11be1c743bc061c1d93a666feca6
Jerry Jones <jones.jeremydavid@gmail.com>
2020-02-06 14:34:56 -0600

    Added <mark> to New page title when creating a new page in the link control

--- packages/block-editor/src/components/link-control/search-create-button.js
+++ packages/block-editor/src/components/link-control/search-create-button.js
@@ -8,6 +8,7 @@ import classnames from 'classnames';
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { Button, Icon } from '@wordpress/components';
+import { __experimentalCreateInterpolateElement } from '@wordpress/element';
 
 export const LinkControlSearchCreate = ( {
 	searchTerm = '',
@@ -33,7 +34,13 @@ export const LinkControlSearchCreate = ( {
 
 			<span className="block-editor-link-control__search-item-header">
 				<span className="block-editor-link-control__search-item-title">
-					{ sprintf( __( 'New page: %s' ), searchTerm ) }
+					{ __experimentalCreateInterpolateElement(
+						sprintf(
+							__( 'New page: <mark>%s</mark>' ),
+							searchTerm
+						),
+						{ mark: <mark /> }
+					) }
 				</span>
 			</span>
 		</Button>
