3a22f087e454ff0d96e9d8a822ae44e92c4041ea
Luke Walczak <lukasz.walczak.pwr@gmail.com>
2020-02-06 16:25:09 +0100

    Button Block (#1616)
    * Add ref to gutenberg repo
    
    * Update ref and add react-native-linear-gradient lib
    
    * Update android in gutenberg-bridge
    
    * Add react-native-linear-gradient podspec
    
    * Update .flowconfig
    
    * Regenerate yarn.lock
    
    * add min and max width for iOS aztec
    
    * Use linear gradient lib from fork
    
    * Fix iOS
    
    * Use linear gradient lib from gb-mobile fork
    
    * Revert changes in initial html
    
    * Use sizeThatFits instead of introduced new function
    
    * Update ref and add color
    
    * Generate bundles for testing purposes
    
    * Generate fresh bundle
    
    * Remove bundle
    
    * Revert bundle changes
    
    * Revert bundles
    
    * Update ref after merge

--- react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
+++ react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
@@ -12,6 +12,8 @@ class RCTAztecView: Aztec.TextView {
     @objc var onPaste: RCTBubblingEventBlock? = nil
     @objc var onContentSizeChange: RCTBubblingEventBlock? = nil
     @objc var onSelectionChange: RCTBubblingEventBlock? = nil
+    @objc var minWidth: CGFloat = 0
+    @objc var maxWidth: CGFloat = 0
     @objc var blockType: NSDictionary? = nil {
         didSet {
             guard let block = blockType, let tag = block["tag"] as? String else {
@@ -175,12 +177,23 @@ class RCTAztecView: Aztec.TextView {
         recognizer.isEnabled = false
     }
 
-    // MARK - View Height: Match to content height
+    // MARK: - View height and width: Match to the content
 
     override func layoutSubviews() {
         super.layoutSubviews()
+        adjustWidth()
         fixLabelPositionForRTLLayout()
         updateContentSizeInRN()
+    }
+
+    private func adjustWidth() {
+        if (maxWidth > 0 && minWidth > 0) {
+            let maxSize = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
+            let newWidth = sizeThatFits(maxSize).width
+            if (newWidth != frame.size.width) {
+                frame.size.width = max(newWidth, minWidth)
+            }
+        }
     }
 
     private func fixLabelPositionForRTLLayout() {
