03cc326213e6c01b3a5e32b9afd48d6f7fa2fa29
Chip Snyder <chip.snyder3@gmail.com>
2020-02-12 17:26:35 -0500

    Add Post Content Control to the Latest post config menu

--- packages/block-library/src/latest-posts/edit.native.js
+++ packages/block-library/src/latest-posts/edit.native.js
@@ -6,51 +6,91 @@ import { View, Text } from 'react-native';
 /**
  * WordPress dependencies
  */
-import { Icon } from '@wordpress/components';
 import { withPreferredColorScheme } from '@wordpress/compose';
 import { coreBlocks } from '@wordpress/block-library';
-import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { postList as icon } from '@wordpress/icons';
+import { InspectorControls } from '@wordpress/block-editor';
+import {
+	Icon,
+	PanelBody,
+	PanelActions,
+	ToggleControl,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
 
-class LatestPostsEdit extends Component {
-	render() {
-		const { getStylesFromColorScheme, name } = this.props;
-		const blockType = coreBlocks[ name ];
+const LatestPostsEdit = ( {
+	attributes,
+	setAttributes,
+	getStylesFromColorScheme,
+	name,
+} ) => {
+	const onClearSettings = () => {
+		setAttributes( {
+			displayPostContent: attributes.displayPostContent.default,
+		} );
+	};
 
-		const blockStyle = getStylesFromColorScheme(
-			styles.latestPostBlock,
-			styles.latestPostBlockDark
-		);
+	const onSetDisplayPostContent = ( value ) => {
+		setAttributes( { displayPostContent: value } );
+	};
 
-		const iconStyle = getStylesFromColorScheme(
-			styles.latestPostBlockIcon,
-			styles.latestPostBlockIconDark
-		);
+	const blockType = coreBlocks[ name ];
 
-		const titleStyle = getStylesFromColorScheme(
-			styles.latestPostBlockMessage,
-			styles.latestPostBlockMessageDark
-		);
+	const { displayPostContent } = attributes;
 
-		const subTitleStyle = getStylesFromColorScheme(
-			styles.latestPostBlockSubtitle,
-			styles.latestPostBlockSubtitleDark
-		);
+	const actions = [
+		{
+			label: __( 'Clear All Settings' ),
+			onPress: onClearSettings,
+		},
+	];
 
-		return (
-			<View style={ blockStyle }>
-				<Icon icon={ icon } { ...iconStyle } />
-				<Text style={ titleStyle }>{ blockType.settings.title }</Text>
-				<Text style={ subTitleStyle }>{ __( 'Configure' ) }</Text>
-			</View>
-		);
-	}
-}
+	const getInspectorControls = () => (
+		<InspectorControls>
+			<PanelBody title={ __( 'Latest posts settings' ) }>
+				<ToggleControl
+					label={ __( 'Show Post Content' ) }
+					checked={ displayPostContent }
+					onChange={ onSetDisplayPostContent }
+				/>
+			</PanelBody>
+			<PanelActions actions={ actions } />
+		</InspectorControls>
+	);
+
+	const blockStyle = getStylesFromColorScheme(
+		styles.latestPostBlock,
+		styles.latestPostBlockDark
+	);
+
+	const iconStyle = getStylesFromColorScheme(
+		styles.latestPostBlockIcon,
+		styles.latestPostBlockIconDark
+	);
+
+	const titleStyle = getStylesFromColorScheme(
+		styles.latestPostBlockMessage,
+		styles.latestPostBlockMessageDark
+	);
+
+	const subTitleStyle = getStylesFromColorScheme(
+		styles.latestPostBlockSubtitle,
+		styles.latestPostBlockSubtitleDark
+	);
+
+	return (
+		<View style={ blockStyle }>
+			{ getInspectorControls() }
+			<Icon icon={ icon } { ...iconStyle } />
+			<Text style={ titleStyle }>{ blockType.settings.title }</Text>
+			<Text style={ subTitleStyle }>{ __( 'Configure' ) }</Text>
+		</View>
+	);
+};
 
 export default withPreferredColorScheme( LatestPostsEdit );
