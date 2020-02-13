550537d52374a7a60b80ae9753617699e608fd00
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2020-02-13 18:19:48 +0100

    RNMobile - Page templates - Split pages in files

---
+++ packages/block-editor/src/components/page-template-picker/templates/index.native.js
@@ -0,0 +1,5 @@
+export { default as About } from './about';
+export { default as Contact } from './contact';
+export { default as Portfolio } from './portfolio';
+export { default as Services } from './services';
+export { default as Team } from './team';
