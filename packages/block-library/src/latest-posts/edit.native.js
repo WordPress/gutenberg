b9061fa843df52dd1ad9159bb4446480357b27d4
Chip Snyder <chip.snyder3@gmail.com>
2020-02-14 15:44:27 -0500

    Refactor LatestPost to allow hooks in component mounting for api requests

--- packages/block-library/src/latest-posts/edit.native.js
+++ packages/block-library/src/latest-posts/edit.native.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { Component } from '@wordpress/element';
 import { View, Text } from 'react-native';
 
 /**
@@ -37,157 +38,158 @@ import {
 	MAX_EXCERPT_LENGTH,
 } from './constants';
 
-const LatestPostsEdit = ( {
-	attributes,
-	setAttributes,
-	getStylesFromColorScheme,
-	name,
-} ) => {
-	const blockType = coreBlocks[ name ];
+class LatestPostsEdit extends Component {
+	render() {
+		const {
+			attributes,
+			setAttributes,
+			getStylesFromColorScheme,
+			name,
+		} = this.props;
 
-	const {
-		displayPostContent,
-		displayPostContentRadio,
-		excerptLength,
-		displayPostDate,
-		order,
-		orderBy,
-		postsToShow,
-	} = attributes;
+		const blockType = coreBlocks[ name ];
+		const {
+			displayPostContent,
+			displayPostContentRadio,
+			excerptLength,
+			displayPostDate,
+			order,
+			orderBy,
+			postsToShow,
+		} = attributes;
 
-	const onClearSettings = () => {
-		setAttributes( {
-			excerptLength: DEFAULT_EXCERPT_LENGTH,
-			displayPostContent: DEFAULT_SHOW_POST_CONTENT,
-			displayPostContentRadio: DEFAULT_POST_CONTENT_RADIO,
-			displayPostDate: DEFAULT_DISPLAY_POST_DATE,
-			order: DEFAULT_ORDER,
-			orderBy: DEFAULT_ORDER_BY,
-			postsToShow: DEFAULT_POSTS_TO_SHOW,
-		} );
-	};
+		const onClearSettings = () => {
+			setAttributes( {
+				excerptLength: DEFAULT_EXCERPT_LENGTH,
+				displayPostContent: DEFAULT_SHOW_POST_CONTENT,
+				displayPostContentRadio: DEFAULT_POST_CONTENT_RADIO,
+				displayPostDate: DEFAULT_DISPLAY_POST_DATE,
+				order: DEFAULT_ORDER,
+				orderBy: DEFAULT_ORDER_BY,
+				postsToShow: DEFAULT_POSTS_TO_SHOW,
+			} );
+		};
 
-	const onSetDisplayPostContent = ( value ) => {
-		setAttributes( { displayPostContent: value } );
-	};
+		const onSetDisplayPostContent = ( value ) => {
+			setAttributes( { displayPostContent: value } );
+		};
 
-	const onSetDisplayPostContentRadio = ( value ) => {
-		setAttributes( { displayPostContentRadio: value } );
-	};
+		const onSetDisplayPostContentRadio = ( value ) => {
+			setAttributes( { displayPostContentRadio: value } );
+		};
 
-	const onSetExcerptLength = ( value ) => {
-		setAttributes( { excerptLength: value } );
-	};
+		const onSetExcerptLength = ( value ) => {
+			setAttributes( { excerptLength: value } );
+		};
 
-	const onSetDisplayPostDate = ( value ) => {
-		setAttributes( { displayPostDate: value } );
-	};
+		const onSetDisplayPostDate = ( value ) => {
+			setAttributes( { displayPostDate: value } );
+		};
 
-	const onSetOrder = ( value ) => {
-		setAttributes( { order: value } );
-	};
+		const onSetOrder = ( value ) => {
+			setAttributes( { order: value } );
+		};
 
-	const onSetOrderBy = ( value ) => {
-		setAttributes( { orderBy: value } );
-	};
+		const onSetOrderBy = ( value ) => {
+			setAttributes( { orderBy: value } );
+		};
 
-	const onSetPostsToShow = ( value ) => {
-		setAttributes( { postsToShow: value } );
-	};
+		const onSetPostsToShow = ( value ) => {
+			setAttributes( { postsToShow: value } );
+		};
 
-	const actions = [
-		{
-			label: __( 'Clear All Settings' ),
-			onPress: onClearSettings,
-		},
-	];
+		const actions = [
+			{
+				label: __( 'Clear All Settings' ),
+				onPress: onClearSettings,
+			},
+		];
 
-	const getInspectorControls = () => (
-		<InspectorControls>
-			<PanelBody title={ __( 'Sorting and filtering' ) }>
-				<QueryControls
-					{ ...{ order, orderBy } }
-					numberOfItems={ postsToShow }
-					// categoriesList={ categoriesList }
-					// selectedCategoryId={ categories }
-					onOrderChange={ onSetOrder }
-					onOrderByChange={ onSetOrderBy }
-					onNumberOfItemsChange={ onSetPostsToShow }
-				/>
-			</PanelBody>
-
-			<PanelBody title={ __( 'Post meta settings' ) }>
-				<ToggleControl
-					label={ __( 'Display post date' ) }
-					checked={ displayPostDate }
-					onChange={ onSetDisplayPostDate }
-				/>
-			</PanelBody>
-
-			<PanelBody title={ __( 'Post Content Settings' ) }>
-				<ToggleControl
-					label={ __( 'Show Post Content' ) }
-					checked={ displayPostContent }
-					onChange={ onSetDisplayPostContent }
-				/>
-				{ displayPostContent && (
-					<SelectControl
-						label={ __( 'Show' ) }
-						value={ displayPostContentRadio }
-						onChangeValue={ onSetDisplayPostContentRadio }
-						options={ [
-							{ label: __( 'Excerpt' ), value: 'excerpt' },
-							{
-								label: __( 'Full Post' ),
-								value: 'full_post',
-							},
-						] }
+		const getInspectorControls = () => (
+			<InspectorControls>
+				<PanelBody title={ __( 'Sorting and filtering' ) }>
+					<QueryControls
+						{ ...{ order, orderBy } }
+						numberOfItems={ postsToShow }
+						onOrderChange={ onSetOrder }
+						onOrderByChange={ onSetOrderBy }
+						onNumberOfItemsChange={ onSetPostsToShow }
 					/>
-				) }
-				{ displayPostContent &&
-					displayPostContentRadio === 'excerpt' && (
-						<RangeControl
-							label={ __( 'Max number of words in excerpt' ) }
-							value={ excerptLength }
-							onChange={ onSetExcerptLength }
-							min={ MIN_EXCERPT_LENGTH }
-							max={ MAX_EXCERPT_LENGTH }
+				</PanelBody>
+
+				<PanelBody title={ __( 'Post meta settings' ) }>
+					<ToggleControl
+						label={ __( 'Display post date' ) }
+						checked={ displayPostDate }
+						onChange={ onSetDisplayPostDate }
+					/>
+				</PanelBody>
+
+				<PanelBody title={ __( 'Post Content Settings' ) }>
+					<ToggleControl
+						label={ __( 'Show Post Content' ) }
+						checked={ displayPostContent }
+						onChange={ onSetDisplayPostContent }
+					/>
+					{ displayPostContent && (
+						<SelectControl
+							label={ __( 'Show' ) }
+							value={ displayPostContentRadio }
+							onChangeValue={ onSetDisplayPostContentRadio }
+							options={ [
+								{ label: __( 'Excerpt' ), value: 'excerpt' },
+								{
+									label: __( 'Full Post' ),
+									value: 'full_post',
+								},
+							] }
 						/>
 					) }
-			</PanelBody>
+					{ displayPostContent &&
+						displayPostContentRadio === 'excerpt' && (
+							<RangeControl
+								label={ __( 'Max number of words in excerpt' ) }
+								value={ excerptLength }
+								onChange={ onSetExcerptLength }
+								min={ MIN_EXCERPT_LENGTH }
+								max={ MAX_EXCERPT_LENGTH }
+							/>
+						) }
+				</PanelBody>
 
-			<PanelActions actions={ actions } />
-		</InspectorControls>
-	);
+				<PanelActions actions={ actions } />
+			</InspectorControls>
+		);
 
-	const blockStyle = getStylesFromColorScheme(
-		styles.latestPostBlock,
-		styles.latestPostBlockDark
-	);
+		const blockStyle = getStylesFromColorScheme(
+			styles.latestPostBlock,
+			styles.latestPostBlockDark
+		);
 
-	const iconStyle = getStylesFromColorScheme(
-		styles.latestPostBlockIcon,
-		styles.latestPostBlockIconDark
-	);
+		const iconStyle = getStylesFromColorScheme(
+			styles.latestPostBlockIcon,
+			styles.latestPostBlockIconDark
+		);
 
-	const titleStyle = getStylesFromColorScheme(
-		styles.latestPostBlockMessage,
-		styles.latestPostBlockMessageDark
-	);
+		const titleStyle = getStylesFromColorScheme(
+			styles.latestPostBlockMessage,
+			styles.latestPostBlockMessageDark
+		);
 
-	const subTitleStyle = getStylesFromColorScheme(
-		styles.latestPostBlockSubtitle,
-		styles.latestPostBlockSubtitleDark
-	);
+		const subTitleStyle = getStylesFromColorScheme(
+			styles.latestPostBlockSubtitle,
+			styles.latestPostBlockSubtitleDark
+		);
 
-	return (
-		<View style={ blockStyle }>
-			{ getInspectorControls() }
-			<Icon icon={ icon } { ...iconStyle } />
-			<Text style={ titleStyle }>{ blockType.settings.title }</Text>
-			<Text style={ subTitleStyle }>{ __( 'Configure' ) }</Text>
-		</View>
-	);
-};
+		return (
+			<View style={ blockStyle }>
+				{ getInspectorControls() }
+				<Icon icon={ icon } { ...iconStyle } />
+				<Text style={ titleStyle }>{ blockType.settings.title }</Text>
+				<Text style={ subTitleStyle }>{ __( 'Configure' ) }</Text>
+			</View>
+		);
+	}
+}
 
 export default withPreferredColorScheme( LatestPostsEdit );
