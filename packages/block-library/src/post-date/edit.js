7d510fc34c97eebd46b6d8a4cb0af47e5a067559
epiqueras <epiquerass@gmail.com>
2020-02-17 10:41:20 -0800

    Block Library: Redesign new Post Date block features.

--- packages/block-library/src/post-date/edit.js
+++ packages/block-library/src/post-date/edit.js
@@ -2,14 +2,23 @@
  * WordPress dependencies
  */
 import { useEntityProp, useEntityId } from '@wordpress/core-data';
+import { useState } from '@wordpress/element';
 import { __experimentalGetSettings, dateI18n } from '@wordpress/date';
-import { Popover, DateTimePicker, ToolbarGroup } from '@wordpress/components';
-import { BlockControls } from '@wordpress/block-editor';
+import { BlockControls, InspectorControls } from '@wordpress/block-editor';
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	Popover,
+	DateTimePicker,
+	PanelBody,
+	CustomSelectControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-function PostDateEditor( { format, setAttributes, isSelected } ) {
+function PostDateEditor( { format, setAttributes } ) {
 	const [ siteFormat ] = useEntityProp( 'root', 'site', 'date_format' );
 	const [ date, setDate ] = useEntityProp( 'postType', 'post', 'date' );
+	const [ isPickerOpen, setIsPickerOpen ] = useState( false );
 	const settings = __experimentalGetSettings();
 	const is12Hour = /a(?!\\)/i.test(
 		settings.formats.time
@@ -19,11 +28,31 @@ function PostDateEditor( { format, setAttributes, isSelected } ) {
 			.reverse()
 			.join( '' )
 	);
+	const formatOptions = Object.keys( settings.formats ).map(
+		( formatKey ) => ( {
+			key: settings.formats[ formatKey ],
+			name: dateI18n( settings.formats[ formatKey ], date ),
+		} )
+	);
+	const resolvedFormat = format || siteFormat || settings.formats.date;
 	return date ? (
 		<time dateTime={ dateI18n( 'c', date ) }>
-			{ dateI18n( format || siteFormat || settings.formats.date, date ) }
-			{ isSelected && (
-				<Popover>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton
+						icon="edit"
+						title={ __( 'Change Date' ) }
+						onClick={ () =>
+							setIsPickerOpen(
+								( _isPickerOpen ) => ! _isPickerOpen
+							)
+						}
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			{ dateI18n( resolvedFormat, date ) }
+			{ isPickerOpen && (
+				<Popover onClose={ setIsPickerOpen.bind( null, false ) }>
 					<DateTimePicker
 						currentDate={ date }
 						onChange={ setDate }
@@ -31,20 +60,23 @@ function PostDateEditor( { format, setAttributes, isSelected } ) {
 					/>
 				</Popover>
 			) }
-			<BlockControls>
-				<ToolbarGroup
-					icon="edit"
-					title={ __( 'Date Format' ) }
-					controls={ Object.keys( settings.formats ).map( ( formatKey ) => ( {
-						title: dateI18n( settings.formats[ formatKey ], date ),
-						onClick() {
-							setAttributes( { format: settings.formats[ formatKey ] } );
-						},
-						isActive: settings.formats[ formatKey ] === format,
-					} ) ) }
-					isCollapsed
-				/>
-			</BlockControls>
+			<InspectorControls>
+				<PanelBody title={ __( 'Format settings' ) }>
+					<CustomSelectControl
+						hideLabelFromVision
+						label={ __( 'Date Format' ) }
+						options={ formatOptions }
+						onChange={ ( { selectedItem } ) =>
+							setAttributes( {
+								format: selectedItem.key,
+							} )
+						}
+						value={ formatOptions.find(
+							( option ) => option.key === resolvedFormat
+						) }
+					/>
+				</PanelBody>
+			</InspectorControls>
 		</time>
 	) : (
 		__( 'No Date' )
@@ -54,16 +86,9 @@ function PostDateEditor( { format, setAttributes, isSelected } ) {
 export default function PostDateEdit( {
 	attributes: { format },
 	setAttributes,
-	isSelected,
 } ) {
 	if ( ! useEntityId( 'postType', 'post' ) ) {
 		return 'Post Date Placeholder';
 	}
-	return (
-		<PostDateEditor
-			format={ format }
-			setAttributes={ setAttributes }
-			isSelected={ isSelected }
-		/>
-	);
+	return <PostDateEditor format={ format } setAttributes={ setAttributes } />;
 }
