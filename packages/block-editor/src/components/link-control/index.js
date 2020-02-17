437ed18f45cc09f487eaf95768b9188bf1cdeb24
Dave Smith <getdavemail@gmail.com>
2020-02-17 11:47:54 +0000

    Improves method name and supporting comments and doc block.
    Addresses https://github.com/WordPress/gutenberg/pull/19775#discussion_r379574073

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -291,7 +291,7 @@ function LinkControl( {
 		// to the text value of the `<input>`. This is because `title` is used
 		// when creating the suggestion. Similarly `url` is used when using keyboard to select
 		// the suggestion (the <form> `onSubmit` handler falls-back to `url`).
-		return maybeURL( val )
+		return isURLLike( val )
 			? results
 			: results.concat( {
 					// the `id` prop is intentionally ommitted here because it
@@ -319,21 +319,23 @@ function LinkControl( {
 
 	/**
 	 * Determines whether a given value could be a URL. Note this does not
-	 * guarantee the value is a URL only that it looks like it might be one
-	 * (thus the naming of the method).
+	 * guarantee the value is a URL only that it looks like it might be one. For
+	 * example, just because a string has `www.` in it doesn't make it a URL,
+	 * but it does make it highly likely that it will be so in the context of
+	 * creating a link it makes sense to treat it like one.
 	 *
-	 * @param {string} val the candidate for being a valid URL (or not).
+	 * @param {string} val the candidate for being URL-like (or not).
+	 * @return {boolean}   whether or not the value is potentially a URL.
 	 */
-	function maybeURL( val ) {
+	function isURLLike( val ) {
 		const isInternal = startsWith( val, '#' );
-
-		return isInternal || isURL( val ) || ( val && val.includes( 'www.' ) );
+		return isURL( val ) || ( val && val.includes( 'www.' ) ) || isInternal;
 	}
 
 	// Effects
 	const getSearchHandler = useCallback(
 		( val, args ) => {
-			return maybeURL( val )
+			return isURLLike( val )
 				? handleDirectEntry( val, args )
 				: handleEntitySearch( val, args );
 		},
