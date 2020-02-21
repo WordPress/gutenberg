d4b9d7652db0ccb962063c66c47dadb14d80e25d
Brent Swisher <brent@brentswisher.com>
2020-02-21 16:14:06 -0500

    Add story with position and update child node of TooltipStory

--- packages/components/src/tooltip/stories/index.js
+++ packages/components/src/tooltip/stories/index.js
@@ -1,13 +1,12 @@
 /**
  * External dependencies
  */
-import { text } from '@storybook/addon-knobs';
+import { text, select } from '@storybook/addon-knobs';
 
 /**
  * Internal dependencies
  */
 import Tooltip from '../';
-import Button from '../../button';
 
 export default {
 	title: 'Components/ToolTip',
@@ -18,7 +17,40 @@ export const _default = () => {
 	const tooltipText = text( 'Text', 'More information' );
 	return (
 		<Tooltip text={ tooltipText }>
-			<Button isSecondary>Hover for more information</Button>
+			<div
+				style={ {
+					margin: '100px',
+					textAlign: 'center',
+					border: '1px solid #ccc',
+				} }
+			>
+				Hover for more information
+			</div>
+		</Tooltip>
+	);
+};
+export const withPosition = () => {
+	const positionOptions = {
+		'top left': 'top left',
+		'top center ': 'top center',
+		'top right': 'top right',
+		'bottom left': 'bottom left',
+		'bottom center ': 'bottom center',
+		'bottom right': 'bottom right',
+	};
+	const tooltipText = text( 'Text', 'More information' );
+	const position = select( 'Position', positionOptions, 'top center' );
+	return (
+		<Tooltip text={ tooltipText } position={ position }>
+			<div
+				style={ {
+					margin: '100px',
+					textAlign: 'center',
+					border: '1px solid #ccc',
+				} }
+			>
+				Hover for more information
+			</div>
 		</Tooltip>
 	);
 };
