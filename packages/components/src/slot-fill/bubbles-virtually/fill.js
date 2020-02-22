7c3e5fc618bb9ac37e4a8609ebeb55e33270146c
Haz <hazdiego@gmail.com>
2020-02-21 21:05:00 -0300

    Add code comments on Fill

--- packages/components/src/slot-fill/bubbles-virtually/fill.js
+++ packages/components/src/slot-fill/bubbles-virtually/fill.js
@@ -13,6 +13,9 @@ export default function Fill( { name, children } ) {
 	const ref = useRef();
 
 	useEffect( () => {
+		// We register fills so we can keep track of their existance.
+		// Some Slot implementations need to know if there're already fills
+		// registered so they can choose to render themselves or not.
 		slot.registerFill( ref );
 		return () => {
 			slot.unregisterFill( ref );
@@ -27,10 +30,18 @@ export default function Fill( { name, children } ) {
 		children = children( slot.fillProps );
 	}
 
-	const { length } = Object.keys( slot.fills );
+	// We re-mount the portal children whenever the number of fills change
+	// See https://github.com/WordPress/gutenberg/pull/19242#discussion_r381152030
+	// We also check if the current fill has been already registered, because
+	// the useEffect above will be called only after the first render, which
+	// could trigger a re-mount here unnecessarily in the second render.
+	const key =
+		slot.fills.indexOf( ref ) !== -1
+			? slot.fills.length
+			: slot.fills.length + 1;
 
 	return createPortal(
-		<Fragment key={ length }>{ children }</Fragment>,
+		<Fragment key={ key }>{ children }</Fragment>,
 		slot.ref.current
 	);
 }
