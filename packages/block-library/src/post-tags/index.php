2f88ee68393890bc10d5abcec9f330aa48efbc65
epiqueras <epiquerass@gmail.com>
2020-02-24 09:51:31 -0800

    Block Library: Add features to the Post Tags block.

--- packages/block-library/src/post-tags/index.php
+++ packages/block-library/src/post-tags/index.php
@@ -8,20 +8,40 @@
 /**
  * Renders the `core/post-tags` block on the server.
  *
+ * @param array $attributes The block attributes.
+ *
  * @return string Returns the filtered post tags for the current post wrapped inside "a" tags.
  */
-function render_block_core_post_tags() {
+function render_block_core_post_tags( $attributes ) {
 	$post = gutenberg_get_post_from_context();
 	if ( ! $post ) {
 		return '';
 	}
 	$post_tags = get_the_tags();
 	if ( ! empty( $post_tags ) ) {
-		$output = '';
-		foreach ( $post_tags as $tag ) {
-			$output .= '<a href="' . get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a>' . ' | ';
-		}
-		return trim( $output, ' | ' );
+		$output = array_map(
+			function ( $tag ) {
+				return '<a href="' .
+				get_tag_link( $tag->term_id ) .
+				'">' .
+				$tag->name .
+				'</a>';
+			},
+			$post_tags
+		);
+		$output = join(
+			$attributes['separator'],
+			$output
+		);
+		return ( isset( $attributes['beforeText'] )
+			? ( $attributes['beforeText'] . ' ' )
+			: ''
+		) .
+		$output .
+		( isset( $attributes['afterText'] )
+			? ( ' ' . $attributes['afterText'] )
+			: ''
+		);
 	}
 }
 
@@ -32,6 +52,18 @@ function register_block_core_post_tags() {
 	register_block_type(
 		'core/post-tags',
 		array(
+			'attributes'      => array(
+				'beforeText' => array(
+					'type' => 'string',
+				),
+				'separator'  => array(
+					'type'    => 'string',
+					'default' => ' | ',
+				),
+				'afterText'  => array(
+					'type' => 'string',
+				),
+			),
 			'render_callback' => 'render_block_core_post_tags',
 		)
 	);
