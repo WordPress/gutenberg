4361c98e0e45dc9c959d469939a9c4ebbcda403f
Jorge <jorge.costa@developer.pt>
2020-02-24 17:21:20 +0000

    Fix: Inconsistency on Import from JSON button look

--- packages/list-reusable-blocks/src/components/import-dropdown/index.js
+++ packages/list-reusable-blocks/src/components/import-dropdown/index.js
@@ -20,7 +20,12 @@ function ImportDropdown( { onUpload } ) {
 			position="bottom right"
 			contentClassName="list-reusable-blocks-import-dropdown__content"
 			renderToggle={ ( { isOpen, onToggle } ) => (
-				<Button aria-expanded={ isOpen } onClick={ onToggle } isPrimary>
+				<Button
+					aria-expanded={ isOpen }
+					onClick={ onToggle }
+					isPrimary
+					isSmall
+				>
 					{ __( 'Import from JSON' ) }
 				</Button>
 			) }
