4ad8629d07186cd52c1d086664b31aedc5a320fe
Joen Asmussen <asmussen@gmail.com>
2020-02-26 13:31:39 +0100

    Icon tweaks.

--- packages/icons/src/library/plus.js
+++ packages/icons/src/library/plus.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const plus = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M12.5 17H11v-5H6v-1.5h5V6h1.5v4.5H17V12h-4.5z" />
+		<Path d="M13.2 17.2h-1.4v-5h-5v-1.4h5V6.2h1.4v4.6h4.6v1.4h-4.6z" />
 	</SVG>
 );
 
