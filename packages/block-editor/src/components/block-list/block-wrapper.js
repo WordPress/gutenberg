06bc974c080458f675c027a2484342a850b219bc
Ella van Durpe <ella@vandurpe.com>
2020-02-28 12:10:44 +0100

    Fix alignment styles

--- packages/block-editor/src/components/block-list/block-wrapper.js
+++ packages/block-editor/src/components/block-list/block-wrapper.js
@@ -48,6 +48,7 @@ const BlockComponent = forwardRef(
 			name,
 			mode,
 			blockTitle,
+			wrapperProps,
 		} = useContext( BlockContext );
 		const { initialPosition } = useSelect(
 			( select ) => {
@@ -203,6 +204,7 @@ const BlockComponent = forwardRef(
 				// Overrideable props.
 				aria-label={ blockLabel }
 				role="group"
+				{ ...wrapperProps }
 				{ ...props }
 				id={ blockElementId }
 				ref={ wrapper }
