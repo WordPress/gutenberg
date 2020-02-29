45c6255e3694f5cea246b1673bd0c49278b92ee2
Matthew Kevins <mmkevins@yahoo.com>
2020-03-01 02:29:38 +1000

    Render waveHeight attribute

--- packages/block-library/src/surf/edit.js
+++ packages/block-library/src/surf/edit.js
@@ -6,6 +6,9 @@ import Text from './text';
 // Let's make some waves!
 // We can add attributes to our block, and get and set them in the edit function
 
-const SurfEdit = () => <Text>Hello world!</Text>;
+const SurfEdit = ( { attributes, setAttributes } ) => {
+	const { waveHeight } = attributes;
 
+	return <Text>Wave height: { waveHeight } ft</Text>;
+};
 export default SurfEdit;
