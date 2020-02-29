2d367a97a48c1076e0331f10f134c68ca934aa1b
Matthew Kevins <mmkevins@yahoo.com>
2020-03-01 00:03:48 +1000

    Add message about web / mobile incompatibility

--- packages/block-library/src/surf/edit.native.js
+++ packages/block-library/src/surf/edit.native.js
@@ -3,4 +3,6 @@
  */
 import { Text } from 'react-native';
 
+// Since web does not have the React Native Text component, we can alias Text
+// to an HTML element
 export default () => <Text>Hello world!</Text>;
