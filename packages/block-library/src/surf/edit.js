417aabe8748904d5230c7e4ad3f2e692032496b7
Matthew Kevins <mmkevins@yahoo.com>
2020-03-01 02:36:58 +1000

    Add component imports and comment

--- packages/block-library/src/surf/edit.js
+++ packages/block-library/src/surf/edit.js
@@ -1,14 +1,23 @@
 /**
+ * WordPress dependencies
+ */
+import { RangeControl, PanelBody } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import Text from './text';
 
 // Let's make some waves!
-// We can add attributes to our block, and get and set them in the edit function
+//
+// We can use some components to make our attribute adjustable
 
 const SurfEdit = ( { attributes, setAttributes } ) => {
 	const { waveHeight } = attributes;
 
 	return <Text>Wave height: { waveHeight } ft</Text>;
 };
+
 export default SurfEdit;
