df29d5cc5fb148489ef8c39052fd42629c9d53f4
Matthew Kevins <mmkevins@yahoo.com>
2020-03-01 02:23:33 +1000

    Add waveHeight attribute and comments

--- packages/block-library/src/surf/edit.js
+++ packages/block-library/src/surf/edit.js
@@ -3,7 +3,8 @@
  */
 import Text from './text';
 
-// Usually, we name our edit function
+// Let's make some waves!
+// We can add attributes to our block, and get and set them in the edit function
 
 const SurfEdit = () => <Text>Hello world!</Text>;
 
