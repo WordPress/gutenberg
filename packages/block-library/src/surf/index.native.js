cf431dca5ff476a9a786d3381895cd78a8713717
Matthew Kevins <mmkevins@yahoo.com>
2020-02-29 22:54:35 +1000

    Add an icon for our block

--- packages/block-library/src/surf/index.native.js
+++ packages/block-library/src/surf/index.native.js
@@ -7,13 +7,15 @@ import { Text } from 'react-native';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Dashicon } from '@wordpress/components';
 
 export const name = 'core/surf';
 
 export const settings = {
-	title: __( 'Surf' ), // let's make sure our user facing strings are translated
+	title: __( 'Surf' ),
 	category: 'common',
 	description: __( 'Add surf conditions.' ),
 	edit: () => <Text>Hello world!</Text>,
 	save: () => <span>Hello world!</span>,
+	icon: <Dashicon icon="palmtree" />, // let's give our block a nice icon :)
 };
