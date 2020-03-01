3059db63ccd7d63f895ce3abf76bc709e38a4f56
James Newell <james.newell@automattic.com>
2020-03-02 10:49:21 +1100

    use explicit exports

--- packages/i18n/src/index.js
+++ packages/i18n/src/index.js
@@ -4,4 +4,4 @@
 
 export { sprintf } from './sprintf';
 export { I18n } from './i18n';
-export * from './default-i18n';
+export { i18n, setLocaleData, __, _x, _n, _nx, isRTL } from './default-i18n';
