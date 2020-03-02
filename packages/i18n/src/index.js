3b29ce262f240813b22457711b65b7cbf6c511a4
James Newell <james.newell@automattic.com>
2020-03-02 11:03:07 +1100

    remove comment

--- packages/i18n/src/index.js
+++ packages/i18n/src/index.js
@@ -1,7 +1,3 @@
-/**
- * Internal dependencies
- */
-
 export { sprintf } from './sprintf';
 export { I18n } from './i18n';
 export { i18n, setLocaleData, __, _x, _n, _nx, isRTL } from './default-i18n';
