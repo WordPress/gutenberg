64e29690393f7907fec570bc8d5fa7c372de1300
jasmussen <joen@automattic.com>
2020-03-03 15:49:08 +0100

    Another batch of icons.

--- packages/block-library/src/html/icon.js
+++ packages/block-library/src/html/icon.js
@@ -5,6 +5,6 @@ import { SVG, Path } from '@wordpress/components';
 
 export default (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M4.5,11h-2V9H1v6h1.5v-2.5h2V15H6V9H4.5V11z M7,10.5h1.5V15H10v-4.5h1.5V9H7V10.5z M14.5,10l-1-1H12v6h1.5v-3.9  l1,1l1-1V15H17V9h-1.5L14.5,10z M19.5,13.5V9H18v6h5v-1.5H19.5z" />
+		<Path d="M4.5 11.4H1.8V9H.7v6h1.1v-2.6h2.8V15h1.1V9H4.5v2.4zM6.4 10h1.7v5h1.1v-5h1.7V9H6.4v1zm9.5-1l-1.5 2.7L13 9h-.9l-.8 6h1.1l.5-4 1.5 2.8L16 11l.5 4h1.1l-.8-6h-.9zm3.8 5V9h-1.1v6h3.6v-1h-2.5z" />
 	</SVG>
 );
