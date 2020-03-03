31a35c26374687ed3f684fa881e0bf3fb5294719
James Newell <james.newell@automattic.com>
2020-03-03 16:39:38 +1100

    improve docs

--- packages/i18n/src/index.js
+++ packages/i18n/src/index.js
@@ -1,3 +1,3 @@
 export { sprintf } from './sprintf';
 export { createI18n } from './create-i18n';
-export { i18n, setLocaleData, __, _x, _n, _nx, isRTL } from './default-i18n';
+export { setLocaleData, __, _x, _n, _nx, isRTL } from './default-i18n';
