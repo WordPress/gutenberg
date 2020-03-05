8a94254b6adac030f5a6ffd8e718972e1f19f48e
Ella van Durpe <wp@iseulde.com>
2020-03-05 18:16:11 +0100

    Light block: automatically add block class (#20658)
    * Light block: automatically add block class
    
    * Add custom class

--- packages/block-library/src/column/edit.js
+++ packages/block-library/src/column/edit.js
@@ -21,13 +21,12 @@ import { __ } from '@wordpress/i18n';
 function ColumnEdit( {
 	attributes,
 	setAttributes,
-	className,
 	updateAlignment,
 	hasChildBlocks,
 } ) {
 	const { verticalAlignment, width } = attributes;
 
-	const classes = classnames( className, 'block-core-columns', {
+	const classes = classnames( 'block-core-columns', {
 		[ `is-vertically-aligned-${ verticalAlignment }` ]: verticalAlignment,
 	} );
 
