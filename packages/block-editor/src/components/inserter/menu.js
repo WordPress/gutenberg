6edd3e82125fc8dd71190656ba76e0d6a3f998fe
Riad Benguella <benguella@gmail.com>
2020-03-05 09:02:39 +0100

    Remove the default help panel

--- packages/block-editor/src/components/inserter/menu.js
+++ packages/block-editor/src/components/inserter/menu.js
@@ -468,76 +468,48 @@ export class InserterMenu extends Component {
 						</div>
 					) }
 				</div>
-				{ hasHelpPanel && (
+				{ hasHelpPanel && hoveredItem && (
 					<div className="block-editor-inserter__menu-help-panel">
-						{ hoveredItem && (
-							<>
-								{ ! isReusableBlock( hoveredItem ) && (
-									<BlockCard blockType={ hoveredItem } />
-								) }
-								<div className="block-editor-inserter__preview">
-									{ isReusableBlock( hoveredItem ) ||
-									hoveredItemBlockType.example ? (
-										<div className="block-editor-inserter__preview-content">
-											<BlockPreview
-												padding={ 10 }
-												viewportWidth={ 500 }
-												blocks={
-													hoveredItemBlockType.example
-														? getBlockFromExample(
-																hoveredItem.name,
-																{
-																	attributes: {
-																		...hoveredItemBlockType
-																			.example
-																			.attributes,
-																		...hoveredItem.initialAttributes,
-																	},
-																	innerBlocks:
-																		hoveredItemBlockType
-																			.example
-																			.innerBlocks,
-																}
-														  )
-														: createBlock(
-																hoveredItem.name,
-																hoveredItem.initialAttributes
-														  )
-												}
-											/>
-										</div>
-									) : (
-										<div className="block-editor-inserter__preview-content-missing">
-											{ __( 'No Preview Available.' ) }
-										</div>
-									) }
-								</div>
-							</>
+						{ ! isReusableBlock( hoveredItem ) && (
+							<BlockCard blockType={ hoveredItem } />
 						) }
-						{ ! hoveredItem && (
-							<div className="block-editor-inserter__menu-help-panel-no-block">
-								<div className="block-editor-inserter__menu-help-panel-no-block-text">
-									<div className="block-editor-inserter__menu-help-panel-title">
-										{ __( 'Content blocks' ) }
-									</div>
-									<p>
-										{ __(
-											'Welcome to the wonderful world of blocks! Blocks are the basis of all content within the editor.'
-										) }
-									</p>
-									<p>
-										{ __(
-											'There are blocks available for all kinds of content: insert text, headings, images, lists, videos, tables, and lots more.'
-										) }
-									</p>
-									<p>
-										{ __(
-											'Browse through the library to learn more about what each block does.'
-										) }
-									</p>
+						<div className="block-editor-inserter__preview">
+							{ isReusableBlock( hoveredItem ) ||
+							hoveredItemBlockType.example ? (
+								<div className="block-editor-inserter__preview-content">
+									<BlockPreview
+										padding={ 10 }
+										viewportWidth={ 500 }
+										blocks={
+											hoveredItemBlockType.example
+												? getBlockFromExample(
+														hoveredItem.name,
+														{
+															attributes: {
+																...hoveredItemBlockType
+																	.example
+																	.attributes,
+																...hoveredItem.initialAttributes,
+															},
+															innerBlocks:
+																hoveredItemBlockType
+																	.example
+																	.innerBlocks,
+														}
+												  )
+												: createBlock(
+														hoveredItem.name,
+														hoveredItem.initialAttributes
+												  )
+										}
+									/>
 								</div>
-							</div>
-						) }
+							) : (
+								<div className="block-editor-inserter__preview-content-missing">
+									{ __( 'No Preview Available.' ) }
+								</div>
+							) }
+						</div>
 					</div>
 				) }
 			</div>
