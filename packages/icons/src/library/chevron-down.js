5be6ddf1d154a1cb89ef0348166e3def0c954a14
Joen Asmussen <joen@automattic.com>
2020-03-06 16:38:49 +0100

    Polish G2 (#20679)
    Co-authored-by: Riad Benguella <benguella@gmail.com>

--- packages/icons/src/library/chevron-down.js
+++ packages/icons/src/library/chevron-down.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const chevronDown = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M5.5 10.6l1-1.2 5.2 4.8 4.8-4.7 1 1-5.7 5.8-6.3-5.7z" />
+		<Path d="M17 9.4L12 14 7 9.4l-1 1.2 6 5.4 6-5.4z" />
 	</SVG>
 );
 
--- packages/icons/src/library/chevron-right.js
+++ packages/icons/src/library/chevron-right.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const chevronRight = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M10.6 18.5l-1.2-1 4.8-5.2-4.7-4.8 1-1 5.8 5.7z" />
+		<Path d="M10.6 6L9.4 7l4.6 5-4.6 5 1.2 1 5.4-6z" />
 	</SVG>
 );
 
