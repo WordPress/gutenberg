8a2df15a66ebdbca87df1bbd86d26f5498577dce
jasmussen <joen@automattic.com>
2020-03-06 10:21:34 +0100

    Update chevron icons.

--- packages/icons/src/library/chevron-left.js
+++ packages/icons/src/library/chevron-left.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const chevronLeft = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M13.4 18.5l-5.7-6.3 5.8-5.7 1 1-4.7 4.8 4.8 5.2z" />
+		<Path d="M14.6 7l-1.2-1L8 12l5.4 6 1.2-1-4.6-5z" />
 	</SVG>
 );
 
