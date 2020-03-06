9fa86b9bba421fc8e09a24b4936048d06a3a98d6
Andrew Duthie <andrew@andrewduthie.com>
2020-03-06 11:35:33 -0500

    Compat: Move polyfill script registration to client-assets.php
    Only scripts registered in gutenberg_register_vendor_scripts are downloaded as part of the plugin build script.

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -205,17 +205,36 @@ function gutenberg_override_style( &$styles, $handle, $src, $deps = array(), $ve
  *
  * @param WP_Scripts $scripts WP_Scripts instance (passed by reference).
  */
-function gutenberg_register_vendor_scripts( &$scripts ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-	// This function is intentionally left empty.
-	//
-	// Scripts such as react and react-dom are expected to be overridden soon,
-	// and it is preferred to keep this function in place so as not to disturb
-	// tooling related to the plugin build process.
-	//
-	// TODO: Remove phpcs exception in function signature once this function
-	// regains its use.
-	//
-	// See https://github.com/WordPress/gutenberg/pull/20628.
+function gutenberg_register_vendor_scripts( &$scripts ) {
+	$suffix = SCRIPT_DEBUG ? '' : '.min';
+
+	/*
+	 * This script registration and the corresponding function should be removed
+	 * once the plugin is updated to support WordPress 5.4.0 and newer.
+	 *
+	 * See: `gutenberg_add_url_polyfill`
+	 */
+	gutenberg_register_vendor_script(
+		$scripts,
+		'wp-polyfill-url',
+		'https://unpkg.com/core-js-url-browser@3.6.4/url' . $suffix . '.js',
+		array(),
+		'3.6.4'
+	);
+
+	/*
+	 * This script registration and the corresponding function should be removed
+	 * removed once the plugin is updated to support WordPress 5.4.0 and newer.
+	 *
+	 * See: `gutenberg_add_dom_rect_polyfill`
+	 */
+	gutenberg_register_vendor_script(
+		$scripts,
+		'wp-polyfill-dom-rect',
+		'https://unpkg.com/polyfill-library@3.42.0/polyfills/DOMRect/polyfill.js',
+		array(),
+		'3.42.0'
+	);
 }
 add_action( 'wp_default_scripts', 'gutenberg_register_vendor_scripts' );
 
