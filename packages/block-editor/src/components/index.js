4cee5dc9bf662cce353adf390b9393a89908f0c7
Jon Q <hello@jonquach.com>
2020-03-09 15:01:53 -0400

    Export LineHeightControl as experimental from block-editor

--- packages/block-editor/src/components/index.js
+++ packages/block-editor/src/components/index.js
@@ -34,6 +34,11 @@ export { default as InnerBlocks } from './inner-blocks';
 export { default as InspectorAdvancedControls } from './inspector-advanced-controls';
 export { default as InspectorControls } from './inspector-controls';
 export { default as __experimentalLinkControl } from './link-control';
+export {
+	default as __experimentalLineHeightControl,
+	getLineHeightControlStyles as __experimentalGetLineHeightControlStyles,
+	getLineHeightControlClassName as __experimentalGetLineHeightControlClassName,
+} from './line-height-control';
 export { default as MediaReplaceFlow } from './media-replace-flow';
 export { default as MediaPlaceholder } from './media-placeholder';
 export { default as MediaUpload } from './media-upload';
