58d57f703f5a5a799a7e71ff2aa457feb1c968b7
Jon Q <hello@jonquach.com>
2020-03-09 14:59:20 -0400

    Add LineHeightControl component to block-editor

---
+++ packages/block-editor/src/components/line-height-control/styles.js
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+import styled from '@emotion/styled';
+
+export const LineHeightControlWrapper = styled.div`
+	margin-bottom: 24px;
+
+	input {
+		display: block;
+		max-width: 60px;
+	}
+`;
