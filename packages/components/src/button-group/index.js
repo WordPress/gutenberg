726ac0e1b8fac5b5e438771d12887b6d09e94e0f
Alex Lende <ajlende@gmail.com>
2020-03-11 19:00:51 +0000

    Add keyboard handlers

--- packages/components/src/button-group/index.js
+++ packages/components/src/button-group/index.js
@@ -6,23 +6,25 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Children, cloneElement } from '@wordpress/element';
+import { Children, cloneElement, useRef } from '@wordpress/element';
 
 function ButtonGroup( {
 	mode,
 	checked,
 	onChange,
 	className,
-	children,
+	children: reactChildren,
 	...props
 } ) {
 	const classes = classnames( 'components-button-group', className );
 	const role = mode === 'radio' ? 'radiogroup' : 'group';
+	const children = Children.toArray( reactChildren );
+	const childRefs = useRef( new Map() );
 
 	return (
 		<div className={ classes } role={ role } { ...props }>
 			{ mode === 'radio'
-				? Children.map( children, ( child, index ) => {
+				? children.map( ( child, index ) => {
 						// TODO: Handle children witout value props
 						const { value } = child.props;
 						const isChecked = checked === value;
@@ -35,12 +37,47 @@ function ButtonGroup( {
 								isChecked || ( ! checked && index === 0 )
 									? 0
 									: -1,
+							ref: ( ref ) =>
+								ref === null
+									? childRefs.current.delete( index )
+									: childRefs.current.set( index, ref ),
+							onKeyDown( e ) {
+								if (
+									e.key === 'ArrowUp' ||
+									e.key === 'ArrowLeft'
+								) {
+									e.preventDefault();
+
+									const prevIndex =
+										( index - 1 + children.length ) %
+										children.length;
+									const prevValue =
+										children[ prevIndex ].props.value;
+
+									childRefs.current.get( prevIndex ).focus();
+									onChange( prevValue );
+								}
+								if (
+									e.key === 'ArrowDown' ||
+									e.key === 'ArrowRight'
+								) {
+									e.preventDefault();
+
+									const nextIndex =
+										( index + 1 ) % children.length;
+									const nextValue =
+										children[ nextIndex ].props.value;
+
+									childRefs.current.get( nextIndex ).focus();
+									onChange( nextValue );
+								}
+							},
 							onClick() {
 								onChange( value );
 							},
 						} );
 				  } )
-				: children }
+				: reactChildren }
 		</div>
 	);
 }
