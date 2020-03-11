8b02d17718f0c92191f735c59503784027fafc3f
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-03-11 12:29:53 +0100

    Refactor doc and web light wrapper

--- packages/compose/src/hooks/use-resize-observer/index.native.js
+++ packages/compose/src/hooks/use-resize-observer/index.native.js
@@ -10,7 +10,7 @@ import { useState, useCallback } from '@wordpress/element';
 /**
  * Hook which allows to listen the resize event of any target element when it changes sizes.
  *
- * @return {Object} Measurements object with properties `width` and `height`.
+ * @return {Array} An array of {Element} `resizeListener` and {?Object} `sizes` with properties `width` and `height`
  *
  * @example
  *
