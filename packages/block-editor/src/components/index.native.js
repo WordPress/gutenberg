eda33953edcfa80b9579c01a911af3d6e96f8545
Jon Q <hello@jonquach.com>
2020-03-11 09:57:55 -0400

    Export LineHeightControls and withLineHeight for native

--- packages/block-editor/src/components/index.native.js
+++ packages/block-editor/src/components/index.native.js
@@ -11,6 +11,10 @@ export * from './font-sizes';
 export { default as AlignmentToolbar } from './alignment-toolbar';
 export { default as InnerBlocks } from './inner-blocks';
 export { default as InspectorControls } from './inspector-controls';
+export {
+	default as __experimentalLineHeightControl,
+	withLineHeight as __experimentalWithLineHeight,
+} from './line-height-control';
 export { default as PlainText } from './plain-text';
 export {
 	default as RichText,
