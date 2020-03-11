5ccd36f2da592f99b746806815ef2a708371d557
Alex Lende <ajlende@gmail.com>
2020-03-11 17:15:20 +0000

    Add radio mode to ButtonGroup with aria attributes

--- packages/components/src/button-group/index.js
+++ packages/components/src/button-group/index.js
@@ -3,10 +3,41 @@
  */
 import classnames from 'classnames';
 
-function ButtonGroup( { className, ...props } ) {
-	const classes = classnames( 'components-button-group', className );
+/**
+ * WordPress dependencies
+ */
+import { Children, cloneElement } from '@wordpress/element';
 
-	return <div { ...props } className={ classes } role="group" />;
+function ButtonGroup( {
+	mode,
+	checked,
+	onChange,
+	className,
+	children,
+	...props
+} ) {
+	const classes = classnames( 'components-button-group', className );
+	const role = mode === 'radio' ? 'radiogroup' : 'group';
+
+	return (
+		<div className={ classes } role={ role } { ...props }>
+			{ mode === 'radio'
+				? Children.map( children, ( child ) => {
+						// TODO: Handle children witout value props
+						const isChecked = checked === child.props.value;
+						return cloneElement( child, {
+							role: mode,
+							'aria-checked': isChecked,
+							isPrimary: isChecked,
+							isSecondary: ! isChecked,
+							onClick() {
+								onChange( child.props.value );
+							},
+						} );
+				  } )
+				: children }
+		</div>
+	);
 }
 
 export default ButtonGroup;
