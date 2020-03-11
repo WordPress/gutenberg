33451443e4f27af3c149a89ea1095887f6690291
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-03-11 16:28:22 +0100

    Add basic PHPUnit tests for block metadata registration

--- lib/compat.php
+++ lib/compat.php
@@ -8,15 +8,13 @@
  * @package gutenberg
  */
 
- if ( ! function_exists( 'register_block_type_from_metadata' ) ) {
+if ( ! function_exists( 'register_block_type_from_metadata' ) ) {
 	/**
 	 * Registers a block type from metadata stored in the `block.json` file.
 	 *
 	 * @since 7.8.0
 	 *
 	 * @param string $path Path to the folder where the `block.json` file is located.
-	 *                     Alternatively it's possible to provide only the name of the
-	 *                     last folder for blocks located in the WordPress core.
 	 * @param array  $args {
 	 *     Optional. Array of block type arguments. Any arguments may be defined, however the
 	 *     ones described below are supported by default. Default empty array.
@@ -26,13 +24,12 @@
 	 * @return WP_Block_Type|false The registered block type on success, or false on failure.
 	 */
 	function register_block_type_from_metadata( $path, $args = array() ) {
-		$path = is_dir( $path ) ? $path : ABSPATH . WPINC . '/blocks/' . $path;
 		$file = trailingslashit( $path ) . 'block.json';
 		if ( ! file_exists( $file ) ) {
 			return false;
 		}
 
-		$metadata = json_decode( file_get_contents( $path ), true );
+		$metadata = json_decode( file_get_contents( $file ), true );
 		if ( ! is_array( $metadata ) ) {
 			return false;
 		}
@@ -45,7 +42,7 @@
 			)
 		);
 	}
- }
+}
 
 /**
  * Adds a polyfill for the WHATWG URL in environments which do not support it.
