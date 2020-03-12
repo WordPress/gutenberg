7dda4422e46976ea4de03d6cbe20f51dfacc54b3
Sérgio Gomes <mail@sgomes.com>
2020-03-12 14:29:15 +0000

    Improve performance testing (#20802)

--- packages/e2e-tests/specs/performance/performance.test.js
+++ packages/e2e-tests/specs/performance/performance.test.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { join } from 'path';
-import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'fs';
 
 /**
  * WordPress dependencies
@@ -17,6 +17,48 @@ function readFile( filePath ) {
 	return existsSync( filePath )
 		? readFileSync( filePath, 'utf8' ).trim()
 		: '';
+}
+
+function deleteFile( filePath ) {
+	if ( existsSync( filePath ) ) {
+		unlinkSync( filePath );
+	}
+}
+
+function isKeyEvent( item ) {
+	return (
+		item.cat === 'devtools.timeline' &&
+		item.name === 'EventDispatch' &&
+		item.dur &&
+		item.args &&
+		item.args.data
+	);
+}
+
+function isKeyDownEvent( item ) {
+	return isKeyEvent( item ) && item.args.data.type === 'keydown';
+}
+
+function isKeyPressEvent( item ) {
+	return isKeyEvent( item ) && item.args.data.type === 'keypress';
+}
+
+function isKeyUpEvent( item ) {
+	return isKeyEvent( item ) && item.args.data.type === 'keyup';
+}
+
+function getEventDurationsForType( trace, filterFunction ) {
+	return trace.traceEvents
+		.filter( filterFunction )
+		.map( ( item ) => item.dur / 1000 );
+}
+
+function getEventDurations( trace ) {
+	return [
+		getEventDurationsForType( trace, isKeyDownEvent ),
+		getEventDurationsForType( trace, isKeyPressEvent ),
+		getEventDurationsForType( trace, isKeyUpEvent ),
+	];
 }
 
 describe( 'Performance', () => {
@@ -49,34 +91,66 @@ describe( 'Performance', () => {
 		};
 
 		let i = 1;
-		let startTime;
-
-		await page.on( 'load', () =>
-			results.load.push( new Date() - startTime )
-		);
-		await page.on( 'domcontentloaded', () =>
-			results.domcontentloaded.push( new Date() - startTime )
-		);
 
 		while ( i-- ) {
-			startTime = new Date();
 			await page.reload( { waitUntil: [ 'domcontentloaded', 'load' ] } );
+			const timings = JSON.parse(
+				await page.evaluate( () =>
+					JSON.stringify( window.performance.timing )
+				)
+			);
+			const {
+				navigationStart,
+				domContentLoadedEventEnd,
+				loadEventEnd,
+			} = timings;
+			results.load.push( loadEventEnd - navigationStart );
+			results.domcontentloaded.push(
+				domContentLoadedEventEnd - navigationStart
+			);
 		}
 
 		await insertBlock( 'Paragraph' );
 
 		i = 200;
+		const traceFile = __dirname + '/trace.json';
+
+		await page.tracing.start( {
+			path: traceFile,
+			screenshots: false,
+			categories: [ 'devtools.timeline' ],
+		} );
 
 		while ( i-- ) {
-			startTime = new Date();
 			await page.keyboard.type( 'x' );
-			results.type.push( new Date() - startTime );
+		}
+
+		await page.tracing.stop();
+
+		const traceResults = JSON.parse( readFile( traceFile ) );
+		const [
+			keyDownEvents,
+			keyPressEvents,
+			keyUpEvents,
+		] = getEventDurations( traceResults );
+
+		if (
+			keyDownEvents.length === keyPressEvents.length &&
+			keyPressEvents.length === keyUpEvents.length
+		) {
+			for ( let j = 0; j < keyDownEvents.length; j++ ) {
+				results.type.push(
+					keyDownEvents[ j ] + keyPressEvents[ j ] + keyUpEvents[ j ]
+				);
+			}
 		}
 
 		writeFileSync(
 			__dirname + '/results.json',
 			JSON.stringify( results, null, 2 )
 		);
+
+		deleteFile( traceFile );
 
 		expect( true ).toBe( true );
 	} );
