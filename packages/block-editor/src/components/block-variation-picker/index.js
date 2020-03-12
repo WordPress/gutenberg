zoomyboy/gutenberg-1

Add visible labels to InnerBlocksTemplatePicker pattern selection buttons Fixes #18737.

packages/block-editor/src/components/block-pattern-picker/index.js
@@ -35,7 +35,11 @@ function BlockPatternPicker( {
 				*/
 				/* eslint-disable jsx-a11y/no-redundant-roles */
 			}
-			<ul className="block-editor-block-pattern-picker__patterns" role="list">
+			<ul
+				className="block-editor-block-pattern-picker__patterns"
+				role="list"
+				aria-label={ __( 'Block patterns' ) }
+			>
 				{ patterns.map( ( pattern ) => (
 					<li key={ pattern.name }>
 						<Button
@@ -44,8 +48,9 @@ function BlockPatternPicker( {
 							iconSize={ 48 }
 							onClick={ () => onSelect( pattern ) }
 							className="block-editor-block-pattern-picker__pattern"
-							label={ pattern.label }
-						/>
+						>
+							{ pattern.label }
+						</Button>
 					</li>
 				) ) }
 			</ul>

WordPress/gutenberg

packages/block-editor/src/components/block-variation-picker/index.js
@@ -37,6 +37,7 @@ function BlockVariationPicker( {
 			<ul
 				className="block-editor-block-variation-picker__variations"
 				role="list"
+				aria-label={ __( 'Block variations' ) }
 			>
 				{ variations.map( ( variation ) => (
 					<li key={ variation.name }>
@@ -46,8 +47,9 @@ function BlockVariationPicker( {
 							iconSize={ 48 }
 							onClick={ () => onSelect( variation ) }
 							className="block-editor-block-variation-picker__variation"
-							label={ variation.title }
-						/>
+						>
+							{ variation.title }
+						</Button>
 					</li>
 				) ) }
 			</ul>