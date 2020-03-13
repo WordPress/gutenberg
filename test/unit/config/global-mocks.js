f8da7a19d3cec213984db3a8e46e30573861927f
Luke Walczak <lukasz.walczak.pwr@gmail.com>
2020-03-13 13:15:22 +0100

    [RNMobile] Create cross platform `useResizeObserver()` hook (#19918)
    * Create a hook to be useResizeObserver() and unify with web API

--- test/unit/config/global-mocks.js
+++ test/unit/config/global-mocks.js
@@ -1,6 +1,11 @@
 jest.mock( '@wordpress/compose', () => {
+	const App = () => null;
 	return {
 		...jest.requireActual( '@wordpress/compose' ),
 		useViewportMatch: jest.fn(),
+		useResizeObserver: jest.fn( () => [
+			<App key={ 'mock-key' } />,
+			{ width: 700, height: 500 },
+		] ),
 	};
 } );
