759710b0acd3466d449a62e9093c25111cc9543d
Riad Benguella <benguella@gmail.com>
2020-03-13 12:27:21 +0100

    Deprecate the BlockNavigationDropdown component

--- packages/block-editor/src/components/block-navigation/dropdown.js
+++ packages/block-editor/src/components/block-navigation/dropdown.js
@@ -1,4 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import deprecated from '@wordpress/deprecated';
+
 function BlockNavigationDropdown() {
+	deprecated( 'BlockNavigationDropdown component', {
+		hint: 'This component is now include in the TableOfContents component.',
+	} );
 	return null;
 }
 
