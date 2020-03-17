e0165e0996aaf2d79a2600ebb01b49d53daed921
KHeo <sainthkh@naver.com>
2020-03-17 18:47:03 +0900

    Add script: dump-ast.

---
+++ packages/docgen/scripts/dump-ast.js
@@ -0,0 +1,17 @@
+/**
+ * External dependencies
+ */
+const ts = require( 'typescript' );
+const { readFileSync } = require( 'fs' );
+const { inspect } = require( 'util' );
+
+const filename = process.argv[ 2 ];
+
+const sourceFile = ts.createSourceFile(
+	filename,
+	readFileSync( filename ).toString(),
+	ts.ScriptTarget.ES2020
+);
+
+// eslint-disable-next-line no-console
+console.log( inspect( sourceFile, false, null ) );
