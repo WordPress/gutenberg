0dfe1b04004fa45fcaa5fc9436f99fd573f00a1a
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-03-17 15:44:57 +0200

    solves tag duplication

--- packages/block-library/src/latest-posts/edit.js
+++ packages/block-library/src/latest-posts/edit.js
@@ -156,14 +156,9 @@ class LatestPostsEdit extends Component {
 		} );
 		request.then( unescapeTerms ).then( ( terms ) => {
 			this.setState( ( state ) => ( {
-				suggestedTags: state.tagsList.concat(
-					terms.filter(
-						( term ) =>
-							! find(
-								state.tagsList,
-								( tag ) => tag.id === term.id
-							)
-					)
+				suggestedTags: terms.filter(
+					( term ) =>
+						! find( state.tagsList, ( tag ) => tag.id === term.id )
 				),
 			} ) );
 		} );
