3df753620c8cf95e6dab4c92d1cd62bc41be0315
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-03-18 14:10:12 +0100

    Support dark mode in SegmentedControls

--- packages/components/src/mobile/segmented-control/index.native.js
+++ packages/components/src/mobile/segmented-control/index.native.js
@@ -7,6 +7,7 @@ import { View, TouchableWithoutFeedback, Text, Platform } from 'react-native';
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
+import { withPreferredColorScheme } from '@wordpress/compose';
 
 /**
  * Internal dependencies
@@ -15,23 +16,38 @@ import styles from './style.scss';
 
 const isIOS = Platform.OS === 'ios';
 
-const Segment = ( { isSelected, title, onPress } ) => {
+const Segment = ( {
+	isSelected,
+	title,
+	onPress,
+	getStylesFromColorScheme,
+} ) => {
 	const isSelectedIOS = isIOS && isSelected;
 
 	const segmentStyle = [ styles.segment, isIOS && styles.segmentIOS ];
 	const outlineStyle = [ styles.outline, isIOS && styles.outlineIOS ];
 
-	// TODO: Support dark mode
+	const selectedStyle = getStylesFromColorScheme(
+		styles.selected,
+		styles.selectedDark
+	);
+	const textStyle = getStylesFromColorScheme(
+		styles.buttonTextDefault,
+		styles.buttonTextDefaultDark
+	);
+	const selectedTextStyle = getStylesFromColorScheme(
+		styles.buttonTextSelected,
+		styles.buttonTextSelectedDark
+	);
+	const shadowStyle = getStylesFromColorScheme( styles.shadowIOS, {} );
+
 	return (
-		<View style={ isSelectedIOS && styles.shadowIOS }>
+		<View style={ isSelectedIOS && shadowStyle }>
 			<TouchableWithoutFeedback onPress={ onPress }>
-				<View style={ [ segmentStyle, isSelected && styles.selected ] }>
+				<View style={ [ segmentStyle, isSelected && selectedStyle ] }>
 					{ isSelected && <View style={ outlineStyle } /> }
 					<Text
-						style={ [
-							styles.buttonTextDefault,
-							isSelected && styles.buttonTextSelected,
-						] }
+						style={ [ textStyle, isSelected && selectedTextStyle ] }
 					>
 						{ title }
 					</Text>
@@ -46,9 +62,13 @@ const SegmentedControls = ( {
 	segmentHandler,
 	addonLeft,
 	addonRight,
+	getStylesFromColorScheme,
 } ) => {
 	const [ activeSegment, setActiveSegment ] = useState( segments[ 0 ] );
-	const containerStyle = [ styles.container, isIOS && styles.containerIOS ];
+	const containerStyle = getStylesFromColorScheme(
+		styles.container,
+		styles.containerDark
+	);
 
 	function onHandlePress( item ) {
 		setActiveSegment( item );
@@ -58,13 +78,14 @@ const SegmentedControls = ( {
 	return (
 		<View style={ styles.row }>
 			<View style={ { flex: 1 } }>{ addonLeft }</View>
-			<View style={ containerStyle }>
+			<View style={ [ containerStyle, isIOS && styles.containerIOS ] }>
 				{ segments.map( ( segment ) => (
 					<Segment
 						title={ sprintf( __( '%s' ), segment ) }
 						onPress={ () => onHandlePress( segment ) }
 						isSelected={ activeSegment === segment }
 						key={ segment }
+						getStylesFromColorScheme={ getStylesFromColorScheme }
 					/>
 				) ) }
 			</View>
@@ -73,4 +94,4 @@ const SegmentedControls = ( {
 	);
 };
 
-export default SegmentedControls;
+export default withPreferredColorScheme( SegmentedControls );
