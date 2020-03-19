47b0f41012f8b40e6c7c79c3b166813b6958d115
KHeo <sainthkh@naver.com>
2020-03-19 11:07:04 +0900

    Remove doctrine and unnecessary files/code. Edit comment.

--- packages/docgen/src/get-jsdoc-from-token.js
+++ packages/docgen/src/get-jsdoc-from-token.js
@@ -1,37 +1,12 @@
 /**
- * External dependencies.
- */
-const doctrine = require( 'doctrine' );
-
-/**
- * Internal dependencies.
- */
-const getLeadingComments = require( './get-leading-comments' );
-const getTypeAsString = require( './get-type-as-string' );
-
-/**
- * Function that takes an Espree token and returns
- * a object representing the leading JSDoc comment of the token,
+ * Function that takes a TypeScript statement and returns
+ * a object representing the leading JSDoc comment of the statement,
  * if any.
  *
- * @param {Object} token Espree token.
+ * @param {Object} statement TypeScript statement.
+ * @param {Object} typeChecker TypeScript typeChecker.
  * @return {Object} Object representing the JSDoc comment.
  */
-module.exports = function( token ) {
-	let jsdoc;
-	const comments = getLeadingComments( token );
-	if ( comments && /^\*\r?\n/.test( comments ) ) {
-		jsdoc = doctrine.parse( comments, {
-			unwrap: true,
-			recoverable: true,
-			sloppy: true,
-		} );
-		jsdoc.tags = jsdoc.tags.map( ( tag ) => {
-			if ( tag.type ) {
-				tag.type = getTypeAsString( tag.type );
-			}
-			return tag;
-		} );
-	}
+module.exports = function( statement, typeChecker ) {
 	return jsdoc;
 };
