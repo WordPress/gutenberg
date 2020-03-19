50358cd6a560fb73dd63fe4254a3cea9af1c1a16
Jakub Binda <jakub.binda@gmail.com>
2020-03-19 14:00:23 +0100

    cleanup code

--- packages/block-library/src/columns/edit.native.js
+++ packages/block-library/src/columns/edit.native.js
@@ -192,9 +192,7 @@ const ColumnsEditContainerWrapper = withDispatch(
 		 */
 		updateColumns( previousColumns, newColumns ) {
 			const { clientId } = ownProps;
-			const { replaceInnerBlocks, selectBlock } = dispatch(
-				'core/block-editor'
-			);
+			const { replaceInnerBlocks } = dispatch( 'core/block-editor' );
 			const { getBlocks } = registry.select( 'core/block-editor' );
 
 			let innerBlocks = getBlocks( clientId );
@@ -215,16 +213,6 @@ const ColumnsEditContainerWrapper = withDispatch(
 					innerBlocks,
 					previousColumns - newColumns
 				);
-			}
-
-			if ( innerBlocks.length < MIN_COLUMNS_NUMBER ) {
-				innerBlocks = [
-					...innerBlocks,
-					...times( MIN_COLUMNS_NUMBER - innerBlocks.length, () => {
-						return createBlock( 'core/column' );
-					} ),
-				];
-				selectBlock( clientId );
 			}
 
 			replaceInnerBlocks( clientId, innerBlocks, false );
