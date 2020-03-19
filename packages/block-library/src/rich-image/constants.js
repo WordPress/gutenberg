9c895cde22d4f56b9776ee8f38d53e63f3210965
Alex Lende <ajlende@gmail.com>
2020-03-19 15:02:04 -0400

    Finish removing filters

--- packages/block-library/src/rich-image/constants.js
+++ packages/block-library/src/rich-image/constants.js
@@ -1,42 +1,5 @@
-/**
- * WordPress dependencies
- */
-
-import { __ } from '@wordpress/i18n';
-
 export const isSupportedBlock = ( blockName ) =>
 	[ 'core/image', 'core/cover' ].includes( blockName );
-
-export const supportedFilters = [
-	{
-		title: __( 'Technicolor' ),
-		value: 'technicolor',
-	},
-	{
-		title: __( 'Black & White' ),
-		value: 'black-and-white',
-	},
-	{
-		title: __( 'Sunshine' ),
-		value: 'sunshine',
-	},
-	{
-		title: __( 'Aviator' ),
-		value: 'aviator',
-	},
-	{
-		title: __( 'Polar Vortex' ),
-		value: 'polar-vortex',
-	},
-	{
-		title: __( 'Rocketeer' ),
-		value: 'rocketeer',
-	},
-	{
-		title: __( 'Black Hole' ),
-		value: 'black-hole',
-	},
-];
 
 export const richImageAttributes = {
 	imageCropWidth: {
@@ -54,9 +17,5 @@ export const richImageAttributes = {
 	imageCropY: {
 		type: 'number',
 		default: 0,
-	},
-	imageFilter: {
-		type: 'string',
-		default: '',
 	},
 };
