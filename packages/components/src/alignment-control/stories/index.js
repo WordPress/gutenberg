54cd5846a01c8734ccaca7f67e481735bd124518
Jon Q <hello@jonquach.com>
2020-03-20 15:07:45 -0400

    Add initial AlignmentControl

---
+++ packages/components/src/alignment-control/stories/index.js
@@ -0,0 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import AlignmentControl from '../';
+
+export default {
+	title: 'Components/AligmentControl',
+	component: AlignmentControl,
+};
+
+export const _default = () => {
+	return <AlignmentControl />;
+};
