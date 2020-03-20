23cd26465db027ddfaa941fb262d4465d15ed751
Chip Snyder <chip.snyder3@gmail.com>
2020-03-20 13:47:31 -0400

    Hide the toolbar when inserting blocks, in order to not hide the block inserter

--- packages/block-editor/src/store/selectors.native.js
+++ packages/block-editor/src/store/selectors.native.js
@@ -5,24 +5,32 @@ import {
 	getBlockHierarchyRootClientId,
 	getBlockOrder,
 	isBlockSelected,
+	isBlockInsertionPointVisible,
 } from './selectors.js';
 
 export * from './selectors.js';
 
 export function shouldAllowSpaceForFloatingToolbar( state, clientId ) {
-
 	const innerBlocks = getBlockOrder( state, clientId );
 	const hasInnerBlocks = innerBlocks.length !== 0;
 
-	const firstChildAdjustsForFloatingToolbar = hasInnerBlocks && shouldAllowSpaceForFloatingToolbar( state, innerBlocks[0] );
+	const firstChildAdjustsForFloatingToolbar =
+		hasInnerBlocks &&
+		shouldAllowSpaceForFloatingToolbar( state, innerBlocks[ 0 ] );
 
-	return shouldShowFloatingToolbar( state, clientId ) || firstChildAdjustsForFloatingToolbar;
+	return (
+		shouldShowFloatingToolbar( state, clientId ) ||
+		firstChildAdjustsForFloatingToolbar
+	);
 }
 
 export function shouldShowFloatingToolbar( state, clientId ) {
+	if ( isBlockInsertionPointVisible( state ) ) {
+		return false;
+	}
 
 	const rootBlockId = getBlockHierarchyRootClientId( state, clientId );
 	const hasRootInnerBlocks = getBlockOrder( state, rootBlockId ).length !== 0;
-	
-	return hasRootInnerBlocks && isBlockSelected( state, clientId )
+
+	return hasRootInnerBlocks && isBlockSelected( state, clientId );
 }
