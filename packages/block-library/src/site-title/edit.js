a83cb80095795fe7998212f287a9a68a8e318a77
Ella van Durpe <wp@iseulde.com>
2020-03-23 13:30:16 +0100

    PlainText v2 (#21076)

--- packages/block-library/src/site-title/edit.js
+++ packages/block-library/src/site-title/edit.js
@@ -3,12 +3,13 @@
  */
 import { useEntityProp } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
-import { Editable } from '@wordpress/block-editor';
+import { PlainText } from '@wordpress/block-editor';
 
 export default function SiteTitleEdit() {
 	const [ title, setTitle ] = useEntityProp( 'root', 'site', 'title' );
 	return (
-		<Editable
+		<PlainText
+			__experimentalVersion={ 2 }
 			tagName="h1"
 			placeholder={ __( 'Site Title' ) }
 			value={ title }
