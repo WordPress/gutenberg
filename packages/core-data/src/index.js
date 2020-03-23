09d478448e2a22c486a3af3aaa7e637346ce7165
Daniel Richards <daniel.richards@automattic.com>
2020-03-23 14:43:38 +0800

    Allow entities to be marked as experimental

--- packages/core-data/src/index.js
+++ packages/core-data/src/index.js
@@ -20,19 +20,24 @@ import { REDUCER_KEY } from './name';
 // The "kind" and the "name" of the entity are combined to generate these shortcuts.
 
 const entitySelectors = defaultEntities.reduce( ( result, entity ) => {
-	const { kind, name } = entity;
-	result[ getMethodName( kind, name ) ] = ( state, key ) =>
+	const { kind, name, isExperimental = false } = entity;
+	const prefix = isExperimental ? '__experimentalGet' : 'get';
+	const singularMethodName = getMethodName( kind, name, prefix );
+	const pluralMethodName = getMethodName( kind, name, prefix, true );
+	result[ singularMethodName ] = ( state, key ) =>
 		selectors.getEntityRecord( state, kind, name, key );
-	result[ getMethodName( kind, name, 'get', true ) ] = ( state, ...args ) =>
+	result[ pluralMethodName ] = ( state, ...args ) =>
 		selectors.getEntityRecords( state, kind, name, ...args );
 	return result;
 }, {} );
 
 const entityResolvers = defaultEntities.reduce( ( result, entity ) => {
-	const { kind, name } = entity;
-	result[ getMethodName( kind, name ) ] = ( key ) =>
+	const { kind, name, isExperimental = false } = entity;
+	const prefix = isExperimental ? '__experimentalGet' : 'get';
+	const singularMethodName = getMethodName( kind, name, prefix );
+	const pluralMethodName = getMethodName( kind, name, prefix, true );
+	result[ singularMethodName ] = ( key ) =>
 		resolvers.getEntityRecord( kind, name, key );
-	const pluralMethodName = getMethodName( kind, name, 'get', true );
 	result[ pluralMethodName ] = ( ...args ) =>
 		resolvers.getEntityRecords( kind, name, ...args );
 	result[ pluralMethodName ].shouldInvalidate = ( action, ...args ) =>
@@ -46,8 +51,9 @@ const entityResolvers = defaultEntities.reduce( ( result, entity ) => {
 }, {} );
 
 const entityActions = defaultEntities.reduce( ( result, entity ) => {
-	const { kind, name } = entity;
-	result[ getMethodName( kind, name, 'save' ) ] = ( key ) =>
+	const { kind, name, isExperimental = false } = entity;
+	const prefix = isExperimental ? '__experimentalSave' : 'save';
+	result[ getMethodName( kind, name, prefix ) ] = ( key ) =>
 		actions.saveEntityRecord( kind, name, key );
 	return result;
 }, {} );
