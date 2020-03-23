2f81601264bfee21b07810ab01166bbd81767ef3
Sergio Estevao <sergioestevao@gmail.com>
2020-03-23 11:20:27 +0000

    Use RichText instead of plain text for code editing.
    This will allow the scrollto caret functionality to work.

--- packages/block-library/src/code/edit.native.js
+++ packages/block-library/src/code/edit.native.js
@@ -6,14 +6,12 @@ import { View } from 'react-native';
 /**
  * WordPress dependencies
  */
-import { PlainText } from '@wordpress/block-editor';
+import { RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { withPreferredColorScheme } from '@wordpress/compose';
-
 /**
  * Internal dependencies
  */
-
 /**
  * Block code style
  */
@@ -22,36 +20,29 @@ import styles from './theme.scss';
 // Note: styling is applied directly to the (nested) PlainText component. Web-side components
 // apply it to the container 'div' but we don't have a proper proposal for cascading styling yet.
 export function CodeEdit( props ) {
-	const {
-		attributes,
-		setAttributes,
-		onFocus,
-		onBlur,
-		getStylesFromColorScheme,
-	} = props;
+	const { attributes, setAttributes, getStylesFromColorScheme } = props;
 	const codeStyle = getStylesFromColorScheme(
 		styles.blockCode,
 		styles.blockCodeDark
 	);
-	const placeholderStyle = getStylesFromColorScheme(
-		styles.placeholder,
-		styles.placeholderDark
-	);
 
 	return (
-		<View>
-			<PlainText
+		<View style={ codeStyle }>
+			<RichText
+				tagName="pre"
+				preserveWhiteSpace
 				value={ attributes.content }
-				style={ codeStyle }
-				multiline={ true }
-				underlineColorAndroid="transparent"
-				onChange={ ( content ) => setAttributes( { content } ) }
+				onChange={ ( nextContent ) => {
+					const cleanContent = nextContent.replace(
+						RegExp( '<br>', 'gim' ),
+						'\n'
+					);
+					setAttributes( {
+						content: cleanContent,
+					} );
+				} }
 				placeholder={ __( 'Write code…' ) }
-				aria-label={ __( 'Code' ) }
-				isSelected={ props.isSelected }
-				onFocus={ onFocus }
-				onBlur={ onBlur }
-				placeholderTextColor={ placeholderStyle.color }
+				style={ codeStyle }
 			/>
 		</View>
 	);
