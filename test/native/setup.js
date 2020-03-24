43ce820a8a0e32982569ed6bcc55560e0be54426
Marko Savic <savicmarko1985@gmail.com>
2020-03-24 17:39:46 +0100

    Revert "[RNMobile] - Implements Dark Mode on Android platform (#19293)"
    This reverts commit 78e749dc88b65aa066b58cd67826d97a215eab1d.

--- test/native/setup.js
+++ test/native/setup.js
@@ -13,7 +13,6 @@ jest.mock( 'react-native-gutenberg-bridge', () => {
 		subscribeSetFocusOnTitle: jest.fn(),
 		subscribeUpdateHtml: jest.fn(),
 		subscribeMediaAppend: jest.fn(),
-		subscribePreferredColorScheme: () => 'light',
 		editorDidMount: jest.fn(),
 		editorDidAutosave: jest.fn(),
 		subscribeMediaUpload: jest.fn(),
