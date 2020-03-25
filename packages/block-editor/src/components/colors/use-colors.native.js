6603642c17bf610b2ce7c00acd3f6e45b376a5eb
Sérgio Estêvão <sergioestevao@gmail.com>
2020-03-25 18:50:38 +0000

    Refactor heading block to share more code with web version. (#20745)
    * Refactor heading block to share more code with web version.
    
    * Remove branching for RN.
    
    We now have an implementation of Block for RN.
    
    * Remove platform checks at block level.
    
    * Fix lint-errors.
    
    * Implement Dropdown menu for React Native.
    
    * Increase headers icon size.
    
    * Show all heading levels on the block controls in RN
    
    We also hide the headings settings in RN because all of them are
    available in the block controls.
    
    * Style separators to have left margin.
    
    * Color selected checkmark on cell
    
    * Add separators for dropdown menus
    
    * Remove extra padding on panel for dropdown
    
    * Fix separator on iOS and Android.

---
+++ packages/block-editor/src/components/colors/use-colors.native.js
@@ -0,0 +1,10 @@
+const TextColor = ( props ) => <>{ props.children }</>;
+
+const InspectorControlsColorPanel = () => null;
+
+export default function __experimentalUseColors() {
+	return {
+		TextColor,
+		InspectorControlsColorPanel,
+	};
+}
