90f92faa629ca4f4040dd79e0097e28aa4a889cf
tellthemachines <isabel@tellthemachines.com>
2020-03-25 12:39:55 +1100

    Added jsdoc

--- packages/block-library/src/navigation/use-api-fetch.js
+++ packages/block-library/src/navigation/use-api-fetch.js
@@ -4,6 +4,13 @@
 import { useEffect } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
+/**
+ * Function that fetches data using apiFetch, and updates the status.
+ *
+ * @param {Function} setIsRequesting function to set requesting state to true or false.
+ * @param {Function} setData Function to set data being returned, or empty array on error.
+ * @param {string} path Query path.
+ */
 export default function useApiFetch( setIsRequesting, setData, path ) {
 	useEffect( () => {
 		// Indicate the fetching status
