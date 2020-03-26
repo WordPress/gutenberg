8dffe92dbd4abd3f0469de844239e5ae3a1c51d7
Jon Surrell <jon.surrell@automattic.com>
2020-03-26 16:53:24 +0100

    Explain lint-staged script with a comment

--- bin/packages/lint-staged-typecheck.js
+++ bin/packages/lint-staged-typecheck.js
@@ -5,6 +5,13 @@ const execa = require( 'execa' );
 
 /* eslint-disable no-console */
 try {
+	// This trivial command is necessary to invoke `tsc` via `lint-staged`
+	//
+	// `lint-staged` passes changed files to the script. That would result in
+	// attempting to run `tsc --build path/to/changed/file.js`, which does not
+	// correspond to a project (a directory with a `tsconfig.json` files)!
+	//
+	// Run here to ignore changed files.
 	execa.sync( 'npm', [ 'run', 'build:package-types' ] );
 } catch ( err ) {
 	console.error( err.message );
