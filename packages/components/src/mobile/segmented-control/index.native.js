e4c87577d89d23e25150d82840e867eeebf48163
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-03-26 12:27:32 +0100

    Animate segemented controls

--- packages/components/src/mobile/segmented-control/index.native.js
+++ packages/components/src/mobile/segmented-control/index.native.js
@@ -7,12 +7,15 @@ import {
 	Text,
 	Platform,
 	LayoutAnimation,
+	Animated,
+	Easing,
 } from 'react-native';
+import { take, values, map, reduce } from 'lodash';
 /**
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import { withPreferredColorScheme } from '@wordpress/compose';
 
 /**
@@ -29,16 +32,12 @@ const Segment = ( {
 	title,
 	onPress,
 	getStylesFromColorScheme,
+	onLayout,
 } ) => {
 	const isSelectedIOS = isIOS && isSelected;
 
 	const segmentStyle = [ styles.segment, isIOS && styles.segmentIOS ];
-	const outlineStyle = [ styles.outline, isIOS && styles.outlineIOS ];
 
-	const selectedStyle = getStylesFromColorScheme(
-		styles.selected,
-		styles.selectedDark
-	);
 	const textStyle = getStylesFromColorScheme(
 		styles.buttonTextDefault,
 		styles.buttonTextDefaultDark
@@ -52,8 +51,7 @@ const Segment = ( {
 	return (
 		<View style={ isSelectedIOS && shadowStyle }>
 			<TouchableWithoutFeedback onPress={ onPress }>
-				<View style={ [ segmentStyle, isSelected && selectedStyle ] }>
-					{ isSelected && <View style={ outlineStyle } /> }
+				<View style={ segmentStyle } onLayout={ onLayout }>
 					<Text
 						style={ [ textStyle, isSelected && selectedTextStyle ] }
 					>
@@ -73,10 +71,44 @@ const SegmentedControls = ( {
 	getStylesFromColorScheme,
 } ) => {
 	const [ activeSegmentIndex, setActiveSegmentIndex ] = useState( 0 );
+	const [ segmentsDimensions, setSegmentsDimensions ] = useState( {
+		'0': { width: 0, height: 0 },
+	} );
+	const [ positionAnimationValue ] = useState( new Animated.Value( 0 ) );
+
+	useEffect( () => {
+		positionAnimationValue.setValue(
+			calculateEndValue( activeSegmentIndex )
+		);
+	}, [ segmentsDimensions ] );
+
 	const containerStyle = getStylesFromColorScheme(
 		styles.container,
 		styles.containerDark
 	);
+
+	function pefrormAnimation( index ) {
+		Animated.timing( positionAnimationValue, {
+			toValue: calculateEndValue( index ),
+			duration: 200,
+			easing: Easing.ease,
+		} ).start();
+	}
+
+	function calculateEndValue( index ) {
+		const { paddingLeft: offset } = isIOS
+			? styles.containerIOS
+			: styles.container;
+		const widths = map( values( segmentsDimensions ), 'width' );
+		const widthsDistance = take( widths, index );
+		const widthsDistanceSum = reduce(
+			widthsDistance,
+			( sum, n ) => sum + n
+		);
+
+		const endValue = index === 0 ? 0 : widthsDistanceSum;
+		return endValue + offset;
+	}
 
 	function onHandlePress( segment, index ) {
 		LayoutAnimation.configureNext(
@@ -88,7 +120,27 @@ const SegmentedControls = ( {
 		);
 		setActiveSegmentIndex( index );
 		segmentHandler( segment );
+		pefrormAnimation( index, segment );
 	}
+
+	function segmentOnLayout( event, index ) {
+		const { width, height } = event.nativeEvent.layout;
+
+		setSegmentsDimensions( {
+			...segmentsDimensions,
+			[ index ]: { width, height },
+		} );
+	}
+
+	const selectedStyle = getStylesFromColorScheme(
+		styles.selected,
+		styles.selectedDark
+	);
+
+	const width = segmentsDimensions[ activeSegmentIndex ].width;
+	const height = segmentsDimensions[ activeSegmentIndex ].height;
+
+	const outlineStyle = [ styles.outline, isIOS && styles.outlineIOS ];
 
 	return (
 		<View style={ styles.row }>
@@ -104,9 +156,23 @@ const SegmentedControls = ( {
 							getStylesFromColorScheme={
 								getStylesFromColorScheme
 							}
+							onLayout={ ( event ) =>
+								segmentOnLayout( event, index )
+							}
 						/>
 					);
 				} ) }
+				<Animated.View
+					style={ [
+						{
+							width,
+							left: positionAnimationValue,
+							height,
+						},
+						selectedStyle,
+						outlineStyle,
+					] }
+				/>
 			</View>
 			<View style={ styles.flex }>{ addonRight }</View>
 		</View>
