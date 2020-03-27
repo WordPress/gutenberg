f224e852ef1cd52dc9a9e9898c0afc662735b55e
James Koster <james@jameskoster.co.uk>
2020-03-27 16:57:41 +0000

    Add "receipt" icon

---
+++ packages/icons/src/library/receipt.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const receipt = (
+	<SVG xmlns="http://www.w3.org/2000/SVG" viewBox="0 0 24 24">
+	  <path d="M4.5 3v18L6 19.5 7.5 21 9 19.5l1.5 1.5 1.5-1.5 1.5 1.5 1.5-1.5 1.5 1.5 1.5-1.5 1.5 1.5V3L18 4.5 16.5 3 15 4.5 13.5 3 12 4.5 10.5 3 9 4.5 7.5 3 6 4.5 4.5 3zM18 6v12H6V6h12zM8 8.25v1.5h8v-1.5H8zm8 3H8v1.5h8v-1.5zm0 3v1.5H8v-1.5h8z" />
+	</SVG>
+);
+
+export default receipt;
