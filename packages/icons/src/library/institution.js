4782f3b1208d3ee4adc447e11a28b98749ecdff2
James Koster <james@jameskoster.co.uk>
2020-03-27 16:56:30 +0000

    Add "Institution" icon

---
+++ packages/icons/src/library/institution.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const institution = (
+	<SVG xmlns="http://www.w3.org/2000/SVG" viewBox="0 0 24 24">
+	  <path fill-rule="evenodd" d="M19.383 7.441l.324.162.558.279.223.112c.472.236.304.947-.223.947H4.5c-.528 0-.696-.711-.224-.947l.224-.112.558-.28.324-.161 6.776-3.388a.5.5 0 01.447 0l6.777 3.388zm-7-1.823l3.646 1.823H8.737l3.646-1.823zm.75 4.823v6h-1.5v-6h1.5zm-8.75 7.5h16v1.5h-16v-1.5zm13.75-1.5v-6h-1.5v6h1.5zm-10-6v6h-1.5v-6h1.5z" clip-rule="evenodd"/>
+	</SVG>
+);
+
+export default institution;
