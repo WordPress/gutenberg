6d099645a664aee5b5282fa7d9764dc08683037f
Andrew Duthie <andrew@andrewduthie.com>
2020-03-27 17:42:41 -0400

    Project Management Automation: Prompt user to link GitHub account to profile

--- packages/project-management-automation/lib/add-first-time-contributor-label.js
+++ packages/project-management-automation/lib/add-first-time-contributor-label.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+const got = /** @type {*} */ ( require( 'got' ) ); // See: https://github.com/sindresorhus/got/issues/1137
+
+/**
  * Internal dependencies
  */
 const debug = require( './debug' );
@@ -7,6 +12,30 @@ const getAssociatedPullRequest = require( './get-associated-pull-request' );
 /** @typedef {import('@actions/github').GitHub} GitHub */
 /** @typedef {import('@octokit/webhooks').WebhookPayloadPush} WebhookPayloadPush */
 /** @typedef {import('./get-associated-pull-request').WebhookPayloadPushCommit} WebhookPayloadPushCommit */
+
+/**
+ * Base endpoint URL for WordPress.org profile lookup by GitHub username.
+ *
+ * @type {string}
+ */
+const BASE_PROFILE_LOOKUP_API_URL =
+	'https://profiles.wordpress.org/wp-json/wporg-github/v1/lookup/';
+
+/**
+ * Message of comment prompting contributor to link their GitHub account from
+ * their WordPress.org profile for props credit.
+ *
+ * @type {string}
+ */
+const ACCOUNT_LINK_PROMPT =
+	'Congratulations on your first merged pull request! We would like to ' +
+	'give you credit for your contribution in the next WordPress release, but ' +
+	'we were unable to find a WordPress.org profile associated with your ' +
+	'GitHub account. At your convenience, please visit the following URL and ' +
+	'click "link your GitHub account" under "GitHub Username" to initiate ' +
+	'the process to link your accounts:\n\nhttps://profiles.wordpress.org/me/profile/edit/\n\n' +
+	'If you do not have a WordPress.org account, you can create one at the ' +
+	'following page:\n\nhttps://login.wordpress.org/register';
 
 /**
  * Adds the 'First Time Contributor' label to PRs merged on behalf of
@@ -62,6 +91,40 @@ async function addFirstTimeContributorLabel( payload, octokit ) {
 		repo,
 		issue_number: pullRequest,
 		labels: [ 'First-time Contributor' ],
+	} );
+
+	debug(
+		`add-first-time-contributor-label: Checking for WordPress username associated with @${ author }`
+	);
+
+	let dotOrgUsername;
+	try {
+		const response = await got(
+			BASE_PROFILE_LOOKUP_API_URL + author,
+			/** @type {import('got').Options} */ ( {
+				responseType: 'json',
+			} )
+		);
+		dotOrgUsername = response.body.slug;
+	} catch ( error ) {
+		debug(
+			`add-first-time-contributor-label: Error retrieving from profile API:\n\n${ error.toString() }`
+		);
+		return;
+	}
+
+	if ( dotOrgUsername ) {
+		debug(
+			`add-first-time-contributor-label: User already known as ${ dotOrgUsername }. No need to prompt for account link!`
+		);
+		return;
+	}
+
+	await octokit.issues.createComment( {
+		owner,
+		repo,
+		issue_number: pullRequest,
+		body: ACCOUNT_LINK_PROMPT,
 	} );
 }
 
--- packages/project-management-automation/lib/test/add-first-time-contributor-label.js
+++ packages/project-management-automation/lib/test/add-first-time-contributor-label.js
@@ -1,9 +1,20 @@
 /**
+ * External dependencies
+ */
+import got from 'got';
+
+/**
  * Internal dependencies
  */
 import addFirstTimeContributorLabel from '../add-first-time-contributor-label';
 
+jest.mock( 'got', () => jest.fn() );
+
 describe( 'addFirstTimeContributorLabel', () => {
+	beforeEach( () => {
+		got.mockReset();
+	} );
+
 	const payload = {
 		ref: 'refs/heads/master',
 		commits: [
@@ -104,8 +115,11 @@ describe( 'addFirstTimeContributorLabel', () => {
 			},
 			issues: {
 				addLabels: jest.fn(),
+				createComment: jest.fn(),
 			},
 		};
+
+		got.mockReturnValue( Promise.resolve( { body: { slug: 'ghostwp' } } ) );
 
 		await addFirstTimeContributorLabel( payload, octokit );
 
@@ -117,6 +131,90 @@ describe( 'addFirstTimeContributorLabel', () => {
 			repo: 'gutenberg',
 			issue_number: 123,
 			labels: [ 'First-time Contributor' ],
+		} );
+		expect( octokit.issues.createComment ).not.toHaveBeenCalled();
+	} );
+
+	it( 'aborts if the request to retrieve WordPress.org user profile fails', async () => {
+		const octokit = {
+			search: {
+				commits: jest.fn( () =>
+					Promise.resolve( {
+						data: {
+							total_count: 0,
+						},
+					} )
+				),
+			},
+			issues: {
+				addLabels: jest.fn(),
+				createComment: jest.fn(),
+			},
+		};
+
+		got.mockImplementation( () => {
+			throw new Error( 'Whoops!' );
+		} );
+
+		await addFirstTimeContributorLabel( payload, octokit );
+
+		expect( octokit.search.commits ).toHaveBeenCalledWith( {
+			q: 'repo:WordPress/gutenberg+author:ghost',
+		} );
+		expect( octokit.issues.addLabels ).toHaveBeenCalledWith( {
+			owner: 'WordPress',
+			repo: 'gutenberg',
+			issue_number: 123,
+			labels: [ 'First-time Contributor' ],
+		} );
+		expect( octokit.issues.createComment ).not.toHaveBeenCalled();
+	} );
+
+	it( 'prompts the user to link their GitHub account to their WordPress.org profile', async () => {
+		const octokit = {
+			search: {
+				commits: jest.fn( () =>
+					Promise.resolve( {
+						data: {
+							total_count: 0,
+						},
+					} )
+				),
+			},
+			issues: {
+				addLabels: jest.fn(),
+				createComment: jest.fn(),
+			},
+		};
+
+		got.mockReturnValue(
+			Promise.resolve( {
+				body: {
+					code: 'user_not_linked',
+					message: 'Github Account not linked.',
+					data: {
+						status: 404,
+					},
+				},
+			} )
+		);
+
+		await addFirstTimeContributorLabel( payload, octokit );
+
+		expect( octokit.search.commits ).toHaveBeenCalledWith( {
+			q: 'repo:WordPress/gutenberg+author:ghost',
+		} );
+		expect( octokit.issues.addLabels ).toHaveBeenCalledWith( {
+			owner: 'WordPress',
+			repo: 'gutenberg',
+			issue_number: 123,
+			labels: [ 'First-time Contributor' ],
+		} );
+		expect( octokit.issues.createComment ).toHaveBeenCalledWith( {
+			owner: 'WordPress',
+			repo: 'gutenberg',
+			issue_number: 123,
+			body: expect.stringMatching( /^Congratulations/ ),
 		} );
 	} );
 } );
