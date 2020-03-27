74e0f950be2e6fc6228d3da836e7cc1f477aa7a6
Jon Q <hello@jonquach.com>
2020-03-27 12:37:14 -0400

    Add basic tests for keyboard movements

--- packages/components/src/alignment-matrix-control/test/index.js
+++ packages/components/src/alignment-matrix-control/test/index.js
@@ -5,11 +5,24 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
 /**
+ * WordPress dependencies
+ */
+import { UP, DOWN, LEFT, RIGHT } from '@wordpress/keycodes';
+
+/**
  * Internal dependencies
  */
 import AlignmentMatrixControl from '../';
 
 let container = null;
+
+function triggerKeyDown( el, { keyCode } ) {
+	// modern browsers, IE9+
+	const e = document.createEvent( 'HTMLEvents' );
+	e.keyCode = keyCode;
+	e.initEvent( 'keydown', false, true );
+	el.dispatchEvent( e );
+}
 
 beforeEach( () => {
 	container = document.createElement( 'div' );
@@ -35,6 +48,96 @@ describe( 'AlignmentMatrixControl', () => {
 			const control = getControl();
 
 			expect( control ).toBeTruthy();
+		} );
+	} );
+
+	describe( 'Keyboard movement', () => {
+		it( 'should move the next value up on UP press', () => {
+			const spy = jest.fn();
+
+			act( () => {
+				render(
+					<AlignmentMatrixControl
+						value={ 'center' }
+						onChange={ spy }
+					/>,
+					container
+				);
+			} );
+
+			const control = getControl();
+
+			act( () => {
+				triggerKeyDown( control, { keyCode: UP } );
+			} );
+
+			expect( spy.mock.calls[ 0 ][ 0 ] ).toBe( 'center top' );
+		} );
+
+		it( 'should move the next value down on DOWN press', () => {
+			const spy = jest.fn();
+
+			act( () => {
+				render(
+					<AlignmentMatrixControl
+						value={ 'center' }
+						onChange={ spy }
+					/>,
+					container
+				);
+			} );
+
+			const control = getControl();
+
+			act( () => {
+				triggerKeyDown( control, { keyCode: DOWN } );
+			} );
+
+			expect( spy.mock.calls[ 0 ][ 0 ] ).toBe( 'bottom center' );
+		} );
+
+		it( 'should move the next value left on LEFT press', () => {
+			const spy = jest.fn();
+
+			act( () => {
+				render(
+					<AlignmentMatrixControl
+						value={ 'center' }
+						onChange={ spy }
+					/>,
+					container
+				);
+			} );
+
+			const control = getControl();
+
+			act( () => {
+				triggerKeyDown( control, { keyCode: LEFT } );
+			} );
+
+			expect( spy.mock.calls[ 0 ][ 0 ] ).toBe( 'center left' );
+		} );
+
+		it( 'should move the next value right on RIGHT press', () => {
+			const spy = jest.fn();
+
+			act( () => {
+				render(
+					<AlignmentMatrixControl
+						value={ 'center' }
+						onChange={ spy }
+					/>,
+					container
+				);
+			} );
+
+			const control = getControl();
+
+			act( () => {
+				triggerKeyDown( control, { keyCode: RIGHT } );
+			} );
+
+			expect( spy.mock.calls[ 0 ][ 0 ] ).toBe( 'center right' );
 		} );
 	} );
 } );
