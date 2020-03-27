41c7486a8c0f132e387270f892986b398bc332cb
James Koster <james@jameskoster.co.uk>
2020-03-27 17:00:33 +0000

    Add "personalize" icon

---
+++ packages/icons/src/library/personalize.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const personalize = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	  <path d="M12 5.5a6.5 6.5 0 101.24 12.882A2.501 2.501 0 0114 13.5h3.396c.32 0 .6-.099.783-.233.17-.124.246-.261.266-.412A6.5 6.5 0 0012 5.5zM4 12a8 8 0 1115.932 1.05c-.16 1.212-1.313 1.95-2.536 1.95H14a1 1 0 100 2c1.71 0 2.454 1.864.857 2.475-.887.34-1.85.525-2.857.525a8 8 0 01-8-8zm3-1a1 1 0 112 0 1 1 0 01-2 0zm9-1a1 1 0 100 2 1 1 0 000-2zM9 8a1 1 0 112 0 1 1 0 01-2 0zm5-1a1 1 0 100 2 1 1 0 000-2z" />
+	</SVG>
+);
+
+export default personalize;
