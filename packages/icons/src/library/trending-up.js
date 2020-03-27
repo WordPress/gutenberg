79f10de28e29d97238d6b56e6695a2ea340a1e1d
James Koster <james@jameskoster.co.uk>
2020-03-27 16:58:13 +0000

    Add "trending up" icon

---
+++ packages/icons/src/library/trending-up.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const trendingUp = (
+	<SVG xmlns="http://www.w3.org/2000/SVG" viewBox="0 0 24 24">
+	  <path d="M19.75 14V8.25H14v1.5h3.19l-3.724 3.723-3.975-3.478-5.995 5.45 1.01 1.11 5.004-4.55 4.025 3.521 4.715-4.715V14h1.5z" />
+	</SVG>
+);
+
+export default trendingUp;
