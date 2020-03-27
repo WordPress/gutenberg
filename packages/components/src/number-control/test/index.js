455cb04b8ccf4f4bccee2f0098f35560debfef03
Jon Q <hello@jonquach.com>
2020-03-27 13:31:38 -0400

    Remove unnecessary test comments from number-control test

--- packages/components/src/number-control/test/index.js
+++ packages/components/src/number-control/test/index.js
@@ -22,13 +22,11 @@ function getInput() {
 }
 
 beforeEach( () => {
-	// setup a DOM element as a render target
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
 } );
 
 afterEach( () => {
-	// cleanup on exiting
 	unmountComponentAtNode( container );
 	container.remove();
 	container = null;
