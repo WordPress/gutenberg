e6fe285161d28611dca449c57165eae1ebbe7a28
Pascal Birchler <pascalb@google.com>
2020-03-27 11:21:24 +0100

    Add i18n rules to new i18n config

--- packages/eslint-plugin/configs/custom.js
+++ packages/eslint-plugin/configs/custom.js
@@ -2,15 +2,8 @@ module.exports = {
 	plugins: [ '@wordpress' ],
 	rules: {
 		'@wordpress/no-unused-vars-before-return': 'error',
-		'@wordpress/valid-sprintf': 'error',
 		'@wordpress/no-base-control-with-label-without-id': 'error',
 		'@wordpress/no-unguarded-get-range-at': 'error',
-		'@wordpress/i18n-translator-comments': 'error',
-		'@wordpress/i18n-text-domain': 'error',
-		'@wordpress/i18n-no-collapsible-whitespace': 'error',
-		'@wordpress/i18n-no-placeholders-only': 'error',
-		'@wordpress/i18n-no-variables': 'error',
-		'@wordpress/i18n-ellipsis': 'error',
 	},
 	overrides: [
 		{
