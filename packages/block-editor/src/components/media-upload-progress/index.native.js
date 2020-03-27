ec4656590f014588ad5765faaff99af2deb3cf49
Matthew Kevins <mmkevins@yahoo.com>
2020-03-27 14:02:21 +1000

    Make renderContent prop optional in MediaUploadProgress

--- packages/block-editor/src/components/media-upload-progress/index.native.js
+++ packages/block-editor/src/components/media-upload-progress/index.native.js
@@ -117,7 +117,7 @@ export class MediaUploadProgress extends React.Component {
 	}
 
 	render() {
-		const { coverUrl, width, height } = this.props;
+		const { coverUrl, width, height, renderContent = () => {} } = this.props;
 		const { isUploadInProgress, isUploadFailed } = this.state;
 		const showSpinner = this.state.isUploadInProgress;
 		const progress = this.state.progress * 100;
@@ -155,7 +155,7 @@ export class MediaUploadProgress extends React.Component {
 							) {
 								finalWidth = width;
 							}
-							return this.props.renderContent( {
+							return renderContent( {
 								isUploadInProgress,
 								isUploadFailed,
 								finalWidth,
@@ -167,7 +167,7 @@ export class MediaUploadProgress extends React.Component {
 					</ImageSize>
 				) }
 				{ ! coverUrl &&
-					this.props.renderContent( {
+					renderContent( {
 						isUploadInProgress,
 						isUploadFailed,
 						retryMessage,
