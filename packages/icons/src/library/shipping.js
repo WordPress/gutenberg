ac2e4ddb6bf03b27d35c2b0d27f438a49c52e946
James Koster <james@jameskoster.co.uk>
2020-03-27 17:00:22 +0000

    Add "shipping" icon

---
+++ packages/icons/src/library/shipping.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const shipping = (
+	<SVG xmlns="http://www.w3.org/2000/SVG" viewBox="0 0 24 24">
+	  <path fill-rule="evenodd" d="M5.75 6A1.75 1.75 0 004 7.75v8.75h1.262a2.5 2.5 0 104.975 0h5.025a2.5 2.5 0 104.975 0H21.5V11.44L18.06 8H16.5V6H5.75zm2 8.25c.7 0 1.332.287 1.785.75H15V7.5H5.75a.25.25 0 00-.25.25V15h.465a2.493 2.493 0 011.785-.75zm-1 2.5a1 1 0 112 0 1 1 0 01-2 0zM19.94 12l-2.5-2.5h-.94V12h3.44zm-3.19 4.75a1 1 0 112 0 1 1 0 01-2 0z" clip-rule="evenodd"/>
+	</SVG>
+);
+
+export default shipping;
