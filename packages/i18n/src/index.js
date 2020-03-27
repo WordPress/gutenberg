87bb2f565994e29056b9ecb64a5431f5127f5fbb
Jon Surrell <jon.surrell@automattic.com>
2020-03-27 23:27:45 +0100

    Export * from create-i18n to include types

--- packages/i18n/src/index.js
+++ packages/i18n/src/index.js
@@ -1,3 +1,3 @@
 export { sprintf } from './sprintf';
-export { createI18n } from './create-i18n';
+export * from './create-i18n';
 export { setLocaleData, __, _x, _n, _nx, isRTL } from './default-i18n';
