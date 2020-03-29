9811795c68f8417bc51dbea4296d448630e6da68
KHeo <sainthkh@naver.com>
2020-03-29 16:26:19 +0900

    Add cases to help understand code.

--- packages/docgen/src/get-export-statements.js
+++ packages/docgen/src/get-export-statements.js
@@ -11,12 +11,22 @@ const { hasExportModifier } = require( './has-modifier' );
 /**
  * ExportAssignment
  *   - export default 3
+ *   - export default ClassDeclaration;
+ *   - export default fnDeclaration;
  *
  * ExportDeclaration:
  *   - export { a, b } from 'c'
+ *   - export { myDeclaration };
+ *   - export * from './module';
  *
  * hasExportModifier
  *   - export const x = 42
+ *   - export default class {}
+ *   - export default class ClassDeclaration {}
+ *   - export default function() {}
+ *   - export default function myDeclaration() {}
+ *   - export class MyDeclaration {}
+ *   - export function myDeclaration() {}
  *
  * @param {SourceFile} sourceFile
  */
