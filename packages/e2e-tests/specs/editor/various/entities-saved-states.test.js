18771c67253cecd340aa0260c35fe6230a4a641b
Addison-Stavlo <stavz01@gmail.com>
2020-03-31 15:33:31 -0400

    started published checks

--- packages/e2e-tests/specs/editor/various/entities-saved-states.test.js
+++ packages/e2e-tests/specs/editor/various/entities-saved-states.test.js
@@ -4,12 +4,8 @@
 import {
 	createNewPost,
 	insertBlock,
-	getEditedPostContent,
-	pressKeyTimes,
-	switchEditorModeTo,
-	setBrowserViewport,
-	publishPost,
-	saveDraft,
+	disablePrePublishChecks,
+	publishPostWithPrePublishChecksDisabled,
 } from '@wordpress/e2e-test-utils';
 
 /**
@@ -32,52 +28,81 @@ describe( 'Multi-entity save flow', () => {
 	} );
 
 	describe( 'Post Editor', () => {
-		it( 'Should not trigger multi-entity save button with only post edited', async () => {
-			await createNewPost();
-			// Edit the page some.
-			await page.keyboard.type( 'Test Post...' );
-			await page.keyboard.press( 'Enter' );
+		describe( 'Pre-Publish state', () => {
+			it( 'Should not trigger multi-entity save button with only post edited', async () => {
+				await createNewPost();
+				await disablePrePublishChecks();
+				// Edit the page some.
+				await page.keyboard.type( 'Test Post...' );
+				await page.keyboard.press( 'Enter' );
 
-			// Button should not have has-changes-dot class.
-			const saveButton = await page.$(
-				'.editor-post-publish-button__button.has-changes-dot'
-			);
-			expect( saveButton ).toBeNull();
+				// Button should not have has-changes-dot class.
+				const saveButton = await page.$(
+					'.editor-post-publish-button__button.has-changes-dot'
+				);
+				expect( saveButton ).toBeNull();
+			} );
+
+			it( 'Should trigger multi-entity save button once template part edited', async () => {
+				// Create new template part.
+				await insertBlock( 'Template Part' );
+				await page.keyboard.type( 'test-template-part' );
+				await page.keyboard.press( 'Tab' );
+				await page.keyboard.type( 'test-theme' );
+				await page.keyboard.press( 'Tab' );
+				await page.keyboard.press( 'Enter' );
+
+				// Make some changes in new Template Part.
+				const tempPart = await page.waitForSelector(
+					'*[data-type="core/template-part"] .block-editor-inner-blocks'
+				);
+				await tempPart.focus();
+				await page.keyboard.press( 'Tab' );
+				await page.keyboard.type( 'some words...' );
+
+				// Button should have has-changes-dot class
+				const saveButton = await page.$(
+					'.editor-post-publish-button__button.has-changes-dot'
+				);
+				expect( saveButton ).not.toBeNull();
+			} );
+			// open save modal, => all boxes checked
+			it( 'Clicking should open modal with boxes checked by default', async () => {
+				await page.click( '.editor-post-publish-button__button' );
+				const checkedBoxes = await page.$$(
+					'.components-checkbox-control__checked'
+				);
+				expect( checkedBoxes.length ).toBe( 2 );
+			} );
+			// save modal => draft saved + no dot on publish
+			it( 'Saving should result in items being saved', async () => {
+				await page.click(
+					'.editor-entities-saved-states__save-button'
+				);
+
+				// Verify post is saved.
+				const draftSaved = await page.waitForSelector(
+					'.editor-post-saved-state.is-saved'
+				);
+				expect( draftSaved ).not.toBeNull();
+
+				// Verify template part is saved.
+				const saveButton = await page.$(
+					'.editor-post-publish-button__button.has-changes-dot'
+				);
+				expect( saveButton ).toBeNull();
+			} );
 		} );
-
-		it( 'Should trigger multi-entity save button once template part edited', async () => {
-			// Create new template part.
-			await insertBlock( 'Template Part' );
-			await page.keyboard.type( 'test-template-part' );
-			await page.keyboard.press( 'Tab' );
-			await page.keyboard.type( 'test-theme' );
-			await page.keyboard.press( 'Tab' );
-			await page.keyboard.press( 'Enter' );
-
-			// Make some changes in new Template Part.
-			const tempPart = await page.waitForSelector(
-				'*[data-type="core/template-part"] .block-editor-inner-blocks'
-			);
-			await tempPart.focus();
-			await page.keyboard.press( 'Tab' );
-			await page.keyboard.type( 'some words...' );
-
-			// Button should have has-changes-dot class
-			const saveButton = await page.$(
-				'.editor-post-publish-button__button.has-changes-dot'
-			);
-			expect( saveButton ).not.toBeNull();
-		} );
-		// open save modal, => all boxes checked
-		it( 'Clicking should open modal with boxes checked by default', async () => {
-			await page.click( '.editor-post-publish-button__button' );
-			const checkedBoxes = await page.$$(
-				'.components-checkbox-control__checked'
-			);
-			expect( checkedBoxes.length ).toBe( 2 );
-		} );
-		// save modal => draft saved + no dot on publish
 		// from published state?
+		describe( 'Published state', () => {
+			it( 'Should not trigger multi-entity save button with only post edited', async () => {
+				await publishPostWithPrePublishChecksDisabled();
+				const saveButton = await page.$(
+					'.editor-post-publish-button__button[aria-disabled=true]'
+				);
+				expect( saveButton ).not.toBeNull();
+			} );
+		} );
 		// edit post =>
 	} );
 } );
