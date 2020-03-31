c30ce34f7c396bf60d20f43adcf1f917c8ab772f
Andrés <andres.maneiro@automattic.com>
2020-03-31 13:53:00 +0200

    Render the InspectorControl after GlobalStyles

--- packages/edit-site/src/components/sidebar/index.js
+++ packages/edit-site/src/components/sidebar/index.js
@@ -16,19 +16,19 @@ function Sidebar() {
 			<ComplementaryArea.Slot scope="core/edit-site" />
 			<ComplementaryArea
 				scope="core/edit-site"
-				complementaryAreaIdentifier="edit-site/block-inspector"
-				title={ __( 'Block Inspector' ) }
-				icon={ cog }
-			>
-				<InspectorSlot bubblesVirtually />
-			</ComplementaryArea>
-			<ComplementaryArea
-				scope="core/edit-site"
 				complementaryAreaIdentifier="edit-site/global-styles"
 				title={ __( 'Global Styles' ) }
 				icon={ typography }
 			>
 				<GlobalStylesControls bubblesVirtually />
+			</ComplementaryArea>
+			<ComplementaryArea
+				scope="core/edit-site"
+				complementaryAreaIdentifier="edit-site/block-inspector"
+				title={ __( 'Block Inspector' ) }
+				icon={ cog }
+			>
+				<InspectorSlot bubblesVirtually />
 			</ComplementaryArea>
 		</div>
 	);
