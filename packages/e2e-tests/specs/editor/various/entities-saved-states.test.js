e79de94858e0f0f482257c94735916a67e9ef55c
Addison-Stavlo <stavz01@gmail.com>
2020-03-30 21:42:21 -0400

    inserted new template

--- packages/e2e-tests/specs/editor/various/entities-saved-states.test.js
+++ packages/e2e-tests/specs/editor/various/entities-saved-states.test.js
@@ -29,7 +29,31 @@ describe( 'entities saved states', () => {
 		await disableExperimentalFeatures( experimentsSettings );
 	} );
 
-	it( 'should run in suite', () => {
+	it( 'should run in suite', async () => {
+		await createNewPost();
+
+		// Edit the page some.
+		await page.keyboard.type( 'Test Post...' );
+		await page.keyboard.press( 'Enter' );
+
+		// Create new template part.
+		await insertBlock( 'Template Part' );
+		const [ slug, theme ] = await page.$$(
+			'.wp-block-template-part__placeholder-input'
+		);
+		slug.value = 'test-template';
+		theme.value = 'test-theme';
+		theme.focus();
+		await page.keyboard.press( 'Tab' );
+		await page.keyboard.press( 'Enter' );
+
+		// Add paragraph inside new template part.
+		const tempPart = await page.waitForSelector(
+			'*[data-type="core/template-part"]'
+		);
+		tempPart.focus();
+		await insertBlock( 'Paragraph' );
+		await page.keyboard.type( 'some words' );
 		expect( true ).toBe( true );
 	} );
 } );
