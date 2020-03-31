1a3f50b14b75c55e0938e7c328a8682a56dafe80
Andrés <andres.maneiro@automattic.com>
2020-03-31 19:10:37 +0200

    Do not render GlobalStyles if theme has no support

--- packages/edit-site/src/components/sidebar/index.js
+++ packages/edit-site/src/components/sidebar/index.js
@@ -6,35 +6,55 @@ import { ComplementaryArea } from '@wordpress/interface';
 import { __ } from '@wordpress/i18n';
 import { cog, typography } from '@wordpress/icons';
 import { GlobalStylesControls } from '@wordpress/global-styles';
+import { useSelect } from '@wordpress/data';
 
 const { Slot: InspectorSlot, Fill: InspectorFill } = createSlotFill(
 	'EditSiteSidebarInspector'
 );
 function Sidebar() {
+	const hasGlobalStylesSupport = useSelect( ( select ) => {
+		return Object.keys( select( 'core/block-editor' ).getSettings() ).some(
+			( key ) => key === '__experimentalGlobalStylesUserEntityId'
+		);
+	} );
 	return (
 		<>
 			<ComplementaryArea.Slot
 				scope="core/edit-site"
 				className="edit-site-complementary-area"
 			/>
-			<ComplementaryArea
-				scope="core/edit-site"
-				complementaryAreaIdentifier="edit-site/global-styles"
-				title={ __( 'Global Styles' ) }
-				icon={ typography }
-				className="edit-site-complementary-area"
-			>
-				<GlobalStylesControls bubblesVirtually />
-			</ComplementaryArea>
-			<ComplementaryArea
-				scope="core/edit-site"
-				complementaryAreaIdentifier="edit-site/block-inspector"
-				title={ __( 'Block Inspector' ) }
-				icon={ cog }
-				className="edit-site-complementary-area"
-			>
-				<InspectorSlot bubblesVirtually />
-			</ComplementaryArea>
+			{ hasGlobalStylesSupport ? (
+				<>
+					<ComplementaryArea
+						scope="core/edit-site"
+						complementaryAreaIdentifier="edit-site/global-styles"
+						title={ __( 'Global Styles' ) }
+						icon={ typography }
+						className="edit-site-complementary-area"
+					>
+						<GlobalStylesControls bubblesVirtually />
+					</ComplementaryArea>
+					<ComplementaryArea
+						scope="core/edit-site"
+						complementaryAreaIdentifier="edit-site/block-inspector"
+						title={ __( 'Block Inspector' ) }
+						icon={ cog }
+						className="edit-site-complementary-area"
+					>
+						<InspectorSlot bubblesVirtually />
+					</ComplementaryArea>
+				</>
+			) : (
+				<ComplementaryArea
+					scope="core/edit-site"
+					complementaryAreaIdentifier="edit-site/block-inspector"
+					title={ __( 'Block Inspector' ) }
+					icon={ cog }
+					className="edit-site-complementary-area"
+				>
+					<InspectorSlot bubblesVirtually />
+				</ComplementaryArea>
+			) }
 		</>
 	);
 }
