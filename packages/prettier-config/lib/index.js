a450cecb9943d28f26dc6e5414a541bb1364f196
Andrew Duthie <andrew@andrewduthie.com>
2020-04-01 07:59:44 -0400

    Prettier Config: Disable ESLint validation for union type

--- packages/prettier-config/lib/index.js
+++ packages/prettier-config/lib/index.js
@@ -6,7 +6,12 @@
  * @property {boolean} [parenSpacing=true] Insert spaces inside parentheses.
  */
 
+// Disable reason: The current JSDoc tooling does not yet understand TypeScript
+// union types.
+
+/* eslint-disable jsdoc/valid-types */
 /** @type {PrettierOptions & WPPrettierOptions} */
+/* eslint-enable jsdoc/valid-types */
 const config = {
 	useTabs: true,
 	tabWidth: 4,
