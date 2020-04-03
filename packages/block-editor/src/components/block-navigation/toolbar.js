7f936e9a416deb7e5fce79ad1fc8496ad12bc63e
Daniel Richards <daniel.richards@automattic.com>
2020-04-03 15:02:07 +0800

    Try a toolbar for the block navigator

---
+++ packages/block-editor/src/components/block-navigation/toolbar.js
@@ -0,0 +1,49 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { Button, ToolbarGroup } from '@wordpress/components';
+import { insertAfter, trash } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import BlockActions from '../block-actions';
+import BlockMover from '../block-mover';
+
+export default function BlockNavigationToolbar() {
+	const blockClientIds = useSelect( ( select ) =>
+		select( 'core/block-editor' ).getSelectedBlockClientIds()
+	);
+
+	return (
+		<BlockActions clientIds={ blockClientIds }>
+			{ ( { canDuplicate, isLocked, onDuplicate, onRemove } ) => (
+				<div className="block-editor-block-navigation-toolbar">
+					<BlockMover
+						clientIds={ blockClientIds }
+						__experimentalOrientation="vertical"
+						hideDragHandle
+					/>
+					{ ! isLocked && (
+						<ToolbarGroup>
+							{ canDuplicate && (
+								<Button
+									onClick={ onDuplicate }
+									label={ __( 'Duplicate block' ) }
+									icon={ insertAfter }
+								/>
+							) }
+							<Button
+								onClick={ onRemove }
+								label={ __( 'Remove block' ) }
+								icon={ trash }
+							/>
+						</ToolbarGroup>
+					) }
+				</div>
+			) }
+		</BlockActions>
+	);
+}
--- packages/edit-navigation/src/components/menu-editor/navigation-structure-panel.js
+++ packages/edit-navigation/src/components/menu-editor/navigation-structure-panel.js
@@ -1,11 +1,20 @@
 /**
  * WordPress dependencies
  */
-import { __experimentalBlockNavigationList } from '@wordpress/block-editor';
+import {
+	__experimentalBlockNavigationToolbar as BlockNavigationToolbar,
+	__experimentalBlockNavigationList as BlockNavigationList,
+} from '@wordpress/block-editor';
 import { Panel, PanelBody } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 export default function NavigationStructurePanel( { blocks, initialOpen } ) {
+	const selectedBlockClientIds = useSelect( ( select ) =>
+		select( 'core/block-editor' ).getSelectedBlockClientIds()
+	);
+
+	const { selectBlock } = useDispatch( 'core/block-editor' );
 	return (
 		<Panel className="edit-navigation-menu-editor__navigation-structure-panel">
 			<PanelBody
@@ -13,13 +22,18 @@ export default function NavigationStructurePanel( { blocks, initialOpen } ) {
 				initialOpen={ initialOpen }
 			>
 				{ !! blocks.length && (
-					<__experimentalBlockNavigationList
-						blocks={ blocks }
-						selectedBlockClientId={ blocks[ 0 ].clientId }
-						selectBlock={ () => {} }
-						showNestedBlocks
-						showAppender
-					/>
+					<>
+						<BlockNavigationToolbar />
+						<BlockNavigationList
+							blocks={ blocks }
+							selectedBlockClientId={
+								selectedBlockClientIds[ 0 ]
+							}
+							selectBlock={ selectBlock }
+							showNestedBlocks
+							showAppender
+						/>
+					</>
 				) }
 			</PanelBody>
 		</Panel>
