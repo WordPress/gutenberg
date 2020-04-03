f0beb82bad56dd32ba4a22d5fc2a23486473d2a3
Pascal Birchler <pascalb@google.com>
2020-04-03 13:02:59 +0200

    Update i18n-no-placeholders-only rule

--- packages/eslint-plugin/rules/i18n-no-placeholders-only.js
+++ packages/eslint-plugin/rules/i18n-no-placeholders-only.js
@@ -40,10 +40,9 @@ module.exports = {
 						continue;
 					}
 
-					const modifiedString = argumentString.replace(
-						REGEXP_SPRINTF_PLACEHOLDER,
-						''
-					);
+					const modifiedString = argumentString
+						.replace( /%%/g, 'VALID_ESCAPED_PERCENTAGE_SIGN' )
+						.replace( REGEXP_SPRINTF_PLACEHOLDER, '' );
 
 					if ( modifiedString.length > 0 ) {
 						continue;
