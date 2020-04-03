ebda2a9ce9df58e9e16c8c143b45692c0e7dd2ee
David Ryan <reachdavidryan@gmail.com>
2020-04-02 18:05:03 -0700

    drop “Link to” from social link block

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -15,10 +15,7 @@
 function render_block_core_social_link( $attributes ) {
 	$service = ( isset( $attributes['service'] ) ) ? $attributes['service'] : 'Icon';
 	$url     = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
-	$label   = ( isset( $attributes['label'] ) ) ?
-		$attributes['label'] :
-		/* translators: %s: Social Link service name */
-		sprintf( __( 'Link to %s' ), block_core_social_link_get_name( $service ) );
+	$label   = ( isset( $attributes['label'] ) ) ? $attributes['label'] : block_core_social_link_get_name( $service );
 	$class_name = isset( $attributes['className'] ) ? ' ' . $attributes['className'] : false;
 
 	// Don't render a link if there is no URL set.
