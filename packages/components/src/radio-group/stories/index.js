da54defbc2e58521e17674ff5a12dd1d2221bfd7
Alex Lende <ajlende@gmail.com>
2020-04-04 09:58:12 -0500

    Remove Radio ids from stories

--- packages/components/src/radio-group/stories/index.js
+++ packages/components/src/radio-group/stories/index.js
@@ -13,22 +13,34 @@ export default { title: 'Components/RadioGroup', component: RadioGroup };
 
 export const _default = () => {
 	/* eslint-disable no-restricted-syntax */
-	// id is required for server side rendering
 	return (
 		<RadioGroup
+			// id is required for server side rendering
 			id="default-radiogroup"
 			accessibilityLabel="options"
 			defaultChecked="option2"
 		>
-			<Radio id="option1-radio" value="option1">
-				Option 1
-			</Radio>
-			<Radio id="option2-radio" value="option2">
-				Option 2
-			</Radio>
-			<Radio id="option3-radio" value="option3">
-				Option 3
-			</Radio>
+			<Radio value="option1">Option 1</Radio>
+			<Radio value="option2">Option 2</Radio>
+			<Radio value="option3">Option 3</Radio>
+		</RadioGroup>
+	);
+	/* eslint-enable no-restricted-syntax */
+};
+
+export const disabled = () => {
+	/* eslint-disable no-restricted-syntax */
+	return (
+		<RadioGroup
+			// id is required for server side rendering
+			id="disabled-radiogroup"
+			disabled
+			accessibilityLabel="options"
+			defaultChecked="option2"
+		>
+			<Radio value="option1">Option 1</Radio>
+			<Radio value="option2">Option 2</Radio>
+			<Radio value="option3">Option 3</Radio>
 		</RadioGroup>
 	);
 	/* eslint-enable no-restricted-syntax */
@@ -38,23 +50,17 @@ const ControlledRadioGroupWithState = () => {
 	const [ checked, setChecked ] = useState( 'option2' );
 
 	/* eslint-disable no-restricted-syntax */
-	// id is required for server side rendering
 	return (
 		<RadioGroup
+			// id is required for server side rendering
 			id="controlled-radiogroup"
 			accessibilityLabel="options"
 			checked={ checked }
 			onChange={ setChecked }
 		>
-			<Radio id="option1-radio" value="option1">
-				Option 1
-			</Radio>
-			<Radio id="option2-radio" value="option2">
-				Option 2
-			</Radio>
-			<Radio id="option3-radio" value="option3">
-				Option 3
-			</Radio>
+			<Radio value="option1">Option 1</Radio>
+			<Radio value="option2">Option 2</Radio>
+			<Radio value="option3">Option 3</Radio>
 		</RadioGroup>
 	);
 	/* eslint-enable no-restricted-syntax */
--- packages/components/src/radio/stories/index.js
+++ packages/components/src/radio/stories/index.js
@@ -6,30 +6,14 @@ import Radio from '../';
 
 export default { title: 'Components/Radio', component: Radio };
 
-// Radio components can be in their own component, but must be a descendent of
-// a RadioGroup component at some point.
-const RadioOptions = () => {
-	/* eslint-disable no-restricted-syntax */
-	// id is required for server side rendering
-	return (
-		<>
-			<Radio id="option1-radio" value="option1">
-				Option 1
-			</Radio>
-			<Radio id="option2-radio" value="option2">
-				Option 2
-			</Radio>
-		</>
-	);
-	/* eslint-enable no-restricted-syntax */
-};
-
 export const _default = () => {
+	// Radio components must be a descendent of a RadioGroup component.
 	/* eslint-disable no-restricted-syntax */
-	// id is required for server side rendering
 	return (
+		// id is required for server side rendering
 		<RadioGroup id="default-radiogroup" accessibilityLabel="options">
-			<RadioOptions />
+			<Radio value="option1">Option 1</Radio>
+			<Radio value="option2">Option 2</Radio>
 		</RadioGroup>
 	);
 	/* eslint-enable no-restricted-syntax */
