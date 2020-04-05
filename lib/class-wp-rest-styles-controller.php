5e17adcc6c535c19f03674b4352ef3fd76250660
Jonny Harris <jon@spacedmonkey.co.uk>
2020-04-05 18:54:20 +0100

    Fix lints

--- lib/class-wp-rest-styles-controller.php
+++ lib/class-wp-rest-styles-controller.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Styles controller.
+ *
+ * @package gutenberg
+ */
 
 /**
  * Class WP_REST_Styles_Controller
@@ -16,9 +21,11 @@ class WP_REST_Styles_Controller extends WP_REST_Dependencies_Controller {
 	}
 
 	/**
-	 * @param $src
-	 * @param $ver
-	 * @param $handle
+	 * Helper to get Style URL.
+	 *
+	 * @param string $src Style URL.
+	 * @param string $ver Version URL.
+	 * @param string $handle Handle name.
 	 *
 	 * @return string
 	 */
@@ -27,46 +34,8 @@ class WP_REST_Styles_Controller extends WP_REST_Dependencies_Controller {
 	}
 
 	/**
-	 * @return array
-	 */
-	public function get_core_assets() {
-		$wp_styles = new WP_Styles();
-		wp_default_styles( $wp_styles );
-		$handles = wp_list_pluck( $wp_styles->registered, 'handle' );
-		$handles = array_values( $handles );
-
-		return $handles;
-	}
-}
-< ? php
-
-/**
- * Class WP_REST_Styles_Controller
- */
-class WP_REST_Styles_Controller extends WP_REST_Dependencies_Controller {
-	/**
-	 * WP_REST_Styles_Controller constructor.
-	 */
-	public function __construct() {
-		$this->namespace               = 'wp/v2';
-		$this->rest_base               = 'styles';
-		$this->editor_block_dependency = 'editor_style';
-		$this->block_dependency        = 'style';
-		$this->object                  = wp_styles();
-	}
-
-	/**
-	 * @param $src
-	 * @param $ver
-	 * @param $handle
+	 * Get core assets.
 	 *
-	 * @return string
-	 */
-	public function get_url( $src, $ver, $handle ) {
-		return $this->object->_css_href( $src, $ver, $handle );
-	}
-
-	/**
 	 * @return array
 	 */
 	public function get_core_assets() {
