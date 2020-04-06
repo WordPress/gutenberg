a892a8351fa1513cf6d4317badd7e8ee656a5c15
Andrés <nosolosw@users.noreply.github.com>
2020-04-06 17:44:30 +0200

    Make font size an implicit attribute of the block (#21153)

--- packages/block-editor/src/hooks/style.js
+++ packages/block-editor/src/hooks/style.js
@@ -1,21 +1,35 @@
 /**
  * External dependencies
  */
-import { mapKeys, kebabCase, isObject, entries } from 'lodash';
+import { mapKeys, kebabCase, isObject, entries, identity } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { addFilter } from '@wordpress/hooks';
 import { hasBlockSupport } from '@wordpress/blocks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { COLOR_SUPPORT_KEY } from './color';
-import { LINE_HEIGHT_SUPPRT_KEY } from './line-height';
+import InspectorControls from '../components/inspector-controls';
+import { COLOR_SUPPORT_KEY, ColorEdit } from './color';
+import { LINE_HEIGHT_SUPPORT_KEY, LineHeightEdit } from './line-height';
+import { FONT_SIZE_SUPPORT_KEY, FontSizeEdit } from './font-size';
 
-const styleSupportKeys = [ COLOR_SUPPORT_KEY, LINE_HEIGHT_SUPPRT_KEY ];
+const styleSupportKeys = [
+	COLOR_SUPPORT_KEY,
+	LINE_HEIGHT_SUPPORT_KEY,
+	FONT_SIZE_SUPPORT_KEY,
+];
+
+const typographySupportKeys = [
+	LINE_HEIGHT_SUPPORT_KEY,
+	FONT_SIZE_SUPPORT_KEY,
+];
 
 const hasStyleSupport = ( blockType ) =>
 	styleSupportKeys.some( ( key ) => hasBlockSupport( blockType, key ) );
@@ -30,11 +44,15 @@ const hasStyleSupport = ( blockType ) =>
 export function getCSSVariables( styles = {} ) {
 	const prefix = '--wp';
 	const token = '--';
+	const valueFormatters = {
+		fontSize: ( value ) => ( value ? value + 'px' : value ),
+	};
 	const getNestedCSSVariables = ( config ) => {
 		let result = {};
 		entries( config ).forEach( ( [ key, value ] ) => {
 			if ( ! isObject( value ) ) {
-				result[ kebabCase( key ) ] = value;
+				const formatter = valueFormatters[ key ] || identity;
+				result[ kebabCase( key ) ] = formatter( value );
 				return;
 			}
 
@@ -127,6 +145,36 @@ export function addEditProps( settings ) {
 	return settings;
 }
 
+/**
+ * Override the default edit UI to include new inspector controls for
+ * all the custom styles configs.
+ *
+ * @param  {Function} BlockEdit Original component
+ * @return {Function}           Wrapped component
+ */
+export const withBlockControls = createHigherOrderComponent(
+	( BlockEdit ) => ( props ) => {
+		const { name: blockName } = props;
+		const hasTypographySupport = typographySupportKeys.some( ( key ) =>
+			hasBlockSupport( blockName, key )
+		);
+
+		return [
+			hasTypographySupport && (
+				<InspectorControls key="typography">
+					<PanelBody title={ __( 'Typography' ) }>
+						<FontSizeEdit { ...props } />
+						<LineHeightEdit { ...props } />
+					</PanelBody>
+				</InspectorControls>
+			),
+			<ColorEdit key="colors" { ...props } />,
+			<BlockEdit key="edit" { ...props } />,
+		];
+	},
+	'withToolbarControls'
+);
+
 addFilter(
 	'blocks.registerBlockType',
 	'core/style/addAttribute',
@@ -143,4 +191,10 @@ addFilter(
 	'blocks.registerBlockType',
 	'core/style/addEditProps',
 	addEditProps
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'core/style/with-block-controls',
+	withBlockControls
 );
