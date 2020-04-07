5268d794698df9b499161184c9d9da870dec8bce
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2020-04-07 15:55:35 +0200

    RNMobile - Layout Picker - Hide / Show depending on the keyboard's visibility and the screen size

--- packages/block-editor/src/components/page-template-picker/picker.native.js
+++ packages/block-editor/src/components/page-template-picker/picker.native.js
@@ -8,7 +8,7 @@ import { useDispatch, useSelect } from '@wordpress/data';
  * External dependencies
  */
 import { logUserEvent, userEvents } from 'react-native-gutenberg-bridge';
-import { Animated } from 'react-native';
+import { Animated, Dimensions, Keyboard } from 'react-native';
 
 /**
  * Internal dependencies
@@ -17,6 +17,9 @@ import Button from './button';
 import Container from './container';
 import getDefaultTemplates from './default-templates';
 import Preview from './preview';
+
+// Used to hide the picker if there's no enough space in the window
+const PICKER_HEIGHT_OFFSET = 150;
 
 const __experimentalPageTemplatePicker = ( {
 	templates = getDefaultTemplates(),
@@ -36,8 +39,39 @@ const __experimentalPageTemplatePicker = ( {
 	const contentOpacity = useRef( new Animated.Value( 0 ) );
 
 	useEffect( () => {
+		if ( shouldShowPicker() && visible && ! pickerVisible ) {
+			setPickerVisible( true );
+		}
 		onPickerAnimation();
+
+		Keyboard.addListener( 'keyboardDidShow', onKeyboardDidShow );
+		Keyboard.addListener( 'keyboardDidHide', onKeyboardDidHide );
+
+		return () => {
+			Keyboard.removeListener( 'keyboardDidShow', onKeyboardDidShow );
+			Keyboard.removeListener( 'keyboardDidHide', onKeyboardDidHide );
+		};
 	}, [ visible ] );
+
+	const onKeyboardDidShow = () => {
+		onKeyboardChange();
+	};
+
+	const onKeyboardDidHide = () => {
+		onKeyboardChange();
+	};
+
+	const onKeyboardChange = () => {
+		setPickerVisible( shouldShowPicker() );
+		onPickerAnimation();
+	};
+
+	const shouldShowPicker = () => {
+		// On smaller devices on landscape we hide the picker
+		// so it doesn't overlap with the editor's content
+		const windowHeight = Dimensions.get( 'window' ).height;
+		return PICKER_HEIGHT_OFFSET < windowHeight / 3;
+	};
 
 	const onApply = () => {
 		editPost( {
@@ -51,10 +85,6 @@ const __experimentalPageTemplatePicker = ( {
 	};
 
 	const onPickerAnimation = () => {
-		if ( visible && ! pickerVisible ) {
-			setPickerVisible( true );
-		}
-
 		Animated.timing( contentOpacity.current, {
 			toValue: visible ? 1 : 0,
 			duration: 300,
