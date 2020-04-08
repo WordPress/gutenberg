cdc2e5f5c5bc627b4d53474c2adec640abfe39ac
Bernie Reiter <ockham@raz.or.at>
2020-04-08 16:40:28 +0200

    Icons: Add types

--- packages/icons/src/icon/index.js
+++ packages/icons/src/icon/index.js
@@ -3,6 +3,22 @@
  */
 import { cloneElement } from '@wordpress/element';
 
+// Disable reason: JSDoc linter doesn't seem to parse the union (`&`) correctly.
+/* eslint-disable jsdoc/valid-types */
+/** @typedef {import('@wordpress/primitives').ComponentPropsWithoutRef<'SVG'>} SVGProps */
+/** @typedef {{icon: string} & {size?: number} & import('@wordpress/primitives').ComponentPropsWithoutRef<'SVG'>} IconProps */
+/* eslint-enable jsdoc/valid-types */
+
+/**
+ * Return an SVG icon.
+ *
+ * @param {IconProps} props        Icon component props
+ * @param {string}    props.icon   Icon name
+ * @param {number}    [props.size] Icon size in pixels
+ * @param {SVGProps}  props.props  Other props will be passed to wrapped SVG component
+ *
+ * @return {JSX.Element}  Icon component
+ */
 function Icon( { icon, size = 24, ...props } ) {
 	return cloneElement( icon, {
 		width: size,
