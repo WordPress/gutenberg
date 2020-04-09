faf3bf92161b2e4fb4d0864d6f2c01b71560b909
Addison-Stavlo <stavz01@gmail.com>
2020-04-09 14:20:13 -0400

    added tests for reducer

--- packages/editor/src/store/test/reducer.js
+++ packages/editor/src/store/test/reducer.js
@@ -16,6 +16,7 @@ import {
 	reusableBlocks,
 	postSavingLock,
 	postAutosavingLock,
+	entitiesSavedStates,
 } from '../reducer';
 
 describe( 'state', () => {
@@ -549,6 +550,54 @@ describe( 'state', () => {
 			} );
 
 			expect( state ).toEqual( {} );
+		} );
+	} );
+
+	describe( 'entitiesSavedStates', () => {
+		it( 'returns expected initialized state by default', () => {
+			const state = entitiesSavedStates( undefined, {} );
+
+			expect( state.isOpen ).toBe( false );
+			expect( typeof state.onRequestClose ).toEqual( 'function' );
+		} );
+
+		it( 'returns expected updated state on OPEN action', () => {
+			const onRequestClose = jest.fn();
+			const state = entitiesSavedStates( undefined, {
+				type: 'OPEN_ENTITIES_SAVED_STATES',
+				onRequestClose,
+			} );
+
+			expect( state ).toEqual( {
+				isOpen: true,
+				onRequestClose,
+			} );
+		} );
+
+		it( 'returns expected updated state on CLOSE action', () => {
+			const onRequestClose = jest.fn();
+			const startingState = { isOpen: true, onRequestClose };
+			const action = {
+				type: 'CLOSE_ENTITIES_SAVED_STATES',
+			};
+			const state = entitiesSavedStates( startingState, action );
+
+			expect( state.isOpen ).toBe( false );
+			expect( typeof state.onRequestClose ).toEqual( 'function' );
+			expect( state.onRequestClose ).not.toEqual( onRequestClose );
+		} );
+
+		it( 'fires onRequestClose with callbackArg on CLOSE action', () => {
+			const onRequestClose = jest.fn();
+			const startingState = { isOpen: true, onRequestClose };
+			const action = {
+				type: 'CLOSE_ENTITIES_SAVED_STATES',
+				callbackArg: 'testArg',
+			};
+			entitiesSavedStates( startingState, action );
+
+			expect( onRequestClose ).toHaveBeenCalledTimes( 1 );
+			expect( onRequestClose ).toHaveBeenCalledWith( 'testArg' );
 		} );
 	} );
 } );
