455aafcc8440b71ff91b3606acf991e83f09af46
Addison-Stavlo <stavz01@gmail.com>
2020-04-09 13:51:18 -0400

    added test for actions

--- packages/editor/src/store/test/actions.js
+++ packages/editor/src/store/test/actions.js
@@ -661,4 +661,24 @@ describe( 'Editor actions', () => {
 			} );
 		} );
 	} );
+
+	describe( 'openEntitiesSavedStates', () => {
+		it( 'should return OPEN_ENTITIES_SAVED_STATES action', () => {
+			const result = actions.openEntitiesSavedStates( 'test' );
+			expect( result ).toEqual( {
+				type: 'OPEN_ENTITIES_SAVED_STATES',
+				onRequestClose: 'test',
+			} );
+		} );
+	} );
+
+	describe( 'closeEntitiesSavedStates', () => {
+		it( 'should return CLOSE_ENTITIES_SAVED_STATES action', () => {
+			const result = actions.closeEntitiesSavedStates( 'test' );
+			expect( result ).toEqual( {
+				type: 'CLOSE_ENTITIES_SAVED_STATES',
+				callbackArg: 'test',
+			} );
+		} );
+	} );
 } );
