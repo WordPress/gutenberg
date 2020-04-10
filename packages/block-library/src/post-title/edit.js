ccb86f5515c3cb046e13470b3eb899e958d10f0e
Andrew Duthie <andrew@andrewduthie.com>
2020-04-10 08:08:35 -0400

    Block Library: Post Title: Include dependencies for useSelect hook

--- packages/block-library/src/post-title/edit.js
+++ packages/block-library/src/post-title/edit.js
@@ -5,8 +5,14 @@ import { useSelect } from '@wordpress/data';
 
 export default function PostTitleEdit( { context } ) {
 	const { postType, postId } = context;
-	const post = useSelect( ( select ) =>
-		select( 'core' ).getEditedEntityRecord( 'postType', postType, postId )
+	const post = useSelect(
+		( select ) =>
+			select( 'core' ).getEditedEntityRecord(
+				'postType',
+				postType,
+				postId
+			),
+		[ 'postType', 'postId' ]
 	);
 
 	if ( ! post ) {
