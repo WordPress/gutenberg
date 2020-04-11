97b907919f6d5e317cead0321722b27d1e07e2b2
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-04-11 06:30:52 +0600

    Add the 'is-direction-vertical' class only when there's a vertical direction defined

--- packages/block-library/src/buttons/save.js
+++ packages/block-library/src/buttons/save.js
@@ -6,7 +6,11 @@ import { InnerBlocks } from '@wordpress/block-editor';
 export default function save( { attributes } ) {
 	const { blockDirection } = attributes;
 	return (
-		<div className={ `is-direction-${ blockDirection }` }>
+		<div
+			className={
+				blockDirection === 'vertical' ? 'is-direction-vertical' : ''
+			}
+		>
 			<InnerBlocks.Content />
 		</div>
 	);
