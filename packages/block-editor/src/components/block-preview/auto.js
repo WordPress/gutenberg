e397ec6ccd46c8370df649b1a3cd470479fee3e2
Riad Benguella <benguella@gmail.com>
2020-04-14 13:22:33 +0100

    Improve the performance of the BlockPreview component (#21574)

--- packages/block-editor/src/components/block-preview/auto.js
+++ packages/block-editor/src/components/block-preview/auto.js
@@ -2,12 +2,15 @@
  * WordPress dependencies
  */
 import { Disabled } from '@wordpress/components';
-import { useResizeObserver } from '@wordpress/compose';
+import { useResizeObserver, pure } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
 import BlockList from '../block-list';
+
+// This is used to avoid rendering the block list if the sizes change.
+const MemoizedBlockList = pure( BlockList );
 
 function AutoBlockPreview( { viewportWidth, __experimentalPadding } ) {
 	const [
@@ -43,7 +46,7 @@ function AutoBlockPreview( { viewportWidth, __experimentalPadding } ) {
 				className="block-editor-block-preview__content"
 			>
 				{ containtResizeListener }
-				<BlockList />
+				<MemoizedBlockList />
 			</Disabled>
 		</div>
 	);
