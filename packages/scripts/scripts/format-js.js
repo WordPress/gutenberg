bf1cfb78788b665cc38809ad3e38c7ee846fe2c4
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2020-04-15 17:21:55 +0200

    ESLint Plugin: Add the recommended Prettier config to enforce WP coding styles (#21602)
    * ESLint Config: Add the recommended Prettier config to enforce WordPress coding styles
    
    * Update CHANGELOG.md

--- packages/scripts/scripts/format-js.js
+++ packages/scripts/scripts/format-js.js
@@ -72,9 +72,11 @@ if ( ! checkResult.success ) {
 // needed for config, otherwise pass in args to default config in packages
 // See: https://prettier.io/docs/en/configuration.html
 let configArgs = [];
-// TODO: once setup, use @wordpress/prettier-config
 if ( ! hasPrettierConfig() ) {
-	configArgs = [ '--config', fromConfigRoot( '.prettierrc.js' ) ];
+	configArgs = [
+		'--config',
+		require.resolve( '@wordpress/prettier-config' ),
+	];
 }
 
 // If `--ignore-path` is not explicitly specified, use the project's or global .eslintignore
