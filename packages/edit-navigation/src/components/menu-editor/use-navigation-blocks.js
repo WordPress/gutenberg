602c2ae85688fae3795efcfdda5054119796f8e0
tellthemachines <isabel@tellthemachines.com>
2020-04-17 17:24:58 +1000

    Submenu nesting and saving new nested items.

--- packages/edit-navigation/src/components/menu-editor/use-navigation-blocks.js
+++ packages/edit-navigation/src/components/menu-editor/use-navigation-blocks.js
@@ -10,11 +10,15 @@ import { createBlock } from '@wordpress/blocks';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useState, useRef, useEffect } from '@wordpress/element';
 
-function createBlockFromMenuItem( menuItem ) {
-	return createBlock( 'core/navigation-link', {
-		label: menuItem.title.raw,
-		url: menuItem.url,
-	} );
+function createBlockFromMenuItem( menuItem, innerBlocks = [] ) {
+	return createBlock(
+		'core/navigation-link',
+		{
+			label: menuItem.title.rendered,
+			url: menuItem.url,
+		},
+		innerBlocks
+	);
 }
 
 function createMenuItemAttributesFromBlock( block ) {
@@ -26,7 +30,8 @@ function createMenuItemAttributesFromBlock( block ) {
 
 export default function useNavigationBlocks( menuId ) {
 	const menuItems = useSelect(
-		( select ) => select( 'core' ).getMenuItems( { menus: menuId } ),
+		( select ) =>
+			select( 'core' ).getMenuItems( { menus: menuId, per_page: -1 } ),
 		[ menuId ]
 	);
 
@@ -41,46 +46,76 @@ export default function useNavigationBlocks( menuId ) {
 			return;
 		}
 
+		const itemIndex = {};
+		menuItems.forEach( ( item ) => {
+			if ( ! itemIndex[ item.parent ] ) {
+				itemIndex[ item.parent ] = [];
+			}
+			itemIndex[ item.parent ].push( item );
+		} );
+
 		menuItemsRef.current = {};
 
-		const innerBlocks = [];
+		const createMenuItemBlocks = ( items ) => {
+			const innerBlocks = [];
+			for ( const item of items ) {
+				let menuItemInnerBlocks = [];
+				if ( itemIndex[ item.id ] && itemIndex[ item.id ].length > 0 ) {
+					menuItemInnerBlocks = createMenuItemBlocks(
+						itemIndex[ item.id ]
+					);
+				}
+				const block = createBlockFromMenuItem(
+					item,
+					menuItemInnerBlocks
+				);
+				menuItemsRef.current[ block.clientId ] = item;
+				innerBlocks.push( block );
+			}
+			return innerBlocks;
+		};
 
-		for ( const menuItem of menuItems ) {
-			const block = createBlockFromMenuItem( menuItem );
-			menuItemsRef.current[ block.clientId ] = menuItem;
-			innerBlocks.push( block );
-		}
-
+		const innerBlocks = createMenuItemBlocks( itemIndex[ 0 ] );
 		setBlocks( [ createBlock( 'core/navigation', {}, innerBlocks ) ] );
 	}, [ menuItems ] );
 
 	const saveBlocks = () => {
-		const { innerBlocks } = blocks[ 0 ];
+		const { clientId, innerBlocks } = blocks[ 0 ];
 
-		for ( const block of innerBlocks ) {
-			const menuItem = menuItemsRef.current[ block.clientId ];
+		const saveNestedBlocks = ( nestedBlocks, parentId ) => {
+			for ( const block of nestedBlocks ) {
+				if ( block.innerBlocks && block.innerBlocks.length > 0 ) {
+					saveNestedBlocks( block.innerBlocks, block.clientId );
+				}
+				const menuItem = menuItemsRef.current[ block.clientId ];
+				const parentItemId =
+					menuItemsRef.current[ parentId ] &&
+					menuItemsRef.current[ parentId ].id;
+				if ( ! menuItem ) {
+					saveMenuItem( {
+						...createMenuItemAttributesFromBlock( block ),
+						menus: menuId,
+						parent: parentItemId || 0,
+					} );
+					continue;
+				}
 
-			if ( ! menuItem ) {
-				saveMenuItem( {
-					...createMenuItemAttributesFromBlock( block ),
-					menus: menuId,
-				} );
-				continue;
+				if (
+					! isEqual(
+						block.attributes,
+						createBlockFromMenuItem( menuItem ).attributes
+					)
+				) {
+					saveMenuItem( {
+						...menuItem,
+						...createMenuItemAttributesFromBlock( block ),
+						menus: menuId, // Gotta do this because REST API doesn't like receiving an array here. Maybe a bug in the REST API?
+					} );
+				}
 			}
+		};
 
-			if (
-				! isEqual(
-					block.attributes,
-					createBlockFromMenuItem( menuItem ).attributes
-				)
-			) {
-				saveMenuItem( {
-					...menuItem,
-					...createMenuItemAttributesFromBlock( block ),
-					menus: menuId, // Gotta do this because REST API doesn't like receiving an array here. Maybe a bug in the REST API?
-				} );
-			}
-		}
+		saveNestedBlocks( innerBlocks, clientId );
 
 		const deletedClientIds = difference(
 			Object.keys( menuItemsRef.current ),
@@ -89,7 +124,7 @@ export default function useNavigationBlocks( menuId ) {
 
 		// Disable reason, this code will eventually be implemented.
 		// eslint-disable-next-line no-unused-vars
-		for ( const clientId of deletedClientIds ) {
+		for ( const deletedClientId of deletedClientIds ) {
 			// TODO - delete menu items.
 		}
 	};
