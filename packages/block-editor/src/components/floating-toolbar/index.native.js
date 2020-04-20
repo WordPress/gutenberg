c3a6a2c401f698295b9518912e5b06be6b6ba759
Jakub Binda <jakub.binda@gmail.com>
2020-04-20 12:17:22 +0200

    add animation to FLoatingToolbar

--- packages/block-editor/src/components/floating-toolbar/index.native.js
+++ packages/block-editor/src/components/floating-toolbar/index.native.js
@@ -1,12 +1,13 @@
 /**
  * External dependencies
  */
-import { View, TouchableWithoutFeedback } from 'react-native';
+import { Animated, Easing, View, TouchableWithoutFeedback } from 'react-native';
 
 /**
  * WordPress dependencies
  */
 import { ToolbarButton, Toolbar } from '@wordpress/components';
+import { useEffect } from '@wordpress/element';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
@@ -18,30 +19,62 @@ import styles from './styles.scss';
 import NavigateUpSVG from './nav-up-icon';
 import Breadcrumb from '../block-list/breadcrumb.native';
 
+const EASE_IN_DURATION = 300;
+const EASE_OUT_DURATION = 50;
+const TRANSLATION_RANGE = 8;
+
+const opacity = new Animated.Value( 0 );
+
 const FloatingToolbar = ( {
 	selectedClientId,
 	parentId,
 	showFloatingToolbar,
 	onNavigateUp,
 	isRTL,
-} ) =>
-	!! showFloatingToolbar && (
-		<TouchableWithoutFeedback accessible={ false }>
-			<View style={ styles.floatingToolbar }>
-				{ !! parentId && (
-					<Toolbar passedStyle={ styles.toolbar }>
-						<ToolbarButton
-							title={ __( 'Navigate Up' ) }
-							onClick={ () => onNavigateUp( parentId ) }
-							icon={ <NavigateUpSVG isRTL={ isRTL } /> }
-						/>
-						<View style={ styles.pipe } />
-					</Toolbar>
-				) }
-				<Breadcrumb clientId={ selectedClientId } />
-			</View>
-		</TouchableWithoutFeedback>
+} ) => {
+	useEffect( () => {
+		const easing = Easing.ease;
+		Animated.timing( opacity, {
+			toValue: showFloatingToolbar ? 1 : 0,
+			duration: showFloatingToolbar
+				? EASE_IN_DURATION
+				: EASE_OUT_DURATION,
+			easing,
+		} ).start();
+	}, [ showFloatingToolbar ] );
+
+	const translation = opacity.interpolate( {
+		inputRange: [ 0, 1 ],
+		outputRange: [ TRANSLATION_RANGE, 0 ],
+	} );
+
+	const animationStyle = {
+		opacity,
+		transform: [ { translateY: translation } ],
+	};
+
+	return (
+		!! opacity && (
+			<TouchableWithoutFeedback accessible={ false }>
+				<Animated.View
+					style={ [ styles.floatingToolbar, animationStyle ] }
+				>
+					{ !! parentId && (
+						<Toolbar passedStyle={ styles.toolbar }>
+							<ToolbarButton
+								title={ __( 'Navigate Up' ) }
+								onClick={ () => onNavigateUp( parentId ) }
+								icon={ <NavigateUpSVG isRTL={ isRTL } /> }
+							/>
+							<View style={ styles.pipe } />
+						</Toolbar>
+					) }
+					<Breadcrumb clientId={ selectedClientId } />
+				</Animated.View>
+			</TouchableWithoutFeedback>
+		)
 	);
+};
 
 export default compose( [
 	withSelect( ( select ) => {
