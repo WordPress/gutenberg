bed00c8b541572287f5570691b300d7d48132c75
Dratwas <drapich.piotr@gmail.com>
2020-04-21 13:41:35 +0200

    revert panel body

--- packages/components/src/panel/body.native.js
+++ packages/components/src/panel/body.native.js
@@ -6,7 +6,7 @@ import { Text, View } from 'react-native';
 /**
  * WordPress dependencies
  */
-import { Component, Children, cloneElement } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 /**
  * Internal dependencies
  */
@@ -19,31 +19,14 @@ export class PanelBody extends Component {
 	}
 
 	render() {
-		const {
-			children,
-			title,
-			separatorType,
-			lastSeparatorType = 'none',
-			style = {},
-		} = this.props;
-
-		const childrenArray = Children.toArray( children );
+		const { children, title, style = {} } = this.props;
 
 		return (
 			<View style={ [ styles.panelContainer, style ] }>
 				{ title && (
 					<Text style={ styles.sectionHeaderText }>{ title }</Text>
 				) }
-				{ /* Set correct separator for each child. */ }
-				{ Children.map( childrenArray, ( child, index ) => {
-					return cloneElement( child, {
-						separatorType:
-							child.props.separatorType ||
-							( index !== childrenArray.length - 1
-								? separatorType
-								: lastSeparatorType ),
-					} );
-				} ) }
+				{ children }
 			</View>
 		);
 	}
