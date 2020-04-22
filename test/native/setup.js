967278f4e6e4161009cb2ec658aaf9f04a52a80b
eToledo <etoledom@icloud.com>
2020-04-22 19:34:53 +0200

    Fix native unit tests

--- test/native/setup.js
+++ test/native/setup.js
@@ -21,6 +21,7 @@ jest.mock( 'react-native-gutenberg-bridge', () => {
 		getOtherMediaOptions: jest.fn(),
 		requestMediaPicker: jest.fn(),
 		requestUnsupportedBlockFallback: jest.fn(),
+		subscribeReplaceBlock: jest.fn(),
 		mediaSources: {
 			deviceLibrary: 'DEVICE_MEDIA_LIBRARY',
 			deviceCamera: 'DEVICE_CAMERA',
