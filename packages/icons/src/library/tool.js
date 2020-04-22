4db3d388a590fc6cf44983437bdb85ee4947c79c
James Koster <james@jameskoster.co.uk>
2020-04-22 16:13:02 +0100

    Add "tool" icon

---
+++ packages/icons/src/library/tool.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const tool = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M12.955 5.532a4.001 4.001 0 014.39-.855l-2.268 2.27a.5.5 0 000 .707l1.414 1.414a.5.5 0 00.707 0l2.27-2.27a4.001 4.001 0 01-5.247 5.246l-7.377 7.377A1.5 1.5 0 014.723 17.3L12.1 9.923a4.001 4.001 0 01.855-4.39z" />
+	</SVG>
+);
+
+export default tool;
