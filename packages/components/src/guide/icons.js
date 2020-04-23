1cc6e95aea98548277aa2eaf8bb256071fc5c37e
Enrique Sánchez <enrique.sanchez.garcia@gmail.com>
2020-04-23 17:57:14 -0500

    Reduce size of page control dots

--- packages/components/src/guide/icons.js
+++ packages/components/src/guide/icons.js
@@ -18,11 +18,11 @@ export const ForwardButtonIcon = () => (
 );
 
 export const PageControlIcon = ( { isSelected } ) => (
-	<SVG width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<SVG width="8" height="8" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<Circle
-			cx="6"
-			cy="6"
-			r="6"
+			cx="4"
+			cy="4"
+			r="4"
 			fill={ isSelected ? '#419ECD' : '#E1E3E6' }
 		/>
 	</SVG>
