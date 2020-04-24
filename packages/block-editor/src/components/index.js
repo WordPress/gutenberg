f46c7fcd0ef955b2213f4b2ccb679abc36085503
Greg Ziółkowski <grzegorz@gziolo.pl>
2020-04-24 12:42:06 +0200

    Block Editor: Add useEditorFeature hook to simplify access to editor features (#21646)
    * Block Editor: Add useEditorFeature hook to work with typography, colors and other features
    
    * Avoid using get_theme_support for drop cap

--- packages/block-editor/src/components/index.js
+++ packages/block-editor/src/components/index.js
@@ -91,3 +91,4 @@ export { default as WritingFlow } from './writing-flow';
 
 export { default as BlockEditorProvider } from './provider';
 export { default as useSimulatedMediaQuery } from './use-simulated-media-query';
+export { default as __experimentalUseEditorFeature } from './use-editor-feature';
