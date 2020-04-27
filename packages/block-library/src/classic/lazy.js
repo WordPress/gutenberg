7d370f15f0f60e39c284e60bf1bef2d94cb63f86
Sara Marcondes <saram@fastmail.com>
2020-04-27 09:01:53 -0700

    Guarantee script execution order
    See inline comment for explanation for why this is necessary.
    
    Also refactors the dependency loading code into a single function.
    All of the logic is shared except for the actual DOM element creation.
    While the component was pretty simple to understand on one read
    through when there was no need for blocking, it didn't make sense to
    repeat that complexity and the comment for it.
    
    This also lets the component hide the complexity of loadScripts and
    loadStyles from the exported function, as well as the already-loaded-
    dependency tracking inside those closures.

--- packages/block-library/src/classic/lazy.js
+++ packages/block-library/src/classic/lazy.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
 /**
@@ -9,102 +9,104 @@ import apiFetch from '@wordpress/api-fetch';
  */
 import { noop } from 'lodash';
 
-const getPath = ( fileType, dependencies ) =>
-	`/wp/v2/${ fileType }/?${ dependencies.map(
+const getPath = ( dependencyType, dependencies ) =>
+	`/wp/v2/${ dependencyType }/?${ dependencies.map(
 		( dependency ) => `dependency=${ dependency }`
 	) }`;
 
-export default class LazyLoad extends Component {
-	constructor( props ) {
-		super( props );
+/**
+ *
+ * @param {'scripts' | 'styles'} dependencyType The type of dependency
+ * @param {Function} createElement A function accepting a URI for a dependency and returning an element to append to head
+ *
+ * @return {Function} A file loader function acepting a list of dependencies and returning a Promise which resolves when all the files have been loaded
+ */
+const createDependencyLoader = ( dependencyType, createElement ) => {
+	const previouslyLoadedDependencies = new Set();
 
-		this.state = {
-			loaded: false,
-		};
-
-		this.loadScripts = this.loadScripts.bind( this );
-		this.loadStyles = this.loadStyles.bind( this );
-	}
-
-	async loadScripts() {
-		const scriptsToLoad = this.props.scripts.filter(
-			( script ) => ! LazyLoad.loadedScripts.has( script )
+	return async ( dependencies ) => {
+		const dependenciesToLoad = dependencies.filter(
+			( handle ) => ! previouslyLoadedDependencies.has( handle )
 		);
 
-		if ( scriptsToLoad.length === 0 ) {
-			return Promise.resolve();
+		if ( dependenciesToLoad.length === 0 ) {
+			return;
 		}
 
-		const path = getPath( 'scripts', scriptsToLoad );
+		const path = getPath( dependencyType, dependenciesToLoad );
 
-		const scriptsWithDeps = await apiFetch( { path } );
+		const orderedDependenciesWithDependencies = await apiFetch( { path } );
 
-		await Promise.all(
-			scriptsWithDeps.map( ( { src: scriptSrc } ) => {
-				return new Promise( ( resolve, reject ) => {
-					const scriptElement = document.createElement( 'script' );
-					scriptElement.src = scriptSrc;
-					scriptElement.defer = true;
-					scriptElement.onload = resolve;
-					scriptElement.onerror = reject;
-					document.body.appendChild( scriptElement );
-				} );
-			} )
-		);
+		/**
+		 * Programatically added `script` tags get parsed and executed as though they
+		 * had `async` on them. That means that if we have script A and B, where B
+		 * depends on A and we add both to the DOM at the same time, there's no
+		 * guaranteed order of execution, B could execute before A and break.
+		 *
+		 * Therefore, we need to block on each script's loading until the next one down
+		 * the list.
+		 *
+		 * The only problem with this is if you have two final dependencies you're trying
+		 * to load and they have completely separate dependency trees, we will _not_ be
+		 * loading those trees in parallel. This isn't currently an issue, so fixing it
+		 * right now would be a premature optimization. It would probably require rethinking
+		 * how the API returns things (i.e., we should probably return things in an
+		 * actual tree format and represent independent trees as duely separate trees if
+		 * we want to be able to load things in parallel). For now, this will have to do.
+		 */
+		for ( const dependency of orderedDependenciesWithDependencies ) {
+			const { src: dependencyUri } = dependency;
 
-		scriptsWithDeps.forEach( ( { handle } ) =>
-			LazyLoad.loadedScripts.add( handle )
-		);
-	}
-
-	async loadStyles() {
-		const stylesToLoad = this.props.styles.filter(
-			( style ) => ! LazyLoad.loadedStyles.has( style )
-		);
-
-		if ( stylesToLoad.length === 0 ) {
-			return Promise.resolve();
+			await new Promise( ( resolve, reject ) => {
+				const element = createElement( dependencyUri );
+				element.onload = resolve;
+				element.onerror = reject;
+				document.head.appendChild( element );
+			} );
 		}
 
-		const path = getPath( 'styles', stylesToLoad );
-
-		const stylesWithDeps = await apiFetch( { path } );
-
-		await Promise.all(
-			stylesWithDeps.map( ( { src: styleHref } ) => {
-				return new Promise( ( resolve, reject ) => {
-					const linkElement = document.createElement( 'link' );
-					linkElement.rel = 'stylesheet';
-					linkElement.href = styleHref;
-					linkElement.onload = resolve;
-					linkElement.onerror = reject;
-					document.head.appendChild( linkElement );
-				} );
-			} )
+		orderedDependenciesWithDependencies.forEach( ( { handle } ) =>
+			previouslyLoadedDependencies.add( handle )
 		);
+	};
+};
 
-		stylesWithDeps.forEach( ( { handle } ) =>
-			LazyLoad.loadedStyles.add( handle )
-		);
+const loadScripts = createDependencyLoader( 'scripts', ( dependencyUri ) => {
+	const scriptElement = document.createElement( 'script' );
+	scriptElement.type = 'application/javascript';
+	scriptElement.src = dependencyUri;
+	return scriptElement;
+} );
+
+const loadStyles = createDependencyLoader( 'styles', ( dependencyUri ) => {
+	const linkElement = document.createElement( 'link' );
+	linkElement.rel = 'stylesheet';
+	linkElement.href = dependencyUri;
+	return linkElement;
+} );
+
+const LazyLoad = ( {
+	scripts,
+	styles,
+	children,
+	placeholder,
+	onLoaded,
+	onError,
+} ) => {
+	const [ loaded, setLoaded ] = useState( false );
+	Promise.all( [ loadScripts( scripts ), loadStyles( styles ) ] )
+		.then( () => onLoaded() )
+		.then( () => {
+			setLoaded( true );
+		} )
+		.catch( onError );
+
+	if ( loaded ) {
+		return children;
 	}
 
-	componentDidMount() {
-		Promise.all( [ this.loadScripts(), this.loadStyles() ] )
-			.then( this.props.onLoaded )
-			.then( () => {
-				this.setState( { loaded: true } );
-			} )
-			.catch( this.props.onError );
-	}
-
-	render() {
-		if ( this.state.loaded ) {
-			return this.props.children;
-		}
-
-		return this.props.placeholder;
-	}
-}
+	return placeholder;
+};
 
 LazyLoad.defaultProps = {
 	scripts: [],
@@ -114,5 +116,4 @@ LazyLoad.defaultProps = {
 	onError: noop,
 };
 
-LazyLoad.loadedScripts = new Set();
-LazyLoad.loadedStyles = new Set();
+export default LazyLoad;
