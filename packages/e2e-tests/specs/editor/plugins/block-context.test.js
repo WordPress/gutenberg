e77a452bbdfba888616b490ae37f804ce2288622
Andrew Duthie <andrew@andrewduthie.com>
2020-04-27 09:44:17 -0400

    E2E Tests: Block Context: Skip preview test

--- packages/e2e-tests/specs/editor/plugins/block-context.test.js
+++ packages/e2e-tests/specs/editor/plugins/block-context.test.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { last } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import {
@@ -6,7 +11,29 @@ import {
 	createNewPost,
 	deactivatePlugin,
 	insertBlock,
+	saveDraft,
 } from '@wordpress/e2e-test-utils';
+
+async function openPreviewPage( editorPage ) {
+	let openTabs = await browser.pages();
+	const expectedTabsCount = openTabs.length + 1;
+	await editorPage.click( '.block-editor-post-preview__button-toggle' );
+	await editorPage.waitFor( '.edit-post-header-preview__button-external' );
+	await editorPage.click( '.edit-post-header-preview__button-external' );
+
+	// Wait for the new tab to open.
+	while ( openTabs.length < expectedTabsCount ) {
+		await editorPage.waitFor( 1 );
+		openTabs = await browser.pages();
+	}
+
+	const previewPage = last( openTabs );
+	// Wait for the preview to load. We can't do interstitial detection here,
+	// because it might load too quickly for us to pick up, so we wait for
+	// the preview to load by waiting for the content to appear.
+	await previewPage.waitForSelector( '.entry-content' );
+	return previewPage;
+}
 
 describe( 'Block context', () => {
 	beforeAll( async () => {
@@ -41,5 +68,46 @@ describe( 'Block context', () => {
 			() => document.activeElement.textContent
 		);
 		expect( innerBlockText ).toBe( 'The record ID is: 123' );
+	} );
+
+	// Disable reason: Block context PHP implementation is temporarily reverted.
+	// This will be unskipped once the implementation is restored. Skipping was
+	// the most direct option for revert given time constraints.
+
+	/* eslint-disable-next-line jest/no-disabled-tests */
+	test.skip( 'Block context is reflected in the preview', async () => {
+		await insertBlock( 'Test Context Provider' );
+		const editorPage = page;
+		const previewPage = await openPreviewPage( editorPage );
+
+		// Check default context values are populated.
+		let content = await previewPage.$eval(
+			'.entry-content',
+			( contentWrapper ) => contentWrapper.textContent.trim()
+		);
+		expect( content ).toBe( 'The record ID is: 0' );
+
+		// Return to editor to change context value to non-default.
+		await editorPage.bringToFront();
+		await editorPage.focus(
+			'[data-type="gutenberg/test-context-provider"] input'
+		);
+		await editorPage.keyboard.press( 'ArrowRight' );
+		await editorPage.keyboard.type( '123' );
+		await editorPage.waitForSelector( '.editor-post-save-draft' ); // Not entirely clear why it's asynchronous, but likely React scheduling prioritizing keyboard event and deferring the UI update.
+		await saveDraft();
+
+		// Check non-default context values are populated.
+		await previewPage.bringToFront();
+		await previewPage.reload();
+		content = await previewPage.$eval(
+			'.entry-content',
+			( contentWrapper ) => contentWrapper.textContent.trim()
+		);
+		expect( content ).toBe( 'The record ID is: 123' );
+
+		// Clean up
+		await editorPage.bringToFront();
+		await previewPage.close();
 	} );
 } );
