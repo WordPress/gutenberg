c168c89288257255e7258ca732250f31319560ed
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-04-27 14:03:12 +0300

    adds a new MenuDelete component
    removes extra getEntities call

---
+++ packages/edit-navigation/src/components/menu-delete/index.js
@@ -0,0 +1,33 @@
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+export default function MenuDelete( { menuId, onDelete } ) {
+	const deleteMenu = async ( recordId ) => {
+		const path = `${ '/__experimental/menus/' + recordId + '?force=true' }`;
+		const deletedRecord = await apiFetch( {
+			path,
+			method: 'DELETE',
+		} );
+		return deletedRecord.previous;
+	};
+
+	const askToDelete = async () => {
+		if (
+			// eslint-disable-next-line no-alert
+			window.confirm( __( 'Are you sure you want to delete this menu?' ) )
+		) {
+			const deletedMenu = await deleteMenu( menuId );
+			onDelete( deletedMenu );
+		}
+	};
+
+	return (
+		<Button isPrimary onClick={ askToDelete }>
+			{ __( 'Delete navigation' ) }
+		</Button>
+	);
+}
--- packages/edit-navigation/src/components/menu-editor/index.js
+++ packages/edit-navigation/src/components/menu-editor/index.js
@@ -1,61 +1,32 @@
 /**
- * External dependencies
- */
-import { find } from 'lodash';
-
-/**
  * WordPress dependencies
  */
-import apiFetch from '@wordpress/api-fetch';
 import {
 	BlockEditorKeyboardShortcuts,
 	BlockEditorProvider,
 } from '@wordpress/block-editor';
 import { useViewportMatch } from '@wordpress/compose';
-import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import useNavigationBlocks from './use-navigation-blocks';
 import MenuEditorShortcuts from './shortcuts';
+<<<<<<< HEAD
 import BlockEditorPanel from './block-editor-panel';
 import NavigationStructurePanel from './navigation-structure-panel';
+=======
+import MenuDelete from '../menu-delete';
+>>>>>>> adds a new MenuDelete component
 
 export default function MenuEditor( {
 	menuId,
 	blockEditorSettings,
 	onDelete,
 } ) {
-	const kind = 'root';
-	const name = 'menu';
 
 	const [ blocks, setBlocks, saveBlocks ] = useNavigationBlocks( menuId );
 	const isLargeViewport = useViewportMatch( 'medium' );
-	const entities = useSelect( ( select ) =>
-		select( 'core' ).getEntitiesByKind( kind )
-	);
-
-	const entity = find( entities, { kind, name } );
-
-	const deleteMenu = async ( recordId ) => {
-		const path = `${ entity.baseURL + '/' + recordId + '?force=true' }`;
-		const deletedRecord = await apiFetch( {
-			path,
-			method: 'DELETE',
-		} );
-		return deletedRecord.previous;
-	};
-
-	const askToDelete = async () => {
-		if (
-			// eslint-disable-next-line no-alert
-			window.confirm( __( 'Are you sure you want to delete this menu?' ) )
-		) {
-			const deletedMenu = await deleteMenu( menuId );
-			onDelete( deletedMenu );
-		}
-	};
 
 	return (
 		<div className="edit-navigation-menu-editor">
