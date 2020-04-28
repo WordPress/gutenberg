a3166c931cdf582d3d2309a04682f77212eaa988
eToledo <etoledom@icloud.com>
2020-04-28 22:42:07 +0200

    Add style to Dismiss button On Unsupported block bottom sheet

--- packages/block-library/src/missing/edit.native.js
+++ packages/block-library/src/missing/edit.native.js
@@ -151,6 +151,7 @@ export class UnsupportedBlockEdit extends Component {
 							label={ __( 'Dismiss' ) }
 							separatorType="topFullWidth"
 							onPress={ this.toggleSheet }
+							labelStyle={ actionButtonStyle }
 						/>
 					</>
 				}
