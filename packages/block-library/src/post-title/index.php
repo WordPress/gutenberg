d5298c0ced81ddd0a05768af91fbaa60c36c8284
Andrew Duthie <andrew@andrewduthie.com>
2020-04-29 16:18:23 -0400

    Block Library: Post Title: Document function attributes

--- packages/block-library/src/post-title/index.php
+++ packages/block-library/src/post-title/index.php
@@ -8,6 +8,10 @@
 /**
  * Renders the `core/post-title` block on the server.
  *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
  * @return string Returns the filtered post title for the current post wrapped inside "h1" tags.
  */
 function render_block_core_post_title( $attributes, $content, $block ) {
