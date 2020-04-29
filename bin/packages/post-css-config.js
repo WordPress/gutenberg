3dfc0e595e305b891fc01eb42d04269713a9a924
Kelly Dwan <ryelle@users.noreply.github.com>
2020-04-29 17:56:29 -0400

    Remove postcss-color-function

--- bin/packages/post-css-config.js
+++ bin/packages/post-css-config.js
@@ -3,5 +3,4 @@ const { adminColorSchemes } = require( '@wordpress/base-styles' );
 module.exports = [
 	require( '@wordpress/postcss-themes' )( adminColorSchemes ),
 	require( 'autoprefixer' )( { grid: true } ),
-	require( 'postcss-color-function' ),
 ];
