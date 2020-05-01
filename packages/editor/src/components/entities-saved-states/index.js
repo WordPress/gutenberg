af9cd4381814277836a5bb718c8528df3f6bb00b
Addison-Stavlo <stavz01@gmail.com>
2020-05-01 14:41:34 -0400

    added text change on disabled button

--- packages/editor/src/components/entities-saved-states/index.js
+++ packages/editor/src/components/entities-saved-states/index.js
@@ -56,12 +56,11 @@ function EntityRecordState( { record, checked, onChange, closePanel } ) {
 		},
 		[ parentBlockId ]
 	);
-
+	const isSelectedText = isSelected ? __( 'Selected' ) : __( 'Select' );
 	const { selectBlock } = useDispatch( 'core/block-editor' );
 	const selectParentBlock = useCallback( () => selectBlock( parentBlockId ), [
 		parentBlockId,
 	] );
-
 	const selectAndDismiss = useCallback( () => {
 		selectBlock( parentBlockId );
 		closePanel();
@@ -81,14 +80,14 @@ function EntityRecordState( { record, checked, onChange, closePanel } ) {
 						className="entities-saved-states__find-entity"
 						disabled={ isSelected }
 					>
-						{ __( 'Select' ) }
+						{ isSelectedText }
 					</Button>
 					<Button
 						onClick={ selectAndDismiss }
 						className="entities-saved-states__find-entity-small"
 						disabled={ isSelected }
 					>
-						{ __( 'Select' ) }
+						{ isSelectedText }
 					</Button>
 				</>
 			) : null }
