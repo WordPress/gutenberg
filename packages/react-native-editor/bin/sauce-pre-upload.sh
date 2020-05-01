73bc4c988c91e64a0f619d54d6750cf587973a06
Tugdual de Kerviler <dekervit@gmail.com>
2020-05-01 13:37:28 +0200

    Try travis config for RN e2e tests

--- packages/react-native-editor/bin/sauce-pre-upload.sh
+++ packages/react-native-editor/bin/sauce-pre-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# check if CIRCLE_BRANCH variable is set
-if [[ -n "$CIRCLE_BRANCH" ]] ; then
+# check if TRAVIS_PULL_REQUEST_BRANCH variable is set
+if [[ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]] ; then
     # replace / with - and assign to new var SAUCE_FILENAME
-    export SAUCE_FILENAME=${CIRCLE_BRANCH//[\/]/-};
+    export SAUCE_FILENAME=${TRAVIS_PULL_REQUEST_BRANCH//[\/]/-};
  else
-    echo "Expected CIRCLE_BRANCH env variable";
+    echo "Expected TRAVIS_PULL_REQUEST_BRANCH env variable";
 fi
