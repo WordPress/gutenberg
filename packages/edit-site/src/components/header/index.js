566c91a563d15bac8e23f16ae66b17478c83d733
Marko Andrijasevic <markoandrijasevic@gmail.com>
2020-05-04 17:55:27 +0200

    Rename to MainDashboardButton

--- packages/edit-site/src/components/header/index.js
+++ packages/edit-site/src/components/header/index.js
@@ -15,7 +15,7 @@ import { PinnedItems } from '@wordpress/interface';
  * Internal dependencies
  */
 import { useEditorContext } from '../editor';
-import __experimentalSiteEditorCloseArea from './close-area';
+import MainDashboardButton from './main-dashboard-button';
 import MoreMenu from './more-menu';
 import TemplateSwitcher from '../template-switcher';
 import SaveButton from '../save-button';
@@ -62,7 +62,7 @@ export default function Header( { openEntitiesSavedStates } ) {
 
 	return (
 		<div className="edit-site-header">
-			<__experimentalSiteEditorCloseArea.Slot />
+			<MainDashboardButton.Slot />
 			<div className="edit-site-header__toolbar">
 				<Inserter
 					position="bottom right"
