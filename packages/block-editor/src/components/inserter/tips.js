4dfe9dee7a4add1a852d3bc64ba02a3ee7db044e
retrofox <rdsuarez@gmail.com>
2020-05-05 10:40:53 -0300

    inserter-menu: add contextual tips

--- packages/block-editor/src/components/inserter/tips.js
+++ packages/block-editor/src/components/inserter/tips.js
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { createInterpolateElement, useState } from '@wordpress/element';
 import { Tip } from '@wordpress/components';
+import {context} from "@actions/github/lib/github";
 
 const globalTips = [
 	createInterpolateElement(
@@ -27,6 +28,53 @@ const globalTips = [
 	__( 'Drag files into the editor to automatically insert media blocks.' ),
 	__( "Change a block's type by pressing the block icon on the toolbar." ),
 ];
+
+const contextualTips = {
+	css: createInterpolateElement(
+		__(
+			'CSS - You can visit the <a>the Customizer</a> to edit the CSS on your site.'
+		),
+		{
+			a: <a href="/customize.php?autofocus[section]=custom_css" target="_blank"/>
+		}
+	),
+
+	theme: createInterpolateElement(
+		__(
+			'theme - You can visit the <a>theme directory</a> to select a different design for your site.'
+		),
+		{
+			a: <a href="/themes.php" target="_blank"/>
+		}
+	),
+
+	plugin: createInterpolateElement(
+		__(
+			'plugin - You can visit the <a>plugin directory</a> to install additional plugins.'
+		),
+		{
+			a: <a href="/plugin-install.php" target="_blank"/>
+		}
+	),
+
+	header: createInterpolateElement(
+		__(
+			'header - You can visit the <a>the Customizer</a> to edit your logo and site title.'
+		),
+		{
+			a: <a href="/customize.php?autofocus[section]=title_tagline" target="_blank"/>
+		}
+	),
+
+	colors: createInterpolateElement(
+		__(
+			'colors - You can visit the <a>the Customizer</a> to edit your logo and site title.'
+		),
+		{
+			a: <a href="/customize.php?autofocus[section]=colors" target="_blank"/>
+		}
+	),
+};
 
 function Tips() {
 	const [ randomIndex ] = useState(
