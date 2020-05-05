d8f1dc557d048b56cb7a12ea7dbc715689d2431b
epiqueras <epiquerass@gmail.com>
2020-05-05 16:14:31 -0700

    Query Block: Add query toolbar with per_page control.

--- packages/block-library/src/query/edit/index.js
+++ packages/block-library/src/query/edit/index.js
@@ -4,21 +4,33 @@
 import { useSelect } from '@wordpress/data';
 import { useMemo, useState } from '@wordpress/element';
 import {
+	BlockControls,
 	BlockContextProvider,
 	InnerBlocks,
 	BlockPreview,
 } from '@wordpress/block-editor';
 
-export default function QueryEdit( { clientId } ) {
+/**
+ * Internal dependencies
+ */
+import QueryToolbar from './query-toolbar';
+
+export default function QueryEdit( {
+	clientId,
+	attributes: { query },
+	setAttributes,
+} ) {
 	const { posts, blocks } = useSelect(
 		( select ) => ( {
 			// TODO: Import UI for customizing query from sibling files.
-			posts: select( 'core' ).getEntityRecords( 'postType', 'post', {
-				per_page: 10,
-			} ),
+			posts: select( 'core' ).getEntityRecords(
+				'postType',
+				'post',
+				query
+			),
 			blocks: select( 'core/block-editor' ).getBlocks( clientId ),
 		} ),
-		[ clientId ]
+		[ query, clientId ]
 	);
 	const blockContexts = useMemo(
 		() =>
@@ -30,28 +42,37 @@ export default function QueryEdit( { clientId } ) {
 	);
 	const [ activeBlockContext, setActiveBlockContext ] = useState();
 
-	if ( ! blockContexts ) {
-		return null;
-	}
-	return blockContexts.map( ( blockContext ) => {
-		return (
-			<BlockContextProvider
-				key={ blockContext.postId }
-				value={ blockContext }
-			>
-				{ blockContext ===
-				( activeBlockContext || blockContexts[ 0 ] ) ? (
-					<InnerBlocks />
-				) : (
-					<BlockPreview
-						blocks={ blocks }
-						__experimentalLive
-						__experimentalOnClick={ () =>
-							setActiveBlockContext( blockContext )
-						}
-					/>
-				) }
-			</BlockContextProvider>
-		);
-	} );
+	return (
+		<>
+			<BlockControls>
+				<QueryToolbar
+					query={ query }
+					setQuery={ ( newQuery ) =>
+						setAttributes( { query: { ...query, ...newQuery } } )
+					}
+				/>
+			</BlockControls>
+			{ blockContexts?.map( ( blockContext ) => {
+				return (
+					<BlockContextProvider
+						key={ blockContext.postId }
+						value={ blockContext }
+					>
+						{ blockContext ===
+						( activeBlockContext || blockContexts[ 0 ] ) ? (
+							<InnerBlocks />
+						) : (
+							<BlockPreview
+								blocks={ blocks }
+								__experimentalLive
+								__experimentalOnClick={ () =>
+									setActiveBlockContext( blockContext )
+								}
+							/>
+						) }
+					</BlockContextProvider>
+				);
+			} ) }
+		</>
+	);
 }
---
+++ packages/block-library/src/query/edit/query-toolbar.js
@@ -0,0 +1,39 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	Toolbar,
+	Dropdown,
+	ToolbarButton,
+	RangeControl,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { list } from '@wordpress/icons';
+
+export default function QueryToolbar( { query, setQuery } ) {
+	return (
+		<Toolbar __experimentalAccessibilityLabel={ __( 'Query Toolbar' ) }>
+			<Dropdown
+				renderToggle={ ( { onToggle } ) => (
+					<ToolbarButton
+						icon={ list }
+						label={ __( 'Number of Posts' ) }
+						onClick={ onToggle }
+					/>
+				) }
+				renderContent={ () => (
+					<RangeControl
+						label={ __( 'Limit the number of posts returned.' ) }
+						min={ 1 }
+						max={ 50 }
+						allowReset
+						value={ query.per_page }
+						onChange={ ( value ) =>
+							setQuery( { per_page: value ?? -1 } )
+						}
+					/>
+				) }
+			/>
+		</Toolbar>
+	);
+}
