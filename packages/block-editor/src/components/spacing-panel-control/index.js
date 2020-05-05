e966e62f57f10bed1653033827c170b116661a3b
Jon Q <hello@jonquach.com>
2020-05-05 12:18:04 -0400

    TEMP: Disable guard for testing

--- packages/block-editor/src/components/spacing-panel-control/index.js
+++ packages/block-editor/src/components/spacing-panel-control/index.js
@@ -14,9 +14,10 @@ export default function SpacingPanelControl( { children, ...props } ) {
 	const isSpacingEnabled = useEditorFeature(
 		'__experimentalEnableCustomSpacing'
 	);
-	// TODO: Renable this guard
+	// TODO: Renable the proper guard
 	// This is temporarily disabled for testing purposes.
-	if ( ! isSpacingEnabled ) return null;
+	if ( !! isSpacingEnabled ) return null;
+	// if ( ! isSpacingEnabled ) return null;
 
 	return (
 		<InspectorControls { ...props }>
