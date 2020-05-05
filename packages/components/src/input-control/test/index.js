96c5de871bdba1ced003da0aa7cb78ec9d62ce51
Jon Q <hello@jonquach.com>
2020-05-05 12:09:26 -0400

    Add value tests for InputControl

--- packages/components/src/input-control/test/index.js
+++ packages/components/src/input-control/test/index.js
@@ -1,51 +1,116 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
-import InputControl from '../';
+import BaseInputControl from '../';
+
+const getInput = () => screen.getByTestId( 'input' );
 
 describe( 'InputControl', () => {
+	const InputControl = ( props ) => (
+		<BaseInputControl { ...props } data-testid="input" />
+	);
+
 	describe( 'Basic rendering', () => {
 		it( 'should render', () => {
-			const result = render( <InputControl /> );
+			render( <InputControl /> );
 
-			const input = result.container.querySelector( 'input' );
+			const input = getInput();
 
 			expect( input ).toBeTruthy();
 		} );
 
 		it( 'should render with specified type', () => {
-			const result = render( <InputControl type="number" /> );
+			render( <InputControl type="number" /> );
 
-			const input = result.container.querySelector( '[type="number"]' );
+			const input = getInput();
 
-			expect( input ).toBeTruthy();
+			expect( input.getAttribute( 'type' ) ).toBe( 'number' );
 		} );
 	} );
 
 	describe( 'Label', () => {
 		it( 'should render label', () => {
-			const result = render(
-				<InputControl label="Hello" value="There" />
-			);
+			render( <InputControl label="Hello" value="There" /> );
 
-			const input = result.getByText( 'Hello' );
+			const input = screen.getByText( 'Hello' );
 
 			expect( input ).toBeTruthy();
 		} );
 
 		it( 'should render label, if floating', () => {
-			const result = render(
+			render(
 				<InputControl isFloatingLabel label="Hello" value="There" />
 			);
 
-			const input = result.getAllByText( 'Hello' );
+			const input = screen.getAllByText( 'Hello' );
 
 			expect( input ).toBeTruthy();
+		} );
+	} );
+
+	describe( 'Value', () => {
+		it( 'should update value onChange', () => {
+			const spy = jest.fn();
+			render( <InputControl value="Hello" onChange={ spy } /> );
+
+			const input = getInput();
+
+			fireEvent.change( input, { target: { value: 'There' } } );
+
+			expect( input.value ).toBe( 'There' );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
+		} );
+
+		it( 'should work as a controlled component', () => {
+			const spy = jest.fn();
+			const { rerender } = render(
+				<InputControl value="Original" onChange={ spy } />
+			);
+
+			const input = getInput();
+
+			fireEvent.change( input, { target: { value: 'State' } } );
+
+			// Assuming <InputControl /> is controlled...
+
+			// Updating the value
+			rerender( <InputControl value="New" onChange={ spy } /> );
+
+			expect( input.value ).toBe( 'New' );
+
+			/**
+			 * onChange called only once. onChange is not called when a
+			 * parent component explicitly passed a (new value) change down to
+			 * the <InputControl />.
+			 */
+			expect( spy ).toHaveBeenCalledTimes( 1 );
+		} );
+
+		it( 'should change back to initial value prop, if controlled', () => {
+			const spy = jest.fn();
+			const { rerender } = render(
+				<InputControl value="Original" onChange={ spy } />
+			);
+
+			const input = getInput();
+
+			// Assuming <InputControl /> is controlled...
+
+			// Updating the value
+			rerender( <InputControl value="New" onChange={ spy } /> );
+
+			expect( input.value ).toBe( 'New' );
+
+			// Change it back to the original value
+			rerender( <InputControl value="Original" onChange={ spy } /> );
+
+			expect( input.value ).toBe( 'Original' );
+			expect( spy ).toHaveBeenCalledTimes( 0 );
 		} );
 	} );
 } );
