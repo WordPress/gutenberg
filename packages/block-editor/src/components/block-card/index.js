e1b03d34755f4a9e37c0c1da281247622f491d41
retrofox <rdsuarez@gmail.com>
2020-05-06 14:02:48 -0300

    block-card: rebder tip if it's defined

--- packages/block-editor/src/components/block-card/index.js
+++ packages/block-editor/src/components/block-card/index.js
@@ -1,21 +1,39 @@
 /**
+ * WordPress dependencies
+ */
+import { Tip } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import BlockIcon from '../block-icon';
 
-function BlockCard( { blockType } ) {
+// const contextualTip = select( 'core/block-editor' ).__experimentalGetBlockInserterTipByContext( context, true );
+
+// console.log( { __experimentalGetBlockTipByType } );
+
+function BlockCard( { blockType, tip } ) {
 	return (
-		<div className="block-editor-block-card">
-			<BlockIcon icon={ blockType.icon } showColors />
-			<div className="block-editor-block-card__content">
-				<h2 className="block-editor-block-card__title">
-					{ blockType.title }
-				</h2>
-				<span className="block-editor-block-card__description">
-					{ blockType.description }
-				</span>
+		<>
+			<div className="block-editor-block-card">
+				<BlockIcon icon={ blockType.icon } showColors />
+				<div className="block-editor-block-card__content">
+					<h2 className="block-editor-block-card__title">
+						{ blockType.title }
+					</h2>
+					<span className="block-editor-block-card__description">
+						{ blockType.description }
+					</span>
+				</div>
 			</div>
-		</div>
+			{ tip && (
+				<div className="block-editor-block-card">
+					<div className="block-editor-block-card__tip">
+						<Tip>{ tip }</Tip>
+					</div>
+				</div>
+			) }
+		</>
 	);
 }
 
