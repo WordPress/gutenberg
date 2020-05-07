55beff0ee2d3e44b04940d4b6e9528769b4721c2
retrofox <rdsuarez@gmail.com>
2020-05-07 08:47:18 -0300

    edit-post: fix block inserter tips links

--- packages/edit-post/src/utils/tips.js
+++ packages/edit-post/src/utils/tips.js
@@ -6,19 +6,6 @@ import { createInterpolateElement } from '@wordpress/element';
 
 export default [
 	[
-		'css',
-		[ 'css', __( 'css' ), 'style', __( 'style' ) ],
-		createInterpolateElement(
-			__(
-				'CSS - You can visit the <a>the Customizer</a> to edit the CSS on your site.'
-			),
-			{
-				a: <a href="/customize.php?autofocus[section]=custom_css" target="_blank"/>
-			}
-		),
-	],
-
-	[
 		'theme',
 		[ 'theme', __( 'theme' ) ],
 		createInterpolateElement(
@@ -26,7 +13,20 @@ export default [
 				'theme - You can visit the <a>theme directory</a> to select a different design for your site.'
 			),
 			{
-				a: <a href="/themes.php" target="_blank"/>
+				a: <a href="./themes.php" target="_blank"/>
+			}
+		),
+	],
+
+	[
+		'css',
+		[ 'css', __( 'css' ), 'style', __( 'style' ) ],
+		createInterpolateElement(
+			__(
+				'CSS - You can visit the the <a>Customizer</a> to edit the CSS on your site.'
+			),
+			{
+				a: <a href="./customize.php?autofocus[section]=custom_css" target="_blank"/>
 			}
 		),
 	],
@@ -39,7 +39,7 @@ export default [
 				'plugin - You can visit the <a>plugin directory</a> to install additional plugins.'
 			),
 			{
-				a: <a href="/plugin-install.php" target="_blank"/>
+				a: <a href="./plugin-install.php" target="_blank"/>
 			}
 		),
 	],
@@ -49,10 +49,10 @@ export default [
 		[ 'header', __( 'header' ) ],
 		createInterpolateElement(
 			__(
-				'header - You can visit the <a>the Customizer</a> to edit your logo and site title.'
+				'header - You can visit the the <a>Customizer</a> to edit your logo and site title.'
 			),
 			{
-				a: <a href="/customize.php?autofocus[section]=title_tagline" target="_blank"/>
+				a: <a href="./customize.php?autofocus[section]=title_tagline" target="_blank"/>
 			}
 		),
 	],
@@ -61,10 +61,10 @@ export default [
 		[ 'colors', __( 'colors' ) ],
 		createInterpolateElement(
 			__(
-				'colors - You can visit the <a>the Customizer</a> to edit the colors on your site.'
+				'colors - You can visit the the <a>Customizer</a> to edit the colors on your site.'
 			),
 			{
-				a: <a href="/customize.php?autofocus[section]=colors" target="_blank"/>
+				a: <a href="./customize.php?autofocus[section]=colors" target="_blank"/>
 			}
 		),
 	],
