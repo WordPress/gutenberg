cfeef7c1678473149bb537d8966d99e7ea686e65
Riad Benguella <benguella@gmail.com>
2020-05-07 12:29:21 +0100

    Refactor to separate search results from the patterns per categories

--- packages/block-editor/src/components/inserter/block-patterns.js
+++ packages/block-editor/src/components/inserter/block-patterns.js
@@ -10,7 +10,7 @@ import { useMemo, useCallback } from '@wordpress/element';
 import { parse, cloneBlock } from '@wordpress/blocks';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { ENTER, SPACE } from '@wordpress/keycodes';
-import { __, sprintf } from '@wordpress/i18n';
+import { __, sprintf, _x } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -20,6 +20,33 @@ import useAsyncList from './use-async-list';
 import InserterPanel from './panel';
 import { searchItems } from './search-items';
 import InserterNoResults from './no-results';
+
+const usePatternsState = ( onInsert ) => {
+	const { blockPatternCategories, patterns } = useSelect( ( select ) => {
+		return {
+			patterns: select( 'core/block-editor' ).getSettings()
+				.__experimentalBlockPatterns,
+			blockPatternCategories: select( 'core/block-editor' ).getSettings()
+				.__experimentalBlockPatternCategories,
+		};
+	}, [] );
+	const { createSuccessNotice } = useDispatch( 'core/notices' );
+	const onClickPattern = useCallback( ( pattern, blocks ) => {
+		onInsert( map( blocks, ( block ) => cloneBlock( block ) ) );
+		createSuccessNotice(
+			sprintf(
+				/* translators: %s: block pattern title. */
+				__( 'Pattern "%s" inserted.' ),
+				pattern.title
+			),
+			{
+				type: 'snackbar',
+			}
+		);
+	}, [] );
+
+	return [ patterns, blockPatternCategories, onClickPattern ];
+};
 
 function BlockPattern( { pattern, onClick } ) {
 	const { content, viewportWidth } = pattern;
@@ -67,54 +94,14 @@ function BlockPatternList( { patterns, showPatterns, onClickPattern } ) {
 	} );
 }
 
-function BlockPatterns( { onInsert, filterValue } ) {
-	const { blockPatternCategories, patterns } = useSelect( ( select ) => {
-		return {
-			patterns: select( 'core/block-editor' ).getSettings()
-				.__experimentalBlockPatterns,
-			blockPatternCategories: select( 'core/block-editor' ).getSettings()
-				.__experimentalBlockPatternCategories,
-		};
-	}, [] );
+function BlockPatternsSearchResults( { filterValue, onInsert } ) {
+	const [ patterns, , onClick ] = usePatternsState( { onInsert } );
+	const currentShownPatterns = useAsyncList( patterns );
+
 	const filteredPatterns = useMemo(
 		() => searchItems( patterns, filterValue ),
 		[ filterValue, patterns ]
 	);
-	// Ordering the patterns per category is important for the async rendering.
-	const orderedPatterns = useMemo( () => {
-		const indexedCategories = {};
-		blockPatternCategories.forEach( ( category, index ) => {
-			indexedCategories[ category.name ] = index;
-		} );
-		const getPatternIndex = ( pattern ) => {
-			if ( ! pattern.categories || ! pattern.categories.length ) {
-				return Infinity;
-			}
-			return Math.min(
-				...pattern.categories.map(
-					( category ) => indexedCategories[ category ]
-				)
-			);
-		};
-		return filteredPatterns.sort( ( a, b ) => {
-			return getPatternIndex( a ) - getPatternIndex( b );
-		} );
-	}, [ filterValue, blockPatternCategories ] );
-	const currentShownPatterns = useAsyncList( orderedPatterns );
-	const { createSuccessNotice } = useDispatch( 'core/notices' );
-	const onClickPattern = useCallback( ( pattern, blocks ) => {
-		onInsert( map( blocks, ( block ) => cloneBlock( block ) ) );
-		createSuccessNotice(
-			sprintf(
-				/* translators: %s: block pattern title. */
-				__( 'Pattern "%s" inserted.' ),
-				pattern.title
-			),
-			{
-				type: 'snackbar',
-			}
-		);
-	}, [] );
 
 	if ( filterValue ) {
 		return !! filteredPatterns.length ? (
@@ -122,33 +109,102 @@ function BlockPatterns( { onInsert, filterValue } ) {
 				<BlockPatternList
 					showPatterns={ currentShownPatterns }
 					patterns={ filteredPatterns }
-					onClickPattern={ onClickPattern }
+					onClickPattern={ onClick }
 				/>
 			</InserterPanel>
 		) : (
 			<InserterNoResults />
 		);
 	}
+}
 
-	return blockPatternCategories.map( ( patternCategory ) => {
-		const categoryPatterns = patterns.filter( ( pattern ) =>
-			pattern.categories.includes( patternCategory.name )
-		);
-		return (
-			!! categoryPatterns.length && (
-				<InserterPanel
-					key={ patternCategory.name }
-					title={ patternCategory.label }
-				>
+function BlockPatternsPerCategories( { onInsert } ) {
+	const [ patterns, categories, onClick ] = usePatternsState( { onInsert } );
+
+	const getPatternIndex = useCallback(
+		( pattern ) => {
+			const indexedCategories = {};
+			categories.forEach( ( category, index ) => {
+				indexedCategories[ category.name ] = index;
+			} );
+			if ( ! pattern.categories || ! pattern.categories.length ) {
+				return Infinity;
+			}
+			return Math.min(
+				...pattern.categories.map( ( category ) =>
+					indexedCategories[ category ] !== undefined
+						? indexedCategories[ category ]
+						: Infinity
+				)
+			);
+		},
+		[ categories ]
+	);
+
+	// Ordering the patterns per category is important for the async rendering.
+	const orderedPatterns = useMemo( () => {
+		return patterns.sort( ( a, b ) => {
+			return getPatternIndex( a ) - getPatternIndex( b );
+		} );
+	}, [ patterns, getPatternIndex ] );
+
+	const currentShownPatterns = useAsyncList( orderedPatterns );
+
+	// Uncategorized Patterns
+	const uncategorizedPatterns = useMemo(
+		() =>
+			patterns.filter(
+				( pattern ) => getPatternIndex( pattern ) === Infinity
+			),
+		[ patterns, getPatternIndex ]
+	);
+
+	return (
+		<>
+			{ categories.map( ( patternCategory ) => {
+				const categoryPatterns = patterns.filter(
+					( pattern ) =>
+						pattern.categories &&
+						pattern.categories.includes( patternCategory.name )
+				);
+				return (
+					!! categoryPatterns.length && (
+						<InserterPanel
+							key={ patternCategory.name }
+							title={ patternCategory.label }
+						>
+							<BlockPatternList
+								showPatterns={ currentShownPatterns }
+								patterns={ categoryPatterns }
+								onClickPattern={ onClick }
+							/>
+						</InserterPanel>
+					)
+				);
+			} ) }
+
+			{ !! uncategorizedPatterns.length && (
+				<InserterPanel title={ _x( 'All', 'Uncategorized patterns' ) }>
 					<BlockPatternList
 						showPatterns={ currentShownPatterns }
-						patterns={ categoryPatterns }
-						onClickPattern={ onClickPattern }
+						patterns={ uncategorizedPatterns }
+						onClickPattern={ onClick }
 					/>
 				</InserterPanel>
-			)
-		);
-	} );
+			) }
+		</>
+	);
+}
+
+function BlockPatterns( { onInsert, filterValue } ) {
+	return filterValue ? (
+		<BlockPatternsSearchResults
+			onInsert={ onInsert }
+			filterValue={ filterValue }
+		/>
+	) : (
+		<BlockPatternsPerCategories onInsert={ onInsert } />
+	);
 }
 
 export default BlockPatterns;
