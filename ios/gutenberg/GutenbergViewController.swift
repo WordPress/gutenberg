f87f43f730a63b2494e2b4d342896e13f9766606
Chip Snyder <chip.snyder3@gmail.com>
2020-05-08 10:31:04 -0400

    Add stub for gutenberg test app

--- ios/gutenberg/GutenbergViewController.swift
+++ ios/gutenberg/GutenbergViewController.swift
@@ -216,6 +216,10 @@ extension GutenbergViewController: GutenbergBridgeDataSource {
     func aztecAttachmentDelegate() -> TextViewAttachmentDelegate {
         return ExampleAttachmentDelegate()
     }
+
+    func gutenbergEditorTheme() -> GutenbergEditorTheme? {
+        return nil
+    }
 }
 
 //MARK: - Navigation bar
