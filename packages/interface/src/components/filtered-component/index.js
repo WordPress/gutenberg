e49901a832ada18f02f59480f21783a273cef3c9
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-05-11 18:24:28 +0200

    Interface: Introduce createFilteredComponent factory method

--- packages/interface/src/components/filtered-component/index.js
+++ packages/interface/src/components/filtered-component/index.js
@@ -5,35 +5,41 @@ import { useEffect } from '@wordpress/element';
 import { createHigherOrderComponent, useInstanceId } from '@wordpress/compose';
 import { addFilter, removeFilter } from '@wordpress/hooks';
 
-const FilteredComponent = ( {
-	as: Component,
-	hookName,
-	namespace,
-	priority,
-	...props
-} ) => {
-	const instanceId = useInstanceId( FilteredComponent );
+export const createFilteredComponent = ( name ) =>
+	function FilteredComponent( {
+		as: Component,
+		hookName = name,
+		namespace,
+		priority,
+		...props
+	} ) {
+		const instanceId = useInstanceId( FilteredComponent );
 
-	// Rules of React hooks enforce unusual handling of the default value.
-	namespace = namespace || instanceId;
+		// Rules of React hooks enforce unusual handling of the default value.
+		namespace = namespace || instanceId;
 
-	useEffect( () => {
-		const callback = createHigherOrderComponent(
-			( OriginalComponent ) => ( originalProps ) => {
-				if ( typeof props.children === 'function' ) {
-					return props.children( originalProps, OriginalComponent );
+		useEffect( () => {
+			const callback = createHigherOrderComponent(
+				( OriginalComponent ) => ( originalProps ) => {
+					if ( typeof props.children === 'function' ) {
+						return props.children(
+							originalProps,
+							OriginalComponent
+						);
+					}
+					return <Component { ...originalProps } { ...props } />;
 				}
-				return <Component { ...originalProps } { ...props } />;
-			}
-		);
-		addFilter( hookName, namespace, callback, priority );
+			);
+			addFilter( hookName, namespace, callback, priority );
 
-		return () => {
-			removeFilter( hookName, namespace );
-		};
-	}, [ Component, hookName, namespace, priority ] );
+			return () => {
+				removeFilter( hookName, namespace );
+			};
+		}, [ Component, hookName, namespace, priority ] );
 
-	return null;
-};
+		return null;
+	};
+
+const FilteredComponent = createFilteredComponent();
 
 export default FilteredComponent;
