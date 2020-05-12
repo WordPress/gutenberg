e6fc26676d415a2156c5f4846aa3d86783ec6e7a
dufresnesteven <steve.dufresne@automattic.com>
2020-05-12 10:43:46 +0900

    Remove mistake tab in comment.

--- lib/class-wp-rest-block-directory-controller.php
+++ lib/class-wp-rest-block-directory-controller.php
@@ -276,7 +276,7 @@ class WP_REST_Block_Directory_Controller extends WP_REST_Controller {
 	private static function parse_block_metadata( $plugin ) {
 		$block = new stdClass();
 
-		// There might be multiple blocks in a plugin. Only the firs	t block is mapped.
+		// There might be multiple blocks in a plugin. Only the first block is mapped.
 		$block_data   = reset( $plugin['blocks'] );
 		$block->name  = $block_data['name'];
 		$block->title = $block_data['title'];
