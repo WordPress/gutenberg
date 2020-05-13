13d4e6cbf365ffbf9e52a20d2ac48a19a1b4508d
Daniel Richards <daniel.richards@automattic.com>
2020-05-13 14:36:45 +0800

    Markup CreateMenuForm component as a proper form

--- packages/edit-navigation/src/components/menu-editor/create-menu-form.js
+++ packages/edit-navigation/src/components/menu-editor/create-menu-form.js
@@ -9,18 +9,19 @@ import { __ } from '@wordpress/i18n';
 export default function CreateMenuForm() {
 	const [ menuName, setMenuName ] = useState( '' );
 	const { saveMenu } = useDispatch( 'core' );
+	const createMenu = () => saveMenu( { name: menuName } );
 
 	return (
-		<>
+		<form onSubmit={ createMenu }>
 			<TextControl
 				label={ __( 'Menu name' ) }
 				value={ menuName }
 				onChange={ setMenuName }
 				placeholder={ __( 'Main Navigation' ) }
 			/>
-			<Button isPrimary onClick={ () => saveMenu( { name: menuName } ) }>
+			<Button type="submit" isPrimary onClick={ createMenu }>
 				{ __( 'Create menu' ) }
 			</Button>
-		</>
+		</form>
 	);
 }
