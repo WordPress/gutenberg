a94bc1a1ab6c401b48e6b1d04c9f08f839986c5e
dufresnesteven <steve.dufresne@automattic.com>
2020-05-15 14:12:45 +0900

    Add a list of installed blocks to the pre-publish sidebar

--- packages/block-directory/src/plugins/index.js
+++ packages/block-directory/src/plugins/index.js
@@ -7,9 +7,15 @@ import { registerPlugin } from '@wordpress/plugins';
  * Internal dependencies
  */
 import InserterMenuDownloadableBlocksPanel from './inserter-menu-downloadable-blocks-panel';
+import DownloadableBlockPrePublishPanel from './downloadable-block-pre-publish-panel';
 
 registerPlugin( 'block-directory', {
 	render() {
-		return <InserterMenuDownloadableBlocksPanel />;
+		return (
+			<>
+				<InserterMenuDownloadableBlocksPanel />
+				<DownloadableBlockPrePublishPanel />
+			</>
+		);
 	},
 } );
