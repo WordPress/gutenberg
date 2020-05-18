573767ec72b99913d1ff1e09b639ce9f2c06e1bb
Steven Dufresne <steve.dufresne@automattic.com>
2020-05-18 14:29:25 +0900

    Block Directory: Add error messages inline. (#20001)
    * Block Directory: Add error messages inline.
    
    * Make the linter happy
    
    * Place the retry button on its own line.
    
    * Update classNames to match standards
    
    * Get the error notices in `DownloadableBlockNotice`
    
    This prevents some prop-drilling by getting the notices when we need them
    
    * Get `isLoading` in `DownloadableBlockHeader`
    
    This prevents some prop-drilling by getting the loading status when we need it
    
    * Avoid `children` abstraction in item
    
    * Refactor install callback
    
    This consolidates the install logic into one callback function, simplifying the list component
    
    * Disable the "Add Block" button when installing
    
    * Fix broken tests.
    
    * Update padding under error messages.
    
    Co-authored-by: Kelly Dwan <ryelle@users.noreply.github.com>

---
+++ packages/block-directory/src/components/downloadable-blocks-list/test/index.js
@@ -0,0 +1,65 @@
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { DownloadableBlocksList } from '../index';
+import DownloadableBlockListItem from '../../downloadable-block-list-item';
+import { items, plugin } from './fixtures';
+
+const getContainer = ( {
+	blocks,
+	selectMock = jest.fn(),
+	hoverMock = jest.fn(),
+	isLoading = false,
+	errorNotices = {},
+	install = jest.fn(),
+} ) => {
+	return shallow(
+		<DownloadableBlocksList
+			items={ blocks }
+			onSelect={ selectMock }
+			onHover={ hoverMock }
+			errorNotices={ errorNotices }
+			isLoading={ isLoading }
+			install={ install }
+		/>
+	);
+};
+
+describe( 'DownloadableBlocksList', () => {
+	describe( 'List rendering', () => {
+		it( 'should render and empty list', () => {
+			const wrapper = getContainer( { blocks: [] } );
+			expect( wrapper.isEmptyRender() ).toBe( true );
+		} );
+
+		it( 'should render plugins items into the list', () => {
+			const wrapper = getContainer( { blocks: items } );
+
+			expect( wrapper.find( DownloadableBlockListItem ).length ).toBe(
+				items.length
+			);
+		} );
+	} );
+	describe( 'Behaviour', () => {
+		it( 'should try to install the block plugin', () => {
+			const install = jest.fn();
+			const errorNotices = {};
+
+			const wrapper = getContainer( {
+				blocks: [ plugin ],
+				install,
+				errorNotices,
+			} );
+			const listItems = wrapper.find( DownloadableBlockListItem );
+
+			listItems.get( 0 ).props.onClick();
+
+			expect( install ).toHaveBeenCalledTimes( 1 );
+		} );
+	} );
+} );
