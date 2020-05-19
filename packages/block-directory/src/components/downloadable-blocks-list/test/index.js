04966bec2431ffed7b48a43ed4c7215df78c22e4
Kelly Dwan <ryelle@users.noreply.github.com>
2020-05-19 14:43:15 -0400

    Remove behavior test, update to use snapshot testing
    Behavior is tested in DownloadableBlockListItem tests

--- packages/block-directory/src/components/downloadable-blocks-list/test/index.js
+++ packages/block-directory/src/components/downloadable-blocks-list/test/index.js
@@ -7,59 +7,36 @@ import { shallow } from 'enzyme';
  * Internal dependencies
  */
 import DownloadableBlocksList from '../index';
-import DownloadableBlockListItem from '../../downloadable-block-list-item';
-import { items, plugin } from './fixtures';
+import { items } from './fixtures';
 
-const getContainer = ( {
-	blocks,
-	selectMock = jest.fn(),
-	hoverMock = jest.fn(),
-	isLoading = false,
-	errorNotices = {},
-	install = jest.fn(),
-} ) => {
-	return shallow(
-		<DownloadableBlocksList
-			items={ blocks }
-			onSelect={ selectMock }
-			onHover={ hoverMock }
-			errorNotices={ errorNotices }
-			isLoading={ isLoading }
-			install={ install }
-		/>
-	);
-};
+jest.mock( '@wordpress/data/src/components/use-dispatch', () => ( {
+	useDispatch: () => ( { installBlockType: jest.fn() } ),
+} ) );
 
 describe( 'DownloadableBlocksList', () => {
 	describe( 'List rendering', () => {
 		it( 'should render and empty list', () => {
-			const wrapper = getContainer( { blocks: [] } );
+			const wrapper = shallow(
+				<DownloadableBlocksList
+					items={ [] }
+					onSelect={ jest.fn() }
+					onHover={ jest.fn() }
+					isLoading={ false }
+				/>
+			);
 			expect( wrapper.isEmptyRender() ).toBe( true );
 		} );
 
 		it( 'should render plugins items into the list', () => {
-			const wrapper = getContainer( { blocks: items } );
-
-			expect( wrapper.find( DownloadableBlockListItem ).length ).toBe(
-				items.length
+			const wrapper = shallow(
+				<DownloadableBlocksList
+					items={ items }
+					onSelect={ jest.fn() }
+					onHover={ jest.fn() }
+					isLoading={ false }
+				/>
 			);
-		} );
-	} );
-	describe( 'Behaviour', () => {
-		it( 'should try to install the block plugin', () => {
-			const install = jest.fn();
-			const errorNotices = {};
-
-			const wrapper = getContainer( {
-				blocks: [ plugin ],
-				install,
-				errorNotices,
-			} );
-			const listItems = wrapper.find( DownloadableBlockListItem );
-
-			listItems.get( 0 ).props.onClick();
-
-			expect( install ).toHaveBeenCalledTimes( 1 );
+			expect( wrapper ).toMatchSnapshot();
 		} );
 	} );
 } );
