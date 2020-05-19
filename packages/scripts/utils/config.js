8c9772c6089a9390d030a54a0406bccee1462b08
Andrew Duthie <andrew@andrewduthie.com>
2020-05-19 16:35:13 -0400

    Scripts: Support split Jest configuration for test commands

--- packages/scripts/utils/config.js
+++ packages/scripts/utils/config.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 const { basename } = require( 'path' );
+const { cond } = require( 'lodash' );
 
 /**
  * Internal dependencies
@@ -12,7 +13,7 @@ const {
 	hasArgInCLI,
 	hasFileArgInCLI,
 } = require( './cli' );
-const { fromConfigRoot, hasProjectFile } = require( './file' );
+const { fromConfigRoot, fromProjectRoot, hasProjectFile } = require( './file' );
 const { hasPackageProp } = require( './package' );
 
 // See https://babeljs.io/docs/en/config-files#configuration-file-types
@@ -24,9 +25,36 @@ const hasBabelConfig = () =>
 	hasProjectFile( '.babelrc' ) ||
 	hasPackageProp( 'babel' );
 
+/**
+ * Returns path to a Jest configuration which should be provided as the explicit
+ * configuration when there is none available for discovery by Jest in the
+ * project environment. Returns undefined if Jest should be allowed to discover
+ * an available configuration. Optionally accepts a suffix for configurations
+ * which can differ by variant.
+ *
+ * This can be used in cases where multiple possible configurations are
+ * supported. Since Jest will only discover `jest.config.js`, or `jest` package
+ * directive, such custom configurations must be specified explicitly.
+ *
+ * @param {string=} suffix Optional suffix of configuration file to accept.
+ *
+ * @return {string=} Override or fallback configuration file path.
+ */
+const getJestOverrideConfigFile = ( suffix ) =>
+	cond( [
+		[
+			() => hasArgInCLI( '-c' ) || hasArgInCLI( '--config' ),
+			() => undefined,
+		],
+		[
+			() => suffix && hasProjectFile( `jest-${ suffix }.config.js` ),
+			() => fromProjectRoot( `jest-${ suffix }.config.js` ),
+		],
+		[ () => hasJestConfig(), () => undefined ],
+		[ () => true, () => fromConfigRoot( 'jest-e2e.config.js' ) ],
+	] )();
+
 const hasJestConfig = () =>
-	hasArgInCLI( '-c' ) ||
-	hasArgInCLI( '--config' ) ||
 	hasProjectFile( 'jest.config.js' ) ||
 	hasProjectFile( 'jest.config.json' ) ||
 	hasPackageProp( 'jest' );
@@ -103,6 +131,7 @@ const getWebpackArgs = () => {
 module.exports = {
 	getWebpackArgs,
 	hasBabelConfig,
+	getJestOverrideConfigFile,
 	hasJestConfig,
 	hasPrettierConfig,
 };
