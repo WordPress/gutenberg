c3f6f5b037a3c72dd5da7bd4ee1f15843782cb0a
Andrew Duthie <andrew@andrewduthie.com>
2020-05-19 17:49:59 -0400

    Scripts: Assume suffix provided as one of available default configurations

--- packages/scripts/utils/config.js
+++ packages/scripts/utils/config.js
@@ -29,14 +29,13 @@ const hasBabelConfig = () =>
  * Returns path to a Jest configuration which should be provided as the explicit
  * configuration when there is none available for discovery by Jest in the
  * project environment. Returns undefined if Jest should be allowed to discover
- * an available configuration. Optionally accepts a suffix for configurations
- * which can differ by variant.
+ * an available configuration.
  *
  * This can be used in cases where multiple possible configurations are
  * supported. Since Jest will only discover `jest.config.js`, or `jest` package
  * directive, such custom configurations must be specified explicitly.
  *
- * @param {string=} suffix Optional suffix of configuration file to accept.
+ * @param {"e2e"|"unit"} suffix Suffix of configuration file to accept.
  *
  * @return {string=} Override or fallback configuration file path.
  */
@@ -47,11 +46,11 @@ const getJestOverrideConfigFile = ( suffix ) =>
 			() => undefined,
 		],
 		[
-			() => suffix && hasProjectFile( `jest-${ suffix }.config.js` ),
+			() => hasProjectFile( `jest-${ suffix }.config.js` ),
 			() => fromProjectRoot( `jest-${ suffix }.config.js` ),
 		],
 		[ () => hasJestConfig(), () => undefined ],
-		[ () => true, () => fromConfigRoot( 'jest-e2e.config.js' ) ],
+		[ () => true, () => fromConfigRoot( `jest-${ suffix }.config.js` ) ],
 	] )();
 
 const hasJestConfig = () =>
