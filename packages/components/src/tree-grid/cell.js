5d33b9a01921dbb06587ebd58dc0c04a451f2152
Daniel Richards <daniel.richards@automattic.com>
2020-05-19 13:29:08 +0800

    Use function to indicate cell has child focusable element

--- packages/components/src/tree-grid/cell.js
+++ packages/components/src/tree-grid/cell.js
@@ -1,6 +1,11 @@
 /**
  * External dependencies
  */
+import { isFunction } from 'lodash';
+
+/**
+ * External dependencies
+ */
 import { unstable_CompositeItem as CompositeItem } from 'reakit/Composite';
 
 /**
@@ -11,10 +16,25 @@ import { forwardRef } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import useTreeGridContext from './context';
+import { useTreeGridContext } from './context';
 
-function TreeGridCell( props, ref ) {
+function TreeGridCell( { children, ...props }, ref ) {
 	const treeGridState = useTreeGridContext();
+
+	// Using a function as a child indicates that the
+	// child renders a focusable element within a cell.
+	// When a treegrid cell has a child focusable, the
+	// cell itself should not be focusable.
+	if ( isFunction( children ) ) {
+		const { className, colSpan, ...compositeProps } = props;
+
+		return (
+			<td role="gridcell" className={ className } colSpan={ colSpan }>
+				<CompositeItem children={ children } { ...compositeProps } />
+			</td>
+		);
+	}
+
 	return (
 		<CompositeItem
 			{ ...treeGridState }
