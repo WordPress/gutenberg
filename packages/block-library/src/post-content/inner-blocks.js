fe0396ba396b9069ed78723ad12223058a7a9763
Noah Allen <noahtallen@gmail.com>
2020-05-19 11:44:41 -0700

    Add basic docs

--- packages/block-library/src/post-content/inner-blocks.js
+++ packages/block-library/src/post-content/inner-blocks.js
@@ -4,6 +4,13 @@
 import { useEntityBlockEditor } from '@wordpress/core-data';
 import { InnerBlocks } from '@wordpress/block-editor';
 
+/**
+ * Renders the content of the current post in a controlled inner block area.
+ *
+ * @param {Object} Props Component props.
+ * @param {string} Props.postType The postType to use for the entity blocks.
+ * @return {Function} Controlled InnerBlocks of the current post.
+ */
 export default function PostContentInnerBlocks( { postType } ) {
 	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
 		'postType',
