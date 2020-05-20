c5bebcc7c7a04952082fbec3ca340cb3419fff5e
epiqueras <epiquerass@gmail.com>
2020-05-20 15:19:17 -0700

    API Fetch: Support preloading in `fetchAllMiddleware`.

--- lib/edit-site-page.php
+++ lib/edit-site-page.php
@@ -191,8 +191,8 @@ function gutenberg_edit_site_init( $hook ) {
 	$preload_paths = array(
 		'/',
 		'/wp/v2/types?context=edit',
-		'/wp/v2/taxonomies?per_page=-1&context=edit',
-		'/wp/v2/pages?per_page=-1&context=edit',
+		'/wp/v2/taxonomies?per_page=100&context=edit',
+		'/wp/v2/pages?per_page=100&context=edit',
 		'/wp/v2/themes?status=active',
 		sprintf( '/wp/v2/templates/%s?context=edit', $current_template_id ),
 		array( '/wp/v2/media', 'OPTIONS' ),
