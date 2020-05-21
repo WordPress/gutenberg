5ca8da26b65ac14a411c3f7fa2550de55da4ce58
Adam Zieliński <adam@adamziel.com>
2020-05-21 11:37:01 +0200

    Disable ellipsis menu in the post editor

--- packages/edit-post/src/components/header/header-toolbar/index.js
+++ packages/edit-post/src/components/header/header-toolbar/index.js
@@ -77,10 +77,7 @@ function HeaderToolbar( { onToggleInserter, isInserterOpen } ) {
 			<EditorHistoryUndo />
 			<EditorHistoryRedo />
 			<TableOfContents hasOutlineItemsDisabled={ isTextModeEnabled } />
-			<BlockNavigationDropdown
-				isDisabled={ isTextModeEnabled }
-				__experimentalWithEllipsisMenu
-			/>
+			<BlockNavigationDropdown isDisabled={ isTextModeEnabled } />
 			{ displayBlockToolbar && (
 				<div className="edit-post-header-toolbar__block-toolbar">
 					<BlockToolbar hideDragHandle />
