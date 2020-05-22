624796ab59ffb72ff8528bf81696cf289d790d05
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-05-22 16:02:40 +0300

    refactored for properly deleteing entity ids from querries

--- packages/core-data/src/queried-data/actions.js
+++ packages/core-data/src/queried-data/actions.js
@@ -18,6 +18,27 @@ export function receiveItems( items ) {
 }
 
 /**
+ * Returns an action object used in signalling that entity records have been
+ * deleted and it needs to be removed from entities state.
+ *
+ * @param {string} kind            Kind of the removed entity.
+ * @param {string} name            Name of the removed entity.
+ * @param {Array|Object} records   Records removed.
+ * @param {Object} query           Original query of the removed items.
+ * @return {Object} Action object.
+ */
+export function removeItems( kind, name, records, query ) {
+	return {
+		type: 'REMOVE_ITEMS',
+		items: castArray( records ),
+		kind,
+		name,
+		query,
+		invalidateCache: false,
+	};
+}
+
+/**
  * Returns an action object used in signalling that queried data has been
  * received.
  *
