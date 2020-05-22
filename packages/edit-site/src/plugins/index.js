39b91ef699a54d4639047add558923e7917b8564
Noah Allen <noahtallen@gmail.com>
2020-05-22 14:49:25 -0700

    Only keep modals we can implement now

--- packages/edit-site/src/plugins/index.js
+++ packages/edit-site/src/plugins/index.js
@@ -10,44 +10,32 @@ import { addQueryArgs } from '@wordpress/url';
  * Internal dependencies
  */
 import CopyContentMenuItem from './copy-content-menu-item';
-import ManageBlocksMenuItem from './manage-blocks-menu-item';
-import KeyboardShortcutsHelpMenuItem from './keyboard-shortcuts-help-menu-item';
 import ToolsMoreMenuGroup from '../components/header/tools-more-menu-group';
-import WelcomeGuideMenuItem from './welcome-guide-menu-item';
 
 registerPlugin( 'edit-site', {
 	render() {
 		return (
 			<>
 				<ToolsMoreMenuGroup>
-					{ ( { onClose } ) => (
-						<>
-							<ManageBlocksMenuItem onSelect={ onClose } />
-							<MenuItem
-								role="menuitem"
-								href={ addQueryArgs( 'edit.php', {
-									post_type: 'wp_block',
-								} ) }
-							>
-								{ __( 'Manage all reusable blocks' ) }
-							</MenuItem>
-							<KeyboardShortcutsHelpMenuItem
-								onSelect={ onClose }
-							/>
-							<WelcomeGuideMenuItem />
-							<CopyContentMenuItem />
-							<MenuItem
-								role="menuitem"
-								href={ __(
-									'https://wordpress.org/support/article/wordpress-editor/'
-								) }
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{ __( 'Help' ) }
-							</MenuItem>
-						</>
-					) }
+					<MenuItem
+						role="menuitem"
+						href={ addQueryArgs( 'edit.php', {
+							post_type: 'wp_block',
+						} ) }
+					>
+						{ __( 'Manage all reusable blocks' ) }
+					</MenuItem>
+					<CopyContentMenuItem />
+					<MenuItem
+						role="menuitem"
+						href={ __(
+							'https://wordpress.org/support/article/wordpress-editor/'
+						) }
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{ __( 'Help' ) }
+					</MenuItem>
 				</ToolsMoreMenuGroup>
 			</>
 		);
