db4ca0d42ad5bfe1f97bf37fee95163026500f5b
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-05-25 08:27:18 -0500

    Add usePrevious hook to @wordpress/compose package (#22540)
    * Add usePrevious hook.
    
    * Use generic template type in JSDoc.

---
+++ packages/compose/src/hooks/use-previous/index.js
@@ -0,0 +1,26 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useRef } from '@wordpress/element';
+
+/**
+ * Use something's value from the previous render.
+ * Based on https://usehooks.com/usePrevious/.
+ *
+ * @template T
+ *
+ * @param {T} value The value to track.
+ *
+ * @return {T} The value from the previous render.
+ */
+export default function usePrevious( value ) {
+	const ref = useRef();
+
+	// Store current value in ref.
+	useEffect( () => {
+		ref.current = value;
+	}, [ value ] ); // Re-run when value changes.
+
+	// Return previous value (happens before update in useEffect above).
+	return ref.current;
+}
