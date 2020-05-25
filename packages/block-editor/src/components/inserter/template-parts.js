7f1b30a4bf54a8333c6dfbea88ac206296c32b6c
Addison-Stavlo <stavz01@gmail.com>
2020-05-25 14:11:57 -0400

    show previews of each template part

--- packages/block-editor/src/components/inserter/template-parts.js
+++ packages/block-editor/src/components/inserter/template-parts.js
@@ -1,7 +1,52 @@
-export default function TemplateParts() {
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { parse } from '@wordpress/blocks';
+import { useMemo } from '@wordpress/element';
+/**
+ * Internal dependencies
+ */
+import BlockPreview from '../block-preview';
+
+function TemplatePartItem( { templatePart } ) {
+	const blocks = useMemo( () => parse( templatePart.content.raw ), [
+		templatePart.content.raw,
+	] );
+
 	return (
 		<div>
-			<h1>TPs!!!</h1>
+			<BlockPreview blocks={ blocks } />
+			<div>{ templatePart.slug }</div>
+		</div>
+	);
+}
+
+export default function TemplateParts() {
+	const templateParts = useSelect( ( select ) => {
+		return select( 'core' ).getEntityRecords(
+			'postType',
+			'wp_template_part',
+			{
+				status: [ 'publish', 'auto-draft' ],
+			}
+		);
+	}, [] );
+
+	if ( ! Array.isArray( templateParts ) ) {
+		return null;
+	}
+
+	return (
+		<div>
+			{ templateParts.map( ( templatePart ) => {
+				return (
+					<TemplatePartItem
+						key={ templatePart.id }
+						templatePart={ templatePart }
+					/>
+				);
+			} ) }
 		</div>
 	);
 }
