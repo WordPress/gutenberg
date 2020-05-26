e08a2c61fb1f5895cddd2f0f73f6eba234fa2b0d
Adam Zieliński <adam@adamziel.com>
2020-05-26 14:13:31 +0200

    Unshift -> shift

--- packages/edit-navigation/src/components/menu-editor/promise-queue.js
+++ packages/edit-navigation/src/components/menu-editor/promise-queue.js
@@ -16,7 +16,7 @@ export default class PromiseQueue {
 
 	run() {
 		while ( this.queue.length && this.active.length <= this.concurrency ) {
-			const action = this.queue.unshift();
+			const action = this.queue.shift();
 			const promise = action().then( () => {
 				this.active.splice( this.active.indexOf( promise ), 1 );
 				this.run();
