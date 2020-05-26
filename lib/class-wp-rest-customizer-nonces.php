db7caa4ac16a15b7b408397d62e5790e2cd96cee
Adam Zieliński <adam@adamziel.com>
2020-05-26 14:03:26 +0200

    Lint

--- lib/class-wp-rest-customizer-nonces.php
+++ lib/class-wp-rest-customizer-nonces.php
@@ -19,30 +19,40 @@ class WP_Rest_Customizer_Nonces extends WP_REST_Controller {
 		$this->rest_base = 'customizer-nonces';
 	}
 
+	/**
+	 * Registers the necessary REST API routes.
+	 *
+	 * @access public
+	 */
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
 			'/' . $this->rest_base . '/get-save-nonce',
 			array(
 				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_save_nonce' ),
-					'args'                => $this->get_collection_params(),
+					'methods'  => WP_REST_Server::READABLE,
+					'callback' => array( $this, 'get_save_nonce' ),
+					'args'     => $this->get_collection_params(),
 				),
 				'schema' => array( $this, 'get_public_item_schema' ),
 			)
 		);
 	}
 
-	public function get_save_nonce(  ) {
+	/**
+	 * Returns the nonce required to request the customizer API endpoint.
+	 *
+	 * @access public
+	 */
+	public function get_save_nonce() {
 		require_once ABSPATH . 'wp-includes/class-wp-customize-manager.php';
 		$wp_customize = new WP_Customize_Manager();
 		$nonce        = wp_create_nonce( 'save-customize_' . $wp_customize->get_stylesheet() );
-		return [
-			"success" => true,
-			"nonce" => $nonce,
-			"stylesheet" => $wp_customize->get_stylesheet()
-		];
+		return array(
+			'success'    => true,
+			'nonce'      => $nonce,
+			'stylesheet' => $wp_customize->get_stylesheet(),
+		);
 	}
 
 }
