0f7f14a61fb88a7995eba44272f865f4edfc1ad7
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2020-05-27 13:58:01 +0200

    Try different jest config

--- test/native/jest_ui.config.js
+++ test/native/jest_ui.config.js
@@ -1,7 +1,22 @@
-const main = require( './jest.config.js' );
+const defaultPlatform = 'android';
+const rnPlatform = process.env.TEST_RN_PLATFORM || defaultPlatform;
+if ( process.env.TEST_RN_PLATFORM ) {
+	// eslint-disable-next-line no-console
+	console.log( 'Setting RN platform to: ' + rnPlatform );
+} else {
+	// eslint-disable-next-line no-console
+	console.log( 'Setting RN platform to: default (' + defaultPlatform + ')' );
+}
 
 module.exports = {
-	...main,
+	verbose: true,
+	haste: {
+		defaultPlatform: rnPlatform,
+		platforms: [ 'android', 'ios', 'native' ],
+	},
+	transform: {
+		'^.+\\.(js|ts|tsx)$': 'babel-jest',
+	},
 	timers: 'real',
 	setupFiles: [],
 	testMatch: [ '**/__device-tests__/**/*.test.[jt]s?(x)' ],
