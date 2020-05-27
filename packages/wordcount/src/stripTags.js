b179de98c3a773bb8e47e5f20b3f9c3fa6b7aff3
Andrew Duthie <andrew@andrewduthie.com>
2020-05-27 14:44:50 -0400

    Replace function expressions with named function or arrow expression (#22647)

--- packages/wordcount/src/stripTags.js
+++ packages/wordcount/src/stripTags.js
@@ -6,7 +6,7 @@
  *
  * @return {string} The manipulated text.
  */
-export default function( settings, text ) {
+export default function stripTags( settings, text ) {
 	if ( settings.HTMLRegExp ) {
 		return text.replace( settings.HTMLRegExp, '\n' );
 	}
