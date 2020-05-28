f6b0f17a6414014f4ec89b7725b290e8be50fa2c
James Koster <james@jameskoster.co.uk>
2020-05-28 11:39:39 +0100

    Update and add icons
    * "Cloud" icon updated to match g2 visual design
    * Add "Mail" icon
    * Add "Warning" icon
    
    Also updates the wordpress/primitives dependency path

---
+++ packages/icons/src/library/mail.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const mail = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M17.264 7.5H6.736L12 12.012 17.264 7.5zM5.5 8.416V16a.5.5 0 00.5.5h12a.5.5 0 00.5-.5V8.416l-6.012 5.153-.488.419-.488-.419L5.5 8.416zM4 8a2 2 0 012-2h12a2 2 0 012 2v8a2 2 0 01-2 2H6a2 2 0 01-2-2V8z" />
+	</SVG>
+);
+
+export default mail;
---
+++ packages/icons/src/library/warning.js
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const warning = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M12 4.75a7.25 7.25 0 100 14.5 7.25 7.25 0 000-14.5zM3.25 12a8.75 8.75 0 1117.5 0 8.75 8.75 0 01-17.5 0zM13 7v6h-2V7h2zm0 10v-2h-2v2h2z" />
+	</SVG>
+);
+
+export default warning;
