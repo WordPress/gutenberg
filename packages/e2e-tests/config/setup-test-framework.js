d7fcc13f85d5f2a086850d02d0d2d3260dc19c60
Daniel Richards <daniel.richards@automattic.com>
2020-05-29 09:09:29 +0800

    Add an exclude rule for the actual failing element

--- packages/e2e-tests/config/setup-test-framework.js
+++ packages/e2e-tests/config/setup-test-framework.js
@@ -248,6 +248,7 @@ async function runAxeTestsForBlockEditor() {
 			// Ignores elements within the media modal.
 			// Related: https://core.trac.wordpress.org/ticket/50273
 			'.media-modal',
+			'.media-modal .attachment',
 			// These properties were not included in the 1.1 spec
 			// through error, they should be allowed on role="row":
 			// https://github.com/w3c/aria/issues/558
