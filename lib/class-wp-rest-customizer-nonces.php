69001ac4c968da8a0fbebfaf4e1221dddea96a6d
Adam Zieliński <adam@adamziel.com>
2020-05-29 11:41:33 +0200

    Add permissions_check to the nonce endpoint

--- lib/class-wp-rest-customizer-nonces.php
+++ lib/class-wp-rest-customizer-nonces.php
@@ -30,13 +30,28 @@ class WP_Rest_Customizer_Nonces extends WP_REST_Controller {
 			'/' . $this->rest_base . '/get-save-nonce',
 			array(
 				array(
-					'methods'  => WP_REST_Server::READABLE,
-					'callback' => array( $this, 'get_save_nonce' ),
-					'args'     => $this->get_collection_params(),
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_save_nonce' ),
+					'permission_callback' => array( $this, 'permissions_check' ),
+					'args'                => $this->get_collection_params(),
 				),
 				'schema' => array( $this, 'get_public_item_schema' ),
 			)
 		);
+	}
+
+	/**
+	 * Checks if a given request has access to read menu items if they have access to edit them.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
+	 */
+	public function permissions_check( $request ) {
+		$post_type = get_post_type_object( 'nav_menu_item' );
+		if ( ! current_user_can( $post_type->cap->edit_posts ) ) {
+			return new WP_Error( 'rest_forbidden_context', __( 'Sorry, you are not allowed to edit posts in this post type.', 'gutenberg' ), array( 'status' => rest_authorization_required_code() ) );
+		}
+		return true;
 	}
 
 	/**
