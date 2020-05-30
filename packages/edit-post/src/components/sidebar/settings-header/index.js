465f432b27af631f904f4c5c9db13b85d2856257
Udit Desai <desaiuditd@gmail.com>
2020-05-30 21:03:18 +1000

    Clean up & remove extra code.

--- packages/edit-post/src/components/sidebar/settings-header/index.js
+++ packages/edit-post/src/components/sidebar/settings-header/index.js
@@ -15,11 +15,13 @@ const SettingsHeader = ( {
 	openBlockSettings,
 	sidebarName,
 } ) => {
+	// translators: Label for the Block Settings Sidebar tab, not selected.
+	const blockLabel = __( 'Block' );
 	const documentLabel = useSelect( ( select ) => {
 		const currentPostType = select( 'core/editor' ).getCurrentPostType();
 		const postType = select( 'core' ).getPostType( currentPostType );
 
-		// translators: Default ARIA label for the Document sidebar tab, not selected.
+		// translators: Default label for the Document sidebar tab, not selected.
 		const defaultDocumentLabel = __( 'Document' );
 
 		return [ 'post', 'page' ].includes( currentPostType )
@@ -37,13 +39,11 @@ const SettingsHeader = ( {
 			  [ sprintf( __( '%1$s (selected)' ), documentLabel ), 'is-active' ]
 			: [ documentLabel, '' ];
 
-	const blockLabel = __( 'Block' );
 	const [ blockAriaLabel, blockActiveClass ] =
 		sidebarName === 'edit-post/block'
-			? // translators: ARIA label for the Settings Sidebar tab, selected.
-			  [ __( 'Block (selected)' ), 'is-active' ]
-			: // translators: ARIA label for the Settings Sidebar tab, not selected.
-			  [ __( 'Block' ), '' ];
+			? // translators: ARIA label for the Block Settings Sidebar tab, selected.
+			  [ sprintf( __( '%1$s (selected)' ), blockLabel ), 'is-active' ]
+			: [ blockLabel, '' ];
 
 	/* Use a list so screen readers will announce how many tabs there are. */
 	return (
