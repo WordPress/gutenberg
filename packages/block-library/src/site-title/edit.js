0f92aa114c5c2c4e1e3085ca61ff7ce2fadf9397
apeatling <apeatling@gmail.com>
2020-06-02 14:42:58 -0700

    Add alignment control to the site title block edit function.

--- packages/block-library/src/site-title/edit.js
+++ packages/block-library/src/site-title/edit.js
@@ -5,19 +5,34 @@ import { useEntityProp } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
 import {
 	PlainText,
+	AlignmentToolbar,
+	BlockControls,
 	__experimentalBlock as Block,
 } from '@wordpress/block-editor';
 
-export default function SiteTitleEdit() {
+export default function SiteTitleEdit( { attributes, setAttributes } ) {
+	const { align } = attributes;
 	const [ title, setTitle ] = useEntityProp( 'root', 'site', 'title' );
+
 	return (
-		<PlainText
-			__experimentalVersion={ 2 }
-			tagName={ Block.h1 }
-			placeholder={ __( 'Site Title' ) }
-			value={ title }
-			onChange={ setTitle }
-			disableLineBreaks
-		/>
+		<>
+			<BlockControls>
+				<AlignmentToolbar
+					value={ align }
+					onChange={ ( nextAlign ) => {
+						setAttributes( { align: nextAlign } );
+					} }
+				/>
+			</BlockControls>
+
+			<PlainText
+				__experimentalVersion={ 2 }
+				tagName={ Block.h1 }
+				placeholder={ __( 'Site Title' ) }
+				value={ title }
+				onChange={ setTitle }
+				disableLineBreaks
+			/>
+		</>
 	);
 }
