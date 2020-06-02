075f33509d97d577f108c891b3ea4386f6ca339d
Adam Zieliński <adam@adamziel.com>
2020-06-02 14:25:16 +0200

    Simplify the refactor (thank you Andrei!)

--- packages/edit-navigation/src/components/menu-editor/index.js
+++ packages/edit-navigation/src/components/menu-editor/index.js
@@ -13,28 +13,9 @@ import { useMemo } from '@wordpress/element';
  */
 import useMenuItems from './use-menu-items';
 import useNavigationBlocks from './use-navigation-blocks';
-import useCreateMissingMenuItems from './use-create-missing-menu-items';
-import useSaveNavigationBlocks from './use-save-navigation-blocks';
 import MenuEditorShortcuts from './shortcuts';
 import BlockEditorPanel from './block-editor-panel';
 import NavigationStructurePanel from './navigation-structure-panel';
-
-const useMenuEditor = ( menuId ) => {
-	const query = useMemo( () => ( { menus: menuId, per_page: -1 } ), [
-		menuId,
-	] );
-	const menuItems = useMenuItems( query );
-	const { blocks, setBlocks, menuItemsRef } = useNavigationBlocks(
-		menuItems
-	);
-	const saveBlocks = useSaveNavigationBlocks( blocks, menuItemsRef, query );
-	const { createMissingMenuItems, onCreated } = useCreateMissingMenuItems(
-		menuItemsRef
-	);
-	const eventuallySaveBlocks = () => onCreated( () => saveBlocks() );
-
-	return [ blocks, setBlocks, eventuallySaveBlocks, createMissingMenuItems ];
-};
 
 export default function MenuEditor( {
 	menuId,
@@ -42,12 +23,20 @@ export default function MenuEditor( {
 	onDeleteMenu,
 } ) {
 	const isLargeViewport = useViewportMatch( 'medium' );
-	const [
-		blocks,
-		setBlocks,
-		saveBlocks,
+	const query = useMemo( () => ( { menus: menuId, per_page: -1 } ), [
+		menuId,
+	] );
+	const {
+		menuItems,
+		saveMenuItems,
 		createMissingMenuItems,
-	] = useMenuEditor( menuId );
+		onCreated,
+	} = useMenuItems( query );
+	const { blocks, setBlocks, menuItemsRef } = useNavigationBlocks(
+		menuItems
+	);
+	const eventuallySaveItems = () =>
+		onCreated( () => saveMenuItems( blocks, menuItemsRef ) );
 
 	return (
 		<div className="edit-navigation-menu-editor">
@@ -58,7 +47,7 @@ export default function MenuEditor( {
 				value={ blocks }
 				onInput={ ( updatedBlocks ) => setBlocks( updatedBlocks ) }
 				onChange={ ( updatedBlocks ) => {
-					createMissingMenuItems( updatedBlocks );
+					createMissingMenuItems( updatedBlocks, menuItemsRef );
 					setBlocks( updatedBlocks );
 				} }
 				settings={ {
@@ -68,13 +57,13 @@ export default function MenuEditor( {
 				} }
 			>
 				<BlockEditorKeyboardShortcuts />
-				<MenuEditorShortcuts saveBlocks={ saveBlocks } />
+				<MenuEditorShortcuts saveBlocks={ eventuallySaveItems } />
 				<NavigationStructurePanel
 					blocks={ blocks }
 					initialOpen={ isLargeViewport }
 				/>
 				<BlockEditorPanel
-					saveBlocks={ saveBlocks }
+					saveBlocks={ eventuallySaveItems }
 					menuId={ menuId }
 					onDeleteMenu={ onDeleteMenu }
 				/>
--- packages/edit-navigation/src/components/menu-editor/use-create-missing-menu-items.js
+++ packages/edit-navigation/src/components/menu-editor/use-create-missing-menu-items.js
@@ -16,41 +16,37 @@ import { flattenBlocks } from './helpers';
  * 1) limit the amount of requests processed at the same time
  * 2) save the menu only after all requests are finalized
  *
- * @param {Object} menuItemsRef Ref holding a mapping from clientId to it's related menuItem
  * @return {function(*=): void} Function registering it's argument to be called once all
  * 													    currently processed menuItems are created.
  */
-export default function useCreateMissingMenuItems( menuItemsRef ) {
+export default function useCreateMissingMenuItems() {
 	const promiseQueueRef = useRef( new PromiseQueue() );
 	const enqueuedBlocksIds = useRef( [] );
-	const createMissingMenuItems = useCallback(
-		( blocks ) => {
-			for ( const { clientId, name } of flattenBlocks( blocks ) ) {
-				// No need to create menuItems for the wrapping navigation block
-				if ( name === 'core/navigation' ) {
-					continue;
-				}
-				// Menu item was already created
-				if ( clientId in menuItemsRef.current ) {
-					continue;
-				}
-				// Already in the queue
-				if ( enqueuedBlocksIds.current.includes( clientId ) ) {
-					continue;
-				}
-				enqueuedBlocksIds.current.push( clientId );
-				promiseQueueRef.current.enqueue( () =>
-					createDraftMenuItem( clientId ).then( ( menuItem ) => {
-						menuItemsRef.current[ clientId ] = menuItem;
-						enqueuedBlocksIds.current.splice(
-							enqueuedBlocksIds.current.indexOf( clientId )
-						);
-					} )
-				);
+	const createMissingMenuItems = ( blocks, menuItemsRef ) => {
+		for ( const { clientId, name } of flattenBlocks( blocks ) ) {
+			// No need to create menuItems for the wrapping navigation block
+			if ( name === 'core/navigation' ) {
+				continue;
 			}
-		},
-		[ menuItemsRef.current ]
-	);
+			// Menu item was already created
+			if ( clientId in menuItemsRef.current ) {
+				continue;
+			}
+			// Already in the queue
+			if ( enqueuedBlocksIds.current.includes( clientId ) ) {
+				continue;
+			}
+			enqueuedBlocksIds.current.push( clientId );
+			promiseQueueRef.current.enqueue( () =>
+				createDraftMenuItem( clientId ).then( ( menuItem ) => {
+					menuItemsRef.current[ clientId ] = menuItem;
+					enqueuedBlocksIds.current.splice(
+						enqueuedBlocksIds.current.indexOf( clientId )
+					);
+				} )
+			);
+		}
+	};
 	const onCreated = useCallback(
 		( callback ) => promiseQueueRef.current.then( callback ),
 		[ promiseQueueRef.current ]
