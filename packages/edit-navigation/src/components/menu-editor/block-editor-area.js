e5a625e339df5789836b125ffebc65c2729aee2c
Adam Zieliński <adam@adamziel.com>
2020-06-03 16:54:35 +0200

    Remove the second save button (from the footer)

--- packages/edit-navigation/src/components/menu-editor/block-editor-area.js
+++ packages/edit-navigation/src/components/menu-editor/block-editor-area.js
@@ -97,9 +97,6 @@ export default function BlockEditorArea( {
 			</CardBody>
 			<CardFooter>
 				<DeleteMenuButton menuId={ menuId } onDelete={ onDeleteMenu } />
-				<Button isPrimary onClick={ saveBlocks }>
-					{ __( 'Save navigation' ) }
-				</Button>
 			</CardFooter>
 		</Card>
 	);
