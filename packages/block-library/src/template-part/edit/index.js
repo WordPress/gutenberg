58be38dead7b29385f44a1165e0e8d0b09f8b9bb
epiqueras <epiquerass@gmail.com>
2020-06-03 17:47:38 -0700

    Template Part: Don't set post ID until the first edit.

--- packages/block-library/src/template-part/edit/index.js
+++ packages/block-library/src/template-part/edit/index.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { useRef, useEffect } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { EntityProvider } from '@wordpress/core-data';
 
 /**
@@ -14,6 +15,7 @@ import TemplatePartPlaceholder from './placeholder';
 export default function TemplatePartEdit( {
 	attributes: { postId: _postId, slug, theme },
 	setAttributes,
+	clientId,
 } ) {
 	const initialPostId = useRef( _postId );
 	const initialSlug = useRef( slug );
@@ -22,17 +24,27 @@ export default function TemplatePartEdit( {
 	// Resolve the post ID if not set, and load its post.
 	const postId = useTemplatePartPost( _postId, slug, theme );
 
-	// Set the post ID, once found, so that edits persist.
+	// Set the post ID, once found, so that edits persist,
+	// but wait until the third inner blocks change,
+	// because the first 2 are just the template part
+	// content loading.
+	const innerBlocks = useSelect(
+		( select ) => select( 'core/block-editor' ).getBlocks( clientId ),
+		[ clientId ]
+	);
+	const blockChanges = useRef( 0 );
 	useEffect( () => {
+		if ( blockChanges.current < 4 ) blockChanges.current++;
+
 		if (
+			blockChanges.current === 3 &&
 			( initialPostId.current === undefined ||
 				initialPostId.current === null ) &&
 			postId !== undefined &&
 			postId !== null
-		) {
+		)
 			setAttributes( { postId } );
-		}
-	}, [ postId ] );
+	}, [ innerBlocks ] );
 
 	if ( postId ) {
 		// Part of a template file, post ID already resolved.
