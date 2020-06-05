8b949bf8bea871a493637f68c17a36df70e263ba
Jorge Costa <jorge.costa@automattic.com>
2020-06-05 09:05:14 +0100

    Add link color picking in global styles (#22722)

--- packages/block-editor/src/hooks/color.js
+++ packages/block-editor/src/hooks/color.js
@@ -34,6 +34,16 @@ export const COLOR_SUPPORT_KEY = '__experimentalColor';
 const hasColorSupport = ( blockType ) =>
 	Platform.OS === 'web' && hasBlockSupport( blockType, COLOR_SUPPORT_KEY );
 
+const hasLinkColorSupport = ( blockType ) => {
+	if ( Platform.OS !== 'web' ) {
+		return false;
+	}
+
+	const colorSupport = getBlockSupport( blockType, COLOR_SUPPORT_KEY );
+
+	return isObject( colorSupport ) && !! colorSupport.linkColor;
+};
+
 const hasGradientSupport = ( blockType ) => {
 	if ( Platform.OS !== 'web' ) {
 		return false;
@@ -121,6 +131,7 @@ export function addSaveProps( props, blockType, attributes ) {
 				backgroundColor ||
 				style?.color?.background ||
 				( hasGradient && ( gradient || style?.color?.gradient ) ),
+			'has-link-color': style?.color?.link,
 		}
 	);
 	props.className = newClassName ? newClassName : undefined;
@@ -150,6 +161,15 @@ export function addEditProps( settings ) {
 
 	return settings;
 }
+
+const getLinkColorFromAttributeValue = ( colors, value ) => {
+	const attributeParsed = /var:preset\|color\|(.+)/.exec( value );
+	if ( attributeParsed && attributeParsed[ 1 ] ) {
+		return getColorObjectByAttributeValues( colors, attributeParsed[ 1 ] )
+			.color;
+	}
+	return value;
+};
 
 /**
  * Inspector control panel containing the color related configuration
@@ -245,6 +265,21 @@ export function ColorEdit( props ) {
 		};
 	};
 
+	const onChangeLinkColor = ( value ) => {
+		const colorObject = getColorObjectByColorValue( colors, value );
+		props.setAttributes( {
+			style: {
+				...props.attributes.style,
+				color: {
+					...props.attributes.style?.color,
+					link: colorObject?.slug
+						? `var:preset|color|${ colorObject.slug }`
+						: value,
+				},
+			},
+		} );
+	};
+
 	return (
 		<ColorPanel
 			enableContrastChecking={
@@ -275,6 +310,20 @@ export function ColorEdit( props ) {
 						? onChangeGradient
 						: undefined,
 				},
+				...( hasLinkColorSupport( blockName )
+					? [
+							{
+								label: __( 'Link Color' ),
+								onColorChange: onChangeLinkColor,
+								colorValue: getLinkColorFromAttributeValue(
+									colors,
+									style?.color?.link
+								),
+								clearable: !! props.attributes.style?.color
+									?.link,
+							},
+					  ]
+					: [] ),
 			] }
 		/>
 	);
