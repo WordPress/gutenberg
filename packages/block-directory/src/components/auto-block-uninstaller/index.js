79e715abe70e2520424af4c30b8434b87b22a8cc
Kelly Dwan <ryelle@users.noreply.github.com>
2020-06-05 13:50:00 -0400

    Add new selectors to get used & unused block types
    This pulls `hasBlockType` out of the block uninstaller, and moves the logic into selectors so we can re-use it for the "blocks to install" list in pre-publish panel.

--- packages/block-directory/src/components/auto-block-uninstaller/index.js
+++ packages/block-directory/src/components/auto-block-uninstaller/index.js
@@ -5,57 +5,16 @@ import { unregisterBlockType } from '@wordpress/blocks';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
 
-/**
- * Check if a block list contains a specific block type. Recursively searches
- * through `innerBlocks` if they exist.
- *
- * @param {Object} blockType A block object to search for.
- * @param {Object[]} blocks  The list of blocks to look through.
- *
- * @return {boolean} Whether the blockType is found.
- */
-function hasBlockType( blockType, blocks = [] ) {
-	if ( ! blocks.length ) {
-		return false;
-	}
-	if ( blocks.some( ( { name } ) => name === blockType.name ) ) {
-		return true;
-	}
-	for ( let i = 0; i < blocks.length; i++ ) {
-		if ( hasBlockType( blockType, blocks[ i ].innerBlocks ) ) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
 export default function AutoBlockUninstaller() {
 	const { uninstallBlockType } = useDispatch( 'core/block-directory' );
-	const usedBlocks = useSelect( ( select ) =>
-		select( 'core/block-editor' ).getBlocks()
-	);
 
 	const shouldRemoveBlockTypes = useSelect( ( select ) => {
 		const { isAutosavingPost, isSavingPost } = select( 'core/editor' );
 		return isSavingPost() && ! isAutosavingPost();
 	} );
 
-	const unusedBlockTypes = useSelect(
-		( select ) => {
-			const { getInstalledBlockTypes } = select( 'core/block-directory' );
-			const installedBlocks = getInstalledBlockTypes();
-
-			const blockList = [];
-			installedBlocks.forEach( ( blockType ) => {
-				if ( ! hasBlockType( blockType, usedBlocks ) ) {
-					blockList.push( blockType );
-				}
-			} );
-
-			return blockList;
-		},
-		[ usedBlocks ]
+	const unusedBlockTypes = useSelect( ( select ) =>
+		select( 'core/block-directory' ).getUnusedBlockTypes()
 	);
 
 	useEffect( () => {
