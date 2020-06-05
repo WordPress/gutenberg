7aa6dad3e82bbb9acf2c2faf7042cdbfcff60bc9
Alex Lende <ajlende@gmail.com>
2020-06-05 17:46:57 -0600

    Add batch image edit /apply endpoint

--- lib/class-wp-rest-image-editor-controller.php
+++ lib/class-wp-rest-image-editor-controller.php
@@ -116,6 +116,72 @@ class WP_REST_Image_Editor_Controller extends WP_REST_Controller {
 				),
 			)
 		);
+
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base . '/apply',
+			array(
+				array(
+					'methods'             => WP_REST_Server::EDITABLE,
+					'callback'            => array( $this, 'apply_edits' ),
+					'permission_callback' => array( $this, 'permission_callback' ),
+					'args'                => array(
+						array(
+							'type'       => 'object',
+							'properties' => array(
+								'crop'   => array(
+									'type'       => 'object',
+									'properties' => array(
+										'x' => array(
+											'type'     => 'integer',
+											'minimum'  => 0,
+											'required' => true,
+										),
+										'y' => array(
+											'type'     => 'integer',
+											'minimum'  => 0,
+											'required' => true,
+										),
+										'w' => array(
+											'type'     => 'integer',
+											'minimum'  => 1,
+											'required' => true,
+										),
+										'h' => array(
+											'type'     => 'integer',
+											'minimum'  => 1,
+											'required' => true,
+										),
+									),
+								),
+								'rotate' => array(
+									'type'       => 'object',
+									'properties' => array(
+										'angle' => array(
+											'type'     => 'integer',
+											'required' => true,
+										),
+									),
+								),
+								'flip'   => array(
+									'type'       => 'object',
+									'properties' => array(
+										'horizontal' => array(
+											'type'     => 'boolean',
+											'required' => true,
+										),
+										'vertical'   => array(
+											'type'     => 'boolean',
+											'required' => true,
+										),
+									),
+								),
+							),
+						),
+					),
+				),
+			)
+		);
 	}
 
 	/**
@@ -178,5 +244,102 @@ class WP_REST_Image_Editor_Controller extends WP_REST_Controller {
 		$modifier = new Image_Editor_Crop( $request['crop_x'], $request['crop_y'], $request['crop_width'], $request['crop_height'] );
 
 		return $this->editor->modify_image( $request['media_id'], $modifier );
+	}
+
+	/**
+	 * Applies all edits in one go.
+	 *
+	 * @since 7.x ?
+	 * @access public
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return array|WP_Error If successful image JSON for the modified image, otherwise a WP_Error.
+	 */
+	public function apply_edits( $request ) {
+		require_once ABSPATH . 'wp-admin/includes/image.php';
+
+		$params = $request->get_params();
+
+		$media_id = $params['media_id'];
+
+		// Get image information.
+		$attachment_info = wp_get_attachment_metadata( $media_id );
+		$media_url       = wp_get_attachment_image_url( $media_id, 'original' );
+
+		if ( ! $attachment_info || ! $media_url ) {
+			return new WP_Error( 'unknown', 'Unable to get meta information for file' );
+		}
+
+		$meta = array( 'original_name' => basename( $media_url ) );
+
+		if ( isset( $attachment_info['richimage'] ) ) {
+			$meta = array_merge( $meta, $attachment_info['richimage'] );
+		}
+
+		// Try and load the image itself.
+		$image_path = get_attached_file( $media_id );
+		if ( empty( $image_path ) ) {
+			return new WP_Error( 'fileunknown', 'Unable to find original media file' );
+		}
+
+		$image_editor = wp_get_image_editor( $image_path );
+		if ( ! $image_editor->load() ) {
+			return new WP_Error( 'fileload', 'Unable to load original media file' );
+		}
+
+		// Finally apply the modifications.
+		if ( isset( $params['crop'] ) ) {
+			$image_editor->crop(
+				$params['crop']['x'],
+				$params['crop']['y'],
+				$params['crop']['w'],
+				$params['crop']['h']
+			);
+		}
+		if ( isset( $params['rotate'] ) ) {
+			$image_editor->rotate( $params['rotate']['angle'] );
+		}
+		if ( isset( $params['flip'] ) ) {
+			$image_editor->flip( $params['flip']['horizontal'], $params['flip']['vertical'] );
+		}
+
+		// TODO: Generate filename based on edits.
+		$target_file = 'edited-' . $meta['original_name'];
+
+		$filename = rtrim( dirname( $image_path ), '/' ) . '/' . $target_file;
+
+		// Save to disk.
+		$saved = $image_editor->save( $filename );
+
+		if ( is_wp_error( $saved ) ) {
+			return $saved;
+		}
+
+		// Update attachment details.
+		$attachment_post = array(
+			'guid'           => $saved['path'],
+			'post_mime_type' => $saved['mime-type'],
+			'post_title'     => pathinfo( $target_file, PATHINFO_FILENAME ),
+			'post_content'   => '',
+			'post_status'    => 'inherit',
+		);
+
+		// Add this as an attachment.
+		$attachment_id = wp_insert_attachment( $attachment_post, $saved['path'], 0 );
+		if ( 0 === $attachment_id ) {
+			return new WP_Error( 'attachment', 'Unable to add image as attachment' );
+		}
+
+		// Generate thumbnails.
+		$metadata = wp_generate_attachment_metadata( $attachment_id, $saved['path'] );
+
+		$metadata['richimage'] = $meta;
+
+		wp_update_attachment_metadata( $attachment_id, $metadata );
+
+		return array(
+			'media_id' => $attachment_id,
+			'url'      => wp_get_attachment_image_url( $attachment_id, 'original' ),
+		);
 	}
 }
