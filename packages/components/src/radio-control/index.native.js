4acba69cb97ff5f39cf4e1fa90d132d4b30f8d3f
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-06-08 14:13:24 +0200

    Simplify radio control

--- packages/components/src/radio-control/index.native.js
+++ packages/components/src/radio-control/index.native.js
@@ -3,31 +3,16 @@
  */
 import RadioCell from '../mobile/bottom-sheet/radio-cell';
 
-function RadioControl( {
-	help,
-	instanceId,
-	className,
-	onChange,
-	selected,
-	options = [],
-	...props
-} ) {
-	const id = `inspector-radio-control-${ instanceId }`;
-
+function RadioControl( { onChange, selected, options = [], ...props } ) {
 	return (
 		<>
 			{ options.map( ( option, index ) => {
 				return (
 					<RadioCell
 						label={ option.label }
-						id={ id }
-						help={ help }
-						className={ className }
-						aria-describedby={ !! help ? id + '__help' : undefined }
 						onPress={ () => onChange( option.value ) }
 						selected={ option.value === selected }
 						key={ `${ option.value }-${ index }` }
-						separatorType={ 'fullWidth' }
 						{ ...props }
 					/>
 				);
