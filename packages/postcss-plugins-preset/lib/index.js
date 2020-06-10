f5a2149a5232886e79bc41fc5e5e4b08050ffa1a
Riad Benguella <benguella@gmail.com>
2020-06-10 11:25:12 +0100

    Update the theme colors to rely on CSS variables

--- packages/postcss-plugins-preset/lib/index.js
+++ packages/postcss-plugins-preset/lib/index.js
@@ -4,6 +4,7 @@
 const { adminColorSchemes } = require( '@wordpress/base-styles' );
 
 module.exports = [
+	require( 'postcss-custom-properties' )(),
 	require( '@wordpress/postcss-themes' )( adminColorSchemes ),
 	require( 'autoprefixer' )( { grid: true } ),
 	require( 'postcss-color-function' ),
