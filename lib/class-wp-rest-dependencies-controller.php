654d41677e24c02dc01ad832163d4c984f43b174
spacedmonkey <jon@spacedmonkey.co.uk>
2020-06-11 00:40:41 +0100

    Normalize data.

--- lib/class-wp-rest-dependencies-controller.php
+++ lib/class-wp-rest-dependencies-controller.php
@@ -142,8 +142,27 @@ class WP_REST_Dependencies_Controller extends WP_REST_Controller {
 	 */
 	public function prepare_item_for_response( $dependency, $request ) {
 		$dependency->url = $this->get_url( $dependency->src, $dependency->ver, $dependency->handle );
-		$response        = rest_ensure_response( (array) $dependency );
-		$dependencies    = $this->prepare_links( $dependency );
+
+		$fields = $this->get_fields_for_response( $request );
+		$data   = array();
+
+		$schema = $this->get_item_schema();
+		foreach ( $schema['properties'] as $key => $attr ) {
+			if ( rest_is_field_included( $key, $fields ) ) {
+				if ( property_exists( $dependency, $key ) ) {
+					$field = $dependency->$key;
+				} elseif ( array_key_exists( 'default', $attr ) ) {
+					$field = $attr['default'];
+				} else {
+					$field = '';
+				}
+				$data[ $key ] = rest_sanitize_value_from_schema( $field, $attr );
+			}
+		}
+
+		$response = rest_ensure_response( $data );
+
+		$dependencies = $this->prepare_links( $dependency );
 		$response->add_links( $dependencies );
 
 		return $response;
@@ -210,15 +229,13 @@ class WP_REST_Dependencies_Controller extends WP_REST_Controller {
 	 * @return array
 	 */
 	public function get_collection_params() {
-		$query_params                       = parent::get_collection_params();
-		$query_params['context']['default'] = 'view';
-
-		$query_params['dependency'] = array(
-			'description' => __( 'Dependency.', 'gutenberg' ),
-			'type'        => 'string',
+		return array(
+			'context'    => $this->get_context_param( array( 'default' => 'view' ) ),
+			'dependency' => array(
+				'description' => __( 'Dependency.', 'gutenberg' ),
+				'type'        => 'string',
+			),
 		);
-
-		return $query_params;
 	}
 
 	/**
@@ -283,6 +300,98 @@ class WP_REST_Dependencies_Controller extends WP_REST_Controller {
 		$handles        = array_filter( $handles );
 
 		return $handles;
+	}
+
+	/**
+	 * Retrieves the dependenies schema, conforming to JSON Schema.
+	 *
+	 * @return array Item schema data.
+	 */
+	public function get_item_schema() {
+		if ( $this->schema ) {
+			return $this->add_additional_fields_schema( $this->schema );
+		}
+
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => $this->rest_base,
+			'type'       => 'object',
+			'properties' => array(
+				'handle'            => array(
+					'description' => __( 'The handle name.', 'gutenberg' ),
+					'type'        => 'string',
+					'default'     => '',
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'src'               => array(
+					'description' => __( 'The handle source.', 'gutenberg' ),
+					'type'        => array( 'boolean', 'string' ),
+					'default'     => false,
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'url'               => array(
+					'description' => __( 'The handle url.', 'gutenberg' ),
+					'type'        => array( 'boolean', 'string' ),
+					'format'      => 'uri',
+					'default'     => false,
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'args'              => array(
+					'description' => __( 'Additional arguments for the handle.', 'gutenberg' ),
+					'type'        => array( 'null', 'integer' ),
+					'default'     => null,
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'ver'               => array(
+					'description' => __( 'The handle version.', 'gutenberg' ),
+					'type'        => array( 'string', 'boolean' ),
+					'default'     => false,
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'extra'             => array(
+					'description'          => __( 'Extra data to supply to the handle.', 'gutenberg' ),
+					'type'                 => 'object',
+					'properties'           => array(),
+					'additionalProperties' => true,
+					'default'              => array(),
+					'context'              => array( 'embed', 'view', 'edit' ),
+					'readonly'             => true,
+				),
+				'textdomain'        => array(
+					'description' => __( 'Translation textdomain set for this dependency.', 'gutenberg' ),
+					'type'        => 'string',
+					'default'     => '',
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'translations_path' => array(
+					'description' => __( 'Translation path set for this dependency.', 'gutenberg' ),
+					'type'        => 'string',
+					'default'     => '',
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'deps'              => array(
+					'description' => __( 'An array of handle dependencies.', 'gutenberg' ),
+					'type'        => 'array',
+					'items'       => array(
+						'type' => 'string',
+					),
+					'default'     => array(),
+					'context'     => array( 'embed', 'view', 'edit' ),
+					'readonly'    => true,
+				),
+			),
+		);
+
+		$this->schema = $schema;
+
+		return $this->add_additional_fields_schema( $this->schema );
 	}
 
 }
