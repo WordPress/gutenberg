7aca5ff758ec54ac34f315c0d111bacbfcdaf80a
Josef Citrine <josef@citrine.dev>
2020-06-10 17:14:19 +0100

    Use React hooks for PanelBody component

--- packages/components/src/panel/body.js
+++ packages/components/src/panel/body.js
@@ -6,7 +6,7 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Component, forwardRef } from '@wordpress/element';
+import { forwardRef, useState } from '@wordpress/element';
 import { chevronUp, chevronDown } from '@wordpress/icons';
 
 /**
@@ -15,76 +15,69 @@ import { chevronUp, chevronDown } from '@wordpress/icons';
 import Button from '../button';
 import Icon from '../icon';
 
-export class PanelBody extends Component {
-	constructor( props ) {
-		super( ...arguments );
-		this.state = {
-			opened: props.initialOpen === undefined ? true : props.initialOpen,
-		};
-		this.toggle = this.toggle.bind( this );
-	}
+export function PanelBody( {
+	title,
+	children,
+	opened,
+	className,
+	icon,
+	forwardedRef,
+	initialOpen,
+	onToggle,
+} ) {
+	const [ isOpenedState, setOpenedState ] = useState(
+		initialOpen === undefined ? true : initialOpen
+	);
+	const isOpened = opened === undefined ? isOpenedState : opened;
 
-	toggle( event ) {
+	const classes = classnames( 'components-panel__body', className, {
+		'is-opened': isOpened,
+	} );
+
+	const toggle = ( event ) => {
 		event.preventDefault();
-		if ( this.props.opened === undefined ) {
-			this.setState( ( state ) => ( {
-				opened: ! state.opened,
-			} ) );
+		if ( opened === undefined ) {
+			setOpenedState( ! isOpenedState );
 		}
 
-		if ( this.props.onToggle ) {
-			this.props.onToggle();
+		if ( onToggle ) {
+			onToggle();
 		}
-	}
+	};
 
-	render() {
-		const {
-			title,
-			children,
-			opened,
-			className,
-			icon,
-			forwardedRef,
-		} = this.props;
-		const isOpened = opened === undefined ? this.state.opened : opened;
-		const classes = classnames( 'components-panel__body', className, {
-			'is-opened': isOpened,
-		} );
-
-		return (
-			<div className={ classes } ref={ forwardedRef }>
-				{ !! title && (
-					<h2 className="components-panel__body-title">
-						<Button
-							className="components-panel__body-toggle"
-							onClick={ this.toggle }
-							aria-expanded={ isOpened }
-						>
-							{ /*
-								Firefox + NVDA don't announce aria-expanded because the browser
-								repaints the whole element, so this wrapping span hides that.
-							*/ }
-							<span aria-hidden="true">
-								<Icon
-									className="components-panel__arrow"
-									icon={ isOpened ? chevronUp : chevronDown }
-								/>
-							</span>
-							{ title }
-							{ icon && (
-								<Icon
-									icon={ icon }
-									className="components-panel__icon"
-									size={ 20 }
-								/>
-							) }
-						</Button>
-					</h2>
-				) }
-				{ isOpened && children }
-			</div>
-		);
-	}
+	return (
+		<div className={ classes } ref={ forwardedRef }>
+			{ !! title && (
+				<h2 className="components-panel__body-title">
+					<Button
+						className="components-panel__body-toggle"
+						onClick={ toggle }
+						aria-expanded={ isOpened }
+					>
+						{ /*
+							Firefox + NVDA don't announce aria-expanded because the browser
+							repaints the whole element, so this wrapping span hides that.
+						*/ }
+						<span aria-hidden="true">
+							<Icon
+								className="components-panel__arrow"
+								icon={ isOpened ? chevronUp : chevronDown }
+							/>
+						</span>
+						{ title }
+						{ icon && (
+							<Icon
+								icon={ icon }
+								className="components-panel__icon"
+								size={ 20 }
+							/>
+						) }
+					</Button>
+				</h2>
+			) }
+			{ isOpened && children }
+		</div>
+	);
 }
 
 const forwardedPanelBody = ( props, ref ) => {
