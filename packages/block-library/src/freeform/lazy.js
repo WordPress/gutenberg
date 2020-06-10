4aea41280796a6084af937fda0e64c38b363ee56
Sara Marcondes <saram@fastmail.com>
2020-06-10 10:41:39 -0700

    Skip translation init if no dependencies were loaded

--- packages/block-library/src/freeform/lazy.js
+++ packages/block-library/src/freeform/lazy.js
@@ -3,28 +3,49 @@
  */
 import { useState, useEffect } from '@wordpress/element';
 
-const loadTinyMCEScripts = async () =>
-	window.wpTinyMCEOrderedScriptURIs.reduce(
-		async ( previousPromise, uri ) => {
+const alreadyLoadedURIs = new Set();
+
+const loadTinyMCEScripts = async () => {
+	let count;
+	await window.wpTinyMCEOrderedScriptURIs.reduce(
+		( previousPromise, uri ) => {
+			if ( alreadyLoadedURIs.has( uri ) ) {
+				// if the dependency has already been loaded, skip it
+				return previousPromise;
+			}
+
 			// we need to serially load depenendencies as each could depend on the previous
-			await previousPromise;
-			return new Promise( ( resolve, reject ) => {
-				const scriptElement = document.createElement( 'script' );
-				scriptElement.type = 'application/javascript';
-				scriptElement.src = uri;
-				scriptElement.onload = resolve;
-				scriptElement.onerror = reject;
-				document.head.appendChild( scriptElement );
-			} );
+			return previousPromise.then( () =>
+				new Promise( ( resolve, reject ) => {
+					const scriptElement = document.createElement( 'script' );
+					scriptElement.type = 'application/javascript';
+					scriptElement.src = uri;
+					scriptElement.onload = resolve;
+					scriptElement.onerror = reject;
+					document.head.appendChild( scriptElement );
+				} ).then( () => {
+					count++;
+					alreadyLoadedURIs.add( uri );
+				} )
+			);
 		},
 		Promise.resolve()
 	);
+	return count;
+};
 
 const LazyLoadTinyMCE = ( { children, placeholder } ) => {
 	const [ isLoaded, setIsLoaded ] = useState( false );
 	useEffect( () => {
-		loadTinyMCEScripts().then( () => {
-			window.wpMceTranslation();
+		loadTinyMCEScripts().then( ( loadedCount ) => {
+			if ( loadedCount > 0 ) {
+				/**
+				 * In the case when all the dependencies have already been loaded
+				 * for another instance of the classic block, we don't need to re-init
+				 * the translations, so we can safely skip it if the loaded count is 0.
+				 */
+				window.wpMceTranslation();
+			}
 			setIsLoaded( true );
 		} );
 	}, [] );
