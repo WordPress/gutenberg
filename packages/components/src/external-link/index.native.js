e3d0ec7c6ea5e220e3fa33c3bc9d1903448a0814
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-06-11 15:28:31 +0200

    [Gutenberg Mobile] Add External Link support to React Native

---
+++ packages/components/src/external-link/index.native.js
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+
+import { TouchableOpacity, Text, Linking } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { forwardRef } from '@wordpress/element';
+import { external, Icon } from '@wordpress/icons';
+
+export function ExternalLink( { href, children } ) {
+	return (
+		<TouchableOpacity
+			onPress={ () => Linking.openURL( href ) }
+			accessibilityLabel={ __( 'Open link in a browser' ) }
+		>
+			<Text>{ children }</Text>
+			<Icon icon={ external } />
+		</TouchableOpacity>
+	);
+}
+
+export default forwardRef( ExternalLink );
