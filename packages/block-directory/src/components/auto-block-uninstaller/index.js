a0112fd1fe470ae762ea22ed8f953c8b25b3074a
Kelly Dwan <ryelle@users.noreply.github.com>
2020-06-11 10:55:43 -0400

    Block Directory: Uninstall unused block types (#22918)
    * Add unused block uninstall code into its own component.
    
    * Tweak saving logic
    
    * Fix mocked API response in test
    
    * Add new selectors to get used & unused block types
    
    This pulls `hasBlockType` out of the block uninstaller, and moves the logic into selectors so we can re-use it for the "blocks to install" list in pre-publish panel.
    
    * Update variable name to reflect nested structure
    
    Co-authored-by: dufresnesteven <steve.dufresne@automattic.com>

---
+++ packages/block-directory/src/components/auto-block-uninstaller/index.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { unregisterBlockType } from '@wordpress/blocks';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+
+export default function AutoBlockUninstaller() {
+	const { uninstallBlockType } = useDispatch( 'core/block-directory' );
+
+	const shouldRemoveBlockTypes = useSelect( ( select ) => {
+		const { isAutosavingPost, isSavingPost } = select( 'core/editor' );
+		return isSavingPost() && ! isAutosavingPost();
+	} );
+
+	const unusedBlockTypes = useSelect( ( select ) =>
+		select( 'core/block-directory' ).getUnusedBlockTypes()
+	);
+
+	useEffect( () => {
+		if ( shouldRemoveBlockTypes && unusedBlockTypes.length ) {
+			unusedBlockTypes.forEach( ( blockType ) => {
+				uninstallBlockType( blockType );
+				unregisterBlockType( blockType.name );
+			} );
+		}
+	}, [ shouldRemoveBlockTypes ] );
+
+	return null;
+}
