ff03248619d3c876d5cfdd53433f247326534e39
Riad Benguella <benguella@gmail.com>
2020-06-15 10:39:25 +0100

    Update the CSS variables setup

--- packages/postcss-plugins-preset/lib/index.js
+++ packages/postcss-plugins-preset/lib/index.js
@@ -1,10 +1,4 @@
-/**
- * WordPress dependencies
- */
-const { adminColorSchemes } = require( '@wordpress/base-styles' );
-
 module.exports = [
 	require( 'postcss-custom-properties' )(),
-	require( '@wordpress/postcss-themes' )( adminColorSchemes ),
 	require( 'autoprefixer' )( { grid: true } ),
 ];
