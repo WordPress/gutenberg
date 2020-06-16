c7a96729fe90d166633947a354a09e92f95a07a9
Kishan Jasani <kishanjasani007@yahoo.in>
2020-06-16 15:49:41 +0530

    RichText: Add missing param docs for getTextContent()

--- packages/rich-text/src/get-text-content.js
+++ packages/rich-text/src/get-text-content.js
@@ -2,7 +2,8 @@
  * Get the textual content of a Rich Text value. This is similar to
  * `Element.textContent`.
  *
- * @param {Object} value Value to use.
+ * @param {Object} value      Value to use.
+ * @param {string} value.text Text contents.
  *
  * @return {string} The text content.
  */
