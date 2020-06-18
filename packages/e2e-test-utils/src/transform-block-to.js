5a65bae95a4fc3f667de446850c95ebc1ec502df
Riad Benguella <benguella@gmail.com>
2020-06-18 12:48:28 +0100

    Try understanding why tests are failing by taking screenshots

--- packages/e2e-test-utils/src/transform-block-to.js
+++ packages/e2e-test-utils/src/transform-block-to.js
@@ -1,4 +1,12 @@
 /**
+ * External dependencies
+ */
+import path from 'path';
+import os from 'os';
+import { v4 as uuid } from 'uuid';
+const childProcess = require( 'child_process' );
+
+/**
  * Internal dependencies
  */
 import { showBlockToolbar } from './show-block-toolbar';
@@ -29,8 +37,32 @@ export async function transformBlockTo( name ) {
 	await page.waitForXPath( xpath, { visible: true } );
 	await insertButton.click();
 
-	// Wait for the transformed block to appear.
-	const BLOCK_SELECTOR = '.block-editor-block-list__block';
-	const BLOCK_NAME_SELECTOR = `[data-title="${ name }"]`;
-	await page.waitForSelector( `${ BLOCK_SELECTOR }${ BLOCK_NAME_SELECTOR }` );
+	try {
+		// Wait for the transformed block to appear.
+		const BLOCK_SELECTOR = '.block-editor-block-list__block';
+		const BLOCK_NAME_SELECTOR = `[data-title="${ name }"]`;
+		await page.waitForSelector(
+			`${ BLOCK_SELECTOR }${ BLOCK_NAME_SELECTOR }`
+		);
+	} catch ( e ) {
+		const takeScreenshotAndUpload = async () => {
+			const filename = uuid();
+			const tmpFileName = path.join( os.tmpdir(), filename + '.png' );
+			await page.screenshot( {
+				path: tmpFileName,
+			} );
+			console.log(
+				childProcess
+					.execSync(
+						'curl --upload-file ' +
+							tmpFileName +
+							' https://transfer.sh/gutenberg.png'
+					)
+					.toString()
+			);
+		};
+
+		takeScreenshotAndUpload();
+		throw e;
+	}
 }
