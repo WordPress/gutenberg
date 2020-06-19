27eac24e4e6796d0668ae38156ebe8d1fd51273e
Adam Zieliński <adam@adamziel.com>
2020-06-19 11:07:51 +0200

    Add comments to exported members of utils.js

--- packages/edit-navigation/src/store/utils.js
+++ packages/edit-navigation/src/store/utils.js
@@ -1,8 +1,32 @@
+/**
+ * "Kind" of the navigation post.
+ *
+ * @type {string}
+ */
 export const KIND = 'root';
+
+/**
+ * "post type" of the navigation post.
+ *
+ * @type {string}
+ */
 export const POST_TYPE = 'postType';
+
+/**
+ * Builds an ID for a new navigation post.
+ *
+ * @param {number} menuId Menu id.
+ * @return {string} An ID.
+ */
 export const buildNavigationPostId = ( menuId ) =>
 	`navigation-post-${ menuId }`;
 
+/**
+ * Builds a query to resolve menu items.
+ *
+ * @param {number} menuId Menu id.
+ * @return {Object} Query.
+ */
 export function menuItemsQuery( menuId ) {
 	return { menus: menuId, per_page: -1 };
 }
