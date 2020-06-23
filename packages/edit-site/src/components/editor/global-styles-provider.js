4e96c1caa6f46f8c4f923ad20f73b662dda573cf
Andrés <andres.maneiro@automattic.com>
2020-06-23 11:26:39 +0200

    Edit entities when user interacts with controls

--- packages/edit-site/src/components/editor/global-styles-provider.js
+++ packages/edit-site/src/components/editor/global-styles-provider.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { createContext, useContext } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 
 const GlobalStylesContext = createContext( {
 	getFontSize: () => {},
@@ -14,7 +14,7 @@ const GlobalStylesContext = createContext( {
 export const useGlobalStylesContext = () => useContext( GlobalStylesContext );
 
 export default ( { children, entityId, baseStyles } ) => {
-	// Trigger entity retrieval.
+	const { editEntityRecord } = useDispatch( 'core' );
 	const userStyles = useSelect( ( select ) => {
 		const userData = select( 'core' ).getEntityRecord(
 			'postType',
@@ -25,18 +25,39 @@ export default ( { children, entityId, baseStyles } ) => {
 		return userData?.content?.raw ? JSON.parse( userData.content.raw ) : {};
 	} );
 
+	// Font Size getter & setter
 	const fromPx = ( value ) => +value?.replace( 'px', '' ) ?? null;
 	const getFontSize = ( blockName ) =>
 		fromPx( userStyles?.[ blockName ]?.styles?.typography?.fontSize ) ??
 		null;
-
 	const setFontSize = ( blockName, newValue ) =>
-		console.log( 'set font size' );
+		editEntityRecord( 'postType', 'wp_global_styles', entityId, {
+			content: JSON.stringify( {
+				[ blockName ]: {
+					styles: {
+						typography: {
+							fontSize: newValue,
+						},
+					},
+				},
+			} ),
+		} );
 
+	// Line height getter & setter
 	const getLineHeight = ( blockName ) =>
 		userStyles?.[ blockName ]?.styles?.typography?.lineHeight ?? null;
 	const setLineHeight = ( blockName, newValue ) =>
-		console.log( 'set line height' );
+		editEntityRecord( 'postType', 'wp_global_styles', entityId, {
+			content: JSON.stringify( {
+				[ blockName ]: {
+					styles: {
+						typography: {
+							lineHeight: newValue,
+						},
+					},
+				},
+			} ),
+		} );
 
 	return (
 		<GlobalStylesContext.Provider
