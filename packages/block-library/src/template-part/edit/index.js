01963018393dce6ff5c25ad30b77d625668cfa56
Addison-Stavlo <stavz01@gmail.com>
2020-06-23 17:54:18 -0400

    Add more visibility to template part

--- packages/block-library/src/template-part/edit/index.js
+++ packages/block-library/src/template-part/edit/index.js
@@ -4,6 +4,7 @@
 import { useRef, useEffect } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { BlockControls } from '@wordpress/block-editor';
+import { sprintf } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -17,6 +18,7 @@ export default function TemplatePartEdit( {
 	attributes: { postId: _postId, slug, theme },
 	setAttributes,
 	clientId,
+	isSelected,
 } ) {
 	const initialPostId = useRef( _postId );
 	const initialSlug = useRef( slug );
@@ -29,11 +31,18 @@ export default function TemplatePartEdit( {
 	// but wait until the third inner blocks change,
 	// because the first 2 are just the template part
 	// content loading.
-	const { innerBlocks } = useSelect(
+	const { innerBlocks, hasSelectedInnerBlock } = useSelect(
 		( select ) => {
-			const { getBlocks } = select( 'core/block-editor' );
+			const {
+				getBlocks,
+				hasSelectedInnerBlock: getHasSelectedInnerBlock,
+			} = select( 'core/block-editor' );
 			return {
 				innerBlocks: getBlocks( clientId ),
+				hasSelectedInnerBlock: getHasSelectedInnerBlock(
+					clientId,
+					true
+				),
 			};
 		},
 		[ clientId ]
@@ -60,18 +69,14 @@ export default function TemplatePartEdit( {
 	if ( postId ) {
 		// Part of a template file, post ID already resolved.
 		return (
-			<>
-				<BlockControls>
-					<TemplatePartNamePanel
-						postId={ postId }
-						setAttributes={ setAttributes }
-					/>
-				</BlockControls>
-				<TemplatePartInnerBlocks
-					postId={ postId }
-					hasInnerBlocks={ innerBlocks.length > 0 }
-				/>
-			</>
+			<TemplatePartEditor
+				postId={ postId }
+				slug={ slug }
+				setAttributes={ setAttributes }
+				isSelected={ isSelected }
+				hasSelectedInnerBlock={ hasSelectedInnerBlock }
+				innerBlocks={ innerBlocks }
+			/>
 		);
 	}
 	if ( ! initialSlug.current && ! initialTheme.current ) {
@@ -81,3 +86,80 @@ export default function TemplatePartEdit( {
 	// Part of a template file, post ID not resolved yet.
 	return null;
 }
+
+const TemplatePartEditor = ( {
+	postId,
+	setAttributes,
+	isSelected,
+	hasSelectedInnerBlock,
+	innerBlocks,
+	slug,
+} ) => {
+	const hasBeenSelectedRef = useRef( false );
+	const { createWarningNotice, removeNotice } = useDispatch( 'core/notices' );
+	const { toggleFeature } = useDispatch( 'core/edit-site' ) || {};
+	const isFocusModeActive = useSelect( ( select ) =>
+		select( 'core/edit-site' )?.isFeatureActive( 'focusMode' )
+	);
+
+	useEffect( () => {
+		// When the Template Part is first selected.
+		if (
+			! hasBeenSelectedRef.current &&
+			( isSelected || hasSelectedInnerBlock )
+		) {
+			createWarningNotice(
+				sprintf(
+					'You are editing %s.  Changes apply to everywhere this block is used.',
+					slug
+				),
+				{
+					type: 'snackbar',
+					isDismissable: true,
+					speak: true,
+					id: 'template-part-edit-warning',
+				}
+			);
+			// Enable spotlight mode in site editor.
+			if ( isFocusModeActive === false ) {
+				toggleFeature( 'focusMode' );
+			}
+		}
+		// When we deselect the Teamplate Part.
+		else if (
+			hasBeenSelectedRef.current &&
+			! ( isSelected || hasSelectedInnerBlock )
+		) {
+			removeNotice( 'template-part-edit-warning' );
+			// Disable spotlight mode in site editor.
+			if ( isFocusModeActive ) {
+				toggleFeature( 'focusMode' );
+			}
+		}
+		// Ensure spotlight mode is on when a Template Part is selected.
+		// Without this if we select footer then select header focus mode will be turned off.
+		else if (
+			isFocusModeActive === false &&
+			( isSelected || hasSelectedInnerBlock )
+		) {
+			toggleFeature( 'focusMode' );
+		}
+
+		hasBeenSelectedRef.current = isSelected || hasSelectedInnerBlock;
+	}, [ isSelected, hasSelectedInnerBlock, isFocusModeActive ] );
+	// Part of a template file, post ID already resolved.
+	return (
+		<>
+			<BlockControls>
+				<TemplatePartNamePanel
+					postId={ postId }
+					setAttributes={ setAttributes }
+				/>
+			</BlockControls>
+			<TemplatePartInnerBlocks
+				postId={ postId }
+				hasInnerBlocks={ innerBlocks.length > 0 }
+			/>
+		</>
+	);
+};
