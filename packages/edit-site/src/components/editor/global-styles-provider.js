6be29f118f9b2e40e0b47020165b76b69a6ba649
Andrés <andres.maneiro@automattic.com>
2020-06-23 12:01:17 +0200

    Use edited data to feed controls & no overwrite existing data

--- packages/edit-site/src/components/editor/global-styles-provider.js
+++ packages/edit-site/src/components/editor/global-styles-provider.js
@@ -16,13 +16,20 @@ export const useGlobalStylesContext = () => useContext( GlobalStylesContext );
 export default ( { children, entityId, stylesheetId, baseStyles } ) => {
 	const { editEntityRecord } = useDispatch( 'core' );
 	const userStyles = useSelect( ( select ) => {
-		const userData = select( 'core' ).getEntityRecord(
+		// Trigger entity retrieval
+		select( 'core' ).getEntityRecord(
 			'postType',
 			'wp_global_styles',
 			entityId
 		);
 
-		return userData?.content?.raw ? JSON.parse( userData.content.raw ) : {};
+		const userData = select( 'core' ).getEditedEntityRecord(
+			'postType',
+			'wp_global_styles',
+			entityId
+		);
+
+		return userData?.content ? JSON.parse( userData.content ) : {};
 	} );
 
 	console.log( 'base styles ', baseStyles );
@@ -31,15 +38,19 @@ export default ( { children, entityId, stylesheetId, baseStyles } ) => {
 	// Font Size getter & setter
 	const fromPx = ( value ) => +value?.replace( 'px', '' ) ?? null;
 	const toPx = ( value ) => value + 'px';
+
 	const getFontSize = ( blockName ) =>
 		fromPx( userStyles?.[ blockName ]?.styles?.typography?.fontSize ) ??
 		null;
+
 	const setFontSize = ( blockName, newValue ) =>
 		editEntityRecord( 'postType', 'wp_global_styles', entityId, {
 			content: JSON.stringify( {
+				...userStyles,
 				[ blockName ]: {
 					styles: {
 						typography: {
+							...userStyles[ blockName ].styles.typography,
 							fontSize: toPx( newValue ),
 						},
 					},
@@ -53,9 +64,11 @@ export default ( { children, entityId, stylesheetId, baseStyles } ) => {
 	const setLineHeight = ( blockName, newValue ) =>
 		editEntityRecord( 'postType', 'wp_global_styles', entityId, {
 			content: JSON.stringify( {
+				...userStyles,
 				[ blockName ]: {
 					styles: {
 						typography: {
+							...userStyles[ blockName ].styles.typography,
 							lineHeight: newValue,
 						},
 					},
