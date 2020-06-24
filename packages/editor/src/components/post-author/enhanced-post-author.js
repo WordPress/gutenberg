a649ad664c768719b53292b3d021d4ce7005767a
Adam Silverstein <adamjs@google.com>
2020-06-24 17:36:27 -0600

    Refresh the user list when the field value changes or the selection is changed.

--- packages/editor/src/components/post-author/enhanced-post-author.js
+++ packages/editor/src/components/post-author/enhanced-post-author.js
@@ -6,7 +6,7 @@ import { debounce } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { useState } from '@wordpress/element';
+import { useState, useMemo, useEffect } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
 /**
@@ -15,25 +15,24 @@ import apiFetch from '@wordpress/api-fetch';
 import ComboboxControl from '../../../../components/build/combobox-control/';
 
 function EnhancedPostAuthor( { postAuthor, id, authors, onUpdateAuthor } ) {
-	let initialAuthors = [];
-	authors.forEach( ( author ) => {
-		initialAuthors = [
-			...initialAuthors,
-			{
+
+	let initialAuthors = useMemo( () => {
+		return authors.map( ( author ) => {
+			return {
 				key: author.id,
 				name: author.name,
 				value: author.name,
 				id: author.id,
-			},
-		];
-	} );
+			};
+		} );
+	}, [ authors ] )
 
 	// Ensure the current author is included in the initial dropdown list.
 	let foundAuthor = initialAuthors.findIndex(
 		( author ) => postAuthor?.id === author.id
 	);
 
-	if ( postAuthor && foundAuthor < 0 ) {
+	if ( foundAuthor < 0 ) {
 		postAuthor.key = initialAuthors.length;
 		initialAuthors = [ postAuthor, ...initialAuthors ];
 		foundAuthor = 0;
@@ -52,14 +51,18 @@ function EnhancedPostAuthor( { postAuthor, id, authors, onUpdateAuthor } ) {
 	// The currently selected author.
 	const [ selectedAuthor, setSelectedAuthor ] = useState( postAuthor );
 
+	// The currently field value.
+	const [ fieldValue, setFieldValue ] = useState( postAuthor.name );
+
 	/**
 	 * Handle author selection.
 	 *
 	 * @param {Object} selectedItem The selected Author.
 	 */
 	const handleSelect = ( { selectedItem } ) => {
-		setSelectedAuthor( selectedItem );
+		setFieldValue( selectedItem.name );
 		onUpdateAuthor( selectedItem.id );
+		setSelectedAuthor( selectedItem );
 	};
 
 	/**
@@ -68,14 +71,18 @@ function EnhancedPostAuthor( { postAuthor, id, authors, onUpdateAuthor } ) {
 	 * @param {string} inputValue The current value of the input field.
 	 */
 	const handleKeydown = ( { inputValue } ) => {
-		if ( '' === inputValue ) {
-			// When the field is cleared, reset the author list to the original list.
+		setFieldValue( inputValue );
+	};
+
+	// Refresh the user list when the field value changes or the selection is changed.
+	useEffect( () => {
+		// When the field is cleared or unchanged, use the original author list.
+		if ( '' === fieldValue || fieldValue === selectedAuthor.name ) {
 			setAvailableAuthors( initialAuthors );
 		} else {
-			// Otherwise search for authors matching the input.
-			searchAuthors( inputValue );
+			searchAuthors( fieldValue );
 		}
-	};
+	}, [ fieldValue, selectedAuthor ] );
 
 	/**
 	 * Search for authors from the rest API.
@@ -96,7 +103,8 @@ function EnhancedPostAuthor( { postAuthor, id, authors, onUpdateAuthor } ) {
 			);
 			setIsLoadingSearchResult( false );
 		} );
-	}, 300 );
+	},
+	300 );
 
 	return (
 		<ComboboxControl
