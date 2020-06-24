650dbfa3754087ce99f76fc9010a169f2f4866d5
Dion Hulse (dd32) <contact@dd32.id.au>
2020-06-24 13:10:41 +1000

    Use a more specific matching pattern to only affect the `<script>` tags which we're interested in.
    This uses the full `<script>` produced by `WP_Scripts::do_item()` ignoring the Translations and before/after inline scripts, which allows for it to exclude `<script%` tags contained within the before/after html chunks.

--- lib/block-directory.php
+++ lib/block-directory.php
@@ -22,18 +22,24 @@ if (
 	/**
 	 * Add data attribute of handle to all script tags output in the wp-admin.
 	 *
-	 * @param string $tag    The `<script>` tag for the enqueued script.
-	 * @param string $handle The script's registered handle.
+	 * @param string $tag     The `<script>` tag for the enqueued script.
+	 * @param string $handle  The script's registered handle.
+	 * @param string $src_esc The script's pre-escaped registered src.
 	 *
-	 * @return string  Filter script tag.
+	 * @return string  Filtered script tag.
 	 */
-	function gutenberg_change_script_tag( $tag, $handle ) {
+	function gutenberg_change_script_tag( $tag, $handle, $src_esc ) {
 		if ( ! is_admin() ) {
 			return $tag;
 		}
-		$tag = str_replace( '<script ', sprintf( '<script data-handle="%s" ', esc_attr( $handle ) ), $tag );
+
+		$tag = str_replace(
+			sprintf( "<script src='%s'></script>", $src_esc ),
+			sprintf( "<script data-handle='%s' src='%s'></script>", esc_attr( $handle ), $src_esc ),
+			$tag
+		);
 
 		return $tag;
 	}
-	add_filter( 'script_loader_tag', 'gutenberg_change_script_tag', 10, 2 );
+	add_filter( 'script_loader_tag', 'gutenberg_change_script_tag', 1, 3 );
 }
