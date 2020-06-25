fdf52c3e87174c7b2a69042e22b6854e7e2b9c3a
Ella van Durpe <ella@vandurpe.com>
2020-06-25 20:37:55 +0300

    Fix move to different parent

--- packages/block-editor/src/components/block-draggable/index.js
+++ packages/block-editor/src/components/block-draggable/index.js
@@ -52,10 +52,7 @@ const BlockDraggable = ( {
 				getBlockRootClientId,
 				getTemplateLock,
 			} = select( 'core/block-editor' );
-			const rootClientId =
-				clientIds.length === 1
-					? getBlockRootClientId( clientIds[ 0 ] )
-					: null;
+			const rootClientId = getBlockRootClientId( clientIds[ 0 ] );
 			const templateLock = rootClientId
 				? getTemplateLock( rootClientId )
 				: null;
@@ -63,7 +60,7 @@ const BlockDraggable = ( {
 			return {
 				index: getBlockIndex( clientIds[ 0 ], rootClientId ),
 				srcRootClientId: rootClientId,
-				isDraggable: clientIds.length === 1 && 'all' !== templateLock,
+				isDraggable: 'all' !== templateLock,
 			};
 		},
 		[ clientIds ]
@@ -103,7 +100,7 @@ const BlockDraggable = ( {
 	const transferData = {
 		type: 'block',
 		srcIndex: index,
-		srcClientId: clientIds[ 0 ],
+		srcClientIds: clientIds,
 		srcRootClientId,
 	};
 
