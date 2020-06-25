36dc7236d54fb8e8aab75379bdb2307ada7c6e09
Daniel Richards <daniel.richards@automattic.com>
2020-06-25 15:33:01 +0800

    Use a percentage based calculation for scrolling when dragging blocks

--- packages/block-editor/src/components/block-draggable/use-scroll-when-dragging.js
+++ packages/block-editor/src/components/block-draggable/use-scroll-when-dragging.js
@@ -6,9 +6,9 @@ import { useCallback, useEffect, useRef } from '@wordpress/element';
 
 const SCROLL_INACTIVE_DISTANCE_PX = 50;
 const SCROLL_INTERVAL_MS = 25;
-const PIXELS_PER_SECOND_PER_DISTANCE = 5;
+const PIXELS_PER_SECOND_PER_PERCENTAGE = 1000;
 const VELOCITY_MULTIPLIER =
-	PIXELS_PER_SECOND_PER_DISTANCE * ( SCROLL_INTERVAL_MS / 1000 );
+	PIXELS_PER_SECOND_PER_PERCENTAGE * ( SCROLL_INTERVAL_MS / 1000 );
 
 /**
  * React hook that scrolls the scroll container when a block is being dragged.
@@ -60,15 +60,38 @@ export default function useScrollWhenDragging( dragElement ) {
 	);
 
 	const scrollOnDragOver = useCallback( ( event ) => {
-		const distanceY = event.clientY - dragStartY.current;
-		if ( distanceY > SCROLL_INACTIVE_DISTANCE_PX ) {
-			velocityY.current =
-				VELOCITY_MULTIPLIER *
-				( distanceY - SCROLL_INACTIVE_DISTANCE_PX );
-		} else if ( distanceY < -SCROLL_INACTIVE_DISTANCE_PX ) {
-			velocityY.current =
-				VELOCITY_MULTIPLIER *
-				( distanceY + SCROLL_INACTIVE_DISTANCE_PX );
+		const scrollParentHeight = scrollParentY.current.offsetHeight;
+
+		if ( event.clientY > dragStartY.current ) {
+			// User is dragging downwards.
+			const moveableDistance = Math.max(
+				scrollParentHeight -
+					dragStartY.current -
+					SCROLL_INACTIVE_DISTANCE_PX,
+				0
+			);
+			const dragDistance = Math.max(
+				event.clientY -
+					dragStartY.current -
+					SCROLL_INACTIVE_DISTANCE_PX,
+				0
+			);
+			const distancePercentage = dragDistance / moveableDistance;
+			velocityY.current = VELOCITY_MULTIPLIER * distancePercentage;
+		} else if ( event.clientY < dragStartY.current ) {
+			// User is dragging upwards.
+			const moveableDistance = Math.max(
+				dragStartY.current - SCROLL_INACTIVE_DISTANCE_PX,
+				0
+			);
+			const dragDistance = Math.max(
+				dragStartY.current -
+					event.clientY -
+					SCROLL_INACTIVE_DISTANCE_PX,
+				0
+			);
+			const distancePercentage = dragDistance / moveableDistance;
+			velocityY.current = -VELOCITY_MULTIPLIER * distancePercentage;
 		} else {
 			velocityY.current = 0;
 		}
