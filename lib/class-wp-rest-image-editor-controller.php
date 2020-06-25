d816481bfda6ed19917f2121960e2f9ad836d40b
Andrew Ozz <743931+azaozz@users.noreply.github.com>
2020-06-24 22:38:24 -0700

    Fix generating file names fro edited files, and use wp_unique_filename()
    Also some better errors and some cleanup of names.

--- lib/class-wp-rest-image-editor-controller.php
+++ lib/class-wp-rest-image-editor-controller.php
@@ -101,90 +101,134 @@ class WP_REST_Image_Editor_Controller extends WP_REST_Controller {
 	public function apply_edits( $request ) {
 		require_once ABSPATH . 'wp-admin/includes/image.php';
 
-		$params = $request->get_params();
+		$params        = $request->get_params();
+		$attachment_id = $params['media_id'];
 
-		$media_id = $params['media_id'];
+		// This also confirms the attachment is an image.
+		$image_file = wp_get_original_image_path( $attachment_id );
+		$image_meta = wp_get_attachment_metadata( $attachment_id );
 
-		// Get image information.
-		$attachment_info = wp_get_attachment_metadata( $media_id );
-		$media_url       = wp_get_attachment_image_url( $media_id, 'original' );
-
-		if ( ! $attachment_info || ! $media_url ) {
+		if ( ! $image_meta || ! $image_file ) {
 			return new WP_Error( 'rest_unknown_attachment', __( 'Unable to get meta information for file.', 'gutenberg' ), array( 'status' => 404 ) );
 		}
 
-		$meta = array( 'original_name' => basename( $media_url ) );
+		$image_editor = wp_get_image_editor( $image_file );
 
-		if ( isset( $attachment_info['richimage'] ) ) {
-			$meta = array_merge( $meta, $attachment_info['richimage'] );
+		if ( is_wp_error( $image_editor ) ) {
+			// This image cannot be edited.
+			return new WP_Error( 'rest_unknown_image_file_type', __( 'Unable to edit this image.', 'gutenberg' ), array( 'status' => 500 ) );
 		}
 
-		// Try and load the image itself.
-		$image_path = get_attached_file( $media_id );
-		if ( empty( $image_path ) ) {
-			return new WP_Error( 'rest_cannot_find_attached_file', __( 'Unable to find original media file.', 'gutenberg' ), array( 'status' => 500 ) );
+		if ( isset( $params['rotation'] ) ) { // TODO: use `rotate` instead?
+			// CW / CCW
+			$result = $image_editor->rotate( 0 - $params['rotation'] );
+
+			// TODO: return the "real" error message?
+			if ( is_wp_error( $result ) ) {
+				return new WP_Error( 'rest_image_rotation_failed', __( 'Unable to rotate this image.', 'gutenberg' ), array( 'status' => 500 ) );
+			}
 		}
 
-		$image_editor = wp_get_image_editor( $image_path );
-		if ( ! $image_editor->load() ) {
-			return new WP_Error( 'rest_cannot_load_editor', __( 'Unable to load original media file.', 'gutenberg' ), array( 'status' => 500 ) );
+		if ( isset( $params['x'], $params['y'], $params['width'], $params['height'] ) ) {
+			$size = $image_editor->get_size();
+
+			// Crop
+			$crop_x = round( ( $size['width'] * floatval( $params['x'] ) ) / 100.0 );
+			$crop_y = round( ( $size['height'] * floatval( $params['y'] ) ) / 100.0 );
+			$width  = round( ( $size['width'] * floatval( $params['width'] ) ) / 100.0 );
+			$height = round( ( $size['height'] * floatval( $params['height'] ) ) / 100.0 );
+
+			$result = $image_editor->crop( $crop_x, $crop_y, $width, $height );
+
+			if ( is_wp_error( $result ) ) {
+				return new WP_Error( 'rest_image_crop_failed', __( 'Unable to crop this image.', 'gutenberg' ), array( 'status' => 500 ) );
+			}
 		}
 
-		if ( isset( $params['rotation'] ) ) {
-			$image_editor->rotate( 0 - $params['rotation'] );
+		// Calculate the file name.
+		$image_ext  = pathinfo( $image_file, PATHINFO_EXTENSION );
+		$image_name = wp_basename( $image_file, ".{$image_ext}" );
+
+		// Do not append multiple `-edited` to the file name.
+		// The user may be editing a previously edited image.
+		if ( preg_match( '/-edited(-\d+)?$/', $image_name ) ) {
+			// Remove any `-1`, `-2`, etc. `wp_unique_filename()` will add the proper number.
+			$image_name = preg_replace( '/-edited(-\d+)?$/', '-edited', $image_name );
+		} else {
+			// Append `-edited` before the extension.
+			$image_name .= '-edited';
 		}
 
-		$size = $image_editor->get_size();
+		$filename = "{$image_name}.{$image_ext}";
 
-		// Finally apply the modifications.
-		$crop_x = round( ( $size['width'] * floatval( $params['x'] ) ) / 100.0 );
-		$crop_y = round( ( $size['height'] * floatval( $params['y'] ) ) / 100.0 );
-		$width  = round( ( $size['width'] * floatval( $params['width'] ) ) / 100.0 );
-		$height = round( ( $size['height'] * floatval( $params['height'] ) ) / 100.0 );
-		$image_editor->crop( $crop_x, $crop_y, $width, $height );
+		// Create the uploads sub-directory if needed.
+		$uploads = wp_upload_dir();
 
-		// TODO: Generate filename based on edits.
-		$target_file = 'edited-' . $meta['original_name'];
-
-		$filename = rtrim( dirname( $image_path ), '/' ) . '/' . $target_file;
+		// Tests the file basename in the (proper/new) upload directory.
+		$filename = wp_unique_filename( $uploads['path'], $filename );
 
 		// Save to disk.
-		$saved = $image_editor->save( $filename );
+		$saved = $image_editor->save( $uploads['path'] . "/$filename" );
 
 		if ( is_wp_error( $saved ) ) {
 			return $saved;
 		}
 
-		// Update attachment details.
+		// Create new attachment post.
+		// TODO: Add possibility to preset the content and excerpt, and maybe the title?
 		$attachment_post = array(
-			'guid'           => $saved['path'],
 			'post_mime_type' => $saved['mime-type'],
-			'post_title'     => pathinfo( $target_file, PATHINFO_FILENAME ),
+			'guid'           => $uploads['url'] . "/$filename",
+			'post_title'     => $filename,
 			'post_content'   => '',
-			'post_status'    => 'inherit',
 		);
 
-		// Add this as an attachment.
-		$attachment_id = wp_insert_attachment( $attachment_post, $saved['path'], 0, true );
+		$new_attachment_id = wp_insert_attachment( $attachment_post, $saved['path'], 0, true );
 
-		if ( is_wp_error( $attachment_id ) ) {
-			if ( 'db_update_error' === $attachment_id->get_error_code() ) {
-				$attachment_id->add_data( array( 'status' => 500 ) );
+		if ( is_wp_error( $new_attachment_id ) ) {
+			if ( 'db_update_error' === $new_attachment_id->get_error_code() ) {
+				$new_attachment_id->add_data( array( 'status' => 500 ) );
 			} else {
-				$attachment_id->add_data( array( 'status' => 400 ) );
+				$new_attachment_id->add_data( array( 'status' => 400 ) );
 			}
 
-			return $attachment_id;
+			return $new_attachment_id;
 		}
 
-		// Generate thumbnails.
-		$metadata = wp_generate_attachment_metadata( $attachment_id, $saved['path'] );
+		// Generate image sub-sizes and meta.
+		$new_image_meta = wp_generate_attachment_metadata( $new_attachment_id, $saved['path'] );
 
-		$metadata['richimage'] = $meta;
+		// TODO: Keep?
+		// Copy the EXIF metadata from the original attachment if not generated for the edited image.
+		// (EXIF is removed when using GD.)
+		if ( ! empty( $image_meta['image_meta'] ) ) {
+			$empty_image_meta = true;
 
-		wp_update_attachment_metadata( $attachment_id, $metadata );
+			if ( isset( $new_image_meta['image_meta'] ) && is_array( $new_image_meta['image_meta'] ) ) {
+				$empty_image_meta = empty( array_filter( array_values( $new_image_meta['image_meta'] ) ) );
+			}
 
-		$path     = '/wp/v2/media/' . $attachment_id;
+			if ( $empty_image_meta ) {
+				$new_image_meta['image_meta'] = $image_meta['image_meta'];
+			}
+		}
+
+		// Reset orientation. At this point the image is edited and orientation is correct.
+		if ( ! empty( $new_image_meta['image_meta']['orientation'] ) ) {
+			$new_image_meta['image_meta']['orientation'] = 1;
+		}
+
+		// TODO: reference to the parent image: attachment_id? path? Anything else?
+		// The attachment_id may change if the site is exported and imported.
+		$new_image_meta['parent_image'] = array(
+			'attachment_id' => $attachment_id,
+			// Path to the originally uploaded image file relative to the uploads directory.
+			'file'          => _wp_relative_upload_path( $image_file ),
+		);
+
+		wp_update_attachment_metadata( $new_attachment_id, $new_image_meta );
+
+		$path     = '/wp/v2/media/' . $new_attachment_id;
 		$response = rest_do_request( $path );
 
 		if ( ! $response->is_error() ) {
