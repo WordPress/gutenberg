cf5f738b088d01014fc60f98a5fe9a02cfa160be
Enrique Sánchez <enrique.sanchez.garcia@gmail.com>
2020-07-01 16:56:20 -0500

    Remove Title Case from pattern titles

--- lib/patterns/text-two-columns-with-images.php
+++ lib/patterns/text-two-columns-with-images.php
@@ -6,7 +6,7 @@
  */
 
 return array(
-	'title'      => __( 'Two Columns of Text with Images', 'gutenberg' ),
+	'title'      => __( 'Two columns of text with images', 'gutenberg' ),
 	'categories' => array( 'columns' ),
 	'content'    => "<!-- wp:group -->\n<div class=\"wp-block-group\"><div class=\"wp-block-group__inner-container\"><!-- wp:columns -->\n<div class=\"wp-block-columns\"><!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:image -->\n<figure class=\"wp-block-image\"><img alt=\"\"/></figure>\n<!-- /wp:image -->\n\n<!-- wp:paragraph -->\n<p>This language, which they did not understand, and the extraordinary appearance of the knight, increased their laughter, which also increased his displeasure, and he would probably have shown it in a less civil way, but for the timely arrival of the innkeeper.</p>\n<!-- /wp:paragraph --></div>\n<!-- /wp:column -->\n\n<!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:image -->\n<figure class=\"wp-block-image\"><img alt=\"\"/></figure>\n<!-- /wp:image -->\n\n<!-- wp:paragraph -->\n<p>He was a man whose burden of fat inclined him to peace and quietness, yet when he observed such a strange disguise of human shape in his old armour and equipage, he could hardly forbear laughter; but having the fear of such a warlike appearance before his eyes, he resolved to give him good words, and therefore accosted him civilly.</p>\n<!-- /wp:paragraph --></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns --></div></div>\n<!-- /wp:group -->",
 );
