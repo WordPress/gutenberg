edd1cb8da73be4819547cb79aa0aa87316c86626
Daniel Richards <daniel.richards@automattic.com>
2020-07-01 13:42:57 +0800

    Fix copy/pasted function name

--- packages/block-library/src/navigation-link/index.php
+++ packages/block-library/src/navigation-link/index.php
@@ -126,7 +126,7 @@ function render_block_core_navigation_link( $content, $block ) {
  * @uses render_block_core_navigation()
  * @throws WP_Error An WP_Error exception parsing the block definition.
  */
-function register_block_core_navigation() {
+function register_block_core_navigation_link() {
 	register_block_type_from_metadata(
 		__DIR__ . '/navigation-link'
 	);
