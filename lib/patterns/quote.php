8a9563896abf56220a5d3476d617456b387e95f8
Riad Benguella <benguella@gmail.com>
2020-07-02 10:00:50 +0100

    Translate patterns

--- lib/patterns/quote.php
+++ lib/patterns/quote.php
@@ -7,7 +7,7 @@
 
 return array(
 	'title'         => __( 'Quote', 'gutenberg' ),
-	'content'       => "<!-- wp:quote {\"className\":\"is-style-large\"} -->\n<blockquote class=\"wp-block-quote is-style-large\"><p>\"Sir Knight, if your worship be disposed to alight, you will fail of nothing here but of a bed as for all other accommodations, you may be supplied to your mind.</p><cite>— Don Quixote</cite></blockquote>\n<!-- /wp:quote -->",
+	'content'       => "<!-- wp:quote {\"className\":\"is-style-large\"} -->\n<blockquote class=\"wp-block-quote is-style-large\"><p>" .__( '"Sir Knight, if your worship be disposed to alight, you will fail of nothing here but of a bed as for all other accommodations, you may be supplied to your mind.', 'gutenberg' ) . "</p><cite>" . __( '— Don Quixote', 'gutenberg' ) . "</cite></blockquote>\n<!-- /wp:quote -->",
 	'viewportWidth' => 800,
 	'categories'    => array( 'text' ),
 );
