5416b7f6fb2ab067eeb36d2d1e5b68dd1ef8fafa
Ben Dwyer <ben@scruffian.com>
2020-07-06 13:50:36 +0100

    Make convert to block buttons a separate component

---
+++ packages/block-library/src/classic/convert-to-blocks-button.js
@@ -0,0 +1,35 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { BlockControls } from '@wordpress/block-editor';
+import { Toolbar, ToolbarButton } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { rawHandler, serialize } from '@wordpress/blocks';
+
+const ConvertToBlocksButton = ( { clientId } ) => {
+	const { replaceBlocks } = useDispatch( 'core/block-editor' );
+	const block = useSelect( ( select ) => {
+		return select( 'core/block-editor' ).getBlock( clientId );
+	} );
+
+	return (
+		<BlockControls>
+			<Toolbar>
+				<ToolbarButton
+					title={ __( 'Convert to Blocks' ) }
+					onClick={ () =>
+						replaceBlocks(
+							block.clientId,
+							rawHandler( { HTML: serialize( block ) } )
+						)
+					}
+				>
+					{ __( 'Convert to Blocks' ) }
+				</ToolbarButton>
+			</Toolbar>
+		</BlockControls>
+	);
+};
+
+export default ConvertToBlocksButton;
--- packages/block-library/src/classic/edit.js
+++ packages/block-library/src/classic/edit.js
@@ -11,9 +11,6 @@ import { __, _x } from '@wordpress/i18n';
 import { BACKSPACE, DELETE, F10, isKeyboardEvent } from '@wordpress/keycodes';
 import { BlockControls } from '@wordpress/block-editor';
 import { Toolbar, ToolbarButton } from '@wordpress/components';
-import { compose } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data';
-import { rawHandler, serialize } from '@wordpress/blocks';
 
 const { wp } = window;
 
@@ -33,7 +30,7 @@ function isTmceEmpty( editor ) {
 	return /^\n?$/.test( body.innerText || body.textContent );
 }
 
-class ClassicEdit extends Component {
+export class ClassicEdit extends Component {
 	constructor( props ) {
 		super( props );
 		this.initialize = this.initialize.bind( this );
@@ -271,19 +268,3 @@ class ClassicEdit extends Component {
 		/* eslint-enable jsx-a11y/no-static-element-interactions */
 	}
 }
-
-export default compose(
-	withSelect( ( select, { clientId } ) => {
-		const block = select( 'core/block-editor' ).getBlock( clientId );
-		return {
-			block,
-		};
-	} ),
-	withDispatch( ( dispatch, { block } ) => ( {
-		convertToBlocks: () =>
-			dispatch( 'core/block-editor' ).replaceBlocks(
-				block.clientId,
-				rawHandler( { HTML: serialize( block ) } )
-			),
-	} ) )
-)( ClassicEdit );
