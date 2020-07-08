628694748e7570ad8a9bc6421db25242f34016b5
Konstantin Obenland <obenland@gmx.de>
2020-07-08 10:08:17 -0700

    Account for timezones without abbreviations
    Converts `-03` to `UTC-3` for timzones like Argentina/Buenos_Aires.
    
    Also updates innline comments.

--- packages/components/src/date-time/timezone.js
+++ packages/components/src/date-time/timezone.js
@@ -15,20 +15,20 @@ import Tooltip from '../tooltip';
 const TimeZone = () => {
 	const { timezone } = getDateSettings();
 
-	// getTimezoneOffset() returns in minutes and opposite how you'd expect
-	// America/Los_Angeles = 420 and not -7
+	// Convert timezone offset to hours.
 	const userTimezoneOffset = -1 * ( new Date().getTimezoneOffset() / 60 );
 
-	// System timezone and user timezone match, nothing needed
-	// compare as numbers because it comes over as string
+	// System timezone and user timezone match, nothing needed.
+	// Compare as numbers because it comes over as string.
 	if ( Number( timezone.offset ) === userTimezoneOffset ) {
 		return null;
 	}
 
 	const offsetSymbol = timezone.offset >= 0 ? '+' : '';
-	const zoneAbbr = timezone.abbr
-		? timezone.abbr
-		: `UTC${ offsetSymbol }${ timezone.offset }`;
+	const zoneAbbr =
+		'' !== timezone.abbr && isNaN( timezone.abbr )
+			? timezone.abbr
+			: `UTC${ offsetSymbol }${ timezone.offset }`;
 
 	const timezoneDetail =
 		'UTC' === timezone.string
@@ -37,7 +37,7 @@ const TimeZone = () => {
 
 	return (
 		<Tooltip position="top center" text={ timezoneDetail }>
-			<div className="components-datetime__timezone"> { zoneAbbr } </div>
+			<div className="components-datetime__timezone">{ zoneAbbr }</div>
 		</Tooltip>
 	);
 };
