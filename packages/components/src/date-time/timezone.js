5488c66bf3ad247a94bd1ef7b05b20a3304c241d
Marcus Kazmierczak <marcus@mkaz.com>
2020-07-07 19:32:50 -0700

    Experiment setting the timezone abbr from PHP
    The system timezone already comes from PHP site, specifically from
    wp.date.setSettings call in wp-includes/script-loader.php
    
    This uses the get_option() to grab the string and setting and surfaces
    them to the wp-date package.
    
    This commit adds an additional property called `abbr` which is the short
    timezone such as EDT or PST, this is calculated using the PHP date and
    time functions.

--- packages/components/src/date-time/timezone.js
+++ packages/components/src/date-time/timezone.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import moment from 'moment';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
@@ -19,31 +14,30 @@ import Tooltip from '../tooltip';
  */
 const TimeZone = () => {
 	const { timezone } = getDateSettings();
-	const userTimezone = moment.tz.guess( true );
 
-	if (
-		! timezone.string ||
-		timezone.string === userTimezone ||
-		'WP' === userTimezone
-	) {
+	// getTimezoneOffset() returns in minutes and opposite how you'd expect
+	// America/Los_Angeles = 420 and not -7
+	const userTimezoneOffset = -1 * ( new Date().getTimezoneOffset() / 60 );
+
+	// System timezone and user timezone match, nothing needed
+	// compare as numbers because it comes over as string
+	if ( Number( timezone.offset ) === userTimezoneOffset ) {
 		return null;
 	}
 
-	const offset =
-		timezone.offset > 0 ? '+' + timezone.offset : timezone.offset;
-	const zoneAbbr = moment.tz( new Date(), timezone.string ).zoneAbbr();
+	const offsetSymbol = timezone.offset >= 0 ? '+' : '';
+	const zoneAbbr = timezone.abbr
+		? timezone.abbr
+		: `UTC${ offsetSymbol }${ timezone.offset }`;
+
 	const timezoneDetail =
 		'UTC' === timezone.string
 			? __( 'Coordinated Universal Time' )
-			: '(UTC' + offset + ') ' + timezone.string.replace( '_', ' ' );
-	const timezoneAbbr =
-		'' !== timezone.string && isNaN( zoneAbbr ) ? zoneAbbr : 'UTC' + offset;
+			: `(${ zoneAbbr }) ${ timezone.string.replace( '_', ' ' ) }`;
 
 	return (
 		<Tooltip position="top center" text={ timezoneDetail }>
-			<div className="components-datetime__timezone">
-				{ timezoneAbbr }
-			</div>
+			<div className="components-datetime__timezone"> { zoneAbbr } </div>
 		</Tooltip>
 	);
 };
