9ca8c70393670d59f5c7118f3814741ede3137ee
Daniel Richards <daniel.richards@automattic.com>
2020-07-08 12:47:20 +0800

    Use correct orientation prop for block mover button

--- packages/block-editor/src/components/block-navigation/block.js
+++ packages/block-editor/src/components/block-navigation/block.js
@@ -120,7 +120,7 @@ export default function BlockNavigationBlock( {
 						<TreeGridItem>
 							{ ( { ref, tabIndex, onFocus } ) => (
 								<BlockMoverUpButton
-									__experimentalOrientation="vertical"
+									orientation="vertical"
 									clientIds={ [ clientId ] }
 									ref={ ref }
 									tabIndex={ tabIndex }
@@ -131,7 +131,7 @@ export default function BlockNavigationBlock( {
 						<TreeGridItem>
 							{ ( { ref, tabIndex, onFocus } ) => (
 								<BlockMoverDownButton
-									__experimentalOrientation="vertical"
+									orientation="vertical"
 									clientIds={ [ clientId ] }
 									ref={ ref }
 									tabIndex={ tabIndex }
