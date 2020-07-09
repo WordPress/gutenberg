b8601093abaaec78e657c7a07760050357096c5e
Ben Dwyer <ben@scruffian.com>
2020-07-09 16:19:35 +0100

    Linter fixes

--- packages/block-library/src/site-logo/index.php
+++ packages/block-library/src/site-logo/index.php
@@ -8,6 +8,8 @@
 /**
  * Renders the `core/site-logo` block on the server.
  *
+ * @param array $attributes The block attributes.
+ *
  * @return string The render.
  */
 function render_block_core_site_logo( $attributes ) {
@@ -20,13 +22,12 @@ function render_block_core_site_logo( $attributes ) {
 			return $image;
 		}
 		$height = floatval( $attributes['width'] ) / ( floatval( $image[1] ) / floatval( $image[2] ) );
-        return array( $image[0], intval( $attributes['width'] ), intval( $height ) );
-    };
+		return array( $image[0], intval( $attributes['width'] ), intval( $height ) );
+	};
 
 	add_filter( 'wp_get_attachment_image_src', $adjust_width_height_filter );
-	//var_dump( $attributes );die;
 	$custom_logo = get_custom_logo();
-	$class_name = "wp-block-site-logo";
+	$class_name  = 'wp-block-site-logo';
 	if ( ! empty( $attributes['className'] ) ) {
 		$class_name .= " {$attributes['className']}";
 	}
@@ -58,11 +59,25 @@ function register_block_core_site_logo() {
 }
 add_action( 'init', 'register_block_core_site_logo' );
 
+/**
+ * Overrides the custom logo with a site logo, if the option is set.
+ *
+ * @param string $custom_logo The custom logo set by a theme.
+ *
+ * @return string The site logo if set.
+ */
 function override_custom_logo_theme_mod( $custom_logo ) {
 	$sitelogo = get_option( 'sitelogo' );
 	return false === $sitelogo ? $custom_logo : $sitelogo;
 }
 
+/**
+ * Syncs the site logo with the theme modified logo.
+ *
+ * @param string $custom_logo The custom logo set by a theme.
+ *
+ * @return string The custom logo.
+ */
 function sync_site_logo_to_theme_mod( $custom_logo ) {
 	if ( $custom_logo ) {
 		update_option( 'sitelogo', $custom_logo );
@@ -70,6 +85,9 @@ function sync_site_logo_to_theme_mod( $custom_logo ) {
 	return $custom_logo;
 }
 
+/**
+ * Register a core site setting for a site logo
+ */
 function register_block_core_site_logo_setting() {
 	register_setting(
 		'general',
