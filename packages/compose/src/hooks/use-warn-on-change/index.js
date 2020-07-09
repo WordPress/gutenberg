be1fb65543ce2dfcaefff5d7b7b9d997b30d327a
Riad Benguella <benguella@gmail.com>
2020-07-09 16:13:44 +0100

    tweak
    Co-authored-by: Zebulan Stanphill <zebulanstanphill@protonmail.com>

--- packages/compose/src/hooks/use-warn-on-change/index.js
+++ packages/compose/src/hooks/use-warn-on-change/index.js
@@ -6,7 +6,7 @@ import usePrevious from '../use-previous';
 function useWarnOnChange( object, prefix = 'Change detection' ) {
 	const previousValues = usePrevious( object );
 
-	Object.entries( previousValues || [] ).forEach( ( [ key, value ] ) => {
+	Object.entries( previousValues ?? [] ).forEach( ( [ key, value ] ) => {
 		if ( value !== object[ key ] ) {
 			// eslint-disable-next-line no-console
 			console.warn(
