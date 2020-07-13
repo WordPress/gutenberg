82d74e3f850e1f9e44a1e355f2a5ec3adba31d3e
Copons <Copons@users.noreply.github.com>
2020-07-13 18:25:50 +0100

    Add icon and keywords

--- packages/block-library/src/site-tagline/index.js
+++ packages/block-library/src/site-tagline/index.js
@@ -2,19 +2,20 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { info as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import metadata from './block.json';
 import edit from './edit';
+import icon from './icon';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
 	title: __( 'Site Tagline' ),
+	keywords: [ __( 'description' ) ],
 	icon,
 	edit,
 };
