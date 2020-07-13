2eaee1c6ed8cbf0a25e2a1fdfc4c6e4f0021f59e
Robert Anderson <robert@noisysocks.com>
2020-07-13 20:11:31 +1000

    Autocomplete: Fix closing slash inserter with ESC (#23859)

--- packages/components/src/autocomplete/index.js
+++ packages/components/src/autocomplete/index.js
@@ -409,7 +409,7 @@ export class Autocomplete extends Component {
 				break;
 
 			case ESCAPE:
-				this.setState( { autocompleter: null } );
+				this.setState( { autocompleter: null, AutocompleterUI: null } );
 				break;
 
 			case ENTER:
@@ -428,6 +428,7 @@ export class Autocomplete extends Component {
 		// Any handled keycode should prevent original behavior. This relies on
 		// the early return in the default case.
 		event.preventDefault();
+		event.stopPropagation();
 	}
 
 	componentDidUpdate( prevProps ) {
