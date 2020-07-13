fbf517d141e0373a2e6feb92c8c0e4d0a9f58dca
Matt Chowning <matt.chowning@automattic.com>
2020-07-13 16:57:42 -0400

    Simplify GutenbergProps object

--- packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
+++ packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
@@ -1,58 +1,34 @@
 package org.wordpress.mobile.WPAndroidGlue
 
 import android.os.Bundle
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class GutenbergProps(
     val enableMentions: Boolean,
     val enableUnsupportedBlockEditor: Boolean,
     val localeSlug: String,
     val postType: String,
     val editorTheme: Bundle?,
-    var translations: Bundle? = null,
-    var isDarkMode: Boolean? = null,
-    var htmlModeEnabled: Boolean? = null
-) : Parcelable {
-
-    constructor(
-        enableMentions: Boolean,
-        enableUnsupportedBlockEditor: Boolean,
-        localeSlug: String,
-        postType: String,
-        editorTheme: Bundle?
-    ) : this(
-            enableMentions = enableMentions,
-            enableUnsupportedBlockEditor = enableUnsupportedBlockEditor,
-            localeSlug = localeSlug,
-            postType = postType,
-            editorTheme = editorTheme,
-            translations = null,
-            isDarkMode = null,
-            htmlModeEnabled = null
-    )
-
-    private fun getCapabilities() = Bundle().apply {
-        putBoolean(PROP_CAPABILITIES_MENTIONS, enableMentions)
-        putBoolean( PROP_CAPABILITIES_UNSUPPORTED_BLOCK_EDITOR, enableUnsupportedBlockEditor)
-    }
+    val translations: Bundle,
+    val isDarkMode: Boolean,
+    val htmlModeEnabled: Boolean
+) {
 
     fun getInitialProps(bundle: Bundle?) = (bundle ?: Bundle()).apply {
         putString(PROP_INITIAL_DATA, "")
         putString(PROP_INITIAL_TITLE, "")
         putString(PROP_LOCALE, localeSlug)
         putString(PROP_POST_TYPE, postType)
-        putBundle(PROP_TRANSLATIONS, requireNotNull(translations))
-        putBoolean(PROP_INITIAL_HTML_MODE_ENABLED, requireNotNull(htmlModeEnabled))
-        putBundle(PROP_CAPABILITIES, getCapabilities())
+        putBundle(PROP_TRANSLATIONS, translations)
+        putBoolean(PROP_INITIAL_HTML_MODE_ENABLED, htmlModeEnabled)
 
-        editorTheme?.getSerializable(PROP_COLORS)?.let { colors ->
-            putSerializable(PROP_COLORS, colors)
-        }
+        putBundle(PROP_CAPABILITIES, Bundle().apply {
+            putBoolean(PROP_CAPABILITIES_MENTIONS, enableMentions)
+            putBoolean(PROP_CAPABILITIES_UNSUPPORTED_BLOCK_EDITOR, enableUnsupportedBlockEditor)
+        })
 
-        editorTheme?.getSerializable(PROP_GRADIENTS)?.let { gradients ->
-            putSerializable(PROP_GRADIENTS, gradients)
+        editorTheme?.also {
+            getSerializable(PROP_COLORS)?.let { putSerializable(PROP_COLORS, it) }
+            getSerializable(PROP_GRADIENTS)?.let { putSerializable(PROP_GRADIENTS, it) }
         }
     }
 
