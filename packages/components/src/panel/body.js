bb00ad891db9937862b16867dcebd2a4d830ea86
Q <hello@jonquach.com>
2020-07-14 15:09:40 -0400

    Panel: Improve scroll view handling when expanding (#23327)
    * Add scrollIntoView interaction for PanelBody
    
    Refactor interactions with Reakit/Disclosure.
    Add custom hooks for ref and rendering handling.
    
    * Adjust PanelBody story to include scrollIntoView example
    
    * Adjust bottom padding for Layout sidebar
    
    * Allow for opened prop to control disclosure state
    
    * Update PanelBody tests
    
    * Update snapshots
    
    * Fix tests for PanelColorSettings + Update snapshots
    
    * Add new PanelBody props to README
    
    * Add react-merge-refs dependency
    
    * Use react-merge-refs util
    
    * Update test snapshots
    
    * Remove disableSmoothScrollIntoView prop from PanelBody
    
    * Update snapshots
    
    * Remove focusable prop
    
    * Remove use-combined-refs - No longer needed
    
    * Remove onToggle from useUpdateEffect deps array
    
    * Adjust onToggle callback handling
    
    * Remove Reakit/Disclosure integration (for now)
    
    * Update PanelBody tests
    
    * Update snapshots
    
    * Fix initialOpen handling for PanelBody
    
    * Remove unnecessary act wrappers in panel body tests

--- packages/components/src/panel/body.js
+++ packages/components/src/panel/body.js
@@ -2,11 +2,14 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import { noop } from 'lodash';
+import mergeRefs from 'react-merge-refs';
 
 /**
  * WordPress dependencies
  */
-import { Component, forwardRef } from '@wordpress/element';
+import { useReducedMotion } from '@wordpress/compose';
+import { forwardRef, useRef } from '@wordpress/element';
 import { chevronUp, chevronDown } from '@wordpress/icons';
 
 /**
@@ -14,82 +17,99 @@ import { chevronUp, chevronDown } from '@wordpress/icons';
  */
 import Button from '../button';
 import Icon from '../icon';
+import { useControlledState, useUpdateEffect } from '../utils';
 
-export class PanelBody extends Component {
-	constructor( props ) {
-		super( ...arguments );
-		this.state = {
-			opened: props.initialOpen === undefined ? true : props.initialOpen,
-		};
-		this.toggle = this.toggle.bind( this );
-	}
+export function PanelBody(
+	{ children, className, icon, initialOpen, onToggle = noop, opened, title },
+	ref
+) {
+	const [ isOpened, setIsOpened ] = useControlledState( opened, {
+		initial: initialOpen === undefined ? true : initialOpen,
+	} );
+	const nodeRef = useRef();
 
-	toggle( event ) {
+	// Defaults to 'smooth' scrolling
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+	const scrollBehavior = useReducedMotion() ? 'auto' : 'smooth';
+
+	const handleOnToggle = ( event ) => {
 		event.preventDefault();
-		if ( this.props.opened === undefined ) {
-			this.setState( ( state ) => ( {
-				opened: ! state.opened,
-			} ) );
+		const next = ! isOpened;
+		setIsOpened( next );
+		onToggle( next );
+	};
+
+	// Runs after initial render
+	useUpdateEffect( () => {
+		if ( isOpened ) {
+			/*
+			 * Scrolls the content into view when visible.
+			 * This improves the UX when there are multiple stacking <PanelBody />
+			 * components in a scrollable container.
+			 */
+			if ( nodeRef.current.scrollIntoView ) {
+				nodeRef.current.scrollIntoView( {
+					inline: 'nearest',
+					block: 'nearest',
+					behavior: scrollBehavior,
+				} );
+			}
 		}
+	}, [ isOpened, scrollBehavior ] );
 
-		if ( this.props.onToggle ) {
-			this.props.onToggle();
-		}
-	}
+	const classes = classnames( 'components-panel__body', className, {
+		'is-opened': isOpened,
+	} );
 
-	render() {
-		const {
-			title,
-			children,
-			opened,
-			className,
-			icon,
-			forwardedRef,
-		} = this.props;
-		const isOpened = opened === undefined ? this.state.opened : opened;
-		const classes = classnames( 'components-panel__body', className, {
-			'is-opened': isOpened,
-		} );
-
-		return (
-			<div className={ classes } ref={ forwardedRef }>
-				{ !! title && (
-					<h2 className="components-panel__body-title">
-						<Button
-							className="components-panel__body-toggle"
-							onClick={ this.toggle }
-							aria-expanded={ isOpened }
-						>
-							{ /*
-								Firefox + NVDA don't announce aria-expanded because the browser
-								repaints the whole element, so this wrapping span hides that.
-							*/ }
-							<span aria-hidden="true">
-								<Icon
-									className="components-panel__arrow"
-									icon={ isOpened ? chevronUp : chevronDown }
-								/>
-							</span>
-							{ title }
-							{ icon && (
-								<Icon
-									icon={ icon }
-									className="components-panel__icon"
-									size={ 20 }
-								/>
-							) }
-						</Button>
-					</h2>
-				) }
-				{ isOpened && children }
-			</div>
-		);
-	}
+	return (
+		<div className={ classes } ref={ mergeRefs( [ nodeRef, ref ] ) }>
+			<PanelBodyTitle
+				icon={ icon }
+				isOpened={ isOpened }
+				onClick={ handleOnToggle }
+				title={ title }
+			/>
+			{ isOpened && children }
+		</div>
+	);
 }
 
-const forwardedPanelBody = ( props, ref ) => {
-	return <PanelBody { ...props } forwardedRef={ ref } />;
-};
-forwardedPanelBody.displayName = 'PanelBody';
+const PanelBodyTitle = forwardRef(
+	( { isOpened, icon, title, ...props }, ref ) => {
+		if ( ! title ) return null;
 
-export default forwardRef( forwardedPanelBody );
+		return (
+			<h2 className="components-panel__body-title">
+				<Button
+					className="components-panel__body-toggle"
+					ref={ ref }
+					{ ...props }
+				>
+					{ /*
+					Firefox + NVDA don't announce aria-expanded because the browser
+					repaints the whole element, so this wrapping span hides that.
+				*/ }
+					<span aria-hidden="true">
+						<Icon
+							className="components-panel__arrow"
+							icon={ isOpened ? chevronUp : chevronDown }
+						/>
+					</span>
+					{ title }
+					{ icon && (
+						<Icon
+							icon={ icon }
+							className="components-panel__icon"
+							size={ 20 }
+						/>
+					) }
+				</Button>
+			</h2>
+		);
+	}
+);
+
+const ForwardedComponent = forwardRef( PanelBody );
+ForwardedComponent.displayName = 'PanelBody';
+
+export default ForwardedComponent;
