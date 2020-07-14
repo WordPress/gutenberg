71df44a1c41d98732dcf35cdd64432159cb2cd28
ntsekouras <ntsekouras@outlook.com>
2020-07-14 14:37:29 +0300

    remove lodash debounce

--- packages/block-library/src/classic/edit.js
+++ packages/block-library/src/classic/edit.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { debounce } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import { BlockControls } from '@wordpress/block-editor';
@@ -138,19 +133,16 @@ export default class ClassicEdit extends Component {
 			bookmark = null;
 		} );
 
-		editor.on(
-			'Paste Change input Undo Redo',
-			debounce( () => {
-				const value = editor.getContent();
+		editor.on( 'Paste Change input Undo Redo', () => {
+			const value = editor.getContent();
 
-				if ( value !== editor._lastChange ) {
-					editor._lastChange = value;
-					setAttributes( {
-						content: value,
-					} );
-				}
-			}, 250 )
-		);
+			if ( value !== editor._lastChange ) {
+				editor._lastChange = value;
+				setAttributes( {
+					content: value,
+				} );
+			}
+		} );
 
 		editor.on( 'keydown', ( event ) => {
 			if ( isKeyboardEvent.primary( event, 'z' ) ) {
