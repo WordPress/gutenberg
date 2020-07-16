6006e5bdfef7bb5e31e8d0f69a21d27ce1125c45
retrofox <rdsuarez@gmail.com>
2020-07-16 11:38:36 -0300

    core: add blocks-style to hooks/

---
+++ packages/block-editor/src/hooks/block-styles.js
@@ -0,0 +1,80 @@
+/**
+ * WordPress dependencies
+ */
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent, compose } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
+import { Fragment } from '@wordpress/element';
+import { hasBlockSupport, getBlockType } from '@wordpress/blocks';
+import { PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import InspectorControls from '../components/inspector-controls';
+import BlockStyles from '../components/block-styles';
+import DefaultStylePicker from '../components/default-style-picker';
+
+export const withInspectorControls = createHigherOrderComponent(
+	compose( [
+		withSelect( ( select ) => {
+			const { getSelectedBlockClientId, getBlockName } = select(
+				'core/block-editor'
+			);
+			const { getBlockStyles } = select( 'core/blocks' );
+			const selectedBlockClientId = getSelectedBlockClientId();
+			const selectedBlockName =
+				selectedBlockClientId && getBlockName( selectedBlockClientId );
+			const blockType =
+				selectedBlockClientId && getBlockType( selectedBlockName );
+			const blockStyles =
+				selectedBlockClientId && getBlockStyles( selectedBlockName );
+			return {
+				blockType,
+				selectedBlockClientId,
+				hasBlockStyles: blockStyles && blockStyles.length > 0,
+			};
+		} ),
+
+		( WrappedComponent ) => ( props ) => {
+			if ( ! props?.hasBlockStyles ) {
+				return <WrappedComponent { ...props } />;
+			}
+
+			const { selectedBlockClientId, blockType } = props;
+
+			return (
+				<Fragment>
+					<WrappedComponent { ...props } />
+
+					<InspectorControls>
+						<div>
+							<PanelBody title={ __( 'Styles' ) }>
+								<BlockStyles
+									clientId={ selectedBlockClientId }
+								/>
+								{ hasBlockSupport(
+									blockType.name,
+									'defaultStylePicker',
+									true
+								) && (
+									<DefaultStylePicker
+										blockName={ blockType.name }
+									/>
+								) }
+							</PanelBody>
+						</div>
+					</InspectorControls>
+				</Fragment>
+			);
+		},
+	] ),
+	'withInspectorControls'
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'core/editor/block-styles/with-inspector-controls',
+	withInspectorControls
+);
