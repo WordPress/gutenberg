c059ef3f95f2b05aa3c34f68ac8691962815e404
Adam Zieliński <adam@adamziel.com>
2020-07-16 16:47:23 +0200

    Roll back obsolete changes

--- packages/editor/src/components/autosave-monitor/test/index.js
+++ packages/editor/src/components/autosave-monitor/test/index.js
@@ -1,117 +1,136 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 
 /**
  * Internal dependencies
  */
 import { AutosaveMonitor } from '../';
 
-const mockThrottledSave = jest.fn();
-const mockCancelSave = jest.fn();
-jest.mock( '../use-throttled-autosave', () => {
-	return () => [ () => mockThrottledSave(), () => mockCancelSave() ];
-} );
-
 describe( 'AutosaveMonitor', () => {
+	const toggleTimer = jest.fn();
+	let wrapper;
 	beforeEach( () => {
-		mockThrottledSave.mockClear();
-		mockCancelSave.mockClear();
+		toggleTimer.mockClear();
+		wrapper = shallow( <AutosaveMonitor />, {
+			lifecycleExperimental: true,
+		} );
+
+		wrapper.instance().toggleTimer = toggleTimer;
 	} );
 
-	it( 'should schedule an autosave when dirty and saveable on initial render', () => {
-		render( <AutosaveMonitor isDirty={ true } isAutosaveable={ true } /> );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 0 );
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 1 );
+	describe( '#componentDidUpdate()', () => {
+		it( 'should start autosave timer when having become dirty and saveable', () => {
+			wrapper.setProps( { isDirty: true, isAutosaveable: true } );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( true );
+		} );
+
+		it( 'should restart autosave timer when edits reference changes', () => {
+			const beforeReference = [];
+			const afterReference = [];
+			wrapper.setProps( {
+				isDirty: true,
+				isAutosaveable: true,
+				editsReference: beforeReference,
+			} );
+			toggleTimer.mockClear();
+
+			wrapper.setProps( {
+				isDirty: true,
+				isAutosaveable: true,
+				editsReference: beforeReference,
+			} );
+
+			expect( toggleTimer ).not.toHaveBeenCalled();
+
+			wrapper.setProps( {
+				isDirty: true,
+				isAutosaveable: true,
+				editsReference: afterReference,
+			} );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( true );
+		} );
+
+		it( 'should stop autosave timer when the autosave is up to date', () => {
+			wrapper.setProps( { isDirty: true, isAutosaveable: false } );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+		} );
+
+		it( 'should stop autosave timer when having become dirty but not autosaveable', () => {
+			wrapper.setProps( { isDirty: true, isAutosaveable: false } );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+		} );
+
+		it( 'should stop autosave timer when having become not dirty', () => {
+			wrapper.setProps( { isDirty: true } );
+			toggleTimer.mockClear();
+			wrapper.setProps( { isDirty: false } );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+		} );
+
+		it( 'should stop autosave timer when having become not autosaveable', () => {
+			wrapper.setProps( { isDirty: true } );
+			toggleTimer.mockClear();
+			wrapper.setProps( { isAutosaveable: false } );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+		} );
+
+		it( 'should avoid scheduling autosave if still dirty but already autosaved for edits', () => {
+			// Explanation: When a published post is autosaved, it's still in a
+			// dirty state since the edits are not saved to the post until the
+			// user clicks "Update". To avoid recurring autosaves, ensure that
+			// an edit has occurred since the last autosave had completed.
+
+			const beforeReference = [];
+			const afterReference = [];
+
+			// A post is non-dirty while autosave is in-flight.
+			wrapper.setProps( {
+				isDirty: false,
+				isAutosaving: true,
+				isAutosaveable: true,
+				editsReference: beforeReference,
+			} );
+			toggleTimer.mockClear();
+			wrapper.setProps( {
+				isDirty: true,
+				isAutosaving: false,
+				isAutosaveable: true,
+				editsReference: beforeReference,
+			} );
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+
+			// Once edit occurs after autosave, resume scheduling.
+			wrapper.setProps( {
+				isDirty: true,
+				isAutosaving: false,
+				isAutosaveable: true,
+				editsReference: afterReference,
+			} );
+
+			expect( toggleTimer.mock.calls[ 1 ][ 0 ] ).toBe( true );
+		} );
 	} );
 
-	it( 'should schedule autosave when having become dirty and saveable', async () => {
-		render( <AutosaveMonitor isDirty={ true } isAutosaveable={ true } /> );
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 1 );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 0 );
+	describe( '#componentWillUnmount()', () => {
+		it( 'should stop autosave timer', () => {
+			wrapper.unmount();
+
+			expect( toggleTimer ).toHaveBeenCalledWith( false );
+		} );
 	} );
 
-	it( 'should stop autosave timer when the autosave is up to date', () => {
-		render( <AutosaveMonitor isDirty={ false } isAutosaveable={ true } /> );
-
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 0 );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 1 );
-	} );
-
-	it( 'should stop autosave timer when having become dirty but not autosaveable', () => {
-		render( <AutosaveMonitor isDirty={ true } isAutosaveable={ false } /> );
-
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 0 );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 1 );
-	} );
-
-	it( 'should stop autosave timer when having become not dirty', () => {
-		const { rerender } = render(
-			<AutosaveMonitor isDirty={ true } isAutosaveable={ true } />
-		);
-		rerender(
-			<AutosaveMonitor isDirty={ false } isAutosaveable={ true } />
-		);
-
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 1 );
-	} );
-
-	it( 'should stop autosave timer when having become not autosaveable', () => {
-		const { rerender } = render(
-			<AutosaveMonitor isDirty={ true } isAutosaveable={ true } />
-		);
-		rerender(
-			<AutosaveMonitor isDirty={ true } isAutosaveable={ false } />
-		);
-
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 1 );
-	} );
-
-	it( 'should delay scheduling another autosave until after the one currently running finishes', () => {
-		const { rerender } = render(
-			<AutosaveMonitor
-				isDirty={ true }
-				isAutosaveable={ true }
-				isAutosaving={ true }
-			/>
-		);
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 0 );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 0 );
-
-		rerender(
-			<AutosaveMonitor
-				isDirty={ true }
-				isAutosaveable={ true }
-				isAutosaving={ false }
-			/>
-		);
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 1 );
-		expect( mockCancelSave ).toHaveBeenCalledTimes( 0 );
-	} );
-
-	it( 'should not schedule another autosave if the one currently running finishes and the post is not dirty anymore', () => {
-		const { rerender } = render(
-			<AutosaveMonitor
-				isDirty={ true }
-				isAutosaveable={ true }
-				isAutosaving={ true }
-			/>
-		);
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 0 );
-
-		rerender(
-			<AutosaveMonitor
-				isDirty={ false }
-				isAutosaveable={ true }
-				isAutosaving={ false }
-			/>
-		);
-		expect( mockThrottledSave ).toHaveBeenCalledTimes( 0 );
-	} );
-
-	it( 'should render nothing', () => {
-		const { container } = render( <AutosaveMonitor /> );
-		expect( container.childElementCount ).toBe( 0 );
+	describe( '#render()', () => {
+		it( 'should render nothing', () => {
+			expect( wrapper.type() ).toBe( null );
+		} );
 	} );
 } );
