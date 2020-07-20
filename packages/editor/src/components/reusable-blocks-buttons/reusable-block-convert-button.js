f598d43f6975ea57b49ba4ee439d465be8e43916
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-07-20 11:56:39 -0500

    ReusableBlockConvertButton: use React hooks. (#24015)
    * ReusableBlockConvertButton: use React hooks.
    
    * Fix/simplify _isVisible initialization.
    
    * Add types JSDoc to component.

--- packages/editor/src/components/reusable-blocks-buttons/reusable-block-convert-button.js
+++ packages/editor/src/components/reusable-blocks-buttons/reusable-block-convert-button.js
@@ -1,24 +1,69 @@
 /**
- * External dependencies
- */
-import { every } from 'lodash';
-
-/**
  * WordPress dependencies
  */
-import { MenuItem } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
 import { hasBlockSupport, isReusableBlock } from '@wordpress/blocks';
-import { withSelect, withDispatch } from '@wordpress/data';
-import { compose } from '@wordpress/compose';
 import { BlockSettingsMenuControls } from '@wordpress/block-editor';
+import { MenuItem } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
-export function ReusableBlockConvertButton( {
-	isVisible,
-	isReusable,
-	onConvertToStatic,
-	onConvertToReusable,
-} ) {
+/**
+ * Menu controls to convert block(s) to reusable block or vice-versa.
+ *
+ * @param {Object}   props           Component props.
+ * @param {string[]} props.clientIds Client ids of selected blocks.
+ *
+ * @return {import('@wordpress/element').WPComponent} The menu controls or null.
+ */
+export default function ReusableBlockConvertButton( { clientIds } ) {
+	const { isReusable, isVisible } = useSelect(
+		( select ) => {
+			const { canUser } = select( 'core' );
+			const { getBlocksByClientId, canInsertBlockType } = select(
+				'core/block-editor'
+			);
+			const { __experimentalGetReusableBlock: getReusableBlock } = select(
+				'core/editor'
+			);
+
+			const blocks = getBlocksByClientId( clientIds ) ?? [];
+
+			const _isReusable =
+				blocks.length === 1 &&
+				blocks[ 0 ] &&
+				isReusableBlock( blocks[ 0 ] ) &&
+				!! getReusableBlock( blocks[ 0 ].attributes.ref );
+
+			// Show 'Convert to Regular Block' when selected block is a reusable block.
+			const _isVisible =
+				_isReusable ||
+				// Hide 'Add to Reusable blocks' when reusable blocks are disabled.
+				( canInsertBlockType( 'core/block' ) &&
+					blocks.every(
+						( block ) =>
+							// Guard against the case where a regular block has *just* been converted.
+							!! block &&
+							// Hide 'Add to Reusable blocks' on invalid blocks.
+							block.isValid &&
+							// Hide 'Add to Reusable blocks' when block doesn't support being made reusable.
+							hasBlockSupport( block.name, 'reusable', true )
+					) &&
+					// Hide 'Add to Reusable blocks' when current doesn't have permission to do that.
+					!! canUser( 'create', 'blocks' ) );
+
+			return {
+				isReusable: _isReusable,
+				isVisible: _isVisible,
+			};
+		},
+		[ clientIds ]
+	);
+
+	const {
+		__experimentalConvertBlockToReusable: convertBlockToReusable,
+		__experimentalConvertBlockToStatic: convertBlockToStatic,
+	} = useDispatch( 'core/editor' );
+
 	if ( ! isVisible ) {
 		return null;
 	}
@@ -30,7 +75,7 @@ export function ReusableBlockConvertButton( {
 					{ ! isReusable && (
 						<MenuItem
 							onClick={ () => {
-								onConvertToReusable();
+								convertBlockToReusable( clientIds );
 								onClose();
 							} }
 						>
@@ -40,7 +85,7 @@ export function ReusableBlockConvertButton( {
 					{ isReusable && (
 						<MenuItem
 							onClick={ () => {
-								onConvertToStatic();
+								convertBlockToStatic( clientIds[ 0 ] );
 								onClose();
 							} }
 						>
@@ -52,61 +97,3 @@ export function ReusableBlockConvertButton( {
 		</BlockSettingsMenuControls>
 	);
 }
-
-export default compose( [
-	withSelect( ( select, { clientIds } ) => {
-		const { getBlocksByClientId, canInsertBlockType } = select(
-			'core/block-editor'
-		);
-		const { __experimentalGetReusableBlock: getReusableBlock } = select(
-			'core/editor'
-		);
-		const { canUser } = select( 'core' );
-
-		const blocks = getBlocksByClientId( clientIds );
-
-		const isReusable =
-			blocks.length === 1 &&
-			blocks[ 0 ] &&
-			isReusableBlock( blocks[ 0 ] ) &&
-			!! getReusableBlock( blocks[ 0 ].attributes.ref );
-
-		// Show 'Convert to Regular Block' when selected block is a reusable block
-		const isVisible =
-			isReusable ||
-			// Hide 'Add to Reusable blocks' when reusable blocks are disabled
-			( canInsertBlockType( 'core/block' ) &&
-				every(
-					blocks,
-					( block ) =>
-						// Guard against the case where a regular block has *just* been converted
-						!! block &&
-						// Hide 'Add to Reusable blocks' on invalid blocks
-						block.isValid &&
-						// Hide 'Add to Reusable blocks' when block doesn't support being made reusable
-						hasBlockSupport( block.name, 'reusable', true )
-				) &&
-				// Hide 'Add to Reusable blocks' when current doesn't have permission to do that
-				!! canUser( 'create', 'blocks' ) );
-
-		return {
-			isReusable,
-			isVisible,
-		};
-	} ),
-	withDispatch( ( dispatch, { clientIds } ) => {
-		const {
-			__experimentalConvertBlockToReusable: convertBlockToReusable,
-			__experimentalConvertBlockToStatic: convertBlockToStatic,
-		} = dispatch( 'core/editor' );
-
-		return {
-			onConvertToStatic() {
-				convertBlockToStatic( clientIds[ 0 ] );
-			},
-			onConvertToReusable() {
-				convertBlockToReusable( clientIds );
-			},
-		};
-	} ),
-] )( ReusableBlockConvertButton );
