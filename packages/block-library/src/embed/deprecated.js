ad8c2a637b492c793fd3a9809a0231734fb63200
ntsekouras <ntsekouras@outlook.com>
2020-07-20 13:28:56 +0300

    add deprecated + save files

---
+++ packages/block-library/src/embed/deprecated.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+
+/**
+ * WordPress dependencies
+ */
+import { RichText } from '@wordpress/block-editor';
+
+const { attributes: blockAttributes } = metadata;
+
+const deprecated = [
+	{
+		attributes: blockAttributes,
+		save( { attributes } ) {
+			const { url, caption, type, providerNameSlug } = attributes;
+
+			if ( ! url ) {
+				return null;
+			}
+
+			const embedClassName = classnames( 'wp-block-embed', {
+				[ `is-type-${ type }` ]: type,
+				[ `is-provider-${ providerNameSlug }` ]: providerNameSlug,
+			} );
+
+			return (
+				<figure className={ embedClassName }>
+					{ `\n${ url }\n` /* URL needs to be on its own line. */ }
+					{ ! RichText.isEmpty( caption ) && (
+						<RichText.Content
+							tagName="figcaption"
+							value={ caption }
+						/>
+					) }
+				</figure>
+			);
+		},
+	},
+];
+
+export default deprecated;
