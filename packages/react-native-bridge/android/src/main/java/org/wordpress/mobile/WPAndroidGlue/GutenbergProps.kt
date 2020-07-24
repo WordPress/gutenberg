eb78b4e101df2da799193bed996f9fad75783fd1
Antonis Lilis <antonis.lilis@automattic.com>
2020-07-24 13:25:11 +0300

    [Mobile] Disable Page Template picker if Modal Layout Picker is enabled (#24061)
    * MLP Android GlueCode

--- packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
+++ packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
@@ -2,7 +2,7 @@ package org.wordpress.mobile.WPAndroidGlue
 
 import android.os.Bundle
 
-data class GutenbergProps(
+data class GutenbergProps @JvmOverloads constructor(
     val enableMentions: Boolean,
     val enableUnsupportedBlockEditor: Boolean,
     val localeSlug: String,
@@ -10,7 +10,8 @@ data class GutenbergProps(
     val editorTheme: Bundle?,
     val translations: Bundle,
     val isDarkMode: Boolean,
-    val htmlModeEnabled: Boolean
+    val htmlModeEnabled: Boolean,
+    val isModalLayoutPickerEnabled: Boolean = false
 ) {
 
     fun getInitialProps(bundle: Bundle?) = (bundle ?: Bundle()).apply {
@@ -24,6 +25,7 @@ data class GutenbergProps(
         putBundle(PROP_CAPABILITIES, Bundle().apply {
             putBoolean(PROP_CAPABILITIES_MENTIONS, enableMentions)
             putBoolean(PROP_CAPABILITIES_UNSUPPORTED_BLOCK_EDITOR, enableUnsupportedBlockEditor)
+            putBoolean(PROP_CAPABILITIES_MODAL_LAYOUT_PICKER, isModalLayoutPickerEnabled)
         })
 
         editorTheme?.also {
@@ -52,5 +54,6 @@ data class GutenbergProps(
         const val PROP_CAPABILITIES = "capabilities"
         const val PROP_CAPABILITIES_MENTIONS = "mentions"
         const val PROP_CAPABILITIES_UNSUPPORTED_BLOCK_EDITOR = "unsupportedBlockEditor"
+        const val PROP_CAPABILITIES_MODAL_LAYOUT_PICKER = "modalLayoutPicker"
     }
 }
