b5acae6354b275b18b8f5d42f78c8e64c2b23fc6
Dion Hulse <contact@dd32.id.au>
2020-07-24 18:11:22 +1000

    Use newer string interpolation for cleaner code and DRYness.

--- packages/e2e-tests/specs/editor/plugins/block-directory-add.test.js
+++ packages/e2e-tests/specs/editor/plugins/block-directory-add.test.js
@@ -124,12 +124,10 @@ const MOCK_BLOCKS_RESPONSES = [
 		),
 	},
 	{
-		// Mock the post-new page when fetched using apiFetch (as indicated by the _locale param)
-		match: ( request ) =>
-			request.url().includes( 'post-new.php?_locale=user' ) &&
-			request.method() === 'GET',
+		// Mock the post-new page as requested via apiFetch for determining new CSS/JS assets.
+		match: ( request ) => request.url().includes( '/post-new.php' ),
 		onRequestMatch: createResponse(
-			'<html><head><script id="mock-block-js" src="https://fake_url.com/block.js"></script></head><body></body></html>',
+			`<html><head><script id="mock-block-js" src="${ MOCK_BLOCK1.assets[ 0 ] }"></script></head><body/></html>`,
 			'text/html; charset=UTF-8'
 		),
 	},
@@ -191,7 +189,7 @@ describe( 'adding blocks from block directory', () => {
 		await addBtn.click();
 
 		await page.waitForSelector(
-			'div[data-type="' + MOCK_BLOCK1.name + '"]'
+			`div[data-type="${ MOCK_BLOCK1.name }"]`
 		);
 
 		// The block will auto select and get added, make sure we see it in the content
