952ad64741a43094b342c2e235152e8f139e8371
Dion Hulse <dion@wordpress.org>
2020-07-27 18:18:08 +1000

    Block Directory: Use local assets with automatic asset detection (#24117)

--- packages/block-directory/src/store/test/controls.js
+++ packages/block-directory/src/store/test/controls.js
@@ -1,45 +1,21 @@
 /**
  * Internal dependencies
  */
-import { loadScript, loadStyle } from '../controls';
+import { loadAsset } from '../controls';
 
 describe( 'controls', () => {
-	const scriptAsset = 'http://www.wordpress.org/plugins/fakeasset.js';
-	const styleAsset = 'http://www.wordpress.org/plugins/fakeasset.css';
+	describe( 'loadAsset', () => {
+		const script = document.createElement( 'script' );
+		const style = document.createElement( 'link' );
 
-	describe( 'loadScript', () => {
 		it( 'should return a Promise when loading a script', () => {
-			const result = loadScript( scriptAsset );
+			const result = loadAsset( script );
 			expect( typeof result.then ).toBe( 'function' );
 		} );
 
-		it( 'should reject when no script is given', async () => {
-			expect.assertions( 1 );
-			const result = loadScript( '' );
-			await expect( result ).rejects.toThrow( Error );
-		} );
-
-		it( 'should reject when a non-js file is given', async () => {
-			const result = loadScript( styleAsset );
-			await expect( result ).rejects.toThrow( Error );
-		} );
-	} );
-
-	describe( 'loadStyle', () => {
 		it( 'should return a Promise when loading a style', () => {
-			const result = loadStyle( styleAsset );
+			const result = loadAsset( style );
 			expect( typeof result.then ).toBe( 'function' );
-		} );
-
-		it( 'should reject when no style is given', async () => {
-			expect.assertions( 1 );
-			const result = loadStyle( '' );
-			await expect( result ).rejects.toThrow( Error );
-		} );
-
-		it( 'should reject when a non-css file is given', async () => {
-			const result = loadStyle( scriptAsset );
-			await expect( result ).rejects.toThrow( Error );
 		} );
 	} );
 } );
