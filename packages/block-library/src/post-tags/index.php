ffad6fa927f9420cf4432bab745b1d694e0b30e3
Jeremy Yip <jeremy.yip@automattic.com>
2020-07-29 09:57:20 -0700

    Add wp block post tags class to php render

--- packages/block-library/src/post-tags/index.php
+++ packages/block-library/src/post-tags/index.php
@@ -20,8 +20,8 @@ function render_block_core_post_tags( $attributes, $content, $block ) {
 
 	$post_tags = get_the_tags( $block->context['postId'] );
 	if ( ! empty( $post_tags ) ) {
-		$output  = '';
-		$output .= '<div>';
+		$classes = 'wp-block-post-tags';
+		$output  = sprintf( '<div class="%1$s">', esc_attr( $classes ) );
 
 		foreach ( $post_tags as $tag ) {
 			$output .= '<a href="' . get_tag_link( $tag->term_id ) . '">' . $tag->name . '</a>' . ' | ';
