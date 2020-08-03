10871217bc48c4f5ded3e13a9088f6ff91da3518
Addison Stavlo <Stavz01@gmail.com>
2020-08-03 16:59:18 -0400

    Block alignment - remove unnecessary class application for dynamic blocks. (#24223)
    * remove unnecessary code
    
    * remove unused variable
    
    * try deprecating archives align attr
    
    * remove deprecate and fix SSR attributes
    
    * remove calendar align attr
    
    * revert changes back to eec0c74
    
    * remove align attr from blocks that done use SSR
    
    * add emun to default align attribute
    
    * update button__squared fixtures
    
    * add '' to enum

--- packages/block-library/src/calendar/index.php
+++ packages/block-library/src/calendar/index.php
@@ -32,11 +32,10 @@ function render_block_core_calendar( $attributes ) {
 	}
 
 	$custom_class_name = empty( $attributes['className'] ) ? '' : ' ' . $attributes['className'];
-	$align_class_name  = empty( $attributes['align'] ) ? '' : ' ' . "align{$attributes['align']}";
 
 	$output = sprintf(
 		'<div class="%1$s">%2$s</div>',
-		esc_attr( 'wp-block-calendar' . $custom_class_name . $align_class_name ),
+		esc_attr( 'wp-block-calendar' . $custom_class_name ),
 		get_calendar( true, false )
 	);
 
