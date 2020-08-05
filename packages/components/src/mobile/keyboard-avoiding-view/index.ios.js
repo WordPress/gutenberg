03d176a38c63d7725d4d6d0f2a319051851a1706
Chip <chip.snyder3@gmail.com>
2020-08-05 13:18:04 -0400

    Revert "[RNMobile] Fix jumping toolbar (#23684)"
    This reverts commit d86cd5f0b36b3de3034096c09e0aaf7509367c4c.

--- packages/components/src/mobile/keyboard-avoiding-view/index.ios.js
+++ packages/components/src/mobile/keyboard-avoiding-view/index.ios.js
@@ -3,76 +3,18 @@
  */
 import {
 	KeyboardAvoidingView as IOSKeyboardAvoidingView,
-	Animated,
-	Keyboard,
 	Dimensions,
 } from 'react-native';
 
-/**
- * WordPress dependencies
- */
-import { useState, useEffect, useRef } from '@wordpress/element';
-
-const AnimatedKeyboardAvoidingView = Animated.createAnimatedComponent(
-	IOSKeyboardAvoidingView
-);
-
-const MIN_HEIGHT = 44;
-
-export const KeyboardAvoidingView = ( {
-	parentHeight,
-	style,
-	withAnimatedHeight = false,
-	...otherProps
-} ) => {
-	const [ keyboardHeight, setKeyboardHeight ] = useState( 0 );
-	const animatedHeight = useRef( new Animated.Value( MIN_HEIGHT ) ).current;
-
+export const KeyboardAvoidingView = ( { parentHeight, ...otherProps } ) => {
 	const { height: fullHeight } = Dimensions.get( 'window' );
 	const keyboardVerticalOffset = fullHeight - parentHeight;
 
-	useEffect( () => {
-		Keyboard.addListener( 'keyboardWillShow', onKeyboardWillShow );
-		Keyboard.addListener( 'keyboardWillHide', onKeyboardWillHide );
-		return () => {
-			Keyboard.removeListener( 'keyboardWillShow', onKeyboardWillShow );
-			Keyboard.removeListener( 'keyboardWillHide', onKeyboardWillHide );
-		};
-	}, [] );
-
-	useEffect( () => {
-		animate();
-	}, [ keyboardHeight ] );
-
-	function onKeyboardWillShow( { endCoordinates } ) {
-		setKeyboardHeight( endCoordinates.height );
-	}
-
-	function onKeyboardWillHide() {
-		setKeyboardHeight( 0 );
-	}
-
-	const paddedKeyboardHeight =
-		keyboardHeight + MIN_HEIGHT - ( style.bottom || 0 );
-
-	function animate() {
-		Animated.timing( animatedHeight, {
-			toValue: keyboardHeight ? paddedKeyboardHeight : MIN_HEIGHT,
-			duration: keyboardHeight ? 0 : 150,
-			useNativeDriver: false,
-		} ).start();
-	}
-
 	return (
-		<AnimatedKeyboardAvoidingView
+		<IOSKeyboardAvoidingView
 			{ ...otherProps }
 			behavior="padding"
 			keyboardVerticalOffset={ keyboardVerticalOffset }
-			style={
-				withAnimatedHeight
-					? [ style, { height: animatedHeight } ]
-					: style
-			}
 		/>
 	);
 };
