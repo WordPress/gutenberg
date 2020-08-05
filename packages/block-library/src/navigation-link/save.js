43973aca87f191cb8fbcde290577ca65bb11c239
Robert Anderson <robert@noisysocks.com>
2020-08-05 16:46:34 +1000

    WIP: Make Navigation a static block

--- packages/block-library/src/navigation-link/save.js
+++ packages/block-library/src/navigation-link/save.js
@@ -1,8 +1,73 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { InnerBlocks } from '@wordpress/block-editor';
+import { RichText, InnerBlocks } from '@wordpress/block-editor';
 
-export default function save() {
-	return <InnerBlocks.Content />;
+/**
+ * Internal dependencies
+ */
+import { ItemSubmenuIcon } from './icons';
+
+export default function save( {
+	attributes: { url, opensInNewTab, rel, nofollow, label, showSubmenuIcon },
+	context: {
+		textColor,
+		customTextColor,
+		backgroundColor,
+		customBackgroundColor,
+		fontSize,
+		customFontSize,
+	},
+	innerBlocks,
+} ) {
+	const classes = classnames( {
+		'has-text-color': textColor || customTextColor,
+		[ `has-${ textColor }-color` ]: textColor,
+		'has-background': backgroundColor || customBackgroundColor,
+		[ `has-${ backgroundColor }-background-color` ]: backgroundColor,
+		[ `has-${ fontSize }-font-size` ]: fontSize,
+		'has-child': innerBlocks.length > 0,
+	} );
+
+	const style = {};
+	if ( ! textColor && customTextColor ) {
+		style.color = customTextColor;
+	}
+	if ( ! backgroundColor && customBackgroundColor ) {
+		style.backgroundColor = customBackgroundColor;
+	}
+	if ( ! fontSize && customFontSize ) {
+		style.fontSize = fontSize;
+	}
+
+	return (
+		<li className={ classes } style={ style }>
+			<a
+				className="wp-block-navigation-link__content"
+				href={ url }
+				target={ opensInNewTab ? '_blank' : undefined }
+				rel={ rel || ( nofollow ? 'nofollow' : undefined ) }
+			>
+				<RichText.Content
+					tagName="span"
+					className="wp-block-navigation-link__label"
+					value={ label }
+				/>
+			</a>
+			{ showSubmenuIcon && (
+				<span className="wp-block-navigation-link__submenu-icon">
+					<ItemSubmenuIcon />
+				</span>
+			) }
+			{ /* TODO: This class name should be navigation-link, not navigation. */ }
+			<ul className="wp-block-navigation__container">
+				<InnerBlocks.Content />
+			</ul>
+		</li>
+	);
 }
--- packages/block-library/src/navigation/save.js
+++ packages/block-library/src/navigation/save.js
@@ -1,8 +1,52 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { InnerBlocks } from '@wordpress/block-editor';
 
-export default function save() {
-	return <InnerBlocks.Content />;
+export default function save( {
+	attributes: {
+		textColor,
+		customTextColor,
+		backgroundColor,
+		customBackgroundColor,
+		fontSize,
+		customFontSize,
+		orientation,
+		itemsJustification,
+	},
+} ) {
+	const classes = classnames( {
+		// TODO: migrate rgbTextColor -> customTextColor, rgbBackgroundColor -> customBackgroundColor
+		'has-text-color': textColor || customTextColor,
+		[ `has-${ textColor }-color` ]: textColor,
+		'has-background': backgroundColor || customBackgroundColor,
+		[ `has-${ backgroundColor }-background-color` ]: backgroundColor,
+		[ `has-${ fontSize }-font-size` ]: fontSize,
+		'is-vertical': orientation === 'vertical',
+		[ `items-justified-${ itemsJustification }` ]: itemsJustification,
+	} );
+
+	const style = {};
+	if ( ! textColor && customTextColor ) {
+		style.color = customTextColor;
+	}
+	if ( ! backgroundColor && customBackgroundColor ) {
+		style.backgroundColor = customBackgroundColor;
+	}
+	if ( ! fontSize && customFontSize ) {
+		style.fontSize = fontSize;
+	}
+
+	return (
+		<nav className={ classes } style={ style }>
+			<ul className="wp-block-navigation__container">
+				<InnerBlocks.Content />
+			</ul>
+		</nav>
+	);
 }
