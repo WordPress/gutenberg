e2c70a167e15028b5753f3ddc49b397d7e4e236f
Andrés <andres.maneiro@automattic.com>
2020-08-06 17:09:30 +0200

    Take editor features data from blocks as well

--- packages/block-editor/src/components/use-editor-feature/index.js
+++ packages/block-editor/src/components/use-editor-feature/index.js
@@ -38,8 +38,6 @@ export default function useEditorFeature( featurePath ) {
 
 	const setting = useSelect(
 		( select ) => {
-			const path = `__experimentalFeatures.${ featurePath }`;
-
 			// 1 - Use deprecated settings, if available.
 			const settings = select( 'core/block-editor' ).getSettings();
 			const deprecatedSettingsValue = deprecatedFlags[ featurePath ]
@@ -49,8 +47,14 @@ export default function useEditorFeature( featurePath ) {
 				return deprecatedSettingsValue;
 			}
 
-			// 2 - Use global __experimentalFeatures otherwise.
-			return get( settings, path );
+			// 2 - Use __experimental features otherwise.
+			// We cascade to the global value if the block one is not available.
+			//
+			// TODO: make it work for blocks that define multiple selectors
+			// such as core/heading or core/post-title.
+			const globalPath = `__experimentalFeatures.global.${ featurePath }`;
+			const blockPath = `__experimentalFeatures.${ blockName }.${ featurePath }`;
+			return get( settings, blockPath ) ?? get( settings, globalPath );
 		},
 		[ blockName, featurePath ]
 	);
