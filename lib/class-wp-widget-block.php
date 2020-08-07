30b5b7518b8a58ceb69151daa4a9cdc97700513f
Adam Zieliński <adam@adamziel.com>
2020-08-07 11:21:02 +0200

    Clean up the endpoints

--- lib/class-wp-widget-block.php
+++ lib/class-wp-widget-block.php
@@ -19,7 +19,6 @@ class WP_Widget_Block extends WP_Widget {
 	 * @var array
 	 */
 	protected $default_instance = array(
-		'title'   => '',
 		'content' => '',
 	);
 
@@ -68,7 +67,6 @@ class WP_Widget_Block extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance            = array_merge( $this->default_instance, $old_instance );
-		$instance['title']   = sanitize_text_field( $new_instance['title'] );
 		$instance['content'] = $new_instance['content'];
 		return $instance;
 	}
@@ -84,7 +82,6 @@ class WP_Widget_Block extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, $this->default_instance );
 		echo do_blocks( $instance['content'] );
 		?>
-		<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" class="title sync-input" type="hidden" value="<?php echo esc_attr( $instance['title'] ); ?>"/>
 		<textarea id="<?php echo $this->get_field_id( 'content' ); ?>" name="<?php echo $this->get_field_name( 'content' ); ?>"  class="content sync-input" hidden><?php echo esc_textarea( $instance['content'] ); ?></textarea>
 		<?php
 	}
