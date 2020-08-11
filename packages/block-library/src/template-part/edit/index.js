61fa1bf623e0a0a187a82f10a2a8af8063e32d2b
Addison-Stavlo <stavz01@gmail.com>
2020-08-11 19:26:37 -0400

    set postId when slug is changed

--- packages/block-library/src/template-part/edit/index.js
+++ packages/block-library/src/template-part/edit/index.js
@@ -24,9 +24,6 @@ export default function TemplatePartEdit( {
 
 	// Resolve the post ID if not set, and load its post.
 	const postId = useTemplatePartPost( _postId, slug, theme );
-	useEffect( () => {
-		setAttributes( { postId } );
-	}, [ postId ] );
 
 	// Set the post ID, once found, so that edits persist,
 	// but wait until the third inner blocks change,
