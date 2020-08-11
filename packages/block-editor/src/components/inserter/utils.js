9d4f1cf90f6e13442f67c6425e0d8e886298772a
Miguel Fonseca <miguelcsf@gmail.com>
2020-08-11 19:18:07 +0100

    Fix provision of variations in block inserter items
    * Simplify overall logic
    * Prioritize base block types, excluding those superseded by
      isDefault:true block variations

--- packages/block-editor/src/components/inserter/utils.js
+++ packages/block-editor/src/components/inserter/utils.js
@@ -29,51 +29,30 @@ const getItemFromVariation = ( item ) => ( variation ) => ( {
  * @return {Array} Normalized inserter items.
  */
 export function includeVariationsInInserterItems( items, limit = Infinity ) {
-	const itemsLength = items.length;
-	if ( itemsLength >= limit ) {
-		// No need to iterate for variations
-		return items.slice( 0, limit );
-	}
+	// Exclude any block type item that is to be replaced by a default
+	// variation.
+	const filteredItems = items.filter(
+		( { variations = [] } ) =>
+			! variations.some( ( { isDefault } ) => isDefault )
+	);
 
-	// If there is a limit set but the items are fewer than the limit,
-	// add the variations to fill the limit with order of blocks.
-	// It should be handled better when decided how to choose the variations to fill.
-	if ( Number.isFinite( limit ) ) {
-		let itemsToAdd = limit - itemsLength;
-		let variationsToAdd = [];
+	// Fill `variationsToAdd` until there are as many items in total as
+	// `limit`.
+	const variationsToAdd = [];
+	if ( filteredItems.length < limit ) {
+		// Show all available blocks with variations
 		for ( const item of items ) {
+			if ( filteredItems.length + variationsToAdd.length >= limit ) {
+				break;
+			}
+
 			const { variations = [] } = item;
-			const variationsLength = variations.length;
-			if ( variationsLength ) {
+			if ( variations.length ) {
 				const variationMapper = getItemFromVariation( item );
-				variationsToAdd = variationsToAdd.concat(
-					variations.slice( 0, itemsToAdd ).map( variationMapper )
-				);
-				itemsToAdd -= variationsLength;
-				if ( itemsToAdd < 1 ) break;
+				variationsToAdd.push( ...variations.map( variationMapper ) );
 			}
 		}
-		return items.concat( variationsToAdd );
 	}
 
-	// Show all available blocks with variations
-	return items.reduce( ( result, item ) => {
-		const { variations = [] } = item;
-		const hasDefaultVariation = variations.some(
-			( { isDefault } ) => isDefault
-		);
-
-		// If there is no default inserter variation provided,
-		// then default block type is displayed.
-		if ( ! hasDefaultVariation ) {
-			result.push( item );
-		}
-
-		if ( variations.length ) {
-			const variationMapper = getItemFromVariation( item );
-			result.push( ...variations.map( variationMapper ) );
-		}
-
-		return result;
-	}, [] );
+	return [ ...filteredItems, ...variationsToAdd ].slice( 0, limit );
 }
