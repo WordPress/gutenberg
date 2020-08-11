67d375a2b6c1293a65ed2a405727a2957e1018d2
Jeremy Yip <jeremy.yip@automattic.com>
2020-08-11 12:35:09 -0700

    Remove redundant notification text

--- packages/block-library/src/template-part/edit/notice.js
+++ packages/block-library/src/template-part/edit/notice.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { useEntityProp } from '@wordpress/core-data';
 
 export default function TemplatePartNotice( { postId, slug } ) {
@@ -17,9 +16,6 @@ export default function TemplatePartNotice( { postId, slug } ) {
 			<div className="wp-block-template-part__notice-layout">
 				<div className="wp-block-template-part__notice-text">
 					{ title || slug }
-				</div>
-				<div className="wp-block-template-part__notice-text">
-					{ __( 'Changes apply everywhere the block is used' ) }
 				</div>
 			</div>
 		</div>
