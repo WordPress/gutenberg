6fd60954b0ba0b0beb7ec487c707f5bb6394cc0e
Joshua Flowers <joshuatf@gmail.com>
2020-08-17 16:03:27 -0400

    Remove opinionated styling

--- packages/components/src/navigation/styles/navigation-styles.js
+++ packages/components/src/navigation/styles/navigation-styles.js
@@ -3,15 +3,8 @@
  */
 import styled from '@emotion/styled';
 
-const horizontalPadding = '30px';
-
 export const Root = styled.div`
-	width: 272px;
-	padding: 35px 0px;
-	> *:not( .components-navigation__menu ) {
-		margin-left: ${ horizontalPadding };
-		margin-right: ${ horizontalPadding };
-	}
+	width: 100%;
 `;
 
 export const Menu = styled.ul`
@@ -22,14 +15,8 @@ export const Menu = styled.ul`
 `;
 
 export const MenuItem = styled.li`
-	display: flex;
-	justify-content: space-between;
 	button {
 		width: 100%;
-		padding-top: 8px;
-		padding-bottom: 8px;
-		padding-left: ${ horizontalPadding };
-		padding-right: ${ horizontalPadding };
 	}
 	&.is-active span {
 		border-bottom: 2px solid var( --wp-admin-theme-color );
