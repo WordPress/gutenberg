10ad10cbed44064e7ee86565e3cda1f847904170
Joshua T Flowers <joshuatf@gmail.com>
2020-08-18 06:04:47 +0300

    Use css-in-js for styling in navigation component (#24522)
    * Migrate styles to css-in-js
    
    * Add spacing to navigation child items
    
    * Remove opinionated styling
    
    * Rename styling components

---
+++ packages/components/src/navigation/styles/navigation-styles.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import styled from '@emotion/styled';
+
+export const Root = styled.div`
+	width: 100%;
+`;
+
+export const MenuUI = styled.ul`
+	padding: 0;
+	margin: 0 0 32px 0;
+	display: flex;
+	flex-direction: column;
+`;
+
+export const MenuItemUI = styled.li`
+	button {
+		width: 100%;
+	}
+	&.is-active span {
+		border-bottom: 2px solid var( --wp-admin-theme-color );
+	}
+`;
