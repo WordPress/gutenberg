a108524d4f621d604ced07ad3dd95a5c9314b40a
Joshua T Flowers <joshuatf@gmail.com>
2020-08-18 12:30:12 +0300

    Add nav badges (#24528)
    * Migrate styles to css-in-js
    
    * Add spacing to navigation child items
    
    * Add badge property and styles to navigation items
    
    * Update UI component naming
    
    * newLine
    
    Co-authored-by: Paul Sealock <psealock@gmail.com>

--- packages/components/src/navigation/styles/navigation-styles.js
+++ packages/components/src/navigation/styles/navigation-styles.js
@@ -3,6 +3,11 @@
  */
 import styled from '@emotion/styled';
 
+/**
+ * Internal dependencies
+ */
+import { LIGHT_GRAY } from '../../utils/colors-values';
+
 export const Root = styled.div`
 	width: 100%;
 `;
@@ -21,4 +26,12 @@ export const MenuItemUI = styled.li`
 	&.is-active span {
 		border-bottom: 2px solid var( --wp-admin-theme-color );
 	}
+`;
+
+export const BadgeUI = styled.span`
+	margin-left: 8px;
+	display: inline-flex;
+	padding: 4px 12px;
+	border-radius: 2px;
+	background-color: ${ LIGHT_GRAY[ 300 ] };
 `;
