224943ef3b1b416a28aab854544563eb6ae61f04
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2020-08-19 17:28:35 +0200

    Make apiFetch's supported paths customizable via hooks
    Also remove wpcom related supported paths

--- packages/react-native-editor/src/api-fetch-setup.js
+++ packages/react-native-editor/src/api-fetch-setup.js
@@ -1,53 +1,58 @@
 /**
- * External dependencies
- */
-/**
  * WordPress dependencies
  */
 import { fetchRequest } from '@wordpress/react-native-bridge';
-
-/**
- * WordPress dependencies
- */
 import apiFetch from '@wordpress/api-fetch';
+import { applyFilters } from '@wordpress/hooks';
 
 const setTimeoutPromise = ( delay ) =>
 	new Promise( ( resolve ) => setTimeout( resolve, delay ) );
 
-const fetchHandler = ( { path }, retries = 20, retryCount = 1 ) => {
-	if ( ! isPathSupported( path ) ) {
-		return Promise.reject( `Unsupported path: ${ path }` );
-	}
-
-	const responsePromise = fetchRequest( path );
-
-	const parseResponse = ( response ) => {
-		if ( typeof response === 'string' ) {
-			response = JSON.parse( response );
+const createFetchHandler = ( supportedPathPatterns ) => {
+	const fetchHandler = ( { path }, retries = 20, retryCount = 1 ) => {
+		if ( ! isPathSupported( path, supportedPathPatterns ) ) {
+			return Promise.reject( `Unsupported path: ${ path }` );
 		}
-		return response;
+
+		const responsePromise = fetchRequest( path );
+
+		const parseResponse = ( response ) => {
+			if ( typeof response === 'string' ) {
+				response = JSON.parse( response );
+			}
+			return response;
+		};
+
+		return responsePromise.then( parseResponse ).catch( ( error ) => {
+			// eslint-disable-next-line no-console
+			console.warn( 'Network Error: ', JSON.stringify( error, null, 2 ) );
+			if ( error.code >= 400 && error.code < 600 ) {
+				return error;
+			} else if ( retries === 0 ) {
+				return Promise.reject( error );
+			}
+			return setTimeoutPromise( 1000 * retryCount ).then( () =>
+				fetchHandler( { path }, retries - 1, retryCount + 1 )
+			);
+		} );
 	};
-
-	return responsePromise.then( parseResponse ).catch( ( error ) => {
-		// eslint-disable-next-line no-console
-		console.warn( 'Network Error: ', JSON.stringify( error, null, 2 ) );
-		if ( error.code >= 400 && error.code < 600 ) {
-			return error;
-		} else if ( retries === 0 ) {
-			return Promise.reject( error );
-		}
-		return setTimeoutPromise( 1000 * retryCount ).then( () =>
-			fetchHandler( { path }, retries - 1, retryCount + 1 )
-		);
-	} );
+	return fetchHandler;
 };
 
-export const isPathSupported = ( path ) =>
-	[
-		/wp\/v2\/(media|categories)\/?\d*?.*/i,
-		/wpcom\/v2\/gutenberg\/.*/i,
-	].some( ( pattern ) => pattern.test( path ) );
+const defaultSupportedPathPatterns = [ /wp\/v2\/(media|categories)\/?\d*?.*/i ];
+
+export const isPathSupported = (
+	path,
+	supportedPathPatterns = defaultSupportedPathPatterns
+) => supportedPathPatterns.some( ( pattern ) => pattern.test( path ) );
 
 export default () => {
+	const supportedPathPatterns = applyFilters(
+		'native.supported_api_fetch_path_patterns',
+		defaultSupportedPathPatterns
+	);
+
+	const fetchHandler = createFetchHandler( supportedPathPatterns );
+
 	apiFetch.setFetchHandler( ( options ) => fetchHandler( options ) );
 };
