8d61839838138cc7254805b8f445d5d8e4ff2577
Ari Stathopoulos <aristath@gmail.com>
2020-08-21 18:54:39 +0300

    Fix deprecation

--- packages/block-library/src/column/deprecated.js
+++ packages/block-library/src/column/deprecated.js
@@ -8,18 +8,50 @@ import classnames from 'classnames';
  */
 import { InnerBlocks } from '@wordpress/block-editor';
 
-const blockAttributes = {
-	verticalAlignment: {
-		type: 'string',
-	},
-	width: {
-		type: 'string',
-	},
-};
-
 const deprecated = [
 	{
-		attributes: blockAttributes,
+		attributes: {
+			verticalAlignment: {
+				type: 'string',
+			},
+			width: {
+				type: 'number',
+				min: 0,
+				max: 100,
+			},
+		},
+		isEligible( attributes ) {
+			return attributes.width && 'string' !== typeof attributes.width;
+		},
+		save( { attributes } ) {
+			attributes.width = attributes.width.toString() + '%';
+			const { verticalAlignment, width } = attributes;
+
+			const wrapperClasses = classnames( {
+				[ `is-vertically-aligned-${ verticalAlignment }` ]: verticalAlignment,
+			} );
+
+			let style;
+			if ( width ) {
+				style = { flexBasis: width };
+			}
+
+			return (
+				<div className={ wrapperClasses } style={ style }>
+					<InnerBlocks.Content />
+				</div>
+			);
+		},
+	},
+	{
+		attributes: {
+			verticalAlignment: {
+				type: 'string',
+			},
+			width: {
+				type: 'string',
+			},
+		},
 		isEligible( attributes ) {
 			return attributes.width && ! isNaN( attributes.width );
 		},
