dc3b8b7d873151d2c8fc59c4e2813de1a06b9f7f
Ari Stathopoulos <aristath@gmail.com>
2020-08-24 11:03:43 +0300

    Add rudimentary units validation

--- packages/block-library/src/column/edit.js
+++ packages/block-library/src/column/edit.js
@@ -13,7 +13,7 @@ import {
 	InspectorControls,
 	__experimentalBlock as Block,
 } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
+import { PanelBody, TextControl, Notice } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
@@ -53,6 +53,59 @@ function ColumnEdit( {
 
 	const hasWidth = ( isNaN( width ) && width ) || Number.isFinite( width );
 
+	const isWidthValid = () => {
+		const validUnits = [
+			'fr',
+			'rem',
+			'em',
+			'ex',
+			'%',
+			'px',
+			'cm',
+			'mm',
+			'in',
+			'pt',
+			'pc',
+			'ch',
+			'vh',
+			'vw',
+			'vmin',
+			'vmax',
+		];
+
+		// Return true for values that can be passed-on as-is.
+		if (
+			! width ||
+			'' === width ||
+			0 === width ||
+			'0' === width ||
+			'auto' === width ||
+			'inherit' === width ||
+			'initial' === width
+		) {
+			return true;
+		}
+
+		// Skip checking if calc().
+		if ( 0 <= width.indexOf( 'calc(' ) && 0 <= width.indexOf( ')' ) ) {
+			return true;
+		}
+
+		// Get the numeric value.
+		const numericValue = parseFloat( width );
+
+		// Get the unit
+		const unit = width.replace( numericValue, '' );
+
+		// Do not allow unitless.
+		if ( ! unit ) {
+			return false;
+		}
+
+		// Check the validity of the numeric value and units.
+		return ! isNaN( numericValue ) && -1 !== validUnits.indexOf( unit );
+	};
+
 	return (
 		<>
 			<BlockControls>
@@ -73,6 +126,13 @@ function ColumnEdit( {
 							width === undefined ? __( 'Auto' ) : undefined
 						}
 					/>
+					{ ! isWidthValid() && (
+						<Notice status="warning" isDismissible={ false }>
+							{ __(
+								'Invalid CSS value. Values should include a number and a unit. Example: 30%, 100px, 20em etc.'
+							) }
+						</Notice>
+					) }
 				</PanelBody>
 			</InspectorControls>
 			<InnerBlocks
