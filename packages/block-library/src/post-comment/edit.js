3ea01113c8821be4a4d24d465049b942394322fd
David Szabo <david.szabo97@gmail.com>
2020-08-24 15:07:25 +0200

    Add comment ID attribute

--- packages/block-library/src/post-comment/edit.js
+++ packages/block-library/src/post-comment/edit.js
@@ -1,14 +1,30 @@
 /**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
- *
- * @param {Object} [props]           Properties passed from the editor.
- * @param {string} [props.className] Class name generated for the block.
- *
- * @return {WPElement} Element to render.
+ * WordPress dependencies
  */
-export default function Edit( { className } ) {
-	return <p className={ className }>FROM THE EDITOR</p>;
+import { __ } from '@wordpress/i18n';
+import { Placeholder, TextControl } from '@wordpress/components';
+import { blockDefault } from '@wordpress/icons';
+
+// TODO: JSDOC types
+export default function Edit( { className, attributes, setAttributes } ) {
+	const { commentId } = attributes;
+
+	if ( ! commentId ) {
+		return (
+			<Placeholder
+				icon={ blockDefault }
+				label={ __( 'Post Comment' ) }
+				instructions={ __( 'Input post comment ID' ) }
+			>
+				<TextControl
+					value={ commentId }
+					onChange={ ( val ) =>
+						setAttributes( { commentId: parseInt( val ) } )
+					}
+				/>
+			</Placeholder>
+		);
+	}
+
+	return <p className={ className }>Comment ID: { commentId }</p>;
 }
