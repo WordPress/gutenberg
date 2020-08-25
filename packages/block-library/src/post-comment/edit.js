250778b5a88a1dd8ae818a6b29f425a94e08cdb3
David Szabo <david.szabo97@gmail.com>
2020-08-25 09:59:57 +0200

    Render inner blocks

--- packages/block-library/src/post-comment/edit.js
+++ packages/block-library/src/post-comment/edit.js
@@ -5,19 +5,14 @@ import { __ } from '@wordpress/i18n';
 import { Placeholder, TextControl, Button } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { blockDefault } from '@wordpress/icons';
-import { useEntityProp } from '@wordpress/core-data';
+import { InnerBlocks } from '@wordpress/block-editor';
+
+const ALLOWED_BLOCKS = [];
 
 // TODO: JSDOC types
 export default function Edit( { className, attributes, setAttributes } ) {
 	const { commentId } = attributes;
 	const [ commentIdInput, setCommentIdInput ] = useState( commentId );
-
-	const [ content ] = useEntityProp(
-		'root',
-		'comment',
-		'content',
-		commentId
-	);
 
 	if ( ! commentId ) {
 		return (
@@ -44,8 +39,8 @@ export default function Edit( { className, attributes, setAttributes } ) {
 	}
 
 	return (
-		<p className={ className }>
-			Comment ID: { commentId } { content }
-		</p>
+		<div className={ className }>
+			<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } />
+		</div>
 	);
 }
--- packages/block-library/src/post-comment/save.js
+++ packages/block-library/src/post-comment/save.js
@@ -1,12 +1,14 @@
 /**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
- *
- * @return {WPElement} Element to render.
+ * WordPress dependencies
  */
-export default function save() {
-	return <p>SAVED CONTENT</p>;
+import { InnerBlocks } from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const { className } = attributes;
+
+	return (
+		<div className={ className }>
+			<InnerBlocks.Content />
+		</div>
+	);
 }
