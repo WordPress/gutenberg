cce8037289e303f828cac3b15c2dffeeb1b0f8d8
Adam Zielinski <adam@adamziel.com>
2020-08-26 13:43:58 +0200

    Update phpunit/class-rest-sidebars-controller-test.php
    Co-authored-by: Timothy Jacobs <timothy@ironbounddesigns.com>

--- phpunit/class-rest-sidebars-controller-test.php
+++ phpunit/class-rest-sidebars-controller-test.php
@@ -422,7 +422,7 @@ class REST_Sidebars_Controller_Test extends WP_Test_REST_Controller_Testcase {
 			)
 		);
 		$response = rest_get_server()->dispatch( $request );
-		$this->assertErrorResponse( 'widgets_cannot_access', $response, 401 );
+		$this->assertErrorResponse( 'widgets_cannot_access', $response, 403 );
 	}
 
 	/**
