43d4dabe7830d7f84150e7ba044cb3261f3b8ef8
Andrés <andres.maneiro@automattic.com>
2020-08-27 17:46:03 +0200

    Get rid of GlobalPanel & BlockPanel in favor of inlining the style components

--- packages/edit-site/src/components/sidebar/global-styles-sidebar.js
+++ packages/edit-site/src/components/sidebar/global-styles-sidebar.js
@@ -6,19 +6,20 @@ import { omit } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { TabPanel } from '@wordpress/components';
+import { PanelBody, TabPanel } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { getBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
-import DefaultSidebar from './default-sidebar';
-import BlockPanel from './block-panel';
-import GlobalPanel from './global-panel';
 import { useGlobalStylesContext } from '../editor/global-styles-provider';
+import DefaultSidebar from './default-sidebar';
 import { GLOBAL_CONTEXT } from '../editor/utils';
+import TypographyPanel from './typography-panel';
+import ColorPanel from './color-panel';
 
-export default ( { identifier, title: panelTitle, icon } ) => {
+export default ( { identifier, title, icon } ) => {
 	const { contexts, getProperty, setProperty } = useGlobalStylesContext();
 
 	if ( typeof contexts !== 'object' || ! contexts?.[ GLOBAL_CONTEXT ] ) {
@@ -27,11 +28,7 @@ export default ( { identifier, title: panelTitle, icon } ) => {
 	}
 
 	return (
-		<DefaultSidebar
-			identifier={ identifier }
-			title={ panelTitle }
-			icon={ icon }
-		>
+		<DefaultSidebar identifier={ identifier } title={ title } icon={ icon }>
 			<TabPanel
 				tabs={ [
 					{ name: 'global', title: __( 'Global' ) },
@@ -39,24 +36,91 @@ export default ( { identifier, title: panelTitle, icon } ) => {
 				] }
 			>
 				{ ( tab ) => {
+					/* Per Block Context */
 					if ( 'block' === tab.name ) {
-						return (
-							<BlockPanel
-								contexts={ omit( contexts, [
-									GLOBAL_CONTEXT,
-								] ) }
-								setProperty={ setProperty }
-								getProperty={ getProperty }
-							/>
-						);
+						return Object.keys(
+							omit( contexts, [ GLOBAL_CONTEXT ] )
+						)
+							.map( ( name ) => {
+								const {
+									supports,
+									selector,
+									name: blockName,
+								} = contexts[ name ];
+
+								/*
+								 * Some block (eg: core/heading) are split in different
+								 * contexts (eg: core/heading/h1, core/heading/h2).
+								 * Because each context maps to a different UI section
+								 * in the sidebar we attach the selector (h1, h2)
+								 * to the title for those blocks.
+								 */
+								const blockType = getBlockType( blockName );
+								let panelTitle = blockType.title;
+								if (
+									'object' ===
+									typeof blockType?.supports
+										?.__experimentalSelector
+								) {
+									panelTitle += ` (${ selector })`;
+								}
+
+								return (
+									<PanelBody
+										key={ 'panel-' + name }
+										title={ panelTitle }
+										initialOpen={ false }
+									>
+										{ [
+											<TypographyPanel
+												key={
+													'typography-panel-' + name
+												}
+												context={ {
+													supports,
+													name,
+												} }
+												getProperty={ getProperty }
+												setProperty={ setProperty }
+											/>,
+											<ColorPanel
+												key={ 'color-panel-' + name }
+												context={ {
+													supports,
+													name,
+												} }
+												getProperty={ getProperty }
+												setProperty={ setProperty }
+											/>,
+										].filter( Boolean ) }
+									</PanelBody>
+								);
+							} )
+							.filter( Boolean );
 					}
-					return (
-						<GlobalPanel
-							context={ contexts[ GLOBAL_CONTEXT ] }
-							setProperty={ setProperty }
+
+					/* Global Context */
+					const { supports, name } = contexts[ GLOBAL_CONTEXT ];
+					return [
+						<TypographyPanel
+							key={ 'typography-panel-' + name }
+							context={ {
+								supports,
+								name,
+							} }
 							getProperty={ getProperty }
-						/>
-					);
+							setProperty={ setProperty }
+						/>,
+						<ColorPanel
+							key={ 'color-panel-' + name }
+							context={ {
+								supports,
+								name,
+							} }
+							getProperty={ getProperty }
+							setProperty={ setProperty }
+						/>,
+					].filter( Boolean );
 				} }
 			</TabPanel>
 		</DefaultSidebar>
