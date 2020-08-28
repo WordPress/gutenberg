6c9969b68dfb2b23dbce79d3cd456cad2ff8fa53
Riad Benguella <benguella@gmail.com>
2020-08-28 09:50:28 +0100

    Use the postcss-custom-properties config to define fallback colors

--- packages/postcss-plugins-preset/lib/index.js
+++ packages/postcss-plugins-preset/lib/index.js
@@ -1,4 +1,14 @@
 module.exports = [
-	require( 'postcss-custom-properties' )(),
+	require( 'postcss-custom-properties' )( {
+		importFrom: [
+			{
+				customProperties: {
+					'--wp-admin-theme-color': '#007cba',
+					'--wp-admin-theme-color-darker-10': '#006ba1',
+					'--wp-admin-theme-color-darker-20': '#005a87',
+				},
+			},
+		],
+	} ),
 	require( 'autoprefixer' )( { grid: true } ),
 ];
