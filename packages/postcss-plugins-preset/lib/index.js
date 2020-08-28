e215c80e1fcb613dab3b26d9a31053ae96725226
Riad Benguella <benguella@gmail.com>
2020-08-28 13:00:15 +0100

    Keep the default CSS variables for npm consumers  (#24890)

--- packages/postcss-plugins-preset/lib/index.js
+++ packages/postcss-plugins-preset/lib/index.js
@@ -1,14 +1,4 @@
 module.exports = [
-	require( 'postcss-custom-properties' )( {
-		importFrom: [
-			{
-				customProperties: {
-					'--wp-admin-theme-color': '#007cba',
-					'--wp-admin-theme-color-darker-10': '#006ba1',
-					'--wp-admin-theme-color-darker-20': '#005a87',
-				},
-			},
-		],
-	} ),
+	require( 'postcss-custom-properties' )(),
 	require( 'autoprefixer' )( { grid: true } ),
 ];
