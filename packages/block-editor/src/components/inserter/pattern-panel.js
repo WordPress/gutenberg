59c6c2d943361d2c07ae10dfe92a21cb6545c3f4
apeatling <apeatling@gmail.com>
2020-09-01 13:59:07 -0700

    Fix scss lint errors.

--- packages/block-editor/src/components/inserter/pattern-panel.js
+++ packages/block-editor/src/components/inserter/pattern-panel.js
@@ -1,60 +1,54 @@
 /**
  * WordPress dependencies
  */
-import { Dropdown, MenuGroup, MenuItem, Button } from '@wordpress/components';
+import { CustomSelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { check } from '@wordpress/icons';
 
 function PatternInserterPanel( {
-	title,
 	selectedCategory,
 	patternCategories,
 	onClickCategory,
 	children,
 } ) {
-	const renderToggle = ( { isOpen, onToggle } ) => (
-		<h2 className="block-editor-inserter__panel-title">
-			<Button onClick={ onToggle } aria-expanded={ isOpen }>
-				{ title ? title : selectedCategory.label }
-			</Button>
-		</h2>
-	);
+	const categoryOptions = () => {
+		const options = [];
 
-	const renderContent = ( { onClose } ) => {
-		return (
-			<MenuGroup label={ __( 'Select a Category' ) }>
-				{ patternCategories.map( ( patternCategory ) => {
-					return (
-						<MenuItem
-							key={ patternCategory.name }
-							onClick={ () => {
-								onClickCategory( patternCategory );
-								onClose();
-							} }
-							className={
-								patternCategory.name === selectedCategory.name
-									? 'is-selected'
-									: ''
-							}
-							icon={ check }
-						>
-							{ patternCategory.label }
-						</MenuItem>
-					);
-				} ) }
-			</MenuGroup>
+		patternCategories.map( ( patternCategory ) => {
+			return options.push( {
+				key: patternCategory.name,
+				name: patternCategory.label,
+				style: {},
+				className:
+					patternCategory.name === selectedCategory.name
+						? 'is-selected'
+						: '',
+			} );
+		} );
+
+		return options;
+	};
+
+	const onChangeSelect = ( selected ) => {
+		onClickCategory(
+			patternCategories.find(
+				( patternCategory ) =>
+					selected.selectedItem.key === patternCategory.name
+			)
 		);
 	};
 
 	return (
 		<>
 			<div className="block-editor-inserter__panel-header">
-				<Dropdown
+				<CustomSelectControl
+					label={ __( 'Select a Category' ) }
+					hideLabelFromVision
 					className="block-editor-inserter__panel-header-dropdown"
-					contentClassName="block-editor-inserter__panel-header-dropdown-content"
-					position="bottom right"
-					renderToggle={ renderToggle }
-					renderContent={ renderContent }
+					onChange={ onChangeSelect }
+					options={ categoryOptions() }
+					value={ categoryOptions().find(
+						( cat ) => cat.key === selectedCategory.name
+					) }
 				/>
 			</div>
 			<div className="block-editor-inserter__panel-content">
