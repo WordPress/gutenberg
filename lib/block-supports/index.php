b034c0d6eaa94d5cfe6ec60537ecd1c4334c1208
Jon Surrell <jon.surrell@automattic.com>
2020-09-02 18:26:18 +0200

    Save DOMDocument node instead of root + str_replace
    We wrapped block HTML in some tags to ensure the DOMDocument parses it
    correctly, then unwrapped it again with str_replace after using
    `saveHTML`.
    
    The `saveHTML` method accepts an optional argument that is the dom node
    to output a subset of the document:
    https://www.php.net/manual/en/domdocument.savehtml.php
    
    Rather than calling `str_replace` on the entire document to get the
    unwrapped block HTML, invoke `saveHTML` on the block root to get the
    unwrapped HTML.
    
    This prevents some additional newlines from being appended to the
    output which was observed when running core block tests with Gutenberg
    activated.

--- lib/block-supports/index.php
+++ lib/block-supports/index.php
@@ -96,7 +96,7 @@ function gutenberg_apply_block_supports( $block_content, $block ) {
 		$block_root->setAttribute( 'style', esc_attr( implode( '; ', $new_styles ) . ';' ) );
 	}
 
-	return str_replace( array( $wrapper_left, $wrapper_right ), '', $dom->saveHtml() );
+	return $dom->saveHtml( $block_root );
 }
 add_filter( 'render_block', 'gutenberg_apply_block_supports', 10, 2 );
 
