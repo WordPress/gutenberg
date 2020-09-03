c311e038e8fc1b7790f21c40edbe314fb62b7c61
lukewalczak <lukasz.walczak.pwr@gmail.com>
2020-09-03 13:41:34 +0200

    Correct infinite loop in columns block

--- packages/block-editor/src/components/block-list/index.native.js
+++ packages/block-editor/src/components/block-list/index.native.js
@@ -53,6 +53,7 @@ export class BlockList extends Component {
 		this.extraData = {
 			parentWidth: this.props.parentWidth,
 			renderFooterAppender: this.props.renderFooterAppender,
+			renderAppender: this.props.renderAppender,
 			onDeleteBlock: this.props.onDeleteBlock,
 			contentStyle: this.props.contentstyle,
 		};
@@ -115,18 +116,21 @@ export class BlockList extends Component {
 			renderFooterAppender,
 			onDeleteBlock,
 			contentStyle,
+			renderAppender,
 		} = this.props;
 		if (
 			this.extraData.parentWidth !== parentWidth ||
 			this.extraData.renderFooterAppender !== renderFooterAppender ||
 			this.extraData.onDeleteBlock !== onDeleteBlock ||
-			this.extraData.contentStyle !== contentStyle
+			this.extraData.contentStyle !== contentStyle ||
+			this.extraData.renderAppender !== renderAppender
 		) {
 			this.extraData = {
 				parentWidth,
 				renderFooterAppender,
 				onDeleteBlock,
 				contentStyle,
+				renderAppender,
 			};
 		}
 		return this.extraData;
