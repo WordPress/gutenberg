fe6fb19b01a918f62664728584d683b3fb812464
Noah Allen <noahtallen@gmail.com>
2020-09-03 16:40:01 -0700

    Use create function and hook to simplify manager
    Also removes need for robust instance management

--- packages/block-editor/src/components/use-block-sync/block-manager.js
+++ packages/block-editor/src/components/use-block-sync/block-manager.js
@@ -8,104 +8,74 @@ import { v4 as uuid } from 'uuid';
  */
 import { useEffect, useRef } from '@wordpress/element';
 
-/**
- * Internal dependencies
- */
-import {
-	setupEntityInstance,
-	isDuplicateEntityInstance,
-	removeEntityInstance,
-} from './entity-instances';
+function createBlockClientIdManager( shouldMapBlocks ) {
+	const DIRECTION_IN = 'in';
+	const DIRECTION_OUT = 'out';
+	const flipDirection = ( direction ) => {
+		return direction === DIRECTION_IN ? DIRECTION_OUT : DIRECTION_IN;
+	};
 
-class BlockManager {
-	static _DIRECTION_IN = 'in';
-	static _DIRECTION_OUT = 'out';
+	const clientIdMap = { [ DIRECTION_IN ]: {}, [ DIRECTION_OUT ]: {} };
 
-	constructor( entityId, instanceId ) {
-		this._entityId = entityId;
-		this._instanceId = instanceId;
-		this._blockIdMap = {
-			in: {},
-			out: {},
-		};
-		setupEntityInstance( entityId, instanceId );
-	}
-
-	// If needed, map to new outgoing IDs.
-	updateOutgoingBlocks( blocks ) {
-		return this._mapBlocks( blocks, this._DIRECTION_OUT );
-	}
-
-	// If needed, map to new incoming IDs.
-	updateIncomingBlocks( blocks ) {
-		return this._mapBlocks( blocks, this._DIRECTION_IN );
-	}
-
-	// Get the mapped ID for an outgoing block directly.
-	getOutgoingBlockId( blockClientId ) {
-		return this._mapBlockId( blockClientId, this._DIRECTION_OUT );
-	}
-
-	// Get the mapped ID for an incoming block directly.
-	getIncomingBlockId( blockClientId ) {
-		return this._mapBlockId( blockClientId, this._DIRECTION_IN );
-	}
-
-	destroy() {
-		removeEntityInstance( this._entityId, this._instanceId );
-	}
-
-	_isDuplicateInstance() {
-		return isDuplicateEntityInstance( this._entityId, this._instanceId );
-	}
-
-	_mapBlocks( blocks, direction ) {
-		if ( ! Array.isArray( blocks ) || ! this._isDuplicateInstance() ) {
+	const mapBlocks = ( blocks, direction ) => {
+		if ( ! Array.isArray( blocks ) || ! shouldMapBlocks ) {
 			return blocks;
 		}
+
 		return blocks.map( ( block ) => ( {
 			...block,
-			innerBlocks: this._mapBlocks( block.innerBlocks, direction ),
-			clientId: this._mapBlockId( block.clientId, direction ),
+			innerBlocks: mapBlocks( block.innerBlocks, direction ),
+			clientId: mapBlockId( block.clientId, direction ),
 		} ) );
-	}
+	};
 
-	_mapBlockId( blockClientId, direction ) {
+	const mapBlockId = ( blockClientId, direction ) => {
 		// Avoid running map code on non-string clientIds. There appear to be times
 		// where this function can be called with `blockClientId = {}`
-		if (
-			typeof blockClientId !== 'string' ||
-			! this._isDuplicateInstance()
-		) {
+		if ( typeof blockClientId !== 'string' || ! shouldMapBlocks ) {
 			return blockClientId;
 		}
 
-		if ( ! this._blockIdMap[ direction ][ blockClientId ] ) {
+		if ( ! clientIdMap[ direction ][ blockClientId ] ) {
 			const newId = uuid();
-			this._blockIdMap[ direction ][ blockClientId ] = newId;
-			this._blockIdMap[ this._flipDirection( direction ) ][
-				newId
-			] = blockClientId;
+			clientIdMap[ direction ][ blockClientId ] = newId;
+			clientIdMap[ flipDirection( direction ) ][ newId ] = blockClientId;
 		}
-		return this._blockIdMap[ direction ][ blockClientId ];
-	}
+		return clientIdMap[ direction ][ blockClientId ];
+	};
 
-	_flipDirection( direction ) {
-		return direction === this._DIRECTION_IN
-			? this._DIRECTION_OUT
-			: this._DIRECTION_IN;
-	}
+	return {
+		updateOutgoingBlocks: ( blocks ) => mapBlocks( blocks, DIRECTION_OUT ),
+		updateIncomingBlocks: ( blocks ) => mapBlocks( blocks, DIRECTION_IN ),
+		getOutgoingBlockId: ( blockId ) => mapBlockId( blockId, DIRECTION_OUT ),
+		getIncomingBlockId: ( blockId ) => mapBlockId( blockId, DIRECTION_IN ),
+	};
 }
 
+const _instanceReference = {};
 export default function useBlockManager( entityId ) {
-	const instanceId = useRef( uuid() ).current;
-	const managerInstance = useRef( new BlockManager( entityId, instanceId ) );
+	const manager = useRef(
+		createBlockClientIdManager( !! _instanceReference[ entityId ] )
+	);
+
+	const entityIdRef = useRef( entityId );
+	if ( entityIdRef.current ) {
+		_instanceReference[ entityIdRef.current ] = true;
+	}
 
 	useEffect( () => {
-		managerInstance.current.destroy();
-		managerInstance.current = new BlockManager( entityId, instanceId );
-		return () => managerInstance.current.destroy();
-	}, [ instanceId, entityId ] );
+		if ( ! entityId ) {
+			entityIdRef.current = null;
+			return;
+		}
+		if ( ! _instanceReference[ entityId ] ) {
+			_instanceReference[ entityId ] = true;
+			manager.current = createBlockClientIdManager( false );
+		} else {
+			manager.current = createBlockClientIdManager( true );
+		}
+		entityIdRef.current = entityId;
+	}, [ entityId ] );
 
-	return managerInstance.current;
+	return manager.current;
 }
