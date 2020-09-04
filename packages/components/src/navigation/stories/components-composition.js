1a88aea96ca366f46ca70e8753df9280caade0f2
Copons <Copons@users.noreply.github.com>
2020-09-04 16:21:18 +0100

    Story parity with other approach

--- packages/components/src/navigation/stories/components-composition.js
+++ packages/components/src/navigation/stories/components-composition.js
@@ -21,51 +21,94 @@ function ComponentsCompositionExample() {
 	return (
 		<Container>
 			<ComponentsCompositionNavigation
-				initialActiveItem="item-5"
+				initialActiveItem="item-1"
 				initialActiveLevel="root"
 			>
 				{ ( { NavigationItem, NavigationLevel } ) => (
 					<>
 						<NavigationLevel level="root" title="Home">
+							<NavigationItem slug="item-1" title="Item 1" />
+							<NavigationItem slug="item-2" title="Item 2" />
 							<NavigationItem
-								slug="item-5"
-								title="Item 5"
-								badge="0"
-							/>
-							<NavigationItem
-								slug="external-link"
-								title="External Link"
-								href="https://wordpress.org"
-							/>
-							<NavigationItem
-								slug="item-1"
-								title="Item 1"
 								badge="2"
-								navigateToLevel="sub-menu"
+								navigateToLevel="category"
+								slug="item-3"
+								title="Category"
 							/>
+							<NavigationItem
+								slug="item-4"
+								title="External link"
+							/>
+							<NavigationItem slug="item-5">
+								<img
+									alt="WordPress Logo"
+									src="https://s.w.org/style/images/about/WordPress-logotype-wmark-white.png"
+									style={ { width: 50, height: 50 } }
+								/>
+							</NavigationItem>
 						</NavigationLevel>
 
 						<NavigationLevel
-							level="sub-menu"
-							title="Sub Menu"
+							level="category"
 							parentLevel="root"
 							parentTitle="Home"
+							title="Category"
 						>
 							<NavigationItem
-								slug="item-2"
-								title="Item 2"
-								navigateToLevel="nested-sub-menu"
+								badge="1"
+								slug="child-1"
+								title="Child 1"
+							/>
+							<NavigationItem slug="child-2" title="Child 2" />
+							<NavigationItem
+								navigateToLevel="nested-category"
+								slug="child-3"
+								title="Nested Category"
 							/>
 						</NavigationLevel>
 
 						<NavigationLevel
-							level="nested-sub-menu"
-							title="SubSub Menu"
-							parentLevel="sub-menu"
-							parentTitle="Sub Menu"
+							level="nested-category"
+							parentLevel="category"
+							parentTitle="Category"
+							title="Nested Category"
 						>
-							<NavigationItem slug="item-3" title="Item 3" />
-							<NavigationItem slug="item-4" title="Item 4" />
+							<NavigationItem
+								slug="sub-child-1"
+								title="Sub Child 1"
+							/>
+							<NavigationItem
+								slug="sub-child-2"
+								title="Sub Child 2"
+							/>
+						</NavigationLevel>
+
+						<NavigationLevel level="root" title="Secondary Menu">
+							<NavigationItem
+								navigateToLevel="secondary-level"
+								slug="secondary-item-1"
+								title="Secondary Item 1"
+							/>
+							<NavigationItem
+								slug="secondary-item-2"
+								title="Secondary Item 2"
+							/>
+						</NavigationLevel>
+
+						<NavigationLevel
+							level="secondary-level"
+							parentLevel="root"
+							parentTitle="Home"
+							title="Secondary Item 1"
+						>
+							<NavigationItem
+								slug="secondary-child-1"
+								title="Secondary Child 1"
+							/>
+							<NavigationItem
+								slug="secondary-child-2"
+								title="Secondary Child 2"
+							/>
 						</NavigationLevel>
 					</>
 				) }
