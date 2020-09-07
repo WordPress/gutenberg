9ced21b344695052db1b3fd10a865b205e5c418e
Nik Tsekouras <ntsekouras@outlook.com>
2020-09-07 16:03:35 +0300

    Add tags support in Query block (#25005)
    * add tags support in Query block
    
    * refactor + tests for getTermsInfo
    
    * fetch more terms to show
    
    * add typedefs and jsdoc at getTermsInfo
    
    * fix add multiple terms
    
    * set $query properly

--- packages/block-library/src/query-loop/edit.js
+++ packages/block-library/src/query-loop/edit.js
@@ -19,7 +19,14 @@ const TEMPLATE = [ [ 'core/post-title' ], [ 'core/post-content' ] ];
 export default function QueryLoopEdit( {
 	clientId,
 	context: {
-		query: { perPage, offset, categoryIds, order, orderBy } = {},
+		query: {
+			perPage,
+			offset,
+			categoryIds,
+			tagIds = [],
+			order,
+			orderBy,
+		} = {},
 		queryContext,
 	},
 } ) {
@@ -31,6 +38,7 @@ export default function QueryLoopEdit( {
 			const query = {
 				offset: perPage ? perPage * ( page - 1 ) + offset : 0,
 				categories: categoryIds,
+				tags: tagIds,
 				order,
 				orderby: orderBy,
 			};
@@ -46,7 +54,7 @@ export default function QueryLoopEdit( {
 				blocks: select( 'core/block-editor' ).getBlocks( clientId ),
 			};
 		},
-		[ perPage, page, offset, categoryIds, order, orderBy, clientId ]
+		[ perPage, page, offset, categoryIds, tagIds, order, orderBy, clientId ]
 	);
 
 	const blockContexts = useMemo(
