b6229f72d793b895d6161056900f13eb98c5d59b
Andrés <andres.maneiro@automattic.com>
2020-09-10 09:33:35 +0200

    Typography panel: use property name in getter/setter

--- packages/edit-site/src/components/sidebar/typography-panel.js
+++ packages/edit-site/src/components/sidebar/typography-panel.js
@@ -7,7 +7,6 @@ import {
 } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { __EXPERIMENTAL_STYLE_PROPERTY as STYLE_PROPERTY } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -30,30 +29,17 @@ export default ( {
 		<PanelBody title={ __( 'Typography' ) } initialOpen={ true }>
 			{ supports.includes( 'fontSize' ) && (
 				<FontSizePicker
-					value={ fromPx(
-						getStyleProperty( name, STYLE_PROPERTY.fontSize )
-					) }
+					value={ fromPx( getStyleProperty( name, 'fontSize' ) ) }
 					onChange={ ( value ) =>
-						setStyleProperty(
-							name,
-							STYLE_PROPERTY.fontSize,
-							toPx( value )
-						)
+						setStyleProperty( name, 'fontSize', toPx( value ) )
 					}
 				/>
 			) }
 			{ supports.includes( 'lineHeight' ) && (
 				<LineHeightControl
-					value={ getStyleProperty(
-						name,
-						STYLE_PROPERTY.lineHeight
-					) }
+					value={ getStyleProperty( name, 'lineHeight' ) }
 					onChange={ ( value ) =>
-						setStyleProperty(
-							name,
-							STYLE_PROPERTY.lineHeight,
-							value
-						)
+						setStyleProperty( name, 'lineHeight', value )
 					}
 				/>
 			) }
