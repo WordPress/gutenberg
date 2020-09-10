22750a80a82ad5d8a2f47003cca072de9fe1f866
Robert Anderson <robert@noisysocks.com>
2020-09-10 13:01:55 +1000

    Navigation: Show Toolbar when there are no menus

--- packages/edit-navigation/src/components/toolbar/index.js
+++ packages/edit-navigation/src/components/toolbar/index.js
@@ -19,41 +19,47 @@ export default function Toolbar( {
 	onSelectMenu,
 	onCancelAddingMenu,
 } ) {
-	const hasSelectedMenu = !! menus && !! selectedMenuId;
-
 	return (
 		<div className="edit-navigation-toolbar">
-			{ hasSelectedMenu || isAddingMenu ? (
+			{ menus ? (
 				<>
-					{ hasSelectedMenu && (
-						<SelectControl
-							className="edit-navigation-toolbar__menu-select"
-							label={ __( 'Currently editing' ) }
-							hideLabelFromVision
-							value={ selectedMenuId }
-							options={ menus.map( ( menu ) => ( {
-								label: menu.name,
-								value: menu.id,
-							} ) ) }
-							onChange={ onSelectMenu }
-						/>
-					) }
+					<SelectControl
+						className="edit-navigation-toolbar__menu-select"
+						label={ __( 'Currently editing' ) }
+						hideLabelFromVision
+						disabled={ ! menus.length }
+						value={ selectedMenuId ?? 0 }
+						options={
+							menus.length
+								? menus.map( ( menu ) => ( {
+										value: menu.id,
+										label: menu.name,
+								  } ) )
+								: [
+										{
+											value: 0,
+											label: __(
+												'— Select navigation —'
+											),
+										},
+								  ]
+						}
+						onChange={ onSelectMenu }
+					/>
 
-					{ ! isAddingMenu && (
+					{ isAddingMenu ? (
+						<AddMenuForm
+							menus={ menus }
+							onCancel={ onCancelAddingMenu }
+							onCreate={ onSelectMenu }
+						/>
+					) : (
 						<>
 							<BlockToolbar __experimentalExpandedControl />
 							<Popover.Slot name="block-toolbar" />
 							<SaveButton menuId={ selectedMenuId } />
 							<BlockInspectorDropdown />
 						</>
-					) }
-
-					{ isAddingMenu && (
-						<AddMenuForm
-							menus={ menus }
-							onCancel={ onCancelAddingMenu }
-							onCreate={ onSelectMenu }
-						/>
 					) }
 				</>
 			) : (
