816bc30cb2697f835e4db97a09c636818c91d2cb
Noah Allen <noahtallen@gmail.com>
2020-09-14 15:43:32 -0700

    Add basic dropdown component to header item

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -1,3 +1,71 @@
-export default function DocumentActions() {
-	return <div className="edit-site-document-actions">Document Actions</div>;
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { Button, Dropdown } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+
+export default function DocumentActions( {
+	templateId,
+	secondaryItem = 'Header',
+	isSecondaryItemActive = false,
+} ) {
+	// TODO: I dislike having this here...
+	const template = useSelect(
+		( select ) => {
+			const { getEntityRecord } = select( 'core' );
+
+			return getEntityRecord( 'postType', 'wp_template', templateId );
+		},
+		[ templateId ]
+	);
+
+	return (
+		<div className="edit-site-document-actions">
+			{ template ? (
+				<Dropdown
+					position="bottom center"
+					renderToggle={ ( { onToggle, isOpen } ) => {
+						return (
+							<Button
+								onClick={ onToggle }
+								className="edit-site-document-actions__document_root"
+								aria-haspopup="true"
+								aria-expanded={ isOpen }
+							>
+								<span> { template.slug } </span>
+								{ secondaryItem && (
+									<>
+										<span className="edit-site-document-actions__separator">
+											:{ ' ' }
+										</span>
+										<span
+											className={ classnames(
+												'edit-site-document-actions__document_item',
+												{
+													'is-active':
+														isSecondaryItemActive ||
+														isOpen,
+												}
+											) }
+										>
+											{ secondaryItem }
+										</span>
+									</>
+								) }
+							</Button>
+						);
+					} }
+					renderContent={ () => <div>TODO: Document Settings</div> }
+				/>
+			) : (
+				__( 'Loading…' )
+			) }
+		</div>
+	);
 }
