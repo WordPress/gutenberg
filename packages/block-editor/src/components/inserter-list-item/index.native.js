8c25354b0132e6370a697e8ef5fdef7e0987b9b0
Carlos Garcia <fluiddot@gmail.com>
2020-09-14 22:10:51 +0200

    Add native version of inserter-list-item

---
+++ packages/block-editor/src/components/inserter-list-item/index.native.js
@@ -0,0 +1,84 @@
+/**
+ * External dependencies
+ */
+import { View, TouchableHighlight, Text } from 'react-native';
+
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { Icon } from '@wordpress/components';
+import { withPreferredColorScheme } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import styles from './style.scss';
+
+function InserterListItem( {
+	getStylesFromColorScheme,
+	item,
+	itemWidth,
+	maxWidth,
+	onSelect,
+} ) {
+	const onPress = () => {
+		onSelect( item );
+	};
+
+	const modalIconWrapperStyle = getStylesFromColorScheme(
+		styles.modalIconWrapper,
+		styles.modalIconWrapperDark
+	);
+	const modalIconStyle = getStylesFromColorScheme(
+		styles.modalIcon,
+		styles.modalIconDark
+	);
+	const modalItemLabelStyle = getStylesFromColorScheme(
+		styles.modalItemLabel,
+		styles.modalItemLabelDark
+	);
+
+	const clipboardBlockStyles = getStylesFromColorScheme(
+		styles.clipboardBlock,
+		styles.clipboardBlockDark
+	);
+
+	const isClipboardBlock = item.id === 'clipboard';
+
+	return (
+		<TouchableHighlight
+			style={ styles.touchableArea }
+			underlayColor="transparent"
+			activeOpacity={ 0.5 }
+			accessibilityLabel={ item.title }
+			onPress={ onPress }
+		>
+			<View style={ [ styles.modalItem, { width: maxWidth } ] }>
+				<View
+					style={ [
+						modalIconWrapperStyle,
+						itemWidth && {
+							width: itemWidth,
+						},
+						isClipboardBlock && clipboardBlockStyles,
+					] }
+				>
+					<View style={ modalIconStyle }>
+						<Icon
+							icon={ item.icon.src || item.icon }
+							fill={ modalIconStyle.fill }
+							size={ modalIconStyle.width }
+						/>
+					</View>
+				</View>
+				<Text style={ modalItemLabelStyle }>
+					{ isClipboardBlock ? __( 'Copied block' ) : item.title }
+				</Text>
+			</View>
+		</TouchableHighlight>
+	);
+}
+
+export default withPreferredColorScheme( InserterListItem );
