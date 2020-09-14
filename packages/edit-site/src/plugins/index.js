f241a5a7b7603b36465fa1c384da75249a3eb331
jasmussen <joen@automattic.com>
2020-09-14 08:13:42 +0200

    Remove duplicate help item.

--- packages/edit-site/src/plugins/index.js
+++ packages/edit-site/src/plugins/index.js
@@ -6,12 +6,12 @@ import downloadjs from 'downloadjs';
 /**
  * WordPress dependencies
  */
-import { MenuItem, VisuallyHidden } from '@wordpress/components';
+import { MenuItem } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 import { addQueryArgs } from '@wordpress/url';
 import apiFetch from '@wordpress/api-fetch';
-import { download, external } from '@wordpress/icons';
+import { download } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -53,23 +53,6 @@ registerPlugin( 'edit-site', {
 						} ) }
 					>
 						{ __( 'Manage all reusable blocks' ) }
-					</MenuItem>
-					<MenuItem
-						role="menuitem"
-						icon={ external }
-						href={ __(
-							'https://wordpress.org/support/article/wordpress-editor/'
-						) }
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						{ __( 'Help' ) }
-						<VisuallyHidden as="span">
-							{
-								/* translators: accessibility text */
-								__( '(opens in a new tab)' )
-							}
-						</VisuallyHidden>
 					</MenuItem>
 				</ToolsMoreMenuGroup>
 			</>
