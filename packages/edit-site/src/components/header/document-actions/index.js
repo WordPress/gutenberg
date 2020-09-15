08e4fbeca75cd1c521182032b9ad53ad6a5c075e
Noah Allen <noahtallen@gmail.com>
2020-09-15 11:50:51 -0700

    Use vertical design for header information

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -6,63 +6,41 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
-import { Button, Dropdown } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { DOWN } from '@wordpress/keycodes';
 
 export default function DocumentActions( {
 	primaryText,
 	secondaryText,
-	isSecondaryItemActive = false,
+	isSecondaryItemActive = true,
 } ) {
+	const hasSecondaryItem = !! secondaryText?.length;
 	return (
 		<div className="edit-site-document-actions">
 			{ primaryText ? (
-				<Dropdown
-					position="bottom center"
-					renderToggle={ ( { onToggle, isOpen } ) => {
-						const openOnArrowDown = ( event ) => {
-							if ( ! isOpen && event.keyCode === DOWN ) {
-								event.preventDefault();
-								event.stopPropagation();
-								onToggle();
+				<>
+					<span
+						className={ classnames(
+							'edit-site-document-actions__primary-item',
+							{
+								'is-active': ! hasSecondaryItem,
 							}
-						};
-						return (
-							<Button
-								onClick={ onToggle }
-								className="edit-site-document-actions__document_root"
-								aria-haspopup="true"
-								aria-expanded={ isOpen }
-								onKeyDown={ openOnArrowDown }
-								label={ __( 'Change document settings.' ) }
-								showTooltip
-							>
-								<span> { primaryText } </span>
-								{ secondaryText && (
-									<>
-										<span className="edit-site-document-actions__separator">
-											:{ ' ' }
-										</span>
-										<span
-											className={ classnames(
-												'edit-site-document-actions__document_item',
-												{
-													'is-active':
-														isSecondaryItemActive ||
-														isOpen,
-												}
-											) }
-										>
-											{ secondaryText }
-										</span>
-									</>
-								) }
-							</Button>
-						);
-					} }
-					renderContent={ () => <div>TODO: Document Settings</div> }
-				/>
+						) }
+					>
+						{ primaryText }
+					</span>
+					{ secondaryText && (
+						<span
+							className={ classnames(
+								'edit-site-document-actions__secondary-item',
+								{
+									'is-active': isSecondaryItemActive,
+								}
+							) }
+						>
+							{ secondaryText }
+						</span>
+					) }
+				</>
 			) : (
 				__( 'Loading…' )
 			) }
