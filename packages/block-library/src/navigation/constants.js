65e1f503cb067e04a49ec902eece184c095bafce
cpap <papazoglou.charalampos@gmail.com>
2020-09-16 11:13:47 +0300

    Navigation menu item separator: - adds separators

--- packages/block-library/src/navigation/constants.js
+++ packages/block-library/src/navigation/constants.js
@@ -5,5 +5,7 @@ import { __ } from '@wordpress/i18n';
 
 export const SEPARATOR_OPTIONS = [
 	{ value: null, label: __( 'None' ) },
-	{ value: '/', label: __( 'Forward Slash' ) },
+	{ value: '/', label: __( 'Forward Slash ( / )' ) },
+	{ value: '•', label: __( 'Bullet ( • )' ) },
+	{ value: '|', label: __( 'Pipe ( | )' ) },
 ];
