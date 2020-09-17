0b8c1a4e8a39fe7b9d93d9c8fcc53e7e6b0f1640
Allison Levine <allison.levine@automattic.com>
2020-09-17 15:05:00 -0400

    Add file block deprecation entry

---
+++ packages/block-library/src/file/deprecated.js
@@ -0,0 +1,84 @@
+/**
+ * WordPress dependencies
+ */
+import { RichText } from '@wordpress/block-editor';
+
+// Version of the file block without PR#22914 accessibility fix.
+const deprecated = [
+	{
+		"attributes": {
+			"id": {
+				"type": "number"
+			},
+			"href": {
+				"type": "string"
+			},
+			"fileName": {
+				"type": "string",
+				"source": "html",
+				"selector": "a:not([download])"
+			},
+			"textLinkHref": {
+				"type": "string",
+				"source": "attribute",
+				"selector": "a:not([download])",
+				"attribute": "href"
+			},
+			"textLinkTarget": {
+				"type": "string",
+				"source": "attribute",
+				"selector": "a:not([download])",
+				"attribute": "target"
+			},
+			"showDownloadButton": {
+				"type": "boolean",
+				"default": true
+			},
+			"downloadButtonText": {
+				"type": "string",
+				"source": "html",
+				"selector": "a[download]"
+			}
+		},
+		"supports": {
+			"align": true
+		},
+		save( { attributes } ) {
+			const {
+				href,
+				fileName,
+				textLinkHref,
+				textLinkTarget,
+				showDownloadButton,
+				downloadButtonText,
+			} = attributes;
+
+			return (
+				href && (
+					<div>
+						{ ! RichText.isEmpty( fileName ) && (
+							<a
+								href={ textLinkHref }
+								target={ textLinkTarget }
+								rel={ textLinkTarget ? 'noreferrer noopener' : false }
+							>
+								<RichText.Content value={ fileName } />
+							</a>
+						) }
+						{ showDownloadButton && (
+							<a
+								href={ href }
+								className="wp-block-file__button"
+								download={ true }
+							>
+								<RichText.Content value={ downloadButtonText } />
+							</a>
+						) }
+					</div>
+				)
+			);
+		}
+	}
+];
+
+export default deprecated;
