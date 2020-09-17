fccad8f647c60751513ff503380558845e950f67
retrofox <rdsuarez@gmail.com>
2020-09-17 17:02:27 -0300

    date-time: handling onChange

--- packages/components/src/date-time/stories/index.js
+++ packages/components/src/date-time/stories/index.js
@@ -19,7 +19,7 @@ export default {
 };
 
 const DateTimePickerWithState = ( { is12Hour } ) => {
-	const [ date, setDate ] = useState();
+	const [ date, setDate ] = useState( new Date() );
 
 	return (
 		<DateTimePicker
