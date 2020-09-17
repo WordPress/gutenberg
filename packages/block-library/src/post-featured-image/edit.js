9a89cc85fcc3188972ddc6be706386ee6a635194
ntsekouras <ntsekouras@outlook.com>
2020-09-17 15:10:46 +0300

    show PostFeaturedImage in editor

--- packages/block-library/src/post-featured-image/edit.js
+++ packages/block-library/src/post-featured-image/edit.js
@@ -1,35 +1,40 @@
 /**
  * WordPress dependencies
  */
-import { useEntityProp, useEntityId } from '@wordpress/core-data';
+import { useEntityProp } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { ResponsiveWrapper } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-function PostFeaturedImageDisplay() {
+function PostFeaturedImageDisplay( { context: { postId, postType } } ) {
 	const [ featuredImage ] = useEntityProp(
 		'postType',
-		'post',
-		'featured_media'
+		postType,
+		'featured_media',
+		postId
 	);
 	const media = useSelect(
 		( select ) =>
 			featuredImage && select( 'core' ).getMedia( featuredImage ),
 		[ featuredImage ]
 	);
-	return media ? (
+	if ( ! media ) {
+		return __( 'Featured Image is not set' );
+	}
+	const alt = media.alt_text || `{postType} Featured Media`;
+	return (
 		<ResponsiveWrapper
 			naturalWidth={ media.media_details.width }
 			naturalHeight={ media.media_details.height }
 		>
-			<img src={ media.source_url } alt="Post Featured Media" />
+			<img src={ media.source_url } alt={ alt } />
 		</ResponsiveWrapper>
-	) : null;
+	);
 }
 
-export default function PostFeaturedImageEdit() {
-	if ( ! useEntityId( 'postType', 'post' ) ) {
-		return __( 'Post Featured Image' );
+export default function PostFeaturedImageEdit( props ) {
+	if ( ! props.context?.postId ) {
+		return __( 'Featured Image' );
 	}
-	return <PostFeaturedImageDisplay />;
+	return <PostFeaturedImageDisplay { ...props } />;
 }
