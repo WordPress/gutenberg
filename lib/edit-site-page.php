9c5fe16346e5d5c2a8421ec3981d5723de52eca6
Ella van Durpe <wp@iseulde.com>
2020-09-17 15:29:34 +0300

    Edit site: fix site base URL (#25409)

--- lib/edit-site-page.php
+++ lib/edit-site-page.php
@@ -130,6 +130,7 @@ function gutenberg_edit_site_init( $hook ) {
 		'imageSizes'        => $available_image_sizes,
 		'isRTL'             => is_rtl(),
 		'maxUploadFileSize' => $max_upload_size,
+		'siteUrl'           => site_url(),
 	);
 
 	list( $color_palette, ) = (array) get_theme_support( 'editor-color-palette' );
