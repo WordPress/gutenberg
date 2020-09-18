8593bb0f0c3a6d30cd0182784d7cd50a7e32e40d
Daniel Richards <daniel.richards@automattic.com>
2020-09-18 11:06:28 +0800

    Switch to a dropdown menu for menu selector

--- packages/edit-navigation/src/components/header/menu-selector.js
+++ packages/edit-navigation/src/components/header/menu-selector.js
@@ -2,55 +2,37 @@
  * WordPress dependencies
  */
 
-import { Button, SelectControl } from '@wordpress/components';
-import { useViewportMatch } from '@wordpress/compose';
-import { useEffect, useState } from '@wordpress/element';
+import {
+	DropdownMenu,
+	MenuGroup,
+	MenuItemsChoice,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 export default function MenuSelector( { activeMenuId, menus, onSelectMenu } ) {
-	const isMobileViewport = useViewportMatch( 'small', '<' );
-	const [ selectedMenuId, setSelectedMenuId ] = useState( activeMenuId );
-
-	// If the activeMenuId changes for any reason external to the selector,
-	// e.g. creating a new menu, update the value to match.
-	useEffect( () => {
-		if ( activeMenuId !== selectedMenuId ) {
-			setSelectedMenuId( activeMenuId );
-		}
-	}, [ activeMenuId ] );
-
 	return (
-		<>
-			<SelectControl
-				label={ __( 'Select menu' ) }
-				hideLabelFromVision={ isMobileViewport }
-				disabled={ ! menus?.length }
-				value={ selectedMenuId ?? 0 }
-				options={
-					menus?.length
-						? menus.map( ( menu ) => ( {
-								value: menu.id,
-								label: menu.name,
-						  } ) )
-						: [
-								{
-									value: 0,
-									label: '-',
-								},
-						  ]
-				}
-				onChange={ ( menuId ) => setSelectedMenuId( Number( menuId ) ) }
-			/>
-			<Button
-				disabled={
-					activeMenuId === selectedMenuId || selectedMenuId === null
-				}
-				__experimentalIsFocusable
-				isSecondary
-				onClick={ () => onSelectMenu( selectedMenuId ) }
-			>
-				{ __( 'Change' ) }
-			</Button>
-		</>
+		<DropdownMenu
+			icon={ null }
+			toggleProps={ {
+				showTooltip: false,
+				children: __( 'Select menu' ),
+				isTertiary: true,
+				disabled: ! menus,
+				__experimentalIsFocusable: true,
+			} }
+		>
+			{ () => (
+				<MenuGroup>
+					<MenuItemsChoice
+						value={ activeMenuId }
+						onSelect={ onSelectMenu }
+						choices={ menus.map( ( menu ) => ( {
+							value: menu.id,
+							label: menu.name,
+						} ) ) }
+					/>
+				</MenuGroup>
+			) }
+		</DropdownMenu>
 	);
 }
