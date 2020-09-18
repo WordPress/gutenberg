1f1daae640cbc3a7852e3a5d0d735d8ddaf6525e
Adam Zieliński <adam@adamziel.com>
2020-09-18 11:39:10 +0200

    Improve readability

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { DefinePlugin } = require( 'webpack' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const postcss = require( 'postcss' );
-const { get, escapeRegExp, compact, toPairs } = require( 'lodash' );
+const { get, escapeRegExp, compact } = require( 'lodash' );
 const { basename, sep } = require( 'path' );
 
 /**
@@ -74,29 +74,6 @@ const transformBlockContent = ( content ) => {
 			)
 	);
 };
-
-const blocksDirectoryMapping = toPairs( {
-	'./packages/block-library/src/': 'build/block-library/blocks/',
-	'./packages/edit-widgets/src/blocks/': 'build/edit-widgets/blocks/',
-} );
-
-const copyBlocksWebpackMapping = blocksDirectoryMapping.flatMap(
-	( [ from, to ] ) => [
-		{
-			from: `${ from }/**/index.php`,
-			test: new RegExp( `([\\w-]+)${ escapeRegExp( sep ) }index\\.php$` ),
-			to: `${ to }/[1].php`,
-			transform: transformBlockContent,
-		},
-		{
-			from: `${ from }/*/block.json`,
-			test: new RegExp(
-				`([\\w-]+)${ escapeRegExp( sep ) }block\\.json$`
-			),
-			to: `${ to }/[1]/block.json`,
-		},
-	]
-);
 
 module.exports = {
 	optimization: {
@@ -218,7 +195,29 @@ module.exports = {
 				},
 			} ) )
 		),
-		new CopyWebpackPlugin( copyBlocksWebpackMapping ),
+		new CopyWebpackPlugin(
+			Object.entries( {
+				'./packages/block-library/src/': 'build/block-library/blocks/',
+				'./packages/edit-widgets/src/blocks/':
+					'build/edit-widgets/blocks/',
+			} ).flatMap( ( [ from, to ] ) => [
+				{
+					from: `${ from }/**/index.php`,
+					test: new RegExp(
+						`([\\w-]+)${ escapeRegExp( sep ) }index\\.php$`
+					),
+					to: `${ to }/[1].php`,
+					transform: transformBlockContent,
+				},
+				{
+					from: `${ from }/*/block.json`,
+					test: new RegExp(
+						`([\\w-]+)${ escapeRegExp( sep ) }block\\.json$`
+					),
+					to: `${ to }/[1]/block.json`,
+				},
+			] )
+		),
 		new DependencyExtractionWebpackPlugin( { injectPolyfill: true } ),
 	].filter( Boolean ),
 	watchOptions: {
