bda2b41e8784e23d05391f8be77c8aada3203db5
retrofox <rdsuarez@gmail.com>
2020-09-21 08:56:12 -0300

    date-time: starting to work on event tooltips

---
+++ packages/components/src/date-time/stories/date-with-locales.js
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import { button, select } from '@storybook/addon-knobs';
+
+//import * as locals from 'date-fns/locale';
+import * as locales from 'date-fns/esm/locale';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import DatePicker from '../date';
+
+export default {
+	title: 'Components/DatePicker',
+	component: DatePicker,
+};
+
+export const withLocales = () => {
+	const [ date, setDate ] = useState();
+
+	button( 'Select Today', () => setDate( new Date() ) );
+
+	return (
+		<DatePicker
+			currentDate={ date }
+			onChange={ setDate }
+			locale={ locales[ select( 'locale', Object.keys( locales ) ) ] }
+		/>
+	);
+};
