6daabbc4d2a3ac60810d87f3ea9f7766bc0e9cc8
Paul Bunkham <paul@dobit.co.uk>
2020-09-22 10:53:58 +0100

    Fix the key warning when rendering the Tooltips

--- packages/components/src/date-time/date.js
+++ packages/components/src/date-time/date.js
@@ -21,7 +21,7 @@ import { Button, Tooltip } from '../';
 const TIMEZONELESS_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 const isRTL = () => document.documentElement.dir === 'rtl';
 
-const DatePickerHeader = ( { date, decreaseMonth, increaseMonth ,locale } ) => (
+const DatePickerHeader = ( { date, decreaseMonth, increaseMonth, locale } ) => (
 	<div className={ 'components-datetime__date-header' }>
 		<Button
 			className={ `components-datetime__date-header-month-button is-previous-month` }
@@ -43,10 +43,10 @@ const DatePickerHeader = ( { date, decreaseMonth, increaseMonth ,locale } ) => (
 
 const renderTooltipContent = ( events ) => {
 	const needToPrune = events?.length > 3;
-	const eventsToRender = needToPrune ? events.slice( 0, 3 ) : events; 
+	const eventsToRender = needToPrune ? events.slice( 0, 3 ) : events;
 	if ( needToPrune ) {
 		eventsToRender.push( {
-			title: __( '… and more' )
+			title: __( '… and more' ),
 		} );
 	}
 
@@ -54,11 +54,11 @@ const renderTooltipContent = ( events ) => {
 		<div className="components-datetime__date--day-events">
 			<ul>
 				{ map( eventsToRender, ( event ) => (
-					<li>{ event.title || __( 'No title' ) }</li>
+					<li key={ format( event.date, 'T' ) }>{ event.title || __( 'No title' ) }</li>
 				) ) }
 			</ul>
 		</div>
-	)
+	);
 };
 
 const renderDayContents = ( date, events ) => {
@@ -66,15 +66,15 @@ const renderDayContents = ( date, events ) => {
 	if ( ! eventsByDay?.length ) {
 		return getDate( date );
 	}
-	
-	return <>
+
+	return (
 		<Tooltip text={ renderTooltipContent( eventsByDay ) }>
 			<span>{ getDate( date ).toString() }</span>
 		</Tooltip>
-	</>;
+	);
 };
 
-const handleChange = ( newDate, currentDate, onChange  ) => {
+const handleChange = ( newDate, currentDate, onChange ) => {
 	// If currentDate is null, use now as momentTime to designate hours, minutes, seconds.
 	const momentDate = new Date( currentDate ?? null );
 	const momentTime = {
@@ -109,14 +109,16 @@ const DatePicker = ( {
 			<ReactDatePicker
 				calendarClassName={ 'components-datetime__date' }
 				selected={ currentDateObj }
-				onChange={ ( newDate ) => handleChange( newDate, currentDate, onChange ) }
+				onChange={ ( newDate ) =>
+					handleChange( newDate, currentDate, onChange )
+				}
 				filterDate={ ( date ) => {
 					return ! isInvalidDate || ! isInvalidDate( date );
 				} }
 				inline
-				renderCustomHeader={ ( props ) =>
+				renderCustomHeader={ ( props ) => (
 					<DatePickerHeader { ...props } locale={ locale } />
-				}
+				) }
 				renderDayContents={ ( _, date ) =>
 					renderDayContents( date, events )
 				}
