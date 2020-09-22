4031a59750442446283526cc96fe84139ad39df3
Chip Snyder <chip.snyder3@gmail.com>
2020-09-22 15:08:36 -0400

    lint

--- packages/block-editor/src/store/defaults.native.js
+++ packages/block-editor/src/store/defaults.native.js
@@ -7,39 +7,39 @@ import {
 } from './defaults.js';
 
 /**
-  * WordPress dependencies
-*/
+ * WordPress dependencies
+ */
 import { _x } from '@wordpress/i18n';
 
 const SETTINGS_DEFAULTS = {
 	...SETTINGS,
 	alignWide: true,
-	fontSizes: [		
-		{		
-			name: _x( 'Small', 'font size name' ),		
-			size: 13,		
-			slug: 'small',		
-		},		
-		{		
-			name: _x( 'Normal', 'font size name' ),		
-			size: 16,		
-			slug: 'normal',		
-		},		
-		{		
-			name: _x( 'Medium', 'font size name' ),		
-			size: 20,		
-			slug: 'medium',		
-		},		
-		{		
-			name: _x( 'Large', 'font size name' ),		
-			size: 36,		
-			slug: 'large',		
-		},		
-		{		
-			name: _x( 'Huge', 'font size name' ),		
-			size: 42,		
-			slug: 'huge',		
-		},		
+	fontSizes: [
+		{
+			name: _x( 'Small', 'font size name' ),
+			size: 13,
+			slug: 'small',
+		},
+		{
+			name: _x( 'Normal', 'font size name' ),
+			size: 16,
+			slug: 'normal',
+		},
+		{
+			name: _x( 'Medium', 'font size name' ),
+			size: 20,
+			slug: 'medium',
+		},
+		{
+			name: _x( 'Large', 'font size name' ),
+			size: 36,
+			slug: 'large',
+		},
+		{
+			name: _x( 'Huge', 'font size name' ),
+			size: 42,
+			slug: 'huge',
+		},
 	],
 };
 
