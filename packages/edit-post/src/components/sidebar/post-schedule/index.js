4671623116af71692c07800f92352970148a093b
retrofox <rdsuarez@gmail.com>
2020-09-22 11:10:27 -0300

    post-schedule: try to avoid moving popover

--- packages/edit-post/src/components/sidebar/post-schedule/index.js
+++ packages/edit-post/src/components/sidebar/post-schedule/index.js
@@ -15,7 +15,7 @@ export function PostSchedule() {
 			<PanelRow className="edit-post-post-schedule">
 				<span className="edit-post-post-schedule__status">{ __( 'Publish' ) }</span>
 				<Dropdown
-					position="bottom left"
+					position="bottom center"
 					contentClassName="edit-post-post-schedule__dialog"
 					renderToggle={ ( { onToggle, isOpen } ) => (
 						<>
