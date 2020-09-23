925fb278b16b1cfee2be7ec7e18545a9aa12355c
David Szabo <david.szabo97@gmail.com>
2020-09-23 12:56:01 +0200

    Remove fragment

--- packages/edit-site/src/components/left-sidebar/navigation-panel/index.js
+++ packages/edit-site/src/components/left-sidebar/navigation-panel/index.js
@@ -15,42 +15,40 @@ const NavigationPanel = () => {
 	const [ showPreview, setShowPreview ] = useState( false );
 
 	return (
-		<>
-			<div className="edit-site-navigation-panel">
-				<Navigation>
-					<NavigationMenu title="Home">
+		<div className="edit-site-navigation-panel">
+			<Navigation>
+				<NavigationMenu title="Home">
+					<NavigationItem
+						item="item-back"
+						title="Back to dashboard"
+						href="index.php"
+					/>
+
+					<NavigationGroup title="Example group">
 						<NavigationItem
-							item="item-back"
-							title="Back to dashboard"
-							href="index.php"
+							item="item-preview"
+							title="Hover to show preview"
+							onMouseEnter={ () => setShowPreview( true ) }
+							onMouseLeave={ () => setShowPreview( false ) }
 						/>
+					</NavigationGroup>
+				</NavigationMenu>
+			</Navigation>
 
-						<NavigationGroup title="Example group">
-							<NavigationItem
-								item="item-preview"
-								title="Hover to show preview"
-								onMouseEnter={ () => setShowPreview( true ) }
-								onMouseLeave={ () => setShowPreview( false ) }
-							/>
-						</NavigationGroup>
-					</NavigationMenu>
-				</Navigation>
-
-				{ showPreview && (
-					<div className="edit-site-navigation-panel__preview">
-						<BlockPreview
-							blocks={ [
-								getBlockFromExample(
-									'core/paragraph',
-									getBlockType( 'core/paragraph' ).example
-								),
-							] }
-							viewportWidth={ 1200 }
-						/>
-					</div>
-				) }
-			</div>
-		</>
+			{ showPreview && (
+				<div className="edit-site-navigation-panel__preview">
+					<BlockPreview
+						blocks={ [
+							getBlockFromExample(
+								'core/paragraph',
+								getBlockType( 'core/paragraph' ).example
+							),
+						] }
+						viewportWidth={ 1200 }
+					/>
+				</div>
+			) }
+		</div>
 	);
 };
 
