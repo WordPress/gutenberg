2edb1d4c4dbc9635961535f62940a96c0c3e88ae
Copons <Copons@users.noreply.github.com>
2020-09-24 10:51:20 +0200

    Make Default more default and move less interesting examples to More Examples

--- packages/components/src/navigation/stories/default.js
+++ packages/components/src/navigation/stories/default.js
@@ -1,69 +1,69 @@
 /**
  * WordPress dependencies
  */
-import { Icon, wordpress } from '@wordpress/icons';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Navigation from '..';
-import NavigationGroup from '../group';
 import NavigationItem from '../item';
 import NavigationMenu from '../menu';
 
 export function DefaultStory() {
-	return (
-		<Navigation activeItem="item-1" className="navigation-story">
-			<NavigationMenu title="Home">
-				<NavigationGroup title="Menu Group">
-					<NavigationItem item="item-1" title="Item 1" />
-					<NavigationItem item="item-2" title="Item 2" />
-					<NavigationItem
-						badge="5"
-						item="item-sub-menu"
-						navigateToMenu="sub-menu"
-						title="Sub-Menu"
-					/>
-					<NavigationItem item="item-3" title="Item 3" />
-				</NavigationGroup>
-				<NavigationGroup title="External Links">
-					<NavigationItem
-						href="https://wordpress.org/"
-						item="item-4"
-						target="_blank"
-						title="WordPress.org"
-					/>
-					<NavigationItem item="item-5">
-						<a
-							className="navigation-story__wordpress-icon"
-							href="https://wordpress.org/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Icon icon={ wordpress } />
-						</a>
-					</NavigationItem>
-				</NavigationGroup>
-			</NavigationMenu>
+	const [ activeItem, setActiveItem ] = useState( 'item-1' );
 
-			<NavigationMenu menu="sub-menu" parentMenu="root" title="Sub-Menu">
-				<NavigationItem item="child-1" title="Child 1" />
-				<NavigationItem item="child-2" title="Child 2" />
+	return (
+		<Navigation activeItem={ activeItem } className="navigation-story">
+			<NavigationMenu title="Home">
 				<NavigationItem
-					badge="2"
+					item="item-1"
+					onClick={ () => setActiveItem( 'item-1' ) }
+					title="Item 1"
+				/>
+				<NavigationItem
+					item="item-2"
+					onClick={ () => setActiveItem( 'item-2' ) }
+					title="Item 2"
+				/>
+				<NavigationItem
+					item="item-sub-menu"
+					navigateToMenu="sub-menu"
+					title="Sub-Menu"
+				/>
+			</NavigationMenu>
+			<NavigationMenu menu="sub-menu" parentMenu="root" title="Sub-Menu">
+				<NavigationItem
+					item="child-1"
+					onClick={ () => setActiveItem( 'child-1' ) }
+					title="Child 1"
+				/>
+				<NavigationItem
+					item="child-2"
+					onClick={ () => setActiveItem( 'child-2' ) }
+					title="Child 2"
+				/>
+				<NavigationItem
 					item="child-nested-sub-menu"
 					navigateToMenu="nested-sub-menu"
 					title="Nested Sub-Menu"
 				/>
 			</NavigationMenu>
-
 			<NavigationMenu
 				menu="nested-sub-menu"
 				parentMenu="sub-menu"
 				title="Nested Sub-Menu"
 			>
-				<NavigationItem item="sub-child-1" title="Sub-Child 1" />
-				<NavigationItem item="sub-child-2" title="Sub-Child 2" />
+				<NavigationItem
+					item="sub-child-1"
+					onClick={ () => setActiveItem( 'sub-child-1' ) }
+					title="Sub-Child 1"
+				/>
+				<NavigationItem
+					item="sub-child-2"
+					onClick={ () => setActiveItem( 'sub-child-2' ) }
+					title="Sub-Child 2"
+				/>
 			</NavigationMenu>
 		</Navigation>
 	);
