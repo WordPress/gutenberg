ceb9458ccea71cee9b38ade91f91e16f862dcdc9
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2020-09-24 11:07:28 +1000

    Update social link attribute checks to use array_key_exists

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -16,10 +16,10 @@
  */
 function render_block_core_social_link( $attributes, $content, $block ) {
 	$open_in_new_tab = $block->context['openInNewTab'];
-	$service         = ( isset( $attributes['service'] ) ) ? $attributes['service'] : 'Icon';
-	$url             = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
-	$label           = ( isset( $attributes['label'] ) ) ? $attributes['label'] : block_core_social_link_get_name( $service );
-	$class_name      = isset( $attributes['className'] ) ? ' ' . $attributes['className'] : false;
+	$service         = array_key_exists( 'service', $attributes ) ? $attributes['service'] : 'Icon';
+	$url             = array_key_exists( 'url', $attributes ) ? $attributes['url'] : false;
+	$label           = array_key_exists( 'label', $attributes ) ? $attributes['label'] : block_core_social_link_get_name( $service );
+	$class_name      = array_key_exists( 'className', $attributes ) ? ' ' . $attributes['className'] : false;
 
 	// Don't render a link if there is no URL set.
 	if ( ! $url ) {
