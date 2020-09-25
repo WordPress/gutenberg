7eb52d89d5d060ecb390c4c8b800540d83dc2463
Carlos Garcia <fluiddot@gmail.com>
2020-09-25 21:20:32 +0200

    Add Lock icon

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -103,6 +103,7 @@ export { default as lifesaver } from './library/lifesaver';
 export { default as link } from './library/link';
 export { default as linkOff } from './library/link-off';
 export { default as list } from './library/list';
+export { default as lock } from './library/lock';
 export { default as loop } from './library/loop';
 export { default as mapMarker } from './library/map-marker';
 export { default as media } from './library/media';
