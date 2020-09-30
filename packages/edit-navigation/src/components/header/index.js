66e8727186e1a29e1658be6320a560d773cf56fb
Daniel Richards <daniel.richards@automattic.com>
2020-09-30 11:43:53 +0800

    Align items left and add create menu header

--- packages/edit-navigation/src/components/header/index.js
+++ packages/edit-navigation/src/components/header/index.js
@@ -21,9 +21,28 @@ import {
 import ManageLocations from './manage-locations';
 import AddMenuForm from './add-menu-form';
 
-export default function Header( { menus, selectedMenuId, onSelectMenu } ) {
+export default function Header( {
+	menus,
+	selectedMenuId,
+	onSelectMenu,
+	isPending,
+} ) {
 	const selectedMenu = find( menus, { id: selectedMenuId } );
 	const menuName = selectedMenu ? selectedMenu.name : undefined;
+	let actionHeaderText;
+
+	if ( menuName ) {
+		actionHeaderText = sprintf(
+			// translators: Name of the menu being edited, e.g. 'Main Menu'.
+			__( 'Editing: %s' ),
+			menuName
+		);
+	} else if ( isPending ) {
+		// Loading text won't be displayed if menus are preloaded.
+		actionHeaderText = __( 'Loading …' );
+	} else {
+		actionHeaderText = __( 'Create new menu' );
+	}
 
 	return (
 		<div className="edit-navigation-header">
@@ -31,79 +50,71 @@ export default function Header( { menus, selectedMenuId, onSelectMenu } ) {
 				{ __( 'Navigation' ) }
 			</h1>
 
-			<div className="edit-navigation-header__status_bar">
-				{ menuName && (
-					<h2 className="edit-navigation-header__status_header">
-						{ sprintf(
-							// translators: Name of the menu being edited, e.g. 'Main Menu'.
-							__( 'Currently editing: %s' ),
-							menuName
-						) }
-					</h2>
-				) }
+			<div className="edit-navigation-header__actions">
+				<h2 className="edit-navigation-header__action_header">
+					{ actionHeaderText }
+				</h2>
 
-				<div className="edit-navigation-header__actions">
-					<DropdownMenu
-						icon={ null }
-						toggleProps={ {
-							showTooltip: false,
-							children: __( 'Select menu' ),
-							isTertiary: true,
-							disabled: ! menus,
-							__experimentalIsFocusable: true,
-						} }
-						popoverProps={ {
-							position: 'bottom center',
-						} }
-					>
-						{ () => (
-							<MenuGroup>
-								<MenuItemsChoice
-									value={ selectedMenuId }
-									onSelect={ onSelectMenu }
-									choices={ menus.map( ( menu ) => ( {
-										value: menu.id,
-										label: menu.name,
-									} ) ) }
-								/>
-							</MenuGroup>
-						) }
-					</DropdownMenu>
-
-					<Dropdown
-						position="bottom center"
-						renderToggle={ ( { isOpen, onToggle } ) => (
-							<Button
-								isTertiary
-								aria-expanded={ isOpen }
-								onClick={ onToggle }
-							>
-								{ __( 'Add new' ) }
-							</Button>
-						) }
-						renderContent={ () => (
-							<AddMenuForm
-								menus={ menus }
-								onCreate={ onSelectMenu }
+				<DropdownMenu
+					icon={ null }
+					toggleProps={ {
+						showTooltip: false,
+						children: __( 'Select menu' ),
+						isTertiary: true,
+						disabled: ! menus,
+						__experimentalIsFocusable: true,
+					} }
+					popoverProps={ {
+						position: 'bottom center',
+					} }
+				>
+					{ () => (
+						<MenuGroup>
+							<MenuItemsChoice
+								value={ selectedMenuId }
+								onSelect={ onSelectMenu }
+								choices={ menus.map( ( menu ) => ( {
+									value: menu.id,
+									label: menu.name,
+								} ) ) }
 							/>
-						) }
-					/>
+						</MenuGroup>
+					) }
+				</DropdownMenu>
 
-					<Dropdown
-						contentClassName="edit-navigation-header__manage-locations"
-						position="bottom center"
-						renderToggle={ ( { isOpen, onToggle } ) => (
-							<Button
-								isTertiary
-								aria-expanded={ isOpen }
-								onClick={ onToggle }
-							>
-								{ __( 'Manage locations' ) }
-							</Button>
-						) }
-						renderContent={ () => <ManageLocations /> }
-					/>
-				</div>
+				<Dropdown
+					position="bottom center"
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<Button
+							isTertiary
+							aria-expanded={ isOpen }
+							onClick={ onToggle }
+						>
+							{ __( 'Add new' ) }
+						</Button>
+					) }
+					renderContent={ () => (
+						<AddMenuForm
+							menus={ menus }
+							onCreate={ onSelectMenu }
+						/>
+					) }
+				/>
+
+				<Dropdown
+					contentClassName="edit-navigation-header__manage-locations"
+					position="bottom center"
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<Button
+							isTertiary
+							aria-expanded={ isOpen }
+							onClick={ onToggle }
+						>
+							{ __( 'Manage locations' ) }
+						</Button>
+					) }
+					renderContent={ () => <ManageLocations /> }
+				/>
 			</div>
 		</div>
 	);
