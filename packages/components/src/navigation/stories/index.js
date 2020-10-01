c6fdae1721bccce59ade8716b905648f0ef21c20
David Szabo <david.szabo97@gmail.com>
2020-10-01 21:37:56 +0200

    Extract story

--- packages/components/src/navigation/stories/index.js
+++ packages/components/src/navigation/stories/index.js
@@ -11,6 +11,7 @@ import { GroupStory } from './group';
 import { ControlledStateStory } from './controlled-state';
 import { SearchStory } from './search';
 import { MoreExamplesStory } from './more-examples';
+import { HideIFEmptyStory } from './hide-if-empty';
 import './style.css';
 
 export default {
@@ -29,44 +30,4 @@ export const controlledState = () => <ControlledStateStory />;
 export const groups = () => <GroupStory />;
 export const search = () => <SearchStory />;
 export const moreExamples = () => <MoreExamplesStory />;
-
-export const Test = () => {
-	return (
-		<Navigation>
-			<NavigationMenu title="Home" menu="root" isEmpty={ false }>
-				<NavigationItem
-					navigateToMenu="root-sub-1"
-					title="To sub 1 (hidden)"
-					hideIfTargetMenuEmpty
-				/>
-
-				<NavigationItem
-					navigateToMenu="root-sub-2"
-					title="To sub 2"
-					hideIfTargetMenuEmpty
-				/>
-
-				<NavigationItem
-					navigateToMenu="root-sub-1-sub-1"
-					title="To sub 1-1 (hidden)"
-					hideIfTargetMenuEmpty
-				/>
-			</NavigationMenu>
-			<NavigationMenu
-				menu="root-sub-1"
-				parentMenu="root"
-				isEmpty={ true }
-			/>
-			<NavigationMenu
-				menu="root-sub-2"
-				parentMenu="root"
-				isEmpty={ false }
-			/>
-			<NavigationMenu
-				menu="root-sub-1-sub-1"
-				parentMenu="root-sub-1"
-				isEmpty={ true }
-			/>
-		</Navigation>
-	);
-};
+export const hideIfEmpty = () => <HideIFEmptyStory />;
