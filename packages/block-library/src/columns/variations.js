f5dafbb1987842e9d2e8a1b8f1b7be857ff3e806
retrofox <rdsuarez@gmail.com>
2020-10-05 17:33:31 -0300

    columns-block: add 100 column variation

--- packages/block-library/src/columns/variations.js
+++ packages/block-library/src/columns/variations.js
@@ -13,6 +13,28 @@ import { __ } from '@wordpress/i18n';
  */
 const variations = [
 	{
+		name: 'one-colum',
+		title: __( '100' ),
+		description: __( 'One Column' ),
+		icon: (
+			<SVG
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<Path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M39 12C40.1046 12 41 12.8954 41 14V34C41 35.1046 40.1046 36 39 36H9C7.89543 36 7 35.1046 7 34V14C7 12.8954 7.89543 12 9 12H39ZM39 34V14H25V34H39ZM23 34H9V14H23V34Z"
+				/>
+			</SVG>
+		),
+		isDefault: true,
+		innerBlocks: [ [ 'core/column' ] ],
+		scope: [ 'block' ],
+	},
+	{
 		name: 'two-columns-equal',
 		title: __( '50 / 50' ),
 		description: __( 'Two columns; equal split' ),
