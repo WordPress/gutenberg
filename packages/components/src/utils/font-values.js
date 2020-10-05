4a1d19f534ee570ee7220c010e4fe703750fe9cb
Sara Marcondes <saram@fastmail.com>
2020-10-05 12:00:35 -0700

    Add font style-mixins

---
+++ packages/components/src/utils/font-values.js
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import { css } from '@emotion/core';
+
+const DEFAULT = {
+	fontFamily: css`
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+	`,
+	fontSize: css`
+		font-size: 13px;
+	`,
+};
+
+export default {
+	default: DEFAULT,
+};
