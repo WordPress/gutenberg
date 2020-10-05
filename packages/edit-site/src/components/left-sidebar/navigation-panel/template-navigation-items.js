1608e3a721b453dc48b9e519f8ccfc8a05a92372
David Szabo <david.szabo97@gmail.com>
2020-10-05 14:07:14 +0200

    Add titles

--- packages/edit-site/src/components/left-sidebar/navigation-panel/template-navigation-items.js
+++ packages/edit-site/src/components/left-sidebar/navigation-panel/template-navigation-items.js
@@ -3,6 +3,37 @@
  */
 import { __experimentalNavigationItem as NavigationItem } from '@wordpress/components';
 
+const TITLES = {
+	'front-page': {
+		title: 'Front page',
+		description: '',
+	},
+	archive: {
+		title: 'Archive',
+		description: '',
+	},
+	single: {
+		title: 'Single',
+		description: '',
+	},
+	singular: {
+		title: 'Singular',
+		description: '',
+	},
+	index: {
+		title: 'Default (index)',
+		description: '',
+	},
+	search: {
+		title: 'Search results',
+		description: '',
+	},
+	'404': {
+		title: '404',
+		description: '',
+	},
+};
+
 export default function TemplateNavigationItems( { templates, onActivate } ) {
 	if ( ! templates ) {
 		return null;
@@ -16,7 +47,7 @@ export default function TemplateNavigationItems( { templates, onActivate } ) {
 		<NavigationItem
 			key={ `template-${ template.id }` }
 			item={ `template-${ template.slug }` }
-			title={ template.slug }
+			title={ TITLES[ template.slug ]?.title ?? template.slug }
 			onClick={ () => onActivate( template.id ) }
 		/>
 	) );
