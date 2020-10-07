60d3068b36137cfeb27cd5db710c3551bd9c2aea
Adam Zieliński <adam@adamziel.com>
2020-10-07 12:54:37 +0200

    Prevent network requests related to ephemeral posts in the widgets editor

--- packages/api-fetch/src/index.js
+++ packages/api-fetch/src/index.js
@@ -66,6 +66,10 @@ const defaultFetchHandler = ( nextOptions ) => {
 	const { url, path, data, parse = true, ...remainingOptions } = nextOptions;
 	let { body, headers } = nextOptions;
 
+	if ( nextOptions?.responseBody ) {
+		return nextOptions.responseBody;
+	}
+
 	// Merge explicitly-provided headers with default values.
 	headers = { ...DEFAULT_HEADERS, ...headers };
 
