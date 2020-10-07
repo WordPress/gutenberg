e20859d2c41c8d41820803999d43569df8d29fa3
Addison-Stavlo <stavz01@gmail.com>
2020-10-07 15:51:00 -0400

    add visually hidden prompt

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -12,6 +12,8 @@ import {
 	getBlockType,
 } from '@wordpress/blocks';
 import { useSelect } from '@wordpress/data';
+import { VisuallyHidden } from '@wordpress/components';
+
 import { last } from 'lodash';
 
 function getBlockDisplayText( block ) {
@@ -83,18 +85,23 @@ export default function DocumentActions( { documentTitle } ) {
 			} ) }
 		>
 			{ documentTitle ? (
-				<h1>
-					<div
-						className={ classnames(
-							'edit-site-document-actions__title',
-							{
-								'is-active': isTitleActive,
-								'is-secondary-title-active': isActive,
-							}
-						) }
-					>
-						{ documentTitle }
-					</div>
+				<>
+					<h1 className="edit-site-document-actions__title-wrapper">
+						<VisuallyHidden>
+							{ __( 'Edit document:' ) }
+						</VisuallyHidden>
+						<div
+							className={ classnames(
+								'edit-site-document-actions__title',
+								{
+									'is-active': isTitleActive,
+									'is-secondary-title-active': isActive,
+								}
+							) }
+						>
+							{ documentTitle }
+						</div>
+					</h1>
 					<div
 						className={ classnames(
 							'edit-site-document-actions__secondary-item',
@@ -105,7 +112,7 @@ export default function DocumentActions( { documentTitle } ) {
 					>
 						{ label ?? '' }
 					</div>
-				</h1>
+				</>
 			) : (
 				__( 'Loading…' )
 			) }
