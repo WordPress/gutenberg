1c713395cef139cedc0cf24c3b551dc8cfb3aaab
Noah Allen <noahtallen@gmail.com>
2020-10-07 15:41:02 -0700

    Fix import

--- packages/edit-site/src/components/template-details/index.js
+++ packages/edit-site/src/components/template-details/index.js
@@ -6,7 +6,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import getTemplateInfo from '../../utils';
+import { getTemplateInfo } from '../../utils';
 
 export default function TemplateDetails( { template } ) {
 	if ( ! template ) {
