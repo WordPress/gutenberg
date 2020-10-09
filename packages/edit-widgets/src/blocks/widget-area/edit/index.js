240c6ccba572c665dcbcd2ba615cbf7a91b74f2f
Adam Zieliński <adam@adamziel.com>
2020-10-09 15:02:44 +0200

    First stab at opening widget areas when block is dragged over them

--- packages/edit-widgets/src/blocks/widget-area/edit/index.js
+++ packages/edit-widgets/src/blocks/widget-area/edit/index.js
@@ -6,6 +6,7 @@ import { DisclosureContent } from 'reakit/Disclosure';
 /**
  * WordPress dependencies
  */
+import { useCallback } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { EntityProvider } from '@wordpress/core-data';
 import { Panel, PanelBody } from '@wordpress/components';
@@ -20,36 +21,47 @@ export default function WidgetAreaEdit( {
 	className,
 	attributes: { id, name },
 } ) {
-	const isOpen = useSelect(
-		( select ) =>
-			select( 'core/edit-widgets' ).getIsWidgetAreaOpen( clientId ),
+	const { isOpen, isDraggingBlocks } = useSelect(
+		( select ) => ( {
+			isOpen: select( 'core/edit-widgets' ).getIsWidgetAreaOpen(
+				clientId
+			),
+			isDraggingBlocks: select( 'core/block-editor' ).isDraggingBlocks(),
+		} ),
 		[ clientId ]
 	);
 	const { setIsWidgetAreaOpen } = useDispatch( 'core/edit-widgets' );
+	const openOnDragOver = useCallback( () => {
+		if ( ! isOpen && isDraggingBlocks ) {
+			setIsWidgetAreaOpen( clientId, true );
+		}
+	}, [ clientId, isOpen, setIsWidgetAreaOpen, isDraggingBlocks ] );
 
 	return (
-		<Panel className={ className }>
-			<PanelBody
-				title={ name }
-				opened={ isOpen }
-				onToggle={ () => {
-					setIsWidgetAreaOpen( clientId, ! isOpen );
-				} }
-			>
-				{ ( { opened } ) => (
-					// This is required to ensure LegacyWidget blocks are not unmounted when the panel is collapsed.
-					// Unmounting legacy widgets may have unintended consequences (e.g. TinyMCE not being properly reinitialized)
-					<DisclosureContent visible={ opened }>
-						<EntityProvider
-							kind="root"
-							type="postType"
-							id={ `widget-area-${ id }` }
-						>
-							<WidgetAreaInnerBlocks />
-						</EntityProvider>
-					</DisclosureContent>
-				) }
-			</PanelBody>
-		</Panel>
+		<div onDragOver={ openOnDragOver }>
+			<Panel className={ className }>
+				<PanelBody
+					title={ name }
+					opened={ isOpen }
+					onToggle={ () => {
+						setIsWidgetAreaOpen( clientId, ! isOpen );
+					} }
+				>
+					{ ( { opened } ) => (
+						// This is required to ensure LegacyWidget blocks are not unmounted when the panel is collapsed.
+						// Unmounting legacy widgets may have unintended consequences (e.g. TinyMCE not being properly reinitialized)
+						<DisclosureContent visible={ opened }>
+							<EntityProvider
+								kind="root"
+								type="postType"
+								id={ `widget-area-${ id }` }
+							>
+								<WidgetAreaInnerBlocks />
+							</EntityProvider>
+						</DisclosureContent>
+					) }
+				</PanelBody>
+			</Panel>
+		</div>
 	);
 }
