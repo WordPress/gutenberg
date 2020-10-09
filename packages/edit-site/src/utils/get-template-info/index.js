7996ca008b43d6c8ee7a9b2adcfab7f90ef573fb
Noah Allen <noahtallen@gmail.com>
2020-10-09 13:23:22 -0700

    Add documentation for template info function

--- packages/edit-site/src/utils/get-template-info/index.js
+++ packages/edit-site/src/utils/get-template-info/index.js
@@ -3,6 +3,13 @@
  */
 import { TEMPLATES_DEFAULT_DETAILS } from './constants';
 
+/**
+ * Given a template entity, return information about it which is ready to be
+ * rendered, such as the title and description.
+ *
+ * @param {Object} template The template for which we need information.
+ * @return {Object} Information about the template, including title and description.
+ */
 export default function getTemplateInfo( template ) {
 	if ( ! template ) {
 		return {};
