fc8adc22b252ea50b9b46d31250161c03a7d903f
Addison-Stavlo <stavz01@gmail.com>
2020-10-09 11:31:45 -0400

    update identifier from textDomain to stylesheet

--- packages/block-library/src/template-part/index.php
+++ packages/block-library/src/template-part/index.php
@@ -15,11 +15,12 @@
 function render_block_core_template_part( $attributes ) {
 	$content = null;
 
+
 	if ( ! empty( $attributes['postId'] ) && get_post_status( $attributes['postId'] ) ) {
 		// If we have a post ID and the post exists, which means this template part
 		// is user-customized, render the corresponding post content.
 		$content = get_post( $attributes['postId'] )->post_content;
-	} elseif ( isset( $attributes['theme'] ) && wp_get_theme()->get( 'TextDomain' ) === $attributes['theme'] ) {
+	} elseif ( isset( $attributes['theme'] ) && wp_get_theme()->stylesheet === $attributes['theme'] ) {
 		$template_part_query = new WP_Query(
 			array(
 				'post_type'      => 'wp_template_part',
