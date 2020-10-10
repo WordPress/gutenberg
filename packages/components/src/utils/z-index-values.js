e15e106019efaf6cdc1a25869e06af9c610bc19c
Sara Marcondes <saram@fastmail.com>
2020-10-09 17:04:58 -0700

    Add zIndex mixin

---
+++ packages/components/src/utils/z-index-values.js
@@ -0,0 +1,5 @@
+export default {
+	// Renders the form-toggle input above the track allowing all clicks
+	// to register on the input rather than on the track or thumb
+	'.components-form-toggle__input': 1,
+};
