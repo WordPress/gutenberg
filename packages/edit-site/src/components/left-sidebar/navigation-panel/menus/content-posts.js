5830aaa6cd83700695708c993e4fbd62d196064e
David Szabo <david.szabo97@gmail.com>
2020-10-12 11:14:44 +0200

    Add all posts

--- packages/edit-site/src/components/left-sidebar/navigation-panel/menus/content-posts.js
+++ packages/edit-site/src/components/left-sidebar/navigation-panel/menus/content-posts.js
@@ -1,7 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { __experimentalNavigationMenu as NavigationMenu } from '@wordpress/components';
+import {
+	__experimentalNavigationMenu as NavigationMenu,
+	__experimentalNavigationItem as NavigationItem,
+} from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -10,12 +14,39 @@ import { __ } from '@wordpress/i18n';
 import NavigationEntityItems from '../navigation-entity-items';
 
 export default function ContentPostsMenu() {
+	const showOnFront = useSelect(
+		( select ) =>
+			select( 'core' ).getEditedEntityRecord( 'root', 'site' )
+				.show_on_front,
+		[]
+	);
+
+	const { setPage } = useDispatch( 'core/edit-site' );
+
+	const onActivateFrontItem = () => {
+		setPage( {
+			type: 'page',
+			path: '/',
+			context: {
+				query: { categoryIds: [] },
+				queryContext: { page: 1 },
+			},
+		} );
+	};
+
 	return (
 		<NavigationMenu
 			menu="content-posts"
 			title={ __( 'Posts' ) }
 			parentMenu="root"
 		>
+			{ showOnFront === 'posts' && (
+				<NavigationItem
+					item={ 'content-/' }
+					title={ __( 'All Posts' ) }
+					onClick={ onActivateFrontItem }
+				/>
+			) }
 			<NavigationEntityItems kind="postType" name="post" />
 		</NavigationMenu>
 	);
