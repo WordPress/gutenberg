51938693e8878ffe27750ae4ca412166dae510af
Riad Benguella <benguella@gmail.com>
2020-10-12 15:22:31 +0100

    Fix end2end test

--- packages/editor/src/components/page-attributes/parent.js
+++ packages/editor/src/components/page-attributes/parent.js
@@ -22,24 +22,29 @@ import { useSelect, useDispatch } from '@wordpress/data';
  */
 import { buildTermsTree } from '../../utils/terms';
 
+function getTitle( post ) {
+	return post.title && post.title.rendered
+		? post.title.rendered
+		: `#${ post.id } (${ __( 'no title' ) })`;
+}
+
 export function PageAttributesParent() {
 	const { editPost } = useDispatch( 'core/editor' );
 	const { parentPost, parentPostId } = useSelect( ( select ) => {
-		const { getEntityRecords } = select( 'core' );
+		const { getEntityRecord } = select( 'core' );
 		const { getEditedPostAttribute } = select( 'core/editor' );
 		const postTypeSlug = getEditedPostAttribute( 'type' );
 		const pageId = getEditedPostAttribute( 'parent' );
 
 		return {
 			parentPostId: pageId,
-			parentPost: getEntityRecords( 'postType', postTypeSlug, {
-				include: [ pageId ],
-			} ),
+			parentPost: pageId
+				? getEntityRecord( 'postType', postTypeSlug, pageId )
+				: null,
 		};
 	}, [] );
-	const [ fieldValue, setFieldValue ] = useState(
-		parentPost ? parentPost[ 0 ].title.raw : false
-	);
+	const [ fieldValue, setFieldValue ] = useState( false );
+
 	const { isLoading, parent, items, postType } = useSelect(
 		( select ) => {
 			const { getPostType, getEntityRecords } = select( 'core' );
@@ -63,6 +68,7 @@ export function PageAttributesParent() {
 				query.search = fieldValue;
 			}
 			const theParentID = getEditedPostAttribute( 'parent' );
+
 			return {
 				parent: theParentID,
 				items: isHierarchical
@@ -97,31 +103,11 @@ export function PageAttributesParent() {
 			pageItems.map( ( item ) => ( {
 				id: item.id,
 				parent: item.parent,
-				name:
-					item.title && item.title.rendered
-						? item.title.rendered
-						: `#${ item.id } (${ __( 'no title' ) })`,
+				name: getTitle( item ),
 			} ) )
 		);
 		const opts = getOptionsFromTree( tree );
 
-		// Ensure the current page is included in the dropdown list (except when searching).
-		const foundParent = opts.findIndex( ( { key } ) => parent === key );
-		if (
-			opts.length > 0 &&
-			foundParent < 0 &&
-			parentPost &&
-			parent &&
-			fieldValue === parentPost[ 0 ].title.rendered
-		) {
-			return [
-				{
-					value: parentPost[ 0 ].id,
-					label: parentPost[ 0 ].title.rendered,
-				},
-				...opts,
-			];
-		}
 		return opts;
 	}, [ parent, parentPost, pageItems ] );
 
