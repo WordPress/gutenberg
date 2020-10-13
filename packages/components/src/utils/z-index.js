f03d8ea018e8d4b922afe65d517b9fd3c149b906
Sara Marcondes <saram@fastmail.com>
2020-10-13 15:46:27 -0700

    Refactor zIndex to match G2 components

--- packages/components/src/utils/z-index.js
+++ packages/components/src/utils/z-index.js
@@ -2,10 +2,14 @@
  * External dependencies
  */
 import { get } from 'lodash';
+import { css } from '@emotion/core';
 
 /**
  * Internal dependencies
  */
 import Z_INDEX from './z-index-values';
 
-export const zIndex = ( key ) => get( Z_INDEX, key, 0 );
+export const zIndex = ( key ) =>
+	css`
+		z-index: ${ get( Z_INDEX, key, 0 ) };
+	`;
