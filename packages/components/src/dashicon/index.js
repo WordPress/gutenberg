087c182747b7557f744aeea580323726dc3d1547
Jon Surrell <jon.surrell@automattic.com>
2020-10-13 18:12:39 +0200

    Type Dashicon

--- packages/components/src/dashicon/index.js
+++ packages/components/src/dashicon/index.js
@@ -1,3 +1,16 @@
+/**
+ * @typedef OwnProps
+ *
+ * @property {string} icon        Icon name
+ * @property {number} [size=20]   Icon size
+ * @property {string} [className] Class name
+ */
+/** @typedef {import('react').ComponentPropsWithoutRef<'span'> & OwnProps} Props */
+
+/**
+ * @param {Props} props
+ * @return {JSX.Element} Element
+ */
 function Dashicon( { icon, size = 20, className, ...extraProps } ) {
 	const iconClass = [
 		'dashicon',
@@ -11,6 +24,10 @@ function Dashicon( { icon, size = 20, className, ...extraProps } ) {
 	return (
 		<span
 			className={ iconClass }
+			// Ignore reason: span attributes are the global attribtues which do not include width/height
+			// See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/span
+			// See https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes
+			// @ts-ignore
 			width={ size }
 			height={ size }
 			{ ...extraProps }
