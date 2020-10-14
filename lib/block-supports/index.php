f74925b5ec4c2194af875f2a8897e5b4268ad1c6
Miguel Fonseca <miguelcsf@gmail.com>
2020-10-14 12:37:01 +0100

    clean up a bit

--- lib/block-supports/index.php
+++ lib/block-supports/index.php
@@ -179,7 +179,7 @@ $block_supports_config = array(
  * @param  string $value Class(es) to insert at the root element.
  * @return string HTML with class(es) inserted.
  */
-function foo_inject_class( $input, $value ) {
+function gutenberg_block_supports_inject_classes( $input, $value ) {
 	$close_token    = '>';
 	$class_token    = 'class="';
 	$close_position = strpos( $input, $close_token );
@@ -216,7 +216,7 @@ function foo_inject_class( $input, $value ) {
  * @param  string $value Inline styles to insert at the root element.
  * @return string HTML with styles inserted.
  */
-function foo_inject_style( $input, $value ) {
+function gutenberg_block_supports_inject_style( $input, $value ) {
 	$close_token    = '>';
 	$style_token    = 'style="';
 	$close_position = strpos( $input, $close_token );
@@ -264,7 +264,7 @@ add_action( 'init', 'gutenberg_register_block_supports', 21 );
 /**
  * TODO.
  */
-function foo_register_block_supports() {
+function gutenberg_block_supports_register_attributes() {
 	global $block_supports_config;
 
 	wp_register_style( 'wp-block-supports', false );
@@ -296,15 +296,15 @@ function foo_register_block_supports() {
 		}
 	}
 }
-add_action( 'init', 'foo_register_block_supports', 22 );
+add_action( 'init', 'gutenberg_block_supports_register_attributes', 22 );
 
 /**
  * TODO.
  */
-function foo_enqueue_style() {
+function gutenberg_block_supports_enqueue_style() {
 	wp_enqueue_style( 'wp-block-supports' );
 }
-add_action( 'wp_footer', 'foo_enqueue_style' );
+add_action( 'wp_footer', 'gutenberg_block_supports_enqueue_style' );
 
 /**
  * TODO.
@@ -388,123 +388,28 @@ function gutenberg_block_supports_classes() {
 }
 
 
-// @codingStandardsIgnoreStart
-// /**
-//  * Filters the frontend output of blocks and apply the block support flags transformations.
-//  *
-//  * @param  string $block_content rendered block content.
-//  * @param  array  $block block object.
-//  * @return string filtered block content.
-//  */
-// function gutenberg_apply_block_supports( $block_content, $block ) {
-// 	die( 42 );
-// 	if ( ! isset( $block['attrs'] ) ) {
-// 		return $block_content;
-// 	}
-//
-// 	$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block['blockName'] );
-// 	// If no render_callback, assume styles have been previously handled.
-// 	if ( ! $block_type || ! $block_type->render_callback ) {
-// 		return $block_content;
-// 	}
-//
-// 	$attributes = array();
-// 	$attributes = gutenberg_apply_colors_support( $attributes, $block['attrs'], $block_type );
-//
-// 	if ( ! count( $attributes ) ) {
-// 		return $block_content;
-// 	}
-//
-// 	$dom = new DOMDocument( '1.0', 'utf-8' );
-//
-// 	// Suppress DOMDocument::loadHTML warnings from polluting the front-end.
-// 	$previous = libxml_use_internal_errors( true );
-//
-// 	// We need to wrap the block in order to handle UTF-8 properly.
-// 	$wrapped_block_html =
-// 		'<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>'
-// 		. $block_content
-// 		. '</body></html>';
-//
-// 	$success = $dom->loadHTML( $wrapped_block_html, LIBXML_HTML_NODEFDTD | LIBXML_COMPACT );
-//
-// 	// Clear errors and reset the use_errors setting.
-// 	libxml_clear_errors();
-// 	libxml_use_internal_errors( $previous );
-//
-// 	if ( ! $success ) {
-// 		return $block_content;
-// 	}
-//
-// 	// Structure is like `<html><head/><body/></html>`, so body is the `lastChild` of our document.
-// 	$body_element = $dom->documentElement->lastChild; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-//
-// 	$xpath      = new DOMXPath( $dom );
-// 	$block_root = $xpath->query( './*', $body_element )[0];
-//
-// 	// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-// 	if ( empty( $block_root ) ) {
-// 		return $block_content;
-// 	}
-//
-// 	// Merge and dedupe new and existing classes and styles.
-// 	$current_classes = explode( ' ', trim( $block_root->getAttribute( 'class' ) ) );
-// 	$classes_to_add  = array_key_exists( 'css_classes', $attributes ) ? $attributes['css_classes'] : array();
-// 	$new_classes     = array_unique( array_filter( array_merge( $current_classes, $classes_to_add ) ) );
-//
-// 	$current_styles = preg_split( '/\s*;\s*/', trim( $block_root->getAttribute( 'style' ) ) );
-// 	$styles_to_add  = array_key_exists( 'inline_styles', $attributes ) ? $attributes['inline_styles'] : array();
-// 	$new_styles     = array_unique( array_map( 'gutenberg_normalize_css_rule', array_filter( array_merge( $current_styles, $styles_to_add ) ) ) );
-//
-// 	// Apply new styles and classes.
-// 	if ( ! empty( $new_classes ) ) {
-// 		// `DOMElement::setAttribute` handles attribute value escaping.
-// 		$block_root->setAttribute( 'class', implode( ' ', $new_classes ) );
-// 	}
-//
-// 	if ( ! empty( $new_styles ) ) {
-// 		// `DOMElement::setAttribute` handles attribute value escaping.
-// 		$block_root->setAttribute( 'style', implode( '; ', $new_styles ) . ';' );
-// 	}
-//
-// 	// Avoid using `$dom->saveHtml( $node )` because the node results may not produce consistent
-// 	// whitespace for PHP < 7.3. Saving the root HTML `$dom->saveHtml()` prevents this behavior.
-// 	$full_html = $dom->saveHtml();
-//
-// 	// Find the <body> open/close tags. The open tag needs to be adjusted so we get inside the tag
-// 	// and not the tag itself.
-// 	$start = strpos( $full_html, '<body>', 0 ) + strlen( '<body>' );
-// 	$end   = strpos( $full_html, '</body>', $start );
-// 	return trim( substr( $full_html, $start, $end - $start ) );
-// }
-// add_filter( 'render_block', 'gutenberg_apply_block_supports', 10, 2 );
-// @codingStandardsIgnoreEnd
-
-
 /**
  * TODO.
  *
  * @param  string $block_content rendered block content.
  * @param  array  $block block object.
  */
-function foo_inject_stuff( $block_content, $block ) {
+function gutenberg_block_supports_inject_attributes( $block_content, $block ) {
+	if ( ! isset( $block['attrs'] ) ) {
+		return $block_content;
+	}
+
+	$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block['blockName'] );
+	// If no render_callback, assume styles have been previously handled.
+	if ( ! $block_type || ! $block_type->render_callback ) {
+		return $block_content;
+	}
+
 	$classes = gutenberg_block_supports_classes();
 	if ( ! empty( $classes ) ) {
-		return foo_inject_class( $block_content, $classes );
+		return gutenberg_block_supports_inject_classes( $block_content, $classes );
 	}
+
 	return $block_content;
 }
-add_filter( 'render_block', 'foo_inject_stuff', 10, 2 );
-
-/**
- * Normalizes spacing in a string representing a CSS rule
- *
- * @example
- * 'color  :red;' becomes 'color:red'
- *
- * @param  string $css_rule_string CSS rule.
- * @return string Normalized CSS rule.
- */
-function gutenberg_normalize_css_rule( $css_rule_string ) {
-	return trim( implode( ': ', preg_split( '/\s*:\s*/', $css_rule_string, 2 ) ), ';' );
-}
+add_filter( 'render_block', 'gutenberg_block_supports_inject_attributes', 10, 2 );
