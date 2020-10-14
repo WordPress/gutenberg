45599fd716e2f8140e6632c9f29430cbc88408ab
Timothy Jacobs <timothy@ironbounddesigns.com>
2020-10-13 23:59:58 -0400

    Syntax error

--- lib/load.php
+++ lib/load.php
@@ -37,6 +37,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	}
 	if ( ! class_exists( 'WP_REST_Widget_Types_Controller' ) ) {
 		require_once dirname( __FILE__ ) . '/class-wp-rest-widget-types-controller.php';
+	}
 	if ( ! class_exists( 'WP_REST_Widgets_Controller' ) ) {
 		require_once dirname( __FILE__ ) . '/class-wp-rest-widgets-controller.php';
 	}
