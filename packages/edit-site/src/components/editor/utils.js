833d25d001a91b3b13a3d4c330db5d26d0ca1e2c
Jorge <jorge.costa@developer.pt>
2020-10-16 22:12:55 +0100

    Global styles: Add preset classes generation on the client side.

--- packages/edit-site/src/components/editor/utils.js
+++ packages/edit-site/src/components/editor/utils.js
@@ -5,6 +5,21 @@ export const PRESET_CATEGORIES = {
 	gradient: { path: [ 'color', 'gradients' ], key: 'gradient' },
 	fontSize: { path: [ 'typography', 'fontSizes' ], key: 'size' },
 };
+export const PRESET_CLASSES = {
+	color: { ...PRESET_CATEGORIES.color, property: 'color' },
+	'background-color': {
+		...PRESET_CATEGORIES.color,
+		property: 'background-color',
+	},
+	'gradient-background': {
+		...PRESET_CATEGORIES.gradient,
+		property: 'background',
+	},
+	'font-size': {
+		...PRESET_CATEGORIES.fontSize,
+		property: 'font-size',
+	},
+};
 export const LINK_COLOR = '--wp--style--color--link';
 export const LINK_COLOR_DECLARATION = `a { color: var(${ LINK_COLOR }, #00e); }`;
 
