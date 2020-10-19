8e5caf2e523742f54a27da67f10ae1f3e8b76891
Adam Zieliński <adam@adamziel.com>
2020-10-19 16:30:09 +0200

    Fix authorship after merge/rebase (2/2)

--- packages/edit-widgets/src/store/batch-processing/actions.js
+++ packages/edit-widgets/src/store/batch-processing/actions.js
@@ -1,3 +1,23 @@
+/**
+ * External dependencies
+ */
+import { v4 as uuid } from 'uuid';
+
+/**
+ * Internal dependencies
+ */
+import {
+	select,
+	dispatch,
+	enqueueItemAndAutocommit as enqueueAutocommitControl,
+	processTransaction,
+} from './controls';
+import { STATE_ERROR, STATE_SUCCESS } from './constants';
+
+export const enqueueItemAndAutocommit = function* ( queue, context, item ) {
+	return yield enqueueAutocommitControl( queue, context, item );
+};
+
 export const enqueueItemAndWaitForResults = function* ( queue, context, item ) {
 	const { itemId } = yield dispatch( 'enqueueItem', queue, context, item );
 	const { promise } = yield* getOrSetupPromise( queue, context );
@@ -10,6 +30,17 @@ export const enqueueItemAndWaitForResults = function* ( queue, context, item ) {
 
 			return batch.results[ itemId ];
 		} ),
+	};
+};
+
+export const enqueueItem = function ( queue, context, item ) {
+	const itemId = uuid();
+	return {
+		type: 'ENQUEUE_ITEM',
+		queue,
+		context,
+		item,
+		itemId,
 	};
 };
 
@@ -38,4 +69,106 @@ const getOrSetupPromise = function* ( queue, context ) {
 	yield setupPromise( queue, context );
 
 	return yield select( 'getPromise', queue, context );
+};
+
+export const processBatch = function* ( queue, context, meta = {} ) {
+	const batchId = uuid();
+	yield prepareBatchForProcessing( queue, context, batchId, meta );
+	const { transactions } = yield select( 'getBatch', batchId );
+
+	yield {
+		queue,
+		context,
+		batchId,
+		type: 'BATCH_START',
+	};
+
+	let failed = false;
+	for ( const transactionId of Object.keys( transactions ) ) {
+		const result = yield* commitTransaction( batchId, transactionId );
+		if ( result.state === STATE_ERROR ) {
+			failed = true;
+			// Don't break the loop as we still need results for any remaining transactions.
+			// Queue processor receives the batch object and may choose whether to
+			// process other transactions or short-circuit with an error.
+		}
+	}
+
+	const promise = yield select( 'getPromise', queue, context );
+	yield {
+		queue,
+		context,
+		batchId,
+		type: 'BATCH_FINISH',
+		state: failed ? STATE_ERROR : STATE_SUCCESS,
+	};
+	const batch = yield select( 'getBatch', batchId );
+
+	if ( promise ) {
+		promise.resolve( batch );
+	}
+
+	return batch;
+};
+
+export function* commitTransaction( batchId, transactionId ) {
+	yield {
+		batchId,
+		transactionId,
+		type: 'COMMIT_TRANSACTION_START',
+	};
+	const batch = yield select( 'getBatch', batchId );
+
+	let failed = false,
+		errors,
+		exception,
+		results;
+	try {
+		results = yield processTransaction( batch, transactionId );
+	} catch ( _exception ) {
+		failed = true;
+
+		// If the error isn't in the expected format, something is wrong - let's rethrow
+		if ( ! _exception.isTransactionError ) {
+			throw _exception;
+		}
+		exception = _exception;
+		errors = exception.errorsById;
+	}
+
+	const finishedAction = {
+		batchId,
+		transactionId,
+		results,
+		errors,
+		exception,
+		type: 'COMMIT_TRANSACTION_FINISH',
+		state: failed ? STATE_ERROR : STATE_SUCCESS,
+	};
+
+	yield finishedAction;
+	return finishedAction;
+}
+
+export function prepareBatchForProcessing(
+	queue,
+	context,
+	batchId,
+	meta = {}
+) {
+	return {
+		type: 'PREPARE_BATCH_FOR_PROCESSING',
+		queue,
+		context,
+		batchId,
+		meta,
+	};
+}
+
+export const registerProcessor = function ( queue, callback ) {
+	return {
+		type: 'REGISTER_PROCESSOR',
+		queue,
+		callback,
+	};
 };
--- packages/edit-widgets/src/store/batch-processing/selectors.js
+++ packages/edit-widgets/src/store/batch-processing/selectors.js
@@ -1,3 +1,10 @@
+export const getBatch = ( state, batchId ) => {
+	return state.batches[ batchId ];
+};
+
+export const getProcessor = ( state, queue ) => {
+	return state.processors[ queue ];
+};
 
 export const getPromise = ( state, queue, context ) => {
 	return state.promises[ queue ]?.[ context ];
