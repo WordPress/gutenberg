15e520facfcd828fa2418a1357acc7b868bd4a66
Glen Davies <glen.davies@a8c.com>
2020-10-20 14:06:35 +1300

    Fix issues with hooks noted in review

--- packages/block-library/src/image/save.js
+++ packages/block-library/src/image/save.js
@@ -66,9 +66,10 @@ export default function save( { attributes } ) {
 		</>
 	);
 
+	const blockProps = useBlockProps.save();
 	if ( isListItem ) {
 		return (
-			<li { ...useBlockProps.save() }>
+			<li { ...blockProps }>
 				<figure className={ classes }>{ figure }</figure>
 			</li>
 		);
@@ -76,7 +77,7 @@ export default function save( { attributes } ) {
 
 	if ( 'left' === align || 'right' === align || 'center' === align ) {
 		return (
-			<div { ...useBlockProps.save() }>
+			<div { ...blockProps }>
 				<figure className={ classes }>{ figure }</figure>
 			</div>
 		);
