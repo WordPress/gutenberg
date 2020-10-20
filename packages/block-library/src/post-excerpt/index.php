300acd5f5651283704d66a1e060e76b04adcbab5
Andrés <andres.maneiro@automattic.com>
2020-10-20 12:44:06 +0200

    PostExcerpt: update get_block_wrapper_attributes

--- packages/block-library/src/post-excerpt/index.php
+++ packages/block-library/src/post-excerpt/index.php
@@ -32,7 +32,11 @@ function render_block_core_post_excerpt( $attributes, $content, $block ) {
 	if ( isset( $attributes['textAlign'] ) ) {
 		$classes .= 'has-text-align-' . $attributes['textAlign'];
 	}
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classes ) );
+	$wrapper_attributes = get_block_wrapper_attributes(
+		$block->name,
+		$attributes,
+		array( 'class' => $classes )
+	);
 
 	$output = sprintf( '<div %1$s>', esc_attr( $wrapper_attributes ) ) . '<p class="wp-block-post-excerpt__excerpt">' . get_the_excerpt( $block->context['postId'] );
 	if ( ! isset( $attributes['showMoreOnNewLine'] ) || $attributes['showMoreOnNewLine'] ) {
