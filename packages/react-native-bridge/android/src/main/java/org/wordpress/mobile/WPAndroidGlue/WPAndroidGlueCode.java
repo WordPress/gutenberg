7168a0306f3b5a7e0ed2b339114b0a2fe6730148
Marko Savic <savicmarko1985@gmail.com>
2020-10-20 19:11:32 -0400

    Add support for document files

--- packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
+++ packages/react-native-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
@@ -147,6 +147,7 @@ public class WPAndroidGlueCode {
         void onCancelUploadForMediaClicked(int mediaId);
         void onCancelUploadForMediaDueToDeletedBlock(int mediaId);
         ArrayList<MediaOption> onGetOtherMediaImageOptions();
+        ArrayList<MediaOption> onGetOtherMediaDocumentOptions();
         void onOtherMediaButtonClicked(String mediaSource, boolean allowMultipleSelection);
     }
 
@@ -353,7 +354,8 @@ public class WPAndroidGlueCode {
                     ArrayList<MediaOption> otherMediaImageOptions = mOnMediaLibraryButtonListener.onGetOtherMediaImageOptions();
                     otherMediaOptionsReceivedCallback.onOtherMediaOptionsReceived(otherMediaImageOptions);
                 } else {
-                    otherMediaOptionsReceivedCallback.onOtherMediaOptionsReceived(new ArrayList<MediaOption>());
+                    ArrayList<MediaOption> otherMediaDocumentOptions = mOnMediaLibraryButtonListener.onGetOtherMediaDocumentOptions();
+                    otherMediaOptionsReceivedCallback.onOtherMediaOptionsReceived(otherMediaDocumentOptions);
                 }
             }
 
