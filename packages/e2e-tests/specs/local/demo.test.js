deb0c19ab433c93022e5d1ce9f976444c1bbb98a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-10-20 13:41:30 +0200

    E2E Tests: Fix demo test by disabling the welcome dialog

--- packages/e2e-tests/specs/local/demo.test.js
+++ packages/e2e-tests/specs/local/demo.test.js
@@ -4,6 +4,7 @@
 import {
 	createEmbeddingMatcher,
 	createJSONResponse,
+	createNewPost,
 	setUpResponseMocking,
 	visitAdminPage,
 } from '@wordpress/e2e-test-utils';
@@ -19,6 +20,9 @@ const MOCK_VIMEO_RESPONSE = {
 
 describe( 'new editor state', () => {
 	beforeAll( async () => {
+		// First, make sure that the block editor is properly configured.
+		await createNewPost();
+
 		await setUpResponseMocking( [
 			{
 				match: createEmbeddingMatcher( 'https://vimeo.com/22439234' ),
