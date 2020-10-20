195b5cc8f821a36c7c48f980e078d7503f2a7cc2
Riad Benguella <benguella@gmail.com>
2020-10-20 09:52:07 +0100

    Fix archives block render function

--- packages/block-library/src/archives/index.php
+++ packages/block-library/src/archives/index.php
@@ -88,16 +88,17 @@ function render_block_core_archives( $attributes ) {
 
 	$classnames = esc_attr( $class );
 
+	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classnames ) );
+
 	if ( empty( $archives ) ) {
 
 		return sprintf(
-			'<div class="%1$s">%2$s</div>',
-			$classnames,
+			'<div %1$s>%2$s</div>',
+			$wrapper_attributes,
 			__( 'No archives to show.' )
 		);
 	}
 
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classnames ) );
 
 	return sprintf(
 		'<ul %1$s>%2$s</ul>',
