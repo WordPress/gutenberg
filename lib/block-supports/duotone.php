5501d1068edcac04f92e02eabdc1bb6be596274f
Alex Lende <ajlende@gmail.com>
2020-10-21 15:40:01 -0600

    Render duotone with the new class-wp-block-supports

--- lib/block-supports/duotone.php
+++ lib/block-supports/duotone.php
@@ -34,13 +34,12 @@ function gutenberg_register_duotone_support( $block_type ) {
  * Add CSS classes and inline styles for colors to the incoming attributes array.
  * This will be applied to the block markup in the front-end.
  *
- * @param  array         $attributes       Comprehensive list of attributes to be applied.
- * @param  array         $block_attributes Block attributes.
  * @param  WP_Block_Type $block_type       Block type.
+ * @param  array         $block_attributes Block attributes.
  *
  * @return array Colors CSS classes and inline styles.
  */
-function gutenberg_apply_duotone_support( $attributes, $block_attributes, $block_type ) {
+function gutenberg_apply_duotone_support( $block_type, $block_attributes ) {
 	$has_duotone_support = false;
 	if ( property_exists( $block_type, 'supports' ) ) {
 		$has_duotone_support = gutenberg_experimental_get( $block_type->supports, array( 'duotone' ), false );
@@ -49,7 +48,7 @@ function gutenberg_apply_duotone_support( $attributes, $block_attributes, $block
 		$has_duotone_attribute = array_key_exists( 'duotone', $block_attributes );
 
 		if ( $has_duotone_attribute ) {
-			$attributes['css_classes'][] = 'duotone-filter-' . $block_attributes['duotone']['slug'];
+			$attributes['class'] = 'duotone-filter-' . $block_attributes['duotone']['slug'];
 		}
 	}
 
@@ -59,13 +58,13 @@ function gutenberg_apply_duotone_support( $attributes, $block_attributes, $block
 /**
  * Render out the duotone stylesheet and SVG.
  *
- * @param  string        $block_content    Rendered block content.
- * @param  array         $block_attributes Block attributes.
  * @param  WP_Block_Type $block_type       Block type.
+ * @param  array         $block_attributes Block attributes.
+ * @param  string        $block_content    Rendered block content.
  *
  * @return string filtered block content.
  */
-function gutenberg_render_duotone_support( $block_content, $block_attributes, $block_type ) {
+function gutenberg_render_duotone_support( $block_type, $block_attributes, $block_content ) {
 	$duotone_support = false;
 	if ( property_exists( $block_type, 'supports' ) ) {
 		$duotone_support = gutenberg_experimental_get( $block_type->supports, array( 'duotone' ), false );
@@ -155,3 +154,13 @@ function gutenberg_render_duotone_support( $block_content, $block_attributes, $b
 
 	return $block_content . $duotone;
 }
+
+// Register the block support.
+WP_Block_Supports::get_instance()->register(
+	'duotone',
+	array(
+		'register_attribute' => 'gutenberg_register_duotone_support',
+		'apply'              => 'gutenberg_apply_duotone_support',
+		'render_block'       => 'gutenberg_render_duotone_support',
+	)
+);
