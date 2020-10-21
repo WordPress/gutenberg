645eab6d77e24698c25fadfe7150eb257f1c56ff
Andrés <andres.maneiro@automattic.com>
2020-10-21 09:36:59 +0200

    Remove confusing comment

--- lib/global-styles.php
+++ lib/global-styles.php
@@ -976,5 +976,4 @@ function gutenberg_experimental_global_styles_register_cpt() {
 
 add_action( 'init', 'gutenberg_experimental_global_styles_register_cpt' );
 add_action( 'wp_enqueue_scripts', 'gutenberg_experimental_global_styles_enqueue_assets' );
-// This is to hook into the edit-post (block-editor store)
 add_filter( 'block_editor_settings', 'gutenberg_experimental_global_styles_settings' );
