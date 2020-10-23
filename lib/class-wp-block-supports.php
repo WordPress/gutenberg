d92575bf8aa987a693bf35eba998f114f08c9de7
Andrés <andres.maneiro@automattic.com>
2020-10-23 16:52:50 +0200

    Stylistic improvements

--- lib/class-wp-block-supports.php
+++ lib/class-wp-block-supports.php
@@ -216,16 +216,16 @@ function wp_block_supports_track_block_to_render( $args ) {
 			// See:
 			// - https://core.trac.wordpress.org/ticket/49927
 			// - commit 910de8f6890c87f93359c6f2edc6c27b9a3f3292 at wordpress-develop.
-			//
-			if ( null !== $block ) {
-				$parent_block                       = WP_Block_Supports::$block_to_render;
-				WP_Block_Supports::$block_to_render = $block->parsed_block;
-				$result                             = $block_render_callback( $attributes, $content, $block );
-				WP_Block_Supports::$block_to_render = $parent_block;
-				return $result;
-			} else {
+
+			if ( null === $block ) {
 				return $block_render_callback( $attributes, $content );
 			}
+
+			$parent_block                       = WP_Block_Supports::$block_to_render;
+			WP_Block_Supports::$block_to_render = $block->parsed_block;
+			$result                             = $block_render_callback( $attributes, $content, $block );
+			WP_Block_Supports::$block_to_render = $parent_block;
+			return $result;
 		};
 	}
 	return $args;
