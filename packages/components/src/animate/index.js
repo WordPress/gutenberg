9a909e160726d3fc6be2be4961ac4956c070ad8f
Jon Surrell <jon.surrell@automattic.com>
2020-10-23 17:21:52 +0200

    Change Animate props

--- packages/components/src/animate/index.js
+++ packages/components/src/animate/index.js
@@ -4,46 +4,40 @@
 import classnames from 'classnames';
 
 /**
- * @typedef {'top' | 'top left' | 'top right' | 'bottom' | 'bottom left' | 'bottom right'} AppearOrigin
+ * WordPress dependencies
  */
-/**
- * @typedef {'appear' | 'loading' | 'slide-in'} Type
- */
-/**
- * @typedef {'left' | 'right'} SlideInOrigin
- */
+import deprecated from '@wordpress/deprecated';
 
 /**
- * @param {Type} type The animation type
+ * @typedef {'top' | 'top left' | 'top right' | 'middle' | 'middle left' | 'middle right' | 'bottom' | 'bottom left' | 'bottom right'} AppearOrigin
+ * @typedef {'left' | 'right'} SlideInOrigin
+ * @typedef {{type: 'appear'; origin?: AppearOrigin}} AppearOptions
+ * @typedef {{type: 'slide-in'; origin?: SlideInOrigin}} SlideInOptions
+ * @typedef {{type: 'loading'}} LoadingOptions
+ * @typedef {AppearOptions|SlideInOptions|LoadingOptions} UseAnimateOptions
+ */
+
+/* eslint-disable jsdoc/valid-types */
+/**
+ * @param {UseAnimateOptions['type']} type The animation type
  * @return {'top'|'left'} Default origin
  */
 function getDefaultOrigin( type ) {
 	return type === 'appear' ? 'top' : 'left';
 }
+/* eslint-enable jsdoc/valid-types */
 
 /**
- * @typedef AppearOptions
- * @property {'appear'} type The type of animation.
- * @property {AppearOrigin} origin The origin of the appearance.
- */
-
-/**
- * @typedef SlideInOptions
- * @property {'slide-in'} type The type of animation.
- * @property {SlideInOrigin} origin The origin of the appearance.
- */
-
-/**
- * @typedef LoadingOptions
- * @property {'loading'} type The type of animation.
- * @property {never} origin .
- */
-
-/**
- * @param {AppearOptions | SlideInOptions | LoadingOptions} options
+ * @param {UseAnimateOptions} options
  * @return {string | undefined} Classname that applies the animations
  */
-export function useAnimate( { type, origin = getDefaultOrigin( type ) } ) {
+export function useAnimate( { type } ) {
+	if ( type === 'loading' ) {
+		return classnames( 'components-animate__loading' );
+	}
+
+	const origin = getDefaultOrigin( type );
+
 	if ( type === 'appear' ) {
 		const [ yAxis, xAxis = 'center' ] = origin.split( ' ' );
 		return classnames( 'components-animate__appear', {
@@ -59,61 +53,29 @@ export function useAnimate( { type, origin = getDefaultOrigin( type ) } ) {
 		);
 	}
 
-	if ( type === 'loading' ) {
-		return classnames( 'components-animate__loading' );
-	}
-
 	return undefined;
 }
 
 /**
- * @typedef AppearPropsOptions
- * @property {AppearOrigin} origin The origin of the appearance.
+ *
+ * @todo Remove options when deprecated handling has been removed.
+ *
+ * @param {{options?: any; children: (props: {className?: string})=> JSX.Element;} & UseAnimateOptions} props
+ * @return {JSX.Element} Element
  */
-
-/**
- * @typedef SlideInPropsOptions
- * @property {SlideInOrigin} origin The origin of the appearance.
- */
-
-/**
- * @typedef ChildrenProps
- * @property {string} className Classnames for the animation.
- */
-
-/**
- * @callback children
- * @param {ChildrenProps} props
- * @return {JSX.Element}
- */
-
-/**
- * @typedef AppearProps
- * @property {'appear'} type The animation type.
- * @property {AppearPropsOptions} options Options for the animation.
- * @property {children} children Children as a function.
- */
-
-/**
- * @typedef SlideInProps
- * @property {'slide-in'} type The animation type.
- * @property {SlideInPropsOptions} options Options for the animation.
- * @property {children} children Children as a function.
- */
-
-/**
- * @typedef LoadingProps
- * @property {'loading'} type The animation type.
- * @property {never} options Unused options
- * @property {children} children Children as a function.
- */
-
-/**
- * @param {AppearProps | SlideInProps | LoadingProps} props Props.
- * @return {JSX.Element} Element.
- */
-export default function Animate( { type, options, children } ) {
+export default function Animate( {
+	type,
+	options: _deprecatedOptions,
+	children,
+	...options
+} ) {
+	if ( Boolean( _deprecatedOptions ) ) {
+		deprecated( '<Animate> options prop', {
+			version: '9.3',
+			hint: 'Pass options directly as props instead.',
+		} );
+	}
 	return children( {
-		className: useAnimate( { type, ...( options || {} ) } ),
+		className: useAnimate( { type, ..._deprecatedOptions, ...options } ),
 	} );
 }
