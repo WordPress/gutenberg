1d692be9e6664166954394b0a99939954379c53d
Copons <Copons@users.noreply.github.com>
2020-10-23 18:34:52 +0100

    Localize template types definitions

--- lib/full-site-editing/templates-definitions.php
+++ lib/full-site-editing/templates-definitions.php
@@ -8,71 +8,71 @@
 function gutenberg_get_template_types_definitions() {
 	return array(
 		'index'          => array(
-			'title'         => 'Default (Index)',
-			'description'   => 'Main template, applied when no other template is found',
+			'title'         => _x( 'Default (Index)', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Main template, applied when no other template is found', 'gutenberg' ),
 		),
 		'home'           => array(
-			'title'         => 'Home',
-			'description'   => 'Template for the latest blog posts',
+			'title'         => _x( 'Home', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Template for the latest blog posts', 'gutenberg' ),
 		),
 		'front-page'     => array(
-			'title'         => 'Front Page',
-			'description'   => 'Front page template, whether it displays the blog posts index or a static page',
+			'title'         => _x( 'Front Page', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Front page template, whether it displays the blog posts index or a static page', 'gutenberg' ),
 		),
 		'archive'        => array(
-			'title'         => 'Default Archive',
-			'description'   => 'Applied to archives like your posts page, categories, or tags',
+			'title'         => _x( 'Default Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Applied to archives like your posts page, categories, or tags', 'gutenberg' ),
 		),
 		'singular'       => array(
-			'title'         => 'Default Singular',
-			'description'   => 'Displays any content on a single page',
+			'title'         => _x( 'Default Singular', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays any content on a single page', 'gutenberg' ),
 		),
 		'single'         => array(
-			'title'         => 'Default Single',
-			'description'   => 'Applied to individual content like a blog post',
+			'title'         => _x( 'Default Single', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Applied to individual content like a blog post', 'gutenberg' ),
 		),
 		'page'           => array(
-			'title'         => 'Default Page',
-			'description'   => 'Applied to individual pages',
+			'title'         => _x( 'Default Page', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Applied to individual pages', 'gutenberg' ),
 		),
 		'404'            => array(
-			'title'         => '404',
-			'description'   => 'Applied when content cannot be found',
+			'title'         => _x( '404 (Not Found)', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Applied when content cannot be found', 'gutenberg' ),
 		),
 		'search'         => array(
-			'title'         => 'Search Results',
-			'description'   => 'Applied to search results',
+			'title'         => __( 'Search Results', 'gutenberg' ),
+			'description'   => __( 'Applied to search results', 'gutenberg' ),
 		),
 		'author'         => array(
-			'title'         => 'Default Author Archive',
-			'description'   => 'Displays a list of posts by a single author',
+			'title'         => _x( 'Default Author Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays a list of posts by a single author', 'gutenberg' ),
 		),
 		'category'       => array(
-			'title'         => 'Default Post Category Archive',
-			'description'   => 'Displays a list of posts in a category',
+			'title'         => _x( 'Default Post Category Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays a list of posts in a category', 'gutenberg' ),
 		),
 		'taxonomy'       => array(
-			'title'         => 'Default Taxonomy Archive',
-			'description'   => 'Displays a list of posts in a taxonomy',
+			'title'         => _x( 'Default Taxonomy Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays a list of posts in a taxonomy', 'gutenberg' ),
 		),
 		'date'           => array(
-			'title'         => 'Default Date Archive',
-			'description'   => 'Displays a list of posts in a date range',
+			'title'         => _x( 'Default Date Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays a list of posts in a date range', 'gutenberg' ),
 		),
 		'tag'            => array(
-			'title'         => 'Default Tag Archive',
-			'description'   => 'Displays a list of posts with a tag',
+			'title'         => _x( 'Default Tag Archive', 'Template name', 'gutenberg' ),
+			'description'   => __( 'Displays a list of posts with a tag', 'gutenberg' ),
 		),
 		'attachment'     => array(
-			'title'         => 'Media',
-			'description'   => 'Displays media content',
+			'title'         => __( 'Media', 'gutenberg' ),
+			'description'   => __( 'Displays media content', 'gutenberg' ),
 		),
 		'embed'          => array(
-			'title'         => 'Embed',
-			'description'   => '[Currently unsupported]',
+			'title'         => __( 'Embed', 'gutenberg' ),
+			'description'   => __( '[Currently unsupported]', 'gutenberg' ),
 		),
 		'privacy-policy' => array(
-			'title'         => 'Privacy Policy',
+			'title'         => __( 'Privacy Policy', 'gutenberg' ),
 			'description'   => '',
 		),
 	);
