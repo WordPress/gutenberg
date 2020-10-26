02fa61ce697ad262a22c6328bb0e8b3c02f200d4
mapk <uraine@gmail.com>
2020-10-26 08:50:36 -0700

    New SVG icons with extra padding built in.

--- packages/block-library/src/query/icons.js
+++ packages/block-library/src/query/icons.js
@@ -4,25 +4,25 @@
 import { Path, SVG } from '@wordpress/components';
 
 export const titleDate = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 30">
-		<Path d="M34 0H0v3h34V0zM12 5H0v1h12V5zM0 17h12v1H0v-1zm34-5H0v3h34v-3zM0 29h12v1H0v-1zm34-5H0v3h34v-3z" />
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  		<Path d="M41 9H7v3h34V9zm-22 5H7v1h12v-1zM7 26h12v1H7v-1zm34-5H7v3h34v-3zM7 38h12v1H7v-1zm34-5H7v3h34v-3z" />
 	</SVG>
 );
 
 export const titleExcerpt = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 30">
-		<Path d="M34 0H0v3h34V0zm-4 5H0v1h30V5zm4 3H0v1h34V8zM0 11h30v1H0v-1zm0 12h30v1H0v-1zm34 3H0v1h34v-1zM0 29h30v1H0v-1zm34-11H0v3h34v-3z" />
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  		<Path d="M41 9H7v3h34V9zm-4 5H7v1h30v-1zm4 3H7v1h34v-1zM7 20h30v1H7v-1zm0 12h30v1H7v-1zm34 3H7v1h34v-1zM7 38h30v1H7v-1zm34-11H7v3h34v-3z" />
 	</SVG>
 );
 
 export const titleDateExcerpt = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 30">
-		<Path d="M34 0H0v3h34V0zM12 5H0v1h12V5zm22 3H0v1h34V8zM0 11h34v1H0v-1zm0 12h12v1H0v-1zm34 3H0v1h34v-1zM0 29h34v1H0v-1zm34-11H0v3h34v-3z" />
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  		<Path d="M41 9H7v3h34V9zm-22 5H7v1h12v-1zm22 3H7v1h34v-1zM7 20h34v1H7v-1zm0 12h12v1H7v-1zm34 3H7v1h34v-1zM7 38h34v1H7v-1zm34-11H7v3h34v-3z" />
 	</SVG>
 );
 
 export const imageDateTitle = (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 30">
-		<Path d="M0 0h34v6H0V0zm12 8H0v1h12V8zm18 3H0v1h30v-1zm0 18H0v1h30v-1zM0 26h12v1H0v-1zm34-8H0v6h34v-6z" />
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+  		<Path d="M7 9h34v6H7V9zm12 8H7v1h12v-1zm18 3H7v1h30v-1zm0 18H7v1h30v-1zM7 35h12v1H7v-1zm34-8H7v6h34v-6z" />
 	</SVG>
 );
