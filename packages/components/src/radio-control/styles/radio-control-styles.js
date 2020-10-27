06dc5a3d0a20b5428197e563dcbb62e5f2eee513
Sara Marcondes <saram@fastmail.com>
2020-10-27 08:21:55 -0700

    Fix specificity

--- packages/components/src/radio-control/styles/radio-control-styles.js
+++ packages/components/src/radio-control/styles/radio-control-styles.js
@@ -47,39 +47,42 @@ const appearBoxShadow = () => {
 };
 
 export const RadioControlInput = styled.input`
-	${ inputControl }
-
-	border: ${ config( 'borderWidth' ) } solid ${ color( 'gray.900' ) };
-	margin-top: 0;
-	margin-right: 6px;
-	transition: none;
-	border-radius: ${ config( 'radiusRound' ) };
-
-	&:checked::before {
-		width: 7px;
-		height: 7px;
-		margin: 8px 0 0 8px;
-		background-color: ${ color( 'white' ) };
-
-		// This border serves as a background color in Windows High Contrast mode.
-		border: 3px solid ${ color( 'white' ) };
-
-		${ breakpoint( 'medium' ) } {
-			width: 6px;
-			height: 6px;
-			margin: 4px 0 0 4px;
+	&,
+	&[type='radio'] {
+		${ inputControl }
+	
+		border: ${ config( 'borderWidth' ) } solid ${ color( 'gray.900' ) };
+		margin-top: 0;
+		margin-right: 6px;
+		transition: none;
+		border-radius: ${ config( 'radiusRound' ) };
+	
+		&:checked::before {
+			width: 7px;
+			height: 7px;
+			margin: 8px 0 0 8px;
+			background-color: ${ color( 'white' ) };
+	
+			// This border serves as a background color in Windows High Contrast mode.
+			border: 3px solid ${ color( 'white' ) };
+	
+			${ breakpoint( 'medium' ) } {
+				width: 6px;
+				height: 6px;
+				margin: 4px 0 0 4px;
+			}
 		}
-	}
-
-	&:focus {
-		${ appearBoxShadow() }
-
-		// Only visible in Windows High Contrast mode.
-		outline: 2px solid transparent;
-	}
-
-	&:checked {
-		background: var( --wp-admin-theme-color );
-		border-color: var( --wp-admin-theme-color );
+	
+		&:focus {
+			${ appearBoxShadow() }
+	
+			// Only visible in Windows High Contrast mode.
+			outline: 2px solid transparent;
+		}
+	
+		&:checked {
+			background: var( --wp-admin-theme-color );
+			border-color: var( --wp-admin-theme-color );
+		}
 	}
 `;
