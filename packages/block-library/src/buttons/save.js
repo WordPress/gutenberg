a03ea51e11a36d0abeecb4ce4e4cea5ffebdffc5
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-10-28 09:55:18 -0500

    Buttons block: overhaul alignment/justification controls (#23168)
    * Buttons block: overhaul alignment/justification controls.
    
    * Make icon names consistent with CSS terminology.
    
    * Move onClick function inline.
    
    * Mobile - Buttons block - Support content justification and ContentJustificationDropdown (#26166)
    
    * Fix control title inconsistency.
    
    Co-authored-by: Gerardo Pacheco <gerardo.pacheco@automattic.com>

--- packages/block-library/src/buttons/save.js
+++ packages/block-library/src/buttons/save.js
@@ -1,11 +1,22 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
-export default function save() {
+export default function save( { attributes: { contentJustification } } ) {
 	return (
-		<div { ...useBlockProps.save() }>
+		<div
+			{ ...useBlockProps.save( {
+				className: classnames( {
+					[ `is-content-justification-${ contentJustification }` ]: contentJustification,
+				} ),
+			} ) }
+		>
 			<InnerBlocks.Content />
 		</div>
 	);
