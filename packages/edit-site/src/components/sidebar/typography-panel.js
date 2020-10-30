7982ce7a439a77708ffd3920910626025dbc0a21
Jorge Costa <jorge.costa@automattic.com>
2020-10-30 16:03:17 +0000

    Fix: Font size picker regression on edit site global styles (#26603)

--- packages/edit-site/src/components/sidebar/typography-panel.js
+++ packages/edit-site/src/components/sidebar/typography-panel.js
@@ -8,7 +8,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { fromPx, toPx, useEditorFeature } from '../editor/utils';
+import { useEditorFeature } from '../editor/utils';
 
 export default ( {
 	context: { supports, name },
@@ -32,9 +32,9 @@ export default ( {
 		<PanelBody title={ __( 'Typography' ) } initialOpen={ true }>
 			{ supports.includes( 'fontSize' ) && (
 				<FontSizePicker
-					value={ fromPx( getStyleProperty( name, 'fontSize' ) ) }
+					value={ getStyleProperty( name, 'fontSize' ) }
 					onChange={ ( value ) =>
-						setStyleProperty( name, 'fontSize', toPx( value ) )
+						setStyleProperty( name, 'fontSize', value )
 					}
 					fontSizes={ fontSizes }
 					disableCustomFontSizes={ disableCustomFontSizes }
