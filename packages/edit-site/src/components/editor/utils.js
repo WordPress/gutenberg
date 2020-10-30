794ca40d1e0a7f27ac8610b321c29fcc0e4477cf
Jorge <jorge.costa@developer.pt>
2020-10-30 14:50:48 +0000

    Fix: Font size picker regression on edit site global styles

--- packages/edit-site/src/components/editor/utils.js
+++ packages/edit-site/src/components/editor/utils.js
@@ -17,19 +17,6 @@ export const PRESET_CATEGORIES = {
 export const LINK_COLOR = '--wp--style--color--link';
 export const LINK_COLOR_DECLARATION = `a { color: var(${ LINK_COLOR }, #00e); }`;
 
-/* Helpers for unit processing */
-export const fromPx = ( value ) => {
-	switch ( typeof value ) {
-		case 'string':
-			return +value.replace( 'px', '' );
-		case 'number':
-		default:
-			return value;
-	}
-};
-
-export const toPx = ( value ) => ( value ? value + 'px' : value );
-
 export function useEditorFeature( featurePath, blockName = GLOBAL_CONTEXT ) {
 	const settings = useSelect( ( select ) => {
 		return select( 'core/edit-site' ).getSettings();
