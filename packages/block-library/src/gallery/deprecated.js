11ef15ca87e4df0c3d3c67fb3ef6dd96e6f30612
Bart <bartlomiej.kalisz@gmail.com>
2020-10-30 13:55:25 +0100

    Deprecate the block & add alt attribute migration

--- packages/block-library/src/gallery/deprecated.js
+++ packages/block-library/src/gallery/deprecated.js
@@ -8,6 +8,7 @@ import { map, some } from 'lodash';
  * WordPress dependencies
  */
 import { RichText } from '@wordpress/block-editor';
+import { __unstableStripHTML } from '@wordpress/dom';
 
 /**
  * Internal dependencies
@@ -19,6 +20,183 @@ import {
 } from './constants';
 
 const deprecated = [
+	{
+		attributes: {
+			images: {
+				type: 'array',
+				default: [],
+				source: 'query',
+				selector: '.blocks-gallery-item',
+				query: {
+					url: {
+						type: 'string',
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'src',
+					},
+					fullUrl: {
+						type: 'string',
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-full-url',
+					},
+					link: {
+						type: 'string',
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-link',
+					},
+					alt: {
+						type: 'string',
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'alt',
+						default: '',
+					},
+					id: {
+						type: 'string',
+						source: 'attribute',
+						selector: 'img',
+						attribute: 'data-id',
+					},
+					caption: {
+						type: 'string',
+						source: 'html',
+						selector: '.blocks-gallery-item__caption',
+					},
+				},
+			},
+			ids: {
+				type: 'array',
+				items: {
+					type: 'number',
+				},
+				default: [],
+			},
+			columns: {
+				type: 'number',
+				minimum: 1,
+				maximum: 8,
+			},
+			caption: {
+				type: 'string',
+				source: 'html',
+				selector: '.blocks-gallery-caption',
+			},
+			imageCrop: {
+				type: 'boolean',
+				default: true,
+			},
+			linkTo: {
+				type: 'string',
+			},
+			sizeSlug: {
+				type: 'string',
+				default: 'large',
+			},
+		},
+		supports: {
+			anchor: true,
+			align: true,
+		},
+		isEligible( { images } ) {
+			return (
+				images &&
+				images.some(
+					( image ) => image.alt !== __unstableStripHTML( image.alt )
+				)
+			);
+		},
+		migrate( attributes ) {
+			return {
+				...attributes,
+				images: attributes.images.map( ( image ) => {
+					if ( image.alt !== __unstableStripHTML( image.alt ) ) {
+						return __unstableStripHTML( image.alt );
+					}
+					return image.alt;
+				} ),
+			};
+		},
+		save( { attributes } ) {
+			const {
+				images,
+				columns = defaultColumnsNumber( attributes ),
+				imageCrop,
+				caption,
+				linkTo,
+			} = attributes;
+
+			return (
+				<figure
+					className={ `columns-${ columns } ${
+						imageCrop ? 'is-cropped' : ''
+					}` }
+				>
+					<ul className="blocks-gallery-grid">
+						{ images.map( ( image ) => {
+							let href;
+
+							switch ( linkTo ) {
+								case LINK_DESTINATION_MEDIA:
+									href = image.fullUrl || image.url;
+									break;
+								case LINK_DESTINATION_ATTACHMENT:
+									href = image.link;
+									break;
+							}
+
+							const img = (
+								<img
+									src={ image.url }
+									alt={ image.alt }
+									data-id={ image.id }
+									data-full-url={ image.fullUrl }
+									data-link={ image.link }
+									className={
+										image.id
+											? `wp-image-${ image.id }`
+											: null
+									}
+								/>
+							);
+
+							return (
+								<li
+									key={ image.id || image.url }
+									className="blocks-gallery-item"
+								>
+									<figure>
+										{ href ? (
+											<a href={ href }>{ img }</a>
+										) : (
+											img
+										) }
+										{ ! RichText.isEmpty(
+											image.caption
+										) && (
+											<RichText.Content
+												tagName="figcaption"
+												className="blocks-gallery-item__caption"
+												value={ image.caption }
+											/>
+										) }
+									</figure>
+								</li>
+							);
+						} ) }
+					</ul>
+					{ ! RichText.isEmpty( caption ) && (
+						<RichText.Content
+							tagName="figcaption"
+							className="blocks-gallery-caption"
+							value={ caption }
+						/>
+					) }
+				</figure>
+			);
+		},
+	},
 	{
 		attributes: {
 			images: {
