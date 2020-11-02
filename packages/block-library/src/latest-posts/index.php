f1ae2d71b2c685241649b5513e921cdfdba2acc8
Romain DORR <rdorr@beapi.fr>
2020-11-03 00:27:16 +0100

    Improve hook name and add comment for filter

--- packages/block-library/src/latest-posts/index.php
+++ packages/block-library/src/latest-posts/index.php
@@ -53,7 +53,14 @@ function render_block_core_latest_posts( $attributes ) {
 		$args['author'] = $attributes['selectedAuthor'];
 	}
 
-	$args = apply_filters( 'block_core_latest_posts_query_args', $args );
+	/**
+	 * Filters the default query arguments.
+	 *
+	 * @since 5.7.0
+	 *
+	 * @param array $args The default query arguments.
+	 */
+	$args = apply_filters( 'block_latest_posts_query_args', $args );
 
 	$recent_posts = get_posts( $args );
 
