ab47ca90d37a568f728b3fe5834af9bec706032f
Andy Fragen <andy@thefragens.com>
2020-11-03 12:24:58 -0800

    add new filter to modify rollback

--- gutenberg.php
+++ gutenberg.php
@@ -24,6 +24,16 @@ add_filter(
 		);
 	}
 );
+add_filter(
+	'github_updater_release_asset_rollback',
+	function ( $rollback, $file ) {
+		if ( $file === plugin_basename( __FILE__ ) ) {
+			return [ 'gutenberg-nightly' ];
+		}
+	},
+	10,
+	2
+);
 add_filter( 'github_updater_no_release_asset_branches', '__return_true' );
 // End GitHub Updater filters.
 
