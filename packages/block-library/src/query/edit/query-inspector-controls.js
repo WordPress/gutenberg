88d95e627a5f707bd8d4e9369fb8cb8484c6ce4d
ntsekouras <ntsekouras@outlook.com>
2020-11-03 15:58:29 +0200

    add missing dep

--- packages/block-library/src/query/edit/query-inspector-controls.js
+++ packages/block-library/src/query/edit/query-inspector-controls.js
@@ -124,14 +124,14 @@ export default function QueryInspectorControls( { query, setQuery } ) {
 		onChangeDebounced();
 		return onChangeDebounced.cancel;
 	}, [ querySearch, onChangeDebounced ] );
-	const stickyOptions = useMemo( () => [
-		{
-			label: __( 'Include' ),
-			value: '',
-		},
-		{ label: __( 'Exclude' ), value: 'exclude' },
-		{ label: __( 'Only' ), value: 'only' },
-	] );
+	const stickyOptions = useMemo(
+		() => [
+			{ label: __( 'Include' ), value: '' },
+			{ label: __( 'Exclude' ), value: 'exclude' },
+			{ label: __( 'Only' ), value: 'only' },
+		],
+		[]
+	);
 	return (
 		<InspectorControls>
 			<PanelBody title={ __( 'Filtering and Sorting' ) }>
