1bcfa6bd45c877da85278a9091d553ab36b21c8e
Glen Davies <glen.davies@a8c.com>
2020-11-04 16:34:31 +1300

    Remove the default columns setting as we don't have access to innerBlocks at the point that the block validation is run

--- packages/block-library/src/gallery/save.js
+++ packages/block-library/src/gallery/save.js
@@ -1,24 +1,10 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { RichText, useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 
-/**
- * Internal dependencies
- */
-import { defaultColumnsNumber } from './shared';
-
-export default function save( { attributes, innerBlocks } ) {
-	const {
-		columns = defaultColumnsNumber( innerBlocks ),
-		imageCrop,
-		caption,
-	} = attributes;
+export default function save( { attributes } ) {
+	const { columns, imageCrop, caption } = attributes;
 	const className = `columns-${ columns } ${ imageCrop ? 'is-cropped' : '' }`;
 
 	return (
