768376bb7e5bac76c92cb0bfd47b4457c5848d68
Adam Zieliński <adam@adamziel.com>
2020-11-06 13:29:55 +0100

    Add usage example

--- packages/data/src/components/use-store-selectors/index.js
+++ packages/data/src/components/use-store-selectors/index.js
@@ -6,19 +6,46 @@ import useSelect from '../use-select';
 /**
  * A shorthand for useSelect() that returns all the selectors from a given store.
  *
- * @param {string}   storeKey    Store to return selectors from.
- * @param {Function} _mapSelect  Function called on every state change. The
- *                               returned value is exposed to the component
- *                               implementing this hook. The function receives
- *                               the object with all the store selectors as
- *                               its only argument.
- * @param {Array}    deps        If provided, this memoizes the mapSelect so the
- *                               same `mapSelect` is invoked on every state
- *                               change unless the dependencies change.
+ * @param {string}   storeKey      Store to return selectors from.
+ * @param {Function} mapSelectors  Function called on every state change. The
+ *                                 returned value is exposed to the component
+ *                                 implementing this hook. The function receives
+ *                                 the object with all the store selectors as
+ *                                 its only argument.
+ * @param {Array}    deps          If provided, this memoizes the mapSelect so the
+ *                                 same `mapSelect` is invoked on every state
+ *                                 change unless the dependencies change.
+ *
+ * @example
+ * ```js
+ * const { useStoreSelectors } = wp.data;
+ *
+ * function HammerPriceDisplay( { currency } ) {
+ *   const price = useStoreSelectors(
+ *     'my-shop',
+ *     ( { getPrice } ) => getPrice( 'hammer', currency ),
+ *     [ currency ]
+ *   );
+ *   return new Intl.NumberFormat( 'en-US', {
+ *     style: 'currency',
+ *     currency,
+ *   } ).format( price );
+ * }
+ *
+ * // Rendered in the application:
+ * // <HammerPriceDisplay currency="USD" />
+ * ```
+ *
+ * In the above example, when `HammerPriceDisplay` is rendered into an
+ * application, the price will be retrieved from the store state using the
+ * `mapSelectors` callback on `useStoreSelectors`. If the currency prop changes then
+ * any price in the state for that currency is retrieved. If the currency prop
+ * doesn't change and other props are passed in that do change, the price will
+ * not change because the dependency is just the currency.
  *
  * @see useSelect
- * @return {Function}  A custom react hook.
+ * @return {Function} A custom react hook.
  */
-export default function useStoreSelectors( storeKey, _mapSelect, deps = [] ) {
-	return useSelect( ( select ) => _mapSelect( select( storeKey ) ), deps );
+export default function useStoreSelectors( storeKey, mapSelectors, deps = [] ) {
+	return useSelect( ( select ) => mapSelectors( select( storeKey ) ), deps );
 }
