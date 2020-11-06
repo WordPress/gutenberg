f7d7618fe727f19594c4bb8f2daeb6b5fe531649
Marin Atanasov <tyxla@abv.bg>
2020-11-06 13:18:23 +0200

    Components: Add storybook example for Disableable

--- packages/components/src/disabled/stories/index.js
+++ packages/components/src/disabled/stories/index.js
@@ -1,7 +1,13 @@
 /**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
-import Disabled from '../';
+import Disabled, { Disableable as DisableableComponent } from '../';
+import Button from '../../button/';
 import SelectControl from '../../select-control/';
 import TextControl from '../../text-control/';
 import TextareaControl from '../../textarea-control/';
@@ -11,21 +17,45 @@ export default {
 	component: Disabled,
 };
 
+const Form = () => (
+	<div>
+		<TextControl label="Text Control" />
+		<TextareaControl label="TextArea Control" />
+		<SelectControl
+			label="Select Control"
+			onChange={ () => {} }
+			options={ [
+				{ value: null, label: 'Select an option', disabled: true },
+				{ value: 'a', label: 'Option A' },
+				{ value: 'b', label: 'Option B' },
+				{ value: 'c', label: 'Option C' },
+			] }
+		/>
+	</div>
+);
+
 export const _default = () => {
 	return (
 		<Disabled>
-			<TextControl label="Text Control" />
-			<TextareaControl label="TextArea Control" />
-			<SelectControl
-				label="Select Control"
-				onChange={ () => {} }
-				options={ [
-					{ value: null, label: 'Select an option', disabled: true },
-					{ value: 'a', label: 'Option A' },
-					{ value: 'b', label: 'Option B' },
-					{ value: 'c', label: 'Option C' },
-				] }
-			/>
+			<Form />
 		</Disabled>
+	);
+};
+
+export const Disableable = () => {
+	const [ isDisabled, setState ] = useState( false );
+	const toggleDisabled = () => {
+		setState( () => ! isDisabled );
+	};
+
+	return (
+		<div>
+			<DisableableComponent disabled={ isDisabled }>
+				<Form />
+			</DisableableComponent>
+			<Button isPrimary onClick={ toggleDisabled }>
+				Toggle Disabled
+			</Button>
+		</div>
 	);
 };
