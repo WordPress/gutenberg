bd01a2bca3289ac35083d2d92d1e27c3a70bce75
Herb Miller <herb@bobbingwide.com>
2020-11-08 18:14:27 +0000

    Don't call wpautop() to avoid unwanted empty paragraphs (#267310

--- packages/block-library/src/template-part/index.php
+++ packages/block-library/src/template-part/index.php
@@ -54,7 +54,6 @@ function render_block_core_template_part( $attributes ) {
 	$content = do_blocks( $content );
 	$content = wptexturize( $content );
 	$content = convert_smilies( $content );
-	$content = wpautop( $content );
 	$content = shortcode_unautop( $content );
 	if ( function_exists( 'wp_filter_content_tags' ) ) {
 		$content = wp_filter_content_tags( $content );
