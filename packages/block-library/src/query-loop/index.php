14eabe552b0a6ec8dace932ee1bef943bd3475ed
Herb Miller <herb@bobbingwide.com>
2020-11-09 11:51:58 +0000

    query-loop block: Run the main query when queryId is not set (#25377)

--- packages/block-library/src/query-loop/index.php
+++ packages/block-library/src/query-loop/index.php
@@ -67,19 +67,58 @@ function render_block_core_query_loop( $attributes, $content, $block ) {
 		}
 	}
 
-	$posts = get_posts( $query );
+	if ( isset( $block->content['queryId']) ) {
 
-	$content = '';
-	foreach ( $posts as $post ) {
-		$content .= (
+		$posts = get_posts( $query );
+
+		$content='';
+		foreach ( $posts as $post ) {
+			$content.= (
+			new WP_Block(
+				$block->parsed_block,
+				array(
+					'postType'=>$post->post_type,
+					'postId'  =>$post->ID,
+				)
+			)
+			)->render( array( 'dynamic'=>false ) );
+		}
+	} else {
+		/**
+		* Not in context of a `core/query` block; assume this is the main query and perform the loop.
+        */
+        $content = render_block_core_query_loop_main_query($attributes, $content, $block);
+	}
+	return $content;
+}
+
+/**
+ * Renders the `core/query-loop` block for the main query on the server.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the output of the query, structured using the layout defined by the block's inner blocks.
+ */
+function render_block_core_query_loop_main_query( $attributes, $content, $block ) {
+	if ( have_posts() ) {
+		$content = '';
+		while ( have_posts() ) {
+			the_post();
+			$post = get_post();
+			$content .= (
 			new WP_Block(
 				$block->parsed_block,
 				array(
 					'postType' => $post->post_type,
-					'postId'   => $post->ID,
+					'postId' => $post->ID,
 				)
 			)
-		)->render( array( 'dynamic' => false ) );
+			)->render(array('dynamic' => false));
+		}
+	} else {
+		$content = __( "No posts found." );
 	}
 	return $content;
 }
