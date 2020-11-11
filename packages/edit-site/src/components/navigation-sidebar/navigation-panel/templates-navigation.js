916b943c32044db367086ddcc06c9efe1fe11da2
David Szabo <david.szabo97@gmail.com>
2020-11-11 15:33:05 +0100

    Reuse MainDashboardButton

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/templates-navigation.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/templates-navigation.js
@@ -7,6 +7,7 @@ import {
 	__experimentalNavigationItem as NavigationItem,
 	__experimentalNavigationBackButton as NavigationBackButton,
 } from '@wordpress/components';
+import { __experimentalMainDashboardButton as MainDashboardButton } from '@wordpress/interface';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
@@ -16,7 +17,6 @@ import { __ } from '@wordpress/i18n';
 import TemplatesMenu from './menus/templates';
 import TemplatePartsMenu from './menus/template-parts';
 import { MENU_ROOT, MENU_TEMPLATE_PARTS, MENU_TEMPLATES } from './constants';
-import NavigationPanelMainBackButton from './main-back-button';
 
 export default function TemplatesNavigation() {
 	const { templateId, templatePartId, templateType, activeMenu } = useSelect(
@@ -51,13 +51,13 @@ export default function TemplatesNavigation() {
 			onActivateMenu={ setNavigationPanelActiveMenu }
 		>
 			{ activeMenu === MENU_ROOT && (
-				<NavigationPanelMainBackButton.Slot>
+				<MainDashboardButton.Slot>
 					<NavigationBackButton
 						backButtonLabel={ __( 'Dashboard' ) }
 						className="edit-site-navigation-panel__back-to-dashboard"
 						href="index.php"
 					/>
-				</NavigationPanelMainBackButton.Slot>
+				</MainDashboardButton.Slot>
 			) }
 
 			<NavigationMenu title={ __( 'Theme' ) }>
