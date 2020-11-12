1c6a9468dbd9d05c2e949a4d8eb7c95c1b97bd1e
Riad Benguella <benguella@gmail.com>
2020-11-12 10:48:20 +0100

    fix end2end test

--- packages/e2e-tests/specs/editor/various/adding-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/adding-blocks.test.js
@@ -259,6 +259,10 @@ describe( 'adding blocks', () => {
 			inserterMenuInputSelector
 		);
 		inserterMenuSearchInput.type( 'cover' );
+		// We need to wait a bit after typing otherwise we might an "early" result
+		// that is going to be "detached" when trying to click on it
+		// eslint-disable-next-line no-restricted-syntax
+		await page.waitFor( 100 );
 		const coverBlock = await page.waitForSelector(
 			'.block-editor-block-types-list .editor-block-list-item-cover'
 		);
