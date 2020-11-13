263d04396eb8fe7efcc90f2d86653392f25ddb1a
Jon Surrell <jon.surrell@automattic.com>
2020-11-13 23:42:48 +0100

    Improve stories (with knobs)

--- packages/components/src/animate/stories/index.js
+++ packages/components/src/animate/stories/index.js
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+import { button, radios } from '@storybook/addon-knobs';
+import { useMemo, useState } from 'react';
+
+/**
  * Internal dependencies
  */
 import Animate from '../';
@@ -6,54 +12,51 @@ import Notice from '../../notice';
 
 export default { title: 'Components/Animate', component: Animate };
 
-export const _default = () => (
-	<Animate>
-		{ ( { className } ) => (
-			<Notice className={ className } status="success">
-				<p>{ `No default animation. Use one of type = "appear", "slide-in", or "loading".` }</p>
-			</Notice>
-		) }
-	</Animate>
-);
+export const _default = () => {
+	const typeToOrigin = useMemo(
+		() => ( {
+			appear: [
+				[
+					'top',
+					'top left',
+					'top right',
+					'middle',
+					'middle left',
+					'middle right',
+					'bottom',
+					'bottom left',
+					'bottom right',
+				],
+				'top left',
+			],
+			'slide-in': [ [ 'left', 'right' ], 'left' ],
+			loading: [ [], undefined ],
+		} ),
+		[]
+	);
+	const [ k, setK ] = useState( Number.MIN_SAFE_INTEGER );
+	button( 'Replay animation', () => setK( ( prevK ) => prevK + 1 ) );
+	const type = radios(
+		'Type',
+		[ 'appear', 'slide-in', 'loading' ],
+		'appear'
+	);
+	const origin = radios( 'Origin', ...typeToOrigin[ type ] );
 
-// Unexported helper for different origins.
-const Appear = ( { origin } ) => (
-	<Animate type="appear" origin={ origin }>
-		{ ( { className } ) => (
-			<Notice className={ className } status="success">
-				<p>Appear animation. Origin: { origin }.</p>
-			</Notice>
-		) }
-	</Animate>
-);
-
-export const appearTopLeft = () => <Appear origin="top left" />;
-export const appearTopRight = () => <Appear origin="top right" />;
-export const appearBottomLeft = () => <Appear origin="bottom left" />;
-export const appearBottomRight = () => <Appear origin="bottom right" />;
-export const appearMiddle = () => <Appear origin="middle" />;
-export const appearMiddleLeft = () => <Appear origin="middle left" />;
-export const appearMiddleRight = () => <Appear origin="middle right" />;
-
-export const loading = () => (
-	<Animate type="loading">
-		{ ( { className } ) => (
-			<Notice className={ className } status="success">
-				<p>Loading animation.</p>
-			</Notice>
-		) }
-	</Animate>
-);
-
-const SlideIn = ( { origin } ) => (
-	<Animate type="slide-in" origin={ origin }>
-		{ ( { className } ) => (
-			<Notice className={ className } status="success">
-				<p>Slide-in animation. Origin: { origin }.</p>
-			</Notice>
-		) }
-	</Animate>
-);
-
-export const slideInLeft = () => <SlideIn origin="left" />;
-export const slideInight = () => <SlideIn origin="right" />;
+	return (
+		<Animate type={ type } origin={ origin } key={ k }>
+			{ ( { className } ) => (
+				<Notice className={ className } status="success">
+					<p>
+						<code>{ type }</code> animation.
+					</p>
+					{ origin && (
+						<p>
+							Origin: <code>{ origin }</code>.
+						</p>
+					) }
+				</Notice>
+			) }
+		</Animate>
+	);
+};
