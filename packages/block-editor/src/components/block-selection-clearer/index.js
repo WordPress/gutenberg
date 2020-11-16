6e3faaa3d2dec90f93b207473c3e9f15dec8ca10
Ella van Durpe <ella@vandurpe.com>
2020-11-16 13:10:33 +0200

    clipboard and selection clearer

--- packages/block-editor/src/components/block-selection-clearer/index.js
+++ packages/block-editor/src/components/block-selection-clearer/index.js
@@ -2,8 +2,9 @@
  * WordPress dependencies
  */
 import { useSelect, useDispatch } from '@wordpress/data';
+import { useEffect, useRef } from '@wordpress/element';
 
-function useBlockSelectionClearer() {
+function useBlockSelectionClearer( ref ) {
 	const hasSelection = useSelect( ( select ) => {
 		const { hasSelectedBlock, hasMultiSelection } = select(
 			'core/block-editor'
@@ -13,14 +14,27 @@ function useBlockSelectionClearer() {
 	} );
 	const { clearSelectedBlock } = useDispatch( 'core/block-editor' );
 
-	return ( event ) => {
-		if ( event.target === event.currentTarget && hasSelection ) {
-			clearSelectedBlock();
+	useEffect( () => {
+		if ( ! hasSelection ) {
+			return;
 		}
-	};
+
+		function onFocus( event ) {
+			if ( event.target === event.currentTarget ) {
+				clearSelectedBlock();
+			}
+		}
+
+		ref.current.addEventListener( 'focus', onFocus );
+
+		return () => {
+			ref.current.removeEventListener( 'focus', onFocus );
+		};
+	}, [ hasSelection, clearSelectedBlock ] );
 }
 
 export default function BlockSelectionClearer( props ) {
-	const onFocus = useBlockSelectionClearer();
-	return <div tabIndex={ -1 } onFocus={ onFocus } { ...props } />;
+	const ref = useRef();
+	useBlockSelectionClearer( ref );
+	return <div tabIndex={ -1 } ref={ ref } { ...props } />;
 }
