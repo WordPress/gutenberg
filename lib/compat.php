2eae664d47f9d217dd0dc5f7adb89d6136959738
Riad Benguella <benguella@gmail.com>
2020-11-16 17:57:12 +0100

    Fix recursive templates rendering

--- lib/compat.php
+++ lib/compat.php
@@ -448,11 +448,11 @@ function gutenberg_inject_default_block_context( $args ) {
 
 			// Inject the post context if not done by Core.
 			$needs_post_id = ! empty( $block_type->uses_context ) && in_array( 'postId', $block_type->uses_context, true );
-			if ( $post instanceof WP_Post && $needs_post_id ) {
+			if ( $post instanceof WP_Post && $needs_post_id && ! isset( $block->context['postId'] ) && 'wp_template' !== $post->post_type ) {
 				$block->context['postId'] = $post->ID;
 			}
 			$needs_post_type = ! empty( $block_type->uses_context ) && in_array( 'postType', $block_type->uses_context, true );
-			if ( $post instanceof WP_Post && $needs_post_type ) {
+			if ( $post instanceof WP_Post && $needs_post_type && ! isset( $block->context['postType'] ) && 'wp_template' !== $post->post_type ) {
 				/*
 				* The `postType` context is largely unnecessary server-side, since the
 				* ID is usually sufficient on its own. That being said, since a block's
@@ -464,7 +464,7 @@ function gutenberg_inject_default_block_context( $args ) {
 
 			// Inject the query context if not done by Core.
 			$needs_query = empty( $block_type->uses_context ) && in_array( 'query', $block_type->uses_context, true );
-			if ( ! isset( $block->context['query'] ) && $needs_query ) {
+			if ( ! isset( $block->context['query'] ) && $needs_query && ! isset( $block->context['query'] ) ) {
 				if ( isset( $wp_query->tax_query->queried_terms['category'] ) ) {
 					$block->context['query'] = array( 'categoryIds' => array() );
 
