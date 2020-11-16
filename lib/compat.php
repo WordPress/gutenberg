e1cb6d25d519d476a73b8054b6175efc2736b0b3
Riad Benguella <benguella@gmail.com>
2020-11-16 18:08:02 +0100

    Exclude template parts as well

--- lib/compat.php
+++ lib/compat.php
@@ -448,11 +448,11 @@ function gutenberg_inject_default_block_context( $args ) {
 
 			// Inject the post context if not done by Core.
 			$needs_post_id = ! empty( $block_type->uses_context ) && in_array( 'postId', $block_type->uses_context, true );
-			if ( $post instanceof WP_Post && $needs_post_id && ! isset( $block->context['postId'] ) && 'wp_template' !== $post->post_type ) {
+			if ( $post instanceof WP_Post && $needs_post_id && ! isset( $block->context['postId'] ) && 'wp_template' !== $post->post_type && 'wp_template_part' !== $post->post_type ) {
 				$block->context['postId'] = $post->ID;
 			}
 			$needs_post_type = ! empty( $block_type->uses_context ) && in_array( 'postType', $block_type->uses_context, true );
-			if ( $post instanceof WP_Post && $needs_post_type && ! isset( $block->context['postType'] ) && 'wp_template' !== $post->post_type ) {
+			if ( $post instanceof WP_Post && $needs_post_type && ! isset( $block->context['postType'] ) && 'wp_template' !== $post->post_type && 'wp_template_part' !== $post->post_type ) {
 				/*
 				* The `postType` context is largely unnecessary server-side, since the
 				* ID is usually sufficient on its own. That being said, since a block's
