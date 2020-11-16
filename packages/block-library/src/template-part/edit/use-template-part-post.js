24358c961fa683b935b232c1dfe6488488bb13e4
David Szabo <david.szabo97@gmail.com>
2020-11-16 21:01:42 +0100

    Fix template parts not loading

--- packages/block-library/src/template-part/edit/use-template-part-post.js
+++ packages/block-library/src/template-part/edit/use-template-part-post.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { keyBy } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { useSelect } from '@wordpress/data';
@@ -33,11 +38,18 @@ export default function useTemplatePartPost( postId, slug, theme ) {
 						theme,
 					}
 				);
+				const themeTaxonomies = keyBy(
+					select( 'core' ).getEntityRecords( 'taxonomy', 'wp_theme', {
+						per_page: -1,
+					} ),
+					'name'
+				);
+
 				const foundPosts = posts?.filter(
 					( post ) =>
 						post.slug === cleanedSlug &&
-						post.meta &&
-						post.meta.theme === theme
+						themeTaxonomies[ theme ] &&
+						post.wp_theme.includes( themeTaxonomies[ theme ].id )
 				);
 				// A published post might already exist if this template part was customized elsewhere
 				// or if it's part of a customized template.
