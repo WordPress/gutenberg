18a8624788bda79a9da6a19f5ca7fe1a02e70482
Herb Miller <herb@bobbingwide.com>
2020-11-17 23:01:31 +0000

    Query block - filter all post types (#27049)

--- packages/block-library/src/query/edit/query-inspector-controls.js
+++ packages/block-library/src/query/edit/query-inspector-controls.js
@@ -57,7 +57,7 @@ export default function QueryInspectorControls( { query, setQuery } ) {
 				termsQuery
 			);
 			const excludedPostTypes = [ 'attachment' ];
-			const filteredPostTypes = getPostTypes()?.filter(
+			const filteredPostTypes = getPostTypes( { per_page: -1 } )?.filter(
 				( { viewable, slug } ) =>
 					viewable && ! excludedPostTypes.includes( slug )
 			);
@@ -130,6 +130,7 @@ export default function QueryInspectorControls( { query, setQuery } ) {
 		onChangeDebounced();
 		return onChangeDebounced.cancel;
 	}, [ querySearch, onChangeDebounced ] );
+
 	return (
 		<InspectorControls>
 			<PanelBody title={ __( 'Settings' ) }>
