7c93112291daa449eff5f936a52bc9d42b5a9cc1
bph <birgit.pauli@gmail.com>
2020-11-17 13:55:57 -0500

    9.4 nightly release

--- gutenberg.php
+++ gutenberg.php
@@ -5,13 +5,8 @@
  * Description: Zip from master of Gutenberg GitHub repo: Gutenberg This is the development plugin for the new block editor in core. Ping @bph on WPSlack for questions. 
  * Requires at least: 5.3
  * Requires PHP: 5.6
-<<<<<<< HEAD
- * Version: 9.3.0.20201117
- * Author: Gutenberg Team / Birgit Pauli-Haack (Gutenberg Times)
-=======
- * Version: 9.4.0-rc.1
+ * Version: 9.4.0.20201117
  * Author: Gutenberg Team
->>>>>>> upstream/master
  * Text Domain: gutenberg
  * GitHub Plugin URI: bph/gutenberg
  * Release Asset: true
