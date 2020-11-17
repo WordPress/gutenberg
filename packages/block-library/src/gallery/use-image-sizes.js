974ad90354355e1b0a276366d1d9ab837a318486
Glen Davies <glen.davies@a8c.com>
2020-11-18 11:52:15 +1300

    Memoise the useImageSizes hook

--- packages/block-library/src/gallery/use-image-sizes.js
+++ packages/block-library/src/gallery/use-image-sizes.js
@@ -3,59 +3,67 @@
  */
 import { get, reduce, map, filter, some } from 'lodash';
 
+/**
+ * WordPress dependencies
+ */
+import { useMemo } from '@wordpress/element';
+
 export default function useImageSizes(
 	images,
 	isSelected,
 	getSettings,
 	getMedia
 ) {
-	const { imageSizes } = getSettings();
+	return useMemo( () => getImageSizing(), [ images, isSelected ] );
 
-	let resizedImages = {};
+	function getImageSizing() {
+		const { imageSizes } = getSettings();
+		let resizedImages = {};
 
-	if ( isSelected ) {
-		resizedImages = reduce(
-			images,
-			( currentResizedImages, img ) => {
-				if ( ! img.id ) {
-					return currentResizedImages;
-				}
-				const image = img.imageData
-					? img.imageData
-					: getMedia( img.id );
-				const sizes = reduce(
-					imageSizes,
-					( currentSizes, size ) => {
-						const defaultUrl = get( image, [
-							'sizes',
-							size.slug,
-							'url',
-						] );
-						const mediaDetailsUrl = get( image, [
-							'media_details',
-							'sizes',
-							size.slug,
-							'source_url',
-						] );
-						return {
-							...currentSizes,
-							[ size.slug ]: defaultUrl || mediaDetailsUrl,
-						};
-					},
-					{}
-				);
-				return {
-					...currentResizedImages,
-					[ parseInt( img.id, 10 ) ]: sizes,
-				};
-			},
-			{}
+		if ( isSelected ) {
+			resizedImages = reduce(
+				images,
+				( currentResizedImages, img ) => {
+					if ( ! img.id ) {
+						return currentResizedImages;
+					}
+					const image = img.imageData
+						? img.imageData
+						: getMedia( img.id );
+					const sizes = reduce(
+						imageSizes,
+						( currentSizes, size ) => {
+							const defaultUrl = get( image, [
+								'sizes',
+								size.slug,
+								'url',
+							] );
+							const mediaDetailsUrl = get( image, [
+								'media_details',
+								'sizes',
+								size.slug,
+								'source_url',
+							] );
+							return {
+								...currentSizes,
+								[ size.slug ]: defaultUrl || mediaDetailsUrl,
+							};
+						},
+						{}
+					);
+					return {
+						...currentResizedImages,
+						[ parseInt( img.id, 10 ) ]: sizes,
+					};
+				},
+				{}
+			);
+		}
+		return map(
+			filter( imageSizes, ( { slug } ) =>
+				some( resizedImages, ( sizes ) => sizes[ slug ] )
+			),
+			( { name, slug } ) => ( { value: slug, label: name } )
 		);
 	}
-	return map(
-		filter( imageSizes, ( { slug } ) =>
-			some( resizedImages, ( sizes ) => sizes[ slug ] )
-		),
-		( { name, slug } ) => ( { value: slug, label: name } )
-	);
 }
