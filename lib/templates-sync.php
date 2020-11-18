f8d9d07f38bc435338b21b9c12d91c1b5d2f7585
Marko Andrijasevic <markoandrijasevic@gmail.com>
2020-11-18 15:56:11 +0100

    Site Editor: fix synchronization checks after template deletion (#27068)
    The function called on template delete action was checking for wrong post types.

--- lib/templates-sync.php
+++ lib/templates-sync.php
@@ -167,7 +167,7 @@ add_action( 'trash_wp_template_part', 'gutenberg_clear_synchronize_last_checks' 
  * @param WP_Post $post WP_Post instance of the deleted post.
  */
 function gutenberg_clear_synchronize_last_checks_after_delete( $post_id, $post ) {
-	if ( 'wp_template' !== $post->post_type || 'wp_template_part' !== $post->post_type ) {
+	if ( 'wp_template' === $post->post_type || 'wp_template_part' === $post->post_type ) {
 		gutenberg_clear_synchronize_last_checks();
 	}
 }
