ebd2c0b06f635d484449ed841b041c097e9d7dcf
eToledo <etoledom@icloud.com>
2020-11-18 10:36:33 +0100

    Fix colors for HR line in Separator block

--- packages/primitives/src/horizontal-rule/index.native.js
+++ packages/primitives/src/horizontal-rule/index.native.js
@@ -3,19 +3,27 @@
  */
 import Hr from 'react-native-hr';
 
-export const HorizontalRule = ( props ) => {
-	const lineStyle = {
-		backgroundColor: '#555d66',
-		height: 2,
-		...props.lineStyle,
-	};
+/**
+ * WordPress dependencies
+ */
+import { withPreferredColorScheme } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import styles from './styles.scss';
+
+const HR = ( { getStylesFromColorScheme, ...props } ) => {
+	const lineStyle = getStylesFromColorScheme( styles.line, styles.lineDark );
 
 	return (
 		<Hr
 			{ ...props }
-			lineStyle={ lineStyle }
+			lineStyle={ [ lineStyle, props.lineStyle ] }
 			marginLeft={ 0 }
 			marginRight={ 0 }
 		/>
 	);
 };
+
+export const HorizontalRule = withPreferredColorScheme( HR );
