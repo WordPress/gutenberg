543ef7cce5509a3d63ab4becb13518c861289c49
Haz <hazdiego@gmail.com>
2020-11-19 09:26:06 -0300

    Pass aria-orientation to listbox

--- packages/block-editor/src/components/inserter-listbox/group.js
+++ packages/block-editor/src/components/inserter-listbox/group.js
@@ -4,7 +4,14 @@
 import { forwardRef } from '@wordpress/element';
 
 function InserterListboxGroup( props, ref ) {
-	return <div role="listbox" { ...props } ref={ ref } />;
+	return (
+		<div
+			role="listbox"
+			aria-orientation="horizontal"
+			{ ...props }
+			ref={ ref }
+		/>
+	);
 }
 
 export default forwardRef( InserterListboxGroup );
