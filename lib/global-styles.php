4f9fbd1b497c0028f48d41195158a2d5e5719241
Jorge Costa <jorge.costa@automattic.com>
2020-11-19 15:37:33 +0000

    Fix: Font size unit back-compatibility does not executes on post edit (#27106)

--- lib/global-styles.php
+++ lib/global-styles.php
@@ -291,10 +291,17 @@ function gutenberg_experimental_global_styles_get_theme_support_settings( $setti
 	}
 
 	if ( isset( $settings['fontSizes'] ) ) {
+		$font_sizes = $settings['fontSizes'];
+		// Back-compatibility for presets without units.
+		foreach ( $font_sizes as &$font_size ) {
+			if ( is_numeric( $font_size['size'] ) ) {
+				$font_size['size'] = $font_size['size'] . 'px';
+			}
+		}
 		if ( ! isset( $theme_settings['global']['settings']['typography'] ) ) {
 			$theme_settings['global']['settings']['typography'] = array();
 		}
-		$theme_settings['global']['settings']['typography']['fontSizes'] = $settings['fontSizes'];
+		$theme_settings['global']['settings']['typography']['fontSizes'] = $font_sizes;
 	}
 
 	// Things that didn't land in core yet, so didn't have a setting assigned.
