5010819e68c03c1dc41cd95590006ee5499afe62
David Szabo <david.szabo97@gmail.com>
2020-11-24 13:16:30 +0100

    Update getTemplateInfo

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/search-results.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/search-results.js
@@ -8,17 +8,32 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { getTemplateInfo } from '../../../utils';
 import { normalizedSearch } from './utils';
+import { useSelect } from '@wordpress/data';
 
 export default function SearchResults( { items, search, renderItem } ) {
+	const templateInfos = useSelect(
+		( select ) => {
+			const { __experimentalGetTemplateInfo: getTemplateInfo } = select(
+				'core/editor'
+			);
+
+			return items.map( ( item ) => ( {
+				slug: item.slug,
+				...getTemplateInfo( item ),
+			} ) );
+		},
+		[ items ]
+	);
 	const itemsFiltered = useMemo( () => {
 		if ( items === null || search.length === 0 ) {
 			return [];
 		}
 
 		return items.filter( ( item ) => {
-			const { title } = getTemplateInfo( item );
+			const { title } = templateInfos.find(
+				( { slug } ) => item.slug === slug
+			);
 			return normalizedSearch( title, search );
 		} );
 	}, [ items, search ] );
