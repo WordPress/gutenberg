c80758d9e0706816191d4606afcc5d7396cd54fd
Ari Stathopoulos <aristath@gmail.com>
2020-11-27 12:38:47 +0200

    another test

--- lib/template-loader.php
+++ lib/template-loader.php
@@ -153,7 +153,7 @@ function gutenberg_resolve_template( $template_type, $template_hierarchy = array
 		function ( $template_a, $template_b ) use ( $slug_priorities ) {
 			$priority_a = $slug_priorities[ $template_a->post_name ] * 2 + ( 'publish' === $template_a->post_status ? 1 : 0 );
 			$priority_b = $slug_priorities[ $template_b->post_name ] * 2 + ( 'publish' === $template_b->post_status ? 1 : 0 );
-			return $priority_b - $priority_a;
+			return $priority_a - $priority_b;
 		}
 	);
 
