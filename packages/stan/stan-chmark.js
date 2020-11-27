e0fe18fddd156f97ab3d447ef024b66429e4609e
Jarda Snajdr <jsnajdr@gmail.com>
2020-11-27 10:21:33 +0100

    Add more cases (redux-map, 10 repeated reads) to the benchmark

--- packages/stan/stan-chmark.js
+++ packages/stan/stan-chmark.js
@@ -8,14 +8,19 @@ const {
 } = require( '@wordpress/stan' );
 
 const items = {};
+const itemsMap = new Map();
+
+const digit = ( n ) => ( e ) => Math.floor( ( n % ( e * 10 ) ) / e );
 for ( let i = 0; i < 100000; i++ ) {
-	const digit = ( n ) => ( e ) => Math.floor( ( n % ( e * 10 ) ) / e );
-	items[ i ] = {
-		title: [ 10000, 1000, 100, 10, 1 ].map( digit( i ) ).join( '' ),
-	};
+	const title = [ 10000, 1000, 100, 10, 1 ].map( digit( i ) ).join( '' );
+	items[ i ] = { title };
+	const mapEntry = new Map();
+	mapEntry.set( 'title', title );
+	itemsMap.set( i, mapEntry );
 }
 
 const getItemProp = ( id, prop ) => items[ id ][ prop ];
+const getMapItemProp = ( id, prop ) => itemsMap.get( id ).get( prop );
 
 const itemsA = createAtom( items );
 
@@ -41,16 +46,33 @@ function runStanChmark() {
 		}
 	}
 
+	function benchReduxMap() {
+		for ( let i = 0; i < 100000; i++ ) {
+			getMapItemProp( i, 'title' );
+		}
+	}
+
 	function benchStan() {
 		for ( let i = 0; i < 100000; i++ ) {
 			registry.get( itemPropS( i, 'title' ) );
 		}
 	}
 
-	timed( benchRedux, 'redux1' );
-	timed( benchRedux, 'redux2' );
-	timed( benchStan, 'stan1' );
-	timed( benchStan, 'stan2' );
+	for ( let i = 0; i < 10; i++ ) {
+		timed( benchRedux, 'redux-' + i );
+	}
+
+	console.log();
+
+	for ( let i = 0; i < 10; i++ ) {
+		timed( benchReduxMap, 'redux-map-' + i );
+	}
+
+	console.log();
+
+	for ( let i = 0; i < 10; i++ ) {
+		timed( benchStan, 'stan-' + i );
+	}
 }
 
 runStanChmark();
