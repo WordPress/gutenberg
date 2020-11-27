e8a2dbbd76677e5baa93c7ee288080b1997a5236
Jon Surrell <jon.surrell@automattic.com>
2020-11-28 00:17:09 +0100

    Type useThrottle

--- packages/compose/src/hooks/use-throttle/index.js
+++ packages/compose/src/hooks/use-throttle/index.js
@@ -9,18 +9,19 @@ import { useMemoOne } from 'use-memo-one';
  */
 import { useEffect } from '@wordpress/element';
 
+/* eslint-disable jsdoc/valid-types */
 /**
  * Throttles a function with Lodash's `throttle`. A new throttled function will
  * be returned and any scheduled calls cancelled if any of the arguments change,
  * including the function to throttle, so please wrap functions created on
  * render in components in `useCallback`.
  *
- * @param {...any} args Arguments passed to Lodash's `throttle`.
- *
- * @return {Function} Throttled function.
+ * @template {(...args: any[]) => any} T
+ * @type {(func: T, wait?: number, options?: import('lodash').ThrottleSettings) => T & import('lodash').Cancelable}
  */
 export default function useThrottle( ...args ) {
 	const throttled = useMemoOne( () => throttle( ...args ), args );
 	useEffect( () => () => throttled.cancel(), [ throttled ] );
 	return throttled;
 }
+/* eslint-enable jsdoc/valid-types */
