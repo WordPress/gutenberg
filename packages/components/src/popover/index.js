08c4ff394f741dcd53cdfe5dd29104759e05ed59
Jon Surrell <jon.surrell@automattic.com>
2020-12-02 22:22:56 +0100

    Revert "Use instanceof checks"
    This reverts commit 16727dc256d9c81526282baefd0fbb503950be23.

--- packages/components/src/popover/index.js
+++ packages/components/src/popover/index.js
@@ -83,10 +83,10 @@ function computeAnchorRect(
 			return getRectangleFromRange( anchorRef );
 		}
 
-		if (
-			anchorRef?.ownerDocument &&
-			anchorRef instanceof anchorRef.ownerDocument.defaultView.Element
-		) {
+		// Duck-type to check if `anchorRef` is an instance of Element
+		// `anchorRef instanceof window.Element` checks will break across document boundaries
+		// such as in an iframe
+		if ( typeof anchorRef?.getBoundingClientRect === 'function' ) {
 			const rect = anchorRef.getBoundingClientRect();
 
 			if ( shouldAnchorIncludePadding ) {
