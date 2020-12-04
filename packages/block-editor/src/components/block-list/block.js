9d87c649832c218bb0094042a5a9f2518e89ead8
Riad Benguella <benguella@gmail.com>
2020-12-04 15:37:39 +0100

    Add an outline mode and use it both Site Editor and Template mode (#27499)

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -110,7 +110,7 @@ function BlockListBlock( {
 	// In addition to withSelect, we should favor using useSelect in this
 	// component going forward to avoid leaking new props to the public API
 	// (editor.BlockListBlock filter)
-	const { isDragging, isHighlighted, isFocusMode } = useSelect(
+	const { isDragging, isHighlighted, isFocusMode, isOutlineMode } = useSelect(
 		( select ) => {
 			const {
 				isBlockBeingDragged,
@@ -121,6 +121,7 @@ function BlockListBlock( {
 				isDragging: isBlockBeingDragged( clientId ),
 				isHighlighted: isBlockHighlighted( clientId ),
 				isFocusMode: getSettings().focusMode,
+				isOutlineMode: getSettings().outlineMode,
 			};
 		},
 		[ clientId ]
@@ -174,6 +175,7 @@ function BlockListBlock( {
 				isLargeViewport &&
 				( isSelected || isAncestorOfSelectedBlock ),
 			'is-focus-mode': isFocusMode && isLargeViewport,
+			'is-outline-mode': isOutlineMode,
 			'has-child-selected': isAncestorOfSelectedBlock && ! isDragging,
 			'is-active-entity': activeEntityBlockId === clientId,
 		},
