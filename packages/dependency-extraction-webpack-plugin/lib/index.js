9453ab24799748b7a54fe6c5f0e353b77211b063
Jarda Snajdr <jsnajdr@gmail.com>
2020-12-04 22:21:00 +0100

    Remove no-longer-needed JSDoc type annotations

--- packages/dependency-extraction-webpack-plugin/lib/index.js
+++ packages/dependency-extraction-webpack-plugin/lib/index.js
@@ -6,8 +6,6 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 // In webpack 5 there is a `webpack.sources` field but for webpack 4 we have to fallback to the `webpack-sources` package.
 const { RawSource } = webpack.sources || require( 'webpack-sources' );
-// Ignore reason: json2php is untyped
-// @ts-ignore
 const json2php = require( 'json2php' );
 
 /**
@@ -18,53 +16,8 @@ const {
 	defaultRequestToHandle,
 } = require( './util' );
 
-/**
- * Map module request to an external.
- *
- * @callback RequestToExternal
- *
- * @param {string} request Module request.
- *
- * @return {string|string[]|void} Return `undefined` to ignore the request.
- *                                     Return `string|string[]` to map the request to an external.
- */
-
-/**
- * Map module request to a script handle.
- *
- * @callback RequestToHandle
- *
- * @param {string} request Module request.
- *
- * @return {string|void} Return `undefined` to use the same name as the module.
- *                            Return `string` to map the request to a specific script handle.
- */
-
-/**
- * @typedef AssetData
- *
- * @property {string}   version      String representing a particular build
- * @property {string[]} dependencies The script dependencies
- */
-
-/**
- * @typedef Options
- *
- * @property {boolean}                     injectPolyfill      Force wp-polyfill to be included in each entry point's dependency list. This is like importing `@wordpress/polyfill` for each entry point.
- * @property {boolean}                     useDefaults         Set to `false` to disable the default WordPress script request handling.
- * @property {'php'|'json'}                outputFormat        The output format for the generated asset file.
- * @property {RequestToExternal|undefined} [requestToExternal] Map module requests to an external.
- * @property {RequestToHandle|undefined}   [requestToHandle]   Map module requests to a script handle.
- * @property {string|null}                 combinedOutputFile  This option is useful only when the combineAssets option is enabled. It allows providing a custom output file for the generated single assets file. It's possible to provide a path that is relative to the output directory.
- * @property {boolean|undefined}           combineAssets       By default, one asset file is created for each entry point. When this flag is set to true, all information about assets is combined into a single assets.(json|php) file generated in the output directory.
- */
-
 class DependencyExtractionWebpackPlugin {
-	/**
-	 * @param {Partial<Options>} options
-	 */
 	constructor( options ) {
-		/** @type {Options} */
 		this.options = Object.assign(
 			{
 				combineAssets: false,
@@ -76,14 +29,12 @@ class DependencyExtractionWebpackPlugin {
 			options
 		);
 
-		/**
+		/*
 		 * Track requests that are externalized.
 		 *
 		 * Because we don't have a closed set of dependencies, we need to track what has
 		 * been externalized so we can recognize them in a later phase when the dependency
 		 * lists are generated.
-		 *
-		 * @type {Set<string>}
 		 */
 		this.externalizedDeps = new Set();
 
@@ -94,14 +45,7 @@ class DependencyExtractionWebpackPlugin {
 		);
 	}
 
-	/* eslint-disable jsdoc/valid-types */
-	/**
-	 * @param {Parameters<WebpackExternalsFunction>[0]} _context
-	 * @param {Parameters<WebpackExternalsFunction>[1]} request
-	 * @param {Parameters<WebpackExternalsFunction>[2]} callback
-	 */
 	externalizeWpDeps( _context, request, callback ) {
-		/* eslint-enable jsdoc/valid-types */
 		let externalRequest;
 
 		// Handle via options.requestToExternal first
@@ -126,10 +70,6 @@ class DependencyExtractionWebpackPlugin {
 		return callback();
 	}
 
-	/**
-	 * @param {string} request
-	 * @return {string} Transformed request
-	 */
 	mapRequestToDependency( request ) {
 		// Handle via options.requestToHandle first
 		if ( typeof this.options.requestToHandle === 'function' ) {
@@ -151,10 +91,6 @@ class DependencyExtractionWebpackPlugin {
 		return request;
 	}
 
-	/**
-	 * @param {Object} asset
-	 * @return {string} Stringified asset
-	 */
 	stringify( asset ) {
 		if ( this.options.outputFormat === 'php' ) {
 			return `<?php return ${ json2php(
@@ -165,10 +101,6 @@ class DependencyExtractionWebpackPlugin {
 		return JSON.stringify( asset );
 	}
 
-	/**
-	 * @param {WebpackCompiler} compiler
-	 * @return {void}
-	 */
 	apply( compiler ) {
 		this.externalsPlugin.apply( compiler );
 
@@ -180,7 +112,6 @@ class DependencyExtractionWebpackPlugin {
 				outputFormat,
 			} = this.options;
 
-			/** @type {Record<string, AssetData>} */
 			const combinedAssetsData = {};
 
 			// Process each entry point independently.
@@ -188,7 +119,6 @@ class DependencyExtractionWebpackPlugin {
 				entrypointName,
 				entrypoint,
 			] of compilation.entrypoints.entries() ) {
-				/** @type {Set<string>} */
 				const entrypointExternalizedWpDeps = new Set();
 				if ( injectPolyfill ) {
 					entrypointExternalizedWpDeps.add( 'wp-polyfill' );
@@ -218,7 +148,6 @@ class DependencyExtractionWebpackPlugin {
 
 				const runtimeChunk = entrypoint.getRuntimeChunk();
 
-				/** @type {AssetData} */
 				const assetData = {
 					// Get a sorted array so we can produce a stable, stringified representation.
 					dependencies: Array.from(
@@ -288,10 +217,6 @@ class DependencyExtractionWebpackPlugin {
 	}
 }
 
-/**
- * @param {string} name
- * @return {string} Basename
- */
 function basename( name ) {
 	if ( ! name.includes( '/' ) ) {
 		return name;
@@ -300,8 +225,3 @@ function basename( name ) {
 }
 
 module.exports = DependencyExtractionWebpackPlugin;
-
-/**
- * @typedef {import('webpack').Compiler} WebpackCompiler
- * @typedef {import('webpack').ExternalsFunctionElement} WebpackExternalsFunction
- */
