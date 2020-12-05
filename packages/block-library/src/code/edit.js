52ea00f63f8af7bff90afa476807d3fd6ff304d7
grzegorz_marzencki <grzegorz.marzencki@a8c.com>
2020-12-05 16:53:01 +0100

    when an element with a tooltip is disabled, add additional layer which catch events

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -3,11 +3,15 @@
  */
 import { __ } from '@wordpress/i18n';
 import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { Button, Tooltip } from '@wordpress/components';
 
 export default function CodeEdit( { attributes, setAttributes, onRemove } ) {
 	const blockProps = useBlockProps();
 	return (
 		<pre { ...blockProps }>
+			<Tooltip text="Enabled button">
+				<Button disabled>Enabled</Button>
+			</Tooltip>
 			<RichText
 				tagName="code"
 				value={ attributes.content }
@@ -16,7 +20,6 @@ export default function CodeEdit( { attributes, setAttributes, onRemove } ) {
 				placeholder={ __( 'Write code…' ) }
 				aria-label={ __( 'Code' ) }
 				preserveWhiteSpace
-				__unstablePastePlainText
 			/>
 		</pre>
 	);
--- packages/block-library/src/code/save.js
+++ packages/block-library/src/code/save.js
@@ -7,10 +7,14 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import { escape } from './utils';
+import { Button, Tooltip } from '@wordpress/components';
 
 export default function save( { attributes } ) {
 	return (
 		<pre { ...useBlockProps.save() }>
+			<Tooltip.Content text="Enabled button">
+				<Button disabled>Enabled</Button>
+			</Tooltip.Content>
 			<RichText.Content
 				tagName="code"
 				value={ escape( attributes.content ) }
