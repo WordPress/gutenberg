bbdfa3bca44cdadcc5c1403d964bdb43389ca0f2
grzegorz_marzencki <grzegorz.marzencki@a8c.com>
2020-12-08 02:47:29 +0100

    file refactor

--- packages/blocks/src/api/raw-handling/phrasing-content-reducer.js
+++ packages/blocks/src/api/raw-handling/phrasing-content-reducer.js
@@ -1,71 +1,79 @@
 /**
  * External dependencies
  */
-import { includes } from 'lodash';
+import { identity, includes } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { wrap, replaceTag } from '@wordpress/dom';
 
-const defaultColor = [ 'rgb(0, 0, 0)', 'rgba(0, 0, 0, 0)', 'black', '#000000' ];
+const getSemanticTags = ( { node } ) => {
+	const {
+		fontWeight,
+		fontStyle,
+		textDecorationLine,
+		textDecoration,
+		verticalAlign,
+	} = node.style;
+	const tags = [];
+	if ( fontWeight === 'bold' || fontWeight === '700' ) {
+		tags.push( 'strong' );
+	}
+
+	if ( fontStyle === 'italic' ) {
+		tags.push( 'em' );
+	}
+
+	// Some DOM implementations (Safari, JSDom) don't support
+	// style.textDecorationLine, so we check style.textDecoration as a
+	// fallback.
+	if (
+		textDecorationLine === 'line-through' ||
+		includes( textDecoration, 'line-through' )
+	) {
+		tags.push( 's' );
+	}
+
+	if ( verticalAlign === 'super' ) {
+		tags.push( 'sup' );
+	} else if ( verticalAlign === 'sub' ) {
+		tags.push( 'sub' );
+	}
+	return tags;
+};
+
+const handleSpan = ( { node, doc } ) => {
+	const semanticTagsToWrap = getSemanticTags( node );
+	semanticTagsToWrap.forEach( ( tag ) =>
+		wrap( doc.createElement( tag ), node )
+	);
+};
+const handleBold = ( { node } ) => replaceTag( node, 'strong' );
+const handleItalic = ( { node } ) => replaceTag( node, 'italic' );
+const handleAnchor = ( { node } ) => {
+	// In jsdom-jscore, 'node.target' can be null.
+	// TODO: Explore fixing this by patching jsdom-jscore.
+	if ( node.target && node.target.toLowerCase() === '_blank' ) {
+		node.rel = 'noreferrer noopener';
+	} else {
+		node.removeAttribute( 'target' );
+		node.removeAttribute( 'rel' );
+	}
+};
+
+const elementNameToHandlerMapper = {
+	span: handleSpan,
+	B: handleBold,
+	I: handleItalic,
+	A: handleAnchor,
+};
 
 export default function phrasingContentReducer( node, doc ) {
 	// In jsdom-jscore, 'node.style' can be null.
 	// TODO: Explore fixing this by patching jsdom-jscore.
-	if ( node.nodeName === 'SPAN' && node.style ) {
-		const {
-			color,
-			fontWeight,
-			fontStyle,
-			textDecorationLine,
-			textDecoration,
-			verticalAlign,
-		} = node.style;
-
-		// adds color to the wrapper
-		if ( color && ! defaultColor.includes( color ) ) {
-			const wrapper = doc.createElement( 'coloured' );
-			wrapper.style.color = color;
-			wrapper.style.backgroundColor = '';
-			wrap( wrapper, node );
-		}
-
-		if ( fontWeight === 'bold' || fontWeight === '700' ) {
-			wrap( doc.createElement( 'strong' ), node );
-		}
-
-		if ( fontStyle === 'italic' ) {
-			wrap( doc.createElement( 'em' ), node );
-		}
-
-		// Some DOM implementations (Safari, JSDom) don't support
-		// style.textDecorationLine, so we check style.textDecoration as a
-		// fallback.
-		if (
-			textDecorationLine === 'line-through' ||
-			includes( textDecoration, 'line-through' )
-		) {
-			wrap( doc.createElement( 's' ), node );
-		}
-
-		if ( verticalAlign === 'super' ) {
-			wrap( doc.createElement( 'sup' ), node );
-		} else if ( verticalAlign === 'sub' ) {
-			wrap( doc.createElement( 'sub' ), node );
-		}
-	} else if ( node.nodeName === 'B' ) {
-		node = replaceTag( node, 'strong' );
-	} else if ( node.nodeName === 'I' ) {
-		node = replaceTag( node, 'em' );
-	} else if ( node.nodeName === 'A' ) {
-		// In jsdom-jscore, 'node.target' can be null.
-		// TODO: Explore fixing this by patching jsdom-jscore.
-		if ( node.target && node.target.toLowerCase() === '_blank' ) {
-			node.rel = 'noreferrer noopener';
-		} else {
-			node.removeAttribute( 'target' );
-			node.removeAttribute( 'rel' );
-		}
-	}
+	const elementType = node.nodeName;
+	const elementHandler =
+		elementNameToHandlerMapper[ elementType ] || identity;
+	elementHandler( { node, doc } );
 }
