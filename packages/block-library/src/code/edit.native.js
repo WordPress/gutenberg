5383eb5ee3514ee4f898a2c1ea627b908cd6317f
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2020-12-09 11:16:09 +0100

    Try RichText in code block

--- packages/block-library/src/code/edit.native.js
+++ packages/block-library/src/code/edit.native.js
@@ -6,7 +6,7 @@ import { View } from 'react-native';
 /**
  * WordPress dependencies
  */
-import { PlainText } from '@wordpress/block-editor';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { withPreferredColorScheme } from '@wordpress/compose';
 
@@ -14,44 +14,47 @@ import { withPreferredColorScheme } from '@wordpress/compose';
  * Internal dependencies
  */
 
-/**
- * Block code style
- */
-import styles from './theme.scss';
+// import styles from './theme.scss';
 
 // Note: styling is applied directly to the (nested) PlainText component. Web-side components
 // apply it to the container 'div' but we don't have a proper proposal for cascading styling yet.
 export function CodeEdit( props ) {
 	const {
 		attributes,
+		mergeBlocks,
 		setAttributes,
-		onFocus,
-		onBlur,
-		getStylesFromColorScheme,
+		onRemove,
+		// getStylesFromColorScheme,
 	} = props;
-	const codeStyle = getStylesFromColorScheme(
-		styles.blockCode,
-		styles.blockCodeDark
-	);
-	const placeholderStyle = getStylesFromColorScheme(
-		styles.placeholder,
-		styles.placeholderDark
-	);
+	// const codeStyle = getStylesFromColorScheme(
+	// 	styles.blockCode,
+	// 	styles.blockCodeDark
+	// );
+	// const placeholderStyle = getStylesFromColorScheme(
+	// 	styles.placeholder,
+	// 	styles.placeholderDark
+	// );
+
+	const blockProps = useBlockProps();
 
 	return (
-		<View>
-			<PlainText
+		<View
+		// style={ codeStyle }
+		>
+			<RichText
+				tagName="code"
 				value={ attributes.content }
-				style={ codeStyle }
-				multiline={ true }
-				underlineColorAndroid="transparent"
 				onChange={ ( content ) => setAttributes( { content } ) }
+				onRemove={ onRemove }
 				placeholder={ __( 'Write code…' ) }
 				aria-label={ __( 'Code' ) }
-				isSelected={ props.isSelected }
-				onFocus={ onFocus }
-				onBlur={ onBlur }
-				placeholderTextColor={ placeholderStyle.color }
+				preserveWhiteSpace
+				__unstablePastePlainText
+				//
+				// From preformatted edit.js
+				identifier="content"
+				onMerge={ mergeBlocks }
+				{ ...blockProps }
 			/>
 		</View>
 	);
