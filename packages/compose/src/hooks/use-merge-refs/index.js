e0df58cf593337fbee31a81b5a2fd1b4d83804df
Ella van Durpe <ella@vandurpe.com>
2020-12-11 20:56:56 +0200

    Elaborate on dependencies

--- packages/compose/src/hooks/use-merge-refs/index.js
+++ packages/compose/src/hooks/use-merge-refs/index.js
@@ -11,6 +11,14 @@ import { useCallback } from '@wordpress/element';
 /** @typedef {import('@wordpress/element').RefObject} RefObject */
 
 /**
+ * A ref should persist throughout a component's lifecycle, so it should not
+ * have any dependencies. If dependencies are needed in a ref callback, assign
+ * them to refs with `useRef` and use the ref in the callback or use
+ * `useEffect`.
+ */
+const DEPENDENCIES = [];
+
+/**
  * Merges the given refs into a new ref, which will persist for the full
  * lifetime of the component, as a ref should be.
  *
@@ -19,5 +27,5 @@ import { useCallback } from '@wordpress/element';
  * @return {Function} A new ref callback.
  */
 export default function useMergeRefs( ...refs ) {
-	return useCallback( mergeRefs( refs ), [] );
+	return useCallback( mergeRefs( refs ), DEPENDENCIES );
 }
