0248332e2e137d08a04d0ccca386a67887d8f79d
Glen Davies <glen.davies@a8c.com>
2020-12-14 08:52:09 +1300

    Revert "Revert "Move block props to the outer wrapper""
    This reverts commit a7504fd8386efd46577e2d9a566e7b816404e090.

--- packages/block-library/src/image/edit.js
+++ packages/block-library/src/image/edit.js
@@ -307,6 +307,7 @@ export function ImageEdit( {
 		'is-resized': !! width || !! height,
 		'is-focused': isSelected,
 		[ `size-${ sizeSlug }` ]: sizeSlug,
+		'list-image': isListItem,
 	} );
 
 	const blockProps = useBlockProps( {
@@ -333,8 +334,8 @@ export function ImageEdit( {
 		return (
 			<>
 				{ controls }
-				<li className="list-image">
-					<figure { ...blockProps }>
+				<li { ...blockProps }>
+					<figure>
 						{ image }
 						{ mediaPlaceholder }
 					</figure>
