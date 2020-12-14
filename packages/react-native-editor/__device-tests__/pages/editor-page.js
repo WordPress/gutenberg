83c1c10b54499f7a1874a17506d4fdf9e0c1b6df
Wendy Chen <wendy.chen@automattic.com>
2020-12-14 13:28:37 -0600

    WC: Moving UI tests to gutenberg with other UI tests instead of keeping at gutenberg-mobile. Adding more convenience functions to EditorPage.

--- packages/react-native-editor/__device-tests__/pages/editor-page.js
+++ packages/react-native-editor/__device-tests__/pages/editor-page.js
@@ -10,7 +10,12 @@ const {
 	typeString,
 	toggleHtmlMode,
 	swipeFromTo,
-} = require( '../helpers/utils' );
+} from '../helpers/utils';
+/**
+ * External dependencies
+ */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import wd from 'wd';
 
 const initializeEditorPage = async () => {
 	const driver = await setupDriver();
@@ -267,6 +272,24 @@ class EditorPage {
 		await toolBarButton.click();
 	}
 
+	async longPressToolBarButton( buttonName ) {
+		let toolBarButton;
+		if ( isAndroid() ) {
+			const blockLocator = `//*[contains(@${ this.accessibilityIdXPathAttrib }, "${ buttonName }")]`;
+			toolBarButton = await this.driver.elementByXPath( blockLocator );
+		} else {
+			toolBarButton = await this.driver.elementByAccessibilityId(
+				buttonName
+			);
+		}
+
+		const action = new wd.TouchAction( this.driver );
+		action.press( { el: toolBarButton } );
+		action.wait( 1000 );
+		action.release();
+		await action.perform();
+	}
+
 	// =========================
 	// Inline toolbar functions
 	// =========================
@@ -363,11 +386,15 @@ class EditorPage {
 	}
 
 	async typeTextToParagraphBlock( block, text, clear ) {
-		const textViewElement = await this.getTextViewForParagraphBlock(
-			block
-		);
-		await typeString( this.driver, textViewElement, text, clear );
-		await this.driver.sleep( 1000 ); // Give time for the block to rerender (such as for accessibility)
+		if ( ! isAndroid() ) {
+			block.type( text );
+		} else {
+			const textViewElement = await this.getTextViewForParagraphBlock(
+				block
+			);
+			await typeString( this.driver, textViewElement, text, clear );
+			await this.driver.sleep( 1000 ); // Give time for the block to rerender (such as for accessibility)
+		}
 	}
 
 	async sendTextToParagraphBlock( position, text, clear ) {
@@ -550,6 +577,46 @@ class EditorPage {
 
 	async sauceJobStatus( allPassed ) {
 		await this.driver.sauceJobStatus( allPassed );
+	}
+	
+	// =============================
+	// Misc functions
+	// =============================
+	async findElement( name, iosElementType ) {
+		const elementName = isAndroid()
+			? '//*'
+			: `//XCUIElementType${ iosElementType }`;
+		const blockLocator = `${ elementName }[contains(@${ this.accessibilityIdXPathAttrib }, "${ name }")]`;
+		const elements = await this.driver.elementsByXPath( blockLocator );
+		return elements[ 0 ];
+	}
+
+	async selectElement( name, iosElementType ) {
+		const el = await this.findElement( name, iosElementType );
+		el.click();
+	}
+
+	async selectBlock(
+		blockName,
+		position = 1,
+		options = { autoscroll: false }
+	) {
+		const block = await this.getBlockAtPosition(
+			blockName,
+			position,
+			options
+		);
+		block.click();
+	}
+
+	async tapCoordinates( x, y, longPress = false ) {
+		const action = new wd.TouchAction( this.driver );
+		action.press( { x, y } );
+		if ( longPress ) {
+			action.wait( 1000 );
+		}
+		action.release();
+		await action.perform();
 	}
 }
 
