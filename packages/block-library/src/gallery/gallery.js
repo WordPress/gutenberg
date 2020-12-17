c465a0d38288be9c78421213db7fac12d2391737
Glen Davies <glen.davies@a8c.com>
2020-12-18 09:37:02 +1300

    put media uploader outside figure so structure matches front end

--- packages/block-library/src/gallery/gallery.js
+++ packages/block-library/src/gallery/gallery.js
@@ -68,7 +68,6 @@ export const Gallery = ( props ) => {
 		>
 			<figure { ...innerBlocksProps }>
 				{ children }
-				{ mediaPlaceholder }
 				<RichTextVisibilityHelper
 					isHidden={ ! isSelected && RichText.isEmpty( caption ) }
 					tagName="figcaption"
@@ -85,6 +84,7 @@ export const Gallery = ( props ) => {
 					}
 				/>
 			</figure>
+			{ mediaPlaceholder }
 		</div>
 	);
 };
