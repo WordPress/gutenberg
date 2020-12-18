aff6e2611a883daf67121ca454efe3cd7077553f
Joel Dean <jdeanjj1000@gmail.com>
2020-12-18 01:38:09 -0500

    added the errorMessage to the PlainText component.

--- packages/block-editor/src/components/media-upload-progress/index.native.js
+++ packages/block-editor/src/components/media-upload-progress/index.native.js
@@ -29,7 +29,7 @@ export class MediaUploadProgress extends React.Component {
 			progress: 0,
 			isUploadInProgress: false,
 			isUploadFailed: false,
-			uploadErrorMessage: "",
+			uploadErrorMessage: '',
 		};
 
 		this.mediaUpload = this.mediaUpload.bind( this );
