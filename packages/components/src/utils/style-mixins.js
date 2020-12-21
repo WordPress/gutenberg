18e6dd91a8d78ad822f11e93583e8cd74ba59185
Riad Benguella <benguella@gmail.com>
2020-12-21 09:41:18 +0100

    Use a consistent way to check isRTL

--- packages/components/src/utils/style-mixins.js
+++ packages/components/src/utils/style-mixins.js
@@ -1,6 +1,5 @@
 export { color, rgba } from './colors';
 export { reduceMotion } from './reduce-motion';
-export { rtl, getRTL, useRTL } from './rtl';
 export { space } from './space';
 export { font } from './font';
 export { breakpoint } from './breakpoint';
