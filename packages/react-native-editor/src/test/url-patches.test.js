2ddaf1f1879abe697d511d1f1532de731b12fb6c
Matthew Kevins <mmkevins@yahoo.com>
2020-12-23 11:19:19 +1000

    Add tests for patched core URL implementation

--- packages/react-native-editor/src/test/url-patches.test.js
+++ packages/react-native-editor/src/test/url-patches.test.js
@@ -1,7 +1,16 @@
 /**
+ * Internal dependencies
+ */
+import patchURL from '../patch-url';
+
+/**
  * External dependencies
  */
 import { URL as CoreURL } from 'react-native/Libraries/Blob/URL';
+
+// use subclass to test patched prototype without side-effects on other tests
+class PatchedCoreURL extends CoreURL {}
+patchURL( PatchedCoreURL );
 
 describe( 'core URL.prototype.search', () => {
 	it( 'throws not implemented error', () => {
@@ -9,21 +18,15 @@ describe( 'core URL.prototype.search', () => {
 		expect( () => url.search ).toThrow( 'not implemented' );
 	} );
 } );
-describe( 'globals URL.prototype.search', () => {
-	beforeAll( () => {
-		const originalURL = global.URL;
-		global.URL = CoreURL;
-		require( '../url-patches' );
-		global.URL = originalURL;
-	} );
 
+describe( 'patched core URL.prototype.search', () => {
 	it( 'works without parameters', () => {
-		const url = new CoreURL( '/', 'http://example.com' );
+		const url = new PatchedCoreURL( '/', 'http://example.com' );
 		expect( url.search ).toEqual( '' );
 	} );
 
 	it( 'works with parameters', () => {
-		const url = new CoreURL(
+		const url = new PatchedCoreURL(
 			'/test-path/file.extension?query=params&more#anchor',
 			'http://example.com'
 		);
@@ -31,12 +34,15 @@ describe( 'globals URL.prototype.search', () => {
 	} );
 
 	it( 'works with key-only parameters', () => {
-		const url = new CoreURL( '/test-path?query', 'http://example.com' );
+		const url = new PatchedCoreURL(
+			'/test-path?query',
+			'http://example.com'
+		);
 		expect( url.search ).toBe( '?query' );
 	} );
 
 	it( 'works with special characters in parameters', () => {
-		const url = new CoreURL(
+		const url = new PatchedCoreURL(
 			'/search?source=hp&ei=tP7kW8-_FoK89QORoa2QBQ&q=test+url&oq=test+url&gs_l=psy-ab.3..0l10',
 			'http://example.com'
 		);
@@ -46,7 +52,7 @@ describe( 'globals URL.prototype.search', () => {
 	} );
 
 	it( 'works with an encoded path', () => {
-		const url = new CoreURL(
+		const url = new PatchedCoreURL(
 			'/this%20is%20a%20test?query',
 			'http://example.com'
 		);
@@ -54,7 +60,7 @@ describe( 'globals URL.prototype.search', () => {
 	} );
 
 	it( 'works with encoded parameters', () => {
-		const url = new URL(
+		const url = new PatchedCoreURL(
 			'/test?query=something%20with%20spaces',
 			'http://example.com'
 		);
@@ -62,7 +68,7 @@ describe( 'globals URL.prototype.search', () => {
 	} );
 
 	it( 'works with bracket parameters', () => {
-		const url = new CoreURL(
+		const url = new PatchedCoreURL(
 			'/beach?foo[]=bar&foo[]=baz',
 			'http://example.com'
 		);
@@ -70,12 +76,15 @@ describe( 'globals URL.prototype.search', () => {
 	} );
 
 	it( 'works with no slash', () => {
-		const url = new URL( '?foo[]=bar&foo[]=baz', 'http://example.com' );
+		const url = new PatchedCoreURL(
+			'?foo[]=bar&foo[]=baz',
+			'http://example.com'
+		);
 		expect( url.search ).toBe( '?foo[]=bar&foo[]=baz' );
 	} );
 
 	it( 'works with multiple ?s', () => {
-		const url = new CoreURL(
+		const url = new PatchedCoreURL(
 			'?foo=bar&foo=baz?test',
 			'http://example.com'
 		);
