64cc123cb089f71452fed232d5f7dc6b276d2fa8
Jessica Duarte <jessicanduarte@gmail.com>
2020-12-23 08:09:09 -0500

    gutenpride template package created

---
+++ packages/gutenpride-template/src/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	defaultValues: {
+		slug: 'block-template-gutenpride',
+		namespace: 'gutenpride',
+		title: 'Gutenpride',
+		description:
+			'A Gutenberg block to show your pride! This block enables you to type text and style it with the color font Gilbert from Type with Pride',
+
+		dashicon: 'smiley',
+	},
+	templatesPath: __dirname,
+};
