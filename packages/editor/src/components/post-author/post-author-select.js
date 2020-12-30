5ebf486efd919bae5ae5d0fb196e0c33087123de
Adam Silverstein <adamjs@google.com>
2020-12-30 10:52:50 -0700

    Refactor PostAuthorSelect as functional component.

--- packages/editor/src/components/post-author/post-author-select.js
+++ packages/editor/src/components/post-author/post-author-select.js
@@ -2,55 +2,39 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { compose } from '@wordpress/compose';
-import { Component } from '@wordpress/element';
-import { withSelect, withDispatch } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { decodeEntities } from '@wordpress/html-entities';
 import { SelectControl } from '@wordpress/components';
 
-export class PostAuthorSelect extends Component {
-	constructor() {
-		super( ...arguments );
-
-		this.setAuthorId = this.setAuthorId.bind( this );
-	}
-
-	setAuthorId( value ) {
-		const { onUpdateAuthor } = this.props;
-		onUpdateAuthor( Number( value ) );
-	}
-
-	render() {
-		const { postAuthor, authors } = this.props;
-		const authorOptions = authors.map( ( author ) => ( {
-			label: decodeEntities( author.name ),
-			value: author.id,
-		} ) );
-
-		return (
-			<SelectControl
-				className="post-author-selector"
-				label={ __( 'Author' ) }
-				options={ authorOptions }
-				onChange={ this.setAuthorId }
-				value={ postAuthor }
-			/>
-		);
-	}
-}
-
-export default compose( [
-	withSelect( ( select ) => {
+function PostAuthorSelect() {
+	const { editPost } = useDispatch( 'core/editor' );
+	const { postAuthor, authors } = useSelect( ( select ) => {
+		const authorsFromAPI = select( 'core' ).getAuthors();
 		return {
 			postAuthor: select( 'core/editor' ).getEditedPostAttribute(
 				'author'
 			),
-			authors: select( 'core' ).getAuthors(),
+			authors: authorsFromAPI.map( ( author ) => ( {
+				label: decodeEntities( author.name ),
+				value: author.id,
+			} ) ),
 		};
-	} ),
-	withDispatch( ( dispatch ) => ( {
-		onUpdateAuthor( author ) {
-			dispatch( 'core/editor' ).editPost( { author } );
-		},
-	} ) ),
-] )( PostAuthorSelect );
+	} );
+
+	const setAuthorId = ( value ) => {
+		const author = Number( value );
+		editPost( { author } );
+	};
+
+	return (
+		<SelectControl
+			className="post-author-selector"
+			label={ __( 'Author' ) }
+			options={ authors }
+			onChange={ setAuthorId }
+			value={ postAuthor }
+		/>
+	);
+}
+
+export default PostAuthorSelect;
