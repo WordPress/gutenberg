3155ceeef7a1a29e61d0a9e84583c42e7341e7f0
Haz <hazdiego@gmail.com>
2021-01-07 15:07:05 +0100

    Add announcement

--- packages/block-editor/src/components/inserter-listbox/group.js
+++ packages/block-editor/src/components/inserter-listbox/group.js
@@ -1,15 +1,33 @@
 /**
  * WordPress dependencies
  */
-import { forwardRef } from '@wordpress/element';
+import { forwardRef, useRef } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { speak } from '@wordpress/a11y';
 
 function InserterListboxGroup( props, ref ) {
+	const hasSpokenRef = useRef( false );
 	return (
 		<div
+			ref={ ref }
 			role="listbox"
 			aria-orientation="horizontal"
+			onFocus={ () => {
+				if ( ! hasSpokenRef.current ) {
+					speak(
+						__(
+							'Use left and right arrow keys to move through blocks'
+						)
+					);
+					hasSpokenRef.current = true;
+				}
+			} }
+			onBlur={ ( event ) => {
+				if ( ! event.currentTarget.contains( event.relatedTarget ) ) {
+					hasSpokenRef.current = false;
+				}
+			} }
 			{ ...props }
-			ref={ ref }
 		/>
 	);
 }
