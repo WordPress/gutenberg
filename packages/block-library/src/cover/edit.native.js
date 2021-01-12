cd1befa3f13576cbb92139f3d80037d2f2626bbd
David Calhoun <dpcalhoun@gmail.com>
2021-01-11 21:01:00 -0600

    Render media within settings panel
    Initial pass at rendering media. Potentially includes unnecessary focus
    styles and media upload callback handlers.

--- packages/block-library/src/cover/edit.native.js
+++ packages/block-library/src/cover/edit.native.js
@@ -341,6 +341,30 @@ const Cover = ( {
 				<PanelBody title={ __( 'Media' ) }>
 					<TextControl
 						leftAlign
+						cellContainerStyle={ { minHeight } }
+					>
+						<View style={ [ styles.background, backgroundColor ] }>
+							<View style={ styles.imageContainer }>
+								<Image
+									editButton={ false }
+									focalPoint={
+										focalPoint || IMAGE_DEFAULT_FOCAL_POINT
+									}
+									isSelected={ isParentSelected }
+									isUploadFailed={ didUploadFail }
+									isUploadInProgress={ isUploadInProgress }
+									onSelectMediaUploadOption={ onSelectMedia }
+									openMediaOptions={
+										openMediaOptionsRef.current
+									}
+									url={ url }
+									width={ styles.image.width }
+								/>
+							</View>
+						</View>
+					</BottomSheet.Cell>
+					<BottomSheet.Cell
+						leftAlign
 						label={ __( 'Clear Media' ) }
 						labelStyle={ styles.clearMediaButton }
 						onPress={ onClearMedia }
