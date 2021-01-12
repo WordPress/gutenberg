c5bce50c36cbc99f477c6f4393a4f4b4cff0acda
ntsekouras <ntsekouras@outlook.com>
2021-01-12 09:38:10 +0200

    Add query pagination blocks

--- packages/block-library/src/query-pagination-numbers/index.js
+++ packages/block-library/src/query-pagination-numbers/index.js
@@ -8,18 +8,14 @@ import { _x, __ } from '@wordpress/i18n';
  */
 import metadata from './block.json';
 import edit from './edit';
-import save from './save';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
-	title: _x( 'Query Pagination', 'block title' ),
-	description: __(
-		'Displays a paginated navigation to next/previous set of posts, when applicable.'
-	),
+	title: _x( 'Query Pagination Numbers', 'block title' ),
+	description: __( 'Displays a list of page numbers for pagination' ),
 	// TODO create icon
 	edit,
-	save,
-	parent: [ 'core/query' ],
+	parent: [ 'core/query-pagination' ],
 };
--- packages/block-library/src/query-pagination-previous/index.js
+++ packages/block-library/src/query-pagination-previous/index.js
@@ -8,18 +8,14 @@ import { _x, __ } from '@wordpress/i18n';
  */
 import metadata from './block.json';
 import edit from './edit';
-import save from './save';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
-	title: _x( 'Query Pagination', 'block title' ),
-	description: __(
-		'Displays a paginated navigation to next/previous set of posts, when applicable.'
-	),
+	title: _x( 'Query Pagination Previous', 'block title' ),
+	description: __( 'Displays the previous posts page link.' ),
 	// TODO create icon
 	edit,
-	save,
-	parent: [ 'core/query' ],
+	parent: [ 'core/query-pagination' ],
 };
