6b1ad3bd8332555b6e17d00c695d81aa23468123
David Calhoun <dpcalhoun@gmail.com>
2021-01-11 21:01:01 -0600

    [wip] Add drag handler for focal point picker

--- packages/components/src/focal-point-picker/index.native.js
+++ packages/components/src/focal-point-picker/index.native.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { Text, View } from 'react-native';
+import { Animated, Text, View } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 /**
  * WordPress dependencies
@@ -9,6 +10,7 @@ import { Text, View } from 'react-native';
 import { __ } from '@wordpress/i18n';
 import { Image, RangeControl } from '@wordpress/components';
 import { Path, SVG } from '@wordpress/primitives';
+import { useState, useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -18,26 +20,109 @@ import styles from './style.scss';
 const MIN_POSITION_VALUE = 0;
 const MAX_POSITION_VALUE = 100;
 
-export default function FocalPointPicker( { focalPoint, onChange, url } ) {
+export default function FocalPointPicker( props ) {
+	const { focalPoint, onChange, shouldEnableBottomSheetScroll, url } = props;
 	const { x, y } = focalPoint;
 
-	function setAxisPostion( axis ) {
-		return ( pos ) => {
-			onChange( {
-				...focalPoint,
-				[ axis ]: pos / 100,
-			} );
-		};
+	const [ containerSize, setContainerSize ] = useState( null );
+
+	// TODO(David): Need to round the value placed into state at some point, to
+	// avoid floats in sliders. Maybe do it in the slider?
+	function setPosition( { x, y } ) {
+		onChange( ( prevState ) => ( {
+			...prevState,
+			...( x ? { x } : {} ),
+			...( y ? { y } : {} ),
+		} ) );
+	}
+
+	_touchX = new Animated.Value( x * containerSize?.width || 1 );
+	_touchY = new Animated.Value( y * containerSize?.height || 1 );
+
+	function onHandlerStateChange( { nativeEvent } ) {
+		switch ( nativeEvent.state ) {
+			case State.BEGAN:
+				shouldEnableBottomSheetScroll( false );
+				break;
+			case State.ACTIVE:
+				shouldEnableBottomSheetScroll( false );
+				break;
+			default:
+				shouldEnableBottomSheetScroll( true );
+				break;
+		}
 	}
 
 	return (
 		<View style={ styles.container }>
 			<View style={ [ styles.media ] }>
-				<Image url={ url } width={ styles.image.width } />
-				<View
+				<PanGestureHandler
+					minDist={ 1 }
+					onHandlerStateChange={ onHandlerStateChange }
+					onGestureEvent={ Animated.event(
+						[ { nativeEvent: { x: _touchX, y: _touchY } } ],
+						{
+							useNativeDriver: true,
+							listener: ( { nativeEvent } ) => {
+								const { x, y } = nativeEvent;
+								setPosition( {
+									x: x / containerSize?.width,
+									y: y / containerSize?.height,
+								} );
+							},
+						}
+					) }
+				>
+					<Animated.View
+						onLayout={ ( event ) => {
+							const { height, width } = event.nativeEvent.layout;
+
+							if (
+								width !== 0 &&
+								height !== 0 &&
+								( containerSize?.width !== width ||
+									containerSize?.height !== height )
+							) {
+								setContainerSize( { width, height } );
+							}
+						} }
+					>
+						<Image url={ url } width={ styles.image.width } />
+					</Animated.View>
+				</PanGestureHandler>
+				<Animated.View
 					style={ [
 						styles.focalPointWrapper,
-						{ top: `${ y * 100 }%`, left: `${ x * 100 }%` },
+						{
+							transform: [
+								{
+									translateX: _touchX.interpolate( {
+										inputRange: [
+											0,
+											containerSize?.width || 0,
+										],
+										outputRange: [
+											0,
+											containerSize?.width || 0,
+										],
+										extrapolate: 'clamp',
+									} ),
+								},
+								{
+									translateY: _touchY.interpolate( {
+										inputRange: [
+											0,
+											containerSize?.height || 0,
+										],
+										outputRange: [
+											0,
+											containerSize?.height || 0,
+										],
+										extrapolate: 'clamp',
+									} ),
+								},
+							],
+						},
 					] }
 				>
 					<SVG
@@ -54,8 +139,9 @@ export default function FocalPointPicker( { focalPoint, onChange, url } ) {
 							d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z"
 						/>
 					</SVG>
-				</View>
+				</Animated.View>
 			</View>
+			{ /* TODO(David): RangeControl is uncontrolled, how might I set its value via the pan gesture? */ }
 			<RangeControl
 				value={ x * 100 }
 				label={ __( 'X-Axis Position' ) }
@@ -63,7 +149,7 @@ export default function FocalPointPicker( { focalPoint, onChange, url } ) {
 				max={ MAX_POSITION_VALUE }
 				initialPosition={ x * 100 }
 				allowReset
-				onChange={ setAxisPostion( 'x' ) }
+				onChange={ ( x ) => setPosition( { x: x / 100 } ) }
 			/>
 			<RangeControl
 				value={ y * 100 }
@@ -72,7 +158,7 @@ export default function FocalPointPicker( { focalPoint, onChange, url } ) {
 				max={ MAX_POSITION_VALUE }
 				initialPosition={ y * 100 }
 				allowReset
-				onChange={ setAxisPostion( 'y' ) }
+				onChange={ ( y ) => setPosition( { y: y / 100 } ) }
 			/>
 		</View>
 	);
