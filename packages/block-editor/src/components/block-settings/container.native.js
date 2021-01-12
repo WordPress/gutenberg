865a227ca042c93ee5e14be95633f8638d490137
David Calhoun <dpcalhoun@gmail.com>
2021-01-11 21:01:04 -0600

    Revert "Leverage full-height sub-sheet for focal point picker"
    This reverts commit c0b75779ab51ee38a1f56b1e405efaa3296e5d83. Allowing
    scrolling of the full-height bottom sheet content for landscape
    orientation proved to be difficult. Now that we set a max-height for the
    focal point picker media, a full-height bottom sheet may not be all that
    necessary.

--- packages/block-editor/src/components/block-settings/container.native.js
+++ packages/block-editor/src/components/block-settings/container.native.js
@@ -50,7 +50,6 @@ function BottomSheetSettings( {
 				</BottomSheet.NavigationScreen>
 				<BottomSheet.NavigationScreen
 					name={ blockSettingsScreens.focalPoint }
-					fullScreen
 				>
 					<FocalPointSettings />
 				</BottomSheet.NavigationScreen>
