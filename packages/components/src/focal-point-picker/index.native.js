f96e9642ca5bed47203845a15701a4cc44be648d
David Calhoun <dpcalhoun@gmail.com>
2021-01-11 21:01:01 -0600

    Add focal point picker handle

--- packages/components/src/focal-point-picker/index.native.js
+++ packages/components/src/focal-point-picker/index.native.js
@@ -8,6 +8,7 @@ import { Text, View } from 'react-native';
  */
 import { __ } from '@wordpress/i18n';
 import { Image, RangeControl } from '@wordpress/components';
+import { Path, SVG } from '@wordpress/primitives';
 
 /**
  * Internal dependencies
@@ -33,6 +34,28 @@ export default function FocalPointPicker( { focalPoint, onChange, url } ) {
 		<View style={ styles.container }>
 			<View style={ [ styles.media ] }>
 				<Image url={ url } width={ styles.image.width } />
+				<View
+					style={ [
+						styles.focalPointWrapper,
+						// TODO(David): Update coordinates with state
+						{ top: '50%', left: '50%' },
+					] }
+				>
+					<SVG
+						style={ styles.focalPointIcon }
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 30 30"
+					>
+						<Path
+							style={ styles.focalPointIconPathOutline }
+							d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"
+						/>
+						<Path
+							style={ styles.focalPointIconPathFill }
+							d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z"
+						/>
+					</SVG>
+				</View>
 			</View>
 			<RangeControl
 				value={ x * 100 }
