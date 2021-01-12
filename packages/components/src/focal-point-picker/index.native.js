40139579e9494d1dda0f9c766057b4f393b97f06
David Calhoun <dpcalhoun@gmail.com>
2021-01-11 21:01:03 -0600

    Limit max-height for focal point picker media

--- packages/components/src/focal-point-picker/index.native.js
+++ packages/components/src/focal-point-picker/index.native.js
@@ -112,61 +112,71 @@ export default function FocalPointPicker( props ) {
 							} }
 							style={ styles.imageContainer }
 						>
-							<Image url={ url } width={ styles.image.width } />
+							<Image height="100%" url={ url } />
+							<Animated.View
+								pointerEvents="none"
+								style={ [
+									styles.focalPointWrapper,
+									{
+										transform: [
+											{
+												translateX: _touchX.interpolate(
+													{
+														inputRange: [
+															0,
+															containerSize?.width ||
+																0,
+														],
+														outputRange: [
+															0,
+															containerSize?.width ||
+																0,
+														],
+														extrapolate: 'clamp',
+													}
+												),
+											},
+											{
+												translateY: _touchY.interpolate(
+													{
+														inputRange: [
+															0,
+															containerSize?.height ||
+																0,
+														],
+														outputRange: [
+															0,
+															containerSize?.height ||
+																0,
+														],
+														extrapolate: 'clamp',
+													}
+												),
+											},
+										],
+									},
+								] }
+							>
+								<SVG
+									style={ styles.focalPointIcon }
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 30 30"
+								>
+									<Path
+										style={
+											styles.focalPointIconPathOutline
+										}
+										d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"
+									/>
+									<Path
+										style={ styles.focalPointIconPathFill }
+										d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z"
+									/>
+								</SVG>
+							</Animated.View>
 						</Animated.View>
 					</PanGestureHandler>
 				</TapGestureHandler>
-				<Animated.View
-					pointerEvents="none"
-					style={ [
-						styles.focalPointWrapper,
-						{
-							transform: [
-								{
-									translateX: _touchX.interpolate( {
-										inputRange: [
-											0,
-											containerSize?.width || 0,
-										],
-										outputRange: [
-											0,
-											containerSize?.width || 0,
-										],
-										extrapolate: 'clamp',
-									} ),
-								},
-								{
-									translateY: _touchY.interpolate( {
-										inputRange: [
-											0,
-											containerSize?.height || 0,
-										],
-										outputRange: [
-											0,
-											containerSize?.height || 0,
-										],
-										extrapolate: 'clamp',
-									} ),
-								},
-							],
-						},
-					] }
-				>
-					<SVG
-						style={ styles.focalPointIcon }
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 30 30"
-					>
-						<Path
-							style={ styles.focalPointIconPathOutline }
-							d="M15 1C7.3 1 1 7.3 1 15s6.3 14 14 14 14-6.3 14-14S22.7 1 15 1zm0 22c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"
-						/>
-						<Path
-							style={ styles.focalPointIconPathFill }
-							d="M15 3C8.4 3 3 8.4 3 15s5.4 12 12 12 12-5.4 12-12S21.6 3 15 3zm0 22C9.5 25 5 20.5 5 15S9.5 5 15 5s10 4.5 10 10-4.5 10-10 10z"
-						/>
-					</SVG>
-				</Animated.View>
 			</View>
 			<RangeControl
 				inputSuffix="%"
