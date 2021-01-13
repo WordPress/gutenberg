5b0aa15855acc02959213dab698ed0c4becaf6b3
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-13 13:56:12 +1100

    Add transformer option to RN bridge Metro config
    This is to match the exact solution given by StackOverflow to see if it
    helps.

--- packages/react-native-bridge/android/metro.config.js
+++ packages/react-native-bridge/android/metro.config.js
@@ -5,5 +5,13 @@
 // - Where this this suggested: https://stackoverflow.com/a/56027775/809944
 // - Example of failing build: https://app.circleci.com/pipelines/github/wordpress-mobile/gutenberg-mobile/9720/workflows/3688f362-78bf-4804-ad60-88762a30f6d3/jobs/51519
 module.exports = {
+	transformer: {
+		getTransformOptions: async () => ({
+			transform: {
+				experimentalImportSupport: false,
+				inlineRequires: false,
+			},
+		}),
+	},
 	maxWorkers: 1
 }
