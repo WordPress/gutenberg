f841d34a3a1a6e5a44bc71abb8c91d1d0c379f10
Shaun Andrews <shaun@automattic.com>
2021-01-12 21:53:59 -0500

    List View: Reduce whitespace and always show nested blocks (#28029)
    * Forcing nested blocks to always show, but there's gotta be a better way.
    
    * Reducing whitespace and simplifying the descenders.
    
    * Adjusting the selected state.
    
    * Moving the hasHierarchy check into the value of blocks.
    
    * This isn't needed. Leftover from some earlier explorations.
    
    * Overriding the hover state when selected.
    
    * Simplifying the descener-lines component and moving it inside the button.
    
    This makes the whole row clickable, regardless of indentation.
    
    * Renaming the descender-lines component to indentation.

--- packages/block-editor/src/components/block-navigation/block-select-button.js
+++ packages/block-editor/src/components/block-navigation/block-select-button.js
@@ -19,6 +19,7 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import BlockIcon from '../block-icon';
+import Indentation from './indentation';
 import useBlockDisplayInformation from '../use-block-display-information';
 import { getBlockPositionDescription } from './utils';
 
@@ -70,6 +71,7 @@ function BlockNavigationBlockSelectButton(
 				onDragEnd={ onDragEnd }
 				draggable={ draggable }
 			>
+				<Indentation level={ level } />
 				<BlockIcon icon={ blockInformation?.icon } showColors />
 				{ blockDisplayName }
 				{ isSelected && (
