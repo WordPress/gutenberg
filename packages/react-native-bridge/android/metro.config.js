83fd8cb76ed87e1efcf678a1b7547c170cbca6cf
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-13 12:47:55 +1100

    Add metro.config.js to React Native bridge package
    This is an attempt to make the CI build that should build and upload its
    AAR to Bintray not run out of memory and fail.

---
+++ packages/react-native-bridge/android/metro.config.js
@@ -0,0 +1,9 @@
+// This is so that when CI calls npm through Gradle, it won't fail because of
+// lack of memory.
+//
+// See:
+// - Where this this suggested: https://stackoverflow.com/a/56027775/809944
+// - Example of failing build: https://app.circleci.com/pipelines/github/wordpress-mobile/gutenberg-mobile/9720/workflows/3688f362-78bf-4804-ad60-88762a30f6d3/jobs/51519
+module.exports = {
+	maxWorkers: 2
+}
