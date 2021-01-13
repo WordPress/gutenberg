cfe936aeee74ec4204962ae84ea5d6badc17a4e4
Joel Dean <jdeanjj1000@gmail.com>
2021-01-13 12:34:31 -0500

    increased driver sleep on Android to reduce test flakiness. (#28000)

--- packages/react-native-editor/__device-tests__/gutenberg-editor-file-@canary.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-file-@canary.test.js
@@ -3,6 +3,7 @@
  */
 import { blockNames } from './pages/editor-page';
 import testData from './helpers/test-data';
+import { isAndroid } from './helpers/utils';
 
 describe( 'Gutenberg Editor File Block tests', () => {
 	it( 'should be able to add a file block', async () => {
@@ -15,7 +16,13 @@ describe( 'Gutenberg Editor File Block tests', () => {
 		const block = await editorPage.getFirstBlockVisible();
 
 		block.click();
-		await editorPage.driver.sleep( 1000 );
+
+		if ( isAndroid ) {
+			await editorPage.driver.sleep( 5000 );
+		} else {
+			await editorPage.driver.sleep( 1000 );
+		}
+
 		await editorPage.chooseMediaLibrary();
 
 		const html = await editorPage.getHtmlContent();
