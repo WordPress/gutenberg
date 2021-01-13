9f148d95634052a7fb37cceb478e7095562ba796
Glen Davies <glen.davies@a8c.com>
2021-01-13 13:50:48 +1300

    Update Gallery to nested image blocks

---
+++ packages/block-library/src/gallery/use-image-sizes.js
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import { get, reduce, map, filter, some } from 'lodash';
+
+export default function useImageSizes( images, isSelected, getSettings ) {
+	const { imageSizes } = getSettings();
+
+	let resizedImages = {};
+
+	if ( isSelected ) {
+		resizedImages = reduce(
+			images,
+			( currentResizedImages, img ) => {
+				if ( ! img.id ) {
+					return currentResizedImages;
+				}
+				const image = img.imageData;
+				const sizes = reduce(
+					imageSizes,
+					( currentSizes, size ) => {
+						const defaultUrl = get( image, [
+							'sizes',
+							size.slug,
+							'url',
+						] );
+						const mediaDetailsUrl = get( image, [
+							'media_details',
+							'sizes',
+							size.slug,
+							'source_url',
+						] );
+						return {
+							...currentSizes,
+							[ size.slug ]: defaultUrl || mediaDetailsUrl,
+						};
+					},
+					{}
+				);
+				return {
+					...currentResizedImages,
+					[ parseInt( img.id, 10 ) ]: sizes,
+				};
+			},
+			{}
+		);
+	}
+	return map(
+		filter( imageSizes, ( { slug } ) =>
+			some( resizedImages, ( sizes ) => sizes[ slug ] )
+		),
+		( { name, slug } ) => ( { value: slug, label: name } )
+	);
+}
