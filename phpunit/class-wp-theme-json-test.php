c7332d514e4afcc4967ade22c588a5b97a10ee10
André <andres.maneiro@automattic.com>
2021-01-15 13:53:19 +0100

    Add failing test to demonstrate bug

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -249,7 +249,7 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 			'settings'         => array(
 				'global' => array(
 					'color' => array(
-						'custom'  => 'false',
+						'custom'  => false,
 						'palette' => array(
 							array(
 								'slug'  => 'red',
@@ -264,7 +264,7 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 				),
 				'core/paragraph' => array(
 					'color' => array(
-						'custom' => 'false',
+						'custom' => false,
 					),
 				),
 			),
@@ -273,20 +273,20 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 					'typography' => array(
 						'fontSize' => '12',
 					),
-				)
+				),
 			),
 		);
 
-		$add_new_context = array(
+		$add_new_block = array(
 			'settings' => array(
 				'core/list' => array(
 					'color' => array(
-						'custom' => 'false',
+						'custom' => false,
 					),
 				),
 			),
-			'styles'   => array(
-				'core/list' => array(
+			'styles' => array(
+				'core/list'   => array(
 					'typography' => array(
 						'fontSize' => '12',
 					),
@@ -302,7 +302,7 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 			'settings' => array(
 				'global' => array(
 					'color' => array(
-						'customGradient' => 'true',
+						'customGradient' => true,
 					),
 				),
 			),
@@ -312,7 +312,7 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 			'settings' => array(
 				'global' => array(
 					'color' => array(
-						'custom' => 'true',
+						'custom' => true,
 					),
 				),
 			),
@@ -321,11 +321,10 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 		$add_styles = array(
 			'styles' => array(
 				'core/paragraph' => array(
-					'typography' => array(
-						'fontSize' => '12',
-					),
-					'color'      => array(
-						'link' => 'pink',
+					'spacing' => array(
+						'padding' => array(
+							'top' => '12px'
+						)
 					),
 				),
 			),
@@ -334,8 +333,10 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 		$add_key_in_styles = array(
 			'styles' => array(
 				'core/paragraph' => array(
-					'typography' => array(
-						'lineHeight' => '12',
+					'spacing' => array(
+						'padding' => array(
+							'bottom' => '12px',
+						),
 					),
 				),
 			),
@@ -571,128 +572,434 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 								),
 							),
 						),
-						'spacing' => array(
-							'customPadding' => false,
-						),
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
-		$expected = array(
-			'settings' => array(
-				'global' => array(
-					'color' => array(
-						'palette' => array(
-							array(
-								'name'  => 'Red',
-								'slug'  => 'red',
-								'color' => '#ff0000',
-							),
-							array(
-								'name'  => 'Green',
-								'slug'  => 'green',
-								'color' => '#00ff00',
-							),
-							array(
-								'name'  => 'Blue',
-								'slug'  => 'blue',
-								'color' => '#0000ff',
-							),
-						),
 					),
 				),
 			),
 		);
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
 
-	function test_remove_insecure_properties_removes_unsafe_preset_settings() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'settings' => array(
-					'global' => array(
-						'color' => array(
-							'palette' => array(
-								array(
-									'name'  => 'Red/><b>ok</ok>',
-									'slug'  => 'red',
-									'color' => '#ff0000',
-								),
-								array(
-									'name'  => 'Green',
-									'slug'  => 'a" attr',
-									'color' => '#00ff00',
-								),
-								array(
-									'name'  => 'Blue',
-									'slug'  => 'blue',
-									'color' => 'var(--custom-v1)',
-								),
-								array(
-									'name'  => 'Pink',
-									'slug'  => 'pink',
-									'color' => '#FFC0CB',
-								),
-							),
-						),
-						'typography' => array(
-							'fontFamilies' => array(
-								array(
-									'name'       => 'Helvetica Arial/><b>test</b>',
-									'slug'       => 'helvetica-arial',
-									'fontFamily' => 'Helvetica Neue, Helvetica, Arial, sans-serif',
-								),
-								array(
-									'name'       => 'Geneva',
-									'slug'       => 'geneva#asa',
-									'fontFamily' => 'Geneva, Tahoma, Verdana, sans-serif',
-								),
-								array(
-									'name'       => 'Cambria',
-									'slug'       => 'cambria',
-									'fontFamily' => 'Cambria, Georgia, serif',
-								),
-								array(
-									'name'       => 'Helvetica Arial',
-									'slug'       => 'helvetica-arial',
-									'fontFamily' => 'var(--custom-var-1)',
-								),
-							),
-						),
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
 		$expected = array(
 			'settings' => array(
-				'global' => array(
-					'color' => array(
-						'palette' => array(
+				'global'         => array(
+					'color'      => array(
+						'custom'         => true,
+						'customGradient' => true,
+						'palette'        => array(
 							array(
-								'name'  => 'Pink',
-								'slug'  => 'pink',
-								'color' => '#FFC0CB',
+								'slug'  => 'color',
+								'color' => 'color',
+							),
+						),
+						'gradients'      => array(
+							array(
+								'slug'     => 'gradient',
+								'gradient' => 'gradient',
 							),
 						),
 					),
 					'typography' => array(
+						'fontSizes'    => array(
+							array(
+								'slug' => 'fontSize',
+								'size' => 'fontSize',
+							),
+						),
 						'fontFamilies' => array(
 							array(
-								'name'       => 'Cambria',
-								'slug'       => 'cambria',
-								'fontFamily' => 'Cambria, Georgia, serif',
+								'slug'       => 'fontFamily',
+								'fontFamily' => 'fontFamily',
 							),
 						),
 					),
 				),
+				'core/paragraph' => array(
+					'color' => array(
+						'custom' => false,
+					),
+				),
+				'core/list'      => array(
+					'color' => array(
+						'custom' => false,
+					),
+				),
+			),
+			'styles'   => array(
+				'global' => array(
+					'typography' => array(
+						'fontSize' => '12',
+					),
+				),
+				'core/paragraph' => array(
+					'spacing' => array(
+						'padding' => array(
+							'top' => '12px',
+							'bottom' => '12px',
+						),
+					),
+				),
+				'core/list'   => array(
+					'typography' => array(
+						'fontSize' => '12',
+					),
+					'color'      => array(
+						'link'       => 'pink',
+						'background' => 'brown',
+					),
+				),
 			),
 		);
+
+		$theme_json = new WP_Theme_JSON( $initial );
+		$theme_json->merge( new WP_Theme_JSON( $add_new_block ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_key_in_settings ) );
+		$theme_json->merge( new WP_Theme_JSON( $update_key_in_settings ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_styles ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_key_in_styles ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_invalid_context ) );
+		$theme_json->merge( new WP_Theme_JSON( $update_presets ) );
+		$result = $theme_json->get_raw_data();
+
 		$this->assertEqualSetsWithIndex( $expected, $result );
 	}
+
+	// function test_remove_insecure_properties_removes_invalid_contexts() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'styles' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'background' => 'green',
+	// 						'text'       => 'var:preset|color|dark-gray',
+	// 					),
+	// 				),
+	// 				'.my-class' => array(
+	// 					'color' => array(
+	// 						'background' => 'green',
+	// 						'text'       => 'var:preset|color|dark-gray',
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'styles' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'background' => 'green',
+	// 					'text'       => 'var:preset|color|dark-gray',
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_invalid_properties() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'styles' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'gradient' => 'linear-gradient(55deg,rgba(6,147,227,1) 0%,rgb(84,177,218) 54%,rgb(155,81,224) 100%)',
+	// 						'text'       => 'var:preset|color|dark-gray',
+	// 					),
+	// 				),
+	// 				'invalid'   => array(
+	// 					'background' => 'green',
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'styles' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'gradient' => 'linear-gradient(55deg,rgba(6,147,227,1) 0%,rgb(84,177,218) 54%,rgb(155,81,224) 100%)',
+	// 					'text'       => 'var:preset|color|dark-gray',
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_unsafe_properties() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'styles' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'gradient' => 'url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+PHNjcmlwdD5hbGVydCgnb2snKTwvc2NyaXB0PjxsaW5lYXJHcmFkaWVudCBpZD0nZ3JhZGllbnQnPjxzdG9wIG9mZnNldD0nMTAlJyBzdG9wLWNvbG9yPScjRjAwJy8+PHN0b3Agb2Zmc2V0PSc5MCUnIHN0b3AtY29sb3I9JyNmY2MnLz4gPC9saW5lYXJHcmFkaWVudD48cmVjdCBmaWxsPSd1cmwoI2dyYWRpZW50KScgeD0nMCcgeT0nMCcgd2lkdGg9JzEwMCUnIGhlaWdodD0nMTAwJScvPjwvc3ZnPg==\')',
+	// 						'text'       => 'var:preset|color|dark-gray',
+	// 					),
+	// 				),
+	// 				'invalid'   => array(
+	// 					'background' => 'green',
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'styles' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'text'       => 'var:preset|color|dark-gray',
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_properties_when_not_allowed_in_a_context() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'styles' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'text'       => 'var:preset|color|dark-gray',
+	// 					),
+	// 					'spacing' => array(
+	// 						'padding' => array(
+	// 							'top' => '1px',
+	// 							'right' => '1px',
+	// 							'bottom' => '1px',
+	// 							'left' => '1px',
+	// 						),
+	// 					),
+	// 				),
+	// 				'invalid'   => array(
+	// 					'background' => 'green',
+	// 				),
+	// 				'core/group' => array(
+	// 					'spacing' => array(
+	// 						'padding' => array(
+	// 							'top' => '1px',
+	// 							'right' => '1px',
+	// 							'bottom' => '1px',
+	// 							'left' => '1px',
+	// 						),
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'styles' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'text'       => 'var:preset|color|dark-gray',
+	// 				),
+	// 			),
+	// 			'core/group' => array(
+	// 				'spacing' => array(
+	// 					'padding' => array(
+	// 						'top' => '1px',
+	// 						'right' => '1px',
+	// 						'bottom' => '1px',
+	// 						'left' => '1px',
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_unsafe_sub_properties() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'styles' => array(
+	// 				'core/group' => array(
+	// 					'spacing' => array(
+	// 						'padding' => array(
+	// 							'top' => '1px',
+	// 							'right' => '1px',
+	// 							'bottom' => 'var(--unsafe-var-y)',
+	// 							'left' => '1px',
+	// 						),
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'styles' => array(
+	// 			'core/group' => array(
+	// 				'spacing' => array(
+	// 					'padding' => array(
+	// 						'top' => '1px',
+	// 						'right' => '1px',
+	// 						'left' => '1px',
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_non_preset_settings() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'settings' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'custom'   => true,
+	// 						'palette' => array(
+	// 							array(
+	// 								'name'  => 'Red',
+	// 								'slug'  => 'red',
+	// 								'color' => '#ff0000',
+	// 							),
+	// 							array(
+	// 								'name'  => 'Green',
+	// 								'slug'  => 'green',
+	// 								'color' => '#00ff00',
+	// 							),
+	// 							array(
+	// 								'name'  => 'Blue',
+	// 								'slug'  => 'blue',
+	// 								'color' => '#0000ff',
+	// 							),
+	// 						),
+	// 					),
+	// 					'spacing' => array(
+	// 						'customPadding' => false,
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'settings' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'palette' => array(
+	// 						array(
+	// 							'name'  => 'Red',
+	// 							'slug'  => 'red',
+	// 							'color' => '#ff0000',
+	// 						),
+	// 						array(
+	// 							'name'  => 'Green',
+	// 							'slug'  => 'green',
+	// 							'color' => '#00ff00',
+	// 						),
+	// 						array(
+	// 							'name'  => 'Blue',
+	// 							'slug'  => 'blue',
+	// 							'color' => '#0000ff',
+	// 						),
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
+
+	// function test_remove_insecure_properties_removes_unsafe_preset_settings() {
+	// 	$theme_json = new WP_Theme_JSON(
+	// 		array(
+	// 			'settings' => array(
+	// 				'global' => array(
+	// 					'color' => array(
+	// 						'palette' => array(
+	// 							array(
+	// 								'name'  => 'Red/><b>ok</ok>',
+	// 								'slug'  => 'red',
+	// 								'color' => '#ff0000',
+	// 							),
+	// 							array(
+	// 								'name'  => 'Green',
+	// 								'slug'  => 'a" attr',
+	// 								'color' => '#00ff00',
+	// 							),
+	// 							array(
+	// 								'name'  => 'Blue',
+	// 								'slug'  => 'blue',
+	// 								'color' => 'var(--custom-v1)',
+	// 							),
+	// 							array(
+	// 								'name'  => 'Pink',
+	// 								'slug'  => 'pink',
+	// 								'color' => '#FFC0CB',
+	// 							),
+	// 						),
+	// 					),
+	// 					'typography' => array(
+	// 						'fontFamilies' => array(
+	// 							array(
+	// 								'name'       => 'Helvetica Arial/><b>test</b>',
+	// 								'slug'       => 'helvetica-arial',
+	// 								'fontFamily' => 'Helvetica Neue, Helvetica, Arial, sans-serif',
+	// 							),
+	// 							array(
+	// 								'name'       => 'Geneva',
+	// 								'slug'       => 'geneva#asa',
+	// 								'fontFamily' => 'Geneva, Tahoma, Verdana, sans-serif',
+	// 							),
+	// 							array(
+	// 								'name'       => 'Cambria',
+	// 								'slug'       => 'cambria',
+	// 								'fontFamily' => 'Cambria, Georgia, serif',
+	// 							),
+	// 							array(
+	// 								'name'       => 'Helvetica Arial',
+	// 								'slug'       => 'helvetica-arial',
+	// 								'fontFamily' => 'var(--custom-var-1)',
+	// 							),
+	// 						),
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 		true
+	// 	);
+	// 	$theme_json->remove_insecure_properties();
+	// 	$result   = $theme_json->get_raw_data();
+	// 	$expected = array(
+	// 		'settings' => array(
+	// 			'global' => array(
+	// 				'color' => array(
+	// 					'palette' => array(
+	// 						array(
+	// 							'name'  => 'Pink',
+	// 							'slug'  => 'pink',
+	// 							'color' => '#FFC0CB',
+	// 						),
+	// 					),
+	// 				),
+	// 				'typography' => array(
+	// 					'fontFamilies' => array(
+	// 						array(
+	// 							'name'       => 'Cambria',
+	// 							'slug'       => 'cambria',
+	// 							'fontFamily' => 'Cambria, Georgia, serif',
+	// 						),
+	// 					),
+	// 				),
+	// 			),
+	// 		),
+	// 	);
+	// 	$this->assertEqualSetsWithIndex( $expected, $result );
+	// }
 }
