ed62a42ed7e31919bc40bb4d3ddd8ea411bed21a
lukewalczak <lukasz.walczak.pwr@gmail.com>
2021-01-15 11:06:32 +0100

    Performance improvements in File related controls

--- packages/block-library/src/file/edit.native.js
+++ packages/block-library/src/file/edit.native.js
@@ -28,7 +28,7 @@ import {
 	ToolbarGroup,
 	PanelBody,
 	ToggleControl,
-	BottomSheet,
+	TextControl,
 	SelectControl,
 	Icon,
 } from '@wordpress/components';
@@ -264,12 +264,15 @@ export class FileEdit extends Component {
 		);
 
 		const isCopyUrlDisabled = isUploadFailed || isUploadInProgress;
-		const dimmedStyle = isCopyUrlDisabled && styles.disabledButton;
-		const finalButtonStyle = Object.assign(
-			{},
-			actionButtonStyle,
-			dimmedStyle
+
+		const dimmedActionButtonStyle = this.props.getStylesFromColorScheme(
+			styles.dimmedActionButton,
+			styles.dimmedActionButtonDark
 		);
+
+		const finalButtonStyle = isCopyUrlDisabled
+			? dimmedActionButtonStyle
+			: actionButtonStyle;
 
 		return (
 			<InspectorControls>
@@ -298,7 +301,7 @@ export class FileEdit extends Component {
 							onChange={ this.onChangeDownloadButtonVisibility }
 						/>
 					) }
-					<BottomSheet.Cell
+					<TextControl
 						disabled={ isCopyUrlDisabled }
 						label={
 							this.state.isUrlCopied
@@ -456,12 +459,13 @@ export class FileEdit extends Component {
 										openMediaOptions
 									) }
 								{ getMediaOptions() }
-								{ this.getInspectorControls(
-									attributes,
-									media,
-									isUploadInProgress,
-									isUploadFailed
-								) }
+								{ isSelected &&
+									this.getInspectorControls(
+										attributes,
+										media,
+										isUploadInProgress,
+										isUploadFailed
+									) }
 								<View style={ styles.container }>
 									<RichText
 										withoutInteractiveFormatting
