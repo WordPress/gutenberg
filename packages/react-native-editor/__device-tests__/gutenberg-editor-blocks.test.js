fb5b44570c10c6168e4451f322b84972ca9eb918
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-01-15 14:01:28 +0100

    Remove unneeded code after merging master

--- packages/react-native-editor/__device-tests__/gutenberg-editor-blocks.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-blocks.test.js
@@ -1,39 +1,11 @@
 /**
  * Internal dependencies
  */
-import EditorPage from './pages/editor-page';
-import { setupDriver, isLocalEnvironment, stopDriver } from './helpers/utils';
 import initialHtml from '../src/initial-html';
 
-jest.setTimeout( 1000000 );
-
 describe( 'Gutenberg Editor Blocks test', () => {
-	let driver;
-	let editorPage;
-	let allPassed = true;
-	const lastBlockAccessibilityLabel = 'Heading with line-height set';
-
-	// Use reporter for setting status for saucelabs Job
-	if ( ! isLocalEnvironment() ) {
-		const reporter = {
-			specDone: async ( result ) => {
-				allPassed = allPassed && result.status !== 'failed';
-			},
-		};
-
-		jasmine.getEnv().addReporter( reporter );
-	}
-
-	beforeAll( async () => {
-		driver = await setupDriver();
-		editorPage = new EditorPage( driver );
-	} );
-
-	it( 'should be able to see visual editor', async () => {
-		await expect( editorPage.getBlockList() ).resolves.toBe( true );
-	} );
-
 	it( 'should be able to create a post with all blocks and scroll to the last one', async () => {
+		const lastBlockAccessibilityLabel = 'Heading with line-height set';
 		await editorPage.setHtmlContent( initialHtml );
 
 		const lastBlockElement = await editorPage.scrollAndReturnElement(
@@ -41,12 +13,5 @@ describe( 'Gutenberg Editor Blocks test', () => {
 		);
 
 		expect( lastBlockElement ).toBeTruthy();
-	} );
-
-	afterAll( async () => {
-		if ( ! isLocalEnvironment() ) {
-			driver.sauceJobStatus( allPassed );
-		}
-		await stopDriver( driver );
 	} );
 } );
