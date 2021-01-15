b8ccdedac12a803a8a2bfe6e3ff8b8ecb5874e5a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-01-15 08:02:52 +0100

    Create Block: Enhancements to the tutorial template

--- packages/create-block-tutorial-template/index.js
+++ packages/create-block-tutorial-template/index.js
@@ -1,13 +1,18 @@
+/**
+ * External dependencies
+ */
+const { join } = require( 'path' );
+
 module.exports = {
 	defaultValues: {
 		slug: 'gutenpride',
-		namespace: 'create-block-tutorial-template',
+		namespace: 'create-block-tutorial',
 		title: 'Gutenpride',
 		description:
 			'A Gutenberg block to show your pride! This block enables you to type text and style it with the color font Gilbert from Type with Pride',
 
-		dashicon: 'smiley',
+		dashicon: 'flag',
 	},
-	templatesPath: __dirname,
-	assetsPath: require( 'path' ).join( __dirname, 'assets' ),
+	templatesPath: join( __dirname, 'templates' ),
+	assetsPath: join( __dirname, 'assets' ),
 };
