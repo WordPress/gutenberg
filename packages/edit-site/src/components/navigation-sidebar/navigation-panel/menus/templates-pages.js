09e4d386bb1ae8120e5e64be310ac2ce85e9694e
David Szabo <david.szabo97@gmail.com>
2021-01-18 14:45:15 +0100

    Remove general templates from pages

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/menus/templates-pages.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/menus/templates-pages.js
@@ -6,11 +6,8 @@ import { map } from 'lodash';
 /**
  * WordPress dependencies
  */
-import {
-	__experimentalNavigationGroup as NavigationGroup,
-	__experimentalNavigationMenu as NavigationMenu,
-} from '@wordpress/components';
-import { __, _x } from '@wordpress/i18n';
+import { __experimentalNavigationMenu as NavigationMenu } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -19,7 +16,6 @@ import TemplateNavigationItem from '../template-navigation-item';
 import { MENU_TEMPLATES, MENU_TEMPLATES_PAGES } from '../constants';
 
 export default function TemplatesPagesMenu( { templates } ) {
-	const defaultTemplate = templates?.find( ( { slug } ) => slug === 'page' );
 	const specificTemplates =
 		templates?.filter( ( { slug } ) => slug.startsWith( 'page-' ) ) ?? [];
 
@@ -28,22 +24,14 @@ export default function TemplatesPagesMenu( { templates } ) {
 			menu={ MENU_TEMPLATES_PAGES }
 			title={ __( 'Pages' ) }
 			parentMenu={ MENU_TEMPLATES }
-			isEmpty={ ! defaultTemplate && specificTemplates.length === 0 }
+			isEmpty={ specificTemplates.length === 0 }
 		>
-			<NavigationGroup title={ _x( 'Specific', 'specific templates' ) }>
-				{ map( specificTemplates, ( template ) => (
-					<TemplateNavigationItem
-						item={ template }
-						key={ `wp_template-${ template.id }` }
-					/>
-				) ) }
-			</NavigationGroup>
-
-			{ defaultTemplate && (
-				<NavigationGroup title={ _x( 'General', 'general templates' ) }>
-					<TemplateNavigationItem item={ defaultTemplate } />
-				</NavigationGroup>
-			) }
+			{ map( specificTemplates, ( template ) => (
+				<TemplateNavigationItem
+					item={ template }
+					key={ `wp_template-${ template.id }` }
+				/>
+			) ) }
 		</NavigationMenu>
 	);
 }
