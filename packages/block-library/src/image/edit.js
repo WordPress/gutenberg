3a38c9d259667699cac60d1c7297375e648f3c82
Glen Davies <glen.davies@a8c.com>
2021-01-21 08:47:38 +1300

    Removed unused prop

--- packages/block-library/src/image/edit.js
+++ packages/block-library/src/image/edit.js
@@ -92,7 +92,6 @@ export function ImageEdit( {
 		width,
 		height,
 		sizeSlug,
-		inhertedAttributes,
 	} = attributes;
 	const [ tempUrl, setTempUrl ] = useState();
 	const altRef = useRef();
@@ -346,7 +345,6 @@ export function ImageEdit( {
 			onUploadError={ onUploadError }
 			containerRef={ ref }
 			context={ context }
-			inhertedAttributes={ inhertedAttributes }
 		/>
 	);
 
