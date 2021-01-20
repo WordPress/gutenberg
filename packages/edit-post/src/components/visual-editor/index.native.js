e1efcce3469f2330bee17843cea2e1d837f5bdc3
lukewalczak <lukasz.walczak.pwr@gmail.com>
2021-01-20 12:29:39 +0100

    Correct autoscroll on ios to be enabled the keyboard is going to appear

--- packages/edit-post/src/components/visual-editor/index.native.js
+++ packages/edit-post/src/components/visual-editor/index.native.js
@@ -3,7 +3,10 @@
  */
 import { Component } from '@wordpress/element';
 import { BlockList } from '@wordpress/block-editor';
-
+/**
+ * External dependencies
+ */
+import { Keyboard } from 'react-native';
 /**
  * Internal dependencies
  */
@@ -13,6 +16,36 @@ export default class VisualEditor extends Component {
 	constructor( props ) {
 		super( props );
 		this.renderHeader = this.renderHeader.bind( this );
+		this.keyboardWillShow = this.keyboardWillShow.bind( this );
+		this.keyboardWillHide = this.keyboardWillHide.bind( this );
+
+		this.state = {
+			isKeyboardOpen: false,
+		};
+	}
+
+	componentWillMount() {
+		this.keyboardWillShowListener = Keyboard.addListener(
+			'keyboardWillShow',
+			this.keyboardWillShow
+		);
+		this.keyboardWillHideListener = Keyboard.addListener(
+			'keyboardWillHide',
+			this.keyboardWillHide
+		);
+	}
+
+	componentWillUnmount() {
+		this.keyboardWillShowListener.remove();
+		this.keyboardWillHideListener.remove();
+	}
+
+	keyboardWillShow() {
+		this.setState( { isKeyboardOpen: true } );
+	}
+
+	keyboardWillHide() {
+		this.setState( { isKeyboardOpen: false } );
 	}
 	renderHeader() {
 		const { setTitleRef } = this.props;
@@ -24,7 +57,7 @@ export default class VisualEditor extends Component {
 			<BlockList
 				header={ this.renderHeader }
 				safeAreaBottomInset={ safeAreaBottomInset }
-				autoScroll={ true }
+				autoScroll={ this.state.isKeyboardOpen }
 			/>
 		);
 	}
