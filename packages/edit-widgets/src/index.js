3bbe9f1a493e7a1d7851547cba2732f95e5d6395
Kai Hao <kevin830726@gmail.com>
2021-01-21 10:25:36 +0800

    Don't pass __internal_widget_id to block types

--- packages/edit-widgets/src/index.js
+++ packages/edit-widgets/src/index.js
@@ -11,7 +11,6 @@ import {
 	__experimentalGetCoreBlocks,
 	__experimentalRegisterExperimentalCoreBlocks,
 } from '@wordpress/block-library';
-import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
@@ -22,18 +21,6 @@ import { create as createLegacyWidget } from './blocks/legacy-widget';
 import * as widgetArea from './blocks/widget-area';
 import Layout from './components/layout';
 
-function __experimentalAddWidgetIdAttribute( settings ) {
-	return {
-		...settings,
-		attributes: {
-			...( settings.attributes || {} ),
-			__internal_widget_id: {
-				type: 'string',
-			},
-		},
-	};
-}
-
 /**
  * Initializes the block editor in the widgets screen.
  *
@@ -41,12 +28,6 @@ function __experimentalAddWidgetIdAttribute( settings ) {
  * @param {Object} settings Block editor settings.
  */
 export function initialize( id, settings ) {
-	addFilter(
-		'blocks.registerBlockType',
-		'gutenberg/edit-widgets/add-widget-id',
-		__experimentalAddWidgetIdAttribute
-	);
-
 	const coreBlocks = __experimentalGetCoreBlocks().filter(
 		( block ) => ! [ 'core/more' ].includes( block.name )
 	);
