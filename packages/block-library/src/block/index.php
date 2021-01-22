bee83e82fabaace0fa8881a95dc47872bd49b62b
Miguel Fonseca <miguelcsf@gmail.com>
2021-01-22 14:17:29 +0000

    Honor WP_DEBUG_DISPLAY before displaying failure point

--- packages/block-library/src/block/index.php
+++ packages/block-library/src/block/index.php
@@ -36,7 +36,12 @@ function render_block_core_block( $attributes ) {
 			);
 		}
 
-		return '[block rendering halted]';
+		// WP_DEBUG_DISPLAY must only be honored when WP_DEBUG. This precedent
+		// is set in `wp_debug_mode()`.
+		$is_debug = defined( 'WP_DEBUG' ) && WP_DEBUG &&
+			defined( 'WP_DEBUG_DISPLAY' ) && WP_DEBUG_DISPLAY;
+
+		return $is_debug ? '[block rendering halted]' : '';
 	}
 
 	$seen_refs[] = $attributes['ref'];
