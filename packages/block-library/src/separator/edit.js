11f43550cb45d86e5e6dd7cd39236f9c5677eb32
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-01-22 21:12:03 +1000

    Improve separator height control
    This change keeps block selection styling and visual cues without losing the normal styling of the `hr`. It also allows the ResizableBox control to naturally represent the separator's height.
    
    The downside is it is a little hacky to get around themes styling separators using bottom borders, background colors and pseudo elements.

--- packages/block-library/src/separator/edit.js
+++ packages/block-library/src/separator/edit.js
@@ -7,48 +7,60 @@ import { clamp } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { ResizableBox } from '@wordpress/components';
+import { HorizontalRule, ResizableBox } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { withColors, useBlockProps } from '@wordpress/block-editor';
 /**
  * Internal dependencies
  */
 import SeparatorSettings from './separator-settings';
 
-const MIN_HEIGHT = 10;
+const MIN_HEIGHT = 20;
 const MIN_DOTS_HEIGHT = 30;
 const MAX_HEIGHT = 500;
 
 function SeparatorEdit( props ) {
-	const {
-		attributes,
-		setAttributes,
-		className,
-		color,
-		setColor,
-		isSelected,
-	} = props;
+	const { attributes, setAttributes, color, setColor, isSelected } = props;
 	const { height } = attributes;
 	const hasDotsStyle = attributes.className?.indexOf( 'is-style-dots' ) >= 0;
+	const [ isResizing, setIsResizing ] = useState( false );
+
+	const onResizeStart = () => {
+		setIsResizing( true );
+	};
 
 	const onResizeStop = ( _event, _direction, elt ) => {
 		const newHeight = Math.round( elt.clientHeight );
 		setAttributes( { height: clamp( newHeight, MIN_HEIGHT, MAX_HEIGHT ) } );
+		setIsResizing( false );
 	};
 
-	const blockProps = useBlockProps( {
-		className: classnames( className, {
-			'has-background': color.color,
-			[ color.class ]: color.class,
-		} ),
-		style: {
-			backgroundColor: color.color,
-			color: color.color,
-		},
-	} );
+	const blockProps = useBlockProps();
 
+	// The block's className and styles are moved to the inner <hr> to retain
+	// the different styling approaches between themes. The use of bottom
+	// borders and background colors prevents using padding internally on the
+	// edit component. Adjusting margins leads to losing visual indicators for
+	// block selection.
 	return (
 		<>
-			<div { ...blockProps }>
+			<div
+				{ ...blockProps }
+				className={ blockProps.className.replace(
+					'wp-block-separator',
+					''
+				) }
+			>
+				<HorizontalRule
+					className={ classnames( blockProps.className, {
+						'has-background': color.color,
+						[ color.class ]: color.class,
+					} ) }
+					style={ {
+						backgroundColor: color.color,
+						color: color.color,
+					} }
+				/>
 				<ResizableBox
 					className={ classnames(
 						'block-library-separator__resize-container',
@@ -68,8 +80,15 @@ function SeparatorEdit( props ) {
 						topLeft: false,
 					} }
 					minHeight={ hasDotsStyle ? MIN_DOTS_HEIGHT : MIN_HEIGHT }
+					onResizeStart={ onResizeStart }
 					onResizeStop={ onResizeStop }
 					showHandle={ isSelected }
+					__experimentalShowTooltip={ true }
+					__experimentalTooltipProps={ {
+						axis: 'y',
+						position: 'bottom',
+						isVisible: isResizing,
+					} }
 				/>
 			</div>
 			<SeparatorSettings color={ color } setColor={ setColor } />
