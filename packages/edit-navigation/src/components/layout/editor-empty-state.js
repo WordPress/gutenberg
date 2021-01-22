4b7f7dedc95d60b6d52812d6ee0624f4e3865ee7
Daniel Richards <daniel.richards@automattic.com>
2021-01-22 17:54:41 +0800

    Add loading spinner to empty state

--- packages/edit-navigation/src/components/layout/editor-empty-state.js
+++ packages/edit-navigation/src/components/layout/editor-empty-state.js
@@ -1,8 +1,17 @@
 /**
  * WordPress dependencies
  */
+import { Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-export default function EditorEmptyState() {
-	return __( 'No menus have been created. Add a new one to get started.' );
+export default function EditorEmptyState( { isPending } ) {
+	return (
+		<div>
+			{ isPending && <Spinner /> }
+			{ ! isPending &&
+				__(
+					'No menus have been created. Add a new one to get started.'
+				) }
+		</div>
+	);
 }
