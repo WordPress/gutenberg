a9ed7ced5e10ec5ff54d792ac18150cd1c5139b8
Matias Ventura <mv@matiasventura.com>
2021-01-22 09:11:36 +0100

    Cover: improve various block states. (#28384)
    - Show alignment matrix and full height in placeholder state but disabled.
    - Allow adding media from the toolbar and clarify when only background color is set.

--- packages/block-editor/src/components/block-alignment-matrix-toolbar/index.js
+++ packages/block-editor/src/components/block-alignment-matrix-toolbar/index.js
@@ -19,6 +19,7 @@ export function BlockAlignmentMatrixToolbar( props ) {
 		label = __( 'Change matrix alignment' ),
 		onChange = noop,
 		value = 'center',
+		isDisabled,
 	} = props;
 
 	const icon = <AlignmentMatrixControl.Icon value={ value } />;
@@ -50,6 +51,7 @@ export function BlockAlignmentMatrixToolbar( props ) {
 							label={ label }
 							icon={ icon }
 							showTooltip
+							disabled={ isDisabled }
 						/>
 					</ToolbarGroup>
 				);
