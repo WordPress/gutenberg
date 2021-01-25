WordPress/gutenberg

commit e100af0d72bb084f73e8dbe6fc224b2b44365c29

Don't add default-custom-properties styles to all compiled files (#28395)
* Don't add default-custom-properties styles to all compiled files

* Additional check for block-library

* Include block-library/src/editor.scss

* revert last commit

bin/packages/build-worker.js
@@ -110,9 +110,10 @@ const BUILD_TASK_BY_EXTENSION = {
 				]
 					// Editor styles should be excluded from the default CSS vars output.
 					.concat(
-						file.includes( 'editor-styles.scss' )
-							? []
-							: [ 'default-custom-properties' ]
+						file.includes( 'common.scss' ) ||
+							! file.includes( 'block-library' )
+							? [ 'default-custom-properties' ]
+							: []
 					)
 					.map( ( imported ) => `@import "${ imported }";` )
 					.join( ' ' ) + contents,

commit cc5942e538bb7eb620169b82b58abdd168467383

Fix regression caused by #28395
See comment on https://github.com/WordPress/gutenberg/pull/28395#issuecomment-766804476

bin/packages/build-worker.js
@@ -111,7 +111,8 @@ const BUILD_TASK_BY_EXTENSION = {
 					// Editor styles should be excluded from the default CSS vars output.
 					.concat(
 						file.includes( 'common.scss' ) ||
-							! file.includes( 'block-library' )
+							( ! file.includes( 'block-library' ) &&
+								! file.includes( 'editor-styles.scss' ) )
 							? [ 'default-custom-properties' ]
 							: []
 					)