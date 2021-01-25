b91836dbf2f412d3cbef02bcc0f4384d5d990647
lukewalczak <lukasz.walczak.pwr@gmail.com>
2021-01-25 15:27:44 +0100

    Refactor after review

--- packages/components/src/text-control/index.native.js
+++ packages/components/src/text-control/index.native.js
@@ -7,35 +7,33 @@ import { memo } from '@wordpress/element';
  */
 import Cell from '../mobile/bottom-sheet/cell';
 
-const TextControl = memo(
-	( {
-		label,
-		hideLabelFromVision,
-		value,
-		help,
-		className,
-		instanceId,
-		onChange,
-		type = 'text',
-		...props
-	} ) => {
-		const id = `inspector-text-control-${ instanceId }`;
+function TextControl( {
+	label,
+	hideLabelFromVision,
+	value,
+	help,
+	className,
+	instanceId,
+	onChange,
+	type = 'text',
+	...props
+} ) {
+	const id = `inspector-text-control-${ instanceId }`;
 
-		return (
-			<Cell
-				label={ label }
-				hideLabelFromVision={ hideLabelFromVision }
-				id={ id }
-				help={ help }
-				className={ className }
-				type={ type }
-				value={ value }
-				onChangeValue={ onChange }
-				aria-describedby={ !! help ? id + '__help' : undefined }
-				{ ...props }
-			/>
-		);
-	}
-);
+	return (
+		<Cell
+			label={ label }
+			hideLabelFromVision={ hideLabelFromVision }
+			id={ id }
+			help={ help }
+			className={ className }
+			type={ type }
+			value={ value }
+			onChangeValue={ onChange }
+			aria-describedby={ !! help ? id + '__help' : undefined }
+			{ ...props }
+		/>
+	);
+}
 
-export default TextControl;
+export default memo( TextControl );
