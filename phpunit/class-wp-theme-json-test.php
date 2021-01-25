7097e4330b2b740b3100ae0ccebc8024b7557a85
André <andres.maneiro@automattic.com>
2021-01-25 17:45:40 +0100

    Fix rebase issues

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -393,198 +393,121 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 			),
 		);
 
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
-
-	function test_remove_insecure_properties_removes_invalid_contexts() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'styles' => array(
-					'global' => array(
-						'color' => array(
-							'background' => 'green',
-							'text'       => 'var:preset|color|dark-gray',
-						),
-					),
-					'.my-class' => array(
-						'color' => array(
-							'background' => 'green',
-							'text'       => 'var:preset|color|dark-gray',
-						),
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
-		$expected = array(
-			'styles' => array(
+		$update_presets = array(
+			'settings' => array(
 				'global' => array(
-					'color' => array(
-						'background' => 'green',
-						'text'       => 'var:preset|color|dark-gray',
-					),
-				),
-			),
-		);
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
-
-	function test_remove_insecure_properties_removes_invalid_properties() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'styles' => array(
-					'global' => array(
-						'color' => array(
-							'gradient' => 'linear-gradient(55deg,rgba(6,147,227,1) 0%,rgb(84,177,218) 54%,rgb(155,81,224) 100%)',
-							'text'     => 'var:preset|color|dark-gray',
+					'color'      => array(
+						'palette'   => array(
+							array(
+								'slug'  => 'blue',
+								'color' => 'blue',
+							),
 						),
-					),
-					'invalid' => array(
-						'background' => 'green',
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
-		$expected = array(
-			'styles' => array(
-				'global' => array(
-					'color' => array(
-						'gradient' => 'linear-gradient(55deg,rgba(6,147,227,1) 0%,rgb(84,177,218) 54%,rgb(155,81,224) 100%)',
-						'text'     => 'var:preset|color|dark-gray',
-					),
-				),
-			),
-		);
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
-
-	function test_remove_insecure_properties_removes_unsafe_properties() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'styles' => array(
-					'global' => array(
-						'color' => array(
-							'gradient' => 'url(\'data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+PHNjcmlwdD5hbGVydCgnb2snKTwvc2NyaXB0PjxsaW5lYXJHcmFkaWVudCBpZD0nZ3JhZGllbnQnPjxzdG9wIG9mZnNldD0nMTAlJyBzdG9wLWNvbG9yPScjRjAwJy8+PHN0b3Agb2Zmc2V0PSc5MCUnIHN0b3AtY29sb3I9JyNmY2MnLz4gPC9saW5lYXJHcmFkaWVudD48cmVjdCBmaWxsPSd1cmwoI2dyYWRpZW50KScgeD0nMCcgeT0nMCcgd2lkdGg9JzEwMCUnIGhlaWdodD0nMTAwJScvPjwvc3ZnPg==\')',
-							'text'     => 'var:preset|color|dark-gray',
-						),
-					),
-					'invalid' => array(
-						'background' => 'green',
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
-		$expected = array(
-			'styles' => array(
-				'global' => array(
-					'color' => array(
-						'text' => 'var:preset|color|dark-gray',
-					),
-				),
-			),
-		);
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
-
-	function test_remove_insecure_properties_removes_properties_when_not_allowed_in_a_context() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'styles' => array(
-					'global' => array(
-						'color' => array(
-							'text'       => 'var:preset|color|dark-gray',
-						),
-						'spacing' => array(
-							'padding' => array(
-								'top'    => '1px',
-								'right'  => '1px',
-								'bottom' => '1px',
-								'left'   => '1px',
+						'gradients' => array(
+							array(
+								'slug'     => 'gradient',
+								'gradient' => 'gradient',
 							),
 						),
 					),
-					'invalid' => array(
-						'background' => 'green',
-					),
-					'core/group' => array(
-						'spacing' => array(
-							'padding' => array(
-								'top'    => '1px',
-								'right'  => '1px',
-								'bottom' => '1px',
-								'left'   => '1px',
+					'typography' => array(
+						'fontSizes'    => array(
+							array(
+								'slug' => 'fontSize',
+								'size' => 'fontSize',
+							),
+						),
+						'fontFamilies' => array(
+							array(
+								'slug'       => 'fontFamily',
+								'fontFamily' => 'fontFamily',
 							),
 						),
 					),
 				),
 			),
-			true
 		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
+
 		$expected = array(
-			'styles' => array(
-				'global' => array(
+			'settings' => array(
+				'global'         => array(
+					'color'      => array(
+						'custom'         => true,
+						'customGradient' => true,
+						'palette'        => array(
+							array(
+								'slug'  => 'blue',
+								'color' => 'blue',
+							),
+						),
+						'gradients'      => array(
+							array(
+								'slug'     => 'gradient',
+								'gradient' => 'gradient',
+							),
+						),
+					),
+					'typography' => array(
+						'fontSizes'    => array(
+							array(
+								'slug' => 'fontSize',
+								'size' => 'fontSize',
+							),
+						),
+						'fontFamilies' => array(
+							array(
+								'slug'       => 'fontFamily',
+								'fontFamily' => 'fontFamily',
+							),
+						),
+					),
+				),
+				'core/paragraph' => array(
 					'color' => array(
-						'text' => 'var:preset|color|dark-gray',
+						'custom' => false,
+					),
+				),
+				'core/list'      => array(
+					'color' => array(
+						'custom' => false,
+					),
+				),
+			),
+			'styles'   => array(
+				'global' => array(
+					'typography' => array(
+						'fontSize' => '12',
 					),
 				),
 				'core/group' => array(
 					'spacing' => array(
 						'padding' => array(
-							'top'    => '1px',
-							'right'  => '1px',
-							'bottom' => '1px',
-							'left'   => '1px',
+							'top' => '12px',
+							'bottom' => '12px',
 						),
+					),
+				),
+				'core/list'   => array(
+					'typography' => array(
+						'fontSize' => '12',
+					),
+					'color'      => array(
+						'background' => 'brown',
 					),
 				),
 			),
 		);
-		$this->assertEqualSetsWithIndex( $expected, $result );
-	}
 
-	function test_remove_insecure_properties_removes_unsafe_sub_properties() {
-		$theme_json = new WP_Theme_JSON(
-			array(
-				'styles' => array(
-					'core/group' => array(
-						'spacing' => array(
-							'padding' => array(
-								'top'    => '1px',
-								'right'  => '1px',
-								'bottom' => 'var(--unsafe-var-y)',
-								'left'   => '1px',
-							),
-						),
-					),
-				),
-			),
-			true
-		);
-		$theme_json->remove_insecure_properties();
-		$result   = $theme_json->get_raw_data();
-		$expected = array(
-			'styles' => array(
-				'core/group' => array(
-					'spacing' => array(
-						'padding' => array(
-							'top'   => '1px',
-							'right' => '1px',
-							'left'  => '1px',
-						),
-					),
-				),
-			),
-		);
+		$theme_json = new WP_Theme_JSON( $initial );
+		$theme_json->merge( new WP_Theme_JSON( $add_new_block ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_key_in_settings ) );
+		$theme_json->merge( new WP_Theme_JSON( $update_key_in_settings ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_styles ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_key_in_styles ) );
+		$theme_json->merge( new WP_Theme_JSON( $add_invalid_context ) );
+		$theme_json->merge( new WP_Theme_JSON( $update_presets ) );
+		$result = $theme_json->get_raw_data();
+
 		$this->assertEqualSetsWithIndex( $expected, $result );
 	}
 
