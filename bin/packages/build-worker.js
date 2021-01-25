9befa5cb959a4190590437212346b477ddcf87d6
Ari Stathopoulos <aristath@gmail.com>
2021-01-25 11:30:34 +0200

    revert last commit

--- bin/packages/build-worker.js
+++ bin/packages/build-worker.js
@@ -111,7 +111,6 @@ const BUILD_TASK_BY_EXTENSION = {
 					// Editor styles should be excluded from the default CSS vars output.
 					.concat(
 						file.includes( 'common.scss' ) ||
-							file.includes( 'block-library/src/editor.scss' ) ||
 							! file.includes( 'block-library' )
 							? [ 'default-custom-properties' ]
 							: []
