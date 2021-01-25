64a4fec113977e0fa1a81783adc5c649b277aa09
Glen Davies <glen.davies@a8c.com>
2021-01-25 13:33:39 +1300

    When dragging an image block into a gallery make sure we don't wipe any custom links

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -165,7 +165,14 @@ export default function Image( {
 		}
 	}, [ isSelected ] );
 
-	useParentAttributes( image, context, inheritedAttributes, setAttributes );
+	const currentAttributes = { linkDestination, linkTarget, href };
+	useParentAttributes(
+		image,
+		currentAttributes,
+		context,
+		inheritedAttributes,
+		setAttributes
+	);
 
 	// If an image is externally hosted, try to fetch the image data. This may
 	// fail if the image host doesn't allow CORS with the domain. If it works,
@@ -209,8 +216,8 @@ export default function Image( {
 
 	function onSetHref( props ) {
 		if (
-			inheritedAttributes.linkDestination ||
-			inheritedAttributes.linkTarget
+			inheritedAttributes?.linkDestination ||
+			inheritedAttributes?.linkTarget
 		) {
 			setAttributes( {
 				inheritedAttributes: {
@@ -251,7 +258,7 @@ export default function Image( {
 	}
 
 	function updateImage( newSizeSlug ) {
-		if ( inheritedAttributes.sizeSlug ) {
+		if ( inheritedAttributes?.sizeSlug ) {
 			setAttributes( {
 				inheritedAttributes: {
 					...inheritedAttributes,
