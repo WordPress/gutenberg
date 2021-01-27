fb95601d178c3253dbbc6b6a1e388ac87be6d581
hsing <hsingyuc.7@gmail.com>
2021-01-26 21:30:52 -0500

    Remove unused to property

--- packages/block-library/src/buttons/transforms.js
+++ packages/block-library/src/buttons/transforms.js
@@ -53,14 +53,6 @@ const transforms = {
 				),
 		},
 	],
-	to: [
-		{
-			type: 'block',
-			blocks: [ 'core/paragraph' ],
-			transform: ( attributes ) =>
-				createBlock( 'core/paragraph', attributes ),
-		},
-	],
 };
 
 export default transforms;
