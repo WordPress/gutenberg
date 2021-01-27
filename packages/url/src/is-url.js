3f46cf47a538c4f8e941b554f9e05c535bfb7d78
Steve Clay <steve@mrclay.org>
2021-01-27 17:44:56 -0500

    Fixes 24895: isURL uses a bit of heuristics

--- packages/url/src/is-url.js
+++ packages/url/src/is-url.js
@@ -15,11 +15,31 @@
  */
 export function isURL( url ) {
 	// A URL can be considered value if the `URL` constructor is able to parse
-	// it. The constructor throws an error for an invalid URL.
+	// it. The constructor throws an error for most invalid URLs.
 	try {
-		new URL( url );
+		const parsed = new URL( url );
+
+		// Special cases
+		if ( /^\s/.test( parsed.pathname ) ) {
+			// Valid paths probably don't start with whitespace
+			return false;
+		}
+
+		// Check given protocol (parsed is lowercase)
+		const m = url.match( /^\s*([^:]+):/ );
+		if ( ! m ) {
+			// No protocol
+			return false;
+		}
+
+		if ( /[A-Z]/.test( m[ 1 ] ) && /[a-z]/.test( m[ 1 ] ) ) {
+			// Mixed case protocol could be a word like "Note:" but let
+			// it pass if it's a recognized protocol.
+			return /^(https?|s?ftp|ssh|mailto)?:$/.test( parsed.protocol );
+		}
+
 		return true;
-	} catch {
+	} catch ( error ) {
 		return false;
 	}
 }
