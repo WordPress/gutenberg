1e2107239fd7cc55c8c36f8d4066ce9952897050
Jon Q <hello@jonquach.com>
2021-01-27 16:26:52 -0500

    Update Grid stories

--- packages/components/src/grid/stories/index.js
+++ packages/components/src/grid/stories/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { number } from '@storybook/addon-knobs';
+
+/**
  * Internal dependencies
  */
 import View from '../../view';
@@ -9,13 +14,33 @@ export default {
 	title: 'Components/Grid',
 };
 
+const Item = ( props ) => (
+	<View
+		css={ {
+			borderRadius: 8,
+			background: '#eee',
+			padding: 8,
+			textAlign: 'center',
+		} }
+		{ ...props }
+	/>
+);
+
 export const _default = () => {
+	const props = {
+		columns: number( 'columns', 4 ),
+		gap: number( 'gap', 2 ),
+	};
 	return (
-		<Grid alignment="bottom" columns={ [ 1, 2, 4 ] }>
-			<View>One</View>
-			<View>Two</View>
-			<View>Three</View>
-			<View>Four</View>
+		<Grid alignment="bottom" { ...props }>
+			<Item>One</Item>
+			<Item>Two</Item>
+			<Item>Three</Item>
+			<Item>Four</Item>
+			<Item>Five</Item>
+			<Item>Six</Item>
+			<Item>Seven</Item>
+			<Item>Eight</Item>
 		</Grid>
 	);
 };
