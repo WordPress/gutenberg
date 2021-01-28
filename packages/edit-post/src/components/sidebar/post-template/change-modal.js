e48c1eedf297e06c3104b63a4c33ff0be1b425d3
Riad Benguella <benguella@gmail.com>
2021-01-28 09:28:45 +0100

    Correctly style the template switching modal

--- packages/edit-post/src/components/sidebar/post-template/change-modal.js
+++ packages/edit-post/src/components/sidebar/post-template/change-modal.js
@@ -2,11 +2,12 @@
  * External dependencies
  */
 import { map } from 'lodash';
+import classnames from 'classnames';
 
 /**
  * WordPress dependencies
  */
-import { Modal } from '@wordpress/components';
+import { Modal, Spinner } from '@wordpress/components';
 import { store as editorStore } from '@wordpress/editor';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
@@ -16,7 +17,7 @@ import { useMemo } from '@wordpress/element';
 import { parse } from '@wordpress/blocks';
 import { ENTER, SPACE } from '@wordpress/keycodes';
 
-function TemplateItem( { slug, name } ) {
+function TemplatePreview( { slug } ) {
 	const { isResolved, template, postId, postType } = useSelect(
 		( select ) => {
 			const {
@@ -47,8 +48,6 @@ function TemplateItem( { slug, name } ) {
 		[ slug ]
 	);
 
-	const { editPost } = useDispatch( editorStore );
-
 	const blocks = useMemo( () => {
 		if ( ! template?.content?.raw ) {
 			return [];
@@ -60,6 +59,16 @@ function TemplateItem( { slug, name } ) {
 		return { postId, postType };
 	}, [ postId, postType ] );
 
+	return ! isResolved ? (
+		<Spinner />
+	) : (
+		<BlockPreview blocks={ blocks } context={ defaultBlockContext } />
+	);
+}
+
+function TemplateItem( { slug, name, isSelected } ) {
+	const { editPost } = useDispatch( editorStore );
+
 	const onSelect = () => {
 		editPost( {
 			template: slug,
@@ -67,42 +76,46 @@ function TemplateItem( { slug, name } ) {
 	};
 
 	return (
-		isResolved && (
-			<div
-				className="edit-post-template-change-modal__item"
-				role="button"
-				tabIndex={ 0 }
-				aria-label={ name }
-				onClick={ onSelect }
-				onKeyDown={ ( event ) => {
-					if ( ENTER === event.keyCode || SPACE === event.keyCode ) {
-						onSelect();
-					}
-				} }
-			>
-				<BlockPreview
-					blocks={ blocks }
-					context={ defaultBlockContext }
-				/>
-				<div className="edit-post-template-change-modal__item-title">
-					{ name }
-				</div>
+		<div
+			className={ classnames( 'edit-post-template-change-modal__item', {
+				'is-selected': isSelected,
+				'is-default': ! slug,
+			} ) }
+			role="button"
+			tabIndex={ 0 }
+			aria-label={ name }
+			onClick={ onSelect }
+			onKeyDown={ ( event ) => {
+				if ( ENTER === event.keyCode || SPACE === event.keyCode ) {
+					onSelect();
+				}
+			} }
+		>
+			<div className="edit-post-template-change-modal__item-preview">
+				{ slug && <TemplatePreview slug={ slug } /> }
 			</div>
-		)
+			<div className="edit-post-template-change-modal__item-title">
+				{ name }
+			</div>
+		</div>
 	);
 }
 
 function TemplateChangeModal( { onClose } ) {
-	const { availableTemplates } = useSelect( ( select ) => {
-		const { getEditorSettings } = select( editorStore );
+	const { availableTemplates, selectedTemplate } = useSelect( ( select ) => {
+		const { getEditorSettings, getEditedPostAttribute } = select(
+			editorStore
+		);
 		const { availableTemplates: _templates } = getEditorSettings();
 		return {
+			selectedTemplate: getEditedPostAttribute( 'template' ),
 			availableTemplates: _templates,
 		};
 	}, [] );
 
 	return (
 		<Modal
+			className="edit-post-post-template-modal"
 			title={ __( 'Change Template' ) }
 			closeLabel={ __( 'Close' ) }
 			onRequestClose={ onClose }
@@ -115,6 +128,10 @@ function TemplateChangeModal( { onClose } ) {
 							key={ slug }
 							slug={ slug }
 							name={ name }
+							isSelected={
+								slug === selectedTemplate ||
+								( ! slug && ! selectedTemplate )
+							}
 						/>
 					);
 				} ) }
