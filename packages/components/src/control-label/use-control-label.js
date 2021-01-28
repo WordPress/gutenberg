01f236d35d85e5f26ee545793b948c46ca2f36a2
Jon Q <hello@jonquach.com>
2021-01-28 16:28:04 -0500

    Refactor use-control-label with local useText

--- packages/components/src/control-label/use-control-label.js
+++ packages/components/src/control-label/use-control-label.js
@@ -2,17 +2,13 @@
  * External dependencies
  */
 import { useContextSystem } from '@wp-g2/context';
-/*
- * @todo: Refactor this with local useText
- * https://github.com/WordPress/gutenberg/pull/28475
- */
-import { useText } from '@wp-g2/components';
 import { cx } from '@wp-g2/styles';
 
 /**
  * Internal dependencies
  */
 import { useFormGroupContextId } from '../form-group';
+import { useText } from '../text/next/use-text';
 import * as styles from './control-label-styles';
 
 export function useControlLabel( props ) {
