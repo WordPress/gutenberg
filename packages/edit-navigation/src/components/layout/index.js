4c7f81a3f61ca21e2c6086c78671abd01b2a4726
Shaun Andrews <shaun@automattic.com>
2021-01-29 13:44:26 -0500

    Moving things around.

--- packages/edit-navigation/src/components/layout/index.js
+++ packages/edit-navigation/src/components/layout/index.js
@@ -61,6 +61,7 @@ export default function Layout( { blockEditorSettings } ) {
 							menus={ menus }
 							selectedMenuId={ selectedMenuId }
 							onSelectMenu={ selectMenu }
+							navigationPost={ navigationPost }
 						/>
 
 						<BlockEditorProvider
