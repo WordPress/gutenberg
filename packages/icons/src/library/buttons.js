48d9236f6d5590c93a5cd15fb0dcc192b0a94bba
jasmussen <joen@automattic.com>
2021-01-29 13:00:01 +0100

    Try: New Buttons icon.

---
+++ packages/icons/src/library/buttons.js
@@ -0,0 +1,16 @@
+/**
+ * WordPress dependencies
+ */
+import { Path, SVG } from '@wordpress/primitives';
+
+const buttons = (
+	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<Path
+			d="M9.5 8h-6c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2v-4c0-1.1-.9-2-2-2zm.5 6c0 .3-.2.5-.5.5h-6c-.3 0-.5-.2-.5-.5v-4c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v4zm10.5-6h-6c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2v-4c0-1.1-.9-2-2-2zm.5 6c0 .3-.2.5-.5.5h-6c-.3 0-.5-.2-.5-.5v-4c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v4zM5 12.8h3v-1.5H5v1.5zm11 0h3v-1.5h-3v1.5z"
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+		/>
+	</SVG>
+);
+
+export default buttons;
