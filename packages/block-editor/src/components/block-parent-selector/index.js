c4e74976ef143a31a2228a04db44b115d5e9dcf1
Matías Ventura <mv@matiasventura.com>
2021-01-29 17:37:09 +0100

    Highlight parent block outline when focusing or hovering the parent icon

--- packages/block-editor/src/components/block-parent-selector/index.js
+++ packages/block-editor/src/components/block-parent-selector/index.js
@@ -5,11 +5,13 @@ import { getBlockType, store as blocksStore } from '@wordpress/blocks';
 import { ToolbarButton } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { __, sprintf } from '@wordpress/i18n';
+import { useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import BlockIcon from '../block-icon';
+import { useShowMoversGestures } from '../block-toolbar/utils';
 
 /**
  * Block parent selector component, displaying the hierarchy of the
@@ -18,32 +20,50 @@ import BlockIcon from '../block-icon';
  * @return {WPComponent} Parent block selector.
  */
 export default function BlockParentSelector() {
-	const { selectBlock } = useDispatch( 'core/block-editor' );
-	const { parentBlockType, firstParentClientId, shouldHide } = useSelect(
-		( select ) => {
-			const {
-				getBlockName,
-				getBlockParents,
-				getSelectedBlockClientId,
-			} = select( 'core/block-editor' );
-			const { hasBlockSupport } = select( blocksStore );
-			const selectedBlockClientId = getSelectedBlockClientId();
-			const parents = getBlockParents( selectedBlockClientId );
-			const _firstParentClientId = parents[ parents.length - 1 ];
-			const parentBlockName = getBlockName( _firstParentClientId );
-			const _parentBlockType = getBlockType( parentBlockName );
-			return {
-				parentBlockType: _parentBlockType,
-				firstParentClientId: _firstParentClientId,
-				shouldHide: ! hasBlockSupport(
-					_parentBlockType,
-					'__experimentalParentSelector',
-					true
-				),
-			};
-		},
-		[]
+	const { selectBlock, toggleBlockHighlight } = useDispatch(
+		'core/block-editor'
 	);
+	const {
+		parentBlockType,
+		firstParentClientId,
+		shouldHide,
+		hasReducedUI,
+	} = useSelect( ( select ) => {
+		const {
+			getBlockName,
+			getBlockParents,
+			getSelectedBlockClientId,
+			getSettings,
+		} = select( 'core/block-editor' );
+		const { hasBlockSupport } = select( blocksStore );
+		const selectedBlockClientId = getSelectedBlockClientId();
+		const parents = getBlockParents( selectedBlockClientId );
+		const _firstParentClientId = parents[ parents.length - 1 ];
+		const parentBlockName = getBlockName( _firstParentClientId );
+		const _parentBlockType = getBlockType( parentBlockName );
+		const settings = getSettings();
+		return {
+			parentBlockType: _parentBlockType,
+			firstParentClientId: _firstParentClientId,
+			shouldHide: ! hasBlockSupport(
+				_parentBlockType,
+				'__experimentalParentSelector',
+				true
+			),
+			hasReducedUI: settings.hasReducedUI,
+		};
+	}, [] );
+
+	const nodeRef = useRef();
+	const { gestures: showMoversGestures } = useShowMoversGestures( {
+		ref: nodeRef,
+		onChange( isFocused ) {
+			if ( isFocused && hasReducedUI ) {
+				return;
+			}
+			toggleBlockHighlight( firstParentClientId, isFocused );
+		},
+	} );
 
 	if ( shouldHide ) {
 		return null;
@@ -54,6 +74,8 @@ export default function BlockParentSelector() {
 			<div
 				className="block-editor-block-parent-selector"
 				key={ firstParentClientId }
+				ref={ nodeRef }
+				{ ...showMoversGestures }
 			>
 				<ToolbarButton
 					className="block-editor-block-parent-selector__button"
