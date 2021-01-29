2d243ca9738192a751409323245fda606f00aa23
Bernie Reiter <ockham@raz.or.at>
2021-01-29 13:57:05 +0100

    Store diffs in artifacts dir

--- packages/e2e-tests/config/setup-visual-regression-test.js
+++ packages/e2e-tests/config/setup-visual-regression-test.js
@@ -1,7 +1,15 @@
 /**
  * External dependencies
  */
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+
+const root = process.env.GITHUB_WORKSPACE || process.cwd();
+const ARTIFACTS_PATH = root + '/artifacts';
+
+// All available options: https://github.com/americanexpress/jest-image-snapshot#%EF%B8%8F-api
+const toMatchImageSnapshot = configureToMatchImageSnapshot( {
+	customDiffDir: ARTIFACTS_PATH,
+} );
 
 // Extend Jest's "expect" with image snapshot functionality.
 expect.extend( { toMatchImageSnapshot } );
