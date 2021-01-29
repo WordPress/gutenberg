35f769ca7fd70cc0b1f18bb1e047980de010e1ce
Bernie Reiter <ockham@raz.or.at>
2021-01-29 13:33:25 +0100

    Default jest-image-snapshot settings seem to do the trick

--- packages/e2e-tests/config/setup-visual-regression-test.js
+++ packages/e2e-tests/config/setup-visual-regression-test.js
@@ -1,13 +1,7 @@
 /**
  * External dependencies
  */
-import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
-
-// All available options: https://github.com/americanexpress/jest-image-snapshot#%EF%B8%8F-api
-const toMatchImageSnapshot = configureToMatchImageSnapshot( {
-	// Maximum diff to allow in px.
-	failureThreshold: 1,
-} );
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
 
 // Extend Jest's "expect" with image snapshot functionality.
 expect.extend( { toMatchImageSnapshot } );
