5c24f3a6590421f90d5869288d1b81dc3811cd90
Bernie Reiter <ockham@raz.or.at>
2021-01-29 18:26:33 +0100

    Rename image snapshots/diffs

--- packages/e2e-tests/config/setup-visual-regression-test.js
+++ packages/e2e-tests/config/setup-visual-regression-test.js
@@ -6,9 +6,20 @@ import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 const root = process.env.GITHUB_WORKSPACE || process.cwd();
 const ARTIFACTS_PATH = root + '/artifacts';
 
+function slugify( testName ) {
+	const slug = testName
+		.toLowerCase()
+		.replace( /:/g, '-' )
+		.replace( /[^0-9a-zA-Z \-\(\)]/g, '' )
+		.replace( / /g, '-' );
+	return slug;
+}
+
 // All available options: https://github.com/americanexpress/jest-image-snapshot#%EF%B8%8F-api
 const toMatchImageSnapshot = configureToMatchImageSnapshot( {
 	customDiffDir: ARTIFACTS_PATH,
+	customSnapshotIdentifier: ( { currentTestName } ) =>
+		slugify( currentTestName ),
 } );
 
 // Extend Jest's "expect" with image snapshot functionality.
