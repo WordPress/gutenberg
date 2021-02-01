ef8014524322e0fdcfa2110c384cee720a69def2
Shaun Andrews <shaun@automattic.com>
2021-02-01 18:03:27 -0500

    Cleaning up the unnecessary stuff from the editor component.

--- packages/edit-navigation/src/components/editor/index.js
+++ packages/edit-navigation/src/components/editor/index.js
@@ -1,12 +1,31 @@
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
-import BlockView from './block-view';
+import { BlockList, ObserveTyping, WritingFlow } from '@wordpress/block-editor';
+import { Spinner } from '@wordpress/components';
+
+/**
+ * The current navigation rendered in the form of a core Navigation block.
+ *
+ * @param {Object}  props           Component props.
+ * @param {boolean} props.isPending Whether the navigation post has loaded.
+ */
 
 export default function Editor( { isPending } ) {
 	return (
 		<div className="edit-navigation-editor">
-			<BlockView isPending={ isPending } />
+			{ isPending ? (
+				<Spinner />
+			) : (
+				// Do we need this div?
+				<div className="editor-styles-wrapper">
+					<WritingFlow>
+						<ObserveTyping>
+							<BlockList />
+						</ObserveTyping>
+					</WritingFlow>
+				</div>
+			) }
 		</div>
 	);
 }
