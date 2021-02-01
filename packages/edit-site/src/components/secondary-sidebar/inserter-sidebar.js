2ad3ff1cce4e745e4c93d1c6661551c94227d517
Copons <Copons@users.noreply.github.com>
2021-02-01 16:25:31 +0000

    Move the Inserter into its own component for readability

---
+++ packages/edit-site/src/components/secondary-sidebar/inserter-sidebar.js
@@ -0,0 +1,45 @@
+/**
+ * WordPress dependencies
+ */
+import { useDispatch } from '@wordpress/data';
+import { Button } from '@wordpress/components';
+import { __experimentalLibrary as Library } from '@wordpress/block-editor';
+import { close } from '@wordpress/icons';
+import {
+	useViewportMatch,
+	__experimentalUseDialog as useDialog,
+} from '@wordpress/compose';
+
+export default function InserterSidebar() {
+	const { setIsInserterOpened } = useDispatch( 'core/edit-site' );
+
+	const isMobile = useViewportMatch( 'medium', '<' );
+	const [ inserterDialogRef, inserterDialogProps ] = useDialog( {
+		onClose: () => setIsInserterOpened( false ),
+	} );
+
+	return (
+		<div
+			ref={ inserterDialogRef }
+			{ ...inserterDialogProps }
+			className="edit-site-editor__inserter-panel"
+		>
+			<div className="edit-site-editor__inserter-panel-header">
+				<Button
+					icon={ close }
+					onClick={ () => setIsInserterOpened( false ) }
+				/>
+			</div>
+			<div className="edit-site-editor__inserter-panel-content">
+				<Library
+					showInserterHelpPanel
+					onSelect={ () => {
+						if ( isMobile ) {
+							setIsInserterOpened( false );
+						}
+					} }
+				/>
+			</div>
+		</div>
+	);
+}
