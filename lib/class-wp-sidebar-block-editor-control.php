ebcfd74c0f24860582f5d82a978004124e692b43
Kai Hao <kevin830726@gmail.com>
2021-02-02 17:54:20 +0800

    Remove needless input

--- lib/class-wp-sidebar-block-editor-control.php
+++ lib/class-wp-sidebar-block-editor-control.php
@@ -8,14 +8,6 @@ class WP_Sidebar_Block_Editor_Control extends WP_Customize_Control {
 	}
 
 	public function render_content() {
-		?>
-		<input
-			id="_customize-input-gutenberg_widget_blocks"
-			type="hidden"
-			value="<?php echo esc_attr( $this->value() ); ?>"
-			<?php $this->link(); ?>
-		/>
-		<?php
 		the_gutenberg_widgets( 'gutenberg_customizer' );
 	}
 }
