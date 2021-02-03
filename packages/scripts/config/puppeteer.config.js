fbf92c4d5bfe019da4d48d5b92b6bc6b64e8fe58
Bernie Reiter <ockham@raz.or.at>
2021-02-03 20:39:28 +0100

    Try disabling font render hinting

--- packages/scripts/config/puppeteer.config.js
+++ packages/scripts/config/puppeteer.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	launch: {
+		args: [ '--font-render-hinting=none' ],
 		devtools: process.env.PUPPETEER_DEVTOOLS === 'true',
 		headless: process.env.PUPPETEER_HEADLESS !== 'false',
 		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 0,
