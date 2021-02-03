bd20f816eda5231d34eefa9764d596956b05527b
Sara Marcondes <saram@fastmail.com>
2021-02-03 09:53:33 -0800

    Update with latest from g2

--- packages/components/src/ui/flex/flex-styles.js
+++ packages/components/src/ui/flex/flex-styles.js
@@ -18,3 +18,27 @@ export const Item = css`
 export const block = css`
 	flex: 1;
 `;
+
+/**
+ * Workaround to optimize DOM rendering.
+ * We'll enhance alignment with naive parent flex assumptions.
+ *
+ * Trade-off:
+ * Far less DOM less. However, UI rendering is not as reliable.
+ */
+
+/**
+ * Improves stability of width/height rendering.
+ * https://github.com/ItsJonQ/g2/pull/149
+ */
+export const ItemsColumn = css`
+	> * {
+		min-height: 0;
+	}
+`;
+
+export const ItemsRow = css`
+	> * {
+		min-width: 0;
+	}
+`;
