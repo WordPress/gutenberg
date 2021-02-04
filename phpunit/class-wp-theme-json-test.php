854502457aa48ee9edb7af59a16cfa736b53460d
André <andres.maneiro@automattic.com>
2021-02-04 14:28:42 +0100

    Add unit test for page templates

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -736,4 +736,22 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 		);
 		$this->assertEqualSetsWithIndex( $expected, $result );
 	}
+
+	function test_get_page_templates() {
+		$theme_json = new WP_Theme_JSON( array(
+			'pageTemplates' => array(
+				'page-home' => array(
+					'title' => 'Some title'
+				),
+			),
+		) );
+
+		$page_templates = $theme_json->get_page_templates();
+
+		$this->assertEqualSetsWithIndex( $page_templates, array(
+			'page-home' => array(
+				'title' => 'Some title',
+			),
+		) );
+	}
 }
