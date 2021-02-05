639a7cfc40d4c9647f3afd637d496da598fd56e6
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-02-05 08:08:02 -0800

    Fix values
    Co-authored-by: Gerardo Pacheco <gerardo.pacheco@automattic.com>

--- packages/block-library/src/button/edit.native.js
+++ packages/block-library/src/button/edit.native.js
@@ -54,10 +54,10 @@ function WidthPanel( { selectedWidth, setAttributes } ) {
 
 	const options = [
 		{ value: 'auto', label: __( 'Auto' ) },
-		{ value: '25', label: '25%' },
-		{ value: '50', label: '50%' },
-		{ value: '75', label: '75%' },
-		{ value: '100', label: '100%' },
+		{ value: 25, label: '25%' },
+		{ value: 50, label: '50%' },
+		{ value: 75, label: '75%' },
+		{ value: 100, label: '100%' },
 	];
 
 	if ( ! selectedWidth ) {
