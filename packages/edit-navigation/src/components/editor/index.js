87e031fa2e4a586b91f9c55f6b978871b35e23df
Daniel Richards <daniel.richards@automattic.com>
2021-02-08 12:54:14 +0800

    Update comments

--- packages/edit-navigation/src/components/editor/index.js
+++ packages/edit-navigation/src/components/editor/index.js
@@ -4,20 +4,12 @@
 import { BlockList, ObserveTyping, WritingFlow } from '@wordpress/block-editor';
 import { Spinner } from '@wordpress/components';
 
-/**
- * The current navigation rendered in the form of a core Navigation block.
- *
- * @param {Object}  props           Component props.
- * @param {boolean} props.isPending Whether the navigation post has loaded.
- */
-
 export default function Editor( { isPending } ) {
 	return (
 		<div className="edit-navigation-editor">
 			{ isPending ? (
 				<Spinner />
 			) : (
-				// Do we need this div?
 				<div className="editor-styles-wrapper">
 					<WritingFlow>
 						<ObserveTyping>
