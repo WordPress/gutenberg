d84d922103fd35ab19509e4a691376666fea8734
Ella van Durpe <ella@vandurpe.com>
2021-02-08 16:15:40 +0200

    Remove formatting options

--- packages/block-library/src/query-pagination-next/edit.js
+++ packages/block-library/src/query-pagination-next/edit.js
@@ -2,19 +2,19 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, PlainText } from '@wordpress/block-editor';
 
 export default function QueryPaginationNextEdit( {
 	attributes: { label },
 	setAttributes,
 } ) {
 	return (
-		<RichText
+		<PlainText
+			__experimentalVersion={ 2 }
 			tagName="a"
 			aria-label={ __( 'Next page link' ) }
 			placeholder={ __( 'Next Page' ) }
 			value={ label }
-			withoutInteractiveFormatting
 			keepPlaceholderOnFocus
 			onChange={ ( newLabel ) => setAttributes( { label: newLabel } ) }
 			{ ...useBlockProps() }
--- packages/block-library/src/query-pagination-previous/edit.js
+++ packages/block-library/src/query-pagination-previous/edit.js
@@ -2,19 +2,19 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, PlainText } from '@wordpress/block-editor';
 
 export default function QueryPaginationPreviousEdit( {
 	attributes: { label },
 	setAttributes,
 } ) {
 	return (
-		<RichText
+		<PlainText
+			__experimentalVersion={ 2 }
 			tagName="a"
 			aria-label={ __( 'Previous page link' ) }
 			placeholder={ __( 'Previous Page' ) }
 			value={ label }
-			withoutInteractiveFormatting
 			keepPlaceholderOnFocus
 			onChange={ ( newLabel ) => setAttributes( { label: newLabel } ) }
 			{ ...useBlockProps() }
