9ee61da673ea8e9b25ad70f4da612e8fea43c071
Ari Stathopoulos <aristath@gmail.com>
2021-02-08 10:29:26 +0200

    add inline comment

--- packages/block-library/src/post-comments-form/index.php
+++ packages/block-library/src/post-comments-form/index.php
@@ -28,6 +28,7 @@ function render_block_core_post_comments_form( $attributes, $content, $block ) {
 	$form               = ob_get_clean();
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classes ) );
 
+	// Enqueue the comment-reply script.
 	global $wp_scripts;
 	$wp_scripts->in_footer[] = 'comment-reply';
 	$wp_scripts->queue[]     = 'comment-reply';
