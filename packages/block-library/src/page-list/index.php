014b7c1471c55749264001d7f22bcce8e79795bf
tellthemachines <isabel@tellthemachines.com>
2021-02-08 11:31:48 +1100

    Order menu items and de-indent comment.

--- packages/block-library/src/page-list/index.php
+++ packages/block-library/src/page-list/index.php
@@ -136,20 +136,20 @@ function nest_pages( $current_level, $children ) {
 	return $current_level;
 }
 
-	/**
-	 * Renders the `core/page-list` block on server.
-	 *
-	 * @param array $attributes The block attributes.
-	 * @param array $content The saved content.
-	 * @param array $block The parsed block.
-	 *
-	 * @return string Returns the page list markup.
-	 */
+/**
+ * Renders the `core/page-list` block on server.
+ *
+ * @param array $attributes The block attributes.
+ * @param array $content The saved content.
+ * @param array $block The parsed block.
+ *
+ * @return string Returns the page list markup.
+ */
 function render_block_core_page_list( $attributes, $content, $block ) {
 	static $block_id = 0;
 	$block_id++;
 
-	$all_pages = get_pages();
+	$all_pages = get_pages( array( 'sort_column' => 'menu_order' ) );
 
 	$top_level_pages = array();
 
