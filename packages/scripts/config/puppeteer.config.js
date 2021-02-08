8d145e007414281dbc9a6fe05e9ec596e31fcf59
Kai Hao <kevin830726@gmail.com>
2021-02-08 13:58:49 +0800

    Remove puppeteer-testing-library in scripts

--- packages/scripts/config/puppeteer.config.js
+++ packages/scripts/config/puppeteer.config.js
@@ -1,10 +1,8 @@
-const { launchArgs } = require( 'puppeteer-testing-library' );
-
 module.exports = {
 	launch: {
 		devtools: process.env.PUPPETEER_DEVTOOLS === 'true',
 		headless: process.env.PUPPETEER_HEADLESS !== 'false',
 		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 0,
-		args: launchArgs(),
+		args: [ '--enable-blink-features=ComputedAccessibilityInfo' ],
 	},
 };
