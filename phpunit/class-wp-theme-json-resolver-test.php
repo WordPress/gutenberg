9d6b04e1e88b05a6bbe2495da0570eb9d61ad8d2
André <andres.maneiro@automattic.com>
2021-02-09 18:22:07 +0100

    Implement new intermediate representation

--- phpunit/class-wp-theme-json-resolver-test.php
+++ phpunit/class-wp-theme-json-resolver-test.php
@@ -13,36 +13,44 @@ class WP_Theme_JSON_Test extends WP_UnitTestCase {
 
         $expected = array(
             array(
-                'path'              => array( 'settings', '*', 'typography', 'fontSizes' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'typography', 'fontSizes' ),
+                'key'     => 'name',
+                'context' => 'Font size name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'typography', 'fontStyles' ),
-                'translatable_keys' => array( 'name' ),
+                'path'             => array( 'settings', '*', 'typography', 'fontStyles' ),
+                'key'     => 'name',
+                'context' => 'Font style name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'typography', 'fontWeights' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'typography', 'fontWeights' ),
+                'key'     => 'name',
+                'context' => 'Font weight name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'typography', 'fontFamilies' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'typography', 'fontFamilies' ),
+                'key'     => 'name',
+                'context' => 'Font family name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'typography', 'textTransforms' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'typography', 'textTransforms' ),
+                'key'     => 'name',
+                'context' => 'Text transform name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'typography', 'textDecorations' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'typography', 'textDecorations' ),
+                'key'     => 'name',
+                'context' => 'Text decoration name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'color', 'palette' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'color', 'palette' ),
+                'key'     => 'name',
+                'context' => 'Color name',
             ),
             array(
-                'path'              => array( 'settings', '*', 'color', 'gradients' ),
-                'translatable_keys' => array( 'name' ),
+                'path'    => array( 'settings', '*', 'color', 'gradients' ),
+                'key'     => 'name',
+                'context' => 'Gradient name',
             ),
         );
 
