6a09785c9754436d80f81066d1a14415cec2562e
Sara Marcondes <saram@fastmail.com>
2021-02-09 08:31:27 -0800

    Add back the focus styles

--- packages/components/src/ui/visually-hidden/use-visually-hidden.js
+++ packages/components/src/ui/visually-hidden/use-visually-hidden.js
@@ -4,36 +4,24 @@
 import { cx } from '@wp-g2/styles';
 
 /**
+ * Internal dependencies
+ */
+import * as styles from './visually-hidden-styles';
+
+/**
  *
  * @param {import('@wp-g2/create-styles').ViewOwnProps<{}, 'div'>} props
  */
-export function useVisuallyHidden( {
-	className,
-	style: propStyles,
-	...props
-} ) {
+export function useVisuallyHidden( { className, ...props } ) {
 	// circumvent the context system and write the classnames ourselves
 	const classes = cx(
 		'components-visually-hidden wp-components-visually-hidden',
-		className
+		className,
+		styles.VisuallyHidden
 	);
-	/** @type {import('react').CSSProperties} */
-	const style = {
-		border: 0,
-		clip: 'rect(0 0 0 0)',
-		height: '1px',
-		margin: '-1px',
-		overflow: 'hidden',
-		padding: 0,
-		position: 'absolute',
-		whiteSpace: 'nowrap',
-		width: '1px',
-		...propStyles,
-	};
 
 	return {
 		className: classes,
-		style,
 		...props,
 	};
 }
