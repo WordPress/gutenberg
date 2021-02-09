09fbc77b10c35a30cc587e19930b6a199a0761bc
Alex Lende <ajlende@gmail.com>
2021-02-09 08:55:59 -1000

    Add duotone to media & text block transforms

--- packages/block-library/src/media-text/transforms.js
+++ packages/block-library/src/media-text/transforms.js
@@ -8,24 +8,26 @@ const transforms = {
 		{
 			type: 'block',
 			blocks: [ 'core/image' ],
-			transform: ( { alt, url, id, anchor } ) =>
+			transform: ( { alt, url, id, anchor, duotone } ) =>
 				createBlock( 'core/media-text', {
 					mediaAlt: alt,
 					mediaId: id,
 					mediaUrl: url,
 					mediaType: 'image',
 					anchor,
+					duotone,
 				} ),
 		},
 		{
 			type: 'block',
 			blocks: [ 'core/video' ],
-			transform: ( { src, id, anchor } ) =>
+			transform: ( { src, id, anchor, duotone } ) =>
 				createBlock( 'core/media-text', {
 					mediaId: id,
 					mediaUrl: src,
 					mediaType: 'video',
 					anchor,
+					duotone,
 				} ),
 		},
 	],
@@ -36,12 +38,13 @@ const transforms = {
 			isMatch: ( { mediaType, mediaUrl } ) => {
 				return ! mediaUrl || mediaType === 'image';
 			},
-			transform: ( { mediaAlt, mediaId, mediaUrl, anchor } ) => {
+			transform: ( { mediaAlt, mediaId, mediaUrl, anchor, duotone } ) => {
 				return createBlock( 'core/image', {
 					alt: mediaAlt,
 					id: mediaId,
 					url: mediaUrl,
 					anchor,
+					duotone,
 				} );
 			},
 		},
@@ -51,11 +54,12 @@ const transforms = {
 			isMatch: ( { mediaType, mediaUrl } ) => {
 				return ! mediaUrl || mediaType === 'video';
 			},
-			transform: ( { mediaId, mediaUrl, anchor } ) => {
+			transform: ( { mediaId, mediaUrl, anchor, duotone } ) => {
 				return createBlock( 'core/video', {
 					id: mediaId,
 					src: mediaUrl,
 					anchor,
+					duotone,
 				} );
 			},
 		},
