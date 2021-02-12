812b83746e4eb31820b92bc1211fbb8a333954a0
David Szabo <david.szabo97@gmail.com>
2021-02-12 16:49:41 +0100

    Add suggestion

--- packages/eslint-plugin/rules/data-no-store-string-literals.js
+++ packages/eslint-plugin/rules/data-no-store-string-literals.js
@@ -1,3 +1,7 @@
+function arrayLast( array ) {
+	return array[ array.length - 1 ];
+}
+
 function getReferences( context, specifiers ) {
 	const variables = specifiers.reduce(
 		( acc, specifier ) =>
@@ -85,6 +89,123 @@ function collectAllNodesFromObjectPropertyFunctionCalls( context, node ) {
 	return possibleCallExpressionNodes;
 }
 
+function getSuggest( context, callNode ) {
+	return [
+		{
+			desc:
+				'Replace literal with store definition. Import store if neccessary.',
+			fix: ( fixer ) => getFixes( fixer, context, callNode ),
+		},
+	];
+}
+
+function getFixes( fixer, context, callNode ) {
+	const storeName = callNode.arguments[ 0 ].value;
+	const storeDefinitions = {
+		core: {
+			import: '@wordpress/core-data',
+			variable: 'coreStore',
+		},
+		'core/block-editor': {
+			import: '@wordpress/block-editor',
+			variable: 'blockEditorStore',
+		},
+		'core/block-directory': {
+			import: '@wordpress/block-directory',
+			variable: 'blockDirectoryStore',
+		},
+		'core/blocks': {
+			import: '@wordpress/blocks',
+			variable: 'blocksStore',
+		},
+		'core/editor': {
+			import: '@wordpress/editor',
+			variable: 'editorStore',
+		},
+		'core/notices': {
+			import: '@wordpress/notices',
+			variable: 'noticesStore',
+		},
+		'core/reusable-blocks': {
+			import: '@wordpress/reusable-blocks',
+			variable: 'reusableBlocksStore',
+		},
+		'core/keyboard-shortcuts': {
+			import: '@wordpress/keyboard-shortcuts',
+			variable: 'keyboardShortcutsStore',
+		},
+		'core/edit-post': {
+			import: '@wordpress/edit-post',
+			variable: 'editPostStore',
+		},
+		'core/edit-site': {
+			import: '@wordpress/edit-site',
+			variable: 'editSiteStore',
+		},
+		'core/interface': {
+			import: '@wordpress/interface',
+			variable: 'interfaceStore',
+		},
+		'core/viewport': {
+			import: '@wordpress/viewport',
+			variable: 'viewportStore',
+		},
+		'core/rich-text': {
+			import: '@wordpress/rich-text',
+			variable: 'richTextStore',
+		},
+	};
+	if ( ! storeDefinitions[ storeName ] ) {
+		return null;
+	}
+	const { variable: variableName, import: importName } = storeDefinitions[
+		storeName
+	];
+
+	const fixes = [
+		fixer.replaceText( callNode.arguments[ 0 ], variableName ),
+	];
+
+	const imports = context
+		.getAncestors()[ 0 ]
+		.body.filter( ( n ) => n.type === 'ImportDeclaration' );
+	const packageImports = imports.filter(
+		( n ) => n.source.value === importName
+	);
+	const i = packageImports.length > 0 ? packageImports[ 0 ] : null;
+	if ( i ) {
+		const alreadyHasStore = i.specifiers.some(
+			( s ) => s.imported.name === 'store'
+		);
+		if ( ! alreadyHasStore ) {
+			const lastSpecifier = arrayLast( i.specifiers );
+			fixes.push(
+				fixer.insertTextAfter(
+					lastSpecifier,
+					`,store as ${ variableName }`
+				)
+			);
+		}
+	} else {
+		const wpImports = imports.filter( ( n ) =>
+			n.source.value.startsWith( '@wordpress/' )
+		);
+		const lastImport =
+			wpImports.length > 0
+				? arrayLast( wpImports )
+				: arrayLast( imports );
+
+		fixes.push(
+			fixer.insertTextAfter(
+				lastImport,
+				`\nimport { store as ${ variableName } } from '${ importName }';`
+			)
+		);
+	}
+
+	return fixes;
+}
+
 module.exports = {
 	meta: {
 		type: 'problem',
@@ -131,6 +252,7 @@ module.exports = {
 							node: callNode.parent,
 							messageId: 'doNotUseStringLiteral',
 							data: { argument: callNode.arguments[ 0 ].value },
+							suggest: getSuggest( context, callNode ),
 						} );
 					} );
 			},
