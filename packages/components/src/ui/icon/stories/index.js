81d3ed69b18ba80d8c7cb82ad00e75eddc1a808a
Sara Marcondes <saram@fastmail.com>
2021-02-12 11:01:21 -0800

    Add library to new Icon stories

--- packages/components/src/ui/icon/stories/index.js
+++ packages/components/src/ui/icon/stories/index.js
@@ -1,7 +1,14 @@
 /**
+ * External dependencies
+ */
+import { omit, omitBy, map } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { SVG, Path } from '@wordpress/primitives';
+import * as icons from '@wordpress/icons';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -25,3 +32,43 @@ export const _default = () => {
 		/>
 	);
 };
+
+const availableIcons = omit( icons, 'Icon' );
+
+const Library = () => {
+	const [ filter, setFilter ] = useState( '' );
+	const filteredIcons = omitBy( availableIcons, ( _icon, name ) => {
+		return name.indexOf( filter ) === -1;
+	} );
+	return (
+		<div style={ { padding: '20px' } }>
+			<label htmlFor="filter-icon" style={ { paddingRight: '10px' } }>
+				Filter Icons
+			</label>
+			<input
+				// eslint-disable-next-line no-restricted-syntax
+				id="filter-icons"
+				type="search"
+				value={ filter }
+				placeholder="Icon name"
+				onChange={ ( event ) => setFilter( event.target.value ) }
+			/>
+			{ map( filteredIcons, ( icon, name ) => {
+				return (
+					<div
+						key={ name }
+						style={ { display: 'flex', alignItems: 'center' } }
+					>
+						<strong style={ { width: '120px' } }>{ name }</strong>
+
+						<Icon icon={ icon } />
+						<Icon icon={ icon } size={ 36 } />
+						<Icon icon={ icon } size={ 48 } />
+					</div>
+				);
+			} ) }
+		</div>
+	);
+};
+
+export const library = () => <Library />;
