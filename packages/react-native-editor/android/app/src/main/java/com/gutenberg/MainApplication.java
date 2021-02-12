6bcfbd9b91a1c5bc7787357b8de779ad69344104
Siobhan <siobhan@automattic.com>
2021-02-12 10:54:10 +0000

    Add setFeaturedImage function to MainApplication
    This commit adds the setFeaturedImage function to the MainApplication file, so that it works as expected with the demo apps.

--- packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainApplication.java
+++ packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainApplication.java
@@ -113,6 +113,10 @@ public class MainApplication extends Application implements ReactApplication, Gu
             }
 
             @Override
+            public void setFeaturedImage(int mediaId) {
+            }
+
+            @Override
             public void editorDidMount(ReadableArray unsupportedBlockNames) {
             }
 
