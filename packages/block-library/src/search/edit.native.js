6267582b34fbaa7bdeb40019304f0b55c4406a95
Amanda Riu <amanda.riu@automattic.com>
2021-02-12 23:25:05 -0500

    Add label and icon toggle toolbar buttons

--- packages/block-library/src/search/edit.native.js
+++ packages/block-library/src/search/edit.native.js
@@ -6,14 +6,52 @@ import { View } from 'react-native';
 /**
  * WordPress dependencies
  */
-import { RichText } from '@wordpress/block-editor';
+import { RichText, BlockControls } from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import { buttonWithIcon, toggleLabel } from './icons';
+
 export default function SearchEdit( { attributes, setAttributes } ) {
-	const { label, showLabel } = attributes;
+	const { label, showLabel, buttonPosition, buttonUseIcon } = attributes;
+
+	const controls = (
+		<BlockControls>
+			<ToolbarGroup>
+				<ToolbarButton
+					title={ __( 'Toggle search label' ) }
+					icon={ toggleLabel }
+					onClick={ () => {
+						setAttributes( {
+							showLabel: ! showLabel,
+						} );
+					} }
+					isActive={ showLabel }
+				/>
+
+				{ 'no-button' !== buttonPosition && (
+					<ToolbarButton
+						title={ __( 'Use button with icon' ) }
+						icon={ buttonWithIcon }
+						onClick={ () => {
+							setAttributes( {
+								buttonUseIcon: ! buttonUseIcon,
+							} );
+						} }
+						isActive={ buttonUseIcon }
+					/>
+				) }
+			</ToolbarGroup>
+		</BlockControls>
+	);
 
 	return (
 		<View>
+			{ controls }
+
 			{ showLabel && (
 				<RichText
 					aria-label={ __( 'Label text' ) }
