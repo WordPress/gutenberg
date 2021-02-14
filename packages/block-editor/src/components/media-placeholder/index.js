95efc3d97f72d94cb51b3c9c54b1c29e7fa8641a
Glen Davies <glen.davies@a8c.com>
2021-02-15 12:44:44 +1300

    Fix css and image upload issues

--- packages/block-editor/src/components/media-placeholder/index.js
+++ packages/block-editor/src/components/media-placeholder/index.js
@@ -128,7 +128,7 @@ export function MediaPlaceholder( {
 
 	const onFilesUpload = ( files ) => {
 		if ( isGallery ) {
-			// Because the Gallery hands the files over to Image component InnerBlocks just
+			// Because the refactored Gallery hands the files over to Image component InnerBlocks just
 			// hand the handling of the files over to the Gallery
 			onSelect( files );
 			return;
@@ -136,7 +136,46 @@ export function MediaPlaceholder( {
 		onFilesPreUpload( files );
 		let setMedia;
 		if ( multiple ) {
-			setMedia = onSelect;
+			// This is still needed to handle v1 versions of the Gallery block. It can be removed
+			// once all Gallery instances are forced to migrate.
+			if ( addToGallery ) {
+				// Since the setMedia function runs multiple times per upload group
+				// and is passed newMedia containing every item in its group each time, we must
+				// filter out whatever this upload group had previously returned to the
+				// gallery before adding and returning the image array with replacement newMedia
+				// values.
+
+				// Define an array to store urls from newMedia between subsequent function calls.
+				let lastMediaPassed = [];
+				setMedia = ( newMedia ) => {
+					// Remove any images this upload group is responsible for (lastMediaPassed).
+					// Their replacements are contained in newMedia.
+					const filteredMedia = ( value ?? [] ).filter( ( item ) => {
+						// If Item has id, only remove it if lastMediaPassed has an item with that id.
+						if ( item.id ) {
+							return ! lastMediaPassed.some(
+								// Be sure to convert to number for comparison.
+								( { id } ) => Number( id ) === Number( item.id )
+							);
+						}
+						// Compare transient images via .includes since gallery may append extra info onto the url.
+						return ! lastMediaPassed.some( ( { urlSlug } ) =>
+							item.url.includes( urlSlug )
+						);
+					} );
+					// Return the filtered media array along with newMedia.
+					onSelect( filteredMedia.concat( newMedia ) );
+					// Reset lastMediaPassed and set it with ids and urls from newMedia.
+					lastMediaPassed = newMedia.map( ( media ) => {
+						// Add everything up to '.fileType' to compare via .includes.
+						const cutOffIndex = media.url.lastIndexOf( '.' );
+						const urlSlug = media.url.slice( 0, cutOffIndex );
+						return { id: media.id, urlSlug };
+					} );
+				};
+			} else {
+				setMedia = onSelect;
+			}
 		} else {
 			setMedia = ( [ media ] ) => onSelect( media );
 		}
