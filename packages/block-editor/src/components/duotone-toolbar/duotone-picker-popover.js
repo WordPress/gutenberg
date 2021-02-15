638d10519e7ddbec4c60dbed237f974b079660bb
jasmussen <joen@automattic.com>
2021-02-15 10:32:01 +0100

    Try using menugroup.

--- packages/block-editor/src/components/duotone-toolbar/duotone-picker-popover.js
+++ packages/block-editor/src/components/duotone-toolbar/duotone-picker-popover.js
@@ -1,7 +1,11 @@
 /**
  * WordPress dependencies
  */
-import { CircularOptionPicker, Popover } from '@wordpress/components';
+import {
+	CircularOptionPicker,
+	Popover,
+	MenuGroup,
+} from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 
 /**
@@ -24,65 +28,64 @@ function DuotonePickerPopover( {
 			headerTitle={ __( 'Duotone' ) }
 			onFocusOutside={ onToggle }
 		>
-			<span className="block-editor-duotone-toolbar__label">
-				{ __( 'Duotone' ) }
-			</span>
-			<CircularOptionPicker
-				options={ duotonePalette.map( ( option ) => {
-					const isSelected = option.slug === value?.slug;
-					const style = {
-						background: getGradientFromCSSColors(
-							option.colors,
-							'135deg'
-						),
-						color: 'transparent',
-					};
-					const code = sprintf(
-						// translators: %s: duotone code e.g: "dark-grayscale" or "7f7f7f-ffffff".
-						__( 'Duotone code: %s' ),
-						option.slug
-					);
-					const label = sprintf(
-						// translators: %s: The name of the option e.g: "Dark grayscale".
-						__( 'Duotone: %s' ),
-						option.name
-					);
+			<MenuGroup label={ __( 'Duotone' ) }>
+				<CircularOptionPicker
+					options={ duotonePalette.map( ( option ) => {
+						const isSelected = option.slug === value?.slug;
+						const style = {
+							background: getGradientFromCSSColors(
+								option.colors,
+								'135deg'
+							),
+							color: 'transparent',
+						};
+						const code = sprintf(
+							// translators: %s: duotone code e.g: "dark-grayscale" or "7f7f7f-ffffff".
+							__( 'Duotone code: %s' ),
+							option.slug
+						);
+						const label = sprintf(
+							// translators: %s: The name of the option e.g: "Dark grayscale".
+							__( 'Duotone: %s' ),
+							option.name
+						);
 
-					return (
-						<CircularOptionPicker.Option
-							key={ option.slug }
-							value={ option.slug }
-							isSelected={ isSelected }
-							tooltipText={ option.name ?? code }
-							style={ style }
-							onClick={ () => {
-								const newValue = {
-									values: getValuesFromColors(
-										option.colors
-									),
-									id: `duotone-filter-${ option.slug }`,
-								};
-								onChange( isSelected ? undefined : newValue );
-							} }
-							aria-label={ option.name ? label : code }
-						/>
-					);
-				} ) }
-				actions={
-					<CircularOptionPicker.ButtonAction
-						onClick={ () => onChange( undefined ) }
-					>
-						{ __( 'Clear' ) }
-					</CircularOptionPicker.ButtonAction>
-				}
-			>
-				<CustomDuotoneBar value={ value } onChange={ onChange } />
-				<CustomDuotonePicker
-					colorPalette={ colorPalette }
-					value={ value }
-					onChange={ onChange }
-				/>
-			</CircularOptionPicker>
+						return (
+							<CircularOptionPicker.Option
+								key={ option.slug }
+								value={ option.slug }
+								isSelected={ isSelected }
+								tooltipText={ option.name ?? code }
+								style={ style }
+								onClick={ () => {
+									const newValue = {
+										values: getValuesFromColors(
+											option.colors
+										),
+										id: `duotone-filter-${ option.slug }`,
+									};
+									onChange( isSelected ? undefined : newValue );
+								} }
+								aria-label={ option.name ? label : code }
+							/>
+						);
+					} ) }
+					actions={
+						<CircularOptionPicker.ButtonAction
+							onClick={ () => onChange( undefined ) }
+						>
+							{ __( 'Clear' ) }
+						</CircularOptionPicker.ButtonAction>
+					}
+				>
+					<CustomDuotoneBar value={ value } onChange={ onChange } />
+					<CustomDuotonePicker
+						colorPalette={ colorPalette }
+						value={ value }
+						onChange={ onChange }
+					/>
+				</CircularOptionPicker>
+			</MenuGroup>
 			<div className="block-editor-duotone-toolbar__description">
 				{ __(
 					'The duotone filter creates a two-color version of your image, where you choose the colors.'
