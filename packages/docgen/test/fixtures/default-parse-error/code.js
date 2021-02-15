ac750272b9de646b6dbd1db20270ab4920c0dea6
André <nosolosw@users.noreply.github.com>
2021-02-15 16:49:43 +0100

    Update packages/docgen/test/fixtures/default-parse-error/code.js

--- packages/docgen/test/fixtures/default-parse-error/code.js
+++ packages/docgen/test/fixtures/default-parse-error/code.js
@@ -3,7 +3,7 @@
  * Function invoking callback after delay with current timestamp in milliseconds
  * since epoch.
  *
- * @param {(timestamp:number=>void} callback Callback function.
+ * @param {(timestamp:number)=>void} callback Callback function.
  */
 export default function invokeCallbackAfterDelay( callback ) {
 	setTimeout( () => callback( Date.now() ), 1000 );
