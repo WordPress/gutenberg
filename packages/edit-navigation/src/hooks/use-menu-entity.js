416f946068875c0d877dc6f5979bcaea17ec2d9c
grzegorz_marzencki <grzegorz.marzencki@a8c.com>
2021-02-16 19:35:22 +0100

    console error fixed

--- packages/edit-navigation/src/hooks/use-menu-entity.js
+++ packages/edit-navigation/src/hooks/use-menu-entity.js
@@ -3,10 +3,6 @@
  */
 import { useSelect, useDispatch } from '@wordpress/data';
 /**
- * External dependencies
- */
-import { isUndefined, negate } from 'lodash';
-/**
  * Internal dependencies
  */
 import { MENU_KIND, MENU_POST_TYPE } from '../utils/constants';
@@ -19,6 +15,7 @@ export default function useMenuEntity( menuId ) {
 	const menuEntityData = [ MENU_KIND, MENU_POST_TYPE, menuId ];
 	const editedMenu = useSelect(
 		( select ) =>
+			menuId &&
 			select( 'core' ).getEditedEntityRecord( ...menuEntityData ),
 		[ menuId ]
 	);
@@ -26,8 +23,7 @@ export default function useMenuEntity( menuId ) {
 	const editedMenuName = menuId && editedMenu.name;
 
 	const saveMenuName = () =>
-		negate( isUndefined )( editedMenuName ) &&
-		saveEditedEntityRecord( ...menuEntityData );
+		editedMenuName && saveEditedEntityRecord( ...menuEntityData );
 
 	const editMenuName = ( name = untitledMenu ) =>
 		editEntityRecord( ...menuEntityData, { name } );
