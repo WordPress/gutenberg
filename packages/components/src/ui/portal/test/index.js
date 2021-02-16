96d3cb59b9d9ea5a0372fd0aeed33d1b3dce2296
Jon Q <hello@jonquach.com>
2021-02-16 13:35:13 -0500

    Update Portal test

--- packages/components/src/ui/portal/test/index.js
+++ packages/components/src/ui/portal/test/index.js
@@ -10,11 +10,24 @@ import { Portal } from '../index';
 
 describe( 'props', () => {
 	test( 'should render correctly', () => {
-		const { container } = render(
-			<Portal>
-				<div>Code is Poetry - WordPress.org</div>
-			</Portal>
+		render(
+			<>
+				<div className="container">
+					<Portal>
+						<div className="element">
+							Code is Poetry - WordPress.org
+						</div>
+					</Portal>
+				</div>
+			</>
 		);
-		expect( container.firstChild ).toMatchSnapshot();
+
+		// This is because the `div.element` is portal'ed outside of the wrapping `.container`
+		expect(
+			document.querySelector( 'body .container .element' )
+		).toBeFalsy();
+
+		// Instead, it is rendered at the `body` level.
+		expect( document.querySelector( 'body .element' ) ).toBeTruthy();
 	} );
 } );
