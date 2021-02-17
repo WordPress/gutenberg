80263e8748759780065905c09d3aa63d03b173c7
Jon Q <hello@jonquach.com>
2021-02-17 14:38:47 -0500

    Add base styles for Popover::before/after element

---
+++ packages/components/src/ui/popover/styles.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import { css, ui } from '@wp-g2/styles';
+
+export const PopoverContent = css`
+	${ ui.zIndex( 'Popover', 10000 ) };
+	box-sizing: border-box;
+	opacity: 0;
+	outline: none;
+	position: relative;
+	transform-origin: center center;
+	transition: opacity ${ ui.get( 'transitionDurationFastest' ) } linear;
+	width: 100%;
+
+	&[data-enter] {
+		opacity: 1;
+	}
+
+	&::before,
+	&::after {
+		display: none;
+	}
+`;
