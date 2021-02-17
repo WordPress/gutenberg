6f94edab3a9e72c518c08092c31b63291c1f8a3a
Daniel Richards <daniel.richards@automattic.com>
2021-02-17 17:51:56 +0800

    Make separator-less menu groups possible

--- packages/edit-navigation/src/components/header/index.js
+++ packages/edit-navigation/src/components/header/index.js
@@ -97,7 +97,7 @@ export default function Header( {
 										} ) ) }
 									/>
 								</MenuGroup>
-								<MenuGroup>
+								<MenuGroup hideSeparator>
 									<MenuItem
 										isTertiary
 										onClick={ () =>
