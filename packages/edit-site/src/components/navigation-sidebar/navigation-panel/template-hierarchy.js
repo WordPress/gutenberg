66ea2e1ac7494e9e8c51d81ccd397deb9fd249a9
David Szabo <david.szabo97@gmail.com>
2021-02-17 15:36:36 +0100

    Simplify isTemplateSuperseded

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/template-hierarchy.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/template-hierarchy.js
@@ -1,45 +1,16 @@
-/**
- * Maps template slugs to their parents.
- */
-const TEMPLATE_PARENTS = {
-	index: null,
-	archive: 'index',
-
-	singular: 'index',
-	single: 'singular',
-	page: 'singular',
-
-	home: 'index',
-	404: 'index',
-	search: 'index',
-
-	'front-page': 'home',
+const TEMPLATE_OVERRIDES = {
+	singular: [ 'single', 'page' ],
+	index: [ 'archive', '404', 'search', 'singular' ],
 };
 
-/**
- * Maps template slugs to their children template slugs.
- */
-const TEMPLATE_DEPS = Object.keys( TEMPLATE_PARENTS ).reduce(
-	( obj, template ) => {
-		const parent = TEMPLATE_PARENTS[ template ];
-		if ( ! obj[ parent ] ) {
-			obj[ parent ] = [];
-		}
-		obj[ parent ].push( template );
-
-		return obj;
-	},
-	{}
-);
-
 export function isTemplateSuperseded( slug, existingSlugs ) {
-	return TEMPLATE_DEPS[ slug ]
-		? TEMPLATE_DEPS[ slug ].every(
-				( dep ) =>
-					isTemplateSuperseded( dep, existingSlugs ) ||
-					!! existingSlugs.find(
-						( existingSlug ) => existingSlug === dep
-					)
-		  )
-		: ! existingSlugs.find( ( existingSlug ) => existingSlug === slug );
+	if ( ! TEMPLATE_OVERRIDES[ slug ] ) {
+		return false;
+	}
+
+	return TEMPLATE_OVERRIDES[ slug ].every(
+		( overrideSlug ) =>
+			existingSlugs.includes( overrideSlug ) ||
+			isTemplateSuperseded( overrideSlug, existingSlugs )
+	);
 }
