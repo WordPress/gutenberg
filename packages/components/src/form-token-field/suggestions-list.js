a388d110f39ffb59a225748c9da818f5aac0eaa6
Quintis1212 <34488718+Quintis1212@users.noreply.github.com>
2021-02-18 17:26:03 +0200

    removed function decodeHTML

--- packages/components/src/form-token-field/suggestions-list.js
+++ packages/components/src/form-token-field/suggestions-list.js
@@ -105,31 +105,6 @@ class SuggestionsList extends Component {
 					);
 
 					/* eslint-disable jsx-a11y/click-events-have-key-events */
-
-					    var label = suggestion.label;
-
-					    var decodeEntities = (function() {
-					      // this prevents any overhead from creating the object each time
-					      var element = document.createElement('div');
-
-					      function decodeHTMLEntities (str) {
-						if(str && typeof str === 'string') {
-						  // strip script/html tags
-						  str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
-						  str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-						  element.innerHTML = str;
-						  str = element.textContent;
-						  element.textContent = '';
-						}
-
-						return str;
-					      }
-
-					      return decodeHTMLEntities;
-					    })();
-
-					    suggestion.label = decodeEntities(label);
-
 					return (
 						<li
 							id={ `components-form-token-suggestions-${ this.props.instanceId }-${ index }` }
