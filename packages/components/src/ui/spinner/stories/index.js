f5c419fd7d15e0091b8b383760920bc03ee6a4c7
Sara Marcondes <saram@fastmail.com>
2021-02-19 06:45:12 -0800

    Components: Add spinner

---
+++ packages/components/src/ui/spinner/stories/index.js
@@ -0,0 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import { Spinner } from '../index';
+
+export default {
+	component: Spinner,
+	title: 'G2 Components (Experimental)/Spinner',
+};
+
+export const _default = () => {
+	return <Spinner />;
+};
