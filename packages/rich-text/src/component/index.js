533909e7ddbe78726684549fcb874d7f05623945
Quintis1212 <frey1@meta.ua>
2021-02-20 11:37:47 +0200

    Bug fix for issue #28926

--- packages/rich-text/src/component/index.js
+++ packages/rich-text/src/component/index.js
@@ -746,7 +746,7 @@ function RichText(
 
 		const transformed = formatTypes.reduce(
 			( accumlator, { __unstableInputRule } ) => {
-				if ( __unstableInputRule ) {
+				if ( __unstableInputRule && event.target.tagName !== "CODE" ) {
 					accumlator = __unstableInputRule( accumlator );
 				}
 
