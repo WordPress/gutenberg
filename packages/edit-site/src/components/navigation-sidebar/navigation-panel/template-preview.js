d51882e599a1e7102e84ef2983c7ab315f1a9cac
David Szabo <david.szabo97@gmail.com>
2021-02-22 16:34:47 +0100

    Add preview

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/template-preview.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/template-preview.js
@@ -4,14 +4,29 @@
 import { parse } from '@wordpress/blocks';
 import { BlockPreview } from '@wordpress/block-editor';
 import { useMemo } from '@wordpress/element';
+import { EntityProvider } from '@wordpress/core-data';
 
-export default function TemplatePreview( { rawContent } ) {
+export default function TemplatePreview( { rawContent, provideEntity } ) {
 	const blocks = useMemo( () => ( rawContent ? parse( rawContent ) : [] ), [
 		rawContent,
 	] );
 
 	if ( ! blocks || blocks.length === 0 ) {
 		return null;
+	}
+
+	if ( provideEntity ) {
+		return (
+			<div className="edit-site-navigation-panel__preview">
+				<EntityProvider
+					kind="postType"
+					type={ provideEntity.type }
+					id={ provideEntity.id }
+				>
+					<BlockPreview blocks={ blocks } viewportWidth={ 1200 } />
+				</EntityProvider>
+			</div>
+		);
 	}
 
 	return (
