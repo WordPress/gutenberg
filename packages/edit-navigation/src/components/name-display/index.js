0a90dda48ea0b256f32ecbd61e31ef2578ce5279
grzegorz_marzencki <grzegorz.marzencki@a8c.com>
2021-02-22 09:02:14 +0100

    add missing file

--- packages/edit-navigation/src/components/name-display/index.js
+++ packages/edit-navigation/src/components/name-display/index.js
@@ -6,12 +6,17 @@ import { BlockControls } from '@wordpress/block-editor';
 /**
  * Internal dependencies
  */
-import { useSelectedMenuData, IsMenuNameControlFocusedContext } from '../../hooks';
+import {
+	useSelectedMenuData,
+	IsMenuNameControlFocusedContext,
+} from '../../hooks';
 import { useContext } from '@wordpress/element';
 
 export default function NameDisplay() {
 	const { menuName } = useSelectedMenuData();
-	const [ , setIsMenuNameEditFocused ] = useContext( IsMenuNameControlFocusedContext );
+	const [ , setIsMenuNameEditFocused ] = useContext(
+		IsMenuNameControlFocusedContext
+	);
 	return (
 		<BlockControls>
 			<ToolbarGroup>
