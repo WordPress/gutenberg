9237cba1211e514da1f20dc9ef863e040ee33fc9
ntsekouras <ntsekouras@outlook.com>
2021-02-22 11:37:34 +0200

    do not render Section if children render nothing

--- packages/edit-post/src/components/preferences-modal/section.js
+++ packages/edit-post/src/components/preferences-modal/section.js
@@ -1,15 +1,24 @@
-const Section = ( { description, title, children } ) => (
-	<section className="edit-post-preferences-modal__section">
-		<h2 className="edit-post-preferences-modal__section-title">
-			{ title }
-		</h2>
-		{ description && (
-			<p className="edit-post-preferences-modal__section-description">
-				{ description }
-			</p>
-		) }
-		{ children }
-	</section>
-);
+/**
+ * WordPress dependencies
+ */
+import { renderToString } from '@wordpress/element';
+
+const Section = ( { description, title, children } ) => {
+	const content = renderToString( children );
+	if ( ! content ) return null;
+	return (
+		<section className="edit-post-preferences-modal__section">
+			<h2 className="edit-post-preferences-modal__section-title">
+				{ title }
+			</h2>
+			{ description && (
+				<p className="edit-post-preferences-modal__section-description">
+					{ description }
+				</p>
+			) }
+			{ children }
+		</section>
+	);
+};
 
 export default Section;
