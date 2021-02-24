61bbbfebc52ad83e98027ef95ad918686c663196
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2021-02-23 18:50:00 -0600

    Fix "Convert to static list" control.

--- packages/block-library/src/table-of-contents/list.js
+++ packages/block-library/src/table-of-contents/list.js
@@ -1,30 +1,28 @@
 const ENTRY_CLASS_NAME = 'wp-block-table-of-contents__entry';
 
 export default function TableOfContentsList( { nestedHeadingList } ) {
-	return (
-		<ul>
-			{ nestedHeadingList.map( ( childNode, index ) => {
-				const { anchor, content } = childNode.heading;
+	return nestedHeadingList.map( ( childNode, index ) => {
+		const { anchor, content } = childNode.heading;
 
-				const entry = anchor ? (
-					<a className={ ENTRY_CLASS_NAME } href={ anchor }>
-						{ content }
-					</a>
-				) : (
-					<span className={ ENTRY_CLASS_NAME }>{ content }</span>
-				);
+		const entry = anchor ? (
+			<a className={ ENTRY_CLASS_NAME } href={ anchor }>
+				{ content }
+			</a>
+		) : (
+			<span className={ ENTRY_CLASS_NAME }>{ content }</span>
+		);
 
-				return (
-					<li key={ index }>
-						{ entry }
-						{ childNode.children ? (
-							<TableOfContentsList
-								nestedHeadingList={ childNode.children }
-							/>
-						) : null }
-					</li>
-				);
-			} ) }
-		</ul>
-	);
+		return (
+			<li key={ index }>
+				{ entry }
+				{ childNode.children ? (
+					<ul>
+						<TableOfContentsList
+							nestedHeadingList={ childNode.children }
+						/>
+					</ul>
+				) : null }
+			</li>
+		);
+	} );
 }
