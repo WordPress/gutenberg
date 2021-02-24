9065c397d855cc863703c7df86774cc5031b5fdd
Rafael Galani <galani.rafael@gmail.com>
2021-02-24 12:09:48 -0300

    Inserter: Add e2e test to make sure last inserted block is being focused (#29187)
    Asserts that the last inserted block has the focus when inserting.

--- packages/e2e-tests/specs/editor/various/inserter.test.js
+++ packages/e2e-tests/specs/editor/various/inserter.test.js
@@ -4,11 +4,15 @@
 import {
 	createNewPost,
 	openGlobalBlockInserter,
+	insertBlock,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'Inserter', () => {
-	it( 'shows block preview when hovering over block in inserter', async () => {
+	beforeEach( async () => {
 		await createNewPost();
+	} );
+
+	it( 'shows block preview when hovering over block in inserter', async () => {
 		await openGlobalBlockInserter();
 		await page.focus( '.editor-block-list-item-paragraph' );
 		const preview = await page.waitForSelector(
@@ -19,5 +23,22 @@ describe( 'Inserter', () => {
 		);
 		const isPreviewVisible = await preview.isIntersectingViewport();
 		expect( isPreviewVisible ).toBe( true );
+	} );
+
+	it( 'last-inserted block should be given and keep the focus', async () => {
+		await page.type(
+			'.block-editor-default-block-appender__content',
+			'Testing inserted block focus'
+		);
+
+		await insertBlock( 'Image' );
+
+		await page.waitForSelector( 'figure[data-type="core/image"]' );
+
+		const selectedBlock = await page.evaluate( () => {
+			return wp.data.select( 'core/block-editor' ).getSelectedBlock();
+		} );
+
+		expect( selectedBlock.name ).toBe( 'core/image' );
 	} );
 } );
