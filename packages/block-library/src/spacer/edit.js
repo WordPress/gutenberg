cbec90f6c4333962e340140d9f38c8f0a4d7ec90
tellthemachines <isabel@tellthemachines.com>
2021-02-24 11:52:23 +1100

    Make spacer orientation depend on parent block.

--- packages/block-library/src/spacer/edit.js
+++ packages/block-library/src/spacer/edit.js
@@ -30,7 +30,9 @@ const SpacerEdit = ( {
 	setAttributes,
 	onResizeStart,
 	onResizeStop,
+	context,
 } ) => {
+	const { orientation } = context;
 	const [ isResizing, setIsResizing ] = useState( false );
 	const { height, width } = attributes;
 	const updateHeight = ( value ) => {
@@ -64,26 +66,26 @@ const SpacerEdit = ( {
 		setIsResizing( false );
 	};
 
-	return (
-		<>
-			<View { ...useBlockProps() }>
+	const resizableBoxWithOrientation = ( blockOrientation ) => {
+		if ( blockOrientation === 'horizontal' ) {
+			return (
 				<ResizableBox
 					className={ classnames(
 						'block-library-spacer__resize-container',
+						'resize-horizontal',
 						{
 							'is-selected': isSelected,
 						}
 					) }
 					size={ {
-						height,
 						width,
+						height: 32,
 					} }
-					minHeight={ MIN_SPACER_HEIGHT }
 					minWidth={ MIN_SPACER_WIDTH }
 					enable={ {
 						top: false,
 						right: true,
-						bottom: true,
+						bottom: false,
 						left: false,
 						topRight: false,
 						bottomRight: false,
@@ -95,28 +97,74 @@ const SpacerEdit = ( {
 					showHandle={ isSelected }
 					__experimentalShowTooltip={ true }
 					__experimentalTooltipProps={ {
-						axis: 'y',
-						position: 'bottom',
+						axis: 'x',
+						position: 'corner',
 						isVisible: isResizing,
 					} }
 				/>
+			);
+		}
+
+		return (
+			<ResizableBox
+				className={ classnames(
+					'block-library-spacer__resize-container',
+					{
+						'is-selected': isSelected,
+					}
+				) }
+				size={ {
+					height,
+				} }
+				minHeight={ MIN_SPACER_HEIGHT }
+				enable={ {
+					top: false,
+					right: false,
+					bottom: true,
+					left: false,
+					topRight: false,
+					bottomRight: false,
+					bottomLeft: false,
+					topLeft: false,
+				} }
+				onResizeStart={ handleOnResizeStart }
+				onResizeStop={ handleOnResizeStop }
+				showHandle={ isSelected }
+				__experimentalShowTooltip={ true }
+				__experimentalTooltipProps={ {
+					axis: 'y',
+					position: 'bottom',
+					isVisible: isResizing,
+				} }
+			/>
+		);
+	};
+
+	return (
+		<>
+			<View { ...useBlockProps() }>
+				{ resizableBoxWithOrientation( orientation ) }
 			</View>
 			<InspectorControls>
 				<PanelBody title={ __( 'Spacer settings' ) }>
-					<RangeControl
-						label={ __( 'Height in pixels' ) }
-						min={ MIN_SPACER_HEIGHT }
-						max={ Math.max( MAX_SPACER_HEIGHT, height ) }
-						value={ height }
-						onChange={ updateHeight }
-					/>
-					<RangeControl
-						label={ __( 'Width in pixels' ) }
-						min={ MIN_SPACER_WIDTH }
-						max={ Math.max( MAX_SPACER_WIDTH, width ) }
-						value={ width }
-						onChange={ updateWidth }
-					/>
+					{ orientation === 'horizontal' && (
+						<RangeControl
+							label={ __( 'Width in pixels' ) }
+							min={ MIN_SPACER_WIDTH }
+							max={ Math.max( MAX_SPACER_WIDTH, width ) }
+							value={ width }
+							onChange={ updateWidth }
+						/>
+					) }
+					{ orientation !== 'horizontal' && (
+						<RangeControl
+							label={ __( 'Height in pixels' ) }
+							min={ MIN_SPACER_HEIGHT }
+							max={ Math.max( MAX_SPACER_HEIGHT, height ) }
+							value={ height }
+							onChange={ updateHeight }
+						/>
+					) }
 				</PanelBody>
 			</InspectorControls>
 		</>
