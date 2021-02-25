3057c4c5f5ddef9a9b3f550e4a553f078da16738
sarayourfriend <sarayourfriend@fastmail.com>
2021-02-25 05:44:34 -0800

    Remove unnecessary ignores

--- typings/gutenberg-env/index.d.ts
+++ typings/gutenberg-env/index.d.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef, no-unused-vars, no-var */
 interface Environment {
 	NODE_ENV: unknown;
 	COMPONENT_SYSTEM_PHASE: number | undefined;
@@ -7,4 +6,3 @@ interface Process {
 	env: Environment;
 }
 declare var process: Process;
-/* eslint-enable no-undef, no-unused-vars, no-var */
