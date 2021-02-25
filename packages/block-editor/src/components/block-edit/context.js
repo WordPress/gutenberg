0bf074f8358ffb6957029e4bbd039c10687145ae
Ella van Durpe <ella@vandurpe.com>
2021-02-25 11:10:22 +0200

    Block edit: avoid memoized block context in favour of useSelect

--- packages/block-editor/src/components/block-edit/context.js
+++ packages/block-editor/src/components/block-edit/context.js
@@ -2,21 +2,36 @@
  * WordPress dependencies
  */
 import { createContext, useContext } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 
-const Context = createContext( {
-	name: '',
-	isSelected: false,
-	clientId: null,
-} );
+/**
+ * Internal dependencies
+ */
+import { store as blockEditorStore } from '../../store';
+
+const Context = createContext();
 const { Provider } = Context;
 
 export { Provider as BlockEditContextProvider };
 
 /**
- * A hook that returns the block edit context.
+ * A hook that returns the block client ID, name and selected status.
  *
- * @return {Object} Block edit context
+ * @return {Object} Block client ID, name and selected status.
  */
 export function useBlockEditContext() {
-	return useContext( Context );
+	const clientId = useContext( Context );
+	return useSelect(
+		( select ) => {
+			const { getBlockName, isBlockSelected } = select(
+				blockEditorStore
+			);
+			return {
+				clientId,
+				name: getBlockName( clientId ),
+				isSelected: isBlockSelected( clientId ),
+			};
+		},
+		[ clientId ]
+	);
 }
