c857bdb3e9cbff5808f7f9d8f5987693eb75ee43
André <nosolosw@users.noreply.github.com>
2021-02-26 11:22:47 +0100

    Implement skip serialization for color key in style att (#29253)

--- packages/block-editor/src/hooks/test/style.js
+++ packages/block-editor/src/hooks/test/style.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import { getInlineStyles } from '../style';
+import { getInlineStyles, omitKeysNotToSerialize } from '../style';
 
 describe( 'getInlineStyles', () => {
 	it( 'should return an empty object when called with undefined', () => {
@@ -25,6 +25,34 @@ describe( 'getInlineStyles', () => {
 			color: 'red',
 			lineHeight: 1.5,
 			fontSize: 10,
+		} );
+	} );
+} );
+
+describe( 'omitKeysNotToSerialize', () => {
+	it( 'should return the same style if no keys are skipped from serialization', () => {
+		const style = {
+			color: { text: 'red' },
+			lineHeight: 2,
+		};
+		expect( omitKeysNotToSerialize( style, {} ) ).toEqual( {
+			color: { text: 'red' },
+			lineHeight: 2,
+		} );
+	} );
+
+	it( 'should omit the color key if it is skipped for serialization', () => {
+		const style = {
+			color: { text: 'red' },
+			lineHeight: 2,
+		};
+		const blockSupports = {
+			color: {
+				__experimentalSkipSerialization: true,
+			},
+		};
+		expect( omitKeysNotToSerialize( style, blockSupports ) ).toEqual( {
+			lineHeight: 2,
 		} );
 	} );
 } );
