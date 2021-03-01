40a8bbec55a5b294eb383bacc79308dd836e8cdc
Jon Q <hello@jonquach.com>
2021-03-01 15:33:33 -0500

    Update documentation in README and JSDoc

--- packages/components/src/ui/popover/component.js
+++ packages/components/src/ui/popover/component.js
@@ -104,4 +104,24 @@ function Popover( props, forwardedRef ) {
 	);
 }
 
+/**
+ * `Popover` is a component to render a floating content modal.
+ * It is similar in purpose to a tooltip, but renders content of any sort,
+ * not only simple text.
+ *
+ * @example
+ * ```jsx
+ * import { Button, Popover, View, Text } from `@wordpress/components/ui`;
+ 
+ * function Example() {
+ * 	return (
+ * 		<Popover trigger={ <Button>Popover</Button> }>
+ * 			<View>
+ * 				<Text>Code is Poetry</Text>
+ * 			</View>
+ * 		</Popover>
+ * 	);
+ * }
+ * ```
+ */
 export default contextConnect( Popover, 'Popover' );
