637019470cdb995d59bc2c84d5bae1e7e7b8d476
Glen Davies <glendaviesnz@users.noreply.github.com>
2021-03-01 14:14:22 +1300

    Move the setting of the default attributes on new blocks to a single useEffect (#29328)
    Co-authored-by: Glen Davies <glen.davies@a8c.com>

---
+++ packages/block-library/src/gallery/use-get-media.js
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+import { some } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
+
+export default function useGetMedia( innerBlockImages ) {
+	const [ currentImageMedia, setCurrentImageMedia ] = useState( [] );
+
+	const imageMedia = useSelect(
+		( select ) => {
+			if (
+				! innerBlockImages?.length ||
+				some(
+					innerBlockImages,
+					( imageBlock ) => ! imageBlock.attributes.id
+				)
+			) {
+				return currentImageMedia;
+			}
+
+			const imageIds = innerBlockImages.map(
+				( imageBlock ) => imageBlock.attributes.id
+			);
+
+			if ( imageIds.length === 0 ) {
+				return currentImageMedia;
+			}
+			const getMedia = select( coreStore ).getMedia;
+			const newImageMedia = imageIds.map( ( img ) => {
+				return getMedia( img );
+			} );
+
+			if ( newImageMedia.some( ( img ) => ! img ) ) {
+				return currentImageMedia;
+			}
+
+			return newImageMedia;
+		},
+		[ innerBlockImages ]
+	);
+
+	if ( imageMedia?.length !== currentImageMedia.length ) {
+		setCurrentImageMedia( imageMedia );
+		return imageMedia;
+	}
+
+	return currentImageMedia;
+}
