c31a492cf2dd19c25e9d95f274dbdba9c2e00914
sarayourfriend <sarayourfriend@fastmail.com>
2021-03-01 03:37:11 -0800

    Components: Add Popover

--- packages/components/src/ui/utils/types.ts
+++ packages/components/src/ui/utils/types.ts
@@ -12,3 +12,31 @@ export type Options< T extends As, P extends ViewOwnProps< {}, T > > = {
 };
 
 export type ResponsiveCSSValue< T > = Array< T | undefined > | T;
+
+export type PopperPlacement =
+	| 'auto'
+	| 'auto-start'
+	| 'auto-end'
+	| 'top'
+	| 'top-start'
+	| 'top-end'
+	| 'right'
+	| 'right-start'
+	| 'right-end'
+	| 'bottom'
+	| 'bottom-start'
+	| 'bottom-end'
+	| 'left'
+	| 'left-start'
+	| 'left-end';
+
+export type PopperProps = {
+	/**
+	 * Position of the popover element.
+	 *
+	 * @default 'auto'
+	 *
+	 * @see https://popper.js.org/docs/v1/#popperplacements--codeenumcode
+	 */
+	placement?: PopperPlacement;
+};
