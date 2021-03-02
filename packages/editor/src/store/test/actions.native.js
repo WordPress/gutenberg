7030c9dfdd27eb78657b03aef0466e63bda1b62d
Joel Dean <jdeanjj1000@gmail.com>
2021-03-01 21:24:37 -0500

    added unit tests for the actions and reducers of the last block inserted

--- packages/editor/src/store/test/actions.native.js
+++ packages/editor/src/store/test/actions.native.js
@@ -1,7 +1,11 @@
 /**
  * Internal dependencies
  */
-import { togglePostTitleSelection } from '../actions';
+import {
+	togglePostTitleSelection,
+	addLastBlockInserted,
+	clearLastBlockInserted,
+} from '../actions';
 
 describe( 'actions native', () => {
 	describe( 'togglePostTitleSelection', () => {
@@ -10,6 +14,26 @@ describe( 'actions native', () => {
 			expect( result ).toEqual( {
 				type: 'TOGGLE_POST_TITLE_SELECTION',
 				isSelected: true,
+			} );
+		} );
+	} );
+
+	describe( 'addLastBlockInserted', () => {
+		it( 'should return the ADD_LAST_BLOCK_INSERTED action', () => {
+			const clientId = 1;
+			const result = addLastBlockInserted( clientId );
+			expect( result ).toEqual( {
+				type: 'ADD_LAST_BLOCK_INSERTED',
+				clientId,
+			} );
+		} );
+	} );
+
+	describe( 'clearLastBlockInserted', () => {
+		it( 'should return the CLEAR_LAST_BLOCK_INSERTED action', () => {
+			const result = clearLastBlockInserted();
+			expect( result ).toEqual( {
+				type: 'CLEAR_LAST_BLOCK_INSERTED',
 			} );
 		} );
 	} );
