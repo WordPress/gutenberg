6742b75b9c04dec736edaf0064ffe585781d76ea
Joel Dean <jdeanjj1000@gmail.com>
2021-03-03 23:18:15 -0500

    created actions for adding and clearing last inserted block event.

--- packages/editor/src/store/test/actions.native.js
+++ packages/editor/src/store/test/actions.native.js
@@ -1,15 +1,39 @@
 /**
  * Internal dependencies
  */
-import { togglePostTitleSelection } from '../actions';
+import {
+	togglePostTitleSelection,
+	addLastBlockInserted,
+	clearLastBlockInserted,
+} from '../actions';
 
-describe( 'Editor actions', () => {
+describe( 'actions native', () => {
 	describe( 'togglePostTitleSelection', () => {
 		it( 'should return the TOGGLE_POST_TITLE_SELECTION action', () => {
 			const result = togglePostTitleSelection( true );
 			expect( result ).toEqual( {
 				type: 'TOGGLE_POST_TITLE_SELECTION',
 				isSelected: true,
+			} );
+		} );
+	} );
+
+	describe( 'addLastBlockInserted', () => {
+		it( 'should return the ADD_LAST_BLOCK_INSERTED action', () => {
+			const expectedClientId = 1;
+			const result = addLastBlockInserted( expectedClientId );
+			expect( result ).toEqual( {
+				type: 'ADD_LAST_BLOCK_INSERTED',
+				clientId: expectedClientId,
+			} );
+		} );
+	} );
+
+	describe( 'clearLastBlockInserted', () => {
+		it( 'should return the CLEAR_LAST_BLOCK_INSERTED action', () => {
+			const result = clearLastBlockInserted();
+			expect( result ).toEqual( {
+				type: 'CLEAR_LAST_BLOCK_INSERTED',
 			} );
 		} );
 	} );
