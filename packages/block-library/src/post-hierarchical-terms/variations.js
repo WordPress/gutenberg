4b52595ed77e0556a6e24f4cea4cb5e80b4e6819
Carolina Nymark <myazalea@hotmail.com>
2021-03-04 10:54:27 +0100

    Update title, description, and icon of Post Categories (#29400)
    * Update title and description of Post Categories
    
    Update title, description and icon of the Post Categories block variation
    
    * Switch to "isActive"
    
    Reverts changes to index.js and add isActive to variations.
    
    * Update variations.js
    
    Co-authored-by: Carolina Nymark <hi@themesbycarolina.com>

--- packages/block-library/src/post-hierarchical-terms/variations.js
+++ packages/block-library/src/post-hierarchical-terms/variations.js
@@ -7,9 +7,11 @@ const variations = [
 	{
 		name: 'category',
 		title: __( 'Post Categories' ),
+		description: __( "Display a post's categories." ),
 		icon: 'category',
 		isDefault: true,
 		attributes: { term: 'category' },
+		isActive: ( blockAttributes ) => blockAttributes.term === 'category',
 	},
 ];
 
