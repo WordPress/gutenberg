e06d2dd8bb48d7e050b7fa39bbed098ce97acdb3
Glen Davies <glen.davies@a8c.com>
2021-03-04 15:40:48 +1300

    Some more e2e fixes

---
+++ packages/block-library/src/gallery/v1/shared.js
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ */
+import { get, pick } from 'lodash';
+
+export const pickRelevantMediaFiles = ( image, sizeSlug = 'large' ) => {
+	const imageProps = pick( image, [ 'alt', 'id', 'link', 'caption' ] );
+	imageProps.url =
+		get( image, [ 'sizes', sizeSlug, 'url' ] ) ||
+		get( image, [ 'media_details', 'sizes', sizeSlug, 'source_url' ] ) ||
+		image.url;
+	const fullUrl =
+		get( image, [ 'sizes', 'full', 'url' ] ) ||
+		get( image, [ 'media_details', 'sizes', 'full', 'source_url' ] );
+	if ( fullUrl ) {
+		imageProps.fullUrl = fullUrl;
+	}
+	return imageProps;
+};
