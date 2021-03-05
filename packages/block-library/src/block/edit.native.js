257bde1de121ba2f1392e87366e502c2c49d0367
Carlos Garcia <fluiddot@gmail.com>
2021-03-05 19:14:21 +0100

    Add editing flow to reusable block

--- packages/block-library/src/block/edit.native.js
+++ packages/block-library/src/block/edit.native.js
@@ -1,68 +1,82 @@
 /**
  * External dependencies
  */
-import {
-	ActivityIndicator,
-	Platform,
-	Text,
-	TouchableWithoutFeedback,
-	View,
-} from 'react-native';
+import { ActivityIndicator, Alert, Text, View } from 'react-native';
 
 /**
  * WordPress dependencies
  */
-import { useState } from '@wordpress/element';
-import { useEntityBlockEditor, store as coreStore } from '@wordpress/core-data';
-import { BottomSheet, Icon, Disabled } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { useState, useRef, useEffect, useCallback } from '@wordpress/element';
 import {
-	BlockEditorProvider,
-	BlockList,
+	useEntityBlockEditor,
+	useEntityProp,
+	store as coreStore,
+} from '@wordpress/core-data';
+import {
+	Disabled,
+	ToolbarGroup,
+	ToolbarButton,
+	TextControl,
+	PanelBody,
+	Icon,
+} from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __, sprintf } from '@wordpress/i18n';
+import {
+	InnerBlocks,
+	BlockControls,
+	InspectorControls,
+	FloatingToolbar,
+	useFloatingToolbarAnimationsContext,
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { usePreferredColorSchemeStyle } from '@wordpress/compose';
-import { help } from '@wordpress/icons';
 import { store as reusableBlocksStore } from '@wordpress/reusable-blocks';
+import { store as noticesStore } from '@wordpress/notices';
+import { ungroup, check, close } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import styles from './editor.scss';
-import EditTitle from './edit-title';
 
-export default function ReusableBlockEdit( {
-	attributes: { ref },
-	clientId,
-	isSelected,
-} ) {
+function ReusableBlockEdit( { attributes: { ref }, clientId, isSelected } ) {
 	const recordArgs = [ 'postType', 'wp_block', ref ];
 
-	const [ showHelp, setShowHelp ] = useState( false );
-	const infoTextStyle = usePreferredColorSchemeStyle(
-		styles.infoText,
-		styles.infoTextDark
-	);
-	const infoTitleStyle = usePreferredColorSchemeStyle(
-		styles.infoTitle,
-		styles.infoTitleDark
-	);
-	const infoSheetIconStyle = usePreferredColorSchemeStyle(
-		styles.infoSheetIcon,
-		styles.infoSheetIconDark
-	);
+	const [ isEditing, setIsEditing ] = useState( false );
+	const originalValue = useRef();
+	const alertShown = useRef( false );
+
 	const spinnerStyle = usePreferredColorSchemeStyle(
 		styles.spinner,
 		styles.spinnerDark
 	);
 
-	const { reusableBlock, hasResolved, isEditing, settings } = useSelect(
+	const floatingToolbarAnimations = useFloatingToolbarAnimationsContext();
+
+	const {
+		originalReusableBlock,
+		reusableBlock,
+		hasEdits,
+		hasResolved,
+		isSaving,
+		edits,
+		hasSelectedInnerBlock,
+	} = useSelect(
 		( select ) => {
 			const { getSettings } = select( blockEditorStore );
 
 			return {
+				originalReusableBlock: select( coreStore ).getEntityRecord(
+					...recordArgs
+				),
 				reusableBlock: select( coreStore ).getEditedEntityRecord(
+					...recordArgs
+				),
+				edits: select( coreStore ).getEntityRecordEdits(
+					...recordArgs
+				),
+				hasEdits: select( coreStore ).hasEditsForEntityRecord(
 					...recordArgs
 				),
 				hasResolved: select( coreStore ).hasFinishedResolution(
@@ -81,10 +95,21 @@ export default function ReusableBlockEdit( {
 					reusableBlocksStore
 				).__experimentalIsEditingReusableBlock( clientId ),
 				settings: getSettings(),
+				hasSelectedInnerBlock: select(
+					blockEditorStore
+				).hasSelectedInnerBlock( clientId ),
 			};
 		},
 		[ ref, clientId ]
 	);
+
+	const { saveEditedEntityRecord, editEntityRecord } = useDispatch(
+		coreStore
+	);
+
+	const { selectBlock } = useDispatch( blockEditorStore );
+
+	const { createNotice } = useDispatch( noticesStore );
 
 	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
 		'postType',
@@ -92,41 +117,118 @@ export default function ReusableBlockEdit( {
 		{ id: ref }
 	);
 
-	function openSheet() {
-		setShowHelp( true );
-	}
+	const [ title, editTitle, fullTitle ] = useEntityProp(
+		'postType',
+		'wp_block',
+		'title',
+		ref
+	);
 
-	function closeSheet() {
-		setShowHelp( false );
-	}
+	/* translators: Changes saved notice message. %s: The reusable block name */
+	const saveMessageFormat = __( "Changes to '%s' saved" );
+	const saveMessage = sprintf( saveMessageFormat, title );
+	/* translators: Changes discarded notice message. %s: The reusable block name */
+	const discardMessageFormat = __( "Changes to '%s' discarded" );
+	const discardMessage = sprintf( discardMessageFormat, title );
+	/* translators: Unsaved changes alert title. */
+	const unsavedChangesTitle = __( 'Unsaved changes' );
+	/* translators: Unsaved changes alert message. %s: The reusable block name */
+	const unsavedChangesMessageFormat = __(
+		"Would you like to save or discard the changes in ''%s'?"
+	);
+	const unsavedChangesMessage = sprintf( unsavedChangesMessageFormat, title );
+	/* translators: Convert to regular blocks notice message. %s: The reusable block name */
+	const convertToRegularFormat = __( "''%s' converted to regular blocks" );
+	const convertToRegular = sprintf( convertToRegularFormat, title );
 
-	function renderSheet() {
-		const infoTitle =
-			Platform.OS === 'android'
-				? __(
-						"Reusable blocks aren't editable on WordPress for Android"
-				  )
-				: __( "Reusable blocks aren't editable on WordPress for iOS" );
+	const setTitle = ( value ) => {
+		editTitle( value );
+		if ( value !== fullTitle.raw && ! isEditing ) {
+			originalValue.current = { title, ...edits };
+			setIsEditing( true );
+		}
+	};
 
-		return (
-			<BottomSheet
-				isVisible={ showHelp }
-				hideHeader
-				onClose={ closeSheet }
-			>
-				<View style={ styles.infoContainer }>
-					<Icon
-						icon={ help }
-						color={ infoSheetIconStyle.color }
-						size={ styles.infoSheetIcon.size }
-					/>
-					<Text style={ [ infoTextStyle, infoTitleStyle ] }>
-						{ infoTitle }
-					</Text>
-				</View>
-			</BottomSheet>
+	const {
+		__experimentalConvertBlockToStatic: convertBlockToStatic,
+	} = useDispatch( reusableBlocksStore );
+
+	// Handlers
+	const enableEditMode = () => {
+		originalValue.current = { title, ...edits };
+		setIsEditing( true );
+	};
+
+	const disableEditMode = () => {
+		setIsEditing( false );
+		if ( hasSelectedInnerBlock ) selectBlock( clientId );
+	};
+
+	const discardChanges = () => {
+		const emptyEdit = Object.keys( edits ).reduce(
+			( carry, item ) => ( {
+				...carry,
+				[ item ]: originalValue.current[ item ],
+			} ),
+			{}
 		);
-	}
+		editEntityRecord( ...recordArgs, emptyEdit );
+		disableEditMode();
+		createNotice( 'info', discardMessage );
+	};
+
+	const saveChanges = () => {
+		saveEditedEntityRecord( ...recordArgs );
+		disableEditMode();
+		createNotice( 'info', saveMessage );
+	};
+
+	const convertToRegularBlocks = () => {
+		createNotice( 'info', convertToRegular );
+		convertBlockToStatic( clientId );
+	};
+
+	const shakeFloatingToolbar = useCallback(
+		() => floatingToolbarAnimations.shake(),
+		[ floatingToolbarAnimations ]
+	);
+
+	// Unsaved changes alert
+	useEffect( () => {
+		if (
+			isEditing &&
+			! ( isSelected || hasSelectedInnerBlock ) &&
+			! alertShown.current
+		) {
+			alertShown.current = true;
+
+			setImmediate( () =>
+				Alert.alert(
+					unsavedChangesTitle,
+					unsavedChangesMessage,
+					[
+						{
+							text: __( 'Discard' ),
+							onPress: () => {
+								alertShown.current = false;
+								discardChanges();
+							},
+							style: 'cancel',
+						},
+						{
+							text: __( 'Save' ),
+							onPress: () => {
+								alertShown.current = false;
+								saveChanges();
+							},
+							style: 'default',
+						},
+					],
+					{ cancelable: false }
+				)
+			);
+		}
+	}, [ isSelected, hasSelectedInnerBlock ] );
 
 	if ( ! hasResolved ) {
 		return (
@@ -142,35 +244,76 @@ export default function ReusableBlockEdit( {
 		);
 	}
 
-	const { title } = reusableBlock;
-	let element = (
-		<BlockEditorProvider
-			settings={ settings }
-			value={ blocks }
-			onChange={ onChange }
-			onInput={ onInput }
-		>
-			<BlockList withFooter={ false } marginHorizontal={ 0 } />
-		</BlockEditorProvider>
-	);
-
-	if ( ! isEditing ) {
-		element = <Disabled>{ element }</Disabled>;
-	}
-
 	return (
-		<TouchableWithoutFeedback
-			disabled={ ! isSelected }
-			accessibilityLabel={ __( 'Help button' ) }
-			accessibilityRole={ 'button' }
-			accessibilityHint={ __( 'Tap here to show help' ) }
-			onPress={ openSheet }
-		>
+		<View>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton
+						onClick={ convertToRegularBlocks }
+						label={ __( 'Convert to regular blocks' ) }
+						icon={ ungroup }
+						showTooltip
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			<InspectorControls>
+				<PanelBody title={ __( 'Reusable block settings' ) }>
+					<TextControl
+						label={ __( 'Name' ) }
+						value={ title }
+						onChange={ setTitle }
+						help={ __(
+							'Updates to block name are applied when changes to the block are saved'
+						) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			{ isSelected && ! isEditing && ! isSaving && (
+				<FloatingToolbar.Fill>
+					<ToolbarButton
+						title={ __( 'Edit' ) }
+						onClick={ enableEditMode }
+					>
+						<Text style={ styles.editButton }>
+							{ __( 'Edit' ) }
+						</Text>
+					</ToolbarButton>
+				</FloatingToolbar.Fill>
+			) }
+			{ isEditing && (
+				<FloatingToolbar.Fill>
+					<ToolbarButton
+						title={ __( 'Discard' ) }
+						onClick={ discardChanges }
+						icon={ <Icon icon={ close } fill={ '#FF0000' } /> }
+						style={ { backgroundColor: 'red' } }
+					/>
+					<ToolbarButton
+						title={ __( 'Save' ) }
+						onClick={ saveChanges }
+						icon={ <Icon icon={ check } fill={ '#00FF00' } /> }
+					/>
+				</FloatingToolbar.Fill>
+			) }
+			{ isSaving && (
+				<FloatingToolbar.Fill>
+					<ActivityIndicator style={ styles.toolbarSpinner } />
+				</FloatingToolbar.Fill>
+			) }
 			<View>
-				{ isSelected && <EditTitle title={ title } /> }
-				{ element }
-				{ renderSheet() }
+				<Disabled
+					isDisabled={ isSelected && ( ! isEditing || isSaving ) }
+					onPressDisabled={ shakeFloatingToolbar }
+				>
+					<InnerBlocks
+						value={ blocks }
+						onChange={ onChange }
+						onInput={ onInput }
+					/>
+				</Disabled>
 			</View>
-		</TouchableWithoutFeedback>
+		</View>
 	);
 }
+
+export default ReusableBlockEdit;
