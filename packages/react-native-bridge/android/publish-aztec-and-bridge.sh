4c67a6c540ba2fda7776c9e3b93d7f8381bf80d4
Oguz Kocer <oguz.kocer@automattic.com>
2021-03-05 15:35:52 -0500

    Adds delay between publishing aztec and bridge android binaries

--- packages/react-native-bridge/android/publish-aztec-and-bridge.sh
+++ packages/react-native-bridge/android/publish-aztec-and-bridge.sh
@@ -32,33 +32,14 @@ echo "Publishing 'react-native-aztec' version '$VERSION'"
 
 if [ $? -eq 0 ]; then
 	echo "Successfully published 'react-native-aztec' version '$VERSION'."
+	echo "Wait 30 seconds for the new 'react-native-aztec' version to be available"
+	sleep 30
 else
 	echo "Failed to publish 'react-native-aztec' version '$VERSION'."
-	exit 1
+	echo "We'll still proceed to build and publish 'react-native-bridge' since this failure might be due to this version being published already, for example when the CI task is restarted."
 fi
 
-# 5. Wait for the new version of `react-native-aztec` to be available
-BINTRAY_AZTEC_URL="https://bintray.com/api/ui/download/wordpress-mobile/maven/com/github/wordpress-mobile/gutenberg-mobile/react-native-aztec/$VERSION/react-native-aztec-$VERSION.pom"
-n=0
-while true; do
-	bintray_aztec_status_code=$(curl -s -o /dev/null -w "%{http_code}" $BINTRAY_AZTEC_URL)
-	if [[ $bintray_aztec_status_code == 302 ]]; then
-		# Found
-		break
-	fi
-
-	n=$((n+1))
-	echo "n is $n"
-	if [[ n -gt 4 ]]; then
-		echo "After 5 delayed attempts, 'react-native-aztec' library is still not available in Bintray, so we can't proceed with publishing the 'react-native-bridge' library"
-		exit 1
-	fi
-	echo "'react-native-aztec' version '$VERSION' is not yet available, retrying in 15 seconds"
-	sleep 15
-done
-
-
-# 6. Publish 'react-native-bridge` library to Bintray
+# 5. Publish 'react-native-bridge` library to Bintray
 echo "Publishing react-native-bridge version '$VERSION'"
 ./gradlew :react-native-bridge:bintrayUpload -q -PpublishReactNativeBridgeVersion=$VERSION -PreactNativeAztecVersion=$VERSION
 
@@ -66,4 +47,5 @@ if [ $? -eq 0 ]; then
 	echo "Successfully published 'react-native-bridge' version '$VERSION'."
 else
 	echo "Failed to publish 'react-native-bridge' version '$VERSION'."
+	exit 1
 fi
