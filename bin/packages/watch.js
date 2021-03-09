b055f228e694667babb6f91d5153e4a5e383c88c
Daniel Richards <daniel.richards@automattic.com>
2021-03-09 13:16:17 +0800

    Update watch task to a single call to watch function

--- bin/packages/watch.js
+++ bin/packages/watch.js
@@ -6,13 +6,12 @@ const watch = require( 'node-watch' );
 const { spawn } = require( 'child_process' );
 const path = require( 'path' );
 const chalk = require( 'chalk' );
-
 /**
  * Internal dependencies
  */
-const getPackages = require( './get-packages' );
-
 const BUILD_SCRIPT = path.resolve( __dirname, './build.js' );
+
+const PACKAGES_DIR = path.resolve( __dirname, '../../packages' );
 
 let filesToBuild = new Map();
 
@@ -26,63 +25,55 @@ const exists = ( filename ) => {
 // Exclude test files including .js files inside of __tests__ or test folders
 // and files with a suffix of .test or .spec (e.g. blocks.test.js),
 // and deceitful source-like files, such as editor swap files.
-const isSourceFile = ( filename ) => {
-	return (
+const isSourceFile = ( filename, skip ) => {
+	const isSource =
+		/.\.(js|json|scss)$/.test( filename ) &&
+		/\/src\/.+/.test( filename ) &&
 		! [
-			/\/(benchmark|__mocks__|__tests__|test|storybook|stories)\/.+.js$/,
+			/\/(benchmark|__mocks__|__tests__|test|storybook|stories)\/.+/,
 			/.\.(spec|test)\.js$/,
-		].some( ( regex ) => regex.test( filename ) ) &&
-		/.\.(js|json|scss)$/.test( filename )
-	);
+		].some( ( regex ) => regex.test( filename ) );
+
+	return isSource ? true : skip;
 };
+
+function getSrcDirectory( filename ) {
+	const matches = /packages\/(.+)\/src\/.+/g.exec( filename );
+	if ( ! matches || ! matches?.length ) {
+		return;
+	}
+	const [ , srcDirectory ] = matches;
+	return srcDirectory;
+}
 
 const rebuild = ( filename ) => filesToBuild.set( filename, true );
 
-getPackages().forEach( ( p ) => {
-	const srcDir = path.resolve( p, 'src' );
-	try {
-		fs.accessSync( srcDir, fs.F_OK );
-		watch(
-			path.resolve( p, 'src' ),
-			{ recursive: true, delay: 500 },
-			( event, filename ) => {
-				if ( ! isSourceFile( filename ) ) {
-					return;
-				}
-
-				const filePath = path.resolve( srcDir, filename );
-				if ( event === 'update' && exists( filePath ) ) {
-					console.log(
-						chalk.green( '->' ),
-						`${ event }: ${ filename }`
-					);
-					rebuild( filePath );
-				} else {
-					const buildFile = path.resolve(
-						srcDir,
-						'..',
-						'build',
-						filename
-					);
-					try {
-						fs.unlinkSync( buildFile );
-						process.stdout.write(
-							chalk.red( '  \u2022 ' ) +
-								path.relative(
-									path.resolve( srcDir, '..', '..' ),
-									buildFile
-								) +
-								' (deleted)' +
-								'\n'
-						);
-					} catch ( e ) {}
-				}
-			}
-		);
-	} catch ( e ) {
-		// doesn't exist
+watch(
+	PACKAGES_DIR,
+	{ recursive: true, delay: 500, filter: isSourceFile },
+	( event, filename ) => {
+		const filePath = path.resolve( PACKAGES_DIR, filename );
+		const srcDir = getSrcDirectory( filePath );
+		if ( event === 'update' && exists( filePath ) ) {
+			console.log( chalk.green( '->' ), `${ event }: ${ filePath }` );
+			rebuild( filePath );
+		} else {
+			const buildFile = path.resolve( srcDir, '..', 'build', filename );
+			try {
+				fs.unlinkSync( buildFile );
+				process.stdout.write(
+					chalk.red( '  \u2022 ' ) +
+						path.relative(
+							path.resolve( srcDir, '..', '..' ),
+							buildFile
+						) +
+						' (deleted)' +
+						'\n'
+				);
+			} catch ( e ) {}
+		}
 	}
-} );
+);
 
 setInterval( () => {
 	const files = Array.from( filesToBuild.keys() );
