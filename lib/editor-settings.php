30deb867c7f095b1d5097a5667b93e4454f24f32
Emmanuel Hesry <ehesry@gmail.com>
2021-03-09 18:46:11 +0100

    Make $editor_name and $editor_script_handle parameters of gutenberg_initialize_editor() function

--- lib/editor-settings.php
+++ lib/editor-settings.php
@@ -85,30 +85,27 @@ add_filter( 'block_editor_settings', 'gutenberg_extend_post_editor_settings' );
 /**
  * Initialize a block-based editor.
  *
- * @param array $settings {
+ * @param string $editor_name          Editor name.
+ * @param string $editor_script_handle Editor script handle.
+ * @param array  $settings {
  *      Elements to initialize a block-based editor.
  *
- *      @type array  $preload_paths        paths need to be preloaded.
- *      @type string $editor_name          editor name.
- *      @type string $editor_script_handle editor script handle.
- *      @type string $initializer_name     editor initialization function name.
- *      @type array  $editor_settings      editor settings.
+ *      @type array  $preload_paths        Array of paths to preload.
+ *      @type string $initializer_name     Editor initialization function name.
+ *      @type array  $editor_settings      Editor settings.
  * }
  * @return void
  */
-function gutenberg_initialize_editor( $settings ) {
+function gutenberg_initialize_editor( $editor_name, $editor_script_handle, $settings ) {
 
 	$defaults = array(
-		'preload_paths'        => array(),
-		'editor_name'          => 'widgets_editor',
-		'editor_script_handle' => 'edit-widgets',
-		'initializer_name'     => 'initialize',
-		'editor_settings'      => array(),
+		'preload_paths'    => array(),
+		'initializer_name' => 'initialize',
+		'editor_settings'  => array(),
 	);
 
 	$settings = wp_parse_args( $settings, $defaults );
 
-	$editor_name = $settings['editor_name'];
 	/**
 	 * Preload common data by specifying an array of REST API paths that will be preloaded.
 	 *
@@ -132,12 +129,12 @@ function gutenberg_initialize_editor( $settings ) {
 		'after'
 	);
 	wp_add_inline_script(
-		"wp-{$settings['editor_script_handle']}",
+		"wp-{$editor_script_handle}",
 		sprintf(
 			'wp.domReady( function() {
 				wp.%s.%s( "%s", %s );
 			} );',
-			lcfirst( str_replace( '-', '', ucwords( $settings['editor_script_handle'], '-' ) ) ),
+			lcfirst( str_replace( '-', '', ucwords( $editor_script_handle, '-' ) ) ),
 			$settings['initializer_name'],
 			str_replace( '_', '-', $editor_name ),
 			wp_json_encode( $settings['editor_settings'] )
