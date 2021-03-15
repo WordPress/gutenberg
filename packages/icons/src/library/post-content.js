4490b4f70d84da7f9c64c4b0c34967bdef543873
jasmussen <joen@automattic.com>
2021-03-15 11:57:31 +0100

    Update post content icon, unuse justify.

--- packages/icons/src/library/post-content.js
+++ packages/icons/src/library/post-content.js
@@ -3,10 +3,10 @@
  */
 import { SVG, Path } from '@wordpress/primitives';
 
-const alignJustify = (
+const postContent = (
 	<SVG xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M4 12.8h16v-1.5H4v1.5zm0 7h12.4v-1.5H4v1.5zM4 4.3v1.5h16V4.3H4z" />
+		<Path d="M4 20h16v-1.5H4V20zm0-4.8h16v-1.5H4v1.5zm0-6.4v1.5h16V8.8H4zM16 4H4v1.5h12V4z" />
 	</SVG>
 );
 
-export default alignJustify;
+export default postContent;
