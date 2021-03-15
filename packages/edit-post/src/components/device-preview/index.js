f1d3a8c76cd9e32035e9e738a40202230cb99f4c
dufresnesteven <steve.dufresne@automattic.com>
2021-03-15 15:41:50 +0900

    Move viewable check up to the Device Preview container.

--- packages/edit-post/src/components/device-preview/index.js
+++ packages/edit-post/src/components/device-preview/index.js
@@ -1,12 +1,18 @@
 /**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { Icon, MenuGroup } from '@wordpress/components';
-import { PostPreviewButton } from '@wordpress/editor';
+import { PostPreviewButton, store as editorStore } from '@wordpress/editor';
 import { external } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { __experimentalPreviewOptions as PreviewOptions } from '@wordpress/block-editor';
 import { useDispatch, useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
@@ -19,17 +25,22 @@ export default function DevicePreview() {
 		isPostSaveable,
 		isSaving,
 		deviceType,
-	} = useSelect(
-		( select ) => ( {
+		isViewable,
+	} = useSelect( ( select ) => {
+		const postType = select( coreStore ).getPostType(
+			select( editorStore ).getEditedPostAttribute( 'type' )
+		);
+
+		return {
 			hasActiveMetaboxes: select( editPostStore ).hasMetaBoxes(),
 			isSaving: select( editPostStore ).isSavingMetaBoxes(),
-			isPostSaveable: select( 'core/editor' ).isEditedPostSaveable(),
+			isPostSaveable: select( editorStore ).isEditedPostSaveable(),
 			deviceType: select(
 				editPostStore
 			).__experimentalGetPreviewDeviceType(),
-		} ),
-		[]
-	);
+			isViewable: get( postType, [ 'viewable' ], false ),
+		};
+	}, [] );
 	const {
 		__experimentalSetPreviewDeviceType: setPreviewDeviceType,
 	} = useDispatch( editPostStore );
@@ -41,24 +52,26 @@ export default function DevicePreview() {
 			deviceType={ deviceType }
 			setDeviceType={ setPreviewDeviceType }
 		>
-			<MenuGroup>
-				<div className="edit-post-header-preview__grouping-external">
-					<PostPreviewButton
-						className={
-							'edit-post-header-preview__button-external'
-						}
-						role="menuitem"
-						forceIsAutosaveable={ hasActiveMetaboxes }
-						forcePreviewLink={ isSaving ? null : undefined }
-						textContent={
-							<>
-								{ __( 'Preview in new tab' ) }
-								<Icon icon={ external } />
-							</>
-						}
-					/>
-				</div>
-			</MenuGroup>
+			{ isViewable && (
+				<MenuGroup>
+					<div className="edit-post-header-preview__grouping-external">
+						<PostPreviewButton
+							className={
+								'edit-post-header-preview__button-external'
+							}
+							role="menuitem"
+							forceIsAutosaveable={ hasActiveMetaboxes }
+							forcePreviewLink={ isSaving ? null : undefined }
+							textContent={
+								<>
+									{ __( 'Preview in new tab' ) }
+									<Icon icon={ external } />
+								</>
+							}
+						/>
+					</div>
+				</MenuGroup>
+			) }
 		</PreviewOptions>
 	);
 }
