64359b1374f09d8822e246fa019aa70ba0af3f81
Siobhan <siobhan@automattic.com>
2021-03-15 12:09:46 +0000

    Update featured-banner.native.js
    With this commit, I'm updating featured-banner.native.js with the latest changes to that component's UI.

--- packages/components/src/mobile/image/featured-banner.native.js
+++ packages/components/src/mobile/image/featured-banner.native.js
@@ -7,7 +7,6 @@ import { View, Text } from 'react-native';
  * WordPress dependencies
  */
 import { Component } from '@wordpress/element';
-import { subscribeFeaturedImageIdChange } from '@wordpress/react-native-bridge';
 // import { __ } from '@wordpress/i18n';
 
 /**
@@ -16,50 +15,10 @@ import { subscribeFeaturedImageIdChange } from '@wordpress/react-native-bridge';
 import styles from './style.scss';
 
 export class FeaturedBanner extends Component {
-	constructor( props ) {
-		super( props );
-
-		this.featuredImageIdChange = this.featuredImageIdChange.bind( this );
-	}
-
-	featuredImageIdChange() {}
-
-	// This code successfully retrieves a featured image's ID when uncommented.
-	//	featuredImageIdChange( payload ) {
-	//				const { featuredImageId } = this.props;
-	//				console.log( 'ID from featured banner: ' + payload.featuredImageId );
-	//	}
-
-	componentDidMount() {
-		this.addFeaturedImageIdListener();
-	}
-
-	componentWillUnmount() {
-		this.removeFeaturedImageIdListener();
-	}
-
-	addFeaturedImageIdListener() {
-		//if we already have a subscription not worth doing it again
-		if ( this.subscriptionParentFeaturedImageIdChange ) {
-			return;
-		}
-		this.subscriptionParentFeaturedImageIdChange = subscribeFeaturedImageIdChange(
-			( payload ) => {
-				this.featuredImageIdChange( payload );
-			}
-		);
-	}
-
-	removeFeaturedImageIdListener() {
-		if ( this.subscriptionParentFeaturedImageIdChange ) {
-			this.subscriptionParentFeaturedImageIdChange.remove();
-		}
-	}
-
 	render() {
 		return (
-			<View>
-				<Text styles={ styles.featuredBanner }>Featured</Text>
+			<View style={ styles.featuredBannerContainer }>
+				<Text style={ styles.featuredBanner }>Featured</Text>
 			</View>
 		);
 	}
