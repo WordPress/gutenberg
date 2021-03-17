c1a77dddded5cfcc5c5f798f7ac246e3e201686b
Andrei Draganescu <github@andreidraganescu.info>
2021-03-17 15:06:59 +0000

    enable listview, reorder items

--- packages/edit-navigation/src/filters/remove-edit-unsupported-features.js
+++ packages/edit-navigation/src/filters/remove-edit-unsupported-features.js
@@ -15,7 +15,7 @@ const removeNavigationBlockEditUnsupportedFeatures = createHigherOrderComponent(
 				{ ...props }
 				hasSubmenuIndicatorSetting={ false }
 				hasItemJustificationControls={ false }
-				hasListViewModal={ false }
+				hasListViewModal={ true }
 			/>
 		);
 	},
