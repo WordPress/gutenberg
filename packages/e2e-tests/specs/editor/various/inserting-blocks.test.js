95c202fe36f63f2ee7c51e4e4a2afc99af70093f
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-03-18 13:33:38 +0100

    E2E tests: Consolidate specs for the inserter

--- packages/e2e-tests/specs/editor/various/inserting-blocks.test.js
+++ packages/e2e-tests/specs/editor/various/inserting-blocks.test.js
@@ -2,13 +2,14 @@
  * WordPress dependencies
  */
 import {
-	createNewPost,
-	insertBlock,
-	getEditedPostContent,
-	pressKeyTimes,
-	setBrowserViewport,
 	closeGlobalBlockInserter,
+	createNewPost,
+	getEditedPostContent,
+	insertBlock,
+	openGlobalBlockInserter,
+	pressKeyTimes,
 	searchForBlock,
+	setBrowserViewport,
 	showBlockToolbar,
 } from '@wordpress/e2e-test-utils';
 
@@ -372,4 +373,39 @@ describe( 'adding blocks', () => {
 		);
 		expect( isFocusInBlock ).toBe( true );
 	} );
+
+	it( 'shows block preview when hovering over block in inserter', async () => {
+		await openGlobalBlockInserter();
+		await page.focus( '.editor-block-list-item-paragraph' );
+		const preview = await page.waitForSelector(
+			'.block-editor-inserter__preview',
+			{
+				visible: true,
+			}
+		);
+		const isPreviewVisible = await preview.isIntersectingViewport();
+		expect( isPreviewVisible ).toBe( true );
+	} );
+
+	it.each( [ 'large', 'small' ] )(
+		'last-inserted block should be given and keep the focus (%s viewport)',
+		async ( viewport ) => {
+			await setBrowserViewport( viewport );
+
+			await page.type(
+				'.block-editor-default-block-appender__content',
+				'Testing inserted block focus'
+			);
+
+			await insertBlock( 'Image' );
+
+			await page.waitForSelector( 'figure[data-type="core/image"]' );
+
+			const selectedBlock = await page.evaluate( () => {
+				return wp.data.select( 'core/block-editor' ).getSelectedBlock();
+			} );
+
+			expect( selectedBlock.name ).toBe( 'core/image' );
+		}
+	);
 } );
