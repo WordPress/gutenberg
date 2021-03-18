e5f725c68a2081afeacb56262c6445358c06e8e1
Ari Stathopoulos <aristath@gmail.com>
2021-03-18 14:57:18 +0200

    do the camel

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -38,7 +38,7 @@ import * as navigation from './navigation';
 import * as navigationLink from './navigation-link';
 import * as latestComments from './latest-comments';
 import * as latestPosts from './latest-posts';
-import * as loginOut from './loginout';
+import * as logInOut from './loginout';
 import * as list from './list';
 import * as missing from './missing';
 import * as more from './more';
@@ -228,7 +228,7 @@ export const __experimentalRegisterExperimentalCoreBlocks =
 								queryPaginationNext,
 								queryPaginationNumbers,
 								queryPaginationPrevious,
-								loginOut,
+								logInOut,
 								postTitle,
 								postContent,
 								postAuthor,
