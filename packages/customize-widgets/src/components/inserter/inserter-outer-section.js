92b1f65746e07fa08f5dd498309c25287d9dc760
Kai Hao <kevin830726@gmail.com>
2021-03-19 18:39:24 +0800

    Handle multiple outer sections

--- packages/customize-widgets/src/components/inserter/inserter-outer-section.js
+++ packages/customize-widgets/src/components/inserter/inserter-outer-section.js
@@ -11,6 +11,31 @@ export function getContainer() {
 }
 
 export default function initializeInserterOuterSection() {
+	const OuterSection = customize.OuterSection;
+	// Override the OuterSection class to handle multiple outer sections.
+	// It closes all the other outer sections whenever one is opened.
+	// The result is that at most one outer section can be opened at the same time.
+	customize.OuterSection = class extends OuterSection {
+		onChangeExpanded( expanded, args ) {
+			if ( expanded ) {
+				customize.section.each( ( section ) => {
+					if (
+						section.params.type === 'outer' &&
+						section.id !== this.id
+					) {
+						if ( section.expanded() ) {
+							section.collapse();
+						}
+					}
+				} );
+			}
+
+			return super.onChangeExpanded( expanded, args );
+		}
+	};
+	// Handle constructor so that "params.type" can be correctly pointed to "outer".
+	customize.sectionConstructor.outer = customize.OuterSection;
+
 	const outerSection = new customize.OuterSection(
 		inserterOuterSectionId,
 		{}
