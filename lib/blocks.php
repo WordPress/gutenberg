1d82324d95f9ca07ccc567051644d35dccd7754b
Matías Ventura <mv@matiasventura.com>
2021-03-19 15:59:52 +0100

    Update todo comment.

--- lib/blocks.php
+++ lib/blocks.php
@@ -345,7 +345,12 @@ function gutenberg_register_legacy_social_link_blocks() {
 
 add_action( 'init', 'gutenberg_register_legacy_social_link_blocks' );
 
-// todo: move to trac ticket
+/**
+ * Filters the default block categories array to add a new one for themes.
+ * Should be removed and turned into a core.trac ticket for merge.
+ *
+ * @param array $categories The list of default block categories.
+ */
 function gutenberg_register_theme_block_category( $categories ) {
 	$categories[] = array(
 		'slug'  => 'theme',
