6cfa651d7fd3d2b404eff5595ec97fce1ac1bb8e
David Calhoun <dpcalhoun@gmail.com>
2021-03-18 22:53:28 -0500

    Clean up integration test

--- packages/block-library/src/cover/test/edit.native.js
+++ packages/block-library/src/cover/test/edit.native.js
@@ -2,12 +2,9 @@
  * External dependencies
  */
 import { Image } from 'react-native';
-import {
-	render as rtlRender,
-	fireEvent,
-	waitFor,
-} from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import { act } from 'react-test-renderer';
+import { useDarkModeContext, eventEmitter } from 'react-native-dark-mode';
 
 /**
  * WordPress dependencies
@@ -23,15 +20,7 @@ import { requestMediaPicker } from '@wordpress/react-native-bridge';
 import { IMAGE_BACKGROUND_TYPE } from '../shared';
 import { metadata, settings, name } from '../index';
 
-function render( props ) {
-	return rtlRender(
-		<SlotFillProvider>
-			<BlockEdit isSelected name={ name } clientId={ 0 } { ...props } />
-			<BottomSheetSettings isVisible />
-		</SlotFillProvider>
-	);
-}
-
+// Avoid errors due to mocked stylesheet files missing required selectors
 jest.mock( '@wordpress/compose', () => ( {
 	...jest.requireActual( '@wordpress/compose' ),
 	withPreferredColorScheme: jest.fn( ( Component ) => ( props ) => (
@@ -43,25 +32,32 @@ jest.mock( '@wordpress/compose', () => ( {
 	) ),
 } ) );
 
-// Mock Image.getSize to avoid unhandled async callback
-const getSizemock = jest.spyOn( Image, 'getSize' );
-const imageSize = Promise.resolve();
-getSizemock.mockReturnValue( imageSize );
+// Simplified tree to render Cover edit within slot
+const CoverEdit = ( props ) => (
+	<SlotFillProvider>
+		<BlockEdit isSelected name={ name } clientId={ 0 } { ...props } />
+		<BottomSheetSettings isVisible />
+	</SlotFillProvider>
+);
 
 const setAttributes = jest.fn();
-const MOCK_URL = 'mock-url';
-const MOCK_FOCAL_POINT = { x: '0.5', y: '0.5' };
-
 const attributes = {
 	backgroundType: IMAGE_BACKGROUND_TYPE,
+	focalPoint: { x: '0.5', y: '0.5' },
 	hasParallax: false,
-	focalPoint: MOCK_FOCAL_POINT,
 	onFocalPointChange: jest.fn(),
 	overlayColor: { color: '#000000' },
-	url: MOCK_URL,
+	url: 'mock-url',
 };
 
+let imageSize;
 beforeAll( () => {
+	// Mock Image.getSize to avoid act warning for unhandled async callback
+	const getSizemock = jest.spyOn( Image, 'getSize' );
+	imageSize = Promise.resolve();
+	getSizemock.mockReturnValue( imageSize );
+
+	// Register required blocks
 	registerBlockType( name, {
 		...metadata,
 		...settings,
@@ -84,21 +80,26 @@ beforeAll( () => {
 } );
 
 afterAll( () => {
+	// Restore mocks
+	Image.getSize.mockRestore();
+
+	// Clean up registered blocks
 	unregisterBlockType( name );
 	unregisterBlockType( 'core/paragraph' );
-	Image.getSize.mockRestore();
 } );
 
 describe( 'when no media is attached', () => {
 	it( 'allows adding an image or video', async () => {
-		const { getByText, findByText, debug } = render( {
-			attributes: {
-				...attributes,
-				url: undefined,
-				backgroundType: undefined,
-			},
-			setAttributes,
-		} );
+		const { getByText, findByText, debug } = render(
+			<CoverEdit
+				attributes={ {
+					...attributes,
+					url: undefined,
+					backgroundType: undefined,
+				} }
+				setAttributes={ setAttributes }
+			/>
+		);
 		fireEvent.press( getByText( 'Add image or video' ) );
 
 		const mediaLibraryButton = await findByText(
@@ -107,21 +108,25 @@ describe( 'when no media is attached', () => {
 		fireEvent.press( mediaLibraryButton );
 
 		expect( requestMediaPicker ).toHaveBeenCalled();
+
 		await act( () => imageSize );
 	} );
 } );
 
 describe( 'when media is attached', () => {
 	it( 'allow editing the focal point', async () => {
-		const { getByText } = render( {
-			attributes,
-			setAttributes,
-		} );
+		const { getByText } = render(
+			<CoverEdit
+				attributes={ attributes }
+				setAttributes={ setAttributes }
+			/>
+		);
 		fireEvent.press( getByText( 'Edit focal point' ) );
 
 		await waitFor( () =>
 			expect( getByText( 'X-Axis Position' ) ).toBeTruthy()
 		);
+
 		await act( () => imageSize );
 	} );
 } );
