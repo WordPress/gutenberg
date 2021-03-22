fbf6bdf6751ccbffb66c38b520548936dfa3f6ad
Kai Hao <kevin830726@gmail.com>
2021-03-22 15:22:21 +0800

    Move patch outside

--- packages/customize-widgets/src/components/inserter/inserter-outer-section.js
+++ packages/customize-widgets/src/components/inserter/inserter-outer-section.js
@@ -4,6 +4,31 @@ const {
 
 const inserterOuterSectionId = 'widgets-inserter';
 
+const OuterSection = customize.OuterSection;
+// Override the OuterSection class to handle multiple outer sections.
+// It closes all the other outer sections whenever one is opened.
+// The result is that at most one outer section can be opened at the same time.
+customize.OuterSection = class extends OuterSection {
+	onChangeExpanded( expanded, args ) {
+		if ( expanded ) {
+			customize.section.each( ( section ) => {
+				if (
+					section.params.type === 'outer' &&
+					section.id !== this.id
+				) {
+					if ( section.expanded() ) {
+						section.collapse();
+					}
+				}
+			} );
+		}
+
+		return super.onChangeExpanded( expanded, args );
+	}
+};
+// Handle constructor so that "params.type" can be correctly pointed to "outer".
+customize.sectionConstructor.outer = customize.OuterSection;
+
 export function getContainer() {
 	return document.getElementById(
 		`sub-accordion-section-${ inserterOuterSectionId }`
@@ -11,31 +36,6 @@ export function getContainer() {
 }
 
 export default function initializeInserterOuterSection() {
-	const OuterSection = customize.OuterSection;
-	// Override the OuterSection class to handle multiple outer sections.
-	// It closes all the other outer sections whenever one is opened.
-	// The result is that at most one outer section can be opened at the same time.
-	customize.OuterSection = class extends OuterSection {
-		onChangeExpanded( expanded, args ) {
-			if ( expanded ) {
-				customize.section.each( ( section ) => {
-					if (
-						section.params.type === 'outer' &&
-						section.id !== this.id
-					) {
-						if ( section.expanded() ) {
-							section.collapse();
-						}
-					}
-				} );
-			}
-
-			return super.onChangeExpanded( expanded, args );
-		}
-	};
-	// Handle constructor so that "params.type" can be correctly pointed to "outer".
-	customize.sectionConstructor.outer = customize.OuterSection;
-
 	const outerSection = new customize.OuterSection(
 		inserterOuterSectionId,
 		{}
