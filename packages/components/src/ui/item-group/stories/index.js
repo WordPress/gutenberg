9d5da2426a769e6dbe8dbfec96fad94aac692a1d
sarayourfriend <sarayourfriend@fastmail.com>
2021-03-22 12:36:47 -0700

    Remove collabsible story

--- packages/components/src/ui/item-group/stories/index.js
+++ packages/components/src/ui/item-group/stories/index.js
@@ -1,18 +1,9 @@
 /* eslint-disable no-alert */
 /* globals alert */
 /**
- * External dependencies
- */
-import { Avatar, Spacer } from '@wp-g2/components';
-
-/**
  * Internal dependencies
  */
 import { ItemGroup, Item } from '..';
-import { HStack } from '../../h-stack';
-import { View } from '../../view';
-import { VStack } from '../../v-stack';
-import { Text } from '../../text';
 import { Popover } from '../../popover';
 import { Button } from '../../button';
 
@@ -34,34 +25,6 @@ export const _default = () => (
 		</Item>
 		<Item action onClick={ () => alert( 'WordPress.org' ) }>
 			Code is Poetry — Click me!
-		</Item>
-	</ItemGroup>
-);
-
-const ExampleItemContent = () => (
-	<HStack spacing={ 3 }>
-		<View>
-			<Avatar name="WordPress" />
-		</View>
-		<Spacer>
-			<VStack spacing={ 1 }>
-				<Text weight="bold">Title</Text>
-				<Text variant="muted">Description</Text>
-			</VStack>
-		</Spacer>
-	</HStack>
-);
-
-export const nonAction = () => (
-	<ItemGroup css={ { width: '350px' } } bordered rounded size="large">
-		<Item>
-			<ExampleItemContent />
-		</Item>
-		<Item>
-			<ExampleItemContent />
-		</Item>
-		<Item>
-			<ExampleItemContent />
 		</Item>
 	</ItemGroup>
 );
