6136557571cf3b372bf858c8f03c953fee1da4f2
Daniel Richards <daniel.richards@automattic.com>
2021-03-23 16:34:06 +0800

    Open switch menu dropdown to center

--- packages/edit-navigation/src/components/header/index.js
+++ packages/edit-navigation/src/components/header/index.js
@@ -70,7 +70,7 @@ export default function Header( {
 						popoverProps={ {
 							className:
 								'edit-navigation-header__menu-switcher-dropdown',
-							position: 'bottom left',
+							position: 'bottom center',
 						} }
 					>
 						{ ( { onClose } ) => (
