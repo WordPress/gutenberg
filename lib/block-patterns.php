4c7fe17391a31b1c5b8811638e2d88dfa68e263d
Kjell Reigstad <kjell@kjellr.com>
2021-03-23 11:38:41 -0400

    Ensure "Columns" category is registered.

--- lib/block-patterns.php
+++ lib/block-patterns.php
@@ -206,6 +206,7 @@ foreach ( $core_block_patterns as $core_block_pattern ) {
 	);
 }
 
+register_block_pattern_category( 'columns', array( 'label' => _x( 'Columns', 'Block pattern category' ) ) );
 register_block_pattern_category( 'header', array( 'label' => _x( 'Headers', 'Block pattern category' ) ) );
 register_block_pattern_category( 'gallery', array( 'label' => _x( 'Gallery', 'Block pattern category' ) ) );
 register_block_pattern_category( 'text', array( 'label' => _x( 'Text', 'Block pattern category' ) ) );
