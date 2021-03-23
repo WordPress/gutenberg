6618a262537258b59083da0458216c7ff4d978b8
sarayourfriend <sarayourfriend@fastmail.com>
2021-03-23 09:16:32 -0700

    api-fetch: Add types to http-v1 middleware

--- packages/api-fetch/src/types.ts
+++ packages/api-fetch/src/types.ts
@@ -1,4 +1,5 @@
-export interface ApiFetchRequestProps {
+export interface ApiFetchRequestProps extends RequestInit {
+	// Override headers, we only accept it as an object due to the `nonce` middleware
 	headers?: Record< string, string >;
 	path?: string;
 	url?: string;
