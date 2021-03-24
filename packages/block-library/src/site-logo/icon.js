8b68949afa7a9b16e3c5ef863d0f081c3264cd2f
Matías Ventura <mv@matiasventura.com>
2021-03-24 21:32:19 +0100

    Update site-logo block icon.
    Props @pablohoneyhoney

--- packages/block-library/src/site-logo/icon.js
+++ packages/block-library/src/site-logo/icon.js
@@ -1,24 +1,24 @@
 /**
  * WordPress dependencies
  */
-import { SVG, Path } from '@wordpress/components';
+import { SVG, Path, Circle } from '@wordpress/components';
 
 export default (
 	<SVG width="24" height="24" viewBox="0 0 24 24">
-		<Path
-			d="M11 6C8.71429 6 7.19048 6.61538 7 6.92308V14L8.71429 12.7692C9.57143 12.1538 11.2857 12.1538 12.4286 12.7692C13.5714 13.3846 14.7143 13.6923 16.4286 13.3846C17.5559 13.1823 18.619 11.9487 19 11.2308C18.9048 11.2308 18.4857 11.1692 17.5714 10.9231C16.6571 10.6769 15.3723 9.2674 15.1818 8.85714C14.5152 7.72894 13.2857 6 11 6Z"
-			fill="black"
+		<Circle
+			cx="12"
+			cy="12"
+			r="7.25"
 			stroke="black"
 			strokeWidth="1.5"
+			fill="none"
 		/>
-		<line
-			x1="6.75"
-			y1="5.75"
-			x2="6.75"
-			y2="19.25"
+		<Path
+			d="M5.5 14.5L9.71429 12L12.5 13.3333L15.75 11L19 13.3333"
 			stroke="black"
 			strokeWidth="1.5"
-			strokeLinecap="round"
+			strokeLinejoin="round"
+			fill="none"
 		/>
 	</SVG>
 );
