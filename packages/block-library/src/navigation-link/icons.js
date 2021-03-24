c1ff3c086930c0803512e77ab42b5199bcb18642
jasmussen <joen@automattic.com>
2021-03-24 10:32:06 +0100

    Move submenu icon to library.

--- packages/block-library/src/navigation-link/icons.js
+++ packages/block-library/src/navigation-link/icons.js
@@ -3,12 +3,6 @@
  */
 import { Path, SVG } from '@wordpress/components';
 
-export const ToolbarSubmenuIcon = () => (
-	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-		<Path d="M2 12c0 3.6 2.4 5.5 6 5.5h.5V19l3-2.5-3-2.5v2H8c-2.5 0-4.5-1.5-4.5-4s2-4.5 4.5-4.5h3.5V6H8c-3.6 0-6 2.4-6 6zm19.5-1h-8v1.5h8V11zm0 5h-8v1.5h8V16zm0-10h-8v1.5h8V6z" />
-	</SVG>
-);
-
 export const ItemSubmenuIcon = () => (
 	<SVG
 		xmlns="http://www.w3.org/2000/svg"
