54d6668d3b9b374bb45a837cd1f771e6c34ffe50
Riad Benguella <benguella@gmail.com>
2021-03-25 10:27:15 +0100

    Quote block: Fix the editor/frontend style parity. (#30190)
    Co-authored-by: Ella van Durpe <ella@vandurpe.com>

--- packages/rich-text/src/component/index.js
+++ packages/rich-text/src/component/index.js
@@ -83,11 +83,17 @@ const INSERTION_INPUT_TYPES_TO_IGNORE = new Set( [
 const whiteSpace = 'pre-wrap';
 
 /**
+ * A minimum width of 1px will prevent the rich text container from collapsing
+ * to 0 width and hiding the caret. This is useful for inline containers.
+ */
+const minWidth = '1px';
+
+/**
  * Default style object for the editable element.
  *
  * @type {Object<string,string>}
  */
-const defaultStyle = { whiteSpace };
+const defaultStyle = { whiteSpace, minWidth };
 
 const EMPTY_ACTIVE_FORMATS = [];
 
