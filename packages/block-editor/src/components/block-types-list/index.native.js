ad768551b12eb795eae7e21e08dcc79aa0589ba9
Carlos Garcia <fluiddot@gmail.com>
2021-03-26 17:06:37 +0100

    Update BlockTypesList to functional component

--- packages/block-editor/src/components/block-types-list/index.native.js
+++ packages/block-editor/src/components/block-types-list/index.native.js
@@ -11,7 +11,7 @@ import {
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import { BottomSheet, InserterButton } from '@wordpress/components';
 
 /**
@@ -21,108 +21,77 @@ import styles from './style.scss';
 
 const MIN_COL_NUM = 3;
 
-export class BlockTypesList extends Component {
-	constructor() {
-		super( ...arguments );
+export default function BlockTypesList( { name, items, onSelect, listProps } ) {
+	const [ numberOfColumns, setNumberOfColumns ] = useState( MIN_COL_NUM );
+	const [ itemWidth, setItemWidth ] = useState();
+	const [ maxWidth, setMaxWidth ] = useState();
 
-		this.onLayout = this.onLayout.bind( this );
-		this.renderItem = this.renderItem.bind( this );
-
-		this.state = {
-			numberOfColumns: MIN_COL_NUM,
+	useEffect( () => {
+		Dimensions.addEventListener( 'change', onLayout );
+		return () => {
+			Dimensions.removeEventListener( 'change', onLayout );
 		};
+	}, [] );
 
-		Dimensions.addEventListener( 'change', this.onLayout );
-	}
-
-	componentWillUnmount() {
-		Dimensions.removeEventListener( 'change', this.onLayout );
-	}
-
-	calculateMinItemWidth( bottomSheetWidth ) {
-		const { paddingLeft, paddingRight } = styles.columnPadding;
-		return (
-			( bottomSheetWidth - 2 * ( paddingLeft + paddingRight ) ) /
-			MIN_COL_NUM
-		);
-	}
-
-	calculateItemWidth() {
+	function calculateItemWidth() {
 		const {
 			paddingLeft: itemPaddingLeft,
 			paddingRight: itemPaddingRight,
 		} = InserterButton.Styles.modalItem;
-		const { width: itemWidth } = InserterButton.Styles.modalIconWrapper;
-		return itemWidth + itemPaddingLeft + itemPaddingRight;
+		const { width } = InserterButton.Styles.modalIconWrapper;
+		return width + itemPaddingLeft + itemPaddingRight;
 	}
 
-	calculateColumnsProperties() {
+	function onLayout() {
+		const sumLeftRightPadding =
+			styles.columnPadding.paddingLeft +
+			styles.columnPadding.paddingRight;
+
 		const bottomSheetWidth = BottomSheet.getWidth();
-		const { paddingLeft, paddingRight } = styles.columnPadding;
-		const itemTotalWidth = this.calculateItemWidth();
-		const containerTotalWidth =
-			bottomSheetWidth - ( paddingLeft + paddingRight );
-		const numofColumns = Math.floor( containerTotalWidth / itemTotalWidth );
+		const containerTotalWidth = bottomSheetWidth - sumLeftRightPadding;
+		const itemTotalWidth = calculateItemWidth();
 
-		if ( numofColumns < MIN_COL_NUM ) {
-			return {
-				numOfColumns: MIN_COL_NUM,
-				itemWidth: this.calculateMinItemWidth( bottomSheetWidth ),
-				maxWidth: containerTotalWidth / MIN_COL_NUM,
-			};
+		const columnsFitToWidth = Math.floor(
+			containerTotalWidth / itemTotalWidth
+		);
+
+		const numColumns = Math.max( MIN_COL_NUM, columnsFitToWidth );
+
+		setNumberOfColumns( numColumns );
+		setMaxWidth( containerTotalWidth / numColumns );
+
+		if ( columnsFitToWidth < MIN_COL_NUM ) {
+			const updatedItemWidth =
+				( bottomSheetWidth - 2 * sumLeftRightPadding ) / MIN_COL_NUM;
+			setItemWidth( updatedItemWidth );
 		}
-		return {
-			numOfColumns: numofColumns,
-			maxWidth: containerTotalWidth / numofColumns,
-		};
 	}
 
-	onLayout() {
-		const {
-			numOfColumns,
-			itemWidth,
-			maxWidth,
-		} = this.calculateColumnsProperties();
-		const numberOfColumns = numOfColumns;
-
-		this.setState( { numberOfColumns, itemWidth, maxWidth } );
-	}
-
-	renderItem( { item } ) {
-		const { itemWidth, maxWidth } = this.state;
-		const { onSelect } = this.props;
-		return (
-			<InserterButton
-				item={ item }
-				itemWidth={ itemWidth }
-				maxWidth={ maxWidth }
-				onSelect={ onSelect }
-			/>
-		);
-	}
-
-	render() {
-		const { name, items, listProps } = this.props;
-		const { numberOfColumns } = this.state;
-
-		return (
-			<FlatList
-				onLayout={ this.onLayout }
-				key={ `InserterUI-${ name }-${ numberOfColumns }` } //re-render when numberOfColumns changes
-				keyboardShouldPersistTaps="always"
-				numColumns={ numberOfColumns }
-				data={ items }
-				ItemSeparatorComponent={ () => (
-					<TouchableWithoutFeedback accessible={ false }>
-						<View style={ styles.rowSeparator } />
-					</TouchableWithoutFeedback>
-				) }
-				keyExtractor={ ( item ) => item.id }
-				renderItem={ this.renderItem }
-				{ ...listProps }
-			/>
-		);
-	}
+	return (
+		<FlatList
+			onLayout={ onLayout }
+			key={ `InserterUI-${ name }-${ numberOfColumns }` } //re-render when numberOfColumns changes
+			keyboardShouldPersistTaps="always"
+			numColumns={ numberOfColumns }
+			data={ items }
+			initialNumToRender={ 3 }
+			ItemSeparatorComponent={ () => (
+				<TouchableWithoutFeedback accessible={ false }>
+					<View style={ styles.rowSeparator } />
+				</TouchableWithoutFeedback>
+			) }
+			keyExtractor={ ( item ) => item.name }
+			renderItem={ ( { item } ) => (
+				<InserterButton
+					{ ...{
+						item,
+						itemWidth,
+						maxWidth,
+						onSelect,
+					} }
+				/>
+			) }
+			{ ...listProps }
+		/>
+	);
 }
-
-export default BlockTypesList;
