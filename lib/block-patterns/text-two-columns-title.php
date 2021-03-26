334c2d20a120b42e2aec77fe35debe99d91c2840
Kjell Reigstad <kjell@kjellr.com>
2021-03-26 14:34:06 -0400

    Remove extra space from text.

--- lib/block-patterns/text-two-columns-title.php
+++ lib/block-patterns/text-two-columns-title.php
@@ -22,7 +22,7 @@ return array(
 	
 	<!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontSize":18}}} -->
-	<p style="font-size:18px"> ' . __( "The ship was making her way steadily through small waves which slapped her and then fizzled like effervescing water, leaving a little border of bubbles and foam on either side. The colourless October sky above was thinly clouded as if by the trail of wood-fire smoke, and the air was wonderfully salt and brisk. Indeed it was too cold to stand still. Mrs. Ambrose drew her arm within her husband's, and as they moved off it could be seen from the way in which her sloping cheek turned up to his that she had something private to communicate." ) . '</p>
+	<p style="font-size:18px">' . __( "The ship was making her way steadily through small waves which slapped her and then fizzled like effervescing water, leaving a little border of bubbles and foam on either side. The colourless October sky above was thinly clouded as if by the trail of wood-fire smoke, and the air was wonderfully salt and brisk. Indeed it was too cold to stand still. Mrs. Ambrose drew her arm within her husband's, and as they moved off it could be seen from the way in which her sloping cheek turned up to his that she had something private to communicate." ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns --></div></div>
