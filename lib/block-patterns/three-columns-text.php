ed279e19bbcf4a7380d37e4f243a1498cb775e93
Kjell Reigstad <kjell@kjellr.com>
2021-03-29 14:18:44 -0400

    Set line height for three columns pattern.

--- lib/block-patterns/three-columns-text.php
+++ lib/block-patterns/three-columns-text.php
@@ -10,8 +10,8 @@ return array(
 	'categories'  => array( 'columns,text' ),
 	'content'     => '<!-- wp:columns {"align":"full","backgroundColor":"white","textColor":"black"} -->
 	<div class="wp-block-columns alignfull has-black-color has-white-background-color has-text-color has-background"><!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px"}}} -->
-	<h3 style="font-size:24px"><strong><a href="http://wordpress.org" data-type="URL" data-id="wordpress.org">' . __( 'Virtual Tour ↗' ) . '</a></strong></h3>
+	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+	<h3 style="font-size:24px;line-height:1.3"><strong><a href="http://wordpress.org" data-type="URL" data-id="wordpress.org">' . __( 'Virtual Tour ↗' ) . '</a></strong></h3>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
@@ -20,8 +20,8 @@ return array(
 	<!-- /wp:column -->
 	
 	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px"}}} -->
-	<h3 style="font-size:24px"><strong><a href="https://wordpress.org">' . __( 'Current Shows ↗' ) . '</a></strong></h3>
+	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+	<h3 style="font-size:24px;line-height:1.3"><strong><a href="https://wordpress.org">' . __( 'Current Shows ↗' ) . '</a></strong></h3>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
@@ -30,8 +30,8 @@ return array(
 	<!-- /wp:column -->
 	
 	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px"}}} -->
-	<h3 style="font-size:24px"><strong><a href="https://wordpress.org">' . __( 'Useful Info ↗' ) . '</a></strong></h3>
+	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+	<h3 style="font-size:24px;line-height:1.3"><strong><a href="https://wordpress.org">' . __( 'Useful Info ↗' ) . '</a></strong></h3>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
