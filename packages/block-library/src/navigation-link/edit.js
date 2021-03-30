eeff17f2d9aaa15cc5476097c25f94fde1c53ebe
Ella van Durpe <ella@vandurpe.com>
2021-03-31 00:13:40 +0200

    RichText: always show placholder on focus

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -450,7 +450,6 @@ export default function NavigationLinkEdit( {
 							}
 							aria-label={ __( 'Navigation link text' ) }
 							placeholder={ itemLabelPlaceholder }
-							keepPlaceholderOnFocus
 							withoutInteractiveFormatting
 							allowedFormats={ [
 								'core/bold',
