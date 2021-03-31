1bafe1d38cfe02db5e8e83e7fcc12b5d28c1f639
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2021-03-31 17:12:01 +0200

    Revert "Put feature behind __DEV__ flag"
    This reverts commit 528a83fde6ca53b7d0126aea077f97ab047627fd.

--- packages/components/src/autocomplete/index.native.js
+++
@@ -1,8 +0,0 @@
-/**
- * Internal dependencies
- */
-import Autocomplete from './index.js';
-
-// Enable for development only
-// eslint-disable-next-line no-undef
-export default __DEV__ ? Autocomplete : () => null;
