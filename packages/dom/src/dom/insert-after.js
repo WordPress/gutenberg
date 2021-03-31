6156092019cee0ffe1a9c0f9a63331ff90b30c66
sarayourfriend <sarayourfriend@fastmail.com>
2021-03-31 06:50:14 -0700

    dom: Add types to clean-node-list

--- packages/dom/src/dom/insert-after.js
+++ packages/dom/src/dom/insert-after.js
@@ -7,8 +7,8 @@ import { assertIsDefined } from '../utils/assert-is-defined';
  * Given two DOM nodes, inserts the former in the DOM as the next sibling of
  * the latter.
  *
- * @param {Element} newNode       Node to be inserted.
- * @param {Element} referenceNode Node after which to perform the insertion.
+ * @param {Node} newNode       Node to be inserted.
+ * @param {Node} referenceNode Node after which to perform the insertion.
  * @return {void}
  */
 export default function insertAfter( newNode, referenceNode ) {
--- packages/dom/src/dom/remove.js
+++ packages/dom/src/dom/remove.js
@@ -6,7 +6,7 @@ import { assertIsDefined } from '../utils/assert-is-defined';
 /**
  * Given a DOM node, removes it from the DOM.
  *
- * @param {Element} node Node to be removed.
+ * @param {Node} node Node to be removed.
  * @return {void}
  */
 export default function remove( node ) {
