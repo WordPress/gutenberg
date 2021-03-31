9be1481da82f33dedeafebef2bbd148074bc5820
grzim <grzegorz.marzencki@a8c.com>
2021-03-31 23:43:18 +0200

    remove comment

--- packages/edit-navigation/src/hooks/use-menu-locations.js
+++ packages/edit-navigation/src/hooks/use-menu-locations.js
@@ -49,9 +49,6 @@ export default function useMenuLocations() {
 			if ( isMounted ) {
 				setMenuLocationsByName( newMenuLocationsByName );
 				takeLocationsSnapshot( newMenuLocationsByName );
-				// flow( [ setMenuLocationsByName, takeLocationsSnapshot ] )(
-				// 	newMenuLocationsByName
-				// );
 			}
 		};
 
