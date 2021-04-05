5b0e8230f51fa07a682fab1c8e86e1430d41ea90
Ari Stathopoulos <aristath@gmail.com>
2021-04-05 10:20:29 +0300

    add post-classes in the loop

--- packages/block-library/src/post-template/index.php
+++ packages/block-library/src/post-template/index.php
@@ -61,7 +61,7 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 				)
 			)
 		)->render( array( 'dynamic' => false ) );
-		$content      .= "<li>{$block_content}</li>";
+		$content      .= '<li class="' . esc_attr( implode( ' ', get_post_class() ) ) . '">' . $block_content . '</li>';
 	}
 
 	wp_reset_postdata();
