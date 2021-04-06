ffac58a8a00df03c397f53838d47fdc95898c04f
Dávid Szabó <david.szabo97@gmail.com>
2021-04-06 13:05:12 +0200

    Server side renderer: Fix errors in template part editor context (#29246)

--- packages/server-side-render/src/index.js
+++ packages/server-side-render/src/index.js
@@ -19,7 +19,11 @@ const ExportedServerSideRender = withSelect( ( select ) => {
 	const coreEditorSelect = select( 'core/editor' );
 	if ( coreEditorSelect ) {
 		const currentPostId = coreEditorSelect.getCurrentPostId();
-		if ( currentPostId ) {
+		// For templates and template parts we use a custom ID format.
+		// Since they aren't real posts, we don't want to use their ID
+		// for server-side rendering. Since they use a string based ID,
+		// we can assume real post IDs are numbers.
+		if ( currentPostId && typeof currentPostId.id === 'number' ) {
 			return {
 				currentPostId,
 			};
