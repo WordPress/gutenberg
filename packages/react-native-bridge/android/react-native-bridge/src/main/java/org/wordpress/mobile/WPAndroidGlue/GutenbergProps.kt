7102a514094a2f92dcf38f6b2ad6cd2c91c27972
Antonis Lilis <antonis.lilis@automattic.com>
2021-04-06 22:05:43 +0300

    Adds new Prop for Global Styles Settings

--- packages/react-native-bridge/android/react-native-bridge/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
+++ packages/react-native-bridge/android/react-native-bridge/src/main/java/org/wordpress/mobile/WPAndroidGlue/GutenbergProps.kt
@@ -33,6 +33,8 @@ data class GutenbergProps @JvmOverloads constructor(
         editorTheme?.also { theme ->
             theme.getSerializable(PROP_COLORS)?.let { putSerializable(PROP_COLORS, it) }
             theme.getSerializable(PROP_GRADIENTS)?.let { putSerializable(PROP_GRADIENTS, it) }
+            theme.getSerializable(PROP_GLOBAL_STYLES)
+                    ?.let { putSerializable(PROP_GLOBAL_STYLES, it) }
         }
     }
 
@@ -63,6 +65,7 @@ data class GutenbergProps @JvmOverloads constructor(
         private const val PROP_TRANSLATIONS = "translations"
         private const val PROP_COLORS = "colors"
         private const val PROP_GRADIENTS = "gradients"
+        private const val PROP_GLOBAL_STYLES = "rawGlobalStylesBaseStyles"
 
         const val PROP_CAPABILITIES = "capabilities"
         const val PROP_CAPABILITIES_CONTACT_INFO_BLOCK = "contactInfoBlock"
