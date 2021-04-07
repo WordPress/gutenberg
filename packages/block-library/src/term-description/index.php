aa1a98a5a8c76c1886a4de9a30d03008d563c5ad
Carolina Nymark <myazalea@hotmail.com>
2021-04-07 15:18:20 +0200

    Term Description block: Don't output if there's no description
    Check if the term description is empty, and if it is empty, return early.

--- packages/block-library/src/term-description/index.php
+++ packages/block-library/src/term-description/index.php
@@ -13,8 +13,9 @@
  * @return string Returns the filtered post content of the current post.
  */
 function render_block_core_term_description( $attributes ) {
+	$term_description = term_description();
 
-	if ( ! is_category() && ! is_tag() && ! is_tax() ) {
+	if ( ! is_category() && ! is_tag() && ! is_tax() || empty( $term_description ) ) {
 		return '';
 	}
 
@@ -23,7 +24,7 @@ function render_block_core_term_description( $attributes ) {
 		: array();
 	$wrapper_attributes = get_block_wrapper_attributes( $extra_attributes );
 
-	return '<div ' . $wrapper_attributes . '>' . term_description() . '</div>';
+	return '<div ' . $wrapper_attributes . '>' . $term_description . '</div>';
 }
 
 /**
