9e28f88ba521d30374c61de37c6d8f4dfa0af71e
André <andres.maneiro@automattic.com>
2021-04-08 09:34:13 +0200

    Add util to transfrom from v0 to v1

--- lib/load.php
+++ lib/load.php
@@ -89,6 +89,7 @@ if ( ! class_exists( 'WP_Block_Template ' ) ) {
 
 // These are used by some FSE features
 // as well as global styles.
+require __DIR__ . '/class-wp-theme-json-schema-v0.php';
 require __DIR__ . '/class-wp-theme-json.php';
 require __DIR__ . '/class-wp-theme-json-resolver.php';
 
