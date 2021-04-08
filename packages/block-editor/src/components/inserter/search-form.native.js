a3c3ca8948df0982b91ca426ac57e96beab5925a
jhnstn <jason@readysetandco.com>
2021-04-07 21:32:11 -0700

    Update platform specific styles

--- packages/block-editor/src/components/inserter/search-form.native.js
+++ packages/block-editor/src/components/inserter/search-form.native.js
@@ -1,7 +1,13 @@
 /**
  * External dependencies
  */
-import { TextInput, View, TouchableHighlight } from 'react-native';
+import {
+	TextInput,
+	Text,
+	View,
+	TouchableHighlight,
+	Platform,
+} from 'react-native';
 
 /**
  * WordPress dependencies
@@ -15,74 +21,165 @@ import {
 	cancelCircleFilled,
 	arrowLeft,
 	search as searchIcon,
+	close,
 } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
+import platformStyles from './searchFormStyles';
 
 function InserterSearchForm( { value, onChange, onLayout = () => {} } ) {
 	const [ isActive, setIsActive ] = useState( false );
 
+	const isIOS = Platform.OS === 'ios';
+
 	const inputRef = useRef();
 
-	const searchFormStyle = usePreferredColorSchemeStyle(
-		styles.searchForm,
-		styles.searchFormDark
+	function isInactive() {
+		return ! value && ! isActive;
+	}
+
+	const containerStyle = usePreferredColorSchemeStyle(
+		styles[ 'inserter-search-form__container' ],
+		styles[ 'inserter-search-form__container--dark' ]
 	);
 
-	const searchFormInputStyle = usePreferredColorSchemeStyle(
-		styles.searchFormInput,
-		styles.searchFormInputDark
+	const containerActiveStyle = {
+		...containerStyle,
+		...usePreferredColorSchemeStyle(
+			platformStyles[ 'inserter-search-form__container--active' ],
+			platformStyles[ 'inserter-search-form__container--active-dark' ]
+		),
+	};
+
+	const formStyle = {
+		...usePreferredColorSchemeStyle(
+			styles[ 'inserter-search-form__form' ],
+			styles[ 'inserter-search-form__form--dark' ]
+		),
+		...usePreferredColorSchemeStyle(
+			platformStyles[ 'inserter-search-form__form' ],
+			platformStyles[ 'inserter-search-form__form--dark' ]
+		),
+	};
+
+	const formActiveStyle = {
+		...formStyle,
+		...usePreferredColorSchemeStyle(
+			platformStyles[ 'inserter-search-form__form--active' ],
+			platformStyles[ 'inserter-search-form__form--active-dark' ]
+		),
+	};
+	const formInputStyle = usePreferredColorSchemeStyle(
+		styles[ 'inserter-search-form__form-input' ],
+		styles[ 'inserter-search-form__form-input--dark' ]
 	);
+
+	const formInputActiveStyle = {
+		...formInputStyle,
+		...usePreferredColorSchemeStyle(
+			platformStyles[ 'inserter-search-form__form-input--active' ],
+			platformStyles[ 'inserter-search-form__form-input--active-dark' ]
+		),
+	};
 
 	const placeholderStyle = usePreferredColorSchemeStyle(
-		styles.searchFormPlaceholder,
-		styles.searchFormPlaceholderDark
+		styles[ 'inserter-search-form__form-input-placeholder' ],
+		styles[ 'inserter-search-form__form-input-placeholder--dark' ]
 	);
+
+	const cancelButtonStyle = styles[ 'inserter-search-form__cancel-button' ];
+
+	const cancelButtonTextStyle = usePreferredColorSchemeStyle(
+		styles[ 'inserter-search-form__cancel-button-text' ],
+		styles[ 'inserter-search-form__cancel-button-text--dark' ]
+	);
+
+	function onCancel() {
+		inputRef.current.blur();
+		onChange( '' );
+		setIsActive( false );
+	}
+
+	function onActivate() {
+		inputRef.current.focus();
+		setIsActive( true );
+	}
+
+	function renderLeftButton() {
+		if ( ! isIOS && isActive ) {
+			return (
+				<ToolbarButton
+					title={ __( 'Cancel Search' ) }
+					icon={ arrowLeft }
+					onClick={ onCancel }
+				/>
+			);
+		}
+
+		return (
+			<ToolbarButton
+				title={ __( 'Search Blocks' ) }
+				icon={ searchIcon }
+				onClick={ onActivate }
+			/>
+		);
+	}
+
+	function renderRightButton() {
+		if ( isInactive() ) {
+			return (
+				<ToolbarButton
+					title={ __( 'Search Blocks' ) }
+					onClick={ onActivate }
+				/>
+			);
+		}
+
+		if ( !! value ) {
+			return (
+				<ToolbarButton
+					title={ __( 'Clear search' ) }
+					icon={
+						<Icon icon={ isIOS ? cancelCircleFilled : close } />
+					}
+					onClick={ () => {
+						onChange( '' );
+					} }
+				/>
+			);
+		}
+	}
 
 	return (
 		<TouchableHighlight accessible={ false } onLayout={ onLayout }>
-			<View style={ searchFormStyle }>
-				{ isActive ? (
-					<ToolbarButton
-						title={ __( 'Cancel search' ) }
-						icon={ arrowLeft }
-						onClick={ () => {
-							inputRef.current.blur();
-							onChange( '' );
-							setIsActive( false );
-						} }
+			<View style={ isActive ? containerActiveStyle : containerStyle }>
+				<View style={ isActive ? formActiveStyle : formStyle }>
+					{ renderLeftButton() }
+					<TextInput
+						ref={ inputRef }
+						style={
+							isActive ? formInputActiveStyle : formInputStyle
+						}
+						placeholderTextColor={ placeholderStyle.color }
+						onChangeText={ onChange }
+						onFocus={ () => setIsActive( true ) }
+						value={ value }
+						placeholder={ __( 'Search blocks' ) }
 					/>
-				) : (
-					<ToolbarButton
-						title={ __( 'Search block' ) }
-						icon={ searchIcon }
-						onClick={ () => {
-							inputRef.current.focus();
-							setIsActive( true );
-						} }
-					/>
-				) }
-				<TextInput
-					ref={ inputRef }
-					style={ searchFormInputStyle }
-					placeholderTextColor={ placeholderStyle.color }
-					onChangeText={ onChange }
-					onFocus={ () => setIsActive( true ) }
-					value={ value }
-					placeholder={ __( 'Search blocks' ) }
-				/>
-
-				{ !! value && (
-					<ToolbarButton
-						title={ __( 'Clear search' ) }
-						icon={ <Icon icon={ cancelCircleFilled } /> }
-						onClick={ () => {
-							onChange( '' );
-						} }
-					/>
+					{ renderRightButton() }
+				</View>
+				{ isActive && isIOS && (
+					<View style={ cancelButtonStyle }>
+						<Text
+							onPress={ onCancel }
+							style={ cancelButtonTextStyle }
+						>
+							{ __( 'Cancel' ) }
+						</Text>
+					</View>
 				) }
 			</View>
 		</TouchableHighlight>
