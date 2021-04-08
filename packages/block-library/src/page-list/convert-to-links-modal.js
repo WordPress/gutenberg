9802ef0b16c3b916b5583b364ac038dc1fc5387c
Kerry Liu <gwwar@users.noreply.github.com>
2021-04-08 10:52:33 -0700

    Page List: add ability to convert to navigation links (#30390)
    * Page List: add ability to convert to navigation links
    * Page List: only allow conversion when pages is <= 100. Disable convert button until pages are available.
    * Page List: add aria-describedby to convert links modal
    * Page List: update copy, add todos and explanation on why ordering can differ from render_callback vs rest api
    Co-authored-by: jasmussen <joen@automattic.com>

---
+++ packages/block-library/src/page-list/convert-to-links-modal.js
@@ -0,0 +1,124 @@
+/**
+ * WordPress dependencies
+ */
+import { Button, Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { store as coreDataStore } from '@wordpress/core-data';
+import { createBlock as create } from '@wordpress/blocks';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+
+const PAGE_FIELDS = [ 'id', 'title', 'link', 'type', 'parent' ];
+const MAX_PAGE_COUNT = 100;
+
+export const convertSelectedBlockToNavigationLinks = ( {
+	pages,
+	clientId,
+	replaceBlock,
+	createBlock,
+} ) => () => {
+	if ( ! pages ) {
+		return;
+	}
+
+	const linkMap = {};
+	const navigationLinks = [];
+	pages.forEach( ( { id, title, link: url, type, parent } ) => {
+		// See if a placeholder exists. This is created if children appear before parents in list
+		const innerBlocks = linkMap[ id ]?.innerBlocks ?? [];
+		linkMap[ id ] = createBlock(
+			'core/navigation-link',
+			{
+				id,
+				label: title.rendered,
+				url,
+				type,
+				kind: 'post-type',
+			},
+			innerBlocks
+		);
+
+		if ( ! parent ) {
+			navigationLinks.push( linkMap[ id ] );
+		} else {
+			if ( ! linkMap[ parent ] ) {
+				// Use a placeholder if the child appears before parent in list
+				linkMap[ parent ] = { innerBlocks: [] };
+			}
+			const parentLinkInnerBlocks = linkMap[ parent ].innerBlocks;
+			parentLinkInnerBlocks.push( linkMap[ id ] );
+		}
+	} );
+
+	replaceBlock( clientId, navigationLinks );
+};
+
+export default function ConvertToLinksModal( { onClose, clientId } ) {
+	const { pages, pagesFinished } = useSelect(
+		( select ) => {
+			const { getEntityRecords, hasFinishedResolution } = select(
+				coreDataStore
+			);
+			const query = [
+				'postType',
+				'page',
+				{
+					per_page: MAX_PAGE_COUNT,
+					_fields: PAGE_FIELDS,
+					// TODO: When https://core.trac.wordpress.org/ticket/39037 REST API support for multiple orderby
+					// values is resolved, update 'orderby' to [ 'menu_order', 'post_title' ] to provide a consistent
+					// sort.
+					orderby: 'menu_order',
+					order: 'asc',
+				},
+			];
+			return {
+				pages: getEntityRecords( ...query ),
+				pagesFinished: hasFinishedResolution(
+					'getEntityRecords',
+					query
+				),
+			};
+		},
+		[ clientId ]
+	);
+	const { replaceBlock } = useDispatch( blockEditorStore );
+
+	return (
+		<Modal
+			closeLabel={ __( 'Close' ) }
+			onRequestClose={ onClose }
+			title={ __( 'Convert to links' ) }
+			className={ 'wp-block-page-list-modal' }
+			aria={ { describedby: 'wp-block-page-list-modal__description' } }
+		>
+			<p id={ 'wp-block-page-list-modal__description' }>
+				{ __(
+					'To edit this navigation menu, convert it to single page links. This allows you to add, re-order, remove items, or edit their labels.'
+				) }
+			</p>
+			<p>
+				{ __(
+					"Note: if you add new pages to your site, you'll need to add them to your navigation menu."
+				) }
+			</p>
+			<div className="wp-block-page-list-modal-buttons">
+				<Button isTertiary onClick={ onClose }>
+					{ __( 'Cancel' ) }
+				</Button>
+				<Button
+					isPrimary
+					disabled={ ! pagesFinished }
+					onClick={ convertSelectedBlockToNavigationLinks( {
+						pages,
+						replaceBlock,
+						clientId,
+						createBlock: create,
+					} ) }
+				>
+					{ __( 'Convert' ) }
+				</Button>
+			</div>
+		</Modal>
+	);
+}
---
+++ packages/block-library/src/page-list/test/convert-to-links-modal.js
@@ -0,0 +1,399 @@
+/**
+ * Internal dependencies
+ */
+import { convertSelectedBlockToNavigationLinks } from '../convert-to-links-modal';
+
+describe( 'page list convert to links', () => {
+	describe( 'convertSelectedBlockToNavigationLinks', () => {
+		it( 'Can create submenus', () => {
+			const pages = [
+				{
+					title: {
+						raw: 'Sample Page',
+						rendered: 'Sample Page',
+					},
+					id: 2,
+					parent: 0,
+					link: 'http://wordpress.local/sample-page/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'About',
+						rendered: 'About',
+					},
+					id: 34,
+					parent: 0,
+					link: 'http://wordpress.local/about/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Contact Page',
+						rendered: 'Contact Page',
+					},
+					id: 37,
+					parent: 0,
+					link: 'http://wordpress.local/contact-page/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test',
+						rendered: 'Test',
+					},
+					id: 229,
+					parent: 0,
+					link: 'http://wordpress.local/test/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'About Sub 1',
+						rendered: 'About Sub 1',
+					},
+					id: 738,
+					parent: 34,
+					link: 'http://wordpress.local/about/about-sub-1/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'About Sub 2',
+						rendered: 'About Sub 2',
+					},
+					id: 740,
+					parent: 34,
+					link: 'http://wordpress.local/about/about-sub-2/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test Sub',
+						rendered: 'Test Sub',
+					},
+					id: 742,
+					parent: 229,
+					link: 'http://wordpress.local/test/test-sub/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test Sub Sub',
+						rendered: 'Test Sub Sub',
+					},
+					id: 744,
+					parent: 742,
+					link: 'http://wordpress.local/test/test-sub/test-sub-sub/',
+					type: 'page',
+				},
+			];
+			const replaceBlock = jest.fn();
+			const createBlock = jest.fn(
+				( name, attributes, innerBlocks ) => ( {
+					name,
+					attributes,
+					innerBlocks,
+				} )
+			);
+			const convertLinks = convertSelectedBlockToNavigationLinks( {
+				pages,
+				clientId: 'testId',
+				replaceBlock,
+				createBlock,
+			} );
+			convertLinks();
+			expect( replaceBlock.mock.calls?.[ 0 ]?.[ 1 ] ).toEqual( [
+				{
+					attributes: {
+						id: 2,
+						kind: 'post-type',
+						label: 'Sample Page',
+						type: 'page',
+						url: 'http://wordpress.local/sample-page/',
+					},
+					innerBlocks: [],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 34,
+						kind: 'post-type',
+						label: 'About',
+						type: 'page',
+						url: 'http://wordpress.local/about/',
+					},
+					innerBlocks: [
+						{
+							attributes: {
+								id: 738,
+								kind: 'post-type',
+								label: 'About Sub 1',
+								type: 'page',
+								url:
+									'http://wordpress.local/about/about-sub-1/',
+							},
+							innerBlocks: [],
+							name: 'core/navigation-link',
+						},
+						{
+							attributes: {
+								id: 740,
+								kind: 'post-type',
+								label: 'About Sub 2',
+								type: 'page',
+								url:
+									'http://wordpress.local/about/about-sub-2/',
+							},
+							innerBlocks: [],
+							name: 'core/navigation-link',
+						},
+					],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 37,
+						kind: 'post-type',
+						label: 'Contact Page',
+						type: 'page',
+						url: 'http://wordpress.local/contact-page/',
+					},
+					innerBlocks: [],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 229,
+						kind: 'post-type',
+						label: 'Test',
+						type: 'page',
+						url: 'http://wordpress.local/test/',
+					},
+					innerBlocks: [
+						{
+							attributes: {
+								id: 742,
+								kind: 'post-type',
+								label: 'Test Sub',
+								type: 'page',
+								url: 'http://wordpress.local/test/test-sub/',
+							},
+							innerBlocks: [
+								{
+									attributes: {
+										id: 744,
+										kind: 'post-type',
+										label: 'Test Sub Sub',
+										type: 'page',
+										url:
+											'http://wordpress.local/test/test-sub/test-sub-sub/',
+									},
+									innerBlocks: [],
+									name: 'core/navigation-link',
+								},
+							],
+							name: 'core/navigation-link',
+						},
+					],
+					name: 'core/navigation-link',
+				},
+			] );
+		} );
+		it( 'Can create submenus, when children appear before parents', () => {
+			const pages = [
+				{
+					title: {
+						raw: 'About Sub 1',
+						rendered: 'About Sub 1',
+					},
+					id: 738,
+					parent: 34,
+					link: 'http://wordpress.local/about/about-sub-1/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'About Sub 2',
+						rendered: 'About Sub 2',
+					},
+					id: 740,
+					parent: 34,
+					link: 'http://wordpress.local/about/about-sub-2/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test Sub Sub',
+						rendered: 'Test Sub Sub',
+					},
+					id: 744,
+					parent: 742,
+					link: 'http://wordpress.local/test/test-sub/test-sub-sub/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test Sub',
+						rendered: 'Test Sub',
+					},
+					id: 742,
+					parent: 229,
+					link: 'http://wordpress.local/test/test-sub/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Sample Page',
+						rendered: 'Sample Page',
+					},
+					id: 2,
+					parent: 0,
+					link: 'http://wordpress.local/sample-page/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'About',
+						rendered: 'About',
+					},
+					id: 34,
+					parent: 0,
+					link: 'http://wordpress.local/about/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Contact Page',
+						rendered: 'Contact Page',
+					},
+					id: 37,
+					parent: 0,
+					link: 'http://wordpress.local/contact-page/',
+					type: 'page',
+				},
+				{
+					title: {
+						raw: 'Test',
+						rendered: 'Test',
+					},
+					id: 229,
+					parent: 0,
+					link: 'http://wordpress.local/test/',
+					type: 'page',
+				},
+			];
+			const replaceBlock = jest.fn();
+			const createBlock = jest.fn(
+				( name, attributes, innerBlocks ) => ( {
+					name,
+					attributes,
+					innerBlocks,
+				} )
+			);
+			const convertLinks = convertSelectedBlockToNavigationLinks( {
+				pages,
+				clientId: 'testId',
+				replaceBlock,
+				createBlock,
+			} );
+			convertLinks();
+			expect( replaceBlock.mock.calls?.[ 0 ]?.[ 1 ] ).toEqual( [
+				{
+					attributes: {
+						id: 2,
+						kind: 'post-type',
+						label: 'Sample Page',
+						type: 'page',
+						url: 'http://wordpress.local/sample-page/',
+					},
+					innerBlocks: [],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 34,
+						kind: 'post-type',
+						label: 'About',
+						type: 'page',
+						url: 'http://wordpress.local/about/',
+					},
+					innerBlocks: [
+						{
+							attributes: {
+								id: 738,
+								kind: 'post-type',
+								label: 'About Sub 1',
+								type: 'page',
+								url:
+									'http://wordpress.local/about/about-sub-1/',
+							},
+							innerBlocks: [],
+							name: 'core/navigation-link',
+						},
+						{
+							attributes: {
+								id: 740,
+								kind: 'post-type',
+								label: 'About Sub 2',
+								type: 'page',
+								url:
+									'http://wordpress.local/about/about-sub-2/',
+							},
+							innerBlocks: [],
+							name: 'core/navigation-link',
+						},
+					],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 37,
+						kind: 'post-type',
+						label: 'Contact Page',
+						type: 'page',
+						url: 'http://wordpress.local/contact-page/',
+					},
+					innerBlocks: [],
+					name: 'core/navigation-link',
+				},
+				{
+					attributes: {
+						id: 229,
+						kind: 'post-type',
+						label: 'Test',
+						type: 'page',
+						url: 'http://wordpress.local/test/',
+					},
+					innerBlocks: [
+						{
+							attributes: {
+								id: 742,
+								kind: 'post-type',
+								label: 'Test Sub',
+								type: 'page',
+								url: 'http://wordpress.local/test/test-sub/',
+							},
+							innerBlocks: [
+								{
+									attributes: {
+										id: 744,
+										kind: 'post-type',
+										label: 'Test Sub Sub',
+										type: 'page',
+										url:
+											'http://wordpress.local/test/test-sub/test-sub-sub/',
+									},
+									innerBlocks: [],
+									name: 'core/navigation-link',
+								},
+							],
+							name: 'core/navigation-link',
+						},
+					],
+					name: 'core/navigation-link',
+				},
+			] );
+		} );
+	} );
+} );
