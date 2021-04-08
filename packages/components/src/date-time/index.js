b13ae8786e17a3414acb33e42bc81852257de513
Damián Suárez <rdsuarez@gmail.com>
2021-04-08 11:03:23 -0300

    Post Schedule: show post events (#29716)
    * post-schedule: collect and pass calendar events
    
    * post-schedule: re-write component using function
    
    * date-time: remove onMonthPreviewed() and highlight
    
    * post-schedule: update current month
    
    * date-time: render day events using own component
    
    * date-time: styling when not selected day
    
    * fixing eslint issues
    
    * date-time: tweak calendar events style
    
    * post-schedule: exlude current post from evenys
    
    * date-time: decouple onMonthChange with keepFocusInside
    
    * date-time: check events before to pick events day
    
    * fix rebasing issues
    
    * date-time: events replace circles by dots
    
    * fix rebasing bug
    
    * post-schedule: split up processing post events
    
    * date-time: revert prop name change
    
    * date-time: add screen reader support
    
    * date-time: update content for screen reader
    
    * date-time: fix eslint error
    
    * post-schedule: get events by post type
    
    * date-time: rephrase events number for readers
    
    * date-time: add events to aria label
    
    * date-time: fix lint error. tidy.
    
    * date-time: set aria-label value
    
    * date-time: update e2e test
    
    * date-time: clean comment
    
    * date-time: join day-with-events string to improve its translation
    
    * date-time: remove ref as hook dependency
    The ref nevers changes its value
    
    * post-scheudle: use title.rendered prop for events

--- packages/components/src/date-time/index.js
+++ packages/components/src/date-time/index.js
@@ -4,6 +4,7 @@
 // Needed to initialise the default datepicker styles.
 // See: https://github.com/airbnb/react-dates#initialize
 import 'react-dates/initialize';
+import { noop } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -25,7 +26,7 @@ function DateTimePicker(
 		currentDate,
 		is12Hour,
 		isInvalidDate,
-		onMonthPreviewed,
+		onMonthPreviewed = noop,
 		onChange,
 		events,
 	},
@@ -52,8 +53,8 @@ function DateTimePicker(
 						currentDate={ currentDate }
 						onChange={ onChange }
 						isInvalidDate={ isInvalidDate }
-						onMonthPreviewed={ onMonthPreviewed }
 						events={ events }
+						onMonthPreviewed={ onMonthPreviewed }
 					/>
 				</>
 			) }
