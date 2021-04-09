a757d3b722d7821acb24c1880eeaf6e1a4768f0b
Alex Lende <ajlende@gmail.com>
2021-04-09 10:41:53 -1000

    Revert "Refactor out stylesheet"
    This reverts commit 1f258ebad11f87ef1ef36359c67b276e376d5e4d.

--- packages/block-editor/src/hooks/duotone.js
+++ packages/block-editor/src/hooks/duotone.js
@@ -30,78 +30,71 @@ import {
  */
 
 /**
- * SVG needed for rendering the duotone filter.
+ * SVG and stylesheet needed for rendering the duotone filter.
  *
  * @param  {Object} props          Duotone props.
+ * @param  {string} props.selector Selector to apply the filter to.
  * @param  {string} props.id       Unique id for this duotone filter.
  * @param  {Values} props.values   R, G, and B values to filter with.
  * @return {WPElement}             Duotone element.
  */
-function DuotoneFilter( { id, values } ) {
-	return (
-		<SVG
-			xmlnsXlink="http://www.w3.org/1999/xlink"
-			viewBox="0 0 0 0"
-			width="0"
-			height="0"
-			focusable="false"
-			role="none"
-			style={ {
-				visibility: 'hidden',
-				position: 'absolute',
-				left: '-9999px',
-				overflow: 'hidden',
-			} }
-		>
-			<defs>
-				<filter id={ id }>
-					<feColorMatrix
-						type="matrix"
-						// Use perceptual brightness to convert to grayscale.
-						// prettier-ignore
-						values=".299 .587 .114 0 0
-						        .299 .587 .114 0 0
-						        .299 .587 .114 0 0
-						        0 0 0 1 0"
-					/>
-					<feComponentTransfer
-						// Use sRGB instead of linearRGB to be consistent with how CSS gradients work.
-						colorInterpolationFilters="sRGB"
-					>
-						<feFuncR
-							type="table"
-							tableValues={ values.r.join( ' ' ) }
-						/>
-						<feFuncG
-							type="table"
-							tableValues={ values.g.join( ' ' ) }
-						/>
-						<feFuncB
-							type="table"
-							tableValues={ values.b.join( ' ' ) }
-						/>
-					</feComponentTransfer>
-				</filter>
-			</defs>
-		</SVG>
-	);
-}
-
-/**
- * Stylesheet needed for rendering the duotone filter.
- *
- * @param  {Object} props          Duotone props.
- * @param  {string} props.id       Unique id for this duotone filter.
- * @param  {string} props.selector Selector to apply the filter to.
- * @return {WPElement}             Duotone element.
- */
-function DuotoneStylesheet( { id, selector } ) {
+function DuotoneFilter( { selector, id, values } ) {
 	const stylesheet = `
 ${ selector } {
 	filter: url( #${ id } );
 }
 `;
-	return <style dangerouslySetInnerHTML={ { __html: stylesheet } } />;
+
+	return (
+		<>
+			<SVG
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 0 0"
+				width="0"
+				height="0"
+				focusable="false"
+				role="none"
+				style={ {
+					visibility: 'hidden',
+					position: 'absolute',
+					left: '-9999px',
+					overflow: 'hidden',
+				} }
+			>
+				<defs>
+					<filter id={ id }>
+						<feColorMatrix
+							type="matrix"
+							// Use perceptual brightness to convert to grayscale.
+							// prettier-ignore
+							values=".299 .587 .114 0 0
+							        .299 .587 .114 0 0
+							        .299 .587 .114 0 0
+							        0 0 0 1 0"
+						/>
+						<feComponentTransfer
+							// Use sRGB instead of linearRGB to be consistent with how CSS gradients work.
+							colorInterpolationFilters="sRGB"
+						>
+							<feFuncR
+								type="table"
+								tableValues={ values.r.join( ' ' ) }
+							/>
+							<feFuncG
+								type="table"
+								tableValues={ values.g.join( ' ' ) }
+							/>
+							<feFuncB
+								type="table"
+								tableValues={ values.b.join( ' ' ) }
+							/>
+						</feComponentTransfer>
+					</filter>
+				</defs>
+			</SVG>
+			<style dangerouslySetInnerHTML={ { __html: stylesheet } } />
+		</>
+	);
 }
 
 function DuotonePanel( { attributes, setAttributes } ) {
@@ -208,8 +201,11 @@ const withDuotoneStyles = createHigherOrderComponent(
 
 		return (
 			<>
-				<DuotoneFilter id={ id } values={ values } />
-				<DuotoneStylesheet id={ id } selector={ selectorsGroup } />
+				<DuotoneFilter
+					selector={ selectorsGroup }
+					id={ id }
+					values={ values }
+				/>
 				<BlockListBlock { ...props } className={ className } />
 			</>
 		);
