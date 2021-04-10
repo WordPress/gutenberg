0d2a822ae15ba56ed5045040933d7536a42f7335
Amanda Riu <amanda.riu@automattic.com>
2021-04-10 00:25:36 -0400

    Simplify access to block child components

--- packages/react-native-editor/__device-tests__/gutenberg-editor-search.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-search.test.js
@@ -3,6 +3,21 @@
  */
 import { blockNames } from './pages/editor-page';
 
+const blockChildTestIds = {
+	label: {
+		android: 'Search block label.',
+		ios: 'search-block-label',
+	},
+	input: {
+		android: 'Search input field.',
+		ios: 'search-block-input',
+	},
+	button: {
+		android: 'Search button.',
+		ios: 'search-block-button',
+	},
+};
+
 describe( 'Gutenberg Editor Search Block tests', () => {
 	it( 'should be able to add a Search block', async () => {
 		await editorPage.addNewBlock( blockNames.search );
@@ -10,27 +25,29 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 			blockNames.search
 		);
 
-		//TODO AMANDA -- testing
-		const placeholderTV = await editorPage.getPlaceholderTextViewForSearchBlock();
-		const placeholder = await placeholderTV.text();
-
-		console.log( `AMANDA-TEST > placeholder value is: ${ placeholder }` );
-
-		const labelTV = await editorPage.getLabelTextViewForSearchBlock(
-			searchBlock
-		);
-		// const textEdit = labelTV.children[ 0 ];
-
-		// const label = await textEdit.text();
-		const label = await labelTV.text();
-		console.log( `AMANDA-TEST > label value is: ${ label.toString() }` );
-
-		// const buttonTV = await editorPage.getButtonTextViewForSearchBlock();
-		// const button = await buttonTV.text();
-
-		// console.log( `AMANDA-TEST > button value is: ${ button }` );
-
 		expect( searchBlock ).toBeTruthy();
-		await editorPage.removeBlockAtPosition( blockNames.search );
+	} );
+
+	it( 'label should be visible and text set', async () => {
+		// const block = await editorPage.getFirstBlockVisible();
+		// block.click();
+
+		const label = await editorPage.getSearchBlockChild(
+			blockChildTestIds.label
+		);
+		const labelValue = await label.text();
+		console.log( `AMANDA-TEST > label value: ${ labelValue }` );
+
+		const input = await editorPage.getSearchBlockChild(
+			blockChildTestIds.input
+		);
+		const inputValue = await input.text();
+		console.log( `AMANDA-TEST > input value: ${ inputValue }` );
+
+		const button = await editorPage.getSearchBlockChild(
+			blockChildTestIds.button
+		);
+		const buttonValue = await button.text();
+		console.log( `AMANDA-TEST > button value: ${ buttonValue }` );
 	} );
 } );
