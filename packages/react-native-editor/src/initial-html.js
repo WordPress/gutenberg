77b356c77aeda872e031a9f69c952f230a5b5435
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-04-12 20:25:17 +0200

    Revert "Add Embed block to initial html example"
    This reverts commit 4381c3947f7d6c1b3c862a4f9e2ed5196aace6d5.

--- packages/react-native-editor/src/initial-html.js
+++ packages/react-native-editor/src/initial-html.js
@@ -232,16 +232,6 @@ else:
 <!-- /wp:buttons -->
 
 <!-- wp:heading -->
-<h2>Embed</h2>
-<!-- /wp:heading -->
-
-<!-- wp:embed {"url":"https://www.youtube.com/watch?v=ssfHW5lwFZg","type":"rich","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure class="wp-block-embed is-type-rich is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-https://www.youtube.com/watch?v=ssfHW5lwFZg
-</div></figure>
-<!-- /wp:embed -->
-
-<!-- wp:heading -->
 <h2>Legacy</h2>
 <!-- /wp:heading -->
 
