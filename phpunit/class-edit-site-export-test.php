778a36190e9f94904aa0799ff1105738cdf43dd8
Dávid Szabó <david.szabo97@gmail.com>
2021-04-13 07:08:43 +0200

    Site Editor: Run inject and remove theme attribute recursively (#30416)
    * Run inject and remove theme attribute recursively
    * Inject theme attribute inside template part content

--- phpunit/class-edit-site-export-test.php
+++ phpunit/class-edit-site-export-test.php
@@ -7,15 +7,20 @@
 
 class Edit_Site_Export_Test extends WP_UnitTestCase {
 	function test_remove_theme_attribute_from_content() {
-		$content_without_theme_attribute = '<!-- wp:template-part {"slug":"header","theme":"tt1-blocks","align":"full","tagName":"header","className":"site-header"} /-->';
-		$template_content                = _remove_theme_attribute_from_content( $content_without_theme_attribute );
-		$expected                        = '<!-- wp:template-part {"slug":"header","align":"full","tagName":"header","className":"site-header"} /-->';
+		$content_with_existing_theme_attribute = '<!-- wp:template-part {"slug":"header","theme":"tt1-blocks","align":"full","tagName":"header","className":"site-header"} /-->';
+		$template_content                      = _remove_theme_attribute_from_content( $content_with_existing_theme_attribute );
+		$expected                              = '<!-- wp:template-part {"slug":"header","align":"full","tagName":"header","className":"site-header"} /-->';
+		$this->assertEquals( $expected, $template_content );
+
+		$content_with_existing_theme_attribute_nested = '<!-- wp:group --><!-- wp:template-part {"slug":"header","theme":"tt1-blocks","align":"full","tagName":"header","className":"site-header"} /--><!-- /wp:group -->';
+		$template_content                             = _remove_theme_attribute_from_content( $content_with_existing_theme_attribute_nested );
+		$expected                                     = '<!-- wp:group --><!-- wp:template-part {"slug":"header","align":"full","tagName":"header","className":"site-header"} /--><!-- /wp:group -->';
 		$this->assertEquals( $expected, $template_content );
 
 		// Does not modify content when there is no existing theme attribute.
-		$content_with_existing_theme_attribute = '<!-- wp:template-part {"slug":"header","align":"full","tagName":"header","className":"site-header"} /-->';
-		$template_content                      = _remove_theme_attribute_from_content( $content_with_existing_theme_attribute );
-		$this->assertEquals( $content_with_existing_theme_attribute, $template_content );
+		$content_without_theme_attribute = '<!-- wp:template-part {"slug":"header","align":"full","tagName":"header","className":"site-header"} /-->';
+		$template_content                = _remove_theme_attribute_from_content( $content_without_theme_attribute );
+		$this->assertEquals( $content_without_theme_attribute, $template_content );
 
 		// Does not remove theme when there is no template part.
 		$content_with_no_template_part = '<!-- wp:post-content /-->';
