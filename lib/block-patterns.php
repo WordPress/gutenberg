24cec1cd4356daf31a7351dcdc3097817ad95cfd
Kjell Reigstad <kjell@kjellr.com>
2021-04-13 08:30:59 -0400

    Use hex values for colors, make titles clickable in the Large Title and Date pattern.

--- lib/block-patterns.php
+++ lib/block-patterns.php
@@ -95,19 +95,20 @@ register_block_pattern(
 		'title'      => __( 'Large Title and Date', 'gutenberg' ),
 		'blockTypes' => array( 'core/query' ),
 		'categories' => array( 'Query' ),
-		'content'    => '<!-- wp:group {"align":"full","backgroundColor":"black","textColor":"white","style":{"spacing":{"padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}}}} -->
-						<div class="wp-block-group alignfull has-white-color has-black-background-color has-text-color has-background" style="padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px">
-						<!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
+		'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"100px","right":"100px","bottom":"100px","left":"100px"}},"color":{"text":"#ffffff","background":"#000000"}}} -->
+						<div class="wp-block-group alignfull has-text-color has-background" style="background-color:#000000;color:#ffffff;padding-top:100px;padding-right:100px;padding-bottom:100px;padding-left:100px"><!-- wp:query {"queryId":3,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
 						<!-- wp:query-loop -->
-						<!-- wp:separator {"color":"white","align":"wide","className":"is-style-wide"} -->
-						<hr class="wp-block-separator alignwide has-text-color has-background has-white-background-color has-white-color is-style-wide"/>
+						<!-- wp:separator {"customColor":"#ffffff","align":"wide","className":"is-style-wide"} -->
+						<hr class="wp-block-separator alignwide has-text-color has-background is-style-wide" style="background-color:#ffffff;color:#ffffff"/>
 						<!-- /wp:separator -->
+
 						<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 						<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
-						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:post-date {"textColor":"white","fontSize":"extra-small"} /--></div>
+						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:post-date {"style":{"color":{"text":"#ffffff"}},"fontSize":"extra-small"} /--></div>
 						<!-- /wp:column -->
+
 						<!-- wp:column {"verticalAlignment":"center","width":"80%"} -->
-						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:80%"><!-- wp:post-title {"style":{"typography":{"fontSize":"72px","lineHeight":"1.1"}},"textColor":"white"} /--></div>
+						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:80%"><!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"72px","lineHeight":"1.1"},"color":{"text":"#ffffff","link":"#ffffff"}}} /--></div>
 						<!-- /wp:column --></div>
 						<!-- /wp:columns -->
 						<!-- /wp:query-loop -->
