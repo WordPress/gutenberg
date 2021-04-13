37564abeb9ba09cec55f7c93ace26a750e52c89f
Kjell Reigstad <kjell@kjellr.com>
2021-04-13 09:24:13 -0400

    Remove Small pattern, rename Large and Medium patterns.

--- lib/block-patterns.php
+++ lib/block-patterns.php
@@ -9,9 +9,9 @@ register_block_pattern_category( 'Query', array( 'label' => __( 'Query', 'gutenb
 
 // Initial Query block patterns.
 register_block_pattern(
-	'query/large-posts',
+	'query/standard-posts',
 	array(
-		'title'      => __( 'Large', 'gutenberg' ),
+		'title'      => __( 'Standard', 'gutenberg' ),
 		'blockTypes' => array( 'core/query' ),
 		'categories' => array( 'Query' ),
 		'content'    => '<!-- wp:query {"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
@@ -31,7 +31,7 @@ register_block_pattern(
 register_block_pattern(
 	'query/medium-posts',
 	array(
-		'title'      => __( 'Medium', 'gutenberg' ),
+		'title'      => __( 'Image at Left', 'gutenberg' ),
 		'blockTypes' => array( 'core/query' ),
 		'categories' => array( 'Query' ),
 		'content'    => '<!-- wp:query {"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
@@ -43,27 +43,6 @@ register_block_pattern(
 						<!-- wp:column {"width":"33.33%"} -->
 						<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:post-title {"isLink":true} /-->
 						<!-- wp:post-excerpt /--></div>
-						<!-- /wp:column --></div>
-						<!-- /wp:columns -->
-						<!-- /wp:query-loop -->
-						<!-- /wp:query -->',
-	)
-);
-
-register_block_pattern(
-	'query/small-posts',
-	array(
-		'title'      => __( 'Small', 'gutenberg' ),
-		'blockTypes' => array( 'core/query' ),
-		'categories' => array( 'Query' ),
-		'content'    => '<!-- wp:query {"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
-						<!-- wp:query-loop -->
-						<!-- wp:columns {"verticalAlignment":"center"} -->
-						<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"25%"} -->
-						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:25%"><!-- wp:post-featured-image {"isLink":true} /--></div>
-						<!-- /wp:column -->
-						<!-- wp:column {"verticalAlignment":"center","width":"75%"} -->
-						<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:75%"><!-- wp:post-title {"isLink":true} /--></div>
 						<!-- /wp:column --></div>
 						<!-- /wp:columns -->
 						<!-- /wp:query-loop -->
