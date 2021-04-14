22b3bd7803d1a1a370e9a64af07367a86065fb3f
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2021-04-15 08:12:56 +1000

    Latest posts block: Show error for password protected posts.
    Merges 5c0e81f18d57952c6199ff33047929206a28cd19 to wp/trunk.

--- packages/block-library/src/latest-posts/index.php
+++ packages/block-library/src/latest-posts/index.php
@@ -135,6 +135,10 @@ function render_block_core_latest_posts( $attributes ) {
 
 			$trimmed_excerpt = get_the_excerpt( $post );
 
+			if ( post_password_required( $post ) ) {
+				$trimmed_excerpt = __( 'This content is password protected.' );
+			}
+
 			$list_items_markup .= sprintf(
 				'<div class="wp-block-latest-posts__post-excerpt">%1$s</div>',
 				$trimmed_excerpt
@@ -143,9 +147,16 @@ function render_block_core_latest_posts( $attributes ) {
 
 		if ( isset( $attributes['displayPostContent'] ) && $attributes['displayPostContent']
 			&& isset( $attributes['displayPostContentRadio'] ) && 'full_post' === $attributes['displayPostContentRadio'] ) {
-			$list_items_markup .= sprintf(
+
+				$post_content = wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) );
+
+				if ( post_password_required( $post ) ) {
+					$post_content = __( 'This content is password protected.' );
+				}
+
+				$list_items_markup .= sprintf(
 				'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
-				wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) )
+				$post_content
 			);
 		}
 
