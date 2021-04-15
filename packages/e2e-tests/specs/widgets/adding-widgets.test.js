249e1c58011a5f4c4613dd8e1dea4ceee2caa893
Robert Anderson <robert@noisysocks.com>
2021-04-15 13:49:44 +1000

    Legacy Widget: Improve backwards compatibility (#30709)
    * Legacy Widget: Improve backwards compatibility
    
    - Fire 'widget-added' and 'widget-updated' events when the widget form
      is initialized or when it changes. This is what most widgets' scripts
      use to initialize and/or attach event listeners.
    - Include hidden inputs that most widgets expect to exist in the DOM.
    
    * Fix Legacy Widget E2E test
    
    * Add onSave to deps
    
    Co-authored-by: Kai Hao <kevin830726@gmail.com>
    
    * Add setFormData to deps
    
    Co-authored-by: Kai Hao <kevin830726@gmail.com>
    
    * Import jquery instead of accessing from window
    
    * Remove unnecessary useCallback
    
    * Widgets E2E tests: Query by label instead of id
    
    * Replace querySelector with extra ref
    
    * Revert "Import jquery instead of accessing from window"
    
    This reverts commit 4d591c5ed18408656a1bbd2e6a31ba39df2149bf.
    
    * Add fallback if jQuery does not exist
    
    Co-authored-by: Kai Hao <kevin830726@gmail.com>

--- packages/e2e-tests/specs/widgets/adding-widgets.test.js
+++ packages/e2e-tests/specs/widgets/adding-widgets.test.js
@@ -461,7 +461,12 @@ describe( 'Widgets screen', () => {
 			'[aria-label="Block: Widget Area"][role="group"]'
 		);
 
-		const legacyWidget = await page.waitForSelector(
+		// Wait for the widget's form to load.
+		await page.waitForSelector(
+			'[data-block][data-type="core/legacy-widget"] input'
+		);
+
+		const legacyWidget = await page.$(
 			'[data-block][data-type="core/legacy-widget"]'
 		);
 
@@ -491,8 +496,13 @@ describe( 'Widgets screen', () => {
 		);
 		await editButton.click();
 
-		// TODO: Should query this with role and label.
-		const titleInput = await legacyWidget.$( 'input' );
+		const [ titleLabel ] = await legacyWidget.$x(
+			'//label[contains(text(), "Title")]'
+		);
+		const titleInputId = await titleLabel.evaluate( ( node ) =>
+			node.getAttribute( 'for' )
+		);
+		const titleInput = await page.$( `#${ titleInputId }` );
 		await titleInput.type( 'Search Title' );
 
 		// Trigger the toolbar to appear.
