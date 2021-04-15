cf1c19e46f2e54ff47e6dc4b419b3340c10ada2d
Riad Benguella <benguella@gmail.com>
2021-04-15 14:12:59 +0100

    Replace the document tab with a template tab in template mode (#30860)

--- packages/edit-post/src/components/sidebar/settings-sidebar/index.js
+++ packages/edit-post/src/components/sidebar/settings-sidebar/index.js
@@ -21,7 +21,8 @@ import DiscussionPanel from '../discussion-panel';
 import PageAttributes from '../page-attributes';
 import MetaBoxes from '../../meta-boxes';
 import PluginDocumentSettingPanel from '../plugin-document-setting-panel';
-import PluginSidebarEditPost from '../../sidebar/plugin-sidebar';
+import PluginSidebarEditPost from '../plugin-sidebar';
+import TemplateSummary from '../template-summary';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { store as editPostStore } from '../../../store';
@@ -32,30 +33,39 @@ const SIDEBAR_ACTIVE_BY_DEFAULT = Platform.select( {
 } );
 
 const SettingsSidebar = () => {
-	const { sidebarName, keyboardShortcut } = useSelect( ( select ) => {
-		// The settings sidebar is used by the edit-post/document and edit-post/block sidebars.
-		// sidebarName represents the sidebar that is active or that should be active when the SettingsSidebar toggle button is pressed.
-		// If one of the two sidebars is active the component will contain the content of that sidebar.
-		// When neither of the the two sidebars is active we can not simply return null, because the PluginSidebarEditPost
-		// component, besides being used to render the sidebar, also renders the toggle button. In that case sidebarName
-		// should contain the sidebar that will be active when the toggle button is pressed. If a block
-		// is selected, that should be edit-post/block otherwise it's edit-post/document.
-		let sidebar = select( interfaceStore ).getActiveComplementaryArea(
-			editPostStore.name
-		);
-		if (
-			! [ 'edit-post/document', 'edit-post/block' ].includes( sidebar )
-		) {
-			if ( select( 'core/block-editor' ).getBlockSelectionStart() ) {
-				sidebar = 'edit-post/block';
+	const { sidebarName, keyboardShortcut, isTemplateMode } = useSelect(
+		( select ) => {
+			// The settings sidebar is used by the edit-post/document and edit-post/block sidebars.
+			// sidebarName represents the sidebar that is active or that should be active when the SettingsSidebar toggle button is pressed.
+			// If one of the two sidebars is active the component will contain the content of that sidebar.
+			// When neither of the the two sidebars is active we can not simply return null, because the PluginSidebarEditPost
+			// component, besides being used to render the sidebar, also renders the toggle button. In that case sidebarName
+			// should contain the sidebar that will be active when the toggle button is pressed. If a block
+			// is selected, that should be edit-post/block otherwise it's edit-post/document.
+			let sidebar = select( interfaceStore ).getActiveComplementaryArea(
+				editPostStore.name
+			);
+			if (
+				! [ 'edit-post/document', 'edit-post/block' ].includes(
+					sidebar
+				)
+			) {
+				if ( select( 'core/block-editor' ).getBlockSelectionStart() ) {
+					sidebar = 'edit-post/block';
+				}
+				sidebar = 'edit-post/document';
 			}
-			sidebar = 'edit-post/document';
-		}
-		const shortcut = select(
-			keyboardShortcutsStore
-		).getShortcutRepresentation( 'core/edit-post/toggle-sidebar' );
-		return { sidebarName: sidebar, keyboardShortcut: shortcut };
-	}, [] );
+			const shortcut = select(
+				keyboardShortcutsStore
+			).getShortcutRepresentation( 'core/edit-post/toggle-sidebar' );
+			return {
+				sidebarName: sidebar,
+				keyboardShortcut: shortcut,
+				isTemplateMode: select( editPostStore ).isEditingTemplate(),
+			};
+		},
+		[]
+	);
 
 	return (
 		<PluginSidebarEditPost
@@ -69,7 +79,7 @@ const SettingsSidebar = () => {
 			icon={ cog }
 			isActiveByDefault={ SIDEBAR_ACTIVE_BY_DEFAULT }
 		>
-			{ sidebarName === 'edit-post/document' && (
+			{ ! isTemplateMode && sidebarName === 'edit-post/document' && (
 				<>
 					<PostStatus />
 					<PluginDocumentSettingPanel.Slot />
@@ -82,6 +92,9 @@ const SettingsSidebar = () => {
 					<PageAttributes />
 					<MetaBoxes location="side" />
 				</>
+			) }
+			{ isTemplateMode && sidebarName === 'edit-post/document' && (
+				<TemplateSummary />
 			) }
 			{ sidebarName === 'edit-post/block' && <BlockInspector /> }
 		</PluginSidebarEditPost>
