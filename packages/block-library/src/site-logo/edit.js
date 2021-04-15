ccd99ad06dd1bb0d179a42de55f20996fc8351f2
Joen A <1204802+jasmussen@users.noreply.github.com>
2021-04-15 14:54:52 +0200

    Site Logo: Fix default size (#30846)
    Add a new "is-default-size" class, and attach the 120px width to it.

--- packages/block-library/src/site-logo/edit.js
+++ packages/block-library/src/site-logo/edit.js
@@ -337,7 +337,7 @@ export default function LogoEdit( {
 	);
 
 	const classes = classnames( className, {
-		'is-resized': !! width,
+		'is-default-size': ! width,
 	} );
 
 	const blockProps = useBlockProps( {
