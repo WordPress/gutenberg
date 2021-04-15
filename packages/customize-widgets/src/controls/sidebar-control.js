4bc2866742ac03a64a620fb12d4742c1c7c6a73e
Daniel Richards <daniel.richards@automattic.com>
2021-04-15 19:04:10 +0800

    Fix scripts not waiting for availability of wp.customize api (#30864)

--- packages/customize-widgets/src/controls/sidebar-control.js
+++ packages/customize-widgets/src/controls/sidebar-control.js
@@ -8,53 +8,56 @@ import { render, unmountComponentAtNode } from '@wordpress/element';
  */
 import SidebarBlockEditor from '../components/sidebar-block-editor';
 import SidebarAdapter from '../components/sidebar-block-editor/sidebar-adapter';
-import InserterOuterSection from './inserter-outer-section';
-
-const {
-	wp: { customize },
-} = window;
+import getInserterOuterSection from './inserter-outer-section';
 
 const getInserterId = ( controlId ) => `widgets-inserter-${ controlId }`;
 
-class SidebarControl extends customize.Control {
-	ready() {
-		this.inserter = new InserterOuterSection(
-			getInserterId( this.id ),
-			{}
-		);
-		customize.section.add( this.inserter );
+export default function getSidebarControl() {
+	const {
+		wp: { customize },
+	} = window;
 
-		this.sectionInstance = customize.section( this.section() );
+	return class SidebarControl extends customize.Control {
+		ready() {
+			const InserterOuterSection = getInserterOuterSection();
+			this.inserter = new InserterOuterSection(
+				getInserterId( this.id ),
+				{}
+			);
+			customize.section.add( this.inserter );
 
-		this.inspector = this.sectionInstance.inspector;
+			this.sectionInstance = customize.section( this.section() );
 
-		this.render();
-	}
-	onChangeSectionExpanded( expanded, args ) {
-		if ( ! args.unchanged ) {
-			// Close the inserter when the section collapses.
-			if ( ! expanded ) {
-				this.inserter.close();
-			}
+			this.inspector = this.sectionInstance.inspector;
 
 			this.render();
 		}
-	}
-	render() {
-		if ( this.sectionInstance.expanded() ) {
-			render(
-				<SidebarBlockEditor
-					sidebar={ new SidebarAdapter( this.setting, customize ) }
-					inserter={ this.inserter }
-					inspector={ this.inspector }
-				/>,
-				this.container[ 0 ]
-			);
-		} else if ( ! this.sectionInstance.hasSubSectionOpened() ) {
-			// Don't unmount the node when the sub section (inspector) is opened.
-			unmountComponentAtNode( this.container[ 0 ] );
-		}
-	}
-}
+		onChangeSectionExpanded( expanded, args ) {
+			if ( ! args.unchanged ) {
+				// Close the inserter when the section collapses.
+				if ( ! expanded ) {
+					this.inserter.close();
+				}
 
-export default SidebarControl;
+				this.render();
+			}
+		}
+		render() {
+			if ( this.sectionInstance.expanded() ) {
+				render(
+					<SidebarBlockEditor
+						sidebar={
+							new SidebarAdapter( this.setting, customize )
+						}
+						inserter={ this.inserter }
+						inspector={ this.inspector }
+					/>,
+					this.container[ 0 ]
+				);
+			} else if ( ! this.sectionInstance.hasSubSectionOpened() ) {
+				// Don't unmount the node when the sub section (inspector) is opened.
+				unmountComponentAtNode( this.container[ 0 ] );
+			}
+		}
+	};
+}
