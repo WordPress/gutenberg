dfc1ac410ead63bb5e505a5206fabbb57a7609e4
Kai Hao <kevin830726@gmail.com>
2021-04-15 11:37:09 +0800

    Fix editing performance in Widgets Customizer (#30654)

--- packages/customize-widgets/src/components/sidebar-block-editor/sidebar-adapter.js
+++ packages/customize-widgets/src/components/sidebar-block-editor/sidebar-adapter.js
@@ -1,8 +1,3 @@
-/**
- * External dependencies
- */
-import { difference, isEqual, without } from 'lodash';
-
 const { wp } = window;
 
 function parseWidgetId( widgetId ) {
@@ -53,12 +48,25 @@ export default class SidebarAdapter {
 		this.setting = setting;
 		this.allSettings = allSettings;
 
+		this.locked = false;
+		this.widgetsCache = new WeakMap();
 		this.subscribers = new Set();
+
+		this.history = [
+			this._getWidgetIds().map( ( widgetId ) =>
+				this.getWidget( widgetId )
+			),
+		];
+		this.historyIndex = 0;
 
 		this._handleSettingChange = this._handleSettingChange.bind( this );
 		this._handleAllSettingsChange = this._handleAllSettingsChange.bind(
 			this
 		);
+		this.canUndo = this.canUndo.bind( this );
+		this.canRedo = this.canRedo.bind( this );
+		this.undo = this.undo.bind( this );
+		this.redo = this.redo.bind( this );
 	}
 
 	subscribe( callback ) {
@@ -79,30 +87,47 @@ export default class SidebarAdapter {
 		}
 	}
 
-	trigger( event ) {
+	getWidgets() {
+		return this.history[ this.historyIndex ];
+	}
+
+	_emit( ...args ) {
 		for ( const callback of this.subscribers ) {
-			callback( event );
+			callback( ...args );
 		}
 	}
 
-	_handleSettingChange( newWidgetIds, oldWidgetIds ) {
-		const addedWidgetIds = difference( newWidgetIds, oldWidgetIds );
-		const removedWidgetIds = difference( oldWidgetIds, newWidgetIds );
+	_getWidgetIds() {
+		return this.setting.get();
+	}
 
-		for ( const widgetId of addedWidgetIds ) {
-			this.trigger( { type: 'widgetAdded', widgetId } );
+	_pushHistory() {
+		this.history = [
+			...this.history.slice( 0, this.historyIndex + 1 ),
+			this._getWidgetIds().map( ( widgetId ) =>
+				this.getWidget( widgetId )
+			),
+		];
+		this.historyIndex += 1;
+	}
+
+	_handleSettingChange() {
+		if ( this.locked ) {
+			return;
 		}
 
-		for ( const widgetId of addedWidgetIds ) {
-			this.trigger( { type: 'widgetRemoved', widgetId } );
-		}
+		const prevWidgets = this.getWidgets();
 
-		if ( ! isEqual( addedWidgetIds, removedWidgetIds ) ) {
-			this.trigger( { type: 'widgetsReordered', newWidgetIds } );
-		}
+		this._pushHistory();
+
+		this._emit( prevWidgets, this.getWidgets() );
 	}
 
 	_handleAllSettingsChange( setting ) {
+		if ( this.locked ) {
+			return;
+		}
+
 		if ( ! setting.id.startsWith( 'widget_' ) ) {
 			return;
 		}
@@ -112,30 +137,14 @@ export default class SidebarAdapter {
 			return;
 		}
 
-		this.trigger( { type: 'widgetChanged', widgetId } );
+		const prevWidgets = this.getWidgets();
+
+		this._pushHistory();
+
+		this._emit( prevWidgets, this.getWidgets() );
 	}
 
-	getWidgetIds() {
-		return this.setting.get();
-	}
-
-	setWidgetIds( widgetIds ) {
-		this.setting.set( widgetIds );
-	}
-
-	getWidget( widgetId ) {
-		const { idBase, number } = parseWidgetId( widgetId );
-		const settingId = widgetIdToSettingId( widgetId );
-		const instance = this.allSettings( settingId ).get();
-		return {
-			id: widgetId,
-			idBase,
-			number,
-			instance,
-		};
-	}
-
-	addWidget( widget, index ) {
+	_createWidget( widget ) {
 		const widgetModel = wp.customize.Widgets.availableWidgets.findWhere( {
 			id_base: widget.idBase,
 		} );
@@ -167,24 +176,131 @@ export default class SidebarAdapter {
 			'',
 			settingArgs
 		);
-		setting.set( {} );
+		setting.set( widget.instance );
 
-		const widgetIds = [ ...this.setting.get() ];
 		const widgetId = settingIdToWidgetId( settingId );
-		widgetIds.splice( index, 0, widgetId );
-		this.setting.set( widgetIds );
 
 		return widgetId;
 	}
 
-	updateWidget( widget ) {
+	_updateWidget( widget ) {
+		const prevWidget = this.getWidget( widget.id );
+
+		// Bail out update.
+		if ( prevWidget === widget ) {
+			return widget.id;
+		}
+
 		const settingId = widgetIdToSettingId( widget.id );
 		this.allSettings( settingId ).set( widget.instance );
 		// TODO: what about the other stuff?
+
+		return widget.id;
 	}
 
-	removeWidget( widgetId ) {
-		const widgetIds = this.setting.get();
-		this.setting.set( without( widgetIds, widgetId ) );
+	getWidget( widgetId ) {
+		if ( ! widgetId ) {
+			return null;
+		}
+
+		const { idBase, number } = parseWidgetId( widgetId );
+		const settingId = widgetIdToSettingId( widgetId );
+		const setting = this.allSettings( settingId );
+
+		if ( ! setting ) {
+			return null;
+		}
+
+		const instance = setting.get();
+
+		if ( this.widgetsCache.has( instance ) ) {
+			return this.widgetsCache.get( instance );
+		}
+
+		const widget = {
+			id: widgetId,
+			idBase,
+			number,
+			instance,
+		};
+
+		this.widgetsCache.set( instance, widget );
+
+		return widget;
+	}
+
+	_updateWidgets( nextWidgets ) {
+		this.locked = true;
+
+		const addedWidgetIds = [];
+
+		const nextWidgetIds = nextWidgets.map( ( nextWidget ) => {
+			if ( nextWidget.id && this.getWidget( nextWidget.id ) ) {
+				addedWidgetIds.push( null );
+
+				return this._updateWidget( nextWidget );
+			}
+
+			const widgetId = this._createWidget( nextWidget );
+
+			addedWidgetIds.push( widgetId );
+
+			return widgetId;
+		} );
+
+		// TODO: We should in theory also handle delete widgets here too.
+
+		this.setting.set( nextWidgetIds );
+
+		this.locked = false;
+
+		return addedWidgetIds;
+	}
+
+	setWidgets( nextWidgets ) {
+		const addedWidgetIds = this._updateWidgets( nextWidgets );
+
+		this._pushHistory();
+
+		return addedWidgetIds;
+	}
+
+	/**
+	 * Undo/Redo related features
+	 */
+	canUndo() {
+		return this.historyIndex > 0;
+	}
+
+	canRedo() {
+		return this.historyIndex < this.history.length - 1;
+	}
+
+	_seek( historyIndex ) {
+		const currentWidgets = this.getWidgets();
+
+		this.historyIndex = historyIndex;
+
+		const widgets = this.history[ this.historyIndex ];
+
+		this._updateWidgets( widgets );
+
+		this._emit( currentWidgets, this.getWidgets() );
+	}
+
+	undo() {
+		if ( ! this.canUndo() ) {
+			return;
+		}
+
+		this._seek( this.historyIndex - 1 );
+	}
+
+	redo() {
+		if ( ! this.canRedo() ) {
+			return;
+		}
+
+		this._seek( this.historyIndex + 1 );
 	}
 }
