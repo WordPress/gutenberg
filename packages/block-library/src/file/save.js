ff41b60c9245e6a966c1323a77edc5ca0a7e6456
Gary <gary@pento.net>
2021-04-15 15:22:31 +1000

    Re-implement #24233.

--- packages/block-library/src/file/save.js
+++ packages/block-library/src/file/save.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { __, sprintf } from '@wordpress/i18n';
 
 export default function save( { attributes } ) {
 	const {
@@ -11,11 +12,31 @@ export default function save( { attributes } ) {
 		textLinkTarget,
 		showDownloadButton,
 		downloadButtonText,
+		displayPreview,
+		previewHeight,
 	} = attributes;
 
 	return (
 		href && (
 			<div { ...useBlockProps.save() }>
+				{ displayPreview && (
+					<>
+						<object
+							className="wp-block-file__embed"
+							data={ href }
+							type="application/pdf"
+							style={ {
+								width: '100%',
+								height: `${ previewHeight }px`,
+							} }
+							aria-label={ sprintf(
+								/* translators: %s: filename. */
+								__( 'Embed of %s.' ),
+								fileName
+							) }
+						/>
+					</>
+				) }
 				{ ! RichText.isEmpty( fileName ) && (
 					<a
 						href={ textLinkHref }
