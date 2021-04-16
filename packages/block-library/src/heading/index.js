d2d38d3d8ef5f6c70233528c554ac654178795c8
Ari Stathopoulos <aristath@gmail.com>
2021-04-16 11:44:42 +0300

    This was not removed on purpose

--- packages/block-library/src/heading/index.js
+++ packages/block-library/src/heading/index.js
@@ -17,6 +17,7 @@ import edit from './edit';
 import metadata from './block.json';
 import save from './save';
 import transforms from './transforms';
+
 const { name } = metadata;
 
 export { metadata, name };
