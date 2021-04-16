589e3d110f3bda889e6dd08308ab67ccf4f52e9d
Daniel Richards <daniel.richards@automattic.com>
2021-04-16 16:30:09 +0800

    Make it a bit nicer

--- packages/customize-widgets/src/filters/move-to-sidebar.js
+++ packages/customize-widgets/src/filters/move-to-sidebar.js
@@ -17,32 +17,59 @@ const { wp } = window;
  */
 import MoveToSidebar from '../components/move-to-sidebar';
 
+/**
+ * Use the customize API to get the setting for the sidebar.
+ * This returns a function that acts as a setter/getter in one for widget ids
+ * in sidebars.
+ *
+ * @param {string} id The id of the sidebar
+ *
+ * @return {Function} A function that when called with no arguments, returns
+ *                    an array of widgetIds for the sidebar. When called with
+ *                    an array, sets that array as the widget ids for the
+ *                    sidebar.
+ */
+function getSidebarWidgetSetting( id ) {
+	return wp.customize( `sidebars_widgets[${ id }]` );
+}
+
+/**
+ * Returns the sidebar object that the widget represented by the provided
+ * widgetId belongs to.
+ *
+ * @param {string} widgetId A widget id
+ *
+ * @return {Object} Configuration of the sidebar that the widget is in.
+ */
 function getSidebarForWidgetId( widgetId ) {
-	const api = wp.customize;
-	return api.Widgets.data.registeredSidebars.find( ( { id } ) => {
-		const sidebarWidgetsSetting = api( `sidebars_widgets[${ id }]` );
+	return wp.customize.Widgets.data.registeredSidebars.find( ( { id } ) => {
+		const sidebarWidgetsSetting = getSidebarWidgetSetting( id );
 		const sidebarWidgetIds = sidebarWidgetsSetting();
 		return sidebarWidgetIds.includes( widgetId );
 	} );
 }
 
+/**
+ * Returns a list of the registered sidebars.
+ *
+ * @return {Object[]} An array of sidebar configurations objects.
+ */
 function getSidebars() {
 	return wp.customize.Widgets.data.registeredSidebars;
 }
 
+/**
+ * Moves a widget from one sidebar to another.
+ *
+ * @param {string} widgetId     The id of the widget to move.
+ * @param {string} oldSidebarId The id of the sidebar to move from.
+ * @param {string} newSidebarId The id of the sidebar to move to.
+ */
 function moveToSidebar( widgetId, oldSidebarId, newSidebarId ) {
-	const api = wp.customize;
+	const oldSidebarWidgetsSetting = getSidebarWidgetSetting( oldSidebarId );
+	const newSidebarWidgetsSetting = getSidebarWidgetSetting( newSidebarId );
 
-	// These calls create a combined setter/getter function for widgets in
-	// sidebars
-	const oldSidebarWidgetsSetting = api(
-		`sidebars_widgets[${ oldSidebarId }]`
-	);
-	const newSidebarWidgetsSetting = api(
-		`sidebars_widgets[${ newSidebarId }]`
-	);
-
-	// Update the widgets int he old and new sidebars
+	// Update the widgets in the old and new sidebars.
 	oldSidebarWidgetsSetting( without( oldSidebarWidgetsSetting(), widgetId ) );
 	newSidebarWidgetsSetting( [ ...newSidebarWidgetsSetting(), widgetId ] );
 }
