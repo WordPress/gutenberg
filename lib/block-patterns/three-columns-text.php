91a3e21c8b16623bf36ea7bdfe3558e8c2b14a1d
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-04-19 13:21:44 +0200

    removed unnecesary data attributes

--- lib/block-patterns/three-columns-text.php
+++ lib/block-patterns/three-columns-text.php
@@ -11,7 +11,7 @@ return array(
 	'content'     => '<!-- wp:columns {"align":"full","style":{"color":{"text":"#000000","background":"#ffffff"}}} -->
 	<div class="wp-block-columns alignfull has-text-color has-background" style="background-color:#ffffff;color:#000000"><!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
-	<h3 style="font-size:24px;line-height:1.3"><strong><a href="http://wordpress.org" data-type="URL" data-id="wordpress.org">' . esc_html__( 'Virtual Tour ↗', 'default' ) . '</a></strong></h3>
+	<h3 style="font-size:24px;line-height:1.3"><strong><a href="http://wordpress.org">' . esc_html__( 'Virtual Tour ↗', 'default' ) . '</a></strong></h3>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
