29d568f9ea8b3b72cff8fffed3893c8239383559
Riad Benguella <benguella@gmail.com>
2021-04-19 13:20:14 +0100

    Remove template link

--- packages/edit-post/src/components/header/view/index.js
+++ packages/edit-post/src/components/header/view/index.js
@@ -7,7 +7,6 @@ import { external, check } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useViewportMatch } from '@wordpress/compose';
-import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
@@ -20,41 +19,23 @@ export default function ViewMenu() {
 		isPostSaveable,
 		isSaving,
 		deviceType,
-		supportsTemplateMode,
-		isTemplateMode,
-		template,
 	} = useSelect( ( select ) => {
-		const { getCurrentPostType, isEditedPostSaveable } = select(
-			editorStore
-		);
-		const { getPostType } = select( coreStore );
+		const { isEditedPostSaveable } = select( editorStore );
 		const {
-			isEditingTemplate,
-			getEditedPostTemplate,
 			hasMetaBoxes,
 			isSavingMetaBoxes,
 			__experimentalGetPreviewDeviceType,
 		} = select( editPostStore );
-
-		const isViewable =
-			getPostType( getCurrentPostType() )?.viewable ?? false;
-		const _supportsTemplateMode =
-			select( editorStore ).getEditorSettings().supportsTemplateMode &&
-			isViewable;
 
 		return {
 			hasActiveMetaboxes: hasMetaBoxes(),
 			isSaving: isSavingMetaBoxes(),
 			isPostSaveable: isEditedPostSaveable(),
 			deviceType: __experimentalGetPreviewDeviceType(),
-			template: _supportsTemplateMode && getEditedPostTemplate(),
-			isTemplateMode: isEditingTemplate(),
-			supportsTemplateMode: _supportsTemplateMode,
 		};
 	}, [] );
 	const {
-		__experimentalSetPreviewDeviceType,
-		setIsEditingTemplate,
+		__experimentalSetPreviewDeviceType: setPreviewDeviceType,
 	} = useDispatch( editPostStore );
 
 	const isMobile = useViewportMatch( 'small', '<' );
@@ -68,13 +49,6 @@ export default function ViewMenu() {
 		);
 	}
 
-	const setPreviewDeviceType = ( newDeviceType ) => {
-		if ( isTemplateMode ) {
-			setIsEditingTemplate( false );
-		}
-		__experimentalSetPreviewDeviceType( newDeviceType );
-	};
-
 	const popoverProps = {
 		position: 'bottom left',
 	};
@@ -86,7 +60,7 @@ export default function ViewMenu() {
 				isTertiary: true,
 				disabled: ! isPostSaveable,
 				/* translators: button label text should, if possible, be under 16 characters. */
-				children: __( 'View' ),
+				children: __( 'Preview' ),
 			} }
 			icon={ null }
 		>
@@ -95,47 +69,23 @@ export default function ViewMenu() {
 					<MenuGroup>
 						<MenuItem
 							onClick={ () => setPreviewDeviceType( 'Desktop' ) }
-							icon={
-								! isTemplateMode &&
-								deviceType === 'Desktop' &&
-								check
-							}
+							icon={ deviceType === 'Desktop' && check }
 						>
 							{ __( 'Desktop' ) }
 						</MenuItem>
 						<MenuItem
 							onClick={ () => setPreviewDeviceType( 'Tablet' ) }
-							icon={
-								! isTemplateMode &&
-								deviceType === 'Tablet' &&
-								check
-							}
+							icon={ deviceType === 'Tablet' && check }
 						>
 							{ __( 'Tablet' ) }
 						</MenuItem>
 						<MenuItem
 							onClick={ () => setPreviewDeviceType( 'Mobile' ) }
-							icon={
-								! isTemplateMode &&
-								deviceType === 'Mobile' &&
-								check
-							}
+							icon={ deviceType === 'Mobile' && check }
 						>
 							{ __( 'Mobile' ) }
 						</MenuItem>
 					</MenuGroup>
-					{ supportsTemplateMode && !! template && (
-						<MenuGroup>
-							<MenuItem
-								onClick={ () =>
-									setIsEditingTemplate( ! isTemplateMode )
-								}
-								icon={ isTemplateMode && check }
-							>
-								{ __( 'Template' ) }
-							</MenuItem>
-						</MenuGroup>
-					) }
 					<MenuGroup>
 						<PostPreviewButton
 							className="edit-post-view__preview-externally"
