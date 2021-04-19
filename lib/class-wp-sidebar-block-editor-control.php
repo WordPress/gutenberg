c6422c46381680005de18b6d3475a7a7d2b4241b
Daniel Richards <daniel.richards@automattic.com>
2021-04-19 16:01:43 +0800

    Use gutenberg_initialize_editor to load the editor and pass through block settings

--- lib/class-wp-sidebar-block-editor-control.php
+++ lib/class-wp-sidebar-block-editor-control.php
@@ -22,6 +22,24 @@ class WP_Sidebar_Block_Editor_Control extends WP_Customize_Control {
 	 * Enqueue the scripts and styles.
 	 */
 	public function enqueue() {
+		$settings = array_merge(
+			gutenberg_get_common_block_editor_settings(),
+			gutenberg_get_legacy_widget_settings()
+		);
+
+		// This purposefully does not rely on apply_filters( 'block_editor_settings', $settings, null );
+		// Applying that filter would bring over multitude of features from the post editor, some of which
+		// may be undesirable. Instead of using that filter, we simply pick just the settings that are needed.
+		$settings = gutenberg_experimental_global_styles_settings( $settings );
+		$settings = gutenberg_extend_block_editor_styles( $settings );
+
+		gutenberg_initialize_editor(
+			'widgets_customizer',
+			'customize-widgets',
+			array(
+				'editor_settings' => $settings,
+			)
+		);
 		wp_enqueue_script( 'wp-customize-widgets' );
 		wp_enqueue_style( 'wp-customize-widgets' );
 		wp_enqueue_script( 'wp-format-library' );
