4579a725ac981ef64c2a0facf526ce20a734cc67
Ari Stathopoulos <aristath@gmail.com>
2021-04-20 09:40:10 +0300

    cs

--- packages/block-library/src/heading/edit.js
+++ packages/block-library/src/heading/edit.js
@@ -13,7 +13,7 @@ import {
 	BlockControls,
 	RichText,
 	useBlockProps,
-	store as blockEditorStore
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
@@ -43,10 +43,13 @@ function HeadingEdit( {
 		} ),
 		style: mergedStyle,
 	} );
-	const allHeadingAnchors = useSelect( ( select ) => {
-		const allBlocks = select( blockEditorStore ).getBlocks();
-		return getAllHeadingAnchors( allBlocks, clientId );
-	}, [ clientId ] );
+	const allHeadingAnchors = useSelect(
+		( select ) => {
+			const allBlocks = select( blockEditorStore ).getBlocks();
+			return getAllHeadingAnchors( allBlocks, clientId );
+		},
+		[ clientId ]
+	);
 	useEffect( () => {
 		const newAnchor = maybeUpdateAnchor(
 			attributes.anchor,
