146d3929744d9e8e9432b5a5c7f0b0d50299f73f
David Szabo <david.szabo97@gmail.com>
2021-04-21 10:47:25 +0200

    Update docs

--- packages/blocks/src/api/registration.js
+++ packages/blocks/src/api/registration.js
@@ -103,17 +103,17 @@ import { store as blocksStore } from '../store';
  *                                             that help users discover the variation
  *                                             while searching.
  * @property {Function|string[]} [isActive]    This can be a function or an array of block attributes.
- *                                             Function that accepts a block's attributes
- *                                             and the variation's attributes and determines
- *                                             if a variation is active. This function doesn't
- *                                             try to find a match dynamically based on all
- *                                             block's attributes, as in many cases some
- *                                             attributes are irrelevant. An example would
- *                                             be for `embed` block where we only care about
- *                                             `providerNameSlug` attribute's value.
- *                                             In case of a string array, it determines the active
- *                                             variation by comparing (strict equality)
- *                                             the block's attributes to the variation's attributes.
+ *                                             Function that accepts a block's attributes and the
+ *                                             variation's attributes and determines if a variation is active.
+ *                                             A function that accepts a block's attributes and the
+ *                                             variation's attributes and determines if a variation is active.
+ *                                             This function doesn't try to find a match dynamically based
+ *                                             on all block's attributes, as in many cases some attributes are irrelevant.
+ *                                             An example would be for `embed` block where we only care
+ *                                             about `providerNameSlug` attribute's value.
+ *                                             We can also use a `string[]` to tell which attributes
+ *                                             should be compared as a shorthand. Each attributes will
+ *                                             be matched and the variation will be active if all of them are matching.
  */
 
 /**
