6a2938b6f6b2d8cada1a7afe1a12f868849a6760
André <andres.maneiro@automattic.com>
2021-04-21 17:38:41 +0200

    fix phpdocs

--- lib/blocks.php
+++ lib/blocks.php
@@ -381,7 +381,7 @@ add_filter( 'block_categories', 'gutenberg_register_theme_block_category' );
  * Checks whether the current block type supports the feature requested.
  *
  * @param WP_Block_Type $block_type Block type to check for support.
- * @param string        $feature    Name of the feature to check support for.
+ * @param array         $feature    Path of the feature to check support for.
  * @param mixed         $default    Fallback value for feature support, defaults to false.
  *
  * @return boolean                  Whether or not the feature is supported.
