114cbb2f7b4568fe8a87207d7893e899bf2077f8
Daniel Richards <daniel.richards@automattic.com>
2021-04-21 15:11:38 +0800

    Build: Update `npm run dev` watch task (#29652)
    * Update watch task to a single call to watch function
    
    * Fix getSrcDirectory
    
    * Fix unlinking deleted files
    
    * Remove path.resolve
    
    * Combine regex
    
    * Handle nested src folders
    
    * First draft of profiling helper script
    
    * Tidy up
    
    * Update test script
    
    * Remove test script
    
    * Remove blank line
    
    * Use async unlink
    
    Ensure a callback is defined for unlink

--- bin/packages/watch.js
+++ bin/packages/watch.js
@@ -11,79 +11,155 @@ const chalk = require( 'chalk' );
  * Internal dependencies
  */
 const getPackages = require( './get-packages' );
-
 const BUILD_SCRIPT = path.resolve( __dirname, './build.js' );
+const PACKAGES_DIR = path.resolve( __dirname, '../../packages' );
+const modulePackages = getPackages();
 
 let filesToBuild = new Map();
 
-const exists = ( filename ) => {
+/**
+ * Determines whether a file exists.
+ *
+ * @param {string} filename
+ *
+ * @return {boolean} True if a file exists.
+ */
+function exists( filename ) {
 	try {
 		return fs.statSync( filename ).isFile();
 	} catch ( e ) {}
 	return false;
-};
+}
 
-// Exclude test files including .js files inside of __tests__ or test folders
-// and files with a suffix of .test or .spec (e.g. blocks.test.js),
-// and deceitful source-like files, such as editor swap files.
-const isSourceFile = ( filename ) => {
+/**
+ * Determine if a file is source code.
+ *
+ * Exclude test files including .js files inside of __tests__ or test folders
+ * and files with a suffix of .test or .spec (e.g. blocks.test.js),
+ * and deceitful source-like files, such as editor swap files.
+ *
+ * @param {string} filename
+ *
+ * @return {boolean} True if the file a source file.
+ */
+function isSourceFile( filename ) {
 	return (
+		/\/src\/.+\.(js|json|scss)$/.test( filename ) &&
 		! [
-			/\/(benchmark|__mocks__|__tests__|test|storybook|stories)\/.+.js$/,
+			/\/(benchmark|__mocks__|__tests__|test|storybook|stories)\/.+/,
 			/.\.(spec|test)\.js$/,
-		].some( ( regex ) => regex.test( filename ) ) &&
-		/.\.(js|json|scss)$/.test( filename )
+		].some( ( regex ) => regex.test( filename ) )
 	);
-};
+}
 
-const rebuild = ( filename ) => filesToBuild.set( filename, true );
+/**
+ * Determine if a file is in a module package.
+ *
+ * getPackages only returns packages that have a package.json with the module
+ * field. Only build these packages.
+ *
+ * @param {string} filename
+ *
+ * @return {boolean} True if the file is in a module package.
+ */
+function isModulePackage( filename ) {
+	return modulePackages.some( ( packagePath ) => {
+		return filename.indexOf( packagePath ) > -1;
+	} );
+}
 
-getPackages().forEach( ( p ) => {
-	const srcDir = path.resolve( p, 'src' );
-	try {
-		fs.accessSync( srcDir, fs.F_OK );
-		watch(
-			path.resolve( p, 'src' ),
-			{ recursive: true, delay: 500 },
-			( event, filename ) => {
-				if ( ! isSourceFile( filename ) ) {
-					return;
-				}
+/**
+ * Is the file something the watch task should monitor or skip?
+ *
+ * @param {string} filename
+ * @param {symbol} skip
+ *
+ * @return {boolean | symbol} True if the file should be watched.
+ */
+function isWatchableFile( filename, skip ) {
+	return isSourceFile( filename ) && isModulePackage( filename )
+		? true
+		: skip;
+}
 
-				const filePath = path.resolve( srcDir, filename );
-				if ( event === 'update' && exists( filePath ) ) {
-					console.log(
-						chalk.green( '->' ),
-						`${ event }: ${ filename }`
-					);
-					rebuild( filePath );
-				} else {
-					const buildFile = path.resolve(
-						srcDir,
-						'..',
-						'build',
-						filename
-					);
-					try {
-						fs.unlinkSync( buildFile );
-						process.stdout.write(
-							chalk.red( '  \u2022 ' ) +
-								path.relative(
-									path.resolve( srcDir, '..', '..' ),
-									buildFile
-								) +
-								' (deleted)' +
-								'\n'
-						);
-					} catch ( e ) {}
-				}
-			}
-		);
-	} catch ( e ) {
-		// doesn't exist
+/**
+ * Returns the associated file in the build folder for a given source file.
+ *
+ * @param {string} srcFile
+ *
+ * @return {string} Path to the build file.
+ */
+function getBuildFile( srcFile ) {
+	// Could just use string.replace, but the user might have the project
+	// checked out and nested under another src folder.
+	const packageDir = srcFile.substr( 0, srcFile.lastIndexOf( '/src/' ) );
+	const filePath = srcFile.substr( srcFile.lastIndexOf( '/src/' ) + 5 );
+	return path.resolve( packageDir, 'build', filePath );
+}
+
+/**
+ * Adds a build file to the set of files that should be rebuilt.
+ *
+ * @param {'update'} event The event name
+ * @param {string} filename
+ */
+function updateBuildFile( event, filename ) {
+	if ( exists( filename ) ) {
+		try {
+			console.log( chalk.green( '->' ), `${ event }: ${ filename }` );
+			filesToBuild.set( filename, true );
+		} catch ( e ) {
+			console.log(
+				chalk.red( 'Error:' ),
+				`Unable to update file: ${ filename } - `,
+				e
+			);
+		}
 	}
-} );
+}
 
+/**
+ * Removes a build file from the build folder
+ * (usually triggered the associated source file was deleted)
+ *
+ * @param {'remove'} event The event name
+ * @param {string} filename
+ */
+function removeBuildFile( event, filename ) {
+	const buildFile = getBuildFile( filename );
+	if ( exists( buildFile ) ) {
+		try {
+			fs.unlink( buildFile, () => {
+				console.log( chalk.red( '<-' ), `${ event }: ${ filename }` );
+			} );
+		} catch ( e ) {
+			console.log(
+				chalk.red( 'Error:' ),
+				`Unable to remove build file: ${ filename } - `,
+				e
+			);
+		}
+	}
+}
+
+// Start watching packages.
+watch(
+	PACKAGES_DIR,
+	{ recursive: true, delay: 500, filter: isWatchableFile },
+	( event, filename ) => {
+		switch ( event ) {
+			case 'update':
+				updateBuildFile( event, filename );
+				break;
+			case 'remove':
+				removeBuildFile( event, filename );
+				break;
+		}
+	}
+);
+
+// Run a separate interval that calls the build script.
+// This effectively acts as a throttle for building files.
 setInterval( () => {
 	const files = Array.from( filesToBuild.keys() );
 	if ( files.length ) {
