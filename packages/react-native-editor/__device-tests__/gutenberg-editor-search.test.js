e552cf4b36ec0cb8645b4ccdc5dd212404bcd46e
Amanda Riu <amanda.riu@automattic.com>
2021-04-23 18:48:27 -0400

    Avoid test flakiness by making search block fields empty before adding text.

--- packages/react-native-editor/__device-tests__/gutenberg-editor-search.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-search.test.js
@@ -11,25 +11,23 @@ const testIds = {
 	button: 'search-block-button',
 };
 
-describe( 'Gutenberg Editor Search Block tests', () => {
-	it( 'should be able to add a Search block', async () => {
-		await editorPage.addNewBlock( blockNames.search );
-		const searchBlock = await editorPage.getBlockAtPosition(
-			blockNames.search
-		);
+const searchBlockHtml = `<!-- wp:search {"label":"","buttonText":""} /-->`;
 
-		expect( searchBlock ).toBeTruthy();
-	} );
+describe( 'Gutenberg Editor Search Block tests.', () => {
+	describe( 'Editing Search Block elements.', () => {
+		it( 'Add search block via HTML', async () => {
+			await editorPage.setHtmlContent( searchBlockHtml );
+		} );
 
-	describe( 'Editing Search Block elements', () => {
-		it( 'able to customize label text', async () => {
+		it( 'Able to customize label text', async () => {
 			await editorPage.sendTextToSearchBlockChild(
 				testIds.label,
 				testData.shortText
 			);
 
-			let expected = '';
-			let actual = '';
+			let actual;
+			let expected;
+
 			if ( isAndroid() ) {
 				// Android pads the string entered into the `PlainText` component so we'll get the
 				// value a different way by asking for it directly.
@@ -40,23 +38,22 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 				actual = inputValue.trim();
 				expected = testData.shortText;
 			} else {
-				// Couldn't figure out how to get the value directly from the element on iOS so here we
-				// switch to html and verify the results.
 				actual = await editorPage.getHtmlContent();
-				expected = `<!-- wp:search {"label":"${ testData.shortText }","buttonText":"Search"} /-->`;
+				expected = `"label":"${ testData.shortText }"`;
 			}
 
-			expect( expected ).toBe( actual );
+			expect( actual ).toContain( expected );
 		} );
 
-		it( 'able to customize placeholder text', async () => {
+		it( 'Able to customize placeholder text', async () => {
 			await editorPage.sendTextToSearchBlockChild(
 				testIds.input,
 				testData.shortText
 			);
 
-			let expected = '';
-			let actual = '';
+			let actual;
+			let expected;
+
 			if ( isAndroid() ) {
 				// Android pads the string entered into the `PlainText` component so we'll get the
 				// value a different way by asking for it directly.
@@ -67,38 +64,56 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 				actual = inputValue.trim();
 				expected = testData.shortText;
 			} else {
-				// Couldn't figure out how to get the value directly from the element on iOS so here we
-				// switch to html and verify the results.
 				actual = await editorPage.getHtmlContent();
-				expected = `<!-- wp:search {"label":"${ testData.shortText }","placeholder":"${ testData.shortText }","buttonText":"Search"} /-->`;
+				expected = `"placeholder":"${ testData.shortText }"`;
 			}
 
-			expect( expected ).toBe( actual );
+			expect( actual ).toContain( expected );
 		} );
 
-		it( 'able to customize button text', async () => {
-			// Changing text on the button doesn't work as expected on Android
-			// so skip This test if running on Android.
-			if ( isAndroid() ) {
-				return;
-			}
-
+		it( 'Able to customize button text', async () => {
 			await editorPage.sendTextToSearchBlockChild(
 				testIds.button,
 				testData.shortButtonText
 			);
 
-			// switch to html and verify html
-			const html = await editorPage.getHtmlContent();
+			let actual;
+			let expected;
 
-			expect( html ).toBe(
-				`<!-- wp:search {"label":"${ testData.shortText }","placeholder":"${ testData.shortText }","buttonText":"${ testData.shortButtonText }"} /-->`
-			);
+			if ( isAndroid() ) {
+				// Android pads the string entered into the `PlainText` component so we'll get the
+				// value a different way by asking for it directly.
+				const input = await editorPage.getSearchBlockTextElement(
+					testIds.button
+				);
+				const inputValue = await input.text();
+				actual = inputValue.trim();
+				expected = testData.shortButtonText;
+			} else {
+				actual = await editorPage.getHtmlContent();
+				expected = `"buttonText":"${ testData.shortButtonText }"`;
+			}
+
+			expect( actual ).toContain( expected );
+		} );
+
+		it( 'Remove Search block', async () => {
+			// Remove the search block to end this suite of tests.
+			await editorPage.removeBlockAtPosition( blockNames.search );
 		} );
 	} );
 
-	describe( 'Search block settings', () => {
-		it( 'able to hide search block label', async () => {
+	describe( 'Changing search block settings.', () => {
+		it( 'Able to add the Search Block.', async () => {
+			await editorPage.addNewBlock( blockNames.search );
+			const searchBlock = await editorPage.getBlockAtPosition(
+				blockNames.search
+			);
+
+			expect( searchBlock ).toBeTruthy();
+		} );
+
+		it( 'Able to hide search block label', async () => {
 			const searchBlock = await editorPage.getBlockAtPosition(
 				blockNames.search
 			);
@@ -111,7 +126,7 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 			expect( html ).toContain( `"showLabel":false` );
 		} );
 
-		it( 'able to change to icon only button', async () => {
+		it( 'Able to change to icon only button', async () => {
 			const searchBlock = await editorPage.getBlockAtPosition(
 				blockNames.search
 			);
@@ -124,7 +139,7 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 			expect( html ).toContain( `"buttonUseIcon":true` );
 		} );
 
-		it( 'able to change button position to inside', async () => {
+		it( 'Able to change button position to inside', async () => {
 			const searchBlock = await editorPage.getBlockAtPosition(
 				blockNames.search
 			);
@@ -140,7 +155,7 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 			expect( html ).toContain( `"buttonPosition":"button-inside"` );
 		} );
 
-		it( 'able change button position to no button', async () => {
+		it( 'Able change button position to no button', async () => {
 			const searchBlock = await editorPage.getBlockAtPosition(
 				blockNames.search
 			);
@@ -154,7 +169,9 @@ describe( 'Gutenberg Editor Search Block tests', () => {
 			// switch to html and verify
 			const html = await editorPage.getHtmlContent();
 			expect( html ).toContain( `"buttonPosition":"no-button"` );
+		} );
 
+		it( 'Remove search block', async () => {
 			// Remove the search block to end this suite of tests.
 			await editorPage.removeBlockAtPosition( blockNames.search );
 		} );
