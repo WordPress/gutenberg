025b2dd5e5dfa3283fea1bc19cd1f6648deb9e81
David Szabo <david.szabo97@gmail.com>
2021-04-23 15:13:22 +0200

    try

--- packages/e2e-test-utils/src/get-all-blocks.js
+++ packages/e2e-test-utils/src/get-all-blocks.js
@@ -9,5 +9,19 @@ import { wpDataSelect } from './wp-data-select';
  * @return {Promise} Promise resolving with an array containing all blocks in the document.
  */
 export async function getAllBlocks() {
-	return wpDataSelect( 'core/block-editor', 'getBlocks' );
+	const blocks = wpDataSelect( 'core/block-editor', 'getBlocks' );
+	//if ( typeof blocks === 'undefined' ) {
+	const listener = ( message ) => {
+		// eslint-disable-next-line no-console
+		console.log( message.text() );
+	};
+	page.on( 'console', listener );
+	await page.evaluate( () => {
+		const _blocks = wp.data.select( 'core/block-editor' ).getBlocks();
+		// eslint-disable-next-line no-console
+		console.log( _blocks, JSON.stringify( _blocks ) );
+	} );
+	page.off( 'console', listener );
+	//}
+	return blocks;
 }
