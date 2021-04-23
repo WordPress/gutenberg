a564edd6053243ee3218129b17bf651f166e19c9
Koen Van den Wijngaert <koen@neok.be>
2021-04-23 11:44:38 +0200

    Add link to `options-general.php` in the site title description

--- packages/block-library/src/site-title/index.js
+++ packages/block-library/src/site-title/index.js
@@ -2,6 +2,8 @@
  * WordPress dependencies
  */
 import { mapMarker as icon } from '@wordpress/icons';
+import { createInterpolateElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -13,6 +15,13 @@ const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
+	description: createInterpolateElement(
+		__(
+			'Displays and allows editing the name of the site. The site title usually appears in the browser title bar, in search results, and more. Also available in <a>Settings > General</a>.'
+		),
+		// eslint-disable-next-line jsx-a11y/anchor-has-content
+		{ a: <a href="options-general.php" target="_blank" /> }
+	),
 	icon,
 	edit,
 };
