30e105838eeca74c475b40f66709500a55c00073
Kai Hao <kevin830726@gmail.com>
2021-04-26 21:17:51 +0800

    Cleanup widgets before runs

--- packages/e2e-tests/specs/experiments/customizing-widgets.test.js
+++ packages/e2e-tests/specs/experiments/customizing-widgets.test.js
@@ -18,6 +18,7 @@ import { find } from 'puppeteer-testing-library';
 
 describe( 'Widgets Customizer', () => {
 	beforeEach( async () => {
+		await cleanupWidgets();
 		await visitAdminPage( 'customize.php' );
 	} );
 
@@ -401,4 +402,26 @@ async function setWidgetsCustomizerExperiment( enabled ) {
 	} );
 
 	await Promise.all( [ submitButton.click(), page.waitForNavigation() ] );
+}
+
+/**
+ * TODO: Deleting widgets in the new widgets screen seems to be unreliable.
+ * We visit the old widgets screen to delete them.
+ * Refactor this to use real interactions in the new widgets screen once the bug is fixed.
+ */
+async function cleanupWidgets() {
+	await visitAdminPage( 'widgets.php' );
+
+	let widget = await page.$( '.widgets-sortables .widget' );
+
+	// We have to do this one-by-one since there might be race condition when deleting multiple widgets at once.
+	while ( widget ) {
+		const deleteButton = await widget.$( 'button.widget-control-remove' );
+		const id = await widget.evaluate( ( node ) => node.id );
+		await deleteButton.evaluate( ( node ) => node.click() );
+		// Wait for the widget to be removed from DOM.
+		await page.waitForSelector( `#${ id }`, { hidden: true } );
+
+		widget = await page.$( '.widgets-sortables .widget' );
+	}
 }
