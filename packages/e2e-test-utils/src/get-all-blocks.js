7edc8250dc84e49e05afceb79ad11197845ee439
David Szabo <david.szabo97@gmail.com>
2021-04-26 09:19:45 +0200

    try

--- packages/e2e-test-utils/src/get-all-blocks.js
+++ packages/e2e-test-utils/src/get-all-blocks.js
@@ -7,22 +7,25 @@ export async function getAllBlocks() {
 	const allBlocks = await page.evaluate( () => {
 		const blocks = wp.data.select( 'core/block-editor' ).getBlocks();
 
-		const blocksCopy = blocks.map( ( block ) => ( { ...block } ) );
-		const queue = [ ...blocksCopy ];
+		const blocksCopy = blocks.map( ( block ) =>
+			Object.assign( {}, block )
+		);
+		const queue = blocksCopy.concat();
 		while ( queue.length > 0 ) {
 			const block = queue.shift();
 
 			delete block.validationIssues;
 
 			if ( block.innerBlocks?.length > 0 ) {
-				block.innerBlocks = block.innerBlocks.map( ( innerBlock ) => ( {
-					...innerBlock,
-				} ) );
-				for ( const innerBlock of block.innerBlocks ) {
-					queue.push( innerBlock );
-				}
+				block.innerBlocks = block.innerBlocks.map( ( innerBlock ) =>
+					Object.assign( {}, innerBlock )
+				);
+				block.innerBlocks.forEach( ( innerBlock ) =>
+					queue.push( innerBlock )
+				);
 			}
 		}
+		return blocksCopy;
 	} );
 
 	if ( typeof allBlocks === 'undefined' ) {
