0e57309577a67fc45f388566fdebc2ad858414bf
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-04-27 06:08:07 -0700

    components: Remove wp-g2 from tooltip

--- packages/components/src/ui/tooltip/styles.js
+++ packages/components/src/ui/tooltip/styles.js
@@ -1,19 +1,25 @@
 /**
  * External dependencies
  */
-import { css, styled, ui } from '@wp-g2/styles';
+import { css } from 'emotion';
+import styled from '@emotion/styled';
+
 /**
  * Internal dependencies
  */
 import { Shortcut } from '../shortcut';
+import * as ZIndex from '../../utils/z-index';
+import CONFIG from '../../utils/config-values';
+import { space } from '../utils/space';
+import { COLORS } from '../../utils/colors-values';
 
 export const TooltipContent = css`
-	${ ui.zIndex( 'Tooltip', 1000002 ) };
+	z-index: ${ ZIndex.Tooltip };
 	box-sizing: border-box;
 	opacity: 0;
 	outline: none;
 	transform-origin: top center;
-	transition: opacity ${ ui.get( 'transitionDurationFastest' ) } ease;
+	transition: opacity ${ CONFIG.transitionDurationFastest } ease;
 
 	&[data-enter] {
 		opacity: 1;
@@ -24,7 +30,7 @@ export const TooltipPopoverView = styled.div`
 	background: rgba( 0, 0, 0, 0.8 );
 	border-radius: 6px;
 	box-shadow: 0 0 0 1px rgba( 255, 255, 255, 0.04 );
-	color: ${ ui.color.white };
+	color: ${ COLORS.white };
 	padding: 4px 8px;
 `;
 
@@ -34,5 +40,5 @@ export const noOutline = css`
 
 export const TooltipShortcut = styled( Shortcut )`
 	display: inline-block;
-	margin-left: ${ ui.space( 1 ) };
+	margin-left: ${ space( 1 ) };
 `;
