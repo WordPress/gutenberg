a5454b4ee29b56a82c61f81a55854de1313b06da
Ari Stathopoulos <aristath@gmail.com>
2021-04-28 10:05:09 +0300

    make sure it's an array

--- lib/blocks.php
+++ lib/blocks.php
@@ -254,6 +254,9 @@ function gutenberg_maybe_inline_styles() {
 
 			// Set `src` to `false` and add styles inline.
 			$wp_styles->registered[ $style['handle'] ]->src = false;
+			if ( empty( $wp_styles->registered[ $style['handle'] ]->extra['after'] ) ) {
+				$wp_styles->registered[ $style['handle'] ]->extra['after'] = array();
+			}
 			array_unshift( $wp_styles->registered[ $style['handle'] ]->extra['after'], $style['css'] );
 
 			// Add the styles size to the $total_inline_size var.
