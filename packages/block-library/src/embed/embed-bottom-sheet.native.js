6c28a38364dce1071917208a836f1c501af9b2b9
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-04-29 17:55:41 +0200

    Create native EmbedBottomSheet component

---
+++ packages/block-library/src/embed/embed-bottom-sheet.native.js
@@ -0,0 +1,60 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	LinkSettingsNavigation,
+	FooterMessageLink,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { isURL } from '@wordpress/url';
+import { useDispatch } from '@wordpress/data';
+import { store as noticesStore } from '@wordpress/notices';
+
+const linkSettingsOptions = {
+	url: {
+		label: __( 'Embed link' ),
+		placeholder: __( 'Add link' ),
+		autoFocus: true,
+		autoFill: true,
+	},
+	footer: {
+		label: (
+			<FooterMessageLink
+				href={ __( 'https://wordpress.org/support/article/embeds/' ) }
+				value={ __( 'Learn more about embeds' ) }
+			/>
+		),
+		separatorType: 'topFullWidth',
+	},
+};
+
+const EmbedBottomSheet = ( { value, onClose } ) => {
+	const [ isVisible, setIsVisible ] = useState( true );
+	const [ url, setUrl ] = useState( value );
+	const { createErrorNotice } = useDispatch( noticesStore );
+
+	function onCloseWithUrl() {
+		if ( isURL( url ) ) {
+			onClose( { url } );
+		} else {
+			createErrorNotice( __( 'Invalid URL. Please enter a valid URL.' ) );
+			onClose( {} );
+		}
+		setIsVisible( false );
+	}
+
+	return (
+		<LinkSettingsNavigation
+			isVisible={ isVisible }
+			url={ url }
+			setAttributes={ ( attributes ) => setUrl( attributes.url ) }
+			onClose={ onCloseWithUrl }
+			options={ linkSettingsOptions }
+			withBottomSheet
+			showIcon
+		/>
+	);
+};
+
+export default EmbedBottomSheet;
