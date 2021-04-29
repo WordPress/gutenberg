a71b7640eda0a8df26192e7742c438215d6cfbad
George Mamadashvili <georgemamadashvili@gmail.com>
2021-04-29 14:41:57 +0400

    WIP: Site/Template editor welcome guide

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -38,6 +38,7 @@ import NavigationSidebar from '../navigation-sidebar';
 import URLQueryController from '../url-query-controller';
 import InserterSidebar from '../secondary-sidebar/inserter-sidebar';
 import ListViewSidebar from '../secondary-sidebar/list-view-sidebar';
+import WelcomeGuide from '../welcome-guide';
 import { store as editSiteStore } from '../../store';
 
 const interfaceLabels = {
@@ -270,6 +271,7 @@ function Editor( { initialSettings } ) {
 										}
 										footer={ <BlockBreadcrumb /> }
 									/>
+									<WelcomeGuide />
 									<Popover.Slot />
 									<PluginArea />
 								</GlobalStylesProvider>
---
+++ packages/edit-site/src/components/welcome-guide/index.js
@@ -0,0 +1,84 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect, useDispatch } from '@wordpress/data';
+import { Guide } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+/**
+ * Internal dependencies
+ */
+import { DocumentationImage } from './images';
+import { store as editSiteStore } from '../../store';
+
+export default function WelcomeGuide() {
+	const isActive = useSelect(
+		( select ) => select( editSiteStore ).isFeatureActive( 'welcomeGuide' ),
+		[]
+	);
+
+	const { toggleFeature } = useDispatch( editSiteStore );
+
+	if ( ! isActive ) {
+		return null;
+	}
+
+	return (
+		<Guide
+			className="edit-site-welcome-guide"
+			contentLabel={ __( 'Welcome to the design editor' ) }
+			finishButtonText={ __( 'Get started' ) }
+			onFinish={ () => toggleFeature( 'welcomeGuide' ) }
+			pages={ [
+				{
+					image: <DocumentationImage />,
+					content: (
+						<>
+							<h1 className="edit-site-welcome-guide__heading">
+								{ __( 'Welcome to the design editor' ) }
+							</h1>
+							<p className="edit-site-welcome-guide__text">
+								{ __(
+									'The Design Editor allows customizing all aspects of the site using the tools of blocks and patterns. Discover a whole new set of blocks to help build a site.'
+								) }
+							</p>
+						</>
+					),
+				},
+				{
+					image: <DocumentationImage />,
+					content: (
+						<>
+							<h1 className="edit-site-welcome-guide__heading">
+								{ __( 'Create and edit templates' ) }
+							</h1>
+							<p className="edit-site-welcome-guide__text">
+								{ __(
+									'Templates express the layout of the site, including the homepage, archives, posts and pages. Add new layout elements like sidebars, edit the navigation, or customise the appearance of search results.'
+								) }
+							</p>
+						</>
+					),
+				},
+				{
+					image: <DocumentationImage />,
+					content: (
+						<>
+							<h1 className="edit-site-welcome-guide__heading">
+								{ __( 'Paint with global styles' ) }
+							</h1>
+							<p className="edit-site-welcome-guide__text">
+								{ __(
+									'Change how different elements and blocks look across the entire site, including colors, typography, and spacing tools.'
+								) }
+							</p>
+						</>
+					),
+				},
+			] }
+		/>
+	);
+}
