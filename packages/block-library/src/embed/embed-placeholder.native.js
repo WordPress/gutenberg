ebd020ad3705ec09b352fd25471cf909e5b8b29a
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-04-29 17:57:34 +0200

    Use EmbedBottomSheet in native EmbedPlaceholder

--- packages/block-library/src/embed/embed-placeholder.native.js
+++ packages/block-library/src/embed/embed-placeholder.native.js
@@ -8,12 +8,25 @@ import { TextInput } from 'react-native';
  */
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import EmbedBottomSheet from './embed-bottom-sheet';
+
 const EmbedPlaceholder = ( { value } ) => {
 	return (
-		<TextInput
-			value={ value }
-			placeholder={ __( 'Enter URL to embed here…' ) }
-		/>
+		<>
+			<TextInput
+				value={ value }
+				placeholder={ __( 'Enter URL to embed here…' ) }
+			/>
+			<EmbedBottomSheet
+				value={ value }
+				onClose={ () => {
+					// TODO:
+				} }
+			/>
+		</>
 	);
 };
 
