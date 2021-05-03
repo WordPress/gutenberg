47f82714a11d7e6959144855d7088ae33d6c73c9
Greg Ziółkowski <grzegorz@gziolo.pl>
2021-05-03 09:17:33 +0200

    Block Library: Move translatable fields to `block.json` files (#31120)
    * Block Library: Move translatable fields to `block.json` files
    
    * Fix title references in the Embed block

--- packages/block-library/src/post-tags/index.js
+++ packages/block-library/src/post-tags/index.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { __, _x } from '@wordpress/i18n';
 import { tag as icon } from '@wordpress/icons';
 
 /**
@@ -14,8 +13,6 @@ const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
-	title: _x( 'Post Tags', 'block title' ),
-	description: __( "Display a post's tags." ),
 	icon,
 	edit,
 };
--- packages/block-library/src/site-logo/index.js
+++ packages/block-library/src/site-logo/index.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { __, _x } from '@wordpress/i18n';
 import { siteLogo as icon } from '@wordpress/icons';
 
 /**
@@ -14,19 +13,7 @@ const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
-	title: _x( 'Site Logo', 'block title' ),
-	description: __(
-		'Useful for displaying a graphic mark, design, or symbol to represent the site. Once a site logo is set, it can be reused in different places and templates. It should not be confused with the site icon, which is the small image used in the dashboard, browser tabs, public search results, etc, to help recognize a site.'
-	),
 	icon,
-	styles: [
-		{
-			name: 'default',
-			label: _x( 'Default', 'block style' ),
-			isDefault: true,
-		},
-		{ name: 'rounded', label: _x( 'Rounded', 'block style' ) },
-	],
 	supports: {
 		align: true,
 		alignWide: false,
