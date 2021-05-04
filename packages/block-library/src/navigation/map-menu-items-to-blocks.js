2893305b8f80fbc1c1714056d30336641e1491a0
Dave Smith <getdavemail@gmail.com>
2021-05-04 09:31:02 +0100

    Add typedefs for nav_menu_item

--- packages/block-library/src/navigation/map-menu-items-to-blocks.js
+++ packages/block-library/src/navigation/map-menu-items-to-blocks.js
@@ -4,23 +4,31 @@
 import { createBlock, parse } from '@wordpress/blocks';
 
 /**
- * Convert block attributes to menu item.
- *
+ * A WP nav_menu_item object.
  * For more documentation on the individual fields present on a menu item please see:
  * https://core.trac.wordpress.org/browser/tags/5.7.1/src/wp-includes/nav-menu.php#L789
  *
- * @param {Object} menuItem the menu item to be converted to block attributes.
- * @param {Object} menuItem.title stores the raw and rendered versions of the title/label for this menu item.
- * @param {Array} menuItem.xfn the XFN relationships expressed in the link of this menu item.
- * @param {Array} menuItem.classes the HTML class attributes for this menu item.
- * @param {string} menuItem.attr_title the HTML title attribute for this menu item.
- * @param {string} menuItem.object The type of object originally represented, such as 'category', 'post', or 'attachment'.
- * @param {string} menuItem.object_id The DB ID of the original object this menu item represents, e.g. ID for posts and term_id for categories.
- * @param {string} menuItem.description The description of this menu item.
- * @param {string} menuItem.url The URL to which this menu item points.
- * @param {string} menuItem.type The family of objects originally represented, such as 'post_type' or 'taxonomy'.
- * @param {string} menuItem.target The target attribute of the link element for this menu item.
- * @return {Object} the block attributes converted from the menu item.
+ * Changes made here should also be mirrored in packages/edit-navigation/src/store/utils.js.
+ *
+ * @typedef WPNavMenuItem
+ *
+ * @property {Object} title stores the raw and rendered versions of the title/label for this menu item.
+ * @property {Array} xfn the XFN relationships expressed in the link of this menu item.
+ * @property {Array} classes the HTML class attributes for this menu item.
+ * @property {string} attr_title the HTML title attribute for this menu item.
+ * @property {string} object The type of object originally represented, such as 'category', 'post', or 'attachment'.
+ * @property {string} object_id The DB ID of the original object this menu item represents, e.g. ID for posts and term_id for categories.
+ * @property {string} description The description of this menu item.
+ * @property {string} url The URL to which this menu item points.
+ * @property {string} type The family of objects originally represented, such as 'post_type' or 'taxonomy'.
+ * @property {string} target The target attribute of the link element for this menu item.
+ */
+
+/**
+ * Convert block attributes to menu item.
+ *
+ * @param {WPNavMenuItem} menuItem the menu item to be converted to block attributes.
+ * @return {Object} the block attributes converted from the WPNavMenuItem item.
  */
 export const menuItemToBlockAttributes = ( {
 	title: menuItemTitleField,
--- packages/edit-navigation/src/store/utils.js
+++ packages/edit-navigation/src/store/utils.js
@@ -20,6 +20,27 @@ import {
 import { NEW_TAB_TARGET_ATTRIBUTE } from '../constants';
 
 /**
+ * A WP nav_menu_item object.
+ * For more documentation on the individual fields present on a menu item please see:
+ * https://core.trac.wordpress.org/browser/tags/5.7.1/src/wp-includes/nav-menu.php#L789
+ *
+ * Changes made here should also be mirrored in packages/block-library/src/navigation/map-menu-items-to-blocks.js.
+ *
+ * @typedef WPNavMenuItem
+ *
+ * @property {Object} title stores the raw and rendered versions of the title/label for this menu item.
+ * @property {Array} xfn the XFN relationships expressed in the link of this menu item.
+ * @property {Array} classes the HTML class attributes for this menu item.
+ * @property {string} attr_title the HTML title attribute for this menu item.
+ * @property {string} object The type of object originally represented, such as 'category', 'post', or 'attachment'.
+ * @property {string} object_id The DB ID of the original object this menu item represents, e.g. ID for posts and term_id for categories.
+ * @property {string} description The description of this menu item.
+ * @property {string} url The URL to which this menu item points.
+ * @property {string} type The family of objects originally represented, such as 'post_type' or 'taxonomy'.
+ * @property {string} target The target attribute of the link element for this menu item.
+ */
+
+/**
  * Builds an ID for a new navigation post.
  *
  * @param {number} menuId Menu id.
@@ -236,20 +257,7 @@ export const blockAttributesToMenuItem = ( {
 /**
  * Convert block attributes to menu item.
  *
- * For more documentation on the individual fields present on a menu item please see:
- * https://core.trac.wordpress.org/browser/tags/5.7.1/src/wp-includes/nav-menu.php#L789
- *
- * @param {Object} menuItem the menu item to be converted to block attributes.
- * @param {Object} menuItem.title stores the raw and rendered versions of the title/label for this menu item.
- * @param {Array} menuItem.xfn the XFN relationships expressed in the link of this menu item.
- * @param {Array} menuItem.classes the HTML class attributes for this menu item.
- * @param {string} menuItem.attr_title the HTML title attribute for this menu item.
- * @param {string} menuItem.object The type of object originally represented, such as 'category', 'post', or 'attachment'.
- * @param {string} menuItem.object_id The DB ID of the original object this menu item represents, e.g. ID for posts and term_id for categories.
- * @param {string} menuItem.description The description of this menu item.
- * @param {string} menuItem.url The URL to which this menu item points.
- * @param {string} menuItem.type The family of objects originally represented, such as 'post_type' or 'taxonomy'.
- * @param {string} menuItem.target The target attribute of the link element for this menu item.
+ * @param {WPNavMenuItem} menuItem the menu item to be converted to block attributes.
  * @return {Object} the block attributes converted from the menu item.
  */
 export const menuItemToBlockAttributes = ( {
