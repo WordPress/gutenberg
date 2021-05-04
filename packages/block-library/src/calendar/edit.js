46467299fcb314adedfdbb7fabc0b7ee827cd487
David Szabo <david.szabo97@gmail.com>
2021-05-04 17:39:46 +0200

    Add placeholder when no posts exist

--- packages/block-library/src/calendar/edit.js
+++ packages/block-library/src/calendar/edit.js
@@ -7,11 +7,14 @@ import memoize from 'memize';
 /**
  * WordPress dependencies
  */
-import { Disabled } from '@wordpress/components';
+import { calendar as icon } from '@wordpress/icons';
+import { Disabled, Placeholder, Spinner } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import ServerSideRender from '@wordpress/server-side-render';
 import { useBlockProps } from '@wordpress/block-editor';
 import { store as editorStore } from '@wordpress/editor';
+import { store as coreStore } from '@wordpress/core-data';
+import { __ } from '@wordpress/i18n';
 
 const getYearMonth = memoize( ( date ) => {
 	if ( ! date ) {
@@ -25,20 +28,46 @@ const getYearMonth = memoize( ( date ) => {
 } );
 
 export default function CalendarEdit( { attributes } ) {
-	const date = useSelect( ( select ) => {
+	const blockProps = useBlockProps();
+	const { date, hasPosts, hasPostsResolved } = useSelect( ( select ) => {
+		const { getEntityRecords, hasFinishedResolution } = select( coreStore );
 		const { getEditedPostAttribute } = select( editorStore );
+
+		const query = {
+			status: 'publish',
+			per_page: 1,
+		};
+		const posts = getEntityRecords( 'postType', 'post', query );
+		const postsResolved = hasFinishedResolution( 'getEntityRecords', [
+			'postType',
+			'post',
+			query,
+		] );
 
 		const postType = getEditedPostAttribute( 'type' );
 		// Dates are used to overwrite year and month used on the calendar.
 		// This overwrite should only happen for 'post' post types.
 		// For other post types the calendar always displays the current month.
-		return postType === 'post'
-			? getEditedPostAttribute( 'date' )
-			: undefined;
+		const _date =
+			postType === 'post' ? getEditedPostAttribute( 'date' ) : undefined;
+		return {
+			date: _date,
+			hasPostsResolved: postsResolved,
+			hasPosts:
+				postsResolved && Array.isArray( posts ) && posts.length === 1,
+		};
 	}, [] );
 
+	if ( ! hasPosts ) {
+		return (
+			<Placeholder icon={ icon } label={ __( 'Calendar' ) }>
+				{ ! hasPostsResolved ? <Spinner /> : __( 'No posts found.' ) }
+			</Placeholder>
+		);
+	}
+
 	return (
-		<div { ...useBlockProps() }>
+		<div { ...blockProps }>
 			<Disabled>
 				<ServerSideRender
 					block="core/calendar"
