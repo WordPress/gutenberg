f060f9c0709f223f717d819de2bf67a48e793b41
Carlos Garcia <fluiddot@gmail.com>
2021-05-05 19:03:44 +0200

    Revert "Merge branch 'update/inserter-block-search' into rnmobile/add/2575-reusable-blocks-inserter"
    This reverts commit 4df39cd8e39906125e39254ae4b5856a4d74249c, reversing
    changes made to 575c58f0b5d9838edef4badd4033676424e72334.
    
    # Conflicts:
    #	packages/block-editor/src/components/inserter/search-form.native.js
    #	packages/block-editor/src/components/inserter/style.native.scss

--- packages/block-editor/src/components/inserter/search-form.native.js
+++ packages/block-editor/src/components/inserter/search-form.native.js
@@ -1,135 +1,46 @@
 /**
  * External dependencies
  */
-import {
-	TextInput,
-	Text,
-	View,
-	Platform,
-	TouchableOpacity,
-} from 'react-native';
+import { TextInput, View, TouchableHighlight } from 'react-native';
 
 /**
  * WordPress dependencies
  */
-import { useState, useRef, useCallback, useMemo } from '@wordpress/element';
-import { usePreferredColorSchemeStyleBem } from '@wordpress/compose';
+import { useState, useRef, useCallback } from '@wordpress/element';
+import { usePreferredColorSchemeStyle } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
-import { Gridicons } from '@wordpress/components';
-import { Icon, cancelCircleFilled, arrowLeft, close } from '@wordpress/icons';
+import { ToolbarButton } from '@wordpress/components';
+import {
+	Icon,
+	cancelCircleFilled,
+	arrowLeft,
+	search as searchIcon,
+} from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
-import platformStyles from './searchFormStyles.scss';
-
-function IconButton( { icon, label, hint, style, ...props } ) {
-	return (
-		<TouchableOpacity
-			{ ...props }
-			accessible={ true }
-			accessibilityRole={ 'button' }
-			accessibilityHint={ hint }
-			accessibilityLabel={ label }
-			style={ { alignItems: 'center' } }
-		>
-			<Icon icon={ icon } fill={ style?.color } color={ style?.color } />
-		</TouchableOpacity>
-	);
-}
 
 function InserterSearchForm( { value, onChange, onFocus } ) {
 	const [ isActive, setIsActive ] = useState( false );
 
-	const isIOS = Platform.OS === 'ios';
-
 	const inputRef = useRef();
 
-	const baseStyles = usePreferredColorSchemeStyleBem(
-		styles,
-		platformStyles
+	const searchFormStyle = usePreferredColorSchemeStyle(
+		styles.searchForm,
+		styles.searchFormDark
 	);
 
-	const activeStyles = useMemo( () => {
-		const activeSelectors = Object.keys( baseStyles ).filter(
-			( key ) => !! key.match( /active/ )
-		);
+	const searchFormInputStyle = usePreferredColorSchemeStyle(
+		styles.searchFormInput,
+		styles.searchFormInputDark
+	);
 
-		const _activeStyles = { ...styles };
-
-		activeSelectors.forEach( ( activeSelector ) => {
-			const selector = activeSelector
-				.replace( /-active/, '' )
-				.replace( /-$/, '' );
-
-			_activeStyles[ selector ] = {
-				...baseStyles[ selector ],
-				...baseStyles[ activeSelector ],
-			};
-		} );
-		return _activeStyles;
-	}, [] );
-
-	const newStyles = useMemo( () => {
-		return isActive ? activeStyles : baseStyles;
-	}, [ isActive ] );
-
-	const {
-		'inserter-search-form__container': containerStyle,
-		'inserter-search-form__input-container': inputContainerStyle,
-		'inserter-search-form__form-input': formInputStyle,
-		'inserter-search-form__form-input-placeholder': placeholderStyle,
-		'inserter-search-form__input-button': inputButtonStyle,
-		'inserter-search-form__input-button-left': inputButtonLeftStyle,
-		'inserter-search-form__input-button-right': inputButtonRightStyle,
-		'inserter-search-form__cancel-button': cancelButtonStyle,
-		'inserter-search-form__cancel-button-text': cancelButtonTextStyle,
-		'inserter-search-form__icon': iconStyle,
-	} = newStyles;
-
-	function clearInput() {
-		onChange( '' );
-	}
-
-	function onCancel() {
-		inputRef.current.blur();
-		clearInput();
-		onActive( false );
-	}
-
-	function renderLeftButton() {
-		if ( ! isIOS && isActive ) {
-			return (
-				<IconButton
-					label={ __( 'Cancel Search' ) }
-					icon={ arrowLeft }
-					onPress={ onCancel }
-					style={ iconStyle }
-				/>
-			);
-		}
-
-		return <Icon icon={ Gridicons.search } fill={ iconStyle.color } />;
-	}
-
-	function renderRightButton() {
-		// Add a View element to properly center the input placeholder via flexbox
-		if ( isIOS && ! isActive ) {
-			return <View />;
-		}
-
-		if ( !! value ) {
-			return (
-				<IconButton
-					label={ __( 'Clear search' ) }
-					icon={ isIOS ? cancelCircleFilled : close }
-					onPress={ clearInput }
-					style={ iconStyle }
-				/>
-			);
-		}
-	}
+	const placeholderStyle = usePreferredColorSchemeStyle(
+		styles.searchFormPlaceholder,
+		styles.searchFormPlaceholderDark
+	);
 
 	const onActive = useCallback(
 		( active ) => {
@@ -142,49 +53,49 @@ function InserterSearchForm( { value, onChange, onFocus } ) {
 	);
 
 	return (
-		<TouchableOpacity
-			style={ containerStyle }
-			onPress={ () => {
-				onActive( true );
-				inputRef.current.focus();
-			} }
-			activeOpacity={ 1 }
-		>
-			<View style={ inputContainerStyle }>
-				<View
-					style={ { ...inputButtonStyle, ...inputButtonLeftStyle } }
-				>
-					{ renderLeftButton() }
-				</View>
+		<TouchableHighlight accessible={ false }>
+			<View style={ searchFormStyle }>
+				{ isActive ? (
+					<ToolbarButton
+						title={ __( 'Cancel search' ) }
+						icon={ arrowLeft }
+						onClick={ () => {
+							inputRef.current.blur();
+							onChange( '' );
+							onActive( false );
+						} }
+					/>
+				) : (
+					<ToolbarButton
+						title={ __( 'Search block' ) }
+						icon={ searchIcon }
+						onClick={ () => {
+							inputRef.current.focus();
+							onActive( true );
+						} }
+					/>
+				) }
 				<TextInput
 					ref={ inputRef }
-					style={ formInputStyle }
+					style={ searchFormInputStyle }
 					placeholderTextColor={ placeholderStyle.color }
 					onChangeText={ onChange }
 					onFocus={ () => onActive( true ) }
 					value={ value }
 					placeholder={ __( 'Search blocks' ) }
 				/>
-				<View
-					style={ { ...inputButtonStyle, ...inputButtonRightStyle } }
-				>
-					{ renderRightButton() }
-				</View>
+
+				{ !! value && (
+					<ToolbarButton
+						title={ __( 'Clear search' ) }
+						icon={ <Icon icon={ cancelCircleFilled } /> }
+						onClick={ () => {
+							onChange( '' );
+						} }
+					/>
+				) }
 			</View>
-			{ isActive && isIOS && (
-				<View style={ cancelButtonStyle }>
-					<Text
-						onPress={ onCancel }
-						style={ cancelButtonTextStyle }
-						accessible={ true }
-						accessibilityRole={ 'button' }
-						accessibilityLabel={ __( 'Cancel Search' ) }
-					>
-						{ __( 'Cancel' ) }
-					</Text>
-				</View>
-			) }
-		</TouchableOpacity>
+		</TouchableHighlight>
 	);
 }
 
