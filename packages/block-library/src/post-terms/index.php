509fc9d794a84aa29e553d207995c8fad0f101e9
Riad Benguella <benguella@gmail.com>
2021-05-05 12:58:03 +0100

    Unify the post tags and post categories block under a unique post terms block. (#31458)

--- packages/block-library/src/post-terms/index.php
+++ packages/block-library/src/post-terms/index.php
@@ -1,35 +1,35 @@
 <?php
 /**
- * Server-side rendering of the `core/post-hierarchical-terms` block.
+ * Server-side rendering of the `core/post-terms` block.
  *
  * @package WordPress
  */
 
 /**
- * Renders the `core/post-hierarchical-terms` block on the server.
+ * Renders the `core/post-terms` block on the server.
  *
  * @param array    $attributes Block attributes.
  * @param string   $content    Block default content.
  * @param WP_Block $block      Block instance.
- * @return string Returns the filtered post hierarchical terms for the current post wrapped inside "a" tags.
+ * @return string Returns the filtered post terms for the current post wrapped inside "a" tags.
  */
-function render_block_core_post_hierarchical_terms( $attributes, $content, $block ) {
+function render_block_core_post_terms( $attributes, $content, $block ) {
 	if ( ! isset( $block->context['postId'] ) || ! isset( $attributes['term'] ) ) {
 		return '';
 	}
 
-	$post_hierarchical_terms = get_the_terms( $block->context['postId'], $attributes['term'] );
-	if ( is_wp_error( $post_hierarchical_terms ) ) {
+	$post_terms = get_the_terms( $block->context['postId'], $attributes['term'] );
+	if ( is_wp_error( $post_terms ) ) {
 		return '';
 	}
-	if ( empty( $post_hierarchical_terms ) ) {
+	if ( empty( $post_terms ) ) {
 		return '';
 	}
 
 	$align_class_name = empty( $attributes['textAlign'] ) ? '' : ' ' . "has-text-align-{$attributes['textAlign']}";
 
 	$terms_links = '';
-	foreach ( $post_hierarchical_terms as $term ) {
+	foreach ( $post_terms as $term ) {
 		$terms_links .= sprintf(
 			'<a href="%1$s">%2$s</a> | ',
 			get_term_link( $term->term_id ),
@@ -47,14 +47,14 @@ function render_block_core_post_hierarchical_terms( $attributes, $content, $bloc
 }
 
 /**
- * Registers the `core/post-hierarchical-terms` block on the server.
+ * Registers the `core/post-terms` block on the server.
  */
-function register_block_core_post_hierarchical_terms() {
+function register_block_core_post_terms() {
 	register_block_type_from_metadata(
-		__DIR__ . '/post-hierarchical-terms',
+		__DIR__ . '/post-terms',
 		array(
-			'render_callback' => 'render_block_core_post_hierarchical_terms',
+			'render_callback' => 'render_block_core_post_terms',
 		)
 	);
 }
-add_action( 'init', 'register_block_core_post_hierarchical_terms' );
+add_action( 'init', 'register_block_core_post_terms' );
