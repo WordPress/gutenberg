974dd72bb8a93252fb5d9666d50c15ccd0521431
Antonis Lilis <antonis.lilis@automattic.com>
2021-05-06 13:24:53 +0300

    Revert "Uses existing ANIMATION_DURATION for clarity"
    This reverts commit 8613bc76cfeebbbb8c6430dea7fee062766b6796.

--- packages/components/src/color-palette/index.native.js
+++ packages/components/src/color-palette/index.native.js
@@ -25,6 +25,7 @@ import ColorIndicator from '../color-indicator';
 import { colorsUtils } from '../mobile/color-settings/utils';
 
 const ANIMATION_DURATION = 200;
+const SEGMENTED_CONTROL_ANIMATION_DURATION_DELAY = 200;
 
 let customIndicatorWidth = 0;
 
@@ -97,10 +98,9 @@ function ColorPalette( {
 	const customText = __( 'Custom' );
 
 	useEffect( () => {
-		const delayedScroll = setTimeout(
-			resetScrollPosition,
-			ANIMATION_DURATION
-		);
+		const delayedScroll = setTimeout( () => {
+			resetScrollPosition();
+		}, SEGMENTED_CONTROL_ANIMATION_DURATION_DELAY );
 		return () => {
 			clearTimeout( delayedScroll );
 		};
