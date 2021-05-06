8aa7c005a3486396b14e52e5c6d52b5e649a957b
Bernie Reiter <ockham@raz.or.at>
2021-05-06 19:05:28 +0200

    Add test for custom block templates

--- phpunit/class-template-loader-test.php
+++ phpunit/class-template-loader-test.php
@@ -18,6 +18,26 @@ class Template_Loader_Test extends WP_UnitTestCase {
 		gutenberg_register_template_part_post_type();
 		gutenberg_register_wp_theme_taxonomy();
 		gutenberg_register_wp_template_part_area_taxonomy();
+
+		// Set up custom template post.
+		$args       = array(
+			'post_type'    => 'wp_template',
+			'post_name'    => 'wp-custom-template-my-block-template',
+			'post_title'   => 'My Custom Block Template',
+			'post_content' => 'Content',
+			'post_excerpt' => 'Description of my block template',
+			'tax_input'    => array(
+				'wp_theme' => array(
+					get_stylesheet(),
+				),
+			),
+		);
+		self::$post = self::factory()->post->create_and_get( $args );
+		wp_set_post_terms( self::$post->ID, get_stylesheet(), 'wp_theme' );
+	}
+
+	public static function wpTearDownAfterClass() {
+		wp_delete_post( self::$post->ID );
 	}
 
 	public function tearDown() {
@@ -135,7 +155,7 @@ class Template_Loader_Test extends WP_UnitTestCase {
 	/**
 	 * Regression: https://github.com/WordPress/gutenberg/issues/31399.
 	 */
-	function test_gutenberg_custom_page_template_takes_precedence_over_all_other_templates() {
+	function test_gutenberg_custom_page_php_template_takes_precedence_over_all_other_templates() {
 		$custom_page_template      = 'templates/full-width.php';
 		$custom_page_template_path = get_stylesheet_directory() . '/' . $custom_page_template;
 		$type                      = 'page';
@@ -147,6 +167,28 @@ class Template_Loader_Test extends WP_UnitTestCase {
 		);
 		$resolved_template_path    = gutenberg_override_query_template( $custom_page_template_path, $type, $templates );
 		$this->assertEquals( $custom_page_template_path, $resolved_template_path );
+	}
+
+	/**
+	 * Covers: https://github.com/WordPress/gutenberg/pull/30438.
+	 */
+	function test_gutenberg_custom_page_block_template_takes_precedence_over_all_other_templates() {
+		global $_wp_current_template_content;
+
+		$custom_page_block_template = 'wp-custom-template-my-block-template';
+		$page_template_path         = get_stylesheet_directory() . '/' . 'page.php';
+		$type                       = 'page';
+		$templates                  = array(
+			$custom_page_block_template,
+			'page-slug.php',
+			'page-1.php',
+			'page.php',
+		);
+		$resolved_template_path     = gutenberg_override_query_template( $page_template_path, $type, $templates );
+		$this->assertEquals( gutenberg_dir_path() . 'lib/template-canvas.php', $resolved_template_path );
+
+		$expected_template = gutenberg_get_block_template( get_stylesheet() . '//' . $custom_page_block_template );
+		$this->assertEquals( $expected_template->content, $_wp_current_template_content );
 	}
 
 	function change_template_directory( $template_dir ) {
