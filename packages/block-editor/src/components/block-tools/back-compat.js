1de2eb8c6735b2f8ae86d49131f2c04d8dc46bc1
Ella van Durpe <ella@vandurpe.com>
2021-05-06 13:06:18 +0300

    Fix compat layer in disabled context

--- packages/block-editor/src/components/block-tools/back-compat.js
+++ packages/block-editor/src/components/block-tools/back-compat.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { useContext } from '@wordpress/element';
+import { Disabled } from '@wordpress/components';
 import deprecated from '@wordpress/deprecated';
 
 /**
@@ -12,9 +13,10 @@ import BlockPopover from './block-popover';
 
 export default function BlockToolsBackCompat( { children } ) {
 	const openRef = useContext( InsertionPointOpenRef );
+	const isDisabled = useContext( Disabled.Context );
 
 	// If context is set, `BlockTools` is a parent component.
-	if ( openRef ) {
+	if ( openRef || isDisabled ) {
 		return children;
 	}
 
