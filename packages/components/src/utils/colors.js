7ebb41ed553143b2044431dce04dad44094f7b1f
Marco Ciampini <marco.ciampo@gmail.com>
2021-05-10 11:28:56 +0200

    typo

--- packages/components/src/utils/colors.js
+++ packages/components/src/utils/colors.js
@@ -10,7 +10,7 @@ import tinycolor from 'tinycolor2';
 import { COLORS } from './colors-values';
 
 /**
- * Generating a CSS complient rgba() color value.
+ * Generating a CSS compliant rgba() color value.
  *
  * @param {string} hexValue The hex value to convert to rgba().
  * @param {number} alpha The alpha value for opacity.
