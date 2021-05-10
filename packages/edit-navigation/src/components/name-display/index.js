6da8424e7e85d7865a02e1337ef2a4e7de289c47
George Mamadashvili <georgemamadashvili@gmail.com>
2021-05-10 09:49:23 +0400

    Navigation Editor: Introduce useMenuEntityProp hook (#31132)
    * Update components to use 'useMenuEntityProp' for convenience

--- packages/edit-navigation/src/components/name-display/index.js
+++ packages/edit-navigation/src/components/name-display/index.js
@@ -9,20 +9,20 @@ import { BlockControls } from '@wordpress/block-editor';
  */
 import {
 	untitledMenu,
-	useMenuEntity,
 	useSelectedMenuId,
+	useMenuEntityProp,
 	IsMenuNameControlFocusedContext,
 } from '../../hooks';
 
 import { sprintf, __ } from '@wordpress/i18n';
 export default function NameDisplay() {
 	const [ menuId ] = useSelectedMenuId();
-	const { editedMenu } = useMenuEntity( menuId );
+	const [ name ] = useMenuEntityProp( 'name', menuId );
 	const [ , setIsMenuNameEditFocused ] = useContext(
 		IsMenuNameControlFocusedContext
 	);
 
-	const menuName = editedMenu?.name ?? untitledMenu;
+	const menuName = name ?? untitledMenu;
 
 	return (
 		<BlockControls>
