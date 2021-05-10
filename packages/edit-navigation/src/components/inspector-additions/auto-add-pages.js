1d21ece9005d2a46910a9ea9ff7eb4a1460d997c
George Mamadashvili <georgemamadashvili@gmail.com>
2021-05-10 09:19:46 +0400

    Revert changes to auto add pages

--- packages/edit-navigation/src/components/inspector-additions/auto-add-pages.js
+++ packages/edit-navigation/src/components/inspector-additions/auto-add-pages.js
@@ -1,19 +1,25 @@
 /**
  * WordPress dependencies
  */
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useState, useEffect } from '@wordpress/element';
 import { ToggleControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-/**
- * Internal dependencies
- */
-import { useMenuEntityProp } from '../../hooks';
-
 export default function AutoAddPages( { menuId } ) {
-	const [ autoAddPages, setAutoAddPages ] = useMenuEntityProp(
-		'auto_add',
-		menuId
-	);
+	const menu = useSelect( ( select ) => select( 'core' ).getMenu( menuId ), [
+		menuId,
+	] );
+
+	const [ autoAddPages, setAutoAddPages ] = useState( null );
+
+	useEffect( () => {
+		if ( autoAddPages === null && menu ) {
+			setAutoAddPages( menu.auto_add );
+		}
+	}, [ autoAddPages, menu ] );
+
+	const { saveMenu } = useDispatch( 'core' );
 
 	return (
 		<ToggleControl
@@ -22,7 +28,13 @@ export default function AutoAddPages( { menuId } ) {
 				'Automatically add published top-level pages to this menu.'
 			) }
 			checked={ autoAddPages ?? false }
-			onChange={ setAutoAddPages }
+			onChange={ ( newAutoAddPages ) => {
+				setAutoAddPages( newAutoAddPages );
+				saveMenu( {
+					...menu,
+					auto_add: newAutoAddPages,
+				} );
+			} }
 		/>
 	);
 }
