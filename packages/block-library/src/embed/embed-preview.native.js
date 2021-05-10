3a5574a3611ffa688ffd7e518f5acaf02326b09d
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-05-10 15:12:28 +0200

    Create native EmbedPreview component

---
+++ packages/block-library/src/embed/embed-preview.native.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { View } from 'react-native';
+
+const EmbedPreview = () => {
+	return (
+		<View style={ { height: 50, widht: '100%', backgroundColor: 'red' } } />
+	);
+};
+
+export default EmbedPreview;
