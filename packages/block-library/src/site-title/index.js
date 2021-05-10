c9ba600776d6955ed42e0ca120106a27f0b93a61
Koen Van den Wijngaert <koen@neok.be>
2021-05-10 10:02:30 +0200

    Site title: use AccessibleSteps for settings link.

--- packages/block-library/src/site-title/index.js
+++ packages/block-library/src/site-title/index.js
@@ -10,6 +10,7 @@ import { __ } from '@wordpress/i18n';
  */
 import metadata from './block.json';
 import edit from './edit';
+import AccessibleSteps from './util/accessible-steps';
 
 const { name } = metadata;
 export { metadata, name };
@@ -17,10 +18,16 @@ export { metadata, name };
 export const settings = {
 	description: createInterpolateElement(
 		__(
-			'Displays and allows editing the name of the site. The site title usually appears in the browser title bar, in search results, and more. Also available in <a>Settings > General</a>.'
+			'Displays and allows editing the name of the site. The site title usually appears in the browser title bar, in search results, and more. Also available in <path/>.'
 		),
-		// eslint-disable-next-line jsx-a11y/anchor-has-content
-		{ a: <a href="options-general.php" /> }
+		{
+			path: (
+				<AccessibleSteps
+					elements={ [ __( 'Settings' ), __( 'General' ) ] }
+					link="options-general.php"
+				/>
+			),
+		}
 	),
 	icon,
 	edit,
