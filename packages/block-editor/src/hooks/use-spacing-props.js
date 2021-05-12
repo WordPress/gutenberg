0c298fc2fcadf55c6d7b638b89ba8cefa3642232
Staci Cooper <staci.cooper@automattic.com>
2021-05-12 16:21:06 -0700

    Reflect style selection on frontend

--- packages/block-editor/src/hooks/use-spacing-props.js
+++ packages/block-editor/src/hooks/use-spacing-props.js
@@ -8,13 +8,14 @@ import { getInlineStyles } from './style';
 // styles still need to be generated so they can be applied to inner elements.
 
 /**
- * Determines the spacing related props for a block derived from its spacing block
- * support attributes.
+ * Provides the CSS class names and inline styles for a block's spacing support
+ * attributes.
  *
  * @param  {Object} attributes Block attributes.
- * @return {Object}            ClassName & style props from spacing block support.
+ *
+ * @return {Object} Spacing block support derived CSS classes & styles.
  */
-export function useSpacingProps( attributes ) {
+export function getSpacingClassesAndStyles( attributes ) {
 	const { style } = attributes;
 
 	// Collect inline styles for spacing.
--- packages/block-library/src/button/edit.js
+++ packages/block-library/src/button/edit.js
@@ -25,7 +25,7 @@ import {
 	useBlockProps,
 	__experimentalUseBorderProps as useBorderProps,
 	__experimentalUseColorProps as useColorProps,
-	__experimentalUseSpacingProps as useSpacingProps,
+	__experimentalGetSpacingClassesAndStyles as useSpacingProps,
 	__experimentalLinkControl as LinkControl,
 } from '@wordpress/block-editor';
 import { rawShortcut, displayShortcut } from '@wordpress/keycodes';
