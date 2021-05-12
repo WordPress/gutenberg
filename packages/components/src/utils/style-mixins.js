683e89d7bfc77fb84b7f4a44a8a9f096dc7615cc
Ella van Durpe <wp@iseulde.com>
2021-05-12 17:30:35 +0300

    Remove config utility function in favour of using the values directly

--- packages/components/src/utils/style-mixins.js
+++ packages/components/src/utils/style-mixins.js
@@ -4,6 +4,5 @@ export { rtl } from './rtl';
 export { space } from './space';
 export { font } from './font';
 export { breakpoint } from './breakpoint';
-export { config } from './config';
 export { default as CONFIG } from './config-values';
 export { COLORS } from './colors-values';
