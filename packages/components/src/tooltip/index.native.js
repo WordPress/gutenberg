6ca264152932a2101c2ae5f9db5f9854021585fd
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-05-11 20:41:37 -0500

    Name ref explicitly
    Include the phrase "ref" in the name to communicate the actual value.

--- packages/components/src/tooltip/index.native.js
+++ packages/components/src/tooltip/index.native.js
@@ -37,7 +37,7 @@ const Tooltip = ( {
 	visible: initialVisible = false,
 } ) => {
 	const referenceElementRef = useRef( null );
-	const referenceMeasureTimeout = useRef( null );
+	const referenceMeasureTimeoutRef = useRef( null );
 	const animationValue = useRef( new Animated.Value( 0 ) ).current;
 	const [ , horizontalPosition = 'center' ] = position.split( ' ' );
 	const [ visible, setVisible ] = useState( initialVisible );
@@ -120,9 +120,9 @@ const Tooltip = ( {
 	];
 
 	const getReferenceElementPosition = () => {
-		clearTimeout( referenceMeasureTimeout.current );
+		clearTimeout( referenceMeasureTimeoutRef.current );
 		// Timeout used allow render to occur before calculating layout
-		referenceMeasureTimeout.current = setTimeout( () => {
+		referenceMeasureTimeoutRef.current = setTimeout( () => {
 			referenceElementRef.current.measure(
 				( _x, _y, width, height, pageX, pageY ) => {
 					setReferenceLayout( {
