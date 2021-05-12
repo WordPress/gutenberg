67ce0aa75a45c4c8289a92731fb36fe9dc357658
Joel Dean <jdeanjj1000@gmail.com>
2021-05-11 19:20:31 -0500

    added help support to switch cell accessibility capabilities

--- packages/components/src/mobile/bottom-sheet/switch-cell.native.js
+++ packages/components/src/mobile/bottom-sheet/switch-cell.native.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { Switch } from 'react-native';
+import { isEmpty } from 'lodash';
 /**
  * WordPress dependencies
  */
@@ -12,28 +13,45 @@ import { __, _x, sprintf } from '@wordpress/i18n';
 import Cell from './cell';
 
 export default function BottomSheetSwitchCell( props ) {
-	const { value, onValueChange, ...cellProps } = props;
+	const { value, onValueChange, help, ...cellProps } = props;
 
 	const onPress = () => {
 		onValueChange( ! value );
 	};
 
-	const accessibilityLabel = value
-		? sprintf(
-				/* translators: accessibility text. Switch setting ON state. %s: Switch title. */
-				_x( '%s. On', 'switch control' ),
-				cellProps.label
-		  )
-		: sprintf(
-				/* translators: accessibility text. Switch setting OFF state. %s: Switch title. */
-				_x( '%s. Off', 'switch control' ),
-				cellProps.label
-		  );
+	const getAccessibilityLabel = () => {
+		if ( isEmpty( help ) ) {
+			return value
+				? sprintf(
+						/* translators: accessibility text. Switch setting ON state. %s: Switch title. */
+						_x( '%s. On', 'switch control' ),
+						cellProps.label
+				  )
+				: sprintf(
+						/* translators: accessibility text. Switch setting OFF state. %s: Switch title. */
+						_x( '%s. Off', 'switch control' ),
+						cellProps.label
+				  );
+		}
+		return value
+			? sprintf(
+					/* translators: accessibility text. Switch setting ON state. %1: Switch title, %2: switch help. */
+					_x( '%1$s, %2$s. On', 'switch control' ),
+					cellProps.label,
+					help
+			  )
+			: sprintf(
+					/* translators: accessibility text. Switch setting OFF state. %1: Switch title, %2: switch help. */
+					_x( '%1$s, %2$s. Off', 'switch control' ),
+					cellProps.label,
+					help
+			  );
+	};
 
 	return (
 		<Cell
 			{ ...cellProps }
-			accessibilityLabel={ accessibilityLabel }
+			accessibilityLabel={ getAccessibilityLabel() }
 			accessibilityRole={ 'none' }
 			accessibilityHint={
 				/* translators: accessibility text (hint for switches) */
