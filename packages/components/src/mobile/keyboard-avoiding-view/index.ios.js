1101bc7d55e193769cfe55f89f12208ccbb2ace7
Antonis Lilis <antonis.lilis@automattic.com>
2021-05-13 18:35:20 +0300

    Sets keyboard vertical offset to zero in landscape mode

--- packages/components/src/mobile/keyboard-avoiding-view/index.ios.js
+++ packages/components/src/mobile/keyboard-avoiding-view/index.ios.js
@@ -40,8 +40,13 @@ export const KeyboardAvoidingView = ( {
 
 	const animatedHeight = useRef( new Animated.Value( MIN_HEIGHT ) ).current;
 
+	const isLandscape = () => {
+		const dim = Dimensions.get( 'screen' );
+		return dim.width >= dim.height;
+	};
+
 	const { height: fullHeight } = Dimensions.get( 'window' );
-	const keyboardVerticalOffset = fullHeight - parentHeight;
+	const keyboardVerticalOffset = isLandscape ? 0 : fullHeight - parentHeight;
 
 	useEffect( () => {
 		SafeArea.getSafeAreaInsetsForRootView().then(
