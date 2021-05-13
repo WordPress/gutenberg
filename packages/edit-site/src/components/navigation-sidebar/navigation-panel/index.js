762cb666141ab9e38bffc3f6251fb552d2ac49bb
Mitchell Austin <mr.fye@oneandthesame.net>
2021-05-13 15:51:37 -0700

    Monopolize navigation in Site Editor

--- packages/edit-site/src/components/navigation-sidebar/navigation-panel/index.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-panel/index.js
@@ -6,34 +6,81 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
+import {
+	__experimentalNavigation as Navigation,
+	__experimentalNavigationBackButton as NavigationBackButton,
+	__experimentalNavigationGroup as NavigationGroup,
+	__experimentalNavigationItem as NavigationItem,
+	__experimentalNavigationMenu as NavigationMenu,
+} from '@wordpress/components';
 import { usePrevious } from '@wordpress/compose';
 import { store as coreDataStore } from '@wordpress/core-data';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useState, useEffect, useRef } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import { ESCAPE } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
  */
-import ContentNavigation from './content-navigation';
-import TemplatesNavigation from './templates-navigation';
-import { MENU_ROOT } from './constants';
+import ContentPagesMenu from './menus/content-pages';
+import ContentCategoriesMenu from './menus/content-categories';
+import ContentPostsMenu from './menus/content-posts';
+import TemplatesMenu from './menus/templates';
+import TemplatePartsMenu from './menus/template-parts';
+import MainDashboardButton from '../../main-dashboard-button';
 import { store as editSiteStore } from '../../../store';
+import {
+	MENU_ROOT,
+	MENU_TEMPLATE_PARTS,
+	MENU_TEMPLATES,
+	MENU_CONTENT_CATEGORIES,
+	MENU_CONTENT_PAGES,
+	MENU_CONTENT_POSTS,
+} from './constants';
 
 const NavigationPanel = ( { isOpen } ) => {
 	const [ contentActiveMenu, setContentActiveMenu ] = useState( MENU_ROOT );
-	const { templatesActiveMenu, siteTitle } = useSelect( ( select ) => {
-		const { getNavigationPanelActiveMenu } = select( editSiteStore );
+	const {
+		page: { context: { postType, postId } = {} } = {},
+		editedPostId,
+		editedPostType,
+		templatesActiveMenu,
+		siteTitle,
+	} = useSelect( ( select ) => {
+		const {
+			getEditedPostType,
+			getEditedPostId,
+			getNavigationPanelActiveMenu,
+			getPage,
+		} = select( editSiteStore );
 		const { getEntityRecord } = select( coreDataStore );
 
 		const siteData =
 			getEntityRecord( 'root', '__unstableBase', undefined ) || {};
 
 		return {
+			page: getPage(),
+			editedPostId: getEditedPostId(),
+			editedPostType: getEditedPostType(),
 			templatesActiveMenu: getNavigationPanelActiveMenu(),
 			siteTitle: siteData.name,
 		};
 	}, [] );
+
+	const { setNavigationPanelActiveMenu } = useDispatch( editSiteStore );
+
+	const isTemplateActiveRoot = templatesActiveMenu === MENU_ROOT;
+	const isContentActiveRoot = contentActiveMenu === MENU_ROOT;
+	let activeMenu;
+	let activeItem;
+	if ( isTemplateActiveRoot && isContentActiveRoot ) {
+		activeMenu = MENU_ROOT;
+	} else {
+		[ activeMenu, activeItem ] = isTemplateActiveRoot
+			? [ contentActiveMenu, `${ postType }-${ postId }` ]
+			: [ templatesActiveMenu, `${ editedPostType }-${ editedPostId }` ];
+	}
 
 	// Ensures focus is moved to the panel area when it is activated
 	// from a separate component (such as document actions in the header).
@@ -80,14 +127,67 @@ const NavigationPanel = ( { isOpen } ) => {
 				</div>
 
 				<div className="edit-site-navigation-panel__scroll-container">
-					{ contentActiveMenu === MENU_ROOT && (
-						<TemplatesNavigation />
-					) }
-					{ templatesActiveMenu === MENU_ROOT && (
-						<ContentNavigation
-							onActivateMenu={ setContentActiveMenu }
-						/>
-					) }
+					<Navigation
+						activeItem={ activeItem }
+						activeMenu={ activeMenu }
+						onActivateMenu={ ( menu ) => {
+							if ( menu === MENU_ROOT ) {
+								setNavigationPanelActiveMenu( menu );
+								setContentActiveMenu( menu );
+							} else if ( menu.startsWith( MENU_TEMPLATES ) ) {
+								setNavigationPanelActiveMenu( menu );
+							} else {
+								setContentActiveMenu( menu );
+							}
+						} }
+					>
+						{ activeMenu === MENU_ROOT && (
+							<MainDashboardButton.Slot>
+								<NavigationBackButton
+									backButtonLabel={ __( 'Dashboard' ) }
+									className="edit-site-navigation-panel__back-to-dashboard"
+									href="index.php"
+								/>
+							</MainDashboardButton.Slot>
+						) }
+
+						<NavigationMenu>
+							<NavigationGroup title={ __( 'Theme' ) }>
+								<NavigationItem
+									title={ __( 'Templates' ) }
+									navigateToMenu={ MENU_TEMPLATES }
+								/>
+								<NavigationItem
+									title={ __( 'Template Parts' ) }
+									navigateToMenu={ MENU_TEMPLATE_PARTS }
+								/>
+							</NavigationGroup>
+
+							<NavigationGroup title={ __( 'Content' ) }>
+								<NavigationItem
+									title={ __( 'Pages' ) }
+									navigateToMenu={ MENU_CONTENT_PAGES }
+								/>
+
+								<NavigationItem
+									title={ __( 'Categories' ) }
+									navigateToMenu={ MENU_CONTENT_CATEGORIES }
+								/>
+
+								<NavigationItem
+									title={ __( 'Posts' ) }
+									navigateToMenu={ MENU_CONTENT_POSTS }
+								/>
+							</NavigationGroup>
+						</NavigationMenu>
+
+						<TemplatesMenu />
+						<TemplatePartsMenu />
+
+						<ContentPagesMenu />
+						<ContentCategoriesMenu />
+						<ContentPostsMenu />
+					</Navigation>
 				</div>
 			</div>
 		</div>
