12c02e5fdf6f8ad88afb79c14dc2ecc1bb22ea9f
Kai Hao <kevin830726@gmail.com>
2021-05-18 01:02:19 +0800

    Handle multiple extensions

--- test/lint/import-resolver.js
+++ test/lint/import-resolver.js
@@ -2,30 +2,30 @@
  * External dependencies
  */
 const resolverNode = require( 'eslint-import-resolver-node' );
+const path = require( 'path' );
+
+const PACKAGES_DIR = path.resolve( __dirname, '../../packages' );
 
 exports.interfaceVersion = 2;
 
 exports.resolve = function ( source, file, config ) {
+	const resolve = ( sourcePath ) =>
+		resolverNode.resolve( sourcePath, file, {
+			...config,
+			extensions: [ '.tsx', '.ts', '.mjs', '.js', '.json', '.node' ],
+		} );
+
 	if ( source.startsWith( '@wordpress/' ) ) {
 		const packageName = source.slice( '@wordpress/'.length );
 
-		let entryFile;
-		try {
-			entryFile = require.resolve( `../../packages/${ packageName }` );
-		} catch {
-			entryFile = require.resolve(
-				`../../packages/${ packageName }/src/index.js`
-			);
+		const result = resolve( path.join( PACKAGES_DIR, packageName ) );
+
+		if ( result.found ) {
+			return result;
 		}
 
-		return {
-			found: true,
-			path: entryFile,
-		};
+		return resolve( path.join( PACKAGES_DIR, `${ packageName }/src/` ) );
 	}
 
-	return resolverNode.resolve( source, file, {
-		...config,
-		extensions: [ '.tsx', '.ts', '.mjs', '.js', '.json', '.node' ],
-	} );
+	return resolve( source );
 };
