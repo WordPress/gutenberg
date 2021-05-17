e07c5d734691d4f8993c2234aeb5f18472a92398
Jacopo Tomasone <Copons@users.noreply.github.com>
2021-05-17 12:59:11 +0100

    Post Editor: Persistent list view (#31047)
    * Move the Inserter sidebar in its own file
    
    * Add the new list view state
    
    * Add the list view sidebar
    
    * Bring changes to the e2e tests
    
    * Fix incorrect class name
    
    * Add missing dependencies array
    
    * Fix shortcuts
    
    * On select block, move caret to the end of its text
    
    * Fix e2e test
    
    * Fix incorrect action prop
    
    * update edit-post-layout references
    
    * fix e2e tests
    
    Co-authored-by: ntsekouras <ntsekouras@outlook.com>

--- packages/edit-post/src/store/actions.js
+++ packages/edit-post/src/store/actions.js
@@ -433,6 +433,19 @@ export function setIsInserterOpened( value ) {
 }
 
 /**
+ * Returns an action object used to open/close the list view.
+ *
+ * @param {boolean} isOpen A boolean representing whether the list view should be opened or closed.
+ * @return {Object} Action object.
+ */
+export function setIsListViewOpened( isOpen ) {
+	return {
+		type: 'SET_IS_LIST_VIEW_OPENED',
+		isOpen,
+	};
+}
+
+/**
  * Returns an action object used to switch to template editing.
  *
  * @param {boolean} value Is editing template.
