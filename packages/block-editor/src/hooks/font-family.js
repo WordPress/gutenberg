b5ff7fdbdc7acc0c98e563fbbff70b73bf4568f4
Bernie Reiter <ockham@raz.or.at>
2021-05-17 21:34:53 +0200

    Client hooks: use class names for font family

--- packages/block-editor/src/hooks/font-family.js
+++ packages/block-editor/src/hooks/font-family.js
@@ -6,34 +6,82 @@ import { find } from 'lodash';
 /**
  * WordPress dependencies
  */
+import { addFilter } from '@wordpress/hooks';
 import { hasBlockSupport } from '@wordpress/blocks';
+import TokenList from '@wordpress/token-list';
+import { createHigherOrderComponent } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-import { cleanEmptyObject } from './utils';
 import useSetting from '../components/use-setting';
 import FontFamilyControl from '../components/font-family';
 
 export const FONT_FAMILY_SUPPORT_KEY = 'typography.__experimentalFontFamily';
 
-const getFontFamilyFromAttributeValue = ( fontFamilies, value ) => {
-	const attributeParsed = /var:preset\|font-family\|(.+)/.exec( value );
-	if ( attributeParsed && attributeParsed[ 1 ] ) {
-		const fontFamilyObject = find( fontFamilies, ( { slug } ) => {
-			return slug === attributeParsed[ 1 ];
-		} );
-		if ( fontFamilyObject ) {
-			return fontFamilyObject.fontFamily;
-		}
+/**
+ * Filters registered block settings, extending attributes to include
+ * the `fontFamily` attribute.
+ *
+ * @param  {Object} settings Original block settings
+ * @return {Object}          Filtered block settings
+ */
+function addAttributes( settings ) {
+	if ( ! hasBlockSupport( settings, FONT_FAMILY_SUPPORT_KEY ) ) {
+		return settings;
 	}
-	return value;
-};
+
+	// Allow blocks to specify a default value if needed.
+	if ( ! settings.attributes.fontFamily ) {
+		Object.assign( settings.attributes, {
+			fontFamily: {
+				type: 'string',
+			},
+		} );
+	}
+
+	return settings;
+}
+
+/**
+ * Override props assigned to save component to inject font family.
+ *
+ * @param  {Object} props      Additional props applied to save element
+ * @param  {Object} blockType  Block type
+ * @param  {Object} attributes Block attributes
+ * @return {Object}            Filtered props applied to save element
+ */
+function addSaveProps( props, blockType, attributes ) {
+	if ( ! hasBlockSupport( blockType, FONT_FAMILY_SUPPORT_KEY ) ) {
+		return props;
+	}
+
+	if (
+		hasBlockSupport(
+			blockType,
+			'__experimentalSkipTypographySerialization'
+		)
+	) {
+		return props;
+	}
+
+	if ( ! attributes?.fontFamily ) {
+		return props;
+	}
+
+	// Use TokenList to dedupe classes.
+	const classes = new TokenList( props.className );
+	classes.add( `has-${ attributes?.fontFamily }-font-family` );
+	const newClassName = classes.value;
+	props.className = newClassName ? newClassName : undefined;
+
+	return props;
+}
 
 export function FontFamilyEdit( {
 	name,
 	setAttributes,
-	attributes: { style = {} },
+	attributes: { fontFamily },
 } ) {
 	const fontFamilies = useSetting( 'typography.fontFamilies' );
 	const isDisable = useIsFontFamilyDisabled( { name } );
@@ -42,26 +90,16 @@ export function FontFamilyEdit( {
 		return null;
 	}
 
-	const value = getFontFamilyFromAttributeValue(
-		fontFamilies,
-		style.typography?.fontFamily
-	);
+	const value = find( fontFamilies, ( { slug } ) => fontFamily === slug )
+		?.fontFamily;
 
 	function onChange( newValue ) {
 		const predefinedFontFamily = find(
 			fontFamilies,
-			( { fontFamily } ) => fontFamily === newValue
+			( { fontFamily: f } ) => f === newValue
 		);
 		setAttributes( {
-			style: cleanEmptyObject( {
-				...style,
-				typography: {
-					...( style.typography || {} ),
-					fontFamily: predefinedFontFamily
-						? `var:preset|font-family|${ predefinedFontFamily.slug }`
-						: newValue || undefined,
-				},
-			} ),
+			fontFamily: predefinedFontFamily?.slug,
 		} );
 	}
 
@@ -89,3 +127,75 @@ export function useIsFontFamilyDisabled( { name } ) {
 		! hasBlockSupport( name, FONT_FAMILY_SUPPORT_KEY )
 	);
 }
+
+/**
+ * Add inline styles for font families.
+ * Ideally, this is not needed and themes load the font-family classes on the
+ * editor.
+ *
+ * @param  {Function} BlockListBlock Original component
+ * @return {Function}                Wrapped component
+ */
+const withFontFamilyInlineStyles = createHigherOrderComponent(
+	( BlockListBlock ) => ( props ) => {
+		const fontFamilies = useSetting( 'typography.fontFamilies' );
+		const {
+			name: blockName,
+			attributes: { fontFamily, style },
+			wrapperProps,
+		} = props;
+
+		// Only add inline styles if the block supports font families,
+		// doesn't skip serialization of font families,
+		// doesn't already have an inline font family,
+		// and does have a class to extract the font family from.
+		if (
+			! hasBlockSupport( blockName, FONT_FAMILY_SUPPORT_KEY ) ||
+			hasBlockSupport(
+				blockName,
+				'__experimentalSkipTypographySerialization'
+			) ||
+			! fontFamily ||
+			style?.typography?.fontFamily
+		) {
+			return <BlockListBlock { ...props } />;
+		}
+
+		const fontFamilyValue = find(
+			fontFamilies,
+			( { slug } ) => slug === fontFamily
+		)?.fontFamily;
+
+		const newProps = {
+			...props,
+			wrapperProps: {
+				...wrapperProps,
+				style: {
+					fontFamily: fontFamilyValue,
+					...wrapperProps?.style,
+				},
+			},
+		};
+
+		return <BlockListBlock { ...newProps } />;
+	},
+	'withFontFamilyInlineStyles'
+);
+
+addFilter(
+	'blocks.registerBlockType',
+	'core/font/addAttribute',
+	addAttributes
+);
+
+addFilter(
+	'blocks.getSaveContent.extraProps',
+	'core/fontFamily/addSaveProps',
+	addSaveProps
+);
+
+addFilter(
+	'editor.BlockListBlock',
+	'core/font-family/with-font-family-inline-styles',
+	withFontFamilyInlineStyles
+);
