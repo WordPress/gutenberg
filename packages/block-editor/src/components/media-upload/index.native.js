e68550eea42a8fd80ab80f17a0513b964d8b3bfd
Joel Dean <jdeanjj1000@gmail.com>
2021-05-19 14:54:20 -0500

    Reduced onPress -> onSelectURL logic since the parameters match.

--- packages/block-editor/src/components/media-upload/index.native.js
+++ packages/block-editor/src/components/media-upload/index.native.js
@@ -196,7 +196,7 @@ export class MediaUpload extends Component {
 					},
 					{
 						text: __( 'Apply' ),
-						onPress: ( url ) => onSelectURL( url ),
+						onPress: onSelectURL,
 					},
 				], // buttons
 				'plain-text', // type
