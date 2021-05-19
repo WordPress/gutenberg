fdb4da7133dd65dda7d118076a4509a36ec2bb3b
Daniel Richards <daniel.richards@automattic.com>
2021-05-19 12:37:53 +0800

    Add widgets editor more menu (#31926)
    * Implement basic features framework
    
    * Implement top toolbar mode
    
    * Persist preferences
    
    * Implement keyboard shortcut help modal and help option
    
    * Implement keep caret inside block option
    
    * Fix shortcut for keyboard shortcut modal
    
    * Remove showIconLabels
    
    * Remove hidden block types
    
    * Add missing import for more menu styles
    
    * Replace edit-post references with edit-widgets
    
    * Hide undo redo and block navigation on mobile viewports
    
    * Switch to isMedium viewport

--- packages/edit-widgets/src/store/selectors.js
+++ packages/edit-widgets/src/store/selectors.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { keyBy } from 'lodash';
+import { get, keyBy } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -220,3 +220,19 @@ export const canInsertBlockInWidgetArea = createRegistrySelector(
 		);
 	}
 );
+
+/**
+ * Returns whether the given feature is enabled or not.
+ *
+ * This function is unstable, as it is mostly copied from the edit-post
+ * package. Editor features and preferences have a lot of scope for
+ * being generalized and refactored.
+ *
+ * @param {Object} state   Global application state.
+ * @param {string} feature Feature slug.
+ *
+ * @return {boolean} Is active.
+ */
+export function __unstableIsFeatureActive( state, feature ) {
+	return get( state.preferences.features, [ feature ], false );
+}
