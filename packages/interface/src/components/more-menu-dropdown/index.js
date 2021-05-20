c52bf90f4ea15cb0ded997653956a63568d3eea8
Daniel Richards <daniel.richards@automattic.com>
2021-05-20 16:28:28 +0800

    Add more menu component

---
+++ packages/interface/src/components/more-menu-dropdown/index.js
@@ -0,0 +1,41 @@
+/**
+ * WordPress dependencies
+ */
+import { DropdownMenu } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { moreVertical } from '@wordpress/icons';
+
+const POPOVER_PROPS = {
+	className: 'interface-more-menu__content',
+	position: 'bottom left',
+};
+
+const TOGGLE_PROPS = {
+	tooltipPosition: 'bottom',
+};
+
+export default function MoreMenuDropdown( {
+	label = __( 'Options' ),
+	popoverProps,
+	toggleProps,
+	children,
+} ) {
+	return (
+		<DropdownMenu
+			className="interface-more-menu-dropdown"
+			icon={ moreVertical }
+			/* translators: button label text should, if possible, be under 16 characters. */
+			label={ label }
+			popoverProps={ {
+				...POPOVER_PROPS,
+				...popoverProps,
+			} }
+			toggleProps={ {
+				...TOGGLE_PROPS,
+				...toggleProps,
+			} }
+		>
+			{ ( onClose ) => children( onClose ) }
+		</DropdownMenu>
+	);
+}
