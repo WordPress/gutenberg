7bcd7d10e25670f99c11b91cfc42d3ab8a6a6c69
Quintis1212 <mataleo1212@gmail.com>
2021-05-23 20:26:02 +0300

    Check width was implement

--- packages/block-editor/src/components/spacing-panel-control/index.js
+++ packages/block-editor/src/components/spacing-panel-control/index.js
@@ -14,6 +14,16 @@ export default function SpacingPanelControl( { children, ...props } ) {
 	const isSpacingEnabled = useEditorFeature( 'spacing.customPadding' );
 
 	if ( ! isSpacingEnabled ) return null;
+	const styles = children?.props?.attributes?.style?.spacing?.padding;
+	const topPX = parseInt( styles?.top );
+	const clientWidth = window.screen.width / 2;
+	if ( styles && topPX > clientWidth ) {
+		alert( 'Very big padding , script will refresh padding' );
+		styles.top = '20px';
+		styles.left = '20px';
+		styles.bottom = '20px';
+		styles.right = '20px';
+	}
 
 	return (
 		<InspectorControls { ...props }>
