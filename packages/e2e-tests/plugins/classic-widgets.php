a61fb357b839352e9089efd0e76dfbf06bf5c50a
Kai Hao <kevin830726@gmail.com>
2021-05-25 16:20:34 +0800

    Add a comment about the test plugin

--- packages/e2e-tests/plugins/classic-widgets.php
+++ packages/e2e-tests/plugins/classic-widgets.php
@@ -7,4 +7,5 @@
  * @package gutenberg-test-classic-widgets
  */
 
+// Disable the widgets block editor and enable the legacy widgets screen.
 add_filter( 'use_widgets_block_editor', '__return_false' );
