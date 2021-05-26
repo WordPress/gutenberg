8f96e6e63401bc9f3309180cfc565b96f59458f9
David Szabo <david.szabo97@gmail.com>
2021-05-26 14:44:40 +0200

    Fix formatting

--- packages/block-library/src/calendar/index.php
+++ packages/block-library/src/calendar/index.php
@@ -71,7 +71,7 @@ add_action( 'init', 'register_block_core_calendar' );
 
 /**
  * Returns whether or not there are currently published posts.
- * 
+ *
  * Used to hide the calendar block when there are no published posts.
  * This compensates for a known Core bug: https://core.trac.wordpress.org/ticket/12016
  *
