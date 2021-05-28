ca50dc591cf7e5bf74c729c807edfc3c57c3a920
Marco Ciampini <marco.ciampo@gmail.com>
2021-05-28 12:05:03 +0200

    Move 'CardHeader' subcomponent to `card-header` subfolder, refactor hook out

---
+++ packages/components/src/card/card-header/hook.js
@@ -0,0 +1,50 @@
+/**
+ * External dependencies
+ */
+import { cx } from 'emotion';
+
+/**
+ * WordPress dependencies
+ */
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { useContextSystem } from '../../ui/context';
+import * as styles from '../styles';
+
+/**
+ * @param {import('../../ui/context').PolymorphicComponentProps<import('../types').HeaderProps, 'div'>} props
+ */
+export function useCardHeader( props ) {
+	const {
+		className,
+		isBorderless = false,
+		isShady = false,
+		size = 'medium',
+		...otherProps
+	} = useContextSystem( props, 'CardHeader' );
+
+	const classes = useMemo(
+		() =>
+			cx(
+				styles.Header,
+				styles.borderRadius,
+				styles.headerFooter,
+				styles.cardPaddings[ size ],
+				isBorderless && styles.borderless,
+				isShady && styles.shady,
+				className
+			),
+		[ className, isBorderless, isShady, size ]
+	);
+
+	return {
+		...otherProps,
+		className: classes,
+		isBorderless,
+		isShady,
+		size,
+	};
+}
