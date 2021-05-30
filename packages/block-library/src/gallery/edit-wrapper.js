1a4345ac33c77edf6a331135de33a6b2ac591860
Matthew Kevins <mkevins@users.noreply.github.com>
2021-05-31 06:51:13 +1000

    Gallery refactor - Infer version from existing content (#32270)
    * Give content precedence over flag in edit wrapper
    
    This modifies the logic in the gallery edit wrapper function to infer
    version information from existing content when the editor encounters
    content created from the new implementation and the flag is not set.

--- packages/block-library/src/gallery/edit-wrapper.js
+++ packages/block-library/src/gallery/edit-wrapper.js
@@ -23,10 +23,15 @@ export default function GalleryEditWrapper( props ) {
 		return settings.__experimentalGalleryRefactor;
 	}, [] );
 
+	// This logic is used to infer version information from content with higher
+	// precedence than the flag. New galleries (and existing empty galleries) will
+	// honor the flag.
+	const hasNewVersionContent = !! attributes?.imageCount;
+	const hasOldVersionContent =
+		0 < attributes?.ids?.length || 0 < attributes?.images?.length;
 	if (
-		! __experimentalGalleryRefactor ||
-		attributes?.ids?.length > 0 ||
-		attributes?.images?.length > 0
+		hasOldVersionContent ||
+		( ! hasNewVersionContent && ! __experimentalGalleryRefactor )
 	) {
 		return <GalleryEditV1 { ...props } />;
 	}
