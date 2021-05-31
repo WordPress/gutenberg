dae56ffc1fb38c992ad422610a08a2e3fcb30711
apeatling <apeatling@gmail.com>
2021-05-31 12:17:09 -0700

    Make sure that the frontend toggle works when using the SVG icon for the button.

--- packages/block-library/src/search/frontend.js
+++ packages/block-library/src/search/frontend.js
@@ -20,45 +20,57 @@ const wpBlockSearch = ( block ) => {
 	}
 	attributeContainer.remove();
 
-	const wrapper = block.querySelector( '.wp-block-search__inside-wrapper' );
+	const hiddenClass = 'wp-block-search__searchfield-hidden';
+	const wrapperClass = '.wp-block-search__inside-wrapper';
+	const buttonClass = '.wp-block-search__button';
+
+	const wrapper = block.querySelector( wrapperClass );
 	const searchField = block.querySelector( '.wp-block-search__input' );
-	const button = block.querySelector( '.wp-block-search__button' );
+	const button = block.querySelector( buttonClass );
 
 	const toggleSearchField = ( e ) => {
-		e.preventDefault();
-
-		if (
-			block.classList.contains( 'wp-block-search__searchfield-hidden' )
-		) {
-			showSearchField(
-				wrapper,
-				searchField,
-				attributes.width,
-				attributes.widthUnit
-			);
-			block.classList.remove( 'wp-block-search__searchfield-hidden' );
-			searchField.focus();
-
-			button.removeEventListener( 'click', toggleSearchField );
-			document.body.addEventListener( 'click', doSearch );
-		} else {
-			hideSearchField( wrapper, searchField, button );
-			block.classList.add( 'wp-block-search__searchfield-hidden' );
-		}
-	};
-
-	const doSearch = ( e ) => {
-		if ( e.target === button || e.target === searchField ) {
+		if ( e.target !== button && ! e.target.closest( buttonClass ) ) {
 			return false;
 		}
 
-		toggleSearchField( e );
+		e.preventDefault();
 
-		document.body.removeEventListener( 'click', doSearch );
-		button.addEventListener( 'click', toggleSearchField );
+		return block.classList.contains( hiddenClass )
+			? doShowSearchField()
+			: doHideSearchField();
 	};
 
-	button.addEventListener( 'click', toggleSearchField );
+	const doShowSearchField = () => {
+		showSearchField(
+			wrapper,
+			searchField,
+			attributes.width,
+			attributes.widthUnit
+		);
+		block.classList.remove( hiddenClass );
+		searchField.focus();
+
+		wrapper.removeEventListener( 'click', toggleSearchField );
+		document.body.addEventListener( 'click', doSearch );
+	};
+
+	const doHideSearchField = () => {
+		hideSearchField( wrapper, searchField, button );
+		block.classList.add( hiddenClass );
+	};
+
+	const doSearch = ( e ) => {
+		if ( e.target.closest( wrapperClass ) ) {
+			return false;
+		}
+
+		doHideSearchField();
+
+		document.body.removeEventListener( 'click', doSearch );
+		wrapper.addEventListener( 'click', toggleSearchField );
+	};
+
+	wrapper.addEventListener( 'click', toggleSearchField );
 };
 
 // eslint-disable-next-line @wordpress/no-global-event-listener
