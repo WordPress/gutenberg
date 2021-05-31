a99904b615a81de42c4008d42cfcf7f29df0b362
George Mamadashvili <georgemamadashvili@gmail.com>
2021-05-31 18:09:20 +0400

    File Block: Add an example preview

--- packages/block-library/src/file/index.js
+++ packages/block-library/src/file/index.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { file as icon } from '@wordpress/icons';
 
 /**
@@ -17,6 +18,14 @@ export { metadata, name };
 
 export const settings = {
 	icon,
+	example: {
+		attributes: {
+			href:
+				'https://upload.wikimedia.org/wikipedia/commons/d/dd/Armstrong_Small_Step.ogg',
+			// translators: Name of the file, which serves as an example for the File block.
+			fileName: __( 'Armstrong_Small_Step' ),
+		},
+	},
 	transforms,
 	edit,
 	save,
