e55aed09a6de49a0202482efbe6ab1b7b30f6768
Ari Stathopoulos <aristath@gmail.com>
2021-05-31 11:30:07 +0300

    update docs

--- packages/editor/src/utils/url.js
+++ packages/editor/src/utils/url.js
@@ -28,11 +28,11 @@ export function getWPAdminURL( page, query ) {
  * This replicates some of what sanitize_title() does in WordPress core, but
  * is only designed to approximate what the slug will be.
  *
- * Converts Latin-1 Supplement and Latin Extended-A letters to basic Latin
- * letters. Removes combining diacritical marks. Converts whitespace, periods,
+ * Converts Latin-1 Supplement and Latin Extended-A letters to basic Latin letters.
+ * Removes combining diacritical marks. Converts whitespace, periods,
  * and forward slashes to hyphens. Removes any remaining non-word characters
- * except hyphens. Converts remaining string to lowercase. It does not account
- * for octets, HTML entities, or other encoded characters.
+ * except hyphens and underscores. Converts remaining string to lowercase.
+ * It does not account for octets, HTML entities, or other encoded characters.
  *
  * @param {string} string Title or slug to be processed
  *
