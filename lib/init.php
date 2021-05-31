6d1ada7be0a52b878314f89a4b4123efbd751742
Robert Anderson <robert@noisysocks.com>
2021-05-31 10:57:56 +1000

    Add back missing add_theme_support()

--- lib/init.php
+++ lib/init.php
@@ -187,4 +187,5 @@ function register_site_icon_url( $response ) {
 
 add_filter( 'rest_index', 'register_site_icon_url' );
 
+add_theme_support( 'widgets-block-editor' );
 add_theme_support( 'block-templates' );
