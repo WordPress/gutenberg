7fb7e8a6f1b2bf41904c0e5149ef7cba30e995eb
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-06-01 18:36:08 +1000

    Add story for block support panel

--- packages/components/src/block-support-panel/stories/index.js
+++ packages/components/src/block-support-panel/stories/index.js
@@ -1,1 +1,69 @@
-// TODO: Add Storybook entries for block support panel.
+/**
+ * External dependencies
+ */
+import styled from '@emotion/styled';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import BlockSupportPanel from '../';
+import Panel from '../../panel';
+import UnitControl from '../../unit-control';
+
+export default {
+	title: 'Components/BlockSupportPanel',
+	component: BlockSupportPanel,
+};
+
+export const _default = () => {
+	const [ height, setHeight ] = useState();
+	const [ width, setWidth ] = useState();
+
+	const resetAll = () => {
+		setHeight( undefined );
+		setWidth( undefined );
+	};
+
+	return (
+		<PanelWrapperView>
+			<Panel>
+				<BlockSupportPanel
+					label="Display options"
+					title="Block Support Panel"
+					resetAll={ resetAll }
+				>
+					<PlaceholderControl
+						hasValue={ () => !! height }
+						label="Height"
+						reset={ () => setHeight( undefined ) }
+						value={ height }
+						onChange={ ( next ) => setHeight( next ) }
+					/>
+					<PlaceholderControl
+						hasValue={ () => !! width }
+						label="Width"
+						reset={ () => setWidth( undefined ) }
+						value={ width }
+						onChange={ ( next ) => setWidth( next ) }
+					/>
+				</BlockSupportPanel>
+			</Panel>
+		</PanelWrapperView>
+	);
+};
+
+function PlaceholderControl( { label, value, onChange } ) {
+	return (
+		<UnitControl label={ label } value={ value } onChange={ onChange } />
+	);
+}
+
+const PanelWrapperView = styled.div`
+	max-width: 232px;
+	font-size: 13px;
+`;
