89238ba7c8550d90d977a51961861a9cc1ded7bb
Marco Ciampini <marco.ciampo@gmail.com>
2021-06-01 14:08:57 +0200

    Refactor `CardMedia` to use `createComponent`

--- packages/components/src/card/card-media/component.js
+++ packages/components/src/card/card-media/component.js
@@ -1,35 +1,13 @@
 /**
  * Internal dependencies
  */
-import { contextConnect } from '../../ui/context';
-import { View } from '../../view';
+import { createComponent } from '../../ui/utils';
 import { useCardMedia } from './hook';
 
-/**
- * @param {import('../../ui/context').PolymorphicComponentProps<import('../types').MediaProps, 'div'>} props
- * @param {import('react').Ref<any>} forwardedRef
- */
-function CardMedia( props, forwardedRef ) {
-	const mediaProps = useCardMedia( props );
+const CardMedia = createComponent( {
+	as: 'div',
+	useHook: useCardMedia,
+	name: 'CardMedia',
+} );
 
-	return <View { ...mediaProps } ref={ forwardedRef } />;
-}
-
-/**
- * `<CardMedia />` can be placed in any order as a direct child of a `<Card />`.
- * The styles will automatically round the corners of the inner media element.
- *
- * @example
- * ```jsx
- * import { Card, CardMedia } from `@wordpress/components`;
- *
- * <Card>
- *  <CardMedia>
- *    <img src="..." />
- *  </CardMedia
- * </Card>
- * ```
- */
-const ConnectedCardMedia = contextConnect( CardMedia, 'CardMedia' );
-
-export default ConnectedCardMedia;
+export default CardMedia;
