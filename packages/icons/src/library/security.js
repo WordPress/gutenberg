76512954397b8238aa3c777a00c8f2b11dca1463
Filipe Varela <keoshi@keoshi.com>
2021-06-01 13:36:27 +0100

    Add new icons
    Adds the following icons:
    
    - `add-card`
    - `bug`/`no-bug`
    - `key`
    - `post-author`
    - `security`

---
+++ packages/icons/src/library/security.js
@@ -0,0 +1,16 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG, Path } from '@wordpress/primitives';
+
+const security = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path
+			d="M12.517 5.059a1.25 1.25 0 00-1.034 0L6.75 7.21v3.608c0 3.313 2.145 6.388 5.005 7.33.159.053.331.053.49 0 2.86-.942 5.005-4.017 5.005-7.33V7.21L12.517 5.06zm-1.655-1.366a2.75 2.75 0 012.276 0l5.612 2.551v4.574c0 3.9-2.504 7.59-6.035 8.755a2.283 2.283 0 01-1.43 0c-3.53-1.164-6.035-4.856-6.035-8.755V6.244l5.612-2.55z"
+			fillRule="evenodd"
+			clipRule="evenodd"
+		/>
+	</SVG>
+);
+
+export default security;
