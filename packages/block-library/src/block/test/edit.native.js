962f87f9e29f69b553c098f429d5f56605630e69
Carlos Garcia <fluiddot@gmail.com>
2021-06-02 19:14:06 +0200

    Move the reusable block fixture to unit test

--- packages/block-library/src/block/test/edit.native.js
+++ packages/block-library/src/block/test/edit.native.js
@@ -18,8 +18,32 @@ import fetchRequest from '@wordpress/api-fetch';
 /**
  * Internal dependencies
  */
-import { getMockedReusableBlock } from './fixture';
 import { registerCoreBlocks } from '../..';
+
+const getMockedReusableBlock = ( id ) => ( {
+	content: {
+		raw: `
+    <!-- wp:heading -->
+    <h2>First Reusable block</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:paragraph -->
+    <p><strong>Bold</strong> <em>Italic</em> <s>Striked</s> Superscript<sup>(1)</sup> Subscript<sub>(2)</sub> <a href="http://www.wordpress.org" target="_blank" rel="noreferrer noopener">Link</a></p>
+    <!-- /wp:paragraph -->
+    
+    !-- wp:heading {"level":4} -->
+    <h4>List</h4>
+    <!-- /wp:heading -->
+    
+    <!-- wp:list -->
+    <ul><li>First Item</li><li>Second Item</li><li>Third Item</li></ul>
+    <!-- /wp:list -->
+`,
+	},
+	id,
+	title: { raw: `Reusable block - ${ id }` },
+	type: 'wp_block',
+} );
 
 beforeAll( () => {
 	// Register all core blocks
