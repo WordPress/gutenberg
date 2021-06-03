32dd651e5625bcf8809499318714d126673e3a6e
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-06-03 15:14:26 -0700

    Disable the Block Actions when there are non instead of not showing them

--- packages/block-editor/src/components/block-mobile-toolbar/block-actions-menu.native.js
+++ packages/block-editor/src/components/block-mobile-toolbar/block-actions-menu.native.js
@@ -219,7 +219,11 @@ const BlockActionsMenu = ( {
 
 	// End early if there are no options to show.
 	if ( ! options.length ) {
-		return null;
+		return <ToolbarButton
+			title={ __( 'Open Block Actions Menu' ) }
+			icon={ moreHorizontalMobile }
+			disabled={ true }
+		/>
 	}
 
 	function onPasteBlock() {
