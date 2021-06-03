1ee595f30e9ea2dc521b97ee83dca10a8a5520a3
André <andres.maneiro@automattic.com>
2021-06-03 09:58:57 +0200

    Fix lint issues

--- lib/class-wp-theme-json-resolver-gutenberg.php
+++ lib/class-wp-theme-json-resolver-gutenberg.php
@@ -367,7 +367,7 @@ class WP_Theme_JSON_Resolver_Gutenberg {
 
 			$json_decoding_error = json_last_error();
 			if ( JSON_ERROR_NONE !== $json_decoding_error ) {
-				trigger_error( "Error when decoding a theme.json schema for user data. " . json_last_error_msg() );
+				trigger_error( 'Error when decoding a theme.json schema for user data. ' . json_last_error_msg() );
 				return new WP_Theme_JSON_Gutenberg( $config );
 			}
 
