085d6324e801c07119802eab9b162b4b63fb1d32
jasmussen <joen@automattic.com>
2021-06-04 09:41:04 +0200

    More linting.

--- packages/block-library/src/post-author/index.js
+++ packages/block-library/src/post-author/index.js
@@ -3,6 +3,10 @@
  */
 import metadata from './block.json';
 import edit from './edit';
+
+/**
+ * WordPress dependencies
+ */
 import { postAuthor as icon } from '@wordpress/icons';
 
 const { name } = metadata;
