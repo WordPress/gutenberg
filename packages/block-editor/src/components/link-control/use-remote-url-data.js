dad3a702f450247829ff18b596120acd2d3d058c
Dave Smith <getdavemail@gmail.com>
2021-06-07 11:06:35 +0100

    Simplify hook implementation

--- packages/block-editor/src/components/link-control/use-remote-url-data.js
+++ packages/block-editor/src/components/link-control/use-remote-url-data.js
@@ -20,61 +20,36 @@ function useRemoteUrlData( url ) {
 		};
 	}, [] );
 
-	function abortableFetch( theUrl, signal ) {
-		return new Promise( ( resolve, reject ) => {
-			// Listen once on the AbortController signal
-			// and reject the promise if abort is triggered.
-			signal.addEventListener(
-				'abort',
-				() => {
-					reject( 'aborted' );
-				},
-				{ once: true }
-			);
-
-			return fetchRemoteUrlData( theUrl, {
-				signal,
-			} )
-				.then( ( data ) => resolve( data ) )
-				.catch( () => reject() );
-		} );
-	}
-
 	useEffect( () => {
-		// eslint-disable-next-line no-undef
-		let controller;
-
-		const fetchRichData = () => {
-			setIsFetching( true );
-
-			// eslint-disable-next-line no-undef
-			controller = new AbortController();
-
-			abortableFetch( url, controller.signal )
-				.then( ( urlData ) => {
-					setRichData( urlData );
-					setIsFetching( false );
-				} )
-				.catch( ( error ) => {
-					// Avoid setting state on unmounted component
-					if ( 'aborted' !== error ) {
-						setIsFetching( false );
-						setRichData( null );
-					}
-				} );
-		};
-
 		// Only make the request if we have an actual URL
 		// and the fetching util is available. In some editors
 		// there may not be such a util.
 		if ( url?.length && fetchRemoteUrlData ) {
-			fetchRichData();
-		}
+			setIsFetching( true );
 
-		// When the URL changes the abort the current request
-		return () => {
-			controller?.abort();
-		};
+			// eslint-disable-next-line no-undef
+			const controller = new AbortController();
+			const signal = controller.signal;
+
+			fetchRemoteUrlData( url, {
+				signal,
+			} )
+				.then( ( urlData ) => {
+					setRichData( urlData );
+					setIsFetching( false );
+				} )
+				.catch( () => {
+					// Avoid setting state on unmounted component
+					if ( ! signal?.aborted ) {
+						setIsFetching( false );
+						setRichData( null );
+					}
+				} );
+			// Cleanup: when the URL changes the abort the current request
+			return () => {
+				controller.abort();
+			};
+		}
 	}, [ url ] );
 
 	return {
