dcaafa77a1bca0e641c42ee8c334d901394129d2
Marco Ciampini <marco.ciampo@gmail.com>
2021-06-07 19:10:57 +0200

    Scrollable: add smoothScroll and scrollDirection to storybook story

--- packages/components/src/scrollable/stories/index.js
+++ packages/components/src/scrollable/stories/index.js
@@ -1,4 +1,11 @@
 /**
+ * External dependencies
+ */
+import { boolean, select } from '@storybook/addon-knobs';
+/* eslint-disable-next-line no-restricted-imports */
+import React from 'react';
+
+/**
  * Internal dependencies
  */
 import { View } from '../../view';
@@ -10,9 +17,66 @@ export default {
 };
 
 export const _default = () => {
+	const targetRef = React.useRef( null );
+
+	const onButtonClick = () => {
+		targetRef.current?.focus();
+	};
+
+	const otherProps = {
+		smoothScroll: boolean(
+			'Scrollable: smoothScroll (hint: move focus in the scrollable container)',
+			false
+		),
+		scrollDirection: select(
+			'Scrollable: scrollDirection',
+			{
+				x: 'x',
+				y: 'y',
+				auto: 'auto',
+			},
+			'y'
+		),
+	};
+
+	const containerWidth = 300;
+	const containerHeight = 400;
+
 	return (
-		<Scrollable style={ { height: 400, width: 300 } }>
-			<View style={ { backgroundColor: '#eee', height: 1000 } } />
+		<Scrollable
+			style={ { height: containerHeight, width: containerWidth } }
+			{ ...otherProps }
+		>
+			<View
+				style={ {
+					backgroundColor: '#eee',
+					height:
+						otherProps.scrollDirection === 'x'
+							? containerHeight
+							: 1000,
+					width:
+						otherProps.scrollDirection === 'y'
+							? containerWidth
+							: 1000,
+					position: 'relative',
+				} }
+			>
+				<button onClick={ onButtonClick }>
+					Move focus to an element out of view
+				</button>
+				<input
+					ref={ targetRef }
+					style={ {
+						position: 'absolute',
+						bottom:
+							otherProps.scrollDirection === 'x' ? 'initial' : 0,
+						right:
+							otherProps.scrollDirection === 'y' ? 'initial' : 0,
+					} }
+					type="text"
+					value="Focus me"
+				/>
+			</View>
 		</Scrollable>
 	);
 };
