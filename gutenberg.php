6a1b8079187f582814257c5cff2136bb12f07b2f
Bernie Reiter <ockham@raz.or.at>
2021-06-07 18:52:49 +0200

    Bump plugin version to 10.7.4

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
  * Requires at least: 5.6
  * Requires PHP: 5.6
- * Version: 10.7.3
+ * Version: 10.7.4
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
