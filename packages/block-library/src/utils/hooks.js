07458b65c1db906388af9ccb814ccb8333830609
ntsekouras <ntsekouras@outlook.com>
2021-06-08 13:10:57 +0300

    add jsdoc

--- packages/block-library/src/utils/hooks.js
+++ packages/block-library/src/utils/hooks.js
@@ -10,6 +10,17 @@ import { store as blockEditorStore } from '@wordpress/block-editor';
 import queryMetaData from '../query/block.json';
 const { name: queryBlockName } = queryMetaData;
 
+/**
+ * Hook that determines if a Post block is editable or not.
+ * The returned value is used to determine if the specific
+ * Post block will be rendered in `readonly` mode or not.
+ *
+ * For now this is checking if a Post block is nested in
+ * a Query block. If it is, the block should not be editable.
+ *
+ * @param {string} clientId The ID of the block to be checked.
+ * @return {boolean} Whether the block can be edited or not.
+ */
 export function useIsEditablePostBlock( clientId ) {
 	return useSelect(
 		( select ) => {
