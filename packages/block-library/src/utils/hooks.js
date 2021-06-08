33c8d8e13d164776d251f33da3b2f086f4c8b7c6
ntsekouras <ntsekouras@outlook.com>
2021-06-08 10:43:56 +0300

    hook creation and Post Title usage

---
+++ packages/block-library/src/utils/hooks.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import queryMetaData from '../query/block.json';
+const { name: queryBlockName } = queryMetaData;
+
+export function useIsEditable( clientId ) {
+	return useSelect(
+		( select ) => {
+			const { getBlockParents, getBlockName } = select(
+				blockEditorStore
+			);
+			const blockParents = getBlockParents( clientId );
+			const hasQueryParent = blockParents.some(
+				( parentClientId ) =>
+					getBlockName( parentClientId ) === queryBlockName
+			);
+			return ! hasQueryParent;
+		},
+		[ clientId ]
+	);
+}
+
+export default { useIsEditable };
