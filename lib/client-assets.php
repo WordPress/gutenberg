91b8c56e4479f783f08e4448bdc6942f8d2e829d
Ari Stathopoulos <aristath@gmail.com>
2021-06-11 14:37:10 +0300

    Plugin: Copy `wp_should_load_separate_core_block_assets` function from core (#32611)
    * copy function from core
    
    * add @todo note.

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -331,7 +331,7 @@ function gutenberg_register_packages_styles( $styles ) {
 	);
 	$styles->add_data( 'wp-components', 'rtl', 'replace' );
 
-	$block_library_filename = gutenberg_should_load_separate_block_assets() ? 'common' : 'style';
+	$block_library_filename = wp_should_load_separate_core_block_assets() ? 'common' : 'style';
 	gutenberg_override_style(
 		$styles,
 		'wp-block-library',
