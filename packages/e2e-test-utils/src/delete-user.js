35b34379dfb3688ac0947ca118412ea847d58bf6
Bernie Reiter <ockham@raz.or.at>
2021-06-14 22:16:27 +0200

    E2E Test Utils: Add createUser and deleteUser functions

---
+++ packages/e2e-test-utils/src/delete-user.js
@@ -0,0 +1,39 @@
+/**
+ * Internal dependencies
+ */
+import { switchUserToAdmin } from './switch-user-to-admin';
+import { switchUserToTest } from './switch-user-to-test';
+import { visitAdminPage } from './visit-admin-page';
+
+/**
+ * Delete a user account.
+ *
+ * @param {string} username User name.
+ */
+export async function deleteUser( username ) {
+	await switchUserToAdmin();
+	await visitAdminPage( 'users.php' );
+
+	const [ userLink ] = await page.$x(
+		`//td[@data-colname="Username"]//a[contains(text(), "${ username }")]`
+	);
+
+	// Focus to unveil actions
+	await userLink.focus();
+
+	// Tab twice to focus 'Delete'
+	await page.keyboard.press( 'Tab' );
+	await page.keyboard.press( 'Tab' );
+
+	await Promise.all( [
+		page.keyboard.press( 'Enter' ),
+		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	] );
+
+	// Confirm
+	await Promise.all( [
+		page.click( 'input#submit' ),
+		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	] );
+	await switchUserToTest();
+}
