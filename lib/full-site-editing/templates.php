ba70233a69ad73ed11ed5468779f1b2d1c411acb
Ari Stathopoulos <aristath@gmail.com>
2021-06-14 11:01:32 +0300

    Only add the content skip link for block templates (#32451)

--- lib/full-site-editing/templates.php
+++ lib/full-site-editing/templates.php
@@ -199,9 +199,19 @@ add_action( 'save_post_wp_template', 'set_unique_slug_on_create_template' );
  * @return void
  */
 function gutenberg_the_skip_link() {
+	// Early exit if on WP 5.8+.
+	if ( function_exists( 'the_block_template_skip_link' ) ) {
+		return;
+	}
 
-	// Early exit if not an FSE theme.
+	// Early exit if not a block theme.
 	if ( ! gutenberg_supports_block_templates() ) {
+		return;
+	}
+
+	// Early exit if not a block template.
+	global $_wp_current_template_content;
+	if ( ! $_wp_current_template_content ) {
 		return;
 	}
 	?>
@@ -261,7 +271,12 @@ function gutenberg_the_skip_link() {
 
 		// Get the site wrapper.
 		// The skip-link will be injected in the beginning of it.
-		parentEl = document.querySelector( '.wp-site-blocks' ) || document.body,
+		parentEl = document.querySelector( '.wp-site-blocks' );
+
+		// Early exit if the root element was not found.
+		if ( ! parentEl ) {
+			return;
+		}
 
 		// Get the skip-link target's ID, and generate one if it doesn't exist.
 		skipLinkTargetID = skipLinkTarget.id;
