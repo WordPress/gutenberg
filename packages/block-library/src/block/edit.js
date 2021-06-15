a4f9b21ee5090556d19c9e321fd0538e2453f008
thisissandip <thisissandipmondal@gmail.com>
2021-06-15 20:48:46 +0530

    move hasEdits to top useSelect

--- packages/block-library/src/block/edit.js
+++ packages/block-library/src/block/edit.js
@@ -2,8 +2,19 @@
  * WordPress dependencies
  */
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useEntityBlockEditor, useEntityProp, store as coreStore } from '@wordpress/core-data';
-import { Placeholder, Spinner, ToolbarGroup, ToolbarButton, TextControl, PanelBody } from '@wordpress/components';
+import {
+	useEntityBlockEditor,
+	useEntityProp,
+	store as coreStore,
+} from '@wordpress/core-data';
+import {
+	Placeholder,
+	Spinner,
+	ToolbarGroup,
+	ToolbarButton,
+	TextControl,
+	PanelBody,
+} from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import {
@@ -25,30 +36,53 @@ import { store as noticesStore } from '@wordpress/notices';
  */
 import ReusableBlockWelcomeGuide from './reusable-block-welcome-guide';
 
-export default function ReusableBlockEdit({ attributes: { ref }, clientId }) {
-	const [hasAlreadyRendered, RecursionProvider] = useNoRecursiveRenders(ref);
+export default function ReusableBlockEdit( { attributes: { ref }, clientId } ) {
+	const [ hasAlreadyRendered, RecursionProvider ] = useNoRecursiveRenders(
+		ref
+	);
 	const { isMissing, hasResolved, hasEdits } = useSelect(
-		(select) => {
-			const persistedBlock = select(coreStore).getEntityRecord('postType', 'wp_block', ref);
-			const hasResolvedBlock = select(coreStore).hasFinishedResolution('getEntityRecord', [
+		( select ) => {
+			const persistedBlock = select( coreStore ).getEntityRecord(
+				'postType',
+				'wp_block',
+				ref
+			);
+			const hasResolvedBlock = select(
+				coreStore
+			).hasFinishedResolution( 'getEntityRecord', [
 				'postType',
 				'wp_block',
 				ref,
-			]);
-			const blockHasEdits = select(coreStore).hasEditsForEntityRecord('postType', 'wp_block', ref);
+			] );
+			const blockHasEdits = select( coreStore ).hasEditsForEntityRecord(
+				'postType',
+				'wp_block',
+				ref
+			);
 			return {
 				hasResolved: hasResolvedBlock,
-				isMissing: hasResolvedBlock && !persistedBlock,
+				isMissing: hasResolvedBlock && ! persistedBlock,
 				hasEdits: blockHasEdits,
 			};
 		},
-		[ref, clientId]
+		[ ref, clientId ]
 	);
 
-	const { __experimentalConvertBlockToStatic: convertBlockToStatic } = useDispatch(reusableBlocksStore);
+	const {
+		__experimentalConvertBlockToStatic: convertBlockToStatic,
+	} = useDispatch( reusableBlocksStore );
 
-	const [blocks, onInput, onChange] = useEntityBlockEditor('postType', 'wp_block', { id: ref });
-	const [title, setTitle] = useEntityProp('postType', 'wp_block', 'title', ref);
+	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
+		'postType',
+		'wp_block',
+		{ id: ref }
+	);
+	const [ title, setTitle ] = useEntityProp(
+		'postType',
+		'wp_block',
+		'title',
+		ref
+	);
 
 	const blockProps = useBlockProps();
 
@@ -58,54 +92,60 @@ export default function ReusableBlockEdit({ attributes: { ref }, clientId }) {
 			value: blocks,
 			onInput,
 			onChange,
-			renderAppender: blocks?.length ? undefined : InnerBlocks.ButtonBlockAppender,
+			renderAppender: blocks?.length
+				? undefined
+				: InnerBlocks.ButtonBlockAppender,
 		}
 	);
-
+	n;
 	// local states for gudie modal
-	const [isGudieOpen, setIsGudieOpen] = useState(false);
+	const [ isGudieOpen, setIsGudieOpen ] = useState( false );
 
-	const { saveEditedEntityRecord } = useDispatch(coreStore);
-	const { createNotice } = useDispatch(noticesStore);
+	const { saveEditedEntityRecord } = useDispatch( coreStore );
+	const { createNotice } = useDispatch( noticesStore );
 
 	// save the unsaved records
 	const saveEditedRecords = () => {
-		saveEditedEntityRecord('postType', 'wp_block', ref);
+		saveEditedEntityRecord( 'postType', 'wp_block', ref );
 		showSnackbar();
 	};
 
 	const showSnackbar = () => {
-		createNotice('success', __('Reusable block saved.'), {
+		createNotice( 'success', __( 'Reusable block saved.' ), {
 			type: 'snackbar',
 			isDismissible: true,
 			actions: [
 				{
-					label: __('Learn more'),
-					onClick: () => setIsGudieOpen(true),
+					label: __( 'Learn more' ),
+					onClick: () => setIsGudieOpen( true ),
 				},
 			],
-		});
+		} );
 	};
 
-	if (hasAlreadyRendered) {
+	if ( hasAlreadyRendered ) {
 		return (
-			<div {...blockProps}>
-				<Warning>{__('Block cannot be rendered inside itself.')}</Warning>
+			<div { ...blockProps }>
+				<Warning>
+					{ __( 'Block cannot be rendered inside itself.' ) }
+				</Warning>
 			</div>
 		);
 	}
 
-	if (isMissing) {
+	if ( isMissing ) {
 		return (
-			<div {...blockProps}>
-				<Warning>{__('Block has been deleted or is unavailable.')}</Warning>
+			<div { ...blockProps }>
+				<Warning>
+					{ __( 'Block has been deleted or is unavailable.' ) }
+				</Warning>
 			</div>
 		);
 	}
 
-	if (!hasResolved) {
+	if ( ! hasResolved ) {
 		return (
-			<div {...blockProps}>
+			<div { ...blockProps }>
 				<Placeholder>
 					<Spinner />
 				</Placeholder>
@@ -115,42 +155,52 @@ export default function ReusableBlockEdit({ attributes: { ref }, clientId }) {
 
 	return (
 		<RecursionProvider>
-			<div {...blockProps}>
+			<div { ...blockProps }>
 				<BlockControls>
 					<ToolbarGroup>
 						<ToolbarButton
-							onClick={() => convertBlockToStatic(clientId)}
-							label={__('Convert to regular blocks')}
-							icon={ungroup}
+							onClick={ () => convertBlockToStatic( clientId ) }
+							label={ __( 'Convert to regular blocks' ) }
+							icon={ ungroup }
 							showTooltip
 						/>
 					</ToolbarGroup>
 					<ToolbarGroup>
 						<ToolbarButton
 							isPrimary
-							style={{
+							style={ {
 								margin: '7px',
 								height: '34px',
-							}}
-							onClick={saveEditedRecords}
-							label={__('Save Globally')}
+							} }
+							onClick={ saveEditedRecords }
+							label={ __( 'Save Globally' ) }
 							showTooltip
-							isDisabled={!hasEdits}>
-							{__('Save')}
+							isDisabled={ ! hasEdits }
+						>
+							{ __( 'Save' ) }
 						</ToolbarButton>
 					</ToolbarGroup>
 				</BlockControls>
 				<InspectorControls>
 					<PanelBody>
-						<TextControl label={__('Name')} value={title} onChange={setTitle} />
+						<TextControl
+							label={ __( 'Name' ) }
+							value={ title }
+							onChange={ setTitle }
+						/>
 					</PanelBody>
 				</InspectorControls>
 				<BlockContentOverlay
-					clientId={clientId}
-					wrapperProps={innerBlocksProps}
-					className='block-library-block__reusable-block-container'
+					clientId={ clientId }
+					wrapperProps={ innerBlocksProps }
+					className="block-library-block__reusable-block-container"
 				/>
-				{isGudieOpen && <ReusableBlockWelcomeGuide isGudieOpen={isGudieOpen} setIsGudieOpen={setIsGudieOpen} />}
+				{ isGudieOpen && (
+					<ReusableBlockWelcomeGuide
+						isGudieOpen={ isGudieOpen }
+						setIsGudieOpen={ setIsGudieOpen }
+					/>
+				) }
 			</div>
 		</RecursionProvider>
 	);
