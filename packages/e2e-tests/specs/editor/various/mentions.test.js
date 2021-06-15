b170db1ee7d72f56cd63235555b8359e55b001cb
Bernie Reiter <ockham@raz.or.at>
2021-06-15 15:50:47 +0200

    Add test for two subsequent mentions

--- packages/e2e-tests/specs/editor/various/mentions.test.js
+++ packages/e2e-tests/specs/editor/various/mentions.test.js
@@ -3,18 +3,40 @@
  */
 import {
 	createNewPost,
+	createUser,
+	deleteUser,
 	getEditedPostContent,
 	clickBlockAppender,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'autocomplete mentions', () => {
 	beforeAll( async () => {
+		await createUser( 'testuser', 'Jane', 'Doe' );
+	} );
+
+	beforeEach( async () => {
 		await createNewPost();
+	} );
+
+	afterAll( async () => {
+		await deleteUser( 'testuser' );
 	} );
 
 	it( 'should insert mention', async () => {
 		await clickBlockAppender();
 		await page.keyboard.type( 'I am @a' );
+		await page.waitForSelector( '.components-autocomplete__result' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '.' );
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'should insert two subsequent mentions', async () => {
+		await clickBlockAppender();
+		await page.keyboard.type( 'I am @ja' );
+		await page.waitForSelector( '.components-autocomplete__result' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( ' @a' );
 		await page.waitForSelector( '.components-autocomplete__result' );
 		await page.keyboard.press( 'Enter' );
 		await page.keyboard.type( '.' );
