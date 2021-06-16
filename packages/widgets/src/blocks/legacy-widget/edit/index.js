deb83302ca9cdcb0fb3e2dce10d88bbaf77f87b4
Andrei Draganescu <github@andreidraganescu.info>
2021-06-16 11:46:12 +0000

    remove the widget switcher block toolbar button

--- packages/widgets/src/blocks/legacy-widget/edit/index.js
+++ packages/widgets/src/blocks/legacy-widget/edit/index.js
@@ -13,8 +13,8 @@ import {
 	BlockIcon,
 	store as blockEditorStore,
 } from '@wordpress/block-editor';
-import { ToolbarButton, Spinner, Placeholder } from '@wordpress/components';
-import { brush as brushIcon, update as updateIcon } from '@wordpress/icons';
+import { Spinner, Placeholder } from '@wordpress/components';
+import { brush as brushIcon } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { useState, useCallback } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
@@ -94,12 +94,7 @@ function NotEmpty( {
 } ) {
 	const [ hasPreview, setHasPreview ] = useState( null );
 
-	const {
-		widgetType,
-		hasResolvedWidgetType,
-		isWidgetTypeHidden,
-		isNavigationMode,
-	} = useSelect(
+	const { widgetType, hasResolvedWidgetType, isNavigationMode } = useSelect(
 		( select ) => {
 			const widgetTypeId = id ?? idBase;
 			const hiddenIds =
@@ -144,22 +139,6 @@ function NotEmpty( {
 
 	return (
 		<>
-			{ ! isWidgetTypeHidden && (
-				<BlockControls group="block">
-					<ToolbarButton
-						label={ __( 'Change widget' ) }
-						icon={ updateIcon }
-						onClick={ () =>
-							setAttributes( {
-								id: null,
-								idBase: null,
-								instance: null,
-							} )
-						}
-					/>
-				</BlockControls>
-			) }
-
 			{ idBase === 'text' && (
 				<BlockControls group="other">
 					<ConvertToBlocksButton
