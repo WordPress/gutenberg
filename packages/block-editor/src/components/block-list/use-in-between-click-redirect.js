73939a05002f8aaeb4301ddb7b4eade191311bcc
Ella van Durpe <ella@vandurpe.com>
2021-06-16 14:11:32 +0300

    Add inline docs

--- packages/block-editor/src/components/block-list/use-in-between-click-redirect.js
+++ packages/block-editor/src/components/block-list/use-in-between-click-redirect.js
@@ -4,10 +4,28 @@
 import { useRefEffect } from '@wordpress/compose';
 import { placeCaretAtVerticalEdge, focus } from '@wordpress/dom';
 
+/**
+ * Checks if the coordinates are within a given DOM rectangle.
+ *
+ * @param {DOMRect} rect Rectangle to check.
+ * @param {number}  x    X coordinate to check.
+ * @param {number}  y    Y coordinate to check.
+ *
+ * @return {boolean} True if within rectangle, false if not.
+ */
 function isWithinRect( rect, x, y ) {
 	return x > rect.left && x < rect.right && y > rect.top && y < rect.bottom;
 }
 
+/**
+ * Checks if the coordinates are within the given document's viewport.
+ *
+ * @param {Document} doc Document to check the viewport.
+ * @param {number}   x   X coordinate to check.
+ * @param {number}   y   Y coordinate to check.
+ *
+ * @return {boolean} True if within the document's viewport, false if not.
+ */
 function isWithinViewport( doc, x, y ) {
 	const { defaultView } = doc;
 	return (
@@ -18,7 +36,25 @@ function isWithinViewport( doc, x, y ) {
 	);
 }
 
-function getChildElementFromPoint( target, x, y, vector ) {
+/**
+ * Gets the closest descendent element from a given point in a given direction.
+ * The given direction vector will be multiplied with an increasing offset until
+ * a descendent is found or until the target element or viewport boundary is
+ * reached.
+ *
+ * @param {HTMLElement} target Target element to check within.
+ * @param {number}      x      X coordinate to check.
+ * @param {number}      y      Y coordinate to check.
+ * @param {Array}       vector Direction vector.
+ *
+ * @return {HTMLElement|undefined} The closest descendent element if found.
+ */
+function getClosestDescendentElementFromPointInDirection(
+	target,
+	x,
+	y,
+	vector
+) {
 	const rect = target.getBoundingClientRect();
 	let offset = 0;
 
@@ -42,7 +78,19 @@ function getChildElementFromPoint( target, x, y, vector ) {
 	}
 }
 
-function getClosestChildElementFromPoint( target, x, y ) {
+/**
+ * Gets the closest descendent element from a given point. Only returns a
+ * descendent if descendents are found in more than one direction (so that the
+ * point falls in-between descendents).
+ *
+ * @param {HTMLElement} target Target element to check within.
+ * @param {number}      x      X coordinate to check.
+ * @param {number}      y      Y coordinate to check.
+ *
+ * @return {HTMLElement|undefined} The closest descendent element if the point
+ *                                 is found to be in-between descendents.
+ */
+function getClosestInBetweenDescendentElementFromPoint( target, x, y ) {
 	const directions = [
 		[ 1, 0 ],
 		[ 0, 1 ],
@@ -55,7 +103,12 @@ function getClosestChildElementFromPoint( target, x, y ) {
 	let closestElement;
 
 	for ( const direction of directions ) {
-		const result = getChildElementFromPoint( target, x, y, direction );
+		const result = getClosestDescendentElementFromPointInDirection(
+			target,
+			x,
+			y,
+			direction
+		);
 
 		if ( ! result ) {
 			continue;
@@ -78,16 +131,25 @@ function getClosestChildElementFromPoint( target, x, y ) {
 	return closestElement;
 }
 
+/**
+ * Behavioural hook that redirects clicks on the element to the closest
+ * focusable element so that there are no "dead zones" and so that small
+ * focusable elements are easier to focus. The redirect only happens if the
+ * click happens in-between two child elements.
+ *
+ * @return {Function} Ref callback.
+ */
 export function useInBetweenClickRedirect() {
 	return useRefEffect( ( node ) => {
 		function onMouseMove( event ) {
 			const { clientX, clientY, target } = event;
 
+			// Don't consider if the container element is not clicked directly.
 			if ( target !== node ) {
 				return;
 			}
 
-			const closestElement = getClosestChildElementFromPoint(
+			const closestElement = getClosestInBetweenDescendentElementFromPoint(
 				target,
 				clientX,
 				clientY
@@ -108,8 +170,7 @@ export function useInBetweenClickRedirect() {
 			placeCaretAtVerticalEdge(
 				closestFocusableElement,
 				isReverse,
-				carectRect,
-				false
+				carectRect
 			);
 		}
 
