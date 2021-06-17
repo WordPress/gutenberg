63e49ddb9a049b3794ac73b3078442065927caaf
Kjell Reigstad <kjell@kjellr.com>
2021-06-17 10:42:45 -0400

    Reorder items

--- lib/block-patterns.php
+++ lib/block-patterns.php
@@ -173,22 +173,6 @@ function register_gutenberg_patterns() {
 							<!-- /wp:column --></div>
 							<!-- /wp:columns -->',
 		),
-		'template-part/header-two-navigation-areas' => array(
-			'title'      => __( 'Header with two navigation areas and logo', 'gutenberg' ),
-			'categories'    => array( 'page-header' ),
-			'blockTypes' => array( 'core/template-part/header' ),
-			'content'    => '<!-- wp:columns {"verticalAlignment":"center","align":"full"} -->
-							<div class="wp-block-columns alignfull are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
-							<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:navigation {"orientation":"horizontal","itemsJustification":"right"} /--></div>
-							<!-- /wp:column -->
-							<!-- wp:column {"verticalAlignment":"center","width":"120px","style":{"spacing":{"padding":{"top":"40px","bottom":"40px"}}}} -->
-							<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-bottom:40px;flex-basis:120px"><!-- wp:site-logo {"align":"center","width":80,"className":"is-style-rounded"} /--></div>
-							<!-- /wp:column -->
-							<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
-							<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:navigation {"orientation":"horizontal","itemsJustification":"left"} /--></div>
-							<!-- /wp:column --></div>
-							<!-- /wp:columns -->',
-		),
 		'template-part/header-with-social-links' => array(
 			'title'      => __( 'Header with social links', 'gutenberg' ),
 			'categories'    => array( 'page-header' ),
@@ -206,6 +190,22 @@ function register_gutenberg_patterns() {
 							<!-- wp:social-link {"url":"#","service":"instagram"} /-->
 							<!-- wp:social-link {"url":"#","service":"mail"} /--></ul>
 							<!-- /wp:social-links --></div>
+							<!-- /wp:column --></div>
+							<!-- /wp:columns -->',
+		),
+		'template-part/header-two-navigation-areas' => array(
+			'title'      => __( 'Header with two navigation areas and logo', 'gutenberg' ),
+			'categories'    => array( 'page-header' ),
+			'blockTypes' => array( 'core/template-part/header' ),
+			'content'    => '<!-- wp:columns {"verticalAlignment":"center","align":"full"} -->
+							<div class="wp-block-columns alignfull are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+							<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:navigation {"orientation":"horizontal","itemsJustification":"right"} /--></div>
+							<!-- /wp:column -->
+							<!-- wp:column {"verticalAlignment":"center","width":"120px","style":{"spacing":{"padding":{"top":"40px","bottom":"40px"}}}} -->
+							<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-bottom:40px;flex-basis:120px"><!-- wp:site-logo {"align":"center","width":80,"className":"is-style-rounded"} /--></div>
+							<!-- /wp:column -->
+							<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+							<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:navigation {"orientation":"horizontal","itemsJustification":"left"} /--></div>
 							<!-- /wp:column --></div>
 							<!-- /wp:columns -->',
 		),
