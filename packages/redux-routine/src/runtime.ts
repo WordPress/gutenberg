b3b7f17e4a30ba844e1112781711cf07f0da1bed
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-22 06:06:46 -0700

    Put back unused parameter names

--- packages/redux-routine/src/runtime.ts
+++ packages/redux-routine/src/runtime.ts
@@ -30,8 +30,8 @@ export default function createRuntime(
 		controls,
 		( control, actionType ): Control => (
 			value,
-			_,
-			__,
+			next,
+			iterate,
 			yieldNext,
 			yieldError
 		) => {
