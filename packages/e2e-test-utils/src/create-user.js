a66eb69d5838148481e7f668629b7b6e8c7e6753
Bernie Reiter <ockham@raz.or.at>
2021-06-22 22:33:29 +0200

    Allow setting user role

--- packages/e2e-test-utils/src/create-user.js
+++ packages/e2e-test-utils/src/create-user.js
@@ -11,13 +11,20 @@ import { switchUserToTest } from './switch-user-to-test';
 import { visitAdminPage } from './visit-admin-page';
 
 /**
+ * @typedef {'Subscriber' | 'Contributor' | 'Author' | 'Editor' | 'Administrator'} Role
+ */
+
+/**
  * Create a new user account.
  *
  * @param {string}  username  User name.
  * @param {string?} firstName First name.
  * @param {string?} lastName  Last name.
+ * @param {string?} role      Role.
+ *
+ * @return {string} Password for the newly created user account
  */
-export async function createUser( username, firstName, lastName ) {
+export async function createUser( username, firstName, lastName, role ) {
 	await switchUserToAdmin();
 	await visitAdminPage( 'user-new.php' );
 
@@ -29,11 +36,18 @@ export async function createUser( username, firstName, lastName ) {
 	if ( lastName ) {
 		await page.type( '#last_name', lastName );
 	}
+	if ( role ) {
+		await page.select( '#role', role );
+	}
+
 	await page.click( '#send_user_notification' );
+
+	const password = await page.$eval( `#pass1`, ( element ) => element.value );
 
 	await Promise.all( [
 		page.click( '#createusersub' ),
 		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
 	] );
 	await switchUserToTest();
+	return password;
 }
