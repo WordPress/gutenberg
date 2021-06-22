583661187b16bb9e1915fc0e922a741417deb8b9
Kai Hao <kevin830726@gmail.com>
2021-06-22 14:15:23 +0800

    Highlight empty widget areas when dragging within

--- packages/edit-widgets/src/blocks/widget-area/edit/index.js
+++ packages/edit-widgets/src/blocks/widget-area/edit/index.js
@@ -15,6 +15,7 @@ import {
  */
 import WidgetAreaInnerBlocks from './inner-blocks';
 import { store as editWidgetsStore } from '../../../store';
+import useIsDraggingWithin from './use-is-dragging-within';
 
 /** @typedef {import('@wordpress/element').RefObject} RefObject */
 
@@ -116,52 +117,4 @@ const useIsDragging = ( elementRef ) => {
 	}, [] );
 
 	return isDragging;
-};
-
-/**
- * A React hook to determine if it's dragging within the target element.
- *
- * @param {RefObject<HTMLElement>} elementRef The target elementRef object.
- *
- * @return {boolean} Is dragging within the target element.
- */
-const useIsDraggingWithin = ( elementRef ) => {
-	const [ isDraggingWithin, setIsDraggingWithin ] = useState( false );
-
-	useEffect( () => {
-		const { ownerDocument } = elementRef.current;
-
-		function handleDragStart( event ) {
-			// Check the first time when the dragging starts.
-			handleDragEnter( event );
-		}
-
-		// Set to false whenever the user cancel the drag event by either releasing the mouse or press Escape.
-		function handleDragEnd() {
-			setIsDraggingWithin( false );
-		}
-
-		function handleDragEnter( event ) {
-			// Check if the current target is inside the item element.
-			if ( elementRef.current.contains( event.target ) ) {
-				setIsDraggingWithin( true );
-			} else {
-				setIsDraggingWithin( false );
-			}
-		}
-
-		// Bind these events to the document to catch all drag events.
-		// Ideally, we can also use `event.relatedTarget`, but sadly that doesn't work in Safari.
-		ownerDocument.addEventListener( 'dragstart', handleDragStart );
-		ownerDocument.addEventListener( 'dragend', handleDragEnd );
-		ownerDocument.addEventListener( 'dragenter', handleDragEnter );
-
-		return () => {
-			ownerDocument.removeEventListener( 'dragstart', handleDragStart );
-			ownerDocument.removeEventListener( 'dragend', handleDragEnd );
-			ownerDocument.removeEventListener( 'dragenter', handleDragEnter );
-		};
-	}, [] );
-
-	return isDraggingWithin;
 };
--- packages/edit-widgets/src/blocks/widget-area/edit/inner-blocks.js
+++ packages/edit-widgets/src/blocks/widget-area/edit/inner-blocks.js
@@ -3,19 +3,39 @@
  */
 import { useEntityBlockEditor } from '@wordpress/core-data';
 import { InnerBlocks } from '@wordpress/block-editor';
+import { useRef } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import useIsDraggingWithin from './use-is-dragging-within';
 
 export default function WidgetAreaInnerBlocks() {
 	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
 		'root',
 		'postType'
 	);
+	const innerBlocksRef = useRef();
+	const isDraggingWithinInnerBlocks = useIsDraggingWithin( innerBlocksRef );
+	const shouldHighlightDropZone =
+		blocks.length === 0 && isDraggingWithinInnerBlocks;
+
 	return (
-		<InnerBlocks
-			value={ blocks }
-			onInput={ onInput }
-			onChange={ onChange }
-			templateLock={ false }
-			renderAppender={ InnerBlocks.ButtonBlockAppender }
-		/>
+		<div
+			className={
+				shouldHighlightDropZone
+					? 'edit-widgets-highlight-drop-zone'
+					: undefined
+			}
+		>
+			<InnerBlocks
+				ref={ innerBlocksRef }
+				value={ blocks }
+				onInput={ onInput }
+				onChange={ onChange }
+				templateLock={ false }
+				renderAppender={ InnerBlocks.ButtonBlockAppender }
+			/>
+		</div>
 	);
 }
