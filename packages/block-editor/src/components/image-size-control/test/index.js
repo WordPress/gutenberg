e4694f9108a229b13f34a66ccb007d70cf657da3
Staci Cooper <staci.cooper@automattic.com>
2021-06-23 12:39:04 -0700

    Move existing tests from hook to component

--- packages/block-editor/src/components/image-size-control/test/index.js
+++ packages/block-editor/src/components/image-size-control/test/index.js
@@ -6,66 +6,12 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 /**
  * Internal dependencies
  */
-import useDimensionHandler from '../use-dimension-handler';
+import ImageSizeControl from '../index';
 
-describe( 'useDimensionHandler hook', () => {
+describe( 'ImageSizeControl', () => {
 	const mockOnChange = jest.fn();
-	const getHeightInput = () => screen.getByTestId( 'heightInput' );
-	const getWidthInput = () => screen.getByTestId( 'widthInput' );
-
-	function TestComponent( {
-		height,
-		width,
-		imageHeight,
-		imageWidth,
-		onChange,
-	} ) {
-		const {
-			currentHeight,
-			currentWidth,
-			updateDimension,
-			updateDimensions,
-		} = useDimensionHandler(
-			height,
-			width,
-			imageHeight,
-			imageWidth,
-			onChange
-		);
-
-		return (
-			<>
-				<input
-					data-testid="heightInput"
-					type="text"
-					value={ currentHeight }
-					onChange={ ( event ) =>
-						updateDimension( 'height', event.target.value )
-					}
-				/>
-				<input
-					data-testid="widthInput"
-					type="text"
-					value={ currentWidth }
-					onChange={ ( event ) =>
-						updateDimension( 'width', event.target.value )
-					}
-				/>
-
-				{ /* Contrived example with hard-coded value serves to demonstrate updates. */ }
-				<input
-					type="button"
-					value="Update"
-					onClick={ () => updateDimensions( 200, 300 ) }
-				/>
-				<input
-					value="Reset"
-					type="button"
-					onClick={ () => updateDimensions() }
-				/>
-			</>
-		);
-	}
+	const getHeightInput = () => screen.getByLabelText( 'Height' );
+	const getWidthInput = () => screen.getByLabelText( 'Width' );
 
 	afterEach( () => {
 		// cleanup on exiting
@@ -74,24 +20,12 @@ describe( 'useDimensionHandler hook', () => {
 
 	it( 'returns custom dimensions when they exist', () => {
 		render(
-			<TestComponent
-				height="100"
-				width="200"
-				imageHeight="300"
-				imageWidth="400"
-				onChange={ mockOnChange }
-			/>
-		);
-
-		expect( getHeightInput().value ).toBe( '100' );
-		expect( getWidthInput().value ).toBe( '200' );
-	} );
-
-	it( 'returns default dimensions when custom dimensions are undefined', () => {
-		render(
-			<TestComponent
-				imageHeight="300"
-				imageWidth="400"
+			<ImageSizeControl
+				imageHeight="100"
+				imageWidth="200"
+				height="300"
+				width="400"
+				slug=""
 				onChange={ mockOnChange }
 			/>
 		);
@@ -100,8 +34,21 @@ describe( 'useDimensionHandler hook', () => {
 		expect( getWidthInput().value ).toBe( '400' );
 	} );
 
+	it( 'returns default dimensions when custom dimensions are undefined', () => {
+		render(
+			<ImageSizeControl
+				imageHeight="100"
+				imageWidth="200"
+				onChange={ mockOnChange }
+			/>
+		);
+
+		expect( getHeightInput().value ).toBe( '100' );
+		expect( getWidthInput().value ).toBe( '200' );
+	} );
+
 	it( 'returns empty string when custom and default dimensions are undefined', () => {
-		render( <TestComponent onChange={ mockOnChange } /> );
+		render( <ImageSizeControl onChange={ mockOnChange } /> );
 
 		expect( getHeightInput().value ).toBe( '' );
 		expect( getWidthInput().value ).toBe( '' );
@@ -111,7 +58,7 @@ describe( 'useDimensionHandler hook', () => {
 		// Simulates an initial render with custom and default dimensions undefined.
 		// This occurs when an image is uploaded for the first time, for example.
 		const { rerender } = render(
-			<TestComponent onChange={ mockOnChange } />
+			<ImageSizeControl onChange={ mockOnChange } />
 		);
 
 		const heightInput = getHeightInput();
@@ -124,7 +71,7 @@ describe( 'useDimensionHandler hook', () => {
 		// When new default dimensions are passed on a rerender (for example after they
 		// are calculated following an image upload), update values to the new defaults.
 		rerender(
-			<TestComponent
+			<ImageSizeControl
 				imageHeight="300"
 				imageWidth="400"
 				onChange={ mockOnChange }
@@ -136,12 +83,12 @@ describe( 'useDimensionHandler hook', () => {
 		expect( widthInput.value ).toBe( '400' );
 	} );
 
-	describe( 'updateDimension', () => {
+	describe( 'updating dimension inputs', () => {
 		it( 'updates height and calls onChange', async () => {
-			render( <TestComponent onChange={ mockOnChange } /> );
+			render( <ImageSizeControl onChange={ mockOnChange } /> );
 
-			const heightInput = screen.getByTestId( 'heightInput' );
-			const widthInput = screen.getByTestId( 'widthInput' );
+			const heightInput = getHeightInput();
+			const widthInput = getWidthInput();
 
 			expect( heightInput.value ).toBe( '' );
 			expect( widthInput.value ).toBe( '' );
@@ -158,7 +105,7 @@ describe( 'useDimensionHandler hook', () => {
 		} );
 
 		it( 'updates width and calls onChange', async () => {
-			render( <TestComponent onChange={ mockOnChange } /> );
+			render( <ImageSizeControl onChange={ mockOnChange } /> );
 
 			const heightInput = getHeightInput();
 			const widthInput = getWidthInput();
@@ -178,7 +125,7 @@ describe( 'useDimensionHandler hook', () => {
 
 		it( 'updates height and calls onChange for empty value', async () => {
 			render(
-				<TestComponent
+				<ImageSizeControl
 					imageHeight="100"
 					imageWidth="100"
 					onChange={ mockOnChange }
@@ -208,7 +155,7 @@ describe( 'useDimensionHandler hook', () => {
 
 		it( 'updates width and calls onChange for empty value', async () => {
 			render(
-				<TestComponent
+				<ImageSizeControl
 					imageHeight="100"
 					imageWidth="100"
 					onChange={ mockOnChange }
@@ -237,12 +184,14 @@ describe( 'useDimensionHandler hook', () => {
 		} );
 	} );
 
-	describe( 'updateDimensions', () => {
-		it( 'updates both height and width', async () => {
+	describe( 'reset button', () => {
+		it( 'resets both height and width to default values', async () => {
 			render(
-				<TestComponent
+				<ImageSizeControl
 					imageHeight="100"
-					imageWidth="100"
+					imageWidth="200"
+					height="300"
+					width="400"
 					onChange={ mockOnChange }
 				/>
 			);
@@ -251,53 +200,21 @@ describe( 'useDimensionHandler hook', () => {
 			const widthInput = getWidthInput();
 
 			// The initial dimension values display first.
-			expect( heightInput.value ).toBe( '100' );
-			expect( widthInput.value ).toBe( '100' );
-
-			fireEvent.click( screen.getByText( 'Update' ) );
-
-			await waitFor( () => {
-				// onChange is called with values for both dimensions.
-				expect( mockOnChange ).toHaveBeenCalledWith( {
-					height: 200,
-					width: 300,
-				} );
-
-				// The new dimension values have been set.
-				expect( heightInput.value ).toBe( '200' );
-				expect( widthInput.value ).toBe( '300' );
-			} );
-		} );
-		it( 'resets dimensions to defaults when values not provided', async () => {
-			render(
-				<TestComponent
-					imageHeight="100"
-					imageWidth="100"
-					height="200"
-					width="200"
-					onChange={ mockOnChange }
-				/>
-			);
-
-			const heightInput = getHeightInput();
-			const widthInput = getWidthInput();
-
-			// The initial dimension values display first.
-			expect( heightInput.value ).toBe( '200' );
-			expect( widthInput.value ).toBe( '200' );
+			expect( heightInput.value ).toBe( '300' );
+			expect( widthInput.value ).toBe( '400' );
 
 			fireEvent.click( screen.getByText( 'Reset' ) );
 
 			await waitFor( () => {
-				// The attributes are set to undefined when values were not provided.
+				// Both attributes are set to undefined to clear custom values.
 				expect( mockOnChange ).toHaveBeenCalledWith( {
 					height: undefined,
 					width: undefined,
 				} );
 
-				// The display values are reset to defaults even though the attributes are undefined.
+				// The inputs display the default values once more.
 				expect( heightInput.value ).toBe( '100' );
-				expect( widthInput.value ).toBe( '100' );
+				expect( widthInput.value ).toBe( '200' );
 			} );
 		} );
 	} );
