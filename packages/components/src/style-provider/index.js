0e494d73fc7a2c1d8d341426137c16ec4e1a1a9a
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-24 06:48:19 -0700

    Prevent collisions

--- packages/components/src/style-provider/index.js
+++ packages/components/src/style-provider/index.js
@@ -6,9 +6,15 @@ import createCache from '@emotion/cache';
 import memoize from 'memize';
 import * as uuid from 'uuid';
 
+const uuidCache = new Set();
+
 const memoizedCreateCacheWithContainer = memoize( ( container ) => {
 	// emotion only accepts alphabetical and hyphenated keys so we just strip the numbers from the UUID. It _should_ be fine.
-	const key = uuid.v4().replace( /[0-9]/g, '' );
+	let key = uuid.v4().replace( /[0-9]/g, '' );
+	while ( uuidCache.has( key ) ) {
+		key = uuid.v4().replace( /[0-9]/g, '' );
+	}
+	uuidCache.add( key );
 	return createCache( { container, key } );
 } );
 
