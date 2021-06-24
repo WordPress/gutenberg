9e95152c5a0287c326ecb19f6227e97aca565b85
tellthemachines <tellthemachines@users.noreply.github.com>
2021-06-24 13:23:13 +1000

    Fix legacy widget edit style bleed (#32871)
    * Fix legacy widget edit style bleed
    
    * Match block width with inserter.
    
    * Make sure all text in legacy widget placeholders is legible.
    
    * Fix background color issue.

--- packages/block-editor/src/components/editor-styles/index.js
+++ packages/block-editor/src/components/editor-styles/index.js
@@ -35,7 +35,7 @@ function useDarkThemeBodyClassName( styles ) {
 				// DOM, so calculate the background color by creating a fake
 				// wrapper.
 				const tempCanvas = ownerDocument.createElement( 'div' );
-				tempCanvas.classList.add( EDITOR_STYLES_SELECTOR );
+				tempCanvas.classList.add( 'editor-styles-wrapper' );
 				body.appendChild( tempCanvas );
 
 				backgroundColor = defaultView
