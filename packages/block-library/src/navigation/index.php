221bdca9ba41e6ed02fc4e29d30d327fca6ca182
tellthemachines <isabel@tellthemachines.com>
2021-06-24 14:42:40 +1000

    lint php

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -174,14 +174,14 @@ function render_block_core_navigation( $attributes, $content, $block ) {
 	);
 
 	$inner_blocks_html = '';
-	$is_list_open = false;
+	$is_list_open      = false;
 	foreach ( $block->inner_blocks as $inner_block ) {
-		if ($inner_block->name === "core/navigation-link" && $is_list_open === false) {
-			$is_list_open = true;
+		if ( 'core/navigation-link' === $inner_block->name && false === $is_list_open ) {
+			$is_list_open       = true;
 			$inner_blocks_html .= '<ul class="wp-block-navigation__container">';
 		}
-		if ($inner_block->name !== "core/navigation-link" && $is_list_open === true) {
-			$is_list_open = false;
+		if ( 'core/navigation-link' !== $inner_block->name && true === $is_list_open ) {
+			$is_list_open       = false;
 			$inner_blocks_html .= '</ul>';
 		}
 		$inner_blocks_html .= $inner_block->render();
