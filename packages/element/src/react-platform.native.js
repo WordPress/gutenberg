6099b36783f05dcdc57d207fd6fa6f2d0bb075e5
Carlos Garcia <fluiddot@gmail.com>
2021-06-25 13:14:00 +0200

    Prevent calling pre-render WP hook multiple times

--- packages/element/src/react-platform.native.js
+++ packages/element/src/react-platform.native.js
@@ -14,8 +14,21 @@ import { applyFilters, doAction } from '@wordpress/hooks';
  */
 import { cloneElement } from './react';
 
+const registeredComponents = {};
+
 const render = ( element, id ) =>
 	AppRegistry.registerComponent( id, () => ( propsFromParent ) => {
+		// This callback can be called multiple times in development when a warning or error
+		// is triggered when executing code below so we have to prevent it.
+		// Reference: https://github.com/WordPress/gutenberg/issues/32882#issuecomment-868414379
+		// eslint-disable-next-line no-undef
+		if ( __DEV__ ) {
+			if ( registeredComponents[ id ] ) {
+				return;
+			}
+			registeredComponents[ id ] = true;
+		}
+
 		const parentProps = omit( propsFromParent || {}, [ 'rootTag' ] );
 
 		doAction( 'native.pre-render', parentProps );
