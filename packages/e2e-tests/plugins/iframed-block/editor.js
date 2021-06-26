6612cac606f07b467841d3ecff32aff31c376e3e
Ella van Durpe <ella@vandurpe.com>
2021-06-26 17:20:12 +0300

    Fix style

--- packages/e2e-tests/plugins/iframed-block/editor.js
+++ packages/e2e-tests/plugins/iframed-block/editor.js
@@ -1,18 +1,18 @@
-( ( { wp: { element, blocks, blockEditor }, jQuery: $ } ) => {
-	const { createElement: el, useEffect, useRef } = element;
+( ( { wp: { element, blocks, blockEditor, compose }, jQuery: $ } ) => {
+	const { createElement: el } = element;
 	const { registerBlockType } = blocks;
 	const { useBlockProps } = blockEditor;
+	const { useRefEffect } = compose;
 
 	registerBlockType( 'test/iframed-block', {
-        edit: function Edit() {
-            const ref = useRef();
-            useEffect( () => {
-                $( ref.current ).test();
-            } );
-            return el( 'p', useBlockProps( { ref } ), 'Iframed Block (edit)' );
-        },
-        save: function Save() {
-            return el( 'p', useBlockProps.save(), 'Iframed Block (saved)' );
-        },
+		edit: function Edit() {
+			const ref = useRefEffect( ( node ) => {
+				$( node ).test();
+			}, [] );
+			return el( 'p', useBlockProps( { ref } ), 'Iframed Block (edit)' );
+		},
+		save: function Save() {
+			return el( 'p', useBlockProps.save(), 'Iframed Block (saved)' );
+		},
 	} );
 } )( window );
