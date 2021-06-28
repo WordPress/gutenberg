d7e5080b939e049eda5a0032f9a3a40204e7d98a
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2021-06-27 22:48:59 -0500

    Polish Custom HTML block.

--- packages/block-library/src/html/index.js
+++ packages/block-library/src/html/index.js
@@ -1,14 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { html as icon } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import edit from './edit';
 import metadata from './block.json';
+import edit from './edit';
 import save from './save';
 import transforms from './transforms';
 
