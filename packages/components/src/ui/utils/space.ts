548e6d6cd4fdbe53a69ec8d8c83d707d76f5b38b
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-28 16:35:57 -0700

    Fix mysteriously broken type

--- packages/components/src/ui/utils/space.ts
+++ packages/components/src/ui/utils/space.ts
@@ -1,13 +1,19 @@
 /**
  * A real number or something parsable as a number
  */
-export type SpaceInput = number | `${ number }`;
+export type SpaceInput = number | string;
 
 const GRID_BASE = '4px';
 
 export function space( value?: SpaceInput ): string | undefined {
 	if ( typeof value === 'undefined' ) {
 		return undefined;
+	}
+
+	const asInt = typeof value === 'number' ? value : parseInt( value, 10 );
+
+	if ( Number.isNaN( asInt ) ) {
+		return value.toString();
 	}
 
 	return `calc(${ GRID_BASE } * ${ value })`;
