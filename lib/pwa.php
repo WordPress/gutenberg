deb33fe4e0ad7d036cda4a564955371aa35751d2
Ella van Durpe <ella@vandurpe.com>
2021-07-01 19:39:00 +0300

    Add service worker (needed for Chrome)

--- lib/pwa.php
+++ lib/pwa.php
@@ -10,7 +10,17 @@ add_filter(
 	function() {
 		// Move to the wp-admin folder when merging with core.
 		$manifest_url = admin_url( '?manifest' );
+		// Must be at the admin root so the scope is correct. Move to the
+		// wp-admin folder when merging with core.
+		$service_worker_url = admin_url( '?service-worker' );
 		echo '<link rel="manifest" crossorigin="use-credentials" href="' . $manifest_url . '">';
+		echo '<script>
+			if( "serviceWorker" in window.navigator ) {
+				window.addEventListener( "load", function() {
+					window.navigator.serviceWorker.register( "' . $service_worker_url . '" );
+				} );
+			}
+		</script>';
 	}
 );
 
@@ -22,6 +32,19 @@ add_filter(
 		}
 
 		require_once __DIR__ . '/manifest.php';
+		exit;
+	}
+);
+
+add_filter(
+	'load-index.php',
+	function() {
+		if ( ! isset( $_GET['service-worker'] ) ) {
+			return;
+		}
+
+		header( 'Content-Type: text/javascript' );
+		echo file_get_contents( __DIR__ . '/service-worker.js' );
 		exit;
 	}
 );
