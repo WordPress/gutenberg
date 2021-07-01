5479e70c12cb836b8193d2cbcdc4b7405977ea5d
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-01 06:20:35 -0700

    Fix missing forwarded ref

--- packages/components/src/ui/item-group/item.tsx
+++ packages/components/src/ui/item-group/item.tsx
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+// eslint-disable-next-line no-restricted-imports
+import type { Ref } from 'react';
+
+/**
  * Internal dependencies
  */
 import { contextConnect, PolymorphicComponentProps } from '../context';
@@ -6,10 +12,13 @@ import type { ItemProps } from './types';
 import { useItem } from './use-item';
 import { ItemView } from './styles';
 
-function Item( props: PolymorphicComponentProps< ItemProps, 'div' > ) {
+function Item(
+	props: PolymorphicComponentProps< ItemProps, 'div' >,
+	forwardedRef: Ref< any >
+) {
 	const contextProps = useItem( props );
 
-	return <ItemView { ...contextProps } />;
+	return <ItemView ref={ forwardedRef } { ...contextProps } />;
 }
 
 const ConnectedItem = contextConnect( Item, 'Item' );
