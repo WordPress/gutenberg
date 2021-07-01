2d92a1e2a40bae1f79b86d29a772c4d267e66334
Marco Ciampini <marco.ciampo@gmail.com>
2021-07-01 09:51:32 +0200

    Do not export FlyoutContent component

--- packages/components/src/flyout/index.js
+++ packages/components/src/flyout/index.js
@@ -1,4 +1,2 @@
 export { default as Flyout } from './flyout';
-// TODO: should the Content component be exported?
-export { default as FlyoutContent } from './flyout-content';
 export * from './context';
