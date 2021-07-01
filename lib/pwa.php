c6af99ac5932c4f3b2c8ab818c85c0dedc95bf04
Ella van Durpe <ella@vandurpe.com>
2021-07-01 21:18:52 +0300

    Fix Chrome PWA

--- lib/pwa.php
+++ lib/pwa.php
@@ -8,12 +8,41 @@
 add_filter(
 	'admin_head',
 	function() {
-		// Move to the wp-admin folder when merging with core.
-		$manifest_url = admin_url( '?manifest' );
+		$icon_sizes = array( 32, 180, 192, 270, 512 );
+
+		// Ideally we have the WordPress logo as the default app logo in the sizes
+		// below.
+		$icons = array();
+
+		if ( has_site_icon() ) {
+			$type = wp_check_filetype( get_site_icon_url() );
+
+			foreach ( $icon_sizes as $size ) {
+				$icons[] = array(
+					'src'   => get_site_icon_url( $size ),
+					'sizes' => $size . 'x' . $size,
+					'type'  => $type['type'],
+				);
+			}
+		}
+
+		$manifest = wp_json_encode(
+			array(
+				'name'        => get_bloginfo( 'name' ),
+				'icons'       => $icons,
+				'display'     => 'standalone',
+				'orientation' => 'portrait',
+				'start_url'   => admin_url(),
+				// Open front-end, login page, and any external URLs in a browser modal.
+				'scope'       => admin_url(),
+			)
+		);
+
 		// Must be at the admin root so the scope is correct. Move to the
 		// wp-admin folder when merging with core.
 		$service_worker_url = admin_url( '?service-worker' );
-		echo '<link rel="manifest" crossorigin="use-credentials" href="' . $manifest_url . '">';
+
+		echo '<link rel="manifest" href=\'data:application/manifest+json,' . $manifest . '\'>';
 		echo '<script>
 			if( "serviceWorker" in window.navigator ) {
 				window.addEventListener( "load", function() {
@@ -21,18 +50,6 @@ add_filter(
 				} );
 			}
 		</script>';
-	}
-);
-
-add_filter(
-	'load-index.php',
-	function() {
-		if ( ! isset( $_GET['manifest'] ) ) {
-			return;
-		}
-
-		require_once __DIR__ . '/manifest.php';
-		exit;
 	}
 );
 
