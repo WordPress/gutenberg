a38fe7a6a07c7e6354b1068509dc5932061cafa8
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-07-01 17:41:48 +1000

    Add custom color sets block support

--- packages/block-editor/src/hooks/color-panel.js
+++ packages/block-editor/src/hooks/color-panel.js
@@ -1,29 +1,82 @@
 /**
  * WordPress dependencies
  */
+import { __experimentalProgressiveDisclosurePanel as ProgressiveDisclosurePanel } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import PanelColorGradientSettings from '../components/colors-gradients/panel-color-gradient-settings';
+import ColorGradientControl from '../components/colors-gradients/control';
 import ContrastChecker from '../components/contrast-checker';
 import InspectorControls from '../components/inspector-controls';
+import useSetting from '../components/use-setting';
 import { __unstableUseBlockRef as useBlockRef } from '../components/block-list/use-block-props/use-block-refs';
 
 function getComputedStyle( node ) {
 	return node.ownerDocument.defaultView.getComputedStyle( node );
 }
 
+// Simple wrapper to allow passing non DOM attribute props that will be
+// picked up an used by the progress disclosure panel
+// e.g. `hasValue` or `onDeselect`
+const ColorSetControlWrapper = ( { children } ) => {
+	return <div className="color-set-control">{ children }</div>;
+};
+
+// Wraps a semantic group of custom color controls and their contrast checker.
+// Making this a single component allows for these controls to be grouped within
+// the progressive disclosure panel's dropdown menu.
+const ColorSetControl = ( {
+	contrastSettings,
+	children,
+	colorSettings,
+	colors,
+	disableCustomColors,
+	disableCustomGradients,
+	gradients,
+	...props
+} ) => {
+	if ( ! colorSettings?.length ) {
+		return null;
+	}
+
+	return (
+		<ColorSetControlWrapper { ...props }>
+			{ colorSettings.map( ( setting, index ) => (
+				<ColorGradientControl
+					key={ index }
+					{ ...{
+						colors,
+						gradients,
+						disableCustomColors,
+						disableCustomGradients,
+						...setting,
+					} }
+				/>
+			) ) }
+			{ contrastSettings && <ContrastChecker { ...contrastSettings } /> }
+			{ children }
+		</ColorSetControlWrapper>
+	);
+};
+
 export default function ColorPanel( {
-	settings,
+	resetAll,
+	colorSets,
 	clientId,
 	enableContrastChecking = true,
+	...props
 } ) {
 	const [ detectedBackgroundColor, setDetectedBackgroundColor ] = useState();
 	const [ detectedColor, setDetectedColor ] = useState();
 	const ref = useBlockRef( clientId );
+
+	const colors = useSetting( 'color.palette' );
+	const gradients = useSetting( 'color.gradients' );
+	const disableCustomColors = ! useSetting( 'color.custom' );
+	const disableCustomGradients = ! useSetting( 'color.customGradient' );
 
 	useEffect( () => {
 		if ( ! enableContrastChecking ) {
@@ -54,18 +107,33 @@ export default function ColorPanel( {
 
 	return (
 		<InspectorControls>
-			<PanelColorGradientSettings
+			<ProgressiveDisclosurePanel
+				label={ __( 'Color options' ) }
 				title={ __( 'Color' ) }
-				initialOpen={ false }
-				settings={ settings }
+				resetAll={ resetAll }
 			>
-				{ enableContrastChecking && (
-					<ContrastChecker
-						backgroundColor={ detectedBackgroundColor }
-						textColor={ detectedColor }
-					/>
-				) }
-			</PanelColorGradientSettings>
+				{ colorSets.map( ( colorSet, index ) => (
+					<ColorSetControl
+						key={ index }
+						{ ...{
+							colors,
+							gradients,
+							disableCustomColors,
+							disableCustomGradients,
+							...colorSet,
+						} }
+						{ ...props }
+					>
+						{ colorSet.checkDefaultContrast &&
+							enableContrastChecking && (
+								<ContrastChecker
+									backgroundColor={ detectedBackgroundColor }
+									textColor={ detectedColor }
+								/>
+							) }
+					</ColorSetControl>
+				) ) }
+			</ProgressiveDisclosurePanel>
 		</InspectorControls>
 	);
 }
