75350d97881329f1ace47f2bf47ddb55e218ac07
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-07-01 12:28:53 +1000

    Handle deprecated numerical border radius

--- packages/block-library/src/search/edit.js
+++ packages/block-library/src/search/edit.js
@@ -75,6 +75,14 @@ export default function SearchEdit( {
 
 	const borderRadius = style?.border?.radius;
 	const borderProps = useBorderProps( attributes );
+
+	// Check for old deprecated numerical border radius. Done as a separate
+	// check so that a borderRadius style won't overwrite the longhand
+	// per-corner styles.
+	if ( typeof borderRadius === 'number' ) {
+		borderProps.style.borderRadius = `${ borderRadius }px`;
+	}
+
 	const unitControlInstanceId = useInstanceId( UnitControl );
 	const unitControlInputId = `wp-block-search__width-${ unitControlInstanceId }`;
 
