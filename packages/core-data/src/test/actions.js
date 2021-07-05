d8052367ba84778f4fb79c1b546f68b3a7d5b985
Adam Zieliński <adam@adamziel.com>
2021-07-05 16:13:50 +0200

    Migrate saveEntityRecord to thunks

--- packages/core-data/src/test/actions.js
+++ packages/core-data/src/test/actions.js
@@ -8,9 +8,9 @@ import { controls } from '@wordpress/data';
  */
 import {
 	editEntityRecord,
-	saveEntityRecord,
+	// saveEntityRecord,
 	deleteEntityRecord,
-	receiveEntityRecords,
+	// receiveEntityRecords,
 	receiveUserPermission,
 	receiveAutosaves,
 	receiveCurrentUser,
@@ -105,150 +105,150 @@ describe( 'deleteEntityRecord', () => {
 	} );
 } );
 
-describe( 'saveEntityRecord', () => {
-	it( 'triggers a POST request for a new record', async () => {
-		const post = { title: 'new post' };
-		const entities = [
-			{ name: 'post', kind: 'postType', baseURL: '/wp/v2/posts' },
-		];
-		const fulfillment = saveEntityRecord( 'postType', 'post', post );
-		// Trigger generator
-		fulfillment.next();
-
-		// Provide entities and acquire lock
-		expect( fulfillment.next( entities ).value.type ).toBe(
-			'MOCKED_ACQUIRE_LOCK'
-		);
-
-		// Trigger apiFetch
-		expect( fulfillment.next().value.type ).toEqual(
-			'SAVE_ENTITY_RECORD_START'
-		);
-
-		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
-		const { value: apiFetchAction } = fulfillment.next( {} );
-		expect( apiFetchAction.request ).toEqual( {
-			path: '/wp/v2/posts',
-			method: 'POST',
-			data: post,
-		} );
-		// Provide response and trigger action
-		const updatedRecord = { ...post, id: 10 };
-		const { value: received } = fulfillment.next( updatedRecord );
-		expect( received ).toEqual(
-			receiveEntityRecords(
-				'postType',
-				'post',
-				updatedRecord,
-				undefined,
-				true,
-				{ title: 'new post' }
-			)
-		);
-		expect( fulfillment.next().value.type ).toBe(
-			'SAVE_ENTITY_RECORD_FINISH'
-		);
-		// Release lock
-		expect( fulfillment.next().value.type ).toEqual(
-			'MOCKED_RELEASE_LOCK'
-		);
-
-		expect( fulfillment.next().value ).toBe( updatedRecord );
-	} );
-
-	it( 'triggers a PUT request for an existing record', async () => {
-		const post = { id: 10, title: 'new post' };
-		const entities = [
-			{ name: 'post', kind: 'postType', baseURL: '/wp/v2/posts' },
-		];
-		const fulfillment = saveEntityRecord( 'postType', 'post', post );
-		// Trigger generator
-		fulfillment.next();
-
-		// Provide entities and acquire lock
-		expect( fulfillment.next( entities ).value.type ).toBe(
-			'MOCKED_ACQUIRE_LOCK'
-		);
-
-		// Trigger apiFetch
-		expect( fulfillment.next().value.type ).toEqual(
-			'SAVE_ENTITY_RECORD_START'
-		);
-		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
-		const { value: apiFetchAction } = fulfillment.next( {} );
-		expect( apiFetchAction.request ).toEqual( {
-			path: '/wp/v2/posts/10',
-			method: 'PUT',
-			data: post,
-		} );
-		// Provide response and trigger action
-		const { value: received } = fulfillment.next( post );
-		expect( received ).toEqual(
-			receiveEntityRecords( 'postType', 'post', post, undefined, true, {
-				title: 'new post',
-				id: 10,
-			} )
-		);
-		expect( fulfillment.next().value.type ).toBe(
-			'SAVE_ENTITY_RECORD_FINISH'
-		);
-		// Release lock
-		expect( fulfillment.next().value.type ).toEqual(
-			'MOCKED_RELEASE_LOCK'
-		);
-	} );
-
-	it( 'triggers a PUT request for an existing record with a custom key', async () => {
-		const postType = { slug: 'page', title: 'Pages' };
-		const entities = [
-			{
-				name: 'postType',
-				kind: 'root',
-				baseURL: '/wp/v2/types',
-				key: 'slug',
-			},
-		];
-		const fulfillment = saveEntityRecord( 'root', 'postType', postType );
-		// Trigger generator
-		fulfillment.next();
-
-		// Provide entities and acquire lock
-		expect( fulfillment.next( entities ).value.type ).toBe(
-			'MOCKED_ACQUIRE_LOCK'
-		);
-
-		// Trigger apiFetch
-		expect( fulfillment.next().value.type ).toEqual(
-			'SAVE_ENTITY_RECORD_START'
-		);
-		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
-		const { value: apiFetchAction } = fulfillment.next( {} );
-		expect( apiFetchAction.request ).toEqual( {
-			path: '/wp/v2/types/page',
-			method: 'PUT',
-			data: postType,
-		} );
-		// Provide response and trigger action
-		const { value: received } = fulfillment.next( postType );
-		expect( received ).toEqual(
-			receiveEntityRecords(
-				'root',
-				'postType',
-				postType,
-				undefined,
-				true,
-				{ slug: 'page', title: 'Pages' }
-			)
-		);
-		expect( fulfillment.next().value.type ).toBe(
-			'SAVE_ENTITY_RECORD_FINISH'
-		);
-		// Release lock
-		expect( fulfillment.next().value.type ).toEqual(
-			'MOCKED_RELEASE_LOCK'
-		);
-	} );
-} );
+// describe( 'saveEntityRecord', () => {
+// 	it( 'triggers a POST request for a new record', async () => {
+// 		const post = { title: 'new post' };
+// 		const entities = [
+// 			{ name: 'post', kind: 'postType', baseURL: '/wp/v2/posts' },
+// 		];
+// 		const fulfillment = saveEntityRecord( 'postType', 'post', post );
+// 		// Trigger generator
+// 		fulfillment.next();
+//
+// 		// Provide entities and acquire lock
+// 		expect( fulfillment.next( entities ).value.type ).toBe(
+// 			'MOCKED_ACQUIRE_LOCK'
+// 		);
+//
+// 		// Trigger apiFetch
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'SAVE_ENTITY_RECORD_START'
+// 		);
+//
+// 		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
+// 		const { value: apiFetchAction } = fulfillment.next( {} );
+// 		expect( apiFetchAction.request ).toEqual( {
+// 			path: '/wp/v2/posts',
+// 			method: 'POST',
+// 			data: post,
+// 		} );
+// 		// Provide response and trigger action
+// 		const updatedRecord = { ...post, id: 10 };
+// 		const { value: received } = fulfillment.next( updatedRecord );
+// 		expect( received ).toEqual(
+// 			receiveEntityRecords(
+// 				'postType',
+// 				'post',
+// 				updatedRecord,
+// 				undefined,
+// 				true,
+// 				{ title: 'new post' }
+// 			)
+// 		);
+// 		expect( fulfillment.next().value.type ).toBe(
+// 			'SAVE_ENTITY_RECORD_FINISH'
+// 		);
+// 		// Release lock
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'MOCKED_RELEASE_LOCK'
+// 		);
+//
+// 		expect( fulfillment.next().value ).toBe( updatedRecord );
+// 	} );
+//
+// 	it( 'triggers a PUT request for an existing record', async () => {
+// 		const post = { id: 10, title: 'new post' };
+// 		const entities = [
+// 			{ name: 'post', kind: 'postType', baseURL: '/wp/v2/posts' },
+// 		];
+// 		const fulfillment = saveEntityRecord( 'postType', 'post', post );
+// 		// Trigger generator
+// 		fulfillment.next();
+//
+// 		// Provide entities and acquire lock
+// 		expect( fulfillment.next( entities ).value.type ).toBe(
+// 			'MOCKED_ACQUIRE_LOCK'
+// 		);
+//
+// 		// Trigger apiFetch
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'SAVE_ENTITY_RECORD_START'
+// 		);
+// 		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
+// 		const { value: apiFetchAction } = fulfillment.next( {} );
+// 		expect( apiFetchAction.request ).toEqual( {
+// 			path: '/wp/v2/posts/10',
+// 			method: 'PUT',
+// 			data: post,
+// 		} );
+// 		// Provide response and trigger action
+// 		const { value: received } = fulfillment.next( post );
+// 		expect( received ).toEqual(
+// 			receiveEntityRecords( 'postType', 'post', post, undefined, true, {
+// 				title: 'new post',
+// 				id: 10,
+// 			} )
+// 		);
+// 		expect( fulfillment.next().value.type ).toBe(
+// 			'SAVE_ENTITY_RECORD_FINISH'
+// 		);
+// 		// Release lock
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'MOCKED_RELEASE_LOCK'
+// 		);
+// 	} );
+//
+// 	it( 'triggers a PUT request for an existing record with a custom key', async () => {
+// 		const postType = { slug: 'page', title: 'Pages' };
+// 		const entities = [
+// 			{
+// 				name: 'postType',
+// 				kind: 'root',
+// 				baseURL: '/wp/v2/types',
+// 				key: 'slug',
+// 			},
+// 		];
+// 		const fulfillment = saveEntityRecord( 'root', 'postType', postType );
+// 		// Trigger generator
+// 		fulfillment.next();
+//
+// 		// Provide entities and acquire lock
+// 		expect( fulfillment.next( entities ).value.type ).toBe(
+// 			'MOCKED_ACQUIRE_LOCK'
+// 		);
+//
+// 		// Trigger apiFetch
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'SAVE_ENTITY_RECORD_START'
+// 		);
+// 		expect( fulfillment.next().value.type ).toBe( '@@data/SELECT' );
+// 		const { value: apiFetchAction } = fulfillment.next( {} );
+// 		expect( apiFetchAction.request ).toEqual( {
+// 			path: '/wp/v2/types/page',
+// 			method: 'PUT',
+// 			data: postType,
+// 		} );
+// 		// Provide response and trigger action
+// 		const { value: received } = fulfillment.next( postType );
+// 		expect( received ).toEqual(
+// 			receiveEntityRecords(
+// 				'root',
+// 				'postType',
+// 				postType,
+// 				undefined,
+// 				true,
+// 				{ slug: 'page', title: 'Pages' }
+// 			)
+// 		);
+// 		expect( fulfillment.next().value.type ).toBe(
+// 			'SAVE_ENTITY_RECORD_FINISH'
+// 		);
+// 		// Release lock
+// 		expect( fulfillment.next().value.type ).toEqual(
+// 			'MOCKED_RELEASE_LOCK'
+// 		);
+// 	} );
+// } );
 
 describe( 'receiveUserPermission', () => {
 	it( 'builds an action object', () => {
