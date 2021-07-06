8d4a984cac919c5241136b941b11a436284ec9f0
Adam Zieliński <adam@adamziel.com>
2021-07-06 18:28:23 +0200

    Default to an empty object on export from oldEditor

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -11,13 +11,8 @@ export * from './utils';
 // Problem: there is quite some code expecting another, legacy object available under window.wp.editor
 // Solution: export all the members of legacy window.wp.editor from this new module to maintain backward compatibility
 // For more context, see https://github.com/WordPress/gutenberg/issues/33203
-export const {
-	autop,
-	removep,
-	initialize,
-	remove,
-	getContent,
-} = window?.wp?.oldEditor;
+export const { autop, removep, initialize, remove, getContent } =
+	window?.wp?.oldEditor || {};
 
 /*
  * Backward compatibility
