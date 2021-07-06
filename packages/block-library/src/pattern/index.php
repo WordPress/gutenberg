2f131bcca3af37b05d0fed28067dada46d69be5c
Ben Dwyer <ben@scruffian.com>
2021-07-06 11:59:05 +0100

    i18n: Add pattern block

---
+++ packages/block-library/src/pattern/index.php
@@ -0,0 +1,21 @@
+<?php
+
+function register_block_core_pattern() {
+	register_block_type_from_metadata(
+		__DIR__ . '/pattern',
+		array(
+			'render_callback' => 'render_block_pattern',
+		)
+	);
+}
+
+function render_block_pattern( $attributes ) {
+	$slug = $attributes['slug'];
+	if( class_exists( 'WP_Block_Type_Registry' ) && WP_Block_Patterns_Registry::get_instance()->is_registered( $slug ) ) {
+		$pattern = WP_Block_Patterns_Registry::get_instance()->get_registered( $slug );
+		return do_blocks( $pattern['content'] );
+
+	}
+}
+
+add_action( 'init', 'register_block_core_pattern' );
