b644a7dc1134919205b1ef5226733363bfd413f2
Adam Zieliński <adam@adamziel.com>
2021-07-06 18:10:49 +0200

    Add an explanatory comment

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -7,6 +7,10 @@ export { storeConfig, store } from './store';
 export * from './components';
 export * from './utils';
 
+// This module is exposed as window.wp.editor
+// Problem: there is quite some code expecting another, legacy object available under window.wp.editor
+// Solution: export all the members of legacy window.wp.editor from this new module to maintain backward compatibility
+// For more context, see https://github.com/WordPress/gutenberg/issues/33203
 export const {
 	autop,
 	removep,
