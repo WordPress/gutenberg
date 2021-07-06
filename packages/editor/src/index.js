01daa00771f4cae9efda94192934a8088aff4f2d
Adam Zieliński <adam@adamziel.com>
2021-07-06 18:08:47 +0200

    Export members of legacy window.wp.editor from the new window.wp.editor module for BC

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -7,6 +7,14 @@ export { storeConfig, store } from './store';
 export * from './components';
 export * from './utils';
 
+export const {
+	autop,
+	removep,
+	initialize,
+	remove,
+	getContent,
+} = window.wp.oldEditor;
+
 /*
  * Backward compatibility
  */
