606916f4831204ddabac7a56f8ffa1e1f83a865d
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-08 07:08:06 -0700

    Add debugging code

--- packages/components/src/utils/test/values.js
+++ packages/components/src/utils/test/values.js
@@ -26,14 +26,14 @@ describe( 'isValueNumeric', () => {
 		}
 	);
 
-	it.each( [
-		'999',
-		'99,33',
-		0.0003,
-		2222,
+	it.only.each( [
+		// '999',
+		// '99,33',
+		// 0.0003,
+		// 2222,
 		'22.222.222',
-		-888,
-		new Number(),
+		// -888,
+		// new Number(),
 	] )(
 		'should return `true` for numeric values %s for locale with period delimiter',
 		( x ) => {
