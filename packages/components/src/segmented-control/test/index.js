bcfd9cdd90b6b48f09667c273a0ca532babb64bc
ntsekouras <ntsekouras@outlook.com>
2021-07-08 16:11:41 +0300

    add + update tests

--- packages/components/src/segmented-control/test/index.js
+++ packages/components/src/segmented-control/test/index.js
@@ -2,33 +2,50 @@
  * External dependencies
  */
 import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { act, Simulate } from 'react-dom/test-utils';
 
 /**
  * Internal dependencies
  */
 import { SegmentedControl } from '../index';
 
-describe( 'props', () => {
+describe( 'SegmentedControl', () => {
 	const options = [
 		{ value: 'olaf', label: 'O' },
 		{ value: 'samantha', label: 'S' },
 	];
 
-	test( 'should render correctly', () => {
-		let container;
+	it( 'should render correctly', () => {
+		const { container } = render(
+			<SegmentedControl
+				baseId="segmented"
+				options={ options }
+				label="Test Segmented Control"
+			/>
+		);
+		expect( container.firstChild ).toMatchSnapshot();
+	} );
 
+	it( 'should call onChange with proper value', () => {
+		let container;
+		const mockOnChange = jest.fn();
 		act( () => {
-			const result = render(
+			( { container } = render(
 				<SegmentedControl
 					baseId="segmented"
 					options={ options }
+					value={ options[ 1 ].value }
+					onChange={ mockOnChange }
 					label="Test Segmented Control"
 				/>
-			);
-			container = result.container;
+			) );
 		} );
-
-		expect( container.firstChild ).toMatchSnapshot();
+		const firstButton = container.querySelector(
+			`button[aria-label="${ options[ 0 ].label }"]`
+		);
+		act( () => {
+			Simulate.click( firstButton );
+		} );
+		expect( mockOnChange ).toHaveBeenCalledWith( options[ 0 ].value );
 	} );
 } );
