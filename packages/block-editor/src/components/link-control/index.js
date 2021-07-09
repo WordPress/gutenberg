b2d3dabbbc8b5340905de2babedf2cb39a4c1018
Dave Smith <getdavemail@gmail.com>
2021-07-09 11:27:59 +0100

    Extract default settings to root component scope to ensure tools div renders by default
    This preserves the existing functionality of the control.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -21,6 +21,7 @@ import LinkControlSearchInput from './search-input';
 import LinkPreview from './link-preview';
 import useCreatePage from './use-create-page';
 import { ViewerFill } from './viewer-slot';
+import { DEFAULT_LINK_SETTINGS } from './constants';
 
 /**
  * Default properties associated with a link control value.
@@ -104,7 +105,7 @@ import { ViewerFill } from './viewer-slot';
 function LinkControl( {
 	searchInputPlaceholder,
 	value,
-	settings,
+	settings = DEFAULT_LINK_SETTINGS,
 	onChange = noop,
 	onRemove,
 	noDirectEntry = false,
@@ -193,7 +194,7 @@ function LinkControl( {
 	const shownUnlinkControl =
 		onRemove && value && ! isEditingLink && ! isCreatingPage;
 
-	const showSettingsDrawer = settings?.length;
+	const showSettingsDrawer = !! settings?.length;
 
 	return (
 		<div
@@ -266,26 +267,25 @@ function LinkControl( {
 				/>
 			) }
 
-			{ showSettingsDrawer ||
-				( shownUnlinkControl && (
-					<div className="block-editor-link-control__tools">
-						<LinkControlSettingsDrawer
-							value={ value }
-							settings={ settings }
-							onChange={ onChange }
-						/>
-						{ shownUnlinkControl && (
-							<Button
-								className="block-editor-link-control__unlink"
-								isDestructive
-								variant="link"
-								onClick={ onRemove }
-							>
-								{ __( 'Unlink' ) }
-							</Button>
-						) }
-					</div>
-				) ) }
+			{ ( showSettingsDrawer || shownUnlinkControl ) && (
+				<div className="block-editor-link-control__tools">
+					<LinkControlSettingsDrawer
+						value={ value }
+						settings={ settings }
+						onChange={ onChange }
+					/>
+					{ shownUnlinkControl && (
+						<Button
+							className="block-editor-link-control__unlink"
+							isDestructive
+							variant="link"
+							onClick={ onRemove }
+						>
+							{ __( 'Unlink' ) }
+						</Button>
+					) }
+				</div>
+			) }
 		</div>
 	);
 }
