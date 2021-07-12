b89aaab11e94798f1edd678a1c53f7366c270c1f
Carlos Garcia <fluiddot@gmail.com>
2021-07-12 12:00:19 +0200

    Disable embed block (enabled only for dev)

--- packages/block-library/src/index.native.js
+++ packages/block-library/src/index.native.js
@@ -266,7 +266,7 @@ export const registerCoreBlocks = () => {
 		audio,
 		reusableBlock,
 		search,
-		embed,
+		devOnly( embed ),
 	].forEach( registerBlock );
 
 	registerBlockVariations( socialLink );
