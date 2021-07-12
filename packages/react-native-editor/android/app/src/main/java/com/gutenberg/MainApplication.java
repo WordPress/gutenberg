2bee10d420c893aa4f1d883be1143bfeccea222e
Carlos Garcia <fluiddot@gmail.com>
2021-07-12 12:13:29 +0200

    Show toast text on preview request call in demo project

--- packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainApplication.java
+++ packages/react-native-editor/android/app/src/main/java/com/gutenberg/MainApplication.java
@@ -242,7 +242,7 @@ public class MainApplication extends Application implements ReactApplication, Gu
 
             @Override
             public void requestPreview() {
-
+                Toast.makeText(MainApplication.this, "requestPreview called", Toast.LENGTH_SHORT).show();
             }
 
         }, isDarkMode());
