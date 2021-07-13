d5d8ebc2a9f61216c4b75f5c7070b48f698cd0bd
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2021-07-13 10:19:30 -0400

    Add text domain to translated string.

--- lib/compat/wordpress-5.8/index.php
+++ lib/compat/wordpress-5.8/index.php
@@ -118,7 +118,7 @@ if ( ! function_exists( 'build_query_vars_from_query_block' ) ) {
 function gutenberg_render_legacy_query_loop_block( $attributes, $content, $block ) {
 	trigger_error(
 	/* translators: %1$s: Block type */
-		sprintf( __( 'Block %1$s has been renamed to Post Template. %1$s will be supported until WordPress version 5.9.' ), $block->name ),
+		sprintf( __( 'Block %1$s has been renamed to Post Template. %1$s will be supported until WordPress version 5.9.', 'gutenberg' ), $block->name ),
 		headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE
 	);
 	return render_block_core_post_template( $attributes, $content, $block );
