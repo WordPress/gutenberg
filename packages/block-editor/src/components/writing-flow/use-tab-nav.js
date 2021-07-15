2b4301ddce5e97fb74966bedf95126ea21cbda68
Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
2021-07-15 20:52:23 +0300

    Writing flow: attempt to fix preview e2e failure (#33467)

--- packages/block-editor/src/components/writing-flow/use-tab-nav.js
+++ packages/block-editor/src/components/writing-flow/use-tab-nav.js
@@ -103,7 +103,13 @@ export default function useTabNav() {
 			const direction = isShift ? 'findPrevious' : 'findNext';
 
 			if ( ! hasMultiSelection() && ! getSelectedBlockClientId() ) {
-				setNavigationMode( true );
+				// Preserve the behaviour of entering navigation mode when
+				// tabbing into the content without a block selection.
+				// `onFocusCapture` already did this previously, but we need to
+				// do it again here because after clearing block selection,
+				// focus land on the writing flow container and pressing Tab
+				// will no longer send focus through the focus capture element.
+				if ( event.target === node ) setNavigationMode( true );
 				return;
 			}
 
