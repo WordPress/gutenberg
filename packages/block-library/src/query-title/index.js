ecdd94bf58056fd5f743716abfe31a1461746af0
Sarah Norris <sarah@sekai.co.uk>
2021-07-19 17:22:11 +0100

    Remove Query Title variations

--- packages/block-library/src/query-title/index.js
+++ packages/block-library/src/query-title/index.js
@@ -3,12 +3,10 @@
  */
 import metadata from './block.json';
 import edit from './edit';
-import variations from './variations';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
 	edit,
-	variations,
 };
