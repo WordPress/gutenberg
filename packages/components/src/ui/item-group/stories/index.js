bbfb31c21f967c37855291c80b7b2f4f0201baed
Marco Ciampini <marco.ciampo@gmail.com>
2021-07-19 18:03:58 +0200

    ItemGroup: update storybook to use knobs and updated prop names

--- packages/components/src/ui/item-group/stories/index.js
+++ packages/components/src/ui/item-group/stories/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-alert */
 /* globals alert */
 /**
+ * External dependencies
+ */
+import { boolean, select } from '@storybook/addon-knobs';
+
+/**
+ * Internal dependencies
+ */
+/**
  * Internal dependencies
  */
 import { ItemGroup, Item } from '..';
@@ -12,22 +20,44 @@ export default {
 	title: 'Components (Experimental)/ItemGroup',
 };
 
-export const _default = () => (
-	<ItemGroup style={ { width: '350px' } } bordered>
-		<Item action onClick={ () => alert( 'WordPress.org' ) }>
-			Code is Poetry — Click me!
-		</Item>
-		<Item action onClick={ () => alert( 'WordPress.org' ) }>
-			Code is Poetry — Click me!
-		</Item>
-		<Item action onClick={ () => alert( 'WordPress.org' ) }>
-			Code is Poetry — Click me!
-		</Item>
-		<Item action onClick={ () => alert( 'WordPress.org' ) }>
-			Code is Poetry — Click me!
-		</Item>
-	</ItemGroup>
-);
+export const _default = () => {
+	const itemGroupProps = {
+		isBordered: boolean( 'ItemGroup: isBordered', true ),
+		size: select(
+			'ItemGroup: size',
+			[ 'small', 'medium', 'large' ],
+			'medium'
+		),
+		isSeparated: boolean( 'ItemGroup: isSeparated', false ),
+		isRounded: boolean( 'ItemGroup: isRounded', false ),
+	};
+
+	const itemProps = {
+		size: select(
+			'Item 1: size',
+			[ 'small', 'medium', 'large' ],
+			'medium'
+		),
+		isAction: boolean( 'Item 1: isAction', true ),
+	};
+
+	return (
+		<ItemGroup style={ { width: '350px' } } { ...itemGroupProps }>
+			<Item { ...itemProps } onClick={ () => alert( 'WordPress.org' ) }>
+				Code is Poetry — Click me!
+			</Item>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
+				Code is Poetry — Click me!
+			</Item>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
+				Code is Poetry — Click me!
+			</Item>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
+				Code is Poetry — Click me!
+			</Item>
+		</ItemGroup>
+	);
+};
 
 export const dropdown = () => (
 	<Flyout
@@ -35,16 +65,16 @@ export const dropdown = () => (
 		trigger={ <Button>Open Popover</Button> }
 	>
 		<ItemGroup style={ { padding: 4 } }>
-			<Item action onClick={ () => alert( 'WordPress.org' ) }>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
 				Code is Poetry — Click me!
 			</Item>
-			<Item action onClick={ () => alert( 'WordPress.org' ) }>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
 				Code is Poetry — Click me!
 			</Item>
-			<Item action onClick={ () => alert( 'WordPress.org' ) }>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
 				Code is Poetry — Click me!
 			</Item>
-			<Item action onClick={ () => alert( 'WordPress.org' ) }>
+			<Item isAction onClick={ () => alert( 'WordPress.org' ) }>
 				Code is Poetry — Click me!
 			</Item>
 		</ItemGroup>
