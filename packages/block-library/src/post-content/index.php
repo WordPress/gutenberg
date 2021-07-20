3c8df8268a6f67be91f7dec3d1cefe53ca5c6424
Ben Dwyer <ben@scruffian.com>
2021-07-20 14:35:26 +0100

    Post Content: Add some text to the Post Content block when used in a 404 context

--- packages/block-library/src/post-content/index.php
+++ packages/block-library/src/post-content/index.php
@@ -17,6 +17,9 @@ function render_block_core_post_content( $attributes, $content, $block ) {
 	static $seen_ids = array();
 
 	if ( ! isset( $block->context['postId'] ) ) {
+		if ( is_404() ) {
+			return __( 'It looks like nothing was found at this location.' );
+		}
 		return '';
 	}
 
@@ -46,7 +49,7 @@ function render_block_core_post_content( $attributes, $content, $block ) {
 	unset( $seen_ids[ $post_id ] );
 
 	if ( empty( $content ) ) {
-		return '';
+		return 'hello';
 	}
 
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'entry-content' ) );
