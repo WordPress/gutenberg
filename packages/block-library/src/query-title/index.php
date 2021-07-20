38b767566aec6fcdcd94d15209341cca06cf988e
Ben Dwyer <ben@scruffian.com>
2021-07-20 12:26:01 +0100

    typo fix

--- packages/block-library/src/query-title/index.php
+++ packages/block-library/src/query-title/index.php
@@ -7,7 +7,7 @@
 
 /**
  * Renders the `core/query-title` block on the server.
- * For now it supports Arhive title and Search title,
+ * For now it supports Archive title and Search title,
  * using queried object information
  *
  * @param array $attributes Block attributes.
