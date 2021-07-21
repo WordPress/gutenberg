48f8c469893338ea53e3aeccedc74e73e074c533
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-21 07:30:57 -0700

    Try adding test

--- packages/components/src/slider/test/index.js
+++ packages/components/src/slider/test/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 /**
  * Internal dependencies
@@ -19,8 +19,28 @@ describe( 'Slider', () => {
 		expect( base.firstChild ).toMatchSnapshot();
 	} );
 
-	it( 'should render valuetext for united values', () => {
+	it( 'should render valuetext for unit-ed values', () => {
 		const { container } = render( <Slider defaultValue="50px" /> );
 		expect( container.firstChild ).toMatchDiffSnapshot( base.firstChild );
+	} );
+
+	it( 'should fire blur and focus events', async () => {
+		const onBlur = jest.fn();
+		const onFocus = jest.fn();
+		render( <Slider onBlur={ onBlur } onFocus={ onFocus } /> );
+		const slider = ( await screen.findAllByRole( 'slider' ) )[ 1 ];
+		fireEvent.focusIn( slider );
+		expect( onFocus ).toHaveBeenCalledTimes( 1 );
+		fireEvent.focusOut( slider );
+		expect( onBlur ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should fire onChange', async () => {
+		const onChange = jest.fn();
+		render( <Slider onChange={ onChange } defaultValue="50" /> );
+		const slider = ( await screen.findAllByRole( 'slider' ) )[ 1 ];
+		fireEvent.focusIn( slider );
+		fireEvent.keyPress( slider, { key: 'ArrowUp', code: 'ArrowUp' } );
+		expect( onChange ).toHaveBeenCalledWith( '51' );
 	} );
 } );
