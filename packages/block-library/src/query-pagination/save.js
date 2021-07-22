5e59ec8825768a60d35f84df201fac8f97807db4
Jeff Ong <jonger4@gmail.com>
2021-07-22 16:44:24 -0400

    Add a render callback.

--- packages/block-library/src/query-pagination/save.js
+++ packages/block-library/src/query-pagination/save.js
@@ -1,12 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks } from '@wordpress/block-editor';
 
 export default function save() {
 	return (
-		<div { ...useBlockProps.save() }>
-			<InnerBlocks.Content />
-		</div>
+		<InnerBlocks.Content />
 	);
 }
