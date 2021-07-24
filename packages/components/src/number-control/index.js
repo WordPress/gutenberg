02fa657d0e20a446cd7c3ac6f5249690267efbaa
Mitchell Austin <mr.fye@oneandthesame.net>
2021-07-23 20:23:28 -0700

    Have `InputControl` avoid calling `onChange` for invalid values
    Also use the blur event to commit invalid values allowing extending
    components like `NumberControl` to resolve the value on commmit.

--- packages/components/src/number-control/index.js
+++ packages/components/src/number-control/index.js
@@ -147,10 +147,12 @@ export function NumberControl(
 		}
 
 		/**
-		 * Handles commit (ENTER key press or on blur if isPressEnterToChange)
+		 * Handles ENTER key press or commits. The latter originates from blur
+		 * events or ENTER key presses when isPressEnterToChange is true).
 		 */
 		if (
-			type === inputControlActionTypes.PRESS_ENTER ||
+			( type === inputControlActionTypes.PRESS_ENTER &&
+				! state.isPressEnterToChange ) ||
 			type === inputControlActionTypes.COMMIT
 		) {
 			const applyEmptyValue = required === false && currentValue === '';
@@ -158,6 +160,8 @@ export function NumberControl(
 			state.value = applyEmptyValue
 				? currentValue
 				: constrainValue( currentValue );
+
+			state.error = null;
 		}
 
 		return state;
