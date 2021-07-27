9ed875811a31f3f85cf1fb09ef0c6785d0c0263b
Marco Ciampini <marco.ciampo@gmail.com>
2021-07-27 11:16:51 +0200

    Add JSDocs to types

--- packages/components/src/item-group/types.ts
+++ packages/components/src/item-group/types.ts
@@ -1,18 +1,60 @@
 type ItemSize = 'small' | 'medium' | 'large';
 
 export interface ItemGroupProps {
+	/**
+	 * Renders borders around each items.
+	 *
+	 * @default false
+	 */
 	isBordered?: boolean;
+	/**
+	 * Renders with rounded corners.
+	 *
+	 * @default true
+	 */
 	isRounded?: boolean;
+	/**
+	 * Renders items individually. Even if `isBordered` is `false`, a border
+	 * in between each item will be still be displayed.
+	 *
+	 * @default false
+	 */
 	isSeparated?: boolean;
+	/**
+	 * Determines the amount of padding within the component.
+	 *
+	 * @default 'medium'
+	 */
 	size?: ItemSize;
 }
 
 export interface ItemProps {
+	/**
+	 * Renders the item as an interactive `button` element.
+	 *
+	 * @default false
+	 */
 	isAction?: boolean;
+	/**
+	 * Determines the amount of padding within the component.
+	 *
+	 * @default 'medium'
+	 */
 	size?: ItemSize;
 }
 
 export type ItemGroupContext = {
+	/**
+	 * When true, each `Item` will be styled as an individual item (e.g. with rounded
+	 * borders), instead of being part of the same UI block with the rest of the items.
+	 *
+	 * @default false
+	 */
 	spacedAround: boolean;
+	/**
+	 * Determines the amount of padding within the component.
+	 *
+	 * @default 'medium'
+	 */
 	size: ItemSize;
 };
