0ee2da51f9aa18bc2c7b6e89072f931f0e2d1430
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-07-27 13:07:22 +1000

    Update border support to use ToolsPanel
    Update border support to display via progressive disclosure panel
    
    Updating border support UI styling for grid layout
    
    Update border global styles panel
    
    Fix border global style hasValue checks

--- packages/block-editor/src/hooks/border.js
+++ packages/block-editor/src/hooks/border.js
@@ -2,19 +2,39 @@
  * WordPress dependencies
  */
 import { getBlockSupport } from '@wordpress/blocks';
-import { PanelBody } from '@wordpress/components';
+import {
+	__experimentalToolsPanel as ToolsPanel,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+} from '@wordpress/components';
 import { Platform } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import {
+	BorderColorEdit,
+	hasBorderColorValue,
+	resetBorderColor,
+} from './border-color';
+import {
+	BorderRadiusEdit,
+	hasBorderRadiusValue,
+	resetBorderRadius,
+} from './border-radius';
+import {
+	BorderStyleEdit,
+	hasBorderStyleValue,
+	resetBorderStyle,
+} from './border-style';
+import {
+	BorderWidthEdit,
+	hasBorderWidthValue,
+	resetBorderWidth,
+} from './border-width';
 import InspectorControls from '../components/inspector-controls';
 import useSetting from '../components/use-setting';
-import { BorderColorEdit } from './border-color';
-import { BorderRadiusEdit } from './border-radius';
-import { BorderStyleEdit } from './border-style';
-import { BorderWidthEdit } from './border-width';
+import { cleanEmptyObject } from './utils';
 
 export const BORDER_SUPPORT_KEY = '__experimentalBorder';
 
@@ -39,22 +59,73 @@ export function BorderPanel( props ) {
 		return null;
 	}
 
+	const defaultBorderControls = getBlockSupport( props.name, [
+		BORDER_SUPPORT_KEY,
+		'__experimentalDefaultControls',
+	] );
+
+	// Callback to reset all block support attributes controlled via this panel.
+	const resetAll = () => {
+		const { style } = props.attributes;
+		const newStyle = cleanEmptyObject( {
+			...style,
+			border: undefined,
+		} );
+
+		props.setAttributes( { style: newStyle } );
+	};
+
 	return (
 		<InspectorControls>
-			<PanelBody
+			<ToolsPanel
 				className="block-editor-hooks__border-controls"
-				title={ __( 'Border' ) }
-				initialOpen={ false }
+				label={ __( 'Border options' ) }
+				header={ __( 'Border' ) }
+				resetAll={ resetAll }
 			>
-				{ ( isWidthSupported || isStyleSupported ) && (
-					<div className="block-editor-hooks__border-controls-row">
-						{ isWidthSupported && <BorderWidthEdit { ...props } /> }
-						{ isStyleSupported && <BorderStyleEdit { ...props } /> }
-					</div>
+				{ isWidthSupported && (
+					<ToolsPanelItem
+						className="single-column"
+						hasValue={ () => hasBorderWidthValue( props ) }
+						label={ __( 'Width' ) }
+						onDeselect={ () => resetBorderWidth( props ) }
+						isShownByDefault={ defaultBorderControls?.width }
+					>
+						<BorderWidthEdit { ...props } />
+					</ToolsPanelItem>
 				) }
-				{ isColorSupported && <BorderColorEdit { ...props } /> }
-				{ isRadiusSupported && <BorderRadiusEdit { ...props } /> }
-			</PanelBody>
+				{ isStyleSupported && (
+					<ToolsPanelItem
+						className="single-column"
+						hasValue={ () => hasBorderStyleValue( props ) }
+						label={ __( 'Style' ) }
+						onDeselect={ () => resetBorderStyle( props ) }
+						isShownByDefault={ defaultBorderControls?.style }
+					>
+						<BorderStyleEdit { ...props } />
+					</ToolsPanelItem>
+				) }
+				{ isColorSupported && (
+					<ToolsPanelItem
+						hasValue={ () => hasBorderColorValue( props ) }
+						label={ __( 'Color' ) }
+						onDeselect={ () => resetBorderColor( props ) }
+						isShownByDefault={ defaultBorderControls?.color }
+					>
+						<BorderColorEdit { ...props } />
+					</ToolsPanelItem>
+				) }
+				{ isRadiusSupported && (
+					<ToolsPanelItem
+						hasValue={ () => hasBorderRadiusValue( props ) }
+						label={ __( 'Radius' ) }
+						onDeselect={ () => resetBorderRadius( props ) }
+						isShownByDefault={ defaultBorderControls?.radius }
+					>
+						<BorderRadiusEdit { ...props } />
+					</ToolsPanelItem>
+				) }
+			</ToolsPanel>
 		</InspectorControls>
 	);
 }
@@ -118,3 +189,22 @@ const useIsBorderDisabled = () => {
 
 	return configs.every( Boolean );
 };
+
+/**
+ * Returns a new style object where the specified border attribute has been
+ * removed.
+ *
+ * @param {Object} style     Styles from block attributes.
+ * @param {string} attribute The border style attribute to clear.
+ *
+ * @return {Object} Style object with the specified attribute removed.
+ */
+export function removeBorderAttribute( style, attribute ) {
+	return cleanEmptyObject( {
+		...style,
+		border: {
+			...style?.border,
+			[ attribute ]: undefined,
+		},
+	} );
+}
