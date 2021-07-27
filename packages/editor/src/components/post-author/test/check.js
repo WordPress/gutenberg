0c185169c52463092e273ebc488d40f5dce4dac0
George Mamadashvili <georgemamadashvili@gmail.com>
2021-07-27 11:59:09 +0400

    Update check component unit tests

--- packages/editor/src/components/post-author/test/check.js
+++ packages/editor/src/components/post-author/test/check.js
@@ -9,45 +9,10 @@ import { shallow } from 'enzyme';
 import { PostAuthorCheck } from '../check';
 
 describe( 'PostAuthorCheck', () => {
-	const users = {
-		data: [
-			{
-				id: 1,
-				name: 'admin',
-				capabilities: {
-					level_1: true,
-				},
-			},
-			{
-				id: 2,
-				name: 'subscriber',
-				capabilities: {
-					level_0: true,
-				},
-			},
-			{
-				id: 3,
-				name: 'andrew',
-				capabilities: {
-					level_1: true,
-				},
-			},
-		],
-	};
-
-	it( 'should not render anything if users unknown', () => {
-		const wrapper = shallow(
-			<PostAuthorCheck authors={ [] } hasAssignAuthorAction={ true }>
-				authors
-			</PostAuthorCheck>
-		);
-		expect( wrapper.type() ).toBe( null );
-	} );
-
-	it( 'should not render anything if single user', () => {
+	it( 'should not render anything if has no authors', () => {
 		const wrapper = shallow(
 			<PostAuthorCheck
-				authors={ users.data.slice( 0, 1 ) }
+				hasAuthors={ false }
 				hasAssignAuthorAction={ true }
 			>
 				authors
@@ -58,7 +23,10 @@ describe( 'PostAuthorCheck', () => {
 
 	it( "should not render anything if doesn't have author action", () => {
 		const wrapper = shallow(
-			<PostAuthorCheck authors={ users } hasAssignAuthorAction={ false }>
+			<PostAuthorCheck
+				hasAuthors={ true }
+				hasAssignAuthorAction={ false }
+			>
 				authors
 			</PostAuthorCheck>
 		);
@@ -67,7 +35,7 @@ describe( 'PostAuthorCheck', () => {
 
 	it( 'should render control', () => {
 		const wrapper = shallow(
-			<PostAuthorCheck authors={ users } hasAssignAuthorAction={ true }>
+			<PostAuthorCheck hasAuthors={ true } hasAssignAuthorAction={ true }>
 				authors
 			</PostAuthorCheck>
 		);
