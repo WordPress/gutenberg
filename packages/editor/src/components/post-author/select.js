04c1addbc48a9a9e71a28865e0054e0c8fce6fb4
George Mamadashvili <georgemamadashvili@gmail.com>
2021-07-27 17:07:15 +0400

    Don't map values inside useSelect to avoid rerendering

--- packages/editor/src/components/post-author/select.js
+++ packages/editor/src/components/post-author/select.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useMemo } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { decodeEntities } from '@wordpress/html-entities';
 import { SelectControl } from '@wordpress/components';
@@ -16,18 +17,22 @@ import { AUTHORS_QUERY } from './constants';
 function PostAuthorSelect() {
 	const { editPost } = useDispatch( editorStore );
 	const { postAuthor, authors } = useSelect( ( select ) => {
-		const authorsFromAPI = select( coreStore ).getUsers( AUTHORS_QUERY );
-
 		return {
 			postAuthor: select( editorStore ).getEditedPostAttribute(
 				'author'
 			),
-			authors: authorsFromAPI.map( ( author ) => ( {
-				label: decodeEntities( author.name ),
-				value: author.id,
-			} ) ),
+			authors: select( coreStore ).getUsers( AUTHORS_QUERY ),
 		};
 	}, [] );
+
+	const authorOptions = useMemo( () => {
+		return ( authors ?? [] ).map( ( author ) => {
+			return {
+				value: author.id,
+				label: decodeEntities( author.name ),
+			};
+		} );
+	}, [ authors ] );
 
 	const setAuthorId = ( value ) => {
 		const author = Number( value );
@@ -38,7 +43,7 @@ function PostAuthorSelect() {
 		<SelectControl
 			className="post-author-selector"
 			label={ __( 'Author' ) }
-			options={ authors }
+			options={ authorOptions }
 			onChange={ setAuthorId }
 			value={ postAuthor }
 		/>
