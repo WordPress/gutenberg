155dcbba5d560678ee266ec5d132b556b4362ada
Kerry Liu <kerryliu@gmail.com>
2021-07-27 13:58:23 -0700

    Components: update snackbar to use framer motion instead of react spring

--- packages/components/src/snackbar/list.js
+++ packages/components/src/snackbar/list.js
@@ -3,18 +3,50 @@
  */
 import classnames from 'classnames';
 import { omit, noop } from 'lodash';
-import { useTransition, animated } from 'react-spring/web.cjs';
+import { motion, AnimatePresence } from 'framer-motion';
 
 /**
  * WordPress dependencies
  */
 import { useReducedMotion } from '@wordpress/compose';
-import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Snackbar from './';
+
+const SNACKBAR_VARIANTS = {
+	init: {
+		height: 0,
+		opacity: 0,
+	},
+	open: {
+		height: 'auto',
+		opacity: 1,
+		transition: {
+			height: { stiffness: 1000, velocity: -100 },
+		},
+	},
+	exit: {
+		x: -300,
+		opacity: 0,
+		transition: {
+			x: { stiffness: 1000, velocity: -100 },
+		},
+	},
+};
+
+const SNACKBAR_REDUCE_MOTION_VARIANTS = {
+	init: {
+		opacity: 0,
+	},
+	open: {
+		opacity: 1,
+	},
+	exit: {
+		opacity: 0,
+	},
+};
 
 /**
  * Renders a list of notices.
@@ -29,42 +61,37 @@ import Snackbar from './';
  */
 function SnackbarList( { notices, className, children, onRemove = noop } ) {
 	const isReducedMotion = useReducedMotion();
-	const [ refMap ] = useState( () => new WeakMap() );
-	const transitions = useTransition( notices, ( notice ) => notice.id, {
-		from: { opacity: 0, height: 0 },
-		enter: ( item ) => async ( next ) =>
-			await next( {
-				opacity: 1,
-				height: refMap.get( item ).offsetHeight,
-			} ),
-		leave: () => async ( next ) => {
-			await next( { opacity: 0 } );
-			await next( { height: 0 } );
-		},
-		immediate: isReducedMotion,
-	} );
-
 	className = classnames( 'components-snackbar-list', className );
 	const removeNotice = ( notice ) => () => onRemove( notice.id );
-
 	return (
 		<div className={ className }>
 			{ children }
-			{ transitions.map( ( { item: notice, key, props: style } ) => (
-				<animated.div key={ key } style={ style }>
-					<div
-						className="components-snackbar-list__notice-container"
-						ref={ ( ref ) => ref && refMap.set( notice, ref ) }
-					>
-						<Snackbar
-							{ ...omit( notice, [ 'content' ] ) }
-							onRemove={ removeNotice( notice ) }
+			<AnimatePresence>
+				{ notices.map( ( notice ) => {
+					return (
+						<motion.div
+							initial={ 'init' }
+							animate={ 'open' }
+							exit={ 'exit' }
+							key={ notice.id }
+							variants={
+								isReducedMotion
+									? SNACKBAR_REDUCE_MOTION_VARIANTS
+									: SNACKBAR_VARIANTS
+							}
 						>
-							{ notice.content }
-						</Snackbar>
-					</div>
-				</animated.div>
-			) ) }
+							<div className="components-snackbar-list__notice-container">
+								<Snackbar
+									{ ...omit( notice, [ 'content' ] ) }
+									onRemove={ removeNotice( notice ) }
+								>
+									{ notice.content }
+								</Snackbar>
+							</div>
+						</motion.div>
+					);
+				} ) }
+			</AnimatePresence>
 		</div>
 	);
 }
