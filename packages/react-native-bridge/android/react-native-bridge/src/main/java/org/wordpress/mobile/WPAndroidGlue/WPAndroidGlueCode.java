a2d6175ac9b8490d925bed1c29d0d9cad9c90143
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-07-28 13:55:23 -0500

    Add note regarding required Double type for Android
    Double utilized to satisfy both React Native's expectations
    (https://bit.ly/3f3tsT4) and Gson's deserialization which uses Double
    for all numbers (https://bit.ly/2VfpvUv).

--- packages/react-native-bridge/android/react-native-bridge/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
+++ packages/react-native-bridge/android/react-native-bridge/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
@@ -505,6 +505,8 @@ public class WPAndroidGlueCode {
 
             @Override
             public void requestBlockTypeImpressions(BlockTypeImpressionsCallback blockTypeImpressionsCallback) {
+                // Double utilized to satisfy both React Native's expectations (https://bit.ly/3f3tsT4)
+                // and Gson's deserialization which uses Double for all numbers (https://bit.ly/2VfpvUv)
                 Map<String, Double> storedImpressions = mOnBlockTypeImpressionsEventListener.onRequestBlockTypeImpressions();
                 WritableMap impressions = Arguments.createMap();
                 for (Map.Entry<String, Double> entry: storedImpressions.entrySet()) {
