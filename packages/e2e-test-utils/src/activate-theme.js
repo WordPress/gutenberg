11db21a991423f61ba48b2c226a3aa1b3cb67da3
Kai Hao <kevin830726@gmail.com>
2021-07-28 15:42:04 +0800

    Refactor activateTheme to use wp-shell

--- packages/e2e-test-utils/src/activate-theme.js
+++ packages/e2e-test-utils/src/activate-theme.js
@@ -1,9 +1,7 @@
 /**
  * Internal dependencies
  */
-import { switchUserToAdmin } from './switch-user-to-admin';
-import { switchUserToTest } from './switch-user-to-test';
-import { visitAdminPage } from './visit-admin-page';
+import { shell } from './wp-shell';
 
 /**
  * Activates an installed theme.
@@ -11,18 +9,7 @@ import { visitAdminPage } from './visit-admin-page';
  * @param {string} slug Theme slug.
  */
 export async function activateTheme( slug ) {
-	await switchUserToAdmin();
-	await visitAdminPage( 'themes.php' );
-
-	const activateButton = await page.$(
-		`div[data-slug="${ slug }"] .button.activate`
-	);
-	if ( ! activateButton ) {
-		switchUserToTest();
-		return;
-	}
-
-	await page.click( `div[data-slug="${ slug }"] .button.activate` );
-	await page.waitForSelector( `div[data-slug="${ slug }"].active` );
-	await switchUserToTest();
+	await shell`
+		switch_theme( '${ slug }' );
+	`;
 }
