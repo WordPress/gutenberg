79fb92b6b07ece1a4c6ffc807122a511c382b1ce
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2021-07-29 15:52:57 +0300

    SauceLabs caps configuration on web maxes out to 1.20.1 so, use that

--- packages/react-native-editor/__device-tests__/helpers/caps.js
+++ packages/react-native-editor/__device-tests__/helpers/caps.js
@@ -4,7 +4,7 @@ const ios = {
 	os: 'iOS',
 	deviceOrientation: 'portrait',
 	automationName: 'XCUITest',
-	appiumVersion: '1.20.2', // Sauce Labs requires appiumVersion to be specified.
+	appiumVersion: '1.20.1', // Sauce Labs requires appiumVersion to be specified.
 	app: undefined, // will be set later, locally this is relative to root of project
 	processArguments: {
 		args: [ 'uitesting' ],
