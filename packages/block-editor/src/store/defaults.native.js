475f976c96ffd024c5426962b96efb468916ee56
Antonis Lilis <antonis.lilis@automattic.com>
2021-08-02 13:02:31 +0300

    Changes new gallery flag name

--- packages/block-editor/src/store/defaults.native.js
+++ packages/block-editor/src/store/defaults.native.js
@@ -10,7 +10,7 @@ const SETTINGS_DEFAULTS = {
 	...SETTINGS,
 	// FOR TESTING ONLY - Later, this will come from a REST API
 	// eslint-disable-next-line no-undef
-	__experimentalGalleryRefactor: __DEV__,
+	__unstableGalleryWithImageBlocks: __DEV__,
 	alignWide: true,
 };
 
