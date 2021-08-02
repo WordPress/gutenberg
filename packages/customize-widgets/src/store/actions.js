f53785fbbf674d46101831206a5154d095e39d0d
Kai Hao <kevin830726@gmail.com>
2021-08-02 15:09:17 +0800

    Fix insertion point in Widgets editors (#33802)
    * Fix insertion point in edit-widgets
    
    * Fix insertion point in customize-widgets
    
    * Fix internal action

--- packages/customize-widgets/src/store/actions.js
+++ packages/customize-widgets/src/store/actions.js
@@ -15,3 +15,22 @@ export function __unstableToggleFeature( feature ) {
 		feature,
 	};
 }
+
+/**
+ * Returns an action object used to open/close the inserter.
+ *
+ * @param {boolean|Object} value                Whether the inserter should be
+ *                                              opened (true) or closed (false).
+ *                                              To specify an insertion point,
+ *                                              use an object.
+ * @param {string}         value.rootClientId   The root client ID to insert at.
+ * @param {number}         value.insertionIndex The index to insert at.
+ *
+ * @return {Object} Action object.
+ */
+export function setIsInserterOpened( value ) {
+	return {
+		type: 'SET_IS_INSERTER_OPENED',
+		value,
+	};
+}
