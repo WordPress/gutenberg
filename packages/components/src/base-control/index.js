6f98642e6598708c210782d80257bc0abb44e73b
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-08-02 09:55:45 -0700

    components: Convert Select to TypeScript (#33784)
    * Convert Select to TypeScript
    
    * Stop spreading props into InputBase and add all HTML attributes for select
    
    * Remove need for `omit`

--- packages/components/src/base-control/index.js
+++ packages/components/src/base-control/index.js
@@ -20,7 +20,7 @@ import {
  *                                                             That element should be passed as a child.
  * @property {import('react').ReactNode} help                  If this property is added, a help text will be
  *                                                             generated using help property as the content.
- * @property {import('react').ReactNode} label                 If this property is added, a label will be generated
+ * @property {import('react').ReactNode} [label]               If this property is added, a label will be generated
  *                                                             using label property as the content.
  * @property {boolean}                   [hideLabelFromVision] If true, the label will only be visible to screen readers.
  * @property {string}                    [className]           The class that will be added with "components-base-control" to the
