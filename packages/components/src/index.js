63520aa5dc45f9b774aad47ece74f318d6ac5c35
Kerry Liu <kerryliu@gmail.com>
2021-08-05 08:33:35 -0700

    Avoid exposing AnimatePresence and AnimateSharedLayout for now

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -16,11 +16,7 @@ export {
 	default as Animate,
 	getAnimateClassName as __unstableGetAnimateClassName,
 } from './animate';
-export {
-	__unstableMotion,
-	__unstableAnimatePresence,
-	__unstableAnimateSharedLayout,
-} from './animation';
+export { __unstableMotion } from './animation';
 export { default as AnglePickerControl } from './angle-picker-control';
 export {
 	default as Autocomplete,
