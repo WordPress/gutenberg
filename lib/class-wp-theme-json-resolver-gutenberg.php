10f2ec1b9f81f0fd7920d87109b1125f7f4fa246
André <nosolosw@users.noreply.github.com>
2021-08-06 14:30:43 +0200

    Remove the experimental prefix and rename `theme.json` files (#33925)

--- lib/class-wp-theme-json-resolver-gutenberg.php
+++ lib/class-wp-theme-json-resolver-gutenberg.php
@@ -150,7 +150,7 @@ class WP_Theme_JSON_Resolver_Gutenberg {
 	 */
 	public static function get_fields_to_translate() {
 		if ( null === self::$theme_json_i18n ) {
-			$file_structure        = self::read_json_file( __DIR__ . '/experimental-i18n-theme.json' );
+			$file_structure        = self::read_json_file( __DIR__ . '/theme-i18n.json' );
 			self::$theme_json_i18n = self::extract_paths_to_translate( $file_structure );
 		}
 		return self::$theme_json_i18n;
@@ -247,7 +247,7 @@ class WP_Theme_JSON_Resolver_Gutenberg {
 			return self::$core;
 		}
 
-		$config     = self::read_json_file( __DIR__ . '/experimental-default-theme.json' );
+		$config     = self::read_json_file( __DIR__ . '/theme.json' );
 		$config     = self::translate( $config );
 		self::$core = new WP_Theme_JSON_Gutenberg( $config, 'core' );
 
