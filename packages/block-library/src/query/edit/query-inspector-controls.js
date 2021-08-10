01022c7bda25e66f75f67c9e6357a76e75eadda4
audrasjb <audrasjb@gmail.com>
2021-08-10 07:50:00 +0200

    Use Tags instead of Keywords in Query Block filters

--- packages/block-library/src/query/edit/query-inspector-controls.js
+++ packages/block-library/src/query/edit/query-inspector-controls.js
@@ -235,7 +235,7 @@ export default function QueryInspectorControls( {
 						}
 					/>
 					<TextControl
-						label={ __( 'Keyword' ) }
+						label={ __( 'Tags' ) }
 						value={ querySearch }
 						onChange={ setQuerySearch }
 					/>
