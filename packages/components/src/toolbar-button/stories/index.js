34dcd8e0dce6cf5288fe89b4a1b7c907ecc3f69e
Marco Ciampini <marco.ciampo@gmail.com>
2021-08-10 17:41:33 +0200

    Fix icon src in `ToolbarButton` Storybook example

--- packages/components/src/toolbar-button/stories/index.js
+++ packages/components/src/toolbar-button/stories/index.js
@@ -4,6 +4,11 @@
 import { text } from '@storybook/addon-knobs';
 
 /**
+ * WordPress dependencies
+ */
+import { wordpress } from '@wordpress/icons';
+
+/**
  * Internal dependencies
  */
 import { Toolbar } from '../../';
@@ -13,11 +18,10 @@ export default { title: 'Components/ToolbarButton', component: ToolbarButton };
 
 export const _default = () => {
 	const label = text( 'Label', 'This is an example label.' );
-	const icon = text( 'Icon', 'wordpress' );
 
 	return (
 		<Toolbar label="Example Toolbar">
-			<ToolbarButton icon={ icon } label={ label } />
+			<ToolbarButton icon={ wordpress } label={ label } />
 		</Toolbar>
 	);
 };
