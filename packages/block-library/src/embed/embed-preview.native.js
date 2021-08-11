8ff6687e3ee0b6f4057f9f55d8cbba4747655afb
Carlos Garcia <fluiddot@gmail.com>
2021-08-11 12:08:57 +0200

    [RNMobile] Embed block: Implement specific Embed preview (#33426)
    * Created an EmbedPreview component to house the caption.
    
    * Disabled the placeholder mode temporarily until the linking is enabled.
    
    * added the react-native-web-view package.
    
    * linked react-native-web-view to ios
    
    * integrated react-native-web-view with android
    
    * initial integration of WebView into the embed-preview component.
    
    * added project configurations for react-native-webview android
    
    * Add sandbox component native version
    
    * Use webview to preview embed block
    
    * Update aspect ratio calculation in sandbox component
    
    * Add native styles to sandbox component
    
    * Open embed content in external browser
    
    * Add comment to sandbox webview
    
    * Update react-native-webview package to wp-fork
    
    * Bump react-native-webview version
    
    * Bump react-native-webview version
    
    * Use provider url as base url for embed webview
    
    * Add forceRender param to trySandbox
    
    * Handle case of setting empty URL after preview
    
    * Enable embed preview interaction when selected
    
    * Add max allowed requests to embed preview
    
    * Prevent navigation in embed preview on Android
    
    * Recreate WebView in Android on orientation change
    
    * Disable embed preview interaction
    
    * Remove unused clientId prop from embed web version
    
    * Enable inline preview only in dev mode
    
    * Remove URL from embed block internal state
    
    I noticed that for the mobile version we don't really need to keep the URL in the internal state, as we were only modifying it once it changed, and that's already being handled with the url block's attribute.
    
    * Use memo in embed preview component
    
    * Use memo in sandbox component
    
    * Rename iframe html to content html and update logic
    
    * Bump react-native-webview package
    
    * Add comments to explain the postMessage function
    
    * Remove onBlur prop from embed preview
    
    * Add sandbox classnames
    
    * Use url instead of provider url as key in sandbox component
    
    * Stop passing scripts to sandbox component
    
    * Add size style calculation to sandbox
    
    * Prevent calling updateContentHtml on very render
    
    * Reset sandbox size when device orientation changes
    
    * Update pods in react-native-editor demo project
    
    Co-authored-by: Joel Dean <jdeanjj1000@gmail.com>

--- packages/block-library/src/embed/embed-preview.native.js
+++ packages/block-library/src/embed/embed-preview.native.js
@@ -3,6 +3,7 @@
  */
 import { TouchableWithoutFeedback } from 'react-native';
 import { isEmpty } from 'lodash';
+import classnames from 'classnames/dedupe';
 
 /**
  * WordPress dependencies
@@ -11,21 +12,27 @@ import { View } from '@wordpress/primitives';
 
 import { BlockCaption } from '@wordpress/block-editor';
 import { __, sprintf } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
+import { memo, useState } from '@wordpress/element';
+import { SandBox } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
+import { getPhotoHtml } from './util';
 import EmbedNoPreview from './embed-no-preview';
 
 const EmbedPreview = ( {
+	className,
 	clientId,
 	icon,
 	insertBlocksAfter,
 	isSelected,
 	label,
-	onBlur,
 	onFocus,
+	preview,
+	previewable,
+	type,
+	url,
 } ) => {
 	const [ isCaptionSelected, setIsCaptionSelected ] = useState( false );
 
@@ -53,6 +60,50 @@ const EmbedPreview = ( {
 		}
 	}
 
+	const { provider_url: providerUrl } = preview;
+	const html = 'photo' === type ? getPhotoHtml( preview ) : preview.html;
+	const parsedHost = new URL( url ).host.split( '.' );
+	const parsedHostBaseUrl = parsedHost
+		.splice( parsedHost.length - 2, parsedHost.length - 1 )
+		.join( '.' );
+	const iframeTitle = sprintf(
+		// translators: %s: host providing embed content e.g: www.youtube.com
+		__( 'Embedded content from %s' ),
+		parsedHostBaseUrl
+	);
+	const sandboxClassnames = classnames(
+		type,
+		className,
+		'wp-block-embed__wrapper'
+	);
+
+	const embedWrapper =
+		/* We should render here: <WpEmbedPreview html={ html } /> */
+		'wp-embed' === type ? null : (
+			<>
+				<TouchableWithoutFeedback
+					onPress={ () => {
+						if ( onFocus ) {
+							onFocus();
+						}
+						if ( isCaptionSelected ) {
+							setIsCaptionSelected( false );
+						}
+					} }
+				>
+					<View pointerEvents="box-only">
+						<SandBox
+							html={ html }
+							title={ iframeTitle }
+							type={ sandboxClassnames }
+							providerUrl={ providerUrl }
+							url={ url }
+						/>
+					</View>
+				</TouchableWithoutFeedback>
+			</>
+		);
+
 	return (
 		<TouchableWithoutFeedback
 			accessible={ ! isSelected }
@@ -60,19 +111,25 @@ const EmbedPreview = ( {
 			disabled={ ! isSelected }
 		>
 			<View>
-				<EmbedNoPreview
-					label={ label }
-					icon={ icon }
-					isSelected={ isSelected }
-					onPress={ () => setIsCaptionSelected( false ) }
-				/>
+				{
+					// eslint-disable-next-line no-undef
+					__DEV__ && previewable ? (
+						embedWrapper
+					) : (
+						<EmbedNoPreview
+							label={ label }
+							icon={ icon }
+							isSelected={ isSelected }
+							onPress={ () => setIsCaptionSelected( false ) }
+						/>
+					)
+				}
 				<BlockCaption
 					accessibilityLabelCreator={ accessibilityLabelCreator }
 					accessible
 					clientId={ clientId }
 					insertBlocksAfter={ insertBlocksAfter }
 					isSelected={ isCaptionSelected }
-					onBlur={ onBlur }
 					onFocus={ onFocusCaption }
 				/>
 			</View>
@@ -80,4 +137,4 @@ const EmbedPreview = ( {
 	);
 };
 
-export default EmbedPreview;
+export default memo( EmbedPreview );
