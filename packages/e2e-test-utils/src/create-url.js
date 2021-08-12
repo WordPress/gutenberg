ee72ed313ef0c05887e4cf9d98e6042d21383bc3
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-12 20:33:56 +0200

    Update whitespaces in JSDoc for `createURL`.

--- packages/e2e-test-utils/src/create-url.js
+++ packages/e2e-test-utils/src/create-url.js
@@ -7,7 +7,7 @@ import { WP_BASE_URL } from './shared/config';
  * Creates new URL by parsing given WPPath and query string, relative to the WP base.
  *
  * @param {string} [WPPath=''] String to be serialized as pathname.
- * @param {string} [query]  String to be serialized as query portion of URL.
+ * @param {string} [query]     String to be serialized as query portion of URL.
  * @return {string} String which represents full URL.
  */
 export function createURL( WPPath = '', query = '' ) {
