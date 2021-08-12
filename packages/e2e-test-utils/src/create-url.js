7340ff29f6f69bb2394915366604809e741e4438
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-12 19:33:36 +0200

    Make `createUrl` merge params from path and query
    Use `URLSearchParams`, so we could accept query params in a variety of formats.
    
    Mitigates https://github.com/WordPress/gutenberg/issues/34044.

--- packages/e2e-test-utils/src/create-url.js
+++ packages/e2e-test-utils/src/create-url.js
@@ -4,16 +4,32 @@
 import { WP_BASE_URL } from './shared/config';
 
 /**
- * Creates new URL by parsing given WPPath and query string, relative to the WP base.
+ * Creates new URL by parsing given WPPath and query params, relative to the WP base.
  *
- * @param {string} [WPPath=''] String to be serialized as pathname.
- * @param {string} [query]  String to be serialized as query portion of URL.
+ * Given `query` params are merged with the ones already present in the path.
+ * Any overlapping ones will be overwritten.
+ *
+ * @example
+ *
+ * ```js
+ * createURL( '/foo?a=b&bar=f', { bar: 'baz', fiz: 'a/b/c' } );
+ * // "http://localhost:8889/foo?a=b&bar=baz&fiz=a%2Fb%2Fc"
+ * ```
+ *
+ * @param {string}              [WPPath=''] String to be serialized as pathname.
+ * @param {string|Object|Array} [query]     Query parameters in any format supported by {@link https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/URLSearchParams | URLSearchParams}.
  * @return {string} String which represents full URL.
  */
 export function createURL( WPPath = '', query = '' ) {
 	const url = new URL( WPPath, WP_BASE_URL );
 
-	url.search = query;
+	// Merge query params if any.
+	if ( query ) {
+		for ( const [ key, value ] of new URLSearchParams( query ) ) {
+			// Overwrite any params if already present.
+			url.searchParams.set( key, value );
+		}
+	}
 
 	return url.href;
 }
--- packages/e2e-test-utils/src/test/create-url.js
+++ packages/e2e-test-utils/src/test/create-url.js
@@ -5,20 +5,52 @@ import { createURL } from '../create-url';
 import { WP_BASE_URL } from '../shared/config';
 
 describe( 'createURL', () => {
-	it( 'called with empty string, should return WP_BASE_URL', async () => {
-		expect( createURL( '' ) ).toEqual( WP_BASE_URL + '/' );
+	it( 'called without arguments, should return WP_BASE_URL', async () => {
+		expect( createURL() ).toEqual( WP_BASE_URL + '/' );
 	} );
 
 	it( 'when given relative path, should parse it relatively to the base', async () => {
 		expect( createURL( '/foo' ) ).toEqual( WP_BASE_URL + '/foo' );
 	} );
 
-	it( 'when given query string, should append to the URL', async () => {
+	it( 'when given query, should escape it and append to the URL', async () => {
+		// String
 		expect( createURL( '', '?bar=baz&fiz=a/b/c' ) ).toEqual(
-			WP_BASE_URL + '/?bar=baz&fiz=a/b/c'
+			WP_BASE_URL + '/?bar=baz&fiz=a%2Fb%2Fc'
 		);
-		expect( createURL( '/foo', '?bar=baz&fiz=a/b/c' ) ).toEqual(
-			WP_BASE_URL + '/foo?bar=baz&fiz=a/b/c'
+		// String without `?`
+		expect( createURL( '/foo', 'bar=baz&fiz=a/b/c' ) ).toEqual(
+			WP_BASE_URL + '/foo?bar=baz&fiz=a%2Fb%2Fc'
+		);
+		// Object
+		expect( createURL( '/foo', { bar: 'baz', fiz: 'a/b/c' } ) ).toEqual(
+			WP_BASE_URL + '/foo?bar=baz&fiz=a%2Fb%2Fc'
+		);
+		// Array
+		expect(
+			createURL( '/foo', [
+				[ 'bar', 'baz' ],
+				[ 'fiz', 'a/b/c' ],
+			] )
+		).toEqual( WP_BASE_URL + '/foo?bar=baz&fiz=a%2Fb%2Fc' );
+	} );
+
+	it( 'when given query and a path that already contains one, should merge both to the URL, overwriting any if overlap', async () => {
+		// ? in both
+		expect( createURL( '/?foo=bar', '?bar=baz&fiz=a/b/c' ) ).toEqual(
+			WP_BASE_URL + '/?foo=bar&bar=baz&fiz=a%2Fb%2Fc'
+		);
+		// overwrite
+		expect( createURL( '/foo?bar=foo', '?bar=baz&fiz=a/b/c' ) ).toEqual(
+			WP_BASE_URL + '/foo?bar=baz&fiz=a%2Fb%2Fc'
+		);
+		// query without ?
+		expect( createURL( '/?a=b', '?bar=baz&fiz=a/b/c' ) ).toEqual(
+			WP_BASE_URL + '/?a=b&bar=baz&fiz=a%2Fb%2Fc'
+		);
+		// overwrite without ?
+		expect( createURL( '/foo?bar=foo', '?bar=baz&fiz=a/b/c' ) ).toEqual(
+			WP_BASE_URL + '/foo?bar=baz&fiz=a%2Fb%2Fc'
 		);
 	} );
 
