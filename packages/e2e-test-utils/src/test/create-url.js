991077176708a57af704744457bba24dba3c98a6
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-12 14:28:05 +0200

    Add tests for `@woocommerce/e2e-utils.createUrl`,
    as there were none.

---
+++ packages/e2e-test-utils/src/test/create-url.js
@@ -0,0 +1,20 @@
+/**
+ * Internal dependencies
+ */
+import { createURL } from '../create-url';
+import { WP_BASE_URL } from '../shared/config';
+
+describe( 'createURL', () => {
+	it( 'called with empty string, should return WP_BASE_URL', async () => {
+		expect( createURL( '' ) ).toEqual( WP_BASE_URL + '/' );
+	} );
+
+	it( 'when given relative path, should parse it relatively to the base', async () => {
+		expect( createURL( '/foo' ) ).toEqual( WP_BASE_URL + '/foo' );
+	} );
+
+	it( 'when given query string, should append to the URL', async () => {
+		expect( createURL( '', '?bar=baz&fiz=a/b/c' ) ).toEqual( WP_BASE_URL + '/?bar=baz&fiz=a/b/c' );
+		expect( createURL( '/foo', '?bar=baz&fiz=a/b/c' ) ).toEqual( WP_BASE_URL + '/foo?bar=baz&fiz=a/b/c' );
+	} );
+} );
