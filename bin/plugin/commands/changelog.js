12689d59c9a22a9e3dcd509be3a6dd4ac85929c3
Dave Smith <getdavemail@gmail.com>
2021-08-12 11:35:39 +0100

    Tweak comment

--- bin/plugin/commands/changelog.js
+++ bin/plugin/commands/changelog.js
@@ -711,7 +711,7 @@ async function getChangelog( settings ) {
 function sortFeatureGroups( featureGroups ) {
 	return Object.keys( featureGroups ).sort(
 		( featureAName, featureBName ) => {
-			// Sort "Unknown" to always be at the end
+			// Sort "uncategorized" items to *always* be at the top of the section
 			if ( featureAName === UNKNOWN_FEATURE_FALLBACK_NAME ) {
 				return -1;
 			} else if ( featureBName === UNKNOWN_FEATURE_FALLBACK_NAME ) {
