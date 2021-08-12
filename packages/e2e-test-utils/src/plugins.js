11151d902d6c1aded483eb576591083934d9e4e8
Kai Hao <kevin830726@gmail.com>
2021-08-12 13:57:14 +0800

    Use kebabCase to replace slugify

--- packages/e2e-test-utils/src/plugins.js
+++ packages/e2e-test-utils/src/plugins.js
@@ -1,29 +1,20 @@
 /**
+ * External dependencies
+ */
+import { kebabCase } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { rest } from './rest-api';
-
-/**
- * Sanitize a string to a slug.
- * A shimmed version of WordPress's `sanitize_title()` in JavaScript.
- *
- * @param {string} str The input string,
- * @return {string} The slug.
- */
-function slugify( str ) {
-	return str
-		.trim() // Trim white spaces.
-		.toLowerCase() // Convert to lower cases.
-		.replace( /\s+/g, '-' ) // Collapse whitespace and replace by a dash.
-		.replace( /-+/g, '-' ) // Collapse dashes.
-		.replace( /[^a-z0-9 -]/g, '' ); // Remove invalid chars.
-}
 
 const pluginsMapPromise = ( async function getPluginsMap() {
 	const plugins = await rest( { path: '/wp/v2/plugins' } );
 	const map = {};
 	for ( const plugin of plugins ) {
-		map[ slugify( plugin.name ) ] = plugin.plugin;
+		// Ideally, we should be using sanitize_title() in PHP rather than kebabCase(),
+		// but we don't have the exact port of it in JS.
+		map[ kebabCase( plugin.name ) ] = plugin.plugin;
 	}
 	return map;
 } )();
