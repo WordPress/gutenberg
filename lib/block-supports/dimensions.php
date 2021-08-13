104714fdc1d12a1afe5cac2f2117387688727b2a
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-08-13 10:39:44 +1000

    Restore old spacing.php and remove spacing from dimensions hook

--- lib/block-supports/dimensions.php
+++ lib/block-supports/dimensions.php
@@ -2,6 +2,10 @@
 /**
  * Dimensions block support flag.
  *
+ * This does not include the `spacing` block support even though that visually
+ * appears under the "Dimensions" panel in the editor. It remains in its
+ * original `spacing.php` file for compatibility with core.
+ *
  * @package gutenberg
  */
 
@@ -21,10 +25,10 @@ function gutenberg_register_dimensions_support( $block_type ) {
 		return;
 	}
 
-	$has_spacing_support = gutenberg_block_has_support( $block_type, array( 'spacing' ), false );
+	$has_dimensions_support = gutenberg_block_has_support( $block_type, array( '__experimentalDimensions' ), false );
 	// Future block supports such as height & width will be added here.
 
-	if ( $has_spacing_support ) {
+	if ( $has_dimensions_support ) {
 		$block_type->attributes['style'] = array(
 			'type' => 'object',
 		);
@@ -38,79 +42,39 @@ function gutenberg_register_dimensions_support( $block_type ) {
  * @param WP_Block_Type $block_type       Block Type.
  * @param array         $block_attributes Block attributes.
  *
- * @return array Block spacing CSS classes and inline styles.
+ * @return array Block dimensions CSS classes and inline styles.
  */
 function gutenberg_apply_dimensions_support( $block_type, $block_attributes ) {
-	$spacing_styles = gutenberg_apply_spacing_support( $block_type, $block_attributes );
-	// Future block supports such as height and width will be added here.
-
-	return $spacing_styles;
-}
-
-/**
- * Add CSS classes for block spacing to the incoming attributes array.
- * This will be applied to the block markup in the front-end.
- *
- * @param WP_Block_Type $block_type       Block Type.
- * @param array         $block_attributes Block attributes.
- *
- * @return array Block spacing CSS classes and inline styles.
- */
-function gutenberg_apply_spacing_support( $block_type, $block_attributes ) {
-	if ( gutenberg_skip_spacing_serialization( $block_type ) ) {
+	if ( gutenberg_skip_dimensions_serialization( $block_type ) ) {
 		return array();
 	}
 
-	$has_padding_support = gutenberg_block_has_support( $block_type, array( 'spacing', 'padding' ), false );
-	$has_margin_support  = gutenberg_block_has_support( $block_type, array( 'spacing', 'margin' ), false );
-	$styles              = array();
+	$styles = array();
 
-	if ( $has_padding_support ) {
-		$padding_value = _wp_array_get( $block_attributes, array( 'style', 'spacing', 'padding' ), null );
-
-		if ( is_array( $padding_value ) ) {
-			foreach ( $padding_value as $key => $value ) {
-				$styles[] = sprintf( 'padding-%s: %s;', $key, $value );
-			}
-		} elseif ( null !== $padding_value ) {
-			$styles[] = sprintf( 'padding: %s;', $padding_value );
-		}
-	}
-
-	if ( $has_margin_support ) {
-		$margin_value = _wp_array_get( $block_attributes, array( 'style', 'spacing', 'margin' ), null );
-
-		if ( is_array( $margin_value ) ) {
-			foreach ( $margin_value as $key => $value ) {
-				$styles[] = sprintf( 'margin-%s: %s;', $key, $value );
-			}
-		} elseif ( null !== $margin_value ) {
-			$styles[] = sprintf( 'margin: %s;', $margin_value );
-		}
-	}
+	// Height support to be added in near future.
+	// Width support to be added in near future.
 
 	return empty( $styles ) ? array() : array( 'style' => implode( ' ', $styles ) );
 }
 
 /**
- * Checks whether serialization of the current block's spacing properties should
- * occur.
+ * Checks whether serialization of the current block's dimensions properties
+ * should occur.
  *
  * @param WP_Block_type $block_type Block type.
  *
  * @return boolean Whether to serialize spacing support styles & classes.
  */
-function gutenberg_skip_spacing_serialization( $block_type ) {
-	$spacing_support = _wp_array_get( $block_type->supports, array( 'spacing' ), false );
-
-	return is_array( $spacing_support ) &&
-		array_key_exists( '__experimentalSkipSerialization', $spacing_support ) &&
-		$spacing_support['__experimentalSkipSerialization'];
+function gutenberg_skip_dimensions_serialization( $block_type ) {
+	$dimensions_support = _wp_array_get( $block_type->supports, array( '__experimentalDimensions' ), false );
+	return is_array( $dimensions_support ) &&
+		array_key_exists( '__experimentalSkipSerialization', $dimensions_support ) &&
+		$dimensions_support['__experimentalSkipSerialization'];
 }
 
 // Register the block support.
 WP_Block_Supports::get_instance()->register(
-	'spacing', // This is deliberately `spacing` instead of `dimensions` for backwards compatibility.
+	'dimensions',
 	array(
 		'register_attribute' => 'gutenberg_register_dimensions_support',
 		'apply'              => 'gutenberg_apply_dimensions_support',
