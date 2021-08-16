34e95aceb9567a8a480317c723a7e1dc4719eb84
Riad Benguella <benguella@gmail.com>
2021-08-16 11:37:58 +0100

    Add documentation for the batch function

--- packages/data/src/utils/emitter.js
+++ packages/data/src/utils/emitter.js
@@ -8,6 +8,9 @@ export function createEmitter() {
 	let isPending = false;
 	const listeners = new Set();
 	const notifyListeners = () =>
+		// We use Array.from to clone the listeners Set
+		// This ensures that we don't run a listener
+		// that was added as a response to another listener.
 		Array.from( listeners ).forEach( ( listener ) => listener() );
 
 	return {
