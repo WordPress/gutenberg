37a49fc3e2befc49abf6a13037d3f56bbadbf457
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-08-16 17:19:15 +0200

    Post Title block test: demo refactor

--- packages/e2e-tests/specs/editor/blocks/post-title.test.js
+++ packages/e2e-tests/specs/editor/blocks/post-title.test.js
@@ -13,23 +13,18 @@ describe( 'Post Title block', () => {
 	} );
 
 	it( 'Can edit the post title', async () => {
-		// Create a block with some text that will trigger a list creation.
 		await insertBlock( 'Post Title' );
-		const editablePostTitleSelector =
-			'.wp-block-post-title[contenteditable="true"]';
-		await page.waitForSelector( editablePostTitleSelector );
-		await page.focus( editablePostTitleSelector );
 
-		// Create a second list item.
-		await page.keyboard.type( 'Just tweaking the post title' );
+		const titleText = 'Just tweaking the post title';
+
+		await page.click( '[aria-label="Block: Post Title"]' );
+		await page.keyboard.type( titleText );
 
 		await saveDraft();
 		await page.reload();
-		await page.waitForSelector( '.edit-post-layout' );
-		const title = await page.$eval(
-			'.editor-post-title__input',
-			( element ) => element.textContent
+
+		await page.waitForSelector(
+			`.editor-post-title >> text="${ titleText }"`
 		);
-		expect( title ).toEqual( 'Just tweaking the post title' );
 	} );
 } );
