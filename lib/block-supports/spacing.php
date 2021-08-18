a974db1cf86d1f27c8153981bf9797f870e17b25
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2021-08-18 17:28:04 +1000

    Remove gap from apply_spacing_support so it isn't applied twice for dynamic blocks

--- lib/block-supports/spacing.php
+++ lib/block-supports/spacing.php
@@ -45,7 +45,6 @@ function gutenberg_apply_spacing_support( $block_type, $block_attributes ) {
 
 	$has_padding_support = gutenberg_block_has_support( $block_type, array( 'spacing', 'padding' ), false );
 	$has_margin_support  = gutenberg_block_has_support( $block_type, array( 'spacing', 'margin' ), false );
-	$has_gap_support     = gutenberg_block_has_support( $block_type, array( 'spacing', 'blockGap' ), false );
 	$styles              = array();
 
 	if ( $has_padding_support ) {
@@ -69,14 +68,6 @@ function gutenberg_apply_spacing_support( $block_type, $block_attributes ) {
 			}
 		} elseif ( null !== $margin_value ) {
 			$styles[] = sprintf( 'margin: %s;', $margin_value );
-		}
-	}
-
-	if ( $has_gap_support ) {
-		$gap_value = _wp_array_get( $block_attributes, array( 'style', 'spacing', 'blockGap' ), null );
-
-		if ( is_string( $gap_value ) ) {
-			$styles[] = sprintf( '--wp--style--block-gap: %s', $gap_value );
 		}
 	}
 
