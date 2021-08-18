c69cd3cd9654800364e8b8e05038de8c0b99e1dd
Riad Benguella <benguella@gmail.com>
2021-08-18 15:38:28 +0100

    Restore webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,5 @@
  */
 const blocksConfig = require( './tools/webpack/blocks' );
 const packagesConfig = require( './tools/webpack/packages' );
-const esmPackagesConfig = require( './tools/webpack/esm-packages' );
 
-module.exports = [/* blocksConfig, packagesConfig, */ esmPackagesConfig ];
+module.exports = [ blocksConfig, packagesConfig ];
