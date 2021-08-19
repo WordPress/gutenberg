40d8269225935b3c141d34de3ebdb01c4b0a4784
Matthew Reishus <mreishus@users.noreply.github.com>
2021-08-19 14:19:16 -0500

    Add comment explaining true/false/undefined for enableCustomFields

--- packages/editor/src/store/defaults.js
+++ packages/editor/src/store/defaults.js
@@ -14,7 +14,11 @@ export const PREFERENCES_DEFAULTS = {
  *  allowedBlockTypes  boolean|Array Allowed block types
  *  richEditingEnabled boolean       Whether rich editing is enabled or not
  *  codeEditingEnabled boolean       Whether code editing is enabled or not
- *  enableCustomFields boolean       Whether the WordPress custom fields are enabled or not
+ *  enableCustomFields boolean       Whether the WordPress custom fields are enabled or not.
+ *                                     true  = the user has opted to show the Custom Fields panel at the bottom of the editor.
+ *                                     false = the user has opted to hide the Custom Fields panel at the bottom of the editor.
+ *                                     undefined = the current environment does not support Custom Fields,
+ *                                                 and the user doesn't see the option toggle.
  *  autosaveInterval   number        Autosave Interval
  *  availableTemplates array?        The available post templates
  *  disablePostFormats boolean       Whether or not the post formats are disabled
