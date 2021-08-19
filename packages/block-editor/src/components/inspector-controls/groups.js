acbe6d08101f4d2bd0acc3672a8d150cb358ec77
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-08-19 10:24:41 +0200

    Remove the `block` group in InspectorControls

--- packages/block-editor/src/components/inspector-controls/groups.js
+++ packages/block-editor/src/components/inspector-controls/groups.js
@@ -4,12 +4,10 @@
 import { createSlotFill } from '@wordpress/components';
 
 const InspectorControlsDefault = createSlotFill( 'InspectorControls' );
-const InspectorControlsBlock = createSlotFill( 'InspectorControlsBlock' );
 const InspectorControlsAdvanced = createSlotFill( 'InspectorAdvancedControls' );
 
 const groups = {
 	default: InspectorControlsDefault,
-	block: InspectorControlsBlock,
 	advanced: InspectorControlsAdvanced,
 };
 
