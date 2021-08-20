1b2f104967f7e155e58694d9590d0a76123ea66e
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-08-20 18:41:12 -0500

    Improve the story

--- packages/components/src/confirm/stories/index.js
+++ packages/components/src/confirm/stories/index.js
@@ -1,18 +1,37 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useState } from 'react';
 
 /**
  * Internal dependencies
  */
-import { Confirm } from '..';
+import Button from '../../button';
+import { Confirm, confirm } from '..';
 
 export default {
 	component: Confirm,
 	title: 'Components (Experimental)/Confirm',
+	argTypes: { proceed: { action: 'proceed' } },
 };
 
 export const _default = () => {
-	return <Confirm />;
+	const [ confirmVal, setConfirmVal ] = useState();
+
+	async function triggerConfirm() {
+		if ( await confirm( { confirmation: 'Are you sure?' } ) ) {
+			setConfirmVal( 'You are sure!' );
+		} else {
+			setConfirmVal( 'Ok, take more time to decide!' );
+		}
+	}
+
+	return (
+		<>
+			<Button variant="primary" onClick={ triggerConfirm }>
+				Trigger Confirm
+			</Button>
+			{ confirmVal && <h1>{ confirmVal }</h1> }
+		</>
+	);
 };
