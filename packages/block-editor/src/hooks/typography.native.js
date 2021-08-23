2a4257a3b0aaa7c9554cc59d161b90c433b68501
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-08-23 10:21:03 -0700

    Rnmobile/add/typography controls (#33605)
    * Allow for a non units to show without the unit picker.
    
    * Add Line Height Controls
    
    * Make variable more clear
    
    * Revert Typography web
    
    * Add support for font sizes
    
    * Add bottom Cell sheet subLabel
    
    * Update the colour of the subheading
    
    * Update colours again for subheading
    
    * Make the Typography UI only available in dev build
    
    * Do not show items that we can't parse as numbers
    
    * Add comment
    
    * Move the FontSizePicker export
    
    * Remove the not needed platform check
    
    * Update comment to be more font size specific.
    
    * Fix unit test for customLineHeight
    
    * Revert the units logic to hide the Unit picker
    
    * Remove the added borderRightWidth for picker that doesn't have childern

--- packages/block-editor/src/hooks/typography.native.js
+++ packages/block-editor/src/hooks/typography.native.js
@@ -12,7 +12,6 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import InspectorControls from '../components/inspector-controls';
-import TextDecorationAndTransformEdit from '../components/text-decoration-and-transform';
 
 import {
 	LINE_HEIGHT_SUPPORT_KEY,
@@ -20,62 +19,30 @@ import {
 	useIsLineHeightDisabled,
 } from './line-height';
 import {
-	FONT_STYLE_SUPPORT_KEY,
-	FONT_WEIGHT_SUPPORT_KEY,
-	FontAppearanceEdit,
-	useIsFontAppearanceDisabled,
-} from './font-appearance';
-import {
-	FONT_FAMILY_SUPPORT_KEY,
-	FontFamilyEdit,
-	useIsFontFamilyDisabled,
-} from './font-family';
-import {
 	FONT_SIZE_SUPPORT_KEY,
 	FontSizeEdit,
 	useIsFontSizeDisabled,
 } from './font-size';
-import {
-	TEXT_DECORATION_SUPPORT_KEY,
-	useIsTextDecorationDisabled,
-} from './text-decoration';
-import {
-	TEXT_TRANSFORM_SUPPORT_KEY,
-	useIsTextTransformDisabled,
-} from './text-transform';
-import {
-	LETTER_SPACING_SUPPORT_KEY,
-	LetterSpacingEdit,
-	useIsLetterSpacingDisabled,
-} from './letter-spacing';
 
 export const TYPOGRAPHY_SUPPORT_KEY = 'typography';
 export const TYPOGRAPHY_SUPPORT_KEYS = [
 	LINE_HEIGHT_SUPPORT_KEY,
 	FONT_SIZE_SUPPORT_KEY,
-	FONT_STYLE_SUPPORT_KEY,
-	FONT_WEIGHT_SUPPORT_KEY,
-	FONT_FAMILY_SUPPORT_KEY,
-	TEXT_DECORATION_SUPPORT_KEY,
-	TEXT_TRANSFORM_SUPPORT_KEY,
-	LETTER_SPACING_SUPPORT_KEY,
 ];
 
 export function TypographyPanel( props ) {
 	const isDisabled = useIsTypographyDisabled( props );
 	const isSupported = hasTypographySupport( props.name );
 
-	if ( isDisabled || ! isSupported ) return null;
+	// only enable TypographyPanel for development
+	// eslint-disable-next-line no-undef
+	if ( isDisabled || ! isSupported || ! __DEV__ ) return null;
 
 	return (
 		<InspectorControls>
 			<PanelBody title={ __( 'Typography' ) }>
-				<FontFamilyEdit { ...props } />
 				<FontSizeEdit { ...props } />
-				<FontAppearanceEdit { ...props } />
 				<LineHeightEdit { ...props } />
-				<TextDecorationAndTransformEdit { ...props } />
-				<LetterSpacingEdit { ...props } />
 			</PanelBody>
 		</InspectorControls>
 	);
@@ -89,13 +56,8 @@ const hasTypographySupport = ( blockName ) => {
 
 function useIsTypographyDisabled( props = {} ) {
 	const configs = [
-		useIsFontAppearanceDisabled( props ),
 		useIsFontSizeDisabled( props ),
 		useIsLineHeightDisabled( props ),
-		useIsFontFamilyDisabled( props ),
-		useIsTextDecorationDisabled( props ),
-		useIsTextTransformDisabled( props ),
-		useIsLetterSpacingDisabled( props ),
 	];
 
 	return configs.filter( Boolean ).length === configs.length;
