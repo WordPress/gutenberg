8a05852c2d1a0fd5f55fa85079442ae303d99ea0
Adam Zieliński <adam@adamziel.com>
2021-08-23 13:39:00 +0200

    Remove unnecessary whitespace

--- lib/compat/wordpress-trunk/widget-render-api-endpoint.php
+++ lib/compat/wordpress-trunk/widget-render-api-endpoint.php
@@ -129,7 +129,3 @@ function setup_widget_render_api_endpoint() {
 
 // Priority should be larger than 99 which is the one used for registering the core routes.
 add_action( 'rest_api_init', 'setup_widget_render_api_endpoint', 100 );
-
-
-
-
