2734d2b6b4a42513f7be429682ebef92a3181030
jhnstn <jason@readysetandco.com>
2021-08-25 11:00:37 -0400

    Filter embed blocks from Inserter search block list

--- packages/block-editor/src/components/inserter/search-results.native.js
+++ packages/block-editor/src/components/inserter/search-results.native.js
@@ -12,6 +12,9 @@ import InserterNoResults from './no-results';
 import { store as blockEditorStore } from '../../store';
 import useBlockTypeImpressions from './hooks/use-block-type-impressions';
 
+const NON_BLOCK_CATEGORIES = [ 'reusable' ];
+const ALLOWED_EMBED_VARIATIONS = [ 'core/embed' ];
+
 function InserterSearchResults( {
 	filterValue,
 	onSelect,
@@ -24,7 +27,19 @@ function InserterSearchResults( {
 			const allItems = select( blockEditorStore ).getInserterItems(
 				rootClientId
 			);
-			const filteredItems = searchItems( allItems, filterValue );
+
+			const blockItems = allItems.filter(
+				( { id, category } ) =>
+					! NON_BLOCK_CATEGORIES.includes( category ) &&
+					// We don't want to show all possible embed variations
+					// as different blocks in the inserter. We'll only show a
+					// few popular ones.
+					( category !== 'embed' ||
+						( category === 'embed' &&
+							ALLOWED_EMBED_VARIATIONS.includes( id ) ) )
+			);
+
+			const filteredItems = searchItems( blockItems, filterValue );
 
 			return { blockTypes: filteredItems };
 		},
