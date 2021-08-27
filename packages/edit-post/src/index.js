bc1e788264e3d80d844d161516e6e95bff1a0729
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-08-27 09:59:33 +0200

    Reapply block type filters for all editor screens

--- packages/edit-post/src/index.js
+++ packages/edit-post/src/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { store as blocksStore, registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from '@wordpress/blocks';
 import {
 	registerCoreBlocks,
 	__experimentalRegisterExperimentalCoreBlocks,
@@ -131,8 +131,7 @@ export function initializeEditor(
 		);
 	}
 
-	dispatch( blocksStore ).__experimentalReapplyBlockTypeFilters();
-
+	// dispatch( blocksStore ).__experimentalReapplyBlockTypeFilters();
 	registerCoreBlocks();
 	if ( process.env.GUTENBERG_PHASE === 2 ) {
 		__experimentalRegisterExperimentalCoreBlocks( {
