3dc4cea8498b6397388d9dce105a6a87842a4ea5
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-08-27 15:19:19 +0200

    Simplify transformBlockTo util

--- packages/e2e-test-utils/src/transform-block-to.js
+++ packages/e2e-test-utils/src/transform-block-to.js
@@ -11,25 +11,10 @@ import { showBlockToolbar } from './show-block-toolbar';
 export async function transformBlockTo( name ) {
 	await showBlockToolbar();
 
-	const switcherToggle = await page.waitForSelector(
-		'.block-editor-block-switcher__toggle'
+	await page.click( '.block-editor-block-switcher__toggle' );
+	await page.click(
+		`.block-editor-block-switcher__popover :text-is("${ name }")`
 	);
-	await switcherToggle.evaluate( ( element ) => element.scrollIntoView() );
-	await page.waitForSelector( '.block-editor-block-switcher__toggle', {
-		visible: true,
-	} );
-	await switcherToggle.click();
-	await page.waitForSelector( '.block-editor-block-switcher__container', {
-		visible: true,
-	} );
-
-	// Find the block button option within the switcher popover.
-	const xpath = `//*[contains(@class, "block-editor-block-switcher__popover")]//button[.='${ name }']`;
-	const insertButton = await page.waitForXPath( xpath, { visible: true } );
-	// Clicks may fail if the button is out of view. Assure it is before click.
-	await insertButton.evaluate( ( element ) => element.scrollIntoView() );
-	await insertButton.click();
-
 	// Wait for the transformed block to appear.
 	const BLOCK_SELECTOR = '.block-editor-block-list__block';
 	const BLOCK_NAME_SELECTOR = `[data-title="${ name }"]`;
