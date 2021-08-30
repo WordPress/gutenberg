beb6a7839f3f731588e599efc721cffab75df345
Kai Hao <kevin830726@gmail.com>
2021-08-30 15:16:18 +0800

    Try reporting flaky tests to issues

--- packages/e2e-tests/jest.config.js
+++ packages/e2e-tests/jest.config.js
@@ -1,5 +1,10 @@
+/**
+ * WordPress dependencies
+ */
+const baseConfig = require( '@wordpress/scripts/config/jest-e2e.config' );
+
 module.exports = {
-	...require( '@wordpress/scripts/config/jest-e2e.config' ),
+	...baseConfig,
 	setupFiles: [ '<rootDir>/config/gutenberg-phase.js' ],
 	setupFilesAfterEnv: [
 		'<rootDir>/config/setup-test-framework.js',
@@ -12,4 +17,10 @@ module.exports = {
 		'/node_modules/',
 		'e2e-tests/specs/performance/',
 	],
+	reporters: [
+		...baseConfig.reporters,
+		process.env.CI &&
+			process.env.GITHUB_EVENT_NAME !== 'pull_request' &&
+			'<rootDir>/config/flaky-tests-reporter.js',
+	].filter( Boolean ),
 };
