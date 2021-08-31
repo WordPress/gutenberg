fa0234378a4dbdc697fab0f565ad3c8d3b854037
Ella van Durpe <ella@vandurpe.com>
2021-08-31 13:55:54 +0300

    Components: simplify Modal with hooks

--- packages/components/src/modal/frame.js
+++ packages/components/src/modal/frame.js
@@ -7,32 +7,32 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 
-import { Component } from '@wordpress/element';
+import { forwardRef } from '@wordpress/element';
 import { ESCAPE } from '@wordpress/keycodes';
 import {
 	useFocusReturn,
 	useFocusOnMount,
+	__experimentalUseFocusOutside as useFocusOutside,
 	useConstrainedTabbing,
 	useMergeRefs,
 } from '@wordpress/compose';
 
-/**
- * Internal dependencies
- */
-import withFocusOutside from '../higher-order/with-focus-outside';
-
-function ModalFrameContent( {
-	overlayClassName,
-	contentLabel,
-	aria: { describedby, labelledby },
-	children,
-	className,
-	role,
-	style,
-	focusOnMount,
-	shouldCloseOnEsc,
-	onRequestClose,
-} ) {
+function ModalFrame(
+	{
+		overlayClassName,
+		contentLabel,
+		aria: { describedby, labelledby },
+		children,
+		className,
+		role = 'dialog',
+		style,
+		focusOnMount = true,
+		shouldCloseOnEsc = true,
+		shouldCloseOnClickOutside = true,
+		onRequestClose,
+	},
+	ref
+) {
 	function handleEscapeKeyDown( event ) {
 		if (
 			shouldCloseOnEsc &&
@@ -48,10 +48,12 @@ function ModalFrameContent( {
 	const focusOnMountRef = useFocusOnMount( focusOnMount );
 	const constrainedTabbingRef = useConstrainedTabbing();
 	const focusReturnRef = useFocusReturn();
+	const focusOutsideProps = useFocusOutside( onRequestClose );
 
 	return (
 		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
 		<div
+			ref={ ref }
 			className={ classnames(
 				'components-modal__screen-overlay',
 				overlayClassName
@@ -71,6 +73,7 @@ function ModalFrameContent( {
 				aria-labelledby={ contentLabel ? null : labelledby }
 				aria-describedby={ describedby }
 				tabIndex="-1"
+				{ ...( shouldCloseOnClickOutside ? focusOutsideProps : {} ) }
 			>
 				{ children }
 			</div>
@@ -78,34 +81,4 @@ function ModalFrameContent( {
 	);
 }
 
-class ModalFrame extends Component {
-	constructor() {
-		super( ...arguments );
-		this.handleFocusOutside = this.handleFocusOutside.bind( this );
-	}
-
-	/**
-	 * Callback function called when clicked outside the modal.
-	 *
-	 * @param {Object} event Mouse click event.
-	 */
-	handleFocusOutside( event ) {
-		if (
-			this.props.shouldCloseOnClickOutside &&
-			this.props.onRequestClose
-		) {
-			this.props.onRequestClose( event );
-		}
-	}
-
-	/**
-	 * Renders the modal frame element.
-	 *
-	 * @return {WPElement} The modal frame element.
-	 */
-	render() {
-		return <ModalFrameContent { ...this.props } />;
-	}
-}
-
-export default withFocusOutside( ModalFrame );
+export default forwardRef( ModalFrame );
--- packages/components/src/modal/index.js
+++ packages/components/src/modal/index.js
@@ -1,8 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { Component, createPortal } from '@wordpress/element';
-import { withInstanceId } from '@wordpress/compose';
+import { createPortal, useEffect, useRef } from '@wordpress/element';
+import { useInstanceId } from '@wordpress/compose';
 import deprecated from '@wordpress/deprecated';
 
 /**
@@ -13,161 +13,79 @@ import ModalHeader from './header';
 import * as ariaHelper from './aria-helper';
 
 // Used to count the number of open modals.
-let parentElement,
-	openModalCount = 0;
+let openModalCount = 0;
 
-class Modal extends Component {
-	constructor( props ) {
-		super( props );
-		this.prepareDOM();
-	}
-
-	/**
-	 * Appends the modal's node to the DOM, so the portal can render the
-	 * modal in it. Also calls the openFirstModal when this is the first modal to be
-	 * opened.
-	 */
-	componentDidMount() {
-		openModalCount++;
-
-		if ( openModalCount === 1 ) {
-			this.openFirstModal();
-		}
-	}
-
-	/**
-	 * Removes the modal's node from the DOM. Also calls closeLastModal when this is
-	 * the last modal to be closed.
-	 */
-	componentWillUnmount() {
-		openModalCount--;
-
-		if ( openModalCount === 0 ) {
-			this.closeLastModal();
-		}
-
-		this.cleanDOM();
-	}
-
-	/**
-	 * Prepares the DOM for the modals to be rendered.
-	 *
-	 * Every modal is mounted in a separate div appended to a parent div
-	 * that is appended to the document body.
-	 *
-	 * The parent div will be created if it does not yet exist, and the
-	 * separate div for this specific modal will be appended to that.
-	 */
-	prepareDOM() {
-		if ( ! parentElement ) {
-			parentElement = document.createElement( 'div' );
-			document.body.appendChild( parentElement );
-		}
-		this.node = document.createElement( 'div' );
-		parentElement.appendChild( this.node );
-	}
-
-	/**
-	 * Removes the specific mounting point for this modal from the DOM.
-	 */
-	cleanDOM() {
-		parentElement.removeChild( this.node );
-	}
-
-	/**
-	 * Prepares the DOM for this modal and any additional modal to be mounted.
-	 *
-	 * It appends an additional div to the body for the modals to be rendered in,
-	 * it hides any other elements from screen-readers and adds an additional class
-	 * to the body to prevent scrolling while the modal is open.
-	 */
-	openFirstModal() {
-		ariaHelper.hideApp( parentElement );
-		document.body.classList.add( this.props.bodyOpenClassName );
-	}
-
-	/**
-	 * Cleans up the DOM after the last modal is closed and makes the app available
-	 * for screen-readers again.
-	 */
-	closeLastModal() {
-		document.body.classList.remove( this.props.bodyOpenClassName );
-		ariaHelper.showApp();
-	}
-
-	/**
-	 * Renders the modal.
-	 *
-	 * @return {WPElement} The modal element.
-	 */
-	render() {
-		const {
-			onRequestClose,
-			title,
-			icon,
-			closeButtonLabel,
-			children,
-			aria,
-			instanceId,
-			isDismissible,
-			isDismissable, //Deprecated
-			// Many of the documented props for Modal are passed straight through
-			// to the ModalFrame component and handled there.
-			...otherProps
-		} = this.props;
-
-		const headingId = title
-			? `components-modal-header-${ instanceId }`
-			: aria.labelledby;
-
-		if ( isDismissable ) {
-			deprecated( 'isDismissable prop of the Modal component', {
-				since: '5.4',
-				alternative:
-					'isDismissible prop (renamed) of the Modal component',
-			} );
-		}
-		// Disable reason: this stops mouse events from triggering tooltips and
-		// other elements underneath the modal overlay.
-		return createPortal(
-			<ModalFrame
-				onRequestClose={ onRequestClose }
-				aria={ {
-					labelledby: headingId,
-					describedby: aria.describedby,
-				} }
-				{ ...otherProps }
-			>
-				<div className={ 'components-modal__content' } role="document">
-					<ModalHeader
-						closeLabel={ closeButtonLabel }
-						headingId={ title && headingId }
-						icon={ icon }
-						isDismissible={ isDismissible || isDismissable }
-						onClose={ onRequestClose }
-						title={ title }
-					/>
-					{ children }
-				</div>
-			</ModalFrame>,
-			this.node
-		);
-	}
-}
-
-Modal.defaultProps = {
-	bodyOpenClassName: 'modal-open',
-	role: 'dialog',
-	title: null,
-	focusOnMount: true,
-	shouldCloseOnEsc: true,
-	shouldCloseOnClickOutside: true,
-	isDismissible: true,
+export default function Modal( {
+	bodyOpenClassName = 'modal-open',
+	title = null,
+	isDismissible = true,
+	isDismissable, // Deprecated
 	/* accessibility */
-	aria: {
+	aria = {
 		labelledby: null,
 		describedby: null,
 	},
-};
+	onRequestClose,
+	icon,
+	closeButtonLabel,
+	children,
+	...otherProps
+} ) {
+	const instanceId = useInstanceId( Modal );
+	const ref = useRef();
 
-export default withInstanceId( Modal );
+	useEffect( () => {
+		openModalCount++;
+
+		if ( openModalCount === 1 ) {
+			ariaHelper.hideApp( ref.current );
+			document.body.classList.add( bodyOpenClassName );
+		}
+
+		return () => {
+			openModalCount--;
+
+			if ( openModalCount === 0 ) {
+				document.body.classList.remove( bodyOpenClassName );
+				ariaHelper.showApp();
+			}
+		};
+	}, [] );
+
+	const headingId = title
+		? `components-modal-header-${ instanceId }`
+		: aria.labelledby;
+
+	if ( isDismissable ) {
+		deprecated( 'isDismissable prop of the Modal component', {
+			since: '5.4',
+			alternative: 'isDismissible prop (renamed) of the Modal component',
+		} );
+	}
+	// Disable reason: this stops mouse events from triggering tooltips and
+	// other elements underneath the modal overlay.
+	return createPortal(
+		<ModalFrame
+			ref={ ref }
+			onRequestClose={ onRequestClose }
+			aria={ {
+				labelledby: headingId,
+				describedby: aria.describedby,
+			} }
+			{ ...otherProps }
+		>
+			<div className={ 'components-modal__content' } role="document">
+				<ModalHeader
+					closeLabel={ closeButtonLabel }
+					headingId={ title && headingId }
+					icon={ icon }
+					isDismissible={ isDismissible || isDismissable }
+					onClose={ onRequestClose }
+					title={ title }
+				/>
+				{ children }
+			</div>
+		</ModalFrame>,
+		document.body
+	);
+}
