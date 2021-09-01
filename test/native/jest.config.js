4ee913874f7dfcf3db401700606109697700b740
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-09-01 10:20:12 -0500

    Fix React Native warning caused by webpack-oriented Jest image mock
    The previous image mock was implemented following the Jest documentation
    related to webpack. However, the `source` prop for React Native's Image
    component does not accept strings. The previous image mock resulted in
    the following warning in testing logs.
    
    ```
    Warning: Failed prop type: Invalid prop `source` supplied to `Image`.
    ```
    
    To address this, the previous image mock was removed in favor of relying
    upon the transform that is provided by the `react-native` Jest preset.
    In order to have this transform applied to images within React
    Navigation, additional regex was added to `transformIgnorePatterns`.
    
    - https://git.io/JEFo0
    - https://bit.ly/3jAqaJL
    - https://bit.ly/2YiuDbS
    - https://git.io/JEFor
    - https://git.io/JEFoK

--- test/native/jest.config.js
+++ test/native/jest.config.js
@@ -50,7 +50,7 @@ module.exports = {
 	moduleNameMapper: {
 		// Mock the CSS modules. See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
 		'\\.(scss)$': '<rootDir>/' + configPath + '/__mocks__/styleMock.js',
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+		'\\.(eot|otf|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/' + configPath + '/__mocks__/fileMock.js',
 		[ `@wordpress\\/(${ transpiledPackageNames.join(
 			'|'
@@ -69,7 +69,7 @@ module.exports = {
 		// See: https://github.com/wordpress-mobile/gutenberg-mobile/pull/257#discussion_r234978268
 		// There is no overloading in jest so we need to rewrite the config from react-native-jest-preset:
 		// https://github.com/facebook/react-native/blob/HEAD/jest-preset.json#L20
-		'node_modules/(?!(simple-html-tokenizer|(jest-)?react-native|@react-native|react-clone-referenced-element))',
+		'node_modules/(?!(simple-html-tokenizer|(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation))',
 	],
 	snapshotSerializers: [
 		'enzyme-to-json/serializer',
