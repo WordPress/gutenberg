7df7f747af62f78b2e52560b2e6e3ecb5e205bf4
ntsekouras <ntsekouras@outlook.com>
2021-09-01 20:44:43 +0300

    Fix linting error in trunk

--- packages/core-data/src/actions.js
+++ packages/core-data/src/actions.js
@@ -7,7 +7,6 @@ import { v4 as uuid } from 'uuid';
 /**
  * WordPress dependencies
  */
-import { controls } from '@wordpress/data';
 import { __unstableAwaitPromise } from '@wordpress/data-controls';
 import triggerFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
