e827a17339e6624d344796ba1b81692ed1317ab7
ramonjd <ramonjd@gmail.com>
2021-09-01 11:51:26 +1000

    Initial commit

---
+++ packages/block-editor/src/components/block-styles/block-styles-preview-panel.js
@@ -0,0 +1,47 @@
+/**
+ * WordPress dependencies
+ */
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import BlockPreview from '../block-preview';
+import { getActiveStyle, replaceActiveStyle } from './utils';
+
+export default function BlockStylesPreviewPanel( {
+	genericPreviewBlock,
+	viewportWidth,
+	style,
+	className,
+	activeStyle,
+} ) {
+	const styleClassName = replaceActiveStyle(
+		className,
+		activeStyle,
+		style
+	);
+	const previewBlocks = useMemo( () => {
+		return {
+			...genericPreviewBlock,
+			attributes: {
+				...genericPreviewBlock.attributes,
+				className: styleClassName + ' block-editor-block-styles__block-preview-container',
+			},
+		};
+	}, [ genericPreviewBlock, styleClassName ] );
+
+	return (
+		<div className="block-editor-block-styles__preview-container">
+			<div className="block-editor-block-styles__preview-content">
+				<BlockPreview
+					viewportWidth={ viewportWidth }
+					blocks={ previewBlocks }
+				/>
+			</div>
+			<div className="block-editor-block-styles__preview-panel-label">
+				{ style.label || style.name }
+			</div>
+		</div>
+	);
+}
