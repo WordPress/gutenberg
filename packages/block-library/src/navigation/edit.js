f1c9696b7ab105a6a2a06463179998d0bfeed7b7
tellthemachines <isabel@tellthemachines.com>
2021-09-03 13:52:36 +1000

    Fix button styles and change toggle label.

--- packages/block-library/src/navigation/edit.js
+++ packages/block-library/src/navigation/edit.js
@@ -266,9 +266,7 @@ function Navigation( {
 									openSubmenusOnClick: value,
 								} );
 							} }
-							label={ __(
-								'Open submenus on click instead of hover'
-							) }
+							label={ __( 'Open submenus on click' ) }
 						/>
 						<ToggleControl
 							checked={ attributes.isResponsive }
