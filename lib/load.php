6c78d068a6864b0e9c0e94825faad3d07e900b26
Riad Benguella <benguella@gmail.com>
2021-09-06 12:41:52 +0100

    Remove more code already in WP 5.7

--- lib/load.php
+++ lib/load.php
@@ -118,12 +118,9 @@ require __DIR__ . '/experiments-page.php';
 require __DIR__ . '/global-styles.php';
 require __DIR__ . '/pwa.php';
 
-require __DIR__ . '/block-supports/generated-classname.php';
 require __DIR__ . '/block-supports/elements.php';
 require __DIR__ . '/block-supports/colors.php';
-require __DIR__ . '/block-supports/align.php';
 require __DIR__ . '/block-supports/typography.php';
-require __DIR__ . '/block-supports/custom-classname.php';
 require __DIR__ . '/block-supports/border.php';
 require __DIR__ . '/block-supports/layout.php';
 require __DIR__ . '/block-supports/spacing.php';
