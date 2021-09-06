0f00012e0c6c70fcfba28a6c69f0a74fd1378075
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-09-06 20:40:47 +0200

    Fix code style.

--- packages/edit-navigation/src/components/sidebar/save-button.js
+++ packages/edit-navigation/src/components/sidebar/save-button.js
@@ -8,7 +8,7 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 
-export default function SaveButton( ) {
+export default function SaveButton() {
 	return (
 		<Button
 			className="edit-navigation-toolbar__save-button"
