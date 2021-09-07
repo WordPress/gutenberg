5b2f0a95de5cc6795ce854ad4b6d5569c9eeafe7
André <583546+oandregal@users.noreply.github.com>
2021-09-07 10:34:03 +0200

    Remove duplicated useValidAlignment hook (#34593)

--- packages/block-editor/src/hooks/align.js
+++ packages/block-editor/src/hooks/align.js
@@ -125,9 +125,6 @@ export const withToolbarControls = createHigherOrderComponent(
 			getBlockSupport( blockName, 'align' ),
 			hasBlockSupport( blockName, 'alignWide', true )
 		);
-		const validAlignments = useAvailableAlignments(
-			blockAllowedAlignments
-		);
 
 		const updateAlignment = ( nextAlign ) => {
 			if ( ! nextAlign ) {
@@ -142,12 +139,12 @@ export const withToolbarControls = createHigherOrderComponent(
 
 		return (
 			<>
-				{ validAlignments.length > 0 && (
+				{ blockAllowedAlignments.length > 0 && (
 					<BlockControls group="block" __experimentalExposeToChildren>
 						<BlockAlignmentControl
 							value={ props.attributes.align }
 							onChange={ updateAlignment }
-							controls={ validAlignments }
+							controls={ blockAllowedAlignments }
 						/>
 					</BlockControls>
 				) }
