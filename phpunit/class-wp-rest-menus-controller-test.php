3cff794d54dd3376a44057f439bd180c8c33552e
Anton Vlasenko <vlasenko.anton@gmail.com>
2021-09-09 23:20:10 +0200

    Fix code style.

--- phpunit/class-wp-rest-menus-controller-test.php
+++ phpunit/class-wp-rest-menus-controller-test.php
@@ -13,11 +13,11 @@
 class WP_REST_Menus_Controller_Test extends WP_REST_Menus_Controller {
 	public function test_it_allows_batch_requests_when_updating_menus() {
 		$rest_server = rest_get_server();
-		// This call is needed to initialize route_options
+		// This call is needed to initialize route_options.
 		$rest_server->get_routes();
-		$route_options = $rest_server->get_route_options('/__experimental/menus/(?P<id>[\d]+)');
+		$route_options = $rest_server->get_route_options( '/__experimental/menus/(?P<id>[\d]+)' );
 
 		$this->assertArrayHasKey( 'allow_batch', $route_options );
-		$this->assertSame(array ('v1' => true,), $route_options['allow_batch']);
+		$this->assertSame( array( 'v1' => true ), $route_options['allow_batch'] );
 	}
 }
