720fca4324b9947893b195ca092e063544647da9
Daniel Richards <daniel.richards@automattic.com>
2021-09-09 13:24:12 +0800

    Update rendering logic for appender

--- packages/edit-navigation/src/filters/add-navigation-editor-custom-appender.js
+++ packages/edit-navigation/src/filters/add-navigation-editor-custom-appender.js
@@ -1,12 +1,59 @@
 /**
  * WordPress dependencies
  */
+import { useSelect } from '@wordpress/data';
 import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { InnerBlocks } from '@wordpress/block-editor';
+import {
+	InnerBlocks,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
 
 function CustomAppender() {
 	return <InnerBlocks.ButtonBlockAppender isToggle />;
+}
+
+function EnhancedNavigationBlock( { blockEdit: BlockEdit, ...props } ) {
+	const clientId = props.clientId;
+	const {
+		noBlockSelected,
+		isSelected,
+		isImmediateParentOfSelectedBlock,
+		selectedBlockHasDescendants,
+	} = useSelect(
+		( select ) => {
+			const {
+				getClientIdsOfDescendants,
+				hasSelectedInnerBlock,
+				getSelectedBlockClientId,
+			} = select( blockEditorStore );
+			const _isImmediateParentOfSelectedBlock = hasSelectedInnerBlock(
+				clientId,
+				false
+			);
+			const selectedBlockId = getSelectedBlockClientId();
+			const _selectedBlockHasDescendants = !! getClientIdsOfDescendants( [
+				selectedBlockId,
+			] )?.length;
+
+			return {
+				isSelected: selectedBlockId === clientId,
+				noBlockSelected: ! selectedBlockId,
+				isImmediateParentOfSelectedBlock: _isImmediateParentOfSelectedBlock,
+				selectedBlockHasDescendants: _selectedBlockHasDescendants,
+			};
+		},
+		[ clientId ]
+	);
+
+	const customAppender =
+		noBlockSelected ||
+		isSelected ||
+		( isImmediateParentOfSelectedBlock && ! selectedBlockHasDescendants )
+			? CustomAppender
+			: false;
+
+	return <BlockEdit { ...props } customAppender={ customAppender } />;
 }
 
 const addNavigationEditorCustomAppender = createHigherOrderComponent(
@@ -15,7 +62,8 @@ const addNavigationEditorCustomAppender = createHigherOrderComponent(
 			return <BlockEdit { ...props } />;
 		}
 
-		return <BlockEdit { ...props } customAppender={ CustomAppender } />;
+		// Use a separate component so that `useSelect` only run on the navigation block.
+		return <EnhancedNavigationBlock blockEdit={ BlockEdit } { ...props } />;
 	},
 	'withNavigationEditorCustomAppender'
 );
