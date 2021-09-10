e10e3402bff93c360230c7fb9e6e46f50eaec145
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2021-09-10 12:00:08 +1000

    Font Appearance Control: Fix selectedItem downshift uncontrolled prop warning

--- packages/block-editor/src/components/font-appearance-control/index.js
+++ packages/block-editor/src/components/font-appearance-control/index.js
@@ -144,11 +144,12 @@ export default function FontAppearanceControl( props ) {
 	}, [ props.options ] );
 
 	// Find current selection by comparing font style & weight against options.
-	const currentSelection = selectOptions.find(
-		( option ) =>
-			option.style.fontStyle === fontStyle &&
-			option.style.fontWeight === fontWeight
-	);
+	const currentSelection =
+		selectOptions.find(
+			( option ) =>
+				option.style.fontStyle === fontStyle &&
+				option.style.fontWeight === fontWeight
+		) || selectOptions[ 0 ];
 
 	// Adjusts field label in case either styles or weights are disabled.
 	const getLabel = () => {
