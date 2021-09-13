c71a933f04d2a234a751d201f19ff4b4d5ee5561
André <583546+oandregal@users.noreply.github.com>
2021-09-13 15:01:44 +0200

    Fix for link color in containers (#34689)
    * Remove important rule
    
    * Contain specificity of links rule using :where so themes can override it
    
    * Output styles before the element
    
    The reason for this is nested content such as:
    
        <group class="wp-element-1">
          <p>Text with <link> and colors inherited from group</p>
          <p class="wp-element-2">Text with <link> and own colors</p>
        </group>
    
    The selectors for the link are
    
    - ".wp-element 1 a" => generated by the group
    - ".wp-element-2 a" => generated by the paragraph
    
    because they have the same specificity the latest will win.
    We need the styles to respect the DOM order of the elements
    that have them.
    
        <style .wp-element-1>
        <style .wp-element-2>
        <group ...>...</group>

--- lib/block-supports/elements.php
+++ lib/block-supports/elements.php
@@ -38,7 +38,7 @@ function gutenberg_render_elements_support( $block_content, $block ) {
 	}
 	$link_color_declaration = esc_html( safecss_filter_attr( "color: $link_color" ) );
 
-	$style = "<style>.$class_name a{" . $link_color_declaration . " !important;}</style>\n";
+	$style = "<style>.$class_name a{" . $link_color_declaration . ";}</style>\n";
 
 	// Like the layout hook this assumes the hook only applies to blocks with a single wrapper.
 	// Retrieve the opening tag of the first HTML element.
@@ -59,7 +59,7 @@ function gutenberg_render_elements_support( $block_content, $block ) {
 		$first_element_offset = $html_element_matches[0][1];
 		$content              = substr_replace( $block_content, ' class="' . $class_name . '"', $first_element_offset + strlen( $first_element ) - 1, 0 );
 	}
-	return $content . $style;
+	return $style . $content;
 
 }
 
