468dab69f28deda788fa9f4d13ea1ac86fc089d2
Grant Kinney <hi@grant.mk>
2021-09-14 13:55:43 -0500

    Revert text decoration changes

--- packages/block-editor/src/components/text-decoration-control/index.js
+++ packages/block-editor/src/components/text-decoration-control/index.js
@@ -2,19 +2,10 @@
  * WordPress dependencies
  */
 import { Button } from '@wordpress/components';
-import {
-	formatStrikethrough,
-	formatUnderline,
-	textColor,
-} from '@wordpress/icons';
+import { formatStrikethrough, formatUnderline } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
 const TEXT_DECORATIONS = [
-	{
-		name: __( 'None' ),
-		value: 'none',
-		icon: textColor,
-	},
 	{
 		name: __( 'Underline' ),
 		value: 'underline',
