a320cf55ecd62172ac74bf1a7ce27a26f6816dd5
Ella van Durpe <ella@vandurpe.com>
2021-09-14 16:14:01 +0300

    RichText: deprecate RichTextShortcut in favour of useShortcut

--- packages/format-library/src/bold/index.js
+++ packages/format-library/src/bold/index.js
@@ -5,10 +5,15 @@ import { __ } from '@wordpress/i18n';
 import { toggleFormat } from '@wordpress/rich-text';
 import {
 	RichTextToolbarButton,
-	RichTextShortcut,
 	__unstableRichTextInputEvent,
 } from '@wordpress/block-editor';
 import { formatBold } from '@wordpress/icons';
+import { useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import {
+	store as keyboardShortcutsStore,
+	useShortcut,
+} from '@wordpress/keyboard-shortcuts';
 
 const name = 'core/bold';
 const title = __( 'Bold' );
@@ -18,7 +23,7 @@ export const bold = {
 	title,
 	tagName: 'strong',
 	className: null,
-	edit( { isActive, value, onChange, onFocus } ) {
+	edit: function Edit( { isActive, value, onChange, onFocus } ) {
 		function onToggle() {
 			onChange( toggleFormat( value, { type: name } ) );
 		}
@@ -28,13 +33,26 @@ export const bold = {
 			onFocus();
 		}
 
+		const { registerShortcut } = useDispatch( keyboardShortcutsStore );
+
+		useShortcut( name, ( event ) => {
+			onToggle();
+			event.preventDefault();
+		} );
+		useEffect( () => {
+			registerShortcut( {
+				name,
+				category: 'text',
+				description: __( 'Make the selected text bold.' ),
+				keyCombination: {
+					modifier: 'primary',
+					character: 'b',
+				},
+			} );
+		}, [ registerShortcut ] );
+
 		return (
 			<>
-				<RichTextShortcut
-					type="primary"
-					character="b"
-					onUse={ onToggle }
-				/>
 				<RichTextToolbarButton
 					name="bold"
 					icon={ formatBold }
--- packages/format-library/src/italic/index.js
+++ packages/format-library/src/italic/index.js
@@ -5,10 +5,15 @@ import { __ } from '@wordpress/i18n';
 import { toggleFormat } from '@wordpress/rich-text';
 import {
 	RichTextToolbarButton,
-	RichTextShortcut,
 	__unstableRichTextInputEvent,
 } from '@wordpress/block-editor';
 import { formatItalic } from '@wordpress/icons';
+import { useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import {
+	store as keyboardShortcutsStore,
+	useShortcut,
+} from '@wordpress/keyboard-shortcuts';
 
 const name = 'core/italic';
 const title = __( 'Italic' );
@@ -18,7 +23,7 @@ export const italic = {
 	title,
 	tagName: 'em',
 	className: null,
-	edit( { isActive, value, onChange, onFocus } ) {
+	edit: function Edit( { isActive, value, onChange, onFocus } ) {
 		function onToggle() {
 			onChange( toggleFormat( value, { type: name } ) );
 		}
@@ -28,13 +33,26 @@ export const italic = {
 			onFocus();
 		}
 
+		const { registerShortcut } = useDispatch( keyboardShortcutsStore );
+
+		useShortcut( name, ( event ) => {
+			onToggle();
+			event.preventDefault();
+		} );
+		useEffect( () => {
+			registerShortcut( {
+				name,
+				category: 'text',
+				description: __( 'Make the selected text italic.' ),
+				keyCombination: {
+					modifier: 'primary',
+					character: 'i',
+				},
+			} );
+		}, [ registerShortcut ] );
+
 		return (
 			<>
-				<RichTextShortcut
-					type="primary"
-					character="i"
-					onUse={ onToggle }
-				/>
 				<RichTextToolbarButton
 					name="italic"
 					icon={ formatItalic }
