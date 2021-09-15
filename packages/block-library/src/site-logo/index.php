9e88ec2dfdea7ad2d898c1474cb93e0c0eb3db92
Staci Cooper <staci.cooper@automattic.com>
2021-09-15 16:16:12 -0700

    Delete custom logo when site logo is removed

--- packages/block-library/src/site-logo/index.php
+++ packages/block-library/src/site-logo/index.php
@@ -160,3 +160,25 @@ function _delete_site_logo_on_remove_custom_logo_on_setup_theme() {
 	add_action( "delete_option_theme_mods_$theme", '_delete_site_logo_on_remove_theme_mods' );
 }
 add_action( 'setup_theme', '_delete_site_logo_on_remove_custom_logo_on_setup_theme', 11 );
+
+/**
+ * Removes the custom_logo theme-mod when the site_logo option gets deleted.
+ *
+ * @param mixed $old_value The old option value.
+ * @param mixed $value     The new option value.
+ *
+ * @return void
+ */
+function _delete_custom_logo_on_remove_site_logo() {
+	// Unhook update and delete actions for custom_logo to prevent a loop of hooks.
+	remove_action( "update_option_theme_mods_$theme", '_delete_site_logo_on_remove_custom_logo', 10 );
+	remove_action( "delete_option_theme_mods_$theme", '_delete_site_logo_on_remove_theme_mods' );
+
+	// Remove the custom logo
+	remove_theme_mod( 'custom_logo' );
+
+	// Restore update and delete actions.
+	add_action( "update_option_theme_mods_$theme", '_delete_site_logo_on_remove_custom_logo', 10, 2 );
+	add_action( "delete_option_theme_mods_$theme", '_delete_site_logo_on_remove_theme_mods' );
+}
+add_action( 'delete_option_site_logo', '_delete_custom_logo_on_remove_site_logo' );
