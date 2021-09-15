0cbf3672f340dcfc72577c14cc72b1c77b37bf1a
Jesús Amieiro <amieiro@gmail.com>
2021-09-15 13:02:08 +0200

    Add a comment in the core class used to implement a Block widget
    See https://core.trac.wordpress.org/ticket/54110

--- lib/class-wp-widget-block.php
+++ lib/class-wp-widget-block.php
@@ -197,6 +197,7 @@ class WP_Widget_Block extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, $this->default_instance );
 		?>
 		<p>
+		/* translators: HTML code of the block, not an option that blocks HTML */
 		<label for="<?php echo $this->get_field_id( 'content' ); ?>"><?php echo __( 'Block HTML:', 'gutenberg' ); ?></label>
 		<textarea id="<?php echo $this->get_field_id( 'content' ); ?>" name="<?php echo $this->get_field_name( 'content' ); ?>" rows="6" cols="50" class="widefat code"><?php echo esc_textarea( $instance['content'] ); ?></textarea>
 		</p>
