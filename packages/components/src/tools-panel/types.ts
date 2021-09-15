7e7984766579abfe1cbe1aa9329f3861f7c9f9d8
Glen Davies <glen.davies@a8c.com>
2021-09-15 15:39:04 +1200

    Start refactor of ToolsPanel component to typescript

---
+++ packages/components/src/tools-panel/types.ts
@@ -0,0 +1,28 @@
+/**
+ * External dependencies
+ */
+// eslint-disable-next-line no-restricted-imports
+import type { ReactNode, Ref } from 'react';
+
+export type toolsPanelforwardRef = Ref< any >;
+
+export interface ToolsPanelProps {
+	label: string;
+	header: string;
+	children: ReactNode;
+	resetAll: () => undefined;
+	className?: string;
+}
+
+export interface ToolsPanelHeaderProps {
+	menuLabel: string;
+	header: string;
+	resetAll: () => undefined;
+	toggleItem: ( label: string ) => undefined;
+}
+
+export interface ToolPanelItem {
+	hasValue: () => boolean;
+	isShownByDefault: boolean;
+	label: string;
+}
