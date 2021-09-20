e46a966ad6fc68526745e1bed685827112b86a92
Thomas Roberts <thomas.roberts@automattic.com>
2021-09-20 16:07:47 +0100

    Add comment case rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
 	rules: {
 		'jest/expect-expect': 'off',
 		'@wordpress/dependency-group': 'error',
+		'@wordpress/comment-case': 'error',
 		'@wordpress/gutenberg-phase': 'error',
 		'@wordpress/react-no-unsafe-timeout': 'error',
 		'@wordpress/i18n-text-domain': [
