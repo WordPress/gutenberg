d3effd38c6b41bf8a7461591d965567930f0b5f7
Carlos Garcia <fluiddot@gmail.com>
2021-09-20 18:50:53 +0200

    Replace withPreferredColorScheme with hook in block icon

--- packages/block-editor/src/components/block-icon/index.native.js
+++ packages/block-editor/src/components/block-icon/index.native.js
@@ -8,38 +8,31 @@ import { View } from 'react-native';
  */
 import { Icon } from '@wordpress/components';
 import { blockDefault } from '@wordpress/icons';
-import { withPreferredColorScheme } from '@wordpress/compose';
+import { usePreferredColorSchemeStyle } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
 import styles from './style.scss';
 
-export function BlockIcon( {
-	icon,
-	fill: fillColor,
-	size,
-	showColors = false,
-	getStylesFromColorScheme,
-} ) {
+export function BlockIcon( { icon, fill, size, showColors = false } ) {
 	if ( icon?.src === 'block-default' ) {
 		icon = {
 			src: blockDefault,
 		};
 	}
 
-	const fill = fillColor || ( showColors && icon?.foreground );
+	const defaultFill = usePreferredColorSchemeStyle(
+		styles.iconPlaceholder,
+		styles.iconPlaceholderDark
+	)?.fill;
+	const iconForeground = showColors ? icon?.foreground : undefined;
 
 	const renderedIcon = (
 		<Icon
 			icon={ icon && icon.src ? icon.src : icon }
-			{ ...( fill && { fill } ) }
+			fill={ fill || iconForeground || defaultFill }
 			{ ...( size && { size } ) }
-			{ ...( ! fill &&
-				getStylesFromColorScheme(
-					styles.iconPlaceholder,
-					styles.iconPlaceholderDark
-				) ) }
 		/>
 	);
 	const style = showColors
@@ -51,4 +44,4 @@ export function BlockIcon( {
 	return <View style={ style }>{ renderedIcon }</View>;
 }
 
-export default withPreferredColorScheme( BlockIcon );
+export default BlockIcon;
