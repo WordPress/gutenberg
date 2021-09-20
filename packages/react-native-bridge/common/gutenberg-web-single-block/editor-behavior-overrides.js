b4debe563d87aa49fb979df0b21ebe9a85c678a3
Siobhan <siobhan@automattic.com>
2021-09-20 10:50:33 +0100

    Add clarifying comment to new JS file
    As this is a somewhat hacky and unintuitive fix, this commit adds a clarifying comment along with a link back to the PR for more details.

--- packages/react-native-bridge/common/gutenberg-web-single-block/editor-behavior-overrides.js
+++ packages/react-native-bridge/common/gutenberg-web-single-block/editor-behavior-overrides.js
@@ -1,3 +1,11 @@
+/*
+This is a hacky fix for a text selection quirk in the UBE. 
+It uses the 'setBaseAndExtent' method to reset the text 
+selection when certain menu items are tapped. This then 
+dismisses the text selection toolbar, preventing it from 
+blocking access to buttons. See PR for further details: 
+https://github.com/WordPress/gutenberg/pull/34668
+*/
 window.addEventListener(
 	'focus',
 	function () {
