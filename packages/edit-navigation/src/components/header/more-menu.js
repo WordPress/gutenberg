1f125439e1cc71dfc0be141c3046d23d49ed9fae
Daniel Richards <daniel.richards@automattic.com>
2021-09-21 16:19:07 +0800

    Add a menu group

--- packages/edit-navigation/src/components/header/more-menu.js
+++ packages/edit-navigation/src/components/header/more-menu.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { MenuItem, VisuallyHidden } from '@wordpress/components';
+import { MenuGroup, MenuItem, VisuallyHidden } from '@wordpress/components';
 import { external } from '@wordpress/icons';
 import { MoreMenuDropdown } from '@wordpress/interface';
 import { __ } from '@wordpress/i18n';
@@ -10,23 +10,25 @@ export default function MoreMenu() {
 	return (
 		<MoreMenuDropdown>
 			{ () => (
-				<MenuItem
-					role="menuitem"
-					icon={ external }
-					href={ __(
-						'https://github.com/WordPress/gutenberg/tree/trunk/packages/edit-navigation/docs/user-documentation.md'
-					) }
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{ __( 'Help' ) }
-					<VisuallyHidden as="span">
-						{
-							/* translators: accessibility text */
-							__( '(opens in a new tab)' )
-						}
-					</VisuallyHidden>
-				</MenuItem>
+				<MenuGroup label={ __( 'Tools' ) }>
+					<MenuItem
+						role="menuitem"
+						icon={ external }
+						href={ __(
+							'https://github.com/WordPress/gutenberg/tree/trunk/packages/edit-navigation/docs/user-documentation.md'
+						) }
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{ __( 'Help' ) }
+						<VisuallyHidden as="span">
+							{
+								/* translators: accessibility text */
+								__( '(opens in a new tab)' )
+							}
+						</VisuallyHidden>
+					</MenuItem>
+				</MenuGroup>
 			) }
 		</MoreMenuDropdown>
 	);
