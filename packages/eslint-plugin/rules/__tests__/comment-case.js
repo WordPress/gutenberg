59986769b4c1e386fcaf83e75a120cabfc5dd95e
Thomas Roberts <thomas.roberts@automattic.com>
2021-09-21 18:30:18 +0100

    Add test cases for rule

--- packages/eslint-plugin/rules/__tests__/comment-case.js
+++ packages/eslint-plugin/rules/__tests__/comment-case.js
@@ -32,6 +32,9 @@ ruleTester.run( 'comment-case', rule, {
 			code: `// @todo type comments don't need to be punctuated`,
 		},
 		{
+			code: `const someVar = 1; // Describe the var.`,
+		},
+		{
 			code: `// My period ending comment
 			// that runs on to the next line.`,
 		},
@@ -47,9 +50,48 @@ ruleTester.run( 'comment-case', rule, {
 	],
 	invalid: [
 		{
+			code: `const someVar = 1; // Describe the var`,
+			output: `const someVar = 1; // Describe the var.`,
+			errors: [ { messageId: 'missingPunctuation' } ],
+		},
+		{
+			code: `const someVar = 1; // no capital letter.`,
+			output: `const someVar = 1; // No capital letter.`,
+			errors: [ { messageId: 'capitalLetter' } ],
+		},
+		{
+			code: `const someVar = 1; // Describe the var
+			const someOtherVar = 2; // Describe this one too`,
+			output: `const someVar = 1; // Describe the var.
+			const someOtherVar = 2; // Describe this one too.`,
+			errors: [
+				{ messageId: 'missingPunctuation' },
+				{ messageId: 'missingPunctuation' },
+			],
+		},
+		{
 			code: `// My comment without a period`,
 			output: `// My comment without a period.`,
 			errors: [ { messageId: 'missingPunctuation' } ],
+		},
+		{
+			code: `//My comment without a space.`,
+			output: `// My comment without a space.`,
+			errors: [ { messageId: 'missingSpace' } ],
+		},
+		{
+			code: `//My multi line
+			//comments without
+			//spaces`,
+			output: `// My multi line
+			// comments without
+			// spaces`, // No period here because this test case is for a single pass.
+			errors: [
+				{ messageId: 'missingSpace' },
+				{ messageId: 'missingSpace' },
+				{ messageId: 'missingSpace' },
+				{ messageId: 'missingPunctuation' },
+			],
 		},
 	],
 } );
