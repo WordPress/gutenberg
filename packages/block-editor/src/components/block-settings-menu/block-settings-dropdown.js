44e165933dfa0e98bccbbf68afb170ca76c7db50
Staci Cooper <staci.cooper@automattic.com>
2021-09-21 15:24:33 -0700

    Remove internal attrs when copying via CopyMenuItem

--- packages/block-editor/src/components/block-settings-menu/block-settings-dropdown.js
+++ packages/block-editor/src/components/block-settings-menu/block-settings-dropdown.js
@@ -11,7 +11,10 @@ import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { moreVertical } from '@wordpress/icons';
 import { Children, cloneElement, useCallback } from '@wordpress/element';
-import { serialize } from '@wordpress/blocks';
+import {
+	serialize,
+	__experimentalRemoveAttributesByRole,
+} from '@wordpress/blocks';
 import { store as keyboardShortcutsStore } from '@wordpress/keyboard-shortcuts';
 import { useCopyToClipboard } from '@wordpress/compose';
 
@@ -33,7 +36,12 @@ const POPOVER_PROPS = {
 };
 
 function CopyMenuItem( { blocks, onCopy } ) {
-	const ref = useCopyToClipboard( () => serialize( blocks ), onCopy );
+	const ref = useCopyToClipboard( () => {
+		blocks = blocks.map( ( block ) =>
+			__experimentalRemoveAttributesByRole( block, 'internal' )
+		);
+		return serialize( blocks );
+	}, onCopy );
 	return <MenuItem ref={ ref }>{ __( 'Copy' ) }</MenuItem>;
 }
 
