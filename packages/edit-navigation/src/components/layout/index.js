c1b10bb7586e80e7764c6ca855dfc02076471ce9
George Mamadashvili <georgemamadashvili@gmail.com>
2021-09-22 14:13:44 +0400

    Remove unused prop from Editor component

--- packages/edit-navigation/src/components/layout/index.js
+++ packages/edit-navigation/src/components/layout/index.js
@@ -172,7 +172,6 @@ export default function Layout( { blockEditorSettings } ) {
 														isPending={
 															! hasLoadedMenus
 														}
-														blocks={ blocks }
 													/>
 												</BlockTools>
 											</div>
