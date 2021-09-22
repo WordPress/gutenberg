d8badf4985b9741cfcbdf4fb7c861242a2a884d6
Riad Benguella <benguella@gmail.com>
2021-09-22 14:24:02 +0100

    Support reduced motion

--- packages/components/src/navigator/screen.js
+++ packages/components/src/navigator/screen.js
@@ -8,6 +8,7 @@ import { motion } from 'framer-motion';
  * WordPress dependencies
  */
 import { useContext } from '@wordpress/element';
+import { useReducedMotion } from '@wordpress/compose';
 
 /**
  * Internal dependencies
@@ -20,43 +21,48 @@ const animationExitDuration = 0.14;
 const animationExitDelay = 0;
 
 function NavigatorScreen( { children, path } ) {
+	const prefersReducedMotion = useReducedMotion();
 	const [ currentPath ] = useContext( NavigatorContext );
 	const isMatch = currentPath.path === path;
 
-	if ( isMatch ) {
-		const animate = {
-			opacity: 1,
-			transition: {
-				delay: animationEnterDelay,
-				duration: animationEnterDuration,
-				ease: 'easeInOut',
-			},
-			x: 0,
-		};
-		const initial = {
-			opacity: 0,
-			x: currentPath.isBack ? -50 : 50,
-		};
-		const exit = {
-			delay: animationExitDelay,
-			opacity: 0,
-			x: currentPath.isBack ? 50 : -50,
-			transition: {
-				duration: animationExitDuration,
-				ease: 'easeInOut',
-			},
-		};
-
-		const animatedProps = {
-			animate,
-			exit,
-			initial,
-		};
-
-		return <motion.div { ...animatedProps }>{ children }</motion.div>;
+	if ( ! isMatch ) {
+		return null;
 	}
 
-	return null;
+	if ( prefersReducedMotion ) {
+		return <div>{ children }</div>;
+	}
+
+	const animate = {
+		opacity: 1,
+		transition: {
+			delay: animationEnterDelay,
+			duration: animationEnterDuration,
+			ease: 'easeInOut',
+		},
+		x: 0,
+	};
+	const initial = {
+		opacity: 0,
+		x: currentPath.isBack ? -50 : 50,
+	};
+	const exit = {
+		delay: animationExitDelay,
+		opacity: 0,
+		x: currentPath.isBack ? 50 : -50,
+		transition: {
+			duration: animationExitDuration,
+			ease: 'easeInOut',
+		},
+	};
+
+	const animatedProps = {
+		animate,
+		exit,
+		initial,
+	};
+
+	return <motion.div { ...animatedProps }>{ children }</motion.div>;
 }
 
 export default NavigatorScreen;
