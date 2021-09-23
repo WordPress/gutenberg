5f6a61ff1a1ed1b1669750e629a0f2934a09362f
Jarda Snajdr <jsnajdr@gmail.com>
2021-09-23 10:26:45 +0200

    block-directory: migrate store actions to thunks (#35031)
    * block-directory: use optional chaining and null coalescing to make selectors nicer
    
    * block-directory: block response does not have assets field and loadAssets doesn't have the param
    
    * block-directory: change mock block item to resemble the real object
    
    * block-directory: migrate store to thunks

--- packages/block-directory/src/store/resolvers.js
+++ packages/block-directory/src/store/resolvers.js
@@ -6,31 +6,29 @@ import { camelCase, mapKeys } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { apiFetch } from '@wordpress/data-controls';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
 import { fetchDownloadableBlocks, receiveDownloadableBlocks } from './actions';
 
-export default {
-	*getDownloadableBlocks( filterValue ) {
-		if ( ! filterValue ) {
-			return;
-		}
+export const getDownloadableBlocks = ( filterValue ) => async ( {
+	dispatch,
+} ) => {
+	if ( ! filterValue ) {
+		return;
+	}
 
-		try {
-			yield fetchDownloadableBlocks( filterValue );
-			const results = yield apiFetch( {
-				path: `wp/v2/block-directory/search?term=${ filterValue }`,
-			} );
-			const blocks = results.map( ( result ) =>
-				mapKeys( result, ( value, key ) => {
-					return camelCase( key );
-				} )
-			);
+	try {
+		dispatch( fetchDownloadableBlocks( filterValue ) );
+		const results = await apiFetch( {
+			path: `wp/v2/block-directory/search?term=${ filterValue }`,
+		} );
+		const blocks = results.map( ( result ) =>
+			mapKeys( result, ( value, key ) => camelCase( key ) )
+		);
 
-			yield receiveDownloadableBlocks( blocks, filterValue );
-		} catch ( error ) {}
-	},
+		dispatch( receiveDownloadableBlocks( blocks, filterValue ) );
+	} catch {}
 };
