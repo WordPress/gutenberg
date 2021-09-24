082e10c0154cd2f4357f16e0fde2be22e379d6aa
André <583546+oandregal@users.noreply.github.com>
2021-09-24 14:40:42 +0200

    Add deprecation for the verse block

--- packages/block-library/src/verse/deprecated.js
+++ packages/block-library/src/verse/deprecated.js
@@ -1,7 +1,42 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { RichText } from '@wordpress/block-editor';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import cleanEmptyObject from '../utils/clean-empty-object';
+
+/**
+ * Migrates the current style.typography.fontFamily attribute,
+ * whose value was "var:preset|font-family|helvetica-arial",
+ * to the style.fontFamily attribute, whose value will be "helvetica-arial".
+ *
+ * @param {Object} attributes The current attributes
+ * @return {Object} The updated attributes.
+ */
+const oldFontFamilyMigration = ( attributes ) => {
+	if ( ! attributes?.style?.typography?.fontFamily ) {
+		return attributes;
+	}
+
+	const fontFamily = attributes.style.typography.fontFamily
+		.split( '|' )
+		.pop();
+	delete attributes.style.typography.fontFamily;
+	attributes.style = cleanEmptyObject( attributes.style );
+
+	return {
+		...attributes,
+		fontFamily,
+	};
+};
 
 const blockAttributes = {
 	content: {
@@ -28,6 +63,52 @@ const deprecated = [
 					value={ content }
 				/>
 			);
+		},
+	},
+	{
+		attributes: {
+			content: {
+				type: 'string',
+				source: 'html',
+				selector: 'pre',
+				default: '',
+				__unstablePreserveWhiteSpace: true,
+				__experimentalRole: 'content',
+			},
+			textAlign: {
+				type: 'string',
+			},
+		},
+		supports: {
+			anchor: true,
+			color: {
+				gradients: true,
+				link: true,
+			},
+			typography: {
+				fontSize: true,
+				__experimentalFontFamily: true,
+			},
+			spacing: {
+				padding: true,
+			},
+		},
+		save( { attributes } ) {
+			const { textAlign, content } = attributes;
+
+			const className = classnames( {
+				[ `has-text-align-${ textAlign }` ]: textAlign,
+			} );
+
+			return (
+				<pre { ...useBlockProps.save( { className } ) }>
+					<RichText.Content value={ content } />
+				</pre>
+			);
+		},
+		migrate: oldFontFamilyMigration,
+		isEligible( { style } ) {
+			return style?.typography?.fontFamily;
 		},
 	},
 ];
