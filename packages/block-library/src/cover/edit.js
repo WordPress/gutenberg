538778b08a17717267f517513caf74e7202e87ec
Glen Davies <glen.davies@a8c.com>
2021-09-24 13:59:28 +1200

    Don't reset dimRatio when image removed

--- packages/block-library/src/cover/edit.js
+++ packages/block-library/src/cover/edit.js
@@ -529,7 +529,6 @@ function CoverEdit( {
 										url: undefined,
 										id: undefined,
 										backgroundType: undefined,
-										dimRatio: undefined,
 										focalPoint: undefined,
 										hasParallax: undefined,
 										isRepeated: undefined,
