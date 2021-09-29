b0cb7fafbb64dd76ddf6664c0602755b489bec0f
Marco Ciampini <marco.ciampo@gmail.com>
2021-09-29 16:55:50 +0200

    `NavigatorProvider`: connect to the Context System, wrap in `View`

--- packages/components/src/navigator/navigator-provider/component.tsx
+++ packages/components/src/navigator/navigator-provider/component.tsx
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+// eslint-disable-next-line no-restricted-imports
+import type { Ref } from 'react';
+
+/**
  * WordPress dependencies
  */
 import { useState } from '@wordpress/element';
@@ -6,24 +12,40 @@ import { useState } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import {
+	contextConnect,
+	useContextSystem,
+	WordPressComponentProps,
+} from '../../ui/context';
+import { View } from '../../view';
 import { NavigatorContext } from '../context';
 import type { NavigatorProviderProps, NavigatorPath } from '../types';
 
-function NavigatorProvider( {
-	initialPath,
-	children,
-}: NavigatorProviderProps ) {
+function NavigatorProvider(
+	props: WordPressComponentProps< NavigatorProviderProps, 'div' >,
+	forwardedRef: Ref< any >
+) {
+	const { initialPath, children, ...otherProps } = useContextSystem(
+		props,
+		'NavigatorProvider'
+	);
+
 	const [ path, setPath ] = useState< NavigatorPath >( {
 		path: initialPath,
 	} );
 
 	return (
-		<NavigatorContext.Provider value={ [ path, setPath ] }>
-			{ children }
-		</NavigatorContext.Provider>
+		<View ref={ forwardedRef } { ...otherProps }>
+			<NavigatorContext.Provider value={ [ path, setPath ] }>
+				{ children }
+			</NavigatorContext.Provider>
+		</View>
 	);
 }
 
-// TODO: context connect
+const ConnectedNavigatorProvider = contextConnect(
+	NavigatorProvider,
+	'NavigatorProvider'
+);
 
-export default NavigatorProvider;
+export default ConnectedNavigatorProvider;
