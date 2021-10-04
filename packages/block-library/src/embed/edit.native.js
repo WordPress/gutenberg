601282c7ef806caccaa79ceae7f91d6c63120bed
Carlos Garcia <fluiddot@gmail.com>
2021-10-04 13:33:25 +0200

    Add missing comment line in embed edit

--- packages/block-library/src/embed/edit.native.js
+++ packages/block-library/src/embed/edit.native.js
@@ -225,6 +225,7 @@ const EmbedEdit = ( props ) => {
 	// after the preview has been rendered can result in unwanted
 	// clipping or scrollbars. The `getAttributesFromPreview` function
 	// that `getMergedAttributes` uses is memoized so that we're not
+	// calculating them on every render.
 	const {
 		type,
 		allowResponsive,
