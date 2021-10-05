1f75f8f6f500a20df5b9d6e317b4d72dd5af4ede
Marco Ciampini <marco.ciampo@gmail.com>
2021-10-05 17:13:45 +0200

    `Navigator`: refactor to TypeScript (#35214)

--- packages/components/src/navigator/use-navigator.ts
+++ packages/components/src/navigator/use-navigator.ts
@@ -7,8 +7,12 @@ import { useContext } from '@wordpress/element';
  * Internal dependencies
  */
 import { NavigatorContext } from './context';
+import type { Navigator } from './types';
 
-function useNavigator() {
+/**
+ * Retrieves a `navigator` instance.
+ */
+function useNavigator(): Navigator {
 	const [ , setPath ] = useContext( NavigatorContext );
 
 	return {
