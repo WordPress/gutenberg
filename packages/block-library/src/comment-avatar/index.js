3c63143f95ac6a98e0a7b880e6e65cddea002190
Carlos Bravo <carlos@ticloop.com>
2021-10-06 13:29:34 +0200

    Update icon name

--- packages/block-library/src/comment-avatar/index.js
+++ packages/block-library/src/comment-avatar/index.js
@@ -7,7 +7,7 @@ import edit from './edit';
 /**
  * WordPress dependencies
  */
-import { postCommentsAvatar as icon } from '@wordpress/icons';
+import { commentAvatar as icon } from '@wordpress/icons';
 
 const { name } = metadata;
 export { metadata, name };
