4850fabe2049cbe6e7705eb13d0fc34c5d7b28a4
André <583546+oandregal@users.noreply.github.com>
2021-10-07 12:03:57 +0200

    Pass the setting down

--- lib/global-styles.php
+++ lib/global-styles.php
@@ -102,8 +102,7 @@ function gutenberg_experimental_global_styles_settings( $settings ) {
 		is_callable( 'get_current_screen' ) &&
 		function_exists( 'gutenberg_is_edit_site_page' ) &&
 		gutenberg_is_edit_site_page( get_current_screen()->id ) &&
-		WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() &&
-		gutenberg_supports_block_templates()
+		gutenberg_experimental_is_global_styles_available_to_user()
 	) {
 		$context = 'site-editor';
 	}
@@ -171,6 +170,8 @@ function gutenberg_experimental_global_styles_settings( $settings ) {
 		$settings['styles']                      = $styles_without_existing_global_styles;
 	}
 
+	$settings['isGlobalStylesUIEnabled'] = gutenberg_experimental_is_global_styles_available_to_user();
+
 	// Copied from get_block_editor_settings() at wordpress-develop/block-editor.php.
 	$settings['__experimentalFeatures'] = $consolidated->get_settings();
 
@@ -232,16 +233,24 @@ function gutenberg_experimental_global_styles_settings( $settings ) {
 }
 
 /**
+ * Whether or not the Global Styles sidebar is available to the user
+ * and the user CPT is registered.
+ *
+ * @return boolean
+ */
+function gutenberg_experimental_is_global_styles_available_to_user() {
+	return WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() || gutenberg_supports_block_templates();
+}
+
+/**
  * Register CPT to store/access user data.
  *
  * @return array|undefined
  */
 function gutenberg_experimental_global_styles_register_user_cpt() {
-	if ( ! WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() ) {
-		return;
+	if ( gutenberg_experimental_is_global_styles_available_to_user() ) {
+		WP_Theme_JSON_Resolver_Gutenberg::register_user_custom_post_type();
 	}
-
-	WP_Theme_JSON_Resolver_Gutenberg::register_user_custom_post_type();
 }
 
 /**
