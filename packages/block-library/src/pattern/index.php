3e602b4fc99e17b76522fb6e47c768e22a28b91f
Jeff Ong <jonger4@gmail.com>
2021-10-07 12:24:44 -0400

    Update packages/block-library/src/pattern/index.php
    Co-authored-by: Miguel Fonseca <miguelcsf@gmail.com>

--- packages/block-library/src/pattern/index.php
+++ packages/block-library/src/pattern/index.php
@@ -4,12 +4,12 @@ function register_block_core_pattern() {
 	register_block_type_from_metadata(
 		__DIR__ . '/pattern',
 		array(
-			'render_callback' => 'render_block_pattern',
+			'render_callback' => 'render_block_core_pattern',
 		)
 	);
 }
 
-function render_block_pattern( $attributes ) {
+function render_block_core_pattern( $attributes ) {
 	$slug = $attributes['slug'];
 	if ( class_exists( 'WP_Block_Patterns_Registry' ) && WP_Block_Patterns_Registry::get_instance()->is_registered( $slug ) ) {
 		$pattern = WP_Block_Patterns_Registry::get_instance()->get_registered( $slug );
