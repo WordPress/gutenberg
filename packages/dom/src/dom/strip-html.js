c8faf56c9216afd49b02841dd1dc9d9935b1027a
Dave Smith <getdavemail@gmail.com>
2021-10-07 20:26:00 +0100

    Fix bug in strip html whereby leading spaces were stripped out

--- packages/dom/src/dom/strip-html.js
+++ packages/dom/src/dom/strip-html.js
@@ -6,9 +6,22 @@
  * @return {string} The text content with any html removed.
  */
 export default function stripHTML( html ) {
+	const leadingSpaces = html.match( /(^\s+)/g );
+
+	// DOM Parser will ignore any space character coming after
+	// the DocType.
+	// see: https://html.spec.whatwg.org/multipage/parsing.html#after-doctype-name-state
+	// As a result any leading space in the provided `html`
+	// argument string will be stripped out.
+	// Manually retrieve these prior to parsing for restoration post-parse.
+	const spacesToRestore = leadingSpaces ? leadingSpaces[ 0 ] : '';
+
 	const document = new window.DOMParser().parseFromString(
 		html,
 		'text/html'
 	);
-	return document.body.textContent || '';
+
+	const content = document.body.textContent || '';
+
+	return spacesToRestore + content;
 }
