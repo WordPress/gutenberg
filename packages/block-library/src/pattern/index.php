90619bbe9e01b9b2519c209c588fdde21d875941
Jeff Ong <jonger4@gmail.com>
2021-10-07 15:40:23 -0400

    Fix phpcs errors.

--- packages/block-library/src/pattern/index.php
+++ packages/block-library/src/pattern/index.php
@@ -1,5 +1,15 @@
 <?php
+/**
+ * Server-side rendering of the `core/pattern` block.
+ *
+ * @package WordPress
+ */
 
+/**
+ *  Registers the `core/pattern` block on the server.
+ *
+ * @return void 
+ */
 function register_block_core_pattern() {
 	register_block_type_from_metadata(
 		__DIR__ . '/pattern',
@@ -9,12 +19,18 @@ function register_block_core_pattern() {
 	);
 }
 
+/**
+ * Renders the `core/pattern` block on the server.
+ *
+ * @param array    $attributes Block attributes.
+ *
+ * @return string Returns the output of the pattern.
+ */
 function render_block_core_pattern( $attributes ) {
 	$slug = $attributes['slug'];
 	if ( class_exists( 'WP_Block_Patterns_Registry' ) && WP_Block_Patterns_Registry::get_instance()->is_registered( $slug ) ) {
 		$pattern = WP_Block_Patterns_Registry::get_instance()->get_registered( $slug );
 		return do_blocks( $pattern['content'] );
-
 	}
 }
 
