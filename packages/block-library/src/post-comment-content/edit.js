83fcd22d19c974d29fb68881f8dbe6db63fb384e
David Arenas <darensr@gmail.com>
2021-10-07 13:56:39 +0200

    Comment Content: Support for `textAlign` attribute
    I've also removed some cases that I think
    should be handled in the parent block:
    - When the comment is not ready yet
    - When the comment does not exist

--- packages/block-library/src/post-comment-content/edit.js
+++ packages/block-library/src/post-comment-content/edit.js
@@ -1,80 +1,78 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { RawHTML } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
-import { store as coreStore } from '@wordpress/core-data';
-import { useBlockProps, Warning } from '@wordpress/block-editor';
-import { Icon, Disabled } from '@wordpress/components';
-import { commentContent } from '@wordpress/icons';
+import { Disabled } from '@wordpress/components';
+import { useEntityProp } from '@wordpress/core-data';
+import {
+	AlignmentControl,
+	BlockControls,
+	useBlockProps,
+} from '@wordpress/block-editor';
 
 /**
  * Renders the `core/post-comment-content` block on the editor.
  *
- * @param {Object} props                   React props.
- * @param {Object} props.context           Inherited context.
- * @param {string} props.context.commentId The comment ID.
+ * @param {Object} props                      React props.
+ * @param {Object} props.setAttributes        Callback for updating block attributes.
+ * @param {Object} props.attributes           Block attributes.
+ * @param {string} props.attributes.textAlign The `textAlign` attribute.
+ * @param {Object} props.context              Inherited context.
+ * @param {string} props.context.commentId    The comment ID.
  *
- * @return {import('react').ReactNode} React element.
+ * @return {JSX.Element} React element.
  */
-export default function Edit( { context: { commentId } } ) {
-	const blockProps = useBlockProps();
+export default function Edit( {
+	setAttributes,
+	attributes: { textAlign },
+	context: { commentId },
+} ) {
+	// Generate the block props adding extra classes.
+	const blockProps = useBlockProps( {
+		className: classnames( {
+			[ `has-text-align-${ textAlign }` ]: textAlign,
+		} ),
+	} );
 
-	// Get the comment using a custom selector. This allows us to
-	// check whether the comment is still loading or ready.
-	const { comment, isLoading } = useSelect(
-		( select ) => {
-			const { getEntityRecord, hasFinishedResolution } = select(
-				coreStore
-			);
-			const queryArgs = [ 'root', 'comment', commentId ];
-			return {
-				comment: getEntityRecord( ...queryArgs ),
-				isLoading: ! hasFinishedResolution(
-					'getEntityRecord',
-					queryArgs
-				),
-			};
-		},
-		[ commentId ]
+	// Get the comment content from the core store.
+	const [ content ] = useEntityProp(
+		'root',
+		'comment',
+		'content',
+		commentId
 	);
 
-	// Show a placeholder chip if the comment is not ready yet.
-	if ( isLoading ) {
-		return (
-			<div { ...blockProps }>
-				<Icon icon={ commentContent } />
-				<p> { __( 'Comment Content' ) }</p>
-			</div>
-		);
-	}
-
-	// Show a sample text when there is no `commentId` in the context.
-	if ( ! commentId ) {
-		return (
-			<div { ...blockProps }>{ __( 'The content of the comment.' ) }</div>
-		);
-	}
-
-	// Show a warning message when the comment does not exist.
-	if ( ! comment ) {
-		return (
-			<div { ...blockProps }>
-				<Warning>
-					{ __( 'The specified comment does not exist.' ) }
-				</Warning>
-			</div>
-		);
-	}
-
-	// At this point, comment should exist and have a content attribute.
-	// Using the optional chaining operator just to avoid possible errors.
 	return (
-		<div { ...blockProps }>
-			<Disabled>
-				<RawHTML key="html">{ comment.content?.rendered }</RawHTML>
-			</Disabled>
-		</div>
+		<>
+			{ /* Add controls to handle text alignment. */ }
+			<BlockControls group="block">
+				<AlignmentControl
+					value={ textAlign }
+					onChange={ ( newAlign ) =>
+						setAttributes( { textAlign: newAlign } )
+					}
+				/>
+			</BlockControls>
+			<div { ...blockProps }>
+				{ ! commentId ? (
+					// Show a sample text when there is no `commentId` in the context.
+					<p>{ __( 'The content of the comment.' ) }</p>
+				) : (
+					// Render the comment content. Note that the parent block is
+					// responsible for showing a warning message if the comment does not
+					// exist. The optional chaining operator is used just to avoid
+					// possible errors related to a missing comment.
+					<Disabled>
+						<RawHTML key="html">{ content?.rendered }</RawHTML>
+					</Disabled>
+				) }
+			</div>
+		</>
 	);
 }
