e0ce9a3557112e9b620fca45d40cca74fa8801f9
Joel Dean <jdeanjj1000@gmail.com>
2021-10-08 15:46:52 -0500

    Changed the variable to something more meaningful.

--- packages/block-editor/src/components/rich-text/index.native.js
+++ packages/block-editor/src/components/rich-text/index.native.js
@@ -475,8 +475,8 @@ function RichTextWrapper(
 						return;
 					}
 
-					const firstBlock = content[ 0 ];
-					const { name } = firstBlock;
+					const derivedBlock = content[ 0 ];
+					const { name } = derivedBlock;
 
 					// When an URL is pasted in an empty paragraph then the EmbedHandlerPicker should showcase options allowing the transformation of that URL
 					// into either an Embed block or a link within the target paragraph. If the paragraph is non-empty, the URL is pasted as text.
