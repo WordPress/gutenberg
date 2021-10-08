0cb643d69fd4be3de5c92ab7a5a8aec71c1c656a
Dave Smith <getdavemail@gmail.com>
2021-10-08 14:21:04 +0100

    Simplify code and improve regex

--- packages/dom/src/dom/strip-html.js
+++ packages/dom/src/dom/strip-html.js
@@ -6,15 +6,14 @@
  * @return {string} The text content with any html removed.
  */
 export default function stripHTML( html ) {
-	const leadingSpaces = html.match( /(^\s+)/g );
-
 	// DOM Parser will ignore any space character coming after
 	// the DocType.
 	// see: https://html.spec.whatwg.org/multipage/parsing.html#after-doctype-name-state
 	// As a result any leading space in the provided `html`
 	// argument string will be stripped out.
 	// Manually retrieve these prior to parsing for restoration post-parse.
-	const spacesToRestore = leadingSpaces ? leadingSpaces[ 0 ] : '';
+	// @ts-ignore
+	const [ spacesToRestore ] = html.match( /^\s*/ );
 
 	const document = new window.DOMParser().parseFromString(
 		html,
