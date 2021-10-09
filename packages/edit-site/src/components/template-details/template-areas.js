d7339e03d811c56ece60abc6cfd988420c534376
Kai Hao <kevin830726@gmail.com>
2021-10-09 13:50:28 +0800

    Add more options to template areas in template details

--- packages/edit-site/src/components/template-details/template-areas.js
+++ packages/edit-site/src/components/template-details/template-areas.js
@@ -1,11 +1,12 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { MenuGroup, MenuItem } from '@wordpress/components';
+import { sprintf, __ } from '@wordpress/i18n';
+import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { getTemplatePartIcon } from '@wordpress/editor';
 import { store as blockEditorStore } from '@wordpress/block-editor';
+import { moreVertical } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -13,7 +14,41 @@ import { store as blockEditorStore } from '@wordpress/block-editor';
 import { store as editSiteStore } from '../../store';
 import { TEMPLATE_PART_AREA_TO_NAME } from '../../store/constants';
 
-function TemplatePartItem( { area, clientId } ) {
+function TemplatePartItemMore( { onClose, templatePart } ) {
+	const { revertTemplate } = useDispatch( editSiteStore );
+
+	function clearCustomizations() {
+		revertTemplate( templatePart );
+		onClose();
+	}
+
+	return (
+		<>
+			<MenuGroup>
+				<MenuItem onClick={ onClose }>
+					{ sprintf(
+						/* translators: %s: template part title */
+						__( 'Edit %s' ),
+						templatePart.title?.rendered
+					) }
+				</MenuItem>
+				<MenuItem onClick={ onClose }>
+					{ __( 'Discard unsaved changes' ) }
+				</MenuItem>
+			</MenuGroup>
+			<MenuGroup>
+				<MenuItem
+					info={ __( 'Restore template to theme default' ) }
+					onClick={ clearCustomizations }
+				>
+					{ __( 'Clear customizations' ) }
+				</MenuItem>
+			</MenuGroup>
+		</>
+	);
+}
+
+function TemplatePartItem( { templatePart, clientId } ) {
 	const { selectBlock, toggleBlockHighlight } = useDispatch(
 		blockEditorStore
 	);
@@ -21,29 +56,48 @@ function TemplatePartItem( { area, clientId } ) {
 	const cancelHighlightBlock = () => toggleBlockHighlight( clientId, false );
 
 	return (
-		<MenuItem
-			icon={ getTemplatePartIcon( area ) }
-			iconPosition="left"
-			onClick={ () => {
-				selectBlock( clientId );
-			} }
-			onMouseOver={ highlightBlock }
-			onMouseLeave={ cancelHighlightBlock }
-			onFocus={ highlightBlock }
-			onBlur={ cancelHighlightBlock }
+		<div
+			role="menuitem"
+			className="edit-site-template-details__template-areas-item"
 		>
-			{ TEMPLATE_PART_AREA_TO_NAME[ area ] }
-		</MenuItem>
+			<MenuItem
+				role="button"
+				icon={ getTemplatePartIcon( templatePart.area ) }
+				iconPosition="left"
+				onClick={ () => {
+					selectBlock( clientId );
+				} }
+				onMouseOver={ highlightBlock }
+				onMouseLeave={ cancelHighlightBlock }
+				onFocus={ highlightBlock }
+				onBlur={ cancelHighlightBlock }
+			>
+				{ TEMPLATE_PART_AREA_TO_NAME[ templatePart.area ] }
+			</MenuItem>
+
+			<DropdownMenu
+				icon={ moreVertical }
+				label="More options"
+				className="edit-site-template-details__template-areas-item-more"
+			>
+				{ ( { onClose } ) => (
+					<TemplatePartItemMore
+						onClose={ onClose }
+						templatePart={ templatePart }
+					/>
+				) }
+			</DropdownMenu>
+		</div>
 	);
 }
 
 export default function TemplateAreas() {
-	const templateAreaBlocks = useSelect(
-		( select ) => select( editSiteStore ).getTemplateAreaBlocks(),
+	const templateParts = useSelect(
+		( select ) => select( editSiteStore ).getTemplateParts(),
 		[]
 	);
 
-	if ( ! Object.keys( templateAreaBlocks ).length ) {
+	if ( ! templateParts.length ) {
 		return null;
 	}
 
@@ -52,15 +106,13 @@ export default function TemplateAreas() {
 			label={ __( 'Areas' ) }
 			className="edit-site-template-details__group edit-site-template-details__template-areas"
 		>
-			{ Object.entries( templateAreaBlocks ).map(
-				( [ area, templateAreaBlock ] ) => (
-					<TemplatePartItem
-						key={ area }
-						area={ area }
-						clientId={ templateAreaBlock.clientId }
-					/>
-				)
-			) }
+			{ templateParts.map( ( { templatePart, block } ) => (
+				<TemplatePartItem
+					key={ templatePart.area }
+					clientId={ block.clientId }
+					templatePart={ templatePart }
+				/>
+			) ) }
 		</MenuGroup>
 	);
 }
