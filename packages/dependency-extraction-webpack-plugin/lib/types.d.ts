481ccc609a5e2ae679ca59e31750cb10040547b1
Greg Ziółkowski <grzegorz@gziolo.pl>
2021-10-12 11:19:20 +0200

    Update types.d.ts

--- packages/dependency-extraction-webpack-plugin/lib/types.d.ts
+++ packages/dependency-extraction-webpack-plugin/lib/types.d.ts
@@ -15,5 +15,5 @@ declare interface DependencyExtractionWebpackPluginOptions {
 	requestToHandle?: ( request: string ) => string | undefined;
 	combinedOutputFile?: string | null;
 	combineAssets?: boolean;
-    externalizedReportFile?: string | undefined;
+	externalizedReportFile?: string | undefined;
 }
