9385b59b591735b5cef4d543edc5dd8e790c45cc
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2021-10-12 15:26:09 +1100

    Update elements block support to use shared function

--- lib/block-supports/elements.php
+++ lib/block-supports/elements.php
@@ -60,15 +60,7 @@ function gutenberg_render_elements_support( $block_content, $block ) {
 		$content              = substr_replace( $block_content, ' class="' . $class_name . '"', $first_element_offset + strlen( $first_element ) - 1, 0 );
 	}
 
-	// Ideally styles should be loaded in the head, but blocks may be parsed
-	// after that, so loading in the footer for now.
-	// See https://core.trac.wordpress.org/ticket/53494.
-	add_action(
-		'wp_footer',
-		function () use ( $style ) {
-			echo $style;
-		}
-	);
+	gutenberg_render_block_support_style( $style );
 
 	return $content;
 }
