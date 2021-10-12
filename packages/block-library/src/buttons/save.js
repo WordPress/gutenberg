1c24d37009af7059e48288fac5bae0011614ebd5
Ella van Durpe <ella@vandurpe.com>
2021-10-12 11:33:00 +0300

    Adjust remaning save functions

--- packages/block-library/src/buttons/save.js
+++ packages/block-library/src/buttons/save.js
@@ -11,16 +11,12 @@ import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 export default function save( {
 	attributes: { contentJustification, orientation },
 } ) {
-	return (
-		<div
-			{ ...useInnerBlocksProps.save(
-				useBlockProps.save( {
-					className: classnames( {
-						[ `is-content-justification-${ contentJustification }` ]: contentJustification,
-						'is-vertical': orientation === 'vertical',
-					} ),
-				} )
-			) }
-		/>
-	);
+	const blockProps = useBlockProps.save( {
+		className: classnames( {
+			[ `is-content-justification-${ contentJustification }` ]: contentJustification,
+			'is-vertical': orientation === 'vertical',
+		} ),
+	} );
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+	return <div { ...innerBlocksProps } />;
 }
