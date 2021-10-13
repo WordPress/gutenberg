9c544b4b4ba50f282d556b953725625b2790ca08
jasmussen <joen@automattic.com>
2021-10-13 13:18:23 +0200

    Try alt test fix.

--- packages/e2e-tests/specs/widgets/editing-widgets.test.js
+++ packages/e2e-tests/specs/widgets/editing-widgets.test.js
@@ -114,7 +114,7 @@ describe( 'Widgets screen', () => {
 			{ selector: '[data-block]' },
 			{ root: widgetArea }
 		);
-		const lastBlock = childBlocks[ childBlocks.length ];
+		const lastBlock = childBlocks[ childBlocks.length - 2 ];
 		const lastBlockBoundingBox = await lastBlock.boundingBox();
 
 		// TODO: Probably need a more accessible way to select this, maybe a test ID or data attribute.
