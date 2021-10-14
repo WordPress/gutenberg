13a60433739f1423ec38abf32ad66cf8ba2d1072
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-14 17:23:09 -0400

    Allow local directories to be passed to --template as relative paths. (#35645)
    * Check if passed template is a directory and resolve it.
    
    * Linting fixes.

--- packages/create-block/lib/templates.js
+++ packages/create-block/lib/templates.js
@@ -3,6 +3,8 @@
  */
 const { command } = require( 'execa' );
 const glob = require( 'fast-glob' );
+const { resolve } = require( 'path' );
+const { existsSync } = require( 'fs' );
 const { mkdtemp, readFile } = require( 'fs' ).promises;
 const { fromPairs, isObject } = require( 'lodash' );
 const npmPackageArg = require( 'npm-package-arg' );
@@ -124,6 +126,9 @@ const getBlockTemplate = async ( templateName ) => {
 	}
 
 	try {
+		if ( existsSync( resolve( templateName ) ) ) {
+			return await configToTemplate( require( resolve( templateName ) ) );
+		}
 		return await configToTemplate( require( templateName ) );
 	} catch ( error ) {
 		if ( error instanceof CLIError ) {
