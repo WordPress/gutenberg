364ab0d3132dd250dcef1e04eebdf872bdf47f81
Riad Benguella <benguella@gmail.com>
2021-10-14 09:53:01 +0100

    Updates per review

--- lib/compat/wordpress-5.9/default-theme-supports.php
+++ lib/compat/wordpress-5.9/default-theme-supports.php
@@ -8,7 +8,6 @@
 if ( gutenberg_is_fse_theme() ) {
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'responsive-embeds' );
-	add_theme_support( 'wp-block-styles' );
 	add_theme_support( 'editor-styles' );
 	add_theme_support(
 		'html5',
@@ -17,4 +16,6 @@ if ( gutenberg_is_fse_theme() ) {
 			'script',
 		)
 	);
+	add_theme_support( 'automatic-feed-links' );
+	add_filter( 'should_load_separate_core_block_assets', '__return_true' );
 }
