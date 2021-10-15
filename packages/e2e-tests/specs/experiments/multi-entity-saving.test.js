e3fb45bca39a1fab9049486472aec812db615659
ramonjd <ramonjd@gmail.com>
2021-10-15 15:59:03 +1100

    Updating e2e test selectors

--- packages/e2e-tests/specs/experiments/multi-entity-saving.test.js
+++ packages/e2e-tests/specs/experiments/multi-entity-saving.test.js
@@ -249,7 +249,7 @@ describe( 'Multi-entity save flow', () => {
 			// Select the header template part via list view.
 			await page.click( '.edit-site-header-toolbar__list-view-toggle' );
 			const headerTemplatePartListViewButton = await page.waitForXPath(
-				'//button[contains(@class, "block-editor-list-view-block-select-button")][contains(., "Header")]'
+				'//a[contains(@class, "block-editor-list-view-block-select-button")][contains(., "Header")]'
 			);
 			headerTemplatePartListViewButton.click();
 			await page.click( 'button[aria-label="Close list view sidebar"]' );
