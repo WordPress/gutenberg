c0c6be48deba33ea2ea3aa5467ddf2c3be0b55c6
Lena Morita <lena@jaguchi.com>
2021-10-18 16:24:28 +0900

    Storybook: Add RTL toolbar (#35711)

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,11 +1,22 @@
 /**
- * WordPress dependencies
- */
-/* eslint-disable no-restricted-syntax */
-import '@wordpress/components/build-style/style.css';
-/* eslint-enable no-restricted-syntax */
-
-/**
  * Internal dependencies
  */
+import { WithRTL } from './decorators/with-rtl';
 import './style.scss';
+
+export const globalTypes = {
+	direction: {
+		name: 'RTL',
+		description: 'Simulate an RTL language.',
+		defaultValue: 'ltr',
+		toolbar: {
+			icon: 'globe',
+			items: [
+				{ value: 'ltr', title: 'LTR' },
+				{ value: 'rtl', title: 'RTL' },
+			],
+		},
+	},
+};
+
+export const decorators = [ WithRTL ];
