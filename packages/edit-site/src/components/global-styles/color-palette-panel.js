c0a7a21423e6c6416bb076d940661b910e1b74ac
Jorge <jorge.costa@developer.pt>
2021-10-19 22:37:13 +0100

    Update: Implement new color palette editor component

--- packages/edit-site/src/components/global-styles/color-palette-panel.js
+++ packages/edit-site/src/components/global-styles/color-palette-panel.js
@@ -2,57 +2,21 @@
  * WordPress dependencies
  */
 import { __experimentalColorEdit as ColorEdit } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
-import { useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { useSetting } from './hooks';
 
-/**
- * Shared reference to an empty array for cases where it is important to avoid
- * returning a new array reference on every invocation, as in a connected or
- * other pure component which performs `shouldComponentUpdate` check on props.
- * This should be used as a last resort, since the normalized data should be
- * maintained by the reducer result in state.
- *
- * @type {Array}
- */
-const EMPTY_ARRAY = [];
-
 export default function ColorPalettePanel( { name } ) {
-	const [ colors, setColors ] = useSetting( 'color.palette', name );
-	const [ userColors ] = useSetting( 'color.palette', name, 'user' );
-	const [ baseGlobalPalette ] = useSetting(
-		'color.palette',
-		undefined,
-		'base'
-	);
-	const [ baseContextualPalette ] = useSetting(
+	const [ userColors, setColors ] = useSetting(
 		'color.palette',
 		name,
-		'base'
+		'user'
 	);
-	const immutableColorSlugs = useMemo( () => {
-		const basePalette = baseContextualPalette ?? baseGlobalPalette;
-		if ( ! basePalette ) {
-			return EMPTY_ARRAY;
-		}
-		return basePalette.map( ( { slug } ) => slug );
-	}, [ baseContextualPalette, baseGlobalPalette ] );
-
 	return (
 		<div className="edit-site-global-styles-color-palette-panel">
-			<ColorEdit
-				immutableColorSlugs={ immutableColorSlugs }
-				colors={ colors }
-				onChange={ setColors }
-				emptyUI={ __(
-					'Colors are empty! Add some colors to create your own color palette.'
-				) }
-				canReset={ colors === userColors }
-			/>
+			<ColorEdit colors={ userColors } onChange={ setColors } />
 		</div>
 	);
 }
