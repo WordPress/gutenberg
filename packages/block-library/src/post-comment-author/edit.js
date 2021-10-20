5fecab92b74717a77257f0daea4254b1b981d593
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-10-20 16:27:48 +0200

    Post Comment: Handle the case where a comment does not exist

--- packages/block-library/src/post-comment-author/edit.js
+++ packages/block-library/src/post-comment-author/edit.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { store as coreStore } from '@wordpress/core-data';
@@ -10,18 +10,23 @@ import { PanelBody, ToggleControl } from '@wordpress/components';
 /**
  * Renders the `core/post-comment-author` block on the editor.
  *
- * @param {Object} props               React props.
- * @param {Object} props.setAttributes Callback for updating block attributes.
- * @param {Object} props.attributes    Block attributes.
- * @param {Object} props.context       Inherited context.
+ * @param {Object} props                       React props.
+ * @param {Object} props.setAttributes         Callback for updating block attributes.
+ * @param {Object} props.attributes            Block attributes.
+ * @param {string} props.attributes.className  Block class name.
+ * @param {string} props.attributes.isLink     Whether the author name should be linked.
+ * @param {string} props.attributes.linkTarget Target of the link.
+ * @param {Object} props.context               Inherited context.
+ * @param {string} props.context.commentId     The comment ID.
  *
  * @return {JSX.Element} React element.
  */
-export default function Edit( { attributes, context, setAttributes } ) {
-	const { className, isLink, linkTarget } = attributes;
-	const { commentId } = context;
+export default function Edit( {
+	attributes: { className, isLink, linkTarget },
+	context: { commentId },
+	setAttributes,
+} ) {
 	const blockProps = useBlockProps( { className } );
-
 	const displayName = useSelect(
 		( select ) => {
 			const { getEntityRecord } = select( coreStore );
@@ -38,6 +43,40 @@ export default function Edit( { attributes, context, setAttributes } ) {
 		[ commentId ]
 	);
 
+	const inspectorControls = (
+		<InspectorControls>
+			<PanelBody title={ __( 'Link settings' ) }>
+				<ToggleControl
+					label={ __( 'Link to authors URL' ) }
+					onChange={ () => setAttributes( { isLink: ! isLink } ) }
+					checked={ isLink }
+				/>
+				{ isLink && (
+					<ToggleControl
+						label={ __( 'Open in new tab' ) }
+						onChange={ ( value ) =>
+							setAttributes( {
+								linkTarget: value ? '_blank' : '_self',
+							} )
+						}
+						checked={ linkTarget === '_blank' }
+					/>
+				) }
+			</PanelBody>
+		</InspectorControls>
+	);
+
+	if ( ! commentId || ! displayName ) {
+		return (
+			<>
+				{ inspectorControls }
+				<div { ...blockProps }>
+					<p>{ _x( 'Post Comment Author', 'block title' ) }</p>
+				</div>
+			</>
+		);
+	}
+
 	const displayAuthor = isLink ? (
 		<a
 			href="#comment-author-pseudo-link"
@@ -51,26 +90,7 @@ export default function Edit( { attributes, context, setAttributes } ) {
 
 	return (
 		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Link settings' ) }>
-					<ToggleControl
-						label={ __( 'Link to authors URL' ) }
-						onChange={ () => setAttributes( { isLink: ! isLink } ) }
-						checked={ isLink }
-					/>
-					{ isLink && (
-						<ToggleControl
-							label={ __( 'Open in new tab' ) }
-							onChange={ ( value ) =>
-								setAttributes( {
-									linkTarget: value ? '_blank' : '_self',
-								} )
-							}
-							checked={ linkTarget === '_blank' }
-						/>
-					) }
-				</PanelBody>
-			</InspectorControls>
+			{ inspectorControls }
 			<div { ...blockProps }>{ displayAuthor }</div>
 		</>
 	);
--- packages/block-library/src/post-comment-date/edit.js
+++ packages/block-library/src/post-comment-date/edit.js
@@ -9,24 +9,77 @@ import {
 	CustomSelectControl,
 	ToggleControl,
 } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 
-export default function Edit( { attributes, context, setAttributes } ) {
-	const { className, format, isLink } = attributes;
-	const { commentId } = context;
+/**
+ * Renders the `core/post-comment-date` block on the editor.
+ *
+ * @param {Object} props                      React props.
+ * @param {Object} props.setAttributes        Callback for updating block attributes.
+ * @param {Object} props.attributes           Block attributes.
+ * @param {string} props.attributes.className Block class name.
+ * @param {string} props.attributes.format    Format of the date.
+ * @param {string} props.attributes.isLink    Whether the author name should be linked.
+ * @param {Object} props.context              Inherited context.
+ * @param {string} props.context.commentId    The comment ID.
+ *
+ * @return {JSX.Element} React element.
+ */
+export default function Edit( {
+	attributes: { className, format, isLink },
+	context: { commentId },
+	setAttributes,
+} ) {
+	const blockProps = useBlockProps( { className } );
+	const [ date ] = useEntityProp( 'root', 'comment', 'date', commentId );
+	const [ siteDateFormat ] = useEntityProp( 'root', 'site', 'date_format' );
 
 	const settings = __experimentalGetSettings();
-	const [ siteDateFormat ] = useEntityProp( 'root', 'site', 'date_format' );
-	const [ date ] = useEntityProp( 'root', 'comment', 'date', commentId );
-
 	const formatOptions = Object.values( settings.formats ).map(
 		( formatOption ) => ( {
 			key: formatOption,
-			name: dateI18n( formatOption, date ),
+			name: dateI18n( formatOption, date || new Date() ),
 		} )
 	);
 	const resolvedFormat = format || siteDateFormat || settings.formats.date;
-	const blockProps = useBlockProps( { className } );
+
+	const inspectorControls = (
+		<InspectorControls>
+			<PanelBody title={ __( 'Format settings' ) }>
+				<CustomSelectControl
+					hideLabelFromVision
+					label={ __( 'Date Format' ) }
+					options={ formatOptions }
+					onChange={ ( { selectedItem } ) =>
+						setAttributes( {
+							format: selectedItem.key,
+						} )
+					}
+					value={ formatOptions.find(
+						( option ) => option.key === resolvedFormat
+					) }
+				/>
+			</PanelBody>
+			<PanelBody title={ __( 'Link settings' ) }>
+				<ToggleControl
+					label={ __( 'Link to comment' ) }
+					onChange={ () => setAttributes( { isLink: ! isLink } ) }
+					checked={ isLink }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+
+	if ( ! commentId || ! date ) {
+		return (
+			<>
+				{ inspectorControls }
+				<div { ...blockProps }>
+					<p>{ _x( 'Post Comment Date', 'block title' ) }</p>
+				</div>
+			</>
+		);
+	}
 
 	let commentDate = (
 		<time dateTime={ dateI18n( 'c', date ) }>
@@ -47,30 +100,7 @@ export default function Edit( { attributes, context, setAttributes } ) {
 
 	return (
 		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Format settings' ) }>
-					<CustomSelectControl
-						hideLabelFromVision
-						label={ __( 'Date Format' ) }
-						options={ formatOptions }
-						onChange={ ( { selectedItem } ) =>
-							setAttributes( {
-								format: selectedItem.key,
-							} )
-						}
-						value={ formatOptions.find(
-							( option ) => option.key === resolvedFormat
-						) }
-					/>
-				</PanelBody>
-				<PanelBody title={ __( 'Link settings' ) }>
-					<ToggleControl
-						label={ __( 'Link to comment' ) }
-						onChange={ () => setAttributes( { isLink: ! isLink } ) }
-						checked={ isLink }
-					/>
-				</PanelBody>
-			</InspectorControls>
+			{ inspectorControls }
 			<div { ...blockProps }>{ commentDate }</div>
 		</>
 	);
