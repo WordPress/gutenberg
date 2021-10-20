691c15a7eaa4979cb5ea360d9a5a65213ff2474a
Sarah Norris <sarah@sekai.co.uk>
2021-10-20 18:23:29 +0100

    Nav block: replace className with classes

--- packages/block-library/src/navigation-link/index.php
+++ packages/block-library/src/navigation-link/index.php
@@ -152,15 +152,10 @@ function render_block_core_navigation_link( $attributes, $content, $block ) {
 	$has_submenu = count( $block->inner_blocks ) > 0;
 	$is_active   = ! empty( $attributes['id'] ) && ( get_the_ID() === $attributes['id'] );
 
-	$class_name     = ! empty( $attributes['className'] ) ? implode( ' ', (array) $attributes['className'] ) : false;
-	$custom_classes = ! empty( $attributes['classes'] ) ? implode( ' ', (array) $attributes['classes'] ) : false;
+	$class_names = ! empty( $attributes['classes'] ) ? implode( ' ', (array) $attributes['classes'] ) : false;
 
-	if ( false !== $class_name ) {
-		$css_classes .= ' ' . $class_name;
-	}
-
-	if ( false !== $custom_classes ) {
-		$css_classes .= ' ' . $custom_classes;
+	if ( false !== $class_names ) {
+		$css_classes .= ' ' . $class_names;
 	}
 
 	$wrapper_attributes = get_block_wrapper_attributes(
