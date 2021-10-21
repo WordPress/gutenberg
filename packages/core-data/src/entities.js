0f90ee424b4304ba2fe9d0eda9665cba7994b745
Jonny Harris <jon@spacedmonkey.co.uk>
2021-10-21 19:53:20 +0100

    Read rest_namespace for REST API.

--- packages/core-data/src/entities.js
+++ packages/core-data/src/entities.js
@@ -173,9 +173,10 @@ function* loadPostTypeEntities() {
 		const isTemplate = [ 'wp_template', 'wp_template_part' ].includes(
 			name
 		);
+		const namespace = postType?.rest_namespace ? postType.rest_namespace : 'wp/v2';
 		return {
 			kind: 'postType',
-			baseURL: '/wp/v2/' + postType.rest_base,
+			baseURL: '/' + namespace + '/' + postType.rest_base,
 			baseURLParams: { context: 'edit' },
 			name,
 			label: postType.labels.singular_name,
@@ -204,9 +205,10 @@ function* loadTaxonomyEntities() {
 		path: '/wp/v2/taxonomies?context=edit',
 	} );
 	return map( taxonomies, ( taxonomy, name ) => {
+		const namespace = taxonomy.rest_namespace ? taxonomy.rest_namespace : 'wp/v2';
 		return {
 			kind: 'taxonomy',
-			baseURL: '/wp/v2/' + taxonomy.rest_base,
+			baseURL: '/' + namespace + '/' + taxonomy.rest_base,
 			baseURLParams: { context: 'edit' },
 			name,
 			label: taxonomy.labels.singular_name,
