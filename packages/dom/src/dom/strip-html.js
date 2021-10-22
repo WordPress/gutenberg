55d9756f4d9e43d3e89327e8ff38aa160c52b49b
Dave Smith <getdavemail@gmail.com>
2021-10-22 09:42:46 +0100

    Use safeHTML to strip scripts and on attributes

--- packages/dom/src/dom/strip-html.js
+++ packages/dom/src/dom/strip-html.js
@@ -1,4 +1,9 @@
 /**
+ * Internal dependencies
+ */
+import safeHTML from './safe-html';
+
+/**
  * Removes any HTML tags from the provided string.
  *
  * @param {string} html The string containing html.
@@ -6,6 +11,10 @@
  * @return {string} The text content with any html removed.
  */
 export default function stripHTML( html ) {
+	// Remove any script tags or on* attributes otherwise their *contents* will be left
+	// in place following removal of HTML tags.
+	html = safeHTML( html );
+
 	const doc = document.implementation.createHTMLDocument( '' );
 	doc.body.innerHTML = html;
 	return doc.body.textContent || '';
