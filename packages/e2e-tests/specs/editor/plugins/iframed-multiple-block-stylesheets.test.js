39ec60db03657c10d653349fd6ceaeddb9539d2c
Ari Stathopoulos <aristath@gmail.com>
2021-10-25 12:16:59 +0300

    Update packages/e2e-tests/specs/editor/plugins/iframed-multiple-block-stylesheets.test.js
    Co-authored-by: Nik Tsekouras <ntsekouras@outlook.com>

--- packages/e2e-tests/specs/editor/plugins/iframed-multiple-block-stylesheets.test.js
+++ packages/e2e-tests/specs/editor/plugins/iframed-multiple-block-stylesheets.test.js
@@ -64,7 +64,7 @@ describe( 'iframed multiple block stylesheets', () => {
 			'rgb(0, 0, 0)'
 		);
 
-		// Skip errors related to block-styles enqueing and the use of add_editor_style.
+		// Skip warnings related to block-styles enqueing and the use of add_editor_style.
 		// The issue is tracked on https://github.com/WordPress/gutenberg/issues/33212.
 		expect( console ).toHaveWarned();
 	} );
