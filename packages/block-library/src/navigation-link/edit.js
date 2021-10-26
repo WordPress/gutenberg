9e23d7eb490ebff49c7d295545a7b9e1cd082e41
tellthemachines <isabel@tellthemachines.com>
2021-10-26 13:49:55 +1100

    Transforms only show for empty link.

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -766,12 +766,18 @@ export default function NavigationLinkEdit( {
 									)
 								}
 								onRemove={ removeLink }
-								renderControlBottom={ () => (
-									<LinkControlTransforms
-										block={ thisBlock }
-										transforms={ featuredTransforms }
-									/>
-								) }
+								renderControlBottom={
+									! url
+										? () => (
+												<LinkControlTransforms
+													block={ thisBlock }
+													transforms={
+														featuredTransforms
+													}
+												/>
+										  )
+										: null
+								}
 							/>
 						</Popover>
 					) }
