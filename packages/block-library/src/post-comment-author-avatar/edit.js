1cab5bb948a60a15da92119a00292d6e92a69e00
Carlos Bravo <carlos@ticloop.com>
2021-10-26 11:35:12 +0200

    Add default option if avatar could not be loaded

--- packages/block-library/src/post-comment-author-avatar/edit.js
+++ packages/block-library/src/post-comment-author-avatar/edit.js
@@ -4,7 +4,7 @@
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody, ResizableBox, RangeControl } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
-import { __, isRTL } from '@wordpress/i18n';
+import { __, _x, isRTL } from '@wordpress/i18n';
 import { useRef } from '@wordpress/element';
 
 export default function Edit( {
@@ -35,56 +35,62 @@ export default function Edit( {
 	const blockProps = useBlockProps();
 	const maxSizeBuffer = maxSize * 2.5;
 
+	const inspectorControls = (
+		<InspectorControls>
+			<PanelBody title={ __( 'Avatar Settings' ) }>
+				<RangeControl
+					label={ __( 'Image size' ) }
+					onChange={ ( newWidth ) =>
+						setAttributes( {
+							width: newWidth,
+							height: newWidth,
+						} )
+					}
+					min={ minSize }
+					max={ maxSizeBuffer }
+					initialPosition={ width }
+					value={ width }
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+
+	const displayAvatar = avatarUrls ? (
+		<ResizableBox
+			size={ {
+				width,
+				height,
+			} }
+			onResizeStop={ ( event, direction, elt, delta ) => {
+				setAttributes( {
+					height: parseInt( height + delta.height, 10 ),
+					width: parseInt( width + delta.width, 10 ),
+				} );
+			} }
+			lockAspectRatio
+			enable={ {
+				top: false,
+				right: ! isRTL(),
+				bottom: true,
+				left: isRTL(),
+			} }
+			minWidth={ minSize }
+			maxWidth={ maxSizeBuffer }
+		>
+			<img
+				src={ avatarUrls[ avatarUrls.length - 1 ] }
+				alt={ `${ authorName } ${ __( 'Avatar' ) }` }
+				{ ...blockProps }
+			/>
+		</ResizableBox>
+	) : (
+		<p>{ _x( 'Post Comment Author Avatar', 'block title' ) }</p>
+	);
+
 	return (
 		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Avatar Settings' ) }>
-					<RangeControl
-						label={ __( 'Image size' ) }
-						onChange={ ( newWidth ) =>
-							setAttributes( {
-								width: newWidth,
-								height: newWidth,
-							} )
-						}
-						min={ minSize }
-						max={ maxSizeBuffer }
-						initialPosition={ width }
-						value={ width }
-					/>
-				</PanelBody>
-			</InspectorControls>
-			<div ref={ containerRef }>
-				{ avatarUrls ? (
-					<ResizableBox
-						size={ {
-							width,
-							height,
-						} }
-						onResizeStop={ ( event, direction, elt, delta ) => {
-							setAttributes( {
-								height: parseInt( height + delta.height, 10 ),
-								width: parseInt( width + delta.width, 10 ),
-							} );
-						} }
-						lockAspectRatio
-						enable={ {
-							top: false,
-							right: ! isRTL(),
-							bottom: true,
-							left: isRTL(),
-						} }
-						minWidth={ minSize }
-						maxWidth={ maxSizeBuffer }
-					>
-						<img
-							src={ avatarUrls[ avatarUrls.length - 1 ] }
-							alt={ `${ authorName } ${ __( 'Avatar' ) }` }
-							{ ...blockProps }
-						/>
-					</ResizableBox>
-				) : null }
-			</div>
+			{ inspectorControls }
+			<div ref={ containerRef }>{ displayAvatar }</div>
 		</>
 	);
 }
