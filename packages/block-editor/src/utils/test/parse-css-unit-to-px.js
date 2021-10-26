493ee1d6b315cac2ed3d755b3efa431dedb8c14e
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-10-26 09:54:15 -0700

    Use test.each for tests instead of a foreach

--- packages/block-editor/src/utils/test/parse-css-unit-to-px.js
+++ packages/block-editor/src/utils/test/parse-css-unit-to-px.js
@@ -10,31 +10,34 @@ describe( 'getPxFromCssUnit', () => {
 	// Absolute units
 	describe( 'absolute unites should return px values', () => {
 		const testData = [
-			{ unit: '25px', expected: '25px' },
-			{ unit: '25.5', expected: '26px' },
-			{ unit: '1cm', expected: '38px' },
-			{ unit: '10mm', expected: '38px' },
-			{ unit: '1in', expected: '96px' },
-			{ unit: '12pt', expected: '16px' },
-			{ unit: '1pc', expected: '16px' },
-			{ unit: '40Q', expected: '38px' }, // 40 Q should be 1 cm
+			[ '25px', '25px' ],
+			[ '25.5', '26px' ],
+			[ '1cm', '38px' ],
+			[ '10mm', '38px' ],
+			[ '1in', '96px' ],
+			[ '12pt', '16px' ],
+			[ '1pc', '16px' ],
+			[ '40Q', '38px' ], // 40 Q should be 1 cm
 		];
 
-		testData.forEach( ( data ) => {
-			it( 'test unit ' + data.unit + '', () => {
-				expect( getPxFromCssUnit( data.unit ) ).toBe( data.expected );
-			} );
-			it( 'test unit ' + data.unit + ' - memoized', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit ) ).toBe(
-					data.expected
-				);
-			} );
-			it( 'test unit ' + data.unit + ' - memoized cached', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit ) ).toBe(
-					data.expected
-				);
-			} );
-		} );
+		test.each( testData )(
+			'test getPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( getPxFromCssUnit( unit ) ).toBe( expected );
+			}
+		);
+		test.each( testData )(
+			'test memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit ) ).toBe( expected );
+			}
+		);
+		test.each( testData )(
+			'test cached memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit ) ).toBe( expected );
+			}
+		);
 	} );
 
 	describe( 'relative unites should return px values', () => {
@@ -47,33 +50,38 @@ describe( 'getPxFromCssUnit', () => {
 		};
 
 		const testData = [
-			{ unit: '2em', expected: '20px' },
-			{ unit: '2rem', expected: '20px' },
-			{ unit: '20vw', expected: '20px' },
-			{ unit: '20vh', expected: '40px' },
-			{ unit: '20vmin', expected: '20px' },
-			{ unit: '20vmax', expected: '40px' },
-			{ unit: '20lh', expected: '40px' },
-			{ unit: '120%', expected: '12px' },
+			[ '2em', '20px' ],
+			[ '2rem', '20px' ],
+			[ '20vw', '20px' ],
+			[ '20vh', '40px' ],
+			[ '20vmin', '20px' ],
+			[ '20vmax', '40px' ],
+			[ '20lh', '40px' ],
+			[ '120%', '12px' ],
 		];
 
-		testData.forEach( ( data ) => {
-			it( 'test unit ' + data.unit + '', () => {
-				expect( getPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
+		test.each( testData )(
+			'test getPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( getPxFromCssUnit( unit, settings ) ).toBe( expected );
+			}
+		);
+		test.each( testData )(
+			'test memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit, settings ) ).toBe(
+					expected
 				);
-			} );
-			it( 'test unit ' + data.unit + ' - memoized', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
+			}
+		);
+		test.each( testData )(
+			'test cached memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit, settings ) ).toBe(
+					expected
 				);
-			} );
-			it( 'test unit ' + data.unit + ' - memoized cached', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
-				);
-			} );
-		} );
+			}
+		);
 	} );
 
 	// Function units
@@ -88,47 +96,52 @@ describe( 'getPxFromCssUnit', () => {
 		};
 
 		const testData = [
-			{ unit: 'min(20px, 25px)', expected: '20px' },
-			{ unit: 'min(20px, 9px, 12pt, 25px)', expected: '9px' },
-			{ unit: 'max(20px, 25px)', expected: '25px' },
-			{ unit: 'clamp(10px, 9px, 25px)', expected: '10px' },
-			{ unit: 'clamp(10px, 35px, 25px)', expected: '25px' },
-			{ unit: 'clamp(10px, 15px, 25px)', expected: '15px' },
-			{ unit: 'min(max(20px,25px), 35px)', expected: '25px' },
-			{ unit: 'max(min(20px,25px), 35px)', expected: '35px' },
-			{ unit: '10px + 25px', expected: '35px' },
-			{ unit: 'calc(10px + 25px)', expected: '35px' },
-			{ unit: 'calc( 2 * 20px)', expected: '40px' },
-			{ unit: 'calc(25px - 10px)', expected: '15px' },
-			{ unit: 'min(10px + 25px, 55pt)', expected: '35px' },
-			{ unit: 'calc(12vw * 10px)', expected: '450px' },
-			{ unit: 'calc(45vw / 10px)', expected: '17px' },
-			{ unit: '', expected: null },
-			{ unit: undefined, expected: null },
-			{ unit: 123, expected: '123px' },
-			{ unit: 123.456, expected: '123px' },
-			{ unit: 'abc', expected: null },
-			{ unit: 'console.log("howdy"); + 10px', expected: null },
-			{ unit: 'calc(12vw * 10px', expected: null }, // missing closing bracket
+			[ 'min(20px, 25px)', '20px' ],
+			[ 'min(20px, 9px, 12pt, 25px)', '9px' ],
+			[ 'max(20px, 25px)', '25px' ],
+			[ 'clamp(10px, 9px, 25px)', '10px' ],
+			[ 'clamp(10px, 35px, 25px)', '25px' ],
+			[ 'clamp(10px, 15px, 25px)', '15px' ],
+			[ 'min(max(20px,25px), 35px)', '25px' ],
+			[ 'max(min(20px,25px), 35px)', '35px' ],
+			[ '10px + 25px', '35px' ],
+			[ 'calc(10px + 25px)', '35px' ],
+			[ 'calc( 2 * 20px)', '40px' ],
+			[ 'calc(25px - 10px)', '15px' ],
+			[ 'min(10px + 25px, 55pt)', '35px' ],
+			[ 'calc(12vw * 10px)', '450px' ],
+			[ 'calc(45vw / 10px)', '17px' ],
+			[ '', null ],
+			[ undefined, null ],
+			[ 123, '123px' ],
+			[ 123.456, '123px' ],
+			[ 'abc', null ],
+			[ 'console.log("howdy"); + 10px', null ],
+			[ 'calc(12vw * 10px', null ], // missing closing bracket
 		];
 
-		testData.forEach( ( data ) => {
-			it( 'test unit ' + data.unit, () => {
-				expect( getPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
+		test.each( testData )(
+			'test getPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( getPxFromCssUnit( unit, settings ) ).toBe( expected );
+			}
+		);
+		test.each( testData )(
+			'test memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit, settings ) ).toBe(
+					expected
 				);
-			} );
-			it( 'test unit ' + data.unit + ' - memoized', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
+			}
+		);
+		test.each( testData )(
+			'test cached memoizedGetPxFromCssUnit( %s )',
+			( unit, expected ) => {
+				expect( memoizedGetPxFromCssUnit( unit, settings ) ).toBe(
+					expected
 				);
-			} );
-			it( 'test unit ' + data.unit + ' - memoized cached', () => {
-				expect( memoizedGetPxFromCssUnit( data.unit, settings ) ).toBe(
-					data.expected
-				);
-			} );
-		} );
+			}
+		);
 	} );
 	// Skip this test it might be useful in dev.
 	it.skip( 'test performance of memoizedGetPxFromCssUnit function', () => {
