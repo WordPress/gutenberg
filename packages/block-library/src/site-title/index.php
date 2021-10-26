6eda11233a64225ebd9531d5ef13caee9a86c055
Carolina Nymark <myazalea@hotmail.com>
2021-10-26 15:14:09 +0200

    correct aria-current for the site title
    Correct aria-current for the site title.
    Add aria-current also when the title is a link on a static front page.

--- packages/block-library/src/site-title/index.php
+++ packages/block-library/src/site-title/index.php
@@ -21,7 +21,7 @@ function render_block_core_site_title( $attributes ) {
 	$tag_name         = 'h1';
 	$align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
 
-	$aria_current = is_home() ? ' aria-current="true"' : '';
+	$aria_current = is_home() || ( is_front_page() && 'page' == get_option( 'show_on_front' ) ) ? ' aria-current="page"' : '';
 
 	if ( isset( $attributes['level'] ) ) {
 		$tag_name = 0 === $attributes['level'] ? 'p' : 'h' . $attributes['level'];
