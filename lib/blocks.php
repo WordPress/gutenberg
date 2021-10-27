70ee6702962fb856b97ff61319b7815ac4174023
Carlos Bravo <37012961+c4rl0sbr4v0@users.noreply.github.com>
2021-10-27 14:03:58 +0200

    Block Library: Add new Edit Comment Link block (#35965)
    * Add edit post comment block
    
    * Replace spaces with tabs
    
    * Rename label for consistency
    
    * Quote the href attribute
    
    * Don't assume roles will have specific capabilities
    
    Co-authored-by: Carolina Nymark <myazalea@hotmail.com>
    
    * Update the block title
    
    * Add fixture
    
    * Move fixtures
    
    * Apply cs
    
    * Add edit post comment block
    
    * Replace spaces with tabs
    
    * Rename label for consistency
    
    * Quote the href attribute
    
    * Don't assume roles will have specific capabilities
    
    Co-authored-by: Carolina Nymark <myazalea@hotmail.com>
    
    * Update the block title
    
    * Add fixture
    
    * Move fixtures
    
    * Apply cs
    
    * Update block to be similar to other comment blocks
    
    * Escape attributes and format php
    
    Co-authored-by: Diede Exterkate <diede@yoast.com>
    Co-authored-by: Carolina Nymark <myazalea@hotmail.com>
    Co-authored-by: Carlos Bravo <carlos@ticloop.com>

--- lib/blocks.php
+++ lib/blocks.php
@@ -74,6 +74,7 @@ function gutenberg_reregister_core_block_types() {
 				'post-comment-author-avatar.php' => 'core/post-comment-author-avatar',
 				'post-comment-content.php'       => 'core/post-comment-content',
 				'post-comment-date.php'          => 'core/post-comment-date',
+				'post-comment-edit.php'          => 'core/post-comment-edit',
 				'post-comments.php'              => 'core/post-comments',
 				'post-comments-count.php'        => 'core/post-comments-count',
 				'post-comments-form.php'         => 'core/post-comments-form',
