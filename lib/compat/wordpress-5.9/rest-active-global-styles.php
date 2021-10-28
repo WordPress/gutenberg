b91a650052c9556fa110f12e8323c1ffff75cfa1
Riad Benguella <benguella@gmail.com>
2021-10-28 09:57:16 +0100

    Avoid duplicate queries

--- lib/compat/wordpress-5.9/rest-active-global-styles.php
+++ lib/compat/wordpress-5.9/rest-active-global-styles.php
@@ -14,27 +14,30 @@
 function gutenberg_add_active_global_styles_link( $response, $theme ) {
 	if ( $theme->get_stylesheet() === wp_get_theme()->get_stylesheet() ) {
 		// This creates a record for the current theme if not existant.
-		WP_Theme_JSON_Resolver_Gutenberg::get_user_custom_post_type_id();
+		$id = WP_Theme_JSON_Resolver_Gutenberg::get_user_custom_post_type_id();
+	} else {
+
+		$wp_query_args       = array(
+			'post_status'    => array( 'publish' ),
+			'post_type'      => 'wp_global_styles',
+			'posts_per_page' => 1,
+			'no_found_rows'  => true,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'wp_theme',
+					'field'    => 'name',
+					'terms'    => $theme->get_stylesheet(),
+				),
+			),
+		);
+		$global_styles_query = new WP_Query( $wp_query_args );
+		$id                  = count( $global_styles_query->posts ) ? $global_styles_query->posts[0]->ID : null;
 	}
 
-	$wp_query_args       = array(
-		'post_status'    => array( 'publish' ),
-		'post_type'      => 'wp_global_styles',
-		'posts_per_page' => 1,
-		'no_found_rows'  => true,
-		'tax_query'      => array(
-			array(
-				'taxonomy' => 'wp_theme',
-				'field'    => 'name',
-				'terms'    => $theme->get_stylesheet(),
-			),
-		),
-	);
-	$global_styles_query = new WP_Query( $wp_query_args );
-	if ( count( $global_styles_query->posts ) ) {
+	if ( $id ) {
 		$response->add_link(
 			'https://api.w.org/user-global-styles',
-			rest_url( 'wp/v2/global-styles/' . $global_styles_query->posts[0]->ID )
+			rest_url( 'wp/v2/global-styles/' . $id )
 		);
 	}
 
