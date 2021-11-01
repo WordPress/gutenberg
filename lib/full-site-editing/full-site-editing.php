ff6cfe0c0a631f37ca9e1e6cbc66aefdf7536b82
Carolina Nymark <hi@themesbycarolina.com>
2021-11-01 01:52:57 +0100

    Move and rename the site editor menu item (#36064)
    * Move and rename the site editor menu item
    
    * Replace toplevel_page_ with appearance_page_
    
    Replace toplevel_page_ with appearance_page_,
    remove menu order filter.

--- lib/full-site-editing/full-site-editing.php
+++ lib/full-site-editing/full-site-editing.php
@@ -75,7 +75,7 @@ function gutenberg_adminbar_items( $wp_admin_bar ) {
 			array(
 				'id'    => 'site-editor',
 				'title' => __( 'Edit site', 'gutenberg' ),
-				'href'  => admin_url( 'admin.php?page=gutenberg-edit-site' ),
+				'href'  => admin_url( 'themes.php?page=gutenberg-edit-site' ),
 			)
 		);
 	}
@@ -84,46 +84,13 @@ function gutenberg_adminbar_items( $wp_admin_bar ) {
 add_action( 'admin_bar_menu', 'gutenberg_adminbar_items', 50 );
 
 /**
- * Activates the 'menu_order' filter and then hooks into 'menu_order'
- */
-add_filter( 'custom_menu_order', '__return_true' );
-add_filter( 'menu_order', 'gutenberg_menu_order' );
-
-/**
- * Filters WordPress default menu order
- *
- * @param array $menu_order Menu Order.
- */
-function gutenberg_menu_order( $menu_order ) {
-	if ( ! gutenberg_is_fse_theme() ) {
-		return $menu_order;
-	}
-
-	$new_positions = array(
-		// Position the site editor before the appearance menu.
-		'gutenberg-edit-site' => array_search( 'themes.php', $menu_order, true ),
-	);
-
-	// Traverse through the new positions and move
-	// the items if found in the original menu_positions.
-	foreach ( $new_positions as $value => $new_index ) {
-		$current_index = array_search( $value, $menu_order, true );
-		if ( $current_index ) {
-			$out = array_splice( $menu_order, $current_index, 1 );
-			array_splice( $menu_order, $new_index, 0, $out );
-		}
-	}
-	return $menu_order;
-}
-
-/**
  * Tells the script loader to load the scripts and styles of custom block on site editor screen.
  *
  * @param bool $is_block_editor_screen Current decision about loading block assets.
  * @return bool Filtered decision about loading block assets.
  */
 function gutenberg_site_editor_load_block_editor_scripts_and_styles( $is_block_editor_screen ) {
-	return ( is_callable( 'get_current_screen' ) && get_current_screen() && 'toplevel_page_gutenberg-edit-site' === get_current_screen()->base )
+	return ( is_callable( 'get_current_screen' ) && get_current_screen() && 'appearance_page_gutenberg-edit-site' === get_current_screen()->base )
 		? true
 		: $is_block_editor_screen;
 }
