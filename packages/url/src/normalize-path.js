115dcd7bfcf6e7789001a4d0e09f003987b22b12
Mustaque Ahmed <amustaque97@gmail.com>
2021-11-02 21:55:48 +0530

    Refactor normalizePath function to sort query param in lexographical order
    This commit is a part of code imporvement. In this changes we are replacing
    manual sort with URLSearchParams interface sort function.

--- packages/url/src/normalize-path.js
+++ packages/url/src/normalize-path.js
@@ -15,20 +15,8 @@ export function normalizePath( path ) {
 		return base;
 	}
 
-	// 'b=1&c=2&a=5'
-	return (
-		base +
-		'?' +
-		query
-			// [ 'b=1', 'c=2', 'a=5' ]
-			.split( '&' )
-			// [ [ 'b, '1' ], [ 'c', '2' ], [ 'a', '5' ] ]
-			.map( ( entry ) => entry.split( '=' ) )
-			// [ [ 'a', '5' ], [ 'b, '1' ], [ 'c', '2' ] ]
-			.sort( ( a, b ) => a[ 0 ].localeCompare( b[ 0 ] ) )
-			// [ 'a=5', 'b=1', 'c=2' ]
-			.map( ( pair ) => pair.join( '=' ) )
-			// 'a=5&b=1&c=2'
-			.join( '&' )
-	);
+	const searchParams = new URLSearchParams( query );
+	searchParams.sort();
+
+	return `${ base }?${ searchParams.toString() }`;
 }
