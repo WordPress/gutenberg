c9ee028528f1aa17d50001dbfa9d6061b613ef42
Adam Zieliński <adam@adamziel.com>
2021-11-03 23:08:40 +0100

    Rely on contexts also in PHP navigation block's render callback

---
+++ packages/block-library/src/navigation-area/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Registers the `core/navigation-area` block on the server.
+ */
+function register_block_core_navigation_area() {
+	register_block_type_from_metadata(
+		__DIR__ . '/navigation-area',
+		array(
+			'provides_context' => array(
+				'navigationArea' => 'area',
+			),
+		)
+	);
+}
+add_action( 'init', 'register_block_core_navigation_area' );
