58bfaefe2df95eb9ffeb63c4b4dbb04a3c6a7792
Carolina Nymark <myazalea@hotmail.com>
2021-11-04 10:19:41 +0100

    [Block: Group] Transform to row variation

--- packages/block-library/src/group/variations.js
+++ packages/block-library/src/group/variations.js
@@ -5,11 +5,20 @@ import { __ } from '@wordpress/i18n';
 
 const variations = [
 	{
+		name: 'group',
+		title: __( 'Group' ),
+		description: __( 'Blocks shown in a column.' ),
+		attributes: { layout: { type: 'default' } },
+		scope: [ 'inserter', 'transform' ],
+		isActive: ( blockAttributes ) =>
+			blockAttributes.layout?.type === 'default',
+	},
+	{
 		name: 'group-row',
 		title: __( 'Row' ),
 		description: __( 'Blocks shown in a row.' ),
 		attributes: { layout: { type: 'flex', allowOrientation: false } },
-		scope: [ 'inserter' ],
+		scope: [ 'inserter', 'transform' ],
 		isActive: ( blockAttributes ) =>
 			blockAttributes.layout?.type === 'flex',
 	},
