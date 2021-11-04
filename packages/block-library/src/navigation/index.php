7e0dccb422603407f493a67425a8db32b55de68d
Michal Czaplinski <mmczaplinski@gmail.com>
2021-11-04 17:31:20 -0500

    Add the portal to js file

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -268,6 +268,9 @@ function render_block_core_navigation( $attributes, $content, $block ) {
 	if ( empty( $inner_blocks ) ) {
 		return '';
 	}
+
+	wp_enqueue_script( 'wp-block-navigation-portal' );
+
 	$colors     = block_core_navigation_build_css_colors( $attributes );
 	$font_sizes = block_core_navigation_build_css_font_sizes( $attributes );
 	$classes    = array_merge(
