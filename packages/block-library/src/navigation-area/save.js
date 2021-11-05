40349dbac032078f310ca355077a3cac036cc0a2
Daniel Richards <daniel.richards@automattic.com>
2021-11-05 16:09:32 +0800

    Remove div from save content

--- packages/block-library/src/navigation-area/save.js
+++ packages/block-library/src/navigation-area/save.js
@@ -1,12 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks } from '@wordpress/block-editor';
 
 export default function save() {
-	return (
-		<div { ...useBlockProps.save() }>
-			<InnerBlocks.Content />
-		</div>
-	);
+	return <InnerBlocks.Content />;
 }
