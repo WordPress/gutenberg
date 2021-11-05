d1b67979ea9140595341be63bd6075ab4dedb7fb
André <583546+oandregal@users.noreply.github.com>
2021-11-05 11:29:06 +0100

    Adds a setting property that enables some other ones

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -188,6 +188,75 @@ class WP_Theme_JSON_Gutenberg_Test extends WP_UnitTestCase {
 		$this->assertEqualSetsWithIndex( $expected_no_origin, $actual_no_origin );
 	}
 
+	function test_get_settings_using_opt_in_key() {
+		$theme_json = new WP_Theme_JSON_Gutenberg(
+			array(
+				'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
+				'settings' => array(
+					'appearance' => true,
+					'blocks'     => array(
+						'core/paragraph' => array(
+							'typography' => array(
+								'lineHeight' => false,
+							),
+						),
+						'core/group'     => array(
+							'appearance' => true,
+							'typography' => array(
+								'lineHeight' => false, // This is overridden by appearance.
+							),
+						),
+					),
+				),
+			)
+		);
+
+		$actual   = $theme_json->get_settings();
+		$expected = array(
+			'border'     => array(
+				'width'  => true,
+				'style'  => true,
+				'radius' => true,
+				'color'  => true,
+			),
+			'spacing'    => array(
+				'margin'  => true,
+				'padding' => true,
+				'units'   => true,
+			),
+			'typography' => array(
+				'customFontSize' => true,
+				'lineHeight'     => true,
+			),
+			'blocks'     => array(
+				'core/paragraph' => array(
+					'typography' => array(
+						'lineHeight' => false,
+					),
+				),
+				'core/group'     => array(
+					'border'     => array(
+						'width'  => true,
+						'style'  => true,
+						'radius' => true,
+						'color'  => true,
+					),
+					'spacing'    => array(
+						'margin'  => true,
+						'padding' => true,
+						'units'   => true,
+					),
+					'typography' => array(
+						'customFontSize' => true,
+						'lineHeight'     => true,
+					),
+				),
+			),
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, $actual );
+	}
+
 	function test_get_stylesheet_support_for_shorthand_and_longhand_values() {
 		$theme_json = new WP_Theme_JSON_Gutenberg(
 			array(
