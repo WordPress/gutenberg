0bec020930e14e227865881163d261c2061418b5
Sören Wrede <soerenwrede@gmail.com>
2021-11-05 16:04:51 +0100

    Fix content param type

--- packages/block-library/src/file/index.php
+++ packages/block-library/src/file/index.php
@@ -9,7 +9,7 @@
  * When the `core/file` block is rendering, check if we need to enqueue the `'wp-block-file-view` script.
  *
  * @param array $attributes The block attributes.
- * @param array $content    The block content.
+ * @param string $content    The block content.
  *
  * @return string Returns the block content.
  */
