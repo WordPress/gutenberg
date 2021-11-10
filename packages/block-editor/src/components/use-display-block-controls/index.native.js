32493653e1b2436e9dadd9c9a312ba5a879df807
Siobhan <siobhan@automattic.com>
2021-11-10 11:56:00 +0000

    [WIP] Remove unused multi-selection code
    Folllowing the previous commit, code that isn't going to be used on the apps is removed from the file that was previously copied/pasted. As the apps don't currently support multiple selection, it's not necessary to account for it when determining whether to display settings.

--- packages/block-editor/src/components/use-display-block-controls/index.native.js
+++ packages/block-editor/src/components/use-display-block-controls/index.native.js
@@ -7,30 +7,14 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import { useBlockEditContext } from '../block-edit/context';
-import { store as blockEditorStore } from '../../store';
 
 export default function useDisplayBlockControls() {
 	const { isSelected, clientId, name } = useBlockEditContext();
-	return useSelect(
-		( select ) => {
-			if ( isSelected ) {
-				return true;
-			}
+	return useSelect( () => {
+		if ( isSelected ) {
+			return true;
+		}
 
-			const {
-				getBlockName,
-				isFirstMultiSelectedBlock,
-				getMultiSelectedBlockClientIds,
-			} = select( blockEditorStore );
-
-			if ( isFirstMultiSelectedBlock( clientId ) ) {
-				return getMultiSelectedBlockClientIds().every(
-					( id ) => getBlockName( id ) === name
-				);
-			}
-
-			return false;
-		},
-		[ clientId, isSelected, name ]
-	);
+		return false;
+	}, [ clientId, isSelected, name ] );
 }
