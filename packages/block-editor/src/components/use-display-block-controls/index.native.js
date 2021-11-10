a23280cbc7323c9fb640e7cf5f5e3d7449566c16
Siobhan <siobhan@automattic.com>
2021-11-10 14:40:03 +0000

    Allow parent blocks to hide settings control
    By only displaying the settings control if '__experimentalExposeControlsToChildren' is true (which is the default), parent blocks can now hide the control by setting this block support to false.

--- packages/block-editor/src/components/use-display-block-controls/index.native.js
+++ packages/block-editor/src/components/use-display-block-controls/index.native.js
@@ -2,19 +2,37 @@
  * WordPress dependencies
  */
 import { useSelect } from '@wordpress/data';
+import { hasBlockSupport } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import { useBlockEditContext } from '../block-edit/context';
+import { store as blockEditorStore } from '../../store';
 
 export default function useDisplayBlockControls() {
 	const { isSelected, clientId, name } = useBlockEditContext();
-	return useSelect( () => {
-		if ( isSelected ) {
-			return true;
-		}
+	return useSelect(
+		( select ) => {
+			const { getBlockName, getBlockRootClientId } = select(
+				blockEditorStore
+			);
 
-		return false;
-	}, [ clientId, isSelected, name ] );
+			const parentId = getBlockRootClientId( clientId );
+			const parentBlockName = getBlockName( parentId );
+
+			const displayControls = hasBlockSupport(
+				parentBlockName,
+				'__experimentalExposeControlsToChildren',
+				true
+			);
+
+			if ( displayControls && isSelected ) {
+				return true;
+			}
+
+			return false;
+		},
+		[ clientId, isSelected, name ]
+	);
 }
