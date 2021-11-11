bd51447bcaf638854c19d247d0fffe6e86a8855e
James Koster <james@jameskoster.co.uk>
2021-11-11 12:29:11 +0000

    style and markup refactor

--- packages/edit-site/src/components/list/table.js
+++ packages/edit-site/src/components/list/table.js
@@ -4,7 +4,13 @@
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { __, sprintf } from '@wordpress/i18n';
-import { VisuallyHidden, Button } from '@wordpress/components';
+import {
+	VisuallyHidden,
+	Button,
+	FlexItem,
+	__experimentalHStack as HStack,
+	__experimentalHeading as Heading,
+} from '@wordpress/components';
 import { moreVertical } from '@wordpress/icons';
 import { addQueryArgs } from '@wordpress/url';
 
@@ -46,22 +52,23 @@ export default function Table( { templateType } ) {
 	}
 
 	return (
-		<table className="edit-site-list-table">
-			<thead>
-				<tr>
-					<th>{ __( 'Name' ) }</th>
-					<th>{ __( 'Description' ) }</th>
-					<th>{ __( 'Theme' ) }</th>
-					<th>
-						<VisuallyHidden>{ __( 'Actions' ) }</VisuallyHidden>
-					</th>
-				</tr>
-			</thead>
+		<ul className="edit-site-list-table">
+			<HStack className="edit-site-list-table-head" as="li">
+				<FlexItem className="edit-site-list-table-column">
+					<Heading level={ 4 }>{ __( 'Template' ) }</Heading>
+				</FlexItem>
+				<FlexItem className="edit-site-list-table-column">
+					<Heading level={ 4 }>{ __( 'Added by' ) }</Heading>
+				</FlexItem>
+				<FlexItem className="edit-site-list-table-column">
+					<VisuallyHidden>{ __( 'Actions' ) }</VisuallyHidden>
+				</FlexItem>
+			</HStack>
 
-			<tbody>
-				{ templates.map( ( template ) => (
-					<tr key={ template.id }>
-						<td>
+			{ templates.map( ( template ) => (
+				<li key={ template.id }>
+					<HStack className="edit-site-list-table-row">
+						<FlexItem className="edit-site-list-table-column">
 							<a
 								href={ addQueryArgs( '', {
 									page: 'gutenberg-edit-site',
@@ -71,19 +78,22 @@ export default function Table( { templateType } ) {
 							>
 								{ template.title.rendered }
 							</a>
-						</td>
-						<td>{ template.description }</td>
-						<td>{ template.theme }</td>
-						<td>
+							{ template.description }
+						</FlexItem>
+
+						<FlexItem className="edit-site-list-table-column">
+							{ template.theme }
+						</FlexItem>
+						<FlexItem className="edit-site-list-table-column">
 							<Button
 								icon={ moreVertical }
 								label={ __( 'Actions' ) }
 								iconSize={ 24 }
 							/>
-						</td>
-					</tr>
-				) ) }
-			</tbody>
-		</table>
+						</FlexItem>
+					</HStack>
+				</li>
+			) ) }
+		</ul>
 	);
 }
