1d4e8e83f76b28b694913e40d04af4e5af51d587
George Mamadashvili <georgemamadashvili@gmail.com>
2021-11-12 15:26:52 +0400

    Use get_file_path_from_theme method

--- lib/class-wp-theme-json-resolver-gutenberg.php
+++ lib/class-wp-theme-json-resolver-gutenberg.php
@@ -347,8 +347,8 @@ class WP_Theme_JSON_Resolver_Gutenberg {
 	public static function theme_has_support() {
 		if ( ! isset( self::$theme_has_support ) ) {
 			self::$theme_has_support = (
-				is_readable( get_stylesheet_directory() . '/theme.json' ) ||
-				is_readable( get_template_directory() . '/theme.json' )
+				is_readable( self::get_file_path_from_theme( 'theme.json' ) ) ||
+				is_readable( self::get_file_path_from_theme( 'theme.json', true ) )
 			);
 		}
 
