33c5293516eaf82e7f5011811099f09407e238d5
Dave Smith <getdavemail@gmail.com>
2021-11-15 16:41:30 +0000

    Expose page on front id in editor settings

--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -35,12 +35,18 @@ function useBlockEditorSettings( settings, hasTemplate ) {
 		hasUploadPermissions,
 		canUseUnfilteredHTML,
 		userCanCreatePages,
+		pageOnFront,
 	} = useSelect( ( select ) => {
 		const { canUserUseUnfilteredHTML } = select( editorStore );
 		const isWeb = Platform.OS === 'web';
-		const { canUser, getUnstableBase, hasFinishedResolution } = select(
-			coreStore
-		);
+		const {
+			canUser,
+			getUnstableBase,
+			hasFinishedResolution,
+			getEntityRecord,
+		} = select( coreStore );
+
+		const siteSettings = getEntityRecord( 'root', 'site' );
 
 		const siteData = getUnstableBase();
 
@@ -64,6 +70,7 @@ function useBlockEditorSettings( settings, hasTemplate ) {
 			hasResolvedLocalSiteData: hasFinishedResolvingSiteData,
 			baseUrl: siteData?.url || '',
 			userCanCreatePages: canUser( 'create', 'pages' ),
+			pageOnFront: siteSettings?.page_on_front,
 		};
 	}, [] );
 
@@ -139,6 +146,7 @@ function useBlockEditorSettings( settings, hasTemplate ) {
 			outlineMode: hasTemplate,
 			__experimentalCreatePageEntity: createPageEntity,
 			__experimentalUserCanCreatePages: userCanCreatePages,
+			pageOnFront,
 		} ),
 		[
 			settings,
@@ -148,6 +156,7 @@ function useBlockEditorSettings( settings, hasTemplate ) {
 			undo,
 			hasTemplate,
 			userCanCreatePages,
+			pageOnFront,
 		]
 	);
 }
