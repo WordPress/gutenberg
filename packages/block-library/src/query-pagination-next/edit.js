8b659ab80a74beec4fb20133ac7b7cea042098a5
Carlos Bravo <carlos@ticloop.com>
2021-11-15 17:02:02 +0100

    Update fixtures

--- packages/block-library/src/query-pagination-next/edit.js
+++ packages/block-library/src/query-pagination-next/edit.js
@@ -17,28 +17,29 @@ export default function QueryPaginationNextEdit( {
 } ) {
 	const displayArrow = arrowMap[ paginationArrow ];
 	return (
-		<a
-			href="#pagination-next-pseudo-link"
-			onClick={ ( event ) => event.preventDefault() }
-			{ ...useBlockProps() }
-		>
-			<PlainText
-				__experimentalVersion={ 2 }
-				tagName="span"
-				aria-label={ __( 'Next page link' ) }
-				placeholder={ __( 'Next Page' ) }
-				value={ label }
-				onChange={ ( newLabel ) =>
-					setAttributes( { label: newLabel } )
-				}
-			/>
-			{ displayArrow && (
-				<span
-					className={ `wp-block-query-pagination-next-arrow is-arrow-${ paginationArrow }` }
-				>
-					{ displayArrow }
-				</span>
-			) }
-		</a>
+		<div { ...useBlockProps() }>
+			<a
+				href="#pagination-next-pseudo-link"
+				onClick={ ( event ) => event.preventDefault() }
+			>
+				<PlainText
+					__experimentalVersion={ 2 }
+					tagName="span"
+					aria-label={ __( 'Next page link' ) }
+					placeholder={ __( 'Next Page' ) }
+					value={ label }
+					onChange={ ( newLabel ) =>
+						setAttributes( { label: newLabel } )
+					}
+				/>
+				{ displayArrow && (
+					<span
+						className={ `wp-block-query-pagination-next-arrow is-arrow-${ paginationArrow }` }
+					>
+						{ displayArrow }
+					</span>
+				) }
+			</a>
+		</div>
 	);
 }
