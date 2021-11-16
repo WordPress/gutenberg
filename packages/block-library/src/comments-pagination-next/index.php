59bfce9bab39dacee4bd6caeb60f3096cb03200b
Carlos Bravo <carlos@ticloop.com>
2021-11-16 09:08:17 +0100

    Update PHP code

--- packages/block-library/src/comments-pagination-next/index.php
+++ packages/block-library/src/comments-pagination-next/index.php
@@ -19,12 +19,15 @@ function render_block_core_comments_pagination_next( $attributes, $content, $blo
 	$comments_number   = (int) get_comments_number();
 	$max_page          = (int) floor( $comments_number / $comments_per_page );
 
-	$default_label      = __( 'Next Comments' );
-	$label              = isset( $attributes['label'] ) && ! empty( $attributes['label'] ) ? $attributes['label'] : $default_label;
-	$arrow              = isset( $block->context['paginationArrow'] ) ? $block->context['paginationArrow'] : '';
-	$next_comments_link = get_next_comments_link( sprintf( '%1s %2s', $label, $arrow ), $max_page );
+	$default_label    = __( 'Next Comments' );
+	$label            = isset( $attributes['label'] ) && ! empty( $attributes['label'] ) ? $attributes['label'] : $default_label;
+	$pagination_arrow = get_query_pagination_arrow( $block, true );
+	if ( $pagination_arrow ) {
+		$label .= $pagination_arrow;
+	}
+	$next_comments_link = get_next_comments_link( $label, $max_page );
 
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'wp-block-next-comments-link' ) );
+	$wrapper_attributes = get_block_wrapper_attributes();
 
 	if ( ! isset( $next_comments_link ) ) {
 		return '';
