3853d34ce10f8c7d5085929077f19dbf495db082
David Arenas <darensr@gmail.com>
2021-11-17 17:41:34 +0100

    Block Library: Fix Comments Pagination's index.php

--- packages/block-library/src/comments-pagination/index.php
+++ packages/block-library/src/comments-pagination/index.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Server-side rendering of the `core/query-pagination` block.
+ * Server-side rendering of the `core/comments-pagination` block.
  *
  * @package WordPress
  */
 
 /**
- * Renders the `core/query-pagination` block on the server.
+ * Renders the `core/comments-pagination` block on the server.
  *
  * @param array  $attributes Block attributes.
  * @param string $content    Block default content.
  *
- * @return string Returns the wrapper for the Query pagination.
+ * @return string Returns the wrapper for the Comments pagination.
  */
 function render_block_core_comments_pagination( $attributes, $content ) {
 	if ( empty( trim( $content ) ) ) {
@@ -26,11 +26,11 @@ function render_block_core_comments_pagination( $attributes, $content ) {
 }
 
 /**
- * Registers the `core/query-pagination` block on the server.
+ * Registers the `core/comments-pagination` block on the server.
  */
 function register_block_core_comments_pagination() {
 	register_block_type_from_metadata(
-		__DIR__ . '/query-pagination',
+		__DIR__ . '/comments-pagination',
 		array(
 			'render_callback' => 'render_block_core_comments_pagination',
 		)
