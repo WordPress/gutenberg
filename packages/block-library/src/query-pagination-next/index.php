9218edccd495697380d58f9b6315104ac0baeddb
Matias Benedetto <matias.benedetto@gmail.com>
2021-11-19 14:23:21 -0300

    Query pagination next: removing custom 'last-page' class.
    We can target the same element and know when we are navigating the last page with a selector like this: "span.wp-block-query-pagination-next"

--- packages/block-library/src/query-pagination-next/index.php
+++ packages/block-library/src/query-pagination-next/index.php
@@ -17,6 +17,7 @@
 function render_block_core_query_pagination_next( $attributes, $content, $block ) {
 	$page_key = isset( $block->context['queryId'] ) ? 'query-' . $block->context['queryId'] . '-page' : 'query-page';
 	$page     = empty( $_GET[ $page_key ] ) ? 1 : (int) $_GET[ $page_key ];
+	$paged    = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 	$max_page = isset( $block->context['query']['pages'] ) ? (int) $block->context['query']['pages'] : 0;
 
 	$wrapper_attributes = get_block_wrapper_attributes();
@@ -26,11 +27,9 @@ function render_block_core_query_pagination_next( $attributes, $content, $block 
 	if ( $pagination_arrow ) {
 		$label .= $pagination_arrow;
 	}
-	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-	if( (int) $wp_query->max_num_pages === $paged ){
-		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'last-page' ) );
-	}
-	$content = sprintf(
+
+	$wrapper_attributes = get_block_wrapper_attributes();
+	$content            = sprintf(
 		'<span %1$s>%2$s</span>',
 		$wrapper_attributes,
 		$label
