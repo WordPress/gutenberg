246c6b3f85244a34e7de5b9d0b9265d6ec1fb490
Daniel Richards <daniel.richards@automattic.com>
2021-11-22 11:36:42 +0800

    Implement shortcuts and labels

--- packages/edit-site/src/components/list/index.js
+++ packages/edit-site/src/components/list/index.js
@@ -1,25 +1,58 @@
 /**
  * WordPress dependencies
  */
+import { store as coreStore } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
 import { InterfaceSkeleton } from '@wordpress/interface';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useViewportMatch } from '@wordpress/compose';
+import { store as keyboardShortcutsStore } from '@wordpress/keyboard-shortcuts';
 
 /**
  * Internal dependencies
  */
+import useRegisterShortcuts from './use-register-shortcuts';
 import Header from './header';
 import NavigationSidebar from '../navigation-sidebar';
 import Table from './table';
 
 export default function List( { templateType } ) {
+	useRegisterShortcuts();
 	const isDesktopViewport = useViewportMatch( 'medium' );
+
+	const { previousShortcut, nextShortcut } = useSelect( ( select ) => {
+		return {
+			previousShortcut: select(
+				keyboardShortcutsStore
+			).getAllShortcutKeyCombinations( 'core/edit-site/previous-region' ),
+			nextShortcut: select(
+				keyboardShortcutsStore
+			).getAllShortcutKeyCombinations( 'core/edit-site/next-region' ),
+		};
+	}, [] );
+
+	const postType = useSelect(
+		( select ) => select( coreStore ).getPostType( templateType ),
+		[ templateType ]
+	);
+
+	const itemsListLabel = postType?.labels?.items_list;
 
 	return (
 		<InterfaceSkeleton
 			className="edit-site-list"
 			labels={ {
+				header: sprintf(
+					// translators: %s - the name of the page, 'Header' as in the header area of that page.
+					__( '%s - Header' ),
+					itemsListLabel
+				),
 				drawer: __( 'Navigation Sidebar' ),
+				content: sprintf(
+					// translators: %s - the name of the page, 'Content' as in the content area of that page.
+					__( '%s - Content' ),
+					itemsListLabel
+				),
 			} }
 			header={ <Header templateType={ templateType } /> }
 			drawer={
@@ -33,6 +66,10 @@ export default function List( { templateType } ) {
 					<Table templateType={ templateType } />
 				</main>
 			}
+			shortcuts={ {
+				previous: previousShortcut,
+				next: nextShortcut,
+			} }
 		/>
 	);
 }
