6d461e11199b765eca64576dc18886aaf0f10328
Michal Czaplinski <mmczaplinski@gmail.com>
2021-11-24 18:30:43 -0500

    Comments Template: Add better comments

--- packages/block-library/src/comment-template/util.js
+++ packages/block-library/src/comment-template/util.js
@@ -1,3 +1,30 @@
+/**
+ *
+ * This function converts a list of comment objects with a `parent` property to
+ * a list of comment objects with a `children` property. The `children` property
+ * is itself a list of comment objects.
+ *
+ * @example
+ * ```
+ * const comments = [
+ *		{ commentId: 1, parent: 0 },
+ *		{ commentId: 2, parent: 1 },
+ *		{ commentId: 3, parent: 2 },
+ *		{ commentId: 4, parent: 1 },
+ *	]
+ * expect( convertToTree( comments ) ).toEqual( [
+ *	{
+ *		commentId: 1,
+ *		children: [
+ *			{ commentId: 2, children: [ { commentId: 3, children: [] } ] },
+ *			{ commentId: 4, children: [] },
+ *			],
+ *		},
+ *	] );
+ * ```
+ *
+ * @param {Array} data - List of comment objects
+ */
 export const convertToTree = ( data ) => {
 	const table = {};
 	if ( ! data ) return [];
