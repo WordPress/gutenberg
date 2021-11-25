f51394f84056e0a59592f67e922892ac11281780
Dave Smith <getdavemail@gmail.com>
2021-11-25 12:02:33 +0000

    Don't try and render unstable location if Nav block has ID

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -254,7 +254,7 @@ function render_block_core_navigation( $attributes, $content, $block ) {
 	$inner_blocks = $block->inner_blocks;
 
 	// If `__unstableLocation` is defined, create inner blocks from the classic menu assigned to that location.
-	if ( empty( $inner_blocks ) && array_key_exists( '__unstableLocation', $attributes ) ) {
+	if ( ! array_key_exists( 'navigationMenuId', $attributes ) && array_key_exists( '__unstableLocation', $attributes ) ) {
 		$menu_items = gutenberg_get_menu_items_at_location( $attributes['__unstableLocation'] );
 		if ( empty( $menu_items ) ) {
 			return '';
