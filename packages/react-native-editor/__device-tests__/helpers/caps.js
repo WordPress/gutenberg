c269a1dda80916f6bf2b1f7d01666e4e88f9a658
Carlos Garcia <fluiddot@gmail.com>
2021-11-25 11:36:16 +0100

    Use iOS 15.0 version on E2E tests (GB-mobile)

--- packages/react-native-editor/__device-tests__/helpers/caps.js
+++ packages/react-native-editor/__device-tests__/helpers/caps.js
@@ -4,7 +4,7 @@ const ios = {
 	os: 'iOS',
 	deviceOrientation: 'portrait',
 	automationName: 'XCUITest',
-	appiumVersion: '1.20.1', // Sauce Labs requires appiumVersion to be specified.
+	appiumVersion: '1.22.0', // Sauce Labs requires appiumVersion to be specified.
 	app: undefined, // will be set later, locally this is relative to root of project
 	processArguments: {
 		args: [ 'uitesting' ],
@@ -20,7 +20,7 @@ exports.iosLocal = {
 
 exports.iosServer = {
 	...ios,
-	platformVersion: '14.5', // Supported Sauce Labs platforms can be found here: https://saucelabs.com/rest/v1/info/platforms/appium
+	platformVersion: '15.0', // Supported Sauce Labs platforms can be found here: https://saucelabs.com/rest/v1/info/platforms/appium
 	deviceName: 'iPhone 11 Simulator',
 };
 
