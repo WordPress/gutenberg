b77ecce61a013c2ab422be92b1b4b4108f40308d
Adam Zieliński <adam@adamziel.com>
2021-11-26 12:46:24 +0100

    Only replace innerBlocks with an empty list, if it's not already empty

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -282,9 +282,11 @@ function Navigation( {
 	// If the ref no longer exists the reset the inner blocks
 	// to provide a clean slate.
 	useEffect( () => {
-		if ( ref === undefined ) {
+		if ( ref === undefined && innerBlocks.length > 0 ) {
 			replaceInnerBlocks( clientId, [] );
 		}
+		// innerBlocks are intentionally not listed as deps. This function is only concerned
+		// with the snapshot from the time when ref became undefined.
 	}, [ clientId, ref ] );
 
 	const startWithEmptyMenu = useCallback( () => {
