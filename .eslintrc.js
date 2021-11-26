fc7e3db200567778533aa82812bb6de35bc8d64a
Thomas Roberts <thomas.roberts@automattic.com>
2021-11-26 10:31:49 +0000

    Undo changes to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,7 +54,6 @@ module.exports = {
 	rules: {
 		'jest/expect-expect': 'off',
 		'@wordpress/dependency-group': 'error',
-		'@wordpress/comment-case': 'warn',
 		'@wordpress/gutenberg-phase': 'error',
 		'@wordpress/react-no-unsafe-timeout': 'error',
 		'@wordpress/i18n-text-domain': [
