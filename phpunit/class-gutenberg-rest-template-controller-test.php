7676a59e4fc947966d36511aedd31e9de57278d3
Adam Zielinski <adam@adamziel.com>
2021-11-26 16:06:36 +0100

    [REST] Restore the missing double slash in the ID received by /templates (#36881)
    * Fall back to a double-slashed template name if a single-slashed one is missing
    
    * Add a unit test
    
    * Use $_SERVER['REQUEST_URI'] as a fallback instead of simply replacing all slashes with a double slash.
    
    * Fallback to parsing the query string instead of doing it by default
    
    * Code style
    
    * Update docstring
    
    * Try doubling the last slash instead of parsing REQUEST_URI
    
    * Use sanitize_callback
    
    * Lint
    
    * Test for both single and double slash
    
    * Add unit tests for _sanitize_template_id
    
    * Update phpunit/class-gutenberg-rest-template-controller-test.php
    
    * Update phpunit/class-gutenberg-rest-template-controller-test.php

--- phpunit/class-gutenberg-rest-template-controller-test.php
+++ phpunit/class-gutenberg-rest-template-controller-test.php
@@ -160,6 +160,76 @@ class Gutenberg_REST_Templates_Controller_Test extends WP_Test_REST_Controller_T
 		);
 	}
 
+	/**
+	 * Ticket 54507
+	 *
+	 * @dataProvider get_template_endpoint_urls
+	 */
+	public function test_get_item_works_with_a_single_slash( $endpoint_url ) {
+		wp_set_current_user( self::$admin_id );
+		$request  = new WP_REST_Request( 'GET', $endpoint_url );
+		$response = rest_get_server()->dispatch( $request );
+
+		$data = $response->get_data();
+		unset( $data['content'] );
+		unset( $data['_links'] );
+
+		$this->assertEquals(
+			array(
+				'id'             => 'tt1-blocks//index',
+				'theme'          => 'tt1-blocks',
+				'slug'           => 'index',
+				'title'          => array(
+					'raw'      => 'Index',
+					'rendered' => 'Index',
+				),
+				'description'    => 'The default template used when no other template is available. This is a required template in WordPress.',
+				'status'         => 'publish',
+				'source'         => 'theme',
+				'type'           => 'wp_template',
+				'wp_id'          => null,
+				'has_theme_file' => true,
+			),
+			$data
+		);
+	}
+
+	/**
+	 *
+	 */
+	public function get_template_endpoint_urls() {
+		return array(
+			array( '/wp/v2/templates/tt1-blocks/index' ),
+			array( '/wp/v2/templates/tt1-blocks//index' ),
+		);
+	}
+
+	/**
+	 * Ticket 54507
+	 *
+	 * @dataProvider get_template_ids_to_sanitize
+	 */
+	public function test_sanitize_template_id( $input_id, $sanitized_id ) {
+		$endpoint = new Gutenberg_REST_Templates_Controller( 'wp_template' );
+		$this->assertEquals(
+			$sanitized_id,
+			$endpoint->_sanitize_template_id( $input_id )
+		);
+	}
+
+	/**
+	 *
+	 */
+	public function get_template_ids_to_sanitize() {
+		return array(
+			array( 'tt1-blocks/index', 'tt1-blocks//index' ),
+			array( 'tt1-blocks//index', 'tt1-blocks//index' ),
+
+			array( 'theme-experiments/tt1-blocks/index', 'theme-experiments/tt1-blocks//index' ),
+			array( 'theme-experiments/tt1-blocks//index', 'theme-experiments/tt1-blocks//index' ),
+		);
+	}
+
 	public function test_create_item() {
 		wp_set_current_user( self::$admin_id );
 		$request = new WP_REST_Request( 'POST', '/wp/v2/templates' );
