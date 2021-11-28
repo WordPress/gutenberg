7886d4f99c16b233a0c6fe6e7b3eb49d98989193
Glen Davies <glen.davies@a8c.com>
2021-11-29 11:10:06 +1300

    Use the existing react testing library and a mock component instead of the react testing hooks library

--- packages/block-library/src/gallery/test/use-get-new-images.js
+++ packages/block-library/src/gallery/test/use-get-new-images.js
@@ -1,91 +1,93 @@
 /**
  * External dependencies
  */
-import { renderHook } from '@testing-library/react-hooks';
+import { render, screen } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
 import useGetNewImages from '../use-get-new-images';
 
+const TestComponent = ( { images, imageData } ) => {
+	const newImages = useGetNewImages( images, imageData );
+	return <div title="testResult">{ JSON.stringify( newImages ) }</div>;
+};
+
 describe( 'gallery block useGetNewImages hook', () => {
-	let hook;
-	beforeEach( () => {
-		hook = renderHook(
-			( { images, imageData } ) => useGetNewImages( images, imageData ),
-			{
-				initialProps: { images: [ { test: 1 } ], imageData: [] },
-			}
-		);
-	} );
-
 	it( 'returns null if no images currently in the gallery', () => {
-		expect( hook.result.current ).toEqual( null );
+		render( <TestComponent images={ [] } imageData={ [] } /> );
+		expect( screen.getByTitle( 'testResult' ) ).toHaveTextContent( 'null' );
 	} );
 
-	it( 'returns an empty array if the only images are those loaded from post content', () => {
-		hook.rerender( {
-			images: [ { clientId: 'abc123', id: 1, fromSavedContent: true } ],
-			imageData: [ { id: 1 } ],
-		} );
-
-		expect( hook.result.current ).toEqual( null );
+	it( 'should not return images that have been loaded from the saved post content', () => {
+		render(
+			<TestComponent
+				images={ [
+					{ clientId: 'abc123', id: 1, fromSavedContent: true },
+				] }
+				imageData={ [ { id: 1 } ] }
+			/>
+		);
+		expect( screen.getByTitle( 'testResult' ) ).toHaveTextContent( 'null' );
 	} );
 
 	it( 'returns array of new images that have been added since the last render', () => {
-		hook.rerender( {
-			images: [ { clientId: 'abc123', id: 1 } ],
-			imageData: [ { id: 1 } ],
-		} );
+		const { rerender } = render(
+			<TestComponent
+				images={ [ { clientId: 'abc123', id: 1 } ] }
+				imageData={ [ { id: 1 } ] }
+			/>
+		);
 
-		expect( hook.result.current ).toEqual( [
-			{ clientId: 'abc123', id: 1 },
-		] );
+		expect( screen.getByTitle( 'testResult' ) ).toHaveTextContent(
+			'[{"clientId":"abc123","id":1}]'
+		);
 
-		hook.rerender( {
-			images: [
-				{ clientId: 'abc123', id: 1 },
-				{ clientId: 'efg456', id: 2 },
-			],
-			imageData: [ { id: 1 }, { id: 2 } ],
-		} );
+		rerender(
+			<TestComponent
+				images={ [
+					{ clientId: 'abc123', id: 1 },
+					{ clientId: 'efg456', id: 2 },
+				] }
+				imageData={ [ { id: 1 }, { id: 2 } ] }
+			/>
+		);
 
-		expect( hook.result.current ).toEqual( [
-			{ clientId: 'efg456', id: 2 },
-		] );
+		expect( screen.getByTitle( 'testResult' ) ).toHaveTextContent(
+			'[{"clientId":"efg456","id":2}]'
+		);
 	} );
 
 	it( 'sees an image as new if it has been deleted and added again', () => {
-		hook.rerender( {
-			images: [
-				{ clientId: 'abc123', id: 1 },
-				{ clientId: 'efg456', id: 2 },
-			],
-			imageData: [ { id: 1 }, { id: 2 } ],
-		} );
+		const { rerender } = render(
+			<TestComponent
+				images={ [
+					{ clientId: 'abc123', id: 1 },
+					{ clientId: 'efg456', id: 2 },
+				] }
+				imageData={ [ { id: 1 }, { id: 2 } ] }
+			/>
+		);
 
-		expect( hook.result.current ).toEqual( [
-			{ clientId: 'abc123', id: 1 },
-			{ clientId: 'efg456', id: 2 },
-		] );
+		rerender(
+			<TestComponent
+				images={ [ { clientId: 'abc123', id: 1 } ] }
+				imageData={ [ { id: 1 } ] }
+			/>
+		);
 
-		hook.rerender( {
-			images: [ { clientId: 'abc123', id: 1 } ],
-			imageData: [ { id: 1 } ],
-		} );
+		rerender(
+			<TestComponent
+				images={ [
+					{ clientId: 'abc123', id: 1 },
+					{ clientId: 'efg456', id: 2 },
+				] }
+				imageData={ [ { id: 1 }, { id: 2 } ] }
+			/>
+		);
 
-		expect( hook.result.current ).toEqual( null );
-
-		hook.rerender( {
-			images: [
-				{ clientId: 'abc123', id: 1 },
-				{ clientId: 'efg456', id: 2 },
-			],
-			imageData: [ { id: 1 }, { id: 2 } ],
-		} );
-
-		expect( hook.result.current ).toEqual( [
-			{ clientId: 'efg456', id: 2 },
-		] );
+		expect( screen.getByTitle( 'testResult' ) ).toHaveTextContent(
+			'[{"clientId":"efg456","id":2}]'
+		);
 	} );
 } );
