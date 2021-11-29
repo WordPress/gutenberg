cb7c810f463e33704c93b8ab3831ff8dd434bd91
André <583546+oandregal@users.noreply.github.com>
2021-11-29 22:25:39 +0100

    Port changes from WordPress core and register GS CPT in its own function

---
+++ lib/compat/wordpress-5.9/register-global-styles-cpt.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Registers a Custom Post Type to store the user's origin config.
+ *
+ * This has been ported to src/wp-includes/post.php
+ */
+function register_global_styles_custom_post_type() {
+	$args = array(
+		'label'        => __( 'Global Styles', 'gutenberg' ),
+		'description'  => 'CPT to store user design tokens',
+		'public'       => false,
+		'show_ui'      => false,
+		'show_in_rest' => false,
+		'rewrite'      => false,
+		'capabilities' => array(
+			'read'                   => 'edit_theme_options',
+			'create_posts'           => 'edit_theme_options',
+			'edit_posts'             => 'edit_theme_options',
+			'edit_published_posts'   => 'edit_theme_options',
+			'delete_published_posts' => 'edit_theme_options',
+			'edit_others_posts'      => 'edit_theme_options',
+			'delete_others_posts'    => 'edit_theme_options',
+		),
+		'map_meta_cap' => true,
+		'supports'     => array(
+			'title',
+			'editor',
+			'revisions',
+		),
+	);
+	register_post_type( 'wp_global_styles', $args );
+}
