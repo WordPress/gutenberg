604561d8be21097925499022de3da5b6cfb89f6d
Carlos Bravo <37012961+c4rl0sbr4v0@users.noreply.github.com>
2021-11-30 19:38:28 +0100

    Block Library: Add the Comments Pagination block (#36872)
    * Block Library: Add new Comments Pagination block
    
    * Block Library: Fix Comments Pagination's index.php
    
    * Block Library: Add $schema to Comments Pagination
    
    * Block Library: Add Comments Pagination fixture
    
    * Hide the component until we have the templates ready
    
    * Block Library: Fix Comments Pagination block's title
    
    Co-authored-by: Greg Ziółkowski <grzegorz@gziolo.pl>
    
    * Block Library: Remove unnecessary CSS classes
    
    Co-authored-by: Carlos Bravo <hey@carlosbravo.me>
    Co-authored-by: Greg Ziółkowski <grzegorz@gziolo.pl>
    Co-authored-by: David Arenas <david.arenas@automattic.com>

---
+++ packages/block-library/src/comments-pagination/edit.js
@@ -0,0 +1,70 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	InspectorControls,
+	useBlockProps,
+	useInnerBlocksProps,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { getBlockSupport } from '@wordpress/blocks';
+import { PanelBody } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { CommentsPaginationArrowControls } from './comments-pagination-arrow-controls';
+
+const getDefaultBlockLayout = ( blockTypeOrName ) => {
+	const layoutBlockSupportConfig = getBlockSupport(
+		blockTypeOrName,
+		'__experimentalLayout'
+	);
+	return layoutBlockSupportConfig?.default;
+};
+
+export default function QueryPaginationEdit( {
+	attributes: { paginationArrow, layout },
+	setAttributes,
+	clientId,
+	name,
+} ) {
+	const usedLayout = layout || getDefaultBlockLayout( name );
+	const hasNextPreviousBlocks = useSelect( ( select ) => {
+		const { getBlocks } = select( blockEditorStore );
+		const innerBlocks = getBlocks( clientId );
+		/**
+		 * Show the `paginationArrow` control only if a
+		 * `QueryPaginationNext/Previous` block exists.
+		 */
+		return innerBlocks?.find( ( innerBlock ) => {
+			return [
+				'core/query-pagination-next',
+				'core/query-pagination-previous',
+			].includes( innerBlock.name );
+		} );
+	}, [] );
+	const blockProps = useBlockProps();
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		__experimentalLayout: usedLayout,
+	} );
+	return (
+		<>
+			{ hasNextPreviousBlocks && (
+				<InspectorControls>
+					<PanelBody title={ __( 'Settings' ) }>
+						<CommentsPaginationArrowControls
+							value={ paginationArrow }
+							onChange={ ( value ) => {
+								setAttributes( { paginationArrow: value } );
+							} }
+						/>
+					</PanelBody>
+				</InspectorControls>
+			) }
+			<div { ...innerBlocksProps } />
+		</>
+	);
+}
