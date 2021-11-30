c8d14bd50d373ad491229c1197cb9908114b0adc
ntsekouras <ntsekouras@outlook.com>
2021-11-30 20:29:49 +0200

    [Block Library - Navigation]: Fix vertical layout

--- lib/block-supports/layout.php
+++ lib/block-supports/layout.php
@@ -96,6 +96,9 @@ function gutenberg_get_layout_style( $selector, $layout, $has_block_gap_support 
 		$style .= "flex-wrap: $flex_wrap;";
 		if ( 'horizontal' === $layout_orientation ) {
 			$style .= 'align-items: center;';
+			if ( ! empty( $layout['setCascadingProperties'] ) && $layout['setCascadingProperties'] ) {
+				$style .= '--layout-direction: row;';
+			}
 			/**
 			 * Add this style only if is not empty for backwards compatibility,
 			 * since we intend to convert blocks that had flex layout implemented
@@ -106,7 +109,6 @@ function gutenberg_get_layout_style( $selector, $layout, $has_block_gap_support 
 				if ( ! empty( $layout['setCascadingProperties'] ) && $layout['setCascadingProperties'] ) {
 					// --layout-justification-setting allows children to inherit the value regardless or row or column direction.
 					$style .= "--layout-justification-setting: {$justify_content_options[ $layout['justifyContent'] ]};";
-					$style .= '--layout-direction: row;';
 					$style .= "--layout-wrap: $flex_wrap;";
 					$style .= "--layout-justify: {$justify_content_options[ $layout['justifyContent'] ]};";
 					$style .= '--layout-align: center;';
@@ -114,12 +116,14 @@ function gutenberg_get_layout_style( $selector, $layout, $has_block_gap_support 
 			}
 		} else {
 			$style .= 'flex-direction: column;';
+			if ( ! empty( $layout['setCascadingProperties'] ) && $layout['setCascadingProperties'] ) {
+				$style .= '--layout-direction: column;';
+			}
 			if ( ! empty( $layout['justifyContent'] ) && array_key_exists( $layout['justifyContent'], $justify_content_options ) ) {
 				$style .= "align-items: {$justify_content_options[ $layout['justifyContent'] ]};";
 				if ( ! empty( $layout['setCascadingProperties'] ) && $layout['setCascadingProperties'] ) {
 					// --layout-justification-setting allows children to inherit the value regardless or row or column direction.
 					$style .= "--layout-justification-setting: {$justify_content_options[ $layout['justifyContent'] ]};";
-					$style .= '--layout-direction: column;';
 					$style .= '--layout-justify: initial;';
 					$style .= "--layout-align: {$justify_content_options[ $layout['justifyContent'] ]};";
 				}
