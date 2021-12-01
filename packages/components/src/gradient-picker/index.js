3b24d1715f8114cb3cbda06005fc77c8667307f9
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2021-12-01 15:16:48 +1100

    Simplify fix by moving check to where we determine multiple origins, add storybook and changelog entries

--- packages/components/src/gradient-picker/index.js
+++ packages/components/src/gradient-picker/index.js
@@ -110,45 +110,36 @@ export default function GradientPicker( {
 	const clearGradient = useCallback( () => onChange( undefined ), [
 		onChange,
 	] );
-	const Component = __experimentalHasMultipleOrigins
-		? MultipleOrigin
-		: SingleOrigin;
-
-	const actions = clearable && (
-		<CircularOptionPicker.ButtonAction onClick={ clearGradient }>
-			{ __( 'Clear' ) }
-		</CircularOptionPicker.ButtonAction>
-	);
-
-	if ( gradients?.length ) {
-		return (
-			<Component
-				className={ className }
-				clearable={ clearable }
-				clearGradient={ clearGradient }
-				gradients={ gradients }
-				onChange={ onChange }
-				value={ value }
-				actions={ actions }
-				content={
-					! disableCustomGradients && (
-						<CustomGradientPicker
-							value={ value }
-							onChange={ onChange }
-						/>
-					)
-				}
-			/>
-		);
-	}
+	const Component =
+		__experimentalHasMultipleOrigins && gradients?.length
+			? MultipleOrigin
+			: SingleOrigin;
 
 	return (
-		<CircularOptionPicker
+		<Component
 			className={ className }
-			options={ [] }
-			actions={ actions }
-		>
-			<CustomGradientPicker value={ value } onChange={ onChange } />
-		</CircularOptionPicker>
+			clearable={ clearable }
+			clearGradient={ clearGradient }
+			gradients={ gradients }
+			onChange={ onChange }
+			value={ value }
+			actions={
+				clearable && (
+					<CircularOptionPicker.ButtonAction
+						onClick={ clearGradient }
+					>
+						{ __( 'Clear' ) }
+					</CircularOptionPicker.ButtonAction>
+				)
+			}
+			content={
+				! disableCustomGradients && (
+					<CustomGradientPicker
+						value={ value }
+						onChange={ onChange }
+					/>
+				)
+			}
+		/>
 	);
 }
