1c34cab2f09719681989db7a230e194220820c72
Michal Czaplinski <mmczaplinski@gmail.com>
2021-11-30 20:00:27 -0500

    Comment Template: Refactor
    Replace the custom convert_to_tree with calls to ::get_children()

--- packages/block-library/src/comment-template/index.php
+++ packages/block-library/src/comment-template/index.php
@@ -6,53 +6,6 @@
  */
 
 /**
- * Convert the flat array of comments to a nested array
- * containing comments with a `comment_children` property.
- *
- * @param WP_Comment[] $comments  - Array of comments.
- * @return array
- */
-function block_core_comment_template_convert_to_tree( $comments ) {
-	$table = array();
-
-	// If there are no comments, we can return early an empty array.
-	if ( empty( $comments ) ) {
-		return array();
-	}
-
-	// First we create an associative array of the form:
-	// [commentId] => [...comment, comment_children: [] ].
-	foreach ( $comments as $comment ) {
-		$comment_with_children                     = (array) $comment;
-		$comment_with_children['comment_children'] = array();
-		$table[ $comment->comment_ID ]             = $comment_with_children;
-	}
-
-	$tree = array();
-
-	foreach ( $comments as $comment ) {
-		if ( ! empty( $comment->comment_parent ) ) {
-			// If the comment has a "parent", then find that parent in the table that
-			// we have created above and push the current comment to the array of its
-			// children.
-			$table[ $comment->comment_parent ]['comment_children'][] = $table[ $comment->comment_ID ];
-		} else {
-			// Otherwise, if the comment has no parent (also works if parent is 0/"0")
-			// that means that it's a top-level comment. So we can find it in the table
-			// and push it to the final tree.
-			//
-			// We have to push by reference using `=&` because this top-level comment might
-			// still have other children. This way, if we add another comment to the
-			// children of the current comment (in the `if` branch), it will be
-			// updated in the final tree.
-			$tree[] =& $table[ $comment->comment_ID ];
-		}
-	}
-
-	return $tree;
-}
-
-/**
  * Function that recursively renders a list of nested comments.
  *
  * @param WP_Comment[] $comments    The array of comments.
@@ -66,14 +19,17 @@ function block_core_comment_template_render_comments( $comments, $block ) {
 		$block_content = ( new WP_Block(
 			$block->parsed_block,
 			array(
-				'commentId' => $comment['comment_ID'],
+				'commentId' => $comment->comment_ID,
 			)
 		) )->render( array( 'dynamic' => false ) );
 
-		// If the comment has children, recurse to create the HTML for the nested comments.
-		if ( ! empty( $comment['comment_children'] ) ) {
+		$children = $comment->get_children();
+
+		// If the comment has children, recurse to create the HTML for the nested
+		// comments.
+		if ( ! empty( $children ) ) {
 			$inner_content  = block_core_comment_template_render_comments(
-				$comment['comment_children'],
+				$children,
 				$block
 			);
 			$block_content .= sprintf( '<ol>%1$s</ol>', $inner_content );
@@ -108,22 +64,24 @@ function render_block_core_comment_template( $attributes, $content, $block ) {
 	$number = $block->context['queryPerPage'];
 
 	// Get an array of comments for the current post.
-	$comments = get_approved_comments( $post_id, array( 'number' => $number ) );
+	$comments = get_approved_comments(
+		$post_id,
+		array(
+			'number'       => $number,
+			'hierarchical' => 'threaded',
+		)
+	);
 
 	if ( count( $comments ) === 0 ) {
 		return '';
 	}
-
-	// Convert the flat array of comments to a nested array of arrays with a
-	// `comment_children` property.
-	$comment_tree = block_core_comment_template_convert_to_tree( $comments );
 
 	$wrapper_attributes = get_block_wrapper_attributes();
 
 	return sprintf(
 		'<ol %1$s>%2$s</ol>',
 		$wrapper_attributes,
-		block_core_comment_template_render_comments( $comment_tree, $block )
+		block_core_comment_template_render_comments( $comments, $block )
 	);
 }
 
