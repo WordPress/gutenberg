8eb1f341285362f6a131fcba81a8d054b6b5b5c5
Daniel Richards <daniel.richards@automattic.com>
2021-12-02 16:51:13 +0800

    Use core version of template and template part post types and REST endpoints for WP 5.9, with back compat for 5.8 (#36898)
    * Use core version of template and template part post types and REST endpoints
    
    * Move files and add explanation
    
    * Only register gutenberg version of templates and template parts when WP5.8.x is active
    
    * Rename WP_Block_Template to Gutenberg_Block_Template so that 5.8 receives improvements to it
    
    * Load moved files
    
    * Update references and sync some code
    
    * Fix filename
    
    * Remove templates tests since Gutenberg now defaults to using the WP core implementation
    
    * Linting and test fixes

--- lib/compat/wordpress-5.9/block-template-utils.php
+++ lib/compat/wordpress-5.9/block-template-utils.php
@@ -21,7 +21,6 @@ if ( ! defined( 'WP_TEMPLATE_PART_AREA_UNCATEGORIZED' ) ) {
 	define( 'WP_TEMPLATE_PART_AREA_UNCATEGORIZED', 'uncategorized' );
 }
 
-
 if ( ! function_exists( 'get_block_theme_folders' ) ) {
 	/**
 	 * For backward compatibility reasons,
@@ -197,7 +196,7 @@ if ( ! function_exists( '_filter_block_template_part_area' ) ) {
 	 */
 	function _filter_block_template_part_area( $type ) {
 		$allowed_areas = array_map(
-			function ( $item ) {
+			static function ( $item ) {
 				return $item['area'];
 			},
 			get_allowed_block_template_part_areas()
@@ -463,7 +462,6 @@ if ( ! function_exists( '_remove_theme_attribute_in_block_template_content' ) ) 
 	 */
 	function _remove_theme_attribute_in_block_template_content( $template_content ) {
 		$has_updated_content = false;
-		$new_content         = '';
 		$template_blocks     = parse_blocks( $template_content );
 
 		$blocks = _flatten_blocks( $template_blocks );
@@ -474,12 +472,8 @@ if ( ! function_exists( '_remove_theme_attribute_in_block_template_content' ) ) 
 			}
 		}
 
-		if ( $has_updated_content ) {
-			foreach ( $template_blocks as $block ) {
-				$new_content .= serialize_block( $block );
-			}
-
-			return $new_content;
+		if ( ! $has_updated_content ) {
+			return $template_content;
 		}
 
 		return $template_content;
@@ -493,26 +487,24 @@ if ( ! function_exists( '_build_block_template_result_from_file' ) ) {
 	 * @param array $template_file Theme file.
 	 * @param array $template_type wp_template or wp_template_part.
 	 *
-	 * @return WP_Block_Template Template.
+	 * @return Gutenberg_Block_Template Template.
 	 */
 	function _build_block_template_result_from_file( $template_file, $template_type ) {
 		$default_template_types = get_default_block_template_types();
 		$template_content       = file_get_contents( $template_file['path'] );
 		$theme                  = wp_get_theme()->get_stylesheet();
 
-		$template                 = new WP_Block_Template();
+		$template                 = new Gutenberg_Block_Template();
 		$template->id             = $theme . '//' . $template_file['slug'];
 		$template->theme          = $theme;
 		$template->content        = _inject_theme_attribute_in_block_template_content( $template_content );
 		$template->slug           = $template_file['slug'];
 		$template->source         = 'theme';
-		$template->origin         = null;
 		$template->type           = $template_type;
 		$template->title          = ! empty( $template_file['title'] ) ? $template_file['title'] : $template_file['slug'];
 		$template->status         = 'publish';
 		$template->has_theme_file = true;
 		$template->is_custom      = true;
-		$template->author         = 0;
 
 		if ( 'wp_template' === $template_type && isset( $default_template_types[ $template_file['slug'] ] ) ) {
 			$template->description = $default_template_types[ $template_file['slug'] ]['description'];
@@ -538,7 +530,7 @@ if ( ! function_exists( '_build_block_template_result_from_post' ) ) {
 	 *
 	 * @param WP_Post $post Template post.
 	 *
-	 * @return WP_Block_Template|WP_Error Template.
+	 * @return Gutenberg_Block_Template|WP_Error Template.
 	 */
 	function _build_block_template_result_from_post( $post ) {
 		$default_template_types = get_default_block_template_types();
@@ -558,7 +550,7 @@ if ( ! function_exists( '_build_block_template_result_from_post' ) ) {
 		$has_theme_file = wp_get_theme()->get_stylesheet() === $theme &&
 			null !== _get_block_template_file( $post->post_type, $post->post_name );
 
-		$template                 = new WP_Block_Template();
+		$template                 = new Gutenberg_Block_Template();
 		$template->wp_id          = $post->ID;
 		$template->id             = $theme . '//' . $post->post_name;
 		$template->theme          = $theme;
@@ -613,7 +605,7 @@ function gutenberg_get_block_templates( $query = array(), $template_type = 'wp_t
 	 *
 	 * @since 10.8
 	 *
-	 * @param WP_Block_Template[]|null $block_templates Return an array of block templates to short-circuit the default query,
+	 * @param Gutenberg_Block_Template[]|null $block_templates Return an array of block templates to short-circuit the default query,
 	 *                                                  or null to allow WP to run it's normal queries.
 	 * @param array $query {
 	 *     Optional. Arguments to retrieve templates.
@@ -716,7 +708,7 @@ function gutenberg_get_block_templates( $query = array(), $template_type = 'wp_t
 	 *
 	 * @since 10.8
 	 *
-	 * @param WP_Block_Template[] $query_result Array of found block templates.
+	 * @param Gutenberg_Block_Template[] $query_result Array of found block templates.
 	 * @param array $query {
 	 *     Optional. Arguments to retrieve templates.
 	 *
@@ -734,7 +726,7 @@ function gutenberg_get_block_templates( $query = array(), $template_type = 'wp_t
  * @param string $id Template unique identifier (example: theme_slug//template_slug).
  * @param array  $template_type wp_template or wp_template_part.
  *
- * @return WP_Block_Template|null Template.
+ * @return Gutenberg_Block_Template|null Template.
  */
 function gutenberg_get_block_template( $id, $template_type = 'wp_template' ) {
 	/**
@@ -744,7 +736,7 @@ function gutenberg_get_block_template( $id, $template_type = 'wp_template' ) {
 	 *
 	 * @since 10.8
 	 *
-	 * @param WP_Block_Template|null $block_template Return block template object to short-circuit the default query,
+	 * @param Gutenberg_Block_Template|null $block_template Return block template object to short-circuit the default query,
 	 *                                               or null to allow WP to run it's normal queries.
 	 * @param string $id Template unique identifier (example: theme_slug//template_slug).
 	 * @param array  $template_type wp_template or wp_template_part.
@@ -791,7 +783,7 @@ function gutenberg_get_block_template( $id, $template_type = 'wp_template' ) {
 	 *
 	 * @since 10.8
 	 *
-	 * @param WP_Block_Template|null $block_template The found block template, or null if there isn't one.
+	 * @param Gutenberg_Block_Template|null $block_template The found block template, or null if there isn't one.
 	 * @param string $id Template unique identifier (example: theme_slug//template_slug).
 	 * @param array  $template_type wp_template or wp_template_part.
 	 */
@@ -806,7 +798,7 @@ if ( ! function_exists( 'get_block_file_template' ) ) {
 	 * @param string $id Template unique identifier (example: theme_slug//template_slug).
 	 * @param array  $template_type wp_template or wp_template_part.
 	 *
-	 * @return WP_Block_Template|null File template.
+	 * @return Gutenberg_Block_Template|null File template.
 	 */
 	function get_block_file_template( $id, $template_type = 'wp_template' ) {
 		/**
@@ -816,7 +808,7 @@ if ( ! function_exists( 'get_block_file_template' ) ) {
 		 *
 		 * @since 10.8
 		 *
-		 * @param WP_Block_Template|null $block_template Return block template object to short-circuit the default query,
+		 * @param Gutenberg_Block_Template|null $block_template Return block template object to short-circuit the default query,
 		 *                                               or null to allow WP to run it's normal queries.
 		 * @param string $id Template unique identifier (example: theme_slug//template_slug).
 		 * @param array  $template_type wp_template or wp_template_part.
@@ -851,7 +843,7 @@ if ( ! function_exists( 'get_block_file_template' ) ) {
 		 *
 		 * @since 10.8
 		 *
-		 * @param null|WP_Block_Template $block_template The found block template.
+		 * @param null|Gutenberg_Block_Template $block_template The found block template.
 		 * @param string $id Template unique identifier (example: theme_slug//template_slug).
 		 * @param array  $template_type wp_template or wp_template_part.
 		 */
--- lib/load.php
+++ lib/load.php
@@ -98,11 +98,11 @@ require __DIR__ . '/compat/wordpress-5.9/register-global-styles-cpt.php';
 require __DIR__ . '/compat/wordpress-5.9/get-global-styles-and-settings.php';
 require __DIR__ . '/compat/wordpress-5.9/json-file-decode.php';
 require __DIR__ . '/compat/wordpress-5.9/translate-settings-using-i18n-schema.php';
+require __DIR__ . '/compat/wordpress-5.9/class-gutenberg-block-template.php';
+require __DIR__ . '/compat/wordpress-5.9/templates.php';
+require __DIR__ . '/compat/wordpress-5.9/template-parts.php';
 require __DIR__ . '/editor-settings.php';
 
-if ( ! class_exists( 'WP_Block_Template' ) ) {
-	require __DIR__ . '/full-site-editing/class-wp-block-template.php';
-}
 
 // These are used by some FSE features
 // as well as global styles.
@@ -115,8 +115,6 @@ require __DIR__ . '/full-site-editing/block-templates.php';
 require __DIR__ . '/full-site-editing/default-template-types.php';
 require __DIR__ . '/full-site-editing/templates-utils.php';
 require __DIR__ . '/full-site-editing/page-templates.php';
-require __DIR__ . '/full-site-editing/templates.php';
-require __DIR__ . '/full-site-editing/template-parts.php';
 require __DIR__ . '/full-site-editing/template-loader.php';
 require __DIR__ . '/full-site-editing/edit-site-page.php';
 require __DIR__ . '/compat/wordpress-5.9/default-theme-supports.php';
