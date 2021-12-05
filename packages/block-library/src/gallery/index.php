1420af4085589920970e18ca6ad96ec0a55f0ce4
Dominik Schilling <dominikschilling+git@gmail.com>
2021-12-05 16:07:16 +0100

    Fix typo and docs formatting

--- packages/block-library/src/gallery/index.php
+++ packages/block-library/src/gallery/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Server-side rendering of the `core/image` block.
+ * Server-side rendering of the `core/gallery` block.
  *
  * @package WordPress
  */
@@ -13,9 +13,8 @@
  * we add a custom `data-id` attribute before rendering the gallery
  * so that the Image Block can pick it up in its render_callback.
  *
- * @param  array $parsed_block A single parsed block object.
- *
- * @return array               The migrated block object.
+ * @param array $parsed_block The block being rendered.
+ * @return array The migrated block object.
  */
 function render_block_core_gallery_data( $parsed_block ) {
 	if ( 'core/gallery' === $parsed_block['blockName'] ) {
