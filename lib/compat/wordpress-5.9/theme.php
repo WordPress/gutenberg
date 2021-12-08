343c62fab1f3a619131361c2b6fd77070f4a4985
George Mamadashvili <georgemamadashvili@gmail.com>
2021-12-08 20:37:46 +0400

    Use compat file

--- lib/compat/wordpress-5.9/theme.php
+++ lib/compat/wordpress-5.9/theme.php
@@ -16,3 +16,13 @@ if ( ! function_exists( 'wp_is_block_theme' ) ) {
 			is_readable( get_theme_file_path( '/templates/index.html' ) );
 	}
 }
+
+/**
+ * Note: We have to maintain this function for backward compatibility with WP 5.8.
+ * Only remove once 5.9 is the minimum supported WordPress version for the Gutenberg plugin.
+ *
+ * @return boolean Whether the current theme is a block theme or not.
+ */
+function gutenberg_is_fse_theme() {
+	return wp_is_block_theme();
+}
