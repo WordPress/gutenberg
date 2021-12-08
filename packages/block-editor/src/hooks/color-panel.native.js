7b193912253de943037c310c9f22c959a05af57b
Siobhan <siobhan@automattic.com>
2021-12-08 01:17:25 +0000

    Detect changes to block's text and/or background color
    With this commit, useEffect and useState hooks are introduced to detect changes to a block's text and/or background colors.

--- packages/block-editor/src/hooks/color-panel.native.js
+++ packages/block-editor/src/hooks/color-panel.native.js
@@ -1,8 +1,12 @@
 /**
  * WordPress dependencies
  */
+import { withSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { useGlobalStyles } from '@wordpress/components';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { compose } from '@wordpress/compose';
 
 /**
  * Internal dependencies
@@ -11,10 +15,27 @@ import PanelColorGradientSettings from '../components/colors-gradients/panel-col
 import ContrastChecker from '../components/contrast-checker';
 import InspectorControls from '../components/inspector-controls';
 
-const ColorPanel = ( { settings } ) => {
+const ColorPanel = ( { settings, baseGlobalStyles } ) => {
 	const globalStyles = useGlobalStyles();
-	const detectedColor = globalStyles.color;
-	const detectedBackgroundColor = globalStyles.backgroundColor;
+
+	const [ detectedBackgroundColor, setDetectedBackgroundColor ] = useState();
+	const [ detectedTextColor, setDetectedTextColor ] = useState();
+
+	useEffect( () => {
+		// The following logic is used to determine current text/background colors:
+		// 1. The globalStyles object is queried to determine whether a color has been
+		// set via a block's settings.
+		// 2. If a block-based theme is in use and no globalStyles exist, the theme's
+		// default/base colors are used.
+		// 3. If no globalStyles exist and a theme isn't block-based, there is no way
+		// to determine the default text/background color and the checker won't run.
+		const textColor = globalStyles?.color || baseGlobalStyles?.text;
+		const backgroundColor =
+			globalStyles?.backgroundColor || baseGlobalStyles?.background;
+
+		setDetectedTextColor( textColor );
+		setDetectedBackgroundColor( backgroundColor );
+	}, [ globalStyles, baseGlobalStyles ] );
 
 	return (
 		<InspectorControls>
@@ -22,16 +43,25 @@ const ColorPanel = ( { settings } ) => {
 				title={ __( 'Color' ) }
 				initialOpen={ false }
 				settings={ settings }
-				backgroundColor={ detectedBackgroundColor }
-				textColor={ detectedColor }
 			>
 				<ContrastChecker
 					backgroundColor={ detectedBackgroundColor }
-					textColor={ detectedColor }
+					textColor={ detectedTextColor }
 				/>
 			</PanelColorGradientSettings>
 		</InspectorControls>
 	);
 };
 
-export default ColorPanel;
+export default compose( [
+	withSelect( ( select ) => {
+		const { getSettings } = select( blockEditorStore );
+		const settings = getSettings();
+		const baseGlobalStyles =
+			settings?.__experimentalGlobalStylesBaseStyles?.color;
+
+		return {
+			baseGlobalStyles,
+		};
+	} ),
+] )( ColorPanel );
