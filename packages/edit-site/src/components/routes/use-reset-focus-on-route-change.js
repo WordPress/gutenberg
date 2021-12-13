e4029fdbc8ca36d7868e74daef0d99e2a80701b1
Kai Hao <kevin830726@gmail.com>
2021-12-13 12:44:57 +0800

    Fix initial page redirection

--- packages/edit-site/src/components/routes/use-reset-focus-on-route-change.js
+++ packages/edit-site/src/components/routes/use-reset-focus-on-route-change.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { Action } from 'history';
+
+/**
  * WordPress dependencies
  */
 import { useRef, useEffect } from '@wordpress/element';
@@ -6,19 +11,30 @@ import { useRef, useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import { useLocation } from './index';
+import { useLocation, useHistory } from './index';
 
 export default function useResetFocusOnRouteChange( targetRef ) {
+	const history = useHistory();
 	const location = useLocation();
 	const isInitialPageLoadRef = useRef( true );
+	const expectRedirectionRef = useRef( false );
 
 	useEffect( () => {
 		// Don't focus for initial page load.
 		if ( isInitialPageLoadRef.current ) {
 			isInitialPageLoadRef.current = false;
+			expectRedirectionRef.current = true;
 			return;
+		}
+		// Don't focus for the initial page redirection.
+		// TODO: This can be removed once #36873 is resolved.
+		if ( expectRedirectionRef.current ) {
+			expectRedirectionRef.current = false;
+			if ( history.action === Action.Replace ) {
+				return;
+			}
 		}
 
 		targetRef.current?.focus();
-	}, [ location, targetRef ] );
+	}, [ location, targetRef, history ] );
 }
