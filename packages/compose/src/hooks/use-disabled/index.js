ee8881f029940d66ebdead8a0ded7ef57f12fcfb
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2021-12-14 16:46:26 +1100

    Move documentation to the hook

--- packages/compose/src/hooks/use-disabled/index.js
+++ packages/compose/src/hooks/use-disabled/index.js
@@ -28,8 +28,29 @@ const DISABLED_ELIGIBLE_NODE_NAMES = [
 	'TEXTAREA',
 ];
 
+/**
+ * In some circumstances, such as block previews, all focusable DOM elements
+ * (input fields, links, buttons, etc.) need to be disabled. This hook adds the
+ * behavior to disable nested DOM elements to the returned ref.
+ *
+ * @return {import('react').RefObject<HTMLElement>} Element Ref.
+ *
+ * @example
+ * ```js
+ * import { __experimentalUseDisabled as useDisabled } from '@wordpress/compose';
+ * const DisabledExample = () => {
+ * 	const disabledRef = useDisabled();
+ *	return (
+ *		<div ref={ disabledRef }>
+ *			<a href="#">This link will have tabindex set to -1</a>
+ *			<input placeholder="This input will have the disabled attribute added to it." type="text" />
+ *		</div>
+ *	);
+ * };
+ * ```
+ */
 export default function useDisabled() {
-	/** @type {import('react').RefObject<HTMLDivElement>} */
+	/** @type {import('react').RefObject<HTMLElement>} */
 	const node = useRef( null );
 
 	const disable = () => {
