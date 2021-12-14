c94887ff4934a5ad425c233e14e64a510d9210ac
Robert Anderson <robert@noisysocks.com>
2021-12-14 11:48:41 +1100

    Remove gutenberg_ prefix from @wordpress/block-library (#37341)
    * Remove gutenberg_ prefix from block library
    
    * Use the full name of the block

--- packages/block-library/src/post-comments/index.php
+++ packages/block-library/src/post-comments/index.php
@@ -74,7 +74,7 @@ add_action( 'init', 'register_block_core_post_comments' );
  *
  * @return array Returns the modified fields.
  */
-function gutenberg_post_comments_block_form_defaults( $fields ) {
+function post_comments_block_form_defaults( $fields ) {
 	if ( wp_is_block_theme() ) {
 		$fields['submit_button'] = '<input name="%1$s" type="submit" id="%2$s" class="%3$s wp-block-button__link" value="%4$s" />';
 		$fields['submit_field']  = '<p class="form-submit wp-block-button">%1$s %2$s</p>';
@@ -82,4 +82,4 @@ function gutenberg_post_comments_block_form_defaults( $fields ) {
 
 	return $fields;
 }
-add_filter( 'comment_form_defaults', 'gutenberg_post_comments_block_form_defaults' );
+add_filter( 'comment_form_defaults', 'post_comments_block_form_defaults' );
