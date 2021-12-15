bd75cc9c0ce57be3e10715150b46a8323625cfa9
Mustaque Ahmed <amustaque97@gmail.com>
2021-12-15 18:26:20 +0530

    Move `getMomentDate` to a separate `utils.js` file
    - moved the `getMomentDate` tests under a new file, eg `test/utils.js` file
    - changed the `getMomentDate` unit tests to avoid rendering `DatePicker` and using enzyme

--- packages/components/src/date-time/date.js
+++ packages/components/src/date-time/date.js
@@ -15,6 +15,11 @@ import { useEffect, useRef } from '@wordpress/element';
 import { isRTL, _n, sprintf } from '@wordpress/i18n';
 
 /**
+ * Internal dependencies
+ */
+import { getMomentDate } from './utils';
+
+/**
  * Module Constants
  */
 const TIMEZONELESS_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
@@ -127,20 +132,6 @@ function DatePicker( {
 
 		// Keep focus on the date picker.
 		keepFocusInside();
-	};
-
-	/**
-	 * Create a Moment object from a date string. With no date supplied, default to a Moment
-	 * object representing now. If a null value is passed, return a null value.
-	 *
-	 * @param {?string} date Date representing the currently selected date or null to signify no selection.
-	 * @return {?moment.Moment} Moment object for selected date or null.
-	 */
-	const getMomentDate = ( date ) => {
-		if ( null === date ) {
-			return null;
-		}
-		return date ? moment( date ) : moment();
 	};
 
 	const getEventsPerDay = ( day ) => {
