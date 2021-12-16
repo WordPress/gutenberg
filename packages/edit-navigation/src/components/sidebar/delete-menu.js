bb09c68dd2b2175213f39971f826f009c70c0871
chad1008 <13856531+chad1008@users.noreply.github.com>
2021-12-16 16:57:20 -0500

    Migrate Edit Navigation screen "Delete menu" button from `confirm()` to the new `ConfirmDialog` component

--- packages/edit-navigation/src/components/sidebar/delete-menu.js
+++ packages/edit-navigation/src/components/sidebar/delete-menu.js
@@ -2,31 +2,43 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Button, PanelBody } from '@wordpress/components';
+import {
+	Button,
+	PanelBody,
+	__experimentalConfirmDialog as ConfirmDialog,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
+
+function DeleteMenuConfirm( props ) {
+	return props.showConfirmDialog ? (
+		<ConfirmDialog onConfirm={ props.onDeleteMenu }>
+			Are you sure you want to delete this navigation? This action cannot
+			be undone.
+		</ConfirmDialog>
+	) : null;
+}
 
 export default function DeleteMenu( { onDeleteMenu, isMenuBeingDeleted } ) {
+	const [ showConfirmDialog, setShowConfirmDialog ] = useState( false );
 	return (
 		<PanelBody>
-			<Button
-				className="edit-navigation-inspector-additions__delete-menu-button"
-				variant="secondary"
-				isDestructive
-				isBusy={ isMenuBeingDeleted }
-				onClick={ () => {
-					if (
-						// eslint-disable-next-line no-alert
-						window.confirm(
-							__(
-								'Are you sure you want to delete this navigation? This action cannot be undone.'
-							)
-						)
-					) {
-						onDeleteMenu();
-					}
-				} }
-			>
-				{ __( 'Delete menu' ) }
-			</Button>
+			<>
+				<Button
+					className="edit-navigation-inspector-additions__delete-menu-button"
+					variant="secondary"
+					isDestructive
+					isBusy={ isMenuBeingDeleted }
+					onClick={ () => {
+						setShowConfirmDialog( true );
+					} }
+				>
+					{ __( 'Delete menu' ) }
+				</Button>
+				<DeleteMenuConfirm
+					onDeleteMenu={ onDeleteMenu }
+					showConfirmDialog={ showConfirmDialog }
+				/>
+			</>
 		</PanelBody>
 	);
 }
