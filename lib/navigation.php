26322710f390be25bf69311c4be4e0adad6e9693
Dave Smith <getdavemail@gmail.com>
2021-12-16 12:46:08 +0000

    Copy changes from Core patch
    See https://github.com/WordPress/wordpress-develop/pull/2056/files

--- lib/navigation.php
+++ lib/navigation.php
@@ -51,7 +51,22 @@ function gutenberg_register_navigation_post_type() {
 			'revisions',
 		),
 		'capabilities'          => array(
-			'create_posts' => 'edit_theme_options',
+			// Meta Capabilities.
+			'edit_post'              => 'edit_post',
+			'read_post'              => 'read_post',
+			'delete_post'            => 'delete_post',
+			// Primitive Capabilities.
+			'edit_posts'             => 'edit_theme_options',
+			'edit_others_posts'      => 'edit_theme_options',
+			'delete_posts'           => 'edit_theme_options',
+			'publish_posts'          => 'edit_theme_options',
+			'read_private_posts'     => 'edit_theme_options',
+			'read'                   => 'read',
+			'delete_private_posts'   => 'edit_theme_options',
+			'delete_published_posts' => 'edit_theme_options',
+			'delete_others_posts'    => 'edit_theme_options',
+			'edit_private_posts'     => 'edit_theme_options',
+			'edit_published_posts'   => 'edit_theme_options',
 		),
 	);
 
