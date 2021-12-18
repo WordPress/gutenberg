1aa42b143c60ee2f2d2d10050488259fa27c9068
Kevin Batdorf <kbat82@gmail.com>
2021-12-17 21:48:12 -0500

    add polling for dom animations

--- packages/components/src/toggle-group-control/toggle-group-control/toggle-group-control-backdrop.tsx
+++ packages/components/src/toggle-group-control/toggle-group-control/toggle-group-control-backdrop.tsx
@@ -15,59 +15,69 @@ function ToggleGroupControlBackdrop( {
 	isAdaptiveWidth,
 	state,
 }: ToggleGroupControlBackdropProps ) {
-	const [ left, setLeft ] = useState( 0 );
+	const [ left, setLeft ] = useState< number | undefined >( undefined );
 	const [ width, setWidth ] = useState( 0 );
 	const [ canAnimate, setCanAnimate ] = useState( false );
-	const [ renderBackdrop, setRenderBackdrop ] = useState( false );
+	const [ targetNode, setTargetNode ] = useState< HTMLElement >();
 
 	useEffect( () => {
 		const containerNode = containerRef?.current;
 		if ( ! containerNode ) return;
-
-		/**
-		 * Workaround for Reakit
-		 */
-		const targetNode = containerNode.querySelector(
+		const target = containerNode.querySelector(
 			`[data-value="${ state }"]`
-		);
-		setRenderBackdrop( !! targetNode );
-		if ( ! targetNode ) {
-			return;
-		}
+		) as HTMLElement;
+		setTargetNode( target );
+	}, [ containerRef, containerWidth, state, isAdaptiveWidth ] );
 
-		const computeDimensions = () => {
-			const {
-				width: offsetWidth,
-				x,
-			} = targetNode.getBoundingClientRect();
+	useEffect( () => {
+		if ( ! containerRef?.current || ! targetNode ) return;
+		// Set the width when the target node changes
+		setWidth( targetNode.offsetWidth );
+	}, [ targetNode ] );
 
-			const { x: parentX } = containerNode.getBoundingClientRect();
+	useEffect( () => {
+		const containerNode = containerRef?.current;
+		if ( ! containerNode || ! targetNode ) return;
 
-			const borderWidth = 1;
-			const offsetLeft = x - parentX - borderWidth;
-
-			setLeft( offsetLeft );
-			setWidth( offsetWidth );
-		};
-		// Fix to make the component appear as expected inside popovers.
-		// If the targetNode width is 0 it means the element was not yet rendered we should allow
-		// some time for the render to happen.
-		// requestAnimationFrame instead of setTimeout with a small time does not seems to work.
-		const dimensionsRequestId = window.setTimeout( computeDimensions, 100 );
-
+		// If the component is being rendered in an animating parent (i.e. popover),
+		// the element's final x/y position may not be available until the parent
+		// finishes rendering. We poll until then and wait for an available frame thereafter
+		let timer = 0;
+		const pollTime = 5;
+		const maxPollTime = 150;
 		let animationRequestId: number;
-		if ( ! canAnimate ) {
-			animationRequestId = window.requestAnimationFrame( () => {
-				setCanAnimate( true );
-			} );
-		}
-		return () => {
-			window.clearTimeout( dimensionsRequestId );
-			window.cancelAnimationFrame( animationRequestId );
-		};
-	}, [ canAnimate, containerRef, containerWidth, state, isAdaptiveWidth ] );
+		const waitForTransition = window.setInterval( () => {
+			timer += pollTime;
 
-	if ( ! renderBackdrop ) {
+			// Check whether the element is in a subpixel position, likely in transition
+			const { x, y, width: targetW } = targetNode.getBoundingClientRect();
+			const inMotion = x % 1 !== 0 || y % 1 !== 0;
+			if ( ( targetW && ! inMotion ) || timer >= maxPollTime ) {
+				const { x: parentX } = containerNode.getBoundingClientRect();
+				const borderWidth = 1;
+				const offsetLeft = x - parentX - borderWidth;
+				animationRequestId = window.requestAnimationFrame( () => {
+					setLeft( offsetLeft );
+				} );
+				window.clearInterval( waitForTransition );
+			}
+		}, pollTime );
+
+		return () => {
+			window.cancelAnimationFrame( animationRequestId );
+			window.clearInterval( waitForTransition );
+		};
+	}, [ targetNode, width ] );
+
+	useEffect( () => {
+		// Don't animate until the position is set
+		if ( ! canAnimate && left !== undefined ) {
+			setCanAnimate( true );
+		}
+	}, [ left ] );
+
+	// Do not render unless we have a target node and it's position is known
+	if ( ! targetNode || left === undefined ) {
 		return null;
 	}
 
