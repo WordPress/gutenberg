6320400bde429e8d122b71f3fbc9bc2adc48772d
Carlos Garcia <fluiddot@gmail.com>
2021-12-20 13:19:46 +0100

    Add plugin translations to setup locale function

--- packages/react-native-editor/src/setup-locale.js
+++ packages/react-native-editor/src/setup-locale.js
@@ -3,26 +3,53 @@
  */
 import { setLocaleData } from '@wordpress/i18n';
 
-export default ( { locale, extraTranslations, domain, getTranslation } ) => {
-	let gutenbergTranslations = getTranslation( locale );
-	if ( locale && ! gutenbergTranslations ) {
-		// Try stripping out the regional
-		locale = locale.replace( /[-_][A-Za-z]+$/, '' );
-		gutenbergTranslations = getTranslation( locale );
-	}
-	const translations = Object.assign(
-		{},
-		gutenbergTranslations,
-		extraTranslations
+/**
+ * Setup locale data for default domain and plugins.
+ *
+ * @typedef {Object} PluginTranslation
+ * @property {string}              domain                Domain of the plugin.
+ * @property {Function}            getTranslation        Function for retrieving translations for a locale.
+ *
+ * @param    {string}              locale                Locale value.
+ * @param    {Object}              extraTranslations     Extra translations to be included.
+ * @param    {Function}            getDefaultTranslation Default domain's function for retrieving translations for a locale.
+ * @param    {PluginTranslation[]} pluginTranslations    Array with plugin translations.
+ */
+export default (
+	locale,
+	extraTranslations,
+	getDefaultTranslation,
+	pluginTranslations
+) => {
+	const setDomainLocaleData = ( getTranslation, domain = 'default' ) => {
+		let translations = getTranslation( locale );
+		if ( locale && ! translations ) {
+			// Try stripping out the regional
+			locale = locale.replace( /[-_][A-Za-z]+$/, '' );
+			translations = getTranslation( locale );
+		}
+		const allTranslations = Object.assign(
+			{},
+			translations,
+			extraTranslations
+		);
+		// eslint-disable-next-line no-console
+		console.log(
+			domain ? `${ domain } - locale` : 'locale',
+			locale,
+			allTranslations
+		);
+		// Only change the locale if it's supported by gutenberg
+		if ( translations || extraTranslations ) {
+			setLocaleData( allTranslations, domain );
+		}
+	};
+
+	// Set up default locale data (Gutenberg)
+	setDomainLocaleData( getDefaultTranslation );
+
+	// Set up plugin translations
+	pluginTranslations.forEach( ( { domain, getTranslation } ) =>
+		setDomainLocaleData( getTranslation, domain )
 	);
-	// eslint-disable-next-line no-console
-	console.log(
-		domain ? `${ domain } - locale` : 'locale',
-		locale,
-		translations
-	);
-	// Only change the locale if it's supported by gutenberg
-	if ( gutenbergTranslations || extraTranslations ) {
-		setLocaleData( translations, domain );
-	}
 };
