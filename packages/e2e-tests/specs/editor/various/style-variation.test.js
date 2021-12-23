65be753cb5ee44d6483ede97259d86682d55b14a
ntsekouras <ntsekouras@outlook.com>
2021-12-23 10:01:36 +0200

    fix e2e test

--- packages/e2e-tests/specs/editor/various/style-variation.test.js
+++ packages/e2e-tests/specs/editor/various/style-variation.test.js
@@ -13,7 +13,7 @@ describe( 'adding blocks', () => {
 		await createNewPost();
 	} );
 
-	it( 'Should switch to the large style of the quote block', async () => {
+	it( 'Should switch to the plain style of the quote block', async () => {
 		// Inserting a quote block
 		await insertBlock( 'Quote' );
 		await page.keyboard.type( 'Quote content' );
@@ -27,6 +27,10 @@ describe( 'adding blocks', () => {
 
 		// Check the content
 		const content = await getEditedPostContent();
-		expect( content ).toMatchSnapshot();
+		expect( content ).toMatchInlineSnapshot( `
+		"<!-- wp:quote {\\"className\\":\\"is-style-plain\\"} -->
+		<blockquote class=\\"wp-block-quote is-style-plain\\"><p>Quote content</p></blockquote>
+		<!-- /wp:quote -->"
+	` );
 	} );
 } );
