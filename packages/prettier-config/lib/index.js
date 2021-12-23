374f4d2f30823b6d6384ba41b24114d44443cc08
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-12-23 08:27:05 +0100

    Scripts: Switch to Prettier 2.5

--- packages/prettier-config/lib/index.js
+++ packages/prettier-config/lib/index.js
@@ -1,26 +1,16 @@
 /** @typedef {import('prettier').Options} PrettierOptions */
 
-/**
- * @typedef WPPrettierOptions
- *
- * @property {boolean} [parenSpacing=true] Insert spaces inside parentheses.
- */
-
-// Disable reason: The current JSDoc tooling does not yet understand TypeScript
-// union types.
-
-/** @type {PrettierOptions & WPPrettierOptions} */
+/** @type {PrettierOptions} */
 const config = {
-	useTabs: true,
-	tabWidth: 4,
-	printWidth: 80,
-	singleQuote: true,
-	trailingComma: 'es5',
-	bracketSpacing: true,
-	parenSpacing: true,
-	jsxBracketSameLine: false,
-	semi: true,
 	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
 };
 
 module.exports = config;
