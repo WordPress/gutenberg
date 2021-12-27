760815a731805fa05f6cbb234660b254ad1ca6c9
Nik Tsekouras <ntsekouras@outlook.com>
2021-12-27 18:33:47 +0200

    [Block Library]: Fix editable post blocks in Query Loop with `zero` `queryId` (#37629)

--- packages/block-library/src/post-featured-image/edit.js
+++ packages/block-library/src/post-featured-image/edit.js
@@ -52,7 +52,7 @@ function PostFeaturedImageDisplay( {
 	setAttributes,
 	context: { postId, postType, queryId },
 } ) {
-	const isDescendentOfQueryLoop = !! queryId;
+	const isDescendentOfQueryLoop = Number.isFinite( queryId );
 	const { isLink, height, width, scale } = attributes;
 	const [ featuredImage, setFeaturedImage ] = useEntityProp(
 		'postType',
