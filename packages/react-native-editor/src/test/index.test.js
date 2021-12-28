ca87af587ea2e436c3293981929c596e5678ffe5
Carlos Garcia <fluiddot@gmail.com>
2021-12-28 10:59:42 +0100

    Add test cases for register gutenberg

---
+++ packages/react-native-editor/src/test/index.test.js
@@ -0,0 +1,182 @@
+/**
+ * External dependencies
+ */
+import { AppRegistry } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import { render } from 'test/helpers';
+
+/**
+ * WordPress dependencies
+ */
+import * as wpHooks from '@wordpress/hooks';
+
+/**
+ * Internal dependencies
+ */
+import { registerGutenberg } from '..';
+import setupLocale from '../setup-locale';
+
+jest.mock( 'react-native/Libraries/ReactNative/AppRegistry' );
+jest.mock( '../setup-locale' );
+
+const initGutenberg = ( registerParams ) => {
+	let EditorComponent;
+	jest.spyOn( AppRegistry, 'registerComponent' ).mockImplementation(
+		( name, componentProvider ) => {
+			EditorComponent = componentProvider();
+		}
+	);
+	registerGutenberg( registerParams );
+	render( <EditorComponent /> );
+};
+
+describe( 'Register Gutenberg', () => {
+	beforeEach( () => {
+		// We need to reset modules to guarantee that setup module is imported on every test.
+		jest.resetModules();
+	} );
+
+	it( 'registers Gutenberg editor component', () => {
+		const registerComponent = jest.spyOn(
+			AppRegistry,
+			'registerComponent'
+		);
+		registerGutenberg();
+		expect( registerComponent ).toHaveBeenCalled();
+	} );
+
+	it( 'sets up locale before editor is initialized', () => {
+		const onModuleImported = jest.fn();
+		jest.doMock( '../setup', () => {
+			// To determine if the setup module is imported, we create a mock function that is called when the module is mocked.
+			onModuleImported();
+
+			return {
+				__esModule: true,
+				default: jest.fn().mockReturnValue( <></> ),
+			};
+		} );
+
+		initGutenberg();
+
+		// "invocationCallOrder" can be used to compare call orders between different mocks.
+		// Reference: https://git.io/JyBk0
+		const setupLocaleCallOrder = setupLocale.mock.invocationCallOrder[ 0 ];
+		const onSetupImportedCallOrder =
+			onModuleImported.mock.invocationCallOrder[ 0 ];
+
+		expect( setupLocaleCallOrder ).toBeLessThan( onSetupImportedCallOrder );
+	} );
+
+	it( 'beforeInit callback is invoked before the editor is initialized', () => {
+		const beforeInitCallback = jest.fn();
+		const onModuleImported = jest.fn();
+		jest.doMock( '../setup', () => {
+			// To determine if the setup module is imported, we create a mock function that is called when the module is mocked.
+			onModuleImported();
+
+			return {
+				__esModule: true,
+				default: jest.fn().mockReturnValue( <></> ),
+			};
+		} );
+
+		initGutenberg( { beforeInitCallback } );
+
+		// "invocationCallOrder" can be used to compare call orders between different mocks.
+		// Reference: https://git.io/JyBk0
+		const beforeInitCallOrder =
+			beforeInitCallback.mock.invocationCallOrder[ 0 ];
+		const onSetupImportedCallOrder =
+			onModuleImported.mock.invocationCallOrder[ 0 ];
+
+		expect( beforeInitCallOrder ).toBeLessThan( onSetupImportedCallOrder );
+	} );
+
+	it( 'dispatches "native.pre-render" hook before the editor is rendered', () => {
+		const doAction = jest.spyOn( wpHooks, 'doAction' );
+
+		// An empty component is provided in order to listen for render calls of the editor component.
+		const onRenderEditor = jest.fn();
+		const EditorComponent = () => {
+			onRenderEditor();
+			return null;
+		};
+		jest.mock( '../setup', () => ( {
+			__esModule: true,
+			default: jest.fn().mockReturnValue( <EditorComponent /> ),
+		} ) );
+
+		initGutenberg();
+
+		const hookCallIndex = 0;
+		// "invocationCallOrder" can be used to compare call orders between different mocks.
+		// Reference: https://git.io/JyBk0
+		const hookCallOrder =
+			doAction.mock.invocationCallOrder[ hookCallIndex ];
+		const onRenderEditorCallOrder =
+			onRenderEditor.mock.invocationCallOrder[ 0 ];
+		const hookName = doAction.mock.calls[ hookCallIndex ][ 0 ];
+
+		expect( hookName ).toBe( 'native.pre-render' );
+		expect( hookCallOrder ).toBeLessThan( onRenderEditorCallOrder );
+	} );
+
+	it( 'dispatches "native.block_editor_props" hook before the editor is rendered', () => {
+		const applyFilters = jest.spyOn( wpHooks, 'applyFilters' );
+
+		// An empty component is provided in order to listen for render calls of the editor component.
+		const onRenderEditor = jest.fn();
+		const EditorComponent = () => {
+			onRenderEditor();
+			return null;
+		};
+		jest.mock( '../setup', () => ( {
+			__esModule: true,
+			default: jest.fn().mockReturnValue( <EditorComponent /> ),
+		} ) );
+
+		initGutenberg();
+
+		const hookCallIndex = 0;
+		// "invocationCallOrder" can be used to compare call orders between different mocks.
+		// Reference: https://git.io/JyBk0
+		const hookCallOrder =
+			applyFilters.mock.invocationCallOrder[ hookCallIndex ];
+		const onRenderEditorCallOrder =
+			onRenderEditor.mock.invocationCallOrder[ 0 ];
+		const hookName = applyFilters.mock.calls[ hookCallIndex ][ 0 ];
+
+		expect( hookName ).toBe( 'native.block_editor_props' );
+		expect( hookCallOrder ).toBeLessThan( onRenderEditorCallOrder );
+	} );
+
+	it( 'dispatches "native.render" hook after the editor is rendered', () => {
+		const doAction = jest.spyOn( wpHooks, 'doAction' );
+
+		// An empty component is provided in order to listen for render calls of the editor component.
+		const onRenderEditor = jest.fn();
+		const EditorComponent = () => {
+			onRenderEditor();
+			return null;
+		};
+		jest.mock( '../setup', () => ( {
+			__esModule: true,
+			default: jest.fn().mockReturnValue( <EditorComponent /> ),
+		} ) );
+
+		initGutenberg();
+
+		const hookCallIndex = 1;
+		// "invocationCallOrder" can be used to compare call orders between different mocks.
+		// Reference: https://git.io/JyBk0
+		const hookCallOrder =
+			doAction.mock.invocationCallOrder[ hookCallIndex ];
+		const onRenderEditorCallOrder =
+			onRenderEditor.mock.invocationCallOrder[ 0 ];
+		const hookName = doAction.mock.calls[ hookCallIndex ][ 0 ];
+
+		expect( hookName ).toBe( 'native.render' );
+		expect( hookCallOrder ).toBeGreaterThan( onRenderEditorCallOrder );
+	} );
+} );
