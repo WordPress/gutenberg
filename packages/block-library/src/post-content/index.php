9a0aef46863e07c237563f20a4d09b7f5899eb9b
Alberto Marin <albertomr84@gmail.com>
2021-12-31 07:25:14 +0000

    fix php styles

--- packages/block-library/src/post-content/index.php
+++ packages/block-library/src/post-content/index.php
@@ -46,8 +46,8 @@ function render_block_core_post_content( $attributes, $content, $block ) {
 	// so that `the_preview` for the current post can apply.
 	// We force this behavior by omitting the third argument (post ID) from the `get_the_content`.
 	$content = get_the_content( null, false );
-	// Check for nextpage to display page links for paginated posts
-	$nextpage = has_block('nextpage') ? wp_link_pages(['echo' => 0]) : false;
+	// Check for nextpage to display page links for paginated posts.
+	$nextpage = has_block( 'nextpage' ) ? wp_link_pages( array( 'echo' => 0 ) ) : false;
 	if ( false !== $nextpage ) {
 		$content = get_the_content() . $nextpage;
 	}
