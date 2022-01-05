3200d91b9bbfc3e4a5376073bd1c5c1b9e00317b
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2022-01-05 14:50:48 +0100

    Mobile - RichText/AztecView - Enable customizing the font size for the Heading and Preformatted block. (#37663)
    * Mobile - Default values - Remove the default font sizes for standard themes
    
    * Mobile - RichText/AztecView:
    - Update the font size if the tag changes, e.g changing the level of a Heading block.
    - Send a new flag to AztecView: blockUseDefaultFont, to enable using the defaultFont for the Heading and Preformatted block to be able to customize the font size for block-based themes.
    
    * Mobile - Update snapshots (Audio, File, and Search) to include the new attribute tag within the text object in RichText.
    
    * Mobile - RichText - Global styles check
    
    * Mobile - AztecView - Remove optional definition for blockUseDefaultFont
    
    * Mobile - Update Aztec version

--- packages/rich-text/src/component/index.native.js
+++ packages/rich-text/src/component/index.native.js
@@ -718,7 +718,7 @@ export class RichText extends Component {
 		//   see https://github.com/WordPress/gutenberg/blob/82e578dcc75e67891c750a41a04c1e31994192fc/packages/react-native-aztec/android/src/main/java/org/wordpress/mobile/ReactNativeAztec/ReactAztecManager.java#L213-L215
 	}
 
-	shouldComponentUpdate( nextProps ) {
+	shouldComponentUpdate( nextProps, nextState ) {
 		if (
 			nextProps.tagName !== this.props.tagName ||
 			nextProps.reversed !== this.props.reversed ||
@@ -776,7 +776,10 @@ export class RichText extends Component {
 			}
 
 			if (
-				nextProps?.style?.fontSize !== this.props?.style?.fontSize ||
+				( nextProps?.style?.fontSize !== this.props?.style?.fontSize &&
+					nextState.currentFontSize !==
+						this.state.currentFontSize ) ||
+				nextState.currentFontSize !== this.state.currentFontSize ||
 				nextProps?.style?.lineHeight !== this.props?.style?.lineHeight
 			) {
 				this.needsSelectionUpdate = true;
@@ -811,7 +814,7 @@ export class RichText extends Component {
 	}
 
 	componentDidUpdate( prevProps ) {
-		const { style } = this.props;
+		const { style, tagName } = this.props;
 		const { currentFontSize } = this.state;
 
 		if ( this.props.value !== this.value ) {
@@ -835,10 +838,12 @@ export class RichText extends Component {
 
 		const currentFontSizeStyle = parseFloat( style?.fontSize );
 		const prevFontSizeStyle = parseFloat( prevProps?.style?.fontSize );
+		const isDifferentTag = prevProps.tagName !== tagName;
 		if (
-			currentFontSize &&
-			( currentFontSizeStyle || prevFontSizeStyle ) &&
-			currentFontSizeStyle !== currentFontSize
+			( currentFontSize &&
+				( currentFontSizeStyle || prevFontSizeStyle ) &&
+				currentFontSizeStyle !== currentFontSize ) ||
+			isDifferentTag
 		) {
 			this.setState( {
 				currentFontSize: this.getFontSize( this.props ),
@@ -945,6 +950,22 @@ export class RichText extends Component {
 		return lineHeight;
 	}
 
+	getBlockUseDefaultFont() {
+		// For block-based themes it enables using the defaultFont
+		// in Aztec for iOS so it allows customizing the font size
+		// for the Preformatted/Code and Heading blocks.
+		if ( ! this.isIOS ) {
+			return;
+		}
+
+		const { baseGlobalStyles, tagName } = this.props;
+		const isBlockBasedTheme =
+			baseGlobalStyles && Object.entries( baseGlobalStyles ).length !== 0;
+		const tagsToMatch = /pre|h([1-6])$/gm;
+
+		return isBlockBasedTheme && tagsToMatch.test( tagName );
+	}
+
 	render() {
 		const {
 			tagName,
@@ -965,6 +986,7 @@ export class RichText extends Component {
 		const record = this.getRecord();
 		const html = this.getHtmlToRender( record, tagName );
 		const editableProps = this.getEditableProps();
+		const blockUseDefaultFont = this.getBlockUseDefaultFont();
 
 		const placeholderStyle = getStylesFromColorScheme(
 			styles.richTextPlaceholder,
@@ -1070,12 +1092,14 @@ export class RichText extends Component {
 							: { maxWidth } ),
 						minHeight: this.state.height,
 					} }
+					blockUseDefaultFont={ blockUseDefaultFont }
 					text={ {
 						text: html,
 						eventCount: this.lastEventCount,
 						selection,
 						linkTextColor:
 							style?.linkColor || defaultTextDecorationColor,
+						tag: tagName,
 					} }
 					placeholder={ this.props.placeholder }
 					placeholderTextColor={
