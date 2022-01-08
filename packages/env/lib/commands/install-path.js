1d31f4eefab908669bb859bb9956c5209987e335
Bruno Ribarić <bruno@ribarich.me>
2022-01-08 11:09:04 +0100

    Add file existence checks

--- packages/env/lib/commands/install-path.js
+++ packages/env/lib/commands/install-path.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 const path = require( 'path' );
+const fs = require( 'fs' );
 
 /**
  * Internal dependencies
@@ -12,8 +13,20 @@ const { readConfig } = require( '../config' );
  * Logs the path to where wp-env files are installed.
  */
 module.exports = async function installPath() {
-	const { workDirectoryPath } = await readConfig(
-		path.resolve( '.wp-env.json' )
-	);
+	const configPath = path.resolve( '.wp-env.json' );
+	if ( ! fs.existsSync( configPath ) ) {
+		console.error( 'Error: .wp-env.json file not found.' );
+		return;
+	}
+
+	const { workDirectoryPath } = await readConfig( configPath );
+
+	if ( ! fs.existsSync( workDirectoryPath ) ) {
+		console.error(
+			'Error: Environment has not yet been created. Try running `wp-env start`.'
+		);
+		return;
+	}
+
 	console.log( workDirectoryPath );
 };
