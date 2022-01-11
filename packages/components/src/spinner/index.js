27d55a485d4eae3b2ecb22989544d89ad609cddb
James Koster <james@jameskoster.co.uk>
2022-01-11 16:13:44 +0000

    Revert to html + css, and simplify the animation

--- packages/components/src/spinner/index.js
+++ packages/components/src/spinner/index.js
@@ -1,9 +1,4 @@
 /**
- * WordPress dependencies
- */
-import { SVG, Circle } from '@wordpress/primitives';
-
-/**
  * External dependencies
  */
 import styled from '@emotion/styled';
@@ -17,30 +12,12 @@ export const StyledSpinner = styled.span`
 	width: ${ CONFIG.spinnerSize };
 	height: ${ CONFIG.spinnerSize };
 	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	position: relative;
-	color: var( --wp-admin-theme-color );
-	box-shadow: inset 0 0 0 1.5px ${ COLORS.gray[ 400 ] };
 	border-radius: 50%;
+	box-shadow: inset 0 0 0 1.5px ${ COLORS.gray[ 400 ] };
+	margin: auto;
+	position: relative;
 `;
 
 export default function Spinner() {
-	return (
-		<StyledSpinner className="components-spinner">
-			<SVG
-				className="components-spinner--svg"
-				viewBox="0 0 18 18"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<Circle
-					className="components-spinner--circle"
-					cx="50%"
-					cy="50%"
-					r="8.25"
-					vectorEffect="non-scaling-stroke"
-				/>
-			</SVG>
-		</StyledSpinner>
-	);
+	return <StyledSpinner className="components-spinner" />;
 }
