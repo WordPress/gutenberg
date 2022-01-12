8f3737b9310da84c0cc47d4ded061c958fccc3f1
Dave Smith <getdavemail@gmail.com>
2022-01-12 12:26:08 +0000

    Move escaping of post content to be as late as possible

--- packages/block-library/src/latest-posts/index.php
+++ packages/block-library/src/latest-posts/index.php
@@ -150,15 +150,15 @@ function render_block_core_latest_posts( $attributes ) {
 		if ( isset( $attributes['displayPostContent'] ) && $attributes['displayPostContent']
 			&& isset( $attributes['displayPostContentRadio'] ) && 'full_post' === $attributes['displayPostContentRadio'] ) {
 
-			$post_content = wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) );
+			$post_content = html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) );
 
 			if ( post_password_required( $post ) ) {
-				$post_content = __( 'This content is password protected.' );
+				$post_content = esc_html__( 'This content is password protected.' );
 			}
 
 			$list_items_markup .= sprintf(
 				'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
-				esc_html( $post_content )
+				wp_kses_post( $post_content )
 			);
 		}
 
