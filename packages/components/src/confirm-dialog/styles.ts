e52736d74c18502b75a96f0b3ffec05549f14043
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-01-12 15:58:07 -0500

    increase ConfirmDialog overlay's z-index to render above popovers

---
+++ packages/components/src/confirm-dialog/styles.ts
@@ -0,0 +1,16 @@
+/**
+ * External dependencies
+ */
+import { css } from '@emotion/react';
+
+/**
+ * The z-index for ConfirmDialog is being set here instead of in
+ * packages/base-styles/_z-index.scss, because this component uses
+ * emotion instead of sass.
+ *
+ * ConfirmDialog needs this higher z-index to ensure it renders on top of
+ * any parent Popover component.
+ */
+export const wrapper = css`
+	z-index: 1000001;
+`;
