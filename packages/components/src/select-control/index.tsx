8aa94cc17b774a239430d12ab3aeaab1aa86519f
Greg Ziółkowski <grzegorz@gziolo.pl>
2022-01-12 08:20:27 +0100

    Testing: Allow type imports for React everywhere (#37862)

--- packages/components/src/select-control/index.tsx
+++ packages/components/src/select-control/index.tsx
@@ -3,7 +3,6 @@
  */
 import { isEmpty, noop } from 'lodash';
 import classNames from 'classnames';
-// eslint-disable-next-line no-restricted-imports
 import type { ChangeEvent, FocusEvent, Ref } from 'react';
 
 /**
