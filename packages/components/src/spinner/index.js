f8a34e1f71ba5b3b221371124353b45a1d88f4e1
James Koster <james@jameskoster.co.uk>
2022-01-12 12:40:09 +0000

    Back to SVG

--- packages/components/src/spinner/index.js
+++ packages/components/src/spinner/index.js
@@ -8,16 +8,45 @@ import styled from '@emotion/styled';
  */
 import { COLORS, CONFIG } from '../utils';
 
+/**
+ * WordPress dependencies
+ */
+import { SVG, Circle } from '@wordpress/primitives';
+
 export const StyledSpinner = styled.span`
-	width: ${ CONFIG.spinnerSize };
-	height: ${ CONFIG.spinnerSize };
-	display: block;
-	border-radius: 50%;
-	box-shadow: inset 0 0 0 1.5px ${ COLORS.gray[ 400 ] };
-	margin: auto;
-	position: relative;
+	width: ${ CONFIG.spinnerSize }px;
+	height: ${ CONFIG.spinnerSize }px;
+	box-shadow: inset 0 0 0 0.75px ${ COLORS.gray[ 400 ] },
+		0 0 0 0.75px ${ COLORS.gray[ 400 ] };
 `;
 
 export default function Spinner() {
-	return <StyledSpinner className="components-spinner" />;
+	const viewBox = `0 0 ${ CONFIG.spinnerSize } ${ CONFIG.spinnerSize }`;
+	const radius = Number( CONFIG.spinnerSize ) / 2;
+	const strokeDasharray = [
+		Number( CONFIG.spinnerSize ),
+		Number( CONFIG.spinnerSize ) * 10,
+	];
+
+	return (
+		<StyledSpinner className="components-spinner">
+			<SVG
+				viewBox={ viewBox }
+				xmlns="http://www.w3.org/2000/svg"
+				className="components-spinner__track"
+				role="img"
+				aria-hidden="true"
+				focusable="false"
+			>
+				<Circle
+					className="components-spinner__indicator"
+					cx="50%"
+					cy="50%"
+					r={ radius }
+					stroke-dasharray={ strokeDasharray }
+					vector-effect="non-scaling-stroke"
+				/>
+			</SVG>
+		</StyledSpinner>
+	);
 }
