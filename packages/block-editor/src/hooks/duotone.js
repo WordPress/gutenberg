3e07d40a946a27b742635b0cda660993b1b6442f
Alex Lende <alex@lende.xyz>
2022-01-12 16:22:11 -0600

    Rename Filter to PresetSvgFilter in HOC

--- packages/block-editor/src/hooks/duotone.js
+++ packages/block-editor/src/hooks/duotone.js
@@ -363,10 +363,10 @@ const withDuotoneStyles = createHigherOrderComponent(
 );
 
 const withDuotoneFilter = createHigherOrderComponent(
-	( Filters ) => ( props ) => {
+	( PresetSvgFilter ) => ( props ) => {
 		return (
 			<>
-				<Filters />
+				<PresetSvgFilter />
 				<DuotoneFilter
 					id={ `wp-duotone-${ props.preset.slug }` }
 					values={ getValuesFromColors( props.preset.colors ) }
