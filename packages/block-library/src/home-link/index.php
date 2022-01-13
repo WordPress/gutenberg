bd645d27dbec903db92d4e21501871baf2672174
Dave Smith <getdavemail@gmail.com>
2022-01-13 10:21:26 +0000

    Remove escaping of entire attribute.
    Only works on values. As this value is unlikely to change we can omit escaping.

--- packages/block-library/src/home-link/index.php
+++ packages/block-library/src/home-link/index.php
@@ -129,7 +129,7 @@ function render_block_core_home_link( $attributes, $content, $block ) {
 
 	$aria_current = is_home() || ( is_front_page() && 'page' === get_option( 'show_on_front' ) ) ? ' aria-current="page"' : '';
 
-	$html = '<li ' . $wrapper_attributes . '><a class="wp-block-home-link__content wp-block-navigation-item__content" rel="home"' . esc_attr( $aria_current );
+	$html = '<li ' . $wrapper_attributes . '><a class="wp-block-home-link__content wp-block-navigation-item__content" rel="home"' . $aria_current;
 
 	// Start appending HTML attributes to anchor tag.
 	$html .= ' href="' . esc_url( home_url() ) . '"';
