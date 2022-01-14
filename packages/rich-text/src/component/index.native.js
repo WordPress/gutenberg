e7eb6d889f080bbacbd4b6efb660306d95edcfa8
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2022-01-14 15:06:58 +0100

    [Mobile] - RichText - Use parsed font size values when comparing new changes (#37951)
    * Mobile - RichText - Use parsed font size values when comparing new changes to avoid not matching values that generate a render loop
    
    * Mobile - E2E - Move typography test to full tests instead of canary
    
    * Mobile - RichText - Avoid using the fontSize value from the props if there's a font size set from the styles
    
    * Mobile - E2E - Remove test and move some test cases into the current integration tests
    
    * Mobile - RichTest - Update tests

--- packages/rich-text/src/component/index.native.js
+++ packages/rich-text/src/component/index.native.js
@@ -836,8 +836,14 @@ export class RichText extends Component {
 			this._editor.blur();
 		}
 
-		const currentFontSizeStyle = parseFloat( style?.fontSize );
-		const prevFontSizeStyle = parseFloat( prevProps?.style?.fontSize );
+		// For font size values changes from the font size picker
+		// we compare previous values to refresh the selected font size,
+		// this is also used when the tag name changes
+		// e.g Heading block and a level change like h1->h2.
+		const currentFontSizeStyle = this.getParsedFontSize( style?.fontSize );
+		const prevFontSizeStyle = this.getParsedFontSize(
+			prevProps?.style?.fontSize
+		);
 		const isDifferentTag = prevProps.tagName !== tagName;
 		if (
 			( currentFontSize &&
@@ -891,6 +897,20 @@ export class RichText extends Component {
 		};
 	}
 
+	getParsedFontSize( fontSize ) {
+		const { height, width } = Dimensions.get( 'window' );
+		const cssUnitOptions = { height, width, fontSize: DEFAULT_FONT_SIZE };
+
+		if ( ! fontSize ) {
+			return fontSize;
+		}
+
+		const selectedPxValue =
+			getPxFromCssUnit( fontSize, cssUnitOptions ) ?? DEFAULT_FONT_SIZE;
+
+		return parseFloat( selectedPxValue );
+	}
+
 	getFontSize( props ) {
 		const { baseGlobalStyles, tagName, fontSize, style } = props;
 		const tagNameFontSize =
@@ -898,30 +918,34 @@ export class RichText extends Component {
 
 		let newFontSize = DEFAULT_FONT_SIZE;
 
+		// For block-based themes, get the default editor font size.
 		if ( baseGlobalStyles?.typography?.fontSize ) {
 			newFontSize = baseGlobalStyles?.typography?.fontSize;
 		}
 
+		// For block-based themes, get the default element font size
+		// e.g h1, h2.
 		if ( tagNameFontSize ) {
 			newFontSize = tagNameFontSize;
 		}
 
+		// For font size values provided from the styles,
+		// usually from values set from the font size picker.
 		if ( style?.fontSize ) {
 			newFontSize = style.fontSize;
 		}
 
-		if ( fontSize && ! tagNameFontSize ) {
+		// Fall-back to a font size provided from its props (if there's any)
+		// and there are no other default values to use.
+		if ( fontSize && ! tagNameFontSize && ! style?.fontSize ) {
 			newFontSize = fontSize;
 		}
-		const { height, width } = Dimensions.get( 'window' );
-		const cssUnitOptions = { height, width, fontSize: DEFAULT_FONT_SIZE };
+
 		// We need to always convert to px units because the selected value
 		// could be coming from the web where it could be stored as a different unit.
-		const selectedPxValue =
-			getPxFromCssUnit( newFontSize, cssUnitOptions ) ??
-			DEFAULT_FONT_SIZE;
+		const selectedPxValue = this.getParsedFontSize( newFontSize );
 
-		return parseFloat( selectedPxValue );
+		return selectedPxValue;
 	}
 
 	getLineHeight() {
