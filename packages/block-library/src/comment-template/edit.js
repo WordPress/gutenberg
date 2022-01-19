394660cf3b82c8eb4e47e618e4e10e45cee355dc
Carlos Bravo <hola@carlosbravo.me>
2022-01-19 14:04:43 +0100

    Add placeholder for comments block if there is no context id

--- packages/block-library/src/comment-template/edit.js
+++ packages/block-library/src/comment-template/edit.js
@@ -256,6 +256,11 @@ export default function CommentTemplateEdit( {
 
 	const { topLevelComments, blocks } = useSelect(
 		( select ) => {
+			if ( ! postId ) {
+				return {
+					rawComments: [ {} ],
+				};
+			}
 			const { getEntityRecords } = select( coreStore );
 			const { getBlocks } = select( blockEditorStore );
 
