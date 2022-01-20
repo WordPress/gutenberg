de9aa4d5fc4e47df71be8cbca77dd6b5304b8807
Carlos Garcia <fluiddot@gmail.com>
2022-01-20 19:09:47 +0100

    Remove trailing whitespace from strings

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -490,7 +490,7 @@ export class ImageEdit extends Component {
 				footerNote={
 					<>
 						{ __(
-							'Describe the purpose of the image. Leave empty if the image is purely decorative. '
+							'Describe the purpose of the image. Leave empty if the image is purely decorative.'
 						) }
 						<FooterMessageLink
 							href={
