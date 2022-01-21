febbaec7b7f474d271231f70d324fa5545ca0d1e
Marco Ciampini <marco.ciampo@gmail.com>
2022-01-21 12:49:21 +0100

    Update storybook example to show different sizes

--- packages/components/src/spinner/stories/index.js
+++ packages/components/src/spinner/stories/index.js
@@ -1,10 +1,39 @@
 /**
+ * External dependencies
+ */
+import { css } from '@emotion/react';
+
+/**
  * Internal dependencies
  */
+import { useCx } from '../../utils';
+import { space } from '../../ui/utils/space';
 import Spinner from '../';
+import { Spacer } from '../../spacer';
 
 export default { title: 'Components/Spinner', component: Spinner };
 
 export const _default = () => {
-	return <Spinner />;
+	const cx = useCx();
+	const mediumSpinner = cx( css`
+		width: ${ space( 20 ) };
+		height: ${ space( 20 ) };
+	` );
+	const largeSpinner = cx( css`
+		width: ${ space( 40 ) };
+		height: ${ space( 40 ) };
+	` );
+	return (
+		<>
+			<Spacer marginBottom={ 6 }>
+				<Spinner />
+			</Spacer>
+			<Spacer marginBottom={ 6 }>
+				<Spinner className={ mediumSpinner } />
+			</Spacer>
+			<Spacer marginBottom={ 6 }>
+				<Spinner className={ largeSpinner } />
+			</Spacer>
+		</>
+	);
 };
