4c222d89080bd78b07089d3b593cea69718d5eb5
Iulia Cazan <iulia@phpcoder.ro>
2022-01-21 15:30:27 +0200

    Adds aria-label to the search button, as accessibility enhancement

--- packages/block-library/src/search/index.php
+++ packages/block-library/src/search/index.php
@@ -84,14 +84,15 @@ function render_block_core_search( $attributes ) {
 			$button_classes        .= ' has-icon';
 			$button_internal_markup =
 				'<svg id="search-icon" class="search-icon" viewBox="0 0 24 24" width="24" height="24">
-			        <path d="M13.5 6C10.5 6 8 8.5 8 11.5c0 1.1.3 2.1.9 3l-3.4 3 1 1.1 3.4-2.9c1 .9 2.2 1.4 3.6 1.4 3 0 5.5-2.5 5.5-5.5C19 8.5 16.5 6 13.5 6zm0 9.5c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"></path>
-			    </svg>';
+					<path d="M13.5 6C10.5 6 8 8.5 8 11.5c0 1.1.3 2.1.9 3l-3.4 3 1 1.1 3.4-2.9c1 .9 2.2 1.4 3.6 1.4 3 0 5.5-2.5 5.5-5.5C19 8.5 16.5 6 13.5 6zm0 9.5c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"></path>
+				</svg>';
 		}
 
 		$button_markup = sprintf(
-			'<button type="submit" class="wp-block-search__button %s" %s>%s</button>',
+			'<button type="submit" class="wp-block-search__button %s" %s aria-label="%s">%s</button>',
 			esc_attr( $button_classes ),
 			$inline_styles['button'],
+			esc_attr( $attributes['label'] ),
 			$button_internal_markup
 		);
 	}
