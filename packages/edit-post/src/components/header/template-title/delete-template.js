edc02bdc9c533f132c7f9a526bfeab6c2476bf22
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-01-21 13:46:12 -0500

    explicitly close ConfirmDialog after confirmation

--- packages/edit-post/src/components/header/template-title/delete-template.js
+++ packages/edit-post/src/components/header/template-title/delete-template.js
@@ -51,6 +51,7 @@ export default function DeleteTemplate() {
 	const onDelete = () => {
 		clearSelectedBlock();
 		setIsEditingTemplate( false );
+		setShowConfirmDialog( false );
 
 		editPost( {
 			template: '',
