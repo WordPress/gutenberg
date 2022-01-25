c7f293e0938334f1b6a27590880ae4d6886298e7
Kai Hao <kevin830726@gmail.com>
2022-01-25 21:19:21 +0800

    Migrate new-post to Playwright

--- packages/e2e-test-utils-playwright/src/page/index.ts
+++ packages/e2e-test-utils-playwright/src/page/index.ts
@@ -6,9 +6,7 @@ import type { Browser, Page, BrowserContext } from '@playwright/test';
 /**
  * Internal dependencies
  */
-import { clickBlockToolbarButton } from './click-block-toolbar-button';
 import { createNewPost } from './create-new-post';
-import { getEditedPostContent } from './get-edited-post-content';
 import { getPageError } from './get-page-error';
 import { insertBlock } from './insert-block';
 import { isCurrentURL } from './is-current-url';
@@ -30,9 +28,7 @@ class PageUtils {
 		this.browser = this.context.browser()!;
 	}
 
-	clickBlockToolbarButton = clickBlockToolbarButton;
 	createNewPost = createNewPost;
-	getEditedPostContent = getEditedPostContent;
 	getPageError = getPageError;
 	insertBlock = insertBlock;
 	isCurrentURL = isCurrentURL;
