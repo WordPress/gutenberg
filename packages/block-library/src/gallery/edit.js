78aca5635d87d44d39e4e99e7c14da634a5a283a
Carlos Garcia <fluiddot@gmail.com>
2022-01-26 11:57:11 +0100

    Set MediaPlaceholder props by platform in gallery

--- packages/block-library/src/gallery/edit.js
+++ packages/block-library/src/gallery/edit.js
@@ -429,24 +429,45 @@ function GalleryEdit( props ) {
 		( img ) => ! img.id && img.url?.indexOf( 'blob:' ) === 0
 	);
 
-	const mediaPlaceholder = (
-		<MediaPlaceholder
-			addToGallery={ false }
-			handleUpload={ false }
-			disableMediaButtons={ imagesUploading }
-			icon={ sharedIcon }
-			labels={ {
+	// MediaPlaceholder props are different between web and native hence, we provide a platform-specific set.
+	const mediaPlaceholderProps = Platform.select( {
+		web: {
+			addToGallery: false,
+			disableMediaButtons: imagesUploading,
+			icon: sharedIcon,
+			labels: {
 				title: __( 'Gallery' ),
 				instructions: PLACEHOLDER_TEXT,
-			} }
+			},
+			value: {},
+			notices: noticeUI,
+			autoOpenMediaUpload: isSelected && wasBlockJustInserted,
+		},
+		native: {
+			addToGallery: hasImageIds,
+			isAppender: hasImages,
+			disableMediaButtons:
+				( hasImages && ! isSelected ) || imagesUploading,
+			icon: ! hasImages && sharedIcon,
+			labels: {
+				title: ! hasImages && __( 'Gallery' ),
+				instructions: ! hasImages && PLACEHOLDER_TEXT,
+			},
+			value: hasImageIds ? images : {},
+			notices: hasImages ? undefined : noticeUI,
+			autoOpenMediaUpload:
+				! hasImages && isSelected && wasBlockJustInserted,
+		},
+	} );
+	const mediaPlaceholder = (
+		<MediaPlaceholder
+			handleUpload={ false }
 			onSelect={ updateImages }
 			accept="image/*"
 			allowedTypes={ ALLOWED_MEDIA_TYPES }
 			multiple
-			value={ {} }
 			onError={ onUploadError }
-			notices={ noticeUI }
-			autoOpenMediaUpload={ isSelected && wasBlockJustInserted }
+			{ ...mediaPlaceholderProps }
 		/>
 	);
 
@@ -537,7 +558,11 @@ function GalleryEdit( props ) {
 			<Gallery
 				{ ...props }
 				images={ images }
-				mediaPlaceholder={ ! hasImages ? mediaPlaceholder : undefined }
+				mediaPlaceholder={
+					! hasImages || Platform.isNative
+						? mediaPlaceholder
+						: undefined
+				}
 				blockProps={ blockProps }
 				insertBlocksAfter={ insertBlocksAfter }
 			/>
