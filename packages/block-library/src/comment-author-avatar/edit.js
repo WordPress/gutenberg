10aeebbcf9e618788fc990a16b4baad97db0f548
Carlos Bravo <hola@carlosbravo.me>
2022-01-27 14:59:54 +0100

    Add placeholder for comment avatar

--- packages/block-library/src/comment-author-avatar/edit.js
+++ packages/block-library/src/comment-author-avatar/edit.js
@@ -6,9 +6,15 @@ import {
 	useBlockProps,
 	__experimentalGetSpacingClassesAndStyles as useSpacingProps,
 } from '@wordpress/block-editor';
-import { PanelBody, ResizableBox, RangeControl } from '@wordpress/components';
+import {
+	PanelBody,
+	ResizableBox,
+	RangeControl,
+	SVG,
+	Path,
+} from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
-import { __, _x, isRTL } from '@wordpress/i18n';
+import { __, isRTL } from '@wordpress/i18n';
 
 export default function Edit( {
 	attributes,
@@ -89,9 +95,40 @@ export default function Edit( {
 			/>
 		</ResizableBox>
 	) : (
-		<p { ...blockProps }>
-			{ _x( 'Comment Author Avatar', 'block title' ) }
-		</p>
+		<div { ...blockProps }>
+			<ResizableBox
+				size={ {
+					width,
+					height,
+				} }
+				showHandle={ isSelected }
+				onResizeStop={ ( event, direction, elt, delta ) => {
+					setAttributes( {
+						height: parseInt( height + delta.height, 10 ),
+						width: parseInt( width + delta.width, 10 ),
+					} );
+				} }
+				lockAspectRatio
+				enable={ {
+					top: false,
+					right: ! isRTL(),
+					bottom: true,
+					left: isRTL(),
+				} }
+				minWidth={ minSize }
+				maxWidth={ maxSizeBuffer }
+			>
+				<SVG
+					className="wp-block-comment-author-avatar__placeholder"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 60 60"
+					preserveAspectRatio="none"
+				>
+					<Path vectorEffect="non-scaling-stroke" d="M60 60 0 0" />
+				</SVG>
+			</ResizableBox>
+		</div>
 	);
 
 	return (
