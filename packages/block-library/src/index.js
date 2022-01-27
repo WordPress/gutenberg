dd62a8fc2744433b7a8e08f206dd1429c93ad0cd
Carolina Nymark <myazalea@hotmail.com>
2022-01-27 04:44:53 +0100

    Add post author avatar block

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -59,6 +59,7 @@ import * as pattern from './pattern';
 import * as pageList from './page-list';
 import * as paragraph from './paragraph';
 import * as postAuthor from './post-author';
+import * as postAuthorAvatar from './post-author-avatar';
 import * as postAuthorName from './post-author-name';
 import * as postAuthorBiography from './post-author-biography';
 import * as postComment from './post-comment';
@@ -207,6 +208,7 @@ export const __experimentalGetCoreBlocks = () => [
 	queryTitle,
 	postAuthorName,
 	postAuthorBiography,
+	postAuthorAvatar,
 ];
 
 /**
