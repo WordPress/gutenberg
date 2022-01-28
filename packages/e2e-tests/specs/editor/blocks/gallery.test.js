6e237632d2995b5d1bccc0f8c446002fa10b4e81
ramonjd <ramonjd@gmail.com>
2022-01-28 16:15:26 +1100

    waiting for selector

--- packages/e2e-tests/specs/editor/blocks/gallery.test.js
+++ packages/e2e-tests/specs/editor/blocks/gallery.test.js
@@ -66,6 +66,8 @@ describe( 'Gallery', () => {
 		);
 		await selectBlockByClientId( galleryBlock.clientId );
 
+		await page.waitForSelector( '.wp-block-cover img["src^="http://"]' );
+
 		await page.click( '.wp-block-gallery>.blocks-gallery-caption' );
 		await page.keyboard.type( galleryCaption );
 
