9258ba4b127ba0f5735bae3b5dd584e3203b3cfb
Mustaque Ahmed <amustaque97@gmail.com>
2022-01-28 15:07:45 +0530

    fix php lint error replace tabs with spaces during mid-line alignment

--- packages/block-library/src/tag-cloud/index.php
+++ packages/block-library/src/tag-cloud/index.php
@@ -15,7 +15,7 @@
 function render_block_core_tag_cloud( $attributes ) {
 	$args      = array(
 		'echo'       => false,
-		'unit'		 => 'px',
+		'unit'       => 'px',
 		'taxonomy'   => $attributes['taxonomy'],
 		'show_count' => $attributes['showTagCounts'],
 		'number'     => $attributes['numberOfTags'],
