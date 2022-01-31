7cc692ad683274b2396b780a3a921b7779adbe13
Glen Davies <glen.davies@a8c.com>
2022-01-31 13:15:35 +1300

    Rationalise switch statements for assigning linkTo value

--- packages/block-library/src/gallery/utils.js
+++ packages/block-library/src/gallery/utils.js
@@ -5,6 +5,8 @@ import {
 	LINK_DESTINATION_ATTACHMENT,
 	LINK_DESTINATION_MEDIA,
 	LINK_DESTINATION_NONE,
+	LINK_DESTINATION_MEDIA_WP_CORE,
+	LINK_DESTINATION_ATTACHMENT_WP_CORE,
 } from './constants';
 import {
 	LINK_DESTINATION_ATTACHMENT as IMAGE_LINK_DESTINATION_ATTACHMENT,
@@ -21,24 +23,17 @@ import {
  * @return {Object}            New attributes to assign to image block.
  */
 export function getHrefAndDestination( image, destination ) {
-	// Need to determine the URL that the selected destination maps to.
 	// Gutenberg and WordPress use different constants so if image_default_link_type
-	// option is set we need to map file -> media and post -> attachment
-	switch ( destination ) {
-		case 'file':
-			destination = LINK_DESTINATION_MEDIA;
-			break;
-		case 'post':
-			destination = LINK_DESTINATION_ATTACHMENT;
-			break;
-	}
+	// option is set we need to map from the WP Core values.
 
 	switch ( destination ) {
+		case LINK_DESTINATION_MEDIA_WP_CORE:
 		case LINK_DESTINATION_MEDIA:
 			return {
 				href: image?.source_url || image?.url, // eslint-disable-line camelcase
 				linkDestination: IMAGE_LINK_DESTINATION_MEDIA,
 			};
+		case LINK_DESTINATION_ATTACHMENT_WP_CORE:
 		case LINK_DESTINATION_ATTACHMENT:
 			return {
 				href: image?.link,
