6087e3c9379a26c363d9f2714b56305ca3e11d01
Carlos Bravo <hola@carlosbravo.me>
2022-01-31 19:35:36 +0100

    Use default avatars for placeholders

--- packages/block-library/src/comment-author-avatar/edit.js
+++ packages/block-library/src/comment-author-avatar/edit.js
@@ -86,10 +86,8 @@ export default function Edit( {
 				bottom: true,
 				left: isRTL(),
 			} }
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 60 60"
-			preserveAspectRatio="none"
+			minWidth={ minSize }
+			maxWidth={ maxSizeBuffer }
 		>
 			<img
 				src={
--- packages/block-library/src/comment-author-avatar/index.php
+++ packages/block-library/src/comment-author-avatar/index.php
@@ -46,74 +46,8 @@ function render_block_core_comment_author_avatar( $attributes, $content, $block 
 	$styles  = isset( $wrapper_attributes['style'] ) ? $wrapper_attributes['style'] : '';
 	$classes = isset( $wrapper_attributes['class'] ) ? $wrapper_attributes['class'] : '';
 
-	// Add border width styles.
-	$has_border_width = ! empty( $attributes['style']['border']['width'] );
-
-	if ( $has_border_width ) {
-		$border_width   = $attributes['style']['border']['width'];
-		$image_styles[] = sprintf( 'border-width: %s;', esc_attr( $border_width ) );
-	}
-
-	// Add border radius styles.
-	$has_border_radius = ! empty( $attributes['style']['border']['radius'] );
-
-	if ( $has_border_radius ) {
-		$border_radius = $attributes['style']['border']['radius'];
-
-		if ( is_array( $border_radius ) ) {
-			// Apply styles for individual corner border radii.
-			foreach ( $border_radius as $key => $value ) {
-				if ( null !== $value ) {
-					// Convert camelCase key to kebab-case.
-					$name = strtolower( preg_replace( '/(?<!^)[A-Z]/', '-$0', $key ) );
-
-					// Add shared styles for individual border radii.
-					$border_style   = sprintf(
-						'border-%s-radius: %s;',
-						esc_attr( $name ),
-						esc_attr( $value )
-					);
-					$image_styles[] = $border_style;
-				}
-			}
-		} else {
-			$border_style   = sprintf( 'border-radius: %s;', esc_attr( $border_radius ) );
-			$image_styles[] = $border_style;
-		}
-	}
-
-	// Add border color styles.
-	$has_border_color = ! empty( $attributes['style']['border']['color'] );
-
-	if ( $has_border_color ) {
-		$border_color   = $attributes['style']['border']['color'];
-		$image_styles[] = sprintf( 'border-color: %s;', esc_attr( $border_color ) );
-	}
-
-	// Add border style (solid, dashed, dotted ).
-	$has_border_style = ! empty( $attributes['style']['border']['style'] );
-
-	if ( $has_border_style ) {
-		$border_style   = $attributes['style']['border']['style'];
-		$image_styles[] = sprintf( 'border-style: %s;', esc_attr( $border_style ) );
-	}
-
-	// Add border classes to the avatar image for both custom colors and palette colors.
-	$image_classes = '';
-	if ( $has_border_color || isset( $attributes['borderColor'] ) ) {
-		$image_classes .= 'has-border-color';
-	}
-	if ( isset( $attributes['borderColor'] ) ) {
-		$image_classes .= ' has-' . $attributes['borderColor'] . '-border-color';
-	}
-
 	/* translators: %s is the Comment Author name */
 	$alt = sprintf( __( '%s Avatar' ), $comment->comment_author );
-
-	$border_styles = safecss_filter_attr( implode( ' ', $image_styles ) );
-
-	$classes = $classes . ' ' . $image_classes;
-	$styles  = $styles . ' ' . $border_styles;
 
 	$avatar_block = get_avatar(
 		$comment,
