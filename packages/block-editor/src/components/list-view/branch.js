f0eb98248cf1e273658f12f908f611a7c43fa82a
Ramon <ramonjd@users.noreply.github.com>
2022-02-01 16:01:12 +1100

    Revert export

--- packages/block-editor/src/components/list-view/branch.js
+++ packages/block-editor/src/components/list-view/branch.js
@@ -28,7 +28,7 @@ import { isClientIdSelected } from './utils';
  * @param {Array}  draggedClientIds a list of dragged client ids
  * @return {number} block count
  */
-export function countBlocks( block, expandedState, draggedClientIds ) {
+function countBlocks( block, expandedState, draggedClientIds ) {
 	const isDragged = draggedClientIds?.includes( block.clientId );
 	if ( isDragged ) {
 		return 0;
