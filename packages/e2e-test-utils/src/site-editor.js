c7ef24a841a07be8780a87e5c58975470f8ac921
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-02-01 02:24:00 +0100

    Refactor `getEditedPostContent` into own function
    That didn't seem to be used anywhere. Perhaps a bit of a red flag.
    Also decided not to go with adding an optional parameter to the
    existing `getEditedPostContent` because the two functions differed
    significantly. Perhaps it is not very consistent, however.

--- packages/e2e-test-utils/src/site-editor.js
+++ packages/e2e-test-utils/src/site-editor.js
@@ -8,6 +8,42 @@ const SELECTORS = {
 	visualEditor: '.edit-site-visual-editor iframe',
 };
 
+/**
+ * Returns a promise which resolves with the edited post content (HTML string).
+ *
+ * @return {Promise<string>} Promise resolving with post content markup.
+ */
+export function getEditedPostContent() {
+	return page.evaluate( () => {
+		const postId = window.wp.data
+			.select( 'core/edit-site' )
+			.getEditedPostId();
+		const postType = window.wp.data
+			.select( 'core/edit-site' )
+			.getEditedPostType();
+		const record = window.wp.data
+			.select( 'core' )
+			.getEditedEntityRecord( 'postType', postType, postId );
+		if ( record ) {
+			if ( typeof record.content === 'function' ) {
+				return record.content( record );
+			} else if ( record.blocks ) {
+				return window.wp.blocks.__unstableSerializeAndClean(
+					record.blocks
+				);
+			} else if ( record.content ) {
+				return record.content;
+			}
+		}
+		return '';
+	} );
+}
+
+/**
+ * Visits the Site Editor main page
+ *
+ * @param {string} query String to be serialized as query portion of URL.
+ */
 export async function goToSiteEditor( query ) {
 	query = addQueryArgs( '', {
 		page: 'gutenberg-edit-site',
