70739186e08a78427cec9f03ce495e92233113b6
Carlos Garcia <fluiddot@gmail.com>
2022-02-02 13:57:23 +0100

    Add test case: handles failed uploads

--- packages/block-library/src/gallery/test/index.native.js
+++ packages/block-library/src/gallery/test/index.native.js
@@ -19,6 +19,7 @@ import { Platform } from '@wordpress/element';
 import {
 	MEDIA_UPLOAD_STATE_UPLOADING,
 	MEDIA_UPLOAD_STATE_SUCCEEDED,
+	MEDIA_UPLOAD_STATE_FAILED,
 } from '@wordpress/block-editor';
 import {
 	requestMediaPicker,
@@ -413,6 +414,116 @@ describe( 'Gallery block', () => {
 			} );
 		} );
 
+		expect( getEditorHtml() ).toMatchSnapshot();
+	} );
+
+	it( 'handles failed uploads (TC006 - Choose from device (stay in editor) - Failed upload)', async () => {
+		const mediaUploadListeners = [];
+		subscribeMediaUpload.mockImplementation( ( callback ) => {
+			mediaUploadListeners.push( callback );
+			return { remove: jest.fn() };
+		} );
+		const triggerMediaUpload = ( payload ) =>
+			mediaUploadListeners.forEach( ( listener ) => listener( payload ) );
+
+		let mediaPickerCallback;
+		requestMediaPicker.mockImplementation(
+			( source, filter, multiple, callback ) => {
+				mediaPickerCallback = callback;
+			}
+		);
+
+		// Initialize with an empty gallery
+		const { galleryBlock, getByText } = initializeWithGalleryBlock(
+			GALLERY_EMPTY,
+			{
+				hasItems: false,
+			}
+		);
+
+		// Upload images from device
+		fireEvent.press( getByText( 'ADD MEDIA' ) );
+		fireEvent.press( getByText( 'Choose from device' ) );
+		expect( requestMediaPicker ).toHaveBeenCalledWith(
+			'DEVICE_MEDIA_LIBRARY',
+			[ 'image' ],
+			true,
+			mediaPickerCallback
+		);
+
+		// Return media items picked
+		await act( async () =>
+			mediaPickerCallback( [
+				{
+					type: 'image',
+					url: MEDIA[ 0 ].localUrl,
+					id: MEDIA[ 0 ].localId,
+				},
+				{
+					type: 'image',
+					url: MEDIA[ 1 ].localUrl,
+					id: MEDIA[ 1 ].localId,
+				},
+			] )
+		);
+
+		// Check that gallery items are visible
+		fireEvent(
+			within( galleryBlock ).getByTestId( 'block-list-wrapper' ),
+			'layout',
+			{
+				nativeEvent: {
+					layout: {
+						width: 100,
+					},
+				},
+			}
+		);
+		const galleryItem1 = within( galleryBlock ).getByA11yLabel(
+			/Image Block\. Row 1/
+		);
+		const galleryItem2 = within( galleryBlock ).getByA11yLabel(
+			/Image Block\. Row 2/
+		);
+		expect( galleryItem1 ).toBeVisible();
+		expect( galleryItem2 ).toBeVisible();
+
+		// Check that images are showing a loading state
+		await act( async () => {
+			triggerMediaUpload( {
+				state: MEDIA_UPLOAD_STATE_UPLOADING,
+				mediaId: MEDIA[ 0 ].localId,
+				progress: 0.5,
+			} );
+			triggerMediaUpload( {
+				state: MEDIA_UPLOAD_STATE_UPLOADING,
+				mediaId: MEDIA[ 1 ].localId,
+				progress: 0.25,
+			} );
+		} );
+		expect( within( galleryItem1 ).getByTestId( 'spinner' ) ).toBeVisible();
+		expect( within( galleryItem2 ).getByTestId( 'spinner' ) ).toBeVisible();
+
+		// Notify that the media items uploads failed
+		await act( async () => {
+			triggerMediaUpload( {
+				state: MEDIA_UPLOAD_STATE_FAILED,
+				mediaId: MEDIA[ 0 ].localId,
+				progress: 0.5,
+			} );
+			triggerMediaUpload( {
+				state: MEDIA_UPLOAD_STATE_FAILED,
+				mediaId: MEDIA[ 1 ].localId,
+				progress: 0.25,
+			} );
+		} );
+
+		expect(
+			within( galleryItem1 ).getByText( /Failed to insert media/ )
+		).toBeVisible();
+		expect(
+			within( galleryItem2 ).getByText( /Failed to insert media/ )
+		).toBeVisible();
 		expect( getEditorHtml() ).toMatchSnapshot();
 	} );
 
