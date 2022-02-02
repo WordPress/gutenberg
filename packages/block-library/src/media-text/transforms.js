886c40de88a4d5c172be2050ae74a10045c9362f
Petter Walbø Johnsgård <petter@dekode.no>
2022-02-02 18:14:52 +0100

    Add transform between cover and media & text

--- packages/block-library/src/media-text/transforms.js
+++ packages/block-library/src/media-text/transforms.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { set } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { createBlock } from '@wordpress/blocks';
@@ -27,6 +32,55 @@ const transforms = {
 					mediaType: 'video',
 					anchor,
 				} ),
+		},
+		{
+			type: 'block',
+			blocks: [ 'core/cover' ],
+			transform: (
+				{
+					alt,
+					anchor,
+					backgroundType,
+					customGradient,
+					customOverlayColor,
+					gradient,
+					id,
+					overlayColor,
+					url,
+				},
+				innerBlocks
+			) => {
+				const additionalAttributes = {};
+
+				if ( customGradient ) {
+					set(
+						additionalAttributes,
+						'style.color.gradient',
+						customGradient
+					);
+				} else if ( customOverlayColor ) {
+					set(
+						additionalAttributes,
+						'style.color.background',
+						customOverlayColor
+					);
+				}
+
+				return createBlock(
+					'core/media-text',
+					{
+						anchor,
+						backgroundColor: overlayColor,
+						gradient,
+						mediaAlt: alt,
+						mediaId: id,
+						mediaType: backgroundType,
+						mediaUrl: url,
+						...additionalAttributes,
+					},
+					innerBlocks
+				);
+			},
 		},
 	],
 	to: [
@@ -57,6 +111,50 @@ const transforms = {
 					src: mediaUrl,
 					anchor,
 				} );
+			},
+		},
+		{
+			type: 'block',
+			blocks: [ 'core/cover' ],
+			transform: (
+				{
+					anchor,
+					backgroundColor,
+					focalPoint,
+					gradient,
+					mediaAlt,
+					mediaId,
+					mediaType,
+					mediaUrl,
+					style,
+				},
+				innerBlocks
+			) => {
+				const additionalAttributes = {};
+
+				if ( style?.color?.gradient ) {
+					additionalAttributes.customGradient = style.color.gradient;
+				} else if ( style?.color?.background ) {
+					additionalAttributes.customOverlayColor =
+						style.color.background;
+				}
+
+				return createBlock(
+					'core/cover',
+					{
+						alt: mediaAlt,
+						anchor,
+						backgroundType: mediaType,
+						dimRatio: !! mediaUrl ? 50 : 100,
+						focalPoint,
+						gradient,
+						id: mediaId,
+						overlayColor: backgroundColor,
+						url: mediaUrl,
+						...additionalAttributes,
+					},
+					innerBlocks
+				);
 			},
 		},
 	],
