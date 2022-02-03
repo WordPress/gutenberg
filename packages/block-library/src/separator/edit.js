e59364e232ae44843103634ee8ece328ad563ef4
Glen Davies <glen.davies@a8c.com>
2022-02-03 17:31:33 +1300

    Fix the editor code

--- packages/block-library/src/separator/edit.js
+++ packages/block-library/src/separator/edit.js
@@ -1,15 +1,37 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { HorizontalRule } from '@wordpress/components';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, getColorClassName } from '@wordpress/block-editor';
 
-export default function SeparatorEdit( { className } ) {
+export default function SeparatorEdit( {
+	attributes: { backgroundColor, style },
+} ) {
+	const customColor = style?.color?.background;
+	// The dots styles uses text for the dots, to change those dots color is
+	// using color, not backgroundColor.
+	const colorClass = getColorClassName( 'color', backgroundColor );
+
+	const className = classnames( {
+		'has-text-color': backgroundColor || customColor,
+		[ colorClass ]: colorClass,
+	} );
+
+	const styles = {
+		color: colorClass ? undefined : customColor,
+	};
+
 	return (
 		<>
 			<HorizontalRule
 				{ ...useBlockProps( {
 					className,
+					style: styles,
 				} ) }
 			/>
 		</>
