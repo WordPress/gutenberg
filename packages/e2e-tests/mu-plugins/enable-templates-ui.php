99b1ba6ff83d2dd842c4661c17a45f902aa80c78
George Mamadashvili <georgemamadashvili@gmail.com>
2022-02-03 16:29:12 +0400

    I want Prettier for PHP

--- packages/e2e-tests/mu-plugins/enable-templates-ui.php
+++ packages/e2e-tests/mu-plugins/enable-templates-ui.php
@@ -10,10 +10,15 @@
 /**
  * Enable Templates & Template Parts post type UI during e2e testing.
  */
-add_filter( 'register_post_type_args', function( $args, $name ) {
-	if ( in_array( $name, array( 'wp_template', 'wp_template_part' ), true ) ) {
-		$args['show_ui'] = true;
-	}
+add_filter(
+	'register_post_type_args',
+	function( $args, $name ) {
+		if ( in_array( $name, array( 'wp_template', 'wp_template_part' ), true ) ) {
+			$args['show_ui'] = true;
+		}
 
-	return $args;
-}, 20, 2 );
+		return $args;
+	},
+	20,
+	2
+);
