fa0933cb4a843321e356ca2b9311485f99a513ec
David Arenas <david.arenas@automattic.com>
2022-02-04 19:13:29 +0100

    Fix `array_reverse` and order in comment template

--- packages/block-library/src/comment-template/index.php
+++ packages/block-library/src/comment-template/index.php
@@ -68,12 +68,12 @@ function render_block_core_comment_template( $attributes, $content, $block ) {
 		return '';
 	}
 
-	$comment_order = ! empty( $block->context['comments/order'] )
+	$comment_order = ! empty( $block->context['comments/inherit'] ) && ! empty( $block->context['comments/order'] )
 		? $block->context['comments/order']
 		: get_option( 'comment_order' );
 
 	if ( 'desc' === $comment_order ) {
-		$comments = reverse_array( $comments );
+		$comments = array_reverse( $comments );
 	}
 
 	$wrapper_attributes = get_block_wrapper_attributes();
