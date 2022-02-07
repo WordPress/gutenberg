ef2dc3520981f16c28a305669e039ec78f4ed6a3
Dave Smith <getdavemail@gmail.com>
2022-02-07 10:21:58 +0000

    Comments Date block - revert core function escaping

--- packages/block-library/src/comment-date/index.php
+++ packages/block-library/src/comment-date/index.php
@@ -31,13 +31,13 @@ function render_block_core_comment_date( $attributes, $content, $block ) {
 	$link               = get_comment_link( $comment );
 
 	if ( ! empty( $attributes['isLink'] ) ) {
-		$formatted_date = sprintf( '<a href="%1s">%2s</a>', esc_url( $link ), esc_html( $formatted_date ) );
+		$formatted_date = sprintf( '<a href="%1s">%2s</a>', esc_url( $link ), $formatted_date );
 	}
 
 	return sprintf(
 		'<div %1$s><time datetime="%2$s">%3$s</time></div>',
 		$wrapper_attributes,
-		esc_attr( get_comment_date( 'c', $comment ) ),
+		get_comment_date( 'c', $comment ),
 		$formatted_date
 	);
 }
