7456e9dce1ac148f1d0e24bcb8694d3ef1fc212f
Dave Smith <getdavemail@gmail.com>
2022-02-07 13:09:33 +0000

    Restore escaping of site title
    See https://github.com/WordPress/gutenberg/pull/38569#discussion_r800534754

--- packages/block-library/src/site-title/index.php
+++ packages/block-library/src/site-title/index.php
@@ -36,7 +36,7 @@ function render_block_core_site_title( $attributes ) {
 		if ( '_blank' === $attributes['linkTarget'] ) {
 			$link_attrs[] = 'target="_blank"';
 		}
-		$site_title = sprintf( '<a %1$s>%2$s</a>', implode( ' ', $link_attrs ), $site_title );
+		$site_title = sprintf( '<a %1$s>%2$s</a>', implode( ' ', $link_attrs ), esc_html( $site_title ) );
 	}
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $align_class_name ) );
 
@@ -44,7 +44,8 @@ function render_block_core_site_title( $attributes ) {
 		'<%1$s %2$s>%3$s</%1$s>',
 		$tag_name,
 		$wrapper_attributes,
-		$site_title
+		// already pre-escaped if it is a link.
+		$attributes['isLink'] ? $site_title : esc_html( $site_title )
 	);
 }
 
