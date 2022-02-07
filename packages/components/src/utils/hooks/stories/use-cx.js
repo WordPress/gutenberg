880a2de1a85a2b1f6cbf20f8f28aedb31aa49fb7
Marco Ciampini <marco.ciampo@gmail.com>
2022-02-07 09:58:43 +0100

    Add preemptively the eslint disable comment

--- packages/components/src/utils/hooks/stories/use-cx.js
+++ packages/components/src/utils/hooks/stories/use-cx.js
@@ -32,6 +32,7 @@ const Example = ( { args, children } ) => {
 const ExampleWithUseMemoWrong = ( { args, children } ) => {
 	const cx = useCx();
 	// Wrong: using 'useMemo' without adding 'cx' to the dependency list.
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	const classes = useMemo( () => cx( ...args ), [ ...args ] );
 	return <span className={ classes }>{ children }</span>;
 };
