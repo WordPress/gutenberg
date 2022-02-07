f45331d606b9f87318ec77c3197e9bb4d35bb082
Jarda Snajdr <jsnajdr@gmail.com>
2022-02-07 14:40:05 +0100

    PostTrash: call trashPost action with no arguments, rewrite to hooks

--- packages/editor/src/components/post-trash/index.js
+++ packages/editor/src/components/post-trash/index.js
@@ -3,47 +3,35 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
-import { withSelect, withDispatch } from '@wordpress/data';
-import { compose } from '@wordpress/compose';
+import { useSelect, useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import { store as editorStore } from '../../store';
 
-function PostTrash( { isNew, postId, postType, ...props } ) {
+export default function PostTrash() {
+	const { isNew, postId } = useSelect( ( select ) => {
+		const store = select( editorStore );
+		return {
+			isNew: store.isEditedPostNew(),
+			postId: store.getCurrentPostId(),
+		};
+	} );
+	const { trashPost } = useDispatch( editorStore );
+
 	if ( isNew || ! postId ) {
 		return null;
 	}
-
-	const onClick = () => props.trashPost( postId, postType );
 
 	return (
 		<Button
 			className="editor-post-trash"
 			isDestructive
 			variant="secondary"
-			onClick={ onClick }
+			onClick={ () => trashPost() }
 		>
 			{ __( 'Move to trash' ) }
 		</Button>
 	);
 }
-
-export default compose( [
-	withSelect( ( select ) => {
-		const {
-			isEditedPostNew,
-			getCurrentPostId,
-			getCurrentPostType,
-		} = select( editorStore );
-		return {
-			isNew: isEditedPostNew(),
-			postId: getCurrentPostId(),
-			postType: getCurrentPostType(),
-		};
-	} ),
-	withDispatch( ( dispatch ) => ( {
-		trashPost: dispatch( editorStore ).trashPost,
-	} ) ),
-] )( PostTrash );
