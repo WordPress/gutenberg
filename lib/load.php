aa732f78dcf097207995c76ec4cafce16ad38eeb
André <583546+oandregal@users.noreply.github.com>
2022-02-08 20:32:38 +0100

    Rename file for a better description

--- lib/load.php
+++ lib/load.php
@@ -100,7 +100,7 @@ require __DIR__ . '/compat/wordpress-6.0/post-lock.php';
 require __DIR__ . '/compat/wordpress-6.0/blocks.php';
 require __DIR__ . '/compat/wordpress-6.0/class-gutenberg-rest-global-styles-controller.php';
 require __DIR__ . '/compat/wordpress-6.0/rest-api.php';
-require __DIR__ . '/compat/wordpress-6.0/global-styles.php';
+require __DIR__ . '/compat/wordpress-6.0/register-webfonts-from-theme-json.php';
 require __DIR__ . '/compat/wordpress-6.0/class-wp-theme-json-resolver-gutenberg.php';
 require __DIR__ . '/compat/experimental/blocks.php';
 
