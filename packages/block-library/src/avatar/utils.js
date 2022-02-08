0446e90823fbc923678bb1b0b9151cac2e94960b
Carlos Bravo <hola@carlosbravo.me>
2022-02-08 16:11:30 +0100

    Refactor edit.js code

--- packages/block-library/src/avatar/utils.js
+++ packages/block-library/src/avatar/utils.js
@@ -2,22 +2,38 @@
  * WordPress dependencies
  */
 import { store as blockEditorStore } from '@wordpress/block-editor';
-import { store as coreStore } from '@wordpress/core-data';
+import { store as coreStore, useEntityProp } from '@wordpress/core-data';
 import { __, sprintf } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
-export function getCommentAvatar( {
-	functionUseEntityProp,
-	functionUseSelect,
-	commentId,
-} ) {
-	const [ avatars ] = functionUseEntityProp(
+function getAvatarSizes( sizes ) {
+	const minSize = sizes ? sizes[ 0 ] : 24;
+	const maxSize = sizes ? sizes[ sizes.length - 1 ] : 96;
+	const maxSizeBuffer = Math.floor( maxSize * 2.5 );
+	return {
+		minSize,
+		maxSize: maxSizeBuffer,
+	};
+}
+
+function useAvatar( avatarUrls ) {
+	const { avatarURL: defaultAvatarUrl } = useSelect( ( select ) => {
+		const { getSettings } = select( blockEditorStore );
+		const { __experimentalDiscussionSettings } = getSettings();
+		return __experimentalDiscussionSettings;
+	} );
+	return avatarUrls ? avatarUrls[ avatarUrls.length - 1 ] : defaultAvatarUrl;
+}
+
+export function useCommentAvatar( { commentId } ) {
+	const [ avatars ] = useEntityProp(
 		'root',
 		'comment',
 		'author_avatar_urls',
 		commentId
 	);
 
-	const [ authorName ] = functionUseEntityProp(
+	const [ authorName ] = useEntityProp(
 		'root',
 		'comment',
 		'author_name',
@@ -25,27 +41,21 @@ export function getCommentAvatar( {
 	);
 	const avatarUrls = avatars ? Object.values( avatars ) : null;
 	const sizes = avatars ? Object.keys( avatars ) : null;
-	const minSize = sizes ? sizes[ 0 ] : 24;
-	const maxSize = sizes ? sizes[ sizes.length - 1 ] : 96;
-	const maxSizeBuffer = Math.floor( maxSize * 2.5 );
-	const { avatarURL: defaultAvatarUrl } = functionUseSelect( ( select ) => {
-		const { getSettings } = select( blockEditorStore );
-		const { __experimentalDiscussionSettings } = getSettings();
-		return __experimentalDiscussionSettings;
-	} );
+	const { minSize, maxSize } = getAvatarSizes( sizes );
 	return {
-		src: avatarUrls
-			? avatarUrls[ avatarUrls.length - 1 ]
-			: defaultAvatarUrl,
+		src: useAvatar( avatarUrls ),
 		minSize,
-		maxSize: maxSizeBuffer,
+		maxSize,
 		// translators: %s is the Author name.
-		alt: sprintf( __( '%s Avatar' ), authorName ),
+		alt: authorName
+			? // translators: %s is the Author name.
+			  sprintf( __( '%s Avatar' ), authorName )
+			: __( 'Default Avatar' ),
 	};
 }
 
-export function getUserAvatar( { postId, postType, functionUseSelect } ) {
-	const { authorDetails } = functionUseSelect(
+export function useUserAvatar( { postId, postType } ) {
+	const { authorDetails } = useSelect(
 		( select ) => {
 			const { getEditedEntityRecord, getUser } = select( coreStore );
 			const _authorId = getEditedEntityRecord(
@@ -66,21 +76,11 @@ export function getUserAvatar( { postId, postType, functionUseSelect } ) {
 	const sizes = authorDetails
 		? Object.keys( authorDetails.avatar_urls )
 		: null;
-	const minSize = sizes ? sizes[ 0 ] : 24;
-	const maxSize = sizes ? sizes[ sizes.length - 1 ] : 96;
-	const maxSizeBuffer = Math.floor( maxSize * 2.5 );
-	const { avatarURL: defaultAvatarUrl } = functionUseSelect( ( select ) => {
-		const { getSettings } = select( blockEditorStore );
-		const { __experimentalDiscussionSettings } = getSettings();
-		return __experimentalDiscussionSettings;
-	} );
+	const { minSize, maxSize } = getAvatarSizes( sizes );
 	return {
-		src: avatarUrls
-			? avatarUrls[ avatarUrls.length - 1 ]
-			: defaultAvatarUrl,
+		src: useAvatar( avatarUrls ),
 		minSize,
-		maxSize: maxSizeBuffer,
-
+		maxSize,
 		alt: authorDetails
 			? // translators: %s is the Author name.
 			  sprintf( __( '%s Avatar' ), authorDetails?.name )
