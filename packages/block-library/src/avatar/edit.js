1a3a0c250e6c0fdb2a835b678980d8b7dff82620
Carlos Bravo <hola@carlosbravo.me>
2022-02-08 16:11:30 +0100

    Refactor edit.js code

--- packages/block-library/src/avatar/edit.js
+++ packages/block-library/src/avatar/edit.js
@@ -7,13 +7,65 @@ import {
 	__experimentalGetSpacingClassesAndStyles as useSpacingProps,
 } from '@wordpress/block-editor';
 import { PanelBody, ResizableBox, RangeControl } from '@wordpress/components';
-import { useEntityProp } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
 import { __, isRTL } from '@wordpress/i18n';
+
 /**
  * Internal dependencies
  */
-import { getUserAvatar, getCommentAvatar } from './utils';
+import { useUserAvatar, useCommentAvatar } from './utils';
+
+const renderInspectorControls = ( { setAttributes, avatar, attributes } ) => (
+	<InspectorControls>
+		<PanelBody title={ __( 'Avatar Settings' ) }>
+			<RangeControl
+				label={ __( 'Image size' ) }
+				onChange={ ( newWidth ) =>
+					setAttributes( {
+						width: newWidth,
+						height: newWidth,
+					} )
+				}
+				min={ avatar.minSize }
+				max={ avatar.maxsize }
+				initialPosition={ attributes?.width }
+				value={ attributes?.width }
+			/>
+		</PanelBody>
+	</InspectorControls>
+);
+
+const renderAvatar = ( {
+	setAttributes,
+	attributes,
+	avatar,
+	blockProps,
+	isSelected,
+} ) => (
+	<ResizableBox
+		size={ {
+			width: attributes.width,
+			height: attributes.height,
+		} }
+		showHandle={ isSelected }
+		onResizeStop={ ( event, direction, elt, delta ) => {
+			setAttributes( {
+				height: parseInt( attributes.height + delta.height, 10 ),
+				width: parseInt( attributes.width + delta.width, 10 ),
+			} );
+		} }
+		lockAspectRatio
+		enable={ {
+			top: false,
+			right: ! isRTL(),
+			bottom: true,
+			left: isRTL(),
+		} }
+		minWidth={ avatar.minSize }
+		maxWidth={ avatar.maxSize }
+	>
+		<img src={ avatar.src } alt={ avatar.alt } { ...blockProps } />
+	</ResizableBox>
+);
 
 export default function Edit( {
 	attributes,
@@ -21,78 +73,30 @@ export default function Edit( {
 	setAttributes,
 	isSelected,
 } ) {
-	const { height, width } = attributes;
-
-	const isComment = !! commentId;
-
 	const blockProps = useBlockProps();
 	const spacingProps = useSpacingProps( attributes );
 
-	let avatar = getUserAvatar( {
-		postId,
-		postType,
-		functionUseSelect: useSelect,
-	} );
-
-	if ( isComment ) {
-		avatar = getCommentAvatar( {
-			functionUseEntityProp: useEntityProp,
-			functionUseSelect: useSelect,
-			commentId,
-		} );
-	}
-
-	const inspectorControls = (
-		<InspectorControls>
-			<PanelBody title={ __( 'Avatar Settings' ) }>
-				<RangeControl
-					label={ __( 'Image size' ) }
-					onChange={ ( newWidth ) =>
-						setAttributes( {
-							width: newWidth,
-							height: newWidth,
-						} )
-					}
-					min={ avatar.minSize }
-					max={ avatar.maxsize }
-					initialPosition={ width }
-					value={ width }
-				/>
-			</PanelBody>
-		</InspectorControls>
-	);
-
-	const resizableAvatar = (
-		<ResizableBox
-			size={ {
-				width,
-				height,
-			} }
-			showHandle={ isSelected }
-			onResizeStop={ ( event, direction, elt, delta ) => {
-				setAttributes( {
-					height: parseInt( height + delta.height, 10 ),
-					width: parseInt( width + delta.width, 10 ),
-				} );
-			} }
-			lockAspectRatio
-			enable={ {
-				top: false,
-				right: ! isRTL(),
-				bottom: true,
-				left: isRTL(),
-			} }
-			minWidth={ avatar.minSize }
-			maxWidth={ avatar.maxSize }
-		>
-			<img src={ avatar.src } alt={ avatar.alt } { ...blockProps } />
-		</ResizableBox>
-	);
+	// I would like to not call useCommentAvatar API requests if there is no commentId
+	// but is not recommended having conditional hook. Any ideas?
+	const commentAvatar = useCommentAvatar( { commentId } );
+	const userAvatar = useUserAvatar( { postId, postType } );
 
 	return (
 		<>
-			{ inspectorControls }
-			<div { ...spacingProps }>{ resizableAvatar }</div>
+			{ renderInspectorControls( {
+				setAttributes,
+				attributes,
+				avatar: commentId ? commentAvatar : userAvatar,
+				isSelected,
+			} ) }
+			<div { ...spacingProps }>
+				{ renderAvatar( {
+					setAttributes,
+					attributes,
+					avatar: commentId ? commentAvatar : userAvatar,
+					blockProps,
+				} ) }
+			</div>
 		</>
 	);
 }
