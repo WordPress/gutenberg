52b34d5b5536571ac3f02049a1103a592da40192
André <583546+oandregal@users.noreply.github.com>
2022-02-11 13:58:54 +0100

    Fix for WordPress 5.9

--- lib/compat/wordpress-5.9/script-loader.php
+++ lib/compat/wordpress-5.9/script-loader.php
@@ -9,37 +9,40 @@
  * This code lives in script-loader.php
  * where we just load styles using wp_get_global_stylesheet.
  */
-if ( ! function_exists( 'wp_get_global_styles' ) ) {
-	// Running on WordPress 5.8.
-	function gutenberg_enqueue_global_styles_assets() {
-		$separate_assets = wp_should_load_separate_core_block_assets();
-		$stylesheet      = wp_get_global_stylesheet();
-		if ( empty( $stylesheet ) ) {
-			return;
-		}
+function gutenberg_enqueue_global_styles_assets() {
+	$separate_assets  = wp_should_load_separate_core_block_assets();
+	$is_block_theme   = wp_is_block_theme();
+	$is_classic_theme = ! $is_block_theme;
 
-		if (
-			( doing_action( 'wp_footer' ) && ! $separate_assets ) ||
-			( doing_action( 'wp_enqueue_scripts' ) && $separate_assets )
-		) {
-			// Block themes are not supported in WordPress 5.8.
-			// Classic themes load the GS stylesheet in the head by default,
-			// and load in body if they opted-in into loading separate assets.
-			return;
-		}
-
-		if ( isset( wp_styles()->registered['global-styles'] ) ) {
-			// There's a GS stylesheet (theme has theme.json),
-			// so we overwrite it.
-			wp_styles()->registered['global-styles']->extra['after'][0] = $stylesheet;
-		} else {
-			// There's no GS stylesheet (theme has no theme.json),
-			// so we enqueue a new one.
-			wp_register_style( 'global-styles', false, array(), true, true );
-			wp_add_inline_style( 'global-styles', $stylesheet );
-			wp_enqueue_style( 'global-styles' );
-		}
+	/*
+	 * Global styles should be printed in the head when loading all styles combined.
+	 * The footer should only be used to print global styles for classic themes with separate core assets enabled.
+	 *
+	 * See https://core.trac.wordpress.org/ticket/53494.
+	 */
+	if (
+		( $is_block_theme && doing_action( 'wp_footer' ) ) ||
+		( $is_classic_theme && doing_action( 'wp_footer' ) && ! $separate_assets ) ||
+		( $is_classic_theme && doing_action( 'wp_enqueue_scripts' ) && $separate_assets )
+	) {
+		return;
 	}
-	add_action( 'wp_enqueue_scripts', 'gutenberg_enqueue_global_styles_assets' );
-	add_action( 'wp_footer', 'gutenberg_enqueue_global_styles_assets' );
+
+	$stylesheet = wp_get_global_stylesheet();
+
+	if ( empty( $stylesheet ) ) {
+		return;
+	}
+
+	if ( isset( wp_styles()->registered['global-styles'] ) ) {
+		// There's a GS stylesheet (theme has theme.json), so we overwrite it.
+		wp_styles()->registered['global-styles']->extra['after'][0] = $stylesheet;
+	} else {
+		// No GS stylesheet (theme has no theme.json), so we enqueue a new one.
+		wp_register_style( 'global-styles', false, array(), true, true );
+		wp_add_inline_style( 'global-styles', $stylesheet );
+		wp_enqueue_style( 'global-styles' );
+	}
 }
+add_action( 'wp_enqueue_scripts', 'gutenberg_enqueue_global_styles_assets' );
+add_action( 'wp_footer', 'gutenberg_enqueue_global_styles_assets' );
