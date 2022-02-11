bb7befb40be4b9645fbbf31c626af2f62ca3fd79
Marco Ciampini <marco.ciampo@gmail.com>
2022-02-11 18:59:24 +0100

    Fix styles

--- packages/components/src/number-control/styles/number-control-styles.ts
+++ packages/components/src/number-control/styles/number-control-styles.ts
@@ -1,17 +1,21 @@
-// @ts-nocheck
 /**
  * External dependencies
  */
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+
 /**
  * Internal dependencies
  */
 import InputControl from '../../input-control';
+import type { Props } from '../types';
 
-const htmlArrowStyles = ( { hideHTMLArrows } ) => {
+const htmlArrowStyles = ( {
+	hideHTMLArrows,
+}: Pick< Props, 'hideHTMLArrows' > ) => {
 	if ( ! hideHTMLArrows ) return ``;
 
+	// TODO: rewrite using Emotion selection from `InputControl` component
 	return css`
 		input[type='number']::-webkit-outer-spin-button,
 		input[type='number']::-webkit-inner-spin-button {
