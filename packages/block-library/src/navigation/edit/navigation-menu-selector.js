ea32685fbabf070e022112b0b94c9e4cfa8ff307
Dave Smith <getdavemail@gmail.com>
2022-02-14 16:45:21 +0000

    Improve consistency of Navigation block hook (#38705)
    * Rename to include "Menu" terminology
    
    Remove "entity" in favour of "menu"
    
    Attempt to fix failing e2e test
    
    * Rename all instances
    
    * Remove unneeded aliasing

--- packages/block-library/src/navigation/edit/navigation-menu-selector.js
+++ packages/block-library/src/navigation/edit/navigation-menu-selector.js
@@ -30,8 +30,8 @@ export default function NavigationMenuSelector( {
 
 	const {
 		navigationMenus,
-		canUserCreateNavigation: canUserCreateNavigationMenu,
-		canUserUpdateNavigationEntity: canUserUpdateNavigationMenu,
+		canUserCreateNavigationMenu,
+		canUserUpdateNavigationMenu,
 		canSwitchNavigationMenu,
 	} = useNavigationMenu();
 
