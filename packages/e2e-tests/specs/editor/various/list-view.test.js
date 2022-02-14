ec26c9fc9a930c2e9903878f3973f2310d0bc29b
Daniel Richards <daniel.richards@automattic.com>
2022-02-14 11:17:15 +0800

    Add e2e test

--- packages/e2e-tests/specs/editor/various/list-view.test.js
+++ packages/e2e-tests/specs/editor/various/list-view.test.js
@@ -6,6 +6,7 @@ import {
 	insertBlock,
 	getEditedPostContent,
 	pressKeyWithModifier,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
 
 async function dragAndDrop( draggableElement, targetElement, offsetY ) {
@@ -53,5 +54,47 @@ describe( 'List view', () => {
 		await dragAndDrop( paragraphBlock, headingBlock, -5 );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	// Check for regressions of https://github.com/WordPress/gutenberg/issues/38763.
+	it( 'shows the correct amount of blocks after a block is removed in the canvas', async () => {
+		// Insert some blocks of different types.
+		await insertBlock( 'Image' );
+		await insertBlock( 'Heading' );
+		await insertBlock( 'Paragraph' );
+
+		// Open list view.
+		await pressKeyWithModifier( 'access', 'o' );
+
+		// The last inserted paragraph block should be selected in List View.
+		await page.waitForXPath(
+			'//a[contains(., "Paragraph(selected block)")]'
+		);
+
+		// Go to the image block in list view.
+		await pressKeyTimes( 'ArrowUp', 2 );
+		const listViewImageBlock = await page.waitForXPath(
+			'//a[contains(., "Image")]'
+		);
+		expect( listViewImageBlock ).toHaveFocus();
+
+		// Select the image block in the canvas.
+		await page.keyboard.press( 'Enter' );
+
+		const canvasImageBlock = await page.waitForSelector(
+			'figure[aria-label="Block: Image"]'
+		);
+		expect( canvasImageBlock ).toHaveFocus();
+
+		// Delete the image block in the canvas.
+		await page.keyboard.press( 'Backspace' );
+
+		// List view should have two rows.
+		const listViewRows = await page.$$( 'tr.block-editor-list-view-leaf' );
+		expect( listViewRows ).toHaveLength( 2 );
+
+		// The console didn't throw an error as reported in
+		// https://github.com/WordPress/gutenberg/issues/38763.
+		expect( console ).not.toHaveErrored();
 	} );
 } );
