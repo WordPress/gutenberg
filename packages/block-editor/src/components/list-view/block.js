c85a1ccecdba6beb4b3da06103fae220b322a836
Daniel Richards <daniel.richards@automattic.com>
2022-02-14 10:33:34 +0800

    Fix list view error when deleting first item

--- packages/block-editor/src/components/list-view/block.js
+++ packages/block-editor/src/components/list-view/block.js
@@ -146,11 +146,13 @@ function ListViewBlock( {
 	} );
 
 	const blockInformation = useBlockDisplayInformation( clientId );
-	const settingsAriaLabel = sprintf(
-		// translators: %s: The title of the block.
-		__( 'Options for %s block' ),
-		blockInformation.title
-	);
+	const settingsAriaLabel = blockInformation
+		? sprintf(
+				// translators: %s: The title of the block.
+				__( 'Options for %s block' ),
+				blockInformation.title
+		  )
+		: __( 'Options' );
 
 	return (
 		<ListViewLeaf
