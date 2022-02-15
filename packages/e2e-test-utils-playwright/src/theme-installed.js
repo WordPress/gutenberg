2c5b83918fae8d7c469d2ac58afb4d4543b70be7
Justin Ahinon <justiny.ahinon@gmail.com>
2022-02-15 14:28:35 +0200

    Update theme-installed.js

--- packages/e2e-test-utils-playwright/src/theme-installed.js
+++ packages/e2e-test-utils-playwright/src/theme-installed.js
@@ -1,18 +1,20 @@
 /**
-  * Checks whether a theme exists on the site.
-  *
-  * @this {import('./').TestUtils}
-  * 
-  * @param {string} slug Theme slug to check.
-  * @return {boolean} Whether the theme exists.
-  */
-export async function isThemeInstalled(slug) {
+ * Checks whether a theme exists on the site.
+ *
+ * @this {import('./').TestUtils}
+ *
+ * @param {string} slug Theme slug to check.
+ * @return {boolean} Whether the theme exists.
+ */
+export async function isThemeInstalled( slug ) {
 	await this.switchUserToAdmin();
-	await this.visitAdminPage('themes.php');
+	await this.visitAdminPage( 'themes.php' );
 
-	const found = await this.page.locator(`[data-slug=${slug}]`).isVisible();
+	const found = await this.page
+		.locator( `[data-slug=${ slug }]` )
+		.isVisible();
 
 	await this.switchUserToTest();
 
-	return Boolean(found);
+	return Boolean( found );
 }
