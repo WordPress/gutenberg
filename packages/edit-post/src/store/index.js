8d5cf87298f953aa59c612b99795ffaa32c9bbce
Nik Tsekouras <ntsekouras@outlook.com>
2022-02-15 16:43:03 +0200

    [Edit Post]: Migrate store actions to thunks (#36551)
    * [Edit Post]: Migrate store actions to thunks
    
    * fix dispatch call
    
    * add first tests
    
    * add more tests
    
    * Fixup package-lock.json
    
    * Add api-fetch as dependency
    
    * part of review feedback
    
    * update test check
    
    * dispatch META_BOXES_INITIALIZED
    
    * remove `return`
    
    Co-authored-by: Jarda Snajdr <jsnajdr@gmail.com>

--- packages/edit-post/src/store/index.js
+++ packages/edit-post/src/store/index.js
@@ -2,7 +2,6 @@
  * WordPress dependencies
  */
 import { createReduxStore, registerStore } from '@wordpress/data';
-import { controls } from '@wordpress/data-controls';
 
 /**
  * Internal dependencies
@@ -16,7 +15,7 @@ const storeConfig = {
 	reducer,
 	actions,
 	selectors,
-	controls,
+	__experimentalUseThunks: true,
 	persist: [ 'preferences' ],
 };
 
