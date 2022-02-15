17cd97056ee6e32d92b05a7720e10ba466118b29
Lena Morita <lena@jaguchi.com>
2022-02-16 06:17:11 +0900

    Heading: Tweak story for Controls

--- packages/components/src/heading/stories/index.js
+++ packages/components/src/heading/stories/index.js
@@ -7,19 +7,15 @@ import { Heading as UnconnectedHeading } from '../component';
 export default {
 	component: UnconnectedHeading,
 	title: 'Components (Experimental)/Heading',
+	argTypes: {
+		color: { control: { type: 'color' } },
+	},
+	parameters: {
+		controls: { expanded: true },
+	},
 };
 
-export const _default = () => {
-	return (
-		<>
-			<Heading level={ 1 } uppercase>
-				Heading
-			</Heading>
-			<Heading level={ 2 }>Heading</Heading>
-			<Heading level={ 3 }>Heading</Heading>
-			<Heading level={ 4 }>Heading</Heading>
-			<Heading level={ 5 }>Heading</Heading>
-			<Heading level={ 6 }>Heading</Heading>
-		</>
-	);
+export const Default = ( props ) => <Heading { ...props } />;
+Default.args = {
+	children: 'Heading',
 };
