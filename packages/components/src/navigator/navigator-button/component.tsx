47f4b1fd6321c76a19096e917242a9e8eb523d9a
Marco Ciampini <marco.ciampo@gmail.com>
2022-02-16 15:25:22 +0100

    Complete the refactor to `ForwardedRef`, remove unnecessary eslint disable comment

--- packages/components/src/navigator/navigator-button/component.tsx
+++ packages/components/src/navigator/navigator-button/component.tsx
@@ -1,8 +1,7 @@
 /**
  * External dependencies
  */
-// eslint-disable-next-line no-restricted-imports
-import type { Ref } from 'react';
+import type { ForwardedRef } from 'react';
 
 /**
  * Internal dependencies
@@ -14,7 +13,7 @@ import type { NavigatorButtonProps } from '../types';
 
 function NavigatorButton(
 	props: WordPressComponentProps< NavigatorButtonProps, 'button' >,
-	forwardedRef: Ref< any >
+	forwardedRef: ForwardedRef< any >
 ) {
 	const navigatorButtonProps = useNavigatorButton( props );
 
