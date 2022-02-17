fa331bac74931a1eca31d70df816895bc3d55280
André <583546+oandregal@users.noreply.github.com>
2022-02-17 09:38:28 +0100

    Port PHPDoc updates

--- lib/compat/wordpress-5.9/script-loader.php
+++ lib/compat/wordpress-5.9/script-loader.php
@@ -51,12 +51,14 @@ add_action( 'wp_footer', 'gutenberg_enqueue_global_styles_assets' );
  * This function takes care of adding inline styles
  * in the proper place, depending on the theme in use.
  *
+ * @since 5.9.1
+ *
  * For block themes, it's loaded in the head.
  * For classic ones, it's loaded in the body
  * because the wp_head action (and wp_enqueue_scripts)
  * happens before the render_block.
  *
- * See https://core.trac.wordpress.org/ticket/53494.
+ * @link https://core.trac.wordpress.org/ticket/53494.
  *
  * @param string $style String containing the CSS styles to be added.
  */
