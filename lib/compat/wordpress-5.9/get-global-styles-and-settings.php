d4731b10ecec262c5ca11fa674c2bb7c1ea8ede0
André <583546+oandregal@users.noreply.github.com>
2022-02-17 11:35:22 +0100

    New settings (works in WordPress 5.9)
    Rename wp_get_global_settings to gutenberg_get_global_settings.
    
    In WordPress 5.9 the wp_* function is already defined,
    so we can't override them. It's calling the existing
    WP_Theme_JSON classes in WordPress core so it won't pick up
    the plugin modification.
    
    In the plugin, to make sure these changes work fine in 5.9
    as well, we need to use the gutenberg_* function instead.

--- lib/compat/wordpress-5.9/get-global-styles-and-settings.php
+++ lib/compat/wordpress-5.9/get-global-styles-and-settings.php
@@ -5,38 +5,33 @@
  * @package gutenberg
  */
 
-if ( ! function_exists( 'wp_get_global_settings' ) ) {
-	/**
-	 * Function to get the settings resulting of merging core, theme, and user data.
-	 *
-	 * @param array $path    Path to the specific setting to retrieve. Optional.
-	 *                       If empty, will return all settings.
-	 * @param array $context {
-	 *     Metadata to know where to retrieve the $path from. Optional.
-	 *
-	 *     @type string $block_name Which block to retrieve the settings from.
-	 *                              If empty, it'll return the settings for the global context.
-	 *     @type string $origin     Which origin to take data from.
-	 *                              Valid values are 'all' (core, theme, and user) or 'base' (core and theme).
-	 *                              If empty or unknown, 'all' is used.
-	 * }
-	 *
-	 * @return array The settings to retrieve.
-	 */
-	function wp_get_global_settings( $path = array(), $context = array() ) {
-		if ( ! empty( $context['block_name'] ) ) {
-			$path = array_merge( array( 'blocks', $context['block_name'] ), $path );
-		}
-
-		$origin = 'custom';
-		if ( isset( $context['origin'] ) && 'base' === $context['origin'] ) {
-			$origin = 'theme';
-		}
-
-		$settings = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data( $origin )->get_settings();
-
-		return _wp_array_get( $settings, $path, $settings );
+/**
+ * Function to get the settings resulting of merging core, theme, and user data.
+ *
+ * @param array $path    Path to the specific setting to retrieve. Optional.
+ *                       If empty, will return all settings.
+ * @param array $context {
+ *     Metadata to know where to retrieve the $path from. Optional.
+ *
+ *     @type string $block_name Which block to retrieve the settings from.
+ *                              If empty, it'll return the settings for the global context.
+ *     @type string $origin     Which origin to take data from.
+ *                              Valid values are 'all' (core, theme, and user) or 'base' (core and theme).
+ *                              If empty or unknown, 'all' is used.
+ * }
+ *
+ * @return array The settings to retrieve.
+ */
+function gutenberg_get_global_settings( $path = array(), $context = array() ) {
+	if ( ! empty( $context['block_name'] ) ) {
+		$path = array_merge( array( 'blocks', $context['block_name'] ), $path );
 	}
+	$origin = 'custom';
+	if ( isset( $context['origin'] ) && 'base' === $context['origin'] ) {
+		$origin = 'theme';
+	}
+	$settings = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data( $origin )->get_settings();
+	return _wp_array_get( $settings, $path, $settings );
 }
 
 if ( ! function_exists( 'wp_get_global_styles' ) ) {
