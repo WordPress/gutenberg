ec99125c00908bd40f44efa60f81dfb0e0af06d0
Adam Zieliński <adam@adamziel.com>
2022-02-17 14:16:29 +0100

    Export updatable types

--- packages/core-data/src/types/index.ts
+++ packages/core-data/src/types/index.ts
@@ -22,6 +22,28 @@ import { WpTemplate } from './wp_template';
 import { WpTemplatePart } from './wp_template_part';
 import { Context, RenderedText } from './common';
 
+export {
+	Attachment,
+	Comment,
+	MenuLocation,
+	NavMenu,
+	NavMenuItem,
+	NavigationArea,
+	Page,
+	Plugin,
+	Post,
+	Settings,
+	Sidebar,
+	Taxonomy,
+	Theme,
+	User,
+	Type,
+	Widget,
+	WidgetType,
+	WpTemplate,
+	WpTemplatePart,
+};
+
 export type EntityRecord< C extends Context > =
 	| Attachment< C >
 	| Comment< C >
@@ -43,6 +65,31 @@ export type EntityRecord< C extends Context > =
 	| WpTemplate< C >
 	| WpTemplatePart< C >;
 
-export type UpdatableRecord< T extends EntityRecord< any > > = {
+export type UpdatableEntityRecord = UpdatableRecord< EntityRecord< 'edit' > >;
+export type UpdatableRecord< T extends EntityRecord< 'edit' > > = {
 	[ K in keyof T ]: T[ K ] extends RenderedText< any > ? string : T[ K ];
 };
+
+export type UpdatableAttachment = UpdatableRecord< Attachment< 'edit' > >;
+export type UpdatableComment = UpdatableRecord< Comment< 'edit' > >;
+export type UpdatableMenuLocation = UpdatableRecord< MenuLocation< 'edit' > >;
+export type UpdatableNavMenu = UpdatableRecord< NavMenu< 'edit' > >;
+export type UpdatableNavMenuItem = UpdatableRecord< NavMenuItem< 'edit' > >;
+export type UpdatableNavigationArea = UpdatableRecord<
+	NavigationArea< 'edit' >
+>;
+export type UpdatablePage = UpdatableRecord< Page< 'edit' > >;
+export type UpdatablePlugin = UpdatableRecord< Plugin< 'edit' > >;
+export type UpdatablePost = UpdatableRecord< Post< 'edit' > >;
+export type UpdatableSettings = UpdatableRecord< Settings< 'edit' > >;
+export type UpdatableSidebar = UpdatableRecord< Sidebar< 'edit' > >;
+export type UpdatableTaxonomy = UpdatableRecord< Taxonomy< 'edit' > >;
+export type UpdatableTheme = UpdatableRecord< Theme< 'edit' > >;
+export type UpdatableUser = UpdatableRecord< User< 'edit' > >;
+export type UpdatableType = UpdatableRecord< Type< 'edit' > >;
+export type UpdatableWidget = UpdatableRecord< Widget< 'edit' > >;
+export type UpdatableWidgetType = UpdatableRecord< WidgetType< 'edit' > >;
+export type UpdatableWpTemplate = UpdatableRecord< WpTemplate< 'edit' > >;
+export type UpdatableWpTemplatePart = UpdatableRecord<
+	WpTemplatePart< 'edit' >
+>;
