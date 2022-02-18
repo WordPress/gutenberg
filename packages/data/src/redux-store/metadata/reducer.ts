1770a1e475b6615fb7b555aa6d089c11b60ed899
Jarda Snajdr <jsnajdr@gmail.com>
2022-02-18 15:12:55 +0100

    Data: normalize selector args when handling START,FINISH_RESOLUTION actions

--- packages/data/src/redux-store/metadata/reducer.ts
+++ packages/data/src/redux-store/metadata/reducer.ts
@@ -8,7 +8,7 @@ import type { Reducer } from 'redux';
 /**
  * Internal dependencies
  */
-import { onSubKey } from './utils';
+import { normalizeArgs, onSubKey } from './utils';
 
 type Action =
 	| ReturnType< typeof import('./actions').startResolution >
@@ -38,7 +38,7 @@ const subKeysIsResolved: Reducer< Record< string, State >, Action > = onSubKey<
 		case 'FINISH_RESOLUTION': {
 			const isStarting = action.type === 'START_RESOLUTION';
 			const nextState = new EquivalentKeyMap( state );
-			nextState.set( action.args, isStarting );
+			nextState.set( normalizeArgs( action.args ), isStarting );
 			return nextState;
 		}
 		case 'START_RESOLUTIONS':
@@ -46,13 +46,13 @@ const subKeysIsResolved: Reducer< Record< string, State >, Action > = onSubKey<
 			const isStarting = action.type === 'START_RESOLUTIONS';
 			const nextState = new EquivalentKeyMap( state );
 			for ( const resolutionArgs of action.args ) {
-				nextState.set( resolutionArgs, isStarting );
+				nextState.set( normalizeArgs( resolutionArgs ), isStarting );
 			}
 			return nextState;
 		}
 		case 'INVALIDATE_RESOLUTION': {
 			const nextState = new EquivalentKeyMap( state );
-			nextState.delete( action.args );
+			nextState.delete( normalizeArgs( action.args ) );
 			return nextState;
 		}
 	}
