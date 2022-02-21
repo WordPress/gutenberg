3fd1fc6c3bce0a9bab8dc3e6c605ee95d5f62ec7
Maggie <maggie.cabrera@automattic.com>
2022-02-21 11:43:20 +0100

    lowered specificity of alignment rules

--- lib/block-supports/layout.php
+++ lib/block-supports/layout.php
@@ -53,7 +53,7 @@ function gutenberg_get_layout_style( $selector, $layout, $has_block_gap_support 
 
 		$style = '';
 		if ( $content_size || $wide_size ) {
-			$style  = "$selector > :not(.alignleft):not(.alignright) {";
+			$style  = "$selector > :where(:not(.alignleft):not(.alignright)) {";
 			$style .= 'max-width: ' . esc_html( $all_max_width_value ) . ';';
 			$style .= 'margin-left: auto !important;';
 			$style .= 'margin-right: auto !important;';
