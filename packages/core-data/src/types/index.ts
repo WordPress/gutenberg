b23a08a40da9a801dba84d3296fbac22dae1a929
Adam Zieliński <adam@adamziel.com>
2022-02-22 11:47:07 +0100

    Use export type and import type declarations

--- packages/core-data/src/types/index.ts
+++ packages/core-data/src/types/index.ts
@@ -1,28 +1,34 @@
 /**
  * Internal dependencies
  */
-import { ExtensibleAttachment, Attachment } from './attachment';
-import { ExtensibleComment, Comment } from './comment';
-import { ExtensibleMenuLocation, MenuLocation } from './menu-location';
-import { ExtensibleNavMenu, NavMenu } from './nav-menu';
-import { ExtensibleNavMenuItem, NavMenuItem } from './nav-menu-item';
-import { ExtensibleNavigationArea, NavigationArea } from './navigation-area';
-import { ExtensiblePage, Page } from './page';
-import { ExtensiblePlugin, Plugin } from './plugin';
-import { ExtensiblePost, Post } from './post';
-import { ExtensibleSettings, Settings } from './settings';
-import { ExtensibleSidebar, Sidebar } from './sidebar';
-import { ExtensibleTaxonomy, Taxonomy } from './taxonomy';
-import { ExtensibleTheme, Theme } from './theme';
-import { ExtensibleUser, User } from './user';
-import { ExtensibleType, Type } from './type';
-import { ExtensibleWidget, Widget } from './widget';
-import { ExtensibleWidgetType, WidgetType } from './widget-type';
-import { ExtensibleWpTemplate, WpTemplate } from './wp-template';
-import { ExtensibleWpTemplatePart, WpTemplatePart } from './wp-template-part';
-import { Context, Updatable } from './helpers';
+import type { ExtensibleAttachment, Attachment } from './attachment';
+import type { ExtensibleComment, Comment } from './comment';
+import type { ExtensibleMenuLocation, MenuLocation } from './menu-location';
+import type { ExtensibleNavMenu, NavMenu } from './nav-menu';
+import type { ExtensibleNavMenuItem, NavMenuItem } from './nav-menu-item';
+import type {
+	ExtensibleNavigationArea,
+	NavigationArea,
+} from './navigation-area';
+import type { ExtensiblePage, Page } from './page';
+import type { ExtensiblePlugin, Plugin } from './plugin';
+import type { ExtensiblePost, Post } from './post';
+import type { ExtensibleSettings, Settings } from './settings';
+import type { ExtensibleSidebar, Sidebar } from './sidebar';
+import type { ExtensibleTaxonomy, Taxonomy } from './taxonomy';
+import type { ExtensibleTheme, Theme } from './theme';
+import type { ExtensibleUser, User } from './user';
+import type { ExtensibleType, Type } from './type';
+import type { ExtensibleWidget, Widget } from './widget';
+import type { ExtensibleWidgetType, WidgetType } from './widget-type';
+import type { ExtensibleWpTemplate, WpTemplate } from './wp-template';
+import type {
+	ExtensibleWpTemplatePart,
+	WpTemplatePart,
+} from './wp-template-part';
+import type { Context, Updatable } from './helpers';
 
-export {
+export type {
 	Updatable,
 	Attachment,
 	Comment,
@@ -68,22 +74,22 @@ export type EntityRecord< C extends Context > =
 
 export type UpdatableEntityRecord = Updatable< EntityRecord< 'edit' > >;
 
-export { ExtensibleAttachment } from './attachment';
-export { ExtensibleComment } from './comment';
-export { ExtensibleMenuLocation } from './menu-location';
-export { ExtensibleNavMenu } from './nav-menu';
-export { ExtensibleNavMenuItem } from './nav-menu-item';
-export { ExtensibleNavigationArea } from './navigation-area';
-export { ExtensiblePage } from './page';
-export { ExtensiblePlugin } from './plugin';
-export { ExtensiblePost } from './post';
-export { ExtensibleSettings } from './settings';
-export { ExtensibleSidebar } from './sidebar';
-export { ExtensibleTaxonomy } from './taxonomy';
-export { ExtensibleTheme } from './theme';
-export { ExtensibleUser } from './user';
-export { ExtensibleType } from './type';
-export { ExtensibleWidget } from './widget';
-export { ExtensibleWidgetType } from './widget-type';
-export { ExtensibleWpTemplate } from './wp-template';
-export { ExtensibleWpTemplatePart } from './wp-template-part';
+export type { ExtensibleAttachment } from './attachment';
+export type { ExtensibleComment } from './comment';
+export type { ExtensibleMenuLocation } from './menu-location';
+export type { ExtensibleNavMenu } from './nav-menu';
+export type { ExtensibleNavMenuItem } from './nav-menu-item';
+export type { ExtensibleNavigationArea } from './navigation-area';
+export type { ExtensiblePage } from './page';
+export type { ExtensiblePlugin } from './plugin';
+export type { ExtensiblePost } from './post';
+export type { ExtensibleSettings } from './settings';
+export type { ExtensibleSidebar } from './sidebar';
+export type { ExtensibleTaxonomy } from './taxonomy';
+export type { ExtensibleTheme } from './theme';
+export type { ExtensibleUser } from './user';
+export type { ExtensibleType } from './type';
+export type { ExtensibleWidget } from './widget';
+export type { ExtensibleWidgetType } from './widget-type';
+export type { ExtensibleWpTemplate } from './wp-template';
+export type { ExtensibleWpTemplatePart } from './wp-template-part';
