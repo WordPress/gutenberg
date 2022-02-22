ba3ca923c63670d0111743e867d4fc44432607b4
Lena Morita <lena@jaguchi.com>
2022-02-22 12:45:04 +0900

    Add more control type hints

--- packages/components/src/heading/stories/index.tsx
+++ packages/components/src/heading/stories/index.tsx
@@ -12,7 +12,15 @@ const meta: ComponentMeta< typeof Heading > = {
 	component: Heading,
 	title: 'Components (Experimental)/Heading',
 	argTypes: {
+		adjustLineHeightForInnerControls: { control: { type: 'text' } },
+		as: { control: { type: 'text' } },
 		color: { control: { type: 'color' } },
+		display: { control: { type: 'text' } },
+		letterSpacing: { control: { type: 'text' } },
+		lineHeight: { control: { type: 'text' } },
+		optimizeReadabilityFor: { control: { type: 'color' } },
+		variant: { control: { type: 'radio' }, options: [ 'muted' ] },
+		weight: { control: { type: 'text' } },
 	},
 	parameters: {
 		controls: { expanded: true },
