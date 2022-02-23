91473b89560dcc340326c49ca9e0b09c214f9854
Carlos Garcia <fluiddot@gmail.com>
2022-02-23 17:15:02 +0100

    Add android-npm project

---
+++ packages/react-native-bridge/android/android-npm/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.github.node-gradle.node") version "3.2.1"
+}
+
+val gutenbergPackageJson = File("$projectDir/../../../..")
+
+node {
+    download.set(true)
+    version.set("14.18.3")
+    nodeProjectDir.set(gutenbergPackageJson)
+
+}
+
