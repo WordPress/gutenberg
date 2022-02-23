b3846a7b89b6edbb5ac9fd7fab565637a4342a4c
Kerry Liu <kerryliu@gmail.com>
2022-02-23 10:51:26 -0800

    Editor: remove the unexported getWPAdminURL and update internal references to use addQueryArgs

--- packages/editor/src/utils/url.js
+++ packages/editor/src/utils/url.js
@@ -1,22 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { addQueryArgs, cleanForSlug as urlCleanForSlug } from '@wordpress/url';
+import { cleanForSlug as urlCleanForSlug } from '@wordpress/url';
 import deprecated from '@wordpress/deprecated';
-
-/**
- * Returns the URL of a WPAdmin Page.
- *
- * TODO: This should be moved to a module less specific to the editor.
- *
- * @param {string} page  Page to navigate to.
- * @param {Object} query Query Args.
- *
- * @return {string} WPAdmin URL.
- */
-export function getWPAdminURL( page, query ) {
-	return addQueryArgs( page, query );
-}
 
 /**
  * Performs some basic cleanup of a string for use as a post slug
