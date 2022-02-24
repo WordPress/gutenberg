77b04c7d5ffe8bb4a98ab9590c0da4a7fabe69cc
Alex Lende <alex@lende.xyz>
2022-02-24 02:20:45 -0600

    Revert extra check in theme.json resolver

--- lib/compat/wordpress-5.9/class-wp-theme-json-resolver-5-9.php
+++ lib/compat/wordpress-5.9/class-wp-theme-json-resolver-5-9.php
@@ -191,12 +191,8 @@ class WP_Theme_JSON_Resolver_5_9 {
 			}
 			$theme_support_data['settings']['color']['defaultGradients'] = $default_gradients;
 
-			$default_duotone = false;
-			if ( ! isset( $theme_support_data['settings']['color']['duotone'] ) ) {
-				// If the theme does not have any duotone filters, we still want to show the core ones.
-				$default_duotone = true;
-			}
-			$theme_support_data['settings']['color']['defaultDuotone'] = $default_duotone;
+			// Classic themes without a theme.json don't support global duotone.
+			$theme_support_data['settings']['color']['defaultDuotone'] = false;
 		}
 		$with_theme_supports = new WP_Theme_JSON_Gutenberg( $theme_support_data );
 		$with_theme_supports->merge( static::$theme );
