5ed84cbd27fad2fed004f591e12fa5227b994a53
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-02-28 12:16:22 +1100

    Revert unneeded change to BoxControl

--- packages/components/src/box-control/unit-control.js
+++ packages/components/src/box-control/unit-control.js
@@ -38,7 +38,7 @@ export default function BoxUnitControl( {
 					isFirst={ isFirst }
 					isLast={ isLast }
 					isOnly={ isOnly }
-					isPressEnterToChange={ true }
+					isPressEnterToChange
 					isResetValueOnUnitChange={ false }
 					value={ value }
 					{ ...props }
