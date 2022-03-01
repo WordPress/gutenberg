f75562ec1a744bec10038bd8814e900abe04da8e
Lena Morita <lena@jaguchi.com>
2022-03-02 00:09:16 +0900

    Clarify `as` prop comment

--- packages/components/src/ui/context/wordpress-component.ts
+++ packages/components/src/ui/context/wordpress-component.ts
@@ -18,7 +18,7 @@ export type WordPressComponentProps<
 	Omit< React.ComponentPropsWithRef< T >, 'as' | keyof P | 'children' > &
 	( IsPolymorphic extends true
 		? {
-				/** The HTML element to render the component as. */
+				/** The HTML element or React component to render the component as. */
 				as?: T | keyof JSX.IntrinsicElements;
 		  }
 		: {} );
