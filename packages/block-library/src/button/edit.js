cfd9d66a3b0bf96a2b7a79ad0ba9b931f1d65284
Delowar Hossain <delowardev@gmail.com>
2022-03-01 17:50:46 +0600

    Button link popover slot update

--- packages/block-library/src/button/edit.js
+++ packages/block-library/src/button/edit.js
@@ -224,7 +224,7 @@ function ButtonEdit( props ) {
 					} }
 					anchorRef={ ref?.current }
 					focusOnMount={ isEditingURL ? 'firstElement' : false }
-					__unstableSlotName={ 'block-toolbar' }
+					__unstableSlotName={ '__unstable-block-tools-after' }
 				>
 					<LinkControl
 						className="wp-block-navigation-link__inline-link-input"
