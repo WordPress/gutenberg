10f6af03a4f6d5f6c85a045a934db236c6005bb2
Adam Zieliński <adam@adamziel.com>
2022-03-02 15:04:34 +0100

    Update the documentation and condition formulation in the resolve-cache-middleware

--- packages/data/src/resolvers-cache-middleware.js
+++ packages/data/src/resolvers-cache-middleware.js
@@ -38,11 +38,11 @@ const createResolversCacheMiddleware = ( registry, reducerKey ) => () => (
 			}
 			resolversByArgs.forEach( ( value, args ) => {
 				// resolversByArgs is the map Map([ args ] => boolean) storing the cache resolution status for a given selector.
-				// If the value is false it means this resolver has finished its resolution which means we need to invalidate it,
-				// if it's true it means it's inflight and the invalidation is not necessary.
+				// If the value is "finished" or "error" it means this resolver has finished its resolution which means we need
+				// to invalidate it, if it's true it means it's inflight and the invalidation is not necessary.
 				if (
-					value?.status === 'resolving' ||
-					! value?.status ||
+					( value?.status !== 'finished' &&
+						value?.status !== 'error' ) ||
 					! resolver.shouldInvalidate( action, ...args )
 				) {
 					return;
