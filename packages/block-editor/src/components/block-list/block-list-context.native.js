daf5b141bd5df3a7422876be708769fd801494a6
Gerardo <gerardo.pacheco@automattic.com>
2022-03-03 15:08:42 +0100

    Mobile - BlockListContext - Add param's docs

--- packages/block-editor/src/components/block-list/block-list-context.native.js
+++ packages/block-editor/src/components/block-list/block-list-context.native.js
@@ -12,6 +12,14 @@ export const DEFAULT_BLOCK_LIST_CONTEXT = {
 const Context = createContext( DEFAULT_BLOCK_LIST_CONTEXT );
 const { Provider, Consumer } = Context;
 
+/**
+ * Finds a block's layout data within the provided data object.
+ *
+ * @param {Object} data     Blocks layouts list.
+ * @param {string} clientId Block's clientId.
+ *
+ * @return {Object} Found block layout data.
+ */
 function findBlockLayoutByClientId( data, clientId ) {
 	return Object.entries( data ).reduce( ( acc, entry ) => {
 		const item = entry[ 1 ];
@@ -28,6 +36,14 @@ function findBlockLayoutByClientId( data, clientId ) {
 	}, null );
 }
 
+/**
+ * Deletes a block's layout data from the provided data object.
+ *
+ * @param {Object} data     Blocks layouts list.
+ * @param {string} clientId Block's clientsId.
+ *
+ * @return {Object} Updated data object.
+ */
 function deleteBlockLayoutByClientId( data, clientId ) {
 	return Object.keys( data ).reduce( ( acc, key ) => {
 		if ( key !== clientId ) {
@@ -47,6 +63,20 @@ function deleteBlockLayoutByClientId( data, clientId ) {
 		return acc;
 	}, {} );
 }
+
+/**
+ * Updates the blocksLayouts object with the block's layout data.
+ *
+ * @param {Object}   blocksLayouts          Blocks layouts list.
+ * @param {Object}   blockData              Block's layout data.
+ * @param {string}   blockData.clientId     Block's clientId.
+ * @param {?string}  blockData.rootClientId Optional. Block's rootClientId.
+ * @param {?boolean} blockData.shouldRemove Optional. Flag to remove it from the blocksLayout list.
+ * @param {number}   blockData.width        Block's width.
+ * @param {number}   blockData.heigth       Block's heigth.
+ * @param {number}   blockData.x            Block's x coordinate (relative to the parent).
+ * @param {number}   blockData.y            Block's y coordinate (relative to the parent).
+ */
 
 function updateBlocksLayouts( blocksLayouts, blockData ) {
 	const { clientId, rootClientId, shouldRemove, ...layoutProps } = blockData;
@@ -89,6 +119,11 @@ function updateBlocksLayouts( blocksLayouts, blockData ) {
 
 export { Provider as BlockListProvider, Consumer as BlockListConsumer };
 
+/**
+ * Hook that returns the block list context.
+ *
+ * @return {Object} Block list context
+ */
 export const useBlockListContext = () => {
 	return useContext( Context );
 };
