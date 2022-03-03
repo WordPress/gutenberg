5a766090191d69e26d569acd5ddabccd0c3f1d5d
Kai Hao <kai@kaihao.dev>
2022-03-03 13:09:09 +0800

    Change back to src/

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
 		'/.git/',
 		'/node_modules/',
 		'/packages/e2e-tests',
-		'/packages/e2e-test-utils-playwright/source/',
+		'/packages/e2e-test-utils-playwright/src/test.ts',
 		'<rootDir>/.*/build/',
 		'<rootDir>/.*/build-module/',
 		'<rootDir>/.+.native.js$',
