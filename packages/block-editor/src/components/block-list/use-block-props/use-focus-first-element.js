b66fbd2c9f9fad9b8dad5310a04b4da08797da71
Alex Stine <alex.stine@yourtechadvisors.com>
2022-03-05 01:29:47 -0500

    Try again to fix my own test.

--- packages/block-editor/src/components/block-list/use-block-props/use-focus-first-element.js
+++ packages/block-editor/src/components/block-list/use-block-props/use-focus-first-element.js
@@ -109,16 +109,15 @@ export function useFocusFirstElement( clientId ) {
 		// Check to see if element is focussable before a generic caret insert.
 		if ( ! target.getAttribute( 'contenteditable' ) ) {
 			const focusElement = focus.tabbable.findNext( target );
+			// Make sure focusElement is valid, form field, and within the current target element.
 			// Ensure is not block inserter trigger, don't want to focus that in the event of the group block which doesn't contain any other focussable elements.
-			const skipBlockInserterTrigger = target.classList.contains(
-				'block-editor-button-block-appender'
-			);
-			// Make sure focusElement is valid, form field, and in current ref.
 			if (
 				focusElement &&
 				isFormElement( focusElement ) &&
 				target.contains( focusElement ) &&
-				! skipBlockInserterTrigger
+				! focusElement.classList.contains(
+					'block-editor-button-block-appender'
+				)
 			) {
 				focusElement.focus();
 				return;
