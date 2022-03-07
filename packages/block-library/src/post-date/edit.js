3cadda210203bda8da562cef27b84e5d499c3bb4
Robert Anderson <robert@noisysocks.com>
2022-03-07 15:51:30 +1100

    Add support for custom formats

--- packages/block-library/src/post-date/edit.js
+++ packages/block-library/src/post-date/edit.js
@@ -8,7 +8,7 @@ import { uniq } from 'lodash';
  * WordPress dependencies
  */
 import { useEntityProp } from '@wordpress/core-data';
-import { useRef } from '@wordpress/element';
+import { useRef, useState, createInterpolateElement } from '@wordpress/element';
 import { dateI18n } from '@wordpress/date';
 import {
 	AlignmentControl,
@@ -23,7 +23,9 @@ import {
 	ToggleControl,
 	DateTimePicker,
 	PanelBody,
-	CustomSelectControl,
+	SelectControl,
+	TextControl,
+	ExternalLink,
 } from '@wordpress/components';
 import { __, _x, sprintf } from '@wordpress/i18n';
 import { edit } from '@wordpress/icons';
@@ -49,34 +51,6 @@ export default function PostDateEdit( {
 	// feel when working with regular expressions.
 	// https://www.php.net/manual/en/datetime.format.php
 	const is12Hour = /(?:^|[^\\])[aAgh]/.test( timeFormat );
-	const formatOptions = [
-		{
-			key: null,
-			name: sprintf(
-				// translators: %s: Example of how the date will look if selected.
-				_x( 'Site default (%s)', 'date format option' ),
-				dateI18n( siteFormat, date )
-			),
-		},
-		// Suggest a short format, medium format, long format, and a
-		// standardised (YYYY-MM-DD) format. The short, medium, and long formats
-		// are localised as different languages have different ways of writing
-		// these. For example, 'F j, Y' (April 20, 2022) in American English
-		// (en_US) is 'j. F Y' (20. April 2022) in German (de). The resultant
-		// array is de-duplicated as some languages will use the same format
-		// string for short, medium, and long formats.
-		...uniq( [
-			_x( 'n/j/Y', 'short date format' ),
-			_x( 'F j, Y', 'medium date format' ),
-			_x( 'l, F j, Y', 'long date format' ),
-			_x( 'n/j/Y h:i A', 'short date format with time' ),
-			_x( 'F j, Y h:i A', 'medium date format with time' ),
-			'Y-m-d',
-		] ).map( ( suggestedFormat ) => ( {
-			key: suggestedFormat,
-			name: dateI18n( suggestedFormat, date ),
-		} ) ),
-	];
 	const blockProps = useBlockProps( {
 		className: classnames( {
 			[ `has-text-align-${ textAlign }` ]: textAlign,
@@ -87,7 +61,7 @@ export default function PostDateEdit( {
 
 	let postDate = date ? (
 		<time dateTime={ dateI18n( 'c', date ) } ref={ timeRef }>
-			{ dateI18n( format ?? siteFormat, date ) }
+			{ dateI18n( format || siteFormat, date ) }
 		</time>
 	) : (
 		__( 'Post Date' )
@@ -146,21 +120,14 @@ export default function PostDateEdit( {
 			</BlockControls>
 
 			<InspectorControls>
-				<PanelBody title={ __( 'Format settings' ) }>
-					<CustomSelectControl
-						hideLabelFromVision
-						label={ __( 'Date Format' ) }
-						options={ formatOptions }
-						onChange={ ( { selectedItem } ) =>
-							setAttributes( {
-								format: selectedItem.key,
-							} )
-						}
-						value={ formatOptions.find(
-							( option ) => option.key === format
-						) }
-					/>
-				</PanelBody>
+				<FormatSettings
+					date={ date }
+					format={ format }
+					siteFormat={ siteFormat }
+					setFormat={ ( nextFormat ) =>
+						setAttributes( { format: nextFormat } )
+					}
+				/>
 				<PanelBody title={ __( 'Link settings' ) }>
 					<ToggleControl
 						label={ sprintf(
@@ -175,5 +142,102 @@ export default function PostDateEdit( {
 			</InspectorControls>
 			<div { ...blockProps }>{ postDate }</div>
 		</>
+	);
+}
+
+function FormatSettings( { date, format, siteFormat, setFormat } ) {
+	// Suggest a short format, medium format, long format, and a standardised
+	// (YYYY-MM-DD) format. The short, medium, and long formats are localised as
+	// different languages have different ways of writing these. For example, 'F
+	// j, Y' (April 20, 2022) in American English (en_US) is 'j. F Y' (20. April
+	// 2022) in German (de). The resultant array is de-duplicated as some
+	// languages will use the same format string for short, medium, and long
+	// formats.
+	const suggestedFormats = uniq( [
+		_x( 'n/j/Y', 'short date format' ),
+		_x( 'F j, Y', 'medium date format' ),
+		_x( 'l, F j, Y', 'long date format' ),
+		_x( 'n/j/Y h:i A', 'short date format with time' ),
+		_x( 'F j, Y h:i A', 'medium date format with time' ),
+		'Y-m-d',
+	] );
+
+	const defaultOption = {
+		label: sprintf(
+			// translators: %s: Example of how the date will look if selected.
+			_x( 'Site default (%s)', 'date format option' ),
+			dateI18n( siteFormat, date )
+		),
+	};
+	const suggestedOptions = suggestedFormats.map( ( suggestedFormat ) => ( {
+		format: suggestedFormat,
+		label: dateI18n( suggestedFormat, date ),
+	} ) );
+	const customOption = {
+		label: _x( 'Custom', 'date format option' ),
+	};
+	const options = [ defaultOption, ...suggestedOptions, customOption ];
+
+	const [ isCustom, setIsCustom ] = useState(
+		() => !! format && ! suggestedFormats.includes( format )
+	);
+
+	let selectedOption;
+	if ( isCustom ) {
+		selectedOption = customOption;
+	} else if ( format ) {
+		selectedOption =
+			suggestedOptions.find(
+				( suggestedOption ) => suggestedOption.format === format
+			) ?? customOption;
+	} else {
+		selectedOption = defaultOption;
+	}
+
+	return (
+		<PanelBody title={ __( 'Format settings' ) }>
+			<SelectControl
+				label={ __( 'Date format' ) }
+				options={ options.map( ( option, index ) => ( {
+					value: index,
+					label: option.label,
+				} ) ) }
+				value={ options.indexOf( selectedOption ) }
+				onChange={ ( value ) => {
+					const option = options[ value ];
+					if ( option === defaultOption ) {
+						setFormat( null );
+						setIsCustom( false );
+					} else if ( option === customOption ) {
+						setFormat( format ?? siteFormat );
+						setIsCustom( true );
+					} else {
+						setFormat( option.format );
+						setIsCustom( false );
+					}
+				} }
+			/>
+			{ isCustom && (
+				<TextControl
+					label={ __( 'Custom format' ) }
+					help={ createInterpolateElement(
+						__(
+							'Enter a date or time <Link>format string</Link>.'
+						),
+						{
+							Link: (
+								<ExternalLink
+									href={ __(
+										'https://wordpress.org/support/article/formatting-date-and-time/'
+									) }
+								/>
+							),
+						}
+					) }
+					value={ format }
+					onChange={ ( value ) => setFormat( value ) }
+				/>
+			) }
+		</PanelBody>
 	);
 }
