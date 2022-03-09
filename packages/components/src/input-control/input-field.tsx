6f5696e9a44f95a2f92d4e361239002a98af7d85
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-09 13:22:48 +0100

    Fix input control blur logic, so that `onChange` is called after clamping the value

--- packages/components/src/input-control/input-field.tsx
+++ packages/components/src/input-control/input-field.tsx
@@ -103,13 +103,14 @@ function InputField(
 		onBlur( event );
 		setIsFocused?.( false );
 
-		// If isPressEnterToChange is set, cause the `onChange` callback to be fired.
-		if ( isPressEnterToChange && isDirty ) {
+		/**
+		 * If isPressEnterToChange is set, this commits the value to
+		 * the onChange callback.
+		 */
+		if ( isDirty || ! event.target.validity.valid ) {
 			wasDirtyOnBlur.current = true;
+			handleOnCommit( event );
 		}
-
-		// Commit the value (which allows consumers to apply their validation)
-		handleOnCommit( event );
 	};
 
 	const handleOnFocus = ( event: FocusEvent< HTMLInputElement > ) => {
