350c82f897619fc73ff97cd9859f87b6660165f0
Robert Anderson <robert@noisysocks.com>
2022-03-09 12:01:32 +1100

    Set $name on WP_Block_Editor_Context
    Configures the Gutenberg plugin to set $name on WP_Block_Editor_Context
    wherever the plugin initializes an editor. This helps plugins identify
    which editor they are customizing in filters such as
    'allowed_block_types_all'.
    
    Passing the extra array param is a no-op on versions of WordPress prior
    to 6.0 which do not have WP_Block_Editor_Context::$name.
    
    See https://github.com/WordPress/wordpress-develop/pull/2374.

--- lib/class-wp-rest-block-editor-settings-controller.php
+++ lib/class-wp-rest-block-editor-settings-controller.php
@@ -75,7 +75,7 @@ class WP_REST_Block_Editor_Settings_Controller extends WP_REST_Controller {
 	 * @return WP_Error|WP_REST_Response Response object on success, or WP_Error object on failure.
 	 */
 	public function get_items( $request ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis
-		$editor_context = new WP_Block_Editor_Context();
+		$editor_context = new WP_Block_Editor_Context( array( 'name' => 'core/edit-site' ) );
 		$settings       = get_block_editor_settings( array(), $editor_context );
 
 		return rest_ensure_response( $settings );
