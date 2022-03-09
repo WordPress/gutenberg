8b580e6e923e99d6a6c89778a38ec418fc24aee5
Gutenberg Repository Automation <gutenberg@wordpress.org>
2022-03-09 08:17:43 +0000

    Merge changes published in the Gutenberg plugin "trunk" branch

--- packages/edit-post/src/components/preferences-modal/test/index.js
+++ packages/edit-post/src/components/preferences-modal/test/index.js
@@ -12,31 +12,31 @@ import { useViewportMatch } from '@wordpress/compose';
 /**
  * Internal dependencies
  */
-import PreferencesModal from '../';
+import EditPostPreferencesModal from '../';
 
 // This allows us to tweak the returned value on each test.
 jest.mock( '@wordpress/data/src/components/use-select', () => jest.fn() );
 jest.mock( '@wordpress/compose/src/hooks/use-viewport-match', () => jest.fn() );
 
-describe( 'PreferencesModal', () => {
+describe( 'EditPostPreferencesModal', () => {
 	describe( 'should match snapshot when the modal is active', () => {
 		it( 'large viewports', () => {
 			useSelect.mockImplementation( () => ( { isModalActive: true } ) );
 			useViewportMatch.mockImplementation( () => true );
-			const wrapper = shallow( <PreferencesModal /> );
+			const wrapper = shallow( <EditPostPreferencesModal /> );
 			expect( wrapper ).toMatchSnapshot();
 		} );
 		it( 'small viewports', () => {
 			useSelect.mockImplementation( () => ( { isModalActive: true } ) );
 			useViewportMatch.mockImplementation( () => false );
-			const wrapper = shallow( <PreferencesModal /> );
+			const wrapper = shallow( <EditPostPreferencesModal /> );
 			expect( wrapper ).toMatchSnapshot();
 		} );
 	} );
 
 	it( 'should not render when the modal is not active', () => {
 		useSelect.mockImplementation( () => ( { isModalActive: false } ) );
-		const wrapper = shallow( <PreferencesModal /> );
+		const wrapper = shallow( <EditPostPreferencesModal /> );
 		expect( wrapper.isEmptyRender() ).toBe( true );
 	} );
 } );
