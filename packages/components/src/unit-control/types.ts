f2144eb254f86dd9003849c0bce3f9004cbbd72b
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-09 18:39:10 +0100

    Add missing default values

--- packages/components/src/unit-control/types.ts
+++ packages/components/src/unit-control/types.ts
@@ -117,12 +117,16 @@ export type UnitControlProps = UnitSelectControlProps &
 		/**
 		 * If true, pressing `UP` or `DOWN` along with the `SHIFT` key will increment
 		 * the value by the `shiftStep` value.
+		 *
+		 * @default true
 		 */
 		isShiftStepEnabled?: boolean;
 		/**
 		 * Amount to increment by when the `SHIFT` key is held down. This shift value
 		 * is a multiplier to the `step` value. For example, if the `step` value is `5`,
 		 * and `shiftStep` is `10`, each jump would increment/decrement by `50`.
+		 *
+		 * @default 10
 		 */
 		shiftStep?: number;
 	};
