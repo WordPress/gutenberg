9a1fdbe2e540e8518e9187c32b85944e269fdbc6
Lena Morita <lena@jaguchi.com>
2022-03-09 22:32:25 +0900

    Improve type docs for `orientation`

--- packages/components/src/divider/types.ts
+++ packages/components/src/divider/types.ts
@@ -24,6 +24,20 @@ export interface OwnProps {
 	marginEnd?: SpaceInput;
 }
 
+interface SeparatorPropsWithAdditionalDescription {
+	/**
+	 * Divider's orientation. When using inside a flex container, you may need to make sure the divider is `stretch` aligned
+	 * in order for it to be visible.
+	 *
+	 * @default 'horizontal'
+	 */
+	orientation?: SeparatorProps[ 'orientation' ];
+}
+
 export interface Props
-	extends Omit< SeparatorProps, 'children' | 'unstable_system' >,
+	extends Omit<
+			SeparatorProps,
+			'children' | 'unstable_system' | 'orientation'
+		>,
+		SeparatorPropsWithAdditionalDescription,
 		OwnProps {}
