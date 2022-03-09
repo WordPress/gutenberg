b1f0a135977af08c061e61706216e73329780ae1
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-09 17:09:25 +0100

    Comment out advanced stories

--- packages/components/src/unit-control/stories/index.tsx
+++ packages/components/src/unit-control/stories/index.tsx
@@ -68,60 +68,60 @@ export const _default = () => {
 	return <Example />;
 };
 
-export const WithSingleUnit = ( props ) => {
-	const [ value, setValue ] = useState( '10px' );
-	return (
-		<ControlWrapperView>
-			<UnitControl
-				{ ...props }
-				value={ value }
-				onChange={ ( v ) => setValue( v ) }
-			/>
-		</ControlWrapperView>
-	);
-};
-WithSingleUnit.args = {
-	label: 'Value',
-	units: CSS_UNITS.slice( 0, 1 ),
-};
+// export const WithSingleUnit = ( props ) => {
+// 	const [ value, setValue ] = useState( '10px' );
+// 	return (
+// 		<ControlWrapperView>
+// 			<UnitControl
+// 				{ ...props }
+// 				value={ value }
+// 				onChange={ ( v ) => setValue( v ) }
+// 			/>
+// 		</ControlWrapperView>
+// 	);
+// };
+// WithSingleUnit.args = {
+// 	label: 'Value',
+// 	units: CSS_UNITS.slice( 0, 1 ),
+// };
 
-export function WithCustomUnits() {
-	const [ value, setValue ] = useState( '10km' );
+// export function WithCustomUnits() {
+// 	const [ value, setValue ] = useState( '10km' );
 
-	const props = {
-		isResetValueOnUnitChange: boolean( 'isResetValueOnUnitChange', true ),
-		label: text( 'label', 'Distance' ),
-		units: object( 'units', [
-			{
-				value: 'km',
-				label: 'km',
-				default: 1,
-			},
-			{
-				value: 'mi',
-				label: 'mi',
-				default: 1,
-			},
-			{
-				value: 'm',
-				label: 'm',
-				default: 1000,
-			},
-			{
-				value: 'yd',
-				label: 'yd',
-				default: 1760,
-			},
-		] ),
-	};
+// 	const props = {
+// 		isResetValueOnUnitChange: boolean( 'isResetValueOnUnitChange', true ),
+// 		label: text( 'label', 'Distance' ),
+// 		units: object( 'units', [
+// 			{
+// 				value: 'km',
+// 				label: 'km',
+// 				default: 1,
+// 			},
+// 			{
+// 				value: 'mi',
+// 				label: 'mi',
+// 				default: 1,
+// 			},
+// 			{
+// 				value: 'm',
+// 				label: 'm',
+// 				default: 1000,
+// 			},
+// 			{
+// 				value: 'yd',
+// 				label: 'yd',
+// 				default: 1760,
+// 			},
+// 		] ),
+// 	};
 
-	return (
-		<ControlWrapperView>
-			<UnitControl
-				{ ...props }
-				value={ value }
-				onChange={ ( v ) => setValue( v ) }
-			/>
-		</ControlWrapperView>
-	);
-}
+// 	return (
+// 		<ControlWrapperView>
+// 			<UnitControl
+// 				{ ...props }
+// 				value={ value }
+// 				onChange={ ( v ) => setValue( v ) }
+// 			/>
+// 		</ControlWrapperView>
+// 	);
+// }
