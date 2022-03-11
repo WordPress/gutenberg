1af81d2b1165a42be466e091b2bf9ee35704a951
Adam Zieliński <adam@adamziel.com>
2022-03-11 11:29:49 +0100

    Update the usage of execute to say __experimentalExecute

--- packages/block-library/src/navigation/use-navigation-entities.js
+++ packages/block-library/src/navigation/use-navigation-entities.js
@@ -54,7 +54,7 @@ export default function useNavigationEntities( menuId ) {
 			per_page: -1,
 			context: 'view',
 		},
-		{ execute: !! menuId }
+		{ __experimentalExecute: !! menuId }
 	);
 
 	return {
