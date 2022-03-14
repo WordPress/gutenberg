e6ff52c71b9693939aae2fdb9169790cf35352d1
Adam Zielinski <adam@adamziel.com>
2022-03-14 13:13:38 +0100

    [Consistency] Use the word "record" for the entity record TypeScript types (#39251)
    * Rename "Entity type" interfaces to "Entity records" since that's what they actually model
    
    * Bring the record types back from the records directory to entity-types directory
    
    * Replace imports from ../helpers with ./helpers
    
    * Remove unnecessary blank line

--- packages/core-data/src/entity-types/widget.ts
+++ packages/core-data/src/entity-types/widget.ts
@@ -1,12 +1,12 @@
 /**
  * Internal dependencies
  */
-import { Context, ContextualField, OmitNevers } from './helpers';
+import type { Context, ContextualField, OmitNevers } from './helpers';
 
-import { BaseEntityTypes as _BaseEntityTypes } from './base-entity-types';
+import type { BaseEntityRecords as _BaseEntityRecords } from './base-entity-records';
 
-declare module './base-entity-types' {
-	export namespace BaseEntityTypes {
+declare module './base-entity-records' {
+	export namespace BaseEntityRecords {
 		export interface Widget< C extends Context > {
 			/**
 			 * Unique identifier for the widget.
@@ -60,5 +60,5 @@ declare module './base-entity-types' {
 }
 
 export type Widget< C extends Context > = OmitNevers<
-	_BaseEntityTypes.Widget< C >
+	_BaseEntityRecords.Widget< C >
 >;
