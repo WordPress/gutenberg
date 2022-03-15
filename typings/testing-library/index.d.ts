827534579b4290787b6c6a7375929d975e7d94f5
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-15 09:42:36 +0100

    Update `toMatchStyleDiffSnapshot` type defs

--- typings/testing-library/index.d.ts
+++ typings/testing-library/index.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="jest"/>
 
 declare namespace jest {
-	interface Matchers< R > {
-		toMatchStyleDiffSnapshot( expected: Element ): CustomMatcherResult;
+	interface Matchers< R, T > {
+		/**
+		 * Similar to `toMatchDiffSnapshot`, but only taking into account the style
+		 * rules associated to the two elements being compared.
+		 */
+		toMatchStyleDiffSnapshot( expected: Element ): R;
 	}
 }
