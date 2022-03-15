0083025b5ddd8eef9617c83509a32d9d1fe18d06
Matias Benedetto <matias.benedetto@gmail.com>
2022-03-15 11:25:23 -0300

    testing apiFetch in jest

--- test/integration/a11y-blocks/a11y-blocks.spec.js
+++ test/integration/a11y-blocks/a11y-blocks.spec.js
@@ -1,5 +1,18 @@
+const apiFetch = require( '@wordpress/api-fetch' );
+
 describe( 'A test suite', () => {
+	let post;
+	beforeEach( async () => {
+		post = await apiFetch( {
+			path: '/wp/v2/posts/',
+			method: 'POST',
+			data: { title: 'New Post Title' },
+		} );
+		console.log( post );
+	} );
+
 	it( 'should pass', () => {
-		expect( true ).toBe( true );
+		expect( post ).toBeTruthy();
+		console.log( post );
 	} );
 } );
