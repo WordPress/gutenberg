e9d9419a030477e5cdcdf52e83846fb4c9ceaa4e
Alefe Souza <contact@alefesouza.com>
2022-03-15 19:30:52 -0300

    Use user-event@14 recommended setup

--- packages/components/src/form-file-upload/test/index.js
+++ packages/components/src/form-file-upload/test/index.js
@@ -1,13 +1,23 @@
 /**
  * External dependencies
  */
-import { render, screen } from '@testing-library/react';
+import { render as RTLrender, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 /**
  * Internal dependencies
  */
 import FormFileUpload from '../';
+
+function render( jsx ) {
+	return {
+		user: userEvent.setup( {
+			// Avoids timeout errors (https://github.com/testing-library/user-event/issues/565#issuecomment-1064579531).
+			delay: null,
+		} ),
+		...RTLrender( jsx ),
+	};
+}
 
 describe( 'FormFileUpload', () => {
 	it( 'should show an Icon Button and a hidden input', () => {
@@ -19,10 +29,10 @@ describe( 'FormFileUpload', () => {
 		expect( input.style.display ).toBe( 'none' );
 	} );
 
-	it( 'should not fire a change event after selecting the same file', () => {
+	it( 'should not fire a change event after selecting the same file', async () => {
 		const onChange = jest.fn();
 
-		render(
+		const { user } = render(
 			<FormFileUpload onChange={ onChange }>
 				My Upload Button
 			</FormFileUpload>
@@ -35,24 +45,21 @@ describe( 'FormFileUpload', () => {
 
 		const input = screen.getByTestId( 'input' );
 
-		userEvent.upload( input, file );
+		await user.upload( input, file );
 
 		// await last upload event propagation
-		setTimeout( () => {
-			userEvent.upload( input, file );
+		setTimeout( async () => {
+			await user.upload( input, file );
 
 			expect( onChange ).toHaveBeenCalledTimes( 1 );
 		}, 0 );
 	} );
 
-	it( 'should fire a change event after selecting the same file if event.target.value is nulled on click', () => {
+	it( 'should fire a change event after selecting the same file if there is a onClick', async () => {
 		const onChange = jest.fn();
 
-		render(
-			<FormFileUpload
-				onClick={ ( e ) => ( e.target.value = null ) }
-				onChange={ onChange }
-			>
+		const { user } = render(
+			<FormFileUpload onClick={ jest.fn() } onChange={ onChange }>
 				My Upload Button
 			</FormFileUpload>
 		);
@@ -63,13 +70,13 @@ describe( 'FormFileUpload', () => {
 		} );
 
 		const input = screen.getByTestId( 'input' );
-		userEvent.upload( input, file );
+		await user.upload( input, file );
 
 		expect( onChange ).toHaveBeenCalledTimes( 1 );
 
 		// await last upload event propagation and onClick event run
-		setTimeout( () => {
-			userEvent.upload( input, file );
+		setTimeout( async () => {
+			await user.upload( input, file );
 
 			expect( onChange ).toHaveBeenCalledTimes( 2 );
 		}, 0 );
