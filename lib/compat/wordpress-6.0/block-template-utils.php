4bfe4f124de47763ff7dbe8a3284912a056f0702
Ben Dwyer <ben@scruffian.com>
2022-03-15 13:18:37 +0000

    Theme Export: Export the whole theme (#39202)
    * Theme Export: Export the whole theme with updated templates and styles
    
    * Ensure paths are normalized
    
    * ignore utility directories
    
    * rename function
    
    * fixing linter errors
    
    * fix lint error

--- lib/compat/wordpress-6.0/block-template-utils.php
+++ lib/compat/wordpress-6.0/block-template-utils.php
@@ -8,6 +8,25 @@
  */
 
 /**
+ * Filters theme directories that should be ignored during export.
+ *
+ * @since 6.0.0
+ *
+ * @param string $path The path of the file in the theme.
+ * @return Bool Whether this file is in an ignored directory.
+ */
+function gutenberg_is_theme_directory_ignored( $path ) {
+	$directories_to_ignore = array( '.git', 'node_modules', 'vendor' );
+	foreach ( $directories_to_ignore as $directory ) {
+		if ( strpos( $path, $directory ) === 0 ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
  * Creates an export of the current templates and
  * template parts from the site editor at the
  * specified path in a ZIP file.
@@ -26,13 +45,38 @@ function gutenberg_generate_block_templates_export_file() {
 	$filename = get_temp_dir() . 'edit-site-export-' . $obscura . '.zip';
 
 	$zip = new ZipArchive();
-	if ( true !== $zip->open( $filename, ZipArchive::CREATE ) ) {
+	if ( true !== $zip->open( $filename, ZipArchive::CREATE | ZipArchive::OVERWRITE ) ) {
 		return new WP_Error( 'unable_to_create_zip', __( 'Unable to open export file (archive) for writing.', 'gutenberg' ) );
 	}
 
-	$zip->addEmptyDir( 'theme' );
-	$zip->addEmptyDir( 'theme/templates' );
-	$zip->addEmptyDir( 'theme/parts' );
+	$theme_name = wp_get_theme()->get( 'TextDomain' );
+
+	$zip->addEmptyDir( $theme_name );
+	$zip->addEmptyDir( $theme_name . '/templates' );
+	$zip->addEmptyDir( $theme_name . '/parts' );
+
+	// Get path of the theme.
+	$theme_path = wp_normalize_path( get_stylesheet_directory() );
+
+	// Create recursive directory iterator.
+	$theme_files = new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator( $theme_path ),
+		RecursiveIteratorIterator::LEAVES_ONLY
+	);
+
+	// Make a copy of the current theme.
+	foreach ( $theme_files as $file ) {
+		// Skip directories as they are added automatically.
+		if ( ! $file->isDir() ) {
+			// Get real and relative path for current file.
+			$file_path     = wp_normalize_path( $file->getRealPath() );
+			$relative_path = substr( $file_path, strlen( $theme_path ) + 1 );
+
+			if ( ! gutenberg_is_theme_directory_ignored( $relative_path ) ) {
+				$zip->addFile( $file_path, $theme_name . '/' . $relative_path );
+			}
+		}
+	}
 
 	// Load templates into the zip file.
 	$templates = gutenberg_get_block_templates();
@@ -40,7 +84,7 @@ function gutenberg_generate_block_templates_export_file() {
 		$template->content = _remove_theme_attribute_in_block_template_content( $template->content );
 
 		$zip->addFromString(
-			'theme/templates/' . $template->slug . '.html',
+			$theme_name . '/templates/' . $template->slug . '.html',
 			$template->content
 		);
 	}
@@ -49,27 +93,18 @@ function gutenberg_generate_block_templates_export_file() {
 	$template_parts = gutenberg_get_block_templates( array(), 'wp_template_part' );
 	foreach ( $template_parts as $template_part ) {
 		$zip->addFromString(
-			'theme/parts/' . $template_part->slug . '.html',
+			$theme_name . '/parts/' . $template_part->slug . '.html',
 			$template_part->content
 		);
 	}
 
+	// Load theme.json into the zip file.
 	$tree = WP_Theme_JSON_Resolver_Gutenberg::get_theme_data();
 	$tree->merge( WP_Theme_JSON_Resolver_Gutenberg::get_user_data() );
 
 	$zip->addFromString(
-		'theme/theme.json',
+		$theme_name . '/theme.json',
 		wp_json_encode( $tree->get_data(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE )
-	);
-
-	$zip->addFile(
-		wp_normalize_path( get_stylesheet_directory() ) . '/index.php',
-		'theme/index.php'
-	);
-
-	$zip->addFile(
-		wp_normalize_path( get_stylesheet_directory() ) . '/style.css',
-		'theme/style.css'
 	);
 
 	// Save changes to the zip file.
