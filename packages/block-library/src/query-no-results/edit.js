23d7e077a0d6463275dd792e01c9c83692b6a15d
Ari Stathopoulos <aristath@gmail.com>
2022-03-16 12:46:27 +0200

    Add content to the default paragraph

--- packages/block-library/src/query-no-results/edit.js
+++ packages/block-library/src/query-no-results/edit.js
@@ -2,8 +2,11 @@
  * WordPress dependencies
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
 
-const TEMPLATE = [ [ 'core/paragraph' ] ];
+const TEMPLATE = [
+	[ 'core/paragraph', { content: __( 'No results found' ) } ],
+];
 
 export default function QueryNoResultsEdit() {
 	const blockProps = useBlockProps();
