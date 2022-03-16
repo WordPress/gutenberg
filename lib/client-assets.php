96d4ca814036d1fcd4d0c6f1934bd58fab592b31
Dominik Schilling <dominikschilling+git@gmail.com>
2022-03-16 15:04:33 +0100

    Move wp-i18n to head (#39501)
    * Try to fix E2E tests
    
    * Do this only for wp-i18n

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -54,6 +54,12 @@ function gutenberg_url( $path ) {
  *                                    Default 'false'.
  */
 function gutenberg_override_script( $scripts, $handle, $src, $deps = array(), $ver = false, $in_footer = false ) {
+	/*
+	 * Force `wp-i18n` script to be registered in the <head> as a
+	 * temporary workaround for https://meta.trac.wordpress.org/ticket/6195.
+	 */
+	$in_footer = 'wp-i18n' === $handle ? false : $in_footer;
+
 	$script = $scripts->query( $handle, 'registered' );
 	if ( $script ) {
 		/*
