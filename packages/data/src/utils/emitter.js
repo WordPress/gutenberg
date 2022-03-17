3e1b9f3c83ec4b74d99b376e2d47a569098fffc1
Riad Benguella <benguella@gmail.com>
2022-03-17 17:59:40 +0100

    Fix isPending reset code

--- packages/data/src/utils/emitter.js
+++ packages/data/src/utils/emitter.js
@@ -7,11 +7,11 @@ export function createEmitter() {
 	let isPending = false;
 	const listeners = new Set();
 	const notifyListeners = () => {
+		isPending = false;
 		// We use Array.from to clone the listeners Set
 		// This ensures that we don't run a listener
 		// that was added as a response to another listener.
 		Array.from( listeners ).forEach( ( listener ) => listener() );
-		isPending = false;
 	};
 
 	return {
