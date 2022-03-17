e9e4fae7e42f5d1772e633cac14fd5b61a531297
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2022-03-17 18:46:12 +0200

    Add some inline comments on the audio, file tests

--- packages/react-native-editor/__device-tests__/gutenberg-editor-audio.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-audio.test.js
@@ -7,21 +7,30 @@ import testData from './helpers/test-data';
 
 describe( 'Gutenberg Editor Audio Block tests', () => {
 	it( 'should be able to add an audio block', async () => {
+		// add an audio block
 		await editorPage.addNewBlock( blockNames.audio );
 
+		// dismiss the media picker automatically opened when adding an audio block
 		await waitForMediaLibrary( editorPage.driver );
 		await editorPage.closePicker();
 
+		// verify there's an audio block
 		const block = await editorPage.getFirstBlockVisible();
 		await expect( block ).toBeTruthy();
 
+		// tap on the audio block
 		block.click();
 
+		// wait for the media picker's Media Library option to come up
 		await waitForMediaLibrary( editorPage.driver );
+
+		// tap on Media Library option
 		await editorPage.chooseMediaLibrary();
 
+		// get the html version of the content
 		const html = await editorPage.getHtmlContent();
 
+		// verify the html matches the expected content
 		expect( html.toLowerCase() ).toBe(
 			testData.audioBlockPlaceholder.toLowerCase()
 		);
--- packages/react-native-editor/__device-tests__/gutenberg-editor-file-@canary.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-file-@canary.test.js
@@ -7,21 +7,30 @@ import testData from './helpers/test-data';
 
 describe( 'Gutenberg Editor File Block tests', () => {
 	it( 'should be able to add a file block', async () => {
+		// add a file block
 		await editorPage.addNewBlock( blockNames.file );
 
+		// dismiss the media picker automatically opened when adding a file block
 		await waitForMediaLibrary( editorPage.driver );
 		await editorPage.closePicker();
 
+		// verify there's a file block
 		const block = await editorPage.getFirstBlockVisible();
 		await expect( block ).toBeTruthy();
 
+		// tap on the file block
 		block.click();
 
+		// wait for the media picker's Media Library option to come up
 		await waitForMediaLibrary( editorPage.driver );
+
+		// tap on Media Library option
 		await editorPage.chooseMediaLibrary();
 
+		// get the html version of the content
 		const html = await editorPage.getHtmlContent();
 
+		// verify the html matches the expected content
 		expect( html.toLowerCase() ).toBe(
 			testData.fileBlockPlaceholder.toLowerCase()
 		);
