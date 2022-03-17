56a6def6db5f82aad2fc979da927c2594c7d8cb7
Carlos Bravo <carlos.bravo@automattic.com>
2022-03-17 18:32:26 +0100

    Updated tests with recommendations

--- packages/e2e-test-utils/src/set-option.js
+++ packages/e2e-test-utils/src/set-option.js
@@ -21,27 +21,26 @@ export async function setOption(
 	await switchUserToAdmin();
 	await visitAdminPage( adminPage );
 
-	await page.focus( `#${ setting }` );
-
+	const currentValue = await page.$eval(
+		`#${ setting }`,
+		( element ) => element.value
+	);
 	if ( typeof value === 'boolean' ) {
-		const currentValue = await page.$eval(
-			`#${ setting }`,
-			( element ) => element.value
-		);
 		// Update the checkbox.
 		if ( ( currentValue && ! value ) || ( ! currentValue && value ) ) {
 			await page.click( `#${ setting }` );
 		}
+	} else {
+		// Update a text field.
+		await page.focus( `#${ setting }` );
+		await pressKeyWithModifier( 'primary', 'a' );
+		await page.type( `#${ setting }`, value );
 	}
-
-	// Update a text field.
-	await pressKeyWithModifier( 'primary', 'a' );
-	await page.type( `#${ setting }`, value );
 
 	await Promise.all( [
 		page.click( '#submit' ),
 		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
 	] );
-
 	await switchUserToTest();
+	return currentValue;
 }
