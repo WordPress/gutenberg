ebf026de8fba0a3fd396aa3ab4efbef004bb38a3
Lena Morita <lena@jaguchi.com>
2022-03-19 00:47:29 +0900

    Add control for `initialPosition`

--- packages/components/src/range-control/stories/index.js
+++ packages/components/src/range-control/stories/index.js
@@ -29,6 +29,7 @@ export default {
 		color: { control: { type: 'color' } },
 		disabled: { control: { type: 'boolean' } },
 		help: { control: { type: 'text' } },
+		initialPosition: { control: { type: 'number' } },
 		marks: { control: { type: 'object' } },
 		min: { control: { type: 'number' } },
 		max: { control: { type: 'number' } },
