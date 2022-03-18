8061aab6813f3e6d4a9418b3001e34263221fcbc
tellthemachines <isabel@tellthemachines.com>
2022-03-18 15:37:28 +1100

    Move preference into modal.

--- packages/edit-site/src/components/header/more-menu/index.js
+++ packages/edit-site/src/components/header/more-menu/index.js
@@ -66,19 +66,6 @@ export default function MoreMenu() {
 									'Spotlight mode deactivated'
 								) }
 							/>
-							<FeatureToggle
-								feature="showIconLabels"
-								label={ __( 'Show button text labels' ) }
-								info={ __(
-									'Show text instead of icons on buttons'
-								) }
-								messageActivated={ __(
-									'Button labels activated'
-								) }
-								messageDeactivated={ __(
-									'Button labels deactivated'
-								) }
-							/>
 							<ModeSwitcher />
 							<ActionItem.Slot
 								name="core/edit-site/plugin-more-menu"
