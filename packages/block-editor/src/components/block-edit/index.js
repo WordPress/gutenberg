ae13af27d8cc7ec0681d1f12c244896fa70d2593
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-03-21 13:22:50 +0200

    Remove superfluous edit

--- packages/block-editor/src/components/block-edit/index.js
+++ packages/block-editor/src/components/block-edit/index.js
@@ -7,7 +7,6 @@ import { useMemo } from '@wordpress/element';
  * Internal dependencies
  */
 import Edit from './edit';
-
 import { BlockEditContextProvider, useBlockEditContext } from './context';
 
 /**
