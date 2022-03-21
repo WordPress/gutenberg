ad54cc5dca44e0a72605e291c164469673ff7b41
Shekh Aezaz <aezazahemad.shekh@multidots.com>
2022-03-21 13:21:32 +0530

    added some extra social icons

---
+++ packages/block-library/src/social-link/icons/bitbucket.js
@@ -0,0 +1,16 @@
+/**
+ * WordPress dependencies
+ */
+import { Path, SVG } from '@wordpress/primitives';
+
+export const BitbucketIcon = () => (
+	<SVG
+		width="24"
+		height="24"
+		preserveAspectRatio="xMidYMid"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="-0.9662264221278978 -0.5824607696358868 257.93281329857973 230.8324730411935"
+	>
+		<Path d="M8.308 0A8.202 8.202 0 0 0 .106 9.516l34.819 211.373a11.155 11.155 0 0 0 10.909 9.31h167.04a8.202 8.202 0 0 0 8.201-6.89l34.82-213.752a8.202 8.202 0 0 0-8.203-9.514zm146.616 152.768h-53.315l-14.436-75.42h80.67z" />
+	</SVG>
+);
