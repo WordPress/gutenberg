95f6ea604a1bf57df8d027b1a7fd207522f27f40
Gerardo <gerardo.pacheco@automattic.com>
2022-03-21 11:50:27 +0100

    Mobile - Draggable component - Add props documentation

--- packages/components/src/draggable/index.native.js
+++ packages/components/src/draggable/index.native.js
@@ -4,6 +4,20 @@
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, { useSharedValue } from 'react-native-reanimated';
 
+/**
+ * Draggable component
+ *
+ * @param {Object}                                      props                       Component props.
+ * @param {JSX.Element}                                 props.children              Children to be rendered.
+ * @param {?number}                                     props.maxDistance           Maximum distance, that defines how far the finger is allowed to travel during a long press gesture.
+ * @param {?number}                                     props.minDuration           Minimum time, that a finger must remain pressed on the corresponding view.
+ * @param {Function}                                    props.onDragEnd             Callback when dragging happens over an element.
+ * @param {Function}                                    props.onDragOver            Callback when dragging ends.
+ * @param {Function}                                    props.onDragStart           Callback when dragging stars.
+ * @param {import('react-native-reanimated').StyleProp} props.wrapperAnimatedStyles Animated styles for the wrapper component.
+ *
+ * @return {JSX.Element} The component to be rendered.
+ */
 export default function Draggable( {
 	children,
 	maxDistance = 1000,
