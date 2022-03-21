097ed4ba0f276c1071977c7831743fa89e4a3915
Glen Davies <glendaviesnz@users.noreply.github.com>
2022-03-22 09:43:24 +1300

    Gallery block: reduce e2e flakiness (#39565)
    * Select Image from list view panel to try and avoid issues with uploaded image getting and losing focus
    
    Co-authored-by: Glen Davies <glen.davies@a8c.com>

--- packages/e2e-tests/specs/editor/blocks/gallery.test.js
+++ packages/e2e-tests/specs/editor/blocks/gallery.test.js
@@ -85,7 +85,13 @@ describe( 'Gallery', () => {
 			'.wp-block-gallery .wp-block-image'
 		);
 
-		await figureElement.click();
+		// The Image needs to be selected from the List view panel due to the
+		// way that Image uploads take and lose focus.
+		await openListView();
+		const imageListLink = await page.waitForXPath(
+			`//a[contains(text(), 'Image')]`
+		);
+		await imageListLink.click();
 
 		const captionElement = await figureElement.$(
 			'.block-editor-rich-text__editable'
