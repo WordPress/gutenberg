63227997d46f1beab73c91bc7aee752545ad2730
Jorge <jorge.costa@developer.pt>
2022-03-22 18:12:13 +0000

    Add: Transforms for the list v2 block

--- packages/block-library/src/list/v2/index.js
+++ packages/block-library/src/list/v2/index.js
@@ -8,11 +8,13 @@ import { list as icon } from '@wordpress/icons';
  */
 import edit from './edit';
 import save from './save';
+import transforms from './transforms';
 
 const settings = {
 	icon,
 	edit,
 	save,
+	transforms,
 };
 
 export default settings;
