2f869c33709f7085da6ef82add26c095692458bc
Glen Davies <glen.davies@a8c.com>
2022-03-23 11:26:30 +1300

    Apply ! important to user preset styles for theme.json themes only and provide opt-out

--- lib/compat/wordpress-5.9/class-wp-theme-json-5-9.php
+++ lib/compat/wordpress-5.9/class-wp-theme-json-5-9.php
@@ -231,8 +231,9 @@ class WP_Theme_JSON_5_9 {
 	 * @var array
 	 */
 	const VALID_SETTINGS = array(
-		'appearanceTools' => null,
-		'border'          => array(
+		'appearanceTools'       => null,
+		'prioritiseUserPresets' => null,
+		'border'                => array(
 			'color'  => null,
 			'radius' => null,
 			'style'  => null,
@@ -953,12 +954,13 @@ class WP_Theme_JSON_5_9 {
 				foreach ( $slugs as $slug ) {
 					$css_var     = static::replace_slug_in_string( $preset_metadata['css_vars'], $slug );
 					$class_name  = static::replace_slug_in_string( $class, $slug );
+					$prioritise_user_presets = ( WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() && true === $settings['prioritiseUserPresets'] ) ? '!important' : '';
 					$stylesheet .= static::to_ruleset(
 						static::append_to_selector( $selector, $class_name ),
 						array(
 							array(
 								'name'  => $property,
-								'value' => 'var(' . $css_var . ') !important',
+								'value' => 'var(' . $css_var . ')' . $prioritise_user_presets,
 							),
 						)
 					);
