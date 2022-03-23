42f8ac0f4f4feae13352e6352a140bd637b3336c
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-23 12:38:01 +0100

    Update toMatchStyleDiffSnapshot docs

--- typings/testing-library/index.d.ts
+++ typings/testing-library/index.d.ts
@@ -1,11 +1,15 @@
 /// <reference types="jest"/>
 
 declare namespace jest {
-	interface Matchers< R, T > {
+	interface Matchers< R > {
 		/**
-		 * Similar to `toMatchDiffSnapshot`, but only taking into account the style
-		 * rules associated to the two elements being compared.
+		 * Similar to the [`toMatchDiffSnapshot` matcher](https://github.com/jest-community/snapshot-diff), this custom matcher allows to snapshot only the difference
+		 * between the _styles_ associated to two different states of a component.
+		 *
+		 * @see [test/unit/config/matchers/to-match-style-diff-snapshot.js](https://github.com/WordPress/gutenberg/blob/trunk/test/unit/config/matchers/to-match-style-diff-snapshot.js)
+		 * @see [Testing Overview docs](https://github.com/WordPress/gutenberg/blob/trunk/docs/contributors/code/testing-overview.md#best-practices)
+		 * @cite https://github.com/testing-library/react-testing-library/issues/36#issuecomment-440442300
 		 */
-		toMatchStyleDiffSnapshot( expected: Element ): R;
+		toMatchStyleDiffSnapshot( expected: Element | null ): R;
 	}
 }
