5aeb66148bc11ed8da40e996ac911b2df1e0327b
Justin Ahinon <justiny.ahinon@gmail.com>
2022-03-23 09:52:02 +0200

    Add new utilities to index.ts

--- packages/e2e-test-utils-playwright/src/page/index.ts
+++ packages/e2e-test-utils-playwright/src/page/index.ts
@@ -16,7 +16,18 @@ import {
 } from './press-key-with-modifier';
 import { showBlockToolbar } from './show-block-toolbar';
 import { visitAdminPage } from './visit-admin-page';
-
+import { focusSelectedBlock } from './inserter/focus-selected-block';
+import { insertBlock } from './inserter/insert-block';
+import { isGlobalInserterOpen } from './inserter/is-global-inserter-open';
+import { openGlobalBlockInserter } from './inserter/open-global-block-inserter';
+import { searchForBlock } from './inserter/search-for-block';
+import { toggleGlobalBlockInserter } from './inserter/toggle-global-block-inserter';
+import { waitForInserterCloseAndContentFocus } from './inserter/wait-for-inserter-close-and-focus-content';
+import { canvas } from './canva';
+import { clickBlockToolbarButton } from './click-block-toolbar-button';
+import { showBlockToolbar } from '../show-block-toolbar';
+import { getEditedPostContent } from './get-edited-post-content';
+import { wpDataSelect } from './wp-data-select';
 class PageUtils {
 	browser: Browser;
 	page: Page;
@@ -36,6 +47,18 @@ class PageUtils {
 	setClipboardData = setClipboardData;
 	showBlockToolbar = showBlockToolbar;
 	visitAdminPage = visitAdminPage;
+	focusSelectedBlock = focusSelectedBlock;
+	insertBlock = insertBlock;
+	isGlobalInserterOpen = isGlobalInserterOpen;
+	openGlobalBlockInserter = openGlobalBlockInserter;
+	searchForBlock = searchForBlock;
+	toggleGlobalBlockInserter = toggleGlobalBlockInserter;
+	waitForInserterCloseAndContentFocus = waitForInserterCloseAndContentFocus;
+	canvas = canvas;
+	clickBlockToolbarButton = clickBlockToolbarButton;
+	showBlockToolbar = showBlockToolbar;
+	getEditedPostContent = getEditedPostContent;
+	wpDataSelect = wpDataSelect;
 }
 
 export { PageUtils };
