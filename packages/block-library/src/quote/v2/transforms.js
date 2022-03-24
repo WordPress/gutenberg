9be7fe4c60d3e51fb6ca5e43f2cd1785938e58f7
André <583546+oandregal@users.noreply.github.com>
2022-03-24 12:48:30 +0100

    Quote v2: add transform to group

--- packages/block-library/src/quote/v2/transforms.js
+++ packages/block-library/src/quote/v2/transforms.js
@@ -8,6 +8,23 @@ const transforms = {
 	to: [
 		{
 			type: 'block',
+			blocks: [ 'core/group' ],
+			transform: ( { attribution }, innerBlocks ) =>
+				createBlock(
+					'core/group',
+					{},
+					attribution
+						? [
+								...innerBlocks,
+								createBlock( 'core/paragraph', {
+									content: attribution,
+								} ),
+						  ]
+						: innerBlocks
+				),
+		},
+		{
+			type: 'block',
 			blocks: [ 'core/pullquote' ],
 			isMatch: ( attributes, block ) => {
 				return block.innerBlocks.every(
