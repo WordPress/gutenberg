6b6b02f484859f051b683b6b8e954f1cb7a5188e
André <583546+oandregal@users.noreply.github.com>
2022-03-24 12:37:31 +0100

    Quote v2: add pullquote transformation if all innerblocks are paragraphs

--- packages/block-library/src/quote/v2/transforms.js
+++ packages/block-library/src/quote/v2/transforms.js
@@ -1,6 +1,27 @@
+/**
+ * WordPress dependencies
+ */
+import { createBlock, serialize } from '@wordpress/blocks';
+
 const transforms = {
 	from: [],
-	to: [],
+	to: [
+		{
+			type: 'block',
+			blocks: [ 'core/pullquote' ],
+			isMatch: ( attributes, block ) => {
+				return block.innerBlocks.every(
+					( { name } ) => name === 'core/paragraph'
+				);
+			},
+			transform: ( { attribution }, innerBlocks ) => {
+				return createBlock( 'core/pullquote', {
+					value: serialize( innerBlocks ),
+					citation: attribution,
+				} );
+			},
+		},
+	],
 };
 
 export default transforms;
