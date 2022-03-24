43db746247bb9972d59526ed40322e87bc8d6e11
André <583546+oandregal@users.noreply.github.com>
2022-03-24 11:22:02 +0100

    Fix save

--- packages/block-library/src/quote/v2/save.js
+++ packages/block-library/src/quote/v2/save.js
@@ -1,27 +1,25 @@
 /**
  * WordPress dependencies
  */
-import {
-	RichText,
-	useBlockProps,
-	useInnerBlocksProps,
-} from '@wordpress/block-editor';
+import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
 	const { attribution } = attributes;
-	const wrapperProps = useBlockProps.save();
-	const innerBlocksProps = useInnerBlocksProps.save();
-	const allBlockProps = useInnerBlocksProps.save( wrapperProps );
+	const blockProps = useBlockProps.save();
 
 	const hasAttribution = ! RichText.isEmpty( attribution );
 	return hasAttribution ? (
-		<figure { ...wrapperProps }>
-			<blockquote { ...innerBlocksProps } />
+		<figure { ...blockProps }>
+			<blockquote>
+				<InnerBlocks.Content />
+			</blockquote>
 			<figcaption>
 				<RichText.Content value={ attribution } />
 			</figcaption>
 		</figure>
 	) : (
-		<blockquote { ...allBlockProps } />
+		<blockquote { ...blockProps }>
+			<InnerBlocks.Content />
+		</blockquote>
 	);
 }
