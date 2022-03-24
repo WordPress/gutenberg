702a0d14eebcc374c26d2a82fbae16e6b5839e9d
André <583546+oandregal@users.noreply.github.com>
2022-03-24 12:15:37 +0100

    Quote v2: add innerblocks support (#39704)
    Co-authored-by: Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
    Co-authored-by: Dennis Snell <dennis.snell@automattic.com>
    Co-authored-by: Ari Stathopoulos <aristath@gmail.com>

--- packages/block-library/src/quote/v2/save.js
+++ packages/block-library/src/quote/v2/save.js
@@ -1,5 +1,25 @@
-function Save() {
-	return <div>Quote block v2</div>;
-}
+/**
+ * WordPress dependencies
+ */
+import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
-export default Save;
+export default function save( { attributes } ) {
+	const { attribution } = attributes;
+	const blockProps = useBlockProps.save();
+
+	const hasAttribution = ! RichText.isEmpty( attribution );
+	return hasAttribution ? (
+		<figure { ...blockProps }>
+			<blockquote>
+				<InnerBlocks.Content />
+			</blockquote>
+			<figcaption>
+				<RichText.Content value={ attribution } />
+			</figcaption>
+		</figure>
+	) : (
+		<blockquote { ...blockProps }>
+			<InnerBlocks.Content />
+		</blockquote>
+	);
+}
