e2af0b25ea0d55c4081fc7fd7e2bda7bd3e3d9a5
Kerry Liu <gwwar@users.noreply.github.com>
2022-03-28 06:03:24 -0700

    Navigation: add dedicated sidebar for managing layout of navigation menus (#39290)
    * Navigation: try adding dedicated sidebar for managing layout of navigation menus
    
    * Fix table width to avoid overflow
    
    * Iterate on var naming and code comments
    
    * Further naming refinement
    
    * More var renaming
    
    * Rename panel title for clarity
    
    * Improve a11y of component
    
    * Revert addition of fixed table layout.
    
    Introduces too many visual bugs
    
    * Wrap items to avoid off screen movers where possible
    
    * Move additional padding
    
    * Revert id on ListView
    
    This is now handled in https://github.com/WordPress/gutenberg/pull/39494
    
    * Use standard getEntityRecords and remove custom entity selector
    
    Now moved to https://github.com/WordPress/gutenberg/pull/39498
    
    * Add handling for no data or limited data
    
    * Collapse all Nav nodes by default
    
    * Only show empty message when there are no menus
    
    * Guard against editing unpublished Menus
    
    * Select Gallery block in list view correctly
    
    * Remove rebase artifact
    
    * Add empty message in case of empty menu
    
    Co-authored-by: Dave Smith <getdavemail@gmail.com>

---
+++ packages/block-editor/src/components/block-toolbar/block-name-context.js
@@ -0,0 +1,8 @@
+/**
+ * WordPress dependencies
+ */
+import { createContext } from '@wordpress/element';
+
+const __unstableBlockNameContext = createContext( '' );
+
+export default __unstableBlockNameContext;
