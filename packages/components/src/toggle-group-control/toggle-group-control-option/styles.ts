0204a7366e87709d2e5fac2a597d6df10e70b60b
ntsekouras <ntsekouras@outlook.com>
2022-03-28 22:43:24 +0300

    add prop to readme and fix styles

--- packages/components/src/toggle-group-control/toggle-group-control-option/styles.ts
+++ packages/components/src/toggle-group-control/toggle-group-control-option/styles.ts
@@ -27,6 +27,7 @@ export const buttonView = css`
 	border: none;
 	border-radius: ${ CONFIG.controlBorderRadius };
 	color: ${ COLORS.gray[ 700 ] };
+	fill: currentColor;
 	cursor: pointer;
 	display: flex;
 	font-family: inherit;
@@ -55,7 +56,6 @@ export const buttonView = css`
 
 export const buttonActive = css`
 	color: ${ COLORS.white };
-	fill: ${ COLORS.white };
 	background-color: ${ COLORS.gray[ 900 ] };
 `;
 
