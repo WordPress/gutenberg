316a4ecbd20570abd8fc16de019c1db19e0809ae
André <583546+oandregal@users.noreply.github.com>
2022-03-29 10:53:59 +0200

    Add deprecation

--- packages/block-library/src/quote/v2/index.js
+++ packages/block-library/src/quote/v2/index.js
@@ -11,6 +11,7 @@ import { addFilter } from '@wordpress/hooks';
 import edit from './edit';
 import save from './save';
 import transforms from './transforms';
+import deprecated from './deprecated';
 
 const settings = {
 	icon,
@@ -30,6 +31,7 @@ const settings = {
 	transforms,
 	edit,
 	save,
+	deprecated,
 };
 
 export default settings;
