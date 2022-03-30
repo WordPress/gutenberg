81768a844b53dcd39ea1ae99ef3f5ba71bbe6b92
Ben Dwyer <ben@scruffian.com>
2022-03-30 11:57:05 +0100

    Try Dark sidebar for Navigation and Global Styles

--- packages/edit-site/src/components/navigation-sidebar/navigation-menu-sidebar/index.js
+++ packages/edit-site/src/components/navigation-sidebar/navigation-menu-sidebar/index.js
@@ -15,14 +15,9 @@ export default function NavigationMenuSidebar() {
 	return (
 		<DefaultSidebar
 			className="edit-site-navigation-menu-sidebar"
-<<<<<<< HEAD:packages/edit-site/src/components/sidebar/navigation-menu-sidebar/index.js
 			identifier="edit-site/navigation-menu"
 			title={ __( 'Navigation Menus' ) }
-=======
 			scope="core/edit-global"
-			identifier="edit-global/navigation-menu"
-			title={ __( 'Navigation' ) }
->>>>>>> 0231f0b300 (Navigation Sidebar: experiment with a persistent vertical display):packages/edit-site/src/components/navigation-sidebar/navigation-menu-sidebar/index.js
 			icon={ navigation }
 			closeLabel={ __( 'Close navigation menu sidebar' ) }
 			panelClassName="edit-site-navigation-menu-sidebar__panel"
