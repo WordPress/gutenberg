16b89d66a0091213bfbc6fad5a0db66fa6589a62
Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
2022-03-30 16:46:30 +0300

    Plugin: Remove the experimental Progressive Web Apps (PWA) integration (#38810)
    * Revert PWA
    
    * Fix package lock
    
    * Fix rebase

--- lib/load.php
+++ lib/load.php
@@ -121,7 +121,6 @@ require __DIR__ . '/navigation-theme-opt-in.php';
 require __DIR__ . '/navigation-page.php';
 require __DIR__ . '/experiments-page.php';
 require __DIR__ . '/global-styles.php';
-require __DIR__ . '/pwa.php';
 
 // TODO: Move this to be loaded from the style engine package, via the build directory.
 // Part of the build process should be to copy the PHP file to the correct location,
