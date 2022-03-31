cf14af73db3320291dfa09b761b3c0b6153a3191
Glen Davies <glen.davies@a8c.com>
2022-03-31 17:19:37 +1300

    Add comment about why updateBlockAttributes is not used

--- packages/block-editor/src/components/convert-to-group-buttons/toolbar.js
+++ packages/block-editor/src/components/convert-to-group-buttons/toolbar.js
@@ -45,6 +45,8 @@ function BlockGroupToolbar() {
 
 		if ( newBlocks && newBlocks.length > 0 ) {
 			if ( variation !== 'group' ) {
+				// Because the block is not in the store yet we can't use
+				// updateBlockAttributes so need to manually update attributes.
 				newBlocks[ 0 ].attributes.layout =
 					variationAttributes[ variation ];
 			}
