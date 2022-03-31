b3ede88bf91e56bbfc39d785e92cb63409b9c9e1
Marco Ciampini <marco.ciampo@gmail.com>
2022-03-31 09:06:23 +0200

    Use alternative link syntax in the JSDoc comment

--- typings/testing-library/index.d.ts
+++ typings/testing-library/index.d.ts
@@ -3,12 +3,17 @@
 declare namespace jest {
 	interface Matchers< R > {
 		/**
-		 * Similar to the [`toMatchDiffSnapshot` matcher](https://github.com/jest-community/snapshot-diff), this custom matcher allows to snapshot only the difference
-		 * between the _styles_ associated to two different states of a component.
+		 * Similar to the [`toMatchDiffSnapshot` matcher], this custom matcher
+		 * allows to snapshot only the difference between the _styles_ associated
+		 * to two different states of a component.
 		 *
-		 * @see [test/unit/config/matchers/to-match-style-diff-snapshot.js](https://github.com/WordPress/gutenberg/blob/trunk/test/unit/config/matchers/to-match-style-diff-snapshot.js)
-		 * @see [Testing Overview docs](https://github.com/WordPress/gutenberg/blob/trunk/docs/contributors/code/testing-overview.md#best-practices)
+		 * @see [to-match-style-diff-snapshot.js]
+		 * @see [Testing Overview docs]
 		 * @cite https://github.com/testing-library/react-testing-library/issues/36#issuecomment-440442300
+		 *
+		 * [`toMatchDiffSnapshot` matcher]: https://github.com/jest-community/snapshot-diff
+		 * [to-match-style-diff-snapshot.js]: https://github.com/WordPress/gutenberg/blob/trunk/test/unit/config/matchers/to-match-style-diff-snapshot.js
+		 * [Testing Overview docs]: https://github.com/WordPress/gutenberg/blob/trunk/docs/contributors/code/testing-overview.md#best-practices
 		 */
 		toMatchStyleDiffSnapshot( expected: Element | null ): R;
 	}
