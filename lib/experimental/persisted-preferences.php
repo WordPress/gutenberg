3f68f83908348e9209ad449aab9135cd2f86135e
Daniel Richards <daniel.richards@automattic.com>
2022-03-31 10:36:36 +0800

    Use a filter to add the necessary script dependencies

--- lib/experimental/persisted-preferences.php
+++ lib/experimental/persisted-preferences.php
@@ -40,12 +40,40 @@ function gutenberg_configure_persisted_preferences() {
 	wp_add_inline_script(
 		'wp-database-persistence-layer',
 		sprintf(
-			'wp.databasePersistenceLayer.__experimentalConfigureDatabasePersistenceLayer( { preloadedData: %s } );',
+			'const { create } = wp.databasePersistenceLayer;
+			const persistenceLayer = create( { preloadedData: %s } );
+			const { store: preferencesStore } = wp.preferences;
+			wp.data.dispatch( "core/preferences" ).setPersistenceLayer( persistenceLayer );',
 			wp_json_encode( $preload_data )
 		),
 		'after'
 	);
+
 }
 
 add_action( 'init', 'gutenberg_configure_persisted_preferences' );
 
+/**
+ * Register dependencies for the inline script that configures the persistence layer.
+ *
+ * Note: When porting this to core add an extra case to the switch here:
+ * https://github.com/WordPress/wordpress-develop/blob/d2ab3d183740c3d1252cb921b18005495007e022/src/wp-includes/script-loader.php#L251-L258
+ *
+ * And the same to the gutenberg client assets file here:
+ * https://github.com/WordPress/gutenberg/blob/3f3c8df23c70a37b7ac4dddebc82030362133593/lib/client-assets.php#L242-L254
+ * ```
+ * case 'wp-database-persistence-layer':
+ *     array_push( $dependencies, 'wp-data', 'wp-preferences' );
+ *     break;
+ * ```
+ *
+ * @param WP_Scripts $scripts An instance of WP_Scripts.
+ */
+function gutenberg_update_database_persistence_layer_deps( $scripts ) {
+	$script = $scripts->query( 'wp-database-persistence-layer', 'registered' );
+	if ( isset( $script->deps ) ) {
+		array_push( $script->deps, 'wp-data', 'wp-preferences' );
+	}
+}
+
+add_action( 'wp_default_scripts', 'gutenberg_update_database_persistence_layer_deps', 11 );
--- packages/database-persistence-layer/src/index.js
+++ packages/database-persistence-layer/src/index.js
@@ -1,17 +1,1 @@
-/**
- * WordPress dependencies
- */
-import { dispatch } from '@wordpress/data';
-import { store as preferencesStore } from '@wordpress/preferences';
-
-/**
- * Internal dependencies
- */
-import create from './create';
-
-export { create };
-
-export function __experimentalConfigureDatabasePersistenceLayer( options ) {
-	const persistenceLayer = create( { options } );
-	dispatch( preferencesStore ).setPersistenceLayer( persistenceLayer );
-}
+export { default as create } from './create';
