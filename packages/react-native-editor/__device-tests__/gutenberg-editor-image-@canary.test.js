4da89852e88a37af4abfcdc5547e8b838cb05db2
jos <17252150+jostnes@users.noreply.github.com>
2022-03-31 12:51:00 +0800

    remove extra click for ios

--- packages/react-native-editor/__device-tests__/gutenberg-editor-image-@canary.test.js
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-image-@canary.test.js
@@ -43,8 +43,6 @@ describe( 'Gutenberg Editor Image Block tests', () => {
 		);
 		if ( isAndroid() ) {
 			await paragraphBlockElement.click();
-		} else {
-			await editorPage.clickParagraphBlockAtPosition( 2 );
 		}
 
 		await editorPage.typeTextToParagraphBlock(
