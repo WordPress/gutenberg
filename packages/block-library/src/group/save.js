c5bb0a9a1ce8ed6f58036b9f2b2a4d8a2bb55460
Justin Ahinon <justiny.ahinon@gmail.com>
2022-04-04 13:20:38 +0200

    Update save.js

--- packages/block-library/src/group/save.js
+++ packages/block-library/src/group/save.js
@@ -1,8 +1,25 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { useInnerBlocksProps, useBlockProps } from '@wordpress/block-editor';
 
-export default function save( { attributes: { tagName: Tag } } ) {
-	return <Tag { ...useInnerBlocksProps.save( useBlockProps.save() ) } />;
+export default function save( {
+	attributes: { tagName: Tag, verticalAlignment },
+} ) {
+	const className = classnames( {
+		[ `is-vertically-aligned-${ verticalAlignment }` ]: verticalAlignment,
+	} );
+
+	return (
+		<Tag
+			{ ...useInnerBlocksProps.save(
+				useBlockProps.save( { className } )
+			) }
+		/>
+	);
 }
