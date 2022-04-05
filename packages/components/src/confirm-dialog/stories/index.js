0f919388e5127dd6257b3b002c341816ee4e16ad
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-04-05 15:28:41 -0400

    ConfirmDialog Stories: add argTypes data

--- packages/components/src/confirm-dialog/stories/index.js
+++ packages/components/src/confirm-dialog/stories/index.js
@@ -18,6 +18,23 @@ import { ConfirmDialog } from '..';
 export default {
 	component: ConfirmDialog,
 	title: 'Components (Experimental)/ConfirmDialog',
+	argTypes: {
+		children: {
+			type: 'reactNode',
+		},
+		title: {
+			type: 'string',
+		},
+		confirmButtonText: {
+			type: 'string',
+		},
+		cancelButtonText: {
+			type: 'string',
+		},
+		isOpen: {
+			type: 'boolean',
+		},
+	},
 };
 
 const daText = () =>
@@ -27,9 +44,9 @@ const Template = ( args ) => {
 	const [ confirmVal, setConfirmVal ] = useState( "Hasn't confirmed yet" );
 	const confirmOutput = args.confirmOutput ?? 'Confirmed!';
 	const modalText = args.jsxMessage ? (
-		<Heading level={ 2 }>{ args.text }</Heading>
+		<Heading level={ 2 }>{ args.children }</Heading>
 	) : (
-		args.text
+		args.children
 	);
 
 	return (
@@ -56,20 +73,20 @@ const Template = ( args ) => {
 // Simplest usage: just declare the component with the required `onConfirm` prop.
 export const _default = Template.bind( {} );
 _default.args = {
-	text: daText(),
+	children: daText(),
 };
 
 // To add a title, pass the `title` prop
 export const WithTitle = Template.bind( {} );
 WithTitle.args = {
 	title: 'Example Title',
-	text: daText(),
+	children: daText(),
 };
 
 // To customize button text, pass the `cancelButtonText` and/or `confirmButtonText` props.
 export const withCustomButtonLabels = Template.bind( {} );
 withCustomButtonLabels.args = {
-	text: daText(),
+	children: daText(),
 	cancelButtonText: 'No thanks',
 	confirmButtonText: 'Yes please!',
 };
@@ -77,18 +94,22 @@ withCustomButtonLabels.args = {
 // To use a JSX message, wrap your text in the appropriate JSX tags.
 export const WithJSXMessage = Template.bind( {} );
 WithJSXMessage.args = {
-	text: daText(),
+	children: daText(),
 	jsxMessage: true,
+};
+// Hide the jsxMessage control, as it isn't an actual prop
+WithJSXMessage.parameters = {
+	controls: { exclude: [ 'jsxMessage' ] },
 };
 
 export const VeeeryLongMessage = Template.bind( {} );
 VeeeryLongMessage.args = {
-	text: daText().repeat( 20 ),
+	children: daText().repeat( 20 ),
 };
 
 export const UncontrolledAndWithExplicitOnCancel = Template.bind( {} );
 UncontrolledAndWithExplicitOnCancel.args = {
-	text: daText(),
+	children: daText(),
 	confirmOutput: 'Confirmed!',
 	cancelOutput: 'Cancelled',
 };
