dd671e9fb24c86b590f6281fb70a2ed4a5454712
Sunil Prajapati <sdprajapati999@gmail.com>
2022-04-05 17:55:38 +0530

    lint fix in edit js

--- packages/block-library/src/site-logo/edit.js
+++ packages/block-library/src/site-logo/edit.js
@@ -416,14 +416,12 @@ export default function LogoEdit( {
 		} );
 	};
 
-	const setIcon = ( newValue ) =>{
-		 
+	const setIcon = ( newValue ) => {
 		newValue = newValue === undefined ? null : newValue;
 		editEntityRecord( 'root', 'site', undefined, {
 			site_icon: newValue,
-		} ); 
-		
-	}
+		} );
+	};
 
 	let alt = null;
 	if ( mediaItemData ) {
