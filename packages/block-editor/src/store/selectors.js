234d3a1fedf2b32d050024b0a54ad3ddd5989bc6
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2022-04-05 14:21:33 -0500

    Revise getClientIdsOfDescendants description to be more accurate.

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -216,9 +216,9 @@ export const __unstableGetClientIdsTree = createSelector(
 );
 
 /**
- * Returns an array containing the clientIds of all descendants of the
- * blocks given. Ids are returned in the same order that they appear in
- * the editor.
+ * Returns an array containing the clientIds of all descendants of the blocks
+ * given. Returned ids are ordered first by the order of the ids given, then
+ * by the order that they appear in the editor.
  *
  * @param {Object} state     Global application state.
  * @param {Array}  clientIds Array of blocks to inspect.
