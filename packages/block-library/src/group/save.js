591bccad234547748443b9481fb90afba51d144f
Justin Ahinon <justiny.ahinon@gmail.com>
2022-04-05 08:27:07 +0200

    Revert "Update save.js"
    This reverts commit c5bb0a9a1ce8ed6f58036b9f2b2a4d8a2bb55460.

--- packages/block-library/src/group/save.js
+++ packages/block-library/src/group/save.js
@@ -1,25 +1,8 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { useInnerBlocksProps, useBlockProps } from '@wordpress/block-editor';
 
-export default function save( {
-	attributes: { tagName: Tag, verticalAlignment },
-} ) {
-	const className = classnames( {
-		[ `is-vertically-aligned-${ verticalAlignment }` ]: verticalAlignment,
-	} );
-
-	return (
-		<Tag
-			{ ...useInnerBlocksProps.save(
-				useBlockProps.save( { className } )
-			) }
-		/>
-	);
+export default function save( { attributes: { tagName: Tag } } ) {
+	return <Tag { ...useInnerBlocksProps.save( useBlockProps.save() ) } />;
 }
