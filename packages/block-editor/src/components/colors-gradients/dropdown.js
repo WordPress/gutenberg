3b1064939994ae4bf9e938445c570f21a5a80079
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-04-06 15:52:04 +1000

    Trial merging color dropdown components
    These changes update the ColorGradientSettingsDropdown to render differently depending upon whether it will be representing a fixed semantic `ItemGroup` or a collection of color controls within a `ToolsPanel` e.g. for the color block supports panel.

--- packages/block-editor/src/components/colors-gradients/dropdown.js
+++ packages/block-editor/src/components/colors-gradients/dropdown.js
@@ -7,12 +7,14 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import {
-	__experimentalItemGroup as ItemGroup,
-	__experimentalItem as Item,
-	__experimentalHStack as HStack,
-	FlexItem,
+	Button,
 	ColorIndicator,
 	Dropdown,
+	FlexItem,
+	__experimentalHStack as HStack,
+	__experimentalItem as Item,
+	__experimentalItemGroup as ItemGroup,
+	__experimentalToolsPanelItem as ToolsPanelItem,
 } from '@wordpress/components';
 
 /**
@@ -20,19 +22,9 @@ import {
  */
 import ColorGradientControl from './control';
 
-export default function ColorGradientSettingsDropdown( {
-	colors,
-	gradients,
-	disableCustomColors,
-	disableCustomGradients,
-	__experimentalHasMultipleOrigins,
-	__experimentalIsRenderedInSidebar,
-	enableAlpha,
-	settings,
-} ) {
-	let dropdownPosition;
-	if ( __experimentalIsRenderedInSidebar ) {
-		dropdownPosition = 'bottom left';
+const WithItemGroup = ( { isItemGroup, children } ) => {
+	if ( ! isItemGroup ) {
+		return children;
 	}
 
 	return (
@@ -41,56 +33,149 @@ export default function ColorGradientSettingsDropdown( {
 			isSeparated
 			className="block-editor-panel-color-gradient-settings__item-group"
 		>
-			{ settings.map(
-				( setting, index ) =>
-					setting && (
-						<Dropdown
-							key={ index }
-							position={ dropdownPosition }
-							className="block-editor-panel-color-gradient-settings__dropdown"
-							contentClassName="block-editor-panel-color-gradient-settings__dropdown-content"
-							renderToggle={ ( { isOpen, onToggle } ) => {
-								return (
-									<Item
-										onClick={ onToggle }
-										className={ classnames(
-											'block-editor-panel-color-gradient-settings__item',
-											{ 'is-open': isOpen }
-										) }
-									>
-										<HStack justify="flex-start">
-											<ColorIndicator
-												className="block-editor-panel-color-gradient-settings__color-indicator"
-												colorValue={
-													setting.gradientValue ??
-													setting.colorValue
-												}
-											/>
-											<FlexItem>
-												{ setting.label }
-											</FlexItem>
-										</HStack>
-									</Item>
-								);
-							} }
-							renderContent={ () => (
-								<ColorGradientControl
-									showTitle={ false }
-									{ ...{
-										colors,
-										gradients,
-										disableCustomColors,
-										disableCustomGradients,
-										__experimentalHasMultipleOrigins,
-										__experimentalIsRenderedInSidebar,
-										enableAlpha,
-										...setting,
-									} }
-								/>
-							) }
-						/>
-					)
-			) }
+			{ children }
 		</ItemGroup>
+	);
+};
+
+const WithToolsPanelItem = ( {
+	isItemGroup,
+	settings,
+	children,
+	...props
+} ) => {
+	if ( isItemGroup ) {
+		return children;
+	}
+
+	return (
+		<ToolsPanelItem
+			hasValue={ settings.hasValue }
+			label={ settings.label }
+			onDeselect={ settings.onDeselect }
+			isShownByDefault={ settings.isShownByDefault }
+			resetAllFilter={ settings.resetAllFilter }
+			{ ...props }
+			className="block-editor-tools-panel-color-gradient-settings__item"
+		>
+			{ children }
+		</ToolsPanelItem>
+	);
+};
+
+const renderToggleItem = ( { gradientValue, colorValue, label } ) => ( {
+	onToggle,
+	isOpen,
+} ) => {
+	return (
+		<Item
+			onClick={ onToggle }
+			className={ classnames(
+				'block-editor-panel-color-gradient-settings__item',
+				{ 'is-open': isOpen }
+			) }
+		>
+			<HStack justify="flex-start">
+				<ColorIndicator
+					className="block-editor-panel-color-gradient-settings__color-indicator"
+					colorValue={ gradientValue ?? colorValue }
+				/>
+				<FlexItem>{ label }</FlexItem>
+			</HStack>
+		</Item>
+	);
+};
+
+const renderToggleButton = ( { gradientValue, colorValue, label } ) => ( {
+	onToggle,
+	isOpen,
+} ) => {
+	return (
+		<Button
+			onClick={ onToggle }
+			aria-expanded={ isOpen }
+			className={ classnames(
+				'block-editor-panel-color-gradient-settings__dropdown',
+				{ 'is-open': isOpen }
+			) }
+		>
+			<HStack justify="flex-start">
+				<ColorIndicator
+					className="block-editor-panel-color-gradient-settings__color-indicator"
+					colorValue={ gradientValue ?? colorValue }
+				/>
+				<FlexItem>{ label }</FlexItem>
+			</HStack>
+		</Button>
+	);
+};
+
+export default function ColorGradientSettingsDropdown( {
+	colors,
+	disableCustomColors,
+	disableCustomGradients,
+	enableAlpha,
+	gradients,
+	isItemGroup = true,
+	settings,
+	__experimentalHasMultipleOrigins,
+	__experimentalIsRenderedInSidebar,
+	...props
+} ) {
+	const dropdownPosition = __experimentalIsRenderedInSidebar
+		? 'bottom left'
+		: undefined;
+
+	const dropdownClassName = isItemGroup
+		? 'block-editor-panel-color-gradient-settings__dropdown'
+		: 'block-editor-tools-panel-color-dropdown';
+
+	return (
+		<WithItemGroup isItemGroup={ isItemGroup }>
+			{ settings.map( ( setting, index ) => {
+				const controlProps = {
+					clearable: isItemGroup ? undefined : false,
+					colorValue: setting.colorValue,
+					colors,
+					disableCustomColors,
+					disableCustomGradients,
+					enableAlpha,
+					gradientValue: setting.gradientValue,
+					gradients,
+					label: setting.label,
+					onColorChange: setting.onColorChange,
+					onGradientChange: setting.onGradientChange,
+					showTitle: false,
+					__experimentalHasMultipleOrigins,
+					__experimentalIsRenderedInSidebar,
+					...setting,
+				};
+
+				return (
+					setting && (
+						<WithToolsPanelItem
+							key={ index }
+							isItemGroup={ isItemGroup }
+							settings={ setting }
+							{ ...props }
+						>
+							<Dropdown
+								position={ dropdownPosition }
+								className={ dropdownClassName }
+								contentClassName="block-editor-panel-color-gradient-settings__dropdown-content"
+								renderToggle={
+									isItemGroup
+										? renderToggleItem( setting )
+										: renderToggleButton( setting )
+								}
+								renderContent={ () => (
+									<ColorGradientControl { ...controlProps } />
+								) }
+							/>
+						</WithToolsPanelItem>
+					)
+				);
+			} ) }
+		</WithItemGroup>
 	);
 }
