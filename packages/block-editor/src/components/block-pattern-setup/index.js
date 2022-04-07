84d46f0ab843872447ca85677e99aa51ad50bedd
Jorge <jorge.costa@developer.pt>
2022-04-07 23:50:45 +0100

    Enhance design

--- packages/block-editor/src/components/block-pattern-setup/index.js
+++ packages/block-editor/src/components/block-pattern-setup/index.js
@@ -10,8 +10,8 @@ import {
 	__unstableCompositeItem as CompositeItem,
 } from '@wordpress/components';
 
-import { useState } from '@wordpress/element';
-import { useInstanceId } from '@wordpress/compose';
+import { useState, useCallback } from '@wordpress/element';
+import { useInstanceId, useResizeObserver } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -23,14 +23,43 @@ import SetupToolbar from './setup-toolbar';
 import usePatternsSetup from './use-patterns-setup';
 import { VIEWMODES } from './constants';
 
+function useBackgroundColor() {
+	const [ backgroundColor, setBackgroundColor ] = useState( 'rgba(0,0,0,0)' );
+	const ref = useCallback(
+		( node ) => {
+			if ( ! node ) {
+				return;
+			}
+			const { ownerDocument } = node;
+			const { defaultView } = ownerDocument;
+			const canvas = ownerDocument.querySelector(
+				'.editor-styles-wrapper'
+			);
+			if ( ! canvas ) {
+				return;
+			}
+			const computedBackgroundColor = defaultView
+				.getComputedStyle( canvas, null )
+				.getPropertyValue( 'background-color' );
+			if ( computedBackgroundColor !== backgroundColor ) {
+				setBackgroundColor( computedBackgroundColor );
+			}
+		},
+		[ backgroundColor, setBackgroundColor ]
+	);
+	return [ backgroundColor, ref ];
+}
+
 const SetupContent = ( {
 	viewMode,
 	activeSlide,
 	patterns,
 	onBlockPatternSelect,
+	height,
 } ) => {
 	const composite = useCompositeState();
 	const containerClass = 'block-editor-block-pattern-setup__container';
+	const [ backgroundColor, nodeRef ] = useBackgroundColor();
 	if ( viewMode === VIEWMODES.carousel ) {
 		const slideClass = new Map( [
 			[ activeSlide, 'active-slide' ],
@@ -38,16 +67,22 @@ const SetupContent = ( {
 			[ activeSlide + 1, 'next-slide' ],
 		] );
 		return (
-			<div className={ containerClass }>
-				<ul className="carousel-container">
-					{ patterns.map( ( pattern, index ) => (
-						<BlockPatternSlide
-							className={ slideClass.get( index ) || '' }
-							key={ pattern.name }
-							pattern={ pattern }
-						/>
-					) ) }
-				</ul>
+			<div
+				className="block-editor-block-pattern-setup__carousel"
+				style={ { backgroundColor, height } }
+				ref={ nodeRef }
+			>
+				<div className={ containerClass }>
+					<ul className="carousel-container">
+						{ patterns.map( ( pattern, index ) => (
+							<BlockPatternSlide
+								className={ slideClass.get( index ) || '' }
+								key={ pattern.name }
+								pattern={ pattern }
+							/>
+						) ) }
+					</ul>
+				</div>
 			</div>
 		);
 	}
@@ -138,6 +173,10 @@ const BlockPatternSetup = ( {
 	const [ showBlank, setShowBlank ] = useState( false );
 	const { replaceBlock } = useDispatch( blockEditorStore );
 	const patterns = usePatternsSetup( clientId, blockName, filterPatternsFn );
+	const [
+		contentResizeListener,
+		{ height: contentHeight },
+	] = useResizeObserver();
 
 	if ( ! patterns?.length || showBlank ) {
 		return startBlankComponent;
@@ -155,32 +194,38 @@ const BlockPatternSetup = ( {
 		  }
 		: undefined;
 	return (
-		<div
-			className={ `block-editor-block-pattern-setup view-mode-${ viewMode }` }
-		>
-			<SetupContent
-				viewMode={ viewMode }
-				activeSlide={ activeSlide }
-				patterns={ patterns }
-				onBlockPatternSelect={ onPatternSelectCallback }
-			/>
-			<SetupToolbar
-				viewMode={ viewMode }
-				setViewMode={ setViewMode }
-				activeSlide={ activeSlide }
-				totalSlides={ patterns.length }
-				handleNext={ () => {
-					setActiveSlide( ( active ) => active + 1 );
-				} }
-				handlePrevious={ () => {
-					setActiveSlide( ( active ) => active - 1 );
-				} }
-				onBlockPatternSelect={ () => {
-					onPatternSelectCallback( patterns[ activeSlide ].blocks );
-				} }
-				onStartBlank={ onStartBlank }
-			/>
-		</div>
+		<>
+			{ contentResizeListener }
+			<div
+				className={ `block-editor-block-pattern-setup view-mode-${ viewMode }` }
+			>
+				<SetupContent
+					viewMode={ viewMode }
+					activeSlide={ activeSlide }
+					patterns={ patterns }
+					onBlockPatternSelect={ onPatternSelectCallback }
+					height={ contentHeight - 2 * 60 }
+				/>
+				<SetupToolbar
+					viewMode={ viewMode }
+					setViewMode={ setViewMode }
+					activeSlide={ activeSlide }
+					totalSlides={ patterns.length }
+					handleNext={ () => {
+						setActiveSlide( ( active ) => active + 1 );
+					} }
+					handlePrevious={ () => {
+						setActiveSlide( ( active ) => active - 1 );
+					} }
+					onBlockPatternSelect={ () => {
+						onPatternSelectCallback(
+							patterns[ activeSlide ].blocks
+						);
+					} }
+					onStartBlank={ onStartBlank }
+				/>
+			</div>
+		</>
 	);
 };
 
