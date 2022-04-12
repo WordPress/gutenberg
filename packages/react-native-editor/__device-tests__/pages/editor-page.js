131dcce0e2443bf1d1b11b62fd8f4b2740bda75c
jos <17252150+jostnes@users.noreply.github.com>
2022-04-12 14:09:49 +0800

    set default switch values

--- packages/react-native-editor/__device-tests__/pages/editor-page.js
+++ packages/react-native-editor/__device-tests__/pages/editor-page.js
@@ -76,10 +76,10 @@ class EditorPage {
 		// Make it optional to use waitForVisible() so we can handle this test by test.
 		// This condition can be removed once we have gone through all test cases.
 		if ( options.useWaitForVisible ) {
-			let elementTypeAndroid = 'android.view.ViewGroup';
-			let elementTypeiOS = 'XCUIElementTypeOther';
-			let textViewElementNameAndroid = '';
-			let textViewElementNameiOS = '';
+			let elementTypeAndroid;
+			let elementTypeiOS;
+			let textViewElementNameAndroid;
+			let textViewElementNameiOS;
 			switch ( blockName ) {
 				case blockNames.cover:
 				case blockNames.image:
@@ -94,8 +94,13 @@ class EditorPage {
 					textViewElementNameAndroid = '/android.widget.EditText';
 					textViewElementNameiOS = '//XCUIElementTypeTextView';
 					break;
+				// for blocks that have not been covered
 				default:
-					throw new Error( `${ blockName } is not defined!` );
+					elementTypeAndroid = 'android.view.ViewGroup';
+					elementTypeiOS = 'XCUIElementTypeOther';
+					textViewElementNameAndroid = '';
+					textViewElementNameiOS = '';
+					break;
 			}
 			blockLocator = isAndroid()
 				? `//${ elementTypeAndroid }[contains(@${ this.accessibilityIdXPathAttrib }, "${ blockName } Block. Row ${ position }")]${ textViewElementNameAndroid }`
