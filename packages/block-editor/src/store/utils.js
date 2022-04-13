e8b0901b05ade8f8915837c58809fb833a5224c6
ntsekouras <ntsekouras@outlook.com>
2022-04-13 10:49:01 +0300

    add jsdoc for mapRichTextSettings util

--- packages/block-editor/src/store/utils.js
+++ packages/block-editor/src/store/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Helper function that maps attribute definition properties to the
+ * ones used by RichText utils like `create, toHTMLString, etc..`.
+ *
+ * @param {Object} attributeDefinition A block's attribute definition object.
+ * @return {Object} The mapped object.
+ */
 export function mapRichTextSettings( attributeDefinition ) {
 	const {
 		multiline: multilineTag,
