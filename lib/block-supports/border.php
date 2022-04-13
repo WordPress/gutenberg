ea485d5dfaea5d325e42251bc76f1599c5563261
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-04-13 14:23:03 +1000

    Make skip serialization check consistent

--- lib/block-supports/border.php
+++ lib/block-supports/border.php
@@ -186,9 +186,10 @@ function gutenberg_generate_individual_border_classes_and_styles( $side, $border
 
 	$border_color = _wp_array_get( $border, array( 'color' ), null );
 
-	$should_skip_color_serialization = gutenberg_should_skip_block_supports_serialization( $block_type, '__experimentalBorder', 'color' );
-
-	if ( $border_color && ! $should_skip_color_serialization ) {
+	if (
+		$border_color &&
+		! gutenberg_should_skip_block_supports_serialization( $block_type, '__experimentalBorder', 'color' )
+	) {
 		$has_color_preset = strpos( $border_color, 'var:preset|color|' ) !== false;
 		if ( $has_color_preset ) {
 			$named_color_slug = substr( $border_color, strrpos( $border_color, '|' ) + 1 );
