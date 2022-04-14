33d2e2c99d499c64e7be4e2245c2bb4faab9e4b2
Bernie Reiter <ockham@raz.or.at>
2022-04-14 15:49:20 +0200

    Add explanatory comment
    Co-authored-by: Michal <mmczaplinski@gmail.com>

--- packages/block-library/src/comment-template/index.php
+++ packages/block-library/src/comment-template/index.php
@@ -39,6 +39,8 @@ function block_core_comment_template_render_comments( $comments, $block ) {
 			$comment_depth -= 1;
 		}
 
+		// The `false` parameter at the end means that we do NOT want the function to `echo` the output but to return a string.
+		// See https://developer.wordpress.org/reference/functions/comment_class/#parameters
 		$comment_classes = comment_class( '', $comment->comment_ID, $comment->comment_post_ID, false );
 
 		$content .= sprintf( '<li %1$s>' . $block_content . '</li>', $comment_classes );
