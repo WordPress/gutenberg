d99e21bd372b676390403a23c8f0cbde55e7426b
Miguel Fonseca <miguelcsf@gmail.com>
2022-04-18 18:12:27 +0100

    inputFieldHasUncollapsedSelection: Rewrite comments, default to true

--- packages/dom/src/dom/input-field-has-uncollapsed-selection.js
+++ packages/dom/src/dom/input-field-has-uncollapsed-selection.js
@@ -5,54 +5,46 @@ import isTextField from './is-text-field';
 import isHTMLInputElement from './is-html-input-element';
 
 /**
- * Check whether the given element, assumed an input field or textarea,
- * contains a (uncollapsed) selection of text.
+ * Check whether the given input field or textarea contains a (uncollapsed)
+ * selection of text.
  *
- * Note: this is perhaps an abuse of the term "selection", since these elements
- * manage selection differently and aren't covered by Selection#collapsed.
+ * CAVEAT: Only specific text-based HTML inputs support the selection APIs
+ * needed to determine whether they have a collapsed or uncollapsed selection.
+ * This function defaults to returning `true` when the selection cannot be
+ * inspected, such as with `<input type="time">`. The rationale is that this
+ * should cause the block editor to defer to the browser's native selection
+ * handling (e.g. copying and pasting), thereby reducing friction for the user.
  *
- * See: https://developer.mozilla.org/en-US/docs/Web/API/Window/getSelection#Related_objects.
+ * See: https://html.spec.whatwg.org/multipage/input.html#do-not-apply
  *
  * @param {Element} element The HTML element.
  *
  * @return {boolean} Whether the input/textareaa element has some "selection".
  */
 export default function inputFieldHasUncollapsedSelection( element ) {
-	if ( ! isTextField( element ) && ! isHTMLInputElement( element ) ) {
+	if ( ! isHTMLInputElement( element ) && ! isTextField( element ) ) {
 		return false;
 	}
 
-	// Unfortunately, the HTML spec states that the `selectionStart` and
-	// `selectionEnd` attributes of `input` elements DO NOT APPLY to most
-	// non-text input types. This effectively means that we cannot inspect the
-	// selection state of numeric inputs, even email inputs, etc.
-	//
-	// The trade-off that this function makes is to assume that any such opaque
-	// element always has an uncollapsed selection. This should cause the block
-	// editor to defer to the browser's native selection handling (e.g. copying
-	// and pasting), thereby reducing friction for the user.
-	//
-	// See: https://html.spec.whatwg.org/multipage/input.html#do-not-apply
-	if ( ! isTextField( element ) ) {
-		return true;
-	}
-
+	// Safari throws a type error when trying to get `selectionStart` and
+	// `selectionEnd` on non-text <input> elements, so a try/catch construct is
+	// necessary.
 	try {
 		const {
 			selectionStart,
 			selectionEnd,
 		} = /** @type {HTMLInputElement | HTMLTextAreaElement} */ ( element );
-
-		return selectionStart !== null && selectionStart !== selectionEnd;
+		return (
+			// `null` means the input type doesn't implement selection, thus we
+			// cannot determine whether the selection is collapsed, so we
+			// default to true.
+			selectionStart === null ||
+			// when not null, compare the two points
+			selectionStart !== selectionEnd
+		);
 	} catch ( error ) {
-		// Safari throws an exception when trying to get `selectionStart`
-		// on non-text <input> elements (which, understandably, don't
-		// have the text selection API). We catch this via a try/catch
-		// block, as opposed to a more explicit check of the element's
-		// input types, because of Safari's non-standard behavior. This
-		// also means we don't have to worry about the list of input
-		// types that support `selectionStart` changing as the HTML spec
-		// evolves over time.
-		return false;
+		// This is Safari's way of saying that the input type doesn't implement
+		// selection, so we default to true.
+		return true;
 	}
 }
