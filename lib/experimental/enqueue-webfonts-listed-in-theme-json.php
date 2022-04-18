c5a5b2b5444afca833fe493b214ab980792eac0e
Ari Stathopoulos <aristath@gmail.com>
2022-04-18 10:42:58 +0300

    Add function_exists check for _wp_enqueue_webfonts_listed_in_theme_json

--- lib/experimental/enqueue-webfonts-listed-in-theme-json.php
+++ lib/experimental/enqueue-webfonts-listed-in-theme-json.php
@@ -16,47 +16,49 @@ function _gutenberg_is_externally_registered_webfont( $webfont ) {
 	return isset( $webfont['origin'] ) && 'gutenberg_wp_webfonts_api' === $webfont['origin'];
 }
 
-/**
- * Enqueue webfonts listed in theme.json.
- *
- * Enqueued webfonts will end up in the front-end as inlined CSS.
- */
-function _wp_enqueue_webfonts_listed_in_theme_json() {
-	$settings = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data()->get_settings();
+if ( ! function_exists( '_wp_enqueue_webfonts_listed_in_theme_json' ) ) {
+	/**
+	 * Enqueue webfonts listed in theme.json.
+	 *
+	 * Enqueued webfonts will end up in the front-end as inlined CSS.
+	 */
+	function _wp_enqueue_webfonts_listed_in_theme_json() {
+		$settings = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data()->get_settings();
 
-	// Bail out early if there are no settings for webfonts.
-	if ( empty( $settings['typography'] ) || empty( $settings['typography']['fontFamilies'] ) ) {
-		return;
-	}
+		// Bail out early if there are no settings for webfonts.
+		if ( empty( $settings['typography'] ) || empty( $settings['typography']['fontFamilies'] ) ) {
+			return;
+		}
 
-	// Look for fontFamilies.
-	foreach ( $settings['typography']['fontFamilies'] as $font_families ) {
-		foreach ( $font_families as $font_family ) {
-			// Skip dynamically included font families. We only want to enqueue explicitly added fonts.
-			if ( _gutenberg_is_externally_registered_webfont( $font_family ) ) {
-				continue;
-			}
-
-			// If no font faces defined.
-			if ( ! isset( $font_family['fontFaces'] ) ) {
-				// And the font family is registered.
-				if ( ! wp_webfonts()->is_font_family_registered( $font_family['fontFamily'] ) ) {
+		// Look for fontFamilies.
+		foreach ( $settings['typography']['fontFamilies'] as $font_families ) {
+			foreach ( $font_families as $font_family ) {
+				// Skip dynamically included font families. We only want to enqueue explicitly added fonts.
+				if ( _gutenberg_is_externally_registered_webfont( $font_family ) ) {
 					continue;
 				}
 
-				// Enqueue the entire family.
-				wp_webfonts()->enqueue_webfont( $font_family );
-				continue;
-			}
+				// If no font faces defined.
+				if ( ! isset( $font_family['fontFaces'] ) ) {
+					// And the font family is registered.
+					if ( ! wp_webfonts()->is_font_family_registered( $font_family['fontFamily'] ) ) {
+						continue;
+					}
 
-			// Loop through all the font faces, enqueueing each one of them.
-			foreach ( $font_family['fontFaces'] as $font_face ) {
-				// Skip dynamically included font faces. We only want to enqueue the font faces listed in theme.json.
-				if ( _gutenberg_is_externally_registered_webfont( $font_face ) ) {
+					// Enqueue the entire family.
+					wp_webfonts()->enqueue_webfont( $font_family );
 					continue;
 				}
 
-				wp_webfonts()->enqueue_webfont( $font_family, $font_face );
+				// Loop through all the font faces, enqueueing each one of them.
+				foreach ( $font_family['fontFaces'] as $font_face ) {
+					// Skip dynamically included font faces. We only want to enqueue the font faces listed in theme.json.
+					if ( _gutenberg_is_externally_registered_webfont( $font_face ) ) {
+						continue;
+					}
+
+					wp_webfonts()->enqueue_webfont( $font_family, $font_face );
+				}
 			}
 		}
 	}
