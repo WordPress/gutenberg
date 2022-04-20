8ce416c29e8328b0c20905e9f3595ccc750ce4e6
André <583546+oandregal@users.noreply.github.com>
2022-04-20 14:47:26 +0200

    e2e test: style variations
    - update paragraph representation
    - select quote block

--- test/e2e/specs/editor/various/style-variation.spec.js
+++ test/e2e/specs/editor/various/style-variation.spec.js
@@ -16,6 +16,9 @@ test.describe( 'adding blocks', () => {
 			attributes: { value: '<p>Quote content</p>' },
 		} );
 
+		// Select the quote block.
+		await page.keyboard.press( 'ArrowDown' );
+
 		await pageUtils.clickBlockToolbarButton( 'Quote' );
 
 		await page.click( 'role=menuitem[name="Plain"i]' );
@@ -24,7 +27,9 @@ test.describe( 'adding blocks', () => {
 		const content = await pageUtils.getEditedPostContent();
 		expect( content ).toBe(
 			`<!-- wp:quote {"className":"is-style-plain"} -->
-<blockquote class="wp-block-quote is-style-plain"><p>Quote content</p></blockquote>
+<blockquote class="wp-block-quote is-style-plain"><!-- wp:paragraph -->
+<p>Quote content</p>
+<!-- /wp:paragraph --></blockquote>
 <!-- /wp:quote -->`
 		);
 	} );
