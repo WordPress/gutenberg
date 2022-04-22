ac30b42881a4d3f9e78e880c359190910b6011ef
Adam Zielinski <adam@adamziel.com>
2022-04-22 15:31:34 +0200

    Update packages/core-data/src/selectors.ts

--- packages/core-data/src/selectors.ts
+++ packages/core-data/src/selectors.ts
@@ -201,7 +201,7 @@ export function getEntityConfig(
  * @param  key   Record's key
  * @param  query Optional query.
  *
- * @return any Record.
+ * @return Record.
  */
 export const getEntityRecord = createSelector(
 	(
