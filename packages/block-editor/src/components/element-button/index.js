5f546da29bb01e6022d8140375e19ff1a889bf3d
Ben Dwyer <ben@scruffian.com>
2022-04-22 14:19:58 +0100

    also output a button rather than richtext, when children are present

--- packages/block-editor/src/components/element-button/index.js
+++ packages/block-editor/src/components/element-button/index.js
@@ -28,5 +28,10 @@ export default class ElementButton extends Component {
 
 ElementButton.Content = ( props ) => {
 	const newProps = getElementProps( props );
+	// If children are set then we only need a button, not RichText.
+	if ( newProps.children ) {
+		return <button { ...newProps } />;
+	}
+
 	return <RichText.Content { ...newProps } />;
 };
