8b6d03c27db2a5d62dd5a68fc66722c4100a7931
Kai Hao <kai@kaihao.dev>
2022-04-24 00:45:11 +0800

    Gracefully handle errors when test results is missing

--- .github/report-flaky-tests/index.js
+++ .github/report-flaky-tests/index.js
@@ -88,35 +88,37 @@ const metaData = {
 				}
 			}
 
-			let testResultsList = body
-				.slice(
-					body.indexOf( TEST_RESULTS_LIST.open ) +
-						TEST_RESULTS_LIST.open.length,
-					body.indexOf( TEST_RESULTS_LIST.close )
-				)
-				/**
-				 * Split the text from:
-				 * ```
-				 * <!-- __TEST_RESULT__ --> Test result 1 <!-- /__TEST_RESULT__ -->
-				 * ...
-				 * <!-- __TEST_RESULT__ --> Test result 2 <!-- /__TEST_RESULT__ -->
-				 * <!-- __TEST_RESULT__ --> Test result 3 <!-- /__TEST_RESULT__ -->
-				 * ```
-				 *
-				 * into:
-				 * ```
-				 * [
-				 *   '<!-- __TEST_RESULT__ --> Test result 1 <!-- /__TEST_RESULT__ -->',
-				 *   '<!-- __TEST_RESULT__ --> Test result 2 <!-- /__TEST_RESULT__ -->',
-				 *   '<!-- __TEST_RESULT__ --> Test result 3 <!-- /__TEST_RESULT__ -->',
-				 * ]
-				 * ```
-				 */
-				.split(
-					new RegExp(
-						`(?<=${ TEST_RESULT.close })\n+(?:\.\.\.\n+)?(?=${ TEST_RESULT.open })`
-					)
-				);
+			let testResultsList = body.includes( TEST_RESULTS_LIST.open )
+				? body
+						.slice(
+							body.indexOf( TEST_RESULTS_LIST.open ) +
+								TEST_RESULTS_LIST.open.length,
+							body.indexOf( TEST_RESULTS_LIST.close )
+						)
+						/**
+						 * Split the text from:
+						 * ```
+						 * <!-- __TEST_RESULT__ --> Test result 1 <!-- /__TEST_RESULT__ -->
+						 * ...
+						 * <!-- __TEST_RESULT__ --> Test result 2 <!-- /__TEST_RESULT__ -->
+						 * <!-- __TEST_RESULT__ --> Test result 3 <!-- /__TEST_RESULT__ -->
+						 * ```
+						 *
+						 * into:
+						 * ```
+						 * [
+						 *   '<!-- __TEST_RESULT__ --> Test result 1 <!-- /__TEST_RESULT__ -->',
+						 *   '<!-- __TEST_RESULT__ --> Test result 2 <!-- /__TEST_RESULT__ -->',
+						 *   '<!-- __TEST_RESULT__ --> Test result 3 <!-- /__TEST_RESULT__ -->',
+						 * ]
+						 * ```
+						 */
+						.split(
+							new RegExp(
+								`(?<=${ TEST_RESULT.close })\n+(?:\.\.\.\n+)?(?=${ TEST_RESULT.open })`
+							)
+						)
+				: [];
 			// GitHub issues has character limits on issue's body,
 			// so we only preserve the first and the 9 latest results.
 			if ( testResultsList.length > 10 ) {
