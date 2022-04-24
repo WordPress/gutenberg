27b625fa071dd4920fa492228f180f0a006a3ad4
Jonny Harris <jon@spacedmonkey.co.uk>
2022-04-25 00:51:49 +0100

    Ensure that post thumbnail is cached in post template block.

--- packages/block-library/src/post-template/index.php
+++ packages/block-library/src/post-template/index.php
@@ -39,6 +39,7 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 	if ( ! $query->have_posts() ) {
 		return '';
 	}
+	update_post_thumbnail_cache( $query );
 
 	$classnames = '';
 	if ( isset( $block->context['displayLayout'] ) && isset( $block->context['query'] ) ) {
