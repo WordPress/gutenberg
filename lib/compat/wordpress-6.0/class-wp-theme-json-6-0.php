c7ecdc15f4bde35cc1015405ced774dd1e84493a
ramonjd <ramonjd@gmail.com>
2022-04-28 06:42:03 +1000

    Remove dupe settings

--- lib/compat/wordpress-6.0/class-wp-theme-json-6-0.php
+++ lib/compat/wordpress-6.0/class-wp-theme-json-6-0.php
@@ -290,59 +290,6 @@ class WP_Theme_JSON_6_0 extends WP_Theme_JSON_5_9 {
 	);
 
 	/**
-	 * The valid properties under the settings key.
-	 *
-	 * @var array
-	 */
-	const VALID_SETTINGS = array(
-		'appearanceTools' => null,
-		'border'          => array(
-			'color'  => null,
-			'radius' => null,
-			'style'  => null,
-			'width'  => null,
-		),
-		'color'           => array(
-			'background'       => null,
-			'custom'           => null,
-			'customDuotone'    => null,
-			'customGradient'   => null,
-			'defaultDuotone'   => null,
-			'defaultGradients' => null,
-			'defaultPalette'   => null,
-			'duotone'          => null,
-			'gradients'        => null,
-			'link'             => null,
-			'palette'          => null,
-			'text'             => null,
-		),
-		'custom'          => null,
-		'layout'          => array(
-			'contentSize' => null,
-			'wideSize'    => null,
-		),
-		'spacing'         => array(
-			'blockGap' => null,
-			'margin'   => null,
-			'padding'  => null,
-			'units'    => null,
-		),
-		'typography'      => array(
-			'customFontSize' => null,
-			'dropCap'        => null,
-			'fluid'          => null,
-			'fontFamilies'   => null,
-			'fontSizes'      => null,
-			'fontStyle'      => null,
-			'fontWeight'     => null,
-			'letterSpacing'  => null,
-			'lineHeight'     => null,
-			'textDecoration' => null,
-			'textTransform'  => null,
-		),
-	);
-
-	/**
 	 * Returns the current theme's wanted patterns(slugs) to be
 	 * registered from Pattern Directory.
 	 *
