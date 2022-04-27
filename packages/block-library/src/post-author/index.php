68d97f81bfb960eaeeef49858689010d17e81196
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-04-27 13:58:32 +0300

    inherit autheor from global query

--- packages/block-library/src/post-author/index.php
+++ packages/block-library/src/post-author/index.php
@@ -15,10 +15,16 @@
  */
 function render_block_core_post_author( $attributes, $content, $block ) {
 	if ( ! isset( $block->context['postId'] ) ) {
-		return '';
+		global $wp_query;
+		if ( $wp_query && isset( $wp_query->query_vars ) && is_array( $wp_query->query_vars ) ) {
+			$query_args = wp_parse_args( $wp_query->query_vars, $query_args );
+
+			$author_id = $query_args[ 'author' ];
+		}
+	} else {
+		$author_id = get_post_field( 'post_author', $block->context['postId'] );
 	}
 
-	$author_id = get_post_field( 'post_author', $block->context['postId'] );
 	if ( empty( $author_id ) ) {
 		return '';
 	}
