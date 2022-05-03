58965eacbc79ad2cbc2742b630cafa62768e8402
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-05-03 14:53:41 +0300

    remove useless merge with block query args since there are none

--- packages/block-library/src/post-author/index.php
+++ packages/block-library/src/post-author/index.php
@@ -17,9 +17,7 @@ function render_block_core_post_author( $attributes, $content, $block ) {
 	if ( ! isset( $block->context['postId'] ) ) {
 		global $wp_query;
 		if ( $wp_query && isset( $wp_query->query_vars ) && is_array( $wp_query->query_vars ) ) {
-			$query_args = wp_parse_args( $wp_query->query_vars, $query_args );
-
-			$author_id = $query_args[ 'author' ];
+			$author_id = $wp_query->query_vars[ 'author' ];
 		}
 	} else {
 		$author_id = get_post_field( 'post_author', $block->context['postId'] );
