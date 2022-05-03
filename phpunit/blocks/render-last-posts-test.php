229b79dcf835243ee042d42c59f62f33064cfbff
Jonny Harris <jon@spacedmonkey.co.uk>
2022-05-03 11:01:50 +0100

    Improve test.

--- phpunit/blocks/render-last-posts-test.php
+++ phpunit/blocks/render-last-posts-test.php
@@ -13,11 +13,20 @@
  */
 class Tests_Blocks_RenderLastPosts extends WP_UnitTestCase {
 	protected static $post;
+	protected static $sticky_post;
 	protected static $attachment_id;
 
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$sticky_post = $factory->post->create_and_get(
+			array(
+				'post_title' => 'Sticky post',
+				'post_date'  => '2008-09-03 00:00:00',
+			)
+		);
+		$factory->post->create_many( 2 );
 		self::$post = $factory->post->create_and_get();
-		$factory->post->create();
+
+		stick_post( self::$sticky_post->ID );
 
 		$file                = DIR_TESTDATA . '/images/canola.jpg';
 		self::$attachment_id = $factory->attachment->create_upload_object( $file, self::$post->ID );
@@ -45,7 +54,7 @@ class Tests_Blocks_RenderLastPosts extends WP_UnitTestCase {
 		render_block_core_latest_posts( $attributes );
 		$args      = $a->get_args();
 		$last_args = end( $args );
-		$this->assertContains( self::$attachment_id, $last_args );
+		$this->assertContains( self::$attachment_id, $last_args[1] );
 	}
 
 	/**
@@ -65,5 +74,6 @@ class Tests_Blocks_RenderLastPosts extends WP_UnitTestCase {
 		$args      = $a->get_args();
 		$last_args = end( $args );
 		$this->assertContains( self::$post->ID, $last_args );
+		$this->assertNotContains( self::$sticky_post->ID, $last_args[1] );
 	}
 }
