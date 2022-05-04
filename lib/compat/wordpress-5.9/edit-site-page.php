7768d5bb0e1fb0c7c5b3f25aa75eeedb0f95869a
Jarda Snajdr <jsnajdr@gmail.com>
2022-05-04 13:30:20 +0200

    Fix formatting

--- lib/compat/wordpress-5.9/edit-site-page.php
+++ lib/compat/wordpress-5.9/edit-site-page.php
@@ -114,12 +114,12 @@ function gutenberg_edit_site_init( $hook ) {
 	}
 
 	$custom_settings = array(
-		'siteUrl'                  => site_url(),
-		'postsPerPage'             => get_option( 'posts_per_page' ),
-		'styles'                   => gutenberg_get_editor_styles(),
-		'defaultTemplateTypes'     => $indexed_template_types,
-		'defaultTemplatePartAreas' => get_allowed_block_template_part_areas(),
-		'__unstableHomeTemplate'   => gutenberg_resolve_home_template(),
+		'siteUrl'                              => site_url(),
+		'postsPerPage'                         => get_option( 'posts_per_page' ),
+		'styles'                               => gutenberg_get_editor_styles(),
+		'defaultTemplateTypes'                 => $indexed_template_types,
+		'defaultTemplatePartAreas'             => get_allowed_block_template_part_areas(),
+		'__unstableHomeTemplate'               => gutenberg_resolve_home_template(),
 		'__experimentalBlockPatterns'          => WP_Block_Patterns_Registry::get_instance()->get_all_registered(),
 		'__experimentalBlockPatternCategories' => WP_Block_Pattern_Categories_Registry::get_instance()->get_all_registered(),
 	);
