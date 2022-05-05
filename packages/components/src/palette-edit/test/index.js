a0e0919d3468839be0ed208bd14510e78b0ae99b
ramonjd <ramonjd@gmail.com>
2022-05-05 19:08:49 +1000

    Extra tests

--- packages/components/src/palette-edit/test/index.js
+++ packages/components/src/palette-edit/test/index.js
@@ -26,6 +26,19 @@ describe( 'getNameForPosition', () => {
 		);
 	} );
 
+	test( 'should ignore user-defined color names', () => {
+		const slugPrefix = 'test-';
+		const elements = [
+			{
+				slug: 'a-sweet-color-2',
+			},
+		];
+
+		expect( getNameForPosition( elements, slugPrefix ) ).toEqual(
+			'Color 1'
+		);
+	} );
+
 	test( 'should return a new color name with an incremented slug id one higher than the current highest', () => {
 		const slugPrefix = 'test-';
 		const elements = [
@@ -36,12 +49,15 @@ describe( 'getNameForPosition', () => {
 				slug: 'test-color-2',
 			},
 			{
-				slug: 'test-color-15',
+				slug: 'test-color-150',
+			},
+			{
+				slug: 'a-sweet-color-100',
 			},
 		];
 
 		expect( getNameForPosition( elements, slugPrefix ) ).toEqual(
-			'Color 16'
+			'Color 151'
 		);
 	} );
 } );
