a59fdbecb2bbbdb614e24651e629cc0f23b2d466
Pooja Killekar <poojakillekar@Poojas-MacBook-Air.local>
2022-05-09 16:18:43 +0530

    Format the code

--- test/e2e/specs/editor/blocks/html.spec.js
+++ test/e2e/specs/editor/blocks/html.spec.js
@@ -1,34 +1,34 @@
 /**
  * WordPress dependencies
  */
-const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+ const { test, expect } = require("@wordpress/e2e-test-utils-playwright");
 
-test.describe( 'HTML block', () => {
-	test.beforeEach( async ({pageUtils}) => {
-		await pageUtils.createNewPost();
-	} );
-
-	test( 'can be created by typing "/html"', async ({page,pageUtils}) => {
-		// Create a Custom HTML block with the slash shortcut.
-		await pageUtils.clickBlockAppender();
-		await page.keyboard.type( '/html' );
-		await page.locator(
-			`//*[contains(@class, "components-autocomplete__result") and contains(@class, "is-selected") and contains(text(), 'Custom HTML')]`
-		);
-		await page.keyboard.press( 'Enter' );
-		await page.keyboard.type( '<p>Pythagorean theorem: ' );
-		await page.keyboard.press( 'Enter' );
-		await page.keyboard.type(
-			'<var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup> </p>'
-		);
-
-		// Check the content
-        const content = await pageUtils.getEditedPostContent();
-        expect( content ).toBe(
-            `<!-- wp:html -->
-<p>Pythagorean theorem: 
-<var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup> </p>
-<!-- /wp:html -->`
-        );
-	} );
-} );
+ test.describe("HTML block", () => {
+   test.beforeEach(async ({ pageUtils }) => {
+	 await pageUtils.createNewPost();
+   });
+ 
+   test('can be created by typing "/html"', async ({ page, pageUtils }) => {
+	 // Create a Custom HTML block with the slash shortcut.
+	 await page.click('role=button[name="Add default block"i]');
+	 await page.keyboard.type("/html");
+	 await page.locator(
+	   `//*[contains(@class, "components-autocomplete__result") and contains(@class, "is-selected") and contains(text(), 'Custom HTML')]`
+	 );
+	 await page.keyboard.press("Enter");
+	 await page.keyboard.type("<p>Pythagorean theorem: ");
+	 await page.keyboard.press("Enter");
+	 await page.keyboard.type(
+	   "<var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup> </p>"
+	 );
+ 
+	 // Check the content
+	 const content = await pageUtils.getEditedPostContent();
+	 expect(content).toBe(
+	   `<!-- wp:html -->
+  <p>Pythagorean theorem: 
+  <var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup> </p>
+  <!-- /wp:html -->`
+	 );
+   });
+ }); 
