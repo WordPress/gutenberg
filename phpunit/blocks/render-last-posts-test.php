ac3da4b0312671f21537945e3d698fdabd20cdf6
Jonny Harris <spacedmonkey@users.noreply.github.com>
2022-05-09 22:17:35 +0100

    Update phpunit/blocks/render-last-posts-test.php
    Co-authored-by: Anton Vlasenko <43744263+anton-vlasenko@users.noreply.github.com>

--- phpunit/blocks/render-last-posts-test.php
+++ phpunit/blocks/render-last-posts-test.php
@@ -65,7 +65,7 @@ class Tests_Blocks_RenderLastPosts extends WP_UnitTestCase {
 		gutenberg_render_block_core_latest_posts( $attributes );
 		$args      = $a->get_args();
 		$last_args = end( $args );
-		$this->assertSameSets( self::$attachment_ids, $last_args[1] );
+		$this->assertSameSets( self::$attachment_ids, $last_args[1], 'Attachment IDs must match' );
 	}
 
 	/**
