4412dcc78a741dbad5901c8a0eb15b64dd3c673d
Bernie Reiter <ockham@raz.or.at>
2022-05-10 17:51:40 +0200

    Don't try to infer author for now

--- packages/block-library/src/post-comments-form/index.php
+++ packages/block-library/src/post-comments-form/index.php
@@ -93,11 +93,11 @@ function add_post_comments_form_onsubmit_handler() {
 
 				const form = document.querySelector( '.comment-form' );
 
-				const author = document.querySelector( '#author' ).value;
+				//const author = document.querySelector( '#author' ).value;
 				const content = document.querySelector( '#comment' ).value;
 
 				const context = {
-					author,
+					//author,
 					content,
 					timestamp: date
 				};
