8501d2f2f070a4325119e723655495a25793f338
Riad Benguella <benguella@gmail.com>
2022-05-10 09:46:08 +0100

    Restore tooltip position

--- packages/components/src/tooltip/index.js
+++ packages/components/src/tooltip/index.js
@@ -64,6 +64,7 @@ const addPopoverToGrandchildren = ( {
 				className="components-tooltip"
 				aria-hidden="true"
 				animate={ false }
+				offset={ 12 }
 			>
 				{ text }
 				<Shortcut
@@ -92,7 +93,13 @@ const emitToChild = ( children, eventName, event ) => {
 };
 
 function Tooltip( props ) {
-	const { children, position, text, shortcut, delay = TOOLTIP_DELAY } = props;
+	const {
+		children,
+		position = 'bottom middle',
+		text,
+		shortcut,
+		delay = TOOLTIP_DELAY,
+	} = props;
 	/**
 	 * Whether a mouse is currently pressed, used in determining whether
 	 * to handle a focus event as displaying the tooltip immediately.
