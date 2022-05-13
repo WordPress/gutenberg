443e57f4c3d2f10c59d78bf412b536119bd4bdec
Daniel Richards <daniel.richards@automattic.com>
2022-05-13 11:46:41 +0800

    Convert to TS

--- packages/e2e-test-utils-playwright/src/selectors/block-selector-engine.ts
+++ packages/e2e-test-utils-playwright/src/selectors/block-selector-engine.ts
@@ -1,8 +1,15 @@
+type BlockSelectorParams = {
+	clientId?: string;
+	name?: string;
+	tag?: string;
+	title?: string;
+};
+
 export default function createBlockSelectorEngine() {
 	// Util functions need to be defined in the `createBlockSelectorEngine`
 	// body. Playwright converts this function to a string, and won't be able
 	// to reach functions that are defined outside the body.
-	function parsePlaywrightSelector( selector ) {
+	function parsePlaywrightSelector( selector: string ): BlockSelectorParams {
 		if ( ! selector ) {
 			return {};
 		}
@@ -14,14 +21,19 @@ export default function createBlockSelectorEngine() {
 		const [ , tag ] = selector.match( /tag="(.+?)"/ ) ?? [];
 
 		return {
-			name: name !== '*' ? name : undefined,
-			title,
 			clientId,
+			name: name !== '*' ? name : undefined,
 			tag,
+			title,
 		};
 	}
 
-	function assembleDOMSelector( { clientId, name, tag, title } ) {
+	function assembleDOMSelector( {
+		clientId,
+		name,
+		tag,
+		title,
+	}: BlockSelectorParams ) {
 		// Always include a `data-block` attribute selector even if there's no
 		// clientId to select. This ensures the selector only returns blocks.
 		const clientIdPart = clientId
@@ -40,14 +52,14 @@ export default function createBlockSelectorEngine() {
 
 	return {
 		// Returns the first element matching given selector in the root's subtree.
-		query( root, selector ) {
+		query( root: Element, selector: string ) {
 			const selectorParams = parsePlaywrightSelector( selector );
 			const DOMSelector = assembleDOMSelector( selectorParams );
 			return root.querySelector( DOMSelector );
 		},
 
 		// Returns all elements matching given selector in the root's subtree.
-		queryAll( root, selector ) {
+		queryAll( root: Element, selector: string ) {
 			const selectorParams = parsePlaywrightSelector( selector );
 			const DOMSelector = assembleDOMSelector( selectorParams );
 			return Array.from( root.querySelectorAll( DOMSelector ) );
