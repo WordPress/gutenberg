ad5b643f18b34ad994dd470a3de73941e8f5b52d
Glen Davies <glen.davies@a8c.com>
2022-05-17 14:45:20 +1200

    Update env to fall back to main instead of master

--- packages/env/lib/config/test/config.js
+++ packages/env/lib/config/test/config.js
@@ -395,7 +395,7 @@ describe( 'readConfig', () => {
 					{
 						type: 'git',
 						url: 'https://github.com/WordPress/gutenberg.git',
-						ref: 'master',
+						ref: 'trunk',
 						path: expect.stringMatching( /^(\/||\\).*gutenberg$/ ),
 						basename: 'gutenberg',
 					},
