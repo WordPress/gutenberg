e165e2cef69c4ea49fd030b9704b1d4d8ddbbd49
Jorge <jorge.costa@developer.pt>
2022-05-17 12:35:25 +0100

    List block v2: Fixes an issue where pressing enter deletes innerblocks.

--- packages/block-library/src/list-item/hooks/index.js
+++ packages/block-library/src/list-item/hooks/index.js
@@ -3,3 +3,4 @@ export { default as useIndentListItem } from './use-indent-list-item';
 export { default as useEnter } from './use-enter';
 export { default as useBackspace } from './use-backspace';
 export { default as useSpace } from './use-space';
+export { default as useSplit } from './use-split';
