5db9b75cbe199428a075388b9daaee2b75155673
Justin Ahinon <justiny.ahinon@gmail.com>
2022-05-18 13:12:37 +0100

    Update preformatted.spec.js

--- test/e2e/specs/editor/blocks/preformatted.spec.js
+++ test/e2e/specs/editor/blocks/preformatted.spec.js
@@ -34,7 +34,7 @@ test.describe( 'Preformatted', () => {
 		await page.keyboard.press( 'Enter' );
 		await page.keyboard.type( '2' );
 		await page.keyboard.press( 'Enter' );
-		await page.click( 'role=button[name="Add default block"i]' );
+		await editor.insertBlock( { name: 'core/paragraph' } );
 		await page.keyboard.type( '3' );
 		await page.keyboard.press( 'ArrowLeft' );
 		await page.keyboard.press( 'Backspace' );
