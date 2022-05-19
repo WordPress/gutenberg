31f8d9d9faa191a56c182e35836a641a4e6246fd
Justin Ahinon <justiny.ahinon@gmail.com>
2022-05-19 09:07:41 +0100

    Migrate group block tests to Playwright

---
+++ test/e2e/specs/editor/blocks/group.spec.js
@@ -0,0 +1,47 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Group', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
+	} );
+
+	test( 'can be created using the block inserter', async ( {
+		editor,
+		page,
+	} ) => {
+		await editor.searchForBlock( 'Group' );
+		await page.locator( '.editor-block-list-item-group' ).click();
+
+		expect( await editor.getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	test( 'can be created using the slash inserter', async ( {
+		editor,
+		page,
+	} ) => {
+		await page.locator( 'role=button[name="Add default block"i]' ).click();
+		await page.keyboard.type( '/group' );
+		await page.waitForSelector(
+			`//*[contains(@class, "components-autocomplete__result") and contains(@class, "is-selected") and contains(text(), 'Group')]`
+		);
+		await page.keyboard.press( 'Enter' );
+
+		expect( await editor.getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	test( 'can have other blocks appended to it using the button appender', async ( {
+		editor,
+		page,
+	} ) => {
+		await editor.searchForBlock( 'Group' );
+		await page.locator( '.editor-block-list-item-group' ).click();
+		await page.locator( '.block-editor-button-block-appender' ).click();
+		await page.locator( '.editor-block-list-item-paragraph' ).click();
+		await page.keyboard.type( 'Group Block with a Paragraph' );
+
+		expect( await editor.getEditedPostContent() ).toMatchSnapshot();
+	} );
+} );
