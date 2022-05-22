b790e137f2798d3238d55d64ed064d39c067e8d9
Hiroshi Urabe <mail@torounit.com>
2022-05-22 18:17:41 +0900

    refactor story

---
+++ packages/components/src/form-token-field/stories/index.tsx
@@ -0,0 +1,122 @@
+/**
+ * External dependencies
+ */
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import FormTokenField from '../';
+import type { TokenItem } from '../types';
+
+const meta: ComponentMeta< typeof FormTokenField > = {
+	component: FormTokenField,
+	title: 'Components/FormTokenField',
+	argTypes: {
+		onChange: {
+			action: 'onChange',
+			control: { type: null },
+		},
+		value: {
+			control: { type: null },
+		},
+		suggestions: {
+			control: { type: null },
+		},
+		__experimentalValidateInput: {
+			control: { type: 'boolean' },
+		},
+	},
+	parameters: {
+		controls: {
+			expanded: true,
+		},
+		docs: { source: { state: 'open' } },
+	},
+};
+export default meta;
+
+const continents = [
+	'Africa',
+	'America',
+	'Antarctica',
+	'Asia',
+	'Europe',
+	'Oceania',
+];
+
+const DefaultTemplate: ComponentStory< typeof FormTokenField > = ( {
+	__experimentalValidateInput,
+	...args
+} ) => {
+	const [ selectedContinents, setSelectedContinents ] = useState<
+		( string | TokenItem )[]
+	>( [] );
+
+	return (
+		<FormTokenField
+			{ ...args }
+			value={ selectedContinents }
+			suggestions={ continents }
+			onChange={ ( tokens ) => setSelectedContinents( tokens ) }
+			__experimentalValidateInput={
+				__experimentalValidateInput
+					? ( newValue ) => continents.includes( newValue )
+					: () => true
+			}
+		/>
+	);
+};
+
+export const Default: ComponentStory<
+	typeof FormTokenField
+> = DefaultTemplate.bind( {} );
+Default.args = {
+	label: 'Type a continent',
+};
+
+export const Async: ComponentStory< typeof FormTokenField > = ( {
+	__experimentalValidateInput,
+	...args
+} ) => {
+	const [ selectedContinents, setSelectedContinents ] = useState<
+		( string | TokenItem )[]
+	>( [] );
+	const [ availableContinents, setAvailableContinents ] = useState<
+		string[]
+	>( [] );
+
+	const searchContinents = ( input: string ) => {
+		const timeout = setTimeout( () => {
+			const available = continents.filter( ( continent ) =>
+				continent.toLowerCase().includes( input.toLowerCase() )
+			);
+			setAvailableContinents( available );
+		}, 1000 );
+
+		return () => clearTimeout( timeout );
+	};
+
+	return (
+		<FormTokenField
+			{ ...args }
+			value={ selectedContinents }
+			suggestions={ availableContinents }
+			onChange={ ( tokens ) => setSelectedContinents( tokens ) }
+			onInputChange={ searchContinents }
+			__experimentalValidateInput={
+				__experimentalValidateInput
+					? ( newValue ) => continents.includes( newValue )
+					: () => true
+			}
+		/>
+	);
+};
+Async.args = {
+	label: 'Type a continent',
+};
