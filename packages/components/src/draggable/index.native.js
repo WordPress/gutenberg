8e6b615ffa951a60e6a3fa79c9cfb146ef3f8691
Carlos Garcia <fluiddot@gmail.com>
2022-05-23 20:50:25 +0200

    Set draggingId shared value before enable dragging
    This change is required for testing, otherwise the dragging id is not passed when the dragging gesture begins.

--- packages/components/src/draggable/index.native.js
+++ packages/components/src/draggable/index.native.js
@@ -190,8 +190,8 @@ const DraggableTrigger = ( {
 				return;
 			}
 
-			isDragging.value = true;
 			draggingId.value = id;
+			isDragging.value = true;
 			if ( onLongPress ) {
 				runOnJS( onLongPress )( id );
 			}
