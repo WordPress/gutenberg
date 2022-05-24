8b15bf4d774ad76be17c280873607580714619b4
Kai Hao <kevin830726@gmail.com>
2022-05-24 16:40:14 +0800

    Apply suggestions from code review

--- test/e2e/specs/editor/blocks/html.spec.js
+++ test/e2e/specs/editor/blocks/html.spec.js
@@ -2,10 +2,12 @@
  * WordPress dependencies
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
 test.describe( 'HTML block', () => {
 	test.beforeEach( async ( { admin } ) => {
 		await admin.createNewPost();
 	} );
+
 	test( 'can be created by typing "/html"', async ( { editor, page } ) => {
 		// Create a Custom HTML block with the slash shortcut.
 		await page.click( 'role=button[name="Add default block"i]' );
@@ -19,7 +21,7 @@ test.describe( 'HTML block', () => {
 		await page.keyboard.type(
 			'<var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup> </p>'
 		);
-		// Check the content
+		// Check the content.
 		const content = await editor.getEditedPostContent();
 		expect( content ).toBe(
 			`<!-- wp:html -->
