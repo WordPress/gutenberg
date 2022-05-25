a0e0e0fb8245c4c25b337af62913cb05e9071708
Kai Hao <kevin830726@gmail.com>
2022-05-25 15:57:34 +0800

    Use page.click()

--- test/e2e/specs/editor/blocks/separator.spec.js
+++ test/e2e/specs/editor/blocks/separator.spec.js
@@ -12,7 +12,7 @@ test.describe( 'Separator', () => {
 		editor,
 		page,
 	} ) => {
-		await page.locator( 'role=button[name="Add default block"i]' ).click();
+		await page.click( 'role=button[name="Add default block"i]' );
 		await page.keyboard.type( '---' );
 		await page.keyboard.press( 'Enter' );
 
