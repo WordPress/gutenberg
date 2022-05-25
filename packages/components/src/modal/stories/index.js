b7aa9d23607b4b54840fa1ad1d7cb5e7af9d1bf4
Marco Ciampini <marco.ciampo@gmail.com>
2022-05-25 16:55:49 +0200

    Apply suggestions from code review
    Remove quotes from storybook example

--- packages/components/src/modal/stories/index.js
+++ packages/components/src/modal/stories/index.js
@@ -41,7 +41,7 @@ const ModalExample = ( props ) => {
 					style={ { maxWidth: '600px' } }
 				>
 					<p>
-						"Lorem ipsum dolor sit amet, consectetur adipiscing
+						Lorem ipsum dolor sit amet, consectetur adipiscing
 						elit, sed do eiusmod tempor incididunt ut labore et
 						dolore magna aliqua. Ut enim ad minim veniam, quis
 						nostrud exercitation ullamco laboris nisi ut aliquip ex
@@ -49,7 +49,7 @@ const ModalExample = ( props ) => {
 						reprehenderit in voluptate velit esse cillum dolore eu
 						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum."
+						anim id est laborum.
 					</p>
 
 					<Button variant="secondary" onClick={ closeModal }>
