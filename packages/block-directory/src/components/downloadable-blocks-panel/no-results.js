a971fd531f8fa5f9cfefa566d5bba56b0e562af7
mburridge <mburridge@zyriab.co.uk>
2022-05-26 16:28:59 +0100

    Expands text and wraps it in a block-editor-inserter__tips class

--- packages/block-directory/src/components/downloadable-blocks-panel/no-results.js
+++ packages/block-directory/src/components/downloadable-blocks-panel/no-results.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Icon, blockDefault } from '@wordpress/icons';
+import { Icon, blockDefault, tip } from '@wordpress/icons';
 
 function DownloadableBlocksNoResults() {
 	return (
@@ -12,17 +12,34 @@ function DownloadableBlocksNoResults() {
 				icon={ blockDefault }
 			/>
 			<p>{ __( 'No results found.' ) }</p>
-			<p>{ __( 'Interested in creating your own block?' ) }</p>
-			<p>
-				<a
-					href="https://developer.wordpress.org/block-editor/"
-					rel="noreferrer noopener"
-					target="_blank"
-				>
-					{ __( 'Get started here' ) }
-				</a>
-				.
-			</p>
+
+			<div className="block-editor-inserter__tips">
+				<div className="components-tip">
+					<Icon icon={ tip } />
+					<h3>{ __( 'Make your own' ) }</h3>
+				</div>
+				<p>
+					It seems that no block exists with the functionality that
+					you need.
+				</p>
+				<p>
+					WordPress is open-source software. You could create a block
+					with the functionality you want and contribute it to the
+					community.
+				</p>
+				<p>
+					<strong>
+						<a
+							href="https://developer.wordpress.org/block-editor/"
+							rel="noreferrer noopener"
+							target="_blank"
+						>
+							{ __( 'Learn more' ) }
+						</a>
+						.
+					</strong>
+				</p>
+			</div>
 		</div>
 	);
 }
