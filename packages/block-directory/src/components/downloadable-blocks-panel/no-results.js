5cb11e210bbd8bd432194bbba9abba519a114c11
mburridge <mburridge@zyriab.co.uk>
2022-05-27 18:04:58 +0100

    Improves appearance by using existing components - Tip and ExternalLink

--- packages/block-directory/src/components/downloadable-blocks-panel/no-results.js
+++ packages/block-directory/src/components/downloadable-blocks-panel/no-results.js
@@ -2,45 +2,32 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Icon, blockDefault, tip } from '@wordpress/icons';
+import { Icon, blockDefault } from '@wordpress/icons';
+import { Tip, ExternalLink } from '@wordpress/components';
 
 function DownloadableBlocksNoResults() {
 	return (
-		<div className="block-editor-inserter__no-results">
-			<Icon
-				className="block-editor-inserter__no-results-icon"
-				icon={ blockDefault }
-			/>
-			<p>{ __( 'No results found.' ) }</p>
-
-			<div className="block-editor-inserter__tips">
-				<div className="components-tip">
-					<Icon icon={ tip } />
-					<h3>{ __( 'Make your own' ) }</h3>
-				</div>
-				<p>
-					It seems that no block exists with the functionality that
-					you need.
-				</p>
-				<p>
-					WordPress is open-source software. You could create a block
-					with the functionality you want and contribute it to the
-					community.
-				</p>
-				<p>
-					<strong>
-						<a
-							href="https://developer.wordpress.org/block-editor/"
-							rel="noreferrer noopener"
-							target="_blank"
-						>
-							{ __( 'Learn more' ) }
-						</a>
-						.
-					</strong>
-				</p>
+		<>
+			<div className="block-editor-inserter__no-results">
+				<Icon
+					className="block-editor-inserter__no-results-icon"
+					icon={ blockDefault }
+				/>
+				<p>{ __( 'No results found.' ) }</p>
 			</div>
-		</div>
+			<div className="block-editor-inserter__tips">
+				<Tip>
+					<div>
+						<p>
+							{ __( 'Interested in creating your own block?' ) }
+						</p>
+						<ExternalLink href="https://developer.wordpress.org/block-editor/">
+							{ __( 'Get started here' ) }.
+						</ExternalLink>
+					</div>
+				</Tip>
+			</div>
+		</>
 	);
 }
 
