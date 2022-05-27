88c3f1194bcd7d2ac266897feabed50b7cf57f57
Marco Ciampini <marco.ciampo@gmail.com>
2022-05-27 12:12:40 +0200

    fix lint errors

--- packages/components/src/modal/stories/index.js
+++ packages/components/src/modal/stories/index.js
@@ -41,11 +41,11 @@ const ModalExample = ( props ) => {
 					style={ { maxWidth: '600px' } }
 				>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing
-						elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis
-						nostrud exercitation ullamco laboris nisi ut aliquip ex
-						ea commodo consequat. Duis aute irure dolor in
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et magna
+						aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea ea
+						commodo consequat. Duis aute irure dolor in
 						reprehenderit in voluptate velit esse cillum dolore eu
 						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 						non proident, sunt in culpa qui officia deserunt mollit
