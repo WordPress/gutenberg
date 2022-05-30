11bd870396d8da111ad4c1c9f7dea20a5d24b341
Glen Davies <glen.davies@a8c.com>
2022-05-31 09:40:31 +1200

    Move the scriptloader method to the 6.1 compat folder

--- lib/compat/wordpress-5.9/script-loader.php
+++ lib/compat/wordpress-5.9/script-loader.php
@@ -46,31 +46,3 @@ function gutenberg_enqueue_global_styles_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'gutenberg_enqueue_global_styles_assets' );
 add_action( 'wp_footer', 'gutenberg_enqueue_global_styles_assets' );
-
-/**
- * This function takes care of adding inline styles
- * in the proper place, depending on the theme in use.
- *
- * For block themes, it's loaded in the head.
- * For classic ones, it's loaded in the body
- * because the wp_head action  happens before
- * the render_block.
- *
- * @link https://core.trac.wordpress.org/ticket/53494.
- *
- * @param string $style String containing the CSS styles to be added.
- * @param int    $priority To set the priority for the add_action.
- */
-function gutenberg_enqueue_block_support_styles( $style, $priority = 10 ) {
-	$action_hook_name = 'wp_footer';
-	if ( wp_is_block_theme() ) {
-		$action_hook_name = 'wp_head';
-	}
-	add_action(
-		$action_hook_name,
-		static function () use ( $style ) {
-			echo "<style>$style</style>\n";
-		},
-		$priority
-	);
-}
