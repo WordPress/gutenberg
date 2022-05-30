66400ad75bec39d1df6986a771de8f5d0c480cd8
Paulo Cruz <paulo.cruz@automattic.com>
2022-05-30 14:09:48 -0300

    Update Pocket Casts embed block variation
    * Update the block description
    
    * Add audio keywork to the block
    
    * Change the Pocket Casts icon to use the brand color

--- packages/block-library/src/embed/icons.js
+++ packages/block-library/src/embed/icons.js
@@ -154,24 +154,27 @@ export const embedWolframIcon = (
 	</SVG>
 );
 
-export const embedPocketCastsIcon = (
-	<SVG
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<Path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M24,12A12,12,0,1,1,12,0,12,12,0,0,1,24,12Z"
-		/>
-		<Path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M2.67,12a9.33,9.33,0,0,1,18.66,0H19a7,7,0,1,0-7,7v2.33A9.33,9.33,0,0,1,2.67,12ZM12,17.6A5.6,5.6,0,1,1,17.6,12h-2A3.56,3.56,0,1,0,12,15.56Z"
-			fill="#fff"
-		/>
-	</SVG>
-);
+export const embedPocketCastsIcon = {
+	foreground: '#f43e37',
+	src: (
+		<SVG
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<Path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M24,12A12,12,0,1,1,12,0,12,12,0,0,1,24,12Z"
+			/>
+			<Path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M2.67,12a9.33,9.33,0,0,1,18.66,0H19a7,7,0,1,0-7,7v2.33A9.33,9.33,0,0,1,2.67,12ZM12,17.6A5.6,5.6,0,1,1,17.6,12h-2A3.56,3.56,0,1,0,12,15.56Z"
+				fill="#fff"
+			/>
+		</SVG>
+	),
+};
