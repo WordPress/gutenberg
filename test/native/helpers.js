29a170bda117096fb2a91fcb470ef5a1f38c1268
Carlos Garcia <fluiddot@gmail.com>
2022-05-31 15:56:41 +0200

    [RNMobile] Add integration tests to cover Drag & Drop functionality (#41364)
    * Add testID prop to Draggable components
    
    * Add unit tests for Draggable component
    
    * Set draggingId shared value before enable dragging
    
    This change is required for testing, otherwise the dragging id is not passed when the dragging gesture begins.
    
    * Mock generateHapticFeedback function
    
    * Add testID to draggable chip component
    
    * Add testID to BlockDraggable component
    
    * Add test helpers for BlockDraggable component
    
    Additionally, helpers related to fake timers have been added and updated in the global helpers file.
    
    * Add drag and drop integration tests
    
    * Update react-native-aztec mock to use AztecInputState

--- test/native/helpers.js
+++ test/native/helpers.js
@@ -31,6 +31,95 @@ provideToNativeHtml.mockImplementation( ( html ) => {
 	serializedHtml = html;
 } );
 
+const frameTime = 1000 / 60;
+
+/**
+ * Set up fake timers for executing a function and restores them afterwards.
+ *
+ * @param {Function} fn Function to trigger.
+ *
+ * @return {*} The result of the function call.
+ */
+export async function withFakeTimers( fn ) {
+	const usingFakeTimers = jest.isMockFunction( setTimeout );
+
+	// Portions of the React Native Animation API rely upon these APIs. However,
+	// Jest's 'legacy' fake timers mutate these globals, which breaks the Animated
+	// API. We preserve the original implementations to restore them later.
+	const requestAnimationFrameCopy = global.requestAnimationFrame;
+	const cancelAnimationFrameCopy = global.cancelAnimationFrame;
+
+	if ( ! usingFakeTimers ) {
+		jest.useFakeTimers( 'legacy' );
+	}
+
+	const result = await fn();
+
+	if ( ! usingFakeTimers ) {
+		jest.useRealTimers();
+
+		global.requestAnimationFrame = requestAnimationFrameCopy;
+		global.cancelAnimationFrame = cancelAnimationFrameCopy;
+	}
+	return result;
+}
+
+/**
+ * Prepare timers for executing a function that uses the Reanimated APIs.
+ *
+ * NOTE: This code is based on a similar function provided by the Reanimated library.
+ * Reference: https://github.com/software-mansion/react-native-reanimated/blob/b4ee4ea9a1f246c461dd1819c6f3d48440a25756/src/reanimated2/jestUtils.ts#L170-L174
+ *
+ * @param {Function} fn Function to trigger.
+ *
+ * @return {*} The result of the function call.
+ */
+export async function withReanimatedTimer( fn ) {
+	return withFakeTimers( async () => {
+		global.requestAnimationFrame = ( callback ) =>
+			setTimeout( callback, frameTime );
+
+		const result = await fn();
+
+		// As part of the clean up, we run all pending timers that might have been derived from animations.
+		act( () => jest.runOnlyPendingTimers() );
+
+		return result;
+	} );
+}
+
+/**
+ * Advance Reanimated animations by time.
+ * This helper should be called within a function invoked by "withReanimatedTimer".
+ *
+ * NOTE: This code is based on a similar function provided by the Reanimated library.
+ * Reference: https://github.com/software-mansion/react-native-reanimated/blob/b4ee4ea9a1f246c461dd1819c6f3d48440a25756/src/reanimated2/jestUtils.ts#L176-L181
+ *
+ * @param {number} time Time to advance timers.
+ */
+export const advanceAnimationByTime = ( time = frameTime ) => {
+	for ( let i = 0; i <= Math.ceil( time / frameTime ); i++ ) {
+		jest.advanceTimersByTime( frameTime );
+	}
+	jest.advanceTimersByTime( frameTime );
+};
+
+/**
+ * Advance Reanimated animations by frames.
+ * This helper should be called within a function invoked by "withReanimatedTimer".
+ *
+ * NOTE: This code is based on a similar function provided by the Reanimated library.
+ * Reference: https://github.com/software-mansion/react-native-reanimated/blob/b4ee4ea9a1f246c461dd1819c6f3d48440a25756/src/reanimated2/jestUtils.ts#L183-L188
+ *
+ * @param {number} count Number of frames to advance timers.
+ */
+export const advanceAnimationByFrame = ( count ) => {
+	for ( let i = 0; i <= count; i++ ) {
+		jest.advanceTimersByTime( frameTime );
+	}
+	jest.advanceTimersByTime( frameTime );
+};
+
 /**
  * Executes a function that triggers store resolvers and waits for them to be finished.
  *
@@ -39,40 +128,26 @@ provideToNativeHtml.mockImplementation( ( html ) => {
  * triggered by updates to the React tree, we manually tick fake timers and
  * await the resolution of the current block execution before proceeding.
  *
- * @param {Function} fn Function that triggers store resolvers.
+ * @param {Function} fn Function that to trigger.
+ *
  * @return {*} The result of the function call.
  */
 export async function waitForStoreResolvers( fn ) {
-	// Portions of the React Native Animation API rely upon these APIs. However,
-	// Jest's 'legacy' fake timers mutate these globals, which breaks the Animated
-	// API. We preserve the original implementations to restore them later.
-	const originalRAF = global.requestAnimationFrame;
-	const originalCAF = global.cancelAnimationFrame;
+	return withFakeTimers( async () => {
+		const result = fn();
 
-	jest.useFakeTimers( 'legacy' );
+		// Advance all timers allowing store resolvers to resolve.
+		act( () => jest.runAllTimers() );
 
-	const result = fn();
+		// The store resolvers perform several API fetches during editor
+		// initialization. The most straightforward approach to ensure all of them
+		// resolve before we consider the editor initialized is to flush micro tasks,
+		// similar to the approach found in `@testing-library/react-native`.
+		// https://github.com/callstack/react-native-testing-library/blob/a010ffdbca906615279ecc3abee423525e528101/src/flushMicroTasks.js#L15-L23.
+		await act( async () => {} );
 
-	// Advance all timers allowing store resolvers to resolve.
-	act( () => jest.runAllTimers() );
-
-	// The store resolvers perform several API fetches during editor
-	// initialization. The most straightforward approach to ensure all of them
-	// resolve before we consider the editor initialized is to flush micro tasks,
-	// similar to the approach found in `@testing-library/react-native`.
-	// https://github.com/callstack/react-native-testing-library/blob/a010ffdbca906615279ecc3abee423525e528101/src/flushMicroTasks.js#L15-L23.
-	await act( async () => {} );
-
-	// Restore the default timer APIs for remainder of test arrangement, act, and
-	// assertion.
-	jest.useRealTimers();
-
-	// Restore the global animation frame APIs to their original state for the
-	// React Native Animated API.
-	global.requestAnimationFrame = originalRAF;
-	global.cancelAnimationFrame = originalCAF;
-
-	return result;
+		return result;
+	} );
 }
 
 /**
@@ -95,7 +170,10 @@ export async function initializeEditor( props, { component } = {} ) {
 			: internalInitializeEditor( uniqueId, postType, postId );
 
 		const screen = render(
-			cloneElement( editorElement, { initialTitle: 'test', ...props } )
+			cloneElement( editorElement, {
+				initialTitle: 'test',
+				...props,
+			} )
 		);
 
 		// A layout event must be explicitly dispatched in BlockList component,
