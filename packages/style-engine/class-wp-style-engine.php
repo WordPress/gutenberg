c33a76565117b062bc70459fde96930c2fbfc596
ramonjd <ramonjd@gmail.com>
2022-06-03 11:48:19 +1000

    Updating comments Fixing typos Renaming variables for clarity Props @andrewserong

--- packages/style-engine/class-wp-style-engine.php
+++ packages/style-engine/class-wp-style-engine.php
@@ -35,8 +35,10 @@ class WP_Style_Engine {
 	 * parse/output valid Gutenberg styles from a block's attributes.
 	 * For every style definition, the follow properties are valid:
 	 *  - classnames   => an array of classnames to be returned for block styles. The key is a classname or pattern.
-	 *                    A value of `true` means the classname should be applied always. Otherwise a valid CSS property
-	 *                    to match the incoming value, e.g., "color" to match var:preset|color|somePresetName.
+	 *                    A value of `true` means the classname should be applied always. Otherwise, a valid CSS property (string)
+	 *                    to match the incoming value, e.g., "color" to match var:preset|color|somePresetSlug.
+	 *  - css_vars     => an array of key value pairs used to generate CSS var values. The key is a CSS var pattern, whose `$slug` fragment will be replaced with a preset slug.
+	 *                    The value should be a valid CSS property (string) to match the incoming value, e.g., "color" to match var:preset|color|somePresetSlug.
 	 *  - property_key => the key that represents a valid CSS property, e.g., "margin" or "border".
 	 *  - path         => a path that accesses the corresponding style value in the block style object.
 	 */
@@ -189,7 +191,7 @@ class WP_Style_Engine {
 	 *
 	 * @param array         $style_value      A single raw style value from the generate() $block_styles array.
 	 * @param array<string> $style_definition A single style definition from BLOCK_STYLE_DEFINITIONS_METADATA.
-	 * @param boolean       $should_return_css_vars Whether to try build and return CSS var values.
+	 * @param boolean       $should_return_css_vars Whether to try to build and return CSS var values.
 	 *
 	 * @return array        An array of CSS rules.
 	 */
@@ -200,13 +202,12 @@ class WP_Style_Engine {
 			return $rules;
 		}
 
-		// Before default processing, style definitions could define a callable `value_func` to generate custom CSS rules at this point.
 		$style_property = $style_definition['property_key'];
 
 		// Build CSS var values from var:? values, e.g, `var(--wp--css--rule-slug )`
 		// Check if the value is a CSS preset and there's a corresponding css_var pattern in the style definition.
 		if ( is_string( $style_value ) && strpos( $style_value, 'var:' ) !== false ) {
-			if ( $should_return_css_vars && $style_definition['css_vars'] ) {
+			if ( $should_return_css_vars && ! empty( $style_definition['css_vars'] ) ) {
 				foreach ( $style_definition['css_vars'] as $css_var_pattern => $property_key ) {
 					$slug = static::get_slug_from_preset_value( $style_value, $property_key );
 					if ( $slug ) {
@@ -222,7 +223,7 @@ class WP_Style_Engine {
 		}
 
 		// Default rule builder.
-		// If the input contains an array, ee assume box model-like properties
+		// If the input contains an array, assume box model-like properties
 		// for styles such as margins and padding.
 		if ( is_array( $style_value ) ) {
 			foreach ( $style_value as $key => $value ) {
@@ -241,8 +242,8 @@ class WP_Style_Engine {
 	 *
 	 * @param array $block_styles An array of styles from a block's attributes.
 	 * @param array $options array(
-	 *     'selector'                     => (string) When a selector is passed, `generate()` will return a full CSS rule `$selector { ...rules }`, otherwise a concatenated string of properties and values.
-	 *     'css_vars'                     => (boolean) Whether to covert CSS values to var() values. If `true` the style engine will try to parse var:? values and output var( --wp--preset--* ) rules. Default is `false`.
+	 *     'selector' => (string) When a selector is passed, `generate()` will return a full CSS rule `$selector { ...rules }`, otherwise a concatenated string of properties and values.
+	 *     'css_vars' => (boolean) Whether to covert CSS values to var() values. If `true` the style engine will try to parse var:? values and output var( --wp--preset--* ) rules. Default is `false`.
 	 * );.
 	 *
 	 * @return array|null array(
@@ -255,9 +256,9 @@ class WP_Style_Engine {
 			return null;
 		}
 
-		$css_rules                    = array();
-		$classnames                   = array();
-		$should_css_vars_from_presets = isset( $options['css_vars'] ) && true === $options['css_vars'];
+		$css_rules              = array();
+		$classnames             = array();
+		$should_return_css_vars = isset( $options['css_vars'] ) && true === $options['css_vars'];
 
 		// Collect CSS and classnames.
 		foreach ( self::BLOCK_STYLE_DEFINITIONS_METADATA as $definition_group ) {
@@ -273,7 +274,7 @@ class WP_Style_Engine {
 				}
 
 				$classnames = array_merge( $classnames, static::get_classnames( $style_value, $style_definition ) );
-				$css_rules  = array_merge( $css_rules, static::get_css( $style_value, $style_definition, $should_css_vars_from_presets ) );
+				$css_rules  = array_merge( $css_rules, static::get_css( $style_value, $style_definition, $should_return_css_vars ) );
 			}
 		}
 
