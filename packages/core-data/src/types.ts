7cda21c98bab505ca7d281abce3cff833bd116e8
Adam Zieliński <adam@adamziel.com>
2022-06-05 15:36:00 +0100

    Rebase on top of trunk

--- packages/core-data/src/types.ts
+++ packages/core-data/src/types.ts
@@ -1,32 +1,33 @@
-export interface AnyFunction {
-	( ...args: any[] ): any;
-}
-
-/**
- * Internal dependencies
- */
-import {
-	DataStores,
-	ReduxStoreConfig,
-	DataRegistry,
-} from '../../data/build-types/types';
-// import { DataStores, ReduxStoreConfig, DataRegistry } from '@wordpress/data';
-
-import * as actions from './actions';
-import * as selectors from './selectors';
-
-type CoreDataState = {};
-declare module '../../data/build-types/types' {
-	interface Stores extends DataStores {
-		'core-data': ReduxStoreConfig<
-			CoreDataState,
-			typeof actions,
-			typeof selectors
-		>;
-	}
-}
-
-const x = {} as DataRegistry;
-x.select( 'core-data' ).getEntityRecords( 'a', 'b', {} );
-
+// export interface AnyFunction {
+// 	( ...args: any[] ): any;
+// }
+//
+// /**
+//  * Internal dependencies
+//  */
+// import {
+// 	DataStores,
+// 	ReduxStoreConfig,
+// 	DataRegistry,
+// } from '@wordpress/data';
+// // } from '../../data/build-types/types';
+// // import { DataStores, ReduxStoreConfig, DataRegistry } from '@wordpress/data';
+//
+// import * as actions from './actions';
+// import * as selectors from './selectors';
+//
+// type CoreDataState = {};
+// declare module '../../data/build-types/types' {
+// 	interface Stores extends DataStores {
+// 		'core-data': ReduxStoreConfig<
+// 			CoreDataState,
+// 			typeof actions,
+// 			typeof selectors
+// 		>;
+// 	}
+// }
+//
+// const x = {} as DataRegistry;
+// x.select( 'core-data' ).getEntityRecords( 'a', 'b', {} );
+//
 export {};
