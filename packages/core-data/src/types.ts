0db578ac314e7180e7656bf9b20d5ca6337a776b
Adam Zieliński <adam@adamziel.com>
2022-06-05 19:47:10 +0200

    Add core-data store types to DataRegistry using module augmentation

--- packages/core-data/src/types.ts
+++ packages/core-data/src/types.ts
@@ -1,11 +1,29 @@
 /**
  * WordPress dependencies
  */
-// import { DataRegistry } from '@wordpress/data';
+import { DataRegistry, DataStores, ReduxStoreConfig } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import * as actions from './actions';
+import * as selectors from './selectors';
+
+type CoreDataState = {};
+declare module '@wordpress/data' {
+	interface Stores extends DataStores {
+		'core-data': ReduxStoreConfig<
+			CoreDataState,
+			typeof actions,
+			typeof selectors
+		>;
+	}
+}
+
 //
-// const x = {} as DataRegistry;
-// x.select( 'core-data' ).getCurrentUser();
-// // x.select( 'core-data' ).getEntityRecord('');
+const x = {} as DataRegistry;
+x.select( 'core-data' ).getCurrentUser();
+x.select( 'core-data' ).getEntityRecord( 'root', 'comment', 15 );
 
 export {};
 
