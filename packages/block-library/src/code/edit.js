bd877469710630029ceee7191af52fc1660a6218
Lena Morita <lena@jaguchi.com>
2022-06-06 17:38:58 +0900

    Modify Code block for demonstration

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -7,17 +7,21 @@ import { RichText, useBlockProps } from '@wordpress/block-editor';
 export default function CodeEdit( { attributes, setAttributes, onRemove } ) {
 	const blockProps = useBlockProps();
 	return (
-		<pre { ...blockProps }>
-			<RichText
-				tagName="code"
-				value={ attributes.content }
-				onChange={ ( content ) => setAttributes( { content } ) }
-				onRemove={ onRemove }
-				placeholder={ __( 'Write code…' ) }
-				aria-label={ __( 'Code' ) }
-				preserveWhiteSpace
-				__unstablePastePlainText
-			/>
-		</pre>
+		<div { ...blockProps } style={ { display: 'flex' } }>
+			<input type="checkbox" />
+			<input type="radio" />
+			<pre>
+				<RichText
+					tagName="code"
+					value={ attributes.content }
+					onChange={ ( content ) => setAttributes( { content } ) }
+					onRemove={ onRemove }
+					placeholder={ __( 'Write code…' ) }
+					aria-label={ __( 'Code' ) }
+					preserveWhiteSpace
+					__unstablePastePlainText
+				/>
+			</pre>
+		</div>
 	);
 }
