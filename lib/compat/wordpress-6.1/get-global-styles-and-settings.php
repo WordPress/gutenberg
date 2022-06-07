e8ba9743a8cb043cd07a6a5715c92ac1b624ded8
Ben Dwyer <ben@scruffian.com>
2022-06-07 12:57:12 +0100

    lint fix

--- lib/compat/wordpress-6.1/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.1/get-global-styles-and-settings.php
@@ -9,7 +9,7 @@
  * Adds global style rules to the inline style for each block.
  */
 function wp_add_global_styles_for_blocks() {
-	$tree = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
+	$tree        = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
 	$block_nodes = $tree->get_styles_block_nodes();
 	foreach ( $block_nodes as $metadata ) {
 		$block_css = $tree->get_styles_for_block( $metadata );
