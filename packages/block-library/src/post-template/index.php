16dd2b8bf47dcb09f1e4397e148d6a512eab6b8d
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-06-07 12:58:24 +0300

    unser all query block defaults which are not editable when inherit is true

--- packages/block-library/src/post-template/index.php
+++ packages/block-library/src/post-template/index.php
@@ -51,17 +51,31 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 	if ( $use_global_query ) {
 		global $wp_query;
 		if ( $wp_query && isset( $wp_query->query_vars ) && is_array( $wp_query->query_vars ) ) {
-			// Unset `offset` because if is set, $wp_query overrides/ignores the paged parameter and breaks pagination.
-			unset( $query_args['offset'] );
+			// Unset all the query block query args that cannot be set by the user
+			// when inherit is set to on.
+			$block_provided_query_args = array_keys( $query_args );
+			$user_visible_query_args = [
+				'ignore_sticky_posts', // Ignore sticky posts
+				'post__in', // Only sticky posts
+				'post__not_in' // Exclude sticky posts
+			];
+			foreach( $block_provided_query_args as $i => $query_args_key ) {
+				if( ! in_array( $query_args_key, $user_visible_query_args ) ) {
+					unset( $query_args[ $query_args_key ] );
+				}
+			}
+			
 
 			// merge the user settings into the defaults of the query
 			$query_args = wp_parse_args( $query_args, $wp_query->query_vars );
-
-			// Unset the `s` query arg because it is automagically filled by `WP_Query::fill_query_vars` and makes the query believe it is a search query when it is not
+			
+			// Unset the `s` query arg because it is automagically filled by
+			// `WP_Query::fill_query_vars` and makes the query believe 
+			// it is a search query when it is not
 			if ( ! is_search() ) {
 				unset( $query_args['s'] );
 			}
-
+			
 			if ( empty( $query_args['post_type'] ) && is_singular() ) {
 				$query_args['post_type'] = get_post_type( get_the_ID() );
 			}
