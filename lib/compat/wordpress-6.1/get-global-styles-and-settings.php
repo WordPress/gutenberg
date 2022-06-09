835e7a80dee2c9fa2c6d224a5bf4e2d16382ef23
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-06-09 10:24:15 +0300

    remove useless edit

--- lib/compat/wordpress-6.1/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.1/get-global-styles-and-settings.php
@@ -12,7 +12,6 @@ function wp_add_global_styles_for_blocks() {
 	$tree = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
 	// TODO some nodes dont have a name...
 	$block_nodes = $tree->get_styles_block_nodes();
-
 	foreach ( $block_nodes as $metadata ) {
 		$block_css  = $tree->get_styles_for_block( $metadata );
 		$block_name = str_replace( 'core/', '', $metadata['name'] );
