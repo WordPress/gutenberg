e0e802007360099eaeb29aabb23d936ced186f95
Petter Walbø Johnsgård <petter@dekode.no>
2022-06-13 21:08:28 +0200

    Apply suggestions from code review
    Co-authored-by: Lena Morita <lena@jaguchi.com>

--- packages/components/src/color-indicator/types.ts
+++ packages/components/src/color-indicator/types.ts
@@ -5,7 +5,7 @@ import type { CSSProperties } from 'react';
 
 export type ColorIndicatorProps = {
 	/**
-	 * The color of the indicator. Any value from the background property is
+	 * The color of the indicator. Any value from the CSS `background` property is
 	 * supported.
 	 */
 	colorValue: CSSProperties[ 'background' ];
