17ca2ce972dca105c30f1c31a883fd6773b30f46
Glen Davies <glen.davies@a8c.com>
2022-06-13 12:13:42 +1200

    Give a theme's spacingSizes setting priority over core spacingScale

--- lib/experimental/class-wp-rest-block-editor-settings-controller.php
+++ lib/experimental/class-wp-rest-block-editor-settings-controller.php
@@ -275,8 +275,13 @@ class WP_REST_Block_Editor_Settings_Controller extends WP_REST_Controller {
 					'type'        => 'array',
 					'context'     => array( 'post-editor', 'site-editor', 'widgets-editor' ),
 				),
-				'spacingSizesSizes'                      => array(
+				'spacingSizes'                      => array(
 					'description' => __( 'Active theme spacing sizes.', 'gutenberg' ),
+					'type'        => 'array',
+					'context'     => array( 'post-editor', 'site-editor', 'widgets-editor' ),
+				),
+				'spacingScale'                      => array(
+					'description' => __( 'Active theme spacing scale.', 'gutenberg' ),
 					'type'        => 'array',
 					'context'     => array( 'post-editor', 'site-editor', 'widgets-editor' ),
 				),
