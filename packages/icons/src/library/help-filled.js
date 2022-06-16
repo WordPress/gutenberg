6050c7b773531228cc2127508e0f0d7b401ffa44
Daniel L. Iser <daniel@code-atlantic.com>
2022-06-16 18:35:05 -0400

    Remove remaining hardcoded sizes from icons

--- packages/icons/src/library/help-filled.js
+++ packages/icons/src/library/help-filled.js
@@ -4,12 +4,7 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const helpFilled = (
-	<SVG
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-		width="24"
-		height="24"
-	>
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
