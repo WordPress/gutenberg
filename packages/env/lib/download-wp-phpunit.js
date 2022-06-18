0bf29272bbb596a835fdad6419dcb7960c6bd6ef
Christopher Allford <christopher.allford@automattic.com>
2022-06-17 21:52:57 -0700

    Improved `WordPress/wp-develop` Git Performance
    This commit minimizes the amount of data that needs
    to be downloaded when we pull in the test framework.
    It does this by not cloning the entire repository and
    only checking out once we've set up the sparse
    checkout.

--- packages/env/lib/download-wp-phpunit.js
+++ packages/env/lib/download-wp-phpunit.js
@@ -12,15 +12,20 @@ const path = require( 'path' );
 
 /**
  * Downloads the WordPress PHPUnit files for each environment into the appropriate directories.
- * 
- * @param {WPConfig} config     The wp-env config object. 
+ *
+ * @param {WPConfig} config     The wp-env config object.
  * @param {Object}   wpVersions The WordPress versions for each environment.
  * @param {Object}   spinner    The spinner object to show progress.
  * @param {boolean}  debug      Indicates whether or not debug mode is active.
  * @return {Promise} Returns a promise which resolves when the downloads finish.
  */
-module.exports = function downloadWPPHPUnit( config, wpVersions, spinner, debug ) {
-    const progresses = {};
+module.exports = function downloadWPPHPUnit(
+	config,
+	wpVersions,
+	spinner,
+	debug
+) {
+	const progresses = {};
 	const getProgressSetter = ( id ) => ( progress ) => {
 		progresses[ id ] = progress;
 		spinner.text =
@@ -33,25 +38,30 @@ module.exports = function downloadWPPHPUnit( config, wpVersions, spinner, debug 
 				.join( '\n' );
 	};
 
-    const promises = [];
-    for ( const env in config.env ) {
-        const wpVersion = wpVersions[ env ] ? wpVersions[ env ] : null;
-        const directory = path.join( config.workDirectoryPath, env === 'development' ? 'WordPress-PHPUnit' : 'tests-WordPress-PHPUnit')
-        promises.push(
-            downloadTestSuite( 
-                directory, 
-                wpVersion, 
-                { onProgress: getProgressSetter, spinner, debug } 
-            )
-        );
-    }
+	const promises = [];
+	for ( const env in config.env ) {
+		const wpVersion = wpVersions[ env ] ? wpVersions[ env ] : null;
+		const directory = path.join(
+			config.workDirectoryPath,
+			env === 'development'
+				? 'WordPress-PHPUnit'
+				: 'tests-WordPress-PHPUnit'
+		);
+		promises.push(
+			downloadTestSuite( directory, wpVersion, {
+				onProgress: getProgressSetter,
+				spinner,
+				debug,
+			} )
+		);
+	}
 
-    return Promise.all(promises);
-}
+	return Promise.all( promises );
+};
 
 /**
  * Downloads the PHPUnit tests for a given WordPress version into the appropriate directory.
- * 
+ *
  * @param {string}   directory          The directory to place the PHPUnit tests in.
  * @param {string}   wpVersion          The version of WordPress to install PHPUnit tests for. Trunk when empty.
  * @param {Object}   options
@@ -59,49 +69,72 @@ module.exports = function downloadWPPHPUnit( config, wpVersions, spinner, debug 
  * @param {Object}   options.spinner    A CLI spinner which indicates progress.
  * @param {boolean}  options.debug      True if debug mode is enabled.
  */
-async function downloadTestSuite( directory, wpVersion, { onProgress, spinner, debug } ) {
-    const log = debug
-    ? ( message ) => {
-            spinner.info( `SimpleGit: ${ message }` );
-            spinner.start();
-      }
-    : () => {};
-    onProgress( 0 );
+async function downloadTestSuite(
+	directory,
+	wpVersion,
+	{ onProgress, spinner, debug }
+) {
+	const log = debug
+		? ( message ) => {
+				spinner.info( `SimpleGit: ${ message }` );
+				spinner.start();
+		  }
+		: () => {};
+	onProgress( 0 );
 
-    const progressHandler = ( { progress } ) => {
+	const progressHandler = ( { progress } ) => {
 		onProgress( progress / 100 );
 	};
 
-    log( 'Cloning or getting the PHPUnit suite from GitHub.' );
+	// Make sure that the version is in X.X.X format. This is required
+	// because WordPress/wordpress-develop uses X.X.X tags but
+	// WordPress uses X.X for non-patch releases.
+	if ( wpVersion && wpVersion.match( /^[0-9]+.[0-9]+$/ ) ) {
+		wpVersion += '.0';
+	}
+
+	log( 'Cloning or getting the PHPUnit suite from GitHub.' );
 	const git = SimpleGit( { progress: progressHandler } );
 
-    const isRepo =
+	const isRepo =
 		fs.existsSync( directory ) &&
 		( await git.cwd( directory ).checkIsRepo( 'root' ) );
 
 	if ( isRepo ) {
 		log( 'Repo already exists, using it.' );
 	} else {
-		await git.clone( 'https://github.com/WordPress/wordpress-develop.git', directory, {
-			'--depth': '1',
-			'--no-single-branch': null,
-		} );
+		await git.clone(
+			'https://github.com/WordPress/wordpress-develop.git',
+			directory,
+			{
+				'--depth': '1',
+				'--no-checkout': null,
+			}
+		);
 		await git.cwd( directory );
+
+		// We use a sparse checkout to minimize the amount of data we need to download.
+		log( 'Enabling sparse checkout.' );
+		await git.raw( 'sparse-checkout', 'set', '--cone', 'tests/phpunit' );
 	}
-    
-    log( 'Fetching.' );
-	await git.fetch( 'origin' );
 
-    // Alpha, Beta, and RC versions are bleeding edge.
-    let ref;
-    if ( ! wpVersion || wpVersion.match(/-(?:alpha|beta|rc)/)) {
-        ref = 'origin/trunk'
-    } else {
-        ref = `origin/tags/${ wpVersion }`
-    }
-    
-    log( `Checking out ${ ref }.` );
-    await git.raw('sparse-checkout', 'set', 'tests/phpunit');
+	// Figure out the ref that we need to checkout to get the correct version of the PHPUnit library.
+	// Alpha, Beta, and RC versions are bleeding edge and should pull from trunk.
+	let ref;
+	const fetchRaw = [];
+	if ( ! wpVersion || wpVersion.match( /-(?:alpha|beta|rc)/ ) ) {
+		ref = 'trunk';
+		fetchRaw.push( 'fetch', 'origin', ref, '--depth', '1' );
+	} else {
+		ref = `tags/${ wpVersion }`;
+		fetchRaw.push( 'fetch', 'origin', 'tag', wpVersion, '--depth', '1' );
+	}
 
-    onProgress( 1 );
+	log( `Fetching ${ ref }.` );
+	await git.raw( fetchRaw );
+
+	log( `Checking out ${ ref }.` );
+	await git.checkout( ref );
+
+	onProgress( 1 );
 }
