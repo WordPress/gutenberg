46107ad7990e8a94d24f8a5dfa5c407d1cf495c3
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-20 10:13:19 +0200

    Create navigation.spec.js

---
+++ test/e2e/specs/editor/blocks/navigation.spec.js
@@ -0,0 +1,56 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Navigation block', () => {
+	test( 'When a menu item is active, it should have the .current-menu-item class name', async ( {
+		admin,
+		editor,
+		page,
+	} ) => {
+		const random = getRandomInt( 9999 );
+		const pageTitle = `Page ${ random }`;
+
+		await admin.createNewPost( {
+			postType: 'page',
+			title: pageTitle,
+		} );
+		await editor.publishPost();
+
+		// Create a new menu
+		await admin.visitAdminPage( 'nav-menus.php?menu=0' );
+		await page.type(
+			'role=textbox[name="Menu Name"i]',
+			`Menu ${ random }`
+		);
+		await page.check( '#locations-primary' );
+		await page.click( '#save_menu_footer' );
+		await page.click( `[aria-label="Most Recent"] >> text=${ pageTitle }` );
+		await page.click( '#submit-posttype-page' );
+		// eslint-disable-next-line no-restricted-syntax
+		await page.waitForTimeout( 1000 );
+		await page.click( '#save_menu_footer' );
+
+		// Go to the created page
+		await admin.visitAdminPage( 'edit.php?post_type=page' );
+		const link = new URL(
+			await page
+				.locator( `role=link[name="“Page ${ random }” \\(Edit\\)"]` )
+				.getAttribute( 'href' )
+		);
+
+		const pageId = link.searchParams.get( 'post' );
+
+		await page.goto( `${ link.origin }/?page_id=${ pageId }` );
+
+		// Check that the menu item has the class name .current-menu-item
+		await expect( page.locator( `.page-item-${ pageId }` ) ).toHaveClass(
+			/current-menu-item/
+		);
+	} );
+} );
+
+function getRandomInt( max ) {
+	return Math.floor( Math.random() * max );
+}
