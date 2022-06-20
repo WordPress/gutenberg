2b45b0266c0d5f2005c5c1f624cad370403250d9
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-20 14:54:38 +0200

    Update navigation.spec.js

--- test/e2e/specs/various/navigation.spec.js
+++ test/e2e/specs/various/navigation.spec.js
@@ -3,20 +3,19 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test.describe( 'Navigation block', () => {
+test.describe( 'Classic menus', () => {
 	test( 'When a menu item is active, it should have the .current-menu-item class name', async ( {
 		admin,
-		editor,
 		page,
+		requestUtils,
 	} ) => {
 		const random = getRandomInt( 9999 );
 		const pageTitle = `Page ${ random }`;
 
-		await admin.createNewPost( {
-			postType: 'page',
-			title: pageTitle,
-		} );
-		await editor.publishPost();
+		const newPage = await requestUtils.createNewPage(
+			pageTitle,
+			'publish'
+		);
 
 		// Create a new menu
 		await admin.visitAdminPage( 'nav-menus.php?menu=0' );
@@ -26,31 +25,23 @@ test.describe( 'Navigation block', () => {
 		);
 		await page.check( '#locations-primary' );
 		await page.click( '#save_menu_footer' );
+
 		await page.click( `[aria-label="Most Recent"] >> text=${ pageTitle }` );
 		await page.click( '#submit-posttype-page' );
-		// eslint-disable-next-line no-restricted-syntax
-		await page.waitForTimeout( 2000 );
+		await page.waitForSelector( '#menu-to-edit .menu-item' );
 
 		await page.click( '#save_menu_footer' );
-		// eslint-disable-next-line no-restricted-syntax
-		await page.waitForTimeout( 2000 );
 
 		// Go to the created page
-		await admin.visitAdminPage( 'edit.php?post_type=page' );
-		const link = new URL(
-			await page
-				.locator( `role=link[name="“Page ${ random }” \\(Edit\\)"]` )
-				.getAttribute( 'href' )
-		);
-
-		const pageId = link.searchParams.get( 'post' );
-
-		await page.goto( `${ link.origin }/?page_id=${ pageId }` );
+		await page.goto( newPage.guid.rendered );
 
 		// Check that the menu item has the class name .current-menu-item
-		await expect( page.locator( `.page-item-${ pageId }` ) ).toHaveClass(
-			/current-menu-item/
-		);
+		await expect(
+			page.locator( `.page-item-${ newPage.id }` )
+		).toHaveClass( /current-menu-item/ );
+
+		// Delete the page
+		await requestUtils.deletePage( newPage.id );
 	} );
 } );
 
