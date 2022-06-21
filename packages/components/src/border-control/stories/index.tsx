4100143476b9ca9fc66aa32e55b0a7fca781a429
Lena Morita <lena@jaguchi.com>
2022-06-22 00:25:31 +0900

    Simplify colors to improve code snippet readability

--- packages/components/src/border-control/stories/index.tsx
+++ packages/components/src/border-control/stories/index.tsx
@@ -36,21 +36,9 @@ export default meta;
 
 // Available border colors.
 const colors = [
-	{ name: 'Gray 0', color: '#f6f7f7' },
-	{ name: 'Gray 5', color: '#dcdcde' },
-	{ name: 'Gray 20', color: '#a7aaad' },
-	{ name: 'Gray 70', color: '#3c434a' },
-	{ name: 'Gray 100', color: '#101517' },
-	{ name: 'Blue 20', color: '#72aee6' },
-	{ name: 'Blue 40', color: '#3582c4' },
-	{ name: 'Blue 70', color: '#0a4b78' },
-	{ name: 'Red 40', color: '#e65054' },
-	{ name: 'Red 70', color: '#8a2424' },
-	{ name: 'Green 10', color: '#68de7c' },
-	{ name: 'Green 40', color: '#00a32a' },
-	{ name: 'Green 60', color: '#007017' },
-	{ name: 'Yellow 10', color: '#f2d675' },
-	{ name: 'Yellow 40', color: '#bd8600' },
+	{ name: 'Blue', color: '#72aee6' },
+	{ name: 'Red', color: '#e65054' },
+	{ name: 'Yellow', color: '#f2d675' },
 ];
 
 // Multiple origin colors.
@@ -58,11 +46,8 @@ const multipleOriginColors = [
 	{
 		name: 'Default',
 		colors: [
-			{ name: 'Gray 0', color: '#f6f7f7' },
-			{ name: 'Gray 5', color: '#dcdcde' },
 			{ name: 'Gray 20', color: '#a7aaad' },
 			{ name: 'Gray 70', color: '#3c434a' },
-			{ name: 'Gray 100', color: '#101517' },
 		],
 	},
 	{
@@ -71,18 +56,13 @@ const multipleOriginColors = [
 			{ name: 'Blue 20', color: '#72aee6' },
 			{ name: 'Blue 40', color: '#3582c4' },
 			{ name: 'Blue 70', color: '#0a4b78' },
-			{ name: 'Red 40', color: '#e65054' },
-			{ name: 'Red 70', color: '#8a2424' },
 		],
 	},
 	{
 		name: 'User',
 		colors: [
-			{ name: 'Green 10', color: '#68de7c' },
-			{ name: 'Green 40', color: '#00a32a' },
-			{ name: 'Green 60', color: '#007017' },
-			{ name: 'Yellow 10', color: '#f2d675' },
-			{ name: 'Yellow 40', color: '#bd8600' },
+			{ name: 'Green', color: '#00a32a' },
+			{ name: 'Yellow', color: '#f2d675' },
 		],
 	},
 ];
