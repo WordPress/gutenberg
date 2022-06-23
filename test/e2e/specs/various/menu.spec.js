74baa52d39c079aab9693aa543642c44a35c9674
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-23 15:15:37 +0200

    Create menu.spec.js

---
+++ test/e2e/specs/various/menu.spec.js
@@ -0,0 +1,63 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Classic menus', () => {
+	test( 'When a menu item is active, it should have the .current-menu-item class name', async ( {
+		admin,
+		page,
+		requestUtils,
+	} ) => {
+		const random = getRandomInt( 9999 );
+		const pageTitle = `Page ${ random }`;
+
+		const newPage = await requestUtils.createNewPage(
+			pageTitle,
+			'publish'
+		);
+
+		// Create a new menu
+		await admin.visitAdminPage( 'nav-menus.php?menu=0' );
+		await page.type(
+			'role=textbox[name="Menu Name"i]',
+			`Menu ${ random }`
+		);
+		await page.check( '#locations-primary' );
+
+		await page.locator( '#save_menu_footer' ).focus();
+		await Promise.all( [
+			page.waitForNavigation(),
+			page.locator( '#save_menu_footer' ).click(),
+		] );
+
+		await page.click( `[aria-label="Most Recent"] >> text=${ pageTitle }` );
+
+		await page.locator( 'input[name="add-post-type-menu-item"]' ).click();
+
+		await expect(
+			page.locator( '#menu-to-edit .menu-item-title' )
+		).toContainText( `${ pageTitle }` );
+
+		await page.locator( '#save_menu_footer' ).focus();
+		await Promise.all( [
+			page.waitForNavigation(),
+			page.locator( '#save_menu_footer' ).click(),
+		] );
+
+		// Go to the created page
+		await page.goto( newPage.guid.rendered );
+
+		// Check that the menu item has the class name .current-menu-item
+		await expect(
+			page.locator( `.page-item-${ newPage.id }` )
+		).toHaveClass( /current-menu-item/ );
+
+		// Delete the page
+		await requestUtils.deletePage( newPage.id );
+	} );
+} );
+
+function getRandomInt( max ) {
+	return Math.floor( Math.random() * max );
+}
