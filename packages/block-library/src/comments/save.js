904074f487394b5a27a55f5ee5b03eba129b3429
Carlos Bravo <carlos.bravo@automattic.com>
2022-06-23 17:51:36 +0200

    Add fallback class of previous block name

--- packages/block-library/src/comments/save.js
+++ packages/block-library/src/comments/save.js
@@ -5,7 +5,12 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function CommentsSave( { attributes: { tagName: Tag } } ) {
 	return (
-		<Tag { ...useBlockProps.save() }>
+		<Tag
+			{ ...useBlockProps.save( {
+				// We add the previous block name for backward compatibility.
+				className: 'wp-block-comments-query-loop',
+			} ) }
+		>
 			<InnerBlocks.Content />
 		</Tag>
 	);
