88265ab42990cdb669b8dc409302b68bb8f03d88
Adam Zieliński <adam@adamziel.com>
2022-06-23 10:54:34 +0200

    Change deps:any type to deps:unknown[]

--- packages/data/src/components/use-select/index.js
+++ packages/data/src/components/use-select/index.js
@@ -41,15 +41,15 @@ const renderQueue = createQueue();
  * [rules of hooks](https://reactjs.org/docs/hooks-rules.html).
  *
  * @template {MapSelect | StoreDescriptor<any>} T
- * @param {T} mapSelect Function called on every state change. The returned value is
- * 											exposed to the component implementing this hook. The function
- * 											receives the `registry.select` method on the first argument
- * 											and the `registry` on the second argument.
- *                      When a store key is passed, all selectors for the store will be
- *                      returned. This is only meant for usage of these selectors in event
- *                      callbacks, not for data needed to create the element tree.
- * @param {*} deps      If provided, this memoizes the mapSelect so the same `mapSelect` is
- * 											invoked on every state change unless the dependencies change.
+ * @param {T}         mapSelect Function called on every state change. The returned value is
+ *                              exposed to the component implementing this hook. The function
+ *                              receives the `registry.select` method on the first argument
+ *                              and the `registry` on the second argument.
+ *                              When a store key is passed, all selectors for the store will be
+ *                              returned. This is only meant for usage of these selectors in event
+ *                              callbacks, not for data needed to create the element tree.
+ * @param {unknown[]} deps      If provided, this memoizes the mapSelect so the same `mapSelect` is
+ *                              invoked on every state change unless the dependencies change.
  *
  * @example
  * ```js
