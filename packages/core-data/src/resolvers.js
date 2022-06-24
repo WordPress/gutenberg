78cfffd931f721a8122f155b00943ab6882150aa
Jonny Harris <jon@spacedmonkey.co.uk>
2022-06-24 15:49:43 +0100

    Fix lints.

--- packages/core-data/src/resolvers.js
+++ packages/core-data/src/resolvers.js
@@ -348,9 +348,8 @@ export const canUserEditEntityRecord =
 export const getAutosaves =
 	( postType, postId ) =>
 	async ( { dispatch, resolveSelect } ) => {
-		const { rest_base: restBase, rest_namespace: restNamespace = 'wp/v2', } = await resolveSelect.getPostType(
-			postType
-		);
+		const { rest_base: restBase, rest_namespace: restNamespace = 'wp/v2' } =
+			await resolveSelect.getPostType( postType );
 		const autosaves = await apiFetch( {
 			path: `/${ restNamespace }/${ restBase }/${ postId }/autosaves?context=edit`,
 		} );
