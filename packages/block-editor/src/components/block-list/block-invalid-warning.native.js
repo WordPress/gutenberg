ca7b784ce87ad14573be88b306da103cea956d40
Siobhan <siobhan@automattic.com>
2022-06-27 22:34:11 +0100

    Implement "attemptBlockRecovery" function
    With this commit, the "attemptBlockRecovery" function is implemented, based on the web's version of the function shown here: https://github.com/WordPress/gutenberg/blob/286e6be2b13fc6ab2059b66d5c79728d811562ee/packages/block-editor/src/components/block-list/block-invalid-warning.js#L122-L124

--- packages/block-editor/src/components/block-list/block-invalid-warning.native.js
+++ packages/block-editor/src/components/block-list/block-invalid-warning.native.js
@@ -2,18 +2,42 @@
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import Warning from '../warning';
+import { store as blockEditorStore } from '../../store';
 
-export default function BlockInvalidWarning( { blockTitle, icon } ) {
+export default function BlockInvalidWarning( { blockTitle, icon, clientId } ) {
 	const accessibilityLabel = sprintf(
 		/* translators: accessibility text for blocks with invalid content. %d: localized block title */
 		__( '%s block. This block has invalid content' ),
 		blockTitle
 	);
+
+	const selector = ( select ) => {
+		const { getBlock } = select( blockEditorStore );
+		const block = getBlock( clientId );
+		return {
+			block,
+		};
+	};
+
+	const { block } = useSelect( selector, [ clientId ] );
+
+	const { replaceBlock } = useDispatch( blockEditorStore );
+
+	const recoverBlock = ( { name, attributes, innerBlocks } ) =>
+		createBlock( name, attributes, innerBlocks );
+
+	const attemptBlockRecovery = () => {
+		replaceBlock( block.clientId, recoverBlock( block ) );
+	};
+
+	attemptBlockRecovery();
 
 	return (
 		<Warning
