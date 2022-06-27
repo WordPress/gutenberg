cbb87873f7004cd199383433ef67f06a10e1863e
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2022-06-27 17:01:44 +1000

    Ensure blockGap controls are not exposed in global styles when experimental skip serializiation is used.
    This ensures that the Gallery block is not exposed in the global styles UI, as its blockGap values are proceeded individually at the block level. Support for the Gallery block in global styles will be looked into in future follow-ups.

--- packages/edit-site/src/components/global-styles/hooks.js
+++ packages/edit-site/src/components/global-styles/hooks.js
@@ -27,10 +27,9 @@ export const useGlobalStylesReset = () => {
 	const canReset = !! config && ! isEqual( config, EMPTY_CONFIG );
 	return [
 		canReset,
-		useCallback(
-			() => setUserConfig( () => EMPTY_CONFIG ),
-			[ setUserConfig ]
-		),
+		useCallback( () => setUserConfig( () => EMPTY_CONFIG ), [
+			setUserConfig,
+		] ),
 	];
 };
 
