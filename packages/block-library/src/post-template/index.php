bb643b55631cf4281f94ebe0ea0ae76c2be8d669
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-06-27 18:11:22 +0300

    uses global query as is and hides and UI that overrides it

--- packages/block-library/src/post-template/index.php
+++ packages/block-library/src/post-template/index.php
@@ -44,46 +44,15 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 	$page_key = isset( $block->context['queryId'] ) ? 'query-' . $block->context['queryId'] . '-page' : 'query-page';
 	$page     = empty( $_GET[ $page_key ] ) ? 1 : (int) $_GET[ $page_key ];
 
-	$query_args = build_query_vars_from_query_block( $block, $page );
-
-	// Override the custom query with the global query if needed.
+	// Use global query if needed.
 	$use_global_query = ( isset( $block->context['query']['inherit'] ) && $block->context['query']['inherit'] );
 	if ( $use_global_query ) {
 		global $wp_query;
-		if ( $wp_query && isset( $wp_query->query_vars ) && is_array( $wp_query->query_vars ) ) {
-			// Unset all the query block query args that cannot be set by the user
-			// when inherit is set to on.
-			$block_provided_query_args = array_keys( $query_args );
-			$user_visible_query_args   = array(
-				'ignore_sticky_posts', // Ignore sticky posts.
-				'post__in', // Only sticky posts.
-				'post__not_in', // Exclude sticky posts.
-			);
-			foreach ( $block_provided_query_args as $query_args_key ) {
-				if ( ! in_array( $query_args_key, $user_visible_query_args, true ) ) {
-					unset( $query_args[ $query_args_key ] );
-				}
-			}
-
-			// merge the user settings into the defaults of the query.
-			$query_args = wp_parse_args( $query_args, $wp_query->query_vars );
-
-			/*
-			* Unset the `s` query arg because it is automagically filled by
-			* `WP_Query::fill_query_vars` and makes the query believe
-			* it is a search query when it is not.
-			*/
-			if ( ! is_search() ) {
-				unset( $query_args['s'] );
-			}
-
-			if ( empty( $query_args['post_type'] ) && is_singular() ) {
-				$query_args['post_type'] = get_post_type( get_the_ID() );
-			}
-		}
+		$query = $wp_query;
+	}  else {
+		$query_args = build_query_vars_from_query_block( $block, $page );
+		$query      = new WP_Query( $query_args );
 	}
-
-	$query = new WP_Query( $query_args );
 
 	if ( ! $query->have_posts() ) {
 		return '';
@@ -130,7 +99,9 @@ function render_block_core_post_template( $attributes, $content, $block ) {
 		$content     .= '<li class="' . esc_attr( $post_classes ) . '">' . $block_content . '</li>';
 	}
 
-	wp_reset_postdata();
+	if ( ! $use_global_query ) {
+		wp_reset_postdata();
+	}
 
 	return sprintf(
 		'<ul %1$s>%2$s</ul>',
