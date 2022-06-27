7c6aed58de1b54641b408a281a1d5a47428bdaf2
Dave Smith <getdavemail@gmail.com>
2022-06-27 13:10:35 +0100

    Add initial tabs

--- packages/edit-site/src/components/global-styles/screen-link-color.js
+++ packages/edit-site/src/components/global-styles/screen-link-color.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { __experimentalColorGradientControl as ColorGradientControl } from '@wordpress/block-editor';
-
+import { TabPanel } from '@wordpress/components';
 /**
  * Internal dependencies
  */
@@ -48,21 +48,48 @@ function ScreenLinkColor( { name } ) {
 			<ScreenHeader
 				title={ __( 'Links' ) }
 				description={ __(
-					'Set the default color used for links across the site.'
+					'Set the colors used for links across the site.'
 				) }
 			/>
-			<ColorGradientControl
-				className="edit-site-screen-link-color__control"
-				colors={ colorsPerOrigin }
-				disableCustomColors={ ! areCustomSolidsEnabled }
-				__experimentalHasMultipleOrigins
-				showTitle={ false }
-				enableAlpha
-				__experimentalIsRenderedInSidebar
-				colorValue={ linkColor }
-				onColorChange={ setLinkColor }
-				clearable={ linkColor === userLinkColor }
-			/>
+
+			<TabPanel
+				className="my-tab-panel"
+				tabs={ [
+					{
+						name: 'default',
+						title: 'Default',
+						className: 'color-text-default',
+					},
+					{
+						name: 'hover',
+						title: 'Hover',
+						className: 'color-text-hover',
+					},
+					{
+						name: 'focus',
+						title: 'Focus',
+						className: 'color-text-focus',
+					},
+				] }
+			>
+				{ ( tab ) => (
+					<>
+						<p>Selected tab: { tab.title }</p>
+						<ColorGradientControl
+							className="edit-site-screen-link-color__control"
+							colors={ colorsPerOrigin }
+							disableCustomColors={ ! areCustomSolidsEnabled }
+							__experimentalHasMultipleOrigins
+							showTitle={ false }
+							enableAlpha
+							__experimentalIsRenderedInSidebar
+							colorValue={ linkColor }
+							onColorChange={ setLinkColor }
+							clearable={ linkColor === userLinkColor }
+						/>
+					</>
+				) }
+			</TabPanel>
 		</>
 	);
 }
