13c3be28706fbda4ab163ee265b25400b6dbc2df
Siobhan <siobhan@automattic.com>
2022-06-28 22:52:34 +0100

    [TESTING] Positioning of code causing failures

--- packages/rich-text/src/component/index.native.js
+++ packages/rich-text/src/component/index.native.js
@@ -660,6 +660,15 @@ export class RichText extends Component {
 		this.comesFromAztec = true;
 		this.firedAfterTextChanged = true; // Selection change event always fires after the fact.
 
+		// Check for and discard events during quick typing. Updating the selection during quick typing isn't
+		// necessary and can cause UI lags. (see https://github.com/WordPress/gutenberg/pull/41682.)
+		if (
+			contentWithoutRootTag !== this.value &&
+			this.lastAztecEventType === 'selection change'
+		) {
+			return;
+		}
+
 		// Update text before updating selection
 		// Make sure there are changes made to the content before upgrading it upward.
 		this.onTextUpdate( event );
