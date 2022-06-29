bc1b4943a2098858cc774967d18560a0de7114d3
Siobhan <siobhan@automattic.com>
2022-06-29 14:03:00 +0100

    Add missing space in leadingOrTrailingSpace check

--- packages/rich-text/src/component/index.native.js
+++ packages/rich-text/src/component/index.native.js
@@ -660,7 +660,7 @@ export class RichText extends Component {
 		// Check for and discard events during quick typing. Updating the selection during quick typing isn't
 		// necessary and can cause UI lags. (see https://github.com/WordPress/gutenberg/pull/41682.)
 		const leadingOrTrailingSpace =
-			contentWithoutRootTag.charAt( 0 ) === '' ||
+			contentWithoutRootTag.charAt( 0 ) === ' ' ||
 			contentWithoutRootTag.substr( contentWithoutRootTag.length - 1 ) ===
 				' ';
 		if (
