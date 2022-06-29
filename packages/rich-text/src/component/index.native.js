117288eea235895c022a2bdaf8ec4963eee045dc
Siobhan <siobhan@automattic.com>
2022-06-29 15:52:13 +0100

    Add further clarification to comment

--- packages/rich-text/src/component/index.native.js
+++ packages/rich-text/src/component/index.native.js
@@ -660,6 +660,7 @@ export class RichText extends Component {
 		// Check for and discard events during quick typing. Updating the selection during quick typing isn't
 		// necessary and can cause UI lags. (see https://github.com/WordPress/gutenberg/pull/41682.)
 		// Note, it's necessary to detect leading or trailing spaces to prevent them from being stripped.
+		// (see https://github.com/WordPress/gutenberg/pull/42046.)
 		const leadingOrTrailingSpace =
 			contentWithoutRootTag.charAt( 0 ) === ' ' ||
 			contentWithoutRootTag.substr( contentWithoutRootTag.length - 1 ) ===
