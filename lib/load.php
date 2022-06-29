ae1cfe358558e14b91cb1dde8e760094ca9aac02
ntsekouras <ntsekouras@outlook.com>
2022-06-29 11:25:35 +0300

    maybe keep gallery for 6.0?

--- lib/load.php
+++ lib/load.php
@@ -59,6 +59,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 require __DIR__ . '/experimental/editor-settings.php';
 
 // WordPress 6.0 compat.
+require __DIR__ . '/compat/wordpress-6.0/block-gallery.php';
 require __DIR__ . '/compat/wordpress-6.0/block-editor-settings.php';
 require __DIR__ . '/compat/wordpress-6.0/get-global-styles-and-settings.php';
 require __DIR__ . '/compat/wordpress-6.0/render-svg-filters.php';
