0c4c1b43722ac09c2eab98f68a47b4e5830a4445
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2022-06-29 16:01:37 +0530

    Migrate Block Mover Test For Playwright

--- test/e2e/specs/editor/various/block-mover.spec.js
+++ test/e2e/specs/editor/various/block-mover.spec.js
@@ -1,14 +1,17 @@
 /**
  * WordPress dependencies
  */
-import { createNewPost, showBlockToolbar } from '@wordpress/e2e-test-utils';
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-describe( 'block mover', () => {
-	beforeEach( async () => {
-		await createNewPost();
+test.describe( 'block mover', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
 	} );
 
-	it( 'should show block mover when more than one block exists', async () => {
+	test( 'should show block mover when more than one block exists', async ( {
+		editor,
+		page,
+	} ) => {
 		// Create a two blocks on the page.
 		await page.click( '.block-editor-default-block-appender' );
 		await page.keyboard.type( 'First Paragraph' );
@@ -16,24 +19,26 @@ describe( 'block mover', () => {
 		await page.keyboard.type( 'Second Paragraph' );
 
 		// Select a block so the block mover is rendered.
-		await page.focus( '[data-type="core/paragraph"]' );
-
-		await showBlockToolbar();
+		await page.focus( 'text=First Paragraph' );
+		await editor.showBlockToolbar();
 
 		const blockMover = await page.$$( '.block-editor-block-mover' );
 		// There should be a block mover.
 		expect( blockMover ).toHaveLength( 1 );
 	} );
 
-	it( 'should hide block mover when only one block exists', async () => {
+	test( 'should hide block mover when only one block exists', async ( {
+		editor,
+		page,
+	} ) => {
 		// Create a single block on the page.
 		await page.click( '.block-editor-default-block-appender' );
 		await page.keyboard.type( 'First Paragraph' );
 
 		// Select a block so the block mover has the possibility of being rendered.
-		await page.focus( '.block-editor-block-list__block' );
+		await page.focus( 'text=First Paragraph' );
 
-		await showBlockToolbar();
+		await editor.showBlockToolbar();
 
 		// Ensure no block mover exists when only one block exists on the page.
 		const blockMover = await page.$$( '.block-editor-block-mover' );
