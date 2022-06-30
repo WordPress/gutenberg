fcae3aaaea1525e7af05de1708274d8332ce84f3
Robert Anderson <robert@noisysocks.com>
2022-06-30 16:50:37 +1000

    Wrap long URLs

--- packages/editor/src/components/post-url/label.js
+++ packages/editor/src/components/post-url/label.js
@@ -3,7 +3,6 @@
  */
 import { useSelect } from '@wordpress/data';
 import { filterURLForDisplay } from '@wordpress/url';
-import { useRef, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -15,43 +14,5 @@ export default function PostURLLabel() {
 		( select ) => select( editorStore ).getCurrentPost().link,
 		[]
 	);
-	return (
-		<LeftTruncatedText>
-			{ filterURLForDisplay( postLink ) }
-		</LeftTruncatedText>
-	);
-}
-
-function LeftTruncatedText( { children: text } ) {
-	const ref = useRef();
-
-	useEffect( () => {
-		const container = ref.current;
-
-		container.innerText = text;
-
-		container.style.display = 'block';
-		container.style.width = '100%';
-		const maxWidth = container.clientWidth;
-
-		container.style.display = null;
-		container.style.width = null;
-
-		const canvas = container.ownerDocument.createElement( 'canvas' );
-		const context = canvas.getContext( '2d' );
-		context.font = window
-			.getComputedStyle( container )
-			.getPropertyValue( 'font' );
-
-		let truncatedText = text;
-		let truncateAmount = 1;
-		while ( context.measureText( truncatedText ).width > maxWidth ) {
-			truncatedText = '…' + text.substring( truncateAmount );
-			truncateAmount++;
-		}
-
-		container.innerText = truncatedText;
-	}, [ text ] );
-
-	return <span ref={ ref }></span>;
+	return filterURLForDisplay( postLink );
 }
