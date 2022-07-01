64d22a9ae58518a76289f7491f5f1fedf7a58a6e
Carolina Nymark <hi@themesbycarolina.com>
2022-07-01 14:17:46 +0200

    Update screen-css.js

--- packages/edit-site/src/components/global-styles/screen-css.js
+++ packages/edit-site/src/components/global-styles/screen-css.js
@@ -17,21 +17,38 @@ import Subtitle from './subtitle';
 
 function ScreenCSS() {
 	/**
-	 * Plan:
-	 * 1: Get the custom_css post type
-	 * 2: Get the slug of the active theme
-	 * 3: Get the CSS content for the *active* theme only!
+	 * Original plan:
+	 * 1: Get the slug of the current theme
+	 * 2: Get the custom_css post type content for the current theme
+	 * 3: Display the content inside the text area
 	 * 4: Fun part? Get changes to save.
-	 * 
+	 *
 	 * X. Is the TextareaControl too primitive? Is there a better choice?
 	 * X. Add the code icon before the button text.
+	 * X. Validate the CSS!
+	 *
+	 * What we learnt:
+	 * getEntityRecords for the custom_css post type does not include the content.
+	 *
+	 * Plan B: Create a new global styles setting for the custom css.
+	 * Use this new setting to display the CSS on the global styles screen.
+	 * Add the custom_css post type to the global styles rest API endpoint,
+	 * and use the existing PHP filters to make sure that the CSS in the
+	 * global styles option and the Customier option matches.
+	 *
+	 *
+	 * X. Use the existing methods for printing the actual CSS on the front and editors?
 	 */
+	const currentTheme = useSelect( ( select ) => {
+		return select( coreStore ).getEntityRecords( 'root', 'theme', {
+			status: 'active',
+		} )[ 0 ].stylesheet;
+	}, [] );
 
-	// So, this does not include the actual CSS,
-	// need to figure out why and if it is stored somewhere else?
 	const customCssPost = useSelect( ( select ) => {
 		return select( coreStore ).getEntityRecords( 'postType', 'custom_css', {
 			per_page: 1,
+			slug: currentTheme,
 		} );
 	}, [] );
 
