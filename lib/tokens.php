433b2f3d1065e207b3c92a1ae7071886ca6917ec
Dennis Snell <dennis.snell@automattic.com>
2022-07-05 10:24:39 +0200

    Stub out main core token replacer.

--- lib/tokens.php
+++ lib/tokens.php
@@ -22,6 +22,31 @@ function register_token( $namespace, $name, $replacer, $priority = 10 ) {
 }
 
 
+function tokens_core_replacer( $rendered, $token ) {
+	global $post_id;
+
+	if ( 'core' !== $token->namespace ) {
+		return $rendered;
+	}
+
+	switch ( $token->name ) {
+		case 'featured-image':
+			return get_the_post_thumbnail_url();
+
+		case 'permalink':
+			$permalink = get_permalink( is_int( $token->value ) ? $token->value : $post_id );
+
+			return false !== $permalink ? $permalink : '';
+
+		case 'plugins_url':
+			return plugins_url();
+
+		default:
+			return $token->fallback;
+	}
+}
+
+
 function tokens_token_replacer( $token ) {
 	return apply_filters( 'render_token', $token->fallback, $token );
 }
@@ -35,11 +60,8 @@ function tokens_swap_tokens( $text ) {
 function tokens_init() {
 	require_once __DIR__ . '/../packages/tokens/token-parser.php';
 
+	add_filter( 'render_token', 'tokens_core_replacer', 2, 10 );
 	add_filter( 'the_content', 'tokens_swap_tokens', 1, 1000 );
-
-	register_token( 'core', 'echo', function ( $rendered, $token ) { return $token->value; } );
-	register_token( 'core', 'featured-image', function ( $rendered, $token ) { return get_the_post_thumbnail_url(); } );
-	register_token( 'core', 'plugins-url', function ( $rendered, $token ) { return plugins_url(); } );
 }
 
 add_action( 'init', 'tokens_init' );
