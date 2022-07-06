493d14fffc89d3f124b160995559fb5c75b03a03
Andrei Draganescu <me@andreidraganescu.info>
2022-07-06 10:27:43 +0300

    Remove sticky posts setting when we inherit the query (#40656)
    * reset the sticky posts query var for inherited query
    
    * merge query vars into query defaults and unset empty s query var
    
    * unser all query block defaults which are not editable when inherit is true
    
    * fixes linting PHP problems
    
    * uses global query as is and hides and UI that overrides it
    
    * removes mistaken changes
    
    * removes query overriding on inherit for no results block, although why?
    
    * Update packages/block-library/src/query-no-results/index.php
    
    * Adds co-authors
    
    Co-authored-by: Jonny Harris <237508+spacedmonkey@users.noreply.github.com>
    Co-authored-by: Huub <50170696+huubl@users.noreply.github.com>
    
    * Update packages/block-library/src/query-no-results/index.php
    
    CS alignments fix
    
    Co-authored-by: Jonny Harris <237508+spacedmonkey@users.noreply.github.com>
    Co-authored-by: Huub <50170696+huubl@users.noreply.github.com>
    Co-authored-by: Ari Stathopoulos <aristath@gmail.com>

--- packages/block-library/src/query/edit/inspector-controls/index.js
+++ packages/block-library/src/query/edit/inspector-controls/index.js
@@ -130,7 +130,7 @@ export default function QueryInspectorControls( {
 						onChange={ setQuery }
 					/>
 				) }
-				{ showSticky && (
+				{ ! inherit && showSticky && (
 					<StickyControl
 						value={ sticky }
 						onChange={ ( value ) => setQuery( { sticky: value } ) }
