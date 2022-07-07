da2c4338bf3a95f23035e70bf59c4ee2347844c7
Ari Stathopoulos <aristath@gmail.com>
2022-07-07 11:56:03 +0300

    some inline docs & cleanup on the store

--- packages/style-engine/class-wp-style-engine-css-rules-store.php
+++ packages/style-engine/class-wp-style-engine-css-rules-store.php
@@ -46,10 +46,20 @@ class WP_Style_Engine_CSS_Rules_Store {
 	 * @param string $selector The CSS selector.
 	 */
 	public static function get_rule( $selector ) {
+
+		// Bail early if there is no selector.
 		if ( empty( $selector ) ) {
 			return;
 		}
 
+		// If the selector is already stored, return it.
+		if ( isset( static::$rules[ $selector ] ) ) {
+			return static::$rules[ $selector ];
+		}
+
+		// Create the selector if it doesn't exist.
+		// To do that, first deconstruct the selector to its parts
+		// and build the hierarchy of selector objects prior to storing it.
 		$selector_parts = static::deconstruct_selector( $selector );
 		$full_selector  = '';
 		foreach ( $selector_parts as $key => $selector_part ) {
@@ -112,27 +122,17 @@ class WP_Style_Engine_CSS_Rules_Store {
 				continue;
 			}
 
+			// Use "SPLIT_CHARACTER" as a delimiter.
 			$value = str_replace( $character, "SPLIT_CHARACTER$character", $value );
+
+			// Pseudo-selectors are a special case.
 			if ( ':' === $character ) {
 				$value = str_replace( ':', '&:', $value );
 			}
+
 			$value     = explode( 'SPLIT_CHARACTER', $value );
 			$new_array = array_merge( $new_array, $value );
 		}
 		return $new_array;
-	}
-
-	/**
-	 * Inject an array inside an array at a specific index.
-	 *
-	 * @static
-	 *
-	 * @param array $array The array to inject into.
-	 * @param array $inject The array to inject.
-	 * @param int   $index The index to inject at.
-	 */
-	public static function inject_array_at_index( $array, $inject, $index ) {
-		$array = array_slice( $array, 0, $index, true ) + $inject + array_slice( $array, $index, count( $array ) - $index, true );
-		return $array;
 	}
 }
