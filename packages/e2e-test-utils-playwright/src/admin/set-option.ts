cef603a2c56afe3c2e378d130c3469bcdaeb1385
Bernie Reiter <ockham@raz.or.at>
2022-07-08 16:07:24 +0200

    Simplify setOption

--- packages/e2e-test-utils-playwright/src/admin/set-option.ts
+++ packages/e2e-test-utils-playwright/src/admin/set-option.ts
@@ -5,16 +5,10 @@ import type { Admin } from './';
 
 export async function setOption( this: Admin, setting: string, value: string ) {
 	await this.visitAdminPage( 'options.php', '' );
-	await this.page.waitForSelector( `#${ setting }` );
 	const previousValue = await this.page.inputValue( `#${ setting }` );
 
-	await this.page.focus( `#${ setting }` );
-	await this.pageUtils.pressKeyWithModifier( 'primary', 'a' );
-	await this.page.type( `#${ setting }`, value );
+	await this.page.fill( `#${ setting }`, value );
 
-	await Promise.all( [
-		this.page.click( '#Update' ),
-		this.page.waitForNavigation( { waitUntil: 'networkidle' } ),
-	] );
+	await this.page.click( '#Update' );
 	return previousValue;
 }
