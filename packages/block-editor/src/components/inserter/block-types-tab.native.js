7b14955186b017ac1aa2c2e8e8f8f05faef17b82
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-07-08 13:47:54 -0500

    chore: Explore native block inserter categories

--- packages/block-editor/src/components/inserter/block-types-tab.native.js
+++ packages/block-editor/src/components/inserter/block-types-tab.native.js
@@ -7,13 +7,23 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import BlockTypesList from '../block-types-list';
+import InserterPanel from './panel';
 import useClipboardBlock from './hooks/use-clipboard-block';
 import { store as blockEditorStore } from '../../store';
 import useBlockTypeImpressions from './hooks/use-block-type-impressions';
 import { filterInserterItems } from './utils';
+import useBlockTypesState from './hooks/use-block-types-state';
 
-function BlockTypesTab( { onSelect, rootClientId, listProps } ) {
+function BlockTypesTab( {
+	onSelect,
+	onInsert = () => {},
+	rootClientId,
+	listProps,
+} ) {
 	const clipboardBlock = useClipboardBlock( rootClientId );
+	const [ _items, categories, collections, onSelectItem ] =
+		useBlockTypesState( rootClientId, onInsert );
+	console.log( '>>>', { categories } );
 
 	const { blockTypes } = useSelect(
 		( select ) => {
@@ -40,12 +50,18 @@ function BlockTypesTab( { onSelect, rootClientId, listProps } ) {
 	};
 
 	return (
-		<BlockTypesList
-			name="Blocks"
-			items={ items }
-			onSelect={ handleSelect }
-			listProps={ listProps }
-		/>
+		<InserterPanel
+			key={ categories[ categories.length - 1 ].slug }
+			title={ categories[ categories.length - 1 ].title }
+			icon={ categories[ categories.length - 1 ].icon }
+		>
+			<BlockTypesList
+				name="Blocks"
+				items={ items }
+				onSelect={ handleSelect }
+				listProps={ listProps }
+			/>
+		</InserterPanel>
 	);
 }
 
