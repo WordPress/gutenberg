b3fcb8b2027d9ad720a007eabab00e09daaf6798
David Arenas <david.arenas@automattic.com>
2022-07-13 20:03:57 +0200

    Return a number in publishPost

--- packages/e2e-test-utils-playwright/src/editor/publish-post.ts
+++ packages/e2e-test-utils-playwright/src/editor/publish-post.ts
@@ -33,5 +33,6 @@ export async function publishPost( this: Editor ) {
 	const urlString = await this.page.inputValue( 'text="Post address"' );
 	const url = new URL( urlString );
 	const postId = url.searchParams.get( 'p' );
-	return postId;
+
+	return typeof postId === 'string' ? parseInt( postId, 10 ) : null;
 }
