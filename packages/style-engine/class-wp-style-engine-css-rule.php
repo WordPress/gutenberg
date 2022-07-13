0a6b4ef50308785eeab8ac887f3c0dc641710c87
Ari Stathopoulos <aristath@gmail.com>
2022-07-13 11:10:49 +0300

    remove parent relations for rules

--- packages/style-engine/class-wp-style-engine-css-rule.php
+++ packages/style-engine/class-wp-style-engine-css-rule.php
@@ -26,13 +26,6 @@ class WP_Style_Engine_CSS_Rule {
 	protected $selector;
 
 	/**
-	 * The parent rule.
-	 *
-	 * @var WP_Style_Engine_CSS_Rule
-	 */
-	protected $parent;
-
-	/**
 	 * The selector declarations.
 	 *
 	 * Contains an array of WP_Style_Engine_CSS_Declarations objects.
@@ -45,12 +38,10 @@ class WP_Style_Engine_CSS_Rule {
 	 * Constructor
 	 *
 	 * @param string                             $selector     The CSS selector.
-	 * @param WP_Style_Engine_CSS_Rule|null      $parent       The parent rule.
 	 * @param WP_Style_Engine_CSS_Declarations[] $declarations An array of WP_Style_Engine_CSS_Declarations objects.
 	 */
-	public function __construct( $selector = '', $parent = null, $declarations = array() ) {
+	public function __construct( $selector = '', $declarations = array() ) {
 		$this->set_selector( $selector );
-		$this->set_parent( $parent );
 		$this->set_declarations( $declarations );
 	}
 
@@ -67,18 +58,6 @@ class WP_Style_Engine_CSS_Rule {
 	}
 
 	/**
-	 * Set the parent selector.
-	 *
-	 * @param WP_Style_Engine_CSS_Rule $parent The parent rule.
-	 */
-	public function set_parent( $parent ) {
-		if ( ! $parent instanceof WP_Style_Engine_CSS_Rule ) {
-			return;
-		}
-		$this->parent = $parent;
-	}
-
-	/**
 	 * Set the declarations.
 	 *
 	 * @param WP_Style_Engine_CSS_Declarations[] $declarations An array of WP_Style_Engine_CSS_Declarations objects.
@@ -91,24 +70,12 @@ class WP_Style_Engine_CSS_Rule {
 	}
 
 	/**
-	 * Get the parent selector.
-	 *
-	 * @return string
-	 */
-	public function get_parent_selector() {
-		return $this->parent ? $this->parent->get_selector() : '';
-	}
-
-	/**
 	 * Get the full selector.
 	 *
 	 * @return string
 	 */
 	public function get_selector() {
-		if ( 0 === strpos( $this->selector, '&' ) ) {
-			return $this->get_parent_selector() . substr( $this->selector, 1 );
-		}
-		return $this->get_parent_selector() . ' ' . $this->selector;
+		return $this->selector;
 	}
 
 	/**
--- packages/style-engine/class-wp-style-engine-css-rules-store.php
+++ packages/style-engine/class-wp-style-engine-css-rules-store.php
@@ -54,87 +54,11 @@ class WP_Style_Engine_CSS_Rules_Store {
 			return;
 		}
 
-		// If the selector is already stored, return it.
-		if ( isset( static::$rules[ $selector ] ) ) {
-			return static::$rules[ $selector ];
-		}
-
-		// Create the selector if it doesn't exist.
-		// To do that, first deconstruct the selector to its parts
-		// and build the hierarchy of selector objects prior to storing it.
-		$selector_parts = static::deconstruct_selector( $selector );
-		$full_selector  = '';
-		foreach ( $selector_parts as $key => $selector_part ) {
-			// Get the parent object if needed.
-			$parent = 0 === $key ? null : static::get_rule( $full_selector );
-			// Compile the full selector for the store.
-			$full_selector .= ( 0 === strpos( $selector_part, '&' ) ) ? substr( $selector_part, 1 ) : ' ' . $selector_part;
-			if ( 0 === $key ) {
-				$full_selector = ltrim( $full_selector );
-			}
-			// Create the rule if it doesn't exist.
-			if ( empty( static::$rules[ $full_selector ] ) ) {
-				static::$rules[ $full_selector ] = new WP_Style_Engine_CSS_Rule( $selector_part, $parent );
-			}
+		// Create the rule if it doesn't exist.
+		if ( empty( static::$rules[ $selector ] ) ) {
+			static::$rules[ $selector ] = new WP_Style_Engine_CSS_Rule( $selector );
 		}
 
 		return static::$rules[ $selector ];
-	}
-
-	/**
-	 * Deconstruct a selector to its parts.
-	 *
-	 * @static
-	 *
-	 * @param string $selector The CSS selector.
-	 *
-	 * @return array An array of selector parts.
-	 */
-	public static function deconstruct_selector( $selector ) {
-		$selector = trim( $selector );
-
-		// Split selector parts on space characters.
-		$parts = explode( ' ', $selector );
-		$parts = array_map( 'trim', $parts );
-
-		// Remove empty parts.
-		$parts = array_filter( $parts );
-
-		// Split parts on ':' characters.
-		$parts = static::deconstruct_selector_array_on_character( $parts, ':' );
-
-		return $parts;
-	}
-
-	/**
-	 * Expands and re-structures an array based on a character.
-	 *
-	 * @static
-	 *
-	 * @param array  $array The array to expand.
-	 * @param string $character The character to expand on.
-	 *
-	 * @return array The expanded array.
-	 */
-	public static function deconstruct_selector_array_on_character( $array, $character ) {
-		$new_array = array();
-		foreach ( $array as $value ) {
-			if ( strpos( $value, $character ) === false ) {
-				$new_array[] = $value;
-				continue;
-			}
-
-			// Use "SPLIT_CHARACTER" as a delimiter.
-			$value = str_replace( $character, "SPLIT_CHARACTER$character", $value );
-
-			// Pseudo-selectors are a special case.
-			if ( ':' === $character ) {
-				$value = str_replace( ':', '&:', $value );
-			}
-
-			$value     = explode( 'SPLIT_CHARACTER', $value );
-			$new_array = array_merge( $new_array, $value );
-		}
-		return $new_array;
 	}
 }
