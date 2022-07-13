434967fc2c5da0cd5863a0b45501f2b1519ff9d9
Petter Walbø Johnsgård <petter@dekode.no>
2022-07-13 21:20:14 +0200

    Apply suggestions from code review
    Co-authored-by: Lena Morita <lena@jaguchi.com>

--- packages/components/src/swatch/index.tsx
+++ packages/components/src/swatch/index.tsx
@@ -9,6 +9,8 @@ import { swatch } from '@wordpress/icons';
 import Icon from '../icon';
 import type { SwatchProps } from './types';
 
+// This component will be deprecated. Use `ColorIndicator` instead.
+// TODO: Consolidate this component with `ColorIndicator`.
 function Swatch( { fill }: SwatchProps ) {
 	return fill ? (
 		<span className="components-swatch" style={ { background: fill } } />
