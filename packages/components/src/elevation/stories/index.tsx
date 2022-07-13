9e63d4b1a986f5aa281c63ac3c256319f5beb80b
Petter Walbø Johnsgård <petter@dekode.no>
2022-07-13 21:08:02 +0200

    Update storybook

--- packages/components/src/elevation/stories/index.tsx
+++ packages/components/src/elevation/stories/index.tsx
@@ -26,6 +26,20 @@ export default meta;
 
 const Template: ComponentStory< typeof Elevation > = ( args ) => {
 	return (
+		<div
+			style={ {
+				width: 150,
+				height: 150,
+				position: 'relative',
+			} }
+		>
+			<Elevation { ...args } />
+		</div>
+	);
+};
+
+const InteractiveTemplate: ComponentStory< typeof Elevation > = ( args ) => {
+	return (
 		<button
 			style={ {
 				border: 0,
@@ -44,7 +58,28 @@ const Template: ComponentStory< typeof Elevation > = ( args ) => {
 export const Default: ComponentStory< typeof Elevation > = Template.bind( {} );
 Default.args = {
 	value: 5,
-	hover: 5,
+};
+
+/**
+ * Enable the `isInteractive` prop to automatically generate values
+ * for the hover/active/focus states.
+ */
+export const WithInteractive: ComponentStory< typeof Elevation > =
+	InteractiveTemplate.bind( {} );
+WithInteractive.args = {
+	...Default.args,
+	isInteractive: true,
+};
+
+/**
+ * You can also provide custom values for the hover/active/focus states
+ * instead of using the `isInteractive` prop.
+ */
+export const WithCustomInteractive: ComponentStory< typeof Elevation > =
+	InteractiveTemplate.bind( {} );
+WithCustomInteractive.args = {
+	...Default.args,
+	hover: 7,
 	active: 1,
 	focus: 10,
 };
