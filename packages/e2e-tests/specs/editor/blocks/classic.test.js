4513c66c4baae5cdc73c909095b08cbbe50adb35
Alex Stine <alex.stine@yourtechadvisors.com>
2022-07-14 11:23:49 -0500

    Unmodify E2E test.

--- packages/e2e-tests/specs/editor/blocks/classic.test.js
+++ packages/e2e-tests/specs/editor/blocks/classic.test.js
@@ -31,7 +31,7 @@ describe( 'Classic', () => {
 		await page.focus( '.mce-content-body' );
 		await page.keyboard.type( 'test' );
 		// Move focus away.
-		await page.keyboard.press( 'ArrowUp' );
+		await pressKeyWithModifier( 'shift', 'Tab' );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
@@ -82,7 +82,7 @@ describe( 'Classic', () => {
 		await page.waitForSelector( '.mce-content-body img' );
 
 		// Move focus away and verify gallery was inserted.
-		await page.keyboard.press( 'Tab' );
+		await pressKeyWithModifier( 'shift', 'Tab' );
 		expect( await getEditedPostContent() ).toMatch(
 			/\[gallery ids=\"\d+\"\]/
 		);
