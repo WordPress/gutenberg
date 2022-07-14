32d49ebe31aea513116f2e944c67b65c39aaf84a
Robert Anderson <robert@noisysocks.com>
2022-07-14 14:09:21 +1000

    Update unit tests

--- packages/editor/src/components/post-type-support-check/test/index.js
+++ packages/editor/src/components/post-type-support-check/test/index.js
@@ -4,79 +4,110 @@
 import { create } from 'react-test-renderer';
 
 /**
+ * WordPress dependencies
+ */
+import { createRegistry, RegistryProvider } from '@wordpress/data';
+
+/**
  * Internal dependencies
  */
-import { PostTypeSupportCheck } from '../';
+import PostTypeSupportCheck from '../';
+
+function createMockRegistry( postType ) {
+	return createRegistry( {
+		'core/editor': {
+			selectors: {
+				getEditedPostAttribute: () => 'post',
+			},
+			reducer: ( state = {} ) => state,
+		},
+		core: {
+			selectors: {
+				getPostType: () => postType,
+			},
+			reducer: ( state = {} ) => state,
+		},
+	} );
+}
 
 describe( 'PostTypeSupportCheck', () => {
 	it( 'renders its children when post type is not known', () => {
-		let postType;
+		const registry = createMockRegistry( null );
+
 		const tree = create(
-			<PostTypeSupportCheck postType={ postType } supportKeys="title">
-				Supported
-			</PostTypeSupportCheck>
+			<RegistryProvider value={ registry }>
+				<PostTypeSupportCheck supportKeys="title">
+					Supported
+				</PostTypeSupportCheck>
+			</RegistryProvider>
 		);
 
 		expect( tree.toJSON() ).toBe( 'Supported' );
 	} );
 
 	it( 'does not render its children when post type is known and not supports', () => {
-		const postType = {
+		const registry = createMockRegistry( {
 			supports: {},
-		};
+		} );
+
 		const tree = create(
-			<PostTypeSupportCheck postType={ postType } supportKeys="title">
-				Supported
-			</PostTypeSupportCheck>
+			<RegistryProvider value={ registry }>
+				<PostTypeSupportCheck supportKeys="title">
+					Supported
+				</PostTypeSupportCheck>
+			</RegistryProvider>
 		);
 
 		expect( tree.toJSON() ).toBe( null );
 	} );
 
 	it( 'renders its children when post type is known and supports', () => {
-		const postType = {
+		const registry = createMockRegistry( {
 			supports: {
 				title: true,
 			},
-		};
+		} );
+
 		const tree = create(
-			<PostTypeSupportCheck postType={ postType } supportKeys="title">
-				Supported
-			</PostTypeSupportCheck>
+			<RegistryProvider value={ registry }>
+				<PostTypeSupportCheck supportKeys="title">
+					Supported
+				</PostTypeSupportCheck>
+			</RegistryProvider>
 		);
 
 		expect( tree.toJSON() ).toBe( 'Supported' );
 	} );
 
 	it( 'renders its children if some of keys supported', () => {
-		const postType = {
+		const registry = createMockRegistry( {
 			supports: {
 				title: true,
 			},
-		};
+		} );
+
 		const tree = create(
-			<PostTypeSupportCheck
-				postType={ postType }
-				supportKeys={ [ 'title', 'thumbnail' ] }
-			>
-				Supported
-			</PostTypeSupportCheck>
+			<RegistryProvider value={ registry }>
+				<PostTypeSupportCheck supportKeys={ [ 'title', 'thumbnail' ] }>
+					Supported
+				</PostTypeSupportCheck>
+			</RegistryProvider>
 		);
 
 		expect( tree.toJSON() ).toBe( 'Supported' );
 	} );
 
 	it( 'does not render its children if none of keys supported', () => {
-		const postType = {
+		const registry = createMockRegistry( {
 			supports: {},
-		};
+		} );
+
 		const tree = create(
-			<PostTypeSupportCheck
-				postType={ postType }
-				supportKeys={ [ 'title', 'thumbnail' ] }
-			>
-				Supported
-			</PostTypeSupportCheck>
+			<RegistryProvider value={ registry }>
+				<PostTypeSupportCheck supportKeys={ [ 'title', 'thumbnail' ] }>
+					Supported
+				</PostTypeSupportCheck>
+			</RegistryProvider>
 		);
 
 		expect( tree.toJSON() ).toBe( null );
