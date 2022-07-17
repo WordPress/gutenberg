e8c396fea144d2ec0d442626f014fcee3d7169f5
Sunil Prajapati <sdprajapati999@gmail.com>
2022-07-17 11:13:22 +0530

    Calling render method from build

--- phpunit/blocks/render-block-post-title-test.php
+++ phpunit/blocks/render-block-post-title-test.php
@@ -63,34 +63,34 @@ class Tests_Blocks_Render_Post_Title extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test render_block_core_post_title() method.
+	 * Test gutenberg_render_block_core_post_title() method.
 	 *
-	 * @covers ::render_block_core_post_title
+	 * @covers ::gutenberg_render_block_core_post_title
 	 */
-	public function test_render_block_core_post_title_without_post() {
+	public function test_gutenberg_render_block_core_post_title_without_post() {
 		$block = new stdClass();
 
 		// call render method with block context.
-		$rendered = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertEmpty( $rendered );
 
 		$block->context = array( 'postId' => 0 );
-		$rendered       = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered       = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertEmpty( $rendered );
 	}
 
 	/**
-	 * Test render_block_core_post_title() method.
+	 * Test gutenberg_render_block_core_post_title() method.
 	 *
-	 * @covers ::render_block_core_post_title
+	 * @covers ::gutenberg_render_block_core_post_title
 	 */
-	public function test_render_block_core_post_title() {
+	public function test_gutenberg_render_block_core_post_title() {
 
 		$block           = new stdClass();
 		$GLOBALS['post'] = self::$post;
 		$block->context  = array( 'postId' => self::$post->ID );
 
-		$rendered = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertNotNull( $rendered );
 		$this->assertStringContainsString( 'Post title block Unit Test', $rendered );
 		$this->assertStringContainsString( '</p', $rendered );
@@ -100,18 +100,18 @@ class Tests_Blocks_Render_Post_Title extends WP_UnitTestCase {
 		$this->assertStringNotContainsString( 'has-text-align', $rendered );
 
 		self::$attributes['level'] = '1';
-		$rendered                  = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered                  = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertStringContainsString( '<h1', $rendered );
 		$this->assertStringContainsString( '</h1>', $rendered );
 
 		self::$attributes['textAlign'] = 'left';
-		$rendered                      = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered                      = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertStringContainsString( 'has-text-align-left', $rendered );
 
 		self::$attributes['isLink']     = true;
 		self::$attributes['linkTarget'] = '_blank';
 		self::$attributes['rel']        = 'no-relative';
-		$rendered                       = render_block_core_post_title( self::$attributes, '', $block );
+		$rendered                       = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
 		$this->assertStringContainsString( get_permalink( self::$post->ID ), $rendered );
 		$this->assertStringContainsString( '<a href=', $rendered );
 		$this->assertStringContainsString( '_blank', $rendered );
