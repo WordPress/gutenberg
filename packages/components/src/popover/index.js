3bb40fc2c0920ce643247e91f4bfbdb96bb29ec1
Marco Ciampini <marco.ciampo@gmail.com>
2022-07-18 15:51:05 +0200

    Add default value to __unstableForcePosition prop

--- packages/components/src/popover/index.js
+++ packages/components/src/popover/index.js
@@ -143,7 +143,7 @@ const Popover = (
 		onFocusOutside,
 		__unstableSlotName = SLOT_NAME,
 		__unstableObserveElement,
-		__unstableForcePosition,
+		__unstableForcePosition = false,
 		__unstableShift = false,
 		...contentProps
 	},
