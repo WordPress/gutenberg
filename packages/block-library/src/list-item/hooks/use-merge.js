3ad7f0a4e509b9eac22c40fd8f71d94870f3c2af
Ella van Durpe <ella@vandurpe.com>
2022-07-20 16:43:15 +0200

    List v2: add forward delete

--- packages/block-library/src/list-item/hooks/use-merge.js
+++ packages/block-library/src/list-item/hooks/use-merge.js
@@ -4,6 +4,13 @@
 import { useRegistry, useDispatch, useSelect } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 
+/**
+ * Internal dependencies
+ */
+import useOutdentListItem from './use-outdent-list-item';
+
+import { name as listItemName } from '../block.json';
+
 export default function useMerge( clientId ) {
 	const registry = useRegistry();
 	const {
@@ -11,9 +18,11 @@ export default function useMerge( clientId ) {
 		getNextBlockClientId,
 		getBlockOrder,
 		getBlockRootClientId,
+		getBlockName,
 	} = useSelect( blockEditorStore );
 	const { mergeBlocks, moveBlocksToPosition } =
 		useDispatch( blockEditorStore );
+	const [ , outdentListItem ] = useOutdentListItem( clientId );
 
 	function getTrailing( id ) {
 		const order = getBlockOrder( id );
@@ -25,24 +34,56 @@ export default function useMerge( clientId ) {
 		return getTrailing( order[ order.length - 1 ] );
 	}
 
+	function getParentListItem( id ) {
+		const listId = getBlockRootClientId( id );
+		const parentListItem = getBlockRootClientId( listId );
+		if ( ! parentListItem ) return;
+		if ( getBlockName( parentListItem ) !== listItemName ) return;
+		return parentListItem;
+	}
+
+	/**
+	 * Return the next list item with respect to the given list item. If none,
+	 * return the next list item of the parent list item if it exists.
+	 *
+	 * @param {string} id A list item client ID.
+	 * @return {string?} The client ID of the next list item.
+	 */
+	function _getNext( id ) {
+		const next = getNextBlockClientId( id );
+		if ( next ) return next;
+		const parentListItem = getParentListItem( id );
+		if ( ! parentListItem ) return;
+		return _getNext( parentListItem );
+	}
+
+	/**
+	 * Given a client ID, return the client ID of the list item on the next
+	 * line, regardless of indentation level.
+	 *
+	 * @param {string} id The client ID of the current list item.
+	 * @return {string?} The client ID of the next list item.
+	 */
 	function getNext( id ) {
-		return (
-			getNextBlockClientId( id ) || getNext( getBlockRootClientId( id ) )
-		);
+		const order = getBlockOrder( id );
+
+		// If the list item does not have a nested list, return the next list
+		// item.
+		if ( ! order.length ) {
+			return _getNext( id );
+		}
+
+		// Get the first list item in the nested list.
+		return getBlockOrder( order[ 0 ] )[ 0 ];
 	}
 
 	return ( forward ) => {
 		if ( forward ) {
-			// Forward "merging" (forward delete) should be equal pressing
-			// Backspace from the next item. If the next item is not at the top
-			// level, the item should be outdented instead of merged.
-			if ( getBlockOrder( clientId ).length ) {
-				// Should be implemented in `./use-backspace`.
-				return;
-			}
-
 			const nextBlockClientId = getNext( clientId );
-			if ( nextBlockClientId ) {
+			if ( ! nextBlockClientId ) return;
+			if ( getParentListItem( nextBlockClientId ) ) {
+				outdentListItem( nextBlockClientId );
+			} else {
 				registry.batch( () => {
 					moveBlocksToPosition(
 						getBlockOrder( nextBlockClientId ),
@@ -56,7 +97,9 @@ export default function useMerge( clientId ) {
 			// Merging is only done from the top level. For lowel levels, the
 			// list item is outdented instead.
 			const previousBlockClientId = getPreviousBlockClientId( clientId );
-			if ( previousBlockClientId ) {
+			if ( getParentListItem( clientId ) ) {
+				outdentListItem( clientId );
+			} else if ( previousBlockClientId ) {
 				const trailingClientId = getTrailing( previousBlockClientId );
 				registry.batch( () => {
 					moveBlocksToPosition(
--- packages/block-library/src/list-item/hooks/use-outdent-list-item.js
+++ packages/block-library/src/list-item/hooks/use-outdent-list-item.js
@@ -44,78 +44,84 @@ export default function useOutdentListItem( clientId ) {
 
 	return [
 		canOutdent,
-		useCallback( () => {
-			const _hasMultiSelection = hasMultiSelection();
-			const clientIds = _hasMultiSelection
-				? getMultiSelectedBlockClientIds()
-				: [ clientId ];
+		useCallback(
+			( _clientId = clientId ) => {
+				const _hasMultiSelection = hasMultiSelection();
+				const clientIds = _hasMultiSelection
+					? getMultiSelectedBlockClientIds()
+					: [ _clientId ];
 
-			const selectionStart = getSelectionStart();
-			const selectionEnd = getSelectionEnd();
+				const listParentId = getBlockRootClientId( _clientId );
+				const listAttributes = getBlockAttributes( listParentId );
+				const listItemParentId = getBlockRootClientId( listParentId );
+				const listItemParentAttributes =
+					getBlockAttributes( listItemParentId );
 
-			const listParentId = getBlockRootClientId( clientId );
-			const listAttributes = getBlockAttributes( listParentId );
-			const listItemParentId = getBlockRootClientId( listParentId );
-			const listItemParentAttributes =
-				getBlockAttributes( listItemParentId );
+				const firstIndex = getBlockIndex( first( clientIds ) );
+				const lastIndex = getBlockIndex( last( clientIds ) );
+				const siblingBlocks = getBlock( listParentId ).innerBlocks;
+				const previousSiblings = siblingBlocks.slice( 0, firstIndex );
+				const afterSiblings = siblingBlocks.slice( lastIndex + 1 );
 
-			const firstIndex = getBlockIndex( first( clientIds ) );
-			const lastIndex = getBlockIndex( last( clientIds ) );
-			const siblingBlocks = getBlock( listParentId ).innerBlocks;
-			const previousSiblings = siblingBlocks.slice( 0, firstIndex );
-			const afterSiblings = siblingBlocks.slice( lastIndex + 1 );
-
-			// Create a new parent list item block with just the siblings
-			// that existed before the first child item being outdent.
-			const newListItemParent = createListItem(
-				listItemParentAttributes,
-				listAttributes,
-				previousSiblings
-			);
-
-			const lastBlock = getBlock( last( clientIds ) );
-			const childList = lastBlock.innerBlocks[ 0 ];
-			const childItems = childList?.innerBlocks || [];
-			const hasChildItems = !! childItems.length;
-
-			const newBlocksExcludingLast = clientIds
-				.slice( 0, -1 )
-				.map( ( _clientId ) => cloneBlock( getBlock( _clientId ) ) );
-
-			// Create a new list item block whose attributes are equal to the
-			// last block being outdent and whose children are the children that it had (if any)
-			// followed by the siblings that existed after it.
-			const newLastItem = createListItem(
-				lastBlock.attributes,
-				hasChildItems ? childList.attributes : listAttributes,
-				[ ...childItems, ...afterSiblings ]
-			);
-
-			// Replace the parent list item block, with a new block containing
-			// the previous siblings before the first block being outdent,
-			// followed by the blocks being outdent with the after siblings added
-			// as children of the last block.
-			replaceBlocks(
-				[ listItemParentId ],
-				[ newListItemParent, ...newBlocksExcludingLast, newLastItem ]
-			);
-
-			// Restore the selection state.
-			if ( ! _hasMultiSelection ) {
-				selectionChange(
-					newLastItem.clientId,
-					selectionEnd.attributeKey,
-					selectionEnd.clientId === selectionStart.clientId
-						? selectionStart.offset
-						: selectionEnd.offset,
-					selectionEnd.offset
+				// Create a new parent list item block with just the siblings
+				// that existed before the first child item being outdent.
+				const newListItemParent = createListItem(
+					listItemParentAttributes,
+					listAttributes,
+					previousSiblings
 				);
-			} else {
-				multiSelect(
-					first( newBlocksExcludingLast ).clientId,
-					newLastItem.clientId
+
+				const lastBlock = getBlock( last( clientIds ) );
+				const childList = lastBlock.innerBlocks[ 0 ];
+				const childItems = childList?.innerBlocks || [];
+				const hasChildItems = !! childItems.length;
+
+				const newBlocksExcludingLast = clientIds
+					.slice( 0, -1 )
+					.map( ( id ) => cloneBlock( getBlock( id ) ) );
+
+				// Create a new list item block whose attributes are equal to the
+				// last block being outdent and whose children are the children that it had (if any)
+				// followed by the siblings that existed after it.
+				const newLastItem = createListItem(
+					lastBlock.attributes,
+					hasChildItems ? childList.attributes : listAttributes,
+					[ ...childItems, ...afterSiblings ]
 				);
-			}
-		}, [ clientId ] ),
+
+				// Replace the parent list item block, with a new block containing
+				// the previous siblings before the first block being outdent,
+				// followed by the blocks being outdent with the after siblings added
+				// as children of the last block.
+				replaceBlocks(
+					[ listItemParentId ],
+					[
+						newListItemParent,
+						...newBlocksExcludingLast,
+						newLastItem,
+					]
+				);
+
+				// Restore the selection state.
+				if ( ! _hasMultiSelection ) {
+					const selectionStart = getSelectionStart();
+					const selectionEnd = getSelectionEnd();
+					selectionChange(
+						newLastItem.clientId,
+						selectionEnd.attributeKey,
+						selectionEnd.clientId === selectionStart.clientId
+							? selectionStart.offset
+							: selectionEnd.offset,
+						selectionEnd.offset
+					);
+				} else {
+					multiSelect(
+						first( newBlocksExcludingLast ).clientId,
+						newLastItem.clientId
+					);
+				}
+			},
+			[ clientId ]
+		),
 	];
 }
