cca762e91ce6d1561bbe0ba2cfc68a6e62821ec5
Ella van Durpe <ella@vandurpe.com>
2022-07-21 14:10:31 +0200

    Add caret format

--- packages/format-library/src/default-formats.js
+++ packages/format-library/src/default-formats.js
@@ -13,7 +13,39 @@ import { subscript } from './subscript';
 import { superscript } from './superscript';
 import { keyboard } from './keyboard';
 
+const caret = {
+	name: 'asblocks/caret',
+	title: 'Example',
+	tagName: 'mark',
+	className: 'caret',
+	attributes: {
+		id: 'id',
+		className: 'class',
+	},
+	edit() {
+		return null;
+	},
+	__experimentalGetPropsForEditableTreePreparation(
+		select,
+		{ blockClientId }
+	) {
+		// Just calling this, will make the text selection to be unreliable
+		// If you comment out this line, then everything works fine.
+		select( 'core/block-editor' ).getBlockName( blockClientId );
+
+		return {
+			carets: [],
+		};
+	},
+	__experimentalCreatePrepareEditableTree() {
+		return ( formats ) => {
+			return formats;
+		};
+	},
+};
+
 export default [
+	caret,
 	bold,
 	code,
 	image,
