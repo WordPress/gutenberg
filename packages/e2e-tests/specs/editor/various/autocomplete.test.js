be77fc52a8e3a7e3f2ba98aea55a4131b217af90
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-07-21 16:38:48 -0400

    add newline regression test

--- packages/e2e-tests/specs/editor/various/autocomplete.test.js
+++ packages/e2e-tests/specs/editor/various/autocomplete.test.js
@@ -4,9 +4,12 @@
 import {
 	activatePlugin,
 	deactivatePlugin,
+	clickBlockAppender,
 	createNewPost,
 	createUser,
 	deleteUser,
+	getEditedPostContent,
+	pressKeyTimes,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'autocomplete mentions', () => {
@@ -25,5 +28,23 @@ describe( 'autocomplete mentions', () => {
 
 	afterAll( async () => {
 		await deleteUser( 'testuser' );
+	} );
+
+	it( 'should allow newlines after a completion', async () => {
+		await clickBlockAppender();
+		await page.keyboard.type( 'I would like an ~o' );
+		await page.waitForSelector( '.components-autocomplete__result' );
+		await pressKeyTimes( 'Enter', 2 );
+		await page.keyboard.type( '...and this is a new paragraph block.' );
+
+		expect( await getEditedPostContent() ).toMatchInlineSnapshot( `
+		"<!-- wp:paragraph -->
+		<p>I would like an 🍊</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p>...and this is a new paragraph block.</p>
+		<!-- /wp:paragraph -->"
+	` );
 	} );
 } );
