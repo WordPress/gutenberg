de75e31a457dda75db87722231f021641ef48f36
Ari Stathopoulos <aristath@gmail.com>
2022-07-21 13:25:31 +0300

    Allow CSS variables in the Declarations class

--- packages/style-engine/class-wp-style-engine-css-declarations.php
+++ packages/style-engine/class-wp-style-engine-css-declarations.php
@@ -122,6 +122,11 @@ class WP_Style_Engine_CSS_Declarations {
 		$declarations_array  = $this->get_declarations();
 		$declarations_output = '';
 		foreach ( $declarations_array as $property => $value ) {
+			// Account for CSS variables.
+			if ( 0 === strpos( $property, '--' ) ) {
+				$declarations_output .= esc_html( "{$property}: {$value};" ) . '; ';
+				continue;
+			}
 			$filtered_declaration = esc_html( safecss_filter_attr( "{$property}: {$value}" ) );
 			if ( $filtered_declaration ) {
 				$declarations_output .= $filtered_declaration . '; ';
