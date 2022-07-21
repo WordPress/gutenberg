2360f68bb9c81328f61a1848dd66024e2e270a2b
Bernie Reiter <ockham@raz.or.at>
2022-07-21 14:43:51 +0200

    Deduplicate

--- packages/block-library/src/comments/edit/comments-legacy.js
+++ packages/block-library/src/comments/edit/comments-legacy.js
@@ -11,11 +11,8 @@ import {
 	BlockControls,
 	Warning,
 	useBlockProps,
-	store as blockEditorStore,
 } from '@wordpress/block-editor';
-import { __, sprintf } from '@wordpress/i18n';
-import { useSelect } from '@wordpress/data';
-import { useEntityProp, store as coreStore } from '@wordpress/core-data';
+import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 
 /**
@@ -30,56 +27,9 @@ export default function CommentsLegacy( {
 } ) {
 	const { textAlign } = attributes;
 
-	const [ commentStatus ] = useEntityProp(
-		'postType',
-		postType,
-		'comment_status',
-		postId
-	);
-
-	const { defaultCommentStatus } = useSelect(
-		( select ) =>
-			select( blockEditorStore ).getSettings()
-				.__experimentalDiscussionSettings
-	);
-
-	const isSiteEditor = postType === undefined || postId === undefined;
-
-	const postTypeSupportsComments = useSelect( ( select ) =>
-		postType
-			? !! select( coreStore ).getPostType( postType )?.supports.comments
-			: false
-	);
-
-	let warning = __(
+	const warning = __(
 		"Comments block: You're currently using this block in legacy mode. The following is just a placeholder, not a real comment. The final styling may differ because it also depends on the current theme. For better compatibility with the Block Editor, please consider switching the block to its editable mode."
 	);
-	let showPlacholder = true;
-
-	if ( ! isSiteEditor && 'open' !== commentStatus ) {
-		if ( 'closed' === commentStatus ) {
-			warning = sprintf(
-				/* translators: 1: Post type (i.e. "post", "page") */
-				__(
-					'Post Comments block: Comments to this %s are not allowed.'
-				),
-				postType
-			);
-			showPlacholder = false;
-		} else if ( ! postTypeSupportsComments ) {
-			warning = sprintf(
-				/* translators: 1: Post type (i.e. "post", "page") */
-				__(
-					'Post Comments block: Comments for this post type (%s) are not enabled.'
-				),
-				postType
-			);
-			showPlacholder = false;
-		} else if ( 'open' !== defaultCommentStatus ) {
-			warning = __( 'Post Comments block: Comments are not enabled.' );
-			showPlacholder = false;
-		}
-	}
 
 	const removeLegacy = () => {
 		setAttributes( { legacy: false } );
@@ -110,10 +60,7 @@ export default function CommentsLegacy( {
 
 			<div { ...blockProps }>
 				<Warning actions={ actions }>{ warning }</Warning>
-
-				{ showPlacholder && (
-					<Placeholder postId={ postId } postType={ postType } />
-				) }
+				<Placeholder postId={ postId } postType={ postType } />
 			</div>
 		</>
 	);
