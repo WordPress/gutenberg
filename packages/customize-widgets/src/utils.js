e5d22fab1667ca2988bd3d1a6db732c42d345f52
Daniel Richards <daniel.richards@automattic.com>
2022-07-22 16:05:58 +0800

    Shorten option name

--- packages/customize-widgets/src/utils.js
+++ packages/customize-widgets/src/utils.js
@@ -107,7 +107,7 @@ export function widgetToBlock( { id, idBase, number, instance } ) {
 
 	if ( idBase === 'block' ) {
 		const parsedBlocks = parse( raw.content, {
-			__unstableFreeformAutoP: false,
+			__unstableAutoP: false,
 		} );
 		block = parsedBlocks.length
 			? parsedBlocks[ 0 ]
