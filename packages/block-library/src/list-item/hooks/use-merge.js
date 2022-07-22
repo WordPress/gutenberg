1353915e7cb8f78690db036e5d36d9905c6de6d6
Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
2022-07-22 12:52:25 +0200

    List v2: add forward delete (#42564)

--- packages/block-library/src/list-item/hooks/use-merge.js
+++ packages/block-library/src/list-item/hooks/use-merge.js
@@ -4,6 +4,13 @@
 import { useRegistry, useDispatch, useSelect } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 
+/**
+ * Internal dependencies
+ */
+import useOutdentListItem from './use-outdent-list-item';
+
+import { name as listItemName } from '../block.json';
+
 export default function useMerge( clientId ) {
 	const registry = useRegistry();
 	const {
@@ -11,38 +18,72 @@ export default function useMerge( clientId ) {
 		getNextBlockClientId,
 		getBlockOrder,
 		getBlockRootClientId,
+		getBlockName,
 	} = useSelect( blockEditorStore );
 	const { mergeBlocks, moveBlocksToPosition } =
 		useDispatch( blockEditorStore );
+	const [ , outdentListItem ] = useOutdentListItem( clientId );
 
-	function getTrailing( id ) {
+	function getTrailingId( id ) {
 		const order = getBlockOrder( id );
 
 		if ( ! order.length ) {
 			return id;
 		}
 
-		return getTrailing( order[ order.length - 1 ] );
+		return getTrailingId( order[ order.length - 1 ] );
 	}
 
-	function getNext( id ) {
-		return (
-			getNextBlockClientId( id ) || getNext( getBlockRootClientId( id ) )
-		);
+	function getParentListItemId( id ) {
+		const listId = getBlockRootClientId( id );
+		const parentListItemId = getBlockRootClientId( listId );
+		if ( ! parentListItemId ) return;
+		if ( getBlockName( parentListItemId ) !== listItemName ) return;
+		return parentListItemId;
+	}
+
+	/**
+	 * Return the next list item with respect to the given list item. If none,
+	 * return the next list item of the parent list item if it exists.
+	 *
+	 * @param {string} id A list item client ID.
+	 * @return {string?} The client ID of the next list item.
+	 */
+	function _getNextId( id ) {
+		const next = getNextBlockClientId( id );
+		if ( next ) return next;
+		const parentListItemId = getParentListItemId( id );
+		if ( ! parentListItemId ) return;
+		return _getNextId( parentListItemId );
+	}
+
+	/**
+	 * Given a client ID, return the client ID of the list item on the next
+	 * line, regardless of indentation level.
+	 *
+	 * @param {string} id The client ID of the current list item.
+	 * @return {string?} The client ID of the next list item.
+	 */
+	function getNextId( id ) {
+		const order = getBlockOrder( id );
+
+		// If the list item does not have a nested list, return the next list
+		// item.
+		if ( ! order.length ) {
+			return _getNextId( id );
+		}
+
+		// Get the first list item in the nested list.
+		return getBlockOrder( order[ 0 ] )[ 0 ];
 	}
 
 	return ( forward ) => {
 		if ( forward ) {
-			// Forward "merging" (forward delete) should be equal pressing
-			// Backspace from the next item. If the next item is not at the top
-			// level, the item should be outdented instead of merged.
-			if ( getBlockOrder( clientId ).length ) {
-				// Should be implemented in `./use-backspace`.
-				return;
-			}
-
-			const nextBlockClientId = getNext( clientId );
-			if ( nextBlockClientId ) {
+			const nextBlockClientId = getNextId( clientId );
+			if ( ! nextBlockClientId ) return;
+			if ( getParentListItemId( nextBlockClientId ) ) {
+				outdentListItem( nextBlockClientId );
+			} else {
 				registry.batch( () => {
 					moveBlocksToPosition(
 						getBlockOrder( nextBlockClientId ),
@@ -56,15 +97,17 @@ export default function useMerge( clientId ) {
 			// Merging is only done from the top level. For lowel levels, the
 			// list item is outdented instead.
 			const previousBlockClientId = getPreviousBlockClientId( clientId );
-			if ( previousBlockClientId ) {
-				const trailingClientId = getTrailing( previousBlockClientId );
+			if ( getParentListItemId( clientId ) ) {
+				outdentListItem( clientId );
+			} else if ( previousBlockClientId ) {
+				const trailingId = getTrailingId( previousBlockClientId );
 				registry.batch( () => {
 					moveBlocksToPosition(
 						getBlockOrder( clientId ),
 						clientId,
 						previousBlockClientId
 					);
-					mergeBlocks( trailingClientId, clientId );
+					mergeBlocks( trailingId, clientId );
 				} );
 			}
 		}
