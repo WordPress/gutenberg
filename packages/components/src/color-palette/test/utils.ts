f42ea1ae4bef2a4ba0e63847c46ea42193240f86
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-07-26 14:54:26 -0700

    `ColorPalette`: Display checkered preview background when `value` is transparent (#42232)
    * ColorPalette: `background` is already styled when `value` is 'transparent'
    
    * Use `colord( currentValue ).alpha()` to determine transparency
    
    * Add a test case for `undefined` value
    
    * Add a changelog entry
    
    * Clarify test description
    
    Co-authored-by: Lena Morita <lena@jaguchi.com>
    
    * Updated expected behavior for `undefined` value
    
    Co-authored-by: Lena Morita <lena@jaguchi.com>

--- packages/components/src/color-palette/test/utils.ts
+++ packages/components/src/color-palette/test/utils.ts
@@ -1,7 +1,10 @@
 /**
  * Internal dependencies
  */
-import { extractColorNameFromCurrentValue } from '../';
+import {
+	extractColorNameFromCurrentValue,
+	showTransparentBackground,
+} from '../';
 
 describe( 'ColorPalette: Utils', () => {
 	describe( 'extractColorNameFromCurrentValue', () => {
@@ -19,6 +22,21 @@ describe( 'ColorPalette: Utils', () => {
 				{ name: 'Blue', color: 'var(--blue)' },
 			] );
 			expect( result ).toBe( 'Blue' );
+		} );
+	} );
+	describe( 'showTransparentBackground', () => {
+		test( 'should return true for undefined color values', () => {
+			expect( showTransparentBackground( undefined ) ).toBe( true );
+		} );
+		test( 'should return true for transparent colors', () => {
+			expect( showTransparentBackground( 'transparent' ) ).toBe( true );
+			expect( showTransparentBackground( '#75757500' ) ).toBe( true );
+		} );
+
+		test( 'should return false for non-transparent colors', () => {
+			expect( showTransparentBackground( '#FFF' ) ).toBe( false );
+			expect( showTransparentBackground( '#757575' ) ).toBe( false );
+			expect( showTransparentBackground( '#f5f5f524' ) ).toBe( false ); // 0.14 alpha.
 		} );
 	} );
 } );
