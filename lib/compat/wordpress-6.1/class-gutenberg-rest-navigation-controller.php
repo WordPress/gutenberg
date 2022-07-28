5f4de974953918b0ecd5a7b6b057b8d4fa0ffe8f
Dave Smith <getdavemail@gmail.com>
2022-07-28 14:50:19 +0100

    Use post_name for query instead of ID

---
+++ lib/compat/wordpress-6.1/class-gutenberg-rest-navigation-controller.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * REST API: Gutenberg_REST_Templates_Controller class
+ *
+ * @package    Gutenberg
+ * @subpackage REST_API
+ */
+
+/**
+ * Base Templates REST API Controller.
+ */
+class Gutenberg_REST_Navigation_Controller extends WP_REST_Posts_Controller {
+
+
+	/**
+	 * Registers the controllers routes.
+	 *
+	 * @return void
+	 */
+	public function register_routes() {
+
+		parent::register_routes();
+
+		// Lists/updates a single nav item based on the given id.
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<id>[\/\w-]+)',
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_item' ),
+					'permission_callback' => array( $this, 'get_item_permissions_check' ),
+					'args'                => array(
+						'id' => array(
+							'description' => __( 'The id of a navigation', 'gutenberg' ),
+							'type'        => 'string',
+						),
+					),
+				),
+			)
+		);
+	}
+
+
+	/**
+	 * Overide WP_REST_Posts_Controller function to query for
+	 * `wp_navigation` posts by post_name / slug instead of ID.
+	 *
+	 * @param string $id the slug of the Navigation post.
+	 * @return WP_Post|null
+	 */
+	protected function get_post( $id ) {
+
+		$error = new WP_Error(
+			'rest_post_invalid_id',
+			__( 'Invalid post ID.' ),
+			array( 'status' => 404 )
+		);
+
+		if ( ! is_string( $id ) ) {
+			return $error;
+		}
+
+		$args = array(
+			'name'           => $id, // query by slug
+			'post_type'      => array( $this->post_type ),
+			'nopaging'       => true,
+			'posts_per_page' => '-1',
+		);
+
+		// The Query
+		$post = new WP_Query( $args );
+
+		if ( empty( $post ) || empty( $post->post->ID ) || $this->post_type !== $post->post->post_type ) {
+			return $error;
+		}
+
+		return $post->post;
+	}
+
+
+
+}
