355333b17d24d81866fe90183a8c19799af75ac8
Marco Ciampini <marco.ciampo@gmail.com>
2022-08-03 17:32:26 +0200

    Add position to placement unit tests

--- packages/components/src/popover/test/index.js
+++ packages/components/src/popover/test/index.js
@@ -12,58 +12,83 @@ import { useRef } from '@wordpress/element';
  * Internal dependencies
  */
 import Popover from '../';
+import { positionToPlacement } from '../utils';
 
 describe( 'Popover', () => {
-	afterEach( () => {
-		if ( document.activeElement ) {
-			document.activeElement.blur();
-		}
-	} );
-
-	it( 'should allow focus-on-open behavior to be disabled', () => {
-		expect( document.activeElement ).toBe( document.body );
-
-		act( () => {
-			render( <Popover focusOnMount={ false } /> );
-
-			jest.advanceTimersByTime( 1 );
+	describe( 'Component', () => {
+		afterEach( () => {
+			if ( document.activeElement ) {
+				document.activeElement.blur();
+			}
 		} );
 
-		expect( document.activeElement ).toBe( document.body );
-	} );
+		it( 'should allow focus-on-open behavior to be disabled', () => {
+			expect( document.activeElement ).toBe( document.body );
 
-	it( 'should render content', () => {
-		let result;
-		act( () => {
-			result = render( <Popover>Hello</Popover> );
+			act( () => {
+				render( <Popover focusOnMount={ false } /> );
+
+				jest.advanceTimersByTime( 1 );
+			} );
+
+			expect( document.activeElement ).toBe( document.body );
 		} );
 
-		expect( result.container.querySelector( 'span' ) ).toMatchSnapshot();
-	} );
+		it( 'should render content', () => {
+			let result;
+			act( () => {
+				result = render( <Popover>Hello</Popover> );
+			} );
 
-	it( 'should pass additional props to portaled element', () => {
-		let result;
-		act( () => {
-			result = render( <Popover role="tooltip">Hello</Popover> );
+			expect(
+				result.container.querySelector( 'span' )
+			).toMatchSnapshot();
 		} );
 
-		expect( result.container.querySelector( 'span' ) ).toMatchSnapshot();
+		it( 'should pass additional props to portaled element', () => {
+			let result;
+			act( () => {
+				result = render( <Popover role="tooltip">Hello</Popover> );
+			} );
+
+			expect(
+				result.container.querySelector( 'span' )
+			).toMatchSnapshot();
+		} );
+
+		it( 'should render correctly when anchorRef is provided', () => {
+			const PopoverWithAnchor = ( args ) => {
+				const anchorRef = useRef( null );
+
+				return (
+					<div>
+						<p ref={ anchorRef }>Anchor</p>
+						<Popover { ...args } anchorRef={ anchorRef } />
+					</div>
+				);
+			};
+
+			render( <PopoverWithAnchor>Popover content</PopoverWithAnchor> );
+
+			expect( screen.getByText( 'Popover content' ) ).toBeInTheDocument();
+		} );
 	} );
 
-	it( 'should render correctly when anchorRef is provided', () => {
-		const PopoverWithAnchor = ( args ) => {
-			const anchorRef = useRef( null );
-
-			return (
-				<div>
-					<p ref={ anchorRef }>Anchor</p>
-					<Popover { ...args } anchorRef={ anchorRef } />
-				</div>
+	describe( 'positionToPlacement', () => {
+		it.each( [
+			[ 'top left', 'top-end' ],
+			[ 'top center', 'top' ],
+			[ 'top right', 'top-start' ],
+			[ 'middle left', 'left' ],
+			[ 'middle center', 'center' ],
+			[ 'middle right', 'right' ],
+			[ 'bottom left', 'bottom-end' ],
+			[ 'bottom center', 'bottom' ],
+			[ 'bottom right', 'bottom-start' ],
+		] )( 'converts `%s` to `%s`', ( inputPosition, expectedPlacement ) => {
+			expect( positionToPlacement( inputPosition ) ).toEqual(
+				expectedPlacement
 			);
-		};
-
-		render( <PopoverWithAnchor>Popover content</PopoverWithAnchor> );
-
-		expect( screen.getByText( 'Popover content' ) ).toBeInTheDocument();
+		} );
 	} );
 } );
