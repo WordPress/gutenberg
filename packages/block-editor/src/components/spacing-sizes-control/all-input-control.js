deaed6c0942877e2838a5a53d7eea90d008fab47
Glen Davies <glen.davies@automattic.com>
2022-08-04 13:15:04 +1200

    Switch to using new applyValueToSides method from components lib

--- packages/block-editor/src/components/spacing-sizes-control/all-input-control.js
+++ packages/block-editor/src/components/spacing-sizes-control/all-input-control.js
@@ -1,13 +1,13 @@
 /**
+ * WordPress dependencies
+ */
+import { __experimentalApplyValueToSides as applyValueToSides } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import SpacingInputControl from './spacing-input-control';
-import {
-	ALL_SIDES,
-	getAllRawValue,
-	isValuesMixed,
-	isValuesDefined,
-} from './utils';
+import { getAllRawValue, isValuesMixed, isValuesDefined } from './utils';
 
 export default function AllInputControl( {
 	onChange,
@@ -20,35 +20,8 @@ export default function AllInputControl( {
 	const hasValues = isValuesDefined( values );
 	const isMixed = hasValues && isValuesMixed( values );
 
-	// TODO: This is a duplicate of https://github.com/WordPress/gutenberg/blob/trunk/packages/components/src/box-control/all-input-control.js#L37
-	// so ideally this should be exported from the components package and imported here.
-	// Applies a value to an object representing top, right, bottom and left
-	// sides while taking into account any custom side configuration.
-	const applyValueToSides = ( currentValues, newValue ) => {
-		const newValues = { ...currentValues };
-
-		if ( sides?.length ) {
-			sides.forEach( ( side ) => {
-				if ( side === 'vertical' ) {
-					newValues.top = newValue;
-					newValues.bottom = newValue;
-				} else if ( side === 'horizontal' ) {
-					newValues.left = newValue;
-					newValues.right = newValue;
-				} else {
-					newValues[ side ] = newValue;
-				}
-			} );
-		} else {
-			ALL_SIDES.forEach( ( side ) => ( newValues[ side ] = newValue ) );
-		}
-
-		return newValues;
-	};
-
 	const handleOnChange = ( next ) => {
-		const nextValues = applyValueToSides( values, next );
-
+		const nextValues = applyValueToSides( values, next, sides );
 		onChange( nextValues );
 	};
 
