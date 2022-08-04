b54163ca4ee7f1782b7c70aa8c33ce0953f3a7f6
Glen Davies <glen.davies@automattic.com>
2022-08-04 13:01:34 +1200

    Remove redundant space

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -34,7 +34,6 @@ export {
 	default as __experimentalBoxControl,
 	applyValueToSides as __experimentalApplyValueToSides,
 } from './box-control';
-
 export { default as Button } from './button';
 export { default as ButtonGroup } from './button-group';
 export {
