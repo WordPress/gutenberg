ff8538bd4b7d7c40e916a282b6696888d559b4ff
Ramon <ramonjd@users.noreply.github.com>
2022-08-10 08:05:08 +1000

    Layout: merge CSS rule for block gap (#43052)
    * Merging similar CSS rule for block gap
    
    * Increasing specificity of selector

--- lib/block-supports/layout.php
+++ lib/block-supports/layout.php
@@ -115,7 +115,7 @@ function gutenberg_get_layout_style( $selector, $layout, $has_block_gap_support 
 						),
 					),
 					array(
-						'selector'     => "$selector > * + *",
+						'selector'     => "$selector$selector > * + *",
 						'declarations' => array(
 							'margin-block-start' => $gap_value,
 							'margin-block-end'   => '0',
