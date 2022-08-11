c4bdc602cce6b885eca90a538a412d7175039689
Daniel Richards <daniel.richards@automattic.com>
2022-08-11 14:38:54 +0800

    Fix drag and drop indicator before first block and after last block (#43135)
    * Fix drag and drop insertion point before first block and after last block
    
    * Improve drag and drop tests to check for insertion point

--- packages/e2e-tests/specs/editor/various/draggable-block.test.js
+++ packages/e2e-tests/specs/editor/various/draggable-block.test.js
@@ -7,28 +7,37 @@ import {
 	deactivatePlugin,
 	activatePlugin,
 	showBlockToolbar,
+	setBrowserViewport,
+	waitForWindowDimensions,
 } from '@wordpress/e2e-test-utils';
 
 describe( 'Draggable block', () => {
-	beforeAll( async () => {
-		await page.setDragInterception( true );
+	// Tests don't seem to pass if beforeAll and afterAll are used.
+	// Unsure why.
+	beforeEach( async () => {
 		await deactivatePlugin(
 			'gutenberg-test-plugin-disables-the-css-animations'
 		);
+
+		// Set the viewport at a larger size than normal to ensure scrolling doesn't occur.
+		// Scrolling can interfere with the drag coordinates.
+		await page.setViewport( { width: 960, height: 1024 } );
+		await waitForWindowDimensions( 960, 1024 );
+		await createNewPost();
+		await page.setDragInterception( true );
 	} );
 
-	afterAll( async () => {
+	afterEach( async () => {
 		await page.setDragInterception( false );
+
+		// Reset the viewport to normal large size.
+		await setBrowserViewport( 'large' );
 		await activatePlugin(
 			'gutenberg-test-plugin-disables-the-css-animations'
 		);
 	} );
 
-	beforeEach( async () => {
-		await createNewPost();
-	} );
-
-	it( 'should drag and drop', async () => {
+	it( 'can drag and drop to the top of a block list', async () => {
 		await page.keyboard.press( 'Enter' );
 		await page.keyboard.type( '1' );
 		await page.keyboard.press( 'Enter' );
@@ -43,15 +52,75 @@ describe( 'Draggable block', () => {
 		);
 		const dragHandlePoint = await dragHandle.clickablePoint();
 
-		const paragraph = await page.$( '[data-type="core/paragraph"]' );
-		const paragraphPoint = await paragraph.clickablePoint();
-		const paragraphRect = await paragraph.boundingBox();
-
-		await page.mouse.dragAndDrop( dragHandlePoint, {
-			x: paragraphPoint.x,
+		const firstParagraph = await page.$( '[data-type="core/paragraph"]' );
+		const targetPoint = await firstParagraph.clickablePoint();
+		const targetRect = await firstParagraph.boundingBox();
+		const target = {
+			x: targetPoint.x,
 			// Drag to the top half.
-			y: paragraphPoint.y - paragraphRect.height / 4,
-		} );
+			y: targetPoint.y - targetRect.height * 0.25,
+		};
+
+		const dragData = await page.mouse.drag( dragHandlePoint, target );
+		await page.mouse.dragEnter( target, dragData );
+		await page.mouse.dragOver( target, dragData );
+
+		// Wait for the insertion point to be visible.
+		const insertionPoint = await page.waitForSelector(
+			'.block-editor-block-list__insertion-point'
+		);
+
+		// Expect the insertion point to be visible above the first paragraph.
+		const insertionPointBoundingBox = await insertionPoint.boundingBox();
+		expect( insertionPointBoundingBox.y ).toBeLessThan( target.y );
+
+		await page.mouse.drop( target, dragData );
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	it( 'can drag and drop to the bottom of a block list', async () => {
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '1' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '2' );
+		await page.keyboard.press( 'ArrowUp' );
+
+		// Confirm correct setup.
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+
+		const [ , secondParagraph ] = await page.$$(
+			'[data-type="core/paragraph"]'
+		);
+
+		await showBlockToolbar();
+		const dragHandle = await page.waitForSelector(
+			'.block-editor-block-mover__drag-handle'
+		);
+		const dragHandlePoint = await dragHandle.clickablePoint();
+
+		const targetPoint = await secondParagraph.clickablePoint();
+		const targetRect = await secondParagraph.boundingBox();
+		const target = {
+			x: targetPoint.x,
+			// Drag to the bottom half.
+			y: targetPoint.y + targetRect.height * 0.25,
+		};
+
+		const dragData = await page.mouse.drag( dragHandlePoint, target );
+		await page.mouse.dragEnter( target, dragData );
+		await page.mouse.dragOver( target, dragData );
+
+		// Wait for the insertion point to be visible.
+		const insertionPoint = await page.waitForSelector(
+			'.block-editor-block-list__insertion-point'
+		);
+
+		// Expect the insertion point to be visible below the first paragraph.
+		const insertionPointBoundingBox = await insertionPoint.boundingBox();
+		expect( insertionPointBoundingBox.y ).toBeGreaterThan( target.y );
+
+		await page.mouse.drop( target, dragData );
 
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
