a3dba0dffac68e10873edfeb437bfcf79174e0fd
Daniel Richards <daniel.richards@automattic.com>
2022-08-15 11:51:04 +0800

    Combine the resize effect with the existing scroll layout effect

--- packages/components/src/popover/index.js
+++ packages/components/src/popover/index.js
@@ -192,24 +192,6 @@ const Popover = (
 	 * https://floating-ui.com/docs/react-dom#variables-inside-middleware-functions.
 	 */
 	const frameOffset = useRef();
-	useLayoutEffect( () => {
-		const { defaultView } = ownerDocument;
-		const { frameElement } = defaultView;
-
-		if ( ! frameElement || ownerDocument === document ) {
-			return undefined;
-		}
-
-		const updateFrameOffset = () => {
-			const iframeRect = frameElement.getBoundingClientRect();
-			frameOffset.current = { x: iframeRect.left, y: iframeRect.top };
-		};
-		updateFrameOffset();
-		defaultView.addEventListener( 'resize', updateFrameOffset );
-
-		return () =>
-			defaultView.removeEventListener( 'resize', updateFrameOffset );
-	}, [ ownerDocument ] );
 
 	const middleware = [
 		frameOffset || offset
@@ -399,14 +381,34 @@ const Popover = (
 
 	// If the reference element is in a different ownerDocument (e.g. iFrame),
 	// we need to manually update the floating's position as the reference's owner
-	// document scrolls.
+	// document scrolls. Also update the frame offset if the view resizes.
 	useLayoutEffect( () => {
 		if ( ownerDocument === document ) {
 			return;
 		}
 
+		const { defaultView } = ownerDocument;
+		const { frameElement } = defaultView;
+
 		ownerDocument.addEventListener( 'scroll', update );
-		return () => ownerDocument.removeEventListener( 'scroll', update );
+
+		let updateFrameOffset;
+		if ( frameElement ) {
+			updateFrameOffset = () => {
+				const iframeRect = frameElement.getBoundingClientRect();
+				frameOffset.current = { x: iframeRect.left, y: iframeRect.top };
+			};
+			updateFrameOffset();
+			defaultView.addEventListener( 'resize', updateFrameOffset );
+		}
+
+		return () => {
+			ownerDocument.removeEventListener( 'scroll', update );
+
+			if ( updateFrameOffset ) {
+				defaultView.removeEventListener( 'resize', updateFrameOffset );
+			}
+		};
 	}, [ ownerDocument ] );
 
 	/** @type {false | string} */
