8551b0264ca07e7c559600842ea60a63de0500b5
Daniel Richards <daniel.richards@automattic.com>
2022-08-15 13:26:23 +0800

    Remove fragment

--- packages/block-editor/src/components/block-settings-menu/index.js
+++ packages/block-editor/src/components/block-settings-menu/index.js
@@ -10,19 +10,17 @@ import BlockSettingsDropdown from './block-settings-dropdown';
 
 export function BlockSettingsMenu( { clientIds, ...props } ) {
 	return (
-		<>
-			<ToolbarGroup>
-				<ToolbarItem>
-					{ ( toggleProps ) => (
-						<BlockSettingsDropdown
-							clientIds={ clientIds }
-							toggleProps={ toggleProps }
-							{ ...props }
-						/>
-					) }
-				</ToolbarItem>
-			</ToolbarGroup>
-		</>
+		<ToolbarGroup>
+			<ToolbarItem>
+				{ ( toggleProps ) => (
+					<BlockSettingsDropdown
+						clientIds={ clientIds }
+						toggleProps={ toggleProps }
+						{ ...props }
+					/>
+				) }
+			</ToolbarItem>
+		</ToolbarGroup>
 	);
 }
 
