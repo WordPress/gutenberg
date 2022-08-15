7cfe6b905676aa8588a087d99456314e73b3542f
Daniel Richards <daniel.richards@automattic.com>
2022-08-15 12:52:36 +0800

    Move button to block toolbar

--- packages/block-editor/src/components/block-settings-menu/index.js
+++ packages/block-editor/src/components/block-settings-menu/index.js
@@ -7,12 +7,10 @@ import { ToolbarGroup, ToolbarItem } from '@wordpress/components';
  * Internal dependencies
  */
 import BlockSettingsDropdown from './block-settings-dropdown';
-import BlockEditVisuallyButton from './block-edit-visually-button';
 
 export function BlockSettingsMenu( { clientIds, ...props } ) {
 	return (
 		<>
-			<BlockEditVisuallyButton clientIds={ clientIds } { ...props } />
 			<ToolbarGroup>
 				<ToolbarItem>
 					{ ( toggleProps ) => (
