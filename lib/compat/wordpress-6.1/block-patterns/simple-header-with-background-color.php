283d4a257fb3687acc20f8c539017a95528d1d4d
Miguel Fonseca <miguelcsf@gmail.com>
2022-08-16 11:34:11 +0100

    Update padding in simple-header-with-background-color

--- lib/compat/wordpress-6.1/block-patterns/simple-header-with-background-color.php
+++ lib/compat/wordpress-6.1/block-patterns/simple-header-with-background-color.php
@@ -10,8 +10,8 @@ return array(
 	'blockTypes' => array( 'core/template-part/header' ),
 	'categories' => array( 'header' ),
 	'content'    => '<!-- wp:group {"align":"full","backgroundColor":"foreground","textColor":"background","layout":{"inherit":true}} -->
-					<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"4em","top":"2em"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-group alignwide" style="padding-top:2em;padding-bottom:4em"><!-- wp:group {"layout":{"type":"flex"}} -->
+					<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"1em","top":"1em"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+					<div class="wp-block-group alignwide" style="padding-top:1em;padding-bottom:1em"><!-- wp:group {"layout":{"type":"flex"}} -->
 					<div class="wp-block-group"><!-- wp:site-logo {"width":60} /-->
 					
 					<!-- wp:site-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}}} /--></div>
