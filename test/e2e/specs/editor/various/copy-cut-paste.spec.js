35bd721b6e45296da4ff84c472753b51e26eeeea
Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
2022-08-16 14:53:10 +0200

    Post title: fix pasting into existing content (#43123)

--- test/e2e/specs/editor/various/copy-cut-paste.spec.js
+++ test/e2e/specs/editor/various/copy-cut-paste.spec.js
@@ -416,4 +416,21 @@ test.describe( 'Copy/cut/paste', () => {
 			await page.evaluate( () => document.activeElement.innerHTML )
 		).toMatchSnapshot();
 	} );
+
+	test( 'should paste single line in post title with existing content', async ( {
+		page,
+		pageUtils,
+	} ) => {
+		await page.keyboard.type( 'ab' );
+		await page.keyboard.press( 'ArrowLeft' );
+		await pageUtils.setClipboardData( {
+			html: '<span style="border: 1px solid black">x</span>',
+		} );
+		await pageUtils.pressKeyWithModifier( 'primary', 'v' );
+		// Ensure the selection is correct.
+		await page.keyboard.type( 'y' );
+		expect(
+			await page.evaluate( () => document.activeElement.innerHTML )
+		).toBe( 'axyb' );
+	} );
 } );
