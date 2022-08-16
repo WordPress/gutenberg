440e05e957174a41be4c41bf19d6c9652ec4f41d
Miguel Fonseca <miguelcsf@gmail.com>
2022-08-16 16:51:26 +0100

    Source image assets from s.w.org

--- lib/compat/wordpress-6.1/block-patterns/simple-header-inside-image.php
+++ lib/compat/wordpress-6.1/block-patterns/simple-header-inside-image.php
@@ -10,8 +10,8 @@ return array(
 	'blockTypes' => array( 'core/template-part/header' ),
 	'categories' => array( 'header' ),
 	'content'    => '<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
-					<div class="wp-block-group alignfull"><!-- wp:cover {"url":"https://headerandfooterpatterns.files.wordpress.com/2022/01/5eca6-header-mountains-scaled-1.jpg","id":57,"dimRatio":0,"minHeight":680,"contentPosition":"center center","isDark":false,"align":"full"} -->
-					<div class="wp-block-cover alignfull is-light" style="min-height:680px"><span aria-hidden="true" class="has-background-dim-0 wp-block-cover__gradient-background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-57" alt="" src="https://headerandfooterpatterns.files.wordpress.com/2022/01/5eca6-header-mountains-scaled-1.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":null,"isStackedOnMobile":false} -->
+					<div class="wp-block-group alignfull"><!-- wp:cover {"url":"https://s.w.org/patterns/files/2022/08/5eca6-header-mountains-scaled-1.jpg","id":57,"dimRatio":0,"minHeight":680,"contentPosition":"center center","isDark":false,"align":"full"} -->
+					<div class="wp-block-cover alignfull is-light" style="min-height:680px"><span aria-hidden="true" class="has-background-dim-0 wp-block-cover__gradient-background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-57" alt="" src="https://s.w.org/patterns/files/2022/08/5eca6-header-mountains-scaled-1.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":null,"isStackedOnMobile":false} -->
 					<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"verticalAlignment":"center"} -->
 					<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"flex"}} -->
 					<div class="wp-block-group"><!-- wp:site-logo {"width":60} /-->
