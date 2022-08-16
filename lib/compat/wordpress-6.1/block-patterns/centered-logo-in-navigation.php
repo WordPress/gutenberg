5d417cc7b989d48f7010913296443869ab9992e6
Miguel Fonseca <miguelcsf@gmail.com>
2022-08-16 11:20:27 +0100

    Fix curly quote
    Co-authored-by: Kelly Dwan <ryelle@users.noreply.github.com>

--- lib/compat/wordpress-6.1/block-patterns/centered-logo-in-navigation.php
+++ lib/compat/wordpress-6.1/block-patterns/centered-logo-in-navigation.php
@@ -10,7 +10,7 @@ return array(
 	'blockTypes' => array( 'core/template-part/header' ),
 	'categories' => array( 'header' ),
 	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"2em","bottom":"4em"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
-					<div class="wp-block-group alignfull" style="padding-top:2em;padding-bottom:4em"><!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent”:”center”}} --><!-- wp:navigation-link {"label":"' . __( 'Home', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
+					<div class="wp-block-group alignfull" style="padding-top:2em;padding-bottom:4em"><!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center"}} --><!-- wp:navigation-link {"label":"' . __( 'Home', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
 					
 					<!-- wp:navigation-link {"label":"' . __( 'About', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
 					
