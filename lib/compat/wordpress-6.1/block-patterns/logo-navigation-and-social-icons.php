8414c6a9eb61e0696a67a47bbe6ca322a5ac5301
Miguel Fonseca <miguelcsf@gmail.com>
2022-08-17 16:51:05 +0100

    logo-navigation-and-social-icons: use Navigation Links instead of Page List

--- lib/compat/wordpress-6.1/block-patterns/logo-navigation-and-social-icons.php
+++ lib/compat/wordpress-6.1/block-patterns/logo-navigation-and-social-icons.php
@@ -12,12 +12,12 @@ return array(
 	'content'    => '<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
 					<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"4em","top":"2em"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 					<div class="wp-block-group alignwide" style="padding-top:2em;padding-bottom:4em"><!-- wp:site-logo {"width":60} /-->
-					
-					<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} --> <!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
-					
+					<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->
+					<!-- wp:navigation-link {"label":"' . __( 'About', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
+					<!-- wp:navigation-link {"label":"' . __( 'Blog', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
+					<!-- wp:navigation-link {"label":"' . __( 'Contact', 'gutenberg' ) . '","url":"#","kind":"custom","isTopLevelLink":true} /-->
 					<!-- wp:social-links {"className":"is-style-logos-only"} -->
 					<ul class="wp-block-social-links is-style-logos-only"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
-					
 					<!-- wp:social-link {"url":"#","service":"twitter"} /--></ul>
 					<!-- /wp:social-links --><!-- /wp:navigation --></div>
 					<!-- /wp:group --></div>
