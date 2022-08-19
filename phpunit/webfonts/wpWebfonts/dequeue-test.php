d4090975ebbddf73992598475db6f850d6351f96
hellofromtonya <tonya.mork@automattic.com>
2022-08-19 06:44:20 -0500

    WP_Webfonts::dequeue()

---
+++ phpunit/webfonts/wpWebfonts/dequeue-test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WP_Webfonts::dequeue() tests.
+ *
+ * @package    WordPress
+ * @subpackage Webfonts
+ */
+
+require_once __DIR__ . '/../wp-webfonts-testcase.php';
+
+/**
+ * @group  webfonts
+ * @covers WP_Webfonts::dequeue
+ */
+class Tests_Webfonts_WpWebfonts_Dequeue extends WP_Webfonts_TestCase {
+
+	public function test_dequeue() {
+		// TODO: Add tests for WP_Webfonts::dequeue().
+	}
+}
