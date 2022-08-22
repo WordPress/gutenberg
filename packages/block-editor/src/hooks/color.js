b910975fcb91a6779d04edf8cef916f771ab7844
ramonjd <ramonjd@gmail.com>
2022-08-22 14:11:17 +1000

    There is no `conwst`

--- packages/block-editor/src/hooks/color.js
+++ packages/block-editor/src/hooks/color.js
@@ -292,7 +292,7 @@ const getLinkColorFromAttributeValue = ( colors, value ) => {
 export function ColorEdit( props ) {
 	const { name: blockName, attributes } = props;
 	// Some color settings have a special handling for deprecated flags in `useSetting`,
-	// so we can't unwrap them by doing conwst { ... } = useSetting('color')
+	// so we can't unwrap them by doing const { ... } = useSetting('color')
 	// until https://github.com/WordPress/gutenberg/issues/37094 is fixed.
 	const userPalette = useSetting( 'color.palette.custom' );
 	const themePalette = useSetting( 'color.palette.theme' );
