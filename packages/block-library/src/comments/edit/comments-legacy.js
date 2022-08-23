50dc0c1128dcc82530aad79b881b99b26bb1a80b
Bernie Reiter <ockham@raz.or.at>
2022-08-23 16:49:06 +0200

    Comments block (legacy mode): Update warning message wording

--- packages/block-library/src/comments/edit/comments-legacy.js
+++ packages/block-library/src/comments/edit/comments-legacy.js
@@ -57,11 +57,10 @@ export default function CommentsLegacy( {
 			<div { ...blockProps }>
 				<Warning actions={ actions }>
 					{ __(
-						"Comments block: You're currently using this block in legacy mode. " +
-							'The following is just a placeholder, not a real comment. ' +
-							'The final styling may differ because it also depends on the current theme. ' +
-							'For better compatibility with the Block Editor, ' +
-							'please consider switching the block to its editable mode.'
+						"Comments block: You're currently using the legacy version. " +
+							'The following is just a placeholder -- it might look different on the frontend. ' +
+							'For a better representation and more customization options, ' +
+							'switch the block to its editable mode.'
 					) }
 				</Warning>
 				<Placeholder postId={ postId } postType={ postType } />
