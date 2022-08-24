59174f28b46ac19ee33a90583135535b2291e426
Ella van Durpe <4710635+ellatrix@users.noreply.github.com>
2022-08-24 20:15:23 +0200

    List: use nested blocks (#42711)
    Co-authored-by: André <583546+oandregal@users.noreply.github.com>

--- packages/block-editor/src/components/writing-flow/use-select-all.js
+++ packages/block-editor/src/components/writing-flow/use-select-all.js
@@ -37,28 +37,25 @@ export default function useSelectAll() {
 				return;
 			}
 
+			event.preventDefault();
+
 			const [ firstSelectedClientId ] = selectedClientIds;
 			const rootClientId = getBlockRootClientId( firstSelectedClientId );
-			let blockClientIds = getBlockOrder( rootClientId );
+			const blockClientIds = getBlockOrder( rootClientId );
 
 			// If we have selected all sibling nested blocks, try selecting up a
 			// level. See: https://github.com/WordPress/gutenberg/pull/31859/
 			if ( selectedClientIds.length === blockClientIds.length ) {
-				blockClientIds = getBlockOrder(
-					getBlockRootClientId( rootClientId )
-				);
-			}
-
-			const firstClientId = first( blockClientIds );
-			const lastClientId = last( blockClientIds );
-
-			if ( firstClientId === lastClientId ) {
-				selectBlock( firstClientId );
+				if ( rootClientId ) {
+					node.ownerDocument.defaultView
+						.getSelection()
+						.removeAllRanges();
+					selectBlock( rootClientId );
+				}
 				return;
 			}
 
-			multiSelect( firstClientId, lastClientId );
-			event.preventDefault();
+			multiSelect( first( blockClientIds ), last( blockClientIds ) );
 		}
 
 		node.addEventListener( 'keydown', onKeyDown );
