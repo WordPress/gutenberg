f0dd6111df6c6c18718747addda3cae988e34d37
Lena Morita <lena@jaguchi.com>
2022-08-31 02:02:58 +0900

    Retire `DARK_GRAY[ 150 ]` → `GRAY[ 600 ]`

--- packages/components/src/utils/colors-values.js
+++ packages/components/src/utils/colors-values.js
@@ -25,7 +25,6 @@ const GRAY = {
 const DARK_GRAY = {
 	500: '#555d66', // Use this most of the time for dark items.
 	300: '#6c7781', // Lightest gray that can be used for AA text contrast.
-	150: '#8d96a0', // Lightest gray that can be used for AA non-text contrast.
 };
 
 const LIGHT_GRAY = {
@@ -56,7 +55,7 @@ const UI = {
 	borderHover: GRAY[ 700 ],
 	borderFocus: ADMIN.themeDark10,
 	borderDisabled: GRAY[ 400 ],
-	textDisabled: DARK_GRAY[ 150 ], // TODO: Replace with WordPress gray
+	textDisabled: GRAY[ 600 ],
 	textDark: white,
 
 	// Matches @wordpress/base-styles
