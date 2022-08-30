b49655d890758ea409f0119d5599682f52e9027a
Alec Geatches <alec.geatches@automattic.com>
2022-08-30 17:59:02 +0800

    Add required schema, required strict skips to test

--- test/integration/theme-schema.test.js
+++ test/integration/theme-schema.test.js
@@ -8,11 +8,19 @@ import Ajv from 'ajv-draft-04';
  */
 import themeSchema from '../../schemas/json/theme.json';
 
+const baseThemeJson = {
+	version: 2,
+};
+
 describe( 'theme.json schema', () => {
-	const ajv = new Ajv();
+	const ajv = new Ajv( {
+		// Used for matching unknown blocks without repeating core blocks names
+		// in settings.blocks and settings.styles
+		allowMatchingProperties: true,
+	} );
 
 	test( 'schema valid', () => {
-		const result = ajv.validate( themeSchema, {} ) || ajv.errors;
+		const result = ajv.validate( themeSchema, baseThemeJson ) || ajv.errors;
 
 		expect( result ).toBe( true );
 	} );
