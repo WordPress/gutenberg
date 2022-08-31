05381d2074f6cfd9d1b6d0f779943291203b829d
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-08-31 13:39:55 +0300

    Lodash: Remove _.omit() from blocks (#43711)

--- packages/blocks/src/api/utils.js
+++ packages/blocks/src/api/utils.js
@@ -304,3 +304,17 @@ export function __experimentalGetBlockAttributesNamesByRole( name, role ) {
 			attributes[ attributeName ]?.__experimentalRole === role
 	);
 }
+
+/**
+ * Return a new object with the specified keys omitted.
+ *
+ * @param {Object} object Original object.
+ * @param {Array}  keys   Keys to be omitted.
+ *
+ * @return {Object} Object with omitted keys.
+ */
+export function omit( object, keys ) {
+	return Object.fromEntries(
+		Object.entries( object ).filter( ( [ key ] ) => ! keys.includes( key ) )
+	);
+}
