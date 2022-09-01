0f5a3897ae525a61d54962f62e7c799d71517ade
Lena Morita <lena@jaguchi.com>
2022-09-02 03:59:23 +0900

    NumberControl: Improve types

--- packages/components/src/number-control/index.tsx
+++ packages/components/src/number-control/index.tsx
@@ -1,8 +1,8 @@
-// @ts-nocheck
 /**
  * External dependencies
  */
 import classNames from 'classnames';
+import type { ForwardedRef } from 'react';
 
 /**
  * WordPress dependencies
@@ -17,8 +17,11 @@ import { Input } from './styles/number-control-styles';
 import * as inputControlActionTypes from '../input-control/reducer/actions';
 import { add, subtract, roundClamp } from '../utils/math';
 import { isValueEmpty } from '../utils/values';
+import type { WordPressComponentProps } from '../ui/context/wordpress-component';
+import type { NumberControlProps } from './types';
+import type { InputState } from '../input-control/reducer/state';
 
-export function NumberControl(
+function UnforwardedNumberControl(
 	{
 		__unstableStateReducer: stateReducerProp,
 		className,
@@ -35,20 +38,21 @@ export function NumberControl(
 		type: typeProp = 'number',
 		value: valueProp,
 		...props
-	},
-	ref
+	}: WordPressComponentProps< NumberControlProps, 'input', false >,
+	ref: ForwardedRef< any >
 ) {
 	const isStepAny = step === 'any';
+	// @ts-expect-error step should be a number but could be string
 	const baseStep = isStepAny ? 1 : parseFloat( step );
 	const baseValue = roundClamp( 0, min, max, baseStep );
-	const constrainValue = ( value, stepOverride ) => {
+	const constrainValue = ( value: number, stepOverride?: number | null ) => {
 		// When step is "any" clamp the value, otherwise round and clamp it.
 		return isStepAny
 			? Math.min( max, Math.max( min, value ) )
 			: roundClamp( value, min, max, stepOverride ?? baseStep );
 	};
 
-	const autoComplete = typeProp === 'number' ? 'off' : null;
+	const autoComplete = typeProp === 'number' ? 'off' : undefined;
 	const classes = classNames( 'components-number-control', className );
 
 	/**
@@ -56,11 +60,14 @@ export function NumberControl(
 	 * This allows us to tap into actions to transform the (next) state for
 	 * InputControl.
 	 *
-	 * @param {Object} state  State from InputControl
-	 * @param {Object} action Action triggering state change
-	 * @return {Object} The updated state to apply to InputControl
+	 * @return The updated state to apply to InputControl
 	 */
-	const numberControlStateReducer = ( state, action ) => {
+	const numberControlStateReducer = (
+		/** State from InputControl. */
+		state: InputState,
+		/** Action triggering state change. */
+		action: inputControlActionTypes.InputAction
+	) => {
 		const nextState = { ...state };
 
 		const { type, payload } = action;
@@ -74,10 +81,12 @@ export function NumberControl(
 			type === inputControlActionTypes.PRESS_UP ||
 			type === inputControlActionTypes.PRESS_DOWN
 		) {
+			// @ts-expect-error TODO: Investigate if this is wrong
 			const enableShift = event.shiftKey && isShiftStepEnabled;
 
 			const incrementalValue = enableShift
-				? parseFloat( shiftStep ) * baseStep
+				? // @ts-expect-error shiftStep should be a number but could be string
+				  parseFloat( shiftStep ) * baseStep
 				: baseStep;
 			let nextValue = isValueEmpty( currentValue )
 				? baseValue
@@ -88,14 +97,18 @@ export function NumberControl(
 			}
 
 			if ( type === inputControlActionTypes.PRESS_UP ) {
+				// @ts-expect-error TODO: Investigate if this is wrong
 				nextValue = add( nextValue, incrementalValue );
 			}
 
 			if ( type === inputControlActionTypes.PRESS_DOWN ) {
+				// @ts-expect-error TODO: Investigate if this is wrong
 				nextValue = subtract( nextValue, incrementalValue );
 			}
 
+			// @ts-expect-error TODO: Investigate if this is wrong
 			nextState.value = constrainValue(
+				// @ts-expect-error TODO: Investigate if this is wrong
 				nextValue,
 				enableShift ? incrementalValue : null
 			);
@@ -105,10 +118,13 @@ export function NumberControl(
 		 * Handles drag to update events
 		 */
 		if ( type === inputControlActionTypes.DRAG && isDragEnabled ) {
+			// @ts-expect-error TODO: Investigate
 			const [ x, y ] = payload.delta;
+			// @ts-expect-error TODO: Investigate
 			const enableShift = payload.shiftKey && isShiftStepEnabled;
 			const modifier = enableShift
-				? parseFloat( shiftStep ) * baseStep
+				? // @ts-expect-error shiftStep should be a number but could be string
+				  parseFloat( shiftStep ) * baseStep
 				: baseStep;
 
 			let directionModifier;
@@ -140,7 +156,9 @@ export function NumberControl(
 				delta = Math.ceil( Math.abs( delta ) ) * Math.sign( delta );
 				const distance = delta * modifier * directionModifier;
 
+				// @ts-expect-error TODO: Investigate if this is wrong
 				nextState.value = constrainValue(
+					// @ts-expect-error TODO: Investigate if this is wrong
 					add( currentValue, distance ),
 					enableShift ? modifier : null
 				);
@@ -156,9 +174,11 @@ export function NumberControl(
 		) {
 			const applyEmptyValue = required === false && currentValue === '';
 
+			// @ts-expect-error TODO: Investigate if this is wrong
 			nextState.value = applyEmptyValue
 				? currentValue
-				: constrainValue( currentValue );
+				: // @ts-expect-error TODO: Investigate if this is wrong
+				  constrainValue( currentValue );
 		}
 
 		return nextState;
@@ -180,6 +200,7 @@ export function NumberControl(
 			required={ required }
 			step={ step }
 			type={ typeProp }
+			// @ts-expect-error TODO: Resolve discrepancy
 			value={ valueProp }
 			__unstableStateReducer={ ( state, action ) => {
 				const baseState = numberControlStateReducer( state, action );
@@ -189,4 +210,6 @@ export function NumberControl(
 	);
 }
 
-export default forwardRef( NumberControl );
+export const NumberControl = forwardRef( UnforwardedNumberControl );
+
+export default NumberControl;
