6f68efceae1030d8c74139de21c5ab8f2f38d2f0
Lena Morita <lena@jaguchi.com>
2022-09-01 20:32:12 +0900

    Components: Retire `LIGHT_GRAY[ 200 ]` and `DARK_GRAY[ 150 ]` (#43719)
    * Retire `LIGHT_GRAY[ 200 ]` → `GRAY[ 100 ]`
    
    * Retire `DARK_GRAY[ 150 ]` → `GRAY[ 600 ]`
    
    * Add todo comments
    
    * Add changelog
    
    * Update snapshots

--- packages/components/CHANGELOG.md
+++ packages/components/CHANGELOG.md
@@ -12,6 +12,11 @@
 -   `ToggleControl`: Add `__nextHasNoMargin` prop for opting into the new margin-free styles ([#43717](https://github.com/WordPress/gutenberg/pull/43717)). 
 -   `CheckboxControl`: Add `__nextHasNoMargin` prop for opting into the new margin-free styles ([#43720](https://github.com/WordPress/gutenberg/pull/43720)).
 
+### Enhancements
+
+-   `CardHeader`, `CardBody`, `CardFooter`: Tweak `isShady` background colors to be consistent with the grays in `@wordpress/base-styles` ([#43719](https://github.com/WordPress/gutenberg/pull/43719)).
+-   `InputControl`, `SelectControl`: Tweak `disabled` colors to be consistent with the grays in `@wordpress/base-styles` ([#43719](https://github.com/WordPress/gutenberg/pull/43719)).
+
 ### Internal
 
 -   Remove unused `normalizeArrowKey` utility function ([#43640](https://github.com/WordPress/gutenberg/pull/43640/)).
--- packages/components/src/utils/colors-values.js
+++ packages/components/src/utils/colors-values.js
@@ -22,18 +22,18 @@ const GRAY = {
 	100: '#f0f0f0',
 };
 
+// TODO: Replace usages of these with the equivalents in `GRAY`
 const DARK_GRAY = {
 	500: '#555d66', // Use this most of the time for dark items.
 	300: '#6c7781', // Lightest gray that can be used for AA text contrast.
-	150: '#8d96a0', // Lightest gray that can be used for AA non-text contrast.
 };
 
+// TODO: Replace usages of these with the equivalents in `GRAY`
 const LIGHT_GRAY = {
 	800: '#b5bcc2',
 	600: '#d7dade',
 	400: '#e8eaeb', // Good for "readonly" input fields and special text selection.
 	300: '#edeff0',
-	200: '#f3f4f5',
 };
 
 // Matches @wordpress/base-styles
@@ -52,12 +52,12 @@ const ADMIN = {
 const UI = {
 	theme: ADMIN.theme,
 	background: white,
-	backgroundDisabled: LIGHT_GRAY[ 200 ], // TODO: Replace with WordPress gray
+	backgroundDisabled: GRAY[ 100 ],
 	border: GRAY[ 700 ],
 	borderHover: GRAY[ 700 ],
 	borderFocus: ADMIN.themeDark10,
 	borderDisabled: GRAY[ 400 ],
-	textDisabled: DARK_GRAY[ 150 ], // TODO: Replace with WordPress gray
+	textDisabled: GRAY[ 600 ],
 	textDark: white,
 
 	// Matches @wordpress/base-styles
