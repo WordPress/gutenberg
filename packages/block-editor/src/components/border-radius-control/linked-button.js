fe22528b599c725e726a0d41c2210abaa17be6ae
jasmussen <joen@automattic.com>
2022-09-02 10:54:02 +0200

    Radius.

--- packages/block-editor/src/components/border-radius-control/linked-button.js
+++ packages/block-editor/src/components/border-radius-control/linked-button.js
@@ -6,20 +6,20 @@ import { link, linkOff } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 
 export default function LinkedButton( { isLinked, ...props } ) {
-	const label = isLinked ? __( 'Unlink Radii' ) : __( 'Link Radii' );
+	const label = isLinked ? __( 'Unlink radii' ) : __( 'Link radii' );
 
 	return (
 		<Tooltip text={ label }>
-			<Button
-				{ ...props }
-				className="component-border-radius-control__linked-button"
-				isPrimary={ isLinked }
-				isSecondary={ ! isLinked }
-				isSmall
-				icon={ isLinked ? link : linkOff }
-				iconSize={ 16 }
-				aria-label={ label }
-			/>
+			<span>
+				<Button
+					{ ...props }
+					className="component-border-radius-control__linked-button"
+					isSmall
+					icon={ isLinked ? link : linkOff }
+					iconSize={ 24 }
+					aria-label={ label }
+				/>
+			</span>
 		</Tooltip>
 	);
 }
