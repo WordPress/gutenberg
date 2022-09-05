205916c98844bc7b90b868c239bdb3c246e59b75
George Mamadashvili <georgemamadashvili@gmail.com>
2022-09-05 16:24:27 +0400

    Try adding remaining test

--- test/e2e/specs/editor/various/draggable-blocks.spec.js
+++ test/e2e/specs/editor/various/draggable-blocks.spec.js
@@ -69,4 +69,76 @@ test.describe( 'Draggable block', () => {
 <p>1</p>
 <!-- /wp:paragraph -->` );
 	} );
+
+	test( 'can drag and drop to the bottom of a block list', async ( {
+		editor,
+		page,
+	} ) => {
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '1' );
+		await page.keyboard.press( 'Enter' );
+		await page.keyboard.type( '2' );
+		await page.keyboard.press( 'ArrowUp' );
+
+		// Confirm correct setup.
+		await expect.poll( editor.getEditedPostContent )
+			.toBe( `<!-- wp:paragraph -->
+<p>1</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>2</p>
+<!-- /wp:paragraph -->` );
+
+		await editor.showBlockToolbar();
+
+		const dragHandle = page.locator(
+			'role=toolbar[name="Block tools"i] >> role=button[name="Drag"i][include-hidden]'
+		);
+		// Hover to the center of the drag handle.
+		await dragHandle.hover();
+		// Start dragging.
+		await page.mouse.down();
+		await dragHandle.hover( {
+			// Move the dragged handle a little bit (to the top left of the handle's
+			// original location) to trigger the dragging chip to appear.
+			position: { x: 0, y: 0 },
+			// Bypass the "actionability" checks because the handle will disappear.
+			force: true,
+		} );
+
+		await expect(
+			page.locator( 'data-testid=block-draggable-chip >> visible=true' )
+		).toBeVisible();
+
+		// Hover on the upper half of the paragraph block to trigger the indicator.
+		const secondParagraph = page.locator(
+			'role=document[name="Paragraph block"i] >> text=2'
+		);
+		const secondParagraphBound = await secondParagraph.boundingBox();
+
+		// Hover on the bottom half of the paragraph block to trigger the indicator.
+		await secondParagraph.hover( {
+			position: {
+				x: 0,
+				y: secondParagraphBound.height * 0.75,
+			},
+		} );
+
+		// await expect(
+		// 	page.locator( 'data-testid=block-list-insertion-point-indicator' )
+		// ).toBeVisible();
+
+		// Drop the paragraph block.
+		await page.mouse.up();
+
+		await expect.poll( editor.getEditedPostContent )
+			.toBe( `<!-- wp:paragraph -->
+<p>2</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>1</p>
+<!-- /wp:paragraph -->` );
+	} );
 } );
