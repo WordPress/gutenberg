37bf4e9b92c8640fedee8f6cd66a62536161e15a
Matias Benedetto <matias.benedetto@gmail.com>
2022-09-07 18:44:26 -0300

    Adding checks

--- lib/compat/wordpress-6.1/class-wp-theme-json-6-1.php
+++ lib/compat/wordpress-6.1/class-wp-theme-json-6-1.php
@@ -1345,7 +1345,8 @@ class WP_Theme_JSON_6_1 extends WP_Theme_JSON_6_0 {
 				}
 			}
 
-			if ( null !== $block_gap_value ) {
+			// If the block should have custom gap, add the gap styles.
+			if ( null !== $block_gap_value && false !== $block_gap_value && '' !== $block_gap_value ) {
 				foreach ( $layout_definitions as $layout_definition_key => $layout_definition ) {
 					// Allow skipping default layout for themes that opt-in to block styles, but opt-out of blockGap.
 					if ( ! $has_block_gap_support && 'default' === $layout_definition_key ) {
