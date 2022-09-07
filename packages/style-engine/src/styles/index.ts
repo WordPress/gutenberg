40efcbe548e7579a436912bd12740212cf305cdc
Maggie <maggie.cabrera@automattic.com>
2022-09-07 13:12:36 +0200

    added outline support for blocks via theme.json (#43526)
    * added outline to theme.json
    
    * linting

--- packages/style-engine/src/styles/index.ts
+++ packages/style-engine/src/styles/index.ts
@@ -4,12 +4,14 @@
 import border from './border';
 import color from './color';
 import shadow from './shadow';
+import outline from './outline';
 import spacing from './spacing';
 import typography from './typography';
 
 export const styleDefinitions = [
 	...border,
 	...color,
+	...outline,
 	...spacing,
 	...typography,
 	...shadow,
