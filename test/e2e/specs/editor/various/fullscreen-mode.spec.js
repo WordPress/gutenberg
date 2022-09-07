5b7c98f1ed6a621545ceb7f5d1aa15f4de075d24
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-09-08 00:12:30 +0600

    Add Full Screen Test case

---
+++ test/e2e/specs/editor/various/fullscreen-mode.spec.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Full Screen', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
+	} );
+
+	test.afterEach( async ( { requestUtils } ) => {
+		await requestUtils.deleteAllPosts();
+	} );
+
+	test( 'should open the fullscreen mode from the more menu', async ( {
+		page,
+	} ) => {
+		// Open Options
+		await page.locator( ' [aria-label="Options"]' ).click();
+
+		// Verify All options are visible
+		page.locator( '.interface-more-menu-dropdown__content' );
+
+		// Choose Full Screen Mode
+		await page
+			.locator( 'text=Fullscreen modeWork without distraction⇧⌥⌘F' )
+			.click();
+
+		// Validate Fullscreen
+		const isFullscreenEnabled = await page.$eval( 'body', ( body ) => {
+			return body.classList.contains( 'is-fullscreen-mode' );
+		} );
+
+		expect( isFullscreenEnabled ).toBe( true );
+
+		const fullscreenCloseButton = await page.locator(
+			'.edit-post-fullscreen-mode-close'
+		);
+
+		expect( fullscreenCloseButton ).not.toBeNull();
+	} );
+} );
