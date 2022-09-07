50a613f11d96a241c2ac466233157ff664ace788
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-09-07 11:06:44 +0300

    Refactor the navigation selector for better readability of code.
    Co-authored-by: Daniel Richards <677833+talldan@users.noreply.github.com>

--- packages/block-library/src/navigation/edit/navigation-menu-selector.js
+++ packages/block-library/src/navigation/edit/navigation-menu-selector.js
@@ -143,9 +143,10 @@ function NavigationMenuSelector( {
 	if ( ! hasNavigationMenus && ! hasClassicMenus ) {
 		return (
 			<Button
-				{ ...toggleProps }
 				className="wp-block-navigation__navigation-selector-button--createnew"
+				isBusy={ ! enableOptions }
 				disabled={ ! enableOptions }
+				__experimentalIsFocusable
 				onClick={ () => {
 					onCreateNew();
 					setIsCreatingMenu( true );
