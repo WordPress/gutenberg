0746c090857734e45a274cd89bb2e3526d12456d
Daniel Richards <daniel.richards@automattic.com>
2022-09-08 14:40:24 +0800

    Add horizontal drag to end test

--- test/e2e/specs/editor/various/draggable-blocks.spec.js
+++ test/e2e/specs/editor/various/draggable-blocks.spec.js
@@ -240,4 +240,87 @@ test.describe( 'Draggable block', () => {
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->` );
 	} );
+
+	test( 'can drag and drop to the end of a horizontal block list', async ( {
+		editor,
+		page,
+	} ) => {
+		// Insert a row.
+		await editor.insertBlock( {
+			name: 'core/group',
+			attributes: {
+				layout: { type: 'flex', flexWrap: 'nowrap' },
+			},
+			innerBlocks: [
+				{
+					name: 'core/paragraph',
+					attributes: {
+						content: '1',
+					},
+				},
+				{
+					name: 'core/paragraph',
+					attributes: {
+						content: '2',
+					},
+				},
+			],
+		} );
+
+		await page.focus( 'role=document[name="Paragraph block"i] >> text=1' );
+		await editor.showBlockToolbar();
+
+		const dragHandle = page.locator(
+			'role=toolbar[name="Block tools"i] >> role=button[name="Drag"i][include-hidden]'
+		);
+		// Hover to the center of the drag handle.
+		await dragHandle.hover();
+		// Start dragging.
+		await page.mouse.down();
+
+		// Move to and hover on the bottom half of the paragraph block to trigger the indicator.
+		const secondParagraph = page.locator(
+			'role=document[name="Paragraph block"i] >> text=2'
+		);
+		const secondParagraphBound = await secondParagraph.boundingBox();
+		// Call the move function twice to make sure the `dragOver` event is sent.
+		// @see https://github.com/microsoft/playwright/issues/17153
+		for ( let i = 0; i < 2; i += 1 ) {
+			await page.mouse.move(
+				secondParagraphBound.x + secondParagraphBound.width * 0.75,
+				secondParagraphBound.y
+			);
+		}
+
+		await expect(
+			page.locator( 'data-testid=block-draggable-chip >> visible=true' )
+		).toBeVisible();
+
+		const indicator = page.locator(
+			'data-testid=block-list-insertion-point-indicator'
+		);
+		await expect( indicator ).toBeVisible();
+		// Expect the indicator to be below the second paragraph.
+		await expect
+			.poll( () =>
+				indicator.boundingBox().then( ( { x, width } ) => x + width )
+			)
+			.toBeGreaterThan(
+				secondParagraphBound.x + secondParagraphBound.width
+			);
+
+		// Drop the paragraph block.
+		await page.mouse.up();
+
+		await expect.poll( editor.getEditedPostContent )
+			.toBe( `<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>2</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>1</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->` );
+	} );
 } );
