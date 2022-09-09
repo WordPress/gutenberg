9ac232cba69c013af69046b60e573ab11437cd7b
ramonjd <ramonjd@gmail.com>
2022-09-09 13:28:49 +1000

    Adding message to script loader block support enqueuing expectations

--- phpunit/script-loader.php
+++ phpunit/script-loader.php
@@ -63,7 +63,16 @@ class WP_Script_Loader_Test extends WP_UnitTestCase {
 
 		gutenberg_enqueue_stored_styles();
 
-		$this->assertEquals( array( '.saruman{color:white;height:100px;border-style:solid;}' ), $wp_styles->get_data( 'core-block-supports', 'after' ) );
-		$this->assertEquals( array( '.gandalf{color:grey;height:90px;border-style:dotted;}' ), $wp_styles->get_data( 'wp-style-engine-my-styles', 'after' ) );
+		$this->assertEquals(
+			array( '.saruman{color:white;height:100px;border-style:solid;}' ),
+			wp_styles()->registered['core-block-supports']->extra['after'],
+			'Registered styles with handle of "core-block-supports" do not match expected value from Style Engine store.'
+		);
+
+		$this->assertEquals(
+			array( '.gandalf{color:grey;height:90px;border-style:dotted;}' ),
+			wp_styles()->registered['wp-style-engine-my-styles']->extra['after'],
+			'Registered styles with handle of "wp-style-engine-my-styles" do not match expected value from the Style Engine store.'
+		);
 	}
 }
