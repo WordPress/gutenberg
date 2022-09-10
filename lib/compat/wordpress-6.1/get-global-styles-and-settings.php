7bee925319f4edee14bf86f19e40a0d68694c529
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-09-10 23:35:47 +0900

    Add redeclaration check of `wp_add_global_styles_for_blocks`

--- lib/compat/wordpress-6.1/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.1/get-global-styles-and-settings.php
@@ -5,44 +5,46 @@
  * @package gutenberg
  */
 
-/**
- * Adds global style rules to the inline style for each block.
- */
-function wp_add_global_styles_for_blocks() {
-	$tree        = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
-	$block_nodes = $tree->get_styles_block_nodes();
-	foreach ( $block_nodes as $metadata ) {
-		$block_css = $tree->get_styles_for_block( $metadata );
+if ( ! function_exists( 'wp_add_global_styles_for_blocks' ) ) {
+	/**
+	 * Adds global style rules to the inline style for each block.
+	 */
+	function wp_add_global_styles_for_blocks() {
+		$tree        = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
+		$block_nodes = $tree->get_styles_block_nodes();
+		foreach ( $block_nodes as $metadata ) {
+			$block_css = $tree->get_styles_for_block( $metadata );
 
-		if ( ! wp_should_load_separate_core_block_assets() ) {
-			wp_add_inline_style( 'global-styles', $block_css );
-			continue;
-		}
+			if ( ! wp_should_load_separate_core_block_assets() ) {
+				wp_add_inline_style( 'global-styles', $block_css );
+				continue;
+			}
 
-		if ( isset( $metadata['name'] ) ) {
-			$block_name = str_replace( 'core/', '', $metadata['name'] );
-			// These block styles are added on block_render.
-			// This hooks inline CSS to them so that they are loaded conditionally
-			// based on whether or not the block is used on the page.
-			wp_add_inline_style( 'wp-block-' . $block_name, $block_css );
-		}
-
-		// The likes of block element styles from theme.json do not have  $metadata['name'] set.
-		if ( ! isset( $metadata['name'] ) && ! empty( $metadata['path'] ) ) {
-			$result = array_values(
-				array_filter(
-					$metadata['path'],
-					function ( $item ) {
-						if ( str_contains( $item, 'core/' ) ) {
-							return true;
-						}
-						return false;
-					}
-				)
-			);
-			if ( isset( $result[0] ) ) {
-				$block_name = str_replace( 'core/', '', $result[0] );
+			if ( isset( $metadata['name'] ) ) {
+				$block_name = str_replace( 'core/', '', $metadata['name'] );
+				// These block styles are added on block_render.
+				// This hooks inline CSS to them so that they are loaded conditionally
+				// based on whether or not the block is used on the page.
 				wp_add_inline_style( 'wp-block-' . $block_name, $block_css );
+			}
+
+			// The likes of block element styles from theme.json do not have  $metadata['name'] set.
+			if ( ! isset( $metadata['name'] ) && ! empty( $metadata['path'] ) ) {
+				$result = array_values(
+					array_filter(
+						$metadata['path'],
+						function ( $item ) {
+							if ( str_contains( $item, 'core/' ) ) {
+								return true;
+							}
+							return false;
+						}
+					)
+				);
+				if ( isset( $result[0] ) ) {
+					$block_name = str_replace( 'core/', '', $result[0] );
+					wp_add_inline_style( 'wp-block-' . $block_name, $block_css );
+				}
 			}
 		}
 	}
