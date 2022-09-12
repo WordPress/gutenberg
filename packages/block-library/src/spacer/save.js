c4704bbb906e25252442531a86e3d8e6c4b857ed
Glen Davies <glen.davies@automattic.com>
2022-09-12 14:32:31 +1200

    Only generate preset var if webw

--- packages/block-library/src/spacer/save.js
+++ packages/block-library/src/spacer/save.js
@@ -2,14 +2,21 @@
  * WordPress dependencies
  */
 import { useBlockProps, getSpacingPresetCssVar } from '@wordpress/block-editor';
+import { Platform } from '@wordpress/element';
 
 export default function save( { attributes: { height, width } } ) {
+	const currentHeight = Platform.isWeb
+		? getSpacingPresetCssVar( height )
+		: height;
+	const currentWidth = Platform.isWeb
+		? getSpacingPresetCssVar( width )
+		: width;
 	return (
 		<div
 			{ ...useBlockProps.save( {
 				style: {
-					height: getSpacingPresetCssVar( height ),
-					width: getSpacingPresetCssVar( width ),
+					height: currentHeight,
+					width: currentWidth,
 				},
 				'aria-hidden': true,
 			} ) }
