f42e47b967a773398749e9cbeaf972bd2f9dd6f0
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2022-09-12 13:32:35 +0530

    Migrate Nonce test to Playwright

---
+++ test/e2e/specs/editor/plugins/nonce.spec.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Nonce', () => {
+	// While using beforeEach/afterEach is suboptimal for multiple tests, they
+	// are used here to ensure that the nonce plugin doesn't interfere with API
+	// calls made in global before/after calls, which may perform API requests.
+	test.beforeEach( async ( { requestUtils } ) => {
+		await requestUtils.activatePlugin( 'gutenberg-test-plugin-nonce' );
+	} );
+	test.afterEach( async ( { requestUtils } ) => {
+		await requestUtils.deactivatePlugin( 'gutenberg-test-plugin-nonce' );
+	} );
+
+	test( 'should refresh when expired', async ( { page, admin } ) => {
+		await admin.createNewPost();
+		await page.keyboard.press( 'Enter' );
+		// eslint-disable-next-line no-restricted-syntax
+		await page.waitForTimeout( 5000 );
+		await page.keyboard.type( 'test' );
+		// `saveDraft` waits for saving to be successful, so this test would
+		// timeout if it's not.
+		await page.click( 'role=button[name=/Save draft/i]' );
+		// We expect a 403 status once.
+		page.on( 'console', ( message ) => {
+			expect( message.type() ).toBe( 'error' );
+		} );
+	} );
+} );
