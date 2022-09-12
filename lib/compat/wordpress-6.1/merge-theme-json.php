a923703ae0826aa2d2d03186004ed436da2b95cc
André <583546+oandregal@users.noreply.github.com>
2022-09-12 12:48:01 +0200

    Document helper function

--- lib/compat/wordpress-6.1/merge-theme-json.php
+++ lib/compat/wordpress-6.1/merge-theme-json.php
@@ -1,13 +1,30 @@
 <?php
+/**
+ * API to interact with a theme.json array.
+ *
+ * @package gutenberg
+ */
 
+/**
+ * Function to merge two theme.json arrays into a single one.
+ *
+ * @param array  $existing_data Array following the theme.json specification.
+ * @param array  $incoming_data Array following the theme.json specification.
+ * @param string $origin Optional. The origin of the data ('default', 'theme', 'user').
+ *                       If this function is used within a callback for filtering global styles data
+ *                       (e.g.: global_styles_filter_theme), the origin will be picked up
+ *                       automatically. If not provided and not running on a callback,
+ *                       it'll the default value ('theme' origin) will be used.
+ * @return array Merged array.
+ */
 function gutenberg_merge_theme_json( $existing_data, $incoming_data, $origin = null ) {
-	if ( $origin === null && doing_filter( 'global_styles_filter_theme' ) ) {
+	if ( null === $origin && doing_filter( 'global_styles_filter_theme' ) ) {
 		$origin = 'theme';
 	}
-	if ( $origin === null && doing_filter( 'global_styles_filter_default' ) ) {
+	if ( null === $origin && doing_filter( 'global_styles_filter_default' ) ) {
 		$origin = 'default';
 	}
-	if ( $origin === null && doing_filter( 'global_styles_filter_blocks' ) ) {
+	if ( null === $origin && doing_filter( 'global_styles_filter_blocks' ) ) {
 		$origin = 'core';
 	}
 
