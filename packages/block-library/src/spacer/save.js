40d6d022cce90068c6b46894f06c496e79b1b51e
Glen Davies <glen.davies@automattic.com>
2022-09-12 15:29:29 +1200

    Revert "Only generate preset var if webw"
    This reverts commit d8a355d1c5f937b8228f808c2ec6ab383b9affda.

--- packages/block-library/src/spacer/save.js
+++ packages/block-library/src/spacer/save.js
@@ -2,21 +2,14 @@
  * WordPress dependencies
  */
 import { useBlockProps, getSpacingPresetCssVar } from '@wordpress/block-editor';
-import { Platform } from '@wordpress/element';
 
 export default function save( { attributes: { height, width } } ) {
-	const currentHeight = Platform.isWeb
-		? getSpacingPresetCssVar( height )
-		: height;
-	const currentWidth = Platform.isWeb
-		? getSpacingPresetCssVar( width )
-		: width;
 	return (
 		<div
 			{ ...useBlockProps.save( {
 				style: {
-					height: currentHeight,
-					width: currentWidth,
+					height: getSpacingPresetCssVar( height ),
+					width: getSpacingPresetCssVar( width ),
 				},
 				'aria-hidden': true,
 			} ) }
