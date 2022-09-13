6b6dafdc1c45c6fb0aa94ddcc60d80b92822d7a9
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2022-09-13 16:49:24 +0530

    Addressed Feedbacks

--- test/e2e/specs/editor/plugins/nonce.spec.js
+++ test/e2e/specs/editor/plugins/nonce.spec.js
@@ -1,6 +1,9 @@
 /**
  * WordPress dependencies
  */
+/**
+ * WordPress dependencies
+ */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'Nonce', () => {
@@ -20,7 +23,7 @@ test.describe( 'Nonce', () => {
 		// eslint-disable-next-line no-restricted-syntax
 		await page.waitForTimeout( 5000 );
 		await page.keyboard.type( 'test' );
-		// `saveDraft` waits for saving to be successful, so this test would
+		// Click on save draft button so that this test would
 		// timeout if it's not.
 		await page.click( 'role=button[name=/Save draft/i]' );
 		// We expect a 403 status once.
