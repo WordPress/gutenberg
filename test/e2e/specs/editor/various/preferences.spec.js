cf3c1cda545cf1339bd4c44c0db52627629faae9
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2022-09-13 13:23:09 +0530

    Migrate Preferences Test to Playwright

---
+++ test/e2e/specs/editor/various/preferences.spec.js
@@ -0,0 +1,50 @@
+/**
+ * WordPress dependencies
+ */
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'preferences', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
+	} );
+
+	test( 'remembers sidebar dismissal between sessions', async ( {
+		page,
+	} ) => {
+		// Open by default.
+		await expect(
+			page.locator( '.edit-post-sidebar__panel-tab.is-active' )
+		).toHaveText( 'Post' );
+
+		// Change to "Block" tab.
+		await page.click( 'role=button[name="Block"i]' );
+
+		// Regression test: Reload resets to document tab.
+		//
+		// See: https://github.com/WordPress/gutenberg/issues/6377
+		// See: https://github.com/WordPress/gutenberg/pull/8995
+		await page.reload();
+		await page.waitForSelector( '.edit-post-layout' );
+		await expect(
+			page.locator( '.edit-post-sidebar__panel-tab.is-active' )
+		).toHaveText( 'Post' );
+
+		// Dismiss.
+		await page.click(
+			'.edit-post-sidebar__panel-tabs [aria-label="Close settings"]'
+		);
+		await expect(
+			page.locator( '.edit-post-sidebar__panel-tab.is-active' )
+		).not.toBeVisible();
+
+		// Remember after reload.
+		await page.reload();
+		await page.waitForSelector( '.edit-post-layout' );
+		await expect(
+			page.locator( '.edit-post-sidebar__panel-tab.is-active' )
+		).not.toBeVisible();
+	} );
+} );
