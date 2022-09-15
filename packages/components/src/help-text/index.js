ade76f0e37cebefed39e9a500b05231986e2b6e1
Siobhan <siobhan@automattic.com>
2022-09-15 20:10:32 +0100

    Adapt code to work with new Typescript component
    As the web's version of the ExternalLink component has been converted to Typescript, it's necessary to make some tweaks to the new HelpText component (which imports ExternalLink). This commit does the following:
    
    - Fixes the path ExternalLink is imported from.
    - Declares specific types for each prop.

--- packages/components/src/help-text/index.js
+++ packages/components/src/help-text/index.js
@@ -1,8 +1,14 @@
 /**
- * WordPress dependencies
+ * Internal dependencies
  */
-import { ExternalLink } from '@wordpress/components';
+import { ExternalLink } from '../external-link';
 
+/**
+ * @param {Object}                    props
+ * @param {string}                    props.moreLinkText
+ * @param {import('react').ReactNode} props.children
+ * @param {string}                    props.url
+ */
 const HelpText = ( { moreLinkText, children, url } ) => {
 	return (
 		<div>
