8ea8fa7d7b3ad456a3f2da4ee5b66683b5a4d341
Miguel Fonseca <miguelcsf@gmail.com>
2022-09-15 16:57:42 +0100

    Block Patterns: Add category for Headings & Titles
    Fixes #40116
    
    With this new category, we can clariy the classification of patterns as
    "Headers". Category "Headers" pertains to global patterns meant to sit
    at the top of a page, while category "Headings & Titles" pertains to
    typically visually distinctive elements that help structure the contents
    of a page.

--- lib/compat/wordpress-6.1/block-patterns.php
+++ lib/compat/wordpress-6.1/block-patterns.php
@@ -188,8 +188,13 @@ function gutenberg_register_core_block_patterns() {
 
 	// Register categories used for block patterns.
 	$pattern_category_registry = WP_Block_Pattern_Categories_Registry::get_instance();
+
 	if ( ! $pattern_category_registry->is_registered( 'footer' ) ) {
 		register_block_pattern_category( 'footer', array( 'label' => __( 'Footers', 'gutenberg' ) ) );
+	}
+
+	if ( ! $pattern_category_registry->is_registered( 'heading' ) ) {
+		register_block_pattern_category( 'heading', array( 'label' => __( 'Headings & Titles', 'gutenberg' ) ) );
 	}
 
 	if ( $should_register_core_patterns ) {
