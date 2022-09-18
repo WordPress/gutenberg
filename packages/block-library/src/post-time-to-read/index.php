49bff6f18ffa0c41b19a3d41ab66f9eaa32d9004
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-09-18 13:54:37 +0900

    Fix php lint error

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -20,17 +20,17 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
 
 	$minutes_to_read = ! empty( $attributes['minutesToRead'] ) ? (int) $attributes['minutesToRead'] : 0;
 
-	$minutes_to_read_string = $minutes_to_read !== 0
+	$minutes_to_read_string = 0 !== $minutes_to_read
 		? sprintf(
-				/* translators: %d is the number of minutes the post will take to read. */
-				_n(
-					'You can read this post in %d minute.',
-					'You can read this post in %d minutes.',
-					$minutes_to_read
-				),
+			/* translators: %d is the number of minutes the post will take to read. */
+			_n(
+				'You can read this post in %d minute.',
+				'You can read this post in %d minutes.',
 				$minutes_to_read
-		  )
-		: __( 'You can read this post less than a minute.' );
+			),
+			$minutes_to_read
+		)
+	: __( 'You can read this post less than a minute.' );
 
 	$align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
 
