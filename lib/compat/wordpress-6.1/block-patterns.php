53b0f8477b5f167410d2a083e32c9312a06a1976
Miguel Fonseca <miguelcsf@gmail.com>
2022-09-19 15:07:51 +0100

    Block Patterns: Add category for banners (#44203)
    * Block Patterns: Add category for Headings & Titles
    
    Fixes #40116
    
    With this new category, we can clariy the classification of patterns as
    "Headers". Category "Headers" pertains to global patterns meant to sit
    at the top of a page, while category "Headings & Titles" pertains to
    typically visually distinctive elements that help structure the contents
    of a page.
    
    * Rename to "Banners"

--- lib/compat/wordpress-6.1/block-patterns.php
+++ lib/compat/wordpress-6.1/block-patterns.php
@@ -188,8 +188,13 @@ function gutenberg_register_core_block_patterns() {
 
 	// Register categories used for block patterns.
 	$pattern_category_registry = WP_Block_Pattern_Categories_Registry::get_instance();
+
 	if ( ! $pattern_category_registry->is_registered( 'footer' ) ) {
 		register_block_pattern_category( 'footer', array( 'label' => __( 'Footers', 'gutenberg' ) ) );
+	}
+
+	if ( ! $pattern_category_registry->is_registered( 'banner' ) ) {
+		register_block_pattern_category( 'banner', array( 'label' => __( 'Banners', 'gutenberg' ) ) );
 	}
 
 	if ( $should_register_core_patterns ) {
