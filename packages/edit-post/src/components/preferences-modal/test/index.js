dd72ac164bafb01b0817c796149c4c9907441bd1
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-09-21 17:19:05 +0300

    fix pref modal bug introduced by prev commit

--- packages/edit-post/src/components/preferences-modal/test/index.js
+++ packages/edit-post/src/components/preferences-modal/test/index.js
@@ -39,7 +39,7 @@ describe( 'EditPostPreferencesModal', () => {
 	} );
 
 	it( 'should not render when the modal is not active', () => {
-		useSelect.mockImplementation( () => [ false, false ] );
+		useSelect.mockImplementation( () => false );
 		render( <EditPostPreferencesModal /> );
 		expect(
 			screen.queryByRole( 'dialog', { name: 'Preferences' } )
