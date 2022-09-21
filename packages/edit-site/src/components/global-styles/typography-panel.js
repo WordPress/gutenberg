0d1244662a97f750503a29a3edabafe2b4a2b7ef
Robert Anderson <robert@noisysocks.com>
2022-09-21 16:27:00 +1000

    Move heading selector and typography preview out of ToolsPanel

--- packages/edit-site/src/components/global-styles/typography-panel.js
+++ packages/edit-site/src/components/global-styles/typography-panel.js
@@ -9,13 +9,11 @@ import {
 } from '@wordpress/block-editor';
 import {
 	FontSizePicker,
-	__experimentalToggleGroupControl as ToggleGroupControl,
-	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
 	__experimentalToolsPanel as ToolsPanel,
 	__experimentalToolsPanelItem as ToolsPanelItem,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
+
 /**
  * Internal dependencies
  */
@@ -182,12 +180,11 @@ function useLetterSpacing( prefix, name ) {
  * - combine the simple ones into a single hook since the logic is the same
  */
 
-export default function TypographyPanel( { name, element } ) {
-	const [ selectedLevel, setCurrentTab ] = useState( 'heading' );
+export default function TypographyPanel( { name, element, headingLevel } ) {
 	const supports = getSupportedGlobalStylesPanels( name );
 	let prefix = '';
 	if ( element === 'heading' ) {
-		prefix = `elements.${ selectedLevel }.`;
+		prefix = `elements.${ headingLevel }.`;
 	} else if ( element && element !== 'text' ) {
 		prefix = `elements.${ element }.`;
 	}
@@ -209,7 +206,7 @@ export default function TypographyPanel( { name, element } ) {
 
 	/* Disable font size controls when the option to style all headings is selected. */
 	let hasFontSizeEnabled = supports.includes( 'fontSize' );
-	if ( element === 'heading' && selectedLevel === 'heading' ) {
+	if ( element === 'heading' && headingLevel === 'heading' ) {
 		hasFontSizeEnabled = false;
 	}
 
@@ -244,172 +241,101 @@ export default function TypographyPanel( { name, element } ) {
 		resetLetterSpacing();
 	};
 
-	const [ backgroundColor ] = useStyle( prefix + 'color.background', name );
-	const [ gradientValue ] = useStyle( prefix + 'color.gradient', name );
-	const [ color ] = useStyle( prefix + 'color.text', name );
-	const extraStyles =
-		element === 'link'
-			? {
-					textDecoration: 'underline',
-			  }
-			: {};
-
 	return (
-		<>
-			<ToolsPanel label={ __( 'Typography' ) } resetAll={ resetAll }>
-				<div
-					className="edit-site-typography-panel__preview span-columns"
-					style={ {
-						fontFamily: fontFamily ?? 'serif',
-						background: gradientValue ?? backgroundColor,
-						color,
-						fontSize,
-						fontStyle,
-						fontWeight,
-						letterSpacing,
-						...extraStyles,
-					} }
+		<ToolsPanel label={ __( 'Typography' ) } resetAll={ resetAll }>
+			{ supports.includes( 'fontFamily' ) && (
+				<ToolsPanelItem
+					label={ __( 'Font family' ) }
+					hasValue={ hasFontFamily }
+					onDeselect={ resetFontFamily }
+					isShownByDefault
 				>
-					Aa
-				</div>
-				{ element === 'heading' && (
-					<div className="span-columns">
-						<ToggleGroupControl
-							label={ __( 'Select heading level' ) }
-							hideLabelFromVision
-							value={ selectedLevel }
-							onChange={ setCurrentTab }
-							isBlock
-							size="__unstable-large"
-							__nextHasNoMarginBottom
-						>
-							<ToggleGroupControlOption
-								value="heading"
-								/* translators: 'All' refers to selecting all heading levels 
-							and applying the same style to h1-h6. */
-								label={ __( 'All' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h1"
-								label={ __( 'H1' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h2"
-								label={ __( 'H2' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h3"
-								label={ __( 'H3' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h4"
-								label={ __( 'H4' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h5"
-								label={ __( 'H5' ) }
-							/>
-							<ToggleGroupControlOption
-								value="h6"
-								label={ __( 'H6' ) }
-							/>
-						</ToggleGroupControl>
-					</div>
-				) }
-				{ supports.includes( 'fontFamily' ) && (
-					<ToolsPanelItem
-						label={ __( 'Font family' ) }
-						hasValue={ hasFontFamily }
-						onDeselect={ resetFontFamily }
-						isShownByDefault
-					>
-						<FontFamilyControl
-							fontFamilies={ fontFamilies }
-							value={ fontFamily }
-							onChange={ setFontFamily }
-							size="__unstable-large"
-							__nextHasNoMarginBottom
-						/>
-					</ToolsPanelItem>
-				) }
-				{ hasFontSizeEnabled && (
-					<ToolsPanelItem
-						label={ __( 'Font size' ) }
-						hasValue={ hasFontSize }
-						onDeselect={ resetFontSize }
-						isShownByDefault
-					>
-						<FontSizePicker
-							value={ fontSize }
-							onChange={ setFontSize }
-							fontSizes={ fontSizes }
-							disableCustomFontSizes={ disableCustomFontSizes }
-							size="__unstable-large"
-							__nextHasNoMarginBottom
-						/>
-					</ToolsPanelItem>
-				) }
-				{ hasAppearanceControl && (
-					<ToolsPanelItem
-						className="single-column"
-						label={ __( 'Appearance' ) } // TODO: Should say 'Font weight' and 'Font style' if only one is enabled
-						hasValue={ hasFontAppearance }
-						onDeselect={ resetFontAppearance }
-						isShownByDefault
-					>
-						<FontAppearanceControl
-							value={ {
-								fontStyle,
-								fontWeight,
-							} }
-							onChange={ ( {
-								fontStyle: newFontStyle,
-								fontWeight: newFontWeight,
-							} ) => {
-								setFontStyle( newFontStyle );
-								setFontWeight( newFontWeight );
-							} }
-							hasFontStyles={ hasFontStyles }
-							hasFontWeights={ hasFontWeights }
-							size="__unstable-large"
-							__nextHasNoMarginBottom
-						/>
-					</ToolsPanelItem>
-				) }
-				{ hasLineHeightEnabled && (
-					<ToolsPanelItem
-						className="single-column"
-						label={ __( 'Line height' ) }
-						hasValue={ hasLineHeight }
-						onDeselect={ resetLineHeight }
-						isShownByDefault
-					>
-						<LineHeightControl
-							__nextHasNoMarginBottom
-							__unstableInputWidth="auto"
-							value={ lineHeight }
-							onChange={ setLineHeight }
-							size="__unstable-large"
-						/>
-					</ToolsPanelItem>
-				) }
-				{ hasLetterSpacingControl && (
-					<ToolsPanelItem
-						className="single-column"
-						label={ __( 'Letter spacing' ) }
-						hasValue={ hasLetterSpacing }
-						onDeselect={ resetLetterSpacing }
-						isShownByDefault
-					>
-						<LetterSpacingControl
-							value={ letterSpacing }
-							onChange={ setLetterSpacing }
-							size="__unstable-large"
-							__unstableInputWidth="auto"
-						/>
-					</ToolsPanelItem>
-				) }
-			</ToolsPanel>
-		</>
+					<FontFamilyControl
+						fontFamilies={ fontFamilies }
+						value={ fontFamily }
+						onChange={ setFontFamily }
+						size="__unstable-large"
+						__nextHasNoMarginBottom
+					/>
+				</ToolsPanelItem>
+			) }
+			{ hasFontSizeEnabled && (
+				<ToolsPanelItem
+					label={ __( 'Font size' ) }
+					hasValue={ hasFontSize }
+					onDeselect={ resetFontSize }
+					isShownByDefault
+				>
+					<FontSizePicker
+						value={ fontSize }
+						onChange={ setFontSize }
+						fontSizes={ fontSizes }
+						disableCustomFontSizes={ disableCustomFontSizes }
+						size="__unstable-large"
+						__nextHasNoMarginBottom
+					/>
+				</ToolsPanelItem>
+			) }
+			{ hasAppearanceControl && (
+				<ToolsPanelItem
+					className="single-column"
+					label={ __( 'Appearance' ) } // TODO: Should say 'Font weight' and 'Font style' if only one is enabled
+					hasValue={ hasFontAppearance }
+					onDeselect={ resetFontAppearance }
+					isShownByDefault
+				>
+					<FontAppearanceControl
+						value={ {
+							fontStyle,
+							fontWeight,
+						} }
+						onChange={ ( {
+							fontStyle: newFontStyle,
+							fontWeight: newFontWeight,
+						} ) => {
+							setFontStyle( newFontStyle );
+							setFontWeight( newFontWeight );
+						} }
+						hasFontStyles={ hasFontStyles }
+						hasFontWeights={ hasFontWeights }
+						size="__unstable-large"
+						__nextHasNoMarginBottom
+					/>
+				</ToolsPanelItem>
+			) }
+			{ hasLineHeightEnabled && (
+				<ToolsPanelItem
+					className="single-column"
+					label={ __( 'Line height' ) }
+					hasValue={ hasLineHeight }
+					onDeselect={ resetLineHeight }
+					isShownByDefault
+				>
+					<LineHeightControl
+						__nextHasNoMarginBottom
+						__unstableInputWidth="auto"
+						value={ lineHeight }
+						onChange={ setLineHeight }
+						size="__unstable-large"
+					/>
+				</ToolsPanelItem>
+			) }
+			{ hasLetterSpacingControl && (
+				<ToolsPanelItem
+					className="single-column"
+					label={ __( 'Letter spacing' ) }
+					hasValue={ hasLetterSpacing }
+					onDeselect={ resetLetterSpacing }
+					isShownByDefault
+				>
+					<LetterSpacingControl
+						value={ letterSpacing }
+						onChange={ setLetterSpacing }
+						size="__unstable-large"
+						__unstableInputWidth="auto"
+					/>
+				</ToolsPanelItem>
+			) }
+		</ToolsPanel>
 	);
 }
