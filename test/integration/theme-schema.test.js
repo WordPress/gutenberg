c19d390eab814b30f1b0c8edf76fdb619ff5fdf2
Alex Lende <alex@lende.xyz>
2022-09-21 11:46:20 -0700

    Add note about using compile instead of validateSchema

--- test/integration/theme-schema.test.js
+++ test/integration/theme-schema.test.js
@@ -15,7 +15,11 @@ describe( 'theme.json schema', () => {
 		allowMatchingProperties: true,
 	} );
 
-	it( 'compiles in strict mode', async () => {
+	it( 'strictly adheres to the draft-04 meta schema', () => {
+		// Use ajv.compile instead of ajv.validateSchema to validate the schema
+		// because validateSchema only checks syntax, whereas, compile checks
+		// if the schema is semantically correct with strict mode.
+		// See https://github.com/ajv-validator/ajv/issues/1434#issuecomment-822982571
 		const result = ajv.compile( themeSchema );
 
 		expect( result.errors ).toBe( null );
