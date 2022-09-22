eba030ce943c7d5335e2adeb7504f5b558987a4a
Ben Dwyer <ben@scruffian.com>
2022-09-22 10:59:09 +0100

    make the callback from the useEffect return a function not a promise

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -264,23 +264,25 @@ function Navigation( {
 		setRef( fallbackNavigationMenus[ 0 ].id );
 	}, [ navigationMenus ] );
 
-	useEffect( async () => {
-		if (
-			! hasResolvedNavigationMenus ||
-			isConvertingClassicMenu ||
-			fallbackNavigationMenus?.length > 0 ||
-			classicMenus?.length !== 1
-		) {
-			return false;
-		}
+	useEffect( () => {
+		( async () => {
+			if (
+				! hasResolvedNavigationMenus ||
+				isConvertingClassicMenu ||
+				fallbackNavigationMenus?.length > 0 ||
+				classicMenus?.length !== 1
+			) {
+				return false;
+			}
 
-		// If there's non fallback navigation menus and
-		// only one classic menu then create a new navigation menu based on it.
-		await convertClassicMenu(
-			classicMenus[ 0 ].id,
-			classicMenus[ 0 ].name,
-			'publish'
-		);
+			// If there's non fallback navigation menus and
+			// only one classic menu then create a new navigation menu based on it.
+			await convertClassicMenu(
+				classicMenus[ 0 ].id,
+				classicMenus[ 0 ].name,
+				'publish'
+			);
+		} )();
 	}, [ hasResolvedNavigationMenus ] );
 
 	const navRef = useRef();
