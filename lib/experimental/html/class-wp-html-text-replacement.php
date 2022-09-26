232b9abcf3a94a079e24076327214387cc4144d7
Dennis Snell <dennis.snell@automattic.com>
2022-09-26 00:08:16 -0700

    Tag processor: update @since version tags to 6.2.0 (#44432)
    Previously we were expecting this to land in 6.1.0 but it was delayed so
    this patch updates the date of inclusion to match the new release date.

--- lib/experimental/html/class-wp-html-text-replacement.php
+++ lib/experimental/html/class-wp-html-text-replacement.php
@@ -4,7 +4,7 @@
  *
  * @package WordPress
  * @subpackage HTML
- * @since 6.1.0
+ * @since 6.2.0
  */
 
 /**
@@ -13,7 +13,7 @@
  * This class is for internal usage of the WP_HTML_Tag_Processor class.
  *
  * @access private
- * @since 6.1.0
+ * @since 6.2.0
  *
  * @see WP_HTML_Tag_Processor
  */
@@ -21,7 +21,7 @@ class WP_HTML_Text_Replacement {
 	/**
 	 * Byte offset into document where replacement span begins.
 	 *
-	 * @since 6.1.0
+	 * @since 6.2.0
 	 * @var int
 	 */
 	public $start;
@@ -29,7 +29,7 @@ class WP_HTML_Text_Replacement {
 	/**
 	 * Byte offset into document where replacement span ends.
 	 *
-	 * @since 6.1.0
+	 * @since 6.2.0
 	 * @var int
 	 */
 	public $end;
@@ -37,7 +37,7 @@ class WP_HTML_Text_Replacement {
 	/**
 	 * Span of text to insert in document to replace existing content from start to end.
 	 *
-	 * @since 6.1.0
+	 * @since 6.2.0
 	 * @var string
 	 */
 	public $text;
@@ -45,7 +45,7 @@ class WP_HTML_Text_Replacement {
 	/**
 	 * Constructor.
 	 *
-	 * @since 6.1.0
+	 * @since 6.2.0
 	 *
 	 * @param int    $start Byte offset into document where replacement span begins.
 	 * @param int    $end   Byte offset into document where replacement span ends.
