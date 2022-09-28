a70644917e92995956e8f569085fb1ad43019a66
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-09-28 20:29:35 +0600

    migrate iframe-rendering test case

---
+++ test/e2e/specs/site-editor/iframe-rendering.spec.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Site editor iframe rendering mode', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( 'emptytheme' );
+	} );
+
+	test.afterAll( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( 'twentytwentyone' );
+	} );
+
+	test( 'Should render editor in standards mode.', async ( {
+		admin,
+		page,
+	} ) => {
+		await admin.visitSiteEditor( {
+			postId: 'emptytheme//index',
+			postType: 'wp_template',
+		} );
+
+		const compatMode = await page.evaluate(
+			() =>
+				document.querySelector( `iframe[name='editor-canvas']` )
+					.contentDocument.compatMode
+		);
+
+		// CSS1Compat = expected standards mode.
+		// BackCompat = quirks mode.
+		expect( compatMode ).toBe( 'CSS1Compat' );
+	} );
+} );
