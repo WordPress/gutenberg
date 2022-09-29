c47ee84dee6af8dd2c761e3d750f93593f5619f8
Ari Stathopoulos <aristath@gmail.com>
2022-09-29 10:33:18 +0300

    Add default action to send an email on form-submission

--- packages/block-library/src/form/index.php
+++ packages/block-library/src/form/index.php
@@ -47,12 +47,19 @@ function render_block_core_form( $attributes, $content, $block ) {
 	 */
 	$method = apply_filters( 'block_form_method', $method, $attributes, $content, $block );
 
+	$form_hash = md5( json_encode( array( $attributes, $block ) ) );
 	return str_replace(
-		'<form ',
-		sprintf(
-			'<form action="%1$s" method="%2$s" ',
-			esc_attr( $action ),
-			esc_attr( $method )
+		array(
+			'<form ',
+			'</form>',
+		),
+		array(
+			sprintf(
+				'<form action="%1$s" method="%2$s" ',
+				esc_attr( $action ),
+				esc_attr( $method )
+			),
+			'<input type="hidden" name="block-form-hash" value="' . $form_hash . '" /><form>',
 		),
 		$content
 	);
@@ -70,3 +77,31 @@ function register_block_core_form() {
 	);
 }
 add_action( 'init', 'register_block_core_form' );
+
+/**
+ * Handles the form submission.
+ *
+ * This is the default action, which sends an email to the admin.
+ * To override the default action, first remove the default action:
+ * remove_action( 'wp', 'submit_core_form_block' );
+ * Then add your own action by creating a function and hooking it to the 'wp' action.
+ */
+function submit_core_form_block() {
+	if ( ! isset( $_POST['block-form-hash'] ) ) {
+		return;
+	}
+	$content = sprintf(
+		/* translators: %s: The request URI. */
+		__( 'Form submission from %1$s', 'gutenberg' ) . '</br>',
+		esc_url( get_site_url() . $_SERVER['REQUEST_URI'] )
+	);
+	foreach ( $_POST as $key => $value ) {
+		if ( 'block-form-hash' === $key ) {
+			continue;
+		}
+		$content .= $key . ': ' . $value . '</br>';
+	}
+
+	wp_mail( get_option( 'admin_email' ), __( 'Form submission', 'gutenberg' ), $content );
+}
+add_action( 'wp', 'submit_core_form_block' );
