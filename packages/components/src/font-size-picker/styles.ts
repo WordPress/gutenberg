62653b6ba51e7b3f3e8f76cf3800f99a499a0d67
Robert Anderson <robert@noisysocks.com>
2022-09-29 14:41:06 +1000

    FontSizePicker: Make control take up full width
    Removes the 280px max-width from FontSizePicker so as to be more
    consistent with other components. Styling such as this is the
    responsibility of the caller.

--- packages/components/src/font-size-picker/styles.ts
+++ packages/components/src/font-size-picker/styles.ts
@@ -10,6 +10,7 @@ import BaseControl from '../base-control';
 import Button from '../button';
 import { space } from '../ui/utils/space';
 import { COLORS } from '../utils';
+import type { FontSizePickerProps } from './types';
 
 export const Container = styled.fieldset`
 	border: 0;
@@ -26,19 +27,18 @@ export const HeaderHint = styled.span`
 	margin-left: ${ space( 1 ) };
 `;
 
-// 280px is the sidebar width.
-// TODO: Remove this, @wordpress/components shouldn't care what a "sidebar" is.
 export const Controls = styled.div< {
 	__nextHasNoMarginBottom: boolean;
 } >`
-	max-width: calc( 280px - ${ space( 4 ) } * 2 );
-
 	${ ( props ) =>
 		! props.__nextHasNoMarginBottom && `margin-bottom: ${ space( 6 ) };` }
 `;
 
-export const ResetButton = styled( Button )`
+export const ResetButton = styled( Button )< {
+	size: FontSizePickerProps[ 'size' ];
+} >`
 	&&& {
-		height: 30px;
+		height: ${ ( props ) =>
+			props.size === '__unstable-large' ? '40px' : '30px' };
 	}
 `;
