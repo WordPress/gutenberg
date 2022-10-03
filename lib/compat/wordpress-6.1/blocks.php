7d99c49fd267de4c1cccda0224f35a20c8e887f6
Juliette <663378+jrfnl@users.noreply.github.com>
2022-10-03 16:25:32 +0200

    CS: remove redundant parentheses for include/require statements (#44544)
    Co-authored-by: jrfnl <jrfnl@users.noreply.github.com>

--- lib/compat/wordpress-6.1/blocks.php
+++ lib/compat/wordpress-6.1/blocks.php
@@ -81,7 +81,7 @@ function gutenberg_block_type_metadata_view_script( $settings, $metadata ) {
 
 		// Replace suffix and extension with `.asset.php` to find the generated dependencies file.
 		$view_asset_file          = substr( $view_script_path, 0, -( strlen( '.js' ) ) ) . '.asset.php';
-		$view_asset               = file_exists( $view_asset_file ) ? require( $view_asset_file ) : null;
+		$view_asset               = file_exists( $view_asset_file ) ? require $view_asset_file : null;
 		$view_script_dependencies = isset( $view_asset['dependencies'] ) ? $view_asset['dependencies'] : array();
 		$view_script_version      = isset( $view_asset['version'] ) ? $view_asset['version'] : false;
 		$result                   = wp_register_script(
