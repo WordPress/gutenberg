62e46a472c3638cfee79fd023937bcd2fd416f84
Luis Herranz <luisherranz@gmail.com>
2022-10-03 17:11:24 +0200

    Move stuff to utils

--- packages/block-library/src/navigation/deep-signal.js
+++ packages/block-library/src/navigation/deep-signal.js
@@ -1,37 +1,15 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable no-undef */
 /**
  * External dependencies
  */
 import { signal } from '@preact/signals';
 
+/**
+ * Internal dependencies
+ */
+import { knownSymbols, shouldWrap } from './utils';
+
 const proxyToSignals = new WeakMap();
 const objToProxy = new WeakMap();
-const knownSymbols = new Set(
-	Object.getOwnPropertyNames( Symbol )
-		.map( ( key ) => Symbol[ key ] )
-		.filter( ( value ) => typeof value === 'symbol' )
-);
-const supported = new Set( [
-	Object,
-	Array,
-	Int8Array,
-	Uint8Array,
-	Uint8ClampedArray,
-	Int16Array,
-	Uint16Array,
-	Int32Array,
-	Uint32Array,
-	Float32Array,
-	Float64Array,
-] );
-const shouldWrap = ( { constructor } ) => {
-	const isBuiltIn =
-		typeof constructor === 'function' &&
-		constructor.name in globalThis &&
-		globalThis[ constructor.name ] === constructor;
-	return ! isBuiltIn || supported.has( constructor );
-};
 
 export const deepSignal = ( obj ) => new Proxy( obj, handlers );
 export const options = { returnSignal: /^\$/ };
--- packages/block-library/src/navigation/utils.js
+++ packages/block-library/src/navigation/utils.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-undef */
+
 // For wrapperless hydration of document.body.
 // See https://gist.github.com/developit/f4c67a2ede71dc2fab7f357f39cff28c
 export const createRootFragment = ( parent, replaceNode ) => {
@@ -22,3 +25,29 @@ export const createRootFragment = ( parent, replaceNode ) => {
 // Helper function to await until the CPU is idle.
 export const idle = () =>
 	new Promise( ( resolve ) => window.requestIdleCallback( resolve ) );
+
+export const knownSymbols = new Set(
+	Object.getOwnPropertyNames( Symbol )
+		.map( ( key ) => Symbol[ key ] )
+		.filter( ( value ) => typeof value === 'symbol' )
+);
+const supported = new Set( [
+	Object,
+	Array,
+	Int8Array,
+	Uint8Array,
+	Uint8ClampedArray,
+	Int16Array,
+	Uint16Array,
+	Int32Array,
+	Uint32Array,
+	Float32Array,
+	Float64Array,
+] );
+export const shouldWrap = ( { constructor } ) => {
+	const isBuiltIn =
+		typeof constructor === 'function' &&
+		constructor.name in globalThis &&
+		globalThis[ constructor.name ] === constructor;
+	return ! isBuiltIn || supported.has( constructor );
+};
