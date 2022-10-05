6cbca5186f315a1b5e94b266a1cf3eed14f3cc21
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-10-05 17:13:26 +0900

    Calculate time only when content is changed

--- packages/block-library/src/post-time-to-read/edit.js
+++ packages/block-library/src/post-time-to-read/edit.js
@@ -7,6 +7,7 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import { _x, _n, __, sprintf } from '@wordpress/i18n';
+import { useMemo } from '@wordpress/element';
 import {
 	AlignmentControl,
 	BlockControls,
@@ -59,42 +60,46 @@ function PostTimeToReadEdit( {
 		id: postId,
 	} );
 
-	// Replicates the logic found in getEditedPostContent().
-	let content;
-	if ( contentStructure instanceof Function ) {
-		content = contentStructure( { blocks } );
-	} else if ( blocks ) {
-		// If we have parsed blocks already, they should be our source of truth.
-		// Parsing applies block deprecations and legacy block conversions that
-		// unparsed content will not have.
-		content = __unstableSerializeAndClean( blocks );
-	} else {
-		content = contentStructure;
-	}
+	const minutesToReadString = useMemo( () => {
+		// Replicates the logic found in getEditedPostContent().
+		let content;
+		if ( contentStructure instanceof Function ) {
+			content = contentStructure( { blocks } );
+		} else if ( blocks ) {
+			// If we have parsed blocks already, they should be our source of truth.
+			// Parsing applies block deprecations and legacy block conversions that
+			// unparsed content will not have.
+			content = __unstableSerializeAndClean( blocks );
+		} else {
+			content = contentStructure;
+		}
 
-	/*
-	 * translators: If your word count is based on single characters (e.g. East Asian characters),
-	 * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
-	 * Do not translate into your own language.
-	 */
-	const wordCountType = _x( 'words', 'Word count type. Do not translate!' );
+		/*
+		 * translators: If your word count is based on single characters (e.g. East Asian characters),
+		 * enter 'characters_excluding_spaces' or 'characters_including_spaces'. Otherwise, enter 'words'.
+		 * Do not translate into your own language.
+		 */
+		const wordCountType = _x(
+			'words',
+			'Word count type. Do not translate!'
+		);
 
-	const minutesToRead = Math.round(
-		wordCount( content, wordCountType ) / AVERAGE_READING_RATE
-	);
+		const minutesToRead = Math.round(
+			wordCount( content, wordCountType ) / AVERAGE_READING_RATE
+		);
 
-	let minutesToReadString = __( 'There is no content.' );
+		if ( minutesToRead === undefined ) {
+			return __( 'There is no content.' );
+		}
 
-	if ( minutesToRead !== undefined ) {
-		minutesToReadString =
-			minutesToRead !== 0
-				? sprintf(
-						/* translators: %d is the number of minutes the post will take to read. */
-						_n( '%d minute', '%d minutes', minutesToRead ),
-						minutesToRead
-				  )
-				: __( 'less than a minute' );
-	}
+		return minutesToRead !== 0
+			? sprintf(
+					/* translators: %d is the number of minutes the post will take to read. */
+					_n( '%d minute', '%d minutes', minutesToRead ),
+					minutesToRead
+			  )
+			: __( 'less than a minute' );
+	}, [ contentStructure, blocks ] );
 
 	const blockProps = useBlockProps( {
 		className: classnames( {
