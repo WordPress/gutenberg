a0a8903543df31bd77a0c89a4461b0097142e6fb
Marco Ciampini <marco.ciampo@gmail.com>
2022-10-05 12:38:24 +0200

    Refactor styles to separate file

--- packages/components/src/theme/index.tsx
+++ packages/components/src/theme/index.tsx
@@ -1,36 +1,9 @@
 /**
- * External dependencies
- */
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-
-import { colord } from 'colord';
-
-/**
  * Internal dependencies
  */
 import type { ThemeProps } from './types';
 import type { WordPressComponentProps } from '../ui/context';
-
-const colors = ( { accent }: ThemeProps ) => {
-	const accentColor = accent
-		? css`
-				--wp-components-color-accent: ${ accent };
-				--wp-components-color-accent-darker-10: ${ colord( accent )
-					.darken( 0.1 )
-					.toHex() };
-				--wp-components-color-accent-darker-20: ${ colord( accent )
-					.darken( 0.2 )
-					.toHex() };
-		  `
-		: undefined;
-
-	return accentColor;
-};
-
-const Wrapper = styled.div`
-	${ colors }
-`;
+import { Wrapper } from './styles';
 
 function Theme( props: WordPressComponentProps< ThemeProps, 'div', true > ) {
 	return <Wrapper { ...props } />;
