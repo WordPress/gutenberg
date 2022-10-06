88eff634bcb85425d8edf3deb6f0c28461d0e1f9
ramonjd <ramonjd@gmail.com>
2022-10-06 15:38:25 +1100

    Moves style engine PHP unit tests to Gutenberg phpunit directory and adds '_Gutenberg' and 'gutenberg_' prefixes/suffixes. After 6.1, the style engine classes and methods are in Core. Moving the tests and using the Gutenberg functions and classes allows us to continue development and avoid class collisions. This PR also proposes to add a bash script to make backporting more easy.

--- phpunit/style-engine/class-wp-style-engine-css-rules-store-test.php
+++ phpunit/style-engine/class-wp-style-engine-css-rules-store-test.php
@@ -6,33 +6,17 @@
  * @subpackage style-engine
  */
 
-// Check for the existence of Style Engine classes and methods.
-// Once the Style Engine has been migrated to Core we can remove the if statements and require imports.
-// Testing new features from the Gutenberg package may require
-// testing against `gutenberg_` and `_Gutenberg` functions and methods in the future.
-if ( ! class_exists( 'WP_Style_Engine_CSS_Declarations' ) ) {
-	require __DIR__ . '/../class-wp-style-engine-css-declarations.php';
-}
-
-if ( ! class_exists( 'WP_Style_Engine_CSS_Rule' ) ) {
-	require __DIR__ . '/../class-wp-style-engine-css-rule.php';
-}
-
-if ( ! class_exists( 'WP_Style_Engine_CSS_Rules_Store' ) ) {
-	require __DIR__ . '/../class-wp-style-engine-css-rules-store.php';
-}
-
 /**
  * Tests for registering, storing and retrieving a collection of CSS Rules (a store).
  *
- * @coversDefaultClass WP_Style_Engine_CSS_Rules_Store
+ * @coversDefaultClass WP_Style_Engine_CSS_Rules_Store_Gutenberg
  */
 class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	/**
 	 * Cleans up stores after each test.
 	 */
 	public function tear_down() {
-		WP_Style_Engine_CSS_Rules_Store::remove_all_stores();
+		WP_Style_Engine_CSS_Rules_Store_Gutenberg::remove_all_stores();
 		parent::tear_down();
 	}
 
@@ -42,9 +26,9 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::__construct
 	 */
 	public function test_should_create_new_store_on_instantiation() {
-		$new_pancakes_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'pancakes-with-strawberries' );
+		$new_pancakes_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'pancakes-with-strawberries' );
 
-		$this->assertInstanceOf( 'WP_Style_Engine_CSS_Rules_Store', $new_pancakes_store );
+		$this->assertInstanceOf( 'WP_Style_Engine_CSS_Rules_Store_Gutenberg', $new_pancakes_store );
 	}
 
 	/**
@@ -53,15 +37,15 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::get_store
 	 */
 	public function test_should_not_create_store_without_a_store_name() {
-		$not_a_store = WP_Style_Engine_CSS_Rules_Store::get_store( '' );
+		$not_a_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( '' );
 
 		$this->assertEmpty( $not_a_store, 'get_store() did not return an empty value with empty string as argument.' );
 
-		$also_not_a_store = WP_Style_Engine_CSS_Rules_Store::get_store( 123 );
+		$also_not_a_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 123 );
 
 		$this->assertEmpty( $also_not_a_store, 'get_store() did not return an empty value with number as argument.' );
 
-		$definitely_not_a_store = WP_Style_Engine_CSS_Rules_Store::get_store( null );
+		$definitely_not_a_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( null );
 
 		$this->assertEmpty( $definitely_not_a_store, 'get_store() did not return an empty value with `null` as argument.' );
 	}
@@ -72,14 +56,14 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::get_store
 	 */
 	public function test_should_return_existing_store() {
-		$new_fish_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'fish-n-chips' );
+		$new_fish_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'fish-n-chips' );
 		$selector       = '.haddock';
 
 		$new_fish_store->add_rule( $selector );
 
 		$this->assertSame( $selector, $new_fish_store->add_rule( $selector )->get_selector(), 'Selector string of store rule does not match expected value' );
 
-		$the_same_fish_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'fish-n-chips' );
+		$the_same_fish_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'fish-n-chips' );
 
 		$this->assertSame( $selector, $the_same_fish_store->add_rule( $selector )->get_selector(), 'Selector string of existing store rule does not match expected value' );
 	}
@@ -90,15 +74,15 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::get_stores
 	 */
 	public function test_should_get_all_existing_stores() {
-		$burrito_store    = WP_Style_Engine_CSS_Rules_Store::get_store( 'burrito' );
-		$quesadilla_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'quesadilla' );
+		$burrito_store    = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'burrito' );
+		$quesadilla_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'quesadilla' );
 
 		$this->assertEquals(
 			array(
 				'burrito'    => $burrito_store,
 				'quesadilla' => $quesadilla_store,
 			),
-			WP_Style_Engine_CSS_Rules_Store::get_stores()
+			WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_stores()
 		);
 	}
 
@@ -108,22 +92,22 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::remove_all_stores
 	 */
 	public function test_should_remove_all_stores() {
-		$dolmades_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'dolmades' );
-		$tzatziki_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'tzatziki' );
+		$dolmades_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'dolmades' );
+		$tzatziki_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'tzatziki' );
 
 		$this->assertEquals(
 			array(
 				'dolmades' => $dolmades_store,
 				'tzatziki' => $tzatziki_store,
 			),
-			WP_Style_Engine_CSS_Rules_Store::get_stores(),
+			WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_stores(),
 			'Return value of get_stores() does not match expectation'
 		);
-		WP_Style_Engine_CSS_Rules_Store::remove_all_stores();
+		WP_Style_Engine_CSS_Rules_Store_Gutenberg::remove_all_stores();
 
 		$this->assertEquals(
 			array(),
-			WP_Style_Engine_CSS_Rules_Store::get_stores(),
+			WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_stores(),
 			'Return value of get_stores() is not an empty array after remove_all_stores() called.'
 		);
 	}
@@ -134,7 +118,7 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::add_rule
 	 */
 	public function test_should_add_rule_to_existing_store() {
-		$new_pie_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'meat-pie' );
+		$new_pie_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'meat-pie' );
 		$selector      = '.wp-block-sauce a:hover';
 		$store_rule    = $new_pie_store->add_rule( $selector );
 		$expected      = '';
@@ -146,7 +130,7 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 			'border-color'  => 'yellow',
 			'border-radius' => '10rem',
 		);
-		$css_declarations = new WP_Style_Engine_CSS_Declarations( $pie_declarations );
+		$css_declarations = new WP_Style_Engine_CSS_Declarations_Gutenberg( $pie_declarations );
 		$store_rule->add_declarations( $css_declarations );
 
 		$store_rule = $new_pie_store->add_rule( $selector );
@@ -161,7 +145,7 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 	 * @covers ::get_all_rules
 	 */
 	public function test_should_get_all_rule_objects_for_a_store() {
-		$new_pizza_store = WP_Style_Engine_CSS_Rules_Store::get_store( 'pizza-with-mozzarella' );
+		$new_pizza_store = WP_Style_Engine_CSS_Rules_Store_Gutenberg::get_store( 'pizza-with-mozzarella' );
 		$selector        = '.wp-block-anchovies a:hover';
 		$store_rule      = $new_pizza_store->add_rule( $selector );
 		$expected        = array(
@@ -175,7 +159,7 @@ class WP_Style_Engine_CSS_Rules_Store_Test extends WP_UnitTestCase {
 			'padding' => '100px',
 		);
 		$new_store_rule           = $new_pizza_store->add_rule( $new_selector );
-		$css_declarations         = new WP_Style_Engine_CSS_Declarations( $newer_pizza_declarations );
+		$css_declarations         = new WP_Style_Engine_CSS_Declarations_Gutenberg( $newer_pizza_declarations );
 		$new_store_rule->add_declarations( array( $css_declarations ) );
 
 		$expected = array(
