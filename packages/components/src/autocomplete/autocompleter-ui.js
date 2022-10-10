6ac98ccd84164ad0534eec8d5db8407f8ee30dc5
BE-Webdesign <cromleyedwin89@gmail.com>
2022-10-10 17:08:09 -0400

    AutocompleterUI: Rerun prettier as it failed to run on save.

--- packages/components/src/autocomplete/autocompleter-ui.js
+++ packages/components/src/autocomplete/autocompleter-ui.js
@@ -100,22 +100,19 @@ export function getAutoCompleterUI( autocompleter ) {
 }
 
 function useOnClickOutside( ref, handler ) {
-	useEffect(
-		() => {
-			const listener = ( event ) => {
-				// Do nothing if clicking ref's element or descendent elements
-				if ( ! ref.current || ref.current.contains( event.target ) ) {
-					return;
-				}
-				handler( event );
-			};
-			document.addEventListener( 'mousedown', listener );
-			document.addEventListener( 'touchstart', listener );
-			return () => {
-				document.removeEventListener( 'mousedown', listener );
-				document.removeEventListener( 'touchstart', listener );
-			};
-		},
-		[ ref, handler ]
-	);
+	useEffect( () => {
+		const listener = ( event ) => {
+			// Do nothing if clicking ref's element or descendent elements
+			if ( ! ref.current || ref.current.contains( event.target ) ) {
+				return;
+			}
+			handler( event );
+		};
+		document.addEventListener( 'mousedown', listener );
+		document.addEventListener( 'touchstart', listener );
+		return () => {
+			document.removeEventListener( 'mousedown', listener );
+			document.removeEventListener( 'touchstart', listener );
+		};
+	}, [ ref, handler ] );
 }
