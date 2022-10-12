ba1668cc327e0f19f87510bffe200f6303122145
Marco Ciampini <marco.ciampo@gmail.com>
2022-10-12 17:35:35 +0200

    Add JSDocs for the default-exported component

--- packages/components/src/theme/index.tsx
+++ packages/components/src/theme/index.tsx
@@ -14,6 +14,28 @@ import { Wrapper } from './styles';
 
 extend( [ namesPlugin, a11yPlugin ] );
 
+/**
+ * `Theme` allows defining theme variables for components in the `@wordpress/components` package.
+ *
+ * Multiple `Theme` components can be nested in order to override specific theme variables.
+ *
+ *
+ * @example
+ * ```jsx
+ * import { __experimentalTheme as Theme } from '@wordpress/components';
+ *
+ * const Example = () => {
+ *   return (
+ *     <Theme accent="red">
+ *       <Button variant="primary">I'm red</Button>
+ *       <Theme accent="blue">
+ *         <Button variant="primary">I'm blue</Button>
+ *       </Theme>
+ *     </Theme>
+ *   );
+ * };
+ * ```
+ */
 function Theme( props: WordPressComponentProps< ThemeProps, 'div', true > ) {
 	const { accent } = props;
 	if ( accent && ! colord( accent ).isValid() ) {
