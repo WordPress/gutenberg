7b9b468d7b12ce21b44d95697ce503c63c0de00d
petitphp <c.boirie@gmail.com>
2022-10-13 19:06:18 +0200

    Remove aspect ratio classes when transforming an embed into other block type

--- packages/block-library/src/embed/transforms.js
+++ packages/block-library/src/embed/transforms.js
@@ -1,12 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { createBlock } from '@wordpress/blocks';
+import { createBlock, hasBlockSupport } from '@wordpress/blocks';
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
 import metadata from './block.json';
+import { removeAspectRatioClasses } from './util';
 
 const { name: EMBED_BLOCK } = metadata;
 
@@ -47,3 +49,64 @@ const transforms = {
 };
 
 export default transforms;
+
+/**
+ * Remove aspect ratio classes when transforming an embed to other block type.
+ *
+ * @param {Object}   result  The transformed block.
+ * @param {Object[]} source  Original blocks transformed.
+ * @param {number}   index   Index of the transformed block on the array of results.
+ * @param {Object[]} results An array all the blocks that resulted from the transformation.
+ * @return {Object} Filtered block transform.
+ */
+function removeAspectRatioClassesDuringTransform(
+	result,
+	source,
+	index,
+	results
+) {
+	if ( ! hasBlockSupport( result.name, 'customClassName', true ) ) {
+		return result;
+	}
+
+	// If the condition verifies we are probably in the presence of a wrapping transform
+	// e.g: nesting paragraphs in a group or columns and in that case the class should not be kept.
+	if ( results.length === 1 && result.innerBlocks.length === source.length ) {
+		return result;
+	}
+
+	// If we are transforming one block to multiple blocks or multiple blocks to one block,
+	// we ignore the class during the transform.
+	if (
+		( results.length === 1 && source.length > 1 ) ||
+		( results.length > 1 && source.length === 1 )
+	) {
+		return result;
+	}
+
+	// If we are in presence of transform between one or more block in the source
+	// that have one or more blocks in the result
+	// we apply the class on source N to the result N,
+	// if source N does not exists we do nothing.
+	if ( source[ index ] && 'core/embed' === source[ index ].name ) {
+		const originClassName = source[ index ]?.attributes.className;
+		if ( originClassName ) {
+			return {
+				...result,
+				attributes: {
+					...result.attributes,
+					className: removeAspectRatioClasses( originClassName ),
+				},
+			};
+		}
+	}
+	return result;
+}
+
+// run after `core/color/addTransforms` filter from custom-class-name support.
+addFilter(
+	'blocks.switchToBlockType.transformedBlock',
+	'core/embed/removeAspectRatioClassesDuringTransform',
+	removeAspectRatioClassesDuringTransform,
+	15
+);
