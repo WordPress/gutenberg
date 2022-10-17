ee8b27fb759ac16f5645f9e28cbd754df60b1d9d
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-10-17 17:18:19 +1000

    Update TabPanel to allow icons on TabButtons

--- packages/components/src/tab-panel/index.tsx
+++ packages/components/src/tab-panel/index.tsx
@@ -124,8 +124,11 @@ export function TabPanel( {
 						selected={ tab.name === selected }
 						key={ tab.name }
 						onClick={ () => handleTabSelection( tab.name ) }
+						label={ tab.icon && tab.title }
+						icon={ tab.icon }
+						showTooltip={ !! tab.icon }
 					>
-						{ tab.title }
+						{ ! tab.icon && tab.title }
 					</TabButton>
 				) ) }
 			</NavigableMenu>
