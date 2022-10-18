b394d918d6855919ee4425952fd497fd253b6de6
ntsekouras <ntsekouras@outlook.com>
2022-10-18 18:16:01 +0300

    update tests

--- packages/e2e-tests/specs/editor/blocks/gallery.test.js
+++ packages/e2e-tests/specs/editor/blocks/gallery.test.js
@@ -111,7 +111,7 @@ describe( 'Gallery', () => {
 
 		const imageListLink = ( await getListViewBlocks( 'Image' ) )[ 0 ];
 		await imageListLink.click();
-		await clickBlockToolbarButton( 'Add caption' );
+		await clickBlockToolbarButton( 'Caption' );
 		const captionElement = await figureElement.$(
 			'.block-editor-rich-text__editable'
 		);
