3ae2f4e6b6f74111dd2d8f3862ebb6b953c9d69d
Derek Blank <derekpblank@gmail.com>
2022-10-18 14:56:28 +1000

    [Mobile] - React Native 0.69.4 Upgrade - iOS

---
+++ packages/react-native-editor/react-native.config.js
@@ -0,0 +1,12 @@
+const path = require( 'path' );
+
+module.exports = {
+	dependencies: {
+		'@wordpress/react-native-bridge': {
+			root: path.resolve( __dirname, '../react-native-bridge' ),
+		},
+		'@wordpress/react-native-aztec': {
+			root: path.resolve( __dirname, '../react-native-aztec' ),
+		},
+	},
+};
