77114d82bfa2f6b16e946fbeabcb40a0a60a3083
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-10-18 12:49:59 +0200

    ESLint: Fix testing-library/prefer-find-by violations (#45062)

--- packages/data/src/components/use-select/test/suspense.js
+++ packages/data/src/components/use-select/test/suspense.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 /**
  * WordPress dependencies
@@ -110,7 +110,7 @@ describe( 'useSuspenseSelect', () => {
 		);
 
 		const rendered = render( <App /> );
-		await waitFor( () => rendered.getByLabelText( 'loaded' ) );
+		await rendered.findByLabelText( 'loaded' );
 
 		// Verify there were 3 attempts to render. Suspended twice because of
 		// `getToken` and `getData` selectors not being resolved, and then finally
@@ -157,7 +157,7 @@ describe( 'useSuspenseSelect', () => {
 		);
 
 		const rendered = render( <App /> );
-		const label = await waitFor( () => rendered.getByLabelText( 'error' ) );
+		const label = await rendered.findByLabelText( 'error' );
 		expect( label ).toHaveTextContent( 'resolution failed' );
 		expect( console ).toHaveErrored();
 	} );
