204df73719d79c3e70c41d89798d765c29b6388d
André <583546+oandregal@users.noreply.github.com>
2022-10-20 17:13:27 +0200

    Add note to backport

--- lib/compat/wordpress-6.2/default-filters.php
+++ lib/compat/wordpress-6.2/default-filters.php
@@ -1,4 +1,10 @@
 <?php
 
+/**
+ * Note for backport: we should also remove the existing filters:
+ *
+ * > add_action( 'switch_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+ * > add_action( 'start_previewing_theme', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+ */
 add_action( 'switch_theme', 'wp_theme_clean_theme_json_cached_data' );
 add_action( 'start_previewing_theme', 'wp_theme_clean_theme_json_cached_data' );
