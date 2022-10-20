231464db74662758049bdf5fdde22aa1c46bc91e
MaggieCabrera <maggie.cabrera@automattic.com>
2022-10-20 12:50:04 +0200

    check the page list block is in the list view

--- test/e2e/specs/editor/blocks/navigation.spec.js
+++ test/e2e/specs/editor/blocks/navigation.spec.js
@@ -14,12 +14,26 @@ test.describe(
 			editor,
 		} ) => {
 			await editor.insertBlock( { name: 'core/navigation' } );
-			// TODO:
-			// Check the list view.
 
+			// Check Page List is in the list view.
+
+			// Open the list view.
+			await editor.page.locator( '[aria-label="List View"i]' ).click();
+			// Click the Navigation block expander, we need to use force because it has aria-hidden set to true.
+			await editor.page
+				.locator(
+					`//a[.//span[text()='Navigation']]/span[contains(@class, 'block-editor-list-view__expander')]`
+				)
+				.click( { force: true } );
+			// Find the Page list selector inside the navigation block.
+			const pageListSelector = await editor.page.locator(
+				`//table[contains(@aria-label,'Block navigation structure')]//a[.//span[text()='Page List']]`
+			);
+
+			expect( pageListSelector ).toBeTruthy();
+
+			// Check the markup of the block is correct.
 			await editor.publishPost();
-
-			// Check the content.
 			const content = await editor.getEditedPostContent();
 			expect( content ).toBe(
 				`<!-- wp:navigation -->
