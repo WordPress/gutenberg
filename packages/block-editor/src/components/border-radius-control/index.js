91b2a2ef36b76ab7be1bae772c73420321116019
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-10-24 20:24:01 +1000

    UnitControl: Normalize wrapper classnames by removing outer wrapper (#45139)

--- packages/block-editor/src/components/border-radius-control/index.js
+++ packages/block-editor/src/components/border-radius-control/index.js
@@ -94,7 +94,7 @@ export default function BorderRadiusControl( { onChange, values } ) {
 				{ isLinked ? (
 					<>
 						<AllInputControl
-							__unstableWrapperClassName="components-border-radius-control__unit-control"
+							className="components-border-radius-control__unit-control"
 							values={ values }
 							min={ MIN_BORDER_RADIUS_VALUE }
 							onChange={ onChange }
