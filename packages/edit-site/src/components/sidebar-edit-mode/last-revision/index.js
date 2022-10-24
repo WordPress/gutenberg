e91724ac9819659ce3369138d8344d754bbb842b
George Hotelling <george.hotelling@automattic.com>
2022-10-24 14:49:00 -0400

    Refactor

---
+++ packages/edit-site/src/components/sidebar-edit-mode/last-revision/index.js
@@ -0,0 +1,73 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { PanelBody, Button } from '@wordpress/components';
+import { sprintf, _n } from '@wordpress/i18n';
+import { backup } from '@wordpress/icons';
+import { addQueryArgs } from '@wordpress/url';
+import { PostTypeSupportCheck } from '@wordpress/editor';
+
+/**
+ * Internal dependencies
+ */
+import { store as editSiteStore } from '../../../store';
+
+const useRevisionData = () =>
+	useSelect( ( select ) => {
+		const {
+			getCurrentTemplateLastRevisionId,
+			getCurrentTemplateRevisionsCount,
+		} = select( editSiteStore );
+		return {
+			lastRevisionId: getCurrentTemplateLastRevisionId(),
+			revisionsCount: getCurrentTemplateRevisionsCount(),
+		};
+	} );
+
+function PostLastRevisionCheck( { children } ) {
+	const { lastRevisionId, revisionsCount } = useRevisionData();
+
+	if ( ! lastRevisionId || revisionsCount < 2 ) {
+		return null;
+	}
+
+	return (
+		<PostTypeSupportCheck supportKeys="revisions">
+			{ children }
+		</PostTypeSupportCheck>
+	);
+}
+
+const PostLastRevision = () => {
+	const { lastRevisionId, revisionsCount } = useRevisionData();
+
+	return (
+		<PostLastRevisionCheck>
+			<Button
+				href={ addQueryArgs( 'revision.php', {
+					revision: lastRevisionId,
+					gutenberg: true,
+				} ) }
+				className="editor-post-last-revision__title"
+				icon={ backup }
+			>
+				{ sprintf(
+					/* translators: %d: number of revisions */
+					_n( '%d Revision', '%d Revisions', revisionsCount ),
+					revisionsCount
+				) }
+			</Button>
+		</PostLastRevisionCheck>
+	);
+};
+
+export default function LastRevision() {
+	return (
+		<PostLastRevisionCheck>
+			<PanelBody className="edit-post-last-revision__panel">
+				<PostLastRevision />
+			</PanelBody>
+		</PostLastRevisionCheck>
+	);
+}
