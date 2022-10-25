6502514921a60e6e30cd5ec3e7cfb14d94b1ff0a
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-10-25 21:54:06 +0900

    Test: add iframe to Modal component

--- packages/components/src/modal/stories/index.story.tsx
+++ packages/components/src/modal/stories/index.story.tsx
@@ -76,6 +76,13 @@ const Template: StoryFn< typeof Modal > = ( { onRequestClose, ...args } ) => {
 
 					<InputControl style={ { marginBottom: '20px' } } />
 
+					<iframe
+						title="Example"
+						width="300"
+						height="200"
+						src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
+					/>
+
 					<Button variant="secondary" onClick={ closeModal }>
 						Close Modal
 					</Button>
