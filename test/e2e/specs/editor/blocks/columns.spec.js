a77f8847deea73e7a8b03a9d08d55a5f5e3e9a3c
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-10-28 02:02:33 +0600

    add inline approach

--- test/e2e/specs/editor/blocks/columns.spec.js
+++ test/e2e/specs/editor/blocks/columns.spec.js
@@ -1,71 +1,54 @@
 /**
  * WordPress dependencies
  */
-const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+const { test, expect } = require('@wordpress/e2e-test-utils-playwright');
 
-test.use( {
-	getBlockFunction: async ( { page }, use ) => {
-		await use( new GetAllBlockInserterItem( { page } ) );
-	},
-} );
-
-test.describe( 'Columns Test', () => {
-	test.beforeEach( async ( { admin } ) => {
+test.describe('Columns', () => {
+	test.beforeEach(async ({ admin }) => {
 		await admin.createNewPost();
-	} );
+	});
 
-	test.afterEach( async ( { requestUtils } ) => {
+	test.afterEach(async ({ requestUtils }) => {
 		await requestUtils.deleteAllPosts();
-	} );
+	});
 
-	test( 'Create Columns and verify', async ( {
+	test('restricts all blocks inside the columns block', async ({
 		page,
 		editor,
-		getBlockFunction,
-	} ) => {
+	}) => {
 		// Open Columns
-		await editor.insertBlock( { name: 'core/columns' } );
-		await page.locator( '[aria-label="Two columns; equal split"]' ).click();
+		await editor.insertBlock({ name: 'core/columns' });
+		await page.locator('[aria-label="Two columns; equal split"]').click();
 
 		// Open List view toggle
-		await page.locator( 'role=button[name="List View"i]' ).click();
+		await page.locator('role=button[name="List View"i]').click();
 
-		await getBlockFunction.get_list_view_block( 'Column' );
+		// block column add
+		await page
+			.locator(
+				'[aria-label="Block navigation structure"] >> text=Column'
+			)
+			.nth(1)
+			.click();
 
 		// Toggle Block inserter
 		await page
-			.locator( 'role=button[name="Toggle block inserter"i]' )
+			.locator('role=button[name="Toggle block inserter"i]')
 			.click();
 
 		// Verify Column
-		expect( await getBlockFunction.get_list_all_block() ).toHaveLength( 1 );
-	} );
-} );
-
-class GetAllBlockInserterItem {
-	constructor( { page } ) {
-		this.page = page;
-	}
-
-	async get_list_view_block( blockLabel ) {
-		return this.page
-			.locator(
-				`//table[contains(@aria-label,'Block navigation structure')]//a[.//span[text()='${ blockLabel }']]`
-			)
-			.first()
-			.click();
-	}
-	async get_list_all_block() {
-		await this.page.waitForTimeout( 500 );
-		const inserterItemTitles = await this.page.evaluate( () => {
+		await page.waitForTimeout(500);
+		const inserterItemTitles = await page.evaluate(() => {
 			return Array.from(
 				document.querySelectorAll(
 					'.block-editor-block-types-list__item-title'
 				)
-			).map( ( inserterItem ) => {
+			).map((inserterItem) => {
 				return inserterItem.innerText;
-			} );
-		} );
-		return [ ...new Set( inserterItemTitles ) ].sort();
-	}
-}
+			});
+		});
+		expect(inserterItemTitles).toHaveLength(1);
+
+		//expect(await getBlockFunction.get_list_all_block()).toHaveLength(1);
+	});
+});
