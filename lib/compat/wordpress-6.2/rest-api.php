28dc97c12a40d5c50186db364ff9dc35cbfb7e63
Kelly Dwan <ryelle@users.noreply.github.com>
2022-10-28 12:16:46 -0400

    Remove the 6.0 filter

--- lib/compat/wordpress-6.2/rest-api.php
+++ lib/compat/wordpress-6.2/rest-api.php
@@ -21,6 +21,4 @@ function gutenberg_register_rest_pattern_directory_6_2() {
 	$pattern_directory_controller = new Gutenberg_REST_Pattern_Directory_Controller_6_2();
 	$pattern_directory_controller->register_routes();
 }
-// Unhook the 6.0 registration to use the 6.2 controller instead.
-remove_action( 'rest_api_init', 'gutenberg_register_rest_pattern_directory' );
 add_action( 'rest_api_init', 'gutenberg_register_rest_pattern_directory_6_2' );
