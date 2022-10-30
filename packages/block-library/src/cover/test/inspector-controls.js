214c3153f3cef3dac6fecd488793c8fc22c5f97b
Glen Davies <glen.davies@automattic.com>
2022-10-31 09:48:46 +1300

    Finish off the inspector control tests

--- packages/block-library/src/cover/test/inspector-controls.js
+++ packages/block-library/src/cover/test/inspector-controls.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 // Need to mock the InspectorControls wrapper as this requires a slot to run
@@ -15,8 +15,12 @@ jest.mock(
 jest.mock(
 	'@wordpress/components/src/tools-panel/tools-panel-item',
 	() =>
-		( { children } ) =>
-			<div className="tools-panel-item">{ children }</div>
+		( { children, props } ) =>
+			(
+				<div className="tools-panel-item" { ...props }>
+					{ children }
+				</div>
+			)
 );
 
 /**
@@ -143,7 +147,7 @@ describe( 'Cover block controls', () => {
 				alt: 'Me',
 			} );
 		} );
-		test( 'sets focalPoint attribute when focal point values changed', async () => {
+		test( 'sets left focalPoint attribute when focal point values changed', async () => {
 			const { user } = setup(
 				<CoverInspectorControls
 					{ ...defaultProps }
@@ -153,9 +157,27 @@ describe( 'Cover block controls', () => {
 					} }
 				/>
 			);
-			await user.type( screen.getByLabelText( 'Left' ), '1' );
+			await user.clear( screen.getByLabelText( 'Left' ) );
+			await user.type( screen.getByLabelText( 'Left' ), '100' );
 			expect( setAttributes ).toHaveBeenCalledWith( {
 				focalPoint: { x: 1, y: 0.5 },
+			} );
+		} );
+		test( 'sets top focalPoint attribute when focal point values changed', async () => {
+			const { user } = setup(
+				<CoverInspectorControls
+					{ ...defaultProps }
+					currentSettings={ {
+						...currentSettings,
+						url: 'http://localhost/myimage.jpg',
+					} }
+				/>
+			);
+
+			await user.clear( screen.getByLabelText( 'Top' ) );
+			await user.type( screen.getByLabelText( 'Top' ), '30' );
+			expect( setAttributes ).toHaveBeenCalledWith( {
+				focalPoint: { x: 0.5, y: 0.3 },
 			} );
 		} );
 		test( 'clears media attributes when clear media button clicked', async () => {
@@ -182,6 +204,64 @@ describe( 'Cover block controls', () => {
 				url: undefined,
 				useFeaturedImage: false,
 			} );
+		} );
+	} );
+} );
+
+describe( 'Color panel', () => {
+	test( 'displays overlay color control', () => {
+		render( <CoverInspectorControls { ...defaultProps } /> );
+		expect( screen.getByText( 'Overlay' ) ).toBeInTheDocument();
+		// It doesn't look like it is possible to test the color selector as
+		// it seems to render outside this component dom tree.
+	} );
+
+	test( 'sets dimRatio attribute when number control value changed', async () => {
+		const { user } = setup(
+			<CoverInspectorControls { ...defaultProps } />
+		);
+		await user.clear(
+			screen.getByRole( 'spinbutton', {
+				name: 'Overlay opacity',
+			} )
+		);
+		await user.type(
+			screen.getByRole( 'spinbutton', {
+				name: 'Overlay opacity',
+			} ),
+			'30'
+		);
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			dimRatio: 30,
+		} );
+	} );
+	test( 'sets dimRatio attribute when range control value changed', () => {
+		setup( <CoverInspectorControls { ...defaultProps } /> );
+
+		fireEvent.change(
+			screen.getByRole( 'slider', {
+				name: 'Overlay opacity',
+			} ),
+			{ target: { value: 25 } }
+		);
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			dimRatio: 25,
+		} );
+	} );
+} );
+
+describe( 'Dimensions panel', () => {
+	test( 'sets minHeight attribute when number control value changed', async () => {
+		const { user } = setup(
+			<CoverInspectorControls { ...defaultProps } />
+		);
+		await user.clear( screen.getByLabelText( 'Minimum height of cover' ) );
+		await user.type(
+			screen.getByLabelText( 'Minimum height of cover' ),
+			'300'
+		);
+		expect( setAttributes ).toHaveBeenCalledWith( {
+			minHeight: 300,
 		} );
 	} );
 } );
