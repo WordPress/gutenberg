52a828658df5142f673a5f5e576fa0503fc12076
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-10-31 16:15:34 +0600

    Addressed feedback

--- test/e2e/specs/editor/blocks/columns.spec.js
+++ test/e2e/specs/editor/blocks/columns.spec.js
@@ -1,54 +1,46 @@
 /**
  * WordPress dependencies
  */
-const { test, expect } = require('@wordpress/e2e-test-utils-playwright');
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test.describe('Columns', () => {
-	test.beforeEach(async ({ admin }) => {
+test.describe( 'Columns', () => {
+	test.beforeEach( async ( { admin } ) => {
 		await admin.createNewPost();
-	});
+	} );
 
-	test.afterEach(async ({ requestUtils }) => {
+	test.afterEach( async ( { requestUtils } ) => {
 		await requestUtils.deleteAllPosts();
-	});
+	} );
 
-	test('restricts all blocks inside the columns block', async ({
+	test( 'restricts all blocks inside the columns block', async ( {
 		page,
 		editor,
-	}) => {
+	} ) => {
 		// Open Columns
-		await editor.insertBlock({ name: 'core/columns' });
-		await page.locator('[aria-label="Two columns; equal split"]').click();
+		await editor.insertBlock( { name: 'core/columns' } );
+		await page.locator( '[aria-label="Two columns; equal split"]' ).click();
 
 		// Open List view toggle
-		await page.locator('role=button[name="List View"i]').click();
+		await page.locator( 'role=button[name="List View"i]' ).click();
 
 		// block column add
 		await page
 			.locator(
-				'[aria-label="Block navigation structure"] >> text=Column'
+				'role=treegrid[name="Block navigation structure"i] >> role=gridcell[name="Column link"i]'
 			)
-			.nth(1)
+			.first()
 			.click();
 
 		// Toggle Block inserter
 		await page
-			.locator('role=button[name="Toggle block inserter"i]')
+			.locator( 'role=button[name="Toggle block inserter"i]' )
 			.click();
 
 		// Verify Column
-		await page.waitForTimeout(500);
-		const inserterItemTitles = await page.evaluate(() => {
-			return Array.from(
-				document.querySelectorAll(
-					'.block-editor-block-types-list__item-title'
-				)
-			).map((inserterItem) => {
-				return inserterItem.innerText;
-			});
-		});
-		expect(inserterItemTitles).toHaveLength(1);
-
-		//expect(await getBlockFunction.get_list_all_block()).toHaveLength(1);
-	});
-});
+		const inserterOptions = page.locator(
+			'role=region[name="Block Library"i] >> role=option'
+		);
+		await expect( inserterOptions ).toHaveCount( 1 );
+		await expect( inserterOptions ).toHaveText( 'Column' );
+	} );
+} );
