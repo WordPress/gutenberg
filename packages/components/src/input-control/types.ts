f5b13ed38be064b2e96230c59342aebb97b0e0c8
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2022-11-01 16:15:21 +1000

    Revert "Try adding style prop via WordPressComponentProps"
    This reverts commit 3c8d736b6e1b9444fae07581beb6846bdfd1bc5c.

--- packages/components/src/input-control/types.ts
+++ packages/components/src/input-control/types.ts
@@ -121,12 +121,7 @@ export interface InputFieldProps extends BaseProps {
 	type?: HTMLInputTypeAttribute;
 }
 
-export interface InputBaseProps
-	extends Pick<
-			WordPressComponentProps< BaseProps, 'div' >,
-			'className' | 'id' | 'style' | keyof BaseProps
-		>,
-		FlexProps {
+export interface InputBaseProps extends BaseProps, FlexProps {
 	children: ReactNode;
 	/**
 	 * Renders an element on the left side of the input.
@@ -171,9 +166,18 @@ export interface InputBaseProps
 	 */
 	disabled?: boolean;
 	/**
+	 * The class name to be added to the wrapper element.
+	 */
+	className?: string;
+	id?: string;
+	/**
 	 * If this property is added, a label will be generated using label property as the content.
 	 */
 	label?: ReactNode;
+	/**
+	 * If this property is added, it will be added to the modal frame `div`.
+	 */
+	style?: CSSProperties;
 }
 
 export interface InputControlProps
