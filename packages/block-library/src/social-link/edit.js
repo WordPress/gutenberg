52d7c1e678dc74cfd08174e05f3f834e27bfb42c
George Hotelling <george.hotelling@automattic.com>
2022-11-01 15:24:14 -0400

    Add rel attribute for Social Icons

--- packages/block-library/src/social-link/edit.js
+++ packages/block-library/src/social-link/edit.js
@@ -12,7 +12,7 @@ import {
 	URLInput,
 	useBlockProps,
 } from '@wordpress/block-editor';
-import { Fragment, useState } from '@wordpress/element';
+import { Fragment, useState, useCallback } from '@wordpress/element';
 import {
 	Button,
 	PanelBody,
@@ -66,7 +66,7 @@ const SocialLinkEdit = ( {
 	isSelected,
 	setAttributes,
 } ) => {
-	const { url, service, label } = attributes;
+	const { url, service, label, rel } = attributes;
 	const { showLabels, iconColorValue, iconBackgroundColorValue } = context;
 	const [ showURLPopover, setPopover ] = useState( false );
 	const classes = classNames( 'wp-social-link', 'wp-social-link-' + service, {
@@ -87,6 +87,13 @@ const SocialLinkEdit = ( {
 			backgroundColor: iconBackgroundColorValue,
 		},
 	} );
+
+	const onSetLinkRel = useCallback(
+		( value ) => {
+			setAttributes( { rel: value } );
+		},
+		[ setAttributes ]
+	);
 
 	return (
 		<Fragment>
@@ -112,6 +119,13 @@ const SocialLinkEdit = ( {
 						/>
 					</PanelRow>
 				</PanelBody>
+			</InspectorControls>
+			<InspectorControls __experimentalGroup="advanced">
+				<TextControl
+					label={ __( 'Link rel' ) }
+					value={ rel || '' }
+					onChange={ onSetLinkRel }
+				/>
 			</InspectorControls>
 			<li { ...blockProps }>
 				<Button
