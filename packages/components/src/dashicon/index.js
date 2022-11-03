c4928cb3956b82358395315ecd314a23b40d3df1
Matias Benedetto <matias.benedetto@gmail.com>
2022-11-03 14:54:19 -0300

    Making size work for icon components using dash icon strings

--- packages/components/src/dashicon/index.js
+++ packages/components/src/dashicon/index.js
@@ -3,6 +3,7 @@
  *
  * @property {import('./types').IconKey} icon        Icon name
  * @property {string}                    [className] Class name
+ * @property {number}                    [size] Size of the icon
  */
 /** @typedef {import('react').ComponentPropsWithoutRef<'span'> & OwnProps} Props */
 
@@ -10,7 +11,8 @@
  * @param {Props} props
  * @return {JSX.Element} Element
  */
-function Dashicon( { icon, className, ...extraProps } ) {
+
+function Dashicon( { icon, className, size = 20, ...extraProps } ) {
 	const iconClass = [
 		'dashicon',
 		'dashicons',
@@ -20,7 +22,17 @@ function Dashicon( { icon, className, ...extraProps } ) {
 		.filter( Boolean )
 		.join( ' ' );
 
-	return <span className={ iconClass } { ...extraProps } />;
+	// For retro-compatibility reasons (for example if people are overriding icon size with CSS), we add inline styles just if the size is different to the default
+	const style =
+		20 !== size
+			? {
+					fontSize: `${ size }px`,
+					width: `${ size }px`,
+					height: `${ size }px`,
+			  }
+			: {};
+
+	return <span className={ iconClass } style={ style } { ...extraProps } />;
 }
 
 export default Dashicon;
