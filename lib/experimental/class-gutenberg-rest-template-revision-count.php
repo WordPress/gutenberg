083114f9fa96f3d414cfac62fa4109d973d9e4ce
George Hotelling <george.hotelling@automattic.com>
2022-11-04 16:15:42 -0400

    Fix warnings

--- lib/experimental/class-gutenberg-rest-template-revision-count.php
+++ lib/experimental/class-gutenberg-rest-template-revision-count.php
@@ -2,8 +2,8 @@
 /**
  * WP_REST_Templates_Controller class
  *
- * When merging into core, prepare_item_for_response() and get_item_schema()
- * should be merged with the parent methods
+ * When merging into core, prepare_revision_links() should be merged with
+ * WP_REST_Templates_Controller::prepare_links().
  *
  * @package gutenberg
  */
@@ -24,7 +24,7 @@ class Gutenberg_REST_Template_Revision_Count extends WP_REST_Templates_Controlle
 		$response = parent::prepare_item_for_response( $item, $request );
 
 		if ( rest_is_field_included( '_links', $fields ) || rest_is_field_included( '_embedded', $fields ) ) {
-			$links = $this->prepare_links( $template );
+			$links = $this->prepare_revision_links( $template );
 			$response->add_links( $links );
 			if ( ! empty( $links['self']['href'] ) ) {
 				$actions = $this->get_available_actions();
@@ -39,37 +39,29 @@ class Gutenberg_REST_Template_Revision_Count extends WP_REST_Templates_Controlle
 	}
 
 	/**
-	 * Prepares links for the request.
+	 * Adds revisions to links.
 	 *
 	 * @since 6.2.0
 	 *
-	 * @param WP_Block_Template $template    Template instance.
+	 * @param WP_Block_Template $template  Template instance.
 	 * @return array Links for the given post.
 	 */
-	protected function prepare_links( $template ) {
-		$base  = sprintf( '%s/%s', $this->namespace, $this->rest_base );
-		$links = array(
-			'self'       => array(
-				'href' => rest_url( trailingslashit( $base ) . $template->id ),
-			),
-			'collection' => array(
-				'href' => rest_url( rest_get_route_for_post_type_items( $this->post_type ) ),
-			),
-			'about'      => array(
-				'href' => rest_url( 'wp/v2/types/' . $this->post_type ),
-			),
-		);
+	protected function prepare_revision_links( $template ) {
+		$links = array();
 
 		if ( post_type_supports( $this->post_type, 'revisions' ) && (int) $template->wp_id ) {
 			$revisions       = wp_get_latest_revision_id_and_total_count( (int) $template->wp_id );
 			$revisions_count = ! is_wp_error( $revisions ) ? $revisions['count'] : 0;
+			$revisions_base  = sprintf( '/%s/%s/%s/revisions', $this->namespace, $this->rest_base, $template->id );
 
 			$links['version-history'] = array(
+				'href'  => rest_url( $revisions_base ),
 				'count' => $revisions_count,
 			);
 
 			if ( $revisions_count > 0 ) {
 				$links['predecessor-version'] = array(
+					'href' => rest_url( $revisions_base . '/' . $revisions['latest_id'] ),
 					'id'   => $revisions['latest_id'],
 				);
 			}
