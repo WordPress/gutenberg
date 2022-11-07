de31dbc4f53d8cd59723870d824e66ce0f4388af
Riad Benguella <benguella@gmail.com>
2022-11-07 09:18:33 +0000

    Address PR feedback

--- bin/prepare-codesandbox.js
+++ bin/prepare-codesandbox.js
@@ -11,40 +11,44 @@ const currentConfig = JSON.parse(
 	fs.readFileSync( path.join( __dirname, '../.wp-env.json' ), 'utf-8' )
 );
 
+const overrideConfig = {
+	env: {
+		development: {
+			config: {
+				WP_SITEURL: `https://${ process.env.CODESANDBOX_HOST.replace(
+					'$PORT',
+					8888
+				) }`,
+				WP_HOME: `https://${ process.env.CODESANDBOX_HOST.replace(
+					'$PORT',
+					8888
+				) }`,
+			},
+			mappings: merge( currentConfig?.env?.development?.mappings ?? {}, {
+				'wp-content/mu-plugins/disable-canonical-redirect.php':
+					'.codesandbox/disable-canonical-redirect.php',
+			} ),
+		},
+		tests: {
+			config: {
+				WP_SITEURL: `https://${ process.env.CODESANDBOX_HOST.replace(
+					'$PORT',
+					8889
+				) }`,
+				WP_HOME: `https://${ process.env.CODESANDBOX_HOST.replace(
+					'$PORT',
+					8889
+				) }`,
+			},
+			mappings: merge( currentConfig?.env?.tests?.mappings ?? {}, {
+				'wp-content/mu-plugins/disable-canonical-redirect.php':
+					'.codesandbox/disable-canonical-redirect.php',
+			} ),
+		},
+	},
+};
+
 fs.writeFileSync(
 	path.join( __dirname, '../.wp-env.override.json' ),
-	`{
-		"env": {
-			"development": {
-				"config": {
-					"WP_SITEURL": "https://${ process.env.CODESANDBOX_HOST.replace(
-						'$PORT',
-						8888
-					) }",
-					"WP_HOME": "https://${ process.env.CODESANDBOX_HOST.replace( '$PORT', 8888 ) }"
-				},
-				"mappings": ${ JSON.stringify(
-					merge( currentConfig?.env?.development?.mappings ?? {}, {
-						'wp-content/mu-plugins/disable-canonical-redirect.php':
-							'.codesandbox/disable-canonical-redirect.php',
-					} )
-				) } 
-			},
-			"tests": {
-				"config": {
-					"WP_SITEURL": "https://${ process.env.CODESANDBOX_HOST.replace(
-						'$PORT',
-						8889
-					) }",
-					"WP_HOME": "https://${ process.env.CODESANDBOX_HOST.replace( '$PORT', 8889 ) }"
-				},
-				"mappings": ${ JSON.stringify(
-					merge( currentConfig?.env?.tests?.mappings ?? {}, {
-						'wp-content/mu-plugins/disable-canonical-redirect.php':
-							'.codesandbox/disable-canonical-redirect.php',
-					} )
-				) } 
-			}
-		}
-	}`
+	JSON.stringify( overrideConfig, null, 2 )
 );
