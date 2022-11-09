f58e23b096ca25c5138f252a2cf19624b8493723
André <583546+oandregal@users.noreply.github.com>
2022-11-09 14:43:46 -0700

    Store int instead of boolean in the cache

--- lib/compat/wordpress-6.2/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.2/get-global-styles-and-settings.php
@@ -17,23 +17,30 @@ if ( ! function_exists( 'wp_theme_has_theme_json' ) ) {
 	function wp_theme_has_theme_json() {
 		$cache_group       = 'theme_json';
 		$cache_key         = 'wp_theme_has_theme_json';
-		$cache_found       = false;
-		$theme_has_support = wp_cache_get( $cache_key, $cache_group, false, $cache_found );
-		if ( $cache_found ) {
-			return $theme_has_support;
+		$theme_has_support = wp_cache_get( $cache_key, $cache_group );
+
+		/**
+		 * $theme_has_support is stored as a int in the cache.
+		 *
+		 * The reason not to store it as a boolean is to avoid working
+		 * with the $found parameter which apparently had some issues in some implementations
+		 * https://developer.wordpress.org/reference/functions/wp_cache_get/
+		 */
+		if ( 0 === $theme_has_support || 1 === $theme_has_support ) {
+			return (bool) $theme_has_support;
 		}
 
 		// Has the own theme a theme.json?
-		$theme_has_support = is_readable( get_stylesheet_directory() . '/theme.json' );
+		$theme_has_support = is_readable( get_stylesheet_directory() . '/theme.json' ) ? 1 : 0;
 
 		// Look up the parent if the child does not have a theme.json.
-		if ( ! $theme_has_support ) {
-			$theme_has_support = is_readable( get_template_directory() . '/theme.json' );
+		if ( 0 === $theme_has_support ) {
+			$theme_has_support = is_readable( get_template_directory() . '/theme.json' ) ? 1 : 0;
 		}
 
 		wp_cache_set( $cache_key, $theme_has_support, $cache_group );
 
-		return $theme_has_support;
+		return (bool) $theme_has_support;
 	}
 }
 
