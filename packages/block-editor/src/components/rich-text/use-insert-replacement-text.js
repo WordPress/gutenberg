5557b304042faa4ade2d3556dcb30beaa49e1340
Ella van Durpe <ella@vandurpe.com>
2022-11-09 14:54:47 -0700

    Rich text: create undo level before autocorrect

---
+++ packages/block-editor/src/components/rich-text/use-insert-replacement-text.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { useRefEffect } from '@wordpress/compose';
+import { useDispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+/**
+ * Internal dependencies
+ */
+import { store as blockEditorStore } from '../../store';
+
+/**
+ * When the browser is about to auto correct, add an undo level so the user can
+ * revert the change.
+ */
+export function useInsertReplacementText() {
+	const { __unstableMarkLastChangeAsPersistent } =
+		useDispatch( blockEditorStore );
+	return useRefEffect( ( element ) => {
+		function onInput( event ) {
+			if ( event.inputType === 'insertReplacementText' ) {
+				__unstableMarkLastChangeAsPersistent();
+			}
+		}
+
+		element.addEventListener( 'beforeinput', onInput );
+		return () => {
+			element.removeEventListener( 'beforeinput', onInput );
+		};
+	}, [] );
+}
