7dbe7bca1c736315f4b626dc8f24d383299fdf83
Jeff Ong <jonger4@gmail.com>
2022-11-15 10:27:56 -0500

    Filter screen reader text.

--- packages/block-library/src/post-excerpt/index.php
+++ packages/block-library/src/post-excerpt/index.php
@@ -26,12 +26,14 @@ function render_block_core_post_excerpt( $attributes, $content, $block ) {
 
 	$post_ID            = $block->context['postId'];
 	$post_title         = get_the_title( $post_ID );
+	$allowed_html       = array( 'abbr' => array(), 'acronym' => array(), 'b' => array(), 'br' => array(), 'cite' => array(), 'code' => array(), 'em' => array(), 'i' => array(), 'span' => array(), 'strong' => array(), 'sub' => array(), 'sup' => array(), 'time' => array() );
+
 	$screen_reader_text = sprintf(
 		/* translators: %s is either the post title or post ID to describe the link for screen readers. */
 		__( ': %s' ),
 		'' !== $post_title ? $post_title : __( 'untitled post ' ) . $post_ID
 	);
-	$more_text           = ! empty( $attributes['moreText'] ) ? '<a class="wp-block-post-excerpt__more-link" href="' . esc_url( get_the_permalink( $post_ID ) ) . '">' . wp_kses_post( $attributes['moreText'] ) . '<span class="screen-reader-text">' . $screen_reader_text . '</span></a>' : '';
+	$more_text           = ! empty( $attributes['moreText'] ) ? '<a class="wp-block-post-excerpt__more-link" href="' . esc_url( get_the_permalink( $post_ID ) ) . '">' . wp_kses_post( $attributes['moreText'] ) . '<span class="screen-reader-text">' . wp_kses( $screen_reader_text, $allowed_html ) . '</span></a>' : '';
 	$filter_excerpt_more = function( $more ) use ( $more_text ) {
 		return empty( $more_text ) ? $more : '';
 	};
