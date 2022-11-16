4d65aa3b7c62c5143dfb804b2c14b6a948688b05
ntsekouras <ntsekouras@outlook.com>
2022-11-16 18:49:44 +0200

    abort previous requests when we make new ones

--- packages/block-editor/src/components/inserter/block-patterns-explorer2/use-patterns-from-directory.js
+++ packages/block-editor/src/components/inserter/block-patterns-explorer2/use-patterns-from-directory.js
@@ -9,6 +9,7 @@ const defaultPerPage = 10;
 const CACHE = new Map();
 
 export default function usePatternsFromDirectory( options = {} ) {
+	const abortController = useRef();
 	const [ patterns, setPatterns ] = useState( [] );
 	const isLoading = useRef();
 	useEffect( () => {
@@ -21,21 +22,36 @@ export default function usePatternsFromDirectory( options = {} ) {
 			setPatterns( CACHE.get( key ) );
 			return;
 		}
+		// If we have a pending request and make a new one, abort the former.
 		if ( isLoading.current ) {
-			// abort previous request
+			abortController.current?.abort();
 		}
+		// We need a new instance of AbortController for each request.
+		abortController.current =
+			typeof AbortController === 'undefined'
+				? undefined
+				: new AbortController();
 		isLoading.current = true;
-		setPatterns( [] );
+		setPatterns( [] ); // Empty the previous results.
 		apiFetch( {
 			path: addQueryArgs( '/wp/v2/pattern-directory/patterns', {
 				...options,
 				per_page: options.per_page || defaultPerPage,
 			} ),
-		} ).then( ( fetchedPatterns ) => {
-			CACHE.set( key, fetchedPatterns );
-			isLoading.current = false;
-			setPatterns( fetchedPatterns );
-		} );
+			signal: abortController.current?.signal,
+		} )
+			.then( ( fetchedPatterns ) => {
+				CACHE.set( key, fetchedPatterns );
+				isLoading.current = false;
+				setPatterns( fetchedPatterns );
+			} )
+			.catch( ( error ) => {
+				// Don't throw the error if we have aborted the request.
+				if ( error.name !== 'AbortError' ) {
+					throw error;
+				}
+			} );
+		return () => abortController.current?.abort?.();
 	}, [ options.search, options.category, options.per_page ] );
 	return [ patterns, isLoading.current ];
 }
