8c31370503da14fb871ff9111ef6950b2f36a6f2
Marco Ciampini <marco.ciampo@gmail.com>
2022-11-16 18:24:26 +0100

    Type refs and internal functions

--- packages/components/src/angle-picker-control/angle-circle.tsx
+++ packages/components/src/angle-picker-control/angle-circle.tsx
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import type { MouseEvent as ReactMouseEvent } from 'react';
+
+/**
  * WordPress dependencies
  */
 import { useEffect, useRef } from '@wordpress/element';
@@ -21,8 +26,15 @@ function AngleCircle( {
 	onChange,
 	...props
 }: WordPressComponentProps< AngleCircleProps, 'div' > ) {
+	const angleCircleRef = useRef< HTMLDivElement | null >( null );
+	const angleCircleCenter = useRef< { x: number; y: number } | undefined >();
+	const previousCursorValue = useRef< CSSStyleDeclaration[ 'cursor' ] >();
 
 	const setAngleCircleCenter = () => {
+		if ( angleCircleRef.current === null ) {
+			return;
+		}
+
 		const rect = angleCircleRef.current.getBoundingClientRect();
 		angleCircleCenter.current = {
 			x: rect.x + rect.width / 2,
@@ -30,14 +42,25 @@ function AngleCircle( {
 		};
 	};
 
-	const changeAngleToPosition = ( event ) => {
-		const { x: centerX, y: centerY } = angleCircleCenter.current;
+	const changeAngleToPosition = (
+		event: ReactMouseEvent | MouseEvent | undefined
+	) => {
+		if ( event === undefined ) {
+			return;
+		}
+
 		// Prevent (drag) mouse events from selecting and accidentally
 		// triggering actions from other elements.
 		event.preventDefault();
 		// Input control needs to lose focus and by preventDefault above, it doesn't.
-		event.target.focus();
-		onChange( getAngle( centerX, centerY, event.clientX, event.clientY ) );
+		( event.target as HTMLDivElement | null )?.focus();
+
+		if ( angleCircleCenter.current !== undefined ) {
+			const { x: centerX, y: centerY } = angleCircleCenter.current;
+			onChange(
+				getAngle( centerX, centerY, event.clientX, event.clientY )
+			);
+		}
 	};
 
 	const { startDrag, isDragging } = useDragging( {
@@ -84,7 +107,12 @@ function AngleCircle( {
 	);
 }
 
-function getAngle( centerX, centerY, pointX, pointY ) {
+function getAngle(
+	centerX: number,
+	centerY: number,
+	pointX: number,
+	pointY: number
+) {
 	const y = pointY - centerY;
 	const x = pointX - centerX;
 
