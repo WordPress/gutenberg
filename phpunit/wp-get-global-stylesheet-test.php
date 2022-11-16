5acfd408b55426ad394c9d987564f02146cbf11a
mmtr <miguel.torres@automattic.com>
2022-11-16 13:31:04 +0100

    Fix test

--- phpunit/wp-get-global-stylesheet-test.php
+++ phpunit/wp-get-global-stylesheet-test.php
@@ -7,19 +7,52 @@
 
 class WP_Get_Global_Stylesheet_Test extends WP_UnitTestCase {
 
+	/**
+	 * Administrator ID.
+	 *
+	 * @var int
+	 */
+	protected static $administrator_id;
+
+	/**
+	 * Theme root directory.
+	 *
+	 * @var string
+	 */
+	private $theme_root;
+
+	/**
+	 * Original theme directory.
+	 *
+	 * @var string
+	 */
+	private $orig_theme_dir;
+
+	public static function set_up_before_class() {
+		parent::set_up_before_class();
+
+		self::$administrator_id = self::factory()->user->create(
+			array(
+				'role'       => 'administrator',
+				'user_email' => 'administrator@example.com',
+			)
+		);
+	}
+
 	public function set_up() {
 		parent::set_up();
-		$this->theme_root = realpath( __DIR__ . '/data/themedir1' );
 
 		$this->orig_theme_dir = $GLOBALS['wp_theme_directories'];
+		$this->theme_root     = realpath( DIR_TESTDATA . '/themedir1' );
 
 		// /themes is necessary as theme.php functions assume /themes is the root if there is only one root.
 		$GLOBALS['wp_theme_directories'] = array( WP_CONTENT_DIR . '/themes', $this->theme_root );
 
+		// Set up the new root.
 		add_filter( 'theme_root', array( $this, 'filter_set_theme_root' ) );
 		add_filter( 'stylesheet_root', array( $this, 'filter_set_theme_root' ) );
 		add_filter( 'template_root', array( $this, 'filter_set_theme_root' ) );
-		$this->queries = array();
+
 		// Clear caches.
 		wp_clean_themes_cache();
 		unset( $GLOBALS['wp_themes'] );
@@ -27,8 +60,15 @@ class WP_Get_Global_Stylesheet_Test extends WP_UnitTestCase {
 
 	public function tear_down() {
 		$GLOBALS['wp_theme_directories'] = $this->orig_theme_dir;
+
+		// Clear up the filters to modify the theme root.
+		remove_filter( 'theme_root', array( $this, 'filter_set_theme_root' ) );
+		remove_filter( 'stylesheet_root', array( $this, 'filter_set_theme_root' ) );
+		remove_filter( 'template_root', array( $this, 'filter_set_theme_root' ) );
+
 		wp_clean_themes_cache();
 		unset( $GLOBALS['wp_themes'] );
+
 		parent::tear_down();
 	}
 
@@ -36,18 +76,23 @@ class WP_Get_Global_Stylesheet_Test extends WP_UnitTestCase {
 		return $this->theme_root;
 	}
 
-	public function test_global_styles_changes_invalidates_cache() {
+	public function test_global_styles_user_cpt_change_invalidates_cached_stylesheet() {
+		add_filter( 'global_stylesheet_can_use_cache', '__return_true' );
 		switch_theme( 'block-theme' );
+		wp_set_current_user( self::$administrator_id );
 
-		$user_cpt = WP_Theme_JSON_Resolver_Gutenberg::get_user_data_from_wp_global_styles( wp_get_theme(), true );
+		$styles = gutenberg_get_global_stylesheet();
+		$this->assertStringNotContainsString( 'hotpink', $styles );
 
-		$config     = json_decode( $user_cpt['post_content'], true );
+		$user_cpt                                = WP_Theme_JSON_Resolver_Gutenberg::get_user_data_from_wp_global_styles( wp_get_theme(), true );
+		$config                                  = json_decode( $user_cpt['post_content'], true );
 		$config['styles']['color']['background'] = 'hotpink';
-		$user_cpt['post_content'] = wp_json_encode( $config );
+		$user_cpt['post_content']                = wp_json_encode( $config );
 
 		wp_update_post( $user_cpt, true, false );
 
 		$styles = gutenberg_get_global_stylesheet();
-		$this->assertStringContainsString( '.button{background-color: hotpink;}', $styles );
+		$this->assertStringContainsString( 'background-color: hotpink;', $styles );
+		remove_filter( 'global_stylesheet_can_use_cache', '__return_true' );
 	}
 }
