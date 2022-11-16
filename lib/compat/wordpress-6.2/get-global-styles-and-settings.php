ee310d744eb2744f4c7f823cf633ba11a8f0cb98
André <583546+oandregal@users.noreply.github.com>
2022-11-16 16:56:47 +0100

    Update lib/compat/wordpress-6.2/get-global-styles-and-settings.php
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- lib/compat/wordpress-6.2/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.2/get-global-styles-and-settings.php
@@ -65,7 +65,7 @@ if ( ! function_exists( '_wp_theme_has_theme_json_clean_cache_upon_upgrading_act
 		if (
 			'update' === $options['action'] &&
 			'theme' === $options['type'] &&
-			array_key_exists( get_stylesheet(), $options['themes'] )
+			isset( $options['themes'][ get_stylesheet() ]  )
 		) {
 			wp_theme_has_theme_json_clean_cache();
 		}
