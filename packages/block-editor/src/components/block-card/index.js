3cd445f60f73cb2d918bbdacc376fc5ff1054c3e
Dave Smith <getdavemail@gmail.com>
2022-11-17 13:04:56 +0000

    Add very simple back button to block inspector

--- packages/block-editor/src/components/block-card/index.js
+++ packages/block-editor/src/components/block-card/index.js
@@ -2,13 +2,22 @@
  * WordPress dependencies
  */
 import deprecated from '@wordpress/deprecated';
-
+import { Button } from '@wordpress/components';
+import { chevronLeft, chevronRight } from '@wordpress/icons';
+import { __, isRTL } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
 import BlockIcon from '../block-icon';
 
-function BlockCard( { title, icon, description, blockType } ) {
+function BlockCard( {
+	title,
+	icon,
+	description,
+	blockType,
+	parentBlockClientId,
+	handleBackButton,
+} ) {
 	if ( blockType ) {
 		deprecated( '`blockType` property in `BlockCard component`', {
 			since: '5.7',
@@ -16,8 +25,22 @@ function BlockCard( { title, icon, description, blockType } ) {
 		} );
 		( { title, icon, description } = blockType );
 	}
+
 	return (
 		<div className="block-editor-block-card">
+			{ parentBlockClientId && (
+				<Button
+					onClick={ handleBackButton }
+					label={ __( 'Navigate to the previous view' ) }
+					style={
+						// TODO: This style override is also used in ToolsPanelHeader.
+						// It should be supported out-of-the-box by Button.
+						{ minWidth: 24, padding: 0 }
+					}
+					icon={ isRTL() ? chevronRight : chevronLeft }
+					isSmall
+				/>
+			) }
 			<BlockIcon icon={ icon } showColors />
 			<div className="block-editor-block-card__content">
 				<h2 className="block-editor-block-card__title">{ title }</h2>
