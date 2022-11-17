8e2ffb8948a90eeb68272e202da6a7c7c97135a6
mmtr <miguel.torres@automattic.com>
2022-11-17 11:02:09 +0100

    Invalidate WP_Theme_JSON_Resolver_Gutenberg cache after toggling a plugin

--- lib/compat/wordpress-6.2/default-filters.php
+++ lib/compat/wordpress-6.2/default-filters.php
@@ -25,7 +25,9 @@
  */
 add_action( 'switch_theme', 'wp_theme_clean_theme_json_cached_data' );
 add_action( 'start_previewing_theme', 'wp_theme_clean_theme_json_cached_data' );
-add_action( 'save_post_wp_global_styles', array( 'WP_Theme_JSON_Resolver', 'clean_cached_data' ) );
+add_action( 'save_post_wp_global_styles', array( 'WP_Theme_JSON_Resolver_Gutenberg', 'clean_cached_data' ) );
+add_action( 'activated_plugin', array( 'WP_Theme_JSON_Resolver_Gutenberg', 'clean_cached_data' ) );
+add_action( 'deactivated_plugin', array( 'WP_Theme_JSON_Resolver_Gutenberg', 'clean_cached_data' ) );
 add_action( 'save_post_wp_global_styles', 'gutenberg_get_global_stylesheet_clean_cache' );
 add_action( 'switch_theme', 'gutenberg_get_global_stylesheet_clean_cache' );
 add_action( 'start_previewing_theme', 'gutenberg_get_global_stylesheet_clean_cache' );
