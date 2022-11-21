04f8d7e2aaa7261d01adafeb280590b0533b5ea5
Riad Benguella <benguella@gmail.com>
2022-11-21 14:22:11 +0100

    Remove useless prop

--- packages/edit-site/src/components/sidebar-navigation-title/index.js
+++ packages/edit-site/src/components/sidebar-navigation-title/index.js
@@ -4,21 +4,15 @@
 import {
 	__experimentalHStack as HStack,
 	__experimentalNavigatorBackButton as NavigatorBackButton,
-	Button,
 } from '@wordpress/components';
 import { isRTL, __, sprintf } from '@wordpress/i18n';
 import { chevronRight, chevronLeft } from '@wordpress/icons';
 
-export default function SidebarNavigationTitle( {
-	parentHref,
-	parentTitle,
-	title,
-} ) {
-	const BackComponent = parentHref ? Button : NavigatorBackButton;
+export default function SidebarNavigationTitle( { parentTitle, title } ) {
 	return (
 		<HStack spacing={ 4 } justify="flex-start">
 			{ parentTitle ? (
-				<BackComponent
+				<NavigatorBackButton
 					className="edit-site-sidebar-navigation-title__back"
 					icon={ isRTL() ? chevronRight : chevronLeft }
 					aria-label={ sprintf(
@@ -26,7 +20,6 @@ export default function SidebarNavigationTitle( {
 						__( 'Navigate to the previous view: %s' ),
 						parentTitle
 					) }
-					href={ parentHref }
 				/>
 			) : (
 				<div className="edit-site-sidebar-navigation-title__icon-placeholder" />
