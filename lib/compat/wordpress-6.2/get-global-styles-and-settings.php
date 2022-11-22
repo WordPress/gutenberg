c75fc6d4b8f09dfc4fb2286814cbccc737ccc40f
Miguel Torres <miguel.torres@automattic.com>
2022-11-22 17:19:07 +0100

    Update `gutenberg_get_global_stylesheet` to use `WP_Object_Cache` (#45679)
    Co-authored-by: André <583546+oandregal@users.noreply.github.com>

--- lib/compat/wordpress-6.2/get-global-styles-and-settings.php
+++ lib/compat/wordpress-6.2/get-global-styles-and-settings.php
@@ -79,3 +79,113 @@ if ( ! function_exists( '_wp_theme_has_theme_json_clean_cache_upon_upgrading_act
 		}
 	}
 }
+
+/**
+ * Returns the stylesheet resulting of merging core, theme, and user data.
+ *
+ * @param array $types Types of styles to load. Optional.
+ *                     It accepts 'variables', 'styles', 'presets' as values.
+ *                     If empty, it'll load all for themes with theme.json support
+ *                     and only [ 'variables', 'presets' ] for themes without theme.json support.
+ *
+ * @return string Stylesheet.
+ */
+function gutenberg_get_global_stylesheet( $types = array() ) {
+	// Ignore cache when `WP_DEBUG` is enabled, so it doesn't interfere with the theme developers workflow.
+	$can_use_cached = empty( $types ) && ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG );
+	$cache_key      = 'gutenberg_get_global_stylesheet';
+	$cache_group    = 'theme_json';
+	if ( $can_use_cached ) {
+		$cached = wp_cache_get( $cache_key, $cache_group );
+		if ( $cached ) {
+			return $cached;
+		}
+	}
+	$tree                = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
+	$supports_theme_json = wp_theme_has_theme_json();
+	if ( empty( $types ) && ! $supports_theme_json ) {
+		$types = array( 'variables', 'presets', 'base-layout-styles' );
+	} elseif ( empty( $types ) ) {
+		$types = array( 'variables', 'styles', 'presets' );
+	}
+
+	/*
+	 * If variables are part of the stylesheet,
+	 * we add them.
+	 *
+	 * This is so themes without a theme.json still work as before 5.9:
+	 * they can override the default presets.
+	 * See https://core.trac.wordpress.org/ticket/54782
+	 */
+	$styles_variables = '';
+	if ( in_array( 'variables', $types, true ) ) {
+		/*
+		 * We only use the default, theme, and custom origins.
+		 * This is because styles for blocks origin are added
+		 * at a later phase (render cycle) so we only render the ones in use.
+		 * @see wp_add_global_styles_for_blocks
+		 */
+		$origins          = array( 'default', 'theme', 'custom' );
+		$styles_variables = $tree->get_stylesheet( array( 'variables' ), $origins );
+		$types            = array_diff( $types, array( 'variables' ) );
+	}
+
+	/*
+	 * For the remaining types (presets, styles), we do consider origins:
+	 *
+	 * - themes without theme.json: only the classes for the presets defined by core
+	 * - themes with theme.json: the presets and styles classes, both from core and the theme
+	 */
+	$styles_rest = '';
+	if ( ! empty( $types ) ) {
+		/*
+		 * We only use the default, theme, and custom origins.
+		 * This is because styles for blocks origin are added
+		 * at a later phase (render cycle) so we only render the ones in use.
+		 * @see wp_add_global_styles_for_blocks
+		 */
+		$origins = array( 'default', 'theme', 'custom' );
+		if ( ! $supports_theme_json ) {
+			$origins = array( 'default' );
+		}
+		$styles_rest = $tree->get_stylesheet( $types, $origins );
+	}
+	$stylesheet = $styles_variables . $styles_rest;
+	if ( $can_use_cached ) {
+		wp_cache_set( $cache_key, $stylesheet, $cache_group );
+	}
+	return $stylesheet;
+}
+
+/**
+ * Clean the cache used by the `gutenberg_get_global_stylesheet` function.
+ */
+function gutenberg_get_global_stylesheet_clean_cache() {
+	wp_cache_delete( 'gutenberg_get_global_stylesheet', 'theme_json' );
+}
+
+/**
+ * Private function to clean the cache used by the `gutenberg_get_global_stylesheet` function after an upgrade.
+ *
+ * It is hooked into the `upgrader_process_complete` action.
+ *
+ * @see default-filters.php
+ *
+ * @param WP_Upgrader $upgrader WP_Upgrader instance.
+ * @param array       $options  Array of bulk item update data.
+ */
+function _gutenberg_get_global_stylesheet_clean_cache_upon_upgrading( $upgrader, $options ) {
+	if ( 'update' !== $options['action'] ) {
+		return;
+	}
+
+	if (
+		'core' === $options['type'] ||
+		'plugin' === $options['type'] ||
+		// Clean cache only if the active theme was updated.
+		( 'theme' === $options['type'] && ( isset( $options['themes'][ get_stylesheet() ] ) || isset( $options['themes'][ get_template() ] ) ) )
+	) {
+		gutenberg_get_global_stylesheet_clean_cache();
+	}
+}
+
