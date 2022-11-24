cc8d6a7157c994d2b247601c65ef07f3c5b52186
Jonny Harris <spacedmonkey@users.noreply.github.com>
2022-11-24 11:27:36 +0000

    Remove usage of wp_get_theme. (#45770)
    * Remove usage of wp_get_theme.
    
    * Move variable.

--- lib/experimental/class-wp-theme-json-resolver-gutenberg.php
+++ lib/experimental/class-wp-theme-json-resolver-gutenberg.php
@@ -35,8 +35,9 @@ class WP_Theme_JSON_Resolver_Gutenberg extends WP_Theme_JSON_Resolver_6_2 {
 
 		// When backporting to core, remove the instanceof Gutenberg class check, as it is only required for the Gutenberg plugin.
 		if ( null === static::$theme || ! static::$theme instanceof WP_Theme_JSON_Gutenberg ) {
+			$wp_theme        = wp_get_theme();
 			$theme_json_data = static::read_json_file( static::get_file_path_from_theme( 'theme.json' ) );
-			$theme_json_data = static::translate( $theme_json_data, wp_get_theme()->get( 'TextDomain' ) );
+			$theme_json_data = static::translate( $theme_json_data, $wp_theme->get( 'TextDomain' ) );
 			$theme_json_data = gutenberg_add_registered_webfonts_to_theme_json( $theme_json_data );
 
 			/**
@@ -48,10 +49,10 @@ class WP_Theme_JSON_Resolver_Gutenberg extends WP_Theme_JSON_Resolver_6_2 {
 			$theme_json_data = $theme_json->get_data();
 			static::$theme   = new WP_Theme_JSON_Gutenberg( $theme_json_data );
 
-			if ( wp_get_theme()->parent() ) {
+			if ( $wp_theme->parent() ) {
 				// Get parent theme.json.
 				$parent_theme_json_data = static::read_json_file( static::get_file_path_from_theme( 'theme.json', true ) );
-				$parent_theme_json_data = static::translate( $parent_theme_json_data, wp_get_theme()->parent()->get( 'TextDomain' ) );
+				$parent_theme_json_data = static::translate( $parent_theme_json_data, $wp_theme->parent()->get( 'TextDomain' ) );
 				$parent_theme_json_data = gutenberg_add_registered_webfonts_to_theme_json( $parent_theme_json_data );
 				$parent_theme           = new WP_Theme_JSON_Gutenberg( $parent_theme_json_data );
 
