5650b9df226912d95b7575f04f98100c084f663f
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2022-11-25 23:59:17 +0900

    LinkedButton: remove unnecessary span tag

--- packages/components/src/box-control/linked-button.js
+++ packages/components/src/box-control/linked-button.js
@@ -13,19 +13,16 @@ import Tooltip from '../tooltip';
 export default function LinkedButton( { isLinked, ...props } ) {
 	const label = isLinked ? __( 'Unlink sides' ) : __( 'Link sides' );
 
-	// TODO: Remove span after merging https://github.com/WordPress/gutenberg/pull/44198
 	return (
 		<Tooltip text={ label }>
-			<span>
-				<Button
-					{ ...props }
-					className="component-box-control__linked-button"
-					isSmall
-					icon={ isLinked ? link : linkOff }
-					iconSize={ 24 }
-					aria-label={ label }
-				/>
-			</span>
+			<Button
+				{ ...props }
+				className="component-box-control__linked-button"
+				isSmall
+				icon={ isLinked ? link : linkOff }
+				iconSize={ 24 }
+				aria-label={ label }
+			/>
 		</Tooltip>
 	);
 }
