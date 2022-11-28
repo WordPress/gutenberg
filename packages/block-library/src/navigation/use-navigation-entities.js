d08be084e3668e82c49fb137657725a7a7d9e8e6
Jeff Ong <jonger4@gmail.com>
2022-11-28 11:54:13 -0500

    Remove ununused hooks.

--- packages/block-library/src/navigation/use-navigation-entities.js
+++ packages/block-library/src/navigation/use-navigation-entities.js
@@ -5,18 +5,16 @@ import { useEntityRecords } from '@wordpress/core-data';
 
 /**
  * @typedef {Object} NavigationEntitiesData
- * @property {Array|undefined} pages                    - a collection of WP Post entity objects of post type "Page".
- * @property {boolean}         isResolvingPages         - indicates whether the request to fetch pages is currently resolving.
- * @property {boolean}         hasResolvedPages         - indicates whether the request to fetch pages has finished resolving.
- * @property {Array|undefined} menus                    - a collection of Menu entity objects.
- * @property {boolean}         isResolvingMenus         - indicates whether the request to fetch menus is currently resolving.
- * @property {boolean}         hasResolvedMenus         - indicates whether the request to fetch menus has finished resolving.
- * @property {Array|undefined} menusItems               - a collection of Menu Item entity objects for the current menuId.
- * @property {boolean}         hasResolvedMenuItems     - indicates whether the request to fetch menuItems has finished resolving.
- * @property {boolean}         hasPages                 - indicates whether there is currently any data for pages.
- * @property {boolean}         hasMenus                 - indicates whether there is currently any data for menus.
- * @property {Array|undefined} menuLocations            - a collection of Menu Location entity objects for the active theme.
- * @property {boolean}         hasResolvedMenuLocations - indicates whether the request to fetch MenuLocations has finished resolving.
+ * @property {Array|undefined} pages                - a collection of WP Post entity objects of post type "Page".
+ * @property {boolean}         isResolvingPages     - indicates whether the request to fetch pages is currently resolving.
+ * @property {boolean}         hasResolvedPages     - indicates whether the request to fetch pages has finished resolving.
+ * @property {Array|undefined} menus                - a collection of Menu entity objects.
+ * @property {boolean}         isResolvingMenus     - indicates whether the request to fetch menus is currently resolving.
+ * @property {boolean}         hasResolvedMenus     - indicates whether the request to fetch menus has finished resolving.
+ * @property {Array|undefined} menusItems           - a collection of Menu Item entity objects for the current menuId.
+ * @property {boolean}         hasResolvedMenuItems - indicates whether the request to fetch menuItems has finished resolving.
+ * @property {boolean}         hasPages             - indicates whether there is currently any data for pages.
+ * @property {boolean}         hasMenus             - indicates whether there is currently any data for menus.
  */
 
 /**
@@ -57,12 +55,6 @@ export default function useNavigationEntities( menuId ) {
 			{ enabled: !! menuId }
 		);
 
-	const { records: menuLocations, hasResolved: hasResolvedMenuLocations } =
-		useEntityRecords( 'root', 'menuLocation', {
-			per_page: -1,
-			context: 'view',
-		} );
-
 	return {
 		pages,
 		isResolvingPages,
@@ -76,8 +68,5 @@ export default function useNavigationEntities( menuId ) {
 
 		menuItems,
 		hasResolvedMenuItems,
-
-		menuLocations,
-		hasResolvedMenuLocations,
 	};
 }
