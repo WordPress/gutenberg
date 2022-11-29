3d9bba65e5a194ce352137dc013e71f74ce392f8
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-11-29 10:30:58 -0500

    add initial types to cell.tsx

--- packages/components/src/alignment-matrix-control/types.ts
+++ packages/components/src/alignment-matrix-control/types.ts
@@ -6,3 +6,17 @@ export type AlignmentMatrixControlIconProps = {
 	value: string;
 	props: {};
 };
+
+export type AlignmentMatrixControlCellProps = {
+	isActive: boolean;
+	value:
+		| 'top left'
+		| 'top center'
+		| 'top right'
+		| 'center left'
+		| 'center center'
+		| 'center right'
+		| 'bottom left'
+		| 'bottom center'
+		| 'bottom right';
+};
