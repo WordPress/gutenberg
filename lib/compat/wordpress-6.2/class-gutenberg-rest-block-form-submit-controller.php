dec8682d68180e2bfcc38b8d270ff0eda1885af3
Ari Stathopoulos <aristath@gmail.com>
2022-11-29 12:58:01 +0200

    Use a new REST API endpoint for form submissions

---
+++ lib/compat/wordpress-6.2/class-gutenberg-rest-block-form-submit-controller.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * REST API: Gutenberg_REST_Block_Form_Submit_Controller class
+ *
+ * @package    WordPress
+ * @subpackage REST_API
+ * @since      6.2.0
+ */
+
+/**
+ * Core class used to submit forms (using the form block) via the REST API.
+ *
+ * @see   WP_REST_Controller
+ *
+ * @since 6.2.0
+ */
+class Gutenberg_REST_Block_Form_Submit_Controller extends WP_REST_Controller {
+
+	/**
+	 * The namespace of this controller's route.
+	 *
+	 * @since 6.2.0
+	 * @var string
+	 */
+	protected $namespace = 'wp/v2';
+
+	/**
+	 * The base of this controller's route.
+	 *
+	 * @since 6.2.0
+	 * @var string
+	 */
+	protected $rest_base = 'block-form-submit';
+
+	/**
+	 * Registers the routes for the objects of the controller.
+	 *
+	 * @see   register_rest_route()
+	 *
+	 * @since 6.2.0
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base,
+			array(
+				array(
+					'methods'             => 'POST',
+					'callback'            => array( $this, 'form_submit' ),
+					'permission_callback' => array( $this, 'form_submit_permissions_check' ),
+				),
+			)
+		);
+	}
+
+	/**
+	 * Checks whether a given request has permission to read block patterns.
+	 *
+	 * @since 6.2.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 *
+	 * @return WP_Error|bool True if the request has read access, WP_Error object otherwise.
+	 */
+	public function form_submit_permissions_check( $request ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		return rest_cookie_check_errors( null );
+	}
+
+	/**
+	 * Retrieves all block pattern categories.
+	 *
+	 * @since 6.2.0
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 */
+	public function form_submit( $request ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		$params = $request->get_params();
+
+		/**
+		 * Allow short-circuiting the form submission.
+		 *
+		 * @since 6.2.0
+		 *
+		 * @param array $params The form parameters.
+		 *
+		 * @return bool True if the form submission should be short-circuited, false otherwise.
+		 */
+		if ( apply_filters( 'gutenberg_form_submit_short_circuit', false, $params ) ) {
+			return;
+		}
+
+		$content = sprintf(
+			/* translators: %s: The request URI. */
+			__( 'Form submission from %1$s', 'gutenberg' ) . '</br>',
+			'<a href="' . esc_url( $params['_wp_http_referer'] ) . '">' . esc_url( $params['_wp_http_referer'] ) . '</a>'
+		);
+
+		$skip_fields = array( 'wp_rest', '_wp_http_referer' );
+		foreach ( $params as $key => $value ) {
+			if ( in_array( $key, $skip_fields, true ) ) {
+				continue;
+			}
+			$content .= $key . ': ' . $value . '</br>';
+		}
+
+		wp_mail( get_option( 'admin_email' ), __( 'Form submission', 'gutenberg' ), $content );
+
+		wp_safe_redirect( get_site_url( null, $params['_wp_http_referer'] ) );
+		exit;
+	}
+}
--- packages/block-library/src/form/index.php
+++ packages/block-library/src/form/index.php
@@ -8,54 +8,20 @@
 /**
  * Renders the `core/form` block on server.
  *
- * @param array    $attributes The block attributes.
- * @param string   $content The saved content.
- * @param WP_Block $block The parsed block.
+ * @param array  $attributes The block attributes.
+ * @param string $content The saved content.
  *
  * @return string The content of the block being rendered.
  */
-function render_block_core_form( $attributes, $content, $block ) {
-
-	// Build the form action attribute.
-	$action = empty( $attributes['action'] ) ? '' : $attributes['action'];
-	$action = empty( $action ) ? '#' : $action;
-
-	/**
-	 * Filters the form action attribute.
-	 *
-	 * @param string $action The form action attribute.
-	 * @param array  $attributes The block attributes.
-	 * @param string $content The saved content.
-	 * @param WP_Block $block The parsed block.
-	 *
-	 * @return string The form action attribute.
-	 */
-	$action = apply_filters( 'block_form_action', $action, $attributes, $content, $block );
-
-	// Build the form method attribute.
-	$method = empty( $attributes['method'] ) ? '' : $attributes['method'];
-	$method = empty( $method ) || ( 'get' !== strtolower( $method ) && 'post' !== strtolower( $method ) ) ? 'post' : $method;
-	/**
-	 * Filters the form method attribute.
-	 *
-	 * @param string $method The form method attribute.
-	 * @param array  $attributes The block attributes.
-	 * @param string $content The saved content.
-	 * @param WP_Block $block The parsed block.
-	 *
-	 * @return string The form method attribute.
-	 */
-	$method = apply_filters( 'block_form_method', $method, $attributes, $content, $block );
-
-	$form_id = empty( $attributes['formId'] ) ? md5( json_encode( array( $attributes, $block ) ) ) : $attributes['formId'];
+function render_block_core_form( $attributes, $content ) {
 
 	// Add the form action & method.
 	$processed_content = new WP_HTML_Tag_Processor( $content );
 	$processed_content->next_tag( 'form' );
-	$processed_content->set_attribute( 'action', esc_attr( $action ) );
-	$processed_content->set_attribute( 'method', esc_attr( $method ) );
+	$processed_content->set_attribute( 'action', esc_attr( rest_url( 'wp/v2/block-form-submit' ) ) );
+	$processed_content->set_attribute( 'method', 'post' );
 
-	$nonce_field = wp_nonce_field( 'wp-block-form', '_wpnonce', true, false );
+	$nonce_field = wp_nonce_field( 'wp-block-form', 'wp_rest', true, false );
 
 	// Inject a hidden input with the nonce to strengthen security form-ID.
 	return str_replace(
@@ -77,37 +43,3 @@ function register_block_core_form() {
 	);
 }
 add_action( 'init', 'register_block_core_form' );
-
-/**
- * Handles the form submission.
- *
- * This is the default action, which sends an email to the admin.
- * To override the default action, first remove the default action:
- * remove_action( 'wp', 'submit_core_form_block' );
- * Then add your own action by creating a function and hooking it to the 'wp' action.
- */
-function submit_core_form_block() {
-
-	$has_valid_nonce = ! empty( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wp-block-form' );
-	if ( ! $has_valid_nonce ) {
-		return;
-	}
-
-	$referer = wp_get_referer();
-	$content = sprintf(
-		/* translators: %s: The request URI. */
-		__( 'Form submission from %1$s', 'gutenberg' ) . '</br>',
-		'<a href="' . esc_url( $referer ) . '">' . esc_url( $referer ) . '</a>'
-	);
-
-	$skip_fields = array( '_wpnonce', '_wp_http_referer' );
-	foreach ( $_POST as $key => $value ) {
-		if ( in_array( $key, $skip_fields, true ) ) {
-			continue;
-		}
-		$content .= $key . ': ' . $value . '</br>';
-	}
-
-	wp_mail( get_option( 'admin_email' ), __( 'Form submission', 'gutenberg' ), $content );
-}
-add_action( 'wp', 'submit_core_form_block' );
