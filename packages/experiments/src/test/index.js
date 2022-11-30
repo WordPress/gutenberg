65750cfda8bb27909b79e51f3270c24bef6e7eb0
Adam Zieliński <adam@adamziel.com>
2022-11-30 18:30:31 +0000

    Add unit tests to demonstrate how to export private functions, function arguments, and react components

--- packages/experiments/src/test/index.js
+++ packages/experiments/src/test/index.js
@@ -215,3 +215,181 @@ describe( 'advanced lock() and unlock()', () => {
 		expect( unlock( object1 ) ).toBe( 'Decorated: sh' );
 	} );
 } );
+
+describe( 'Specific use-cases of sharing private APIs', () => {
+	let lock, unlock;
+	beforeEach( () => {
+		// This would live in @experiments/test:
+		// Opt-in to experimental APIs
+		const experimentsAPI = __dangerousOptInToUnstableAPIsOnlyForCoreModules(
+			requiredConsent,
+			'@experiments/test'
+		);
+		lock = experimentsAPI.lock;
+		unlock = experimentsAPI.unlock;
+	} );
+
+	it( 'Should enable privately exporting experimental functions', () => {
+		/**
+		 * Problem: The private __experimentalFunction should not be publicly
+		 * exposed to the consumers of package1.
+		 */
+		function __experimentalFunction() {}
+		/**
+		 * Solution: Privately lock it inside a publicly exported object.
+		 *
+		 * In `package1/index.js` we'd say:
+		 *
+		 * ```js
+		 * export const experiments = {};
+		 * lock(experiments, {
+		 *     __experimentalFunction
+		 * });
+		 * ```
+		 *
+		 * Let's simulate in the test code:
+		 */
+		const experiments = {};
+		const package1Exports = {
+			experiments,
+		};
+		lock( experiments, { __experimentalFunction } );
+
+		/**
+		 * Then, in the consumer package we'd say:
+		 *
+		 * ```js
+		 * import { experiments } from 'package1';
+		 * const { __experimentalFunction } = unlock( experiments );
+		 * ```
+		 *
+		 * Let's simulate that, too:
+		 */
+		const unlockedFunction = unlock(
+			package1Exports.experiments
+		).__experimentalFunction;
+		expect( unlockedFunction ).toBe( __experimentalFunction );
+	} );
+
+	it( 'Should enable exporting functions with private experimental arguments', () => {
+		/**
+		 * The publicly exported function does not have any experimental
+		 * arguments.
+		 *
+		 * @param {any} data The data to log
+		 */
+		function logData( data ) {
+			// Internally, it calls the experimental version of the function
+			// with fixed default values for the experimental arguments.
+			__experimentalLogData( data, 'plain' );
+		}
+		/**
+		 * The private experimental function is not publicly exported. Instead, it's
+		 * "locked" inside of the public logData function. It can be unlocked by any
+		 * participant of the private importing system.
+		 *
+		 * @param {any}    data                 The data to log
+		 * @param {string} __experimentalFormat The logging format to use.
+		 */
+		function __experimentalLogData( data, __experimentalFormat ) {
+			if ( __experimentalFormat === 'table' ) {
+				// eslint-disable-next-line no-console
+				console.table( data );
+			} else {
+				// eslint-disable-next-line no-console
+				console.log( data );
+			}
+		}
+		lock( logData, __experimentalLogData );
+		/**
+		 * In package/log-data.js:
+		 *
+		 * ```js
+		 * lock( logData, __experimentalLogData );
+		 * export logData;
+		 * ```
+		 *
+		 * Then, in package/index.js:
+		 *
+		 * ```js
+		 * export { logData } from './log-data';
+		 * ```
+		 *
+		 * And that's it! The public function is publicly exported, and the
+		 * experimental function is available via unlock( logData ):
+		 *
+		 * ```js
+		 * import { logData } from 'package1';
+		 * const experimenalLogData = unlock( logData );
+		 * ```
+		 */
+		expect( unlock( logData ) ).toBe( __experimentalLogData );
+	} );
+
+	it( 'Should enable exporting React Components with private experimental properties', () => {
+		// eslint-disable-next-line jsdoc/require-param
+		/**
+		 * The publicly exported component does not have any experimental
+		 * properties.
+		 */
+		function DataTable( { data } ) {
+			// Internally, it calls the experimental version of the function
+			// with fixed default values for the experimental arguments.
+			return (
+				<ExperimentalDataTable
+					data={ data }
+					__experimentalFancyFormatting={ false }
+				/>
+			);
+		}
+		// eslint-disable-next-line jsdoc/require-param
+		/**
+		 * The private experimental component is not publicly exported. Instead, it's
+		 * "locked" inside of the public logData function. It can be unlocked by any
+		 * participant of the private importing system.
+		 */
+		function ExperimentalDataTable( {
+			data,
+			__experimentalFancyFormatting,
+		} ) {
+			const className = __experimentalFancyFormatting
+				? 'savage-css'
+				: 'regular-css';
+			return (
+				<table className={ className }>
+					{ data.map( ( row, i ) => (
+						<tr key={ i }>
+							{ row.map( ( col, j ) => (
+								<td key={ j }>{ col }</td>
+							) ) }
+						</tr>
+					) ) }
+				</table>
+			);
+		}
+		lock( DataTable, ExperimentalDataTable );
+		/**
+		 * In package/data-table.js:
+		 *
+		 * ```js
+		 * lock( DataTable, ExperimentalDataTable );
+		 * export DataTable;
+		 * ```
+		 *
+		 * Then, in package/index.js:
+		 *
+		 * ```js
+		 * export { DataTable } from './data-table';
+		 * ```
+		 *
+		 * And that's it! The public function is publicly exported, and the
+		 * experimental function is available via unlock( logData ):
+		 *
+		 * ```js
+		 * import { DataTable } from 'package1';
+		 * const ExperimentalDataTable = unlock( DataTable );
+		 * ```
+		 */
+		expect( unlock( DataTable ) ).toBe( ExperimentalDataTable );
+	} );
+} );
