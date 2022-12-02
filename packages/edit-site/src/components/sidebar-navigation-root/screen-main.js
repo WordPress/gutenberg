c57663286fb26337779e1e8c081ae2e6ba89c8f0
André <583546+oandregal@users.noreply.github.com>
2022-12-02 11:52:07 +0100

    Set template being edited, no matter the theme

--- packages/edit-site/src/components/sidebar-navigation-root/screen-main.js
+++ packages/edit-site/src/components/sidebar-navigation-root/screen-main.js
@@ -11,7 +11,7 @@ import {
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { layout, symbolFilled } from '@wordpress/icons';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { useViewportMatch } from '@wordpress/compose';
 
 /**
@@ -26,11 +26,14 @@ import getIsListPage from '../../utils/get-is-list-page';
 
 export default function SidebarNavigationScreenMain() {
 	const { params } = useLocation();
+	const templateId = useSelect( ( select ) => {
+		return select( editSiteStore ).getEditedPostId();
+	} );
 	const isListPage = getIsListPage( params );
 	const isEditorPage = ! isListPage;
 	const templatesLink = useLink( {
 		postType: 'wp_template',
-		postId: 'twentytwentythree//index', // TODO: retrieve current theme
+		postId: templateId,
 	} );
 	const templatePartsLink = useLink( {
 		postType: 'wp_template_part',
