3a2d2a1cb9651756b0e3eca1fdb88ce27249944f
Ben Dwyer <ben@scruffian.com>
2022-12-02 10:00:40 +0000

    Also lock the page list item blocks

--- packages/block-library/src/page-list/edit.js
+++ packages/block-library/src/page-list/edit.js
@@ -105,6 +105,7 @@ export default function PageListEdit( {
 		allowedBlocks: [ 'core/page-list-item' ],
 		renderAppender: false,
 		__unstableDisableDropZone: true,
+		templateLock: 'all',
 	} );
 
 	const getBlockContent = () => {
