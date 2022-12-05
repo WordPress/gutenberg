dcd939fa1da5f997f5404ce1660f73d52531ab6d
ramonjd <ramonjd@gmail.com>
2022-12-05 15:52:01 +1100

    Removing tests so we can debug them separately

--- packages/components/src/palette-edit/test/index.js
+++ packages/components/src/palette-edit/test/index.js
@@ -1,12 +1,7 @@
 /**
- * External dependencies
- */
-import { render, fireEvent, screen } from '@testing-library/react';
-
-/**
  * Internal dependencies
  */
-import PaletteEdit, { getNameForPosition } from '../';
+import { getNameForPosition } from '../';
 
 describe( 'getNameForPosition', () => {
 	test( 'should return 1 by default', () => {
@@ -64,54 +59,5 @@ describe( 'getNameForPosition', () => {
 		expect( getNameForPosition( elements, slugPrefix ) ).toEqual(
 			'Color 151'
 		);
-	} );
-} );
-
-describe( 'PaletteEdit', () => {
-	const defaultProps = {
-		gradients: false,
-		colors: [ { color: '#ffffff', name: 'Base', slug: 'base' } ],
-		onChange: jest.fn(),
-		paletteLabel: 'Test label',
-		emptyMessage: 'Test empty message',
-		canOnlyChangeValues: true,
-		canReset: true,
-		slugPrefix: '',
-	};
-
-	it( 'opens color selector for color palettes', () => {
-		render( <PaletteEdit { ...defaultProps } /> );
-		fireEvent.click( screen.getByLabelText( 'Color: Base' ) );
-		expect( screen.getByLabelText( 'Hex color' ) ).toBeInTheDocument();
-	} );
-
-	it( 'opens gradient selector for gradient palettes', async () => {
-		const defaultGradientProps = {
-			gradients: [
-				{
-					gradient:
-						'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
-					name: 'Vivid cyan blue to vivid purple',
-					slug: 'vivid-cyan-blue-to-vivid-purple',
-				},
-			],
-			onChange: jest.fn(),
-			paletteLabel: 'Test label',
-			emptyMessage: 'Test empty message',
-			canOnlyChangeValues: true,
-			canReset: true,
-			slugPrefix: '',
-		};
-		render( <PaletteEdit { ...defaultGradientProps } /> );
-
-		fireEvent.click(
-			screen.getByLabelText( 'Gradient: Vivid cyan blue to vivid purple' )
-		);
-
-		expect(
-			await screen.findByLabelText(
-				'Gradient control point at position 0% with color code rgba(6,147,227,1).'
-			)
-		).toBeInTheDocument();
 	} );
 } );
