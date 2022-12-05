bea1bcf49421a2e52b7029868bcc316e37abaab4
Robert Anderson <robert@noisysocks.com>
2022-12-05 12:11:15 +1100

    Add Style Book to Global Styles (#45960)
    * Add Style Book
    
    * Disable toolbar butons when Style Book is open
    
    * Disable Style Book when in Code Editor mode
    
    * Make cursor a hand when hovering an example
    
    * Remove top and bottom margin from block inside preview
    
    * Add border beneath tab bar, make tab bar scroll on mobile
    
    * Improve performance
    
    * Improve a11y
    
    * Add E2E tests
    
    * Make example titles uppercase
    
    * Hide toolbar when Style Book is open
    
    * Make tabs always have white background
    
    * Update tests
    
    * Add a little note of encouragement for future developers
    
    * Add border radius to Story Book
    
    * Fix vertical alignment of close button
    
    * Reduce font size and increase font weight of example labels
    
    * Remove hover outline
    
    * Use theme color for text instead of white or black
    
    * Use math.div()

--- packages/edit-site/src/components/block-editor/resizable-editor.js
+++ packages/edit-site/src/components/block-editor/resizable-editor.js
@@ -3,20 +3,10 @@
  */
 import { useState, useRef, useCallback } from '@wordpress/element';
 import { ResizableBox } from '@wordpress/components';
-import {
-	__experimentalUseResizeCanvas as useResizeCanvas,
-	__unstableEditorStyles as EditorStyles,
-	__unstableIframe as Iframe,
-	__unstableUseMouseMoveTypingReset as useMouseMoveTypingReset,
-	store as blockEditorStore,
-} from '@wordpress/block-editor';
-import { useSelect } from '@wordpress/data';
-import { useMergeRefs, useResizeObserver } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
-import { store as editSiteStore } from '../../store';
 import ResizeHandle from './resize-handle';
 
 // Removes the inline styles in the drag handles.
@@ -32,37 +22,22 @@ const HANDLE_STYLES_OVERRIDE = {
 	left: undefined,
 };
 
-function ResizableEditor( { enableResizing, settings, children, ...props } ) {
-	const [ resizeObserver, sizes ] = useResizeObserver();
-	const { deviceType, isZoomOutMode, canvasMode } = useSelect(
-		( select ) => ( {
-			deviceType:
-				select( editSiteStore ).__experimentalGetPreviewDeviceType(),
-			canvasMode: select( editSiteStore ).__unstableGetCanvasMode(),
-			isZoomOutMode:
-				select( blockEditorStore ).__unstableGetEditorMode() ===
-				'zoom-out',
-		} ),
-		[]
-	);
-	const deviceStyles = useResizeCanvas( deviceType );
+function ResizableEditor( { enableResizing, height, children } ) {
 	const [ width, setWidth ] = useState( '100%' );
-	const iframeRef = useRef();
-	const mouseMoveTypingResetRef = useMouseMoveTypingReset();
-	const ref = useMergeRefs( [ iframeRef, mouseMoveTypingResetRef ] );
-	const isResizingEnabled = enableResizing && canvasMode !== 'view';
+	const resizableRef = useRef();
 	const resizeWidthBy = useCallback( ( deltaPixels ) => {
-		if ( iframeRef.current ) {
-			setWidth( iframeRef.current.offsetWidth + deltaPixels );
+		if ( resizableRef.current ) {
+			setWidth( resizableRef.current.offsetWidth + deltaPixels );
 		}
 	}, [] );
-
 	return (
 		<ResizableBox
+			ref={ ( api ) => {
+				resizableRef.current = api?.resizable;
+			} }
 			size={ {
-				width: isResizingEnabled ? width : '100%',
-				height:
-					isResizingEnabled && sizes.height ? sizes.height : '100%',
+				width: enableResizing ? width : '100%',
+				height: enableResizing && height ? height : '100%',
 			} }
 			onResizeStop={ ( event, direction, element ) => {
 				setWidth( element.style.width );
@@ -71,10 +46,10 @@ function ResizableEditor( { enableResizing, settings, children, ...props } ) {
 			maxWidth="100%"
 			maxHeight="100%"
 			enable={ {
-				right: isResizingEnabled,
-				left: isResizingEnabled,
+				right: enableResizing,
+				left: enableResizing,
 			} }
-			showHandle={ isResizingEnabled }
+			showHandle={ enableResizing }
 			// The editor is centered horizontally, resizing it only
 			// moves half the distance. Hence double the ratio to correctly
 			// align the cursor to the resizer handle.
@@ -99,42 +74,7 @@ function ResizableEditor( { enableResizing, settings, children, ...props } ) {
 				right: HANDLE_STYLES_OVERRIDE,
 			} }
 		>
-			<Iframe
-				scale={ ( isZoomOutMode && 0.45 ) || undefined }
-				frameSize={ isZoomOutMode ? 100 : undefined }
-				style={ isResizingEnabled ? {} : deviceStyles }
-				head={
-					<>
-						<EditorStyles styles={ settings.styles } />
-						<style>{
-							// Forming a "block formatting context" to prevent margin collapsing.
-							// @see https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Block_formatting_context
-							`.is-root-container { display: flow-root; }
-							body { position: relative; }`
-						}</style>
-						{ isResizingEnabled && (
-							<style>
-								{
-									// Some themes will have `min-height: 100vh` for the root container,
-									// which isn't a requirement in auto resize mode.
-									`.is-root-container { min-height: 0 !important; }`
-								}
-							</style>
-						) }
-					</>
-				}
-				assets={ settings.__unstableResolvedAssets }
-				ref={ ref }
-				name="editor-canvas"
-				className="edit-site-visual-editor__editor-canvas"
-				readonly={ canvasMode === 'view' }
-				{ ...props }
-			>
-				{ /* Filters need to be rendered before children to avoid Safari rendering issues. */ }
-				{ settings.svgFilters }
-				{ resizeObserver }
-				{ children }
-			</Iframe>
+			{ children }
 		</ResizableBox>
 	);
 }
