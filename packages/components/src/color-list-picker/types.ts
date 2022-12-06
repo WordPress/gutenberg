ab69c88d6b05b5b2c71c854a469b8ac1ab03505a
chad1008 <13856531+chad1008@users.noreply.github.com>
2022-12-06 13:41:08 -0500

    add types for `disableCustomColors` and `enableAlpha`

--- packages/components/src/color-list-picker/types.ts
+++ packages/components/src/color-list-picker/types.ts
@@ -2,9 +2,14 @@ export type ColorListPickerProps = {
 	colors: Array< { name: string; color: string } >;
 	labels: Array< string >;
 	value?: Array< string >;
+	disableCustomColors?: boolean;
+	enableAlpha?: boolean;
 };
 
-export type ColorOptionProps = Pick< ColorListPickerProps, 'colors' > & {
+export type ColorOptionProps = Pick<
+	ColorListPickerProps,
+	'colors' | 'disableCustomColors' | 'enableAlpha'
+> & {
 	label: ColorListPickerProps[ 'labels' ][ number ];
 	value: string; // TODO: can we extract this from ColorListPickerProps['value']? That prop being optional is a little tricky
 };
