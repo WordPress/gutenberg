4f10b7fcc4972eaf876d890f73713b46b00c3259
Alba Rincón <alba.rincon@automattic.com>
2022-12-09 15:31:12 +0100

    Remove `/lib` changes and add the new setting under 6.2 `site-editor.php`

--- lib/compat/wordpress-6.1/template-parts-screen.php
+++ lib/compat/wordpress-6.1/template-parts-screen.php
@@ -103,8 +103,6 @@ function gutenberg_template_parts_screen_init( $hook ) {
 		}
 	);
 
-	$extra_template_types = apply_filters( 'extra_template_types', array() );
-
 	$indexed_template_types = array();
 	foreach ( get_default_block_template_types() as $slug => $template_type ) {
 		$template_type['slug']    = (string) $slug;
@@ -116,7 +114,6 @@ function gutenberg_template_parts_screen_init( $hook ) {
 		'siteUrl'                   => site_url(),
 		'postsPerPage'              => get_option( 'posts_per_page' ),
 		'styles'                    => get_block_editor_theme_styles(),
-		'extraTemplateTypes'        => $extra_template_types,
 		'defaultTemplateTypes'      => $indexed_template_types,
 		'defaultTemplatePartAreas'  => get_allowed_block_template_part_areas(),
 		'supportsLayout'            => wp_theme_has_theme_json(),
--- lib/compat/wordpress-6.2/site-editor.php
+++ lib/compat/wordpress-6.2/site-editor.php
@@ -6,19 +6,30 @@
  */
 
 /**
- * Remove home template setting for WP 6.2.
+ * Remove home template setting and add filtered extraTemplatesTypes setting for WP 6.2.
  *
  * @param array                   $settings Existing block editor settings.
  * @param WP_Block_Editor_Context $context The current block editor context.
  * @return array
  */
-function gutenberg_site_editor_unset_homepage_setting( $settings, $context ) {
+function gutenberg_site_editor_update_settings( $settings, $context ) {
 	if ( isset( $context->post ) ) {
 		return $settings;
 	}
 
 	unset( $settings['__unstableHomeTemplate'] );
 
+	/**
+	 * Filters the list of extra templates.
+	 *
+	 * Template types added in this filter will show up under the `Add new` menu in the Site Editor.
+	 *
+	 * @since 6.2.0
+	 *
+	 * @param array $extra_template_types An array of template types, formatted as [ slug, title, description ] ].
+	 */
+	$settings['extraTemplateTypes'] = apply_filters( 'extra_template_types', array() );
+
 	return $settings;
 }
-add_filter( 'block_editor_settings_all', 'gutenberg_site_editor_unset_homepage_setting', 10, 2 );
+add_filter( 'block_editor_settings_all', 'gutenberg_site_editor_update_settings', 10, 2 );
