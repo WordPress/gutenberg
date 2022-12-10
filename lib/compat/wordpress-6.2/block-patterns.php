803f7c503a390eb752017d98ede0211ee5485b55
ntsekouras <ntsekouras@outlook.com>
2022-12-10 13:42:51 +0200

    [Patterns]: Remove bundled patterns

--- lib/compat/wordpress-6.2/block-patterns.php
+++ lib/compat/wordpress-6.2/block-patterns.php
@@ -141,51 +141,6 @@ function gutenberg_register_core_block_patterns_categories() {
 add_action( 'init', 'gutenberg_register_core_block_patterns_categories' );
 
 /**
- * Registers Gutenberg-bundled patterns, with a focus on headers and footers
- * for site editing.
- *
- * @since 6.2.0
- * @access private
- */
-function gutenberg_register_core_block_patterns() {
-	if ( ! get_theme_support( 'core-block-patterns' ) ) {
-		return;
-	}
-
-	$core_block_patterns = array(
-		'centered-footer',
-		'centered-footer-with-social-links',
-		'centered-header',
-		'centered-logo-in-navigation',
-		'footer-with-background-color-and-three-columns',
-		'footer-with-credit-line-and-navigation',
-		'footer-with-large-font-size',
-		'footer-with-navigation-and-credit-line',
-		'footer-with-search-site-title-and-credit-line',
-		'footer-with-site-title-and-credit-line',
-		'header-with-large-font-size',
-		'left-aligned-footer',
-		'right-aligned-footer',
-		'simple-header',
-		'simple-header-inside-image',
-		'simple-header-with-background-color',
-		'simple-header-with-image',
-		'simple-header-with-tagline',
-		'simple-header-with-tagline-2',
-		'site-title-and-menu-button',
-		'site-title-and-vertical-navigation',
-	);
-
-	foreach ( $core_block_patterns as $core_block_pattern ) {
-		register_block_pattern(
-			'core/' . $core_block_pattern,
-			require __DIR__ . '/block-patterns/' . $core_block_pattern . '.php'
-		);
-	}
-}
-add_action( 'init', 'gutenberg_register_core_block_patterns' );
-
-/**
  * Register any patterns that the active theme may provide under its
  * `./patterns/` directory. Each pattern is defined as a PHP file and defines
  * its metadata using plugin-style headers. The minimum required definition is:
