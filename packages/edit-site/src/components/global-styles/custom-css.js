1df72bf8d0a0862707702545f4ca91d89c805eca
Glen Davies <glen.davies@automattic.com>
2022-12-13 11:39:31 +1300

    Add theme.json custom css to input box

--- packages/edit-site/src/components/global-styles/custom-css.js
+++ packages/edit-site/src/components/global-styles/custom-css.js
@@ -1,7 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { TextareaControl } from '@wordpress/components';
+import { TextareaControl, Panel, PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -10,14 +11,53 @@ import { useStyle } from './hooks';
 
 function CustomCSSControl() {
 	const [ customCSS, setCustomCSS ] = useStyle( 'css' );
+	const [ themeCSS ] = useStyle( 'css', null, 'base' );
+	const ignoreThemeCustomCSS = '/* IgnoreThemeCustomCSS */';
+
+	const themeCustomCSS =
+		! customCSS && themeCSS
+			? `/* ${ __(
+					'Theme Custom CSS start'
+			  ) } */\n${ themeCSS }\n/* ${ __( 'Theme Custom CSS end' ) } */`
+			: undefined;
+
+	function handleOnChange( value ) {
+		if ( themeCSS && value === '' ) {
+			setCustomCSS( ignoreThemeCustomCSS );
+			return;
+		}
+		setCustomCSS( value );
+	}
+
+	const originalThemeCustomCSS =
+		themeCSS && customCSS && themeCustomCSS !== customCSS
+			? themeCSS
+			: undefined;
 
 	return (
-		<TextareaControl
-			value={ customCSS }
-			onChange={ ( value ) => setCustomCSS( value ) }
-			rows={ 15 }
-			className="edit-site-global-styles__custom-css-input"
-		/>
+		<>
+			<TextareaControl
+				value={
+					customCSS?.replace( ignoreThemeCustomCSS, '' ) ||
+					themeCustomCSS
+				}
+				onChange={ ( value ) => handleOnChange( value ) }
+				rows={ 15 }
+				className="edit-site-global-styles__custom-css-input"
+			/>
+			{ originalThemeCustomCSS && (
+				<Panel>
+					<PanelBody
+						title={ __( 'Original Theme Custom CSS' ) }
+						initialOpen={ false }
+					>
+						<pre className="edit-site-global-styles__custom-css-theme-css">
+							{ originalThemeCustomCSS }
+						</pre>
+					</PanelBody>
+				</Panel>
+			) }
+		</>
 	);
 }
 
