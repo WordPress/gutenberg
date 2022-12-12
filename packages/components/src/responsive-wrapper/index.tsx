b4e56a3d1d793fe0a64d317fb21f8ae9a83b871c
Lena Morita <lena@jaguchi.com>
2022-12-13 05:12:03 +0900

    Add main JSDoc

--- packages/components/src/responsive-wrapper/index.tsx
+++ packages/components/src/responsive-wrapper/index.tsx
@@ -14,6 +14,22 @@ import { useResizeObserver } from '@wordpress/compose';
  */
 import type { ResponsiveWrapperProps } from './types';
 
+/**
+ * A wrapper component that maintains its aspect ratio when resized.
+ *
+ * ```jsx
+ * import { ResponsiveWrapper } from '@wordpress/components';
+ *
+ * const MyResponsiveWrapper = () => (
+ * 	<ResponsiveWrapper naturalWidth={ 2000 } naturalHeight={ 680 }>
+ * 		<img
+ * 			src="https://s.w.org/style/images/about/WordPress-logotype-standard.png"
+ * 			alt="WordPress"
+ * 		/>
+ * 	</ResponsiveWrapper>
+ * );
+ * ```
+ */
 function ResponsiveWrapper( {
 	naturalWidth,
 	naturalHeight,
