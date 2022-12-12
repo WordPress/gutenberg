0e9f0b3d4960fa010b949806c99bd234a3527fb3
Dave Smith <getdavemail@gmail.com>
2022-12-12 12:24:34 +0000

    Return stable reference from createPage hook

--- packages/block-editor/src/components/link-control/use-create-page.js
+++ packages/block-editor/src/components/link-control/use-create-page.js
@@ -2,43 +2,46 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useEffect, useState, useRef } from '@wordpress/element';
+import { useEffect, useState, useRef, useCallback } from '@wordpress/element';
 
 export default function useCreatePage( handleCreatePage ) {
 	const cancelableCreateSuggestion = useRef();
 	const [ isCreatingPage, setIsCreatingPage ] = useState( false );
 	const [ errorMessage, setErrorMessage ] = useState( null );
 
-	const createPage = async function ( suggestionTitle ) {
-		setIsCreatingPage( true );
-		setErrorMessage( null );
+	const createPage = useCallback(
+		async function ( suggestionTitle ) {
+			setIsCreatingPage( true );
+			setErrorMessage( null );
 
-		try {
-			// Make cancellable in order that we can avoid setting State
-			// if the component unmounts during the call to `createSuggestion`
-			cancelableCreateSuggestion.current = makeCancelable(
-				// Using Promise.resolve to allow createSuggestion to return a
-				// non-Promise based value.
-				Promise.resolve( handleCreatePage( suggestionTitle ) )
-			);
+			try {
+				// Make cancellable in order that we can avoid setting State
+				// if the component unmounts during the call to `createSuggestion`
+				cancelableCreateSuggestion.current = makeCancelable(
+					// Using Promise.resolve to allow createSuggestion to return a
+					// non-Promise based value.
+					Promise.resolve( handleCreatePage( suggestionTitle ) )
+				);
 
-			return await cancelableCreateSuggestion.current.promise;
-		} catch ( error ) {
-			if ( error && error.isCanceled ) {
-				return; // bail if canceled to avoid setting state
+				return await cancelableCreateSuggestion.current.promise;
+			} catch ( error ) {
+				if ( error && error.isCanceled ) {
+					return; // bail if canceled to avoid setting state
+				}
+
+				setErrorMessage(
+					error.message ||
+						__(
+							'An unknown error occurred during creation. Please try again.'
+						)
+				);
+				throw error;
+			} finally {
+				setIsCreatingPage( false );
 			}
-
-			setErrorMessage(
-				error.message ||
-					__(
-						'An unknown error occurred during creation. Please try again.'
-					)
-			);
-			throw error;
-		} finally {
-			setIsCreatingPage( false );
-		}
-	};
+		},
+		[ setIsCreatingPage, setErrorMessage, handleCreatePage ]
+	);
 
 	/**
 	 * Handles cancelling any pending Promises that have been made cancelable.
