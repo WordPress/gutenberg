3f4bea1da8fdc1ccf0c112e7cd6473e1a97834ed
Robert Anderson <robert@noisysocks.com>
2022-12-13 11:37:04 +1100

    Disable button when there are no changes to push

--- packages/edit-site/src/hooks/push-changes-to-global-styles.js
+++ packages/edit-site/src/hooks/push-changes-to-global-styles.js
@@ -16,7 +16,7 @@ import {
 	__EXPERIMENTAL_STYLE_PROPERTY as STYLE_PROPERTY,
 	getBlockType,
 } from '@wordpress/blocks';
-import { useContext } from '@wordpress/element';
+import { useContext, useMemo, useCallback } from '@wordpress/element';
 import { useDispatch } from '@wordpress/data';
 import { store as noticesStore } from '@wordpress/notices';
 
@@ -30,84 +30,98 @@ import {
 	STYLE_PATH_TO_PRESET_BLOCK_ATTRIBUTE,
 } from '../components/global-styles/utils';
 
+function getChangesToPush( name, attributes ) {
+	return getSupportedGlobalStylesPanels( name ).flatMap( ( key ) => {
+		const { value: path } = STYLE_PROPERTY[ key ];
+		const presetAttributeKey = path.join( '.' );
+		const presetAttributeValue =
+			attributes[
+				STYLE_PATH_TO_PRESET_BLOCK_ATTRIBUTE[ presetAttributeKey ]
+			];
+		const value = presetAttributeValue
+			? `var:preset|${ STYLE_PATH_TO_CSS_VAR_INFIX[ presetAttributeKey ] }|${ presetAttributeValue }`
+			: get( attributes.style, path );
+		return value ? [ { key, path, value } ] : [];
+	} );
+}
+
 function cloneDeep( object ) {
 	return JSON.parse( JSON.stringify( object ) );
 }
 
-function usePushBlockStylesToUserStyles( { name, attributes, setAttributes } ) {
+function PushChangesToGlobalStylesControl( {
+	name,
+	attributes,
+	setAttributes,
+} ) {
+	const changes = useMemo(
+		() => getChangesToPush( name, attributes ),
+		[ name, attributes ]
+	);
+
 	const { user: userConfig, setUserConfig } =
 		useContext( GlobalStylesContext );
+
 	const { createSuccessNotice } = useDispatch( noticesStore );
 
-	return () => {
-		const { style: blockStyles = {} } = attributes;
-
-		let newBlockStyles = null;
-		let newUserConfig = null;
-		const changedKeys = [];
-
-		const supportedKeys = getSupportedGlobalStylesPanels( name );
-		for ( const key of supportedKeys ) {
-			const { value: valuePath } = STYLE_PROPERTY[ key ];
-			const presetAttributeKey = valuePath.join( '.' );
-			const presetAttributeValue =
-				attributes[
-					STYLE_PATH_TO_PRESET_BLOCK_ATTRIBUTE[ presetAttributeKey ]
-				];
-			const value = get( attributes.style, valuePath );
-
-			if ( ! value && ! presetAttributeValue ) {
-				continue;
-			}
-
-			newBlockStyles ??= cloneDeep( blockStyles );
-			set( newBlockStyles, valuePath, undefined );
-
-			const pushedValue = presetAttributeValue
-				? `var:preset|${ STYLE_PATH_TO_CSS_VAR_INFIX[ presetAttributeKey ] }|${ presetAttributeValue }`
-				: value;
-
-			newUserConfig ??= cloneDeep( userConfig );
-			set(
-				newUserConfig,
-				[ 'styles', 'blocks', name, ...valuePath ],
-				pushedValue
-			);
-
-			changedKeys.push( key );
+	const pushChanges = useCallback( () => {
+		if ( changes.length === 0 ) {
+			return;
 		}
 
-		if ( changedKeys.length ) {
-			// TODO: Make these two calls not create an undo level since we have
-			// our own Undo functionality in the toast below. In the longer term
-			// we should use the regular undo/redo functionality. Doing this
-			// requires us to be able to create a single undo level here instead
-			// of two.
-			setAttributes( { style: newBlockStyles } );
-			setUserConfig( () => newUserConfig );
+		const { style: blockStyles } = attributes;
 
-			createSuccessNotice(
-				sprintf(
-					// translators: %1$s: Title of the block e.g. 'Heading'. %2$s: List of style properties e.g. 'Color, Link Color, Font Size'.
-					__( 'Applied to all %1$s blocks: %2$s.' ),
-					getBlockType( name ).title,
-					changedKeys.map( capitalCase ).join( ', ' )
-				),
-				{
-					type: 'snackbar',
-					actions: [
-						{
-							label: __( 'Undo' ),
-							onClick() {
-								setAttributes( { style: blockStyles } );
-								setUserConfig( () => userConfig );
-							},
+		const newBlockStyles = cloneDeep( blockStyles );
+		const newUserConfig = cloneDeep( userConfig );
+
+		for ( const { path, value } of changes ) {
+			set( newBlockStyles, path, undefined );
+			set( newUserConfig, [ 'styles', 'blocks', name, ...path ], value );
+		}
+
+		setAttributes( { style: newBlockStyles } );
+		setUserConfig( () => newUserConfig );
+
+		createSuccessNotice(
+			sprintf(
+				// translators: %1$s: Title of the block e.g. 'Heading'. %2$s: List of style properties e.g. 'Color, Link Color, Font Size'.
+				__( 'Applied to all %1$s blocks: %2$s.' ),
+				getBlockType( name ).title,
+				changes.map( ( { key } ) => capitalCase( key ) ).join( ', ' )
+			),
+			{
+				type: 'snackbar',
+				actions: [
+					{
+						label: __( 'Undo' ),
+						onClick() {
+							setAttributes( { style: blockStyles } );
+							setUserConfig( () => userConfig );
 						},
-					],
-				}
-			);
-		}
-	};
+					},
+				],
+			}
+		);
+	}, [ changes, attributes, userConfig, name ] );
+
+	return (
+		<BaseControl
+			help={ __(
+				"Apply this block's styles to all blocks of this type. Note that only typography, spacing, dimensions, color and border styles will be applied."
+			) }
+		>
+			<BaseControl.VisualLabel>
+				{ __( 'Styles' ) }
+			</BaseControl.VisualLabel>
+			<Button
+				variant="primary"
+				disabled={ changes.length === 0 }
+				onClick={ pushChanges }
+			>
+				{ __( 'Push changes to Global Styles' ) }
+			</Button>
+		</BaseControl>
+	);
 }
 
 const withPushChangesToGlobalStyles = createHigherOrderComponent(
@@ -116,21 +130,7 @@ const withPushChangesToGlobalStyles = createHigherOrderComponent(
 			<>
 				<BlockEdit { ...props } />
 				<InspectorAdvancedControls>
-					<BaseControl
-						help={ __(
-							"Apply this block's styles to all blocks of this type. Note that only typography, spacing, dimensions, color and border styles will be applied."
-						) }
-					>
-						<BaseControl.VisualLabel>
-							{ __( 'Styles' ) }
-						</BaseControl.VisualLabel>
-						<Button
-							variant="primary"
-							onClick={ usePushBlockStylesToUserStyles( props ) }
-						>
-							{ __( 'Push changes to Global Styles' ) }
-						</Button>
-					</BaseControl>
+					<PushChangesToGlobalStylesControl { ...props } />
 				</InspectorAdvancedControls>
 			</>
 		)
