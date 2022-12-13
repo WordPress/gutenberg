7f46c1f5c04c5a5057c423464a02e30ef2903d86
ntsekouras <ntsekouras@outlook.com>
2022-12-13 11:38:59 +0200

    add animationDuration attribute and control

--- packages/block-editor/src/hooks/effects.js
+++ packages/block-editor/src/hooks/effects.js
@@ -3,12 +3,109 @@
  */
 import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import {
+	PanelBody,
+	__experimentalNumberControl as NumberControl,
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	__experimentalToggleGroupControlOptionIcon as ToggleGroupControlOptionIcon,
+} from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import {
+	arrowLeft,
+	arrowRight,
+	arrowUp,
+	close,
+	swatch,
+} from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
-import BlockEditorEffectsPanel from '../components/inspector-controls-tabs/effects-panel';
 import { InspectorControls } from '../components';
+import { store as blockEditorStore } from '../store';
+
+const effectsOptions = [
+	{
+		label: __( 'Slide from left' ),
+		value: 'slide-from-left',
+		icon: arrowRight,
+	},
+	{
+		label: __( 'Slide from right' ),
+		value: 'slide-from-right',
+		icon: arrowLeft,
+	},
+	{
+		label: __( 'Slide from bottom' ),
+		value: 'slide-from-bottom',
+		icon: arrowUp,
+	},
+	{
+		label: __( 'Fade in' ),
+		value: 'fade-in',
+		icon: swatch,
+	},
+	{
+		label: __( 'None' ),
+		value: undefined,
+		icon: close,
+	},
+];
+
+// TODO: attributes would be: `effect` with the data and `effectType`(entrance, exit, etc..).
+
+export default function BlockEditorEffectsPanel( { clientId } ) {
+	const attributes = useSelect(
+		( select ) => {
+			const { getBlockAttributes } = select( blockEditorStore );
+			return getBlockAttributes( clientId );
+		},
+		[ clientId ]
+	);
+	const { effect, effectDuration } = attributes;
+	const { updateBlockAttributes } = useDispatch( blockEditorStore );
+	return (
+		<PanelBody
+			className="block-editor-block-inspector__effects"
+			title={ __( 'Effects' ) }
+			initialOpen
+		>
+			<ToggleGroupControl
+				className="block-editor-hooks__effects-entrance-contorls"
+				label={ __( 'Entrance effect' ) }
+				value={ effect }
+				onChange={ ( value ) =>
+					updateBlockAttributes( clientId, { effect: value } )
+				}
+			>
+				{ effectsOptions.map( ( option, index ) => (
+					<ToggleGroupControlOptionIcon
+						key={ option.value || index }
+						{ ...option }
+						showTooltip
+					/>
+				) ) }
+			</ToggleGroupControl>
+			{ effect && (
+				<NumberControl
+					label={ __( 'Duration(seconds)' ) }
+					step={ 0.1 }
+					value={ effectDuration || '' }
+					onChange={ ( value ) =>
+						updateBlockAttributes( clientId, {
+							effectDuration: value,
+						} )
+					}
+					min={ 0.1 }
+					__unstableInputWidth="120px"
+					spinControls="custom"
+					__nextHasNoMarginBottom
+				/>
+			) }
+		</PanelBody>
+	);
+}
 
 /**
  * Filters registered block settings, extending attributes to include
@@ -27,73 +124,15 @@ function addAttributes( settings ) {
 			attribute: 'data-effect',
 			selector: '[data-effect]',
 		},
+		effectDuration: {
+			type: 'string',
+			source: 'attribute',
+			attribute: 'data-effect-duration',
+			selector: '[data-effect-duration]',
+		},
 	};
 	return settings;
 }
-
-/**
- * Override the default edit UI to include a new block inspector control for
- * assigning the effect value.
- *
- * @param {WPComponent} BlockEdit Original component.
- *
- * @return {WPComponent} Wrapped component.
- */
-export const withInspectorControl = createHigherOrderComponent(
-	( BlockEdit ) => {
-		return ( props ) => {
-			if ( props.isSelected ) {
-				return (
-					<>
-						<BlockEdit { ...props } />
-						<InspectorControls>
-							<BlockEditorEffectsPanel
-								clientId={ props.clientId }
-							/>
-						</InspectorControls>
-					</>
-				);
-			}
-			return <BlockEdit { ...props } />;
-		};
-	},
-	'withInspectorControl'
-);
-
-/**
- * Override props assigned to save component to inject the `data-effect`
- * attribute.
- *
- * @param {Object} extraProps Additional props applied to save element.
- * @param {Object} blockType  Block type.
- * @param {Object} attributes Current block attributes.
- *
- * @return {Object} Filtered props applied to save element.
- */
-export function addSaveProps( extraProps, blockType, attributes ) {
-	if ( attributes.effect ) {
-		extraProps[ 'data-effect' ] = attributes.effect;
-	}
-
-	return extraProps;
-}
-
-addFilter(
-	'blocks.registerBlockType',
-	'core/effects/addAttribute',
-	addAttributes
-);
-addFilter(
-	'editor.BlockEdit',
-	'core/editor/effects/with-inspector-control',
-	withInspectorControl
-);
-
-addFilter(
-	'blocks.getSaveContent.extraProps',
-	'core/effects/save-props',
-	addSaveProps
-);
 
 /**
  * Filters registered block settings to expand the block edit wrapper
@@ -115,6 +154,68 @@ function addEditProps( settings ) {
 
 	return settings;
 }
+
+/**
+ * Override the default edit UI to include a new block inspector control for
+ * assigning the effect value.
+ *
+ * @param {WPComponent} BlockEdit Original component.
+ *
+ * @return {WPComponent} Wrapped component.
+ */
+export const withInspectorControl = createHigherOrderComponent(
+	( BlockEdit ) => {
+		return ( props ) => {
+			if ( ! props.isSelected ) {
+				return <BlockEdit { ...props } />;
+			}
+			return (
+				<>
+					<BlockEdit { ...props } />
+					<InspectorControls>
+						<BlockEditorEffectsPanel clientId={ props.clientId } />
+					</InspectorControls>
+				</>
+			);
+		};
+	},
+	'withInspectorControl'
+);
+
+/**
+ * Override props assigned to save component to inject the `data-effect`
+ * attribute.
+ *
+ * @param {Object} extraProps Additional props applied to save element.
+ * @param {Object} blockType  Block type.
+ * @param {Object} attributes Current block attributes.
+ *
+ * @return {Object} Filtered props applied to save element.
+ */
+export function addSaveProps( extraProps, blockType, attributes ) {
+	return {
+		...extraProps,
+		'data-effect': attributes.effect,
+		'data-effect-duration': attributes.effectDuration,
+	};
+}
+
+addFilter(
+	'blocks.registerBlockType',
+	'core/effects/addAttribute',
+	addAttributes
+);
+addFilter(
+	'editor.BlockEdit',
+	'core/editor/effects/with-inspector-control',
+	withInspectorControl
+);
+
+addFilter(
+	'blocks.getSaveContent.extraProps',
+	'core/effects/save-props',
+	addSaveProps
+);
 
 addFilter(
 	'blocks.registerBlockType',
