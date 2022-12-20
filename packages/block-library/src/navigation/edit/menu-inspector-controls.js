8eb3fa0bdd96e7e2361ccba545d5938dbc57714d
Andrei Draganescu <github@andreidraganescu.info>
2022-12-20 13:06:37 +0000

    add a custom menu via context to off cavas editor blocks

--- packages/block-library/src/navigation/edit/menu-inspector-controls.js
+++ packages/block-library/src/navigation/edit/menu-inspector-controls.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { createBlock } from '@wordpress/blocks';
 import {
 	__experimentalOffCanvasEditor as OffCanvasEditor,
 	InspectorControls,
@@ -10,15 +11,85 @@ import {
 	PanelBody,
 	__experimentalHStack as HStack,
 	__experimentalHeading as Heading,
+	DropdownMenu,
+	MenuItem,
+	MenuGroup,
 } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
+import { moreVertical } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import ManageMenusButton from './manage-menus-button';
 import NavigationMenuSelector from './navigation-menu-selector';
+
+const POPOVER_PROPS = {
+	className: 'block-editor-block-settings-menu__popover',
+	position: 'bottom right',
+	variant: 'toolbar',
+};
+
+const LeafMoreMenu = ( props ) => {
+	const { clientId, block } = props;
+
+	const { insertBlock, replaceBlock, removeBlocks } =
+		useDispatch( blockEditorStore );
+
+	return (
+		<DropdownMenu
+			icon={ moreVertical }
+			label={ __( 'Options' ) }
+			className="block-editor-block-settings-menu"
+			popoverProps={ POPOVER_PROPS }
+			noIcons
+			{ ...props }
+		>
+			{ ( { onClose } ) => (
+				<MenuGroup>
+					<MenuItem
+						onClick={ () => {
+							const newLink = createBlock(
+								'core/navigation-link'
+							);
+							if ( block.name === 'core/navigation-submenu' ) {
+								const updateSelectionOnInsert = false;
+								insertBlock(
+									newLink,
+									block.innerBlocks.length,
+									clientId,
+									updateSelectionOnInsert
+								);
+							} else {
+								// Convert to a submenu if the block currently isn't one.
+								const newSubmenu = createBlock(
+									'core/navigation-submenu',
+									block.attributes,
+									block.innerBlocks
+										? [ ...block.innerBlocks, newLink ]
+										: [ newLink ]
+								);
+								replaceBlock( clientId, newSubmenu );
+							}
+							onClose();
+						} }
+					>
+						{ __( 'Add submenu item' ) }
+					</MenuItem>
+					<MenuItem
+						onClick={ () => {
+							removeBlocks( [ clientId ], false );
+							onClose();
+						} }
+					>
+						{ __( 'Remove item' ) }
+					</MenuItem>
+				</MenuGroup>
+			) }
+		</DropdownMenu>
+	);
+};
 
 const MenuInspectorControls = ( {
 	clientId,
@@ -88,6 +159,7 @@ const MenuInspectorControls = ( {
 								blocks={ clientIdsTree }
 								isExpanded={ true }
 								selectBlockInCanvas={ false }
+								LeafMoreMenu={ LeafMoreMenu }
 							/>
 						) }
 					</>
