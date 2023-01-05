921161ee91a45161a2558ed01acad6a9a5bac8f1
Alex Lende <alex@lende.xyz>
2023-01-05 10:10:40 -0600

    Inline custom hooks

--- packages/block-library/src/page-list/edit.js
+++ packages/block-library/src/page-list/edit.js
@@ -105,8 +105,44 @@ export default function PageListEdit( {
 	setAttributes,
 } ) {
 	const { parentPageID } = attributes;
-	const [ pages ] = useGetPages();
-	const { pagesByParentId, totalPages, hasResolvedPages } = usePageData();
+	const { records: pages, hasResolved: hasResolvedPages } = useEntityRecords(
+		'postType',
+		'page',
+		{
+			orderby: 'menu_order',
+			order: 'asc',
+			_fields: [ 'id', 'link', 'parent', 'title', 'menu_order' ],
+			per_page: -1,
+			context: 'view',
+		}
+	);
+	useMemo( () => {
+		// TODO: Once the REST API supports passing multiple values to
+		// 'orderby', this can be removed.
+		// https://core.trac.wordpress.org/ticket/39037
+
+		const sortedPages = [ ...( pages ?? [] ) ].sort( ( a, b ) => {
+			if ( a.menu_order === b.menu_order ) {
+				return a.title.rendered.localeCompare( b.title.rendered );
+			}
+			return a.menu_order - b.menu_order;
+		} );
+		const pagesByParentId = sortedPages.reduce( ( accumulator, page ) => {
+			const { parent } = page;
+			if ( accumulator.has( parent ) ) {
+				accumulator.get( parent ).push( page );
+			} else {
+				accumulator.set( parent, [ page ] );
+			}
+			return accumulator;
+		}, new Map() );
+
+		return {
+			pagesByParentId,
+			hasResolvedPages,
+			totalPages: pages?.length ?? null,
+		};
+	}, [ pages, hasResolvedPages ] );
 
 	const isNavigationChild = 'showSubmenuIcon' in context;
 	const allowConvertToLinks =
@@ -288,52 +324,4 @@ export default function PageListEdit( {
 			/>
 		</>
 	);
-}
-
-function useGetPages() {
-	const { records: pages, hasResolved: hasResolvedPages } = useEntityRecords(
-		'postType',
-		'page',
-		{
-			orderby: 'menu_order',
-			order: 'asc',
-			_fields: [ 'id', 'link', 'parent', 'title', 'menu_order' ],
-			per_page: -1,
-			context: 'view',
-		}
-	);
-
-	return [ pages, hasResolvedPages ];
-}
-
-function usePageData( pageId = 0 ) {
-	const [ pages, hasResolvedPages ] = useGetPages();
-
-	return useMemo( () => {
-		// TODO: Once the REST API supports passing multiple values to
-		// 'orderby', this can be removed.
-		// https://core.trac.wordpress.org/ticket/39037
-
-		const sortedPages = [ ...( pages ?? [] ) ].sort( ( a, b ) => {
-			if ( a.menu_order === b.menu_order ) {
-				return a.title.rendered.localeCompare( b.title.rendered );
-			}
-			return a.menu_order - b.menu_order;
-		} );
-		const pagesByParentId = sortedPages.reduce( ( accumulator, page ) => {
-			const { parent } = page;
-			if ( accumulator.has( parent ) ) {
-				accumulator.get( parent ).push( page );
-			} else {
-				accumulator.set( parent, [ page ] );
-			}
-			return accumulator;
-		}, new Map() );
-
-		return {
-			pagesByParentId,
-			hasResolvedPages,
-			totalPages: pages?.length ?? null,
-		};
-	}, [ pageId, pages, hasResolvedPages ] );
 }
