ba71d017684f307fe1a5b1c5edef97cf9f88c8e5
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-01-06 23:27:09 +0900

    Fix unit test and utils

--- packages/block-library/src/table/utils.js
+++ packages/block-library/src/table/utils.js
@@ -3,12 +3,16 @@
  * Returns undefined if the parameter is not a positive number
  * or the default value (1) for rowspan/colspan.
  *
- * @param {number|undefined} rowColSpan normalized value.
+ * @param {number|undefined} rowColSpan rowspan/colspan value.
+ *
+ * @return {string|undefined} normalized rowspan/colspan value.
  */
 export function normalizeRowColSpan( rowColSpan ) {
 	const parsedValue = parseInt( rowColSpan, 10 );
 	if ( ! Number.isInteger( parsedValue ) ) {
 		return undefined;
 	}
-	return parsedValue < 0 || parsedValue === 1 ? undefined : parsedValue;
+	return parsedValue < 0 || parsedValue === 1
+		? undefined
+		: parsedValue.toString();
 }
