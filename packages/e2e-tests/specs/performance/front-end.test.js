1e287767a3aab794bf5b5cd5169361aad4b9c603
André <583546+oandregal@users.noreply.github.com>
2023-01-10 18:56:51 +0100

    Add skeleton for front-end tests

---
+++ packages/e2e-tests/specs/performance/front-end.test.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import { basename, join } from 'path';
+import { writeFileSync } from 'fs';
+
+/**
+ * WordPress dependencies
+ */
+import { createURL } from '@wordpress/e2e-test-utils';
+
+describe( 'Front End Performance', () => {
+	const results = {
+		TTFB: [],
+	};
+
+	afterAll( async () => {
+		const resultsFilename = basename( __filename, '.js' ) + '.results.json';
+		writeFileSync(
+			join( __dirname, resultsFilename ),
+			JSON.stringify( results, null, 2 )
+		);
+	} );
+
+	it( 'TTFB', async () => {
+		let i = 5;
+		while ( i-- ) {
+			await page.goto( createURL( '/' ) );
+			const navigationTimingJson = await page.evaluate( () =>
+				JSON.stringify( performance.getEntriesByType( 'navigation' ) )
+			);
+			const [ navigationTiming ] = JSON.parse( navigationTimingJson );
+			results.TTFB.push(
+				navigationTiming.responseStart - navigationTiming.requestStart
+			);
+		}
+	} );
+} );
