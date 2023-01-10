6a84534a16faeb214bcb2db7ff702e64c4c6d73a
Sunil Prajapati <sdprajapati999@gmail.com>
2023-01-10 12:18:24 +0530

    Addressed feedbacks

--- phpunit/blocks/render-block-post-title-test.php
+++ phpunit/blocks/render-block-post-title-test.php
@@ -4,6 +4,7 @@
  *
  * @package WordPress
  * @subpackage Blocks
+ * @covers ::gutenberg_render_block_core_post_title
  */
 
 /**
@@ -11,7 +12,7 @@
  *
  * @group blocks
  */
-class Tests_Blocks_Render_Post_Title extends WP_UnitTestCase {
+class Tests_Blocks_GutenbergRenderBlockCorePostTitle extends WP_UnitTestCase {
 	/**
 	 * Post object.
 	 *
@@ -93,31 +94,86 @@ class Tests_Blocks_Render_Post_Title extends WP_UnitTestCase {
 		$block->context  = array( 'postId' => self::$post->ID );
 
 		$rendered = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
-		$this->assertNotNull( $rendered );
-		$this->assertStringContainsString( 'Post title block Unit Test', $rendered );
-		$this->assertStringContainsString( '</p', $rendered );
-		$this->assertStringContainsString( '</p>', $rendered );
-		$this->assertStringNotContainsString( get_permalink( self::$post->ID ), $rendered );
-		$this->assertStringNotContainsString( '<a href=', $rendered );
-		$this->assertStringNotContainsString( 'has-text-align', $rendered );
+		$this->assertNotNull(
+			$rendered,
+			'Passed $rendered is null.'
+		);
+		$this->assertStringContainsString(
+			'Post title block Unit Test',
+			$rendered,
+			'Passed $rendered does not contain post title string.'
+		);
+		$this->assertStringContainsString(
+			'</p',
+			$rendered,
+			'Passed $rendered does not contain html P tag.'
+		);
+		$this->assertStringContainsString(
+			'</p>',
+			$rendered,
+			'Passed $rendered does not contain html P tag.'
+		);
+		$this->assertStringNotContainsString(
+			get_permalink( self::$post->ID ),
+			$rendered,
+			'Passed $rendered contain post link.'
+		);
+		$this->assertStringNotContainsString(
+			'<a href=',
+			$rendered,
+			'Passed $rendered contain html anchor tag.'
+		);
+		$this->assertStringNotContainsString(
+			'has-text-align',
+			$rendered,
+			'Passed $rendered contain expected string.'
+		);
 
 		self::$attributes['level'] = '1';
 		$rendered                  = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
-		$this->assertStringContainsString( '<h1', $rendered );
-		$this->assertStringContainsString( '</h1>', $rendered );
+		$this->assertStringContainsString(
+			'<h1',
+			$rendered,
+			'Passed $rendered does not contain HTML heading tag.'
+		);
+		$this->assertStringContainsString(
+			'</h1>',
+			$rendered,
+			'Passed $rendered does not contain HTML heading tag.'
+		);
 
 		self::$attributes['textAlign'] = 'left';
 		$rendered                      = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
-		$this->assertStringContainsString( 'has-text-align-left', $rendered );
+		$this->assertStringContainsString(
+			'has-text-align-left',
+			$rendered,
+			'Passed $rendered does not contain expected text alignment class.'
+		);
 
 		self::$attributes['isLink']     = true;
 		self::$attributes['linkTarget'] = '_blank';
 		self::$attributes['rel']        = 'no-relative';
 		$rendered                       = gutenberg_render_block_core_post_title( self::$attributes, '', $block );
-		$this->assertStringContainsString( get_permalink( self::$post->ID ), $rendered );
-		$this->assertStringContainsString( '<a href=', $rendered );
-		$this->assertStringContainsString( '_blank', $rendered );
-		$this->assertStringContainsString( 'no-relative', $rendered );
+		$this->assertStringContainsString(
+			get_permalink( self::$post->ID ),
+			$rendered,
+			'Passed $rendered does not contain post link.'
+		);
+		$this->assertStringContainsString(
+			'<a href=',
+			$rendered,
+			'Passed $rendered does not contain html anchor tag.'
+		);
+		$this->assertStringContainsString(
+			'_blank',
+			$rendered,
+			'Passed $rendered does not contain expected string.'
+		);
+		$this->assertStringContainsString(
+			'no-relative',
+			$rendered,
+			'Passed $rendered does not contain expected string.'
+		);
 
 	}
 }
