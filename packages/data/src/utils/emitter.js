4418c8288debc3c07d6d9b4cdc4d6d49ac1db90d
Dennis Snell <dennis.snell@automattic.com>
2023-01-11 17:35:09 -0800

    Perf: Avoid cloning listeners in data event emitter
    In the core event emitter class for the `@data` module we have a note
    explaining that we clone the set of listeners to avoid calling newly
    created listeners during the `notifyListeners` loop.
    
    In this patch we use a special token inserted at the end of the listeners
    set that indicates when we've reached the last previously-existing listener
    while iterating. This is a result of `Set` iteration following insertion
    order.
    
    By adding, watching for, and removing this token we can safely iterate
    the set without cloning on every call, which can reduce pressure on the
    garbage collector and lead to lower memory use and reduced GC overhead.
    
    The results of this change may or may not be easily measurable, but if it's
    found to be a positive change regardless it might still be worth introducing
    since this code is in the critical path of all state updates, and it runs
    following every key press.

--- packages/data/src/utils/emitter.js
+++ packages/data/src/utils/emitter.js
@@ -7,11 +7,43 @@ export function createEmitter() {
 	let isPaused = false;
 	let isPending = false;
 	const listeners = new Set();
-	const notifyListeners = () =>
-		// We use Array.from to clone the listeners Set
-		// This ensures that we don't run a listener
-		// that was added as a response to another listener.
-		Array.from( listeners ).forEach( ( listener ) => listener() );
+	const deletedListeners = new Set();
+	const existingEndToken = () => {};
+
+	const notifyListeners = () => {
+		// Drop everything that's already been removed from the queue.
+		for ( const deletedListener of deletedListeners ) {
+			listeners.delete( deletedListener );
+		}
+		deletedListeners.clear();
+
+		// Some listeners might add new listeners while we're
+		// iterating; we want to avoid calling them now though.
+		// Since Set iteration occurs in insertion order we can
+		// add a unique symbol to indicate the last existing
+		// listener and stop iterating once we find it. All
+		// listeners added after the symbol _must_ have been
+		// added while iterating, and we can skip them until
+		// the next time this is called.
+		// Add this, but make sure it's the last item in the set.
+		listeners.delete( existingEndToken );
+		listeners.add( existingEndToken );
+		for ( const listener of listeners ) {
+			if ( existingEndToken === listener ) {
+				break;
+			}
+
+			listener();
+
+			// Listeners deleted on this go-around are run one
+			// final time and then removed from future queues.
+			if ( deletedListeners.has( listener ) ) {
+				deletedListeners.delete( listener );
+				listeners.delete( listener );
+			}
+		}
+		listeners.delete( existingEndToken );
+	};
 
 	return {
 		get isPaused() {
@@ -20,7 +52,11 @@ export function createEmitter() {
 
 		subscribe( listener ) {
 			listeners.add( listener );
-			return () => listeners.delete( listener );
+
+			// Because we call listeners that have been removed during
+			// the process of notifying existing listeners, we enqueue
+			// them for removal instead of directly removing them.
+			return () => deletedListeners.add( listener );
 		},
 
 		pause() {
