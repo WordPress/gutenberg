e3f89fd1c69f7ab1da5e115ef35c1bfa46ceb345
Bernie Reiter <ockham@raz.or.at>
2023-01-12 14:24:25 +0100

    Test set_content_inside_balanced_tags followed by set_attribute works

--- phpunit/html/wp-html-processor-test.php
+++ phpunit/html/wp-html-processor-test.php
@@ -257,4 +257,13 @@ HTML
 		$tags->set_content_inside_balanced_tags( 'This is the even newer section content.' );
 		$this->assertSame( '<div>outside</div><section>This is the even newer section content.</section>', $tags->get_updated_html() );
 	}
+
+	public function test_set_content_inside_balanced_tags_followed_by_set_attribute_works() {
+		$tags = new WP_HTML_Processor( self::HTML );
+
+		$tags->next_tag( 'section' );
+		$tags->set_content_inside_balanced_tags( 'This is the new section content.' );
+		$tags->set_attribute( 'id', 'thesection' );
+		$this->assertSame( '<div>outside</div><section id="thesection">This is the new section content.</section>', $tags->get_updated_html() );
+	}
 }
