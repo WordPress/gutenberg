01e85c62362114370e9d4ecb423a0a12c43e49aa
tellthemachines <isabel@tellthemachines.com>
2023-01-12 15:17:15 +1100

    Try removing default align-items styles from flex layout

--- packages/block-editor/src/layouts/flex.js
+++ packages/block-editor/src/layouts/flex.js
@@ -138,8 +138,7 @@ export default {
 			: 'wrap';
 		const verticalAlignment =
 			verticalAlignmentMap[ layout.verticalAlignment ];
-		const alignItems =
-			alignItemsMap[ layout.justifyContent ] || alignItemsMap.left;
+		const alignItems = alignItemsMap[ layout.justifyContent ];
 
 		let output = '';
 		const rules = [];
@@ -160,7 +159,9 @@ export default {
 				rules.push( `justify-content: ${ verticalAlignment }` );
 			}
 			rules.push( 'flex-direction: column' );
-			rules.push( `align-items: ${ alignItems }` );
+			if ( alignItems ) {
+				rules.push( `align-items: ${ alignItems }` );
+			}
 		}
 
 		if ( rules.length ) {
