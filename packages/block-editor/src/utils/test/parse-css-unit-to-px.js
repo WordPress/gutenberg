2d44f063833189b81e2208a7036e2a094a1f1f54
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2023-01-13 11:45:03 +0100

    [Mobile] - RichText - Parse CSS values and avoid setting undefined ones (#47080)
    * parseCssUnitToPx - Fix issue with decimals in math expressions
    
    * Mobile - RichText - Parse CSS values from line height values and avoid undefined or NaN values being sent to the native TextInput

--- packages/block-editor/src/utils/test/parse-css-unit-to-px.js
+++ packages/block-editor/src/utils/test/parse-css-unit-to-px.js
@@ -115,6 +115,7 @@ describe( 'getPxFromCssUnit', () => {
 			[ 'abc', null ],
 			[ 'console.log("howdy"); + 10px', null ],
 			[ 'calc(12vw * 10px', null ], // Missing closing bracket.
+			[ 'calc( 1em + 0.875rem )', '30px' ], // Decimals
 		];
 
 		test.each( testData )( 'getPxFromCssUnit( %s )', ( unit, expected ) => {
