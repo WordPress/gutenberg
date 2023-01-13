e1d964a2e3490d7173bf2e6e45119ca2d58ace3b
Marco Ciampini <marco.ciampo@gmail.com>
2023-01-13 17:49:59 +0100

    Add "With Children" example

--- packages/components/src/dropdown-menu/stories/index.js
+++ packages/components/src/dropdown-menu/stories/index.js
@@ -2,21 +2,30 @@
  * Internal dependencies
  */
 import DropdownMenu from '../';
+import { MenuGroup, MenuItem } from '../../';
 
 /**
  * WordPress dependencies
  */
-import { menu, arrowUp, arrowDown, chevronDown } from '@wordpress/icons';
+import {
+	menu,
+	arrowUp,
+	arrowDown,
+	chevronDown,
+	more,
+	trash,
+} from '@wordpress/icons';
 
 export default {
 	title: 'Components/DropdownMenu',
 	component: DropdownMenu,
 	argTypes: {
 		className: { control: { type: 'text' } },
+		children: { control: { type: null } },
 		disableOpenOnArrowDown: { control: { type: 'boolean' } },
 		icon: {
-			options: [ 'menu', 'chevronDown' ],
-			mapping: { menu, chevronDown },
+			options: [ 'menu', 'chevronDown', 'more' ],
+			mapping: { menu, chevronDown, more },
 			control: { type: 'select' },
 		},
 		menuProps: {
@@ -57,4 +66,27 @@ Default.args = {
 	toggleProps: {
 		showTooltip: true,
 	},
+};
+
+export const WithChildren = Template.bind( {} );
+WithChildren.args = {
+	label: 'Select a direction.',
+	icon: more,
+	children: ( { onClose } ) => (
+		<>
+			<MenuGroup>
+				<MenuItem icon={ arrowUp } onClick={ onClose }>
+					Move Up
+				</MenuItem>
+				<MenuItem icon={ arrowDown } onClick={ onClose }>
+					Move Down
+				</MenuItem>
+			</MenuGroup>
+			<MenuGroup>
+				<MenuItem icon={ trash } onClick={ onClose }>
+					Remove
+				</MenuItem>
+			</MenuGroup>
+		</>
+	),
 };
