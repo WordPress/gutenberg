67e768a84061ded8d86f3b47cbfc501ff5ea5412
Markus Dobmann <m.do@posteo.net>
2023-01-16 15:41:32 +0100

    create stories for `MenuItemsChoice` /wip

--- packages/components/src/menu-items-choice/index.tsx
+++ packages/components/src/menu-items-choice/index.tsx
@@ -17,30 +17,34 @@ function MenuItemsChoice( {
 	onSelect,
 	value,
 }: MenuItemsChoiceProps ) {
-	return choices.map( ( item ) => {
-		const isSelected = value === item.value;
-		return (
-			<MenuItem
-				key={ item.value }
-				role="menuitemradio"
-				icon={ isSelected && check }
-				info={ item.info }
-				isSelected={ isSelected }
-				shortcut={ item.shortcut }
-				className="components-menu-items-choice"
-				onClick={ () => {
-					if ( ! isSelected ) {
-						onSelect( item.value );
-					}
-				} }
-				onMouseEnter={ () => onHover( item.value ) }
-				onMouseLeave={ () => onHover() }
-				aria-label={ item[ 'aria-label' ] }
-			>
-				{ item.label }
-			</MenuItem>
-		);
-	} );
+	return (
+		<>
+			{ choices.map( ( item ) => {
+				const isSelected = value === item.value;
+				return (
+					<MenuItem
+						key={ item.value }
+						role="menuitemradio"
+						icon={ isSelected && check }
+						info={ item.info }
+						isSelected={ isSelected }
+						shortcut={ item.shortcut }
+						className="components-menu-items-choice"
+						onClick={ () => {
+							if ( ! isSelected ) {
+								onSelect( item.value );
+							}
+						} }
+						onMouseEnter={ () => onHover( item.value ) }
+						onMouseLeave={ () => onHover() }
+						aria-label={ item[ 'aria-label' ] }
+					>
+						{ item.label }
+					</MenuItem>
+				);
+			} ) }
+		</>
+	);
 }
 
 export default MenuItemsChoice;
---
+++ packages/components/src/menu-items-choice/stories/index.tsx
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import MenuItemsChoice from '..';
+import MenuGroup from '../../menu-group';
+
+const meta: ComponentMeta< typeof MenuItemsChoice > = {
+	component: MenuItemsChoice,
+	title: 'Components/MenuItemsChoice',
+	argTypes: {},
+	parameters: {
+		controls: {
+			expanded: true,
+		},
+		docs: { source: { state: 'open' } },
+	},
+};
+export default meta;
+
+const Template: ComponentStory< typeof MenuItemsChoice > = ( args ) => {
+	return (
+		<MenuGroup label="Editor">
+			<MenuItemsChoice { ...args } />
+		</MenuGroup>
+	);
+};
+
+export const Default: ComponentStory< typeof MenuItemsChoice > = Template.bind(
+	{}
+);
+Default.args = {
+	choices: [
+		{
+			value: 'arbitrary-choice-1',
+			label: 'Arbitrary Label #1',
+			info: 'Arbitrary Explanatory 1',
+		},
+		{
+			value: 'arbitrary-choice-2',
+			label: 'Arbitrary Label #2',
+			info: 'Arbitrary Explanatory 2',
+		},
+		{
+			value: 'arbitrary-choice-3',
+			label: 'Arbitrary Label #3',
+			info: 'Arbitrary Explanatory 3',
+		},
+	],
+	value: 'arbitrary-choice-1',
+};
