eb5171962c51c5c56514b9dab6b23876a094e560
George Mamadashvili <georgemamadashvili@gmail.com>
2023-01-16 18:42:08 +0400

    Global Styles: Don't remove Custom CSS for users with the correct caps (#47062)
    * Global Styles: Don't remove Custom CSS for users with the correct caps
    
    * Try fixing core kses_init_filters override
    
    * Add comment about priority change
    
    * Explicitly grant 'edit_css' capabilities
    
    * Use grant_super_admin() for multisite
    
    * Use dataProvider
    
    * Use assertSame*
    
    Co-authored-by: hellofromtonya <tonya.mork@automattic.com>

--- lib/class-wp-theme-json-gutenberg.php
+++ lib/class-wp-theme-json-gutenberg.php
@@ -2818,7 +2818,12 @@ class WP_Theme_JSON_Gutenberg {
 				continue;
 			}
 
-			$output = static::remove_insecure_styles( $input );
+			// The global styles custom CSS is not sanitized, but can only be edited by users with 'edit_css' capability.
+			if ( isset( $input['css'] ) && current_user_can( 'edit_css' ) ) {
+				$output = $input;
+			} else {
+				$output = static::remove_insecure_styles( $input );
+			}
 
 			/*
 			 * Get a reference to element name from path.
