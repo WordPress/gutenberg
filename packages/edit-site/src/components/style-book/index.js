fdc180a53110b48e98be3460bb8be1b65152ea1e
Robert Anderson <robert@noisysocks.com>
2023-01-20 19:28:29 +1100

    Stabilise BlockPreview props (#47231)
    * Stabilise BlockPreview props
    
    * Remove unnecessary __experimentalStyles migration
    
    * Add version to deprecated() calls
    
    * Default padding to 0, allow additionalStyles to override default styling

--- packages/edit-site/src/components/style-book/index.js
+++ packages/edit-site/src/components/style-book/index.js
@@ -176,7 +176,7 @@ const Example = memo( ( { title, blocks, isSelected, onClick } ) => (
 			<BlockPreview
 				blocks={ blocks }
 				viewportWidth={ 0 }
-				__experimentalStyles={ [
+				additionalStyles={ [
 					{
 						css:
 							'.wp-block:first-child { margin-top: 0; }' +
