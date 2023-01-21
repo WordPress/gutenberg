6bf975b13796c3707bc0c97a901f78b20e8ccbe5
Dave Smith <getdavemail@gmail.com>
2023-01-21 10:17:24 +0800

    Hide action buttons when not in editing mode

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -274,6 +274,7 @@ function LinkControl( {
 	// See https://github.com/WordPress/gutenberg/pull/33849/#issuecomment-932194927.
 	const showTextControl = hasLinkValue && hasTextControl;
 
+	const isEditing = ( isEditingLink || ! value ) && ! isCreatingPage;
 	return (
 		<div
 			tabIndex={ -1 }
@@ -286,7 +287,7 @@ function LinkControl( {
 				</div>
 			) }
 
-			{ ( isEditingLink || ! value ) && ! isCreatingPage && (
+			{ isEditing && (
 				<>
 					<div
 						className={ classnames( {
@@ -360,19 +361,21 @@ function LinkControl( {
 					</div>
 				) }
 
-				<div className="block-editor-link-control__search-actions">
-					<Button
-						variant="primary"
-						onClick={ handleSubmit }
-						className="xblock-editor-link-control__search-submit"
-						disabled={ currentInputIsEmpty } // Disallow submitting empty values.
-					>
-						{ __( 'Apply' ) }
-					</Button>
-					<Button variant="tertiary" onClick={ handleCancel }>
-						{ __( 'Cancel' ) }
-					</Button>
-				</div>
+				{ isEditing && (
+					<div className="block-editor-link-control__search-actions">
+						<Button
+							variant="primary"
+							onClick={ handleSubmit }
+							className="xblock-editor-link-control__search-submit"
+							disabled={ currentInputIsEmpty } // Disallow submitting empty values.
+						>
+							{ __( 'Apply' ) }
+						</Button>
+						<Button variant="tertiary" onClick={ handleCancel }>
+							{ __( 'Cancel' ) }
+						</Button>
+					</div>
+				) }
 			</div>
 
 			{ renderControlBottom && renderControlBottom() }
