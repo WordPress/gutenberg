85573d35e321f9b39f7409a22cb80b895e4f4b65
hbhalodia <hit.bhalodia@rtcamp.com>
2023-01-25 14:38:05 +0530

    Add option to align list items with list markers

--- packages/block-library/src/list/save.js
+++ packages/block-library/src/list/save.js
@@ -1,13 +1,26 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { ordered, type, reversed, start } = attributes;
+	const { align, ordered, position, type, reversed, start } = attributes;
+	const className = classnames( {
+		[ `has-text-align-${ align }` ]: align,
+		[ `list-style-position-${
+			position === false ? 'outside' : 'inside'
+		}` ]: position,
+	} );
 	const TagName = ordered ? 'ol' : 'ul';
 	return (
-		<TagName { ...useBlockProps.save( { type, reversed, start } ) }>
+		<TagName
+			{ ...useBlockProps.save( { className, type, reversed, start } ) }
+		>
 			<InnerBlocks.Content />
 		</TagName>
 	);
