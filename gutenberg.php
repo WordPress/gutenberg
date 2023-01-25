6a04341f0f580b92c27ea8c01e6b18b299bd3df3
annezazu <annezazu@gmail.com>
2023-01-25 12:26:45 -0800

    Quick update to the Gutenberg plugin description
    Addresses https://github.com/WordPress/gutenberg/issues/32544 by offering a slight update to the plugin description to match more of the current efforts underway.

--- gutenberg.php
+++ gutenberg.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gutenberg
  * Plugin URI: https://github.com/WordPress/gutenberg
- * Description: Printing since 1440. This is the development plugin for the new block editor in core.
+ * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.0
  * Requires PHP: 5.6
  * Version: 15.0.1
