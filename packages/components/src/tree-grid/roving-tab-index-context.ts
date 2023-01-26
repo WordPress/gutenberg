3ff85faad328e4e08be231028cf98c12c2f30b25
Lena Morita <lena@jaguchi.com>
2023-01-27 00:32:35 +0900

    Add types for roving tab index

--- packages/components/src/tree-grid/roving-tab-index-context.ts
+++ packages/components/src/tree-grid/roving-tab-index-context.ts
@@ -3,7 +3,12 @@
  */
 import { createContext, useContext } from '@wordpress/element';
 
-const RovingTabIndexContext = createContext();
+const RovingTabIndexContext = createContext< {
+	lastFocusedElement?: Element | null;
+	setLastFocusedElement?: React.Dispatch<
+		React.SetStateAction< Element | null >
+	>;
+} >( {} );
 export const useRovingTabIndexContext = () =>
 	useContext( RovingTabIndexContext );
 export const RovingTabIndexProvider = RovingTabIndexContext.Provider;
--- packages/components/src/tree-grid/roving-tab-index.tsx
+++ packages/components/src/tree-grid/roving-tab-index.tsx
@@ -12,11 +12,12 @@ import { RovingTabIndexProvider } from './roving-tab-index-context';
  * Provider for adding roving tab index behaviors to tree grid structures.
  *
  * @see https://github.com/WordPress/gutenberg/blob/HEAD/packages/components/src/tree-grid/README.md
- *
- * @param {Object}    props          Component props.
- * @param {WPElement} props.children Children to be rendered
  */
-export default function RovingTabIndex( { children } ) {
+export default function RovingTabIndex( {
+	children,
+}: {
+	children: React.ReactNode;
+} ) {
 	const [ lastFocusedElement, setLastFocusedElement ] = useState();
 
 	// Use `useMemo` to avoid creation of a new object for the providerValue
