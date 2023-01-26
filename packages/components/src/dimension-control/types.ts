a52be55c8d209c81d81b035ccb56ee580cdfb2ef
Markus Dobmann <m.do@posteo.net>
2023-01-26 16:21:32 +0100

    adjust type description wording

--- packages/components/src/dimension-control/types.ts
+++ packages/components/src/dimension-control/types.ts
@@ -5,38 +5,38 @@ import type { IconType } from '../icon';
 
 export type Size = {
 	/**
-	 * Human-readable name of the size
+	 * Human-readable name of the size.
 	 */
 	name: string;
 	/**
-	 * Short unique identifying name of the size
+	 * Short unique identifying name of the size.
 	 */
 	slug: string;
 };
 
 export type DimensionControlProps< IconProps = unknown > = {
 	/**
-	 * Label for the control
+	 * Label for the control.
 	 */
 	label: string;
 	/**
-	 * An array of sizes to choose from
+	 * An array of sizes to choose from.
 	 */
 	sizes: Size[];
 	/**
-	 * Optional icon rendered in front on the label
+	 * Optional icon rendered in front on the label.
 	 */
 	icon?: IconType< IconProps >;
 	/**
-	 * Used to externally control the current value of the control
+	 * Used to externally control the current value of the control.
 	 */
 	value?: string;
 	/**
-	 *		Function called with the control's internal state changes. The `value` property is equal to a given size slug
+	 * Function called with the control's internal state changes. The `value` property is equal to a given size slug.
 	 */
 	onChange?: ( value?: string ) => void;
 	/**
-	 * CSS class applied to `SelectControl`
+	 * CSS class applied to `SelectControl`.
 	 */
 	className?: string;
 };
