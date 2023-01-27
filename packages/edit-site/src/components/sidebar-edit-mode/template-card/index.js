df2bbe4a3bf8c6468f12412b9b286395ab33fd3f
André <583546+oandregal@users.noreply.github.com>
2023-01-27 17:27:12 +0100

    List patterns in the template inspector

--- packages/edit-site/src/components/sidebar-edit-mode/template-card/index.js
+++ packages/edit-site/src/components/sidebar-edit-mode/template-card/index.js
@@ -6,6 +6,11 @@ import { Icon } from '@wordpress/components';
 import { store as editorStore } from '@wordpress/editor';
 import { store as coreStore } from '@wordpress/core-data';
 import { decodeEntities } from '@wordpress/html-entities';
+import { __, sprintf } from '@wordpress/i18n';
+import {
+	store as blockEditorStore,
+	__experimentalBlockPatternsList as BlockPatternsList,
+} from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -14,10 +19,37 @@ import { store as editSiteStore } from '../../../store';
 import TemplateActions from './template-actions';
 import TemplateAreas from './template-areas';
 
+const MaybeBlockPatterns = ( { template, blockPatterns, title } ) => {
+	if ( template.type !== 'wp_template_part' || blockPatterns?.length === 0 ) {
+		return null;
+	}
+
+	return (
+		<>
+			<p>
+				{ sprintf(
+					// translators: %s: Type of template part (i.e. header, footer, etc.)
+					__(
+						'Choose a pre-designed pattern to switch up the look and feel of your %s '
+					),
+					title.toLowerCase()
+				) }
+			</p>
+			<BlockPatternsList
+				blockPatterns={ blockPatterns }
+				shownPatterns={ blockPatterns }
+				onClickPattern={ ( pattern ) => {
+					console.log( 'pattern clicked ', pattern );
+				} }
+			/>
+		</>
+	);
+};
 export default function TemplateCard() {
 	const {
 		info: { title, description, icon },
 		template,
+		blockPatterns,
 	} = useSelect( ( select ) => {
 		const { getEditedPostType, getEditedPostId } = select( editSiteStore );
 		const { getEditedEntityRecord } = select( coreStore );
@@ -30,7 +62,13 @@ export default function TemplateCard() {
 
 		const info = record ? getTemplateInfo( record ) : {};
 
-		return { info, template: record };
+		const patterns = record?.slug
+			? select( blockEditorStore ).getPatternsByBlockTypes(
+					`core/template-part/${ record.slug }`
+			  )
+			: [];
+
+		return { info, template: record, blockPatterns: patterns };
 	}, [] );
 
 	if ( ! title && ! description ) {
@@ -51,6 +89,11 @@ export default function TemplateCard() {
 					{ decodeEntities( description ) }
 				</div>
 				<TemplateAreas />
+				<MaybeBlockPatterns
+					template={ template }
+					blockPatterns={ blockPatterns }
+					title={ title }
+				/>
 			</div>
 		</div>
 	);
