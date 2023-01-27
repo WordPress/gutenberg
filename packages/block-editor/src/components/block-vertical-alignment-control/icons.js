cfefdcdcb45244f90ccbfaad58f3c5a086cc5efd
tellthemachines <isabel@tellthemachines.com>
2023-01-27 16:08:58 +1100

    Remove todo comment.

--- packages/block-editor/src/components/block-vertical-alignment-control/icons.js
+++ packages/block-editor/src/components/block-vertical-alignment-control/icons.js
@@ -21,7 +21,6 @@ export const alignTop = (
 	</SVG>
 );
 
-// Todo: Replace with the real icons.
 export const alignStretch = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 		<Path d="M4 4L20 4L20 5.5L4 5.5L4 4ZM10 7L14 7L14 17L10 17L10 7ZM20 18.5L4 18.5L4 20L20 20L20 18.5Z" />
