4419a760b4dbfe4aea2451989baeaf95f1b76d50
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-01-27 14:18:00 +0100

    Update openDocumentSettingsSidebar to wait until button is enabled

--- packages/e2e-test-utils-playwright/src/editor/open-document-settings-sidebar.ts
+++ packages/e2e-test-utils-playwright/src/editor/open-document-settings-sidebar.ts
@@ -5,14 +5,18 @@ import type { Editor } from './index';
 import { expect } from '../test';
 
 /**
- * Clicks on the button in the header which opens Document Settings sidebar when it is closed.
+ * Clicks on the button in the header which opens Document Settings sidebar when
+ * it is closed.
  *
  * @param {Editor} this
  */
 export async function openDocumentSettingsSidebar( this: Editor ) {
-	const editorSettingsButton = this.page.locator(
-		'role=region[name="Editor top bar"i] >> role=button[name="Settings"i]'
-	);
+	const editorSettingsButton = this.page
+		.getByRole( 'region', { name: 'Editor top bar' } )
+		.getByRole( 'button', {
+			name: 'Settings',
+			disabled: false,
+		} );
 
 	const isEditorSettingsOpened =
 		( await editorSettingsButton.getAttribute( 'aria-expanded' ) ) ===
@@ -22,9 +26,9 @@ export async function openDocumentSettingsSidebar( this: Editor ) {
 		await editorSettingsButton.click();
 
 		await expect(
-			this.page.locator(
-				'role=region[name="Editor settings"i] >> role=button[name^="Close settings"i]'
-			)
+			this.page
+				.getByRole( 'region', { name: 'Editor settings' } )
+				.getByRole( 'button', { name: 'Close settings' } )
 		).toBeVisible();
 	}
 }
