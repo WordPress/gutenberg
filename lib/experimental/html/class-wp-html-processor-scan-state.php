b4c54a2e80c38112c9a07b8b53d9b0d333c7dea5
Bernie Reiter <ockham@raz.or.at>
2023-01-31 11:34:56 +0100

    Add PHPDoc for WP_HTML_Processor_Scan_State methods

--- lib/experimental/html/class-wp-html-processor-scan-state.php
+++ lib/experimental/html/class-wp-html-processor-scan-state.php
@@ -18,10 +18,32 @@
  * @see WP_HTML_Processor
  */
 class WP_HTML_Processor_Scan_State {
-	public $budget      = 1000;
-	public $open_tags   = array();
+	/**
+	 * The maximum number of tags we'll traverse in search of a matching closing tag.
+	 *
+	 * @var integer
+	 */
+	public $budget = 1000;
+
+	/**
+	 * A stack of the opening tags that we have visited.
+	 *
+	 * @var string[]
+	 */
+	public $open_tags = array();
+
+	/**
+	 * The maximum depth of nested tags we're willing to traverse.
+	 *
+	 * @var int
+	 */
 	public $match_depth = null;
 
+	/**
+	 * The depth of nested opening tags, counted from where we started.
+	 *
+	 * @return int The depth of nested tags.
+	 */
 	public function relative_depth() {
 		return count( $this->open_tags );
 	}
