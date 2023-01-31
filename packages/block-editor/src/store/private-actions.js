7f8ece2cf0940ebfe2be7a543622304858180f4d
Daniel Richards <daniel.richards@automattic.com>
2023-01-31 11:39:24 +0800

    Lock the clearBlockSelection editor setting

--- packages/block-editor/src/store/private-actions.js
+++ packages/block-editor/src/store/private-actions.js
@@ -11,7 +11,7 @@ import { Platform } from '@wordpress/element';
  *
  * @see https://github.com/WordPress/gutenberg/pull/46131
  */
-const privateSettings = [ 'inserterMediaCategories' ];
+const privateSettings = [ 'clearBlockSelection', 'inserterMediaCategories' ];
 
 /**
  * Action that updates the block editor settings and
