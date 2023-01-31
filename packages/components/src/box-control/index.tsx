c6391b045179ae91c37e1b898159b31097a57d19
Lena Morita <lena@jaguchi.com>
2023-02-01 05:03:22 +0900

    Add main JSDoc

--- packages/components/src/box-control/index.tsx
+++ packages/components/src/box-control/index.tsx
@@ -46,6 +46,32 @@ function useUniqueId( idProp?: string ) {
 
 	return idProp || instanceId;
 }
+
+/**
+ * BoxControl components let users set values for Top, Right, Bottom, and Left.
+ * This can be used as an input control for values like `padding` or `margin`.
+ *
+ * ```jsx
+ * import { __experimentalBoxControl as BoxControl } from '@wordpress/components';
+ * import { useState } from '@wordpress/element';
+ *
+ * const Example = () => {
+ * 	const [ values, setValues ] = useState( {
+ * 		top: '50px',
+ * 		left: '10%',
+ * 		right: '10%',
+ * 		bottom: '50px',
+ * 	} );
+ *
+ * 	return (
+ * 		<BoxControl
+ * 			values={ values }
+ * 			onChange={ ( nextValues ) => setValues( nextValues ) }
+ * 		/>
+ * 	);
+ * };
+ * ```
+ */
 function BoxControl( {
 	id: idProp,
 	inputProps = defaultInputProps,
