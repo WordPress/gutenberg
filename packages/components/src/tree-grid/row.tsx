8ae06f99e338ddc73ebc4c51975e9da91260bc67
Lena Morita <lena@jaguchi.com>
2023-02-02 02:33:55 +0900

    Remove unnecessary eslint-disable

--- packages/components/src/tree-grid/row.tsx
+++ packages/components/src/tree-grid/row.tsx
@@ -21,12 +21,6 @@ function UnforwardedTreeGridRow(
 	ref: React.ForwardedRef< HTMLTableRowElement >
 ) {
 	return (
-		// Disable reason: Due to an error in the ARIA 1.1 specification, the
-		// aria-posinset and aria-setsize properties are not supported on row
-		// elements. This is being corrected in ARIA 1.2. Consequently, the
-		// linting rule fails when validating this markup.
-		//
-		// eslint-disable-next-line jsx-a11y/role-supports-aria-props
 		<tr
 			{ ...props }
 			ref={ ref }
