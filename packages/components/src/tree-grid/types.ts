37031c57d72b59b8b6a285cab92c5e2c23d78c15
Lena Morita <lena@jaguchi.com>
2023-02-02 03:11:45 +0900

    Tweak aria types

--- packages/components/src/tree-grid/types.ts
+++ packages/components/src/tree-grid/types.ts
@@ -7,23 +7,23 @@ export type TreeGridRowProps = {
 	 * An integer value designating the level in the hierarchical tree structure.
 	 * Counting starts at 1. A value of `1` indicates the root level of the structure.
 	 */
-	level: React.AriaAttributes[ 'aria-level' ];
+	level: NonNullable< React.AriaAttributes[ 'aria-level' ] >;
 	/**
 	 * An integer value that represents the position in the set.
 	 * A set is the count of elements at a specific level. Counting starts at 1.
 	 */
-	positionInSet: React.AriaAttributes[ 'aria-posinset' ];
+	positionInSet: NonNullable< React.AriaAttributes[ 'aria-posinset' ] >;
 	/**
 	 * An integer value that represents the total number of items in the set,
 	 * at this specific level of the hierarchy.
 	 */
-	setSize: React.AriaAttributes[ 'aria-setsize' ];
+	setSize: NonNullable< React.AriaAttributes[ 'aria-setsize' ] >;
 	/**
 	 * An optional value that designates whether a row is expanded or collapsed.
 	 * Currently this value only sets the correct aria-expanded property on a row,
 	 * it has no other built-in behavior.
 	 */
-	isExpanded?: React.AriaAttributes[ 'aria-expanded' ];
+	isExpanded?: boolean;
 };
 
 type RovingTabIndexItemPassThruProps = {
