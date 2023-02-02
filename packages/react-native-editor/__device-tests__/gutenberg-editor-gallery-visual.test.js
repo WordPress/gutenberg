d7723f6f692decf5e7e614b1fa3418a8a2cf1817
Gerardo <gerardo.pacheco@automattic.com>
2023-02-02 21:07:17 +0100

    Mobile - Gallery E2E Visual tests - Initial tests

---
+++ packages/react-native-editor/__device-tests__/gutenberg-editor-gallery-visual.test.js
@@ -0,0 +1,40 @@
+/**
+ * Internal dependencies
+ */
+import { blockNames } from './pages/editor-page';
+import testData from './helpers/test-data';
+
+describe( 'Gutenberg Editor Visual test for Gallery Block', () => {
+	it( `should be able to render the placeholder correctly for ${ editorPage.getPlatform() }`, async () => {
+		await editorPage.addNewBlock( blockNames.gallery );
+
+		await editorPage.closePicker();
+		const emptyGalleryBlock = await editorPage.getBlockAtPosition(
+			blockNames.gallery
+		);
+		expect( emptyGalleryBlock ).toBeTruthy();
+
+		// Visual test check
+		const screenshot = await editorPage.takeScreenshot();
+		expect( screenshot ).toMatchImageSnapshot();
+
+		await editorPage.removeBlockAtPosition( blockNames.gallery );
+	} );
+
+	it( `should be able to render a gallery correctly for ${ editorPage.getPlatform() }`, async () => {
+		await editorPage.setHtmlContent(
+			[ testData.galleryBlock ].join( '\n\n' )
+		);
+		const galleryBlock = await editorPage.getBlockAtPosition(
+			blockNames.gallery
+		);
+		expect( galleryBlock ).toBeTruthy();
+
+		// Wait for images to load
+		await editorPage.driver.sleep( 5000 );
+
+		// Visual test check
+		const screenshot = await editorPage.takeScreenshot();
+		expect( screenshot ).toMatchImageSnapshot();
+	} );
+} );
