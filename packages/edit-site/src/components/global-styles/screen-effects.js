b13b4a5db4d00b875815384c619e0b0f0d2356dc
Madhu Dollu <madhusudhan.dollu@gmail.com>
2023-02-02 21:56:31 +0530

    Shadow: move shadow to own panel (#47634)
    * move shadow into newly created effects panel
    
    * rename effects to shadow
    
    * rename label shadows to shadow in popover

--- packages/edit-site/src/components/global-styles/screen-effects.js
+++ packages/edit-site/src/components/global-styles/screen-effects.js
@@ -7,22 +7,22 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import ScreenHeader from './header';
-import BorderPanel, { useHasBorderPanel } from './border-panel';
 import BlockPreviewPanel from './block-preview-panel';
 import { getVariationClassName } from './utils';
+import ShadowPanel, { useHasShadowControl } from './shadow-panel';
 
-function ScreenBorder( { name, variation = '' } ) {
-	const hasBorderPanel = useHasBorderPanel( name );
+function ScreenEffects( { name, variation = '' } ) {
 	const variationClassName = getVariationClassName( variation );
+	const hasShadowPanel = useHasShadowControl( name );
 	return (
 		<>
-			<ScreenHeader title={ __( 'Border & Shadow' ) } />
+			<ScreenHeader title={ __( 'Shadow' ) } />
 			<BlockPreviewPanel name={ name } variation={ variationClassName } />
-			{ hasBorderPanel && (
-				<BorderPanel name={ name } variation={ variation } />
+			{ hasShadowPanel && (
+				<ShadowPanel name={ name } variation={ variation } />
 			) }
 		</>
 	);
 }
 
-export default ScreenBorder;
+export default ScreenEffects;
--- packages/edit-site/src/components/global-styles/ui.js
+++ packages/edit-site/src/components/global-styles/ui.js
@@ -38,6 +38,7 @@ import ScreenBorder from './screen-border';
 import StyleBook from '../style-book';
 import ScreenCSS from './screen-css';
 import { unlock } from '../../experiments';
+import ScreenEffects from './screen-effects';
 
 const SLOT_FILL_NAME = 'GlobalStylesMenu';
 const { Slot: GlobalStylesMenuSlot, Fill: GlobalStylesMenuFill } =
@@ -226,6 +227,10 @@ function ContextScreens( { name, parentMenu = '', variation = '' } ) {
 
 			<GlobalStylesNavigationScreen path={ parentMenu + '/border' }>
 				<ScreenBorder name={ name } variation={ variation } />
+			</GlobalStylesNavigationScreen>
+
+			<GlobalStylesNavigationScreen path={ parentMenu + '/effects' }>
+				<ScreenEffects name={ name } variation={ variation } />
 			</GlobalStylesNavigationScreen>
 
 			<GlobalStylesNavigationScreen path={ parentMenu + '/layout' }>
