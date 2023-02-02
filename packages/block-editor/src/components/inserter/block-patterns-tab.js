c68233d250c49089ac57bdb55f9133be54875c9f
Andy Peatling <apeatling@users.noreply.github.com>
2023-02-02 08:00:21 -0800

    Update packages/block-editor/src/components/inserter/block-patterns-tab.js
    Co-authored-by: George Mamadashvili <georgemamadashvili@gmail.com>

--- packages/block-editor/src/components/inserter/block-patterns-tab.js
+++ packages/block-editor/src/components/inserter/block-patterns-tab.js
@@ -195,8 +195,7 @@ function BlockPatternsTabs( {
 } ) {
 	const [ showPatternsExplorer, setShowPatternsExplorer ] = useState( false );
 	const categories = usePatternsCategories( rootClientId );
-	const initialCategory =
-		selectedCategory || ( categories.length && categories[ 0 ] );
+	const initialCategory = selectedCategory || categories[ 0 ];
 	const isMobile = useViewportMatch( 'medium', '<' );
 	return (
 		<>
