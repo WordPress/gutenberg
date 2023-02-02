1c620c997a1dea5a7b63bb241e936999cfc6e7a3
Alex Lende <alex@lende.xyz>
2023-02-02 10:47:40 -0600

    Move aspect-ratio-dropdown into components

--- packages/block-editor/src/components/aspect-ratio-dropdown/index.js
+++ packages/block-editor/src/components/aspect-ratio-dropdown/index.js
@@ -9,8 +9,8 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { useSettings } from '../use-settings';
-import { POPOVER_PROPS } from './constants';
-import { useImageEditingContext } from './context';
+import { POPOVER_PROPS } from '../image-editor/constants';
+import { useImageEditingContext } from '../image-editor/context';
 
 function AspectGroup( { aspectRatios, isDisabled, label, onClick, value } ) {
 	return (
