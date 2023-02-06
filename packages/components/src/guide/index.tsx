5ff109f2d74d5c893573f63a7cf0bc05a3da7345
Markus Dobmann <m.do@posteo.net>
2023-02-06 12:58:31 +0100

    guide: add jsdoc desc + example

--- packages/components/src/guide/index.tsx
+++ packages/components/src/guide/index.tsx
@@ -19,6 +19,38 @@ import Button from '../button';
 import PageControl from './page-control';
 import type { GuideProps } from './types';
 
+/**
+ * `Guide` is a React component that renders a _user guide_ in a modal. The guide consists of several pages which the user can step through one by one. The guide is finished when the modal is closed or when the user clicks _Finish_ on the last page of the guide.
+ *
+ * ```jsx
+ * function MyTutorial() {
+ * 	const [ isOpen, setIsOpen ] = useState( true );
+ *
+ * 	if ( ! isOpen ) {
+ * 		return null;
+ * 	}
+ *
+ * 	return (
+ * 		<Guide
+ * 			onFinish={ () => setIsOpen( false ) }
+ * 			pages={ [
+ * 				{
+ * 					content: <p>Welcome to the ACME Store!</p>,
+ * 				},
+ * 				{
+ * 					image: <img src="https://acmestore.com/add-to-cart.png" />,
+ * 					content: (
+ * 						<p>
+ * 							Click <i>Add to Cart</i> to buy a product.
+ * 						</p>
+ * 					),
+ * 				},
+ * 			] }
+ * 		/>
+ * 	);
+ * }
+ * ```
+ */
 function Guide( {
 	children,
 	className,
