9cc89e11bf30fac247bef364cc4a3e0a0e62a04c
Markus Dobmann <m.do@posteo.net>
2023-02-06 12:44:57 +0100

    move jsdoc example above component decl
    This is necessary so it's shown on the documentation page of the Storybook entry.

--- packages/components/src/menu-items-choice/index.tsx
+++ packages/components/src/menu-items-choice/index.tsx
@@ -11,6 +11,39 @@ import type { MenuItemsChoiceProps } from './types';
 
 const noop = () => {};
 
+/**
+ * `MenuItemsChoice` functions similarly to a set of `MenuItem`s, but allows the user to select one option from a set of multiple choices.
+ *
+ *
+ * ```jsx
+ * import { MenuGroup, MenuItemsChoice } from '@wordpress/components';
+ * import { useState } from '@wordpress/element';
+ *
+ * const MyMenuItemsChoice = () => {
+ * 	const [ mode, setMode ] = useState( 'visual' );
+ * 	const choices = [
+ * 		{
+ * 			value: 'visual',
+ * 			label: 'Visual editor',
+ * 		},
+ * 		{
+ * 			value: 'text',
+ * 			label: 'Code editor',
+ * 		},
+ * 	];
+ *
+ * 	return (
+ * 		<MenuGroup label="Editor">
+ * 			<MenuItemsChoice
+ * 				choices={ choices }
+ * 				value={ mode }
+ * 				onSelect={ ( newMode ) => setMode( newMode ) }
+ * 			/>
+ * 		</MenuGroup>
+ * 	);
+ * };
+ * ```
+ */
 function MenuItemsChoice( {
 	choices = [],
 	onHover = noop,
@@ -47,37 +80,4 @@ function MenuItemsChoice( {
 	);
 }
 
-/**
- * `MenuItemsChoice` functions similarly to a set of `MenuItem`s, but allows the user to select one option from a set of multiple choices.
- *
- *
- * ```jsx
- * import { MenuGroup, MenuItemsChoice } from '@wordpress/components';
- * import { useState } from '@wordpress/element';
- *
- * const MyMenuItemsChoice = () => {
- * 	const [ mode, setMode ] = useState( 'visual' );
- * 	const choices = [
- * 		{
- * 			value: 'visual',
- * 			label: 'Visual editor',
- * 		},
- * 		{
- * 			value: 'text',
- * 			label: 'Code editor',
- * 		},
- * 	];
- *
- * 	return (
- * 		<MenuGroup label="Editor">
- * 			<MenuItemsChoice
- * 				choices={ choices }
- * 				value={ mode }
- * 				onSelect={ ( newMode ) => setMode( newMode ) }
- * 			/>
- * 		</MenuGroup>
- * 	);
- * };
- * ```
- */
 export default MenuItemsChoice;
