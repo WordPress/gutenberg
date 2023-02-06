95264105287a85274c4c8766cc38d4c83309d336
Markus Dobmann <m.do@posteo.net>
2023-02-06 12:15:34 +0100

    story: use predefined set of icons

--- packages/components/src/dimension-control/stories/index.tsx
+++ packages/components/src/dimension-control/stories/index.tsx
@@ -8,12 +8,27 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { DimensionControl } from '..';
 import sizes from '../sizes';
 
+/**
+ * WordPress dependencies
+ */
+import { desktop, tablet, mobile } from '@wordpress/icons';
+
 export default {
 	component: DimensionControl,
 	title: 'Components (Experimental)/DimensionControl',
 	argTypes: {
 		onChange: { action: 'onChange' },
 		value: { control: { type: null } },
+		icon: {
+			control: { type: 'select' },
+			options: [ '-', 'desktop', 'tablet', 'mobile' ],
+			mapping: {
+				undefined,
+				desktop,
+				tablet,
+				mobile,
+			},
+		},
 	},
 } as ComponentMeta< typeof DimensionControl >;
 
