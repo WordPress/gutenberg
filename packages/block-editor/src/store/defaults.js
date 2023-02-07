8aa958eb3ae4e86ecb29123efcd9beb0cc8234ca
Andrei Draganescu <andrei.draganescu@automattic.com>
2023-02-07 15:23:11 +0200

    adds all allowed innerblocks to the inspector animation experiment

--- packages/block-editor/src/store/defaults.js
+++ packages/block-editor/src/store/defaults.js
@@ -175,6 +175,13 @@ export const SETTINGS_DEFAULTS = {
 		'core/navigation': { enterDirection: 'leftToRight' },
 		'core/navigation-submenu': { enterDirection: 'rightToLeft' },
 		'core/navigation-link': { enterDirection: 'rightToLeft' },
+		'core/search': { enterDirection: 'rightToLeft' },
+		'core/social-links': { enterDirection: 'rightToLeft' },
+		'core/page-list': { enterDirection: 'rightToLeft' },
+		'core/spacer': { enterDirection: 'rightToLeft' },
+		'core/home-link': { enterDirection: 'rightToLeft' },
+		'core/site-title': { enterDirection: 'rightToLeft' },
+		'core/site-logo': { enterDirection: 'rightToLeft' },
 	},
 
 	generateAnchors: false,
