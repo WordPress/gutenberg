9b6e85c8b44438951f680ade47451a7c3b6643b6
Dave Smith <getdavemail@gmail.com>
2023-02-07 11:05:18 +0000

    Mock useNavigationMenu to control test

--- packages/block-library/src/navigation/edit/test/navigation-menu-selector.js
+++ packages/block-library/src/navigation/edit/test/navigation-menu-selector.js
@@ -1,17 +1,30 @@
 /**
  * External dependencies
  */
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { prettyDOM } from '@testing-library/dom';
-import NavigationMenuSelector from '../navigation-menu-selector';
-// import useNavigationMenu from '../../use-navigation-menu';
 
-jest.mock( '../../use-navigation-menu', () => {} );
+/**
+ * Internal dependencies
+ */
+import NavigationMenuSelector from '../navigation-menu-selector';
+import useNavigationMenu from '../../use-navigation-menu';
+
+jest.mock( '../../use-navigation-menu', () => {
+	// This allows us to tweak the returned value on each test.
+	const mock = jest.fn();
+	return mock;
+} );
 
 describe( 'NavigationMenuSelector', () => {
 	describe( 'Toggle', () => {
-		it( 'should show dropdown toggle with loading state', async () => {
+		it( 'should show dropdown toggle with loading message when menus have not resolved', async () => {
+			useNavigationMenu.mockReturnValue( {
+				navigationMenus: [],
+				isResolvingNavigationMenus: true,
+				hasResolvedNavigationMenus: false,
+			} );
+
 			render( <NavigationMenuSelector /> );
 
 			expect( screen.getByRole( 'button' ) ).toHaveAttribute(
@@ -20,13 +33,24 @@ describe( 'NavigationMenuSelector', () => {
 			);
 		} );
 
-		it( 'should show dropdown toggle with loading state', async () => {
+		it( 'should show dropdown toggle correct prompt when navigation menus have resolved', async () => {
+			useNavigationMenu.mockReturnValue( {
+				navigationMenus: [],
+				isResolvingNavigationMenus: false,
+				hasResolvedNavigationMenus: true,
+			} );
+
 			const user = userEvent.setup();
 			render( <NavigationMenuSelector /> );
 
 			const button = screen.getByRole( 'button' );
 
 			await user.click( button );
+
+			expect( screen.getByRole( 'button' ) ).toHaveAttribute(
+				'aria-label',
+				'Choose a Navigation menu'
+			);
 		} );
 	} );
 } );
