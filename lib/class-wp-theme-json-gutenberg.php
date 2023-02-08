23986e2b593f781269e50366b5dc2de4a0c81bbc
tellthemachines <isabel@tellthemachines.com>
2023-02-08 16:50:23 +1100

    Try reducing specificity of layout margin rules

--- lib/class-wp-theme-json-gutenberg.php
+++ lib/class-wp-theme-json-gutenberg.php
@@ -1298,7 +1298,7 @@ class WP_Theme_JSON_Gutenberg {
 										$spacing_rule['selector']
 									);
 								} else {
-									$format          = static::ROOT_BLOCK_SELECTOR === $selector ? '%1$s .%2$s%3$s' : '%1$s-%2$s%1$s-%2$s%3$s';
+									$format          = static::ROOT_BLOCK_SELECTOR === $selector ? ':where(%s .%s%s)' : '$s-$s$s';
 									$layout_selector = sprintf(
 										$format,
 										$selector,
