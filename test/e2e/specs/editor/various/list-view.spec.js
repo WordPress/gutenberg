c29c64cb74f32d70a02c705071bd58f3a9f8bea0
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-02-09 15:36:19 +0100

    Migrate first block removal regression test

--- test/e2e/specs/editor/various/list-view.spec.js
+++ test/e2e/specs/editor/various/list-view.spec.js
@@ -12,7 +12,7 @@ test.describe( 'List view', () => {
 		editor,
 		page,
 	} ) => {
-		// Insert some blocks of different types.
+		// Insert a couple of blocks of different types.
 		await editor.insertBlock( { name: 'core/heading' } );
 		await editor.insertBlock( { name: 'core/image' } );
 		await editor.insertBlock( { name: 'core/paragraph' } );
@@ -32,5 +32,73 @@ test.describe( 'List view', () => {
 		await paragraphBlockDragButton.dragTo( headingBlock, { x: 0, y: 0 } );
 
 		expect( await editor.getEditedPostContent() ).toMatchSnapshot();
+	} );
+
+	// Check for regressions of https://github.com/WordPress/gutenberg/issues/38763.
+	test( 'shows the correct amount of blocks after a block is removed in the canvas', async ( {
+		editor,
+		page,
+		pageUtils,
+	} ) => {
+		// Insert a couple of blocks of different types.
+		await editor.insertBlock( { name: 'core/image' } );
+		await editor.insertBlock( { name: 'core/heading' } );
+		await editor.insertBlock( { name: 'core/paragraph' } );
+
+		// Open list view.
+		await pageUtils.pressKeyWithModifier( 'access', 'o' );
+
+		const blockList = page.getByRole( 'treegrid', {
+			name: 'Block navigation structure',
+		} );
+
+		// The last inserted paragraph block should be selected.
+		await blockList
+			.getByRole( 'gridcell', {
+				name: 'Paragraph link',
+				selected: true,
+			} )
+			.waitFor();
+
+		// Go to the image block in list view.
+		await pageUtils.pressKeyTimes( 'ArrowUp', 2 );
+		await expect(
+			blockList
+				.getByRole( 'gridcell', {
+					name: 'Image link',
+				} )
+				.locator( 'a' )
+		).toBeFocused();
+
+		// Select the image block in the canvas.
+		await page.keyboard.press( 'Enter' );
+		const imageBlock = page.getByRole( 'document', {
+			name: 'Block: Image',
+		} );
+		await expect(
+			imageBlock.getByRole( 'button', { name: 'Upload' } )
+		).toBeFocused();
+		await page.keyboard.press( 'ArrowUp' );
+		await expect( imageBlock ).toBeFocused();
+
+		// Start listeninig to console errors.
+		let hasThrownConsoleError = false;
+		page.on( 'console', ( msg ) => {
+			if ( msg.type() === 'error' ) {
+				hasThrownConsoleError = true;
+			}
+		} );
+
+		// Delete the image block in the canvas.
+		await page.keyboard.press( 'Backspace' );
+
+		// List view should have two rows.
+		await expect(
+			blockList.getByRole( 'gridcell', { name: /link/i } )
+		).toHaveCount( 2 );
+
+		// Ensure console didn't throw an error as reported in
+		// https://github.com/WordPress/gutenberg/issues/38763.
+		await expect( hasThrownConsoleError ).toBeFalsy();
 	} );
 } );
