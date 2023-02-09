19ad515a125cc125ca70a8bf34a1c0478a7618b4
André <583546+oandregal@users.noreply.github.com>
2023-02-09 19:53:24 +0100

    Add support for Largest Contentful Paint metric

--- packages/e2e-tests/specs/performance/front-end-classic-theme.test.js
+++ packages/e2e-tests/specs/performance/front-end-classic-theme.test.js
@@ -12,6 +12,7 @@ import { createURL, logout } from '@wordpress/e2e-test-utils';
 describe( 'Front End Performance', () => {
 	const results = {
 		timeToFirstByte: [],
+		largestContentfulPaint: [],
 	};
 
 	beforeAll( async () => {
@@ -40,6 +41,46 @@ describe( 'Front End Performance', () => {
 			results.timeToFirstByte.push(
 				navigationTiming.responseStart - navigationTiming.startTime
 			);
+		}
+	} );
+
+	it( 'Largest Contentful Paint (LCP)', async () => {
+		// Based on https://addyosmani.com/blog/puppeteer-recipes/#performance-observer-lcp
+		function calcLCP() {
+			window.largestContentfulPaint = 0;
+
+			const observer = new PerformanceObserver( ( entryList ) => {
+				const entries = entryList.getEntries();
+				const lastEntry = entries[ entries.length - 1 ];
+				window.largestContentfulPaint =
+					lastEntry.renderTime || lastEntry.loadTime;
+			} );
+
+			observer.observe( {
+				type: 'largest-contentful-paint',
+				buffered: true,
+			} );
+
+			document.addEventListener( 'visibilitychange', () => {
+				if ( document.visibilityState === 'hidden' ) {
+					observer.takeRecords();
+					observer.disconnect();
+				}
+			} );
+		}
+
+		// We derive the 75th percentile of the TTFB based on these results.
+		// By running it 16 times, the percentile value would be (75/100)*16=12,
+		// meaning that we discard the worst 4 values.
+		let i = 16;
+		while ( i-- ) {
+			await page.evaluateOnNewDocument( calcLCP );
+			await page.goto( createURL( '/' ) );
+
+			const lcp = await page.evaluate(
+				() => window.largestContentfulPaint
+			);
+			results.largestContentfulPaint.push( lcp );
 		}
 	} );
 } );
