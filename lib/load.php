2bbf3b2f03288342273527dfafdbf1a9f00e67de
Bernie Reiter <ockham@raz.or.at>
2023-02-13 11:17:32 +0100

    Fix comment

--- lib/load.php
+++ lib/load.php
@@ -100,7 +100,7 @@ if ( ! class_exists( 'WP_HTML_Tag_Processor' ) ) {
 	require __DIR__ . '/compat/wordpress-6.2/html-api/class-wp-html-tag-processor.php';
 }
 
-// WordPress 6.2 compat.
+// WordPress 6.3 compat.
 require __DIR__ . '/compat/wordpress-6.3/html-api/class-gutenberg-html-tag-processor-6-3.php';
 
 // Experimental features.
