ec93dcadf5d9a7c2ce1210b992211d80243e6c70
Gerardo <gerardo.pacheco@automattic.com>
2023-02-13 11:51:06 +0100

    Mobile - Update eslintrc config

--- packages/react-native-editor/__device-tests__/.eslintrc.js
+++ packages/react-native-editor/__device-tests__/.eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
 	globals: {
 		// Defined in 'jest_ui_test_environment.js'
 		editorPage: true,
-		E2ETestData: true,
-		E2EUtils: true,
-		E2EBlockNames: true,
+		e2eTestData: true,
+		e2eUtils: true,
 	},
 	rules: {
 		'jest/expect-expect': 'off',
