36dcb00e720898a5b193c3417dd00c237a2425fb
Glen Davies <glendaviesnz@users.noreply.github.com>
2023-02-15 02:55:06 +1300

    Template editor: only disable the save button if no changes rather than hiding it  (#47895)
    * Only show button save state of the button itself initiated the save
    
    * Revert "Only show button save state of the button itself initiated the save"
    
    This reverts commit d6d3f20ed61b46045664dd3decebb5891e6e3102.
    
    * Disable the button when nothing to save, rather than hiding it

--- packages/edit-site/src/components/sidebar/index.js
+++ packages/edit-site/src/components/sidebar/index.js
@@ -2,9 +2,7 @@
  * WordPress dependencies
  */
 import { memo } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
 import { __experimentalNavigatorProvider as NavigatorProvider } from '@wordpress/components';
-import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
@@ -37,16 +35,6 @@ function SidebarScreens() {
 }
 
 function Sidebar() {
-	const { isDirty } = useSelect( ( select ) => {
-		const { __experimentalGetDirtyEntityRecords } = select( coreStore );
-		const dirtyEntityRecords = __experimentalGetDirtyEntityRecords();
-		// The currently selected entity to display.
-		// Typically template or template part in the site editor.
-		return {
-			isDirty: dirtyEntityRecords.length > 0,
-		};
-	}, [] );
-
 	return (
 		<>
 			<NavigatorProvider
@@ -55,11 +43,10 @@ function Sidebar() {
 			>
 				<SidebarScreens />
 			</NavigatorProvider>
-			{ isDirty && (
-				<div className="edit-site-sidebar__footer">
-					<SaveButton />
-				</div>
-			) }
+
+			<div className="edit-site-sidebar__footer">
+				<SaveButton />
+			</div>
 		</>
 	);
 }
