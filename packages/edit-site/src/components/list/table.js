97cf79af9144d5aa585fc7f2f07ec736251f2e3f
Riad Benguella <benguella@gmail.com>
2023-02-16 09:38:57 +0100

    Refactor the site editor URLs for better backward compatibility (#48063)

--- packages/edit-site/src/components/list/table.js
+++ packages/edit-site/src/components/list/table.js
@@ -84,10 +84,6 @@ export default function Table( { templateType } ) {
 							<Heading level={ 4 }>
 								<Link
 									params={ {
-										path:
-											template.type === 'wp_template'
-												? '/templates/single'
-												: '/template-parts/single',
 										postId: template.id,
 										postType: template.type,
 									} }
