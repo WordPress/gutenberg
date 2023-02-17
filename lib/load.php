7e907cdf66aa309b4f97afff9e1c3c6251b37a9a
Ari Stathopoulos <aristath@gmail.com>
2023-02-17 08:47:09 +0200

    Add a filter for wp_kses_allowed_html

--- lib/load.php
+++ lib/load.php
@@ -49,6 +49,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require_once __DIR__ . '/compat/wordpress-6.3/block-patterns.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-blocks-controller.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/footnotes.php';
+	require_once __DIR__ . '/compat/wordpress-6.3/kses-allowed-html.php';
 
 	// WordPress 6.4 compat.
 	require_once __DIR__ . '/compat/wordpress-6.4/class-gutenberg-rest-global-styles-revisions-controller-6-4.php';
