6d4b45273dcac380d5dad023eddd877dea82fc5a
Adam Silverstein <adamjs@google.com>
2023-02-16 23:50:51 -0700

    start on accept restrictions for drop zone

--- packages/block-editor/src/components/media-placeholder/index.js
+++ packages/block-editor/src/components/media-placeholder/index.js
@@ -264,7 +264,7 @@ export function MediaPlaceholder( {
 		}
 
 		return (
-			<DropZone onFilesDrop={ onFilesUpload } onHTMLDrop={ onHTMLDrop } />
+			<DropZone onFilesDrop={ onFilesUpload } onHTMLDrop={ onHTMLDrop } accept = { accept } />
 		);
 	};
 
