eb41893ccba2ef6cc6729c3ffcf46d8c701141fa
Marco Ciampini <marco.ciampo@gmail.com>
2023-02-20 14:55:55 +0100

    Add HStack e2e story + playwright test

---
+++ packages/components/src/h-stack/stories/e2e/index.tsx
@@ -0,0 +1,112 @@
+/**
+ * External dependencies
+ */
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import { View } from '../../../view';
+import { HStack } from '../..';
+
+const E2E_CONTROLS_PROPS: {
+	name: keyof Omit< React.ComponentProps< typeof HStack >, 'children' >;
+	required: boolean;
+	values: Record< string, any >;
+}[] = [
+	{
+		name: 'alignment',
+		required: false,
+		values: {
+			top: 'top',
+			topLeft: 'topLeft',
+			topRight: 'topRight',
+			left: 'left',
+			center: 'center',
+			right: 'right',
+			bottom: 'bottom',
+			bottomLeft: 'bottomLeft',
+			bottomRight: 'bottomRight',
+			edge: 'edge',
+			stretch: 'stretch',
+		},
+	},
+	{
+		name: 'direction',
+		required: false,
+		values: {
+			row: 'row',
+			column: 'column',
+			// responsive: 'responsive',
+		},
+	},
+	// {
+	// 	name: 'expanded',
+	// 	required: false,
+	// 	values: {
+	// 		true: true,
+	// 		false: false,
+	// 	},
+	// },
+	// {
+	// 	name: 'isReversed',
+	// 	required: false,
+	// 	values: {
+	// 		true: true,
+	// 		false: false,
+	// 	},
+	// },
+	{
+		name: 'justify',
+		required: false,
+		values: {
+			spaceAround: 'space-around',
+			spaceBetween: 'space-between',
+			spaceEvenly: 'space-evenly',
+			stretch: 'stretch',
+			center: 'center',
+			end: 'end',
+			flexEnd: 'flex-end',
+			flexStart: 'flex-start',
+			start: 'start',
+		},
+	},
+	// {
+	// 	name: 'wrap',
+	// 	required: false,
+	// 	values: {
+	// 		true: true,
+	// 		false: false,
+	// 	},
+	// },
+];
+
+const meta: ComponentMeta< typeof HStack > = {
+	component: HStack,
+	title: 'Components (Experimental)/HStack',
+};
+export default meta;
+
+const Template: ComponentStory< typeof HStack > = ( props ) => {
+	return (
+		<HStack
+			style={ { background: '#eee', minHeight: '3rem' } }
+			{ ...props }
+		>
+			{ [ 'One', 'Two', 'Three', 'Four', 'Five' ].map( ( text ) => (
+				<View key={ text } style={ { background: '#b9f9ff' } }>
+					{ text }
+				</View>
+			) ) }
+		</HStack>
+	);
+};
+
+export const Default: ComponentStory< typeof HStack > = Template.bind( {} );
+Default.args = {
+	spacing: 3,
+	// The `customE2EControlsProps` is used by custom decorator
+	// used for Storybook-powered e2e tests
+	// @ts-expect-error
+	customE2EControlsProps: E2E_CONTROLS_PROPS,
+};
---
+++ test/storybook-playwright/specs/hstack.spec.ts
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import { test } from '@playwright/test';
+
+/**
+ * Internal dependencies
+ */
+import {
+	gotoStoryId,
+	testAllSnapshotsCombinationsWithE2EControls,
+} from '../utils';
+
+test.describe( 'HStack', () => {
+	test.beforeEach( async ( { page } ) => {
+		await gotoStoryId( page, 'components-experimental-hstack--default', {
+			decorators: { marginChecker: 'show' },
+		} );
+	} );
+
+	test( 'should render', async ( { page } ) => {
+		// This test is going to run slow. Tripe the default timeout.
+		test.slow();
+
+		await page.waitForSelector( '.components-h-stack' );
+
+		await testAllSnapshotsCombinationsWithE2EControls( page );
+	} );
+} );
