9adb374159fd7e4ecfdc1485a5dd1f566d9cebf2
George Mamadashvili <georgemamadashvili@gmail.com>
2023-02-23 11:07:40 +0400

    Add basic e2e tests

---
+++ test/gutenberg-test-themes/emptyhybrid/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+if ( ! function_exists( 'emptyhybrid_support' ) ) :
+	function emptyhybrid_support() {
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+		// Enable block-based template parts support.
+		add_theme_support( 'block-template-parts' );
+	}
+	add_action( 'after_setup_theme', 'emptyhybrid_support' );
+endif;
+
+/**
+ * Enqueue scripts and styles.
+ */
+function emptyhybrid_scripts() {
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'emptytheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'emptyhybrid_scripts' );
