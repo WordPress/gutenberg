ae74f2ef81e4bfcae72333b99fdf30fb05c42e17
scruffian <ben@scruffian.com>
2023-02-23 09:56:59 +0000

    Theme JSON: Don't output rules that 'unset' properties

--- lib/class-wp-theme-json-gutenberg.php
+++ lib/class-wp-theme-json-gutenberg.php
@@ -1488,6 +1488,9 @@ class WP_Theme_JSON_Gutenberg {
 		$declaration_block = array_reduce(
 			$declarations,
 			static function ( $carry, $element ) {
+				if ( 'unset' === $element['value'] ) {
+					return $carry;
+				}
 				return $carry .= $element['name'] . ': ' . $element['value'] . ';'; },
 			''
 		);
