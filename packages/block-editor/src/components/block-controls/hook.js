9835e7f32f9c872797cf2cb06879af9d8e12880c
Ella van Durpe <ella@vandurpe.com>
2023-02-24 19:00:20 +0200

    Update useBlockControlsFill

--- packages/block-editor/src/components/block-controls/hook.js
+++ packages/block-editor/src/components/block-controls/hook.js
@@ -1,37 +1,14 @@
 /**
- * WordPress dependencies
- */
-import { store as blocksStore } from '@wordpress/blocks';
-import { useSelect } from '@wordpress/data';
-
-/**
  * Internal dependencies
  */
 import groups from './groups';
-import { store as blockEditorStore } from '../../store';
 import { useBlockEditContext } from '../block-edit/context';
 import useDisplayBlockControls from '../use-display-block-controls';
 
 export default function useBlockControlsFill( group, shareWithChildBlocks ) {
 	const isDisplayed = useDisplayBlockControls();
-	const { clientId } = useBlockEditContext();
-	const isParentDisplayed = useSelect(
-		( select ) => {
-			const { getBlockName, hasSelectedInnerBlock } =
-				select( blockEditorStore );
-			const { hasBlockSupport } = select( blocksStore );
-			return (
-				shareWithChildBlocks &&
-				hasBlockSupport(
-					getBlockName( clientId ),
-					'__experimentalExposeControlsToChildren',
-					false
-				) &&
-				hasSelectedInnerBlock( clientId )
-			);
-		},
-		[ shareWithChildBlocks, clientId ]
-	);
+	const { shouldDisplayControls } = useBlockEditContext();
+	const isParentDisplayed = shareWithChildBlocks && shouldDisplayControls;
 
 	if ( isDisplayed ) {
 		return groups[ group ]?.Fill;
