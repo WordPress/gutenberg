a00f3f14a27bdd714dcf8638f62d85e55f0e7a57
Adam Zieliński <adam@adamziel.com>
2023-02-28 11:57:40 +0100

    Refactor the remaining uses of ALLOW_EXPERIMENT_REREGISTRATION

--- typings/gutenberg-env/index.d.ts
+++ typings/gutenberg-env/index.d.ts
@@ -2,7 +2,6 @@ interface Environment {
 	NODE_ENV: unknown;
 	IS_GUTENBERG_PLUGIN?: boolean;
 	IS_WORDPRESS_CORE?: boolean;
-	ALLOW_EXPERIMENT_REREGISTRATION?: boolean;
 }
 interface Process {
 	env: Environment;
