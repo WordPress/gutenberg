3416969cf0de4ec94280732700578a8f3cd42a12
Alex Lende <alex@lende.xyz>
2023-03-01 16:55:17 -0600

    Remove tear_down because I don't think it's used

--- phpunit/block-supports/duotone-test.php
+++ phpunit/block-supports/duotone-test.php
@@ -7,15 +7,8 @@
  */
 
 class WP_Block_Supports_Duotone_Test extends WP_UnitTestCase {
-	public function tear_down() {
-		wp_clean_themes_cache();
-		unset( $GLOBALS['wp_themes'] );
-		WP_Style_Engine_CSS_Rules_Store_Gutenberg::remove_all_stores();
-		parent::tear_down();
-	}
-
 	public function test_gutenberg_render_duotone_support_preset() {
-		$block = array(
+		$block         = array(
 			'blockName' => 'core/image',
 			'attrs'     => array( 'style' => array( 'color' => array( 'duotone' => 'var:preset|duotone|slug' ) ) ),
 		);
