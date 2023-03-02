f8a66b8870ccd001343ec29c050a5aedb1df0f79
Dave Smith <getdavemail@gmail.com>
2023-03-02 16:24:24 +0000

    Add Navigation REST class with fallback endpoint

---
+++ lib/experimental/class-wp-rest-navigation-controller.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * REST API: Gutenberg_REST_Navigation_Controller class
+ *
+ * @package    Gutenberg
+ * @subpackage REST_API
+ */
+
+/**
+ * Base Templates REST API Controller.
+ */
+class Gutenberg_REST_Navigation_Controller extends WP_REST_Posts_Controller {
+
+	/**
+	 * Registers the controllers routes.
+	 *
+	 * @return void
+	 */
+	public function register_routes() {
+
+		parent::register_routes();
+
+		// Lists a single nav item based on the given id or slug.
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/fallbacks',
+			array(
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_fallbacks' ),
+					'permission_callback' => array( $this, 'get_fallbacks_permissions_check' ),
+					'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::READABLE ),
+				),
+				'allow_batch' => $this->allow_batch,
+				'schema'      => array( $this, 'get_public_item_schema' ),
+			)
+		);
+	}
+
+	public function get_fallbacks_permissions_check( $request ) {
+		return true;
+	}
+
+	public function get_fallbacks() {
+		return $this->block_core_navigation_create_fallback();
+	}
+
+	private function block_core_navigation_create_fallback() {
+		$should_skip = apply_filters( 'block_core_navigation_skip_fallback', false );
+		if ( $should_skip ) {
+			return;
+		}
+
+		// Get the most recently published Navigation post.
+		$navigation_post = block_core_navigation_get_most_recently_published_navigation();
+
+		// If there are no navigation posts then try to find a classic menu
+		// and convert it into a block based navigation menu.
+		if ( ! $navigation_post ) {
+			$navigation_post = block_core_navigation_maybe_use_classic_menu_fallback();
+		}
+
+		// If there are no navigation posts then default to a list of Pages.
+		if ( ! $navigation_post ) {
+			$navigation_post = block_core_navigation_get_default_fallback();
+		}
+
+		return $navigation_post;
+	}
+
+
+
+
+
+
+}
--- lib/experimental/rest-api.php
+++ lib/experimental/rest-api.php
@@ -101,3 +101,13 @@ function gutenberg_auto_draft_get_sample_permalink( $permalink, $id, $title, $na
 	return $permalink;
 }
 add_filter( 'get_sample_permalink', 'gutenberg_auto_draft_get_sample_permalink', 10, 5 );
+
+function gutenberg_update_navigation_rest_controller( $args, $post_type ) {
+	if ( in_array( $post_type, array( 'wp_navigation' ), true ) ) {
+		// Original set in
+		// https://github.com/WordPress/wordpress-develop/blob/6cbed78c94b9d8c6a9b4c8b472b88ee0cd56528c/src/wp-includes/post.php#L528.
+		$args['rest_controller_class'] = 'Gutenberg_REST_Navigation_Controller';
+	}
+	return $args;
+}
+add_filter( 'register_post_type_args', 'gutenberg_update_navigation_rest_controller', 10, 2 );
