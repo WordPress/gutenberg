5f1229cba53add7c875ba81ec13bc039a36de432
ntsekouras <ntsekouras@outlook.com>
2023-03-04 13:13:50 +0200

    Order initial block items in Navigation with PrivateInserter

--- packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
@@ -5,7 +5,10 @@ import { __ } from '@wordpress/i18n';
 import { useCallback, useMemo } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
-import { BlockEditorProvider, Inserter } from '@wordpress/block-editor';
+import {
+	BlockEditorProvider,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
 import { createBlock } from '@wordpress/blocks';
 
 /**
@@ -40,6 +43,11 @@ function SidebarNavigationScreenWrapper( { children, actions } ) {
 		/>
 	);
 }
+
+const prioritizedInserterBlocks = [
+	'core/navigation-link/page',
+	'core/navigation-link',
+];
 
 export default function SidebarNavigationScreenNavigationMenus() {
 	const history = useHistory();
@@ -107,6 +115,18 @@ export default function SidebarNavigationScreenNavigationMenus() {
 		},
 		[ history ]
 	);
+	const orderInitialBlockItems = useCallback( ( items ) => {
+		items.sort( ( { id: aName }, { id: bName } ) => {
+			// Sort block items according to `prioritizedInserterBlocks`.
+			let aIndex = prioritizedInserterBlocks.indexOf( aName );
+			let bIndex = prioritizedInserterBlocks.indexOf( bName );
+			// All other block items should come after that.
+			if ( aIndex < 0 ) aIndex = prioritizedInserterBlocks.length;
+			if ( bIndex < 0 ) bIndex = prioritizedInserterBlocks.length;
+			return aIndex - bIndex;
+		} );
+		return items;
+	}, [] );
 
 	if ( hasResolvedNavigationMenus && ! hasNavigationMenus ) {
 		return (
@@ -123,7 +143,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 			</SidebarNavigationScreenWrapper>
 		);
 	}
-
+	const { PrivateInserter } = unlock( blockEditorPrivateApis );
 	return (
 		<BlockEditorProvider
 			settings={ storedSettings }
@@ -133,7 +153,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 		>
 			<SidebarNavigationScreenWrapper
 				actions={
-					<Inserter
+					<PrivateInserter
 						rootClientId={ blocks[ 0 ].clientId }
 						position="bottom right"
 						isAppender
@@ -144,6 +164,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 							as: SidebarButton,
 							label: __( 'Add menu item' ),
 						} }
+						orderInitialBlockItems={ orderInitialBlockItems }
 					/>
 				}
 			>
