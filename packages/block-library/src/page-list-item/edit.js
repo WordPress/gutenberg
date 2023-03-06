3bbc9ccad661a4671c3790b5046a284143cf81dd
MaggieCabrera <maggie.cabrera@automattic.com>
2023-03-06 10:32:36 +0100

    show untitled pages on page list on the editor

--- packages/block-library/src/page-list-item/edit.js
+++ packages/block-library/src/page-list-item/edit.js
@@ -5,6 +5,7 @@ import classnames from 'classnames';
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
@@ -39,6 +40,9 @@ export default function PageListItemEdit( { context, attributes } ) {
 	const isNavigationChild = 'showSubmenuIcon' in context;
 	const frontPageId = useFrontPageId();
 
+	const pageLabel =
+		label !== '' ? decodeEntities( label ) : __( '(No title)' );
+
 	const innerBlocksColors = getColors( context, true );
 
 	const navigationChildBlockProps =
@@ -67,7 +71,7 @@ export default function PageListItemEdit( { context, attributes } ) {
 						className="wp-block-navigation-item__content wp-block-navigation-submenu__toggle"
 						aria-expanded="false"
 					>
-						{ decodeEntities( label ) }
+						{ pageLabel }
 					</button>
 					<span className="wp-block-page-list__submenu-icon wp-block-navigation__submenu-icon">
 						<ItemSubmenuIcon />
@@ -80,7 +84,7 @@ export default function PageListItemEdit( { context, attributes } ) {
 					} ) }
 					href={ link }
 				>
-					{ decodeEntities( label ) }
+					{ pageLabel }
 				</a>
 			) }
 			{ hasChildren && (
