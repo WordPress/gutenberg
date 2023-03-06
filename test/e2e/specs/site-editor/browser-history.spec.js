e2bdfce1f247794b335e27846cc8c49355b915f3
Riad Benguella <benguella@gmail.com>
2023-03-06 10:28:02 +0100

    Test tweaks

--- test/e2e/specs/site-editor/browser-history.spec.js
+++ test/e2e/specs/site-editor/browser-history.spec.js
@@ -3,8 +3,6 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-const baseUrl = process.env.WP_BASE_URL || 'http://localhost:8889';
-
 test.describe( 'Site editor browser history', () => {
 	test.beforeAll( async ( { requestUtils } ) => {
 		await requestUtils.activateTheme( 'emptytheme' );
@@ -15,30 +13,27 @@ test.describe( 'Site editor browser history', () => {
 	} );
 
 	test( 'Back button works properly', async ( { admin, page } ) => {
-		// Navigate to a template.
 		await admin.visitAdminPage( 'index.php' );
 		await admin.visitSiteEditor();
-
-		expect( page.url() ).toEqual( baseUrl + '/wp-admin/site-editor.php' );
+		await expect( page ).toHaveURL( '/wp-admin/site-editor.php' );
 
 		// Navigate to a single template
 		await page.click( 'role=button[name="Templates"]' );
 		await page.click( 'role=button[name="Index"]' );
-		expect( page.url() ).toEqual(
-			baseUrl +
-				'/wp-admin/site-editor.php?postType=wp_template&postId=emptytheme%2F%2Findex'
+		await expect( page ).toHaveURL(
+			'/wp-admin/site-editor.php?postType=wp_template&postId=emptytheme%2F%2Findex'
 		);
 
 		// Navigate back to the template list
 		await page.goBack();
-		expect( page.url() ).toEqual(
-			baseUrl + '/wp-admin/site-editor.php?path=%2Fwp_template'
+		await expect( page ).toHaveURL(
+			'/wp-admin/site-editor.php?path=%2Fwp_template'
 		);
 
 		// Navigate back to the dashboard
 		await page.goBack();
 		await page.goBack();
-		expect( page.url() ).toEqual( baseUrl + '/wp-admin/index.php' );
+		await expect( page ).toHaveURL( '/wp-admin/index.php' );
 	} );
 
 	test( 'Opens the template list from the template details view', async ( {
@@ -55,11 +50,11 @@ test.describe( 'Site editor browser history', () => {
 		await page.click( 'role=button[name="Show template details"]' );
 		await page.click( 'role=link[name="Manage all templates"]' );
 
-		expect( page.url() ).toEqual(
-			baseUrl + '/wp-admin/site-editor.php?path=%2Fwp_template%2Fall'
+		await expect( page ).toHaveURL(
+			'/wp-admin/site-editor.php?path=%2Fwp_template%2Fall'
 		);
 
-		const title = page.locator( '.edit-site-list-header__title' );
+		const title = page.getByRole( 'heading', { level: 1 } );
 		await expect( title ).toHaveText( 'Templates' );
 	} );
 } );
