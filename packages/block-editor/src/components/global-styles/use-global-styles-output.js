5f5958710646b84aa53f47bb159e1f0e23084e1c
tellthemachines <isabel@tellthemachines.com>
2023-03-07 13:36:38 +1100

    Repeat compound classname for specificity

--- packages/block-editor/src/components/global-styles/use-global-styles-output.js
+++ packages/block-editor/src/components/global-styles/use-global-styles-output.js
@@ -399,7 +399,7 @@ export function getLayoutStyles( {
 										? `${ selector } .${ className }${
 												spacingStyle?.selector || ''
 										  }`
-										: `:root ${ selector }-${ className }${
+										: `${ selector }-${ className }${ selector }-${ className }${
 												spacingStyle?.selector || ''
 										  }`;
 							}
