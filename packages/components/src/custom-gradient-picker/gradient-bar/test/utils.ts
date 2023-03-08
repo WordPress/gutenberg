9c33aedfac96dca3164541e5fc38569b6b7df2db
Marco Ciampini <marco.ciampo@gmail.com>
2023-03-08 18:03:49 +0100

    Use jest mocks for getBoundingClientRect

--- packages/components/src/custom-gradient-picker/gradient-bar/test/utils.ts
+++ packages/components/src/custom-gradient-picker/gradient-bar/test/utils.ts
@@ -5,53 +5,54 @@ import { getHorizontalRelativeGradientPosition } from '../utils';
 
 describe( 'getHorizontalRelativeGradientPosition', () => {
 	it( 'should return relative percentage position', () => {
-		const containerElement = document.createElement( 'div' );
-		Object.defineProperty(
-			containerElement,
-			'getBoundingClientRect',
-			() => ( {
-				value: {
+		jest.spyOn(
+			window.HTMLElement.prototype,
+			'getBoundingClientRect'
+		).mockImplementationOnce(
+			() =>
+				( {
 					x: 0,
 					width: 1000,
-				},
-			} )
+				} as DOMRect )
 		);
 
+		const containerElement = document.createElement( 'div' );
 		expect(
 			getHorizontalRelativeGradientPosition( 500, containerElement )
 		).toBe( 50 );
 	} );
 
 	it( 'should subtract the x position of the container from the mouse position', () => {
-		const containerElement = document.createElement( 'div' );
-		Object.defineProperty(
-			containerElement,
-			'getBoundingClientRect',
-			() => ( {
-				value: {
+		jest.spyOn(
+			window.HTMLElement.prototype,
+			'getBoundingClientRect'
+		).mockImplementationOnce(
+			() =>
+				( {
 					x: 50,
 					width: 1000,
-				},
-			} )
+				} as DOMRect )
 		);
 
+		const containerElement = document.createElement( 'div' );
 		expect(
 			getHorizontalRelativeGradientPosition( 550, containerElement )
 		).toBe( 50 );
 	} );
 
 	it( 'should clamp to a whole percentage number', () => {
-		const containerElement = document.createElement( 'div' );
-		Object.defineProperty(
-			containerElement,
-			'getBoundingClientRect',
-			() => ( {
-				value: {
+		jest.spyOn(
+			window.HTMLElement.prototype,
+			'getBoundingClientRect'
+		).mockImplementationOnce(
+			() =>
+				( {
 					x: 0,
 					width: 1000,
-				},
-			} )
+				} as DOMRect )
 		);
+
+		const containerElement = document.createElement( 'div' );
 
 		expect(
 			getHorizontalRelativeGradientPosition( 333, containerElement )
@@ -59,17 +60,17 @@ describe( 'getHorizontalRelativeGradientPosition', () => {
 	} );
 
 	it( 'should clamp to zero when mouse position is less the x position', () => {
-		const containerElement = document.createElement( 'div' );
-		Object.defineProperty(
-			containerElement,
-			'getBoundingClientRect',
-			() => ( {
-				value: {
+		jest.spyOn(
+			window.HTMLElement.prototype,
+			'getBoundingClientRect'
+		).mockImplementationOnce(
+			() =>
+				( {
 					x: 50,
 					width: 1000,
-				},
-			} )
+				} as DOMRect )
 		);
+		const containerElement = document.createElement( 'div' );
 
 		expect(
 			getHorizontalRelativeGradientPosition( 2, containerElement )
@@ -77,17 +78,18 @@ describe( 'getHorizontalRelativeGradientPosition', () => {
 	} );
 
 	it( 'should clamp to 100 when mouse position is greater than width', () => {
-		const containerElement = document.createElement( 'div' );
-		Object.defineProperty(
-			containerElement,
-			'getBoundingClientRect',
-			() => ( {
-				value: {
-					x: 0,
+		jest.spyOn(
+			window.HTMLElement.prototype,
+			'getBoundingClientRect'
+		).mockImplementationOnce(
+			() =>
+				( {
+					x: 50,
 					width: 1000,
-				},
-			} )
+				} as DOMRect )
 		);
+
+		const containerElement = document.createElement( 'div' );
 
 		expect(
 			getHorizontalRelativeGradientPosition( 1500, containerElement )
