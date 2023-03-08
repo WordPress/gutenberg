b188a195bb8fc17c1c6960f3b4541a73e795482d
Dave Smith <getdavemail@gmail.com>
2023-03-08 13:03:03 +0000

    Rename away from “permissions”

--- lib/compat/wordpress-6.3/navigation-block-preloading.php
+++ lib/compat/wordpress-6.3/navigation-block-preloading.php
@@ -12,7 +12,7 @@
  * @param array $preload_paths    Preload paths to be filtered.
  * @return array
  */
-function gutenberg_preload_navigation_permissions( $preload_paths ) {
+function gutenberg_preload_navigation_posts( $preload_paths ) {
 
 	$navigation_rest_route = rest_get_route_for_post_type_items(
 		'wp_navigation'
@@ -53,4 +53,4 @@ function gutenberg_preload_navigation_permissions( $preload_paths ) {
 
 	return $preload_paths;
 }
-add_filter( 'block_editor_rest_api_preload_paths', 'gutenberg_preload_navigation_permissions', 10, 2 );
+add_filter( 'block_editor_rest_api_preload_paths', 'gutenberg_preload_navigation_posts', 10, 2 );
