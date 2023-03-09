16e2e174771c790bd45f6479e393f22ee8853910
Gerardo <gerardo.pacheco@automattic.com>
2023-03-09 22:52:02 +0100

    Mobile - Remove react-native-hr mock

--- test/native/setup.js
+++ test/native/setup.js
@@ -119,8 +119,6 @@ jest.mock(
 		props.isVisible ? mockComponent( 'Modal' )( props ) : null
 );
 
-jest.mock( 'react-native-hr', () => () => 'Hr' );
-
 jest.mock( 'react-native-svg', () => {
 	const { forwardRef } = require( 'react' );
 	return {
