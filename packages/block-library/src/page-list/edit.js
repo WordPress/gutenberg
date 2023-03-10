58c76d6fd69f88a1224705f2f92ad241bc0b0702
scruffian <ben@scruffian.com>
2023-03-10 13:02:08 +0000

    Use the page title in the canvas

--- packages/block-library/src/page-list/edit.js
+++ packages/block-library/src/page-list/edit.js
@@ -204,7 +204,7 @@ export default function PageListEdit( {
 					// translators: displayed when a page has an empty title.
 					page.title?.rendered?.trim() !== ''
 						? page.title?.rendered
-						: __( '(No title)' ),
+						: __( '(no title)' ),
 				title: page.title?.rendered,
 				link: page.url,
 				hasChildren,
