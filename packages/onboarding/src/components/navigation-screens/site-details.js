ed856b714b2146062ffd010aa684c4eb97b40dea
ntsekouras <ntsekouras@outlook.com>
2023-03-11 18:22:18 +0200

    add core data and data deps

--- packages/onboarding/src/components/navigation-screens/site-details.js
+++ packages/onboarding/src/components/navigation-screens/site-details.js
@@ -1,3 +1,29 @@
+/**
+ * WordPress dependencies
+ */
+import { TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
+
 export default function SiteDetails() {
-	return <div>Site details content</div>;
+	const { editEntityRecord } = useDispatch( coreStore );
+	const title = useSelect( ( select ) => {
+		const { getEditedEntityRecord } = select( coreStore );
+		return getEditedEntityRecord( 'root', 'site' )?.title;
+	}, [] );
+	return (
+		<div>
+			<h1>Site details content</h1>
+			<TextControl
+				label={ __( 'Site Title' ) }
+				value={ title || '' }
+				onChange={ ( newTitle ) => {
+					editEntityRecord( 'root', 'site', undefined, {
+						title: newTitle,
+					} );
+				} }
+			/>
+		</div>
+	);
 }
