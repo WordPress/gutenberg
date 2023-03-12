1e9a639d0dc913db59fa10ab19e5f99b8f5a3bf1
Riad Benguella <benguella@gmail.com>
2023-03-12 13:00:24 +0000

    Style the onboarding sidebar

--- packages/onboarding/src/components/sidebar/index.js
+++ packages/onboarding/src/components/sidebar/index.js
@@ -1,43 +1,55 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { memo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import {
-	__experimentalUseNavigator as useNavigator,
-	__experimentalItemGroup as ItemGroup,
-	__experimentalItem as Item,
-} from '@wordpress/components';
-
-/**
- * Internal dependencies
- */
-import Header from './header';
+import { Button, __experimentalVStack as VStack } from '@wordpress/components';
 
 export const onboardingSteps = [
 	{ label: __( 'Site details' ), path: '/site-details' },
 	{ label: __( 'Add pages' ), path: '/add-pages' },
 ];
 
-function Sidebar() {
-	const navigator = useNavigator();
+function SidebarItem( { activeStep, step, children } ) {
+	const isAllowed = activeStep >= step;
 	return (
-		<>
-			<Header />
-			<ItemGroup role="list">
-				{ onboardingSteps.map( ( step ) => (
-					<Item
-						role="listitem"
-						key={ step.path }
-						onClick={ () => navigator.goTo( step.path ) }
-						// aria-label={  }
-						// aria-current={}
-					>
-						{ step.label }
-					</Item>
-				) ) }
-			</ItemGroup>
-		</>
+		<Button
+			disabled={ ! isAllowed }
+			className={ classnames( 'onboarding-sidebar__item', {
+				'is-allowed': isAllowed,
+				'is-active': activeStep === step,
+			} ) }
+		>
+			{ children }
+		</Button>
+	);
+}
+
+function Sidebar( { step = 1 } ) {
+	return (
+		<div className="onboarding-sidebar">
+			<h1 className="onboarding-sidebar__title">
+				{ __( 'Bootstrap your site' ) }
+			</h1>
+			<p>{ __( 'Your website is three clicks a way' ) }</p>
+
+			<VStack className="onboarding-sidebar__steps">
+				<SidebarItem activeStep={ step } step={ 1 }>
+					{ __( 'Choose a starting site' ) }
+				</SidebarItem>
+				<SidebarItem activeStep={ step } step={ 2 }>
+					{ __( 'Pick the styles' ) }
+				</SidebarItem>
+				<SidebarItem activeStep={ step } step={ 3 }>
+					{ __( 'Launch' ) }
+				</SidebarItem>
+			</VStack>
+		</div>
 	);
 }
 
