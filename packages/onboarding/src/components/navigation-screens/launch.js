47b3b2db6a34a6f637109ddd0a2f8f531d7d0396
Riad Benguella <benguella@gmail.com>
2023-03-13 12:33:16 +0000

    Add launch API call

--- packages/onboarding/src/components/navigation-screens/launch.js
+++ packages/onboarding/src/components/navigation-screens/launch.js
@@ -1,9 +1,39 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
+import { Button } from '@wordpress/components';
+
 export default function Launch( { theme, category, variation } ) {
+	const [ isLoading, setIsLoading ] = useState( true );
+
+	useEffect( () => {
+		async function installThemeAndVariation() {
+			setIsLoading( true );
+			await apiFetch( {
+				path:
+					'/wp-block-editor/v2/themes-directory-saving?theme_slug=' +
+					theme,
+				method: 'POST',
+			} );
+			setIsLoading( false );
+		}
+		installThemeAndVariation();
+	}, [ theme, variation ] );
+
+	if ( isLoading ) {
+		return <p>Loading...</p>;
+	}
 	return (
 		<div>
+			<h1>Site installed properly.</h1>
 			<p>Theme: { theme }</p>
 			<p>Category: { category }</p>
 			<p>Variation: { variation?.title }</p>
+			<Button variant="primary" href="/site-editor.php">
+				Customize your site
+			</Button>
 		</div>
 	);
 }
