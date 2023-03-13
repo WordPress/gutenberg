402264c242c6f662913a13e5b1c3a248bfbf27bb
Riad Benguella <benguella@gmail.com>
2023-03-13 14:03:21 +0000

    Simplify layout

--- packages/onboarding/src/components/layout/index.js
+++ packages/onboarding/src/components/layout/index.js
@@ -9,30 +9,17 @@ import {
 	FlexItem,
 } from '@wordpress/components';
 import { useState } from '@wordpress/element';
-// eslint-disable-next-line no-restricted-imports
-import {
-	GlobalStylesProvider,
-	StyleBook,
-	GlobalStylesRenderer,
-	store as editSiteStore,
-} from '@wordpress/edit-site';
-import { BlockEditorProvider } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import Sidebar from '../sidebar';
 import SiteHub from '../site-hub';
-import { useSelect } from '@wordpress/data';
 import ScreenChooseStyles from '../screen-choose-styles';
 import ScreenThemePicker from '../screen-theme-picker';
 import ScreenLaunch from '../screen-launch';
 
 export default function Layout() {
-	const settings = useSelect(
-		( select ) => select( editSiteStore ).getSettings(),
-		[]
-	);
 	const [ theme, setTheme ] = useState();
 	const [ category, setCategory ] = useState();
 	const [ variation, setVariation ] = useState();
@@ -54,46 +41,33 @@ export default function Layout() {
 					<SiteHub />
 					<Sidebar step={ step } />
 				</VStack>
-				<GlobalStylesProvider setting={ settings }>
-					<GlobalStylesRenderer />
-					<BlockEditorProvider settings={ settings }>
-						<FlexItem
-							className="onboarding-layout__content"
-							isBlock
-						>
-							<div className="onboarding-layout__canvas">
-								<NavigatorScreen path="/">
-									<ScreenThemePicker
-										theme={ theme }
-										category={ category }
-										setCategory={ setCategory }
-										setTheme={ setTheme }
-									/>
-								</NavigatorScreen>
-
-								<NavigatorScreen path="/step/2">
-									<ScreenChooseStyles
-										theme={ theme }
-										category={ category }
-										variation={ variation }
-										setVariation={ setVariation }
-									/>
-								</NavigatorScreen>
-								<StyleBook
-									isSelected={ () => {} }
-									onSelect={ () => {} }
-								/>
-								<NavigatorScreen path="/step/3">
-									<ScreenLaunch
-										theme={ theme }
-										category={ category }
-										variation={ variation }
-									/>
-								</NavigatorScreen>
-							</div>
-						</FlexItem>
-					</BlockEditorProvider>
-				</GlobalStylesProvider>
+				<FlexItem className="onboarding-layout__content" isBlock>
+					<div className="onboarding-layout__canvas">
+						<NavigatorScreen path="/">
+							<ScreenThemePicker
+								theme={ theme }
+								category={ category }
+								setCategory={ setCategory }
+								setTheme={ setTheme }
+							/>
+						</NavigatorScreen>
+						<NavigatorScreen path="/step/2">
+							<ScreenChooseStyles
+								theme={ theme }
+								category={ category }
+								variation={ variation }
+								setVariation={ setVariation }
+							/>
+						</NavigatorScreen>
+						<NavigatorScreen path="/step/3">
+							<ScreenLaunch
+								theme={ theme }
+								category={ category }
+								variation={ variation }
+							/>
+						</NavigatorScreen>
+					</div>
+				</FlexItem>
 			</Flex>
 		</NavigatorProvider>
 	);
--- packages/onboarding/src/components/screen-choose-styles/index.js
+++ packages/onboarding/src/components/screen-choose-styles/index.js
@@ -2,16 +2,28 @@
  * WordPress dependencies
  */
 // eslint-disable-next-line no-restricted-imports
-import { StyleVariationsContainer, StyleBook } from '@wordpress/edit-site';
+
+// eslint-disable-next-line no-restricted-imports
+import {
+	GlobalStylesProvider,
+	GlobalStylesRenderer,
+	store as editSiteStore,
+	StyleVariationsContainer,
+	StyleBook,
+} from '@wordpress/edit-site';
+import {
+	BlockEditorProvider,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
 import {
 	__experimentalVStack as VStack,
 	__experimentalHeading as Heading,
 	Flex,
 	FlexItem,
 } from '@wordpress/components';
-import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -19,35 +31,50 @@ import { __ } from '@wordpress/i18n';
 import { unlock } from '../../private-apis';
 
 export default function ScreenChooseStyles( { setVariation } ) {
+	const settings = useSelect(
+		( select ) => select( editSiteStore ).getSettings(),
+		[]
+	);
 	const { useGlobalStylesReset } = unlock( blockEditorPrivateApis );
 	const [ , onReset ] = useGlobalStylesReset();
 	useEffect( onReset, [ onReset ] );
 	return (
-		<Flex
-			justify="flex-start"
-			align="stretch"
-			gap={ 8 }
-			className="onboarding-screen-choose-styles"
-		>
-			<FlexItem className="onboarding-screen-choose-styles__variations">
-				<VStack>
-					<Heading level={ 2 }>{ __( 'Choose your style' ) }</Heading>
-					<p>
-						{ __(
-							"Your style will define how the rest of your website looks. Don't worry, you can change that later."
-						) }
-					</p>
-				</VStack>
-				<StyleVariationsContainer onSelect={ setVariation } />
-			</FlexItem>
-			<FlexItem
-				isBlock
-				className="onboarding-screen-choose-styles__stylebook"
-			>
-				<StyleBook.Slot>
-					{ ( [ styleBook ] ) => <div>{ styleBook }</div> }
-				</StyleBook.Slot>
-			</FlexItem>
-		</Flex>
+		<GlobalStylesProvider setting={ settings }>
+			<GlobalStylesRenderer />
+			<BlockEditorProvider settings={ settings }>
+				<Flex
+					justify="flex-start"
+					align="stretch"
+					gap={ 8 }
+					className="onboarding-screen-choose-styles"
+				>
+					<FlexItem className="onboarding-screen-choose-styles__variations">
+						<VStack>
+							<Heading level={ 2 }>
+								{ __( 'Choose your style' ) }
+							</Heading>
+							<p>
+								{ __(
+									"Your style will define how the rest of your website looks. Don't worry, you can change that later."
+								) }
+							</p>
+						</VStack>
+						<StyleVariationsContainer onSelect={ setVariation } />
+					</FlexItem>
+					<FlexItem
+						isBlock
+						className="onboarding-screen-choose-styles__stylebook"
+					>
+						<StyleBook
+							isSelected={ () => {} }
+							onSelect={ () => {} }
+						/>
+						<StyleBook.Slot>
+							{ ( [ styleBook ] ) => <div>{ styleBook }</div> }
+						</StyleBook.Slot>
+					</FlexItem>
+				</Flex>
+			</BlockEditorProvider>
+		</GlobalStylesProvider>
 	);
 }
