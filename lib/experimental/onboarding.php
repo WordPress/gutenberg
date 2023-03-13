dc9cc94c7b69e950bced28583d7e37e228af5c6e
André <583546+oandregal@users.noreply.github.com>
2023-03-13 11:55:55 +0000

    Allow running only some steps (theme_json, templates, parts)

--- lib/experimental/onboarding.php
+++ lib/experimental/onboarding.php
@@ -74,10 +74,12 @@ add_action( 'admin_menu', 'remove_themes_menu', 999 );
  * and saves its theme.json, templates, and parts to the database.
  *
  * @param string $theme_slug The slug of the theme to process.
+ * @param array  $steps      The steps to process. Valid values are 'templates', 'parts', and 'theme_json'.
+ *                           Defaults to templates and parts.
  *
  * @return boolean|WP_Error True on success, WP_Error on failure.
  */
-function gutenberg_save_theme_to_database( $theme_slug ) {
+function gutenberg_save_theme_to_database( $theme_slug, $steps = array( 'templates', 'parts' ) ) {
 	/*
 	 * 1. Download ZIP.
 	 */
@@ -108,30 +110,6 @@ function gutenberg_save_theme_to_database( $theme_slug ) {
 		return $result;
 	}
 
-	/*
-	 * 2. Save theme.json to database.
-	 */
-	$theme_json      = null;
-	$theme_json_file = $unzip_dir . '/' . $theme_slug . '/theme.json';
-	if ( ! file_exists( $theme_json_file ) ) {
-		return new WP_Error( 'theme_json_not_found for the theme ' . $theme_slug . ' at ' . $unzip_dir );
-	}
-
-	// TODO: escape font-families
-	$wp_get_theme    = wp_get_theme();
-	$theme_json_data = wp_json_file_decode( $theme_json_file, array( 'associative' => true ) );
-	if ( null === $theme_json_data ) {
-		return new WP_Error( 'theme.json at ' . $theme_json_file . ' does not exists or could not be decoded.' );
-	}
-
-	$theme_json_data          = WP_Theme_JSON_Resolver_Gutenberg::translate( $theme_json_data, $wp_get_theme->get( 'TextDomain' ) );
-	$theme_json               = new WP_Theme_JSON_Gutenberg( $theme_json_data, 'custom' );
-	$user_cpt                 = WP_Theme_JSON_Resolver_Gutenberg::get_user_data_from_wp_global_styles( $wp_get_theme, true );
-	$user_cpt['post_content'] = wp_json_encode( $theme_json->get_raw_data() );
-	if ( false === $user_cpt['post_content'] ) {
-		return new WP_Error( 'theme.json could not be encoded to JSON.' );
-	}
-
 	global $wpdb;
 	/*
 	 * This code tries to save data in the database (theme.json, templates, and parts) in a single transaction.
@@ -146,80 +124,111 @@ function gutenberg_save_theme_to_database( $theme_slug ) {
 	 *
 	 */
 	$wpdb->query( 'START TRANSACTION' );
-	$post_id = wp_update_post( $user_cpt, true, false );
-	if ( is_wp_error( $post_id ) ) {
-		$wpdb->query( 'ROLLBACK' );
-		return $post_id;
+
+	/*
+	 * 2. Save theme.json to database.
+	 */
+	if ( in_array( 'theme_json', $steps, true ) ) {
+		$theme_json      = null;
+		$theme_json_file = $unzip_dir . '/' . $theme_slug . '/theme.json';
+		if ( ! file_exists( $theme_json_file ) ) {
+			return new WP_Error( 'theme_json_not_found for the theme ' . $theme_slug . ' at ' . $unzip_dir );
+		}
+
+		// TODO: escape font-families
+		$wp_get_theme    = wp_get_theme();
+		$theme_json_data = wp_json_file_decode( $theme_json_file, array( 'associative' => true ) );
+		if ( null === $theme_json_data ) {
+			return new WP_Error( 'theme.json at ' . $theme_json_file . ' does not exists or could not be decoded.' );
+		}
+
+		$theme_json_data          = WP_Theme_JSON_Resolver_Gutenberg::translate( $theme_json_data, $wp_get_theme->get( 'TextDomain' ) );
+		$theme_json               = new WP_Theme_JSON_Gutenberg( $theme_json_data, 'custom' );
+		$user_cpt                 = WP_Theme_JSON_Resolver_Gutenberg::get_user_data_from_wp_global_styles( $wp_get_theme, true );
+		$user_cpt['post_content'] = wp_json_encode( $theme_json->get_raw_data() );
+		if ( false === $user_cpt['post_content'] ) {
+			return new WP_Error( 'theme.json could not be encoded to JSON.' );
+		}
+
+		$post_id = wp_update_post( $user_cpt, true, false );
+		if ( is_wp_error( $post_id ) ) {
+			$wpdb->query( 'ROLLBACK' );
+			return $post_id;
+		}
 	}
 
 	/*
 	 * 3. Save templates to the database.
 	 */
-	$templates_dir = $unzip_dir . '/' . $theme_slug . '/templates';
-	// TODO: is there other templates directory to be checked?
-	if ( ! file_exists( $templates_dir ) ) {
-		$wpdb->query( 'ROLLBACK' );
-		return new WP_Error( 'Templates directory not found at ' . $templates_dir );
-	}
-
-	$template_files = glob( "$templates_dir/*.html" );
-	if ( false === $template_files ) {
-		$wpdb->query( 'ROLLBACK' );
-		return new WP_Error( 'No template files at ' . $templates_dir );
-	}
-
-	foreach ( $template_files as $template_file ) {
-		$template_cpt = array(
-			'post_type'    => 'wp_template',
-			'post_status'  => 'publish',
-			'post_title'   => basename( $template_file, '.html' ),
-			'post_name'    => basename( $template_file, '.html' ),
-			'post_content' => file_get_contents( $template_file ), // TODO: preprocess (translate? escape? etc.)
-			'post_author'  => get_current_user_id(), // TODO: is this the right user?
-			'tax_input'    => array(
-				'wp_theme' => array( 'emptytheme' ), // The base theme.
-			),
-		);
-		$result       = wp_insert_post( $template_cpt, true );
-		if ( is_wp_error( $result ) ) {
+	if ( in_array( 'templates', $steps, true ) ) {
+		$templates_dir = $unzip_dir . '/' . $theme_slug . '/templates';
+		// TODO: is there other templates directory to be checked?
+		if ( ! file_exists( $templates_dir ) ) {
 			$wpdb->query( 'ROLLBACK' );
-			return $result;
+			return new WP_Error( 'Templates directory not found at ' . $templates_dir );
+		}
+
+		$template_files = glob( "$templates_dir/*.html" );
+		if ( false === $template_files ) {
+			$wpdb->query( 'ROLLBACK' );
+			return new WP_Error( 'No template files at ' . $templates_dir );
+		}
+
+		foreach ( $template_files as $template_file ) {
+			$template_cpt = array(
+				'post_type'    => 'wp_template',
+				'post_status'  => 'publish',
+				'post_title'   => basename( $template_file, '.html' ),
+				'post_name'    => basename( $template_file, '.html' ),
+				'post_content' => file_get_contents( $template_file ), // TODO: preprocess (translate? escape? etc.)
+				'post_author'  => get_current_user_id(), // TODO: is this the right user?
+				'tax_input'    => array(
+					'wp_theme' => array( 'emptytheme' ), // The base theme.
+				),
+			);
+			$result       = wp_insert_post( $template_cpt, true );
+			if ( is_wp_error( $result ) ) {
+				$wpdb->query( 'ROLLBACK' );
+				return $result;
+			}
 		}
 	}
 
 	/*
 	 * 4. Save parts to the database.
 	 */
-	$parts_dir = $unzip_dir . '/' . $theme_slug . '/parts';
-	// TODO: is there other parts directory to be checked?
-	if ( ! file_exists( $parts_dir ) ) {
-		$wpdb->query( 'ROLLBACK' );
-		return new WP_Error( 'Parts directory not found at ' . $parts_dir );
-	}
-
-	$part_files = glob( "$parts_dir/*.html" );
-	if ( false === $part_files ) {
-		$wpdb->query( 'ROLLBACK' );
-		return new WP_Error( 'No part files at ' . $parts_dir );
-	}
-
-	foreach ( $part_files as $part_file ) {
-		$part_cpt = array(
-			'post_type'    => 'wp_template_part',
-			'post_status'  => 'publish',
-			'post_title'   => basename( $part_file, '.html' ),
-			'post_name'    => basename( $part_file, '.html' ),
-			'post_content' => file_get_contents( $part_file ), // TODO: preprocess (translate? escape? etc.)
-			'post_author'  => get_current_user_id(), // TODO: is this the right user?
-			'tax_input'    => array(
-				'wp_theme'           => array( 'emptytheme' ), // The base theme.
-				'template_part_area' => WP_TEMPLATE_PART_AREA_UNCATEGORIZED, // TODO: categorize.
-			),
-		);
-		$result   = wp_insert_post( $part_cpt, true );
-		if ( is_wp_error( $result ) ) {
+	if ( in_array( 'parts', $steps, true ) ) {
+		$parts_dir = $unzip_dir . '/' . $theme_slug . '/parts';
+		// TODO: is there other parts directory to be checked?
+		if ( ! file_exists( $parts_dir ) ) {
 			$wpdb->query( 'ROLLBACK' );
-			return $result;
+			return new WP_Error( 'Parts directory not found at ' . $parts_dir );
+		}
+
+		$part_files = glob( "$parts_dir/*.html" );
+		if ( false === $part_files ) {
+			$wpdb->query( 'ROLLBACK' );
+			return new WP_Error( 'No part files at ' . $parts_dir );
+		}
+
+		foreach ( $part_files as $part_file ) {
+			$part_cpt = array(
+				'post_type'    => 'wp_template_part',
+				'post_status'  => 'publish',
+				'post_title'   => basename( $part_file, '.html' ),
+				'post_name'    => basename( $part_file, '.html' ),
+				'post_content' => file_get_contents( $part_file ), // TODO: preprocess (translate? escape? etc.)
+				'post_author'  => get_current_user_id(), // TODO: is this the right user?
+				'tax_input'    => array(
+					'wp_theme'           => array( 'emptytheme' ), // The base theme.
+					'template_part_area' => WP_TEMPLATE_PART_AREA_UNCATEGORIZED, // TODO: categorize.
+				),
+			);
+			$result   = wp_insert_post( $part_cpt, true );
+			if ( is_wp_error( $result ) ) {
+				$wpdb->query( 'ROLLBACK' );
+				return $result;
+			}
 		}
 	}
 
