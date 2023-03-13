b1dce4e1320cf4bb723cf9f162013d4881d7c0ba
Glen Davies <glendaviesnz@users.noreply.github.com>
2023-03-14 09:02:44 +1300

    Fix non-us spelling (#48976)

--- packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/index.js
@@ -19,7 +19,7 @@ const config = {
 	wp_template_part: {
 		title: __( 'All template parts' ),
 		description: __(
-			'Create new template parts, or reset any customisations made to the template parts supplied by your theme.'
+			'Create new template parts, or reset any customizations made to the template parts supplied by your theme.'
 		),
 	},
 };
