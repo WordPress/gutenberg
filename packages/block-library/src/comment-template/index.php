4486a6b22efb62365e2e5976263883558716da8c
George Mamadashvili <georgemamadashvili@gmail.com>
2023-03-14 17:50:32 +0400

    Revert "sprintf requires more than 1 params (#49054)"
    This reverts commit 784081e279dc1d26e8786e6bff4f97460449a508.

--- packages/block-library/src/comment-template/index.php
+++ packages/block-library/src/comment-template/index.php
@@ -58,10 +58,11 @@ function block_core_comment_template_render_comments( $comments, $block ) {
 				$block_content .= sprintf( '<ol>%1$s</ol>', $inner_content );
 				--$comment_depth;
 			} else {
-				$block_content .= block_core_comment_template_render_comments(
+				$inner_content  = block_core_comment_template_render_comments(
 					$children,
 					$block
 				);
+				$block_content .= sprintf( $inner_content );
 			}
 		}
 
