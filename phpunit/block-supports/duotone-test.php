476fe04eee63a563725d19c88623fc789ebc8607
Jerry Jones <jones.jeremydavid@gmail.com>
2023-03-14 09:51:09 -0500

    Fix duotone test to use real global preset value

--- phpunit/block-supports/duotone-test.php
+++ phpunit/block-supports/duotone-test.php
@@ -10,10 +10,10 @@ class WP_Block_Supports_Duotone_Test extends WP_UnitTestCase {
 	public function test_gutenberg_render_duotone_support_preset() {
 		$block         = array(
 			'blockName' => 'core/image',
-			'attrs'     => array( 'style' => array( 'color' => array( 'duotone' => 'var:preset|duotone|slug' ) ) ),
+			'attrs'     => array( 'style' => array( 'color' => array( 'duotone' => 'var:preset|duotone|blue-orange' ) ) ),
 		);
 		$block_content = '<figure class="wp-block-image size-full"><img src="/my-image.jpg" /></figure>';
-		$expected      = '<figure class="wp-duotone-slug wp-block-image size-full"><img src="/my-image.jpg" /></figure>';
+		$expected      = '<figure class="wp-duotone-blue-orange wp-block-image size-full"><img src="/my-image.jpg" /></figure>';
 		$this->assertSame( $expected, gutenberg_render_duotone_support( $block_content, $block ) );
 	}
 
