06939b15538f2de9ffab0e922ab6721619fe9c0c
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-03-14 09:10:20 -0400

    Restore original gradient bar util JSDoc comments, minus type info

--- packages/components/src/custom-gradient-picker/gradient-bar/utils.ts
+++ packages/components/src/custom-gradient-picker/gradient-bar/utils.ts
@@ -6,10 +6,27 @@
 import { MINIMUM_DISTANCE_BETWEEN_POINTS } from './constants';
 import type { ControlPoint } from '../types';
 
+/**
+ * Clamps a number between 0 and 100.
+ *
+ * @param value Value to clamp.
+ *
+ * @return Value clamped between 0 and 100.
+ */
 export function clampPercent( value: number ) {
 	return Math.max( 0, Math.min( 100, value ) );
 }
 
+/**
+ * Check if a control point is overlapping with another.
+ *
+ * @param value        Array of control points.
+ * @param initialIndex Index of the position to test.
+ * @param newPosition  New position of the control point.
+ * @param minDistance  Distance considered to be overlapping.
+ *
+ * @return True if the point is overlapping.
+ */
 export function isOverlapping(
 	value: ControlPoint[],
 	initialIndex: number,
@@ -29,6 +46,15 @@ export function isOverlapping(
 	} );
 }
 
+/**
+ * Adds a control point from an array and returns the new array.
+ *
+ * @param points   Array of control points.
+ * @param position Position to insert the new point.
+ * @param color    Color to update the control point at index.
+ *
+ * @return New array of control points.
+ */
 export function addControlPoint(
 	points: ControlPoint[],
 	position: number,
@@ -43,12 +69,28 @@ export function addControlPoint(
 	return newPoints;
 }
 
+/**
+ * Removes a control point from an array and returns the new array.
+ *
+ * @param points Array of control points.
+ * @param index  Index to remove.
+ *
+ * @return New array of control points.
+ */
 export function removeControlPoint( points: ControlPoint[], index: number ) {
 	return points.filter( ( _point, pointIndex ) => {
 		return pointIndex !== index;
 	} );
 }
-
+/**
+ * Updates a control point from an array and returns the new array.
+ *
+ * @param points   Array of control points.
+ * @param index    Index to update.
+ * @param newPoint New control point to replace the index.
+ *
+ * @return New array of control points.
+ */
 export function updateControlPoint(
 	points: ControlPoint[],
 	index: number,
@@ -59,6 +101,15 @@ export function updateControlPoint(
 	return newValue;
 }
 
+/**
+ * Updates the position of a control point from an array and returns the new array.
+ *
+ * @param points      Array of control points.
+ * @param index       Index to update.
+ * @param newPosition Position to move the control point at index.
+ *
+ * @return New array of control points.
+ */
 export function updateControlPointPosition(
 	points: ControlPoint[],
 	index: number,
@@ -74,6 +125,15 @@ export function updateControlPointPosition(
 	return updateControlPoint( points, index, newPoint );
 }
 
+/**
+ * Updates the position of a control point from an array and returns the new array.
+ *
+ * @param points   Array of control points.
+ * @param index    Index to update.
+ * @param newColor Color to update the control point at index.
+ *
+ * @return New array of control points.
+ */
 export function updateControlPointColor(
 	points: ControlPoint[],
 	index: number,
@@ -86,6 +146,15 @@ export function updateControlPointColor(
 	return updateControlPoint( points, index, newPoint );
 }
 
+/**
+ * Updates the position of a control point from an array and returns the new array.
+ *
+ * @param points   Array of control points.
+ * @param position Position of the color stop.
+ * @param newColor Color to update the control point at index.
+ *
+ * @return New array of control points.
+ */
 export function updateControlPointColorByPosition(
 	points: ControlPoint[],
 	position: ControlPoint[ 'position' ],
@@ -95,6 +164,14 @@ export function updateControlPointColorByPosition(
 	return updateControlPointColor( points, index, newColor );
 }
 
+/**
+ * Gets the horizontal coordinate when dragging a control point with the mouse.
+ *
+ * @param mouseXCoordinate Horizontal coordinate of the mouse position.
+ * @param containerElement Container for the gradient picker.
+ *
+ * @return Whole number percentage from the left.
+ */
 export function getHorizontalRelativeGradientPosition(
 	mouseXCoordinate: number,
 	containerElement: HTMLDivElement | null
