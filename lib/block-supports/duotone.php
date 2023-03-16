46ad8b743a27c2abe8077cb2b7300854d1f68c38
Jerry Jones <jones.jeremydavid@gmail.com>
2023-03-16 16:32:53 -0500

    Change deprecated magic constant to correct name of __FUNCTION__
    Co-authored-by: Alex Lende <alex@lende.xyz>

--- lib/block-supports/duotone.php
+++ lib/block-supports/duotone.php
@@ -430,7 +430,7 @@ function gutenberg_register_duotone_support( $block_type ) {
  * @return string                Filtered block content.	
  */
 function gutenberg_render_duotone_support( $block_content, $block ) {
-	_deprecated_function( __METHOD__, '6.3.0', 'WP_Duotone_Gutenberg::render_duotone_support' );
+	_deprecated_function( __FUNCTION__, '6.3.0', 'WP_Duotone_Gutenberg::render_duotone_support' );
 	return WP_Duotone_Gutenberg::render_duotone_support( $block_content, $block );
 }
 
