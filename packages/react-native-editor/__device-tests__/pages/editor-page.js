4e4a509e3bbfe983d90e4e7bdeb192fa6474e3d3
David Calhoun <github@davidcalhoun.me>
2023-03-17 16:43:25 -0400

    test: Remove unused test helper argument
    The test that relied upon this functionality, adding a block before the
    currently selected block, is now an integration test.

--- packages/react-native-editor/__device-tests__/pages/editor-page.js
+++ packages/react-native-editor/__device-tests__/pages/editor-page.js
@@ -11,7 +11,6 @@ const {
 	doubleTap,
 	isAndroid,
 	isEditorVisible,
-	longPressMiddleOfElement,
 	setClipboard,
 	setupDriver,
 	stopDriver,
@@ -385,37 +384,10 @@ class EditorPage {
 		return await this.driver.elementsByAccessibilityId( 'Document tools' );
 	}
 
-	async addNewBlock(
-		blockName,
-		{ relativePosition = false, skipInserterOpen = false } = {}
-	) {
+	async addNewBlock( blockName, { skipInserterOpen = false } = {} ) {
 		if ( ! skipInserterOpen ) {
 			const addButton = await this.getAddBlockButton();
-
-			if ( relativePosition === 'before' ) {
-				// On Android it doesn't get the right size of the button
-				const customElementSize = {
-					width: 43,
-					height: 43,
-				};
-
-				await longPressMiddleOfElement(
-					this.driver,
-					addButton,
-					8000,
-					customElementSize
-				);
-				const addBlockBeforeButtonLocator = isAndroid()
-					? '//android.widget.Button[@content-desc="Add Block Before"]'
-					: '//XCUIElementTypeButton[@name="Add Block Before"]';
-
-				await clickIfClickable(
-					this.driver,
-					addBlockBeforeButtonLocator
-				);
-			} else {
-				await addButton.click();
-			}
+			await addButton.click();
 		}
 
 		// Click on block of choice.
