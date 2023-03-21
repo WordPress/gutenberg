abb6e7710beab30df0c8eefc71023046af54a547
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-03-21 16:10:52 +1100

    Revert: Make sticky block action on template part block

--- packages/block-editor/src/private-apis.js
+++ packages/block-editor/src/private-apis.js
@@ -7,11 +7,6 @@ import { lock } from './lock-unlock';
 import OffCanvasEditor from './components/off-canvas-editor';
 import LeafMoreMenu from './components/off-canvas-editor/leaf-more-menu';
 import { ComposedPrivateInserter as PrivateInserter } from './components/inserter';
-import { default as useConvertToGroupButtonProps } from './components/convert-to-group-buttons/use-convert-to-group-button-props';
-import {
-	hasStickyPositionSupport,
-	useIsPositionDisabled,
-} from './hooks/position';
 
 /**
  * Private @wordpress/block-editor APIs.
@@ -23,7 +18,4 @@ lock( privateApis, {
 	LeafMoreMenu,
 	OffCanvasEditor,
 	PrivateInserter,
-	useConvertToGroupButtonProps,
-	hasStickyPositionSupport,
-	useIsPositionDisabled,
 } );
