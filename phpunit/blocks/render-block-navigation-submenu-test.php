bb098ed43949f4f8ac9cf0f142643919b157a087
Dave Smith <getdavemail@gmail.com>
2023-03-21 17:15:33 +0000

    Test for mixed custom and presets

--- phpunit/blocks/render-block-navigation-submenu-test.php
+++ phpunit/blocks/render-block-navigation-submenu-test.php
@@ -172,6 +172,42 @@ class Render_Block_Navigation_Submenu_Test extends WP_UnitTestCase {
 		);
 	}
 
+    /**
+	 * @group submenu-color-inheritance
+	 * @covers ::gutenberg_render_block_core_navigation_submenu
+	 */
+	public function test_should_apply_mix_of_preset_and_custom_colors_inherited_from_parent_block_via_context() {
+		$page_id = self::$page->ID;
+
+		$parsed_blocks = parse_blocks(
+			'<!-- wp:navigation-submenu {"label":"Submenu Label","type":"page","id":' . $page_id . ',"url":"http://localhost:8888/?page_id=' . $page_id . '","kind":"post-type"} -->
+            <!-- wp:navigation-link {"label":"Submenu Item Link Label","type":"page","id":' . $page_id . ',"url":"http://localhost:8888/?page_id=' . $page_id . '","kind":"post-type"} /-->
+        <!-- /wp:navigation-submenu -->'
+		);
+
+		$this->assertEquals( 1, count( $parsed_blocks ), 'Submenu block not parsable.' );
+
+		$block = $parsed_blocks[0];
+
+		// Colors inherited from parent Navigation block.
+		$context = array(
+			'overlayTextColor'       => 'purple',
+			'customOverlayBackgroundColor' => '#E10E0E',
+		);
+
+		$navigation_link_block = new WP_Block( $block, $context );
+
+		$this->assertStringContainsString(
+			'<ul style="background-color:' . $context['customOverlayBackgroundColor'] . ';" class="wp-block-navigation__submenu-container has-text-color has-' . $context['overlayTextColor'] . '-color has-background">',
+			gutenberg_render_block_core_navigation_submenu(
+				$navigation_link_block->attributes,
+				array(),
+				$navigation_link_block
+			),
+			'Submenu block colors inherited from context not applied correctly'
+		);
+	}
+
 	/**
 	 * @group submenu-color-inheritance
 	 * @covers ::gutenberg_render_block_core_navigation_submenu
