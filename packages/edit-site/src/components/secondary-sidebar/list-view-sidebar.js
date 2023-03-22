012bea15e47c3baddf3f4b8062bb6eea87150dca
scruffian <ben@scruffian.com>
2023-03-22 09:18:26 +0000

    remove the appender from the list view

--- packages/edit-site/src/components/secondary-sidebar/list-view-sidebar.js
+++ packages/edit-site/src/components/secondary-sidebar/list-view-sidebar.js
@@ -60,7 +60,7 @@ export default function ListViewSidebar() {
 					focusOnMountRef,
 				] ) }
 			>
-				<PrivateListView showAppender={ true } />
+				<PrivateListView />
 			</div>
 		</div>
 	);
