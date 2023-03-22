6fdd51f5bfadf99bc02d8ae5e8d339aea50c8072
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-03-22 14:02:23 +1100

    Add test to ensure ceiling is skipped when min font size is provided

--- phpunit/block-supports/typography-test.php
+++ phpunit/block-supports/typography-test.php
@@ -567,6 +567,17 @@ class WP_Block_Supports_Typography_Test extends WP_UnitTestCase {
 				'should_use_fluid_typography' => true,
 				'expected_output'             => 'clamp(64px, 4rem + ((1vw - 7.68px) * 16.346), 200px)',
 			),
+
+			'should not use ceiling for minimum font size when custom min font size is set' => array(
+				'font_size'                   => array(
+					'size'  => '200px',
+					'fluid' => array(
+						'min' => '100px',
+					),
+				),
+				'should_use_fluid_typography' => true,
+				'expected_output'             => 'clamp(100px, 6.25rem + ((1vw - 7.68px) * 12.019), 200px)',
+			),
 		);
 	}
 
