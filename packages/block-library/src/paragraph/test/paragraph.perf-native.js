3903b6ff635c5693386a0fe8f5698e4aa39d14e0
Derek Blank <derekpblank@gmail.com>
2023-03-23 17:43:00 +1000

    Add Paragraph performance test case

---
+++ packages/block-library/src/paragraph/test/paragraph.perf-native.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import { fireEvent, measurePerformance, screen } from 'test/helpers';
+
+/**
+ * WordPress dependencies
+ */
+/**
+ * Internal dependencies
+ */
+import Paragraph from '../edit';
+
+describe( 'RichText Performance', () => {
+	it( 'performance is stable when typing text using the Paragraph block', async () => {
+		const scenario = async () => {
+			const paragraph = screen.getByPlaceholderText( 'Start writing…' );
+
+			fireEvent( paragraph, 'focus' );
+			fireEvent.changeText(
+				paragraph,
+				'<strong>Bold</strong> <em>italic</em> <s>strikethrough</s> text'
+			);
+		};
+
+		await measurePerformance(
+			<Paragraph
+				attributes={ {} }
+				setAttributes={ jest.fn() }
+				onChange={ jest.fn() }
+				insertBlocksAfter={ jest.fn() }
+			/>,
+			{
+				scenario,
+			}
+		);
+	} );
+} );
