e85b9c8bde679a4ea43d87a23292d96b6143c82a
Dave Smith <getdavemail@gmail.com>
2023-03-23 09:51:56 +0000

    Add example of dedicated class tests over REST endpoint tests

---
+++ phpunit/experimental/class-wp-navigation-gutenberg-test.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Tests WP_Navigation_Gutenberg
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tests for the WP_Navigation_Gutenberg class.
+ */
+class WP_Navigation_Gutenberg_Test extends WP_UnitTestCase {
+
+	public function test_it_exists() {
+		$this->assertTrue( class_exists( 'WP_Navigation_Gutenberg' ) );
+	}
+
+	public function test_should_return_a_default_fallback_navigation_menu_in_absence_of_other_fallbacks() {
+
+		$data = WP_Navigation_Gutenberg::get_fallback_menu();
+
+		$this->assertInstanceOf( 'WP_Post', $data );
+
+		$this->assertEquals( 'wp_navigation', $data->post_type, 'Fallback menu type should be `wp_navigation`' );
+
+		$this->assertEquals( 'Navigation', $data->post_title, 'Fallback menu title should be the default fallback title' );
+
+		$this->assertEquals( 'navigation', $data->post_name, 'Fallback menu slug (post_name) should be the default slug' );
+
+		$this->assertEquals( '<!-- wp:page-list /-->', $data->post_content );
+
+		$navs_in_db = $this->get_navigations_in_database();
+
+		$this->assertCount( 1, $navs_in_db, 'The fallback Navigation post should be the only one in the database.' );
+	}
+
+	private function get_navigations_in_database() {
+		$navs_in_db = new WP_Query(
+			array(
+				'post_type'      => 'wp_navigation',
+				'post_status'    => 'publish',
+				'posts_per_page' => -1,
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+			)
+		);
+
+		return $navs_in_db->posts ? $navs_in_db->posts : array();
+	}
+
+}
