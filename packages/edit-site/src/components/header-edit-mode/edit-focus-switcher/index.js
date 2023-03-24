c1dbd9d0d8bcf96dfe95ca00e19de80e6174901c
Saxon Fletcher <saxonafletcher@gmail.com>
2023-03-24 11:03:20 +1000

    add notification when switching to template

--- packages/edit-site/src/components/header-edit-mode/edit-focus-switcher/index.js
+++ packages/edit-site/src/components/header-edit-mode/edit-focus-switcher/index.js
@@ -4,11 +4,14 @@
 import { TabPanel } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useEntityRecord } from '@wordpress/core-data';
+import { useEffect, useRef } from '@wordpress/element';
+import { store as noticesStore } from '@wordpress/notices';
 
 /**
  * Internal dependencies
  */
 import { store as editSiteStore } from '../../../store';
+import { unlock } from '../../../private-apis';
 import useEditedEntityRecord from '../../use-edited-entity-record';
 
 // TODO: 'edit focus' could be confused with 'editor mode'
@@ -21,7 +24,35 @@ export default function EditFocusSwitcher() {
 		useEntityRecord( 'postType', postType, postId );
 	const { isLoaded: isTemplateLoaded } = useEditedEntityRecord();
 
+	const shownNotification = useRef( false );
+
 	const { setEditFocus } = useDispatch( editSiteStore );
+
+	const { editFocus } = useSelect(
+		( select ) => ( {
+			editFocus: unlock( select( editSiteStore ) ).getEditFocus(),
+		} ),
+		[]
+	);
+
+	const { createInfoNotice } = useDispatch( noticesStore );
+
+	useEffect( () => {
+		if ( editFocus === 'template' && ! shownNotification.current ) {
+			shownNotification.current = true;
+			createInfoNotice( 'You are editing a template', {
+				type: 'snackbar',
+				actions: [
+					{
+						label: 'Back to page',
+						onClick: () => {
+							setEditFocus( 'post' );
+						},
+					},
+				],
+			} );
+		}
+	}, [ editFocus, createInfoNotice, setEditFocus ] );
 
 	if ( ! hasPostResolved && ! isTemplateLoaded ) {
 		return;
@@ -29,14 +60,17 @@ export default function EditFocusSwitcher() {
 
 	return (
 		<TabPanel
+			activeClass="is-active"
 			tabs={ [
 				{
 					name: 'post',
 					title: post.type,
+					className: editFocus === 'post' ? 'is-active' : '',
 				},
 				{
 					name: 'template',
 					title: 'template',
+					className: editFocus === 'template' ? 'is-active' : '',
 				},
 			] }
 			onSelect={ setEditFocus }
