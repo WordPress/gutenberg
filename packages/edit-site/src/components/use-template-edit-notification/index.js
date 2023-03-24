5a2f13df7f709e2ccc0b8e2ef0483ec279a9fd86
Robert Anderson <robert@noisysocks.com>
2023-03-24 12:08:38 +1100

    Only show notification once

--- packages/edit-site/src/components/use-template-edit-notification/index.js
+++ packages/edit-site/src/components/use-template-edit-notification/index.js
@@ -3,7 +3,7 @@
  */
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useRef } from '@wordpress/element';
 import { store as noticesStore } from '@wordpress/notices';
 import { __ } from '@wordpress/i18n';
 
@@ -49,8 +49,15 @@ export default function useTemplateEditNotification() {
 	const { createNotice } = useDispatch( noticesStore );
 	const { setEditFocus } = useDispatch( editSiteStore );
 
+	const shownNotification = useRef( false );
+
 	useEffect( () => {
-		if ( isPostEditFocus && isTemplateBlockSelected ) {
+		if (
+			isPostEditFocus &&
+			isTemplateBlockSelected &&
+			! shownNotification.current
+		) {
+			shownNotification.current = true;
 			createNotice(
 				'info',
 				__( 'Edit your template to edit this block' ),
