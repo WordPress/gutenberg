1ff98a84b5a3bcf027a71f855bf9b0bd876715e9
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-03-29 13:21:47 +1000

    Add DocBlock to getBlockCSSSelector

--- packages/block-editor/src/components/global-styles/get-block-css-selector.js
+++ packages/block-editor/src/components/global-styles/get-block-css-selector.js
@@ -8,6 +8,17 @@ import { get, isEmpty } from 'lodash';
  */
 import { scopeSelector } from './utils';
 
+/**
+ * Determine the CSS selector for the block type and target provided, returning
+ * it if available.
+ *
+ * @param {import('@wordpress/blocks').Block} blockType        The block's type.
+ * @param {string|string[]}                   target           The desired selector's target e.g. `root`, delimited string, or array path.
+ * @param {Object}                            options          Options object.
+ * @param {boolean}                           options.fallback Whether or not to fallback to broader selector.
+ *
+ * @return {?string} The CSS selector or `null` if no selector available.
+ */
 export function getBlockCSSSelector(
 	blockType,
 	target = 'root',
