96b9006e5672acdb8dce603445dc15c5f988303e
David Calhoun <github@davidcalhoun.me>
2023-03-30 11:53:36 -0400

    test: Add Writing Flow tests (#49352)
    * test: Multi-line Quote should render expected markup
    
    * test: Multi-line Pullquote should render expected markup
    
    * test: Multi-line Verse should render expected markup
    
    * test: Multi-line Preformatted should render expected markup
    
    * test: Rename Preformatted block tests
    
    The "Preformatted" description feels more appropriate and mirrors other
    block tests. I am uncertain as to why this was named differently
    originally.
    
    * test: Add note to expand multi-line test coverage
    
    Our helpers do not support typing multiple lines of text into Rich Text
    inputs. Ideally, we could add this feature to improve our test coverage.
    
    * test: Paragraph should left/center/right align text
    
    * test: Paragraph should bold/italicize/strikethrough text
    
    * test: Paragraph should preserve alignment when split
    
    * test: Paragraph should link text with selection
    
    * test: Paragraph should link text without selection
    
    * test: Paragraph should link text with clipboard contents
    
    * test: Await bottom sheet navigation events to avoid act warnings
    
    * test: Paragraph should not remove whitespace when formatting
    
    * test: Expand native test matcher to include top-level test directory
    
    This allows placing integration tests that target high-level features
    that span across multiple areas of the product, e.g. editor undo/redo
    history.
    
    * test: Editor History should remove and add blocks
    
    * test: Editor History should remove and add text
    
    * test: Editor History should remove and add text formatting
    
    * test: Separate top-level and nested test directory matcher
    
    The previous matcher caused fixture files to be run as tests. Also,
    splitting the regex likely improves the comprehensibility of the
    expression.
    
    * refactor: Use existing getBlock test helper
    
    * test: Avoid stale comment reference

--- packages/block-library/src/preformatted/test/edit.native.js
+++ packages/block-library/src/preformatted/test/edit.native.js
@@ -1,14 +1,29 @@
 /**
  * External dependencies
  */
-import { render } from 'test/helpers';
+import {
+	addBlock,
+	changeAndSelectTextOfRichText,
+	fireEvent,
+	getEditorHtml,
+	initializeEditor,
+	render,
+	setupCoreBlocks,
+} from 'test/helpers';
+
+/**
+ * WordPress dependencies
+ */
+import { ENTER } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
  */
 import PreformattedEdit from '../edit';
 
-describe( 'core/more/edit/native', () => {
+setupCoreBlocks();
+
+describe( 'Preformatted', () => {
 	it( 'renders without crashing', () => {
 		const screen = render(
 			<PreformattedEdit
@@ -41,5 +56,33 @@ describe( 'core/more/edit/native', () => {
 			/>
 		);
 		expect( screen.toJSON() ).toMatchSnapshot();
+	} );
+
+	it( 'should produce expected markup for multiline text', async () => {
+		// Arrange
+		const screen = await initializeEditor();
+
+		// Act
+		await addBlock( screen, 'Preformatted' );
+		const verseTextInput = await screen.findByPlaceholderText(
+			'Write preformatted text…'
+		);
+		const string = 'A great statement.';
+		changeAndSelectTextOfRichText( verseTextInput, string, {
+			selectionStart: string.length,
+			selectionEnd: string.length,
+		} );
+		fireEvent( verseTextInput, 'onKeyDown', {
+			nativeEvent: {},
+			preventDefault() {},
+			keyCode: ENTER,
+		} );
+
+		// Assert
+		expect( getEditorHtml() ).toMatchInlineSnapshot( `
+		"<!-- wp:preformatted -->
+		<pre class="wp-block-preformatted">A great statement.<br></pre>
+		<!-- /wp:preformatted -->"
+	` );
 	} );
 } );
