4c6d1ecc5eff807aff84b55e130fffc817320b02
Ella van Durpe <ella@vandurpe.com>
2023-03-30 12:05:09 +0300

    Fix PHP linting errors

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -644,7 +644,7 @@ $GLOBALS['notes'] = array();
 add_filter(
 	'do_shortcode_tag',
 	function( $output, $tag, $attr, $m ) {
-		if ( $tag !== '#' ) {
+		if ( '#' !== $tag ) {
 			return $output;
 		}
 
