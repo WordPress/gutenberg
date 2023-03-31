0c5f357e1b4ab59e9883b6c95765d0448aaf8ece
Dave Smith <getdavemail@gmail.com>
2023-03-31 12:19:45 +0100

    Reinstate state “reset” but avoid passing internalInputValue as dep to effect

--- packages/block-editor/src/components/link-control/use-internal-input-value.js
+++ packages/block-editor/src/components/link-control/use-internal-input-value.js
@@ -1,12 +1,23 @@
 /**
  * WordPress dependencies
  */
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 
 export default function useInternalInputValue( value ) {
 	const [ internalInputValue, setInternalInputValue ] = useState(
 		value || ''
 	);
+
+	// If the value prop changes, update the internal state.
+	useEffect( () => {
+		setInternalInputValue( ( prevValue ) => {
+			if ( value && value !== prevValue ) {
+				return value;
+			}
+
+			return prevValue;
+		} );
+	}, [ value ] );
 
 	return [ internalInputValue, setInternalInputValue ];
 }
