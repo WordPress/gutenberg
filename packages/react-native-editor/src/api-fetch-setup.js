b5d54a6315faefdef095c901d8544398abdbbc0b
Carlos Garcia <fluiddot@gmail.com>
2023-03-31 18:13:56 +0200

    Remove WPCOM endpoint

--- packages/react-native-editor/src/api-fetch-setup.js
+++ packages/react-native-editor/src/api-fetch-setup.js
@@ -12,7 +12,7 @@ const SUPPORTED_ENDPOINTS = {
 		/wp\/v2\/search\?.*/i,
 		/oembed\/1\.0\/proxy\?.*/i,
 	],
-	POST: [ /wpcom\/v2\/(media)\/.*/i ],
+	POST: [],
 };
 
 // [ONLY ON ANDROID] The requests made to these endpoints won't be cached.
