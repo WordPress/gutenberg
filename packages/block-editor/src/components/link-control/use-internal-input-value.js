eaed518cc137b22368fa095681a019a2c7d0ce2f
Dave Smith <getdavemail@gmail.com>
2023-03-31 11:37:45 +0100

    Remove syncing

--- packages/block-editor/src/components/link-control/use-internal-input-value.js
+++ packages/block-editor/src/components/link-control/use-internal-input-value.js
@@ -1,22 +1,12 @@
 /**
  * WordPress dependencies
  */
-import { useState, useEffect } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 
 export default function useInternalInputValue( value ) {
 	const [ internalInputValue, setInternalInputValue ] = useState(
 		value || ''
 	);
-
-	useEffect( () => {
-		/**
-		 * If the value changes then sync this
-		 * back up with state.
-		 */
-		if ( value && value !== internalInputValue ) {
-			setInternalInputValue( value );
-		}
-	}, [ value ] );
 
 	return [ internalInputValue, setInternalInputValue ];
 }
