4328a6a7640ff7fa95801142fc3574aa81a1d9af
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-04-01 11:49:37 +0900

    Add e2e test

--- test/e2e/specs/editor/blocks/columns.spec.js
+++ test/e2e/specs/editor/blocks/columns.spec.js
@@ -12,7 +12,7 @@ test.describe( 'Columns', () => {
 		await requestUtils.deleteAllPosts();
 	} );
 
-	test( 'restricts all blocks inside the columns block', async ( {
+	test.skip( 'restricts all blocks inside the columns block', async ( {
 		page,
 		editor,
 	} ) => {
@@ -42,5 +42,43 @@ test.describe( 'Columns', () => {
 		);
 		await expect( inserterOptions ).toHaveCount( 1 );
 		await expect( inserterOptions ).toHaveText( 'Column' );
+	} );
+
+	test( 'prevent the removal of locked column block from the column count change UI', async ( {
+		page,
+		editor,
+		pageUtils,
+	} ) => {
+		// Open Columns
+		await editor.insertBlock( { name: 'core/columns' } );
+		await page
+			.locator( '[aria-label="Three columns; equal split"]' )
+			.click();
+
+		// Lock last column block
+		await editor.selectBlocks(
+			page.locator( 'role=document[name="Block: Column (3 of 3)"i]' )
+		);
+		await editor.clickBlockToolbarButton( 'Options' );
+		await page.click( 'role=menuitem[name="Lock"i]' );
+		await page.locator( 'role=checkbox[name="Prevent removal"i]' ).check();
+		await page.click( 'role=button[name="Apply"i]' );
+
+		// Select columns block
+		await editor.selectBlocks(
+			page.locator( 'role=document[name="Block: Columns"i]' )
+		);
+
+		const columnsChangeInput = page.locator(
+			'role=spinbutton[name="Columns"i]'
+		);
+
+		// The min attribute should take into account locked columns
+		await expect( columnsChangeInput ).toHaveAttribute( 'min', '3' );
+
+		// Changing the number of columns should take into account locked columns
+		await page.fill( 'role=spinbutton[name="Columns"i]', '1' );
+		await pageUtils.pressKeys( 'Tab' );
+		await expect( columnsChangeInput ).toHaveValue( '3' );
 	} );
 } );
