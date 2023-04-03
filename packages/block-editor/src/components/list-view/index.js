fb0ea421559cf4ec196dcac64b11735ae1da9daa
Ben Dwyer <ben@scruffian.com>
2023-04-03 13:45:57 +0100

    Update packages/block-editor/src/components/list-view/index.js
    Co-authored-by: Andrew Serong <14988353+andrewserong@users.noreply.github.com>

--- packages/block-editor/src/components/list-view/index.js
+++ packages/block-editor/src/components/list-view/index.js
@@ -79,7 +79,7 @@ function ListViewComponent(
 ) {
 	// This can be removed once we no longer need to support the blocks prop.
 	if ( blocks ) {
-		deprecated( '`blocks` property in `ListViewComponent`', {
+		deprecated( '`blocks` property in `wp.blockEditor.__experimentalListView`', {
 			since: '6.3',
 			alternative: '`rootClientId` property',
 		} );
