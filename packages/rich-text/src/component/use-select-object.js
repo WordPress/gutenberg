9c6345dcc84a5a907cb2c5460f6e85744c601104
Andrei Draganescu <andrei.draganescu@automattic.com>
2023-04-04 17:30:47 +0300

    stub code for demo

--- packages/rich-text/src/component/use-select-object.js
+++ packages/rich-text/src/component/use-select-object.js
@@ -3,10 +3,40 @@
  */
 import { useRefEffect } from '@wordpress/compose';
 
+import apiFetch from '@wordpress/api-fetch';
+
+async function getPageOrPostFromSlug( slug ) {
+	const pages = await apiFetch( { path: `/wp/v2/pages?slug=${ slug }` } );
+
+	if ( pages.length ) {
+		return pages[ 0 ];
+	}
+
+	const posts = await apiFetch( { path: `/wp/v2/posts?slug=${ slug }` } );
+
+	if ( posts.length ) {
+		return posts[ 0 ];
+	}
+}
+
+function buildEditorURI( id ) {
+	return `https://gutenberg.test/wp-admin/post.php?post=${ id }&action=edit`;
+}
+
 export function useSelectObject() {
 	return useRefEffect( ( element ) => {
-		function onClick( event ) {
-			const { target } = event;
+		async function onClick( event ) {
+			const { target, metaKey } = event;
+
+			if ( true === metaKey ) {
+				const { href } = target;
+				//split slug by / and get item before last
+				const slug = href.split( '/' ).slice( -2, -1 )[ 0 ];
+				const postOrPage = await getPageOrPostFromSlug( slug );
+				const goTo = buildEditorURI( postOrPage.id );
+				window.location = goTo;
+				return;
+			}
 
 			// If the child element has no text content, it must be an object.
 			if ( target === element || target.textContent ) {
