a7a47df9c5bd2ff92681af21f19a8302fe0c05f0
ramon <ramonjd@gmail.com>
2023-04-05 16:56:40 +1000

    Relocate js utils tests for isGlobalStyleConfigEqual

--- packages/block-editor/src/components/global-styles/test/utils.js
+++ packages/block-editor/src/components/global-styles/test/utils.js
@@ -1,11 +1,7 @@
 /**
  * Internal dependencies
  */
-import {
-	getPresetVariableFromValue,
-	getValueFromVariable,
-	isGlobalStyleConfigEqual,
-} from '../utils';
+import { getPresetVariableFromValue, getValueFromVariable } from '../utils';
 
 describe( 'editor utils', () => {
 	const themeJson = {
@@ -206,56 +202,5 @@ describe( 'editor utils', () => {
 				expect( actual ).toBe( stylesWithRefs.styles.color.text );
 			} );
 		} );
-	} );
-
-	describe( 'isGlobalStyleConfigEqual', () => {
-		test.each( [
-			{ original: null, variation: null, expected: false },
-			{ original: {}, variation: undefined, expected: false },
-			{
-				original: {
-					styles: {
-						color: { text: 'var(--wp--preset--color--red)' },
-					},
-				},
-				variation: {
-					styles: {
-						color: { text: 'var(--wp--preset--color--blue)' },
-					},
-				},
-				expected: false,
-			},
-			{ original: {}, variation: undefined, expected: false },
-			{
-				original: {
-					styles: {
-						color: { text: 'var(--wp--preset--color--red)' },
-					},
-					settings: {
-						typography: {
-							fontSize: true,
-						},
-					},
-				},
-				variation: {
-					styles: {
-						color: { text: 'var(--wp--preset--color--red)' },
-					},
-					settings: {
-						typography: {
-							fontSize: true,
-						},
-					},
-				},
-				expected: true,
-			},
-		] )(
-			'.isGlobalStyleConfigEqual( $original, $variation )',
-			( { original, variation, expected } ) => {
-				expect( isGlobalStyleConfigEqual( original, variation ) ).toBe(
-					expected
-				);
-			}
-		);
 	} );
 } );
