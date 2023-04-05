08a3eed0f636aa012789816fb485efc8dfeebb49
ramon <ramonjd@gmail.com>
2023-04-05 16:33:37 +1000

    Moving changes to global styles controller to 6.2 compat Removing secondary button styling

--- lib/load.php
+++ lib/load.php
@@ -52,6 +52,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-pattern-directory-controller-6-3.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-templates-controller-6-3.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/rest-api.php';
+	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-global-styles-controller-6-3.php';
 
 	// Experimental.
 	if ( ! class_exists( 'WP_Rest_Customizer_Nonces' ) ) {
