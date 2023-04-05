16ce93dfba26ec4698dd2962ac4936958a8e61c1
Derek Blank <derekpblank@gmail.com>
2023-04-05 15:12:42 +1000

    Add BlockDraggable native performance test

---
+++ packages/block-editor/src/components/block-draggable/test/performance/block-draggable.native.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import { Text, View } from 'react-native';
+import { measurePerformance, screen } from 'test/helpers';
+/**
+ * Internal dependencies
+ */
+import { fireLongPress } from '../helpers';
+
+/**
+ * Internal dependencies
+ */
+import BlockDraggable from '../../index.native';
+
+describe( 'Block Draggable Performance', () => {
+	const onLongPress = jest.fn();
+
+	it( 'performance is stable when dragging block', async () => {
+		const scenario = async () => {
+			const draggable = screen.getByTestId( 'draggable-test' );
+
+			fireLongPress( draggable, 'draggable-test' );
+		};
+
+		await measurePerformance(
+			<View testID="container">
+				<View />
+				<BlockDraggable
+					clientId="clientId"
+					draggingClientId="clientId"
+					enabled={ true }
+					onLongPress={ onLongPress }
+					testID="draggable-test"
+				>
+					{ () => (
+						<View>
+							<Text>Block Draggable</Text>
+						</View>
+					) }
+				</BlockDraggable>
+			</View>,
+			{
+				scenario,
+			}
+		);
+	} );
+} );
