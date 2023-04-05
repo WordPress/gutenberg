b78ed57595b9d6afd488c387d83dd9d410a13244
Ari Stathopoulos <aristath@gmail.com>
2023-04-05 15:46:59 +0300

    reducer should be empty

--- packages/patterns/src/store/reducer.js
+++ packages/patterns/src/store/reducer.js
@@ -3,17 +3,4 @@
  */
 import { combineReducers } from '@wordpress/data';
 
-export function isEditingPattern( state = {}, action ) {
-	if ( action?.type === 'SET_EDITING_PATTERN' ) {
-		return {
-			...state,
-			[ action.clientId ]: action.isEditing,
-		};
-	}
-
-	return state;
-}
-
-export default combineReducers( {
-	isEditingPattern,
-} );
+export default combineReducers( {} );
