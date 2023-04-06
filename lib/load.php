8a06aa28ce46f00121d5a914bf9e0b0caded89c7
Ari Stathopoulos <aristath@gmail.com>
2023-04-06 09:34:42 +0300

    rename file

--- lib/load.php
+++ lib/load.php
@@ -50,7 +50,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require_once __DIR__ . '/compat/wordpress-6.3/rest-api.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/theme-previews.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-patterns-controller.php';
-	require_once __DIR__ . '/compat/wordpress-6.3/post.php';
+	require_once __DIR__ . '/compat/wordpress-6.3/user-block-patterns.php';
 
 	// Experimental.
 	if ( ! class_exists( 'WP_Rest_Customizer_Nonces' ) ) {
