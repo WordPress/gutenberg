ec44ad54c317b9cc6d55e97c631d0658814be5d5
Jerry Jones <jones.jeremydavid@gmail.com>
2023-04-07 13:51:44 -0500

    ede test: grouping into describe test sections

--- test/e2e/specs/editor/various/shortcut-focus-toolbar.spec.js
+++ test/e2e/specs/editor/various/shortcut-focus-toolbar.spec.js
@@ -8,95 +8,116 @@ test.describe( 'Focus toolbar shortcut (alt + F10)', () => {
 		await admin.createNewPost();
 	} );
 
-	test( 'should focus the top level toolbar when no block is selected', async ( {
-		page,
-		pageUtils,
-	} ) => {
-		await page.type(
-			'.editor-post-title__input',
-			'Focus toolbar shortcut (alt + F10)'
-		);
-		// Focus the top level toolbar.
-		await pageUtils.pressKeys( 'alt+F10' );
+	test.describe( 'In default view options in edit mode:', () => {
+		test( 'should focus the top level toolbar when no block is selected', async ( {
+			page,
+			pageUtils,
+		} ) => {
+			await page.type(
+				'.editor-post-title__input',
+				'Focus toolbar shortcut (alt + F10)'
+			);
+			// Focus the top level toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
 
-		// The first top level toolbar button should be focused.
-		await expect(
-			page.getByRole( 'button', { name: 'Toggle block inserter' } )
-		).toBeFocused();
+			// The first top level toolbar button should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Toggle block inserter' } )
+			).toBeFocused();
+		} );
+
+		test( 'should focus the top level toolbar when on an empty block', async ( {
+			page,
+			pageUtils,
+		} ) => {
+			// Move from title into an empty block
+			await page.keyboard.press( 'Enter' );
+			// Focus the top level toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
+
+			// The first top level toolbar button should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Toggle block inserter' } )
+			).toBeFocused();
+		} );
+
+		test( 'should focus the block toolbar when a block is selected', async ( {
+			editor,
+			page,
+			pageUtils,
+		} ) => {
+			await editor.insertBlock( { name: 'core/paragraph' } );
+			await page.keyboard.type( 'Paragraph' );
+			// Focus the block toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
+			// The paragraph block toolbar button should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Paragraph' } )
+			).toBeFocused();
+		} );
+
+		test( 'should focus the block toolbar when a block is selected and the toolbar is visible', async ( {
+			editor,
+			page,
+			pageUtils,
+		} ) => {
+			await editor.insertBlock( { name: 'core/paragraph' } );
+			await page.keyboard.type( 'Paragraph' );
+			// We need to force the toolbar to show. Otherwise, the bug from
+			// https://github.com/WordPress/gutenberg/pull/49644 won't surface in the e2e tests.
+			await editor.showBlockToolbar();
+
+			// Focus the block toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
+			// The block toolbar should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Paragraph' } )
+			).toBeFocused();
+
+			// The document toolbar popup should not be visible
+			await expect(
+				page.locator( 'text=Toggle block inserter' )
+			).not.toBeVisible();
+		} );
 	} );
 
-	test( 'should focus the top level toolbar when on an empty block', async ( {
-		page,
-		pageUtils,
-	} ) => {
-		// Move from title into an empty block
-		await page.keyboard.press( 'Enter' );
-		// Focus the top level toolbar.
-		await pageUtils.pressKeys( 'alt+F10' );
+	test.describe( 'In default view options in select mode:', () => {
+		test( 'should focus the top level toolbar from paragraph block', async ( {
+			editor,
+			page,
+			pageUtils,
+		} ) => {
+			await editor.insertBlock( { name: 'core/paragraph' } );
+			await page.keyboard.type( 'Paragraph' );
+			// We need to force the toolbar to show. Otherwise, the bug from
+			// https://github.com/WordPress/gutenberg/pull/49644 won't surface in the e2e tests.
+			await editor.showBlockToolbar();
+			// Use select mode
+			await page.keyboard.press( 'Escape' );
 
-		// The first top level toolbar button should be focused.
-		await expect(
-			page.getByRole( 'button', { name: 'Toggle block inserter' } )
-		).toBeFocused();
-	} );
+			// Focus the block toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
 
-	test( 'should focus the block toolbar when a block is selected', async ( {
-		editor,
-		page,
-		pageUtils,
-	} ) => {
-		await editor.insertBlock( { name: 'core/paragraph' } );
-		await page.keyboard.type( 'Paragraph' );
-		// Focus the block toolbar.
-		await pageUtils.pressKeys( 'alt+F10' );
-		// The paragraph block toolbar button should be focused.
-		await expect(
-			page.getByRole( 'button', { name: 'Paragraph' } )
-		).toBeFocused();
-	} );
+			// The first top level toolbar button should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Toggle block inserter' } )
+			).toBeFocused();
+		} );
 
-	test( 'should focus the block toolbar when a block is selected and the toolbar is visible', async ( {
-		editor,
-		page,
-		pageUtils,
-	} ) => {
-		await editor.insertBlock( { name: 'core/paragraph' } );
-		await page.keyboard.type( 'Paragraph' );
-		// We need to force the toolbar to show. Otherwise, the bug from
-		// https://github.com/WordPress/gutenberg/pull/49644 won't surface in the e2e tests.
-		await editor.showBlockToolbar();
+		test( 'should focus the top level toolbar from titlek', async ( {
+			page,
+			pageUtils,
+		} ) => {
+			// Use select mode
+			await page.keyboard.press( 'Escape' );
 
-		// Focus the block toolbar.
-		await pageUtils.pressKeys( 'alt+F10' );
-		// The block toolbar should be focused.
-		await expect(
-			page.getByRole( 'button', { name: 'Paragraph' } )
-		).toBeFocused();
+			// Focus the block toolbar.
+			await pageUtils.pressKeys( 'alt+F10' );
 
-		// The document toolbar popup should not be visible
-		await expect(
-			page.locator( 'text=Toggle block inserter' )
-		).not.toBeVisible();
-	} );
-
-	test( 'should focus the top level toolbar when in select mode', async ( {
-		editor,
-		page,
-		pageUtils,
-	} ) => {
-		await editor.insertBlock( { name: 'core/paragraph' } );
-		await page.keyboard.type( 'Paragraph' );
-		// We need to force the toolbar to show. Otherwise, the bug from
-		// https://github.com/WordPress/gutenberg/pull/49644 won't surface in the e2e tests.
-		await editor.showBlockToolbar();
-		// Use select mode
-		await page.keyboard.press( 'Escape' );
-		// Focus the block toolbar.
-		await pageUtils.pressKeys( 'alt+F10' );
-
-		// The first top level toolbar button should be focused.
-		await expect(
-			page.getByRole( 'button', { name: 'Toggle block inserter' } )
-		).toBeFocused();
+			// The first top level toolbar button should be focused.
+			await expect(
+				page.getByRole( 'button', { name: 'Toggle block inserter' } )
+			).toBeFocused();
+		} );
 	} );
 } );
