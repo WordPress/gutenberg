f1b56848c71d009a3caa76c0246c8dccbf2ea26c
Aurooba Ahmed <hey@aurooba.com>
2023-04-08 15:39:27 -0600

    Add explainer text for setGroupingBlockName
    Add extra explainer text on what setGroupingBlockName does and that it needs to be executed once the DOM is fully loaded.

--- packages/blocks/src/api/registration.js
+++ packages/blocks/src/api/registration.js
@@ -523,6 +523,10 @@ export function setDefaultBlockName( name ) {
 /**
  * Assigns name of block for handling block grouping interactions.
  *
+ * This function lets you select a different block to group other blocks in instead of the
+ * default `core/group` block. This function must be used in a component or when the DOM is fully
+ * loaded. See https://developer.wordpress.org/block-editor/reference-guides/packages/packages-dom-ready/
+ *
  * @param {string} name Block name.
  *
  * @example
