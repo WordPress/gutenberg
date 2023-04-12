ae83a5e327b6488f0029fd3a4d8753c5395081f9
Marco Ciampini <marco.ciampo@gmail.com>
2023-04-12 18:29:14 +0200

    Pick only selected props for DropdownMenu

--- packages/components/src/ui/radix-dropdown/types.ts
+++ packages/components/src/ui/radix-dropdown/types.ts
@@ -10,20 +10,55 @@ import type { IconType } from '../../icon';
 
 export type DropdownMenuProps = {
 	/**
-	 * The props passed to the dropdown's root element
+	 * The open state of the dropdown menu when it is initially rendered. Use when
+	 * you do not need to control its open state.
+	 *
 	 */
-	rootProps?: Omit< DropdownMenuPrimitive.DropdownMenuProps, 'children' >;
+	defaultOpen?: DropdownMenuPrimitive.DropdownMenuProps[ 'defaultOpen' ];
 	/**
-	 * The props passed to the dropdown's content
+	 * The controlled open state of the dropdown menu. Must be used in conjunction
+	 * with `onOpenChange`.
 	 */
-	contentProps?: Omit<
-		DropdownMenuPrimitive.DropdownMenuContentProps,
-		'children'
-	>;
-	portalProps?: Omit<
-		DropdownMenuPrimitive.DropdownMenuPortalProps,
-		'children'
-	>;
+	open?: DropdownMenuPrimitive.DropdownMenuProps[ 'open' ];
+	/**
+	 * Event handler called when the open state of the dropdown menu changes.
+	 */
+	onOpenChange?: DropdownMenuPrimitive.DropdownMenuProps[ 'onOpenChange' ];
+	/**
+	 * The modality of the dropdown menu. When set to true, interaction with
+	 * outside elements will be disabled and only menu content will be visible to
+	 * screen readers.
+	 *
+	 * @default true
+	 */
+	modal?: DropdownMenuPrimitive.DropdownMenuProps[ 'modal' ];
+	/**
+	 * The preferred side of the trigger to render against when open.
+	 * Will be reversed when collisions occur and avoidCollisions is enabled.
+	 *
+	 * @default 'bottom'
+	 */
+	side?: DropdownMenuPrimitive.DropdownMenuContentProps[ 'side' ];
+	/**
+	 * The distance in pixels from the trigger.
+	 *
+	 * @default 0
+	 */
+	sideOffset?: DropdownMenuPrimitive.DropdownMenuContentProps[ 'sideOffset' ];
+	/**
+	 * The preferred alignment against the trigger.
+	 * May change when collisions occur.
+	 *
+	 * @default 'center'
+	 */
+	align?: DropdownMenuPrimitive.DropdownMenuContentProps[ 'align' ];
+	/**
+	 * An offset in pixels from the "start" or "end" alignment options.
+	 *
+	 * @default 0
+	 */
+	alignOffset?: DropdownMenuPrimitive.DropdownMenuContentProps[ 'alignOffset' ];
+
 	/**
 	 * The contents rendered inside the trigger
 	 */
