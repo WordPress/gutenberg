cba444260e2aec250b130595c632e939431e34d5
Jarda Snajdr <jsnajdr@gmail.com>
2023-04-13 17:10:54 +0200

    toStyles: add more unit tests, for feature, variation and duotone selectors

--- packages/block-editor/src/components/global-styles/test/use-global-styles-output.js
+++ packages/block-editor/src/components/global-styles/test/use-global-styles-output.js
@@ -484,6 +484,124 @@ describe( 'global styles renderer', () => {
 					'.has-white-color{color: var(--wp--preset--color--white) !important;}.has-white-background-color{background-color: var(--wp--preset--color--white) !important;}.has-white-border-color{border-color: var(--wp--preset--color--white) !important;}.has-black-color{color: var(--wp--preset--color--black) !important;}.has-black-background-color{background-color: var(--wp--preset--color--black) !important;}.has-black-border-color{border-color: var(--wp--preset--color--black) !important;}h1.has-blue-color,h2.has-blue-color,h3.has-blue-color,h4.has-blue-color,h5.has-blue-color,h6.has-blue-color{color: var(--wp--preset--color--blue) !important;}h1.has-blue-background-color,h2.has-blue-background-color,h3.has-blue-background-color,h4.has-blue-background-color,h5.has-blue-background-color,h6.has-blue-background-color{background-color: var(--wp--preset--color--blue) !important;}h1.has-blue-border-color,h2.has-blue-border-color,h3.has-blue-border-color,h4.has-blue-border-color,h5.has-blue-border-color,h6.has-blue-border-color{border-color: var(--wp--preset--color--blue) !important;}'
 			);
 		} );
+
+		it( 'should handle feature selectors', () => {
+			const tree = {
+				styles: {
+					blocks: {
+						'core/image': {
+							color: {
+								text: 'red',
+							},
+							spacing: {
+								padding: {
+									top: '1px',
+								},
+							},
+							border: {
+								color: 'red',
+								radius: '9999px',
+							},
+						},
+					},
+				},
+			};
+
+			const blockSelectors = {
+				'core/image': {
+					selector: '.wp-image',
+					featureSelectors: {
+						spacing: '.wp-image-spacing',
+						border: {
+							root: '.wp-image-border',
+							color: '.wp-image-border-color',
+						},
+					},
+				},
+			};
+
+			expect( toStyles( tree, blockSelectors ) ).toEqual(
+				'body {margin: 0;}' +
+					'.wp-image-spacing{padding-top: 1px;}.wp-image-border-color{border-color: red;}.wp-image-border{border-radius: 9999px;}.wp-image{color: red;}' +
+					'.wp-site-blocks > .alignleft { float: left; margin-right: 2em; }.wp-site-blocks > .alignright { float: right; margin-left: 2em; }.wp-site-blocks > .aligncenter { justify-content: center; margin-left: auto; margin-right: auto; }'
+			);
+		} );
+
+		it( 'should handle block variations', () => {
+			const tree = {
+				styles: {
+					blocks: {
+						'core/image': {
+							variations: {
+								foo: {
+									color: {
+										text: 'blue',
+									},
+									spacing: {
+										padding: {
+											top: '2px',
+										},
+									},
+									border: {
+										color: 'blue',
+									},
+								},
+							},
+						},
+					},
+				},
+			};
+
+			const blockSelectors = {
+				'core/image': {
+					selector: '.wp-image',
+					featureSelectors: {
+						spacing: '.wp-image-spacing',
+						border: {
+							root: '.wp-image-border',
+							color: '.wp-image-border-color',
+						},
+					},
+					styleVariationSelectors: {
+						foo: '.is-style-foo.wp-image',
+					},
+				},
+			};
+
+			expect( toStyles( tree, blockSelectors ) ).toEqual(
+				'body {margin: 0;}' +
+					'.is-style-foo.wp-image.wp-image-spacing{padding-top: 2px;}.is-style-foo.wp-image.wp-image-border-color{border-color: blue;}.is-style-foo.wp-image{color: blue;}' +
+					'.wp-site-blocks > .alignleft { float: left; margin-right: 2em; }.wp-site-blocks > .alignright { float: right; margin-left: 2em; }.wp-site-blocks > .aligncenter { justify-content: center; margin-left: auto; margin-right: auto; }'
+			);
+		} );
+
+		it( 'should handle duotone filter', () => {
+			const tree = {
+				styles: {
+					blocks: {
+						'core/image': {
+							filter: {
+								duotone: 'blur(5px)',
+							},
+						},
+					},
+				},
+			};
+
+			const blockSelectors = {
+				'core/image': {
+					selector: '.wp-image',
+					duotoneSelector: '.wp-image img',
+				},
+			};
+
+			expect( toStyles( tree, blockSelectors ) ).toEqual(
+				'body {margin: 0;}' +
+					'.wp-image img{filter: blur(5px);}' +
+					'.wp-site-blocks > .alignleft { float: left; margin-right: 2em; }.wp-site-blocks > .alignright { float: right; margin-left: 2em; }.wp-site-blocks > .aligncenter { justify-content: center; margin-left: auto; margin-right: auto; }'
+			);
+		} );
+
 		it( 'should output content and wide size variables if values are specified', () => {
 			const tree = {
 				settings: {
@@ -678,8 +796,9 @@ describe( 'global styles renderer', () => {
 				selectors: imageSelectors,
 			};
 			const blockTypes = [ imageBlock ];
+			const getBlockStyles = () => [ { name: 'foo' } ];
 
-			expect( getBlockSelectors( blockTypes, () => {} ) ).toEqual( {
+			expect( getBlockSelectors( blockTypes, getBlockStyles ) ).toEqual( {
 				'core/image': {
 					name: imageBlock.name,
 					selector: imageSelectors.root,
@@ -689,6 +808,9 @@ describe( 'global styles renderer', () => {
 						root: '.my-image',
 						border: '.my-image img, .my-image .crop-area',
 						filter: { duotone: 'img' },
+					},
+					styleVariationSelectors: {
+						foo: '.is-style-foo.my-image',
 					},
 					hasLayoutSupport: false,
 				},
