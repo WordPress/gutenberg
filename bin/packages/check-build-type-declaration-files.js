d715b0ff6c6f9ee2dbf95f84e3c52f68283652fb
Noah Allen <noahtallen@gmail.com>
2023-04-13 13:51:19 -0700

    Refactor to move logic out of main function

--- bin/packages/check-build-type-declaration-files.js
+++ bin/packages/check-build-type-declaration-files.js
@@ -1,13 +1,10 @@
 /**
- * The purpose of this script is to double-check certain published build types.
- * When we translate JSDoc into types that we wish to publish, we occasionally
- * disable the type checker for JS files. Unfortunately, this can result in invalid
- * index.d.ts files being published.
+ * This script verifies the published index.d.ts file for every package which both
+ * builds types and also sets checkJs to false in its tsconfig.json. (This scenario
+ * can cause unchecked errors in JS files to be included in the compiled types.)
  *
- * This script verifies the publishd index.d.ts file for every package which both
- * builds types and also sets checkJs to false in its tsconfig.json. This is done
- * by running `tsc --noEmit` on the $package/build-types/index.d.ts file. This also
- * verifies everything index.d.ts references, which essentially checks the entire
+ * We do so by running `tsc --noEmit` on the $package/build-types/index.d.ts file.
+ * This also verifies everything index.d.ts references, so it checks the entire
  * public api of the type declarations for that package.
  *
  * @see https://github.com/WordPress/gutenberg/pull/49650 for more discussion.
@@ -21,16 +18,53 @@ const path = require( 'path' );
 const { exec } = require( 'child_process' );
 const chalk = require( 'chalk' );
 
-// Tsconfig can be JSON5, which accepts comments, which need to be stripped so we can parse the JSON.
-function stripJsonComments( jsonString ) {
-	const lines = jsonString.split( '\n' );
-	const cleanedLines = lines.map( ( line ) => line.replace( /\/\/.*$/, '' ) );
-	return cleanedLines.join( '\n' );
+/**
+ * Returns whether a package needs its compiled types to be double-checked. This
+ * needs to happen when both of these are true:
+ * 1. The package compiles types. (It has a tsconfig file.)
+ * 2. The tsconfig sets checkJs to false.
+ *
+ * NOTE: In the future, if we run into issues parsing JSON, we should migrate to
+ * a proper json5 parser, such as the json5 npm package. The current regex just
+ * handles comments, which at the time is the only thing we use from JSON5.
+ *
+ * @param {string} packagePath Path to the package.
+ * @return {boolean} whether or not the package checksJs.
+ */
+async function packageNeedsExtraCheck( packagePath ) {
+	const configPath = path.join( packagePath, 'tsconfig.json' );
+
+	try {
+		const tsconfigRaw = await fs.readFile( configPath, 'utf-8' );
+		// Removes comments from the JSON5 string to convert it to plain JSON.
+		const jsonString = tsconfigRaw.replace( /\s+\/\/.*$/gm, '' );
+		const config = JSON.parse( jsonString );
+
+		// If checkJs both exists and is false, then we need the extra check.
+		return config.compilerOptions?.checkJs === false;
+	} catch ( e ) {
+		if ( e.code !== 'ENOENT' ) {
+			throw e;
+		}
+
+		// No tsconfig means no checkJs
+		return false;
+	}
 }
 
-async function readTsConfig( tsconfigPath ) {
-	const tsconfigRaw = await fs.readFile( tsconfigPath, 'utf-8' );
-	return JSON.parse( stripJsonComments( tsconfigRaw ) );
+// Returns the path to the build-types declaration file for a package if it exists.
+// Throws an error and exits the script otherwise.
+async function getDecFile( packagePath ) {
+	const decFile = path.join( packagePath, 'build-types', 'index.d.ts' );
+	try {
+		await fs.access( decFile );
+		return decFile;
+	} catch ( err ) {
+		console.error(
+			`Cannot access this declaration file. You may need to run tsc again: ${ decFile }`
+		);
+		process.exit( 1 );
+	}
 }
 
 async function typecheckDeclarations( file ) {
@@ -45,24 +79,6 @@ async function typecheckDeclarations( file ) {
 	} );
 }
 
-// Returns the path to the build-types declaration file for a package if it exists.
-// Throws an error and exits the script otherwise.
-async function getDecFile( packagePath ) {
-	const decFile = path.join( packagePath, 'build-types', 'index.d.ts' );
-	try {
-		await fs.access( decFile );
-		return decFile;
-	} catch ( err ) {
-		if ( err.code !== 'ENOENT' ) {
-			throw err;
-		}
-		console.error(
-			`This declaration file should exist. You may need to run tsc again: ${ decFile }`
-		);
-		process.exit( 1 );
-	}
-}
-
 async function checkUnverifiedDeclarationFiles() {
 	const packageDir = path.resolve( 'packages' );
 	const packageDirs = (
@@ -71,24 +87,15 @@ async function checkUnverifiedDeclarationFiles() {
 		.filter( ( dirent ) => dirent.isDirectory() )
 		.map( ( dirent ) => path.join( packageDir, dirent.name ) );
 
+	// Finds the compiled type declarations for each package which both checks
+	// types and has checkJs disabled.
 	const declarations = (
 		await Promise.all(
-			packageDirs.map( async ( pkg ) => {
-				try {
-					const tsconfig = await readTsConfig(
-						path.join( pkg, 'tsconfig.json' )
-					);
-
-					if ( tsconfig.compilerOptions?.checkJs === false ) {
-						return getDecFile( pkg );
-					}
-				} catch ( error ) {
-					if ( error.code !== 'ENOENT' ) {
-						throw error;
-					}
-				}
-				return null;
-			} )
+			packageDirs.map( async ( pkg ) =>
+				( await packageNeedsExtraCheck( pkg ) )
+					? getDecFile( pkg )
+					: null
+			)
 		)
 	 ).filter( Boolean );
 
