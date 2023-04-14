b07ff231b82c98e74eaac314b98a4a3d0c24b36c
MaggieCabrera <maggie.cabrera@automattic.com>
2023-04-14 16:49:40 +0200

    add a filter slot and insert StylesFiltersPanel on the duotone hook

--- packages/block-editor/src/components/inspector-controls-tabs/styles-tab.js
+++ packages/block-editor/src/components/inspector-controls-tabs/styles-tab.js
@@ -32,6 +32,7 @@ const StylesTab = ( { blockName, clientId, hasBlockStyles } ) => {
 				label={ __( 'Color' ) }
 				className="color-block-support-panel__inner-wrapper"
 			/>
+			<InspectorControls.Slot group="filter" label={ __( 'Filter' ) } />
 			<InspectorControls.Slot
 				group="typography"
 				label={ __( 'Typography' ) }
