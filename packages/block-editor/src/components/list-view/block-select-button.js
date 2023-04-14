6f963941215d97b53ed81237d6d45b116dabf7bf
Alex Stine <alex.stine@yourtechadvisors.com>
2023-04-14 11:05:35 -0500

    List view: Refactor ARIA attributes (#48461)
    * Fix ARIA attributes.
    
    * Add assertive for selection read.
    
    * Remove useless useEffect. Refactor expanded to use isExpanded attribute.
    
    * Add prop to disable aria-expanded on tree grid tr so I can override on the link.
    
    * Try to fix failing e2e test
    
    * Update list view tests
    
    * Restore removed useEffect.
    
    * Revert TreeGrid changes.
    
    * Finish revert.
    
    * Eliminate aria-expanded on main tr.
    
    * Add data-expanded back to TreeGrid. Need to detect when to expand via keyboard.
    
    * Fix expander.
    
    * Move isExpanded undefined to tr directly.
    
    * Fix E2E.
    
    * Retain dark color for button when expanded
    
    * Add title fallback.
    
    * Save some verbosity on block options button.
    
    * Fix E2E.
    
    * Add components changelog entry.
    
    ---------
    
    Co-authored-by: Andrew Serong <14988353+andrewserong@users.noreply.github.com>

--- packages/block-editor/src/components/list-view/block-select-button.js
+++ packages/block-editor/src/components/list-view/block-select-button.js
@@ -35,6 +35,9 @@ function ListViewBlockSelectButton(
 		onDragStart,
 		onDragEnd,
 		draggable,
+		isExpanded,
+		ariaLabel,
+		ariaDescribedBy,
 	},
 	ref
 ) {
@@ -76,7 +79,9 @@ function ListViewBlockSelectButton(
 				onDragEnd={ onDragEnd }
 				draggable={ draggable }
 				href={ `#block-${ clientId }` }
-				aria-hidden={ true }
+				aria-label={ ariaLabel }
+				aria-describedby={ ariaDescribedBy }
+				aria-expanded={ isExpanded }
 			>
 				<ListViewExpander onClick={ onToggleExpanded } />
 				<BlockIcon icon={ blockInformation?.icon } showColors />
--- packages/block-editor/src/components/list-view/use-block-selection.js
+++ packages/block-editor/src/components/list-view/use-block-selection.js
@@ -144,7 +144,7 @@ export default function useBlockSelection() {
 			}
 
 			if ( label ) {
-				speak( label );
+				speak( label, 'assertive' );
 			}
 		},
 		[
