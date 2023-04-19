68b4ba94d10fd3437d5de655cc4858d6cd3d026a
Jarda Snajdr <jsnajdr@gmail.com>
2023-04-19 09:14:20 +0200

    Post template title edit: update only the avaiableTemplates setting

--- packages/edit-post/src/components/header/template-title/edit-template-title.js
+++ packages/edit-post/src/components/header/template-title/edit-template-title.js
@@ -67,7 +67,6 @@ export default function EditTemplateTitle() {
 						)
 					);
 					updateEditorSettings( {
-						...settings,
 						availableTemplates: newAvailableTemplates,
 					} );
 					editEntityRecord( 'postType', 'wp_template', template.id, {
