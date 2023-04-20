10afc013ea052645067842ced82d52043862b465
Ari Stathopoulos <aristath@gmail.com>
2023-04-20 12:48:37 +0300

    WIP - lang attribute

---
+++ packages/format-library/src/lang/index.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { toggleFormat } from '@wordpress/rich-text';
+import { RichTextToolbarButton } from '@wordpress/block-editor';
+
+const name = 'core/lang';
+const title = __( 'Language selection' );
+
+function Edit( { isActive, value, onChange, onFocus } ) {
+	function onToggle() {
+		onChange( toggleFormat( value, { type: name, title } ) );
+	}
+
+	function onClick() {
+		onToggle();
+		onFocus();
+	}
+
+	return (
+		<RichTextToolbarButton
+			icon={ 'translation' }
+			title={ title }
+			onClick={ onClick }
+			isActive={ isActive }
+			role="menuitemcheckbox"
+		/>
+	);
+}
+
+export const lang = {
+	name,
+	title,
+	tagName: 'span',
+	className: 'wp-lang',
+	edit: Edit,
+	attributes: {
+		lang: 'it',
+		dir: 'ltr',
+	},
+};
