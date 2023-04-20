a7998ceaa9a98417e5bf7b1495d44e7d6c921cce
Glen Davies <glen.davies@automattic.com>
2023-04-21 10:25:20 +1200

    Initial attempt to leave a pattern block wrapped around a pattern

--- packages/block-library/src/pattern/index.js
+++ packages/block-library/src/pattern/index.js
@@ -4,12 +4,14 @@
 import initBlock from '../utils/init-block';
 import metadata from './block.json';
 import PatternEdit from './edit';
+import PatternSave from './save';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
 	edit: PatternEdit,
+	save: PatternSave,
 };
 
 export const init = () => initBlock( { name, metadata, settings } );
