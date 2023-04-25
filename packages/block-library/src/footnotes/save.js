ca086ac5bf69e072ffb975b97d36a03ac670fb40
Ella van Durpe <ella@vandurpe.com>
2023-04-25 08:23:35 +0300

    wip

--- packages/block-library/src/footnotes/save.js
+++ packages/block-library/src/footnotes/save.js
@@ -3,13 +3,31 @@
  */
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 
-export default function Save( { attributes } ) {
+/**
+ * Internal dependencies
+ */
+import { order } from './edit';
+
+export default function Save( { attributes, clientId } ) {
+	const footnotes = Object.fromEntries(
+		attributes.footnotes.map( ( { content, id } ) => [ id, content ] )
+	);
+	order.set(
+		clientId,
+		new Set( [
+			...( order.get( clientId ) || [] ),
+			...Object.keys( footnotes ),
+		] )
+	);
 	return (
 		<footer { ...useBlockProps.save() }>
 			<ol>
-				{ attributes.footnotes.map( ( { id, content } ) => (
+				{ Array.from( order.get( clientId ) ).map( ( id ) => (
 					<li id={ id } key={ id }>
-						<RichText.Content tagName="span" value={ content } />{ ' ' }
+						<RichText.Content
+							tagName="span"
+							value={ footnotes[ id ] }
+						/>{ ' ' }
 						<a href={ `#${ id }-link` }>↩︎</a>
 					</li>
 				) ) }
