2f4ce6939bfd2507dc18a3b839c2ed9d1d6d750e
Jonny Harris <spacedmonkey@users.noreply.github.com>
2023-04-25 15:51:20 +0100

    Apply suggestions from code review

--- lib/load.php
+++ lib/load.php
@@ -113,6 +113,7 @@ require __DIR__ . '/compat/wordpress-6.3/blocks.php';
 remove_action( 'plugins_loaded', '_wp_theme_json_webfonts_handler' ); // Turns off WP 6.0's stopgap handler for Webfonts API.
 require __DIR__ . '/experimental/block-editor-settings-mobile.php';
 require __DIR__ . '/experimental/class-gutenberg-themes-controller.php';
+require __DIR__ . '/experimental/block-editor-settings.php';
 require __DIR__ . '/experimental/blocks.php';
 require __DIR__ . '/experimental/navigation-theme-opt-in.php';
 require __DIR__ . '/experimental/kses.php';
