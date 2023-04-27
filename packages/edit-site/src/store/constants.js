dfc8952774214815e91f9630c7c8c942f98e7900
Robert Anderson <robert@noisysocks.com>
2023-04-27 13:54:25 +1000

    Add CONTENT_BLOCK_TYPES doc comment

--- packages/edit-site/src/store/constants.js
+++ packages/edit-site/src/store/constants.js
@@ -10,6 +10,12 @@ export const TEMPLATE_PART_AREA_FOOTER = 'footer';
 export const TEMPLATE_PART_AREA_SIDEBAR = 'sidebar';
 export const TEMPLATE_PART_AREA_GENERAL = 'uncategorized';
 
+/**
+ * List of block types to treat as content blocks when editing a page in the
+ * site editor. These blocks will be unlocked and editable.
+ *
+ * @type {string[]}
+ */
 export const CONTENT_BLOCK_TYPES = [
 	'core/post-featured-image',
 	'core/post-title',
