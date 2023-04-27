a4fa2a5b3f96a07d6021e3f0a2579d20702ea28a
Gerardo <gerardo.pacheco@automattic.com>
2023-04-27 14:18:25 +0200

    Mobile - Preformatted - Check for existing color attribute within baseColors

--- packages/block-library/src/preformatted/edit.native.js
+++ packages/block-library/src/preformatted/edit.native.js
@@ -29,7 +29,8 @@ export default function PreformattedEdit( props ) {
 		...( style?.color && { color: style.color } ),
 	};
 	const hasBaseColors =
-		style?.baseColors && Object.entries( style.baseColors ).length !== 0;
+		style?.baseColors?.color &&
+		Object.entries( style.baseColors ).length !== 0;
 	const containerStyles = [
 		wpBlockPreformatted,
 		style?.backgroundColor && { backgroundColor: style.backgroundColor },
