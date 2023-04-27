2991d221bde9f5ae766262c63cce9a332787834a
Carlos Garcia <fluiddot@gmail.com>
2023-04-27 18:40:30 +0200

    Force a11y focus on show/hide subsheet

--- packages/components/src/mobile/bottom-sheet/sub-sheet/index.native.js
+++ packages/components/src/mobile/bottom-sheet/sub-sheet/index.native.js
@@ -1,12 +1,24 @@
 /**
  * External dependencies
  */
-import { SafeAreaView } from 'react-native';
+import {
+	AccessibilityInfo,
+	SafeAreaView,
+	View,
+	findNodeHandle,
+	Platform,
+} from 'react-native';
 
 /**
  * WordPress dependencies
  */
-import { Children, useEffect, useContext } from '@wordpress/element';
+import {
+	Children,
+	useEffect,
+	useContext,
+	useRef,
+	useState,
+} from '@wordpress/element';
 import { createSlotFill, BottomSheetContext } from '@wordpress/components';
 
 const { Fill, Slot } = createSlotFill( 'BottomSheetSubSheet' );
@@ -18,6 +30,9 @@ const BottomSheetSubSheet = ( {
 	isFullScreen,
 } ) => {
 	const { setIsFullScreen } = useContext( BottomSheetContext );
+	const [ buttonNode, setButtonNode ] = useState();
+	const [ contentNode, setContentNode ] = useState();
+	const a11yFocusContent = useRef( false );
 
 	useEffect( () => {
 		if ( showSheet ) {
@@ -28,14 +43,61 @@ const BottomSheetSubSheet = ( {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ showSheet, isFullScreen ] );
 
+	useEffect( () => {
+		if ( ! showSheet && a11yFocusContent.current && buttonNode ) {
+			console.log( 'request a11y focus (button)', buttonNode );
+			AccessibilityInfo.setAccessibilityFocus( buttonNode );
+			setContentNode( undefined );
+			a11yFocusContent.current = false;
+		}
+		if ( showSheet && ! a11yFocusContent.current && contentNode ) {
+			console.log( 'request a11y focus (content)', contentNode );
+			AccessibilityInfo.setAccessibilityFocus( contentNode );
+			setButtonNode( undefined );
+			a11yFocusContent.current = true;
+		}
+	}, [ showSheet, buttonNode, contentNode ] );
+
+	const updateButtonNode = ( ref ) => {
+		if ( ! ref ) {
+			return;
+		}
+
+		const node = findNodeHandle( ref );
+		if ( node && node !== buttonNode ) {
+			setButtonNode( node );
+		}
+	};
+
+	const updateContentNode = ( ref ) => {
+		if ( ! ref ) {
+			return;
+		}
+
+		const node = findNodeHandle( ref );
+		if ( node && node !== contentNode ) {
+			setContentNode( node );
+		}
+	};
+
 	return (
 		<>
 			{ showSheet && (
 				<Fill>
-					<SafeAreaView>{ children }</SafeAreaView>
+					<SafeAreaView
+						accessible={ Platform.OS === 'android' }
+						ref={ updateContentNode }
+					>
+						{ children }
+					</SafeAreaView>
 				</Fill>
 			) }
-			{ Children.count( children ) > 0 && navigationButton }
+			<View
+				accessible={ Platform.OS === 'android' }
+				ref={ updateButtonNode }
+			>
+				{ Children.count( children ) > 0 && navigationButton }
+			</View>
 		</>
 	);
 };
