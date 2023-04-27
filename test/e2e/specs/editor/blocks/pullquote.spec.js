82506d1f5a39b261b449f3301988b23ea31cb309
pavanpatil1 <=>
2023-04-27 15:20:49 +0530

    Update the test case as per best practices

--- test/e2e/specs/editor/blocks/pullquote.spec.js
+++ test/e2e/specs/editor/blocks/pullquote.spec.js
@@ -9,17 +9,13 @@ test.describe( 'Quote', () => {
 	} );
 
 	test( 'can be created by converting a quote and converted back to quote', async ( {
-		admin,
 		editor,
 		page,
 	} ) => {
 		await page.click( 'role=button[name="Add default block"i]' );
 
 		await page.keyboard.type( 'test' );
-
-		await page.mouse.move( 50, 50 );
-		await page.mouse.move( 75, 75 );
-		await page.mouse.move( 100, 100 );
+		await editor.showBlockToolbar();
 
 		await page.click( 'role=button[name="Paragraph"i]' );
 
@@ -27,24 +23,23 @@ test.describe( 'Quote', () => {
 			"button[class='components-button components-menu-item__button editor-block-list-item-quote']"
 		);
 
-		expect( await editor.getEditedPostContent() ).toMatchSnapshot( `
-		"<!-- wp:quote -->
-<blockquote class=\"wp-block-quote\">
-<p>test</p>
-</blockquote>
-<!-- /wp:quote -->"
-	` );
+		await expect.poll( editor.getBlocks ).toMatchObject( [
+			{
+				name: 'core/quote',
+			},
+		] );
 
 		await page.click( 'role=button[name="Quote"i]' );
 
 		await page.click(
 			"button[class='components-button components-menu-item__button editor-block-list-item-pullquote']"
 		);
-		expect( await editor.getEditedPostContent() ).toMatchSnapshot( `
-		"<!-- wp:pullquote -->
-<figure class=\\"wp-block-pullquote\\"><blockquote><p>test</p></blockquote></figure>
-<!-- /wp:pullquote -->"
-	` );
+
+		await expect.poll( editor.getBlocks ).toMatchObject( [
+			{
+				name: 'core/pullquote',
+			},
+		] );
 
 		await page.click( 'role=button[name="Pullquote"i]' );
 
@@ -52,12 +47,10 @@ test.describe( 'Quote', () => {
 			"button[class='components-button components-menu-item__button editor-block-list-item-quote']"
 		);
 
-		expect( await editor.getEditedPostContent() ).toMatchSnapshot( `
-		"<!-- wp:quote -->
-<blockquote class=\\"wp-block-quote\\"><!-- wp:paragraph -->
-<p>test</p>
-<!-- /wp:paragraph --></blockquote>
-<!-- /wp:quote -->"
-	` );
+		await expect.poll( editor.getBlocks ).toMatchObject( [
+			{
+				name: 'core/quote',
+			},
+		] );
 	} );
 } );
