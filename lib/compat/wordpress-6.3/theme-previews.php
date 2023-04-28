d9a0ca10f042078b768f98c58aa3f38f7c74903f
scruffian <ben@scruffian.com>
2023-04-28 12:32:59 +0200

    fix PHPCS

--- lib/compat/wordpress-6.3/theme-previews.php
+++ lib/compat/wordpress-6.3/theme-previews.php
@@ -80,13 +80,11 @@ function add_live_preview_button() {
 			const themePath = themeInfo.querySelector('h2.theme-name').id.replace('-name', '');
 			const themeName = themeInfo.querySelector('h2.theme-name').innerText;
 			const livePreviewButton = document.createElement('a');
-			/* translators: %s: theme name */
-			livePreviewButton.setAttribute('aria-label', '
 			<?php
 				/* translators: %s: theme name */
-				echo esc_attr_x( 'Live Preview %s', 'theme' );
+				$button_label = esc_attr_x( 'Live Preview %s', 'theme' );
 			?>
-			'.replace('%s', themeName));
+			livePreviewButton.setAttribute('aria-label', '<?php echo $button_label; ?>'.replace('%s', themeName));
 			livePreviewButton.setAttribute('class', 'button button-primary');
 			livePreviewButton.setAttribute(
 				'href',
