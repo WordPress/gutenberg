7ea8b3d320e2a9db2e0bb7977807b7cd57d5f793
Alex Stine <alex.stine@yourtechadvisors.com>
2023-04-29 01:33:14 -0500

    Add TabPanel to document overview sidebar.

--- packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
+++ packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
@@ -1,13 +1,8 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { __experimentalListView as ListView } from '@wordpress/block-editor';
-import { Button } from '@wordpress/components';
+import { Button, TabPanel } from '@wordpress/components';
 import {
 	useFocusOnMount,
 	useFocusReturn,
@@ -15,7 +10,7 @@ import {
 } from '@wordpress/compose';
 import { useDispatch } from '@wordpress/data';
 import { focus } from '@wordpress/dom';
-import { useRef, useState } from '@wordpress/element';
+import { useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { closeSmall } from '@wordpress/icons';
 import { useShortcut } from '@wordpress/keyboard-shortcuts';
@@ -41,41 +36,35 @@ export default function ListViewSidebar() {
 		}
 	}
 
-	const [ tab, setTab ] = useState( 'list-view' );
-
 	// This ref refers to the sidebar as a whole.
 	const sidebarRef = useRef();
-	// This ref refers to the list view tab button.
-	const listViewTabRef = useRef();
-	// This ref refers to the outline tab button.
-	const outlineTabRef = useRef();
+	// This ref refers to the tab panel.
+	const tabPanelRef = useRef();
 	// This ref refers to the list view application area.
 	const listViewRef = useRef();
 
 	/*
 	 * Callback function to handle list view or outline focus.
 	 *
-	 * @param {string} currentTab The current tab. Either list view or outline.
-	 *
 	 * @return void
 	 */
-	function handleSidebarFocus( currentTab ) {
-		// List view tab is selected.
-		if ( currentTab === 'list-view' ) {
-			// Either focus the list view or the list view tab button. Must have a fallback because the list view does not render when there are no blocks.
-			const listViewApplicationFocus = focus.tabbable.find(
-				listViewRef.current
-			)[ 0 ];
-			const listViewFocusArea = sidebarRef.current.contains(
-				listViewApplicationFocus
-			)
-				? listViewApplicationFocus
-				: listViewTabRef.current;
-			listViewFocusArea.focus();
-			// Outline tab is selected.
-		} else {
-			outlineTabRef.current.focus();
+	function handleSidebarFocus() {
+		// Either focus the list view or the tab panel. Must have a fallback because the list view does not render when there are no blocks.
+		const listViewApplicationFocus = focus.tabbable.find(
+			listViewRef.current
+		)[ 0 ];
+		const listViewFocusArea = sidebarRef.current.contains(
+			listViewApplicationFocus
+		)
+			? listViewApplicationFocus
+			: focus.tabbable.find( tabPanelRef.current )[ 0 ];
+		// Do not focus when focus is already there.
+		if (
+			sidebarRef.current.ownerDocument.activeElement === listViewFocusArea
+		) {
+			return;
 		}
+		listViewFocusArea.focus();
 	}
 
 	// This only fires when the sidebar is open because of the conditional rendering. It is the same shortcut to open but that is defined as a global shortcut and only fires when the sidebar is closed.
@@ -89,9 +78,20 @@ export default function ListViewSidebar() {
 			setIsListViewOpened( false );
 			// If the list view or outline does not have focus, focus should be moved to it.
 		} else {
-			handleSidebarFocus( tab );
+			handleSidebarFocus();
 		}
 	} );
+
+	function renderTabContent( tabName ) {
+		if ( tabName === 'list-view' ) {
+			return (
+				<div className="edit-post-editor__list-view-panel-content">
+					<ListView />
+				</div>
+			);
+		}
+		return <ListViewOutline />;
+	}
 
 	return (
 		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
@@ -109,54 +109,34 @@ export default function ListViewSidebar() {
 					label={ __( 'Close' ) }
 					onClick={ () => setIsListViewOpened( false ) }
 				/>
-				<ul>
-					<li>
-						<Button
-							ref={ listViewTabRef }
-							onClick={ () => {
-								setTab( 'list-view' );
-							} }
-							className={ classnames(
-								'edit-post-sidebar__panel-tab',
-								{ 'is-active': tab === 'list-view' }
-							) }
-							aria-current={ tab === 'list-view' }
-						>
-							{ __( 'List View' ) }
-						</Button>
-					</li>
-					<li>
-						<Button
-							ref={ outlineTabRef }
-							onClick={ () => {
-								setTab( 'outline' );
-							} }
-							className={ classnames(
-								'edit-post-sidebar__panel-tab',
-								{ 'is-active': tab === 'outline' }
-							) }
-							aria-current={ tab === 'outline' }
-						>
-							{ __( 'Outline' ) }
-						</Button>
-					</li>
-				</ul>
 			</div>
-			<div
-				ref={ useMergeRefs( [
+			<TabPanel
+				ref={ tabPanelRef }
+				selectOnMove={ false }
+				itemRef={ useMergeRefs( [
 					contentFocusReturnRef,
 					focusOnMountRef,
 					listViewRef,
 				] ) }
-				className="edit-post-editor__list-view-container"
+				tabs={ [
+					{
+						name: 'list-view',
+						title: 'List View',
+						className: 'edit-post-sidebar__panel-tab',
+					},
+					{
+						name: 'outline',
+						title: 'Outline',
+						className: 'edit-post-sidebar__panel-tab',
+					},
+				] }
 			>
-				{ tab === 'list-view' && (
-					<div className="edit-post-editor__list-view-panel-content">
-						<ListView />
+				{ ( tab ) => (
+					<div className="edit-post-editor__list-view-container">
+						{ renderTabContent( tab.name ) }
 					</div>
 				) }
-				{ tab === 'outline' && <ListViewOutline /> }
-			</div>
+			</TabPanel>
 		</div>
 	);
 }
