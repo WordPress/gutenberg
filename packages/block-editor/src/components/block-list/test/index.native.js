8e44adc876d233473f1b7982176d5dbb5d0c622a
David Calhoun <github@davidcalhoun.me>
2023-05-01 14:28:32 -0400

    test: Restructure BlockList test descriptions
    Increase explicitness of test descriptions and repurpose tests for
    non-compact lists.

--- packages/block-editor/src/components/block-list/test/index.native.js
+++ packages/block-editor/src/components/block-list/test/index.native.js
@@ -13,88 +13,90 @@ import {
 
 setupCoreBlocks( [ 'core/paragraph', 'core/group' ] );
 
-// Note: The Group block leverage BlockListCompact via `useCompact` when
-// rendering its inner blocks, thus it is an appropriate test subject.
-describe( 'BlockListCompact', () => {
-	describe( 'when a non-last block is selected', () => {
-		beforeEach( async () => {
-			// Arrange
-			await initializeEditor();
-			await addBlock( screen, 'Group' );
-			const groupBlock = await getBlock( screen, 'Group' );
-			fireEvent.press(
-				within( groupBlock ).getByTestId( 'appender-button' )
-			);
-			await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
-			fireEvent.press( screen.getByLabelText( 'Navigate Up' ) );
-			fireEvent.press(
-				within( groupBlock ).getByTestId( 'appender-button' )
-			);
-			await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
+describe( 'BlockList', () => {
+	// Note: The Group block leverage BlockListCompact via `useCompact` when
+	// rendering its inner blocks, thus it is an appropriate test subject.
+	describe( 'when compact list enabled', () => {
+		describe( 'when a non-last block is selected', () => {
+			beforeEach( async () => {
+				// Arrange
+				await initializeEditor();
+				await addBlock( screen, 'Group' );
+				const groupBlock = await getBlock( screen, 'Group' );
+				fireEvent.press(
+					within( groupBlock ).getByTestId( 'appender-button' )
+				);
+				await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
+				fireEvent.press( screen.getByLabelText( 'Navigate Up' ) );
+				fireEvent.press(
+					within( groupBlock ).getByTestId( 'appender-button' )
+				);
+				await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
+			} );
+
+			it( 'renders an insertion point before the block', async () => {
+				// Act
+				const paragraphBlock = await getBlock( screen, 'Paragraph', {
+					rowIndex: 1,
+				} );
+				fireEvent.press( paragraphBlock );
+				fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
+				fireEvent.press( screen.getByText( 'Add Block Before ' ) );
+
+				// Assert
+				expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
+				expect(
+					screen.getByTestId( 'block-insertion-point-before-row-1' )
+				).toBeTruthy();
+			} );
+
+			it( 'renders an insertion point after the block', async () => {
+				// Act
+				const paragraphBlock = await getBlock( screen, 'Paragraph', {
+					rowIndex: 1,
+				} );
+				fireEvent.press( paragraphBlock );
+				fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
+				fireEvent.press( screen.getByText( 'Add Block After ' ) );
+
+				// Assert
+				expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
+				expect(
+					screen.getByTestId( 'block-insertion-point-before-row-2' )
+				).toBeTruthy();
+			} );
 		} );
 
-		it( 'renders an insertion point before the block', async () => {
-			// Act
-			const paragraphBlock = await getBlock( screen, 'Paragraph', {
-				rowIndex: 1,
+		describe( 'when the last block is selected', () => {
+			it( 'renders an insertion point after the block', async () => {
+				// Arrange
+				await initializeEditor();
+				await addBlock( screen, 'Group' );
+				const groupBlock = await getBlock( screen, 'Group' );
+				fireEvent.press(
+					within( groupBlock ).getByTestId( 'appender-button' )
+				);
+				await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
+				fireEvent.press( screen.getByLabelText( 'Navigate Up' ) );
+				fireEvent.press(
+					within( groupBlock ).getByTestId( 'appender-button' )
+				);
+				await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
+
+				// Act
+				const paragraphBlock = await getBlock( screen, 'Paragraph', {
+					rowIndex: 2,
+				} );
+				fireEvent.press( paragraphBlock );
+				fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
+				fireEvent.press( screen.getByText( 'Add Block After ' ) );
+
+				// Assert
+				expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
+				expect(
+					screen.getByTestId( 'block-insertion-point-after-row-2' )
+				).toBeTruthy();
 			} );
-			fireEvent.press( paragraphBlock );
-			fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
-			fireEvent.press( screen.getByText( 'Add Block Before ' ) );
-
-			// Assert
-			expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
-			expect(
-				screen.getByTestId( 'block-insertion-point-before-row-1' )
-			).toBeTruthy();
-		} );
-
-		it( 'renders an insertion point after the block', async () => {
-			// Act
-			const paragraphBlock = await getBlock( screen, 'Paragraph', {
-				rowIndex: 1,
-			} );
-			fireEvent.press( paragraphBlock );
-			fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
-			fireEvent.press( screen.getByText( 'Add Block After ' ) );
-
-			// Assert
-			expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
-			expect(
-				screen.getByTestId( 'block-insertion-point-before-row-2' )
-			).toBeTruthy();
-		} );
-	} );
-
-	describe( 'when the last block is selected', () => {
-		it( 'renders an insertion point after the block', async () => {
-			// Arrange
-			await initializeEditor();
-			await addBlock( screen, 'Group' );
-			const groupBlock = await getBlock( screen, 'Group' );
-			fireEvent.press(
-				within( groupBlock ).getByTestId( 'appender-button' )
-			);
-			await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
-			fireEvent.press( screen.getByLabelText( 'Navigate Up' ) );
-			fireEvent.press(
-				within( groupBlock ).getByTestId( 'appender-button' )
-			);
-			await addBlock( screen, 'Paragraph', { isPickerOpened: true } );
-
-			// Act
-			const paragraphBlock = await getBlock( screen, 'Paragraph', {
-				rowIndex: 2,
-			} );
-			fireEvent.press( paragraphBlock );
-			fireEvent( screen.getByLabelText( 'Add block' ), 'longPress' );
-			fireEvent.press( screen.getByText( 'Add Block After ' ) );
-
-			// Assert
-			expect( screen.getByText( 'ADD BLOCK HERE' ) ).toBeTruthy();
-			expect(
-				screen.getByTestId( 'block-insertion-point-after-row-2' )
-			).toBeTruthy();
 		} );
 	} );
 } );
