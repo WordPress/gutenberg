2702df3f1cde9d976e58b9921650c63268d70674
Martin Jost <31563167+CreativeDive@users.noreply.github.com>
2023-05-02 08:51:27 +0200

    Add classNames to settings menu dropdown

--- packages/block-editor/src/components/block-settings-menu/block-settings-dropdown.js
+++ packages/block-editor/src/components/block-settings-menu/block-settings-dropdown.js
@@ -43,7 +43,10 @@ function CopyMenuItem( { blocks, onCopy, label } ) {
 	const copyMenuItemBlocksLabel =
 		blocks.length > 1 ? __( 'Copy blocks' ) : __( 'Copy block' );
 	const copyMenuItemLabel = label ? label : copyMenuItemBlocksLabel;
-	return <MenuItem ref={ ref }>{ copyMenuItemLabel }</MenuItem>;
+	return <MenuItem 
+		ref={ ref }
+		className={ 'block-action-copy' }
+	>{ copyMenuItemLabel }</MenuItem>;
 }
 
 export function BlockSettingsDropdown( {
@@ -233,6 +236,7 @@ export function BlockSettingsDropdown( {
 													}
 												/>
 											}
+											className={ 'block-action-select-parent' }
 											onClick={ () =>
 												selectBlock(
 													firstParentClientId
@@ -259,6 +263,7 @@ export function BlockSettingsDropdown( {
 								/>
 								{ canDuplicate && (
 									<MenuItem
+										className={ 'block-action-duplicate' }
 										onClick={ pipe(
 											onClose,
 											onDuplicate,
@@ -272,6 +277,7 @@ export function BlockSettingsDropdown( {
 								{ canInsertDefaultBlock && (
 									<>
 										<MenuItem
+											className={ 'block-action-insert-before' }
 											onClick={ pipe(
 												onClose,
 												onInsertBefore
@@ -281,6 +287,7 @@ export function BlockSettingsDropdown( {
 											{ __( 'Insert before' ) }
 										</MenuItem>
 										<MenuItem
+											className={ 'block-action-insert-after' }
 											onClick={ pipe(
 												onClose,
 												onInsertAfter
@@ -293,6 +300,7 @@ export function BlockSettingsDropdown( {
 								) }
 								{ canMove && ! onlyBlock && (
 									<MenuItem
+										className={ 'block-action-move-to' }
 										onClick={ pipe( onClose, onMoveTo ) }
 									>
 										{ __( 'Move to' ) }
@@ -311,7 +319,10 @@ export function BlockSettingsDropdown( {
 									onCopy={ onCopy }
 									label={ __( 'Copy styles' ) }
 								/>
-								<MenuItem onClick={ onPasteStyles }>
+								<MenuItem 
+									className={ 'block-action-paste-styles' }
+									onClick={ onPasteStyles }
+								>
 									{ __( 'Paste styles' ) }
 								</MenuItem>
 							</MenuGroup>
@@ -330,6 +341,7 @@ export function BlockSettingsDropdown( {
 							{ canRemove && (
 								<MenuGroup>
 									<MenuItem
+										className={ 'block-action-delete' }
 										onClick={ pipe(
 											onClose,
 											onRemove,
