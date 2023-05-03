177b85a29f31b95f1cab0a166d936d3fd2a36f5c
Glen Davies <glen.davies@automattic.com>
2023-05-03 16:12:27 +1200

    Fix issue with block invalidation on first load

--- packages/block-library/src/pattern/save.js
+++ packages/block-library/src/pattern/save.js
@@ -3,7 +3,13 @@
  */
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
-export default function save( { attributes: { inheritedAlignment } } ) {
+export default function save( {
+	attributes: { inheritedAlignment },
+	innerBlocks,
+} ) {
+	if ( innerBlocks?.length === 0 ) {
+		return;
+	}
 	const blockProps = useBlockProps.save( {
 		className: inheritedAlignment && `align${ inheritedAlignment }`,
 	} );
