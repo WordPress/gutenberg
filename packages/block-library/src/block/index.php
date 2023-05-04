174c5fa89730dc6680df0e9b92ac50ed5803f086
Daniel Richards <daniel.richards@automattic.com>
2023-05-04 15:42:01 +0800

    Update dynamic block code

--- packages/block-library/src/block/index.php
+++ packages/block-library/src/block/index.php
@@ -13,42 +13,63 @@
  * @return string Rendered HTML of the referenced block.
  */
 function render_block_core_block( $attributes ) {
-	static $seen_refs = array();
+	if ( 'reusable' === $attributes['type'] ) {
+		static $seen_refs = array();
 
-	if ( empty( $attributes['ref'] ) ) {
-		return '';
+		if ( empty( $attributes['ref'] ) ) {
+			return '';
+		}
+
+		$reusable_block = get_post( $attributes['ref'] );
+		if ( ! $reusable_block || 'wp_block' !== $reusable_block->post_type ) {
+			return '';
+		}
+
+		if ( isset( $seen_refs[ $attributes['ref'] ] ) ) {
+			// WP_DEBUG_DISPLAY must only be honored when WP_DEBUG. This precedent
+			// is set in `wp_debug_mode()`.
+			$is_debug = WP_DEBUG && WP_DEBUG_DISPLAY;
+
+			return $is_debug ?
+				// translators: Visible only in the front end, this warning takes the place of a faulty block.
+				__( '[block rendering halted]' ) :
+				'';
+		}
+
+		if ( 'publish' !== $reusable_block->post_status || ! empty( $reusable_block->post_password ) ) {
+			return '';
+		}
+
+		$seen_refs[ $attributes['ref'] ] = true;
+
+		// Handle embeds for reusable blocks.
+		global $wp_embed;
+		$content = $wp_embed->run_shortcode( $reusable_block->post_content );
+		$content = $wp_embed->autoembed( $content );
+
+		$content = do_blocks( $content );
+		unset( $seen_refs[ $attributes['ref'] ] );
+		return $content;
 	}
 
-	$reusable_block = get_post( $attributes['ref'] );
-	if ( ! $reusable_block || 'wp_block' !== $reusable_block->post_type ) {
-		return '';
+	if ( 'pattern' === $attributes['type'] ) {
+		if ( empty( $attributes['slug'] ) ) {
+			return '';
+		}
+
+		if ( isset( $attributes['syncStatus'] ) && 'unsynced' === $attributes['syncStatus'] ) {
+			return sprintf( $wrapper, $content );
+		}
+
+		$slug     = $attributes['slug'];
+		$registry = WP_Block_Patterns_Registry::get_instance();
+		if ( ! $registry->is_registered( $slug ) ) {
+			return '';
+		}
+
+		$pattern = $registry->get_registered( $slug );
+		return do_blocks( $pattern['content'] );
 	}
-
-	if ( isset( $seen_refs[ $attributes['ref'] ] ) ) {
-		// WP_DEBUG_DISPLAY must only be honored when WP_DEBUG. This precedent
-		// is set in `wp_debug_mode()`.
-		$is_debug = WP_DEBUG && WP_DEBUG_DISPLAY;
-
-		return $is_debug ?
-			// translators: Visible only in the front end, this warning takes the place of a faulty block.
-			__( '[block rendering halted]' ) :
-			'';
-	}
-
-	if ( 'publish' !== $reusable_block->post_status || ! empty( $reusable_block->post_password ) ) {
-		return '';
-	}
-
-	$seen_refs[ $attributes['ref'] ] = true;
-
-	// Handle embeds for reusable blocks.
-	global $wp_embed;
-	$content = $wp_embed->run_shortcode( $reusable_block->post_content );
-	$content = $wp_embed->autoembed( $content );
-
-	$content = do_blocks( $content );
-	unset( $seen_refs[ $attributes['ref'] ] );
-	return $content;
 }
 
 /**
