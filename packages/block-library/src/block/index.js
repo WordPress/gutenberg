f9a893e9f6fb405f9093359f4614275092f19c82
Daniel Richards <daniel.richards@automattic.com>
2023-05-04 16:26:50 +0800

    Show pattern name in list view

--- packages/block-library/src/block/index.js
+++ packages/block-library/src/block/index.js
@@ -1,7 +1,10 @@
 /**
  * WordPress dependencies
  */
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { select } from '@wordpress/data';
 import { symbol as icon } from '@wordpress/icons';
+import { decodeEntities } from '@wordpress/html-entities';
 
 /**
  * Internal dependencies
@@ -21,6 +24,21 @@ export const settings = {
 	save,
 	icon,
 	variations,
+	__experimentalLabel: ( { type, slug } ) => {
+		// Attempt to find entity title if block is a template part.
+		// Require slug to request, otherwise entity is uncreated and will throw 404.
+		if ( type !== 'pattern' || ! slug ) {
+			return;
+		}
+
+		const entity =
+			select( blockEditorStore ).__experimentalGetParsedPattern( slug );
+		if ( ! entity ) {
+			return;
+		}
+
+		return entity.title ? decodeEntities( entity.title ) : undefined;
+	},
 };
 
 export const init = () => initBlock( { name, metadata, settings } );
