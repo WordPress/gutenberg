27eb2b628156ec7af8b5aacbfcf1aca504559d63
Gerardo <gerardo.pacheco@automattic.com>
2023-05-04 16:36:59 +0200

    Mobile - BlockList - Block: Add missing isSelected dependency

--- packages/block-editor/src/components/block-list/block.native.js
+++ packages/block-editor/src/components/block-list/block.native.js
@@ -229,7 +229,7 @@ function BlockListBlock( {
 				order: blockOrder,
 			};
 		},
-		[ clientId, name, rootClientId ]
+		[ clientId, isSelected, name, rootClientId ]
 	);
 	const { removeBlock, selectBlock } = useDispatch( blockEditorStore );
 	const parentLayout = useLayout() || {};
