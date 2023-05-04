81b147f583c96aaed2be400f855ff3798495d945
Daniel Richards <daniel.richards@automattic.com>
2023-05-04 15:50:41 +0800

    Convert patterns to blocks

--- packages/block-library/src/pattern/edit.js
+++ packages/block-library/src/pattern/edit.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { createBlock } from '@wordpress/blocks';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
 import {
@@ -9,23 +10,22 @@ import {
 } from '@wordpress/block-editor';
 
 const PatternEdit = ( { attributes, clientId } ) => {
-	const selectedPattern = useSelect(
+	const { slug } = attributes;
+	const { blocks: patternBlocks } = useSelect(
 		( select ) =>
-			select( blockEditorStore ).__experimentalGetParsedPattern(
-				attributes.slug
-			),
-		[ attributes.slug ]
+			select( blockEditorStore ).__experimentalGetParsedPattern( slug ),
+		[ slug ]
 	);
 
 	const { replaceBlocks, __unstableMarkNextChangeAsNotPersistent } =
 		useDispatch( blockEditorStore );
 
 	// Run this effect when the component loads.
-	// This adds the Pattern's contents to the post.
+	// This converts a pattern in content to wp:block type.
 	// This change won't be saved.
 	// It will continue to pull from the pattern file unless changes are made to its respective template part.
 	useEffect( () => {
-		if ( selectedPattern?.blocks ) {
+		if ( patternBlocks ) {
 			// We batch updates to block list settings to avoid triggering cascading renders
 			// for each container block included in a tree and optimize initial render.
 			// Since the above uses microtasks, we need to use a microtask here as well,
@@ -33,10 +33,18 @@ const PatternEdit = ( { attributes, clientId } ) => {
 			// inner blocks but doesn't have blockSettings in the state.
 			window.queueMicrotask( () => {
 				__unstableMarkNextChangeAsNotPersistent();
-				replaceBlocks( clientId, selectedPattern.blocks );
+				const block = createBlock(
+					'core/block',
+					{
+						type: 'pattern',
+						slug,
+					},
+					patternBlocks
+				);
+				replaceBlocks( clientId, block );
 			} );
 		}
-	}, [ clientId, selectedPattern?.blocks ] );
+	}, [ clientId, patternBlocks, slug ] );
 
 	const props = useBlockProps();
 
