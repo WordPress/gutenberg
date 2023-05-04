ec6fb41d370774fac360e81d4a193d7585df6720
scruffian <ben@scruffian.com>
2023-05-04 15:59:55 +0100

    fix PHPCS

--- lib/compat/wordpress-6.3/theme-previews.php
+++ lib/compat/wordpress-6.3/theme-previews.php
@@ -5,12 +5,12 @@
  * @package gutenberg
  */
 
- /**
-  * Filters the blog option to return the path for the previewed theme.
-  *
-  * @param string $current_stylesheet The current theme's stylesheet or template path.
-  * @return string The previewed theme's stylesheet or template path.
-  */
+/**
+ * Filters the blog option to return the path for the previewed theme.
+ *
+ * @param string $current_stylesheet The current theme's stylesheet or template path.
+ * @return string The previewed theme's stylesheet or template path.
+ */
 function gutenberg_get_theme_preview_path( $current_stylesheet = null ) {
 	// Don't allow non-admins to preview themes.
 	if ( ! current_user_can( 'switch_themes' ) ) {
