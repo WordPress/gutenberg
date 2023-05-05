9063c1c8fdb39c4041c8a4bc68bf0138a790aeae
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-05-05 16:32:13 -0700

    Fixed `apt-get` install

--- packages/env/lib/init-config.js
+++ packages/env/lib/init-config.js
@@ -233,7 +233,7 @@ function installDependencies( environment, config ) {
 RUN apt-get -qy update
 
 # Install some basic PHP dependencies.
-RUN apt-get -qy install$PHPIZE_DEPS && touch /usr/local/etc/php/php.ini
+RUN apt-get -qy install $PHPIZE_DEPS && touch /usr/local/etc/php/php.ini
 
 # Set up sudo so they can have root access.
 RUN apt-get -qy install sudo
