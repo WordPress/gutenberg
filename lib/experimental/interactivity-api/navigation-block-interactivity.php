b553b047d3d89ea72c5406ee57e677e23ac67d4c
Mario Santos <santosguillamot@gmail.com>
2023-05-05 10:02:04 +0200

    Add comments with the markup to the PHP filter

--- lib/experimental/interactivity-api/navigation-block-interactivity.php
+++ lib/experimental/interactivity-api/navigation-block-interactivity.php
@@ -9,6 +9,49 @@
 $gutenberg_experiments = get_option( 'gutenberg-experiments' );
 if ( gutenberg_is_experiment_enabled( 'gutenberg-interactivity-api-navigation-block' ) ) {
 	function gutenberg_block_core_navigation_add_directives_to_markup( $block_content ) {
+		/**
+		 * Interactivity API directives are added using the Tag Processor while it is experimental.
+		 * The final HTML of the navigation block will look similar to this:
+		 * 
+		 * <nav
+		 *   data-wp-island
+		 *   data-wp-context='{ "core": { "navigation": { "isMenuOpen": false, "overlay": true, "roleAttribute": "" } } }'
+		 * >
+		 *   <button
+		 *     class="wp-block-navigation__responsive-container-open"
+		 *     data-wp-on.click="actions.core.navigation.openMenu"
+		 *     data-wp-on.keydown="actions.core.navigation.handleMenuKeydown"
+		 *   >
+		 *   <div
+		 *     class="wp-block-navigation__responsive-container"
+		 *     data-wp-class.has-modal-open="context.core.navigation.isMenuOpen"
+		 *     data-wp-class.is-menu-open="context.core.navigation.isMenuOpen"
+		 *     data-wp-bind.aria-hidden="!context.core.navigation.isMenuOpen"
+		 *     data-wp-effect="effects.core.navigation.initModal"
+		 *     data-wp-on.keydow="actions.core.navigation.handleMenuKeydown"
+		 *     data-wp-on.focusout="actions.core.navigation.handleMenuFocusout"
+		 *     tabindex="-1"
+		 *   >
+		 *     <div class="wp-block-navigation__responsive-close">
+		 *       <div
+		 *         class="wp-block-navigation__responsive-dialog"
+		 *         data-wp-bind.aria-modal="context.core.navigation.isMenuOpen"
+		 *         data-wp-bind.role="selectors.core.navigation.roleAttribute"
+		 *         data-wp-effect="effects.core.navigation.focusFirstElement"   
+		 *       >
+		 *         <button
+		 *           class="wp-block-navigation__responsive-container-close"
+		 *           data-wp-on.click="actions.core.navigation.closeMenu"
+		 *         >
+		 *           <svg>
+		 *         <button>
+		 *         MENU ITEMS
+		 *       </div>
+		 *     </div>
+		 *   </div>
+		 * </nav>
+		 */
+
 		$w = new WP_HTML_Tag_Processor( $block_content );
 		// Add directives to the `<nav>` element.
 		if ( $w->next_tag( 'nav' ) ) {
@@ -72,6 +115,34 @@ if ( gutenberg_is_experiment_enabled( 'gutenberg-interactivity-api-navigation-bl
 	};
 
 	function gutenberg_block_core_navigation_add_directives_to_submenu( $w ) {
+		/**
+		 * Interactivity API directives are added using the Tag Processor while it is experimental.
+		 * The final HTML of the navigation-submenu and the page-list blocks will look similar to this:
+		 * 
+		 * <li
+		 *   class="has-child"
+		 *   data-wp-context='{ "core": { "navigation": { "isMenuOpen": false, "overlay": false } } }'
+		 * >
+		 *   <button
+		 *     class="wp-block-navigation-submenu__toggle"
+		 *     data-wp-on.click="actions.core.navigation.openMenu"
+		 *     data-wp-bind.aria-expanded="context.core.navigation.isMenuOpen"
+		 *     data-wp-on.keydown="actions.core.navigation.handleMenuKeydown"
+		 *     data-wp-on.focusout="actions.core.navigation.handleMenuFocusout"
+		 *   >
+		 *   </button>
+		 *   <span>Title</span>
+		 *   <ul
+		 *     class="wp-block-navigation__submenu-container"
+		 *     data-wp-effect="effects.core.navigation.initModal"
+		 *     data-wp-on.focusout="actions.core.navigation.handleMenuFocusout"
+		 *     data-wp-on.keydown="actions.core.navigation.handleMenuKeydown"
+		 *   >
+		 *     SUBMENU ITEMS
+		 *   </ul>
+		 * </li>
+		 */
+
 		while ( $w->next_tag(
 			array(
 				'tag_name'   => 'LI',
