f52d567a18cd51c386bee6a17b11b5d06175e8fa
Marin Atanasov <tyxla@abv.bg>
2023-05-08 14:16:17 +0300

    Move experiment toggle to the higheest priority

--- lib/experimental/disable-tinymce.php
+++ lib/experimental/disable-tinymce.php
@@ -1,15 +1,17 @@
 <?php
 
+$gutenberg_experiments = get_option( 'gutenberg-experiments' );
+if ( ! $gutenberg_experiments || ! array_key_exists( 'gutenberg-no-tinymce', $gutenberg_experiments ) ) {
+	return;
+}
+
 if ( isset( $_COOKIE[ 'requiresTinymce' ] ) ) {
 	return;
 }
 
 add_action( 'admin_init', 'gutenberg_disable_tinymce' );
 function gutenberg_disable_tinymce() {
-	$gutenberg_experiments = get_option( 'gutenberg-experiments' );
-	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-no-tinymce', $gutenberg_experiments ) ) {
-		require __DIR__ . '/wp-editors-class-placeholder.php';
-	}
+	require __DIR__ . '/wp-editors-class-placeholder.php';
 }
 
 add_action( 'admin_enqueue_scripts', 'gutenberg_enqueue_tinymce_proxy' );
