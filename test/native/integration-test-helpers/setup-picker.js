5c0fcb666fdfd95be9ab53f2dc50aa71eb5e8da6
Siobhan <siobhan@automattic.com>
2023-05-10 15:30:10 +0100

    refactor: Rename new arg for clarity

--- test/native/integration-test-helpers/setup-picker.js
+++ test/native/integration-test-helpers/setup-picker.js
@@ -13,21 +13,21 @@ import { ActionSheetIOS } from 'react-native';
  * Sets up the Picker component for testing.
  *
  * @typedef {Object} PickerMockFunctions
- * @property {Function}                                          selectOption      Selects one of the options of the picker.
+ * @property {Function}                                          selectOption          Selects one of the options of the picker.
  *
- * @param    {import('@testing-library/react-native').RenderAPI} screen            A Testing Library screen.
- * @param    {string[]}                                          options           Array with the options of the picker.
- * @param    {Function}                                          onPickerTriggered Optional function when a picker is manually triggered.
+ * @param    {import('@testing-library/react-native').RenderAPI} screen                A Testing Library screen.
+ * @param    {string[]}                                          options               Array with the options of the picker.
+ * @param    {Function}                                          onPickerButtonPressed Optional function when a picker is manually triggered via a button.
  *
  * @return {PickerMockFunctions} Picker functions.
  */
 export async function setupPicker(
 	screen,
 	options,
-	onPickerTriggered = () => {}
+	onPickerButtonPressed = () => {}
 ) {
-	await onPickerTriggered();
 	let selectOption = async ( option ) => {
+		await onPickerButtonPressed();
 		fireEvent.press( screen.getByText( option ) );
 	};
 	if ( Platform.isIOS ) {
