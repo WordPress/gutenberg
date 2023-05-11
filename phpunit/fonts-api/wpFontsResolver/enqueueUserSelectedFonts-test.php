f87724b2a267e0112747a4c96014956c00dd75f4
hellofromtonya <tonya.mork@automattic.com>
2023-05-11 12:54:07 -0500

    Adds print tests. Moves datasets to trait.

--- phpunit/fonts-api/wpFontsResolver/enqueueUserSelectedFonts-test.php
+++ phpunit/fonts-api/wpFontsResolver/enqueueUserSelectedFonts-test.php
@@ -13,13 +13,6 @@ require_once __DIR__ . '/../wp-fonts-testcase.php';
  * @covers WP_Fonts_Resolver::enqueue_user_selected_fonts
  */
 class Tests_Fonts_WpFontsResolver_EnqueueUserSelectedFonts extends WP_Fonts_TestCase {
-	const FONTS_THEME = 'fonts-block-theme';
-	/**
-	 * Administrator ID.
-	 *
-	 * @var int
-	 */
-	private static $administrator_id = 0;
 
 	public static function set_up_before_class() {
 		self::$requires_switch_theme_fixtures = true;
@@ -99,158 +92,40 @@ class Tests_Fonts_WpFontsResolver_EnqueueUserSelectedFonts extends WP_Fonts_Test
 	 * @return array
 	 */
 	public function data_should_enqueue_when_user_selected_fonts() {
-		$fonts = array(
-			'font1-400-normal' => array(
-				'fontFamily' => 'var:preset|font-family|font1',
-				'fontStyle'  => 'normal',
-				'fontWeight' => '400',
-			),
-			'font1-400-italic' => array(
-				'fontFamily' => 'var:preset|font-family|font1',
-				'fontStyle'  => 'italic',
-				'fontWeight' => '400',
-			),
-			'font2-600-normal' => array(
-				'fontFamily' => 'var:preset|font-family|font2',
-				'fontStyle'  => 'normal',
-				'fontWeight' => '600',
-			),
-			'font2-600-italic' => array(
-				'fontFamily' => 'var:preset|font-family|font2',
-				'fontStyle'  => 'italic',
-				'fontWeight' => '600',
-			),
-			'font3-900-normal' => array(
-				'fontFamily' => 'var:preset|font-family|font3',
-				'fontStyle'  => 'normal',
-				'fontWeight' => '900',
-			),
-		);
+		$global_styles = $this->get_mock_user_selected_fonts_global_styles();
 
 		return array(
-			'link'                  => array(
-				'styles'   => array(
-					'elements' => array(
-						'link' => array(
-							'typography' => $fonts['font1-400-italic'],
-						),
-					),
-				),
+			'heading, caption, text'   => array(
+				'styles'   => $global_styles['font1'],
 				'expected' => array( 'font1' ),
 			),
-			'heading'               => array(
-				'styles'   => array(
-					'elements' => array(
-						'heading' => array(
-							'typography' => $fonts['font2-600-italic'],
-						),
-					),
-				),
+			'heading, button'          => array(
+				'styles'   => $global_styles['font2'],
 				'expected' => array( 'font2' ),
 			),
-			'caption'               => array(
-				'styles'   => array(
-					'elements' => array(
-						'caption' => array(
-							'typography' => $fonts['font2-600-normal'],
-						),
-					),
-				),
-				'expected' => array( 'font2' ),
+			'text'                     => array(
+				'styles'   => $global_styles['font3'],
+				'expected' => array( 'font3' ),
 			),
-			'button'                => array(
-				'styles'   => array(
-					'elements' => array(
-						'button' => array(
-							'typography' => $fonts['font1-400-normal'],
-						),
-					),
+			'all'                      => array(
+				'styles'   => $global_styles['all'],
+				'expected' => array(
+					0 => 'font1',
+					// font1 occurs 2 more times and gets removed as duplicates.
+					3 => 'font2',
+					4 => 'font3',
 				),
-				'expected' => array( 'font1' ),
 			),
-			'text'                  => array(
-				'styles'   => array(
-					'typography' => $fonts['font1-400-normal'],
+			'all with invalid element' => array(
+				'styles'   => $global_styles['all with invalid element'],
+				'expected' => array(
+					0 => 'font1',
+					// font1 occurs 2 more times and gets removed as duplicates.
+					3 => 'font2',
+					// Skips font2 for the "invalid" element.
+					4 => 'font3',
 				),
-				'expected' => array( 'font1' ),
-			),
-			'all elements'          => array(
-				'styles'   => array(
-					'elements' => array(
-						'link'    => array(
-							'typography' => $fonts['font1-400-italic'],
-						),
-						'heading' => array(
-							'typography' => $fonts['font2-600-italic'],
-						),
-						'caption' => array(
-							'typography' => $fonts['font2-600-normal'],
-						),
-						'button'  => array(
-							'typography' => $fonts['font1-400-normal'],
-						),
-					),
-				),
-				'expected' => array( 'font1', 'font2' ),
-			),
-			'all elements and text' => array(
-				'styles'   => array(
-					'elements'   => array(
-						'link'    => array(
-							'typography' => $fonts['font1-400-italic'],
-						),
-						'heading' => array(
-							'typography' => $fonts['font2-600-italic'],
-						),
-						'caption' => array(
-							'typography' => $fonts['font3-900-normal'],
-						),
-						'button'  => array(
-							'typography' => $fonts['font1-400-normal'],
-						),
-					),
-					'typography' => $fonts['font1-400-normal'],
-				),
-				'expected' => array( 'font1', 'font2', 'font3' ),
-			),
-			'with invalid element'  => array(
-				'styles'   => array(
-					'elements' => array(
-						'button'  => array(
-							'typography' => $fonts['font1-400-normal'],
-						),
-						'invalid' => array(
-							'typography' => $fonts['font3-900-normal'],
-						),
-					),
-				),
-				'expected' => array( 'font1' ),
 			),
 		);
-	}
-
-	/**
-	 * Sets up the global styles.
-	 *
-	 * @param array $styles User-selected styles structure.
-	 */
-	private function set_up_global_styles( array $styles ) {
-		switch_theme( static::FONTS_THEME );
-
-		if ( empty( $styles ) ) {
-			return;
-		}
-
-		// Make sure there is data from the user origin.
-		wp_set_current_user( self::$administrator_id );
-		$user_cpt = WP_Theme_JSON_Resolver::get_user_data_from_wp_global_styles( wp_get_theme(), true );
-		$config   = json_decode( $user_cpt['post_content'], true );
-
-		// Add the test styles.
-		$config['styles'] = $styles;
-
-		// Update the global styles and settings post.
-		$user_cpt['post_content'] = wp_json_encode( $config );
-		wp_update_post( $user_cpt, true, false );
 	}
 }
