14c440bafc4735a7f4f3ef9193a79d64d42c2323
Ari Stathopoulos <aristath@gmail.com>
2023-05-12 09:07:51 +0300

    CS: phpcs can't detect the filedoc

--- packages/block-serialization-default-parser/parser.php
+++ packages/block-serialization-default-parser/parser.php
@@ -5,6 +5,7 @@
  * @package WordPress
  */
 
+// Require files.
 require_once __DIR__ . '/class-wp-block-parser-block.php';
 require_once __DIR__ . '/class-wp-block-parser-frame.php';
 require_once __DIR__ . '/class-wp-block-parser.php';
