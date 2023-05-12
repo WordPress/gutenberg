b1634477649db2049fa7132b7f8f5e06980dc10a
Dave Smith <getdavemail@gmail.com>
2023-05-12 11:57:55 +0100

    Tidy up scaffolded test

--- packages/e2e-tests/specs/editor/plugins/inner-blocks-prioritized-inserter-blocks.test.js
+++ packages/e2e-tests/specs/editor/plugins/inner-blocks-prioritized-inserter-blocks.test.js
@@ -7,14 +7,13 @@ import {
 	deactivatePlugin,
 	getAllBlockInserterItemTitles,
 	insertBlock,
-	openGlobalBlockInserter,
 	closeGlobalBlockInserter,
-	clickBlockToolbarButton,
 } from '@wordpress/e2e-test-utils';
 
+const QUICK_INSERTER_RESULTS_SELECTOR =
+	'.block-editor-inserter__quick-inserter-results';
+
 describe( 'Prioritized Inserter Blocks Setting on InnerBlocks', () => {
-	const imageBlockSelector =
-		'.block-editor-rich-text__editable[data-type="core/paragraph"]';
 	beforeAll( async () => {
 		await activatePlugin(
 			'gutenberg-test-innerblocks-prioritized-inserter-blocks'
@@ -31,7 +30,7 @@ describe( 'Prioritized Inserter Blocks Setting on InnerBlocks', () => {
 		);
 	} );
 
-	it( 'allows all blocks if the allowed blocks setting was not set', async () => {
+	it( 'uses defaulting ordering if prioritzed blocks setting was not set', async () => {
 		const parentBlockSelector =
 			'[data-type="test/prioritized-inserter-blocks-unset"]';
 		await insertBlock( 'Prioritized Inserter Blocks Unset' );
@@ -40,12 +39,10 @@ describe( 'Prioritized Inserter Blocks Setting on InnerBlocks', () => {
 		await page.waitForSelector( parentBlockSelector );
 
 		await page.click(
-			`[data-type="test/prioritized-inserter-blocks-unset"] .block-list-appender .block-editor-inserter__toggle`
+			`${ parentBlockSelector } .block-list-appender .block-editor-inserter__toggle`
 		);
 
-		await page.waitForSelector(
-			'.block-editor-inserter__quick-inserter-results'
-		);
+		await page.waitForSelector( QUICK_INSERTER_RESULTS_SELECTOR );
 
 		await expect( await getAllBlockInserterItemTitles() ).toHaveLength( 6 );
 	} );
