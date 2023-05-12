e286463b9868adc4a409ab209730c032c9a1d181
Daniel Richards <daniel.richards@automattic.com>
2023-05-12 13:29:10 +0800

    Keep block fully dynamic for now by removing save.js

--- packages/block-library/src/pattern/index.js
+++ packages/block-library/src/pattern/index.js
@@ -5,13 +5,12 @@ import initBlock from '../utils/init-block';
 import metadata from './block.json';
 import PatternEditV1 from './v1/edit';
 import PatternEditV2 from './edit';
-import PatternSave from './save';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = window?.__experimentalEnablePatternEnhancements
-	? { edit: PatternEditV2, save: PatternSave }
+	? { edit: PatternEditV2 }
 	: { edit: PatternEditV1 };
 
 export const init = () => initBlock( { name, metadata, settings } );
