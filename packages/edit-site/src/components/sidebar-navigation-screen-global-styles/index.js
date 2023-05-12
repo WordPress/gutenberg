40afb5370a08173e4f6fedb05b3e5bd4aa253ff2
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-05-12 13:45:09 +1000

    Styles Navigation Screen: Add button to open Style Book

--- packages/edit-site/src/components/sidebar-navigation-screen-global-styles/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-global-styles/index.js
@@ -1,8 +1,9 @@
 /**
  * WordPress dependencies
  */
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { edit } from '@wordpress/icons';
+import { edit, seen } from '@wordpress/icons';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { __experimentalNavigatorButton as NavigatorButton } from '@wordpress/components';
@@ -51,7 +52,20 @@ export function SidebarNavigationItemGlobalStyles( props ) {
 
 export default function SidebarNavigationScreenGlobalStyles() {
 	const { openGeneralSidebar } = useDispatch( editSiteStore );
-	const { setCanvasMode } = unlock( useDispatch( editSiteStore ) );
+	const { setCanvasMode, setEditorCanvasContainerView } = unlock(
+		useDispatch( editSiteStore )
+	);
+	const [ isStyleBookOpened, setIsStyleBookOpened ] = useState( false );
+
+	// When the style book is opened, switch to the style book view.
+	// This is done in a useEffect to ensure that the canvas mode is changed,
+	// and the global styles sidebar is opened before attempting to open the style book.
+	useEffect( () => {
+		if ( isStyleBookOpened ) {
+			setEditorCanvasContainerView( 'style-book' );
+		}
+	}, [ setEditorCanvasContainerView, isStyleBookOpened ] );
+
 	return (
 		<SidebarNavigationScreen
 			title={ __( 'Styles' ) }
@@ -60,16 +74,32 @@ export default function SidebarNavigationScreenGlobalStyles() {
 			) }
 			content={ <StyleVariationsContainer /> }
 			actions={
-				<SidebarButton
-					icon={ edit }
-					label={ __( 'Edit styles' ) }
-					onClick={ () => {
-						// switch to edit mode.
-						setCanvasMode( 'edit' );
-						// open global styles sidebar.
-						openGeneralSidebar( 'edit-site/global-styles' );
-					} }
-				/>
+				<div>
+					<SidebarButton
+						icon={ seen }
+						label={ __( 'Style Book' ) }
+						onClick={ () => {
+							// Switch to edit mode.
+							setCanvasMode( 'edit' );
+							// Open global styles sidebar.
+							openGeneralSidebar( 'edit-site/global-styles' );
+							// Open style book, via the useEffect above.
+							// This is done via a local state change to ensure that the canvas mode is changed,
+							// and the global styles sidebar is opened before attempting to open the style book.
+							setIsStyleBookOpened( true );
+						} }
+					/>
+					<SidebarButton
+						icon={ edit }
+						label={ __( 'Edit styles' ) }
+						onClick={ () => {
+							// Switch to edit mode.
+							setCanvasMode( 'edit' );
+							// Open global styles sidebar.
+							openGeneralSidebar( 'edit-site/global-styles' );
+						} }
+					/>
+				</div>
 			}
 		/>
 	);
