e580bc3afbeb0f72e75a13f9f1b5283bea6a639a
Glen Davies <glen.davies@automattic.com>
2023-05-12 16:47:59 +1200

    Change save function check back

--- packages/block-library/src/pattern/save.js
+++ packages/block-library/src/pattern/save.js
@@ -4,7 +4,7 @@
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
 export default function save( { attributes: { syncStatus }, innerBlocks } ) {
-	if ( innerBlocks?.length === 0 && syncStatus !== 'partial' ) {
+	if ( innerBlocks?.length === 0 || syncStatus !== 'partial' ) {
 		return;
 	}
 	const blockProps = useBlockProps.save();
