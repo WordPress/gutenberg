d66e670b6c253e8d89cd63044a44297c1380c139
André <583546+oandregal@users.noreply.github.com>
2023-05-15 09:59:33 +0200

    Move `gutenberg_get_remote_theme_patterns` (#50597)

--- lib/global-styles-and-settings.php
+++ lib/global-styles-and-settings.php
@@ -265,3 +265,15 @@ function gutenberg_get_global_styles( $path = array(), $context = array() ) {
 
 	return _wp_array_get( $styles, $path, $styles );
 }
+
+/**
+ * Returns the current theme's wanted patterns (slugs) to be
+ * registered from Pattern Directory.
+ *
+ * @since 6.3.0
+ *
+ * @return string[]
+ */
+function gutenberg_get_remote_theme_patterns() {
+	return WP_Theme_JSON_Resolver_Gutenberg::get_theme_data( array(), array( 'with_supports' => false ) )->get_patterns();
+}
