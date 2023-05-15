586c8c6d6eec644ad14d93b4b939adadf0127231
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-05-15 15:55:46 +1000

    Try exposing Style Book in browse mode

--- packages/edit-site/src/components/sidebar-navigation-screen-global-styles/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-global-styles/index.js
@@ -16,6 +16,7 @@ import { unlock } from '../../private-apis';
 import { store as editSiteStore } from '../../store';
 import SidebarButton from '../sidebar-button';
 import SidebarNavigationItem from '../sidebar-navigation-item';
+import StyleBook from '../style-book';
 
 export function SidebarNavigationItemGlobalStyles( props ) {
 	const { openGeneralSidebar } = useDispatch( editSiteStore );
@@ -49,11 +50,31 @@ export function SidebarNavigationItemGlobalStyles( props ) {
 	);
 }
 
+function GlobalStylesStyleBook( { onSelect } ) {
+	return (
+		<StyleBook
+			isSelected={ () => false }
+			onSelect={ async ( blockName ) => {
+				await onSelect( blockName );
+			} }
+		/>
+	);
+}
+
 export default function SidebarNavigationScreenGlobalStyles() {
 	const { openGeneralSidebar } = useDispatch( editSiteStore );
 	const { setCanvasMode, setEditorCanvasContainerView } = unlock(
 		useDispatch( editSiteStore )
 	);
+
+	const { isStyleBookOpened } = useSelect( ( select ) => {
+		const { getEditorCanvasContainerView } = unlock(
+			select( editSiteStore )
+		);
+		return {
+			isStyleBookOpened: 'style-book' === getEditorCanvasContainerView(),
+		};
+	}, [] );
 
 	const openGlobalStyles = async () =>
 		Promise.all( [
@@ -62,32 +83,48 @@ export default function SidebarNavigationScreenGlobalStyles() {
 		] );
 
 	return (
-		<SidebarNavigationScreen
-			title={ __( 'Styles' ) }
-			description={ __(
-				'Choose a different style combination for the theme styles.'
+		<>
+			<SidebarNavigationScreen
+				title={ __( 'Styles' ) }
+				description={ __(
+					'Choose a different style combination for the theme styles.'
+				) }
+				content={ <StyleVariationsContainer /> }
+				actions={
+					<div>
+						<SidebarButton
+							icon={ seen }
+							label={ __( 'Style Book' ) }
+							onClick={ async () => {
+								if ( ! isStyleBookOpened ) {
+									setEditorCanvasContainerView(
+										'style-book'
+									);
+								} else {
+									setEditorCanvasContainerView( undefined );
+								}
+							} }
+							isPressed={ isStyleBookOpened }
+						/>
+						<SidebarButton
+							icon={ edit }
+							label={ __( 'Edit styles' ) }
+							onClick={ async () => await openGlobalStyles() }
+						/>
+					</div>
+				}
+			/>
+			{ isStyleBookOpened && (
+				<GlobalStylesStyleBook
+					onSelect={ async () => {
+						await openGlobalStyles();
+						// Open the Style Book once the canvas mode is set to edit,
+						// and the global styles sidebar is open. This ensures that
+						// the Style Book is not prematurely closed.
+						setEditorCanvasContainerView( 'style-book' );
+					} }
+				/>
 			) }
-			content={ <StyleVariationsContainer /> }
-			actions={
-				<div>
-					<SidebarButton
-						icon={ seen }
-						label={ __( 'Style Book' ) }
-						onClick={ async () => {
-							await openGlobalStyles();
-							// Open the Style Book once the canvas mode is set to edit,
-							// and the global styles sidebar is open. This ensures that
-							// the Style Book is not prematurely closed.
-							setEditorCanvasContainerView( 'style-book' );
-						} }
-					/>
-					<SidebarButton
-						icon={ edit }
-						label={ __( 'Edit styles' ) }
-						onClick={ async () => await openGlobalStyles() }
-					/>
-				</div>
-			}
-		/>
+		</>
 	);
 }
