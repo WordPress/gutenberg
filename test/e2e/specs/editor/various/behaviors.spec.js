7f7db2c8394073f01158e3c6f58c88a9c457f03f
Michal Czaplinski <mmczaplinski@gmail.com>
2023-05-15 23:42:14 -0500

    Add more e2e tests

--- test/e2e/specs/editor/various/behaviors.spec.js
+++ test/e2e/specs/editor/various/behaviors.spec.js
@@ -9,6 +9,15 @@ const path = require( 'path' );
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'Testing behaviors functionality', () => {
+	const filename = '1024x768_e2e_test_image_size.jpeg';
+	const filepath = path.join( './test/e2e/assets', filename );
+
+	const createMedia = async ( { admin, requestUtils } ) => {
+		await admin.createNewPost();
+		const media = await requestUtils.uploadMedia( filepath );
+		return media;
+	};
+
 	test.afterAll( async ( { requestUtils } ) => {
 		await requestUtils.activateTheme( 'twentytwentyone' );
 		await requestUtils.deleteAllPosts();
@@ -18,19 +27,14 @@ test.describe( 'Testing behaviors functionality', () => {
 		await requestUtils.deleteAllMedia();
 	} );
 
-	test( 'Lightbox option should be selected in an image block, as defined in theme.json', async ( {
+	test( 'Lightbox behavior should be selected by default as defined in the core theme.json', async ( {
 		admin,
 		editor,
 		requestUtils,
 		page,
 	} ) => {
-		await admin.createNewPost();
-		const filename = '1024x768_e2e_test_image_size.jpeg';
-		const filepath = path.join( './test/e2e/assets', filename );
-
-		await admin.createNewPost();
-		const media = await requestUtils.uploadMedia( filepath );
-
+		await requestUtils.activateTheme( 'twentytwentyone' );
+		const media = await createMedia( { admin, requestUtils } );
 		await editor.insertBlock( {
 			name: 'core/image',
 			attributes: {
@@ -45,21 +49,17 @@ test.describe( 'Testing behaviors functionality', () => {
 		await expect( page.getByLabel( 'Behavior' ) ).toHaveValue( 'lightbox' );
 	} );
 
-	test( 'None option should be selected in an image block, as defined in theme.json', async ( {
+	test( 'Behaviors UI can be disabled in the `theme.json`', async ( {
 		admin,
 		editor,
 		requestUtils,
 		page,
 	} ) => {
-		// Lightbox is the default behavior, so we need a theme that has it disabled. Change if we change the default.
-		await requestUtils.activateTheme( 'behaviors' );
-		await admin.createNewPost();
-
-		const filename = '1024x768_e2e_test_image_size.jpeg';
-		const filepath = path.join( './test/e2e/assets', filename );
-
-		await admin.createNewPost();
-		const media = await requestUtils.uploadMedia( filepath );
+		// { "lightbox": true } is the default behavior, so we activate the
+		// `behaviors` theme where it is disabled by default. Change if we change
+		// the default value in the core theme.json file.
+		await requestUtils.activateTheme( 'behaviors-disabled' );
+		const media = await createMedia( { admin, requestUtils } );
 
 		await editor.insertBlock( {
 			name: 'core/image',
@@ -71,7 +71,60 @@ test.describe( 'Testing behaviors functionality', () => {
 		} );
 
 		await page.getByRole( 'button', { name: 'Advanced' } ).click();
-		// Right now the selector should not appear.
+
+		// No behaviors dropdown should be present.
 		await expect( page.getByLabel( 'Behavior' ) ).toHaveCount( 0 );
+	} );
+
+	test( "Block's value for behaviors takes precedence over the theme's value", async ( {
+		admin,
+		editor,
+		requestUtils,
+		page,
+	} ) => {
+		await requestUtils.activateTheme( 'twentytwentyone' );
+		const media = await createMedia( { admin, requestUtils } );
+
+		await editor.insertBlock( {
+			name: 'core/image',
+			attributes: {
+				alt: filename,
+				id: media.id,
+				url: media.source_url,
+				// Explicitly set the value for behaviors to false.
+				behaviors: { lightbox: false },
+			},
+		} );
+
+		await page.getByRole( 'button', { name: 'Advanced' } ).click();
+		await expect( page.getByLabel( 'Behavior' ) ).toHaveCount( 1 );
+		await expect( page.getByLabel( 'Behavior' ) ).toHaveValue( '' );
+	} );
+
+	test( 'You can set the default value for the behaviors in the theme.json', async ( {
+		admin,
+		editor,
+		requestUtils,
+		page,
+	} ) => {
+		// In this theme, the default value for settings.behaviors.lightbox is `false`.
+		await requestUtils.activateTheme( 'behaviors-false' );
+		const media = await createMedia( { admin, requestUtils } );
+
+		await editor.insertBlock( {
+			name: 'core/image',
+			attributes: {
+				alt: filename,
+				id: media.id,
+				url: media.source_url,
+			},
+		} );
+
+		await page.getByRole( 'button', { name: 'Advanced' } ).click();
+		await expect( page.getByLabel( 'Behavior' ) ).toHaveCount( 1 );
+
+		// The default value for behaviors in `theme.json` is `false`, so the
+		// dropdown should be present, but the value should be `""` (None).
+		await expect( page.getByLabel( 'Behavior' ) ).toHaveValue( '' );
 	} );
 } );
