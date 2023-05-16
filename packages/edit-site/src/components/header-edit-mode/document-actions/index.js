cd2ecf4ed9169825d86c0e996d211fb1ffd2f33a
Saxon Fletcher <saxonafletcher@gmail.com>
2023-05-16 16:52:32 +1000

    update the site editor title bar

--- packages/edit-site/src/components/header-edit-mode/document-actions/index.js
+++ packages/edit-site/src/components/header-edit-mode/document-actions/index.js
@@ -11,27 +11,30 @@ import {
 	__experimentalGetBlockLabel as getBlockLabel,
 	getBlockType,
 } from '@wordpress/blocks';
-import { useSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import {
-	Dropdown,
 	Button,
 	VisuallyHidden,
+	Icon,
+	__experimentalHStack as HStack,
 	__experimentalText as Text,
+	Path,
+	SVG,
 } from '@wordpress/components';
-import { chevronDown } from '@wordpress/icons';
-import { useState, useMemo } from '@wordpress/element';
+import { layout, page, symbol, chevronLeftSmall } from '@wordpress/icons';
 import {
 	store as blockEditorStore,
 	useBlockDisplayInformation,
-	BlockIcon,
 } from '@wordpress/block-editor';
-import { store as preferencesStore } from '@wordpress/preferences';
+import { privateApis as commandsPrivateApis } from '@wordpress/commands';
 
 /**
  * Internal dependencies
  */
-import TemplateDetails from '../../template-details';
 import useEditedEntityRecord from '../../use-edited-entity-record';
+import { unlock } from '../../../private-apis';
+
+const { store: commandsStore } = unlock( commandsPrivateApis );
 
 function getBlockDisplayText( block ) {
 	if ( block ) {
@@ -66,38 +69,31 @@ function useSecondaryText() {
 	return {};
 }
 
-export default function DocumentActions() {
-	const showIconLabels = useSelect(
-		( select ) =>
-			select( preferencesStore ).get(
-				'core/edit-site',
-				'showIconLabels'
-			),
-		[]
-	);
+const titleIcon = {
+	page,
+	wp_template: layout,
+	wp_template_part: symbol,
+};
+
+export default function DocumentActions( {
+	showBack = false,
+	onBack,
+	backLabel = __( 'Back' ),
+} ) {
 	const { isLoaded, record, getTitle } = useEditedEntityRecord();
-	const { label, icon } = useSecondaryText();
-
-	// Use internal state instead of a ref to make sure that the component
-	// re-renders when the popover's anchor updates.
-	const [ popoverAnchor, setPopoverAnchor ] = useState( null );
-
-	// Memoize popoverProps to avoid returning a new object every time.
-	const popoverProps = useMemo(
-		() => ( {
-			// Use the title wrapper as the popover anchor so that the dropdown is
-			// centered over the whole title area rather than just one part of it.
-			anchor: popoverAnchor,
-			placement: 'bottom',
-		} ),
-		[ popoverAnchor ]
-	);
+	const { label } = useSecondaryText();
+	const { open: openCommand } = useDispatch( commandsStore );
 
 	// Return a simple loading indicator until we have information to show.
 	if ( ! isLoaded ) {
 		return (
 			<div className="edit-site-document-actions">
-				{ __( 'Loading…' ) }
+				<Text
+					size="body"
+					className="edit-site-document-actions__title-wrapper"
+				>
+					{ __( 'Loading…' ) }
+				</Text>
 			</div>
 		);
 	}
@@ -116,16 +112,41 @@ export default function DocumentActions() {
 			? __( 'template part' )
 			: __( 'template' );
 
+	const handleBack = ( e ) => {
+		e.stopPropagation();
+		onBack();
+	};
 	return (
 		<div
 			className={ classnames( 'edit-site-document-actions', {
 				'has-secondary-label': !! label,
 			} ) }
+			role="button"
+			tabIndex={ 0 }
+			onClick={ openCommand }
+			onKeyDown={ openCommand }
 		>
-			<div
-				ref={ setPopoverAnchor }
-				className="edit-site-document-actions__title-wrapper"
+			{ showBack && (
+				<HStack alignment="left">
+					<Button
+						className="edit-site-document-actions__back"
+						onClick={ handleBack }
+						icon={ chevronLeftSmall }
+					>
+						{ backLabel }
+					</Button>
+				</HStack>
+			) }
+			<HStack
+				spacing={ 2 }
+				className={ classnames(
+					'edit-site-document-actions__title-wrapper',
+					{
+						'is-global': !! record.type !== 'page',
+					}
+				) }
 			>
+				<Icon icon={ titleIcon[ record.type ] } />
 				<Text
 					size="body"
 					className="edit-site-document-actions__title"
@@ -140,39 +161,36 @@ export default function DocumentActions() {
 					</VisuallyHidden>
 					{ getTitle() }
 				</Text>
-				<div className="edit-site-document-actions__secondary-item">
-					<BlockIcon icon={ icon } showColors />
-					<Text size="body">{ label ?? '' }</Text>
-				</div>
-
-				<Dropdown
-					popoverProps={ popoverProps }
-					renderToggle={ ( { isOpen, onToggle } ) => (
-						<Button
-							className="edit-site-document-actions__get-info"
-							icon={ chevronDown }
-							aria-expanded={ isOpen }
-							aria-haspopup="true"
-							onClick={ onToggle }
-							variant={ showIconLabels ? 'tertiary' : undefined }
-							label={ sprintf(
-								/* translators: %s: the entity to see details about, like "template"*/
-								__( 'Show %s details' ),
-								entityLabel
-							) }
+			</HStack>
+			<HStack
+				spacing={ 1 }
+				alignment="right"
+				className="edit-site-document-actions__shortcut"
+			>
+				<Icon
+					icon={
+						<SVG
+							width="24px"
+							height="24px"
+							viewBox="0 0 24 24"
+							strokeWidth="1.5"
+							fill="none"
+							xmlns="http://www.w3.org/2000/SVG"
+							color="currentColor"
 						>
-							{ showIconLabels && __( 'Details' ) }
-						</Button>
-					) }
-					contentClassName="edit-site-document-actions__info-dropdown"
-					renderContent={ ( { onClose } ) => (
-						<TemplateDetails
-							template={ record }
-							onClose={ onClose }
-						/>
-					) }
+							<Path
+								d="M9 6v12M15 6v12M9 6a3 3 0 10-3 3h12a3 3 0 10-3-3M9 18a3 3 0 11-3-3h12a3 3 0 11-3 3"
+								stroke="currentColor"
+								strokeWidth="1.5"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							></Path>
+						</SVG>
+					}
+					size={ 16 }
 				/>
-			</div>
+				<Text>K</Text>
+			</HStack>
 		</div>
 	);
 }
