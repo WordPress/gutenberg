b028cbbb12801580812ef24138e866e3f327196e
Jeff Ong <jonger4@gmail.com>
2023-05-17 15:24:27 -0400

    Handle keyboard navigation, event handling, and refactor.

--- packages/block-library/src/search/view.js
+++ packages/block-library/src/search/view.js
@@ -1,42 +1,53 @@
 window.addEventListener( 'DOMContentLoaded', () => {
 	const hiddenClass = 'wp-block-search__searchfield-hidden';
-	const wrapperClass = '.wp-block-search__inside-wrapper';
-	const buttonClass = '.wp-block-search__button';
 
 	Array.from(
 		document.getElementsByClassName(
 			'wp-block-search__button-behavior-expand'
 		)
 	).forEach( ( block ) => {
-		const wrapper = block.querySelector( wrapperClass );
 		const searchField = block.querySelector( '.wp-block-search__input' );
-		const button = block.querySelector( buttonClass );
+		const searchButton = block.querySelector( '.wp-block-search__button' );
+		const activeElement = block.ownerDocument.activeElement;
 
-		const toggleSearchField = ( e ) => {
-			if ( e.target !== button && ! e.target.closest( buttonClass ) ) {
-				return false;
+		const toggleSearchField = ( showSearchField ) => {
+			if ( showSearchField ) {
+				return block.classList.remove( hiddenClass );
 			}
 
-			e.preventDefault();
-
-			return block.classList.contains( hiddenClass )
-				? doShowSearchField()
-				: doHideSearchField();
+			return block.classList.add( hiddenClass );
 		};
 
-		const doShowSearchField = () => {
-			block.classList.remove( hiddenClass );
-			searchField.focus();
-
-			wrapper.removeEventListener( 'click', toggleSearchField );
-			button.addEventListener( 'blur', toggleSearchField );
-			document.body.addEventListener( 'click', doHideSearchField );
+		const hideSearchField = ( e ) => {
+			if (
+				e.type === 'blur' &&
+				( e.relatedTarget !== searchButton ||
+					e.target === searchButton )
+			) {
+				return toggleSearchField( false );
+			}
+			if (
+				! e.target.closest( '.wp-block-search__inside-wrapper' ) &&
+				activeElement !== searchButton &&
+				activeElement !== searchField
+			) {
+				return toggleSearchField( false );
+			}
 		};
 
-		const doHideSearchField = () => {
-			block.classList.add( hiddenClass );
+		const handleButtonClick = ( e ) => {
+			if ( block.classList.contains( hiddenClass ) ) {
+				e.preventDefault();
+				searchField.focus();
+			}
 		};
 
-		searchField.addEventListener( 'focus', doShowSearchField );
+		searchField.addEventListener( 'focus', () =>
+			toggleSearchField( true )
+		);
+		searchField.addEventListener( 'blur', hideSearchField );
+		searchButton.addEventListener( 'click', handleButtonClick );
+		searchButton.addEventListener( 'blur', hideSearchField );
+		document.body.addEventListener( 'click', hideSearchField );
 	} );
 } );
