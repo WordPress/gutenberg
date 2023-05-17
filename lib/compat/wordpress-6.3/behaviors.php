0ef7b5e0e709962d24ba66368394482303de7ef2
Michal Czaplinski <mmczaplinski@gmail.com>
2023-05-16 19:51:22 -0500

    Update the comment

--- lib/compat/wordpress-6.3/behaviors.php
+++ lib/compat/wordpress-6.3/behaviors.php
@@ -9,7 +9,7 @@ add_filter(
 	'block_editor_settings_all',
 	function( $settings ) {
 
-		// Make sure to also get the value from the core theme.json file.
+		// TODO: Make sure to also get the value from the core theme.json file.
 		$settings['behaviors'] = WP_Theme_JSON_Resolver_Gutenberg::get_theme_data()->get_data()['behaviors'];
 		return $settings;
 	},
