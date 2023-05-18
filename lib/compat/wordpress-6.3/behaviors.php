1dd6df1397c00b4634ac4feeee7b098f6fc64dad
Carlos Bravo <carlos.bravo@automattic.com>
2023-05-18 17:47:47 +0200

    Found a much better way to fix php tests

--- lib/compat/wordpress-6.3/behaviors.php
+++ lib/compat/wordpress-6.3/behaviors.php
@@ -8,15 +8,8 @@
 add_filter(
 	'block_editor_settings_all',
 	function( $settings ) {
-		// Prevents testing error 'Undefined index: behaviors' in Gutenberg_REST_Block_Editor_Settings_Controller_Test.
-		// This should be removed once the REST API is updated to use the new theme.json file.
-		$is_mobile_context = (
-			defined( 'REST_REQUEST' ) &&
-			REST_REQUEST &&
-			isset( $_GET['context'] ) &&
-			'mobile' === $_GET['context']
-		);
-		if ( ! $is_mobile_context ) {
+		$theme_data = WP_Theme_JSON_Resolver_Gutenberg::get_theme_data()->get_data();
+		if ( array_key_exists( 'behaviors', $theme_data ) ) {
 			$settings['behaviors'] = WP_Theme_JSON_Resolver_Gutenberg::get_theme_data()->get_data()['behaviors'];
 		}
 		// TODO: Make sure to also get the value from the core theme.json file.
