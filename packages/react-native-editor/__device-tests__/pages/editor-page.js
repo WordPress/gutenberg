714d1f05f655e64df1daa76b046224143a2798f7
Gerardo Pacheco <gerardo.pacheco@automattic.com>
2023-05-18 19:59:15 +0200

    Mobile - E2E test - Update code to use the new navigateUp helper (#50736)
    * Mobile - E2E test - Update code to use the new navigateUp helper
    
    * Mobile - E2E helpers - Update navigateUp helper to include recursive option with a false default value
    
    * Mobile - E2E Helpers - Rename navigateUp to moveBlockSelectionUp with toRoot as options param
    
    * Mobile - E2E test - Update usage of navigateUp to new moveBlockSelectionUp name

--- packages/react-native-editor/__device-tests__/pages/editor-page.js
+++ packages/react-native-editor/__device-tests__/pages/editor-page.js
@@ -499,7 +499,7 @@ class EditorPage {
 		await toolBarButton.click();
 	}
 
-	async navigateUp() {
+	async moveBlockSelectionUp( options = { toRoot: false } ) {
 		let navigateUpElements = [];
 		do {
 			await this.driver.sleep( 2000 );
@@ -508,6 +508,9 @@ class EditorPage {
 			);
 			if ( navigateUpElements.length > 0 ) {
 				await navigateUpElements[ 0 ].click();
+			}
+			if ( ! options.toRoot ) {
+				break;
 			}
 		} while ( navigateUpElements.length > 0 );
 	}
