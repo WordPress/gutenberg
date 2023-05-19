0517411b6f833ec65739433dd9a63b6f43e1ef73
Sarah Norris <sarah@sekai.co.uk>
2023-05-19 11:10:05 +0100

    Update toggle label and description

--- packages/block-library/src/query-pagination/query-pagination-label-control.js
+++ packages/block-library/src/query-pagination/query-pagination-label-control.js
@@ -8,9 +8,9 @@ export function QueryPaginationLabelControl( { value, onChange } ) {
 	return (
 		<ToggleControl
 			__nextHasNoMarginBottom
-			label={ __( 'Show pagination text' ) }
+			label={ __( 'Show label text' ) }
 			help={ __(
-				'Toggle off to hide the pagination label text, e.g. "Next Page".'
+				'Toggle off to hide the label text, e.g. "Next Page".'
 			) }
 			onChange={ onChange }
 			checked={ value === true }
