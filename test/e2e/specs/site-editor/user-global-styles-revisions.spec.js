46bfa6a650d35671e326df2d4d2c46995f01d55c
George Mamadashvili <georgemamadashvili@gmail.com>
2023-05-23 14:10:46 +0400

    Remove redundant calls to disable the Styles welcome guide

--- test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
+++ test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
@@ -131,15 +131,6 @@ class UserGlobalStylesRevisions {
 		this.requestUtils = requestUtils;
 	}
 
-	async disableWelcomeGuide() {
-		// Turn off the welcome guide.
-		await this.page.evaluate( () => {
-			window.wp.data
-				.dispatch( 'core/preferences' )
-				.set( 'core/edit-site', 'welcomeGuideStyles', false );
-		} );
-	}
-
 	async getGlobalStylesRevisions() {
 		const stylesPostId =
 			await this.requestUtils.getCurrentThemeGlobalStylesPostId();
@@ -159,7 +150,6 @@ class UserGlobalStylesRevisions {
 	}
 
 	async openStylesPanel() {
-		await this.disableWelcomeGuide();
 		await this.page
 			.getByRole( 'region', { name: 'Editor top bar' } )
 			.getByRole( 'button', { name: 'Styles' } )
