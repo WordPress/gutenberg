b8dc7dd6ff6bec3b9a4576dd8341f27fca5b9dfb
Mario Santos <santosguillamot@gmail.com>
2023-05-24 18:39:46 +0200

    Remove experimental flag for the Interactivity API

--- lib/load.php
+++ lib/load.php
@@ -104,10 +104,8 @@ require __DIR__ . '/experimental/navigation-theme-opt-in.php';
 require __DIR__ . '/experimental/kses.php';
 require __DIR__ . '/experimental/l10n.php';
 require __DIR__ . '/experimental/navigation-fallback.php';
-if ( gutenberg_is_experiment_enabled( 'gutenberg-interactivity-api-core-blocks' ) ) {
-	require __DIR__ . '/experimental/interactivity-api/script-loader.php';
-	require __DIR__ . '/experimental/interactivity-api/blocks.php';
-}
+require __DIR__ . '/experimental/interactivity-api/script-loader.php';
+require __DIR__ . '/experimental/interactivity-api/blocks.php';
 
 // Fonts API.
 if ( ! class_exists( 'WP_Fonts' ) ) {
