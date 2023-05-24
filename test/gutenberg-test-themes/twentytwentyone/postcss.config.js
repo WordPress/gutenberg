b315dd15dfbee9eefaf1f6f5914607098714707b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-05-24 12:54:27 +0200

    Upload twentytwentyone@1.7 and twentytwentythree@1.0 to test themes

---
+++ test/gutenberg-test-themes/twentytwentyone/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	plugins: [
+		require('postcss-nested'),
+		require('postcss-css-variables')({
+			preserve: false,
+			preserveAtRulesOrder: true
+		}),
+		require('postcss-calc')({
+			precision: 0
+		}),
+		require('postcss-discard-duplicates'),
+		require('postcss-merge-rules')
+	]
+};
