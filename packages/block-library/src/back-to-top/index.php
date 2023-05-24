c84f3a96d524a6e21d5065cd3e9e393db6cfa12a
Carolina Nymark <hi@themesbycarolina.com>
2023-05-24 11:48:56 +0200

    Block themes: Only add the target id if the block exists on the page

--- packages/block-library/src/back-to-top/index.php
+++ packages/block-library/src/back-to-top/index.php
@@ -36,9 +36,19 @@ function register_block_core_back_to_top() {
 add_action( 'init', 'register_block_core_back_to_top' );
 
 /**
- * Add the target id to the top of each page.
+ * Block themes: If the block exists on the page, add the target id.
+ * Classic themes: We can't check if the block is in use, so we always add the target id.
  */
 function block_core_back_to_top_target() {
 	echo '<div id="wp-back-to-top"></div>';
 }
-add_action( 'wp_body_open', 'block_core_back_to_top_target' );
+if ( wp_is_block_theme() ) {
+	add_filter( 'render_block', function( $html, $block ) {
+		if ( 'core/back-to-top' === $block['blockName'] ) {
+			add_action( 'wp_body_open', 'block_core_back_to_top_target' );
+		}
+		return $html;
+	}, 10,2 );
+} else {
+	add_action( 'wp_body_open', 'block_core_back_to_top_target' );
+}
