b396c0fce7d69022055640994a242b264dd68d7c
Mario Santos <santosguillamot@gmail.com>
2023-05-25 10:59:34 +0200

    Fix file PHP unit tests

--- phpunit/blocks/render-block-file-test.php
+++ phpunit/blocks/render-block-file-test.php
@@ -27,7 +27,13 @@ class Tests_Blocks_Render_File extends WP_UnitTestCase {
 		);
 		$content    = '<div class="wp-block-file"><object class="wp-block-file__embed" data="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" type="application/pdf" style="width:100%;height:370px" aria-label="yolo"></object><a id="wp-block-file--media-_clientId_0" href="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf">yolo</a><a href="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-_clientId_0">Download</a></div>';
 
-		$new_content = gutenberg_render_block_core_file( $attributes, $content );
+		$parsed_blocks = parse_blocks(
+			'<!-- wp:file {"href":"http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf","displayPreview":true} -->'
+		);
+		$parsed_block  = $parsed_blocks[0];
+		$block         = new WP_Block( $parsed_block );
+
+		$new_content = gutenberg_render_block_core_file( $attributes, $content, $block );
 		$this->assertStringContainsString( 'aria-label="Embed of yolo."', $new_content );
 	}
 
@@ -45,7 +51,13 @@ class Tests_Blocks_Render_File extends WP_UnitTestCase {
 		);
 		$content    = '<div class="wp-block-file"><object class="wp-block-file__embed" data="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" type="application/pdf" style="width:100%;height:370px" aria-label="custom filename"></object><a id="wp-block-file--media-_clientId_0" href="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf">custom filename</a><a href="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-_clientId_0">Download</a></div>';
 
-		$new_content = gutenberg_render_block_core_file( $attributes, $content );
+		$parsed_blocks = parse_blocks(
+			'<!-- wp:file {"href":"http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf","displayPreview":true} -->'
+		);
+		$parsed_block  = $parsed_blocks[0];
+		$block         = new WP_Block( $parsed_block );
+
+		$new_content = gutenberg_render_block_core_file( $attributes, $content, $block );
 		$this->assertStringContainsString( 'aria-label="Embed of custom filename."', $new_content );
 	}
 
@@ -63,7 +75,13 @@ class Tests_Blocks_Render_File extends WP_UnitTestCase {
 		);
 		$content    = '<div class="wp-block-file"><object class="wp-block-file__embed" data="' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" type="application/pdf" style="width:100%;height:370px" aria-label="PDF embed"></object><a id="wp-block-file--media-_clientId_0" href="http://' . WP_TESTS_DOMAIN . 'wp-content/uploads/2021/04/yolo.pdf">yolo</a><a href="http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-_clientId_0">Download</a></div>';
 
-		$new_content = gutenberg_render_block_core_file( $attributes, $content );
+		$parsed_blocks = parse_blocks(
+			'<!-- wp:file {"href":"http://' . WP_TESTS_DOMAIN . '/wp-content/uploads/2021/04/yolo.pdf","displayPreview":true} -->'
+		);
+		$parsed_block  = $parsed_blocks[0];
+		$block         = new WP_Block( $parsed_block );
+
+		$new_content = gutenberg_render_block_core_file( $attributes, $content, $block );
 		$this->assertStringContainsString( 'aria-label="PDF embed"', $new_content );
 	}
 }
