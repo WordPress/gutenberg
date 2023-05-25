5e556dc6de2ba4fbe3180ca32a30840485ac8fbc
Iulia Cazan <iulia@phpcoder.ro>
2023-05-25 12:05:20 +0300

    Removed new line

--- packages/block-library/src/post-terms/edit.js
+++ packages/block-library/src/post-terms/edit.js
@@ -117,11 +117,12 @@ export default function PostTermsEdit( {
 						tagName="span"
 					/>
 				) }
+
 				{ ( ! hasPost || ! term ) && (
 					<span>{ blockInformation.title }</span>
 				) }
-				{ hasPost &&
-					! isLoading &&
+
+				{ hasPost && ! isLoading &&
 					hasPostTerms &&
 					postTerms
 						.map( ( postTerm ) =>
