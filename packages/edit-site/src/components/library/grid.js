a29c418225f43bb231d53fe9059f0c020d927576
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-05-25 19:47:54 +1000

    Add dodgy placeholder display for patterns or template parts

--- packages/edit-site/src/components/library/grid.js
+++ packages/edit-site/src/components/library/grid.js
@@ -1,4 +1,11 @@
 /**
+ * WordPress dependencies
+ */
+import { Button, __experimentalHStack as HStack } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { moreHorizontal } from '@wordpress/icons';
+
+/**
  * Internal dependencies
  */
 import usePatterns from './use-patterns';
@@ -6,9 +13,34 @@ import usePatterns from './use-patterns';
 export default function Grid() {
 	const patterns = usePatterns();
 
+	if ( ! patterns ) {
+		return null;
+	}
+
+	if ( ! patterns.length ) {
+		return <div>{ __( 'No patterns found.' ) }</div>;
+	}
+
 	return (
 		<div className="edit-site-library__grid">
-			{ patterns.map( () => '' ) }
+			{ patterns.map( ( pattern ) => (
+				<div key={ pattern.id } className="edit-site-library__pattern">
+					<div className="edit-site-library__preview">
+						{ pattern.id }
+					</div>
+					<HStack
+						className="edit-site-library__footer"
+						justify="space-between"
+					>
+						<span>{ pattern.title.rendered }</span>
+						<Button
+							className="edit-site-library__button"
+							icon={ moreHorizontal }
+							isSmall
+						/>
+					</HStack>
+				</div>
+			) ) }
 		</div>
 	);
 }
