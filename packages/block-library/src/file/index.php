83111455337d8cc96dc5b3a6fac11ccd6db9cbaa
Mario Santos <santosguillamot@gmail.com>
2023-05-25 11:11:52 +0200

    Fix PHP comment spaces

--- packages/block-library/src/file/index.php
+++ packages/block-library/src/file/index.php
@@ -8,8 +8,8 @@
 /**
  * When the `core/file` block is rendering, check if we need to enqueue the `'wp-block-file-view` script.
  *
- * @param array  $attributes The block attributes.
- * @param string $content    The block content.
+ * @param array    $attributes The block attributes.
+ * @param string   $content    The block content.
  * @param WP_Block $block      The parsed block.
  *
  * @return string Returns the block content.
--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -9,8 +9,8 @@
  * Renders the `core/image` block on the server,
  * adding a data-id attribute to the element if core/gallery has added on pre-render.
  *
- * @param  array  $attributes The block attributes.
- * @param  string $content    The block content.
+ * @param  array   $attributes The block attributes.
+ * @param  string  $content    The block content.
  * @param WP_Block $block      The parsed block.
  * @return string Returns the block content with the data-id attribute added.
  */
