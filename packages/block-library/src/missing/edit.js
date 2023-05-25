b782ce2159951a5240f7c883004e70c0ea83040b
Marin Atanasov <tyxla@abv.bg>
2023-05-25 17:03:21 +0300

    Consider the freeform block, or any raw HTML to be Unknown block

--- packages/block-library/src/missing/edit.js
+++ packages/block-library/src/missing/edit.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __, _x, sprintf } from '@wordpress/i18n';
 import { RawHTML } from '@wordpress/element';
 import { Button } from '@wordpress/components';
 import { createBlock } from '@wordpress/blocks';
@@ -16,28 +16,45 @@ import { safeHTML } from '@wordpress/dom';
 function MissingBlockWarning( { attributes, convertToHTML, clientId } ) {
 	const { originalName, originalUndelimitedContent } = attributes;
 	const hasContent = !! originalUndelimitedContent;
-	const hasHTMLBlock = useSelect(
+	const { hasFreeformBlock, hasHTMLBlock } = useSelect(
 		( select ) => {
 			const { canInsertBlockType, getBlockRootClientId } =
 				select( blockEditorStore );
 
-			return canInsertBlockType(
-				'core/html',
-				getBlockRootClientId( clientId )
-			);
+			return {
+				hasFreeformBlock: canInsertBlockType(
+					'core/freeform',
+					getBlockRootClientId( clientId )
+				),
+				hasHTMLBlock: canInsertBlockType(
+					'core/html',
+					getBlockRootClientId( clientId )
+				),
+			};
 		},
 		[ clientId ]
 	);
 
 	const actions = [];
 	let messageHTML;
+
+	let blockName = originalName;
+
+	// Defaulting to "Unknown" if the Classic block isn't registered.
+	if (
+		! hasFreeformBlock &&
+		( ! blockName || blockName === 'core/freeform' )
+	) {
+		blockName = _x( 'Unknown', 'The name we use for an unknown block.' );
+	}
+
 	if ( hasContent && hasHTMLBlock ) {
 		messageHTML = sprintf(
 			/* translators: %s: block name */
 			__(
 				'Your site doesn’t include support for the "%s" block. You can leave this block intact, convert its content to a Custom HTML block, or remove it entirely.'
 			),
-			originalName
+			blockName
 		);
 		actions.push(
 			<Button key="convert" onClick={ convertToHTML } variant="primary">
@@ -50,7 +67,7 @@ function MissingBlockWarning( { attributes, convertToHTML, clientId } ) {
 			__(
 				'Your site doesn’t include support for the "%s" block. You can leave this block intact or remove it entirely.'
 			),
-			originalName
+			blockName
 		);
 	}
 
