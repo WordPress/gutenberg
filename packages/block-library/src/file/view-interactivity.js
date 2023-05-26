02701e9b56bb64c407c897d76f69d5ab4466f671
David Arenas <david.arenas@automattic.com>
2023-05-26 11:35:15 +0200

    Add view prefix to interactivity.js files

--- packages/block-library/src/file/view-interactivity.js
+++ packages/block-library/src/file/view-interactivity.js
@@ -1,7 +1,10 @@
 /**
+ * WordPress dependencies
+ */
+import { store } from '@wordpress/interactivity';
+/**
  * Internal dependencies
  */
-import { store } from '../utils/interactivity';
 import { browserSupportsPdfs as hasPdfPreview } from './utils';
 
 store( {
