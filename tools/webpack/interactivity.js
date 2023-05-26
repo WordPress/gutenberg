b24a7fbcc697ae7c294ec1be8b1557c6517b37ab
David Arenas <david.arenas@automattic.com>
2023-05-26 11:23:32 +0200

    Create a custom webpack config for interactivity

---
+++ tools/webpack/interactivity.js
@@ -0,0 +1,69 @@
+/**
+ * External dependencies
+ */
+const { join } = require( 'path' );
+
+/**
+ * Internal dependencies
+ */
+const { baseConfig } = require( './shared' );
+
+module.exports = {
+	...baseConfig,
+	watchOptions: {
+		aggregateTimeout: 200,
+	},
+	name: 'interactivity',
+	entry: {
+		runtime: './packages/interactivity/src/index.js',
+	},
+	output: {
+		devtoolNamespace: 'wp',
+		filename: './build/interactivity/[name].min.js',
+		path: join( __dirname, '..', '..' ),
+	},
+	optimization: {
+		...baseConfig.optimization,
+		runtimeChunk: {
+			name: 'vendors',
+		},
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					name: 'vendors',
+					test: /[\\/]node_modules[\\/]/,
+					minSize: 0,
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(j|t)sx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: require.resolve( 'babel-loader' ),
+						options: {
+							cacheDirectory:
+								process.env.BABEL_CACHE_DIRECTORY || true,
+							babelrc: false,
+							configFile: false,
+							presets: [
+								[
+									'@babel/preset-react',
+									{
+										runtime: 'automatic',
+										importSource: 'preact',
+									},
+								],
+							],
+						},
+					},
+				],
+			},
+		],
+	},
+};
