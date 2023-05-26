1c0a55fda3296f6f7a334c02203cb91ffb30ef50
Dave Smith <getdavemail@gmail.com>
2023-05-26 13:51:41 +0100

    Make is-url-like stricter

--- packages/block-editor/src/components/link-control/is-url-like.js
+++ packages/block-editor/src/components/link-control/is-url-like.js
@@ -1,4 +1,9 @@
 /**
+ * WordPress dependencies
+ */
+import { getProtocol, isValidProtocol, isValidFragment } from '@wordpress/url';
+
+/**
  * Determines whether a given value could be a URL. Note this does not
  * guarantee the value is a URL only that it looks like it might be one. For
  * example, just because a string has `www.` in it doesn't make it a URL,
@@ -10,9 +15,20 @@
  * @return {boolean} whether or not the value is potentially a URL.
  */
 export default function isURLLike( val ) {
-	const isInternal = val?.startsWith( '#' );
-	const isWWW = val?.startsWith( 'www.' );
-	const isHTTPProtocol = /^(http|https)/.test( val );
+	const hasSpaces = val.includes( ' ' );
 
-	return isHTTPProtocol || isWWW || isInternal;
+	if ( hasSpaces ) {
+		return false;
+	}
+
+	const protocol = getProtocol( val );
+	const protocolIsValid = isValidProtocol( protocol );
+
+	const isWWW = val?.startsWith( 'www.' );
+	const isHTTPProtocol = /^(http|https)/.test( val ) && protocolIsValid;
+	const isMailTo = val?.startsWith( 'mailto:' ) && protocolIsValid;
+	const isTel = val?.startsWith( 'tel:' ) && protocolIsValid;
+	const isInternal = val?.startsWith( '#' ) && isValidFragment( val );
+
+	return isHTTPProtocol || isWWW || isMailTo || isTel || isInternal;
 }
