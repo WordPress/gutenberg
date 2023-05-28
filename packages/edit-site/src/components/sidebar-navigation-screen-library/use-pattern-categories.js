67848995a585402f2cf6bf1d4acef6ad4efdf6e2
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-05-29 09:29:32 +1000

    Try displaying patterns and template parts

--- packages/edit-site/src/components/sidebar-navigation-screen-library/use-pattern-categories.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-library/use-pattern-categories.js
@@ -1,34 +1,79 @@
 /**
  * WordPress dependencies
  */
-import { store as blockEditorStore } from '@wordpress/block-editor';
+import { store as coreStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { unlock } from '../../private-apis';
+import { store as editSiteStore } from '../../store';
 
 export default function usePatternCategories() {
-	return useSelect( ( select ) => {
-		const { __experimentalGetAllowedPatterns, getSettings } =
-			select( blockEditorStore );
-		const patterns = __experimentalGetAllowedPatterns();
-		const patternCategories = {};
+	const { blockPatterns, blockPatternCategories } = useSelect( ( select ) => {
+		const { getSettings } = unlock( select( editSiteStore ) );
+		const settings = getSettings();
 
-		getSettings().__experimentalBlockPatternCategories.forEach(
-			( category ) => {
+		return {
+			blockPatterns:
+				settings.__experimentalAdditionalBlockPatterns ??
+				settings.__experimentalBlockPatterns,
+			blockPatternCategories:
+				settings.__experimentalAdditionalBlockPatternCategories ??
+				settings.__experimentalBlockPatternCategories,
+		};
+	} );
+
+	const { restBlockPatterns, restBlockPatternCategories } = useSelect(
+		( select ) => ( {
+			restBlockPatterns: select( coreStore ).getBlockPatterns(),
+			restBlockPatternCategories:
+				select( coreStore ).getBlockPatternCategories(),
+		} )
+	);
+
+	const patterns = useMemo(
+		() =>
+			[
+				...( blockPatterns || [] ),
+				...( restBlockPatterns || [] ),
+			].filter(
+				( x, index, arr ) =>
+					index === arr.findIndex( ( y ) => x.name === y.name )
+			),
+		[ blockPatterns, restBlockPatterns ]
+	);
+
+	const categories = useMemo( () => {
+		const combinedCategories = [
+			...( blockPatternCategories || [] ),
+			...( restBlockPatternCategories || [] ),
+		];
+
+		const categoryMap = {};
+
+		combinedCategories.forEach( ( category ) => {
+			if ( ! categoryMap[ category.name ] ) {
 				category.count = 0;
-				patternCategories[ category.name ] = category;
+				categoryMap[ category.name ] = category;
 			}
-		);
+		} );
 
 		patterns.forEach( ( pattern ) => {
-			pattern.categories.forEach( ( patternCategory ) => {
-				if ( patternCategories[ patternCategory ] ) {
-					patternCategories[ patternCategory ].count += 1;
+			pattern.categories?.forEach( ( patternCategory ) => {
+				if ( categoryMap[ patternCategory ] ) {
+					categoryMap[ patternCategory ].count += 1;
 				}
 			} );
 		} );
 
-		return {
-			hasPatterns: !! patterns.length,
-			patternCategories: Object.values( patternCategories ),
-		};
-	}, [] );
+		return categoryMap;
+	}, [ blockPatternCategories, restBlockPatternCategories, patterns ] );
+
+	return {
+		hasPatterns: !! patterns.length,
+		patternCategories: Object.values( categories ),
+	};
 }
