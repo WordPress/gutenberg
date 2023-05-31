0e0d2ccaf724b2f3be2f7a7215c614331a635cc8
Ari Stathopoulos <aristath@gmail.com>
2023-05-31 12:59:27 +0300

    lib/block-supports/settings.php

--- lib/block-supports/settings.php
+++ lib/block-supports/settings.php
@@ -26,8 +26,7 @@ function _gutenberg_add_block_level_presets_class( $block_content, $block ) {
 	}
 
 	// return early if no settings are found on the block attributes.
-	$block_settings = _wp_array_get( $block, array( 'attrs', 'settings' ), null );
-	if ( empty( $block_settings ) ) {
+	if ( ! isset( $block['attrs']['settings'] ) || empty( $block['attrs']['settings'] ) ) {
 		return $block_content;
 	}
 
@@ -59,8 +58,7 @@ function _gutenberg_add_block_level_preset_styles( $pre_render, $block ) {
 	}
 
 	// return early if no settings are found on the block attributes.
-	$block_settings = _wp_array_get( $block, array( 'attrs', 'settings' ), null );
-	if ( empty( $block_settings ) ) {
+	if ( ! isset( $block['attrs']['settings'] ) || empty( $block['attrs']['settings'] ) ) {
 		return null;
 	}
 
