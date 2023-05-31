8c9825e25f636e51f3f4cbe752cbcbdc0044ee3d
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-05-31 12:01:18 +1000

    Add category select to create pattern modal

--- packages/edit-site/src/components/create-pattern-modal/index.js
+++ packages/edit-site/src/components/create-pattern-modal/index.js
@@ -5,15 +5,29 @@ import {
 	TextControl,
 	Button,
 	Modal,
+	SelectControl,
 	__experimentalHStack as HStack,
 	__experimentalVStack as VStack,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 
+/**
+ * Internal dependencies
+ */
+import usePatternCategories from '../sidebar-navigation-screen-library/use-pattern-categories';
+
 export default function CreatePatternModal( { closeModal, onCreate } ) {
 	const [ name, setName ] = useState( '' );
+	const [ categoryName, setCategoryName ] = useState( '' );
 	const [ isSubmitting, setIsSubmitting ] = useState( false );
+
+	const { patternCategories } = usePatternCategories();
+
+	const options = patternCategories.map( ( category ) => ( {
+		label: category.label,
+		value: category.name,
+	} ) );
 
 	return (
 		<Modal
@@ -21,6 +35,12 @@ export default function CreatePatternModal( { closeModal, onCreate } ) {
 			onRequestClose={ closeModal }
 			overlayClassName="edit-site-create-pattern-modal"
 		>
+			<p>
+				{ __(
+					'Turn this block into a pattern for you to reuse later'
+				) }
+			</p>
+
 			<form
 				onSubmit={ async ( event ) => {
 					event.preventDefault();
@@ -33,11 +53,20 @@ export default function CreatePatternModal( { closeModal, onCreate } ) {
 			>
 				<VStack spacing="4">
 					<TextControl
-						__nextHasNoMarginBottom
+						className="edit-site-create-pattern-modal__input"
 						label={ __( 'Name' ) }
-						value={ name }
 						onChange={ setName }
+						placeholder={ __( 'My pattern' ) }
 						required
+						value={ name }
+						__nextHasNoMarginBottom
+					/>
+					<SelectControl
+						label={ __( 'Category' ) }
+						onChange={ setCategoryName }
+						options={ options }
+						size="__unstable-large"
+						value={ categoryName }
 					/>
 					<HStack justify="right">
 						<Button
