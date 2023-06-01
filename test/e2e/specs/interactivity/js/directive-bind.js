599e907f9d5e012284e60db69288fdfef7cd082c
David Arenas <david.arenas@automattic.com>
2023-06-01 17:01:02 +0200

    Move directive bind tests

---
+++ test/e2e/specs/interactivity/js/directive-bind.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+const { store } = window.wp.interactivity;
+
+store( {
+	state: {
+		url: '/some-url',
+		checked: true,
+		show: false,
+		width: 1,
+	},
+	foo: {
+		bar: 1,
+	},
+	actions: {
+		toggle: ( { state, foo } ) => {
+			state.url = '/some-other-url';
+			state.checked = ! state.checked;
+			state.show = ! state.show;
+			state.width += foo.bar;
+		},
+	},
+} );
