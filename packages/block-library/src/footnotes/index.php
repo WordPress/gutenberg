1d2ef9d4a002aa176b5bb489ae16fd86481bd1eb
Ella van Durpe <ella@vandurpe.com>
2023-06-02 19:24:59 +0300

    Fix PHP linting

--- packages/block-library/src/footnotes/index.php
+++ packages/block-library/src/footnotes/index.php
@@ -1,19 +1,18 @@
 <?php
 /**
- * Server-side rendering of the `core/comment-template` block.
+ * Server-side rendering of the `core/footnotes` block.
  *
  * @package WordPress
  */
 
 /**
- * Renders the `core/comment-template` block on the server.
+ * Renders the `core/footnotes` block on the server.
  *
  * @param array    $attributes Block attributes.
  * @param string   $content    Block default content.
  * @param WP_Block $block      Block instance.
  *
- * @return string Returns the HTML representing the comments using the layout
- * defined by the block's inner blocks.
+ * @return string Returns the HTML representing the footnotes.
  */
 function render_block_core_footnotes( $attributes, $content, $block ) {
 	// Bail out early if the post ID is not set for some reason.
@@ -25,10 +24,8 @@ function render_block_core_footnotes( $attributes, $content, $block ) {
 		return;
 	}
 
-    $footnotes = get_post_meta( $block->context['postId'], 'footnotes', true );
-    $footnotes = json_decode( $footnotes, true );
-
-    var_dump($footnotes);
+	$footnotes = get_post_meta( $block->context['postId'], 'footnotes', true );
+	$footnotes = json_decode( $footnotes, true );
 
 	if ( count( $footnotes ) === 0 ) {
 		return '';
@@ -36,15 +33,15 @@ function render_block_core_footnotes( $attributes, $content, $block ) {
 
 	$wrapper_attributes = get_block_wrapper_attributes();
 
-    $block_content = '';
+	$block_content = '';
 
-    foreach ( $footnotes as $footnote ) {
-        $block_content .= sprintf(
-            '<li id="%1$s">%2$s <a href="#%1$s-link">↩︎</a></li>',
-            $footnote['id'],
-            $footnote['content']
-        );
-    }
+	foreach ( $footnotes as $footnote ) {
+		$block_content .= sprintf(
+			'<li id="%1$s">%2$s <a href="#%1$s-link">↩︎</a></li>',
+			$footnote['id'],
+			$footnote['content']
+		);
+	}
 
 	return sprintf(
 		'<ol %1$s>%2$s</ol>',
@@ -60,7 +57,7 @@ function register_block_core_footnotes() {
 	register_block_type_from_metadata(
 		__DIR__ . '/footnotes',
 		array(
-			'render_callback'   => 'render_block_core_footnotes',
+			'render_callback' => 'render_block_core_footnotes',
 		)
 	);
 }
