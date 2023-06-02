bd1af68facf2d11135a8022f90e6947deb4c5930
Ricardo Artemio Morales <ric.morales22@gmail.com>
2023-06-02 18:33:50 +0200

    Fix PHP spacing for linter

--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -82,7 +82,7 @@ function render_block_core_image( $attributes, $content ) {
 		$m->set_attribute( 'data-wp-bind--src', 'selectors.core.image.imageSrc' );
 		$modal_content = $m->get_updated_html();
 
-		$background_color   = esc_attr( wp_get_global_styles( array( 'color', 'background' ) ) );
+		$background_color = esc_attr( wp_get_global_styles( array( 'color', 'background' ) ) );
 
 		$close_button_icon  = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" aria-hidden="true" focusable="false"><path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path></svg>';
 		$close_button_color = esc_attr( wp_get_global_styles( array( 'color', 'text' ) ) );
