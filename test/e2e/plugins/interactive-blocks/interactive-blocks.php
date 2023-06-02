b1d696690588b497aa76cd5ed638021e9fde09d2
David Arenas <david.arenas@automattic.com>
2023-06-02 14:43:30 +0200

    Add interactive-blocks plugin for e2e tests

---
+++ test/e2e/plugins/interactive-blocks/interactive-blocks.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Gutenberg Test Interactive Blocks
+ * Plugin URI: https://github.com/WordPress/gutenberg
+ * Author: Gutenberg Team
+ *
+ * @package gutenberg-test-interactive-blocks
+ */
+
+ // Register all blocks found in the `build/blocks` folder.
+function auto_register_block_types() {
+	if ( file_exists( __DIR__ . '/build/' ) ) {
+		$block_json_files = glob( __DIR__ . '/build/**/block.json' );
+
+		// auto register all blocks that were found.
+		foreach ( $block_json_files as $filename ) {
+			$block_folder = dirname( $filename );
+			register_block_type( $block_folder );
+		};
+	};
+}
