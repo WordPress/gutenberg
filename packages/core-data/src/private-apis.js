30736341534bc3d4b4aae448405e32cd1a20fa2b
Ella van Durpe <ella@vandurpe.com>
2023-06-02 19:14:41 +0300

    Fix tests

--- packages/core-data/src/private-apis.js
+++ packages/core-data/src/private-apis.js
@@ -6,5 +6,5 @@ import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/pri
 export const { lock, unlock } =
 	__dangerousOptInToUnstableAPIsOnlyForCoreModules(
 		'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.',
-		'@wordpress/block-library'
+		'@wordpress/core-data'
 	);
