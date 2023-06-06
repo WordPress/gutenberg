7fbe8608c9fb500960025143e1034c8a0de5c027
Glen Davies <glen.davies@automattic.com>
2023-06-07 10:45:47 +1200

    Linting fixes

--- lib/compat/wordpress-6.3/block-patterns.php
+++ lib/compat/wordpress-6.3/block-patterns.php
@@ -1,6 +1,48 @@
 <?php
 /**
- * Add categories tp new wp_patterns taxonomy for WP 6.3.
+ * Overrides Core's wp-includes/block-patterns.php to add new wp_patterns taxonomy for WP 6.3.
+ *
+ * @package gutenberg
+ */
+
+ /**
+ * Adds a new taxonomy for organizing patterns.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.3.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/51144
+ *
+ * @return void
+ */
+function gutenberg_register_taxonomy_patterns() {
+	$labels = array(
+		'name'          => _x( 'Patterns', 'taxonomy general name' ),
+		'singular_name' => _x( 'Pattern', 'taxonomy singular name' ),
+		'search_items'  => __( 'Search Patterns' ),
+		'all_items'     => __( 'All Pattern Categories' ),
+		'edit_item'     => __( 'Edit Pattern Category' ),
+		'update_item'   => __( 'Update Pattern Category' ),
+		'add_new_item'  => __( 'Add New Pattern Category' ),
+		'new_item_name' => __( 'New Pattern Category Name' ),
+		'menu_name'     => __( 'Pattern' ),
+	);
+	$args   = array(
+		'hierarchical'      => false,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_in_menu'      => true,
+		'show_in_nav_menus' => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'show_in_rest'      => true,
+		'rewrite'           => array( 'slug' => 'wp_pattern' ),
+	);
+	register_taxonomy( 'wp_pattern', array( 'wp_block' ), $args );
+}
+add_action( 'init', 'gutenberg_register_taxonomy_patterns' );
+
+/**
+ * Add categories to new wp_patterns taxonomy for WP 6.3.
  *
  * @package gutenberg
  */
@@ -9,7 +51,7 @@ function gutenberg_register_wp_patterns_taxonomy_categories() {
 		array(
 			'slug'        => 'banner',
 			'label'       => _x( 'Banners', 'Block pattern category', 'gutenberg' ),
-			'description' => __( '', 'gutenberg' ),
+			'description' => __( 'Patterns used for adding banners', 'gutenberg' ),
 		),
 		array(
 			'slug'        => 'buttons',
@@ -56,7 +98,7 @@ function gutenberg_register_wp_patterns_taxonomy_categories() {
 		array(
 			'slug'        => 'services',
 			'label'       => _x( 'Services', 'Block pattern category', 'gutenberg' ),
-			'description' => __( 'Briefly describe what your business does and how you can help.', 'gutenberg' ),	
+			'description' => __( 'Briefly describe what your business does and how you can help.', 'gutenberg' ),
 		),
 		array(
 			'slug'        => 'contact',
