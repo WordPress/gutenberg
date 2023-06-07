b3ad57b96761f2fecc26d136d5b2d1f65f5ea273
Bernie Reiter <ockham@raz.or.at>
2023-06-07 09:58:21 +0200

    Revert "Change syntax again"
    This reverts commit 2ae6616a0199824842f081692adf2654f4fe1588.

--- lib/experimental/blocks.php
+++ lib/experimental/blocks.php
@@ -181,7 +181,7 @@ function gutenberg_register_auto_inserted_blocks( $settings, $metadata ) {
 	);
 
 	$auto_insert = $metadata['autoInsert'];
-	foreach ( $auto_insert as $position => $block ) {
+	foreach ( $auto_insert as $block => $position ) {
 		if ( ! isset( $property_mappings[ $position ] ) ) {
 			continue;
 		}
