285504aa7366a50e983d25bbcd8e4cae8af60053
Alex Lende <alex@lende.xyz>
2023-06-08 12:04:00 -0500

    Add JSDoc types for debugging

--- packages/block-editor/src/components/image-size-control/index.js
+++ packages/block-editor/src/components/image-size-control/index.js
@@ -13,6 +13,51 @@ import { __ } from '@wordpress/i18n';
 const IMAGE_SIZE_PRESETS = [ 25, 50, 75, 100 ];
 const noop = () => {};
 
+/**
+ * @typedef {Object} ImageSizeOptions
+ * @property {string} value Image size slug.
+ * @property {string} label Image size label.
+ */
+
+/**
+ * @typedef {Object} ImageSize
+ * @property {number} [width]  Image width.
+ * @property {number} [height] Image height.
+ */
+
+/**
+ * @callback OnChange
+ * @param {ImageSize} imageSize Image size object.
+ * @return {void}
+ */
+
+/**
+ * @callback OnChangeImage
+ * @param {string} slug Image size slug.
+ * @return {void}
+ */
+
+/**
+ * @typedef {Object} ImageSizeControlProps
+ * @property {string}             [imageSizeHelp]       Help text for the image size select control.
+ * @property {number}             [width]               Specified width for the image.
+ * @property {number}             [height]              Specified height for the image.
+ * @property {number}             [naturalWidth]        Width of the image source.
+ * @property {number}             [naturalHeight]       Height of the image source.
+ * @property {ImageSizeOptions[]} [imageSizeOptions=[]] Array of image size options.
+ * @property {boolean}            [isResizable=true]    Whether the image is resizable.
+ * @property {string}             [slug]                Slug of the selected image size.
+ * @property {OnChange}           [onChange]            Function to call when the styled image size changes.
+ * @property {OnChangeImage}      [onChangeImage=noop]  Function to call when the source image resolution changes.
+ */
+
+/**
+ * Image size control.
+ *
+ * @param {ImageSizeControlProps} props Component props.
+ *
+ * @return {import('@wordpress/element').WPElement} Image size control.
+ */
 export default function ImageSizeControl( {
 	imageSizeHelp,
 	width,
