ab685cdf8e199573846fe21dc23a0cb41ae4d4a8
Thomas Roberts <thomas.roberts@automattic.com>
2023-06-08 10:41:08 +0100

    Export openPostInEditor function from Admin utils

--- packages/e2e-test-utils-playwright/src/admin/index.ts
+++ packages/e2e-test-utils-playwright/src/admin/index.ts
@@ -8,6 +8,7 @@ import type { Browser, Page, BrowserContext } from '@playwright/test';
  */
 import { createNewPost } from './create-new-post';
 import { getPageError } from './get-page-error';
+import { openPostInEditor } from './open-post-in-editor';
 import { visitAdminPage } from './visit-admin-page';
 import { visitSiteEditor } from './visit-site-editor';
 import type { PageUtils } from '../page-utils';
@@ -34,6 +35,8 @@ export class Admin {
 	createNewPost: typeof createNewPost = createNewPost.bind( this );
 	/** @borrows getPageError as this.getPageError */
 	getPageError: typeof getPageError = getPageError.bind( this );
+	/** @borrows openPostInEditor as this.openPostInEditor */
+	openPostInEditor: typeof openPostInEditor = openPostInEditor.bind( this );
 	/** @borrows visitAdminPage as this.visitAdminPage */
 	visitAdminPage: typeof visitAdminPage = visitAdminPage.bind( this );
 	/** @borrows visitSiteEditor as this.visitSiteEditor */
