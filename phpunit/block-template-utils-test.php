ceb98ab53657a735da83e35cd9778a37e258e8db
ramon <ramonjd@gmail.com>
2023-06-09 13:14:35 +1000

    All these file changes are so that we can add `modified` to the template and template part objects in the rest response for WP_REST_Templates_Controller For theme templates, it will add the modified value when we have a post id (which is once the template is first save after user change)

--- phpunit/block-template-utils-test.php
+++ phpunit/block-template-utils-test.php
@@ -6,12 +6,18 @@
  */
 
 class Tests_Block_Template_Utils extends WP_UnitTestCase {
+	const TEST_THEME = 'emptytheme';
+
+	private static $template_post;
+
+	private static $template_part_post;
+
 	public function set_up() {
 		parent::set_up();
 		switch_theme( 'emptytheme' );
 	}
 
-	public static function wpSetUpBeforeClass() {
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 		register_post_type(
 			'custom_book',
 			array(
@@ -20,6 +26,46 @@ class Tests_Block_Template_Utils extends WP_UnitTestCase {
 			)
 		);
 		register_taxonomy( 'book_type', 'custom_book' );
+
+		// Set up template post.
+		self::$template_post = $factory->post->create_and_get(
+			array(
+				'post_type'    => 'wp_template',
+				'post_name'    => 'my_template',
+				'post_title'   => 'My Template',
+				'post_content' => 'Content',
+				'post_excerpt' => 'Description of my template',
+				'tax_input'    => array(
+					'wp_theme' => array(
+						self::TEST_THEME,
+					),
+				),
+			)
+		);
+
+		wp_set_post_terms( self::$template_post->ID, self::TEST_THEME, 'wp_theme' );
+
+		// Set up template part post.
+		self::$template_part_post = $factory->post->create_and_get(
+			array(
+				'post_type'    => 'wp_template_part',
+				'post_name'    => 'my_template_part',
+				'post_title'   => 'My Template Part',
+				'post_content' => 'Content',
+				'post_excerpt' => 'Description of my template part',
+				'tax_input'    => array(
+					'wp_theme'              => array(
+						self::TEST_THEME,
+					),
+					'wp_template_part_area' => array(
+						WP_TEMPLATE_PART_AREA_HEADER,
+					),
+				),
+			)
+		);
+
+		wp_set_post_terms( self::$template_part_post->ID, WP_TEMPLATE_PART_AREA_HEADER, 'wp_template_part_area' );
+		wp_set_post_terms( self::$template_part_post->ID, self::TEST_THEME, 'wp_theme' );
 	}
 
 	public static function wpTearDownAfterClass() {
@@ -249,5 +295,41 @@ class Tests_Block_Template_Utils extends WP_UnitTestCase {
 			),
 			$hierarchy
 		);
+	}
+
+	public function test_build_block_template_result_from_post() {
+		$template = _gutenberg_build_block_template_result_from_post(
+			self::$template_post,
+			'wp_template'
+		);
+
+		$this->assertNotWPError( $template );
+		$this->assertSame( get_stylesheet() . '//my_template', $template->id );
+		$this->assertSame( get_stylesheet(), $template->theme );
+		$this->assertSame( 'my_template', $template->slug );
+		$this->assertSame( 'publish', $template->status );
+		$this->assertSame( 'custom', $template->source );
+		$this->assertSame( 'My Template', $template->title );
+		$this->assertSame( 'Description of my template', $template->description );
+		$this->assertSame( 'wp_template', $template->type );
+		$this->assertSame( self::$template_post->post_modified, $template->modified );
+
+		// Test template parts.
+		$template_part = _gutenberg_build_block_template_result_from_post(
+			self::$template_part_post,
+			'wp_template_part'
+		);
+
+		$this->assertNotWPError( $template_part );
+		$this->assertSame( get_stylesheet() . '//my_template_part', $template_part->id );
+		$this->assertSame( get_stylesheet(), $template_part->theme );
+		$this->assertSame( 'my_template_part', $template_part->slug );
+		$this->assertSame( 'publish', $template_part->status );
+		$this->assertSame( 'custom', $template_part->source );
+		$this->assertSame( 'My Template Part', $template_part->title );
+		$this->assertSame( 'Description of my template part', $template_part->description );
+		$this->assertSame( 'wp_template_part', $template_part->type );
+		$this->assertSame( WP_TEMPLATE_PART_AREA_HEADER, $template_part->area );
+		$this->assertSame( self::$template_part_post->post_modified, $template->modified );
 	}
 }
