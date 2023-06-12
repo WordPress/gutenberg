97875d2a1c04ced5cff975f804d2eb54fe56dab7
James Koster <james@jameskoster.co.uk>
2023-06-12 16:46:31 +0100

    Update template titles

--- lib/compat/wordpress-6.2/block-template-utils.php
+++ lib/compat/wordpress-6.2/block-template-utils.php
@@ -110,7 +110,7 @@ function gutenberg_get_template_hierarchy( $slug, $is_custom = false, $template_
 function gutenberg_get_default_block_template_types( $default_template_types ) {
 	if ( isset( $default_template_types['index'] ) ) {
 		$default_template_types['index'] = array(
-			'title'       => _x( 'Index', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'All Posts & Pages', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Used as a fallback template for all pages when a more specific template is not defined.',
 				'gutenberg'
@@ -119,7 +119,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['home'] ) ) {
 		$default_template_types['home'] = array(
-			'title'       => _x( 'Home', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Blog page', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Displays the latest posts as either the site homepage or a custom page defined under reading settings. If it exists, the Front Page template overrides this template when posts are shown on the front page.',
 				'gutenberg'
@@ -128,7 +128,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['front-page'] ) ) {
 		$default_template_types['front-page'] = array(
-			'title'       => _x( 'Front Page', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Homepage', 'Template name', 'gutenberg' ),
 			'description' => __(
 				"Displays your site's front page, whether it is set to display latest posts or a static page. The Front Page template takes precedence over all templates.",
 				'gutenberg'
@@ -137,7 +137,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['singular'] ) ) {
 		$default_template_types['singular'] = array(
-			'title'       => _x( 'Singular', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Single Entires', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Displays any single entry, such as a post or a page. This template will serve as a fallback when a more specific template (e.g., Single Post, Page, or Attachment) cannot be found.',
 				'gutenberg'
@@ -146,19 +146,19 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['single'] ) ) {
 		$default_template_types['single'] = array(
-			'title'       => _x( 'Single', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Single Posts', 'Template name', 'gutenberg' ),
 			'description' => __( 'Displays single posts on your website unless a custom template has been applied to that post or a dedicated template exists.', 'gutenberg' ),
 		);
 	}
 	if ( isset( $default_template_types['page'] ) ) {
 		$default_template_types['page'] = array(
-			'title'       => _x( 'Page', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Pages', 'Template name', 'gutenberg' ),
 			'description' => __( 'Display all static pages unless a custom template has been applied or a dedicated template exists.', 'gutenberg' ),
 		);
 	}
 	if ( isset( $default_template_types['archive'] ) ) {
 		$default_template_types['archive'] = array(
-			'title'       => _x( 'Archive', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'All Archives', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Displays any archive, including posts by a single author, category, tag, taxonomy, custom post type, and date. This template will serve as a fallback when more specific templates (e.g., Category or Tag) cannot be found.',
 				'gutenberg'
@@ -167,7 +167,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['author'] ) ) {
 		$default_template_types['author'] = array(
-			'title'       => _x( 'Author', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Author Archives', 'Template name', 'gutenberg' ),
 			'description' => __(
 				"Displays a single author's post archive. This template will serve as a fallback when a more a specific template (e.g., Author: Admin) cannot be found.",
 				'gutenberg'
@@ -176,7 +176,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['category'] ) ) {
 		$default_template_types['category'] = array(
-			'title'       => _x( 'Category', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Category Archives', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Displays a post category archive. This template will serve as a fallback when more specific template (e.g., Category: Recipes) cannot be found.',
 				'gutenberg'
@@ -194,13 +194,13 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['date'] ) ) {
 		$default_template_types['date'] = array(
-			'title'       => _x( 'Date', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Date Archives', 'Template name', 'gutenberg' ),
 			'description' => __( 'Displays a post archive when a specific date is visited (e.g., example.com/2023/).', 'gutenberg' ),
 		);
 	}
 	if ( isset( $default_template_types['tag'] ) ) {
 		$default_template_types['tag'] = array(
-			'title'       => _x( 'Tag', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Tag Archives', 'Template name', 'gutenberg' ),
 			'description' => __(
 				'Displays a post tag archive. This template will serve as a fallback when more specific template (e.g., Tag: Pizza) cannot be found.',
 				'gutenberg'
@@ -209,13 +209,13 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['attachment'] ) ) {
 		$default_template_types['attachment'] = array(
-			'title'       => _x( 'Media', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Attachment Pages', 'Template name', 'gutenberg' ),
 			'description' => __( 'Displays when a visitor views the dedicated page that exists for any media attachment.', 'gutenberg' ),
 		);
 	}
 	if ( isset( $default_template_types['search'] ) ) {
 		$default_template_types['search'] = array(
-			'title'       => _x( 'Search', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Search Results', 'Template name', 'gutenberg' ),
 			'description' => __( 'Displays when a visitor performs a search on your website.', 'gutenberg' ),
 		);
 	}
@@ -230,7 +230,7 @@ function gutenberg_get_default_block_template_types( $default_template_types ) {
 	}
 	if ( isset( $default_template_types['404'] ) ) {
 		$default_template_types['404'] = array(
-			'title'       => _x( '404', 'Template name', 'gutenberg' ),
+			'title'       => _x( 'Error: 404', 'Template name', 'gutenberg' ),
 			'description' => __( 'Displays when a visitor views a non-existent page, such as a dead link or a mistyped URL.', 'gutenberg' ),
 		);
 	}
