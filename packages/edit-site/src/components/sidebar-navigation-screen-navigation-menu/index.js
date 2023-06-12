2eedae9cad3ca56464331140080c82aeae6be945
Maggie <maggie.cabrera@automattic.com>
2023-06-12 12:43:58 +0200

    Allow renaming, duplication and deleting of Navigation menus from Browse Mode Sidebar (#50880)
    * added more menu to navigation with delete menu option
    
    * modal for the rename function
    
    * prop refactoring, title on rename popup
    
    * very ugly renaming of the menu
    
    * duplicate function
    
    * added snackbar notices to each action
    
    * navigate to correct places on delete and duplicate. Added Copy after title on duplication
    
    * i18n
    
    Co-authored-by: Ben Dwyer <ben@scruffian.com>
    
    * Another i18n
    
    Co-authored-by: Ben Dwyer <ben@scruffian.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ben Dwyer <ben@scruffian.com>
    
    * Use self documenting var name
    
    * Use consistent confirmatory action wording
    
    * Refactor MoreMenu to component
    
    * Extract Modal to seperate component
    
    * Refactor change function
    
    * Use local state and only edit record on “save” confirmation
    
    * Localise state closer to where it’s needed
    
    * Disable “Save” button until menu title is modified
    
    * Remove file import introduced during rebase
    
    * Handle Save errors
    
    * Error handling for Delete action
    
    * Force all actions to throw on error
    
    * Add error handling for Duplicate.
    
    Also pass error message and not error object.
    
    * Improve loading feedback when saving or deleting
    
    * Make prop naming consistent
    
    * i18n of modal title
    
    * Add confirmatory step prior to delete action
    
    * Fix rename input focus border clipping
    
    * Use default variant to get correct styles
    
    * Make `Copy` translatable
    
    * Updating confirmatory wording
    
    ---------
    
    Co-authored-by: Dave Smith <getdavemail@gmail.com>
    Co-authored-by: Ben Dwyer <ben@scruffian.com>

--- packages/edit-site/src/components/sidebar-navigation-screen-navigation-menu/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-navigation-menu/index.js
@@ -1,18 +1,20 @@
 /**
  * WordPress dependencies
  */
-import { useEntityRecord } from '@wordpress/core-data';
+import { useEntityRecord, store as coreStore } from '@wordpress/core-data';
 import {
 	__experimentalUseNavigator as useNavigator,
 	Spinner,
 } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useCallback, useMemo } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 import { BlockEditorProvider } from '@wordpress/block-editor';
 import { createBlock } from '@wordpress/blocks';
 import { decodeEntities } from '@wordpress/html-entities';
+
+import { store as noticesStore } from '@wordpress/notices';
 
 /**
  * Internal dependencies
@@ -25,23 +27,166 @@ import {
 } from '../../utils/is-previewing-theme';
 import { SidebarNavigationScreenWrapper } from '../sidebar-navigation-screen-navigation-menus';
 import NavigationMenuContent from '../sidebar-navigation-screen-navigation-menus/navigation-menu-content';
+import ScreenNavigationMoreMenu from './more-menu';
 
 const { useHistory } = unlock( routerPrivateApis );
 const noop = () => {};
 
 export default function SidebarNavigationScreenNavigationMenu() {
+	const {
+		deleteEntityRecord,
+		saveEntityRecord,
+		editEntityRecord,
+		saveEditedEntityRecord,
+	} = useDispatch( coreStore );
+
+	const { createSuccessNotice, createErrorNotice } =
+		useDispatch( noticesStore );
+
 	const postType = `wp_navigation`;
 	const {
+		goTo,
 		params: { postId },
 	} = useNavigator();
 
-	const { record: navigationMenu, isResolving: isLoading } = useEntityRecord(
+	const { record: navigationMenu, isResolving } = useEntityRecord(
 		'postType',
 		postType,
 		postId
 	);
 
+	const { getEditedEntityRecord, isSaving, isDeleting } = useSelect(
+		( select ) => {
+			const {
+				isSavingEntityRecord,
+				isDeletingEntityRecord,
+				getEditedEntityRecord: getEditedEntityRecordSelector,
+			} = select( coreStore );
+
+			return {
+				isSaving: isSavingEntityRecord( 'postType', postType, postId ),
+				isDeleting: isDeletingEntityRecord(
+					'postType',
+					postType,
+					postId
+				),
+				getEditedEntityRecord: getEditedEntityRecordSelector,
+			};
+		},
+		[ postId, postType ]
+	);
+
+	const isLoading = isResolving || isSaving || isDeleting;
+
 	const menuTitle = navigationMenu?.title?.rendered || navigationMenu?.slug;
+
+	const handleSave = async ( edits = {} ) => {
+		// Prepare for revert in case of error.
+		const originalRecord = getEditedEntityRecord(
+			'postType',
+			'wp_navigation',
+			postId
+		);
+
+		// Apply the edits.
+		editEntityRecord( 'postType', postType, postId, edits );
+
+		// Attempt to persist.
+		try {
+			await saveEditedEntityRecord( 'postType', postType, postId, {
+				throwOnError: true,
+			} );
+			createSuccessNotice( __( 'Renamed Navigation menu' ), {
+				type: 'snackbar',
+			} );
+		} catch ( error ) {
+			// Revert to original in case of error.
+			editEntityRecord( 'postType', postType, postId, originalRecord );
+
+			createErrorNotice(
+				sprintf(
+					/* translators: %s: error message describing why the navigation menu could not be renamed. */
+					__( `Unable to rename Navigation menu (%s).` ),
+					error?.message
+				),
+
+				{
+					type: 'snackbar',
+				}
+			);
+		}
+	};
+
+	const handleDelete = async () => {
+		try {
+			await deleteEntityRecord(
+				'postType',
+				postType,
+				postId,
+				{
+					force: true,
+				},
+				{
+					throwOnError: true,
+				}
+			);
+			createSuccessNotice( __( 'Deleted Navigation menu' ), {
+				type: 'snackbar',
+			} );
+			goTo( '/navigation' );
+		} catch ( error ) {
+			createErrorNotice(
+				sprintf(
+					/* translators: %s: error message describing why the navigation menu could not be deleted. */
+					__( `Unable to delete Navigation menu (%s).` ),
+					error?.message
+				),
+
+				{
+					type: 'snackbar',
+				}
+			);
+		}
+	};
+	const handleDuplicate = async () => {
+		try {
+			const savedRecord = await saveEntityRecord(
+				'postType',
+				postType,
+				{
+					title: sprintf(
+						/* translators: %s: Navigation menu title */
+						__( '%s (Copy)' ),
+						menuTitle
+					),
+					content: navigationMenu?.content?.raw,
+					status: 'publish',
+				},
+				{
+					throwOnError: true,
+				}
+			);
+
+			if ( savedRecord ) {
+				createSuccessNotice( __( 'Duplicated Navigation menu' ), {
+					type: 'snackbar',
+				} );
+				goTo( `/navigation/${ postType }/${ savedRecord.id }` );
+			}
+		} catch ( error ) {
+			createErrorNotice(
+				sprintf(
+					/* translators: %s: error message describing why the navigation menu could not be deleted. */
+					__( `Unable to duplicate Navigation menu (%s).` ),
+					error?.message
+				),
+
+				{
+					type: 'snackbar',
+				}
+			);
+		}
+	};
 
 	if ( isLoading ) {
 		return (
@@ -74,6 +219,14 @@ export default function SidebarNavigationScreenNavigationMenu() {
 
 	return (
 		<SidebarNavigationScreenWrapper
+			actions={
+				<ScreenNavigationMoreMenu
+					menuTitle={ decodeEntities( menuTitle ) }
+					onDelete={ handleDelete }
+					onSave={ handleSave }
+					onDuplicate={ handleDuplicate }
+				/>
+			}
 			title={ decodeEntities( menuTitle ) }
 			description={ __(
 				'Navigation menus are a curated collection of blocks that allow visitors to get around your site.'
