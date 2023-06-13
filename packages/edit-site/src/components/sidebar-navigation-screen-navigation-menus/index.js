3999707fa21a2c705b2b1a40d09cb633dbb540e3
scruffian <ben@scruffian.com>
2023-06-13 10:10:13 +0100

    Add a draft status to the navigation name

--- packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useEntityRecords } from '@wordpress/core-data';
 
 import { decodeEntities } from '@wordpress/html-entities';
@@ -20,6 +20,25 @@ import { PRELOADED_NAVIGATION_MENUS_QUERY } from './constants';
 import { useLink } from '../routes/link';
 import SingleNavigationMenu from '../sidebar-navigation-screen-navigation-menu/single-navigation-menu';
 import useNavigationMenuHandlers from '../sidebar-navigation-screen-navigation-menu/use-navigation-menu-handlers';
+
+// Copied from packages/block-library/src/navigation/edit/navigation-menu-selector.js.
+function buildMenuLabel( title, id, status ) {
+	if ( ! title?.rendered ) {
+		/* translators: %s is the index of the menu in the list of menus. */
+		return sprintf( __( '(no title %s)' ), id );
+	}
+
+	if ( status === 'publish' ) {
+		return decodeEntities( title?.rendered );
+	}
+
+	return sprintf(
+		// translators: %1s: title of the menu; %2s: status of the menu (draft, pending, etc.).
+		__( '%1$s (%2$s)' ),
+		decodeEntities( title?.rendered ),
+		status
+	);
+}
 
 export default function SidebarNavigationScreenNavigationMenus() {
 	const { records: navigationMenus, isResolving: isLoading } =
@@ -67,16 +86,14 @@ export default function SidebarNavigationScreenNavigationMenus() {
 	return (
 		<SidebarNavigationScreenWrapper>
 			<ItemGroup>
-				{ navigationMenus?.map( ( navMenu ) => (
+				{ navigationMenus?.map( ( { id, title, status }, index ) => (
 					<NavMenuItem
-						postId={ navMenu.id }
-						key={ navMenu.id }
+						postId={ id }
+						key={ id }
 						withChevron
 						icon={ navigation }
 					>
-						{ decodeEntities(
-							navMenu.title?.rendered || navMenu.slug
-						) }
+						{ buildMenuLabel( title, index + 1, status ) }
 					</NavMenuItem>
 				) ) }
 			</ItemGroup>
