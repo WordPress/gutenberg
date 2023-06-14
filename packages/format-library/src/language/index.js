abec2ba3901678715b102d94d9363ee57631d48a
George Mamadashvili <georgemamadashvili@gmail.com>
2023-06-14 12:13:00 +0400

    Formats: Avoid rerendering language edit component when typing (#51440)

--- packages/format-library/src/language/index.js
+++ packages/format-library/src/language/index.js
@@ -29,21 +29,10 @@ export const language = {
 	title,
 };
 
-function Edit( props ) {
-	const { contentRef, isActive, onChange, value } = props;
-	const popoverAnchor = useAnchor( {
-		editableContentElement: contentRef.current,
-		settings: language,
-	} );
-
-	const [ lang, setLang ] = useState( '' );
-	const [ dir, setDir ] = useState( 'ltr' );
-
+function Edit( { isActive, value, onChange, contentRef } ) {
 	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
 	const togglePopover = () => {
 		setIsPopoverVisible( ( state ) => ! state );
-		setLang( '' );
-		setDir( 'ltr' );
 	};
 
 	return (
@@ -62,63 +51,81 @@ function Edit( props ) {
 				isActive={ isActive }
 				role="menuitemcheckbox"
 			/>
-
 			{ isPopoverVisible && (
-				<Popover
-					className="block-editor-format-toolbar__language-popover"
-					anchor={ popoverAnchor }
-					placement="bottom"
+				<InlineLanguageUI
+					value={ value }
+					onChange={ onChange }
 					onClose={ togglePopover }
-				>
-					<form
-						className="block-editor-format-toolbar__language-container-content"
-						onSubmit={ ( event ) => {
-							onChange(
-								applyFormat( value, {
-									type: name,
-									attributes: {
-										lang,
-										dir,
-									},
-								} )
-							);
-							togglePopover();
-							event.preventDefault();
-						} }
-					>
-						<TextControl
-							label={ title }
-							value={ lang }
-							onChange={ ( val ) => setLang( val ) }
-							help={ __(
-								'A valid language attribute, like "en" or "fr".'
-							) }
-						/>
-						<SelectControl
-							label={ __( 'Text direction' ) }
-							value={ dir }
-							options={ [
-								{
-									label: __( 'Left to right' ),
-									value: 'ltr',
-								},
-								{
-									label: __( 'Right to left' ),
-									value: 'rtl',
-								},
-							] }
-							onChange={ ( val ) => setDir( val ) }
-						/>
-						<HStack alignment="right">
-							<Button
-								variant="primary"
-								type="submit"
-								text={ __( 'Apply' ) }
-							/>
-						</HStack>
-					</form>
-				</Popover>
+					contentRef={ contentRef }
+				/>
 			) }
 		</>
+	);
+}
+
+function InlineLanguageUI( { value, contentRef, onChange, onClose } ) {
+	const popoverAnchor = useAnchor( {
+		editableContentElement: contentRef.current,
+		settings: language,
+	} );
+
+	const [ lang, setLang ] = useState( '' );
+	const [ dir, setDir ] = useState( 'ltr' );
+
+	return (
+		<Popover
+			className="block-editor-format-toolbar__language-popover"
+			anchor={ popoverAnchor }
+			placement="bottom"
+			onClose={ onClose }
+		>
+			<form
+				className="block-editor-format-toolbar__language-container-content"
+				onSubmit={ ( event ) => {
+					event.preventDefault();
+					onChange(
+						applyFormat( value, {
+							type: name,
+							attributes: {
+								lang,
+								dir,
+							},
+						} )
+					);
+					onClose();
+				} }
+			>
+				<TextControl
+					label={ title }
+					value={ lang }
+					onChange={ ( val ) => setLang( val ) }
+					help={ __(
+						'A valid language attribute, like "en" or "fr".'
+					) }
+				/>
+				<SelectControl
+					label={ __( 'Text direction' ) }
+					value={ dir }
+					options={ [
+						{
+							label: __( 'Left to right' ),
+							value: 'ltr',
+						},
+						{
+							label: __( 'Right to left' ),
+							value: 'rtl',
+						},
+					] }
+					onChange={ ( val ) => setDir( val ) }
+				/>
+				<HStack alignment="right">
+					<Button
+						variant="primary"
+						type="submit"
+						text={ __( 'Apply' ) }
+					/>
+				</HStack>
+			</form>
+		</Popover>
 	);
 }
