9a725bf1205d96293cd1a6056b05bc1ec6242a69
David Arenas <david.arenas@automattic.com>
2023-06-14 23:36:02 +0200

    Refactor fixtures and use them for bind and effect

--- test/e2e/specs/interactivity/fixtures/index.ts
+++ test/e2e/specs/interactivity/fixtures/index.ts
@@ -1,68 +1,25 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
-import { test as base, type Page } from '@playwright/test';
-import { join } from 'path';
+import { test as base } from '@wordpress/e2e-test-utils-playwright';
+export { expect } from '@wordpress/e2e-test-utils-playwright';
+
+/**
+ * Internal dependencies
+ */
+import InteractivityUtils from './interactivity-utils';
 
 type Fixtures = {
-	/**
-	 * Allow visiting local HTML files as if they were under a real domain,
-	 * mainly to avoid errors from `fetch` calls
-	 *
-	 * It looks for HTML files inside the `/e2e/html` folder, and uses
-	 * `Page.goto` under the hood.
-	 *
-	 * @example
-	 * ```ts
-	 * test.beforeEach(async ({ goToFile }) => {
-	 *     await goToFile('directives-context.html');
-	 * });
-	 * ```
-	 *
-	 * @param filename The name of the HTML file to visit.
-	 * @param options  Same options object accepted by `page.goto`.
-	 *
-	 * @return Promise.
-	 */
-	goToFile: (
-		...params: Parameters< Page[ 'goto' ] >
-	) => ReturnType< Page[ 'goto' ] >;
+	interactivityUtils: InteractivityUtils;
 };
 
 export const test = base.extend< Fixtures >( {
-	goToFile: async ( { page }, use ) => {
-		await page.route( '**/*.html', async ( route, req ) => {
-			const { pathname } = new URL( req.url() );
-			await route.fulfill( {
-				path: join( __dirname, '../html', pathname ),
-			} );
-		} );
-
-		await page.route( '**/*.js', async ( route, req ) => {
-			const { pathname } = new URL( req.url() );
-
-			const path = pathname.endsWith( 'build/interactivity/index.min.js' )
-				? join(
-						__dirname,
-						'../../../../../build/interactivity/index.min.js'
-				  )
-				: join( __dirname, '..', pathname );
-
-			await route.fulfill( { path } );
-		} );
-
-		await page.route( '**/*.css', async ( route, req ) => {
-			const { pathname } = new URL( req.url() );
-			await route.fulfill( {
-				path: join( __dirname, '..', pathname ),
-			} );
-		} );
-
-		await use( async ( filename, options ) => {
-			const url = new URL( filename, 'http://a.b' ).href;
-			return await page.goto( url, options );
-		} );
-	},
+	interactivityUtils: [
+		async ( { requestUtils }, use ) => {
+			await use( new InteractivityUtils( { requestUtils } ) );
+		},
+		// @ts-ignore: The required type is 'test', but can be 'worker' too. See
+		// https://playwright.dev/docs/test-fixtures#worker-scoped-fixtures
+		{ scope: 'worker' },
+	],
 } );
-
-export { expect } from '@playwright/test';
---
+++ test/e2e/specs/interactivity/fixtures/interactivity-utils.ts
@@ -0,0 +1,39 @@
+/**
+ * WordPress dependencies
+ */
+import type { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
+
+export default class InteractivityUtils {
+	posts: Map< string, number >;
+	requestUtils: RequestUtils;
+
+	constructor( { requestUtils }: { requestUtils: RequestUtils } ) {
+		this.posts = new Map();
+		this.requestUtils = requestUtils;
+	}
+
+	async addPostWithBlock( blockName: string ) {
+		const payload = {
+			content: `<!-- wp:${ blockName } /-->`,
+			status: 'publish' as 'publish',
+			date_gmt: '2023-01-01T00:00:00',
+		};
+
+		const { id } = await this.requestUtils.createPost( payload );
+		this.posts.set( blockName, id );
+	}
+
+	async activatePlugins() {
+		await this.requestUtils.activateTheme( 'emptytheme' );
+		await this.requestUtils.activatePlugin(
+			'gutenberg-test-interactive-blocks'
+		);
+	}
+
+	async deactivatePlugins() {
+		await this.requestUtils.activateTheme( 'twentytwentyone' );
+		await this.requestUtils.deactivatePlugin(
+			'gutenberg-test-interactive-blocks'
+		);
+	}
+}
