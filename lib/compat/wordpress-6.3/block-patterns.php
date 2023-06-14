278bc623859879aa89484df68bc9cbf6409f2a1f
Glen Davies <glen.davies@automattic.com>
2023-06-14 13:48:35 +1200

    Make new taxonomy private for now

--- lib/compat/wordpress-6.3/block-patterns.php
+++ lib/compat/wordpress-6.3/block-patterns.php
@@ -18,24 +18,14 @@ function gutenberg_register_taxonomy_patterns() {
 	$labels = array(
 		'name'          => _x( 'Pattern Categories', 'taxonomy general name' ),
 		'singular_name' => _x( 'Pattern Category', 'taxonomy singular name' ),
-		'search_items'  => __( 'Search Pattern Categoriess' ),
-		'all_items'     => __( 'All Pattern Categories' ),
-		'edit_item'     => __( 'Edit Pattern Category' ),
-		'update_item'   => __( 'Update Pattern Category' ),
-		'add_new_item'  => __( 'Add New Pattern Category' ),
-		'new_item_name' => __( 'New Pattern Category Name' ),
-		'menu_name'     => __( 'Pattern Categories' ),
 	);
 	$args   = array(
 		'hierarchical'      => false,
 		'labels'            => $labels,
-		'show_ui'           => true,
-		'show_in_menu'      => true,
-		'show_in_nav_menus' => true,
-		'show_admin_column' => true,
-		'query_var'         => true,
+		'show_ui'           => false,
 		'show_in_rest'      => true,
 		'rewrite'           => array( 'slug' => 'wp_pattern' ),
+		'public' => false
 	);
 	register_taxonomy( 'wp_pattern', array( 'wp_block' ), $args );
 }
