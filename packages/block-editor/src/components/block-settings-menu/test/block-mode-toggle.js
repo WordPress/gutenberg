e6342a704bf828a86f52bae0773a9b831510d30e
Marco Ciampini <marco.ciampo@gmail.com>
2023-06-15 21:19:56 +0200

    Fix unit test by wrapping the BlockModeToggle in dropdownmenu

--- packages/block-editor/src/components/block-settings-menu/test/block-mode-toggle.js
+++ packages/block-editor/src/components/block-settings-menu/test/block-mode-toggle.js
@@ -4,14 +4,24 @@
 import { render, screen } from '@testing-library/react';
 
 /**
+ * WordPress dependencies
+ */
+import { privateApis as componentsPrivateApis } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import { BlockModeToggle } from '../block-mode-toggle';
+import { unlock } from '../../../lock-unlock';
+
+const { DropdownMenuV2 } = unlock( componentsPrivateApis );
 
 describe( 'BlockModeToggle', () => {
 	it( "should not render the HTML mode button if the block doesn't support it", () => {
 		render(
-			<BlockModeToggle blockType={ { supports: { html: false } } } />
+			<DropdownMenuV2 defaultOpen>
+				<BlockModeToggle blockType={ { supports: { html: false } } } />
+			</DropdownMenuV2>
 		);
 
 		expect(
@@ -21,10 +31,12 @@ describe( 'BlockModeToggle', () => {
 
 	it( 'should render the HTML mode button', () => {
 		render(
-			<BlockModeToggle
-				blockType={ { supports: { html: true } } }
-				mode="visual"
-			/>
+			<DropdownMenuV2 defaultOpen>
+				<BlockModeToggle
+					blockType={ { supports: { html: true } } }
+					mode="visual"
+				/>
+			</DropdownMenuV2>
 		);
 
 		expect(
@@ -34,10 +46,12 @@ describe( 'BlockModeToggle', () => {
 
 	it( 'should render the Visual mode button', () => {
 		render(
-			<BlockModeToggle
-				blockType={ { supports: { html: true } } }
-				mode="html"
-			/>
+			<DropdownMenuV2 defaultOpen>
+				<BlockModeToggle
+					blockType={ { supports: { html: true } } }
+					mode="html"
+				/>
+			</DropdownMenuV2>
 		);
 
 		expect(
@@ -47,11 +61,13 @@ describe( 'BlockModeToggle', () => {
 
 	it( 'should not render the Visual mode button if code editing is disabled', () => {
 		render(
-			<BlockModeToggle
-				blockType={ { supports: { html: true } } }
-				mode="html"
-				isCodeEditingEnabled={ false }
-			/>
+			<DropdownMenuV2 defaultOpen>
+				<BlockModeToggle
+					blockType={ { supports: { html: true } } }
+					mode="html"
+					isCodeEditingEnabled={ false }
+				/>
+			</DropdownMenuV2>
 		);
 
 		expect(
