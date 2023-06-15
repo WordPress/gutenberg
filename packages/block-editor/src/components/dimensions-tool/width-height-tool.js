251b241140c46ca1d0207343e5092b0b75f5ec98
Alex Lende <alex@lende.xyz>
2023-06-15 11:52:53 -0500

    Add JSDoc types for WidthHeightTool

--- packages/block-editor/src/components/dimensions-tool/width-height-tool.js
+++ packages/block-editor/src/components/dimensions-tool/width-height-tool.js
@@ -17,6 +17,36 @@ const SingleColumnToolsPanelItem = styled( ToolsPanelItem )`
 	grid-column: span 1;
 `;
 
+/**
+ * @typedef {import('@wordpress/components/build-types/unit-control/types').WPUnitControlUnit} WPUnitControlUnit
+ */
+
+/**
+ * @typedef {Object} WidthHeightToolValue
+ * @property {string} [width]
+ * @property {string} [height]
+ */
+
+/**
+ * @callback WidthHeightToolOnChange
+ * @param {WidthHeightToolValue} nextValue
+ * @return {void}
+ */
+
+/**
+ * @typedef {Object} WidthHeightToolProps
+ * @property {string}                   [panelId]          ID of the panel that contains the controls.
+ * @property {WidthHeightToolValue}     [value]            Current dimensions values.
+ * @property {WidthHeightToolOnChange}  [onChange]         Callback to update the dimensions values.
+ * @property {WPUnitControlUnit[]}      [units]            Units options.
+ * @property {boolean}                  [isShownByDefault] Whether the panel is shown by default.
+ */
+
+/**
+ * Component that renders controls to edit the dimensions of an image or container.
+ * @param {WidthHeightToolProps} props The component props.
+ * @return {import('@wordpress/element').WPElement} The dimensions controls.
+ */
 export default function WidthHeightTool( {
 	panelId,
 	value,
