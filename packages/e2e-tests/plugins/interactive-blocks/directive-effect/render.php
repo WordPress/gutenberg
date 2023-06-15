a2898b4ffd20b4b41bdcd734985b65c368113ce1
David Arenas <david.arenas@automattic.com>
2023-06-15 12:00:15 +0200

    Create directive-effect block for e2e testing

---
+++ packages/e2e-tests/plugins/interactive-blocks/directive-effect/render.php
@@ -0,0 +1,19 @@
+<div data-wp-island>
+	<div data-wp-fakeshow="state.isOpen">
+		<input
+			data-testid="input"
+			data-wp-effect="effects.elementAddedToTheDOM"
+		/>
+	</div>
+
+	<div
+		data-wp-text="selectors.elementInTheDOM"
+		data-testid="element in the DOM"
+	></div>
+
+	<div data-wp-effect="effects.changeFocus"></div>
+
+	<button data-testid="toggle" data-wp-on.click="actions.toggle">
+		Update
+	</button>
+</div>
