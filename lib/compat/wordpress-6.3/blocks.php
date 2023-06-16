e5b1ab7eaf0a4f447108dcfaaf2b6615464c7a90
Glen Davies <glen.davies@automattic.com>
2023-06-16 11:27:39 +1000

    Rename reusable blocks to patterns & allow user created patterns

--- lib/compat/wordpress-6.3/blocks.php
+++ lib/compat/wordpress-6.3/blocks.php
@@ -26,3 +26,94 @@ function gutenberg_add_selectors_property_to_block_type_settings( $settings, $me
 	return $settings;
 }
 add_filter( 'block_type_metadata_settings', 'gutenberg_add_selectors_property_to_block_type_settings', 10, 2 );
+
+/**
+ * Adds custom fields support to the wp_block post type so an unsynced option can be added and renames
+ * from Reusable block to Pattern.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.3.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/51144
+ *
+ * @param array  $args Register post type args.
+ * @param string $post_type The post type string.
+ *
+ * @return array Register post type args.
+ */
+function gutenberg_add_custom_fields_to_wp_block( $args, $post_type ) {
+	if ( 'wp_block' === $post_type ) {
+		$args['labels']['name']                     = _x( 'Patterns', 'post type general name' );
+		$args['labels']['singular_name']            = _x( 'Pattern', 'post type singular name' );
+		$args['labels']['add_new_item']             = __( 'Add new Pattern' );
+		$args['labels']['new_item']                 = __( 'New Pattern' );
+		$args['labels']['edit_item']                = __( 'Edit Pattern' );
+		$args['labels']['view_item']                = __( 'View Pattern' );
+		$args['labels']['all_items']                = __( 'All Patterns' );
+		$args['labels']['search_items']             = __( 'Search Patterns' );
+		$args['labels']['not_found']                = __( 'No Patterns found.' );
+		$args['labels']['not_found_in_trash']       = __( 'No Patterns found in Trash.' );
+		$args['labels']['filter_items_list']        = __( 'Filter Patterns list' );
+		$args['labels']['items_list_navigation']    = __( 'Patterns list navigation' );
+		$args['labels']['items_list']               = __( 'Patterns list' );
+		$args['labels']['item_published']           = __( 'Pattern published.' );
+		$args['labels']['item_published_privately'] = __( 'Pattern published privately.' );
+		$args['labels']['item_reverted_to_draft']   = __( 'Pattern reverted to draft.' );
+		$args['labels']['item_scheduled']           = __( 'Pattern scheduled.' );
+		$args['labels']['item_updated']             = __( 'Pattern updated.' );
+		array_push( $args['supports'], 'custom-fields' );
+	}
+
+	return $args;
+}
+add_filter( 'register_post_type_args', 'gutenberg_add_custom_fields_to_wp_block', 10, 2 );
+
+/**
+ * Adds wp_block_sync_status meta fields to the wp_block post type so an unsynced option can be added.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.3.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/51144
+ *
+ * @return void
+ */
+function gutenberg_wp_block_register_post_meta() {
+	$post_type = 'wp_block';
+	register_post_meta(
+		$post_type,
+		'wp_block',
+		array(
+			'auth_callback'     => function() {
+				return current_user_can( 'edit_posts' );
+			},
+			'sanitize_callback' => 'gutenberg_wp_block_sanitize_post_meta',
+			'single'            => true,
+			'type'              => 'object',
+			'show_in_rest'      => array(
+				'schema' => array(
+					'type'       => 'object',
+					'properties' => array(
+						'sync_status' => array(
+							'type' => 'string',
+						),
+					),
+				),
+			),
+		)
+	);
+}
+/**
+ * Sanitizes the array of wp_block post meta categories array.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.3.
+ *
+ * @see https://github.com/WordPress/gutenberg/pull/51144
+ *
+ * @param array $meta_value Array of values to sanitize.
+ *
+ * @return array Sanitized array of values.
+ */
+function gutenberg_wp_block_sanitize_post_meta( $meta_value ) {
+	$meta_value['sync_status'] = sanitize_text_field( $meta_value['sync_status'] );
+	return $meta_value;
+}
+add_action( 'init', 'gutenberg_wp_block_register_post_meta' );
--- packages/reusable-blocks/src/components/reusable-blocks-menu-items/reusable-block-convert-button.js
+++ packages/reusable-blocks/src/components/reusable-blocks-menu-items/reusable-block-convert-button.js
@@ -14,10 +14,11 @@ import {
 	TextControl,
 	__experimentalHStack as HStack,
 	__experimentalVStack as VStack,
+	ToggleControl,
 } from '@wordpress/components';
 import { symbol } from '@wordpress/icons';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
 import { store as coreStore } from '@wordpress/core-data';
 
@@ -38,6 +39,7 @@ export default function ReusableBlockConvertButton( {
 	clientIds,
 	rootClientId,
 } ) {
+	const [ syncType, setSyncType ] = useState( 'unsynced' );
 	const [ isModalOpen, setIsModalOpen ] = useState( false );
 	const [ title, setTitle ] = useState( '' );
 	const canConvert = useSelect(
@@ -77,7 +79,7 @@ export default function ReusableBlockConvertButton( {
 
 			return _canConvert;
 		},
-		[ clientIds ]
+		[ clientIds, rootClientId ]
 	);
 
 	const { __experimentalConvertBlocksToReusable: convertBlocksToReusable } =
@@ -88,17 +90,36 @@ export default function ReusableBlockConvertButton( {
 	const onConvert = useCallback(
 		async function ( reusableBlockTitle ) {
 			try {
-				await convertBlocksToReusable( clientIds, reusableBlockTitle );
-				createSuccessNotice( __( 'Reusable block created.' ), {
-					type: 'snackbar',
-				} );
+				await convertBlocksToReusable(
+					clientIds,
+					reusableBlockTitle,
+					syncType
+				);
+				createSuccessNotice(
+					sprintf(
+						// translators: %s: The sync status of the block that is created.
+						__( '%s created.' ),
+						syncType === 'fully'
+							? __( 'Synced Pattern' )
+							: __( 'Unsynced Pattern' )
+					),
+					{
+						type: 'snackbar',
+					}
+				);
 			} catch ( error ) {
 				createErrorNotice( error.message, {
 					type: 'snackbar',
 				} );
 			}
 		},
-		[ clientIds ]
+		[
+			convertBlocksToReusable,
+			clientIds,
+			syncType,
+			createSuccessNotice,
+			createErrorNotice,
+		]
 	);
 
 	if ( ! canConvert ) {
@@ -111,15 +132,13 @@ export default function ReusableBlockConvertButton( {
 				<>
 					<MenuItem
 						icon={ symbol }
-						onClick={ () => {
-							setIsModalOpen( true );
-						} }
+						onClick={ () => setIsModalOpen( true ) }
 					>
-						{ __( 'Create Reusable block' ) }
+						{ __( 'Create a Pattern' ) }
 					</MenuItem>
 					{ isModalOpen && (
 						<Modal
-							title={ __( 'Create Reusable block' ) }
+							title={ __( 'Create a pattern' ) }
 							onRequestClose={ () => {
 								setIsModalOpen( false );
 								setTitle( '' );
@@ -141,6 +160,23 @@ export default function ReusableBlockConvertButton( {
 										label={ __( 'Name' ) }
 										value={ title }
 										onChange={ setTitle }
+									/>
+
+									<ToggleControl
+										label={ __(
+											'Keep all pattern instances in sync'
+										) }
+										help={ __(
+											'Editing the original pattern will also update anywhere the pattern is used.'
+										) }
+										checked={ syncType === 'fully' }
+										onChange={ () => {
+											setSyncType(
+												syncType === 'fully'
+													? 'unsynced'
+													: 'fully'
+											);
+										} }
 									/>
 									<HStack justify="right">
 										<Button
