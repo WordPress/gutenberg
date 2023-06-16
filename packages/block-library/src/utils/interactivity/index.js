d001b5554d49d7f46a018576805ce13d6d912684
Luis Herranz <luisherranz@gmail.com>
2023-06-16 12:43:46 +0200

    Remove the hydration console log of the Interactivity API (#51571)

--- packages/block-library/src/utils/interactivity/index.js
+++ packages/block-library/src/utils/interactivity/index.js
@@ -12,6 +12,4 @@ registerDirectives();
 
 document.addEventListener( 'DOMContentLoaded', async () => {
 	await init();
-	// eslint-disable-next-line no-console
-	console.log( 'Interactivity API started' );
 } );
