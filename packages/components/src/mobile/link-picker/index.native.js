de6da369e8c7da118f5026c5ffc374e4e839723d
David Calhoun <github@davidcalhoun.me>
2023-06-17 08:51:41 -0400

    perf: Reduce unnecessary LinkPicker re-renders
    Combining state with an object in a single `useState` meant a new object
    was set to the state regardless of whether new values were present. This
    resulted in unexpected `act` warnings caused by unnecessary re-renders.
    
    Refactoring LinkPicker to separate the two state values into separate
    `useState` calls resolves this issue by using primitives that can be
    compared directly, which results in `useState` not triggering re-renders
    when the values are the same.

--- packages/components/src/mobile/link-picker/index.native.js
+++ packages/components/src/mobile/link-picker/index.native.js
@@ -57,10 +57,8 @@ export const LinkPicker = ( {
 	onLinkPicked,
 	onCancel: cancel,
 } ) => {
-	const [ { value, clipboardUrl }, setValue ] = useState( {
-		value: initialValue,
-		clipboardUrl: '',
-	} );
+	const [ value, setValue ] = useState( initialValue );
+	const [ clipboardUrl, setClipboardUrl ] = useState( '' );
 	const directEntry = createDirectEntry( value );
 
 	// The title of a direct entry is displayed as the raw input value, but if we
@@ -74,7 +72,8 @@ export const LinkPicker = ( {
 	};
 
 	const clear = () => {
-		setValue( { value: '', clipboardUrl } );
+		setValue( '' );
+		setClipboardUrl( '' );
 	};
 
 	const omniCellStyle = usePreferredColorSchemeStyle(
@@ -89,11 +88,10 @@ export const LinkPicker = ( {
 
 	useEffect( () => {
 		getURLFromClipboard()
-			.then( ( url ) => setValue( { value, clipboardUrl: url } ) )
-			.catch( () => setValue( { value, clipboardUrl: '' } ) );
-		// Disable reason: deferring this refactor to the native team.
-		// see https://github.com/WordPress/gutenberg/pull/41166
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+			.then( ( url ) => {
+				setClipboardUrl( url );
+			} )
+			.catch( () => setClipboardUrl( '' ) );
 	}, [] );
 
 	// TODO: Localize the accessibility label.
@@ -116,7 +114,7 @@ export const LinkPicker = ( {
 					autoCorrect={ false }
 					keyboardType="url"
 					onChangeValue={ ( newValue ) => {
-						setValue( { value: newValue, clipboardUrl } );
+						setValue( newValue );
 					} }
 					onSubmit={ onSubmit }
 					/* eslint-disable-next-line jsx-a11y/no-autofocus */
