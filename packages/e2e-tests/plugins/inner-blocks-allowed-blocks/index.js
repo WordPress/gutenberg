82ff0e09f3718718e4e6ddad3c430dbe9f8940b2
George Mamadashvili <georgemamadashvili@gmail.com>
2023-06-20 14:05:17 +0400

    Migrate 'Allowed Blocks Setting on InnerBlocks' tests to Playwright (#51677)
    * Migrate 'Allowed Blocks Setting on InnerBlocks' tests to Playwright
    
    * Remove old test file
    
    * Fix inline comment
    
    * Avoid ESLint warning without extensive comment

--- packages/e2e-tests/plugins/inner-blocks-allowed-blocks/index.js
+++ packages/e2e-tests/plugins/inner-blocks-allowed-blocks/index.js
@@ -1,87 +1,46 @@
 ( function () {
-	const { withSelect } = wp.data;
+	const { useSelect } = wp.data;
 	const { registerBlockType } = wp.blocks;
 	const { createElement: el } = wp.element;
 	const { InnerBlocks } = wp.blockEditor;
-	const __ = wp.i18n.__;
 	const divProps = {
 		className: 'product',
 		style: { outline: '1px solid gray', padding: 5 },
 	};
-	const template = [
-		[ 'core/image' ],
-		[ 'core/paragraph', { placeholder: __( 'Add a description' ) } ],
-		[ 'core/quote' ],
-	];
+
 	const allowedBlocksWhenSingleEmptyChild = [ 'core/image', 'core/list' ];
 	const allowedBlocksWhenMultipleChildren = [ 'core/gallery', 'core/video' ];
-
-	const save = function () {
-		return el( 'div', divProps, el( InnerBlocks.Content ) );
-	};
-	registerBlockType( 'test/allowed-blocks-unset', {
-		title: 'Allowed Blocks Unset',
-		icon: 'carrot',
-		category: 'text',
-
-		edit() {
-			return el( 'div', divProps, el( InnerBlocks, { template } ) );
-		},
-
-		save,
-	} );
-
-	registerBlockType( 'test/allowed-blocks-set', {
-		title: 'Allowed Blocks Set',
-		icon: 'carrot',
-		category: 'text',
-
-		edit() {
-			return el(
-				'div',
-				divProps,
-				el( InnerBlocks, {
-					template,
-					allowedBlocks: [
-						'core/button',
-						'core/gallery',
-						'core/list',
-						'core/media-text',
-						'core/quote',
-					],
-				} )
-			);
-		},
-
-		save,
-	} );
 
 	registerBlockType( 'test/allowed-blocks-dynamic', {
 		title: 'Allowed Blocks Dynamic',
 		icon: 'carrot',
 		category: 'text',
 
-		edit: withSelect( function ( select, ownProps ) {
-			const getBlockOrder = select( 'core/block-editor' ).getBlockOrder;
-			return {
-				numberOfChildren: getBlockOrder( ownProps.clientId ).length,
-			};
-		} )( function ( props ) {
+		edit: function Edit( props ) {
+			const numberOfChildren = useSelect(
+				( select ) => {
+					const { getBlockCount } = select( 'core/block-editor' );
+					return getBlockCount( props.clientId );
+				},
+				[ props.clientId ]
+			);
+
 			return el(
 				'div',
 				{
 					...divProps,
-					'data-number-of-children': props.numberOfChildren,
+					'data-number-of-children': numberOfChildren,
 				},
 				el( InnerBlocks, {
 					allowedBlocks:
-						props.numberOfChildren < 2
+						numberOfChildren < 2
 							? allowedBlocksWhenSingleEmptyChild
 							: allowedBlocksWhenMultipleChildren,
 				} )
 			);
-		} ),
-
-		save,
+		},
+		save() {
+			return el( 'div', divProps, el( InnerBlocks.Content ) );
+		}
 	} );
 } )();
