4137c7bef734438601528f8c20a44b9373786b2c
Alex Lende <alex+github.com@lende.xyz>
2023-06-20 15:26:26 -0500

    Add test for setting custom aspect ratio and then resetting

--- packages/block-editor/src/components/dimensions-tool/test/index.js
+++ packages/block-editor/src/components/dimensions-tool/test/index.js
@@ -62,6 +62,45 @@ describe( 'DimensionsTool', () => {
 
 	it( 'when aspect ratio is changed to original and width is set height should be removed', async () => {} );
 
+	it( 'when aspect ratio is change to custom by setting width and height then removing a width value should return the original aspect ratio', async () => {
+		const user = userEvent.setup();
+		const onChange = jest.fn();
+
+		const value = {
+			aspectRatio: '16/9',
+			scale: 'cover',
+		};
+
+		render( <Example initialValue={ value } onChange={ onChange } /> );
+
+		const aspectRatioSelect = screen.getByRole( 'combobox', {
+			name: 'Aspect ratio',
+		} );
+
+		const widthInput = screen.getByRole( 'spinbutton', {
+			name: 'Width',
+		} );
+
+		const heightInput = screen.getByRole( 'spinbutton', {
+			name: 'Height',
+		} );
+
+		await user.type( widthInput, '100' );
+		await user.type( heightInput, '200' );
+		expect( aspectRatioSelect ).toHaveValue( 'custom' );
+
+		await user.clear( widthInput, '' );
+		expect( aspectRatioSelect ).toHaveValue( '16/9' );
+
+		// Use toStrictEqual so undefined properties are treated differently
+		// from missing properties.
+		// expect( onChange.mock.calls ).toStrictEqual( [
+		// 	[ { aspectRatio: '16/9', scale: 'contain' } ],
+		// 	[ {} ],
+		// 	[ { aspectRatio: '16/9', scale: 'contain' } ],
+		// ] );
+	} );
+
 	it( 'when custom scale is set then aspect ratio is set to original and then aspect ratio is changed back', async () => {
 		const user = userEvent.setup();
 		const onChange = jest.fn();
