b3a6122f62e0700c675e2e8a9bcbf689010e5f03
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2023-06-20 18:09:38 +0300

    Lodash: Refactor away from `_.kebabCase()` in block editor (#51687)
    * Block Editor: Introduce kebabCase util
    
    * Block Editor: Introduce unit tests for kebabCase()
    
    * Block Editor: Replace _.kebabCase() with our custom util
    
    * Block Editor: Update kebabCase() to handle other types
    
    * Cover all _wp_to_kebab_case() scenarios
    
    * Minor formatting fixes

--- packages/block-editor/src/utils/object.js
+++ packages/block-editor/src/utils/object.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { paramCase } from 'change-case';
+
+/**
  * Converts a path to an array of its fragments.
  * Supports strings, numbers and arrays:
  *
@@ -17,6 +22,36 @@ function normalizePath( path ) {
 	}
 
 	return [ path ];
+}
+
+/**
+ * Converts any string to kebab case.
+ * Backwards compatible with Lodash's `_.kebabCase()`.
+ * Backwards compatible with `_wp_to_kebab_case()`.
+ *
+ * @see https://lodash.com/docs/4.17.15#kebabCase
+ * @see https://developer.wordpress.org/reference/functions/_wp_to_kebab_case/
+ *
+ * @param {string} str String to convert.
+ * @return {string} Kebab-cased string
+ */
+export function kebabCase( str ) {
+	let input = str;
+	if ( typeof str !== 'string' ) {
+		input = str?.toString?.() ?? '';
+	}
+
+	// See https://github.com/lodash/lodash/blob/b185fcee26b2133bd071f4aaca14b455c2ed1008/lodash.js#L4970
+	input = input.replace( /['\u2019]/, '' );
+
+	return paramCase( input, {
+		splitRegexp: [
+			/(?!(?:1ST|2ND|3RD|[4-9]TH)(?![a-z]))([a-z0-9])([A-Z])/g, // fooBar => foo-bar, 3Bar => 3-bar
+			/(?!(?:1st|2nd|3rd|[4-9]th)(?![a-z]))([0-9])([a-z])/g, // 3bar => 3-bar
+			/([A-Za-z])([0-9])/g, // Foo3 => foo-3, foo3 => foo-3
+			/([A-Z])([A-Z][a-z])/g, // FOOBar => foo-bar
+		],
+	} );
 }
 
 /**
