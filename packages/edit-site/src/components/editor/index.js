929f99db3a08b111a4d7df920df9a22fa66ee718
Marin Atanasov <tyxla@abv.bg>
2023-06-21 14:02:31 +0300

    Move color scheming to canvas spinner

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -15,7 +15,6 @@ import {
 	BlockContextProvider,
 	BlockBreadcrumb,
 	store as blockEditorStore,
-	privateApis as blockEditorPrivateApis,
 } from '@wordpress/block-editor';
 import {
 	InterfaceSkeleton,
@@ -43,8 +42,6 @@ import CanvasSpinner from '../canvas-spinner';
 import { unlock } from '../../lock-unlock';
 import useEditedEntityRecord from '../use-edited-entity-record';
 import { SidebarFixedBottomSlot } from '../sidebar-edit-mode/sidebar-fixed-bottom';
-
-const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 const interfaceLabels = {
 	/* translators: accessibility text for the editor content landmark region. */
@@ -163,13 +160,9 @@ export default function Editor( { isLoading } ) {
 	// action in <URlQueryController> from double-announcing.
 	useTitle( hasLoadedPost && title );
 
-	const [ textColor ] = useGlobalStyle( 'color.text' );
-
 	return (
 		<>
-			{ isLoading ? (
-				<CanvasSpinner spinnerStyle={ { color: textColor } } />
-			) : null }
+			{ isLoading ? <CanvasSpinner /> : null }
 			{ isEditMode && <WelcomeGuide /> }
 			<EntityProvider kind="root" type="site">
 				<EntityProvider
