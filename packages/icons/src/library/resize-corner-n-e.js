33f63d3f51c15c9c75df020ffe1658bd1ab96092
Rich Tabor <hi@richtabor.com>
2023-06-21 16:21:26 -0400

    Update spacer block icon

--- packages/icons/src/library/resize-corner-n-e.js
+++ packages/icons/src/library/resize-corner-n-e.js
@@ -5,7 +5,7 @@ import { Path, SVG } from '@wordpress/primitives';
 
 const resizeCornerNE = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M12.5 4.2v1.6h4.7L5.8 17.2V12H4.2v7.8H12v-1.6H6.8L18.2 6.8v4.7h1.6V4.2z" />
+		<Path d="M7 18h5v1.5H4.5V12H6v5L17 6h-4V4.5h6.5V11H18V7L7 18Z" />
 	</SVG>
 );
 
