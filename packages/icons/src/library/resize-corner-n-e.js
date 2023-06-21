34b182af9fefed46d10fdfb94fe332d30fcac7fd
Rich Tabor <hi@richtabor.com>
2023-06-21 16:29:07 -0400

    Update resize-corner-n-e.js

--- packages/icons/src/library/resize-corner-n-e.js
+++ packages/icons/src/library/resize-corner-n-e.js
@@ -5,7 +5,7 @@ import { Path, SVG } from '@wordpress/primitives';
 
 const resizeCornerNE = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<Path d="M7 18h5v1.5H4.5V12H6v5L17 6h-4V4.5h6.5V11H18V7L7 18Z" />
+		<Path d="M7 18h4.5v1.5h-7v-7H6V17L17 6h-4.5V4.5h7v7H18V7L7 18Z" />
 	</SVG>
 );
 
