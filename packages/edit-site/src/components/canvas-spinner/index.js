e14c749fc3aa3182827ea66e8c98a011773b559e
Marin Atanasov <tyxla@abv.bg>
2023-06-22 14:48:36 +0300

    Edit Site: Improve spinner colors

--- packages/edit-site/src/components/canvas-spinner/index.js
+++ packages/edit-site/src/components/canvas-spinner/index.js
@@ -1,4 +1,12 @@
 /**
+ * External dependencies
+ */
+import { colord, extend } from 'colord';
+import mixPlugin from 'colord/plugins/mix';
+
+extend( [ mixPlugin ] );
+
+/**
  * WordPress dependencies
  */
 import { Spinner } from '@wordpress/components';
@@ -8,15 +16,26 @@ import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import { unlock } from '../../lock-unlock';
+import { useStylesPreviewColors } from '../global-styles/hooks';
 
 const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 export default function CanvasSpinner() {
 	const [ textColor ] = useGlobalStyle( 'color.text' );
+	const { highlightedColors } = useStylesPreviewColors();
+
+	const trackColor = highlightedColors[ 0 ]?.color ?? textColor;
+	const trackColord = colord( trackColor );
+	const indicatorColor = trackColord.isDark()
+		? trackColord.tints( 3 )[ 1 ].toHex()
+		: trackColord.shades( 3 )[ 1 ].toHex();
 
 	return (
 		<div className="edit-site-canvas-spinner">
-			<Spinner style={ { color: textColor } } />
+			<Spinner
+				style={ { color: trackColor } }
+				trackStyles={ { stroke: indicatorColor } }
+			/>
 		</div>
 	);
 }
