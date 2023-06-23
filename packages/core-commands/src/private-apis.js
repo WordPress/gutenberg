e4156b73214f72e623116b3f53c99d0abab20c08
ntsekouras <ntsekouras@outlook.com>
2023-06-23 15:35:59 +0300

    Add manage all custom patterns command

--- packages/core-commands/src/private-apis.js
+++ packages/core-commands/src/private-apis.js
@@ -2,11 +2,13 @@
  * Internal dependencies
  */
 import { useAddPostTypeCommands } from './add-post-type-commands';
+import { useManageReusableBlocksCommand } from './manage-reusable-blocks-command';
 import { useSiteEditorNavigationCommands } from './site-editor-navigation-commands';
 import { lock } from './lock-unlock';
 
 function useCommands() {
 	useAddPostTypeCommands();
+	useManageReusableBlocksCommand();
 	useSiteEditorNavigationCommands();
 }
 
