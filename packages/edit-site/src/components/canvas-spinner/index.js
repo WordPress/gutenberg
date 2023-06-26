ffa5b9605dec61ee8bfce50d626e83cf4afca0b0
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2023-06-26 13:11:58 +0300

    Edit Site: Make loading spinner colors consistent (#51857)
    * Edit Site: Make loading spinner colors consistent
    
    * Reduce opacity of spinner track

--- packages/edit-site/src/components/canvas-spinner/index.js
+++ packages/edit-site/src/components/canvas-spinner/index.js
@@ -2,21 +2,11 @@
  * WordPress dependencies
  */
 import { Spinner } from '@wordpress/components';
-import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
-
-/**
- * Internal dependencies
- */
-import { unlock } from '../../lock-unlock';
-
-const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 export default function CanvasSpinner() {
-	const [ textColor ] = useGlobalStyle( 'color.text' );
-
 	return (
 		<div className="edit-site-canvas-spinner">
-			<Spinner style={ { color: textColor } } />
+			<Spinner />
 		</div>
 	);
 }
