c5266ba69d8d0e75db5fcdf0ccf9012f6a3512f9
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-06-27 15:50:18 +0200

    Revert "Build packages correctly"
    This reverts commit 74ba9864afd9766c14011fea1414dd79fa1a5aa4.

--- bin/plugin/commands/performance.js
+++ bin/plugin/commands/performance.js
@@ -285,7 +285,7 @@ async function runPerformanceTests( branches, options ) {
 
 	log( '    >> Installing dependencies and building packages' );
 	await runShellScript(
-		'npm ci && npm run build:packages',
+		'npm ci && node ./bin/packages/build.js',
 		performanceTestDirectory
 	);
 	log( '    >> Creating the environment folders' );
