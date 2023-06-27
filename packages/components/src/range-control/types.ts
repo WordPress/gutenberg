40f841cb9b2e039df317619ccd1307ddf41b3a83
Rich Tabor <hi@richtabor.com>
2023-06-27 09:31:58 -0400

    RangeControl: Add support for large 40px number input size (#49105)
    Co-authored-by: chad1008 <13856531+chad1008@users.noreply.github.com>

--- packages/components/src/range-control/types.ts
+++ packages/components/src/range-control/types.ts
@@ -204,6 +204,12 @@ export type RangeControlProps = Pick<
 		 */
 		shiftStep?: number;
 		/**
+		 * Start opting into the larger default height that will become the default size in a future version.
+		 *
+		 * @default false
+		 */
+		__next40pxDefaultSize?: boolean;
+		/**
 		 * Forcing the Tooltip UI to show or hide. This is overridden to `false`
 		 * when `step` is set to the special string value `any`.
 		 */
