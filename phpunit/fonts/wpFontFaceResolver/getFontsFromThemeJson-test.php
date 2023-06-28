610f4034b6b8dc49a98e2a3cdb26f67953f59be6
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-06-28 15:31:13 +0200

    Add a draft test for WP_Font_Face_Resolver.

---
+++ phpunit/fonts/wpFontFaceResolver/getFontsFromThemeJson-test.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Test WP_Theme_JSON_Resolver_Gutenberg class.
+ *
+ * @package Gutenberg
+ */
+
+class Tests_Fonts_WpFontFaceResolver_EnqueueUserSelectedFonts extends WP_UnitTestCase {
+
+
+
+}
