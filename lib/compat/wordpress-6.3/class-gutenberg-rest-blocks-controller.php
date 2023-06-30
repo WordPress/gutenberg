41ed77be374fc0cef860e369439ffab529950bd2
Glen Davies <glen.davies@automattic.com>
2023-06-30 17:03:09 +1200

    Align equals

--- lib/compat/wordpress-6.3/class-gutenberg-rest-blocks-controller.php
+++ lib/compat/wordpress-6.3/class-gutenberg-rest-blocks-controller.php
@@ -40,7 +40,7 @@ class Gutenberg_REST_Blocks_Controller extends WP_REST_Blocks_Controller {
 		unset( $data['content']['rendered'] );
 
 		// Add the core sync_status meta as top level property to the response.
-		$wp_sync_status = $data['meta']['wp_sync_status'];
+		$wp_sync_status         = $data['meta']['wp_sync_status'];
 		$data['wp_sync_status'] = $wp_sync_status;
 		unset( $data['meta']['wp_sync_status'] );
 		return $data;
