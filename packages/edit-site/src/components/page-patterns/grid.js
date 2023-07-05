88b88ab40eb9f312c8d9bad0943320e4a5442525
Saxon Fletcher <saxonafletcher@gmail.com>
2023-07-05 15:32:17 +1000

    grid for patterns

--- packages/edit-site/src/components/page-patterns/grid.js
+++ packages/edit-site/src/components/page-patterns/grid.js
@@ -4,10 +4,8 @@
 import {
 	__unstableComposite as Composite,
 	__unstableUseCompositeState as useCompositeState,
-	FlexBlock,
 	Button,
 } from '@wordpress/components';
-import { useViewportMatch } from '@wordpress/compose';
 import { useState, useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
@@ -18,12 +16,8 @@ import GridItem from './grid-item';
 
 const PAGE_SIZE = 100;
 
-const isEven = ( _, index ) => index % 2 === 0;
-const isOdd = ( _, index ) => index % 2 !== 0;
-
 export default function Grid( { categoryId, items } ) {
 	const composite = useCompositeState( { orientation: 'vertical' } );
-	const isSmallerViewport = useViewportMatch( 'large', '<' );
 	const [ page, setPage ] = useState( 1 );
 	const gridRef = useRef();
 
@@ -42,41 +36,14 @@ export default function Grid( { categoryId, items } ) {
 				className="edit-site-patterns__grid"
 				ref={ gridRef }
 			>
-				{ isSmallerViewport ? (
-					<FlexBlock>
-						{ list.map( ( item ) => (
-							<GridItem
-								key={ item.name }
-								item={ item }
-								categoryId={ categoryId }
-								composite={ composite }
-							/>
-						) ) }
-					</FlexBlock>
-				) : (
-					<>
-						<FlexBlock>
-							{ list.filter( isEven ).map( ( item ) => (
-								<GridItem
-									key={ item.name }
-									item={ item }
-									categoryId={ categoryId }
-									composite={ composite }
-								/>
-							) ) }
-						</FlexBlock>
-						<FlexBlock>
-							{ list.filter( isOdd ).map( ( item ) => (
-								<GridItem
-									key={ item.name }
-									item={ item }
-									categoryId={ categoryId }
-									composite={ composite }
-								/>
-							) ) }
-						</FlexBlock>
-					</>
-				) }
+				{ list.map( ( item ) => (
+					<GridItem
+						key={ item.name }
+						item={ item }
+						categoryId={ categoryId }
+						composite={ composite }
+					/>
+				) ) }
 			</Composite>
 			{ items.length >= maxCount && (
 				<Button
