706a763f526cb447f12812596eb505fe45fb685c
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-07-05 17:41:49 +1000

    Directly create new parts or patterns when duplicating

--- packages/edit-site/src/components/page-patterns/duplicate-menu-item.js
+++ packages/edit-site/src/components/page-patterns/duplicate-menu-item.js
@@ -2,50 +2,124 @@
  * WordPress dependencies
  */
 import { MenuItem } from '@wordpress/components';
-import { useState } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
+import { store as coreStore } from '@wordpress/core-data';
+import { useDispatch } from '@wordpress/data';
+import { __, sprintf } from '@wordpress/i18n';
+import { store as noticesStore } from '@wordpress/notices';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
 
 /**
  * Internal dependencies
  */
-import CreatePatternModal from '../create-pattern-modal';
-import CreateTemplatePartModal from '../create-template-part-modal';
-import { TEMPLATE_PARTS } from './utils';
+import {
+	TEMPLATE_PARTS,
+	PATTERNS,
+	SYNC_TYPES,
+	USER_PATTERNS,
+	USER_PATTERN_CATEGORY,
+} from './utils';
+import {
+	useExistingTemplateParts,
+	getUniqueTemplatePartTitle,
+	getCleanTemplatePartSlug,
+} from '../../utils/template-part-create';
+import { unlock } from '../../lock-unlock';
+
+const { useHistory } = unlock( routerPrivateApis );
 
 export default function DuplicateMenuItem( {
 	item,
 	label = __( 'Duplicate' ),
 	onClose,
 } ) {
-	const [ isModalOpen, setIsModalOpen ] = useState( false );
+	const { createErrorNotice } = useDispatch( noticesStore );
+	const { saveEntityRecord } = useDispatch( coreStore );
 
-	function handleClose() {
-		setIsModalOpen( false );
-		onClose();
+	const history = useHistory();
+	const existingTemplateParts = useExistingTemplateParts();
+
+	async function createTemplatePart() {
+		try {
+			const copiedTitle = sprintf(
+				/* translators: %s: Existing template part title */
+				__( '%s (Copy)' ),
+				item.title
+			);
+			const title = getUniqueTemplatePartTitle(
+				copiedTitle,
+				existingTemplateParts
+			);
+			const slug = getCleanTemplatePartSlug( title );
+			const { area, content } = item.templatePart;
+
+			await saveEntityRecord(
+				'postType',
+				'wp_template_part',
+				{ slug, title, content, area },
+				{ throwOnError: true }
+			);
+
+			onClose();
+		} catch ( error ) {
+			const errorMessage =
+				error.message && error.code !== 'unknown_error'
+					? error.message
+					: __(
+							'An error occurred while creating the template part.'
+					  );
+
+			createErrorNotice( errorMessage, { type: 'snackbar' } );
+			onClose();
+		}
 	}
 
-	const CreateModal =
-		item.type === TEMPLATE_PARTS
-			? CreateTemplatePartModal
-			: CreatePatternModal;
+	async function createPattern() {
+		try {
+			const isThemePattern = item.type === PATTERNS;
+			const title = sprintf(
+				/* translators: %s: Existing pattern title */
+				__( '%s (Copy)' ),
+				item.title
+			);
 
-	const modalProps = {
-		blocks: item.blocks || [],
-		closeModal: handleClose,
-		onCreate: handleClose,
-		onError: handleClose,
-		title:
-			item.type !== TEMPLATE_PARTS
-				? __( 'Duplicate pattern' )
-				: undefined,
-	};
+			await saveEntityRecord(
+				'postType',
+				'wp_block',
+				{
+					content: isThemePattern
+						? item.content
+						: item.reusableBlock.content,
+					meta: isThemePattern
+						? { sync_status: SYNC_TYPES.unsynced }
+						: item.reusableBlock.meta,
+					status: 'publish',
+					title,
+				},
+				{ throwOnError: true }
+			);
 
-	return (
-		<>
-			<MenuItem onClick={ () => setIsModalOpen( true ) }>
-				{ label }
-			</MenuItem>
-			{ isModalOpen && <CreateModal { ...modalProps } /> }
-		</>
-	);
+			onClose();
+
+			// If this was a theme pattern, we're "copying to my patterns", so
+			// we need to navigate to that category to display the new pattern.
+			history.push( {
+				categoryType: USER_PATTERNS,
+				categoryId: USER_PATTERN_CATEGORY,
+				path: '/patterns',
+			} );
+		} catch ( error ) {
+			const errorMessage =
+				error.message && error.code !== 'unknown_error'
+					? error.message
+					: __( 'An error occurred while creating the pattern.' );
+
+			createErrorNotice( errorMessage, { type: 'snackbar' } );
+			onClose();
+		}
+	}
+
+	const createItem =
+		item.type === TEMPLATE_PARTS ? createTemplatePart : createPattern;
+
+	return <MenuItem onClick={ createItem }>{ label }</MenuItem>;
 }
