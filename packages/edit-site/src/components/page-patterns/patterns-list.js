c522b128dbac1c3b043f9eeb7a7ef37e0a650aa5
James Koster <james@jameskoster.co.uk>
2023-07-06 01:53:31 +0100

    Patterns: Update pattern copy (#52340)

--- packages/edit-site/src/components/page-patterns/patterns-list.js
+++ packages/edit-site/src/components/page-patterns/patterns-list.js
@@ -79,7 +79,7 @@ export default function PatternsList( { categoryId, type } ) {
 						<Heading level={ 4 }>{ __( 'Synced' ) }</Heading>
 						<Text variant="muted" as="p">
 							{ __(
-								'Patterns that are kept in sync across your site'
+								'Patterns that are kept in sync across the site'
 							) }
 						</Text>
 					</VStack>
@@ -97,7 +97,7 @@ export default function PatternsList( { categoryId, type } ) {
 						<Heading level={ 4 }>{ __( 'Standard' ) }</Heading>
 						<Text variant="muted" as="p">
 							{ __(
-								'Patterns that can be changed freely without affecting your site'
+								'Patterns that can be changed freely without affecting the site'
 							) }
 						</Text>
 					</VStack>
