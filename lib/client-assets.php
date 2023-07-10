9bb939febf6b5b479a87fb17f88660626e2d3b7f
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-07-10 10:41:07 +1000

    Remove unused wp-nux CSS dependency.
    See
    * https://github.com/WordPress/wordpress-develop/commit/8a6afdd52e572ded7aa350f0d24f40da799596e9
    * https://core.trac.wordpress.org/ticket/57827

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -269,7 +269,7 @@ function gutenberg_register_packages_styles( $styles ) {
 		$styles,
 		'wp-editor',
 		gutenberg_url( 'build/editor/style.css' ),
-		array( 'wp-components', 'wp-block-editor', 'wp-nux', 'wp-reusable-blocks' ),
+		array( 'wp-components', 'wp-block-editor', 'wp-reusable-blocks' ),
 		$version
 	);
 	$styles->add_data( 'wp-editor', 'rtl', 'replace' );
@@ -278,7 +278,7 @@ function gutenberg_register_packages_styles( $styles ) {
 		$styles,
 		'wp-edit-post',
 		gutenberg_url( 'build/edit-post/style.css' ),
-		array( 'wp-components', 'wp-block-editor', 'wp-editor', 'wp-edit-blocks', 'wp-block-library', 'wp-commands', 'wp-nux' ),
+		array( 'wp-components', 'wp-block-editor', 'wp-editor', 'wp-edit-blocks', 'wp-block-library', 'wp-commands' ),
 		$version
 	);
 	$styles->add_data( 'wp-edit-post', 'rtl', 'replace' );
