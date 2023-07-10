06535a79c7b5eef637555747944ca115eeeea36d
Glen Davies <glen.davies@automattic.com>
2023-07-10 16:17:54 +1200

    Allow for title being undefined while block is still saving

--- packages/block-editor/src/components/use-block-display-information/index.js
+++ packages/block-editor/src/components/use-block-display-information/index.js
@@ -77,16 +77,18 @@ export default function useBlockDisplayInformation( clientId ) {
 			const blockName = getBlockName( clientId );
 			const blockType = getBlockType( blockName );
 			if ( ! blockType ) return null;
+			let title = blockType.title;
 			const attributes = getBlockAttributes( clientId );
 			const match = getActiveBlockVariation( blockName, attributes );
 			const isReusable = isReusableBlock( blockType );
+			if ( isReusable ) {
+				title = __experimentalGetReusableBlockTitle( attributes.ref );
+			}
 			const isSynced = isReusable || isTemplatePart( blockType );
 			const positionLabel = getPositionTypeLabel( attributes );
 			const blockTypeInfo = {
 				isSynced,
-				title: isReusable
-					? __experimentalGetReusableBlockTitle( attributes.ref )
-					: blockType.title,
+				title: title ? title : blockType.title,
 				icon: blockType.icon,
 				description: blockType.description,
 				anchor: attributes?.anchor,
