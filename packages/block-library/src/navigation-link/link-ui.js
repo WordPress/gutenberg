d46234dc1e2221b9d4367dfb45a175b7796289d0
Vicente Canales <vicente.canales@automattic.com>
2023-07-11 15:14:44 -0400

    Add empty URL check

--- packages/block-library/src/navigation-link/link-ui.js
+++ packages/block-library/src/navigation-link/link-ui.js
@@ -193,7 +193,7 @@ export function LinkUI( props ) {
 				showInitialSuggestions={ true }
 				withCreateSuggestion={ userCanCreate }
 				createSuggestion={ handleCreate }
-				forceIsEditingLink={ ! props.isDraft }
+				forceIsEditingLink={ ! props.isDraft && ! url }
 				createSuggestionButtonText={ ( searchTerm ) => {
 					let format;
 
