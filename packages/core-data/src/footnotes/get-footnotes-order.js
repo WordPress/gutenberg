badef7cc1bd1c6ef6a0589c77093ef7db5896e79
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2023-07-12 16:59:31 +0100

    Move getFootnotesOrder computation to the block level

--- packages/core-data/src/footnotes/get-footnotes-order.js
+++ packages/core-data/src/footnotes/get-footnotes-order.js
@@ -3,13 +3,10 @@
  */
 import getRichTextValuesCached from './get-rich-text-values-cached';
 
-export default function getFootnotesOrder( blocks ) {
-	const values = blocks.map( getRichTextValuesCached );
-	const content = values.join( '' );
-
+function getBlockFootnotesOrder( block ) {
+	const content = getRichTextValuesCached( block ).join( '' );
 	const newOrder = [];
 
-	// This can be avoided when
 	// https://github.com/WordPress/gutenberg/pull/43204 lands. We can then
 	// get the order directly from the rich text values.
 	if ( content.indexOf( 'data-fn' ) !== -1 ) {
@@ -21,4 +18,8 @@ export default function getFootnotesOrder( blocks ) {
 	}
 
 	return newOrder;
+}
+
+export default function getFootnotesOrder( blocks ) {
+	return blocks.flatMap( getBlockFootnotesOrder );
 }
