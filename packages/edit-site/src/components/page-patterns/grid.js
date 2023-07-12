b841a5e621d3b8bcc71319b4b1ed4c11e0d5d335
Glen Davies <glen.davies@automattic.com>
2023-07-12 13:18:44 +1200

    Try adding client side pagination to patterns list

--- packages/edit-site/src/components/page-patterns/grid.js
+++ packages/edit-site/src/components/page-patterns/grid.js
@@ -1,8 +1,8 @@
 /**
  * WordPress dependencies
  */
-import { __experimentalText as Text } from '@wordpress/components';
-import { useRef } from '@wordpress/element';
+import { __experimentalHStack as HStack } from '@wordpress/components';
+import { useRef, useState } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 
 /**
@@ -10,20 +10,87 @@ import { __, sprintf } from '@wordpress/i18n';
  */
 import GridItem from './grid-item';
 
-const PAGE_SIZE = 100;
+const PAGE_SIZE = 20;
+
+function Pagination( { currentPage, numPages, changePage, totalItems } ) {
+	return (
+		<HStack className="edit-site-patterns__grid-pagination">
+			<div className="edit-site-patterns__grid-pagination-label">
+				{
+					// translators: %s: Total number of patterns.
+					sprintf( __( '%s items' ), totalItems )
+				}
+			</div>
+
+			<button
+				onClick={ () => changePage( 1 ) }
+				disabled={ currentPage === 1 }
+			>
+				«
+			</button>
+			<button
+				onClick={ () => changePage( currentPage - 1 ) }
+				disabled={ currentPage === 1 }
+			>
+				‹
+			</button>
+			<div className="edit-site-patterns__grid-pagination-label">
+				{
+					// translators: %1$s: Current page number, %2$s: Total number of pages.
+					sprintf( __( '%1$s of %2$s' ), currentPage, numPages )
+				}
+			</div>
+			<button
+				onClick={ () => changePage( currentPage + 1 ) }
+				disabled={ currentPage === numPages }
+			>
+				›
+			</button>
+			<button
+				onClick={ () => changePage( numPages ) }
+				disabled={ currentPage === numPages }
+			>
+				»
+			</button>
+		</HStack>
+	);
+}
 
 export default function Grid( { categoryId, items, ...props } ) {
+	const [ currentPage, setCurrentPage ] = useState( 1 );
 	const gridRef = useRef();
 
 	if ( ! items?.length ) {
 		return null;
 	}
+	items.sort( ( a, b ) =>
+		a.title.localeCompare( b.title, undefined, {
+			numeric: true,
+			sensitivity: 'base',
+		} )
+	);
+	const numPages = Math.ceil( items.length / PAGE_SIZE );
+	const totalItems = items.length;
+	const pageIndex = currentPage - 1;
+	const list = items.slice(
+		pageIndex * PAGE_SIZE,
+		pageIndex * PAGE_SIZE + PAGE_SIZE
+	);
 
-	const list = items.slice( 0, PAGE_SIZE );
-	const restLength = items.length - PAGE_SIZE;
+	const changePage = ( page ) => {
+		const scrollContainer =
+			document.getElementsByClassName( 'edit-site-patterns' );
+
+		scrollContainer[ 0 ].scrollTo( 0, 0 );
+
+		setCurrentPage( page );
+	};
 
 	return (
 		<>
+			<Pagination
+				{ ...{ currentPage, numPages, changePage, totalItems } }
+			/>
 			<ul
 				role="listbox"
 				className="edit-site-patterns__grid"
@@ -38,15 +105,9 @@ export default function Grid( { categoryId, items, ...props } ) {
 					/>
 				) ) }
 			</ul>
-			{ restLength > 0 && (
-				<Text variant="muted" as="p" align="center">
-					{ sprintf(
-						/* translators: %d: number of patterns */
-						__( '+ %d more patterns discoverable by searching' ),
-						restLength
-					) }
-				</Text>
-			) }
+			<Pagination
+				{ ...{ currentPage, numPages, changePage, totalItems } }
+			/>
 		</>
 	);
 }
--- packages/edit-site/src/components/page-patterns/patterns-list.js
+++ packages/edit-site/src/components/page-patterns/patterns-list.js
@@ -15,7 +15,7 @@ import {
 import { __, isRTL } from '@wordpress/i18n';
 import { chevronLeft, chevronRight } from '@wordpress/icons';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
-import { useViewportMatch, useAsyncList } from '@wordpress/compose';
+import { useViewportMatch } from '@wordpress/compose';
 
 /**
  * Internal dependencies
@@ -70,7 +70,6 @@ export default function PatternsList( { categoryId, type } ) {
 	const hasPatterns = patterns.length;
 	const title = SYNC_FILTERS[ syncFilter ];
 	const description = SYNC_DESCRIPTIONS[ syncFilter ];
-	const shownPatterns = useAsyncList( patterns );
 
 	return (
 		<VStack spacing={ 6 }>
@@ -145,7 +144,7 @@ export default function PatternsList( { categoryId, type } ) {
 			{ hasPatterns && (
 				<Grid
 					categoryId={ categoryId }
-					items={ shownPatterns }
+					items={ patterns }
 					aria-labelledby={ titleId }
 					aria-describedby={ descriptionId }
 				/>
