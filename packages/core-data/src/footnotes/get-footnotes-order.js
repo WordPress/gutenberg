62f34aef3b4d28e8c555cba60ef703ce4bb8854d
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2023-07-12 17:00:09 +0100

    Add per-block caching to getFootnotesOrder

--- packages/core-data/src/footnotes/get-footnotes-order.js
+++ packages/core-data/src/footnotes/get-footnotes-order.js
@@ -3,21 +3,26 @@
  */
 import getRichTextValuesCached from './get-rich-text-values-cached';
 
-function getBlockFootnotesOrder( block ) {
-	const content = getRichTextValuesCached( block ).join( '' );
-	const newOrder = [];
+const cache = new WeakMap();
 
-	// https://github.com/WordPress/gutenberg/pull/43204 lands. We can then
-	// get the order directly from the rich text values.
-	if ( content.indexOf( 'data-fn' ) !== -1 ) {
-		const regex = /data-fn="([^"]+)"/g;
-		let match;
-		while ( ( match = regex.exec( content ) ) !== null ) {
-			newOrder.push( match[ 1 ] );
+function getBlockFootnotesOrder( block ) {
+	if ( ! cache.has( block ) ) {
+		const content = getRichTextValuesCached( block ).join( '' );
+		const newOrder = [];
+
+		// https://github.com/WordPress/gutenberg/pull/43204 lands. We can then
+		// get the order directly from the rich text values.
+		if ( content.indexOf( 'data-fn' ) !== -1 ) {
+			const regex = /data-fn="([^"]+)"/g;
+			let match;
+			while ( ( match = regex.exec( content ) ) !== null ) {
+				newOrder.push( match[ 1 ] );
+			}
 		}
+		cache.set( block, newOrder );
 	}
 
-	return newOrder;
+	return cache.get( block );
 }
 
 export default function getFootnotesOrder( blocks ) {
