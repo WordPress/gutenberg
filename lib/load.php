0c1a9c6d45674847915e534162f20b197d90fbba
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2023-07-12 14:28:25 +0100

    Trim footnote anchors from excerpts (#52518)
    * Trim footnote anchors from excerpts
    
    * Add comments, fix spacing, appease linter

--- lib/load.php
+++ lib/load.php
@@ -57,6 +57,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require_once __DIR__ . '/compat/wordpress-6.3/link-template.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/block-patterns.php';
 	require_once __DIR__ . '/compat/wordpress-6.3/class-gutenberg-rest-blocks-controller.php';
+	require_once __DIR__ . '/compat/wordpress-6.3/footnotes.php';
 
 	// Experimental.
 	if ( ! class_exists( 'WP_Rest_Customizer_Nonces' ) ) {
