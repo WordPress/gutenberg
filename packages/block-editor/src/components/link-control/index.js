2d7dae7bb7606c0c5edfb8b2a4dfe81eef7dbba4
Rich Tabor <hi@richtabor.com>
2023-07-13 18:12:59 -0400

    Add keyboardReturn submit button

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -14,6 +14,7 @@ import { ENTER } from '@wordpress/keycodes';
 import { isShallowEqualObjects } from '@wordpress/is-shallow-equal';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as preferencesStore } from '@wordpress/preferences';
+import { keyboardReturn } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -388,6 +389,15 @@ function LinkControl( {
 							}
 							hideLabelFromVision={ ! showTextControl }
 						/>
+						<div className="block-editor-link-control__search-enter">
+							<Button
+								onClick={ isDisabled ? noop : handleSubmit }
+								label={ __( 'Submit' ) }
+								icon={ keyboardReturn }
+								className="block-editor-link-control__search-submit"
+								aria-disabled={ isDisabled }
+							/>
+						</div>
 					</div>
 					{ errorMessage && (
 						<Notice
