716d9926c92928506efe68756896b1ee4d4d0d94
Ben Dwyer <ben@scruffian.com>
2023-07-13 14:40:13 +0100

    Update test/e2e/specs/editor/blocks/navigation-colors.spec.js

--- test/e2e/specs/editor/blocks/navigation-colors.spec.js
+++ test/e2e/specs/editor/blocks/navigation-colors.spec.js
@@ -123,7 +123,7 @@ test.describe( 'Navigation colors', () => {
 
 		await editor.canvas.click( 'body' );
 
-		// Focus the navigation block inside the header template part
+		// Focus the navigation block inside the header template part.
 		await editor.canvas
 			.getByRole( 'document', { name: 'Block: header' } )
 			.focus();
