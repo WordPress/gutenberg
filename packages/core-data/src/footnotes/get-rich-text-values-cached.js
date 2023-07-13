919d6c5c57414e9ef660c287af4f7b58726bb168
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2023-07-13 08:49:51 +0100

    Don't call `unlock` at module level
    Fixes unit tests failing at
    packages/editor/src/components/document-outline

--- packages/core-data/src/footnotes/get-rich-text-values-cached.js
+++ packages/core-data/src/footnotes/get-rich-text-values-cached.js
@@ -8,13 +8,19 @@ import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
  */
 import { unlock } from '../private-apis';
 
-const { getRichTextValues } = unlock( blockEditorPrivateApis );
+// Avoid calling `unlock` at the module level, deferring the call until needed
+// in `getRichTextValuesCached`.
+let unlockedApis;
 
 const cache = new WeakMap();
 
 export default function getRichTextValuesCached( block ) {
+	if ( ! unlockedApis ) {
+		unlockedApis = unlock( blockEditorPrivateApis );
+	}
+
 	if ( ! cache.has( block ) ) {
-		const values = getRichTextValues( [ block ] );
+		const values = unlockedApis.getRichTextValues( [ block ] );
 		cache.set( block, values );
 	}
 	return cache.get( block );
