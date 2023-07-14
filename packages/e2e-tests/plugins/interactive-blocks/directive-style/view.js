6be06ecf9ee8b5b2d707989eb70e3536579c93e4
Luis Herranz <luisherranz@gmail.com>
2023-07-14 15:09:47 +0200

    Add e2e plugin to test the style directive
    Co-authored-by: Carlos Bravo <c4rl0sbr4v0@users.noreply.github.com>

---
+++ packages/e2e-tests/plugins/interactive-blocks/directive-style/view.js
@@ -0,0 +1,25 @@
+( ( { wp } ) => {
+	const { store } = wp.interactivity;
+
+	store( {
+		state: {
+			falseValue: false,
+			color: "red",
+			border: "2px solid yellow"
+		},
+		actions: {
+			toggleColor: ( { state } ) => {
+				state.color = state.color === "red" ? "blue" : "red";
+			},
+			switchColorToFalse: ({ state }) => {
+				state.color = false;
+			},
+			toggleBorder: ( { state } ) => {
+				state.color = state.color === "2px solid yellow" ? "1px solid black" : "2px solid yellow";
+			},
+			toggleContextValue: ( { context } ) => {
+				context.color = context.color === "red" ? "blue" : "red";
+			},
+		},
+	} );
+} )( window );
