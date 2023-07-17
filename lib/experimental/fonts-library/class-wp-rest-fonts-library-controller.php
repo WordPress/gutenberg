5ef49fdc71d34d2059d618b31544e4a7a43a4712
Vicente Canales <vicente.canales@automattic.com>
2023-07-17 17:27:27 -0400

    linting: add comments on rest fonts library file

--- lib/experimental/fonts-library/class-wp-rest-fonts-library-controller.php
+++ lib/experimental/fonts-library/class-wp-rest-fonts-library-controller.php
@@ -1,19 +1,32 @@
 <?php
+/**
+ * Fonts Library Backend Controller
+ *
+ * @package    Gutenberg
+ * @subpackage Fonts API's BC Layer
+ * @since      X.X.X
+ */
+
 include( ABSPATH . 'wp-admin/includes/admin.php' );
 
 /**
  * Fonts Library Controller class
  *
  * @package gutenberg
- * @since x.x.x
  */
-class WP_Fonts_Library_Controller extends WP_REST_Controller {
+class WP_REST_Fonts_Library_Controller extends WP_REST_Controller {
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		$this->rest_base = 'fonts_library';
 		$this->namespace = 'wp/v2';
 	}
 
+	/**
+	 * Registers the routes for the objects of the controller.
+	 */
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
@@ -83,10 +96,10 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 			);
 		}
 
-		// Create fonts directory if it doesn't exist
+		// Create fonts directory if it doesn't exist.
 		wp_mkdir_p( WP_FONTS_DIR );
 
-		// The update endpoints requires write access to the temp and the fonts directories
+		// The update endpoints requires write access to the temp and the fonts directories.
 		$temp_dir = get_temp_dir();
 		if ( ! is_writable( $temp_dir ) || ! wp_is_writable( WP_FONTS_DIR ) ) {
 			return new WP_Error(
@@ -107,11 +120,11 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access for the item, WP_Error object otherwise.
 	 */
-	function read_fonts_library_permissions_check( $request ) {
+	function read_fonts_library_permissions_check() {
 		if ( ! current_user_can( 'edit_posts' ) ) {
 			return new WP_Error(
 				'rest_cannot_read_fonts_library',
-				__( 'Sorry, you are not allowed to read the fonts library on this site.' ),
+				__( 'Sorry, you are not allowed to read the fonts library on this site.', 'gutenberg' ),
 				array(
 					'status' => rest_authorization_required_code(),
 				)
@@ -137,7 +150,7 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 		}
 		return new WP_Error(
 			'rest_cant_read_google_fonts',
-			__( 'Error reading Google Fonts JSON file.' ),
+			__( 'Error reading Google Fonts JSON file.', 'gutenberg' ),
 			array( 'status' => 500 )
 		);
 	}
@@ -151,21 +164,21 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 	 * @return WP_REST_Response|WP_Error The updated fonts library post content.
 	 */
 	function install_fonts( $request ) {
-		// Get new fonts to install
+		// Get new fonts to install.
 		$fonts_to_install = $request->get_param( 'fontFamilies' );
 
 		// As we are receiving form data, the font families are encoded as a string.
-		// We are using form data because local fonts need to use that format to attach the files in the request
+		// We are using form data because local fonts need to use that format to attach the files in the request.
 		$fonts_to_install = json_decode( $fonts_to_install, true );
 
 		if ( empty( $fonts_to_install ) ) {
 			return new WP_Error( 'no_fonts_to_install', __( 'No fonts to install' ), array( 'status' => 400 ) );
 		}
 
-		// Get uploaded files (used when installing local fonts)
+		// Get uploaded files (used when installing local fonts).
 		$files = $request->get_file_params();
 
-		// Download or move the new fonts assets to the fonts folder
+		// Download or move the new fonts assets to the fonts folder.
 		$fonts = $this->download_or_move_fonts( $fonts_to_install, $files );
 
 		$response = array();
@@ -184,7 +197,7 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 	/**
 	 * Download or move new font assets to the fonts folder
 	 *
-	 * @param array $fonts Fonts to install.
+	 * @param array $font_families Font families to install.
 	 * @param array $files Uploaded files (used when installing local fonts).
 	 *
 	 * @return array New fonts with all assets downloaded referenced in the font families definition.
@@ -203,6 +216,9 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 		return $new_fonts;
 	}
 
+	/**
+	 * Registers the fonts library post type.
+	 */
 	function register_post_type() {
 		$args = array(
 			'public'       => true,
@@ -214,8 +230,11 @@ class WP_Fonts_Library_Controller extends WP_REST_Controller {
 
 }
 
+/**
+ * Registers the routes for the objects of the controller.
+ */
 function fonts_library_register_routes() {
-	$fonts_library = new WP_Fonts_Library_Controller();
+	$fonts_library = new WP_REST_Fonts_Library_Controller();
 	$fonts_library->register_routes();
 	$fonts_library->register_post_type();
 }
