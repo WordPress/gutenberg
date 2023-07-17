0020e54b7793f1b8feb51e2dd5253c9410c6199d
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2023-07-17 16:14:40 +0100

    Add TODO note about RNMobile's circular dependencies

--- packages/core-data/src/footnotes/get-rich-text-values-cached.js
+++ packages/core-data/src/footnotes/get-rich-text-values-cached.js
@@ -8,8 +8,16 @@ import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
  */
 import { unlock } from '../private-apis';
 
-// Avoid calling `unlock` at the module level, deferring the call until needed
-// in `getRichTextValuesCached`.
+// TODO: The following line should have been:
+//
+//   const unlockedApis = unlock( blockEditorPrivateApis );
+//
+// But there are hidden circular dependencies in RNMobile code, specifically in
+// certain native components in the `components` package that depend on
+// `block-editor`. What follows is a workaround that defers the `unlock` call
+// to prevent native code from failing.
+//
+// Fix once https://github.com/WordPress/gutenberg/issues/52692 is closed.
 let unlockedApis;
 
 const cache = new WeakMap();
