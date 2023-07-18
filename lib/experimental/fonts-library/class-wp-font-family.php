b1be3fe8e39e999990cadbe0171b890fd9037934
Sarah Norris <sarah@sekai.co.uk>
2023-07-18 17:39:48 +0100

    Replace include for admin.php with file.php

--- lib/experimental/fonts-library/class-wp-font-family.php
+++ lib/experimental/fonts-library/class-wp-font-family.php
@@ -199,6 +199,11 @@ class WP_Font_Family {
 			return false;
 		}
 
+		// Include file with download_url() if function doesn't exist.
+		if ( ! function_exists( 'download_url' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+		}
+
 		// Downloads the font asset or returns false.
 		$temp_file = download_url( $src );
 		if ( is_wp_error( $temp_file ) ) {
