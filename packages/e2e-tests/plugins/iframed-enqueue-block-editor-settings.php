53f44224d404c48be29cdcefdcbedbbedfa3d9cd
Ella <ella@vandurpe.com>
2023-07-19 18:46:45 +0200

    Add e2e test for theme styles

--- packages/e2e-tests/plugins/iframed-enqueue-block-editor-settings.php
+++ packages/e2e-tests/plugins/iframed-enqueue-block-editor-settings.php
@@ -11,7 +11,7 @@ add_action(
 	'block_editor_settings_all',
 	function( $settings ) {
 		$settings['styles'][] = array(
-			'css' => 'p { border: 1px solid red }',
+			'css'            => 'p { border: 1px solid red }',
 			'__unstableType' => 'plugin',
 		);
 		return $settings;
--- packages/e2e-tests/specs/editor/plugins/iframed-enqueue-block-editor-settings.test.js
+++ packages/e2e-tests/specs/editor/plugins/iframed-enqueue-block-editor-settings.test.js
@@ -52,12 +52,55 @@ describe( 'iframed block editor settings styles', () => {
 					...settings.styles,
 					{
 						css: 'p { border-width: 2px; }',
+						__unstableType: 'plugin',
 					},
 				],
 			} );
 		} );
 
 		// Expect a 2px border (added in JS).
+		expect( await getComputedStyle( canvas(), 'p', 'border-width' ) ).toBe(
+			'2px'
+		);
+	} );
+
+	it( 'should load theme styles added through block editor settings', async () => {
+		await page.waitForSelector( 'iframe[name="editor-canvas"]' );
+
+		await page.evaluate( () => {
+			// Make sure that theme styles are added even if the theme styles
+			// preference is off.
+			window.wp.data
+				.dispatch( 'core/edit-post' )
+				.toggleFeature( 'themeStyles' );
+			const settings = window.wp.data
+				.select( 'core/editor' )
+				.getEditorSettings();
+			wp.data.dispatch( 'core/editor' ).updateEditorSettings( {
+				...settings,
+				styles: [
+					...settings.styles,
+					{
+						css: 'p { border-width: 2px; }',
+						__unstableType: 'theme',
+					},
+				],
+			} );
+		} );
+
+		// Expect a 1px border because theme styles are disabled.
+		expect( await getComputedStyle( canvas(), 'p', 'border-width' ) ).toBe(
+			'1px'
+		);
+
+		await page.evaluate( () => {
+			// Now enable theme styles.
+			window.wp.data
+				.dispatch( 'core/edit-post' )
+				.toggleFeature( 'themeStyles' );
+		} );
+
+		// Expect a 2px border because theme styles are enabled.
 		expect( await getComputedStyle( canvas(), 'p', 'border-width' ) ).toBe(
 			'2px'
 		);
