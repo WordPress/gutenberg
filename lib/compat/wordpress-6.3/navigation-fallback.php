2e7b14b1fe9b44c7e84a51b24b723db1bc51146a
Ben Dwyer <ben@scruffian.com>
2023-07-19 13:02:09 +0100

    Update lib/compat/wordpress-6.3/navigation-fallback.php
    Co-authored-by: Dave Smith <getdavemail@gmail.com>

--- lib/compat/wordpress-6.3/navigation-fallback.php
+++ lib/compat/wordpress-6.3/navigation-fallback.php
@@ -27,7 +27,7 @@ function gutenberg_add_fields_to_navigation_fallback_embeded_links( $schema ) {
 
 	// Expose sub properties of content field.
 	// These aren't exposed by the posts controller by default, see:
-	// https://github.com/WordPress/wordpress-develop/blob/5c3c6258e468c67ba00bbd13db29994f1a57a52a/src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php#L2425
+	// https://github.com/WordPress/wordpress-develop/blob/5c3c6258e468c67ba00bbd13db29994f1a57a52a/src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php#L2425.
 	$schema['properties']['content']['properties']['raw']['context']           = array_merge( $schema['properties']['content']['properties']['raw']['context'], array( 'embed' ) );
 	$schema['properties']['content']['properties']['rendered']['context']      = array_merge( $schema['properties']['content']['properties']['rendered']['context'], array( 'embed' ) );
 	$schema['properties']['content']['properties']['block_version']['context'] = array_merge( $schema['properties']['content']['properties']['block_version']['context'], array( 'embed' ) );
