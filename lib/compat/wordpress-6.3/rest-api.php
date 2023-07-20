d5ab7238e53d0947d4bb0853464b1c58325b6130
Carlos Bravo <37012961+c4rl0sbr4v0@users.noreply.github.com>
2023-07-20 15:04:47 +0200

    Behaviors: Extend Global Styles API to read/write behaviors config. (#52370)
    * Add behaviors to global styles API
    
    * Add behaviors to global styles API endpoint
    
    * Fix coding standards warnings
    
    * Add behavior selector to the site editor, should read for theme.json
    
    * Now the database saves global behaviors
    
    * Fix selector value according to behavior selected
    
    * Add animation to the selector
    
    * Remove not needed themebehaviors
    
    * Use settings for the display
    
    * Show theme json default animation in selector
    
    * If all behaviors are disabled, prevent selector showing on site editor
    
    * Define set function
    
    * Fix if settings behaviors are undefined
    
    * Add apply globally, not yet autosaving
    
    * Add apply globally, still need to work with styles too
    
    * Update to work with new setImmutably function
    
    * Make it work with global styles too
    
    * Make default option work
    
    * Prevent behaviors ui showing if experimental setting is disabled
    
    * Make compatible with supports
    
    * Move `__experimentalUseHasBehaviorsPanel` to hooks.js
    
    * Rename `__experimentalUseGlobalBehaviors` to `useGlobalBehaviors` when destructuring in screen-block.js
    
    * Remove `shouldDecodeEncode` because it's  always true.
    
    * Add global revisions by extending 6.3 controller
    
    * Update php unit test
    
    * Add a default behaviors object to StyleVariationsContainer
    
    * Move all endpoints to 6.4 folder
    
    * Remove `shouldEncodeDecode` parameter from __experimentalUseGlobalBehaviors
    
    * Remove `shouldReturnBehaviors` parameter from __experimentalUseGlobalBehaviors
    
    * Revert test change that sneaked in previous commit
    
    * Skip already existing in Core test that is interfering
    
    * Remove unrelated line that sneaked in thu rebase
    
    ---------
    
    Co-authored-by: Michal Czaplinski <mmczaplinski@gmail.com>

--- lib/compat/wordpress-6.3/rest-api.php
+++ lib/compat/wordpress-6.3/rest-api.php
@@ -53,24 +53,6 @@ function gutenberg_update_templates_template_parts_rest_controller( $args, $post
 add_filter( 'register_post_type_args', 'gutenberg_update_templates_template_parts_rest_controller', 10, 2 );
 
 /**
- * Registers the Global Styles Revisions REST API routes.
- */
-function gutenberg_register_global_styles_revisions_endpoints() {
-	$global_styles_revisions_controller = new Gutenberg_REST_Global_Styles_Revisions_Controller_6_3();
-	$global_styles_revisions_controller->register_routes();
-}
-add_action( 'rest_api_init', 'gutenberg_register_global_styles_revisions_endpoints' );
-
-/**
- * Registers the Global Styles REST API routes.
- */
-function gutenberg_register_global_styles_endpoints() {
-	$global_styles_controller = new Gutenberg_REST_Global_Styles_Controller_6_3();
-	$global_styles_controller->register_routes();
-}
-add_action( 'rest_api_init', 'gutenberg_register_global_styles_endpoints' );
-
-/**
  * Add the `modified` value to the `wp_template` schema.
  *
  * @since 6.3.0 Added 'modified' property and response value.
