1cf6cc729c6d9b7395d3d4d3cbfc72c62185bfd9
Matias Benedetto <matias.benedetto@gmail.com>
2023-07-20 15:53:21 -0300

    Addding a failing test for local fonts upload

--- phpunit/fonts-library/class-wp-font-family-test.php
+++ phpunit/fonts-library/class-wp-font-family-test.php
@@ -104,4 +104,79 @@ class WP_Font_Family_Test extends WP_UnitTestCase {
 	}
 
 
+    function test_install_and_uninstall_local_fonts () {
+        // TODO: Fix this test. Is failing because the font file is not being copied from the temp dir to the fonts folder
+        // We need to figure out why move_uploaded_file call in the WP_Font_Family::move_font_face_asset function is not working while testing
+        $local_font = array(
+            'name'  => 'Inter',
+            'slug' => 'inter',
+            'fontFamily' => 'Inter',
+            'fontFace' => [
+                array (
+                    'fontFamily' => 'Inter',
+                    'fontStyle' => 'normal',
+                    'fontWeight' => '400',
+                    'file' => 'files0',
+                ),
+                array (
+                    'fontFamily' => 'Inter',
+                    'fontStyle' => 'normal',
+                    'fontWeight' => '500',
+                    'file' => 'files1',
+                )
+            ]
+        );
+
+        $files = array(
+            "files0" => array(
+                "name" => "inter1.ttf",
+                "full_path" => "inter1.ttf",
+                "type" => "font/ttf",
+                "tmp_name" => tempnam(sys_get_temp_dir(), 'Inter-'),
+                "error" => 0,
+                "size" => 156764,
+            ),
+            "files1" => array(
+                "name" => "inter2.ttf",
+                "full_path" => "inter2.ttf",
+                "type" => "font/ttf",
+                "tmp_name" => tempnam(sys_get_temp_dir(), 'Inter-'),
+                "error" => 0,
+                "size" => 156524,
+            ),
+        );
+
+        $font = new WP_Font_Family( $local_font );
+		$data = $font->install( $files );
+
+        // Check that the post was created
+		$post = $font->get_font_post();
+		$this->assertInstanceof( 'WP_Post', $post );
+
+        // Check that the post has the correct data
+		$this->assertEquals( $post->post_title, 'Inter' );
+		$this->assertEquals( $post->post_name, 'inter' );
+		$content = json_decode( $post->post_content, true );
+		$this->assertEquals( $content['fontFamily'], 'Inter' );
+		$this->assertEquals( $content['slug'], 'inter' );
+
+        // Check that the font file src was updated to the local font asset
+		$this->assertTrue( str_ends_with( $content['fontFace'][0]['src'], '/inter_normal_400.ttf' ) );
+        $this->assertTrue( str_ends_with( $content['fontFace'][1]['src'], '/inter_normal_500.ttf' ) );
+
+		// Check that the font file was created
+		$this->assertTrue( file_exists( WP_FONTS_DIR . '/inter_normal_400.ttf' ) );
+        $this->assertTrue( file_exists( WP_FONTS_DIR . '/inter_normal_500.ttf' ) );
+
+        $font->uninstall();
+
+		// Check that the post was deleted
+		$post = $font->get_font_post();
+		$this->assertNull( $post );
+
+		// Check that the font asset was deleted
+		$this->assertFalse( file_exists( WP_FONTS_DIR . '/inter_normal_400.ttf' ) );
+        $this->assertFalse( file_exists( WP_FONTS_DIR . '/inter_normal_500.ttf' ) );
+    }
+
 }
