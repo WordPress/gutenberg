9176b2f2e911246dd560217be12f415b46d88a52
David Arenas <david.arenas@automattic.com>
2023-07-21 20:46:00 +0200

    Fix expected and add missing commas

--- phpunit/experimental/interactivity-api/class-wp-interactivity-store-test.php
+++ phpunit/experimental/interactivity-api/class-wp-interactivity-store-test.php
@@ -171,15 +171,15 @@ class WP_Interactivity_Store_Test extends WP_UnitTestCase {
 			array(
 				'state' => array(
 					'amps' => 'http://site.test/?foo=1&baz=2&bar=3',
-					'tags' => 'Do not do this: <!-- <script>'
-				)
+					'tags' => 'Do not do this: <!-- <script>',
+				),
 			)
 		);
 		ob_start();
 		WP_Interactivity_Store::render();
 		$rendered = ob_get_clean();
 		$this->assertSame(
-			'<script id="wp-interactivity-store-data" type="application/json">{"state":{"amps":"http:\/\/site.test\/?foo=1\u0026baz=2\u0026bar=3","content":"Do not do this: \u003C!-- \u003Cscript\u003E"}}</script>',
+			'<script id="wp-interactivity-store-data" type="application/json">{"state":{"amps":"http:\/\/site.test\/?foo=1\u0026baz=2\u0026bar=3","tags":"Do not do this: \u003C!-- \u003Cscript\u003E"}}</script>',
 			$rendered
 		);
 	}
