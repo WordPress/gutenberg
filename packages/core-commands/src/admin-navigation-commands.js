29f21696b456ac41e4107da2da2bdc7c5444fc85
Glen Davies <glen.davies@automattic.com>
2023-07-21 12:16:39 +1200

    update the Manage all my patterns command to redirect to site editor patterns list

--- packages/core-commands/src/admin-navigation-commands.js
+++ packages/core-commands/src/admin-navigation-commands.js
@@ -4,8 +4,26 @@
 import { useCommand } from '@wordpress/commands';
 import { __ } from '@wordpress/i18n';
 import { external, plus } from '@wordpress/icons';
+import { useSelect } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { addQueryArgs, getPath } from '@wordpress/url';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
+
+/**
+ * Internal dependencies
+ */
+import { unlock } from './lock-unlock';
+
+const { useHistory } = unlock( routerPrivateApis );
 
 export function useAdminNavigationCommands() {
+	const history = useHistory();
+	const getSettings = useSelect(
+		( select ) => select( blockEditorStore ).getSettings
+	);
+	const settings = getSettings();
+	const isBlockTheme = settings.__unstableIsBlockBasedTheme;
+
 	useCommand( {
 		name: 'core/add-new-post',
 		label: __( 'Add new post' ),
@@ -25,8 +43,26 @@ export function useAdminNavigationCommands() {
 	useCommand( {
 		name: 'core/manage-reusable-blocks',
 		label: __( 'Manage all of my patterns' ),
-		callback: () => {
-			document.location.href = 'edit.php?post_type=wp_block';
+		callback: ( { close } ) => {
+			const isSiteEditor = getPath( window.location.href )?.includes(
+				'site-editor.php'
+			);
+			// __unstableIsBlockBasedTheme is not defined in the site editor so we need to check the context.
+			// If not the site editor and not a block based theme then redirect to the old wp-admin patterns page.
+			if ( ! isSiteEditor && ! isBlockTheme ) {
+				document.location.href = 'edit.php?post_type=wp_block';
+			} else {
+				const args = {
+					path: '/patterns',
+				};
+				const targetUrl = addQueryArgs( 'site-editor.php', args );
+				if ( isSiteEditor ) {
+					history.push( args );
+				} else {
+					document.location = targetUrl;
+				}
+				close();
+			}
 		},
 		icon: external,
 	} );
