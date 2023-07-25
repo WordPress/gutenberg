6716f6697c290df39eb898514ce64b8ae3c58ca0
Marin Atanasov <tyxla@abv.bg>
2023-07-25 15:00:41 +0300

    Block Library: Set freeform handler only if Classic block exists

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -282,7 +282,7 @@ export const registerCoreBlocks = (
 	blocks.forEach( ( { init } ) => init() );
 
 	setDefaultBlockName( paragraph.name );
-	if ( window.wp && window.wp.oldEditor ) {
+	if ( window.wp && window.wp.oldEditor && blocks.includes( classic ) ) {
 		setFreeformContentHandlerName( classic.name );
 	}
 	setUnregisteredTypeHandlerName( missing.name );
