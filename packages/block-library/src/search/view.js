6cf6643284ef8b5116e286ad07f260ddfadb0909
Weston Ruter <westonruter@google.com>
2023-07-25 15:10:15 -0700

    Use defer loading strategy for frontend view scripts (#52536)
    * Load interactivity API scripts with defer loading strategy
    
    * Load comment-reply script with defer loading strategy
    
    * Load search view script with defer loading strategy
    
    * Defer the view scripts for the Navigation and File blocks
    
    * Use DCA event instead of window load event for Navigation viewScripts
    
    * Optimize submenu-on-click view script
    
    * Fully leverage event delegation to allow async loading strategy.
    * Keep track of whether a submenu is open to short-circuit event handlers.
    * Use passive event listeners.
    
    * Optimize modal view script
    
    * Fully leverage event delegation to allow async loading strategy.
    * Remove event listener for anchor clicks in modal when modal is closed.
    * Use passive event listeners.
    
    * Utilize asset file for wp-block--search-view
    
    * Conditionally enqueue navigation view scripts only if needed
    
    * Update Navigation block to remove/add view scripts in the same way the File block does
    
    * Update Search block to include view script in same way as File block and Navigation block
    
    * Refactor Search block view script
    
    * Adopt asynchronous loading strategy.
    * Use event delegation.
    * Collapse expanded blocks when tabbing out of expanded Search block.
    * Only attach keydown/keyup event handlers while Search block is expanded.
    * Ensure search button's aria-label is translated.
    * Ensure Search button's type is restored to 'button' instead of deleting type (since no type is same as 'submit').
    * Use passive event listeners.
    
    * Use more DOM properties instead of attributes
    
    * Use more precise reflected terminology
    
    * Remove resolved TODO comment
    
    * Use event delegation to open MicroModal
    
    * Remove excessive type check
    
    * Fix closing submenus when there are multiple Navigation blocks on a page
    
    * Clarify block.json comment
    
    * Add core merge note for comment-reply script strategy
    
    * Use defer strategy instead of async for Navigation and Search blocks
    
    * Defer all block view scripts
    
    * Ensure interactivity scripts remain in footer in WP<6.3
    
    * Remove defer from comment-reply for now
    
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- packages/block-library/src/search/view.js
+++ packages/block-library/src/search/view.js
@@ -1,68 +1,172 @@
-window.addEventListener( 'DOMContentLoaded', () => {
-	const hiddenClass = 'wp-block-search__searchfield-hidden';
+/*eslint-env browser*/
 
-	Array.from(
-		document.getElementsByClassName(
-			'wp-block-search__button-behavior-expand'
-		)
-	).forEach( ( block ) => {
-		const searchField = block.querySelector( '.wp-block-search__input' );
-		const searchButton = block.querySelector( '.wp-block-search__button' );
-		const searchLabel = block.querySelector( '.wp-block-search__label' );
-		const ariaLabel = searchButton.getAttribute( 'aria-label' );
-		const id = searchField.getAttribute( 'id' );
+/** @type {?HTMLFormElement} */
+let expandedSearchBlock = null;
 
-		const toggleSearchField = ( showSearchField ) => {
-			if ( showSearchField ) {
-				searchField.removeAttribute( 'aria-hidden' );
-				searchField.removeAttribute( 'tabindex' );
-				searchButton.removeAttribute( 'aria-expanded' );
-				searchButton.removeAttribute( 'aria-controls' );
-				searchButton.setAttribute( 'type', 'submit' );
-				searchButton.setAttribute( 'aria-label', 'Submit Search' );
+const hiddenClass = 'wp-block-search__searchfield-hidden';
 
-				return block.classList.remove( hiddenClass );
-			}
+/**
+ * Toggles aria-label with data-toggled-aria-label.
+ *
+ * @param {HTMLElement} element
+ */
+function toggleAriaLabel( element ) {
+	if ( ! ( 'toggledAriaLabel' in element.dataset ) ) {
+		throw new Error( 'Element lacks toggledAriaLabel in dataset.' );
+	}
 
-			searchButton.removeAttribute( 'type' );
-			searchField.setAttribute( 'aria-hidden', 'true' );
-			searchField.setAttribute( 'tabindex', '-1' );
-			searchButton.setAttribute( 'aria-expanded', 'false' );
-			searchButton.setAttribute( 'aria-controls', id );
-			searchButton.setAttribute( 'aria-label', ariaLabel );
-			return block.classList.add( hiddenClass );
-		};
+	const ariaLabel = element.dataset.toggledAriaLabel;
+	element.dataset.toggledAriaLabel = element.ariaLabel;
+	element.ariaLabel = ariaLabel;
+}
 
-		const hideSearchField = ( e ) => {
-			if ( ! e.target.closest( '.wp-block-search' ) ) {
-				return toggleSearchField( false );
-			}
+/**
+ * Gets search input.
+ *
+ * @param {HTMLFormElement} block Search block.
+ * @return {HTMLInputElement} Search input.
+ */
+function getSearchInput( block ) {
+	return block.querySelector( '.wp-block-search__input' );
+}
 
-			if ( e.key === 'Escape' ) {
-				searchButton.focus();
-				return toggleSearchField( false );
-			}
-		};
+/**
+ * Gets search button.
+ *
+ * @param {HTMLFormElement} block Search block.
+ * @return {HTMLButtonElement} Search button.
+ */
+function getSearchButton( block ) {
+	return block.querySelector( '.wp-block-search__button' );
+}
 
-		const handleButtonClick = ( e ) => {
-			if ( block.classList.contains( hiddenClass ) ) {
-				e.preventDefault();
-				searchField.focus();
-				toggleSearchField( true );
-			}
-		};
+/**
+ * Handles keydown event to collapse an expanded Search block (when pressing Escape key).
+ *
+ * @param {KeyboardEvent} event
+ */
+function handleKeydownEvent( event ) {
+	if ( ! expandedSearchBlock ) {
+		// In case the event listener wasn't removed in time.
+		return;
+	}
 
-		searchButton.removeAttribute( 'type' );
-		searchField.addEventListener( 'keydown', ( e ) => {
-			hideSearchField( e );
-		} );
-		searchButton.addEventListener( 'click', handleButtonClick );
-		searchButton.addEventListener( 'keydown', ( e ) => {
-			hideSearchField( e );
-		} );
-		if ( searchLabel ) {
-			searchLabel.addEventListener( 'click', handleButtonClick );
-		}
-		document.body.addEventListener( 'click', hideSearchField );
+	if ( event.key === 'Escape' ) {
+		const block = expandedSearchBlock; // This is nullified by collapseExpandedSearchBlock().
+		collapseExpandedSearchBlock();
+		getSearchButton( block ).focus();
+	}
+}
+
+/**
+ * Handles keyup event to collapse an expanded Search block (e.g. when tabbing out of expanded Search block).
+ *
+ * @param {KeyboardEvent} event
+ */
+function handleKeyupEvent( event ) {
+	if ( ! expandedSearchBlock ) {
+		// In case the event listener wasn't removed in time.
+		return;
+	}
+
+	if ( event.target.closest( '.wp-block-search' ) !== expandedSearchBlock ) {
+		collapseExpandedSearchBlock();
+	}
+}
+
+/**
+ * Expands search block.
+ *
+ * Inverse of what is done in collapseExpandedSearchBlock().
+ *
+ * @param {HTMLFormElement} block Search block.
+ */
+function expandSearchBlock( block ) {
+	// Make sure only one is open at a time.
+	if ( expandedSearchBlock ) {
+		collapseExpandedSearchBlock();
+	}
+
+	const searchField = getSearchInput( block );
+	const searchButton = getSearchButton( block );
+
+	searchButton.type = 'submit';
+	searchField.ariaHidden = 'false';
+	searchField.tabIndex = 0;
+	searchButton.ariaExpanded = 'true';
+	searchButton.removeAttribute( 'aria-controls' ); // Note: Seemingly not reflected with searchButton.ariaControls.
+	toggleAriaLabel( searchButton );
+	block.classList.remove( hiddenClass );
+
+	searchField.focus(); // Note that Chrome seems to do this automatically.
+
+	// The following two must be inverse of what is done in collapseExpandedSearchBlock().
+	document.addEventListener( 'keydown', handleKeydownEvent, {
+		passive: true,
 	} );
-} );
+	document.addEventListener( 'keyup', handleKeyupEvent, {
+		passive: true,
+	} );
+
+	expandedSearchBlock = block;
+}
+
+/**
+ * Collapses the expanded search block.
+ *
+ * Inverse of what is done in expandSearchBlock().
+ */
+function collapseExpandedSearchBlock() {
+	if ( ! expandedSearchBlock ) {
+		throw new Error( 'Expected expandedSearchBlock to be defined.' );
+	}
+	const block = expandedSearchBlock;
+	const searchField = getSearchInput( block );
+	const searchButton = getSearchButton( block );
+
+	searchButton.type = 'button';
+	searchField.ariaHidden = 'true';
+	searchField.tabIndex = -1;
+	searchButton.ariaExpanded = 'false';
+	searchButton.setAttribute( 'aria-controls', searchField.id ); // Note: Seemingly not reflected with searchButton.ariaControls.
+	toggleAriaLabel( searchButton );
+	block.classList.add( hiddenClass );
+
+	// The following two must be inverse of what is done in expandSearchBlock().
+	document.removeEventListener( 'keydown', handleKeydownEvent, {
+		passive: true,
+	} );
+	document.removeEventListener( 'keyup', handleKeyupEvent, {
+		passive: true,
+	} );
+
+	expandedSearchBlock = null;
+}
+
+// Listen for click events anywhere on the document so this script can be loaded asynchronously in the head.
+document.addEventListener(
+	'click',
+	( event ) => {
+		// Get the ancestor expandable Search block of the clicked element.
+		const block = event.target.closest(
+			'.wp-block-search__button-behavior-expand'
+		);
+
+		/*
+		 * If there is already an expanded search block and either the current click was not for a Search block or it was
+		 * for another block, then collapse the currently-expanded block.
+		 */
+		if ( expandedSearchBlock && block !== expandedSearchBlock ) {
+			collapseExpandedSearchBlock();
+		}
+
+		// If the click was on or inside a collapsed Search block, expand it.
+		if (
+			block instanceof HTMLFormElement &&
+			block.classList.contains( hiddenClass )
+		) {
+			expandSearchBlock( block );
+		}
+	},
+	{ passive: true }
+);
