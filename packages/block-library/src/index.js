ec67e057b8621bdb37d61d669287d17753b77629
Marin Atanasov <tyxla@abv.bg>
2023-07-26 10:44:03 +0300

    Better way to check for classic block existence

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -282,7 +282,11 @@ export const registerCoreBlocks = (
 	blocks.forEach( ( { init } ) => init() );
 
 	setDefaultBlockName( paragraph.name );
-	if ( window.wp && window.wp.oldEditor && blocks.includes( classic ) ) {
+	if (
+		window.wp &&
+		window.wp.oldEditor &&
+		blocks.some( ( { name } ) => name === classic.name )
+	) {
 		setFreeformContentHandlerName( classic.name );
 	}
 	setUnregisteredTypeHandlerName( missing.name );
