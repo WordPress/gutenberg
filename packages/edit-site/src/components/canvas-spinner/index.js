2c76206f1859171db9429b5d0096d2b732378412
Marin Atanasov <tyxla@abv.bg>
2023-07-27 16:34:18 +0300

    Edit Site: Use progress bar for loading screen

--- packages/edit-site/src/components/canvas-spinner/index.js
+++ packages/edit-site/src/components/canvas-spinner/index.js
@@ -1,12 +1,42 @@
 /**
+ * External dependencies
+ */
+import { colord, extend } from 'colord';
+import mixPlugin from 'colord/plugins/mix';
+
+extend( [ mixPlugin ] );
+
+/**
  * WordPress dependencies
  */
-import { Spinner } from '@wordpress/components';
+import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
+import { privateApis as componentsPrivateApis } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { unlock } from '../../lock-unlock';
+import { useStylesPreviewColors } from '../global-styles/hooks';
+
+const { ProgressBar } = unlock( componentsPrivateApis );
+const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 export default function CanvasSpinner() {
+	const [ textColor ] = useGlobalStyle( 'color.text' );
+	const { highlightedColors } = useStylesPreviewColors();
+
+	const trackColor = highlightedColors[ 0 ]?.color ?? textColor;
+	const trackColord = colord( trackColor );
+	const indicatorColor = trackColord.isDark()
+		? trackColord.tints( 3 )[ 1 ].toHex()
+		: trackColord.shades( 3 )[ 1 ].toHex();
+
 	return (
 		<div className="edit-site-canvas-spinner">
-			<Spinner />
+			<ProgressBar
+				indicatorColor={ indicatorColor }
+				trackColor={ trackColor }
+			/>
 		</div>
 	);
 }
