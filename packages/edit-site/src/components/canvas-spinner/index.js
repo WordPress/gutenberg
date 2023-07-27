dd1730345ad0945b8d9b6b524a0ef9fdf2bf12df
Marin Atanasov <tyxla@abv.bg>
2023-07-27 18:43:05 +0300

    Polish theme colors and fallbacks

--- packages/edit-site/src/components/canvas-spinner/index.js
+++ packages/edit-site/src/components/canvas-spinner/index.js
@@ -22,14 +22,16 @@ const { ProgressBar } = unlock( componentsPrivateApis );
 const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 export default function CanvasSpinner() {
-	const [ textColor ] = useGlobalStyle( 'color.text' );
+	const [ fallbackTrackColor ] = useGlobalStyle( 'color.text' );
 	const { highlightedColors } = useStylesPreviewColors();
 
-	const trackColor = highlightedColors[ 0 ]?.color ?? textColor;
+	const trackColor = highlightedColors[ 1 ]?.color ?? fallbackTrackColor;
 	const trackColord = colord( trackColor );
-	const indicatorColor = trackColord.isDark()
+	const fallbackIndicatorColor = trackColord.isDark()
 		? trackColord.tints( 3 )[ 1 ].toHex()
 		: trackColord.shades( 3 )[ 1 ].toHex();
+	const indicatorColor =
+		highlightedColors[ 0 ]?.color ?? fallbackIndicatorColor;
 
 	return (
 		<div className="edit-site-canvas-spinner">
