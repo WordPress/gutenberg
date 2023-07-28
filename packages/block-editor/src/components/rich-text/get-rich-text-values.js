194364ec92edffdf48e40b5f6b705f7973daf29d
Ella <ella@vandurpe.com>
2023-07-28 15:56:25 +0100

    Alternate approach

--- packages/block-editor/src/components/rich-text/get-rich-text-values.js
+++ packages/block-editor/src/components/rich-text/get-rich-text-values.js
@@ -13,8 +13,6 @@ import {
 import InnerBlocks from '../inner-blocks';
 import { Content } from './content';
 
-const elementToInnerBlocksMap = new WeakMap();
-
 /*
  * This function is similar to `@wordpress/element`'s `renderToString` function,
  * except that it does not render the elements to a string, but instead collects
@@ -33,10 +31,6 @@ function addValuesForElement( element, values, innerBlocks ) {
 		case 'string':
 		case 'number':
 			return;
-	}
-
-	if ( elementToInnerBlocksMap.has( element ) ) {
-		innerBlocks = elementToInnerBlocksMap.get( element ).innerBlocks;
 	}
 
 	const { type, props } = element;
@@ -81,34 +75,17 @@ function addValuesForElements( children, ...args ) {
 	}
 }
 
-function _getSaveElement( name, attributes, innerBlocks ) {
-	return getSaveElement(
-		name,
-		attributes,
-		// We need to pass the inner blocks as save elements so that these
-		// elements can be used by `useInnerBlocksProps.save`
-		// (getInnerBlocksProps in the blocks package). The save element tree
-		// could in turn have inner blocks, which we may need when rendering
-		// `InnerBlocks.Content`.
-		innerBlocks.map( ( block ) => {
-			const saveElement = _getSaveElement(
-				block.name,
-				block.attributes,
-				block.innerBlocks
-			);
-			if ( ! saveElement ) {
-				return null;
-			}
-			elementToInnerBlocksMap.set( saveElement, block );
-			return saveElement;
-		} )
-	);
-}
-
 function addValuesForBlocks( values, blocks ) {
 	for ( let i = 0; i < blocks.length; i++ ) {
 		const { name, attributes, innerBlocks } = blocks[ i ];
-		const saveElement = _getSaveElement( name, attributes, innerBlocks );
+		const saveElement = getSaveElement(
+			name,
+			attributes,
+			// Instead of letting save elements use `useInnerBlocksProps.save`,
+			// force them to use InnerBlocks.Content instead so we can intercept
+			// a single component.
+			<InnerBlocks.Content />
+		);
 		addValuesForElement( saveElement, values, innerBlocks );
 	}
 }
