856bd5c6c691f9a1a19a6988073782bd9f2d83d4
Glen Davies <glen.davies@automattic.com>
2023-07-31 14:47:41 +1200

    Remove unused exports

--- packages/reusable-blocks/src/components/index.js
+++ packages/reusable-blocks/src/components/index.js
@@ -1,2 +1,1 @@
 export { default as ReusableBlocksMenuItems } from './reusable-blocks-menu-items';
-export { default as __experimentalCreatePatternModal } from './create-pattern-modal';
--- packages/reusable-blocks/src/index.js
+++ packages/reusable-blocks/src/index.js
@@ -1,3 +1,2 @@
 export { store } from './store';
 export * from './components';
-export { privateApis } from './private-apis';
