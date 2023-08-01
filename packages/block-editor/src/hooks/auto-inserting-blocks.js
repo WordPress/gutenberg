d385c35d2a573db0bdc0fd1ae59f271e52921a11
Bernie Reiter <ockham@raz.or.at>
2023-08-01 13:57:56 +0200

    Show basic list of auto-inserted block names in panel

--- packages/block-editor/src/hooks/auto-inserting-blocks.js
+++ packages/block-editor/src/hooks/auto-inserting-blocks.js
@@ -5,26 +5,46 @@ import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
 import { PanelBody } from '@wordpress/components';
 import { createHigherOrderComponent } from '@wordpress/compose';
-// import { store as blocksStore } from '@wordpress/blocks';
-// import { useSelect } from '@wordpress/data';
+import { store as blocksStore } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import { InspectorControls } from '../components';
 
-function AutoInsertingBlocksControl() {
-	// const blocks = useSelect( ( select ) => {
-	// 	const { getBlockTypes } = select( blocksStore );
-	// 	return getBlockTypes();
-	// }, [] );
+function AutoInsertingBlocksControl( props ) {
+	const blocks = useSelect( ( select ) => {
+		const { getBlockTypes } = select( blocksStore );
+		return getBlockTypes();
+	}, [] );
+
+	const autoInsertedBlocksForCurrentBlock = blocks.reduce(
+		( autoInsertedBlocks, block ) => {
+			if ( ! block.autoInsert ) {
+				return autoInsertedBlocks;
+			}
+
+			const name = Object.keys( block.autoInsert ).find(
+				( n ) => n === props.blockName
+			);
+			if ( name !== undefined ) {
+				autoInsertedBlocks[ block.name ] = block.autoInsert[ name ];
+			}
+			return autoInsertedBlocks;
+		},
+		{}
+	);
 
 	return (
 		<InspectorControls>
-			<PanelBody
-				title={ __( 'Plugins' ) }
-				initialOpen={ true }
-			></PanelBody>
+			<PanelBody title={ __( 'Plugins' ) } initialOpen={ true }>
+				{ Object.keys( autoInsertedBlocksForCurrentBlock ).map(
+					( blockName ) => {
+						return <div key={ blockName }>{ blockName }</div>;
+					}
+				) }
+			</PanelBody>
 		</InspectorControls>
 	);
 }
@@ -36,7 +56,7 @@ export const withAutoInsertingBlocks = createHigherOrderComponent(
 			return (
 				<>
 					{ blockEdit }
-					<AutoInsertingBlocksControl />
+					<AutoInsertingBlocksControl blockName={ props.name } />
 				</>
 			);
 		};
