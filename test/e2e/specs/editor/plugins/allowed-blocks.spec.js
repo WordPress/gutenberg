5d10e25be59fbff6cff17025a560f09fe8e77479
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2023-08-01 17:40:09 +0530

    Fix Style Lint Issue

--- test/e2e/specs/editor/plugins/allowed-blocks.spec.js
+++ test/e2e/specs/editor/plugins/allowed-blocks.spec.js
@@ -20,31 +20,41 @@ test.describe( 'Allowed Blocks Filter', () => {
 		page,
 	} ) => {
 		// The paragraph block is available.
-		await page.getByRole('button', { name: 'Toggle block inserter' }).click();
+		await page
+			.getByRole( 'button', { name: 'Toggle block inserter' } )
+			.click();
 
-		await page.getByPlaceholder('Search', { exact: true }).fill('Paragraph');
+		await page
+			.getByPlaceholder( 'Search', { exact: true } )
+			.fill( 'Paragraph' );
 
-		expect(  page.getByRole('option', { name: 'Paragraph' }) ).toBeVisible();
+		await expect(
+			page.getByRole( 'option', { name: 'Paragraph' } )
+		).toBeVisible();
 
 		// The gallery block is not available.
-		await page.getByPlaceholder('Search', { exact: true }).click({
-			clickCount: 3
-		  });
+		await page.getByPlaceholder( 'Search', { exact: true } ).click( {
+			clickCount: 3,
+		} );
 		await page.keyboard.press( 'Backspace' );
 
-		await page.getByPlaceholder('Search', { exact: true }).fill('Gallery');
+		await page
+			.getByPlaceholder( 'Search', { exact: true } )
+			.fill( 'Gallery' );
 
-		expect( page.getByRole('option', { name: 'Gallery' })).toBeHidden()
+		await expect(
+			page.getByRole( 'option', { name: 'Gallery' } )
+		).toBeHidden();
 	} );
 
 	test( 'should remove not allowed blocks from the block manager', async ( {
 		page,
 	} ) => {
-		await page.getByRole('button', { name: 'Options' }).click();
+		await page.getByRole( 'button', { name: 'Options' } ).click();
 
-		await page.getByRole('menuitem', { name: 'Preferences' }).click();
+		await page.getByRole( 'menuitem', { name: 'Preferences' } ).click();
 
-		await page.getByRole('tab', { name: 'Blocks' }).click();
+		await page.getByRole( 'tab', { name: 'Blocks' } ).click();
 
 		const BLOCK_LABEL_SELECTOR =
 			'.edit-post-block-manager__checklist-item .components-checkbox-control__label';
