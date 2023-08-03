88dd0b561294ad02b6ba6424f22445bec7bf9ad2
Michal Czaplinski <mmczaplinski@gmail.com>
2023-08-03 12:29:52 +0100

    Update names of HTML processor instances in meta.php

--- lib/experimental/custom-sources/meta.php
+++ lib/experimental/custom-sources/meta.php
@@ -11,8 +11,8 @@ return array(
 		// We should probably also check if the meta field exists but for now it's okay because
 		// if it doesn't, `get_post_meta()` will just return an empty string.
 		$meta_value = get_post_meta( $block_instance->context['postId'], $meta_field, true );
-		$p          = new WP_HTML_Tag_Processor( $block_content );
-		$found      = $p->next_tag(
+		$tags          = new WP_HTML_Tag_Processor( $block_content );
+		$found      = $tags->next_tag(
 			array(
 				// TODO: In the future, when blocks other than Paragraph and Image are
 				// supported, we should build the full query from CSS selector.
@@ -22,15 +22,15 @@ return array(
 		if ( ! $found ) {
 			return $block_content;
 		}
-		$tag_name = $p->get_tag();
+		$tag_name = $tags->get_tag();
 		$markup   = "<$tag_name>$meta_value</$tag_name>";
-		$p2       = new WP_HTML_Tag_Processor( $markup );
-		$p2->next_tag();
-		$names = $p->get_attribute_names_with_prefix( '' );
+		$updated_tags       = new WP_HTML_Tag_Processor( $markup );
+		$updated_tags->next_tag();
+		$names = $tags->get_attribute_names_with_prefix( '' );
 		foreach ( $names as $name ) {
-			$p2->set_attribute( $name, $p->get_attribute( $name ) );
+			$updated_tags->set_attribute( $name, $tags->get_attribute( $name ) );
 		}
 
-		return $p2 . '';
+		return $updated_tags->get_updated_html() . '';
 	},
 );
