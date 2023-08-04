3aad5b5a785c4e52f82d33a6d8d9522fd3a059d4
Marin Atanasov <tyxla@abv.bg>
2023-08-04 15:16:58 +0300

    ProgressBar: Use the theme system accent for indicator color

--- packages/components/src/progress-bar/styles.ts
+++ packages/components/src/progress-bar/styles.ts
@@ -43,7 +43,7 @@ export const Indicator = styled.div< {
 	top: 0;
 	height: 100%;
 	border-radius: ${ CONFIG.radiusBlockUi };
-	background-color: ${ COLORS.ui.theme };
+	background-color: var( --wp-components-color-accent, ${ COLORS.ui.theme } );
 
 	${ ( { isIndeterminate, value } ) =>
 		isIndeterminate
