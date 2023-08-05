44fab105f18eee760da7d20d24e054bcc19ab848
George Mamadashvili <georgemamadashvili@gmail.com>
2023-08-05 09:29:31 +0400

    Update packages/edit-post/src/components/sidebar/plugin-pre-publish-panel/index.js
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- packages/edit-post/src/components/sidebar/plugin-pre-publish-panel/index.js
+++ packages/edit-post/src/components/sidebar/plugin-pre-publish-panel/index.js
@@ -65,7 +65,7 @@ const PluginPrePublishPanel = ( {
 	initialOpen = false,
 	icon,
 } ) => {
-	const context = usePluginContext();
+	const { icon: pluginIcon } = usePluginContext();
 
 	return (
 		<Fill>
