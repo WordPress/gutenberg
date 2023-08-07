6a2785f6e51674a1ff0b9fe275ed00103cbc5321
Michal Czaplinski <mmczaplinski@gmail.com>
2023-08-07 14:58:18 +0100

    Do not export `addAttribute()` or `withInspectorControl()`

--- packages/block-editor/src/hooks/custom-fields.js
+++ packages/block-editor/src/hooks/custom-fields.js
@@ -20,7 +20,7 @@ import { useBlockEditingMode } from '../components/block-editing-mode';
  *
  * @return {Object} Filtered block settings.
  */
-export function addAttribute( settings ) {
+function addAttribute( settings ) {
 	if ( hasBlockSupport( settings, '__experimentalConnections', true ) ) {
 		// Gracefully handle if settings.attributes is undefined.
 		settings.attributes = {
@@ -44,91 +44,87 @@ export function addAttribute( settings ) {
  *
  * @return {WPComponent} Wrapped component.
  */
-export const withInspectorControl = createHigherOrderComponent(
-	( BlockEdit ) => {
-		return ( props ) => {
-			const blockEditingMode = useBlockEditingMode();
-			const hasCustomFieldsSupport = hasBlockSupport(
-				props.name,
-				'__experimentalConnections',
-				false
-			);
+const withInspectorControl = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		const blockEditingMode = useBlockEditingMode();
+		const hasCustomFieldsSupport = hasBlockSupport(
+			props.name,
+			'__experimentalConnections',
+			false
+		);
 
-			// Check if the current block is a paragraph or image block.
-			// Currently, only these two blocks are supported.
-			if ( ! [ 'core/paragraph', 'core/image' ].includes( props.name ) ) {
-				return <BlockEdit { ...props } />;
-			}
+		// Check if the current block is a paragraph or image block.
+		// Currently, only these two blocks are supported.
+		if ( ! [ 'core/paragraph', 'core/image' ].includes( props.name ) ) {
+			return <BlockEdit { ...props } />;
+		}
 
-			// If the block is a paragraph or image block, we need to know which
-			// attribute to use for the connection. Only the `content` attribute
-			// of the paragraph block and the `url` attribute of the image block are supported.
-			let attributeName;
-			if ( props.name === 'core/paragraph' ) attributeName = 'content';
-			if ( props.name === 'core/image' ) attributeName = 'url';
+		// If the block is a paragraph or image block, we need to know which
+		// attribute to use for the connection. Only the `content` attribute
+		// of the paragraph block and the `url` attribute of the image block are supported.
+		let attributeName;
+		if ( props.name === 'core/paragraph' ) attributeName = 'content';
+		if ( props.name === 'core/image' ) attributeName = 'url';
 
-			if ( hasCustomFieldsSupport && props.isSelected ) {
-				return (
-					<>
-						<BlockEdit { ...props } />
-						{ blockEditingMode === 'default' && (
-							<InspectorControls>
-								<PanelBody
-									title={ __( 'Connections' ) }
-									initialOpen={ true }
-								>
-									<TextControl
-										__nextHasNoMarginBottom
-										autoComplete="off"
-										label={ __( 'Custom field meta_key' ) }
-										value={
-											props.attributes?.connections
-												?.attributes?.[ attributeName ]
-												?.value || ''
-										}
-										onChange={ ( nextValue ) => {
-											if ( nextValue === '' ) {
-												props.setAttributes( {
-													connections: undefined,
-													[ attributeName ]:
-														undefined,
-													placeholder: undefined,
-												} );
-											} else {
-												props.setAttributes( {
-													connections: {
-														attributes: {
-															// The attributeName will be either `content` or `url`.
-															[ attributeName ]: {
-																// Source will be variable, could be post_meta, user_meta, term_meta, etc.
-																// Could even be a custom source like a social media attribute.
-																source: 'meta_fields',
-																value: nextValue,
-															},
+		if ( hasCustomFieldsSupport && props.isSelected ) {
+			return (
+				<>
+					<BlockEdit { ...props } />
+					{ blockEditingMode === 'default' && (
+						<InspectorControls>
+							<PanelBody
+								title={ __( 'Connections' ) }
+								initialOpen={ true }
+							>
+								<TextControl
+									__nextHasNoMarginBottom
+									autoComplete="off"
+									label={ __( 'Custom field meta_key' ) }
+									value={
+										props.attributes?.connections
+											?.attributes?.[ attributeName ]
+											?.value || ''
+									}
+									onChange={ ( nextValue ) => {
+										if ( nextValue === '' ) {
+											props.setAttributes( {
+												connections: undefined,
+												[ attributeName ]: undefined,
+												placeholder: undefined,
+											} );
+										} else {
+											props.setAttributes( {
+												connections: {
+													attributes: {
+														// The attributeName will be either `content` or `url`.
+														[ attributeName ]: {
+															// Source will be variable, could be post_meta, user_meta, term_meta, etc.
+															// Could even be a custom source like a social media attribute.
+															source: 'meta_fields',
+															value: nextValue,
 														},
 													},
-													[ attributeName ]:
-														undefined,
-													placeholder: sprintf(
-														'This content will be replaced on the frontend by the value of "%s" custom field.',
-														nextValue
-													),
-												} );
-											}
-										} }
-									/>
-								</PanelBody>
-							</InspectorControls>
-						) }
-					</>
-				);
-			}
+												},
+												[ attributeName ]: undefined,
+												placeholder: sprintf(
+													'This content will be replaced on the frontend by the value of "%s" custom field.',
+													nextValue
+												),
+											} );
+										}
+									} }
+								/>
+							</PanelBody>
+						</InspectorControls>
+					) }
+				</>
+			);
+		}
 
-			return <BlockEdit { ...props } />;
-		};
-	},
-	'withInspectorControl'
-);
+		return <BlockEdit { ...props } />;
+	};
+}, 'withInspectorControl' );
+
 if ( window.__experimentalConnections ) {
 	addFilter(
 		'blocks.registerBlockType',
