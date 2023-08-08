6158b234c3380df9f6aa5db078cb32d359a5f98d
Gerardo <gerardo.pacheco@automattic.com>
2023-08-08 19:46:35 +0200

    Remove unneeded changes

--- packages/react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
+++ packages/react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
@@ -67,6 +67,7 @@ class RCTAztecView: Aztec.TextView {
         let label = UILabel(frame: .zero)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .natural
+        label.font = font
 
         return label
     }()
@@ -574,17 +575,7 @@ class RCTAztecView: Aztec.TextView {
     var placeholderAttributes: [NSAttributedString.Key: Any] {
         var placeholderAttributes = typingAttributes
         placeholderAttributes[.foregroundColor] = placeholderTextColor
-        placeholderAttributes[.font] = placeholderFont
         return placeholderAttributes
-    }
-
-    @objc var placeholderFont: UIFont {
-        set {
-            placeholderLabel.font = newValue
-        }
-        get {
-            return placeholderLabel.font
-        }
     }
 
     @objc var placeholderTextColor: UIColor {
