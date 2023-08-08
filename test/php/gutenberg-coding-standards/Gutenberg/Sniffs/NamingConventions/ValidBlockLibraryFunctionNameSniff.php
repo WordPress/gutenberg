8f74d2adea8ad342bf67f57bbcce74e44096b6a4
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-08-08 19:32:18 +0200

    Improve error message.

--- test/php/gutenberg-coding-standards/Gutenberg/Sniffs/NamingConventions/ValidBlockLibraryFunctionNameSniff.php
+++ test/php/gutenberg-coding-standards/Gutenberg/Sniffs/NamingConventions/ValidBlockLibraryFunctionNameSniff.php
@@ -108,7 +108,7 @@ final class ValidBlockLibraryFunctionNameSniff implements Sniff {
 			return;
 		}
 
-		$errorMessage = 'In this file, only the following prefixes for PHP function names are allowed: "' . implode( '", "', $allowed_function_prefixes ) . '"';
+		$errorMessage = 'Only the following prefixes for PHP function names are allowed in this file: "' . implode( '", "', $allowed_function_prefixes ) . '"';
 		$phpcsFile->addError( $errorMessage, $functionToken, 'FunctionNameInvalid' );
 	}
 
