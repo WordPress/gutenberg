7a19920fb5763813b1bd9c5a5968811b86a928a5
Kai Hao <kevin830726@gmail.com>
2023-08-08 10:20:14 +0800

    Fix Synced Patterns' color in quick inserter (#53327)
    * Fix Synced Patterns' color in quick inserter
    
    * Fix test
    
    * Remove legacy condition

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -1963,7 +1963,12 @@ const buildBlockTypeItem =
 export const getInserterItems = createSelector(
 	( state, rootClientId = null ) => {
 		const buildReusableBlockInserterItem = ( reusableBlock ) => {
-			const icon = symbol;
+			const icon = ! reusableBlock.wp_pattern_sync_status
+				? {
+						src: symbol,
+						foreground: 'var(--wp-block-synced-color)',
+				  }
+				: symbol;
 			const id = `core/block/${ reusableBlock.id }`;
 			const { time, count = 0 } = getInsertUsage( state, id ) || {};
 			const frecency = calculateFrecency( time, count );
