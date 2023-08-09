afc0b70302c59951c0db847ae65265e06b08a5bb
Mario Santos <santosguillamot@gmail.com>
2023-08-09 14:24:52 +0200

    Remove old functions

--- packages/block-library/src/search/view-interactivity.js
+++ packages/block-library/src/search/view-interactivity.js
@@ -3,28 +3,6 @@
  */
 import { store as wpStore } from '@wordpress/interactivity';
 
-function toggleAriaLabel( element ) {
-	if ( ! ( 'toggledAriaLabel' in element.dataset ) ) {
-		throw new Error( 'Element lacks toggledAriaLabel in dataset.' );
-	}
-
-	const ariaLabel = element.dataset.toggledAriaLabel;
-	element.dataset.toggledAriaLabel = element.ariaLabel;
-	element.ariaLabel = ariaLabel;
-}
-
-function toggleSearch( { context, ref } ) {
-	if ( context.core.search.isSearchCollapsed ) {
-		context.core.search.isSearchCollapsed =
-			! context.core.search.isSearchCollapsed;
-		ref.type = 'submit';
-		ref.ariaExpanded = 'true';
-		ref.removeAttribute( 'aria-controls' );
-		toggleAriaLabel( ref );
-		// TODO: Focus on sibling input. (In Chrome seems to make it automatically)
-	}
-}
-
 wpStore( {
 	selectors: {
 		core: {
@@ -57,7 +35,6 @@ wpStore( {
 	actions: {
 		core: {
 			search: {
-				toggleSearch,
 				openSearchInput: ( { context } ) => {
 					if ( ! context.core.search.isSearchInputVisible ) {
 						context.core.search.isSearchInputVisible = true;
