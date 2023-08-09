bba74afe9ed4622404e51d5c3e6991783bc5ccb8
Bernie Reiter <ockham@raz.or.at>
2023-08-09 13:55:31 +0200

    Add inline TODO note

--- packages/block-editor/src/hooks/auto-inserting-blocks.js
+++ packages/block-editor/src/hooks/auto-inserting-blocks.js
@@ -49,6 +49,7 @@ function AutoInsertingBlocksControl( props ) {
 					if ( [ 'before', 'after' ].includes( relativePosition ) ) {
 						const direction = relativePosition === 'after' ? 1 : -1;
 						let clientId = props.clientId;
+						// TODO: Also stop when we encounter a non-auto-inserted block.
 						while (
 							( clientId = getAdjacentBlockClientId(
 								clientId,
