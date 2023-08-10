b2a2ba6c6d5056c9687b3db1325219087dc6fcc9
hellofromtonya <tonya.mork@automattic.com>
2023-08-10 12:35:01 -0500

    init global function: renames & adds Core merge notes.
    The function is for the plugin only and will not be merged into Core.
    
    However, the code within the function will go into Core, albeit
    in different Core functions.
    
    `@core-merge` annotation is added with Core merge notes to help
    whomever creates the Core PR.

--- lib/experimental/fonts-library/fonts-library.php
+++ lib/experimental/fonts-library/fonts-library.php
@@ -9,18 +9,27 @@
  * @since      6.4.0
  */
 
-if ( ! function_exists( 'fonts_library_init' ) ) {
+if ( ! function_exists( 'gutenberg_init_fonts_library' ) ) {
 	/**
 	 * Registers the routes for the objects of the controller.
 	 *
+	 * This function will not be merged into Core. However, the
+	 * code in the function will be. @core-merge annotation
+	 * provides instructions on where the could needs to go
+	 * in Core.
+	 *
 	 * @since 6.4.0
 	 */
-	function fonts_library_init() {
+	function gutenberg_init_fonts_library() {
+		// @core-merge: This code will not go into Core. Rather,
+		// the code in the static method goes into Core's `create_initial_post_types()`.
 		WP_Fonts_Library::register_post_type();
+
+		// @core-merge: This code will go into Core's `create_initial_rest_routes()`.
 		$fonts_library_controller = new WP_REST_Fonts_Library_Controller();
 		$fonts_library_controller->register_routes();
 	}
 
-	add_action( 'rest_api_init', 'fonts_library_init' );
+	add_action( 'rest_api_init', 'gutenberg_init_fonts_library' );
 }
 
