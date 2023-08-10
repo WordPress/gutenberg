82968c9d685929b2bea1775acfaee80837ab9dfb
hellofromtonya <tonya.mork@automattic.com>
2023-08-10 12:48:55 -0500

    Remove function_exist wrap from Gutenberg init function.
    This function will only exists in Gutenberg, thus it does
    not the guard to protect it from the same function in Core
    (that is once the Fonts Library is merged into Core).

--- lib/experimental/fonts-library/fonts-library.php
+++ lib/experimental/fonts-library/fonts-library.php
@@ -9,31 +9,29 @@
  * @since      6.4.0
  */
 
-if ( ! function_exists( 'gutenberg_init_fonts_library' ) ) {
-	/**
-	 * Registers the routes for the objects of the controller.
-	 *
-	 * This function will not be merged into Core. However, the
-	 * code in the function will be. @core-merge annotation
-	 * provides instructions on where the could needs to go
-	 * in Core.
-	 *
-	 * @since 6.4.0
-	 */
-	function gutenberg_init_fonts_library() {
-		// @core-merge: This code will go into Core's `create_initial_post_types()`.
-		$args = array(
-			'public'       => true,
-			'label'        => 'Font Library',
-			'show_in_rest' => true,
-		);
-		register_post_type( 'wp_font_family', $args );
+/**
+ * Registers the routes for the objects of the controller.
+ *
+ * This function will not be merged into Core. However, the
+ * code in the function will be. @core-merge annotation
+ * provides instructions on where the could needs to go
+ * in Core.
+ *
+ * @since 6.4.0
+ */
+function gutenberg_init_fonts_library() {
+	// @core-merge: This code will go into Core's `create_initial_post_types()`.
+	$args = array(
+		'public'       => true,
+		'label'        => 'Font Library',
+		'show_in_rest' => true,
+	);
+	register_post_type( 'wp_font_family', $args );
 
-		// @core-merge: This code will go into Core's `create_initial_rest_routes()`.
-		$fonts_library_controller = new WP_REST_Fonts_Library_Controller();
-		$fonts_library_controller->register_routes();
-	}
-
-	add_action( 'rest_api_init', 'gutenberg_init_fonts_library' );
+	// @core-merge: This code will go into Core's `create_initial_rest_routes()`.
+	$fonts_library_controller = new WP_REST_Fonts_Library_Controller();
+	$fonts_library_controller->register_routes();
 }
+
+add_action( 'rest_api_init', 'gutenberg_init_fonts_library' );
 
