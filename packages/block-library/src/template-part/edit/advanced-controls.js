1435b5e83e3339cac7942a4e29fcd1619665a5c3
George Mamadashvili <georgemamadashvili@gmail.com>
2023-08-15 14:27:38 +0400

    Move the HTML element description map outside of the component

--- packages/block-library/src/template-part/edit/advanced-controls.js
+++ packages/block-library/src/template-part/edit/advanced-controls.js
@@ -12,6 +12,27 @@ import { useSelect } from '@wordpress/data';
  */
 import { TemplatePartImportControls } from './import-controls';
 
+const htmlElementMessages = {
+	header: __(
+		'The <header> element should represent introductory content, typically a group of introductory or navigational aids.'
+	),
+	main: __(
+		'The <main> element should be used for the primary content of your document only.'
+	),
+	section: __(
+		"The <section> element should represent a standalone portion of the document that can't be better represented by another element."
+	),
+	article: __(
+		'The <article> element should represent a self-contained, syndicatable portion of the document.'
+	),
+	aside: __(
+		"The <aside> element should represent a portion of a document whose content is only indirectly related to the document's main content."
+	),
+	footer: __(
+		'The <footer> element should represent a footer for its nearest sectioning element (e.g.: <section>, <article>, <main> etc.).'
+	),
+};
+
 export function TemplatePartAdvancedControls( {
 	tagName,
 	setAttributes,
@@ -47,27 +68,6 @@ export function TemplatePartAdvancedControls( {
 		label,
 		value: _area,
 	} ) );
-
-	const htmlElementMessages = {
-		header: __(
-			'The <header> element should represent introductory content, typically a group of introductory or navigational aids.'
-		),
-		main: __(
-			'The <main> element should be used for the primary content of your document only. '
-		),
-		section: __(
-			"The <section> element should represent a standalone portion of the document that can't be better represented by another element."
-		),
-		article: __(
-			'The <article> element should represent a self-contained, syndicatable portion of the document.'
-		),
-		aside: __(
-			"The <aside> element should represent a portion of a document whose content is only indirectly related to the document's main content."
-		),
-		footer: __(
-			'The <footer> element should represent a footer for its nearest sectioning element (e.g.: <section>, <article>, <main> etc.).'
-		),
-	};
 
 	return (
 		<InspectorControls group="advanced">
