8ea99fb1b90f963bd7c67c18d26c425b31d974d9
George Mamadashvili <georgemamadashvili@gmail.com>
2023-08-15 14:07:43 +0400

    Fix React hook warnings

--- packages/editor/src/components/unsaved-changes-warning/index.js
+++ packages/editor/src/components/unsaved-changes-warning/index.js
@@ -15,34 +15,34 @@ import { store as coreStore } from '@wordpress/core-data';
 export default function UnsavedChangesWarning() {
 	const { __experimentalGetDirtyEntityRecords } = useSelect( coreStore );
 
-	/**
-	 * Warns the user if there are unsaved changes before leaving the editor.
-	 *
-	 * @param {Event} event `beforeunload` event.
-	 *
-	 * @return {string | undefined} Warning prompt message, if unsaved changes exist.
-	 */
-	const warnIfUnsavedChanges = ( event ) => {
-		// We need to call the selector directly in the listener to avoid race
-		// conditions with `BrowserURL` where `componentDidUpdate` gets the
-		// new value of `isEditedPostDirty` before this component does,
-		// causing this component to incorrectly think a trashed post is still dirty.
-		const dirtyEntityRecords = __experimentalGetDirtyEntityRecords();
-		if ( dirtyEntityRecords.length > 0 ) {
-			event.returnValue = __(
-				'You have unsaved changes. If you proceed, they will be lost.'
-			);
-			return event.returnValue;
-		}
-	};
-
 	useEffect( () => {
+		/**
+		 * Warns the user if there are unsaved changes before leaving the editor.
+		 *
+		 * @param {Event} event `beforeunload` event.
+		 *
+		 * @return {string | undefined} Warning prompt message, if unsaved changes exist.
+		 */
+		const warnIfUnsavedChanges = ( event ) => {
+			// We need to call the selector directly in the listener to avoid race
+			// conditions with `BrowserURL` where `componentDidUpdate` gets the
+			// new value of `isEditedPostDirty` before this component does,
+			// causing this component to incorrectly think a trashed post is still dirty.
+			const dirtyEntityRecords = __experimentalGetDirtyEntityRecords();
+			if ( dirtyEntityRecords.length > 0 ) {
+				event.returnValue = __(
+					'You have unsaved changes. If you proceed, they will be lost.'
+				);
+				return event.returnValue;
+			}
+		};
+
 		window.addEventListener( 'beforeunload', warnIfUnsavedChanges );
 
 		return () => {
 			window.removeEventListener( 'beforeunload', warnIfUnsavedChanges );
 		};
-	}, [] );
+	}, [ __experimentalGetDirtyEntityRecords ] );
 
 	return null;
 }
