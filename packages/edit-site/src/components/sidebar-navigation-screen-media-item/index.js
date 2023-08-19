9e88f386ef76ed6b92bd6bfa60679f147ce8fd60
ramon <ramonjd@gmail.com>
2023-08-19 16:44:03 +1200

    MORE DEETS YO

--- packages/edit-site/src/components/sidebar-navigation-screen-media-item/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-media-item/index.js
@@ -1,11 +1,17 @@
 /**
  * WordPress dependencies
  */
-import { __experimentalUseNavigator as useNavigator } from '@wordpress/components';
+import {
+	Button,
+	__experimentalUseNavigator as useNavigator,
+} from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { store as coreStore } from '@wordpress/core-data';
 import { decodeEntities } from '@wordpress/html-entities';
+import { humanTimeDiff } from '@wordpress/date';
+import { createInterpolateElement } from '@wordpress/element';
+import { download } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -17,6 +23,57 @@ import {
 	SidebarNavigationScreenDetailsPanelLabel,
 	SidebarNavigationScreenDetailsPanelValue,
 } from '../sidebar-navigation-screen-details-panel';
+
+function getMediaDetails( record ) {
+	if ( ! record ) {
+		return [];
+	}
+
+	const details = [];
+
+	if ( record?.mime_type ) {
+		details.push( {
+			label: __( 'File type' ),
+			value: record.mime_type,
+		} );
+	}
+
+	if ( record?.modified ) {
+		details.push( {
+			label: __( 'Last modified' ),
+			value: createInterpolateElement(
+				sprintf(
+					/* translators: %s: is the relative time when the post was last modified. */
+					__( '<time>%s</time>' ),
+					humanTimeDiff( record.modified )
+				),
+				{
+					time: <time dateTime={ record.modified } />,
+				}
+			),
+		} );
+	}
+
+	if ( record?.media_details ) {
+		const { width, height, filesize } = record.media_details;
+
+		if ( width && height ) {
+			details.push( {
+				label: __( 'Dimensions (px)' ),
+				value: `${ width } × ${ height }`,
+			} );
+		}
+
+		if ( filesize ) {
+			details.push( {
+				label: __( 'File size' ),
+				value: `${ filesize / 1000 } kb`,
+			} );
+		}
+	}
+
+	return details;
+}
 
 export default function SidebarNavigationScreenMediaItem() {
 	const { params } = useNavigator();
@@ -47,17 +104,28 @@ export default function SidebarNavigationScreenMediaItem() {
 					spacing={ 5 }
 					title={ __( 'Details' ) }
 				>
-					<SidebarNavigationScreenDetailsPanelRow>
-						<SidebarNavigationScreenDetailsPanelLabel>
-							Something
-						</SidebarNavigationScreenDetailsPanelLabel>
-						<SidebarNavigationScreenDetailsPanelValue>
-							Yep
-						</SidebarNavigationScreenDetailsPanelValue>
-					</SidebarNavigationScreenDetailsPanelRow>
+					{ getMediaDetails( record ).map( ( { label, value } ) => (
+						<SidebarNavigationScreenDetailsPanelRow key={ label }>
+							<SidebarNavigationScreenDetailsPanelLabel>
+								{ label }
+							</SidebarNavigationScreenDetailsPanelLabel>
+							<SidebarNavigationScreenDetailsPanelValue>
+								{ value }
+							</SidebarNavigationScreenDetailsPanelValue>
+						</SidebarNavigationScreenDetailsPanelRow>
+					) ) }
 				</SidebarNavigationScreenDetailsPanel>
 			}
-			footer={ __( 'Media Item Footer' ) }
+			footer={
+				<Button
+					className="edit-site-sidebar__media-item-download-button"
+					href={ record?.source_url }
+					download
+					icon={ download }
+				>
+					{ __( 'Download file' ) }
+				</Button>
+			}
 		/>
 	);
 }
