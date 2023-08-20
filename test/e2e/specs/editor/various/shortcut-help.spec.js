3940f79234e83ef619facfb139bc1e9cfce90531
Mitchell Austin <mr.fye@oneandthesame.net>
2023-08-20 10:19:10 -0700

    Assert focus return

--- test/e2e/specs/editor/various/shortcut-help.spec.js
+++ test/e2e/specs/editor/various/shortcut-help.spec.js
@@ -8,21 +8,24 @@ test.describe( 'keyboard shortcut help modal', () => {
 		await admin.createNewPost();
 	} );
 
-	test( 'opens from the options menu and closes with its close button', async ( {
+	test( 'opens from the options menu, closes with its close button and returns focus', async ( {
 		page,
 	} ) => {
 		await page
 			.locator( 'role=region[name="Editor top bar"]' )
 			.locator( '[aria-label="Options"]' )
 			.click();
-		await page
-			.locator( 'role=menuitem', { hasText: /^Keyboard shortcuts/i } )
-			.click();
+		const menuItem = page.locator( 'role=menuitem', {
+			hasText: /^Keyboard shortcuts/i,
+		} );
 		const dialog = page.locator( 'role=dialog[name="Keyboard shortcuts"]' );
+
+		await menuItem.click();
 		await expect( dialog ).toBeVisible();
 
 		await page.locator( 'role=button[name="Close"]' ).click();
 		await expect( dialog ).toBeHidden();
+		await expect( menuItem ).toBeFocused();
 	} );
 
 	test( 'toggles open/closed using the keyboard shortcut (access+h)', async ( {
