b0017ce360c6a5867969bbd6ca205795337a8b8a
Luis Herranz <luisherranz@gmail.com>
2023-08-21 11:59:37 +0200

    Replace key with data-wp-key

--- packages/e2e-tests/plugins/interactive-blocks/directive-key/view.js
+++ packages/e2e-tests/plugins/interactive-blocks/directive-key/view.js
@@ -1,7 +1,7 @@
 ( ( { wp } ) => {
 	const { store, navigate } = wp.interactivity;
 
-	const add = '<div data-wp-interactive data-wp-navigation-id="some-id"><ul><li key="id-1">1</li><li key="id-2" data-testid="second-item">2</li><li key="id-3">3</li></ul><button data-testid="add" data-wp-on--click="actions.add">Add</button></div>'
+	const add = '<div data-wp-interactive data-wp-navigation-id="some-id"><ul><li data-wp-key="id-1">1</li><li data-wp-key="id-2" data-testid="second-item">2</li><li data-wp-key="id-3">3</li></ul><button data-testid="add" data-wp-on--click="actions.add">Add</button></div>'
 
 	store( {
 		actions: {
