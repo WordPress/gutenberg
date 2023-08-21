782d4f117e649a8d7fdfd3a60b2544567d575c3f
Robert Anderson <robert@noisysocks.com>
2023-08-22 11:14:07 +1200

    Add doc comment

--- packages/edit-site/src/components/page-content-focus-manager/constants.js
+++ packages/edit-site/src/components/page-content-focus-manager/constants.js
@@ -1,3 +1,7 @@
+/**
+ * Block types that are considered to be page content. These are the only blocks
+ * editable when hasPageContentFocus() is true.
+ */
 export const PAGE_CONTENT_BLOCK_TYPES = [
 	'core/post-title',
 	'core/post-featured-image',
