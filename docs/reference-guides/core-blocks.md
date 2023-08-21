2791034d1feebf2dd9131f652e497071acc3d477
Luis Herranz <luisherranz@gmail.com>
2023-08-21 16:41:57 +0200

    Only add supports.interactivity and enqueue the view.js file if the "Enhanced Pagination" option is enabled

--- docs/reference-guides/core-blocks.md
+++ docs/reference-guides/core-blocks.md
@@ -664,7 +664,7 @@ An advanced block that allows displaying post types based on different query par
 
 -	**Name:** core/query
 -	**Category:** theme
--	**Supports:** align (full, wide), interactivity, layout, ~~html~~
+-	**Supports:** align (full, wide), layout, ~~html~~
 -	**Attributes:** enhancedPagination, namespace, query, queryId, tagName
 
 ## No results
--- packages/block-library/src/query/index.php
+++ packages/block-library/src/query/index.php
@@ -15,15 +15,36 @@
  *
  * @return string Returns the modified output of the query block.
  */
-function render_block_core_query( $attributes, $content ) {
+function render_block_core_query( $attributes, $content, $block ) {
+	$should_load_view_script = $attributes['enhancedPagination'];
+	$view_js_file            = 'wp-block-query-view';
+
 	if ( $attributes['enhancedPagination'] ) {
 		$p = new WP_HTML_Tag_Processor( $content );
 		if ( $p->next_tag() ) {
+			// Add the necessary directives.
 			$p->set_attribute( 'data-wp-interactive', true );
 			$p->set_attribute( 'data-wp-navigation-id', 'query-' . $attributes['queryId'] );
+			$content = $p->get_updated_html();
+
+			// Make the block as interactive.
+			$block->block_type->supports['interactivity'] = true;
 		}
-		$content = $p->get_updated_html();
 	}
+
+	if ( ! wp_script_is( $view_js_file ) ) {
+		$script_handles = $block->block_type->view_script_handles;
+
+		// If the script is not needed, and it is still in the `view_script_handles`, remove it.
+		if ( ! $should_load_view_script && in_array( $view_js_file, $script_handles, true ) ) {
+			$block->block_type->view_script_handles = array_diff( $script_handles, array( $view_js_file ) );
+		}
+		// If the script is needed, but it was previously removed, add it again.
+		if ( $should_load_view_script && ! in_array( $view_js_file, $script_handles, true ) ) {
+			$block->block_type->view_script_handles = array_merge( $script_handles, array( $view_js_file ) );
+		}
+	}
+
 	return $content;
 }
 
