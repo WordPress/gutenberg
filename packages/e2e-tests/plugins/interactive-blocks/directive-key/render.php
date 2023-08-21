c0676129418bbd9030af933a3f864918f4b96c35
Luis Herranz <luisherranz@gmail.com>
2023-08-21 11:59:37 +0200

    Fix test using key

--- packages/e2e-tests/plugins/interactive-blocks/directive-key/render.php
+++ packages/e2e-tests/plugins/interactive-blocks/directive-key/render.php
@@ -6,4 +6,4 @@
  */
 ?>
 
-<div data-wp-interactive data-wp-navigation-id="some-id"><ul><li data-testid="first-item">2</li><li>3</li></ul><button data-testid="add" data-wp-on--click="actions.add">Add</button></div>
+<div data-wp-interactive data-wp-navigation-id="some-id"><ul><li key="id-2" data-testid="first-item">2</li><li key="id-3">3</li></ul><button data-testid="add" data-wp-on--click="actions.add">Add</button></div>
