1a5824029490d927e5dd1db60282a06262a44e89
Tonya Mork <tonya.mork@automattic.com>
2023-08-22 15:00:34 -0500

    Font Face: Prepare for merge into Core (#53858)
    * Moves the Font Face files into the compat directory.
    * Adds `@since 6.4.0`.
    * Adds Core merge instructions.
    
    Note: Leaves the BC Layer files in experimental, as these are for only the plugin.

--- lib/compat/wordpress-6.4/fonts/fonts.php
+++ lib/compat/wordpress-6.4/fonts/fonts.php
@@ -4,17 +4,19 @@
  *
  * @package    WordPress
  * @subpackage Fonts
- * @since      X.X.X
+ * @since      6.4.0
  */
 
-add_action( 'wp_head', 'wp_print_font_faces', 50 );
-add_action( 'admin_print_styles', 'wp_print_font_faces', 50 );
-
 if ( ! function_exists( 'wp_print_font_faces' ) ) {
+	// @core-merge: will merge into Core's `wp-includes/default-filters.php` file.
+	add_action( 'wp_head', 'wp_print_font_faces', 50 );
+	// @core-merge: will merge into Core's `wp-admin/includes/admin-filters.php.` file.
+	add_action( 'admin_print_styles', 'wp_print_font_faces', 50 );
+
 	/**
 	 * Generates and prints font-face styles for given fonts or theme.json fonts.
 	 *
-	 * @since X.X.X
+	 * @since 6.4.0
 	 *
 	 * @param array $fonts Optional. The fonts to generate and print @font-face styles.
 	 *                     Default empty array.
@@ -41,18 +43,20 @@ if ( ! function_exists( 'wp_print_font_faces' ) ) {
 
 		$wp_font_face->generate_and_print( $fonts );
 	}
+
+	// @core-merge: do not merge this code into Core.
+	add_filter(
+		'block_editor_settings_all',
+		static function( $settings ) {
+			ob_start();
+			// @core-merge: add only this line into Core's `_wp_get_iframed_editor_assets()` function after `wp_print_styles()`.
+			wp_print_font_faces();
+			$styles = ob_get_clean();
+
+			// Add the font-face styles to iframed editor assets.
+			$settings['__unstableResolvedAssets']['styles'] .= $styles;
+			return $settings;
+		},
+		11
+	);
 }
-
-add_filter(
-	'block_editor_settings_all',
-	static function( $settings ) {
-		ob_start();
-		wp_print_font_faces();
-		$styles = ob_get_clean();
-
-		// Add the font-face styles to iframed editor assets.
-		$settings['__unstableResolvedAssets']['styles'] .= $styles;
-		return $settings;
-	},
-	11
-);
