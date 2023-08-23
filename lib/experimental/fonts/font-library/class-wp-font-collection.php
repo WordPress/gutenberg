bdaacf8505a4f6f10c81b7923c3eb6dc0d9f5065
hellofromtonya <tonya.mork@automattic.com>
2023-08-23 12:13:11 -0500

    Improves contructor error handling.
    * Ensures each required param is of the right data type.
    * Improves each param check error to include expected data type.

--- lib/experimental/fonts/font-library/class-wp-font-collection.php
+++ lib/experimental/fonts/font-library/class-wp-font-collection.php
@@ -40,16 +40,16 @@ class WP_Font_Collection {
 	 */
 	public function __construct( $id, $config ) {
 
-		if ( empty( $id ) ) {
-			throw new Exception( 'Font Collection is missing the id.' );
+		if ( empty( $id ) && is_string( $id ) ) {
+			throw new Exception( 'Font Collection ID is required as a non-empty string.' );
 		}
 
 		if ( empty( $config ) ) {
-			throw new Exception( 'Font Collection is missing the config.' );
+			throw new Exception( 'Font Collection config options is required as a non-empty array.' );
 		}
 
-		if ( empty( $config['data_json_file'] ) ) {
-			throw new Exception( 'Font Collection is missing the data_json_file.' );
+		if ( empty( $config['data_json_file'] ) && is_string( $config['data_json_file']  ) ) {
+			throw new Exception( 'Font Collection config "data_json_file" option is required as a non-empty string.' );
 		}
 
 		$config['id'] = $id;
