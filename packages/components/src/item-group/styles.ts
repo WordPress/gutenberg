a03ca52eaeb843580897157bfaf1f427f66b55b4
Joe Fusco <joe.fusco@wpengine.com>
2023-08-24 15:07:43 -0400

    Soften (outline) button corners to match existing UI
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- packages/components/src/item-group/styles.ts
+++ packages/components/src/item-group/styles.ts
@@ -42,6 +42,7 @@ export const unstyledButton = ( as: 'a' | 'button' ) => {
 			// Windows high contrast mode.
 			outline: 2px solid transparent;
 			outline-offset: 0;
+			border-radius: 2px;
 		}
 	`;
 };
