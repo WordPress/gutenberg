57919960c02e5f3eb6746f9a260f2ad5e0b09883
Dave Smith <getdavemail@gmail.com>
2023-08-30 16:52:11 +0100

    Reinstate focus trap and focus first element

--- packages/format-library/src/link/inline.js
+++ packages/format-library/src/link/inline.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { useRef, createInterpolateElement } from '@wordpress/element';
+import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import { speak } from '@wordpress/a11y';
 import { Popover } from '@wordpress/components';
@@ -197,9 +197,9 @@ function InlineLinkUI( {
 	// See https://github.com/WordPress/gutenberg/pull/34742.
 	const forceRemountKey = useLinkInstanceKey( popoverAnchor );
 
-	// The focusOnMount prop shouldn't evolve during render of a Popover
-	// otherwise it causes a render of the content.
-	const focusOnMount = useRef( addingLink ? 'firstElement' : false );
+	// Ensure focus is set on the first element when the popover is opened.
+	// This also ensures a focus trap is active on the Popover.
+	const focusOnMountWithTrap = 'firstElement';
 
 	async function handleCreate( pageTitle ) {
 		const page = await createPageEntity( {
@@ -230,7 +230,7 @@ function InlineLinkUI( {
 	return (
 		<Popover
 			anchor={ popoverAnchor }
-			focusOnMount={ focusOnMount.current }
+			focusOnMount={ focusOnMountWithTrap }
 			onClose={ stopAddingLink }
 			onFocusOutside={ () => stopAddingLink( false ) }
 			placement="bottom"
