46f9324a906fc40aeae74463b7fc8f0a9b66f10a
Jerry Jones <jones.jeremydavid@gmail.com>
2023-09-01 15:47:42 -0500

    Handle Escape on Toolbar Option 2: Use addEventListener on the selectedblock toolbar, attached to the NavigableToolbar
    This method requires forwarding a ref from the editor level down to the navigable toolbar so that the escape unselect shortcut can be blocked and the navigable toolbar event listener will still fire.
    
    Blocking the global escape event shouldn't be necessary, but we have a combination of a few things all combining to create a situation where:
    - Because the block toolbar uses createPortal to populate the block toolbar fills, we can't rely on the React event bubbling to hit the onKeyDown listener for the block toolbar
    - Since we can't use the React tree, we use the DOM tree which _should_ handle the event bubbling correctly from a `createPortal` element.
    - This bubbles via the React tree, which hits this `unselect` escape keypress before the block toolbar DOM event listener has access to it.

--- packages/block-editor/src/components/block-tools/index.js
+++ packages/block-editor/src/components/block-tools/index.js
@@ -62,6 +62,8 @@ export default function BlockTools( {
 		moveBlocksDown,
 	} = useDispatch( blockEditorStore );
 
+	const selectedBlockToolsRef = useRef( null );
+
 	function onKeyDown( event ) {
 		if ( event.defaultPrevented ) return;
 
@@ -104,6 +106,15 @@ export default function BlockTools( {
 				insertBeforeBlock( clientIds[ 0 ] );
 			}
 		} else if ( isMatch( 'core/block-editor/unselect', event ) ) {
+			if ( selectedBlockToolsRef.current.contains( event.target ) ) {
+				// This shouldn't be necessary, but we have a combination of a few things all combining to create a situation where:
+				// - Because the block toolbar uses createPortal to populate the block toolbar fills, we can't rely on the React event bubbling to hit the onKeyDown listener for the block toolbar
+				// - Since we can't use the React tree, we use the DOM tree which _should_ handle the event bubbling correctly from a `createPortal` element.
+				// - This bubbles via the React tree, which hits this `unselect` escape keypress before the block toolbar DOM event listener has access to it.
+				// An alternative would be to remove the addEventListener on the navigableToolbar and use this event to handle it directly right here. That feels hacky too though.
+				return;
+			}
+
 			const clientIds = getSelectedBlockClientIds();
 			if ( clientIds.length ) {
 				event.preventDefault();
@@ -140,7 +151,10 @@ export default function BlockTools( {
 					__unstableContentRef={ __unstableContentRef }
 				/>
 				<Fill name="__experimentalSelectedBlockTools">
-					<SelectedBlockTools isFixed={ hasFixedToolbar } />
+					<SelectedBlockTools
+						ref={ selectedBlockToolsRef }
+						isFixed={ hasFixedToolbar }
+					/>
 				</Fill>
 				{ /* Used for the inline rich text toolbar. */ }
 				<Popover.Slot name="block-toolbar" ref={ blockToolbarRef } />
