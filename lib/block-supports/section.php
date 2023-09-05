05f88ce0788ae042b1450f06300b7e2869a92d87
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-09-05 17:50:12 +1000

    Add section block type styles to frontend

--- lib/block-supports/section.php
+++ lib/block-supports/section.php
@@ -69,10 +69,57 @@ function gutenberg_render_section_support( $block_content, $block ) {
 	return $tags->get_updated_html();
 }
 
+/**
+ * Render the section's styles.
+ *
+ * In the case of nested sections, we want the parent section's styles to be
+ * rendered before their descendants. This solves the issue of a block type
+ * being styled in both the parent and descendant: we want the descendant style
+ * to take priority, and this is done by loaded it after, in the DOM order.
+ *
+ * @param string|null $pre_render The pre-rendered content. Default null.
+ * @param array       $block      The block being rendered.
+ *
+ * @return null
+ */
+function gutenberg_render_section_support_styles( $pre_render, $block ) {
+	$section_block_styles = _wp_array_get( $block, array( 'attrs', 'style', 'blocks' ), null );
+
+	if ( ! $section_block_styles ) {
+		return null;
+	}
+
+	$class_name = gutenberg_get_section_class_name( $block );
+	$config     = array(
+		'version' => 2,
+		'styles'  => array( 'blocks' => $section_block_styles ),
+	);
+
+	$theme_json     = new WP_Theme_JSON_Gutenberg( $config, 'blocks' );
+	$section_styles = $theme_json->get_stylesheet(
+		array( 'styles' ),
+		array( 'custom' ),
+		array(
+			'skip_root_layout_styles' => true,
+			'scope'                   => ".$class_name",
+		)
+	);
+
+	if ( empty( $section_styles ) ) {
+		return null;
+	}
+
+	wp_register_style( 'section-styles', false );
+	wp_add_inline_style( 'section-styles', $section_styles );
+	wp_enqueue_style( 'section-styles' );
+}
+
+
 // Register the block support.
 WP_Block_Supports::get_instance()->register(
 	'section',
 	array( 'register_attribute' => 'gutenberg_register_section_support' )
 );
 
+add_filter( 'pre_render_block', 'gutenberg_render_section_support_styles', 10, 2 );
 add_filter( 'render_block', 'gutenberg_render_section_support', 10, 2 );
