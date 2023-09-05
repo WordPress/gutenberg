08f2e48c7d02ceb0d6e42a19a9723525a2df5f06
Matias Benedetto <matias.benedetto@gmail.com>
2023-09-05 10:35:47 -0300

    removing duplicated test files after merge with trunk

--- phpunit/tests/fonts/font-library/wpFontLibrary/getFontCollections.php
+++ phpunit/tests/fonts/font-library/wpFontLibrary/getFontCollections.php
@@ -14,19 +14,19 @@ class Tests_Fonts_WpFontLibrary_GetFontCollections extends WP_UnitTestCase {
 
 	public static function set_up_before_class() {
 		$my_font_collection_config = array(
-			'id'             => 'my-font-collection',
-			'name'           => 'My Font Collection',
-			'description'    => 'Demo about how to a font collection to your WordPress Font Library.',
-			'data_json_file' => path_join( __DIR__, 'my-font-collection-data.json' ),
+			'id'          => 'my-font-collection',
+			'name'        => 'My Font Collection',
+			'description' => 'Demo about how to a font collection to your WordPress Font Library.',
+			'src'         => path_join( __DIR__, 'my-font-collection-data.json' ),
 		);
 
 		wp_register_font_collection( $my_font_collection_config );
 
 		$another_font_collection_config = array(
-			'id'             => 'another-font-collection',
-			'name'           => 'Another Font Collection',
-			'description'    => 'Demo about how to a font collection to your WordPress Font Library.',
-			'data_json_file' => path_join( __DIR__, 'another-font-collection-data.json' ),
+			'id'          => 'another-font-collection',
+			'name'        => 'Another Font Collection',
+			'description' => 'Demo about how to a font collection to your WordPress Font Library.',
+			'src'         => path_join( __DIR__, 'another-font-collection-data.json' ),
 		);
 
 		wp_register_font_collection( $another_font_collection_config );
@@ -42,4 +42,5 @@ class Tests_Fonts_WpFontLibrary_GetFontCollections extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'another-font-collection', $font_collections, 'The array should have the key of the registered font collection id.' );
 		$this->assertInstanceOf( 'WP_Font_Collection', $font_collections['another-font-collection'], 'The value of the array $font_collections[id] should be an instance of WP_Font_Collection class.' );
 	}
+
 }
