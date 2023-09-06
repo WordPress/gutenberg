a2017c00c3cd36dea42ab5fb29679638dac3a080
Matias Benedetto <matias.benedetto@gmail.com>
2023-09-06 16:10:17 -0300

    fix merging font families functions

--- packages/edit-site/src/components/global-styles/font-library-modal/utils.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils.js
@@ -29,37 +29,39 @@ export function getFontFaceVariantName( face ) {
 	return `${ weightName } ${ styleName }`;
 }
 
-function mergeFontFaces ( existing, incoming ) {
+export function mergeFontFaces ( existing = [], incoming = [] ) {
 	const map = new Map();
 	for ( const face of existing ) {
 		map.set( `${ face.fontWeight }${ face.fontStyle }`, face );
 	}
 	for ( const face of incoming ) {
-		map.set( `${ face.fontWeight }${ face.fontStyle }`, face ); // This will overwrite if the src already exists, keeping it unique
+		// This will overwrite if the src already exists, keeping it unique.
+		map.set( `${ face.fontWeight }${ face.fontStyle }`, face ); 
 	}
 	return Array.from( map.values() );
 };
 
 export function mergeFontFamilies( existing = [], incoming = [] ) {
 	const map = new Map();
-
-	// Process the first array
+	// Add the existing array to the map.
 	for ( const font of existing ) {
 		map.set( font.slug, { ...font } );
 	}
-
-	// Process the second array
+	// Add the incoming array to the map, overwriting existing values excepting fontFace that need to be merged.
 	for ( const font of incoming ) {
 		if ( map.has( font.slug ) ) {
+			const { fontFace: incomingFontFaces, ...restIncoming } = font;
 			const existingFont = map.get( font.slug );
-			existingFont.fontFace = mergeFontFaces(
+			// Merge the fontFaces existing with the incoming fontFaces.
+			const mergedFontFaces = mergeFontFaces(
 				existingFont.fontFace,
-				font.fontFace
+				incomingFontFaces
 			);
+			// Except for the fontFace key all the other keys are overwritten with the incoming values.
+			map.set( font.slug, { ...restIncoming, fontFace: mergedFontFaces } );
 		} else {
 			map.set( font.slug, { ...font } );
 		}
 	}
-
 	return Array.from( map.values() );
 }
