0cc7e36d433276a6039ac7e82866cd126bce9ac9
Andrew Hayward <andrew.hayward@automattic.com>
2023-09-06 18:43:33 +0100

    Adding tests for `AlignmentMatrixControl`
    Making sure we have sufficient coverage to be confident that any changes don't break this component.

--- packages/components/src/alignment-matrix-control/test/index.tsx
+++ packages/components/src/alignment-matrix-control/test/index.tsx
@@ -24,27 +24,90 @@ describe( 'AlignmentMatrixControl', () => {
 
 			expect( getControl() ).toBeInTheDocument();
 		} );
+
+		it( 'should be centered by default', async () => {
+			const user = userEvent.setup();
+
+			render( <AlignmentMatrixControl /> );
+
+			await user.tab();
+			expect( getCell( 'center center' ) ).toHaveFocus();
+		} );
 	} );
 
-	describe( 'Change value', () => {
-		const alignments = [ 'center left', 'center center', 'bottom center' ];
-		const user = userEvent.setup();
+	describe( 'Should change value', () => {
+		describe( 'with Mouse', () => {
+			describe( 'on cell click', () => {
+				it.each( [
+					'top left',
+					'top center',
+					'top right',
+					'center left',
+					'center center',
+					'center right',
+					'bottom left',
+					'bottom center',
+					'bottom right',
+				] )( '%s', async ( alignment ) => {
+					const user = userEvent.setup();
+					const spy = jest.fn();
 
-		it.each( alignments )(
-			'should change value on %s cell click',
-			async ( alignment ) => {
-				const spy = jest.fn();
+					render(
+						<AlignmentMatrixControl
+							value="center"
+							onChange={ spy }
+						/>
+					);
 
-				render(
-					<AlignmentMatrixControl value="center" onChange={ spy } />
-				);
+					await user.click( getCell( alignment ) );
 
-				await user.click( getCell( alignment ) );
+					expect( getCell( alignment ) ).toHaveFocus();
 
-				expect( getCell( alignment ) ).toHaveFocus();
+					expect( spy ).toHaveBeenCalledWith( alignment );
+				} );
+			} );
+		} );
 
-				expect( spy ).toHaveBeenCalledWith( alignment );
-			}
-		);
+		describe( 'with Keyboard', () => {
+			describe( 'on arrow press', () => {
+				it.each( [
+					[ 'ArrowUp', 'top center' ],
+					[ 'ArrowLeft', 'center left' ],
+					[ 'ArrowDown', 'bottom center' ],
+					[ 'ArrowRight', 'center right' ],
+				] )( '%s', async ( keyRef, cellRef ) => {
+					const user = userEvent.setup();
+					const spy = jest.fn();
+
+					render( <AlignmentMatrixControl onChange={ spy } /> );
+
+					await user.tab();
+					await user.keyboard( `[${ keyRef }]` );
+					expect( getCell( cellRef ) ).toHaveFocus();
+					expect( spy ).toHaveBeenCalledWith( cellRef );
+				} );
+			} );
+
+			describe( 'but not at at edge', () => {
+				it.each( [
+					[ 'ArrowUp', 'top left' ],
+					[ 'ArrowLeft', 'top left' ],
+					[ 'ArrowDown', 'bottom right' ],
+					[ 'ArrowRight', 'bottom right' ],
+				] )( '%s', async ( keyRef, cellRef ) => {
+					const user = userEvent.setup();
+					const spy = jest.fn();
+
+					render( <AlignmentMatrixControl onChange={ spy } /> );
+
+					const cell = getCell( cellRef );
+					await user.click( cell );
+
+					await user.keyboard( `[${ keyRef }]` );
+					expect( cell ).toHaveFocus();
+					expect( spy ).toHaveBeenCalledWith( cellRef );
+				} );
+			} );
+		} );
 	} );
 } );
