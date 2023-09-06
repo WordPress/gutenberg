6fcb6de927453ae4f091d9449828a037898c6bc3
BaNgan <bangank36@gmail.com>
2023-09-06 08:36:08 +0000

    Fix workflow_run issue

--- packages/project-management-automation/lib/tasks/pr-preview-link/index.js
+++ packages/project-management-automation/lib/tasks/pr-preview-link/index.js
@@ -18,12 +18,12 @@ async function prPreviewLink( payload, octokit ) {
 	const owner = payload.repository.owner.login;
 	const pullRequestNumber = payload.pull_request.number;
 
-	const workflowRun = payload?.workflow_run
-    const repoHtmlUrl = payload?.repository?.html_url
-    const checkSuiteNumber = workflowRun?.check_suite_id
-    const artifactsUrl = workflowRun?.artifacts_url
+	// const workflowRun = payload?.workflow_run
+    // const repoHtmlUrl = payload?.repository?.html_url
+    // const checkSuiteNumber = workflowRun?.check_suite_id
+    // const artifactsUrl = workflowRun?.artifacts_url
 
-	debug( JSON.stringify( {workflowRun, repoHtmlUrl, checkSuiteNumber, artifactsUrl}) );
+	// debug( JSON.stringify( {workflowRun, repoHtmlUrl, checkSuiteNumber, artifactsUrl}) );
 	
 	debug( 'artifacts: detail data.' );
 	// Retrieve artifacts for a specific workflow run
