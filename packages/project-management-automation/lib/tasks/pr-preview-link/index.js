7b47cd3895f27f1f05b69bd73f91d957f3b4967f
BaNgan <bangank36@gmail.com>
2023-09-07 04:08:54 +0000

    Fix number

--- packages/project-management-automation/lib/tasks/pr-preview-link/index.js
+++ packages/project-management-automation/lib/tasks/pr-preview-link/index.js
@@ -17,7 +17,7 @@ async function prPreviewLink( payload, octokit ) {
 	const repo = payload.repository.name;
 	const owner = payload.repository.owner.login;
 	const action = payload.action;
-	const pullRequestNumber = payload.workflow_run.pull_requests[0].head.number; // fake one payload.pull_request.number
+	const pullRequestNumber = payload.workflow_run.pull_requests[0].number; // fake one payload.pull_request.number
 
 	if (action === 'in_progress') {
 		await octokit.rest.issues.createComment( {
