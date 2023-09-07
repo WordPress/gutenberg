7b1f6e0966f75447da927acc34cc20b900955954
Dave Smith <getdavemail@gmail.com>
2023-09-07 13:34:43 +0100

    Update Core implemenation for button

--- packages/block-library/src/button/edit.js
+++ packages/block-library/src/button/edit.js
@@ -311,7 +311,7 @@ function ButtonEdit( props ) {
 						richTextRef.current?.focus();
 					} }
 					anchor={ popoverAnchor }
-					focusOnMount={ isEditingURL ? 'firstElement' : false }
+					focusOnMount={ 'firstElement' }
 					__unstableSlotName={ '__unstable-block-tools-after' }
 					shift
 				>
