f2f228f8f53439eb3320a7b1f4e1b931781124e3
Sarah Norris <sarah@sekai.co.uk>
2023-09-08 12:17:53 +0100

    Refactor e2e test order

--- test/e2e/specs/site-editor/font-library.spec.js
+++ test/e2e/specs/site-editor/font-library.spec.js
@@ -4,69 +4,71 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'Font Library', () => {
-	test.beforeAll( async ( { requestUtils } ) => {
-		await requestUtils.activateTheme( 'twentytwentythree' );
-	} );
-
-	test.beforeEach( async ( { admin, editor } ) => {
-		await admin.visitSiteEditor( {
-			postId: 'twentytwentythree//index',
-			postType: 'wp_template',
+	test.describe( 'When a blank theme is active', () => {
+		test.beforeAll( async ( { requestUtils } ) => {
+			await requestUtils.activateTheme( 'emptytheme' );
 		} );
-		await editor.canvas.click( 'body' );
-	} );
 
-	test( 'should display the "Manage Fonts" icon when a blank theme is active', async ( {
-		admin,
-		editor,
-		page,
-		requestUtils,
-	} ) => {
-		await requestUtils.activateTheme( 'emptytheme' );
-		await admin.visitSiteEditor( {
-			postId: 'emptytheme//index',
-			postType: 'wp_template',
+		test.beforeEach( async ( { admin, editor } ) => {
+			await admin.visitSiteEditor( {
+				postId: 'emptytheme//index',
+				postType: 'wp_template',
+			} );
+			await editor.canvas.click( 'body' );
 		} );
-		await editor.canvas.click( 'body' );
 
-		await page.getByRole( 'button', { name: /styles/i } ).click();
-		await page
-			.getByRole( 'button', { name: /typography styles/i } )
-			.click();
-		const manageFontsIcon = page.getByRole( 'button', {
-			name: /manage fonts/i,
-		} );
-		await expect( manageFontsIcon ).toBeVisible();
-	} );
-
-	test( 'should display the "Manage Fonts" icon when a theme with bundled fonts is active', async ( {
-		page,
-	} ) => {
-		await page.getByRole( 'button', { name: /styles/i } ).click();
-		await page
-			.getByRole( 'button', { name: /typography styles/i } )
-			.click();
-		const manageFontsIcon = page.getByRole( 'button', {
-			name: /manage fonts/i,
-		} );
-		await expect( manageFontsIcon ).toBeVisible();
-	} );
-
-	test( 'should open the "Manage Fonts" modal when clicking the "Manage Fonts" icon', async ( {
-		page,
-	} ) => {
-		await page.getByRole( 'button', { name: /styles/i } ).click();
-		await page
-			.getByRole( 'button', { name: /typography styles/i } )
-			.click();
-		await page
-			.getByRole( 'button', {
+		test( 'should display the "Manage Fonts" icon', async ( { page } ) => {
+			await page.getByRole( 'button', { name: /styles/i } ).click();
+			await page
+				.getByRole( 'button', { name: /typography styles/i } )
+				.click();
+			const manageFontsIcon = page.getByRole( 'button', {
 				name: /manage fonts/i,
-			} )
-			.click();
-		await expect( page.getByRole( 'dialog' ) ).toBeVisible();
-		await expect(
-			page.getByRole( 'heading', { name: 'Fonts' } )
-		).toBeVisible();
+			} );
+			await expect( manageFontsIcon ).toBeVisible();
+		} );
+	} );
+
+	test.describe( 'When a theme with bundled fonts is active', () => {
+		test.beforeAll( async ( { requestUtils } ) => {
+			await requestUtils.activateTheme( 'twentytwentythree' );
+		} );
+
+		test.beforeEach( async ( { admin, editor } ) => {
+			await admin.visitSiteEditor( {
+				postId: 'twentytwentythree//index',
+				postType: 'wp_template',
+			} );
+			await editor.canvas.click( 'body' );
+		} );
+
+		test( 'should display the "Manage Fonts" icon', async ( { page } ) => {
+			await page.getByRole( 'button', { name: /styles/i } ).click();
+			await page
+				.getByRole( 'button', { name: /typography styles/i } )
+				.click();
+			const manageFontsIcon = page.getByRole( 'button', {
+				name: /manage fonts/i,
+			} );
+			await expect( manageFontsIcon ).toBeVisible();
+		} );
+
+		test( 'should open the "Manage Fonts" modal when clicking the "Manage Fonts" icon', async ( {
+			page,
+		} ) => {
+			await page.getByRole( 'button', { name: /styles/i } ).click();
+			await page
+				.getByRole( 'button', { name: /typography styles/i } )
+				.click();
+			await page
+				.getByRole( 'button', {
+					name: /manage fonts/i,
+				} )
+				.click();
+			await expect( page.getByRole( 'dialog' ) ).toBeVisible();
+			await expect(
+				page.getByRole( 'heading', { name: 'Fonts' } )
+			).toBeVisible();
+		} );
 	} );
 } );
