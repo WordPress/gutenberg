69295b2827e62a5cb77596c7a5e0ebc871f9d203
David Arenas <david.arenas@automattic.com>
2023-09-11 12:05:01 +0200

    Add modal to disable enhance pagination

---
+++ packages/block-library/src/query/edit/enhanced-pagination-modal.js
@@ -0,0 +1,66 @@
+/**
+ * WordPress dependencies
+ */
+import { useDispatch } from '@wordpress/data';
+import {
+	Button,
+	Modal,
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useContainsThirdPartyBlocks } from '../utils';
+
+const disableEnhancedPaginationDescription = __(
+	'Third-party blocks are not supported inside a Query Loop block with "Enhanced Pagination" enabled.'
+);
+
+const modalDescriptionId =
+	'wp-block-query-enhanced-pagination-modal__description';
+
+export default function EnhancedPaginationModal( {
+	clientId,
+	attributes: { enhancedPagination },
+	setAttributes,
+} ) {
+	const containsThirdPartyBlocks = useContainsThirdPartyBlocks( clientId );
+
+	// eslint-disable-next-line @wordpress/data-no-store-string-literals
+	const { undo } = useDispatch( 'core/editor' );
+
+	return (
+		containsThirdPartyBlocks &&
+		enhancedPagination && (
+			<Modal
+				title={ __( 'Disable "Enhanced Pagination"?' ) }
+				className={ 'wp-block-query-enhanced-pagination-modal' }
+				aria={ {
+					describedby: modalDescriptionId,
+				} }
+				isDismissible={ false }
+				shouldCloseOnEsc={ false }
+				shouldCloseOnClickOutside={ false }
+			>
+				<p id={ modalDescriptionId }>
+					{ disableEnhancedPaginationDescription }
+				</p>
+				<VStack alignment="right" spacing={ 3 }>
+					<Button
+						variant="primary"
+						onClick={ () => {
+							setAttributes( { enhancedPagination: false } );
+						} }
+					>
+						{ __( 'Disable it and keep third-party blocks' ) }
+					</Button>
+					<Button variant="tertiary" onClick={ undo }>
+						{ __( 'Undo changes' ) }
+					</Button>
+				</VStack>
+			</Modal>
+		)
+	);
+}
