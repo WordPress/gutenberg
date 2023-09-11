d68c59661ee9d95f4402a5472bf9d4440e5a85e4
Adam Silverstein <adamjs@google.com>
2023-09-10 22:37:06 -0600

    Fix overwriting of published post meta when previewing footnote changes

--- packages/block-library/src/footnotes/index.php
+++ packages/block-library/src/footnotes/index.php
@@ -242,7 +242,8 @@ function _wp_rest_api_autosave_meta( $autosave ) {
 		return;
 	}
 
-	update_post_meta( $id, 'footnotes', wp_slash( $body['meta']['footnotes'] ) );
+	// Can't use update_post_meta() because it doesn't allow revisions.
+	update_metadata( 'post', $id, 'footnotes', wp_slash( $body['meta']['footnotes'] ) );
 }
 // See https://github.com/WordPress/wordpress-develop/blob/2103cb9966e57d452c94218bbc3171579b536a40/src/wp-includes/rest-api/endpoints/class-wp-rest-autosaves-controller.php#L391C1-L391C1.
 add_action( 'wp_creating_autosave', '_wp_rest_api_autosave_meta' );
