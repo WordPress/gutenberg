49c584dd912f03b7736e764ba866cdcc5d8f5b38
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-09-12 13:20:19 +1000

    Remove unnecessary comment

--- packages/block-library/src/block/save.js
+++ packages/block-library/src/block/save.js
@@ -4,8 +4,5 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
 export default function save() {
-	// We'll probably save alignment class here soon.
-	const blockProps = useBlockProps.save();
-
-	return <div { ...blockProps } />;
+	return <div { ...useBlockProps.save() } />;
 }
