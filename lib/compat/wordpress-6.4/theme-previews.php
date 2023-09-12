0fbdccde43a20a3c51c0e9f4af418961bec83835
okmttdhr <okmttdhr@users.noreply.github.com>
2023-09-12 14:43:51 +0900

    Add a comment about a filter

--- lib/compat/wordpress-6.4/theme-previews.php
+++ lib/compat/wordpress-6.4/theme-previews.php
@@ -11,13 +11,16 @@
  * @param array $settings The editor settings.
  * @return array The editor settings.
  */
-
 function gutenberg_theme_preview_block_editor_settings_all( $settings ) {
 	$settings['__experimentalDashboardLink'] = 'themes.php';
 	return $settings;
 }
 
-// Attaches filters to enable theme previews in the Site Editor.
+/**
+ * Attaches filters to enable theme previews in the Site Editor.
+ * This would go inside of `initialize_theme_preview_hooks`
+ * to avoid the global scope when we port this to the core.
+ */
 if ( ! empty( $_GET['wp_theme_preview'] ) ) {
 	add_filter( 'block_editor_settings_all', 'gutenberg_theme_preview_block_editor_settings_all' );
 }
