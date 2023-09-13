6b70a41f12c101a272e5142f1fb59db59f51a138
Rich Tabor <hi@richtabor.com>
2023-09-13 17:50:52 -0400

    showTooltip false

--- packages/block-editor/src/components/block-styles/index.js
+++ packages/block-editor/src/components/block-styles/index.js
@@ -81,6 +81,7 @@ function BlockStyles( { clientId, onSwitch = noop, onHoverClassName = noop } ) {
 							key={ style.name }
 							variant="secondary"
 							label={ buttonText }
+							showTooltip={ false }
 							onMouseEnter={ () => styleItemHandler( style ) }
 							onFocus={ () => styleItemHandler( style ) }
 							onMouseLeave={ () => styleItemHandler( null ) }
