f41449d10babb15728b11951452cfb264b3a5665
Sarah Norris <sarah@sekai.co.uk>
2023-09-14 22:08:53 +0100

    Update CheckboxControl to allow for custom label

--- packages/components/src/checkbox-control/index.tsx
+++ packages/components/src/checkbox-control/index.tsx
@@ -125,12 +125,14 @@ export function CheckboxControl(
 					/>
 				) : null }
 			</span>
-			<label
-				className="components-checkbox-control__label"
-				htmlFor={ id }
-			>
-				{ label }
-			</label>
+			{ label !== false && (
+				<label
+					className="components-checkbox-control__label"
+					htmlFor={ id }
+				>
+					{ label }
+				</label>
+			) }
 		</BaseControl>
 	);
 }
