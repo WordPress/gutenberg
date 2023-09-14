a8c5605f5dd077a601aefce6f58409f54d7d4447
Ella <4710635+ellatrix@users.noreply.github.com>
2023-09-14 16:34:12 +0300

    Block editor: remove `__unstableElementContext` and filter `EditorStyles` instead (#52888)

--- packages/block-editor/src/store/private-actions.js
+++ packages/block-editor/src/store/private-actions.js
@@ -275,3 +275,18 @@ export function setOpenedBlockSettingsMenu( clientId ) {
 		clientId,
 	};
 }
+
+export function setStyleOverride( id, style ) {
+	return {
+		type: 'SET_STYLE_OVERRIDE',
+		id,
+		style,
+	};
+}
+
+export function deleteStyleOverride( id ) {
+	return {
+		type: 'DELETE_STYLE_OVERRIDE',
+		id,
+	};
+}
