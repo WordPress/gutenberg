4d21a7edeaf88ea6848b34353164a0b8c92753f1
Jerry Jones <jones.jeremydavid@gmail.com>
2023-09-14 13:56:49 -0500

    Fix comment of getLastFocus method

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -3023,7 +3023,7 @@ export const isGroupable = createRegistrySelector(
 		}
 );
 
-/*
+/**
  * Returns the element of the last element that had focus when focus left the editor canvas.
  *
  * @param {Object} state Block editor state.
