87fbea640ef5863bc9e79318b7f958b4d836c5de
Carlos Garcia <fluiddot@gmail.com>
2023-09-15 19:28:24 +0200

    Update description of max depth exceeded warning based on UBE support

--- packages/block-editor/src/components/inner-blocks/warning-max-depth-exceeded.native.js
+++ packages/block-editor/src/components/inner-blocks/warning-max-depth-exceeded.native.js
@@ -17,6 +17,7 @@ import Warning from '../warning';
 import UnsupportedBlockDetails from '../unsupported-block-details';
 import { store as blockEditorStore } from '../../store';
 import { MAX_NESTING_DEPTH } from './constants';
+import useUnsupportedBlockEditor from '../use-unsupported-block-editor';
 
 const WarningMaxDepthExceeded = ( { clientId } ) => {
 	const [ showDetails, setShowDetails ] = useState( false );
@@ -33,6 +34,11 @@ const WarningMaxDepthExceeded = ( { clientId } ) => {
 	);
 	const { replaceBlocks } = useDispatch( blockEditorStore );
 
+	const {
+		isUnsupportedBlockEditorSupported,
+		canEnableUnsupportedBlockEditor,
+	} = useUnsupportedBlockEditor( clientId );
+
 	const onUngroup = () => {
 		if ( ! innerBlocks.length ) {
 			return;
@@ -41,11 +47,26 @@ const WarningMaxDepthExceeded = ( { clientId } ) => {
 		replaceBlocks( clientId, innerBlocks );
 	};
 
-	/* translators: Warning related to having blocks deeply nested. %d: The deepest nesting level. */
-	const descriptionFormat = __(
-		'Blocks nested deeper than %d levels may not render properly in the mobile editor. For this reason, we recommend flattening the content by ungrouping the block or editing the block using the web editor.'
-	);
-	const description = sprintf( descriptionFormat, MAX_NESTING_DEPTH );
+	let description;
+	// When UBE can't be used, the description mentions using the web browser to edit the block.
+	if (
+		! isUnsupportedBlockEditorSupported &&
+		! canEnableUnsupportedBlockEditor
+	) {
+		/* translators: Warning related to having blocks deeply nested. %d: The deepest nesting level. */
+		const descriptionFormat = __(
+			'Blocks nested deeper than %d levels may not render properly in the mobile editor. For this reason, we recommend flattening the content by ungrouping the block or editing the block using your web browser.'
+		);
+		description = sprintf( descriptionFormat, MAX_NESTING_DEPTH );
+	}
+	// Otherwise, the description mentions using the web editor (i.e. UBE).
+	else {
+		/* translators: Warning related to having blocks deeply nested. %d: The deepest nesting level. */
+		const descriptionFormat = __(
+			'Blocks nested deeper than %d levels may not render properly in the mobile editor. For this reason, we recommend flattening the content by ungrouping the block or editing the block using the web editor.'
+		);
+		description = sprintf( descriptionFormat, MAX_NESTING_DEPTH );
+	}
 
 	return (
 		<TouchableWithoutFeedback
