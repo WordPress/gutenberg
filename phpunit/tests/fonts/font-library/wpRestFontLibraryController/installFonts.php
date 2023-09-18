39bb1f9d26477d76cbedddf230b2cefda86f518e
Jeff Ong <jonger4@gmail.com>
2023-09-18 10:03:58 -0700

    WIP test.

--- phpunit/tests/fonts/font-library/wpRestFontLibraryController/installFonts.php
+++ phpunit/tests/fonts/font-library/wpRestFontLibraryController/installFonts.php
@@ -501,4 +501,29 @@ class Tests_Fonts_WPRESTFontLibraryController_InstallFonts extends WP_REST_Font_
 			),
 		);
 	}
+
+	/**
+	 * Tests that font faces with same font style and weight but different extensions, installed on separate requests, are merged.
+	 *
+	 * @dataProvider data_install_with_duplicate_font_faces
+	 * @param array $font_families Font families to install in theme.json format.
+	 * @param array $files         Font files to install.
+	 */
+	public function test_install_duplicate_fonts_separate_requests( $font_families, $files = array(), $expected_response ) {
+		$install_request    = new WP_REST_Request( 'POST', '/wp/v2/fonts' );
+		$font_families_json = json_encode( $font_families );
+		$install_request->set_param( 'fontFamilies', $font_families_json );
+		$install_request->set_file_params( $files );
+
+		$response = rest_get_server()->dispatch( $install_request );
+		$this->assertSame( 200, $response->get_status(), 'The response status is not 200.' );
+		$data = $response->get_data();
+
+		// Install the same font family again.
+		$response = rest_get_server()->dispatch( $install_request );
+		$this->assertSame( 200, $response->get_status(), 'The response status is not 200.' );
+		$data = $response->get_data();
+		$this->assertCount( 1, $expected_response[0]['fontFace'], 'Duplicate font faces were not removed / added incorrectly.' );
+		$this->assertStringEndsWith( $expected_response[0]['fontFace'][0]['src'], $data[0]['fontFace'][0]['src'], 'The src attribute does not match the expected font file.' );
+	}
 }
