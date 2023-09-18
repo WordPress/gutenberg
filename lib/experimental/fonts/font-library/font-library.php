7c77c89a43dc3e0ce3d3fcc3e47da1c3d9d7d84f
Matias Benedetto <matias.benedetto@gmail.com>
2023-09-18 12:05:38 -0300

    add warning comment
    Co-authored-by: Tonya Mork <tonya.mork@automattic.com>

--- lib/experimental/fonts/font-library/font-library.php
+++ lib/experimental/fonts/font-library/font-library.php
@@ -23,7 +23,7 @@ function gutenberg_init_font_library_routes() {
 	// @core-merge: This code will go into Core's `create_initial_post_types()`.
 	$args = array(
 		'public'       => false,
-		'_builtin'     => true,
+		'_builtin'     => true,  /* internal use only. don't use this when registering your own post type. */
 		'label'        => 'Font Library',
 		'show_in_rest' => true,
 	);
