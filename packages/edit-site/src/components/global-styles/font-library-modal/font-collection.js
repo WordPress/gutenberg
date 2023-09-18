5be02810bb6217a8a620c357eea0e72745184dd1
Matias Benedetto <matias.benedetto@gmail.com>
2023-09-18 16:11:39 -0300

    adding ui filters for font collections

--- packages/edit-site/src/components/global-styles/font-library-modal/font-collection.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/font-collection.js
@@ -1,16 +1,37 @@
 /**
  * WordPress dependencies
  */
-import { useContext, useEffect } from '@wordpress/element';
-import { Spinner } from '@wordpress/components';
+import { useContext, useEffect, useState, useMemo } from '@wordpress/element';
+import {
+	__experimentalSpacer as Spacer,
+	__experimentalInputControl as InputControl,
+	__experimentalText as Text,
+	SelectControl,
+	Spinner,
+	Icon,
+	FlexItem,
+	Flex,
+} from '@wordpress/components';
+import { debounce } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import { search, closeSmall } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import TabLayout from './tab-layout';
 import { FontLibraryContext } from './context';
+import FontsGrid from './fonts-grid';
+import FontCard from './font-card';
+import filterFonts from './utils/filter-fonts';
+
+const DEFAULT_CATEGORY = {
+	id: 'all',
+	name: __( 'All' ),
+};
 
 function FontCollection( { id } ) {
+	const [ filters, setFilters ] = useState( {} );
 	const { collections, getFontCollection } = useContext( FontLibraryContext );
 	const selectedCollection = collections.find(
 		( collection ) => collection.id === id
@@ -18,7 +39,39 @@ function FontCollection( { id } ) {
 
 	useEffect( () => {
 		getFontCollection( id );
+		resetFilters();
 	}, [ id, getFontCollection ] );
+
+	const collectionFonts = useMemo(
+		() => selectedCollection?.data?.fontFamilies ?? [],
+		[ selectedCollection ]
+	);
+	const collectionCategories = selectedCollection?.data?.categories ?? [];
+
+	const categories = [ DEFAULT_CATEGORY, ...collectionCategories ];
+
+	const fonts = useMemo(
+		() => filterFonts( collectionFonts, filters ),
+		[ collectionFonts, filters ]
+	);
+
+	const handleCategoryFilter = ( category ) => {
+		setFilters( { ...filters, category } );
+	};
+
+	const handleUpdateSearchInput = ( value ) => {
+		setFilters( { ...filters, search: value } );
+	};
+
+	const debouncedUpdateSearchInput = debounce( handleUpdateSearchInput, 300 );
+
+	const resetFilters = () => {
+		setFilters( {} );
+	};
+
+	const resetSearch = () => {
+		setFilters( { ...filters, search: '' } );
+	};
 
 	return (
 		<TabLayout
@@ -27,10 +80,65 @@ function FontCollection( { id } ) {
 		>
 			{ ! selectedCollection.data && <Spinner /> }
 
-			{ Array.isArray( selectedCollection?.data?.fontFamilies ) &&
-				selectedCollection.data.fontFamilies.map( ( font ) => (
-					<div key={ font.family }>{ font.name }</div>
+			<Flex>
+				<FlexItem>
+					<InputControl
+						value={ filters.search }
+						placeholder={ __( 'Font name…' ) }
+						label={ __( 'Search' ) }
+						onChange={ debouncedUpdateSearchInput }
+						prefix={ <Icon icon={ search } /> }
+						suffix={
+							filters?.search ? (
+								<Icon
+									icon={ closeSmall }
+									onClick={ resetSearch }
+								/>
+							) : null
+						}
+					/>
+				</FlexItem>
+				<FlexItem>
+					<SelectControl
+						label={ __( 'Category' ) }
+						value={ filters.category }
+						onChange={ handleCategoryFilter }
+					>
+						{ categories &&
+							categories.map( ( category ) => (
+								<option
+									value={ category.id }
+									key={ category.id }
+								>
+									{ category.name }
+								</option>
+							) ) }
+					</SelectControl>
+				</FlexItem>
+			</Flex>
+
+			<Spacer margin={ 4 } />
+
+			{ ! selectedCollection?.data?.fontFamilies && <Spinner /> }
+
+			{ !! selectedCollection?.data?.fontFamilies?.length &&
+				! fonts.length && (
+					<Text>
+						{ __(
+							'No fonts found. Try with a different seach term'
+						) }
+					</Text>
+				) }
+
+			<FontsGrid>
+				{ fonts.map( ( font ) => (
+					<FontCard
+						key={ font.slug }
+						font={ font }
+						onClick={ () => {} }
+					/>
 				) ) }
+			</FontsGrid>
 		</TabLayout>
 	);
 }
