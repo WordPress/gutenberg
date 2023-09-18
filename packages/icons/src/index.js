52e66f22323bf19a972ea00d88e9024085e3d73c
scruffian <ben@scruffian.com>
2023-09-18 12:44:24 +0100

    Add textHorizontal back

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -256,6 +256,7 @@ export { default as sidesRight } from './library/sides-right';
 export { default as sidesTop } from './library/sides-top';
 export { default as sidesVertical } from './library/sides-vertical';
 export { default as textColor } from './library/text-color';
+export { default as textHorizontal } from './library/text-horizontal';
 export { default as textHorizontalRTL } from './library/text-horizontal-rtl';
 export { default as textUpright } from './library/text-upright';
 export { default as textVertical } from './library/text-vertical';
