d2e54f94f0710d0ff1b8a233f0417d7e5dca1be3
Dave Smith <getdavemail@gmail.com>
2023-09-19 13:49:27 +0100

    Make e2e test locator less strict

--- test/e2e/specs/editor/blocks/navigation-list-view.spec.js
+++ test/e2e/specs/editor/blocks/navigation-list-view.spec.js
@@ -274,7 +274,7 @@ test.describe( 'Navigation block - List view editing', () => {
 					hasText: 'Block 2 of 2, Level 1', // proxy for filtering by description.
 				} )
 				.getByText( 'Top Level Item 2' )
-		).not.toBeVisible();
+		).toBeHidden();
 	} );
 
 	test( `can edit menu items`, async ( { page, editor, requestUtils } ) => {
@@ -525,7 +525,7 @@ test.describe( 'Navigation block - List view editing', () => {
 		// Check that despite being the last inserted block, the Link UI is not displayed
 		// in this scenario because it was not **just** inserted into the List View (i.e.
 		// we have unmounted the list view and then remounted it).
-		await expect( linkControl.getSearchInput() ).not.toBeVisible();
+		await expect( linkControl.getSearchInput() ).toBeHidden();
 	} );
 } );
 
@@ -573,9 +573,7 @@ class LinkControl {
 		await expect( result ).toBeVisible();
 
 		return result
-			.locator(
-				'.components-menu-item__info-wrapper .components-menu-item__item'
-			) // this is the only way to get the label text without the URL.
+			.locator( '.components-menu-item__item' ) // this is the only way to get the label text without the URL.
 			.innerText();
 	}
 }
