bdc3c2e8aa98b8a150fee7718000277f03354cf3
Carolina Nymark <hi@themesbycarolina.com>
2023-09-20 08:11:10 +0200

    Try to fix coding standard issues

--- lib/global-styles-and-settings.php
+++ lib/global-styles-and-settings.php
@@ -291,12 +291,20 @@ function gutenberg_get_theme_directory_pattern_slugs() {
 	return WP_Theme_JSON_Resolver_Gutenberg::get_theme_data( array(), array( 'with_supports' => false ) )->get_patterns();
 }
 
-function add_variation_class_to_body( $classes ) {
+/**
+ * Adds the `uses-style-variation` and `is-style-variation-name` classes to the body if a theme style variation is used.
+ *
+ * @since 6.4.0
+ *
+ * @param array $classes Classes for the body element.
+ * @return array Filtered classes for the body element.
+ */
+function gutenberg_add_variation_class_to_body( $classes ) {
 	$custom = wp_get_global_settings( array( 'custom' ) );
-	if ( isset($custom['variation']) && 'Default' !== $custom['variation'] ) {
+	if ( isset( $custom['variation'] ) && 'Default' !== $custom['variation'] ) {
 		$classes[] = 'uses-style-variation';
-		$classes[] = _wp_to_kebab_case('is style variation ' . $custom['variation']);
+		$classes[] = _wp_to_kebab_case( 'is style variation ' . $custom['variation'] );
 	}
 	return $classes;
 }
-add_filter( 'body_class','add_variation_class_to_body' );
+add_filter( 'body_class', 'gutenberg_add_variation_class_to_body' );
