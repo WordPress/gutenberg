4846744bd2f75b4fceb33d2f855e87d210040c3c
Luis Herranz <luisherranz@gmail.com>
2023-09-21 11:38:00 +0200

    Add missing global declaration in interactivity files (#54683)

--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -286,6 +286,8 @@ HTML;
  * Ensure that the view script has the `wp-interactivity` dependency.
  *
  * @since 6.4.0
+ *
+ * @global WP_Scripts $wp_scripts
  */
 function block_core_image_ensure_interactivity_dependency() {
 	global $wp_scripts;
