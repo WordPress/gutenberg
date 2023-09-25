e500adaeed2e0c199fa3eb1a8d0fe7ff23bdc429
Jarda Snajdr <jsnajdr@gmail.com>
2023-09-25 13:35:17 +0200

    Work around bug in EquivalentKeyMap delete method

--- packages/data/src/resolvers-cache-middleware.js
+++ packages/data/src/resolvers-cache-middleware.js
@@ -19,14 +19,20 @@ const createResolversCacheMiddleware =
 				return;
 			}
 			resolversByArgs.forEach( ( value, args ) => {
+				// Works around a bug in `EquivalentKeyMap` where `map.delete` merely sets an entry value
+				// to `undefined` and `map.forEach` then iterates also over these orphaned entries.
+				if ( value === undefined ) {
+					return;
+				}
+
 				// resolversByArgs is the map Map([ args ] => boolean) storing the cache resolution status for a given selector.
 				// If the value is "finished" or "error" it means this resolver has finished its resolution which means we need
 				// to invalidate it, if it's true it means it's inflight and the invalidation is not necessary.
-				if (
-					( value.status !== 'finished' &&
-						value.status !== 'error' ) ||
-					! resolver.shouldInvalidate( action, ...args )
-				) {
+				if ( value.status !== 'finished' && value.status !== 'error' ) {
+					return;
+				}
+
+				if ( ! resolver.shouldInvalidate( action, ...args ) ) {
 					return;
 				}
 
