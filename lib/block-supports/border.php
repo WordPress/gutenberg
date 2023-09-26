41572797eccc8efd61546c7c7d3319dbe239f4e5
Ari Stathopoulos <aristath@gmail.com>
2023-09-26 09:51:23 +0300

    Revert "Move methods to their respective block-supports"
    This reverts commit 42b17dfa7ca895425fceafd55623d8a782a38225.

--- lib/block-supports/border.php
+++ lib/block-supports/border.php
@@ -154,60 +154,6 @@ function gutenberg_has_border_feature_support( $block_type, $feature, $default_v
 	return block_has_support( $block_type, array( '__experimentalBorder', $feature ), $default_value );
 }
 
-/**
- * Style value parser that returns a CSS definition array comprising style properties
- * that have keys representing individual style properties, otherwise known as longhand CSS properties.
- * e.g., "$style_property-$individual_feature: $value;", which could represent the following:
- * "border-{top|right|bottom|left}-{color|width|style}: {value};" or,
- * "border-image-{outset|source|width|repeat|slice}: {value};"
- *
- * @param array $style_value                    A single raw style value from $block_styles array.
- * @param array $individual_property_definition A single style definition from static::$block_style_definition_metadata,
- *                                              representing an individual property of a CSS property, e.g., 'top' in 'border-top'.
- * @param array $options                        {
- *     Optional. An array of options. Default empty array.
- *
- *     @type bool $convert_vars_to_classnames Whether to skip converting incoming CSS var patterns, e.g., `var:preset|<PRESET_TYPE>|<PRESET_SLUG>`, to var( --wp--preset--* ) values. Default `false`.
- * }
- *
- * @return string[] An associative array of CSS definitions, e.g., array( "$property" => "$value", "$property" => "$value" ).
- */
-function _gutenberg_block_supports_get_individual_property_css_declarations( $style_value, $individual_property_definition, $options = array() ) {
-	if ( ! is_array( $style_value ) || empty( $style_value ) || empty( $individual_property_definition['path'] ) ) {
-		return array();
-	}
-
-	/*
-		* The first item in $individual_property_definition['path'] array tells us the style property, e.g., "border".
-		* We use this to get a corresponding CSS style definition such as "color" or "width" from the same group.
-		* The second item in $individual_property_definition['path'] array refers to the individual property marker, e.g., "top".
-		*/
-	$definition_group_key    = $individual_property_definition['path'][0];
-	$individual_property_key = $individual_property_definition['path'][1];
-	$should_skip_css_vars    = isset( $options['convert_vars_to_classnames'] ) && true === $options['convert_vars_to_classnames'];
-	$css_declarations        = array();
-
-	foreach ( $style_value as $css_property => $value ) {
-		if ( empty( $value ) ) {
-			continue;
-		}
-
-		// Build a path to the individual rules in definitions.
-		$style_definition_path = array( $definition_group_key, $css_property );
-		$style_definition      = _wp_array_get( WP_Style_Engine_Gutenberg::$block_style_definition_metadata, $style_definition_path, null );
-
-		if ( $style_definition && isset( $style_definition['property_keys']['individual'] ) ) {
-			// Set a CSS var if there is a valid preset value.
-			if ( is_string( $value ) && str_contains( $value, 'var:' ) && ! $should_skip_css_vars && ! empty( $individual_property_definition['css_vars'] ) ) {
-				$value = WP_Style_Engine_Gutenberg::get_css_var_value( $value, $individual_property_definition['css_vars'] );
-			}
-			$individual_css_property                      = sprintf( $style_definition['property_keys']['individual'], $individual_property_key );
-			$css_declarations[ $individual_css_property ] = $value;
-		}
-	}
-	return $css_declarations;
-}
-
 // Register the block support.
 WP_Block_Supports::get_instance()->register(
 	'border',
@@ -253,28 +199,28 @@ WP_Style_Engine_Gutenberg::register_block_style_definitions_metadata(
 			'path'          => array( 'border', 'width' ),
 		),
 		'top'    => array(
-			'value_func' => '_gutenberg_block_supports_get_individual_property_css_declarations',
+			'value_func' => array( 'WP_Style_Engine_Gutenberg', 'get_individual_property_css_declarations' ),
 			'path'       => array( 'border', 'top' ),
 			'css_vars'   => array(
 				'color' => '--wp--preset--color--$slug',
 			),
 		),
 		'right'  => array(
-			'value_func' => '_gutenberg_block_supports_get_individual_property_css_declarations',
+			'value_func' => array( 'WP_Style_Engine_Gutenberg', 'get_individual_property_css_declarations' ),
 			'path'       => array( 'border', 'right' ),
 			'css_vars'   => array(
 				'color' => '--wp--preset--color--$slug',
 			),
 		),
 		'bottom' => array(
-			'value_func' => '_gutenberg_block_supports_get_individual_property_css_declarations',
+			'value_func' => array( 'WP_Style_Engine_Gutenberg', 'get_individual_property_css_declarations' ),
 			'path'       => array( 'border', 'bottom' ),
 			'css_vars'   => array(
 				'color' => '--wp--preset--color--$slug',
 			),
 		),
 		'left'   => array(
-			'value_func' => '_gutenberg_block_supports_get_individual_property_css_declarations',
+			'value_func' => array( 'WP_Style_Engine_Gutenberg', 'get_individual_property_css_declarations' ),
 			'path'       => array( 'border', 'left' ),
 			'css_vars'   => array(
 				'color' => '--wp--preset--color--$slug',
