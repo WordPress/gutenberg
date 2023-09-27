416c0a17b449ce7c3d82f0c923f8235987b9f9d9
brookewp <brooke.kaminski@automattic.com>
2023-09-26 17:24:15 -0700

    Remove context folder from ui

--- packages/components/src/ui/context/constants.js
+++
@@ -1,8 +0,0 @@
-export const COMPONENT_NAMESPACE = 'data-wp-component';
-export const CONNECTED_NAMESPACE = 'data-wp-c16t';
-
-/**
- * Special key where the connected namespaces are stored.
- * This is attached to Context connected components as a static property.
- */
-export const CONNECT_STATIC_NAMESPACE = '__contextSystemKey__';
