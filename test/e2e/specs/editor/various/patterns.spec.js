376542d603018640a79b5bc443d1d225ec463b82
Glen Davies <glendaviesnz@users.noreply.github.com>
2023-09-28 19:05:52 +1300

    Name the checkbox
    Co-authored-by: Kai Hao <kevin830726@gmail.com>

--- test/e2e/specs/editor/various/patterns.spec.js
+++ test/e2e/specs/editor/various/patterns.spec.js
@@ -32,7 +32,10 @@ test.describe( 'Unsynced pattern', () => {
 		await createPatternDialog
 			.getByRole( 'textbox', { name: 'Name' } )
 			.fill( 'My unsynced pattern' );
-		await createPatternDialog.getByRole( 'checkbox' ).setChecked( false );
+		await createPatternDialog.getByRole(
+			'checkbox',
+			{ name: 'Synced' }
+		).setChecked( false );
 
 		await page.keyboard.press( 'Enter' );
 
