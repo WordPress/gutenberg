e649d10bd032bfa5de1f1287cc22d572a93fe3f5
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-10-03 10:44:50 +0900

    Move code example section

--- packages/components/src/textarea-control/index.tsx
+++ packages/components/src/textarea-control/index.tsx
@@ -12,29 +12,6 @@ import { StyledTextarea } from './styles/textarea-control-styles';
 import type { TextareaControlProps } from './types';
 import type { WordPressComponentProps } from '../context';
 
-/**
- * TextareaControls are TextControls that allow for multiple lines of text, and
- * wrap overflow text onto a new line. They are a fixed height and scroll
- * vertically when the cursor reaches the bottom of the field.
- *
- * ```jsx
- * import { TextareaControl } from '@wordpress/components';
- * import { useState } from '@wordpress/element';
- *
- * const MyTextareaControl = () => {
- *   const [ text, setText ] = useState( '' );
- *
- *   return (
- *     <TextareaControl
- *       label="Text"
- *       help="Enter some text"
- *       value={ text }
- *       onChange={ ( value ) => setText( value ) }
- *     />
- *   );
- * };
- * ```
- */
 function UnforwardedTextareaControl(
 	props: WordPressComponentProps< TextareaControlProps, 'textarea', false >,
 	ref: React.ForwardedRef< HTMLTextAreaElement >
@@ -78,6 +55,29 @@ function UnforwardedTextareaControl(
 	);
 }
 
+/**
+ * TextareaControls are TextControls that allow for multiple lines of text, and
+ * wrap overflow text onto a new line. They are a fixed height and scroll
+ * vertically when the cursor reaches the bottom of the field.
+ *
+ * ```jsx
+ * import { TextareaControl } from '@wordpress/components';
+ * import { useState } from '@wordpress/element';
+ *
+ * const MyTextareaControl = () => {
+ *   const [ text, setText ] = useState( '' );
+ *
+ *   return (
+ *     <TextareaControl
+ *       label="Text"
+ *       help="Enter some text"
+ *       value={ text }
+ *       onChange={ ( value ) => setText( value ) }
+ *     />
+ *   );
+ * };
+ * ```
+ */
 export const TextareaControl = forwardRef( UnforwardedTextareaControl );
 
 export default TextareaControl;
