a2237d1862548aeddab746991bae248ac11a7d8a
Ella <ella@vandurpe.com>
2023-10-04 16:34:10 +0300

    Restore __unstableUseClipboardHandler

--- packages/block-editor/src/components/copy-handler/index.js
+++ packages/block-editor/src/components/copy-handler/index.js
@@ -1,10 +1,32 @@
 /**
+ * WordPress dependencies
+ */
+import deprecated from '@wordpress/deprecated';
+
+/**
  * Internal dependencies
  */
 import useClipboardHandler from '../writing-flow/use-clipboard-handler';
 
 /**
  * @deprecated
+ */
+export const __unstableUseClipboardHandler = () => {
+	deprecated( '__unstableUseClipboardHandler', {
+		alternative: 'BlockCanvas or WritingFlow',
+		version: '6.4',
+	} );
+	return useClipboardHandler();
+};
+
+/**
+ * @deprecated
  * @param {Object} props
  */
-export default ( props ) => <div { ...props } ref={ useClipboardHandler() } />;
+export default function CopyHandler( props ) {
+	deprecated( 'CopyHandler', {
+		alternative: 'BlockCanvas or WritingFlow',
+		version: '6.4',
+	} );
+	return <div { ...props } ref={ useClipboardHandler() } />;
+}
