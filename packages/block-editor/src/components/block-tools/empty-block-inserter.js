3001bcf43ca5c8dc13b9c9d46acc569a81a49a3e
Jerry Jones <jones.jeremydavid@gmail.com>
2023-10-04 11:00:42 -0500

    Refactor selectors on selected block popover and empty block inserter

--- packages/block-editor/src/components/block-tools/empty-block-inserter.js
+++ packages/block-editor/src/components/block-tools/empty-block-inserter.js
@@ -17,141 +17,124 @@ import BlockPopover from '../block-popover';
 import useBlockToolbarPopoverProps from './use-block-toolbar-popover-props';
 import Inserter from '../inserter';
 
-function selector( select ) {
-	const {
-		__unstableGetEditorMode,
-		hasMultiSelection,
-		isTyping,
-		getLastMultiSelectedBlockClientId,
-	} = select( blockEditorStore );
-
-	return {
-		editorMode: __unstableGetEditorMode(),
-		hasMultiSelection: hasMultiSelection(),
-		isTyping: isTyping(),
-		lastClientId: hasMultiSelection()
-			? getLastMultiSelectedBlockClientId()
-			: null,
-	};
-}
-
 function EmptyBlockInserter( {
 	clientId,
 	rootClientId,
-	isEmptyDefaultBlock,
+	lastClientId,
+	isInsertionPointVisible,
 	capturingClientId,
 	__unstablePopoverSlot,
 	__unstableContentRef,
 } ) {
-	const { editorMode, isTyping, lastClientId } = useSelect( selector, [] );
-
-	const isInsertionPointVisible = useSelect(
-		( select ) => {
-			const {
-				isBlockInsertionPointVisible,
-				getBlockInsertionPoint,
-				getBlockOrder,
-			} = select( blockEditorStore );
-
-			if ( ! isBlockInsertionPointVisible() ) {
-				return false;
-			}
-
-			const insertionPoint = getBlockInsertionPoint();
-			const order = getBlockOrder( insertionPoint.rootClientId );
-			return order[ insertionPoint.index ] === clientId;
-		},
-		[ clientId ]
-	);
-
-	const showEmptyBlockSideInserter =
-		! isTyping && editorMode === 'edit' && isEmptyDefaultBlock;
-
 	const popoverProps = useBlockToolbarPopoverProps( {
 		contentElement: __unstableContentRef?.current,
 		clientId,
 	} );
 
-	if ( showEmptyBlockSideInserter ) {
-		return (
-			<BlockPopover
-				clientId={ capturingClientId || clientId }
-				__unstableCoverTarget
-				bottomClientId={ lastClientId }
-				className={ classnames(
-					'block-editor-block-list__block-side-inserter-popover',
-					{
-						'is-insertion-point-visible': isInsertionPointVisible,
-					}
-				) }
-				__unstablePopoverSlot={ __unstablePopoverSlot }
-				__unstableContentRef={ __unstableContentRef }
-				resize={ false }
-				shift={ false }
-				{ ...popoverProps }
-			>
-				<div className="block-editor-block-list__empty-block-inserter">
-					<Inserter
-						position="bottom right"
-						rootClientId={ rootClientId }
-						clientId={ clientId }
-						__experimentalIsQuick
-					/>
-				</div>
-			</BlockPopover>
-		);
-	}
-
-	return null;
-}
-
-function wrapperSelector( select ) {
-	const {
-		getSelectedBlockClientId,
-		getFirstMultiSelectedBlockClientId,
-		getBlockRootClientId,
-		getBlock,
-		getBlockParents,
-		__experimentalGetBlockListSettingsForBlocks,
-	} = select( blockEditorStore );
-
-	const clientId =
-		getSelectedBlockClientId() || getFirstMultiSelectedBlockClientId();
-
-	if ( ! clientId ) {
-		return;
-	}
-
-	const { name, attributes = {} } = getBlock( clientId ) || {};
-	const blockParentsClientIds = getBlockParents( clientId );
-
-	// Get Block List Settings for all ancestors of the current Block clientId.
-	const parentBlockListSettings = __experimentalGetBlockListSettingsForBlocks(
-		blockParentsClientIds
+	return (
+		<BlockPopover
+			clientId={ capturingClientId || clientId }
+			__unstableCoverTarget
+			bottomClientId={ lastClientId }
+			className={ classnames(
+				'block-editor-block-list__block-side-inserter-popover',
+				{
+					'is-insertion-point-visible': isInsertionPointVisible,
+				}
+			) }
+			__unstablePopoverSlot={ __unstablePopoverSlot }
+			__unstableContentRef={ __unstableContentRef }
+			resize={ false }
+			shift={ false }
+			{ ...popoverProps }
+		>
+			<div className="block-editor-block-list__empty-block-inserter">
+				<Inserter
+					position="bottom right"
+					rootClientId={ rootClientId }
+					clientId={ clientId }
+					__experimentalIsQuick
+				/>
+			</div>
+		</BlockPopover>
 	);
-
-	// Get the clientId of the topmost parent with the capture toolbars setting.
-	const capturingClientId = blockParentsClientIds.find(
-		( parentClientId ) =>
-			parentBlockListSettings[ parentClientId ]
-				?.__experimentalCaptureToolbars
-	);
-
-	return {
-		clientId,
-		rootClientId: getBlockRootClientId( clientId ),
-		name,
-		isEmptyDefaultBlock:
-			name && isUnmodifiedDefaultBlock( { name, attributes } ),
-		capturingClientId,
-	};
 }
 
 export default function WrappedEmptyBlockInserter( {
 	__unstablePopoverSlot,
 	__unstableContentRef,
 } ) {
-	const selected = useSelect( wrapperSelector, [] );
+	const selected = useSelect( ( select ) => {
+		const {
+			getSelectedBlockClientId,
+			getFirstMultiSelectedBlockClientId,
+			getBlockRootClientId,
+			getBlock,
+			getBlockParents,
+			__experimentalGetBlockListSettingsForBlocks,
+			isBlockInsertionPointVisible,
+			getBlockInsertionPoint,
+			getBlockOrder,
+			__unstableGetEditorMode, // showEmptyBlockSideInserter start
+			hasMultiSelection,
+			isTyping,
+			getLastMultiSelectedBlockClientId, // showEmptyBlockSideInserter end
+		} = select( blockEditorStore );
+
+		const clientId =
+			getSelectedBlockClientId() || getFirstMultiSelectedBlockClientId();
+
+		if ( ! clientId ) {
+			return;
+		}
+
+		const { name, attributes = {} } = getBlock( clientId ) || {};
+		const blockParentsClientIds = getBlockParents( clientId );
+
+		// Get Block List Settings for all ancestors of the current Block clientId.
+		const parentBlockListSettings =
+			__experimentalGetBlockListSettingsForBlocks(
+				blockParentsClientIds
+			);
+
+		// Get the clientId of the topmost parent with the capture toolbars setting.
+		const capturingClientId = blockParentsClientIds.find(
+			( parentClientId ) =>
+				parentBlockListSettings[ parentClientId ]
+					?.__experimentalCaptureToolbars
+		);
+
+		let isInsertionPointVisible = false;
+
+		if ( isBlockInsertionPointVisible() ) {
+			const insertionPoint = getBlockInsertionPoint();
+			const order = getBlockOrder( insertionPoint.rootClientId );
+			isInsertionPointVisible =
+				order[ insertionPoint.index ] === clientId;
+		}
+
+		const isEmptyDefaultBlock =
+			name && isUnmodifiedDefaultBlock( { name, attributes } );
+		const editorMode = __unstableGetEditorMode();
+
+		return {
+			clientId,
+			rootClientId: getBlockRootClientId( clientId ),
+			name,
+			capturingClientId,
+			isInsertionPointVisible,
+			shouldShowBreadcrumb:
+				! hasMultiSelection() &&
+				( editorMode === 'navigation' || editorMode === 'zoom-out' ),
+			showEmptyBlockSideInserter:
+				! isTyping() &&
+				__unstableGetEditorMode() === 'edit' &&
+				isEmptyDefaultBlock,
+			lastClientId: hasMultiSelection()
+				? getLastMultiSelectedBlockClientId()
+				: null,
+		};
+	}, [] );
 
 	if ( ! selected ) {
 		return null;
@@ -161,11 +144,13 @@ export default function WrappedEmptyBlockInserter( {
 		clientId,
 		rootClientId,
 		name,
-		isEmptyDefaultBlock,
 		capturingClientId,
+		isInsertionPointVisible,
+		lastClientId,
+		showEmptyBlockSideInserter,
 	} = selected;
 
-	if ( ! name ) {
+	if ( ! name || ! showEmptyBlockSideInserter ) {
 		return null;
 	}
 
@@ -173,10 +158,11 @@ export default function WrappedEmptyBlockInserter( {
 		<EmptyBlockInserter
 			clientId={ clientId }
 			rootClientId={ rootClientId }
-			isEmptyDefaultBlock={ isEmptyDefaultBlock }
+			isInsertionPointVisible={ isInsertionPointVisible }
 			capturingClientId={ capturingClientId }
 			__unstablePopoverSlot={ __unstablePopoverSlot }
 			__unstableContentRef={ __unstableContentRef }
+			lastClientId={ lastClientId }
 		/>
 	);
 }
