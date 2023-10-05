8ac5f11d41308aadacd413ae9a520a29605f6bbe
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-10-05 17:02:30 +0200

    Make visitSiteEditor consistent with other utils

--- packages/e2e-test-utils-playwright/src/admin/visit-site-editor.ts
+++ packages/e2e-test-utils-playwright/src/admin/visit-site-editor.ts
@@ -8,62 +8,40 @@ import { addQueryArgs } from '@wordpress/url';
  */
 import type { Admin } from './';
 
-export interface SiteEditorQueryParams {
-	postId: string | number;
-	postType: string;
+interface SiteEditorOptions {
+	postId?: string | number;
+	postType?: string;
+	path?: string;
+	canvas?: string;
+	showWelcomeGuide?: boolean;
 }
 
-const CANVAS_SELECTOR = 'iframe[title="Editor canvas"i] >> visible=true';
-
 /**
- * Visits the Site Editor main page
- *
- * By default, it also skips the welcome guide. The option can be disabled if need be.
+ * Visits the Site Editor main page.
  *
  * @param this
- * @param query            Query params to be serialized as query portion of URL.
- * @param skipWelcomeGuide Whether to skip the welcome guide as part of the navigation.
+ * @param options Options to visit the site editor.
  */
 export async function visitSiteEditor(
 	this: Admin,
-	query: SiteEditorQueryParams,
-	skipWelcomeGuide = true
+	options: SiteEditorOptions = {}
 ) {
-	const path = addQueryArgs( '', {
-		...query,
+	const query = addQueryArgs( '', {
+		postId: options.postId,
+		postType: options.postType,
+		path: options.path,
+		canvas: options.canvas,
 	} ).slice( 1 );
 
-	await this.visitAdminPage( 'site-editor.php', path );
+	await this.visitAdminPage( 'site-editor.php', query );
 
-	if ( skipWelcomeGuide ) {
-		await this.page.evaluate( () => {
-			window.wp.data
-				.dispatch( 'core/preferences' )
-				.set( 'core/edit-site', 'welcomeGuide', false );
-
-			window.wp.data
-				.dispatch( 'core/preferences' )
-				.set( 'core/edit-site', 'welcomeGuideStyles', false );
-
-			window.wp.data
-				.dispatch( 'core/preferences' )
-				.set( 'core/edit-site', 'welcomeGuidePage', false );
-
-			window.wp.data
-				.dispatch( 'core/preferences' )
-				.set( 'core/edit-site', 'welcomeGuideTemplate', false );
+	if ( ! options.showWelcomeGuide ) {
+		await this.editor.setPreferences( 'core/edit-site', {
+			welcomeGuide: false,
+			welcomeGuideStyles: false,
+			welcomeGuidePage: false,
+			welcomeGuideTemplate: false,
 		} );
-	}
-
-	// Check if the current page has an editor canvas first.
-	if ( ( await this.page.locator( CANVAS_SELECTOR ).count() ) > 0 ) {
-		// The site editor initially loads with an empty body,
-		// we need to wait for the editor canvas to be rendered.
-		await this.page
-			.frameLocator( CANVAS_SELECTOR )
-			.locator( 'body > *' )
-			.first()
-			.waitFor();
 	}
 
 	// TODO: Ideally the content underneath the canvas loader should be marked inert until it's ready.
