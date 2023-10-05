d2167e75c6c6d09d287535892346bd0f7f0d671a
Jerry Jones <jones.jeremydavid@gmail.com>
2023-10-05 15:23:50 -0500

    Block tools should be behind site editor header

--- packages/edit-post/src/components/header/index.js
+++ packages/edit-post/src/components/header/index.js
@@ -51,6 +51,7 @@ function Header( {
 
 	return (
 		<div role="menubar" className="edit-post-header">
+			{ /* Floating header style to be able to hide the block popovers behind the header */ }
 			<div className="edit-post-header__background-style"></div>
 			<MainDashboardButton.Slot>
 				<motion.div
