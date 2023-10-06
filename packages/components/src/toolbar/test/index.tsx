b8abb3fd11ef4f89378134c4464912eb720bb61b
brookewp <brooke.kaminski@automattic.com>
2023-10-06 11:00:05 -0700

    Add test for new variant

--- packages/components/src/toolbar/test/index.tsx
+++ packages/components/src/toolbar/test/index.tsx
@@ -25,5 +25,13 @@ describe( 'Toolbar', () => {
 				screen.getByLabelText( 'control2', { selector: 'button' } )
 			).toBeInTheDocument();
 		} );
+
+		it( 'should render a toolbar without styles if variant has been defined', () => {
+			render( <Toolbar label="blocks" variant="unstyled" /> );
+
+			expect( screen.getByRole( 'toolbar' ) ).toHaveClass(
+				'is-unstyled'
+			);
+		} );
 	} );
 } );
