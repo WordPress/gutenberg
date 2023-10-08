37ff16f75bc68ec2e14bab0c6d3a3667cbb04e6e
Marco Ciampini <marco.ciampo@gmail.com>
2023-10-08 10:47:06 +0200

    Add test story

--- packages/components/src/color-picker/stories/index.story.tsx
+++ packages/components/src/color-picker/stories/index.story.tsx
@@ -12,6 +12,8 @@ import { useState } from '@wordpress/element';
  * Internal dependencies
  */
 import { ColorPicker } from '../component';
+import Dropdown from '../../dropdown';
+import DropdownContentWrapper from '../../dropdown/dropdown-content-wrapper';
 
 const meta: Meta< typeof ColorPicker > = {
 	component: ColorPicker,
@@ -46,3 +48,57 @@ const Template: StoryFn< typeof ColorPicker > = ( { onChange, ...props } ) => {
 };
 
 export const Default = Template.bind( {} );
+
+export const WithDropdown: StoryFn< typeof ColorPicker > = ( {
+	onChange,
+	...props
+} ) => {
+	const [ color, setColor ] = useState< string | undefined >();
+
+	return (
+		<div style={ { width: '100%', height: '600px', position: 'relative' } }>
+			<Dropdown
+				renderContent={ () => (
+					<DropdownContentWrapper paddingSize="none">
+						<ColorPicker
+							{ ...props }
+							color={ color }
+							onChange={ ( ...changeArgs ) => {
+								onChange?.( ...changeArgs );
+								setColor( ...changeArgs );
+							} }
+						/>
+					</DropdownContentWrapper>
+				) }
+				renderToggle={ ( { isOpen, onToggle } ) => (
+					<button
+						onClick={ onToggle }
+						style={ {
+							marginLeft: '100px',
+							marginTop: '100px',
+							zIndex: 1,
+							position: 'relative',
+						} }
+					>
+						{ isOpen ? 'Close' : 'Open' }
+					</button>
+				) }
+				popoverProps={ {
+					resize: false,
+					placement: 'right-start',
+				} }
+			/>
+			<iframe
+				style={ {
+					position: 'absolute',
+					top: 0,
+					left: 0,
+					height: '100%',
+					width: '100%',
+					zIndex: 0,
+				} }
+				title="test"
+			/>
+		</div>
+	);
+};
