65ea7a84dda122c174a4c21c5eb3085f3f6365a4
Riad Benguella <benguella@gmail.com>
2023-10-09 09:55:38 +0100

    DataViews: Update the data views component to pass an view object

--- packages/edit-site/src/components/dataviews/dataviews.js
+++ packages/edit-site/src/components/dataviews/dataviews.js
@@ -28,18 +28,64 @@ import TextFilter from './text-filter';
 export default function DataViews( {
 	data,
 	fields,
+	view,
+	onChangeView,
 	isLoading,
 	paginationInfo,
-	options,
+	options: { pageCount },
 } ) {
 	const dataView = useReactTable( {
 		data,
 		columns: fields,
-		...options,
+		manualSorting: true,
+		manualFiltering: true,
+		manualPagination: true,
+		enableRowSelection: true,
+		state: {
+			sorting: [
+				{ id: view.sort.column, desc: view.sort.direction === 'desc' },
+			],
+			globalFilter: view.search,
+			pagination: {
+				pageIndex: view.page,
+				pageSize: view.perPage,
+			},
+		},
+		onSortingChange: ( sortingUpdater ) => {
+			onChangeView( ( currentView ) => {
+				const [ { id, desc } ] =
+					typeof sortingUpdater === 'function'
+						? sortingUpdater( [
+								{
+									id: currentView.sort.column,
+									desc: currentView.sort.direction === 'desc',
+								},
+						  ] )
+						: sortingUpdater;
+				return {
+					...currentView,
+					sort: { column: id, direction: desc ? 'desc' : 'asc' },
+				};
+			} );
+		},
+		onGlobalFilterChange: ( value ) => {
+			onChangeView( { ...view, search: value, page: 0 } );
+		},
+		onPaginationChange: ( paginationUpdater ) => {
+			onChangeView( ( currentView ) => {
+				const { pageIndex, pageSize } = paginationUpdater( {
+					pageIndex: currentView.page,
+					pageSize: currentView.perPage,
+				} );
+				return { ...view, page: pageIndex, perPage: pageSize };
+			} );
+		},
 		getCoreRowModel: getCoreRowModel(),
 		getFilteredRowModel: getFilteredRowModel(),
 		getSortedRowModel: getSortedRowModel(),
 		getPaginationRowModel: getPaginationRowModel(),
+		meta: { resetQuery: () => onChangeView( { ...view } ) },
+		pageCount,
 	} );
 	return (
 		<div className="dataviews-wrapper">
