d8bc25dddbf23e05bc2a0b4339a13789ec11777c
brookewp <brooke.kaminski@automattic.com>
2023-10-10 14:31:58 -0700

    Update const naming and add comment

--- packages/url/src/filter-url-for-display.js
+++ packages/url/src/filter-url-for-display.js
@@ -21,12 +21,13 @@ export function filterURLForDisplay( url, maxLength = null ) {
 		filteredURL = filteredURL.replace( '/', '' );
 	}
 
-	const mediaRegexp = /\/([^\/?]+)\.(?:[\w]+)(?=\?|$)/;
+	// capture file name from URL
+	const fileRegexp = /\/([^\/?]+)\.(?:[\w]+)(?=\?|$)/;
 
 	if (
 		! maxLength ||
 		filteredURL.length <= maxLength ||
-		! filteredURL.match( mediaRegexp )
+		! filteredURL.match( fileRegexp )
 	) {
 		return filteredURL;
 	}
