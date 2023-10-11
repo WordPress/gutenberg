f19fdb1c9c59e89f01b627ce116d0e98be1f89a1
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-10-11 10:49:30 +1000

    Patterns: Register new media related categories

---
+++ lib/compat/wordpress-6.5/block-patterns.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Extends Core's wp-includes/block-patterns.php to add new media related
+ * pattern categories for WP 6.5.
+ *
+ * @package gutenberg
+ */
+
+/**
+ * Adds new pattern categories for better organization of media related patterns.
+ *
+ * Note: This should be removed when the minimum required WP version is >= 6.5.
+ *
+ * @return void
+ */
+function gutenberg_register_media_pattern_categories() {
+	unregister_block_pattern_category( 'media' );
+
+	register_block_pattern_category(
+		'images',
+		array(
+			'label'       => _x( 'Images', 'Block pattern category' ),
+			'description' => __( 'Different layouts containing images.' ),
+		)
+	);
+	register_block_pattern_category(
+		'videos',
+		array(
+			'label'       => _x( 'Videos', 'Block pattern category' ),
+			'description' => __( 'Different layouts containing videos.' ),
+		)
+	);
+	register_block_pattern_category(
+		'audio',
+		array(
+			'label'       => _x( 'Audio', 'Block pattern category' ),
+			'description' => __( 'Different layouts containing audio.' ),
+		)
+	);
+}
+add_action( 'init', 'gutenberg_register_media_pattern_categories' );
