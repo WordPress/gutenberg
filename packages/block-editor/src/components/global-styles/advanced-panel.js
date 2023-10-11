d4072bf4dd9ea2b862870b4ca32118a46e4143f7
Glen Davies <glen.davies@automattic.com>
2023-10-11 15:56:47 +0200

    Revert back to lazy load with react suspense

--- packages/block-editor/src/components/global-styles/advanced-panel.js
+++ packages/block-editor/src/components/global-styles/advanced-panel.js
@@ -1,96 +1,24 @@
 /**
- * External dependencies
- */
-import { EditorView, basicSetup } from 'codemirror';
-import { css } from '@codemirror/lang-css';
-
-/**
  * WordPress dependencies
  */
-import {
-	TextareaControl,
-	Tooltip,
-	__experimentalVStack as VStack,
-} from '@wordpress/components';
-import { useState, useRef, useEffect } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
-import { Icon, info } from '@wordpress/icons';
-import { lazy, Suspense } from '@wordpress/element';
+import { Suspense, lazy } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { default as transformStyles } from '../../utils/transform-styles';
-
-const CSSEditor = lazy( () => import( './custom-css-editor' ) );
-
+const CSSEditor = lazy( () => import( './css-editor' ) );
+console.log( CSSEditor );
 export default function AdvancedPanel( {
 	value,
 	onChange,
 	inheritedValue = value,
 } ) {
-	const editorRef = useRef();
-	useEffect( () => {
-		if ( editorRef.current ) {
-			new EditorView( {
-				extensions: [ basicSetup, css() ],
-				parent: editorRef.current,
-			} );
-		}
-	}, [ editorRef.current ] );
-
 	// Custom CSS
-	const [ cssError, setCSSError ] = useState( null );
 	const customCSS = inheritedValue?.css;
-	function handleOnChange( newValue ) {
-		onChange( {
-			...value,
-			css: newValue,
-		} );
-		if ( cssError ) {
-			const [ transformed ] = transformStyles(
-				[ { css: value } ],
-				'.editor-styles-wrapper'
-			);
-			if ( transformed ) {
-				setCSSError( null );
-			}
-		}
-	}
-	function handleOnBlur( event ) {
-		if ( ! event?.target?.value ) {
-			setCSSError( null );
-			return;
-		}
-
-		const [ transformed ] = transformStyles(
-			[ { css: event.target.value } ],
-			'.editor-styles-wrapper'
-		);
-
-		setCSSError(
-			transformed === null
-				? __( 'There is an error with your CSS structure.' )
-				: null
-		);
-	}
 
 	return (
-		<VStack spacing={ 3 }>
-			<Suspense>
-				<CSSEditor />
-			</Suspense>
-
-			{ cssError && (
-				<Tooltip text={ cssError }>
-					<div className="block-editor-global-styles-advanced-panel__custom-css-validation-wrapper">
-						<Icon
-							icon={ info }
-							className="block-editor-global-styles-advanced-panel__custom-css-validation-icon"
-						/>
-					</div>
-				</Tooltip>
-			) }
-		</VStack>
+		<Suspense>
+			<CSSEditor onChange={ onChange } value={ customCSS } />
+		</Suspense>
 	);
 }
---
+++ packages/block-editor/src/components/global-styles/css-editor.js
@@ -0,0 +1,95 @@
+/**
+ * External dependencies
+ */
+import { EditorView, basicSetup } from 'codemirror';
+import { css } from '@codemirror/lang-css';
+
+/**
+ * WordPress dependencies
+ */
+import { useState, useRef, useEffect, useId } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Tooltip, __experimentalVStack as VStack } from '@wordpress/components';
+import { Icon, info } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import { default as transformStyles } from '../../utils/transform-styles';
+
+export default function CSSEditor( { value, onChange } ) {
+	const editorRef = useRef();
+
+	// Custom CSS
+	const [ cssError, setCSSError ] = useState( null );
+
+	useEffect( () => {
+		if ( editorRef.current ) {
+			new EditorView( {
+				extensions: [ basicSetup, css() ],
+				parent: editorRef.current,
+			} );
+			// editor.on( 'change', ( codemirror ) =>
+			// 	handleOnChange( codemirror.getValue() )
+			// );
+		}
+		// We only want to run this once, so we can ignore the dependency array.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [] );
+
+	function handleOnChange( newValue ) {
+		onChange( {
+			...value,
+			css: newValue,
+		} );
+		if ( cssError ) {
+			const [ transformed ] = transformStyles(
+				[ { css: value } ],
+				'.editor-styles-wrapper'
+			);
+			if ( transformed ) {
+				setCSSError( null );
+			}
+		}
+	}
+	function handleOnBlur( event ) {
+		if ( ! event?.target?.value ) {
+			setCSSError( null );
+			return;
+		}
+
+		const [ transformed ] = transformStyles(
+			[ { css: event.target.value } ],
+			'.editor-styles-wrapper'
+		);
+
+		setCSSError(
+			transformed === null
+				? __( 'There is an error with your CSS structure.' )
+				: null
+		);
+	}
+	const cssEditorId = useId();
+	return (
+		<VStack spacing={ 3 }>
+			<label htmlFor={ cssEditorId }>
+				{ __( 'Additional CSS' ) }
+				<textarea
+					ref={ editorRef }
+					onBlur={ handleOnBlur }
+					id={ cssEditorId }
+				></textarea>
+			</label>
+			{ cssError && (
+				<Tooltip text={ cssError }>
+					<div className="block-editor-global-styles-advanced-panel__custom-css-validation-wrapper">
+						<Icon
+							icon={ info }
+							className="block-editor-global-styles-advanced-panel__custom-css-validation-icon"
+						/>
+					</div>
+				</Tooltip>
+			) }
+		</VStack>
+	);
+}
