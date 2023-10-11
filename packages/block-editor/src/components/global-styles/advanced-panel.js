3278246e848e815b53ce3a81e2fa7872c0902d03
Glen Davies <glen.davies@automattic.com>
2023-10-11 15:51:54 +0200

    add coce-mirror

--- packages/block-editor/src/components/global-styles/advanced-panel.js
+++ packages/block-editor/src/components/global-styles/advanced-panel.js
@@ -1,7 +1,19 @@
 /**
+ * WordPress dependencies
+ */
+import {
+	TextareaControl,
+	Tooltip,
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Icon, info } from '@wordpress/icons';
+
+/**
  * Internal dependencies
  */
-import CSSEditor from './css-editor';
+import { default as transformStyles } from '../../utils/transform-styles';
 
 export default function AdvancedPanel( {
 	value,
@@ -9,7 +21,62 @@ export default function AdvancedPanel( {
 	inheritedValue = value,
 } ) {
 	// Custom CSS
+	const [ cssError, setCSSError ] = useState( null );
 	const customCSS = inheritedValue?.css;
+	function handleOnChange( newValue ) {
+		onChange( {
+			...value,
+			css: newValue,
+		} );
+		if ( cssError ) {
+			const [ transformed ] = transformStyles(
+				[ { css: value } ],
+				'.editor-styles-wrapper'
+			);
+			if ( transformed ) {
+				setCSSError( null );
+			}
+		}
+	}
+	function handleOnBlur( event ) {
+		if ( ! event?.target?.value ) {
+			setCSSError( null );
+			return;
+		}
 
-	return <CSSEditor onChange={ onChange } value={ customCSS } />;
+		const [ transformed ] = transformStyles(
+			[ { css: event.target.value } ],
+			'.editor-styles-wrapper'
+		);
+
+		setCSSError(
+			transformed === null
+				? __( 'There is an error with your CSS structure.' )
+				: null
+		);
+	}
+
+	return (
+		<VStack spacing={ 3 }>
+			<TextareaControl
+				label={ __( 'Additional CSS' ) }
+				__nextHasNoMarginBottom
+				value={ customCSS }
+				onChange={ ( newValue ) => handleOnChange( newValue ) }
+				onBlur={ handleOnBlur }
+				className="block-editor-global-styles-advanced-panel__custom-css-input"
+				spellCheck={ false }
+			/>
+			{ cssError && (
+				<Tooltip text={ cssError }>
+					<div className="block-editor-global-styles-advanced-panel__custom-css-validation-wrapper">
+						<Icon
+							icon={ info }
+							className="block-editor-global-styles-advanced-panel__custom-css-validation-icon"
+						/>
+					</div>
+				</Tooltip>
+			) }
+		</VStack>
+	);
 }
