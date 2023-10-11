9fac3a59a4fc7ff598399b1773ccbb2ffe48c032
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-10-11 17:15:40 +0200

    Check author filter

--- packages/edit-site/src/components/page-pages/index.js
+++ packages/edit-site/src/components/page-pages/index.js
@@ -175,14 +175,13 @@ export default function PagePages() {
 									<DropdownMenuItemV2
 										key={ author.id }
 										prefix={
-											view.filters?.author !==
-												undefined && (
+											view.filters?.author ===
+												author.id && (
 												<Icon icon={ check } />
 											)
 										}
 										role="menuitemcheckbox"
-										onSelect={ ( event ) => {
-											event.preventDefault();
+										onSelect={ () => {
 											setView( ( currentView ) => {
 												return {
 													...currentView,
@@ -223,7 +222,7 @@ export default function PagePages() {
 				enableSorting: false,
 			},
 		],
-		[ postStatuses, authors ]
+		[ postStatuses, authors, view ]
 	);
 
 	const trashPostAction = useTrashPostAction();
