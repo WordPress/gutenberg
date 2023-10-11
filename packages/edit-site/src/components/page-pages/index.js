fde57a70b9eb76338fc5575940123d771f4e60f5
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-10-11 17:38:43 +0200

    Better description

--- packages/edit-site/src/components/page-pages/index.js
+++ packages/edit-site/src/components/page-pages/index.js
@@ -171,7 +171,7 @@ export default function PagePages() {
 							}
 						>
 							{ [
-								{ id: undefined, name: __( 'None' ) },
+								{ id: undefined, name: __( 'All' ) },
 								...authors,
 							].map( ( author ) => {
 								return (
