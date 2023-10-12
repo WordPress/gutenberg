5df0ab4a8abb886611f34092bedb8f6947377d66
Damián Suárez <rdsuarez@gmail.com>
2023-10-12 08:42:00 -0300

    Update packages/edit-post/src/components/sidebar/post-excerpt/index.js
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- packages/edit-post/src/components/sidebar/post-excerpt/index.js
+++ packages/edit-post/src/components/sidebar/post-excerpt/index.js
@@ -52,7 +52,7 @@ export default function PostExcerpt() {
 							{ fills }
 						</>
 					) }
-				</PluginPostExcerp.Slot>
+				</PluginPostExcerpt.Slot>
 			</PanelBody>
 		</PostExcerptCheck>
 	);
