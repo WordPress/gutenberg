a0708133990a50ae49b38bee064418da22d06cfb
George Mamadashvili <georgemamadashvili@gmail.com>
2023-10-12 13:54:29 +0400

    Migrate 'Post Title block' e2e tests to Playwright
    Co-authored-by: Justin Ahinon <justiny.ahinon@gmail.com>
    Co-authored-by: Alvi Tazwar <55917380+alvitazwar@users.noreply.github.com>

---
+++ test/e2e/specs/editor/blocks/block-title.spec.js
@@ -0,0 +1,36 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Post Title block', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
+	} );
+
+	test( 'Can edit the post title', async ( { editor, page } ) => {
+		await editor.insertBlock( { name: 'core/post-title' } );
+
+		// Add the post title
+		await editor.canvas
+			.getByRole( 'textbox', {
+				name: 'Add title',
+			} )
+			.fill( 'Just tweaking the post title' );
+
+		// Save the post draft and reload.
+		await page.getByRole( 'button', { name: 'Save draft' } ).click();
+		await expect(
+			page
+				.getByRole( 'button', { name: 'Dismiss this notice' } )
+				.filter( { hasText: 'Draft saved' } )
+		).toBeVisible();
+		await page.reload();
+
+		const titleBlock = editor.canvas.getByRole( 'document', {
+			name: 'Block: Title',
+		} );
+		await expect( titleBlock ).toBeVisible();
+		await expect( titleBlock ).toHaveText( 'Just tweaking the post title' );
+	} );
+} );
