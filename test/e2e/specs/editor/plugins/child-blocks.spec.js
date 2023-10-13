688f9fc5e56270834c2f53198e2b3b9b625b2199
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2023-10-13 11:54:42 +0530

    Update locators

--- test/e2e/specs/editor/plugins/child-blocks.spec.js
+++ test/e2e/specs/editor/plugins/child-blocks.spec.js
@@ -3,12 +3,6 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test.use( {
-	ChildBook: async ( { page }, use ) => {
-		await use( new ChildBook( { page } ) );
-	},
-} );
-
 test.describe( 'Child Blocks', () => {
 	test.beforeAll( async ( { requestUtils } ) => {
 		await requestUtils.activatePlugin( 'gutenberg-test-child-blocks' );
@@ -22,78 +16,72 @@ test.describe( 'Child Blocks', () => {
 		await requestUtils.deactivatePlugin( 'gutenberg-test-child-blocks' );
 	} );
 
-	test( 'are hidden from the global block inserter', async ( {
-		page,
-		ChildBook,
-	} ) => {
-		await page
-			.getByRole( 'button', { name: 'Toggle block inserter' } )
-			.click();
-		await expect(
-			await ChildBook.getAllBlockInserterItemTitles()
-		).not.toContain( 'Child Blocks Child' );
+	test( 'are hidden from the global block inserter', async ( { page } ) => {
+		const blockInserter = page
+			.getByRole( 'toolbar', { name: 'Document tools' } )
+			.getByRole( 'button', { name: 'Toggle block inserter' } );
+		const blockLibrary = page.getByRole( 'region', {
+			name: 'Block Library',
+		} );
+
+		await blockInserter.click();
+		await expect( blockLibrary ).toBeVisible();
+		expect( blockLibrary.getByRole( 'option' ) ).not.toContain( [
+			'Child Blocks Child',
+		] );
 	} );
 
-	test( 'shows up in a parent block', async ( {
-		page,
-		editor,
-		ChildBook,
-	} ) => {
+	test( 'shows up in a parent block', async ( { page, editor } ) => {
 		await editor.insertBlock( {
 			name: 'test/child-blocks-unrestricted-parent',
 		} );
 
-		await page.click(
-			'[data-type="test/child-blocks-unrestricted-parent"] .block-editor-default-block-appender'
-		);
 		await page
-			.getByRole( 'button', { name: 'Toggle block inserter' } )
+			.getByRole( 'document', {
+				name: 'Block: Child Blocks Unrestricted Parent',
+			} )
 			.click();
-		const inserterItemTitles =
-			await ChildBook.getAllBlockInserterItemTitles();
-		expect( inserterItemTitles ).toContain( 'Child Blocks Child' );
-		expect( inserterItemTitles.length ).toBeGreaterThan( 10 );
+		const blockInserter = page
+			.getByRole( 'toolbar', { name: 'Document tools' } )
+			.getByRole( 'button', { name: 'Toggle block inserter' } );
+		const blockLibrary = page.getByRole( 'region', {
+			name: 'Block Library',
+		} );
+
+		await blockInserter.click();
+		await expect( blockLibrary ).toBeVisible();
+		expect(
+			await blockLibrary.getByRole( 'option' ).count()
+		).toBeGreaterThan( 10 );
 	} );
 
 	test( 'display in a parent block with allowedItems', async ( {
 		page,
 		editor,
-		ChildBook,
 	} ) => {
 		await editor.insertBlock( {
 			name: 'test/child-blocks-restricted-parent',
 		} );
 
-		await page.click(
-			'[data-type="test/child-blocks-restricted-parent"] .block-editor-default-block-appender'
-		);
 		await page
-			.getByRole( 'button', { name: 'Toggle block inserter' } )
+			.getByRole( 'document', {
+				name: 'Block: Child Blocks Restricted Parent',
+			} )
 			.click();
-		const allowedBlocks = await ChildBook.getAllBlockInserterItemTitles();
-		expect( allowedBlocks.sort() ).toEqual( [
+
+		const blockInserter = page
+			.getByRole( 'toolbar', { name: 'Document tools' } )
+			.getByRole( 'button', { name: 'Toggle block inserter' } );
+		const blockLibrary = page.getByRole( 'region', {
+			name: 'Block Library',
+		} );
+
+		await blockInserter.click();
+		await expect( blockLibrary ).toBeVisible();
+		await expect( blockLibrary.getByRole( 'option' ) ).toHaveText( [
+			'Paragraph',
 			'Child Blocks Child',
 			'Image',
-			'Paragraph',
 		] );
 	} );
 } );
-
-class ChildBook {
-	constructor( { page } ) {
-		this.page = page;
-	}
-
-	async getAllBlockInserterItemTitles() {
-		const inserterItemTitles = await this.page.evaluate( () => {
-			return Array.from(
-				document.querySelectorAll(
-					'.block-editor-block-types-list__item-title'
-				)
-			).map( ( inserterItem ) => {
-				return inserterItem.innerText;
-			} );
-		} );
-		return [ ...new Set( inserterItemTitles ) ];
-	}
-}
