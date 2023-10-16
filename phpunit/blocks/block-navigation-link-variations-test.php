d79387e12fafac668a927f516e5c58003bae46f7
Fabian Todt <mail@fabiantodt.at>
2023-10-16 12:15:03 +0200

    tests: better assertions

--- phpunit/blocks/block-navigation-link-variations-test.php
+++ phpunit/blocks/block-navigation-link-variations-test.php
@@ -49,11 +49,11 @@ class Block_Navigation_Link_Variations_Test extends WP_UnitTestCase {
 	public function test_navigation_link_variations_custom_post_type() {
 		$registry       = WP_Block_Type_Registry::get_instance();
 		$nav_link_block = $registry->get_registered( 'core/navigation-link' );
-		$this->assertFalse( empty( $nav_link_block->variations ) );
+		$this->assertNotEmpty( $nav_link_block->variations, 'Block has no variations' );
 		$variation = $this->get_variation_by_name( 'custom_book', $nav_link_block->variations );
-		$this->assertIsArray( $variation );
-		$this->assertArrayHasKey( 'title', $variation );
-		$this->assertEquals( 'Custom Book', $variation['title'] );
+		$this->assertIsArray( $variation, 'Block variation is not an array' );
+		$this->assertArrayHasKey( 'title', $variation, 'Block variation has no title' );
+		$this->assertEquals( 'Custom Book', $variation['title'], 'Variation title is different than the post type label' );
 	}
 
 	/**
@@ -62,11 +62,11 @@ class Block_Navigation_Link_Variations_Test extends WP_UnitTestCase {
 	public function test_navigation_link_variations_custom_taxonomy() {
 		$registry       = WP_Block_Type_Registry::get_instance();
 		$nav_link_block = $registry->get_registered( 'core/navigation-link' );
-		$this->assertFalse( empty( $nav_link_block->variations ) );
+		$this->assertNotEmpty( $nav_link_block->variations, 'Block has no variations' );
 		$variation = $this->get_variation_by_name( 'book_type', $nav_link_block->variations );
-		$this->assertIsArray( $variation );
-		$this->assertArrayHasKey( 'title', $variation );
-		$this->assertEquals( 'Book Type', $variation['title'] );
+		$this->assertIsArray( $variation, 'Block variation is not an array' );
+		$this->assertArrayHasKey( 'title', $variation, 'Block variation has no title' );
+		$this->assertEquals( 'Book Type', $variation['title'], 'Variation title is different than the post type label' );
 	}
 
 	/**
