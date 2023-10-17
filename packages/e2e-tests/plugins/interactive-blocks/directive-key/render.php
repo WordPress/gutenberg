74b665a049b6644ebfd453377270f0bf8045d3c4
David Arenas <david.arenas@automattic.com>
2023-10-17 16:12:13 +0200

    Update wp-key tests

--- packages/e2e-tests/plugins/interactive-blocks/directive-key/render.php
+++ packages/e2e-tests/plugins/interactive-blocks/directive-key/render.php
@@ -7,7 +7,10 @@
 
 ?>
 
-<div data-wp-interactive data-wp-navigation-id="some-id">
+<div
+	data-wp-interactive='{ "namespace": "directive-key" }'
+	data-wp-navigation-id="some-id"
+>
 	<ul>
 		<li data-wp-key="id-2" data-testid="first-item">2</li>
 		<li data-wp-key="id-3">3</li>
