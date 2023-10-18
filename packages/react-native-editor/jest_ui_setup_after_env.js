9cb9ec5cda7c96b7cb82a7f14bd3dfc75149cb54
Gerardo <gerardo.pacheco@automattic.com>
2023-10-18 13:08:00 +0200

    Improve Screenrecording on Android by making sure of the following:
    The sdcard folder should exist in the emulator, if not there's a script that will create it.
    
    For skipped tests, it will remove empty folders that weren't needed.
    
    When having multiple Android emulators opened, it will use the deviceID that Appium is currently using.

--- packages/react-native-editor/jest_ui_setup_after_env.js
+++ packages/react-native-editor/jest_ui_setup_after_env.js
@@ -14,6 +14,7 @@ jest.setTimeout( 1000000 ); // In milliseconds.
 
 let iOSScreenRecordingProcess;
 let androidScreenRecordingProcess;
+let deviceID;
 
 const isMacOSEnvironment = () => {
 	return process.platform === 'darwin';
@@ -21,20 +22,75 @@ const isMacOSEnvironment = () => {
 
 const IOS_RECORDINGS_DIR = './ios-screen-recordings';
 const ANDROID_RECORDINGS_DIR = './android-screen-recordings';
+const ANDROID_EMULATOR_DIR = '/sdcard/';
 
 const getScreenRecordingFileNameBase = ( testPath, id ) => {
 	const suiteName = path.basename( testPath, '.test.js' );
 	return `${ suiteName }.${ id }`;
 };
 
+function deleteRecordingFile( filePath ) {
+	if ( fs.existsSync( filePath ) ) {
+		try {
+			fs.unlinkSync( filePath );
+		} catch ( error ) {
+			// eslint-disable-next-line no-console
+			console.error(
+				`Failed to delete ${ filePath }. Error: ${ error.message }`
+			);
+		}
+	}
+}
+
+async function getAndroidDeviceID() {
+	try {
+		const session = await global.editorPage.driver.getSession();
+		return session.deviceUDID;
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.error( 'Failed to fetch Android device ID:', error.message );
+		return null;
+	}
+}
+
 let allPassed = true;
+
+function logAndroidError( data ) {
+	// eslint-disable-next-line no-console
+	console.log( `Android screen recording error => ${ data }` );
+}
+
+// Setup the listener before each test.
+beforeEach( () => {
+	if (
+		androidScreenRecordingProcess &&
+		androidScreenRecordingProcess.stderr
+	) {
+		androidScreenRecordingProcess.stderr.on( 'data', logAndroidError );
+	}
+} );
+
+// Cleanup the listener after each test.
+afterEach( () => {
+	if (
+		androidScreenRecordingProcess &&
+		androidScreenRecordingProcess.stderr
+	) {
+		androidScreenRecordingProcess.stderr.removeListener(
+			'data',
+			logAndroidError
+		);
+	}
+} );
 
 // eslint-disable-next-line jest/no-jasmine-globals, no-undef
 jasmine.getEnv().addReporter( {
-	specStarted: ( { testPath, id } ) => {
+	specStarted: async ( { testPath, id } ) => {
 		if ( ! isMacOSEnvironment() ) {
 			return;
 		}
+
+		deviceID = await getAndroidDeviceID();
 
 		const fileName =
 			getScreenRecordingFileNameBase( testPath, id ) + '.mp4';
@@ -44,7 +100,20 @@ jasmine.getEnv().addReporter( {
 				fs.mkdirSync( ANDROID_RECORDINGS_DIR );
 			}
 
+			// Use the "mkdir -p" command to create the
+			// ANDROID_EMULATOR_DIR directory if it doesn't exists.
+			try {
+				childProcess.execSync(
+					`adb -s ${ deviceID } shell "mkdir -p ${ ANDROID_EMULATOR_DIR }" 2>/dev/null`
+				);
+			} catch ( error ) {
+				// eslint-disable-next-line no-console
+				console.error( `Failed to create the directory: ${ error }` );
+			}
+
 			androidScreenRecordingProcess = childProcess.spawn( 'adb', [
+				'-s',
+				deviceID,
 				'shell',
 				'screenrecord',
 				'--verbose',
@@ -52,14 +121,8 @@ jasmine.getEnv().addReporter( {
 				'1M',
 				'--size',
 				'720x1280',
-				`/sdcard/${ fileName }`,
+				`${ ANDROID_EMULATOR_DIR }${ fileName }`,
 			] );
-
-			androidScreenRecordingProcess.stderr.on( 'data', ( data ) => {
-				// eslint-disable-next-line no-console
-				console.log( `Android screen recording error => ${ data }` );
-			} );
-
 			return;
 		}
 
@@ -85,6 +148,7 @@ jasmine.getEnv().addReporter( {
 	},
 	specDone: ( { testPath, id, status } ) => {
 		allPassed = allPassed && status !== 'failed';
+		const isTestSkipped = status === 'pending';
 
 		if ( ! isMacOSEnvironment() ) {
 			return;
@@ -97,9 +161,16 @@ jasmine.getEnv().addReporter( {
 			// Wait for kill.
 			childProcess.execSync( 'sleep 1' );
 
+			const recordingFilePath = `${ ANDROID_RECORDINGS_DIR }/${ fileNameBase }.mp4`;
+
+			if ( isTestSkipped ) {
+				deleteRecordingFile( recordingFilePath );
+				return;
+			}
+
 			try {
 				childProcess.execSync(
-					`adb pull /sdcard/${ fileNameBase }.mp4 ${ ANDROID_RECORDINGS_DIR }`
+					`adb -s ${ deviceID } pull ${ ANDROID_EMULATOR_DIR }${ fileNameBase }.mp4 ${ ANDROID_RECORDINGS_DIR }`
 				);
 			} catch ( error ) {
 				// Some (old) Android devices don't support screen recording or
@@ -113,22 +184,27 @@ jasmine.getEnv().addReporter( {
 				);
 			}
 
-			const oldPath = `${ ANDROID_RECORDINGS_DIR }/${ fileNameBase }.mp4`;
 			const newPath = `${ ANDROID_RECORDINGS_DIR }/${ fileNameBase }.${ status }.mp4`;
 
-			if ( fs.existsSync( oldPath ) ) {
-				fs.renameSync( oldPath, newPath );
+			if ( fs.existsSync( recordingFilePath ) ) {
+				fs.renameSync( recordingFilePath, newPath );
 			}
 			return;
 		}
 
 		iOSScreenRecordingProcess.kill( 'SIGINT' );
 
-		const oldPath = `${ IOS_RECORDINGS_DIR }/${ fileNameBase }.mp4`;
+		const recordingFilePath = `${ IOS_RECORDINGS_DIR }/${ fileNameBase }.mp4`;
+
+		if ( isTestSkipped ) {
+			deleteRecordingFile( recordingFilePath );
+			return;
+		}
+
 		const newPath = `${ IOS_RECORDINGS_DIR }/${ fileNameBase }.${ status }.mp4`;
 
-		if ( fs.existsSync( oldPath ) ) {
-			fs.renameSync( oldPath, newPath );
+		if ( fs.existsSync( recordingFilePath ) ) {
+			fs.renameSync( recordingFilePath, newPath );
 		}
 	},
 } );
