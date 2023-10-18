af34a42bd3fa1045fcd4b9d7542f451aa27ce620
Carolina Nymark <myazalea@hotmail.com>
2023-10-18 10:46:35 +0200

    Cover: Add aria-label to fixed and repeated image backgrounds (#50990)
    * Cover: Add aria-label to fixed and repeated backgrounds
    
    * Update deprecated.js
    
    * Update duotone and fixed background fixture
    
    * Add a new fixture for a cover block with fixed background and alternative text as aria-label.
    
    * Update packages/block-library/src/cover/deprecated.js
    
    * Oops: Hide the alt text field for video cover blocks
    
    * Try to fix merge conflicts
    
    * Try to fix merge conflicts
    
    * Use constants for attributes and supports

--- packages/block-library/src/cover/edit/index.js
+++ packages/block-library/src/cover/edit/index.js
@@ -534,7 +534,8 @@ function CoverEdit( {
 					) : (
 						<div
 							ref={ mediaElement }
-							role="img"
+							role={ alt ? 'img' : undefined }
+							aria-label={ alt ? alt : undefined }
 							className={ classnames(
 								classes,
 								'wp-block-cover__image-background'
