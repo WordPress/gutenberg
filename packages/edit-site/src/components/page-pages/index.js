d3d28ec2da16c93ef508cf5c167ffa83b56c88bf
Jorge Costa <jorge.costa@developer.pt>
2023-10-18 23:00:52 +0100

    Add: Ability to persist dataviews on the database.

--- packages/edit-site/src/components/page-pages/index.js
+++ packages/edit-site/src/components/page-pages/index.js
@@ -19,6 +19,8 @@ import Link from '../routes/link';
 import { DataViews } from '../dataviews';
 import useTrashPostAction from '../actions/trash-post';
 import Media from '../media';
+import DataviewsContext from '../dataviews/context';
+import { DEFAULT_STATUSES } from '../dataviews/provider';
 
 const EMPTY_ARRAY = [];
 const defaultConfigPerViewType = {
@@ -29,28 +31,8 @@ const defaultConfigPerViewType = {
 };
 
 export default function PagePages() {
-	// DEFAULT_STATUSES is intentionally sorted. Items do not have spaces in between them.
-	// The reason for that is to match defaultStatuses because we compare both strings below (see useEffect).
-	const DEFAULT_STATUSES = 'draft,future,pending,private,publish'; // All statuses but 'trash'.
-	const [ view, setView ] = useState( {
-		type: 'list',
-		filters: {
-			search: '',
-			status: DEFAULT_STATUSES,
-		},
-		page: 1,
-		perPage: 5,
-		sort: {
-			field: 'date',
-			direction: 'desc',
-		},
-		visibleFilters: [ 'search', 'author', 'status' ],
-		// All fields are visible by default, so it's
-		// better to keep track of the hidden ones.
-		hiddenFields: [ 'date', 'featured-image' ],
-		layout: {},
-	} );
-
+	const { view, setView } = useContext( DataviewsContext );
+	// Request post statuses to get the proper labels.
 	const { records: statuses } = useEntityRecords( 'root', 'status' );
 	const defaultStatuses = useMemo( () => {
 		return statuses === null
@@ -239,7 +221,7 @@ export default function PagePages() {
 
 			setView( updatedView );
 		},
-		[ view ]
+		[ view, setView ]
 	);
 
 	// TODO: we need to handle properly `data={ data || EMPTY_ARRAY }` for when `isLoading`.
