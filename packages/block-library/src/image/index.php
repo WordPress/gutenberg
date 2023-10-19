f469741b033db076ea3cba3590619b5e93cb4800
Artemio Morales <artemio.morales@a8c.com>
2023-10-19 12:51:23 -0500

    Update default fullscreen icon for lightbox trigger (#55463)

--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -243,11 +243,8 @@ function block_core_image_render_lightbox( $block_content, $block ) {
 			data-wp-style--top="context.core.image.imageButtonTop"
 			style="background: #000"
 		>
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-				<path d="M9 5H5V9" stroke="#FFFFFF" stroke-width="1.5"/>
-				<path d="M15 19L19 19L19 15" stroke="#FFFFFF" stroke-width="1.5"/>
-				<path d="M15 5H19V9" stroke="#FFFFFF" stroke-width="1.5"/>
-				<path d="M9 19L5 19L5 15" stroke="#FFFFFF" stroke-width="1.5"/>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#FFFFFF">
+				<Path stroke="#FFFFFF" d="M6 4a2 2 0 0 0-2 2v3h1.5V6a.5.5 0 0 1 .5-.5h3V4H6Zm3 14.5H6a.5.5 0 0 1-.5-.5v-3H4v3a2 2 0 0 0 2 2h3v-1.5Zm6 1.5v-1.5h3a.5.5 0 0 0 .5-.5v-3H20v3a2 2 0 0 1-2 2h-3Zm3-16a2 2 0 0 1 2 2v3h-1.5V6a.5.5 0 0 0-.5-.5h-3V4h3Z" />
 			</svg>
 		</button>';
 
