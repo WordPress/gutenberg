66c11af206da502d5b1c6854aef33826a399ede8
Glen Davies <glen.davies@automattic.com>
2023-10-21 20:49:07 +0200

    Revert "Patterns: fix capabilities settings for pattern categories (#55379)"
    This reverts commit 5b10d27ec406cbdec84e14408cd350ba9e8c544a.

--- lib/compat/wordpress-6.4/blocks.php
+++ lib/compat/wordpress-6.4/blocks.php
@@ -21,27 +21,3 @@ function gutenberg_add_custom_capabilities_to_wp_block( $args ) {
 	return $args;
 }
 add_filter( 'register_wp_block_post_type_args', 'gutenberg_add_custom_capabilities_to_wp_block', 10, 1 );
-
-/**
- * Updates the wp_block REST enpoint in order to modify the wp_pattern_category action
- * links that are returned because as although the taxonomy is flat Author level users
- * are only allowed to assign categories.
- *
- * Note: This should be removed when the minimum required WP version is >= 6.4.
- *
- * @see https://github.com/WordPress/gutenberg/pull/55379
- *
- * @param array  $args Register post type args.
- * @param string $post_type The post type string.
- *
- * @return array Register post type args.
- */
-function gutenberg_update_patterns_block_rest_controller_class( $args, $post_type ) {
-	if ( 'wp_block' === $post_type ) {
-		$args['rest_controller_class'] = 'Gutenberg_REST_Blocks_Controller_6_4';
-	}
-
-	return $args;
-}
-
-add_filter( 'register_post_type_args', 'gutenberg_update_patterns_block_rest_controller_class', 11, 2 );
