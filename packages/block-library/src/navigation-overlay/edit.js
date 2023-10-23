4a20db4e7ad705f6e357f073f1e0fe63d5557d2e
scruffian <ben@scruffian.com>
2023-10-23 17:11:04 +0200

    Add supports

--- packages/block-library/src/navigation-overlay/edit.js
+++ packages/block-library/src/navigation-overlay/edit.js
@@ -1,16 +1,64 @@
 /**
  * WordPress dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import {
+	InnerBlocks,
+	useBlockProps,
+	useInnerBlocksProps,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
 
-const NavigationOverlayEdit = () => {
-	const props = useBlockProps();
-
-	return (
-		<>
-			<div { ...props }>hello</div>
-		</>
+const NavigationOverlayEdit = ( {
+	attributes,
+	clientId,
+	__unstableLayoutClassNames: layoutClassNames,
+} ) => {
+	const { hasInnerBlocks, themeSupportsLayout } = useSelect(
+		( select ) => {
+			const { getBlock, getSettings } = select( blockEditorStore );
+			const block = getBlock( clientId );
+			return {
+				hasInnerBlocks: !! ( block && block.innerBlocks.length ),
+				themeSupportsLayout: getSettings()?.supportsLayout,
+			};
+		},
+		[ clientId ]
 	);
+
+	const { templateLock, allowedBlocks, layout = {} } = attributes;
+
+	// Layout settings.
+	const { type = 'default' } = layout;
+	const layoutSupportEnabled =
+		themeSupportsLayout || type === 'flex' || type === 'grid';
+
+	// Hooks.
+	const blockProps = useBlockProps( {
+		className: ! layoutSupportEnabled ? layoutClassNames : null,
+	} );
+
+	// Default to the regular appender being rendered.
+	let renderAppender;
+	if ( ! hasInnerBlocks ) {
+		// When there is no placeholder, but the block is also empty,
+		// use the larger button appender.
+		renderAppender = InnerBlocks.ButtonBlockAppender;
+	}
+
+	const innerBlocksProps = useInnerBlocksProps(
+		layoutSupportEnabled
+			? blockProps
+			: { className: 'wp-block-navigation-overlay' },
+		{
+			templateLock,
+			allowedBlocks,
+			renderAppender,
+			__unstableDisableLayoutClassNames: ! layoutSupportEnabled,
+		}
+	);
+
+	return <div { ...innerBlocksProps } />;
 };
 
 export default NavigationOverlayEdit;
--- packages/block-library/src/navigation-overlay/index.js
+++ packages/block-library/src/navigation-overlay/index.js
@@ -1,15 +1,23 @@
 /**
+ * WordPress dependencies
+ */
+import { fullscreen } from '@wordpress/icons';
+
+/**
  * Internal dependencies
  */
 import initBlock from '../utils/init-block';
 import metadata from './block.json';
 import NavigationOverlayEdit from './edit';
+import save from './save';
 
 const { name } = metadata;
 export { metadata, name };
 
 export const settings = {
 	edit: NavigationOverlayEdit,
+	save,
+	icon: fullscreen,
 };
 
 export const init = () => initBlock( { name, metadata, settings } );
