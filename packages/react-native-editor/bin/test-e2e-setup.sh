9d4613a9a958a532c7472e801a1c099facbd141b
David Calhoun <github@davidcalhoun.me>
2023-10-24 09:49:46 -0400

    test: Improve E2E test consistency (#55554)
    * test: Mitigate conflicts between development server caches
    
    Occasionally, cryptic npm package dependency errors can occur between
    test runs. They appear to originate from stale development caches, e.g.
    Metro server. Clearing these caches, which E2E tests should not rely
    upon mitigates the likelihood of these errors.
    
    * test: Prefix informative E2E test setup logs
    
    Merely an attempt to add structure to the outputted logs.
    
    * test: Avoid setup scripts during test runs
    
    There may be clarity and efficiency by having an explicit setup script.
    It was avoid running unnecessary scripts during test runs. It is also
    referenced in the current documentation as a script to run
    intentionally.

--- packages/react-native-editor/bin/test-e2e-setup.sh
+++ packages/react-native-editor/bin/test-e2e-setup.sh
@@ -12,15 +12,19 @@ fi
 output=$($APPIUM_CMD driver list --installed --json)
 
 if echo "$output" | grep -q 'uiautomator2'; then
-    echo 'UiAutomator2 is installed, skipping installation.'
+    echo '[info] UiAutomator2 is installed, skipping installation.'
 else
-    echo 'UiAutomator2 not found, installing...'
+    echo '[info] UiAutomator2 not found, installing...'
     $APPIUM_CMD driver install uiautomator2
 fi
 
 if echo "$output" | grep -q 'xcuitest'; then
-    echo 'XCUITest is installed, skipping installation.'
+    echo '[info] XCUITest is installed, skipping installation.'
 else
-    echo 'XCUITest not found, installing...'
+    echo '[info] XCUITest not found, installing...'
     $APPIUM_CMD driver install xcuitest
 fi
+
+# Mitigate conflicts between development server caches and E2E tests
+npm run clean:runtime > /dev/null
+echo '[info] Runtime cache cleaned.'
