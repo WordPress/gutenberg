aa44e93ba629bb2032cef7adf7a7bbf00c263344
Dave Smith <getdavemail@gmail.com>
2023-10-25 12:07:02 +0200

    Reimplement outside the hook

--- packages/block-editor/src/components/block-settings-menu-controls/index.js
+++ packages/block-editor/src/components/block-settings-menu-controls/index.js
@@ -6,12 +6,18 @@ import {
 	MenuGroup,
 	MenuItem,
 	__experimentalStyleProvider as StyleProvider,
+	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+	Button,
+	TextControl,
+	Modal,
 } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { pipe } from '@wordpress/compose';
-import { __ } from '@wordpress/i18n';
+import { pipe, useInstanceId } from '@wordpress/compose';
+import { __, sprintf } from '@wordpress/i18n';
 import { getBlockSupport } from '@wordpress/blocks';
 import { useState } from '@wordpress/element';
+import { speak } from '@wordpress/a11y';
 
 /**
  * Internal dependencies
@@ -159,7 +165,7 @@ function useBlockRename( name ) {
 
 const emptyString = ( testString ) => testString?.trim()?.length === 0;
 
-function BlockRenameControl( clientId ) {
+function BlockRenameControl( { clientId } ) {
 	const [ renamingBlock, setRenamingBlock ] = useState( false );
 
 	const { metadata } = useSelect(
@@ -200,8 +206,7 @@ function BlockRenameControl( clientId ) {
 			>
 				{ __( 'Rename' ) }
 			</MenuItem>
-			{ renamingBlock && <div>Rename!</div> }
-			{ /* { renamingBlock && (
+			{ renamingBlock && (
 				<RenameModal
 					blockName={ customName || '' }
 					originalBlockName={ blockInformation?.title }
@@ -220,7 +225,98 @@ function BlockRenameControl( clientId ) {
 						onChange( newName );
 					} }
 				/>
-			) } */ }
+			) }
 		</>
+	);
+}
+
+function RenameModal( { blockName, originalBlockName, onClose, onSave } ) {
+	const [ editedBlockName, setEditedBlockName ] = useState( blockName );
+
+	const nameHasChanged = editedBlockName !== blockName;
+	const nameIsOriginal = editedBlockName === originalBlockName;
+	const nameIsEmpty = emptyString( editedBlockName );
+
+	const isNameValid = nameHasChanged || nameIsOriginal;
+
+	const autoSelectInputText = ( event ) => event.target.select();
+
+	const dialogDescription = useInstanceId(
+		RenameModal,
+		`block-editor-rename-modal__description`
+	);
+
+	const handleSubmit = () => {
+		const message =
+			nameIsOriginal || nameIsEmpty
+				? sprintf(
+						/* translators: %s: new name/label for the block */
+						__( 'Block name reset to: "%s".' ),
+						editedBlockName
+				  )
+				: sprintf(
+						/* translators: %s: new name/label for the block */
+						__( 'Block name changed to: "%s".' ),
+						editedBlockName
+				  );
+
+		// Must be assertive to immediately announce change.
+		speak( message, 'assertive' );
+		onSave( editedBlockName );
+
+		// Immediate close avoids ability to hit save multiple times.
+		onClose();
+	};
+
+	return (
+		<Modal
+			title={ __( 'Rename' ) }
+			onRequestClose={ onClose }
+			overlayClassName="block-editor-block-rename-modal"
+			aria={ {
+				describedby: dialogDescription,
+			} }
+			focusOnMount="firstContentElement"
+		>
+			<p id={ dialogDescription }>
+				{ __( 'Enter a custom name for this block.' ) }
+			</p>
+			<form
+				onSubmit={ ( e ) => {
+					e.preventDefault();
+
+					if ( ! isNameValid ) {
+						return;
+					}
+
+					handleSubmit();
+				} }
+			>
+				<VStack spacing="3">
+					<TextControl
+						__nextHasNoMarginBottom
+						value={ editedBlockName }
+						label={ __( 'Block name' ) }
+						hideLabelFromVision={ true }
+						placeholder={ originalBlockName }
+						onChange={ setEditedBlockName }
+						onFocus={ autoSelectInputText }
+					/>
+					<HStack justify="right">
+						<Button variant="tertiary" onClick={ onClose }>
+							{ __( 'Cancel' ) }
+						</Button>
+
+						<Button
+							aria-disabled={ ! isNameValid }
+							variant="primary"
+							type="submit"
+						>
+							{ __( 'Save' ) }
+						</Button>
+					</HStack>
+				</VStack>
+			</form>
+		</Modal>
 	);
 }
