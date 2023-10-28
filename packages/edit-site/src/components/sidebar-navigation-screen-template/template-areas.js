1e570e30c7cb9fa9f10d706963167467ae356954
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-10-28 17:57:25 +0900

    Site Editor Sidebar: Add "Areas" details panel to all templates

---
+++ packages/edit-site/src/components/sidebar-navigation-screen-template/template-areas.js
@@ -0,0 +1,144 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
+import {
+	__experimentalTruncate as Truncate,
+	__experimentalItemGroup as ItemGroup,
+} from '@wordpress/components';
+import { header, footer, layout } from '@wordpress/icons';
+import { useMemo } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
+
+/**
+ * Internal dependencies
+ */
+import {
+	SidebarNavigationScreenDetailsPanel,
+	SidebarNavigationScreenDetailsPanelRow,
+} from '../sidebar-navigation-screen-details-panel';
+import { unlock } from '../../lock-unlock';
+import { store as editSiteStore } from '../../store';
+import { useLink } from '../routes/link';
+import SidebarNavigationItem from '../sidebar-navigation-item';
+import { TEMPLATE_PART_POST_TYPE } from '../../utils/constants';
+
+const EMPTY_OBJECT = {};
+
+function TemplateAreaButton( { postId, icon, title } ) {
+	const icons = {
+		header,
+		footer,
+	};
+	const linkInfo = useLink( {
+		postType: TEMPLATE_PART_POST_TYPE,
+		postId,
+	} );
+
+	return (
+		<SidebarNavigationItem
+			className="edit-site-sidebar-navigation-screen-template__template-area-button"
+			{ ...linkInfo }
+			icon={ icons[ icon ] ?? layout }
+			withChevron
+		>
+			<Truncate
+				limit={ 20 }
+				ellipsizeMode="tail"
+				numberOfLines={ 1 }
+				className="edit-site-sidebar-navigation-screen-template__template-area-label-text"
+			>
+				{ decodeEntities( title ) }
+			</Truncate>
+		</SidebarNavigationItem>
+	);
+}
+
+export default function TemplateAreas() {
+	const { templatePartAreas, currentTemplateParts } = useSelect(
+		( select ) => {
+			const { getEntityRecord } = select( coreStore );
+			const { getSettings, getCurrentTemplateTemplateParts } = unlock(
+				select( editSiteStore )
+			);
+			const siteSettings = getEntityRecord( 'root', 'site' );
+			const _postsPageRecord = siteSettings?.page_for_posts
+				? getEntityRecord(
+						'postType',
+						'page',
+						siteSettings?.page_for_posts
+				  )
+				: EMPTY_OBJECT;
+
+			return {
+				allowCommentsOnNewPosts:
+					siteSettings?.default_comment_status === 'open',
+				postsPageTitle: _postsPageRecord?.title?.rendered,
+				postsPageId: _postsPageRecord?.id,
+				postsPerPage: siteSettings?.posts_per_page,
+				templatePartAreas: getSettings()?.defaultTemplatePartAreas,
+				currentTemplateParts: getCurrentTemplateTemplateParts(),
+			};
+		},
+		[]
+	);
+
+	/*
+	 * Merge data in currentTemplateParts with templatePartAreas,
+	 * which contains the template icon and fallback labels
+	 */
+	const templateAreas = useMemo( () => {
+		// Keep track of template part IDs that have already been added to the array.
+		const templatePartIds = new Set();
+		const filterOutDuplicateTemplateParts = ( currentTemplatePart ) => {
+			// If the template part has already been added to the array, skip it.
+			if ( templatePartIds.has( currentTemplatePart.templatePart.id ) ) {
+				return;
+			}
+			// Add to the array of template part IDs.
+			templatePartIds.add( currentTemplatePart.templatePart.id );
+			return currentTemplatePart;
+		};
+
+		return currentTemplateParts.length && templatePartAreas
+			? currentTemplateParts
+					.filter( filterOutDuplicateTemplateParts )
+					.map( ( { templatePart, block } ) => ( {
+						...templatePartAreas?.find(
+							( { area } ) => area === templatePart?.area
+						),
+						...templatePart,
+						clientId: block.clientId,
+					} ) )
+			: [];
+	}, [ currentTemplateParts, templatePartAreas ] );
+
+	if ( ! templateAreas.length ) {
+		return null;
+	}
+
+	return (
+		<SidebarNavigationScreenDetailsPanel
+			title={ __( 'Areas' ) }
+			spacing={ 3 }
+		>
+			<ItemGroup>
+				{ templateAreas.map(
+					( { clientId, label, icon, theme, slug, title } ) => (
+						<SidebarNavigationScreenDetailsPanelRow
+							key={ clientId }
+						>
+							<TemplateAreaButton
+								postId={ `${ theme }//${ slug }` }
+								title={ title?.rendered || label }
+								icon={ icon }
+							/>
+						</SidebarNavigationScreenDetailsPanelRow>
+					)
+				) }
+			</ItemGroup>
+		</SidebarNavigationScreenDetailsPanel>
+	);
+}
