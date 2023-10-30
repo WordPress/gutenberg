f8098ea1d3f2b0014024fa31f93da68c833c421b
David Calhoun <github@davidcalhoun.me>
2023-10-30 17:58:20 -0400

    refactor: Convert spaces to tabs to match project
    Tabs are the project configuration's preferred format.

--- packages/react-native-editor/bin/test-e2e-setup.sh
+++ packages/react-native-editor/bin/test-e2e-setup.sh
@@ -3,26 +3,26 @@
 set -o pipefail
 
 if command -v appium >/dev/null 2>&1; then
-    APPIUM_CMD="appium"
+	APPIUM_CMD="appium"
 else
-    # Use relative path to access the locally installed appium
-    APPIUM_CMD="../../../node_modules/.bin/appium"
+	# Use relative path to access the locally installed appium
+	APPIUM_CMD="../../../node_modules/.bin/appium"
 fi
 
 output=$($APPIUM_CMD driver list --installed --json)
 
 if echo "$output" | grep -q 'uiautomator2'; then
-    echo '[info] UiAutomator2 is installed, skipping installation.'
+	echo '[info] UiAutomator2 is installed, skipping installation.'
 else
-    echo '[info] UiAutomator2 not found, installing...'
-    $APPIUM_CMD driver install uiautomator2
+	echo '[info] UiAutomator2 not found, installing...'
+	$APPIUM_CMD driver install uiautomator2
 fi
 
 if echo "$output" | grep -q 'xcuitest'; then
-    echo '[info] XCUITest is installed, skipping installation.'
+	echo '[info] XCUITest is installed, skipping installation.'
 else
-    echo '[info] XCUITest not found, installing...'
-    $APPIUM_CMD driver install xcuitest
+	echo '[info] XCUITest not found, installing...'
+	$APPIUM_CMD driver install xcuitest
 fi
 
 # Mitigate conflicts between development server caches and E2E tests
