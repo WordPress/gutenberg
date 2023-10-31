d839e1bb82a3431f10549b45bbaab8269266e6f1
Riad Benguella <benguella@gmail.com>
2023-10-31 09:34:39 +0100

    Do not select any suggetions if shouldFilter is false

--- packages/components/src/combobox-control/index.tsx
+++ packages/components/src/combobox-control/index.tsx
@@ -270,18 +270,26 @@ function ComboboxControl( props: ComboboxControlProps ) {
 
 	// Update current selections when the filter input changes.
 	useEffect( () => {
-		const hasMatchingSuggestions = matchingSuggestions.length > 0;
-		const hasSelectedMatchingSuggestions =
-			getIndexOfMatchingSuggestion(
-				selectedSuggestion,
-				matchingSuggestions
-			) > 0;
-
-		if ( hasMatchingSuggestions && ! hasSelectedMatchingSuggestions ) {
-			// If the current selection isn't present in the list of suggestions, then automatically select the first item from the list of suggestions.
-			setSelectedSuggestion( matchingSuggestions[ 0 ] );
+		if ( ! shouldFilter ) {
+			setSelectedSuggestion( null );
+			return;
 		}
-	}, [ matchingSuggestions, selectedSuggestion ] );
+
+		// If the current selection isn't present in the list of suggestions, then automatically select the first item from the list of suggestions.
+		setSelectedSuggestion( ( previousSuggetion ) => {
+			const hasMatchingSuggestions = matchingSuggestions.length > 0;
+			const hasSelectedMatchingSuggestions =
+				getIndexOfMatchingSuggestion(
+					previousSuggetion,
+					matchingSuggestions
+				) > 0;
+
+			if ( hasMatchingSuggestions && ! hasSelectedMatchingSuggestions ) {
+				return matchingSuggestions[ 0 ];
+			}
+			return previousSuggetion;
+		} );
+	}, [ shouldFilter, matchingSuggestions ] );
 
 	// Announcements.
 	useEffect( () => {
