855663be8677744db33389c93d9270017da092d2
Luis Herranz <luisherranz@gmail.com>
2023-10-31 12:47:19 +0100

    Switch to inverse control

--- packages/block-library/src/query/edit/inspector-controls/enhanced-pagination-control.js
+++ packages/block-library/src/query/edit/inspector-controls/enhanced-pagination-control.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { ToggleControl, Notice } from '@wordpress/components';
+import { ToggleControl, ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -14,59 +14,36 @@ export default function EnhancedPaginationControl( {
 	setAttributes,
 	clientId,
 } ) {
-	const {
-		hasBlocksFromPlugins,
-		hasPostContentBlock,
-		hasPatternOrTemplatePartBlocks,
-	} = useUnsupportedBlocks( clientId );
+	const { hasBlocksFromPlugins, hasPostContentBlock } =
+		useUnsupportedBlocks( clientId );
 
-	const showAuto =
-		! hasBlocksFromPlugins &&
-		! hasPostContentBlock &&
-		hasPatternOrTemplatePartBlocks;
-
-	let notice = null;
-	if ( hasBlocksFromPlugins ) {
-		notice =
-			'Blocks from plugins are not supported yet. For the enhanced pagination to work, remove the blocks, then re-enable "Enhanced pagination" in the Query Block settings.';
-	} else if ( hasPostContentBlock ) {
-		notice = __(
-			'The Post Content block is not supported yet. For the enhanced pagination to work, remove the block, then re-enable "Enhanced pagination" in the Query Block settings.'
-		);
-	} else if ( enhancedPagination && hasPatternOrTemplatePartBlocks ) {
-		notice = __(
-			'Blocks from plugins are not supported yet. Please note that if you add them to the patterns or template parts that are currently inside this Query block, this enhanced pagination will be automatically disabled.'
-		);
-	}
-
-	const label = showAuto
-		? __( 'Enhanced pagination (auto)' )
-		: __( 'Enhanced pagination' );
+	const help = enhancedPagination ? (
+		<>
+			{ __(
+				'Browsing between pages will be seamless unless unexpected content is detected.'
+			) }{ ' ' }
+			<ExternalLink href="https://wordpress.org/">
+				{ __( 'Learn more' ) }
+			</ExternalLink>
+			{ '.' }
+		</>
+	) : (
+		__( 'Browsing between pages requires a full page reload.' )
+	);
 
 	return (
 		<>
 			<ToggleControl
-				label={ label }
-				help={ __(
-					'Browsing between pages won’t require a full page reload.'
-				) }
-				checked={ !! enhancedPagination }
+				label={ __( 'Force page reload' ) }
+				help={ help }
+				checked={ ! enhancedPagination }
 				disabled={ hasBlocksFromPlugins || hasPostContentBlock }
 				onChange={ ( value ) => {
 					setAttributes( {
-						enhancedPagination: !! value,
+						enhancedPagination: ! value,
 					} );
 				} }
 			/>
-			{ notice && (
-				<Notice
-					status="warning"
-					isDismissible={ false }
-					className="wp-block-query__enhanced-pagination-notice"
-				>
-					{ notice }
-				</Notice>
-			) }
 		</>
 	);
 }
