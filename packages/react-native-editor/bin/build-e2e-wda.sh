7cc8941a838c0be465c616a650681f776448ad93
Gerardo <gerardo.pacheco@automattic.com>
2023-11-02 16:20:45 +0100

    E2E Testing - Build E2E WDA - Allow reading the platform version from an environment value

--- packages/react-native-editor/bin/build-e2e-wda.sh
+++ packages/react-native-editor/bin/build-e2e-wda.sh
@@ -5,6 +5,7 @@ set -o pipefail
 # Load configurations from JSON file
 CONFIG_FILE="$(pwd)/__device-tests__/helpers/device-config.json"
 IOS_DEVICE_NAME=$(jq -r '.ios.local.deviceName' "$CONFIG_FILE")
-IOS_PLATFORM_VERSION=$(jq -r '.ios.local.platformVersion' "$CONFIG_FILE")
+# Use the value from the environment variable if set, otherwise use the value from the JSON file
+IOS_PLATFORM_VERSION="${IOS_PLATFORM_VERSION:-$(jq -r '.ios.local.platformVersion' "$CONFIG_FILE")}"
 
 xcodebuild -project ~/.appium/node_modules/appium-xcuitest-driver/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj -scheme WebDriverAgentRunner -destination "platform=iOS Simulator,name=$IOS_DEVICE_NAME,OS=$IOS_PLATFORM_VERSION" -derivedDataPath ios/build/WDA
