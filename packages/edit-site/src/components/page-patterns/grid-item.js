f20ab35bc4a049b2d708330355eeb172d44fcf17
ramon <ramonjd@gmail.com>
2023-11-07 13:44:17 +1100

    Remove IE11-specific code.

--- packages/edit-site/src/components/page-patterns/grid-item.js
+++ packages/edit-site/src/components/page-patterns/grid-item.js
@@ -61,21 +61,13 @@ import { unlock } from '../../lock-unlock';
  */
 function download( fileName, content, contentType ) {
 	const file = new window.Blob( [ content ], { type: contentType } );
-
-	// IE11 can't use the click to download technique
-	// we use a specific IE11 technique instead.
-	if ( window.navigator.msSaveOrOpenBlob ) {
-		window.navigator.msSaveOrOpenBlob( file, fileName );
-	} else {
-		const a = document.createElement( 'a' );
-		a.href = URL.createObjectURL( file );
-		a.download = fileName;
-
-		a.style.display = 'none';
-		document.body.appendChild( a );
-		a.click();
-		document.body.removeChild( a );
-	}
+	const a = document.createElement( 'a' );
+	a.href = URL.createObjectURL( file );
+	a.download = fileName;
+	a.style.display = 'none';
+	document.body.appendChild( a );
+	a.click();
+	document.body.removeChild( a );
 }
 
 const { useGlobalStyle } = unlock( blockEditorPrivateApis );
