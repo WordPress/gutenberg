f7f69376cf3d7de9d9ccd6547a604c98e566be06
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-11-08 08:55:47 +0100

    Set proper spacing/alignment for control

--- packages/edit-site/src/components/dataviews/reset-filters.js
+++ packages/edit-site/src/components/dataviews/reset-filters.js
@@ -1,22 +1,24 @@
 /**
  * WordPress dependencies
  */
-import { Button } from '@wordpress/components';
+import { BaseControl, Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 export default ( { onChangeView } ) => {
 	return (
-		<Button
-			variant="tertiary"
-			onClick={ () => {
-				onChangeView( ( currentView ) => ( {
-					...currentView,
-					page: 1,
-					filters: [],
-				} ) );
-			} }
-		>
-			{ __( 'Reset filters' ) }
-		</Button>
+		<BaseControl>
+			<Button
+				variant="tertiary"
+				onClick={ () => {
+					onChangeView( ( currentView ) => ( {
+						...currentView,
+						page: 1,
+						filters: [],
+					} ) );
+				} }
+			>
+				{ __( 'Reset filters' ) }
+			</Button>
+		</BaseControl>
 	);
 };
