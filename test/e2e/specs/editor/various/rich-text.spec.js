50dde724901910b02f0dbad1bae6af57a1645c64
Ella <ella@vandurpe.com>
2023-11-10 11:16:04 +0200

    Rich text: run e2e tests in Firefox and Webkit

--- test/e2e/specs/editor/various/rich-text.spec.js
+++ test/e2e/specs/editor/various/rich-text.spec.js
@@ -3,7 +3,7 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test.describe( 'RichText', () => {
+test.describe( 'RichText (@firefox, @webkit)', () => {
 	test.beforeEach( async ( { admin } ) => {
 		await admin.createNewPost();
 	} );
