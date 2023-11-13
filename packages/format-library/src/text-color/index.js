f23a040872f74afa6d9a540477bfcc0364ea1a5a
Ella <ella@vandurpe.com>
2023-11-13 08:29:47 +0200

    Rich text: highlight format: gracefully handle old span format

--- packages/format-library/src/text-color/index.js
+++ packages/format-library/src/text-color/index.js
@@ -135,23 +135,5 @@ export const textColor = {
 		style: 'style',
 		class: 'class',
 	},
-	/*
-	 * Since this format relies on the <mark> tag, it's important to
-	 * prevent the default yellow background color applied by most
-	 * browsers. The solution is to detect when this format is used with a
-	 * text color but no background color, and in such cases to override
-	 * the default styling with a transparent background.
-	 *
-	 * @see https://github.com/WordPress/gutenberg/pull/35516
-	 */
-	__unstableFilterAttributeValue( key, value ) {
-		if ( key !== 'style' ) return value;
-		// We should not add a background-color if it's already set.
-		if ( value && value.includes( 'background-color' ) ) return value;
-		const addedCSS = [ 'background-color', transparentValue ].join( ':' );
-		// Prepend `addedCSS` to avoid a double `;;` as any the existing CSS
-		// rules will already include a `;`.
-		return value ? [ addedCSS, value ].join( ';' ) : addedCSS;
-	},
 	edit: TextColorEdit,
 };
