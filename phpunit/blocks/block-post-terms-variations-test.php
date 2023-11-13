37040be8d2487304ea618d9a6c6b5520ec32ac25
Fabian Todt <mail@fabiantodt.at>
2023-11-13 12:18:39 +0100

    add unit test for variations

---
+++ phpunit/blocks/block-post-terms-variations-test.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Post Terms block variations tests.
+ *
+ * @package WordPress
+ * @subpackage Blocks
+ */
+
+/**
+ * Tests for the Post Terms block variations for taxonomies.
+ *
+ * @group blocks
+ */
+class Block_Post_Terms_Variations_Test extends WP_UnitTestCase {
+	public function set_up() {
+		parent::set_up();
+		register_taxonomy(
+			'book_type',
+			array( 'post' ),
+			array(
+				'labels' => array(
+					'name' => 'Book Type',
+				),
+			)
+		);
+	}
+
+	public function tear_down() {
+		unregister_taxonomy( 'book_type' );
+		parent::tear_down();
+	}
+
+	/**
+	 * @covers ::register_block_core_post_terms_variation
+	 */
+	public function test_post_terms_variations_custom_taxonomy() {
+		$registry         = WP_Block_Type_Registry::get_instance();
+		$post_terms_block = $registry->get_registered( 'core/post-terms' );
+		$this->assertNotEmpty( $post_terms_block->variations, 'Block has no variations' );
+		$variation = $this->get_variation_by_name( 'book_type', $post_terms_block->variations );
+		$this->assertIsArray( $variation, 'Block variation is not an array' );
+		$this->assertArrayHasKey( 'title', $variation, 'Block variation has no title' );
+		$this->assertEquals( 'Book Type', $variation['title'], 'Variation title is different than the taxonomy label' );
+	}
+
+	/**
+	 * Get a variation by its name from an array of variations.
+	 *
+	 * @param string $variation_name The name (= slug) of the variation.
+	 * @param array  $variations An array of variations.
+	 * @return array|null The found variation or null.
+	 */
+	private function get_variation_by_name( $variation_name, $variations ) {
+		$found_variation = null;
+		foreach ( $variations as $variation ) {
+			if ( $variation['name'] === $variation_name ) {
+				$found_variation = $variation;
+			}
+		}
+
+		return $found_variation;
+	}
+}
