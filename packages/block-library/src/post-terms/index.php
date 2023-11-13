d5bd454324bd4a7860f33d40c8808bda7606cdb8
Fabian Todt <mail@fabiantodt.at>
2023-11-13 12:18:33 +0100

    register variations on taxonomy registration

--- packages/block-library/src/post-terms/index.php
+++ packages/block-library/src/post-terms/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Server-side rendering of the `core/post-terms` block.
+ * Server-side registering and rendering of the `core/post-terms` block.
  *
  * @package WordPress
  */
@@ -59,11 +59,38 @@ function render_block_core_post_terms( $attributes, $content, $block ) {
 }
 
 /**
- * Returns the available variations for the `core/post-terms` block.
+ * Returns a post terms variation
  *
- * @return array The available variations for the block.
+ * @param WP_Taxonomy|stdClass $taxonomy taxonomy entity.
+ *
+ * @return array
  */
-function build_post_term_block_variations() {
+function block_core_post_terms_build_variation_for_post_terms( $taxonomy ) {
+	$variation = array(
+		'name'        => $taxonomy->name,
+		'title'       => $taxonomy->label,
+		'description' => sprintf(
+			/* translators: %s: taxonomy's label */
+			__( 'Display a list of assigned terms from the taxonomy: %s' ),
+			$taxonomy->label
+		),
+		'attributes'  => array(
+			'term' => $taxonomy->name,
+		),
+		'isActive'    => array( 'term' ),
+		'scope'       => array( 'inserter', 'transform' ),
+	);
+
+	return $variation;
+}
+
+/**
+ * Registers the `core/post-terms` block on the server.
+ */
+function register_block_core_post_terms() {
+	// This will only handle taxonomies registered until this point (init on priority 9).
+	// See action hooks below for other taxonomies.
+	// See https://github.com/WordPress/gutenberg/issues/52569 for details.
 	$taxonomies = get_taxonomies(
 		array(
 			'publicly_queryable' => true,
@@ -80,20 +107,7 @@ function build_post_term_block_variations() {
 
 	// Create and register the eligible taxonomies variations.
 	foreach ( $taxonomies as $taxonomy ) {
-		$variation = array(
-			'name'        => $taxonomy->name,
-			'title'       => $taxonomy->label,
-			'description' => sprintf(
-				/* translators: %s: taxonomy's label */
-				__( 'Display a list of assigned terms from the taxonomy: %s' ),
-				$taxonomy->label
-			),
-			'attributes'  => array(
-				'term' => $taxonomy->name,
-			),
-			'isActive'    => array( 'term' ),
-			'scope'       => array( 'inserter', 'transform' ),
-		);
+		$variation = block_core_post_terms_build_variation_for_post_terms( $taxonomy );
 		// Set the category variation as the default one.
 		if ( 'category' === $taxonomy->name ) {
 			$variation['isDefault'] = true;
@@ -121,3 +135,31 @@ function register_block_core_post_terms() {
 	);
 }
 add_action( 'init', 'register_block_core_post_terms' );
+// Register actions for all taxonomies, to add variations when they are registered.
+// All taxonomies registered before register_block_core_post_terms, will be handled by that function.
+add_action( 'registered_taxonomy', 'register_block_core_post_terms_taxonomy_variation', 10, 3 );
+
+/**
+ * Register a custom taxonomy variation for post terms on taxonomy registration
+ * Handles all taxonomies registered after the block is registered in register_block_core_post_terms
+ *
+ * @param string       $taxonomy Taxonomy slug.
+ * @param array|string $object_type Object type or array of object types.
+ * @param array        $args Array of taxonomy registration arguments.
+ * @return void
+ */
+function register_block_core_post_terms_taxonomy_variation( $taxonomy, $object_type, $args ) {
+	if ( isset( $args['show_in_nav_menus'] ) && $args['show_in_nav_menus'] ) {
+		$variation = block_core_post_terms_build_variation_for_post_terms( (object) $args );
+		// Directly set the variations on the registered block type
+		// because there's no server side registration for variations (see #47170).
+		$post_terms_block_type = WP_Block_Type_Registry::get_instance()->get_registered( 'core/post-terms' );
+		// If the block is not registered yet, bail early.
+		// Variation will be registered in register_block_core_post_terms then.
+		if ( ! $post_terms_block_type ) {
+			return;
+		}
+
+		$post_terms_block_type->variations[] = $variation;
+	}
+}
