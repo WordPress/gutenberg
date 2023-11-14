85fc9ac078bc9f1f4fdd7866713a31650056fed1
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2023-11-14 14:19:56 +1000

    Add overly simplistic control to set section attribute

--- packages/block-editor/src/hooks/section.js
+++ packages/block-editor/src/hooks/section.js
@@ -7,7 +7,21 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import { getBlockSupport } from '@wordpress/blocks';
+import {
+	TextControl,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+} from '@wordpress/components';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { useCallback } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import InspectorControls from '../components/inspector-controls';
+import useDisplayBlockControls from '../components/use-display-block-controls';
+import { useBlockEditingMode } from '..';
 
 export const SECTION_SUPPORT_KEY = 'section';
 
@@ -65,10 +79,10 @@ function addEditProps( settings ) {
 			return props;
 		}
 
-		const newClassName = classnames(
-			props.className,
-			`wp-section-${ attributes.section }`
-		);
+		// TODO: Handle fallback logic if the current section index isn't available.
+		const sectionClass = `wp-section-${ attributes.section }`;
+		const newClassName = classnames( props.className, sectionClass );
+
 		props.className = newClassName ? newClassName : undefined;
 
 		return props;
@@ -77,13 +91,91 @@ function addEditProps( settings ) {
 	return settings;
 }
 
+function SectionPanelItem( props ) {
+	const { attributes, clientId, setAttributes } = props;
+
+	const resetAllFilter = useCallback( ( previousValue ) => {
+		return {
+			...previousValue,
+			section: undefined,
+		};
+	}, [] );
+
+	return (
+		<ToolsPanelItem
+			hasValue={ () => !! attributes.section }
+			label={ __( 'Section' ) }
+			onDeselect={ () => setAttributes( { section: undefined } ) }
+			isShownByDefault={ true }
+			resetAllFilter={ resetAllFilter }
+			panelId={ clientId }
+		>
+			<TextControl
+				__nextHasNoMarginBottom
+				label={ __( 'Index' ) }
+				value={ attributes.section ?? '' }
+				onChange={ ( nextValue ) => {
+					setAttributes( {
+						section:
+							nextValue !== ''
+								? parseInt( nextValue, 10 )
+								: undefined,
+					} );
+				} }
+			/>
+		</ToolsPanelItem>
+	);
+}
+
+function SectionPanel( props ) {
+	// TODO: Add theme.json setting to disable section styling.
+
+	if ( ! hasSectionSupport( props.name ) ) {
+		return null;
+	}
+
+	return (
+		<InspectorControls group="section">
+			<SectionPanelItem { ...props } />
+		</InspectorControls>
+	);
+}
+
+const withSectionControls = createHigherOrderComponent(
+	( BlockEdit ) => ( props ) => {
+		if ( ! hasSectionSupport( props.name ) ) {
+			return <BlockEdit key="edit" { ...props } />;
+		}
+
+		const shouldDisplayControls = useDisplayBlockControls();
+		const blockEditingMode = useBlockEditingMode();
+
+		return (
+			<>
+				{ shouldDisplayControls && blockEditingMode === 'default' && (
+					<SectionPanel { ...props } />
+				) }
+				<BlockEdit key="edit" { ...props } />
+			</>
+		);
+	},
+	'withSectionControls'
+);
+
 addFilter(
 	'blocks.registerBlockType',
 	'core/section/addAttribute',
 	addAttribute
 );
+
 addFilter(
 	'blocks.registerBlockType',
 	'core/section/addEditProps',
 	addEditProps
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'core/section/with-section-controls',
+	withSectionControls
 );
