251aed66f7e8ff655ee8945c50477e1695b3e092
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-11-15 15:56:12 +0100

    Only replace the value if the current context is "edit".

--- packages/block-library/src/post-excerpt/index.php
+++ packages/block-library/src/post-excerpt/index.php
@@ -94,6 +94,10 @@ function register_block_core_post_excerpt_length_filter( $value ) {
 		return $value;
 	}
 
+	if ( empty( $_REQUEST['context'] ) || ( 'edit' !== $_REQUEST['context'] ) ) {
+		return $value;
+	}
+
 	return 100;
 }
 add_filter( 'excerpt_length', 'register_block_core_post_excerpt_length_filter', PHP_INT_MAX );
