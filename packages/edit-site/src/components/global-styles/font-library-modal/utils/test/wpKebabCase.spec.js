a35475d53c9e9ef86e533cb2edd6be9702e422fc
Vicente Canales <vicente.canales@automattic.com>
2023-11-16 13:23:36 -0300

    Add tests for wpKebabCase

---
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/test/wpKebabCase.spec.js
@@ -0,0 +1,30 @@
+/**
+ * Internal dependencies
+ */
+import { wpKebabCase } from '../index';
+
+describe( 'wpKebabCase', () => {
+	it( 'should insert a dash between a letter and a digit', () => {
+		const input = 'abc1def';
+		const expectedOutput = 'abc-1def';
+		expect( wpKebabCase( input ) ).toEqual( expectedOutput );
+	} );
+
+	it( 'should insert a dash between a digit and a letter', () => {
+		const input = 'abc1def2ghi';
+		const expectedOutput = 'abc-1def-2ghi';
+		expect( wpKebabCase( input ) ).toEqual( expectedOutput );
+	} );
+
+	it( 'should not insert a dash between two letters', () => {
+		const input = 'abcdef';
+		const expectedOutput = 'abcdef';
+		expect( wpKebabCase( input ) ).toEqual( expectedOutput );
+	} );
+
+	it( 'should not insert a dash between a digit and a hyphen', () => {
+		const input = 'abc1-def';
+		const expectedOutput = 'abc-1-def';
+		expect( wpKebabCase( input ) ).toEqual( expectedOutput );
+	} );
+} );
