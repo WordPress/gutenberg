447a6de8eb79f307921a22fb8814cc01affded46
Riad Benguella <benguella@gmail.com>
2023-11-20 14:05:51 +0100

    Extract a PostPanelRow component from the different sidebar panels (#56238)

--- packages/editor/src/private-apis.js
+++ packages/editor/src/private-apis.js
@@ -5,11 +5,13 @@ import { ExperimentalEditorProvider } from './components/provider';
 import { lock } from './lock-unlock';
 import { EntitiesSavedStatesExtensible } from './components/entities-saved-states';
 import useBlockEditorSettings from './components/provider/use-block-editor-settings';
+import PostPanelRow from './components/post-panel-row';
 
 export const privateApis = {};
 lock( privateApis, {
 	ExperimentalEditorProvider,
 	EntitiesSavedStatesExtensible,
+	PostPanelRow,
 
 	// This is a temporary private API while we're updating the site editor to use EditorProvider.
 	useBlockEditorSettings,
