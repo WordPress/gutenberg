86a463024ba349565ea4f82821648dc0dff36c81
Jorge Costa <jorge.costa@developer.pt>
2023-11-20 16:46:38 +0000

    Dataviews: All templates: Add: Sorting to template author.

--- lib/compat/wordpress-6.5/rest-api.php
+++ lib/compat/wordpress-6.5/rest-api.php
@@ -19,3 +19,59 @@ function gutenberg_register_global_styles_revisions_endpoints() {
 }
 
 add_action( 'rest_api_init', 'gutenberg_register_global_styles_revisions_endpoints' );
+
+function gutenberg_get_wp_templates_author_text_field( $template_object ) {
+	if ( 'wp_template' === $template_object['type'] || 'wp_template_part' === $template_object['type'] ) {
+		// Added by theme.
+		// Template originally provided by a theme, but customized by a user.
+		// Templates originally didn't have the 'origin' field so identify
+		// older customized templates by checking for no origin and a 'theme'
+		// or 'custom' source.
+		if ( $template_object['has_theme_file'] &&
+			( 'theme' === $template_object['origin'] || (
+				empty( $template_object['origin'] ) && in_array( $template_object['source'], array(
+					'theme',
+					'custom',
+				), true ) )
+			)
+		) {
+			$theme_name = wp_get_theme( $template_object['theme'] )->get( 'Name' );
+			return empty( $theme_name ) ? $template_object['theme'] : $theme_name;
+		}
+
+		// Added by plugin.
+		if ( $template_object['has_theme_file'] && $template_object['origin'] === 'plugin' ) {
+			$plugins = get_plugins();
+			$plugin = $plugins[ plugin_basename( sanitize_text_field( $template_object['theme'] . '.php' ) ) ];
+			return empty( $plugin['Name'] ) ? $template_object['theme'] : $plugin['Name'];
+		}
+
+		// Added by site.
+		// Template was created from scratch, but has no author. Author support
+		// was only added to templates in WordPress 5.9. Fallback to showing the
+		// site logo and title.
+		if ( empty( $template_object['has_theme_file'] ) && $template_object['source'] === 'custom' && empty( $template_object['author'] ) ) {
+			return get_bloginfo( 'name' );
+		}
+	}
+
+	// Added by user.
+	return get_user_by( 'id', $template_object['author'] );
+}
+
+/**
+ * Registers the Global Styles Revisions REST API routes.
+ */
+function gutenberg_register_wp_templates_author_text_field() {
+	register_rest_field(
+		'wp_template',
+		'author_text',
+		array(
+			'get_callback'    => 'gutenberg_get_wp_templates_author_text_field',
+			'update_callback' => null,
+			'schema'          => null,
+		)
+	);
+}
+
+add_action( 'rest_api_init', 'gutenberg_register_wp_templates_author_text_field' );
--- packages/edit-site/src/components/page-templates/dataviews-templates.js
+++ packages/edit-site/src/components/page-templates/dataviews-templates.js
@@ -177,6 +177,15 @@ export default function DataviewsTemplates() {
 						: titleB.localeCompare( titleA );
 				} );
 			}
+			if ( view.sort.field === 'author' ) {
+				filteredTemplates.sort( ( a, b ) => {
+					const authorA = a.author_text;
+					const authorB = b.author_text;
+					return view.sort.direction === 'asc'
+						? authorA.localeCompare( authorB )
+						: authorB.localeCompare( authorA );
+				} );
+			}
 		}
 		// Handle pagination.
 		const start = ( view.page - 1 ) * view.perPage;
@@ -237,9 +246,12 @@ export default function DataviewsTemplates() {
 			{
 				header: __( 'Author' ),
 				id: 'author',
-				render: ( { item } ) => <AuthorField item={ item } />,
+				getValue: ( { item } ) => item.author_text,
+				render: ( { item } ) => {
+					return <AuthorField item={ item } />;
+				},
 				enableHiding: false,
-				enableSorting: false,
+				enableSorting: true,
 			},
 		],
 		[]
