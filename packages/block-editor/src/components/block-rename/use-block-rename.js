95c006a8f5a11a837ce1a53c065cdc7441393c6a
Dave Smith <getdavemail@gmail.com>
2023-11-21 14:46:59 +0000

    Update support hook to check correct support value

--- packages/block-editor/src/components/block-rename/use-block-rename.js
+++ packages/block-editor/src/components/block-rename/use-block-rename.js
@@ -4,11 +4,7 @@
 import { getBlockSupport } from '@wordpress/blocks';
 
 export default function useBlockRename( name ) {
-	const metaDataSupport = getBlockSupport(
-		name,
-		'__experimentalMetadata',
-		false
-	);
+	const metaDataSupport = getBlockSupport( name, 'renaming', true );
 
 	const supportsBlockNaming = !! (
 		true === metaDataSupport || metaDataSupport?.name
