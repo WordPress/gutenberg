05dd1123bc425e62e8988da284fc24413a651c79
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-11-22 17:10:30 +0100

    top-level filter: make value an array

--- packages/edit-site/src/components/dataviews/in-filter.js
+++ packages/edit-site/src/components/dataviews/in-filter.js
@@ -22,23 +22,30 @@ const {
 
 export default ( { filter, view, onChangeView } ) => {
 	const filterInView = view.filters.find( ( f ) => f.field === filter.field );
-	const activeElement = filter.elements.find(
-		( element ) => element.value === filterInView?.value
-	);
+	const activeElementLabels = filter.elements
+		.filter( ( element ) => filterInView?.value?.includes( element.value ) )
+		.map( ( element ) => element.label );
 
 	return (
 		<DropdownMenu
 			key={ filter.field }
 			trigger={
 				<Button variant="tertiary" size="compact" label={ filter.name }>
-					{ activeElement !== undefined
-						? sprintf(
-								/* translators: 1: Filter name. 2: filter value. e.g.: "Author is Admin". */
-								__( '%1$s is %2$s' ),
-								filter.name,
-								activeElement.label
-						  )
-						: filter.name }
+					{ activeElementLabels.length === 0 && filter.name }
+					{ activeElementLabels.length === 1 &&
+						sprintf(
+							/* translators: 1: Filter name. 2: filter value. e.g.: "Author is Admin". */
+							__( '%1$s is %2$s' ),
+							filter.name,
+							activeElementLabels[ 0 ]
+						) }
+					{ activeElementLabels.length > 1 &&
+						sprintf(
+							/* translators: 1: Filter name. 2: filter value. e.g.: "Author in Admin, Editor". */
+							__( '%1$s in %2$s' ),
+							filter.name,
+							activeElementLabels.join( ',' )
+						) }
 					<Icon icon={ chevronDown } style={ { flexShrink: 0 } } />
 				</Button>
 			}
@@ -48,8 +55,11 @@ export default ( { filter, view, onChangeView } ) => {
 					<DropdownMenuCheckboxItem
 						key={ element.value }
 						value={ element.value }
-						checked={ activeElement?.value === element.value }
-						onSelect={ () =>
+						checked={ activeElementLabels.includes(
+							element.label
+						) }
+						onSelect={ ( event ) => {
+							event.preventDefault();
 							onChangeView( ( currentView ) => ( {
 								...currentView,
 								page: 1,
@@ -60,15 +70,21 @@ export default ( { filter, view, onChangeView } ) => {
 									{
 										field: filter.field,
 										operator: OPERATOR_IN,
-										value:
-											activeElement?.value ===
+										value: filterInView.value.includes(
 											element.value
-												? undefined
-												: element.value,
+										)
+											? filterInView.value.filter(
+													( value ) =>
+														value !== element.value
+											  )
+											: [
+													...filterInView.value,
+													element.value,
+											  ],
 									},
 								],
-							} ) )
-						}
+							} ) );
+						} }
 					>
 						{ element.label }
 					</DropdownMenuCheckboxItem>
