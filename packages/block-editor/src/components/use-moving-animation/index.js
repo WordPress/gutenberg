2b7a5f9f81143f8350b007425531e73c63abd24b
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-11-22 12:20:22 +1100

    useMovingAnimation: Clear translate3d rule when animation is finished

--- packages/block-editor/src/components/use-moving-animation/index.js
+++ packages/block-editor/src/components/use-moving-animation/index.js
@@ -129,7 +129,7 @@ function useMovingAnimation( {
 		const finishedMoving = x === 0 && y === 0;
 		ref.current.style.transformOrigin = 'center center';
 		ref.current.style.transform = finishedMoving
-			? undefined
+			? null
 			: `translate3d(${ x }px,${ y }px,0)`;
 		ref.current.style.zIndex = isSelected ? '1' : '';
 
