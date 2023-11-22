cf6942581a378c1c6f94a6e2652ab52582955945
Jerry Jones <jones.jeremydavid@gmail.com>
2023-11-22 15:38:47 -0600

    Only use block popover on large screens

--- packages/block-editor/src/components/block-tools/index.js
+++ packages/block-editor/src/components/block-tools/index.js
@@ -196,6 +196,7 @@ export default function BlockTools( {
 				) }
 
 				{ ! hasFixedToolbar &&
+					isLargeViewport &&
 					! showEmptyBlockSideInserter &&
 					hasSelectedBlock &&
 					! isEmptyDefaultBlock && (
