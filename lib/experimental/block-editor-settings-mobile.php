ce92b82b111b9d98a3f8ad9eb84c09abe83b5521
Matthew Kevins <mmkevins@yahoo.com>
2023-11-24 14:13:27 +1000

    Extract mobile editor settings filter to a separate file
    The important part of this is that the filter is a separate filter.
    Incidentally, this also fixes a bug in which the previous draft
    implementation returned a filtered response even for non-mobile
    contexts.

--- lib/experimental/block-editor-settings-mobile.php
+++ lib/experimental/block-editor-settings-mobile.php
@@ -5,110 +5,6 @@
  * @package gutenberg
  */
 
-if ( ! function_exists( 'filter_by_supported_block_editor_settings_mobile' ) ) {
-	/**
-	 * Keeps only supported settings for the mobile block editor.
-	 *
-	 * This is used to control the editor settings payload. Keys can be specified
-	 * as `true` to be allowed, which will also allow entire nested structures.
-	 * Alternatively, nested structures can have nested allow-lists for their keys.
-	 *
-	 * @param array $initial_array Existing block editor settings.
-	 *
-	 * @param array $allow_list_array Structured allow-list.
-	 *
-	 * @return array New block editor settings.
-	 */
-	function filter_by_supported_block_editor_settings_mobile( $initial_array, $allow_list_array ) {
-		$result = array();
-
-		foreach ( $allow_list_array as $key => $value ) {
-			if ( ! array_key_exists( $key, $initial_array ) ) {
-				continue;
-			}
-
-			$initial_value = $initial_array[ $key ];
-
-			if ( array_key_exists( $key, $initial_array ) ) {
-				if ( is_array( $value ) && is_array( $initial_value ) ) {
-					$result[ $key ] = filter_by_supported_block_editor_settings_mobile( $initial_value, $value );
-				} else {
-					$result[ $key ] = $initial_value;
-				}
-			}
-		}
-
-		return $result;
-	}
-}
-
-if ( ! defined( 'BLOCK_EDITOR_SETTINGS_MOBILE_ALLOW_LIST' ) ) {
-	define(
-		'BLOCK_EDITOR_SETTINGS_MOBILE_ALLOW_LIST',
-		array(
-			'alignWide'                        => true,
-			'allowedBlockTypes'                => true,
-			'allowedMimeTypes'                 => true,
-			'defaultEditorStyles'              => true,
-			'blockCategories'                  => true,
-			'isRTL'                            => true,
-			'imageDefaultSize'                 => true,
-			'imageDimensions'                  => true,
-			'imageEditing'                     => true,
-			'imageSizes'                       => true,
-			'maxUploadFileSize'                => true,
-			'__unstableGalleryWithImageBlocks' => true,
-			'disableCustomColors'              => true,
-			'disableCustomFontSizes'           => true,
-			'disableCustomGradients'           => true,
-			'disableLayoutStyles'              => true,
-			'enableCustomLineHeight'           => true,
-			'enableCustomSpacing'              => true,
-			'enableCustomUnits'                => true,
-			'colors'                           => true,
-			'fontSizes'                        => true,
-			'__experimentalStyles'             => array(
-				'elements'   => true,
-				'spacing'    => true,
-				'blocks'     => true,
-				'color'      => true,
-				'typography' => true,
-			),
-			'__experimentalFeatures'           => array(
-				'appearanceTools'               => true,
-				'useRootPaddingAwareAlignments' => true,
-				'border'                        => true,
-				'color'                         => true,
-				'shadow'                        => true,
-				'spacing'                       => true,
-				'typography'                    => array(
-					'dropCap'        => true,
-					'fontSizes'      => true,
-					'fontStyle'      => true,
-					'fontWeight'     => true,
-					'letterSpacing'  => true,
-					'textColumns'    => true,
-					'textDecoration' => true,
-					'textTransform'  => true,
-					'writingMode'    => true,
-				),
-				'blocks'                        => true,
-				'background'                    => true,
-				'dimensions'                    => true,
-				'position'                      => true,
-			),
-			'gradients'                        => true,
-			'disableCustomSpacingSizes'        => true,
-			'spacingSizes'                     => true,
-			'__unstableIsBlockBasedTheme'      => true,
-			'localAutosaveInterval'            => true,
-			'__experimentalDiscussionSettings' => true,
-			'__experimentalEnableQuoteBlockV2' => true,
-			'__experimentalEnableListBlockV2'  => true,
-		)
-	);
-}
-
 /**
  * Adds settings to the mobile block editor.
  *
@@ -137,7 +33,7 @@ function gutenberg_get_block_editor_settings_mobile( $settings ) {
 		$settings['__experimentalEnableListBlockV2'] = true;
 	}
 
-	return filter_by_supported_block_editor_settings_mobile( $settings, BLOCK_EDITOR_SETTINGS_MOBILE_ALLOW_LIST );
+	return $settings;
 }
 
 add_filter( 'block_editor_settings_all', 'gutenberg_get_block_editor_settings_mobile', PHP_INT_MAX );
