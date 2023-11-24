9753a14fcc5392838400525d36da09e25fdada97
Luis Herranz <luisherranz@gmail.com>
2023-11-24 12:14:42 +0100

    Add basic ToggleGroupControl

--- packages/block-library/src/bug/edit.js
+++ packages/block-library/src/bug/edit.js
@@ -1,14 +1,34 @@
 /**
  * WordPress dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+	PanelBody,
+} from '@wordpress/components';
 
 export default function Test( { attributes, setAttributes } ) {
 	const { testAttribute } = attributes;
 	const blockProps = useBlockProps();
 	return (
 		<>
-			<div { ...blockProps }>hi</div>
+			<InspectorControls group="list">
+				<PanelBody title={ null }>
+					<ToggleGroupControl
+						label="Bug"
+						value={ testAttribute }
+						onChange={ ( value ) =>
+							setAttributes( { testAttribute: value } )
+						}
+					>
+						<ToggleGroupControlOption value="off" label="OFF" />
+						<ToggleGroupControlOption value="on" label="ON" />
+					</ToggleGroupControl>
+				</PanelBody>
+			</InspectorControls>
+
+			<div { ...blockProps }>Bug</div>
 		</>
 	);
 }
