0a1e7bd139b989a1bac6f97bfec469616df3213b
Matthew Kevins <mmkevins@yahoo.com>
2023-11-24 14:15:04 +1000

    Load mobile-specific filter for the editor settings endpoint

--- lib/load.php
+++ lib/load.php
@@ -127,6 +127,7 @@ require __DIR__ . '/compat/wordpress-6.5/class-wp-navigation-block-renderer.php'
 
 // Experimental features.
 require __DIR__ . '/experimental/block-editor-settings-mobile.php';
+require __DIR__ . '/experimental/block-editor-settings-mobile-allowed.php';
 require __DIR__ . '/experimental/blocks.php';
 require __DIR__ . '/experimental/navigation-theme-opt-in.php';
 require __DIR__ . '/experimental/kses.php';
