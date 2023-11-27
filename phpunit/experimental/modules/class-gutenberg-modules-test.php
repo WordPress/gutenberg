6d8d965f966d5b7156af2081de0e309e7ce76f53
Carlos Bravo <carlos.bravo@automattic.com>
2023-11-27 18:42:14 +0100

    Add unit tests

---
+++ phpunit/experimental/modules/class-gutenberg-modules-test.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Tests Gutenberg_Classic_To_Block_Menu_Converter
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tests for the Gutenberg_Classic_To_Block_Menu_Converter_Test class.
+ */
+class Gutenberg_Modules_Test extends WP_UnitTestCase {
+	public function test_register() {
+		// Prepare data for the test.
+		$module_identifier = 'test_module';
+		$src               = 'test_src';
+		$dependencies      = array(
+			'static'  => array( 'dep1' ),
+			'dynamic' => array( 'dep2' ),
+		);
+		$version           = '1.0.0';
+
+		// Call the function with test data.
+		Gutenberg_Modules::register( $module_identifier, $src, $dependencies, $version );
+
+		// Use reflection to access the private static property $registered.
+		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
+		$property   = $reflection->getProperty( 'registered' );
+		$property->setAccessible( true );
+		$registered = $property->getValue();
+
+		// Assert that the module was registered correctly.
+		$this->assertArrayHasKey( $module_identifier, $registered );
+		$this->assertSame( $src, $registered[ $module_identifier ]['src'] );
+		$this->assertSame( $version, $registered[ $module_identifier ]['version'] );
+		$this->assertSame( $dependencies, $registered[ $module_identifier ]['dependencies'] );
+	}
+
+	public function test_enqueue() {
+		// Prepare data for the test.
+		$module_identifier = 'test_module_enqueued';
+
+		// Call the function with test data.
+		Gutenberg_Modules::enqueue( $module_identifier );
+
+		// Use reflection to access the private static property $enqueued.
+		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
+		$property   = $reflection->getProperty( 'enqueued' );
+		$property->setAccessible( true );
+		$enqueued = $property->getValue();
+
+		// Assert that the module was enqueued correctly.
+		$this->assertContains( $module_identifier, $enqueued );
+	}
+
+	public function test_get_dependencies() {
+		// Prepare data for the test.
+		$module_identifier  = 'test_module';
+		$dependency         = 'dependency_module';
+		$registered_modules = array(
+			$module_identifier => array(
+				'dependencies' => array(
+					'static'  => array( $dependency ),
+					'dynamic' => array(
+						'test_dynamic',
+					),
+				),
+			),
+			$dependency        => array(
+				'dependencies' => array(
+					'static'  => array( 'test_static' ),
+					'dynamic' => array( 'test_dynamic' ),
+				),
+			),
+		);
+
+		// Use reflection to modify the private static property $registered.
+		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
+		$property   = $reflection->getProperty( 'registered' );
+		$property->setAccessible( true );
+		$property->setValue( $registered_modules );
+
+		// Use reflection to call the private method get_dependencies.
+		$method = $reflection->getMethod( 'get_dependencies' );
+		$method->setAccessible( true );
+		$dependencies = $method->invokeArgs( null, array( array( $module_identifier ) ) );
+		// Assert that the dependencies are correct.
+		$this->assertArrayHasKey( $dependency, $dependencies );
+		$this->assertSame( $registered_modules[ $dependency ]['dependencies'], $dependencies['dependency_module']['dependencies'] );
+	}
+
+	public function test_get_version_query_string() {
+		// Prepare data for the test.
+		$version = '1.0.0';
+		// Use reflection to call the private method get_version_query_string.
+		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
+		$method     = $reflection->getMethod( 'get_version_query_string' );
+		$method->setAccessible( true );
+		$version_query_string = $method->invokeArgs( null, array( $version ) );
+
+		// Assert that the version query string is correct.
+		$this->assertSame( '?ver=' . $version, $version_query_string );
+
+		// Version is false.
+		$version_query_string = $method->invokeArgs( null, array( false ) );
+		$this->assertSame( '?ver=' . get_bloginfo( 'version' ), $version_query_string );
+		// Default.
+		$version_query_string = $method->invokeArgs( null, array( null ) );
+		$this->assertSame( '', $version_query_string );
+	}
+}
