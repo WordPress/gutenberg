c7cf21d95f930eb0dac1c33d86fc64771bfec838
James Koster <james@jameskoster.co.uk>
2023-11-28 13:13:47 +0000

    Update trash icon

--- packages/icons/src/library/trash.js
+++ packages/icons/src/library/trash.js
@@ -5,10 +5,10 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const trash = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path 
-			fillRule="evenodd" 
-			clipRule="evenodd" 
-			d="M15.75 7H19v1.5h-1.25V18A1.75 1.75 0 0 1 16 19.75H8A1.75 1.75 0 0 1 6.25 18V8.5H5V7h3.25V6c0-.966.784-1.75 1.75-1.75h4c.966 0 1.75.784 1.75 1.75v1Zm-6 0V6a.25.25 0 0 1 .25-.25h4a.25.25 0 0 1 .25.25v1h-4.5Zm-2 1.5V18c0 .138.112.25.25.25h8a.25.25 0 0 0 .25-.25V8.5h-8.5ZM9.5 10H11v7H9.5v-7Zm5 0H13v7h1.5v-7Z"
+		<Path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M8.325 7H5v1.5h14V7h-3.325a3.751 3.751 0 0 0-7.35 0Zm1.553 0h4.244a2.251 2.251 0 0 0-4.244 0ZM6.25 17v-7h1.5v7c0 .69.56 1.25 1.25 1.25h6c.69 0 1.25-.56 1.25-1.25v-7h1.5v7A2.75 2.75 0 0 1 15 19.75H9A2.75 2.75 0 0 1 6.25 17Z"
 		/>
 	</SVG>
 );
