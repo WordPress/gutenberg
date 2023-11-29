5181df36e8998aa064c902269a0ffafb83e71fcb
Jerry Jones <jones.jeremydavid@gmail.com>
2023-11-29 16:53:04 -0600

    Remove isFixed from BlockToolbar component
    Huge change. I'm sorry. Completely reworked the CSS to designate a fixed toolbar as the baseline with the popover styles as overrides.
    
    Moved top toolbar specific styles into the headers of the respective areas rather than bundled with block toolbar.

--- packages/edit-post/src/components/header/index.js
+++ packages/edit-post/src/components/header/index.js
@@ -127,11 +127,7 @@ function Header( {
 								}
 							) }
 						>
-							<BlockToolbar
-								isFixed
-								hideDragHandle
-								variant="unstyled"
-							/>
+							<BlockToolbar hideDragHandle variant="unstyled" />
 						</div>
 						<Popover.Slot
 							ref={ blockToolbarRef }
