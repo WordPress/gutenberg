3cd253a9af2a65a2535ca98b31471be5b5f91f39
Carlos Bravo <carlos.bravo@automattic.com>
2023-11-29 13:36:04 +0100

    Update test to be more output oriented

--- phpunit/experimental/modules/class-gutenberg-modules-test.php
+++ phpunit/experimental/modules/class-gutenberg-modules-test.php
@@ -10,116 +10,57 @@
  */
 class Gutenberg_Modules_Test extends WP_UnitTestCase {
 
-	public function get_test_data() {
-		return array(
-			'parent_module'         => 'module_1',
-			'parent_module_src'     => 'module_1_src',
-			'dependant_module'      => 'module_2',
-			'dependant_module_src'  => 'module_2_src',
-			'dependant_module_deps' => array(
-				'module_1',
-			),
-			'version'               => '1.0.0',
-			'empty_deps'            => array(),
-		);
+	protected $old_wp_scripts;
+	protected $old_modules_markup;
+
+	public function set_up() {
+		parent::set_up();
+
+		$this->old_wp_scripts = isset( $GLOBALS['wp_scripts'] ) ? $GLOBALS['wp_scripts'] : null;
+		remove_action( 'wp_default_scripts', 'wp_default_scripts' );
+		remove_action( 'wp_default_scripts', 'wp_default_packages' );
+		$GLOBALS['wp_scripts']    = new WP_Scripts();
+		$this->old_modules_markup = get_echo( array( 'Gutenberg_Modules', 'print_enqueued_modules' ) );
 	}
 
-	public function test_register() {
-		$test_data         = $this->get_test_data();
-		$module_identifier = $test_data['parent_module'];
-		$src               = $test_data['parent_module_src'];
-		$dependencies      = $test_data['empty_deps'];
-		$version           = $test_data['version'];
-
-		gutenberg_register_module( $module_identifier, $src, $dependencies, $version );
-
-		// Use reflection to access the private static property $registered.
-		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
-		$property   = $reflection->getProperty( 'registered' );
-		$property->setAccessible( true );
-		$registered = $property->getValue();
-
-		// Assert that the module was registered correctly.
-		$this->assertArrayHasKey( $module_identifier, $registered );
-		$this->assertSame( $src, $registered[ $module_identifier ]['src'] );
-		$this->assertSame( $version, $registered[ $module_identifier ]['version'] );
-		$this->assertSame( $dependencies, array() );
+	public function tear_down() {
+		$GLOBALS['wp_scripts'] = $this->old_wp_scripts;
+		add_action( 'wp_default_scripts', 'wp_default_scripts' );
+		parent::tear_down();
 	}
 
-	public function test_enqueue() {
-		$test_data         = $this->get_test_data();
-		$module_identifier = $test_data['parent_module'];
+	public function test_wp_enqueue_module() {
+		global $wp_version;
+		gutenberg_register_module( 'no-deps-no-version', 'interactivity-api-1.js' );
+		gutenberg_enqueue_module( 'no-deps-no-version' );
+		gutenberg_register_module( 'deps-no-version', 'interactivity-api-2.js', array( 'no-deps-no-version' ) );
+		gutenberg_enqueue_module( 'deps-no-version' );
 
-		gutenberg_enqueue_module( $module_identifier );
+		$modules_markup    = get_echo( array( 'Gutenberg_Modules', 'print_enqueued_modules' ) );
+		$import_map_markup = get_echo( array( 'Gutenberg_Modules', 'print_import_map' ) );
+		$preload_markup    = get_echo( array( 'Gutenberg_Modules', 'print_module_preloads' ) );
 
-		// Use reflection to access the private static property $enqueued.
-		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
-		$property   = $reflection->getProperty( 'enqueued' );
-		$property->setAccessible( true );
-		$enqueued = $property->getValue();
+		$previous_tags      = new WP_HTML_Tag_Processor( $this->old_modules_markup );
+		$previous_src_stack = array();
+		while ( $previous_tags->next_tag( array( 'type' => 'module' ) ) ) {
+			$previous_src_stack[] = $previous_tags->get_attribute( 'src' );
+		}
+		// Test that there are 2 new <script type="module"> added to the markup.
+		$tags      = new WP_HTML_Tag_Processor( $modules_markup );
+		$src_stack = array();
+		while ( $tags->next_tag( array( 'type' => 'module' ) ) ) {
+			$src_stack[] = $tags->get_attribute( 'src' );
+		}
+		$new_src_stack = array_values( array_diff( $src_stack, $previous_src_stack ) );
+		$this->assertEquals( 2, count( $new_src_stack ) );
+		$this->assertEquals( 'interactivity-api-1.js?ver=' . $wp_version, $new_src_stack[0] );
+		$this->assertEquals( 'interactivity-api-2.js?ver=' . $wp_version, $new_src_stack[1] );
+		// We test that there is 1 <script type="importmap"> added to the markup.
+		$tags = new WP_HTML_Tag_Processor( $import_map_markup );
+		$this->assertEquals( true, $tags->next_tag( array( 'rel' => 'importmap' ) ) );
 
-		// Assert that the module was enqueued correctly.
-		$this->assertContains( $module_identifier, $enqueued );
-	}
-
-	public function test_get_dependencies() {
-		$test_data             = $this->get_test_data();
-		$parent_module         = $test_data['parent_module'];
-		$dependant_module      = $test_data['dependant_module'];
-		$dependant_module_src  = $test_data['dependant_module_src'];
-		$dependant_module_deps = $test_data['dependant_module_deps'];
-		$version               = $test_data['version'];
-
-		gutenberg_register_module( $dependant_module, $dependant_module_src, $dependant_module_deps, $version );
-
-		// Use reflection to call the private method get_dependencies.
-		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
-		$method     = $reflection->getMethod( 'get_dependencies' );
-		$method->setAccessible( true );
-		$dependencies = $method->invokeArgs( null, array( array( $dependant_module ) ) );
-
-		$this->assertArrayHasKey( $parent_module, $dependencies );
-		$this->assertArrayHasKey( 'src', $dependencies[ $parent_module ] );
-		$this->assertArrayHasKey( 'version', $dependencies[ $parent_module ] );
-		$this->assertArrayHasKey( 'dependencies', $dependencies[ $parent_module ] );
-	}
-
-	public function test_get_version_query_string() {
-		$test_data = $this->get_test_data();
-		$version   = $test_data['version'];
-
-		// Use reflection to call the private method get_version_query_string.
-		$reflection = new ReflectionClass( 'Gutenberg_Modules' );
-		$method     = $reflection->getMethod( 'get_version_query_string' );
-		$method->setAccessible( true );
-		$version_query_string = $method->invokeArgs( null, array( $version ) );
-
-		// Assert that the version query string is correct.
-		$this->assertSame( '?ver=' . $version, $version_query_string );
-
-		// Version is false.
-		$version_query_string = $method->invokeArgs( null, array( false ) );
-		$this->assertSame( '?ver=' . get_bloginfo( 'version' ), $version_query_string );
-
-		// Default.
-		$version_query_string = $method->invokeArgs( null, array( null ) );
-		$this->assertSame( '', $version_query_string );
-	}
-
-	public function test_get_import_map() {
-		$test_data         = $this->get_test_data();
-		$parent_module     = $test_data['parent_module'];
-		$parent_module_src = $test_data['parent_module_src'];
-		$dependant_module  = $test_data['dependant_module'];
-		$version           = $test_data['version'];
-
-		gutenberg_enqueue_module( $dependant_module );
-
-		$import_map = Gutenberg_Modules::get_import_map();
-
-		// Assert that the import map is correct.
-		$this->assertArrayHasKey( 'imports', $import_map );
-		$this->assertArrayHasKey( $parent_module, $import_map['imports'] );
-		$this->assertSame( $parent_module_src . '?ver=' . $version, $import_map['imports'][ $parent_module ] );
+		// We test that there is 1 <link type="modulepreload"> added to the markup.
+		$tags = new WP_HTML_Tag_Processor( $preload_markup );
+		$this->assertEquals( true, $tags->next_tag( array( 'rel' => 'modulepreload' ) ) );
 	}
 }
