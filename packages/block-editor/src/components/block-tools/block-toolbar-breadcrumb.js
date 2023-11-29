aa1125630fbb383bbea5e7d94277a3915b9ced3c
Jerry Jones <jones.jeremydavid@gmail.com>
2023-11-29 10:48:58 -0600

    Remove shouldUseKeyboardFocusShortcut prop from use in BlockToolbar

--- packages/block-editor/src/components/block-tools/block-toolbar-breadcrumb.js
+++ packages/block-editor/src/components/block-tools/block-toolbar-breadcrumb.js
@@ -4,15 +4,9 @@
 import classnames from 'classnames';
 
 /**
- * WordPress dependencies
- */
-import { useSelect } from '@wordpress/data';
-
-/**
  * Internal dependencies
  */
 import BlockSelectionButton from './block-selection-button';
-import { store as blockEditorStore } from '../../store';
 import BlockPopover from '../block-popover';
 import useBlockToolbarPopoverProps from './use-block-toolbar-popover-props';
 import useSelectedBlockToolProps from './use-selected-block-tool-props';
@@ -28,45 +22,25 @@ export default function BlockToolbarBreadcrumb( {
 		rootClientId,
 	} = useSelectedBlockToolProps( clientId );
 
-	const { shouldShowBreadcrumb } = useSelect( ( select ) => {
-		const { hasMultiSelection, __unstableGetEditorMode } =
-			select( blockEditorStore );
-
-		const editorMode = __unstableGetEditorMode();
-
-		return {
-			shouldShowBreadcrumb:
-				! hasMultiSelection() &&
-				( editorMode === 'navigation' || editorMode === 'zoom-out' ),
-		};
-	}, [] );
-
 	const popoverProps = useBlockToolbarPopoverProps( {
 		contentElement: __unstableContentRef?.current,
 		clientId,
 	} );
 
-	if ( shouldShowBreadcrumb ) {
-		return (
-			<BlockPopover
-				clientId={ capturingClientId || clientId }
-				bottomClientId={ lastClientId }
-				className={ classnames(
-					'block-editor-block-list__block-popover',
-					{
-						'is-insertion-point-visible': isInsertionPointVisible,
-					}
-				) }
-				resize={ false }
-				{ ...popoverProps }
-			>
-				<BlockSelectionButton
-					clientId={ clientId }
-					rootClientId={ rootClientId }
-				/>
-			</BlockPopover>
-		);
-	}
-
-	return null;
+	return (
+		<BlockPopover
+			clientId={ capturingClientId || clientId }
+			bottomClientId={ lastClientId }
+			className={ classnames( 'block-editor-block-list__block-popover', {
+				'is-insertion-point-visible': isInsertionPointVisible,
+			} ) }
+			resize={ false }
+			{ ...popoverProps }
+		>
+			<BlockSelectionButton
+				clientId={ clientId }
+				rootClientId={ rootClientId }
+			/>
+		</BlockPopover>
+	);
 }
