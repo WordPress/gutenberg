35d03dd6757fa10b100fe5e6d4d0855a0f0511fd
Mario Santos <santosguillamot@gmail.com>
2023-11-30 15:48:58 +0100

    Filter to add the bindings attribute automatically

--- packages/editor/src/components/block-bindings/index.js
+++ packages/editor/src/components/block-bindings/index.js
@@ -16,82 +16,105 @@ const blockBindingsWhitelist = {
 	'core/image': [ 'url', 'title' ],
 };
 
-export default function BlockBindingsButton( BlockEdit ) {
-	return ( props ) => {
-		// Only add the Block Bindings button to the blocks in the whitelist.
-		if ( ! ( props.name in blockBindingsWhitelist ) ) {
-			return <BlockEdit { ...props } />;
-		}
-		const { setAttributes } = props;
+export default function BlockBindingsButton( props ) {
+	const { setAttributes } = props;
 
-		const [ addingBinding, setAddingBinding ] = useState( false );
-		function BindingsUI() {
-			return (
-				<Popover
-					popoverAnchor={ popoverAnchor }
-					onClose={ () => {
-						setAddingBinding( false );
-					} }
-					onFocusOutside={ () => {
-						setAddingBinding( false );
-					} }
-					placement="bottom"
-					shift
-					className="block-bindings-ui-popover"
+	const [ addingBinding, setAddingBinding ] = useState( false );
+	function BindingsUI() {
+		return (
+			<Popover
+				popoverAnchor={ popoverAnchor }
+				onClose={ () => {
+					setAddingBinding( false );
+				} }
+				onFocusOutside={ () => {
+					setAddingBinding( false );
+				} }
+				placement="bottom"
+				shift
+				className="block-bindings-ui-popover"
+				{ ...props }
+			>
+				{ /* TODO: Add logic to select the attribute to bind */ }
+
+				{ /* TODO: This component could potentially be defined by each source. */ }
+				<MetadataSourceUI
 					{ ...props }
-				>
-					{ /* TODO: Add logic to select the attribute to bind */ }
+					setAddingBinding={ setAddingBinding }
+				/>
 
-					{ /* TODO: This component could potentially be defined by each source. */ }
-					<MetadataSourceUI
-						{ ...props }
-						setAddingBinding={ setAddingBinding }
-					/>
-
-					{ /*
+				{ /*
                         TODO: Add a better way to "clear" the binding.
                         We don't have to remove the whole bindings attribute but just the one we are binding.
                         We can explore if we can get back to the previous content or keep the value of the custom field.
                     */ }
-					<button
-						className="block-bindings-clear-button"
-						onClick={ () => {
-							if ( props.attributes.bindings ) {
-								setAttributes( {
-									content: '',
-									url: '',
-									bindings: {},
-								} );
-							}
-							setAddingBinding( false );
-						} }
-					>
-						Clear bindings
-					</button>
-				</Popover>
-			);
-		}
-
-		const [ popoverAnchor, setPopoverAnchor ] = useState();
-		return (
-			<>
-				<BlockEdit { ...props } />
-				<BlockControls group="other">
-					<Button
-						onClick={ () => {
-							setAddingBinding( ! addingBinding );
-						} }
-						aria-expanded={ true }
-						icon={ pluginsIcon }
-						ref={ setPopoverAnchor }
-					></Button>
-					{ addingBinding && <BindingsUI /> }
-				</BlockControls>
-			</>
+				<button
+					className="block-bindings-clear-button"
+					onClick={ () => {
+						if ( props.attributes.bindings ) {
+							setAttributes( {
+								content: '',
+								url: '',
+								bindings: {},
+							} );
+						}
+						setAddingBinding( false );
+					} }
+				>
+					Clear bindings
+				</button>
+			</Popover>
 		);
-	};
+	}
+
+	const [ popoverAnchor, setPopoverAnchor ] = useState();
+	return (
+		<>
+			<BlockControls group="other">
+				<Button
+					onClick={ () => {
+						setAddingBinding( ! addingBinding );
+					} }
+					aria-expanded={ true }
+					icon={ pluginsIcon }
+					ref={ setPopoverAnchor }
+				></Button>
+				{ addingBinding && <BindingsUI /> }
+			</BlockControls>
+		</>
+	);
 }
 
-addFilter( 'editor.BlockEdit', 'core', BlockBindingsButton );
+if ( window.__experimentalConnections ) {
+	addFilter(
+		'blocks.registerBlockType',
+		'block-directory/fallback',
+		( settings, name ) => {
+			if ( ! ( name in blockBindingsWhitelist ) ) {
+				return settings;
+			}
+
+			// Add "bindings" attribute.
+			if ( ! settings.attributes.bindings ) {
+				settings.attributes.bindings = {
+					type: 'object',
+				};
+			}
+
+			// Add bindings button to the block toolbar.
+			const OriginalComponent = settings.edit;
+			settings.edit = ( props ) => {
+				return (
+					<>
+						<OriginalComponent { ...props } />
+						<BlockBindingsButton { ...props } />
+					</>
+				);
+			};
+
+			return settings;
+		}
+	);
+}
 
 // TODO: Add also some components to the sidebar.
