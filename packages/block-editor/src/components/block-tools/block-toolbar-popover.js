b872f2e79bb30f2eaabcedefea2e8d41fa864bce
Jerry Jones <jones.jeremydavid@gmail.com>
2023-11-30 16:56:29 -0600

    Hide block toolbar popover when typing

--- packages/block-editor/src/components/block-tools/block-toolbar-popover.js
+++ packages/block-editor/src/components/block-tools/block-toolbar-popover.js
@@ -4,18 +4,28 @@
 import classnames from 'classnames';
 
 /**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+
+/**
  * Internal dependencies
  */
+import { store as blockEditorStore } from '../../store';
 import BlockToolbar from '../block-toolbar';
 import BlockPopover from '../block-popover';
 import useBlockToolbarPopoverProps from './use-block-toolbar-popover-props';
 import useSelectedBlockToolProps from './use-selected-block-tool-props';
-import { useShouldContextualToolbarShow } from '../../utils/use-should-contextual-toolbar-show';
 
 export default function BlockToolbarPopover( {
 	clientId,
 	__unstableContentRef,
 } ) {
+	const isTyping = useSelect(
+		( select ) => select( blockEditorStore ).isTyping(),
+		[]
+	);
+
 	const { capturingClientId, isInsertionPointVisible, lastClientId } =
 		useSelectedBlockToolProps( clientId );
 
@@ -24,15 +34,13 @@ export default function BlockToolbarPopover( {
 		clientId,
 	} );
 
-	const { shouldShowContextualToolbar } = useShouldContextualToolbarShow();
-
 	return (
 		<BlockPopover
 			clientId={ capturingClientId || clientId }
 			bottomClientId={ lastClientId }
 			className={ classnames( 'block-editor-block-list__block-popover', {
 				'is-insertion-point-visible': isInsertionPointVisible,
-				'is-hidden': ! shouldShowContextualToolbar, // Leave the toolbar in the DOM so it can be focused at the same roving tabindex it was previously at
+				'is-hidden': isTyping, // Leave the toolbar in the DOM so it can be focused at the same roving tabindex it was previously at
 			} ) }
 			resize={ false }
 			{ ...popoverProps }
