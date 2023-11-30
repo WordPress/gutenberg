defec0c46f696a93c59dd940457f0f6c87f9bfc9
David Arenas <david.arenas@automattic.com>
2023-11-30 18:32:28 +0100

    Fix wp-class tests

--- phpunit/experimental/interactivity-api/directives/wp-class-test.php
+++ phpunit/experimental/interactivity-api/directives/wp-class-test.php
@@ -14,8 +14,9 @@
  */
 class Tests_Directives_WpClass extends WP_UnitTestCase {
 	public function test_directive_adds_class() {
-		$markup = '<div data-wp-class--red="context.myblock.isRed" class="blue">Test</div>';
-		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$markup = '<div data-wp-class--red="context.isRed" class="blue">Test</div>';
+		$tags   = new WP_Directive_Processor( $markup );
+		$tags->push_namespace( 'myblock' );
 		$tags->next_tag();
 
 		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'isRed' => true ) ) );
@@ -23,7 +24,7 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 		gutenberg_interactivity_process_wp_class( $tags, $context );
 
 		$this->assertSame(
-			'<div data-wp-class--red="context.myblock.isRed" class="blue red">Test</div>',
+			'<div data-wp-class--red="context.isRed" class="blue red">Test</div>',
 			$tags->get_updated_html()
 		);
 		$this->assertStringContainsString( 'red', $tags->get_attribute( 'class' ) );
@@ -31,8 +32,9 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 	}
 
 	public function test_directive_removes_class() {
-		$markup = '<div data-wp-class--blue="context.myblock.isBlue" class="red blue">Test</div>';
-		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$markup = '<div data-wp-class--blue="context.isBlue" class="red blue">Test</div>';
+		$tags   = new WP_Directive_Processor( $markup );
+		$tags->push_namespace( 'myblock' );
 		$tags->next_tag();
 
 		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'isBlue' => false ) ) );
@@ -40,7 +42,7 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 		gutenberg_interactivity_process_wp_class( $tags, $context );
 
 		$this->assertSame(
-			'<div data-wp-class--blue="context.myblock.isBlue" class="red">Test</div>',
+			'<div data-wp-class--blue="context.isBlue" class="red">Test</div>',
 			$tags->get_updated_html()
 		);
 		$this->assertStringNotContainsString( 'blue', $tags->get_attribute( 'class' ) );
@@ -48,8 +50,9 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 	}
 
 	public function test_directive_removes_empty_class_attribute() {
-		$markup = '<div data-wp-class--blue="context.myblock.isBlue" class="blue">Test</div>';
-		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$markup = '<div data-wp-class--blue="context.isBlue" class="blue">Test</div>';
+		$tags   = new WP_Directive_Processor( $markup );
+		$tags->push_namespace( 'myblock' );
 		$tags->next_tag();
 
 		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'isBlue' => false ) ) );
@@ -57,8 +60,8 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 		gutenberg_interactivity_process_wp_class( $tags, $context );
 
 		$this->assertSame(
-			// WP_HTML_Tag_Processor has a TODO note to prune whitespace after classname removal.
-			'<div data-wp-class--blue="context.myblock.isBlue" >Test</div>',
+			// WP_Directive_Processor has a TODO note to prune whitespace after classname removal.
+			'<div data-wp-class--blue="context.isBlue" >Test</div>',
 			$tags->get_updated_html()
 		);
 		$this->assertNull( $tags->get_attribute( 'class' ) );
@@ -66,8 +69,9 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 	}
 
 	public function test_directive_does_not_remove_non_existant_class() {
-		$markup = '<div data-wp-class--blue="context.myblock.isBlue" class="green red">Test</div>';
-		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$markup = '<div data-wp-class--blue="context.isBlue" class="green red">Test</div>';
+		$tags   = new WP_Directive_Processor( $markup );
+		$tags->push_namespace( 'myblock' );
 		$tags->next_tag();
 
 		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'isBlue' => false ) ) );
@@ -75,7 +79,7 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 		gutenberg_interactivity_process_wp_class( $tags, $context );
 
 		$this->assertSame(
-			'<div data-wp-class--blue="context.myblock.isBlue" class="green red">Test</div>',
+			'<div data-wp-class--blue="context.isBlue" class="green red">Test</div>',
 			$tags->get_updated_html()
 		);
 		$this->assertSame( 'green red', $tags->get_attribute( 'class' ) );
@@ -83,8 +87,9 @@ class Tests_Directives_WpClass extends WP_UnitTestCase {
 	}
 
 	public function test_directive_ignores_empty_class_name() {
-		$markup = '<div data-wp-class.="context.myblock.isRed" class="blue">Test</div>';
-		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$markup = '<div data-wp-class="context.isRed" class="blue">Test</div>';
+		$tags   = new WP_Directive_Processor( $markup );
+		$tags->push_namespace( 'myblock' );
 		$tags->next_tag();
 
 		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'isRed' => true ) ) );
