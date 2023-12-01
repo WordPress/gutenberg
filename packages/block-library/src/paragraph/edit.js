c2dfa2988ebb45d2bf442fc5946dbb5985ac765f
Ella <4710635+ellatrix@users.noreply.github.com>
2023-12-01 21:40:09 +0100

    Block lib: use RichText.isEmpty where forgotten (#56726)

--- packages/block-library/src/paragraph/edit.js
+++ packages/block-library/src/paragraph/edit.js
@@ -154,13 +154,13 @@ function ParagraphBlock( {
 				onReplace={ onReplace }
 				onRemove={ onRemove }
 				aria-label={
-					content
-						? __( 'Block: Paragraph' )
-						: __(
+					RichText.isEmpty( content )
+						? __(
 								'Empty block; start writing or type forward slash to choose a block'
 						  )
+						: __( 'Block: Paragraph' )
 				}
-				data-empty={ content ? false : true }
+				data-empty={ RichText.isEmpty( content ) }
 				placeholder={ placeholder || __( 'Type / to choose a block' ) }
 				data-custom-placeholder={ placeholder ? true : undefined }
 				__unstableEmbedURLOnPaste
