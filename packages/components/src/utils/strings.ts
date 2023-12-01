d570f04a1eddfbf9677ec13876df5bed177fdf2b
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-12-01 15:08:19 +0900

    PaletteEdit: Consider digits when generating kebab-cased slug

--- packages/components/src/utils/strings.ts
+++ packages/components/src/utils/strings.ts
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import removeAccents from 'remove-accents';
+import { paramCase } from 'change-case';
 
 const ALL_UNICODE_DASH_CHARACTERS = new RegExp(
 	`[${ [
@@ -80,4 +81,30 @@ export const normalizeTextString = ( value: string ): string => {
  */
 export function escapeRegExp( string: string ): string {
 	return string.replace( /[\\^$.*+?()[\]{}|]/g, '\\$&' );
+}
+
+/**
+ * Converts any string to kebab case.
+ *
+ * @param {string} str String to convert.
+ *
+ * @return {string} Kebab-cased string
+ */
+export function kebabCase( str: any ) {
+	let input = str;
+	if ( typeof str !== 'string' ) {
+		input = str?.toString?.() ?? '';
+	}
+
+	// See https://github.com/lodash/lodash/blob/b185fcee26b2133bd071f4aaca14b455c2ed1008/lodash.js#L4970
+	input = input.replace( /['\u2019]/, '' );
+
+	return paramCase( input, {
+		splitRegexp: [
+			/(?!(?:1ST|2ND|3RD|[4-9]TH)(?![a-z]))([a-z0-9])([A-Z])/g, // fooBar => foo-bar, 3Bar => 3-bar
+			/(?!(?:1st|2nd|3rd|[4-9]th)(?![a-z]))([0-9])([a-z])/g, // 3bar => 3-bar
+			/([A-Za-z])([0-9])/g, // Foo3 => foo-3, foo3 => foo-3
+			/([A-Z])([A-Z][a-z])/g, // FOOBar => foo-bar
+		],
+	} );
 }
