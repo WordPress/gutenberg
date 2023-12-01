373d5b3d0ae238548d58413b32b484344b27caf3
Derek Blank <derekpblank@gmail.com>
2023-12-01 12:28:26 +1000

    Move OfflineStatus component to editor package

--- packages/editor/src/components/index.native.js
+++ packages/editor/src/components/index.native.js
@@ -3,6 +3,7 @@ export { default as AutosaveMonitor } from './autosave-monitor';
 export { default as PostTitle } from './post-title';
 export { default as EditorHistoryRedo } from './editor-history/redo';
 export { default as EditorHistoryUndo } from './editor-history/undo';
+export { default as OfflineStatus } from './offline-status';
 
 // State Related Components.
 export { default as EditorProvider } from './provider';
