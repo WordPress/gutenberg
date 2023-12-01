754360603a9d42db883e1df1a5503d21d1670f0e
Mario Santos <santosguillamot@gmail.com>
2023-12-01 10:10:06 +0100

    Use getEntityRecord to fetch the REST API

--- packages/editor/src/components/block-bindings/metadata.js
+++ packages/editor/src/components/block-bindings/metadata.js
@@ -1,63 +1,53 @@
 /**
  * WordPress dependencies
  */
-import { useState, useEffect } from '@wordpress/element';
+import { useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
 import { SearchControl } from '@wordpress/components';
-import apiFetch from '@wordpress/api-fetch';
 
 export default function MetadataSourceUI( props ) {
 	const { setAttributes, setIsActiveAttribute, context } = props;
 
-	const [ metadata, setMetadata ] = useState( [] );
-	useEffect( () => {
-		// Fetching the REST API to get the available custom fields.
-		// TODO: Only run the fetch once. Right now it is triggered each time I click the button.
-		// TODO: Review if we can avoid the first fetch. Using it right now to get the rest_base and the rest_namespace.
-		// TODO: Review if it works with taxonomies.
-		apiFetch( {
-			path: '/wp/v2/types/' + context.postType,
-		} ).then( ( type ) => {
-			apiFetch( {
-				path:
-					'/' +
-					type.rest_namespace +
-					'/' +
-					type.rest_base +
-					'/' +
-					context.postId,
-			} ).then( ( posts ) => {
-				const fetchedMetadata = [];
-				function addMetadata( array, newData ) {
-					Object.entries( newData ).forEach( ( [ key, value ] ) => {
-						// Prettifying the name. But I guess it is not necessary.
-						// Plugins could provide it somehow.
-						const prettyName = key
-							.split( '_' )
-							.map(
-								( word ) =>
-									word.charAt( 0 ).toUpperCase() +
-									word.slice( 1 )
-							)
-							.join( ' ' );
-						array.push( {
-							name: prettyName,
-							key,
-							value,
-						} );
-						return array;
-					} );
-				}
-				addMetadata( fetchedMetadata, posts.meta );
-
-				// TODO: Add filter in case plugins want to add/remove/modify fields.
-				// For example, ACF has its own field named "acf". Adding it manually.
-				addMetadata( fetchedMetadata, posts.acf );
-
-				setMetadata( fetchedMetadata );
+	// Fetching the REST API to get the available custom fields.
+	// TODO: Review if it works with taxonomies.
+	const data = useSelect(
+		( select ) => {
+			const { getEntityRecord } = select( coreStore );
+			return getEntityRecord(
+				'postType',
+				context.postType,
+				context.postId
+			);
+		},
+		[ context.postType, context.postId ]
+	);
+	const metadata = [];
+	function addMetadata( array, newData ) {
+		Object.entries( newData ).forEach( ( [ key, value ] ) => {
+			// Prettifying the name. But I guess it is not necessary.
+			// Plugins could provide it somehow.
+			const prettyName = key
+				.split( '_' )
+				.map(
+					( word ) => word.charAt( 0 ).toUpperCase() + word.slice( 1 )
+				)
+				.join( ' ' );
+			array.push( {
+				name: prettyName,
+				key,
+				value,
 			} );
+			return array;
 		} );
-	}, [] );
+	}
+	addMetadata( metadata, data.meta );
+
+	// TODO: Add filter in case plugins want to add/remove/modify fields.
+	// For example, ACF has its own field named "acf". Adding it manually.
+	// Other example could be post meta (Post Title) not included in "meta".
+	addMetadata( metadata, data.acf );
 
 	const [ selectedField, setSelectedField ] = useState( null );
 	// TODO: Try to abstract this function to be reused across all the sources.
