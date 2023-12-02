88b311f5115481e190b550ebf0000ec38c822f9e
Rich Tabor <hi@richtabor.com>
2023-12-01 20:55:15 -0500

    Another round of HiDPI icon fixes (#56532)
    * Update plus.js
    
    * Update page.js
    
    * Update pages.js
    
    * Update crop.js
    
    * embedDailymotionIcon
    
    * Update archive.js
    
    * Update copy.js
    
    * Update columns.js
    
    * Update file.js
    
    * Update post-excerpt.js
    
    * Update post-list.js
    
    * Update tag.js
    
    * Add tableOfContents to icons
    
    * Add back fill for embedDailymotionIcon
    
    * Update snapshot
    
    * Update test
    
    * Update crop.js
    
    * Fix test
    
    * Copy

--- packages/icons/src/library/page.js
+++ packages/icons/src/library/page.js
@@ -5,7 +5,8 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const page = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<Path d="M7 5.5h10a.5.5 0 01.5.5v12a.5.5 0 01-.5.5H7a.5.5 0 01-.5-.5V6a.5.5 0 01.5-.5zM17 4H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V6a2 2 0 00-2-2zm-1 3.75H8v1.5h8v-1.5zM8 11h8v1.5H8V11zm6 3.25H8v1.5h6v-1.5z" />
+		<Path d="M15.5 7.5h-7V9h7V7.5Zm-7 3.5h7v1.5h-7V11Zm7 3.5h-7V16h7v-1.5Z" />
+		<Path d="M17 4H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2ZM7 5.5h10a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5H7a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5Z" />
 	</SVG>
 );
 
