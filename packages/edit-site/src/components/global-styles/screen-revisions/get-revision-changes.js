803c4d9ee88dadae88500a35209475301c5561a5
ramon <ramonjd@gmail.com>
2023-12-04 09:22:33 +1100

    getGlobalStylesChanges was doing nothing! Removed.

--- packages/edit-site/src/components/global-styles/screen-revisions/get-revision-changes.js
+++ packages/edit-site/src/components/global-styles/screen-revisions/get-revision-changes.js
@@ -23,28 +23,6 @@ const translationMap = {
 	'typography.fontWeight': __( 'font weight' ),
 };
 
-/**
- * Returns an array of translated strings describing high level global styles and settings.
- *
- * @param {Object} revision
- * @param {Object} revision.settings Global styles settings.
- * @param {Object} revision.styles   Global styles.
- * @return {string[] | []} An array of translated labels.
- */
-export function getGlobalStylesChanges( { settings = {}, styles = {} } ) {
-	const changes = [];
-	if ( Object.keys( settings ).length > 0 ) {
-		changes.push( __( 'Global settings' ) );
-	}
-	Object.keys( styles ).forEach( ( key ) => {
-		if ( translationMap[ key ] ) {
-			changes.push( translationMap[ key ] );
-		}
-	} );
-
-	return changes;
-}
-
 function getTranslation( key, blockNames ) {
 	if ( translationMap[ key ] ) {
 		return translationMap[ key ];
