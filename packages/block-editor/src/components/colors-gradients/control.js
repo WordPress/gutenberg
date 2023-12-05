e0a7f53a44684019b8cba60ffd924f1b11e5d7b3
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-12-05 11:02:41 -0500

    try waiting to unlock Tabs

--- packages/block-editor/src/components/colors-gradients/control.js
+++ packages/block-editor/src/components/colors-gradients/control.js
@@ -21,8 +21,6 @@ import {
 import { useSettings } from '../use-settings';
 import { unlock } from '../../lock-unlock';
 
-const { Tabs } = unlock( componentsPrivateApis );
-
 const colorsAndGradientKeys = [
 	'colors',
 	'disableCustomColors',
@@ -108,6 +106,11 @@ function ColorGradientControlInner( {
 			{ tabPanels[ type ] }
 		</div>
 	);
+
+	// Unlocking `Tabs` too early causes the `unlock` method to receive an empty
+	// object, due to circular dependencies.
+	// See https://github.com/WordPress/gutenberg/issues/52692
+	const { Tabs } = unlock( componentsPrivateApis );
 
 	return (
 		<BaseControl
