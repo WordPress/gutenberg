e8abd11b22799b4f802ca8a9db73393b464df632
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-12-07 17:29:39 +0100

    After format

--- packages/dataviews/src/types.js
+++ packages/dataviews/src/types.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { dateI18n, getDate, getSettings } from '@wordpress/date';
+import { __experimentalVStack as VStack } from '@wordpress/components';
 
 const renderLinkFormat = ( { format, item, children } ) => {
 	const props = format.renderProps( { item } );
@@ -16,6 +17,11 @@ const renderDateFormat = ( { children } ) => {
 	return <time>{ formattedDate }</time>;
 };
 
+const renderAfterFormat = ( { format, item } ) => {
+	const props = format.renderProps( { item } );
+	return <span { ...props }>{ format.renderChildren( { item } ) }</span>;
+};
+
 export const renderDate = ( { field, item } ) => {
 	field.formats.unshift( { type: 'date' } );
 
@@ -24,16 +30,28 @@ export const renderDate = ( { field, item } ) => {
 
 export const renderText = ( { field, item } ) => {
 	const value = field.getValue( { item } );
-	return field.formats.reduce( ( acc, format ) => {
+	const result = field.formats.reduce( ( acc, format ) => {
 		if ( format.type === 'link' ) {
 			return renderLinkFormat( { format, item, children: acc } );
 		}
 		if ( format.type === 'date' ) {
 			return renderDateFormat( { format, item, children: acc } );
 		}
-
 		return acc;
 	}, value );
+
+	const afterFormat = field.formats.find(
+		( format ) => format.type === 'after'
+	);
+	if ( afterFormat ) {
+		return (
+			<VStack spacing={ 1 }>
+				{ result }
+				{ renderAfterFormat( { format: afterFormat, item } ) }
+			</VStack>
+		);
+	}
+	return result;
 };
 
 export const renderEnumeration = renderText;
