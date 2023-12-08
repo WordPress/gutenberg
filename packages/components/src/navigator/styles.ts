c8e5fd044b5d75d2d21779f23140fdf20b729c78
Marco Ciampini <marco.ciampo@gmail.com>
2023-12-08 17:55:28 +0100

    Move navigator screen styles to separate file, use CSS animations instead of framer motion

--- packages/components/src/navigator/styles.ts
+++ packages/components/src/navigator/styles.ts
@@ -9,3 +9,62 @@ export const navigatorProviderWrapper = css`
 
 	contain: strict;
 `;
+
+const fadeInFromRight = keyframes( {
+	'0%': {
+		opacity: 0,
+		transform: `translateX(50px)`,
+	},
+	'100%': { opacity: 1, transform: 'none' },
+} );
+
+const fadeInFromLeft = keyframes( {
+	'0%': {
+		opacity: 0,
+		transform: `translateX(-50px)`,
+	},
+	'100%': { opacity: 1, transform: 'none' },
+} );
+
+type NavigatorScreenAnimationProps = {
+	isInitial?: boolean;
+	isBack?: boolean;
+	isRTL: boolean;
+};
+
+const navigatorScreenAnimation = ( {
+	isInitial,
+	isBack,
+	isRTL,
+}: NavigatorScreenAnimationProps ) => {
+	if ( isInitial && ! isBack ) {
+		return css``;
+	}
+
+	const animationName =
+		( isRTL && isBack ) || ( ! isRTL && ! isBack )
+			? fadeInFromRight
+			: fadeInFromLeft;
+
+	return css`
+		animation-duration: 0.14s;
+		/* easeInOutCubic */
+		animation-timing-function: cubic-bezier( 0.65, 0, 0.35, 1 );
+		will-change: transform, opacity;
+		/* Default animation.*/
+		animation-name: ${ animationName };
+
+		@media ( prefers-reduced-motion ) {
+			animation-duration: 0s;
+		}
+	`;
+};
+
+export const navigatorScreen = ( props: NavigatorScreenAnimationProps ) => css`
+	/* Ensures horizontal overflow is visually accessible */
+	overflow-x: auto;
+	/* In case the root has a height, it should not be exceeded */
+	max-height: 100%;
+
+	${ navigatorScreenAnimation( props ) }
+`;
