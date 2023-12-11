69180b8a9df70d8cd38ae7ad650b00a986d47e3b
Markus Dobmann <m.do@posteo.net>
2023-12-11 20:59:54 +0100

    artificially slow down blockpreview renders

--- packages/edit-site/src/components/page-patterns/grid.js
+++ packages/edit-site/src/components/page-patterns/grid.js
@@ -3,14 +3,31 @@
  */
 import GridItem from './grid-item';
 
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+
 export default function Grid( { categoryId, items, ...props } ) {
+	const [ currentIndex, setCurrentIndex ] = useState( 0 );
+
+	useEffect( () => {
+		if ( ! items.length ) return;
+		if ( currentIndex < items.length ) {
+			const timer = setTimeout( () => {
+				setCurrentIndex( currentIndex + 1 );
+			}, 500 );
+			return () => clearTimeout( timer );
+		}
+	}, [ currentIndex, items.length ] );
+
 	if ( ! items?.length ) {
 		return null;
 	}
 
 	return (
 		<ul className="edit-site-patterns__grid" { ...props }>
-			{ items.map( ( item ) => (
+			{ items.slice( 0, currentIndex ).map( ( item ) => (
 				<GridItem
 					key={ item.name }
 					item={ item }
