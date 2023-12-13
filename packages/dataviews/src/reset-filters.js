584708242850d5c6aa2980865e0d9bed08d45b9b
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-12-13 10:46:00 +0100

    Hide reset and filter summaries in list view

--- packages/dataviews/src/reset-filters.js
+++ packages/dataviews/src/reset-filters.js
@@ -4,7 +4,16 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import { LAYOUT_LIST } from './constants';
+
 export default ( { view, onChangeView } ) => {
+	if ( view.type === LAYOUT_LIST ) {
+		return null;
+	}
+
 	return (
 		<Button
 			disabled={ view.search === '' && view.filters?.length === 0 }
