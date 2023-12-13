76e3ec5cc8fd241fb90067d37bc92c9e052ba6f1
Dave Smith <getdavemail@gmail.com>
2023-12-13 10:21:34 +0000

    Use steps to break up test

--- test/e2e/specs/site-editor/navigation-editor.spec.js
+++ test/e2e/specs/site-editor/navigation-editor.spec.js
@@ -19,131 +19,137 @@ test.describe( 'Editing Navigation Menus', () => {
 		requestUtils,
 		editor,
 	} ) => {
-		// We could Navigate directly to editing the Navigation Menu but we intentionally do not do this.
-		//
-		// Why? To provide coverage for a bug that caused the Navigation Editor behaviours to fail
-		// only when navigating through the editor screens (rather than going directly to the editor by URL).
-		// See: https://github.com/WordPress/gutenberg/pull/56856.
-		//
-		// Example (what we could do):
-		// await admin.visitSiteEditor( {
-		// 	postId: createdMenu?.id,
-		// 	postType: 'wp_navigation',
-		// } );
-		//
-		await admin.visitSiteEditor();
+		await test.step( 'Manually browse to focus mode for a Navigation Menu', async () => {
+			// We could Navigate directly to editing the Navigation Menu but we intentionally do not do this.
+			//
+			// Why? To provide coverage for a bug that caused the Navigation Editor behaviours to fail
+			// only when navigating through the editor screens (rather than going directly to the editor by URL).
+			// See: https://github.com/WordPress/gutenberg/pull/56856.
+			//
+			// Example (what we could do):
+			// await admin.visitSiteEditor( {
+			// 	postId: createdMenu?.id,
+			// 	postType: 'wp_navigation',
+			// } );
+			//
+			await admin.visitSiteEditor();
 
-		// create a Navigation Menu called "Test Menu" using the REST API helpers
-		const createdMenu = await requestUtils.createNavigationMenu( {
-			title: 'Primary Menu',
-			content:
-				'<!-- wp:navigation-link {"label":"WordPress","type":"custom","url":"http://www.wordpress.org/","kind":"custom"} /-->',
-		} );
-
-		// Add another so we get a list of Navigation menus in the editor.
-		await requestUtils.createNavigationMenu( {
-			title: 'Another One',
-			content:
-				'<!-- wp:navigation-link {"label":"Another Item","type":"custom","url":"http://www.wordpress.org/","kind":"custom"} /-->',
-		} );
-
-		const editorSidebar = page.getByRole( 'region', {
-			name: 'Navigation',
-		} );
-
-		await editorSidebar
-			.getByRole( 'button', {
-				name: 'Navigation',
-			} )
-			.click();
-
-		// Wait for list of Navigations to appear.
-		await expect(
-			editorSidebar.getByRole( 'heading', {
-				name: 'Navigation',
-				level: 1,
-			} )
-		).toBeVisible();
-
-		await editorSidebar
-			.getByRole( 'button', {
-				name: 'Primary Menu',
-			} )
-			.click();
-
-		await expect( page ).toHaveURL(
-			`wp-admin/site-editor.php?postId=${ createdMenu?.id }&postType=wp_navigation`
-		);
-
-		// Wait for list of Navigations to appear.
-		editorSidebar.getByRole( 'heading', {
-			name: 'Primary Menu',
-			level: 1,
-		} );
-
-		// Switch to editing the Navigation Menu
-		await editorSidebar
-			.getByRole( 'link', {
-				name: 'Edit',
-			} )
-			.click();
-
-		// Open List View.
-		await pageUtils.pressKeys( 'access+o' );
-
-		const listView = page
-			.getByRole( 'region', {
-				name: 'List View',
-			} )
-			.getByRole( 'treegrid', {
-				name: 'Block navigation structure',
+			// create a Navigation Menu called "Test Menu" using the REST API helpers
+			const createdMenu = await requestUtils.createNavigationMenu( {
+				title: 'Primary Menu',
+				content:
+					'<!-- wp:navigation-link {"label":"WordPress","type":"custom","url":"http://www.wordpress.org/","kind":"custom"} /-->',
 			} );
 
-		await expect( listView ).toBeVisible();
+			// Add another so we get a list of Navigation menus in the editor.
+			await requestUtils.createNavigationMenu( {
+				title: 'Another One',
+				content:
+					'<!-- wp:navigation-link {"label":"Another Item","type":"custom","url":"http://www.wordpress.org/","kind":"custom"} /-->',
+			} );
 
-		const navBlockNode = listView.getByRole( 'link', {
-			name: 'Navigation (locked)',
-			exact: true,
+			const editorSidebar = page.getByRole( 'region', {
+				name: 'Navigation',
+			} );
+
+			await editorSidebar
+				.getByRole( 'button', {
+					name: 'Navigation',
+				} )
+				.click();
+
+			// Wait for list of Navigations to appear.
+			await expect(
+				editorSidebar.getByRole( 'heading', {
+					name: 'Navigation',
+					level: 1,
+				} )
+			).toBeVisible();
+
+			await editorSidebar
+				.getByRole( 'button', {
+					name: 'Primary Menu',
+				} )
+				.click();
+
+			await expect( page ).toHaveURL(
+				`wp-admin/site-editor.php?postId=${ createdMenu?.id }&postType=wp_navigation`
+			);
+
+			// Wait for list of Navigations to appear.
+			editorSidebar.getByRole( 'heading', {
+				name: 'Primary Menu',
+				level: 1,
+			} );
+
+			// Switch to editing the Navigation Menu
+			await editorSidebar
+				.getByRole( 'link', {
+					name: 'Edit',
+				} )
+				.click();
 		} );
 
-		// The Navigation block should be present and locked.
-		await expect( navBlockNode ).toBeVisible();
+		await test.step( 'Check Navigation block is present and locked', async () => {
+			// Open List View.
+			await pageUtils.pressKeys( 'access+o' );
 
-		// The block should have no options menu.
-		await expect(
-			listView.getByRole( 'button', {
-				name: 'Options for Navigation',
+			const listView = page
+				.getByRole( 'region', {
+					name: 'List View',
+				} )
+				.getByRole( 'treegrid', {
+					name: 'Block navigation structure',
+				} );
+
+			await expect( listView ).toBeVisible();
+
+			const navBlockNode = listView.getByRole( 'link', {
+				name: 'Navigation (locked)',
 				exact: true,
-			} )
-		).toBeHidden();
+			} );
 
-		// Select the Navigation block.
-		await navBlockNode.click();
+			// The Navigation block should be present and locked.
+			await expect( navBlockNode ).toBeVisible();
 
-		// Open the document settings sidebar
-		await editor.openDocumentSettingsSidebar();
+			// The block should have no options menu.
+			await expect(
+				listView.getByRole( 'button', {
+					name: 'Options for Navigation',
+					exact: true,
+				} )
+			).toBeHidden();
 
-		const sidebar = page.getByRole( 'region', {
-			name: 'Editor settings',
+			// Select the Navigation block.
+			await navBlockNode.click();
 		} );
 
-		await expect( sidebar ).toBeVisible();
+		await test.step( 'Check Navigation block has no controls other than editable list view', async () => {
+			// Open the document settings sidebar
+			await editor.openDocumentSettingsSidebar();
 
-		// Check that the `Menu` control is visible.
-		// This is effectively the contents of the "List View" tab.
-		await expect(
-			sidebar.getByRole( 'heading', { name: 'Menu', exact: true } )
-		).toBeVisible();
+			const sidebar = page.getByRole( 'region', {
+				name: 'Editor settings',
+			} );
 
-		// Check the standard tabs are not present.
-		await expect(
-			sidebar.getByRole( 'tab', { name: 'List View' } )
-		).toBeHidden();
-		await expect(
-			sidebar.getByRole( 'tab', { name: 'Settings' } )
-		).toBeHidden();
-		await expect(
-			sidebar.getByRole( 'tab', { name: 'Styles' } )
-		).toBeHidden();
+			await expect( sidebar ).toBeVisible();
+
+			// Check that the `Menu` control is visible.
+			// This is effectively the contents of the "List View" tab.
+			await expect(
+				sidebar.getByRole( 'heading', { name: 'Menu', exact: true } )
+			).toBeVisible();
+
+			// Check the standard tabs are not present.
+			await expect(
+				sidebar.getByRole( 'tab', { name: 'List View' } )
+			).toBeHidden();
+			await expect(
+				sidebar.getByRole( 'tab', { name: 'Settings' } )
+			).toBeHidden();
+			await expect(
+				sidebar.getByRole( 'tab', { name: 'Styles' } )
+			).toBeHidden();
+		} );
 	} );
 } );
