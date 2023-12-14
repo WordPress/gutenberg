5e50682bf98bb36f383c6ad049fa4c3c643788bc
Riad Benguella <benguella@gmail.com>
2023-12-14 10:27:52 +0100

    Site Editor: Add Post Taxonomies panel

--- packages/edit-site/src/components/sidebar-edit-mode/page-panels/index.js
+++ packages/edit-site/src/components/sidebar-edit-mode/page-panels/index.js
@@ -12,7 +12,11 @@ import { humanTimeDiff } from '@wordpress/date';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { decodeEntities } from '@wordpress/html-entities';
-import { PostLastRevisionPanel, store as editorStore } from '@wordpress/editor';
+import {
+	PostLastRevisionPanel,
+	PostTaxonomiesPanel,
+	store as editorStore,
+} from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -95,6 +99,7 @@ export default function PagePanels() {
 				</PanelBody>
 			) }
 			<PostLastRevisionPanel />
+			<PostTaxonomiesPanel />
 		</>
 	);
 }
