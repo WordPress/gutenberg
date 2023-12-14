8c010bd5510745ea07d9373108fd2cb05e2b3b1e
André <583546+oandregal@users.noreply.github.com>
2023-12-14 12:34:56 +0100

    DataViews: centralize control of filter visibility in the Filters component (#57056)

--- packages/dataviews/src/filters.js
+++ packages/dataviews/src/filters.js
@@ -4,7 +4,12 @@
 import FilterSummary from './filter-summary';
 import AddFilter from './add-filter';
 import ResetFilters from './reset-filters';
-import { ENUMERATION_TYPE, OPERATOR_IN, OPERATOR_NOT_IN } from './constants';
+import {
+	ENUMERATION_TYPE,
+	OPERATOR_IN,
+	OPERATOR_NOT_IN,
+	LAYOUT_LIST,
+} from './constants';
 
 const sanitizeOperators = ( field ) => {
 	let operators = field.filterBy?.operators;
@@ -57,7 +62,7 @@ export default function Filters( { fields, view, onChangeView } ) {
 	const filterComponents = [
 		addFilter,
 		...filters.map( ( filter ) => {
-			if ( ! filter.isVisible ) {
+			if ( ! filter.isVisible || view.type === LAYOUT_LIST ) {
 				return null;
 			}
 
@@ -72,7 +77,7 @@ export default function Filters( { fields, view, onChangeView } ) {
 		} ),
 	];
 
-	if ( filterComponents.length > 1 ) {
+	if ( filterComponents.length > 1 && view.type !== LAYOUT_LIST ) {
 		filterComponents.push(
 			<ResetFilters
 				key="reset-filters"
