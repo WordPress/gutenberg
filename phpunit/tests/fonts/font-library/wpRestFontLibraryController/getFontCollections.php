8206fc28b79464ec6be37ff92ece087d4fe2d4ff
Matias Benedetto <matias.benedetto@gmail.com>
2023-12-15 14:01:11 -0300

    update endpoit paths in tests
    Co-authored-by: Jeff Ong <5375500+jffng@users.noreply.github.com>
    Co-authored-by: Jason Crist <146530+pbking@users.noreply.github.com>

--- phpunit/tests/fonts/font-library/wpRestFontLibraryController/getFontCollections.php
+++ phpunit/tests/fonts/font-library/wpRestFontLibraryController/getFontCollections.php
@@ -14,7 +14,7 @@
 class Tests_Fonts_WPRESTFontLibraryController_GetFontCollections extends WP_REST_Font_Library_Controller_UnitTestCase {
 
 	public function test_get_font_collections_with_no_collection_registered() {
-		$request  = new WP_REST_Request( 'GET', '/wp/v2/fonts/collections' );
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/font-collections' );
 		$response = rest_get_server()->dispatch( $request );
 		$this->assertSame( 200, $response->get_status() );
 		$this->assertSame( array(), $response->get_data() );
@@ -34,7 +34,7 @@ class Tests_Fonts_WPRESTFontLibraryController_GetFontCollections extends WP_REST
 		);
 		wp_register_font_collection( $config );
 
-		$request  = new WP_REST_Request( 'GET', '/wp/v2/fonts/collections' );
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/font-collections' );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 		$this->assertSame( 200, $response->get_status(), 'The response status is not 200.' );
