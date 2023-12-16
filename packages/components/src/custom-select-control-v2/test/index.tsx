4ecfc468bc61ee3ee273a28b3429b03b6d997c9d
brookewp <brooke.kaminski@automattic.com>
2023-12-15 16:35:58 -0800

    Add section for legacy tests

--- packages/components/src/custom-select-control-v2/test/index.tsx
+++ packages/components/src/custom-select-control-v2/test/index.tsx
@@ -99,195 +99,16 @@ const ControlledCustomSelect = () => {
 	);
 };
 
-describe.each( [
-	[ 'legacy: uncontrolled', LegacyCustomSelectControl ],
-	[ 'legacy: controlled', LegacyControlledCustomSelect ],
-	[ 'uncontrolled', UncontrolledCustomSelect ],
-	[ 'controlled', ControlledCustomSelect ],
-] )( 'CustomSelect %s', ( ...modeAndComponent ) => {
-	const [ mode, Component ] = modeAndComponent;
+describe( 'Legacy tests', () => {
+	describe.each( [
+		[ 'legacy: uncontrolled', LegacyCustomSelectControl ],
+		[ 'legacy: controlled', LegacyControlledCustomSelect ],
+		[ 'uncontrolled', UncontrolledCustomSelect ],
+		[ 'controlled', ControlledCustomSelect ],
+	] )( 'CustomSelect %s', ( ...modeAndComponent ) => {
+		const [ mode, Component ] = modeAndComponent;
 
-	it( 'Should replace the initial selection when a new item is selected', async () => {
-		const user = userEvent.setup();
-
-		render( <Component { ...props } /> );
-
-		const currentSelectedItem = screen.getByRole( 'combobox', {
-			expanded: false,
-		} );
-
-		await user.click( currentSelectedItem );
-
-		await user.click(
-			screen.getByRole( 'option', {
-				name: 'crimson clover',
-			} )
-		);
-
-		expect( currentSelectedItem ).toHaveTextContent( 'crimson clover' );
-
-		await user.click( currentSelectedItem );
-
-		await user.click(
-			screen.getByRole( 'option', {
-				name: 'poppy',
-			} )
-		);
-
-		expect( currentSelectedItem ).toHaveTextContent( 'poppy' );
-	} );
-
-	it( 'Should keep current selection if dropdown is closed without changing selection', async () => {
-		const user = userEvent.setup();
-
-		render( <Component { ...props } /> );
-
-		const currentSelectedItem = screen.getByRole( 'combobox', {
-			expanded: false,
-		} );
-
-		await user.tab();
-		await user.keyboard( '{enter}' );
-		expect(
-			screen.getByRole( 'listbox', {
-				name: 'label!',
-			} )
-		).toBeVisible();
-
-		await user.keyboard( '{escape}' );
-		expect(
-			screen.queryByRole( 'listbox', {
-				name: 'label!',
-			} )
-		).not.toBeInTheDocument();
-
-		expect( currentSelectedItem ).toHaveTextContent(
-			props.options[ 0 ].name
-		);
-	} );
-
-	// Test against legacy component only
-	// For optional key value pairs of options prop ( style, className, and __experimentalHint )
-	if ( mode !== 'uncontrolled' && mode !== 'controlled' ) {
-		it( 'Should apply class only to options that have a className defined', async () => {
-			const user = userEvent.setup();
-
-			render( <Component { ...props } /> );
-
-			await user.click(
-				screen.getByRole( 'combobox', {
-					expanded: false,
-				} )
-			);
-
-			// return an array of items _with_ a className added
-			const itemsWithClass = props.options.filter(
-				( option ) => option.className !== undefined
-			);
-
-			// assert against filtered array
-			itemsWithClass.map( ( { name } ) =>
-				expect( screen.getByRole( 'option', { name } ) ).toHaveClass(
-					customClass
-				)
-			);
-
-			// return an array of items _without_ a className added
-			const itemsWithoutClass = props.options.filter(
-				( option ) => option.className === undefined
-			);
-
-			// assert against filtered array
-			itemsWithoutClass.map( ( { name } ) =>
-				expect(
-					screen.getByRole( 'option', { name } )
-				).not.toHaveClass( customClass )
-			);
-		} );
-
-		it( 'Should apply styles only to options that have styles defined', async () => {
-			const user = userEvent.setup();
-			const customStyles =
-				'background-color: rgb(127, 255, 212); rotate: 13deg;';
-
-			render( <Component { ...props } /> );
-
-			await user.click(
-				screen.getByRole( 'combobox', {
-					expanded: false,
-				} )
-			);
-
-			// return an array of items _with_ styles added
-			const styledItems = props.options.filter(
-				( option ) => option.style !== undefined
-			);
-
-			// assert against filtered array
-			styledItems.map( ( { name } ) =>
-				expect( screen.getByRole( 'option', { name } ) ).toHaveStyle(
-					customStyles
-				)
-			);
-
-			// return an array of items _without_ styles added
-			const unstyledItems = props.options.filter(
-				( option ) => option.style === undefined
-			);
-
-			// assert against filtered array
-			unstyledItems.map( ( { name } ) =>
-				expect(
-					screen.getByRole( 'option', { name } )
-				).not.toHaveStyle( customStyles )
-			);
-		} );
-
-		it( 'does not show selected hint by default', () => {
-			render(
-				<LegacyCustomSelectControl
-					label="Custom select"
-					options={ [
-						{
-							key: 'one',
-							name: 'One',
-							__experimentalHint: 'Hint',
-						},
-					] }
-				/>
-			);
-			expect(
-				screen.getByRole( 'combobox', { name: 'Custom select' } )
-			).not.toHaveTextContent( 'Hint' );
-		} );
-
-		it( 'shows selected hint when __experimentalShowSelectedHint is set', async () => {
-			const user = userEvent.setup();
-
-			render(
-				<LegacyCustomSelectControl
-					label="Custom select"
-					options={ [
-						{
-							key: 'one',
-							name: 'One',
-							__experimentalHint: 'Hint',
-						},
-					] }
-					__experimentalShowSelectedHint
-				/>
-			);
-
-			await user.click(
-				screen.getByRole( 'combobox', { name: 'Custom select' } )
-			);
-
-			expect( screen.getByText( 'Hint' ) ).toBeVisible();
-		} );
-	}
-
-	describe( 'Keyboard behavior and accessibility', () => {
-		it( 'Should be able to change selection using keyboard', async () => {
+		it( 'Should replace the initial selection when a new item is selected', async () => {
 			const user = userEvent.setup();
 
 			render( <Component { ...props } /> );
@@ -296,23 +117,28 @@ describe.each( [
 				expanded: false,
 			} );
 
-			await user.tab();
-			expect( currentSelectedItem ).toHaveFocus();
+			await user.click( currentSelectedItem );
 
-			await user.keyboard( '{enter}' );
-			expect(
-				screen.getByRole( 'listbox', {
-					name: 'label!',
+			await user.click(
+				screen.getByRole( 'option', {
+					name: 'crimson clover',
 				} )
-			).toHaveFocus();
-
-			await user.keyboard( '{arrowdown}' );
-			await user.keyboard( '{enter}' );
+			);
 
 			expect( currentSelectedItem ).toHaveTextContent( 'crimson clover' );
+
+			await user.click( currentSelectedItem );
+
+			await user.click(
+				screen.getByRole( 'option', {
+					name: 'poppy',
+				} )
+			);
+
+			expect( currentSelectedItem ).toHaveTextContent( 'poppy' );
 		} );
 
-		it( 'Should be able to type characters to select matching options', async () => {
+		it( 'Should keep current selection if dropdown is closed without changing selection', async () => {
 			const user = userEvent.setup();
 
 			render( <Component { ...props } /> );
@@ -327,91 +153,271 @@ describe.each( [
 				screen.getByRole( 'listbox', {
 					name: 'label!',
 				} )
-			).toHaveFocus();
+			).toBeVisible();
 
-			await user.keyboard( '{a}' );
-			await user.keyboard( '{enter}' );
-			expect( currentSelectedItem ).toHaveTextContent( 'amber' );
-		} );
-
-		it( 'Can change selection with a focused input and closed dropdown if typed characters match an option', async () => {
-			const user = userEvent.setup();
-
-			render( <Component { ...props } /> );
-
-			const currentSelectedItem = screen.getByRole( 'combobox', {
-				expanded: false,
-			} );
-
-			await user.tab();
-			expect( currentSelectedItem ).toHaveFocus();
-
-			await user.keyboard( '{a}' );
-			await user.keyboard( '{q}' );
-
+			await user.keyboard( '{escape}' );
 			expect(
 				screen.queryByRole( 'listbox', {
 					name: 'label!',
-					hidden: true,
 				} )
 			).not.toBeInTheDocument();
 
-			await user.keyboard( '{enter}' );
-			expect( currentSelectedItem ).toHaveTextContent( 'aquamarine' );
+			expect( currentSelectedItem ).toHaveTextContent(
+				props.options[ 0 ].name
+			);
 		} );
 
-		it( 'Should have correct aria-selected value for selections', async () => {
-			const user = userEvent.setup();
+		// Test against legacy component only
+		// For optional key value pairs of options prop ( style, className, and __experimentalHint )
+		if ( mode !== 'uncontrolled' && mode !== 'controlled' ) {
+			it( 'Should apply class only to options that have a className defined', async () => {
+				const user = userEvent.setup();
 
-			render( <Component { ...props } /> );
+				render( <Component { ...props } /> );
 
-			const currentSelectedItem = screen.getByRole( 'combobox', {
-				expanded: false,
+				await user.click(
+					screen.getByRole( 'combobox', {
+						expanded: false,
+					} )
+				);
+
+				// return an array of items _with_ a className added
+				const itemsWithClass = props.options.filter(
+					( option ) => option.className !== undefined
+				);
+
+				// assert against filtered array
+				itemsWithClass.map( ( { name } ) =>
+					expect(
+						screen.getByRole( 'option', { name } )
+					).toHaveClass( customClass )
+				);
+
+				// return an array of items _without_ a className added
+				const itemsWithoutClass = props.options.filter(
+					( option ) => option.className === undefined
+				);
+
+				// assert against filtered array
+				itemsWithoutClass.map( ( { name } ) =>
+					expect(
+						screen.getByRole( 'option', { name } )
+					).not.toHaveClass( customClass )
+				);
 			} );
 
-			await user.click( currentSelectedItem );
+			it( 'Should apply styles only to options that have styles defined', async () => {
+				const user = userEvent.setup();
+				const customStyles =
+					'background-color: rgb(127, 255, 212); rotate: 13deg;';
 
-			// get all items except for first option
-			const unselectedItems = props.options.filter(
-				( { name } ) => name !== props.options[ 0 ].name
-			);
+				render( <Component { ...props } /> );
 
-			// assert that all other items have aria-selected="false"
-			unselectedItems.map( ( { name } ) =>
+				await user.click(
+					screen.getByRole( 'combobox', {
+						expanded: false,
+					} )
+				);
+
+				// return an array of items _with_ styles added
+				const styledItems = props.options.filter(
+					( option ) => option.style !== undefined
+				);
+
+				// assert against filtered array
+				styledItems.map( ( { name } ) =>
+					expect(
+						screen.getByRole( 'option', { name } )
+					).toHaveStyle( customStyles )
+				);
+
+				// return an array of items _without_ styles added
+				const unstyledItems = props.options.filter(
+					( option ) => option.style === undefined
+				);
+
+				// assert against filtered array
+				unstyledItems.map( ( { name } ) =>
+					expect(
+						screen.getByRole( 'option', { name } )
+					).not.toHaveStyle( customStyles )
+				);
+			} );
+
+			it( 'does not show selected hint by default', () => {
+				render(
+					<LegacyCustomSelectControl
+						label="Custom select"
+						options={ [
+							{
+								key: 'one',
+								name: 'One',
+								__experimentalHint: 'Hint',
+							},
+						] }
+					/>
+				);
 				expect(
-					screen.getByRole( 'option', { name, selected: false } )
-				).toBeVisible()
-			);
+					screen.getByRole( 'combobox', { name: 'Custom select' } )
+				).not.toHaveTextContent( 'Hint' );
+			} );
 
-			// assert that first item has aria-selected="true"
-			expect(
-				screen.getByRole( 'option', {
-					name: props.options[ 0 ].name,
-					selected: true,
-				} )
-			).toBeVisible();
+			it( 'shows selected hint when __experimentalShowSelectedHint is set', async () => {
+				const user = userEvent.setup();
 
-			// change the current selection
-			await user.click( screen.getByRole( 'option', { name: 'poppy' } ) );
+				render(
+					<LegacyCustomSelectControl
+						label="Custom select"
+						options={ [
+							{
+								key: 'one',
+								name: 'One',
+								__experimentalHint: 'Hint',
+							},
+						] }
+						__experimentalShowSelectedHint
+					/>
+				);
 
-			// click combobox to mount listbox with options again
-			await user.click( currentSelectedItem );
+				await user.click(
+					screen.getByRole( 'combobox', { name: 'Custom select' } )
+				);
 
-			// check that first item is has aria-selected="false" after new selection
-			expect(
-				screen.getByRole( 'option', {
-					name: props.options[ 0 ].name,
-					selected: false,
-				} )
-			).toBeVisible();
+				expect( screen.getByText( 'Hint' ) ).toBeVisible();
+			} );
+		}
 
-			// check that new selected item now has aria-selected="true"
-			expect(
-				screen.getByRole( 'option', {
-					name: 'poppy',
-					selected: true,
-				} )
-			).toBeVisible();
+		describe( 'Keyboard behavior and accessibility', () => {
+			it( 'Should be able to change selection using keyboard', async () => {
+				const user = userEvent.setup();
+
+				render( <Component { ...props } /> );
+
+				const currentSelectedItem = screen.getByRole( 'combobox', {
+					expanded: false,
+				} );
+
+				await user.tab();
+				expect( currentSelectedItem ).toHaveFocus();
+
+				await user.keyboard( '{enter}' );
+				expect(
+					screen.getByRole( 'listbox', {
+						name: 'label!',
+					} )
+				).toHaveFocus();
+
+				await user.keyboard( '{arrowdown}' );
+				await user.keyboard( '{enter}' );
+
+				expect( currentSelectedItem ).toHaveTextContent(
+					'crimson clover'
+				);
+			} );
+
+			it( 'Should be able to type characters to select matching options', async () => {
+				const user = userEvent.setup();
+
+				render( <Component { ...props } /> );
+
+				const currentSelectedItem = screen.getByRole( 'combobox', {
+					expanded: false,
+				} );
+
+				await user.tab();
+				await user.keyboard( '{enter}' );
+				expect(
+					screen.getByRole( 'listbox', {
+						name: 'label!',
+					} )
+				).toHaveFocus();
+
+				await user.keyboard( '{a}' );
+				await user.keyboard( '{enter}' );
+				expect( currentSelectedItem ).toHaveTextContent( 'amber' );
+			} );
+
+			it( 'Can change selection with a focused input and closed dropdown if typed characters match an option', async () => {
+				const user = userEvent.setup();
+
+				render( <Component { ...props } /> );
+
+				const currentSelectedItem = screen.getByRole( 'combobox', {
+					expanded: false,
+				} );
+
+				await user.tab();
+				expect( currentSelectedItem ).toHaveFocus();
+
+				await user.keyboard( '{a}' );
+				await user.keyboard( '{q}' );
+
+				expect(
+					screen.queryByRole( 'listbox', {
+						name: 'label!',
+						hidden: true,
+					} )
+				).not.toBeInTheDocument();
+
+				await user.keyboard( '{enter}' );
+				expect( currentSelectedItem ).toHaveTextContent( 'aquamarine' );
+			} );
+
+			it( 'Should have correct aria-selected value for selections', async () => {
+				const user = userEvent.setup();
+
+				render( <Component { ...props } /> );
+
+				const currentSelectedItem = screen.getByRole( 'combobox', {
+					expanded: false,
+				} );
+
+				await user.click( currentSelectedItem );
+
+				// get all items except for first option
+				const unselectedItems = props.options.filter(
+					( { name } ) => name !== props.options[ 0 ].name
+				);
+
+				// assert that all other items have aria-selected="false"
+				unselectedItems.map( ( { name } ) =>
+					expect(
+						screen.getByRole( 'option', { name, selected: false } )
+					).toBeVisible()
+				);
+
+				// assert that first item has aria-selected="true"
+				expect(
+					screen.getByRole( 'option', {
+						name: props.options[ 0 ].name,
+						selected: true,
+					} )
+				).toBeVisible();
+
+				// change the current selection
+				await user.click(
+					screen.getByRole( 'option', { name: 'poppy' } )
+				);
+
+				// click combobox to mount listbox with options again
+				await user.click( currentSelectedItem );
+
+				// check that first item is has aria-selected="false" after new selection
+				expect(
+					screen.getByRole( 'option', {
+						name: props.options[ 0 ].name,
+						selected: false,
+					} )
+				).toBeVisible();
+
+				// check that new selected item now has aria-selected="true"
+				expect(
+					screen.getByRole( 'option', {
+						name: 'poppy',
+						selected: true,
+					} )
+				).toBeVisible();
+			} );
 		} );
 	} );
 } );
