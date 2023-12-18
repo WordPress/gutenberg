159cb0a39b565c2bf031e27644d8ee74e1dfb204
Mario Santos <santosguillamot@gmail.com>
2023-12-18 18:57:02 +0100

    Add post-data and site-data sources in the editor

---
+++ packages/editor/src/components/block-bindings/sources/site-data.js
@@ -0,0 +1,77 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { addFilter } from '@wordpress/hooks';
+/**
+ * Internal dependencies
+ */
+import BlockBindingsFill from '../bindings-ui.js';
+import BlockBindingsFieldsList from '../fields-list.js';
+
+const SiteData = ( props ) => {
+	// TODO: Explore if it makes sense to create a custom endpoint for this.
+	const siteData = useSelect( ( select ) => {
+		const { getEntityRecord } = select( coreStore );
+		return getEntityRecord( 'root', 'site' );
+	}, [] );
+
+	// Adapt the data to the format expected by the fields list.
+	// TODO: Ensure the key and label work with translations.
+	const fields = [
+		{
+			key: 'blogname',
+			label: 'Site title',
+			value: siteData.title,
+		},
+		{
+			key: 'blogdescription',
+			label: 'Site description',
+			value: siteData.description,
+		},
+		{
+			key: 'siteurl',
+			label: 'Site url',
+			value: siteData.description,
+		},
+	];
+
+	return (
+		<BlockBindingsFieldsList
+			fields={ fields }
+			source="site_data"
+			label="Site data"
+			{ ...props }
+		/>
+	);
+};
+
+if ( window.__experimentalConnections ) {
+	const withCoreSources = createHigherOrderComponent(
+		( BlockEdit ) => ( props ) => {
+			const { isSelected } = props;
+
+			return (
+				<>
+					{ isSelected && (
+						<>
+							<BlockBindingsFill>
+								<SiteData />
+							</BlockBindingsFill>
+						</>
+					) }
+					<BlockEdit key="edit" { ...props } />
+				</>
+			);
+		},
+		'withToolbarControls'
+	);
+
+	addFilter(
+		'editor.BlockEdit',
+		'core/block-bindings-ui/add-sources',
+		withCoreSources
+	);
+}
