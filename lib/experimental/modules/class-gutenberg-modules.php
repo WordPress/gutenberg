00423c4ab73eb1591bca9621ce409a18e9dc92a3
Luis Herranz <luisherranz@gmail.com>
2023-12-19 19:18:29 +0100

    Refactor and finish tests

--- lib/experimental/modules/class-gutenberg-modules.php
+++ lib/experimental/modules/class-gutenberg-modules.php
@@ -20,20 +20,20 @@ class Gutenberg_Modules {
 	private static $registered = array();
 
 	/**
-	 * An array of modules that were enqueued before they were registered.
+	 * An array of module identifiers that were enqueued before registered.
 	 *
 	 * @var array
 	 */
 	private static $enqueued_modules_before_register = array();
 
 	/**
-	 * Registers the module if no module with that module identifier already
-	 * exists.
+	 * Registers the module if no module with that module identifier has already
+	 * been registered.
 	 *
-	 * @param string           $module_identifier The identifier of the module. Should be unique. It will be used in the final import map.
-	 * @param string           $src               Full URL of the module, or path of the script relative to the WordPress root directory.
-	 * @param array            $dependencies      Optional. An array of module identifiers of the dependencies of this module. The dependencies can be strings or arrays. If they are arrays, they need an `id` key with the module identifier, and can contain a `type` key with either `static` or `dynamic`. By default, dependencies that don't contain a type are considered static.
-	 * @param string|bool|null $version           Optional. Defaults to false. String specifying module version number. It is added to the URL as a query string for cache busting purposes. If SCRIPT_DEBUG is true, a timestamp is used. If it is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+	 * @param string            $module_identifier The identifier of the module. Should be unique. It will be used in the final import map.
+	 * @param string            $src               Full URL of the module, or path of the script relative to the WordPress root directory.
+	 * @param array             $dependencies      Optional. An array of module identifiers of the dependencies of this module. The dependencies can be strings or arrays. If they are arrays, they need an `id` key with the module identifier, and can contain a `type` key with either `static` or `dynamic`. By default, dependencies that don't contain a type are considered static.
+	 * @param string|false|null $version           Optional. String specifying module version number. Defaults to false. It is added to the URL as a query string for cache busting purposes. If SCRIPT_DEBUG is true, the version is the current timestamp. If $version is set to false, the version number is the currently installed WordPress version. If $version is set to null, no version is added.
 	 */
 	public static function register( $module_identifier, $src, $dependencies = array(), $version = false ) {
 		if ( ! isset( self::$registered[ $module_identifier ] ) ) {
@@ -58,29 +58,41 @@ class Gutenberg_Modules {
 				'enqueued'     => in_array( $module_identifier, self::$enqueued_modules_before_register, true ),
 				'dependencies' => $deps,
 			);
-
-		} else {
-			// TODO: Check version, and if it's bigger, override.
 		}
 	}
 
 	/**
-	 * Enqueues a module in the page.
+	 * Marks the module to be enqueued in the page.
 	 *
 	 * @param string $module_identifier The identifier of the module.
 	 */
 	public static function enqueue( $module_identifier ) {
 		if ( isset( self::$registered[ $module_identifier ] ) ) {
 			self::$registered[ $module_identifier ]['enqueued'] = true;
-		} else {
+		} elseif ( ! in_array( $module_identifier, self::$enqueued_modules_before_register, true ) ) {
 			self::$enqueued_modules_before_register[] = $module_identifier;
+		}
+	}
+
+	/**
+	 * Unmarks the module so it is not longer enqueued in the page.
+	 *
+	 * @param string $module_identifier The identifier of the module.
+	 */
+	public static function dequeue( $module_identifier ) {
+		if ( isset( self::$registered[ $module_identifier ] ) ) {
+			self::$registered[ $module_identifier ]['enqueued'] = false;
+		}
+		$key = array_search( $module_identifier, self::$enqueued_modules_before_register, true );
+		if ( false !== $key ) {
+			array_splice( self::$enqueued_modules_before_register, $key, 1 );
 		}
 	}
 
 	/**
 	 * Returns the import map array.
 	 *
-	 * @return array Associative array with 'imports' key mapping to an array of module identifiers and their respective source strings.
+	 * @return array Array with an 'imports' key mapping to an array of module identifiers and their respective source URLs, including the version query.
 	 */
 	public static function get_import_map() {
 		$imports = array();
@@ -91,7 +103,7 @@ class Gutenberg_Modules {
 	}
 
 	/**
-	 * Prints the import map.
+	 * Prints the import map using <script type="importmap">.
 	 */
 	public static function print_import_map() {
 		$import_map = self::get_import_map();
@@ -116,12 +128,13 @@ class Gutenberg_Modules {
 	}
 
 	/**
-	 * Prints the link tag with rel="modulepreload" for all the static
-	 * dependencies of the enqueued modules.
+	 * Prints the the static dependencies of the enqueued modules using <link rel="modulepreload">.
 	 */
 	public static function print_preloaded_modules() {
 		foreach ( self::get_dependencies( array_keys( self::get_enqueued() ), array( 'static' ) ) as $module_identifier => $module ) {
+			if ( true !== $module['enqueued'] ) {
 				echo '<link rel="modulepreload" href="' . $module['src'] . self::get_version_query_string( $module['version'] ) . '" id="' . $module_identifier . '">';
+			}
 		}
 	}
 
@@ -146,9 +159,11 @@ class Gutenberg_Modules {
 	}
 
 	/**
-	 * Gets the module's version. It either returns a timestamp (if SCRIPT_DEBUG
-	 * is true), the explicit version of the module if it is set and not false, or
-	 * an empty string if none of the above conditions are met.
+	 * Gets the version of a module.
+	 *
+	 * If SCRIPT_DEBUG is true, the version is the current timestamp. If $version
+	 * is set to false, the version number is the currently installed WordPress
+	 * version. If $version is set to null, no version is added.
 	 *
 	 * @param array $version The version of the module.
 	 * @return string A string presenting the version.
@@ -167,10 +182,7 @@ class Gutenberg_Modules {
 	/**
 	 * Retrieves an array of enqueued modules.
 	 *
-	 * Compiles an associative array of modules that have been enqueued for use on
-	 * the page, containing module information such as source and version.
-	 *
-	 * @return array Associative array of enqueued modules keyed by module identifier.
+	 * @return array Array of modules keyed by module identifier.
 	 */
 	private static function get_enqueued() {
 		$enqueued = array();
@@ -183,7 +195,7 @@ class Gutenberg_Modules {
 	}
 
 	/**
-	 * Retrieves all unique dependencies for given modules depending on type.
+	 * Retrieves all the dependencies for given modules depending on type.
 	 *
 	 * This method is recursive to also retrieve dependencies of the dependencies.
 	 * It will consolidate an array containing unique dependencies based on the
@@ -191,7 +203,7 @@ class Gutenberg_Modules {
 	 *
 	 * @param array $module_identifiers The identifiers of the modules for which to gather dependencies.
 	 * @param array $types              Optional. Types of dependencies to retrieve: 'static', 'dynamic', or both. Default is both.
-	 * @return array Associative array of modules keyed by module identifier.
+	 * @return array Array of modules keyed by module identifier.
 	 */
 	private static function get_dependencies( $module_identifiers, $types = array( 'static', 'dynamic' ) ) {
 		return array_reduce(
@@ -199,7 +211,11 @@ class Gutenberg_Modules {
 			function ( $dependency_modules, $module_identifier ) use ( $types ) {
 				$dependencies = array();
 				foreach ( self::$registered[ $module_identifier ]['dependencies'] as $dependency ) {
-					if ( in_array( $dependency['type'], $types, true ) && isset( self::$registered[ $dependency['id'] ] ) ) {
+					if (
+						in_array( $dependency['type'], $types, true ) &&
+						isset( self::$registered[ $dependency['id'] ] ) &&
+						! isset( $dependency_modules[ $dependency['id'] ] )
+					) {
 						$dependencies[ $dependency['id'] ] = self::$registered[ $dependency['id'] ];
 					}
 				}
@@ -211,25 +227,34 @@ class Gutenberg_Modules {
 }
 
 /**
- * Registers a JavaScript module. It will be added to the import map.
+ * Registers the module if no module with that module identifier has already
+ * been registered.
  *
- * @param string           $module_identifier The identifier of the module. Should be unique. It will be used in the final import map.
- * @param string           $src               Full URL of the module, or path of the script relative to the WordPress root directory.
- * @param array            $dependencies      Optional. An array of module identifiers of the dependencies of this module. The dependencies can be strings or arrays. If they are arrays, they need an `id` key with the module identifier, and can contain a `type` key with either `static` or `dynamic`. By default, dependencies are considered static.
- * @param string|bool|null $version           Optional. Defaults to false. String specifying module version number. It is added to the URL as a query string for cache busting purposes. If SCRIPT_DEBUG is true, a timestamp is used. If it is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+ * @param string            $module_identifier The identifier of the module. Should be unique. It will be used in the final import map.
+ * @param string            $src               Full URL of the module, or path of the script relative to the WordPress root directory.
+ * @param array             $dependencies      Optional. An array of module identifiers of the dependencies of this module. The dependencies can be strings or arrays. If they are arrays, they need an `id` key with the module identifier, and can contain a `type` key with either `static` or `dynamic`. By default, dependencies that don't contain a type are considered static.
+ * @param string|false|null $version           Optional. String specifying module version number. Defaults to false. It is added to the URL as a query string for cache busting purposes. If SCRIPT_DEBUG is true, the version is the current timestamp. If $version is set to false, the version number is the currently installed WordPress version. If $version is set to null, no version is added.
  */
 function gutenberg_register_module( $module_identifier, $src, $dependencies = array(), $version = false ) {
 	Gutenberg_Modules::register( $module_identifier, $src, $dependencies, $version );
 }
 
 /**
- * Enqueues a JavaScript module. It will be added to both the import map and a
- * script tag with the "module" type.
+ * Marks the module to be enqueued in the page.
  *
- * @param string $module_identifier The identifier of the module. Should be unique. It will be used in the final import map.
+ * @param string $module_identifier The identifier of the module.
  */
 function gutenberg_enqueue_module( $module_identifier ) {
 	Gutenberg_Modules::enqueue( $module_identifier );
+}
+
+/**
+ * Unmarks the module so it is not longer enqueued in the page.
+ *
+ * @param string $module_identifier The identifier of the module.
+ */
+function gutenberg_dequeue_module( $module_identifier ) {
+	Gutenberg_Modules::dequeue( $module_identifier );
 }
 
 // Prints the import map in the head tag.
--- phpunit/experimental/modules/class-gutenberg-modules-test.php
+++ phpunit/experimental/modules/class-gutenberg-modules-test.php
@@ -25,12 +25,7 @@ class Gutenberg_Modules_Test extends WP_UnitTestCase {
 		parent::tear_down();
 	}
 
-	public function test_gutenberg_enqueue_module() {
-		gutenberg_register_module( '@wordpress/some-module', '/some-module.js' );
-		gutenberg_enqueue_module( '@wordpress/some-module' );
-		gutenberg_register_module( '@wordpress/other-module', '/other-module.js' );
-		gutenberg_enqueue_module( '@wordpress/other-module' );
-
+	public function get_enqueued_modules() {
 		$modules_markup   = get_echo( array( 'Gutenberg_Modules', 'print_enqueued_modules' ) );
 		$p                = new WP_HTML_Tag_Processor( $modules_markup );
 		$enqueued_modules = array();
@@ -44,122 +39,16 @@ class Gutenberg_Modules_Test extends WP_UnitTestCase {
 			$enqueued_modules[ $p->get_attribute( 'id' ) ] = $p->get_attribute( 'src' );
 		}
 
-		$this->assertEquals( 2, count( $enqueued_modules ) );
-		$this->assertEquals( true, strpos( $enqueued_modules['@wordpress/some-module'], '/some-module.js' ) === 0 );
-		$this->assertEquals( true, strpos( $enqueued_modules['@wordpress/other-module'], '/other-module.js' ) === 0 );
+		return $enqueued_modules;
 	}
 
-	public function test_gutenberg_enqueue_module_works_before_register() {
-		gutenberg_enqueue_module( '@wordpress/some-module' );
-		gutenberg_register_module( '@wordpress/some-module', '/some-module.js' );
-		gutenberg_enqueue_module( '@wordpress/other-module' ); // Not enqueued.
-
-		$modules_markup   = get_echo( array( 'Gutenberg_Modules', 'print_enqueued_modules' ) );
-		$p                = new WP_HTML_Tag_Processor( $modules_markup );
-		$enqueued_modules = array();
-
-		while ( $p->next_tag(
-			array(
-				'tag'  => 'SCRIPT',
-				'type' => 'module',
-			)
-		) ) {
-			$enqueued_modules[ $p->get_attribute( 'id' ) ] = $p->get_attribute( 'src' );
-		}
-
-		$this->assertEquals( 1, count( $enqueued_modules ) );
-		$this->assertEquals( true, strpos( $enqueued_modules['@wordpress/some-module'], '/some-module.js' ) === 0 );
-	}
-
-	public function test_gutenberg_import_map_dependencies() {
-		gutenberg_register_module( '@wordpress/no-dep', '/no-dep.js' );
-		gutenberg_register_module( '@wordpress/some-dep', '/some-dep.js' );
-		gutenberg_register_module( '@wordpress/some-module', '/some-module.js', array( '@wordpress/some-dep' ) );
-		gutenberg_enqueue_module( '@wordpress/some-module' );
-
+	public function get_import_map() {
 		$import_map_markup = get_echo( array( 'Gutenberg_Modules', 'print_import_map' ) );
 		preg_match( '/<script type="importmap">([^<]+)<\/script>/s', $import_map_markup, $import_map_string );
-		$import_map = json_decode( $import_map_string[1], true )['imports'];
-
-		$this->assertEquals( true, strpos( $import_map['@wordpress/some-dep'], '/some-dep.js' ) === 0 );
-		$this->assertEquals( false, isset( $import_map['@wordpress/no-dep'] ) );
+		return json_decode( $import_map_string[1], true )['imports'];
 	}
 
-	public function test_gutenberg_import_map_recursive_dependencies() {
-		gutenberg_register_module( '@wordpress/no-dep', '/no-dep.js' );
-		gutenberg_register_module( '@wordpress/some-nested-static-dep', '/some-nested-static-dep.js' );
-		gutenberg_register_module( '@wordpress/some-nested-dynamic-dep', '/some-nested-dynamic-dep.js' );
-		gutenberg_register_module(
-			'@wordpress/some-static-dep',
-			'/some-static-dep.js',
-			array(
-				array(
-					'id'   => '@wordpress/some-nested-static-dep',
-					'type' => 'static',
-				),
-				array(
-					'id'   => '@wordpress/some-nested-dynamic-dep',
-					'type' => 'dynamic',
-				),
-			)
-		);
-		gutenberg_register_module( '@wordpress/some-dynamic-dep', '/some-dynamic-dep.js' );
-		gutenberg_register_module(
-			'@wordpress/some-module',
-			'/some-module.js',
-			array(
-				'@wordpress/some-static-dep',
-				array(
-					'id'   => '@wordpress/some-dynamic-dep',
-					'type' => 'dynamic',
-				),
-			)
-		);
-		gutenberg_enqueue_module( '@wordpress/some-module' );
-
-		$import_map_markup = get_echo( array( 'Gutenberg_Modules', 'print_import_map' ) );
-		preg_match( '/<script type="importmap">([^<]+)<\/script>/s', $import_map_markup, $import_map_string );
-		$import_map = json_decode( $import_map_string[1], true )['imports'];
-
-		$this->assertEquals( true, strpos( $import_map['@wordpress/some-static-dep'], '/some-static-dep.js' ) === 0 );
-		$this->assertEquals( true, strpos( $import_map['@wordpress/some-dynamic-dep'], '/some-dynamic-dep.js' ) === 0 );
-		$this->assertEquals( true, strpos( $import_map['@wordpress/some-nested-static-dep'], '/some-nested-static-dep.js' ) === 0 );
-		$this->assertEquals( true, strpos( $import_map['@wordpress/some-nested-dynamic-dep'], '/some-nested-dynamic-dep.js' ) === 0 );
-		$this->assertEquals( false, isset( $import_map['@wordpress/no-dep'] ) );
-	}
-
-	public function test_gutenberg_enqueue_module_preloads_static_dependencies() {
-		gutenberg_register_module( '@wordpress/no-dep', '/no-dep.js' );
-		gutenberg_register_module( '@wordpress/some-nested-static-dep', '/some-nested-static-dep.js' );
-		gutenberg_register_module( '@wordpress/some-nested-dynamic-dep', '/some-nested-dynamic-dep.js' );
-		gutenberg_register_module(
-			'@wordpress/some-static-dep',
-			'/some-static-dep.js',
-			array(
-				array(
-					'id'   => '@wordpress/some-nested-static-dep',
-					'type' => 'static',
-				),
-				array(
-					'id'   => '@wordpress/some-nested-dynamic-dep',
-					'type' => 'dynamic',
-				),
-			)
-		);
-		gutenberg_register_module( '@wordpress/some-dynamic-dep', '/some-dynamic-dep.js', array( '@wordpress/some-nested-dep' ) );
-		gutenberg_register_module(
-			'@wordpress/some-module',
-			'/some-module.js',
-			array(
-				'@wordpress/some-static-dep',
-				array(
-					'id'   => 'some-dynamic-dep',
-					'type' => 'dynamic',
-				),
-			)
-		);
-		gutenberg_enqueue_module( '@wordpress/some-module' );
-
+	public function get_preloaded_modules() {
 		$preloaded_markup  = get_echo( array( 'Gutenberg_Modules', 'print_preloaded_modules' ) );
 		$p                 = new WP_HTML_Tag_Processor( $preloaded_markup );
 		$preloaded_modules = array();
@@ -173,12 +62,211 @@ class Gutenberg_Modules_Test extends WP_UnitTestCase {
 			$preloaded_modules[ $p->get_attribute( 'id' ) ] = $p->get_attribute( 'href' );
 		}
 
+		return $preloaded_modules;
+	}
+
+	public function test_gutenberg_enqueue_module() {
+		gutenberg_register_module( 'foo', '/foo.js' );
+		gutenberg_register_module( 'bar', '/bar.js' );
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'bar' );
+
+		$enqueued_modules = $this->get_enqueued_modules();
+
+		$this->assertEquals( 2, count( $enqueued_modules ) );
+		$this->assertEquals( true, str_starts_with( $enqueued_modules['foo'], '/foo.js' ) );
+		$this->assertEquals( true, str_starts_with( $enqueued_modules['bar'], '/bar.js' ) );
+	}
+
+	public function test_gutenberg_dequeue_module() {
+		gutenberg_register_module( 'foo', '/foo.js' );
+		gutenberg_register_module( 'bar', '/bar.js' );
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'bar' );
+		gutenberg_dequeue_module( 'foo' ); // Dequeued.
+
+		$enqueued_modules = $this->get_enqueued_modules();
+
+		$this->assertEquals( 1, count( $enqueued_modules ) );
+		$this->assertEquals( false, isset( $enqueued_modules['foo'] ) );
+		$this->assertEquals( true, isset( $enqueued_modules['bar'] ) );
+	}
+
+	public function test_gutenberg_enqueue_module_works_before_register() {
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_register_module( 'foo', '/foo.js' );
+		gutenberg_enqueue_module( 'bar' ); // Not registered.
+
+		$enqueued_modules = $this->get_enqueued_modules();
+
+		$this->assertEquals( 1, count( $enqueued_modules ) );
+		$this->assertEquals( true, str_starts_with( $enqueued_modules['foo'], '/foo.js' ) );
+		$this->assertEquals( false, isset( $enqueued_modules['bar'] ) );
+	}
+
+	public function test_gutenberg_dequeue_module_works_before_register() {
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'bar' );
+		gutenberg_dequeue_module( 'foo' );
+		gutenberg_register_module( 'foo', '/foo.js' );
+		gutenberg_register_module( 'bar', '/bar.js' );
+
+		$enqueued_modules = $this->get_enqueued_modules();
+
+		$this->assertEquals( 1, count( $enqueued_modules ) );
+		$this->assertEquals( false, isset( $enqueued_modules['foo'] ) );
+		$this->assertEquals( true, isset( $enqueued_modules['bar'] ) );
+	}
+
+	public function test_gutenberg_import_map_dependencies() {
+		gutenberg_register_module( 'foo', '/foo.js', array( 'dep' ) );
+		gutenberg_register_module( 'dep', '/dep.js' );
+		gutenberg_register_module( 'no-dep', '/no-dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+
+		$import_map = $this->get_import_map();
+
+		$this->assertEquals( 1, count( $import_map ) );
+		$this->assertEquals( true, str_starts_with( $import_map['dep'], '/dep.js' ) );
+		$this->assertEquals( false, isset( $import_map['no-dep'] ) );
+	}
+
+	public function test_gutenberg_import_map_no_duplicate_dependencies() {
+		gutenberg_register_module( 'foo', '/foo.js', array( 'dep' ) );
+		gutenberg_register_module( 'bar', '/bar.js', array( 'dep' ) );
+		gutenberg_register_module( 'dep', '/dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'bar' );
+
+		$import_map = $this->get_import_map();
+
+		$this->assertEquals( 1, count( $import_map ) );
+		$this->assertEquals( true, str_starts_with( $import_map['dep'], '/dep.js' ) );
+	}
+
+	public function test_gutenberg_import_map_recursive_dependencies() {
+		gutenberg_register_module(
+			'foo',
+			'/foo.js',
+			array(
+				'static-dep',
+				array(
+					'id'   => 'dynamic-dep',
+					'type' => 'dynamic',
+				),
+			)
+		);
+		gutenberg_register_module(
+			'static-dep',
+			'/static-dep.js',
+			array(
+				array(
+					'id'   => 'nested-static-dep',
+					'type' => 'static',
+				),
+				array(
+					'id'   => 'nested-dynamic-dep',
+					'type' => 'dynamic',
+				),
+			)
+		);
+		gutenberg_register_module( 'dynamic-dep', '/dynamic-dep.js' );
+		gutenberg_register_module( 'nested-static-dep', '/nested-static-dep.js' );
+		gutenberg_register_module( 'nested-dynamic-dep', '/nested-dynamic-dep.js' );
+		gutenberg_register_module( 'no-dep', '/no-dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+
+		$import_map = $this->get_import_map();
+
+		$this->assertEquals( true, str_starts_with( $import_map['static-dep'], '/static-dep.js' ) );
+		$this->assertEquals( true, str_starts_with( $import_map['dynamic-dep'], '/dynamic-dep.js' ) );
+		$this->assertEquals( true, str_starts_with( $import_map['nested-static-dep'], '/nested-static-dep.js' ) );
+		$this->assertEquals( true, str_starts_with( $import_map['nested-dynamic-dep'], '/nested-dynamic-dep.js' ) );
+		$this->assertEquals( false, isset( $import_map['no-dep'] ) );
+	}
+
+	public function test_gutenberg_enqueue_preloaded_static_dependencies() {
+		gutenberg_register_module(
+			'foo',
+			'/foo.js',
+			array(
+				'static-dep',
+				array(
+					'id'   => 'dynamic-dep',
+					'type' => 'dynamic',
+				),
+			)
+		);
+		gutenberg_register_module(
+			'static-dep',
+			'/static-dep.js',
+			array(
+				array(
+					'id'   => 'nested-static-dep',
+					'type' => 'static',
+				),
+				array(
+					'id'   => 'nested-dynamic-dep',
+					'type' => 'dynamic',
+				),
+			)
+		);
+		gutenberg_register_module( 'dynamic-dep', '/dynamic-dep.js' );
+		gutenberg_register_module( 'nested-static-dep', '/nested-static-dep.js' );
+		gutenberg_register_module( 'nested-dynamic-dep', '/nested-dynamic-dep.js' );
+		gutenberg_register_module( 'no-dep', '/no-dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+
+		$preloaded_modules = $this->get_preloaded_modules();
+
 		$this->assertEquals( 2, count( $preloaded_modules ) );
-		$this->assertEquals( true, strpos( $preloaded_modules['@wordpress/some-static-dep'], '/some-static-dep.js' ) === 0 );
-		$this->assertEquals( true, strpos( $preloaded_modules['@wordpress/some-nested-static-dep'], '/some-nested-static-dep.js' ) === 0 );
-		$this->assertEquals( false, isset( $import_map['@wordpress/no-dep'] ) );
-		$this->assertEquals( false, isset( $import_map['@wordpress/some-dynamic-dep'] ) );
-		$this->assertEquals( false, isset( $import_map['@wordpress/some-nested-dynamic-dep'] ) );
+		$this->assertEquals( true, str_starts_with( $preloaded_modules['static-dep'], '/static-dep.js' ) );
+		$this->assertEquals( true, str_starts_with( $preloaded_modules['nested-static-dep'], '/nested-static-dep.js' ) );
+		$this->assertEquals( false, isset( $import_map['no-dep'] ) );
+		$this->assertEquals( false, isset( $import_map['dynamic-dep'] ) );
+		$this->assertEquals( false, isset( $import_map['nested-dynamic-dep'] ) );
+	}
+
+	public function test_gutenberg_preloaded_dependencies_filter_enqueued_modules() {
+		gutenberg_register_module(
+			'foo',
+			'/foo.js',
+			array(
+				'dep',
+				'enqueued-dep',
+			)
+		);
+		gutenberg_register_module( 'dep', '/dep.js' );
+		gutenberg_register_module( 'enqueued-dep', '/enqueued-dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'enqueued-dep' ); // Not preloaded.
+
+		$preloaded_modules = $this->get_preloaded_modules();
+
+		$this->assertEquals( 1, count( $preloaded_modules ) );
+		$this->assertEquals( true, isset( $preloaded_modules['dep'] ) );
+		$this->assertEquals( false, isset( $preloaded_modules['enqueued-dep'] ) );
+	}
+
+	public function test_gutenberg_enqueued_modules_with_dependants_add_import_map() {
+		gutenberg_register_module(
+			'foo',
+			'/foo.js',
+			array(
+				'dep',
+				'enqueued-dep',
+			)
+		);
+		gutenberg_register_module( 'dep', '/dep.js' );
+		gutenberg_register_module( 'enqueued-dep', '/enqueued-dep.js' );
+		gutenberg_enqueue_module( 'foo' );
+		gutenberg_enqueue_module( 'enqueued-dep' ); // Also in the import map.
+
+		$import_map = $this->get_import_map();
+
+		$this->assertEquals( 2, count( $import_map ) );
+		$this->assertEquals( true, isset( $import_map['dep'] ) );
+		$this->assertEquals( true, isset( $import_map['enqueued-dep'] ) );
 	}
 
 	public function test_get_version_query_string() {
@@ -194,6 +282,4 @@ class Gutenberg_Modules_Test extends WP_UnitTestCase {
 		$result = $get_version_query_string->invoke( null, null );
 		$this->assertEquals( '', $result );
 	}
-
-	// public function test_gutenberg_dont_crash_on_missing_dependency() {}.
 }
