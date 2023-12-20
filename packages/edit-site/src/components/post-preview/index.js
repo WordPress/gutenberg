7fbe60458d2655ee444711a23c06577e50d00a2a
André <583546+oandregal@users.noreply.github.com>
2023-12-20 12:13:40 +0100

    DataViews: improve preview (#57116)

--- packages/edit-site/src/components/post-preview/index.js
+++ packages/edit-site/src/components/post-preview/index.js
@@ -3,12 +3,14 @@
  */
 import Editor from '../editor';
 import { useInitEditedEntity } from '../sync-state-with-url/use-init-edited-entity-from-url';
+import { useIsSiteEditorLoading } from '../layout/hooks';
 
 export default function PostPreview( { postType, postId } ) {
 	useInitEditedEntity( {
 		postId,
 		postType,
 	} );
+	const isEditorLoading = useIsSiteEditorLoading();
 
-	return <Editor />;
+	return <Editor isLoading={ isEditorLoading } />;
 }
