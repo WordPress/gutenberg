c3f709a329dba397e46f78ce784eeed3017dc989
Mario Santos <santosguillamot@gmail.com>
2023-12-20 12:26:26 +0100

    Add post meta source using Fill and component

--- packages/editor/src/hooks/index.js
+++ packages/editor/src/hooks/index.js
@@ -4,3 +4,6 @@
 import './custom-sources-backwards-compatibility';
 import './default-autocompleters';
 import './pattern-partial-syncing';
+
+// Block bindings sources.
+import './block-bindings-sources/post-meta';
