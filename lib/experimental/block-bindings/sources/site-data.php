41fb4e6377d071b8a5cf873d226c2e8eaaa64f16
Mario Santos <santosguillamot@gmail.com>
2023-12-20 13:35:42 +0100

    Remove unused attributes

--- lib/experimental/block-bindings/sources/site-data.php
+++ lib/experimental/block-bindings/sources/site-data.php
@@ -6,7 +6,7 @@
  */
 
 if ( function_exists( 'register_block_bindings_source' ) ) {
-	$site_data_source_callback = function ( $source_attrs, $block_content, $block, $block_instance ) {
+	$site_data_source_callback = function ( $source_attrs ) {
 		return get_option( $source_attrs['value'] );
 	};
 	register_block_bindings_source(
