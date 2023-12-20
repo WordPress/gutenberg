588fc12533fbe465bf26db1d27f78f8c003e0cd4
Ramon <ramonjd@users.noreply.github.com>
2023-12-21 07:26:26 +1100

    Global styles revisions: integrate style book (#56800)
    * first commit
    
    * Passing down an actions array to the editor site canvas - here we can add a style book toggle for revisions.
    
    * Add e2e test
    
    * If you open revisions then style book, the close button (in the canvas area) will exit style book, but the revisions panel remains open.
    If you open style book then open revisions, style book will remain toggled on.
    
    Render the style book window immediately to avoid a flash of the underlying editor
    
    * Updating e2e tests
    
    * remove alt color for style book
    
    * If you open both Style Book and Revisions, then close Revisions, the Style Book remains open
    Added e2e tests to reflect this

--- test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
+++ test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
@@ -145,6 +145,50 @@ test.describe( 'Global styles revisions', () => {
 			page.getByLabel( 'Global styles revisions list' )
 		).toBeVisible();
 	} );
+
+	test( 'should allow switching to style book view', async ( {
+		page,
+		editor,
+		userGlobalStylesRevisions,
+	} ) => {
+		await editor.canvas.locator( 'body' ).click();
+		await userGlobalStylesRevisions.openStylesPanel();
+		const revisionsButton = page.getByRole( 'button', {
+			name: 'Revisions',
+		} );
+		const styleBookButton = page.getByRole( 'button', {
+			name: 'Style Book',
+		} );
+		await revisionsButton.click();
+		// We can see the Revisions list.
+		await expect(
+			page.getByLabel( 'Global styles revisions list' )
+		).toBeVisible();
+		await expect(
+			page.locator( 'iframe[name="revisions"]' )
+		).toBeVisible();
+		await expect(
+			page.locator( 'iframe[name="style-book-canvas"]' )
+		).toBeHidden();
+		await styleBookButton.click();
+		await expect(
+			page.locator( 'iframe[name="style-book-canvas"]' )
+		).toBeVisible();
+		await expect( page.locator( 'iframe[name="revisions"]' ) ).toBeHidden();
+
+		// Deactivating revisions view while the style book is open should close revisions,
+		// but not the style book.
+		await revisionsButton.click();
+
+		// Style book is still visible but...
+		await expect(
+			page.locator( 'iframe[name="style-book-canvas"]' )
+		).toBeVisible();
+		// The Revisions list is hidden.
+		await expect(
+			page.getByLabel( 'Global styles revisions list' )
+		).toBeHidden();
+	} );
 } );
 
 class UserGlobalStylesRevisions {
