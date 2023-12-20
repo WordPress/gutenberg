712467e8fa80c4879a94d27c7723c64d562dfd89
Mario Santos <santosguillamot@gmail.com>
2023-12-20 08:11:27 +0100

    Add pattern source

---
+++ lib/experimental/block-bindings/sources/pattern.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Add the metadata source to the block bindings API.
+ *
+ * @package gutenberg
+ */
+
+if ( function_exists( 'register_block_bindings_source' ) ) {
+	$pattern_source_callback = function ( $source_attrs, $block_content, $block, $block_instance ) {
+		if ( ! _wp_array_get( $block_instance->attributes, array( 'metadata', 'id' ), false ) ) {
+			return;
+		}
+		$block_id = $block_instance->attributes['metadata']['id'];
+		return _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id ), false );
+	};
+	register_block_bindings_source(
+		'pattern_attributes',
+		array(
+			'label' => __( 'Pattern Attributes' ),
+			'apply' => $pattern_source_callback,
+		)
+	);
+}
