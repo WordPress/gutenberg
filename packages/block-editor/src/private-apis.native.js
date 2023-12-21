3991a708ca6f3c0a33fb89ce47f600d40cabb62b
Ella <ella@vandurpe.com>
2023-12-21 09:40:39 +0200

    Add private keys for native

--- packages/block-editor/src/private-apis.native.js
+++ packages/block-editor/src/private-apis.native.js
@@ -4,6 +4,7 @@
 import * as globalStyles from './components/global-styles';
 import { ExperimentalBlockEditorProvider } from './components/provider';
 import { lock } from './lock-unlock';
+import { default as settingsKeys } from './private-settings-keys';
 
 /**
  * Private @wordpress/block-editor APIs.
@@ -12,4 +13,5 @@ export const privateApis = {};
 lock( privateApis, {
 	...globalStyles,
 	ExperimentalBlockEditorProvider,
+	settingsKeys,
 } );
