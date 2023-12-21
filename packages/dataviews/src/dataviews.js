2122329dbe1cf9907b6e6c8665d2cb30df1bd467
André Maneiro <583546+oandregal@users.noreply.github.com>
2023-12-21 16:19:39 +0100

    Make deferredRendering optional by providing a good default (false).

--- packages/dataviews/src/dataviews.js
+++ packages/dataviews/src/dataviews.js
@@ -31,7 +31,7 @@ export default function DataViews( {
 	paginationInfo,
 	supportedLayouts,
 	onSelectionChange,
-	deferredRendering,
+	deferredRendering = false,
 } ) {
 	const [ selection, setSelection ] = useState( [] );
 
