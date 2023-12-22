65d2cf912ecbf873888c0fb6bd501b01c55f5df2
Mario Santos <santosguillamot@gmail.com>
2023-12-22 16:10:12 +0100

    Fix PHP logic for tax_meta source

--- lib/experimental/block-bindings/sources/tax-meta.php
+++ lib/experimental/block-bindings/sources/tax-meta.php
@@ -7,7 +7,15 @@
 
 if ( function_exists( 'register_block_bindings_source' ) ) {
 	$term_meta_source_callback = function ( $source_attrs ) {
-		return get_metadata( 'term', $source_attrs['termId'], $source_attrs['value'] );
+		// Use the postId attribute if available, otherwise get it from the context.
+		if ( isset( $source_attrs['termId'] ) ) {
+			$term_id = $source_attrs['termId'];
+		} else {
+			// I tried using $block_instance->context['postId'] but it wasn't available in the image block.
+			$term_id = get_queried_object_id();
+		}
+
+		return get_metadata( 'term', $term_id, $source_attrs['value'] )[0];
 	};
 	register_block_bindings_source(
 		'tax_meta',
