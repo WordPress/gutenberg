ec51bda7727dd252f2252c4727d58f4f06be8576
Luis Herranz <luisherranz@gmail.com>
2023-12-24 16:46:22 +0100

    Fix wp-style tests

--- phpunit/experimental/interactivity-api/directives/wp-style-test.php
+++ phpunit/experimental/interactivity-api/directives/wp-style-test.php
@@ -31,7 +31,7 @@ class Tests_Directives_WpStyle extends WP_UnitTestCase {
 	}
 
 	public function test_directive_ignores_empty_style() {
-		$markup = '<div data-wp-style.="context.myblock.color" style="background: blue;">Test</div>';
+		$markup = '<div data-wp-style="context.myblock.color" style="background: blue;">Test</div>';
 		$tags   = new WP_HTML_Tag_Processor( $markup );
 		$tags->next_tag();
 
@@ -41,6 +41,19 @@ class Tests_Directives_WpStyle extends WP_UnitTestCase {
 
 		$this->assertSame( $markup, $tags->get_updated_html() );
 		$this->assertStringNotContainsString( 'color: green;', $tags->get_attribute( 'style' ) );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-style directive changed context' );
+	}
+
+	public function test_directive_works_without_style_attribute() {
+		$markup = '<div data-wp-style--color="context.myblock.color">Test</div>';
+		$tags   = new WP_HTML_Tag_Processor( $markup );
+		$tags->next_tag();
+
+		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'color' => 'green' ) ) );
+		$context        = $context_before;
+		gutenberg_interactivity_process_wp_style( $tags, $context );
+
+		$this->assertSame( 'color: green;', $tags->get_attribute( 'style' ) );
 		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-style directive changed context' );
 	}
 }
