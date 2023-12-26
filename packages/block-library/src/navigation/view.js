31fca4a2e9fe9ec58de021f2e40095df165ebf84
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-12-26 17:42:59 +0900

    Don't use dom package

--- packages/block-library/src/navigation/view.js
+++ packages/block-library/src/navigation/view.js
@@ -1,8 +1,17 @@
 /**
  * WordPress dependencies
  */
-import { focus } from '@wordpress/dom';
 import { store, getContext, getElement } from '@wordpress/interactivity';
+
+const focusableSelectors = [
+	'a[href]',
+	'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+	'select:not([disabled]):not([aria-hidden])',
+	'textarea:not([disabled]):not([aria-hidden])',
+	'button:not([disabled]):not([aria-hidden])',
+	'[contenteditable]',
+	'[tabindex]:not([tabindex^="-"])',
+];
 
 // This is a fix for Safari in iOS/iPadOS. Without it, Safari doesn't focus out
 // when the user taps in the body. It can be removed once we add an overlay to
@@ -160,7 +169,8 @@ const { state, actions } = store( 'core/navigation', {
 			const ctx = getContext();
 			const { ref } = getElement();
 			if ( state.isMenuOpen ) {
-				const focusableElements = focus.tabbable.find( ref );
+				const focusableElements =
+					ref.querySelectorAll( focusableSelectors );
 				ctx.modal = ref;
 				ctx.firstFocusableElement = focusableElements[ 0 ];
 				ctx.lastFocusableElement =
@@ -170,8 +180,9 @@ const { state, actions } = store( 'core/navigation', {
 		focusFirstElement() {
 			const { ref } = getElement();
 			if ( state.isMenuOpen ) {
-				const [ firstTabbable ] = focus.tabbable.find( ref );
-				firstTabbable?.focus();
+				const focusableElements =
+					ref.querySelectorAll( focusableSelectors );
+				focusableElements?.[ 0 ]?.focus();
 			}
 		},
 	},
