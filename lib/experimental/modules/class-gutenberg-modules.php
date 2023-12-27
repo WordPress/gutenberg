f13a53a6f908bc64d32dbf824b107107375819fc
Carlos Bravo <carlos.bravo@automattic.com>
2023-12-27 11:51:46 +0100

    Move only required modules

--- lib/experimental/modules/class-gutenberg-modules.php
+++ lib/experimental/modules/class-gutenberg-modules.php
@@ -263,14 +263,14 @@ function gutenberg_dequeue_module( $module_identifier ) {
 }
 
 $modules_position = wp_is_block_theme() ? 'wp_head' : 'wp_footer';
-// Prints the import map in the head tag.
+// Prints the import map in the head tag in block themes. Otherwise in the footer.
 add_action( $modules_position, array( 'Gutenberg_Modules', 'print_import_map' ) );
 
-// Prints the enqueued modules in the head tag.
+// Prints the enqueued modules in the head tag in block themes. Otherwise in the footer.
 add_action( $modules_position, array( 'Gutenberg_Modules', 'print_enqueued_modules' ) );
 
-// Prints the preloaded modules in the head tag.
-add_action( $modules_position, array( 'Gutenberg_Modules', 'print_module_preloads' ) );
+// Prints the preloaded modules in the head tag in block themes.
+add_action( 'wp_head', array( 'Gutenberg_Modules', 'print_module_preloads' ) );
 
 // Prints the script that loads the import map polyfill in the footer.
 add_action( 'wp_footer', array( 'Gutenberg_Modules', 'print_import_map_polyfill' ), 11 );
