1f9b753b325faaa94107e7e98a4b67132c91e620
Carlos Bravo <37012961+c4rl0sbr4v0@users.noreply.github.com>
2023-12-28 13:24:51 +0100

    Server directive processing: Stop processing non-interactive blocks (#56302)
    * Initial commit
    
    * It works, but is stripping comments
    
    * Added an extra return for debugging
    
    * Refactor to use string instead of arrays to compare
    
    * Use hidden textarea to save comments in production html
    
    * Use divs as delimiters, comments not working for interactive innner components
    
    * Back to array for references and comment delimiters
    
    * Something is working :-)
    
    * Refactor, process working
    
    * experiment replacing inner blocks
    
    * now working if there is only 1 interactive block
    
    * now working with 2 interactive blocks
    
    * Commented stopping at first custom element, after innerblocks is not being processed otherwise
    
    * Try using bookmarks, fix autoclosing tag not working
    
    * Not using bookmarks anymore, just processing inner inside processing function
    
    * Seems to be working, still needs a good test battery
    
    * Fix interactivity API directive processing
    
    * Remove tests that will be updated in next commits
    
    * Add markup tests
    
    * Use correct div group structure
    
    * Small refactor
    
    * remove not needed block name in interactive markers
    
    * Add more markup tests
    
    * refactor tests
    
    * Make inner blocks optional
    
    * Move directives declaration to declare them only if needed
    
    * Refactor more code, thanks to @darerodz
    
    * Remove only the first ocurrence
    
    * Improve comments and format
    
    * Update tests to use HTML API, fix non interactive blocks not being parsed
    
    * Unmark children of interactive blocks according to @DAreRodz comment
    
    * Add a p tag check test
    
    * Execute directives by priority
    
    * Remove gutenberg name from tests and use camelCase for context property
    
    * Fix empty style attribute error
    
    * Add test for directive ordering
    
    * Fix evaluate should only execute anonymous functions test
    
    * Fix wp-style tests
    
    * Improve tests
    
    * Test that we don't process non-interactive blocks
    
    * Stop unmarking children of interactive blocks
    
    * Move directives inside gutenberg_process_interactive_html
    
    ---------
    
    Co-authored-by: Luis Herranz <luisherranz@gmail.com>
    Co-authored-by: David Arenas <david.arenas@automattic.com>

--- phpunit/experimental/interactivity-api/directive-processing-test.php
+++ phpunit/experimental/interactivity-api/directive-processing-test.php
@@ -4,81 +4,97 @@
  *
  * @package Gutenberg
  * @subpackage Interactivity API
- *
- * @phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
  */
 
-
-class Helper_Class {
-	// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
-	public function process_foo_test( $tags, $context ) {
-	}
-
-	public function increment( $store ) {
-		return $store['state']['count'] + $store['context']['count'];
-	}
-
-	public static function static_increment( $store ) {
-		return $store['state']['count'] + $store['context']['count'];
-	}
-}
-
-function gutenberg_test_process_directives_helper_increment( $store ) {
-	return $store['state']['count'] + $store['context']['count'];
-}
-
-/**
- * Tests for the gutenberg_interactivity_process_rendered_html function.
- *
- * @group  interactivity-api
- * @covers gutenberg_interactivity_process_rendered_html
- */
 class Tests_Process_Directives extends WP_UnitTestCase {
-	public function test_correctly_call_attribute_directive_processor_on_closing_tag() {
+	public function set_up() {
+		parent::set_up();
 
-		// PHPUnit cannot stub functions, only classes.
-		$test_helper = $this->createMock( Helper_Class::class );
-
-		$test_helper->expects( $this->exactly( 2 ) )
-				->method( 'process_foo_test' )
-				->with(
-					$this->callback(
-						function ( $p ) {
-							return 'DIV' === $p->get_tag() && (
-								// Either this is a closing tag...
-								$p->is_tag_closer() ||
-								// ...or it is an open tag, and has the directive attribute set.
-								( ! $p->is_tag_closer() && 'abc' === $p->get_attribute( 'foo-test' ) )
-							);
-						}
-					)
-				);
-
-		$directives = array(
-			'foo-test' => array( $test_helper, 'process_foo_test' ),
+		register_block_type(
+			'test/context-level-1',
+			array(
+				'render_callback' => function ( $attributes, $content ) {
+					return '<div data-wp-interactive=\'{ "namespace": "test" }\' data-wp-context=\'{ "myText": "level-1" }\'> <input class="level-1-input-1" data-wp-bind--value="context.myText">' . $content . '<input class="level-1-input-2" data-wp-bind--value="context.myText"></div>';
+				},
+				'supports'        => array(
+					'interactivity' => true,
+				),
+			)
 		);
 
-		$markup = '<div>Example: <div foo-test="abc"><img><span>This is a test></span><div>Here is a nested div</div></div></div>';
-		$tags   = new WP_Directive_Processor( $markup );
-		$tags->process_rendered_html( $tags, 'foo-', $directives );
+		register_block_type(
+			'test/context-level-2',
+			array(
+				'render_callback' => function ( $attributes, $content ) {
+					return '<div data-wp-interactive=\'{ "namespace": "test" }\' data-wp-context=\'{ "myText": "level-2" }\'><input class="level-2-input-1" data-wp-bind--value="context.myText">' . $content . '</div>';
+				},
+				'supports'        => array(
+					'interactivity' => true,
+				),
+			)
+		);
+
+		register_block_type(
+			'test/context-read-only',
+			array(
+				'render_callback' => function () {
+					return '<div data-wp-interactive=\'{ "namespace": "test" }\'><input class="read-only-input-1" data-wp-bind--value="context.myText"></div>';
+				},
+				'supports'        => array(
+					'interactivity' => true,
+				),
+			)
+		);
+
+		register_block_type(
+			'test/non-interactive-with-directive',
+			array(
+				'render_callback' => function () {
+					return '<input class="non-interactive-with-directive" data-wp-bind--value="context.myText">';
+				},
+			)
+		);
+
+		register_block_type(
+			'test/context-level-with-manual-inner-block-rendering',
+			array(
+				'render_callback' => function ( $attributes, $content, $block ) {
+					$inner_blocks_html = '';
+					foreach ( $block->inner_blocks as $inner_block ) {
+						$inner_blocks_html .= $inner_block->render();
+					}
+					return '<div data-wp-interactive=\'{ "namespace": "test" }\' data-wp-context=\'{ "myText": "some value" }\'>' . $inner_blocks_html . '</div>';
+				},
+				'supports'        => array(
+					'interactivity' => true,
+				),
+			)
+		);
+
+		register_block_type(
+			'test/directives-ordering',
+			array(
+				'render_callback' => function () {
+					return '<input data-wp-interactive=\'{ "namespace": "test" }\' data-wp-context=\'{ "isClass": true, "value": "some-value", "display": "none" }\' data-wp-bind--value="context.value" class="other-class" data-wp-class--some-class="context.isClass" data-wp-style--display="context.display">';
+				},
+				'supports'        => array(
+					'interactivity' => true,
+				),
+			)
+		);
 	}
 
-	public function test_directives_with_double_hyphen_processed_correctly() {
-		$test_helper = $this->createMock( Helper_Class::class );
-		$test_helper->expects( $this->atLeastOnce() )
-				->method( 'process_foo_test' );
-
-		$directives = array(
-			'foo-test' => array( $test_helper, 'process_foo_test' ),
-		);
-
-		$markup = '<div foo-test--value="abc"></div>';
-		$tags   = new WP_Directive_Processor( $markup );
-		$tags->process_rendered_html( $tags, 'foo-', $directives );
+	public function tear_down() {
+		unregister_block_type( 'test/context-level-1' );
+		unregister_block_type( 'test/context-level-2' );
+		unregister_block_type( 'test/context-read-only' );
+		unregister_block_type( 'test/non-interactive-with-directive' );
+		unregister_block_type( 'test/context-level-with-manual-inner-block-rendering' );
+		unregister_block_type( 'test/directives-ordering' );
+		parent::tear_down();
 	}
 
 	public function test_interactivity_process_directives_in_root_blocks() {
-
 		$block_content =
 		'<!-- wp:paragraph -->' .
 			'<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>' .
@@ -87,15 +103,11 @@ class Tests_Process_Directives extends WP_UnitTestCase {
 			'<p>Welcome to WordPress.</p>' .
 		'<!-- /wp:paragraph -->';
 
-		$parsed_block = parse_blocks( $block_content )[0];
-
-		$source_block = $parsed_block;
-
-		$rendered_content = render_block( $parsed_block );
-
+		$parsed_block        = parse_blocks( $block_content )[0];
+		$source_block        = $parsed_block;
+		$rendered_content    = render_block( $parsed_block );
 		$parsed_block_second = parse_blocks( $block_content )[1];
-
-		$fake_parent_block = array();
+		$fake_parent_block   = array();
 
 		// Test that root block is intially emtpy.
 		$this->assertEmpty( WP_Directive_Processor::$root_block );
@@ -117,16 +129,103 @@ class Tests_Process_Directives extends WP_UnitTestCase {
 		gutenberg_process_directives_in_root_blocks( $rendered_content, $parsed_block );
 		$this->assertEmpty( WP_Directive_Processor::$root_block );
 	}
-}
 
+	public function test_directive_processing_of_interactive_block() {
+		$post_content    = '<!-- wp:test/context-level-1 /-->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'level-1-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'level-1-input-2' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+	}
 
-/**
- * Tests for the gutenberg_interactivity_evaluate_reference function.
- *
- * @group  interactivity-api
- * @covers gutenberg_interactivity_evaluate_reference
- */
-class Tests_Utils_Evaluate extends WP_UnitTestCase {
+	public function test_directive_processing_two_interactive_blocks_at_same_level() {
+		$post_content    = '<!-- wp:group --><div class="wp-block-group"><!-- wp:test/context-level-1 /--><!-- wp:test/context-level-2 /--></div><!-- /wp:group -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'level-1-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'level-1-input-2' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'level-2-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-2', $value );
+	}
+
+	public function test_directives_are_processed_at_tag_end() {
+		$post_content    = '<!-- wp:test/context-level-1 --><!-- wp:test/context-level-2 /--><!-- wp:test/context-read-only /--><!-- /wp:test/context-level-1 -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'level-1-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'level-2-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-2', $value );
+		$p->next_tag( array( 'class_name' => 'read-only-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'level-1-input-2' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+	}
+
+	public function test_non_interactive_children_of_interactive_is_rendered() {
+		$post_content    = '<!-- wp:test/context-level-1 --><!-- wp:test/context-read-only /--><!-- wp:paragraph --><p>Welcome</p><!-- /wp:paragraph --><!-- /wp:test/context-level-1 -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'level-1-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag( array( 'class_name' => 'read-only-input-1' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+		$p->next_tag();
+		$this->assertSame( 'P', $p->get_tag() );
+		$p->next_tag( array( 'class_name' => 'level-1-input-2' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'level-1', $value );
+	}
+
+	public function test_non_interactive_blocks_are_not_processed() {
+		$post_content    = '<!-- wp:test/context-level-1 --><!-- wp:test/non-interactive-with-directive /--><!-- /wp:test/context-level-1 -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'non-interactive-with-directive' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( null, $value );
+	}
+
+	public function test_non_interactive_blocks_with_manual_inner_block_rendering_are_not_processed() {
+		$post_content    = '<!-- wp:test/context-level-with-manual-inner-block-rendering --><!-- wp:test/non-interactive-with-directive /--><!-- /wp:test/context-level-with-manual-inner-block-rendering -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag( array( 'class_name' => 'non-interactive-with-directive' ) );
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( null, $value );
+	}
+
+	public function test_directives_ordering() {
+		$post_content    = '<!-- wp:test/directives-ordering -->';
+		$rendered_blocks = do_blocks( $post_content );
+		$p               = new WP_HTML_Tag_Processor( $rendered_blocks );
+		$p->next_tag();
+
+		$value = $p->get_attribute( 'class' );
+		$this->assertSame( 'other-class some-class', $value );
+
+		$value = $p->get_attribute( 'value' );
+		$this->assertSame( 'some-value', $value );
+
+		$value = $p->get_attribute( 'style' );
+		$this->assertSame( 'display: none;', $value );
+	}
+
 	public function test_evaluate_function_should_access_state() {
 		// Init a simple store.
 		wp_store(
@@ -142,6 +241,7 @@ class Tests_Utils_Evaluate extends WP_UnitTestCase {
 				),
 			)
 		);
+
 		$this->assertSame( 1, gutenberg_interactivity_evaluate_reference( 'state.core.number' ) );
 		$this->assertTrue( gutenberg_interactivity_evaluate_reference( 'state.core.bool' ) );
 		$this->assertSame( 'hi', gutenberg_interactivity_evaluate_reference( 'state.core.nested.string' ) );
@@ -158,10 +258,12 @@ class Tests_Utils_Evaluate extends WP_UnitTestCase {
 				),
 			),
 		);
+
 		$this->assertSame( 2, gutenberg_interactivity_evaluate_reference( 'context.local.number', $context ) );
 		$this->assertFalse( gutenberg_interactivity_evaluate_reference( 'context.local.bool', $context ) );
 		$this->assertTrue( gutenberg_interactivity_evaluate_reference( '!context.local.bool', $context ) );
 		$this->assertSame( 'bye', gutenberg_interactivity_evaluate_reference( 'context.local.nested.string', $context ) );
+
 		// Previously defined state is also accessible.
 		$this->assertSame( 1, gutenberg_interactivity_evaluate_reference( 'state.core.number' ) );
 		$this->assertTrue( gutenberg_interactivity_evaluate_reference( 'state.core.bool' ) );
@@ -174,7 +276,6 @@ class Tests_Utils_Evaluate extends WP_UnitTestCase {
 
 	public function test_evaluate_function_should_execute_anonymous_functions() {
 		$context = new WP_Directive_Context( array( 'count' => 2 ) );
-		$helper  = new Helper_Class();
 
 		wp_store(
 			array(
@@ -182,14 +283,13 @@ class Tests_Utils_Evaluate extends WP_UnitTestCase {
 					'count' => 3,
 				),
 				'selectors' => array(
-					'anonymous_function'           => function ( $store ) {
+					'anonymous_function'  => function ( $store ) {
 						return $store['state']['count'] + $store['context']['count'];
 					},
 					// Other types of callables should not be executed.
-					'function_name'                => 'gutenberg_test_process_directives_helper_increment',
-					'class_method'                 => array( $helper, 'increment' ),
-					'class_static_method'          => 'Helper_Class::static_increment',
-					'class_static_method_as_array' => array( 'Helper_Class', 'static_increment' ),
+					'function_name'       => 'gutenberg_test_process_directives_helper_increment',
+					'class_method'        => array( $this, 'increment' ),
+					'class_static_method' => array( 'Tests_Process_Directives', 'static_increment' ),
 				),
 			)
 		);
@@ -200,16 +300,12 @@ class Tests_Utils_Evaluate extends WP_UnitTestCase {
 			gutenberg_interactivity_evaluate_reference( 'selectors.function_name', $context->get_context() )
 		);
 		$this->assertSame(
-			array( $helper, 'increment' ),
+			array( $this, 'increment' ),
 			gutenberg_interactivity_evaluate_reference( 'selectors.class_method', $context->get_context() )
 		);
 		$this->assertSame(
-			'Helper_Class::static_increment',
+			array( 'Tests_Process_Directives', 'static_increment' ),
 			gutenberg_interactivity_evaluate_reference( 'selectors.class_static_method', $context->get_context() )
-		);
-		$this->assertSame(
-			array( 'Helper_Class', 'static_increment' ),
-			gutenberg_interactivity_evaluate_reference( 'selectors.class_static_method_as_array', $context->get_context() )
 		);
 	}
 }
