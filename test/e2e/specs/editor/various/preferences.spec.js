20a29a95f72b7c2cc6c8c584ab6d2e7360849e99
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2023-12-29 10:10:25 +0530

    Migrate Preferences Test to Playwright

---
+++ test/e2e/specs/editor/various/preferences.spec.js
@@ -0,0 +1,42 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'preferences', () => {
+	test( 'remembers sidebar dismissal between sessions', async ( {
+		page,
+		admin,
+	} ) => {
+		const post = 'role=button[name="Post (selected)"i]';
+		await admin.createNewPost();
+
+		// Open by default.
+		await expect( page.locator( post ) ).toHaveText( 'Post' );
+
+		// Change to "Block" tab.
+		await page
+			.getByRole( 'button', { name: 'Block', exact: true } )
+			.click();
+
+		await expect(
+			page.locator( 'role=button[name="Block (selected)"i]' )
+		).toHaveText( 'Block' );
+
+		// Regression test: Reload resets to document tab.
+		//
+		// See: https://github.com/WordPress/gutenberg/issues/6377
+		// See: https://github.com/WordPress/gutenberg/pull/8995
+		await page.reload();
+		await expect( page.locator( post ) ).toHaveText( 'Post' );
+
+		// Dismiss.
+		await page.getByRole( 'button', { name: 'Close settings' } ).click();
+		await expect( page.locator( post ) ).not.toBeVisible();
+
+		// Remember after reload.
+		await page.reload();
+
+		await expect( page.locator( post ) ).not.toBeVisible();
+	} );
+} );
