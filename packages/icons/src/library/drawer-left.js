929f32636f981060d38731040f9cd29dee7e37ad
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-12-30 18:50:52 +0900

    Remove width/height of newly added icons

--- packages/icons/src/library/drawer-left.js
+++ packages/icons/src/library/drawer-left.js
@@ -4,12 +4,7 @@
 import { SVG, Path } from '@wordpress/primitives';
 
 const drawerLeft = (
-	<SVG
-		width="24"
-		height="24"
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-	>
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
