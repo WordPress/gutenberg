eb43d98a99d2a691b6607564570a5786fafe4b7a
Riad Benguella <benguella@gmail.com>
2024-01-03 07:33:29 +0100

    Delete the edit site setting as well

--- packages/preferences-persistence/src/migrations/preferences-package-data/convert-editor-settings.js
+++ packages/preferences-persistence/src/migrations/preferences-package-data/convert-editor-settings.js
@@ -17,10 +17,18 @@ export default function convertEditorSettings( data ) {
 			};
 			delete newData[ 'core/edit-post' ][ setting ];
 		}
+
+		if ( data?.[ 'core/edit-post' ]?.[ setting ] !== undefined ) {
+			delete newData[ 'core/edit-site' ][ setting ];
+		}
 	} );
 
 	if ( Object.keys( newData?.[ 'core/edit-post' ] ?? {} )?.length === 0 ) {
 		delete newData[ 'core/edit-post' ];
+	}
+
+	if ( Object.keys( newData?.[ 'core/edit-site' ] ?? {} )?.length === 0 ) {
+		delete newData[ 'core/edit-site' ];
 	}
 
 	return newData;
