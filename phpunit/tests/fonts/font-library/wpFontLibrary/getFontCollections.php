302c1487c0298a75b4a0094e77ec87256f15ccdf
Matias Benedetto <matias.benedetto@gmail.com>
2024-01-08 07:31:38 -0600

    Font Library: unregister font collection (#54701)
    * remove font collection
    
    * add wp_unregister_font_collection function
    
    * format php
    
    * fix function comment
    
    * add tests for unregister_font_collection
    
    * removig unused variable
    
    * update wording on comments
    
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>
    
    * keep track of unregistered font collection ids in an array
    
    * add tests to assure that get_font_collections returns an empty array if all the font collections were unregistered
    
    * fix tests
    
    * test the initial empty value of the font library collections
    
    * format
    
    * simplify unregistering of font collections and add _doing_it_wrong call
    
    * add translation domain
    
    * adding _doing_it_wrong if you are registering collections with the same id or unregistering a non existing collection id
    
    * updating tests
    
    * replace 6.4.0 by 6.5.0 in comments
    
    Co-authored-by: Vicente Canales <1157901+vcanales@users.noreply.github.com>
    
    * update version
    
    Co-authored-by: Grant Kinney <creativecoder@users.noreply.github.com>
    
    * consolidate code as only one function to avoid code repetition
    
    * create base test case
    
    * format php
    
    * assertWPError
    
    * check that collection was not unregistered by mistake
    
    * calling parent test class mehtods
    
    * format php
    
    ---------
    
    Co-authored-by: Colin Stewart <79332690+costdev@users.noreply.github.com>
    Co-authored-by: Vicente Canales <1157901+vcanales@users.noreply.github.com>
    Co-authored-by: Grant Kinney <creativecoder@users.noreply.github.com>

--- phpunit/tests/fonts/font-library/wpFontLibrary/getFontCollections.php
+++ phpunit/tests/fonts/font-library/wpFontLibrary/getFontCollections.php
@@ -10,11 +10,13 @@
  *
  * @covers WP_Font_Library::get_font_collections
  */
-class Tests_Fonts_WpFontLibrary_GetFontCollections extends WP_UnitTestCase {
+class Tests_Fonts_WpFontLibrary_GetFontCollections extends WP_Font_Library_UnitTestCase {
+	public function test_should_get_an_empty_list() {
+		$font_collections = WP_Font_Library::get_font_collections();
+		$this->assertEmpty( $font_collections, 'Should return an empty array.' );
+	}
 
-	public static function set_up_before_class() {
-		$font_library = new WP_Font_Library();
-
+	public function test_should_get_mock_font_collection() {
 		$my_font_collection_config = array(
 			'id'          => 'my-font-collection',
 			'name'        => 'My Font Collection',
@@ -22,23 +24,11 @@ class Tests_Fonts_WpFontLibrary_GetFontCollections extends WP_UnitTestCase {
 			'src'         => path_join( __DIR__, 'my-font-collection-data.json' ),
 		);
 
-		$font_library::register_font_collection( $my_font_collection_config );
-	}
+		WP_Font_Library::register_font_collection( $my_font_collection_config );
 
-	public function test_should_get_the_default_font_collection() {
-		$font_collections = WP_Font_Library::get_font_collections();
-		$this->assertArrayHasKey( 'default-font-collection', $font_collections, 'Default Google Fonts collection should be registered' );
-		$this->assertInstanceOf( 'WP_Font_Collection', $font_collections['default-font-collection'], 'The value of the array $font_collections[id] should be an instance of WP_Font_Collection class.' );
-	}
-
-	public function test_should_get_the_right_number_of_collections() {
 		$font_collections = WP_Font_Library::get_font_collections();
 		$this->assertNotEmpty( $font_collections, 'Sould return an array of font collections.' );
-		$this->assertCount( 2, $font_collections, 'Should return an array with one font collection.' );
-	}
-
-	public function test_should_get_mock_font_collection() {
-		$font_collections = WP_Font_Library::get_font_collections();
+		$this->assertCount( 1, $font_collections, 'Should return an array with one font collection.' );
 		$this->assertArrayHasKey( 'my-font-collection', $font_collections, 'The array should have the key of the registered font collection id.' );
 		$this->assertInstanceOf( 'WP_Font_Collection', $font_collections['my-font-collection'], 'The value of the array $font_collections[id] should be an instance of WP_Font_Collection class.' );
 	}
