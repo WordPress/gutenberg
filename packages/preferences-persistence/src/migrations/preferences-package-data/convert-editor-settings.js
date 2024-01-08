820430c25150416315f392c29a7558fcf65a9af5
Riad Benguella <benguella@gmail.com>
2024-01-08 09:15:16 +0100

    Editor: Add the show most used blocks preference to the site editor

--- packages/preferences-persistence/src/migrations/preferences-package-data/convert-editor-settings.js
+++ packages/preferences-persistence/src/migrations/preferences-package-data/convert-editor-settings.js
@@ -11,6 +11,7 @@ export default function convertEditorSettings( data ) {
 		'focusMode',
 		'inactivePanels',
 		'keepCaretInsideBlock',
+		'mostUsedBlocks',
 		'openPanels',
 		'showBlockBreadcrumbs',
 		'showIconLabels',
