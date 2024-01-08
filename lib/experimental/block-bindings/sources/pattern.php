0d686c985d5a6518c7f49eacc3d555a9f464c46b
Ricardo Artemio Morales <ric.morales22@gmail.com>
2024-01-08 07:54:28 -0500

    Fix bug wherein original pattern values were not being displayed

--- lib/experimental/block-bindings/sources/pattern.php
+++ lib/experimental/block-bindings/sources/pattern.php
@@ -8,10 +8,10 @@
 if ( function_exists( 'register_block_bindings_source' ) ) {
 	$pattern_source_callback = function ( $source_attrs, $block_content, $block, $block_instance ) {
 		if ( ! _wp_array_get( $block_instance->attributes, array( 'metadata', 'id' ), false ) ) {
-			return;
+			return null;
 		}
 		$block_id = $block_instance->attributes['metadata']['id'];
-		return _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id ), false );
+		return _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id ), null );
 	};
 	register_block_bindings_source(
 		'pattern_attributes',
