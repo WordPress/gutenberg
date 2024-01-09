004b727135356226971f9acbd18e888dcba1898d
George Mamadashvili <georgemamadashvili@gmail.com>
2024-01-09 12:15:42 +0400

    Use test steps

--- test/e2e/specs/editor/various/dropdown-menu.spec.js
+++ test/e2e/specs/editor/various/dropdown-menu.spec.js
@@ -8,10 +8,7 @@ test.describe( 'Dropdown Menu', () => {
 		await admin.createNewPost();
 	} );
 
-	test( 'allows navigation through each item using arrow keys', async ( {
-		page,
-		pageUtils,
-	} ) => {
+	test( 'keyboard navigiation', async ( { page, pageUtils } ) => {
 		await page
 			.getByRole( 'region', { name: 'Editor top bar' } )
 			.getByRole( 'button', { name: 'Options' } )
@@ -22,65 +19,42 @@ test.describe( 'Dropdown Menu', () => {
 		// Catch any issues with the selector, which could cause a false positive test result.
 		expect( totalItems ).toBeGreaterThan( 0 );
 
-		// Expect the first menu item to be focused.
-		await expect( menuItems.first() ).toBeFocused();
+		await test.step( 'allows navigation through each item using arrow keys', async () => {
+			// Expect the first menu item to be focused.
+			await expect( menuItems.first() ).toBeFocused();
 
-		// Arrow down to the last item.
-		await pageUtils.pressKeys( 'ArrowDown', { times: totalItems - 1 } );
-		await expect( menuItems.last() ).toBeFocused();
+			// Arrow down to the last item.
+			await pageUtils.pressKeys( 'ArrowDown', { times: totalItems - 1 } );
+			await expect( menuItems.last() ).toBeFocused();
 
-		// Arrow back up to the first item.
-		await pageUtils.pressKeys( 'ArrowUp', { times: totalItems - 1 } );
-		await expect( menuItems.first() ).toBeFocused();
-	} );
+			// Arrow back up to the first item.
+			await pageUtils.pressKeys( 'ArrowUp', { times: totalItems - 1 } );
+			await expect( menuItems.first() ).toBeFocused();
+		} );
 
-	test( 'loops to the beginning and end when navigating past the boundaries of the menu', async ( {
-		page,
-	} ) => {
-		await page
-			.getByRole( 'region', { name: 'Editor top bar' } )
-			.getByRole( 'button', { name: 'Options' } )
-			.click();
-		const menuItems = page.locator( '.components-menu-item__button' );
-		const totalItems = await menuItems.count();
+		await test.step( 'loops to the beginning and end when navigating past the boundaries of the menu', async () => {
+			// Expect the first menu item to be focused.
+			await expect( menuItems.first() ).toBeFocused();
 
-		// Catch any issues with the selector, which could cause a false positive test result.
-		expect( totalItems ).toBeGreaterThan( 0 );
+			// Arrow up to the last item.
+			await page.keyboard.press( 'ArrowUp' );
+			await expect( menuItems.last() ).toBeFocused();
 
-		// Expect the first menu item to be focused.
-		await expect( menuItems.first() ).toBeFocused();
+			// Arrow back down to the first item.
+			await page.keyboard.press( 'ArrowDown' );
+			await expect( menuItems.first() ).toBeFocused();
+		} );
 
-		// Arrow up to the last item.
-		await page.keyboard.press( 'ArrowUp' );
-		await expect( menuItems.last() ).toBeFocused();
+		await test.step( 'ignores arrow key navigation that is orthogonal to the orientation of the menu, but stays open', async () => {
+			// Expect the first menu item to be focused.
+			await expect( menuItems.first() ).toBeFocused();
 
-		// Arrow back down to the first item.
-		await page.keyboard.press( 'ArrowDown' );
-		await expect( menuItems.first() ).toBeFocused();
-	} );
+			// Press left and right keys an arbitrary (but > 1) number of times.
+			await pageUtils.pressKeys( 'ArrowLeft', { times: 5 } );
+			await pageUtils.pressKeys( 'ArrowRight', { times: 5 } );
 
-	test( 'ignores arrow key navigation that is orthogonal to the orientation of the menu, but stays open', async ( {
-		page,
-		pageUtils,
-	} ) => {
-		await page
-			.getByRole( 'region', { name: 'Editor top bar' } )
-			.getByRole( 'button', { name: 'Options' } )
-			.click();
-		const menuItems = page.locator( '.components-menu-item__button' );
-		const totalItems = await menuItems.count();
-
-		// Catch any issues with the selector, which could cause a false positive test result.
-		expect( totalItems ).toBeGreaterThan( 0 );
-
-		// Expect the first menu item to be focused.
-		await expect( menuItems.first() ).toBeFocused();
-
-		// Press left and right keys an arbitrary (but > 1) number of times.
-		await pageUtils.pressKeys( 'ArrowLeft', { times: 5 } );
-		await pageUtils.pressKeys( 'ArrowRight', { times: 5 } );
-
-		// Expect the first menu item to still be focused.
-		await expect( menuItems.first() ).toBeFocused();
+			// Expect the first menu item to still be focused.
+			await expect( menuItems.first() ).toBeFocused();
+		} );
 	} );
 } );
