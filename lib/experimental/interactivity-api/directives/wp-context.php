4096915fb8c7cbbcadd88933dd9d31da0a534f07
David Arenas <david.arenas@automattic.com>
2024-01-09 16:08:17 +0100

    Handle boolean attributes in wp-context

--- lib/experimental/interactivity-api/directives/wp-context.php
+++ lib/experimental/interactivity-api/directives/wp-context.php
@@ -21,7 +21,9 @@ function gutenberg_interactivity_process_wp_context( $tags, $context, $ns ) {
 	$attr_value = $tags->get_attribute( 'data-wp-context' );
 
 	//Separate namespace and value from the context directive attribute.
-	list( $ns, $data ) = WP_Directive_Processor::parse_attribute_value( $attr_value, $ns );
+	list( $ns, $data ) = is_string( $attr_value ) && ! empty( $attr_value )
+		? WP_Directive_Processor::parse_attribute_value( $attr_value, $ns )
+		: array( $ns, null );
 
 	// Add parsed data to the context under the corresponding namespace.
 	$context->set_context( array( $ns => is_array( $data ) ? $data : array() ) );
