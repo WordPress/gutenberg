f88dcca2e550a9b1bf41e3656e78b8b5dfb8b60b
arthur <arthur.chu@automattic.com>
2024-01-10 15:15:15 +0800

    Update comments

--- packages/api-fetch/src/middlewares/theme-preview.js
+++ packages/api-fetch/src/middlewares/theme-preview.js
@@ -7,7 +7,7 @@ import { addQueryArgs, getQueryArg, removeQueryArgs } from '@wordpress/url';
  * This appends a `wp_theme_preview` parameter to the REST API request URL if
  * the admin URL contains a `theme` GET parameter.
  *
- * If the REST API request URL has contained the `wp_theme_preview` parameter,
+ * If the REST API request URL has contained the `wp_theme_preview` parameter as `''`,
  * then bypass this middleware.
  *
  * @param {Record<string, any>} themePath
