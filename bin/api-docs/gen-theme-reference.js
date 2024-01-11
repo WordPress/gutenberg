2214b1941930434854b6907ff302a772015e3ca3
Matias Benedetto <matias.benedetto@gmail.com>
2024-01-11 15:21:48 -0300

    resolve json schema references

--- bin/api-docs/gen-theme-reference.js
+++ bin/api-docs/gen-theme-reference.js
@@ -75,6 +75,40 @@ const keys = ( maybeObject ) => {
 };
 
 /**
+ * Get definition from ref.
+ *
+ * @param {string} ref
+ * @return {Object} definition
+ *
+ * @example
+ * getDefinition( '#/definitions/typographyProperties/properties/fontFamily' )
+ * // returns themejson.definitions.typographyProperties.properties.fontFamily
+ *
+ */
+const resolveDefintionRef = ( ref ) => {
+	const refParts = ref.split( '/' );
+	const definition = refParts[ refParts.length - 1 ];
+	return themejson.definitions[ definition ];
+};
+
+/**
+ * Get properties from an array.
+ *
+ * @param {Object} items
+ * @return {Object} properties
+ *
+ */
+const getPropertiesFromArray = ( items ) => {
+	// if its a $ref resolve it
+	if ( items.$ref ) {
+		return resolveDefintionRef( items.$ref ).properties;
+	}
+
+	// otherwise just return the properties
+	return items.properties;
+};
+
+/**
  * Convert settings properties to markup.
  *
  * @param {Object} struct
@@ -96,7 +130,10 @@ const getSettingsPropertiesMarkup = ( struct ) => {
 		const def = 'default' in props[ key ] ? props[ key ].default : '';
 		const ps =
 			props[ key ].type === 'array'
-				? keys( props[ key ].items.properties ).sort().join( ', ' )
+				? // ? keys( props[ key ].items.properties ).sort().join( ', ' )
+				  keys( getPropertiesFromArray( props[ key ].items ) )
+						.sort()
+						.join( ', ' )
 				: '';
 		markup += `| ${ key } | ${ props[ key ].type } | ${ def } | ${ ps } |\n`;
 	} );
