8e27b15ad5a964d11859ce2880c64e3e2a88c2b3
Glen Davies <glen.davies@automattic.com>
2024-01-11 13:55:57 +1300

    Remove the history from site editor, don't think it matters that back link not in header as browser back button works

--- packages/edit-site/src/components/block-editor/use-change-entity.js
+++ packages/edit-site/src/components/block-editor/use-change-entity.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { privateApis as routerPrivateApis } from '@wordpress/router';
-import { useState } from '@wordpress/element';
+
 /**
  * Internal dependencies
  */
@@ -12,10 +12,6 @@ import { getPostLinkProps } from '../routes/link';
 const { useHistory } = unlock( routerPrivateApis );
 
 export function useChangeEntity() {
-	// When moving from a page to edit its template we need to track this as history
-	// so that the back button displays in the docuement bar the same as in the post
-	// editor.
-	const [ hasHistory, setHasHistory ] = useState( false );
 	const history = useHistory();
 
 	const getEntityLoader = ( params, state ) => {
@@ -23,24 +19,12 @@ export function useChangeEntity() {
 
 		return {
 			href,
-			loadEntity: ( event ) => {
-				event?.preventDefault();
-				if ( params.postType === 'wp_template' ) {
-					setHasHistory( true );
-				}
-				onClick( event );
-			},
+			loadEntity: ( event ) => onClick( event ),
 		};
-	};
-
-	const handleOnBack = () => {
-		setHasHistory( false );
-		history.back();
 	};
 
 	return {
 		getEntityLoader,
-		hasHistory,
-		goBack: handleOnBack,
+		goBack: history.back,
 	};
 }
