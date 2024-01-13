b80df7fe1eb04a2490fe5718473ce18ec596ea2a
Akira Tachibana <atachibana@unofficialtokyo.com>
2024-01-13 14:05:55 +0900

    Remove unused argument from sprintf

--- packages/dataviews/src/pagination.js
+++ packages/dataviews/src/pagination.js
@@ -30,12 +30,11 @@ const Pagination = memo( function Pagination( {
 				<HStack justify="flex-start" expanded={ false } spacing={ 2 }>
 					{ createInterpolateElement(
 						sprintf(
-							// translators: %1$s: Current page number, %2$s: Total number of pages.
+							// translators: %s: Total number of pages.
 							_x(
-								'Page <CurrenPageControl /> of %2$s',
+								'Page <CurrenPageControl /> of %s',
 								'paging'
 							),
-							view.page,
 							totalPages
 						),
 						{
