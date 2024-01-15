1e6f54c4c4fe2c5feb31c5d60bdc3d6f8c267635
David Arenas <david.arenas@automattic.com>
2024-01-15 15:26:23 +0100

    Scaffold files for wp-each e2e tests

---
+++ test/e2e/specs/interactivity/directive-each.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies
+ */
+import { test, expect } from './fixtures';
+
+test.describe( 'data-wp-each', () => {
+	test( 'should hydrate correctly', async ( { page } ) => {} );
+	test( 'should not remount items when the order changes', async ( {
+		page,
+	} ) => {} );
+	test( 'should use `item` as the defaul item name in the context', async ( {
+		page,
+	} ) => {} );
+	test( 'should use the specified item name in the context', async ( {
+		page,
+	} ) => {} );
+	test( 'should use `item` as the key if `wp-each-key` is not specified', async ( {
+		page,
+	} ) => {} );
+	test( 'should use the specified `wp-each-key` to compute item ids', async ( {
+		page,
+	} ) => {} );
+	test( 'should work on navigation', async ( { page } ) => {} );
+} );
