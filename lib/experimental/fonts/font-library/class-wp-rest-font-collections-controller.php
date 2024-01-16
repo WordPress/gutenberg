78c694198589944a0c5f2f9f0d938fc885b53c8f
Matias Benedetto <matias.benedetto@gmail.com>
2024-01-16 11:05:35 -0300

    fix get_items parameters

--- lib/experimental/fonts/font-library/class-wp-rest-font-collections-controller.php
+++ lib/experimental/fonts/font-library/class-wp-rest-font-collections-controller.php
@@ -96,7 +96,7 @@ class WP_REST_Font_Collections_Controller extends WP_REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function get_items() {
+	public function get_items( $request ) {
 		$collections = array();
 		foreach ( WP_Font_Library::get_font_collections() as $collection ) {
 			$collections[] = $collection->get_config();
