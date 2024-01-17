b3b3ebb2a5b6aca39adc401d7e958105bdc09d43
David Arenas <david.arenas@automattic.com>
2024-01-17 11:11:44 +0100

    Register interactivity router module

--- lib/experimental/interactivity-api/modules.php
+++ lib/experimental/interactivity-api/modules.php
@@ -9,13 +9,19 @@
 /**
  * Register the `@wordpress/interactivity` module.
  */
-function gutenberg_register_interactivity_module() {
+function gutenberg_register_interactivity_modules() {
 	gutenberg_register_module(
 		'@wordpress/interactivity',
 		gutenberg_url( '/build/interactivity/index.min.js' ),
 		array(),
 		defined( 'GUTENBERG_VERSION' ) ? GUTENBERG_VERSION : get_bloginfo( 'version' )
 	);
+	gutenberg_register_module(
+		'@wordpress/interactivity/router',
+		gutenberg_url( '/build/interactivity/router.min.js' ),
+		array( '@wordpress/interactivity' ),
+		defined( 'GUTENBERG_VERSION' ) ? GUTENBERG_VERSION : get_bloginfo( 'version' )
+	);
 }
 
-add_action( 'wp_enqueue_scripts', 'gutenberg_register_interactivity_module' );
+add_action( 'wp_enqueue_scripts', 'gutenberg_register_interactivity_modules' );
