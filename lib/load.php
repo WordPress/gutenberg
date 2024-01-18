6cba39a836dbc61e9f6fd18b3f19dd724d0dbf1d
Matias Benedetto <matias.benedetto@gmail.com>
2024-01-18 11:41:29 -0300

    removing files and files loading no longer needed

--- lib/load.php
+++ lib/load.php
@@ -139,78 +139,42 @@ require __DIR__ . '/experimental/modules/class-gutenberg-modules.php';
 // Fonts API / Font Face.
 remove_action( 'plugins_loaded', '_wp_theme_json_webfonts_handler' ); // Turns off WordPress 6.0's stopgap handler.
 
+// Loads the Font Library.
+require __DIR__ . '/experimental/fonts/font-library/class-wp-font-collection.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-font-library.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-font-family-utils.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-font-family.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-font-families-controller.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-font-collections-controller.php';
+require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-autosave-font-families-controller.php';
+require __DIR__ . '/experimental/fonts/font-library/font-library.php';
+
+// Load the Font Face.
+require __DIR__ . '/compat/wordpress-6.4/fonts/font-face/class-wp-font-face.php';
+require __DIR__ . '/compat/wordpress-6.4/fonts/font-face/class-wp-font-face-resolver.php';
+
+
 /*
- * If the Font Library is available, load the Font Face files, else load the Fonts API.
- * This strategy is temporary until the Font Library is merged. It's used here to allow
- * the Font Face (redesigned Fonts API) to be merged before the Font Library while
- * keeping Fonts API available for sites that are using it.
- */
-if (
-	! defined( 'FONT_LIBRARY_DISABLED' ) || ! FONT_LIBRARY_DISABLED
-) {
-	// Loads the Font Library.
-	if ( ! class_exists( 'WP_Font_Library' ) ) {
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-font-collection.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-font-library.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-font-family-utils.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-font-family.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-font-families-controller.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-font-collections-controller.php';
-		require __DIR__ . '/experimental/fonts/font-library/class-wp-rest-autosave-font-families-controller.php';
-		require __DIR__ . '/experimental/fonts/font-library/font-library.php';
-	}
+	* As _gutenberg_get_iframed_editor_assets_6_4() overrides Core's _wp_get_iframed_editor_assets(),
+	* load this file to ensure wp_print_font_faces() is invoked to load the styles into the
+	* iframed editor.
+	*/
+require __DIR__ . '/compat/wordpress-6.4/fonts/fonts.php';
 
-	// Load the Font Face.
-	if ( ! class_exists( 'WP_Font_Face' ) ) {
-		require __DIR__ . '/compat/wordpress-6.4/fonts/font-face/class-wp-font-face.php';
-		require __DIR__ . '/compat/wordpress-6.4/fonts/font-face/class-wp-font-face-resolver.php';
-	}
-
-	/*
-	 * As _gutenberg_get_iframed_editor_assets_6_4() overrides Core's _wp_get_iframed_editor_assets(),
-	 * load this file to ensure wp_print_font_faces() is invoked to load the styles into the
-	 * iframed editor.
-	 */
-	require __DIR__ . '/compat/wordpress-6.4/fonts/fonts.php';
-
-	// Load the BC Layer to avoid fatal errors of extenders using the Fonts API.
-	// @core-merge: do not merge the BC layer files into WordPress Core.
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-provider.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-utils.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-provider-local.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-resolver.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-gutenberg-fonts-api-bc-layer.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/webfonts-deprecations.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-utils.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-provider.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-provider-local.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts.php';
-	require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-web-fonts.php';
-} elseif ( ! class_exists( 'WP_Fonts' ) ) {
-
-	// Turns off Font Face hooks in Core.
-	// @since 6.4.0.
-	remove_action( 'wp_head', 'wp_print_font_faces', 50 );
-	remove_action( 'admin_print_styles', 'wp_print_font_faces', 50 );
-
-	// Fonts API files.
-	require __DIR__ . '/experimental/fonts-api/class-wp-fonts-provider.php';
-	require __DIR__ . '/experimental/fonts-api/class-wp-fonts-utils.php';
-	require __DIR__ . '/experimental/fonts-api/class-wp-fonts.php';
-	require __DIR__ . '/experimental/fonts-api/class-wp-fonts-provider-local.php';
-	require __DIR__ . '/experimental/fonts-api/class-wp-fonts-resolver.php';
-	require __DIR__ . '/experimental/fonts-api/fonts-api.php';
-
-	// BC Layer files, which will not be backported to WP Core.
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-gutenberg-fonts-api-bc-layer.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/webfonts-deprecations.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-wp-webfonts-utils.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-wp-webfonts-provider.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-wp-webfonts-provider-local.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-wp-webfonts.php';
-	require __DIR__ . '/experimental/fonts-api/bc-layer/class-wp-web-fonts.php';
-}
+// Load the BC Layer to avoid fatal errors of extenders using the Fonts API.
+// @core-merge: do not merge the BC layer files into WordPress Core.
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-provider.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-utils.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-provider-local.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-fonts-resolver.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-gutenberg-fonts-api-bc-layer.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/webfonts-deprecations.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-utils.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-provider.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts-provider-local.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-webfonts.php';
+require __DIR__ . '/experimental/fonts/font-face/bc-layer/class-wp-web-fonts.php';
 
 // Plugin specific code.
 require __DIR__ . '/script-loader.php';
