12eb0233efa14785144858549aadd4655040cb47
Marco Ciampini <marco.ciampo@gmail.com>
2024-01-18 07:48:51 +0100

    Delete unnecessary removeExtraPropsAddedByContext utility

--- packages/components/src/context/index.ts
+++ packages/components/src/context/index.ts
@@ -9,5 +9,4 @@ export {
 	getConnectNamespace,
 } from './context-connect';
 export { useContextSystem } from './use-context-system';
-export { removeExtraPropsAddedByContext } from './remove-extra-context-props';
 export * from './wordpress-component';
