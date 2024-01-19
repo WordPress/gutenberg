cc8e4f86a129d30136479e0020181fe9a003ff0f
Sarah Norris <sarah@sekai.co.uk>
2024-01-19 12:31:12 +0000

    Remove spacer from LocalFonts

--- packages/edit-site/src/components/global-styles/font-library-modal/upload-fonts.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/upload-fonts.js
@@ -1,20 +1,10 @@
 /**
- * WordPress dependencies
- */
-import { __experimentalSpacer as Spacer } from '@wordpress/components';
-
-/**
  * Internal dependencies
  */
 import LocalFonts from './local-fonts';
 
 function UploadFonts() {
-	return (
-		<>
-			<Spacer margin={ 8 } />
-			<LocalFonts />
-		</>
-	);
+	return <LocalFonts />;
 }
 
 export default UploadFonts;
