3a50d7c5933d1e3d734fcafd5b52baff2d5cc1e9
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2024-01-22 12:04:19 -0600

    (Preferences)(hotfix)(17.5) Hotfix for missing preferences in the `core` scope (#58031)
    * Preferences: Add a proxy to retrieve the deprecated preferences with a deprecation message (#58016)
    
    * (Preferences)(Hotfix)(17.5) Fallback to the passed scope when value from the original get is `undefined`
    
    ---------
    
    Co-authored-by: Riad Benguella <benguella@gmail.com>

--- packages/preferences/src/store/selectors.js
+++ packages/preferences/src/store/selectors.js
@@ -1,4 +1,53 @@
 /**
+ * WordPress dependencies
+ */
+import deprecated from '@wordpress/deprecated';
+
+const withDeprecatedKeys = ( originalGet ) => ( state, scope, name ) => {
+	const settingsToMoveToCore = [
+		'allowRightClickOverrides',
+		'distractionFree',
+		'editorMode',
+		'fixedToolbar',
+		'focusMode',
+		'hiddenBlockTypes',
+		'inactivePanels',
+		'keepCaretInsideBlock',
+		'mostUsedBlocks',
+		'openPanels',
+		'showBlockBreadcrumbs',
+		'showIconLabels',
+		'showListViewByDefault',
+	];
+
+	if (
+		settingsToMoveToCore.includes( name ) &&
+		[ 'core/edit-post', 'core/edit-site' ].includes( scope )
+	) {
+		deprecated(
+			`wp.data.select( 'core/preferences' ).get( '${ scope }', '${ name }' )`,
+			{
+				since: '6.5',
+				alternative: `wp.data.select( 'core/preferences' ).get( 'core', '${ name }' )`,
+			}
+		);
+
+		const value = originalGet( state, 'core', name );
+
+		// Hotfix for 17.5. Some of the preferences in the list above haven't been
+		// migrated to core in 17.5 (i.e: `editorMode`, https://github.com/WordPress/gutenberg/pull/57642))
+		// so we should fallback to the passed scope to avoid unexpected `undefined` values.
+		if ( value === undefined ) {
+			return originalGet( state, scope, name );
+		}
+
+		return originalGet( state, 'core', name );
+	}
+
+	return originalGet( state, scope, name );
+};
+
+/**
  * Returns a boolean indicating whether a prefer is active for a particular
  * scope.
  *
@@ -8,7 +57,7 @@
  *
  * @return {*} Is the feature enabled?
  */
-export function get( state, scope, name ) {
+export const get = withDeprecatedKeys( ( state, scope, name ) => {
 	const value = state.preferences[ scope ]?.[ name ];
 	return value !== undefined ? value : state.defaults[ scope ]?.[ name ];
-}
+} );
