87b64aed15271ad5f25d44031f8bbbf9c017c9cf
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-01-23 12:28:04 +0100

    Better management of layout param
    - if the experiment is enabled, reloading the page maintains the layout param
    - if the experiment is disabled:
      - the layout param is removed from the URL
      - no layouts other than table are allowed to be loaded

--- packages/edit-site/src/components/sync-state-with-url/use-sync-path-with-url.js
+++ packages/edit-site/src/components/sync-state-with-url/use-sync-path-with-url.js
@@ -102,6 +102,19 @@ export default function useSyncPathWithURL() {
 					path: navigatorLocation.path,
 				} );
 			} else if (
+				navigatorLocation.path === '/wp_template/all' &&
+				! window?.__experimentalAdminViews
+			) {
+				// When the experiment is disabled, we only support table layout.
+				// Clear it out from the URL, so layouts other than table cannot be accessed.
+				updateUrlParams( {
+					postType: undefined,
+					categoryType: undefined,
+					categoryId: undefined,
+					path: navigatorLocation.path,
+					layout: undefined,
+				} );
+			} else if (
 				// These sidebar paths are special in the sense that the url in these pages may or may not have a postId and we need to retain it if it has.
 				// The "type" property should be kept as well.
 				( navigatorLocation.path === '/page' &&
@@ -109,7 +122,7 @@ export default function useSyncPathWithURL() {
 				( navigatorLocation.path === '/wp_template' &&
 					window?.__experimentalAdminViews ) ||
 				( navigatorLocation.path === '/wp_template/all' &&
-					! window?.__experimentalAdminViews )
+					window?.__experimentalAdminViews )
 			) {
 				updateUrlParams( {
 					postType: undefined,
