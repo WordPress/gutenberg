f752a9425769aa0f38bcf5b3f891534a70a85c20
David Arenas <david.arenas@automattic.com>
2024-01-23 18:22:32 +0100

    Convert `navigate` and `prefetch` into actions

--- packages/interactivity/router/index.js
+++ packages/interactivity/router/index.js
@@ -1,5 +1,3 @@
-// Import all from @wordpress/interactivity
-
 /**
  * WordPress dependencies
  */
@@ -49,25 +47,6 @@ const regionsToVdom = ( dom ) => {
 	return { regions, title };
 };
 
-/**
- * Prefetchs the page with the passed URL.
- *
- * The function normalizes the URL and stores internally the fetch promise, to
- * avoid triggering a second fetch for an ongoing request.
- *
- * @param {string}  url             The page URL.
- * @param {Object}  [options]       Options object.
- * @param {boolean} [options.force] Force fetching the URL again.
- * @param {string}  [options.html]  HTML string to be used instead of fetching
- *                                  the requested URL.
- */
-export const prefetch = ( url, options = {} ) => {
-	url = cleanUrl( url );
-	if ( options.force || ! pages.has( url ) ) {
-		pages.set( url, fetchPage( url, options ) );
-	}
-};
-
 // Render all interactive regions contained in the given page.
 const renderRegions = ( page ) => {
 	const attrName = `data-${ directivePrefix }-navigation-id`;
@@ -83,57 +62,6 @@ const renderRegions = ( page ) => {
 
 // Variable to store the current navigation.
 let navigatingTo = '';
-
-/**
- * Navigates to the specified page.
- *
- * This function normalizes the passed href, fetchs the page HTML if needed, and
- * updates any interactive regions whose contents have changed. It also creates
- * a new entry in the browser session history.
- *
- * @param {string}  href              The page href.
- * @param {Object}  [options]         Options object.
- * @param {boolean} [options.force]   If true, it forces re-fetching the URL.
- * @param {string}  [options.html]    HTML string to be used instead of fetching
- *                                    the requested URL.
- * @param {boolean} [options.replace] If true, it replaces the current entry in
- *                                    the browser session history.
- * @param {number}  [options.timeout] Time until the navigation is aborted, in
- *                                    milliseconds. Default is 10000.
- *
- * @return {Promise} Promise that resolves once the navigation is completed or
- *                   aborted.
- */
-export const navigate = async ( href, options = {} ) => {
-	const url = cleanUrl( href );
-	navigatingTo = href;
-	prefetch( url, options );
-
-	// Create a promise that resolves when the specified timeout ends. The
-	// timeout value is 10 seconds by default.
-	const timeoutPromise = new Promise( ( resolve ) =>
-		setTimeout( resolve, options.timeout ?? 10000 )
-	);
-
-	const page = await Promise.race( [ pages.get( url ), timeoutPromise ] );
-
-	// Once the page is fetched, the destination URL could have changed (e.g.,
-	// by clicking another link in the meantime). If so, bail out, and let the
-	// newer execution to update the HTML.
-	if ( navigatingTo !== href ) return;
-
-	if ( page ) {
-		renderRegions( page );
-		window.history[ options.replace ? 'replaceState' : 'pushState' ](
-			{},
-			'',
-			href
-		);
-	} else {
-		window.location.assign( href );
-		await new Promise( () => {} );
-	}
-};
 
 // Listen to the back and forward buttons and restore the page if it's in the
 // cache.
@@ -155,11 +83,78 @@ pages.set(
 
 export const { state, actions } = store( 'core/router', {
 	actions: {
-		*navigate( ...args ) {
-			return yield navigate( ...args );
+		/**
+		 * Navigates to the specified page.
+		 *
+		 * This function normalizes the passed href, fetchs the page HTML if
+		 * needed, and updates any interactive regions whose contents have
+		 * changed. It also creates a new entry in the browser session history.
+		 *
+		 * @param {string}  href              The page href.
+		 * @param {Object}  [options]         Options object.
+		 * @param {boolean} [options.force]   If true, it forces re-fetching the
+		 *                                    URL.
+		 * @param {string}  [options.html]    HTML string to be used instead of
+		 *                                    fetching the requested URL.
+		 * @param {boolean} [options.replace] If true, it replaces the current
+		 *                                    entry in the browser session
+		 *                                    history.
+		 * @param {number}  [options.timeout] Time until the navigation is
+		 *                                    aborted, in milliseconds. Default
+		 *                                    is 10000.
+		 *
+		 * @return {Promise} Promise that resolves once the navigation is
+		 *                   completed or aborted.
+		 */
+		*navigate( href, options = {} ) {
+			const url = cleanUrl( href );
+			navigatingTo = href;
+			actions.prefetch( url, options );
+
+			// Create a promise that resolves when the specified timeout ends.
+			// The timeout value is 10 seconds by default.
+			const timeoutPromise = new Promise( ( resolve ) =>
+				setTimeout( resolve, options.timeout ?? 10000 )
+			);
+
+			const page = yield Promise.race( [
+				pages.get( url ),
+				timeoutPromise,
+			] );
+
+			// Once the page is fetched, the destination URL could have changed
+			// (e.g., by clicking another link in the meantime). If so, bail
+			// out, and let the newer execution to update the HTML.
+			if ( navigatingTo !== href ) return;
+
+			if ( page ) {
+				renderRegions( page );
+				window.history[
+					options.replace ? 'replaceState' : 'pushState'
+				]( {}, '', href );
+			} else {
+				window.location.assign( href );
+				yield new Promise( () => {} );
+			}
 		},
-		*prefetch( ...args ) {
-			return yield prefetch( ...args );
+
+		/**
+		 * Prefetchs the page with the passed URL.
+		 *
+		 * The function normalizes the URL and stores internally the fetch
+		 * promise, to avoid triggering a second fetch for an ongoing request.
+		 *
+		 * @param {string}  url             The page URL.
+		 * @param {Object}  [options]       Options object.
+		 * @param {boolean} [options.force] Force fetching the URL again.
+		 * @param {string}  [options.html]  HTML string to be used instead of
+		 *                                  fetching the requested URL.
+		 */
+		*prefetch( url, options = {} ) {
+			url = cleanUrl( url );
+			if ( options.force || ! pages.has( url ) ) {
+				pages.set( url, fetchPage( url, options ) );
+			}
 		},
 	},
 } );
