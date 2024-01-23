03ae82a8330775688e1a0957b1049d3036daa0dc
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-01-23 18:00:43 +0100

    Make patterns powered by dataviews stable

--- packages/edit-site/src/components/layout/router.js
+++ packages/edit-site/src/components/layout/router.js
@@ -11,7 +11,6 @@ import { useIsSiteEditorLoading } from './hooks';
 import Editor from '../editor';
 import DataviewsPatterns from '../page-patterns/dataviews-patterns';
 import PagePages from '../page-pages';
-import PagePatterns from '../page-patterns';
 import PageTemplatesTemplateParts from '../page-templates-template-parts';
 
 import {
@@ -112,11 +111,7 @@ export default function useLayoutAreas() {
 	if ( path === '/patterns' ) {
 		return {
 			areas: {
-				content: window?.__experimentalAdminViews ? (
-					<DataviewsPatterns />
-				) : (
-					<PagePatterns />
-				),
+				content: <DataviewsPatterns />,
 			},
 		};
 	}
