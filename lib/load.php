c6ac9c010551aed63a5e31f12b0796b08e941747
Daniel Richards <daniel.richards@automattic.com>
2024-01-23 16:49:32 +0800

    Stabilize the pattern overrides block context (#58102)
    * Add pattern/overrides context to individual block.json files
    
    * Remove addition of block context usage via blocks supports
    
    * Add overrides attribute to pattern block and share it via providesContext
    
    * Revert "Add overrides attribute to pattern block and share it via providesContext"
    
    This reverts commit aca9b906e676a16bfa117ebfaa406e9141b72817.
    
    * Remove the experiment flag after reverting pattern block index.php changes
    
    * Register overrides attribute in block.json

--- lib/load.php
+++ lib/load.php
@@ -213,7 +213,6 @@ require __DIR__ . '/block-supports/dimensions.php';
 require __DIR__ . '/block-supports/duotone.php';
 require __DIR__ . '/block-supports/shadow.php';
 require __DIR__ . '/block-supports/background.php';
-require __DIR__ . '/block-supports/pattern.php';
 
 // Data views.
 require_once __DIR__ . '/experimental/data-views.php';
