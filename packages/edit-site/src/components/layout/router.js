5f2d4facdf5381592996b753a1a005b9685c66d2
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-01-23 18:44:28 +0100

    Nicer diff

--- packages/edit-site/src/components/layout/router.js
+++ packages/edit-site/src/components/layout/router.js
@@ -9,8 +9,8 @@ import { privateApis as routerPrivateApis } from '@wordpress/router';
 import { unlock } from '../../lock-unlock';
 import { useIsSiteEditorLoading } from './hooks';
 import Editor from '../editor';
-import PagePatterns from '../page-patterns';
 import PagePages from '../page-pages';
+import PagePatterns from '../page-patterns';
 import PageTemplatesTemplateParts from '../page-templates-template-parts';
 
 import {
