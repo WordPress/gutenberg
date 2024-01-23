e4bc421225535345f424a67421e7fbee5a17498c
Marco Ciampini <marco.ciampo@gmail.com>
2024-01-23 14:06:32 +0100

    Remove unncecessary input

--- packages/components/src/button/test/index.tsx
+++ packages/components/src/button/test/index.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 /**
