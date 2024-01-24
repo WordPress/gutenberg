38a86cf691ecafb5a22bbdf8d61fe84f00cae0d2
Rich Tabor <hi@richtabor.com>
2024-01-24 09:17:52 -0500

    Move zoom out to the preview dropdown

--- packages/editor/src/components/preview-dropdown/index.js
+++ packages/editor/src/components/preview-dropdown/index.js
@@ -10,11 +10,18 @@ import {
 	Icon,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { check, desktop, mobile, tablet, external } from '@wordpress/icons';
+import {
+	check,
+	desktop,
+	mobile,
+	tablet,
+	external,
+	chevronUpDown,
+} from '@wordpress/icons';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { store as preferencesStore } from '@wordpress/preferences';
-
+import { store as blockEditorStore } from '@wordpress/block-editor';
 /**
  * Internal dependencies
  */
@@ -22,22 +29,35 @@ import { store as editorStore } from '../../store';
 import PostPreviewButton from '../post-preview-button';
 
 export default function PreviewDropdown( { forceIsAutosaveable, disabled } ) {
-	const { deviceType, homeUrl, isTemplate, isViewable, showIconLabels } =
-		useSelect( ( select ) => {
-			const { getDeviceType, getCurrentPostType } = select( editorStore );
-			const { getUnstableBase, getPostType } = select( coreStore );
-			const { get } = select( preferencesStore );
-			const _currentPostType = getCurrentPostType();
-			return {
-				deviceType: getDeviceType(),
-				homeUrl: getUnstableBase()?.home,
-				isTemplate: _currentPostType === 'wp_template',
-				isViewable: getPostType( _currentPostType )?.viewable ?? false,
-				showIconLabels: get( 'core', 'showIconLabels' ),
-			};
-		}, [] );
+	const {
+		deviceType,
+		homeUrl,
+		isTemplate,
+		isViewable,
+		showIconLabels,
+		blockEditorMode,
+	} = useSelect( ( select ) => {
+		const { __unstableGetEditorMode } = select( blockEditorStore );
+		const { getDeviceType, getCurrentPostType } = select( editorStore );
+		const { getUnstableBase, getPostType } = select( coreStore );
+		const { get } = select( preferencesStore );
+		const _currentPostType = getCurrentPostType();
+		return {
+			blockEditorMode: __unstableGetEditorMode(),
+			deviceType: getDeviceType(),
+			homeUrl: getUnstableBase()?.home,
+			isTemplate: _currentPostType === 'wp_template',
+			isViewable: getPostType( _currentPostType )?.viewable ?? false,
+			showIconLabels: get( 'core', 'showIconLabels' ),
+		};
+	}, [] );
 	const { setDeviceType } = useDispatch( editorStore );
+	const { __unstableSetEditorMode } = useDispatch( blockEditorStore );
+	const isZoomedOutViewExperimentEnabled =
+		window?.__experimentalEnableZoomedOutView;
+	const isZoomedOutView = blockEditorMode === 'zoom-out';
 	const isMobile = useViewportMatch( 'medium', '<' );
+
 	if ( isMobile ) return null;
 
 	const popoverProps = {
@@ -75,23 +95,48 @@ export default function PreviewDropdown( { forceIsAutosaveable, disabled } ) {
 					<MenuGroup>
 						<MenuItem
 							onClick={ () => setDeviceType( 'Desktop' ) }
-							icon={ deviceType === 'Desktop' && check }
+							icon={
+								deviceType === 'Desktop' &&
+								! isZoomedOutView &&
+								check
+							}
 						>
 							{ __( 'Desktop' ) }
 						</MenuItem>
 						<MenuItem
-							onClick={ () => setDeviceType( 'Tablet' ) }
+							onClick={ () => {
+								setDeviceType( 'Tablet' );
+								__unstableSetEditorMode( 'edit' );
+							} }
 							icon={ deviceType === 'Tablet' && check }
 						>
 							{ __( 'Tablet' ) }
 						</MenuItem>
 						<MenuItem
-							onClick={ () => setDeviceType( 'Mobile' ) }
+							onClick={ () => {
+								setDeviceType( 'Mobile' );
+								__unstableSetEditorMode( 'edit' );
+							} }
 							icon={ deviceType === 'Mobile' && check }
 						>
 							{ __( 'Mobile' ) }
 						</MenuItem>
 					</MenuGroup>
+					{ isZoomedOutViewExperimentEnabled && (
+						<MenuGroup>
+							<MenuItem
+								onClick={ () => {
+									setDeviceType( 'Desktop' );
+									__unstableSetEditorMode(
+										isZoomedOutView ? 'edit' : 'zoom-out'
+									);
+								} }
+								icon={ isZoomedOutView ? check : chevronUpDown }
+							>
+								{ __( 'Zoom out' ) }
+							</MenuItem>
+						</MenuGroup>
+					) }
 					{ isTemplate && (
 						<MenuGroup>
 							<MenuItem
