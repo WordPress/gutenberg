12d1ab74a30f0789756e036ac2bc80e8be2116c4
Rich Tabor <hi@richtabor.com>
2024-01-24 09:20:12 -0500

    Remove existing control from document tools

--- packages/edit-site/src/components/header-edit-mode/document-tools/index.js
+++ packages/edit-site/src/components/header-edit-mode/document-tools/index.js
@@ -1,16 +1,9 @@
 /**
  * WordPress dependencies
  */
-import { useViewportMatch } from '@wordpress/compose';
-import { store as blockEditorStore } from '@wordpress/block-editor';
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { chevronUpDown } from '@wordpress/icons';
-import { Button, ToolbarItem } from '@wordpress/components';
-import {
-	store as editorStore,
-	privateApis as editorPrivateApis,
-} from '@wordpress/editor';
+import { privateApis as editorPrivateApis } from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -19,11 +12,7 @@ import { unlock } from '../../../lock-unlock';
 
 const { DocumentTools: EditorDocumentTools } = unlock( editorPrivateApis );
 
-export default function DocumentTools( {
-	blockEditorMode,
-	hasFixedToolbar,
-	isDistractionFree,
-} ) {
+export default function DocumentTools() {
 	const { isVisualMode } = useSelect( ( select ) => {
 		const { getEditorMode } = select( editorStore );
 
@@ -31,38 +20,11 @@ export default function DocumentTools( {
 			isVisualMode: getEditorMode() === 'visual',
 		};
 	}, [] );
-	const { __unstableSetEditorMode } = useDispatch( blockEditorStore );
-	const { setDeviceType } = useDispatch( editorStore );
-	const isLargeViewport = useViewportMatch( 'medium' );
-	const isZoomedOutViewExperimentEnabled =
-		window?.__experimentalEnableZoomedOutView && isVisualMode;
-	const isZoomedOutView = blockEditorMode === 'zoom-out';
 
 	return (
 		<EditorDocumentTools
 			disableBlockTools={ ! isVisualMode }
 			listViewLabel={ __( 'List View' ) }
-		>
-			{ isZoomedOutViewExperimentEnabled &&
-				isLargeViewport &&
-				! isDistractionFree &&
-				! hasFixedToolbar && (
-					<ToolbarItem
-						as={ Button }
-						className="edit-site-header-edit-mode__zoom-out-view-toggle"
-						icon={ chevronUpDown }
-						isPressed={ isZoomedOutView }
-						/* translators: button label text should, if possible, be under 16 characters. */
-						label={ __( 'Zoom-out View' ) }
-						onClick={ () => {
-							setDeviceType( 'Desktop' );
-							__unstableSetEditorMode(
-								isZoomedOutView ? 'edit' : 'zoom-out'
-							);
-						} }
-						size="compact"
-					/>
-				) }
-		</EditorDocumentTools>
+		/>
 	);
 }
