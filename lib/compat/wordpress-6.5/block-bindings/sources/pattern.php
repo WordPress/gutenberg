a69d979d7c3db675641f6bca4617d321931b07a0
Michal Czaplinski <mmczaplinski@gmail.com>
2024-01-24 15:16:21 +0000

    Remove changes that sneaked in during the rebase

--- lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
+++ lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
@@ -9,23 +9,8 @@ if ( function_exists( 'wp_block_bindings_register_source' ) ) {
 		if ( ! _wp_array_get( $block_instance->attributes, array( 'metadata', 'id' ), false ) ) {
 			return null;
 		}
-		$block_id           = $block_instance->attributes['metadata']['id'];
-		$attribute_override = _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id, $attribute_name ), null );
-		if ( null === $attribute_override ) {
-			return null;
-		}
-		switch ( $attribute_override[0] ) {
-			case 0: // remove
-				/**
-				 * TODO: This currently doesn't remove the attribute, but only set it to an empty string.
-				 * It's a temporary solution until the block binding API supports different operations.
-				 */
-				return '';
-			case 1: // replace
-				return $attribute_override[1];
-			default:
-				return null;
-		}
+		$block_id = $block_instance->attributes['metadata']['id'];
+		return _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id, $attribute_name ), null );
 	};
 	wp_block_bindings_register_source(
 		'pattern_attributes',
