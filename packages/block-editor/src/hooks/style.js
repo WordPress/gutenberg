2e01f2208e1e43492236684315f1654cb54e2973
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2024-01-25 09:10:12 +1100

    Dimensions: Add Aspect Ratio block support (#56897)
    * Try: Dimensions Aspect Ratio block support
    
    * Try server-rendering output
    
    * Try outputting has-aspect-ratio classname
    
    * Ensure block support is checked properly
    
    * Try adding global styles support
    
    * Update docs, add appropriate checks
    
    * Try unsetting minHeight
    
    * Try unsetting min-height when applying aspect ratio and vice versa
    
    * Allow Cover block to expand when the content extends beyond the boundaries of the aspect-ratio
    
    * Hide cover block resize handle when aspect ratio is set, clear aspect ratio when updating cover block min height
    
    * Clear aspect-ratio if minHeight is set on the site frontend
    
    * Attempt to get unset rules playing nicely in the editor with global styles
    
    * Hide aspectRatio control from global styles for the group block
    
    * Skip output of has-aspect-ratio if value isn't set

--- packages/block-editor/src/hooks/style.js
+++ packages/block-editor/src/hooks/style.js
@@ -133,10 +133,14 @@ const skipSerializationPathsEdit = {
  */
 const skipSerializationPathsSave = {
 	...skipSerializationPathsEdit,
+	[ `${ DIMENSIONS_SUPPORT_KEY }.aspectRatio` ]: [
+		`${ DIMENSIONS_SUPPORT_KEY }.aspectRatio`,
+	], // Skip serialization of aspect ratio in save mode.
 	[ `${ BACKGROUND_SUPPORT_KEY }` ]: [ BACKGROUND_SUPPORT_KEY ], // Skip serialization of background support in save mode.
 };
 
 const skipSerializationPathsSaveChecks = {
+	[ `${ DIMENSIONS_SUPPORT_KEY }.aspectRatio` ]: true,
 	[ `${ BACKGROUND_SUPPORT_KEY }` ]: true,
 };
 
