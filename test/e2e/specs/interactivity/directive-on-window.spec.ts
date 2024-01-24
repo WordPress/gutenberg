197ab8f58601b8a165c865f243630709b902f416
Carlos Bravo <carlos.bravo@automattic.com>
2024-01-24 14:00:49 +0100

    Fix flaky test on-window, remove duplicate expect on-document

--- test/e2e/specs/interactivity/directive-on-window.spec.ts
+++ test/e2e/specs/interactivity/directive-on-window.spec.ts
@@ -29,16 +29,27 @@ test.describe( 'data-wp-on-window', () => {
 		page,
 	} ) => {
 		const counter = page.getByTestId( 'counter' );
+		const isEventAttached = page.getByTestId( 'isEventAttached' );
 		const visibilityButton = page.getByTestId( 'visibility' );
+
+		await expect( counter ).toHaveText( '0' );
+		await expect( isEventAttached ).toHaveText( 'yes' );
 		await page.setViewportSize( { width: 600, height: 600 } );
 		await expect( counter ).toHaveText( '1' );
+
 		// Remove the element.
 		await visibilityButton.click();
+
 		// This resize should not increase the counter.
 		await page.setViewportSize( { width: 300, height: 600 } );
+
 		// Add the element back.
 		await visibilityButton.click();
 		await expect( counter ).toHaveText( '1' );
+
+		// Wait until the effects run again.
+		await expect( isEventAttached ).toHaveText( 'yes' );
+
 		await page.setViewportSize( { width: 200, height: 600 } );
 		await expect( counter ).toHaveText( '2' );
 	} );
