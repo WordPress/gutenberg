4fb53e09e2c1a6423c0e6a47b94bbfb7a843af3b
James Koster <james@jameskoster.co.uk>
2024-01-24 14:29:16 +0000

    Reduce page selector prominence

--- packages/dataviews/src/pagination.js
+++ packages/dataviews/src/pagination.js
@@ -6,8 +6,8 @@ import {
 	__experimentalHStack as HStack,
 	SelectControl,
 } from '@wordpress/components';
-import { createInterpolateElement, memo } from '@wordpress/element';
-import { sprintf, __, _x } from '@wordpress/i18n';
+import { memo } from '@wordpress/element';
+import { sprintf, __ } from '@wordpress/i18n';
 import { chevronRight, chevronLeft } from '@wordpress/icons';
 
 const Pagination = memo( function Pagination( {
@@ -27,36 +27,33 @@ const Pagination = memo( function Pagination( {
 				justify="end"
 				className="dataviews-pagination"
 			>
-				<HStack justify="flex-start" expanded={ false } spacing={ 2 }>
-					{ createInterpolateElement(
-						sprintf(
+				<HStack justify="flex-start" expanded={ false } spacing={ 1 }>
+					<SelectControl
+						aria-label={ __( 'Current page' ) }
+						value={ view.page }
+						className="dataviews-pagination__page-selector"
+						options={ Array.from( Array( totalPages ) ).map(
+							( _, i ) => {
+								const page = i + 1;
+								return { value: page, label: page };
+							}
+						) }
+						onChange={ ( newValue ) => {
+							onChangeView( {
+								...view,
+								page: +newValue,
+							} );
+						} }
+						size={ 'compact' }
+						__nextHasNoMarginBottom
+					/>
+					<span>
+						{ sprintf(
 							// translators: %s: Total number of pages.
-							_x( 'Page <CurrenPageControl /> of %s', 'paging' ),
+							'of %s',
 							totalPages
-						),
-						{
-							CurrenPageControl: (
-								<SelectControl
-									aria-label={ __( 'Current page' ) }
-									value={ view.page }
-									options={ Array.from(
-										Array( totalPages )
-									).map( ( _, i ) => {
-										const page = i + 1;
-										return { value: page, label: page };
-									} ) }
-									onChange={ ( newValue ) => {
-										onChangeView( {
-											...view,
-											page: +newValue,
-										} );
-									} }
-									size={ 'compact' }
-									__nextHasNoMarginBottom
-								/>
-							),
-						}
-					) }
+						) }
+					</span>
 				</HStack>
 				<HStack expanded={ false } spacing={ 1 }>
 					<Button
