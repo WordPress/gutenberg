59e23bb14454768699e917c6b9f31d9a77d90fa9
Carolina Nymark <hi@themesbycarolina.com>
2024-01-25 08:42:08 +0100

    simplify the check for the taxonomy attribute in index.php

--- packages/block-library/src/post-navigation-link/index.php
+++ packages/block-library/src/post-navigation-link/index.php
@@ -99,8 +99,6 @@ function render_block_core_post_navigation_link( $attributes, $content ) {
 		}
 	}
 
-	$taxonomy = isset( $attributes['taxonomy'] ) ? $attributes['taxonomy'] : '';
-
 	/**
 	 * The dynamic portion of the function name, `$navigation_type`,
 	 * Refers to the type of adjacency, 'next' or 'previous'.
@@ -110,8 +108,8 @@ function render_block_core_post_navigation_link( $attributes, $content ) {
 	 */
 	$get_link_function = "get_{$navigation_type}_post_link";
 
-	if ( $taxonomy ) {
-		$content = $get_link_function( $format, $link, true, '', $taxonomy );
+	if ( ! empty( $attributes['taxonomy'] ) ) {
+		$content = $get_link_function( $format, $link, true, '', $attributes['taxonomy'] );
 	} else {
 		$content = $get_link_function( $format, $link );
 	}
