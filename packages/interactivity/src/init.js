4cc10a7712521cef1aeba407a39788b44ec31262
Weston Ruter <westonruter@google.com>
2024-01-25 13:09:33 -0800

    Delay hydration of nodes until they near the viewport

--- packages/interactivity/src/init.js
+++ packages/interactivity/src/init.js
@@ -30,17 +30,44 @@ function yieldToMain() {
 
 // Initialize the router with the initial DOM.
 export const init = async () => {
+	const pendingNodes = new Set();
+
+	const intersectionObserver = new window.IntersectionObserver(
+		async ( entries ) => {
+			for ( const entry of entries ) {
+				if ( ! entry.isIntersecting ) {
+					continue;
+				}
+
+				const node = entry.target;
+				intersectionObserver.unobserve( node );
+				pendingNodes.delete( node );
+				if ( pendingNodes.size === 0 ) {
+					intersectionObserver.disconnect();
+				}
+
+				if ( ! hydratedIslands.has( node ) ) {
+					await yieldToMain();
+					const fragment = getRegionRootFragment( node );
+					const vdom = toVdom( node );
+					await yieldToMain();
+					hydrate( vdom, fragment );
+				}
+			}
+		},
+		{
+			root: null, // To watch for intersection relative to the device's viewport.
+			rootMargin: '100% 0% 100% 0%', // Intersect when within 1 viewport approaching from top or bottom.
+			threshold: 0.0, // As soon as even one pixel is visible.
+		}
+	);
+
 	const nodes = document.querySelectorAll(
 		`[data-${ directivePrefix }-interactive]`
 	);
 
 	for ( const node of nodes ) {
-		if ( ! hydratedIslands.has( node ) ) {
-			await yieldToMain();
-			const fragment = getRegionRootFragment( node );
-			const vdom = toVdom( node );
-			await yieldToMain();
-			hydrate( vdom, fragment );
-		}
+		pendingNodes.add( node );
+		intersectionObserver.observe( node );
 	}
 };
