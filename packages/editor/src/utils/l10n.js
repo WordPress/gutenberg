e875b898e38029156a33eede0dd22b06ea5053fc
Andrea Fercia <a.fercia@gmail.com>
2024-01-25 14:07:16 +0100

    Fix translatable string in font library modal local fonts.

---
+++ packages/editor/src/utils/l10n.js
@@ -0,0 +1,9 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export const l10n = {
+	/* translators: Used between list items, there is a space after the comma. */
+	listItemSeparator: __( ', ' ),
+};
