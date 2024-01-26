339db9c13aea9a7d0052be83fffab954f0925a23
Fabian Kägy <mail@fabian-kaegy.de>
2024-01-26 10:53:56 +0100

    change rest api priviliges of /templates endpoint to allow anyone with `edit_posts` capability to view templates

--- lib/compat/wordpress-6.4/class-gutenberg-rest-templates-controller-6-4.php
+++ lib/compat/wordpress-6.4/class-gutenberg-rest-templates-controller-6-4.php
@@ -72,4 +72,29 @@ class Gutenberg_REST_Templates_Controller_6_4 extends WP_REST_Templates_Controll
 
 		return $links;
 	}
+
+	/**
+	 * Checks if a given request has access to read templates.
+	 *
+	 * @since 6.5
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
+	 */
+	public function get_items_permissions_check( $request ) {
+		/*
+		 * Allow access to anyone who can edit posts.
+		 */
+		if ( ! current_user_can( 'edit_posts' ) ) {
+			return new WP_Error(
+				'rest_cannot_manage_templates',
+				__( 'Sorry, you are not allowed to access the templates on this site.' ),
+				array(
+					'status' => rest_authorization_required_code(),
+				)
+			);
+		}
+
+		return true;
+	}
 }
