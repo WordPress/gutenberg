cfacffdb6dd52d0ffb5f38611c0461663d9c2af2
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-01-29 13:10:16 +0100

    Resort to waiting for canvas after editor is open

--- packages/e2e-test-utils-playwright/src/editor/set-preferences.ts
+++ packages/e2e-test-utils-playwright/src/editor/set-preferences.ts
@@ -20,7 +20,7 @@ export async function setPreferences(
 	context: PreferencesContext,
 	preferences: Record< string, any >
 ) {
-	await this.canvas.locator( 'body' ).waitFor( { state: 'visible' } );
+	await this.page.waitForFunction( () => window?.wp?.data );
 
 	await this.page.evaluate(
 		async ( props ) => {
