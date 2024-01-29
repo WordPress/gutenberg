f808acdb78850651fc54b3f412202763472c332a
Mario Santos <santosguillamot@gmail.com>
2024-01-29 13:58:06 +0100

    Remove old function from pattern source

--- lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
+++ lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
@@ -4,38 +4,6 @@
  *
  * @package gutenberg
  */
-if ( function_exists( 'wp_block_bindings_register_source' ) ) {
-	$pattern_source_callback = function ( $source_attrs, $block_instance, $attribute_name ) {
-		if ( ! _wp_array_get( $block_instance->attributes, array( 'metadata', 'id' ), false ) ) {
-			return null;
-		}
-		$block_id           = $block_instance->attributes['metadata']['id'];
-		$attribute_override = _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id, $attribute_name ), null );
-		if ( null === $attribute_override ) {
-			return null;
-		}
-		switch ( $attribute_override[0] ) {
-			case 0: // remove
-				/**
-				 * TODO: This currently doesn't remove the attribute, but only set it to an empty string.
-				 * It's a temporary solution until the block binding API supports different operations.
-				 */
-				return '';
-			case 1: // replace
-				return $attribute_override[1];
-			default:
-				return null;
-		}
-	};
-	wp_block_bindings_register_source(
-		'core/pattern-attributes',
-		array(
-			'label' => __( 'Pattern Attributes' ),
-			'apply' => $pattern_source_callback,
-		)
-	);
-}
-
 if ( ! function_exists( 'gutenberg_register_block_bindings_pattern_overrides_source' ) && ! function_exists( 'gutenberg_block_bindings_pattern_overrides_callback' ) ) {
 	function gutenberg_block_bindings_pattern_overrides_callback( $source_attrs ) {
 		if ( ! isset( $source_attrs['key'] ) ) {
