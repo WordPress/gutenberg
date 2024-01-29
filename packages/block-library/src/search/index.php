38989b48bde2165ccf7d3f34b44adc947b1a2b3d
Carlos Bravo <carlos.bravo@automattic.com>
2024-01-29 17:06:46 +0100

    Update docs with removed attributes

--- packages/block-library/src/search/index.php
+++ packages/block-library/src/search/index.php
@@ -9,10 +9,9 @@
  * Dynamically renders the `core/search` block.
  *
  * @since 6.3.0 Using block.json `viewScript` to register script, and update `view_script_handles()` only when needed.
+ * @since 6.5.0 $content and $block parameters are no longer used.
  *
- * @param array    $attributes The block attributes.
- * @param string   $content    The saved content.
- * @param WP_Block $block      The parsed block.
+ * @param array $attributes The block attributes.
  *
  * @return string The search block markup.
  */
