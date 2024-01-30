c5434185076f592365ca7d84ceeb157218003ea7
Jorge Costa <jorge.costa@developer.pt>
2024-01-30 19:41:46 +0000

    updates

--- packages/dataviews/src/view-grid.js
+++ packages/dataviews/src/view-grid.js
@@ -13,11 +13,115 @@ import {
 	Tooltip,
 } from '@wordpress/components';
 import { useAsyncList } from '@wordpress/compose';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import ItemActions from './item-actions';
+import SingleSelectionCheckbox from './single-selection-checkbox';
+
+function GridItem( {
+	selection,
+	data,
+	onSelectionChange,
+	getItemId,
+	item,
+	actions,
+	mediaField,
+	primaryField,
+	visibleFields,
+} ) {
+	const [ hasNoPointerEvents, setHasNoPointerEvents ] = useState( false );
+	const id = getItemId( item );
+	const isSelected = selection.includes( id );
+	return (
+		<VStack
+			spacing={ 0 }
+			key={ id }
+			className={ classnames( 'dataviews-view-grid__card', {
+				'is-selected': isSelected,
+				'has-no-pointer-events': hasNoPointerEvents,
+			} ) }
+			onMouseDown={ ( event ) => {
+				if ( event.ctrlKey || event.metaKey ) {
+					setHasNoPointerEvents( true );
+					if ( ! isSelected ) {
+						onSelectionChange(
+							data.filter( ( _item ) => {
+								const itemId = getItemId?.( _item );
+								return (
+									itemId === id ||
+									selection.includes( itemId )
+								);
+							} )
+						);
+					} else {
+						onSelectionChange(
+							data.filter( ( _item ) => {
+								const itemId = getItemId?.( _item );
+								return (
+									itemId !== id &&
+									selection.includes( itemId )
+								);
+							} )
+						);
+					}
+				}
+			} }
+			onClick={ () => {
+				if ( hasNoPointerEvents ) {
+					setHasNoPointerEvents( false );
+				}
+			} }
+		>
+			<div className="dataviews-view-grid__media">
+				{ mediaField?.render( { item } ) }
+			</div>
+			<HStack
+				justify="space-between"
+				className="dataviews-view-grid__title-actions"
+			>
+				<SingleSelectionCheckbox
+					id={ id }
+					item={ item }
+					selection={ selection }
+					onSelectionChange={ onSelectionChange }
+					getItemId={ getItemId }
+					data={ data }
+					primaryField={ primaryField }
+				/>
+				<HStack className="dataviews-view-grid__primary-field">
+					{ primaryField?.render( { item } ) }
+				</HStack>
+				<ItemActions item={ item } actions={ actions } isCompact />
+			</HStack>
+			<VStack className="dataviews-view-grid__fields" spacing={ 3 }>
+				{ visibleFields.map( ( field ) => {
+					const renderedValue = field.render( {
+						item,
+					} );
+					if ( ! renderedValue ) {
+						return null;
+					}
+					return (
+						<VStack
+							className="dataviews-view-grid__field"
+							key={ field.id }
+							spacing={ 1 }
+						>
+							<Tooltip text={ field.header } placement="left">
+								<div className="dataviews-view-grid__field-value">
+									{ renderedValue }
+								</div>
+							</Tooltip>
+						</VStack>
+					);
+				} ) }
+			</VStack>
+		</VStack>
+	);
+}
 
 export default function ViewGrid( {
 	data,
@@ -52,87 +156,19 @@ export default function ViewGrid( {
 			className="dataviews-view-grid"
 		>
 			{ usedData.map( ( item ) => {
-				const id = getItemId( item );
-				const isSelected = selection.includes( getItemId( item ) );
 				return (
-					<VStack
-						spacing={ 0 }
-						key={ id }
-						className={ classnames( 'dataviews-view-grid__card', {
-							'is-selected': isSelected,
-						} ) }
-						onClick={ ( event ) => {
-							if ( event.ctrlKey || event.metaKey ) {
-								if ( ! isSelected ) {
-									onSelectionChange(
-										data.filter( ( _item ) => {
-											const itemId = getItemId?.( _item );
-											return (
-												itemId === id ||
-												selection.includes( itemId )
-											);
-										} )
-									);
-								} else {
-									onSelectionChange(
-										data.filter( ( _item ) => {
-											const itemId = getItemId?.( _item );
-											return (
-												itemId !== id &&
-												selection.includes( itemId )
-											);
-										} )
-									);
-								}
-							}
-						} }
-					>
-						<div className="dataviews-view-grid__media">
-							{ mediaField?.render( { item } ) }
-						</div>
-						<HStack
-							justify="space-between"
-							className="dataviews-view-grid__title-actions"
-						>
-							<HStack className="dataviews-view-grid__primary-field">
-								{ primaryField?.render( { item } ) }
-							</HStack>
-							<ItemActions
-								item={ item }
-								actions={ actions }
-								isCompact
-							/>
-						</HStack>
-						<VStack
-							className="dataviews-view-grid__fields"
-							spacing={ 3 }
-						>
-							{ visibleFields.map( ( field ) => {
-								const renderedValue = field.render( {
-									item,
-								} );
-								if ( ! renderedValue ) {
-									return null;
-								}
-								return (
-									<VStack
-										className="dataviews-view-grid__field"
-										key={ field.id }
-										spacing={ 1 }
-									>
-										<Tooltip
-											text={ field.header }
-											placement="left"
-										>
-											<div className="dataviews-view-grid__field-value">
-												{ renderedValue }
-											</div>
-										</Tooltip>
-									</VStack>
-								);
-							} ) }
-						</VStack>
-					</VStack>
+					<GridItem
+						key={ getItemId( item ) }
+						selection={ selection }
+						data={ data }
+						onSelectionChange={ onSelectionChange }
+						getItemId={ getItemId }
+						item={ item }
+						actions={ actions }
+						mediaField={ mediaField }
+						primaryField={ primaryField }
+						visibleFields={ visibleFields }
+					/>
 				);
 			} ) }
 		</Grid>
