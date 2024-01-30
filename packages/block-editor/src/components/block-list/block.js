740855b9c23fee955996b2d94e2f0e65386f376b
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2024-01-30 16:04:23 +1100

    Drag and drop: Allow dropping within template parts

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -517,6 +517,7 @@ function BlockListBlockProvider( props ) {
 				__unstableIsFullySelected,
 				__unstableSelectionHasUnmergeableBlock,
 				isBlockBeingDragged,
+				isDraggingBlocks,
 				hasBlockMovingClientId,
 				canInsertBlockType,
 				getBlockRootClientId,
@@ -594,7 +595,9 @@ function BlockListBlockProvider( props ) {
 					blockEditingMode === 'disabled' &&
 					isBlockSubtreeDisabled( clientId ),
 				isOutlineEnabled: outlineMode,
-				hasOverlay: __unstableHasActiveBlockOverlayActive( clientId ),
+				hasOverlay:
+					__unstableHasActiveBlockOverlayActive( clientId ) &&
+					! isDraggingBlocks(),
 				initialPosition:
 					_isSelected && __unstableGetEditorMode() === 'edit'
 						? getSelectedBlocksInitialCaretPosition()
