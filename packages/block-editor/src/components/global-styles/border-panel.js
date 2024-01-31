1ef8a7221513b356a82412da34c49a57cdccceb1
Vicente Canales <vicente.canales@automattic.com>
2024-01-31 10:45:22 -0300

    move shadow control components to own file

--- packages/block-editor/src/components/global-styles/border-panel.js
+++ packages/block-editor/src/components/global-styles/border-panel.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import {
@@ -13,19 +8,10 @@ import {
 	__experimentalToolsPanel as ToolsPanel,
 	__experimentalToolsPanelItem as ToolsPanelItem,
 	__experimentalItemGroup as ItemGroup,
-	__experimentalHStack as HStack,
-	__experimentalVStack as VStack,
-	__experimentalHeading as Heading,
-	__experimentalGrid as Grid,
-	__experimentalDropdownContentWrapper as DropdownContentWrapper,
 	BaseControl,
-	Dropdown,
-	Button,
-	FlexItem,
 } from '@wordpress/components';
 import { useCallback, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { shadow as shadowIcon, Icon, check } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -36,6 +22,7 @@ import { getValueFromVariable, TOOLSPANEL_DROPDOWNMENU_PROPS } from './utils';
 import { mergeOrigins } from '../../store/get-block-settings';
 import { setImmutably } from '../../utils/object';
 import { getBorderPanelLabel } from '../../hooks/border';
+import { ShadowPopover } from './shadow-panel-components';
 
 function useHasShadowControl( settings ) {
 	return !! settings?.shadow;
@@ -300,111 +287,5 @@ export default function BorderPanel( {
 				</>
 			) }
 		</Wrapper>
-	);
-}
-
-const ShadowPopover = ( { shadow, onShadowChange, settings } ) => {
-	const popoverProps = {
-		placement: 'left-start',
-		offset: 36,
-		shift: true,
-	};
-
-	return (
-		<Dropdown
-			popoverProps={ popoverProps }
-			className="block-editor-global-styles-effects-panel__shadow-dropdown"
-			renderToggle={ renderShadowToggle() }
-			renderContent={ () => (
-				<DropdownContentWrapper paddingSize="medium">
-					<ShadowPopoverContainer
-						shadow={ shadow }
-						onShadowChange={ onShadowChange }
-						settings={ settings }
-					/>
-				</DropdownContentWrapper>
-			) }
-		/>
-	);
-};
-
-function renderShadowToggle() {
-	return ( { onToggle, isOpen } ) => {
-		const toggleProps = {
-			onClick: onToggle,
-			className: classnames( { 'is-open': isOpen } ),
-			'aria-expanded': isOpen,
-		};
-
-		return (
-			<Button { ...toggleProps }>
-				<HStack justify="flex-start">
-					<Icon
-						className="block-editor-global-styles-effects-panel__toggle-icon"
-						icon={ shadowIcon }
-						size={ 24 }
-					/>
-					<FlexItem>{ __( 'Box shadow' ) }</FlexItem>
-				</HStack>
-			</Button>
-		);
-	};
-}
-
-function ShadowPopoverContainer( { shadow, onShadowChange, settings } ) {
-	const defaultShadows = settings?.shadow?.presets?.default;
-	const themeShadows = settings?.shadow?.presets?.theme;
-	const defaultPresetsEnabled = settings?.shadow?.defaultPresets;
-
-	const shadows = [
-		...( defaultPresetsEnabled ? defaultShadows : [] ),
-		...( themeShadows || [] ),
-	];
-
-	return (
-		<div className="block-editor-global-styles-effects-panel__shadow-popover-container">
-			<VStack spacing={ 4 }>
-				<Heading level={ 5 }>{ __( 'Presets' ) }</Heading>
-				<ShadowPresets
-					presets={ shadows }
-					activeShadow={ shadow }
-					onSelect={ onShadowChange }
-				/>
-			</VStack>
-		</div>
-	);
-}
-
-function ShadowPresets( { presets, activeShadow, onSelect } ) {
-	return ! presets ? null : (
-		<Grid columns={ 6 } gap={ 0 } align="center" justify="center">
-			{ presets.map( ( { name, slug, shadow } ) => (
-				<ShadowIndicator
-					key={ slug }
-					label={ name }
-					isActive={ shadow === activeShadow }
-					onSelect={ () =>
-						onSelect( shadow === activeShadow ? undefined : shadow )
-					}
-					shadow={ shadow }
-				/>
-			) ) }
-		</Grid>
-	);
-}
-
-function ShadowIndicator( { label, isActive, onSelect, shadow } ) {
-	return (
-		<div className="block-editor-global-styles-effects-panel__shadow-indicator-wrapper">
-			<Button
-				className="block-editor-global-styles-effects-panel__shadow-indicator"
-				onClick={ onSelect }
-				label={ label }
-				style={ { boxShadow: shadow } }
-				showTooltip
-			>
-				{ isActive && <Icon icon={ check } /> }
-			</Button>
-		</div>
 	);
 }
