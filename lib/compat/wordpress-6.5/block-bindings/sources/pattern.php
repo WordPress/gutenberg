05eb127737b3799b21f02455b22ad5ae1c716b53
Daniel Richards <daniel.richards@automattic.com>
2024-01-31 17:44:27 +0800

    Revert "Use a patch format and support `linkTarget` of `core/button` for Pattern Overrides (#58165)" (#58489)
    This reverts commit a0fccd15db464ee7f1769f63fe4135c19dba4890.

--- lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
+++ lib/compat/wordpress-6.5/block-bindings/sources/pattern.php
@@ -8,23 +8,8 @@ function gutenberg_block_bindings_pattern_overrides_callback( $source_attrs, $bl
 	if ( ! _wp_array_get( $block_instance->attributes, array( 'metadata', 'id' ), false ) ) {
 		return null;
 	}
-	$block_id           = $block_instance->attributes['metadata']['id'];
-	$attribute_override = _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id, $attribute_name ), null );
-	if ( null === $attribute_override ) {
-		return null;
-	}
-	switch ( $attribute_override[0] ) {
-		case 0: // remove
-			/**
-			 * TODO: This currently doesn't remove the attribute, but only set it to an empty string.
-			 * It's a temporary solution until the block binding API supports different operations.
-			 */
-			return '';
-		case 1: // replace
-			return $attribute_override[1];
-		default:
-			return null;
-	}
+	$block_id = $block_instance->attributes['metadata']['id'];
+	return _wp_array_get( $block_instance->context, array( 'pattern/overrides', $block_id, $attribute_name ), null );
 }
 
 function gutenberg_register_block_bindings_pattern_overrides_source() {
