abdd0e1811b16563a0eeae00157da526c5d6e0bf
David Arenas <david.arenas@automattic.com>
2024-01-31 12:23:36 +0100

    Expose private APIs with a simple funciton

--- packages/interactivity/src/index.js
+++ packages/interactivity/src/index.js
@@ -2,7 +2,9 @@
  * Internal dependencies
  */
 import registerDirectives from './directives';
-import { init } from './init';
+import { init, getRegionRootFragment, initialVdom } from './init';
+import { directivePrefix } from './constants';
+import { toVdom } from './vdom';
 
 export { store } from './store';
 export { directive, getContext, getElement, getNamespace } from './hooks';
@@ -15,13 +17,26 @@ export {
 	useCallback,
 	useMemo,
 } from './utils';
-export { directivePrefix } from './constants';
-export { toVdom } from './vdom';
-export { getRegionRootFragment } from './init';
 
 export { h as createElement, cloneElement, render } from 'preact';
 export { useContext, useState, useRef } from 'preact/hooks';
 export { deepSignal } from 'deepsignal';
+
+const requiredConsent =
+	'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
+
+export const privateApis = ( lock ) => {
+	if ( lock === requiredConsent ) {
+		return {
+			directivePrefix,
+			getRegionRootFragment,
+			initialVdom,
+			toVdom,
+		};
+	}
+
+	throw new Error( 'TODO: Add a more appropriate message.' );
+};
 
 document.addEventListener( 'DOMContentLoaded', async () => {
 	registerDirectives();
