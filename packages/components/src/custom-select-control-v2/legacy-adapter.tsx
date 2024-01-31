e0caf2d3c576d65541fe9731fb64cb6c44f4dd07
brookewp <brooke.kaminski@automattic.com>
2024-01-31 15:34:01 -0800

    Remove export that is no longer needed

--- packages/components/src/custom-select-control-v2/legacy-adapter.tsx
+++ packages/components/src/custom-select-control-v2/legacy-adapter.tsx
@@ -5,7 +5,7 @@ import _LegacyCustomSelect from './legacy-component';
 import _NewCustomSelect from './default-component';
 import type { CustomSelectProps, LegacyCustomSelectProps } from './types';
 
-export function isLegacy( props: any ): props is LegacyCustomSelectProps {
+function isLegacy( props: any ): props is LegacyCustomSelectProps {
 	return typeof props.options !== 'undefined';
 }
 
