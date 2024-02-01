9d578ca92f05fdae79f2bf9dafb3921790852eae
Gerardo <gerardo.pacheco@automattic.com>
2024-02-01 11:59:30 +0100

    Update react-native-config.js to include all packages that we are manually linking. This is needed to be able to use React Native's applyNativeModulesAppBuildGradle which includes needed configuration for other integration things. This auto links modules so we need to skip the ones we manually link.

--- packages/react-native-editor/react-native.config.js
+++ packages/react-native-editor/react-native.config.js
@@ -3,13 +3,100 @@
  */
 const path = require( 'path' );
 
+// Currently all native libraries are linked manually for both platforms.
 module.exports = {
 	dependencies: {
 		'@wordpress/react-native-bridge': {
 			root: path.resolve( __dirname, '../react-native-bridge' ),
+			platforms: {
+				android: null,
+				ios: null,
+			},
 		},
 		'@wordpress/react-native-aztec': {
 			root: path.resolve( __dirname, '../react-native-aztec' ),
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'@react-native-clipboard/clipboard': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'@react-native-community/blur': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'@react-native-masked-view/masked-view': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-fast-image': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-gesture-handler': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-reanimated': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-get-random-values': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-linear-gradient': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-safe-area-context': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-screens': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-svg': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-video': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
+		},
+		'react-native-webview': {
+			platforms: {
+				android: null,
+				ios: null,
+			},
 		},
 	},
 	project: {
