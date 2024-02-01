71b2c1a58dd8137547497e457b8d137ff6ba1179
brookewp <brooke.kaminski@automattic.com>
2024-02-01 15:44:54 -0800

    Add WordPressComponentProps to default export

--- packages/components/src/custom-select-control-v2/legacy-adapter.tsx
+++ packages/components/src/custom-select-control-v2/legacy-adapter.tsx
@@ -4,13 +4,17 @@
 import _LegacyCustomSelect from './legacy-component';
 import _NewCustomSelect from './default-component';
 import type { CustomSelectProps, LegacyCustomSelectProps } from './types';
+import type { WordPressComponentProps } from '../context';
 
 function isLegacy( props: any ): props is LegacyCustomSelectProps {
 	return typeof props.options !== 'undefined';
 }
 
 export function DefaultExport(
-	props: LegacyCustomSelectProps | CustomSelectProps
+	props: WordPressComponentProps<
+		LegacyCustomSelectProps | CustomSelectProps,
+		'button'
+	>
 ) {
 	if ( isLegacy( props ) ) {
 		return <_LegacyCustomSelect { ...props } />;
