1ea02d5db34949d380d271ea6f2c5615d671b36f
Jarda Snajdr <jsnajdr@gmail.com>
2024-02-01 10:03:19 +0100

    Revert "Experiment: capture input in suspense placeholder"
    This reverts commit 5e969f3a8cd1334208364824a0251045b9ba2607.

--- packages/block-library/src/utils/lazy-load.js
+++ packages/block-library/src/utils/lazy-load.js
@@ -1,62 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { Suspense, lazy, useEffect, useState } from '@wordpress/element';
-import { useBlockProps } from '@wordpress/block-editor';
-
-const FallbackEdit = ( { tempContent, setTempContent } ) => {
-	const blockProps = useBlockProps();
-	const onChange = ( e ) => {
-		setTempContent( e.target.value );
-	};
-
-	return (
-		<div { ...blockProps }>
-			<input value={ tempContent } onChange={ onChange } />
-		</div>
-	);
-};
-
-// Sets the block content on mount
-const Init = ( { content, setContent } ) => {
-	useEffect( () => {
-		if ( content.length > 0 ) {
-			setContent( content );
-		}
-	}, [] );
-
-	return null;
-};
-
-// Add delay to the module load for better debugging
-const delay = async ( cb, ms = 0 ) => {
-	if ( ms > 0 ) {
-		await new Promise( ( r ) => setTimeout( r, ms ) );
-	}
-	return await cb();
-};
+import { Suspense, lazy } from '@wordpress/element';
 
 export default function lazyEdit( cb ) {
 	// eslint-disable-next-line @wordpress/no-unused-vars-before-return
-	const Load = lazy( () => delay( cb, 3000 ) );
+	const Load = lazy( cb );
 	return function Edit( props ) {
-		// captures what was typed into the placeholder while loading
-		const [ tempContent, setTempContent ] = useState( '' );
 		return (
-			<Suspense
-				fallback={
-					<FallbackEdit
-						tempContent={ tempContent }
-						setTempContent={ setTempContent }
-					/>
-				}
-			>
-				<Init
-					content={ tempContent }
-					setContent={ ( content ) =>
-						props.setAttributes( { content } )
-					}
-				/>
+			<Suspense fallback={ null }>
 				<Load { ...props } />
 			</Suspense>
 		);
