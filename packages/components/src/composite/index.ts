533616c8222448a47ee88fd5718bc82bc8beb99c
Andrew Hayward <andrew.hayward@automattic.com>
2024-02-02 15:46:18 +0000

    Updating usage comment

--- packages/components/src/composite/index.ts
+++ packages/components/src/composite/index.ts
@@ -1,8 +1,7 @@
 // Originally this pointed at a Reakit implementation of
 // `Composite`, but we are removing Reakit entirely from the
-// codebase. Until the new 'current' Ariakit implementation
-// is unlocked and no longer private, we will continue to
-// support the Reakit API through the 'legacy' version,
-// which uses Ariakit under the hood.
+// codebase. We will continue to support the Reakit API
+// through the 'legacy' version, which uses Ariakit under
+// the hood.
 
 export * from './legacy';
