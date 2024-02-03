41631a05d5b3cda7dd5658b5fac7fe80a817c8b7
George Mamadashvili <georgemamadashvili@gmail.com>
2024-02-03 15:16:14 +0400

    Pattern: Use the '__experimentalLabel' method to get a title

--- packages/block-library/src/block/index.js
+++ packages/block-library/src/block/index.js
@@ -2,6 +2,9 @@
  * WordPress dependencies
  */
 import { symbol as icon } from '@wordpress/icons';
+import { store as coreStore } from '@wordpress/core-data';
+import { select } from '@wordpress/data';
+import { decodeEntities } from '@wordpress/html-entities';
 
 /**
  * Internal dependencies
@@ -17,6 +20,22 @@ export { metadata, name };
 export const settings = {
 	edit,
 	icon,
+	__experimentalLabel: ( { ref } ) => {
+		if ( ! ref ) {
+			return;
+		}
+
+		const entity = select( coreStore ).getEditedEntityRecord(
+			'postType',
+			'wp_block',
+			ref
+		);
+		if ( ! entity?.title ) {
+			return;
+		}
+
+		return decodeEntities( entity.title );
+	},
 };
 
 export const init = () => initBlock( { name, metadata, settings } );
