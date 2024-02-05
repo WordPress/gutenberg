b9bbe961d04256a5879de63fbf8e4813caeabd98
Fabian Kägy <mail@fabian-kaegy.de>
2024-02-05 17:43:19 +0100

    fix php cs issues

--- lib/compat/wordpress-6.4/class-gutenberg-rest-templates-controller-6-4.php
+++ lib/compat/wordpress-6.4/class-gutenberg-rest-templates-controller-6-4.php
@@ -78,17 +78,16 @@ class Gutenberg_REST_Templates_Controller_6_4 extends WP_REST_Templates_Controll
 	 *
 	 * @since 6.5
 	 *
-	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
 	 */
-	public function get_items_permissions_check( $request ) {
+	public function get_items_permissions_check() {
 		/*
 		 * Allow access to anyone who can edit posts.
 		 */
 		if ( ! current_user_can( 'edit_posts' ) ) {
 			return new WP_Error(
 				'rest_cannot_manage_templates',
-				__( 'Sorry, you are not allowed to access the templates on this site.' ),
+				__( 'Sorry, you are not allowed to access the templates on this site.', 'default' ),
 				array(
 					'status' => rest_authorization_required_code(),
 				)
@@ -103,17 +102,16 @@ class Gutenberg_REST_Templates_Controller_6_4 extends WP_REST_Templates_Controll
 	 *
 	 * @since 6.5
 	 *
-	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
 	 */
-	public function get_item_permissions_check( $request ) {
+	public function get_item_permissions_check() {
 		/*
 		 * Allow access to anyone who can edit posts.
 		 */
 		if ( ! current_user_can( 'edit_posts' ) ) {
 			return new WP_Error(
 				'rest_cannot_manage_templates',
-				__( 'Sorry, you are not allowed to access the templates on this site.' ),
+				__( 'Sorry, you are not allowed to access the templates on this site.', 'default' ),
 				array(
 					'status' => rest_authorization_required_code(),
 				)
