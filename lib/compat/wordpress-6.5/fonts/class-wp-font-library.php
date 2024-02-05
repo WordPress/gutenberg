b8db2bd9bd86e8805e9649693d17ba0cfadcf061
Grant Kinney <hi@grant.mk>
2024-02-05 13:21:16 -0600

    Fix translators comment indentation

--- lib/compat/wordpress-6.5/fonts/class-wp-font-library.php
+++ lib/compat/wordpress-6.5/fonts/class-wp-font-library.php
@@ -52,7 +52,7 @@ if ( ! class_exists( 'WP_Font_Library' ) ) {
 
 			if ( $this->is_collection_registered( $new_collection->slug ) ) {
 				$error_message = sprintf(
-				/* translators: %s: Font collection slug. */
+					/* translators: %s: Font collection slug. */
 					__( 'Font collection with slug: "%s" is already registered.', 'gutenberg' ),
 					$new_collection->slug
 				);
