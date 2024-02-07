0102a237e55b2a3fec12927ec5d5032544997843
Riad Benguella <benguella@gmail.com>
2024-02-07 13:49:50 +0100

    fix unit tests

--- phpunit/tests/fonts/font-library/wpRestFontCollectionsController.php
+++ phpunit/tests/fonts/font-library/wpRestFontCollectionsController.php
@@ -52,8 +52,9 @@ class Tests_REST_WpRestFontCollectionsController extends WP_Test_REST_Controller
 	 */
 	public function test_register_routes() {
 		$routes = rest_get_server()->get_routes();
-		$this->assertCount( 1, $routes['/wp/v2/font-collections'], 'Rest server has not the collections path initialized.' );
-		$this->assertCount( 1, $routes['/wp/v2/font-collections/(?P<slug>[\/\w-]+)'], 'Rest server has not the collection path initialized.' );
+		// Double becaue the route is registered by both core and gutenberg.
+		$this->assertCount( 2, $routes['/wp/v2/font-collections'], 'Rest server has not the collections path initialized.' );
+		$this->assertCount( 2, $routes['/wp/v2/font-collections/(?P<slug>[\/\w-]+)'], 'Rest server has not the collection path initialized.' );
 
 		$this->assertArrayHasKey( 'GET', $routes['/wp/v2/font-collections'][0]['methods'], 'Rest server has not the GET method for collections initialized.' );
 		$this->assertArrayHasKey( 'GET', $routes['/wp/v2/font-collections/(?P<slug>[\/\w-]+)'][0]['methods'], 'Rest server has not the GET method for collection initialized.' );
