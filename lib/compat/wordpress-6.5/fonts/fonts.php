f5d3cbf75045f6af80932b2578dbd09360373b69
Riad Benguella <benguella@gmail.com>
2024-02-07 14:50:41 +0100

    Font Library: Do not register the google fonts collection if already registered (#58769)
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: getdave <get_dave@git.wordpress.org>
    Co-authored-by: matiasbenedetto <mmaattiiaass@git.wordpress.org>

--- lib/compat/wordpress-6.5/fonts/fonts.php
+++ lib/compat/wordpress-6.5/fonts/fonts.php
@@ -150,9 +150,12 @@ if ( ! function_exists( 'wp_unregister_font_collection' ) ) {
 }
 
 function gutenberg_register_font_collections() {
+	if ( null !== WP_Font_Library::get_instance()->get_font_collection( 'google-fonts' ) ) {
+		return;
+	}
 	wp_register_font_collection( 'google-fonts', 'https://s.w.org/images/fonts/17.7/collections/google-fonts-with-preview.json' );
 }
-add_action( 'init', 'gutenberg_register_font_collections' );
+add_action( 'init', 'gutenberg_register_font_collections', 11 );
 
 // @core-merge: This code should probably go into Core's src/wp-includes/functions.php.
 if ( ! function_exists( 'wp_get_font_dir' ) ) {
