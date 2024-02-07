5e2233fdc6ccaaaab7771a7b7db4d710a0b79b00
Jarda Snajdr <jsnajdr@gmail.com>
2024-02-07 14:29:51 +0100

    useInputRules: track into which lazy loaded block are we currently typing

--- packages/block-editor/src/components/rich-text/use-input-rules.js
+++ packages/block-editor/src/components/rich-text/use-input-rules.js
@@ -42,83 +42,82 @@ function findSelection( blocks ) {
 	return null;
 }
 
+function findPrefixTransform( prefixText ) {
+	const prefixTransforms = getBlockTransforms( 'from' ).filter(
+		( t ) => t.type === 'prefix'
+	);
+	return findTransform( prefixTransforms, ( t ) => t.prefix === prefixText );
+}
+
+function inputRule( props, ctx, onReplace ) {
+	const { getValue, onChange, selectionChange } = props;
+
+	if ( ! ctx.inserterActive ) {
+		return;
+	}
+
+	// We must use getValue() here because value may be update
+	// asynchronously.
+	const value = getValue();
+	const { start, text } = value;
+	const characterBefore = text.slice( start - 1, start );
+
+	// The character right before the caret must be a plain space.
+	if ( characterBefore !== ' ' ) {
+		return;
+	}
+
+	const trimmedTextBefore = text.slice( 0, start ).trim();
+	const transformation = findPrefixTransform( trimmedTextBefore );
+	if ( ! transformation ) {
+		return;
+	}
+
+	// remove the prefix string from the input because it's going to be processed by the transform
+	onChange( remove( value, 0, start ) );
+
+	const block = transformation.transform( START_OF_SELECTED_AREA );
+	const selection = findSelection( [ block ] );
+	if ( selection ) {
+		ctx.inserterActive = selection[ 0 ].name === 'core/paragraph';
+	}
+	lazyLoadBlock( block ).then( () => {
+		if ( selection ) {
+			const [ selectedBlock, selectedAttribute ] = selection;
+			const valueStr = toHTMLString( { value: getValue() } );
+			// To do: refactor this to use rich text's selection instead, so
+			// we no longer have to use on this hack inserting a special
+			// character.
+			selectedBlock.attributes[ selectedAttribute ] =
+				selectedBlock.attributes[ selectedAttribute ]
+					.toString()
+					.replace( START_OF_SELECTED_AREA, valueStr );
+			selectionChange( selectedBlock.clientId, selectedAttribute, 0, 0 );
+		}
+		onReplace( [ block ] );
+	} );
+
+	return true;
+}
+
 export function useInputRules( props ) {
 	const {
 		__unstableMarkLastChangeAsPersistent,
 		__unstableMarkAutomaticChange,
 	} = useDispatch( blockEditorStore );
+
 	const propsRef = useRef( props );
 	propsRef.current = props;
+
+	const transformCtxRef = useRef( {
+		inserterActive: props.__unstableAllowPrefixTransformations,
+	} );
+
 	return useRefEffect( ( element ) => {
-		async function inputRule() {
-			const { getValue, onChange, onReplace, selectionChange } =
-				propsRef.current;
-
-			if ( ! onReplace ) {
-				return;
-			}
-
-			// We must use getValue() here because value may be update
-			// asynchronously.
-			const value = getValue();
-			const { start, text } = value;
-			const characterBefore = text.slice( start - 1, start );
-
-			// The character right before the caret must be a plain space.
-			if ( characterBefore !== ' ' ) {
-				return;
-			}
-
-			const trimmedTextBefore = text.slice( 0, start ).trim();
-			const prefixTransforms = getBlockTransforms( 'from' ).filter(
-				( { type } ) => type === 'prefix'
-			);
-			const transformation = findTransform(
-				prefixTransforms,
-				( { prefix } ) => trimmedTextBefore === prefix
-			);
-
-			if ( ! transformation ) {
-				return;
-			}
-
-			onChange( remove( value, 0, start ) );
-			const block = transformation.transform( START_OF_SELECTED_AREA );
-			await lazyLoadBlock( block );
-
-			const selection = findSelection( [ block ] );
-			if ( selection ) {
-				const [ selectedBlock, selectedAttribute ] = selection;
-				const valueNow = getValue();
-				const valueStr = toHTMLString( { value: valueNow } );
-				// To do: refactor this to use rich text's selection instead, so
-				// we no longer have to use on this hack inserting a special
-				// character.
-				selectedBlock.attributes[ selectedAttribute ] =
-					selectedBlock.attributes[ selectedAttribute ]
-						.toString()
-						.replace( START_OF_SELECTED_AREA, valueStr );
-				selectionChange(
-					selectedBlock.clientId,
-					selectedAttribute,
-					0,
-					0
-				);
-			}
-			onReplace( [ block ] );
-			__unstableMarkAutomaticChange();
-
-			return true;
-		}
-
-		async function onInput( event ) {
+		function onInput( event ) {
 			const { inputType, type } = event;
-			const {
-				getValue,
-				onChange,
-				__unstableAllowPrefixTransformations,
-				formatTypes,
-			} = propsRef.current;
+			const { getValue, onChange, onReplace, formatTypes } =
+				propsRef.current;
 
 			// Only run input rules when inserting text.
 			if ( inputType !== 'insertText' && type !== 'compositionend' ) {
@@ -126,20 +125,27 @@ export function useInputRules( props ) {
 			}
 
 			if (
-				__unstableAllowPrefixTransformations &&
-				( await inputRule() )
+				onReplace &&
+				inputRule(
+					propsRef.current,
+					transformCtxRef.current,
+					( blocks ) => {
+						onReplace( blocks );
+						__unstableMarkAutomaticChange();
+					}
+				)
 			) {
 				return;
 			}
 
 			const value = getValue();
 			const transformed = formatTypes.reduce(
-				( accumlator, { __unstableInputRule } ) => {
+				( accumulator, { __unstableInputRule } ) => {
 					if ( __unstableInputRule ) {
-						accumlator = __unstableInputRule( accumlator );
+						return __unstableInputRule( accumulator );
 					}
 
-					return accumlator;
+					return accumulator;
 				},
 				preventEventDiscovery( value )
 			);
