a8274f69a676e97e0cbe3e2e347fb0e596d05b34
Juan Aldasoro <juanfraa@gmail.com>
2024-02-08 18:46:07 +0100

    Remove `formatFontFamily` from `loadFontFaceInBrowser` to avoid problems loading fonts with several words in the font name.

--- packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
@@ -9,7 +9,6 @@ import { privateApis as componentsPrivateApis } from '@wordpress/components';
 import { FONT_WEIGHTS, FONT_STYLES } from './constants';
 import { unlock } from '../../../../lock-unlock';
 import { fetchInstallFontFace } from '../resolvers';
-import { formatFontFamily } from './preview-styles';
 
 /**
  * Browser dependencies
@@ -98,14 +97,10 @@ export async function loadFontFaceInBrowser( fontFace, source, addTo = 'all' ) {
 		return;
 	}
 
-	const newFont = new window.FontFace(
-		formatFontFamily( fontFace.fontFamily ),
-		dataSource,
-		{
-			style: fontFace.fontStyle,
-			weight: fontFace.fontWeight,
-		}
-	);
+	const newFont = new window.FontFace( fontFace.fontFamily, dataSource, {
+		style: fontFace.fontStyle,
+		weight: fontFace.fontWeight,
+	} );
 
 	const loadedFace = await newFont.load();
 
