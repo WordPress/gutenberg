214ee29451a3e4b64fabe84a78a5caa91552e8d7
Rich Tabor <hi@richtabor.com>
2024-02-08 13:59:05 -0500

    Try disabling 50% when its not possible

--- packages/editor/src/components/preview-dropdown/index.js
+++ packages/editor/src/components/preview-dropdown/index.js
@@ -10,14 +10,7 @@ import {
 	Icon,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import {
-	check,
-	desktop,
-	mobile,
-	tablet,
-	external,
-	chevronUpDown,
-} from '@wordpress/icons';
+import { check, desktop, mobile, tablet, external } from '@wordpress/icons';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import { store as preferencesStore } from '@wordpress/preferences';
@@ -86,11 +79,7 @@ export default function PreviewDropdown( { forceIsAutosaveable, disabled } ) {
 			popoverProps={ popoverProps }
 			toggleProps={ toggleProps }
 			menuProps={ menuProps }
-			icon={
-				isZoomedOutView
-					? chevronUpDown
-					: deviceIcons[ deviceType.toLowerCase() ]
-			}
+			icon={ deviceIcons[ deviceType.toLowerCase() ] }
 			label={ __( 'View' ) }
 			disableOpenOnArrowDown={ disabled }
 		>
@@ -129,7 +118,6 @@ export default function PreviewDropdown( { forceIsAutosaveable, disabled } ) {
 						<MenuGroup>
 							<MenuItem
 								onClick={ () => {
-									setDeviceType( 'Desktop' );
 									__unstableSetEditorMode( 'edit' );
 								} }
 								icon={ ! isZoomedOutView && check }
@@ -142,6 +130,7 @@ export default function PreviewDropdown( { forceIsAutosaveable, disabled } ) {
 									__unstableSetEditorMode( 'zoom-out' );
 								} }
 								icon={ isZoomedOutView && check }
+								disabled={ deviceType !== 'Desktop' }
 							>
 								{ __( 'Zoom to 50%' ) }
 							</MenuItem>
