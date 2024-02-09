92f13238aebe4702c3ab271e5540444949e25ab0
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2024-02-09 12:05:24 +1100

    Color Palette: Respect defaultPalette setting

--- packages/block-editor/src/components/color-palette/with-color-context.js
+++ packages/block-editor/src/components/color-palette/with-color-context.js
@@ -2,18 +2,42 @@
  * WordPress dependencies
  */
 import { createHigherOrderComponent } from '@wordpress/compose';
+import { useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { useSettings } from '../use-settings';
 
+const EMPTY_COLORS = [];
+
 export default createHigherOrderComponent( ( WrappedComponent ) => {
 	return ( props ) => {
-		const [ colorsFeature, enableCustomColors ] = useSettings(
-			'color.palette',
+		const [
+			customColors,
+			themeColors,
+			defaultColors,
+			defaultPaletteEnabled,
+			enableCustomColors,
+		] = useSettings(
+			'color.palette.custom',
+			'color.palette.theme',
+			'color.palette.default',
+			'color.defaultPalette',
 			'color.custom'
 		);
+
+		const colorsFeature = useMemo(
+			() => [
+				...( customColors || EMPTY_COLORS ),
+				...( themeColors || EMPTY_COLORS ),
+				...( defaultColors && defaultPaletteEnabled
+					? defaultColors
+					: EMPTY_COLORS ),
+			],
+			[ customColors, themeColors, defaultColors, defaultPaletteEnabled ]
+		);
+
 		const {
 			colors = colorsFeature,
 			disableCustomColors = ! enableCustomColors,
