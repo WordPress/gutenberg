38b539d0264c1764e306602616884c9d07943b67
Mario Santos <santosguillamot@gmail.com>
2024-02-09 20:14:43 +0100

    Remove old hook used for the editor

--- packages/block-editor/src/hooks/use-bindings-attributes.js
+++ packages/block-editor/src/hooks/use-bindings-attributes.js
@@ -112,24 +112,3 @@ addFilter(
 	'core/editor/custom-sources-backwards-compatibility/shim-attribute-source',
 	shimAttributeSource
 );
-
-// Add the context to all blocks.
-addFilter(
-	'blocks.registerBlockType',
-	'core/block-bindings-ui',
-	( settings, name ) => {
-		if ( ! ( name in BLOCK_BINDINGS_ALLOWED_BLOCKS ) ) {
-			return settings;
-		}
-		const contextItems = [ 'postId', 'postType', 'queryId' ];
-		const usesContextArray = settings.usesContext;
-		const oldUsesContextArray = new Set( usesContextArray );
-		contextItems.forEach( ( item ) => {
-			if ( ! oldUsesContextArray.has( item ) ) {
-				usesContextArray.push( item );
-			}
-		} );
-		settings.usesContext = usesContextArray;
-		return settings;
-	}
-);
