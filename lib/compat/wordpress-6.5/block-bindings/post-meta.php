4194fcd8858930f2c870e6a9ae01373bcc58da8d
Mario Santos <santosguillamot@gmail.com>
2024-02-09 20:14:44 +0100

    Remove possibility of passing post id from source

--- lib/compat/wordpress-6.5/block-bindings/post-meta.php
+++ lib/compat/wordpress-6.5/block-bindings/post-meta.php
@@ -17,13 +17,7 @@ function gutenberg_block_bindings_post_meta_callback( $source_attrs, $block_inst
 		return null;
 	}
 
-	// Use the postId attribute if available
-	if ( isset( $source_attrs['postId'] ) ) {
-		$post_id = $source_attrs['postId'];
-	} else {
-		$post_id = isset( $block_instance->context['postId'] ) ? $block_instance->context['postId'] : get_the_ID();
-	}
-
+	$post_id = isset( $block_instance->context['postId'] );
 	// If a post isn't public, we need to prevent unauthorized users from accessing the post meta.
 	$post = get_post( $post_id );
 	if ( ( ! is_post_publicly_viewable( $post ) && ! current_user_can( 'read_post', $post_id ) ) || post_password_required( $post ) ) {
