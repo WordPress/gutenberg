e29db06a2e04d7148094d0addc6056d7934e03a8
Juan Aldasoro <juanfraa@gmail.com>
2024-02-09 17:30:05 +0100

    Only wrap multi-word font families with quotes when there are several fonts. Avoid using quotes when it's only one font, so Google Chrome can load it properly.

--- packages/edit-site/src/components/global-styles/font-library-modal/utils/preview-styles.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/preview-styles.js
@@ -31,21 +31,25 @@ function extractFontWeights( fontFaces ) {
 }
 
 export function formatFontFamily( input ) {
-	return input
-		.split( ',' )
-		.map( ( font ) => {
-			font = font.trim(); // Remove any leading or trailing white spaces
-			// If the font doesn't start with quotes and contains a space, then wrap in quotes.
-			// Check that string starts with a single or double quote and not a space
-			if (
-				! ( font.startsWith( '"' ) || font.startsWith( "'" ) ) &&
-				font.indexOf( ' ' ) !== -1
-			) {
-				return `"${ font }"`;
-			}
-			return font; // Return font as is if no transformation is needed
-		} )
-		.join( ', ' );
+	return input.includes( ',' )
+		? input
+				.split( ',' )
+				.map( ( font ) => {
+					font = font.trim(); // Remove any leading or trailing white spaces
+					// If the font doesn't start with quotes and contains a space, then wrap in quotes.
+					// Check that string starts with a single or double quote and not a space
+					if (
+						! (
+							font.startsWith( '"' ) || font.startsWith( "'" )
+						) &&
+						font.indexOf( ' ' ) !== -1
+					) {
+						return `"${ font }"`;
+					}
+					return font; // Return font as is if no transformation is needed
+				} )
+				.join( ', ' )
+		: input.replace( /"/g, '' );
 }
 
 export function getFamilyPreviewStyle( family ) {
