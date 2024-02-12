4a958408035ad013bd1d6b799856fb8e23672447
Mario Santos <santosguillamot@gmail.com>
2024-02-12 16:07:47 +0100

    Remove unnecessary quotes

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -778,7 +778,7 @@ function block_core_navigation_add_directives_to_submenu( $tags, $block_attribut
 		)
 	) ) {
 		// Add directives to the parent `<li>`.
-		$tags->set_attribute( 'data-wp-interactive', '"core/navigation"' );
+		$tags->set_attribute( 'data-wp-interactive', 'core/navigation' );
 		$tags->set_attribute( 'data-wp-context', '{ "submenuOpenedBy": {}, "type": "submenu" }' );
 		$tags->set_attribute( 'data-wp-watch', 'callbacks.initMenu' );
 		$tags->set_attribute( 'data-wp-on--focusout', 'actions.handleMenuFocusout' );
