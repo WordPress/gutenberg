f61a275613caea1b2d0f41a752272f4345814095
David Arenas <david.arenas@automattic.com>
2024-02-12 15:01:33 +0100

    Use tag index as the `data-wp-key` value

--- packages/block-library/src/query-pagination-numbers/index.php
+++ packages/block-library/src/query-pagination-numbers/index.php
@@ -91,18 +91,16 @@ function render_block_core_query_pagination_numbers( $attributes, $content, $blo
 	}
 
 	if ( $enhanced_pagination ) {
-		$p      = new WP_HTML_Tag_Processor( $content );
-		$dots_i = 0;
+		$p         = new WP_HTML_Tag_Processor( $content );
+		$tag_index = 0;
 		while ( $p->next_tag(
 			array( 'class_name' => 'page-numbers' )
 		) ) {
+			if ( null === $p->get_attribute( 'data-wp-key' ) ) {
+				$p->set_attribute( 'data-wp-key', 'index-' . $tag_index++ );
+			}
 			if ( 'A' === $p->get_tag() ) {
 				$p->set_attribute( 'data-wp-on--click', 'core/query::actions.navigate' );
-				$p->set_attribute( 'data-wp-key', $p->get_attribute( 'href' ) );
-			} elseif ( $p->has_class( 'current' ) ) {
-				$p->set_attribute( 'data-wp-key', 'current' );
-			} elseif ( $p->has_class( 'dots' ) ) {
-				$p->set_attribute( 'data-wp-key', 'dots-' . $dots_i++ );
 			}
 		}
 		$content = $p->get_updated_html();
