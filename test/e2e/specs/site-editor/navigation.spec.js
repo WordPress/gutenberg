08c81eb9de78d53da4378e3f465aff154e2c56df
Jerry Jones <jones.jeremydavid@gmail.com>
2024-02-13 11:16:34 -0600

    Extract tabTo logic to util

--- test/e2e/specs/site-editor/navigation.spec.js
+++ test/e2e/specs/site-editor/navigation.spec.js
@@ -3,6 +3,12 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
+test.use( {
+	editorNavigationUtils: async ( { page, pageUtils }, use ) => {
+		await use( new EditorNavigationUtils( { page, pageUtils } ) );
+	},
+} );
+
 test.describe( 'Site editor navigation', () => {
 	test.beforeAll( async ( { requestUtils } ) => {
 		await requestUtils.activateTheme( 'emptytheme' );
@@ -14,6 +20,7 @@ test.describe( 'Site editor navigation', () => {
 
 	test( 'Can use keyboard to navigate the site editor', async ( {
 		admin,
+		editorNavigationUtils,
 		page,
 		pageUtils,
 	} ) => {
@@ -22,18 +29,7 @@ test.describe( 'Site editor navigation', () => {
 		// Test: Can navigate to a sidebar item and into its subnavigation frame without losing focus
 		// Go to the Pages button
 
-		for ( let i = 0; i < 10; i++ ) {
-			await pageUtils.pressKeys( 'Tab' );
-			const activeLabel = await page.evaluate( () => {
-				return (
-					document.activeElement.getAttribute( 'aria-label' ) ||
-					document.activeElement.textContent
-				);
-			} );
-			if ( activeLabel === 'Pages' ) {
-				break;
-			}
-		}
+		await editorNavigationUtils.tabToLabel( 'Pages', { times: 10 } );
 
 		await expect(
 			page.getByRole( 'button', { name: 'Pages' } )
@@ -50,15 +46,7 @@ test.describe( 'Site editor navigation', () => {
 		).toBeFocused();
 
 		// Test: Can navigate into the iframe using the keyboard
-		for ( let i = 0; i < 10; i++ ) {
-			await pageUtils.pressKeys( 'Tab' );
-			const activeNode = await page.evaluate( () => {
-				return document.activeElement.nodeName;
-			} );
-			if ( activeNode === 'IFRAME' ) {
-				break;
-			}
-		}
+		await editorNavigationUtils.tabToNode( 'IFRAME', { times: 10 } );
 		// Getting the actual iframe as a cleaner locator was suprisingly tricky,
 		// so we're using a css selector with .is-focused which should be present when the iframe has focus.
 		await expect(
@@ -104,3 +92,39 @@ test.describe( 'Site editor navigation', () => {
 		).toBeFocused();
 	} );
 } );
+
+class EditorNavigationUtils {
+	constructor( { page, pageUtils } ) {
+		this.page = page;
+		this.pageUtils = pageUtils;
+	}
+
+	async tabToLabel( label, { times = 10 } ) {
+		for ( let i = 0; i < times; i++ ) {
+			await this.pageUtils.pressKeys( 'Tab' );
+			const activeLabel = await this.page.evaluate( () => {
+				return (
+					document.activeElement.getAttribute( 'aria-label' ) ||
+					document.activeElement.textContent
+				);
+			} );
+			if ( activeLabel === label ) {
+				return activeLabel;
+			}
+		}
+		return null;
+	}
+
+	async tabToNode( nodeName, { times = 10 } ) {
+		for ( let i = 0; i < times; i++ ) {
+			await this.pageUtils.pressKeys( 'Tab' );
+			const activeNode = await this.page.evaluate( () => {
+				return document.activeElement.nodeName;
+			} );
+			if ( activeNode === nodeName ) {
+				break;
+			}
+		}
+		return null;
+	}
+}
