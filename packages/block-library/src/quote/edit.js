c7fc476cefa9b7ea41fcdcf4cfdd94fec0f1a1ea
David Calhoun <github@davidcalhoun.me>
2024-02-15 11:46:59 -0500

    fix: Avoid passing incompatible styles to native components
    The `display: block` style attribute is not supported by React Native
    and results in an error.

--- packages/block-library/src/quote/edit.js
+++ packages/block-library/src/quote/edit.js
@@ -105,7 +105,7 @@ export default function QuoteEdit( {
 				<Caption
 					attributeKey="citation"
 					tagName={ isWebPlatform ? 'cite' : undefined }
-					style={ { display: 'block' } }
+					style={ isWebPlatform && { display: 'block' } }
 					isSelected={ isSelected }
 					attributes={ attributes }
 					setAttributes={ setAttributes }
