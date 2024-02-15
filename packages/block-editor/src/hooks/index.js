7268a558150ada40d4359c1db8cc2b4e66bb479d
Damián Suárez <rdsuarez@gmail.com>
2024-02-15 11:12:56 -0300

    move changes to the use-binding-attributes file

--- packages/block-editor/src/hooks/index.js
+++ packages/block-editor/src/hooks/index.js
@@ -28,8 +28,7 @@ import contentLockUI from './content-lock-ui';
 import './metadata';
 import blockHooks from './block-hooks';
 import blockRenaming from './block-renaming';
-import './block-binding-support';
-// import './use-bindings-attributes';
+import './use-bindings-attributes';
 
 createBlockEditFilter(
 	[
