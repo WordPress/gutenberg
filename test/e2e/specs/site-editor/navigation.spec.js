e9a28610df49bd71ded1db60846376ea53d64d95
Jerry Jones <jones.jeremydavid@gmail.com>
2024-02-15 08:43:20 -0600

    Use times param directly instead of as object
    Co-authored-by: Aki Hamano <54422211+t-hamano@users.noreply.github.com>

--- test/e2e/specs/site-editor/navigation.spec.js
+++ test/e2e/specs/site-editor/navigation.spec.js
@@ -104,7 +104,7 @@ class EditorNavigationUtils {
 		this.pageUtils = pageUtils;
 	}
 
-	async tabToLabel( label, { times = 10 } ) {
+	async tabToLabel( label, times = 10 ) {
 		for ( let i = 0; i < times; i++ ) {
 			await this.pageUtils.pressKeys( 'Tab' );
 			const activeLabel = await this.page.evaluate( () => {
