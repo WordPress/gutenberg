5a9a7a1325d7cdbbe0099b1e904d93af285d52ba
Jorge Costa <jorge.costa@developer.pt>
2024-02-16 11:22:57 +0000

    Fix priority issue

--- packages/edit-site/src/components/page-patterns/dataviews-pattern-actions.js
+++ packages/edit-site/src/components/page-patterns/dataviews-pattern-actions.js
@@ -75,7 +75,9 @@ export const exportJSONaction = {
 			return {
 				name: `${
 					name +
-					( nameCount[ name ] > 1 ? '-' + nameCount[ name ] - 1 : '' )
+					( nameCount[ name ] > 1
+						? '-' + ( nameCount[ name ] - 1 )
+						: '' )
 				}.json`,
 				lastModified: new Date(),
 				input: getJsonFromItem( item ),
