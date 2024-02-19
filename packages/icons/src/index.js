cece812d3a56da726bd5b252384bba325dca54a9
Michal Czaplinski <mmczaplinski@gmail.com>
2024-02-19 18:06:39 +0000

    Add connection icon to BlockControls toolbar button

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -59,6 +59,7 @@ export { default as commentAuthorName } from './library/comment-author-name';
 export { default as commentContent } from './library/comment-content';
 export { default as commentReplyLink } from './library/comment-reply-link';
 export { default as commentEditLink } from './library/comment-edit-link';
+export { default as connection } from './library/connection';
 export { default as cover } from './library/cover';
 export { default as create } from './library/create';
 export { default as crop } from './library/crop';
