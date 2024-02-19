aa293441a3f9a080efdbaca4f4d720221a3ae348
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-02-19 14:47:18 +0900

    Fix typo

--- packages/block-library/src/post-author-name/index.php
+++ packages/block-library/src/post-author-name/index.php
@@ -14,7 +14,7 @@
  * @return string Returns the rendered post author name block.
  */
 function render_block_core_post_author_name( $attributes, $content, $block ) {
-	if ( ! isset( $block->context['postId'] ) ) {
+	if ( isset( $block->context['postId'] ) ) {
 		$author_id = get_post_field( 'post_author', $block->context['postId'] );
 	} else {
 		$author_id = get_query_var( 'author' );
