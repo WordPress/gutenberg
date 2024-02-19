41c7203f632ee581d9cfa63a16c392c9f1adff86
Jason Crist <jcrist@pbking.com>
2024-02-19 08:59:39 -0500

    Refactored 'installed-fonts' component to use Navigator Components

--- packages/edit-site/src/components/global-styles/font-library-modal/installed-fonts.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/installed-fonts.js
@@ -1,32 +1,34 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { useContext, useEffect, useState } from '@wordpress/element';
 import {
-	privateApis as componentsPrivateApis,
+	Button,
+	__experimentalConfirmDialog as ConfirmDialog,
 	__experimentalHStack as HStack,
-	__experimentalSpacer as Spacer,
-	__experimentalText as Text,
 	__experimentalNavigatorProvider as NavigatorProvider,
 	__experimentalNavigatorScreen as NavigatorScreen,
 	__experimentalNavigatorToParentButton as NavigatorToParentButton,
-	__experimentalHeading as Heading,
-	Button,
+	__experimentalUseNavigator as useNavigator,
+	Notice,
+	__experimentalSpacer as Spacer,
 	Spinner,
-	FlexItem,
+	__experimentalText as Text,
+	__experimentalVStack as VStack,
+	privateApis as componentsPrivateApis,
 } from '@wordpress/components';
+import { useContext, useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import { chevronLeft } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
-import TabPanelLayout from './tab-panel-layout';
-import { FontLibraryContext } from './context';
-import LibraryFontDetails from './library-font-details';
-import LibraryFontCard from './library-font-card';
-import ConfirmDeleteDialog from './confirm-delete-dialog';
 import { unlock } from '../../../lock-unlock';
+import { FontLibraryContext } from './context';
+import FontFaceDemo from './font-demo';
+import LibraryFontCard from './library-font-card';
+import LibraryFontVariant from './library-font-variant';
+import { sortFontFaces } from './utils/sort-font-faces';
 const { ProgressBar } = unlock( componentsPrivateApis );
 
 function InstalledFonts() {
@@ -38,24 +40,195 @@ function InstalledFonts() {
 		refreshLibrary,
 		uninstallFontFamily,
 		isResolvingLibrary,
+		isInstalling,
+		saveFontFamilies,
+		fontFamiliesHasChanges,
 		notice,
 		setNotice,
 	} = useContext( FontLibraryContext );
 	const [ isConfirmDeleteOpen, setIsConfirmDeleteOpen ] = useState( false );
 
+	const shouldDisplayDeleteButton =
+		!! libraryFontSelected && libraryFontSelected?.source !== 'theme';
+
+	const handleUninstallClick = () => {
+		setIsConfirmDeleteOpen( true );
+	};
+
+	const getFontFacesToDisplay = ( font ) => {
+		if ( ! font ) {
+			return [];
+		}
+		if ( ! font.fontFace || ! font.fontFace.length ) {
+			return [
+				{
+					fontFamily: font.fontFamily,
+					fontStyle: 'normal',
+					fontWeight: '400',
+				},
+			 ]
+		}
+		return sortFontFaces ( font.fontFace );
+	}
+
+
+	useEffect( () => {
+		handleSetLibraryFontSelected( libraryFontSelected );
+		refreshLibrary();
+	}, [] );
+
+	return (
+		<div className="font-library-modal__tabpanel-layout">
+
+			{ isResolvingLibrary && (
+				<HStack align="center">
+					<Spacer/>
+					<Spinner />
+					<Spacer/>
+				</HStack>
+			) }
+
+			{ notice && (
+				<>
+					<Spacer margin={ 1 } />
+					<Notice
+						status={ notice.type }
+						onRemove={ () => setNotice( null ) }
+					>
+						{ notice.message }
+					</Notice>
+					<Spacer margin={ 1 } />
+				</>
+			) }
+
+			<NavigatorProvider initialPath="/">
+				<NavigatorScreen path="/">
+					{ baseCustomFonts.length > 0 && ( <>
+						<Text className="font-library-modal__subtitle">
+							{ __( 'Installed Fonts' ) }
+						</Text>
+						<Spacer margin={ 2 } />
+						{ baseCustomFonts.map( ( font ) => (
+							<LibraryFontCard
+								font={ font }
+								key={ font.slug }
+								onClick={ () => {
+									handleSetLibraryFontSelected( font );
+								} }
+							/>
+						) ) }
+						<Spacer margin={ 8 } />
+					</> ) }
+
+					{ baseThemeFonts.length > 0 && ( <>
+						<Text className="font-library-modal__subtitle">
+							{ __( 'Theme Fonts' ) }
+						</Text>
+						<Spacer margin={ 2 } />
+						{ baseThemeFonts.map( ( font ) => (
+							<LibraryFontCard
+								font={ font }
+								key={ font.slug }
+								onClick={ () => {
+									handleSetLibraryFontSelected( font );
+								} }
+							/>
+						) ) }
+					</> ) }
+					<Spacer margin={ 16 } />
+				</NavigatorScreen>
+
+				<NavigatorScreen path="/fontFamily">
+
+					<ConfirmDeleteDialog
+						font={ libraryFontSelected }
+						isOpen={ isConfirmDeleteOpen }
+						setIsOpen={ setIsConfirmDeleteOpen }
+						setNotice={ setNotice }
+						uninstallFontFamily={ uninstallFontFamily }
+						handleSetLibraryFontSelected={ handleSetLibraryFontSelected }
+					/>
+
+					<HStack spacing={ 2 } aligh="left">
+						<NavigatorToParentButton
+							icon={ chevronLeft }
+							isSmall
+							onClick={ () => {
+								handleSetLibraryFontSelected( null );
+							} }
+							aria-label={ __( 'Navigate to the previous view' ) }
+						/>
+						<FontFaceDemo font={ libraryFontSelected } />
+					</HStack>
+					<Spacer margin={ 4 } />
+					<Text>
+						{ __(
+							'Choose font variants. Keep in mind that too many variants could make your site slower.'
+						) }
+					</Text>
+					<Spacer margin={ 4 } />
+					<VStack spacing={ 0 }>
+						<Spacer margin={ 8 } />
+						{ getFontFacesToDisplay( libraryFontSelected ).map( ( face, i ) => (
+						<LibraryFontVariant
+							font={ libraryFontSelected }
+							face={ face }
+							key={ `face${ i }` }
+						/>
+						) ) }
+					</VStack>
+
+				</NavigatorScreen>
+			</NavigatorProvider>
+
+			<HStack justify="flex-end" className="font-library-modal__tabpanel-layout__footer">
+				{ isInstalling && <ProgressBar /> }
+				{ shouldDisplayDeleteButton && (
+				<Button
+					isDestructive
+					variant="tertiary"
+					onClick={ handleUninstallClick }
+				>
+					{ __( 'Delete' ) }
+				</Button>
+				) }
+				<Button
+					variant="primary"
+					onClick={ saveFontFamilies }
+					disabled={ ! fontFamiliesHasChanges }
+					__experimentalIsFocusable
+				>
+					{ __( 'Update' ) }
+				</Button>
+			</HStack>
+		</div>
+	);
+}
+
+function ConfirmDeleteDialog( {
+	font,
+	isOpen,
+	setIsOpen,
+	setNotice,
+	uninstallFontFamily,
+	handleSetLibraryFontSelected
+} ) {
+
+	const navigator = useNavigator();
+
 	const handleConfirmUninstall = async () => {
 		setNotice( null );
 
 		try {
-			await uninstallFontFamily( libraryFontSelected );
+			await uninstallFontFamily( font );
 			setNotice( {
 				type: 'success',
 				message: __( 'Font family uninstalled successfully.' ),
 			} );
 
-			// If the font was succesfully uninstalled it is unselected.
+			navigator.goBack();
 			handleSetLibraryFontSelected( null );
-			setIsConfirmDeleteOpen( false );
+			setIsOpen( false );
 		} catch ( error ) {
 			setNotice( {
 				type: 'error',
@@ -66,149 +239,27 @@ function InstalledFonts() {
 		}
 	};
 
-	const handleUninstallClick = async () => {
-		setIsConfirmDeleteOpen( true );
-	};
-
 	const handleCancelUninstall = () => {
-		setIsConfirmDeleteOpen( false );
+		setIsOpen( false );
 	};
 
-	const shouldDisplayDeleteButton =
-		!! libraryFontSelected && libraryFontSelected?.source !== 'theme';
-
-	useEffect( () => {
-		handleSetLibraryFontSelected( libraryFontSelected );
-		refreshLibrary();
-	}, [] );
-
 	return (
-		<TabPanelLayout
-			notice={ notice }
-			footer={
-				<Footer
-					shouldDisplayDeleteButton={ shouldDisplayDeleteButton }
-					handleUninstallClick={ handleUninstallClick }
-				/>
-			}
+		<ConfirmDialog
+			isOpen={ isOpen }
+			cancelButtonText={ __( 'Cancel' ) }
+			confirmButtonText={ __( 'Delete' ) }
+			onCancel={ handleCancelUninstall }
+			onConfirm={ handleConfirmUninstall }
 		>
-			<ConfirmDeleteDialog
-				font={ libraryFontSelected }
-				isConfirmDeleteOpen={ isConfirmDeleteOpen }
-				handleConfirmUninstall={ handleConfirmUninstall }
-				handleCancelUninstall={ handleCancelUninstall }
-			/>
-
-			{ isResolvingLibrary && (
-				<FlexItem>
-					<Spacer margin={ 2 } />
-					<Spinner />
-					<Spacer margin={ 2 } />
-				</FlexItem>
-			) }
-
-			<NavigatorProvider initialPath="/">
-				<NavigatorScreen path="/">
-					{ baseCustomFonts.length > 0 && (
-						<>
-							<Text className="font-library-modal__subtitle">
-								{ __( 'Installed Fonts' ) }
-							</Text>
-
-							<Spacer margin={ 2 } />
-
-							{ baseCustomFonts.map( ( font ) => (
-								<LibraryFontCard
-									font={ font }
-									key={ font.slug }
-									onClick={ () => {
-										handleSetLibraryFontSelected( font );
-									} }
-								/>
-							) ) }
-							<Spacer margin={ 8 } />
-						</>
-					) }
-
-					{ baseThemeFonts.length > 0 && (
-						<>
-							<Text className="font-library-modal__subtitle">
-								{ __( 'Theme Fonts' ) }
-							</Text>
-
-							<Spacer margin={ 2 } />
-
-							{ baseThemeFonts.map( ( font ) => (
-								<LibraryFontCard
-									font={ font }
-									key={ font.slug }
-									onClick={ () => {
-										handleSetLibraryFontSelected( font );
-									} }
-								/>
-							) ) }
-						</>
-					) }
-					<Spacer margin={ 16 } />
-				</NavigatorScreen>
-
-				<NavigatorScreen path="/fontFamily">
-					<HStack spacing={ 2 } aligh="left">
-						<NavigatorToParentButton
-							icon={ chevronLeft }
-							isSmall
-							aria-label={ __( 'Navigate to the previous view' ) }
-						/>
-
-						<Heading level={ 2 } size={ 13 }>
-							{ libraryFontSelected?.name }
-						</Heading>
-					</HStack>
-					<Spacer margin={ 4 } />
-					<Text>
-						{ __(
-							'Choose font variants. Keep in mind that too many variants could make your site slower.'
-						) }
-					</Text>
-
-					<LibraryFontDetails
-						font={ libraryFontSelected }
-						isConfirmDeleteOpen={ isConfirmDeleteOpen }
-						handleConfirmUninstall={ handleConfirmUninstall }
-						handleCancelUninstall={ handleCancelUninstall }
-					/>
-				</NavigatorScreen>
-			</NavigatorProvider>
-		</TabPanelLayout>
-	);
-}
-
-function Footer( { shouldDisplayDeleteButton, handleUninstallClick } ) {
-	const { saveFontFamilies, fontFamiliesHasChanges, isInstalling } =
-		useContext( FontLibraryContext );
-	return (
-		<HStack justify="flex-end">
-			{ isInstalling && <ProgressBar /> }
-			<div>
-				{ shouldDisplayDeleteButton && (
-					<Button
-						isDestructive
-						variant="tertiary"
-						onClick={ handleUninstallClick }
-					>
-						{ __( 'Delete' ) }
-					</Button>
+			{ font &&
+				sprintf(
+					/* translators: %s: Name of the font. */
+					__(
+						'Are you sure you want to delete "%s" font and all its variants and assets?'
+					),
+					font.name
 				) }
-			</div>
-			<Button
-				variant="primary"
-				onClick={ saveFontFamilies }
-				disabled={ ! fontFamiliesHasChanges }
-				__experimentalIsFocusable
-			>
-				{ __( 'Update' ) }
-			</Button>
-		</HStack>
+		</ConfirmDialog>
 	);
 }
 
