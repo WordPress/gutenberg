21815917bbdd809eebe058a593f682fc0327c154
Nik Tsekouras <ntsekouras@outlook.com>
2024-02-19 11:15:58 +0200

    Revert footer in pages list with DataViews (#59151)
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>

--- packages/edit-site/src/components/sidebar/index.js
+++ packages/edit-site/src/components/sidebar/index.js
@@ -14,6 +14,7 @@ import {
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 import { __ } from '@wordpress/i18n';
 import { useViewportMatch } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
