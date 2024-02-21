e47211842b25917344d665c87ee1a530d49b2970
scruffian <ben@scruffian.com>
2024-02-21 17:47:39 +0100

    Refactor showBlocks to isZoomOutMode

--- packages/block-editor/src/components/inserter/menu.js
+++ packages/block-editor/src/components/inserter/menu.js
@@ -66,13 +66,13 @@ function InserterMenu(
 			insertionIndex: __experimentalInsertionIndex,
 			shouldFocusBlock,
 		} );
-	const { showBlocks, showPatterns } = useSelect(
+	const { isZoomOutMode, showPatterns } = useSelect(
 		( select ) => {
 			const { hasAllowedPatterns, __unstableGetEditorMode } = unlock(
 				select( blockEditorStore )
 			);
 			return {
-				showBlocks: __unstableGetEditorMode() !== 'zoom-out',
+				isZoomOutMode: __unstableGetEditorMode() === 'zoom-out',
 				showPatterns: hasAllowedPatterns( destinationRootClientId ),
 			};
 		},
@@ -80,7 +80,7 @@ function InserterMenu(
 	);
 
 	const mediaCategories = useMediaCategories( destinationRootClientId );
-	const showMedia = mediaCategories.length > 0 && showBlocks;
+	const showMedia = mediaCategories.length > 0 && ! isZoomOutMode;
 
 	const onInsert = useCallback(
 		( blocks, meta, shouldForceFocusBlock ) => {
@@ -252,14 +252,14 @@ function InserterMenu(
 								__experimentalInsertionIndex
 							}
 							showBlockDirectory
-							showBlocks={ showBlocks }
+							showBlocks={ ! isZoomOutMode }
 							shouldFocusBlock={ shouldFocusBlock }
 						/>
 					</div>
 				) }
 				{ showAsTabs && (
 					<InserterTabs
-						showBlocks={ showBlocks }
+						showBlocks={ ! isZoomOutMode }
 						showPatterns={ showPatterns }
 						showMedia={ showMedia }
 						onSelect={ handleSetSelectedTab }
