082c6b13847a93d513e98adfdedc22bb3c989739
Sarah Norris <sarah@sekai.co.uk>
2024-02-23 13:48:51 +0000

    Fix PHP formatting

--- packages/e2e-tests/plugins/font-library-permissions.php
+++ packages/e2e-tests/plugins/font-library-permissions.php
@@ -10,10 +10,15 @@
 /**
  * Add custom permissions to disallow creating and deleting font families.
  */
-add_filter( 'register_post_type_args', function( $args, $post_type ) {
-	if ( $post_type === 'wp_font_family' ) {
-		$args['capabilities']['create_posts'] = 'do_not_allow';
-		$args['capabilities']['delete_published_posts'] = 'do_not_allow';
-	}
-	return $args;
-}, 10, 2 );
+add_filter(
+	'register_post_type_args',
+	function ( $args, $post_type ) {
+		if ( $post_type === 'wp_font_family' ) {
+			$args['capabilities']['create_posts']           = 'do_not_allow';
+			$args['capabilities']['delete_published_posts'] = 'do_not_allow';
+		}
+		return $args;
+	},
+	10,
+	2
+);
