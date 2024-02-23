9867c3e83d87bce7b77946b0bbc91163c85f80d6
Grant Kinney <hi@grant.mk>
2024-02-23 15:37:59 -0600

    Moves font REST API registration to correct hook

--- lib/compat/wordpress-6.5/fonts/fonts.php
+++ lib/compat/wordpress-6.5/fonts/fonts.php
@@ -200,18 +200,21 @@ add_filter( 'user_has_cap', 'gutenberg_maybe_grant_upload_font_cap', 10, 2 );
 
 /**
  * Initializes REST routes.
- *
- * @since 6.5
  */
 function gutenberg_create_initial_rest_routes() {
-	$font_collections_controller = new WP_REST_Font_Collections_Controller();
-	$font_collections_controller->register_routes();
+	global $wp_version;
+
+	// Runs only if the Font Library is not available in core ( i.e. in core < 6.5-alpha ).
+	if ( version_compare( $wp_version, '6.5-alpha', '<' ) ) {
+		$font_collections_controller = new WP_REST_Font_Collections_Controller();
+		$font_collections_controller->register_routes();
+	}
 }
+
+add_action( 'rest_api_init', 'gutenberg_create_initial_rest_routes' );
 
 /**
  * Initializes REST routes and post types.
- *
- * @since 6.5
  */
 function gutenberg_init_font_library() {
 	global $wp_version;
@@ -219,7 +222,6 @@ function gutenberg_init_font_library() {
 	// Runs only if the Font Library is not available in core ( i.e. in core < 6.5-alpha ).
 	if ( version_compare( $wp_version, '6.5-alpha', '<' ) ) {
 		gutenberg_create_initial_post_types();
-		gutenberg_create_initial_rest_routes();
 	}
 }
 
