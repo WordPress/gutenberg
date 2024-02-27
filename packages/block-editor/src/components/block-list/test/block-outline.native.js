d5ec513aa1e621db98484b708fd94038dc368f01
David Calhoun <github@davidcalhoun.me>
2024-02-27 12:44:24 -0500

    test: Simplify test cases covering conditional logic
    Deeply nested tests are difficult to comprehend and the selected state
    is fairly top-level, so testing each block by its selection feels like
    unnecessary complexity.

--- packages/block-editor/src/components/block-list/test/block-outline.native.js
+++ packages/block-editor/src/components/block-list/test/block-outline.native.js
@@ -9,243 +9,104 @@ import { render, screen } from 'test/helpers';
 import BlockOutline from '../block-outline';
 
 describe( 'BlockOutline', () => {
-	describe( 'when the block is a non-rich text design block', () => {
-		describe( 'when selected', () => {
+	describe( 'containing an unselected block', () => {
+		it( 'should not render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected={ false }
+					blockCategory="design"
+					name="core/group"
+				/>
+			);
+
+			expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
+		} );
+	} );
+
+	describe( 'containing a block with inner blocks', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="design"
+					name="core/group"
+					hasInnerBlocks
+				/>
+			);
+
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
+		} );
+	} );
+
+	describe( 'containing a design category block', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="design"
+					name="core/group"
+				/>
+			);
+
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
+		} );
+	} );
+
+	describe( 'containing a text category block', () => {
+		it( 'should not render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="text"
+					name="core/paragraph"
+				/>
+			);
+
+			expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
+		} );
+
+		describe( 'with inner blocks', () => {
 			it( 'should render an outline', async () => {
 				render(
 					<BlockOutline
 						isSelected
-						blockCategory="design"
-						name="core/group"
+						blockCategory="text"
+						name="core/list"
+						hasInnerBlocks
 					/>
 				);
 
 				expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
 			} );
 		} );
+	} );
 
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="design"
-						name="core/group"
-					/>
-				);
+	describe( 'containing a non-text category block in the root list', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					isRootList
+					blockCategory="widgets"
+					name="core/latest-posts"
+				/>
+			);
 
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
 		} );
 	} );
 
-	describe( 'when the block is a rich text design block', () => {
-		describe( 'when selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="design"
-						name="core/button"
-					/>
-				);
+	describe( 'containing a spacer block', () => {
+		it( 'should not render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="design"
+					name="core/spacer"
+				/>
+			);
 
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="design"
-						name="core/button"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a text block', () => {
-		describe( 'when selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="text"
-						name="core/paragraph"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="text"
-						name="core/paragraph"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a social link block', () => {
-		describe( 'when selected', () => {
-			it( 'should render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="embed"
-						name="core/social-link"
-					/>
-				);
-
-				expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="embed"
-						name="core/social-link"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a media block', () => {
-		describe( 'when selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="media"
-						name="core/image"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="media"
-						name="core/image"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a freeform block', () => {
-		describe( 'when selected', () => {
-			it( 'should render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="text"
-						name="core/freeform"
-					/>
-				);
-
-				expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="text"
-						name="core/freeform"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a missing block', () => {
-		describe( 'when selected', () => {
-			it( 'should render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="text"
-						name="core/missing"
-					/>
-				);
-
-				expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="text"
-						name="core/missing"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-	} );
-
-	describe( 'when the block is a spacer block', () => {
-		describe( 'when selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected
-						blockCategory="design"
-						name="core/spacer"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						blockCategory="design"
-						name="core/spacer"
-					/>
-				);
-
-				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
-			} );
+			expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
 		} );
 
 		describe( 'when in the root list', () => {
@@ -264,35 +125,88 @@ describe( 'BlockOutline', () => {
 		} );
 	} );
 
-	describe( 'when the block is a non-text block in the root list', () => {
-		describe( 'when selected', () => {
-			it( 'should render an outline', async () => {
+	describe( 'containing a button block', () => {
+		it( 'should not render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="design"
+					name="core/button"
+				/>
+			);
+
+			expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
+		} );
+
+		describe( 'when in the root list', () => {
+			it( 'should not render an outline', async () => {
 				render(
 					<BlockOutline
 						isSelected
 						isRootList
-						blockCategory="widgets"
-						name="core/latest-posts"
-					/>
-				);
-
-				expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
-			} );
-		} );
-
-		describe( 'when not selected', () => {
-			it( 'should not render an outline', async () => {
-				render(
-					<BlockOutline
-						isSelected={ false }
-						isRootList
-						blockCategory="widgets"
-						name="core/latest-posts"
+						blockCategory="design"
+						name="core/button"
 					/>
 				);
 
 				expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
 			} );
+		} );
+	} );
+
+	describe( 'containing a social link block', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="embed"
+					name="core/social-link"
+				/>
+			);
+
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
+		} );
+	} );
+
+	describe( 'containing a media block', () => {
+		it( 'should not render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="media"
+					name="core/image"
+				/>
+			);
+
+			expect( screen.queryByTestId( 'block-outline' ) ).toBeNull();
+		} );
+	} );
+
+	describe( 'containing a freeform block', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="text"
+					name="core/freeform"
+				/>
+			);
+
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
+		} );
+	} );
+
+	describe( 'containing a missing block', () => {
+		it( 'should render an outline', async () => {
+			render(
+				<BlockOutline
+					isSelected
+					blockCategory="text"
+					name="core/missing"
+				/>
+			);
+
+			expect( screen.getByTestId( 'block-outline' ) ).toBeVisible();
 		} );
 	} );
 } );
