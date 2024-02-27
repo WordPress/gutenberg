260cb3afd5b426fb958f4f7645f4fbe81b50f079
Carlos Garcia <fluiddot@gmail.com>
2024-02-27 18:40:21 +0100

    Implement copy buttons in Error boundary component

--- packages/editor/src/components/error-boundary/index.native.js
+++ packages/editor/src/components/error-boundary/index.native.js
@@ -1,18 +1,24 @@
 /**
+ * External dependencies
+ */
+import { Text, TouchableOpacity, View } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+/**
  * WordPress dependencies
  */
 import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
 import { select } from '@wordpress/data';
 import { Warning } from '@wordpress/block-editor';
-import { useCopyToClipboard } from '@wordpress/compose';
 import { logException } from '@wordpress/react-native-bridge';
+import { usePreferredColorSchemeStyle } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
 import { store as editorStore } from '../../store';
+import styles from './style';
 
 function getContent() {
 	try {
@@ -26,12 +32,31 @@ function getContent() {
 	} catch ( error ) {}
 }
 
-function CopyButton( { text, children } ) {
-	const ref = useCopyToClipboard( text );
+function CopyButton( { text, label, accessibilityLabel, accessibilityHint } ) {
+	const containerStyle = usePreferredColorSchemeStyle(
+		styles[ 'copy-button__container' ],
+		styles[ 'copy-button__container--dark' ]
+	);
+	const textStyle = usePreferredColorSchemeStyle(
+		styles[ 'copy-button__text' ],
+		styles[ 'copy-button__text--dark' ]
+	);
+
 	return (
-		<Button variant="secondary" ref={ ref }>
-			{ children }
-		</Button>
+		<TouchableOpacity
+			activeOpacity={ 0.5 }
+			accessibilityLabel={ accessibilityLabel }
+			style={ containerStyle }
+			accessibilityRole={ 'button' }
+			accessibilityHint={ accessibilityHint }
+			onPress={ () => {
+				Clipboard.setString(
+					typeof text === 'function' ? text() : text || ''
+				);
+			} }
+		>
+			<Text style={ textStyle }>{ label }</Text>
+		</TouchableOpacity>
 	);
 }
 
@@ -64,14 +89,22 @@ class ErrorBoundary extends Component {
 			return this.props.children;
 		}
 
-		const actions = [
-			<CopyButton key="copy-post" text={ getContent }>
-				{ __( 'Copy Post Text' ) }
-			</CopyButton>,
-			<CopyButton key="copy-error" text={ error.stack }>
-				{ __( 'Copy Error' ) }
-			</CopyButton>,
-		];
+		const actions = (
+			<View style={ styles[ 'error-boundary__actions-container' ] }>
+				<CopyButton
+					label={ __( 'Copy Post Text' ) }
+					accessibilityLabel={ __( 'Button to copy post text' ) }
+					accessibilityHint={ __( 'Tap here to copy post text' ) }
+					text={ getContent }
+				/>
+				<CopyButton
+					label={ __( 'Copy Error' ) }
+					accessibilityLabel={ __( 'Button to copy error' ) }
+					accessibilityHint={ __( 'Tap here to copy error' ) }
+					text={ error.stack }
+				/>
+			</View>
+		);
 
 		return (
 			<Warning
@@ -79,6 +112,8 @@ class ErrorBoundary extends Component {
 				message={ __(
 					'The editor has encountered an unexpected error.'
 				) }
+				containerStyle={ styles[ 'error-boundary__container' ] }
+				messageStyle={ styles[ 'error-boundary__message' ] }
 			/>
 		);
 	}
