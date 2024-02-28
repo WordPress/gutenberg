c813923d7d5f18175271f7a1e4c76c15cec15e9d
Sarah Norris <sarah@sekai.co.uk>
2024-02-28 15:05:41 +0000

    Reduce tests to just display logic for Upload tab

--- test/e2e/specs/site-editor/font-library.spec.js
+++ test/e2e/specs/site-editor/font-library.spec.js
@@ -88,8 +88,6 @@ test.describe( 'Font Library', () => {
 	} );
 
 	test.describe( 'When user has permissions to edit font families', () => {
-		// Timeouts are increased in the following tests to account for the time it takes to load the font library tabs.
-
 		test.beforeAll( async ( { requestUtils } ) => {
 			await requestUtils.activateTheme( 'twentytwentythree' );
 		} );
@@ -106,79 +104,27 @@ test.describe( 'Font Library', () => {
 					name: 'Manage Fonts',
 				} )
 				.click();
+
+			// We need to wait the response from the `wp_font_family` request in order to test the Upload tab.
+			await page.waitForResponse(
+				( resp ) =>
+					resp
+						.url()
+						.includes(
+							'/index.php?rest_route=%2Fwp%2Fv2%2Ffont-families'
+						) && resp.status() === 200,
+				{ timeout: 50000 }
+			);
 		} );
 
-		test( 'should display the "Upload" tab and upload a local font', async ( {
-			page,
-			request,
-		} ) => {
+		test( 'should display the "Upload" tab', async ( { page } ) => {
 			await expect(
 				page.getByRole( 'tab', { name: 'Upload' } )
-			).toBeVisible( { timeout: 40000 } );
-
-			// Ensure font does not exist before uploading
-			const installedFonts = await request.get(
-				`/wp-json/wp/v2/font-families`
-			);
-			console.log( installedFonts );
-			const commissionerFont = await request.get(
-				`/wp-json/wp/v2/font-families?slug=commissioner&_embed=true`
-			);
-			if ( commissionerFont.length > 0 ) {
-				await request.delete(
-					`/wp-json/wp/v2/font-families/${ commissionerFont[ 0 ].id }?force=true`
-				);
-			}
-
-			// Upload a local font
-			await page.getByRole( 'tab', { name: 'Upload' } ).click();
-			const fileChooserPromise = page.waitForEvent( 'filechooser' );
-			await page.getByRole( 'button', { name: 'Upload Font' } ).click();
-			const fileChooser = await fileChooserPromise;
-			await fileChooser.setFiles(
-				'./test/e2e/assets/Commissioner-Regular.ttf'
-			);
-
-			// Check font was installed successfully
-			await expect(
-				page
-					.getByLabel( 'Upload' )
-					.getByText( 'Fonts were installed successfully.' )
-			).toBeVisible( { timeout: 40000 } );
-			await page.getByRole( 'tab', { name: 'Library' } ).click();
-			await expect(
-				page.getByRole( 'button', { name: 'Commissioner' } )
 			).toBeVisible();
-		} );
-
-		test( 'should display the default collections tab', async ( {
-			page,
-		} ) => {
-			await expect(
-				page.getByRole( 'tab', { name: 'Install Fonts' } )
-			).toBeVisible( { timeout: 40000 } );
-		} );
-
-		test( 'should display the "Delete" button and delete the font', async ( {
-			page,
-		} ) => {
-			await page
-				.getByRole( 'button', { name: 'Commissioner' } )
-				.click( { timeout: 40000 } );
-
-			await expect(
-				page.getByRole( 'button', { name: 'Delete' } )
-			).toBeVisible( { timeout: 40000 } );
-
-			// Delete the font
-			await page.getByRole( 'button', { name: 'Delete' } ).click();
-			await page.getByRole( 'button', { name: 'Delete' } ).click();
 		} );
 	} );
 
 	test.describe( 'When user does not have permission to edit font families', () => {
-		// Timeouts are increased in the following tests to account for the time it takes to load the font library tabs.
-
 		test.beforeAll( async ( { requestUtils } ) => {
 			await requestUtils.activateTheme( 'twentytwentythree' );
 			await requestUtils.activatePlugin(
@@ -198,6 +144,17 @@ test.describe( 'Font Library', () => {
 					name: 'Manage Fonts',
 				} )
 				.click();
+
+			// We need to wait the response from the `wp_font_family` request in order to test the Upload tab.
+			await page.waitForResponse(
+				( resp ) =>
+					resp
+						.url()
+						.includes(
+							'/index.php?rest_route=%2Fwp%2Fv2%2Ffont-families'
+						) && resp.status() === 200,
+				{ timeout: 50000 }
+			);
 		} );
 
 		test.afterAll( async ( { requestUtils } ) => {
@@ -209,43 +166,6 @@ test.describe( 'Font Library', () => {
 		test( 'should not display the "Upload" tab', async ( { page } ) => {
 			await expect(
 				page.getByRole( 'tab', { name: 'Upload' } )
-			).toBeHidden( { timeout: 40000 } );
-		} );
-
-		test( 'should not display the default collections tab', async ( {
-			page,
-		} ) => {
-			await expect(
-				page.getByRole( 'tab', { name: 'Install Fonts' } )
-			).toBeHidden( { timeout: 40000 } );
-		} );
-
-		test( 'should not display the "Delete" button in the footer', async ( {
-			page,
-			requestUtils,
-		} ) => {
-			// Upload a local font, so we can test the delete button
-			await requestUtils.deactivatePlugin(
-				'gutenberg-test-font-library-permissions'
-			);
-			await page
-				.getByRole( 'tab', { name: 'Upload' } )
-				.click( { timeout: 40000 } );
-			const fileChooserPromise = page.waitForEvent( 'filechooser' );
-			await page.getByRole( 'button', { name: 'Upload Font' } ).click();
-			const fileChooser = await fileChooserPromise;
-			await fileChooser.setFiles(
-				'./test/e2e/assets/Commissioner-Regular.ttf'
-			);
-			await requestUtils.activatePlugin(
-				'gutenberg-test-font-library-permissions'
-			);
-
-			await page.getByRole( 'tab', { name: 'Library' } ).click();
-			await page.getByRole( 'button', { name: 'Commissioner' } ).click();
-
-			await expect(
-				page.getByRole( 'button', { name: 'Delete' } )
 			).toBeHidden();
 		} );
 	} );
