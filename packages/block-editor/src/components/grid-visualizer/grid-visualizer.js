0bd9683d8e67271a2dfa965ec6c5746c8751324a
Robert Anderson <robert@noisysocks.com>
2024-02-29 14:49:52 +1100

    Use DropZones in GridVisualizer to set positions of grid items

--- packages/block-editor/src/components/grid-visualizer/grid-visualizer.js
+++ packages/block-editor/src/components/grid-visualizer/grid-visualizer.js
@@ -1,7 +1,14 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { useState, useEffect } from '@wordpress/element';
+import { DropZone } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -9,6 +16,8 @@ import { useState, useEffect } from '@wordpress/element';
 import { __unstableUseBlockElement as useBlockElement } from '../block-list/use-block-props/use-block-refs';
 import BlockPopoverCover from '../block-popover/cover';
 import { getComputedCSS } from './utils';
+import { parseDropEvent } from '../use-on-block-drop';
+import { store as blockEditorStore } from '../../store';
 
 export function GridVisualizer( { clientId } ) {
 	const blockElement = useBlockElement( clientId );
@@ -16,44 +25,10 @@ export function GridVisualizer( { clientId } ) {
 		return null;
 	}
 	return (
-		<BlockPopoverCover
-			className="block-editor-grid-visualizer"
+		<GridVisualizerGrid
 			clientId={ clientId }
-			__unstablePopoverSlot="block-toolbar"
-		>
-			<GridVisualizerGrid blockElement={ blockElement } />
-		</BlockPopoverCover>
-	);
-}
-
-function GridVisualizerGrid( { blockElement } ) {
-	const [ gridInfo, setGridInfo ] = useState( () =>
-		getGridInfo( blockElement )
-	);
-	useEffect( () => {
-		const observers = [];
-		for ( const element of [ blockElement, ...blockElement.children ] ) {
-			const observer = new window.ResizeObserver( () => {
-				setGridInfo( getGridInfo( blockElement ) );
-			} );
-			observer.observe( element );
-			observers.push( observer );
-		}
-		return () => {
-			for ( const observer of observers ) {
-				observer.disconnect();
-			}
-		};
-	}, [ blockElement ] );
-	return (
-		<div
-			className="block-editor-grid-visualizer__grid"
-			style={ gridInfo.style }
-		>
-			{ Array.from( { length: gridInfo.numItems }, ( _, i ) => (
-				<div key={ i } className="block-editor-grid-visualizer__item" />
-			) ) }
-		</div>
+			blockElement={ blockElement }
+		/>
 	);
 }
 
@@ -70,6 +45,8 @@ function getGridInfo( blockElement ) {
 	const numRows = gridTemplateRows.split( ' ' ).length;
 	const numItems = numColumns * numRows;
 	return {
+		numColumns,
+		numRows,
 		numItems,
 		style: {
 			gridTemplateColumns,
@@ -78,4 +55,97 @@ function getGridInfo( blockElement ) {
 			padding: getComputedCSS( blockElement, 'padding' ),
 		},
 	};
+}
+
+function range( start, length ) {
+	return Array.from( { length }, ( _, i ) => start + i );
+}
+
+function GridVisualizerGrid( { clientId, blockElement } ) {
+	const [ gridInfo, setGridInfo ] = useState( () =>
+		getGridInfo( blockElement )
+	);
+	const [ isDisabled, setIsDisabled ] = useState( true );
+
+	useEffect( () => {
+		const observers = [];
+		for ( const element of [ blockElement, ...blockElement.children ] ) {
+			const observer = new window.ResizeObserver( () => {
+				setGridInfo( getGridInfo( blockElement ) );
+			} );
+			observer.observe( element );
+			observers.push( observer );
+		}
+		return () => {
+			for ( const observer of observers ) {
+				observer.disconnect();
+			}
+		};
+	}, [ blockElement ] );
+
+	useEffect( () => {
+		function onGlobalDrag() {
+			setIsDisabled( false );
+		}
+
+		function onGlobalDragEnd() {
+			setIsDisabled( true );
+		}
+		document.addEventListener( 'drag', onGlobalDrag );
+		document.addEventListener( 'dragend', onGlobalDragEnd );
+		return () => {
+			document.removeEventListener( 'drag', onGlobalDrag );
+			document.removeEventListener( 'dragend', onGlobalDragEnd );
+		};
+	}, [] );
+
+	return (
+		<BlockPopoverCover
+			className={ classnames( 'block-editor-grid-visualizer', {
+				'is-disabled': isDisabled,
+			} ) }
+			clientId={ clientId }
+			__unstablePopoverSlot="block-toolbar"
+		>
+			<div
+				className="block-editor-grid-visualizer__grid"
+				style={ gridInfo.style }
+			>
+				{ range( 1, gridInfo.numRows ).map( ( row ) =>
+					range( 1, gridInfo.numColumns ).map( ( column ) => (
+						<GridVisualizerCell
+							key={ `${ row }-${ column }` }
+							column={ column }
+							row={ row }
+						/>
+					) )
+				) }
+			</div>
+		</BlockPopoverCover>
+	);
+}
+
+function GridVisualizerCell( { column, row } ) {
+	const { updateBlockAttributes } = useDispatch( blockEditorStore );
+
+	function onDrop( event ) {
+		const { srcClientIds } = parseDropEvent( event );
+		if ( ! srcClientIds?.length ) {
+			return;
+		}
+		updateBlockAttributes( srcClientIds, {
+			style: {
+				layout: {
+					columnStart: column,
+					rowStart: row,
+				},
+			},
+		} );
+	}
+
+	return (
+		<div className="block-editor-grid-visualizer__cell">
+			<DropZone label={ 'Drop to pin block' } onDrop={ onDrop } />
+		</div>
+	);
 }
