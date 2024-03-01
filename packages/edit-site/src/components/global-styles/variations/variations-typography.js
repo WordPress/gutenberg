b13fa98a14f5a7689865f739c9609bd22d26d9fd
scruffian <ben@scruffian.com>
2024-03-01 13:25:29 +0000

    Global Styles: Remove preset headers

--- packages/edit-site/src/components/global-styles/variations/variations-typography.js
+++ packages/edit-site/src/components/global-styles/variations/variations-typography.js
@@ -6,7 +6,6 @@ import {
 	__experimentalGrid as Grid,
 	__experimentalVStack as VStack,
 } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
 import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 
 /**
@@ -17,7 +16,6 @@ import { unlock } from '../../../lock-unlock';
 import { useCurrentMergeThemeStyleVariationsWithUserConfig } from '../../../hooks/use-theme-style-variations/use-theme-style-variations-by-property';
 import TypographyExample from '../typography-example';
 import PreviewIframe from '../preview-iframe';
-import Subtitle from '../subtitle';
 import { getFontFamilies } from '../utils';
 import Variation from './variation';
 
@@ -64,7 +62,6 @@ export default function TypographyVariations() {
 
 	return (
 		<VStack spacing={ 3 }>
-			<Subtitle level={ 3 }>{ __( 'Presets' ) }</Subtitle>
 			<Grid
 				columns={ 3 }
 				className="edit-site-global-styles-style-variations-container"
