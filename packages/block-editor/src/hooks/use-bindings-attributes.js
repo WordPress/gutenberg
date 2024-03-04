204d858f68470d488eb9b33956130fe8c7609785
Damián Suárez <rdsuarez@gmail.com>
2024-03-03 20:55:29 -0500

    clean

--- packages/block-editor/src/hooks/use-bindings-attributes.js
+++ packages/block-editor/src/hooks/use-bindings-attributes.js
@@ -4,13 +4,7 @@
 import { getBlockType, store as blocksStore } from '@wordpress/blocks';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { useSelect } from '@wordpress/data';
-import {
-	useLayoutEffect,
-	useCallback,
-	useState,
-	useMemo,
-	useRef,
-} from '@wordpress/element';
+import { useMemo } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import { RichTextData } from '@wordpress/rich-text';
 
@@ -62,79 +56,11 @@ export function canBindAttribute( blockName, attributeName ) {
 	);
 }
 
-/**
- * This component is responsible for detecting and
- * propagating data changes from the source to the block.
- *
- * @param {Object}   props                   - The component props.
- * @param {string}   props.attrName          - The attribute name.
- * @param {Function} props.onPropValueChange - The function to call when the attribute value changes.
- * @param {Object}   props.settings          - The block bindings settings.
- * @return {null}                              Data-handling component. Render nothing.
- */
-const BindingConnector = ( { attrName, settings, onPropValueChange } ) => {
-	const { useSource } = settings;
-	const newPropValue = useSource();
-	const prevPropValue = useRef( newPropValue );
-
-	useLayoutEffect( () => {
-		if ( prevPropValue.current === newPropValue ) {
-			return;
-		}
-
-		// Propagate the source value to the block attribute.
-		onPropValueChange( { [ attrName ]: newPropValue } );
-		prevPropValue.current = newPropValue;
-	}, [ newPropValue, attrName, onPropValueChange ] );
-
-	return null;
-};
-
-/**
- * BlockBindingBridge acts like a component wrapper
- * that connects the bound attributes of a block
- * to the source helper.
- * For this, it creates a BindingConnector for each bound attribute.
- *
- * @param {Object}   props                   - The component props.
- * @param {Object}   props.bindings          - The block bindings settings.
- * @param {Function} props.onPropValueChange - The function to call when the attribute value changes.
- * @return {null}                              Data-handling component. Render nothing.
- */
-function BlockBindingBridge( { bindings, onPropValueChange } ) {
-	return (
-		<>
-			{ Object.entries( bindings ).map( ( [ attrName, settings ] ) => {
-				return (
-					<BindingConnector
-						key={ attrName }
-						attrName={ attrName }
-						settings={ settings }
-						onPropValueChange={ onPropValueChange }
-					/>
-				);
-			} ) }
-		</>
-	);
-}
-
 const withBlockBindingSupport = createHigherOrderComponent(
 	( BlockEdit ) => ( props ) => {
 		const blockBindingsSources = unlock(
 			useSelect( blocksStore )
 		).getAllBlockBindingsSources();
-
-		/*
-		 * A trigger to refresh
-		 * the bound attributesvalues
-		 * when the source data changes.
-		 * It is used to force the re-render of the BlockEdit component.
-		 */
-		const [ refreshTrigger, setTrigger ] = useState( 1 );
-		const refreshExternalSourceData = setTrigger.bind(
-			null,
-			refreshTrigger + 1
-		);
 
 		/*
 		 * Binding object:
@@ -152,10 +78,6 @@ const withBlockBindingSupport = createHigherOrderComponent(
 		 * },
 		 */
 		const bindings = useMemo( () => {
-			if ( ! refreshTrigger ) {
-				return {};
-			}
-
 			return Object.fromEntries(
 				Object.entries( props.attributes.metadata?.bindings || {} )
 					.filter(
@@ -216,7 +138,7 @@ const withBlockBindingSupport = createHigherOrderComponent(
 						];
 					} )
 			);
-		}, [ blockBindingsSources, props, refreshTrigger ] );
+		}, [ blockBindingsSources, props ] );
 
 		// Pick bound attributes from the (memoized) bindings object.
 		const boundAttributes = Object.fromEntries(
@@ -226,58 +148,15 @@ const withBlockBindingSupport = createHigherOrderComponent(
 			} )
 		);
 
-		/**
-		 * Helper function to update the block attributes,
-		 * handling both bound and unbound attributes.
-		 *
-		 * For unbound attributes, it calls the BlockEdit `setAttributes` callback.
-		 * For bound attributes, it calls the source `update` handler function.
-		 *
-		 * @param {Object} nextAttributes - The next attributes to update.
-		 * @return {void}
-		 */
-		const updateAttributes = useCallback(
-			( nextAttributes ) => {
-				const unboundAttributes = {};
-				Object.entries( nextAttributes ).forEach(
-					( [ boundAttributeName, value ] ) => {
-						if ( ! ( boundAttributeName in bindings ) ) {
-							//Collect unbound attributes.
-							unboundAttributes[ boundAttributeName ] = value;
-						} else {
-							// Update bound attribute, one by one.
-							const settings = bindings[ boundAttributeName ];
-							settings.update( value );
-						}
-					}
-				);
-
-				// Update unbound attributes.
-				if ( Object.keys( unboundAttributes ).length ) {
-					props.setAttributes( unboundAttributes );
-				}
-			},
-			[ bindings, props ]
-		);
-
 		if ( Object.keys( bindings ).length <= 0 ) {
 			return <BlockEdit { ...props } />;
 		}
 
 		return (
-			<>
-				<BlockBindingBridge
-					blockProps={ props }
-					bindings={ bindings }
-					onPropValueChange={ refreshExternalSourceData }
-				/>
-
-				<BlockEdit
-					{ ...props }
-					attributes={ { ...props.attributes, ...boundAttributes } }
-					setAttributes={ updateAttributes }
-				/>
-			</>
+			<BlockEdit
+				{ ...props }
+				attributes={ { ...props.attributes, ...boundAttributes } }
+			/>
 		);
 	},
 	'withBlockBindingSupport'
--- packages/editor/src/bindings/post-meta.js
+++ packages/editor/src/bindings/post-meta.js
@@ -54,5 +54,5 @@ export default {
 		};
 	},
 
-	lockAttributesEditing: false,
+	// lockAttributesEditing: false,
 };
