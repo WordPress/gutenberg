7303bd1c426381ffbd30df84d3e7ed4500c37884
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-03-05 12:54:48 +0900

    Add unit test

---
+++ packages/block-editor/src/hooks/test/text-align.js
@@ -0,0 +1,131 @@
+/**
+ * WordPress dependencies
+ */
+import { applyFilters } from '@wordpress/hooks';
+import {
+	getBlockTypes,
+	registerBlockType,
+	unregisterBlockType,
+} from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import { getValidTextAlignments, addAssignedTextAlign } from '../text-align';
+
+const noop = () => {};
+
+describe( 'textAlign', () => {
+	const blockSettings = {
+		save: noop,
+		category: 'text',
+		title: 'block title',
+		edit: ( { children } ) => <>{ children }</>,
+	};
+
+	afterEach( () => {
+		getBlockTypes().forEach( ( block ) => {
+			unregisterBlockType( block.name );
+		} );
+	} );
+
+	describe( 'addAttribute()', () => {
+		const filterRegisterBlockType = applyFilters.bind(
+			null,
+			'blocks.registerBlockType'
+		);
+
+		it( 'should do nothing if the block settings does not define text align support', () => {
+			const settings = filterRegisterBlockType( blockSettings );
+
+			expect( settings.attributes ).toBeUndefined();
+		} );
+
+		it( 'should assign a new text align attribute', () => {
+			const settings = filterRegisterBlockType( {
+				...blockSettings,
+				supports: {
+					textAlign: true,
+				},
+			} );
+
+			expect( settings.attributes ).toHaveProperty( 'textAlign' );
+		} );
+	} );
+
+	describe( 'getValidTextAlignments()', () => {
+		it( 'should return an empty array if block does not define align support', () => {
+			expect( getValidTextAlignments() ).toEqual( [] );
+		} );
+
+		it( 'should return all custom text aligns set', () => {
+			expect( getValidTextAlignments( [ 'left', 'right' ] ) ).toEqual( [
+				'left',
+				'right',
+			] );
+		} );
+
+		it( 'should return all text aligns sorted when provided in the random order', () => {
+			expect(
+				getValidTextAlignments( [ 'right', 'center', 'left' ] )
+			).toEqual( [ 'left', 'center', 'right' ] );
+		} );
+
+		it( 'should return all text aligns if block defines text align support as true', () => {
+			expect( getValidTextAlignments( true ) ).toEqual( [
+				'left',
+				'center',
+				'right',
+			] );
+		} );
+
+		it( 'should remove incorrect text aligns', () => {
+			expect(
+				getValidTextAlignments( [ 'left', 'right', 'justify' ] )
+			).toEqual( [ 'left', 'right' ] );
+		} );
+	} );
+
+	describe( 'addAssignedTextAlign', () => {
+		it( 'should do nothing if block does not support text align', () => {
+			registerBlockType( 'core/foo', blockSettings );
+
+			const props = addAssignedTextAlign(
+				{
+					className: 'foo',
+				},
+				'core/foo',
+				{
+					textAlign: 'center',
+				}
+			);
+
+			expect( props ).toEqual( {
+				className: 'foo',
+			} );
+		} );
+
+		it( 'should do add text align classname if block supports text align', () => {
+			registerBlockType( 'core/foo', {
+				...blockSettings,
+				supports: {
+					textAlign: true,
+				},
+			} );
+
+			const props = addAssignedTextAlign(
+				{
+					className: 'foo',
+				},
+				'core/foo',
+				{
+					textAlign: 'center',
+				}
+			);
+
+			expect( props ).toEqual( {
+				className: 'has-text-align-center foo',
+			} );
+		} );
+	} );
+} );
