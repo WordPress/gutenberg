94f31faaf7ffb17c0d1f1b299c7aee569fe7659b
Damián Suárez <rdsuarez@gmail.com>
2024-03-06 18:44:29 -0400

    move binding helpers to its main folder

--- packages/block-editor/src/store/reducer.js
+++ packages/block-editor/src/store/reducer.js
@@ -15,6 +15,10 @@ import { store as blocksStore } from '@wordpress/blocks';
 import { PREFERENCES_DEFAULTS, SETTINGS_DEFAULTS } from './defaults';
 import { insertAt, moveTo } from './array';
 import { unlock } from '../lock-unlock';
+import {
+	canBindAttribute,
+	canBindBlock,
+} from '../../../editor/src/bindings/utils';
 
 const identity = ( x ) => x;
 
@@ -89,39 +93,6 @@ function flattenBlocks( blocks, transform = identity ) {
 	}
 
 	return result;
-}
-
-const BLOCK_BINDINGS_ALLOWED_BLOCKS = {
-	'core/paragraph': [ 'content' ],
-	'core/heading': [ 'content' ],
-	'core/image': [ 'url', 'title', 'alt' ],
-	'core/button': [ 'url', 'text', 'linkTarget' ],
-};
-
-/**
- * Based on the given block name,
- * check if it is possible to bind the block.
- *
- * @param {string} blockName - The block name.
- * @return {boolean} Whether it is possible to bind the block to sources.
- */
-export function canBindBlock( blockName ) {
-	return blockName in BLOCK_BINDINGS_ALLOWED_BLOCKS;
-}
-
-/**
- * Based on the given block name and attribute name,
- * check if it is possible to bind the block attribute.
- *
- * @param {string} blockName     - The block name.
- * @param {string} attributeName - The attribute name.
- * @return {boolean} Whether it is possible to bind the block attribute.
- */
-export function canBindAttribute( blockName, attributeName ) {
-	return (
-		canBindBlock( blockName ) &&
-		BLOCK_BINDINGS_ALLOWED_BLOCKS[ blockName ].includes( attributeName )
-	);
 }
 
 function getFlattenedClientIds( blocks ) {
