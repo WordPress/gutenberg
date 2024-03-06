e23aa828caa91dbb0650ad9d54064e7faab56049
Nik Tsekouras <ntsekouras@outlook.com>
2024-03-06 12:55:52 +0200

    Consolidate template actions components (#59586)
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
    Co-authored-by: jameskoster <jameskoster@git.wordpress.org>

--- packages/edit-site/src/components/sidebar-edit-mode/template-panel/index.js
+++ packages/edit-site/src/components/sidebar-edit-mode/template-panel/index.js
@@ -19,16 +19,20 @@ import { __ } from '@wordpress/i18n';
 import { useAsyncList } from '@wordpress/compose';
 import { serialize } from '@wordpress/blocks';
 import { __experimentalBlockPatternsList as BlockPatternsList } from '@wordpress/block-editor';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
 
 /**
  * Internal dependencies
  */
 import { store as editSiteStore } from '../../../store';
-import TemplateActions from './template-actions';
+import TemplateActions from '../../template-actions';
 import TemplateAreas from './template-areas';
 import SidebarCard from '../sidebar-card';
 import { useAvailablePatterns } from './hooks';
 import { TEMPLATE_PART_POST_TYPE } from '../../../utils/constants';
+import { unlock } from '../../../lock-unlock';
+
+const { useHistory } = unlock( routerPrivateApis );
 
 const CARD_ICONS = {
 	wp_block: symbol,
@@ -77,7 +81,7 @@ export default function TemplatePanel() {
 		},
 		[]
 	);
-
+	const history = useHistory();
 	const availablePatterns = useAvailablePatterns( record );
 	const { editEntityRecord } = useDispatch( coreStore );
 
@@ -100,7 +104,19 @@ export default function TemplatePanel() {
 					title={ decodeEntities( title ) }
 					icon={ CARD_ICONS[ record?.type ] ?? icon }
 					description={ decodeEntities( description ) }
-					actions={ <TemplateActions template={ record } /> }
+					actions={
+						<TemplateActions
+							postType={ postType }
+							postId={ postId }
+							className="edit-site-template-card__actions"
+							toggleProps={ { size: 'small' } }
+							onRemove={ () => {
+								history.push( {
+									path: `/${ postType }/all`,
+								} );
+							} }
+						/>
+					}
 				>
 					<TemplateAreas />
 				</SidebarCard>
