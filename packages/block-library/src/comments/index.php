4edb50a6d9f1446d361ecbd057be50093e0a4383
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-03-07 19:26:52 +0100

    Add @since tags.

--- packages/block-library/src/comments/index.php
+++ packages/block-library/src/comments/index.php
@@ -124,6 +124,8 @@ add_filter( 'comment_form_defaults', 'comments_block_form_defaults' );
  * Enqueues styles from the legacy `core/post-comments` block. These styles are
  * required only by the block's fallback.
  *
+ * @since 6.1.0
+ *
  * @param string $block_name Name of the new block type.
  */
 function enqueue_legacy_post_comments_block_styles( $block_name ) {
