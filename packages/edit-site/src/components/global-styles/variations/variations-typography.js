6546d51c65120d7838a75fa77cb50974555fd203
scruffian <ben@scruffian.com>
2024-03-07 13:10:00 +0000

    add border radius

--- packages/edit-site/src/components/global-styles/variations/variations-typography.js
+++ packages/edit-site/src/components/global-styles/variations/variations-typography.js
@@ -77,6 +77,7 @@ export default function TypographyVariations() {
 									>
 										{ ( { ratio, key } ) => (
 											<HStack
+												key={ key }
 												spacing={ 10 * ratio }
 												justify="center"
 												style={ {
@@ -85,7 +86,6 @@ export default function TypographyVariations() {
 												} }
 											>
 												<TypographyExample
-													key={ key }
 													variation={ variation }
 													fontSize={ 85 * ratio }
 												/>
