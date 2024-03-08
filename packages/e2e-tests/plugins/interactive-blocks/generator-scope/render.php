09889c5016d76c072de4b6b70d7f1b49a2003228
Jon Surrell <sirreal@users.noreply.github.com>
2024-03-08 13:16:53 +0100

    Use consistent HTML quoting

--- packages/e2e-tests/plugins/interactive-blocks/generator-scope/render.php
+++ packages/e2e-tests/plugins/interactive-blocks/generator-scope/render.php
@@ -11,6 +11,6 @@
 	<?php echo wp_interactivity_data_wp_context( array( 'result' => '' ) ); ?>
 >
 	<input readonly data-wp-bind--value="context.result" data-testid="result" />
-	<button type="button" data-wp-on--click='callbacks.resolve' data-testid='resolve'>Async resolve</button>
-	<button type="button" data-wp-on--click='callbacks.reject' data-testid='reject'>Async reject</button>
+	<button type="button" data-wp-on--click="callbacks.resolve" data-testid="resolve">Async resolve</button>
+	<button type="button" data-wp-on--click="callbacks.reject" data-testid="reject">Async reject</button>
 </div>
