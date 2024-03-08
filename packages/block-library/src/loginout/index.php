f14395a4a3e5e8ec1a7cb3acbd2c0ca1f94554f1
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-03-08 18:25:31 +0100

    Add @since tags.

--- packages/block-library/src/loginout/index.php
+++ packages/block-library/src/loginout/index.php
@@ -8,6 +8,8 @@
 /**
  * Renders the `core/loginout` block on server.
  *
+ * @since 5.8.0
+ *
  * @param array $attributes The block attributes.
  *
  * @return string Returns the login-out link or form.
