b14634d4e0b420be578059e984a4d682f7eaff7a
Jon Surrell <sirreal@users.noreply.github.com>
2024-03-08 13:09:52 +0100

    Fix lint

--- packages/e2e-tests/plugins/interactive-blocks/generator-scope/render.php
+++ packages/e2e-tests/plugins/interactive-blocks/generator-scope/render.php
@@ -8,7 +8,7 @@
 
 <div
 	data-wp-interactive="test/generator-scope"
-	<?php echo wp_interactivity_data_wp_context( array( 'result' => '' ) ) ?>
+	<?php echo wp_interactivity_data_wp_context( array( 'result' => '' ) ); ?>
 >
 	<input readonly data-wp-bind--value="context.result" data-testid="result" />
 	<button type="button" data-wp-on--click='callbacks.resolve' data-testid='resolve'>Async resolve</button>
