99d8d5ef65c14fd30450e488ac1088877ee33130
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-03-08 18:03:51 +0100

    Add @since tags.

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -963,6 +963,8 @@ function block_core_navigation_filter_out_empty_blocks( $parsed_blocks ) {
 /**
  * Returns true if the navigation block contains a nested navigation block.
  *
+ * @since 6.2.0
+ *
  * @param WP_Block_List $inner_blocks Inner block instance to be normalized.
  * @return bool true if the navigation block contains a nested navigation block.
  */
