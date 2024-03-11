fa2ca72a2be17da1cc4a831543abac2a8ff83eba
André <583546+oandregal@users.noreply.github.com>
2024-03-11 17:18:04 +0100

    DataViews: implement multiple selection for filters (#59610)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: jameskoster <jameskoster@git.wordpress.org>
    Co-authored-by: jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
    Co-authored-by: andrewhayward <andrewhayward@git.wordpress.org>

--- packages/dataviews/src/utils.js
+++ packages/dataviews/src/utils.js
@@ -7,7 +7,13 @@ import { privateApis as componentsPrivateApis } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import { OPERATORS } from './constants';
+import {
+	ALL_OPERATORS,
+	OPERATOR_IS,
+	OPERATOR_IS_NOT,
+	OPERATOR_IS_ANY,
+	OPERATOR_IS_NONE,
+} from './constants';
 import { unlock } from './lock-unlock';
 
 const { DropdownMenuSeparatorV2: DropdownMenuSeparator } = unlock(
@@ -68,12 +74,40 @@ export function getPaginationResults( { data, view } ) {
 
 export const sanitizeOperators = ( field ) => {
 	let operators = field.filterBy?.operators;
+
+	// Assign default values.
 	if ( ! operators || ! Array.isArray( operators ) ) {
-		operators = Object.keys( OPERATORS );
+		operators = [ OPERATOR_IS_ANY, OPERATOR_IS_NONE ];
 	}
-	return operators.filter( ( operator ) =>
-		Object.keys( OPERATORS ).includes( operator )
+
+	// Transform legacy in, notIn operators to is, isNot.
+	// To be removed in the future.
+	if ( operators.includes( 'in' ) ) {
+		operators = operators.filter( ( operator ) => operator !== 'is' );
+		operators.push( 'is' );
+	}
+	if ( operators.includes( 'notIn' ) ) {
+		operators = operators.filter( ( operator ) => operator !== 'notIn' );
+		operators.push( 'isNot' );
+	}
+
+	// Make sure only valid operators are used.
+	operators = operators.filter( ( operator ) =>
+		ALL_OPERATORS.includes( operator )
 	);
+
+	// Do not allow mixing single & multiselection operators.
+	// Remove multiselection operators if any of the single selection ones is present.
+	if (
+		operators.includes( OPERATOR_IS ) ||
+		operators.includes( OPERATOR_IS_NOT )
+	) {
+		operators = operators.filter( ( operator ) =>
+			[ OPERATOR_IS, OPERATOR_IS_NOT ].includes( operator )
+		);
+	}
+
+	return operators;
 };
 
 export function WithDropDownMenuSeparators( { children } ) {
