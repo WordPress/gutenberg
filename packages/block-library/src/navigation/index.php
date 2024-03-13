8f07a376926250e27029ca38bcf9df08f1cf59f4
Andrei Draganescu <andrei.draganescu@automattic.com>
2024-03-13 19:38:32 +0200

    always return empty strin

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -134,12 +134,10 @@ class WP_Navigation_Block_Renderer {
 		if ( ! empty( $inner_block_content ) ) {
 			if ( static::does_block_need_a_list_item_wrapper( $inner_block ) ) {
 				return '<li class="wp-block-navigation-item">' . $inner_block_content . '</li>';
-			} else {
-				$inner_block_content = '';
 			}
-
 			return $inner_block_content;
 		}
+		return '';
 	}
 
 	/**
