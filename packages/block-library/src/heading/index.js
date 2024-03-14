a3df5a50bbc057b01d267613e2c6add17af3e0b5
George Mamadashvili <georgemamadashvili@gmail.com>
2024-03-14 14:21:32 +0400

    Check content length

--- packages/block-library/src/heading/index.js
+++ packages/block-library/src/heading/index.js
@@ -2,7 +2,6 @@
  * WordPress dependencies
  */
 import { heading as icon } from '@wordpress/icons';
-import { toHTMLString } from '@wordpress/rich-text';
 import { __, sprintf } from '@wordpress/i18n';
 
 /**
@@ -31,21 +30,16 @@ export const settings = {
 		const { content, level } = attributes;
 
 		const customName = attributes?.metadata?.name;
-		const contentHTML =
-			typeof content === 'string'
-				? content
-				: toHTMLString( {
-						value: content,
-				  } );
+		const hasContent = content?.length > 0;
 
 		// In the list view, use the block's content as the label.
 		// If the content is empty, fall back to the default label.
-		if ( context === 'list-view' && ( customName || contentHTML ) ) {
-			return customName || contentHTML;
+		if ( context === 'list-view' && ( customName || hasContent ) ) {
+			return customName || content;
 		}
 
 		if ( context === 'accessibility' ) {
-			return ! contentHTML || contentHTML.length === 0
+			return ! hasContent
 				? sprintf(
 						/* translators: accessibility text. %s: heading level. */
 						__( 'Level %s. Empty.' ),
@@ -55,7 +49,7 @@ export const settings = {
 						/* translators: accessibility text. 1: heading level. 2: heading content. */
 						__( 'Level %1$s. %2$s' ),
 						level,
-						contentHTML
+						content
 				  );
 		}
 	},
