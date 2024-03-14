813274c7a5471bfacbf75b83953572d0c1ecf00c
Jon Surrell <sirreal@users.noreply.github.com>
2024-03-14 09:13:53 +0100

    Revert "Ensure stores exist for vdom namespaces"
    This reverts commit 17953d95b07cdc140d5cbad98bb585fcd572cde1.

--- packages/interactivity/src/init.js
+++ packages/interactivity/src/init.js
@@ -8,7 +8,6 @@ import { hydrate } from 'preact';
 import { toVdom, hydratedIslands } from './vdom';
 import { createRootFragment } from './utils';
 import { directivePrefix } from './constants';
-import { store, stores, universalUnlock } from './store';
 
 // Keep the same root fragment for each interactive region node.
 const regionRootFragments = new WeakMap();
@@ -34,28 +33,9 @@ export const initialVdom = new WeakMap();
 
 // Initialize the router with the initial DOM.
 export const init = async () => {
-	/** @type { NodeListOf<HTMLElement>} */
 	const nodes = document.querySelectorAll(
 		`[data-${ directivePrefix }-interactive]`
 	);
-
-	for ( const node of nodes ) {
-		// Before initializing the vdom, make sure a store exists for the namespace.
-		// This ensures that directives can subscribe to the store even if it has
-		// not yet been created on the client so that directives can be updated when
-		// stores are later created.
-		let namespace = /** @type {HTMLElement} */ ( node ).dataset[
-			`${ directivePrefix }Interactive`
-		];
-		try {
-			namespace = JSON.parse( namespace ).namespace;
-		} catch {}
-		if ( ! stores.has( namespace ) ) {
-			store( namespace, undefined, {
-				lock: universalUnlock,
-			} );
-		}
-	}
 
 	for ( const node of nodes ) {
 		if ( ! hydratedIslands.has( node ) ) {
