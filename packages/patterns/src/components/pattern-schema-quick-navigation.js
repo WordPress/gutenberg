0d70339050a7592d775d73833ad9d64494e6ce6e
Daniel Richards <daniel.richards@automattic.com>
2024-03-15 14:05:15 +0800

    Add new quick navigation component for pattern schema

---
+++ packages/patterns/src/components/pattern-schema-quick-navigation.js
@@ -0,0 +1,35 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import {
+	store as blockEditorStore,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { unlock } from '../lock-unlock';
+import { isOverridableBlock } from '../api';
+
+const { BlockQuickNavigation } = unlock( blockEditorPrivateApis );
+
+export default function PatternSchemaQuickNavigation() {
+	const allClientIds = useSelect(
+		( select ) =>
+			unlock( select( blockEditorStore ) ).getClientIdsWithDescendants(),
+		[]
+	);
+	const { getBlock } = useSelect( blockEditorStore );
+	const clientIdsWithOverrides = useMemo(
+		() =>
+			allClientIds.filter( ( { clientId } ) => {
+				const block = getBlock( clientId );
+				return isOverridableBlock( block );
+			} ),
+		[ allClientIds, getBlock ]
+	);
+	return <BlockQuickNavigation clientIds={ clientIdsWithOverrides } />;
+}
