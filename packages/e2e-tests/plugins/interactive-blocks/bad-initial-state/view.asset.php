c9b061076a9180e59371228e480ad7bcaf8a2a21
Jon Surrell <sirreal@users.noreply.github.com>
2024-03-15 11:22:13 +0100

    Remove bad-initial-state specific test stuff

--- packages/e2e-tests/plugins/interactive-blocks/bad-initial-state/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array( 'dependencies' => array( '@wordpress/interactivity' ) );
--- packages/e2e-tests/plugins/interactive-blocks/bad-initial-state/view.js
+++
@@ -1,9 +0,0 @@
-/**
- * WordPress dependencies
- */
-import { store } from '@wordpress/interactivity';
-
-// A non-object state should never be allowed.
-store( 'test/bad-initial-state', { state: [ 'wrong' ] } );
-
-store( 'test/bad-initial-state', { state: { 0: 'right' } } );
