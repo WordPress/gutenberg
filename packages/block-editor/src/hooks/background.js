09ac286d26e4ebd102d9a48b81aa7a0b8445fae9
ramon <ramonjd@gmail.com>
2024-03-19 10:07:40 +1100

    Reset repeat for cover

--- packages/block-editor/src/hooks/background.js
+++ packages/block-editor/src/hooks/background.js
@@ -511,6 +511,10 @@ function BackgroundSizePanelItem( {
 			nextPosition = undefined;
 		}
 
+		if ( next === 'cover' ) {
+			nextRepeat = undefined;
+		}
+
 		if (
 			( currentValueForToggle === 'cover' ||
 				currentValueForToggle === 'contain' ) &&
