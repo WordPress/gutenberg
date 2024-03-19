c1e5703867ec8c295f24757f10e89b173380d8f2
Rich Tabor <hi@richtabor.com>
2024-03-19 14:47:36 -0400

    Add "Presets" subtitle, adjust grid

--- packages/edit-site/src/components/global-styles/variations/variations-color.js
+++ packages/edit-site/src/components/global-styles/variations/variations-color.js
@@ -5,10 +5,12 @@ import {
 	__experimentalGrid as Grid,
 	__experimentalVStack as VStack,
 } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import Subtitle from '../subtitle';
 import Variation from './variation';
 import StylesPreviewColors from '../preview-colors';
 import { useColorVariations } from '../hooks';
@@ -22,7 +24,8 @@ export default function ColorVariations() {
 
 	return (
 		<VStack spacing={ 3 }>
-			<Grid columns={ 3 }>
+			<Subtitle level={ 3 }>{ __( 'Presets' ) }</Subtitle>
+			<Grid columns={ 3 } gap={ 2 }>
 				{ colorVariations.map( ( variation, index ) => (
 					<Variation key={ index } variation={ variation }>
 						{ () => <StylesPreviewColors /> }
