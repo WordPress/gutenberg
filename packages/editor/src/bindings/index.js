bf9b46ddb3f14edd1288e7f5687e10de930d2be4
Damián Suárez <rdsuarez@gmail.com>
2024-03-21 20:30:54 +0000

    introduce and register core/post-entity

--- packages/editor/src/bindings/index.js
+++ packages/editor/src/bindings/index.js
@@ -10,10 +10,12 @@ import { dispatch } from '@wordpress/data';
 import { unlock } from '../lock-unlock';
 import patternOverrides from './pattern-overrides';
 import postMeta from './post-meta';
+import postEntity from './post-entity';
 
 const { registerBindingsSource } = unlock( dispatch( bindingsStore ) );
 
 registerBindingsSource( postMeta );
+registerBindingsSource( postEntity );
 
 if ( process.env.IS_GUTENBERG_PLUGIN ) {
 	registerBindingsSource( patternOverrides );
---
+++ packages/editor/src/bindings/post-entity.js
@@ -0,0 +1,41 @@
+/**
+ * WordPress dependencies
+ */
+import { store as coreStore } from '@wordpress/core-data';
+import { __ } from '@wordpress/i18n';
+import { select, dispatch } from '@wordpress/data';
+/**
+ * Internal dependencies
+ */
+import { store as editorStore } from '../store';
+
+export default {
+	name: 'core/post-entity',
+	label: __( 'Post Entity' ),
+	connect( { prop, id } ) {
+		if ( ! prop ) {
+			throw new Error( 'The "prop" argument is required.' );
+		}
+
+		const { getEditedEntityRecord } = select( coreStore );
+		const { editEntityRecord } = dispatch( coreStore );
+		const { getCurrentPostId } = select( editorStore );
+
+		id = id || getCurrentPostId();
+
+		return {
+			get: () => {
+				const record = getEditedEntityRecord( 'postType', 'post', id );
+				return record[ prop ]?.rendered || record[ prop ];
+			},
+
+			update: ( newValue ) => {
+				editEntityRecord( 'postType', 'post', id, {
+					[ prop ]: newValue,
+				} );
+			},
+		};
+	},
+
+	lockAttributesEditing: false,
+};
