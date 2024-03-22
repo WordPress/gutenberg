ee71d29ac8e02320b7ed04e22ae2413bd3ae2865
MaggieCabrera <maggie.cabrera@automattic.com>
2024-03-22 17:22:43 +0100

    position toolbar popover to the left and change colors on zoomed-out mode

--- packages/block-editor/src/components/block-tools/block-toolbar-breadcrumb.js
+++ packages/block-editor/src/components/block-tools/block-toolbar-breadcrumb.js
@@ -1,4 +1,8 @@
 /**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+/**
  * External dependencies
  */
 import clsx from 'clsx';
@@ -15,6 +19,7 @@ import BlockSelectionButton from './block-selection-button';
 import { PrivateBlockPopover } from '../block-popover';
 import useBlockToolbarPopoverProps from './use-block-toolbar-popover-props';
 import useSelectedBlockToolProps from './use-selected-block-tool-props';
+import { store as blockEditorStore } from '../../store';
 
 function BlockToolbarBreadcrumb( { clientId, __unstableContentRef }, ref ) {
 	const {
@@ -29,12 +34,20 @@ function BlockToolbarBreadcrumb( { clientId, __unstableContentRef }, ref ) {
 		clientId,
 	} );
 
+	const isZoomedOutViewExperimentEnabled =
+		window?.__experimentalEnableZoomedOutView && editorMode === 'zoom-out';
+
+	if ( isZoomedOutViewExperimentEnabled ) {
+		popoverProps.placement = 'left-start';
+	}
+
 	return (
 		<PrivateBlockPopover
 			clientId={ capturingClientId || clientId }
 			bottomClientId={ lastClientId }
 			className={ clsx( 'block-editor-block-list__block-popover', {
 				'is-insertion-point-visible': isInsertionPointVisible,
+				'is-vertical': isZoomedOutViewExperimentEnabled,
 			} ) }
 			resize={ false }
 			{ ...popoverProps }
