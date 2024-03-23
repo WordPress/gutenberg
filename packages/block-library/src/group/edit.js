c9802beeeb3fdf02dcdb8d31dafd17fa5abfceb3
mujuonly <muju.only@gmail.com>
2024-03-23 17:23:40 +0530

    Update edit.js
    Remove unwanted space

--- packages/block-library/src/group/edit.js
+++ packages/block-library/src/group/edit.js
@@ -34,7 +34,7 @@ function GroupEditControls( { tagName, onSelectTagName } ) {
 			'The <header> element should represent introductory content, typically a group of introductory or navigational aids.'
 		),
 		main: __(
-			'The <main> element should be used for the primary content of your document only. '
+			'The <main> element should be used for the primary content of your document only.'
 		),
 		section: __(
 			"The <section> element should represent a standalone portion of the document that can't be better represented by another element."
