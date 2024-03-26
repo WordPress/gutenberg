02eea0ef3761acbc153b04e8505aba937e2711d0
Dave Smith <getdavemail@gmail.com>
2024-03-26 09:24:57 +0000

    Update comment block to coding standards
    Co-authored-by: Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>

--- packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
@@ -236,8 +236,10 @@ export function makeFontFacesFormData( font ) {
 export async function batchInstallFontFaces( fontFamilyId, fontFacesData ) {
 	const responses = [];
 
-	// Uses the same response format as Promise.allSettled, but executes requests in sequence to work around a race condition
-	// that can cause an error when the fonts directory doesn't exist yet.
+	/*
+	 * Uses the same response format as Promise.allSettled, but executes requests in sequence to work 
+	 * around a race condition that can cause an error when the fonts directory doesn't exist yet.
+	 */
 	for ( const faceData of fontFacesData ) {
 		try {
 			const response = await fetchInstallFontFace(
