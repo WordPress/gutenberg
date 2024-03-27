36e090173ee052f9d5bbb8a92eca3304e2df2bcb
Mario Santos <santosguillamot@gmail.com>
2024-03-27 10:12:19 +0100

    Clean PHP code and docs

--- lib/experimental/full-page-client-side-navigation.php
+++ lib/experimental/full-page-client-side-navigation.php
@@ -3,7 +3,9 @@
  * Registers full page client-side navigation option using the Interactivity API and adds the necessary directives.
  */
 
-// Enqueue the interactivity router script.
+/**
+ * Enqueue the interactivity router script.
+ */
 function _gutenberg_enqueue_interactivity_router() {
 	// Set the navigation mode to full page client-side navigation.
 	wp_interactivity_config( 'core/router', array( 'navigationMode' => 'fullPage' ) );
@@ -12,9 +14,14 @@ function _gutenberg_enqueue_interactivity_router() {
 
 add_action( 'wp_enqueue_scripts', '_gutenberg_enqueue_interactivity_router' );
 
-// Add directives to all links.
-// This should probably be done per site, not by default when this option is enabled.
-function _gutenberg_add_enhanced_pagination( $parsed_block ) {
+/**
+ * Set enhancedPagination attribute for query loop when the experiment is enabled.
+ *
+ * @param array $parsed_block The parsed block.
+ *
+ * @return array The same parsed block with the modified attribute.
+ */
+function _gutenberg_add_enhanced_pagination_to_query_block( $parsed_block ) {
 	if ( 'core/query' !== $parsed_block['blockName'] ) {
 		return $parsed_block;
 	}
@@ -23,10 +30,17 @@ function _gutenberg_add_enhanced_pagination( $parsed_block ) {
 	return $parsed_block;
 }
 
-add_filter( 'render_block_data', '_gutenberg_add_enhanced_pagination', 10 );
+add_filter( 'render_block_data', '_gutenberg_add_enhanced_pagination_to_query_block' );
 
-// Add directives to all links.
-// This should probably be done per site, not by default when this option is enabled.
+/**
+ * Add directives to all links.
+ *
+ * Note: This should probably be done per site, not by default when this option is enabled.
+ *
+ * @param array $content The block content.
+ *
+ * @return array The same block content with the directives needed.
+ */
 function _gutenberg_add_client_side_navigation_directives( $content ) {
 	$p = new WP_HTML_Tag_Processor( $content );
 	while ( $p->next_tag( array( 'tag_name' => 'a' ) ) ) {
@@ -43,4 +57,4 @@ function _gutenberg_add_client_side_navigation_directives( $content ) {
 }
 
 // TODO: Explore moving this to the server directive processing.
-add_filter( 'render_block', '_gutenberg_add_client_side_navigation_directives', 10, 2 );
+add_filter( 'render_block', '_gutenberg_add_client_side_navigation_directives' );
