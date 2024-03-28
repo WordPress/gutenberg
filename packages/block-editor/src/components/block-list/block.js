59a8a796bedf072e6cc64e24d8226b250eaef9ec
Ella <ella@vandurpe.com>
2024-03-28 10:16:58 +0200

    Block editor: add comment for unmemoized context

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -225,6 +225,11 @@ function BlockListBlock( {
 	// We set a new context with the adjusted and filtered wrapperProps (through
 	// `editor.BlockListBlock`), which the `BlockListBlockProvider` did not have
 	// access to.
+	// Note that the context value doesn't have to be memoized in this case
+	// because when it changes, this component will be re-rendered anyway, and
+	// none of the consumers (BlockListBlock and useBlockProps) are memoized or
+	// "pure". This is different from the public BlockEditContext, where
+	// consumers might be memoized or "pure".
 	return (
 		<PrivateBlockContext.Provider
 			value={ {
