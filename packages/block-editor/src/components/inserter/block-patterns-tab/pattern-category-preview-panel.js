7b4fbf11c444ff89a6fe46e275dd44a1cc0ef9fa
Jerry Jones <jones.jeremydavid@gmail.com>
2024-03-28 09:22:58 -0500

    Let tabs component handle focus

--- packages/block-editor/src/components/inserter/block-patterns-tab/pattern-category-preview-panel.js
+++ packages/block-editor/src/components/inserter/block-patterns-tab/pattern-category-preview-panel.js
@@ -1,11 +1,4 @@
 /**
- * WordPress dependencies
- */
-import { useRef, useEffect } from '@wordpress/element';
-
-import { focus } from '@wordpress/dom';
-
-/**
  * Internal dependencies
  */
 
@@ -20,34 +13,19 @@ export function PatternCategoryPreviewPanel( {
 	showTitlesAsTooltip,
 	patternFilter,
 } ) {
-	const container = useRef();
-
-	useEffect( () => {
-		const timeout = setTimeout( () => {
-			const [ firstTabbable ] = focus.tabbable.find( container.current );
-			firstTabbable?.focus();
-		} );
-		return () => clearTimeout( timeout );
-	}, [ category ] );
-
 	// Move to zoom out mode when this component is mounted
 	// and back to the previous mode when unmounted.
 	useZoomOut();
 
 	return (
-		<div
-			ref={ container }
-			className="block-editor-inserter__patterns-category-dialog"
-		>
-			<PatternCategoryPreviews
-				key={ category.name }
-				rootClientId={ rootClientId }
-				onInsert={ onInsert }
-				onHover={ onHover }
-				category={ category }
-				showTitlesAsTooltip={ showTitlesAsTooltip }
-				patternFilter={ patternFilter }
-			/>
-		</div>
+		<PatternCategoryPreviews
+			key={ category.name }
+			rootClientId={ rootClientId }
+			onInsert={ onInsert }
+			onHover={ onHover }
+			category={ category }
+			showTitlesAsTooltip={ showTitlesAsTooltip }
+			patternFilter={ patternFilter }
+		/>
 	);
 }
