a2cd4b4880f290528b4dcb9d15bee4509166c219
Jarda Snajdr <jsnajdr@gmail.com>
2024-04-02 13:27:38 +0200

    Merge Codemirror code into one chunk

--- packages/block-editor/src/components/global-styles/editor-view.js
+++ packages/block-editor/src/components/global-styles/editor-view.js
@@ -7,22 +7,6 @@ import { useEffect, useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
- * Dynamically import the language support for the editor.
- * 
- * @param {string} mode - Language mode for the editor. Currently supports 'css' and 'html'.
- */
-function importLanguageSupport( mode ) {
-	switch ( mode ) {
-		case 'css':
-			return import( '@codemirror/lang-css' );
-		case 'html':
-			return import( '@codemirror/lang-html' );
-		default:
-			return import( '@codemirror/lang-css' );
-	}
-}
-
-/**
  * @typedef {Object} Config
  * @property {Function} [callback] - Callback after the editor is initialized.
  * @property {string}   content    - Text content of the editor.
@@ -39,76 +23,46 @@ function importLanguageSupport( mode ) {
  * @param {string} [props.editorInstructionsText] - Instructions text for accessibility.
  * @param {Config} props.initialConfig            - Initial configuration for the editor. This can only be used for the initial setup of the editor.
  */
-const EditorView = ({
+const EditorView = ( {
 	editorId,
-	editorInstructionsText, 
-	initialConfig: {
-		callback,
-		content,
-		onChange,
-		onBlur,
-		mode,
-	},
-}) => {
-	const editorRef = useRef(null);
+	editorInstructionsText,
+	initialConfig: { callback, content, onChange, onBlur, mode },
+} ) => {
+	const editorRef = useRef( null );
 	const instanceId = useInstanceId( EditorView );
-	useEffect( () => {
-		( async () => {
-			/**
-			 * Lazy load CodeMirror by using Webpack's dynamic import.
-			 * This should be replaced with native dynamic import once it's supported.
-			 * @see https://github.com/WordPress/gutenberg/pull/60155
-			 */
-			const [{ EditorView: CmEditorView, basicSetup }, { indentWithTab }, { keymap }, languageSupport] = 
-				await Promise.all([
-					import( 'codemirror' ), 
-					import( '@codemirror/commands' ), 
-					import( '@codemirror/view' ),
-					importLanguageSupport( mode )
-				])
 
-			if ( editorRef.current ) {
-				new CmEditorView( {
-					doc: content,
-					extensions: [
-						basicSetup,
-						languageSupport[mode](),
-						keymap.of( [ indentWithTab ] ),
-						CmEditorView.updateListener.of( ( editor ) => {
-							if ( editor.docChanged ) {
-								onChange( editor.state.doc.toString() );
-							}
-						} ),
-						...(onBlur ?
-						[CmEditorView.focusChangeEffect.of(
-							( editorState, focusing ) => {
-								if ( ! focusing ) {
-									onBlur( editorState.doc.toString() );
-								}
-								return null;
-							}
-						)] : []),
-					],
-					parent: editorRef.current,
-				} );
-				if (typeof callback === 'function') {
+	useEffect( () => {
+		import( /* webpackChunkName: "codemirror" */ './code-mirror' )
+			.then( ( { default: createEditorView } ) => {
+				if ( editorRef.current ) {
+					return createEditorView( {
+						parent: editorRef.current,
+						mode,
+						content,
+						onChange,
+						onBlur,
+					} );
+				}
+			} )
+			.then( () => {
+				if ( typeof callback === 'function' ) {
 					callback();
 				}
-			}
-		} )();
-		// We only want to run this once when the editor is initialized, so we can ignore the dependency array.
+			} );
+		// We only want to run this once, so we can ignore the dependency array.
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
+
 	return (
 		<>
-			{editorInstructionsText && (<VisuallyHidden
-				id={ instanceId }
-			>
-				{ editorInstructionsText }
-				{ __(
-					`Press Escape then Tab to move focus out of the editor.`
-				) }
-			</VisuallyHidden>)}
+			{ editorInstructionsText && (
+				<VisuallyHidden id={ instanceId }>
+					{ editorInstructionsText }
+					{ __(
+						`Press Escape then Tab to move focus out of the editor.`
+					) }
+				</VisuallyHidden>
+			) }
 			<div
 				ref={ editorRef }
 				id={ editorId }
