b68641dcdcaefeecb54398907cf1aacab0e675d5
André <583546+oandregal@users.noreply.github.com>
2024-04-02 13:45:09 +0200

    Templates: fix deferred rendering for grid layout (#60361)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>

--- packages/edit-site/src/components/page-templates-template-parts/index.js
+++ packages/edit-site/src/components/page-templates-template-parts/index.js
@@ -419,7 +419,10 @@ export default function PageTemplatesTemplateParts( { postType } ) {
 				view={ view }
 				onChangeView={ onChangeView }
 				onSelectionChange={ onSelectionChange }
-				deferredRendering={ ! view.hiddenFields?.includes( 'preview' ) }
+				deferredRendering={
+					view.type === LAYOUT_GRID ||
+					! view.hiddenFields?.includes( 'preview' )
+				}
 			/>
 		</Page>
 	);
