934b49f8f828cb95cff7e13967bf9c9e34c320c8
Andrea Fercia <a.fercia@gmail.com>
2024-04-03 12:45:03 +0200

    Improve edit template confirm dialog copy.

--- packages/editor/src/components/editor-canvas/edit-template-blocks-notification.js
+++ packages/editor/src/components/editor-canvas/edit-template-blocks-notification.js
@@ -68,7 +68,7 @@ export default function EditTemplateBlocksNotification( { contentRef } ) {
 			onCancel={ () => setIsDialogOpen( false ) }
 		>
 			{ __(
-				'You’ve tried to select a block that is part of a template, which may be used on other posts and pages.'
+				'Would you like to edit the template this block is part of?'
 			) }
 		</ConfirmDialog>
 	);
