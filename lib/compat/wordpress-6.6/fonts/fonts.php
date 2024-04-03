04396fb2aa0c1007268048d4b923884fa4314ff6
Grant Kinney <hi@grant.mk>
2024-04-02 21:50:28 -0500

    Adds WP version check to Gutenberg modified code

--- lib/compat/wordpress-6.6/fonts/fonts.php
+++ lib/compat/wordpress-6.6/fonts/fonts.php
@@ -10,7 +10,9 @@
  * @return array Filtered post type registration arguments.
  */
 function gutenberg_register_font_post_type_args( $arg, $post_type ) {
-	if ( 'wp_font_face' === $post_type ) {
+	global $wp_version;
+
+	if ( 'wp_font_face' === $post_type && version_compare( $wp_version, '6.6', '<' ) ) {
 		$arg['rest_controller_class'] = 'Gutenberg_REST_Font_Faces_Controller';
 	}
 
@@ -26,7 +28,9 @@ add_filter( 'register_post_type_args', 'gutenberg_register_font_post_type_args',
 	 * @param WP_Post $post    Post object.
 	 */
 function gutenberg_after_delete_font_family( $post_id, $post ) {
-	if ( 'wp_font_family' !== $post->post_type ) {
+	global $wp_version;
+
+	if ( 'wp_font_family' !== $post->post_type || version_compare( $wp_version, '6.6', '>=' ) ) {
 		return;
 	}
 
