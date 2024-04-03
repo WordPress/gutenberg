92338849a752dacbd31356d4ae9e592dfee252f8
Riad Benguella <benguella@gmail.com>
2024-04-03 13:07:46 +0200

    Site editor: Update hub markup and animation (#60410)
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: jasmussen <joen@git.wordpress.org>
    Co-authored-by: jameskoster <jameskoster@git.wordpress.org>

--- packages/edit-site/src/components/site-hub/index.js
+++ packages/edit-site/src/components/site-hub/index.js
@@ -97,70 +97,55 @@ const SiteHub = memo( ( { isTransparent, className } ) => {
 				ease: 'easeOut',
 			} }
 		>
-			<HStack
-				justify="space-between"
-				alignment="center"
-				className="edit-site-site-hub__container"
-			>
-				<HStack
-					justify="flex-start"
-					className="edit-site-site-hub__text-content"
-					spacing="0"
+			<HStack justify="flex-start" spacing="0">
+				<motion.div
+					className={ classnames(
+						'edit-site-site-hub__view-mode-toggle-container',
+						{
+							'has-transparent-background': isTransparent,
+						}
+					) }
+					layout
+					transition={ {
+						type: 'tween',
+						duration: disableMotion ? 0 : HUB_ANIMATION_DURATION,
+						ease: 'easeOut',
+					} }
 				>
-					<motion.div
-						className={ classnames(
-							'edit-site-site-hub__view-mode-toggle-container',
-							{
-								'has-transparent-background': isTransparent,
-							}
-						) }
-						layout
-						transition={ {
-							type: 'tween',
-							duration: disableMotion
-								? 0
-								: HUB_ANIMATION_DURATION,
-							ease: 'easeOut',
-						} }
+					<Button
+						{ ...siteIconButtonProps }
+						className="edit-site-layout__view-mode-toggle"
 					>
-						<Button
-							{ ...siteIconButtonProps }
-							className="edit-site-layout__view-mode-toggle"
-						>
-							<motion.div
-								initial={ false }
-								animate={ {
-									scale: canvasMode === 'view' ? 0.5 : 1,
-								} }
-								whileHover={ {
-									scale: canvasMode === 'view' ? 0.5 : 0.96,
-								} }
-								transition={ {
-									type: 'tween',
-									duration: disableMotion
-										? 0
-										: HUB_ANIMATION_DURATION,
-									ease: 'easeOut',
-								} }
-							>
-								<SiteIcon className="edit-site-layout__view-mode-toggle-icon" />
-							</motion.div>
-						</Button>
-					</motion.div>
-
-					<AnimatePresence>
 						<motion.div
-							layout={ canvasMode === 'edit' }
+							initial={ false }
 							animate={ {
-								opacity: canvasMode === 'view' ? 1 : 0,
+								scale: canvasMode === 'view' ? 0.5 : 1,
 							} }
-							exit={ {
-								opacity: 0,
+							whileHover={ {
+								scale: canvasMode === 'view' ? 0.5 : 0.96,
 							} }
-							className={ classnames(
-								'edit-site-site-hub__site-title',
-								{ 'is-transparent': isTransparent }
-							) }
+							transition={ {
+								type: 'tween',
+								duration: disableMotion
+									? 0
+									: HUB_ANIMATION_DURATION,
+								ease: 'easeOut',
+							} }
+						>
+							<SiteIcon className="edit-site-layout__view-mode-toggle-icon" />
+						</motion.div>
+					</Button>
+				</motion.div>
+
+				<AnimatePresence initial={ false }>
+					{ canvasMode === 'view' && (
+						<HStack
+							as={ motion.div }
+							initial={ { opacity: 0 } }
+							animate={ {
+								opacity: isTransparent ? 0 : 1,
+							} }
+							exit={ { opacity: 0 } }
 							transition={ {
 								type: 'tween',
 								duration: disableMotion ? 0 : 0.2,
@@ -168,37 +153,31 @@ const SiteHub = memo( ( { isTransparent, className } ) => {
 								delay: canvasMode === 'view' ? 0.1 : 0,
 							} }
 						>
-							{ decodeEntities( siteTitle ) }
-						</motion.div>
-					</AnimatePresence>
-					{ canvasMode === 'view' && (
-						<Button
-							href={ homeUrl }
-							target="_blank"
-							label={ __( 'View site (opens in a new tab)' ) }
-							aria-label={ __(
-								'View site (opens in a new tab)'
-							) }
-							icon={ external }
-							className={ classnames(
-								'edit-site-site-hub__site-view-link',
-								{ 'is-transparent': isTransparent }
-							) }
-						/>
+							<div className="edit-site-site-hub__title">
+								{ decodeEntities( siteTitle ) }
+							</div>
+
+							<Button
+								href={ homeUrl }
+								target="_blank"
+								label={ __( 'View site (opens in a new tab)' ) }
+								aria-label={ __(
+									'View site (opens in a new tab)'
+								) }
+								icon={ external }
+								className="edit-site-site-hub__site-view-link"
+							/>
+
+							<Button
+								className="edit-site-site-hub_toggle-command-center"
+								icon={ search }
+								onClick={ () => openCommandCenter() }
+								label={ __( 'Open command palette' ) }
+								shortcut={ displayShortcut.primary( 'k' ) }
+							/>
+						</HStack>
 					) }
-				</HStack>
-				{ canvasMode === 'view' && (
-					<Button
-						className={ classnames(
-							'edit-site-site-hub_toggle-command-center',
-							{ 'is-transparent': isTransparent }
-						) }
-						icon={ search }
-						onClick={ () => openCommandCenter() }
-						label={ __( 'Open command palette' ) }
-						shortcut={ displayShortcut.primary( 'k' ) }
-					/>
-				) }
+				</AnimatePresence>
 			</HStack>
 		</motion.div>
 	);
