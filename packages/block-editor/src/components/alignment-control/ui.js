c718590cdf4c5a6910f3bed5dc7295eafb53a07c
Arthur Normand <arthur@normand.space>
2024-04-04 19:43:38 -0400

    Enable justifying text

--- packages/block-editor/src/components/alignment-control/ui.js
+++ packages/block-editor/src/components/alignment-control/ui.js
@@ -3,7 +3,7 @@
  */
 import { __, isRTL } from '@wordpress/i18n';
 import { ToolbarDropdownMenu, ToolbarGroup } from '@wordpress/components';
-import { alignLeft, alignRight, alignCenter } from '@wordpress/icons';
+import {alignLeft, alignRight, alignCenter, alignJustify} from '@wordpress/icons';
 
 const DEFAULT_ALIGNMENT_CONTROLS = [
 	{
@@ -21,6 +21,11 @@ const DEFAULT_ALIGNMENT_CONTROLS = [
 		title: __( 'Align text right' ),
 		align: 'right',
 	},
+	{
+		icon: alignJustify,
+		title: __( 'Justify text' ),
+		align: 'justify',
+	}
 ];
 
 const POPOVER_PROPS = {
