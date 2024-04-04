2ff5f645682235aa6d4da7649256775f6328e842
Ella <4710635+ellatrix@users.noreply.github.com>
2024-04-04 21:23:04 +0300

    Patterns: guard for unknown pattern in server-side resolver (#60464)
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

--- lib/compat/wordpress-6.6/resolve-patterns.php
+++ lib/compat/wordpress-6.6/resolve-patterns.php
@@ -21,8 +21,15 @@ function gutenberg_replace_pattern_blocks( $blocks, &$inner_content = null ) {
 				continue;
 			}
 
-			$registry           = WP_Block_Patterns_Registry::get_instance();
-			$pattern            = $registry->get_registered( $slug );
+			$registry = WP_Block_Patterns_Registry::get_instance();
+			$pattern  = $registry->get_registered( $slug );
+
+			// Skip unknown patterns.
+			if ( ! $pattern ) {
+				++$i;
+				continue;
+			}
+
 			$blocks_to_insert   = parse_blocks( $pattern['content'] );
 			$seen_refs[ $slug ] = true;
 			$blocks_to_insert   = gutenberg_replace_pattern_blocks( $blocks_to_insert );
