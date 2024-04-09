35a1d2ff5dd058ae149619888c06654b0aed6cac
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2024-04-09 16:02:57 +1000

    Generate per-application variation styles and enforce unique classname

--- lib/block-supports/variations.php
+++ lib/block-supports/variations.php
@@ -7,6 +7,152 @@
  */
 
 /**
+ * Get the class name for this application of this block's variation styles.
+ *
+ * @param array  $block     Block object.
+ * @param string $variation Slug for the variation.
+ *
+ * @return string The unique class name.
+ */
+function gutenberg_get_variation_class_name( $block, $variation ) {
+	return 'is-style-' . $variation . '--' . md5( serialize( $block ) );
+}
+
+/**
+ * Determine a block style variation name from a CSS class string.
+ *
+ * @param string $class_string CSS class string to look for a variation in.
+ *
+ * @return string|null The variation name if found.
+ */
+function gutenberg_get_variation_name_from_class( $class_string ) {
+	if ( ! is_string( $class_string ) ) {
+		return null;
+	}
+
+	preg_match( '/\bis-style-(?!default)(\S+)\b/', $class_string, $matches );
+	return $matches ? $matches[1] : null;
+}
+
+/**
+ * Render the block style variation's styles.
+ *
+ * In the case of nested blocks with variations applies, we want the parent
+ * variation's styles to be rendered before their descendants. This solves the
+ * issue of a block type being styled in both the parent and descendant: we want
+ * the descendant style to take priority, and this is done by loading it after,
+ * in the DOM order. This is why the variation stylesheet generation is in a
+ * different filter.
+ *
+ * @param array $parsed_block The parsed block.
+ *
+ * @return array The same parsed block with variation classname added if appropriate.
+ */
+function gutenberg_render_variation_support_styles( $parsed_block ) {
+	$classes   = $parsed_block['attrs']['className'] ?? null;
+	$variation = gutenberg_get_variation_name_from_class( $classes );
+
+	if ( ! $variation ) {
+		return $parsed_block;
+	}
+
+	$tree           = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
+	$theme_json     = $tree->get_raw_data();
+	$variation_data = $theme_json['styles']['blocks'][ $parsed_block['blockName'] ]['variations'][ $variation ] ?? array();
+
+	if ( empty( $variation_data ) ) {
+		return $parsed_block;
+	}
+
+	$config = array(
+		'version' => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
+		'styles'  => $variation_data,
+	);
+
+	$class_name         = gutenberg_get_variation_class_name( $parsed_block, $variation );
+	$updated_class_name = $parsed_block['attrs']['className'] . " $class_name";
+
+	$class_name = ".$class_name";
+
+	if ( ! is_admin() ) {
+		remove_filter( 'wp_theme_json_get_style_nodes', 'wp_filter_out_block_nodes' );
+	}
+
+	$variation_theme_json = new WP_Theme_JSON_Gutenberg( $config, 'blocks' );
+	$variation_styles     = $variation_theme_json->get_stylesheet(
+		array( 'styles' ),
+		array( 'custom' ),
+		array(
+			'root_selector'           => $class_name,
+			'skip_root_layout_styles' => true,
+			'scope'                   => $class_name,
+		)
+	);
+
+	if ( ! is_admin() ) {
+		add_filter( 'wp_theme_json_get_style_nodes', 'wp_filter_out_block_nodes' );
+	}
+
+	if ( empty( $variation_styles ) ) {
+		return $parsed_block;
+	}
+
+	wp_register_style( 'variation-styles', false, array( 'global-styles' ) );
+	wp_add_inline_style( 'variation-styles', $variation_styles );
+
+	/*
+	 * Add variation instance class name to block's className string so it can
+	 * be enforced in the block markup via render_block filter.
+	 */
+	_wp_array_set( $parsed_block, array( 'attrs', 'className' ), $updated_class_name );
+
+	return $parsed_block;
+}
+
+/**
+ * Ensure the variation block support class name generated and added to
+ * block attributes in the `render_block_data` filter gets applied to the
+ * block's markup.
+ *
+ * @see gutenberg_render_variation_support_styles
+ *
+ * @param  string $block_content Rendered block content.
+ * @param  array  $block         Block object.
+ *
+ * @return string                Filtered block content.
+ */
+function gutenberg_render_variation_class_name( $block_content, $block ) {
+	if ( ! $block_content || empty( $block['attrs']['className'] ) ) {
+		return $block_content;
+	}
+
+	/*
+	 * Matches a class prefixed by `is-style`, followed by the
+	 * variation slug, then `--`, and finally a hash.
+	 *
+	 * See `gutenberg_get_variation_class_name` for class generation.
+	 */
+	preg_match( '/\bis-style-(\S+?--\w+)\b/', $block['attrs']['className'], $matches );
+
+	if ( empty( $matches ) ) {
+		return $block_content;
+	}
+
+	$tags = new WP_HTML_Tag_Processor( $block_content );
+
+	if ( $tags->next_tag() ) {
+		/*
+		 * Ensure the variation instance class name set in the
+		 * `render_block_data` filter is applied in markup.
+		 * See `gutenberg_render_variation_support_styles`.
+		 */
+		$tags->add_class( $matches[0] );
+	}
+
+	return $tags->get_updated_html();
+}
+
+/**
  * Collects block style variation data for merging with theme.json data.
  * As each block style variation is processed it is registered if it hasn't
  * been already. This registration is required for later sanitization of
@@ -173,8 +319,19 @@ function gutenberg_resolve_block_style_variations_from_styles_registry( $theme_j
 	return gutenberg_merge_block_style_variations_data( $variations_data, $theme_json );
 }
 
+/**
+ * Enqueues styles for block style variations.
+ */
+function gutenberg_enqueue_variation_styles() {
+	wp_enqueue_style( 'variation-styles' );
+}
+
 // Register the block support.
 WP_Block_Supports::get_instance()->register( 'variation', array() );
+
+add_filter( 'render_block_data', 'gutenberg_render_variation_support_styles', 10, 2 );
+add_filter( 'render_block', 'gutenberg_render_variation_class_name', 10, 2 );
+add_action( 'wp_enqueue_scripts', 'gutenberg_enqueue_variation_styles', 1 );
 
 // Resolve block style variations from all their potential sources. The order here is deliberate.
 add_filter( 'wp_theme_json_data_theme', 'gutenberg_resolve_block_style_variations_from_primary_theme_json', 10, 1 );
