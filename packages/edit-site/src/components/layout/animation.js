949cf831691d4e3013f1469ebc44dbdf23b3601a
James Koster <james@jameskoster.co.uk>
2024-04-09 11:27:02 +0100

    Duration

--- packages/edit-site/src/components/layout/animation.js
+++ packages/edit-site/src/components/layout/animation.js
@@ -15,7 +15,7 @@ function getAbsolutePosition( element ) {
 	};
 }
 
-const ANIMATION_DURATION = 250;
+const ANIMATION_DURATION = 400;
 
 /**
  * Hook used to compute the styles required to move a div into a new position.
