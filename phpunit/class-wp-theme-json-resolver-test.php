760b9b49c45968c47cdc1dec7929f23addc3c48c
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2024-04-10 13:48:33 +1000

    Update theme.json resolver unit tests for get_style_variations

--- phpunit/class-wp-theme-json-resolver-test.php
+++ phpunit/class-wp-theme-json-resolver-test.php
@@ -981,32 +981,107 @@ class WP_Theme_JSON_Resolver_Gutenberg_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that get_style_variations returns all variations, including parent theme variations if the theme is a child,
-	 * and that the child variation overwrites the parent variation of the same name.
+	 * Tests that `get_style_variations` returns all the appropriate variations,
+	 * including parent variations if the theme is a child, and that the child
+	 * variation overwrites the parent variation of the same name.
 	 *
-	 * @covers WP_Theme_JSON_Resolver_Gutenberg::get_style_variations
+	 * Note: This covers both theme style variations (`/styles`) and block style
+	 * variations (`/block-styles`).
+	 *
+	 * @covers WP_Theme_JSON_Resolver::get_style_variations
+	 *
+	 * @dataProvider data_get_style_variations
+	 *
+	 * @param string $theme               Name of the theme to use.
+	 * @param string $dir                 The directory to retrieve variation json files from.
+	 * @param array  $expected_variations Collection of expected variations.
 	 */
-	public function test_get_style_variations_returns_all_variations() {
-		// Switch to a child theme.
-		switch_theme( 'block-theme-child' );
+	public function test_get_style_variations( $theme, $dir, $expected_variations ) {
+		switch_theme( $theme );
 		wp_set_current_user( self::$administrator_id );
 
-		$actual_settings   = WP_Theme_JSON_Resolver_Gutenberg::get_style_variations();
-		$expected_settings = array(
-			array(
-				'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
-				'title'    => 'variation-a',
-				'settings' => array(
-					'blocks' => array(
-						'core/paragraph' => array(
+		$actual_variations = WP_Theme_JSON_Resolver_Gutenberg::get_style_variations( $dir );
+
+		wp_recursive_ksort( $actual_variations );
+		wp_recursive_ksort( $expected_variations );
+
+		$this->assertSame( $expected_variations, $actual_variations );
+	}
+
+	/**
+	 * Data provider for test_get_style_variations
+	 *
+	 * @return array
+	 */
+	public function data_get_style_variations() {
+		return array(
+			'theme_style_variations' => array(
+				'theme'               => 'block-theme-child',
+				'dir'                 => 'styles',
+				'expected_variations' => array(
+					array(
+						'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
+						'title'    => 'variation-a',
+						'settings' => array(
+							'blocks' => array(
+								'core/paragraph' => array(
+									'color' => array(
+										'palette' => array(
+											'theme' => array(
+												array(
+													'slug' => 'dark',
+													'name' => 'Dark',
+													'color' => '#010101',
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+					array(
+						'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
+						'title'    => 'variation-b',
+						'settings' => array(
+							'blocks' => array(
+								'core/post-title' => array(
+									'color' => array(
+										'palette' => array(
+											'theme' => array(
+												array(
+													'slug' => 'dark',
+													'name' => 'Dark',
+													'color' => '#010101',
+												),
+											),
+										),
+									),
+								),
+							),
+						),
+					),
+					array(
+						'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
+						'title'    => 'Block theme variation',
+						'settings' => array(
 							'color' => array(
 								'palette' => array(
 									'theme' => array(
 										array(
-											'slug'  => 'dark',
-											'name'  => 'Dark',
-											'color' => '#010101',
+											'slug'  => 'foreground',
+											'name'  => 'Foreground',
+											'color' => '#3F67C6',
 										),
+									),
+								),
+							),
+						),
+						'styles'   => array(
+							'blocks' => array(
+								'core/post-title' => array(
+									'typography' => array(
+										'fontWeight' => '700',
 									),
 								),
 							),
@@ -1014,61 +1089,34 @@ class WP_Theme_JSON_Resolver_Gutenberg_Test extends WP_UnitTestCase {
 					),
 				),
 			),
-			array(
-				'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
-				'title'    => 'variation-b',
-				'settings' => array(
-					'blocks' => array(
-						'core/post-title' => array(
+			'block_style_variations' => array(
+				'theme'               => 'block-theme-child-with-block-style-variations',
+				'dir'                 => 'block-styles',
+				'expected_variations' => array(
+					array(
+						'supportedBlockTypes' => array( 'core/group', 'core/columns', 'core/media-text' ),
+						'version'             => 2,
+						'title'               => 'block-style-variation-a',
+						'styles'              => array(
 							'color' => array(
-								'palette' => array(
-									'theme' => array(
-										array(
-											'slug'  => 'dark',
-											'name'  => 'Dark',
-											'color' => '#010101',
-										),
-									),
-								),
+								'background' => 'darkcyan',
+								'text'       => 'aliceblue',
+							),
+						),
+					),
+					array(
+						'supportedBlockTypes' => array( 'core/group', 'core/columns' ),
+						'version'             => 2,
+						'title'               => 'block-style-variation-b',
+						'styles'              => array(
+							'color' => array(
+								'background' => 'midnightblue',
+								'text'       => 'lightblue',
 							),
 						),
 					),
 				),
 			),
-			array(
-				'version'  => WP_Theme_JSON_Gutenberg::LATEST_SCHEMA,
-				'title'    => 'Block theme variation',
-				'settings' => array(
-					'color' => array(
-						'palette' => array(
-							'theme' => array(
-								array(
-									'slug'  => 'foreground',
-									'name'  => 'Foreground',
-									'color' => '#3F67C6',
-								),
-							),
-						),
-					),
-				),
-				'styles'   => array(
-					'blocks' => array(
-						'core/post-title' => array(
-							'typography' => array(
-								'fontWeight' => '700',
-							),
-						),
-					),
-				),
-			),
-		);
-
-		wp_recursive_ksort( $actual_settings );
-		wp_recursive_ksort( $expected_settings );
-
-		$this->assertSame(
-			$expected_settings,
-			$actual_settings
 		);
 	}
 
