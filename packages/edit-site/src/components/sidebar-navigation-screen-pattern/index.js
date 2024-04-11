9e59f77f557f9217c74f909a94f42091bd710341
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-04-11 14:05:06 +0200

    Details page for parts: always go back to patterns

--- packages/edit-site/src/components/sidebar-navigation-screen-pattern/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-pattern/index.js
@@ -4,7 +4,6 @@
 import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { pencil } from '@wordpress/icons';
-import { getQueryArgs } from '@wordpress/url';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 
 /**
@@ -17,7 +16,6 @@ import usePatternDetails from './use-pattern-details';
 import { store as editSiteStore } from '../../store';
 import { unlock } from '../../lock-unlock';
 import TemplateActions from '../template-actions';
-import { TEMPLATE_PART_POST_TYPE } from '../../utils/constants';
 
 const { useLocation, useHistory } = unlock( routerPrivateApis );
 
@@ -27,20 +25,12 @@ export default function SidebarNavigationScreenPattern() {
 	const {
 		params: { postType, postId },
 	} = location;
-	const { categoryType } = getQueryArgs( window.location.href );
 	const { setCanvasMode } = unlock( useDispatch( editSiteStore ) );
 
 	useInitEditedEntityFromURL();
 
 	const patternDetails = usePatternDetails( postType, postId );
-
-	// The absence of a category type in the query params for template parts
-	// indicates the user has arrived at the template part via the "manage all"
-	// page and the back button should return them to that list page.
-	const backPath =
-		! categoryType && postType === TEMPLATE_PART_POST_TYPE
-			? { path: '/wp_template_part/all' }
-			: { path: '/patterns' };
+	const backPath = '/patterns';
 
 	return (
 		<SidebarNavigationScreen
