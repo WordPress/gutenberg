87bf436f363b5cd707dbf8799eb9c87305539851
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-04-11 23:27:00 +0200

    Add TimeInput unit tests

---
+++ packages/components/src/date-time/time-input/test/index.tsx
@@ -0,0 +1,146 @@
+/**
+ * External dependencies
+ */
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+/**
+ * Internal dependencies
+ */
+import TimeInput from '..';
+
+describe( 'TimePicker', () => {
+	it( 'should call onChange with updated values | 24-hours format', async () => {
+		const user = userEvent.setup();
+
+		const onChangeSpy = jest.fn();
+
+		render(
+			<TimeInput hours={ 0 } minutes={ 0 } onChange={ onChangeSpy } />
+		);
+
+		const hoursInput = screen.getByLabelText( 'Hours' );
+		const minutesInput = screen.getByLabelText( 'Minutes' );
+
+		await user.clear( minutesInput );
+		await user.type( minutesInput, '35' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( { hours: 0, minutes: 35 } );
+		onChangeSpy.mockClear();
+
+		await user.clear( hoursInput );
+		await user.type( hoursInput, '12' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( {
+			hours: 12,
+			minutes: 35,
+		} );
+		onChangeSpy.mockClear();
+
+		await user.clear( hoursInput );
+		await user.type( hoursInput, '23' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( {
+			hours: 23,
+			minutes: 35,
+		} );
+		onChangeSpy.mockClear();
+
+		await user.clear( minutesInput );
+		await user.type( minutesInput, '0' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( { hours: 23, minutes: 0 } );
+	} );
+
+	it( 'should call onChange with updated values | 12-hours format', async () => {
+		const user = userEvent.setup();
+
+		const onChangeSpy = jest.fn();
+
+		render(
+			<TimeInput
+				is12Hour
+				hours={ 0 }
+				minutes={ 0 }
+				onChange={ onChangeSpy }
+			/>
+		);
+
+		const hoursInput = screen.getByLabelText( 'Hours' );
+		const minutesInput = screen.getByLabelText( 'Minutes' );
+		const amButton = screen.getByText( 'AM' );
+		const pmButton = screen.getByText( 'PM' );
+
+		expect( amButton ).toHaveClass( 'is-primary' );
+		expect( pmButton ).not.toHaveClass( 'is-primary' );
+		expect( hoursInput ).not.toHaveValue( 0 );
+		expect( hoursInput ).toHaveValue( 12 );
+
+		await user.clear( minutesInput );
+		await user.type( minutesInput, '35' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( { hours: 0, minutes: 35 } );
+		expect( amButton ).toHaveClass( 'is-primary' );
+
+		await user.clear( hoursInput );
+		await user.type( hoursInput, '12' );
+		await user.keyboard( '{Tab}' );
+
+		expect( onChangeSpy ).toHaveBeenCalledWith( { hours: 0, minutes: 35 } );
+
+		await user.click( pmButton );
+		expect( onChangeSpy ).toHaveBeenCalledWith( {
+			hours: 12,
+			minutes: 35,
+		} );
+		expect( pmButton ).toHaveClass( 'is-primary' );
+	} );
+
+	it( 'should call onChange with defined minutes steps', async () => {
+		const user = userEvent.setup();
+
+		const onChangeSpy = jest.fn();
+
+		render(
+			<TimeInput
+				hours={ 0 }
+				minutes={ 0 }
+				minutesStep={ 5 }
+				onChange={ onChangeSpy }
+			/>
+		);
+
+		const minutesInput = screen.getByLabelText( 'Minutes' );
+
+		await user.clear( minutesInput );
+		await user.keyboard( '{ArrowUp}' );
+
+		expect( minutesInput ).toHaveValue( 5 );
+
+		await user.keyboard( '{ArrowUp}' );
+		await user.keyboard( '{ArrowUp}' );
+
+		expect( minutesInput ).toHaveValue( 15 );
+
+		await user.keyboard( '{ArrowDown}' );
+
+		expect( minutesInput ).toHaveValue( 10 );
+
+		await user.clear( minutesInput );
+		await user.type( minutesInput, '44' );
+		await user.keyboard( '{Tab}' );
+
+		expect( minutesInput ).toHaveValue( 45 );
+
+		await user.clear( minutesInput );
+		await user.type( minutesInput, '51' );
+		await user.keyboard( '{Tab}' );
+
+		expect( minutesInput ).toHaveValue( 50 );
+	} );
+} );
