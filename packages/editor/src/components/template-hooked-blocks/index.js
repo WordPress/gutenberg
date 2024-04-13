252fe1834d535c3eb7cd0da143ed1838a9ffba75
Bernie Reiter <ockham@raz.or.at>
2024-04-13 13:47:08 +0200

    Merge into one function

--- packages/editor/src/components/template-hooked-blocks/index.js
+++ packages/editor/src/components/template-hooked-blocks/index.js
@@ -6,25 +6,21 @@ import { PanelBody } from '@wordpress/components';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 
-function _findAnchorBlocks( block ) {
-	let hookedBlocks = new Set();
-	if ( Array.isArray( block.attributes?.metadata?.ignoredHookedBlocks ) ) {
-		block.attributes.metadata.ignoredHookedBlocks.forEach(
-			( hookedBlock ) => {
-				hookedBlocks.add( hookedBlock );
-			}
-		);
-	}
-	block.innerBlocks.forEach( ( innerBlock ) => {
-		hookedBlocks = hookedBlocks.union( _findAnchorBlocks( innerBlock ) );
-	} );
-	return hookedBlocks;
-}
-
 function findAnchorBlocks( blocks ) {
 	let hookedBlocks = new Set();
 	blocks.forEach( ( block ) => {
-		hookedBlocks = hookedBlocks.union( _findAnchorBlocks( block ) );
+		if (
+			Array.isArray( block.attributes?.metadata?.ignoredHookedBlocks )
+		) {
+			block.attributes.metadata.ignoredHookedBlocks.forEach(
+				( hookedBlock ) => {
+					hookedBlocks.add( hookedBlock );
+				}
+			);
+		}
+		hookedBlocks = hookedBlocks.union(
+			findAnchorBlocks( block.innerBlocks )
+		);
 	} );
 	return hookedBlocks;
 }
