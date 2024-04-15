fca19815bc10fe9e06273650d9160fcc06d3d123
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2024-04-15 11:43:12 +1000

    Fix typo

--- lib/block-supports/block-style-variations.php
+++ lib/block-supports/block-style-variations.php
@@ -37,7 +37,7 @@ function gutenberg_get_block_style_variation_name_from_class( $class_string ) {
 /**
  * Render the block style variation's styles.
  *
- * In the case of nested blocks with variations applies, we want the parent
+ * In the case of nested blocks with variations applied, we want the parent
  * variation's styles to be rendered before their descendants. This solves the
  * issue of a block type being styled in both the parent and descendant: we want
  * the descendant style to take priority, and this is done by loading it after,
