ef8a89b05df2c3cba4bb9b0943558a82ae7bb265
Jorge Costa <jorge.costa@developer.pt>
2024-04-17 14:42:45 +0100

    Fix: Use core instead of core/editor on normalizeComplementaryAreaScope.

--- packages/interface/src/store/deprecated.js
+++ packages/interface/src/store/deprecated.js
@@ -7,10 +7,10 @@ export function normalizeComplementaryAreaScope( scope ) {
 	if ( [ 'core/edit-post', 'core/edit-site' ].includes( scope ) ) {
 		deprecated( `${ scope } interface scope`, {
 			alternative:
-				'Use the core/editor interface scope instead. core/edit-post and core/edit-site are merging.',
+				'Use the core interface scope instead. core/edit-post and core/edit-site are merging.',
 			version: '6.6',
 		} );
-		return 'core/editor';
+		return 'core';
 	}
 
 	return scope;
