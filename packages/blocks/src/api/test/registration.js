be32c0bd4762f883bfb24de7b41730aa8a2cf8c6
George Mamadashvili <georgemamadashvili@gmail.com>
2024-04-18 12:31:14 +0400

    Add unit test

--- packages/blocks/src/api/test/registration.js
+++ packages/blocks/src/api/test/registration.js
@@ -398,6 +398,39 @@ describe( 'blocks', () => {
 			} );
 		} );
 
+		it( 'should merge settings provided by server and client', () => {
+			const blockName = 'core/test-block-with-merged-settings';
+			unstable__bootstrapServerSideBlockDefinitions( {
+				[ blockName ]: {
+					variations: [ { name: 'foo', label: 'Foo' } ],
+				},
+			} );
+
+			const blockType = {
+				title: 'block settings merge',
+				variations: [ { name: 'bar', label: 'Bar' } ],
+			};
+			registerBlockType( blockName, blockType );
+			expect( getBlockType( blockName ) ).toEqual( {
+				name: blockName,
+				save: expect.any( Function ),
+				title: 'block settings merge',
+				icon: { src: BLOCK_ICON_DEFAULT },
+				attributes: {},
+				providesContext: {},
+				usesContext: [],
+				keywords: [],
+				selectors: {},
+				supports: {},
+				styles: [],
+				variations: [
+					{ name: 'foo', label: 'Foo' },
+					{ name: 'bar', label: 'Bar' },
+				],
+				blockHooks: {},
+			} );
+		} );
+
 		// This test can be removed once the polyfill for blockHooks gets removed.
 		it( 'should polyfill blockHooks using metadata on the client when not set on the server', () => {
 			const blockName = 'tests/hooked-block';
