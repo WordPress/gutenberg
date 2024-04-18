6156f07f74d5f146876328b85f1a091c79219681
ramon <ramonjd@gmail.com>
2024-04-18 15:53:35 +1000

    Getting URI from themes API response

--- lib/block-editor-settings.php
+++ lib/block-editor-settings.php
@@ -76,13 +76,6 @@ function gutenberg_get_block_editor_settings( $settings ) {
 		}
 	}
 
-	$current_theme            = wp_get_theme();
-	$settings['currentTheme'] = array(
-		// Directory of the current theme. Will be the child theme directory if a child theme is active.
-		'directoryURI' => $current_theme->get_stylesheet_directory_uri(),
-		// `__unstableIsBlockBasedTheme` could also be relocated here, e.g., 'isBlockTheme' => $current_theme->is_block_theme().
-	);
-
 	$settings['styles'] = array_merge( $global_styles, get_block_editor_theme_styles() );
 
 	$settings['__experimentalFeatures'] = gutenberg_get_global_settings();
