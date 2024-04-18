b1103b40b6c468d4a4712ef11d071177c8b1b514
Riad Benguella <benguella@gmail.com>
2024-04-18 14:12:41 +0100

    Fix deprecations

--- packages/interface/src/store/deprecated.js
+++ packages/interface/src/store/deprecated.js
@@ -6,8 +6,8 @@ import deprecated from '@wordpress/deprecated';
 export function normalizeComplementaryAreaScope( scope ) {
 	if ( [ 'core/edit-post', 'core/edit-site' ].includes( scope ) ) {
 		deprecated( `${ scope } interface scope`, {
-			alternative:
-				'Use the core interface scope instead. core/edit-post and core/edit-site are merging.',
+			alternative: 'core interface scope',
+			hint: 'core/edit-post and core/edit-site are merging.',
 			version: '6.6',
 		} );
 		return 'core';
@@ -19,15 +19,15 @@ export function normalizeComplementaryAreaScope( scope ) {
 export function normalizeComplementaryAreaName( scope, name ) {
 	if ( scope === 'core' && name === 'edit-site/template' ) {
 		deprecated( `edit-site/template sidebar`, {
-			alternative: 'edit-post/document.',
+			alternative: 'edit-post/document',
 			version: '6.6',
 		} );
 		return 'edit-post/document';
 	}
 
 	if ( scope === 'core' && name === 'edit-site/block-inspector' ) {
-		deprecated( `edit-site/template sidebar`, {
-			alternative: 'edit-post/document.',
+		deprecated( `edit-site/block-inspector sidebar`, {
+			alternative: 'edit-post/block',
 			version: '6.6',
 		} );
 		return 'edit-post/block';
