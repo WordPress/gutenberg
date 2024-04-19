058cc37096e567f8e0dfb4dbac92f1038cbda836
Jarda Snajdr <jsnajdr@gmail.com>
2024-04-19 11:00:32 +0200

    SlotFill: replace valtio with custom ObservableMap (#60879)
    * SlotFill: replace valtio with custom ObservableMap
    * Fill PR number in changelog
    * Use Set for listeners
    
    Co-authored-by: jsnajdr <jsnajdr@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: jeryj <jeryj@git.wordpress.org>

--- packages/components/src/slot-fill/bubbles-virtually/use-slot-fills.ts
+++ packages/components/src/slot-fill/bubbles-virtually/use-slot-fills.ts
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { useSnapshot } from 'valtio';
-
-/**
  * WordPress dependencies
  */
 import { useContext } from '@wordpress/element';
@@ -13,12 +8,9 @@ import { useContext } from '@wordpress/element';
  */
 import SlotFillContext from './slot-fill-context';
 import type { SlotKey } from '../types';
+import { useObservableValue } from './observable-map';
 
 export default function useSlotFills( name: SlotKey ) {
 	const registry = useContext( SlotFillContext );
-	const fills = useSnapshot( registry.fills, { sync: true } );
-	// The important bit here is that this call ensures that the hook
-	// only causes a re-render if the "fills" of a given slot name
-	// change, not any fills.
-	return fills.get( name );
+	return useObservableValue( registry.fills, name );
 }
