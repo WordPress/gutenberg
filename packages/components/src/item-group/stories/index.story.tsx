25650294254f5ab78de4882b29228614d82f7fac
Marcelo Serpa <boss@fullofcaffeine.com>
2024-04-18 18:18:15 -0600

    Remove "experimental" designation for `ItemGroup`

--- packages/components/src/item-group/stories/index.story.tsx
+++ packages/components/src/item-group/stories/index.story.tsx
@@ -15,7 +15,7 @@ const meta: Meta< typeof ItemGroup > = {
 	component: ItemGroup,
 	// @ts-expect-error - See https://github.com/storybookjs/storybook/issues/23170
 	subcomponents: { Item },
-	title: 'Components (Experimental)/ItemGroup',
+	title: 'Components/ItemGroup',
 	argTypes: {
 		as: { control: { type: null } },
 		children: { control: { type: null } },
