16fd3934444164f703f13ebc2bf134642f27043b
Kai Hao <kai@kaihao.dev>
2024-04-19 17:52:39 +0800

    Try to fix RN tests

--- packages/patterns/src/index.native.js
+++ packages/patterns/src/index.native.js
@@ -1,11 +1,12 @@
 /**
  * Internal dependencies
  */
-import './store';
+export { store } from './store';
 import { lock } from './lock-unlock';
 
 export const privateApis = {};
 lock( privateApis, {
 	CreatePatternModal: () => null,
 	PatternsMenuItems: () => null,
+	RenameBlockModalControl: () => null,
 } );
