5192d86d60a1ec1f130637b5b21d87dcbd0f4c7b
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-04-22 14:35:18 +0200

    Inline strings

--- packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/index.js
@@ -11,14 +11,6 @@ import SidebarNavigationScreen from '../sidebar-navigation-screen';
 import { unlock } from '../../lock-unlock';
 import DataviewsTemplatesSidebarContent from './content';
 
-const config = {
-	title: __( 'Manage templates' ),
-	description: __(
-		'Create new templates, or reset any customizations made to the templates supplied by your theme.'
-	),
-	contentTitle: __( 'All templates' ),
-};
-
 const { useLocation } = unlock( routerPrivateApis );
 
 export default function SidebarNavigationScreenTemplatesBrowse() {
@@ -28,13 +20,14 @@ export default function SidebarNavigationScreenTemplatesBrowse() {
 
 	return (
 		<SidebarNavigationScreen
-			title={ config.title }
-			description={ config.description }
-			backPath={ config.backPath }
+			title={ __( 'Manage templates' ) }
+			description={ __(
+				'Create new templates, or reset any customizations made to the templates supplied by your theme.'
+			) }
 			content={
 				<DataviewsTemplatesSidebarContent
 					activeView={ activeView }
-					title={ config.contentTitle }
+					title={ __( 'All templates' ) }
 				/>
 			}
 		/>
