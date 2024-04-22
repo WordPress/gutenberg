83b757228e7d705ea0ec108c8010bc67b5f814cc
Andrei Draganescu <me@andreidraganescu.info>
2024-04-22 10:18:46 +0300

    Update packages/block-editor/src/components/inner-blocks/index.js
    Co-authored-by: Ben Dwyer <ben@scruffian.com>

--- packages/block-editor/src/components/inner-blocks/index.js
+++ packages/block-editor/src/components/inner-blocks/index.js
@@ -217,7 +217,7 @@ export function useInnerBlocksProps( props = {}, options = {} ) {
 
 			// in zoom out mode, we want to disable the drop zone for the sections.
 			// the inner blocks belonging to the section drop zone is
-			// already disabled by the blocks themselves being disabled
+			// already disabled by the blocks themselves being disabled.
 			let _isDropZoneDisabled = blockEditingMode === 'disabled';
 			if ( __unstableGetEditorMode() === 'zoom-out' ) {
 				const { sectionRootClientId } = unlock( getSettings() );
