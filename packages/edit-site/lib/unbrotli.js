31c9281cb530a9e9e26854ce7f7d434a9219c64b
Matias Benedetto <matias.benedetto@gmail.com>
2024-04-22 17:56:37 -0300

    add lib-font credits and license in source code

--- packages/edit-site/lib/unbrotli.js
+++ packages/edit-site/lib/unbrotli.js
@@ -1,3 +1,33 @@
+/**
+ * Credits:
+ *
+ * lib-font
+ * https://github.com/Pomax/lib-font
+ * https://github.com/Pomax/lib-font/blob/master/lib/unbrotli.js
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 pomax@nihongoresources.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /* eslint eslint-comments/no-unlimited-disable: 0 */
 /* eslint-disable */
 ( function ( f ) {
@@ -332,12 +362,11 @@
 						0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
 						1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
 						0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
-						1, 0, 1, 0, 1, 0, 1, /* UTF8 lead byte range. */
+						1, 0, 1, 0, 1, 0, 1 /* UTF8 lead byte range. */, 2, 3,
 						2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2,
 						3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3,
 						2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2,
-						3, 2, 3, 2, 3, 2, 3,
-						/* CONTEXT_UTF8 second last byte. */
+						3, 2, 3, 2, 3, /* CONTEXT_UTF8 second last byte. */
 						/* ASCII range. */
 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
@@ -351,11 +380,10 @@
 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-						0, 0, 0, 0, /* UTF8 lead byte range. */
-						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2,
+						0, 0, 0, 0 /* UTF8 lead byte range. */, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2,
 						2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-						2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-						/* CONTEXT_SIGNED, second last byte. */
+						2, 2, 2, 2, 2, /* CONTEXT_SIGNED, second last byte. */
 						0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
 						2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
 						2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -447,9 +475,8 @@
 
 					exports.lookupOffsets = new Uint16Array( [
 						/* CONTEXT_LSB6 */
-						1024, 1536, /* CONTEXT_MSB6 */
-						1280, 1536, /* CONTEXT_UTF8 */
-						0, 256, /* CONTEXT_SIGNED */
+						1024, 1536 /* CONTEXT_MSB6 */, 1280,
+						1536 /* CONTEXT_UTF8 */, 0, 256 /* CONTEXT_SIGNED */,
 						768, 512,
 					] );
 				},
