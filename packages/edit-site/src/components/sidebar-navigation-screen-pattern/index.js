2dbe1c5d15ad27deba110c825a3c1852321c440b
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-04-22 14:41:09 +0200

    Document backpath

--- packages/edit-site/src/components/sidebar-navigation-screen-pattern/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-pattern/index.js
@@ -35,6 +35,19 @@ export default function SidebarNavigationScreenPattern() {
 			.supportsTemplatePartsMode;
 	}, [] );
 
+	/**
+	 * This sidebar needs to temporarily accomodate two different "URLs" backpaths:
+	 *
+	 * 1. path = /patterns
+	 *    Block based themes. Also classic themes can access this URL, though it's not linked anywhere.
+	 *
+	 * 2. path = /wp_template_part/all
+	 *    Classic themes with support for block-template-parts. We need to list only Template Parts in this case.
+	 *    The URL is accessible from the Appearance > Template Parts menu.
+	 *
+	 * Depending on whether the theme supports block-template-parts, we go back to Patterns or Template screens.
+	 * This is temporary. We aim to consolidate to /patterns.
+	 */
 	const backPath =
 		isTemplatePartsMode && postType === 'wp_template_part'
 			? { path: '/wp_template_part/all' }
--- packages/edit-site/src/components/sidebar-navigation-screen-patterns/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-patterns/index.js
@@ -137,7 +137,7 @@ export default function SidebarNavigationScreenPatterns() {
 	 *    Block based themes. Also classic themes can access this URL, though it's not linked anywhere.
 	 *
 	 * 2. path = /wp_template_part/all
-	 *    Classic themes with support for block-template-parts). We need to list only] Template Parts in this case.
+	 *    Classic themes with support for block-template-parts. We need to list only Template Parts in this case.
 	 *    The URL is accessible from the Appearance > Template Parts menu.
 	 *
 	 * This is temporary. We aim to consolidate to /patterns.
