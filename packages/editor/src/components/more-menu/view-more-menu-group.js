4ac6137aed5aece36ac2af49e26ba9d3295fdbe0
Riad Benguella <benguella@gmail.com>
2024-04-22 09:05:13 +0100

    Fix mobile unit tests

--- packages/editor/src/components/more-menu/view-more-menu-group.js
+++ packages/editor/src/components/more-menu/view-more-menu-group.js
@@ -2,9 +2,10 @@
  * WordPress dependencies
  */
 import { createSlotFill } from '@wordpress/components';
+import { Platform } from '@wordpress/element';
 
 const { Fill: ViewMoreMenuGroup, Slot } = createSlotFill(
-	Symbol( 'ToolsMoreMenuGroup' )
+	Platform.OS === 'web' ? Symbol( 'ViewMoreMenuGroup' ) : 'ViewMoreMenuGroup'
 );
 
 ViewMoreMenuGroup.Slot = ( { fillProps } ) => <Slot fillProps={ fillProps } />;
