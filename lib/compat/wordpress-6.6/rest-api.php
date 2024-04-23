968d4d28ab030703f595a757d06b272b22894722
huubl <50170696+huubl@users.noreply.github.com>
2024-04-23 20:56:14 +0200

    Move REST API functions to rest-api.php

--- lib/compat/wordpress-6.6/rest-api.php
+++ lib/compat/wordpress-6.6/rest-api.php
@@ -29,3 +29,53 @@ if ( ! function_exists( 'wp_api_template_access_controller' ) ) {
 	}
 }
 add_filter( 'register_post_type_args', 'wp_api_template_access_controller', 10, 2 );
+
+/**
+ * Registers the REST route for retrieving the post classes of a post.
+ */
+function gutenberg_register_post_class_rest_route() {
+	register_rest_route(
+		'wp/v2',
+		'/postclass/(?P<id>\d+)',
+		array(
+			'methods'             => WP_REST_Server::READABLE,
+			'permission_callback' => '__return_true',
+			'callback'            => 'gutenberg_get_post_classes',
+		)
+	);
+}
+add_action( 'rest_api_init', 'gutenberg_register_post_class_rest_route' );
+
+/**
+ * Callback function for the REST route.
+ *
+ * @param WP_REST_Request $request Current request.
+ */
+function gutenberg_get_post_classes( WP_REST_Request $request ) {
+	$id               = (int) $request['id'];
+	$post             = get_post( $id );
+	$post_type        = get_post_type( $post );
+	$valid_post_types = get_post_types(
+		array(
+			'public'       => true,
+			'show_in_rest' => true,
+		)
+	);
+
+	if ( ! in_array( $post_type, $valid_post_types, true ) ) {
+		return new WP_Error( 'rest_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
+	}
+
+	$controller = new WP_REST_Posts_Controller( $post_type );
+
+	$check = $controller->get_item_permissions_check( $request );
+
+	if ( true !== $check ) {
+		return $check;
+	}
+
+	// Get the post classes and return them
+	$post_classes = get_post_class( '', $id );
+
+	return $post_classes;
+}
