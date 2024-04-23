6a677005d508493931d7a1aa5f9c63f76632d4f4
Ella <ella@vandurpe.com>
2024-04-23 15:40:06 +0300

    useMatchMedia: cache queries

--- packages/compose/src/hooks/use-media-query/index.js
+++ packages/compose/src/hooks/use-media-query/index.js
@@ -3,6 +3,8 @@
  */
 import { useMemo, useSyncExternalStore } from '@wordpress/element';
 
+const matchMediaCache = new Map();
+
 /**
  * A new MediaQueryList object for the media query
  *
@@ -10,12 +12,21 @@ import { useMemo, useSyncExternalStore } from '@wordpress/element';
  * @return {MediaQueryList|null} A new object for the media query
  */
 function getMediaQueryList( query ) {
+	if ( ! query ) {
+		return null;
+	}
+
+	if ( matchMediaCache.has( query ) ) {
+		return matchMediaCache.get( query );
+	}
+
 	if (
-		query &&
 		typeof window !== 'undefined' &&
 		typeof window.matchMedia === 'function'
 	) {
-		return window.matchMedia( query );
+		const match = window.matchMedia( query );
+		matchMediaCache.set( query, match );
+		return match;
 	}
 
 	return null;
