9aa524a20990af224bc11d3e6c601d61fee02126
Ella <ella@vandurpe.com>
2024-04-25 13:17:48 +0300

    Only add for site editor

--- lib/compat/wordpress-6.6/rest-api.php
+++ lib/compat/wordpress-6.6/rest-api.php
@@ -30,11 +30,15 @@ if ( ! function_exists( 'wp_api_template_access_controller' ) ) {
 }
 add_filter( 'register_post_type_args', 'wp_api_template_access_controller', 10, 2 );
 
-function gutenberg_block_editor_preload_paths_6_6( $paths ) {
-	$paths[] = array( '/wp/v2/settings', 'OPTIONS' );
-	$paths[] = '/wp/v2/settings';
-	$paths[] = array( '/wp/v2/templates', 'OPTIONS' );
-	$paths[] = '/wp/v2/types?context=edit';
+function gutenberg_block_editor_preload_paths_6_6( $paths, $context ) {
+	if ( $context->name === 'core/edit-site' ) {
+		// When merging back to core, these should be added here:
+		// https://github.com/WordPress/wordpress-develop/blob/7159243c090e429a7d2a1fd2a9509e323f67a39d/src/wp-admin/site-editor.php#L90-L117
+		$paths[] = array( '/wp/v2/settings', 'OPTIONS' );
+		$paths[] = '/wp/v2/settings';
+		$paths[] = array( '/wp/v2/templates', 'OPTIONS' );
+		$paths[] = '/wp/v2/types?context=edit';
+	}
 	return $paths;
 }
-add_filter( 'block_editor_rest_api_preload_paths', 'gutenberg_block_editor_preload_paths_6_6', 10 );
+add_filter( 'block_editor_rest_api_preload_paths', 'gutenberg_block_editor_preload_paths_6_6', 10, 2 );
