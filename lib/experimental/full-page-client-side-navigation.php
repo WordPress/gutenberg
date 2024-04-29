bc715925c4c75051724444224caa1ac0f4b05986
Weston Ruter <westonruter@google.com>
2024-04-29 10:47:24 -0700

    Inject client-side navigation directives via Tag Processor and output buffering

--- lib/experimental/full-page-client-side-navigation.php
+++ lib/experimental/full-page-client-side-navigation.php
@@ -42,16 +42,57 @@ add_filter( 'render_block_data', '_gutenberg_add_enhanced_pagination_to_query_bl
  * @return array The same block content with the directives needed.
  */
 function _gutenberg_add_client_side_navigation_directives( $content ) {
-	$p = new WP_HTML_Tag_Processor( $content );
-	// Hack to add the necessary directives to the body tag.
-	// TODO: Find a proper way to add directives to the body tag.
 	static $body_interactive_added;
 	if ( ! $body_interactive_added ) {
 		$body_interactive_added = true;
-		return (string) $p . '<body data-wp-interactive="core/experimental" data-wp-context="{}">';
+		add_filter( 'gutenberg_template_output_buffer', static function ( $html ) {
+			$p = new WP_HTML_Tag_Processor( $html );
+			if ( $p->next_tag( array( 'tag_name' => 'BODY' ) ) ) {
+				$p->set_attribute( 'data-wp-interactive', 'core/experimental' );
+				$p->set_attribute( 'data-wp-context', '{}' );
+				$html = $p->get_updated_html();
+			}
+			return $html;
+		} );
 	}
-	return (string) $p;
+	return $content;
 }
 
 // TODO: Explore moving this to the server directive processing.
 add_filter( 'render_block', '_gutenberg_add_client_side_navigation_directives' );
+
+/**
+ * Starts output buffering at the end of the 'template_include' filter.
+ *
+ * This is to implement #43258 in core.
+ *
+ * This is a hack which would eventually be replaced with something like this in wp-includes/template-loader.php:
+ *
+ *          $template = apply_filters( 'template_include', $template );
+ *     +    ob_start( 'wp_template_output_buffer_callback' );
+ *          if ( $template ) {
+ *              include $template;
+ *          } elseif ( current_user_can( 'switch_themes' ) ) {
+ *
+ * @since 0.1.0
+ * @link https://core.trac.wordpress.org/ticket/43258
+ *
+ * @param string $passthrough Value for the template_include filter which is passed through.
+ *
+ * @return string Unmodified value of $passthrough.
+ */
+function _gutenberg_buffer_template_output( string $passthrough ): string {
+	ob_start(
+		static function ( string $output ): string {
+			/**
+			 * Filters the template output buffer prior to sending to the client.
+			 *
+			 * @param string $output Output buffer.
+			 * @return string Filtered output buffer.
+			 */
+			return (string) apply_filters( 'gutenberg_template_output_buffer', $output );
+		}
+	);
+	return $passthrough;
+}
+add_filter( 'template_include', '_gutenberg_buffer_template_output', PHP_INT_MAX );
