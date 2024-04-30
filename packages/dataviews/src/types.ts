db5e39180b3aadfb965941545c22593fe055e494
Riad Benguella <benguella@gmail.com>
2024-04-30 13:50:09 +0100

    DataViews: More dataviews types (#61193)
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: sirreal <jonsurrell@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>

--- packages/dataviews/src/types.ts
+++ packages/dataviews/src/types.ts
@@ -11,9 +11,11 @@ interface Option {
 }
 
 interface filterByConfig {
-	operators?: string[];
+	operators?: Operator[];
 	isPrimary?: boolean;
 }
+
+type Operator = 'is' | 'isNot' | 'isAny' | 'isNone' | 'isAll' | 'isNotAll';
 
 export interface Field {
 	/**
@@ -40,40 +42,103 @@ export interface Field {
 	/**
 	 * The width of the field column.
 	 */
-	width?: string | number;
+	width: string | number | undefined;
 
 	/**
 	 * The minimum width of the field column.
 	 */
-	maxWidth?: string | number;
+	maxWidth: string | number | undefined;
 
 	/**
 	 * The maximum width of the field column.
 	 */
-	minWidth?: string | number;
+	minWidth: string | number | undefined;
 
 	/**
 	 * Whether the field is sortable.
 	 */
-	enableSorting?: boolean;
+	enableSorting: boolean | undefined;
 
 	/**
 	 * Whether the field is searchable.
 	 */
-	enableGlobalSearch?: boolean;
+	enableGlobalSearch: boolean | undefined;
 
 	/**
 	 * Whether the field is filterable.
 	 */
-	enableHiding?: boolean;
+	enableHiding: boolean | undefined;
 
 	/**
 	 * The list of options to pick from when using the field as a filter.
 	 */
-	elements?: Option[];
+	elements: Option[] | undefined;
 
 	/**
 	 * Filter config for the field.
 	 */
-	filterBy?: filterByConfig;
+	filterBy: filterByConfig | undefined;
+}
+
+export type NormalizedField = Required< Field >;
+
+export type Data = Item[];
+
+export interface Filter {
+	/**
+	 * The field to filter by.
+	 */
+	field: string;
+
+	/**
+	 * The operator to use.
+	 */
+	operator: Operator;
+
+	/**
+	 * The value to filter by.
+	 */
+	value: any;
+}
+
+export interface View {
+	/**
+	 * The layout of the view.
+	 */
+	type: string;
+
+	/**
+	 * The global search term.
+	 */
+	search?: string;
+
+	/**
+	 * The filters to apply.
+	 */
+	filters: Filter[];
+
+	/**
+	 * The sorting configuration.
+	 */
+	sort?: {
+		/**
+		 * The field to sort by.
+		 */
+		field: string;
+
+		/**
+		 * The direction to sort by.
+		 */
+		direction: string;
+	};
+
+	/**
+	 * The active page
+	 */
+	page?: number;
+
+	/**
+	 * The number of items per page
+	 */
+	perPage?: number;
 }
