e118f77711be7af378292ef2b2e0f350b5d2b825
MaggieCabrera <maggie.cabrera@automattic.com>
2024-05-02 10:42:46 +0200

    add motion div to the inserter

--- packages/block-editor/src/components/block-tools/zoom-out-mode-inserters.js
+++ packages/block-editor/src/components/block-tools/zoom-out-mode-inserters.js
@@ -1,7 +1,14 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { useSelect } from '@wordpress/data';
+import { __unstableMotion as motion } from '@wordpress/components';
+import { useReducedMotion } from '@wordpress/compose';
 import { useEffect, useState } from '@wordpress/element';
 
 /**
@@ -31,6 +38,41 @@ function ZoomOutModeInserters( { __unstableContentRef } ) {
 		};
 	}, [] );
 
+	const toggleProps = {
+		type: 'button',
+		icon: undefined,
+	};
+
+	const disableMotion = useReducedMotion();
+	const lineVariants = {
+		// Initial position starts from the center and invisible.
+		start: {
+			opacity: 0,
+			scale: 0,
+		},
+		// The line expands to fill the container. If the inserter is visible it
+		// is delayed so it appears orchestrated.
+		rest: {
+			opacity: 1,
+			scale: 1,
+			transition: { delay: 0.5, type: 'tween' },
+		},
+		hover: {
+			opacity: 1,
+			scale: 1,
+			transition: { delay: 0.5, type: 'tween' },
+		},
+	};
+	const inserterVariants = {
+		start: {
+			scale: disableMotion ? 1 : 0,
+		},
+		rest: {
+			scale: 1,
+			transition: { delay: 0.4, type: 'tween' },
+		},
+	};
+
 	if ( ! isReady ) {
 		return null;
 	}
@@ -46,13 +88,35 @@ function ZoomOutModeInserters( { __unstableContentRef } ) {
 				nextClientId={ blockOrder[ index + 1 ] }
 				__unstableContentRef={ __unstableContentRef }
 			>
-				<div className="block-editor-block-list__insertion-point-inserter is-with-inserter">
-					<Inserter
-						position="bottom center"
-						clientId={ blockOrder[ index + 1 ] }
-						__experimentalIsQuick
+				<motion.div
+					layout={ ! disableMotion }
+					initial={ disableMotion ? 'rest' : 'start' }
+					animate="rest"
+					whileHover="hover"
+					whileTap="pressed"
+					exit="start"
+					className="block-editor-block-list__insertion-point is-vertical is-with-inserter"
+				>
+					<motion.div
+						variants={ lineVariants }
+						className="block-editor-block-list__insertion-point-indicator"
+						data-testid="block-list-insertion-point-indicator"
 					/>
-				</div>
+
+					<motion.div
+						variants={ inserterVariants }
+						className={ classnames(
+							'block-editor-block-list__insertion-point-inserter'
+						) }
+					>
+						<Inserter
+							position="bottom center"
+							clientId={ blockOrder[ index + 1 ] }
+							__experimentalIsQuick
+							{ ...toggleProps }
+						/>
+					</motion.div>
+				</motion.div>
 			</BlockPopoverInbetween>
 		);
 	} );
