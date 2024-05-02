2e97b30d792946877769ad51659a98785ed04f3d
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-05-02 21:10:46 +0900

    Reduce nesting level

--- lib/compat/wordpress-6.6/compat.php
+++ lib/compat/wordpress-6.6/compat.php
@@ -16,13 +16,15 @@ function gutenberg_change_patterns_link_and_remove_template_parts_submenu_item()
 	if ( ! wp_is_block_theme() ) {
 		global $submenu;
 
-		if ( ! empty( $submenu['themes.php'] ) ) {
-			foreach ( $submenu['themes.php'] as $key => $item ) {
-				if ( 'edit.php?post_type=wp_block' === $item[2] ) {
-					$submenu['themes.php'][ $key ][2] = 'site-editor.php?path=/patterns';
-				} elseif ( 'site-editor.php?path=/wp_template_part/all' === $item[2] ) {
-					unset( $submenu['themes.php'][ $key ] );
-				}
+		if ( empty( $submenu['themes.php'] ) ) {
+			return;
+		}
+
+		foreach ( $submenu['themes.php'] as $key => $item ) {
+			if ( 'edit.php?post_type=wp_block' === $item[2] ) {
+				$submenu['themes.php'][ $key ][2] = 'site-editor.php?path=/patterns';
+			} elseif ( 'site-editor.php?path=/wp_template_part/all' === $item[2] ) {
+				unset( $submenu['themes.php'][ $key ] );
 			}
 		}
 	}
