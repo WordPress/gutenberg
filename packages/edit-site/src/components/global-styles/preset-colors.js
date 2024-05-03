58169d9918999f484a553773bb51c86b485b5174
Ben Dwyer <ben@scruffian.com>
2024-05-03 16:05:32 +0100

    remove animation

--- packages/edit-site/src/components/global-styles/preset-colors.js
+++ packages/edit-site/src/components/global-styles/preset-colors.js
@@ -1,9 +1,4 @@
 /**
- * WordPress dependencies
- */
-import { __unstableMotion as motion } from '@wordpress/components';
-
-/**
  * Internal dependencies
  */
 import { useStylesPreviewColors } from './hooks';
@@ -11,23 +6,12 @@ import { useStylesPreviewColors } from './hooks';
 export default function PresetColors() {
 	const { paletteColors } = useStylesPreviewColors();
 	return paletteColors.slice( 0, 5 ).map( ( { slug, color }, index ) => (
-		<motion.div
+		<div
 			key={ `${ slug }-${ index }` }
 			style={ {
 				flexGrow: 1,
 				height: '100%',
 				background: color,
-			} }
-			animate={ {
-				scale: 1,
-				opacity: 1,
-			} }
-			initial={ {
-				scale: 0.1,
-				opacity: 0,
-			} }
-			transition={ {
-				delay: index === 1 ? 0.2 : 0.1,
 			} }
 		/>
 	) );
