426307e00176a9b577e5cc5ecebc8e9ac933ea9c
Ella <ella@vandurpe.com>
2024-05-07 16:55:50 +0900

    Make it work :)

--- packages/block-editor/src/components/block-tools/zoom-out-mode-inserters.js
+++ packages/block-editor/src/components/block-tools/zoom-out-mode-inserters.js
@@ -15,52 +15,25 @@ import BlockPopoverInbetween from '../block-popover/inbetween';
 import { store as blockEditorStore } from '../../store';
 import { unlock } from '../../lock-unlock';
 
-function ZoomOutModeInserters( { __unstableContentRef } ) {
-	const [ isReady, setIsReady ] = useState( false );
+function Inserter( {
+	previousClientId,
+	nextClientId,
+	sectionRootClientId,
+	disableMotion,
+	__unstableContentRef,
+	index,
+} ) {
 	const { setInserterIsOpened, insertionIndex } = useSelect(
 		( select ) => {
-			const { getSettings, getBlockIndex, getBlockCount } =
-				select( blockEditorStore );
+			const { getSettings, getBlockCount } = select( blockEditorStore );
 			const settings = getSettings();
-			const index = getBlockIndex( clientId );
-			const blockCount = getBlockCount();
-
 			return {
 				setInserterIsOpened: settings.__experimentalSetIsInserterOpened,
-				insertionIndex: index === -1 ? blockCount : index,
+				insertionIndex: index === -1 ? getBlockCount() : index,
 			};
 		},
-		[ clientId ]
+		[ index ]
 	);
-	const blockOrder = useSelect( ( select ) => {
-		const { sectionRootClientId } = unlock(
-			select( blockEditorStore ).getSettings()
-		);
-		return select( blockEditorStore ).getBlockOrder( sectionRootClientId );
-	}, [] );
-
-	// Defer the initial rendering to avoid the jumps due to the animation.
-	useEffect( () => {
-		const timeout = setTimeout( () => {
-			setIsReady( true );
-		}, 500 );
-		return () => {
-			clearTimeout( timeout );
-		};
-	}, [] );
-
-	useEffect( () => {
-		if ( setInserterIsOpened ) {
-			setInserterIsOpened( false );
-		}
-	}, [ setInserterIsOpened ] );
-
-	const disableMotion = useReducedMotion();
-
-	if ( ! isReady ) {
-		return null;
-	}
-
 	const lineVariants = {
 		// Initial position starts from the center and invisible.
 		start: {
@@ -96,55 +69,113 @@ function ZoomOutModeInserters( { __unstableContentRef } ) {
 		'Add pattern',
 		'Generic label for pattern inserter button'
 	);
+	return (
+		<BlockPopoverInbetween
+			previousClientId={ previousClientId }
+			nextClientId={ nextClientId }
+			__unstableContentRef={ __unstableContentRef }
+			className="block-editor-button-pattern-inserter"
+		>
+			<motion.div
+				layout={ ! disableMotion }
+				initial={ disableMotion ? 'rest' : 'start' }
+				animate="rest"
+				whileHover="hover"
+				whileTap="pressed"
+				exit="start"
+				className="block-editor-block-list__insertion-point is-vertical is-pattern-inserter"
+			>
+				<motion.div
+					variants={ lineVariants }
+					className="block-editor-block-list__insertion-point-indicator"
+					data-testid="block-list-insertion-point-indicator"
+				/>
+				<motion.div
+					variants={ patternInserterVariants }
+					className="block-editor-block-list__insertion-point-inserter"
+				>
+					<Button
+						variant="primary"
+						icon={ plus }
+						size="compact"
+						className="block-editor-button-pattern-inserter__button"
+						onClick={ () => {
+							setInserterIsOpened( {
+								rootClientId: sectionRootClientId,
+								insertionIndex,
+								tab: 'patterns',
+								category: 'all',
+							} );
+						} }
+						label={ label }
+					/>
+				</motion.div>
+			</motion.div>
+		</BlockPopoverInbetween>
+	);
+}
+
+function ZoomOutModeInserters( { __unstableContentRef } ) {
+	const [ isReady, setIsReady ] = useState( false );
+	// const { setInserterIsOpened } = useSelect( ( select ) => {
+	// 	const { getSettings, getBlockIndex, getBlockCount } =
+	// 		select( blockEditorStore );
+	// 	const settings = getSettings();
+	// 	const index = getBlockIndex( clientId );
+	// 	const blockCount = getBlockCount();
+
+	// 	return {
+	// 		setInserterIsOpened: settings.__experimentalSetIsInserterOpened,
+	// 		insertionIndex: index === -1 ? blockCount : index,
+	// 	};
+	// }, [] );
+	const { blockOrder, sectionRootClientId } = useSelect( ( select ) => {
+		const { sectionRootClientId: root } = unlock(
+			select( blockEditorStore ).getSettings()
+		);
+		return {
+			blockOrder: select( blockEditorStore ).getBlockOrder( root ),
+			sectionRootClientId: root,
+		};
+	}, [] );
+
+	// Defer the initial rendering to avoid the jumps due to the animation.
+	useEffect( () => {
+		const timeout = setTimeout( () => {
+			setIsReady( true );
+		}, 500 );
+		return () => {
+			clearTimeout( timeout );
+		};
+	}, [] );
+
+	// useEffect( () => {
+	// 	if ( setInserterIsOpened ) {
+	// 		setInserterIsOpened( false );
+	// 	}
+	// }, [ setInserterIsOpened ] );
+
+	const disableMotion = useReducedMotion();
+
+	if ( ! isReady ) {
+		return null;
+	}
 
 	return [ undefined, ...blockOrder ].map( ( clientId, index ) => {
 		if ( index === blockOrder.length - 1 ) {
 			return null;
 		}
 		return (
-			<BlockPopoverInbetween
+			<Inserter
 				key={ index }
 				previousClientId={ clientId }
 				nextClientId={ blockOrder[ index ] }
+				sectionRootClientId={ sectionRootClientId }
+				disableMotion={ disableMotion }
+				index={ index }
+				blockOrder={ blockOrder }
 				__unstableContentRef={ __unstableContentRef }
-				className="block-editor-button-pattern-inserter"
-			>
-				<motion.div
-					key={ clientId }
-					layout={ ! disableMotion }
-					initial={ disableMotion ? 'rest' : 'start' }
-					animate="rest"
-					whileHover="hover"
-					whileTap="pressed"
-					exit="start"
-					className="block-editor-block-list__insertion-point is-vertical is-pattern-inserter"
-				>
-					<motion.div
-						variants={ lineVariants }
-						className="block-editor-block-list__insertion-point-indicator"
-						data-testid="block-list-insertion-point-indicator"
-					/>
-					<motion.div
-						variants={ patternInserterVariants }
-						className="block-editor-block-list__insertion-point-inserter"
-					>
-						<Button
-							variant="primary"
-							icon={ plus }
-							size="compact"
-							className="block-editor-button-pattern-inserter__button"
-							onClick={ () => {
-								setInserterIsOpened( {
-									clientId,
-									insertionIndex,
-									filterValue,
-								} );
-							} }
-							label={ label }
-						/>
-					</motion.div>
-				</motion.div>
-			</BlockPopoverInbetween>
+			/>
 		);
 	} );
 }
