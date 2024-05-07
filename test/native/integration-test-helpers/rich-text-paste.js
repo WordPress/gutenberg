c68395c5fe48be3bddaa2fbc7bfaf34b54e2af24
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2024-05-07 14:20:33 +0300

    Revert "Testing: Remove `react-test-renderer` use (#61263)" (#61439)
    This reverts commit 28054e341c15f52f483fab4ba63408208ba000b7.
    
    Co-authored-by: tyxla <tyxla@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

--- test/native/integration-test-helpers/rich-text-paste.js
+++ test/native/integration-test-helpers/rich-text-paste.js
@@ -6,10 +6,10 @@ import { fireEvent } from '@testing-library/react-native';
 /**
  * Paste content into a RichText component.
  *
- * @param {HTMLElement} richText       RichText test instance.
- * @param {Object}      content        Content to paste.
- * @param {string}      content.text   Text format of the content.
- * @param {string}      [content.html] HTML format of the content. If not provided, text format will be used.
+ * @param {import('react-test-renderer').ReactTestInstance} richText       RichText test instance.
+ * @param {Object}                                          content        Content to paste.
+ * @param {string}                                          content.text   Text format of the content.
+ * @param {string}                                          [content.html] HTML format of the content. If not provided, text format will be used.
  */
 export const pasteIntoRichText = ( richText, { text, html } ) => {
 	fireEvent( richText, 'focus' );
