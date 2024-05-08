cb402f12c189cdbd6054f7147e99474cc09e5f01
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-05-08 11:42:00 +0900

    Remove internal hook

--- packages/core-commands/src/admin-navigation-commands.js
+++ packages/core-commands/src/admin-navigation-commands.js
@@ -4,20 +4,23 @@
 import { useCommand } from '@wordpress/commands';
 import { __ } from '@wordpress/i18n';
 import { plus, symbol } from '@wordpress/icons';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 import { addQueryArgs, getPath } from '@wordpress/url';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 
 /**
  * Internal dependencies
  */
-import { useIsTemplatesAccessible } from './hooks';
 import { unlock } from './lock-unlock';
 
 const { useHistory } = unlock( routerPrivateApis );
 
 export function useAdminNavigationCommands() {
 	const history = useHistory();
-	const isTemplatesAccessible = useIsTemplatesAccessible();
+	const canCreateTemplate = useSelect( ( select ) => {
+		return select( coreStore ).canUser( 'create', 'templates' );
+	}, [] );
 
 	const isSiteEditor = getPath( window.location.href )?.includes(
 		'site-editor.php'
@@ -45,10 +48,10 @@ export function useAdminNavigationCommands() {
 		icon: symbol,
 		callback: ( { close } ) => {
 			// The site editor and templates both check whether the user
-			// can read templates. We can leverage that here and this
+			// can create templates. We can leverage that here and this
 			// command links to the classic dashboard manage patterns page
 			// if the user can't access it.
-			if ( isTemplatesAccessible ) {
+			if ( canCreateTemplate ) {
 				const args = {
 					path: '/patterns',
 				};
