8b03884a708e32dc65cac91d4c15e8aac7dc5628
Jon Surrell <sirreal@users.noreply.github.com>
2024-05-08 16:46:39 +0200

    Fix the process problem

--- typings/gutenberg-env/index.d.ts
+++ typings/gutenberg-env/index.d.ts
@@ -3,9 +3,15 @@ interface Environment {
 	IS_GUTENBERG_PLUGIN?: boolean;
 	IS_WORDPRESS_CORE?: boolean;
 }
-interface Process {
-	env: Environment;
+
+declare namespace NodeJS {
+	export interface ProcessEnv extends Environment {}
+
+	export interface Process {
+		env: ProcessEnv;
+	}
 }
-declare var process: Process;
+
+declare var process: NodeJS.Process;
 
 declare var SCRIPT_DEBUG: boolean;
