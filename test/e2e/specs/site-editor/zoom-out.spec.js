2771fa0b6f80cc2b552ee5da2176e262b5b7908c
Maggie Cabrera <maggie@onemaggie.com>
2024-05-09 14:17:04 +0900

    fixed test to not use the experiment

--- test/e2e/specs/site-editor/zoom-out.spec.js
+++ test/e2e/specs/site-editor/zoom-out.spec.js
@@ -4,15 +4,26 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'Zoom Out', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( 'emptytheme' );
+	} );
+
+	test.beforeEach( async ( { admin, editor } ) => {
+		await admin.visitSiteEditor();
+		await editor.canvas.locator( 'body' ).click();
+	} );
+
+	test.afterAll( async ( { requestUtils } ) => {
+		await requestUtils.activateTheme( 'twentytwentyone' );
+	} );
+
 	test( 'Clicking on inserter while on zoom-out should open the patterns tab on the inserter', async ( {
 		page,
 	} ) => {
-		const zoomOutButton = page.getByRole( 'button', {
-			name: 'Zoom-out View',
-			exact: true,
-		} );
+		// Trigger zoom out on Global Styles because there the inserter is not open.
+		await page.getByRole( 'button', { name: 'Styles' } ).click();
+		await page.getByRole( 'button', { name: 'Browse styles' } ).click();
 
-		await zoomOutButton.click();
 		await expect( page.getByLabel( 'Add pattern' ) ).toHaveCount( 3 );
 		await page.getByLabel( 'Add pattern' ).first().click();
 		await expect( page.getByLabel( 'Add pattern' ) ).toHaveCount( 2 );
