12d8e9abd40507ad934d32d6c0afb00b5023d37c
Maggie Cabrera <maggie@onemaggie.com>
2024-05-09 11:21:38 +0900

    add e2e test

--- test/e2e/specs/site-editor/zoom-out.spec.js
+++ test/e2e/specs/site-editor/zoom-out.spec.js
@@ -61,4 +61,25 @@ test.describe( 'Zoom Out', () => {
 
 		await expect( zoomOutButton ).toBeFocused();
 	} );
+
+	test( 'Clicking on inserter while on zoom-out should open the patterns tab on the inserter', async ( {
+		page,
+	} ) => {
+		const zoomOutButton = page.getByRole( 'button', {
+			name: 'Zoom-out View',
+			exact: true,
+		} );
+
+		await zoomOutButton.click();
+		await expect( page.getByLabel( 'Add pattern' ) ).toHaveCount( 3 );
+		await page.getByLabel( 'Add pattern' ).first().click();
+		await expect( page.getByLabel( 'Add pattern' ) ).toHaveCount( 2 );
+
+		await expect(
+			page
+				.locator( '#tabs-2-allPatterns-view div' )
+				.filter( { hasText: 'All' } )
+				.nth( 1 )
+		).toBeVisible();
+	} );
 } );
