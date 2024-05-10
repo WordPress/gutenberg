b687afbba919cf5dd2ba72f5409d17fc888873ca
Jon Surrell <sirreal@users.noreply.github.com>
2024-05-10 19:54:41 +0200

    Some env improvements

--- typings/gutenberg-env/index.d.ts
+++ typings/gutenberg-env/index.d.ts
@@ -9,6 +9,17 @@ declare namespace NodeJS {
 
 declare var process: NodeJS.Process;
 
+/**
+ * Whether the code is running in WordPress with SCRIPT_DEBUG flag.
+ */
 declare var SCRIPT_DEBUG: undefined | boolean;
+
+/**
+ * Whether code is running within the Gutenberg plugin.
+ *
+ * When the codebase is built for the plugin, this variable will be set to `true`.
+ * When building for WordPress Core, it will be set to `false` or `undefined`.
+ */
 declare var IS_GUTENBERG_PLUGIN: undefined | boolean;
+
 declare var IS_WORDPRESS_CORE: undefined | boolean;
