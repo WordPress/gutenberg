9d72873672e196fcac505877170278eca7f30def
Jack Stevens <jack.stevens@news.co.uk>
2024-05-10 16:52:39 +0100

    chore: convert data-controls test to typescript

--- packages/data-controls/src/test/index.ts
+++ packages/data-controls/src/test/index.ts
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import type { APIFetchOptions } from '@wordpress/api-fetch';
 import triggerFetch from '@wordpress/api-fetch';
 
 jest.mock( '@wordpress/api-fetch' );
@@ -13,14 +14,14 @@ import { controls } from '../index';
 describe( 'controls', () => {
 	describe( 'API_FETCH', () => {
 		afterEach( () => {
-			triggerFetch.mockClear();
+			( triggerFetch as unknown as jest.Mock ).mockClear();
 		} );
 		it( 'invokes the triggerFetch function', () => {
-			controls.API_FETCH( { request: '' } );
+			controls.API_FETCH( { request: '' as APIFetchOptions } );
 			expect( triggerFetch ).toHaveBeenCalledTimes( 1 );
 		} );
 		it( 'invokes the triggerFetch function with the passed in request', () => {
-			controls.API_FETCH( { request: 'foo' } );
+			controls.API_FETCH( { request: 'foo' as APIFetchOptions } );
 			expect( triggerFetch ).toHaveBeenCalledWith( 'foo' );
 		} );
 	} );
