b45d5985e3131458a675e796dadcfdf29985ac88
Jon Surrell <sirreal@users.noreply.github.com>
2024-05-13 16:35:39 +0200

    Update tests

--- packages/eslint-plugin/rules/__tests__/is-gutenberg-plugin.js
+++ packages/eslint-plugin/rules/__tests__/is-gutenberg-plugin.js
@@ -14,52 +14,90 @@ const ruleTester = new RuleTester( {
 	},
 } );
 
-const ERROR_MESSAGE =
-	'The `process.env.IS_GUTENBERG_PLUGIN` constant should only be used as the condition in an if statement or ternary expression.';
-
 ruleTester.run( 'is-gutenberg-plugin', rule, {
 	valid: [
-		{ code: `if ( process.env.IS_GUTENBERG_PLUGIN ) {}` },
-		{ code: `if ( ! process.env.IS_GUTENBERG_PLUGIN ) {}` },
+		{ code: `if ( globalThis.IS_GUTENBERG_PLUGIN ) {}` },
+		{ code: `if ( globalThis.IS_WORDPRESS ) {}` },
+		{ code: `if ( globalThis.SCRIPT_DEBUG ) {}` },
+		{ code: `if ( ! globalThis.IS_GUTENBERG_PLUGIN ) {}` },
 		{
 			// Ensure whitespace is ok.
 			code: `if (
-				process.env.
+				globalThis.
 				IS_GUTENBERG_PLUGIN
 			) {}`,
 		},
-		{ code: `const test = process.env.IS_GUTENBERG_PLUGIN ? foo : bar` },
-		{ code: `const test = ! process.env.IS_GUTENBERG_PLUGIN ? bar : foo` },
+		{ code: `const test = globalThis.IS_GUTENBERG_PLUGIN ? foo : bar` },
+		{ code: `const test = ! globalThis.IS_GUTENBERG_PLUGIN ? bar : foo` },
 		{
 			// Ensure whitespace is ok.
-			code: `const test = ! process.env.
+			code: `const test = ! globalThis.
 				IS_GUTENBERG_PLUGIN ? bar : foo`,
 		},
 	],
 	invalid: [
 		{
 			code: `if ( IS_GUTENBERG_PLUGIN ) {}`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			errors: [
+				{
+					messageId: 'usedWithoutGlobalThis',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
+		},
+		{
+			code: `if ( SCRIPT_DEBUG ) {}`,
+			errors: [
+				{
+					messageId: 'usedWithoutGlobalThis',
+					data: { name: 'SCRIPT_DEBUG' },
+				},
+			],
 		},
 		{
 			code: `if ( window[ 'IS_GUTENBERG_PLUGIN' ] ) {}`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			errors: [
+				{
+					messageId: 'usedWithoutGlobalThis',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
 		},
 		{
-			code: `if ( true ) { process.env.IS_GUTENBERG_PLUGIN === 2 }`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			code: `if ( true ) { globalThis.IS_GUTENBERG_PLUGIN === 2 }`,
+			errors: [
+				{
+					messageId: 'usedOutsideConditional',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
 		},
 		{
-			code: `if ( process.env.IS_GUTENBERG_PLUGIN === 2 ) {}`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			code: `if ( globalThis.IS_GUTENBERG_PLUGIN === 2 ) {}`,
+			errors: [
+				{
+					messageId: 'usedOutsideConditional',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
 		},
 		{
-			code: `if ( true || process.env.IS_GUTENBERG_PLUGIN === 2 ) {}`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			code: `if ( true || globalThis.IS_GUTENBERG_PLUGIN === 2 ) {}`,
+			errors: [
+				{
+					messageId: 'usedOutsideConditional',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
 		},
 		{
-			code: `const isFeatureActive = process.env.IS_GUTENBERG_PLUGIN;`,
-			errors: [ { message: ERROR_MESSAGE } ],
+			code: `const isFeatureActive = globalThis.IS_GUTENBERG_PLUGIN;`,
+			errors: [
+				{
+					messageId: 'usedOutsideConditional',
+					data: { name: 'IS_GUTENBERG_PLUGIN' },
+				},
+			],
 		},
 	],
 } );
