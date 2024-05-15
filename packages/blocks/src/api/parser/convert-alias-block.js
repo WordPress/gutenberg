e646c5c0cce294412c9351150d3b35698a328dc6
Bernie Reiter <ockham@raz.or.at>
2024-05-15 14:52:06 +0200

    Extract canonical blockName, store in metadata.canonical

--- packages/blocks/src/api/parser/convert-alias-block.js
+++ packages/blocks/src/api/parser/convert-alias-block.js
@@ -1,4 +1,9 @@
 /**
+ * Internal dependencies
+ */
+import { getBlockVariations, getBlockType } from '../registration';
+
+/**
  * Convert alias blocks to their canonical form. This function is used
  * both in the parser level for previous content and to convert such blocks
  * used in Custom Post Types templates.
@@ -9,13 +14,27 @@
  * @return {[string, Object]} The block's name and attributes, changed accordingly if a match was found
  */
 export function convertAliasBlockNameAndAttributes( name, attributes ) {
-	const canonicalBlockName = attributes.metadata?.alias;
-	let blockName = name;
-	const newAttributes = { ...attributes };
-	if ( canonicalBlockName ) {
-		blockName = canonicalBlockName;
-		newAttributes.metadata.alias = name;
+	let canonicalBlockName = name;
+
+	const blockVariation = name.split( '/' )?.[ 2 ];
+	if ( blockVariation ) {
+		// FIXME: Stabler way of extracting canonical block name.
+		canonicalBlockName = name.replace( `/${ blockVariation }`, '' );
+		// const variations = getBlockVariations( canonicalBlockName );
+		// TODO: Validate. (Check if variation exists in variations array.)
 	}
 
-	return [ blockName, newAttributes ];
+	const { metadata = {}, ...otherAttributes } = attributes;
+
+	let newAttributes = { ...otherAttributes };
+
+	// TODO: Tidy up a bit more. Replace with attribute inference (in serializer)?
+	if ( canonicalBlockName ) {
+		newAttributes = {
+			...otherAttributes,
+			metadata: { ...metadata, canonical: canonicalBlockName },
+		};
+	}
+
+	return [ canonicalBlockName, newAttributes ];
 }
