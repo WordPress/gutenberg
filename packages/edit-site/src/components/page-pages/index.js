d3cec8969be80c8985a920adcf7db713ba4a2827
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-05-16 10:42:18 +0200

    Add support for pending review

--- packages/edit-site/src/components/page-pages/index.js
+++ packages/edit-site/src/components/page-pages/index.js
@@ -335,7 +335,8 @@ export default function PagePages() {
 					const isModified =
 						getDate( item.date ) < getDate( item.modified );
 					const isDraft = item.status === 'draft';
-					if ( isModified || isDraft ) {
+					const isPending = item.status === 'pending';
+					if ( isModified || isDraft || isPending ) {
 						return (
 							<>
 								{ __( 'Modified: ' ) }
