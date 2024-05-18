9ec48c941c8eae2ba91a78d654b458e6b719347c
Brian Gosnell <brian.gosnell@wpengine.com>
2024-05-18 00:10:54 -0500

    Add JSDoc to PostVisibility, PostVisibilityCheck, and PostVisibilityLabel (#61735)
    Co-authored-by: dbrian <bgosnell@git.wordpress.org>
    Co-authored-by: colorful-tones <colorful-tones@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>

--- packages/editor/src/components/post-visibility/index.js
+++ packages/editor/src/components/post-visibility/index.js
@@ -17,6 +17,13 @@ import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '
 import { visibilityOptions } from './utils';
 import { store as editorStore } from '../../store';
 
+/**
+ * Allows users to set the visibility of a post.
+ *
+ * @param {Object}   props         The component props.
+ * @param {Function} props.onClose Function to call when the popover is closed.
+ * @return {JSX.Element} The rendered component.
+ */
 export default function PostVisibility( { onClose } ) {
 	const instanceId = useInstanceId( PostVisibility );
 
