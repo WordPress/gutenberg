3de34063f0aca56a5fbbaa65d399507f05b8b39f
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2024-05-20 03:06:56 +0530

    Allow Cache directory value WP_ENV_HOME to Access from .wp-env.json and .wp-env-override.json

--- packages/env/lib/config/get-cache-directory.js
+++ packages/env/lib/config/get-cache-directory.js
@@ -7,14 +7,33 @@ const fs = require( 'fs' ).promises;
 const os = require( 'os' );
 
 /**
+ * Internal dependencies
+ */
+const readRawConfigFile = require( './read-raw-config-file' );
+
+/**
  * Gets the directory in which generated files are created.
  *
  * By default: '~/.wp-env/'. On Linux with snap packages: '~/wp-env/'. Can be
  * overridden with the WP_ENV_HOME environment variable.
  *
+ * @param {string|null} configFilePath Config Path for .wp-env.json file if exists.
+ *
  * @return {Promise<string>} The absolute path to the `wp-env` home directory.
  */
-module.exports = async function getCacheDirectory() {
+module.exports = async function getCacheDirectory( configFilePath = null ) {
+	/**
+	 * Allows user to override download location from .wp-env.json|.wp-env-override.json file.
+	 * Moving this variable allows us not only move from node environment variables, but
+	 * allows us to keep all our WP config at one place neat and tidy.
+	 */
+	if ( configFilePath ) {
+		const rawConfig = await readRawConfigFile( configFilePath );
+		if ( rawConfig.config.WP_ENV_HOME ) {
+			return path.resolve( rawConfig.config.WP_ENV_HOME );
+		}
+	}
+
 	// Allow user to override download location.
 	if ( process.env.WP_ENV_HOME ) {
 		return path.resolve( process.env.WP_ENV_HOME );
@@ -22,7 +41,7 @@ module.exports = async function getCacheDirectory() {
 
 	/**
 	 * Installing docker with Snap Packages on Linux is common, but does not
-	 * support hidden directories. Therefore we use a public directory when
+	 * support hidden directories. Therefore, we use a public directory when
 	 * snap packages exist.
 	 *
 	 * @see https://github.com/WordPress/gutenberg/issues/20180#issuecomment-587046325
