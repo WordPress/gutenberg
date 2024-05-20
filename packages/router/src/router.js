dcb09b4e567c2371a3afa0a7fde22c763b9c9cd6
Riad Benguella <benguella@gmail.com>
2024-05-20 11:55:46 +0100

    Use useSyncExternalStore for location

--- packages/router/src/router.js
+++ packages/router/src/router.js
@@ -3,9 +3,8 @@
  */
 import {
 	createContext,
-	useState,
-	useEffect,
 	useContext,
+	useSyncExternalStore,
 } from '@wordpress/element';
 
 /**
@@ -24,24 +23,25 @@ export function useHistory() {
 	return useContext( HistoryContext );
 }
 
+const locationCache = new WeakMap();
 function getLocationWithParams( location ) {
+	if ( locationCache.get( location ) ) {
+		return locationCache.get( location );
+	}
 	const searchParams = new URLSearchParams( location.search );
-	return {
+	const ret = {
 		...location,
 		params: Object.fromEntries( searchParams.entries() ),
 	};
+	locationCache.set( location, ret );
+
+	return ret;
 }
 
 export function RouterProvider( { children } ) {
-	const [ location, setLocation ] = useState( () =>
+	const location = useSyncExternalStore( history.listen, () =>
 		getLocationWithParams( history.location )
 	);
-
-	useEffect( () => {
-		return history.listen( ( { location: updatedLocation } ) => {
-			setLocation( getLocationWithParams( updatedLocation ) );
-		} );
-	}, [] );
 
 	return (
 		<HistoryContext.Provider value={ history }>
