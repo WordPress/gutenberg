8c0f89771cf9e73ece82261005e5aaad639da560
ntsekouras <ntsekouras@outlook.com>
2024-05-20 16:45:55 +0300

    php linter

--- lib/compat/wordpress-6.6/post.php
+++ lib/compat/wordpress-6.6/post.php
@@ -21,7 +21,7 @@ add_action( 'init', 'gutenberg_add_excerpt_support_to_wp_block' );
  * @return object Object with all the labels as member variables.
  */
 function gutenberg_update_wp_template_labels( $labels ) {
-	$labels->add_new = __( 'Add Template', 'gutenberg' );
+	$labels->add_new      = __( 'Add Template', 'gutenberg' );
 	$labels->add_new_item = __( 'Add Template', 'gutenberg' );
 	$labels->item_updated = __( 'Template updated.', 'gutenberg' );
 	return $labels;
@@ -35,7 +35,7 @@ add_filter( 'post_type_labels_wp_template', 'gutenberg_update_wp_template_labels
  * @return object Object with all the labels as member variables.
  */
 function gutenberg_update_wp_template__part_labels( $labels ) {
-	$labels->add_new = __( 'Add Template Part', 'gutenberg' );
+	$labels->add_new      = __( 'Add Template Part', 'gutenberg' );
 	$labels->add_new_item = __( 'Add Template Part', 'gutenberg' );
 	$labels->item_updated = __( 'Template part updated.', 'gutenberg' );
 	return $labels;
@@ -49,7 +49,7 @@ add_filter( 'post_type_labels_wp_template_part', 'gutenberg_update_wp_template__
  * @return object Object with all the labels as member variables.
  */
 function gutenberg_update_wp_block_labels( $labels ) {
-	$labels->add_new = __( 'Add Pattern', 'gutenberg' );
+	$labels->add_new      = __( 'Add Pattern', 'gutenberg' );
 	$labels->add_new_item = __( 'Add Pattern', 'gutenberg' );
 	return $labels;
 }
@@ -62,7 +62,7 @@ add_filter( 'post_type_labels_wp_block', 'gutenberg_update_wp_block_labels', 10,
  * @return object Object with all the labels as member variables.
  */
 function gutenberg_update_page_labels( $labels ) {
-	$labels->add_new = __( 'Add Page', 'gutenberg' );
+	$labels->add_new      = __( 'Add Page', 'gutenberg' );
 	$labels->add_new_item = __( 'Add Page', 'gutenberg' );
 	return $labels;
 }
