87aa946e6ebccc2e0115cf090807d1ca457be91f
Andrei Draganescu <andrei.draganescu@automattic.com>
2024-05-21 17:58:41 +0300

    add the media library button back

--- packages/block-editor/src/components/inserter/media-tab/media-tab.js
+++ packages/block-editor/src/components/inserter/media-tab/media-tab.js
@@ -1,15 +1,22 @@
 /**
  * WordPress dependencies
  */
-import { useMemo } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { useCallback, useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import MediaUploadCheck from '../../media-upload/check';
+import MediaUpload from '../../media-upload';
 import { MediaCategoryPanel } from './media-panel';
 import { useMediaCategories } from './hooks';
 import InserterContentNavigator from '../inserter-content-navigator';
 import InserterNoResults from '../no-results';
+import { getBlockAndPreviewFromMedia } from './utils';
+
+const ALLOWED_MEDIA_TYPES = [ 'image', 'video', 'audio' ];
 
 function MediaTab( { rootClientId, onInsert } ) {
 	const mediaCategories = useMediaCategories( rootClientId );
@@ -21,21 +28,63 @@ function MediaTab( { rootClientId, onInsert } ) {
 			} ) ),
 		[ mediaCategories ]
 	);
+	const baseCssClass = 'block-editor-inserter__media-tabs';
+	const onSelectMedia = useCallback(
+		( media ) => {
+			if ( ! media?.url ) {
+				return;
+			}
+			const [ block ] = getBlockAndPreviewFromMedia( media, media.type );
+			onInsert( block );
+		},
+		[ onInsert ]
+	);
 
 	if ( ! categories.length ) {
 		return <InserterNoResults />;
 	}
 
 	return (
-		<InserterContentNavigator categories={ categories }>
-			{ ( category ) => (
-				<MediaCategoryPanel
-					onInsert={ onInsert }
-					rootClientId={ rootClientId }
-					category={ category }
+		<>
+			<InserterContentNavigator
+				className={ baseCssClass }
+				categories={ categories }
+			>
+				{ ( category ) => (
+					<MediaCategoryPanel
+						onInsert={ onInsert }
+						rootClientId={ rootClientId }
+						category={ category }
+					/>
+				) }
+			</InserterContentNavigator>
+			<MediaUploadCheck>
+				<MediaUpload
+					multiple={ false }
+					onSelect={ onSelectMedia }
+					allowedTypes={ ALLOWED_MEDIA_TYPES }
+					render={ ( { open } ) => (
+						<Button
+							onClick={ ( event ) => {
+								// Safari doesn't emit a focus event on button elements when
+								// clicked and we need to manually focus the button here.
+								// The reason is that core's Media Library modal explicitly triggers a
+								// focus event and therefore a `blur` event is triggered on a different
+								// element, which doesn't contain the `data-unstable-ignore-focus-outside-for-relatedtarget`
+								// attribute making the Inserter dialog to close.
+								event.target.focus();
+								open();
+							} }
+							className="block-editor-inserter__media-library-button"
+							variant="secondary"
+							data-unstable-ignore-focus-outside-for-relatedtarget=".media-modal"
+						>
+							{ __( 'Open Media Library' ) }
+						</Button>
+					) }
 				/>
-			) }
-		</InserterContentNavigator>
+			</MediaUploadCheck>
+		</>
 	);
 }
 
