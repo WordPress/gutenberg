dacd26125f8ecf629fb730615e17c8964b2362ed
Tom Cafferkey <tjcafferkey@gmail.com>
2024-05-23 13:56:35 +0100

    Add filter check for function update_ignored_hooked_blocks_postmeta

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -1620,7 +1620,7 @@ $rest_insert_wp_navigation_core_callback = 'block_core_navigation_' . 'update_ig
  * Injection of hooked blocks into the Navigation block relies on some functions present in WP >= 6.5
  * that are not present in Gutenberg's WP 6.5 compatibility layer.
  */
-if ( function_exists( 'set_ignored_hooked_blocks_metadata' ) && ! has_filter( 'rest_pre_insert_wp_navigation', $rest_insert_wp_navigation_core_callback ) ) {
+if ( function_exists( 'set_ignored_hooked_blocks_metadata' ) && ( ! has_filter( 'rest_pre_insert_wp_navigation', $rest_insert_wp_navigation_core_callback ) && ! has_filter( 'rest_pre_insert_wp_navigation', 'update_ignored_hooked_blocks_postmeta' ) ) ) {
 	add_filter( 'rest_pre_insert_wp_navigation', 'block_core_navigation_update_ignore_hooked_blocks_meta' );
 }
 
