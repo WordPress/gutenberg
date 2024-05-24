491532f152b15a26e6ee23113d5cd5bf59a4fc35
Tim Howe <tim@hallme.com>
2024-05-24 11:00:48 -0400

    Removed the if statement the would prevent the post modified date from displaying if it was not past the publish date.

--- packages/block-library/src/post-date/index.php
+++ packages/block-library/src/post-date/index.php
@@ -33,17 +33,12 @@ function render_block_core_post_date( $attributes, $content, $block ) {
 	}
 
 	/*
-	 * If the "Display last modified date" setting is enabled,
-	 * only display the modified date if it is later than the publishing date.
+	 * If the "Display last modified date" setting is enabled.
 	 */
 	if ( isset( $attributes['displayType'] ) && 'modified' === $attributes['displayType'] ) {
-		if ( get_the_modified_date( 'Ymdhi', $post_ID ) > get_the_date( 'Ymdhi', $post_ID ) ) {
-			$formatted_date   = get_the_modified_date( empty( $attributes['format'] ) ? '' : $attributes['format'], $post_ID );
-			$unformatted_date = esc_attr( get_the_modified_date( 'c', $post_ID ) );
-			$classes[]        = 'wp-block-post-date__modified-date';
-		} else {
-			return '';
-		}
+		$formatted_date   = get_the_modified_date( empty( $attributes['format'] ) ? '' : $attributes['format'], $post_ID );
+		$unformatted_date = esc_attr( get_the_modified_date( 'c', $post_ID ) );
+		$classes[]        = 'wp-block-post-date__modified-date';
 	}
 
 	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => implode( ' ', $classes ) ) );
