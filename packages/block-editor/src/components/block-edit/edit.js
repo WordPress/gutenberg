3a26e71fb5322ff4269e6406375fc1f4063ac285
Mario Santos <santosguillamot@gmail.com>
2024-05-24 11:45:23 +0200

    Go back to syncing store in edit component

--- packages/block-editor/src/components/block-edit/edit.js
+++ packages/block-editor/src/components/block-edit/edit.js
@@ -12,12 +12,14 @@ import {
 	hasBlockSupport,
 	getBlockType,
 } from '@wordpress/blocks';
-import { useContext, useMemo } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
+import { useContext, useLayoutEffect, useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import BlockContext from '../block-context';
+import { store as blockEditorStore } from '../../store';
 
 /**
  * Default value used for blocks which do not define their own context needs,
@@ -48,9 +50,17 @@ const Edit = ( props ) => {
 const EditWithFilters = withFilters( 'editor.BlockEdit' )( Edit );
 
 const EditWithGeneratedProps = ( props ) => {
-	const { attributes = {}, name } = props;
+	const { clientId, attributes = {}, name } = props;
 	const blockType = getBlockType( name );
 	const blockContext = useContext( BlockContext );
+
+	// Sync the block context with the block editor store.
+	const { updateBlockContext } = useDispatch( blockEditorStore );
+	useLayoutEffect( () => {
+		if ( blockContext && Object.keys( blockContext ).length > 0 ) {
+			updateBlockContext( clientId, blockContext );
+		}
+	}, [ clientId, blockContext, updateBlockContext ] );
 
 	// Assign context values using the block type's declared context needs.
 	const context = useMemo( () => {
