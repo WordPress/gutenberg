7d6d7505bb93445cf9a15a7113e1c9b89406d824
Jarda Snajdr <jsnajdr@gmail.com>
2024-05-24 13:18:50 +0200

    Add postcss-modules to support local selectors

--- packages/postcss-plugins-preset/lib/index.js
+++ packages/postcss-plugins-preset/lib/index.js
@@ -1,1 +1,7 @@
-module.exports = [ require( 'autoprefixer' )( { grid: true } ) ];
+module.exports = [
+	require( 'autoprefixer' )( { grid: true } ),
+	require( 'postcss-modules' )( {
+		scopeBehaviour: 'global',
+		getJSON: () => {},
+	} ),
+];
