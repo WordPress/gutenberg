ea3fc593c996e6e8c154d33adeaa4283a994258b
Damon Cook <damonacook@gmail.com>
2024-05-27 12:31:59 -0400

    Refine ErrorBoundary JSDocs for editor component

--- packages/editor/src/components/error-boundary/index.js
+++ packages/editor/src/components/error-boundary/index.js
@@ -76,7 +76,11 @@ class ErrorBoundary extends Component {
 }
 
 /**
- * ErrorBoundary component.
+ * ErrorBoundary is used to catch JavaScript errors anywhere in a child component tree, log those errors, and display a fallback UI.
+ *
+ * It uses the lifecycle methods getDerivedStateFromError and componentDidCatch to catch errors in a child component tree.
+ *
+ * getDerivedStateFromError is used to render a fallback UI after an error has been thrown, and componentDidCatch is used to log error information.
  *
  * @class ErrorBoundary
  * @augments Component
