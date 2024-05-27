2b94b3b5b28c5ea22a36d358f926cf98212bfa83
Dani Guardiola <hi@daniguardio.la>
2024-05-27 15:07:40 +0200

    Remove useless clsx calls. (#61969)
    * Remove useless clsx calls.
    
    * Review fixes.
    
    Co-authored-by: DaniGuardiola <daniguardiola@git.wordpress.org>
    Co-authored-by: fullofcaffeine <fullofcaffeine@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>

--- packages/components/src/focal-point-picker/focal-point.tsx
+++ packages/components/src/focal-point-picker/focal-point.tsx
@@ -6,7 +6,6 @@ import { PointerCircle } from './styles/focal-point-style';
 /**
  * External dependencies
  */
-import clsx from 'clsx';
 import type { FocalPointProps } from './types';
 import type { WordPressComponentProps } from '../context';
 
@@ -15,9 +14,13 @@ export default function FocalPoint( {
 	top = '50%',
 	...props
 }: WordPressComponentProps< FocalPointProps, 'div' > ) {
-	const classes = clsx( 'components-focal-point-picker__icon_container' );
-
 	const style = { left, top };
 
-	return <PointerCircle { ...props } className={ classes } style={ style } />;
+	return (
+		<PointerCircle
+			{ ...props }
+			className="components-focal-point-picker__icon_container"
+			style={ style }
+		/>
+	);
 }
