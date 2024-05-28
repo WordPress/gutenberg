4b83b29af93127cc2c4d1e9106c6318a0af02b88
Mario Santos <santosguillamot@gmail.com>
2024-05-28 10:14:07 +0200

    Don't use fallbacks

--- packages/editor/src/bindings/post-meta.js
+++ packages/editor/src/bindings/post-meta.js
@@ -4,28 +4,17 @@
 import { store as coreDataStore } from '@wordpress/core-data';
 import { _x } from '@wordpress/i18n';
 
-/**
- * Internal dependencies
- */
-import { store as editorStore } from '../store';
-
 export default {
 	name: 'core/post-meta',
 	label: _x( 'Post Meta', 'block bindings source' ),
-	usesContext: [ 'postId', 'postType' ],
 	getPlaceholder( { args } ) {
 		return args.key;
 	},
 	getValue( { select, context, args } ) {
-		const postId =
-			context?.postId || select( editorStore ).getCurrentPostId();
-		const postType =
-			context?.postType || select( editorStore ).getCurrentPostType();
-
 		return select( coreDataStore ).getEditedEntityRecord(
 			'postType',
-			postType,
-			postId
+			context?.postType,
+			context?.postId
 		).meta?.[ args.key ];
 	},
 };
