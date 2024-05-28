66e82235cdf1d0b3ccc654c5e9f9e27f8219ebba
Marcelo Serpa <boss@fullofcaffeine.com>
2024-05-28 11:34:45 -0600

    Update docs and add story to illustrate a custom width

--- packages/components/src/progress-bar/stories/index.story.tsx
+++ packages/components/src/progress-bar/stories/index.story.tsx
@@ -29,3 +29,34 @@ const Template: StoryFn< typeof ProgressBar > = ( { ...args } ) => {
 
 export const Default: StoryFn< typeof ProgressBar > = Template.bind( {} );
 Default.args = {};
+
+const withCustomWidthCustomCSS = `
+	.custom-progress-bar {
+		width: 100%;
+	}
+`;
+
+/**
+ * A progress bar with a custom overriden width.
+ *
+ * You can override the default `width` or further customize the behavior
+ * and visuals by passing a custom CSS class via the `cssName` prop.
+ *
+ * This example shows a progress bar with an overriden `width` of `100%` which
+ * makes it fit all available horizontal space of the parent element. The CSS
+ * class looks like this:
+ *
+ * ```css
+ * .custom-progress-bar {
+ *   width: 100%;
+ * }
+ * ```
+ */
+export const WithCustomWidth: StoryFn< typeof ProgressBar > = () => {
+	return (
+		<>
+			<style>{ withCustomWidthCustomCSS }</style>
+			<ProgressBar className="custom-progress-bar" />
+		</>
+	);
+};
