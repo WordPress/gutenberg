5ac4874bf954aafe052d8209219a0780c9fd479a
Mario Santos <santosguillamot@gmail.com>
2024-05-30 10:44:57 +0200

    Revert "Add edit value posibility for post meta, add function to check if is admin"
    This reverts commit 9659455eed4343c455760b2f9076fe3a5bdbf2d7.

--- packages/editor/src/bindings/post-meta.js
+++ packages/editor/src/bindings/post-meta.js
@@ -8,7 +8,6 @@ import { _x } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { store as editorStore } from '../store';
-import { __experimentalIsAdminUser } from '../store/selectors';
 
 export default {
 	name: 'core/post-meta',
@@ -22,20 +21,5 @@ export default {
 			context?.postType,
 			context?.postId
 		).meta?.[ args.key ];
-	},
-	setValue( { registry, context, args, value } ) {
-		const postType = context.postType
-			? context.postType
-			: registry.select( editorStore ).getCurrentPostType();
-		registry
-			.dispatch( coreDataStore )
-			.editEntityRecord( 'postType', postType, context.postId, {
-				meta: {
-					[ args.key ]: value,
-				},
-			} );
-	},
-	lockAttributesEditing() {
-		return ! __experimentalIsAdminUser();
 	},
 };
