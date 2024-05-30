d42b518d7471a3d8ef44dbb7f27473a8dde6b136
Damon Cook <colorful-tones@users.noreply.github.com>
2024-05-30 11:27:38 -0400

    Documentation: PostDiscussionPanel editor component (#61966)
    * Add PostDiscussionPanel editor component JSDocs
    
    * Auto-generate editor docs or PostDiscussionPanel component
    
    * Refine PostDiscussionPanel editor component JSDocs
    
    * Auto-generate editor docs for PostDiscussionPanel updates

--- packages/editor/src/components/post-discussion/panel.js
+++ packages/editor/src/components/post-discussion/panel.js
@@ -86,6 +86,14 @@ function PostDiscussionToggle( { isOpen, onClick } ) {
 	);
 }
 
+/**
+ * This component allows to update comment and pingback
+ * settings for the current post. Internally there are
+ * checks whether the current post has support for the
+ * above and if the `discussion-panel` panel is enabled.
+ *
+ * @return {JSX.Element|null} The rendered PostDiscussionPanel component.
+ */
 export default function PostDiscussionPanel() {
 	const { isEnabled } = useSelect( ( select ) => {
 		const { isEditorPanelEnabled } = select( editorStore );
