827d8b439801babfc5ef3d27e5dfd60b452753b8
Carlos Bravo <carlos.bravo@automattic.com>
2024-05-30 10:44:57 +0200

    Add edit value posibility for post meta, add function to check if is admin

--- packages/editor/src/bindings/post-meta.js
+++ packages/editor/src/bindings/post-meta.js
@@ -4,6 +4,12 @@
 import { store as coreDataStore } from '@wordpress/core-data';
 import { _x } from '@wordpress/i18n';
 
+/**
+ * Internal dependencies
+ */
+import { store as editorStore } from '../store';
+import { __experimentalIsAdminUser } from '../store/selectors';
+
 export default {
 	name: 'core/post-meta',
 	label: _x( 'Post Meta', 'block bindings source' ),
@@ -16,5 +22,20 @@ export default {
 			context?.postType,
 			context?.postId
 		).meta?.[ args.key ];
+	},
+	setValue( { registry, context, args, value } ) {
+		const postType = context.postType
+			? context.postType
+			: registry.select( editorStore ).getCurrentPostType();
+		registry
+			.dispatch( coreDataStore )
+			.editEntityRecord( 'postType', postType, context.postId, {
+				meta: {
+					[ args.key ]: value,
+				},
+			} );
+	},
+	lockAttributesEditing() {
+		return ! __experimentalIsAdminUser();
 	},
 };
