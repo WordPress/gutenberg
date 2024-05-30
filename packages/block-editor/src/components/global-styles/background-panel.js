ebfb30e36beed8b8e93b3feec33eac9c0f8a5700
ramon <ramonjd@gmail.com>
2024-05-30 14:40:52 +1000

    Check for existing backgroundPosition

--- packages/block-editor/src/components/global-styles/background-panel.js
+++ packages/block-editor/src/components/global-styles/background-panel.js
@@ -448,7 +448,7 @@ function BackgroundSizeToolsPanelItem( {
 			 * when the toggle switches to "Tile". This is to increase the chance that
 			 * the image's focus point is visible.
 			 */
-			if ( !! style?.background?.backgroundImage?.id ) {
+			if ( !! style?.background?.backgroundImage?.id && ! nextPosition ) {
 				nextPosition = '50% 0';
 			}
 		}
