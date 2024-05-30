ee3d1eaa5cd666fce0cc266fb298c6035209827d
Narendra Sishodiya <32844880+narenin@users.noreply.github.com>
2024-05-30 20:04:46 +0530

    Fixed : Update `alt text decision tree` links to be translatable (#62076)
    * Implemented: Update alt text decision tree links to be translatable
    
    * Implemented: Update alt text decision tree links to be translatable
    
    * Implemented Suggestions
    
    * Implemented comment related changes
    
    Co-authored-by: narenin <narenin@git.wordpress.org>
    Co-authored-by: joedolson <joedolson@git.wordpress.org>
    Co-authored-by: michalczaplinski <czapla@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -606,7 +606,14 @@ export default function Image( {
 										<>{ lockAltControlsMessage }</>
 									) : (
 										<>
-											<ExternalLink href="https://www.w3.org/WAI/tutorials/images/decision-tree">
+											<ExternalLink
+												href={
+													// translators: Localized tutorial, if one exists. W3C Web Accessibility Initiative link has list of existing translations.
+													__(
+														'https://www.w3.org/WAI/tutorials/images/decision-tree/'
+													)
+												}
+											>
 												{ __(
 													'Describe the purpose of the image.'
 												) }
@@ -693,7 +700,14 @@ export default function Image( {
 										<>{ lockAltControlsMessage }</>
 									) : (
 										<>
-											<ExternalLink href="https://www.w3.org/WAI/tutorials/images/decision-tree">
+											<ExternalLink
+												href={
+													// translators: Localized tutorial, if one exists. W3C Web Accessibility Initiative link has list of existing translations.
+													__(
+														'https://www.w3.org/WAI/tutorials/images/decision-tree/'
+													)
+												}
+											>
 												{ __(
 													'Describe the purpose of the image.'
 												) }
