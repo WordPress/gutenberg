58781924a799c69058910b849f3b8eec3b3b424c
Artemio Morales <artemio.morales@a8c.com>
2024-05-31 16:38:57 +0200

    Add indicator for metadata changes to Save Panel when reviewing modified entities (#61811)
    * Add rough prototype showing meta in entities panel
    
    * Simplify detection of metadata changes
    
    Removed unnecessary code added in previous commit
    and instead modified existing functions to add a flag on
    existing dirtyEntityRecord structures to indicate when
    metadata changes have been made.
    
    * Remove obsolete code
    
    * Add indicator for bindings to save entities panel
    
    * Modify message to read 'Post Meta'
    
    * Add store function to check if meta has changed
    
    * Remove obsolete check
    
    * Simplify logic to check if meta has changed
    
    * Update tests
    
    * Make hasMetaChanges selector private
    
    * Suggestion: Move logic to `hasPostMetaChanges` selector
    
    * Change test formatting
    
    * Don't show save panel in pre-publish
    
    * Get `hasPostMetaChanges` from the proper place
    
    * Add end-to-end test
    
    * Update class name
    
    * Clarify naming
    
    * Show Post Meta in relevant post
    
    * Remove extra change
    
    * Move test metadata test util
    
    * Update comments
    
    * Prevent save panel from appearing when just footnotes are modified
    
    * Update package-lock.json
    
    ---------
    
    Co-authored-by: Mario Santos <santosguillamot@gmail.com>

--- packages/editor/src/components/post-publish-button/index.js
+++ packages/editor/src/components/post-publish-button/index.js
@@ -11,6 +11,7 @@ import { compose } from '@wordpress/compose';
  */
 import PublishButtonLabel from './label';
 import { store as editorStore } from '../../store';
+import { unlock } from '../../lock-unlock';
 
 const noop = () => {};
 
@@ -45,14 +46,24 @@ export class PostPublishButton extends Component {
 
 	createOnClick( callback ) {
 		return ( ...args ) => {
-			const { hasNonPostEntityChanges, setEntitiesSavedStatesCallback } =
-				this.props;
+			const {
+				hasNonPostEntityChanges,
+				hasPostMetaChanges,
+				setEntitiesSavedStatesCallback,
+				isPublished,
+			} = this.props;
 			// If a post with non-post entities is published, but the user
 			// elects to not save changes to the non-post entities, those
 			// entities will still be dirty when the Publish button is clicked.
 			// We also need to check that the `setEntitiesSavedStatesCallback`
 			// prop was passed. See https://github.com/WordPress/gutenberg/pull/37383
-			if ( hasNonPostEntityChanges && setEntitiesSavedStatesCallback ) {
+			//
+			// TODO: Explore how to manage `hasPostMetaChanges` and pre-publish workflow properly.
+			if (
+				( hasNonPostEntityChanges ||
+					( hasPostMetaChanges && isPublished ) ) &&
+				setEntitiesSavedStatesCallback
+			) {
 				// The modal for multiple entity saving will open,
 				// hold the callback for saving/publishing the post
 				// so that we can call it if the post entity is checked.
@@ -212,7 +223,8 @@ export default compose( [
 			isSavingNonPostEntityChanges,
 			getEditedPostAttribute,
 			getPostEdits,
-		} = select( editorStore );
+			hasPostMetaChanges,
+		} = unlock( select( editorStore ) );
 		return {
 			isSaving: isSavingPost(),
 			isAutoSaving: isAutosavingPost(),
@@ -229,6 +241,7 @@ export default compose( [
 			postStatus: getEditedPostAttribute( 'status' ),
 			postStatusHasChanged: getPostEdits()?.status,
 			hasNonPostEntityChanges: hasNonPostEntityChanges(),
+			hasPostMetaChanges: hasPostMetaChanges(),
 			isSavingNonPostEntityChanges: isSavingNonPostEntityChanges(),
 		};
 	} ),
