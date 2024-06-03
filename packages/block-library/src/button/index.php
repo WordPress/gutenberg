ef3f7b3f1875628875556115d8554cebbfdca920
Daniel Richards <daniel.richards@automattic.com>
2024-06-03 15:55:53 +0800

    Dynamically check button text and avoid rendering a button when empty

---
+++ packages/block-library/src/button/index.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Server-side rendering of the `core/button` block.
+ *
+ * @package WordPress
+ */
+
+ /**
+ * Renders the `core/button` block on the server,
+ *
+ * @since 6.6.0
+ *
+ * @param array    $attributes The block attributes.
+ * @param string   $content    The block content.
+ * @param WP_Block $block      The block object.
+ *
+ * @return string The block content.
+ */
+function render_block_core_button( $attributes, $content, $block ) {
+	$p = new WP_HTML_Tag_Processor( $content );
+
+	if ( ! $p->next_tag( 'a' ) ) {
+		return '';
+	}
+
+	$text = '';
+	while ( $p->next_token() ) {
+		switch ( $p->get_token_name() ) {
+			case '#text':
+				$text .= $p->get_modifiable_text();
+				break;
+
+			case 'BR':
+				$text .= '';
+				break;
+		}
+	}
+
+	// When there's no `text` set, avoid rendering an inaccessible button.
+	if ( trim( $text ) === '' ) {
+		return '';
+	}
+
+	return $content;
+}
+
+/**
+ * Registers the `core/button` block on server.
+ *
+ * @since 6.6.0
+ */
+function register_block_core_button() {
+	register_block_type_from_metadata(
+		__DIR__ . '/button',
+		array(
+			'render_callback' => 'render_block_core_button',
+		)
+	);
+}
+add_action( 'init', 'register_block_core_button' );
