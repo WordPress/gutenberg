758adcf3fa252449023c15bc8f51440dcda52510
Mario Santos <santosguillamot@gmail.com>
2024-06-03 12:52:33 +0200

    Remove `syncDerivedUpdates` mention

--- packages/block-editor/src/store/undo-ignore.js
+++ packages/block-editor/src/store/undo-ignore.js
@@ -1,4 +1,4 @@
 // Keep track of the blocks that should not be pushing an additional
 // undo stack when editing the entity.
-// See the implementation of `syncDerivedUpdates` and `useBlockSync`.
+// See the implementation of `useBlockSync`.
 export const undoIgnoreBlocks = new WeakSet();
