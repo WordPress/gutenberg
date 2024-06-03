79c637dc518e2a583d28fb5555928521639b6013
Mitchell Austin <mr.fye@oneandthesame.net>
2024-06-02 19:44:30 -0700

    Incorporate PostTypeSupportCheck in DocumentBar

--- packages/editor/src/components/document-bar/index.js
+++ packages/editor/src/components/document-bar/index.js
@@ -29,6 +29,7 @@ import { decodeEntities } from '@wordpress/html-entities';
 import { TEMPLATE_POST_TYPES, GLOBAL_POST_TYPES } from '../../store/constants';
 import { store as editorStore } from '../../store';
 import { unlock } from '../../lock-unlock';
+import PostTypeSupportCheck from '../post-type-support-check';
 
 const TYPE_LABELS = {
 	// translators: 1: Pattern title.
@@ -181,20 +182,25 @@ export default function DocumentBar() {
 						}
 					>
 						<BlockIcon icon={ templateIcon } />
-						<Text
-							size="body"
-							as="h1"
-							aria-label={
-								TYPE_LABELS[ postType ]
-									? // eslint-disable-next-line @wordpress/valid-sprintf
-									  sprintf( TYPE_LABELS[ postType ], title )
-									: undefined
-							}
-						>
-							{ title
-								? decodeEntities( title )
-								: __( 'No Title' ) }
-						</Text>
+						<PostTypeSupportCheck supportKeys="title">
+							<Text
+								size="body"
+								as="h1"
+								aria-label={
+									TYPE_LABELS[ postType ]
+										? // eslint-disable-next-line @wordpress/valid-sprintf
+										  sprintf(
+												TYPE_LABELS[ postType ],
+												title
+										  )
+										: undefined
+								}
+							>
+								{ title
+									? decodeEntities( title )
+									: __( 'No Title' ) }
+							</Text>
+						</PostTypeSupportCheck>
 					</motion.div>
 					<span className="editor-document-bar__shortcut">
 						{ displayShortcut.primary( 'k' ) }
