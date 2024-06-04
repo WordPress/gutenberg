201444189216698fa517c6fdea70850256a9f110
Brian Coords <bacoords@gmail.com>
2024-06-04 06:32:28 -0700

    adds support for italic via markdown

--- packages/format-library/src/italic/index.js
+++ packages/format-library/src/italic/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { toggleFormat } from '@wordpress/rich-text';
+import { toggleFormat, remove, applyFormat } from '@wordpress/rich-text';
 import {
 	RichTextToolbarButton,
 	RichTextShortcut,
@@ -18,6 +18,39 @@ export const italic = {
 	title,
 	tagName: 'em',
 	className: null,
+	__unstableInputRule( value ) {
+		const ASTERISK = '*';
+		const UNDERSCORE = '_';
+		const { start, text } = value;
+		const characterBefore = text[ start - 1 ];
+
+		// Quick check the text for the necessary character.
+		if ( characterBefore !== ASTERISK && characterBefore !== UNDERSCORE ) {
+			return value;
+		}
+
+		if ( start - 2 < 0 ) {
+			return value;
+		}
+
+		const indexBefore = text.lastIndexOf( characterBefore, start - 2 );
+		if ( indexBefore === -1 ) {
+			return value;
+		}
+
+		const startIndex = indexBefore;
+		const endIndex = start - 2;
+
+		if ( startIndex === endIndex ) {
+			return value;
+		}
+
+		value = remove( value, startIndex, startIndex + 1 );
+		value = remove( value, endIndex, endIndex + 1 );
+		value = applyFormat( value, { type: name }, startIndex, endIndex );
+
+		return value;
+	},
 	edit( { isActive, value, onChange, onFocus } ) {
 		function onToggle() {
 			onChange( toggleFormat( value, { type: name, title } ) );
