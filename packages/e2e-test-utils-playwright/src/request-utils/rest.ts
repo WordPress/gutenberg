393c29763003a4e39d357278bd67c5612e3ab366
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-06-05 12:45:12 +0200

    add retry mechanism to the REST API discovery

--- packages/e2e-test-utils-playwright/src/request-utils/rest.ts
+++ packages/e2e-test-utils-playwright/src/request-utils/rest.ts
@@ -3,6 +3,7 @@
  */
 import * as fs from 'fs/promises';
 import { dirname } from 'path';
+import { expect } from '@playwright/test';
 import type { APIRequestContext } from '@playwright/test';
 
 /**
@@ -22,18 +23,31 @@ function splitRequestsToChunks( requests: BatchRequest[], chunkSize: number ) {
 }
 
 async function getAPIRootURL( request: APIRequestContext ) {
-	// Discover the API root url using link header.
-	// See https://developer.wordpress.org/rest-api/using-the-rest-api/discovery/#link-header
-	const response = await request.head( WP_BASE_URL );
-	const links = response.headers().link;
-	const restLink = links?.match( /<([^>]+)>; rel="https:\/\/api\.w\.org\/"/ );
+	let restLink: unknown = null;
 
-	if ( ! restLink ) {
-		throw new Error( `Failed to discover REST API endpoint.
- Link header: ${ links }` );
-	}
+	// Retry until the REST API root URL is discovered.
+	// See https://github.com/WordPress/gutenberg/issues/61627
+	await expect
+		.poll(
+			async () => {
+				// Discover the API root url using link header.
+				// See https://developer.wordpress.org/rest-api/using-the-rest-api/discovery/#link-header
+				const response = await request.head( WP_BASE_URL );
+				const links = response.headers().link;
+				restLink = links?.match(
+					/<([^>]+)>; rel="https:\/\/api\.w\.org\/"/
+				);
 
-	const [ , rootURL ] = restLink;
+				return restLink;
+			},
+			{
+				message: 'Failed to discover REST API endpoint.',
+				timeout: 60_000, // 1 minute.
+			}
+		)
+		.not.toBeFalsy();
+
+	const [ , rootURL ] = restLink as RegExpMatchArray;
 
 	return rootURL;
 }
