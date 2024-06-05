93dd22d9c5e69d25ab9890fc45fdbff2dc7e5790
Bernie Reiter <96308+ockham@users.noreply.github.com>
2024-06-05 19:18:27 +0200

    Block Variations: Compare objects based on given properties (#62272)
    If a block variation has an isActive property that is an array of strings (attribute names), and one of the attributes referenced therein is an object, compare that attribute based on the object properties specified by the variation, rather than by reference.
    
    Co-authored-by: ockham <bernhard-reiter@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: cbravobernal <cbravobernal@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>
    Co-authored-by: gziolo <gziolo@git.wordpress.org

--- packages/blocks/src/store/selectors.js
+++ packages/blocks/src/store/selectors.js
@@ -12,7 +12,7 @@ import { RichTextData } from '@wordpress/rich-text';
 /**
  * Internal dependencies
  */
-import { getValueFromObjectPath } from './utils';
+import { getValueFromObjectPath, matchesAttributes } from './utils';
 
 /** @typedef {import('../api/registration').WPBlockVariation} WPBlockVariation */
 /** @typedef {import('../api/registration').WPBlockVariationScope} WPBlockVariationScope */
@@ -277,7 +277,10 @@ export function getActiveBlockVariation( state, blockName, attributes, scope ) {
 				if ( blockAttributeValue instanceof RichTextData ) {
 					blockAttributeValue = blockAttributeValue.toHTMLString();
 				}
-				return variationAttributeValue === blockAttributeValue;
+				return matchesAttributes(
+					blockAttributeValue,
+					variationAttributeValue
+				);
 			} );
 			if ( isMatch && definedAttributesLength > maxMatchedAttributes ) {
 				match = variation;
