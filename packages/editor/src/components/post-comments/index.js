c54e617a8cfe23889825c8de643e2576eb8bcf9a
Vipul Gupta <vipulgup04@gmail.com>
2024-06-06 20:36:44 +0530

    fix: The changes as per feedback.

--- packages/editor/src/components/post-comments/index.js
+++ packages/editor/src/components/post-comments/index.js
@@ -31,12 +31,10 @@ const COMMENT_OPTIONS = [
 			<>
 				{ __( 'Closed' ) }
 				<Text variant="muted" size={ 12 }>
-					<Text variant="muted" size={ 12 }>
-						{ __( 'Visitors cannot add new comments or replies.' ) }
-					</Text>
-					<Text variant="muted" size={ 12 }>
-						{ __( 'Existing comments remain visible.' ) }
-					</Text>
+					{ __( 'Visitors cannot add new comments or replies.' ) }
+				</Text>
+				<Text variant="muted" size={ 12 }>
+					{ __( 'Existing comments remain visible.' ) }
 				</Text>
 			</>
 		),
