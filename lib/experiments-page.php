79ef3c5f864f550d75f22e86201abc987f7c0b5b
Riad Benguella <benguella@gmail.com>
2024-06-06 15:06:13 +0100

    Posts Dashboard: Add a new experimental empty page

--- lib/experiments-page.php
+++ lib/experiments-page.php
@@ -139,6 +139,18 @@ function gutenberg_initialize_experiments_settings() {
 		)
 	);
 
+	add_settings_field(
+		'gutenberg-new-posts-dashboard',
+		__( 'Redesigned posts dashboard', 'gutenberg' ),
+		'gutenberg_display_experiment_field',
+		'gutenberg-experiments',
+		'gutenberg_experiments_section',
+		array(
+			'label' => __( 'Enable a redesigned posts dashboard.', 'gutenberg' ),
+			'id'    => 'gutenberg-new-posts-dashboard',
+		)
+	);
+
 	register_setting(
 		'gutenberg-experiments',
 		'gutenberg-experiments'
