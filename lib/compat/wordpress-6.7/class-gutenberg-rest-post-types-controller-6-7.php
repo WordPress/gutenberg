702a5be1037513f389e0aa2cf475b70810df0ed9
Tyler Bailey <tyler.bailey@10up.com>
2024-06-06 12:53:29 -0400

    Update rendering_mode filters to always be prefixed with post_type_

--- lib/compat/wordpress-6.7/class-gutenberg-rest-post-types-controller-6-7.php
+++ lib/compat/wordpress-6.7/class-gutenberg-rest-post-types-controller-6-7.php
@@ -35,17 +35,17 @@ class Gutenberg_REST_Post_Types_Controller_6_7 extends WP_REST_Post_Types_Contro
 			 * @param WP_Post_Type $post_type              Post type object.
 			 * @return string Default rendering mode for the post type.
 			 */
-			$rendering_mode = apply_filters( "{$item->name}_default_rendering_mode", $item->default_rendering_mode, $item );
+			$rendering_mode = apply_filters( "post_type_{$item->name}_default_rendering_mode", $item->default_rendering_mode, $item );
 
 			/**
 			 * Filters the block editor rendering mode for a post type.
 			 *
 			 * @since 6.6.0
-			 * @param string $default_rendering_mode Default rendering mode for the post type.
-			 * @param string $post_type              Post type name.
+			 * @param string       $default_rendering_mode Default rendering mode for the post type.
+			 * @param WP_Post_Type $post_type              Post type name.
 			 * @return string Default rendering mode for the post type.
 			 */
-			$response->data['default_rendering_mode'] = apply_filters( 'post_type_default_rendering_mode', $rendering_mode, $item->name );
+			$response->data['default_rendering_mode'] = apply_filters( 'post_type_default_rendering_mode', $rendering_mode, $item );
 		}
 
 		return rest_ensure_response( $response );
