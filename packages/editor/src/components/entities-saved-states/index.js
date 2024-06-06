db36fef77326e4e69e42cf02da9d0ef6d1230526
Damon Cook <damonacook@gmail.com>
2024-06-06 13:27:21 -0400

    Add JSDoc block for EntitiesSavedStatesExtensible function

--- packages/editor/src/components/entities-saved-states/index.js
+++ packages/editor/src/components/entities-saved-states/index.js
@@ -40,6 +40,23 @@ export default function EntitiesSavedStates( {
 	);
 }
 
+/**
+ * Renders a panel for saving entities with dirty records.
+ *
+ * @param {Object}   props                       The component props.
+ * @param {string}   props.additionalPrompt      Additional prompt to display.
+ * @param {Function} props.close                 Function to close the panel.
+ * @param {Function} props.onSave                Function to call when saving entities.
+ * @param {boolean}  props.saveEnabled           Flag indicating if save is enabled.
+ * @param {string}   props.saveLabel             Label for the save button.
+ * @param {Function} props.renderDialog          Function to render a custom dialog.
+ * @param {Array}    props.dirtyEntityRecords    Array of dirty entity records.
+ * @param {boolean}  props.isDirty               Flag indicating if there are dirty entities.
+ * @param {Function} props.setUnselectedEntities Function to set unselected entities.
+ * @param {Array}    props.unselectedEntities    Array of unselected entities.
+ *
+ * @return {JSX.Element} The rendered component.
+ */
 export function EntitiesSavedStatesExtensible( {
 	additionalPrompt = undefined,
 	close,
