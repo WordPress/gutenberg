4a7405eb930849ba7f07367f836d08235d2fd84b
Riad Benguella <benguella@gmail.com>
2024-06-07 10:20:52 +0100

    Fix PHPCS

--- lib/experimental/posts/load.php
+++ lib/experimental/posts/load.php
@@ -14,7 +14,7 @@ function gutenberg_posts_dashboard() {
 	wp_register_style(
 		'wp-gutenberg-posts-dashboard',
 		gutenberg_url( 'build/edit-site/posts.css', __FILE__ ),
-		array(),
+		array()
 	);
 	wp_enqueue_style( 'wp-gutenberg-posts-dashboard' );
 	wp_add_inline_script( 'wp-edit-site', 'window.wp.editSite.initializePostsDashboard( "gutenberg-posts-dashboard" );', 'after' );
@@ -28,7 +28,7 @@ function gutenberg_posts_dashboard() {
  */
 function gutenberg_replace_posts_dashboard() {
 	$gutenberg_experiments = get_option( 'gutenberg-experiments' );
-	if ( ! $gutenberg_experiments || ! array_key_exists( 'gutenberg-new-posts-dashboard', $gutenberg_experiments ) || ! $gutenberg_experiments[ 'gutenberg-new-posts-dashboard' ] ) {
+	if ( ! $gutenberg_experiments || ! array_key_exists( 'gutenberg-new-posts-dashboard', $gutenberg_experiments ) || ! $gutenberg_experiments['gutenberg-new-posts-dashboard'] ) {
 		return;
 	}
 	$ptype_obj = get_post_type_object( 'post' );
