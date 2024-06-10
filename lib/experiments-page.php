031e8d56bef83c89bbc62398794905436a71c3bc
poojabhimani12 <82029773+poojabhimani12@users.noreply.github.com>
2024-06-10 13:15:22 +0530

    resolved conflicts

--- lib/experiments-page.php
+++ lib/experiments-page.php
@@ -140,6 +140,18 @@ function gutenberg_initialize_experiments_settings() {
 	);
 
 	add_settings_field(
+		'gutenberg-new-posts-dashboard',
+		__( 'Redesigned posts dashboard', 'gutenberg' ),
+		'gutenberg_display_experiment_field',
+		'gutenberg-experiments',
+		'gutenberg_experiments_section',
+		array(
+			'label' => __( 'Enable a redesigned posts dashboard.', 'gutenberg' ),
+			'id'    => 'gutenberg-new-posts-dashboard',
+		)
+	);
+
+	add_settings_field(
 		'gutenberg-block-comment',
 		__( 'Enable block Comment', 'gutenberg' ),
 		'gutenberg_display_experiment_field',
