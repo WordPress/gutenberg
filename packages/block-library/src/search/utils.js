e58e4e5dca965dc3523bdc30a001cacccc7d8e97
Amit Raj <harsh.harshraj.ra@gmail.com>
2024-06-11 01:14:22 +0530

    Add taxonomy controls component for searching and selecting terms

--- packages/block-library/src/search/utils.js
+++ packages/block-library/src/search/utils.js
@@ -1,4 +1,11 @@
 /**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { useMemo } from '@wordpress/element';
+import { store as coreStore } from '@wordpress/core-data';
+
+/**
  * Constants
  */
 export const PC_WIDTH_DEFAULT = 50;
@@ -15,3 +22,64 @@ export const MIN_WIDTH = 220;
 export function isPercentageUnit( unit ) {
 	return unit === '%';
 }
+
+/**
+ * Returns a helper object that contains:
+ * 1. An `options` object from the available post types, to be passed to a `SelectControl`.
+ * 2. A helper map with available taxonomies per post type.
+ *
+ * @return {Object} The helper object related to post types.
+ */
+export const usePostTypes = () => {
+	const postTypes = useSelect( ( select ) => {
+		const { getPostTypes } = select( coreStore );
+		const excludedPostTypes = [ 'attachment' ];
+		const filteredPostTypes = getPostTypes( { per_page: -1 } )?.filter(
+			( { viewable, slug } ) =>
+				viewable && ! excludedPostTypes.includes( slug )
+		);
+		return filteredPostTypes;
+	}, [] );
+	const postTypesTaxonomiesMap = useMemo( () => {
+		if ( ! postTypes?.length ) {
+			return;
+		}
+		return postTypes.reduce( ( accumulator, type ) => {
+			accumulator[ type.slug ] = type.taxonomies;
+			return accumulator;
+		}, {} );
+	}, [ postTypes ] );
+	const postTypesSelectOptions = useMemo(
+		() =>
+			( postTypes || [] ).map( ( { labels, slug } ) => ( {
+				label: labels.singular_name,
+				value: slug,
+			} ) ),
+		[ postTypes ]
+	);
+	return { postTypesTaxonomiesMap, postTypesSelectOptions };
+};
+
+/**
+ * Hook that returns the taxonomies associated with a specific post type.
+ *
+ * @param {string} postType The post type from which to retrieve the associated taxonomies.
+ * @return {Object[]} An array of the associated taxonomies.
+ */
+export const useTaxonomies = ( postType ) => {
+	const taxonomies = useSelect(
+		( select ) => {
+			const { getTaxonomies } = select( coreStore );
+			return getTaxonomies( {
+				type: postType,
+				per_page: -1,
+			} );
+		},
+		[ postType ]
+	);
+	return useMemo( () => {
+		return taxonomies?.filter(
+			( { visibility } ) => !! visibility?.publicly_queryable
+		);
+	}, [ taxonomies ] );
+};
