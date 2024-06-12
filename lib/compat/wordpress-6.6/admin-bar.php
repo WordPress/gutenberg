c96b20f4b723fb280273ba83c7f529839d69c18e
Utsav Patel <utsav.patel1512@outlook.com>
2024-06-12 13:48:06 +0530

    return early if in multisite

--- lib/compat/wordpress-6.6/admin-bar.php
+++ lib/compat/wordpress-6.6/admin-bar.php
@@ -25,7 +25,7 @@ function gutenberg_admin_bar_edit_site_menu( $wp_admin_bar ) {
 	}
 
 	// Don't show for users who can't edit theme options or when in the admin.
-	if ( ! current_user_can( 'edit_theme_options' ) || is_admin() ) {
+	if ( ! current_user_can( 'edit_theme_options' ) || is_admin() || ( is_blog_admin() && is_multisite() && current_user_can( 'manage_sites' ) ) ) {
 		return;
 	}
 
@@ -43,18 +43,6 @@ function gutenberg_admin_bar_edit_site_menu( $wp_admin_bar ) {
 			),
 		)
 	);
-
-	if ( is_blog_admin() && is_multisite() && current_user_can( 'manage_sites' ) ) {
-		$wp_admin_bar->add_node(
-			array(
-				'parent' => 'site-name',
-				'id'     => 'edit-site',
-				'title'  => __( 'Manage Site' ),
-				'href'   => network_admin_url( 'site-info.php?id=' . get_current_blog_id() ),
-			)
-		);
-	}
-
 }
 remove_action( 'admin_bar_menu', 'wp_admin_bar_edit_site_menu', 40 );
 add_action( 'admin_bar_menu', 'gutenberg_admin_bar_edit_site_menu', 41 );
