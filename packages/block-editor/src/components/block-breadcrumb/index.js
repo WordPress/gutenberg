795350140178ab0fceb27560eb9e528bbbfe5a1f
Jerry Jones <jones.jeremydavid@gmail.com>
2024-06-12 15:41:23 -0500

    Fix wrapper selector

--- packages/block-editor/src/components/block-breadcrumb/index.js
+++ packages/block-editor/src/components/block-breadcrumb/index.js
@@ -68,7 +68,9 @@ function BlockBreadcrumb( { rootLabelText } ) {
 						onClick={ () => {
 							clearSelectedBlock();
 							// Focus the editor iframe.
-							blockRef.current?.closest( 'body' )?.focus();
+							blockRef.current
+								?.closest( '[aria-label="Block canvas"]' )
+								?.focus();
 						} }
 					>
 						{ rootLabel }
