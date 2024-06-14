73b821ab5f5dda774bba0f2f00f95955ed97abf0
tellthemachines <isabel@tellthemachines.com>
2024-06-14 16:54:35 +1000

    Show appender on manual grid

--- packages/block-library/src/group/edit.js
+++ packages/block-library/src/group/edit.js
@@ -96,7 +96,6 @@ function GroupEdit( { attributes, name, setAttributes, clientId } ) {
 	const { type = 'default' } = layout;
 	const layoutSupportEnabled =
 		themeSupportsLayout || type === 'flex' || type === 'grid';
-	const isManualGrid = type === 'grid' && !! layout.columnCount;
 
 	// Hooks.
 	const ref = useRef();
@@ -110,7 +109,7 @@ function GroupEdit( { attributes, name, setAttributes, clientId } ) {
 
 	// Default to the regular appender being rendered.
 	let renderAppender;
-	if ( showPlaceholder || isManualGrid ) {
+	if ( showPlaceholder ) {
 		// In the placeholder state, ensure the appender is not rendered.
 		// This is needed because `...innerBlocksProps` is used in the placeholder
 		// state so that blocks can dragged onto the placeholder area
