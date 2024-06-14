bfcc4cf0735ee3de12152e4898a483089b8d6ce2
Alex Lende <alex+github.com@lende.xyz>
2024-06-14 00:01:41 -0500

    Sort spacing sizes when all slugs begin numerically

--- packages/block-editor/src/components/spacing-sizes-control/hooks/use-spacing-sizes.js
+++ packages/block-editor/src/components/spacing-sizes-control/hooks/use-spacing-sizes.js
@@ -44,15 +44,8 @@ export default function useSpacingSizes() {
 			...defaultSizes,
 		];
 
-		// Only sort if more than one origin has presets defined in order to
-		// preserve order for themes that don't include default presets and
-		// want a custom order.
-		if (
-			( customSizes.length && 1 ) +
-				( themeSizes.length && 1 ) +
-				( defaultSizes.length && 1 ) >
-			1
-		) {
+		// Using numeric slugs opts-in to sorting by slug.
+		if ( sizes.every( ( { slug } ) => /^[0-9]/.test( slug ) ) ) {
 			sizes.sort( ( a, b ) => compare( a.slug, b.slug ) );
 		}
 
