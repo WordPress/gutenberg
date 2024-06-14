5da8e339ec1e79e436a19a1f55844e1911fe060c
Utsav Patel <utsav.patel1512@outlook.com>
2024-06-14 15:19:30 +0530

    Fix: remove all param types

--- packages/blob/src/index.ts
+++ packages/blob/src/index.ts
@@ -1,13 +1,11 @@
-type BlobPart = BufferSource | Blob | string;
-
 const cache: Record< string, File > = {};
 
 /**
  * Create a blob URL from a file.
  *
- * @param {File} file The file to create a blob URL for.
+ * @param file The file to create a blob URL for.
  *
- * @return {string} The blob URL.
+ * @return The blob URL.
  */
 export function createBlobURL( file: File ): string {
 	const url = window.URL.createObjectURL( file );
@@ -22,9 +20,9 @@ export function createBlobURL( file: File ): string {
  * `createBlobURL` and not removed by `revokeBlobURL`, otherwise it will return
  * `undefined`.
  *
- * @param {string} url The blob URL.
+ * @param url The blob URL.
  *
- * @return {File|undefined} The file for the blob URL.
+ * @return The file for the blob URL.
  */
 export function getBlobByURL( url: string ): File | undefined {
 	return cache[ url ];
@@ -35,9 +33,9 @@ export function getBlobByURL( url: string ): File | undefined {
  * `createBlobURL` and not removed by `revokeBlobURL`, otherwise it will return
  * `undefined`.
  *
- * @param {string} url The blob URL.
+ * @param url The blob URL.
  *
- * @return {string|undefined} The blob type.
+ * @return The blob type.
  */
 export function getBlobTypeByURL( url: string ): string | undefined {
 	return getBlobByURL( url )?.type.split( '/' )[ 0 ]; // 0: media type , 1: file extension eg ( type: 'image/jpeg' ).
@@ -46,7 +44,7 @@ export function getBlobTypeByURL( url: string ): string | undefined {
 /**
  * Remove the resource and file cache from memory.
  *
- * @param {string} url The blob URL.
+ * @param url The blob URL.
  */
 export function revokeBlobURL( url: string ): void {
 	if ( cache[ url ] ) {
@@ -59,9 +57,9 @@ export function revokeBlobURL( url: string ): void {
 /**
  * Check whether a url is a blob url.
  *
- * @param {string|undefined} url The URL.
+ * @param url The URL.
  *
- * @return {boolean} Is the url a blob url?
+ * @return Is the url a blob url?
  */
 export function isBlobURL( url: string | undefined ): boolean {
 	if ( ! url || ! url.indexOf ) {
@@ -89,9 +87,9 @@ export function isBlobURL( url: string | undefined ): boolean {
  * 	downloadBlob( filename, fileContent, 'application/json' );
  * ```
  *
- * @param {string}   filename    File name.
- * @param {BlobPart} content     File content (BufferSource | Blob | string).
- * @param {string}   contentType (Optional) File mime type. Default is `''`.
+ * @param filename    File name.
+ * @param content     File content (BufferSource | Blob | string).
+ * @param contentType (Optional) File mime type. Default is `''`.
  */
 export function downloadBlob(
 	filename: string,
