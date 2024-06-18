2fdcec76a6fa87be2637602264f2f9ad76ae4d0a
André <583546+oandregal@users.noreply.github.com>
2024-06-18 04:03:50 +0200

    Revert "Revert test data for `WithSlug` variation (#62579)" (#62587)
    * Revert "Revert test data for `WithSlug` variation (#62579)"
    
    This reverts commit 2917269cf70405990ab4b1cd3f961a714dc5f5f6.
    
    * Clear up test caching
    
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    Co-authored-by: aaronrobertshaw <aaronrobertshaw@git.wordpress.org>
    Co-authored-by: tellthemachines <isabel_brison@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>

--- phpunit/block-supports/block-style-variations-test.php
+++ phpunit/block-supports/block-style-variations-test.php
@@ -10,6 +10,41 @@
 
 class WP_Block_Supports_Block_Style_Variations_Test extends WP_UnitTestCase {
 	/**
+	 * Administrator ID.
+	 *
+	 * @var int
+	 */
+	protected static $administrator_id;
+
+	/**
+	 * WP_Theme_JSON_Resolver_Gutenberg::$blocks_cache property.
+	 *
+	 * @var ReflectionProperty
+	 */
+	private static $property_blocks_cache;
+
+	/**
+	 * Original value of the WP_Theme_JSON_Resolver_Gutenberg::$blocks_cache property.
+	 *
+	 * @var array
+	 */
+	private static $property_blocks_cache_orig_value;
+
+	/**
+	 * WP_Theme_JSON_Resolver_Gutenberg::$core property.
+	 *
+	 * @var ReflectionProperty
+	 */
+	private static $property_core;
+
+	/**
+	 * Original value of the WP_Theme_JSON_Resolver_Gutenberg::$core property.
+	 *
+	 * @var WP_Theme_JSON_Gutenberg
+	 */
+	private static $property_core_orig_value;
+
+	/**
 	 * Theme root directory.
 	 *
 	 * @var string|null
@@ -22,6 +57,31 @@ class WP_Block_Supports_Block_Style_Variations_Test extends WP_UnitTestCase {
 	 * @var array|null
 	 */
 	private $orig_theme_dir;
+
+	public static function set_up_before_class() {
+		parent::set_up_before_class();
+
+		self::$administrator_id = self::factory()->user->create(
+			array(
+				'role'       => 'administrator',
+				'user_email' => 'administrator@example.com',
+			)
+		);
+
+		static::$property_blocks_cache = new ReflectionProperty( WP_Theme_JSON_Resolver_Gutenberg::class, 'blocks_cache' );
+		static::$property_blocks_cache->setAccessible( true );
+		static::$property_blocks_cache_orig_value = static::$property_blocks_cache->getValue();
+
+		static::$property_core = new ReflectionProperty( WP_Theme_JSON_Resolver_Gutenberg::class, 'core' );
+		static::$property_core->setAccessible( true );
+		static::$property_core_orig_value = static::$property_core->getValue();
+	}
+
+	public static function tear_down_after_class() {
+		static::$property_blocks_cache->setValue( null, static::$property_blocks_cache_orig_value );
+		static::$property_core->setValue( null, static::$property_core_orig_value );
+		parent::tear_down_after_class();
+	}
 
 	public function set_up() {
 		parent::set_up();
@@ -94,6 +154,22 @@ class WP_Block_Supports_Block_Style_Variations_Test extends WP_UnitTestCase {
 			),
 		);
 
+		/*
+		 * This style is to be deliberately overwritten by the theme.json partial
+		 * See `phpunit/data/themedir1/block-theme/styles/block-style-variation-with-slug.json`.
+		 */
+		register_block_style(
+			'core/group',
+			array(
+				'name'       => 'WithSlug',
+				'style_data' => array(
+					'color' => array(
+						'background' => 'whitesmoke',
+						'text'       => 'black',
+					),
+				),
+			)
+		);
 		register_block_style(
 			'core/group',
 			array(
@@ -106,6 +182,12 @@ class WP_Block_Supports_Block_Style_Variations_Test extends WP_UnitTestCase {
 		$group_styles = $theme_json['styles']['blocks']['core/group'] ?? array();
 		$expected     = array(
 			'variations' => array(
+				'WithSlug'                => array(
+					'color' => array(
+						'background' => 'aliceblue',
+						'text'       => 'midnightblue',
+					),
+				),
 				'my-variation'            => $variation_styles_data,
 
 				/*
@@ -129,6 +211,7 @@ class WP_Block_Supports_Block_Style_Variations_Test extends WP_UnitTestCase {
 		);
 
 		unregister_block_style( 'core/group', 'my-variation' );
+		unregister_block_style( 'core/group', 'WithSlug' );
 
 		$this->assertSameSetsWithIndex( $group_styles, $expected );
 	}
