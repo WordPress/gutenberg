87fe4f95f0b19dd7f776258f0737fe6bf2272e3d
sunilprajapati2503 <sunil.prajapati@multidots.com>
2024-06-19 17:24:47 +0530

    Fix linting errors

--- packages/format-library/src/collab-comment/collab-board/index.js
+++ packages/format-library/src/collab-comment/collab-board/index.js
@@ -50,9 +50,11 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 	const [ isResolved, setIsResolved ] = useState( false );
 	const [ isEditing, setIsEditing ] = useState( null );
 	const [ showConfirmation, setShowConfirmation ] = useState( false );
-	const [ threadId, setThreadId ] = useState( classList
-		? classList.slice( 'block-editor-collab__'.length )
-		: Date.now() );
+	const [ threadId, setThreadId ] = useState(
+		classList
+			? classList.slice( 'block-editor-collab__'.length )
+			: Date.now()
+	);
 
 	// Get the dispatch functions to save the comment and update the block attributes.
 	const { updateBlockAttributes } = useDispatch( blockEditorStore );
@@ -65,15 +67,11 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 	}, [ threadId ] );
 
 	// Fetch the current post, current user, and the selected block clientId.
-	const {
-		postType,
-		currentUser,
-		clientId
-	} = useSelect(
+	const { postType, currentUser, clientId } = useSelect(
 		( select ) => {
 			return {
 				postType: select( editorStore ).getCurrentPostType(),
- 				currentUser: select( coreStore ).getCurrentUser()?.name || null,
+				currentUser: select( coreStore ).getCurrentUser()?.name || null,
 				clientId:
 					select( blockEditorStore ).getSelectedBlockClientId() ||
 					null,
@@ -82,16 +80,14 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 		[ threadId ]
 	);
 
-	const [ meta, setMeta ] = useEntityProp(
-		'postType',
-		postType,
-		'meta',
-	);
+	const [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
 
 	const allThreads = meta?.collab ? JSON.parse( meta.collab ) : [];
 	const currentThread = allThreads[ threadId ] ?? {};
 	const isCurrentThreadResolved = currentThread.threadIsResolved || false;
-	const commentsCount = isCurrentThreadResolved ? 0 : currentThread.comments?.length || 0;
+	const commentsCount = isCurrentThreadResolved
+		? 0
+		: currentThread.comments?.length || 0;
 
 	// Helper function to generate a new comment.
 	const generateNewComment = () => ( {
@@ -118,14 +114,14 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 
 	const updateCommentMeta = ( updatedComments ) => {
 		setMeta( { ...meta, collab: JSON.stringify( updatedComments ) } );
-	}
+	};
 
 	// Function to save the comment.
 	const saveComment = () => {
 		const newComment = generateNewComment();
 		const updatedComments = getUpdatedComments( newComment, threadId );
 
-		updateCommentMeta ( updatedComments );
+		updateCommentMeta( updatedComments );
 		setInputComment( '' );
 	};
 
@@ -146,7 +142,7 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 			} );
 		}
 
-		updateCommentMeta ( editedComments );
+		updateCommentMeta( editedComments );
 		setInputComment( '' );
 		setIsEditing( null );
 	};
@@ -165,7 +161,7 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 		};
 
 		// Save the updated comments.
-		updateCommentMeta ( updatedComments );
+		updateCommentMeta( updatedComments );
 		removeBorder();
 		setThreadId( null );
 		onClose();
@@ -195,7 +191,7 @@ const CollabBoard = ( { contentRef, onClose } ) => {
 		}
 
 		// Save the updated comments.
-		updateCommentMeta ( updatedComments );
+		updateCommentMeta( updatedComments );
 	};
 
 	// Function to show the confirmation overlay.
