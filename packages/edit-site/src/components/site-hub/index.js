da986707e344e028ea62a162e10618e8ea4200a7
Utsav Patel <utsav.patel1512@outlook.com>
2024-06-19 15:28:16 +0530

    update: remove unnessary styles and added focus icon visibility

--- packages/edit-site/src/components/site-hub/index.js
+++ packages/edit-site/src/components/site-hub/index.js
@@ -88,7 +88,7 @@ const SiteHub = memo(
 								<VisuallyHidden as="span">
 									{
 										/* translators: accessibility text */
-										__( '(opens in a new tab)' )
+										__( 'View site (opens in a new tab)' )
 									}
 								</VisuallyHidden>
 							</Button>
