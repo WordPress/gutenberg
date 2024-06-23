d3f8c81844742f8103c38218123c290c3a77b02a
Mikey Binns <hello@mikeybinns.com>
2024-06-23 16:27:41 +0100

    fix WPFormat references

--- packages/rich-text/src/component/use-anchor-ref.js
+++ packages/rich-text/src/component/use-anchor-ref.js
@@ -13,7 +13,7 @@ import { getActiveFormat } from '../get-active-format';
  * @template T
  * @typedef {import('@wordpress/element').RefObject<T>} RefObject<T>
  */
-/** @typedef {import('../register-format-type').WPFormat} WPFormat */
+/** @typedef {import('../types').RichTextFormatFull} RichTextFormatFull */
 /** @typedef {import('../types').RichTextValue} RichTextValue */
 
 /**
@@ -26,7 +26,7 @@ import { getActiveFormat } from '../get-active-format';
  * @param {RefObject<HTMLElement>} $1.ref      React ref of the element
  *                                             containing  the editable content.
  * @param {RichTextValue}          $1.value    Value to check for selection.
- * @param {WPFormat}               $1.settings The format type's settings.
+ * @param {RichTextFormatFull}     $1.settings The format type's settings.
  *
  * @return {Element|Range} The active element or selection range.
  */
