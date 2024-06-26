90669a8d8f023ff3754ed41fd39e2a17310f09a6
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-06-26 17:21:24 +0200

    Rename API from gutenberg_register_block_template() to gutenberg_register_template()

--- lib/block-templates.php
+++ lib/block-templates.php
@@ -5,6 +5,6 @@
  * @package gutenberg
  */
 
-function gutenberg_register_block_template( $template_name, $args = array() ) {
+function gutenberg_register_template( $template_name, $args = array() ) {
 	return WP_Block_Templates_Registry::get_instance()->register( $template_name, $args );
 }
