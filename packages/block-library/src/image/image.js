49bba7210895673350f76ab7cce561b26ffed3b2
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2024-06-27 10:49:19 +0200

    Pattern overrides: Fix aspect ratio not working in image with overrides (#62828)
    * Remove only supported attributes
    
    * Try alternative approach
    
    * Update comment
    
    * Return early while editing the original pattern
    
    * Move conditional inside conditional
    
    * Add missing check
    
    * Go back to running always bindings
    
    * Use block context to detect presence of parent pattern for overrides (#62861)
    
    * Use block context to detect presence of parent pattern
    
    * Regenerate fixtures
    
    * Update image block to use context for checking a parent pattern block exists
    
    * Rename context to `pattern/overrides` to be consistent with php code
    
    * Move pattern overrides context shim to pattern overrides hooks
    
    * Remove shim
    
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: SantosGuillamot <santosguillamot@git.wordpress.org>
    
    * Reduce diff
    
    * Change array order
    
    ---------
    
    Co-authored-by: SantosGuillamot <santosguillamot@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: cbravobernal <cbravobernal@git.wordpress.org>

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -445,16 +445,12 @@ export default function Image( {
 				return {};
 			}
 			const { getBlockBindingsSource } = unlock( select( blocksStore ) );
-			const { getBlockParentsByBlockName } = unlock(
-				select( blockEditorStore )
-			);
 			const {
 				url: urlBinding,
 				alt: altBinding,
 				title: titleBinding,
 			} = metadata?.bindings || {};
-			const hasParentPattern =
-				getBlockParentsByBlockName( clientId, 'core/block' ).length > 0;
+			const hasParentPattern = !! context[ 'pattern/overrides' ];
 			const urlBindingSource = getBlockBindingsSource(
 				urlBinding?.source
 			);
@@ -510,7 +506,12 @@ export default function Image( {
 					: __( 'Connected to dynamic data' ),
 			};
 		},
-		[ clientId, isSingleSelected, metadata?.bindings ]
+		[
+			arePatternOverridesEnabled,
+			context,
+			isSingleSelected,
+			metadata?.bindings,
+		]
 	);
 
 	const showUrlInput =
