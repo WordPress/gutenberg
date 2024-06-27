7ec0d1c3a3590857de02cd556fcb4d7387ea4ad9
Dani Guardiola <hi@daniguardio.la>
2024-06-27 20:57:50 +0200

    JSDoc tweak.

--- packages/components/src/tabs/tablist.tsx
+++ packages/components/src/tabs/tablist.tsx
@@ -38,11 +38,15 @@ type AnyFunction = ( ...args: any ) => any;
  * Creates a stable callback function that has access to the latest state and
  * can be used within event handlers and effect callbacks. Throws when used in
  * the render phase.
+ *
  * @example
+ *
+ * ```tsx
  * function Component(props) {
  *   const onClick = useEvent(props.onClick);
  *   React.useEffect(() => {}, [onClick]);
  * }
+ * ```
  */
 function useEvent< T extends AnyFunction >( callback?: T ) {
 	const ref = useRef< AnyFunction | undefined >( () => {
