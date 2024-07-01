a8cc36b0f54ba58c3a11fb6bdff25f640117d9d9
Matias Benedetto <matias.benedetto@gmail.com>
2024-07-01 23:33:15 +0400

    removes font size custom ordering

--- packages/edit-site/src/components/global-styles/font-sizes/font-sizes.js
+++ packages/edit-site/src/components/global-styles/font-sizes/font-sizes.js
@@ -4,7 +4,6 @@
 import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 import { __, sprintf } from '@wordpress/i18n';
 import {
-	__experimentalParseQuantityAndUnitFromRawValue as parseQuantityAndUnitFromRawValue,
 	__experimentalItemGroup as ItemGroup,
 	__experimentalVStack as VStack,
 	__experimentalHStack as HStack,
@@ -22,36 +21,6 @@ import Subtitle from '../subtitle';
 import { NavigationButtonAsItem } from '../navigation-button';
 import { getNewIndexFromPresets } from '../utils';
 
-/**
- * Coefficients to normalize font sizes to pixels.
- *
- * em/rm on units are bases the default font size of 16px.
- * Viewport units are based on a 1920x1080 screen.
- */
-const NORMALIZED_FONT_SIZE_COEFFICIENT = {
-	px: 1,
-	em: 16,
-	rem: 16,
-	vw: 19.2,
-	vh: 10.8,
-};
-
-/*
- * Normalize a font size value to a specific unit.
- *
- * @param {string} size The font size value to normalize.
- * @return {string} The normalized font size value.
- */
-function normalizeFontSize( size ) {
-	const [ quantity, unit ] = parseQuantityAndUnitFromRawValue( size );
-	const normalizedSize =
-		quantity * ( NORMALIZED_FONT_SIZE_COEFFICIENT[ unit ] ?? 1 );
-	if ( isNaN( normalizedSize ) || isNaN( quantity ) ) {
-		return 1;
-	}
-	return normalizedSize;
-}
-
 function FontSizes() {
 	const [ fontSizes, setFontSizes ] = useGlobalSetting(
 		'typography.fontSizes'
@@ -59,12 +28,6 @@ function FontSizes() {
 
 	// Get the font sizes from the theme or use the default ones.
 	const sizes = fontSizes.theme ?? fontSizes.default ?? [];
-	const normalizedSizes = sizes
-		.map( ( fontSize ) => ( {
-			...fontSize,
-			normalizedSize: normalizeFontSize( fontSize.size ),
-		} ) )
-		.sort( ( a, b ) => a.normalizedSize - b.normalizedSize );
 
 	const handleAddFontSize = () => {
 		const index = getNewIndexFromPresets( sizes, 'custom-' );
@@ -90,7 +53,7 @@ function FontSizes() {
 				/>
 			</HStack>
 			<ItemGroup isBordered isSeparated>
-				{ normalizedSizes.map( ( size ) => (
+				{ sizes.map( ( size ) => (
 					<NavigationButtonAsItem
 						key={ size.slug }
 						path={ '/typography/font-sizes/' + size.slug }
