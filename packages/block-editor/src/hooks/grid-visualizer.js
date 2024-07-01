cae62a320b959bb4e22d9f96f2adf6e62508d8f1
tellthemachines <isabel@tellthemachines.com>
2024-07-01 17:39:51 +1000

    Add basic visualizer hook

---
+++ packages/block-editor/src/hooks/grid-visualizer.js
@@ -0,0 +1,36 @@
+/**
+ * WordPress dependencies
+ */
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { addFilter } from '@wordpress/hooks';
+
+/**
+ * Internal dependencies
+ */
+import { GridVisualizer } from '../components/grid';
+
+const addGridVisualizerToBlockEdit = createHigherOrderComponent(
+	( BlockEdit ) => ( props ) => {
+		if (
+			! props.attributes?.layout ||
+			props.attributes.layout?.type !== 'grid'
+		) {
+			return <BlockEdit { ...props } />;
+		}
+
+		return (
+			<>
+				<GridVisualizer clientId={ props.clientId } />
+
+				<BlockEdit { ...props } />
+			</>
+		);
+	},
+	'addGridVisualizerToBlockEdit'
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'core/editor/grid-visualizer',
+	addGridVisualizerToBlockEdit
+);
