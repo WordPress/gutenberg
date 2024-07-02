73592062b62520058dff6f4527ab2197bc39ff02
Ella <ella@vandurpe.com>
2024-07-02 09:04:00 +0300

    Add comment

--- test/performance/specs/post-editor.spec.js
+++ test/performance/specs/post-editor.spec.js
@@ -668,7 +668,9 @@ test.describe( 'Post Editor Performance', () => {
 
 				const startTime = performance.now();
 
+				// This is the WP v6.5 and older locator.
 				const oldLocator = page.getByRole( 'button', { name: 'Test' } );
+				// This is the WP v6.6 and newer locator.
 				const newLocator = page.getByRole( 'tab', { name: 'Test' } );
 
 				await oldLocator.or( newLocator ).click();
