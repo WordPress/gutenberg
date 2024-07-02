74c34ef79d16b4ab763fc9f78b4c7713f8f5b7de
Lena Morita <lena@jaguchi.com>
2024-07-03 08:04:08 +0900

    ToolbarButton: Defer fix

--- packages/components/src/toolbar/toolbar-button/index.tsx
+++ packages/components/src/toolbar/toolbar-button/index.tsx
@@ -60,6 +60,10 @@ function UnforwardedToolbarButton(
 						className
 					) }
 					isPressed={ isActive }
+					// TODO: Should be focusable disabled, but adding `__experimentalIsFocusable` will trigger a
+					// focus bug when ToolbarButton is disabled via the `disabled` prop.
+					// Must address first: https://github.com/WordPress/gutenberg/issues/63070
+					// eslint-disable-next-line no-restricted-syntax
 					disabled={ isDisabled }
 					data-toolbar-item
 					{ ...extraProps }
@@ -85,6 +89,10 @@ function UnforwardedToolbarButton(
 				<Button
 					label={ title }
 					isPressed={ isActive }
+					// TODO: Should be focusable disabled, but adding `__experimentalIsFocusable` will trigger a
+					// focus bug when ToolbarButton is disabled via the `disabled` prop.
+					// Must address first: https://github.com/WordPress/gutenberg/issues/63070
+					// eslint-disable-next-line no-restricted-syntax
 					disabled={ isDisabled }
 					{ ...toolbarItemProps }
 				>
