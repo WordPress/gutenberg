47a19cd7fed18ea6c8d87a8fdcf6a48e53a6f066
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-03 16:00:08 +0200

    bench.sh

--- bench.sh
+++ bench.sh
@@ -2,6 +2,9 @@
 
 caffeinate -dsiu \
 hyperfine \
-	-L checkout fix/improve-components-ts-perf,trunk \
+	-L checkout cf45f4595b6201ff22d4aa8f9d44a3b0c30bf76a,d4baf294767,120b554aa46,475be71ee27,283a1e003f5 \
 	--prepare 'git checkout {checkout}' './node_modules/.bin/tsc --build packages/components --force' -n '{checkout}' \
-	--export-markdown bench.md
+	--export-markdown bench.md \
+	--show-output
+
+
