231bb367ccf3e3da15d6950ca4c2549334739d79
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-03 18:04:25 +0200

    bench

--- bench.sh
+++ bench.sh
@@ -3,5 +3,5 @@
 caffeinate -dsiu \
 hyperfine \
 	-L checkout origin/trunk,fix/improve-components-ts-perf \
-	--prepare 'git checkout {checkout}' './node_modules/.bin/tsc --build packages/components --force' -n '{checkout}' \
+	--prepare 'git checkout {checkout}' 'node --max-old-space-size=16000 ./node_modules/.bin/tsc --build packages/components --force' -n '{checkout}' \
 	--export-markdown bench.md
