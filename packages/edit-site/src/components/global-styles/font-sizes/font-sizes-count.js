1a22c99593c9bfecd8565c7c6b87f9bac3013baa
Matias Benedetto <matias.benedetto@gmail.com>
2024-07-04 18:19:03 +0400

    remove number in font size presets drilldown

--- packages/edit-site/src/components/global-styles/font-sizes/font-sizes-count.js
+++ packages/edit-site/src/components/global-styles/font-sizes/font-sizes-count.js
@@ -1,8 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
-import { __, _n, sprintf, isRTL } from '@wordpress/i18n';
+import { __, isRTL } from '@wordpress/i18n';
 import {
 	__experimentalItemGroup as ItemGroup,
 	__experimentalVStack as VStack,
@@ -14,26 +13,10 @@ import { Icon, chevronLeft, chevronRight } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import { unlock } from '../../../lock-unlock';
-const { useGlobalSetting } = unlock( blockEditorPrivateApis );
 import Subtitle from '../subtitle';
 import { NavigationButtonAsItem } from '../navigation-button';
 
 function FontSizes() {
-	const [ fontSizes ] = useGlobalSetting( 'typography.fontSizes' );
-
-	const [ defaultFontSizesEnabled ] = useGlobalSetting(
-		'typography.defaultFontSizes'
-	);
-
-	// Count the number of font sizes from the theme or use the default ones.
-	const fontSizesCount = [
-		// Default font sizes are only counted if enabled.
-		...( defaultFontSizesEnabled ? fontSizes.default || [] : [] ),
-		...( fontSizes.theme || [] ),
-		...( fontSizes.custom || [] ),
-	].length;
-
 	return (
 		<VStack spacing={ 2 }>
 			<HStack justify="space-between">
@@ -45,17 +28,7 @@ function FontSizes() {
 					aria-label={ __( 'Edit font size presets' ) }
 				>
 					<HStack direction="row">
-						<FlexItem>
-							{ sprintf(
-								/* translators: %d: number of font sizes */
-								_n(
-									'%d Font size preset',
-									'%d Font size presets',
-									fontSizesCount
-								),
-								fontSizesCount
-							) }
-						</FlexItem>
+						<FlexItem>{ __( 'Font size presets' ) }</FlexItem>
 						<Icon icon={ isRTL() ? chevronLeft : chevronRight } />
 					</HStack>
 				</NavigationButtonAsItem>
