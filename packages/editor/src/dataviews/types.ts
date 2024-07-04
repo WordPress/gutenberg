5847ea996a0b8be0eb32c06556898e95bfe794a8
Riad Benguella <benguella@gmail.com>
2024-07-04 10:10:00 +0200

    Better type

--- packages/editor/src/dataviews/types.ts
+++ packages/editor/src/dataviews/types.ts
@@ -17,6 +17,7 @@ export interface TemplateOrTemplatePart extends BasePost {
 	type: 'wp_template' | 'wp_template_part';
 	source: string;
 	has_theme_file: boolean;
+	id: string;
 }
 
 export type Post = TemplateOrTemplatePart | BasePost;
