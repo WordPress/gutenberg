deb650226b20299eb127ecd47264baed50a46977
Andrea Fercia <a.fercia@gmail.com>
2024-07-04 13:41:44 +0200

    Add back test.

--- test/e2e/specs/editor/various/is-typing.spec.js
+++ test/e2e/specs/editor/various/is-typing.spec.js
@@ -36,4 +36,36 @@ test.describe( 'isTyping', () => {
 		// Toolbar Popover is hidden again
 		await expect( blockToolbarPopover ).toBeHidden();
 	} );
+
+	test( 'should not close the dropdown when typing in it', async ( {
+		editor,
+		page,
+	} ) => {
+		// Add a block with a dropdown in the toolbar that contains an input.
+		await editor.insertBlock( { name: 'core/query' } );
+
+		await editor.canvas
+			.getByRole( 'document', { name: 'Block: Query Loop' } )
+			.getByRole( 'button', { name: 'Start blank' } )
+			.click();
+
+		await editor.canvas
+			.getByRole( 'button', { name: 'Title & Date' } )
+			.click();
+
+		// Moving the mouse shows the toolbar.
+		await editor.showBlockToolbar();
+		// Open the dropdown.
+		const displaySettings = page.getByRole( 'button', {
+			name: 'Display settings',
+		} );
+		await displaySettings.click();
+		const itemsPerPageInput = page.getByLabel( 'Items per Page' );
+		// Make sure we're where we think we are
+		await expect( itemsPerPageInput ).toBeFocused();
+		// Type inside the dropdown's input
+		await page.keyboard.type( '00' );
+		// The input should still be visible.
+		await expect( itemsPerPageInput ).toBeVisible();
+	} );
 } );
