9c4d5673e2cfb475793c8066e9c2d7239dc777cc
Riad Benguella <benguella@gmail.com>
2024-07-04 12:01:58 +0200

    DataViews: Register the reset template and template part action like any third-party action (#63017)
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>

--- packages/editor/src/dataviews/types.ts
+++ packages/editor/src/dataviews/types.ts
@@ -11,11 +11,16 @@ export interface BasePost {
 	status?: PostStatus;
 	title: string | { rendered: string };
 	type: string;
+	id: string | number;
 }
 export interface TemplateOrTemplatePart extends BasePost {
-	type: 'template' | 'template-part';
+	type: 'wp_template' | 'wp_template_part';
 	source: string;
 	has_theme_file: boolean;
+	id: string;
 }
 
 export type Post = TemplateOrTemplatePart | BasePost;
+
+// Will be unnecessary after typescript 5.0 upgrade.
+export type CoreDataError = { message?: string; code?: string };
