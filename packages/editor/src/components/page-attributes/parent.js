c45ab39a344a4f1022bac5dfcd13451bad33d37e
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-07-05 14:40:40 +1000

    Possesive.

--- packages/editor/src/components/page-attributes/parent.js
+++ packages/editor/src/components/page-attributes/parent.js
@@ -250,7 +250,7 @@ export function ParentRow() {
 						/>
 						<div>
 							{
-								/* translators: The domain name should be a reserved domain name to prevent linking to third party sites outside the WordPress projects control. You may also wish to use wordpress.org or a wordpress.org sub-domain. */
+								/* translators: The domain name should be a reserved domain name to prevent linking to third party sites outside the WordPress project's control. You may also wish to use wordpress.org or a wordpress.org sub-domain. */
 								__(
 									"Child pages inherit characteristics from their parent, such as URL structure. For instance, if 'Web Design' is a child of 'Services', its URL would be example.org/services/web-design."
 								)
