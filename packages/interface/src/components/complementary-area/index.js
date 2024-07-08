150a197bf98609db44bf864301e546d8e570bc55
Andrea Fercia <a.fercia@gmail.com>
2024-07-08 14:00:27 +0200

    Use compact variant for close butotn in complementary area.

--- packages/interface/src/components/complementary-area/index.js
+++ packages/interface/src/components/complementary-area/index.js
@@ -303,7 +303,7 @@ function ComplementaryArea( {
 					smallScreenTitle={ smallScreenTitle }
 					toggleButtonProps={ {
 						label: closeLabel,
-						size: 'small',
+						size: 'compact',
 						shortcut: toggleShortcut,
 						scope,
 						identifier,
