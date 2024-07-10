fd86a59f86777c767cfbed2096e8c9f242a6a934
Matias Benedetto <matias.benedetto@gmail.com>
2024-07-10 17:19:55 +0400

    use getComputedFluidTypographyValue to compute the size of the preview

--- packages/edit-site/src/components/global-styles/font-sizes/font-size-preview.js
+++ packages/edit-site/src/components/global-styles/font-sizes/font-size-preview.js
@@ -1,7 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
+import {
+	getComputedFluidTypographyValue,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -12,11 +15,23 @@ const { useGlobalStyle } = unlock( blockEditorPrivateApis );
 
 function FontSizePreview( { fontSize } ) {
 	const [ font ] = useGlobalStyle( 'typography' );
+
+	const input =
+		fontSize?.fluid?.min && fontSize?.fluid?.max
+			? {
+					minimumFontSize: fontSize.fluid.min,
+					maximumFontSize: fontSize.fluid.max,
+			  }
+			: {
+					fontSize: fontSize.size,
+			  };
+
+	const computedFontSize = getComputedFluidTypographyValue( input );
 	return (
 		<div
 			className="edit-site-typography-preview"
 			style={ {
-				fontSize: fontSize.size,
+				fontSize: computedFontSize,
 				fontFamily: font?.fontFamily ?? 'serif',
 			} }
 		>
