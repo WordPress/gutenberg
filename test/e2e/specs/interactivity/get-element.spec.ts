82c1bc209ddd955025e2866ca449f6a269f4ecf7
Michal Czaplinski <mmczaplinski@gmail.com>
2024-07-10 14:18:39 +0100

    Remove prefix from e2e test assertions

--- test/e2e/specs/interactivity/get-element.spec.ts
+++ test/e2e/specs/interactivity/get-element.spec.ts
@@ -20,7 +20,7 @@ test.describe( 'store', () => {
 
 	test( 'initial attributes can be accessed', async ( { page } ) => {
 		const el = page.getByTestId( 'read from attributes' );
-		await expect( el ).toHaveText( '+Initial value' );
+		await expect( el ).toHaveText( 'Initial value' );
 	} );
 
 	test( 'mutated attributes via DOM manipulation can be accessed', async ( {
@@ -29,7 +29,7 @@ test.describe( 'store', () => {
 		const button = page.getByTestId( 'mutate DOM' );
 		await button.click();
 		const el = page.getByTestId( 'read from attributes' );
-		await expect( el ).toHaveText( '++New DOM value' );
+		await expect( el ).toHaveText( 'New DOM value' );
 	} );
 
 	test( 'mutated attributes via data-wp-bind can be accessed', async ( {
