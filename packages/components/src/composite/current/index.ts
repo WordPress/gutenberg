0a68587dbf8e65e6eb134267c7cf2948d3eb35bd
Marco Ciampini <marco.ciampo@gmail.com>
2024-07-15 16:41:58 +0200

    Add JSDocs to Composite exports

--- packages/components/src/composite/current/index.ts
+++ packages/components/src/composite/current/index.ts
@@ -21,12 +21,114 @@ import {
 } from '@ariakit/react';
 import type { CompositeStore, CompositeStoreProps } from '@ariakit/react';
 
-// Exports
+/**
+ * Creates a composite store.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <Composite.Item>Item</Composite.Item>
+ *   <Composite.Item>Item</Composite.Item>
+ *   <Composite.Item>Item</Composite.Item>
+ * </Composite.Root>
+ * ```
+ */
 export const useStore = useCompositeStore;
+
+/**
+ * Renders a composite widget.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <Composite.Item>Item 1</Composite.Item>
+ *   <Composite.Item>Item 2</Composite.Item>
+ * </Composite.Root>
+ * ```
+ */
 export const Root = Composite;
+
+/**
+ * Renders a group element for composite items.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <Composite.Group>
+ *     <Composite.GroupLabel>Label</Composite.GroupLabel>
+ *     <Composite.Item>Item 1</Composite.Item>
+ *     <Composite.Item>Item 2</Composite.Item>
+ *   </CompositeGroup>
+ * </Composite.Root>
+ * ```
+ */
 export const Group = CompositeGroup;
+
+/**
+ * Renders a label in a composite group. This component must be wrapped with
+ * `CompositeGroup` so the `aria-labelledby` prop is properly set on the
+ * composite group element.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <Composite.Group>
+ *     <Composite.GroupLabel>Label</Composite.GroupLabel>
+ *     <Composite.Item>Item 1</Composite.Item>
+ *     <Composite.Item>Item 2</Composite.Item>
+ *   </CompositeGroup>
+ * </Composite.Root>
+ * ```
+ */
 export const GroupLabel = CompositeGroupLabel;
+
+/**
+ * Renders a composite item.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <Composite.Item>Item 1</Composite.Item>
+ *   <Composite.Item>Item 2</Composite.Item>
+ *   <Composite.Item>Item 3</Composite.Item>
+ * </Composite.Root>
+ * ```
+ */
 export const Item = CompositeItem;
+
+/**
+ * Renders a composite row. Wrapping `Composite.Item` elements within
+ * `Composite.Row` will create a two-dimensional composite widget, such as a
+ * grid.
+ * @see https://ariakit.org/components/composite
+ * @example
+ * ```jsx
+ * const useCompositeStore = Composite.useStore;
+ * const store = useCompositeStore();
+ * <Composite.Root store={store}>
+ *   <CompositeRow>
+ *     <Composite.Item>Item 1.1</Composite.Item>
+ *     <Composite.Item>Item 1.2</Composite.Item>
+ *     <Composite.Item>Item 1.3</Composite.Item>
+ *   </CompositeRow>
+ *   <CompositeRow>
+ *     <Composite.Item>Item 2.1</Composite.Item>
+ *     <Composite.Item>Item 2.2</Composite.Item>
+ *     <Composite.Item>Item 2.3</Composite.Item>
+ *   </CompositeRow>
+ * </Composite.Root>
+ * ```
+ */
 export const Row = CompositeRow;
 
 export type Store = CompositeStore;
