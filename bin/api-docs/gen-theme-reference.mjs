743cddf13d2a74d5c8954a66aa2d2ec3da20bbe6
Alex Lende <alex+github.com@lende.xyz>
2024-07-17 09:05:11 -0500

    Update variable names

--- bin/api-docs/gen-theme-reference.mjs
+++ bin/api-docs/gen-theme-reference.mjs
@@ -19,7 +19,7 @@ import $RefParser from '@apidevtools/json-schema-ref-parser';
  *
  * @type {URL}
  */
-const THEME_JSON_SCHEMA_FILE = new URL(
+const THEME_JSON_SCHEMA_URL = new URL(
 	'../../schemas/json/theme.json',
 	import.meta.url
 );
@@ -29,7 +29,7 @@ const THEME_JSON_SCHEMA_FILE = new URL(
  *
  * @type {URL}
  */
-const THEME_JSON_REF_DOC = new URL(
+const REFERENCE_DOC_URL = new URL(
 	'../../docs/reference-guides/theme-json-reference/theme-json-living.md',
 	import.meta.url
 );
@@ -140,20 +140,21 @@ function generateTypes( schema ) {
 /**
  * Generate documentation from theme.json schema.
  *
- * @param {JSONSchema} themejson JSON schema
+ * @param {JSONSchema} themeJson theme.json JSON schema
  * @return {string} generated documentation
  */
-function generateDocs( themejson ) {
-	let autogen = '';
+function generateDocs( themeJson ) {
+	/** Markdown content. */
+	let md = '';
 
 	/* --------------- *
 	 * Settings        *
 	 * --------------- */
-	autogen += '## settings\n\n';
-	autogen += `${ themejson.properties.settings.description }\n\n`;
+	md += '## settings\n\n';
+	md += `${ themeJson.properties.settings.description }\n\n`;
 	const settings = [
 		// Top-level only properties.
-		...Object.entries( themejson.properties.settings.allOf[ 1 ].properties )
+		...Object.entries( themeJson.properties.settings.allOf[ 1 ].properties )
 			.filter( ( [ property ] ) => property !== 'blocks' )
 			.map( ( [ property, subschema ] ) => [
 				property,
@@ -163,16 +164,16 @@ function generateDocs( themejson ) {
 				},
 			] ),
 		// Top-level and blocks properties.
-		...themejson.properties.settings.allOf[ 0 ].allOf.flatMap(
+		...themeJson.properties.settings.allOf[ 0 ].allOf.flatMap(
 			( subschema ) => Object.entries( subschema.properties )
 		),
 	];
 	for ( const [ section, schema ] of settings ) {
-		autogen += `### ${ section }\n\n`;
-		autogen += `${ schema.description }\n\n`;
+		md += `### ${ section }\n\n`;
+		md += `${ schema.description }\n\n`;
 		if ( schema.properties ) {
-			autogen += '| Property | Description | Type | Default |\n';
-			autogen += '| -------- | ----------- | ---- | ------- |\n';
+			md += '| Property | Description | Type | Default |\n';
+			md += '| -------- | ----------- | ---- | ------- |\n';
 			const properties = Object.entries( schema.properties );
 			for ( const [ property, subschema ] of properties ) {
 				const description =
@@ -182,109 +183,111 @@ function generateDocs( themejson ) {
 					'default' in subschema
 						? `\`${ JSON.stringify( subschema.default ) }\``
 						: '';
-				autogen += `| ${ property } | ${ description } | ${ types } | ${ defaultValue } |\n`;
+				md += `| ${ property } | ${ description } | ${ types } | ${ defaultValue } |\n`;
 			}
-			autogen += '\n';
+			md += '\n';
 		}
-		autogen += `---\n\n`;
+		md += `---\n\n`;
 	}
 
 	/* --------------- *
 	 * Styles          *
 	 * --------------- */
-	autogen += '## styles\n\n';
-	autogen += `${ themejson.properties.styles.description }\n\n`;
+	md += '## styles\n\n';
+	md += `${ themeJson.properties.styles.description }\n\n`;
 	const styles = Object.entries(
-		themejson.properties.styles.allOf[ 0 ].properties
+		themeJson.properties.styles.allOf[ 0 ].properties
 	);
 	for ( const [ section, schema ] of styles ) {
-		autogen += `### ${ section }\n\n`;
-		autogen += `${ schema.description }\n\n`;
+		md += `### ${ section }\n\n`;
+		md += `${ schema.description }\n\n`;
 		if ( schema.properties ) {
-			autogen += '| Property | Description | Type |\n';
-			autogen += '| -------- | ----------- | ---- |\n';
+			md += '| Property | Description | Type |\n';
+			md += '| -------- | ----------- | ---- |\n';
 			const properties = Object.entries( schema.properties );
 			for ( const [ property, subschema ] of properties ) {
 				// Assuming that the first line of a description is a summary.
 				const description =
 					subschema.description?.split( '\n', 1 )[ 0 ] ?? '';
 				const types = generateTypes( subschema );
-				autogen += `| ${ property } | ${ description } | ${ types } |\n`;
+				md += `| ${ property } | ${ description } | ${ types } |\n`;
 			}
-			autogen += '\n';
+			md += '\n';
 		}
-		autogen += `---\n\n`;
+		md += `---\n\n`;
 	}
 
 	/* --------------- *
 	 * customTemplates *
 	 * --------------- */
-	autogen += '## customTemplates\n\n';
-	autogen += `${ themejson.properties.customTemplates.description }\n\n`;
-	autogen += '| Property | Description | Type |\n';
-	autogen += '| -------- | ----------- | ---- |\n';
+	md += '## customTemplates\n\n';
+	md += `${ themeJson.properties.customTemplates.description }\n\n`;
+	md += '| Property | Description | Type |\n';
+	md += '| -------- | ----------- | ---- |\n';
 	const customTemplatesProperties = Object.entries(
-		themejson.properties.customTemplates.items.properties
+		themeJson.properties.customTemplates.items.properties
 	);
 	for ( const [ property, subschema ] of customTemplatesProperties ) {
 		const { description } = subschema;
 		const types = generateTypes( subschema );
-		autogen += `| ${ property } | ${ description } | ${ types } |\n`;
+		md += `| ${ property } | ${ description } | ${ types } |\n`;
 	}
-	autogen += '\n';
+	md += '\n';
 
 	/* --------------- *
 	 * templateParts   *
 	 * --------------- */
-	autogen += '## templateParts\n\n';
-	autogen += `${ themejson.properties.templateParts.description }\n\n`;
-	autogen += '| Property | Description | Type |\n';
-	autogen += '| -------- | ----------- | ---- |\n';
+	md += '## templateParts\n\n';
+	md += `${ themeJson.properties.templateParts.description }\n\n`;
+	md += '| Property | Description | Type |\n';
+	md += '| -------- | ----------- | ---- |\n';
 	const templatePartsProperties = Object.entries(
-		themejson.properties.templateParts.items.properties
+		themeJson.properties.templateParts.items.properties
 	);
 	for ( const [ property, subschema ] of templatePartsProperties ) {
 		const { description } = subschema;
 		const types = generateTypes( subschema );
-		autogen += `| ${ property } | ${ description } | ${ types } |\n`;
+		md += `| ${ property } | ${ description } | ${ types } |\n`;
 	}
-	autogen += '\n';
+	md += '\n';
 
 	/* --------------- *
 	 * patterns        *
 	 * --------------- */
-	autogen += '## patterns\n\n';
-	autogen += `Type: ${ generateTypes( themejson.properties.patterns ) }.\n\n`;
-	autogen += themejson.properties.patterns.description + '\n';
+	md += '## patterns\n\n';
+	md += `Type: ${ generateTypes( themeJson.properties.patterns ) }.\n\n`;
+	md += themeJson.properties.patterns.description + '\n';
 
-	return autogen;
+	return md;
 }
 
 /**
  * Main function.
  */
 async function main() {
-	const themejson = await $RefParser.dereference(
-		THEME_JSON_SCHEMA_FILE.pathname,
+	const themeJson = await $RefParser.dereference(
+		THEME_JSON_SCHEMA_URL.pathname,
 		{
 			parse: { binary: false, text: false, yaml: false },
 			resolve: { external: false },
 		}
 	);
 
-	const content = await fs.readFile( THEME_JSON_REF_DOC, {
+	const themeJsonReference = await fs.readFile( REFERENCE_DOC_URL, {
 		encoding: 'utf8',
 		flag: 'r',
 	} );
 
-	const autogen = generateDocs( themejson );
-	const newContent = content.replace(
+	const generatedDocs = generateDocs( themeJson );
+	const updatedThemeJsonReference = themeJsonReference.replace(
 		// `.` does not match new lines, but `[^]` will.
 		new RegExp( `${ START_TOKEN }[^]*${ END_TOKEN }` ),
-		`${ START_TOKEN }\n${ autogen }\n${ END_TOKEN }`
+		`${ START_TOKEN }\n${ generatedDocs }\n${ END_TOKEN }`
 	);
 
-	await fs.writeFile( THEME_JSON_REF_DOC, newContent, { encoding: 'utf8' } );
+	await fs.writeFile( REFERENCE_DOC_URL, updatedThemeJsonReference, {
+		encoding: 'utf8',
+	} );
 }
 
 main().catch( ( error ) => {
