e13c5a6b6bebc7c5563fd0c923fe8c1633885c93
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-07-17 12:47:25 +0200

    Inline get_callback function

--- lib/compat/wordpress-6.7/rest-api.php
+++ lib/compat/wordpress-6.7/rest-api.php
@@ -51,26 +51,6 @@ if ( ! function_exists( 'wp_api_template_registry' ) ) {
 add_filter( 'register_post_type_args', 'wp_api_template_registry', 10, 2 );
 
 /**
- * Registers additional fields for wp_template rest api.
- *
- * @access private
- * @internal
- *
- * @param  array $template_object Template object.
- * @return string                 Human readable text for the author.
- */
-function _gutenberg_get_wp_templates_plugin_field( $template_object ) {
-	if ( $template_object ) {
-		$registered_template = WP_Templates_Registry::get_instance()->get_by_slug( $template_object['slug'] );
-		if ( $registered_template ) {
-			return $registered_template->plugin;
-		}
-	}
-
-	return;
-}
-
-/**
  * Adds `plugin` fields to WP_REST_Templates_Controller class.
  */
 function gutenberg_register_wp_rest_templates_controller_plugin_field() {
@@ -79,7 +59,16 @@ function gutenberg_register_wp_rest_templates_controller_plugin_field() {
 		'wp_template',
 		'plugin',
 		array(
-			'get_callback'    => '_gutenberg_get_wp_templates_plugin_field',
+			'get_callback'    => function ( $template_object ) {
+				if ( $template_object ) {
+					$registered_template = WP_Templates_Registry::get_instance()->get_by_slug( $template_object['slug'] );
+					if ( $registered_template ) {
+						return $registered_template->plugin;
+					}
+				}
+
+				return;
+			},
 			'update_callback' => null,
 			'schema'          => array(
 				'type'        => 'string',
