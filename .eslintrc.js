5c9c9720c93489d1af0a6680f0c74dc7d053d5ad
Lena Morita <lena@jaguchi.com>
2024-07-24 03:18:38 +0900

    Add eslint rule for unlabeled RangeControl

--- .eslintrc.js
+++ .eslintrc.js
@@ -278,7 +278,6 @@ module.exports = {
 			},
 		},
 		{
-			// Temporary rules until we're ready to officially deprecate the bottom margins.
 			files: [ 'packages/*/src/**/*.[tj]s?(x)' ],
 			excludedFiles: [
 				'packages/components/src/**/@(test|stories)/**',
@@ -289,6 +288,7 @@ module.exports = {
 					'error',
 					...restrictedSyntax,
 					...restrictedSyntaxComponents,
+					// Temporary rules until we're ready to officially deprecate the bottom margins.
 					...[
 						'CheckboxControl',
 						'ComboboxControl',
@@ -301,6 +301,12 @@ module.exports = {
 						message:
 							componentName +
 							' should have the `__nextHasNoMarginBottom` prop to opt-in to the new margin-free styles.',
+					} ) ),
+					// Accessibility rules for components.
+					...[ 'RangeControl' ].map( ( componentName ) => ( {
+						selector: `JSXOpeningElement[name.name="${ componentName }"]:not(:has(JSXAttribute[name.name="label"]))`,
+						message:
+							'Component must be accessibly labeled with a `label` prop. Use the `hideLabelFromVision` prop if desired. Only ignore this error if the component is correctly associated with a separate `<label>` element.',
 					} ) ),
 				],
 			},
