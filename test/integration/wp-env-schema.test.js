fe3986c3365c28018fe75e7dd778ea296d524e18
Aki Hamano <tetsuaki.hamano@gmail.com>
2024-07-25 13:31:16 +0900

    Simplify

--- test/integration/wp-env-schema.test.js
+++ test/integration/wp-env-schema.test.js
@@ -2,15 +2,14 @@
  * External dependencies
  */
 import Ajv from 'ajv';
-import glob from 'fast-glob';
 
 /**
  * Internal dependencies
  */
 import wpEnvSchema from '../../schemas/json/wp-env.json';
+import wpEnvJsonFile from '../../.wp-env.json';
 
 describe( '.wp-env.json schema', () => {
-	const jsonFiles = glob.sync( [ '.wp-env.json' ], { onlyFiles: true } );
 	const ajv = new Ajv( {
 		allowMatchingProperties: true,
 	} );
@@ -25,13 +24,9 @@ describe( '.wp-env.json schema', () => {
 		expect( result.errors ).toBe( null );
 	} );
 
-	test( 'found .wp-env.json files', () => {
-		expect( jsonFiles.length ).toBeGreaterThan( 0 );
-	} );
-
-	test.each( jsonFiles )( 'validates schema for `%s`', ( filepath ) => {
+	test( 'validates schema for .wp-env.json', () => {
 		// We want to validate the .wp-env.json file using the local schema.
-		const { $schema, ...metadata } = require( filepath );
+		const { $schema, ...metadata } = wpEnvJsonFile;
 
 		// we expect the $schema property to be present in the .wp-env.json file
 		expect( $schema ).toBeTruthy();
