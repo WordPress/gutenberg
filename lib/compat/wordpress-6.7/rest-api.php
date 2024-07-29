70094bedcabb341263d9c923aea25ee9524639ac
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-07-29 23:49:37 +0200

    Initial commit. Add meta field to post types.

--- lib/compat/wordpress-6.7/rest-api.php
+++ lib/compat/wordpress-6.7/rest-api.php
@@ -114,3 +114,27 @@ function gutenberg_override_default_rest_server() {
 	return 'Gutenberg_REST_Server';
 }
 add_filter( 'wp_rest_server_class', 'gutenberg_override_default_rest_server', 1 );
+
+if ( ! function_exists( 'gutenberg_register_wp_rest_post_types_meta_fields' ) ) {
+	/**
+	 * Adds `template` and `template_lock` fields to WP_REST_Post_Types_Controller class.
+	 */
+	function gutenberg_register_wp_rest_post_types_meta_fields() {
+		register_rest_field(
+			'type',
+			'meta',
+			array(
+				'get_callback' => function ( $item ) {
+					return get_registered_meta_keys( $item['slug'] );
+				},
+				'schema'       => array(
+					'type'        => 'array',
+					'description' => __( 'Meta Keys', 'gutenberg' ),
+					'readonly'    => true,
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+			)
+		);
+	}
+}
+add_action( 'rest_api_init', 'gutenberg_register_wp_rest_post_types_meta_fields' );
