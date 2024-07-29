4b873d66118aa94ba2166e1d743fa4379d265fe1
André <583546+oandregal@users.noreply.github.com>
2024-07-29 10:22:34 +0200

    DataForm: add author to quick edit page/post list (#63983)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>

--- packages/dataviews/src/components/dataform/stories/index.story.tsx
+++ packages/dataviews/src/components/dataform/stories/index.story.tsx
@@ -25,16 +25,26 @@ const fields = [
 		label: 'Order',
 		type: 'integer' as const,
 	},
+	{
+		id: 'author',
+		label: 'Author',
+		type: 'integer' as const,
+		elements: [
+			{ value: 1, label: 'Jane' },
+			{ value: 2, label: 'John' },
+		],
+	},
 ];
 
 export const Default = () => {
 	const [ post, setPost ] = useState( {
 		title: 'Hello, World!',
 		order: 2,
+		author: 1,
 	} );
 
 	const form = {
-		visibleFields: [ 'title', 'order' ],
+		visibleFields: [ 'title', 'order', 'author' ],
 	};
 
 	return (
