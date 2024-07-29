982b68ce294854a9e6d77357ff12b1f802dd231b
Ramon <ramonjd@users.noreply.github.com>
2024-07-29 13:32:37 +1000

    Style Book: clearly denote heading levels (#64038)
    * Use Heading descriptors to describe the heading variants.
    
    * Add Heading 6
    
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: ndiego <ndiego@git.wordpress.org>

--- packages/edit-site/src/components/style-book/index.js
+++ packages/edit-site/src/components/style-book/index.js
@@ -136,24 +136,28 @@ function getExamples() {
 		category: 'text',
 		blocks: [
 			createBlock( 'core/heading', {
-				content: __( 'Code Is Poetry' ),
+				content: __( 'Heading 1' ),
 				level: 1,
 			} ),
 			createBlock( 'core/heading', {
-				content: __( 'Code Is Poetry' ),
+				content: __( 'Heading 2' ),
 				level: 2,
 			} ),
 			createBlock( 'core/heading', {
-				content: __( 'Code Is Poetry' ),
+				content: __( 'Heading 3' ),
 				level: 3,
 			} ),
 			createBlock( 'core/heading', {
-				content: __( 'Code Is Poetry' ),
+				content: __( 'Heading 4' ),
 				level: 4,
 			} ),
 			createBlock( 'core/heading', {
-				content: __( 'Code Is Poetry' ),
+				content: __( 'Heading 5' ),
 				level: 5,
+			} ),
+			createBlock( 'core/heading', {
+				content: __( 'Heading 6' ),
+				level: 6,
 			} ),
 		],
 	};
