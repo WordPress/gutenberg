11a35b9ff0f562d7932a4c1ce2d5f25f8d518af4
Mario Santos <santosguillamot@gmail.com>
2024-07-30 16:43:34 +0200

    Add comments

--- packages/block-editor/src/utils/block-bindings.js
+++ packages/block-editor/src/utils/block-bindings.js
@@ -12,10 +12,37 @@ import { useBlockEditContext } from '../components/block-edit';
 export function useBlockBindingsUtils() {
 	const { clientId } = useBlockEditContext();
 	const { updateBlockAttributes } = useDispatch( blockEditorStore );
-	// TODO: Review if this is correct.
 	const { getBlockAttributes } = useSelect( blockEditorStore );
 
-	// TODO: Add docs.
+	/**
+	 * Updates the value of the bindings connected to block attributes.
+	 * It removes the binding when the new value is `undefined`.
+	 *
+	 * @param {Object} bindings        Bindings including the attributes to update and the new object.
+	 * @param {string} bindings.source The source name to connect to.
+	 * @param {Object} [bindings.args] Object containing the arguments needed by the source.
+	 *
+	 * @example
+	 * ```js
+	 * import { useBlockBindingsUtils } from '@wordpress/block-editor'
+	 *
+	 * const { removeAllBlockBindings } = useBlockBindingsUtils();
+	 * updateBlockBindings( {
+	 *     url: {
+	 *         source: 'core/post-meta',
+	 *         args: {
+	 *             key: 'url_custom_field',
+	 *         },
+	 * 	   },
+	 *     alt: {
+	 *         source: 'core/post-meta',
+	 *         args: {
+	 *             key: 'text_custom_field',
+	 *         },
+	 * 	   }
+	 * } );
+	 * ```
+	 */
 	const updateBlockBindings = ( bindings ) => {
 		const { metadata } = getBlockAttributes( clientId );
 		const newBindings = { ...metadata?.bindings };
@@ -43,7 +70,18 @@ export function useBlockBindingsUtils() {
 					: newMetadata,
 		} );
 	};
-	// TODO: Add docs.
+
+	/**
+	 * Removes the bindings property of the `metadata` attribute.
+	 *
+	 * @example
+	 * ```js
+	 * import { useBlockBindingsUtils } from '@wordpress/block-editor'
+	 *
+	 * const { removeAllBlockBindings } = useBlockBindingsUtils();
+	 * removeAllBlockBindings();
+	 * ```
+	 */
 	const removeAllBlockBindings = () => {
 		const { metadata } = getBlockAttributes( clientId );
 		const newMetadata = { ...metadata };
