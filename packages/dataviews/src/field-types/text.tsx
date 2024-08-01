58166dbfb0c9ec8563dadff6fc382940849ae308
André <583546+oandregal@users.noreply.github.com>
2024-08-01 15:20:18 +0200

    DataForm: centralize edit logic in field type definitions (#64171)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>

--- packages/dataviews/src/field-types/text.tsx
+++ packages/dataviews/src/field-types/text.tsx
@@ -1,7 +1,17 @@
 /**
+ * WordPress dependencies
+ */
+import { TextControl } from '@wordpress/components';
+import { useCallback } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
-import type { SortDirection, ValidationContext } from '../types';
+import type {
+	SortDirection,
+	ValidationContext,
+	DataFormControlProps,
+} from '../types';
 
 function sort( valueA: any, valueB: any, direction: SortDirection ) {
 	return direction === 'asc'
@@ -20,7 +30,36 @@ function isValid( value: any, context?: ValidationContext ) {
 	return true;
 }
 
+function Edit< Item >( {
+	data,
+	field,
+	onChange,
+}: DataFormControlProps< Item > ) {
+	const { id, label, placeholder } = field;
+	const value = field.getValue( { item: data } );
+
+	const onChangeControl = useCallback(
+		( newValue: string ) =>
+			onChange( ( prevItem: Item ) => ( {
+				...prevItem,
+				[ id ]: newValue,
+			} ) ),
+		[ id, onChange ]
+	);
+
+	return (
+		<TextControl
+			label={ label }
+			placeholder={ placeholder }
+			value={ value ?? '' }
+			onChange={ onChangeControl }
+			__next40pxDefaultSize
+		/>
+	);
+}
+
 export default {
 	sort,
 	isValid,
+	Edit,
 };
