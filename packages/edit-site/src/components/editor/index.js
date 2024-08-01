c04a6f4446feb41ac2b55b262e10368fec6604b0
Joen A. <1204802+jasmussen@users.noreply.github.com>
2024-08-01 10:38:32 +0200

    Update: chevron to home icon. (#64161)

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -22,7 +22,7 @@ import { store as noticesStore } from '@wordpress/notices';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 import { store as preferencesStore } from '@wordpress/preferences';
 import { decodeEntities } from '@wordpress/html-entities';
-import { Icon, chevronLeft } from '@wordpress/icons';
+import { Icon, homeButton } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -292,7 +292,7 @@ export default function EditSiteEditor( { isPostsList = false } ) {
 											) }
 											variants={ toggleHomeIconVariants }
 										>
-											<Icon icon={ chevronLeft } />
+											<Icon icon={ homeButton } />
 										</motion.div>
 									</motion.div>
 								)
