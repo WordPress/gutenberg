d12d992cbf4fc0777ef1b3a48b754c294f059c4f
Carolina Nymark <hi@themesbycarolina.com>
2024-08-01 12:28:13 +0200

    try to fix CS issues

--- lib/compat/wordpress-6.7/taxonomy.php
+++ lib/compat/wordpress-6.7/taxonomy.php
@@ -8,12 +8,15 @@
 /**
  * Add support for the post format taxonomy in the REST API.
  * See wp-includes/taxonomy.php line 170.
- * 
+ *
  * Needed to add post formats to the taxonomy filter in the query loop.
- * 
+ *
  * @since 6.7.0
  */
-add_filter('register_post_format_taxonomy_args', function( $args ) {
-	$args['show_in_rest'] = true;
-	return $args;
-} );
+add_filter(
+	'register_post_format_taxonomy_args',
+	function( $args ) {
+		$args['show_in_rest'] = true;
+		return $args;
+	}
+);
