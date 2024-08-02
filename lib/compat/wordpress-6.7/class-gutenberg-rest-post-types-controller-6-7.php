b0800134922e43ba352cf4970d7d67967da3cf5b
Tyler Bailey <tyler.bailey@10up.com>
2024-08-02 15:04:42 -0400

    Adjust order of rendering_mode filters

--- lib/compat/wordpress-6.7/class-gutenberg-rest-post-types-controller-6-7.php
+++ lib/compat/wordpress-6.7/class-gutenberg-rest-post-types-controller-6-7.php
@@ -26,7 +26,18 @@ class Gutenberg_REST_Post_Types_Controller_6_7 extends WP_REST_Post_Types_Contro
 		// Property will only exist if the post type supports the block editor.
 		if ( 'edit' === $context && property_exists( $item, 'default_rendering_mode' ) ) {
 			/**
+			 * Filters the block editor rendering mode for a post type.
+			 *
+			 * @since 6.7.0
+			 * @param string       $default_rendering_mode Default rendering mode for the post type.
+			 * @param WP_Post_Type $post_type              Post type name.
+			 * @return string Default rendering mode for the post type.
+			 */
+			$rendering_mode = apply_filters( 'post_type_default_rendering_mode', $item->default_rendering_mode, $item );
+
+			/**
 			 * Filters the block editor rendering mode for a specific post type.
+			 * Applied after the generic `post_type_default_rendering_mode` filter.
 			 *
 			 * The dynamic portion of the hook name, `$item->name`, refers to the post type slug.
 			 *
@@ -35,17 +46,7 @@ class Gutenberg_REST_Post_Types_Controller_6_7 extends WP_REST_Post_Types_Contro
 			 * @param WP_Post_Type $post_type              Post type object.
 			 * @return string Default rendering mode for the post type.
 			 */
-			$rendering_mode = apply_filters( "post_type_{$item->name}_default_rendering_mode", $item->default_rendering_mode, $item );
-
-			/**
-			 * Filters the block editor rendering mode for a post type.
-			 *
-			 * @since 6.7.0
-			 * @param string       $default_rendering_mode Default rendering mode for the post type.
-			 * @param WP_Post_Type $post_type              Post type name.
-			 * @return string Default rendering mode for the post type.
-			 */
-			$rendering_mode = apply_filters( 'post_type_default_rendering_mode', $rendering_mode, $item );
+			$rendering_mode = apply_filters( "post_type_{$item->name}_default_rendering_mode", $rendering_mode, $item );
 
 			// Validate the filtered rendering mode.
 			if ( ! in_array( $rendering_mode, gutenberg_post_type_rendering_modes(), true ) ) {
