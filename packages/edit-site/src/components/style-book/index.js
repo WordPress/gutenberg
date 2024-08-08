93b2b7fa190ca4fd8ad2a3af10295620917fd42c
Dani Guardiola <hi@daniguardio.la>
2024-08-08 15:29:12 +0200

    Fix style-book tabs.

--- packages/edit-site/src/components/style-book/index.js
+++ packages/edit-site/src/components/style-book/index.js
@@ -249,16 +249,18 @@ function StyleBook( {
 				{ showTabs ? (
 					<div className="edit-site-style-book__tabs">
 						<Tabs>
-							<Tabs.TabList>
-								{ tabs.map( ( tab ) => (
-									<Tabs.Tab
-										tabId={ tab.name }
-										key={ tab.name }
-									>
-										{ tab.title }
-									</Tabs.Tab>
-								) ) }
-							</Tabs.TabList>
+							<div className="edit-site-style-book__tablist-container">
+								<Tabs.TabList>
+									{ tabs.map( ( tab ) => (
+										<Tabs.Tab
+											tabId={ tab.name }
+											key={ tab.name }
+										>
+											{ tab.title }
+										</Tabs.Tab>
+									) ) }
+								</Tabs.TabList>
+							</div>
 							{ tabs.map( ( tab ) => (
 								<Tabs.TabPanel
 									key={ tab.name }
