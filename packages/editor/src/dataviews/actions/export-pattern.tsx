fa2dcd094b49d98cedfd344ac6962d4924d723e4
Riad Benguella <benguella@gmail.com>
2024-08-08 15:42:57 +0200

    Restore export pattern check

--- packages/editor/src/dataviews/actions/export-pattern.tsx
+++ packages/editor/src/dataviews/actions/export-pattern.tsx
@@ -37,7 +37,7 @@ const exportPattern: Action< Pattern > = {
 	id: 'export-pattern',
 	label: __( 'Export as JSON' ),
 	supportsBulk: true,
-	isEligible: ( item ) => item.type !== 'wp_template_part',
+	isEligible: ( item ) => item.type === 'wp_block',
 	callback: async ( items ) => {
 		if ( items.length === 1 ) {
 			return downloadBlob(
