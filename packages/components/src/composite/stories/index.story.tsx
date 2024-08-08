918ae6a5b2c8d6fd4e2b15ead318d2ed22c4f225
Marco Ciampini <marco.ciampo@gmail.com>
2024-08-08 20:01:16 +0200

    Manual Storybook args table

--- packages/components/src/composite/stories/index.story.tsx
+++ packages/components/src/composite/stories/index.story.tsx
@@ -34,21 +34,156 @@ const meta: Meta< typeof UseCompositeStorePlaceholder > = {
 			canvas: { sourceState: 'shown' },
 			source: { transform },
 			extractArgTypes: ( component: React.FunctionComponent ) => {
-				const name = component.displayName;
-				const path = name
-					?.replace(
-						/([a-z])([A-Z])/g,
-						( _, a, b ) => `${ a }-${ b.toLowerCase() }`
-					)
-					.toLowerCase();
-				const url = `https://ariakit.org/reference/${ path }`;
-				return {
-					props: {
-						name: 'Props',
-						description: `See <a href="${ url }">Ariakit docs</a> for <code>${ name }</code>`,
-						table: { type: { summary: undefined } },
+				const commonArgTypes = {
+					render: {
+						name: 'render',
+						description:
+							'Allows the component to be rendered as a different HTML element or React component. The value can be a React element or a function that takes in the original component props and gives back a React element with the props merged.',
+						table: {
+							type: {
+								summary:
+									'RenderProp<React.HTMLAttributes<any> & { ref?: React.Ref<any> | undefined; }> | React.ReactElement<any, string | React.JSXElementConstructor<any>>',
+							},
+						},
+						// type: { required: true },
+					},
+					children: {
+						name: 'children',
+						description: 'The contents of the component.',
+						table: { type: { summary: 'React.ReactNode' } },
 					},
 				};
+				const argTypes = {
+					useCompositeStore: {
+						activeId: {
+							name: 'activeId',
+							description:
+								'The current active item id. The active item is the element within the composite widget that has either DOM or virtual focus.',
+							table: { type: { summary: 'string | null' } },
+						},
+						defaultActiveId: {
+							name: 'defaultActiveId',
+							description:
+								'The composite item id that should be active by default when the composite widget is rendered. If `null`, the composite element itself will have focus and users will be able to navigate to it using arrow keys. If `undefined`, the first enabled item will be focused.',
+							table: { type: { summary: 'string | null' } },
+						},
+						setActiveId: {
+							name: 'setActiveId',
+							description:
+								'A callback that gets called when the activeId state changes.',
+							table: {
+								type: {
+									summary:
+										'((activeId: string | null | undefined) => void)',
+								},
+							},
+						},
+						focusLoop: {
+							name: 'focusLoop',
+							description:
+								'Determines how the focus behaves when the user reaches the end of the composite widget.',
+							table: {
+								defaultValue: {
+									summary: 'false',
+								},
+								type: {
+									summary:
+										"boolean | 'horizontal' | 'vertical' | 'both'",
+								},
+							},
+						},
+						focusShift: {
+							name: 'focusShift',
+							description:
+								"Works only on two-dimensional composite widgets. If enabled, moving up or down when there's no next item or when the next item is disabled will shift to the item right before it.",
+							table: {
+								defaultValue: {
+									summary: 'false',
+								},
+								type: {
+									summary: 'boolean',
+								},
+							},
+						},
+						focusWrap: {
+							name: 'focusWrap',
+							description:
+								'Works only on two-dimensional composite widgets. If enabled, moving to the next item from the last one in a row or column will focus on the first item in the next row or column and vice-versa.',
+							table: {
+								defaultValue: {
+									summary: 'false',
+								},
+								type: {
+									summary: 'boolean',
+								},
+							},
+						},
+						virtualFocus: {
+							name: 'virtualFocus',
+							description:
+								'If enabled, the composite element will act as an aria-activedescendant⁠ container instead of roving tabindex⁠. DOM focus will remain on the composite element while its items receive virtual focus. In both scenarios, the item in focus will carry the data-active-item attribute.',
+							table: {
+								defaultValue: {
+									summary: 'false',
+								},
+								type: {
+									summary: 'boolean',
+								},
+							},
+						},
+						orientation: {
+							name: 'orientation',
+							description:
+								"Defines the orientation of the composite widget. If the composite has a single row or column (one-dimensional), the orientation value determines which arrow keys can be used to move focus. It doesn't have any effect on two-dimensional composites.",
+							table: {
+								defaultValue: {
+									summary: "'both'",
+								},
+								type: {
+									summary:
+										"'horizontal' | 'vertical' | 'both'",
+								},
+							},
+						},
+						rtl: {
+							name: 'rtl',
+							description:
+								'Determines how the next and previous functions will behave. If rtl is set to true, they will be inverted. This only affects the composite widget behavior. You still need to set dir="rtl" on HTML/CSS.',
+							table: {
+								defaultValue: {
+									summary: 'false',
+								},
+								type: {
+									summary: 'boolean',
+								},
+							},
+						},
+					},
+					Composite: {
+						...commonArgTypes,
+						store: {
+							name: 'store',
+							description:
+								'Object returned by the `useCompositeStore` hook.',
+							table: {
+								type: {
+									summary:
+										'CompositeStore<CompositeStoreItem>',
+								},
+							},
+						},
+					},
+					'Composite.Group': commonArgTypes,
+					'Composite.GroupLabel': commonArgTypes,
+					'Composite.Row': commonArgTypes,
+					'Composite.Item': commonArgTypes,
+				};
+
+				const name = component.displayName ?? 'useCompositeStore';
+
+				return name in argTypes
+					? argTypes[ name as keyof typeof argTypes ]
+					: {};
 			},
 		},
 	},
