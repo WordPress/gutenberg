5cfd217a3e1482998145451985b9c6b182b118c7
Hiroshi Urabe <mail@torounit.com>
2024-08-11 19:55:04 +0900

    test for fallback

--- test/e2e/specs/editor/blocks/media-text.spec.js
+++ test/e2e/specs/editor/blocks/media-text.spec.js
@@ -66,7 +66,7 @@ test.describe( 'Media & Text', () => {
 		const secondaryImage = await requestUtils.uploadMedia(
 			path.join(
 				'./test/e2e/assets',
-				'3200x2400_e2e_test_image_responsive_lightbox.jpeg'
+				'1024x768_e2e_test_image_size.jpeg'
 			)
 		);
 
@@ -80,6 +80,65 @@ test.describe( 'Media & Text', () => {
 		expect( await editor.getEditedPostContent() )
 			.toMatch( `<!-- wp:media-text {"mediaId":${ secondaryImage.id },"mediaLink":"${ secondaryImage.link }","mediaType":"image","mediaSizeSlug":"medium"} -->
 <div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="${ secondaryImage.media_details.sizes.medium.source_url }" alt="" class="wp-image-${ secondaryImage.id } size-medium"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…"} -->
+<p></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->` );
+	} );
+
+	test( `Should fall back to full if an image of the same size does not exist.`, async ( {
+		editor,
+		page,
+		requestUtils,
+	} ) => {
+		const primaryImage = await requestUtils.uploadMedia(
+			path.join(
+				'./test/e2e/assets',
+				'1024x768_e2e_test_image_size.jpeg'
+			)
+		);
+
+		await editor.insertBlock( {
+			name: 'core/media-text',
+		} );
+
+		const mediaAndTextBlock = editor.canvas.locator(
+			'role=document[name="Block: Media & Text"i]'
+		);
+		await expect( mediaAndTextBlock ).toBeVisible();
+		await editor.canvas
+			.locator( 'role=button[name="Media Library"i]' )
+			.click();
+		const mediaLibrary = page.locator(
+			'role=dialog[name="Select or Upload Media"i]'
+		);
+		await expect( mediaLibrary ).toBeVisible();
+		await page.locator( `role=checkbox[checked=false]` ).click();
+		await page.locator( 'role=button[name="Select"i]' ).click();
+		await page
+			.locator( `role=combobox[name="Resolution"i]` )
+			.selectOption( 'Medium' );
+
+		expect( await editor.getEditedPostContent() )
+			.toMatch( `<!-- wp:media-text {"mediaId":${ primaryImage.id },"mediaLink":"${ primaryImage.link }","mediaType":"image","mediaSizeSlug":"medium"} -->
+<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="${ primaryImage.media_details.sizes.medium.source_url }" alt="" class="wp-image-${ primaryImage.id } size-medium"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…"} -->
+<p></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->` );
+
+		const secondaryImage = await requestUtils.uploadMedia(
+			path.join( './test/e2e/assets', '10x10_e2e_test_image_z9T8jK.png' )
+		);
+
+		await editor.clickBlockToolbarButton( 'Replace' );
+		await page
+			.locator( 'role=menuitem[name="Open Media Library"i]' )
+			.click();
+
+		await page.locator( `role=checkbox[checked=false]` ).click();
+		await page.locator( 'role=button[name="Select"i]' ).click();
+		expect( await editor.getEditedPostContent() )
+			.toMatch( `<!-- wp:media-text {"mediaId":${ secondaryImage.id },"mediaLink":"${ secondaryImage.link }","mediaType":"image"} -->
+<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="${ secondaryImage.source_url }" alt="" class="wp-image-${ secondaryImage.id } size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…"} -->
 <p></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:media-text -->` );
