f777aa26265174341722911132f3d972d0c54681
Ella <ella@vandurpe.com>
2024-08-15 16:17:24 +0200

    Fix select all with formatting

--- packages/dom/src/dom/is-entirely-selected.js
+++ packages/dom/src/dom/is-entirely-selected.js
@@ -1,7 +1,6 @@
 /**
  * Internal dependencies
  */
-import { assertIsDefined } from '../utils/assert-is-defined';
 import isInputOrTextArea from './is-input-or-text-area';
 
 /**
@@ -26,59 +25,16 @@ export default function isEntirelySelected( element ) {
 
 	const { ownerDocument } = element;
 	const { defaultView } = ownerDocument;
-	assertIsDefined( defaultView, 'defaultView' );
+
+	if ( ! defaultView ) {
+		return false;
+	}
+
 	const selection = defaultView.getSelection();
-	assertIsDefined( selection, 'selection' );
-	const range = selection.rangeCount ? selection.getRangeAt( 0 ) : null;
 
-	if ( ! range ) {
-		return true;
+	if ( ! selection ) {
+		return false;
 	}
 
-	const { startContainer, endContainer, startOffset, endOffset } = range;
-
-	if (
-		startContainer === element &&
-		endContainer === element &&
-		startOffset === 0 &&
-		endOffset === element.childNodes.length
-	) {
-		return true;
-	}
-
-	const lastChild = element.lastChild;
-	assertIsDefined( lastChild, 'lastChild' );
-	const endContainerContentLength =
-		endContainer.nodeType === endContainer.TEXT_NODE
-			? /** @type {Text} */ ( endContainer ).data.length
-			: endContainer.childNodes.length;
-
-	return (
-		isDeepChild( startContainer, element, 'firstChild' ) &&
-		isDeepChild( endContainer, element, 'lastChild' ) &&
-		startOffset === 0 &&
-		endOffset === endContainerContentLength
-	);
-}
-
-/**
- * Check whether the contents of the element have been entirely selected.
- * Returns true if there is no possibility of selection.
- *
- * @param {HTMLElement|Node}         query     The element to check.
- * @param {HTMLElement}              container The container that we suspect "query" may be a first or last child of.
- * @param {"firstChild"|"lastChild"} propName  "firstChild" or "lastChild"
- *
- * @return {boolean} True if query is a deep first/last child of container, false otherwise.
- */
-function isDeepChild( query, container, propName ) {
-	/** @type {HTMLElement | ChildNode | null} */
-	let candidate = container;
-	do {
-		if ( query === candidate ) {
-			return true;
-		}
-		candidate = candidate[ propName ];
-	} while ( candidate );
-	return false;
+	return element.textContent === selection.toString();
 }
