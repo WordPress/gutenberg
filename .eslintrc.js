8e84c47554964a0eeb02349638ca8235698c43a1
Lena Morita <lena@jaguchi.com>
2024-08-16 06:30:54 +0900

    NumberControl: Add lint rule for 40px size prop usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -315,6 +315,7 @@ module.exports = {
 						'BorderControl',
 						'DimensionControl',
 						'FontSizePicker',
+						'NumberControl',
 						'ToggleGroupControl',
 					].map( ( componentName ) => ( {
 						// Falsy `__next40pxDefaultSize` without a non-default `size` prop.
