c46823f50873e677a17428d45799c1531339966d
akasunil <sdprajapati999@gmail.com>
2024-08-16 18:53:52 +0530

    Remove styles from empty post navigation link element

--- packages/block-library/src/post-navigation-link/index.php
+++ packages/block-library/src/post-navigation-link/index.php
@@ -118,7 +118,7 @@ function render_block_core_post_navigation_link( $attributes, $content ) {
 
 	return sprintf(
 		'<div %1$s>%2$s</div>',
-		$wrapper_attributes,
+		! empty( $content ) ? $wrapper_attributes : "class=\"post-navigation-link {$classes}\"",
 		$content
 	);
 }
