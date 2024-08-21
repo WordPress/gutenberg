9853ca326538881d3792c9654b64cd1e9a1dbaff
David Arenas <david.arenas@automattic.com>
2024-08-21 21:13:03 +0200

    Move context handlers outside

--- packages/interactivity/src/proxies/context.ts
+++ packages/interactivity/src/proxies/context.ts
@@ -13,6 +13,78 @@ const contextObjectToFallback = new WeakMap();
 
 const descriptor = Reflect.getOwnPropertyDescriptor;
 
+const contextHandlers: ProxyHandler< object > = {
+	get: ( target: object, k: string ) => {
+		const fallback = contextObjectToFallback.get( target );
+		// Always subscribe to prop changes in the current context.
+		const currentProp = target[ k ];
+
+		// Return the inherited prop when missing in target.
+		if ( ! ( k in target ) && k in fallback ) {
+			return fallback[ k ];
+		}
+
+		// Proxify plain objects that were not directly assigned.
+		if (
+			k in target &&
+			! contextAssignedObjects.get( target )?.has( k ) &&
+			isPlainObject( currentProp )
+		) {
+			return proxifyContext( currentProp );
+		}
+
+		// Return the stored proxy for `currentProp` when it exists.
+		if ( contextObjectToProxy.has( currentProp ) ) {
+			return contextObjectToProxy.get( currentProp );
+		}
+
+		/*
+		 * For other cases, return the value from target, also
+		 * subscribing to changes in the parent context when the current
+		 * prop is not defined.
+		 */
+		return k in target ? currentProp : fallback[ k ];
+	},
+	set: ( target, k, value ) => {
+		const fallback = contextObjectToFallback.get( target );
+		const obj = k in target || ! ( k in fallback ) ? target : fallback;
+
+		/*
+		 * Assigned object values should not be proxified so they point
+		 * to the original object and don't inherit unexpected
+		 * properties.
+		 */
+		if ( value && typeof value === 'object' ) {
+			if ( ! contextAssignedObjects.has( obj ) ) {
+				contextAssignedObjects.set( obj, new Set() );
+			}
+			contextAssignedObjects.get( obj ).add( k );
+		}
+
+		/*
+		 * When the value is a proxy, it's because it comes from the
+		 * context, so the inner value is assigned instead.
+		 */
+		if ( contextProxyToObject.has( value ) ) {
+			const innerValue = contextProxyToObject.get( value );
+			obj[ k ] = innerValue;
+		} else {
+			obj[ k ] = value;
+		}
+
+		return true;
+	},
+	ownKeys: ( target ) => [
+		...new Set( [
+			...Object.keys( contextObjectToFallback.get( target ) ),
+			...Object.keys( target ),
+		] ),
+	],
+	getOwnPropertyDescriptor: ( target, k ) =>
+		descriptor( target, k ) ||
+		descriptor( contextObjectToFallback.get( target ), k ),
+};
+
 /**
  * Wrap a context object with a proxy to reproduce the context stack. The proxy
  * uses the passed `inherited` context as a fallback to look up for properties
@@ -34,78 +106,7 @@ export const proxifyContext = (
 	// Update the fallback object reference when it changes.
 	contextObjectToFallback.set( current, inherited );
 	if ( ! contextObjectToProxy.has( current ) ) {
-		const proxy = new Proxy( current, {
-			get: ( target: object, k: string ) => {
-				const fallback = contextObjectToFallback.get( current );
-				// Always subscribe to prop changes in the current context.
-				const currentProp = target[ k ];
-
-				// Return the inherited prop when missing in target.
-				if ( ! ( k in target ) && k in fallback ) {
-					return fallback[ k ];
-				}
-
-				// Proxify plain objects that were not directly assigned.
-				if (
-					k in target &&
-					! contextAssignedObjects.get( target )?.has( k ) &&
-					isPlainObject( currentProp )
-				) {
-					return proxifyContext( currentProp );
-				}
-
-				// Return the stored proxy for `currentProp` when it exists.
-				if ( contextObjectToProxy.has( currentProp ) ) {
-					return contextObjectToProxy.get( currentProp );
-				}
-
-				/*
-				 * For other cases, return the value from target, also
-				 * subscribing to changes in the parent context when the current
-				 * prop is not defined.
-				 */
-				return k in target ? currentProp : fallback[ k ];
-			},
-			set: ( target, k, value ) => {
-				const fallback = contextObjectToFallback.get( current );
-				const obj =
-					k in target || ! ( k in fallback ) ? target : fallback;
-
-				/*
-				 * Assigned object values should not be proxified so they point
-				 * to the original object and don't inherit unexpected
-				 * properties.
-				 */
-				if ( value && typeof value === 'object' ) {
-					if ( ! contextAssignedObjects.has( obj ) ) {
-						contextAssignedObjects.set( obj, new Set() );
-					}
-					contextAssignedObjects.get( obj ).add( k );
-				}
-
-				/*
-				 * When the value is a proxy, it's because it comes from the
-				 * context, so the inner value is assigned instead.
-				 */
-				if ( contextProxyToObject.has( value ) ) {
-					const innerValue = contextProxyToObject.get( value );
-					obj[ k ] = innerValue;
-				} else {
-					obj[ k ] = value;
-				}
-
-				return true;
-			},
-			ownKeys: ( target ) => [
-				...new Set( [
-					...Object.keys( contextObjectToFallback.get( current ) ),
-					...Object.keys( target ),
-				] ),
-			],
-			getOwnPropertyDescriptor: ( target, k ) =>
-				descriptor( target, k ) ||
-				descriptor( contextObjectToFallback.get( current ), k ),
-		} );
+		const proxy = new Proxy( current, contextHandlers );
 		contextObjectToProxy.set( current, proxy );
 		contextProxyToObject.set( proxy, current );
 	}
