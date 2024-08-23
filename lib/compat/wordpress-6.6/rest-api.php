84733c58b7d7dadc9c22c7c2a764b7f15a4df84a
rishishah-multidots <rishi.shah@multidots.com>
2024-08-23 19:41:23 +0530

    resolve PHP coding standards errors

--- lib/compat/wordpress-6.6/rest-api.php
+++ lib/compat/wordpress-6.6/rest-api.php
@@ -168,8 +168,8 @@ if ( ! function_exists( 'update_comment_meta_from_rest_request' ) ) {
 
 		if ( isset( $request['comment_type'] ) && ! empty( $request['comment_type'] ) ) {
 			$comment_data = array(
-				'comment_ID' => $comment->comment_ID,
-				'comment_type' => $request['comment_type'],
+				'comment_ID'       => $comment->comment_ID,
+				'comment_type'     => $request['comment_type'],
 				'comment_approved' => isset( $request['comment_approved'] ) ? $request['comment_approved'] : 0,
 			);
 
@@ -177,8 +177,8 @@ if ( ! function_exists( 'update_comment_meta_from_rest_request' ) ) {
 		}
 
 		$author = get_user_by( 'login', $response->data['author_name'] );
-		if ($author) {
-			$avatar_url = get_avatar_url( $author->ID );
+		if ( $author ) {
+			$avatar_url                           = get_avatar_url( $author->ID );
 			$response->data['author_avatar_urls'] = array(
 				'default' => $avatar_url,
 				'48'      => add_query_arg( 's', 48, $avatar_url ),
@@ -187,13 +187,13 @@ if ( ! function_exists( 'update_comment_meta_from_rest_request' ) ) {
 		}
 
 		$comment_id = $comment->comment_ID;
-		$meta_key = 'block_comment';
+		$meta_key   = 'block_comment';
 		$meta_value = get_comment_meta( $comment_id, $meta_key, true );
 
-		if ( !empty( $meta_value ) ) {
+		if ( ! empty( $meta_value ) ) {
 			$response->data['meta'] = $meta_value;
 		}
-		
+
 		return $response;
 	}
 }
