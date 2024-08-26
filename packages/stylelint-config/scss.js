62d4d2b2971669db4fec72a974d2c608dea4fb5d
Mikey Binns <38146638+mikeybinns@users.noreply.github.com>
2024-08-27 00:39:36 +0100

    update stylelint deps to latest and convert deprecated rules to @stylistic

--- packages/stylelint-config/scss.js
+++ packages/stylelint-config/scss.js
@@ -18,14 +18,14 @@ module.exports = {
 			},
 		],
 
-		'block-opening-brace-space-before': 'always',
-		'block-closing-brace-newline-after': [
+		'@stylistic/block-opening-brace-space-before': 'always',
+		'@stylistic/block-closing-brace-newline-after': [
 			'always',
 			{
 				ignoreAtRules: [ 'if', 'else' ],
 			},
 		],
-		'at-rule-name-space-after': 'always',
+		'@stylistic/at-rule-name-space-after': 'always',
 		'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
 		'scss/at-else-closing-brace-space-after': 'always-intermediate',
 		'scss/at-else-empty-line-before': 'never',
