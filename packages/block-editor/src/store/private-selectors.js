90390b267ccfc95e51c7d68333846f10acb0a90c
Ella <ella@vandurpe.com>
2024-08-27 18:41:32 +0200

    Inserter: fix loading indicator for reusable blocks

--- packages/block-editor/src/store/private-selectors.js
+++ packages/block-editor/src/store/private-selectors.js
@@ -421,9 +421,11 @@ const EMPTY_ARRAY = [];
 export const getReusableBlocks = createRegistrySelector(
 	( select ) => ( state ) => {
 		const reusableBlocksSelect = state.settings[ reusableBlocksSelectKey ];
-		return reusableBlocksSelect
-			? reusableBlocksSelect( select )
-			: state.settings.__experimentalReusableBlocks ?? EMPTY_ARRAY;
+		return (
+			( reusableBlocksSelect
+				? reusableBlocksSelect( select )
+				: state.settings.__experimentalReusableBlocks ) ?? EMPTY_ARRAY
+		);
 	}
 );
 
