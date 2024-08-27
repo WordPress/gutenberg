94335a4d7bf1ece68487e31a68dd84b06c38ea69
Luigi Teschio <gigitux@gmail.com>
2024-08-27 12:02:55 +0200

    improve default image control

--- packages/dataviews/src/dataform-controls/image.tsx
+++ packages/dataviews/src/dataform-controls/image.tsx
@@ -5,6 +5,9 @@ import {
 	Button,
 	FormFileUpload,
 	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+	BaseControl,
+	VisuallyHidden,
 } from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
 
@@ -12,13 +15,15 @@ import { useCallback } from '@wordpress/element';
  * Internal dependencies
  */
 import type { DataFormControlProps } from '../types';
+import { __ } from '@wordpress/i18n';
 
 export default function Image< Item >( {
 	data,
 	field,
 	onChange,
+	hideLabelFromVision,
 }: DataFormControlProps< Item > ) {
-	const { id } = field;
+	const { id, label } = field;
 	const value = field.getValue( { item: data } );
 
 	const onChangeControl = useCallback(
@@ -31,41 +36,48 @@ export default function Image< Item >( {
 
 	return (
 		<fieldset className="dataviews-controls__image">
-			{ value && (
-				<img
-					src={ value }
-					alt=""
-					style={ {
-						maxWidth: '200px',
-						maxHeight: '200px',
-					} }
-				/>
+			{ hideLabelFromVision ? (
+				<VisuallyHidden as="legend">{ label }</VisuallyHidden>
+			) : (
+				<BaseControl.VisualLabel as="legend">
+					{ label }
+				</BaseControl.VisualLabel>
 			) }
-			<HStack>
-				<FormFileUpload
-					accept="image/*"
-					onChange={ ( event ) => {
-						if ( event.target.files?.length ) {
-							const objectUrl = URL.createObjectURL(
-								event.target.files[ 0 ]
-							);
-							onChangeControl( objectUrl );
-						}
-					} }
-				>
-					<Button variant="primary">Upload</Button>
-				</FormFileUpload>
-				{ value && (
-					<Button
-						variant="secondary"
-						onClick={ () => {
-							onChangeControl( '' );
+			<VStack
+				className="dataviews-controls__image-container"
+				justify="flex-start"
+			>
+				{ value && <img src={ value } alt="" /> }
+				{ ! value && (
+					<span className="dataviews-controls__image-placeholder" />
+				) }
+				<HStack justify="flex-start">
+					<FormFileUpload
+						accept="image/*"
+						__next40pxDefaultSize
+						onChange={ ( event ) => {
+							if ( event.target.files?.length ) {
+								const objectUrl = URL.createObjectURL(
+									event.target.files[ 0 ]
+								);
+								onChangeControl( objectUrl );
+							}
 						} }
 					>
-						Clean
-					</Button>
-				) }
-			</HStack>
+						<Button variant="primary">{ __( 'Upload' ) }</Button>
+					</FormFileUpload>
+					{ value && (
+						<Button
+							variant="secondary"
+							onClick={ () => {
+								onChangeControl( '' );
+							} }
+						>
+							{ __( 'Remove' ) }
+						</Button>
+					) }
+				</HStack>
+			</VStack>
 		</fieldset>
 	);
 }
