2c8922e159c825ce0bf3869c548c5a65a0389945
Jerry Jones <jones.jeremydavid@gmail.com>
2024-08-28 08:29:20 -0500

    Remove ability to crop image if content only mode (#64838)

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -358,7 +358,11 @@ export default function Image( {
 	}, [ isSingleSelected ] );
 
 	const canEditImage = id && naturalWidth && naturalHeight && imageEditing;
-	const allowCrop = isSingleSelected && canEditImage && ! isEditingImage;
+	const allowCrop =
+		isSingleSelected &&
+		canEditImage &&
+		! isEditingImage &&
+		! isContentOnlyMode;
 
 	function switchToCover() {
 		replaceBlocks(
