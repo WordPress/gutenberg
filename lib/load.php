f8f9c1e4d8934315b24296911136ab5ae3d87432
George Mamadashvili <georgemamadashvili@gmail.com>
2024-08-28 08:34:58 +0400

    Disable autosave endpoints

--- lib/load.php
+++ lib/load.php
@@ -109,6 +109,7 @@ require __DIR__ . '/compat/wordpress-6.7/block-bindings.php';
 require __DIR__ . '/compat/wordpress-6.7/script-modules.php';
 require __DIR__ . '/compat/wordpress-6.7/class-wp-block-templates-registry.php';
 require __DIR__ . '/compat/wordpress-6.7/compat.php';
+require __DIR__ . '/compat/wordpress-6.7/post.php';
 
 // Experimental features.
 require __DIR__ . '/experimental/block-editor-settings-mobile.php';
