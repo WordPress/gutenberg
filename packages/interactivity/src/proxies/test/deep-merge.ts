0fd4b7ad7e96a837658a40c0da8735945b8fba6e
David Arenas <david.arenas@automattic.com>
2024-08-29 13:33:20 +0200

    Remove extraneous label

--- packages/interactivity/src/proxies/test/deep-merge.ts
+++ packages/interactivity/src/proxies/test/deep-merge.ts
@@ -88,7 +88,7 @@ describe( 'Interactivity API', () => {
 			expect( sourceExecuted ).toBe( false );
 		} );
 
-		https: it( 'should handle setters', () => {
+		it( 'should handle setters', () => {
 			let targetValue = 1;
 			const target = {
 				get a() {
