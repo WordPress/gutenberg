60832b839f8f423636930221af920d3fdea8735a
Pascal Birchler <pascalb@google.com>
2024-08-30 14:43:20 +0200

    Use `ButtonGroup`

--- packages/language-chooser/src/language-chooser/active-controls.tsx
+++ packages/language-chooser/src/language-chooser/active-controls.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
+import { Button, ButtonGroup } from '@wordpress/components';
 
 interface ActiveControlsProps {
 	onMoveUp: () => void;
@@ -21,79 +21,71 @@ function ActiveControls( {
 	isRemoveDisabled,
 }: ActiveControlsProps ) {
 	return (
-		<div className="language-chooser__active-locales-controls">
-			<ul>
-				<li>
-					<Button
-						variant="secondary"
-						showTooltip
-						aria-keyshortcuts="ArrowUp"
-						aria-label={ sprintf(
-							/* translators: accessibility text */
-							__( 'Move up (%s)' ),
-							/* translators: keyboard shortcut (Arrow Up) */
-							__( 'Up' )
-						) }
-						label={
-							/* translators: keyboard shortcut (Arrow Up) */
-							__( 'Up' )
-						}
-						disabled={ isMoveUpDisabled }
-						accessibleWhenDisabled
-						onClick={ onMoveUp }
-						__next40pxDefaultSize
-					>
-						{ __( 'Move Up' ) }
-					</Button>
-				</li>
-				<li>
-					<Button
-						variant="secondary"
-						showTooltip
-						aria-keyshortcuts="ArrowDown"
-						aria-label={ sprintf(
-							/* translators: accessibility text */
-							__( 'Move down (%s)' ),
-							/* translators: keyboard shortcut (Arrow Down) */
-							__( 'Down' )
-						) }
-						label={
-							/* translators: keyboard shortcut (Arrow Down) */
-							__( 'Down' )
-						}
-						disabled={ isMoveDownDisabled }
-						accessibleWhenDisabled
-						onClick={ onMoveDown }
-						__next40pxDefaultSize
-					>
-						{ __( 'Move Down' ) }
-					</Button>
-				</li>
-				<li>
-					<Button
-						variant="secondary"
-						showTooltip
-						aria-keyshortcuts="Delete"
-						aria-label={ sprintf(
-							/* translators: accessibility text */
-							__( 'Remove from list (%s)' ),
-							/* translators: keyboard shortcut (Delete / Backspace) */
-							__( 'Delete' )
-						) }
-						label={
-							/* translators: keyboard shortcut (Delete / Backspace) */
-							__( 'Delete' )
-						}
-						disabled={ isRemoveDisabled }
-						accessibleWhenDisabled
-						onClick={ onRemove }
-						__next40pxDefaultSize
-					>
-						{ __( 'Remove' ) }
-					</Button>
-				</li>
-			</ul>
-		</div>
+		<ButtonGroup>
+			<Button
+				variant="secondary"
+				showTooltip
+				aria-keyshortcuts="ArrowUp"
+				aria-label={ sprintf(
+					/* translators: accessibility text */
+					__( 'Move up (%s)' ),
+					/* translators: keyboard shortcut (Arrow Up) */
+					__( 'Up' )
+				) }
+				label={
+					/* translators: keyboard shortcut (Arrow Up) */
+					__( 'Up' )
+				}
+				disabled={ isMoveUpDisabled }
+				accessibleWhenDisabled
+				onClick={ onMoveUp }
+				__next40pxDefaultSize
+			>
+				{ __( 'Move Up' ) }
+			</Button>
+			<Button
+				variant="secondary"
+				showTooltip
+				aria-keyshortcuts="ArrowDown"
+				aria-label={ sprintf(
+					/* translators: accessibility text */
+					__( 'Move down (%s)' ),
+					/* translators: keyboard shortcut (Arrow Down) */
+					__( 'Down' )
+				) }
+				label={
+					/* translators: keyboard shortcut (Arrow Down) */
+					__( 'Down' )
+				}
+				disabled={ isMoveDownDisabled }
+				accessibleWhenDisabled
+				onClick={ onMoveDown }
+				__next40pxDefaultSize
+			>
+				{ __( 'Move Down' ) }
+			</Button>
+			<Button
+				variant="secondary"
+				showTooltip
+				aria-keyshortcuts="Delete"
+				aria-label={ sprintf(
+					/* translators: accessibility text */
+					__( 'Remove from list (%s)' ),
+					/* translators: keyboard shortcut (Delete / Backspace) */
+					__( 'Delete' )
+				) }
+				label={
+					/* translators: keyboard shortcut (Delete / Backspace) */
+					__( 'Delete' )
+				}
+				disabled={ isRemoveDisabled }
+				accessibleWhenDisabled
+				onClick={ onRemove }
+				__next40pxDefaultSize
+			>
+				{ __( 'Remove' ) }
+			</Button>
+		</ButtonGroup>
 	);
 }
 
