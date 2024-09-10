b470bad58505aa1c8865098e7f62e3777573fe99
Dave Smith <getdavemail@gmail.com>
2024-09-10 10:26:23 +0100

    Add toggle to block

---
+++ packages/editor/src/hooks/block-settings-inspector-toggle.js
@@ -0,0 +1,63 @@
+/**
+ * WordPress dependencies
+ */
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { __unstableBlockSettingsMenuFirstItem as BlockSettingsMenuFirstItem } from '@wordpress/block-editor';
+import { MenuItem } from '@wordpress/components';
+import { store as interfaceStore } from '@wordpress/interface';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __, isRTL } from '@wordpress/i18n';
+import { drawerLeft, drawerRight } from '@wordpress/icons';
+
+/**
+ * Override the default edit UI to include a new block inspector control for
+ * assigning a partial syncing controls to supported blocks in the pattern editor.
+ * Currently, only the `core/paragraph` block is supported.
+ *
+ * @param {Component} BlockEdit Original component.
+ *
+ * @return {Component} Wrapped component.
+ */
+const withBlockToolbar = createHigherOrderComponent(
+	( BlockEdit ) => ( props ) => {
+		return (
+			<>
+				<BlockEdit key="edit" { ...props } />
+				{ props.isSelected && <BlockInspectorToggle /> }
+			</>
+		);
+	},
+	'withBlockToolbar'
+);
+
+const BlockInspectorToggle = () => {
+	const { enableComplementaryArea } = useDispatch( interfaceStore );
+
+	const isOpen = useSelect(
+		( select ) =>
+			select( interfaceStore ).getActiveComplementaryArea( 'core' ) ===
+			'edit-post/block',
+		[]
+	);
+
+	return (
+		<BlockSettingsMenuFirstItem>
+			<MenuItem
+				onClick={ () => {
+					enableComplementaryArea( 'core', 'edit-post/block' );
+				} }
+				icon={ isRTL() ? drawerLeft : drawerRight }
+				disabled={ isOpen }
+			>
+				{ __( 'Open Block settings' ) }
+			</MenuItem>
+		</BlockSettingsMenuFirstItem>
+	);
+};
+
+addFilter(
+	'editor.BlockEdit',
+	'core/editor/with-block-settings-inspector-toggle',
+	withBlockToolbar
+);
