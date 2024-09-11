69e0c0e195dd355d9ea79fc046a161a10baefa15
AKSHAT2802 <akshat.kakkad2802@gmail.com>
2024-09-11 15:12:34 +0530

    Remove redundant code and add const

--- packages/block-library/src/navigation/edit/deleted-navigation-warning.js
+++ packages/block-library/src/navigation/edit/deleted-navigation-warning.js
@@ -7,40 +7,27 @@ import { __ } from '@wordpress/i18n';
 import { createInterpolateElement } from '@wordpress/element';
 
 function DeletedNavigationWarning( { onCreateNew, isNotice = false } ) {
+	const message = createInterpolateElement(
+		__(
+			'Navigation Menu has been deleted or is unavailable. <button>Create a new Menu?</button>'
+		),
+		{
+			button: (
+				<Button
+					__next40pxDefaultSize
+					onClick={ onCreateNew }
+					variant="link"
+				/>
+			),
+		}
+	);
+
 	return isNotice ? (
 		<Notice status="warning" isDismissible={ false }>
-			{ createInterpolateElement(
-				__(
-					'Navigation Menu has been deleted or is unavailable. <button>Create a new Menu?</button>'
-				),
-				{
-					button: (
-						<Button
-							__next40pxDefaultSize
-							onClick={ onCreateNew }
-							variant="link"
-						/>
-					),
-				}
-			) }
+			{ message }
 		</Notice>
 	) : (
-		<Warning>
-			{ createInterpolateElement(
-				__(
-					'Navigation Menu has been deleted or is unavailable. <button>Create a new Menu?</button>'
-				),
-				{
-					button: (
-						<Button
-							__next40pxDefaultSize
-							onClick={ onCreateNew }
-							variant="link"
-						/>
-					),
-				}
-			) }
-		</Warning>
+		<Warning>{ message }</Warning>
 	);
 }
 
