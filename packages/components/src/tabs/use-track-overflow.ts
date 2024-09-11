141fedcb670b8dd377c044a9b421ad6361d629fe
Dani Guardiola <hi@daniguardio.la>
2024-09-11 18:29:41 +0200

    Fix IntersectionObserver logic.

--- packages/components/src/tabs/use-track-overflow.ts
+++ packages/components/src/tabs/use-track-overflow.ts
@@ -11,15 +11,18 @@ export function useTrackOverflow(
 		last: HTMLElement | undefined | null;
 	}
 ) {
-	const [ first, setFirst ] = useState( true );
-	const [ last, setLast ] = useState( true );
+	const [ first, setFirst ] = useState( false );
+	const [ last, setLast ] = useState( false );
 	const [ observer, setObserver ] = useState< IntersectionObserver >();
 
-	const callback: IntersectionObserverCallback = useEvent( ( [ entry ] ) => {
-		if ( entry.target === children.first ) {
-			setFirst( ! entry.isIntersecting );
-		} else if ( entry.target === children.last ) {
-			setLast( ! entry.isIntersecting );
+	const callback: IntersectionObserverCallback = useEvent( ( entries ) => {
+		for ( const entry of entries ) {
+			if ( entry.target === children.first ) {
+				setFirst( ! entry.isIntersecting );
+			}
+			if ( entry.target === children.last ) {
+				setLast( ! entry.isIntersecting );
+			}
 		}
 	} );
 
