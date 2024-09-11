f3db5bc096909c745c7981ef6c57438df51ea1e2
Jon Surrell <sirreal@users.noreply.github.com>
2024-09-11 16:11:09 +0200

    Remove 'gutenberg' text domain from __

--- lib/experimental/script-modules.php
+++ lib/experimental/script-modules.php
@@ -220,7 +220,7 @@ function gutenberg_register_script_modules() {
 	add_filter(
 		'script_module_data_@wordpress/a11y',
 		function ( $data ) {
-			$data['i18n'] = array( 'Notifications' => __( 'Notifications', 'gutenberg' ) );
+			$data['i18n'] = array( 'Notifications' => __( 'Notifications' ) );
 			return $data;
 		}
 	);
