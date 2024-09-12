f1d4e24bdbf6370b1960e39383bf16a19a21da7d
ramon <ramonjd@gmail.com>
2024-09-12 16:48:49 +1000

    initial commit - moving controls for background image into dedicated component. Moving styles. This is in preparation for adding colors and other background controls later.

---
+++ packages/edit-site/src/components/global-styles/screen-background.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import BackgroundPanel from './background-panel';
+import ScreenHeader from './header';
+import { unlock } from '../../lock-unlock';
+
+const { useHasBackgroundPanel, useGlobalSetting } = unlock(
+	blockEditorPrivateApis
+);
+
+function ScreenBackground() {
+	const [ settings ] = useGlobalSetting( '' );
+	const hasBackgroundPanel = useHasBackgroundPanel( settings );
+	return (
+		<>
+			<ScreenHeader
+				title={ __( 'Background' ) }
+				description={ __(
+					'Edit styles that apply to the site’s background.'
+				) }
+			/>
+			{ hasBackgroundPanel && <BackgroundPanel /> }
+		</>
+	);
+}
+
+export default ScreenBackground;
