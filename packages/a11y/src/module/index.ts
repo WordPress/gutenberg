6552c6b2386f084a6885338b89878c44ac4649f6
Jon Surrell <sirreal@users.noreply.github.com>
2024-09-12 12:06:34 +0200

    Fix typos and improve comment

--- packages/a11y/src/module/index.ts
+++ packages/a11y/src/module/index.ts
@@ -4,8 +4,8 @@
 import { makeSetupFunction } from '../shared/index';
 export { speak } from '../shared/index';
 
-// Without an i18n script-module, "Notifications" (the only localized text used in this module)
-// will be trasnlated on the server and provided as script-module data.
+// Without an i18n Script Module, "Notifications" (the only localized text used in this module)
+// will be translated on the server and provided as script-module data.
 let notificationsText = 'Notifications';
 try {
 	const textContent = document.getElementById(
