06dffd31b4d8c4e0f8ae86980bb2583aea4f8fc4
Peter Wilson <git-commit-by@peterwilson.cc>
2024-09-13 12:15:45 +1000

    Use regex to extract the constant.

--- phpunit/class-test-plugin-version-meta-test.php
+++ phpunit/class-test-plugin-version-meta-test.php
@@ -13,6 +13,14 @@ class Test_PluginMetaData_Test extends WP_UnitTestCase {
 	 */
 	public function test_minimum_required_wordpress_version() {
 		$file_meta = get_file_data( __DIR__ . '/../gutenberg.php', array( 'RequiresWP' => 'Requires at least' ) );
-		$this->assertSame( $file_meta['RequiresWP'], GUTENBERG_MINIMUM_WP_VERSION, 'The minimum required WordPress version does not match the plugin header.' );
+		/*
+		 * Gutenberg.php isn't loaded in the test environment.
+		 *
+		 * Read the file directly and use regex to extract the constant.
+		 */
+		preg_match( '/GUTENBERG_MINIMUM_WP_VERSION\', \'(.*?)\'/', file_get_contents( __DIR__ . '/../gutenberg.php' ), $matches );
+		$version_in_file = $matches[1];
+
+		$this->assertSame( $file_meta['RequiresWP'], $version_in_file, 'The minimum required WordPress version does not match the plugin header.' );
 	}
 }
