c917442277b8ba39220055d8177e7e673f2a9d11
ramon <ramonjd@gmail.com>
2024-09-16 15:34:51 +1000

    formatting,yo

--- lib/rest-api.php
+++ lib/rest-api.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return array Array of arguments for registering a post type.
  */
 function gutenberg_override_global_styles_endpoint( array $args ): array {
-	$args['rest_controller_class'] = 'WP_REST_Global_Styles_Controller_Gutenberg';
+	$args['rest_controller_class']           = 'WP_REST_Global_Styles_Controller_Gutenberg';
 	$args['revisions_rest_controller_class'] = 'Gutenberg_REST_Global_Styles_Revisions_Controller_6_6';
 
 	return $args;
