34b87ab5bffddea2ee9e749bf5ec0db287f3219c
akasunil <sdprajapati999@gmail.com>
2024-09-16 22:50:30 +0530

    Fix phpcs errors

--- lib/compat/wordpress-6.7/query-loop-block-filter.php
+++ lib/compat/wordpress-6.7/query-loop-block-filter.php
@@ -1,26 +1,43 @@
 <?php
 /**
- * Add date query support to query loop block.
+ * Add date query to the query loop block.
  *
  * @package gutenberg
+ */
+
+/**
+ * Filter the query loop block query vars.
+ *
+ * @param array  $query Query vars.
+ * @param object $block Block data.
+ *
+ * @return array
  */
 function gutenberg_filter_query_loop_block_query_vars( $query, $block ) {
 
 	if ( ! empty( $block->context['query']['after'] ) || $block->context['query']['before'] ) {
-		$query['date_query'] = [
-			[
+		$query['date_query'] = array(
+			array(
 				'after'     => $block->context['query']['after'],
 				'before'    => $block->context['query']['before'],
 				'inclusive' => true,
-			],
-		];
+			),
+		);
 	}
 
 	return $query;
 }
 add_filter( 'query_loop_block_query_vars', 'gutenberg_filter_query_loop_block_query_vars', 10, 2 );
 
-function gutenberg_filter_rest_post_query(  $args, $request ) {
+/**
+ * Remove date query from REST API request if its empty.
+ *
+ * @param array $args    Query arguments.
+ * @param array $request Request data.
+ *
+ * @return array
+ */
+function gutenberg_filter_rest_post_query( $args, $request ) {
 	if ( empty( $request['after'] ) && empty( $request['before'] ) ) {
 		unset( $args['date_query'] );
 	}
