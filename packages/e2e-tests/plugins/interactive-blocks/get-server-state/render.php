e7ca8e5e587c5b7ae9e702fe6fdffa25e9278adc
David Arenas <david.arenas@automattic.com>
2024-09-17 22:16:15 +0200

    Add e2e tests for `getServerState()`

---
+++ packages/e2e-tests/plugins/interactive-blocks/get-server-state/render.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * HTML for testing the getServerState() function.
+ *
+ * @package gutenberg-test-interactive-blocks
+ */
+
+if ( isset( $attributes['state'] ) ) {
+	wp_interactivity_state( 'test/get-server-state', $attributes['state'] );
+}
+?>
+
+<div
+	data-wp-interactive="test/get-server-state"
+	data-wp-watch="callbacks.updateState"
+>
+	<div data-testid="prop" data-wp-text="state.prop"></div>
+	<div data-testid="nested.prop" data-wp-text="state.nested.prop"></div>
+	<div data-testid="newProp" data-wp-text="state.newProp"></div>
+	<div data-testid="nested.newProp" data-wp-text="state.nested.newProp"></div>
+
+	<button
+		data-testid="tryToUpdateServerState"
+		data-wp-on--click="actions.tryToUpdateServerState"
+	>
+		try update
+	</button>
+
+
+	<nav>
+		<?php
+		if ( isset( $attributes['links'] ) ) {
+			foreach ( $attributes['links'] as $key => $link ) {
+				$i = $key += 1;
+				echo <<<HTML
+				<a
+					data-testid="link $i"
+					data-wp-on--click="actions.navigate"
+					href="$link"
+				>link $i</a>
+HTML;
+			}
+		}
+		?>
+	</nav>
+</div>
