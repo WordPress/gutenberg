b0e67249df90acb2a06c27bc42e2dd1c351fb2df
Jon Surrell <sirreal@users.noreply.github.com>
2024-09-17 12:37:45 +0200

    Improve comment

--- packages/a11y/src/module/index.ts
+++ packages/a11y/src/module/index.ts
@@ -6,6 +6,6 @@ export { speak } from '../shared/index';
 /**
  * This no-op function is exported to provide compatibility with the `wp-a11y` Script.
  *
- * The Script Module should include the relevant HTML as part of page load.
+ * Filters should inject the relevant HTML on page load instead of requiring setup.
  */
 export const setup = () => {};
