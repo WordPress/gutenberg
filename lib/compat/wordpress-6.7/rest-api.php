f8f18fc6c205fd4fb6d21da8e284f0593cad10c8
Mario Santos <santosguillamot@gmail.com>
2024-09-17 23:06:57 +0200

    Only expose public fields

--- lib/compat/wordpress-6.7/rest-api.php
+++ lib/compat/wordpress-6.7/rest-api.php
@@ -125,9 +125,19 @@ if ( ! function_exists( 'gutenberg_register_wp_rest_post_types_meta_fields' ) ) 
 			'meta',
 			array(
 				'get_callback' => function ( $item ) {
-					$default_fields = get_registered_meta_keys( 'post' );
+					$public_fields = array();
+					$global_fields = get_registered_meta_keys( 'post' );
 					$post_type_fields = get_registered_meta_keys( 'post', $item['slug'] );
-					return array_merge( $default_fields, $post_type_fields );
+					foreach ( array_merge( $global_fields, $post_type_fields ) as $key => $properties ) {
+						// Only expose fields with `show_in_rest` set to true.
+						if ( $properties['show_in_rest'] ) {
+							$public_fields[ $key ] = array(
+								'default'     => $properties['default'] ?? '',
+								'description' => $properties['description'],
+							);
+						}
+					}
+					return $public_fields;
 				},
 				'schema'       => array(
 					'type'        => 'array',
