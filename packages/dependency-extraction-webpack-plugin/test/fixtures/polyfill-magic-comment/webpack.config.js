28f1b5b308a62098f0e1e253cb734c83b2fa1356
Sérgio Gomes <mail@sgomes.com>
2024-09-18 16:28:34 +0100

    Simplify DEWP test
    Co-authored-by: Jon Surrell <sirreal@users.noreply.github.com>

--- packages/dependency-extraction-webpack-plugin/test/fixtures/polyfill-magic-comment/webpack.config.js
+++ packages/dependency-extraction-webpack-plugin/test/fixtures/polyfill-magic-comment/webpack.config.js
@@ -4,11 +4,5 @@
 const DependencyExtractionWebpackPlugin = require( '../../..' );
 
 module.exports = {
-	plugins: [
-		new DependencyExtractionWebpackPlugin( {
-			requestToExternalModule( request ) {
-				return request.startsWith( '@wordpress/' );
-			},
-		} ),
-	],
+	plugins: [ new DependencyExtractionWebpackPlugin() ],
 };
