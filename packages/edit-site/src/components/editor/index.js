1aeb68529a798915faee77b23f0712f3b66ddaca
James Koster <james@jameskoster.co.uk>
2024-09-18 14:33:08 +0100

    Use menu icon in home button

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -22,7 +22,7 @@ import { store as noticesStore } from '@wordpress/notices';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
 import { store as preferencesStore } from '@wordpress/preferences';
 import { decodeEntities } from '@wordpress/html-entities';
-import { Icon, homeButton } from '@wordpress/icons';
+import { Icon, menu } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -292,7 +292,7 @@ export default function EditSiteEditor( { isPostsList = false } ) {
 											) }
 											variants={ toggleHomeIconVariants }
 										>
-											<Icon icon={ homeButton } />
+											<Icon icon={ menu } />
 										</motion.div>
 									</motion.div>
 								)
