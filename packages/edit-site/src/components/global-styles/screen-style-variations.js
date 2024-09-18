03b4412e223674e8d93afb8c21afa7563f0b46b6
Utsav Patel <utsav.patel1512@outlook.com>
2024-09-18 23:12:59 +0530

    remove: zoomout mount from browse styles

--- packages/edit-site/src/components/global-styles/screen-style-variations.js
+++ packages/edit-site/src/components/global-styles/screen-style-variations.js
@@ -3,7 +3,6 @@
  */
 import { Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useZoomOut } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -12,10 +11,6 @@ import ScreenHeader from './header';
 import SidebarNavigationScreenGlobalStylesContent from '../sidebar-navigation-screen-global-styles/content';
 
 function ScreenStyleVariations() {
-	// Move to zoom out mode when this component is mounted
-	// and back to the previous mode when unmounted.
-	useZoomOut();
-
 	return (
 		<>
 			<ScreenHeader
