20ad01b5b25f9737c90b56a2efbebd2a1ce36b6a
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2024-09-19 16:19:45 +0300

    Add comments and exclude more dirs

--- react-scanner.config.js
+++ react-scanner.config.js
@@ -1,7 +1,24 @@
 module.exports = {
+	// Crawl the entire repo
 	crawlFrom: './',
+	// Needed for properly reporting components with dot notation
 	includeSubComponents: true,
+	// Exclude usage in tests, stories, and React Native files.
 	globs: [ '**/!(test|stories)/!(.native.).@(js|ts)?(x)' ],
-	exclude: [ 'node_modules', 'vendor' ],
+	// Exclude any vendor or docs directories
+	exclude: [
+		'bin',
+		'build',
+		'build-module',
+		'docs',
+		'node_modules',
+		'patches',
+		'platform-docs',
+		'test',
+		'tools',
+		'typings',
+		'vendor',
+	],
+	// Filter out any non-component React elements
 	importedFrom: /.*/,
 };
