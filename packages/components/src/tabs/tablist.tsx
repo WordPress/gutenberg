d3d1d841c00bd549d62fd853b378e8a1714ac1a8
Dani Guardiola <hi@daniguardio.la>
2024-09-20 01:46:00 +0200

    Fix scroll bug

--- packages/components/src/tabs/tablist.tsx
+++ packages/components/src/tabs/tablist.tsx
@@ -42,12 +42,12 @@ export const TabList = forwardRef<
 
 	const [ animationEnabled, setAnimationEnabled ] = useState( false );
 	useOnValueUpdate( selectedId, ( { previousValue } ) => {
-		selectedElement?.scrollIntoView( {
-			behavior: 'instant',
-			block: 'nearest',
-			inline: 'nearest',
-		} );
 		if ( previousValue ) {
+			selectedElement?.scrollIntoView( {
+				behavior: 'instant',
+				block: 'nearest',
+				inline: 'nearest',
+			} );
 			setAnimationEnabled( true );
 		}
 	} );
