d0dbb5286779d88dc6970261e446930cbaaa58c3
ramon <ramonjd@gmail.com>
2024-09-20 15:51:41 +1000

    Getting all top-level categories.

--- packages/edit-site/src/components/style-book/index.js
+++ packages/edit-site/src/components/style-book/index.js
@@ -30,7 +30,7 @@ import { ENTER, SPACE } from '@wordpress/keycodes';
  */
 import { unlock } from '../../lock-unlock';
 import EditorCanvasContainer from '../editor-canvas-container';
-import { STYLE_BOOK_CATEGORIES, STYLE_BOOK_IFRAME_STYLES } from './constants';
+import { STYLE_BOOK_IFRAME_STYLES } from './constants';
 import {
 	getExamplesByCategory, getTopLevelStyleBookCategories,
 } from './categories';
@@ -247,7 +247,7 @@ const StyleBookBody = ( {
 const Examples = memo(
 	( { className, examples, category, label, isSelected, onSelect } ) => {
 		const categoryDefinition = category
-			? STYLE_BOOK_CATEGORIES.find(
+			? getTopLevelStyleBookCategories().find(
 					( _category ) => _category.slug === category
 			  )
 			: null;
