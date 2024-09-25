cb44f51ad33d703015816e53aa98616b41ead2ea
Luigi Teschio <gigitux@gmail.com>
2024-09-25 17:00:53 +0200

    Migrate store and actions from editor package to fields package #2 (#65289)
    Co-authored-by: gigitux <gigitux@git.wordpress.org>
    Co-authored-by: louwie17 <louwie17@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: oandregal <oandregal@git.wordpress.org>

--- packages/fields/src/actions/duplicate-post.tsx
+++ packages/fields/src/actions/duplicate-post.tsx
@@ -18,9 +18,9 @@ import type { Action } from '@wordpress/dataviews';
 /**
  * Internal dependencies
  */
-import { titleField } from '../../fields';
-import type { BasePost, CoreDataError } from '../../types';
-import { getItemTitle } from '../utils';
+import { titleField } from '../fields';
+import type { BasePost, CoreDataError } from '../types';
+import { getItemTitle } from './utils';
 
 const fields = [ titleField ];
 const formDuplicateAction = {
