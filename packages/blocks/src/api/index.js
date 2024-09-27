93323fd1a3cecd0771ead22b16a9661c53be6212
Kai Hao <kevin830726@gmail.com>
2024-09-27 14:18:41 +0800

    Fix unable to remove empty blocks on merge (#65262)
    Co-authored-by: kevin940726 <kevin940726@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>
    Co-authored-by: andrewserong <andrewserong@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: getdave <get_dave@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: kspilarski <kel-dc@git.wordpress.org>
    Co-authored-by: ndiego <ndiego@git.wordpress.org>
    Co-authored-by: richtabor <richtabor@git.wordpress.org>
    * Fix unable to remove empty blocks on merge
    
    * Update to the stabilized API
    
    * Rename the utils

--- packages/blocks/src/api/index.js
+++ packages/blocks/src/api/index.js
@@ -8,6 +8,7 @@ import {
 	getBlockBindingsSource,
 	getBlockBindingsSources,
 } from './registration';
+import { isUnmodifiedBlockContent } from './utils';
 
 // The blocktype is the most important concept within the block API. It defines
 // all aspects of the block configuration and its interfaces, including `edit`
@@ -183,4 +184,5 @@ lock( privateApis, {
 	unregisterBlockBindingsSource,
 	getBlockBindingsSource,
 	getBlockBindingsSources,
+	isUnmodifiedBlockContent,
 } );
