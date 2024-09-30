22e19b477428b586e851bc8b4ccf769254de00f2
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-09-30 12:59:53 +0200

    Block bindings: Refactor passing select and dispatch instead of full Registry. (#65710)
    * Try use select and dispatch instead of registry
    
    * Pass `select` to `canUserEditValue`
    
    * Pass `select` to `getFieldsList`
    
    * Fix `setValues`
    
    ---------
    
    Co-authored-by: cbravobernal <cbravobernal@git.wordpress.org>
    Co-authored-by: SantosGuillamot <santosguillamot@git.wordpress.org>

--- packages/block-library/src/image/edit.js
+++ packages/block-library/src/image/edit.js
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 import { isBlobURL, createBlobURL } from '@wordpress/blob';
 import { store as blocksStore, createBlock } from '@wordpress/blocks';
 import { Placeholder } from '@wordpress/components';
-import { useDispatch, useSelect, useRegistry } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import {
 	BlockIcon,
 	useBlockProps,
@@ -113,7 +113,6 @@ export function ImageEdit( {
 
 	const [ temporaryURL, setTemporaryURL ] = useState( attributes.blob );
 
-	const registry = useRegistry();
 	const containerRef = useRef();
 	// Only observe the max width from the parent container when the parent layout is not flex nor grid.
 	// This won't work for them because the container width changes with the image.
@@ -381,7 +380,7 @@ export function ImageEdit( {
 				lockUrlControls:
 					!! metadata?.bindings?.url &&
 					! blockBindingsSource?.canUserEditValue?.( {
-						registry,
+						select,
 						context,
 						args: metadata?.bindings?.url?.args,
 					} ),
