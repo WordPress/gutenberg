6ec08a746e5ea8aa45bba757c4af6618f81a433c
Au Pleguezuelo <auareyou@gmail.com>
2024-09-30 20:10:31 -0400

    auareyou changes
    Color changes and added static directory to swap out logo

--- storybook/theme.js
+++ storybook/theme.js
@@ -6,13 +6,36 @@ import { create } from '@storybook/theming/create';
 export default create( {
 	base: 'light',
 	brandTitle: 'WordPress Components',
-	brandImage:
-		'https://s.w.org/style/images/about/WordPress-logotype-wmark.png',
+	brandImage: './wp-org-logo.png',
+
+	// Typography
+	fontBase: '"Inter", sans-serif',
+	fontCode: 'monospace',
+
+	//
+	colorPrimary: '#3858E9',
+	colorSecondary: '#3858E9',
 
 	// UI
 	appBg: '#ffffff',
 	appContentBg: '#ffffff',
 	appPreviewBg: '#ffffff',
-	appBorderColor: '#F6F7F7',
-	appBorderRadius: 8,
+	appBorderColor: '#DCDCDE',
+	appBorderRadius: 4,
+
+	// Text colors
+	textColor: '#10162F',
+	textInverseColor: '#ffffff',
+
+	// Toolbar default and active colors
+	barTextColor: '#9E9E9E',
+	barSelectedColor: '#3858E9',
+	barHoverColor: '#3858E9',
+	barBg: '#ffffff',
+
+	// Form colors
+	inputBg: '#ffffff',
+	inputBorder: '#10162F',
+	inputTextColor: '#10162F',
+	inputBorderRadius: 2,
 } );
