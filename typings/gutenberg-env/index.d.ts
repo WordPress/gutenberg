4a83415c18f5f1753d585360d376fd22e25dbd78
Volodymyr Shelmuk <v.shelmuk@inpsyde.com>
2024-10-02 12:52:25 +0300

    ESLint: fix degradation for types building

--- typings/gutenberg-env/index.d.ts
+++ typings/gutenberg-env/index.d.ts
@@ -7,12 +7,13 @@ declare namespace NodeJS {
 	}
 }
 
-declare const process: NodeJS.Process;
+/* eslint-disable no-var */
+declare var process: NodeJS.Process;
 
 /**
  * Whether the code is running in WordPress with SCRIPT_DEBUG flag.
  */
-declare const SCRIPT_DEBUG: undefined | boolean;
+declare var SCRIPT_DEBUG: undefined | boolean;
 
 /**
  * Whether code is running within the Gutenberg plugin.
@@ -20,6 +21,7 @@ declare const SCRIPT_DEBUG: undefined | boolean;
  * When the codebase is built for the plugin, this variable will be set to `true`.
  * When building for WordPress Core, it will be set to `false` or `undefined`.
  */
-declare const IS_GUTENBERG_PLUGIN: undefined | boolean;
+declare var IS_GUTENBERG_PLUGIN: undefined | boolean;
 
-declare const IS_WORDPRESS_CORE: undefined | boolean;
+declare var IS_WORDPRESS_CORE: undefined | boolean;
+/* eslint-enable no-var */
