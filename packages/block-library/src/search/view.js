f0944db3f7d2838bdffc2a9ca9e1b397253e4db6
Michal Czaplinski <mmczaplinski@gmail.com>
2024-10-08 20:06:22 +0100

    Refactor the view.js of search block

--- packages/block-library/src/search/view.js
+++ packages/block-library/src/search/view.js
@@ -7,21 +7,6 @@ const isEmpty = ( obj ) =>
 	[ Object, Array ].includes( ( obj || {} ).constructor ) &&
 	! Object.entries( obj || {} ).length;
 
-const updateURL = async ( value, name ) => {
-	const url = new URL( window.location );
-	const { actions } = await import( '@wordpress/interactivity-router' );
-
-	if ( 's' === name ) {
-		if ( ! isEmpty( value ) ) {
-			url.searchParams.set( 'search', value );
-		} else {
-			url.searchParams.delete( 'search' );
-		}
-	}
-
-	actions.navigate( `${ window.location.pathname }${ url.search }` );
-};
-
 const { state, actions } = store(
 	'core/search',
 	{
@@ -101,12 +86,24 @@ const { state, actions } = store(
 					return;
 				}
 
+				const url = new URL( window.location );
+
 				if ( 's' === name ) {
 					state.search = value;
+					if ( ! isEmpty( value ) ) {
+						url.searchParams.set( 'search', value );
+					} else {
+						url.searchParams.delete( 'search' );
+					}
 				}
 
-				// If not, navigate to the new URL.
-				yield updateURL( value, name );
+				const { actions: routerActions } = yield import(
+					'@wordpress/interactivity-router'
+				);
+
+				routerActions.navigate(
+					`${ window.location.pathname }${ url.search }`
+				);
 			},
 		},
 	},
