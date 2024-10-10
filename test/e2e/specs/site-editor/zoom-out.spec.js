be1e548f5ebf343dd620c7b8403c424a4d2ea10c
MaggieCabrera <maggie.cabrera@automattic.com>
2024-10-10 18:19:23 +0200

    reformat comments

--- test/e2e/specs/site-editor/zoom-out.spec.js
+++ test/e2e/specs/site-editor/zoom-out.spec.js
@@ -25,7 +25,7 @@ test.describe( 'Zoom Out', () => {
 		const iframe = page.locator( 'iframe[name="editor-canvas"]' );
 		const html = editor.canvas.locator( 'html' );
 
-		//Check that the html is scaled
+		// Check that the html is scaled.
 		await expect( html ).toHaveCSS(
 			'transform',
 			'matrix(0.75, 0, 0, 0.75, 0, 0)'
@@ -33,15 +33,15 @@ test.describe( 'Zoom Out', () => {
 		const iframeRect = await iframe.boundingBox();
 		const htmlRect = await html.boundingBox();
 
-		//Check that the iframe is larger than the html
+		// Check that the iframe is larger than the html.
 		expect( iframeRect.width ).toBeGreaterThan( htmlRect.width );
 
-		//Check that the zoomed out content has a frame around it
+		// Check that the zoomed out content has a frame around it.
 		expect( htmlRect.x ).toBeGreaterThan( iframeRect.x );
 
-		//We use border to separate the content from the frame so we need
-		//to check that that is present too, since boundingBox()
-		//includes the border to calculate the position
+		// We use border to separate the content from the frame so we need
+		// to check that that is present too, since boundingBox()
+		// includes the border to calculate the position.
 		const borderWidths = await html.evaluate( ( el ) => {
 			const styles = window.getComputedStyle( el );
 			return {
