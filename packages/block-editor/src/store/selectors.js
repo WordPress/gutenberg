88de21bac0c2652beb2b835fbce0ea3e2a1bdf89
André <583546+oandregal@users.noreply.github.com>
2024-10-21 11:50:54 +0200

    Block inserter: prevent editor from crashing if `blockType.parent` is a string (#66234)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: gziolo <gziolo@git.wordpress.org>

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -1628,8 +1628,18 @@ const isBlockVisibleInTheInserter = (
 	checkedBlocks.add( blockName );
 
 	// If parent blocks are not visible, child blocks should be hidden too.
-	if ( !! blockType.parent?.length ) {
-		return blockType.parent.some(
+	//
+	// In some scenarios, blockType.parent may be a string.
+	// A better approach would be sanitize parent in all the places that can be modified:
+	// block registration, processBlockType, filters, etc.
+	// In the meantime, this is a hotfix to prevent the editor from crashing.
+	const parent =
+		typeof blockType.parent === 'string' ||
+		blockType.parent instanceof String
+			? [ blockType.parent ]
+			: blockType.parent;
+	if ( Array.isArray( parent ) ) {
+		return parent.some(
 			( name ) =>
 				( blockName !== name &&
 					isBlockVisibleInTheInserter(
@@ -1643,6 +1653,7 @@ const isBlockVisibleInTheInserter = (
 				name === 'core/post-content'
 		);
 	}
+
 	return true;
 };
 
