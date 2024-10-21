a914aedb91cf9b646e8551ce3da8e672caac634c
michalczaplinski <michalczaplinski@users.noreply.github.com>
2024-10-21 16:17:00 +0000

    deploy: 119caccd9130cecbd24fe62df55221af33415afb

---
+++ sb-manager/index.js
@@ -0,0 +1,1 @@
+import{Provider,Root,renderStorybookUI}from"./chunk-Z6UFU3II.js";import"./chunk-2IXBUOFS.js";import"./chunk-NGTUFCUO.js";import"./chunk-INSKDKQB.js";import"./chunk-ZEU7PDD3.js";export{Provider,Root,renderStorybookUI};
