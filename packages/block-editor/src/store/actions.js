86616b1417a5d569b937c7b519d82c0a0f8e63cc
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-10-22 10:56:30 -0300

    Move deprecated setter logic to __experimentalUpdateSettings

--- packages/block-editor/src/store/actions.js
+++ packages/block-editor/src/store/actions.js
@@ -1591,23 +1591,7 @@ export function updateBlockListSettings( clientId, settings ) {
  * @return {Object} Action object
  */
 export function updateSettings( settings ) {
-	let updatedSettings = settings;
-
-	if ( Object.hasOwn( updatedSettings, '__unstableIsPreviewMode' ) ) {
-		deprecated(
-			"__unstableIsPreviewMode argument in wp.data.dispatch('core/block-editor').updateSettings",
-			{
-				since: '6.8',
-				alternative: 'isPreviewMode',
-			}
-		);
-
-		updatedSettings = { ...updatedSettings };
-		updatedSettings.isPreviewMode = updatedSettings.__unstableIsPreviewMode;
-		delete updatedSettings.__unstableIsPreviewMode;
-	}
-
-	return __experimentalUpdateSettings( updatedSettings, {
+	return __experimentalUpdateSettings( settings, {
 		stripExperimentalSettings: true,
 	} );
 }
