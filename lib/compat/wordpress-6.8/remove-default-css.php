566e021eca28023073b675d73f354b485c5ed532
Ella <4710635+ellatrix@users.noreply.github.com>
2024-10-24 19:17:32 +0200

    php lint

--- lib/compat/wordpress-6.8/remove-default-css.php
+++ lib/compat/wordpress-6.8/remove-default-css.php
@@ -1,9 +1,11 @@
 <?php
 
-add_action( 'admin_enqueue_scripts', function( $hook ) {
+function gutenberg_remove_default_css( $hook ) {
 	// Maybe also remove on post.php when Gutenberg is enabled for posts.
 	if ( 'site-editor.php' !== $hook ) {
 		return;
 	}
 	wp_dequeue_style( 'colors' );
-} );
+}
+
+add_action( 'admin_enqueue_scripts', 'gutenberg_remove_default_css' );
