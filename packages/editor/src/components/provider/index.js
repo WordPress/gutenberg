6ed53708651c272c3096c5c4ba2367ff4fa130f0
Dave Smith <getdavemail@gmail.com>
2024-10-24 13:06:00 +0100

    Avoid special content only settings menu in Zoom Out

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -13,6 +13,7 @@ import {
 	BlockEditorProvider,
 	BlockContextProvider,
 	privateApis as blockEditorPrivateApis,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { store as noticesStore } from '@wordpress/notices';
 import { privateApis as editPatternsPrivateApis } from '@wordpress/patterns';
@@ -188,6 +189,15 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 				},
 				[ post.type ]
 			);
+
+		const isZoomOut = useSelect( ( select ) => {
+			const { __unstableGetEditorMode } = unlock(
+				select( blockEditorStore )
+			);
+
+			return __unstableGetEditorMode() === 'zoom-out';
+		} );
+
 		const shouldRenderTemplate = !! template && mode !== 'post-only';
 		const rootLevelPost = shouldRenderTemplate ? template : post;
 		const defaultBlockContext = useMemo( () => {
@@ -334,7 +344,9 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 								<>
 									<PatternsMenuItems />
 									<TemplatePartMenuItems />
-									<ContentOnlySettingsMenu />
+									{ ! isZoomOut && (
+										<ContentOnlySettingsMenu />
+									) }
 									{ mode === 'template-locked' && (
 										<DisableNonPageContentBlocks />
 									) }
