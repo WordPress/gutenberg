22ce8f9b3339baf61ede9aef8e23165e7927f09e
Damián Suárez <rdsuarez@gmail.com>
2024-10-30 16:49:58 +0000

    add With Component Labels story

--- packages/components/src/radio-control/stories/index.story.tsx
+++ packages/components/src/radio-control/stories/index.story.tsx
@@ -12,6 +12,7 @@ import { useState } from '@wordpress/element';
  * Internal dependencies
  */
 import RadioControl from '..';
+import { Path, SVG } from '@wordpress/primitives';
 
 const meta: Meta< typeof RadioControl > = {
 	component: RadioControl,
@@ -89,6 +90,54 @@ WithOptionDescriptions.args = {
 			label: 'Password Protected',
 			value: 'password',
 			description: 'Protected by a password',
+		},
+	],
+};
+
+export const WithComponentLabels: StoryFn< typeof RadioControl > =
+	Template.bind( {} );
+
+function Rating( { stars, ariaLabel }: { stars: number; ariaLabel: string } ) {
+	const starPath =
+		'M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z';
+	return (
+		<div style={ { display: 'flex' } } aria-label={ ariaLabel }>
+			{ Array.from( { length: stars }, ( _, index ) => (
+				<SVG
+					key={ index }
+					width={ 24 }
+					height={ 24 }
+					viewBox="0 0 24 24"
+				>
+					<Path d={ starPath } />
+				</SVG>
+			) ) }
+		</div>
+	);
+}
+
+WithComponentLabels.args = {
+	label: 'Rating',
+	options: [
+		{
+			label: <Rating stars={ 5 } ariaLabel="Five Stars" />,
+			value: '5',
+		},
+		{
+			label: <Rating stars={ 4 } ariaLabel="Four Stars" />,
+			value: '4',
+		},
+		{
+			label: <Rating stars={ 3 } ariaLabel="Three Stars" />,
+			value: '3',
+		},
+		{
+			label: <Rating stars={ 2 } ariaLabel="Two Stars" />,
+			value: '2',
+		},
+		{
+			label: <Rating stars={ 1 } ariaLabel="One Star" />,
+			value: '1',
 		},
 	],
 };
