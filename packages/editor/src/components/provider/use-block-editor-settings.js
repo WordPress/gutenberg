7001b9f8ac6fcc054d192cb84578ea7d1ebcd896
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-03-03 15:43:40 +0530

    Add Boundary Mode

--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -147,6 +147,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 		pageForPosts,
 		restBlockPatternCategories,
 		sectionRootClientId,
+		boundaryMode,
 		deviceType,
 		isNavigationOverlayContext,
 		isRevisionsMode,
@@ -222,6 +223,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 					postId
 				)?._links?.hasOwnProperty( 'wp:action-unfiltered-html' ),
 				focusMode: get( 'core', 'focusMode' ),
+				boundaryMode: get( 'core', 'boundaryMode' ),
 				hasFixedToolbar:
 					get( 'core', 'fixedToolbar' ) || ! isLargeViewport,
 				hiddenBlockTypes: get( 'core', 'hiddenBlockTypes' ),
@@ -370,6 +372,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 			allowedBlockTypes,
 			allowRightClickOverrides,
 			focusMode,
+			boundaryMode,
 			hasFixedToolbar,
 			isDistractionFree,
 			keepCaretInsideBlock,
@@ -461,6 +464,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 		allowedBlockTypes,
 		allowRightClickOverrides,
 		focusMode,
+		boundaryMode,
 		hasFixedToolbar,
 		isDistractionFree,
 		keepCaretInsideBlock,
