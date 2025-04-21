a02513c7ebb52614dd95aa3c7fe0e9f6ae9f7b5e
Takshil Kunadia <takshilkunadia@gmail.com>
2025-04-21 19:18:45 +0530

    refactor: rename cache directory path variable for clarity

--- packages/env/lib/config/load-config.js
+++ packages/env/lib/config/load-config.js
@@ -76,7 +76,7 @@ module.exports = async function loadConfig(
 
 	// Descriptive cache directory name
 	const directory = path.dirname( configFilePath ).replace( /^.*[\\/]/, '' );
-	const verboseCacheDirectoryPath = path.resolve(
+	const descriptiveCacheDirectoryPath = path.resolve(
 		await getCacheDirectory(),
 		'wp-env-' + directory + '-' + md5( configFilePath )
 	);
@@ -84,7 +84,7 @@ module.exports = async function loadConfig(
 	// If cache doesn't exist, create with new name
 	const cacheDirectoryPath = existsSync( md5CacheDirectoryPath )
 		? md5CacheDirectoryPath
-		: verboseCacheDirectoryPath;
+		: descriptiveCacheDirectoryPath;
 
 	// Parse any configuration we found in the given directory.
 	// This comes merged and prepared for internal consumption.
