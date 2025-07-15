d6272aeee523fb2ad0c172d6d23f9258c1a43a8a
MaggieCabrera <maggie.cabrera@automattic.com>
2025-07-15 18:19:49 +0200

    linting and moving styles to the specific blocks

--- lib/class-wp-theme-json-gutenberg.php
+++ lib/class-wp-theme-json-gutenberg.php
@@ -616,8 +616,8 @@ class WP_Theme_JSON_Gutenberg {
 	);
 
 	const __EXPERIMENTAL_ELEMENT_CLASS_NAMES = array(
-		'button'    => 'wp-element-button',
-		'caption'   => 'wp-element-caption',
+		'button'  => 'wp-element-button',
+		'caption' => 'wp-element-caption',
 	);
 
 	/**
