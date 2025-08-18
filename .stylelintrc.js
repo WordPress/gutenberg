afe4086b795fdf5ac845085798c4c64891383a64
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-08-18 15:29:28 +0530

    feat: Consolidate rules for simplification

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -17,11 +17,24 @@ module.exports = {
 		],
 		'declaration-property-value-disallowed-list': [
 			{
+				'flex-direction': [ '/-reverse$/' ],
 				'/.*/': [ '/--wp-components-color-/' ],
 			},
 			{
-				message: ( property, value ) =>
-					`Avoid using "${ value }" in "${ property }". --wp-components-color-* variables are not ready to be used outside of the components package.`,
+				message: ( property, value ) => {
+					// Don't allow reverse values for flex-direction.
+					if (
+						property === 'flex-direction' &&
+						/-reverse$/.test( value )
+					) {
+						return `Avoid "${ value }" value for the "${ property }" property. For accessibility reasons, visual, reading, and DOM order must match. Only use the reverse values when they do not affect reading order, meaning, and interaction.`;
+					}
+					// Don't allow --wp-components-color-* variables outside of the components package.
+					if ( /--wp-components-color-/.test( value ) ) {
+						return `Avoid using "${ value }" in "${ property }". --wp-components-color-* variables are not ready to be used outside of the components package.`;
+					}
+					return `Avoid "${ value }" for "${ property }".`;
+				},
 			},
 		],
 		'font-weight-notation': null,
