a0cae5de12ba18b28d4eeef4d337d497169ac439
Shreya Shrivastava <shreya2202shri@gmail.com>
2025-08-26 13:57:06 +0530

    fix: add color contrast warning for buttons

--- packages/block-editor/src/hooks/contrast-checker.js
+++ packages/block-editor/src/hooks/contrast-checker.js
@@ -2,12 +2,15 @@
  * WordPress dependencies
  */
 import { useLayoutEffect, useReducer } from '@wordpress/element';
+import { getBlockType } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import ContrastChecker from '../components/contrast-checker';
 import { useBlockElement } from '../components/block-list/use-block-props/use-block-refs';
+import { store as blockEditorStore } from '../store';
 
 function getComputedValue( node, property ) {
 	return node.ownerDocument.defaultView
@@ -15,19 +18,32 @@ function getComputedValue( node, property ) {
 		.getPropertyValue( property );
 }
 
-function getBlockElementColors( blockEl ) {
+function getBlockElementColors( blockEl, blockType ) {
 	if ( ! blockEl ) {
 		return {};
 	}
 
-	const firstLinkElement = blockEl.querySelector( 'a' );
+	const rootSelector = blockType?.selectors?.root || null;
+	let targetElement = blockEl;
+
+	if ( rootSelector ) {
+		const [ , ...rest ] = rootSelector.split( ' ' );
+		if ( rest.length ) {
+			const innerSelector = rest.join( ' ' );
+			targetElement = blockEl.querySelector( innerSelector ) || blockEl;
+		}
+	}
+
+	// Retrieve colors from the target element.
+	const firstLinkElement = targetElement.querySelector( 'a' );
 	const linkColor = !! firstLinkElement?.innerText
 		? getComputedValue( firstLinkElement, 'color' )
 		: undefined;
 
-	const textColor = getComputedValue( blockEl, 'color' );
+	const textColor = getComputedValue( targetElement, 'color' );
 
-	let backgroundColorNode = blockEl;
+	// Traverse up the DOM tree to find the background color.
+	let backgroundColorNode = targetElement;
 	let backgroundColor = getComputedValue(
 		backgroundColorNode,
 		'background-color'
@@ -63,6 +79,13 @@ function reducer( prevColors, newColors ) {
 
 export default function BlockColorContrastChecker( { clientId } ) {
 	const blockEl = useBlockElement( clientId );
+	const blockType = useSelect(
+		( select ) => {
+			const block = select( blockEditorStore ).getBlock( clientId );
+			return block ? getBlockType( block.name ) : null;
+		},
+		[ clientId ]
+	);
 	const [ colors, setColors ] = useReducer( reducer, {} );
 
 	// There are so many things that can change the color of a block
@@ -73,7 +96,7 @@ export default function BlockColorContrastChecker( { clientId } ) {
 		}
 
 		function updateColors() {
-			setColors( getBlockElementColors( blockEl ) );
+			setColors( getBlockElementColors( blockEl, blockType ) );
 		}
 
 		// Combine `useLayoutEffect` and two rAF calls to ensure that values are read
