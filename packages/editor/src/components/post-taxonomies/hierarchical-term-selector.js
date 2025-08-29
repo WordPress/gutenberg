8fbcd0a1c6358d5938e81208fa321c4747d2c4dd
George Mamadashvili <georgemamadashvili@gmail.com>
2025-08-29 10:14:26 +0400

    Code Quality: Simplify permission checks

--- packages/editor/src/components/post-taxonomies/hierarchical-term-selector.js
+++ packages/editor/src/components/post-taxonomies/hierarchical-term-selector.js
@@ -192,14 +192,14 @@ export function HierarchicalTermSelector( { slug } ) {
 
 			return {
 				hasCreateAction: _taxonomy
-					? post._links?.[
+					? !! post._links?.[
 							'wp:action-create-' + _taxonomy.rest_base
-					  ] ?? false
+					  ]
 					: false,
 				hasAssignAction: _taxonomy
-					? post._links?.[
+					? !! post._links?.[
 							'wp:action-assign-' + _taxonomy.rest_base
-					  ] ?? false
+					  ]
 					: false,
 				terms: _taxonomy
 					? getEditedPostAttribute( _taxonomy.rest_base )
