5f6fcd3c83ddc373540aaafec3229c2fdefa4b2e
chriszarate <chris.zarate@automattic.com>
2025-08-29 11:32:26 -0600

    Disable autosave warnings when collaborative editing is enabled

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -296,7 +296,17 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 
 			updatePostLock( settings.postLock );
 			setupEditor( post, initialEdits, settings.template );
-			if ( settings.autosave ) {
+
+			// Disable the warning notice if collaborative editing is enabled.
+			//
+			// @TODO
+			//
+			// In the future, we may wish to implement a more sophisticated check -- for
+			// example, if collaborative editing is enabled but the provider is
+			// disconnected, we may want to provide the user with options. For now,
+			// however, since we effectively lock the editor when the provider is not
+			// connected, this simplistic approach will work.
+			if ( settings.autosave && ! window.__experimentalEnableSync ) {
 				createWarningNotice(
 					__(
 						'There is an autosave of this post that is more recent than the version below.'
