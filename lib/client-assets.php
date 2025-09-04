6c381102fcdd5c474718a6a85bd3203c5edb0e04
Weston Ruter <weston@ruter.net>
2025-09-03 17:20:51 -0700

    Add reference to related GB issue

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -655,6 +655,7 @@ function gutenberg_default_script_modules() {
 
 		// All script modules in Gutenberg are (currently) related to the Interactivity API which prioritizes server-side rendering.
 		// Therefore, the modules should be fetched with a low priority to avoid network contention with any LCP element resource.
+		// For allowing a block to opt-in to another fetchpriority, see <https://github.com/WordPress/gutenberg/issues/71366>.
 		$args = array(
 			'fetchpriority' => 'low',
 		);
