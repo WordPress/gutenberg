c88c7c56a3a410f8521f29ab0b857712c5dc7f51
Dave Smith <getdavemail@gmail.com>
2025-09-04 10:53:21 +0100

    refactor: simplify DialogWrapper instance ID generation
    Use DialogWrapper as the component for useInstanceId instead of deriving
    it from className. This removes unnecessary complexity and unused imports.

--- packages/block-library/src/navigation-link/dialog-wrapper.js
+++ packages/block-library/src/navigation-link/dialog-wrapper.js
@@ -5,12 +5,6 @@ import { Button, VisuallyHidden } from '@wordpress/components';
 import { __, isRTL } from '@wordpress/i18n';
 import { chevronLeftSmall, chevronRightSmall } from '@wordpress/icons';
 import { useInstanceId, useFocusOnMount } from '@wordpress/compose';
-import { LinkControl } from '@wordpress/block-editor';
-
-/**
- * Internal dependencies
- */
-import { LinkUIPageCreator } from './page-creator';
 
 /**
  * Shared BackButton component for consistent navigation across LinkUI sub-components.
@@ -46,22 +40,13 @@ function BackButton( { className, onBack } ) {
  * @param {Object}   props.children    Child components to render inside the dialog.
  */
 function DialogWrapper( { className, title, description, onBack, children } ) {
-	// Derive component and componentName from className
-	let component = LinkControl;
-	if ( className === 'link-ui-block-inserter' ) {
-		component = LinkControl;
-	} else if ( className === 'link-ui-page-creator' ) {
-		component = LinkUIPageCreator;
-	}
-	const componentName = className;
-
 	const dialogTitleId = useInstanceId(
-		component,
-		`${ componentName }__title`
+		DialogWrapper,
+		`${ className }__title`
 	);
 	const dialogDescriptionId = useInstanceId(
-		component,
-		`${ componentName }__description`
+		DialogWrapper,
+		`${ className }__description`
 	);
 	const focusOnMountRef = useFocusOnMount( 'firstElement' );
 	const backButtonClassName = `${ className }__back`;
