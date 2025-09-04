d1919871adcde6a55ed9204c9d11b2a433aee646
ntsekouras <ntsekouras@outlook.com>
2025-09-04 12:55:33 +0300

    remove tel regex validation

--- packages/dataviews/src/field-types/telephone.tsx
+++ packages/dataviews/src/field-types/telephone.tsx
@@ -31,25 +31,12 @@ function sort( valueA: any, valueB: any, direction: SortDirection ) {
 		: valueB.localeCompare( valueA );
 }
 
-// Basic international phone number validation
-// Supports formats like: +1234567890, (123) 456-7890, 123-456-7890, 123.456.7890, etc.
-const phoneRegex =
-	/^[\+]?[(]?[\+]?\d{0,3}[)]?[-\s\.]?\d{0,3}[-\s\.]?\d{3,4}[-\s\.]?\d{3,4}$/;
-
 export default {
 	sort,
 	isValid: {
 		custom: ( item: any, field: NormalizedField< any > ) => {
 			const value = field.getValue( { item } );
-
-			if (
-				! [ undefined, '', null ].includes( value ) &&
-				! phoneRegex.test( value )
-			) {
-				return __( 'Value must be a valid phone number.' );
-			}
-
-			if ( field.elements ) {
+			if ( field?.elements ) {
 				const validValues = field.elements.map( ( f ) => f.value );
 				if ( ! validValues.includes( value ) ) {
 					return __( 'Value must be one of the elements.' );
