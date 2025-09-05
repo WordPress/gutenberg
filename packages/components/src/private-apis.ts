adf041fcdadbe7803ba785e8a242b95ef2c6b6aa
Jorge <jorge.costa@developer.pt>
2025-09-05 10:08:33 +0100

    Add @ icon prefix to email control with proper spacing

--- packages/components/src/private-apis.ts
+++ packages/components/src/private-apis.ts
@@ -14,6 +14,7 @@ import Badge from './badge';
 import { DateCalendar, DateRangeCalendar, TZDate } from './calendar';
 import {
 	ValidatedCheckboxControl,
+	ValidatedInputControl,
 	ValidatedNumberControl,
 	ValidatedTextControl,
 	ValidatedToggleControl,
@@ -33,6 +34,7 @@ lock( privateApis, {
 	DateCalendar,
 	DateRangeCalendar,
 	TZDate,
+	ValidatedInputControl,
 	ValidatedCheckboxControl,
 	ValidatedNumberControl,
 	ValidatedTextControl,
--- packages/dataviews/src/dataform-controls/email.tsx
+++ packages/dataviews/src/dataform-controls/email.tsx
@@ -1,8 +1,21 @@
 /**
+ * WordPress dependencies
+ */
+import {
+	Icon,
+	privateApis,
+	__experimentalInputControlPrefixWrapper as InputControlPrefixWrapper,
+} from '@wordpress/components';
+import { useCallback, useState } from '@wordpress/element';
+import { atSymbol } from '@wordpress/icons';
+
+/**
  * Internal dependencies
  */
 import type { DataFormControlProps } from '../types';
-import ValidatedText from './utils/validated-text';
+import { unlock } from '../lock-unlock';
+
+const { ValidatedInputControl } = unlock( privateApis );
 
 export default function Email< Item >( {
 	data,
@@ -10,9 +23,59 @@ export default function Email< Item >( {
 	onChange,
 	hideLabelFromVision,
 }: DataFormControlProps< Item > ) {
+	const { id, label, placeholder, description } = field;
+	const value = field.getValue( { item: data } );
+	const [ customValidity, setCustomValidity ] =
+		useState<
+			React.ComponentProps<
+				typeof ValidatedInputControl
+			>[ 'customValidity' ]
+		>( undefined );
+
+	const onChangeControl = useCallback(
+		( newValue: string ) =>
+			onChange( {
+				[ id ]: newValue,
+			} ),
+		[ id, onChange ]
+	);
+
 	return (
-		<ValidatedText
-			{ ...{ data, field, onChange, hideLabelFromVision, type: 'email' } }
+		<ValidatedInputControl
+			required={ !! field.isValid?.required }
+			onValidate={ ( newValue: any ) => {
+				const message = field.isValid?.custom?.(
+					{
+						...data,
+						[ id ]: newValue,
+					},
+					field
+				);
+
+				if ( message ) {
+					setCustomValidity( {
+						type: 'invalid',
+						message,
+					} );
+					return;
+				}
+
+				setCustomValidity( undefined );
+			} }
+			customValidity={ customValidity }
+			label={ label }
+			placeholder={ placeholder }
+			value={ value ?? '' }
+			help={ description }
+			onChange={ onChangeControl }
+			hideLabelFromVision={ hideLabelFromVision }
+			type="email"
+			prefix={
+				<InputControlPrefixWrapper variant="icon">
+					<Icon icon={ atSymbol } />
+				</InputControlPrefixWrapper>
+			}
+			__next40pxDefaultSize
 		/>
 	);
 }
