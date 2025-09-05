fcf900b78f4d79a4d20603e684e2b861c2c9b762
Gautam Mehta <gautam.mehta@rtcamp.com>
2025-09-05 14:03:02 +0530

    fix: remove unrelated changes

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -4,7 +4,6 @@
 import { __ } from '@wordpress/i18n';
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { createBlock, getDefaultBlockName } from '@wordpress/blocks';
-import { useEffect, useState } from '@wordpress/element';
 
 export default function CodeEdit( {
 	attributes,
@@ -14,29 +13,6 @@ export default function CodeEdit( {
 	mergeBlocks,
 } ) {
 	const blockProps = useBlockProps();
-	const [ supportsBreakSpaces, setSupportsBreakSpaces ] = useState( false );
-
-	// Check if the browser supports 'break-spaces' in CSS.
-	useEffect( () => {
-		const isSupported =
-			typeof CSS !== 'undefined' &&
-			// eslint-disable-next-line no-undef
-			CSS.supports &&
-			// eslint-disable-next-line no-undef
-			CSS.supports( 'white-space', 'break-spaces' );
-		setSupportsBreakSpaces( isSupported );
-	}, [] );
-
-	/**
-	 * If the browser supports 'break-spaces', we can use it for better
-	 * handling of whitespace in code blocks. Otherwise, we fall back to
-	 * 'pre-wrap', which is more widely supported.
-	 *
-	 * @constant
-	 * @type {string}
-	 */
-	const whiteSpaceStyle = supportsBreakSpaces ? 'break-spaces' : 'pre-wrap';
-
 	return (
 		<pre { ...blockProps }>
 			<RichText
@@ -53,7 +29,6 @@ export default function CodeEdit( {
 				__unstableOnSplitAtDoubleLineEnd={ () =>
 					insertBlocksAfter( createBlock( getDefaultBlockName() ) )
 				}
-				style={ { whiteSpace: whiteSpaceStyle } }
 			/>
 		</pre>
 	);
