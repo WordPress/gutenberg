4a569e4d90b2002300b8f728abe3ef934e1b53ec
ella <ella@automattic.com>
2025-09-04 17:45:43 -0700

    Add templates php filter so Woo templates show up

--- lib/compat/wordpress-6.8/class-gutenberg-rest-static-templates-controller.php
+++ lib/compat/wordpress-6.8/class-gutenberg-rest-static-templates-controller.php
@@ -100,6 +100,24 @@ class Gutenberg_REST_Static_Templates_Controller extends WP_REST_Templates_Contr
 
 		$query_result = array_merge( $query_result, $matching_registered_templates );
 
+		/**
+		 * Filters the array of queried block templates array after they've been fetched.
+		 *
+		 * @since 5.9.0
+		 *
+		 * @param WP_Block_Template[] $query_result Array of found block templates.
+		 * @param array               $query {
+		 *     Arguments to retrieve templates. All arguments are optional.
+		 *
+		 *     @type string[] $slug__in  List of slugs to include.
+		 *     @type int      $wp_id     Post ID of customized template.
+		 *     @type string   $area      A 'wp_template_part_area' taxonomy value to filter by (for 'wp_template_part' template type only).
+		 *     @type string   $post_type Post type to get the templates for.
+		 * }
+		 * @param string              $template_type wp_template or wp_template_part.
+		 */
+		$query_result = apply_filters( 'get_block_templates', $query_result, $query, 'wp_template' );
+
 		$templates = array();
 		foreach ( $query_result as $template ) {
 			$item               = $this->prepare_item_for_response( $template, $request );
