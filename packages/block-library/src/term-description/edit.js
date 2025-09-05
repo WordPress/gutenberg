138df24864cdb49a8c3c535c6a65df0c949a5ef0
M. L. Giannotta <sunyatasattva@gmail.com>
2025-09-05 22:20:19 +0200

    Enhance Term Description Block with Context Support
    - Added `usesContext` to `block.json` for `termId` and `taxonomy`.
    - Implemented `useTermDescription` hook to fetch term descriptions based on
    context or fallback to template parsing.
    - Updated `TermDescriptionEdit` component to display term descriptions or a
    placeholder based on the fetched data.
    
    This improves the block's functionality by allowing it to dynamically retrieve and
    display term descriptions in various contexts.

--- packages/block-library/src/term-description/edit.js
+++ packages/block-library/src/term-description/edit.js
@@ -13,18 +13,27 @@ import {
 	AlignmentControl,
 } from '@wordpress/block-editor';
 
+/**
+ * Internal dependencies
+ */
+import { useTermDescription } from './use-term-description';
+
 export default function TermDescriptionEdit( {
 	attributes,
 	setAttributes,
 	mergedStyle,
+	context: { termId, taxonomy },
 } ) {
 	const { textAlign } = attributes;
+	const { termDescription } = useTermDescription( termId, taxonomy );
+
 	const blockProps = useBlockProps( {
 		className: clsx( {
 			[ `has-text-align-${ textAlign }` ]: textAlign,
 		} ),
 		style: mergedStyle,
 	} );
+
 	return (
 		<>
 			<BlockControls group="block">
@@ -36,9 +45,15 @@ export default function TermDescriptionEdit( {
 				/>
 			</BlockControls>
 			<div { ...blockProps }>
-				<div className="wp-block-term-description__placeholder">
-					<span>{ __( 'Term Description' ) }</span>
-				</div>
+				{ termDescription ? (
+					<div
+						dangerouslySetInnerHTML={ { __html: termDescription } }
+					/>
+				) : (
+					<div className="wp-block-term-description__placeholder">
+						<span>{ __( 'Term Description' ) }</span>
+					</div>
+				) }
 			</div>
 		</>
 	);
