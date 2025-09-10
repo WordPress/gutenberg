47423d469762c011719f6d681ba551dfea0efa20
Sarah Norris <sarah@sekai.co.uk>
2025-09-10 09:07:37 +0100

    Move Term Template to experimental array

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -237,7 +237,6 @@ const getAllBlocks = () => {
 		homeLink,
 		logInOut,
 		termDescription,
-		termTemplate,
 		queryTitle,
 		postAuthorBiography,
 	];
@@ -248,6 +247,7 @@ const getAllBlocks = () => {
 		blocks.push( accordionHeader );
 		blocks.push( accordionPanel );
 		blocks.push( termsQuery );
+		blocks.push( termTemplate );
 	}
 
 	if ( window?.__experimentalEnableFormBlocks ) {
