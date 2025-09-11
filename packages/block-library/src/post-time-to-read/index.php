8ea7ecf854bdb1a852c1d5e5a4a96da5a3125f94
Ben Dwyer <ben@scruffian.com>
2025-09-11 10:56:20 +0100

    php linting

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -14,9 +14,8 @@
  * @param  WP_Block $block      Block instance.
  * @return string Returns the rendered post author name block.
  */
-function render_block_core_post_time_to_read($attributes, $content, $block)
-{
-	if (! isset($block->context['postId'])) {
+function render_block_core_post_time_to_read( $attributes, $content, $block ) {
+	if ( ! isset( $block->context['postId'] ) ) {
 		return '';
 	}
 
@@ -33,28 +32,28 @@ function render_block_core_post_time_to_read($attributes, $content, $block)
 
 	$word_count_type = wp_get_word_count_type();
 
-	$total_words = wp_word_count($content, $word_count_type);
+	$total_words = wp_word_count( $content, $word_count_type );
 
 	if (! empty($attributes['displayAsRange'])) {
-		$min_minutes = max(1, (int) round($total_words / $max_reading_rate));
-		$max_minutes = max(1, (int) round($total_words / $min_reading_rate));
+		$min_minutes = max(1, ( int ) round( $total_words / $max_reading_rate ) );
+		$max_minutes = max(1, ( int ) round( $total_words / $min_reading_rate ) );
 		if ($min_minutes === $max_minutes) {
 			$max_minutes = $min_minutes + 1;
 		}
 		/* translators: 1: minimum minutes, 2: maximum minutes to read the post. */
-		$minutes_to_read_string = sprintf(_x('%1$s–%2$s minutes', 'Range of minutes to read'), $min_minutes, $max_minutes);
+		$minutes_to_read_string = sprintf(_x( '%1$s–%2$s minutes', 'Range of minutes to read' ), $min_minutes, $max_minutes);
 	} else {
-		$minutes_to_read = max(1, (int) round($total_words / $average_reading_rate));
+		$minutes_to_read = max( 1, ( int ) round( $total_words / $average_reading_rate ) );
 		$minutes_to_read_string = sprintf(
 			/* translators: %s: the number of minutes to read the post. */
-			_n('%s minute', '%s minutes', $minutes_to_read),
+			_n( '%s minute', '%s minutes', $minutes_to_read ),
 			$minutes_to_read
 		);
 	}
 
-	$align_class_name = empty($attributes['textAlign']) ? '' : "has-text-align-{$attributes['textAlign']}";
+	$align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
 
-	$wrapper_attributes = get_block_wrapper_attributes(array('class' => $align_class_name));
+	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $align_class_name ) );
 
 	return sprintf(
 		'<div %1$s>%2$s</div>',
@@ -66,8 +65,7 @@ function render_block_core_post_time_to_read($attributes, $content, $block)
 /**
  * Registers the `core/post-time-to-read` block on the server.
  */
-function register_block_core_post_time_to_read()
-{
+function register_block_core_post_time_to_read() {
 	register_block_type_from_metadata(
 		__DIR__ . '/post-time-to-read',
 		array(
@@ -75,4 +73,4 @@ function register_block_core_post_time_to_read()
 		)
 	);
 }
-add_action('init', 'register_block_core_post_time_to_read');
+add_action( 'init', 'register_block_core_post_time_to_read' );
