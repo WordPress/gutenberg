30ec8f40a5e814516a9fa4222338cd892916f5b3
Ben Dwyer <ben@scruffian.com>
2025-09-11 12:00:11 +0100

    linting fix

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -35,15 +35,15 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
 	$total_words = wp_word_count( $content, $word_count_type );
 
 	if ( ! empty( $attributes['displayAsRange'] ) ) {
-		$min_minutes = max( 1, ( int ) round( $total_words / $max_reading_rate ) );
-		$max_minutes = max( 1, ( int ) round( $total_words / $min_reading_rate ) );
+		$min_minutes = max( 1, (int) round( $total_words / $max_reading_rate ) );
+		$max_minutes = max( 1, (int) round( $total_words / $min_reading_rate ) );
 		if ( $min_minutes === $max_minutes ) {
 			$max_minutes = $min_minutes + 1;
 		}
 		/* translators: 1: minimum minutes, 2: maximum minutes to read the post. */
 		$minutes_to_read_string = sprintf( _x( '%1$s–%2$s minutes', 'Range of minutes to read' ), $min_minutes, $max_minutes );
 	} else {
-		$minutes_to_read        = max( 1, ( int ) round( $total_words / $average_reading_rate ) );
+		$minutes_to_read        = max( 1, (int) round( $total_words / $average_reading_rate ) );
 		$minutes_to_read_string = sprintf(
 			/* translators: %s: the number of minutes to read the post. */
 			_n( '%s minute', '%s minutes', $minutes_to_read ),
