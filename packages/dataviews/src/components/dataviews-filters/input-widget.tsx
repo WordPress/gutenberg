d1e45c86b845a3b60e660c17fa2aeef754c7688d
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-09-12 17:51:09 +0200

    Filters: prepare data and field

--- packages/dataviews/src/components/dataviews-filters/input-widget.tsx
+++ packages/dataviews/src/components/dataviews-filters/input-widget.tsx
@@ -32,37 +32,60 @@ export default function InputWidget( {
 	const currentFilter = view.filters?.find(
 		( f ) => f.field === filter.field
 	);
+	const currentValue = getCurrentValue( filter, currentFilter );
 
+	/*
+	 * We are reusing the field.Edit component for filters. By doing so,
+	 * we get for free a filter control specific to the field type
+	 * and other aspects of the field API (Edit control configuration, etc.).
+	 *
+	 * This approach comes with an issue: the field.Edit controls work with getValue
+	 * and setValue methods, which take an item (Item) as parameter. But, at this point,
+	 * we don't have an item and we don't know how to create one, either.
+	 *
+	 * So, what we do is to prepare the data and the relevant field configuration
+	 * as if Item was a plain object whose keys are the field ids:
+	 *
+	 * {
+	 *   [ fieldOne.id ]: value,
+	 *   [ fieldTwo.id ]: value,
+	 * }
+	 *
+	 */
 	const field = useMemo( () => {
 		const currentField = fields.find( ( f ) => f.id === filter.field );
 		if ( currentField ) {
 			return {
 				...currentField,
+				// Deactivate validation for filters.
 				isValid: {
+					required: false,
 					custom: () => null,
+				},
+				// Configure getValue/setValue as if Item was a plain object.
+				getValue: ( { item }: { item: any } ) => {
+					return item[ currentField.id ];
+				},
+				setValue: ( { item, value }: { item: any; value: any } ) => {
+					return {
+						...item,
+						[ currentField.id ]: value,
+					};
 				},
 			};
 		}
 		return currentField;
 	}, [ fields, filter.field ] );
-	const currentValue = getCurrentValue( filter, currentFilter );
 	const data = useMemo( () => {
 		return ( view.filters ?? [] ).reduce(
 			( acc, activeFilter ) => {
-				const activeFilterField = fields.find(
-					( f ) => f.id === activeFilter.field
-				);
-				if ( activeFilterField ) {
-					acc = activeFilterField.setValue( {
-						item: {},
-						value: activeFilter.value,
-					} );
-				}
+				// We can now assume the field is stored as a Item prop.
+				acc[ activeFilter.field ] = activeFilter.value;
 				return acc;
 			},
 			{} as Record< string, any >
 		);
-	}, [ fields, view.filters ] );
+	}, [ view.filters ] );
 
 	const handleChange = useEvent( ( updatedData: Record< string, any > ) => {
 		if ( ! field || ! currentFilter ) {
