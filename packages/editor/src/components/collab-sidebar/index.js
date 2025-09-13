a326d7ab6fbf05ae26713ddf236e926d729620b6
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-09-13 13:49:51 -0600

    try: finding and using anchor for popover

--- packages/editor/src/components/collab-sidebar/index.js
+++ packages/editor/src/components/collab-sidebar/index.js
@@ -58,6 +58,7 @@ function CollabSidebarContent( {
 	setShowCommentBoard,
 	styles,
 	comments,
+	showCommentsinPopover,
 } ) {
 	const { createNotice } = useDispatch( noticesStore );
 	const { saveEntityRecord, deleteEntityRecord } = useDispatch( coreStore );
@@ -226,6 +227,7 @@ function CollabSidebarContent( {
 				onCommentReopen={ onCommentReopen }
 				showCommentBoard={ showCommentBoard }
 				setShowCommentBoard={ setShowCommentBoard }
+				showCommentsinPopover = { showCommentsinPopover }
 			/>
 		</div>
 	);
@@ -399,7 +401,7 @@ export default function CollabSidebar() {
 					styles={ {
 						backgroundColor,
 					} }
-					showCommentsinPopover
+					showCommentsinPopover = { true }
 				/>
 			</PluginSidebar>
 		</>
