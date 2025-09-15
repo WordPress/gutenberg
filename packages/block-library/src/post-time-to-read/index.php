3f934d5186b4ed218c8711998dbd3745ce8ef0ee
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:28:24 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -27,14 +27,14 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
 
 	$total_words = wp_word_count( $content, $word_count_type );
 
-    if ( ! empty($attributes['displayAsRange'])) {
-        // Calculate faster reading rate with 20% speed = lower minutes,
-        // and slower reading rate with 20% speed = higher minutes.
-        $min_minutes = max(1, (int) round($total_words / $average_reading_rate * 0.8));
-        $max_minutes = max(1, (int) round($total_words / $average_reading_rate * 1.2));
-        if ($min_minutes === $max_minutes) {
-            $max_minutes = $min_minutes + 1;
-        }
+	if ( ! empty($attributes['displayAsRange'])) {
+		// Calculate faster reading rate with 20% speed = lower minutes,
+		// and slower reading rate with 20% speed = higher minutes.
+		$min_minutes = max(1, (int) round($total_words / $average_reading_rate * 0.8));
+		$max_minutes = max(1, (int) round($total_words / $average_reading_rate * 1.2));
+		if ($min_minutes === $max_minutes) {
+		$max_minutes = $min_minutes + 1;
+		}
 	/* translators: 1: minimum minutes, 2: maximum minutes to read the post. */
 		$minutes_to_read_string = sprintf(
 			_x('%1$s–%2$s minutes', 'Range of minutes to read'),
