d19ab9b7e370d2e32e6cd7deae3a881d1d3e2ed8
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:25:46 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -9,10 +9,9 @@
 /**
  * Renders the `core/post-time-to-read` block on the server.
  *
- * @param  array  $attributes  Block attributes.
- * @param  string  $content  Block default content.
- * @param  WP_Block  $block  Block instance.
- *
+ * @param  array    $attributes Block attributes.
+ * @param  string   $content    Block default content.
+ * @param  WP_Block $block      Block instance.
  * @return string Returns the rendered post author name block.
  */
 function render_block_core_post_time_to_read( $attributes, $content, $block ) {
