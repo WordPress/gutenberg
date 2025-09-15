d2d45fe1e645f4db3073ffc10289fe660d8fbab8
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:22:04 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -51,7 +51,7 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
         );
     }
 
-    $align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
+	$align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
 
 	$wrapper_attributes = get_block_wrapper_attributes(array('class' => $align_class_name));
 
