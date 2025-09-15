b88d17876b79bac878aabf24b8dccd2df3f89fb4
karthick.murugan <karthick.murugan02@gmail.com>
2025-09-15 18:33:28 +0530

    Phpcs Update

--- phpunit/class-block-comment-filter-test.php
+++ phpunit/class-block-comment-filter-test.php
@@ -1,17 +1,23 @@
 <?php
+/**
+ * Unit tests for block comment filter functions.
+ *
+ * @package Gutenberg
+ * @since 6.9.0
+ */
 
 /**
  * This class contains unit tests for the block comment filter functions.
  *
  * @package Gutenberg
  */
-class Tests_blockCommentFilter extends WP_UnitTestCase {
+class Tests_BlockCommentFilter extends WP_UnitTestCase {
 
 	/**
 	 * Tests that `update_comment_type_in_rest_api_6_9` updates comment type correctly.
 	 */
 	public function test_update_comment_type_in_rest_api_6_9() {
-		// Mock request and prepared comment
+		// Mock request and prepared comment.
 		$request = new WP_REST_Request( WP_REST_Server::READABLE );
 		$request->set_param( 'comment_type', 'block_comment' );
 		$request->set_param( 'comment_approved', '1' );
@@ -21,10 +27,10 @@ class Tests_blockCommentFilter extends WP_UnitTestCase {
 			'comment_approved' => '0',
 		);
 
-		// Call the function
+		// Call the function.
 		$updated_comment = update_comment_type_in_rest_api_6_9( $prepared_comment, $request );
 
-		// Assertions
+		// Assertions.
 		$this->assertEquals( 'block_comment', $updated_comment['comment_type'] );
 		$this->assertEquals( '1', $updated_comment['comment_approved'] );
 	}
@@ -34,13 +40,13 @@ class Tests_blockCommentFilter extends WP_UnitTestCase {
 	 */
 	public function test_update_get_avatar_comment_type() {
 
-		// Mock comment types
+		// Mock comment types.
 		$comment_types = array( 'comment', 'pingback' );
 
-		// Call the function
+		// Call the function.
 		$updated_comment_types = update_get_avatar_comment_type( $comment_types );
 
-		// Assertions
+		// Assertions.
 		$this->assertContains( 'block_comment', $updated_comment_types );
 	}
 
