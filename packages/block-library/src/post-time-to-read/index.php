dbdfb7468d11eb09a486b6e8c919892d33f2ab82
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:25:39 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -16,7 +16,7 @@
  * @return string Returns the rendered post author name block.
  */
 function render_block_core_post_time_to_read( $attributes, $content, $block ) {
-	if ( ! isset($block->context['postId'])) {
+	if ( ! isset( $block->context['postId'] ) ) {
 		return '';
 	}
 
