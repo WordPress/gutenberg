ae0c9b5c51836eae18577fa672c252ae0efde40c
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:28:18 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -35,20 +35,20 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
         if ($min_minutes === $max_minutes) {
             $max_minutes = $min_minutes + 1;
         }
-        /* translators: 1: minimum minutes, 2: maximum minutes to read the post. */
-        $minutes_to_read_string = sprintf(
-            _x('%1$s–%2$s minutes', 'Range of minutes to read'),
-            $min_minutes,
-            $max_minutes
-        );
-    } else {
-        $minutes_to_read        = max(1, (int) round($total_words / $average_reading_rate));
-        $minutes_to_read_string = sprintf(
-        /* translators: %s: the number of minutes to read the post. */
-            _n('%s minute', '%s minutes', $minutes_to_read),
-            $minutes_to_read
-        );
-    }
+	/* translators: 1: minimum minutes, 2: maximum minutes to read the post. */
+		$minutes_to_read_string = sprintf(
+			_x('%1$s–%2$s minutes', 'Range of minutes to read'),
+			$min_minutes,
+			$max_minutes
+		);
+	} else {
+		$minutes_to_read        = max(1, (int) round($total_words / $average_reading_rate));
+		$minutes_to_read_string = sprintf(
+		/* translators: %s: the number of minutes to read the post. */
+		_n('%s minute', '%s minutes', $minutes_to_read),
+			$minutes_to_read
+		);
+	}
 
 	$align_class_name = empty( $attributes['textAlign'] ) ? '' : "has-text-align-{$attributes['textAlign']}";
 
