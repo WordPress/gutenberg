280cecca589ca87d87105e38a196008f1398d451
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-09-15 15:02:33 +0200

    DataForm radio control: support validation and description

--- packages/dataviews/src/dataform-controls/radio.tsx
+++ packages/dataviews/src/dataform-controls/radio.tsx
@@ -1,13 +1,21 @@
 /**
+ * External dependencies
+ */
+import deepMerge from 'deepmerge';
+
+/**
  * WordPress dependencies
  */
-import { RadioControl } from '@wordpress/components';
-import { useCallback } from '@wordpress/element';
+import { privateApis } from '@wordpress/components';
+import { useCallback, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import type { DataFormControlProps } from '../types';
+import { unlock } from '../lock-unlock';
+
+const { ValidatedRadioControl } = unlock( privateApis );
 
 export default function Radio< Item >( {
 	data,
@@ -15,8 +23,14 @@ export default function Radio< Item >( {
 	onChange,
 	hideLabelFromVision,
 }: DataFormControlProps< Item > ) {
-	const { label, getValue, setValue } = field;
+	const { label, description, elements, getValue, setValue } = field;
 	const value = getValue( { item: data } );
+	const [ customValidity, setCustomValidity ] =
+		useState<
+			React.ComponentProps<
+				typeof ValidatedRadioControl
+			>[ 'customValidity' ]
+		>( undefined );
 
 	const onChangeControl = useCallback(
 		( newValue: string ) =>
@@ -24,12 +38,42 @@ export default function Radio< Item >( {
 		[ data, onChange, setValue ]
 	);
 
-	if ( field.elements ) {
+	const onValidateControl = useCallback(
+		( newValue: any ) => {
+			const message = field.isValid?.custom?.(
+				deepMerge(
+					data,
+					setValue( {
+						item: data,
+						value: newValue,
+					} ) as Partial< Item >
+				),
+				field
+			);
+
+			if ( message ) {
+				setCustomValidity( {
+					type: 'invalid',
+					message,
+				} );
+				return;
+			}
+
+			setCustomValidity( undefined );
+		},
+		[ data, field, setValue ]
+	);
+
+	if ( elements ) {
 		return (
-			<RadioControl
+			<ValidatedRadioControl
+				required={ !! field.isValid?.required }
+				onValidate={ onValidateControl }
+				customValidity={ customValidity }
 				label={ label }
+				help={ description }
 				onChange={ onChangeControl }
-				options={ field.elements }
+				options={ elements }
 				selected={ value }
 				hideLabelFromVision={ hideLabelFromVision }
 			/>
