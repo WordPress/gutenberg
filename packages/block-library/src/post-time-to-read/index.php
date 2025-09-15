229b092b36f2bfca9e26ac1d0bb1312c419925c5
Ben Dwyer <ben@scruffian.com>
2025-09-15 17:35:01 +0100

    Update packages/block-library/src/post-time-to-read/index.php

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -31,7 +31,7 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
 		// Calculate faster reading rate with 20% speed = lower minutes,
 		// and slower reading rate with 20% speed = higher minutes.
 		$min_minutes = max( 1, (int) round( $total_words / $average_reading_rate * 0.8 ) );
-		$max_minutes = max(1, (int) round($total_words / $average_reading_rate * 1.2));
+		$max_minutes = max( 1, (int) round( $total_words / $average_reading_rate * 1.2 ) );
 		if ($min_minutes === $max_minutes) {
 		$max_minutes = $min_minutes + 1;
 		}
