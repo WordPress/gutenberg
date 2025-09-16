1bb9a107dd352c822da027a66976b6e6311cd48f
Daniel Richards <daniel.richards@automattic.com>
2025-09-16 18:29:48 +0800

    Stub out media and link controls

--- packages/block-library/src/heading/index.js
+++ packages/block-library/src/heading/index.js
@@ -69,4 +69,17 @@ export const settings = {
 	variations,
 };
 
+if ( window.__experimentalContentOnlyPatternInsertion ) {
+	settings.controls = [
+		{
+			label: __( 'Content' ),
+			type: 'RichText',
+			shownByDefault: true,
+			mapping: {
+				value: 'content',
+			},
+		},
+	];
+}
+
 export const init = () => initBlock( { name, metadata, settings } );
