7711d050fd80a1a90a20f44be739e83d287006b4
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-09-16 11:32:54 +0200

    Add typing to iAPI's getConfig()

--- packages/interactivity/src/store.ts
+++ packages/interactivity/src/store.ts
@@ -20,7 +20,7 @@ const serverStates = new Map();
  * @param namespace Store's namespace from which to retrieve the config.
  * @return Defined config for the given namespace.
  */
-export const getConfig = ( namespace?: string ) =>
+export const getConfig = < T extends object >( namespace?: string ): T =>
 	storeConfigs.get( namespace || getNamespace() ) || {};
 
 /**
