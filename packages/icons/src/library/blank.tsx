bf94f22ac2852d61b271f54b3da9d01b2103aab9
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-09-16 15:59:16 +0530

    fix: add blank icon for suggestions without an icon

---
+++ packages/icons/src/library/blank.tsx
@@ -0,0 +1,12 @@
+/**
+ * WordPress dependencies
+ */
+import { SVG } from '@wordpress/primitives';
+
+const blank = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		{ /* Empty SVG. Used to maintain the 24x24 viewBox for consistent spacing */ }
+	</SVG>
+);
+
+export default blank;
