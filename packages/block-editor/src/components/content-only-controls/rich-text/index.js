536df4a6bc642946d06afb6a4c498aaddbf76c7d
Daniel Richards <daniel.richards@automattic.com>
2025-09-17 17:28:39 +0800

    Semi functioning rich text control

--- packages/block-editor/src/components/content-only-controls/rich-text/index.js
+++ packages/block-editor/src/components/content-only-controls/rich-text/index.js
@@ -1,23 +1,74 @@
 /**
  * WordPress dependencies
  */
-import {
-	__experimentalToolsPanelItem as ToolsPanelItem,
-	TextControl,
-} from '@wordpress/components';
+import { __experimentalToolsPanelItem as ToolsPanelItem } from '@wordpress/components';
+import { useInstanceId, useMergeRefs } from '@wordpress/compose';
 import { __unstableStripHTML as stripHTML } from '@wordpress/dom';
+import { useState } from '@wordpress/element';
+import {
+	__unstableUseRichText as useRichText,
+	// removeFormat,
+} from '@wordpress/rich-text';
 
-export default function RichText( {
+/**
+ * Internal dependencies
+ */
+import { useFormatTypes } from '../rich-text';
+
+export default function RichTextControl( {
 	clientId,
 	control,
 	blockType,
 	attributeValues,
 	updateAttributes,
 } ) {
+	const instanceId = useInstanceId( RichTextControl );
+	const controlId = `block-editor-content-only-controls__rich-text-${ instanceId }`;
 	const valueKey = control.mapping.value;
-	const value = attributeValues[ valueKey ];
+	const attrValue = attributeValues[ valueKey ];
 	const defaultValue =
 		blockType.attributes[ valueKey ]?.defaultValue ?? undefined;
+	const [ selection, setSelection ] = useState( {
+		start: undefined,
+		end: undefined,
+	} );
+
+	// const {
+	// 	formatTypes,
+	// 	prepareHandlers,
+	// 	valueHandlers,
+	// 	changeHandlers,
+	// 	dependencies,
+	// } = useFormatTypes( {
+	// 	clientId,
+	// 	identifier,
+	// 	allowedFormats: adjustedAllowedFormats,
+	// 	withoutInteractiveFormatting,
+	// 	disableNoneEssentialFormatting: isContentOnlyWriteMode,
+	// } );
+
+	const { value, ref: richTextRef } = useRichText( {
+		value: attrValue,
+		onChange( html, { __unstableFormats, __unstableText } ) {
+			updateAttributes( { [ valueKey ]: html } );
+			// Object.values( changeHandlers ).forEach( ( changeHandler ) => {
+			// 	changeHandler( __unstableFormats, __unstableText );
+			// } );
+		},
+		selectionStart: selection.start,
+		selectionEnd: selection.end,
+		onSelectionChange: ( start, end ) => setSelection( { start, end } ),
+		// placeholder: bindingsPlaceholder || placeholder,
+		// __unstableIsSelected: isSelected,
+		// __unstableDisableFormats: disableFormats,
+		// preserveWhiteSpace,
+		// __unstableDependencies: dependencies,
+		// __unstableAfterParse: addEditorOnlyFormats,
+		// __unstableBeforeSerialize: removeEditorOnlyFormats,
+		// __unstableAddInvisibleFormats: addInvisibleFormats,
+	} );
+
+	const hasVisibleLabel = ! control.shownByDefault;
 
 	return (
 		<ToolsPanelItem
@@ -28,21 +79,21 @@ export default function RichText( {
 					value !== defaultValue && stripHTML( value )?.length !== 0
 				);
 			} }
-			onDeselect={ () => {
-				updateAttributes( { [ valueKey ]: defaultValue } );
-			} }
+			onDeselect={ () => {} }
 			isShownByDefault={ control.shownByDefault }
 		>
-			<TextControl
-				__nextHasNoMarginBottom
-				__next40pxDefaultSize
-				label={ control.label }
-				value={ value ? stripHTML( value ) : '' }
-				onChange={ ( newValue ) => {
-					updateAttributes( { [ valueKey ]: newValue } );
-				} }
-				autoComplete="off"
-				hideLabelFromVision={ control.shownByDefault }
+			{ hasVisibleLabel && (
+				<label htmlFor={ controlId }>{ control.label }</label>
+			) }
+			<div
+				tagName="div"
+				role="textbox"
+				id={ hasVisibleLabel ? controlId : undefined }
+				className="block-editor-content-only-controls__rich-text"
+				aria-label={ hasVisibleLabel ? undefined : control.label }
+				aria-multiline={ ! control.args?.disableLineBreaks }
+				ref={ useMergeRefs( [ richTextRef ] ) }
+				contentEditable
 			/>
 		</ToolsPanelItem>
 	);
