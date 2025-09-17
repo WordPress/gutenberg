0397fbd05f17fe9a22429518356351db0fa2eeca
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-09-17 14:32:31 +0200

    Fix merge conflicts

--- packages/block-library/src/terms-query/inspector-controls/ordering-controls.js
+++ packages/block-library/src/terms-query/inspector-controls/ordering-controls.js
@@ -11,45 +11,45 @@ export default function OrderingControls( { attributes, setQuery } ) {
 	const { termQuery } = attributes;
 
 	return (
-		<>
-			<ToolsPanelItem
-				hasValue={ () => termQuery.order !== 'asc' }
-				label={ __( 'Order' ) }
-				onDeselect={ () => setQuery( { order: 'asc' } ) }
-				isShownByDefault
-			>
-				<SelectControl
-					__nextHasNoMarginBottom
-					__next40pxDefaultSize
-					label={ __( 'Order' ) }
-					options={ [
-						{ label: __( 'Ascending' ), value: 'asc' },
-						{ label: __( 'Descending' ), value: 'desc' },
-					] }
-					value={ termQuery.order }
-					onChange={ ( order ) => setQuery( { order } ) }
-				/>
-			</ToolsPanelItem>
-
-			<ToolsPanelItem
-				hasValue={ () => termQuery.orderBy !== 'name' }
+		<ToolsPanelItem
+			hasValue={ () =>
+				termQuery.orderBy !== 'name' || termQuery.order !== 'asc'
+			}
+			label={ __( 'Order by' ) }
+			onDeselect={ () => setQuery( { orderBy: 'name', order: 'asc' } ) }
+			isShownByDefault
+		>
+			<SelectControl
+				__nextHasNoMarginBottom
+				__next40pxDefaultSize
 				label={ __( 'Order by' ) }
-				onDeselect={ () => setQuery( { orderBy: 'name' } ) }
-				isShownByDefault
-			>
-				<SelectControl
-					__nextHasNoMarginBottom
-					__next40pxDefaultSize
-					label={ __( 'Order by' ) }
-					options={ [
-						{ label: __( 'Name' ), value: 'name' },
-						{ label: __( 'Slug' ), value: 'slug' },
-						{ label: __( 'Count' ), value: 'count' },
-					] }
-					value={ termQuery.orderBy }
-					onChange={ ( orderBy ) => setQuery( { orderBy } ) }
-				/>
-			</ToolsPanelItem>
-		</>
+				options={ [
+					{
+						label: __( 'Name: A → Z' ),
+						value: 'name/asc',
+					},
+					{
+						label: __( 'Name: Z → A' ),
+						value: 'name/desc',
+					},
+					{
+						label: __( 'Count, high to low' ),
+						value: 'count/desc',
+					},
+					{
+						label: __( 'Count, low to high' ),
+						value: 'count/asc',
+					},
+				] }
+				value={ termQuery.orderBy + '/' + termQuery.order }
+				onChange={ ( orderBy ) => {
+					const [ newOrderBy, newOrder ] = orderBy.split( '/' );
+					setQuery( {
+						orderBy: newOrderBy,
+						order: newOrder,
+					} );
+				} }
+			/>
+		</ToolsPanelItem>
 	);
 }
