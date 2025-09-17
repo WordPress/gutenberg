b357a0b4b84766b8d4231931f3935534bbca9f05
Daniel Richards <daniel.richards@automattic.com>
2025-09-17 10:36:40 +0800

    Move controls to separate files

---
+++ packages/block-editor/src/components/content-only-controls/media/index.js
@@ -0,0 +1,175 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	Button,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+	__experimentalGrid as Grid,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { lineSolid } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import MediaUpload from '../../media-upload';
+import MediaUploadCheck from '../../media-upload/check';
+
+export default function Media( {
+	clientId,
+	control,
+	blockType,
+	attributeValues,
+	updateAttributes,
+} ) {
+	const idKey = control.mapping.id;
+	const srcKey = control.mapping.src;
+	const captionKey = control.mapping.caption;
+	const altKey = control.mapping.alt;
+
+	const src = attributeValues[ srcKey ];
+	const caption = attributeValues[ captionKey ];
+	const alt = attributeValues[ altKey ];
+
+	const idDefaultValue =
+		blockType.attributes[ idKey ]?.defaultValue ?? undefined;
+	const srcDefaultValue =
+		blockType.attributes[ srcKey ]?.defaultValue ?? undefined;
+	const captionDefaultValue =
+		blockType.attributes[ captionKey ]?.defaultValue ?? undefined;
+	const altDefaultValue =
+		blockType.attributes[ altKey ]?.defaultValue ?? undefined;
+
+	// TODO - pluralize.
+	let chooseItemLabel;
+	if ( control.args.allowedTypes.length === 1 ) {
+		const allowedType = control.args.allowedTypes[ 0 ];
+		if ( allowedType === 'image' ) {
+			chooseItemLabel = __( 'Choose image' );
+		} else if ( allowedType === 'video' ) {
+			chooseItemLabel = __( 'Choose video' );
+		} else if ( allowedType === 'application' ) {
+			chooseItemLabel = __( 'Choose file' );
+		} else {
+			chooseItemLabel = __( 'Choose media item' );
+		}
+	} else {
+		chooseItemLabel = __( 'Choose media item' );
+	}
+
+	return (
+		<MediaUploadCheck>
+			<ToolsPanelItem
+				panelId={ clientId }
+				label={ control.label }
+				hasValue={ () => !! src }
+				onDeselect={ () => {
+					updateAttributes( {
+						[ idKey ]: idDefaultValue,
+						[ srcKey ]: srcDefaultValue,
+						[ captionKey ]: captionDefaultValue,
+						[ altKey ]: altDefaultValue,
+					} );
+				} }
+				isShownByDefault={ control.shownByDefault }
+			>
+				<MediaUpload
+					onSelect={ ( selectedMedia ) => {
+						if ( selectedMedia.id && selectedMedia.url ) {
+							const optionalAttributes = {};
+
+							if ( ! caption && selectedMedia.caption ) {
+								optionalAttributes[ captionKey ] =
+									selectedMedia.caption;
+							}
+							if ( ! alt && selectedMedia.alt ) {
+								optionalAttributes[ altKey ] =
+									selectedMedia.alt;
+							}
+
+							updateAttributes( {
+								[ idKey ]: selectedMedia.id,
+								[ srcKey ]: selectedMedia.url,
+								...optionalAttributes,
+							} );
+						}
+					} }
+					allowedTypes={ control.args.allowedTypes }
+					multiple={ control.args.multiple }
+					render={ ( { open } ) => {
+						return (
+							<div
+								role="button"
+								tabIndex={ -1 }
+								onClick={ () => {
+									open();
+								} }
+								onKeyDown={ open }
+							>
+								<Grid
+									rowGap={ 0 }
+									columnGap={ 8 }
+									templateColumns="24px 1fr 24px"
+								>
+									{ src && (
+										<>
+											<img
+												className="block-editor-content-only-controls__media-preview"
+												alt=""
+												width={ 24 }
+												height={ 24 }
+												src={ src }
+											/>
+											<span className="block-editor-content-only-controls__media-title">
+												{
+													// TODO - use media title instead of src.
+													src
+												}
+											</span>
+										</>
+									) }
+									{ ! src && (
+										<>
+											<span
+												className="block-editor-content-only-controls__media-placeholder"
+												style={ {
+													width: '24px',
+													height: '24px',
+												} }
+											/>
+											<span className="block-editor-content-only-controls__media-title">
+												{ chooseItemLabel }
+											</span>
+										</>
+									) }
+									{ src && (
+										<>
+											<Button
+												size="small"
+												className="block-editor-content-only-controls__remove-button"
+												icon={ lineSolid }
+												onClick={ ( event ) => {
+													event.stopPropagation();
+													updateAttributes( {
+														[ idKey ]:
+															idDefaultValue,
+														[ srcKey ]:
+															srcDefaultValue,
+														[ captionKey ]:
+															captionDefaultValue,
+														[ altKey ]:
+															altDefaultValue,
+													} );
+												} }
+											/>
+										</>
+									) }
+								</Grid>
+							</div>
+						);
+					} }
+				/>
+			</ToolsPanelItem>
+		</MediaUploadCheck>
+	);
+}
