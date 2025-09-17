39ca8be217aef78e540c5b7368858040b546d803
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-09-17 18:26:11 +0900

    Remove unintentionally added comment

--- packages/editor/src/components/collab-sidebar/index.js
+++ packages/editor/src/components/collab-sidebar/index.js
@@ -351,7 +351,7 @@ export default function CollabSidebar() {
 		? resultComments.find( ( thread ) => thread.id === blockCommentId )
 		: null;
 
-	// If postId is not a valid number, do not render the comment sidebar. 1Code has comments. Press enter to view.
+	// If postId is not a valid number, do not render the comment sidebar.
 	if ( ! ( !! postId && typeof postId === 'number' ) ) {
 		return null;
 	}
