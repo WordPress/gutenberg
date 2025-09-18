9e7ee2f9a422b3ee4b9a10701fa5feede14fcaaf
Peter Wilson <git-commit-by@peterwilson.cc>
2025-09-18 12:54:05 +1000

    Test comment IDs are included/excluded as expected.

--- phpunit/experimental/block-comments-test.php
+++ phpunit/experimental/block-comments-test.php
@@ -11,6 +11,20 @@
 class Block_Comments_Test extends WP_UnitTestCase {
 
 	/**
+	 * Comment IDs created for the test.
+	 *
+	 * @var int[]
+	 */
+	public static $comment_ids = array();
+
+	/**
+	 * Block comment IDs created for the test.
+	 *
+	 * @var int[]
+	 */
+	public static $block_comment_ids = array();
+
+	/**
 	 * Set up shared fixtures.
 	 *
 	 * @param WP_UnitTest_Factory $factory The WP test factory.
@@ -24,16 +38,18 @@ class Block_Comments_Test extends WP_UnitTestCase {
 		);
 
 		// Create three regular comments.
-		$factory->comment->create_many( 3, array( 'comment_post_ID' => $post_id ) );
+		self::$comment_ids = $factory->comment->create_many( 3, array( 'comment_post_ID' => $post_id ) );
+		self::$comment_ids = array_map( 'intval', self::$comment_ids );
 
 		// Create two block comments.
-		$factory->comment->create_many(
+		self::$block_comment_ids = $factory->comment->create_many(
 			2,
 			array(
 				'comment_post_ID' => $post_id,
 				'comment_type'    => 'block_comment',
 			)
 		);
+		self::$block_comment_ids = array_map( 'intval', self::$block_comment_ids );
 	}
 
 	/**
@@ -47,6 +63,9 @@ class Block_Comments_Test extends WP_UnitTestCase {
 		$query = new WP_Comment_Query();
 		$query->query( $query_vars );
 		$query->assertCount( 3, $query->comments );
+		$comment_ids = wp_list_pluck( $query->comments, 'comment_ID' );
+		$comment_ids = array_map( 'intval', $comment_ids );
+		$this->assertEmpty( array_intersect( $comment_ids, self::$block_comment_ids ) );
 		$this->assertContains( 'block_comment', $query->query_vars['type__not_in'] );
 		$this->assertNotContains( 'block_comment', (array) $query->query_vars['type'] );
 		$this->assertNotContains( 'block_comment', (array) $query->query_vars['type__in'] );
@@ -110,6 +129,9 @@ class Block_Comments_Test extends WP_UnitTestCase {
 	public function test_not_exclude_block_comments_queries( $query_vars, $expected_count = 2 ) {
 		$query = new WP_Comment_Query();
 		$query->query( $query_vars );
+		$comment_ids = wp_list_pluck( $query->comments, 'comment_ID' );
+		$comment_ids = array_map( 'intval', $comment_ids );
+		$this->assertNotEmpty( array_intersect( $comment_ids, self::$block_comment_ids ) );
 		$query->assertCount( $expected_count, $query->comments );
 		$this->assertNotContains( 'block_comment', (array) $query->query_vars['type__not_in'] );
 		$this->assertContains( 'block_comment', array_merge( (array) $query->query_vars['type'], (array) $query->query_vars['type__in'] ) );
@@ -150,9 +172,15 @@ class Block_Comments_Test extends WP_UnitTestCase {
 		$query = new WP_Comment_Query();
 		$query->query( array() );
 		$this->assertCount( 3, $query->comments );
+		$comment_ids = wp_list_pluck( $query->comments, 'comment_ID' );
+		$comment_ids = array_map( 'intval', $comment_ids );
+		$this->assertEmpty( array_intersect( $comment_ids, self::$block_comment_ids ) );
 
 		$query2 = new WP_Comment_Query();
 		$query2->query( array( 'type' => 'block_comment' ) );
+		$comment_ids2 = wp_list_pluck( $query2->comments, 'comment_ID' );
+		$comment_ids2 = array_map( 'intval', $comment_ids2 );
 		$this->assertCount( 2, $query2->comments );
+		$this->assertSameSets( $comment_ids2, self::$block_comment_ids );
 	}
 }
