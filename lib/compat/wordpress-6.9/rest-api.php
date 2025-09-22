fa444fe0e5c24f4d93609060b7462ea89c03d4ce
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-09-22 08:04:33 -0600

    Enable empty comments by substituting a placeholder

--- lib/compat/wordpress-6.9/rest-api.php
+++ lib/compat/wordpress-6.9/rest-api.php
@@ -29,3 +29,18 @@ function gutenberg_rest_theme_export_link_rel( $response, $theme ) {
 	return $response;
 }
 add_filter( 'rest_prepare_theme', 'gutenberg_rest_theme_export_link_rel', 10, 2 );
+
+/**
+ * Use the `comments_save_pre` filter to remove the dummy content added to work around
+ * the REST API's limitation which does not allow empty content.
+ *
+ * Note: Comments with empty content will be supported in WordPress 6.9. Once that becomes
+ * the minimum required version, this function can be removed and actual empty comments sent.
+ */
+function gutenberg_remove_dummy_content_from_empty_comments( $comment ) {
+	if ( '<!-- GUTENBERG_COMMENT_PLACEHOLDER -->' === $comment->comment_content ) {
+		$comment->comment_content = '';
+	}
+	return $comment;
+}
+add_filter( 'comments_save_pre', 'gutenberg_remove_dummy_content_from_empty_comments' );
