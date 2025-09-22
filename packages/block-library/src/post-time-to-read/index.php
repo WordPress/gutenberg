d5ce39147170b4b9b0ef650f2dd854acf7739eb6
Ben Dwyer <ben@scruffian.com>
2025-09-22 15:47:29 +0100

    Update packages/block-library/src/post-time-to-read/index.php
    Co-authored-by: Aki Hamano <54422211+t-hamano@users.noreply.github.com>

--- packages/block-library/src/post-time-to-read/index.php
+++ packages/block-library/src/post-time-to-read/index.php
@@ -26,9 +26,6 @@ function render_block_core_post_time_to_read( $attributes, $content, $block ) {
 	if ( ! empty( $attributes['displayAsRange'] ) ) {
 		// Calculate faster reading rate with 20% speed = lower minutes,
 		// and slower reading rate with 20% speed = higher minutes.
-		var_dump( $total_words / $average_reading_rate );
-		var_dump( $total_words / $average_reading_rate * 1.2 );
-		var_dump( round( $total_words / $average_reading_rate * 1.2 ) );
 		$min_minutes = max( 1, (int) round( $total_words / $average_reading_rate * 0.8 ) );
 		$max_minutes = max( 1, (int) round( $total_words / $average_reading_rate * 1.2 ) );
 		if ( $min_minutes === $max_minutes ) {
