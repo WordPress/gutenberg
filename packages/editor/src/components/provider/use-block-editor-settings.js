77daf10b903bc402f88f5936279eb3317a7e0e24
Bernie Reiter <ockham@raz.or.at>
2025-09-23 12:22:20 +0200

    Actually expose blockBindingsSupportedAttributes on the client side

--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -45,6 +45,7 @@ const BLOCK_EDITOR_SETTINGS = [
 	'__experimentalFeatures',
 	'__experimentalGlobalStylesBaseStyles',
 	'alignWide',
+	'blockBindingsSupportedAttributes',
 	'blockInspectorTabs',
 	'maxUploadFileSize',
 	'allowedMimeTypes',
