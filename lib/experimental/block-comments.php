760fa1283fabeeea9f1cae285353edf8789a6864
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-09-24 08:57:19 -0600

    Move all functionality to lib/experimental/block-comments.php

--- lib/experimental/block-comments.php
+++ lib/experimental/block-comments.php
@@ -93,3 +93,275 @@ if ( ! function_exists( 'exclude_block_comments_from_admin' ) ) {
 	}
 	add_action( 'comments_clauses', 'exclude_block_comments_from_admin', 10, 2 );
 }
+
+/**
+ * Post List filters
+ */
+
+/**
+ * Add a new column for editorial comments.
+ *
+ * @param array $columns Existing columns.
+ * @return array Modified columns.
+ */
+function gutenberg_add_editorial_comments_column( $columns ) {
+	// Add column immediately after the existing comments column.
+	$editorial_comments = '<div class="dashicons dashicons-admin-comments" title="' . esc_attr__( 'Editorial Comments', 'gutenberg' ) . '"></div>';
+	$comment_position   = array_search( 'comments', array_keys( $columns ), true );
+	$columns_before     = array_slice( $columns, 0, $comment_position + 1, true );
+	$columns_after      = array_slice( $columns, $comment_position + 1, null, true );
+	$columns            = $columns_before + array( 'editorial_comments' => $editorial_comments ) + $columns_after;
+	return $columns;
+}
+add_filter( 'manage_posts_columns', 'gutenberg_add_editorial_comments_column' );
+
+/**
+ * Render the editorial comments count for each row.
+ *
+ * Use a format similar to the core comments column. Each row shows a comment icon
+ * with the count of resolved comments. A red dot with a number in it shows the count
+ * of unresolved comments.
+ *
+ * @param string $column_name The name of the column to render.
+ * @param int    $post_id     The post ID.
+ * @return void
+ */
+function gutenberg_render_editorial_comments_column( $column_name, $post_id ) {
+	if ( 'editorial_comments' !== $column_name ) {
+		return;
+	}
+
+	$approve_count = get_comments(
+		array(
+			'post_id' => $post_id,
+			'type'    => 'block_comment',
+			'status'  => 'approve',
+			'count'   => true,
+		)
+	);
+
+	$unapprove_count = get_comments(
+		array(
+			'post_id' => $post_id,
+			'type'    => 'block_comment',
+			'status'  => 'hold',
+			'count'   => true,
+		)
+	);
+	print( '<div class="column-comments">' );
+	gutenberg_comments_bubble( $post_id, $unapprove_count, $approve_count );
+	print( '</div>' );
+}
+add_action( 'manage_posts_custom_column', 'gutenberg_render_editorial_comments_column', 10, 2 );
+
+/**
+ * Displays a comment count bubble.
+ *
+ * @param int $post_id          The post ID.
+ * @param int $pending_comments Number of pending comments.
+ */
+function gutenberg_comments_bubble( $post_id, $pending_comments, $approved_comments ) {
+	$post_object   = get_post( $post_id );
+	$edit_post_cap = $post_object ? 'edit_post' : 'edit_posts';
+
+	if ( ! current_user_can( $edit_post_cap, $post_id )
+		&& ( post_password_required( $post_id )
+			|| ! current_user_can( 'read_post', $post_id ) )
+	) {
+		// The user has no access to the post and thus cannot see the comments.
+		return false;
+	}
+
+	$approved_comments_number = number_format_i18n( $approved_comments );
+	$pending_comments_number  = number_format_i18n( $pending_comments );
+
+	$approved_only_phrase = sprintf(
+		/* translators: %s: Number of comments. */
+		_n( '%s comment', '%s comments', $approved_comments ),
+		$approved_comments_number
+	);
+
+	$approved_phrase = sprintf(
+		/* translators: %s: Number of comments. */
+		_n( '%s approved comment', '%s approved comments', $approved_comments ),
+		$approved_comments_number
+	);
+
+	$pending_phrase = sprintf(
+		/* translators: %s: Number of comments. */
+		_n( '%s pending comment', '%s pending comments', $pending_comments ),
+		$pending_comments_number
+	);
+
+	if ( ! $approved_comments && ! $pending_comments ) {
+		// No comments at all.
+		printf(
+			'<span aria-hidden="true">&#8212;</span>' .
+			'<span class="screen-reader-text">%s</span>',
+			__( 'No comments' )
+		);
+	} elseif ( $approved_comments && 'trash' === get_post_status( $post_id ) ) {
+		// Don't link the comment bubble for a trashed post.
+		printf(
+			'<span class="post-com-count post-com-count-approved">' .
+				'<span class="comment-count-approved" aria-hidden="true">%s</span>' .
+				'<span class="screen-reader-text">%s</span>' .
+			'</span>',
+			$approved_comments_number,
+			$pending_comments ? $approved_phrase : $approved_only_phrase
+		);
+	} elseif ( $approved_comments ) {
+		// Link the comment bubble to approved comments.
+		printf(
+			'<a href="%s" class="post-com-count post-com-count-approved">' .
+				'<span class="comment-count-approved" aria-hidden="true">%s</span>' .
+				'<span class="screen-reader-text">%s</span>' .
+			'</a>',
+			esc_url(
+				add_query_arg(
+					array(
+						'p'              => $post_id,
+						'comment_status' => 'approved',
+						'comment_type'   => 'block_comment',
+					),
+					admin_url( 'edit-comments.php' )
+				)
+			),
+			$approved_comments_number,
+			$pending_comments ? $approved_phrase : $approved_only_phrase
+		);
+	} else {
+		// Don't link the comment bubble when there are no approved comments.
+		printf(
+			'<span class="post-com-count post-com-count-no-comments">' .
+				'<span class="comment-count comment-count-no-comments" aria-hidden="true">%s</span>' .
+				'<span class="screen-reader-text">%s</span>' .
+			'</span>',
+			$approved_comments_number,
+			$pending_comments ?
+			/* translators: Hidden accessibility text. */
+			__( 'No approved comments' ) :
+			/* translators: Hidden accessibility text. */
+			__( 'No comments' )
+		);
+	}
+
+	if ( $pending_comments ) {
+		printf(
+			'<a href="%s" class="post-com-count post-com-count-pending">' .
+				'<span class="comment-count-pending" aria-hidden="true">%s</span>' .
+				'<span class="screen-reader-text">%s</span>' .
+			'</a>',
+			esc_url(
+				add_query_arg(
+					array(
+						'p'              => $post_id,
+						'comment_status' => 'moderated',
+						'comment_type'   => 'block_comment',
+
+					),
+					admin_url( 'edit-comments.php' )
+				)
+			),
+			$pending_comments_number,
+			$pending_phrase
+		);
+	} else {
+		printf(
+			'<span class="post-com-count post-com-count-pending post-com-count-no-pending">' .
+				'<span class="comment-count comment-count-no-pending" aria-hidden="true">%s</span>' .
+				'<span class="screen-reader-text">%s</span>' .
+			'</span>',
+			$pending_comments_number,
+			$approved_comments ?
+			/* translators: Hidden accessibility text. */
+			__( 'No pending comments' ) :
+			/* translators: Hidden accessibility text. */
+			__( 'No comments' )
+		);
+	}
+}
+
+/**
+ * Filter the editorial comment so it is sortable.
+ *
+ * @param array $columns Existing sortable columns.
+ * @return array Modified sortable columns.
+ */
+function gutenberg_filter_edit_post_sortable_columns( $columns ) {
+	$columns['editorial_comments'] = 'editorial_comments';
+	return $columns;
+}
+add_filter( 'manage_edit-post_sortable_columns', 'gutenberg_filter_edit_post_sortable_columns' );
+
+/**
+ * Filters for the comments page.
+ */
+
+/**
+ * Use the 'admin_comment_types_dropdown' filter to add Editorial Comments to the comment type dropdown.
+ */
+function gutenberg_add_editorial_comments_to_comment_type_dropdown( $types ) {
+	$types['block_comment'] = __( 'Editorial Comments', 'gutenberg' );
+	return $types;
+}
+add_filter( 'admin_comment_types_dropdown', 'gutenberg_add_editorial_comments_to_comment_type_dropdown' );
+
+/**
+ * Add a submenu item under "Comments" for "Editorial Comments". This screen shows comments with the "block_comment" type.
+ */
+function gutenberg_add_editorial_comments_submenu() {
+	add_submenu_page(
+		'edit-comments.php',
+		__( 'Editorial Comments', 'gutenberg' ),
+		__( 'Editorial Comments', 'gutenberg' ),
+		'edit_posts',
+		admin_url( 'edit-comments.php?comment_type=block_comment' ),
+		''
+	);
+}
+add_action( 'admin_menu', 'gutenberg_add_editorial_comments_submenu' );
+
+/**
+ * Use the `comment_status_links` filter to remove non-applicable options Spam and Trash.
+ *
+ * Also, rename "Pending" to "Open" and "Approved" to "Resolved" for editorial comments.
+ *
+ * @param array $status_links The existing status links.
+ * @return array The modified status links.
+ */
+function gutenberg_filter_editorial_comments_status_links( $status_links ) {
+	if ( isset( $_GET['comment_type'] ) && 'block_comment' === $_GET['comment_type'] ) {
+		unset( $status_links['spam'] );
+		unset( $status_links['trash'] );
+	}
+	if ( isset( $status_links['moderated'] ) ) {
+		$status_links['moderated'] = str_replace( __( 'Pending' ), __( 'Open', 'gutenberg' ), $status_links['moderated'] );
+	}
+	if ( isset( $status_links['approved'] ) ) {
+		$status_links['approved'] = str_replace( __( 'Approved' ), __( 'Resolved', 'gutenberg' ), $status_links['approved'] );
+	}
+	return $status_links;
+}
+add_filter( 'comment_status_links', 'gutenberg_filter_editorial_comments_status_links' );
+
+
+/**
+ * Adjust the bulk edit options on the comments screen when the comment type is block_comment.
+ *
+ * The options should be Resolve (approve), Reopen (unapprove), and Delete.
+ * @param array $bulk_actions The existing bulk actions.
+ * @return array The modified bulk actions.
+ *
+ */
+function gutenberg_filter_editorial_comments_bulk_actions( $bulk_actions ) {
+	if ( isset( $_GET['comment_type'] ) && 'block_comment' === $_GET['comment_type'] ) {
+		$bulk_actions = array(
+			'approve'   => __( 'Resolve', 'gutenberg' ),
+			'unapprove' => __( 'Reopen', 'gutenberg' ),
+			'delete'    => __( 'Delete', 'gutenberg' ),
+		);
+	}
+	return $bulk_actions;
+}
+add_filter( 'bulk_actions-edit-comments', 'gutenberg_filter_editorial_comments_bulk_actions' );
--- lib/load.php
+++ lib/load.php
@@ -47,8 +47,6 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require __DIR__ . '/compat/wordpress-6.9/term-data-block-bindings.php';
 	require __DIR__ . '/compat/wordpress-6.9/rest-api.php';
 	require __DIR__ . '/compat/wordpress-6.9/class-gutenberg-hierarchical-sort.php';
-	require __DIR__ . '/compat/wordpress-6.9/post-list.php';
-	require __DIR__ . '/compat/wordpress-6.9/editorial-comments.php';
 
 	// Plugin specific code.
 	require_once __DIR__ . '/class-wp-rest-global-styles-controller-gutenberg.php';
