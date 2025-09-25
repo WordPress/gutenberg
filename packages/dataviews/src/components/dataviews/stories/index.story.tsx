7d967d1e22598f398b36a1f2af4389ad2296aa3e
Riad Benguella <benguella@gmail.com>
2025-09-25 19:06:47 +0100

    DataViews: Expose FiltersToggled subcomponent.

--- packages/dataviews/src/components/dataviews/stories/index.story.tsx
+++ packages/dataviews/src/components/dataviews/stories/index.story.tsx
@@ -273,7 +273,7 @@ function PlanetOverview( { planets }: { planets: SpaceObject[] } ) {
 						<DataViews.FiltersToggle />
 						<DataViews.Search label={ __( 'moons by planet' ) } />
 					</HStack>
-					<DataViews.Filters />
+					<DataViews.FiltersToggled />
 				</VStack>
 
 				<VStack>
