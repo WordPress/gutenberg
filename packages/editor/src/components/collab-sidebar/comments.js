abd2a9e784ab93d6d3d724ce46964516af8d244c
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2025-09-29 23:40:04 +0900

    Block Comments: Make comment thread focusable and improve keyboard navigation (#71883)
    * Block Comments: Make commment thread focusable
    
    * Restore thread id attirbute
    
    * Remove event.stopPropagation
    
    * Close focused thread by enter key
    
    * Close focused thread by esc key
    
    * Focus current thread when the thread is resolved
    
    * Add thread comment as aria-label
    
    * Add aria-label
    
    * Fix e2e test
    
    * Improve aria-label
    
    * Use comment excerpt for aria-label
    
    * Add "Comment: " prefix to comment aria label
    
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: joedolson <joedolson@git.wordpress.org>
    Co-authored-by: adamsilverstein <adamsilverstein@git.wordpress.org>

--- packages/editor/src/components/collab-sidebar/comments.js
+++ packages/editor/src/components/collab-sidebar/comments.js
@@ -6,12 +6,13 @@ import clsx from 'clsx';
 /**
  * WordPress dependencies
  */
-import { useState, RawHTML } from '@wordpress/element';
+import { useState, RawHTML, useRef } from '@wordpress/element';
 import {
 	__experimentalHStack as HStack,
 	__experimentalVStack as VStack,
 	__experimentalConfirmDialog as ConfirmDialog,
 	Button,
+	FlexItem,
 	privateApis as componentsPrivateApis,
 } from '@wordpress/components';
 import { useDebounce } from '@wordpress/compose';
@@ -19,6 +20,7 @@ import { useDebounce } from '@wordpress/compose';
 import { published, moreVertical } from '@wordpress/icons';
 import { __, _x, sprintf, _n } from '@wordpress/i18n';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { __unstableStripHTML as stripHTML } from '@wordpress/dom';
 import {
 	store as blockEditorStore,
 	privateApis as blockEditorPrivateApis,
@@ -30,6 +32,7 @@ import {
 import { unlock } from '../../lock-unlock';
 import CommentAuthorInfo from './comment-author-info';
 import CommentForm from './comment-form';
+import { getCommentExcerpt } from './utils';
 
 const { useBlockElement } = unlock( blockEditorPrivateApis );
 const { Menu } = unlock( componentsPrivateApis );
@@ -60,7 +63,7 @@ export function Comments( {
 			? getBlockAttributes( clientId )?.metadata?.commentId
 			: null;
 	}, [] );
-	const [ focusThread = blockCommentId, setFocusThread ] = useState();
+	const [ selectedThread = blockCommentId, setSelectedThread ] = useState();
 
 	const hasThreads = Array.isArray( threads ) && threads.length > 0;
 	if ( ! hasThreads ) {
@@ -86,8 +89,8 @@ export function Comments( {
 			onAddReply={ onAddReply }
 			onCommentDelete={ onCommentDelete }
 			onEditComment={ onEditComment }
-			isFocused={ focusThread === thread.id }
-			setFocusThread={ setFocusThread }
+			isSelected={ selectedThread === thread.id }
+			setSelectedThread={ setSelectedThread }
 			setShowCommentBoard={ setShowCommentBoard }
 		/>
 	) );
@@ -98,10 +101,11 @@ function Thread( {
 	onEditComment,
 	onAddReply,
 	onCommentDelete,
-	isFocused,
-	setFocusThread,
+	isSelected,
+	setSelectedThread,
 	setShowCommentBoard,
 } ) {
+	const threadRef = useRef( null );
 	const { toggleBlockHighlight } = useDispatch( blockEditorStore );
 	const relatedBlockElement = useBlockElement( thread.blockClientId );
 	const debouncedToggleBlockHighlight = useDebounce(
@@ -119,7 +123,7 @@ function Thread( {
 
 	const handleCommentSelect = ( { id, blockClientId } ) => {
 		setShowCommentBoard( false );
-		setFocusThread( id );
+		setSelectedThread( id );
 		if ( blockClientId && relatedBlockElement ) {
 			relatedBlockElement.scrollIntoView( {
 				behavior: 'instant',
@@ -128,8 +132,12 @@ function Thread( {
 		}
 	};
 
-	const clearThreadFocus = () => {
-		setFocusThread( null );
+	const focusThread = () => {
+		threadRef.current?.focus();
+	};
+
+	const unselectThread = () => {
+		setSelectedThread( null );
 		setShowCommentBoard( false );
 	};
 
@@ -139,26 +147,68 @@ function Thread( {
 		: undefined;
 	const restReplies = !! replies.length ? replies.slice( 0, -1 ) : [];
 
+	const commentExcerpt = getCommentExcerpt(
+		stripHTML( thread.content.rendered ),
+		10
+	);
+	const ariaLabel = sprintf(
+		// translators: %s: comment excerpt
+		__( 'Comment: %s' ),
+		commentExcerpt
+	);
+
 	return (
+		// Disable reason: role="listitem" does in fact support aria-expanded.
+		// eslint-disable-next-line jsx-a11y/role-supports-aria-props
 		<VStack
 			className={ clsx( 'editor-collab-sidebar-panel__thread', {
-				'editor-collab-sidebar-panel__focus-thread': isFocused,
+				'is-selected': isSelected,
 			} ) }
-			id={ thread.id }
+			id={ `thread-${ thread.id }` }
 			spacing="2"
 			onClick={ () => handleCommentSelect( thread ) }
 			onMouseEnter={ onMouseEnter }
 			onMouseLeave={ onMouseLeave }
 			onFocus={ onMouseEnter }
 			onBlur={ onMouseLeave }
+			onKeyDown={ ( event ) => {
+				// Expand or Collapse thread.
+				if (
+					event.key === 'Enter' &&
+					event.currentTarget === event.target
+				) {
+					if ( isSelected ) {
+						unselectThread();
+					} else {
+						handleCommentSelect( thread );
+					}
+				}
+				// Collapse thread and focus the thread.
+				if ( event.key === 'Escape' ) {
+					unselectThread();
+					focusThread();
+				}
+			} }
+			tabIndex={ 0 }
+			role="listitem"
+			ref={ threadRef }
+			aria-label={ ariaLabel }
+			aria-expanded={ isSelected }
 		>
 			<CommentBoard
 				thread={ thread }
-				onEdit={ onEditComment }
+				onEdit={ ( params = {} ) => {
+					const { status } = params;
+					onEditComment( params );
+					if ( status === 'approved' ) {
+						unselectThread();
+						focusThread();
+					}
+				} }
 				onDelete={ onCommentDelete }
 				status={ thread.status }
 			/>
-			{ isFocused &&
+			{ isSelected &&
 				replies.map( ( reply ) => (
 					<VStack
 						key={ reply.id }
@@ -181,13 +231,13 @@ function Thread( {
 						/>
 					</VStack>
 				) ) }
-			{ ! isFocused && restReplies.length > 0 && (
+			{ ! isSelected && restReplies.length > 0 && (
 				<HStack className="editor-collab-sidebar-panel__more-reply-separator">
 					<Button
 						size="compact"
 						variant="tertiary"
 						className="editor-collab-sidebar-panel__more-reply-button"
-						onClick={ () => setFocusThread( thread.id ) }
+						onClick={ () => setSelectedThread( thread.id ) }
 					>
 						{ sprintf(
 							// translators: %s: number of replies.
@@ -201,7 +251,7 @@ function Thread( {
 					</Button>
 				</HStack>
 			) }
-			{ ! isFocused && lastReply && (
+			{ ! isSelected && lastReply && (
 				<CommentBoard
 					thread={ lastReply }
 					onEdit={
@@ -214,7 +264,7 @@ function Thread( {
 					}
 				/>
 			) }
-			{ isFocused && (
+			{ isSelected && (
 				<VStack
 					className="editor-collab-sidebar-panel__child-thread"
 					spacing="2"
@@ -237,8 +287,9 @@ function Thread( {
 								} );
 							} }
 							onCancel={ ( event ) => {
+								threadRef.current?.focus();
 								event.stopPropagation(); // Prevent the parent onClick from being triggered
-								clearThreadFocus();
+								unselectThread();
 							} }
 							submitButtonText={
 								'approved' === thread.status
@@ -314,8 +365,14 @@ const CommentBoard = ( { thread, onEdit, onDelete, status } ) => {
 					date={ thread?.date }
 					userId={ thread?.author }
 				/>
-				<span className="editor-collab-sidebar-panel__comment-status">
-					<HStack alignment="right" justify="flex-end" spacing="0">
+				<FlexItem
+					className="editor-collab-sidebar-panel__comment-status"
+					onClick={ ( event ) => {
+						// Prevent the thread from being selected.
+						event.stopPropagation();
+					} }
+				>
+					<HStack spacing="0">
 						{ canResolve && (
 							<Button
 								label={ _x(
@@ -350,10 +407,7 @@ const CommentBoard = ( { thread, onEdit, onDelete, status } ) => {
 								{ moreActions.map( ( action ) => (
 									<Menu.Item
 										key={ action.id }
-										onClick={ ( event ) => {
-											event.stopPropagation();
-											action.onClick();
-										} }
+										onClick={ () => action.onClick() }
 									>
 										<Menu.ItemLabel>
 											{ action.title }
@@ -363,7 +417,7 @@ const CommentBoard = ( { thread, onEdit, onDelete, status } ) => {
 							</Menu.Popover>
 						</Menu>
 					</HStack>
-				</span>
+				</FlexItem>
 			</HStack>
 			{ 'edit' === actionState ? (
 				<CommentForm
