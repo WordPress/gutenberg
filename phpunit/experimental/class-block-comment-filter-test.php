01d27079df4d7c9f168293bfb0506c9412a0efce
karthick.murugan <karthick.murugan02@gmail.com>
2025-09-29 19:37:41 +0530

    update bootstrap file

--- phpunit/experimental/class-block-comment-filter-test.php
+++ phpunit/experimental/class-block-comment-filter-test.php
@@ -5,8 +5,6 @@
  * @package Gutenberg
  */
 
-require_once dirname( __DIR__, 2 ) . '/lib/experimental/block-comments.php';
-
 /**
  * This class contains unit tests for the block comment filter functions.
  *
