ce9caf778676733e9e16c9780ea4a38eb2d8796b
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-09-30 09:40:01 -0600

    Simplify tests

--- phpunit/experimental/rest-api-comment-permissions-test.php
+++ phpunit/experimental/rest-api-comment-permissions-test.php
@@ -42,7 +42,7 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 		}
 
 		// Set the current user to the admin role to create posts and comments.
-		wp_set_current_user( self::$user_ids['administrator'] );
+		wp_set_current_user( self::$user_ids['contributor'] );
 
 		// Add some comments and block comments to a post.
 		self::$post_id = $this->factory->post->create();
@@ -78,7 +78,7 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 	 * @param string $comment type The type of comment to test.
 	 * @param bool $expected_permission The expected permission result.
 	 *
-	 * @dataProvider test_comment_read_permissions_data_provider
+	 * @dataProvider data_comment_read_permissions_data_provider
 	 */
 	public function test_comment_read_permissions( $role, $comment_type, $expected_permission ) {
 
@@ -106,7 +106,7 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 	 * Each entry is an array of ( role, comment_type, expected_permission ).
 	 * @return array[]
 	 */
-	public function test_comment_read_permissions_data_provider() {
+	public function data_comment_read_permissions_data_provider() {
 		return array(
 
 			'Administrator can see standard comments' => array( 'administrator', 'comment', true ),
@@ -126,127 +126,4 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 		);
 	}
 
-
-	/**
-	 * Test that for each user role, the permissions are correct when creating comments.
-	 *
-	 * @param string $role The user role to test.
-	 * @param string $comment type The type of comment to test.
-	 * @param bool $expected_permission The expected permission result.
-	 *
-	 * @dataProvider test_comment_write_permissions_data_provider
-	 */
-	public function test_comment_write_permissions( $role, $comment_type, $expected_permission ) {
-
-		// Set the current user.
-		wp_set_current_user( self::$user_ids[ $role ] );
-
-		// Create a new comment of type $comment_type for the test post.
-		$request = new WP_REST_Request( 'POST', '/wp/v2/comments' );
-		$params  = array(
-			'post'         => self::$post_id,
-			'content'      => 'Test comment content for ' . $role . ' as ' . $comment_type,
-			'comment_type' => $comment_type,
-		);
-		$request->add_header( 'Content-Type', 'application/json' );
-		$request->set_body( wp_json_encode( $params ) );
-		$response = rest_get_server()->dispatch( $request );
-
-		if ( $expected_permission ) {
-			// Expect a 201 Created response.
-			$this->assertEquals( 201, $response->get_status() );
-		} else {
-			$this->assertEquals( 403, $response->get_status() );
-		}
-	}
-
-	/**
-	 * Data provider for comment write permissions tests.
-	 * Each entry is an array of ( role, comment_type, expected_permission ).
-	 * @return array[]
-	 */
-	public function test_comment_write_permissions_data_provider() {
-		return array(
-			'Administrator can write standard comments' => array( 'administrator', 'comment', true ),
-			'Administrator can write Block comments'    => array( 'administrator', 'block_comment', true ),
-
-			'Editor can write standard comments'        => array( 'editor', 'comment', true ),
-			'Editor can write Block comments'           => array( 'editor', 'block_comment', true ),
-
-			'Author can write standard comments'        => array( 'author', 'comment', true ),
-			'Author can write Block comments'           => array( 'author', 'block_comment', true ),
-
-			'Contributor can write standard comments'   => array( 'contributor', 'comment', true ),
-			'Contributor can write Block comments'      => array( 'contributor', 'block_comment', true ),
-
-			'subscriber can write standard comments'    => array( 'subscriber', 'comment', true ),
-			'subscriber can write Block comments'       => array( 'subscriber', 'block_comment', false ),
-		);
-	}
-
-	/**
-	 * Test that for each user role, permissions are set correctly when moderating comments.
-	 *
-	 * @param string $role The user role to test.
-	 * @param string $comment_type The type of comment to test.
-	 * @param bool $expected_permission The expected permission result.
-	 *
-	 * @dataProvider test_comment_moderation_permissions_data_provider
-	 */
-	public function test_comment_moderation_permissions( $role, $comment_type, $expected_permission ) {
-
-		wp_set_current_user( self::$user_ids[ $role ] );
-
-		// Get an existing comment of type $comment_type for the test post.
-		$comments = get_comments(
-			array(
-				'post_id' => self::$post_id,
-				'type'    => $comment_type,
-			)
-		);
-		$this->assertNotEmpty( $comments, "No comments found of type $comment_type" );
-		$comment_id = $comments[0]->comment_ID;
-
-		// Moderate the comment by changing its approved status.
-		$request = new WP_REST_Request( 'PUT', '/wp/v2/comments/' . $comment_id );
-		$params  = array(
-			'content' => 'Updated comment content',
-			'status'  => 'approved',
-		);
-		$request->add_header( 'Content-Type', 'application/json' );
-		$request->set_body( wp_json_encode( $params ) );
-		$response = rest_get_server()->dispatch( $request );
-
-		if ( $expected_permission ) {
-			$this->assertEquals( 200, $response->get_status() );
-			$comment = $response->get_data();
-			$this->assertEquals( 'approved', $comment['status'] );
-		} else {
-			$this->assertEquals( 403, $response->get_status() );
-		}
-	}
-
-	/**
-	 * Data provider for comment moderation permissions tests.
-	 * Each entry is an array of ( role, comment_type, expected_permission ).
-	 * @return array[]
-	 */
-	public function test_comment_moderation_permissions_data_provider() {
-		return array(
-			'administrator can moderate standard comments' => array( 'administrator', 'comment', true ),
-			'administrator can moderate Block comments'    => array( 'administrator', 'block_comment', true ),
-
-			'editor can moderate standard comments'        => array( 'editor', 'comment', true ),
-			'editor can moderate Block comments'           => array( 'editor', 'block_comment', true ),
-
-			'author can moderate standard comments'        => array( 'author', 'comment', false ),
-			'author can moderate Block comments'           => array( 'author', 'block_comment', true ),
-
-			'contributor can moderate standard comments'   => array( 'contributor', 'comment', false ),
-			'contributor can moderate Block comments'      => array( 'contributor', 'block_comment', true ),
-
-			'subscriber can moderate standard comments'    => array( 'subscriber', 'comment', false ),
-			'subscriber can moderate Block comments'       => array( 'subscriber', 'block_comment', false ),
-		);
-	}
 }
