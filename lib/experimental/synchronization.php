4ff1303c4cc72d0adbf7f69ed6b9e4cb5c97277e
chriszarate <chris.zarate@automattic.com>
2025-10-01 14:53:51 -0600

    Add "collaborative-editing" post type supports

--- lib/experimental/synchronization.php
+++ lib/experimental/synchronization.php
@@ -22,3 +22,20 @@ function gutenberg_rest_api_init_collaborative_editing() {
 	wp_add_inline_script( 'wp-sync', 'window.__experimentalCollaborativeEditingSecret = "' . $collaborative_editing_secret . '";', 'before' );
 }
 add_action( 'admin_init', 'gutenberg_rest_api_init_collaborative_editing' );
+
+/**
+ * Add support for collaborative editing to some built-in post types.
+ */
+function gutenberg_add_collaborative_editing_post_type_support() {
+	$gutenberg_experiments = get_option( 'gutenberg-experiments' );
+	if ( ! $gutenberg_experiments || ! array_key_exists( 'gutenberg-sync-collaboration', $gutenberg_experiments ) ) {
+		return;
+	}
+
+	foreach ( array( 'page', 'post' ) as $post_type ) {
+		if ( post_type_exists( $post_type ) ) {
+			add_post_type_support( $post_type, 'collaborative-editing' );
+		}
+	}
+}
+add_action( 'init', 'gutenberg_add_collaborative_editing_post_type_support', 10, 0 );
