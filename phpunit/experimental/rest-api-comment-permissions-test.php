d9a0e2fac85117d120947fdff68dd66a2908ca70
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-10-01 12:02:15 -0600

    Test cleanup

--- phpunit/experimental/rest-api-comment-permissions-test.php
+++ phpunit/experimental/rest-api-comment-permissions-test.php
@@ -40,35 +40,52 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 			$user_id                 = $this->factory->user->create( array( 'role' => $role ) );
 			self::$user_ids[ $role ] = $user_id;
 		}
-
-		// Set the current user to the admin role to create posts and comments.
-		wp_set_current_user( self::$user_ids['contributor'] );
-
-		// Add some comments and block comments to a post.
-		self::$post_id = $this->factory->post->create();
-		for ( $i = 0; $i < self::$num_comments; $i++ ) {
-			$this->factory->comment->create(
-				array(
-					'comment_post_ID' => self::$post_id,
-					'comment_type'    => '',
-				)
-			);
-			$this->factory->comment->create(
-				array(
-					'comment_post_ID' => self::$post_id,
-					'comment_type'    => 'block_comment',
-				)
-			);
-		}
 	}
 
+
 	public function tear_down() {
-		// Clean up: delete the test post and comments.
-		wp_delete_post( self::$post_id, true );
 		foreach ( self::$user_ids as $user_id ) {
 			wp_delete_user( $user_id );
 		}
 		parent::tear_down();
+	}
+/**
+	 * Create a test post with standard comments and block comments.
+	 *
+	 * @return int The post ID.
+	 */
+	function create_test_post_with_block_comment() {
+		$post_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Test Post for Block Comments',
+				'post_content' => 'This is a test post to check block comment permissions.',
+				'post_status'  => 'publish',
+			)
+		);
+
+		// Add some comments and block comments to a post.
+		$post_id = $this->factory->post->create();
+		for ( $i = 0; $i < self::$num_comments; $i++ ) {
+
+			$this->factory->comment->create(
+				array(
+					'comment_post_ID'  => $post_id,
+					'comment_type'     => 'block_comment',
+					'comment_approved' => 1,
+
+				)
+			);
+			$this->factory->comment->create(
+				array(
+					'comment_post_ID'  => $post_id,
+					'comment_type'     => 'comment',
+					'comment_approved' => 1,
+
+				)
+			);
+		}
+
+		return $post_id;
 	}
 
 	/**
@@ -81,25 +98,29 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 	 * @dataProvider data_comment_read_permissions_data_provider
 	 */
 	public function test_comment_read_permissions( $role, $comment_type, $expected_permission ) {
-
-		// Set the current user.
 		wp_set_current_user( self::$user_ids[ $role ] );
+		$post_id = $this->create_test_post_with_block_comment( self::$user_ids[ $role ] );
 
 		// Use the REST API to read all comments of type $comment_type for the test post.
 		$request = new WP_REST_Request( 'GET', '/wp/v2/comments' );
-		$request->set_param( 'post', self::$post_id );
+		$request->set_param( 'post', $post_id );
 		$request->set_param( 'type', $comment_type );
 		$request->set_param( 'per_page', 100 );
-		$request->set_param( 'context', 'edit' );
-		$response = rest_do_request( $request );
+		if ( 'block_comment' === $comment_type ) {
+			$request->set_param( 'context', 'edit' );
+		}
+		$response = rest_get_server()->dispatch( $request );
 
 		if ( $expected_permission ) {
-			$this->assertEquals( 200, $response->get_status() );
+			$this->assertTrue( current_user_can( 'edit_post', $post_id ) );
 			$comments = $response->get_data();
 			$this->assertEquals( self::$num_comments, count( $comments ) );
+
 		} else {
-			$this->assertEquals( 403, $response->get_status() );
+			$this->assertFalse( current_user_can( 'edit_post', $post_id ) );
 		}
+
+		wp_delete_post( $post_id, true );
 	}
 
 	/**
@@ -119,10 +140,10 @@ class WP_Test_REST_Block_Comment_Permissions extends WP_UnitTestCase {
 			'Author can see standard comments'        => array( 'author', 'comment', true ),
 			'Author can see Block comments'           => array( 'author', 'block_comment', true ),
 
-			'Contributor can see standard comments'   => array( 'contributor', 'comment', true ),
-			'Contributor can see Block comments'      => array( 'contributor', 'block_comment', true ),
+			'Contributor can see standard comments'   => array( 'contributor', 'comment', false ),
+			'Contributor can see Block comments'      => array( 'contributor', 'block_comment', false ),
 
-			'subscriber can see standard comments'    => array( 'subscriber', 'comment', true ),
+			'subscriber can see standard comments'    => array( 'subscriber', 'comment', false ),
 			'subscriber can see Block comments'       => array( 'subscriber', 'block_comment', false ),
 		);
 	}
