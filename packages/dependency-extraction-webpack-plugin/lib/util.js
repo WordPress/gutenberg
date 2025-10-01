348321438e9115d0f2f20888766dcde88b6c66fe
chriszarate <chris.zarate@automattic.com>
2025-10-01 14:51:36 -0600

    Remove @wordpress/sync from bundled packages

--- packages/dependency-extraction-webpack-plugin/lib/util.js
+++ packages/dependency-extraction-webpack-plugin/lib/util.js
@@ -8,7 +8,6 @@ const BUNDLED_PACKAGES = [
 	'@wordpress/dataviews/wp',
 	'@wordpress/icons',
 	'@wordpress/interface',
-	'@wordpress/sync',
 	'@wordpress/undo-manager',
 	'@wordpress/upload-media',
 	'@wordpress/fields',
