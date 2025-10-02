a841d6f87d4752d72ccd347995e8b08dd83cea79
Dave Smith <getdavemail@gmail.com>
2025-10-02 13:22:25 +0100

    Initial e2e test coverage

--- test/e2e/specs/editor/blocks/navigation.spec.js
+++ test/e2e/specs/editor/blocks/navigation.spec.js
@@ -671,6 +671,85 @@ test.describe( 'Navigation block', () => {
 			} )
 		).toHaveLength( 1 );
 	} );
+
+	test.describe( 'Navigation Link Entity bindings', () => {
+		// eslint-disable-next-line no-unused-vars
+		let testPage1, testPage2, testPage3;
+
+		test.beforeEach( async ( { requestUtils } ) => {
+			testPage1 = await requestUtils.createPage( {
+				title: 'Test Page 1',
+				status: 'publish',
+			} );
+
+			testPage2 = await requestUtils.createPage( {
+				title: 'Test Page 2',
+				status: 'publish',
+			} );
+
+			testPage3 = await requestUtils.createPage( {
+				title: 'Test Page 3',
+				status: 'publish',
+			} );
+		} );
+
+		test.afterEach( async ( { requestUtils } ) => {
+			await requestUtils.deleteAllPages();
+		} );
+
+		test( 'can bind to a page', async ( {
+			editor,
+			page,
+			admin,
+			navigation,
+			requestUtils,
+			pageUtils,
+		} ) => {
+			await admin.createNewPost();
+
+			// create an empty menu for use - avoids Page List block
+			const menu = await requestUtils.createNavigationMenu( {
+				title: 'Test Menu',
+				content: '',
+			} );
+
+			await editor.insertBlock( {
+				name: 'core/navigation',
+				attributes: {
+					ref: menu.id,
+				},
+			} );
+
+			// Insert a link to a Page
+			await expect( navigation.getNavBlockInserter() ).toBeVisible();
+			await pageUtils.pressKeys( 'ArrowDown' );
+			await navigation.useBlockInserter();
+			await navigation.addPage( 'Test Page 1' );
+
+			// Select the Nav Link block we just inserted
+			// await editor.selectBlocks( navBlock );
+
+			// Check the Inspector controls for the Nav Link block
+			// to verify the Link field is:
+			// - disabled
+			// - has the correct URL matching the page URL
+			// - has the correct help text (description)
+			await editor.openDocumentSettingsSidebar();
+			const settingsControls = page
+				.getByRole( 'region', { name: 'Editor settings' } )
+				.getByRole( 'tabpanel', { name: 'Settings' } );
+
+			await expect( settingsControls ).toBeVisible();
+
+			const linkInput = settingsControls.getByRole( 'textbox', {
+				name: 'Link',
+				description: 'Synced with the selected page',
+			} );
+
+			await expect( linkInput ).toBeDisabled();
+			await expect( linkInput ).toHaveValue( testPage1.link );
+		} );
+	} );
 } );
 
 class Navigation {
@@ -758,6 +837,13 @@ class Navigation {
 		await expect( linkControlSearch ).toBeFocused();
 
 		await this.page.keyboard.type( label, { delay: 50 } );
+
+		// Wait for the search results to be visible
+		await expect(
+			this.page.getByRole( 'listbox', {
+				name: 'Search results',
+			} )
+		).toBeVisible();
 
 		await this.pageUtils.pressKeys( 'ArrowDown' );
 
