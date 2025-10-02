1a9448629409ee737cdbc61915684587865ff1a5
ntsekouras <ntsekouras@outlook.com>
2025-10-02 08:36:27 +0300

    php lint

--- packages/block-library/src/post-breadcrumbs/index.php
+++ packages/block-library/src/post-breadcrumbs/index.php
@@ -72,7 +72,7 @@ function render_block_core_post_breadcrumbs( $attributes, $content, $block ) {
 	// Build wrapper attributes.
 	$wrapper_attributes = get_block_wrapper_attributes(
 		array(
-			'style' => '--separator: "' . esc_attr( $separator ) . '";',
+			'style'      => '--separator: "' . esc_attr( $separator ) . '";',
 			'aria-label' => __( 'Breadcrumbs' ),
 		)
 	);
