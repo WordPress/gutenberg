e266b752a7ccdcb1659e66c5e12266d9d90d2076
Ella <4710635+ellatrix@users.noreply.github.com>
2025-10-08 16:55:26 +0200

    Template activation: reduce templates listed as options for post/page (#72141)
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

--- packages/edit-site/src/components/add-new-template/index.js
+++ packages/edit-site/src/components/add-new-template/index.js
@@ -211,7 +211,9 @@ function NewTemplateModal( { onClose } ) {
 					status: 'publish',
 					title,
 					// This adds a post meta field in template that is part of `is_custom` value calculation.
-					is_wp_suggestion: isWPSuggestion,
+					meta: {
+						is_wp_suggestion: isWPSuggestion,
+					},
 				},
 				{ throwOnError: true }
 			);
