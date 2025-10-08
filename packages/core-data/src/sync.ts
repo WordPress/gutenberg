82a997a5d49d6c04b2c83e4d3594cf7b90c9b831
chriszarate <chris.zarate@automattic.com>
2025-10-08 13:16:48 -0600

    Allow SyncProvider to be filterable

---
+++ packages/core-data/src/sync.ts
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+import { applyFilters } from '@wordpress/hooks';
+import { getWebRTCSyncProvider, SyncProvider } from '@wordpress/sync';
+
+declare global {
+	interface Window {
+		__experimentalEnableSync?: boolean;
+	}
+}
+
+let syncProvider: SyncProvider | null = null;
+
+/**
+ * Returns the current sync provider, filterable by external code.
+ *
+ * If no sync provider is set, it returns a fallback no-op sync provider to
+ * remove the need for defensive checks in the code that uses it.
+ *
+ * @return The current sync provider.
+ */
+export function getSyncProvider(): SyncProvider {
+	if ( syncProvider ) {
+		return syncProvider;
+	}
+
+	syncProvider = applyFilters(
+		'core.getSyncProvider',
+		null
+	) as SyncProvider | null;
+
+	// Ensure returned sync provider is an instance of SyncProvider.
+	if ( ! ( syncProvider instanceof SyncProvider ) ) {
+		syncProvider = null;
+	}
+
+	// If the filter does not produce a provider and the experimental flag is set,
+	// get the WebRTC sync provider.
+	if ( ! syncProvider && window.__experimentalEnableSync ) {
+		if ( globalThis.IS_GUTENBERG_PLUGIN ) {
+			syncProvider = getWebRTCSyncProvider();
+		}
+	}
+
+	// If no sync provider is set, use a fallback no-op sync provider.
+	if ( ! syncProvider ) {
+		syncProvider = new SyncProvider();
+	}
+
+	return syncProvider;
+}
