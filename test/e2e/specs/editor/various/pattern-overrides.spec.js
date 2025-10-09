cc55007b40b944a54aecea3c6ff1789b34b37855
tellthemachines <isabel@tellthemachines.com>
2025-10-09 17:00:24 +1100

    remove write mode tool use from e2e tests

--- test/e2e/specs/editor/various/pattern-overrides.spec.js
+++ test/e2e/specs/editor/various/pattern-overrides.spec.js
@@ -262,7 +262,6 @@ test.describe( 'Pattern Overrides', () => {
 			} );
 
 			await editor.setContent( '' );
-			await editor.switchEditorTool( 'Design' );
 
 			// Insert a `<main>` group block.
 			// In zoomed out and write mode it acts as the section root.
@@ -343,8 +342,6 @@ test.describe( 'Pattern Overrides', () => {
 			} );
 
 			await test.step( 'Zoomed in / Write mode - pattern as a section', async () => {
-				await editor.switchEditorTool( 'Write' );
-
 				// The pattern block is still editable as a section.
 				await expect( patternBlock ).not.toHaveAttribute(
 					'inert',
@@ -369,36 +366,7 @@ test.describe( 'Pattern Overrides', () => {
 				);
 			} );
 
-			await test.step( 'Zoomed out / Write mode - pattern as a section', async () => {
-				await page.getByLabel( 'Zoom Out' ).click();
-				// In zoomed out only the pattern block is editable,
-				// as in this scenario it's a section.
-				await expect( patternBlock ).not.toHaveAttribute(
-					'inert',
-					'true'
-				);
-
-				// Ensure the pattern block is selected before checking the child blocks
-				// to ensure the click-through behavior isn't interfering.
-				await editor.selectBlocks( patternBlock );
-
-				// None of the child blocks are editable in zoomed out mode.
-				await expect( blockWithOverrides ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithBindings ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithoutOverridesOrBindings ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-			} );
-
-			await test.step( 'Zoomed out / Design mode - pattern as a section', async () => {
-				await editor.switchEditorTool( 'Design' );
+			await test.step( 'Zoomed out - pattern as a section', async () => {
 				// In zoomed out only the pattern block is editable,
 				// as in this scenario it's a section.
 				await expect( patternBlock ).not.toHaveAttribute(
@@ -429,49 +397,7 @@ test.describe( 'Pattern Overrides', () => {
 			await editor.selectBlocks( patternBlock );
 			await editor.clickBlockOptionsMenuItem( 'Group' );
 
-			await test.step( 'Zoomed in / Write mode - pattern nested in a section', async () => {
-				await editor.switchEditorTool( 'Write' );
-				// The pattern block is not inert as it has editable content, but it shouldn't be selectable.
-				// TODO: find a way to test that the block is not selectable.
-				await expect( patternBlock ).not.toHaveAttribute(
-					'inert',
-					'true'
-				);
-				// Child blocks of the pattern are editable as normal.
-				await expect( blockWithOverrides ).not.toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithBindings ).not.toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithoutOverridesOrBindings ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-			} );
-
-			await test.step( 'Zoomed out / Write mode - pattern nested in a section', async () => {
-				await page.getByLabel( 'Zoom Out' ).click();
-				// None of the pattern is editable in zoomed out when nested in a section.
-				await expect( patternBlock ).toHaveAttribute( 'inert', 'true' );
-				await expect( blockWithOverrides ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithBindings ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-				await expect( blockWithoutOverridesOrBindings ).toHaveAttribute(
-					'inert',
-					'true'
-				);
-			} );
-
 			await test.step( 'Zoomed out / Design mode - pattern nested in a section', async () => {
-				await editor.switchEditorTool( 'Design' );
 				// None of the pattern is editable in zoomed out when nested in a section.
 				await expect( patternBlock ).toHaveAttribute( 'inert', 'true' );
 				await expect( blockWithOverrides ).toHaveAttribute(
