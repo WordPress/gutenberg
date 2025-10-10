ec12d5b016af3886cf6d4065ef5902c56296ef7d
ankitsharma38 <ankitsharma7805@gmail.com>
2025-10-11 03:51:58 +0530

    Integrate word wrap toggle and copy functionality into code block editor

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -4,6 +4,12 @@
 import { __ } from '@wordpress/i18n';
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 import { createBlock, getDefaultBlockName } from '@wordpress/blocks';
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { CodeCopyButton } from '@wordpress/components';
 
 export default function CodeEdit( {
 	attributes,
@@ -12,24 +18,51 @@ export default function CodeEdit( {
 	insertBlocksAfter,
 	mergeBlocks,
 } ) {
-	const blockProps = useBlockProps();
+	const [ isWordWrapped, setIsWordWrapped ] = useState( false );
+	const blockProps = useBlockProps( {
+		style: {
+			position: 'relative',
+		},
+	} );
+
+	const preStyle = {
+		whiteSpace: isWordWrapped ? 'pre-wrap' : 'pre',
+		overflowX: isWordWrapped ? 'visible' : 'auto',
+	};
+
 	return (
-		<pre { ...blockProps }>
-			<RichText
-				tagName="code"
-				identifier="content"
-				value={ attributes.content }
-				onChange={ ( content ) => setAttributes( { content } ) }
-				onRemove={ onRemove }
-				onMerge={ mergeBlocks }
-				placeholder={ __( 'Write code…' ) }
-				aria-label={ __( 'Code' ) }
-				preserveWhiteSpace
-				__unstablePastePlainText
-				__unstableOnSplitAtDoubleLineEnd={ () =>
-					insertBlocksAfter( createBlock( getDefaultBlockName() ) )
-				}
-			/>
-		</pre>
+		<div { ...blockProps }>
+			<div
+				style={ {
+					position: 'absolute',
+					top: '8px',
+					right: '8px',
+					zIndex: 1,
+				} }
+			>
+				<CodeCopyButton
+					text={ attributes.content || '' }
+					onWordWrapToggle={ setIsWordWrapped }
+					isWordWrapped={ isWordWrapped }
+				/>
+			</div>
+			<pre style={ preStyle }>
+				<RichText
+					tagName="code"
+					identifier="content"
+					value={ attributes.content }
+					onChange={ ( content ) => setAttributes( { content } ) }
+					onRemove={ onRemove }
+					onMerge={ mergeBlocks }
+					placeholder={ __( 'Write code…' ) }
+					aria-label={ __( 'Code' ) }
+					preserveWhiteSpace
+					__unstablePastePlainText
+					__unstableOnSplitAtDoubleLineEnd={ () =>
+						insertBlocksAfter( createBlock( getDefaultBlockName() ) )
+					}
+				/>
+			</pre>
+		</div>
 	);
 }
