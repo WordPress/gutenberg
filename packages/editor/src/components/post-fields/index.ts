f180ae8110b542f9aa30b555600d61677b189fa6
Tung Du <dinhtungdu@gmail.com>
2025-10-12 13:19:11 +0700

    refactor: use async elements for author field

--- packages/editor/src/components/post-fields/index.ts
+++ packages/editor/src/components/post-fields/index.ts
@@ -1,8 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { useEffect, useMemo } from '@wordpress/element';
-import { useEntityRecords } from '@wordpress/core-data';
+import { useEffect } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 import type { Field } from '@wordpress/dataviews';
 import type { BasePostWithEmbeddedAuthor } from '@wordpress/fields';
@@ -14,13 +13,7 @@ import { unlock } from '../../lock-unlock';
 import { store as editorStore } from '../../store';
 
 interface UsePostFieldsReturn {
-	isLoading: boolean;
 	fields: Field< BasePostWithEmbeddedAuthor >[];
-}
-
-interface Author {
-	id: number;
-	name: string;
 }
 
 function usePostFields( {
@@ -33,41 +26,17 @@ function usePostFields( {
 		registerPostTypeSchema( postType );
 	}, [ registerPostTypeSchema, postType ] );
 
-	const { defaultFields } = useSelect(
+	const { fields } = useSelect(
 		( select ) => {
 			const { getEntityFields } = unlock( select( editorStore ) );
 			return {
-				defaultFields: getEntityFields( 'postType', postType ),
+				fields: getEntityFields( 'postType', postType ),
 			};
 		},
 		[ postType ]
 	);
 
-	const { records: authors, isResolving: isLoadingAuthors } =
-		useEntityRecords< Author >( 'root', 'user', { per_page: -1 } );
-
-	const fields = useMemo(
-		() =>
-			defaultFields.map(
-				( field: Field< BasePostWithEmbeddedAuthor > ) => {
-					if ( field.id === 'author' ) {
-						return {
-							...field,
-							elements: authors?.map( ( { id, name } ) => ( {
-								value: id,
-								label: name,
-							} ) ),
-						};
-					}
-
-					return field;
-				}
-			) as Field< BasePostWithEmbeddedAuthor >[],
-		[ authors, defaultFields ]
-	);
-
 	return {
-		isLoading: isLoadingAuthors,
 		fields,
 	};
 }
