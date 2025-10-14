29baecaefdfb15e476079fedc7463bacc27d12b8
Dave Smith <getdavemail@gmail.com>
2025-10-14 09:35:08 +0100

    Simplify test plugin

--- packages/e2e-tests/plugins/gutenberg-test-pretty-permalinks.php
+++ packages/e2e-tests/plugins/gutenberg-test-pretty-permalinks.php
@@ -12,76 +12,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * E2E Test Pretty Permalinks Plugin
+ * Plugin activation - enable pretty permalinks
  */
-class E2E_Pretty_Permalinks {
+function gutenberg_test_pretty_permalinks_activate() {
+	// Enable pretty permalinks
+	update_option( 'permalink_structure', '/%postname%/' );
 
-	/**
-	 * Store the original permalink structure
-	 */
-	private $original_permalink_structure;
-
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'enable_pretty_permalinks' ) );
-		register_activation_hook( __FILE__, array( $this, 'activate' ) );
-		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-	}
-
-	/**
-	 * Plugin activation - store original permalink structure and enable pretty permalinks
-	 */
-	public function activate() {
-		// Store the current permalink structure
-		$this->original_permalink_structure = get_option( 'permalink_structure', '' );
-
-		// Enable pretty permalinks
-		update_option( 'permalink_structure', '/%postname%/' );
-
-		// Flush rewrite rules
-		global $wp_rewrite;
-		$wp_rewrite->set_permalink_structure( '/%postname%/' );
-		$wp_rewrite->flush_rules();
-
-		// Store the original structure for later restoration
-		update_option( 'e2e_original_permalink_structure', $this->original_permalink_structure );
-	}
-
-	/**
-	 * Plugin deactivation - restore original permalink structure
-	 */
-	public function deactivate() {
-		// Get the original permalink structure
-		$original_structure = get_option( 'e2e_original_permalink_structure', '' );
-
-		// Restore the original permalink structure
-		update_option( 'permalink_structure', $original_structure );
-
-		// Flush rewrite rules
-		global $wp_rewrite;
-		$wp_rewrite->set_permalink_structure( $original_structure );
-		$wp_rewrite->flush_rules();
-
-		// Clean up the stored original structure
-		delete_option( 'e2e_original_permalink_structure' );
-	}
-
-	/**
-	 * Ensure pretty permalinks are enabled (in case of any issues)
-	 */
-	public function enable_pretty_permalinks() {
-		// Double-check that pretty permalinks are enabled
-		$current_structure = get_option( 'permalink_structure', '' );
-		if ( $current_structure !== '/%postname%/' ) {
-			update_option( 'permalink_structure', '/%postname%/' );
-			global $wp_rewrite;
-			$wp_rewrite->set_permalink_structure( '/%postname%/' );
-			$wp_rewrite->flush_rules();
-		}
-	}
+	// Flush rewrite rules
+	global $wp_rewrite;
+	$wp_rewrite->set_permalink_structure( '/%postname%/' );
+	$wp_rewrite->flush_rules();
 }
+register_activation_hook( __FILE__, 'gutenberg_test_pretty_permalinks_activate' );
 
-// Initialize the plugin
-new E2E_Pretty_Permalinks();
+/**
+ * Plugin deactivation - disable pretty permalinks
+ */
+function gutenberg_test_pretty_permalinks_deactivate() {
+	// Disable pretty permalinks (set to default/empty)
+	update_option( 'permalink_structure', '' );
+
+	// Flush rewrite rules
+	global $wp_rewrite;
+	$wp_rewrite->set_permalink_structure( '' );
+	$wp_rewrite->flush_rules();
+}
+register_deactivation_hook( __FILE__, 'gutenberg_test_pretty_permalinks_deactivate' );
