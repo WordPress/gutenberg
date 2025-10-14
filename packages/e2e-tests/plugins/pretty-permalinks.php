649a015f0a82fbf47f6c423b5db0b1509fe6bbb5
Jerry Jones <jones.jeremydavid@gmail.com>
2025-10-14 15:15:10 -0500

    Change permalink update method

--- packages/e2e-tests/plugins/pretty-permalinks.php
+++ packages/e2e-tests/plugins/pretty-permalinks.php
@@ -13,23 +13,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Enable pretty permalinks for E2E tests
+ * Enable pretty permalinks when plugin loads.
+ * We use 'init' instead of activation hooks because activation hooks
+ * don't fire reliably when activating via REST API (which e2e tests use).
  */
-function gutenberg_e2e_enable_pretty_permalinks() {
-	// Flush rewrite rules with hard flush to create .htaccess
-	global $wp_rewrite;
-	$wp_rewrite->set_permalink_structure( '/%postname%/' );
-	$wp_rewrite->flush_rules( true );
+function gutenberg_e2e_setup_pretty_permalinks() {
+	$current_structure = get_option( 'permalink_structure' );
+
+	// Only set if not already set to avoid unnecessary writes
+	if ( $current_structure !== '/%postname%/' ) {
+		global $wp_rewrite;
+		$wp_rewrite->set_permalink_structure( '/%postname%/' );
+		$wp_rewrite->flush_rules( true ); // Hard flush to create .htaccess
+	}
 }
-register_activation_hook( __FILE__, 'gutenberg_e2e_enable_pretty_permalinks' );
+add_action( 'init', 'gutenberg_e2e_setup_pretty_permalinks', 1 );
 
 /**
- * Disable pretty permalinks (reset to default)
+ * Restore plain permalinks when plugin is deactivated.
+ * Deactivation hooks do work via REST API.
  */
-function gutenberg_e2e_disable_pretty_permalinks() {
-	// Flush rewrite rules with hard flush to update .htaccess
+function gutenberg_e2e_restore_plain_permalinks() {
 	global $wp_rewrite;
 	$wp_rewrite->set_permalink_structure( '' );
-	$wp_rewrite->flush_rules( true );
+	$wp_rewrite->flush_rules( true ); // Hard flush to update .htaccess
 }
-register_deactivation_hook( __FILE__, 'gutenberg_e2e_disable_pretty_permalinks' );
+register_deactivation_hook( __FILE__, 'gutenberg_e2e_restore_plain_permalinks' );
