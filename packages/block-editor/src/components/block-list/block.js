d68f8fe62af68bfd66a589376af5687292cef056
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-10-16 18:49:47 +0200

    Revert "Block Bindings: Move bindable attributes to privateContext. (#72351)"
    This reverts commit 3ca7316bfe87eab3d58484d7a20a851c282bf6ee.

--- packages/block-editor/src/components/block-list/block.js
+++ packages/block-editor/src/components/block-list/block.js
@@ -606,15 +606,7 @@ function BlockListBlockProvider( props ) {
 			const attributes = getBlockAttributes( clientId );
 			const { name: blockName, isValid } = blockWithoutAttributes;
 			const blockType = getBlockType( blockName );
-			const {
-				supportsLayout,
-				isPreviewMode,
-				__experimentalBlockBindingsSupportedAttributes,
-			} = getSettings();
-
-			const bindableAttributes =
-				__experimentalBlockBindingsSupportedAttributes?.[ blockName ];
-
+			const { supportsLayout, isPreviewMode } = getSettings();
 			const hasLightBlockWrapper = blockType?.apiVersion > 1;
 			const previewContext = {
 				isPreviewMode,
@@ -718,7 +710,6 @@ function BlockListBlockProvider( props ) {
 					? blocksWithSameName[ 0 ]
 					: false,
 				isBlockHidden: _isBlockHidden( clientId ),
-				bindableAttributes,
 			};
 		},
 		[ clientId, rootClientId ]
@@ -762,7 +753,6 @@ function BlockListBlockProvider( props ) {
 		defaultClassName,
 		originalBlockClientId,
 		isBlockHidden,
-		bindableAttributes,
 	} = selectedProps;
 
 	// Users of the editor.BlockListBlock filter used to be able to
@@ -812,7 +802,6 @@ function BlockListBlockProvider( props ) {
 		themeSupportsLayout,
 		canMove,
 		isBlockHidden,
-		bindableAttributes,
 	};
 
 	if (
