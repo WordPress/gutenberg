5f1d67b412f3df5c1f9b8707abb718de6636f5b0
tellthemachines <isabel@tellthemachines.com>
2025-10-17 13:17:21 +1100

    ignore the stupid error

--- packages/editor/src/dataviews/fields/content-preview/content-preview-view.tsx
+++ packages/editor/src/dataviews/fields/content-preview/content-preview-view.tsx
@@ -3,10 +3,9 @@
  */
 import { __ } from '@wordpress/i18n';
 import {
-	// @ts-ignore
 	BlockPreview,
-	// @ts-ignore
 	privateApis as blockEditorPrivateApis,
+	// @ts-ignore
 } from '@wordpress/block-editor';
 import type { BasePost } from '@wordpress/fields';
 import { useSelect } from '@wordpress/data';
