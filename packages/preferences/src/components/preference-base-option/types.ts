f3887d76aeaced77590861c89210406da5f18ea2
Joshua Lipstone <joshualip@plaudit.com>
2025-10-17 17:35:37 -0500

    Removed the explicit ReactNode imports

--- packages/preferences/src/components/preference-base-option/types.ts
+++ packages/preferences/src/components/preference-base-option/types.ts
@@ -3,13 +3,8 @@
  */
 import type { ToggleControl } from '@wordpress/components';
 
-/**
- * External dependencies
- */
-import type { ReactNode } from 'react';
-
 type ToggleControlProps = Parameters< typeof ToggleControl >[ 0 ];
 export type BaseOptionProps = Pick<
 	ToggleControlProps,
 	'help' | 'label' | 'onChange'
-> & { isChecked: ToggleControlProps[ 'checked' ]; children: ReactNode };
+> & { isChecked: ToggleControlProps[ 'checked' ]; children: React.ReactNode };
--- packages/preferences/src/components/preferences-modal/types.ts
+++ packages/preferences/src/components/preferences-modal/types.ts
@@ -2,14 +2,10 @@
  * WordPress dependencies
  */
 import type { Modal } from '@wordpress/components';
-/**
- * External dependencies
- */
-import type { ReactNode } from 'react';
 
 type ModalProps = Parameters< typeof Modal >[ 0 ];
 
 export type PreferencesModalProps = {
 	closeModal: ModalProps[ 'onRequestClose' ];
-	children: ReactNode;
+	children: React.ReactNode;
 };
