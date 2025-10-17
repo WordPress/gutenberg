f0372081cca4be172df2935b7aae953e189a93c1
Bernie Reiter <ockham@raz.or.at>
2025-10-17 14:49:02 +0200

    Prettier

--- packages/editor/src/hooks/pattern-overrides.js
+++ packages/editor/src/hooks/pattern-overrides.js
@@ -40,7 +40,9 @@ const withPatternOverrideControls = createHigherOrderComponent(
 			( select ) => {
 				const { __experimentalBlockBindingsSupportedAttributes } =
 					select( blockEditorStore ).getSettings();
-				return !! __experimentalBlockBindingsSupportedAttributes?.[ props.name ];
+				return !! __experimentalBlockBindingsSupportedAttributes?.[
+					props.name
+				];
 			},
 			[ props.name ]
 		);
