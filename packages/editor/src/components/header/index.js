e8c0bddd04cc43a720413a14846a172194b6c062
chriszarate <chris.zarate@automattic.com>
2025-10-17 12:44:48 -0600

    Real-time collaboration: [no-pr] Add <EditorsPresence> and <BlockCanvasCover> slot fills
    ---------
    
    Co-authored-by: Chris Zarate <chris@zarate.org>

--- packages/editor/src/components/header/index.js
+++ packages/editor/src/components/header/index.js
@@ -31,6 +31,7 @@ import {
 	NAVIGATION_POST_TYPE,
 } from '../../store/constants';
 import { unlock } from '../../lock-unlock';
+import { Slot as EditorPresenceSlot } from '../editor-presence';
 
 const toolbarVariations = {
 	distractionFreeDisabled: { y: '-50px' },
@@ -159,6 +160,15 @@ function Header( {
 					variants={ toolbarVariations }
 					transition={ { type: 'tween' } }
 				>
+					<EditorPresenceSlot>
+						{ ( fills ) =>
+							fills?.length ? (
+								<div className="editor-header__editor-presence">
+									{ fills }
+								</div>
+							) : null
+						}
+					</EditorPresenceSlot>
 					<DocumentBar />
 				</motion.div>
 			) }
