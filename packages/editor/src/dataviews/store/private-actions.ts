2e2b758cd5f073d8a2110e80ad5481d0c303d64b
ella <ella@automattic.com>
2025-10-21 11:52:38 +0200

    Flip

--- packages/editor/src/dataviews/store/private-actions.ts
+++ packages/editor/src/dataviews/store/private-actions.ts
@@ -141,7 +141,7 @@ export const registerPostTypeSchema =
 			duplicatePost;
 
 		// @ts-ignore
-		if ( globalThis.IS_GUTENBERG_PLUGIN ) {
+		if ( ! globalThis.IS_GUTENBERG_PLUGIN ) {
 			// Outside Gutenberg, disable duplication except for wp_template.
 			if ( 'wp_template' !== postTypeConfig.slug ) {
 				canDuplicate = undefined;
