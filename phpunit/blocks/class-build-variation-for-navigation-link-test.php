c2140a77a4fbdc992d1671a3950d7560ff889a1f
Dave Smith <getdavemail@gmail.com>
2025-10-21 12:04:56 +0100

    Simple initial CPT test

--- phpunit/blocks/class-build-variation-for-navigation-link-test.php
+++ phpunit/blocks/class-build-variation-for-navigation-link-test.php
@@ -30,7 +30,7 @@ class Class_Build_Variation_For_Navigation_Link_Test extends WP_UnitTestCase {
 
 		$variation = gutenberg_build_variation_for_navigation_link( $post_type, 'post-type' );
 
-		// Verify it returns an array
+		// Verify it returns an arraygs
 		$this->assertIsArray( $variation, 'Function should return an array' );
 
 		// Verify it has the expected keys
@@ -46,27 +46,21 @@ class Class_Build_Variation_For_Navigation_Link_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that the function works with a taxonomy.
+	 * Test that a custom post type variation has correct title and description format.
 	 */
-	public function test_function_works_with_taxonomy() {
-		// Test with a real built-in taxonomy
-		$taxonomy = get_taxonomy( 'category' );
-		$this->assertNotNull( $taxonomy, 'Taxonomy object should exist' );
+	public function test_custom_post_type_variation_format() {
+		// Create a mock custom post type object
+		$post_type = new stdClass();
+		$post_type->name = 'product';
+		$post_type->labels = new stdClass();
+		$post_type->labels->singular_name = 'Product';
 
-		$variation = gutenberg_build_variation_for_navigation_link( $taxonomy, 'taxonomy' );
+		$variation = gutenberg_build_variation_for_navigation_link( $post_type, 'post-type' );
 
-		// Verify it returns an array
-		$this->assertIsArray( $variation, 'Function should return an array' );
+		// Verify the title format
+		$this->assertEquals( 'Product Link', $variation['title'], 'Custom post type should have appropriately named title' );
 
-		// Verify it has the expected keys
-		$this->assertArrayHasKey( 'name', $variation, 'Variation should have name key' );
-		$this->assertArrayHasKey( 'title', $variation, 'Variation should have title key' );
-		$this->assertArrayHasKey( 'description', $variation, 'Variation should have description key' );
-		$this->assertArrayHasKey( 'attributes', $variation, 'Variation should have attributes key' );
-
-		// Verify attributes is an array with expected keys
-		$this->assertIsArray( $variation['attributes'], 'Attributes should be an array' );
-		$this->assertArrayHasKey( 'type', $variation['attributes'], 'Attributes should have type key' );
-		$this->assertArrayHasKey( 'kind', $variation['attributes'], 'Attributes should have kind key' );
+		// Verify the description format
+		$this->assertEquals( 'A link to a product', $variation['description'], 'Custom post type should have clear, unambiguous description' );
 	}
 }
