cf49dc8886b58c29c3be55b1f448bbafb58fb05e
Max Schmeling <max@maxschmeling.com>
2025-10-23 13:07:03 -0600

    Real-time collaboration: [no-pr] Only render presence UI container one time
    * Moved <EditorsPresence> component to editor-presence
    
    * Renamed <EditorsPresence> to <EditorPresence>
    
    * Convert <EditorPresence> file to tsx
    
    * Update EditorPresence Slot to only render one container
    
    * Add basic docs for <EditorPresence>
    
    * Update packages/editor/src/components/editor-presence/index.tsx
    
    Co-authored-by: Chris Zarate <chris@zarate.org>
    
    * Update docs for <EditorPresence>
    
    * Make <EditorPresence> private
    
    ---------
    
    Co-authored-by: Chris Zarate <chris@zarate.org>

--- packages/editor/src/components/index.js
+++ packages/editor/src/components/index.js
@@ -16,7 +16,6 @@ export { default as EditorKeyboardShortcutsRegister } from './global-keyboard-sh
 export { default as EditorHistoryRedo } from './editor-history/redo';
 export { default as EditorHistoryUndo } from './editor-history/undo';
 export { default as EditorNotices } from './editor-notices';
-export { EditorsPresence } from './editors-presence';
 export { default as EditorSnackbars } from './editor-snackbars';
 export { default as EntitiesSavedStates } from './entities-saved-states';
 export { useIsDirty as useEntitiesSavedStatesIsDirty } from './entities-saved-states/hooks/use-is-dirty';
