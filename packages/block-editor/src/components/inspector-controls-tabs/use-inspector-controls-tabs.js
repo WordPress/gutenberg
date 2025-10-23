e864e2532ba700216e70334a086e84135834033c
Ben Dwyer <ben@scruffian.com>
2025-10-23 09:32:01 +0100

    always show the styles tab if the experiment is enabled

--- packages/block-editor/src/components/inspector-controls-tabs/use-inspector-controls-tabs.js
+++ packages/block-editor/src/components/inspector-controls-tabs/use-inspector-controls-tabs.js
@@ -99,7 +99,11 @@ export default function useInspectorControlsTabs(
 		tabs.push( TAB_SETTINGS );
 	}
 
-	if ( hasBlockStyles || hasStyleFills ) {
+	if (
+		hasBlockStyles ||
+		hasStyleFills ||
+		window?.__experimentalContentOnlyPatternInsertion
+	) {
 		tabs.push( TAB_STYLES );
 	}
 
