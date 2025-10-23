a989d74290d16917c03215694d7c2f180e4242d3
Max Schmeling <max@maxschmeling.com>
2025-10-23 13:07:03 -0600

    Real-time collaboration: [pr-72495] Only render presence UI container one time (#72495)
    * Moved <EditorsPresence> component to editor-presence
    
    * Renamed <EditorsPresence> to <EditorPresence>
    
    * Convert <EditorPresence> file to tsx
    
    * Update EditorPresence Slot to only render one container
    
    * Add basic docs for <EditorPresence>
    
    * Update packages/editor/src/components/editor-presence/index.tsx
    
    Co-authored-by: Chris Zarate <chris@zarate.org>
    
    * Update docs for <EditorPresence>
    
    * Make <EditorPresence> private
    
    ---------
    
    Co-authored-by: Chris Zarate <chris@zarate.org>

---
+++ packages/editor/src/components/editor-presence/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * WordPress dependencies
+ */
+import { createSlotFill } from '@wordpress/components';
+
+const { Fill, Slot } = createSlotFill( 'EditorPresence' );
+
+/**
+ * Renders the SlotFill for editor presence.
+ */
+export const EditorPresence = Fill;
+
+export { Slot };
