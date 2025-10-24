cbcdc25a2d3d8dc07730a5e02186ef52ee8fa15c
Ramon <ramonjd@users.noreply.github.com>
2025-10-24 18:05:46 +1100

    Add 'type' property to activeField as boolean in page templates (#72278) (#72648)
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: levinbaria <levinbaria@git.wordpress.org>
    Co-authored-by: jennydupuy <jdy68@git.wordpress.org>

--- packages/edit-site/src/components/page-templates/fields.js
+++ packages/edit-site/src/components/page-templates/fields.js
@@ -150,6 +150,7 @@ export const authorField = {
 export const activeField = {
 	label: __( 'Status' ),
 	id: 'active',
+	type: 'boolean',
 	getValue: ( { item } ) => item._isActive,
 	render: function Render( { item } ) {
 		if ( item._isCustom ) {
