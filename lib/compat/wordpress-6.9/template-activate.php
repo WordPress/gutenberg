2075f35cebeef1ea22bc9dcd0c248c53df7aa5b0
ella <ella@automattic.com>
2025-10-27 21:16:14 +0100

    Template activation: fix undefined array key PHP warning

--- lib/compat/wordpress-6.9/template-activate.php
+++ lib/compat/wordpress-6.9/template-activate.php
@@ -257,8 +257,8 @@ function gutenberg_resolve_block_template( $template_type, $template_hierarchy, 
 	// START CORE MODIFICATIONS //
 	//////////////////////////////
 
-	$object            = get_queried_object();
-	$specific_template = $object ? get_page_template_slug( $object ) : null;
+	$object_id         = get_queried_object_id();
+	$specific_template = $object_id && get_post( $object_id ) ? get_page_template_slug( $object_id ) : null;
 	$active_templates  = (array) get_option( 'active_templates', array() );
 
 	// We expect one template for each slug. Use the active template if it is
@@ -314,7 +314,7 @@ function gutenberg_resolve_block_template( $template_type, $template_hierarchy, 
 	);
 	$templates = array_merge( $templates, gutenberg_get_registered_block_templates( $query ) );
 
-	if ( $specific_template ) {
+	if ( $specific_template && in_array( $specific_template, $remaining_slugs, true ) ) {
 		$templates = array_merge( $templates, get_block_templates( array( 'slug__in' => array( $specific_template ) ) ) );
 	}
 
