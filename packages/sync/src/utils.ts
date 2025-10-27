f190bc6393b6c3db85c7109a6118721df322b3c1
Chris Zarate <chris.zarate@automattic.com>
2025-10-27 12:57:40 -0600

    Real-time collaboration: [pr-72373][merged-trunk] Implement CRDT persistence for collaborative editing
    * Implement CRDT persistence
    
    * Add tests for SyncManager and CRDT persistence

--- packages/sync/src/utils.ts
+++ packages/sync/src/utils.ts
@@ -2,15 +2,18 @@
  * External dependencies
  */
 import * as Y from 'yjs';
+import * as buffer from 'lib0/buffer';
 
 /**
  * Internal dependencies
  */
 import {
+	CRDT_DOC_META_PERSISTENCE_KEY,
 	CRDT_DOC_VERSION,
 	CRDT_STATE_MAP_KEY,
 	CRDT_STATE_VERSION_KEY,
 } from './config';
+import type { CRDTDoc } from './types';
 
 export function createYjsDoc( documentMeta: Record< string, unknown > ): Y.Doc {
 	// Meta is not synced and does not get persisted with the document.
@@ -24,4 +27,38 @@ export function createYjsDoc( documentMeta: Record< string, unknown > ): Y.Doc {
 	stateMap.set( CRDT_STATE_VERSION_KEY, CRDT_DOC_VERSION );
 
 	return ydoc;
+}
+
+export function serializeCrdtDoc( crdtDoc: CRDTDoc ): string {
+	return JSON.stringify( {
+		document: buffer.toBase64( Y.encodeStateAsUpdateV2( crdtDoc ) ),
+	} );
+}
+
+export function deserializeCrdtDoc(
+	serializedCrdtDoc: string
+): CRDTDoc | null {
+	try {
+		const { document } = JSON.parse( serializedCrdtDoc );
+
+		// Mark this document as from persistence.
+		const docMetaMap = new Map< string, boolean >();
+		docMetaMap.set( CRDT_DOC_META_PERSISTENCE_KEY, true );
+
+		// Apply the document as an update against a new (temporary) Y.Doc.
+		const ydoc = createYjsDoc( { meta: docMetaMap } );
+		const yupdate = buffer.fromBase64( document );
+		Y.applyUpdateV2( ydoc, yupdate );
+
+		// Overwrite the client ID (which is from a previous session) with a random
+		// client ID. Deserialized documents should not be used directly. Instead,
+		// their state should be applied to another in-use document.
+		//
+		// eslint-disable-next-line no-restricted-syntax
+		ydoc.clientID = Math.floor( Math.random() * 1000000000 );
+
+		return ydoc;
+	} catch ( e ) {
+		return null;
+	}
 }
