ca04f4bcf7b4f2e94fc9dbb2542932e9ea3c825a
Ahmed <ahmed.el.azzabi@automattic.com>
2025-10-28 11:29:42 +0100

    Revert "Fix default `isOpened` value for card header"
    This reverts commit 492f4e0b197b5e45e4868954be8937c696b60451.

--- packages/dataviews/src/dataform-layouts/card/index.tsx
+++ packages/dataviews/src/dataform-layouts/card/index.tsx
@@ -50,7 +50,7 @@ const NonCollapsibleCardHeader = ( {
 );
 
 export function useCardHeader( layout: NormalizedCardLayout ) {
-	const { isOpened = true, isCollapsible } = layout;
+	const { isOpened, isCollapsible } = layout;
 	const [ isOpen, setIsOpen ] = useState( isOpened );
 
 	const toggle = useCallback( () => {
