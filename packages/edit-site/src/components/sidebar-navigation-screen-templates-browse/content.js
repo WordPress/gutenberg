feb1b27e66bcc6a16201cb942660a15fccad5748
ella <ella@automattic.com>
2025-10-29 09:53:33 +0100

    Template activation: update sidebar icons

--- packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/content.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-templates-browse/content.js
@@ -13,7 +13,7 @@ import { addQueryArgs } from '@wordpress/url';
  */
 import SidebarNavigationItem from '../sidebar-navigation-item';
 import { useAddedBy } from '../page-templates/hooks';
-import { layout } from '@wordpress/icons';
+import { addTemplate, published } from '@wordpress/icons';
 import { unlock } from '../../lock-unlock';
 
 const { useLocation } = unlock( routerPrivateApis );
@@ -62,14 +62,14 @@ export default function DataviewsTemplatesSidebarContent() {
 		<ItemGroup className="edit-site-sidebar-navigation-screen-templates-browse">
 			<SidebarNavigationItem
 				to="/template"
-				icon={ layout }
+				icon={ published }
 				aria-current={ activeView === 'active' }
 			>
 				{ __( 'Active templates' ) }
 			</SidebarNavigationItem>
 			<SidebarNavigationItem
 				to={ addQueryArgs( '/template', { activeView: 'user' } ) }
-				icon={ layout }
+				icon={ addTemplate }
 				aria-current={ activeView === 'user' }
 			>
 				{
