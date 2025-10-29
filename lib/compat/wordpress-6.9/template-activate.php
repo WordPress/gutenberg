e5d16a0452e9193504ac6b746ac39c143a6b5839
ella <ella@automattic.com>
2025-10-29 11:44:53 +0100

    Adjust priority

--- lib/compat/wordpress-6.9/template-activate.php
+++ lib/compat/wordpress-6.9/template-activate.php
@@ -480,8 +480,15 @@ function gutenberg_locate_block_template( $template, $type, array $templates ) {
 // We need to set the theme for the template when it's created. See:
 // https://github.com/WordPress/wordpress-develop/blob/b2c8d8d2c8754cab5286b06efb4c11e2b6aa92d5/src/wp-includes/rest-api/endpoints/class-wp-rest-templates-controller.php#L571-L578
 // Priority 9 so it runs before default hooks like
-// `inject_ignored_hooked_blocks_metadata_attributes`.
-add_action( 'rest_pre_insert_wp_template', 'gutenberg_set_active_template_theme', 9, 2 );
+// `inject_ignored_hooked_blocks_metadata_attributes`. Unfortunately, core is
+// also using priority 9, so we have to delay adding the filter so it runs after
+// core.
+add_action(
+	'init',
+	function () {
+		add_action( 'rest_pre_insert_wp_template', 'gutenberg_set_active_template_theme', 9, 2 );
+	}
+);
 function gutenberg_set_active_template_theme( $changes, $request ) {
 	if ( str_starts_with( $request->get_route(), '/wp/v2/templates' ) ) {
 		return $changes;
