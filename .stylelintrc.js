db3526e40d16c0e7d2be0fa6c0f97da2a3883ea9
Andrew Duthie <andrew.duthie@automattic.com>
2025-10-29 14:15:18 -0400

    Exempt vertical properties from logical properties enforcement
    Co-Authored-By: Lena Morita <555336+mirka@users.noreply.github.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -53,7 +53,36 @@ module.exports = {
 		{
 			files: [ '**/*.module.css' ],
 			rules: {
-				'plugin/use-logical-properties-and-values': true,
+				'declaration-property-max-values': {
+					// Prevents left/right values with shorthand property names (unclear for RTL)
+					margin: 3,
+					padding: 3,
+					'border-width': 3,
+					'border-color': 3,
+					'border-style': 3,
+					'border-radius': 3,
+					inset: 3,
+				},
+				'plugin/use-logical-properties-and-values': [
+					true,
+					{
+						ignore: [
+							// Doesn't affect RTL styles
+							'width',
+							'min-width',
+							'max-width',
+							'height',
+							'min-height',
+							'max-height',
+							'margin-top',
+							'margin-bottom',
+							'padding-top',
+							'padding-bottom',
+							'top',
+							'bottom',
+						],
+					},
+				],
 			},
 		},
 	],
