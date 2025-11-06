19d9e3564eb9b0ee37c230c591d91962eb95f308
Jorge Costa <jorge.costa@developer.pt>
2025-11-06 15:59:06 +0000

    correctly transform between variations

--- packages/block-library/src/heading/variations.js
+++ packages/block-library/src/heading/variations.js
@@ -2,9 +2,20 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { justifyStretch } from '@wordpress/icons';
+import { justifyStretch, heading } from '@wordpress/icons';
 
 const variations = [
+	{
+		name: 'paragraph',
+		title: __( 'Heading' ),
+		description: __(
+			'Introduce new sections and organize content to help visitors (and search engines) understand the structure of your content.'
+		),
+		isDefault: true,
+		scope: [ 'block', 'inserter', 'transform' ],
+		attributes: { fitText: undefined },
+		icon: heading,
+	},
 	{
 		name: 'stretch-heading',
 		title: __( 'Stretch Heading' ),
--- packages/block-library/src/paragraph/variations.js
+++ packages/block-library/src/paragraph/variations.js
@@ -2,15 +2,28 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { justifyStretch } from '@wordpress/icons';
+import { justifyStretch, paragraph } from '@wordpress/icons';
 
 const variations = [
+	{
+		name: 'paragraph',
+		title: __( 'Paragraph' ),
+		description: __(
+			'Start with the basic building block of all narrative.'
+		),
+		isDefault: true,
+		scope: [ 'block', 'inserter', 'transform' ],
+		attributes: { fitText: undefined },
+		icon: paragraph,
+	},
 	{
 		name: 'stretch-text',
 		title: __( 'Stretch Text' ),
 		description: __( 'Text that resizes to fit its container.' ),
 		icon: justifyStretch,
-		attributes: { fitText: true },
+		attributes: {
+			fitText: true,
+		},
 		scope: [ 'inserter', 'transform' ],
 		isActive: ( blockAttributes ) => blockAttributes.fitText === true,
 	},
