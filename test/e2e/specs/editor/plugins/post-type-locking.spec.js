1b4a1aaa0e28333cc76bcab8e25af3ff606957e5
Jorge Costa <jorge.costa@developer.pt>
2025-11-06 18:28:12 +0000

    test fix

--- test/e2e/specs/editor/plugins/post-type-locking.spec.js
+++ test/e2e/specs/editor/plugins/post-type-locking.spec.js
@@ -352,6 +352,7 @@ test.describe( 'Post-type locking', () => {
 					.getByRole( 'menu', { name: 'Paragraph', exact: true } )
 					.getByRole( 'menuitem' )
 			).toHaveText( [
+				'Paragraph',
 				'Stretch Text',
 				'Heading',
 				'List',
