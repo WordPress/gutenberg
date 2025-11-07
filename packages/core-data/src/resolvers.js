adc840832d41e5aae01be82c85de3620a61d4429
Chris Zarate <chris.zarate@automattic.com>
2025-11-06 17:13:50 -0700

    Real-time collaboration: [no-pr] Sync collections
    * Real-time collaboration: Sync collections
    
    * Create markEntityAsSaved utility function
    
    * Improve comment

--- packages/core-data/src/resolvers.js
+++ packages/core-data/src/resolvers.js
@@ -381,6 +381,30 @@ export const getEntityRecords =
 				};
 			}
 
+			if (
+				window.__experimentalEnableSync &&
+				entityConfig.syncConfig &&
+				-1 === query.per_page
+			) {
+				if ( globalThis.IS_GUTENBERG_PLUGIN ) {
+					const objectType = `${ kind }/${ name }`;
+					getSyncManager()?.loadCollection(
+						entityConfig.syncConfig,
+						objectType,
+						{
+							refetchRecords: async () => {
+								dispatch.receiveEntityRecords(
+									kind,
+									name,
+									await apiFetch( { path, parse: true } ),
+									query
+								);
+							},
+						}
+					);
+				}
+			}
+
 			// If we request fields but the result doesn't contain the fields,
 			// explicitly set these fields as "undefined"
 			// that way we consider the query "fulfilled".
