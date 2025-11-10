d3524e45263550a44490d541399b228e68085c9f
hbhalodia <hit.bhalodia@rtcamp.com>
2025-11-10 12:27:39 +0530

    Update to use useRef instead of useState

--- packages/dataviews/src/components/dataviews/index.tsx
+++ packages/dataviews/src/components/dataviews/index.tsx
@@ -195,19 +195,20 @@ function DataViews< Item >( {
 	}, [ hasPrimaryOrLockedFilters, isShowingFilter ] );
 
 	// Show filter panel when filters are added (but allow manual hiding).
-	const [ prevFiltersLength, setPrevFiltersLength ] = useState(
-		view.filters?.length || 0
-	);
+	const prevFiltersLengthRef = useRef( view.filters?.length || 0 );
 
 	useEffect( () => {
 		const currentFiltersLength = view.filters?.length || 0;
 
-		if ( currentFiltersLength > prevFiltersLength && ! isShowingFilter ) {
+		if (
+			currentFiltersLength > prevFiltersLengthRef.current &&
+			! isShowingFilter
+		) {
 			setIsShowingFilter( true );
 		}
 
-		setPrevFiltersLength( currentFiltersLength );
-	}, [ view.filters, isShowingFilter, prevFiltersLength ] );
+		prevFiltersLengthRef.current = currentFiltersLength;
+	}, [ view.filters, isShowingFilter ] );
 
 	// Attach scroll event listener for infinite scroll
 	useEffect( () => {
