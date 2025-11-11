c57fc493015e10e2bf192f49197a9d392c0aa155
Ella <4710635+ellatrix@users.noreply.github.com>
2025-11-11 10:45:44 +0100

    Math format: remove empty format on serialise (#73143)
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>

---
+++ test/e2e/specs/editor/various/format-library/math.spec.js
@@ -0,0 +1,93 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Format Library - Math', () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost();
+	} );
+
+	test( 'should insert math format with LaTeX', async ( {
+		editor,
+		page,
+		pageUtils,
+	} ) => {
+		await editor.canvas
+			.getByRole( 'button', { name: 'Add default block' } )
+			.click();
+
+		await page.keyboard.type( 'equation: ' );
+
+		await editor.clickBlockToolbarButton( 'More' );
+		await page.getByRole( 'menuitem', { name: 'Math' } ).click();
+
+		expect( await editor.getBlocks() ).toMatchObject( [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content: 'equation: ',
+				},
+			},
+		] );
+
+		const mathInput = page.locator(
+			'.block-editor-format-toolbar__math-input input'
+		);
+		await mathInput.fill( 'x^2' );
+
+		expect( await editor.getBlocks() ).toMatchObject( [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content:
+						'equation: <math data-latex="x^2"><semantics><msup><mi>x</mi><mn>2</mn></msup><annotation encoding="application/x-tex">x^2</annotation></semantics></math>',
+				},
+			},
+		] );
+
+		// Test typing before.
+		await pageUtils.pressKeys( 'shift+Tab' );
+		await page.keyboard.press( 'ArrowLeft' );
+		await page.keyboard.type( 'a' );
+
+		expect( await editor.getBlocks() ).toMatchObject( [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content:
+						'equation: a<math data-latex="x^2"><semantics><msup><mi>x</mi><mn>2</mn></msup><annotation encoding="application/x-tex">x^2</annotation></semantics></math>',
+				},
+			},
+		] );
+
+		// Test typing after.
+		await page.keyboard.press( 'ArrowRight' );
+		await page.keyboard.type( 'b' );
+
+		expect( await editor.getBlocks() ).toMatchObject( [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content:
+						'equation: a<math data-latex="x^2"><semantics><msup><mi>x</mi><mn>2</mn></msup><annotation encoding="application/x-tex">x^2</annotation></semantics></math>b',
+				},
+			},
+		] );
+
+		// Test removing math element.
+		await pageUtils.pressKeys( 'ArrowLeft' );
+		await pageUtils.pressKeys( 'shift+ArrowLeft' );
+		await page.keyboard.press( 'Tab' );
+		await page.keyboard.press( 'Backspace' );
+
+		expect( await editor.getBlocks() ).toMatchObject( [
+			{
+				name: 'core/paragraph',
+				attributes: {
+					content: 'equation: ab',
+				},
+			},
+		] );
+	} );
+} );
