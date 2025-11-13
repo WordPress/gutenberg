9c8c2fa4894952f9f3eebe8782084139ff4a8d9b
Jorge Costa <jorge.costa@developer.pt>
2025-11-13 16:33:25 +0000

    Improve fit text condition

--- lib/block-supports/typography.php
+++ lib/block-supports/typography.php
@@ -244,7 +244,7 @@ function gutenberg_typography_get_preset_inline_style_value( $style_value, $css_
  * @return string                Filtered block content.
  */
 function gutenberg_render_typography_support( $block_content, $block ) {
-	if ( ! empty( $block['attrs']['fitText'] ) && ! is_admin() ) {
+	if ( ! empty( $block['attrs']['fitText'] ) && $block['attrs']['fitText'] && ! is_admin() ) {
 		wp_enqueue_script_module( '@wordpress/block-editor/utils/fit-text-frontend' );
 
 		// Add Interactivity API directives for fit text to work with client-side navigation.
