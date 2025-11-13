6cb55bd916aeb0ee24786cf1cc99b76693e1b748
tellthemachines <isabel@tellthemachines.com>
2025-11-13 16:56:13 +1100

    Reuse mediaUpload filter for new media modal by wrapping it in a class component

--- packages/block-editor/src/private-apis.js
+++ packages/block-editor/src/private-apis.js
@@ -57,7 +57,6 @@ import {
 	useBlockElementRef,
 	useBlockElement,
 } from './components/block-list/use-block-props/use-block-refs';
-import { default as MediaUploadModal } from './components/media-upload-modal';
 
 /**
  * Private @wordpress/block-editor APIs.
@@ -111,5 +110,4 @@ lock( privateApis, {
 	essentialFormatKey,
 	useBlockElement,
 	useBlockElementRef,
-	MediaUploadModal,
 } );
--- packages/editor/src/hooks/media-upload.js
+++ packages/editor/src/hooks/media-upload.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { Component } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import {
 	MediaUpload,
@@ -16,16 +17,77 @@ const { MediaUploadModal: MediaUploadModalComponent } = unlock(
 	mediaUtilsPrivateApis
 );
 
+/**
+ * Class component wrapper for MediaUploadModal to maintain compatibility
+ * with the stable MediaUpload component API (render prop pattern).
+ */
+class MediaUploadModalWrapper extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = {
+			isOpen: false,
+		};
+		this.openModal = this.openModal.bind( this );
+		this.closeModal = this.closeModal.bind( this );
+	}
+
+	openModal() {
+		this.setState( { isOpen: true } );
+	}
+
+	closeModal() {
+		this.setState( { isOpen: false } );
+		this.props.onClose?.();
+	}
+
+	render() {
+		const {
+			allowedTypes,
+			multiple,
+			value,
+			onSelect,
+			title,
+			modalClass,
+			render,
+		} = this.props;
+		const { isOpen } = this.state;
+
+		return (
+			<>
+				{ render( { open: this.openModal } ) }
+				<MediaUploadModalComponent
+					allowedTypes={ allowedTypes }
+					multiple={ multiple }
+					value={ value }
+					onSelect={ ( media ) => {
+						onSelect( media );
+						this.closeModal();
+					} }
+					onClose={ this.closeModal }
+					title={ title }
+					isOpen={ isOpen }
+					modalClass={ modalClass }
+				/>
+			</>
+		);
+	}
+}
+
 if ( window.__experimentalDataViewsMediaModal ) {
-	// Create a new filter for the MediaUploadModal component
+	// Use the wrapper component for editor.MediaUpload when the experimental flag is enabled
 	addFilter(
-		'editor.MediaUploadModal',
-		'core/editor/components/media-upload-modal',
+		'editor.MediaUpload',
+		'core/editor/components/media-upload',
 		() => {
-			return MediaUploadModalComponent;
+			return MediaUploadModalWrapper;
+		}
+	);
+} else {
+	addFilter(
+		'editor.MediaUpload',
+		'core/editor/components/media-upload',
+		() => {
+			return MediaUpload;
 		}
 	);
 }
-addFilter( 'editor.MediaUpload', 'core/editor/components/media-upload', () => {
-	return MediaUpload;
-} );
