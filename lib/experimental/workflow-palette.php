5442f2ed90998551b36fa15f45f0bb75bca1037c
Jorge Costa <jorge.costa@developer.pt>
2025-11-17 20:26:00 +0000

    remove some comments

--- lib/experimental/workflow-palette.php
+++ lib/experimental/workflow-palette.php
@@ -9,11 +9,8 @@
  * Enqueue workflow palette assets on all admin pages.
  */
 function gutenberg_enqueue_workflow_palette_assets() {
-	// When in production, use the plugin's version as the asset version;
-	// else (for development or test) default to use the current time.
 	$version = defined( 'GUTENBERG_VERSION' ) && ! SCRIPT_DEBUG ? GUTENBERG_VERSION : time();
 
-	// Register and enqueue the workflow style.
 	wp_register_style(
 		'wp-workflow',
 		gutenberg_url( 'build/styles/workflow/style.css' ),
@@ -23,7 +20,6 @@ function gutenberg_enqueue_workflow_palette_assets() {
 	wp_style_add_data( 'wp-workflow', 'rtl', 'replace' );
 	wp_enqueue_style( 'wp-workflow' );
 
-	// Enqueue the workflow script module.
 	wp_enqueue_script_module( '@wordpress/workflow' );
 }
 
