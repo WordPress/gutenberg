091420ab73b570b23a5506c17177344a4d12d3c2
ella <ella@automattic.com>
2025-11-18 11:09:42 +0100

    Template activation: move to experiment

--- lib/experiments-page.php
+++ lib/experiments-page.php
@@ -22,6 +22,26 @@ if ( ! function_exists( 'the_gutenberg_experiments' ) ) {
 		<form method="post" action="options.php">
 			<?php settings_fields( 'gutenberg-experiments' ); ?>
 			<?php do_settings_sections( 'gutenberg-experiments' ); ?>
+			<table class="form-table">
+				<tr>
+					<th scope="row">
+						<label for="active_templates"><?php echo __( 'Template Activation', 'gutenberg' ); ?></label>
+					</th>
+					<td>
+						<label for="active_templates">
+							<input
+								type="checkbox"
+								name="active_templates"
+								id="active_templates"
+								value="1"
+								<?php checked( 1, gutenberg_is_experiment_enabled( 'active_templates' ) ); ?>
+							/>
+							<?php echo __( 'Allows multiple templates of the same type to be created, of which one can be active at a time.', 'gutenberg' ); ?>
+							<p class="description"><?php echo __( 'Warning: when you deactivate this experiment, it is best to delete all created templates except for the active ones.', 'gutenberg' ); ?></p>
+						</label>
+					</td>
+				</tr>
+			</table>
 			<?php submit_button(); ?>
 		</form>
 		</div>
@@ -247,4 +267,25 @@ function gutenberg_display_experiment_section() {
 	<p><?php echo __( "The block editor includes experimental features that are usable while they're in development. Select the ones you'd like to enable. These features are likely to change, so avoid using them in production.", 'gutenberg' ); ?></p>
 
 	<?php
+}
+
+add_action( 'admin_init', 'gutenberg_handle_template_activate_setting_submission' );
+function gutenberg_handle_template_activate_setting_submission() {
+	if ( ! isset( $_POST['option_page'] ) || 'gutenberg-experiments' !== $_POST['option_page'] ) {
+		return;
+	}
+
+	if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'gutenberg-experiments-options' ) ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	if ( isset( $_POST['active_templates'] ) && '1' === $_POST['active_templates'] ) {
+		update_option( 'active_templates', gutenberg_migrate_existing_templates() );
+	} else {
+		delete_option( 'active_templates' );
+	}
 }
