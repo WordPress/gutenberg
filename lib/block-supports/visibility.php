0ded8184b12699aa26580e0f14fd252b620364a9
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-11-20 08:04:59 +0900

    Change file name and hook name

--- lib/block-supports/visibility.php
+++ lib/block-supports/visibility.php
@@ -12,7 +12,7 @@
  * @param array  $block         Block object.
  * @return string Filtered block content.
  */
-function gutenberg_render_block_visibility_support( $block_content, $block ) {
+function gutenberg_render_visibility_support( $block_content, $block ) {
 	$block_type = WP_Block_Type_Registry::get_instance()->get_registered( $block['blockName'] );
 
 	if (
@@ -40,7 +40,7 @@ function gutenberg_render_block_visibility_support( $block_content, $block ) {
 	return $block_content;
 }
 
-if ( function_exists( 'wp_render_block_visibility_support' ) ) {
-	remove_filter( 'render_block', 'wp_render_block_visibility_support' );
+if ( function_exists( 'wp_render_visibility_support' ) ) {
+	remove_filter( 'render_block', 'wp_render_visibility_support' );
 }
-add_filter( 'render_block', 'gutenberg_render_block_visibility_support', 10, 2 );
+add_filter( 'render_block', 'gutenberg_render_visibility_support', 10, 2 );
