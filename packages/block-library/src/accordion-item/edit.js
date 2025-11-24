ec3cdbea85e00065f4ea956c18d30320f1980ccb
Daniel Richards <daniel.richards@automattic.com>
2025-11-24 11:22:11 +0800

    Simplify, always hide the appender

--- packages/block-library/src/accordion-item/edit.js
+++ packages/block-library/src/accordion-item/edit.js
@@ -5,10 +5,8 @@ import { __ } from '@wordpress/i18n';
 import {
 	useBlockProps,
 	useInnerBlocksProps,
-	InnerBlocks,
 	InspectorControls,
 	store as blockEditorStore,
-	useBlockEditingMode,
 } from '@wordpress/block-editor';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
@@ -77,8 +75,6 @@ export default function Edit( {
 		} ),
 	} );
 
-	const blockEditingMode = useBlockEditingMode();
-
 	// Get heading level from context.
 	const headingLevel = context && context[ 'core/accordion-heading-level' ];
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
@@ -97,13 +93,10 @@ export default function Edit( {
 		templateLock: 'all',
 		directInsert: true,
 		templateInsertUpdatesSelection: true,
-		renderAppender:
-			// Hide the appender in contentOnly mode, this is required as `contentOnly` lock overrides the
-			// `templateLock: 'all'` set above. With the Accordion Item and Header/Panel blocks being content
-			// blocks, contentOnly mode would usually allow more blocks to be inserted.
-			blockEditingMode === 'contentOnly'
-				? false
-				: InnerBlocks.ButtonBlockAppender,
+		// You should never be able to appender more blocks to this block, it has a fixed template.
+		// This prevents an issue where in `contentOnly` mode, users can appender Accordion Headers and Panels
+		// because of the contentOnly content insertion feature.
+		renderAppender: false,
 	} );
 
 	return (
