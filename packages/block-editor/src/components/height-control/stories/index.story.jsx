8cb375caa3a94e9c84403e90586bd1343a888da1
Juan Aldasoro <juanfraa@gmail.com>
2025-11-24 12:59:04 +0100

    Storybook: Document arg types for HeightControl

--- packages/block-editor/src/components/height-control/stories/index.story.jsx
+++ packages/block-editor/src/components/height-control/stories/index.story.jsx
@@ -11,6 +11,39 @@ import HeightControl from '../';
 export default {
 	component: HeightControl,
 	title: 'BlockEditor/HeightControl',
+	parameters: {
+		docs: {
+			canvas: { sourceState: 'shown' },
+			description: {
+				component:
+					'HeightControl renders a SliderControl that lets the user enter a height value.',
+			},
+		},
+	},
+	argTypes: {
+		value: {
+			control: { type: null },
+			description: 'The current height value.',
+			table: {
+				type: { summary: 'string' },
+			},
+		},
+		onChange: {
+			action: 'onChange',
+			control: { type: null },
+			description: 'A callback function invoked when the height changes.',
+			table: {
+				type: { summary: 'function' },
+			},
+		},
+		label: {
+			control: 'text',
+			description: 'A label for the control.',
+			table: {
+				type: { summary: 'string' },
+			},
+		},
+	},
 };
 
 const Template = ( props ) => {
