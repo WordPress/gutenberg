dfc8f22278eb6a02162db004f58fffcb1f6e8766
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-11-25 17:21:29 +0100

    Display in storybook

--- packages/dataviews/src/stories/dataviews.story.tsx
+++ packages/dataviews/src/stories/dataviews.story.tsx
@@ -82,6 +82,7 @@ const defaultLayouts = {
 export const Default = ( {
 	perPageSizes = [ 10, 25, 50, 100 ],
 	hasClickableItems = true,
+	backgroundColor,
 } ) => {
 	const [ view, setView ] = useState< View >( {
 		...DEFAULT_VIEW,
@@ -94,28 +95,39 @@ export const Default = ( {
 		return filterSortAndPaginate( data, view, fields );
 	}, [ view ] );
 	return (
-		<DataViews
-			getItemId={ ( item ) => item.id.toString() }
-			paginationInfo={ paginationInfo }
-			data={ shownData }
-			view={ view }
-			fields={ fields }
-			onChangeView={ setView }
-			actions={ actions }
-			renderItemLink={ ( { item, ...props }: { item: SpaceObject } ) => (
-				<button
-					style={ { background: 'none', border: 'none', padding: 0 } }
-					onClick={ () => {
-						// eslint-disable-next-line no-alert
-						alert( 'Clicked: ' + item.name.title );
-					} }
-					{ ...props }
-				/>
-			) }
-			isItemClickable={ () => hasClickableItems }
-			defaultLayouts={ defaultLayouts }
-			config={ { perPageSizes } }
-		/>
+		<div style={ { '--wp-dataviews-color-background': backgroundColor } }>
+			<DataViews
+				getItemId={ ( item ) => item.id.toString() }
+				paginationInfo={ paginationInfo }
+				data={ shownData }
+				view={ view }
+				fields={ fields }
+				onChangeView={ setView }
+				actions={ actions }
+				renderItemLink={ ( {
+					item,
+					...props
+				}: {
+					item: SpaceObject;
+				} ) => (
+					<button
+						style={ {
+							background: 'none',
+							border: 'none',
+							padding: 0,
+						} }
+						onClick={ () => {
+							// eslint-disable-next-line no-alert
+							alert( 'Clicked: ' + item.name.title );
+						} }
+						{ ...props }
+					/>
+				) }
+				isItemClickable={ () => hasClickableItems }
+				defaultLayouts={ defaultLayouts }
+				config={ { perPageSizes } }
+			/>
+		</div>
 	);
 };
 
@@ -132,6 +144,10 @@ Default.argTypes = {
 	hasClickableItems: {
 		control: 'boolean',
 		description: 'Are the items clickable',
+	},
+	backgroundColor: {
+		control: 'color',
+		description: 'Background color of the DataViews component',
 	},
 };
 
