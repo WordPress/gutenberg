78b83d881c0ea937297460a827a593f479c1d58e
Fabian Kägy <mail@fabian-kaegy.de>
2025-11-26 11:04:54 +0100

    fix style url

--- lib/compat/wordpress-7.0/script-loader.php
+++ lib/compat/wordpress-7.0/script-loader.php
@@ -14,7 +14,7 @@ function gutenberg_register_admin_schemes_stylesheet( $styles ) {
 
 	$styles->add(
 		'wp-base-styles',
-		gutenberg_url( 'build/base-styles/admin-schemes.css' )
+		gutenberg_url( 'build/styles/base-styles/admin-schemes.css' )
 	);
 }
 add_action( 'wp_default_styles', 'gutenberg_register_admin_schemes_stylesheet' );
