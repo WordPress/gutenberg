cd5749281904c2899caa720821c4b64960093975
Andrew Duthie <andrew.duthie@automattic.com>
2025-12-01 16:03:23 -0500

    Scripts: Update check-licenses to do second pass using lockfile

--- packages/scripts/scripts/check-licenses.js
+++ packages/scripts/scripts/check-licenses.js
@@ -43,8 +43,32 @@ query += `:not(${ getLicenses( gpl2 )
 	.join( ',' ) })`;
 
 // Use `npm query` to grab a list of all the packages.
-const child = spawn.sync( 'npm', [ 'query', query ] );
+let child = spawn.sync( 'npm', [ 'query', query ] );
 
-const packages = JSON.parse( child.stdout.toString() );
+let packages = JSON.parse( child.stdout.toString() );
+if ( ! packages.length ) {
+	process.exit( 0 );
+}
+
+// For each package flagged as having an unknown license, do a second query pass
+// using details from the package-lock.json file to filter out those which have
+// a known license based on lockfile details. This is necessary as the details
+// based on the filesystem and the lockfile are not always in sync.
+query += `:is(${ packages
+	.map( ( { pkgid } ) => {
+		const [ name ] = pkgid.split( '@' );
+		return `[name=${ JSON.stringify( name ) }]`;
+	} )
+	.join( ',' ) })`;
+
+child = spawn.sync( 'npm', [ 'query', query, '--package-lock-only' ] );
+
+const lockfileOnlyPkgids = JSON.parse( child.stdout.toString() ).map(
+	( { pkgid } ) => pkgid
+);
+
+packages = packages.filter( ( { pkgid } ) =>
+	lockfileOnlyPkgids.includes( pkgid )
+);
 
 checkDeps( packages, { ignored, gpl2 } );
