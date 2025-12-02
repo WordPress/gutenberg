5168654b3b0207f89ec618d3325bb0046100e6e3
ingeniumed <ingeniumed@users.noreply.github.com>
2025-12-02 12:14:49 +1100

    revert unnecessary change

--- packages/editor/src/dataviews/fields/content-preview/content-preview-view.tsx
+++ packages/editor/src/dataviews/fields/content-preview/content-preview-view.tsx
@@ -5,7 +5,7 @@ import { __ } from '@wordpress/i18n';
 import {
 	BlockPreview,
 	privateApis as blockEditorPrivateApis,
-	// @ts-expect-error no types
+	// @ts-ignore
 } from '@wordpress/block-editor';
 import type { BasePost } from '@wordpress/fields';
 import { useSelect } from '@wordpress/data';
