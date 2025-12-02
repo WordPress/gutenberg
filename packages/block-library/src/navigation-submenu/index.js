91d10fd1a680baeb44b89ade92ba72267c116878
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-12-02 19:00:14 +0900

    BlockBreadcrumb: Show custom block name

--- packages/block-library/src/navigation-submenu/index.js
+++ packages/block-library/src/navigation-submenu/index.js
@@ -33,10 +33,12 @@ export const settings = {
 
 		const customName = attributes?.metadata?.name;
 
-		// In the list view, use the block's menu label as the label.
 		// If the menu label is empty, fall back to the default label.
-		if ( context === 'list-view' && ( customName || label ) ) {
-			return attributes?.metadata?.name || label;
+		if (
+			( context === 'list-view' || context === 'breadcrumb' ) &&
+			( customName || label )
+		) {
+			return customName || label;
 		}
 
 		return label;
