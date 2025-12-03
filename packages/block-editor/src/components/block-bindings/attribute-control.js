32cc2d2e47f451ab4f4e0111edeba8dd002468ba
Bernie Reiter <ockham@raz.or.at>
2025-12-03 13:57:05 +0100

    Update props

--- packages/block-editor/src/components/block-bindings/attribute-control.js
+++ packages/block-editor/src/components/block-bindings/attribute-control.js
@@ -170,17 +170,17 @@ export default function BlockBindingsAttributeControl( {
 								isMobile ? 'bottom-start' : 'left-start'
 							}
 						>
-							{ Object.entries(
-								compatibleFieldsForAttribute
-							).map( ( [ sourceKey, fields ] ) => (
-								<BlockBindingsSourceFieldsList
-									key={ sourceKey }
-									args={ binding?.args }
-									attribute={ attribute }
-									sourceKey={ sourceKey }
-									fields={ fields }
-								/>
-							) ) }
+							{ Object.keys( compatibleFieldsForAttribute ).map(
+								( sourceKey ) => (
+									<BlockBindingsSourceFieldsList
+										key={ sourceKey }
+										args={ binding?.args }
+										attribute={ attribute }
+										blockName={ blockName }
+										sourceKey={ sourceKey }
+									/>
+								)
+							) }
 						</Menu>
 					</Menu.Popover>
 				) }
