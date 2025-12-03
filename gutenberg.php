e1875dd1a1a284fe0b2fd8b0ffc69704ccbe4f8e
ingeniumed <ingeniumed@users.noreply.github.com>
2025-12-03 11:41:38 +1100

    Fix phpcs error

--- gutenberg.php
+++ gutenberg.php
@@ -17,7 +17,7 @@ defined( 'GUTENBERG_DEVELOPMENT_MODE' ) or define( 'GUTENBERG_DEVELOPMENT_MODE',
 ### END AUTO-GENERATED DEFINES
 defined( 'GUTENBERG_MINIMUM_WP_VERSION' ) or define( 'GUTENBERG_MINIMUM_WP_VERSION', '6.7' );
 // Custom constant to indicate that this is a custom fork of Gutenberg.
-defined( 'IS_CUSTOM_GUTENBERG_FORK') or define( 'IS_CUSTOM_GUTENBERG_FORK', true );
+defined( 'IS_CUSTOM_GUTENBERG_FORK' ) or define( 'IS_CUSTOM_GUTENBERG_FORK', true );
 
 gutenberg_pre_init();
 
