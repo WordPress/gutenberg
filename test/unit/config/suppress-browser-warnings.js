a0cc46e561a1b8df5c1f5fb5e89be7ebbd328e49
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-12-08 21:11:15 +0900

    Update Jest mock to suppress deprecation warnings

--- test/unit/config/suppress-browser-warnings.js
+++ test/unit/config/suppress-browser-warnings.js
@@ -1,33 +1,32 @@
 /**
  * Suppress specific browser warnings for unit tests
  *
- * This file mocks the @wordpress/warning module to suppress specific
- * warnings that are expected or not relevant in the test environment,
- * while allowing other warnings to pass through normally.
+ * This file mocks the @wordpress/deprecated module to suppress specific
+ * deprecation warnings that are expected or not relevant in the test environment,
+ * while allowing other deprecation warnings to pass through normally.
  *
  * Note: This mock can be removed once the apiVersion:3 becomes the default.
  */
 
-jest.mock( '@wordpress/warning', () => {
-	const mockOriginalWarning =
-		jest.requireActual( '@wordpress/warning' ).default;
+jest.mock( '@wordpress/deprecated', () => {
+	const mockOriginalDeprecated = jest.requireActual(
+		'@wordpress/deprecated'
+	).default;
 
 	return {
 		__esModule: true,
-		default: jest.fn( ( message ) => {
-			const suppressedWarningRegexes = [
-				/The block ".*" is registered with API version 2 or lower/, // apiVersion warnings
+		default: jest.fn( ( feature, options = {} ) => {
+			const suppressedFeatures = [
+				'Block with API version 2 or lower', // apiVersion deprecation warnings
 			];
-			if ( typeof message === 'string' ) {
-				const shouldSuppress = suppressedWarningRegexes.some(
-					( regex ) => regex.test( message )
-				);
-				if ( shouldSuppress ) {
+			if ( typeof feature === 'string' ) {
+				const shouldSuppress = suppressedFeatures.includes( feature );
+				if ( shouldSuppress && options.since === '6.9' ) {
 					return;
 				}
 			}
-			if ( typeof mockOriginalWarning === 'function' ) {
-				mockOriginalWarning( message );
+			if ( typeof mockOriginalDeprecated === 'function' ) {
+				mockOriginalDeprecated( feature, options );
 			}
 		} ),
 	};
