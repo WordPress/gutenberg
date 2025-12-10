a4b132ed36925ece3d43bc84db4b6427a569eafb
James Koster <jay.koster@automattic.com>
2025-12-10 13:42:23 +0000

    Do not render an item for visible locked items

--- packages/dataviews/src/components/dataviews-view-config/properties-section.tsx
+++ packages/dataviews/src/components/dataviews-view-config/properties-section.tsx
@@ -124,22 +124,6 @@ export function PropertiesSection( {
 			) }
 			<VStack className="dataviews-view-config__properties" spacing={ 0 }>
 				<ItemGroup isBordered isSeparated size="medium">
-					{ visibleLockedFields.map( ( { field, isVisibleFlag } ) => {
-						return (
-							<FieldItem
-								key={ field.id }
-								field={ field }
-								isVisible
-								onToggleVisibility={ () => {
-									onChangeView( {
-										...view,
-										[ isVisibleFlag ]: false,
-									} );
-								} }
-							/>
-						);
-					} ) }
-
 					{ hiddenLockedFields.map( ( { field, isVisibleFlag } ) => {
 						return (
 							<FieldItem
