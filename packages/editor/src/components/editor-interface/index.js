2e38185f1eb6167c92c8c04eeaeb819bb8fa2eee
Matías Ventura <mv@matiasventura.com>
2025-12-10 16:25:01 +0100

    Remove unnecessary files

--- packages/editor/src/components/editor-interface/index.js
+++ packages/editor/src/components/editor-interface/index.js
@@ -19,7 +19,6 @@ import { useState, useCallback } from '@wordpress/element';
  */
 import { store as editorStore } from '../../store';
 import { unlock } from '../../lock-unlock';
-import ResponsiveEditingNotice from '../responsive-editing-notice';
 import EditorNotices from '../editor-notices';
 import Header from '../header';
 import InserterSidebar from '../inserter-sidebar';
@@ -185,7 +184,6 @@ export default function EditorInterface( {
 							{ children }
 						</>
 					) }
-					{ ! isPreviewMode && <ResponsiveEditingNotice /> }
 				</>
 			}
 			footer={
--- packages/editor/src/components/responsive-editing-notice/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * The responsive editing notice was replaced by the purple icon indicator
- * on the device preview dropdown in the header.
- *
- * This component is kept for backwards compatibility but renders nothing.
- */
-export default function ResponsiveEditingNotice() {
-	return null;
-}
