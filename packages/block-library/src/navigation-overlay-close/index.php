65367c3d22bb170d5d4534563a67d90bcdb65381
Dave Smith <getdavemail@gmail.com>
2025-12-10 11:18:11 +0000

    Register server side

---
+++ packages/block-library/src/navigation-overlay-close/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Server-side registering of the `core/navigation-overlay-close` block.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Registers the navigation overlay close block.
+ *
+ * @since X.X.X
+ */
+function register_block_core_navigation_overlay_close() {
+	register_block_type_from_metadata(
+		__DIR__ . '/navigation-overlay-close'
+	);
+}
+add_action( 'init', 'register_block_core_navigation_overlay_close' );
+
