c4322917da62c8108f2614dd7e1197bbb85d8bb7
Andrew Duthie <andrew.duthie@automattic.com>
2025-12-11 14:08:26 -0500

    UI: Ensure Stack is exported from root package exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,2 @@
 export * from './box';
+export * from './stack';
---
+++ packages/ui/src/test/index.test.ts
@@ -0,0 +1,20 @@
+import { join, dirname, basename } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import glob from 'fast-glob';
+
+describe( 'index', () => {
+	it( 'should export something from each component', async () => {
+		// As described in the CONTRIBUTING.md file, each component should be
+		// exported from an index.ts in its implementation directory.
+		const components = await glob( join( __dirname, '../*/index.ts' ) );
+		const directories = components.map( ( c ) => basename( dirname( c ) ) );
+		const index = await readFile(
+			join( __dirname, '../index.ts' ),
+			'utf-8'
+		);
+
+		for ( const directory of directories ) {
+			expect( index ).toContain( `'./${ directory }'` );
+		}
+	} );
+} );
