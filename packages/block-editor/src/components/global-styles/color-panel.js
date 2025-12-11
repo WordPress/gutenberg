e715d0ce288a43913af7d4da15d84464880c29eb
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-12-11 18:09:55 +0900

    Put min-height in stylesheet

--- packages/block-editor/src/components/global-styles/color-panel.js
+++ packages/block-editor/src/components/global-styles/color-panel.js
@@ -157,12 +157,6 @@ const popoverProps = {
 	placement: 'left-start',
 	offset: 36,
 	shift: true,
-	style: {
-		// Set minimum height to prevent content from being squashed when
-		// the popover is triggered near the bottom of the page, and flip
-		// the position accordingly.
-		minHeight: '160px',
-	},
 };
 
 const { Tabs } = unlock( componentsPrivateApis );
