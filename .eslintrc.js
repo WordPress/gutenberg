5a8102a34d1fb12bf6fca8570def547322a4fb09
Lena Morita <lena@jaguchi.com>
2025-12-16 06:22:03 +0900

    Remove prop usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -283,7 +283,6 @@ module.exports = {
 						'SearchControl',
 						'SelectControl',
 						'ToggleControl',
-						'TreeSelect',
 					].map( ( componentName ) => ( {
 						selector: `JSXOpeningElement[name.name="${ componentName }"]:not(:has(JSXAttribute[name.name="__nextHasNoMarginBottom"]))`,
 						message:
