1d54a6b240b619e7d4fb66611cd188ccd19c37aa
ella <ella@automattic.com>
2025-12-16 09:46:11 +0100

    whoops

--- packages/dom/src/dom/get-rectangle-from-range.js
+++ packages/dom/src/dom/get-rectangle-from-range.js
@@ -6,7 +6,7 @@ import { assertIsDefined } from '../utils/assert-is-defined';
 /**
  * Get the rectangle of a given Range. Returns `null` if no suitable rectangle
  * can be found. Use instead of `Range.getBoundingClientRect()`, which is often
- * broken, especially for uncollapsed ranges.
+ * broken, especially for collapsed ranges.
  *
  * @param {Range} range The range.
  *
