98be676cc1d73560f3a250ae7f259a6628c47b7e
Lena Morita <lena@jaguchi.com>
2025-12-17 18:48:16 +0900

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -276,14 +276,12 @@ module.exports = {
 					...restrictedSyntax,
 					...restrictedSyntaxComponents,
 					// Temporary rules until we're ready to officially deprecate the bottom margins.
-					...[ 'BaseControl' ].map(
-						( componentName ) => ( {
-							selector: `JSXOpeningElement[name.name="${ componentName }"]:not(:has(JSXAttribute[name.name="__nextHasNoMarginBottom"]))`,
-							message:
-								componentName +
-								' should have the `__nextHasNoMarginBottom` prop to opt-in to the new margin-free styles.',
-						} )
-					),
+					...[ 'BaseControl' ].map( ( componentName ) => ( {
+						selector: `JSXOpeningElement[name.name="${ componentName }"]:not(:has(JSXAttribute[name.name="__nextHasNoMarginBottom"]))`,
+						message:
+							componentName +
+							' should have the `__nextHasNoMarginBottom` prop to opt-in to the new margin-free styles.',
+					} ) ),
 					// Temporary rules until we're ready to officially default to the new size.
 					...[
 						'BorderBoxControl',
