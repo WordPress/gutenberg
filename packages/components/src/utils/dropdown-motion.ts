aefa7abb365cb2eb15351d94f4943775bda11242
Lena Morita <lena@jaguchi.com>
2025-12-19 02:55:39 +0900

    CustomSelectControl: Apply new motion

---
+++ packages/components/src/utils/dropdown-motion.ts
@@ -0,0 +1,7 @@
+export const DROPDOWN_MOTION = Object.freeze( {
+	SLIDE_DISTANCE: '4px',
+	SLIDE_DURATION: '300ms',
+	SLIDE_EASING: 'cubic-bezier( 0, 0, 0, 1 )',
+	FADE_DURATION: '80ms',
+	FADE_EASING: 'linear',
+} );
