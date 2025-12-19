19395b26dd54a376e2ace444fc7c31a0ec9bca73
Riad Benguella <benguella@gmail.com>
2025-12-19 12:47:34 +0100

    Make list view a block support

--- packages/block-library/src/buttons/edit.js
+++ packages/block-library/src/buttons/edit.js
@@ -6,24 +6,9 @@ import clsx from 'clsx';
 /**
  * WordPress dependencies
  */
-import {
-	useBlockProps,
-	useInnerBlocksProps,
-	InspectorControls,
-	privateApis as blockEditorPrivateApis,
-	store as blockEditorStore,
-} from '@wordpress/block-editor';
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { store as blocksStore } from '@wordpress/blocks';
-import { PanelBody } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
-
-/**
- * Internal dependencies
- */
-import { unlock } from '../lock-unlock';
-
-const { PrivateListView } = unlock( blockEditorPrivateApis );
 
 const DEFAULT_BLOCK = {
 	name: 'core/button',
@@ -40,27 +25,22 @@ const DEFAULT_BLOCK = {
 	],
 };
 
-function ButtonsEdit( { attributes, className, clientId } ) {
+function ButtonsEdit( { attributes, className } ) {
 	const { fontSize, layout, style } = attributes;
 	const blockProps = useBlockProps( {
 		className: clsx( className, {
 			'has-custom-font-size': fontSize || style?.typography?.fontSize,
 		} ),
 	} );
-	const { hasButtonVariations, hasChildren } = useSelect(
-		( select ) => {
-			const buttonVariations = select( blocksStore ).getBlockVariations(
-				'core/button',
-				'inserter'
-			);
-			return {
-				hasButtonVariations: buttonVariations.length > 0,
-				hasChildren:
-					!! select( blockEditorStore ).getBlockCount( clientId ),
-			};
-		},
-		[ clientId ]
-	);
+	const { hasButtonVariations } = useSelect( ( select ) => {
+		const buttonVariations = select( blocksStore ).getBlockVariations(
+			'core/button',
+			'inserter'
+		);
+		return {
+			hasButtonVariations: buttonVariations.length > 0,
+		};
+	}, [] );
 
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		defaultBlock: DEFAULT_BLOCK,
@@ -71,26 +51,7 @@ function ButtonsEdit( { attributes, className, clientId } ) {
 		orientation: layout?.orientation ?? 'horizontal',
 	} );
 
-	return (
-		<>
-			<InspectorControls group="list">
-				<PanelBody title={ null }>
-					{ ! hasChildren && (
-						<p className="block-editor-block-inspector__no-blocks">
-							{ __( 'No buttons yet.' ) }
-						</p>
-					) }
-					<PrivateListView
-						rootClientId={ clientId }
-						isExpanded
-						description={ __( 'Buttons' ) }
-						showAppender
-					/>
-				</PanelBody>
-			</InspectorControls>
-			<div { ...innerBlocksProps } />
-		</>
-	);
+	return <div { ...innerBlocksProps } />;
 }
 
 export default ButtonsEdit;
