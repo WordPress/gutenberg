8d07d5a0514572b332276db99e28369d5967adf7
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-12-22 18:03:39 +0100

    Migrate table

--- packages/dataviews/src/dataviews-layouts/table/column-primary.tsx
+++ packages/dataviews/src/dataviews-layouts/table/column-primary.tsx
@@ -6,10 +6,8 @@ import type { ComponentProps, ReactElement } from 'react';
 /**
  * WordPress dependencies
  */
-import {
-	__experimentalHStack as HStack,
-	__experimentalVStack as VStack,
-} from '@wordpress/components';
+import { Stack } from '@wordpress/ui';
+import '@wordpress/theme/design-tokens.css';
 
 /**
  * Internal dependencies
@@ -41,7 +39,7 @@ function ColumnPrimary< Item >( {
 	isItemClickable: ( item: Item ) => boolean;
 } ) {
 	return (
-		<HStack spacing={ 3 } alignment="flex-start" justify="flex-start">
+		<Stack direction="row" gap="sm" align="flex-start" justify="flex-start">
 			{ mediaField && (
 				<ItemClickWrapper
 					item={ item }
@@ -64,9 +62,9 @@ function ColumnPrimary< Item >( {
 					/>
 				</ItemClickWrapper>
 			) }
-			<VStack
-				spacing={ 0 }
-				alignment="flex-start"
+			<Stack
+				direction="column"
+				align="flex-start"
 				className="dataviews-view-table__primary-column-content"
 			>
 				{ titleField && (
@@ -91,8 +89,8 @@ function ColumnPrimary< Item >( {
 						field={ descriptionField }
 					/>
 				) }
-			</VStack>
-		</HStack>
+			</Stack>
+		</Stack>
 	);
 }
 
