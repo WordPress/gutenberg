19a580bf69bc58fb028447c08d120787fbf97779
chriszarate <chris.zarate@automattic.com>
2025-12-22 15:36:50 -0500

    Implement HTTP polling provider

--- packages/sync/src/types.ts
+++ packages/sync/src/types.ts
@@ -14,20 +14,6 @@ import type { Awareness } from 'y-protocols/awareness';
  */
 import type { WORDPRESS_META_KEY_FOR_CRDT_DOC_PERSISTENCE } from './config';
 
-/* globalThis */
-declare global {
-	interface Window {
-		__experimentalCollaborativeEditingSecret?: string;
-		wp?: {
-			ajax?: {
-				settings?: {
-					url?: string;
-				};
-			};
-		};
-	}
-}
-
 export type CRDTDoc = Y.Doc;
 export type EntityID = string;
 export type ObjectID = string;
