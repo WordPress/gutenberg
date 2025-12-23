2a3f2a0592b389f99d2b16fa4847b1de9837f7f6
SirLouen <sir.louen@gmail.com>
2025-12-23 20:23:28 +0100

    fix: Update to include context handling

--- packages/block-library/src/button/test/get-experimental-label.js
+++ packages/block-library/src/button/test/get-experimental-label.js
@@ -11,25 +11,40 @@ describe( 'Button block __experimentalLabel', () => {
 			metadata: { name: 'My Custom Button' },
 			text: 'Click me',
 		};
-		expect( getLabel( attributes ) ).toBe( 'My Custom Button' );
+		expect( getLabel( attributes, { context: 'list-view' } ) ).toBe(
+			'My Custom Button'
+		);
 	} );
 
 	it( 'returns text when no custom name is set', () => {
 		const attributes = {
 			text: 'My Custom Button',
 		};
-		expect( getLabel( attributes ) ).toBe( 'My Custom Button' );
+		expect( getLabel( attributes, { context: 'list-view' } ) ).toBe(
+			'My Custom Button'
+		);
 	} );
 
-	it( 'returns "Button" when text is empty', () => {
+	it( 'returns undefined when text is empty', () => {
 		const attributes = {
 			text: '',
 		};
-		expect( getLabel( attributes ) ).toBe( 'Button' );
+		expect(
+			getLabel( attributes, { context: 'list-view' } )
+		).toBeUndefined();
 	} );
 
-	it( 'returns "Button" when empty attributes', () => {
+	it( 'returns undefined when empty attributes', () => {
 		const attributes = {};
-		expect( getLabel( attributes ) ).toBe( 'Button' );
+		expect(
+			getLabel( attributes, { context: 'list-view' } )
+		).toBeUndefined();
+	} );
+
+	it( 'returns undefined when context is not list-view', () => {
+		const attributes = {
+			text: 'Click me',
+		};
+		expect( getLabel( attributes, { context: 'other' } ) ).toBeUndefined();
 	} );
 } );
