d593f48e80edabdb01b97039b7a12809c57d50b3
Mikael Korpela <mikael.korpela@automattic.com>
2025-12-26 13:54:01 +0200

    Add toolbar-block-appender component

---
+++ packages/block-editor/src/components/toolbar-block-appender/README.md
@@ -0,0 +1,42 @@
+# ToolbarBlockAppender
+
+`ToolbarBlockAppender` provides button with a `+` (plus) icon which when clicked will trigger the default Block `Inserter` UI to allow a Block to be inserted.
+
+## Usage
+
+In a block's `edit` implementation, render a `<ToolbarBlockAppender />` component passing in the `rootClientId`.
+
+```jsx
+function render( { clientId } ) {
+	return (
+		<div>
+			<p>Some rendered content here</p>
+		</div>
+		<ToolbarBlockAppender rootClientId={ clientId } />
+	);
+}
+```
+
+_Note:_
+
+## Props
+
+### `rootClientId`
+
+-   **Type:** `String`
+-   **Required** `true`
+-   **Default:** `undefined`
+
+The `clientId` of the Block from who's root new Blocks should be inserted. This prop is required by the block `Inserter` component. Typically this is the `clientID` of the Block where the prop is being rendered.
+
+### `className`
+
+-   **Type:** `String`
+-   **Default:** `""`
+
+A CSS `class` to be _prepended_ to the default class of `"toolbar-block-appender"`.
+
+## Examples
+
+The [`<InnerBlocks>` component](../inner-blocks/) exposes an enhanced version of `ToolbarBlockAppender` to allow consumers to choose it as an alternative to the standard behaviour of auto-inserting the default Block (typically `core/paragraph`).
+
--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -27,6 +27,7 @@ import {
 	__experimentalUseMultipleOriginColorsAndGradients as useMultipleOriginColorsAndGradients,
 	useBlockEditingMode,
 	BlockControls,
+	ToolbarBlockAppender,
 } from '@wordpress/block-editor';
 import { EntityProvider, store as coreStore } from '@wordpress/core-data';
 
@@ -37,13 +38,9 @@ import {
 	ToggleControl,
 	Spinner,
 	Notice,
-	ToolbarButton,
-	ToolbarGroup,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { speak } from '@wordpress/a11y';
-import { page } from '@wordpress/icons';
-import { createBlock } from '@wordpress/blocks';
 import { useInstanceId } from '@wordpress/compose';
 
 /**
@@ -82,39 +79,12 @@ import { DEFAULT_BLOCK } from '../constants';
 /**
  * Component that renders the Add page button for the Navigation block.
  *
- * @param {Object} props          Component props.
- * @param {string} props.clientId Block client ID.
- * @return {JSX.Element|null} The Add page button component or null if not applicable.
+ * @return {JSX.Element} The Add page toolbar button component
  */
-function NavigationAddPageButton( { clientId } ) {
-	const { insertBlock } = useDispatch( blockEditorStore );
-	const { getBlockCount } = useSelect( blockEditorStore );
-
-	const onAddPage = useCallback( () => {
-		// Get the current number of blocks to insert at the end
-		const blockCount = getBlockCount( clientId );
-
-		// Create a new navigation link block (default block)
-		const newBlock = createBlock( DEFAULT_BLOCK.name, {
-			kind: DEFAULT_BLOCK.attributes.kind,
-			type: DEFAULT_BLOCK.attributes.type,
-		} );
-
-		// Insert the block at the end of the navigation
-		insertBlock( newBlock, blockCount, clientId );
-	}, [ clientId, insertBlock, getBlockCount ] );
-
+function NavigationAddPageToolbarButton() {
 	return (
 		<BlockControls>
-			<ToolbarGroup>
-				<ToolbarButton
-					name="add-page"
-					icon={ page }
-					onClick={ onAddPage }
-				>
-					{ __( 'Add page' ) }
-				</ToolbarButton>
-			</ToolbarGroup>
+			<ToolbarBlockAppender defaultBlock={ DEFAULT_BLOCK } />
 		</BlockControls>
 	);
 }
@@ -971,9 +941,7 @@ function Navigation( {
 					blockEditingMode={ blockEditingMode }
 				/>
 				{ blockEditingMode === 'default' && stylingInspectorControls }
-				{ blockEditingMode === 'contentOnly' && isEntityAvailable && (
-					<NavigationAddPageButton clientId={ clientId } />
-				) }
+				{ isEntityAvailable && <NavigationAddPageToolbarButton /> }
 				{ blockEditingMode === 'default' && isEntityAvailable && (
 					<InspectorControls group="advanced">
 						{ hasResolvedCanUserUpdateNavigationMenu &&
