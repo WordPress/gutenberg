e0dc8a3c8c47f31e8b4960b588cf5c88c9ad9934
André Menrath <99024746+Menrath@users.noreply.github.com>
2025-12-28 13:07:06 +0100

    https://github.com/WordPress/gutenberg/pull/74195#discussion_r2647158914

--- packages/env/lib/config/parse-source-string.js
+++ packages/env/lib/config/parse-source-string.js
@@ -68,17 +68,10 @@ function parseSourceString( sourceString, { cacheDirectoryPath } ) {
 	);
 
 	if ( zipFields ) {
-		const wpOrgFields = sourceString.match(
-			/^https?:\/\/downloads\.wordpress\.org\/(?:plugin|theme)\/([^\s\.]*)([^\s]*)?\.zip$/
+		const rawBasename = path.basename( zipFields[ 1 ] );
+		const basename = encodeURIComponent(
+			rawBasename.replace( /\.(\d+\.)*\d+$/, '' )
 		);
-
-		const fallbackBasename = path.basename( zipFields[ 1 ] )
-			.replace( /\.(\d+\.)*\d+$/, '' );
-		
-		const basename = wpOrgFields
-			? encodeURIComponent( wpOrgFields[ 1 ] )
-			: encodeURIComponent( path.basename( zipFields[ 1 ] )
-				.replace( /\.(\d+\.)*\d+$/, '' ));
 
 		return {
 			type: 'zip',
