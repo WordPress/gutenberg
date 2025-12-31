bd2b4bb241ce89ceb2e87147e6697e0f4b567eb0
hbhalodia <hit.bhalodia@rtcamp.com>
2025-12-31 16:20:12 +0530

    GH-73872 Fix integration tests to update code logic to always include noreferrer noopener when we have target as _blank

--- packages/blocks/src/api/raw-handling/phrasing-content-reducer.js
+++ packages/blocks/src/api/raw-handling/phrasing-content-reducer.js
@@ -46,9 +46,16 @@ export default function phrasingContentReducer( node, doc ) {
 		// In jsdom-jscore, 'node.target' can be null.
 		// TODO: Explore fixing this by patching jsdom-jscore.
 		if ( node.target && node.target.toLowerCase() === '_blank' ) {
-			if ( ! node.rel || node.rel.trim() === '' ) {
-				node.rel = 'noreferrer noopener';
-			}
+			const additionalRel = ( node.rel || '' )
+				.split( /\s+/ )
+				.filter(
+					( part ) =>
+						part && part !== 'noreferrer' && part !== 'noopener'
+				)
+				.join( ' ' );
+			node.rel = additionalRel
+				? `noreferrer noopener ${ additionalRel }`
+				: 'noreferrer noopener';
 		} else {
 			node.removeAttribute( 'target' );
 			node.removeAttribute( 'rel' );
