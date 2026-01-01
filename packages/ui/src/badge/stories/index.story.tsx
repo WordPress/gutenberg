fe2b666c7e8419f276bed831d9a0d4b5d1aae351
Lena Morita <lena@jaguchi.com>
2026-01-02 03:19:42 +0900

    Remove redundant background color from Badge story

--- packages/ui/src/badge/stories/index.story.tsx
+++ packages/ui/src/badge/stories/index.story.tsx
@@ -93,7 +93,6 @@ export const AllIntents: Story = {
 				gridTemplateColumns: 'max-content min-content',
 				gap: '1rem',
 				color: 'var(--wpds-color-fg-content-neutral)',
-				backgroundColor: 'var(--wpds-color-bg-surface-neutral-strong)',
 			} }
 		>
 			{ (
