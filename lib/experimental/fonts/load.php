e988490f5c4a6a99ebd2186f685e3f33b58fcdfa
Riad Benguella <benguella@gmail.com>
2026-01-06 09:19:06 +0100

    Core Merge: Deduplicate Font Library page and routes

--- lib/experimental/fonts/load.php
+++ lib/experimental/fonts/load.php
@@ -5,12 +5,17 @@
  * @package gutenberg
  */
 
-add_action( 'admin_menu', 'gutenberg_register_fonts_menu_item' );
+// Priority 11 to run after Core's menu.php sets up the fonts menu.
+add_action( 'admin_menu', 'gutenberg_register_fonts_menu_item', 11 );
 
 /**
  * Registers the Fonts menu item under Appearance using the font-library page.
+ * Removes Core's fonts menu item first to prevent duplication.
  */
 function gutenberg_register_fonts_menu_item() {
+	// Remove Core's fonts menu item if it exists.
+	remove_submenu_page( 'themes.php', 'font-library.php' );
+
 	add_submenu_page(
 		'themes.php',
 		__( 'Fonts', 'gutenberg' ),
