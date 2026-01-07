f4faf31a2f6065b7e41f4b66b6116af1fd197bd0
saranshsinhaa <saranshsinha084@gmail.com>
2026-01-08 03:23:10 +0530

    Add storybook for ColorPaletteControl

---
+++ packages/block-editor/src/components/color-palette/stories/control.story.js
@@ -0,0 +1,81 @@
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import ColorPaletteControl from '../control';
+
+export default {
+	title: 'BlockEditor/ColorPaletteControl',
+	component: ColorPaletteControl,
+	parameters: {
+		controls: { expanded: true },
+		docs: {
+			canvas: { sourceState: 'shown' },
+			description: {
+				component:
+					'ColorPaletteControl is a wrapper designed for the Block Inspector sidebar. It provides a label, a color swatch grid, and a color value input',
+			},
+		},
+	},
+	argTypes: {
+		onChange: {
+			control: false,
+			table: { disable: true },
+			action: 'onChange',
+		},
+		value: {
+			control: false,
+			table: { disable: true },
+		},
+		label: {
+			control: 'text',
+			description: 'Label for the control.',
+		},
+		disableCustomColors: {
+			control: 'boolean',
+			description:
+				'Whether to allow the user to pick a custom hex color.',
+		},
+		colors: {
+			control: 'object',
+			description: 'Array of color definitions ({ name, color, slug }).',
+		},
+	},
+};
+
+/* eslint-disable react/jsx-filename-extension */
+export const Default = {
+	render: function Template( { onChange, ...args } ) {
+		const [ color, setColor ] = useState( args.value );
+
+		return (
+			<div style={ { maxWidth: '280px' } }>
+				<ColorPaletteControl
+					{ ...args }
+					value={ color }
+					onChange={ ( newColor ) => {
+						setColor( newColor );
+						onChange( newColor );
+					} }
+				/>
+			</div>
+		);
+	},
+	args: {
+		label: 'Text Color',
+		value: '#000000',
+		disableCustomColors: false,
+		colors: [
+			{ name: 'Black', color: '#000000', slug: 'black' },
+			{ name: 'Gray', color: '#555555', slug: 'gray' },
+			{ name: 'White', color: '#ffffff', slug: 'white' },
+			{ name: 'Red', color: '#cf2e2e', slug: 'red' },
+			{ name: 'Blue', color: '#0693e3', slug: 'blue' },
+		],
+	},
+};
+/* eslint-enable react/jsx-filename-extension */
