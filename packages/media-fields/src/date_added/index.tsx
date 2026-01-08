355d7b6524c071d0cd7207daa73132274153d259
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2026-01-08 12:36:00 +1100

    Use format attribute instead

--- packages/media-fields/src/date_added/index.tsx
+++ packages/media-fields/src/date_added/index.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { dateI18n, getDate, getSettings } from '@wordpress/date';
+import { getSettings } from '@wordpress/date';
 import type { Field } from '@wordpress/dataviews';
 
 /**
@@ -10,20 +10,15 @@ import type { Field } from '@wordpress/dataviews';
  */
 import type { MediaItem } from '../types';
 
-const getFormattedDate = ( dateToDisplay: string | null ) =>
-	dateI18n(
-		getSettings().formats.datetimeAbbreviated,
-		getDate( dateToDisplay )
-	);
-
 const dateAddedField: Partial< Field< MediaItem > > = {
 	id: 'date',
 	type: 'datetime',
 	label: __( 'Date added' ),
-	getValue: ( { item }: { item: MediaItem } ) =>
-		item?.date ? getFormattedDate( item.date ) : '',
 	filterBy: {
 		operators: [ 'before', 'after' ],
+	},
+	format: {
+		datetime: getSettings().formats.datetimeAbbreviated,
 	},
 	readOnly: true,
 };
--- packages/media-fields/src/date_modified/index.tsx
+++ packages/media-fields/src/date_modified/index.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { dateI18n, getDate, getSettings } from '@wordpress/date';
+import { getSettings } from '@wordpress/date';
 import type { Field } from '@wordpress/dataviews';
 
 /**
@@ -10,20 +10,15 @@ import type { Field } from '@wordpress/dataviews';
  */
 import type { MediaItem } from '../types';
 
-const getFormattedDate = ( dateToDisplay: string | null ) =>
-	dateI18n(
-		getSettings().formats.datetimeAbbreviated,
-		getDate( dateToDisplay )
-	);
-
 const dateModifiedField: Partial< Field< MediaItem > > = {
 	id: 'modified',
 	type: 'datetime',
 	label: __( 'Date modified' ),
-	getValue: ( { item }: { item: MediaItem } ) =>
-		item?.modified ? getFormattedDate( item.modified ) : '',
 	filterBy: {
 		operators: [ 'before', 'after' ],
+	},
+	format: {
+		datetime: getSettings().formats.datetimeAbbreviated,
 	},
 	readOnly: true,
 };
