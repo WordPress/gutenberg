43a9a916a4db7555569e0dc2ae42e62a83b480be
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-01-09 18:44:27 +0530

    Remove webpack from ignored licenses list

--- bin/check-licenses.mjs
+++ bin/check-licenses.mjs
@@ -10,7 +10,7 @@ import spawn from 'cross-spawn';
  */
 import { checkDeps, getLicenses } from '../packages/scripts/utils/license.js';
 
-const ignored = [ '@ampproject/remapping', 'webpack' ];
+const ignored = [ '@ampproject/remapping' ];
 
 /*
  * `wp-scripts check-licenses` uses prod and dev dependencies of the package to scan for dependencies. With npm workspaces, workspace packages (the @wordpress/* packages) are not listed in the main package json and this approach does not work.
