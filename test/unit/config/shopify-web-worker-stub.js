4341d752ae2dda4f8d2c2ee2813c508e2dd882f9
adamsilverstein <adamsilverstein@earthboundhosting.com>
2026-01-08 16:02:13 -0800

    stub shopify/web-worker

---
+++ test/unit/config/shopify-web-worker-stub.js
@@ -0,0 +1,8 @@
+/**
+ * Stub for @shopify/web-worker transformed imports.
+ *
+ * When the @shopify/web-worker babel plugin transforms createWorkerFactory calls,
+ * it creates webpack loader paths that Jest cannot resolve. This stub is used
+ * as a replacement for those paths.
+ */
+module.exports = {};
