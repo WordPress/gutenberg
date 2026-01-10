3a28bc5c857f105a4ed2f13a2a6df2ec6a31c91f
Seth Rubenstein <srubenstein@pewresearch.org>
2026-01-09 22:32:21 -0500

    Cleanup on styles and blockgap

--- packages/block-library/src/tabs/index.php
+++ packages/block-library/src/tabs/index.php
@@ -6,67 +6,6 @@
  */
 
 /**
- * Build inline CSS custom properties for gap settings.
- *
- * @param array $attributes Block attributes.
- * @param bool  $is_vertical Whether the tabs are vertical.
- *
- * @return string Inline CSS string.
- */
-function block_core_tabs_generate_gap_styles( array $attributes, bool $is_vertical ): string {
-	if ( empty( $attributes['style'] ) || ! is_array( $attributes['style'] ) ) {
-		return '--wp--style--tabs-gap-default: 0.5em;';
-	}
-	if ( empty( $attributes['style']['spacing'] ) || ! is_array( $attributes['style']['spacing'] ) ) {
-		return '--wp--style--tabs-gap-default: 0.5em;';
-	}
-	if ( ! array_key_exists( 'blockGap', $attributes['style']['spacing'] ) ) {
-		return '--wp--style--tabs-gap-default: 0.5em;';
-	}
-
-	$block_gap = $attributes['style']['spacing']['blockGap'];
-
-	if ( is_array( $block_gap ) ) {
-		if ( array_key_exists( 'left', $block_gap ) && array_key_exists( 'top', $block_gap ) ) {
-			$block_gap_horizontal = $block_gap['left'];
-			$block_gap_vertical   = $block_gap['top'];
-		} elseif ( array_key_exists( 'left', $block_gap ) ) {
-			$block_gap_horizontal = $block_gap['left'];
-			$block_gap_vertical   = '0.5em';
-		} elseif ( array_key_exists( 'top', $block_gap ) ) {
-			$block_gap_horizontal = '0.5em';
-			$block_gap_vertical   = $block_gap['top'];
-		} else {
-			return '--wp--style--tabs-gap-default: 0.5em;';
-		}
-	} elseif ( is_string( $block_gap ) ) {
-		return '--wp--style--tabs-gap-default: 0.5em;';
-	}
-
-	$block_gap_horizontal = preg_match( '/^var:preset\|spacing\|\d+$/', (string) $block_gap_horizontal )
-		? 'var(--wp--preset--spacing--' . substr( (string) $block_gap_horizontal, strrpos( (string) $block_gap_horizontal, '|' ) + 1 ) . ')'
-		: (string) $block_gap_horizontal;
-
-	$block_gap_vertical = preg_match( '/^var:preset\|spacing\|\d+$/', (string) $block_gap_vertical )
-		? 'var(--wp--preset--spacing--' . substr( (string) $block_gap_vertical, strrpos( (string) $block_gap_vertical, '|' ) + 1 ) . ')'
-		: (string) $block_gap_vertical;
-
-	$list_gap  = $block_gap_horizontal;
-	$block_gap = $block_gap_vertical;
-
-	if ( $is_vertical ) {
-		$list_gap  = $block_gap_vertical;
-		$block_gap = $block_gap_horizontal;
-	}
-
-	return wp_sprintf(
-		'--wp--style--unstable-tabs-list-gap: %s; --wp--style--unstable-tabs-gap: %s;',
-		$list_gap,
-		$block_gap
-	);
-}
-
-/**
  * Extract tabs list from tab-panels innerblocks.
  *
  * @param array $innerblocks Parsed inner blocks of tabs block.
@@ -195,13 +134,6 @@ function block_core_tabs_render_block_callback( array $attributes, string $conte
 	);
 	$tag_processor->set_attribute( 'data-wp-init', 'callbacks.onTabsInit' );
 	$tag_processor->set_attribute( 'data-wp-on--keydown', 'actions.handleTabKeyDown' );
-
-	/**
-	 * Process style attribute - only gap styles now, colors handled by tabs-menu.
-	 */
-	$style  = (string) $tag_processor->get_attribute( 'style' );
-	$style .= block_core_tabs_generate_gap_styles( $attributes, $is_vertical );
-	$tag_processor->set_attribute( 'style', $style );
 
 	$output = $tag_processor->get_updated_html();
 
