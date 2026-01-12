24290d85be56e4bd1359e541cff968480cf951b5
ella <ella@automattic.com>
2026-01-12 10:48:12 +0100

    wip

--- packages/e2e-test-utils-playwright/src/request-utils/gutenberg-experiments.ts
+++ packages/e2e-test-utils-playwright/src/request-utils/gutenberg-experiments.ts
@@ -9,14 +9,18 @@ import type { RequestUtils } from './index';
  * @param this
  * @param experiments Array of experimental flags to enable. Pass in an empty array to disable all experiments.
  *                    Use 'active_templates' for the template activation feature.
+ * @return Returns true if experiments were set successfully, false if Gutenberg plugin is not active.
  */
 async function setGutenbergExperiments(
 	this: RequestUtils,
 	experiments: string[]
-) {
+): Promise< boolean > {
 	const response = await this.request.get(
 		'/wp-admin/admin.php?page=gutenberg-experiments'
 	);
+	if ( response.status() !== 200 ) {
+		return false;
+	}
 	const html = await response.text();
 	const nonce = html.match( /name="_wpnonce" value="([^"]+)"/ )![ 1 ];
 
@@ -56,6 +60,8 @@ async function setGutenbergExperiments(
 		form: formData,
 		failOnStatusCode: true,
 	} );
+
+	return true;
 }
 
 export { setGutenbergExperiments };
