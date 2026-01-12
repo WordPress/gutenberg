1bb6aef95ebe0c98ee1f460d61a89a0402fb82bc
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2026-01-12 17:15:04 +1100

    Only fade in when the image needed to be loaded

--- packages/media-fields/src/author/view.tsx
+++ packages/media-fields/src/author/view.tsx
@@ -7,7 +7,7 @@ import clsx from 'clsx';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
+import { useState, useCallback, useEffect } from '@wordpress/element';
 import { commentAuthorAvatar as authorIcon } from '@wordpress/icons';
 import { __experimentalHStack as HStack, Icon } from '@wordpress/components';
 import type { DataViewRenderFieldProps } from '@wordpress/dataviews';
@@ -23,18 +23,38 @@ export default function AuthorView( {
 	const author = item?._embedded?.author?.[ 0 ];
 	const text = author?.name;
 	const imageUrl = author?.avatar_urls?.[ 48 ];
-	const [ isImageLoaded, setIsImageLoaded ] = useState( false );
+	const [ loadingState, setLoadingState ] = useState<
+		'instant' | 'loading' | 'loaded'
+	>( 'loading' );
+
+	useEffect( () => {
+		setLoadingState( 'loading' );
+	}, [ imageUrl ] );
+
+	const imgRef = useCallback( ( img: HTMLImageElement | null ) => {
+		if ( img?.complete ) {
+			setLoadingState( 'instant' );
+		}
+	}, [] );
+
+	const handleLoad = () => {
+		if ( loadingState === 'loading' ) {
+			setLoadingState( 'loaded' );
+		}
+	};
 
 	return (
 		<HStack alignment="left" spacing={ 0 }>
 			{ !! imageUrl && (
 				<div
 					className={ clsx( 'media-author-field__avatar', {
-						'is-loaded': isImageLoaded,
+						'is-loading': loadingState === 'loading',
+						'is-loaded': loadingState === 'loaded',
 					} ) }
 				>
 					<img
-						onLoad={ () => setIsImageLoaded( true ) }
+						ref={ imgRef }
+						onLoad={ handleLoad }
 						alt={ __( 'Author avatar' ) }
 						src={ imageUrl }
 					/>
