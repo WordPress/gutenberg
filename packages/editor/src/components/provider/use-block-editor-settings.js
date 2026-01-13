771148fa3cd4b512bf8ed0d62fd4bc8530b6dded
adamsilverstein <adamsilverstein@earthboundhosting.com>
2026-01-12 16:28:22 -0800

    add imageSizes support to media upload settings and block editor settings

--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -130,6 +130,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 		restBlockPatternCategories,
 		sectionRootClientId,
 		deviceType,
+		imageSizes,
 	} = useSelect(
 		( select ) => {
 			const {
@@ -150,6 +151,9 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 			} )
 				? getEntityRecord( 'root', 'site' )
 				: undefined;
+
+			// Fetch image sizes from REST API index for client-side media processing.
+			const baseData = getEntityRecord( 'root', '__unstableBase' );
 
 			function getSectionRootBlock() {
 				if ( renderingMode === 'template-locked' ) {
@@ -196,6 +200,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 				restBlockPatternCategories: getBlockPatternCategories(),
 				sectionRootClientId: getSectionRootBlock(),
 				deviceType: getDeviceType(),
+				imageSizes: baseData?.image_sizes,
 			};
 		},
 		[ postType, postId, isLargeViewport, renderingMode ]
@@ -338,6 +343,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 				: undefined,
 			mediaUpload: hasUploadPermissions ? mediaUpload : undefined,
 			mediaSideload: hasUploadPermissions ? mediaSideload : undefined,
+			imageSizes: hasUploadPermissions ? imageSizes : undefined,
 			__experimentalBlockPatterns: blockPatterns,
 			[ selectBlockPatternsKey ]: ( select ) => {
 				const { hasFinishedResolution, getBlockPatternsForPostType } =
@@ -421,6 +427,7 @@ function useBlockEditorSettings( settings, postType, postId, renderingMode ) {
 		editMediaEntity,
 		wrappedOnNavigateToEntityRecord,
 		deviceType,
+		imageSizes,
 	] );
 }
 
