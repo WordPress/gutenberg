88ebf667e5aa13da141dfa67659e15c4772f14db
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2026-01-13 08:33:45 +0000

    Simplify sync support check in post-locked-modal
    Refactor condition to check sync support directly, removing experimental check.

--- packages/editor/src/components/post-locked-modal/index.js
+++ packages/editor/src/components/post-locked-modal/index.js
@@ -155,7 +155,7 @@ function PostLockedModal() {
 	}
 
 	// Avoid sending the modal if sync is supported, but retain functionality around locks etc.
-	if ( window.__experimentalEnableSync && supportsSync ) {
+	if ( supportsSync ) {
 		if ( globalThis.IS_GUTENBERG_PLUGIN ) {
 			return null;
 		}
