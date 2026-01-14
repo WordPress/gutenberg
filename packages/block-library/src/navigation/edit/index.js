31c6cf064ee8ed077e5f5fa1bf03986c4baefbb6
Ben Dwyer <ben@scruffian.com>
2026-01-14 15:01:16 +0000

    remove parts we don't need from the navigation block

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -77,7 +77,6 @@ import AccessibleDescription from './accessible-description';
 import AccessibleMenuDescription from './accessible-menu-description';
 import { unlock } from '../../lock-unlock';
 import { useToolsPanelDropdownMenuProps } from '../../utils/hooks';
-import { isWithinNavigationOverlay } from '../../utils/is-within-overlay';
 import { DEFAULT_BLOCK } from '../constants';
 
 /**
@@ -272,12 +271,6 @@ function Navigation( {
 		hasIcon,
 		icon = 'handle',
 	} = attributes;
-
-	// Check if we're inside a navigation overlay template part.
-	const isNavigationOverlay = useSelect(
-		() => isWithinNavigationOverlay(),
-		[]
-	);
 
 	const ref = attributes.ref;
 
@@ -504,7 +497,6 @@ function Navigation( {
 				) ]: !! backgroundColor?.slug,
 				[ `has-text-decoration-${ textDecoration }` ]: textDecoration,
 				'block-editor-block-content-overlay': hasBlockOverlay,
-				'is-within-navigation-overlay': isNavigationOverlay,
 			},
 			layoutClassNames
 		),
