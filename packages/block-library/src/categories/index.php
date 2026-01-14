c0f144a3620c34ad60abf32a0581847d4381731b
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2026-01-14 11:56:48 +0530

    Fix: Only navigate on valid term selection in taxonomy dropdown

--- packages/block-library/src/categories/index.php
+++ packages/block-library/src/categories/index.php
@@ -120,7 +120,8 @@ function build_dropdown_script_block_core_categories( $dropdown_id ) {
 				if ( 'escape' === dropdown.dataset.lastkey ) {
 					return;
 				}
-				if ( dropdown.value && dropdown instanceof HTMLSelectElement ) {
+				// Only navigate if a valid term is selected (not the default "Select [taxonomy]" option)
+				if ( dropdown.value && dropdown.value !== '-1' && dropdown instanceof HTMLSelectElement ) {
 					const url = new URL( homeUrl );
 					url.searchParams.set( dropdown.name, dropdown.value );
 					location.href = url.href;
