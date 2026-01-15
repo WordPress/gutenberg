1b2bb4e88a7052571782e1cc053af080de54109b
Marco Ciampini <marco.ciampo@gmail.com>
2026-01-15 21:44:21 +0100

    Remove useless import comments

--- packages/eslint-plugin/rules/__tests__/components-no-missing-40px-size-prop.js
+++ packages/eslint-plugin/rules/__tests__/components-no-missing-40px-size-prop.js
@@ -1,11 +1,4 @@
-/**
- * External dependencies
- */
 import { RuleTester } from 'eslint';
-
-/**
- * Internal dependencies
- */
 import rule from '../components-no-missing-40px-size-prop';
 
 const ruleTester = new RuleTester( {
--- packages/eslint-plugin/rules/__tests__/components-no-unsafe-button-disabled.js
+++ packages/eslint-plugin/rules/__tests__/components-no-unsafe-button-disabled.js
@@ -1,11 +1,4 @@
-/**
- * External dependencies
- */
 import { RuleTester } from 'eslint';
-
-/**
- * Internal dependencies
- */
 import rule from '../components-no-unsafe-button-disabled';
 
 const ruleTester = new RuleTester( {
