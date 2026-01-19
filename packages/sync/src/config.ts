e9cc4bc27ae5369f96628d6758118cd4b37f0d13
ingeniumed <ingeniumed@users.noreply.github.com>
2026-01-20 10:50:35 +1100

    Fix the typo in the constant

--- packages/sync/src/config.ts
+++ packages/sync/src/config.ts
@@ -67,7 +67,7 @@ export const REMOVAL_DELAY_IN_MS = 5000;
 /**
  * Delay in milliseconds before updating the cursor position.
  */
-export const LOCAL_CURSOR_UPDATE_DEBOUNCE_IN_MS = 500;
+export const LOCAL_CURSOR_UPDATE_DEBOUNCE_IN_MS = 5;
 
 /**
  * Delay in milliseconds before throttling the cursor position updates.
