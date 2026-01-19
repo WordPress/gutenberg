9ff04baa9c6cac20f115a6e39c7dd9b0c8b2f6ce
ntsekouras <ntsekouras@outlook.com>
2026-01-19 16:46:08 +0200

    use default isValidRequired

--- packages/dataviews/src/field-types/media.tsx
+++ packages/dataviews/src/field-types/media.tsx
@@ -2,20 +2,8 @@
  * Internal dependencies
  */
 import type { FieldType } from '../types/private';
-import type { NormalizedField } from '../types';
 import getValueFormatted from './utils/get-value-formatted-default';
-
-function isEmpty( value: any ) {
-	return [ 0, undefined, '', null ].includes( value );
-}
-
-function isValidRequired< Item >( item: Item, field: NormalizedField< Item > ) {
-	const value = field.getValue( { item } );
-	if ( Array.isArray( value ) ) {
-		return !! value.length && value.some( ( v ) => ! isEmpty( v ) );
-	}
-	return ! isEmpty( value );
-}
+import isValidRequired from './utils/is-valid-required';
 
 export default {
 	type: 'media',
