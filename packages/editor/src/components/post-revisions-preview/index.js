1629a01da2aa7290e56a2751499927a59f2bf844
ella <ella@automattic.com>
2026-01-20 16:01:56 +0100

    minimise diff

--- packages/editor/src/components/post-revisions-preview/index.js
+++ packages/editor/src/components/post-revisions-preview/index.js
@@ -2,16 +2,18 @@
  * WordPress dependencies
  */
 import { InterfaceSkeleton, ComplementaryArea } from '@wordpress/interface';
-import { useState, useCallback, useMemo } from '@wordpress/element';
+import { useState, useMemo } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
  */
-import usePostRevisions from './use-post-revisions';
 import RevisionsHeader from './revisions-header';
 import RevisionsCanvas from './revisions-canvas';
+import { store as editorStore } from '../../store';
 
-export { usePostRevisions, RevisionsHeader, RevisionsCanvas };
+export { RevisionsHeader, RevisionsCanvas };
 
 /**
  * Revisions mode interface component.
@@ -22,24 +24,45 @@ export { usePostRevisions, RevisionsHeader, RevisionsCanvas };
  * @return {JSX.Element} The revisions interface component.
  */
 export function RevisionsInterface( props ) {
-	const { revisions, isLoading: isRevisionsLoading } = usePostRevisions();
+	const { revisions, isLoading: isRevisionsLoading } = useSelect(
+		( select ) => {
+			const { getCurrentPostId, getCurrentPostType } =
+				select( editorStore );
+			const { getRevisions, isResolving } = select( coreStore );
+
+			const postId = getCurrentPostId();
+			const postType = getCurrentPostType();
+
+			if ( ! postId || ! postType ) {
+				return;
+			}
+
+			const query = { per_page: -1, context: 'edit' };
+			return {
+				revisions: getRevisions( 'postType', postType, postId, query ),
+				isLoading: isResolving( 'getRevisions', [
+					'postType',
+					postType,
+					postId,
+					query,
+				] ),
+			};
+		},
+		[]
+	);
+
 	const [ selectedRevisionIndex, setSelectedRevisionIndex ] = useState( 0 );
 
 	// Sort revisions by date (newest first) and memoize.
 	const sortedRevisions = useMemo( () => {
-		if ( ! revisions.length ) {
-			return [];
-		}
-		return [ ...revisions ].sort(
-			( a, b ) => new Date( b.date ) - new Date( a.date )
+		return (
+			revisions?.sort(
+				( a, b ) => new Date( b.date ) - new Date( a.date )
+			) ?? []
 		);
 	}, [ revisions ] );
 
-	const selectedRevision = sortedRevisions[ selectedRevisionIndex ] || null;
-
-	const handleSelectRevisionIndex = useCallback( ( index ) => {
-		setSelectedRevisionIndex( index );
-	}, [] );
+	const selectedRevision = sortedRevisions[ selectedRevisionIndex ];
 
 	return (
 		<InterfaceSkeleton
@@ -49,7 +72,7 @@ export function RevisionsInterface( props ) {
 					revisions={ sortedRevisions }
 					selectedRevision={ selectedRevision }
 					selectedIndex={ selectedRevisionIndex }
-					onSelectIndex={ handleSelectRevisionIndex }
+					onSelectIndex={ setSelectedRevisionIndex }
 					isLoading={ isRevisionsLoading }
 				/>
 			}
