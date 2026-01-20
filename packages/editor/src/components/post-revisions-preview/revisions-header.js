9c8631e9bee8d766b825d164dc4733fe153e6ad1
ella <ella@automattic.com>
2026-01-20 16:50:33 +0100

    Header skeleton

--- packages/editor/src/components/post-revisions-preview/revisions-header.js
+++ packages/editor/src/components/post-revisions-preview/revisions-header.js
@@ -3,7 +3,7 @@
  */
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useViewportMatch } from '@wordpress/compose';
-import { Button, __unstableMotion as motion } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { store as preferencesStore } from '@wordpress/preferences';
 import { PinnedItems } from '@wordpress/interface';
 import { __, _x } from '@wordpress/i18n';
@@ -12,7 +12,7 @@ import { seen } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import BackButton, { useHasBackButton } from '../header/back-button';
+import HeaderSkeleton from '../header/header-skeleton';
 import MoreMenu from '../more-menu';
 import PostPreviewButton from '../post-preview-button';
 import PreviewDropdown from '../preview-dropdown';
@@ -24,22 +24,6 @@ import {
 	NAVIGATION_POST_TYPE,
 } from '../../store/constants';
 import { unlock } from '../../lock-unlock';
-
-const toolbarVariations = {
-	distractionFreeDisabled: { y: '-50px' },
-	distractionFreeHover: { y: 0 },
-	distractionFreeHidden: { y: '-50px' },
-	visible: { y: 0 },
-	hidden: { y: 0 },
-};
-
-const backButtonVariations = {
-	distractionFreeDisabled: { x: '-100%' },
-	distractionFreeHover: { x: 0 },
-	distractionFreeHidden: { x: '-100%' },
-	visible: { x: 0 },
-	hidden: { x: 0 },
-};
 
 /**
  * Header component for revisions preview mode.
@@ -74,7 +58,6 @@ function RevisionsHeader() {
 		PATTERN_POST_TYPE,
 	].includes( postType );
 
-	const hasBackButton = useHasBackButton();
 	const canRestore = !! currentRevisionId;
 
 	const handleRestore = () => {
@@ -84,21 +67,9 @@ function RevisionsHeader() {
 	};
 
 	return (
-		<div className="editor-header edit-post-header">
-			{ hasBackButton && (
-				<motion.div
-					className="editor-header__back-button"
-					variants={ backButtonVariations }
-					transition={ { type: 'tween' } }
-				>
-					<BackButton.Slot />
-				</motion.div>
-			) }
-			<motion.div
-				variants={ toolbarVariations }
-				className="editor-header__toolbar"
-				transition={ { type: 'tween' } }
-			>
+		<HeaderSkeleton
+			className="editor-revisions-header"
+			toolbar={
 				<Button
 					__next40pxDefaultSize
 					accessibleWhenDisabled
@@ -107,48 +78,41 @@ function RevisionsHeader() {
 					label={ _x( 'Show changes', 'revisions' ) }
 					disabled
 				/>
-			</motion.div>
-			<div
-				className="editor-header__center"
-				style={ { clipPath: 'none' } }
-			>
-				<RevisionsSlider />
-			</div>
-			<motion.div
-				variants={ toolbarVariations }
-				transition={ { type: 'tween' } }
-				className="editor-header__settings"
-			>
-				<PreviewDropdown disabled={ disablePreviewOption } />
+			}
+			center={ <RevisionsSlider /> }
+			settings={
+				<>
+					<PreviewDropdown disabled={ disablePreviewOption } />
 
-				<PostPreviewButton className="editor-header__post-preview-button" />
+					<PostPreviewButton className="editor-header__post-preview-button" />
 
-				{ ( isWideViewport || ! showIconLabels ) && (
-					<PinnedItems.Slot scope="core" />
-				) }
+					{ ( isWideViewport || ! showIconLabels ) && (
+						<PinnedItems.Slot scope="core" />
+					) }
 
-				<Button
-					__next40pxDefaultSize
-					variant="secondary"
-					size="compact"
-					onClick={ () => setCurrentRevisionId( null ) }
-				>
-					{ __( 'Exit' ) }
-				</Button>
-				<Button
-					__next40pxDefaultSize
-					accessibleWhenDisabled
-					variant="primary"
-					size="compact"
-					className="editor-revisions-header__restore-button"
-					disabled={ ! canRestore }
-					onClick={ handleRestore }
-				>
-					{ __( 'Restore' ) }
-				</Button>
-				<MoreMenu />
-			</motion.div>
-		</div>
+					<Button
+						__next40pxDefaultSize
+						variant="secondary"
+						size="compact"
+						onClick={ () => setCurrentRevisionId( null ) }
+					>
+						{ __( 'Exit' ) }
+					</Button>
+					<Button
+						__next40pxDefaultSize
+						accessibleWhenDisabled
+						variant="primary"
+						size="compact"
+						className="editor-revisions-header__restore-button"
+						disabled={ ! canRestore }
+						onClick={ handleRestore }
+					>
+						{ __( 'Restore' ) }
+					</Button>
+					<MoreMenu />
+				</>
+			}
+		/>
 	);
 }
 
