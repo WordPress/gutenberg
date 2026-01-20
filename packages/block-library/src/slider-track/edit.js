5a02b15c25d1f194857dfe1c8e5dcc3b13078b70
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2026-01-20 14:09:19 +0100

    Add Add Slide button

--- packages/block-library/src/slider-track/edit.js
+++ packages/block-library/src/slider-track/edit.js
@@ -6,11 +6,27 @@ import clsx from 'clsx';
 /**
  * WordPress dependencies
  */
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	BlockControls,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
 
 const SLIDE_TEMPLATE = [ [ 'core/slide' ] ];
 
-function SliderTrackEdit() {
+function SliderTrackEdit( { clientId } ) {
+	const { insertBlock } = useDispatch( blockEditorStore );
+
+	const addSlide = () => {
+		const newSlideBlock = createBlock( 'core/slide' );
+		insertBlock( newSlideBlock, undefined, clientId );
+	};
+
 	const blockProps = useBlockProps( {
 		className: clsx( 'wp-block-slider-track' ),
 	} );
@@ -21,7 +37,22 @@ function SliderTrackEdit() {
 		renderAppender: false,
 	} );
 
-	return <div { ...innerBlocksProps }>{ innerBlocksProps.children }</div>;
+	return (
+		<>
+			<BlockControls group="block">
+				<ToolbarGroup>
+					<ToolbarButton
+						className="components-toolbar__control"
+						label={ __( 'Add Slide' ) }
+						onClick={ addSlide }
+						showTooltip
+						text={ __( 'Add Slide' ) }
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			<div { ...innerBlocksProps }>{ innerBlocksProps.children }</div>
+		</>
+	);
 }
 
 export default SliderTrackEdit;
