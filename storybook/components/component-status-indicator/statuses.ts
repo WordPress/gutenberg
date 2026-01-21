4902fa6e073f3fe082413cfb164ceaa8176a7d2c
Lena Morita <lena@jaguchi.com>
2026-01-21 15:24:43 +0000

    Remove unused descriptions from statuses

--- storybook/components/component-status-indicator/statuses.ts
+++ storybook/components/component-status-indicator/statuses.ts
@@ -7,32 +7,26 @@ export type ComponentStatus =
 export const statuses: {
 	value: ComponentStatus;
 	label: string;
-	description: string;
 	icon: string;
 }[] = [
 	{
 		value: 'stable',
 		label: 'Stable',
-		description: 'This component can be used safely.',
 		icon: '✅',
 	},
 	{
 		value: 'use-with-caution',
 		label: 'Use with caution',
-		description: 'See notes.',
 		icon: '⚠️',
 	},
 	{
 		value: 'not-recommended',
 		label: 'Not recommended',
-		description: 'Do not use this component.',
 		icon: '🚫',
 	},
 	{
 		value: 'unaudited',
 		label: 'Unaudited',
-		description:
-			'This component has not been audited yet, and is not necessarily recommended for use.',
 		icon: '❓',
 	},
 ];
