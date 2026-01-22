f050dfc9d7e6620062a6b4b0013fdd65f0f0adea
MaggieCabrera <maggie.cabrera@automattic.com>
2026-01-22 15:46:41 +0100

    better description and title

--- lib/experimental/overlay-patterns.php
+++ lib/experimental/overlay-patterns.php
@@ -83,10 +83,10 @@ function gutenberg_register_overlay_block_patterns() {
 		)
 	);
 	register_block_pattern(
-		'gutenberg/navigation-overlay-centered',
+		'gutenberg/navigation-overlay-centered-with-extras',
 		array(
-			'title'       => __( 'Overlay with centered navigation', 'gutenberg' ),
-			'description' => _x( 'A navigation overlay with vertically and horizontally centered navigation', 'Block pattern description', 'gutenberg' ),
+			'title'       => __( 'Overlay with site info and CTA', 'gutenberg' ),
+			'description' => _x( 'A navigation overlay with vertically and horizontally centered navigation, site info, and a CTA', 'Block pattern description', 'gutenberg' ),
 			'content'     => '<!-- wp:group {"metadata":{"name":"' . esc_attr( __( 'Navigation Overlay', 'gutenberg' ) ) . '"},"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"dimensions":{"minHeight":"100vh"}},"backgroundColor":"white","layout":{"type":"default"}} -->
 <div class="wp-block-group has-white-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group alignwide"><!-- wp:navigation-overlay-close /--></div>
