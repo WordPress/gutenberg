f51e4a4aff52dab02ed0aedc1a0048a5ac95bdb7
chriszarate <chris.zarate@automattic.com>
2026-01-23 10:01:45 -0700

    Fix selection types

--- packages/core-data/src/types.ts
+++ packages/core-data/src/types.ts
@@ -2,7 +2,16 @@ export interface AnyFunction {
 	( ...args: any[] ): any;
 }
 
-// Avoid a circular dependency with @wordpress/editor
+/**
+ * Avoid a circular dependency with @wordpress/editor
+ *
+ * Additionaly, this type marks `attributeKey` and `offset` as possibly
+ * `undefined`, which can happen in two known scenarios:
+ *
+ * 1. If a user has an entire block highlighted (e.g., a `core/image` block).
+ * 2. If there's an intermediate selection state while inserting a block, those
+ *    properties will be temporarily`undefined`.
+ */
 export interface WPBlockSelection {
 	clientId: string;
 	attributeKey?: string;
