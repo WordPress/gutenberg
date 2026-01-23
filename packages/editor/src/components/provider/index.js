6d23e649c19a865f7c1f55f1e3d73a8a9cedc10d
Jerry Jones <jones.jeremydavid@gmail.com>
2026-01-23 11:04:25 -0600

    Allow template part editing from Pages and Posts
    Removes DisableNonPageContentBlocks to allow contentOnly editing of template parts on pages and posts.

--- packages/editor/src/components/provider/index.js
+++ packages/editor/src/components/provider/index.js
@@ -26,7 +26,6 @@ import { store as editorStore } from '../../store';
 import { ATTACHMENT_POST_TYPE } from '../../store/constants';
 import useBlockEditorSettings from './use-block-editor-settings';
 import { unlock } from '../../lock-unlock';
-import DisableNonPageContentBlocks from './disable-non-page-content-blocks';
 import NavigationBlockEditingMode from './navigation-block-editing-mode';
 import { useHideBlocksFromInserter } from './use-hide-blocks-from-inserter';
 import useCommands from '../commands';
@@ -402,9 +401,6 @@ export const ExperimentalEditorProvider = withRegistryProvider(
 								<>
 									<PatternsMenuItems />
 									<TemplatePartMenuItems />
-									{ mode === 'template-locked' && (
-										<DisableNonPageContentBlocks />
-									) }
 									{ type === 'wp_navigation' && (
 										<NavigationBlockEditingMode />
 									) }
