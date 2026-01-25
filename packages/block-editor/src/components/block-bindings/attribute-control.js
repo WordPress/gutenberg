c41c53de3fd0c9e2f9cfc60ab5fe148fbc5aae91
Mitchell Austin <mr.fye@oneandthesame.net>
2026-01-25 14:20:23 -0800

    Only run select hook when UI visible and re-render for fields list

--- packages/block-editor/src/components/block-bindings/attribute-control.js
+++ packages/block-editor/src/components/block-bindings/attribute-control.js
@@ -33,50 +33,96 @@ import { store as blockEditorStore } from '../../store';
 
 const { Menu } = unlock( componentsPrivateApis );
 
-export default function BlockBindingsAttributeControl( {
-	attribute,
-	binding,
-	blockName,
-} ) {
+export default function BlockBindingsAttributeControl( props ) {
+	const { attribute, binding } = props;
 	const { updateBlockBindings } = useBlockBindingsUtils();
-	const isMobile = useViewportMatch( 'medium', '<' );
+	const { canUpdateBlockBindings } = useSelect( ( select ) => ( {
+		canUpdateBlockBindings:
+			select( blockEditorStore ).getSettings().canUpdateBlockBindings,
+	} ) );
 
+	return (
+		<ToolsPanelItem
+			hasValue={ () => !! binding }
+			label={ attribute }
+			onDeselect={ () => {
+				if ( !! binding ) {
+					updateBlockBindings( {
+						[ attribute ]: undefined,
+					} );
+				}
+			} }
+		>
+			<AttributeBindingMenu
+				{ ...props }
+				isWritable={ canUpdateBlockBindings }
+			/>
+		</ToolsPanelItem>
+	);
+}
+
+function AttributeBindingMenu( { attribute, binding, blockName, isWritable } ) {
+	const isMobile = useViewportMatch( 'medium', '<' );
 	const blockContext = useContext( BlockContext );
-	const selected = useSelect(
+	const {
+		attributeType,
+		getAllSources,
+		getSourceFieldsList,
+		hasCompatibleFields,
+		invalidationKey,
+	} = useSelect(
 		( select ) => {
 			const {
 				getAllBlockBindingsSources,
 				getBlockBindingsSourceFieldsList,
 				getBlockType,
 			} = unlock( select( blocksStore ) );
-			const _attributeType =
-				getBlockType( blockName ).attributes?.[ attribute ]?.type;
 
+			let _attributeType =
+				getBlockType( blockName ).attributes?.[ attribute ]?.type;
+			if ( _attributeType === 'rich-text' ) {
+				_attributeType = 'string';
+			}
+
+			const allSources = getAllBlockBindingsSources();
+			const sourceNames = Object.keys( allSources );
+			const allCompatibleFieldKeys = sourceNames.reduce(
+				( compatibleFieldKeys, sourceName ) => {
+					const fieldsList = getBlockBindingsSourceFieldsList(
+						allSources[ sourceName ],
+						blockContext
+					);
+					for ( const field of fieldsList ) {
+						if ( field.type === _attributeType ) {
+							compatibleFieldKeys.push( field.args.key );
+						}
+					}
+					return compatibleFieldKeys;
+				},
+				[]
+			);
 			return {
-				attributeType:
-					_attributeType === 'rich-text' ? 'string' : _attributeType,
-				getBlockBindingsSourceFieldsList,
-				registeredSources: getAllBlockBindingsSources(),
+				attributeType: _attributeType,
+				getAllSources: getAllBlockBindingsSources,
+				getSourceFieldsList: getBlockBindingsSourceFieldsList,
+				hasCompatibleFields: allCompatibleFieldKeys.length > 0,
+				invalidationKey:
+					allCompatibleFieldKeys.join( '' ) + sourceNames.join( '' ),
 			};
 		},
-		[ attribute, blockName ]
+		[ attribute, blockName, blockContext ]
 	);
 
-	const {
-		attributeType,
-		getBlockBindingsSourceFieldsList,
-		registeredSources,
-	} = selected;
 	const compatibleFields = useMemo( () => {
-		const sourceFields = {};
-		Object.entries( registeredSources ).forEach(
-			( [ sourceName, source ] ) => {
-				const fieldsList = getBlockBindingsSourceFieldsList(
-					source,
-					blockContext
-				);
+		void invalidationKey; // Avoids react-hooks/exhaustive-deps warning this is unnecessary.
+		if ( ! hasCompatibleFields ) {
+			return {};
+		}
+		return Object.entries( getAllSources() ).reduce(
+			( sourceFields, [ sourceName, source ] ) => {
+				const fieldsList = getSourceFieldsList( source, blockContext );
 				if ( ! fieldsList?.length ) {
-					return;
+					return sourceFields;
 				}
 				const compatibleFieldsList = fieldsList.filter(
 					( field ) => field.type === attributeType
@@ -84,28 +130,21 @@ export default function BlockBindingsAttributeControl( {
 				if ( compatibleFieldsList.length ) {
 					sourceFields[ sourceName ] = compatibleFieldsList;
 				}
-			}
+				return sourceFields;
+			},
+			{}
 		);
-		return sourceFields;
 	}, [
 		attributeType,
 		blockContext,
-		getBlockBindingsSourceFieldsList,
-		registeredSources,
+		getAllSources,
+		getSourceFieldsList,
+		hasCompatibleFields,
+		invalidationKey,
 	] );
 
-	const { canUpdateBlockBindings } = useSelect( ( select ) => {
-		return {
-			canUpdateBlockBindings:
-				select( blockEditorStore ).getSettings().canUpdateBlockBindings,
-		};
-	}, [] );
-
-	const hasCompatibleFields = Object.keys( compatibleFields ).length > 0;
-
-	// Lock the UI when the user can't update bindings or there are no fields to connect to.
-	const isAttributeReadOnly =
-		! canUpdateBlockBindings || ! hasCompatibleFields;
+	// Keep UI enabled only when there are fields to connect to.
+	isWritable &&= hasCompatibleFields;
 
 	const { source: boundSourceName, args } = binding || {};
 	const source = getBlockBindingsSource( boundSourceName );
@@ -126,7 +165,7 @@ export default function BlockBindingsAttributeControl( {
 		displayText = __( 'Source not registered' );
 	} else {
 		displayText =
-			compatibleFields?.[ boundSourceName ]?.find( ( field ) =>
+			compatibleFields[ boundSourceName ]?.find( ( field ) =>
 				fastDeepEqual( field.args, args )
 			)?.label ||
 			source?.label ||
@@ -134,59 +173,41 @@ export default function BlockBindingsAttributeControl( {
 	}
 
 	return (
-		<ToolsPanelItem
-			hasValue={ () => !! binding }
-			label={ attribute }
-			onDeselect={
-				!! hasCompatibleFields &&
-				( () => {
-					updateBlockBindings( {
-						[ attribute ]: undefined,
-					} );
-				} )
-			}
-		>
-			<Menu placement={ isMobile ? 'bottom-start' : 'left-start' }>
-				<Menu.TriggerButton
-					render={ <Item /> }
-					disabled={ ! hasCompatibleFields }
-				>
-					<VStack
-						className="block-editor-bindings__item"
-						spacing={ 0 }
+		<Menu placement={ isMobile ? 'bottom-start' : 'left-start' }>
+			<Menu.TriggerButton
+				render={ <Item /> }
+				disabled={ ! hasCompatibleFields }
+			>
+				<VStack className="block-editor-bindings__item" spacing={ 0 }>
+					<Text truncate>{ attribute }</Text>
+					<Text
+						truncate
+						variant={ isValid ? 'muted' : undefined }
+						isDestructive={ ! isValid }
 					>
-						<Text truncate>{ attribute }</Text>
-						<Text
-							truncate
-							variant={ isValid ? 'muted' : undefined }
-							isDestructive={ ! isValid }
-						>
-							{ displayText }
-						</Text>
-					</VStack>
-				</Menu.TriggerButton>
-				{ ! isAttributeReadOnly && (
-					<Menu.Popover gutter={ isMobile ? 8 : 36 }>
-						<Menu
-							placement={
-								isMobile ? 'bottom-start' : 'left-start'
-							}
-						>
-							{ Object.entries( compatibleFields ).map(
-								( [ sourceKey, fields ] ) => (
-									<BlockBindingsSourceFieldsList
-										key={ sourceKey }
-										args={ binding?.args }
-										attribute={ attribute }
-										sourceKey={ sourceKey }
-										fields={ fields }
-									/>
-								)
-							) }
-						</Menu>
-					</Menu.Popover>
+						{ displayText }
+					</Text>
+				</VStack>
+			</Menu.TriggerButton>
+			<Menu.Popover gutter={ isMobile ? 8 : 36 }>
+				{ isWritable && (
+					<Menu
+						placement={ isMobile ? 'bottom-start' : 'left-start' }
+					>
+						{ Object.entries( compatibleFields ).map(
+							( [ sourceKey, fields ] ) => (
+								<BlockBindingsSourceFieldsList
+									key={ sourceKey }
+									args={ binding?.args }
+									attribute={ attribute }
+									sourceKey={ sourceKey }
+									fields={ fields }
+								/>
+							)
+						) }
+					</Menu>
 				) }
-			</Menu>
-		</ToolsPanelItem>
+			</Menu.Popover>
+		</Menu>
 	);
 }
