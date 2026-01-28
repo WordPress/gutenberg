ab3d79f1297219872e33b2a2f7aa94dfdcdc5f1c
Ben Dwyer <ben@scruffian.com>
2026-01-28 21:45:18 +0000

    remove unneeded code

--- lib/experimental/editor-settings.php
+++ lib/experimental/editor-settings.php
@@ -43,12 +43,6 @@ function gutenberg_enable_experiments() {
 	if ( gutenberg_is_experiment_enabled( 'gutenberg-media-editor' ) ) {
 		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalMediaEditor = true', 'before' );
 	}
-	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-theme-previews', $gutenberg_experiments ) ) {
-		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalEnableThemePreviews = true', 'before' );
-	}
-	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-pattern-enhancements', $gutenberg_experiments ) ) {
-		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalEnablePatternEnhancements = true', 'before' );
-	}
 	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-playlist-block', $gutenberg_experiments ) ) {
 		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalEnablePlaylistBlock = true', 'before' );
 	}
