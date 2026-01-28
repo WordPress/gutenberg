84401b523734c54d904c95ed09342d3d0fe85ee3
noruzzamans <noruzzamanrubel@gmail.com>
2026-01-28 12:38:26 +0600

    Docs: Add missing @global documentation in block library

--- packages/block-library/src/template-part/index.php
+++ packages/block-library/src/template-part/index.php
@@ -160,6 +160,9 @@ function render_block_core_template_part( $attributes ) {
 	$content = wp_filter_content_tags( $content, "template_part_{$area}" );
 
 	// Handle embeds for block template parts.
+	/**
+	 * @global WP_Embed $wp_embed WordPress Embed object.
+	 */
 	global $wp_embed;
 	$content = $wp_embed->autoembed( $content );
 
