05d7f6003c93d103a1896a3553b4f2ec84901f05
James Koster <jay.koster@automattic.com>
2026-01-29 09:54:23 +0000

    Update instances of old tokens

--- storybook/decorators/with-design-system-theme.tsx
+++ storybook/decorators/with-design-system-theme.tsx
@@ -51,7 +51,7 @@ export function WithDesignSystemTheme(
 								background:
 									'var(--wpds-color-bg-surface-neutral-strong)',
 								padding:
-									'var(--wpds-dimension-padding-surface-sm) var(--wpds-dimension-padding-surface-sm) var(--wpds-dimension-padding-surface-xs)',
+									'var(--wpds-dimension-padding-md) var(--wpds-dimension-padding-md) var(--wpds-dimension-padding-xs)',
 								outline:
 									'1px dashed var(--wpds-color-stroke-surface-neutral)',
 								outlineOffset: '2px',
