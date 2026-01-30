4b257ef53764c80fcc3db5a31c844d5812df0fee
Daniel Richards <daniel.richards@automattic.com>
2026-01-30 16:36:11 +0800

    Switch to List View tab when clicking the block in the content panel if it has support

--- packages/block-editor/src/components/inspector-controls-tabs/content-tab.js
+++ packages/block-editor/src/components/inspector-controls-tabs/content-tab.js
@@ -6,15 +6,28 @@ import {
 	__experimentalUseSlotFills as useSlotFills,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { hasBlockSupport } from '@wordpress/blocks';
+import { useSelect, useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import BlockQuickNavigation from '../block-quick-navigation';
 import groups from '../inspector-controls/groups';
+import { store as blockEditorStore } from '../../store';
+import { unlock } from '../../lock-unlock';
 
 const ContentTab = ( { contentClientIds } ) => {
 	const contentFills = useSlotFills( groups.content?.name );
+
+	const getBlockName = useSelect(
+		( select ) => select( blockEditorStore ).getBlockName,
+		[]
+	);
+	// eslint-disable-next-line @wordpress/no-unused-vars-before-return
+	const { showBlockAttributeGroup } = unlock(
+		useDispatch( blockEditorStore )
+	);
 
 	if ( ! contentClientIds || contentClientIds.length === 0 ) {
 		return null;
@@ -30,6 +43,19 @@ const ContentTab = ( { contentClientIds } ) => {
 	// Collapse the Content panel when there are fills in the content group.
 	const initialOpen = ! hasContentFills;
 
+	const handleSelect = ( clientId ) => {
+		const blockName = getBlockName( clientId );
+		// Navigation block doesn't have List View block support, but
+		// it does have a custom implementation that is shown within
+		// patterns, so it's included in this condition.
+		if (
+			blockName === 'core/navigation' ||
+			hasBlockSupport( blockName, 'listView' )
+		) {
+			showBlockAttributeGroup( 'list' );
+		}
+	};
+
 	return (
 		<>
 			{ ! shouldShowBlockFields && (
@@ -37,7 +63,10 @@ const ContentTab = ( { contentClientIds } ) => {
 					title={ __( 'Content' ) }
 					initialOpen={ initialOpen }
 				>
-					<BlockQuickNavigation clientIds={ contentClientIds } />
+					<BlockQuickNavigation
+						clientIds={ contentClientIds }
+						onSelect={ handleSelect }
+					/>
 				</PanelBody>
 			) }
 		</>
