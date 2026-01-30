cde178c7689a0fca2c70c11b744be0afb3ed1e5f
Daniel Richards <daniel.richards@automattic.com>
2026-01-30 16:25:56 +0800

    Collapse content panel when there are fills in the content tab

--- packages/block-editor/src/components/inspector-controls-tabs/content-tab.js
+++ packages/block-editor/src/components/inspector-controls-tabs/content-tab.js
@@ -1,26 +1,42 @@
 /**
  * WordPress dependencies
  */
-import { PanelBody } from '@wordpress/components';
+import {
+	PanelBody,
+	__experimentalUseSlotFills as useSlotFills,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import BlockQuickNavigation from '../block-quick-navigation';
+import groups from '../inspector-controls/groups';
 
 const ContentTab = ( { contentClientIds } ) => {
+	const contentFills = useSlotFills( groups.content?.name );
+
 	if ( ! contentClientIds || contentClientIds.length === 0 ) {
 		return null;
 	}
 
+	// If there are other panels rendering in the content tab using fills,
+	// collapse the content panel to make space.
+	const hasContentFills = Boolean( contentFills && contentFills.length );
+
 	const shouldShowBlockFields =
 		window?.__experimentalContentOnlyInspectorFields;
+
+	// Collapse the Content panel when there are fills in the content group.
+	const initialOpen = ! hasContentFills;
 
 	return (
 		<>
 			{ ! shouldShowBlockFields && (
-				<PanelBody title={ __( 'Content' ) }>
+				<PanelBody
+					title={ __( 'Content' ) }
+					initialOpen={ initialOpen }
+				>
 					<BlockQuickNavigation clientIds={ contentClientIds } />
 				</PanelBody>
 			) }
