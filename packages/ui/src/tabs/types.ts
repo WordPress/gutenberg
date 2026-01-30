078ca1635d8e2631fd27e4c159af1b1015075d66
Marco Ciampini <marco.ciampo@gmail.com>
2026-01-30 16:09:29 +0100

    Follow Base UI renaming convention

--- packages/ui/src/tabs/types.ts
+++ packages/ui/src/tabs/types.ts
@@ -1,15 +1,15 @@
 import type { ReactNode } from 'react';
-import type { Tabs } from '@base-ui/react/tabs';
+import type { Tabs as _Tabs } from '@base-ui/react/tabs';
 import type { ComponentProps } from '../utils/types';
 
-export type TabRootProps = ComponentProps< typeof Tabs.Root > & {
+export type TabRootProps = ComponentProps< typeof _Tabs.Root > & {
 	/**
 	 * The content to be rendered inside the component.
 	 */
 	children?: ReactNode;
 };
 
-export type TabListProps = ComponentProps< typeof Tabs.List > & {
+export type TabListProps = ComponentProps< typeof _Tabs.List > & {
 	/**
 	 * The content to be rendered inside the component.
 	 */
@@ -21,14 +21,14 @@ export type TabListProps = ComponentProps< typeof Tabs.List > & {
 	variant?: 'minimal' | 'default';
 };
 
-export type TabProps = ComponentProps< typeof Tabs.Tab > & {
+export type TabProps = ComponentProps< typeof _Tabs.Tab > & {
 	/**
 	 * The content to be rendered inside the component.
 	 */
 	children?: ReactNode;
 };
 
-export type TabPanelProps = ComponentProps< typeof Tabs.Panel > & {
+export type TabPanelProps = ComponentProps< typeof _Tabs.Panel > & {
 	/**
 	 * The content to be rendered inside the component.
 	 */
