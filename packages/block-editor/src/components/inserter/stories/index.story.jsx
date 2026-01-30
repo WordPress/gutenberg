f8563d3007b2f4de050112b1d9be485013d2ab47
saranshsinhaa <saranshsinha084@gmail.com>
2026-01-30 18:17:32 +0530

    Add story for inserter component in csf3

--- packages/block-editor/src/components/inserter/stories/index.story.jsx
+++ packages/block-editor/src/components/inserter/stories/index.story.jsx
@@ -11,8 +11,6 @@ import { ExperimentalBlockEditorProvider } from '../../provider';
 import { patternCategories, patterns, reusableBlocks } from './utils/fixtures';
 import Inserter from '../';
 
-export default { title: 'BlockEditor/Inserter' };
-
 // For the purpose of this story, we need to register the core blocks samples.
 registerCoreBlocks();
 
@@ -23,75 +21,134 @@ const wrapperStyle = {
 	display: 'inline-block',
 };
 
-export const LibraryWithoutPatterns = () => {
-	return (
-		<ExperimentalBlockEditorProvider>
-			<div style={ wrapperStyle }>
-				<BlockLibrary showInserterHelpPanel />
-			</div>
-		</ExperimentalBlockEditorProvider>
-	);
+export default {
+	title: 'BlockEditor/Inserter',
+	component: Inserter,
+	parameters: {
+		docs: {
+			canvas: { sourceState: 'shown' },
+			description: {
+				component:
+					'The Inserter is the primary interface for adding blocks, patterns, and media to the content.',
+			},
+		},
+	},
 };
 
-export const LibraryWithPatterns = () => {
-	return (
-		<ExperimentalBlockEditorProvider
-			settings={ {
-				__experimentalBlockPatternCategories: patternCategories,
-				__experimentalBlockPatterns: patterns,
-			} }
-		>
-			<div style={ wrapperStyle }>
-				<BlockLibrary showInserterHelpPanel />
-			</div>
-		</ExperimentalBlockEditorProvider>
-	);
+export const LibraryWithoutPatterns = {
+	parameters: {
+		docs: {
+			description: {
+				story: 'Displays the standard block library panel without any patterns registered.',
+			},
+		},
+	},
+	render: () => {
+		return (
+			<ExperimentalBlockEditorProvider>
+				<div style={ wrapperStyle }>
+					<BlockLibrary showInserterHelpPanel />
+				</div>
+			</ExperimentalBlockEditorProvider>
+		);
+	},
 };
 
-export const LibraryWithPatternsAndReusableBlocks = () => {
-	return (
-		<ExperimentalBlockEditorProvider
-			settings={ {
-				__experimentalBlockPatternCategories: patternCategories,
-				__experimentalBlockPatterns: patterns,
-				__experimentalReusableBlocks: reusableBlocks,
-			} }
-		>
-			<div style={ wrapperStyle }>
-				<BlockLibrary showInserterHelpPanel />
-			</div>
-		</ExperimentalBlockEditorProvider>
-	);
+export const LibraryWithPatterns = {
+	parameters: {
+		docs: {
+			description: {
+				story: 'Displays the block library panel including the "Patterns" tab.',
+			},
+		},
+	},
+	render: () => {
+		return (
+			<ExperimentalBlockEditorProvider
+				settings={ {
+					__experimentalBlockPatternCategories: patternCategories,
+					__experimentalBlockPatterns: patterns,
+				} }
+			>
+				<div style={ wrapperStyle }>
+					<BlockLibrary showInserterHelpPanel />
+				</div>
+			</ExperimentalBlockEditorProvider>
+		);
+	},
 };
 
-export const FullInserter = () => {
-	return (
-		<ExperimentalBlockEditorProvider
-			settings={ {
-				__experimentalBlockPatternCategories: patternCategories,
-				__experimentalBlockPatterns: patterns,
-				__experimentalReusableBlocks: reusableBlocks,
-			} }
-		>
-			<div style={ wrapperStyle }>
-				<Inserter />
-			</div>
-		</ExperimentalBlockEditorProvider>
-	);
+export const LibraryWithPatternsAndReusableBlocks = {
+	parameters: {
+		docs: {
+			description: {
+				story: 'Displays the block library including both Patterns and Reusable Blocks tab.',
+			},
+		},
+	},
+	render: () => {
+		return (
+			<ExperimentalBlockEditorProvider
+				settings={ {
+					__experimentalBlockPatternCategories: patternCategories,
+					__experimentalBlockPatterns: patterns,
+					__experimentalReusableBlocks: reusableBlocks,
+				} }
+			>
+				<div style={ wrapperStyle }>
+					<BlockLibrary showInserterHelpPanel />
+				</div>
+			</ExperimentalBlockEditorProvider>
+		);
+	},
 };
 
-export const QuickInserter = () => {
-	return (
-		<ExperimentalBlockEditorProvider
-			settings={ {
-				__experimentalBlockPatternCategories: patternCategories,
-				__experimentalBlockPatterns: patterns,
-				__experimentalReusableBlocks: reusableBlocks,
-			} }
-		>
-			<div style={ wrapperStyle }>
-				<Inserter __experimentalIsQuick />
-			</div>
-		</ExperimentalBlockEditorProvider>
-	);
+export const FullInserter = {
+	parameters: {
+		docs: {
+			description: {
+				story: 'Shows the full `<Inserter />` component, which includes the toggle button that opens the library panel.',
+			},
+		},
+	},
+	render: () => {
+		return (
+			<ExperimentalBlockEditorProvider
+				settings={ {
+					__experimentalBlockPatternCategories: patternCategories,
+					__experimentalBlockPatterns: patterns,
+					__experimentalReusableBlocks: reusableBlocks,
+				} }
+			>
+				<div style={ wrapperStyle }>
+					<Inserter />
+				</div>
+			</ExperimentalBlockEditorProvider>
+		);
+	},
+};
+
+export const QuickInserter = {
+	parameters: {
+		docs: {
+			description: {
+				story: 'Shows the "Quick Inserter" (slash inserter) variant.',
+			},
+		},
+	},
+	render: () => {
+		return (
+			<ExperimentalBlockEditorProvider
+				settings={ {
+					__experimentalBlockPatternCategories: patternCategories,
+					__experimentalBlockPatterns: patterns,
+					__experimentalReusableBlocks: reusableBlocks,
+				} }
+			>
+				<div style={ wrapperStyle }>
+					<Inserter __experimentalIsQuick />
+				</div>
+			</ExperimentalBlockEditorProvider>
+		);
+	},
 };
