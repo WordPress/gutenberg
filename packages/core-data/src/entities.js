795dfb797b8fdd2fa7f9fe0df6eb36afc54c5021
Adam Silverstein <adam@earthbound.com>
2026-02-03 15:09:21 -0800

    remove unintended changes

--- packages/core-data/src/entities.js
+++ packages/core-data/src/entities.js
@@ -43,8 +43,6 @@ export const rootEntitiesConfig = [
 		baseURLParams: {
 			// Please also change the preload path when changing this.
 			// @see lib/compat/wordpress-7.0/preload.php
-			// @see packages/wp-build/templates/page.php.template
-			// @see packages/wp-build/templates/page-wp-admin.php.template
 			_fields: [
 				'description',
 				'gmt_offset',
