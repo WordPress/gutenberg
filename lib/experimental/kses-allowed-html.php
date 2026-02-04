d7f0b45e71ee137f6b753ef1d762f943fd0ea61a
Seth Rubenstein <srubenstein@pewresearch.org>
2026-02-03 19:35:15 -0500

    Dialog KSES cleanup

--- lib/experimental/kses-allowed-html.php
+++ lib/experimental/kses-allowed-html.php
@@ -50,23 +50,27 @@ add_filter( 'wp_kses_allowed_html', 'gutenberg_kses_allowed_html' );
  * @return array The allowed tags.
  */
 function gutenberg_kses_allow_dialog( $allowedtags ) {
+	if ( ! gutenberg_is_experiment_enabled( 'gutenberg-block-experiments' ) ) {
+		return $allowedtags;
+	}
+
 	$allowedtags['dialog'] = array(
-		'id'                                      => array(),
-		'class'                                   => array(),
-		'style'                                   => array(),
-		'role'                                    => array(),
-		'aria-modal'                              => array(),
-		'aria-labelledby'                         => array(),
-		'open'                                    => array(),
+		'id'                                    => array(),
+		'class'                                 => array(),
+		'style'                                 => array(),
+		'role'                                  => array(),
+		'aria-modal'                            => array(),
+		'aria-labelledby'                       => array(),
+		'open'                                  => array(),
 		// Interactivity API directives.
-		'data-wp-interactive'                     => array(),
-		'data-wp-class--active'                   => array(),
-		'data-wp-class--show-closing-animation'   => array(),
-		'data-wp-on--click'                       => array(),
-		'data-wp-init--on-auto-activation'        => array(),
-		'data-wp-on-document--keydown'            => array(),
-		'data-wp-watch--on-dialog-open'           => array(),
-		'data-wp-watch--on-dialog-close'          => array(),
+		'data-wp-interactive'                   => array(),
+		'data-wp-class--active'                 => array(),
+		'data-wp-class--show-closing-animation' => array(),
+		'data-wp-on--click'                     => array(),
+		'data-wp-init--on-auto-activation'      => array(),
+		'data-wp-on-document--keydown'          => array(),
+		'data-wp-watch--on-dialog-open'         => array(),
+		'data-wp-watch--on-dialog-close'        => array(),
 	);
 	return $allowedtags;
 }
