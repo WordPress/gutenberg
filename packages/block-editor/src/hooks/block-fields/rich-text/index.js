d1d8951ea6fcf8041228cfd62d26e84b7cdf43b4
Bernie Reiter <ockham@raz.or.at>
2026-02-04 13:17:31 +0100

    Add visual highlight (purple border) for bound RichText fields

--- packages/block-editor/src/hooks/block-fields/rich-text/index.js
+++ packages/block-editor/src/hooks/block-fields/rich-text/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import clsx from 'clsx';
+
+/**
  * WordPress dependencies
  */
 import { BaseControl, useBaseControlProps } from '@wordpress/components';
@@ -30,6 +35,7 @@ export default function RichTextControl( {
 	config = {},
 } ) {
 	const registry = useRegistry();
+	const isBound = field.id in ( data?.metadata?.bindings || {} );
 	const attrValue = field.getValue( { item: data } );
 	const fieldConfig = field.config || {};
 	const { clientId } = config;
@@ -146,7 +152,10 @@ export default function RichTextControl( {
 			) }
 			<BaseControl { ...baseControlProps }>
 				<div
-					className="block-editor-content-only-controls__rich-text"
+					className={ clsx(
+						'block-editor-content-only-controls__rich-text',
+						{ 'is-connected': isBound }
+					) }
 					role="textbox"
 					aria-multiline={ ! fieldConfig?.disableLineBreaks }
 					ref={ useMergeRefs( [
