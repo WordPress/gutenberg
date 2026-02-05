afe67522082e6346cda9961dfe0cc5fef52f3c39
Ryan Welcher <me@ryanwelcher.com>
2026-02-05 10:37:49 -0500

    Remove the DataViewsPicker UI implementation.

--- packages/block-library/src/icon/components/index.js
+++ packages/block-library/src/icon/components/index.js
@@ -1,3 +1,2 @@
-export { default as InserterModal } from './inserter';
 export { default as IconPlaceholder } from './icon-placeholder';
 export { default as CustomInserterModal } from './custom-inserter';
