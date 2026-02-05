fe78d429015c3c1db398456981f6f7b5c47c42aa
Ryan Welcher <me@ryanwelcher.com>
2026-02-05 12:05:56 -0500

    Clean up the modal icon grid view.

--- packages/block-library/src/icon/components/custom-inserter/icon-grid.js
+++ packages/block-library/src/icon/components/custom-inserter/icon-grid.js
@@ -7,7 +7,6 @@ import clsx from 'clsx';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Icon, blockDefault } from '@wordpress/icons';
 import { Button } from '@wordpress/components';
 
 /**
@@ -18,53 +17,45 @@ import HtmlRenderer from '../../../utils/html-renderer';
 export default function IconGrid( props ) {
 	const { shownIcons, updateIconAtts, attributes } = props;
 
-	const noResults = (
-		<div className="block-editor-inserter__no-results">
-			<Icon
-				icon={ blockDefault }
-				className="block-editor-inserter__no-results-icon"
-			/>
-			<p>{ __( 'No results found.' ) }</p>
-		</div>
-	);
-
-	const searchResults = (
-		<div className="icons-list">
-			{ shownIcons.map( ( icon ) => {
-				return (
-					<Button
-						key={ `icon-${ icon.name }` }
-						className={ clsx(
-							'icons-list__item',
-							'block-editor-block-types-list__item',
-							{
-								'is-active': icon.name === attributes?.icon,
-								'has-no-icon-fill': icon?.hasNoIconFill,
-							}
-						) }
-						onClick={ () => updateIconAtts( icon.name ) }
-						__next40pxDefaultSize
-					>
-						<span className="icons-list__item-icon">
-							<HtmlRenderer
-								html={ icon.content }
-								wrapperProps={ {
-									style: { width: `24px` },
-								} }
-							/>
-						</span>
-						<span className="icons-list__item-title">
-							{ icon.label }
-						</span>
-					</Button>
-				);
-			} ) }
-		</div>
-	);
-
 	return (
-		<div className="icon-inserter__content-grid">
-			{ shownIcons.length === 0 ? noResults : searchResults }
+		<div className="wp-block-icon__inserter-content-grid">
+			{ shownIcons.length === 0 ? (
+				<div className="wp-block-icon__inserter-content-grid-no-results">
+					<p>{ __( 'No results found.' ) }</p>
+				</div>
+			) : (
+				<div className="wp-block-icon__inserter-content-grid-icons-list">
+					{ shownIcons.map( ( icon ) => {
+						return (
+							<Button
+								key={ `icon-${ icon.name }` }
+								className={ clsx(
+									'wp-block-icon__inserter-content-grid-icons-list-item',
+									'block-editor-block-types-list__item',
+									{
+										'is-active':
+											icon.name === attributes?.icon,
+									}
+								) }
+								onClick={ () => updateIconAtts( icon.name ) }
+								__next40pxDefaultSize
+							>
+								<span className="wp-block-icon__inserter-content-grid-icons-list-item-icon">
+									<HtmlRenderer
+										html={ icon.content }
+										wrapperProps={ {
+											style: { width: `24px` },
+										} }
+									/>
+								</span>
+								<span className="wp-block-icon__inserter-content-grid-icons-list-item-title">
+									{ icon.label }
+								</span>
+							</Button>
+						);
+					} ) }
+				</div>
+			) }
 		</div>
 	);
 }
