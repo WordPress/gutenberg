30d4a7e575760f0e3de2fbc40741ea8eb9bab2a8
adamsilverstein <adamsilverstein@earthboundhosting.com>
2026-02-06 08:50:42 -0800

    type fix

--- lib/compat/wordpress-6.9/block-comments.php
+++ lib/compat/wordpress-6.9/block-comments.php
@@ -48,6 +48,39 @@ function gutenberg_register_block_comment_metadata() {
 			},
 		)
 	);
+
+	register_meta(
+		'comment',
+		'_wp_reactions',
+		array(
+			'type'          => 'object',
+			'description'   => __( 'Emoji reactions', 'gutenberg' ),
+			'single'        => true,
+			'default'       => (object) array(),
+			'show_in_rest'  => array(
+				'schema' => array(
+					'type'                 => 'object',
+					'additionalProperties' => array(
+						'type'  => 'array',
+						'items' => array(
+							'type'       => 'object',
+							'properties' => array(
+								'userId'    => array(
+									'type' => 'integer',
+								),
+								'timestamp' => array(
+									'type' => 'string',
+								),
+							),
+						),
+					),
+				),
+			),
+			'auth_callback' => function ( $allowed, $meta_key, $object_id ) {
+				return current_user_can( 'edit_comment', $object_id );
+			},
+		)
+	);
 }
 add_action( 'init', 'gutenberg_register_block_comment_metadata' );
 
