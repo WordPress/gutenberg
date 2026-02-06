3187e99e81e2eec9727b575426ec58d13833494f
Vrishabhsk <vrishabhsk@gmail.com>
2026-02-06 13:29:57 +0400

    Replace object render with wrapper

--- packages/block-library/src/file/save.js
+++ packages/block-library/src/file/save.js
@@ -22,14 +22,7 @@ export default function save( { attributes } ) {
 		showDownloadButton,
 		downloadButtonText,
 		displayPreview,
-		previewHeight,
 	} = attributes;
-
-	const pdfEmbedLabel = RichText.isEmpty( fileName )
-		? 'PDF embed'
-		: // To do: use toPlainText, but we need ensure it's RichTextData. See
-		  // https://github.com/WordPress/gutenberg/pull/56710.
-		  fileName.toString();
 
 	const hasFilename = ! RichText.isEmpty( fileName );
 
@@ -41,18 +34,7 @@ export default function save( { attributes } ) {
 		href && (
 			<div { ...useBlockProps.save() }>
 				{ displayPreview && (
-					<>
-						<object
-							className="wp-block-file__embed"
-							data={ href }
-							type="application/pdf"
-							style={ {
-								width: '100%',
-								height: `${ previewHeight }px`,
-							} }
-							aria-label={ pdfEmbedLabel }
-						/>
-					</>
+					<div className="wp-block-file__embed"></div>
 				) }
 				{ hasFilename && (
 					<a
