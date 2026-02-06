27cd4f0681ad111916454b1cea0ec616ca0e6c20
ntsekouras <ntsekouras@outlook.com>
2026-02-06 13:09:29 +0200

    register post meta only for pages

--- lib/compat/wordpress-7.0/media-edit-test.php
+++ lib/compat/wordpress-7.0/media-edit-test.php
@@ -14,8 +14,6 @@
  * - test_media_single: integer type for single+expanded testing
  */
 function gutenberg_register_media_edit_test_rest_fields() {
-	$post_types = get_post_types( array( 'show_in_rest' => true ) );
-
 	// Define test fields: name => is_array.
 	$fields = array(
 		'test_media_gallery'   => true,  // array - multiple+expanded.
@@ -23,46 +21,44 @@ function gutenberg_register_media_edit_test_rest_fields() {
 		'test_media_single'    => false, // integer - single+expanded.
 	);
 
-	foreach ( $post_types as $post_type ) {
-		foreach ( $fields as $field_name => $is_array ) {
-			register_rest_field(
-				$post_type,
-				$field_name,
-				array(
-					'schema'          => $is_array
-						? array(
-							'type'    => 'array',
-							'items'   => array( 'type' => 'integer' ),
-							'context' => array( 'view', 'edit' ),
-							'default' => array(),
-						)
-						: array(
-							'type'    => 'integer',
-							'context' => array( 'view', 'edit' ),
-							'default' => 0,
-						),
-					'get_callback'    => function ( $post ) use ( $field_name, $is_array ) {
-						if ( ! isset( $post['id'] ) ) {
-							return $is_array ? array() : 0;
-						}
-						$meta = get_post_meta( $post['id'], $field_name, true );
-						if ( $is_array ) {
-							return ( empty( $meta ) || ! is_array( $meta ) ) ? array() : array_map( 'intval', $meta );
-						}
-						return $meta ? intval( $meta ) : 0;
-					},
-					'update_callback' => function ( $value, $post ) use ( $field_name, $is_array ) {
-						if ( $is_array ) {
-							$value = is_array( $value ) ? array_map( 'intval', $value ) : ( $value ? array( intval( $value ) ) : array() );
-						} else {
-							$value = intval( $value );
-						}
-						update_post_meta( $post->ID, $field_name, $value );
-						return true;
-					},
-				)
-			);
-		}
+	foreach ( $fields as $field_name => $is_array ) {
+		register_rest_field(
+			'page',
+			$field_name,
+			array(
+				'schema'          => $is_array
+					? array(
+						'type'    => 'array',
+						'items'   => array( 'type' => 'integer' ),
+						'context' => array( 'view', 'edit' ),
+						'default' => array(),
+					)
+					: array(
+						'type'    => 'integer',
+						'context' => array( 'view', 'edit' ),
+						'default' => 0,
+					),
+				'get_callback'    => function ( $post ) use ( $field_name, $is_array ) {
+					if ( ! isset( $post['id'] ) ) {
+						return $is_array ? array() : 0;
+					}
+					$meta = get_post_meta( $post['id'], $field_name, true );
+					if ( $is_array ) {
+						return ( empty( $meta ) || ! is_array( $meta ) ) ? array() : array_map( 'intval', $meta );
+					}
+					return $meta ? intval( $meta ) : 0;
+				},
+				'update_callback' => function ( $value, $post ) use ( $field_name, $is_array ) {
+					if ( $is_array ) {
+						$value = is_array( $value ) ? array_map( 'intval', $value ) : ( $value ? array( intval( $value ) ) : array() );
+					} else {
+						$value = intval( $value );
+					}
+					update_post_meta( $post->ID, $field_name, $value );
+					return true;
+				},
+			)
+		);
 	}
 }
 add_action( 'rest_api_init', 'gutenberg_register_media_edit_test_rest_fields' );
