cb3f588bed7f9b49df2a8f37cc71cd188a5f3472
André Maneiro <583546+oandregal@users.noreply.github.com>
2026-02-13 13:26:56 +0100

    Add current template to the list

--- packages/fields/src/fields/template/index.ts
+++ packages/fields/src/fields/template/index.ts
@@ -1,11 +1,12 @@
 /**
  * WordPress dependencies
  */
-import type { Field } from '@wordpress/dataviews';
+import type { Field, Option } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
 import { resolveSelect } from '@wordpress/data';
 import type { WpTemplate } from '@wordpress/core-data';
 import { store as coreDataStore } from '@wordpress/core-data';
+import { decodeEntities } from '@wordpress/html-entities';
 
 /**
  * Internal dependencies
@@ -19,6 +20,9 @@ const templateField: Field< BasePost > = {
 	type: 'text',
 	label: __( 'Template' ),
 	getElements: async ( args ) => {
+		// LIST OF CUSTOM TEMPLATES
+		// This uses the same logic as the editor inspector
+		// (useAvailableTemplates hook at packages/editor/src/components/post-template/hooks.js).
 		const templates: WpTemplate[] =
 			( await resolveSelect( coreDataStore ).getEntityRecords(
 				'postType',
@@ -28,10 +32,63 @@ const templateField: Field< BasePost > = {
 					post_type: args?.item?.type || 'page',
 				}
 			) ) ?? EMPTY_ARRAY;
-		return templates.map( ( { slug, title } ) => ( {
-			value: slug,
-			label: title.rendered || slug,
-		} ) );
+		const templateElements = templates
+			.filter(
+				( template ) => template.is_custom && !! template.content.raw // Skip empty templates.
+			)
+			.map( ( { slug, title } ) => ( {
+				value: slug,
+				label: title.rendered || slug,
+			} ) );
+
+		// CURRENT TEMPLATE
+		// This uses the same logic as the editor inspector
+		// (BlockThemeControl at packages/editor/src/components/post-template/block-theme.js)
+		let slugToCheck;
+		const postType = args?.item?.type;
+		const slug = args?.item?.slug;
+		// In `draft` status we might not have a slug available, so we use the `single`
+		// post type templates slug(ex page, single-post, single-product etc..).
+		// Pages do not need the `single` prefix in the slug to be prioritized
+		// through template hierarchy.
+		if ( slug ) {
+			slugToCheck =
+				postType === 'page'
+					? `${ postType }-${ slug }`
+					: `single-${ postType }-${ slug }`;
+		} else {
+			slugToCheck = postType === 'page' ? 'page' : `single-${ postType }`;
+		}
+
+		let currentTemplateElement: Option | undefined;
+		if ( postType ) {
+			const templateId = await resolveSelect(
+				coreDataStore
+			).getDefaultTemplateId( {
+				slug: slugToCheck,
+			} );
+
+			const currentTemplate = ( await resolveSelect(
+				coreDataStore
+			).getEntityRecord( 'postType', 'wp_template', templateId ) ) as
+				| WpTemplate
+				| undefined;
+			if ( currentTemplate ) {
+				currentTemplateElement = {
+					// If the template field is empty it means it'll be matched to a template from the hierarchy,
+					// which is the current template. Instead of using the currentTemplate.slug as value,
+					// we want to use the empty string so that this element is the one selected in the control.
+					// And, inversely, when selecting this element, the user will be resetting the template
+					// to the default value.
+					value: '',
+					label: decodeEntities( currentTemplate.title.rendered ),
+				};
+			}
+		}
+
+		return [ currentTemplateElement, ...templateElements ].filter(
+			( item ): item is Option => !! item
+		);
 	},
 	enableSorting: false,
 	filterBy: false,
