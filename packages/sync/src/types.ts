7b9fb8da0b0b24f5b0c7da12112750dbb54f596b
chriszarate <chris.zarate@automattic.com>
2026-02-16 17:17:47 -0500

    Fix type

--- packages/sync/src/types.ts
+++ packages/sync/src/types.ts
@@ -48,7 +48,7 @@ export interface ObjectMeta extends Record< string, unknown > {
  * Add new event types here as needed.
  */
 export interface ProviderEventMap {
-	'sync-connection-status': SyncConnectionState;
+	status: ConnectionStatus;
 }
 
 /**
