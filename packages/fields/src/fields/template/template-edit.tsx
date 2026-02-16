41997f7d6002b21e3c1c572fda7af7ac84b7f9b2
André Maneiro <583546+oandregal@users.noreply.github.com>
2026-02-16 12:07:41 +0100

    Disable editing for homepage and blog posts templates

--- packages/fields/src/fields/template/template-edit.tsx
+++ packages/fields/src/fields/template/template-edit.tsx
@@ -14,7 +14,6 @@ import { __ } from '@wordpress/i18n';
  */
 import { getItemTitle } from '../../actions/utils';
 import type { BasePost } from '../../types';
-import { unlock } from '../../lock-unlock';
 import { getDefaultTemplateLabel } from './utils';
 
 const EMPTY_ARRAY: [] = [];
@@ -26,14 +25,12 @@ export const TemplateEdit = ( {
 }: DataFormControlProps< BasePost > ) => {
 	const { id } = field;
 	const postType = data.type;
-	const postId =
-		typeof data.id === 'number' ? data.id : parseInt( data.id, 10 );
 
 	const slug = data.slug;
 
-	const { canSwitchTemplate, templates } = useSelect(
+	const templates = useSelect(
 		( select ) => {
-			const allTemplates =
+			return (
 				select( coreStore ).getEntityRecords< WpTemplate >(
 					'postType',
 					'wp_template',
@@ -41,24 +38,10 @@ export const TemplateEdit = ( {
 						per_page: -1,
 						post_type: postType,
 					}
-				) ?? EMPTY_ARRAY;
-
-			const { getHomePage, getPostsPageId } = unlock(
-				select( coreStore )
+				) ?? EMPTY_ARRAY
 			);
-
-			const isPostsPage = getPostsPageId() === +postId;
-			const isFrontPage =
-				postType === 'page' && getHomePage()?.postId === +postId;
-
-			const allowSwitchingTemplate = ! isPostsPage && ! isFrontPage;
-
-			return {
-				templates: allTemplates,
-				canSwitchTemplate: allowSwitchingTemplate,
-			};
 		},
-		[ postId, postType ]
+		[ postType ]
 	);
 
 	const currentTemplateLabel = useSelect(
@@ -95,7 +78,6 @@ export const TemplateEdit = ( {
 			value={ value }
 			options={ options }
 			onChange={ onChangeControl }
-			disabled={ ! canSwitchTemplate }
 		/>
 	);
 };
