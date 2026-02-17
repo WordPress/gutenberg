098c5c1a7ca975d4fbd189122a99855fe4130761
Luigi Teschio <gigitux@gmail.com>
2026-02-17 11:18:40 +0100

    improve classname

--- packages/boot/src/components/root/index.tsx
+++ packages/boot/src/components/root/index.tsx
@@ -68,7 +68,7 @@ export default function Root() {
 						} ) }
 					>
 						<SavePanel />
-						<SnackbarNotices className="editor-notices__snackbar" />
+						<SnackbarNotices className="boot-notices__snackbar" />
 						{ isMobileViewport && (
 							<Page.SidebarToggleFill>
 								<Button
