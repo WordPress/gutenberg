2e7f18668589421f1abfb0243b9b9a6e516c741b
MaggieCabrera <maggie.cabrera@automattic.com>
2026-02-17 13:06:07 +0100

    added test to cover this issue

--- test/e2e/specs/site-editor/navigation-overlay-template-part.spec.js
+++ test/e2e/specs/site-editor/navigation-overlay-template-part.spec.js
@@ -207,5 +207,50 @@ test.describe( 'Navigation Overlay Template Part', () => {
 			await closeButtons.last().click();
 			await expect( openMenuButton ).toBeVisible();
 		} );
+
+		test( 'As a site builder, I want to insert a core navigation overlay pattern with CTA and see it on the frontend', async ( {
+			page,
+			editor,
+		} ) => {
+			await page.getByRole( 'tab', { name: 'Template Part' } ).click();
+
+			const designTab = page.getByRole( 'button', {
+				name: 'Design',
+			} );
+			await expect( designTab ).toBeVisible();
+
+			await designTab.click();
+
+			// Find and click the "Overlay with site info and CTA" pattern
+			const ctaPattern = page
+				.getByRole( 'option' )
+				.filter( { hasText: /site info and CTA/i } );
+			await expect( ctaPattern ).toBeVisible();
+			await ctaPattern.click();
+
+			const siteTitleBlock = editor.canvas.getByRole( 'document', {
+				name: /Block: Site Title/i,
+			} );
+			await expect( siteTitleBlock ).toBeVisible();
+
+			await page
+				.getByRole( 'region', { name: 'Editor top bar' } )
+				.getByRole( 'button', { name: 'Back', exact: true } )
+				.click();
+
+			await editor.saveSiteEditorEntities();
+
+			await page.goto( '/' );
+
+			const openMenuButton = page.getByRole( 'button', {
+				name: 'Open menu',
+			} );
+			await openMenuButton.click();
+
+			const siteTitleFrontend = page.getByRole( 'link', {
+				name: new RegExp( '.+' ),
+			} );
+			await expect( siteTitleFrontend.first() ).toBeVisible();
+		} );
 	} );
 } );
