b97cdce01b9d46641cefd73ebd8e1d4e5b83ed9d
adamsilverstein <adamsilverstein@earthboundhosting.com>
2026-02-18 21:59:17 +0700

    Move __clientSideMediaProcessing global out of experimental
    This JS global is no longer experimental. Moving it to
    lib/media/load.php which already gates on the feature
    being enabled.

--- lib/experimental/editor-settings.php
+++ lib/experimental/editor-settings.php
@@ -22,9 +22,6 @@ function gutenberg_enable_experiments() {
 	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-dataviews-media-modal', $gutenberg_experiments ) ) {
 		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalDataViewsMediaModal = true', 'before' );
 	}
-	if ( gutenberg_is_client_side_media_processing_enabled() ) {
-		wp_add_inline_script( 'wp-block-editor', 'window.__clientSideMediaProcessing = true', 'before' );
-	}
 	if ( $gutenberg_experiments && array_key_exists( 'gutenberg-content-only-inspector-fields', $gutenberg_experiments ) ) {
 		wp_add_inline_script( 'wp-block-editor', 'window.__experimentalContentOnlyInspectorFields = true', 'before' );
 	}
