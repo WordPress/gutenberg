a4379087194dce7264dd1675e742c698f8ef84ce
Ramon <ramonjd@gmail.com>
2026-02-18 17:38:22 +1100

    Refactor navigation block tests to use updated function for block type verification
    * Update test assertions to use block_core_navigation_block_tree_has_block_type instead of the deprecated block_core_navigation_block_contains_core_navigation.
    * Add coverage annotations for the new function in the test cases.

--- phpunit/blocks/render-block-navigation-test.php
+++ phpunit/blocks/render-block-navigation-test.php
@@ -66,29 +66,29 @@ class Render_Block_Navigation_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * @expectedDeprecated block_core_navigation_block_contains_core_navigation
+	 * @covers ::block_core_navigation_block_tree_has_block_type
 	 */
 	public function test_block_core_navigation_block_contains_core_navigation() {
 		$parsed_blocks = parse_blocks( '<!-- wp:navigation /-->' );
 		$inner_blocks  = new WP_Block_List( $parsed_blocks );
-		$this->assertTrue( block_core_navigation_block_contains_core_navigation( $inner_blocks ) );
+		$this->assertTrue( block_core_navigation_block_tree_has_block_type( $inner_blocks, 'core/navigation' ) );
 	}
 
 	/**
-	 * @expectedDeprecated block_core_navigation_block_contains_core_navigation
+	 * @covers ::block_core_navigation_block_tree_has_block_type
 	 */
 	public function test_block_core_navigation_block_contains_core_navigation_deep() {
 		$parsed_blocks = parse_blocks( '<!-- wp:group --><!-- /wp:group --><!-- wp:group --><!-- wp:group --><!-- wp:navigation /--><!-- /wp:group --><!-- /wp:group -->' );
 		$inner_blocks  = new WP_Block_List( $parsed_blocks );
-		$this->assertTrue( block_core_navigation_block_contains_core_navigation( $inner_blocks ) );
+		$this->assertTrue( block_core_navigation_block_tree_has_block_type( $inner_blocks, 'core/navigation' ) );
 	}
 
 	/**
-	 * @expectedDeprecated block_core_navigation_block_contains_core_navigation
+	 * @covers ::block_core_navigation_block_tree_has_block_type
 	 */
 	public function test_block_core_navigation_block_contains_core_navigation_no_navigation() {
 		$parsed_blocks = parse_blocks( '<!-- wp:group --><!-- wp:group --><!-- /wp:group --><!-- /wp:group -->' );
 		$inner_blocks  = new WP_Block_List( $parsed_blocks );
-		$this->assertFalse( block_core_navigation_block_contains_core_navigation( $inner_blocks ) );
+		$this->assertFalse( block_core_navigation_block_tree_has_block_type( $inner_blocks, 'core/navigation' ) );
 	}
 }
