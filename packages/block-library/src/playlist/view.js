cdf5d18ddd87e035e1a2ef0c895730dcc79d1371
Ben Dwyer <ben@scruffian.com>
2026-02-19 16:47:16 +0000

    import the CSS into the style.scss file

--- packages/block-library/src/playlist/view.js
+++ packages/block-library/src/playlist/view.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import '@arraypress/waveform-player/dist/waveform-player.css';
-
-/**
  * WordPress dependencies
  */
 import { store, getContext, getElement } from '@wordpress/interactivity';
