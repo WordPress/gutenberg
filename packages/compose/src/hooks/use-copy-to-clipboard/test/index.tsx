4f9660e386af20fc7a16f90d045e01653f45021f
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-02-20 23:47:11 +0900

    ADd unit tests

---
+++ packages/compose/src/hooks/use-copy-to-clipboard/test/index.tsx
@@ -0,0 +1,96 @@
+/**
+ * External dependencies
+ */
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+/**
+ * Internal dependencies
+ */
+import useCopyToClipboard from '../';
+
+interface TestComponentProps {
+	text: string | ( () => string );
+	onSuccess?: () => void;
+}
+
+describe( 'useCopyToClipboard', () => {
+	const TestComponent = ( { text, onSuccess }: TestComponentProps ) => {
+		const ref = useCopyToClipboard( text, onSuccess );
+		return <button ref={ ref }>Copy</button>;
+	};
+
+	it( 'should copy text on click', async () => {
+		const user = userEvent.setup();
+		render( <TestComponent text="test text" /> );
+
+		const writeTextMock = jest
+			.spyOn( navigator.clipboard, 'writeText' )
+			.mockResolvedValue();
+
+		await user.click( screen.getByRole( 'button' ) );
+
+		expect( writeTextMock ).toHaveBeenCalledTimes( 1 );
+		expect( writeTextMock ).toHaveBeenCalledWith( 'test text' );
+	} );
+
+	it( 'should call onSuccess when copy succeeds', async () => {
+		const user = userEvent.setup();
+		const onSuccess = jest.fn();
+		render( <TestComponent text="test text" onSuccess={ onSuccess } /> );
+
+		await user.click( screen.getByRole( 'button' ) );
+
+		expect( onSuccess ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should call onSuccess when copy empty text', async () => {
+		const user = userEvent.setup();
+		const onSuccess = jest.fn();
+		render( <TestComponent text="" onSuccess={ onSuccess } /> );
+
+		await user.click( screen.getByRole( 'button' ) );
+
+		expect( onSuccess ).toHaveBeenCalledTimes( 1 );
+	} );
+
+	it( 'should not call onSuccess when copy fails', async () => {
+		const user = userEvent.setup();
+		const onSuccess = jest.fn();
+		render( <TestComponent text="test text" onSuccess={ onSuccess } /> );
+
+		jest.spyOn( navigator.clipboard, 'writeText' ).mockRejectedValue(
+			new Error()
+		);
+
+		await user.click( screen.getByRole( 'button' ) );
+
+		expect( onSuccess ).not.toHaveBeenCalled();
+	} );
+
+	it( 'should not call onSuccess after unmount', async () => {
+		let resolvePromise: () => void;
+		const delayedPromise = new Promise< void >( ( resolve ) => {
+			resolvePromise = resolve;
+		} );
+		jest.spyOn( navigator.clipboard, 'writeText' ).mockReturnValue(
+			delayedPromise
+		);
+
+		const user = userEvent.setup();
+		const onSuccess = jest.fn();
+		const { unmount } = render(
+			<TestComponent text="test" onSuccess={ onSuccess } />
+		);
+
+		await user.click( screen.getByRole( 'button' ) );
+		unmount();
+
+		await act( async () => {
+			resolvePromise();
+			await new Promise( ( resolve ) => setTimeout( resolve, 0 ) );
+		} );
+
+		expect( onSuccess ).not.toHaveBeenCalled();
+	} );
+} );
