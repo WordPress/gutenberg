1812aa7c5bf4a2e9d4d6d503c38a4184a79157f5
Jorge Costa <jorge.costa@developer.pt>
2026-02-23 21:39:23 +0000

    fix open ai logo

--- routes/connections-home/default-connectors.tsx
+++ routes/connections-home/default-connectors.tsx
@@ -15,8 +15,8 @@ import { __ } from '@wordpress/i18n';
 // OpenAI logo as inline SVG
 const OpenAILogo = () => (
 	<svg
-		width="40"
-		height="40"
+		width="32"
+		height="32"
 		viewBox="0 0 24 24"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
