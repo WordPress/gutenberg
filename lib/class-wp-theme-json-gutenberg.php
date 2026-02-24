2dbc144c37bb5dd5fd37b092fd74983360f1be5d
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2026-02-24 11:32:16 +0100

    Add background gradient support to style engine and global styles output
    - Add gradient property to BackgroundStyle type definition
    - Update JS style engine to combine gradient and backgroundImage into
      a comma-separated background-image CSS value
    - Add gradient definition to PHP style engine class
    - Update compute_style_properties in class-wp-theme-json-gutenberg.php
      to collect both gradient and backgroundImage before passing to the
      style engine for combined background-image CSS output

--- lib/class-wp-theme-json-gutenberg.php
+++ lib/class-wp-theme-json-gutenberg.php
@@ -2560,12 +2560,21 @@ class WP_Theme_JSON_Gutenberg {
 			 * For an uploaded image (images with a database ID), apply size and position
 			 * defaults equal to those applied in block supports in lib/background.php.
 			 */
-			if ( 'background-image' === $css_property && ! empty( $value ) ) {
-				$background_styles = gutenberg_style_engine_get_styles(
-					array( 'background' => array( 'backgroundImage' => $value ) )
-				);
-
-				$value = $background_styles['declarations'][ $css_property ];
+			if ( 'background-image' === $css_property ) {
+				$background_image_input = array();
+				if ( ! empty( $value ) ) {
+					$background_image_input['backgroundImage'] = $value;
+				}
+				$gradient_value = $styles['background']['gradient'] ?? null;
+				if ( ! empty( $gradient_value ) ) {
+					$background_image_input['gradient'] = $gradient_value;
+				}
+				if ( ! empty( $background_image_input ) ) {
+					$background_styles = gutenberg_style_engine_get_styles(
+						array( 'background' => $background_image_input )
+					);
+					$value = $background_styles['declarations'][ $css_property ] ?? null;
+				}
 			}
 			if ( empty( $value ) && static::ROOT_BLOCK_SELECTOR !== $selector && ! empty( $styles['background']['backgroundImage']['id'] ) ) {
 				if ( 'background-size' === $css_property ) {
