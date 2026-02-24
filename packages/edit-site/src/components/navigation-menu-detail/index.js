4e51612c38d3f43798ae1d57ccaf7114b601ef36
Dave Smith <getdavemail@gmail.com>
2026-02-24 12:48:09 +0000

    more progress

--- packages/edit-site/src/components/navigation-menu-detail/index.js
+++ packages/edit-site/src/components/navigation-menu-detail/index.js
@@ -5,6 +5,7 @@ import { Page } from '@wordpress/admin-ui';
 import { __ } from '@wordpress/i18n';
 import { useMemo } from '@wordpress/element';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
+import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 import { DataViews } from '@wordpress/dataviews';
 import { addQueryArgs } from '@wordpress/url';
 
@@ -14,8 +15,11 @@ import { addQueryArgs } from '@wordpress/url';
 import { TEMPLATE_PART_POST_TYPE, LAYOUT_GRID } from '../../utils/constants';
 import { unlock } from '../../lock-unlock';
 import useMenuUsedInTemplateParts from '../../hooks/use-menu-used-in-template-parts';
+import usePatternSettings from '../page-patterns/use-pattern-settings';
+import { previewField } from '../page-patterns/fields';
 
 const { useHistory, useLocation } = unlock( routerPrivateApis );
+const { ExperimentalBlockEditorProvider } = unlock( blockEditorPrivateApis );
 const EMPTY_ARRAY = [];
 
 const defaultLayouts = { grid: {} };
@@ -24,9 +28,11 @@ const DEFAULT_VIEW = {
 	type: LAYOUT_GRID,
 	sort: { field: 'title', direction: 'asc' },
 	titleField: 'title',
+	mediaField: 'preview',
 };
 
 const TEMPLATE_PART_FIELDS = [
+	previewField,
 	{
 		id: 'title',
 		header: __( 'Title' ),
@@ -66,6 +72,7 @@ function useNavigateToTemplatePart( menuId ) {
 export default function NavigationMenuDetail( { menuId } ) {
 	const { templateParts, isResolving } = useMenuUsedInTemplateParts( menuId );
 	const navigateToTemplatePart = useNavigateToTemplatePart( menuId );
+	const settings = usePatternSettings();
 
 	const view = useMemo( () => DEFAULT_VIEW, [] );
 
@@ -84,22 +91,27 @@ export default function NavigationMenuDetail( { menuId } ) {
 	const data = templateParts ?? EMPTY_ARRAY;
 
 	return (
-		<Page title={ __( 'Active Menu locations' ) }>
-			<DataViews
-				data={ data }
-				fields={ TEMPLATE_PART_FIELDS }
-				view={ view }
-				onChangeView={ () => {} }
-				actions={ [ editInContextAction ] }
-				isLoading={ isResolving }
-				paginationInfo={ { totalItems: data.length, totalPages: 1 } }
-				getItemId={ getItemId }
-				defaultLayouts={ defaultLayouts }
-				isItemClickable={ () => true }
-				onClickItem={ ( item ) => {
-					navigateToTemplatePart( item.id );
-				} }
-			/>
-		</Page>
+		<ExperimentalBlockEditorProvider settings={ settings }>
+			<Page title={ __( 'Active Menu locations' ) }>
+				<DataViews
+					data={ data }
+					fields={ TEMPLATE_PART_FIELDS }
+					view={ view }
+					onChangeView={ () => {} }
+					actions={ [ editInContextAction ] }
+					isLoading={ isResolving }
+					paginationInfo={ {
+						totalItems: data.length,
+						totalPages: 1,
+					} }
+					getItemId={ getItemId }
+					defaultLayouts={ defaultLayouts }
+					isItemClickable={ () => true }
+					onClickItem={ ( item ) => {
+						navigateToTemplatePart( item.id );
+					} }
+				/>
+			</Page>
+		</ExperimentalBlockEditorProvider>
 	);
 }
