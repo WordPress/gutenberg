1e17207e34e17dae1ecbaf1b6dc1a68cfe0efd6a
Jorge Costa <jorge.costa@developer.pt>
2026-02-25 19:26:55 +0000

    Update lib/experimental/connectors/default-connectors.php
    Co-authored-by: Weston Ruter <weston@ruter.net>

--- lib/experimental/connectors/default-connectors.php
+++ lib/experimental/connectors/default-connectors.php
@@ -152,7 +152,7 @@ function _gutenberg_mask_openai_api_key( string $value ): string {
  * @return string The value to persist.
  */
 function _gutenberg_validate_openai_api_key_on_save( string $value, string $old_value ): string {
-	if ( empty( $value ) ) {
+	if ( '' === $value ) {
 		return $value;
 	}
 	$valid = _gutenberg_is_api_key_valid( $value, 'openai' );
