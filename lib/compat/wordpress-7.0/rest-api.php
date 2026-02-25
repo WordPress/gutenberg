49360dee528fbe896611f09e117de2539ec34577
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2026-02-25 21:26:42 +0900

    Improve description
    Co-authored-by: Dave Smith <getdavemail@gmail.com>

--- lib/compat/wordpress-7.0/rest-api.php
+++ lib/compat/wordpress-7.0/rest-api.php
@@ -65,7 +65,7 @@ function gutenberg_register_overlay_template_part_area( $areas ) {
 		'area'        => 'navigation-overlay',
 		'label'       => __( 'Navigation Overlay', 'gutenberg' ),
 		'description' => __(
-			'The Navigation Overlay template defines a full-screen overlay area that typically contains navigation links and can be toggled on and off.'
+			'The Navigation Overlay template defines a overlay area that typically contains navigation links and can be toggled open and closed.'
 		),
 		'icon'        => 'navigation-overlay',
 		'area_tag'    => 'div',
