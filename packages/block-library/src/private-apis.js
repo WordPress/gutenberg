fbc0fb7006acd9adf85b5e4db5de4048358d3ebe
Jerry Jones <jones.jeremydavid@gmail.com>
2026-03-03 10:44:52 -0600

    Refactor to use NavigationLinkUI

--- packages/block-library/src/private-apis.js
+++ packages/block-library/src/private-apis.js
@@ -3,11 +3,7 @@
  */
 import { default as BlockKeyboardShortcuts } from './block-keyboard-shortcuts';
 import { NAVIGATION_OVERLAY_TEMPLATE_PART_AREA } from './navigation/constants';
-import {
-	LinkUI,
-	updateAttributes,
-	useEntityBinding,
-} from './navigation-link/shared';
+import { NavigationLinkUI } from './navigation/edit/navigation-link-ui';
 import { lock } from './lock-unlock';
 
 /**
@@ -17,7 +13,5 @@ export const privateApis = {};
 lock( privateApis, {
 	BlockKeyboardShortcuts,
 	NAVIGATION_OVERLAY_TEMPLATE_PART_AREA,
-	LinkUI,
-	updateAttributes,
-	useEntityBinding,
+	NavigationLinkUI,
 } );
