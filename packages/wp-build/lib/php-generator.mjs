521eb9ca0a48a280da8414b352e5cf74c5950aa7
Jonathan Desrosiers <359867+desrosj@users.noreply.github.com>
2026-03-06 09:32:09 -0500

    Allow plugin name to be overridden.
    This allows the plugin name setting to be overridden more easily so that GitHub Actions can change the behavior of the build package.

--- packages/wp-build/lib/php-generator.mjs
+++ packages/wp-build/lib/php-generator.mjs
@@ -27,7 +27,12 @@ export async function getPhpReplacements( rootDir, baseUrlExpression ) {
 		throw new Error( 'Could not read root package.json' );
 	}
 
-	const name = process.env.npm_package_config_WP_PLUGIN_NAME || 'gutenberg';
+	const name =
+		process.env.WP_PLUGIN_NAME ??
+		process.env.npm_package_config_WP_PLUGIN_NAME ??
+		// @ts-expect-error specific override to package.json
+		rootPackageJson.wpPlugin?.name ??
+		'gutenberg';
 	const version = rootPackageJson.version;
 
 	return {
