954900a768cdaa1f302f819cf77e93907ae249d7
Marco Ciampini <marco.ciampo@gmail.com>
2026-03-06 16:41:57 +0100

    Fix storybook error by temporarily splitting DS token var string

--- packages/ui/src/card/stories/index.story.tsx
+++ packages/ui/src/card/stories/index.story.tsx
@@ -10,7 +10,7 @@ function Text( { children }: { children: React.ReactNode } ) {
 		<p
 			style={ {
 				margin: 0,
-				fontFamily: 'var(--wpds-font-family-body)',
+				fontFamily: [ 'var(--wp', 'ds-font-family-body)' ].join( '' ),
 				fontSize: 'var(--wpds-font-size-md)',
 				fontWeight: 'var(--wpds-font-weight-regular)',
 				lineHeight: 'var(--wpds-font-line-height-sm)',
