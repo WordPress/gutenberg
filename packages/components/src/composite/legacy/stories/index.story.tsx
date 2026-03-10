9a8652f5acfb0f230ddbd66e562c3d3556834c23
jasmussen <joen@git.wordpress.org>
2026-03-10 15:35:03 +0100

    Storybook: Rename "Components (Deprecated)" to "Deprecated"

--- packages/components/src/composite/legacy/stories/index.story.tsx
+++ packages/components/src/composite/legacy/stories/index.story.tsx
@@ -15,7 +15,7 @@ import {
 import { UseCompositeStatePlaceholder, transform } from './utils';
 
 const meta: Meta< typeof UseCompositeStatePlaceholder > = {
-	title: 'Components (Deprecated)/Composite (Unstable)',
+	title: 'Deprecated/Composite (Unstable)',
 	id: 'components-composite-unstable',
 	component: UseCompositeStatePlaceholder,
 	subcomponents: {
