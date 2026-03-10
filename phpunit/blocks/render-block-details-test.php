2df734422ca6c65bc10b2df5638d01fc5a716dad
Weston Ruter <weston@ruter.net>
2026-03-09 18:48:18 -0700

    Add test case to preserve fetchpriority=high in expanded Details block

--- phpunit/blocks/render-block-details-test.php
+++ phpunit/blocks/render-block-details-test.php
@@ -50,4 +50,23 @@ class Tests_Blocks_Render_Details extends WP_UnitTestCase {
 		$this->assertTrue( $processor->next_tag( 'IMG' ) );
 		$this->assertNotSame( 'low', $processor->get_attribute( 'fetchpriority' ) );
 	}
+
+	/**
+	 * @covers ::block_core_details_set_img_fetchpriority_low
+	 */
+	public function test_should_preserve_fetchpriority_high_on_img_in_expanded_details_block(): void {
+		$details_block = <<<'BLOCK_CONTENT'
+			<!-- wp:details {"showContent":true} -->
+			<details class="wp-block-details" open><summary>Expanded</summary><!-- wp:image {"linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="https://example.com/image.jpg" fetchpriority="high" alt="" /></figure>
+			<!-- /wp:image --></details>
+			<!-- /wp:details -->
+		BLOCK_CONTENT;
+
+		$rendered_block = do_blocks( $details_block );
+
+		$processor = new WP_HTML_Tag_Processor( $rendered_block );
+		$this->assertTrue( $processor->next_tag( 'IMG' ) );
+		$this->assertSame( 'high', $processor->get_attribute( 'fetchpriority' ) );
+	}
 }
