cfef494fd03cb41f2e16f31f331424c9b5ab8f80
Adam Silverstein <adam@earthbound.com>
2026-03-10 20:39:46 -0700

    Update lib/media/class-gutenberg-rest-attachments-controller.php
    Co-authored-by: Weston Ruter <weston@ruter.net>

--- lib/media/class-gutenberg-rest-attachments-controller.php
+++ lib/media/class-gutenberg-rest-attachments-controller.php
@@ -76,7 +76,7 @@ class Gutenberg_REST_Attachments_Controller extends WP_REST_Attachments_Controll
 
 		$result = parent::create_item_permissions_check( $request );
 
-		if ( ! $request['generate_sub_sizes'] ) {
+		if ( false === $request['generate_sub_sizes'] ) {
 			remove_filter( 'wp_prevent_unsupported_mime_type_uploads', '__return_false' );
 		}
 
