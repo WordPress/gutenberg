ecc6ed8ba886afc200ed8b6f2443a390711ae232
hbhalodia <hit.bhalodia@rtcamp.com>
2026-03-12 14:27:27 +0530

    Add reset icon for image block and update style to match backgroundImage global styles

--- packages/block-editor/src/hooks/block-fields/media/index.js
+++ packages/block-editor/src/hooks/block-fields/media/index.js
@@ -13,6 +13,7 @@ import {
 	image as imageIcon,
 	media as mediaIcon,
 	video as videoIcon,
+	reset as resetIcon,
 } from '@wordpress/icons';
 
 /**
@@ -130,103 +131,126 @@ export default function Media( { data, field, onChange, config = {} } ) {
 
 	return (
 		<MediaUploadCheck>
-			<MediaReplaceFlow
-				className="block-editor-content-only-controls__media-replace-flow"
-				allowedTypes={ allowedTypes }
-				mediaId={ id }
-				mediaURL={ url }
-				multiple={ multiple }
-				popoverProps={ popoverProps }
-				onReset={ () => {
-					onChange(
-						field.setValue( {
-							item: data,
-							value: {},
-						} )
-					);
-				} }
-				{ ...( useFeaturedImage && {
-					useFeaturedImage: !! value?.featuredImage,
-					onToggleFeaturedImage: () => {
-						onChange(
-							field.setValue( {
-								item: data,
-								value: {
-									featuredImage: ! value?.featuredImage,
-								},
-							} )
-						);
-					},
-				} ) }
-				onSelect={ ( selectedMedia ) => {
-					if ( selectedMedia.id && selectedMedia.url ) {
-						const newValue = {
-							...selectedMedia,
-							mediaType: selectedMedia.media_type,
-						};
+			<div className="block-editor-content-only-controls">
+				<div className="block-editor-content-only-controls__media-replace">
+					<MediaReplaceFlow
+						className="block-editor-content-only-controls__media-replace-flow"
+						allowedTypes={ allowedTypes }
+						mediaId={ id }
+						mediaURL={ url }
+						multiple={ multiple }
+						popoverProps={ popoverProps }
+						onReset={ () => {
+							onChange(
+								field.setValue( {
+									item: data,
+									value: {},
+								} )
+							);
+						} }
+						{ ...( useFeaturedImage && {
+							useFeaturedImage: !! value?.featuredImage,
+							onToggleFeaturedImage: () => {
+								onChange(
+									field.setValue( {
+										item: data,
+										value: {
+											featuredImage:
+												! value?.featuredImage,
+										},
+									} )
+								);
+							},
+						} ) }
+						onSelect={ ( selectedMedia ) => {
+							if ( selectedMedia.id && selectedMedia.url ) {
+								const newValue = {
+									...selectedMedia,
+									mediaType: selectedMedia.media_type,
+								};
 
-						// Turn off featured image when manually selecting media
-						if ( useFeaturedImage ) {
-							newValue.featuredImage = false;
-						}
+								// Turn off featured image when manually selecting media
+								if ( useFeaturedImage ) {
+									newValue.featuredImage = false;
+								}
 
-						onChange(
-							field.setValue( {
-								item: data,
-								value: newValue,
-							} )
-						);
-					}
-				} }
-				renderToggle={ ( buttonProps ) => (
+								onChange(
+									field.setValue( {
+										item: data,
+										value: newValue,
+									} )
+								);
+							}
+						} }
+						renderToggle={ ( buttonProps ) => (
+							<Button
+								__next40pxDefaultSize
+								className="block-editor-content-only-controls__media"
+								{ ...buttonProps }
+							>
+								<Grid
+									rowGap={ 0 }
+									columnGap={ 8 }
+									templateColumns="24px 1fr"
+									className="block-editor-content-only-controls__media-row"
+								>
+									{ url && (
+										<>
+											<MediaThumbnail
+												attachment={ attachment }
+												field={ field }
+												data={ data }
+												config={ config }
+											/>
+											<span className="block-editor-content-only-controls__media-title">
+												{
+													// TODO - truncate long titles or url smartly (e.g. show filename).
+													attachment?.title?.raw &&
+													attachment?.title?.raw !==
+														''
+														? attachment?.title?.raw
+														: url
+												}
+											</span>
+										</>
+									) }
+									{ ! url && (
+										<>
+											<span
+												className="block-editor-content-only-controls__media-placeholder"
+												style={ {
+													width: '24px',
+													height: '24px',
+												} }
+											/>
+											<span className="block-editor-content-only-controls__media-title">
+												{ chooseItemLabel }
+											</span>
+										</>
+									) }
+								</Grid>
+							</Button>
+						) }
+					/>
+				</div>
+				{ url && (
 					<Button
 						__next40pxDefaultSize
-						className="block-editor-content-only-controls__media"
-						{ ...buttonProps }
-					>
-						<Grid
-							rowGap={ 0 }
-							columnGap={ 8 }
-							templateColumns="24px 1fr"
-							className="block-editor-content-only-controls__media-row"
-						>
-							{ url && (
-								<>
-									<MediaThumbnail
-										attachment={ attachment }
-										field={ field }
-										data={ data }
-										config={ config }
-									/>
-									<span className="block-editor-content-only-controls__media-title">
-										{
-											// TODO - truncate long titles or url smartly (e.g. show filename).
-											attachment?.title?.raw &&
-											attachment?.title?.raw !== ''
-												? attachment?.title?.raw
-												: url
-										}
-									</span>
-								</>
-							) }
-							{ ! url && (
-								<>
-									<span
-										className="block-editor-content-only-controls__media-placeholder"
-										style={ {
-											width: '24px',
-											height: '24px',
-										} }
-									/>
-									<span className="block-editor-content-only-controls__media-title">
-										{ chooseItemLabel }
-									</span>
-								</>
-							) }
-						</Grid>
-					</Button>
+						label={ __( 'Reset' ) }
+						className="block-editor-content-only-controls__media-reset"
+						size="small"
+						icon={ resetIcon }
+						onClick={ () => {
+							onChange(
+								field.setValue( {
+									item: data,
+									value: {},
+								} )
+							);
+						} }
+					/>
 				) }
-			/>
+			</div>
 		</MediaUploadCheck>
 	);
 }
