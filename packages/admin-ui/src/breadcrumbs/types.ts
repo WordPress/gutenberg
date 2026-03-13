fb0f1113bc0a19eaf3917019fa535dff248da0c7
Marco Ciampini <marco.ciampo@gmail.com>
2026-03-13 16:08:29 +0100

    admin-ui/Breadcrumbs: enforce "to" prop on all but last item

--- packages/admin-ui/src/breadcrumbs/types.ts
+++ packages/admin-ui/src/breadcrumbs/types.ts
@@ -1,4 +1,16 @@
-export interface BreadcrumbItem {
+export interface BreadcrumbLinkItem {
+	/**
+	 * The label text for the breadcrumb item.
+	 */
+	label: string;
+
+	/**
+	 * The router path that the breadcrumb item should link to.
+	 */
+	to: string;
+}
+
+export interface BreadcrumbCurrentItem {
 	/**
 	 * The label text for the breadcrumb item.
 	 */
@@ -11,12 +23,15 @@ export interface BreadcrumbItem {
 	to?: string;
 }
 
+export type BreadcrumbItem = BreadcrumbLinkItem | BreadcrumbCurrentItem;
+
 export interface BreadcrumbsProps extends React.HTMLAttributes< HTMLElement > {
 	/**
 	 * An array of items to display in the breadcrumb trail.
-	 * The last item is considered the current item.
+	 * The last item is considered the current item and has an optional `to` prop.
+	 * All preceding items require a `to` prop.
 	 */
-	items: BreadcrumbItem[];
+	items: [] | [ ...BreadcrumbLinkItem[], BreadcrumbCurrentItem ];
 	/**
 	 * A boolean to show/hide the current item in the trail.
 	 * Note that when `false` the current item is only visually hidden.
