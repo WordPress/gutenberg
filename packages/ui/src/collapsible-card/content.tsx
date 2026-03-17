074865744f5fba79ddcf0dda358f3e0416b9ed83
Marco Ciampini <marco.ciampo@gmail.com>
2026-03-17 18:32:02 +0100

    Expose hiddenUntilFound on the content subcomponent

--- packages/ui/src/collapsible-card/content.tsx
+++ packages/ui/src/collapsible-card/content.tsx
@@ -11,14 +11,14 @@ import type { ContentProps } from './types';
  */
 export const Content = forwardRef< HTMLDivElement, ContentProps >(
 	function CollapsibleCardContent(
-		{ className, render, children, ...restProps },
+		{ className, render, children, hiddenUntilFound = true, ...restProps },
 		ref
 	) {
 		return (
 			<Collapsible.Panel
 				ref={ ref }
 				className={ clsx( styles.content, className ) }
-				hiddenUntilFound
+				hiddenUntilFound={ hiddenUntilFound }
 				{ ...restProps }
 			>
 				<Card.Content
