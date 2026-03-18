afc01c39d3f569c296ff6cf47766485b6121cdb0
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2026-03-19 04:13:02 +1100

    Real Time Collaboration: Introduce filters for the polling intervals. (#76518)
    * Introduce filters for the RTC polling intervals.
    
    Introduces four filters for the polling intervals in the real time collaboration client:
    
    * `sync.pollingManager.pollingIntervalNoCollaborators` the interval in milliseconds when there are no collaborators in the room
    * `sync.pollingManager.pollingIntervalWithCollaborators` the interval in milliseconds when there are collaborators in the room
    
    * Remove background tab interval filter
    
    * Match filter to constant name
    
    ---------
    
    Co-authored-by: peterwilsoncc <peterwilsoncc@git.wordpress.org>
    Co-authored-by: chriszarate <czarate@git.wordpress.org>

--- packages/sync/src/providers/http-polling/config.ts
+++ packages/sync/src/providers/http-polling/config.ts
@@ -1,8 +1,22 @@
+/**
+ * WordPress dependencies
+ */
+import { applyFilters } from '@wordpress/hooks';
+
 export const DEFAULT_CLIENT_LIMIT_PER_ROOM = 3;
 export const MAX_ERROR_BACKOFF_IN_MS = 30 * 1000; // 30 seconds
 export const MAX_UPDATE_SIZE_IN_BYTES = 1 * 1024 * 1024; // 1 MB
-export const POLLING_INTERVAL_IN_MS = 1000; // 1 second or 1000 milliseconds
-export const POLLING_INTERVAL_WITH_COLLABORATORS_IN_MS = 250; // 250 milliseconds
+
+export const POLLING_INTERVAL_IN_MS = applyFilters(
+	'sync.pollingManager.pollingInterval',
+	1000 // 1 second or 1000 milliseconds
+) as number;
+
+export const POLLING_INTERVAL_WITH_COLLABORATORS_IN_MS = applyFilters(
+	'sync.pollingManager.pollingIntervalWithCollaborators',
+	250 // 250 milliseconds
+) as number;
+
 // Must be less than the server-side AWARENESS_TIMEOUT (30 s) to avoid
 // false disconnects when the tab is in the background.
 export const POLLING_INTERVAL_BACKGROUND_TAB_IN_MS = 25 * 1000; // 25 seconds
