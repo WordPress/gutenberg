5bb291c557c7386d51a8a0fe3ce117d009f3fa37
Manzoor Wani <manzoorwani.jk@gmail.com>
2026-03-19 04:50:43 +0530

    Fix CI failures: ignore example-* dirs and fix RuleTester config format
    Add example-*/** to global ignores in eslint.config.mjs since these
    directories are generated by @wordpress/create-block during CI build
    tests and should not be linted.
    
    Convert remaining no-dom-globals-in-* test files from eslintrc format
    (top-level parser/parserOptions) to flat config format
    (languageOptions.parser as object reference).

--- packages/eslint-plugin/rules/__tests__/no-dom-globals-in-module-scope.js
+++ packages/eslint-plugin/rules/__tests__/no-dom-globals-in-module-scope.js
@@ -93,11 +93,13 @@ ruleTester.run( 'no-dom-globals-in-module-scope', rule, {
 
 // TypeScript-specific tests for shouldSkipReference.
 const tsRuleTester = new RuleTester( {
-	parser: require.resolve( '@typescript-eslint/parser' ),
-	parserOptions: {
+	languageOptions: {
+		parser: require( '@typescript-eslint/parser' ),
 		ecmaVersion: 2020,
 		sourceType: 'module',
-		ecmaFeatures: { jsx: true },
+		parserOptions: {
+			ecmaFeatures: { jsx: true },
+		},
 	},
 } );
 
--- packages/eslint-plugin/rules/__tests__/no-dom-globals-in-react-cc-render.js
+++ packages/eslint-plugin/rules/__tests__/no-dom-globals-in-react-cc-render.js
@@ -54,11 +54,13 @@ ruleTester.run( 'no-dom-globals-in-react-cc-render', rule, {
 
 // TypeScript-specific tests for shouldSkipReference.
 const tsRuleTester = new RuleTester( {
-	parser: require.resolve( '@typescript-eslint/parser' ),
-	parserOptions: {
+	languageOptions: {
+		parser: require( '@typescript-eslint/parser' ),
 		ecmaVersion: 2020,
 		sourceType: 'module',
-		ecmaFeatures: { jsx: true },
+		parserOptions: {
+			ecmaFeatures: { jsx: true },
+		},
 	},
 } );
 
