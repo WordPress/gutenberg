62d38ea7815ef4df87108ae78126802602325def
Carolina Nymark <hi@themesbycarolina.com>
2026-03-21 09:18:56 +0100

    Enqueue the button block style
    Since we're not rendering a core/button block, enqueue the button block style.
    Also:
    Use wp_theme_get_element_class_name( 'button' ) instead of hard coding wp-element-button directly.
    If for any reason, the button style is not registered, then don't try to enqueue it.

--- lib/compat/wordpress-7.1/password-form.php
+++ lib/compat/wordpress-7.1/password-form.php
@@ -12,17 +12,23 @@ function gutenberg_password_form_7_1( $content ) {
 	}
 
 	$processor = new WP_HTML_Tag_Processor( $content );
-
 	while ( $processor->next_tag( array( 'tag_name' => 'input' ) ) ) {
-		if ( 'submit' !== $processor->get_attribute( 'type' ) ) {
+		if ( 'submit' !== strtolower( (string) $processor->get_attribute( 'type' ) ) ) {
 			continue;
 		}
 
 		$processor->add_class( 'wp-block-button__link' );
-		$processor->add_class( 'wp-element-button' );
+		$processor->add_class( wp_theme_get_element_class_name( 'button' ) );
 		break;
 	}
 
-	return $processor->get_updated_html();
+	$updated_html = $processor->get_updated_html();
+
+	// Since we're not rendering a core/button block, enqueue the button block style.
+	if ( wp_style_is( 'wp-block-button', 'registered' ) ) {
+		wp_enqueue_style( 'wp-block-button' );
+	}
+
+	return $updated_html;
 }
-add_filter( 'the_password_form', 'gutenberg_password_form_7_1' );
+add_filter( 'the_password_form', 'gutenberg_password_form_7_1', PHP_INT_MAX );
