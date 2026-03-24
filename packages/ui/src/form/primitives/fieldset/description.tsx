67a1a7f0e89f6267c1efb1e352cdd6a484be376c
Lena Morita <lena@jaguchi.com>
2026-03-24 21:21:00 +0900

    Fieldset: Add defense styles

--- packages/ui/src/form/primitives/fieldset/description.tsx
+++ packages/ui/src/form/primitives/fieldset/description.tsx
@@ -4,6 +4,7 @@ import { forwardRef, useEffect, useId } from '@wordpress/element';
 import fieldStyles from '../../../utils/css/field.module.css';
 import { useFieldsetContext } from './context';
 import type { FieldsetDescriptionProps } from './types';
+import defenseStyles from '../../../utils/css/global-css-defense.module.css';
 
 export const FieldsetDescription = forwardRef<
 	HTMLParagraphElement,
@@ -27,7 +28,14 @@ export const FieldsetDescription = forwardRef<
 		render,
 		ref,
 		props: mergeProps< 'p' >(
-			{ className: clsx( fieldStyles.description, className ), id },
+			{
+				className: clsx(
+					defenseStyles.p,
+					fieldStyles.description,
+					className
+				),
+				id,
+			},
 			restProps
 		),
 	} );
