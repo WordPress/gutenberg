553849026daae139d89364cf4e1114212b278504
Jon Surrell <sirreal@users.noreply.github.com>
2026-03-26 10:36:41 +0100

    Improve css string documentation

--- packages/global-styles-ui/src/font-library/utils/index.ts
+++ packages/global-styles-ui/src/font-library/utils/index.ts
@@ -97,6 +97,29 @@ export function mergeFontFamilies(
 /**
  * Construct a quoted CSS String from a plain JavaScript value.
  *
+ * A plain JavaScript string will be transformed to a quoted CSS string suitable for interpolation
+ * in CSS text.
+ *
+ * The input is not trimmed. Trim should be performed before calling this method, if desired.
+ *
+ * @example
+ * ```js
+ * const fontName = "A font's <problem>";
+ * const fontFaceRule = `@font-face { font-family: ${createCssString(fontName)}; }`
+ * ```
+ *
+ * `fontFaceRule` will have the value `'@font-face { font-family: "A font\\27 s \\3C problem\\3E "; }'`.
+ * The browser will parse this rule as equivalent to: `@font-face { font-family: "A font's <problem>"; }`.
+ *
+ * Some characters that are valid in CSS strings are escaped to avoid problems with subsequent
+ * processing that may not correctly parse CSS:
+ *
+ * - "<", ">", and "&" are replaced to prevent issues with KSES and other sanitization that
+ *   is confused by HTML-like text.
+ * - CSS syntax characters are replaced to prevent issues where CSS may be processed by simple
+ *   string splits or search and replaces: "',;{}
+ * - Plain "\" backslashes are escaped.
+ *
  * @param value The JavaScript string to serialize as a quoted CSS string.
  * @return A quoted, CSS-safe font-family string.
  */
@@ -105,14 +128,6 @@ export function createCssString( value: string ): string {
 		/*
 		 * CSS Unicode escaping for problematic characters.
 		 * https://www.w3.org/TR/css-syntax-3/#escaping
-		 *
-		 * These characters are not required by CSS but may be problematic in WordPress:
-		 *
-		 * - Normalize and replace newlines. https://www.w3.org/TR/css-syntax-3/#input-preprocessing
-		 * - "<", ">", and "&" are replaced to prevent issues with KSES and other sanitization that
-		 *   is confused by HTML-like text.
-		 * - CSS syntax characters are replaced to prevent issues where CSS may be processed by simple
-		 *   string splits or search and replaces: "',;{}
 		 *
 		 * Note that the Unicode escape sequences are used rather than backslash-escaping so the
 		 * problematic characters are removed completely. CSS Unicode escapes are formed by a
@@ -125,7 +140,7 @@ export function createCssString( value: string ): string {
 		// Pre-processing replaces NULLs and some newlines. Replace and escape as necessary.
 		.replaceAll( '\0', '\uFFFD' )
 
-		// Carriage return + line feed must be the first newline replacement.
+		// Normalize and replace newlines. https://www.w3.org/TR/css-syntax-3/#input-preprocessing
 		.replaceAll( '\r\n', '\\A ' )
 		.replaceAll( '\r', '\\A ' )
 		.replaceAll( '\f', '\\A ' )
