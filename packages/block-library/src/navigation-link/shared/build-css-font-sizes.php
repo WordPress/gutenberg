c68b0d1c9c9da99e922ed6a1a87ddb9f919f7d5e
Ben Dwyer <ben@scruffian.com>
2026-03-28 10:34:26 +0900

    Apply suggestion from @scruffian

--- packages/block-library/src/navigation-link/shared/build-css-font-sizes.php
+++ packages/block-library/src/navigation-link/shared/build-css-font-sizes.php
@@ -9,7 +9,7 @@
  * Build an array with CSS classes and inline styles defining the font sizes
  * which will be applied to the navigation markup in the front-end.
  *
- * @since 5.9.0
+ * @since 7.1.0
  *
  * @param  array $context Navigation block context.
  * @return array Font size CSS classes and inline styles.
