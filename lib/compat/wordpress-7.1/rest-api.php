b30292f3735a634957fc6562ba4eacfe0f7b1850
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-03-31 18:27:34 +0900

    Restore code that was accidentally deleted

---
+++ lib/compat/wordpress-7.1/rest-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WordPress 7.1 compatibility functions for the Gutenberg
+ * editor plugin changes related to REST API.
+ *
+ * @package gutenberg
+ */
+
+/**
+ * Registers the View Config REST API routes.
+ */
+function gutenberg_register_view_config_controller_endpoints() {
+	$view_config_controller = new Gutenberg_REST_View_Config_Controller_7_1();
+	$view_config_controller->register_routes();
+}
+add_action( 'rest_api_init', 'gutenberg_register_view_config_controller_endpoints', PHP_INT_MAX );
--- lib/load.php
+++ lib/load.php
@@ -81,6 +81,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 
 	// WordPress 7.1 compat.
 	require __DIR__ . '/compat/wordpress-7.1/class-gutenberg-rest-view-config-controller-7-1.php';
+	require __DIR__ . '/compat/wordpress-7.1/rest-api.php';
 
 	// Plugin specific code.
 	require_once __DIR__ . '/class-wp-rest-global-styles-controller-gutenberg.php';
