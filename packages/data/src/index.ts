aba0d6637b1fff20608e4f808707aefa7223a78c
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-04-02 00:07:28 -0700

    Move deprecated type aliases back into types.ts
    The deprecated types (SelectFunction, DispatchFunction, DispatchReturn,
    UseSelectReturn, UseDispatchReturn) were moved from types.ts to a
    separate deprecated.ts file. This broke downstream consumers that
    deep-import from @wordpress/data/build-types/types.
    
    Move them back to types.ts to preserve the deep import paths. The
    types are marked with @deprecated JSDoc tags to guide consumers
    toward their replacements.

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -36,7 +36,6 @@ export { resolveSelect } from './resolve-select';
 export { suspendSelect } from './suspend-select';
 
 export type * from './types';
-export type * from './deprecated';
 
 /**
  * Object of available plugins to use with a registry.
