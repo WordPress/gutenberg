42517d89f686a3d3268385861bc4baefd88000c1
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2026-04-08 13:20:34 +1000

    Fix global styles and default control display for min height and width supports

--- packages/block-editor/src/hooks/dimensions.js
+++ packages/block-editor/src/hooks/dimensions.js
@@ -102,6 +102,10 @@ export function DimensionsPanel( { clientId, name, setAttributes, settings } ) {
 		'__experimentalDefaultControls',
 	] );
 	const defaultControls = {
+		// In the block inspector, minHeight and minWidth should not
+		// be shown by default unless the block explicitly opts in.
+		minHeight: false,
+		minWidth: false,
 		...defaultDimensionsControls,
 		...defaultSpacingControls,
 	};
