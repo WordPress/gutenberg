dfbed041e322ce06435e0a0d9cc96b4fbdd7aa46
Shreya Shrivastava <shreya2202shri@gmail.com>
2026-04-14 17:33:30 +0530

    refactor: simplify render by removing custom render prop

--- packages/ui/src/badge/badge.tsx
+++ packages/ui/src/badge/badge.tsx
@@ -1,4 +1,3 @@
-import { mergeProps } from '@base-ui/react';
 import clsx from 'clsx';
 import { forwardRef } from '@wordpress/element';
 import { type BadgeProps } from './types';
@@ -9,28 +8,19 @@ import { Text } from '../text';
  * A badge component for displaying labels with semantic intent.
  */
 export const Badge = forwardRef< HTMLSpanElement, BadgeProps >( function Badge(
-	{ children, intent = 'none', render, className, ...props },
+	{ intent = 'none', className, ...props },
 	ref
 ) {
 	return (
 		<Text
+			ref={ ref }
 			variant="body-sm"
-			render={
-				render ?? (
-					<span
-						ref={ ref }
-						{ ...mergeProps( props, {
-							className: clsx(
-								styles.badge,
-								styles[ `is-${ intent }-intent` ],
-								className
-							),
-						} ) }
-					/>
-				)
-			}
-		>
-			{ children }
-		</Text>
+			className={ clsx(
+				styles.badge,
+				styles[ `is-${ intent }-intent` ],
+				className
+			) }
+			{ ...props }
+		/>
 	);
 } );
