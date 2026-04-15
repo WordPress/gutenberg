285518940113645dcd296eeaa14e760b9ab798f8
Alec Geatches <alec.geatches@automattic.com>
2026-04-15 13:01:30 -0700

    Add guard around new clear_contributors() method

--- lib/compat/wordpress-7.0/collaboration.php
+++ lib/compat/wordpress-7.0/collaboration.php
@@ -261,7 +261,9 @@ if ( ! function_exists( 'gutenberg_clear_sync_contributors_on_save' ) ) {
 
 		$room    = 'postType/' . $post->post_type . ':' . $post_id;
 		$storage = new WP_Sync_Post_Meta_Storage();
-		$storage->clear_contributors( $room );
+		if ( method_exists( $storage, 'clear_contributors' ) ) {
+			$storage->clear_contributors( $room );
+		}
 	}
 	add_action( 'save_post', 'gutenberg_clear_sync_contributors_on_save', 10, 2 );
 }
