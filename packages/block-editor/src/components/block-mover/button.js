f7df3dd7b8fc82beeb4099df67f48bf284a57b13
Lakshyajeet Singh Goyal <lakshyajeet.goyal@rtcamp.com>
2026-04-23 15:37:33 +0530

    fix: block-mover tooltip for smaller viewports

--- packages/block-editor/src/components/block-mover/button.js
+++ packages/block-editor/src/components/block-mover/button.js
@@ -8,7 +8,7 @@ import clsx from 'clsx';
  */
 import { getBlockType } from '@wordpress/blocks';
 import { Button, VisuallyHidden } from '@wordpress/components';
-import { useInstanceId } from '@wordpress/compose';
+import { useInstanceId, useViewportMatch } from '@wordpress/compose';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { forwardRef } from '@wordpress/element';
 import { __, isRTL } from '@wordpress/i18n';
@@ -66,6 +66,7 @@ const BlockMoverButton = forwardRef(
 			? clientIds
 			: [ clientIds ];
 		const blocksCount = normalizedClientIds.length;
+		const isMobileViewport = useViewportMatch( 'small', '<' );
 
 		const {
 			blockType,
@@ -139,7 +140,9 @@ const BlockMoverButton = forwardRef(
 						orientation
 					) }
 					tooltipPosition={
-						direction === 'down' && orientation === 'vertical'
+						! isMobileViewport &&
+						direction === 'down' &&
+						orientation === 'vertical'
 							? 'bottom'
 							: 'top'
 					}
