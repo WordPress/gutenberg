25287b6b2253b881905e423dc0ee752fb5ffdb55
Damián Suárez <rdsuarez@gmail.com>
2026-04-23 10:32:47 -0400

    Grid: move styles to CSS modules follow @wordpress/ui pattern; drop SCSS+BEM; use logical properties

--- storybook/package-styles/config.js
+++ storybook/package-styles/config.js
@@ -19,8 +19,6 @@ import mediaFieldsLtr from '../package-styles/media-fields-ltr.lazy.scss?inline'
 import mediaFieldsRtl from '../package-styles/media-fields-rtl.lazy.scss?inline';
 import adminUiLtr from '../package-styles/admin-ui-ltr.lazy.scss?inline';
 import adminUiRtl from '../package-styles/admin-ui-rtl.lazy.scss?inline';
-import gridLtr from '../package-styles/grid-ltr.lazy.scss?inline';
-import gridRtl from '../package-styles/grid-rtl.lazy.scss?inline';
 import designTokens from '../package-styles/design-tokens.lazy.scss?inline';
 
 /**
@@ -84,8 +82,8 @@ const CONFIG = [
 	},
 	{
 		componentIdMatcher: /^grid-/,
-		ltr: [ designTokens, gridLtr ],
-		rtl: [ designTokens, gridRtl ],
+		ltr: [ designTokens ],
+		rtl: [ designTokens ],
 	},
 	{
 		componentIdMatcher: /^mediaeditor-/,
