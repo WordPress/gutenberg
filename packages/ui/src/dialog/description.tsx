d71e7bc3fe739b09d75ef86bd76740ef88615a4b
Marco Ciampini <marco.ciampo@gmail.com>
2026-04-24 15:20:34 +0200

    Uniform title and description styles across drawer/dialog/popover

--- packages/ui/src/dialog/description.tsx
+++ packages/ui/src/dialog/description.tsx
@@ -1,7 +1,6 @@
 import { Dialog as _Dialog } from '@base-ui/react/dialog';
 import { forwardRef } from '@wordpress/element';
 import { Text } from '../text';
-import styles from './style.module.css';
 import type { DescriptionProps } from './types';
 
 /**
@@ -16,7 +15,6 @@ const Description = forwardRef< HTMLParagraphElement, DescriptionProps >(
 				ref={ ref }
 				variant="body-md"
 				render={ <_Dialog.Description { ...props } /> }
-				className={ styles.description }
 			>
 				{ children }
 			</Text>
