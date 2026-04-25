9b1ae0dd7fd01064de28763feaa0e88abe6b479a
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-04-25 20:17:27 +0900

    GlobalStylesUI: Add reset action to Additional CSS screen
    Provide a way for users to clear custom CSS from the Additional CSS
    screen via an options menu, mirroring reset affordances elsewhere in
    Global Styles.

--- packages/global-styles-ui/src/screen-css.tsx
+++ packages/global-styles-ui/src/screen-css.tsx
@@ -2,7 +2,16 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { ExternalLink } from '@wordpress/components';
+import {
+	ExternalLink,
+	Button,
+	FlexItem,
+	FlexBlock,
+	__experimentalSpacer as Spacer,
+	privateApis as componentsPrivateApis,
+} from '@wordpress/components';
+import { Stack } from '@wordpress/ui';
+import { moreVertical } from '@wordpress/icons';
 // @ts-expect-error: Not typed yet.
 import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 
@@ -15,6 +24,7 @@ import { unlock } from './lock-unlock';
 
 // Access AdvancedPanel from block-editor private APIs
 const { AdvancedPanel: StylesAdvancedPanel } = unlock( blockEditorPrivateApis );
+const { Menu } = unlock( componentsPrivateApis );
 
 function ScreenCSS() {
 	// Get user-only styles (should not decode/encode to preserve raw CSS)
@@ -29,25 +39,58 @@ function ScreenCSS() {
 
 	return (
 		<>
-			<ScreenHeader
-				title={ __( 'Additional CSS' ) }
-				description={
-					<>
-						{ __(
-							'You can add custom CSS to further customize the appearance and layout of your site.'
-						) }
-						<br />
-						<ExternalLink
-							href={ __(
-								'https://developer.wordpress.org/advanced-administration/wordpress/css/'
-							) }
-							className="global-styles-ui-screen-css-help-link"
-						>
-							{ __( 'Learn more about CSS' ) }
-						</ExternalLink>
-					</>
-				}
-			/>
+			<Stack direction="row" justify="space-between" align="flex-start">
+				<FlexBlock>
+					<ScreenHeader
+						title={ __( 'Additional CSS' ) }
+						description={
+							<>
+								{ __(
+									'You can add custom CSS to further customize the appearance and layout of your site.'
+								) }
+								<br />
+								<ExternalLink
+									href={ __(
+										'https://developer.wordpress.org/advanced-administration/wordpress/css/'
+									) }
+									className="global-styles-ui-screen-css-help-link"
+								>
+									{ __( 'Learn more about CSS' ) }
+								</ExternalLink>
+							</>
+						}
+					/>
+				</FlexBlock>
+				<FlexItem>
+					<Spacer marginTop={ 3 } marginBottom={ 0 } paddingX={ 4 }>
+						<Menu>
+							<Menu.TriggerButton
+								render={
+									<Button
+										size="small"
+										icon={ moreVertical }
+										label={ __( 'CSS options' ) }
+									/>
+								}
+							/>
+							<Menu.Popover>
+								<Menu.Item
+									disabled={ ! style?.css }
+									onClick={ () => {
+										const { css: _css, ...rest } =
+											style ?? {};
+										setStyle( rest );
+									} }
+								>
+									<Menu.ItemLabel>
+										{ __( 'Reset' ) }
+									</Menu.ItemLabel>
+								</Menu.Item>
+							</Menu.Popover>
+						</Menu>
+					</Spacer>
+				</FlexItem>
+			</Stack>
 			<div className="global-styles-ui-screen-css">
 				<StylesAdvancedPanel
 					value={ style }
