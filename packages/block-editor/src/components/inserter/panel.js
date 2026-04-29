f6b1fac49ee758cb63daed47a6fd447d8a233763
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2026-04-29 13:20:18 +0530

    Block Editor: Migrate InserterPanel title to Text from @wordpress/ui

--- packages/block-editor/src/components/inserter/panel.js
+++ packages/block-editor/src/components/inserter/panel.js
@@ -2,14 +2,19 @@
  * WordPress dependencies
  */
 import { Icon } from '@wordpress/components';
+import { Text } from '@wordpress/ui';
 
 function InserterPanel( { title, icon, children } ) {
 	return (
 		<>
 			<div className="block-editor-inserter__panel-header">
-				<h2 className="block-editor-inserter__panel-title">
+				<Text
+					render={ <h2 /> }
+					variant="heading-sm"
+					className="block-editor-inserter__panel-title"
+				>
 					{ title }
-				</h2>
+				</Text>
 				<Icon icon={ icon } />
 			</div>
 			<div className="block-editor-inserter__panel-content">
