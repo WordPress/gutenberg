b43e869bf8a6adcc8181f0c3bf5f217ce75de9b6
adithya-naik <jatothadithyanaik@gmail.com>
2026-04-30 23:28:50 +0530

    Fix: Buttons block shows inserter picker when multiple allowed blocks are registered

--- packages/block-library/src/buttons/edit.js
+++ packages/block-library/src/buttons/edit.js
@@ -6,7 +6,11 @@ import clsx from 'clsx';
 /**
  * WordPress dependencies
  */
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	useInnerBlocksProps,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { store as blocksStore } from '@wordpress/blocks';
 
@@ -25,27 +29,34 @@ const DEFAULT_BLOCK = {
 	],
 };
 
-function ButtonsEdit( { attributes, className } ) {
+function ButtonsEdit( { attributes, className, clientId } ) {
 	const { fontSize, layout, style } = attributes;
 	const blockProps = useBlockProps( {
 		className: clsx( className, {
 			'has-custom-font-size': fontSize || style?.typography?.fontSize,
 		} ),
 	} );
-	const { hasButtonVariations } = useSelect( ( select ) => {
-		const buttonVariations = select( blocksStore ).getBlockVariations(
-			'core/button',
-			'inserter'
-		);
-		return {
-			hasButtonVariations: buttonVariations.length > 0,
-		};
-	}, [] );
+
+	const { hasButtonVariations, hasMultipleAllowedBlocks } = useSelect(
+		( select ) => {
+			const buttonVariations = select( blocksStore ).getBlockVariations(
+				'core/button',
+				'inserter'
+			);
+			const allowedBlocks =
+				select( blockEditorStore ).getAllowedBlocks( clientId );
+			return {
+				hasButtonVariations: buttonVariations.length > 0,
+				hasMultipleAllowedBlocks:
+					allowedBlocks && allowedBlocks.length > 1,
+			};
+		},
+		[ clientId ]
+	);
 
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		defaultBlock: DEFAULT_BLOCK,
-		// This check should be handled by the `Inserter` internally to be consistent across all blocks that use it.
-		directInsert: ! hasButtonVariations,
+		directInsert: ! hasButtonVariations && ! hasMultipleAllowedBlocks,
 		template: [ [ 'core/button' ] ],
 		templateInsertUpdatesSelection: true,
 		orientation: layout?.orientation ?? 'horizontal',
