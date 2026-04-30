2bf0cfba84621ccdbbef270f6a335fec73d43900
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-04-30 12:29:12 +0900

    Test: see how many errors occur by intentionally disabling the rules

--- tools/eslint/config.mjs
+++ tools/eslint/config.mjs
@@ -296,9 +296,7 @@ export default dedupePlugins( [
 				},
 			],
 			// TODO: Re-enable after fixing existing violations across the codebase.
-			'react-hooks/immutability': 'off',
 			'react-hooks/refs': 'off',
-			'react-hooks/static-components': 'off',
 		},
 		plugins: {
 			'react-hooks': reactHooksPlugin,
