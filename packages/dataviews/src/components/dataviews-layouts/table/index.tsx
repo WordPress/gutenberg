b03ce0035f455236b393c1e2b92d5433636b452b
Luigi Teschio <gigitux@gmail.com>
2026-05-04 12:27:50 +0200

    lint code

--- packages/dataviews/src/components/dataviews-layouts/table/index.tsx
+++ packages/dataviews/src/components/dataviews-layouts/table/index.tsx
@@ -46,6 +46,11 @@ import type { SetSelection } from '../../../types/private';
 import ColumnHeaderMenu from './column-header-menu';
 import ColumnPrimary from './column-primary';
 import { useScrollState } from './use-scroll-state';
+import {
+	getHierarchyRows,
+	getItemHierarchyLevel,
+	getVisibleHierarchyRows,
+} from './hierarchy-utils';
 import getDataByGroup from '../utils/get-data-by-group';
 import { PropertiesSection } from '../../dataviews-view-config/properties-section';
 import { useDelayedLoading } from '../../../hooks/use-delayed-loading';
@@ -63,14 +68,6 @@ function getEffectiveAlign(
 	return undefined;
 }
 
-interface HierarchyRow< Item > {
-	item: Item;
-	id: string;
-	level: number;
-	parentId?: string;
-	childCount: number;
-}
-
 interface TableRenderRow< Item > {
 	item: Item;
 	id: string;
@@ -78,95 +75,6 @@ interface TableRenderRow< Item > {
 	hierarchyLevel?: number;
 	childCount?: number;
 	isExpanded?: boolean;
-}
-
-function getItemHierarchyLevel< Item >(
-	item: Item,
-	getItemLevel: ( item: Item ) => number
-) {
-	const level = getItemLevel( item );
-	return Number.isFinite( level ) ? Math.max( 0, level ) : 0;
-}
-
-function getHierarchyRows< Item >(
-	data: Item[],
-	getItemId: ( item: Item ) => string,
-	getItemLevel: ( item: Item ) => number
-) {
-	const hierarchyRows: HierarchyRow< Item >[] = [];
-
-	data.forEach( ( item, index ) => {
-		const id = getItemId( item ) || index.toString();
-		const level = getItemHierarchyLevel( item, getItemLevel );
-		const parent = hierarchyRows.findLast(
-			( hierarchyRow ) => hierarchyRow.level < level
-		);
-		const hierarchyRow = {
-			item,
-			id,
-			level,
-			parentId: parent?.id,
-			childCount: 0,
-		};
-
-		if ( parent ) {
-			parent.childCount += 1;
-		}
-
-		hierarchyRows.push( hierarchyRow );
-	} );
-
-	return hierarchyRows;
-}
-
-function getVisibleHierarchyRows< Item >(
-	hierarchyRows: HierarchyRow< Item >[],
-	expandedItemIds: Set< string >
-) {
-	const isCollapsedParent = ( hierarchyRow: HierarchyRow< Item > ) =>
-		hierarchyRow.childCount > 0 && ! expandedItemIds.has( hierarchyRow.id );
-
-	const getClosestVisibleAncestor = (
-		visibleHierarchyRows: HierarchyRow< Item >[],
-		hierarchyRow: HierarchyRow< Item >
-	) =>
-		visibleHierarchyRows.findLast(
-			( visibleHierarchyRow ) =>
-				visibleHierarchyRow.level < hierarchyRow.level
-		);
-
-	const isHiddenByCollapsedAncestor = (
-		visibleHierarchyRows: HierarchyRow< Item >[],
-		hierarchyRow: HierarchyRow< Item >
-	) => {
-		const closestVisibleAncestor = getClosestVisibleAncestor(
-			visibleHierarchyRows,
-			hierarchyRow
-		);
-
-		return (
-			!! closestVisibleAncestor &&
-			isCollapsedParent( closestVisibleAncestor )
-		);
-	};
-
-	return hierarchyRows.reduce< HierarchyRow< Item >[] >(
-		( visibleHierarchyRows, hierarchyRow ) => {
-			if (
-				isHiddenByCollapsedAncestor(
-					visibleHierarchyRows,
-					hierarchyRow
-				)
-			) {
-				return visibleHierarchyRows;
-			}
-
-			visibleHierarchyRows.push( hierarchyRow );
-
-			return visibleHierarchyRows;
-		},
-		[]
-	);
 }
 
 interface TableColumnFieldProps< Item > {
@@ -635,15 +543,14 @@ function ViewTable< Item >( {
 			} ) );
 		}
 
-		return getVisibleHierarchyRows(
-			getHierarchyRows( items, getItemId, getItemLevel ),
-			expandedItemIds
-		).map( ( hierarchyRow ) => ( {
-			...hierarchyRow,
-			level: undefined,
-			hierarchyLevel: hierarchyRow.level,
-			isExpanded: expandedItemIds.has( hierarchyRow.id ),
-		} ) );
+		return getVisibleHierarchyRows( hierarchyRows, expandedItemIds ).map(
+			( hierarchyRow ) => ( {
+				...hierarchyRow,
+				level: undefined,
+				hierarchyLevel: hierarchyRow.level,
+				isExpanded: expandedItemIds.has( hierarchyRow.id ),
+			} )
+		);
 	};
 	if ( ! hasData ) {
 		return (
