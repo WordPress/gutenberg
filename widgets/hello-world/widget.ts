30ba4c8882452845b8bb47fa21250e5b910a871e
Damián Suárez <rdsuarez@gmail.com>
2026-05-05 08:26:26 -0300

    do not translate widget title

--- widgets/hello-world/widget.ts
+++ widgets/hello-world/widget.ts
@@ -1,12 +1,7 @@
 /**
- * Internal dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Widget type definition
  */
 export default {
 	name: 'wordpress/hello-world',
-	title: __( 'Hello World' ),
+	title: 'Hello World',
 };
