0a4bb4ab3b1b587528f051f0d06bb01685c92e84
Lena Morita <lena@jaguchi.com>
2026-05-12 03:54:46 +0900

    Document SelectControl compound export

--- packages/ui/src/form/select-control/index.ts
+++ packages/ui/src/form/select-control/index.ts
@@ -3,6 +3,12 @@ import { Item } from './item';
 
 Item.displayName = 'SelectControl.Item';
 
+/**
+ * A complete select field with integrated label and description.
+ */
 export const SelectControl = Object.assign( _SelectControl, {
+	/**
+	 * An item rendered inside a `SelectControl` popup.
+	 */
 	Item,
 } );
