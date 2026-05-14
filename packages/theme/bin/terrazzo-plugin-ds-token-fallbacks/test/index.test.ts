7bd9018abec06a88782c8f51cae96cf1a215dab6
Andrew Duthie <andrew.duthie@automattic.com>
2026-05-14 12:36:22 -0400

    Restore Terrazzo import mock
    Terrazzo is ESM-only, which our current version of Jest doesn't support

--- packages/theme/bin/terrazzo-plugin-ds-token-fallbacks/test/index.test.ts
+++ packages/theme/bin/terrazzo-plugin-ds-token-fallbacks/test/index.test.ts
@@ -1,5 +1,7 @@
 import { computeBrandFallback } from '../index';
 
+jest.mock( '@terrazzo/plugin-css', () => ( { FORMAT_ID: 'css/value' } ) );
+
 describe( 'computeBrandFallback', () => {
 	it( 'throws on colors with alpha (8-digit hex)', () => {
 		expect( () => computeBrandFallback( '#3858e980' ) ).toThrow(
