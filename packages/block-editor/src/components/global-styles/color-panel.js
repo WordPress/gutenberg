9c04a788875c759c984f664afe3a9b97d4f9e245
USERSATOSHI <tushar.bharti@rtcamp.com>
2026-05-15 12:35:02 +0530

    refactor: useMemo on elements and useCallback is back on resetAllFilter

--- packages/block-editor/src/components/global-styles/color-panel.js
+++ packages/block-editor/src/components/global-styles/color-panel.js
@@ -19,7 +19,7 @@ import {
 	Button,
 	privateApis as componentsPrivateApis,
 } from '@wordpress/components';
-import { useMemo, useRef } from '@wordpress/element';
+import { useCallback, useMemo, useRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { getValueFromVariable } from '@wordpress/global-styles-engine';
 import { reset as resetIcon } from '@wordpress/icons';
@@ -476,6 +476,9 @@ export default function ColorPanel( {
 
 	// Text Color
 	const showTextPanel = useHasTextPanel( settings );
+	const showCaptionPanel = useHasCaptionPanel( settings );
+	const showButtonPanel = useHasButtonPanel( settings );
+	const showHeadingPanel = useHasHeadingPanel( settings );
 	const textColor = decodeValue( inheritedValue?.color?.text );
 	const userTextColor = decodeValue( value?.color?.text );
 	const hasTextColor = () => !! userTextColor;
@@ -515,79 +518,85 @@ export default function ColorPanel( {
 	const resetTextColor = () => setTextColor( undefined );
 
 	// Elements
-	const elements = [
-		{
-			name: 'caption',
-			label: __( 'Captions' ),
-			showPanel: useHasCaptionPanel( settings ),
-		},
-		{
-			name: 'button',
-			label: __( 'Button' ),
-			showPanel: useHasButtonPanel( settings ),
-		},
-		{
-			name: 'heading',
-			label: __( 'Heading' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h1',
-			label: __( 'H1' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h2',
-			label: __( 'H2' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h3',
-			label: __( 'H3' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h4',
-			label: __( 'H4' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h5',
-			label: __( 'H5' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-		{
-			name: 'h6',
-			label: __( 'H6' ),
-			showPanel: useHasHeadingPanel( settings ),
-		},
-	];
+	const elements = useMemo(
+		() => [
+			{
+				name: 'caption',
+				label: __( 'Captions' ),
+				showPanel: showCaptionPanel,
+			},
+			{
+				name: 'button',
+				label: __( 'Button' ),
+				showPanel: showButtonPanel,
+			},
+			{
+				name: 'heading',
+				label: __( 'Heading' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h1',
+				label: __( 'H1' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h2',
+				label: __( 'H2' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h3',
+				label: __( 'H3' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h4',
+				label: __( 'H4' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h5',
+				label: __( 'H5' ),
+				showPanel: showHeadingPanel,
+			},
+			{
+				name: 'h6',
+				label: __( 'H6' ),
+				showPanel: showHeadingPanel,
+			},
+		],
+		[ showCaptionPanel, showButtonPanel, showHeadingPanel ]
+	);
 
-	const resetAllFilter = ( previousValue ) => {
-		return {
-			...previousValue,
-			color: undefined,
-			elements: {
-				...previousValue?.elements,
-				link: {
-					...previousValue?.elements?.link,
-					color: undefined,
-					':hover': {
+	const resetAllFilter = useCallback(
+		( previousValue ) => {
+			return {
+				...previousValue,
+				color: undefined,
+				elements: {
+					...previousValue?.elements,
+					link: {
+						...previousValue?.elements?.link,
 						color: undefined,
-					},
-				},
-				...elements.reduce( ( acc, element ) => {
-					return {
-						...acc,
-						[ element.name ]: {
-							...previousValue?.elements?.[ element.name ],
+						':hover': {
 							color: undefined,
 						},
-					};
-				}, {} ),
-			},
-		};
-	};
+					},
+					...elements.reduce( ( acc, element ) => {
+						return {
+							...acc,
+							[ element.name ]: {
+								...previousValue?.elements?.[ element.name ],
+								color: undefined,
+							},
+						};
+					}, {} ),
+				},
+			};
+		},
+		[ elements ]
+	);
 
 	const items = [
 		showTextPanel && {
