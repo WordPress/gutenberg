18b1271e36154cad30192f6777163aaa32a87a32
Rishabh Gupta <rishabh.gupta@rtcamp.com>
2026-05-19 11:56:37 +0530

    Image: Restore aria-label on lightbox trigger button for single images

--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -250,7 +250,8 @@ function block_core_image_render_lightbox( $block_content, $block, $block_instan
 					'galleryId'              => $block_instance->context['galleryId'] ?? null,
 					'customAriaLabel'        => $custom_aria_label ?? null,
 					'navigationButtonType'   => $block_instance->context['navigationButtonType'] ?? 'icon',
-					'triggerButtonAriaLabel' => null,
+					/* translators: %s: Image alt text. */
+					'triggerButtonAriaLabel' => $alt ? sprintf( __( 'Enlarge image: %s' ), $alt ) : __( 'Enlarge image' ),
 				),
 			),
 		)
