29cce33582e063bcedd324e32051dee8e9722c61
Ashar Fuadi <fushar@gmail.com>
2026-05-19 18:33:53 +0700

    Refactor getAdminThemeColors()

--- packages/admin-ui/src/admin-theme-colors/index.ts
+++ packages/admin-ui/src/admin-theme-colors/index.ts
@@ -1,24 +1,18 @@
-const ADMIN_THEME_PRIMARY_COLORS = new Map< string, string >( [
-	[ 'light', '#0085ba' ],
-	[ 'modern', '#3858e9' ],
-	[ 'blue', '#096484' ],
-	[ 'coffee', '#46403c' ],
-	[ 'ectoplasm', '#523f6d' ],
-	[ 'midnight', '#e14d43' ],
-	[ 'ocean', '#627c83' ],
-	[ 'sunrise', '#dd823b' ],
-] );
+type AdminThemeColors = {
+	primary?: string;
+	bg: string;
+};
 
-const ADMIN_THEME_BACKGROUND_COLORS = new Map< string, string >( [
-	[ 'fresh', '#1d2327' ],
-	[ 'modern', '#1e1e1e' ],
-	[ 'midnight', '#363b3f' ],
-	[ 'coffee', '#7c726c' ],
-	[ 'ocean', '#5f787f' ],
-	[ 'blue', '#0e7da4' ],
-	[ 'ectoplasm', '#8468ab' ],
-	[ 'sunrise', '#cc4541' ],
-	[ 'light', '#e5e5e5' ],
+const ADMIN_THEME_COLORS = new Map< string, AdminThemeColors >( [
+	[ 'fresh', { bg: '#1d2327' } ],
+	[ 'modern', { primary: '#3858e9', bg: '#1e1e1e' } ],
+	[ 'midnight', { primary: '#e14d43', bg: '#363b3f' } ],
+	[ 'coffee', { primary: '#46403c', bg: '#7c726c' } ],
+	[ 'ocean', { primary: '#627c83', bg: '#5f787f' } ],
+	[ 'blue', { primary: '#096484', bg: '#0e7da4' } ],
+	[ 'ectoplasm', { primary: '#523f6d', bg: '#8468ab' } ],
+	[ 'sunrise', { primary: '#dd823b', bg: '#cc4541' } ],
+	[ 'light', { primary: '#0085ba', bg: '#e5e5e5' } ],
 ] );
 
 /**
@@ -29,14 +23,12 @@ const ADMIN_THEME_BACKGROUND_COLORS = new Map< string, string >( [
  *
  * @return The primary and background colors for the active admin color scheme.
  */
-export function getAdminThemeColors() {
+export function getAdminThemeColors(): AdminThemeColors | undefined {
 	const scheme =
-		document.body.className.match( /admin-color-([\w-]+)/ )?.[ 1 ];
+		document.body.className.match( /admin-color-([\w-]+)/ )?.[ 1 ] ??
+		'fresh';
 
-	return {
-		primary: scheme ? ADMIN_THEME_PRIMARY_COLORS.get( scheme ) : undefined,
-		bg:
-			( scheme && ADMIN_THEME_BACKGROUND_COLORS.get( scheme ) ) ||
-			ADMIN_THEME_BACKGROUND_COLORS.get( 'fresh' ),
-	};
+	return (
+		ADMIN_THEME_COLORS.get( scheme ) ?? ADMIN_THEME_COLORS.get( 'fresh' )
+	);
 }
