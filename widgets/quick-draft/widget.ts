d5188ebc7b53ce4acfcb9942dd5cabf7ec600588
Damián Suárez <rdsuarez@gmail.com>
2026-05-23 16:19:47 +0100

    fix missing i18n on quick-draft widget title

--- widgets/quick-draft/widget.ts
+++ widgets/quick-draft/widget.ts
@@ -1,7 +1,12 @@
 /**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/*
  * Widget type definition
  */
 export default {
 	name: 'core/quick-draft',
-	title: 'Quick Draft',
+	title: __( 'Quick Draft' ),
 };
