bf4771debeaad6a289038383d89053d58fdf299b
macayu17 <anayush1406@gmail.com>
2026-05-25 23:56:02 +0530

    Button.Icon: Add icon library story

--- packages/ui/src/button/test/icon.test.tsx
+++ packages/ui/src/button/test/icon.test.tsx
@@ -1,5 +1,5 @@
 import { createRef } from '@wordpress/element';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { ButtonIcon } from '../icon';
 
 describe( 'Button.Icon', () => {
@@ -9,20 +9,5 @@ describe( 'Button.Icon', () => {
 		render( <ButtonIcon ref={ ref } icon={ <svg /> } /> );
 
 		expect( ref.current ).toBeInstanceOf( SVGSVGElement );
-	} );
-
-	it( 'preserves the icon viewBox', () => {
-		render(
-			<ButtonIcon
-				aria-label="Button icon"
-				role="img"
-				icon={ <svg viewBox="-2 -2 24 24" /> }
-			/>
-		);
-
-		expect( screen.getByRole( 'img', { hidden: true } ) ).toHaveAttribute(
-			'viewBox',
-			'-2 -2 24 24'
-		);
 	} );
 } );
