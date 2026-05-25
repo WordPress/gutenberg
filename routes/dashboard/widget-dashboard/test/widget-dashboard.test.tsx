4b577390b4287ec390a47a08337b58b4131a195f
Mikael Korpela <mikael@ihminen.org>
2026-05-25 16:55:23 +0300

    Fix test

--- routes/dashboard/widget-dashboard/test/widget-dashboard.test.tsx
+++ routes/dashboard/widget-dashboard/test/widget-dashboard.test.tsx
@@ -4,6 +4,7 @@
 import '@testing-library/jest-dom';
 import type { ComponentType } from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 /**
  * WordPress dependencies
