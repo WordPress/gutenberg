9346d8b7f5f26d786208438b8ec3d242edbd5299
Damián Suárez <rdsuarez@gmail.com>
2026-05-26 15:24:02 +0100

    remove unused description from hello-dolly
    full-bleed chrome renders only icon and title, never description

--- widgets/hello-dolly/widget.ts
+++ widgets/hello-dolly/widget.ts
@@ -10,8 +10,5 @@ import { audio } from '@wordpress/icons';
 export default {
 	name: 'core/hello-dolly',
 	title: __( 'Hello Dolly' ),
-	description: __(
-		'This is not just a widget, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the dahboard.'
-	),
 	icon: audio,
 };
