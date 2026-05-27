90f787b2e43116511824dafeb79755f835d5bbca
Jorge Costa <jorge.costa@developer.pt>
2026-05-27 09:54:00 +0200

    Fix: Don't send meta on the duplicate action just because of empty footnotes.

--- packages/fields/src/actions/duplicate-post.tsx
+++ packages/fields/src/actions/duplicate-post.tsx
@@ -64,7 +64,7 @@ const duplicatePost: Action< BasePost > = {
 
 			const isTemplate = item.type === 'wp_template';
 
-			const newItemObject = {
+			const newItemObject: Record< string, unknown > = {
 				status: isTemplate ? 'publish' : 'draft',
 				title: item.title,
 				slug: isTemplate ? item.slug : item.title || __( 'No title' ),
@@ -77,7 +77,6 @@ const duplicatePost: Action< BasePost > = {
 					typeof item.excerpt === 'string'
 						? item.excerpt
 						: item.excerpt?.raw,
-				meta: item.meta,
 				parent: item.parent,
 				password: item.password,
 				template: item.template,
@@ -86,6 +85,20 @@ const duplicatePost: Action< BasePost > = {
 				menu_order: item.menu_order,
 				ping_status: item.ping_status,
 			};
+			if ( item.meta ) {
+				const metaKeys = Object.keys( item.meta );
+				if ( metaKeys.length > 0 ) {
+					const { footnotes, ...restMeta } = item.meta;
+					if (
+						footnotes ||
+						Object.keys( restMeta ).length > 0
+					) {
+						newItemObject.meta = footnotes
+							? { footnotes, ...restMeta }
+							: restMeta;
+					}
+				}
+			}
 			const assignablePropertiesPrefix = 'wp:action-assign-';
 			// Get all the properties that the current user is able to assign normally author, categories, tags,
 			// and custom taxonomies.
