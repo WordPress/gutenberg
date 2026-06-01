2abd964b23694b31d12e608c3637c89b3f7f2fc0
Aki Hamano <tetsuaki.hamano@gmail.com>
2026-06-01 21:35:37 +0900

    Revert "Navigation: Wrap morph onComplete in withScope for Interactivity actions"
    This reverts commit dea448fba15bf5647c642338a3d6a05a485e909f.

--- packages/block-library/src/navigation/view.js
+++ packages/block-library/src/navigation/view.js
@@ -5,7 +5,6 @@ import {
 	store,
 	getContext,
 	getElement,
-	withScope,
 	withSyncEvent,
 } from '@wordpress/interactivity';
 
@@ -423,14 +422,10 @@ const { state, actions } = store(
 							hamburgerBtn,
 							ref,
 							ctx.morphStartRect,
-							// onComplete fires from WAAPI's onfinish,
-							// which runs outside the Interactivity scope;
-							// withScope restores it so actions.closeMenu
-							// can call getContext().
-							withScope( () => {
+							() => {
 								actions.closeMenu( 'click' );
 								actions.closeMenu( 'focus' );
-							} )
+							}
 						);
 						return;
 					}
