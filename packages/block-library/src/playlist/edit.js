78fafd6fc637f47b461ed982616458c9d06dbbd8
Jarda Snajdr <jsnajdr@gmail.com>
2026-06-22 15:32:15 +0200

    Fix side-effect edits in few more blocks

--- packages/block-library/src/playlist/edit.js
+++ packages/block-library/src/playlist/edit.js
@@ -63,14 +63,16 @@ const PlaylistEdit = ( {
 	const waveformStyle =
 		attributes.className?.match( /is-style-([\w-]+)/ )?.[ 1 ] || 'bars';
 	const blockProps = useBlockProps();
-	const { replaceInnerBlocks, __unstableMarkNextChangeAsNotPersistent } =
-		useDispatch( blockEditorStore );
+	const {
+		replaceInnerBlocks,
+		updateBlockAttributes,
+		__unstableMarkNextChangeAsNotPersistent,
+	} = useDispatch( blockEditorStore );
 	const { createErrorNotice } = useDispatch( noticesStore );
 	const dropdownMenuProps = useToolsPanelDropdownMenuProps();
 	function onUploadError( message ) {
 		createErrorNotice( message, { type: 'snackbar' } );
 	}
-	const { updateBlockAttributes } = useDispatch( blockEditorStore );
 
 	const { innerBlockTracks } = useSelect(
 		( select ) => {
@@ -121,6 +123,7 @@ const PlaylistEdit = ( {
 		if ( tracks.length === 0 ) {
 			// If there are no tracks but currentTrack is set, set it to null.
 			if ( currentTrack !== null ) {
+				__unstableMarkNextChangeAsNotPersistent();
 				updateBlockAttributes( clientId, { currentTrack: null } );
 			}
 		} else if (
@@ -128,6 +131,7 @@ const PlaylistEdit = ( {
 			firstTrackId &&
 			firstTrackId !== currentTrack
 		) {
+			__unstableMarkNextChangeAsNotPersistent();
 			updateBlockAttributes( clientId, { currentTrack: firstTrackId } );
 		}
 	}, [
@@ -136,6 +140,7 @@ const PlaylistEdit = ( {
 		firstTrackId,
 		clientId,
 		updateBlockAttributes,
+		__unstableMarkNextChangeAsNotPersistent,
 	] );
 
 	const onSelectTracks = useCallback(
