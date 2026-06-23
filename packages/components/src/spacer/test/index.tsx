82e797a53de174801591fb12af72827e47a87156
Marco Ciampini <marco.ciampo@gmail.com>
2026-06-23 16:37:19 +0200

    Components: migrate Spacer to SCSS module

--- packages/components/src/spacer/test/index.tsx
+++ packages/components/src/spacer/test/index.tsx
@@ -16,192 +16,169 @@ describe( 'props', () => {
 	} );
 
 	test( 'should render margin', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer margin={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer margin={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should render marginX', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginX={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginX={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-inline-start': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-inline-end': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should render marginY', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginY={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginY={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-start': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should render marginTop', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginTop={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginTop={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-start': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should render marginBottom', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginBottom={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginBottom={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render marginLeft', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginLeft={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginLeft={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-inline-start': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should render marginRight', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer marginRight={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer marginRight={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-inline-end': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+		} );
 	} );
 
 	test( 'should override margin props from less to more specific', () => {
-		render( <Spacer data-testid="spacer" /> );
 		render(
 			<Spacer
 				margin={ 10 }
 				marginX={ 3 }
 				marginRight={ 5 }
 				marginBottom={ 1 }
-				data-testid="customized-spacer"
+				data-testid="spacer"
 			/>
 		);
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
-	} );
-
-	test( 'should preserve explicit margin props regardless of Emotion insertion order', () => {
-		render( <Spacer marginRight={ 5 } data-testid="primer" /> );
-		render(
-			<Spacer margin={ 10 } marginRight={ 5 } data-testid="target" />
-		);
-
-		const styles = window.getComputedStyle(
-			screen.getByTestId( 'target' )
-		);
-
-		expect( styles.marginRight ).toBe( 'calc(20px)' );
-		expect( styles.marginTop ).toBe( 'calc(40px)' );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin': 'calc(4px * 10)',
+			'--wp-components-spacer-margin-inline-start': 'calc(4px * 3)',
+			'--wp-components-spacer-margin-inline-end': 'calc(4px * 5)',
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 1)',
+		} );
 	} );
 
 	test( 'should render padding', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer padding={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer padding={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingX', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer paddingX={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer paddingX={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-inline-start': 'calc(4px * 5)',
+			'--wp-components-spacer-padding-inline-end': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingY', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer paddingY={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer paddingY={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-block-start': 'calc(4px * 5)',
+			'--wp-components-spacer-padding-block-end': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingTop', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer paddingTop={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer paddingTop={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-block-start': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingBottom', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render(
-			<Spacer paddingBottom={ 5 } data-testid="customized-spacer" />
-		);
+		render( <Spacer paddingBottom={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-block-end': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingLeft', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer paddingLeft={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer paddingLeft={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-inline-start': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should render paddingRight', () => {
-		render( <Spacer data-testid="spacer" /> );
-		render( <Spacer paddingRight={ 5 } data-testid="customized-spacer" /> );
+		render( <Spacer paddingRight={ 5 } data-testid="spacer" /> );
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-inline-end': 'calc(4px * 5)',
+		} );
 	} );
 
 	test( 'should override padding props from less to more specific', () => {
-		render( <Spacer data-testid="spacer" /> );
 		render(
 			<Spacer
 				padding={ 10 }
 				paddingY={ 2 }
 				paddingTop={ 5 }
 				paddingLeft={ 3 }
-				data-testid="customized-spacer"
+				data-testid="spacer"
 			/>
 		);
 
-		expect(
-			screen.getByTestId( 'customized-spacer' )
-		).toMatchStyleDiffSnapshot( screen.getByTestId( 'spacer' ) );
-	} );
-
-	test( 'should preserve explicit padding props regardless of Emotion insertion order', () => {
-		render( <Spacer paddingRight={ 5 } data-testid="primer" /> );
-		render(
-			<Spacer padding={ 10 } paddingRight={ 5 } data-testid="target" />
-		);
-
-		const styles = window.getComputedStyle(
-			screen.getByTestId( 'target' )
-		);
-
-		expect( styles.paddingRight ).toBe( 'calc(20px)' );
-		expect( styles.paddingTop ).toBe( 'calc(40px)' );
+		expect( screen.getByTestId( 'spacer' ) ).toHaveStyle( {
+			'--wp-components-spacer-margin-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding': 'calc(4px * 10)',
+			'--wp-components-spacer-padding-block-start': 'calc(4px * 5)',
+			'--wp-components-spacer-padding-block-end': 'calc(4px * 2)',
+			'--wp-components-spacer-padding-inline-start': 'calc(4px * 3)',
+		} );
 	} );
 } );
