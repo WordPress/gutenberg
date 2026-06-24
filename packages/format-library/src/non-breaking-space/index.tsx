3827a0eaec4d6d75b6697788ad80ebaede8f6ace
im3dabasia <eshaan.dabasiya@rtcamp.com>
2026-06-24 20:16:28 +0530

    feat: Migrate nbsp folder to TS

--- packages/format-library/src/non-breaking-space/index.tsx
+++ packages/format-library/src/non-breaking-space/index.tsx
@@ -1,16 +1,22 @@
 import { __ } from '@wordpress/i18n';
+import type { RichTextValue } from '@wordpress/rich-text';
 import { insert } from '@wordpress/rich-text';
 import { RichTextShortcut } from '@wordpress/block-editor';
 
 const name = 'core/non-breaking-space';
 const title = __( 'Non breaking space' );
 
+interface NonBreakingSpaceEditProps {
+	value: RichTextValue;
+	onChange: ( value: RichTextValue ) => void;
+}
+
 export const nonBreakingSpace = {
 	name,
 	title,
 	tagName: 'nbsp',
 	className: null,
-	edit( { value, onChange } ) {
+	edit( { value, onChange }: NonBreakingSpaceEditProps ) {
 		function addNonBreakingSpace() {
 			onChange( insert( value, '\u00a0' ) );
 		}
