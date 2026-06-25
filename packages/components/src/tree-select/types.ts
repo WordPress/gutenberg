7577740f2f148e795415b074613dd6f973b19cc3
Lena Morita <lena@jaguchi.com>
2026-06-25 23:44:44 +0900

    Inline TreeSelect __next40pxDefaultSize type stub

--- packages/components/src/tree-select/types.ts
+++ packages/components/src/tree-select/types.ts
@@ -13,24 +13,18 @@ export interface Tree {
 	children?: Tree[];
 }
 
-type DeprecatedTreeSelectProps = {
+// `TreeSelect` inherits props from `SelectControl`, but only
+// in single selection mode (ie. when the `multiple` prop is not defined).
+export interface TreeSelectProps
+	extends Omit<
+		SelectControlSingleSelectionProps,
+		'value' | 'multiple' | 'onChange' | '__next40pxDefaultSize'
+	> {
 	/**
-	 * Start opting into the larger default height that will become the default size in a future version.
-	 *
 	 * @deprecated Default behavior since WordPress 7.1. Prop can be safely removed.
 	 * @ignore
 	 */
 	__next40pxDefaultSize?: boolean;
-};
-
-// `TreeSelect` inherits props from `SelectControl`, but only
-// in single selection mode (ie. when the `multiple` prop is not defined).
-export interface TreeSelectProps
-	extends DeprecatedTreeSelectProps,
-		Omit<
-			SelectControlSingleSelectionProps,
-			'value' | 'multiple' | 'onChange' | '__next40pxDefaultSize'
-		> {
 	/**
 	 * If this property is added, an option will be added with this label to represent empty selection.
 	 */
