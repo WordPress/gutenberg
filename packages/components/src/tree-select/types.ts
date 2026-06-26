4df6cbdd4d0dda9253e3e8e7783bcf9a92747d1b
Lena Morita <lena@jaguchi.com>
2026-06-26 19:30:50 +0900

    Tweak

--- packages/components/src/tree-select/types.ts
+++ packages/components/src/tree-select/types.ts
@@ -21,6 +21,8 @@ export interface TreeSelectProps
 		'value' | 'multiple' | 'onChange' | '__next40pxDefaultSize'
 	> {
 	/**
+	 * Start opting into the larger default height that will become the default size in a future version.
+	 *
 	 * @deprecated Default behavior since WordPress 7.1. Prop can be safely removed.
 	 * @ignore
 	 */
