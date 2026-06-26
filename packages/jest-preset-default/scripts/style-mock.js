d9cc1919d342bf1b40008582d23b5b5cf41726f3
Marco Ciampini <marco.ciampo@gmail.com>
2026-06-26 16:49:40 +0200

    Jest: Use change-case for style mock classes

--- packages/jest-preset-default/scripts/style-mock.js
+++ packages/jest-preset-default/scripts/style-mock.js
@@ -1,8 +1,4 @@
-const kebabCase = ( property ) =>
-	property
-		.replace( /([a-z0-9])([A-Z])/g, '$1-$2' )
-		.replace( /([A-Z])([A-Z][a-z])/g, '$1-$2' )
-		.toLowerCase();
+const { paramCase: kebabCase } = require( 'change-case' );
 
 const cssClass = ( property ) => `style-${ kebabCase( property ) }`;
 
