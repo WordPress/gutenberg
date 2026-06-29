3dd7f1a6f58c502d3f1de52d2e12d824d5d88bc4
Swanand01 <swanandmathekar@gmail.com>
2026-06-29 17:40:15 +0530

    fix: add wp-site-blocks class to `MemoizedBlockList`

--- packages/block-editor/src/components/block-preview/auto.js
+++ packages/block-editor/src/components/block-preview/auto.js
@@ -108,7 +108,10 @@ function ScaledBlockPreview( {
 			>
 				<EditorStyles styles={ editorStyles } />
 				{ contentResizeListener }
-				<MemoizedBlockList renderAppender={ false } />
+				<MemoizedBlockList
+					renderAppender={ false }
+					className="wp-site-blocks"
+				/>
 			</Iframe>
 		</Disabled>
 	);
