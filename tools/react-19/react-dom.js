b2b158edc99c725dcfe9e64eb2ed69310283ef88
Jarda Snajdr <jsnajdr@gmail.com>
2026-06-29 14:02:10 +0200

    Fix react-dom secretInternals

--- tools/react-19/react-dom.js
+++ tools/react-19/react-dom.js
@@ -3,6 +3,8 @@ const { warnCompat } = require( './warn-compat' );
 const SECRET_INTERNALS_KEY =
 	'__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED';
 
+const secretInternals = {};
+
 const reactDOM = {
 	...require( 'react-dom' ),
 	...require( 'react-dom/client' ),
@@ -17,7 +19,7 @@ Object.defineProperty( reactDOM, SECRET_INTERNALS_KEY, {
 			'react-dom-secret-internals',
 			`Accessing \`${ SECRET_INTERNALS_KEY }\` was removed in React 19 and is provided by a compatibility shim.`
 		);
-		return {};
+		return secretInternals;
 	},
 } );
 
