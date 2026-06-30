4ee380f34d9a3341399221320726bafef3282701
Marco Ciampini <marco.ciampo@gmail.com>
2026-06-30 22:43:07 +0200

    theme: Build package-only declarations

---
+++ packages/theme/src/package-types.ts
@@ -0,0 +1,19 @@
+import type { ReactElement } from 'react';
+import type { ThemeProviderProps } from './types';
+
+/**
+ * @deprecated Private `@wordpress/theme` APIs will be removed in WordPress 7.3.
+ * Use public `@wordpress/theme` APIs for supported theming use cases.
+ */
+export declare const privateApis: {};
+
+/**
+ * Context provider that generates a theme from a set of seed color values and
+ * configuration, producing a set of design token overrides as CSS custom
+ * properties.
+ */
+export declare const ThemeProvider: (
+	props: ThemeProviderProps
+) => ReactElement;
+
+export type * from './prebuilt/ts/token-types';
