48741313cc0e8a825338fff712735b4970e0b391
Andrew Duthie <andrew.duthie@automattic.com>
2026-06-30 15:28:37 -0400

    Automated Testing: Enforce no-unresolved checks for test files

--- tools/eslint/config.mjs
+++ tools/eslint/config.mjs
@@ -374,9 +374,6 @@ export default dedupePlugins( [
 	{
 		files: developmentFiles,
 		rules: {
-			'import/default': 'off',
-			'import/no-unresolved': 'off',
-			'import/named': 'off',
 			'@wordpress/data-no-store-string-literals': 'off',
 		},
 	},
@@ -387,6 +384,7 @@ export default dedupePlugins( [
 		files: [ '**/fixtures/**' ],
 		rules: {
 			'import/no-extraneous-dependencies': 'off',
+			'import/no-unresolved': 'off',
 		},
 	},
 
