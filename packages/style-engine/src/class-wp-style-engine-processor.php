e668b74ca6e905ed50ce467ebf6029c13bc1bb14
Ramon <ramonjd@gmail.com>
2026-06-30 19:52:36 +1000

    Add comment to clarify declaration options in style engine processor

--- packages/style-engine/src/class-wp-style-engine-processor.php
+++ packages/style-engine/src/class-wp-style-engine-processor.php
@@ -145,6 +145,7 @@ if ( ! class_exists( 'WP_Style_Engine_Processor' ) ) {
 				$declarations        = $rule->get_declarations()->get_declarations();
 				$declaration_options = $rule->get_declarations()->get_declaration_options();
 				ksort( $declarations );
+				// Declaration options are keyed by property and are part of the rule identity.
 				ksort( $declaration_options );
 				foreach ( $declaration_options as $property => $options ) {
 					if ( is_array( $options ) ) {
