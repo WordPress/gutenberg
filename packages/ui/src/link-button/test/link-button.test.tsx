acbf582e7eed9f12242404907e5bc0667fdaced0
Mikael Korpela <mikael@ihminen.org>
2026-07-02 12:57:25 +0300

    Remove reduntant tests

--- packages/ui/src/link-button/test/link-button.test.tsx
+++ packages/ui/src/link-button/test/link-button.test.tsx
@@ -49,41 +49,5 @@ describe( 'LinkButton', () => {
 				'_blank'
 			);
 		} );
-
-		it( 'does not set target="_blank" when false', () => {
-			render(
-				<LinkButton href="https://example.com">External</LinkButton>
-			);
-
-			expect( screen.getByRole( 'link' ) ).not.toHaveAttribute(
-				'target'
-			);
-		} );
-
-		it( 'renders an accessible arrow indicator', () => {
-			render(
-				<LinkButton href="https://example.com" openInNewTab>
-					External
-				</LinkButton>
-			);
-
-			expect(
-				screen.getByLabelText( '(opens in a new tab)' )
-			).toBeVisible();
-		} );
-
-		it( 'includes the new tab notice in the link name', () => {
-			render(
-				<LinkButton href="https://example.com" openInNewTab>
-					External
-				</LinkButton>
-			);
-
-			expect(
-				screen.getByRole( 'link', {
-					name: 'External (opens in a new tab)',
-				} )
-			).toBeVisible();
-		} );
 	} );
 } );
