45fc56fe5fa3b95333474aacdf6e64f5829dd4cd
Mustafa Bharmal <mjbharmal2002@gmail.com>
2026-07-08 00:14:54 +0530

    Block Library: Parse Markdown when converting Classic blocks

--- packages/block-library/src/freeform/convert-to-blocks-button.js
+++ packages/block-library/src/freeform/convert-to-blocks-button.js
@@ -4,8 +4,9 @@
 import { __ } from '@wordpress/i18n';
 import { ToolbarButton } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { rawHandler, serialize } from '@wordpress/blocks';
+import { pasteHandler, serialize } from '@wordpress/blocks';
 import { store as blockEditorStore } from '@wordpress/block-editor';
+import { removep } from '@wordpress/autop';
 
 const ConvertToBlocksButton = ( { clientId } ) => {
 	const { replaceBlocks } = useDispatch( blockEditorStore );
@@ -18,12 +19,17 @@ const ConvertToBlocksButton = ( { clientId } ) => {
 
 	return (
 		<ToolbarButton
-			onClick={ () =>
+			onClick={ () => {
+				const plainContent = removep( serialize( block ) );
 				replaceBlocks(
 					block.clientId,
-					rawHandler( { HTML: serialize( block ) } )
-				)
-			}
+					pasteHandler( {
+						HTML: plainContent,
+						plainText: plainContent,
+						mode: 'BLOCKS',
+					} )
+				);
+			} }
 		>
 			{ __( 'Convert to blocks' ) }
 		</ToolbarButton>
