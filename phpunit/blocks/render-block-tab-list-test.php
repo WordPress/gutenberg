d8cb2b28e18e67d86bd187bd6299098b17e622f6
Jerry Jones <jones.jeremydavid@gmail.com>
2026-07-07 11:17:02 -0500

    Add test for default aria-label on tab list output

--- phpunit/blocks/render-block-tab-list-test.php
+++ phpunit/blocks/render-block-tab-list-test.php
@@ -41,4 +41,33 @@ class Tests_Blocks_Render_Tab_List extends WP_UnitTestCase {
 		$this->assertTrue( $processor->next_tag( array( 'class_name' => 'wp-block-tab-list' ) ) );
 		$this->assertSame( 'Product details', $processor->get_attribute( 'aria-label' ) );
 	}
+
+	/**
+	 * @covers ::block_core_tab_list_render_callback
+	 */
+	public function test_should_add_default_aria_label(): void {
+		$tabs_block = <<<'BLOCK_CONTENT'
+			<!-- wp:tabs -->
+			<div class="wp-block-tabs"><!-- wp:tab-list -->
+			<div role="tablist" class="wp-block-tab-list"><button type="button" role="tab">Description</button><button type="button" role="tab">Reviews</button></div>
+			<!-- /wp:tab-list -->
+
+			<!-- wp:tab-panels -->
+			<div class="wp-block-tab-panels"><!-- wp:tab-panel {"label":"Description"} -->
+			<section role="tabpanel" tabindex="0" class="wp-block-tab-panel"></section>
+			<!-- /wp:tab-panel -->
+
+			<!-- wp:tab-panel {"label":"Reviews"} -->
+			<section role="tabpanel" tabindex="0" class="wp-block-tab-panel"></section>
+			<!-- /wp:tab-panel --></div>
+			<!-- /wp:tab-panels --></div>
+			<!-- /wp:tabs -->
+		BLOCK_CONTENT;
+
+		$rendered_block = do_blocks( $tabs_block );
+
+		$processor = new WP_HTML_Tag_Processor( $rendered_block );
+		$this->assertTrue( $processor->next_tag( array( 'class_name' => 'wp-block-tab-list' ) ) );
+		$this->assertSame( 'Tabbed content', $processor->get_attribute( 'aria-label' ) );
+	}
 }
