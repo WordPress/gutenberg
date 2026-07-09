d765efb76a2687a46174a428e1a047d07a644d4d
Daniel Richards <daniel.richards@automattic.com>
2026-07-09 16:23:06 +0800

    Tidy up

--- packages/block-editor/src/utils/style-states.js
+++ packages/block-editor/src/utils/style-states.js
@@ -9,16 +9,34 @@ import {
 
 export const DEFAULT_STATE_VALUE = 'default';
 
+/**
+ * Returns true when a viewport style state value is non-default.
+ *
+ * @param {Object} styleState Style state value.
+ * @return {boolean} Whether a viewport state value is non-default.
+ */
 export function hasViewportStyleStateValue( styleState ) {
 	return (
 		!! styleState?.viewport && styleState.viewport !== DEFAULT_STATE_VALUE
 	);
 }
 
+/**
+ * Returns true when a pseudo style state value is non-default.
+ *
+ * @param {Object} styleState Style state value.
+ * @return {boolean} Whether a pseudo state value is non-default.
+ */
 export function hasPseudoStyleStateValue( styleState ) {
 	return !! styleState?.pseudo && styleState.pseudo !== DEFAULT_STATE_VALUE;
 }
 
+/**
+ * Returns true when any style state value is non-default.
+ *
+ * @param {Object} styleState Style state value.
+ * @return {boolean} Whether any state value is non-default.
+ */
 export function hasStyleStateValue( styleState ) {
 	return Object.values( styleState ?? {} ).some(
 		( value ) => value && value !== DEFAULT_STATE_VALUE
@@ -26,45 +44,67 @@ export function hasStyleStateValue( styleState ) {
 }
 
 /**
- * Returns the style object path for the selected block style state.
+ * Returns the style object path for the provided style state.
  *
- * @param {Object} selectedState Selected block style state.
- * @return {string[]} Object path for the selected state styles.
+ * @param {Object} styleState Style state value.
+ * @return {string[]} Object path for the style state.
  */
-function getStyleStatePath( selectedState ) {
-	if ( ! hasStyleStateValue( selectedState ) ) {
+function getStyleStatePath( styleState ) {
+	if ( ! hasStyleStateValue( styleState ) ) {
 		return [];
 	}
 
-	return [ selectedState.viewport, selectedState.pseudo ].filter(
+	return [ styleState.viewport, styleState.pseudo ].filter(
 		( state ) => state && state !== DEFAULT_STATE_VALUE
 	);
 }
 
-export function getStyleForState( style, selectedState ) {
-	const path = getStyleStatePath( selectedState );
+/**
+ * Returns the style object value for the provided style state.
+ *
+ * @param {Object} style      Block style object.
+ * @param {Object} styleState Style state value.
+ * @return {Object|undefined} Style object value for the state.
+ */
+export function getStyleForState( style, styleState ) {
+	const path = getStyleStatePath( styleState );
 	if ( ! path.length ) {
 		return style;
 	}
 	return getValueFromObjectPath( style, path );
 }
 
-export function setStyleForState( style, selectedState, newStyle ) {
-	const path = getStyleStatePath( selectedState );
+/**
+ * Sets the style object value for the provided style state.
+ *
+ * @param {Object} style      Block style object.
+ * @param {Object} styleState Style state value.
+ * @param {Object} newStyle   New style object value for the state.
+ * @return {Object|undefined} Updated block style object.
+ */
+export function setStyleForState( style, styleState, newStyle ) {
+	const path = getStyleStatePath( styleState );
 	if ( ! path.length ) {
 		return cleanEmptyObject( newStyle );
 	}
 	return cleanEmptyObject( setImmutably( style, path, newStyle ) );
 }
 
-export function scopeResetAllFilterToState( selectedState, resetAllFilter ) {
-	if ( ! resetAllFilter || ! hasStyleStateValue( selectedState ) ) {
+/**
+ * Scopes a reset-all filter to the provided style state.
+ *
+ * @param {Object}   styleState     Style state value.
+ * @param {Function} resetAllFilter Reset-all filter to scope.
+ * @return {Function|undefined} Scoped reset-all filter.
+ */
+export function scopeResetAllFilterToState( styleState, resetAllFilter ) {
+	if ( ! resetAllFilter || ! hasStyleStateValue( styleState ) ) {
 		return resetAllFilter;
 	}
 
 	return ( attributes ) => {
 		const existingStateStyle =
-			getStyleForState( attributes?.style, selectedState ) || {};
+			getStyleForState( attributes?.style, styleState ) || {};
 		const updatedStateAttributes = resetAllFilter( {
 			style: existingStateStyle,
 		} );
@@ -82,7 +122,7 @@ export function scopeResetAllFilterToState( selectedState, resetAllFilter ) {
 		return {
 			style: setStyleForState(
 				attributes?.style,
-				selectedState,
+				styleState,
 				updatedStateStyle
 			),
 		};
