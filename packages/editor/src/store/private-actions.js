47d04763b4b02c28da836b27aec9d3651fab37a6
Himanshu Pathak <himanshu.pathak@rtcamp.com>
2026-07-09 12:22:14 +0530

    Add assertive ARIA announcement and prevent duplicate speech when restoring a revision

--- packages/editor/src/store/private-actions.js
+++ packages/editor/src/store/private-actions.js
@@ -11,6 +11,7 @@ import apiFetch from '@wordpress/api-fetch';
 import { parse, __unstableSerializeAndClean } from '@wordpress/blocks';
 import { decodeEntities } from '@wordpress/html-entities';
 import { dateI18n, getSettings as getDateSettings } from '@wordpress/date';
+import { speak } from '@wordpress/a11y';
 
 /**
  * Internal dependencies
@@ -747,18 +748,20 @@ export const restoreRevision =
 		// Save the post to persist the restored revision.
 		await dispatch.savePost();
 
-		// Show success notice.
-		registry.dispatch( noticesStore ).createSuccessNotice(
-			sprintf(
-				/* translators: %s: Date and time of the revision. */
-				__( 'Restored to revision from %s.' ),
-				dateI18n( getDateSettings().formats.datetime, revision.date )
-			),
-			{
-				type: 'snackbar',
-				id: 'editor-revision-restored',
-			}
+		const message = sprintf(
+			/* translators: %s: Date and time of the revision. */
+			__( 'Restored to revision from %s.' ),
+			dateI18n( getDateSettings().formats.datetime, revision.date )
 		);
+
+		speak( message, 'assertive' );
+
+		// Show success notice.
+		registry.dispatch( noticesStore ).createSuccessNotice( message, {
+			type: 'snackbar',
+			id: 'editor-revision-restored',
+			speak: false,
+		} );
 	};
 
 /**
