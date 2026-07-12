0f803c4313d0d562243b0ea61d5aae1f90072d55
Weston Ruter <weston@ruter.net>
2026-07-11 17:58:27 -0700

    Add native type hints to gutenberg_get_note_followers()

--- lib/compat/wordpress-7.1/notes-mentions.php
+++ lib/compat/wordpress-7.1/notes-mentions.php
@@ -196,7 +196,7 @@ if ( ! function_exists( 'gutenberg_get_note_followers' ) ) {
 	 * @param int $root_id Top-level note ID.
 	 * @return list<int> Follower user IDs.
 	 */
-	function gutenberg_get_note_followers( $root_id ) {
+	function gutenberg_get_note_followers( int $root_id ): array {
 		$followers = get_comment_meta( $root_id, '_wp_note_followers' );
 		if ( ! is_array( $followers ) ) {
 			return array();
