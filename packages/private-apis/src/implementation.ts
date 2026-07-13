b5b17733a9f85dffa518c3143393119ee29ea98f
Andrew Duthie <andrew.duthie@automattic.com>
2026-07-13 16:34:44 -0400

    Private APIs: Remove design system packages from packages using private APIs

--- packages/private-apis/src/implementation.ts
+++ packages/private-apis/src/implementation.ts
@@ -42,7 +42,6 @@ const CORE_MODULES_USING_PRIVATE_APIS = [
 	'@wordpress/routes',
 	'@wordpress/storybook',
 	'@wordpress/sync',
-	'@wordpress/theme',
 	'@wordpress/dataviews',
 	'@wordpress/fields',
 	'@wordpress/lazy-editor',
@@ -51,7 +50,6 @@ const CORE_MODULES_USING_PRIVATE_APIS = [
 	'@wordpress/upload-media',
 	'@wordpress/global-styles-engine',
 	'@wordpress/global-styles-ui',
-	'@wordpress/ui',
 	'@wordpress/views',
 	'@wordpress/widget-dashboard',
 ];
