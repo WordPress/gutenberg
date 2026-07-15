66bad92a446c4dc08872bd6a08ea665dd0154752
George Mamadashvili <georgemamadashvili@gmail.com>
2026-07-15 11:24:50 +0400

    Notes: Add placeholders to the RichText fields

--- packages/editor/src/components/collab-sidebar/add-note.js
+++ packages/editor/src/components/collab-sidebar/add-note.js
@@ -119,7 +119,10 @@ export function AddNote( { onSubmit, sidebarRef, floating } ) {
 						}
 					} }
 					onCancel={ unselectNote }
-					labels={ { input: __( 'New note' ) } }
+					labels={ {
+						input: __( 'New note' ),
+						placeholder: __( 'Write note or mention with @' ),
+					} }
 				/>
 			</NoteCard>
 		</FloatingContainer>
