bf6f2dd7186a40c6190b9e8b84d25cfec19ccfc7
Shail Mehta <shailmehta25@gmail.com>
2026-07-16 21:05:56 +0530

    Fix root Prettier config dropping CSS/SCSS overrides

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,11 @@
 // Import the default config file and expose it in the project root.
 // Useful for editor integrations.
+const baseConfig = require( './packages/prettier-config' );
+
 module.exports = {
-	...require( './packages/prettier-config' ),
+	...baseConfig,
 	overrides: [
+		...( baseConfig.overrides ?? [] ),
 		{
 			files: [ 'changelog.txt' ],
 			options: { parser: 'markdown' },
