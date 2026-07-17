4421a3de7215a8d667a8631cb9d7df52ffe66458
im3dabasia <eshaan.dabasiya@rtcamp.com>
2026-07-17 17:19:24 +0530

    Fix: TS compilation and double class regression issue

--- packages/components/src/base-control/index.tsx
+++ packages/components/src/base-control/index.tsx
@@ -28,7 +28,7 @@ const UnconnectedBaseControl = (
 	} = useContextSystem( props, 'BaseControl' );
 
 	return (
-		<Wrapper className={ className }>
+		<Wrapper className={ clsx( 'components-base-control', className ) }>
 			<StyledField className="components-base-control__field">
 				{ label &&
 					id &&
--- packages/components/src/base-control/styles/base-control-styles.tsx
+++ packages/components/src/base-control/styles/base-control-styles.tsx
@@ -2,7 +2,13 @@
  * External dependencies
  */
 import clsx from 'clsx';
-import type * as React from 'react';
+import type {
+	ElementType,
+	ForwardedRef,
+	ReactElement,
+	ReactNode,
+	ComponentProps,
+} from 'react';
 
 /**
  * WordPress dependencies
@@ -16,12 +22,14 @@ import type { WordPressComponentProps } from '../../context';
 import { PolymorphicElement } from '../../utils/polymorphic-element';
 import styles from './style.module.scss';
 
-type StyledElementProps< T extends React.ElementType > =
-	WordPressComponentProps< { children?: React.ReactNode }, T >;
+type StyledElementProps< T extends ElementType > = WordPressComponentProps<
+	{ children?: ReactNode },
+	T
+>;
 
-function UnforwardedWrapper< T extends React.ElementType = 'div' >(
+function UnforwardedWrapper< T extends ElementType = 'div' >(
 	{ className, ...props }: StyledElementProps< T >,
-	ref: React.ForwardedRef< any >
+	ref: ForwardedRef< any >
 ) {
 	return (
 		<PolymorphicElement
@@ -36,9 +44,9 @@ function UnforwardedWrapper< T extends React.ElementType = 'div' >(
 	);
 }
 
-function UnforwardedStyledField< T extends React.ElementType = 'div' >(
+function UnforwardedStyledField< T extends ElementType = 'div' >(
 	{ className, ...props }: StyledElementProps< T >,
-	ref: React.ForwardedRef< any >
+	ref: ForwardedRef< any >
 ) {
 	return (
 		<PolymorphicElement
@@ -53,86 +61,74 @@ function UnforwardedStyledField< T extends React.ElementType = 'div' >(
 	);
 }
 
-function UnforwardedStyledLabel< T extends React.ElementType = 'label' >(
+function UnforwardedStyledLabel< T extends ElementType = 'label' >(
 	{ as, className, ...props }: StyledElementProps< T >,
-	ref: React.ForwardedRef< any >
+	ref: ForwardedRef< any >
 ) {
 	return (
-		<PolymorphicElement
-			as={ as ?? 'label' }
+		<PolymorphicElement< T >
+			as={ ( as ?? 'label' ) as T }
 			ref={ ref }
-			{ ...props }
-			className={ clsx(
-				styles.label,
-				'components-base-control__label',
-				className
-			) }
+			{ ...( props as ComponentProps< T > ) }
+			className={ clsx( styles.label, className ) }
 		/>
 	);
 }
 
-function UnforwardedStyledHelp< T extends React.ElementType = 'p' >(
+function UnforwardedStyledHelp< T extends ElementType = 'p' >(
 	{ as, className, ...props }: StyledElementProps< T >,
-	ref: React.ForwardedRef< any >
+	ref: ForwardedRef< any >
 ) {
 	return (
-		<PolymorphicElement
-			as={ as ?? 'p' }
+		<PolymorphicElement< T >
+			as={ ( as ?? 'p' ) as T }
 			ref={ ref }
-			{ ...props }
-			className={ clsx(
-				styles.help,
-				'components-base-control__help',
-				className
-			) }
+			{ ...( props as ComponentProps< T > ) }
+			className={ clsx( styles.help, className ) }
 		/>
 	);
 }
 
-function UnforwardedStyledVisualLabel< T extends React.ElementType = 'span' >(
+function UnforwardedStyledVisualLabel< T extends ElementType = 'span' >(
 	{ as, className, ...props }: StyledElementProps< T >,
-	ref: React.ForwardedRef< any >
+	ref: ForwardedRef< any >
 ) {
 	return (
-		<PolymorphicElement
-			as={ as ?? 'span' }
+		<PolymorphicElement< T >
+			as={ ( as ?? 'span' ) as T }
 			ref={ ref }
-			{ ...props }
-			className={ clsx(
-				styles.label,
-				'components-base-control__label',
-				className
-			) }
+			{ ...( props as ComponentProps< T > ) }
+			className={ clsx( styles.label, className ) }
 		/>
 	);
 }
 
 export const Wrapper = forwardRef( UnforwardedWrapper ) as <
-	T extends React.ElementType = 'div',
+	T extends ElementType = 'div',
 >(
-	props: StyledElementProps< T > & { ref?: React.ForwardedRef< any > }
-) => React.ReactElement | null;
+	props: StyledElementProps< T > & { ref?: ForwardedRef< any > }
+) => ReactElement | null;
 
 export const StyledField = forwardRef( UnforwardedStyledField ) as <
-	T extends React.ElementType = 'div',
+	T extends ElementType = 'div',
 >(
-	props: StyledElementProps< T > & { ref?: React.ForwardedRef< any > }
-) => React.ReactElement | null;
+	props: StyledElementProps< T > & { ref?: ForwardedRef< any > }
+) => ReactElement | null;
 
 export const StyledLabel = forwardRef( UnforwardedStyledLabel ) as <
-	T extends React.ElementType = 'label',
+	T extends ElementType = 'label',
 >(
-	props: StyledElementProps< T > & { ref?: React.ForwardedRef< any > }
-) => React.ReactElement | null;
+	props: StyledElementProps< T > & { ref?: ForwardedRef< any > }
+) => ReactElement | null;
 
 export const StyledHelp = forwardRef( UnforwardedStyledHelp ) as <
-	T extends React.ElementType = 'p',
+	T extends ElementType = 'p',
 >(
-	props: StyledElementProps< T > & { ref?: React.ForwardedRef< any > }
-) => React.ReactElement | null;
+	props: StyledElementProps< T > & { ref?: ForwardedRef< any > }
+) => ReactElement | null;
 
 export const StyledVisualLabel = forwardRef( UnforwardedStyledVisualLabel ) as <
-	T extends React.ElementType = 'span',
+	T extends ElementType = 'span',
 >(
-	props: StyledElementProps< T > & { ref?: React.ForwardedRef< any > }
-) => React.ReactElement | null;
+	props: StyledElementProps< T > & { ref?: ForwardedRef< any > }
+) => ReactElement | null;
