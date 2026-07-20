a9b0db6635453d2e964446e366458224964b4a25
Andrew Duthie <andrew.duthie@automattic.com>
2026-07-20 11:34:31 -0400

    Revert "fix: wrap notice in ThemeProvider with 0 corner radius"
    This reverts commit 3bebab33de52c06b3c8f9b78e39f893fdced464b.

--- packages/editor/src/components/editor-interface/index.js
+++ packages/editor/src/components/editor-interface/index.js
@@ -19,7 +19,6 @@ import { useViewportMatch } from '@wordpress/compose';
 import { useState, useCallback } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 import { InlineNotices } from '@wordpress/notices';
-import { ThemeProvider } from '@wordpress/theme';
 
 /**
  * Internal dependencies
@@ -57,15 +56,13 @@ function Notices() {
 	}, [] );
 
 	return (
-		<ThemeProvider cornerRadius="none">
-			<InlineNotices
-				className="editor-notices"
-				pinnedNoticesClassName="editor-notices__pinned"
-				dismissibleNoticesClassName="editor-notices__dismissible"
-			>
-				{ ! isValidTemplate && <TemplateValidationNotice /> }
-			</InlineNotices>
-		</ThemeProvider>
+		<InlineNotices
+			className="editor-notices"
+			pinnedNoticesClassName="editor-notices__pinned"
+			dismissibleNoticesClassName="editor-notices__dismissible"
+		>
+			{ ! isValidTemplate && <TemplateValidationNotice /> }
+		</InlineNotices>
 	);
 }
 
