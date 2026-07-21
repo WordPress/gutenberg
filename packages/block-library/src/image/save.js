5ea3f65227aba5be4c70ca55c751288b2f58c04c
yashjawale <yash25.j@gmail.com>
2026-07-21 13:51:52 +0530

    try: replace role attribute with aria-hidden

--- packages/block-library/src/image/save.js
+++ packages/block-library/src/image/save.js
@@ -104,7 +104,7 @@ export default function save( { attributes } ) {
 				return style;
 			} )() }
 			title={ title }
-			role={ isDecorative ? 'none' : undefined }
+			aria-hidden={ isDecorative ? true : undefined }
 		/>
 	);
 
