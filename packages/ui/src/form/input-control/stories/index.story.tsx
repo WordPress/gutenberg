1424a60e5dd91c561d714f2e3318aa145b0b93ac
Lena Morita <lena@jaguchi.com>
2026-07-24 03:03:56 +0900

    Hide native spin controls on number Input by default.

--- packages/ui/src/form/input-control/stories/index.story.tsx
+++ packages/ui/src/form/input-control/stories/index.story.tsx
@@ -119,44 +119,31 @@ export const NumberWithSteppers: Story = {
 		const [ value, setValue ] = useState( 0 );
 
 		return (
-			<>
-				<style>
-					{ `
-					  .my-number-with-steppers input[type='number'] {
-							-moz-appearance: textfield;
-					  }
-						.my-number-with-steppers ::-webkit-inner-spin-button {
-							appearance: none;
-						}
-					` }
-				</style>
-				<InputControl
-					{ ...args }
-					value={ value }
-					onValueChange={ ( v ) => setValue( parseInt( v, 10 ) ) }
-					className="my-number-with-steppers"
-					suffix={
-						<InputLayout.Slot padding="minimal">
-							<Stack direction="row" gap="xs">
-								<IconButton
-									label="Increment"
-									icon={ plus }
-									onClick={ () => setValue( value + 1 ) }
-									size="small"
-									variant="minimal"
-								/>
-								<IconButton
-									label="Decrement"
-									icon={ reset }
-									onClick={ () => setValue( value - 1 ) }
-									size="small"
-									variant="minimal"
-								/>
-							</Stack>
-						</InputLayout.Slot>
-					}
-				/>
-			</>
+			<InputControl
+				{ ...args }
+				value={ value }
+				onValueChange={ ( v ) => setValue( parseInt( v, 10 ) ) }
+				suffix={
+					<InputLayout.Slot padding="minimal">
+						<Stack direction="row" gap="xs">
+							<IconButton
+								label="Increment"
+								icon={ plus }
+								onClick={ () => setValue( value + 1 ) }
+								size="small"
+								variant="minimal"
+							/>
+							<IconButton
+								label="Decrement"
+								icon={ reset }
+								onClick={ () => setValue( value - 1 ) }
+								size="small"
+								variant="minimal"
+							/>
+						</Stack>
+					</InputLayout.Slot>
+				}
+			/>
 		);
 	},
 	args: {
