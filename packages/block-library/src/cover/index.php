6be2989a3268351c050d8081c4c081d9e23c9ff9
yashjawale <yash25.j@gmail.com>
2026-07-23 16:09:27 +0530

    fix: nested array check for media-text & cover blocks

--- packages/block-library/src/cover/index.php
+++ packages/block-library/src/cover/index.php
@@ -135,8 +135,14 @@ function render_block_core_cover( $attributes, $content ) {
 	}
 
 	$cover_focal_point = $attributes['focalPoint'] ?? null;
-	$object_position   = is_array( $cover_focal_point )
-		? round( $cover_focal_point['x'] * 100 ) . '% ' . round( $cover_focal_point['y'] * 100 ) . '%'
+	$focal_point_x     = null;
+	$focal_point_y     = null;
+	if ( is_array( $cover_focal_point ) ) {
+		$focal_point_x = isset( $cover_focal_point['x'] ) && is_numeric( $cover_focal_point['x'] ) ? $cover_focal_point['x'] : null;
+		$focal_point_y = isset( $cover_focal_point['y'] ) && is_numeric( $cover_focal_point['y'] ) ? $cover_focal_point['y'] : null;
+	}
+	$object_position = null !== $focal_point_x && null !== $focal_point_y
+		? round( $focal_point_x * 100 ) . '% ' . round( $focal_point_y * 100 ) . '%'
 		: null;
 
 	if ( ! ( $attributes['hasParallax'] || $attributes['isRepeated'] ) ) {
