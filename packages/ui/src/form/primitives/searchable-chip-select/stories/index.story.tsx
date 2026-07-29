37bc6d69c50c87903ef1aca917fa346b0b270a82
Lena Morita <lena@jaguchi.com>
2026-07-30 05:44:01 +0900

    Hide items control
    Co-authored-by: Mikael Korpela <mikael@ihminen.org>

--- packages/ui/src/form/primitives/searchable-chip-select/stories/index.story.tsx
+++ packages/ui/src/form/primitives/searchable-chip-select/stories/index.story.tsx
@@ -13,6 +13,7 @@ const meta: Meta< typeof SearchableChipSelect > = {
 			SearchableChipSelect.ChipWithRemove,
 	},
 	argTypes: {
+		items: { control: false },
 		onValueChange: { action: fn() },
 	},
 	parameters: {
