747f467a758b06435e6cb1231b56fca33e294885
Marco Ciampini <marco.ciampo@gmail.com>
2026-07-30 04:31:13 -0400

    test: Migrate preferences persistence to Vitest

--- packages/preferences-persistence/src/migrations/preferences-package-data/test/convert-complementary-areas.js
+++ packages/preferences-persistence/src/migrations/preferences-package-data/test/convert-complementary-areas.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { describe, expect, it } from 'vitest';
+
+/**
  * Internal dependencies
  */
 import convertComplementaryAreas from '../convert-complementary-areas';
