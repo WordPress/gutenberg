f8dd1afb84e0cf241a8fbd282a7fbbc10502ecf8
Marco Ciampini <marco.ciampo@gmail.com>
2026-07-30 03:04:43 -0400

    test: Migrate A11y and Design System MCP to Vitest

--- packages/a11y/src/test/index.test.ts
+++ packages/a11y/src/test/index.test.ts
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+
+/**
  * WordPress dependencies
  */
 import domReady from '@wordpress/dom-ready';
@@ -10,19 +15,18 @@ import { setup, speak } from '../';
 import clear from '../shared/clear';
 import filterMessage from '../shared/filter-message';
 
-jest.mock( '../shared/clear', () => {
-	return jest.fn();
-} );
-jest.mock( '@wordpress/dom-ready', () => {
-	return jest.fn( ( callback: () => void ) => {
+vi.mock( import( '../shared/clear' ), () => ( {
+	default: vi.fn(),
+} ) );
+vi.mock( import( '@wordpress/dom-ready' ), () => ( {
+	default: vi.fn( ( callback: VoidFunction ) => {
 		callback();
-	} );
-} );
-jest.mock( '../shared/filter-message', () => {
-	return jest.fn( ( message: string ) => {
-		return message;
-	} );
-} );
+		return undefined;
+	} ),
+} ) );
+vi.mock( import( '../shared/filter-message' ), () => ( {
+	default: vi.fn( ( message: string ) => message ),
+} ) );
 
 describe( 'speak', () => {
 	let containerPolite = document.getElementById( 'a11y-speak-polite' );
