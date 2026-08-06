2dab68ff95a3ed202ffe120dde74d4226b973d41
Riad Benguella <benguella@gmail.com>
2026-08-06 11:55:44 +0200

    Flaky tests: Report as a PR comment only, stop creating issues (#81218)

--- packages/report-flaky-tests/src/__tests__/run.test.ts
+++ packages/report-flaky-tests/src/__tests__/run.test.ts
@@ -8,8 +8,6 @@ import * as core from '@actions/core';
  */
 import { run } from '../run';
 
-jest.useFakeTimers( 'modern' ).setSystemTime( new Date( '2020-05-10' ) );
-
 const mockPushEventContext = {
 	runId: 100,
 	repo: {
@@ -56,12 +54,7 @@ jest.mock( '@actions/core', () => ( {
 } ) );
 
 const mockAPI = {
-	fetchAllIssuesLabeledFlaky: jest.fn(),
-	findMergeBaseCommit: jest.fn(),
-	updateIssue: jest.fn(),
-	createIssue: jest.fn(),
 	createCommentOnPR: jest.fn(),
-	createCommentOnCommit: jest.fn(),
 };
 jest.mock( '../github-api', () => ( {
 	GitHubAPI: jest.fn( () => mockAPI ),
@@ -72,64 +65,49 @@ jest.mock( 'fs/promises', () => ( {
 	readFile: jest.fn(),
 } ) );
 
+async function mockFlakyTestsArtifact() {
+	const playwrightFlakyTest = await import(
+		'../__fixtures__/should copy only partial selection of text blocks.json'
+	).then( ( json ) => json.default );
+	const jestFlakyTest = await import(
+		'../__fixtures__/Should insert new template part on creation.json'
+	).then( ( json ) => json.default );
+
+	( core.getInput as jest.Mock )
+		// token
+		.mockReturnValueOnce( 'repo-token' )
+		// artifact-path
+		.mockReturnValueOnce( 'flaky-tests' );
+
+	// Replacing the cwd for the test for consistent snapshot results.
+	playwrightFlakyTest.path = playwrightFlakyTest.path.replace(
+		'/home/runner/work/gutenberg/gutenberg',
+		process.cwd()
+	);
+
+	const mockedFs = require( 'fs/promises' );
+	mockedFs.readdir.mockImplementationOnce( () =>
+		Promise.resolve( [
+			`${ playwrightFlakyTest.title }.json`,
+			`${ jestFlakyTest.title }.json`,
+		] )
+	);
+	mockedFs.readFile
+		.mockImplementationOnce( () =>
+			Promise.resolve( JSON.stringify( playwrightFlakyTest ) )
+		)
+		.mockImplementationOnce( () =>
+			Promise.resolve( JSON.stringify( jestFlakyTest ) )
+		);
+}
+
 describe( 'Report flaky tests', () => {
 	afterEach( () => {
 		jest.clearAllMocks();
 	} );
 
-	it( 'should report flaky tests to issue on pull request', async () => {
-		const existingFlakyTest = await import(
-			'../__fixtures__/should copy only partial selection of text blocks.json'
-		).then( ( json ) => json.default );
-		const newFlakyTest = await import(
-			'../__fixtures__/Should insert new template part on creation.json'
-		).then( ( json ) => json.default );
-		const flakyIssues = await import(
-			'../__fixtures__/flaky-issues.json'
-		).then( ( json ) => json.default );
-
-		( core.getInput as jest.Mock )
-			// token
-			.mockReturnValueOnce( 'repo-token' )
-			// artifact-path
-			.mockReturnValueOnce( 'flaky-tests' )
-			// label
-			.mockReturnValueOnce( '[Type] Flaky Test' );
-
-		// Replacing the cwd for the test for consistent snapshot results.
-		existingFlakyTest.path = existingFlakyTest.path.replace(
-			'/home/runner/work/gutenberg/gutenberg',
-			process.cwd()
-		);
-
-		const mockedFs = require( 'fs/promises' );
-		mockedFs.readdir.mockImplementationOnce( () =>
-			Promise.resolve( [
-				`${ existingFlakyTest.title }.json`,
-				`${ newFlakyTest.title }.json`,
-			] )
-		);
-		mockedFs.readFile
-			.mockImplementationOnce( () =>
-				Promise.resolve( JSON.stringify( existingFlakyTest ) )
-			)
-			.mockImplementationOnce( () =>
-				Promise.resolve( JSON.stringify( newFlakyTest ) )
-			);
-
-		mockAPI.fetchAllIssuesLabeledFlaky.mockImplementationOnce(
-			() => flakyIssues
-		);
-
-		mockAPI.updateIssue.mockImplementationOnce( () => ( {
-			number: 1,
-			html_url: 'html_url',
-		} ) );
-
-		mockAPI.createIssue.mockImplementationOnce( () => ( {
-			number: 2,
-			html_url: 'html_url',
-		} ) );
+	it( 'should comment on the pull request', async () => {
+		await mockFlakyTestsArtifact();
 
 		mockAPI.createCommentOnPR.mockImplementationOnce( () => ( {
 			html_url: 'comment_html_url',
@@ -137,194 +115,35 @@ describe( 'Report flaky tests', () => {
 
 		await run();
 
-		const existingFlakyIssue = flakyIssues.find(
-			( issue ) =>
-				issue.title === `[Flaky Test] ${ existingFlakyTest.title }`
-		)!;
-		expect( mockAPI.updateIssue ).toHaveBeenCalledWith(
-			expect.objectContaining( {
-				issue_number: existingFlakyIssue.number,
-				state: 'open',
-			} )
-		);
-		expect( mockAPI.updateIssue.mock.calls[ 0 ][ 0 ].body ).toMatchSnapshot(
-			'Updated existing flaky issue'
-		);
-
-		expect( mockAPI.createIssue ).not.toHaveBeenCalled();
-
 		expect( mockAPI.createCommentOnPR ).toHaveBeenCalledTimes( 1 );
 		expect( mockAPI.createCommentOnPR.mock.calls[ 0 ][ 0 ] ).toBe( 10 );
-		expect( mockAPI.createCommentOnPR.mock.calls[ 0 ][ 1 ] )
-			.toMatchInlineSnapshot( `
-		"<!-- flaky-tests-report-comment -->
-		**Flaky tests detected in headSHA.**
-		Some tests passed with failed attempts. The failures may not be related to this commit but are still reported for visibility. See [the documentation](https://github.com/WordPress/gutenberg/blob/HEAD/docs/contributors/code/testing-overview.md#flaky-tests) for more information.
-
-		🔍  Workflow run URL: https://github.com/WordPress/gutenberg/actions/runs/100
-		📝  Reported issues:
-		- #1 in \`/test/e2e/specs/editor/various/copy-cut-paste.spec.js\`"
-	` );
+		expect(
+			mockAPI.createCommentOnPR.mock.calls[ 0 ][ 1 ]
+		).toMatchSnapshot();
 	} );
 
-	it( 'should report flaky tests to issue on push', async () => {
+	it( 'should skip events other than pull requests', async () => {
 		mockGetContext.mockImplementation( () => mockPushEventContext );
-
-		const existingFlakyTest = await import(
-			'../__fixtures__/should copy only partial selection of text blocks.json'
-		).then( ( json ) => json.default );
-		const newFlakyTest = await import(
-			'../__fixtures__/Should insert new template part on creation.json'
-		).then( ( json ) => json.default );
-		const flakyIssues = await import(
-			'../__fixtures__/flaky-issues.json'
-		).then( ( json ) => json.default );
-
-		( core.getInput as jest.Mock )
-			// token
-			.mockReturnValueOnce( 'repo-token' )
-			// artifact-path
-			.mockReturnValueOnce( 'flaky-tests' )
-			// label
-			.mockReturnValueOnce( '[Type] Flaky Test' );
-
-		// Replacing the cwd for the test for consistent snapshot results.
-		existingFlakyTest.path = existingFlakyTest.path.replace(
-			'/home/runner/work/gutenberg/gutenberg',
-			process.cwd()
-		);
-
-		const mockedFs = require( 'fs/promises' );
-		mockedFs.readdir.mockImplementationOnce( () =>
-			Promise.resolve( [
-				`${ existingFlakyTest.title }.json`,
-				`${ newFlakyTest.title }.json`,
-			] )
-		);
-		mockedFs.readFile
-			.mockImplementationOnce( () =>
-				Promise.resolve( JSON.stringify( existingFlakyTest ) )
-			)
-			.mockImplementationOnce( () =>
-				Promise.resolve( JSON.stringify( newFlakyTest ) )
-			);
-
-		mockAPI.fetchAllIssuesLabeledFlaky.mockImplementationOnce(
-			() => flakyIssues
-		);
-
-		mockAPI.updateIssue.mockImplementationOnce( () => ( {
-			number: 1,
-			html_url: 'html_url',
-		} ) );
-
-		mockAPI.createIssue.mockImplementationOnce( () => ( {
-			number: 2,
-			html_url: 'html_url',
-		} ) );
-
-		mockAPI.createCommentOnCommit.mockImplementationOnce( () => ( {
-			html_url: 'comment_html_url',
-		} ) );
 
 		await run();
 
-		const existingFlakyIssue = flakyIssues.find(
-			( issue ) =>
-				issue.title === `[Flaky Test] ${ existingFlakyTest.title }`
-		)!;
-		expect( mockAPI.updateIssue ).toHaveBeenCalledWith(
-			expect.objectContaining( {
-				issue_number: existingFlakyIssue.number,
-				state: 'open',
-			} )
-		);
-		expect( mockAPI.updateIssue.mock.calls[ 0 ][ 0 ].body ).toMatchSnapshot(
-			'Updated existing flaky issue'
-		);
-
-		expect( mockAPI.createIssue ).toHaveBeenCalledWith(
-			expect.objectContaining( {
-				title: `[Flaky Test] ${ newFlakyTest.title }`,
-			} )
-		);
-		expect( mockAPI.createIssue.mock.calls[ 0 ][ 0 ].body ).toMatchSnapshot(
-			'Created new flaky issue'
-		);
-
-		expect( mockAPI.createCommentOnCommit ).toHaveBeenCalledTimes( 1 );
-		expect( mockAPI.createCommentOnCommit.mock.calls[ 0 ][ 0 ] ).toBe(
-			'commitSHA'
-		);
-		expect( mockAPI.createCommentOnCommit.mock.calls[ 0 ][ 1 ] )
-			.toMatchInlineSnapshot( `
-		"<!-- flaky-tests-report-comment -->
-		**Flaky tests detected in commitSHA.**
-		Some tests passed with failed attempts. The failures may not be related to this commit but are still reported for visibility. See [the documentation](https://github.com/WordPress/gutenberg/blob/HEAD/docs/contributors/code/testing-overview.md#flaky-tests) for more information.
-
-		🔍  Workflow run URL: https://github.com/WordPress/gutenberg/actions/runs/100
-		📝  Reported issues:
-		- #1 in \`/test/e2e/specs/editor/various/copy-cut-paste.spec.js\`
-		- #2 in \`specs/site-editor/template-part.test.js\`"
-	` );
+		// It bails out before even reading the artifact.
+		expect( require( 'fs/promises' ).readdir ).not.toHaveBeenCalled();
+		expect( mockAPI.createCommentOnPR ).not.toHaveBeenCalled();
 
 		mockGetContext.mockImplementation( () => mockPullRequestEventContext );
 	} );
 
-	it( 'should skip for outdated branches', async () => {
-		const flakyTest = await import(
-			'../__fixtures__/should copy only partial selection of text blocks.json'
-		).then( ( json ) => json.default );
-		const flakyIssues = await import(
-			'../__fixtures__/flaky-issues.json'
-		).then( ( json ) => json.default );
-
+	it( 'should not comment when there are no flaky tests', async () => {
 		( core.getInput as jest.Mock )
 			.mockReturnValueOnce( 'repo-token' )
-			.mockReturnValueOnce( 'flaky-tests-report' )
-			.mockReturnValueOnce( '[Type] Flaky Test' );
-
-		// Replacing the cwd for the test for consistent snapshot results.
-		flakyTest.path = flakyTest.path.replace(
-			'/home/runner/work/gutenberg/gutenberg',
-			process.cwd()
-		);
+			.mockReturnValueOnce( 'flaky-tests' );
 
 		const mockedFs = require( 'fs/promises' );
-		mockedFs.readdir.mockImplementationOnce( () =>
-			Promise.resolve( [ `${ flakyTest.title }.json` ] )
-		);
-		mockedFs.readFile.mockImplementationOnce( () =>
-			Promise.resolve( JSON.stringify( flakyTest ) )
-		);
-
-		mockAPI.fetchAllIssuesLabeledFlaky.mockImplementationOnce(
-			() => flakyIssues
-		);
-
-		const latestAncestorDate = new Date( '2022-05-10' );
-		mockAPI.findMergeBaseCommit.mockImplementationOnce( () => ( {
-			committer: {
-				date: latestAncestorDate.toISOString(),
-			},
-		} ) );
-
-		// The issue is closed after the latest ancestor date,
-		// indicating that the code base at this point is outdated.
-		const flakyIssue = flakyIssues.find(
-			( issue ) => issue.title === `[Flaky Test] ${ flakyTest.title }`
-		)!;
-		flakyIssue.state = 'closed';
-		// @ts-expect-error: "closed_at" hasn't been typed yet.
-		flakyIssue.closed_at = new Date( '2022-05-15' ).toISOString();
+		mockedFs.readdir.mockImplementationOnce( () => Promise.resolve( [] ) );
 
 		await run();
 
-		expect( mockAPI.findMergeBaseCommit ).toHaveBeenCalledTimes( 1 );
-
-		expect( mockAPI.updateIssue ).not.toHaveBeenCalled();
-
 		expect( mockAPI.createCommentOnPR ).not.toHaveBeenCalled();
-		expect( mockAPI.createCommentOnCommit ).not.toHaveBeenCalled();
 	} );
 } );
