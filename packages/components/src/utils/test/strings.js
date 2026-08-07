2fbca7bc136017c680bf8f50f43d2cb57084148c
André Maneiro <583546+oandregal@users.noreply.github.com>
2026-08-07 13:55:47 +0200

    Remove dependency group comment blocks from test files
    Trunk's ESLint config no longer allows dependency group comments.

--- packages/components/src/utils/test/strings.js
+++ packages/components/src/utils/test/strings.js
@@ -1,6 +1,3 @@
-/**
- * Internal dependencies
- */
 import { normalizeTextString } from '../strings';
 
 describe( 'normalizeTextString', () => {
--- packages/string/src/test/index.js
+++ packages/string/src/test/index.js
@@ -1,6 +1,3 @@
-/**
- * Internal dependencies
- */
 import { kebabCase } from '..';
 
 describe( 'kebabCase', () => {
