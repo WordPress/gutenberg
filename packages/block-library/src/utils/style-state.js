fe08d0a19c73088d3478eb0c98a25945726589d0
Shail Mehta <shailmehta25@gmail.com>
2026-08-10 21:32:46 +0530

    Fix Lint

--- packages/block-library/src/utils/style-state.js
+++ packages/block-library/src/utils/style-state.js
@@ -181,13 +181,13 @@ export function resetStateDimensions( style, selectedState, keys ) {
  * style slice; using the state-aware path again nests the viewport key.
  *
  * @param {Object}   options
- * @param {Object}   [options.attributes]               Block attributes.
- * @param {Object}   [options.style]                    Style object to reset. Defaults to `attributes.style`.
- * @param {Object}   options.selectedState               Currently selected viewport/pseudo style state.
- * @param {boolean}  options.hasSelectedStyleState       Whether a style state is currently selected.
- * @param {boolean}  [options.persistAspectRatioDefault] Store cleared aspect ratio as `auto`.
- * @param {string[]} options.keys                        Dimension keys to reset.
- * @param {Object}   [options.defaultAttributes]         Attribute defaults to merge in when no style state is selected.
+ * @param {Object}   [options.attributes]                Block attributes.
+ * @param {Object}   [options.style]                     Style object to reset. Defaults to `attributes.style`.
+ * @param {Object}   options.selectedState                Currently selected viewport/pseudo style state.
+ * @param {boolean}  options.hasSelectedStyleState        Whether a style state is currently selected.
+ * @param {boolean}  [options.persistAspectRatioDefault]  Store cleared aspect ratio as `auto`.
+ * @param {string[]} options.keys                         Dimension keys to reset.
+ * @param {Object}   [options.defaultAttributes]          Attribute defaults to merge in when no style state is selected.
  */
 export function getDimensionResetAttributes( {
 	attributes = {},
