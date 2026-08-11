4c3f3f4f15785210170beddc9f946e74f31bd679
André <583546+oandregal@users.noreply.github.com>
2026-08-11 22:23:02 +0200

    DataViews: vendor ValidatedCheckboxControl (#81435)
    Co-authored-by: oandregal <oandregal@git.wordpress.org>
    Co-authored-by: jorgefilipecosta <jorgefilipecosta@git.wordpress.org>
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>

--- packages/dataviews/src/components/validated-form-controls/index.ts
+++ packages/dataviews/src/components/validated-form-controls/index.ts
@@ -1,4 +1,5 @@
 export { ControlWithError } from './control-with-error';
+export { ValidatedCheckboxControl } from './checkbox-control';
 export { ValidatedNumberControl } from './number-control';
 export { ValidatedSelectControl } from './select-control';
 export type { ValidatedControlProps } from './types';
