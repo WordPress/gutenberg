ac16ce2d1a370265ab96716149f9931c913b2684
Marco Ciampini <marco.ciampo@gmail.com>
2026-08-12 20:00:30 +0200

    Test: Reuse shared development flag

--- test/unit/config/setup-globals.vitest.js
+++ test/unit/config/setup-globals.vitest.js
@@ -181,10 +181,6 @@ globalThis.wpVitest = {
 	mockVisibleElements,
 };
 
-// Run all tests with development tools enabled.
-// eslint-disable-next-line @wordpress/wp-global-usage
-globalThis.SCRIPT_DEBUG = true;
-
 if ( typeof globalThis.window !== 'undefined' ) {
 	globalThis.window.tinyMCEPreInit = {
 		baseURL: 'about:blank',
