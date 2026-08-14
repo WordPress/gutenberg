f36f2bb2f307de2ba584c37ce3ea5de1045b9566
Riad Benguella <benguella@gmail.com>
2026-08-14 13:35:43 +0200

    Block API: Declare block keyboard shortcuts on variations and transforms (#81588)
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: scruffian <scruffian@git.wordpress.org>

--- packages/edit-post/src/components/layout/index.js
+++ packages/edit-post/src/components/layout/index.js
@@ -25,7 +25,6 @@ import { chevronDown, chevronUp } from '@wordpress/icons';
 import { SnackbarNotices, store as noticesStore } from '@wordpress/notices';
 import { store as preferencesStore } from '@wordpress/preferences';
 import { privateApis as commandsPrivateApis } from '@wordpress/commands';
-import { privateApis as blockLibraryPrivateApis } from '@wordpress/block-library';
 import { addQueryArgs } from '@wordpress/url';
 import { decodeEntities } from '@wordpress/html-entities';
 import { store as coreStore } from '@wordpress/core-data';
@@ -62,7 +61,6 @@ const { useCommandContext } = unlock( commandsPrivateApis );
 const { useDrag } = unlock( componentsPrivateApis );
 const { Editor, FullscreenMode, UploadProgressSnackbar } =
 	unlock( editorPrivateApis );
-const { BlockKeyboardShortcuts } = unlock( blockLibraryPrivateApis );
 const DESIGN_POST_TYPES = [
 	'wp_template',
 	'wp_template_part',
@@ -592,7 +590,6 @@ function Layout( {
 								<AutosaveMonitor />
 								<LocalAutosaveMonitor />
 								<EditPostKeyboardShortcuts />
-								<BlockKeyboardShortcuts />
 								{ currentPostType === 'wp_block' && (
 									<InitPatternModal />
 								) }
