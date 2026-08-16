ddb9fdf3431d05d27cb79c63b5b1aae40669997e
macayu17 <anayush1406@gmail.com>
2026-08-16 17:02:20 +0530

    Storybook: Show button text labels in docs

--- storybook/preview.jsx
+++ storybook/preview.jsx
@@ -9,6 +9,7 @@ import {
 import { WithGlobalCSS } from './decorators/with-global-css';
 import { WithMaxWidthWrapper } from './decorators/with-max-width-wrapper';
 import { WithRTL } from './decorators/with-rtl';
+import { WithButtonTextLabels } from './decorators/with-button-text-labels';
 import { WithDesignSystemTheme } from './decorators/with-design-system-theme';
 import { ComponentStatusIndicator } from './components/component-status-indicator';
 import theme from './theme';
@@ -65,6 +66,7 @@ export const globalTypes = {
 export const decorators = [
 	WithGlobalCSS,
 	WithRTL,
+	WithButtonTextLabels,
 	WithMaxWidthWrapper,
 	WithDesignSystemTheme,
 ];
