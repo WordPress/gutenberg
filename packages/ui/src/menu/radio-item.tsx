9d43db4a1abfdab91679d8e8ac2d06f835747236
Marco Ciampini <marco.ciampo@gmail.com>
2026-08-17 15:13:52 +0200

    Menu: Refine radio indicator and item layout

--- packages/ui/src/menu/radio-item.tsx
+++ packages/ui/src/menu/radio-item.tsx
@@ -1,13 +1,19 @@
 import { Menu as _Menu } from '@base-ui/react/menu';
 import clsx from 'clsx';
 import { forwardRef } from '@wordpress/element';
-import { check } from '@wordpress/icons';
+import { Circle, SVG } from '@wordpress/primitives';
 import { Icon } from '../icon';
 import resetStyles from '../utils/css/resets.module.css';
 import styles from './style.module.css';
 import { MenuItemContentContext } from './context';
 import { ItemContent, useItemContent } from './item';
 import type { RadioItemProps } from './types';
+
+const radioCheck = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Circle cx={ 12 } cy={ 12 } r={ 3 } />
+	</SVG>
+);
 
 /**
  * Renders a menu item that works like a radio button in a group.
@@ -52,7 +58,7 @@ const RadioItem = forwardRef< HTMLDivElement, RadioItemProps >(
 					keepMounted
 					className={ styles[ 'item-selection-indicator' ] }
 				>
-					<Icon icon={ check } size={ 24 } aria-hidden="true" />
+					<Icon icon={ radioCheck } size={ 24 } aria-hidden="true" />
 				</_Menu.RadioItemIndicator>
 				<MenuItemContentContext.Provider value={ contentContextValue }>
 					<ItemContent
