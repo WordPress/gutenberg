36235190cab8504021c81091ccf5c397c7ed173c
Joe Fusco <hello@josephfus.co>
2026-08-16 21:17:57 -0700

    Add blank line for coding standards

--- lib/experimental/collaboration/collaboration.php
+++ lib/experimental/collaboration/collaboration.php
@@ -80,6 +80,7 @@ if ( ! function_exists( 'gutenberg_register_collaboration_rest_routes' ) ) {
 		if ( ! $sync_storage instanceof WP_Sync_Storage ) {
 			$sync_storage = new WP_Sync_Post_Meta_Storage();
 		}
+
 		$sync_server  = new WP_HTTP_Polling_Sync_Server( $sync_storage );
 		$sync_server->register_routes();
 
