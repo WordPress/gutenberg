a8d3a0800831595b2f74f601e1d96f798b9f87cb
Lena Morita <lena@jaguchi.com>
2026-08-22 01:40:16 +0900

    Restore ValidatedInputControl export order in index.

--- packages/components/src/validated-form-controls/components/index.ts
+++ packages/components/src/validated-form-controls/components/index.ts
@@ -1,5 +1,5 @@
 export * from './custom-select-control';
+export * from './input-control';
 export * from './range-control';
 export * from './content-editable-control';
 export * from './textarea-control';
-export * from './input-control';
