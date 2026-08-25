58bc9d27b1cb39218c5594f8c4752379cf03d75d
ntsekouras <ntsekouras@outlook.com>
2026-08-25 10:33:40 +0300

    move to ui's InputControl

--- packages/ui/src/form/primitives/input/test/index.test.tsx
+++ packages/ui/src/form/primitives/input/test/index.test.tsx
@@ -10,39 +10,4 @@ describe( 'Input', () => {
 
 		expect( ref.current ).toBeInstanceOf( HTMLInputElement );
 	} );
-
-	describe( 'data-empty-value', () => {
-		it( 'flags an input with an empty controlled value', () => {
-			const ref = createRef< HTMLInputElement >();
-
-			render(
-				<Input ref={ ref } type="time" value="" onChange={ () => {} } />
-			);
-
-			expect( ref.current ).toHaveAttribute( 'data-empty-value' );
-		} );
-
-		it( 'does not flag an input with a value', () => {
-			const ref = createRef< HTMLInputElement >();
-
-			render(
-				<Input
-					ref={ ref }
-					type="time"
-					value="12:30"
-					onChange={ () => {} }
-				/>
-			);
-
-			expect( ref.current ).not.toHaveAttribute( 'data-empty-value' );
-		} );
-
-		it( 'does not flag an uncontrolled input', () => {
-			const ref = createRef< HTMLInputElement >();
-
-			render( <Input ref={ ref } type="time" /> );
-
-			expect( ref.current ).not.toHaveAttribute( 'data-empty-value' );
-		} );
-	} );
 } );
