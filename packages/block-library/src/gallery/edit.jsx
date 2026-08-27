e9784c1cdc045cbf9ceafccefb0c1d36022ec1cc
tellthemachines <isabel@tellthemachines.com>
2026-08-27 16:07:38 +1000

    shared isObject

--- packages/block-library/src/gallery/edit.jsx
+++ packages/block-library/src/gallery/edit.jsx
@@ -116,10 +116,6 @@ const PLACEHOLDER_TEXT = __(
 const DEFAULT_BLOCK = { name: 'core/image' };
 const EMPTY_ARRAY = [];
 
-function isObject( value ) {
-	return !! value && typeof value === 'object' && ! Array.isArray( value );
-}
-
 export default function GalleryEdit( props ) {
 	const {
 		setAttributes,
