a2f45b40ef2eb898119fd1013d4039eb55d3e651
George Mamadashvili <georgemamadashvili@gmail.com>
2026-09-02 12:49:21 +0400

    Adjust e2e tests

--- test/e2e/specs/editor/various/preview.spec.js
+++ test/e2e/specs/editor/various/preview.spec.js
@@ -374,7 +374,7 @@ test.describe( 'Preview with private custom post type', () => {
 		await page.getByRole( 'button', { name: 'View', exact: true } ).click();
 
 		await expect(
-			page.locator( 'role=menuitem[name="Preview in new tab"i]' )
+			page.getByRole( 'menuitem', { name: 'Preview in new tab' } )
 		).toBeHidden();
 	} );
 } );
