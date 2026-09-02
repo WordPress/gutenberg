d17783183a8d281e7dd4da0a6b4c346020025ca6
Lena Morita <lena@jaguchi.com>
2026-09-03 03:40:01 +0900

    UI: Import Vitest APIs in Radio tests

--- packages/ui/src/form/primitives/radio/test/index.jsdom.test.tsx
+++ packages/ui/src/form/primitives/radio/test/index.jsdom.test.tsx
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import { render } from '@testing-library/react';
 import { RadioGroup } from '@base-ui/react/radio-group';
 import { createRef } from '@wordpress/element';
