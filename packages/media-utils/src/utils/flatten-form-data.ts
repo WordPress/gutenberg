5b9c646d6ba72b4ed2728a6ec3226d88e4d3a2e0
antwonw <519934+antwonw@users.noreply.github.com>
2026-09-02 10:07:35 -0700

    Media Utils: Preserve arrays in multipart form data

--- packages/media-utils/src/utils/flatten-form-data.ts
+++ packages/media-utils/src/utils/flatten-form-data.ts
@@ -14,16 +14,20 @@ function isPlainObject( data: unknown ): data is Record< string, unknown > {
 /**
  * Recursively flatten data passed to form data, to allow using multi-level objects.
  *
- * @param {FormData}      formData Form data object.
- * @param {string}        key      Key to amend to form data object
- * @param {string|Object} data     Data to be amended to form data.
+ * @param {FormData} formData Form data object.
+ * @param {string}   key      Key to amend to form data object
+ * @param {unknown}  data     Data to be amended to form data.
  */
 export function flattenFormData(
 	formData: FormData,
 	key: string,
-	data: string | undefined | Record< string, string >
+	data: unknown
 ) {
-	if ( isPlainObject( data ) ) {
+	if ( Array.isArray( data ) ) {
+		for ( const [ index, value ] of data.entries() ) {
+			flattenFormData( formData, `${ key }[${ index }]`, value );
+		}
+	} else if ( isPlainObject( data ) ) {
 		for ( const [ name, value ] of Object.entries( data ) ) {
 			flattenFormData( formData, `${ key }[${ name }]`, value );
 		}
