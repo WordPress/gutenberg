22d9de053d9893c2ff961573acd0679208fc6868
antwonw <519934+antwonw@users.noreply.github.com>
2026-09-03 23:21:37 -0700

    Media Utils: Simplify multipart data flattening

--- packages/media-utils/src/utils/flatten-form-data.ts
+++ packages/media-utils/src/utils/flatten-form-data.ts
@@ -14,20 +14,16 @@ function isPlainObject( data: unknown ): data is Record< string, unknown > {
 /**
  * Recursively flatten data passed to form data, to allow using multi-level objects.
  *
- * @param {FormData} formData Form data object.
- * @param {string}   key      Key to amend to form data object
- * @param {unknown}  data     Data to be amended to form data.
+ * @param formData Form data object.
+ * @param key      Key to amend to form data object.
+ * @param data     Data to be amended to form data.
  */
 export function flattenFormData(
 	formData: FormData,
 	key: string,
 	data: unknown
 ) {
-	if ( Array.isArray( data ) ) {
-		for ( const [ index, value ] of data.entries() ) {
-			flattenFormData( formData, `${ key }[${ index }]`, value );
-		}
-	} else if ( isPlainObject( data ) ) {
+	if ( Array.isArray( data ) || isPlainObject( data ) ) {
 		for ( const [ name, value ] of Object.entries( data ) ) {
 			flattenFormData( formData, `${ key }[${ name }]`, value );
 		}
