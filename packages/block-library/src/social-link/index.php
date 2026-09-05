3b7ba436e57c0f2cf9448bf416fe5c642f1c2c26
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2026-09-05 15:48:24 +0530

    Remove invalid `@return` tag from the `block_core_social_link_get_services` hook docblock

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -349,7 +349,6 @@ function block_core_social_link_services( $service = '', $field = '' ) {
 	 * @since 6.9.0
 	 *
 	 * @param array $services_data The list of services. Each item is an array containing a 'name' and 'icon' key.
-	 * @return array The list of social services.
 	 */
 	$services_data = apply_filters( 'block_core_social_link_get_services', $services_data );
 
