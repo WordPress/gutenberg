3b3d533eced909918dddb57a152c8fc17a180bee
Nik Tsekouras <ntsekouras@outlook.com>
2026-09-05 11:45:27 +0300

    Query Loop: Fall back to `post` when the query has no `postType` (#82465)
    Co-authored-by: ntsekouras <ntsekouras@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: adamsilverstein <adamsilverstein@git.wordpress.org>
    Co-authored-by: jmccall75 <jmccallweb@git.wordpress.org>
    
    (cherry picked from commit b44a056ee1f5ae7000644ece47e872997766fd5d)

--- packages/block-library/src/query/edit/inspector-controls/index.js
+++ packages/block-library/src/query/edit/inspector-controls/index.js
@@ -49,7 +49,9 @@ export default function QueryInspectorControls( props ) {
 		orderBy,
 		author: authorIds,
 		pages,
-		postType,
+		// Match `build_query_vars_from_query_block()`, which queries posts when
+		// `query` has no post type.
+		postType = 'post',
 		perPage,
 		offset,
 		sticky,
@@ -179,7 +181,7 @@ export default function QueryInspectorControls( props ) {
 		isControlAllowed( allowedControls, 'excludeCurrent' );
 	const postTypeSingularName = useSelect(
 		( select ) =>
-			select( coreStore ).getPostType( postType )?.labels.singular_name,
+			select( coreStore ).getPostType( postType )?.labels?.singular_name,
 		[ postType ]
 	);
 
