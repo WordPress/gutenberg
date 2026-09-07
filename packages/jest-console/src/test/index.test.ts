c7ca88e1da4168fb6f966d3a190bd90daa8f8b91
Marco Ciampini <marco.ciampo@gmail.com>
2026-09-07 22:50:04 +0200

    Test: Switch Jest console lint suppression to Vitest

--- packages/jest-console/src/test/index.test.ts
+++ packages/jest-console/src/test/index.test.ts
@@ -138,7 +138,7 @@ describe( 'jest-console', () => {
 				// own lifecycle is defined to run before ours. Infer
 				// that we're being watched by testing the console
 				// method as being a spy.
-				// eslint-disable-next-line jest/no-standalone-expect
+				// eslint-disable-next-line vitest/no-standalone-expect
 				vitestExpect(
 					getSpy( methodName ).assertionsNumber
 				).toBeGreaterThanOrEqual( 0 );
