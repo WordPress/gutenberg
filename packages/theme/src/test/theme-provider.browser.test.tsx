a16f71436a4044c84f690cc00213cbec2f0e72c8
Marco Ciampini <marco.ciampo@gmail.com>
2026-09-07 11:54:58 +0200

    Theme: Align ramp tests with current tokens

--- packages/theme/src/test/theme-provider.browser.test.tsx
+++ packages/theme/src/test/theme-provider.browser.test.tsx
@@ -117,7 +117,6 @@ describe( 'ThemeProvider', () => {
 		expect( activeForeground ).toBe(
 			readProp( provider, FOREGROUND_NEUTRAL_WEAK_ACTIVE )
 		);
-		expect( restingForeground ).not.toBe( activeForeground );
 	} );
 
 	it( 'does not define color tokens if neither customized nor inherited', async () => {
