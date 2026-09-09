9951ba643e69e4863add7018de6a2afbe6d366df
Marco Ciampini <marco.ciampo@gmail.com>
2026-09-09 16:53:26 +0200

    Test: Restore Jest-only matcher imports

--- packages/block-library/src/utils/test/waveform-player.jsdom.test.jsx
+++ packages/block-library/src/utils/test/waveform-player.jsdom.test.jsx
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
 import { act, render } from '@testing-library/react';
 import { WaveformPlayer } from '../waveform-player';
 import {
--- packages/block-library/src/utils/test/waveform-utils.jsdom.test.js
+++ packages/block-library/src/utils/test/waveform-utils.jsdom.test.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
 import {
 	applyWaveformPlayerStyles,
 	createWaveformContainer,
