deb540a906b5138ee515c815495dc431abe3a383
Lena Morita <lena@jaguchi.com>
2026-09-11 19:46:23 +0900

    Select.Popup: Drop max-height when aligning with the trigger

--- packages/ui/src/form/primitives/select/positioner.tsx
+++ packages/ui/src/form/primitives/select/positioner.tsx
@@ -3,6 +3,7 @@ import { Select as _Select } from '@base-ui/react/select';
 import { forwardRef } from '@wordpress/element';
 import type { PositionerProps } from './types';
 import resetStyles from '../../../utils/css/resets.module.css';
+import itemPopupStyles from '../../../utils/css/item-popup.module.css';
 import styles from './style.module.css';
 import { ITEM_POPUP_POSITIONER_PROPS } from '../constants';
 
@@ -10,15 +11,21 @@ import { ITEM_POPUP_POSITIONER_PROPS } from '../constants';
  * Used to apply custom positioning to `Select`'s listbox content.
  */
 const Positioner = forwardRef< HTMLDivElement, PositionerProps >(
-	function SelectPositioner( { className, ...props }, ref ) {
+	function SelectPositioner(
+		{ className, alignItemWithTrigger = true, ...props },
+		ref
+	) {
 		return (
 			<_Select.Positioner
 				{ ...ITEM_POPUP_POSITIONER_PROPS }
 				{ ...props }
+				alignItemWithTrigger={ alignItemWithTrigger }
 				ref={ ref }
 				className={ clsx(
 					resetStyles[ 'box-sizing' ],
 					styles.positioner,
+					alignItemWithTrigger &&
+						itemPopupStyles[ 'is-align-item-with-trigger' ],
 					className
 				) }
 			/>
