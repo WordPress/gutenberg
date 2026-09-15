d0326e211bbee5a680e02d4eecd2eaf8096c00a8
Lena Morita <lena@jaguchi.com>
2026-09-15 17:07:16 +0900

    UI: Align Select popup checkmarks with the first label line

--- packages/ui/src/form/primitives/combobox/item.tsx
+++ packages/ui/src/form/primitives/combobox/item.tsx
@@ -22,15 +22,20 @@ export const Item = forwardRef< HTMLDivElement, ComboboxItemProps >(
 				ref={ ref }
 				{ ...restProps }
 			>
-				<Icon
-					icon={ variant === 'creatable' ? plus : check }
-					className={ clsx(
-						itemPopupStyles[ 'item-icon' ],
-						variant !== 'creatable' &&
-							itemPopupStyles[ 'item-indicator-icon' ]
-					) }
-					size={ 24 }
-				/>
+				<span
+					className={ itemPopupStyles[ 'item-icon' ] }
+					aria-hidden="true"
+				>
+					<Icon
+						icon={ variant === 'creatable' ? plus : check }
+						className={
+							variant !== 'creatable'
+								? itemPopupStyles[ 'item-indicator-icon' ]
+								: undefined
+						}
+						size={ 24 }
+					/>
+				</span>
 				{ children }
 			</_Combobox.Item>
 		);
