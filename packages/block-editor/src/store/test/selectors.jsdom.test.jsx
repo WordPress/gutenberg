a68183cdce21cd8af2920d2115f674c84335cbfe
Marco Ciampini <marco.ciampo@gmail.com>
2026-09-17 18:23:31 +0200

    Block editor: Isolate mutable test fixtures

--- packages/block-editor/src/store/test/selectors.jsdom.test.jsx
+++ packages/block-editor/src/store/test/selectors.jsdom.test.jsx
@@ -4658,64 +4658,67 @@ describe( 'selectors', () => {
 	} );
 
 	describe( 'getActiveBlockIdByBlockName', () => {
-		const state = {
-			selection: {
-				selectionStart: {
-					clientId: 'client-id-04',
+		let state;
+		beforeEach( () => {
+			state = {
+				selection: {
+					selectionStart: {
+						clientId: 'client-id-04',
+					},
+					selectionEnd: {
+						clientId: 'client-id-04',
+					},
 				},
-				selectionEnd: {
-					clientId: 'client-id-04',
+				blocks: {
+					parents: new Map(
+						Object.entries( {
+							'client-id-01': '',
+							'client-id-02': 'client-id-01',
+							'client-id-03': 'client-id-02',
+							'client-id-04': 'client-id-03',
+							'client-id-05': 'client-id-03',
+						} )
+					),
+					byClientId: new Map(
+						Object.entries( {
+							'client-id-01': {
+								clientId: 'client-id-01',
+								name: 'core/columns',
+							},
+							'client-id-02': {
+								clientId: 'client-id-02',
+								name: 'core/navigation',
+							},
+							'client-id-03': {
+								clientId: 'client-id-03',
+								name: 'core/navigation-link',
+							},
+							'client-id-04': {
+								clientId: 'client-id-04',
+								name: 'core/navigation-link',
+							},
+							'client-id-05': {
+								clientId: 'client-id-05',
+								name: 'core/navigation-link',
+							},
+						} )
+					),
+					cache: {
+						'client-id-01': {},
+						'client-id-02': {},
+						'client-id-03': {},
+						'client-id-04': {},
+						'client-id-05': {},
+					},
+					order: new Map(
+						Object.entries( {
+							'client-id-03': [ 'client-id-04', 'client-id-05' ],
+						} )
+					),
+					controlledInnerBlocks: new Set(),
 				},
-			},
-			blocks: {
-				parents: new Map(
-					Object.entries( {
-						'client-id-01': '',
-						'client-id-02': 'client-id-01',
-						'client-id-03': 'client-id-02',
-						'client-id-04': 'client-id-03',
-						'client-id-05': 'client-id-03',
-					} )
-				),
-				byClientId: new Map(
-					Object.entries( {
-						'client-id-01': {
-							clientId: 'client-id-01',
-							name: 'core/columns',
-						},
-						'client-id-02': {
-							clientId: 'client-id-02',
-							name: 'core/navigation',
-						},
-						'client-id-03': {
-							clientId: 'client-id-03',
-							name: 'core/navigation-link',
-						},
-						'client-id-04': {
-							clientId: 'client-id-04',
-							name: 'core/navigation-link',
-						},
-						'client-id-05': {
-							clientId: 'client-id-05',
-							name: 'core/navigation-link',
-						},
-					} )
-				),
-				cache: {
-					'client-id-01': {},
-					'client-id-02': {},
-					'client-id-03': {},
-					'client-id-04': {},
-					'client-id-05': {},
-				},
-				order: new Map(
-					Object.entries( {
-						'client-id-03': [ 'client-id-04', 'client-id-05' ],
-					} )
-				),
-				controlledInnerBlocks: new Set(),
-			},
-		};
+			};
+		} );
 		it( 'Should return first active matching block (including self) when single block selected', () => {
 			expect(
 				getActiveBlockIdByBlockNames( state, [
