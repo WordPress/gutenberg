fd07072f8c3fd0bfd76cf0e8732497387b82f778
Lena Morita <lena@jaguchi.com>
2026-09-19 03:24:54 +0900

    UI: Restore Switch Field labeling note

--- packages/ui/src/form/primitives/switch/switch.tsx
+++ packages/ui/src/form/primitives/switch/switch.tsx
@@ -8,6 +8,8 @@ import type { SwitchProps } from './types';
 /**
  * A low-level switch primitive.
  *
+ * Use `Field` to associate an accessible label.
+ *
  * Prefer `SwitchControl` for labeled items.
  */
 export const Switch = forwardRef< HTMLSpanElement, SwitchProps >(
