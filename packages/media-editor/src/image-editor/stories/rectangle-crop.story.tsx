088fac43c1dfdd590a628343d1067bd4c9a76ae4
Lena Morita <lena@jaguchi.com>
2026-09-21 20:30:09 +0900

    Prevent Storybook-only styles from leaking between stories.
    Side-effect CSS imports stay in the preview document after you leave a story, so convert those sheets to CSS modules with hashed class names.

--- packages/media-editor/src/image-editor/stories/rectangle-crop.story.tsx
+++ packages/media-editor/src/image-editor/stories/rectangle-crop.story.tsx
@@ -40,7 +40,7 @@ import {
 	getVisibleBounds,
 } from '../core/camera';
 import { getSourceRegion } from '../core/source-region';
-import './style.css';
+import styles from './style.module.css';
 
 const SAMPLE_IMAGE = 'image-editor-demo.jpeg';
 
@@ -135,7 +135,7 @@ const DefaultComponent = () => {
 
 	return (
 		<div>
-			<div className="image-editor-story__container">
+			<div className={ styles.container }>
 				<Cropper
 					src={ SAMPLE_IMAGE }
 					controller={ controller }
@@ -320,9 +320,9 @@ const WithControlsComponent = () => {
 				type="file"
 				accept="image/*"
 				onChange={ handleFileChange }
-				className="image-editor-story__hidden-file"
+				className={ styles[ 'hidden-file' ] }
 			/>
-			<div className="image-editor-story__toolbar">
+			<div className={ styles.toolbar }>
 				<Flex align="center" gap={ 2 } wrap>
 					<FlexItem>
 						<Button
@@ -440,7 +440,7 @@ const WithControlsComponent = () => {
 						</Button>
 					</FlexItem>
 				</Flex>
-				<div className="image-editor-story__sliders">
+				<div className={ styles.sliders }>
 					<RangeControl
 						label="Fine rotation"
 						min={ -MAX_ROTATION_OFFSET }
@@ -460,7 +460,7 @@ const WithControlsComponent = () => {
 				</div>
 			</div>
 
-			<div className="image-editor-story__resizable">
+			<div className={ styles.resizable }>
 				<Cropper
 					src={ src }
 					controller={ controller }
@@ -491,7 +491,7 @@ const WithControlsComponent = () => {
 
 			<div style={ { marginTop: 16 } }>
 				<strong>Current State:</strong>
-				<pre className="image-editor-story__state">
+				<pre className={ styles.state }>
 					{ JSON.stringify(
 						{
 							rotation: state.rotation,
@@ -737,9 +737,9 @@ const DebugComponent = () => {
 				type="file"
 				accept="image/*"
 				onChange={ handleFileChange }
-				className="image-editor-story__hidden-file"
+				className={ styles[ 'hidden-file' ] }
 			/>
-			<div className="image-editor-story__toolbar">
+			<div className={ styles.toolbar }>
 				<Flex align="center" gap={ 2 } wrap>
 					<FlexItem>
 						<Button
@@ -858,7 +858,7 @@ const DebugComponent = () => {
 						</Button>
 					</FlexItem>
 				</Flex>
-				<div className="image-editor-story__sliders">
+				<div className={ styles.sliders }>
 					<RangeControl
 						label="Fine rotation"
 						min={ -MAX_ROTATION_OFFSET }
@@ -885,7 +885,7 @@ const DebugComponent = () => {
 					ref={ containerRef }
 					style={ { flex: '1 1 60%', minWidth: 0 } }
 				>
-					<div className="image-editor-story__container">
+					<div className={ styles.container }>
 						<Cropper
 							src={ src }
 							controller={ controller }
@@ -907,10 +907,10 @@ const DebugComponent = () => {
 						maxHeight: 500,
 					} }
 				>
-					<div className="image-editor-story__export-preview">
+					<div className={ styles[ 'export-preview' ] }>
 						{ previewSrc ? (
 							<img
-								className="image-editor-story__export-image"
+								className={ styles[ 'export-image' ] }
 								src={ previewSrc }
 								alt="Crop preview"
 							/>
