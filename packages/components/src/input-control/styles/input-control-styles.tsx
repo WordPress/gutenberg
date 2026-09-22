1550c7bc1dc969ccaedd43fafa2477d6503c27e5
Lena Morita <lena@jaguchi.com>
2026-09-22 11:10:53 +0900

    InputControl: Darken the field border on hover.
    Match the active stroke used by the design system input layout, including SelectControl and CustomSelectControl. Disabled and borderless fields keep their resting border.

--- packages/components/src/input-control/styles/input-control-styles.tsx
+++ packages/components/src/input-control/styles/input-control-styles.tsx
@@ -62,6 +62,18 @@ const backdropDisabledStyles = ( {
 	`;
 };
 
+const backdropHoverStyles = ( { disabled, isBorderless }: BackdropProps ) => {
+	if ( disabled || isBorderless ) {
+		return undefined;
+	}
+
+	return css`
+		.components-input-control__container:hover &&& {
+			border-color: ${ COLORS.ui.borderActive };
+		}
+	`;
+};
+
 export const BackdropUI = styled.div< BackdropProps >`
 	&&& {
 		box-sizing: border-box;
@@ -81,6 +93,8 @@ export const BackdropUI = styled.div< BackdropProps >`
 		${ rtl( { paddingLeft: 2 } ) }
 		${ backdropDisabledStyles }
 	}
+
+	${ backdropHoverStyles }
 `;
 
 export const Root = styled( Flex )`
